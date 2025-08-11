e57000b9cba44397bf38dc2ab4db61eff2b60802
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:46 +0300

    Add Content Management/clanweb-1.3.4/_inc/bottom.inc.php

---
+++ Content Management/clanweb-1.3.4/_inc/bottom.inc.php
@@ -0,0 +1,7 @@
+
+
+<!-- 	<?=$lang_copy?> // -->
+   <div class="footer"><span class="copy">Powered by ClanWeb v.<?php echo $script;?> &copy; <a href="http://www.clanadmintools.com/" target="_blank">ClanAdmin Tools</a> 2003-2005
+   <br/> <?=$lang_contact?> </span></div>
+  </body>
+</html>
