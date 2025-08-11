41b83a951f8869335ba3a05e5c9ee0b5b9462404
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:33 +0300

    Add Content Management/clanweb-1.3.4/lang/se/lang_main.php

---
+++ Content Management/clanweb-1.3.4/lang/se/lang_main.php
@@ -0,0 +1,136 @@
+<?php
+//	-----------------------------------------
+// 	$File: lang_main.php
+// 	-----------------------------------------
+// 	$Copyright: (c) ClanAdmin Tools 2003-2005
+// 	$Last modified: 2004-08-28
+// 	$email: support@clanadmintools.com
+// 	$License: GPL - See LICENSE for more info
+//	-----------------------------------------
+/*********************************************************
+	Language file to (c)ClanAdmin Tools(CAT): Version 1.3.3
+ 	Language: Swedish
+	By: ArreliuS
+	Mail: arrelius@error-404.se
+
+	Last modified: 2004-08-28
+**********************************************************/
+
+//Meny
+$lang_main = "Huvudsida";
+$lang_news = "Nyhet";
+$lang_crew = "Medlemmar";
+$lang_reported = "Anmälda inlägg";
+$lang_admins = "Administratörer";
+$lang_match = "Matcher";
+$lang_support = "Stöd CAT";
+$lang_faq = "FAQ";
+$lang_upload = "Ladda upp fil";
+$lang_browse_files = "Visa filer";
+$lang_motd = "MOTD";
+$lang_logout = "Logga ut";
+
+//Funktioner
+$lang_login = "Logga in";
+$lang_add_news = "Lägg till nyhet";
+$lang_add_member = "Lägg till medlem";
+$lang_add_match = "Lägg till match";
+$lang_add_user = "Lägg till användare";
+$lang_add_motd = "Lägg till MOTD";
+$lang_add_admin = "Lägg till administratör";
+$lang_edit_admin = "Redigera administratör";
+$lang_edit_news = "Redigera nyhet";
+$lang_edit_member = "Redigera medlem";
+$lang_edit_members = "Redigera medlemmar";
+$lang_edit_match = "Redigera match";
+$lang_edit_user = "Redigera användare";
+$lang_delete_news = "Radera nyhet";
+$lang_delete_member = "Radera medlem";
+$lang_delete_match = "Radera match";
+$lang_delete_user = "Radera användare";
+$lang_delete_admin = "Radera administratör";
+$lang_delete_comment = "Radera kommentar";
+$lang_headadmin = "Huvudadministratör";
+$lang_upload_file = "Ladda upp fil";
+$lang_file_uploaded = "Filen har blivit uppladdad";
+$lang_deleted = "Raderad";
+$lang_comment_deleted = "Kommentaren har blivit raderad av administratören";
+
+$lang_username = "Användarnamn";
+$lang_password = "Lösenord";
+$lang_profile = "Profil";
+$lang_name = "Namn";
+$lang_picture = "Bild";
+$lang_age = "Ålder";
+$lang_quote = "Citat";
+$lang_sex = "Kön";
+$lang_work = "Sysselsättning";
+$lang_location = "Bor";
+$lang_position = "Uppgift";
+$lang_cpu = "Processor";
+$lang_mouse ="Mus";
+$lang_mousepad ="Musmatta";
+$lang_screen = "Skärm";
+$lang_os = "Operativsystem";
+$lang_gfx ="Grafikkort";
+$lang_memory = "Minne";
+$lang_hdd = "Hårddisk";
+$lang_resolution = "Upplösning";
+$lang_email = "E-mail";
+
+$lang_team_one = "Lag Ett";
+$lang_team_two = "Lag Två";
+$lang_type = "Speltyp";
+$lang_point_one = "Poäng för lag ett";
+$lang_point_two = "Poäng för lag två";
+$lang_map ="Karta/Kartor";
+$lang_lineup = "Laguppställning";
+$lang_report = "Rapport";
+
+$lang_topic ="Rubrik";
+$lang_enter_news = "Skriv in nyhet";
+$lang_date = "Datum";
+$lang_approve = "Godkänn";
+$lang_delete = "Radera";
+
+$lang_member_deleted = "Medlemmen har blivit raderad!";
+$lang_news_deleted = "Nyheten har blivit raderad!";
+$lang_match_deleted = "Matchen har blivit raderad!";
+$lang_user_deleted = "Användaren har blivit raderad!";
+$lang_comment_removed = "Kommentaren har blivit raderad.";
+
+$lang_member_updated = "Medlemmen har blivit uppdaterad";
+$lang_news_updated = "Nyheten har blivit uppdaterad";
+$lang_match_updated = "Matchen har blivit uppdaterad";
+$lang_user_updated = "Användaren har blivit uppdaterad";
+
+$lang_member_added = "Medlemmen har blivit tillagd";
+$lang_news_added = "Nyheten har blivit tillagd";
+$lang_match_added = "Matchen har blivit tillagd";
+$lang_user_added = "Användaren har blivit tillagd";
+$lang_motd_added = "MOTD har blivit tillagd";
+$lang_comment_added = "Din kommentar är registrerad.";
+$lang_comment_approved = "Kommentaren har blivit godkänd";
+
+// Error messages
+$lang_wrong = "Du skrev in ett ogiltligt användarnamn och/eller lösenord. Försök igen.";
+$lang_error = "Ett fel uppstod. Var god försök igen.";
+$lang_denied = "Du saknar behörighet att visa denna sidan!";
+
+
+//Information
+$lang_contact = "Frågor,kommentarer och tips skickas till <a href=\"mailto:support@clanadmintools.com\" target=\"_blank\">support@clanadmintools.com</a>";
+$lang_copy = "ClanAdmin Tools ,(CAT), är fritt att använda, modifiera och vidardistruburera så länge copyrightkoden förblir intakt. Var god och respektera detta.\n 	ClanAdmin Tools ,(CAT), är skapat av ArreliuS för e-Sport klaner över hela Sverige.\n Använd detta scriptet till vilket annat syfte som helst så länge (c)-koden förblir intakt och att ni inte bryter emot licensavtalet som medföljer denna applikation.\n ClanAdmin Tools ,(CAT), distribureras av Error 404.\n Buggrapportering sker till support@clanadmintools.com, för att ladda hem den senaste versionen av ClanAdmin Tools var god besök http://www.clanadmintools.com/ för alla de senaste uppdateringarna.\n All form av support/förslag/kommentarer etc sker genom mail eller genom forumet.";
+$lang_publish_info = "<i>Kodat av ArreliuS 2003. Notera att n&auml;r du trycker p&aring; l&auml;gg till-knappen s&aring; accepterar du automatiskt att informationen publiceras på nätet. K&auml;nner du att du inte kan acceptera detta var god och kontakta en admin. Jag (ArreliuS) kan inte hållas ansvarig för vad detta scriptet används till. Så om du upptäcker att detta scriptet används till olagliga personregister så kontakta polismyndigheten för detta.Det hjälper inte att maila mig i dessa fallen. </i>"; 
+$lang_loggedout = "Du är nu utloggad. Välkommen åter!";
+$lang_motd_title ="Meddelande för dagen";
+$lang_admin_notice = "Kryssa bara i denna boxen om du vill ge användaren rättigheter att lägga till, redigera och radera andra användare.";
+
+//installera
+$lang_install = "Installera";
+$lang_update = "Uppdatera";
+
+
+// EoF
+
+?>
