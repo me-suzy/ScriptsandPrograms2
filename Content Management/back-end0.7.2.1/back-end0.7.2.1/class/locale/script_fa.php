e46592121b35e477a2a4a8c812ee8098c6cfed64
me-suzy <me.suzana@gmail.com>
2025-08-11 17:42:56 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/class/locale/script_fa.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/class/locale/script_fa.php
@@ -0,0 +1,10 @@
+<?php
+// Details of script properties for Persian
+   $_BE['script']['fa'] = array(
+      'lang'   => 'fa',
+      'charset'=> 'UTF-8',
+      'left'   => 'right',
+      'right'  => 'left',
+      'dir'    => 'rtl'
+       );
+?>
