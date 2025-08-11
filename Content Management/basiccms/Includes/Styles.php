a0a04429dcdf65537b83218c5a18bed1ae142c6c
me-suzy <me.suzana@gmail.com>
2025-08-11 17:59:19 +0300

    Add Content Management/basiccms/Includes/Styles.php

---
+++ Content Management/basiccms/Includes/Styles.php
@@ -0,0 +1,55 @@
+<STYLE TYPE="text/css">
+	BODY {
+		background-color: #FFFFFF;
+		Font-family: Arial, Helvetica, sans-serif;
+	}
+	.cssBorder {
+      	BORDER-BOTTOM: black 1px solid;
+      	BORDER-LEFT: black 1px solid;
+      	BORDER-RIGHT: black 1px solid;
+      	BORDER-TOP: black 1px solid;
+      	PADDING-LEFT: 2px;
+	}
+	#tableback {
+		border-color:#FFFFFF;
+		border-style: solid dotted;
+	#iframecolor {
+		background-color:#FFFFFF;
+		COLOR:#FFFFFF;
+	}
+	.button {
+		BORDER-RIGHT: #666666 1px solid;
+		BORDER-TOP: #666666 1px solid;
+		FONT-SIZE: 11px;
+		BORDER-LEFT: #666666 1px solid;
+		WIDTH: 90px;
+		CURSOR: hand;
+		COLOR: #555555;
+		BORDER-BOTTOM: #666666 1px solid;
+		FONT-FAMILY: Verdana,  Arial, Helvetica, sans-serif;
+		BACKGROUND-COLOR: #dadeeb
+	}
+	.buttonover {
+		BORDER-RIGHT: #666666 1px solid;
+		BORDER-TOP: #666666 1px solid;
+		FONT-SIZE: 11px;
+		BACKGROUND: #b0c4de;
+		BORDER-LEFT: #666666 1px solid;
+		WIDTH: 90px;
+		CURSOR: hand;
+		COLOR: #555555;
+		BORDER-BOTTOM: #666666 1px solid;
+		FONT-FAMILY: Verdana,  Arial, Helvetica, sans-serif
+	}
+	.smbutton {
+		BORDER-RIGHT: #666666 1px solid;
+		BORDER-TOP: #666666 1px solid;
+		FONT-SIZE: 8pt;
+		BORDER-LEFT: #666666 1px solid;
+		CURSOR: hand;
+		COLOR: #000000;
+		BORDER-BOTTOM: #666666 1px solid;
+		FONT-FAMILY: Verdana,  Arial, Helvetica, sans-serif;
+		BACKGROUND-COLOR: #d4d0c8
+	}
+</STYLE>
