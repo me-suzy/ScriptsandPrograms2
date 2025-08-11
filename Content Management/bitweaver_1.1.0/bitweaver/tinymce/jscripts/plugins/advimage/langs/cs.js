f8ac997228e4460fff06556451b1c05151f8c2f4
me-suzy <me.suzana@gmail.com>
2025-08-11 18:48:10 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/advimage/langs/cs.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/advimage/langs/cs.js
@@ -0,0 +1,6 @@
+// UK lang variables
+
+tinyMCELang['lang_insert_image_alt2'] = 'Název obrázku';
+tinyMCELang['lang_insert_image_onmousemove'] = 'Alternativní obrázek'
+tinyMCELang['lang_insert_image_mouseover'] = 'pøi najetí myi';
+tinyMCELang['lang_insert_image_mouseout'] = 'pøi odjetí myi';
