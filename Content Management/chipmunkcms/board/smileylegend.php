9a011f1ed1bd16251bb6d2eb7a52490cbc1063b5
me-suzy <me.suzana@gmail.com>
2025-08-11 19:45:13 +0300

    Add Content Management/chipmunkcms/board/smileylegend.php

---
+++ Content Management/chipmunkcms/board/smileylegend.php
@@ -0,0 +1,9 @@
+:) = <img src="images/smile.gif"><br>
+   :) = <img src='images/smile.gif'><br>
+    :( = <img src='images/sad.gif'><br>
+    :p = <img src='images/tongue.gif'><br>
+    ;) = <img src='images/wink.gif'><br>
+    ;smirk = <img src='images/smirk.gif'><br>
+    :blush = <img src='images/blush.gif'><br>
+    :angry = <img src='images/angry.gif'><br>
+    :o =     <img src='images/shocked.gif'><br>
