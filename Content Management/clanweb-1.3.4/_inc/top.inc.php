a07984505e62a69917441357fdb45d739eed103d
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:51 +0300

    Add Content Management/clanweb-1.3.4/_inc/top.inc.php

---
+++ Content Management/clanweb-1.3.4/_inc/top.inc.php
@@ -0,0 +1,30 @@
+<html>
+ <head>
+  <title>ClanAdmin Tools <?php echo $script;?></title>
+  <link rel="stylesheet" href="./css/style.css" type="text/css"/>
+<script type="text/javascript">
+<!--
+function MM_openBrWindow(theURL,winName,features) { 
+  window.open(theURL,winName,features);
+}
+//-->
+</script>
+<style type="text/css">
+<!--
+form input:focus, form textarea:focus{
+background-color: #EAEBFF;
+
+}
+form input, form textarea{
+     border: 1px solid #BEB882;
+     	     -moz-border-radius: 4px;
+	     }
+.button { background-color: white; font-size: 10px; }
+-->
+</style>
+<meta name="ROBOTS" content="NOINDEX, FOLLOW" />
+ </head>
+  <body>
+  <img src="gfx/logo_header.jpg" style="position: absolute; left: 0px; top: 0px;"/> 
+   <?php require('menu.php'); ?>
+   
