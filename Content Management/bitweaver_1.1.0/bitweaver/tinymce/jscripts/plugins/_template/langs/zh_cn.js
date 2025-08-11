d179e5a1c99f780c7775c0e9413d445f83d99db7
me-suzy <me.suzana@gmail.com>
2025-08-11 19:04:15 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/_template/langs/zh_cn.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/_template/langs/zh_cn.js
@@ -0,0 +1,7 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+/* Remember to namespace the language parameters lang_<your plugin>_<some name> */
+
+tinyMCELang['lang_template_title'] = 'Õâ½öÊÇÄ£°åµÄµ¯³ö´°¿Ú';
+tinyMCELang['lang_template_desc'] = 'Õâ½öÊÇÄ£°åµÄ°´Å¥';
