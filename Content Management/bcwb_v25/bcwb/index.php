485b9bca0991eeec4f2ae23603cf7fa51339c9e2
me-suzy <me.suzana@gmail.com>
2025-08-11 17:59:29 +0300

    Add Content Management/bcwb_v25/bcwb/index.php

---
+++ Content Management/bcwb_v25/bcwb/index.php
@@ -0,0 +1,21 @@
+<?PHP
+/************************************************************************/
+/* BCWB: Business Card Web Builder                                      */
+/* ============================================                         */
+/*                                                                      */
+/* 	The author of this program code:                                    */
+/*  Dmitry Sheiko (sheiko@cmsdevelopment.com)	                    	*/
+/* 	Copyright by Dmitry Sheiko											*/
+/* 	http://bcwb.cmsdevelopment.com     			                        */
+/*                                                                      */
+/* This program is free software. You can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/************************************************************************/
+ 
+include ("config.inc.php");
+include($root_path.'include/startup.inc.php');
+
+$bcwb_data = new bcwb_data;
+@include($root_path.'include/startup/counter.inc.php');
+?>
