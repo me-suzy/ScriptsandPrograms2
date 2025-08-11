519c4be3a1cc760042e0cafaaa243d6816d0fd45
me-suzy <me.suzana@gmail.com>
2025-08-11 17:59:17 +0300

    Add Content Management/basiccms/Includes/PortalConection.php

---
+++ Content Management/basiccms/Includes/PortalConection.php
@@ -0,0 +1,9 @@
+<?php 
+ define("HOSTM", "localhost");
+ define("dbUser", "root");
+ define("dbPassword", "pass");
+define("dbToUse", "basiccms");
+ define("FromEMail", "example@example.com");
+define("ImageUploadPath","C:\inetpub\wwwroot\basiccms\Images");
+define("ImageUploadPathRelative","../../Images/");
+?>
