9f1aaafd0f5ee61b677637ad2b05f7517f9838d9
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:53 +0300

    Add Content Management/clanweb-1.3.4/_inc/top_1.inc.php

---
+++ Content Management/clanweb-1.3.4/_inc/top_1.inc.php
@@ -0,0 +1,20 @@
+<html>
+ <head>
+  <title>ClanWeb</title>
+  <link rel="stylesheet" href="./css/style.css" type="text/css" />
+  <style type="text/css">
+<!--
+form input:focus, form textarea:focus{
+background-color: #EAEBFF;
+
+}
+form input, form textarea{
+     border: 1px solid #BEB882;
+     	     -moz-border-radius: 4px;
+	     }
+.button { background-color: white; font-size: 10px; }
+-->
+</style>
+<meta name="ROBOTS" content="NOINDEX, FOLLOW" />
+ </head>
+  <body>
