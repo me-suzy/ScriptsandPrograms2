c38b4ffba70f1108e4095b3a6257074ec34eeb53
me-suzy <me.suzana@gmail.com>
2025-08-11 18:09:58 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/kernel/smarty_bit/block.tr.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/kernel/smarty_bit/block.tr.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * File:     block.translate.php
+ * Type:     block
+ * Name:     translate
+ * Purpose:  translate a block of text
+ * -------------------------------------------------------------
+ */
+//global $lang;
+//include_once('lang/language.php');
+function smarty_block_tr($params, $content, &$gBitSmarty)
+{
+	global $gBitLanguage, $gBitSystem;
+	echo( tra( $content ) );
+}
+?>
