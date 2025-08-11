f5971482c6574467a7ed1368d66eb17e128a1d5a
me-suzy <me.suzana@gmail.com>
2025-08-11 18:08:31 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/kernel/menu_register_inc.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/kernel/menu_register_inc.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_kernel/menu_register_inc.php,v 1.2.2.4 2005/09/08 12:57:28 squareing Exp $
+ * @package kernel
+ * @subpackage functions
+ *
+ * This file only needs to be called once, and only when you plan on rendering the app menu, or something similar.
+ */
+    global $gBitUser, $gBitSystem, $gBitSmarty;
+
+	// =========================== Global ===========================
+//	$gBitSystem->registerAppMenu( 'global', NULL, NULL, 'bitpackage:kernel/menu_global.tpl' );
+
+
+	// =========================== Smarty ===========================
+	array_multisort( $gBitSystem->mAppMenu );
+	$gBitSmarty->assign_by_ref('appMenu',$gBitSystem->mAppMenu );
+
+
+	// =========================== Admin menu ===========================
+	if( $gBitUser->isAdmin() ) {
+		$adminMenu = array();
+		foreach( array_keys( $gBitSystem->mPackages ) as $package ) {
+			$package = strtolower( $package );
+			$tpl = "bitpackage:$package/menu_".$package."_admin.tpl";
+			if( ($gBitSystem->isPackageActive( $package ) || $package == 'kernel') && @$gBitSmarty->template_exists( $tpl ) ) {
+				$adminMenu[$package]['tpl'] = $tpl;
+				$adminMenu[$package]['style'] = 'display:' . (empty($package) || (isset($_COOKIE[$package . 'admenu']) && ($_COOKIE[$package . 'admenu'] == 'o')) ? 'block;' : 'none;');
+			}
+		}
+		array_multisort( $adminMenu );
+		$gBitSmarty->assign_by_ref( 'adminMenu', $adminMenu );
+		$layoutstyle = 'display:'.((isset($_COOKIE['layoutadmenu']) && ($_COOKIE['layoutadmenu'] == 'o')) ? 'block;' : 'none;');
+		$gBitSmarty->assign_by_ref( 'layoutstyle', $layoutstyle );
+	}
+?>
