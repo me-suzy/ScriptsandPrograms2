c1650b051a975df87e22033e367548600c675a7f
me-suzy <me.suzana@gmail.com>
2025-08-11 19:35:24 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/wiki/modules/mod_breadcrumb.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/wiki/modules/mod_breadcrumb.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * $Header: /cvsroot/bitweaver/_bit_wiki/modules/mod_breadcrumb.php,v 1.1.1.1.2.2 2005/07/26 15:50:48 drewslater Exp $
+ *
+ * Copyright (c) 2004 bitweaver.org
+ * Copyright (c) 2003 tikwiki.org
+ * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ *
+ * $Id: mod_breadcrumb.php,v 1.1.1.1.2.2 2005/07/26 15:50:48 drewslater Exp $
+ * @package wiki
+ * @subpackage modules
+ */
+
+/**
+ * @param maxlen = max number of displayed characters for the page name
+ */
+if (!isset($_SESSION["breadCrumb"])) {
+	$_SESSION["breadCrumb"] = array();
+}
+$bbreadCrumb = array_reverse($_SESSION["breadCrumb"]);
+$gBitSmarty->assign('breadCrumb', $bbreadCrumb);
+$gBitSmarty->assign('maxlen', isset($module_params["maxlen"]) ? $module_params["maxlen"] : 0);
+?>
