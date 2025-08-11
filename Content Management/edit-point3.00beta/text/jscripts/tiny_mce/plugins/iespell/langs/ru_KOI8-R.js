14b1baf04630c51e1af760f57dc7f102aaaa0c8c
me-suzy <me.suzana@gmail.com>
2025-08-12 02:38:44 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/iespell/langs/ru_KOI8-R.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/iespell/langs/ru_KOI8-R.js
@@ -0,0 +1,7 @@
+// RU lang variables KOI8-R
+
+tinyMCE.addToLang('',{
+iespell_desc : 'úÁÐÕÓÔÉÔØ ÐÒÏ×ÅÒËÕ ÏÒÆÏÇÒÁÆÉÉ',
+iespell_download : "ieSpell ÎÅ ÏÂÎÁÒÕÖÅÎ. ÎÁÖÍÉÔÅ OK, ÞÔÏÂÙ ÐÅÒÅÊÔÉ ÎÁ ÓÔÒÁÎÉÃÕ ÚÁÇÒÕÚËÉ."
+});
+
