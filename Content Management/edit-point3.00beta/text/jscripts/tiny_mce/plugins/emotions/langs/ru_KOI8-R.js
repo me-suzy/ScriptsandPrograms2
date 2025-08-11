e0e2cf8880a616fc4682a6397c4c045ad792d93a
me-suzy <me.suzana@gmail.com>
2025-08-12 02:36:32 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/emotions/langs/ru_KOI8-R.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/emotions/langs/ru_KOI8-R.js
@@ -0,0 +1,22 @@
+// RU lang variables KOI8-R
+
+tinyMCE.addToLang('emotions',{
+title : '÷ÓÔÁ×ÉÔØ ÓÍÁÊÌÉË',
+desc : 'óÍÁÊÌÉËÉ',
+cool : 'Cool',
+cry : 'ðÌÁÞ',
+embarassed : 'Embarassed',
+foot_in_mouth : 'Foot in mouth',
+frown : 'îÁÈÍÕÒÅÎÎÏÓÔØ',
+innocent : 'ó×ÑÔÏÊ',
+kiss : 'ðÏÃÅÌÕÊ',
+laughing : 'óÍÅÈ',
+money_mouth : 'Money mouth',
+sealed : 'úÁËÌÅÅÎÎÙÊ',
+smile : 'õÌÙÂËÁ',
+surprised : 'óÀÒÐÒÉÚ',
+tongue_out : '÷ÙÓÕÎÕÔÙÊ ÑÚÙË',
+undecided : 'Undecided',
+wink : 'Wink',
+yell : 'Yell'
+});
