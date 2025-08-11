a306c5e42499be73cedba7611e1867c4e69aa990
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:28 +0300

    Add Content Management/clanweb-1.3.4/lang/se/faq.php

---
+++ Content Management/clanweb-1.3.4/lang/se/faq.php
@@ -0,0 +1,48 @@
+<?php
+/*********************************************************
+								faq.php
+								-----------
+					$Copyright: (c) ClanAdmin Tools 2003, 2004
+					$Created: 2004-07-14 by ArreliuS
+					$email: support@clanadmintools.com
+
+	   ClanAdmin Tools is free software; you can redistribute it and/or modify
+	   it under the terms of the GNU General Public License as published by
+	   the Free Software Foundation; either version 2 of the License, or
+	   (at your option) any later version.
+
+**********************************************************/
+/*********************************************************
+	Language file to (c)ClanAdmin Tools(CAT): Version 1.3.x
+ 	Language: swedish
+	By: ArreliuS
+	Mail: arrelius@error-404.se
+
+	Last modified: 2004-08-28
+**********************************************************/
+// To add a own faq item just write the which name its going to
+// be called by and what text it's going to contain.
+// ex. 'foo' => 'boo',
+
+//FAQ
+
+$faq = array(
+
+'Installation' => 'Installation',
+'Specs' => '<li>Vad krävs för att kunna använda clanadmin tools?</li>- Det krävs en webbserver som stödjer PHP - 4.0.5 eller bättre och MySQL - 3.23.32 eller bättre.',
+
+'Misc' => 'Blandade frågor',
+'How_to_add' => '<li>Hur lägger jag till nyheter,matcher osv?</li>- Detta är en väldigt enkel procedur. Det är bara att klicka på "Lägg till nyhet" längst upp och sedan fylla i formuläret och spara.',
+'How_to_edit' => '<li>Hur redigerar jag till nyheter,matcher osv?</li>- Det är bara att klicka på editera ikonen bredvid nyheten, matchen du vill redigera.',
+'Who' => '<li>Vem har gjort clanadmin tools?</li>- CAT är kodat och modifierat av Error 404.',
+'Purpose' => '<li>Vad kan jag göra med clanadmin tools?</li>- Med hjälp av ClanAdmin så kan du administrera just din klans hemsida. Du kan lägga till/redigera/ta bort nyheter från din sida och lägga till/redigera/ta bort medlemmar på ett smidigt sätt.',
+
+'Bugreport' => 'Felrapportering',
+'What_to_do' => '<li>Jag får upp en massa fel! Vad ska jag göra?</li>- Läs readme-filen för hur du ska gå tillväga.',
+'Mysql_prob' => '<li>Jag installerade allt korrekt men ändå så sparas inget i MySql-databasen.</li>-Kolla först om du kommer åt din mysql-databas via någon annan applikation. Om du gör det så bör du kontrollera cfg.php och kolla så att allt är korrekt.',
+
+'Develope' => 'Utveckla',
+'Idea' => '<li>Jag har ett par idéer till systemet som jag skulle vilja att ni tar er en titt på. </li>- Javisst, ideér och feedback är alltid välkommet. Maila oss eller skriv i vårt forum så återkommer vi.',
+);
+
+?>
