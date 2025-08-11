34b39252f286ff146ebfaa34c543317198ca3fa1
me-suzy <me.suzana@gmail.com>
2025-08-11 17:50:44 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/public_html/url_test.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/public_html/url_test.php
@@ -0,0 +1,8 @@
+<?php
+   echo "<ul>\n";
+   echo "  <li>This data is coming from url_test.php</li>\n";
+   echo "  <li><a href=http://www.odc.net/>odc.net</a></li>\n";
+   echo "  <li><a href=http://www.php.net/>php.net</a></li>\n";
+   echo "  <li><a href=http://www.phpslash.org/>phpslash.org</a></li>\n";
+   echo "</ul>\n";
+?>
