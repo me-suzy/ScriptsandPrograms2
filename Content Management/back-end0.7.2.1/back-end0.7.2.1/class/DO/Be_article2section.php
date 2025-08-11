2beb3e1a6d6c90e9dbe5e4fb8d7d3527e881aa48
me-suzy <me.suzana@gmail.com>
2025-08-11 17:38:52 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/class/DO/Be_article2section.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/class/DO/Be_article2section.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Table Definition for be_article2section
+ */
+global $_PSL;
+require_once($_PSL['classdir'] . '/BE_DataObject.class');
+
+class DO_Be_article2section extends BE_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    var $__table = 'be_article2section';              // table name
+    var $articleID;                       // int(5)  not_null multiple_key unsigned
+    var $sectionID;                       // int(5)  not_null multiple_key unsigned
+
+    /* ZE2 compatibility trick*/
+    function __clone() { return $this;}
+
+    /* Static get */
+    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DO_Be_article2section',$k,$v); }
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
