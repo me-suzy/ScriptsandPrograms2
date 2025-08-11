b132f191c76798d423769aa9115191d41cdbc912
me-suzy <me.suzana@gmail.com>
2025-08-11 20:08:55 +0300

    Add Content Management/cmscout1.00RC2/pm/drafts.php

---
+++ Content Management/cmscout1.00RC2/pm/drafts.php
@@ -0,0 +1,42 @@
+<?php
+/**************************************************************************
+    FILENAME        :   drafts.php
+    PURPOSE OF FILE :   Draft box for Personal Messengers
+    LAST UPDATED    :   12 August 2005
+    COPYRIGHT       :   © 2005 CMScout Group
+    WWW             :   www.cmscout.za.org
+    LICENSE         :   GPL vs2.0
+    
+    
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+**************************************************************************/
+?>
+<?php
+if (!defined('SCOUT_NUKE'))
+    die("You have accessed this page illegally, please go use the main menu");
+
+$username = $check['uname'];
+
+$sql = $data->select_query("pms", "WHERE fromuser='$username' AND type=4 ORDER BY date ASC");
+$numpm = $data->num_rows($sql);
+$inboxpm = array();
+while($inboxpm[] = $data->fetch_array($sql));
+
+$tpl->assign("pm", $inboxpm);
+$tpl->assign("numpm", $numpm);
+$tpl->assign("onpage", "Drafts");
+$pagenum = 1;
+?>
