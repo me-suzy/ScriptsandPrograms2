dacfecf483abca7197fd2722538a0b03c9a959f6
me-suzy <me.suzana@gmail.com>
2025-08-11 18:31:14 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/search/refresh.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/search/refresh.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * $Header: /cvsroot/bitweaver/_bit_search/refresh.php,v 1.1.1.1.2.5 2005/10/30 09:29:51 lsces Exp $
+ *
+ * Copyright (c) 2004 bitweaver.org
+ * Copyright (c) 2003 tikwiki.org
+ * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ *
+ * $Id: refresh.php,v 1.1.1.1.2.5 2005/10/30 09:29:51 lsces Exp $
+ * @author  Luis Argerich (lrargerich@yahoo.com)
+ * @package search
+ * @subpackage functions
+ */
+ 
+/**
+ * refresh_search_index
+ */
+function refresh_search_index() {
+	global $gBitSystem;
+	// first write close the session. refreshing can take a huge amount of time
+	session_write_close();
+
+	// check if we have to run. Run every n-th click:
+	global $search_refresh_rate, $gBitSystem;
+
+	//$search_refresh_rate=1; //debug
+	list($usec, $sec) = explode(" ",microtime());
+	srand (ceil($sec+100*$usec));
+	if(rand(1,$search_refresh_rate)==1) {
+		require_once('refresh_functions.php');
+		// get a random location
+		$locs=array();
+		if( $gBitSystem->isPackageActive( 'wiki' ) ) {
+			// if wiki is active, let's always refresh
+			random_refresh_index_wiki();
+		}
+		if( $gBitSystem->isPackageActive( 'articles' ) ) {
+			$locs[]="random_refresh_index_articles";
+		}
+		if( $gBitSystem->isPackageActive( 'blogs' ) ) {
+			$locs[]="random_refresh_index_blogs";
+			$locs[]="random_refresh_index_blog_posts";
+		}
+
+		// comments can be everywhere?
+		$locs[]="random_refresh_index_comments";
+		// some refreshes to enhance the refreshing stats
+		$locs[]="refresh_index_oldest";
+		//print_r($locs);
+		$location=$locs[rand(0,count($locs)-1)];
+		// random refresh
+
+		// hack around php database driver issues when a different database from bitweaver is accessed elsewhere during page  render
+		// this happens in the phpBB package when phpBB is in a different db from bitweaver in MySQL
+		// This only works on some databases
+		global $gBitSystem, $gBitDbName;
+		$gBitSystem->mDb->mDb->SelectDB( $gBitDbName );
+
+		//echo "$location";
+		call_user_func ($location);
+	}
+}
+
+?>
