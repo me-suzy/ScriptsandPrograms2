af31917398e28bf13f321a0c33a5859871793895
me-suzy <me.suzana@gmail.com>
2025-08-11 19:00:34 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/searchreplace/langs/cs.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/searchreplace/langs/cs.js
@@ -0,0 +1,19 @@
+// CS lang variables
+
+tinyMCELang['lang_searchreplace_search_desc'] = 'Najdi';
+tinyMCELang['lang_searchreplace_searchnext_desc'] = 'Najdi znova';
+tinyMCELang['lang_searchreplace_replace_desc'] = 'Najdi/Nahradit';
+tinyMCELang['lang_searchreplace_notfound'] = 'Vyhledávaní ukonc(eno. R(etezec nemusel být nalezen.';
+tinyMCELang['lang_searchreplace_search_title'] = 'Najdi';
+tinyMCELang['lang_searchreplace_replace_title'] = 'Najdi/Nahradit';
+tinyMCELang['lang_searchreplace_allreplaced'] = 'Vechny výskyty r(etezce byli zme(neny.';
+tinyMCELang['lang_searchreplace_findwhat'] = 'Najít';
+tinyMCELang['lang_searchreplace_replacewith'] = 'Nahradit';
+tinyMCELang['lang_searchreplace_direction'] = 'Sme(r';
+tinyMCELang['lang_searchreplace_up'] = 'Nahoru';
+tinyMCELang['lang_searchreplace_down'] = 'Dolu*';
+tinyMCELang['lang_searchreplace_case'] = 'Pr(esná zhoda';
+tinyMCELang['lang_searchreplace_findnext'] = 'Najdi&nbsp;dalí';
+tinyMCELang['lang_searchreplace_replace'] = 'Nahradit';
+tinyMCELang['lang_searchreplace_replaceall'] = 'Nahradit&nbsp;ve';
+tinyMCELang['lang_searchreplace_cancel'] = 'Zruit';
