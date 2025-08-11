6eeb8d33182db16bbf56436467e0354392417d59
me-suzy <me.suzana@gmail.com>
2025-08-11 18:55:19 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/insertdatetime/langs/zh_cn.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/insertdatetime/langs/zh_cn.js
@@ -0,0 +1,9 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+tinyMCELang['lang_insertdate_desc'] = '²åÈëµ±Ç°ÈÕÆÚ';
+tinyMCELang['lang_inserttime_desc'] = '²åÈëµ±Ç°Ê±¼ä';
+tinyMCELang['lang_inserttime_months_long'] = new Array("Ò»ÔÂ·Ý", "¶þÔÂ·Ý", "ÈýÔÂ·Ý", "ËÄÔÂ·Ý", "ÎåÔÂ·Ý", "ÁùÔÂ·Ý", "ÆßÔÂ·Ý", "°ËÔÂ·Ý", "¾ÅÔÂ·Ý", "Ê®ÔÂ·Ý", "Ê®Ò»ÔÂ·Ý", "Ê®¶þÔÂ·Ý");
+tinyMCELang['lang_inserttime_months_short'] = new Array("Ò»ÔÂ", "¶þÔÂ", "ÈýÔÂ", "ËÄÔÂ", "ÎåÔÂ", "ÁùÔÂ", "ÆßÔÂ", "°ËÔÂ", "¾ÅÔÂ", "Ê®ÔÂ", "Ê®Ò»ÔÂ", "Ê®¶þÔÂ");
+tinyMCELang['lang_inserttime_day_long'] = new Array("ÐÇÆÚÈÕ", "ÐÇÆÚÒ»", "ÐÇÆÚ¶þ", "ÐÇÆÚÈý", "ÐÇÆÚËÄ", "ÐÇÆÚÎå", "ÐÇÆÚÁù", "ÐÇÆÚÈÕ");
+tinyMCELang['lang_inserttime_day_short'] = new Array("ÖÜÈÕ", "ÖÜÒ»", "ÖÜ¶þ", "ÖÜÈý", "ÖÜËÄ", "ÖÜÎå", "ÖÜÁù", "ÖÜÈÕ");
