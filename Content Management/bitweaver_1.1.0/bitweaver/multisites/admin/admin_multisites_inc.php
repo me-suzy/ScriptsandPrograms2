afd3e19774ff8a5023179a01873a1cfc518a8a8d
me-suzy <me.suzana@gmail.com>
2025-08-11 18:16:07 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/multisites/admin/admin_multisites_inc.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/multisites/admin/admin_multisites_inc.php
@@ -0,0 +1,77 @@
+<?php
+// $Header: /cvsroot/bitweaver/_bit_multisites/admin/admin_multisites_inc.php,v 1.1.1.1.2.1 2005/07/29 16:15:04 squareing Exp $
+// Copyright (c) 2005 bitweaver Sample
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+include_once( THEMES_PKG_PATH.'theme_control_lib.php' );
+
+if( !empty( $_REQUEST['ms_id'] ) && !empty( $_REQUEST['action'] ) ) {
+	if( $_REQUEST['action'] == 'edit' ) {
+		$editSite = $gMultisites->getMultisites( $_REQUEST['ms_id'] );
+		$editSite = $editSite[$_REQUEST['ms_id']];
+		$gBitSmarty->assign( 'editSite', $editSite );
+	} elseif( $_REQUEST['action'] == 'delete' ) {
+		if( $gMultisites->expunge( $_REQUEST['ms_id'] ) ) {
+			$successMsg = "The server was successfully deleted";
+		}
+	}
+}
+
+$layoutSettings = array(
+	'feature_top_bar' => array(
+		'label' => 'Top bar menu',
+	),
+	'feature_top_bar_dropdown' => array(
+		'label' => 'Dropdown menu',
+	),
+	'feature_right_column' => array(
+		'label' => 'Right Module Column',
+	),
+	'feature_left_column' => array(
+		'label' => 'Left Module Column',
+	),
+);
+$gBitSmarty->assign( 'layoutSettings',$layoutSettings );
+
+if( !empty( $_REQUEST['store_server'] ) ) {
+	// Special handling for linked items: bitIndex and urlIndex
+	if (!empty($_REQUEST["server_prefs"]["urlIndex"]) && $_REQUEST["server_prefs"]["bitIndex"] == 'custom_home') {
+		$_REQUEST["server_prefs"]["bitIndex"] = $_REQUEST["server_prefs"]["urlIndex"];
+	}
+
+	// prepare the checkbox data for storage
+	foreach( array_keys( $layoutSettings ) as $pref ) {
+		$_REQUEST['server_prefs'][$pref] = ( isset( $_REQUEST[$pref][0] ) ? 'y' : 'n' );
+	}
+
+	if( $gMultisites->store( $_REQUEST ) ) {
+		$successMsg = "The server was successfully saved";
+	}
+}
+
+if( !empty( $_REQUEST['store_preferences'] ) ) {
+	if( $gMultisites->storePreferences( $_REQUEST ) ) {
+		$successMsg = "The preferences were successfully saved";
+	}
+}
+
+$gBitSmarty->assign( 'successMsg', empty( $successMsg ) ? NULL : $successMsg );
+$gBitSmarty->assign( 'warningMsg', empty( $warningMsg ) ? NULL : $warningMsg );
+if( !empty( $gMultisites->mErrors ) ) {
+	foreach( $gMultisites->mErrors as $error ) {
+		$errorMsg[] = $error;
+	}
+	$gBitSmarty->assign( 'errorMsg', $errorMsg );
+}
+
+$gBitSmarty->assign( 'listMultisites', $gMultisites->getMultisites() );
+
+// Get list of available styles
+$styles = &$tcontrollib->getStyles( NULL, TRUE );
+$gBitSmarty->assign( "styles", $styles );
+
+// Get list of available languages
+$languages = $gBitLanguage->listLanguages();
+$gBitSmarty->assign("languages",$languages );
+?>
