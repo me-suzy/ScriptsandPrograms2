661a808de1b8c866ed27c8bc7683a40034e7d04a
me-suzy <me.suzana@gmail.com>
2025-08-12 02:38:18 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/iespell/langs/el.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/iespell/langs/el.js
@@ -0,0 +1,7 @@
+// Greek lang variables by Jacaranda Bill
+
+tinyMCE.addToLang('',{
+iespell_desc : 'Ïñèïãñá&ouml;éêüò Ýë&aring;ã÷ïò',
+iespell_download : "Ôï ðñüãñáììá ieSpell &auml;&aring;í âñÝèçê&aring;. ÐáôÞóô&aring; OK ãéá íá ì&aring;ôáâ&aring;ßô&aring; óôçí éóôïó&aring;ëß&auml;á ëÞøçò ôïõ ðñïãñÜììáôïò."
+});
+
