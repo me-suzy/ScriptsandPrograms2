1e4c644ce753d55865a39b9dd81f56b318ba9c46
me-suzy <me.suzana@gmail.com>
2025-08-11 18:53:29 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/iespell/langs/ko.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/iespell/langs/ko.js
@@ -0,0 +1,4 @@
+// KO lang variables
+
+tinyMCELang['lang_iespell_desc'] = '¸ÂÃã¹ý °Ë»ç ½ÇÇà';
+tinyMCELang['lang_iespell_download'] = "ieSpellÀ» Ã£À» ¼ö ¾ø½À´Ï´Ù. OK¸¦ ´©¸£¸é ´Ù¿î·Îµå ÆäÀÌÁö·Î ÀÌµ¿ÇÕ´Ï´Ù."
