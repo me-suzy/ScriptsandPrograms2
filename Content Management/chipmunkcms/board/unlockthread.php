a78ad0809cc198f1b8c809f203fc451f7395f0ae
me-suzy <me.suzana@gmail.com>
2025-08-11 19:45:27 +0300

    Add Content Management/chipmunkcms/board/unlockthread.php

---
+++ Content Management/chipmunkcms/board/unlockthread.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+include "connect.php";
+?>
+<?php
+print "<link rel='stylesheet' href='style.css' type='text/css'>";
+print "<table class='maintable'>";
+print "<tr class='headline'><td><center>Lock Thread</center></td></tr>";
+print "<tr class='forumrow'><td>";
+$user=$_SESSION['user'];
+$getuser="SELECT * from b_users where username='$user'";
+$getuser2=mysql_query($getuser) or die("Could not get user info");
+$getuser3=mysql_fetch_array($getuser2);
+$ID=$_GET['ID'];
+$gettopic="SELECT * from b_posts, b_users where b_posts.ID='$ID' and b_users.userID=b_posts.author";
+$gettopic2=mysql_query($gettopic) or die("Could not get topic");
+$gettopic3=mysql_fetch_array($gettopic2);
+if($getuser3[status]<1 || ($getuser3[status]<=$gettopic3[status]&&$getuser3[userID]!=$gettopic3[userID]))
+{
+  die("<table class='maintable'><tr class='headline'><td><center>Unlock Thread</center></td></tr><tr class='forumrow'><td><center>You do not have permission to unlock this thread</center></td></tr></table>");
+}
+else
+{
+  if(isset($_POST['submit']))
+  {
+    $threadid=$_POST['threadid'];
+    $lock="Update b_posts set locked='0' where ID='$threadid'";
+    mysql_query($lock) or die("Could not lock thread");
+    $lock3="Update b_posts set locked='0' where threadparent='$threadid'";
+    mysql_query($lock3) or die("Could not lock posts");
+    print "Thread unlocked";
+
+  }
+  else
+  {
+    print "<form action='unlockthread.php' method='post'>";
+    print "Are You sure you want to unlock this thread?<br>";
+    print "<input type='hidden' name='threadid' value='$ID'>";
+    print "<input type='submit' name='submit' value='Unlock Thread'></form>";  
+  }
+}
