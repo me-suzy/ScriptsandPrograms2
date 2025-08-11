7ce5ddef5eb7838fc09caede92428037443cd7df
me-suzy <me.suzana@gmail.com>
2025-08-11 18:48:00 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/advhr/langs/zh_cn.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/advhr/langs/zh_cn.js
@@ -0,0 +1,7 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+tinyMCELang['lang_insert_advhr_desc']    = '²åÈë/±à¼­ Ë®Æ½±ê³ß'
+tinyMCELang['lang_insert_advhr_width']   = '¿í¶È';
+tinyMCELang['lang_insert_advhr_size']    = '¸ß¶È';
+tinyMCELang['lang_insert_advhr_noshade'] = 'ÎÞÒõÓ°';
