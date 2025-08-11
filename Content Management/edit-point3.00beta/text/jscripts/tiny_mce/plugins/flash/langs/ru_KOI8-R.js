5cc207063356ff378bed5a1ea72e1c135106d2fb
me-suzy <me.suzana@gmail.com>
2025-08-12 02:37:21 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/flash/langs/ru_KOI8-R.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/flash/langs/ru_KOI8-R.js
@@ -0,0 +1,11 @@
+// RU lang variables KOI8-R
+
+tinyMCE.addToLang('flash',{
+title : '÷ÓÔÁ×ÉÔØ / òÅÄÁËÔÉÒÏ×ÁÔØ Flash ÒÏÌÉË',
+desc : '÷ÓÔÁ×ÉÔØ / òÅÄÁËÔÉÒÏ×ÁÔØ Flash ÒÏÌÉË',
+file : 'Flash-æÁÊÌ (.swf)',
+size : 'òÁÚÍÅÒ',
+list : 'Flash ÆÁÊÌÙ',
+props : 'ó×ÏÊÓÔ×Á Flash',
+general : 'ïÓÎÏ×ÎÏÅ'
+});
