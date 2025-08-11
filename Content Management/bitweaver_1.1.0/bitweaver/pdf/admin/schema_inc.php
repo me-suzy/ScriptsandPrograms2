9b13af8ebd831204302765f1d60f5b62975814f2
me-suzy <me.suzana@gmail.com>
2025-08-11 18:17:44 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/pdf/admin/schema_inc.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/pdf/admin/schema_inc.php
@@ -0,0 +1,14 @@
+<?php
+$gBitInstaller->registerPackageInfo( PDF_PKG_NAME, array(
+	'description' => "PDF generation package for creating PDF files from Liberty content.",
+	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
+	'version' => '0.1',
+	'state' => 'beta',
+	'dependencies' => 'html',
+) );
+
+// ### Default UserPermissions
+$gBitInstaller->registerUserPermissions( PDF_PKG_NAME, array(
+	array('bit_p_pdf_generation', 'Can create PDF files from content', 'registered', PDF_PKG_NAME),
+) );
+?>
