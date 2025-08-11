8edaca1b97a1a6d504c542afba3d3bb24a79ca8b
me-suzy <me.suzana@gmail.com>
2025-08-11 17:42:23 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/class/locale/de.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/class/locale/de.php
@@ -0,0 +1,410 @@
+<?php
+/*
+*  de (German) Language file for PHPSlash
+*  translations named using POSIX locale conventions ( en, es, de, etc)
+*
+*  Please don't use HTML code here! 
+*  Bitte keinen HTML Code verwenden! 
+*/
+   $pslstrings = array(
+
+      // about.php
+      // article.php
+      // backend.php
+      // comment.php
+      "No Subject Given" =>
+      "Kein Betreff",
+
+      "Re: " =>
+      "Re: ",
+
+      "Anonymous" =>
+      "Anonym",
+
+      "Comments" =>
+      "Kommentare",
+
+      "You didn't supply a good submit value" =>
+      "Du hast keinen gültigen Wert eingegeben",
+
+      // commentShow.php
+      // commentSubmit.php
+      // config.php
+      "Home" =>
+      "Home",
+
+      "Add Story" =>
+      "Nachricht hinzufügen",
+
+      "Archives" =>
+      "Archiv",
+
+      "Polls" =>
+      "Umfragen",
+
+      "About" =>
+      "Über uns",
+
+      "Topic" =>
+      "Thema",
+
+      "Story" =>
+      "Nachricht",
+
+      "Submission" =>
+      "Beitrag",
+
+      "Section" =>
+      "Rubrik", 
+
+      "Block" =>
+      "Block",
+
+      "Poll" =>
+      "Umfrage",
+
+      "Author" =>
+      "Autor",
+
+      "Glossary" =>
+      "Glossar",
+
+      "Mailing List" =>
+      "Mailingliste",
+
+      "Variable" =>
+      "Variable",
+
+      "Logging" =>
+      "Logging",
+
+      // functions.inc
+      "NO TITLE" =>
+      "Kein Titel",
+
+      "@!#$" =>
+      "@!#$",
+
+      // glossary.php
+      // index.php
+      // indextpl3col.php
+      // login.php
+      "%s logged out." =>
+      "%s ist ausgeloggt.",
+      // mailinglist.php
+      // poll.php
+      "VOTE: " =>
+      "Abstimmen: ",
+
+      "View Pollbooth" =>
+      "Abstimmungsergebnisse",
+
+       "%s Poll" =>
+       "%s Poll",
+      // pollBooth.php
+      // search.php
+      "Searching %s" =>
+      "Suche %s",
+
+      "All Topics" =>
+      "Alle Themen",
+
+      "All Sections" =>
+      "Alle Rubriken",
+
+      "All Authors" =>
+      "Alle Autoren",
+      // submisson.php
+      "Thanks for this submission.  We have it and will set our fearless editorial staff upon it right this second." =>
+      "Danke für diesen Beitrag.  Wir haben ihn erhalten und werden ihn uns gleich ansehen.",
+      
+      "Something broke, I'm not sure what though??" =>
+      "Es ist ein Fehler aufgetreten, ich weiß nicht woran es genau liegt?!",
+
+      "New Submission" =>
+      "Neuer Beitrag",
+      
+      // submissionAdmin.php
+      // /admin files
+      // authorAdmin
+      "Error! Invalid Privileges" => 
+      "Fehler! Ungültige Privilegien",
+      
+      "Sorry. You do not have the necessary privilege to view this page." =>
+      "Leider hast Du nicht die notwendigen Privilegien um diese Seite aufzurufen.",
+
+      // blockadmin.php
+      // commentAdmin.php
+      // glossaryAdmin.php
+      // infologAdmin.php
+      // mailinglistAdmin.php
+      // pollAdmin.php
+      // sectionAdmin.php
+      // storyAdmin.php
+      // submissionAdmin.php
+      // topicAdmin.php
+      // variableAdmin.php
+      
+      // /class files
+     
+      // Author.class
+      // Block.class
+      // Block_i.class
+      // Block_render_cal.class
+      "January" =>
+      "Januar",
+
+      "February" =>
+      "Februar",
+ 
+      "March" =>
+      "März",
+
+      "April" =>
+      "April",
+
+      "May" =>
+      "Mai",
+
+      "June" =>
+      "Juni",
+      
+      "July" =>
+      "Juli",
+
+      "August" =>
+      "August",
+
+      "September" =>
+      "September",
+
+      "October" =>
+      "Oktober",
+
+      "November" =>
+      "November",
+
+      "December" =>
+      "Dezember",
+      
+      // Block_render_html.class
+      // Block_render_poll.class
+      // Block_render_query.class
+      // Block_render_quote.class
+      // Block_render_rss.class
+      // Block_render_section.class
+      // Block_render_topic.class
+      // Block_render_url.class
+      // Comment.class
+      "Nested" => 
+      "Verschachtelt",
+      
+      "Threaded" => 
+      "Thematisch",
+      
+      "Flat" => 
+      "Chronologisch",
+      
+      "Index" => 
+      "Index",
+      
+      "Bulk Delete" => 
+      "Alles löschen",
+      
+      "Show Pending" => 
+      "Zu Erledigendes zeigen",
+      
+      // Glossary.class
+      "Add a new Term" =>
+      "Neuen Begriff hinzufügen",
+      
+      // Infolog.class
+      // Mailinglist.class
+      "Email Headline Updates" =>
+      "E-Mail Headline Updates",
+
+      "You need to enter an email address" =>
+      "Du mußt eine E-Mail Adresse eingeben",
+
+      "There was an error inserting your subscription information into the database." =>
+      "Beim Speichern Deines Beitrages in der Datenbank ist ein Fehler aufgetreten.",
+
+      "There was an error inserting your frequency data." =>
+      "Es trat beim Speichern ein Fehler auf.",
+
+      
+      "Thank you. You will receive a confirmation email soon to verify your subscription." =>
+      "Danke! Du wirst in Kürze eine Bestätigung per E-Mail erhalten. Du mußt den Empfang dann bestätigen.",
+
+      "You have been successfully subscribed, but there was a problem sending your confirmation email. You should, however receive your newsletter soon." =>
+      "Du hast die Mailingliste erfolgreich abonniert, aber es konnte keine Bestätigung per E-Mail versendet werden.",
+
+      "The email address '%s' is not found" =>
+      "Die E-Mail Adresse '%s' wurde nicht gefunden",
+
+      "Email: '%s' is not found" =>
+      "E-Mail: '%s' wurde nicht gefunden",
+
+      "Email: '%s' has been removed" =>
+      "E-Mail: '%s' wurde gel&ouml;scht",
+
+      "Thank you. for your subscription to our site. If there is anything we can do to improve our service,  please let us know!" =>
+      "Danke für Dein Interesse an unserer Website. Wenn wir etwas tun können um den Service zu verbessern, schreib uns bitte.",
+
+      "You've been removed from the mailing list, but there was a problem sending out the final email." =>
+      "Du wurdest von der Mailingliste entfernt, aber es trat beim Senden der Bestätigung ein Problem auf.",
+
+      "Unsubscribing all Mailing List Members" =>
+      "Alle Mitglieder aus der Mailingliste entfernen",
+
+      "Now ignoring abort calls..." =>
+      "Abbruch Befehle werden ignoriert...",
+
+      "Sending mail to the following users:" =>
+      "Die E-Mail wurde an folgende Personen verschickt:",
+
+      "Mail Sent Successfully!" =>
+      "E-Mail wurde erfolgreich verschickt!",
+
+      "Error Sending Email!" =>
+      "Fehler beim Senden der E-Mail!",
+
+      "You didn't give a email subject!  Halting." =>
+      "Du hast keinen Betreff eingegeben! Der Vorgang wurde abgebrochen.",
+      
+      // Navbar.class
+      "Login" =>
+      "Login",
+      
+      "Logout %s" =>
+      "Logout %s",
+      
+      // Poll.class
+      "Your vote has been registered" =>
+      "Deine Abstimmung wurde gespeichert",
+
+      "Your vote hasn't been registered because there was no remote address:  %s" =>
+      "Deine Abstimmung kann nicht berücksichtigt werden, weil Deine Rechneradresse unbekannt ist:  %s",
+
+      "Your vote hasn't been registered because you (or someone from your computer) has already voted." =>
+      "Deine Abstimmung kann nicht berücksichtigt werden, weil Du (oder jemand mit der gleichen IP Adresse) schon abgestimmt hat.",
+
+
+      // Section.class
+      // Story.class
+      "More" =>
+      "Mehr",
+
+      "Related Links" =>
+      "Verwandte Links",
+
+      "Reply" =>
+      "Antwort",
+      
+      "Also filed under" =>
+      "Außerdem eingetragen unter",
+
+      "comments" =>
+      "Kommentare",
+ 
+      "comment" =>
+      "Kommentar",
+      
+      "Comments?" =>
+      "Kommentieren?",
+      
+      // Submission.class
+      "Anonymous Coward" =>
+      "Anonymer Feigling"
+
+   );
+
+
+// Back-End Translations
+
+
+/*
+*  de (German) Language file for BACK-END CMS
+*
+*  Translation by DIGA-Design 26.11.2002
+*/
+
+   $pslstrings += array(
+
+      // BE_section
+      "BE_Section" =>         "Rubrik",
+      "BE_Link" =>            "Links",
+      "BE_LinkAdmin" =>       "Link-Verwaltung",
+      "BE_Article" =>         "Artikel",
+      "BE_Upload" =>          "Hochladen",
+      "BE_Strike" =>          "Schlagzeilen-Verwaltung",
+      "BE_Gallery" =>         "Galerie",
+      "Glossary Admin" =>     "Glossar-Verwaltung",
+
+// General
+
+      "Navigation"         => "Navigation",
+      "Administration"     => "Verwaltung",
+      "Spotlight Articles" => "Artikel im Rampenlicht",
+      "New Articles"       => "Neue Artikel",
+      "Related Articles"   => "Verwandte Artikel",
+      "Random Links"       => "Zufalls-Links",
+
+      "Print Friendly"     => "Druckerfreundlich Version",
+      "PDF Version"        => "PDF Version",
+      "Email to a Friend"  => "An einen Freund mailen",
+
+      "Edit"               => "Bearbeiten",
+      "Delete"             => "LЖschen",
+      'be_previews'        => 'Voransicht(en)',
+
+      'Inserting Record'   => 'Datensatz einfЭgen',
+      'Updating Record'    => 'Datensatz aktualisieren',
+
+      'be_duplicate_url'   => 'Dieser URL Name wurde schon verwendet: ',
+      'be_section_fetch_err' => "Konnte Rubrik nicht finden: ",
+
+// index.php
+      'be_article_fetch_err' => "Konnte Artikel nicht finden: ",
+
+
+// BE_SectionAdmin.php
+      'be_section_save_err'=> 'Konnte Rubrik nicht speichern',
+      "Edit Section"       => "Rubrik bearbeiten",
+
+
+// BE_Article.class
+      "Article"            => "Artikel",
+// BE_Article_admin.class
+
+// BE_articleAdmin.php
+      "Edit Article"       => "Artikel bearbeiten",
+      "be_article_delete_err" => "LЖschen des Artikels fehlgeschlagen",
+      "be_article_fetch_err" => "Tut mir leid, gewЭnschter Artikel nicht verfЭgbar",
+
+
+// BE_Link_admin.class
+      'be_no_linktitle'    => "[Dieser Link hat keinen Titel]",
+      'be_need_title'      => "Wir benЖtigen zumindest einen Artikel in der Standardsprache",
+      'be_need_section'    => 'Sie haben keine Rubrik ausgewДhlt.',
+// BE_linkAdmin.php
+      'be_delete_failed'   => 'LЖschen des Objektes (id %s) fehlgeschlagen',
+#      'be_edit'           => 'Bearbeiten',
+      'be_err_fetchlink'   => 'Tut mir leid, der Link (id %s) ist nicht verfЭgbar',
+
+
+// BE_Search.class
+      "be_searching"       => "Suche nach %s",
+      "be_no_matches"      => "Keine Treffer zu Ihrer Suche gefunden",
+      'be_search_start'    => "Starten Sie Ihre Suche, indem Sie das Formular ausfЭllen und auf den Knopf 'Suchen' klicken",
+
+// BE_Gallery_base.class
+      "Updating File"      => "Aktualisiere Datei",
+      "Writing to File "   => "Schreibe in Datei ",
+
+// Dummy final entry
+      'xxx'                => 'xxx'
+
+   );
+
+?>
