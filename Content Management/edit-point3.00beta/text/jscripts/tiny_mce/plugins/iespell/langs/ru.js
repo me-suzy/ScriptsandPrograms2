21424d470c34d723c82a3f7631d2da80f011d44d
me-suzy <me.suzana@gmail.com>
2025-08-12 02:38:42 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/iespell/langs/ru.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/iespell/langs/ru.js
@@ -0,0 +1,7 @@
+// RU lang variables cp1251
+
+tinyMCE.addToLang('',{
+iespell_desc : 'Çàïóñòèòü ïðîâåðêó îðôîãðàôèè',
+iespell_download : "ieSpell íå îáíàðóæåí. íàæìèòå OK, ÷òîáû ïåðåéòè íà ñòðàíèöó çàãðóçêè."
+});
+
