0f5164c04f1c05a62058a40cae94d4b144c18523
me-suzy <me.suzana@gmail.com>
2025-08-11 18:52:32 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/fullscreen/langs/zh_cn.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/fullscreen/langs/zh_cn.js
@@ -0,0 +1,5 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+tinyMCELang['lang_fullscreen_title'] = 'È«ÆÁÄ£Ê½'
+tinyMCELang['lang_fullscreen_desc'] = 'ÇÐ»»È«ÆÁÄ£Ê½'
