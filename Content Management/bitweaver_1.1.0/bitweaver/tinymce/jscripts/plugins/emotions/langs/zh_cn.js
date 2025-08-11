36b0b63ea9a1fa6fa04bbec395d5ea16b0f2014a
me-suzy <me.suzana@gmail.com>
2025-08-11 18:50:28 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/emotions/langs/zh_cn.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/emotions/langs/zh_cn.js
@@ -0,0 +1,6 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+tinyMCELang['lang_insert_emotions_title'] = '²åÈë±íÇé';
+tinyMCELang['lang_emotions_desc'] = '±íÇé';
+
