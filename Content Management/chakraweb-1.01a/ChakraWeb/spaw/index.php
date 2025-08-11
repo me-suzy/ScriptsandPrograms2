dea7e46844e49a20f9e2d870b96b55b968555c90
me-suzy <me.suzana@gmail.com>
2025-08-11 19:38:08 +0300

    Add Content Management/chakraweb-1.01a/ChakraWeb/spaw/index.php

---
+++ Content Management/chakraweb-1.01a/ChakraWeb/spaw/index.php
@@ -0,0 +1,11 @@
+<?php 
+// ----------------------------------------------------------------------
+// ModName: index.php
+// Purpose: Redirect to the homegape
+// Author:  Mitra Dwi Wiyono (mdwiyono@quick4all.com)
+// ----------------------------------------------------------------------
+
+
+Header("Location: /index.html");
+
+?>
