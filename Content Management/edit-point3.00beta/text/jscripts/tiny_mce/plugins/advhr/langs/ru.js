72aa58f092c736bbdadaee4ae40426ea7a908387
me-suzy <me.suzana@gmail.com>
2025-08-12 02:32:41 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/advhr/langs/ru.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/advhr/langs/ru.js
@@ -0,0 +1,8 @@
+// RU cp1251 lang variables
+
+tinyMCE.addToLang('',{
+insert_advhr_desc : 'Âñòàâèòü / ðåäàêòèðîâàòü ãîðèçîíòàëüíûé ðàçäåëèòåëü',
+insert_advhr_width : 'Øèðèíà',
+insert_advhr_size : 'Âûñîòà',
+insert_advhr_noshade : 'Áåç òåíè'
+});
