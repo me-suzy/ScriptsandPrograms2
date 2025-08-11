1e705f7eb0dbef7ff89fd8b8b8e187d95eaaceec
me-suzy <me.suzana@gmail.com>
2025-08-12 02:58:34 +0300

    Add Content Management/ezpublish-3.7.2/ezpublish-3.7.2/kernel/class/classlist.php

---
+++ Content Management/ezpublish-3.7.2/ezpublish-3.7.2/kernel/class/classlist.php
@@ -0,0 +1,119 @@
+<?php
+//
+// Created on: <16-Apr-2002 11:00:12 amos>
+//
+// Copyright (C) 1999-2005 eZ systems as. All rights reserved.
+//
+// This source file is part of the eZ publish (tm) Open Source Content
+// Management System.
+//
+// This file may be distributed and/or modified under the terms of the
+// "GNU General Public License" version 2 as published by the Free
+// Software Foundation and appearing in the file LICENSE included in
+// the packaging of this file.
+//
+// Licencees holding a valid "eZ publish professional licence" version 2
+// may use this file in accordance with the "eZ publish professional licence"
+// version 2 Agreement provided with the Software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE.
+//
+// The "eZ publish professional licence" version 2 is available at
+// http://ez.no/ez_publish/licences/professional/ and in the file
+// PROFESSIONAL_LICENCE included in the packaging of this file.
+// For pricing of this licence please contact us via e-mail to licence@ez.no.
+// Further contact information is available at http://ez.no/company/contact/.
+//
+// The "GNU General Public License" (GPL) is available at
+// http://www.gnu.org/copyleft/gpl.html.
+//
+// Contact licence@ez.no if any conditions of this licencing isn't clear to
+// you.
+//
+
+
+include_once( "kernel/classes/ezcontentclass.php" );
+include_once( "lib/ezutils/classes/ezhttppersistence.php" );
+include_once( "kernel/classes/ezcontentclassclassgroup.php" );
+
+$Module =& $Params["Module"];
+$GroupID = false;
+if ( isset( $Params["GroupID"] ) )
+    $GroupID =& $Params["GroupID"];
+
+$http =& eZHTTPTool::instance();
+$http->setSessionVariable( 'FromGroupID', $GroupID );
+if ( $http->hasPostVariable( "RemoveButton" ) )
+{
+    if ( $http->hasPostVariable( 'DeleteIDArray' ) )
+    {
+        $deleteIDArray =& $http->postVariable( 'DeleteIDArray' );
+        if ( $deleteIDArray !== null )
+        {
+            $http->setSessionVariable( 'DeleteClassIDArray', $deleteIDArray );
+            $Module->redirectTo( $Module->functionURI( 'removeclass' ) . '/'  . $GroupID . '/' );
+        }
+    }
+}
+
+if ( $http->hasPostVariable( "NewButton" ) )
+{
+    if ( $http->hasPostVariable( "CurrentGroupID" ) )
+        $GroupID = $http->postVariable( "CurrentGroupID" );
+    if ( $http->hasPostVariable( "CurrentGroupName" ) )
+        $GroupName = $http->postVariable( "CurrentGroupName" );
+
+    $params = array( null, $GroupID, $GroupName );
+    $Module->run( "edit", $params );
+    return;
+}
+
+if ( !isset( $TemplateData ) or !is_array( $TemplateData ) )
+{
+    $TemplateData = array( array( "name" => "groupclasses",
+                                  "http_base" => "ContentClass",
+                                  "data" => array( "command" => "groupclass_list",
+                                                   "type" => "class" ) ) );
+}
+
+$Module->setTitle( ezi18n( 'kernel/class', 'Class list of group' ) . ' ' . $GroupID );
+include_once( "kernel/common/template.php" );
+$tpl =& templateInit();
+
+include_once( "kernel/classes/datatypes/ezuser/ezuser.php" );
+$user =& eZUser::currentUser();
+foreach( $TemplateData as $tpldata )
+{
+    $tplname = $tpldata["name"];
+
+    $groupInfo =  eZContentClassGroup::fetch( $GroupID );
+
+    if( !$groupInfo )
+    {
+       return $Module->handleError( EZ_ERROR_KERNEL_NOT_AVAILABLE, 'kernel' );
+    }
+
+    $list = eZContentClassClassGroup::fetchClassList( 0, $GroupID, $asObject = true );
+    $groupModifier =& eZContentObject::fetch( $groupInfo->attribute( 'modifier_id') );
+    $tpl->setVariable( $tplname, $list );
+    $tpl->setVariable( "class_count", count( $list ) );
+    $tpl->setVariable( "GroupID", $GroupID );
+    $tpl->setVariable( "group", $groupInfo );
+    $tpl->setVariable( "group_modifier", $groupModifier );
+}
+
+$group = eZContentClassGroup::fetch( $GroupID );
+$groupName = $group->attribute( 'name' );
+
+
+$tpl->setVariable( "module", $Module );
+
+$Result = array();
+$Result['content'] =& $tpl->fetch( "design:class/classlist.tpl" );
+$Result['path'] = array( array( 'url' => '/class/grouplist/',
+                                'text' => ezi18n( 'kernel/class', 'Classes' ) ),
+                         array( 'url' => false,
+                                'text' => $groupName ) );
+?>
