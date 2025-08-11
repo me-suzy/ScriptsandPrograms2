6941660f81bb4f05dda9bd95feb9e1dfd4441b8e
me-suzy <me.suzana@gmail.com>
2025-08-11 18:07:42 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/install/upgrade_database.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/install/upgrade_database.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_install/upgrade_database.php,v 1.1.1.1.2.2 2005/07/26 15:50:07 drewslater Exp $
+ * @package install
+ * @subpackage upgrade
+ */
+
+/**
+ * required setup
+ */
+$app = '';
+include_once( INSTALL_PKG_PATH.'install_database.php' );
+// update the MySQL settings in the db server array
+if( function_exists( 'mysql_connect' ) ) {
+	// check version of mysql server
+	if( preg_match( '/[4-9]\./', @mysql_get_server_info() ) ) {
+		$dbtodsn['mysql'] = 'MySQL '.mysql_get_server_info();
+	} elseif( preg_match( '/3\./', @mysql_get_server_info() ) ) {
+		$dbtodsn['mysql'] = 'MySQL '.mysql_get_server_info().' WARNING!!!';
+	} else {
+		$dbtodsn['mysql'] = 'MySQL 3.x';
+	}
+}
+$gBitSmarty->assign( 'include_template', INSTALL_PKG_PATH."templates/install_database".$app.".tpl" );
+?>
