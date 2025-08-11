432e0672ae90acf013772980fce3dfd0632991e2
me-suzy <me.suzana@gmail.com>
2025-08-11 18:54:50 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/insertdatetime/langs/ko.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/insertdatetime/langs/ko.js
@@ -0,0 +1,8 @@
+// KO lang variables
+
+tinyMCELang['lang_insertdate_desc'] = '³¯Â¥ ³Ö±â';
+tinyMCELang['lang_inserttime_desc'] = '½Ã°£ ³Ö±â';
+tinyMCELang['lang_inserttime_months_long'] = new Array("1¿ù", "2¿ù", "3¿ù", "4¿ù", "5¿ù", "6¿ù", "7¿ù", "8¿ù", "9¿ù", "10¿ù", "11¿ù", "12¿ù");
+tinyMCELang['lang_inserttime_months_short'] = new Array("1¿ù", "2¿ù", "3¿ù", "4¿ù", "5¿ù", "6¿ù", "7¿ù", "8¿ù", "9¿ù", "10¿ù", "11¿ù", "12¿ù");
+tinyMCELang['lang_inserttime_day_long'] = new Array("ÀÏ¿äÀÏ", "¿ù¿äÀÏ", "È­¿äÀÏ", "¼ö¿äÀÏ", "¸ñ¿äÀÏ", "±Ý¿äÀÏ", "Åä¿äÀÏ", "ÀÏ¿äÀÏ");
+tinyMCELang['lang_inserttime_day_short'] = new Array("ÀÏ", "¿ù", "È­", "¼ö", "¸ñ", "±Ý", "Åä", "ÀÏ");
