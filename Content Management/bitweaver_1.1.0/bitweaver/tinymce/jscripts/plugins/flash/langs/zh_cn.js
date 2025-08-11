c8bfe049b928561f466f30ec52c54b7083110969
me-suzy <me.suzana@gmail.com>
2025-08-11 18:51:46 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/flash/langs/zh_cn.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/flash/langs/zh_cn.js
@@ -0,0 +1,8 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+tinyMCELang['lang_insert_flash']      = '²åÈë/±à¼­ FlashµçÓ°';
+tinyMCELang['lang_insert_flash_file'] = 'FlashÎÄ¼þ(.swf)';
+tinyMCELang['lang_insert_flash_size'] = '³ß´ç';
+tinyMCELang['lang_insert_flash_list'] = 'FlashÎÄ¼þÁÐ±í';
+tinyMCELang['lang_flash_props'] = 'FlashÊôÐÔ';
