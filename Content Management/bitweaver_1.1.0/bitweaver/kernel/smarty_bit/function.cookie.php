7297f52633198fdb555142e77c292754f52ada23
me-suzy <me.suzana@gmail.com>
2025-08-11 18:10:09 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/kernel/smarty_bit/function.cookie.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/kernel/smarty_bit/function.cookie.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * smarty_function_cookie
+ */
+function smarty_function_cookie($params, &$gBitSmarty)
+{
+    global $taglinelib;
+    include_once( KERNEL_PKG_PATH.'tagline_lib.php' );
+    extract($params);
+    // Param = zone
+  
+  	if( $taglinelib ) {
+	    $data = $taglinelib->pick_cookie();
+   		print($data);
+	}
+}
+
+/* vim: set expandtab: */
+
+?>
