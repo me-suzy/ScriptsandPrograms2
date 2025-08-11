5c5b8999cc09da9fc26eec77d1fe8a7e320ad898
me-suzy <me.suzana@gmail.com>
2025-08-11 19:36:27 +0300

    Add Content Management/chakraweb-1.01a/ChakraWeb/phpmod/index.php

---
+++ Content Management/chakraweb-1.01a/ChakraWeb/phpmod/index.php
@@ -0,0 +1,12 @@
+<?php 
+// ----------------------------------------------------------------------
+// ModName: index.php
+// Purpose: Simple trick to avoid Apache show the content of directory.
+//          Here we redirect to the homepage
+// Author:  Mitra Dwi Wiyono (mdwiyono@quick4all.com)
+// ----------------------------------------------------------------------
+
+
+Header("Location: /index.html");
+
+?>
