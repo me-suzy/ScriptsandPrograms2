c18c19c9e4669cdf5fae3101bdeeb956b26ec37a
me-suzy <me.suzana@gmail.com>
2025-08-11 19:46:58 +0300

    Add Content Management/chipmunkcms/board/pm/delpm.php

---
+++ Content Management/chipmunkcms/board/pm/delpm.php
@@ -0,0 +1,48 @@
+<?php
+include '../connect.php';
+session_start();
+?>
+
+<link rel="stylesheet" href="../style.css" type="text/css">
+<?php
+if (isset($_SESSION['user'])) 
+  {
+    $user=$_SESSION['user'];
+    $getuser="SELECT * from b_users where username='$user'";
+    $getuser2=mysql_query($getuser) or die("Could not get user info");
+    $getuser3=mysql_fetch_array($getuser2);
+    print "<center>";
+    print "<table class='maintable'>";
+    print "<tr class='headline'><td><center>Delete PM - <A href='pm.php'><font color='white'>Back to PM main</font></a></center></td></tr>";
+    print "<tr class='forumrow'><td>";
+    if(isset($_POST['submit']))
+    {
+       $theid=$_POST['theid'];
+       $delmessage="DELETE FROM b_pms where pmID='$theid' and receiver='$getuser3[userID]'";
+       mysql_query($delmessage) or die("Could not delete message");
+       print "PM deleted, back to <A href='pm.php'>PM Main</a>"; 
+
+
+    }
+    else
+    {
+       $ID=$_GET['ID'];
+       print "Are you sure you want to delete this PM?<br>";
+       print "<form action='delpm.php' method='post'>";
+       print "<input type='hidden' value='$ID' name='theid'>";
+       print "<input type='submit' name='submit' value='delete'></form>";
+   
+
+    }
+    print "</td></tr></table>";       
+    print "<font size='1'>Script Produced by © <A href='http://www.chipmunk-scripts.com'>Chipmunk Scripts</a></font>";
+    
+  }
+else
+  {
+    print "Sorry, not logged in  please <A href='login.php'>Login</a><br>";
+  
+  }
+
+?>
+
