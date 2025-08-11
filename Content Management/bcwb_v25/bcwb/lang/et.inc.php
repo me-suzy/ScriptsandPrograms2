35bc0a47eaa3a3b9cd13e6c0940d5bbb82ebf447
me-suzy <me.suzana@gmail.com>
2025-08-11 18:00:35 +0300

    Add Content Management/bcwb_v25/bcwb/lang/et.inc.php

---
+++ Content Management/bcwb_v25/bcwb/lang/et.inc.php
@@ -0,0 +1,119 @@
+<?PHP
+// vim: set expandtab tabstop=4 shiftwidth=4: 
+// +----------------------------------------------------------------------+
+// | BCWB: Business Card Web Builder                                      |
+// +----------------------------------------------------------------------+
+// | Author:  Dmitry Sheiko <sheiko@cmsdevelopment.com>	                  |
+// | Copyright (c) 2004 Dmitry Sheiko                                     |
+// | http://bcwb.cmsdevelopment.com                                       |
+// +----------------------------------------------------------------------+
+// | This source file is free software; you can redistribute it and/or    |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This source file is distributed in the hope that it will be useful,  |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// +----------------------------------------------------------------------+
+// Release: 23.02.04 (d/m/y)
+// $Id$
+
+$default_charset="UTF-8";
+
+$lang=array(
+
+	"Icon" => "Icon",
+	"CreateIcon" => "Create icon",
+	"Width" => "Width",
+	"Height" => "Height",
+
+	"Need_set_permissions" => "Need set permissions attributes to files/folders",
+	"Run" => "Run",
+	"Archive" => "Archive",
+	"Moved" => "Moved",
+	"Restore_backup" => "Restore backup",
+	"Create_backup" => "Create backup",
+	"Need_authorization" => "Here is a need to authorise",
+	"System_instaled" => "System was installed",
+	"Reports" => "Reports",
+	"Time" => "Time",
+	"Date" => "Date",
+	"Reffer" => "Reffers",	
+	"Visitor" => "Visitor",
+	"Statistic" => "Statistic",
+	"Status" => "Status",
+	"Installations" => "Installations",
+	"Enable" => "Enable",
+	"Disable" => "Disable (recommended)",
+	"URI" => "URI (HTTP-address)",
+	"Admin_area_folder" => "Administrative area folder",
+
+
+		"Save" => "Salvesta",
+		"Cancel" => "Katkesta",
+		"Edit" => "Redigeeri",
+		"Edit_page" => "Redigeeri" ,
+		"Delete" => "Kustuta",
+		"View_page" => "Vaata lehekulge",
+		"Structure" => "Struktuur",
+		"Add_item" => "Lisa loik",
+		"Create_subitem" => "Loo all-loik",
+		"Logout" => "Valju",
+		"Template_select" => "Shabloon",
+		"Helpdesk_window" => "Abiaken",
+		"Close_window" => "Sule aken",
+		"NoUrl" => "Taida URL vali",
+		"You_are_sure" => "Oled kindel",
+		"Page_not_found" => "Lehekulge ei leitud",
+		"Page_not_found_desc" => "Lehekulg on suletud, umbernimetatud voi ligipaas ajutiselt puudub" ,
+		"Authorization_in_admin_area" => "Autoriseerimine admin.piirkonnas",
+		"Insert_Picture" => "Lisa pilt",
+		"Browse_Picture_Source" => "Vali pilt",
+		"Preview" => "Eelvaade",
+		"Alternate_Text" => "Kaastekst",
+		"Layout" => "Paigutus",
+		"Alignment" => "Tasandamine",
+		"Middle" => "Keskpaik",
+		"Bottom" => "Alla",
+		"Top" => "Ules",
+		"Right" => "Paremale",
+		"Left" => "Vasakule",
+		"Image" => "Pilt",
+		"Font" => "Kiri",
+		"Justify" => "Keskele",
+		"Can_not_save_uploaded_file" => "Ei onnestunud faili laadida",
+		"Border_Thickness" => "Raami paksus",
+		"Spacing" => "Sonavahe",
+		"Horizontal" => "Horisontaalne",
+		"Vertical" => "Vertikaalne",
+		"Cancel" => "Katkesta",
+		"Insert_Picture" => "Lisa pilt",
+		"Insert_File" => "Lisa fail",
+		"cleanHTML" => "Puhasta HTML",
+		"Indent" => "Taandrida",
+		"List" => "Nimestik",
+		"File" => "Fail",
+		"Page" => "Lehekulg",
+		"Show" => "Naita",
+		"Link" => "Link",
+		"Insert_Link" => "Lisa link",
+		"Page_name" => "Lehekulje nimi",
+		"Adding_item" => "All-lingi lisamine",
+		"Creating_subitem" => "Olemasoleva loigu loomine",
+		"Is_it_necessary_fill_fileld" => "Taida valjad",
+		"XHTML_error" => "Lehekulg puudub",
+		"send_message" => "Saada sonum",
+		"no_send_message" => "Sonumit ei onnestunud saata",
+
+
+		"Not_found_content_pointer" => "Ei leidnud uhtegi rahuldavat pointerit. Kontrolli xslt-faili jargmiselt: ",
+
+
+		"Url" => array( "title"=> "URL-dokumendi vali", "description" => "Kirjeldus" ),
+		"Title" => array( "title"=> "Dokumendi vali", "description" => "Dokumendiakna pealkiri" ),
+		"Template" => array("title"=> "Dokumendi shablooni vali", "description" => "Dokumendi andmete esitlus XSLT vormingus"),
+
+);
+?>
