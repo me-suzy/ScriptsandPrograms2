079521d97f3e55a24af4456dcfbf05c8a58182c4
me-suzy <me.suzana@gmail.com>
2025-08-11 19:46:53 +0300

    Add Content Management/chipmunkcms/board/admin/var.php

---
+++ Content Management/chipmunkcms/board/admin/var.php
@@ -0,0 +1,20 @@
+<?php
+$playrps="Yes";
+$usepms="Yes"; //alow pm use, yes or no
+$maxpms=20; //maximum number of pms in each users inbox
+$avatarheight=70;
+$avatarwidth=70;
+$allowavatar="Yes"; //Yes for Yes No for No
+$cookiename=chipmunkcookie; //name of your cookie
+$sitetitle="Chipmunk Forums -- PHP Help and support";  //name of your site
+$adminemail="webmaster@chipmunk-scripts.com"; //your admin e-mail
+$notifyme="No"; //notify you when people register
+$notifysub="Yes"; //notify you when people submit links to directory
+$boardpath="http://www.chipmunk-scripts.com/board";
+$guestposting="Yes"; //alow guest posting or not
+$allowsigs="Yes"; //allow user signatures, yes or no
+$requirekey="Yes"; //require users to activate their accoutns or not
+$numtopicsperpage='15'; // number of topics displayed per forum page
+$numrepliesperpage='20'; //number of replies displayed per thread page
+
+?>
