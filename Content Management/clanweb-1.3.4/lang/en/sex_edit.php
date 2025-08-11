0e45849b8faea3c0eee056f49f5b204854a6a1c2
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:27 +0300

    Add Content Management/clanweb-1.3.4/lang/en/sex_edit.php

---
+++ Content Management/clanweb-1.3.4/lang/en/sex_edit.php
@@ -0,0 +1,7 @@
+<select name="sex" class="textfelt">
+                <option>
+                <?=$sex?>
+                </option>
+                <option>Male</option>
+                <option>Female</option>
+              </select>
