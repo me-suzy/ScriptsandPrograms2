7680793cd6feb26760bea120917dba319844f28c
me-suzy <me.suzana@gmail.com>
2025-08-11 17:38:36 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/class/DO/Auth_user_md5.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/class/DO/Auth_user_md5.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Table Definition for auth_user_md5
+ */
+global $_PSL;
+require_once($_PSL['classdir'] . '/BE_DataObject.class');
+
+class DO_Auth_user_md5 extends BE_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    var $__table = 'auth_user_md5';                   // table name
+    var $user_id;                         // string(32)  not_null primary_key
+    var $username;                        // string(32)  not_null unique_key
+    var $password;                        // string(32)  not_null
+    var $perms;                           // string(255)  
+
+    /* ZE2 compatibility trick*/
+    function __clone() { return $this;}
+
+    /* Static get */
+    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DO_Auth_user_md5',$k,$v); }
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
