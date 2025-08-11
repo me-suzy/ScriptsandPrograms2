d34675c25782c025150a7ed93d0a152719887b77
me-suzy <me.suzana@gmail.com>
2025-08-11 19:48:34 +0300

    Add Content Management/clanweb-1.3.4/replace.php

---
+++ Content Management/clanweb-1.3.4/replace.php
@@ -0,0 +1,46 @@
+<?php
+// Replace-script ClanAdmin Tools by ArreliuS
+
+// Flags
+$newspost = str_replace(":sweden:","<img src=gfx/flags/se.gif border=0>",$newspost);
+$newspost = str_replace(":uniking:","<img src=gfx/flags/uk.gif border=0>",$newspost);
+$newspost = str_replace(":usa:","<img src=gfx/flags/us.gif border=0>",$newspost);
+$newspost = str_replace(":russia:","<img src=gfx/flags/ru.gif border=0>",$newspost);
+$newspost = str_replace(":china:","<img src=gfx/flags/cn.gif border=0>",$newspost);
+$newspost = str_replace(":switzerland:","<img src=gfx/flags/ch.gif border=0>",$newspost);
+$newspost = str_replace(":belgium:","<img src=gfx/flags/be.gif border=0>",$newspost);
+$newspost = str_replace(":italy:","<img src=gfx/flags/it.gif border=0>",$newspost);
+$newspost = str_replace(":denmark:","<img src=gfx/flags/dk.gif border=0>",$newspost);
+$newspost = str_replace(":norway:","<img src=gfx/flags/no.gif border=0>",$newspost);
+$newspost = str_replace(":chile:","<img src=gfx/flags/cl.gif border=0>",$newspost);
+$newspost = str_replace(":germany:","<img src=gfx/flags/de.gif border=0>",$newspost);
+$newspost = str_replace(":spain:","<img src=gfx/flags/es.gif border=0>",$newspost);
+$newspost = str_replace(":portugal:","<img src=gfx/flags/pt.gif border=0>",$newspost);
+$newspost = str_replace(":korea:","<img src=gfx/flags/kr.gif border=0>",$newspost);
+$newspost = str_replace(":taiwan:","<img src=gfx/flags/tw.gif border=0>",$newspost);
+$newspost = str_replace(":finland:","<img src=gfx/flags/fi.gif border=0>",$newspost);
+$newspost = str_replace(":france:)","<img src=gfx/flags/fr.gif border=0>",$newspost);
+$newspost = str_replace(":greece:)","<img src=gfx/flags/gr.gif border=0>",$newspost);
+$newspost = str_replace(":nederland:)","<img src=gfx/flags/nl.gif border=0>",$newspost);
+$newspost = str_replace(":iceland:)","<img src=gfx/flags/is.gif border=0>",$newspost);
+$newspost = str_replace(":jamaica:)","<img src=gfx/flags/jm.gif border=0>",$newspost);
+$newspost = str_replace(":brazil:)","<img src=gfx/flags/br.gif border=0>",$newspost);
+$newspost = str_replace(":bulgaria:)","<img src=gfx/flags/bg.gif border=0>",$newspost);
+$newspost = str_replace(":austrailia:)","<img src=gfx/flags/au.gif border=0>",$newspost);
+$newspost = str_replace(":turkey:)","<img src=gfx/flags/tr.gif border=0>",$newspost);
+
+// Other
+$newspost = str_replace(":cpl:)","<img src=gfx/flags/cpl.gif border=0>",$newspost);
+$newspost = str_replace(":cal:)","<img src=gfx/flags/cal.gif border=0>",$newspost);
+
+// HTML
+$motd = str_replace("[br]","<br>",$motd);
+$motd = str_replace("[b]","<b>",$motd);
+$motd = str_replace("[/b]","</b>",$motd);
+$motd = str_replace("[B]","<b>",$motd);
+$motd = str_replace("[/B]","</b>",$motd);
+$motd = str_replace("[i]","<i>",$motd);
+$motd = str_replace("[/i]","</i>",$motd);
+
+// EoF
+?>
