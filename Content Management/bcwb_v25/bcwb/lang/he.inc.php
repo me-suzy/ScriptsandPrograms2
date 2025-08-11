d060ca4527e69469aa16f009fd4250157307c161
me-suzy <me.suzana@gmail.com>
2025-08-11 18:00:39 +0300

    Add Content Management/bcwb_v25/bcwb/lang/he.inc.php

---
+++ Content Management/bcwb_v25/bcwb/lang/he.inc.php
@@ -0,0 +1,122 @@
+﻿<?PHP
+// vim: set expandtab tabstop=4 shiftwidth=4: 
+// +----------------------------------------------------------------------+
+// | BCWB: Business Card Web Builder                                      |
+// +----------------------------------------------------------------------+
+// | Author:  Dmitry Sheiko <sheiko@cmsdevelopment.com>	                  |
+// | Copyright (c) 2004 Dmitry Sheiko                                     |
+// | http://bcwb.cmsdevelopment.com                                       |
+// +----------------------------------------------------------------------+
+// | This source file is free software; you can redistribute it and/or    |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This source file is distributed in the hope that it will be useful,  |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// +----------------------------------------------------------------------+
+// Release: 23.02.04 (d/m/y)
+// $Id$
+
+$default_charset="UTF-8";
+
+/* 24/04/2004 - Hilay Selivansky - Hebrew and BiDi support*/
+$language_dir="RTL";
+
+$lang=array(
+
+	"Icon" => "Icon",
+	"CreateIcon" => "Create icon",
+	"Width" => "Width",
+	"Height" => "Height",
+
+	"Need_set_permissions" => "Need set permissions attributes to files/folders",
+	"Run" => "Run",
+	"Archive" => "Archive",
+	"Moved" => "Moved",
+	"Restore_backup" => "Restore backup",
+	"Create_backup" => "Create backup",
+	"Need_authorization" => "Here is a need to authorise",
+	"System_instaled" => "System was installed",
+	"Reports" => "Reports",
+	"Time" => "Time",
+	"Date" => "Date",
+	"Reffer" => "Reffers",	
+	"Visitor" => "Visitor",
+	"Statistic" => "Statistic",
+	"Status" => "Status",
+	"Installations" => "Installations",
+	"Enable" => "Enable",
+	"Disable" => "Disable (recommended)",
+	"URI" => "URI (HTTP-address)",
+	"Admin_area_folder" => "Administrative area folder",
+
+		"Save" => "שמור",
+		"Cancel" => "בטל",
+		"Edit" => "ערוך",
+		"Edit_page" => "ערוך דף",
+		"Delete" => "מחק", 
+		"View_page" => "צפה בדף",
+		"Structure" => "מבנה אתר",
+		"Add_item" => "הוסף רמה",
+		"Create_subitem" => "הוסף תת-רמה",
+		"Logout" => "התנתק",
+		"Template_select" => "תבנית",
+		"Helpdesk_window" =>"חלון עזרה",
+		"Close_window" => "סגור חלון",
+		"NoUrl" => "שדה זה הינו שדה חובה",
+		"You_are_sure" => "האם את/ה בטוח/ה?",
+		"Page_not_found" => "הדף אותו ביקשת אינו קיים",
+		"Page_not_found_desc" => "דף זה כנראה ואינו קיים עוד או שעבר למיקום שונה",
+		"Authorization_in_admin_area" => "אישור באזור מנהל",
+		"Insert_Picture" => "הכנס תמונה",
+		"Browse_Picture_Source" =>"בחר בתמונה",
+		"Preview" => "תצוגה מקדימה",
+		"Alternate_Text" => "טקסט נלווה", 
+		"Layout" => "מערך",
+		"Alignment" => "ישור",
+		"Middle" => "אמצעי",
+		"Bottom" => "תחתון",
+		"Top" => "עליון",
+		"Right" => "ימין",
+		"Left" => "שמאל",
+		"Image" => "תמונה",
+		"Font" => "גופן",
+		"Justify" => "אמצעי",
+		"Can_not_save_uploaded_file" => "לא ניתן להעלות קובץ",
+		"Border_Thickness" => "עובי מסגרת",
+		"Spacing" => "ריווח",
+		"Horizontal" => "אופקי",
+		"Vertical" =>"אנכי",
+		"Cancel" => "בטל",
+		"Insert_Picture" => "הכנס תמונה",
+		"Insert_File" =>"הכנס קובץ",
+		"cleanHTML" => "נקה תגי HTML",
+		"Indent" => "הזחה",
+		"List" => "רשימה",
+		"File" => "קובץ",
+		"Page" => "דף",
+		"Show" => "הצג",
+		"Link" => "לינק",
+		"Insert_Link" => "הכנס לינק",
+		"Link" => "לינק",
+		"Page_name" => "שם דף",
+		"Adding_item" => "הוסף רמה",
+		"Creating_subitem" => "הוסף תת-רמה",
+		"Is_it_necessary_fill_fileld" => "נא מלא את כל שדות הגדרת הדף, שמור את השינויים וערוך דף חדש",
+		"XHTML_error" => "המרת תוכן הדף לא הצליחה. הדף ימחק מהמערכת",
+		"send_message" => "ההודעה נשלחה בהצלחה",
+		"no_send_message" => "לא ניתן היה לשלוח את ההודעה",
+		
+		
+		"Not_found_content_pointer" => "מכיל התוכן לא נמצא. בדוק את קובץ XSLT עבור ההודעות הבאות:",
+		
+		
+		"Url" => array( "title"=> "Field ULR of document", "description" => "Variable from HTTP address of document. For example description, about" ),
+		"Title" => array( "title"=> "Field meta-heading of document", "description" => "Heading of document window. Its has special value for search machines."),
+		"Template" => array("title"=> "Field of pattern", "description" => "Pattern of document data presentation in XSLT format."),
+
+);
+?>
