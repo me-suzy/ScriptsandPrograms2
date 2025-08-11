52d1b87c44f122f135a6357c73abe6668fc4d9b6
me-suzy <me.suzana@gmail.com>
2025-08-11 19:34:43 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/wiki/page.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/wiki/page.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * $Header: /cvsroot/bitweaver/_bit_wiki/page.php,v 1.1.1.1.2.2 2005/07/26 15:50:32 drewslater Exp $
+ *
+ * Copyright (c) 2004 bitweaver.org
+ * Copyright (c) 2003 tikwiki.org
+ * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ *
+ * $Id: page.php,v 1.1.1.1.2.2 2005/07/26 15:50:32 drewslater Exp $
+ * @package wiki
+ * @subpackage functions
+ */
+
+/**
+ * required setup
+ */
+require_once( '../bit_setup_inc.php' );
+include_once (HTML_PKG_PATH.'htmlpages_lib.php');
+if ($feature_html_pages != 'y') {
+	$gBitSmarty->assign('msg', tra("This feature is disabled").": feature_html_pages");
+	$gBitSystem->display( 'error.tpl' );
+	die;
+}
+if (!$gBitUser->hasPermission( 'bit_p_view_html_pages' )) {
+	$gBitSmarty->assign('msg', tra("You dont have permission to use this feature"));
+	$gBitSystem->display( 'error.tpl' );
+	die;
+}
+if (!isset($_REQUEST["title"])) {
+	$gBitSmarty->assign('msg', tra("No page indicated"));
+	$gBitSystem->display( 'error.tpl' );
+	die;
+}
+$page_data = $htmlpageslib->get_html_page($_REQUEST["title"]);
+$gBitSmarty->assign('type', $page_data["type"]);
+$gBitSmarty->assign('refresh', $page_data["refresh"]);
+$gBitSmarty->assign('title', $_REQUEST["title"]);
+$parsed = $htmlpageslib->parse_html_page($_REQUEST["title"], $page_data["content"]);
+$gBitSmarty->assign_by_ref('parsed', $parsed);
+$section = 'html_pages';
+
+// Display the template
+$gBitSystem->display( 'bitpackage:wiki/page.tpl');
+?>
