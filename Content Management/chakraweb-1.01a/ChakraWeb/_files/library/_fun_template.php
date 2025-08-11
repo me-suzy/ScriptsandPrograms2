2f9e4e63f326345a54f02266e83e2914ad42ffb7
me-suzy <me.suzana@gmail.com>
2025-08-11 19:39:36 +0300

    Add Content Management/chakraweb-1.01a/ChakraWeb/_files/library/_fun_template.php

---
+++ Content Management/chakraweb-1.01a/ChakraWeb/_files/library/_fun_template.php
@@ -0,0 +1,14 @@
+<?php
+// ----------------------------------------------------------------------
+// ModName: 
+// Purpose: 
+// Author:  Mitra Dwi Wiyono (mdwiyono@quick4all.com)
+// ----------------------------------------------------------------------
+
+if (!defined('LOADED_AS_LIBRARY')) 
+    die ("You can't access [fun_name.php] file directly...");
+
+
+
+
+?>
