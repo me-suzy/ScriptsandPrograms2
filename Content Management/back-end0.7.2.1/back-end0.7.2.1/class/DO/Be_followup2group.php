ba5c67074409cfb1cb60ad032ae6d306b1fb2a5f
me-suzy <me.suzana@gmail.com>
2025-08-11 17:39:30 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/class/DO/Be_followup2group.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/class/DO/Be_followup2group.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Table Definition for be_followup2group
+ */
+global $_PSL;
+require_once($_PSL['classdir'] . '/BE_DataObject.class');
+
+class DO_Be_followup2group extends BE_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    var $__table = 'be_followup2group';               // table name
+    var $id;                              // int(10)  not_null primary_key unsigned auto_increment
+    var $groupType;                       // string(5)  not_null
+    var $followupID;                      // int(10)  not_null
+    var $groupID;                         // int(10)  not_null
+
+    /* ZE2 compatibility trick*/
+    function __clone() { return $this;}
+
+    /* Static get */
+    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DO_Be_followup2group',$k,$v); }
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
