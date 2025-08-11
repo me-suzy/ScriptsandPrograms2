cdb83fe4a96d43b73a8c1c07111cc6dd6f9352b1
me-suzy <me.suzana@gmail.com>
2025-08-11 18:07:09 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/install/install_data.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/install/install_data.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_install/install_data.php,v 1.1.1.1.2.4 2005/08/11 05:59:47 lsces Exp $
+ * @package install
+ * @subpackage functions
+ */
+
+// Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+// assign next step in installation process
+$gBitSmarty->assign( 'next_step',$step );
+
+$pumpList = false;
+foreach( array_keys( $gBitSystem->mPackages ) as $package ) {
+	if( $gBitInstaller->isPackageActive( $package ) ) {
+		if( file_exists( 'pump_'.$package.'_inc.php' ) ) {
+			$pumpList[] = $package;
+		}
+	}
+}
+$gBitSmarty->assign( 'pumpList',$pumpList );
+
+/**
+ * datapump setup
+ */
+if( isset( $_REQUEST['fSubmitDataPump'] ) ) {
+	foreach( $pumpList as $pump ) {
+		include_once( 'pump_'.$pump.'_inc.php' );
+	}
+	$gBitSmarty->assign( 'pumpedData',$pumpedData );
+	$app = '_done';
+	$gBitSmarty->assign( 'next_step',$step + 1 );
+	$gBitSystem->storePreference( 'wikiHomePage',$pumpedData['Wiki'][0] );
+} elseif( isset( $_REQUEST['skip'] ) ) {
+	$app = '_done';
+	$goto = $step + 1;
+	$gBitSmarty->assign( 'next_step',$goto );
+	header( "Location: install.php?step=$goto" );
+}
+?>
