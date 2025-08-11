f1ac0741ae764dbb583d302ff027515d3ec88c4b
me-suzy <me.suzana@gmail.com>
2025-08-11 17:49:12 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/public_html/censor.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/public_html/censor.php
@@ -0,0 +1,22 @@
+<?php
+   /**
+    * The format of the censor file is:
+    *
+    *   < ?php
+    *   $profan = array(
+    *      'badword',
+    *      'badimuswordimus',
+    *      'lots more badwords'
+    *   );
+    *   ? >
+    * This format is compatible with the phorum censor.php file.
+    *
+    */
+   $profan = array(
+    
+   // start list of comma separated "bad" words or phrases
+    
+   // end of "bad" words
+    
+   );
+?>
