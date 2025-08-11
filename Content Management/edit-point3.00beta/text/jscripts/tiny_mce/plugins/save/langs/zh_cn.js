2c9ac1ed8aa2d36bdfb3739d391df09544134179
me-suzy <me.suzana@gmail.com>
2025-08-12 02:45:20 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/save/langs/zh_cn.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/save/langs/zh_cn.js
@@ -0,0 +1,7 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµ&Auml;&Ouml;ÐÎ&Auml;&Ouml;§³&Ouml;
+
+tinyMCE.addToLang('',{
+save_desc : '±£´æ'
+});
+ 
