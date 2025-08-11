763e6ae159027cd9435cf4ccf83d5d1da23fc0a9
me-suzy <me.suzana@gmail.com>
2025-08-11 20:03:15 +0300

    Add Content Management/cms/cms/language/ariso.php

---
+++ Content Management/cms/cms/language/ariso.php
@@ -0,0 +1,152 @@
+<?php
+// ----------------------------------------------------------------------
+// Khaled Content Management System
+// Copyright (C) 2004 by Khaled Al-Shamaa.
+// http://www.al-shamaa.com/
+// ----------------------------------------------------------------------
+// LICENSE
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Filename: ar_iso.php
+// Original  Author(s): Khaled Al-Sham'aa khaled@al-shamaa.com
+// Purpose:  Define system messages in Arabic (ISO) language
+// ----------------------------------------------------------------------
+
+define("CHARSET", "iso-8859-6");
+define("DIRECTION", "RTL");
+define("ALIGN", "Right");
+
+define("MISS_USERID", "ÇäÑÌÇÁ ÅÏÎÇä ÇÓå ÇäåÓÊÎÏå");
+define("MISS_PW", "ÇäÑÌÇÁ ÅÏÎÇä ãäåÉ ÇäÓÑ");
+define("LBL_USERID", "ÇÓå ÇäåÓÊÎÏå");
+define("LBL_PW", "ãäåÉ ÇäÓÑ");
+define("LBL_REM", "ÊÐãÑæê");
+define("LBL_LOGIN", "ÊÓÌêä ÇäÏÎèä");
+define("LBL_LOGOUT", "ÊÓÌêä ÇäÎÑèÌ");
+define("LBL_INCORRECT", "Î×Ã áê ÇÓå ÇäåÓÊÎÏå Ãè ãäåÉ ÇäåÑèÑ");
+define("LBL_EDIT_LINK", "ÅæâÑ çæÇ äÊÙÏä åÍÊèêÇÊ çÐç ÇäÕáÍÉ");
+define("LBL_ADDPAGE", "ÃÖá ÕáÍÉ ÌÏêÏÉ åÊÕäÉ ÈçÐç ÇäÕáÍÉ");
+define("LBL_ADD", "ÅÖÇáÉ");
+define("LBL_EDIT", "ÊÙÏêä");
+define("LBL_EDIT_ACC", "ÊÙÏêä ÍÓÇÈã");
+define("LBL_TITLE", "ÙæèÇæ ÇäÕáÍÉ");
+define("LBL_CONTENT", "åÍÊèé ÇäÕáÍÉ");
+define("LBL_BACK", "ÇäÙèÏÉ Åäé");
+define("LBL_FULLNAME", "ÇäÇÓå ÇäãÇåä");
+define("LBL_HOME", "ÇäÕáÍÉ ÇäÑÆêÓêÉ");
+define("LBL_VIEWPAGE", "ÃØçÑ çÐç ÇäÕáÍÉ");
+define("INVALID_MSG", "åÙäèåÇÊ ÚêÑ ÕÍêÍÉ");
+define("LBL_ACTIVE", "âå ÈÊáÙêä çÐç ÇäÕáÍÉ");
+define("LBL_DEACTIVE", "ÃäÚ ÊáÙêä çÐç ÇäÕáÍÉ");
+define("LBL_DELETE", "ÅÍÐá çÐç ÇäÕáÍÉ");
+define("LBL_SCHADUAL", "ÌÏèä çÐç ÇäÕáÍÉ ÒåæêÇ");
+define("LBL_LINKS", "äÇÆÍÉ ÇäèÕäÇÊ ÇäÊÔÙÈêÉ");
+define("CONFIRM_DEL_MSG", "çä ÃæÊ åÊÃãÏ åæ Ãæã ÊÑêÏ ÍÐá çÐç ÇäÕáÍÉ¿");
+define("CONFIRM_DEL_LBL", "ÊÃãêÏ ÇäÍÐá");
+define("SITE_TITLE", "æØÇå ÑÇåÇ äÅÏÇÑÉ ÇäåÍÊèé");
+define("TELL_SUBJECT", "ÑÈåÇ Êãèæ åçÊåÇ ÈçÐç ÇäÕáÍÉ!");
+define("TELL_BODY", "ÑÈåÇ ÊÑÚÈ áê ÊÕáÍ åÍÊèé çÐç ÇäÕáÍÉ: ");
+define("LBL_FAV", "ÃÖá çÐç ÇäÕáÍÉ Åäé âÇÆåÉ ÇäåáÖäÉ äÏêã");
+define("LBL_TELL", "ÃÑÓä ÙæèÇæ çÐç ÇäÕáÍÉ Åäé ÕÏêâ");
+define("LBL_PRINT", "Å×ÈÙ çÐç ÇäÕáÍÉ");
+define("LBL_PDF", "æÓÎÉ Èê Ïê Åá åæ åÍÊèé çÐç ÇäÕáÍÉ");
+define("LBL_VIEWS1", "äâÏ Êå ÒêÇÑÉ çÐç ÇäÕáÍÉ ");
+define("LBL_VIEWS2", " åÑÉ");
+define("LBL_MAP", "ÎÑê×É ÇäåèâÙ");
+define("LBL_SEARCH", "ÇäÈÍË");
+define("LBL_SEARCH_CMD", "ÅÈÍË");
+define("LBL_TERM", "æÊÇÆÌ ÇäÈÍË Ùæ ");
+define("LBL_UNDER", "äÇÊÒÇä çÐç ÇäÕáÍÉ âêÏ ÇäÅæÔÇÁ");
+define("EMAIL_THANKS", "ÔãÑÇ äÅÊÕÇäã ÈæÇ¬ Óèá æâèå ÈÇäÑÏ Ùäêã ÎäÇä ÇäÃÑÈÙ èÇäÙÔÑêæ ÓÇÙÉ ÇäÊÇäêÉ");
+define("EMAIL_SORRY", "ÙÐÑÇ äãæ ÑÓÇäÊã äå êÊå ÈËçÇ æØÑÇ äèÌèÏ Ù×ä áæê áê ÇäåèâÙ¬ ÇäÑÌÇÁ ÇäåÍÇèäÉ áêåÇ ÈÙÏ");
+define("EMAIL_ALERT", "ÇäÑÌÇÁ ÇäÊÃãÏ åæ ÕÍÉ ÙæèÇæ ÇäÈÑêÏ ÇäÅäãÊÑèæê ÇäÐê âåÊ ÈÅÏÎÇäç");
+define("EMAIL_CONFIRM", "çä ÃæÊ åÊÃãÏ åæ ÕÍÉ ÙæèÇæ ÇäÈÑêÏ ÇäÅäãÊÑèæê ÇäÐê âåÊ ÈÅÏÎÇäç¿");
+define("EMAIL_NAME", "ÇäÇÓå");
+define("EMAIL_ADDRESS", "ÙæèÇæ ÇäÈÑêÏ ÇäÇäãÊÑèæê");
+define("EMAIL_SUBJECT", "ÇäåèÖèÙ");
+define("EMAIL_BODY", "æÕ ÇäÑÓÇäÉ");
+define("EMAIL_SEND", "ÅÑÓÇä");
+define("EMAIL_LINK", "ÅÊÕä ÈæÇ");
+define("LBL_RESET", "ÊÑÇÌÙ");
+define("LBL_META", "åÙÑñáÇÊ åêÊñÇ");
+define("LBL_FROM", "åæ");
+define("LBL_TO", "Åäé");
+define("ALT_CALENDAR", "ÍÏÏ ÇäÊÇÑêÎ");
+define("INVALID_DATE", "ÕêÚÉ ÊÇÑêÎ ÎÇ×ÆÉ (êèå/ÔçÑ/ÓæÉ) çê ÇäÕêÚÉ ÇäÕÍêÍÉ");
+define("LBL_ORD_UP", "Åæâä çÐç ÇäèÕäÉ ÇäÊÔÙÈêÉ ääÃÙäé");
+define("LBL_ORD_DOWN", "Åæâä çÐç ÇäèÕäÉ ÇäÊÔÙÈêÉ Åäé ÇäÃÓáä");
+
+define("LBL_TO_SELL", "âå ÈÈêÙ çÐç ÇäÓäÙÉ");
+define("LBL_PRICE", "ÇäËåæ");
+define("LBL_CART", "ÓäÉ ÊÓèâê");
+define("LBL_CART_ADD", "ÃÖá çÐç ÇäÓäÙÉ Åäé ÓäÉ ÇäÊÓèâ");
+define("LBL_CART_DEL", "âå ÈÍÐá çÐç ÇäÓäÙÉ");
+
+define("IN_CART1", "çæÇã");
+define("IN_CART2", "ÓäÙÉ áê ÓäÊã");
+define("EMPTY_CART_CONFIRM", "çä ÃæÊ åÊÃãÏ åæ Ãæã ÊÑê ÅáÑÇÚ ÇäÓäÉ åæ åÍÊèêÇÊçÇ¿");
+define("EMPTY_CART", "ÅáÑÇÚ ÓäÉ ÇäÊÓèâ");
+define("CART_ITEM_TITLE", "ÇäÓäÙÉ");
+define("CART_ITEM_QUANTITY", "ÇäãåêÉ");
+define("CART_ITEM_PRICE", "ÇäËåæ");
+define("CART_ITEM_SUBTOTAL", "ÇäåÌÇåêÙ ÇäÌÒÆêÉ");
+define("DEL_CART_CONFIRM", "çä ÃæÊ åÊÃãÏ åæ Ãæã ÊÑêÏ ÅÒÇäÉ çÐç ÇäÓäÙÉ åæ ÓäÊã¿");
+define("CART_TOTAL", "ÇäåÌåèÙ Çäãäê");
+define("CART_CLOSE", "ÊÇÈÙ ÇäÊÓèâ");
+define("CART_CHECKOUT", "ÃÊå ÙåäêÉ ÇäÔÑÇÁ");
+define("LBL_PHONE", "Ñâå ÇäçÇÊá");
+define("LBL_NAME", "ÇäÇÓå ÇäãÇåä");
+define("LBL_ADDRESS", "ÇäÙæèÇæ");
+define("LBL_CITY", "ÇäåÏêæÉ");
+define("LBL_STATE", "ÇäåÍÇáØÉ");
+define("LBL_ZIP", "ÕæÏèâ ÇäÈÑêÏ");
+define("LBL_COUNTRY", "ÇäÏèäÉ");
+define("LBL_SELECT", "ÇäÑÌÇÁ ÇäÇÎÊêÇÑ");
+define("LBL_BUY", "ÅÔÊÑê ÇäÂæ");
+define("CHECKDONE", "äâÏ âåÊ ÈÙåäêÉ ÇäÏáÙ¬ Óèá æÊÕä Èã âÑêÈÇ. êåãæã ÊáÍÕ áÇÊèÑÊã Ùæ ×Ñêâ ÇäæâÑ Ùäé ÇäÑÇÈ× ÇäÊÇäê (ÇäÑÌÇÁ ÍáØç åæ ÃÌä ÓÌäÇÊã ÇäÎÇÕÉ):");
+define("LBL_MARQUEE", "ÔÑê× ÇäÃÎÈÇÑ");
+
+define("LBL_BACKUP", "ÊÕÏêÑ");
+define("LBL_RESTORE", "ÇÓÊêÑÇÏ");
+define("LBL_COPY", "æÓÎ Åäé ÇäÍÇáØÉ");
+define("LAST_UPDATED_AT", "ÂÎÑ ÊÙÏêä áê");
+define("LAST_UPDATED_BY", "ÈèÇÓ×É");
+
+define("LBL_INTERACTIVE", "Çäæå× ÇäÊáÇÙäê");
+define("MODE_NONE", "ÈÏèæ (ÇäÇáÊÑÇÖê)");
+define("MODE_ADMIN", "ÊÓÊÙÑÖ áâ× åæ âÈä åÏêÑ ÇäæØÇå");
+define("MODE_MODIRATED", "ÊÓÊÙÑÖ åæ âÈä ÇäÒèÇÑ (Ïèæ åÑÇâÈÉ)");
+define("MODE_OPEN", "ÊÓÊÙÑÖ åæ âÈä ÇäÒèÇÑ (åÙ åÑÇâÈÉ)");
+define("MODE_EMAIL", "æåèÐÌ åÑÓä ÈÇäÈÑêÏ ÇäÅäãÊÑèæê");
+define("INTERAVTIVE_ADD", "ÅÖÇáÉ ÌÏêÏÉ");
+define("ENTRY_NAME", "ÇäÇÓå");
+define("ENTRY_EMAIL", "ÇäÈÑêÏ ÇäÇäãÊÑèæê");
+define("ENTRY_CONTENT", "ÇäåÍÊèé");
+define("ENTRY_ADD", "ÃÖá");
+define("ENTRY_DEL", "ÅÍÐá çÐç ÇäåÔÇÑãÉ");
+define("ENTRY_SHOW", "ÃØçÑ çÐç ÇäåÔÇÑãÉ");
+define("ENTRY_HIDE", "ÃÎáê çÐç ÇäåÔÇÑãÉ");
+define("ENTRY_DEL_MSG", "çä ÃæÊ åÊÃãÏ åæ Ãæã ÊÑÚÈ áê ÍÐá çÐç ÇäåÔÇÑãÉ¿");
+
+define("NO_PRIVILEGE", "ÙÐÑÇë¬ äêÓ äÏêã ÇäÕäÇÍêÉ ÇäÊê ÊÓåÍ äã ÈÇäâêÇå ÈÐäã");
+define("LBL_UPLOAD", "ÊÍåêä åäá Åäé ÇäåèâÙ");
+define("CMD_UPLOAD", "ÊÍåêä");
+define("ERR_FILE_EXIST", "çæÇäã åäá ÂÎÑ Ùäé ÇäåèâÙ êÍåä ÐÇÊ ÇäÇÓå");
+define("ERR_FILE_BIG", "ÍÌå Çäåäá ÇäÐê ÊÍÇèä ÊÍåêäç êÊÌÇèÒ ÇäÍÏ ÇäÃâÕé ÇäåÓåèÍ Èç");
+define("ERR_FILE_FAILD", "äâÏ áÔäÊ ÙåäêÉ ÇäÊÍåêä¬ ÇäÑÌÇÁ ÅÙÇÏÉ ÇäåÍÇèäÉ åÌÏññÏÇë");
+define("ERR_FILE_BLOCKED", "ÙÐÑÇ¬ äêÓ ÈÇÓÊ×ÇÙÊã ÊÍåêä åäá åæ Ðäã ÇäæèÙ");
+define("F_NAME", "ÇÓå Çäåäá");
+define("F_SIZE", "ÍÌå Çäåäá");
+define("F_TIME", "Òåæ ÊÍåêä Çäåäá");
+?>
