6e4a778e1a5ddb9838749db11370ee017eb8b1d5
me-suzy <me.suzana@gmail.com>
2025-08-12 02:41:49 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/insertdatetime/langs/pl.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/insertdatetime/langs/pl.js
@@ -0,0 +1,12 @@
+// PL lang variables
+
+tinyMCE.addToLang('',{
+insertdate_def_fmt : '%Y-%m-%d',
+inserttime_def_fmt : '%H:%M:%S',
+insertdate_desc : 'Wstaw aktualn¹ datê',
+inserttime_desc : 'Wstaw aktualny czas',
+inserttime_months_long : new Array("Styczeñ", "Luty", "Marzec", "Kwiecieñ", "Maj", "Czerwiec", "Lipiec", "Sierpieñ", "Wrzesien", "Padziernik", "Listopad", "Grudzieñ"),
+inserttime_months_short : new Array("Stcz", "Lut", "Mar", "Kwi", "Maj", "Czer", "Lip", "Sier", "Wrze", "Pa", "List", "Grudz"),
+inserttime_day_long : new Array("Niedziela", "Poniedzia³ek", "Wtorek", "roda", "Czwartek", "Pi¹tek", "Sobota", "Niedziela"),
+inserttime_day_short : new Array("Nie", "Pon", "Wto", "ro", "Czw", "Pi¹", "Sob", "Nie")
+});
