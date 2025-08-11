74c096ba0b9e4ba277422b45053181cb854cacd4
me-suzy <me.suzana@gmail.com>
2025-08-11 18:00:43 +0300

    Add Content Management/bcwb_v25/bcwb/lang/it.inc.php

---
+++ Content Management/bcwb_v25/bcwb/lang/it.inc.php
@@ -0,0 +1,114 @@
+﻿<?PHP
+// vim: set expandtab tabstop=4 shiftwidth=4: 
+// +----------------------------------------------------------------------+
+// | BCWB: Business Card Web Builder                                      |
+// +----------------------------------------------------------------------+
+// | Author:  Dmitry Sheiko <sheiko@cmsdevelopment.com>	                  |
+// | Copyright (c) 2004 Dmitry Sheiko                                     |
+// | http://bcwb.cmsdevelopment.com                                       |
+// +----------------------------------------------------------------------+
+// | This source file is free software; you can redistribute it and/or    |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This source file is distributed in the hope that it will be useful,  |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// +----------------------------------------------------------------------+
+// Release: 23.02.04 (d/m/y)
+// $Id$
+
+$default_charset = "UTF-8";
+
+// Italian language
+// Translator: Lanzani Marco
+
+$lang=array(
+	"Icon" => "Icon",
+	"CreateIcon" => "Create icon",
+	"Width" => "Width",
+	"Height" => "Height",
+
+	"Need_set_permissions" => "Need set permissions attributes to files/folders",
+	"Run" => "Run",
+	"Archive" => "Archive",
+	"Moved" => "Moved",
+	"Restore_backup" => "Restore backup",
+	"Create_backup" => "Create backup",
+	"Need_authorization" => "Here is a need to authorize",
+	"System_instaled" => "System was installed",
+	"Reports" => "Reports",
+	"Time" => "Time",
+	"Date" => "Date",
+	"Reffer" => "Reffers",	
+	"Visitor" => "Visitor",
+	"Statistic" => "Statistic",
+	"Status" => "Status",
+	"Installations" => "Installations",
+	"Enable" => "Enable",
+	"Disable" => "Disable (recommended)",
+	"URI" => "URI (HTTP-address)",
+	"Admin_area_folder" => "Administrative area folder",
+"Save" => "Salva",
+"Cancel" => "Annulla",
+"Edit" => "Modifica",
+"Edit_page" => "Modifica la pagina",
+"Delete" => "Elimina",
+"View_page" => "Visualizza",
+"Structure" => "Struttura",
+"Add_item" => "Aggiungi elemento",
+"Create_subitem" => "Crea sotto-elemento",
+"Logout" => "Esci",
+"Template" => "Template",
+"Help_window" => "Help",
+"Close_window" => "Chiudi",
+"This_field should be filled" => "Questo campo deve essere riempito",
+"Are_you sure" => "Sei sicuro",
+"The_page_was_not_found" => "La pagina non è stata trovata",
+"This page may have been moved, or may no longer exist" => "Questa pagina può essere stata spostata o rimossa",
+"Authorization_in_administration_area" => "Autorizzazione nell'area di Amministrazione",
+"Insert_Picture" => "Inserisci l'immagine",
+"Browse_Picture Source" => "Browse delle immagini",
+"Preview" => "Anteprima",
+"Accompanying Text" => "Accompagnare Testo",
+"Layout" => "Layout",
+"Alignment" => "Allineamento",
+"Middle" => "Parte Centrale",
+"Bottom" => "Parte Inferiore",
+"Top" => "Parte Superiore",
+"Right" => "Parte Destra",
+"Left" => "Parte Sinistra",
+"Image" => "Immagine",
+"Font" => "Font",
+"Centering" => "Centrare",
+"Can_not_upload_file" => "Il file non può essere uploadato",
+"Border thickness" => "Spessore del Bordo",
+"Spacing" => "Spaziatura",
+"Horizontal" => "Orizzontale",
+"Vertical" => "Verticale",
+"Insert_File" => "Inserisci File",
+"Clean_HTML" => "Pulisci codice HTML",
+"Kibe_spacing" => "Spaziatura di Kibe",
+"List" => "Lista",
+"File" => "File",
+"Page" => "Pagina",
+"Show" => "Visualizza",
+"Link" => "Collegamento",
+"Insert_Link" => "Inserisci Collegamento",
+"Page_name" => "Nome della pagina",
+"Add_section" => "Aggiungi sezione",
+"Creating_subsection" => "Creazione della sottosezione",
+"Fill_in_page properties fields, save changes and proceed to editing new page" => "Riempi i campi delle proprietà della pagina, salva le modifiche e continua nella modifica della nuova pagina",
+"Convertor XHTML could_not process the page contents. The page will be removed" => "Il parser XHTML non ha potuto analizzare il contenuto della pagina. La pagina sarà rimossa",
+"Container of contents was not found. Check xslt-file for the following structures: " => "Il contenitore dei contenuti non è stato trovato. Controlla il file XSLT per le seguenti strutture: ",
+"send_message" => "Invia Messaggio",
+"no_send_message" => "It was not possible to send the message",
+
+	
+		"Url" => array( "title"=> "URL", "description" => "Variabile dall'indirizzo del HTTP del documento. Per esempio descrizione, circa" ),
+		"Title" => array( "title"=> "Meta-Title", "description" => "Intestazione della finestra del documento. Valore significativo per i Motori di Ricerca."),
+		"Template" => array("title"=> "Template", "description" => "Template di presentazione di dati del documento tramite XSLT."),
+
+); ?>
