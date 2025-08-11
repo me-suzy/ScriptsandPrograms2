e207c918e57d86db80cb9b7ebc6272b67dfa94b5
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:30 +0300

    Add Content Management/clanweb-1.3.4/lang/se/help_match.php

---
+++ Content Management/clanweb-1.3.4/lang/se/help_match.php
@@ -0,0 +1,11 @@
+
+<ul>
+  <li>Lag 1 och 2: Lagen som spelade</li>
+  <li>Typ: PCW/CB eller turnering</li>
+  <li>Fyll i lag1s poäng resp. lag2s poäng</li>
+  <li>Kartor: Vilka kartor spelades? Om det var 2 kartor separera med ett &quot;/&quot;-tecken.</li>
+  <li>Lineup: Vilka spelade? Separera med &quot;,&quot;-tecken. Alla nick ska 
+    skrivas in utan klan-tag.</li>
+  <li>Rapport: N&aring;got speciellt som h&auml;nde under matchen som kan vara 
+    v&auml;rt att veta? </li>
+</ul>
