17533bdced8b3917cb5cdb90f23ab01e3cb43c11
me-suzy <me.suzana@gmail.com>
2025-08-11 18:00:27 +0300

    Add Content Management/bcwb_v25/bcwb/lang/by.inc.php

---
+++ Content Management/bcwb_v25/bcwb/lang/by.inc.php
@@ -0,0 +1,124 @@
+﻿<?PHP
+// vim: set expandtab tabstop=4 shiftwidth=4: 
+// +----------------------------------------------------------------------+
+// | BCWB: Business Card Web Builder                                      |
+// +----------------------------------------------------------------------+
+// | Author:  Dmitry Sheiko <sheiko@cmsdevelopment.com>	                  |
+// | Copyright (c) 2004 Dmitry Sheiko                                     |
+// | http://bcwb.cmsdevelopment.com                                       |
+// +----------------------------------------------------------------------+
+// | This source file is free software; you can redistribute it and/or    |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This source file is distributed in the hope that it will be useful,  |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// +----------------------------------------------------------------------+
+// Release: 23.02.04 (d/m/y)
+// $Id$
+
+$default_charset = "UTF-8";
+
+$lang=array(
+	"Icon" => "Icon",
+	"CreateIcon" => "Create icon",
+	"Width" => "Width",
+	"Height" => "Height",
+
+	"Need_set_permissions" => "Требуется назначить атрибуты файлам/папкам",
+	"Run" => "Выполнить",
+	"Archive" => "Архив",
+	"Moved" => "Перемещены",
+	"Restore_backup" => "Восстановить резервную копию",
+	"Create_backup" => "Создать резервную копию",
+	"Need_authorization" => "Требуется авторизация",
+	"System_instaled" => "Система была успешно инсталированна",
+	"Reports" => "Отчеты",
+	"Time" => "Время",
+	"Date" => "Дата",
+	"Reffer" => "Ссылающиеся страницы",	
+	"Visitor" => "Посетитель",
+	"Statistic" => "Статистика",
+	"Status" => "Состояние",
+	"Installations" => "Установки",
+	"Enable" => "Есть",
+	"Disable" => "Нет (рекомендуется)",
+	"URI" => "URI (HTTP-адрес)",
+	"Admin_area_folder" => "Папка для области администрирования",
+	
+
+  "Save" => "Захаваць",
+  "Cancel" => "Адмяніць",
+  "Edit" => "Рэдагаваць",
+  "Edit_page" => "Рэдагаваць",
+  "Delete" => "Знішчыць",
+  "View_page" => "Праглядзець старонку",
+  "Structure" => "Структура",
+  "Add_item" => "Дадаць раздзел",
+  "Create_subitem" => "Стварыць/Дадаць падраздзел",
+  "Logout" => "Выхад",
+  "Template_select" => "Шаблон",
+  "Helpdesk_window" => "Акно інфармацыйнай дапамогі",
+  "Close_window" => "Закрыць акно",
+  "NoUrl" => "Трэба запоўніць поле URL",
+  "You_are_sure" => "Вы ўпэўнены",
+  "Page_not_found" => "Немагчыма знайсці старонку",
+  "Page_not_found_desc" => "Магчыма, гэтая старонка была знішчана, перайменавана ці яна часова не даступная.",
+  "Authorization_in_admin_area" => "Аўтарызацыя на адміністратыўную тэрыторыю",
+  "Insert_Picture" => "Уставіць выяву/малюнак",
+  "Browse_Picture_Source" => "Выбраць файл з выявай/малюнкам",
+  "Preview" => "Прагляд",
+  "Alternate_Text" => "Суправаджальны тэкст",
+  "Layout" => "Размяшчэнне",
+  "Alignment" => "Выроўніванне",
+  "Middle" => "Пасярэдзіне",
+  "Bottom" => "Унізе",
+  "Top" => "Уверсе",
+  "Right" => "Справа",
+  "Left" => "Злева",
+  "Image" => "Выява/малюнак",
+  "Font" => "Шрыфт",
+  "Justify" => "Цэнтроўка",
+  "Can_not_save_uploaded_file" => "Нельга загрузіць файл",
+  "Border_Thickness" => "Таўшчыня абводкі",
+  "Spacing" => "Водступы",
+  "Horizontal" => "Гарызантальны",
+  "Vertical" => "Вертыкальны",
+  "Cancel" => "Адмена",
+  "Insert_Picture" => "Уставіць выяву",
+  "Insert_File" => "Уставіць файл",
+  "cleanHTML" => "Ачысціць HTML",
+  "Indent" => "Водступ",
+  "List" => "Спіс",
+  "File" => "Файл",
+  "Page" => "Старонка",
+  "Show" => "Адлюстроўваць",
+  "Link" => "Спасылка",
+  "Insert_Link" => "Уставіць спасылку",
+  "Link" => "Спасылка",
+  "Page_name" => "Назва страронкі",
+  "Adding_item" => "Дабаўленне раздзела",
+  "Creating_subitem" => "Стварэнне ўкладзенага падраздзела",
+  "Is_it_necessary_fill_fileld" => "Патрэбна запоўніць палі свойстваў старонкі,
+захаваць змены і перайсці да рэдагавання новай старонкі.",
+  "XHTML_error" => "Канвертар HTML2XHTML не здолеў апрацаваць змест старонкі. Старонка будзе знішчана.",
+		"send_message" => "Сообщение успешно доставлено",
+		"no_send_message" => "Не удалось отправить сообщение",
+
+
+
+  "Not_found_content_pointer" => "Не знойдзена ні воднага кантэйнера
+зместу. Праверце xslt-файл на прадмет наступных канструкцый: ",
+
+  "Url" => array( "title"=> "Поле URL дакумента", "description" =>
+"Пераменная з HTTP-адраса дакумента. Напрыклад, about" ),
+  "Title" => array( "title"=> "Поле мета-назва дакумента", "description"
+=> "Загаловак акна дакумента. Мае асаблівую вагу для пошукавых машын."),
+  "Template" => array("title"=> "Поле шаблона дакумента", "description" =>
+"Шаблон прадстаўлення дадзеных дакумента ў фармаце XSLT"),
+
+);
+?>
