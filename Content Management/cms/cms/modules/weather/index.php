e995bfac66148b7b41c689a622d441eda6c8cf09
me-suzy <me.suzana@gmail.com>
2025-08-11 20:03:20 +0300

    Add Content Management/cms/cms/modules/weather/index.php

---
+++ Content Management/cms/cms/modules/weather/index.php
@@ -0,0 +1,12 @@
+<table align=center border=0><tr><td>
+<script src='http://voap.weather.com/weather/oap/SYXX0008?template=GENXV&par=null&unit=1&key=0c3f4da0b649ea385cad4903c0f80618'></script>
+</td><td>
+<script src='http://voap.weather.com/weather/oap/SYXX0003?template=GENXV&par=null&unit=1&key=0c3f4da0b649ea385cad4903c0f80618'></script>
+</td><td>
+<script src='http://voap.weather.com/weather/oap/SYXX0004?template=GENXV&par=null&unit=1&key=0c3f4da0b649ea385cad4903c0f80618'></script>
+</td><td>
+<script src='http://voap.weather.com/weather/oap/LEXX0003?template=GENXV&par=null&unit=1&key=0c3f4da0b649ea385cad4903c0f80618'></script>
+</td><td>
+<script src='http://voap.weather.com/weather/oap/JOXX0002?template=GENXV&par=null&unit=1&key=0c3f4da0b649ea385cad4903c0f80618'></script>
+</td><td>
+</td></tr></table>
