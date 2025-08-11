83bc58e66448980ec0d08f859d8d5221cdb9d0c5
me-suzy <me.suzana@gmail.com>
2025-08-11 17:42:28 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/class/locale/en_CA.LC_TIME.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/class/locale/en_CA.LC_TIME.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Local date/time information for PHPSlash (en_CA locale)
+ *
+ * $Id: en_CA.LC_TIME.php,v 1.2 2005/04/28 22:06:58 mgifford Exp $
+ */
+
+$pslstrings = array(
+                 'date_format_long'       => '%B %e, %Y',
+                 'date_format_short'      => '%m/%d/%Y',
+                 'time_format_12'         => '%I:%M%p',
+                 'time_format_24'         => '%H:%M',
+                 'date_time_format_long'  => '%A %B %e, %Y %I:%M %p',
+                 'date_time_format_short' => '%m/%d/%Y %H:%M'
+              );
+
+
+?>
