b211e59292b494bf5d2b1a1f7a71d385a601b896
me-suzy <me.suzana@gmail.com>
2025-08-12 02:54:31 +0300

    Add Content Management/encapscms-0.3.6/encapscms-0.3.6/core/core.php

---
+++ Content Management/encapscms-0.3.6/encapscms-0.3.6/core/core.php
@@ -0,0 +1,15 @@
+<?
+include_once($root."core/Config.php");
+include_once($root."core/DB_sql.php");
+//include_once($root."core/Header.php");
+include_once($root."core/BlogsCats.php");
+//include_once($root."core/PageController.php");
+include_once($root."core/Block.php");
+include_once($root."core/Block_sub.php");
+//include_once($root."core/Lang.php");
+
+include_once($root."core/Gallery.php");
+include_once($root."core/Pager.php");
+include_once($root."core/GalleryCategory.php");
+include_once($root."core/Misc.php");
+?>
