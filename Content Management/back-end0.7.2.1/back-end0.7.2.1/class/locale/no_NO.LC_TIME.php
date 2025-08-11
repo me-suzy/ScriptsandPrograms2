3b85ed978b72795da60cf19010f07318f6d86bfb
me-suzy <me.suzana@gmail.com>
2025-08-11 17:42:51 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/class/locale/no_NO.LC_TIME.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/class/locale/no_NO.LC_TIME.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Local date/time information for PHPSlash (no locale)
+ *
+ * $Id: no_NO.LC_TIME.php,v 1.2 2005/04/28 22:06:58 mgifford Exp $
+ */
+
+$pslstrings = array(
+                 'date_format_long'       => '%e. %B %Y',
+                 'date_format_short'      => '%d/%m/%Y',
+                 'time_format_12'         => '%Ih%M%p',
+                 'time_format_24'         => '%H:%M',
+                 'date_time_format_long'  => '%a %e. %B %Y %H:%M',
+                 'date_time_format_short' => '%d/%m/%Y %H:%M'
+              );
+
+?>
