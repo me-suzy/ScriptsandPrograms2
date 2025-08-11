68e253fc36f39e81435210d63ee2777e1f815437
me-suzy <me.suzana@gmail.com>
2025-08-12 02:33:17 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/advimage/langs/he.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/advimage/langs/he.js
@@ -0,0 +1,27 @@
+// HE lang variables
+
+tinyMCE.addToLang('advimage',{
+tab_general : 'ëììé',
+tab_appearance : 'îøàä',
+tab_advanced : 'îú÷ãí',
+general : 'ëììé',
+title : 'ëåúøú',
+preview : 'úöåâä î÷ãéîä',
+constrain_proportions : 'äâáìú îéîãéí',
+langdir : 'ëéååï ùôä',
+langcode : '÷åã ùôä',
+long_desc : '÷éùåø ìäñáø àøåê',
+style : 'ñâðåï',
+classes : 'Classéí',
+ltr : 'ùîàì ìéîéï',
+rtl : 'éîéï ìùîàì',
+id : 'Id',
+image_map : 'îôú úîåðä',
+swap_image : 'äçìôú úîåðä',
+alt_image : 'úîåðä çìåôéú',
+mouseover : 'ëùñîï äòëáø îòì äúîåðä',
+mouseout : 'ëùñîï äòëáø òåæá àú äúîåðä',
+misc : 'àçø',
+example_img : 'úöåâä&nbsp;î÷ãéîä&nbsp;ùì&nbsp;îøàä',
+missing_alt : 'äàí àúä áèåç ùáøöåðê ìäîùéê îáìé ìëìåì úéàåø ùì äúîåðä? ììà úéàåø, éúëï ëé äúîåðä ìà úäéä ðâéùä ìîùúîùéí áòìé îåâáìåéåú, àå ëàìå äîùúîùéí áãôãôï è÷ñèåàìé, àå ùîñééøéí áøùú áìé ìäöéâ úîåðåú.'
+});
