07b52d21f25418312f9d85b56372bc059e5a3894
me-suzy <me.suzana@gmail.com>
2025-08-11 17:39:13 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/class/DO/Be_categories.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/class/DO/Be_categories.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Table Definition for be_categories
+ */
+global $_PSL;
+require_once($_PSL['classdir'] . '/BE_DataObject.class');
+
+class DO_Be_categories extends BE_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    var $__table = 'be_categories';                   // table name
+    var $category_id;                     // int(5)  not_null primary_key auto_increment
+    var $category_type;                   // string(8)  not_null multiple_key
+    var $category_code;                   // string(8)  not_null
+    var $languageID;                      // string(3)  not_null multiple_key
+    var $name;                            // string(50)  not_null
+
+    /* ZE2 compatibility trick*/
+    function __clone() { return $this;}
+
+    /* Static get */
+    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DO_Be_categories',$k,$v); }
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
