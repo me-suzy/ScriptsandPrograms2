9a64906fa87f9e5f7342d4dc91391da4d48ef59e
me-suzy <me.suzana@gmail.com>
2025-08-11 19:34:25 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/wiki/download_wiki_attachment.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/wiki/download_wiki_attachment.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * $Header: /cvsroot/bitweaver/_bit_wiki/download_wiki_attachment.php,v 1.1.1.1.2.1 2005/06/27 17:47:42 lsces Exp $
+ *
+ * Copyright (c) 2004 bitweaver.org
+ * Copyright (c) 2003 tikwiki.org
+ * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ *
+ * $Id: download_wiki_attachment.php,v 1.1.1.1.2.1 2005/06/27 17:47:42 lsces Exp $
+ * @package wiki
+ * @subpackage functions
+ */
+
+/**
+ * required setup
+ */
+require_once( '../bit_setup_inc.php' );
+if (!$gBitUser->hasPermission( 'bit_p_wiki_view_attachments' ) && !$gBitUser->hasPermission( 'bit_p_wiki_admin_attachments' )) {
+	die;
+}
+if (!isset($_REQUEST["att_id"])) {
+	die;
+}
+$info = $gBitSystem->get_wiki_attachment($_REQUEST["att_id"]);
+$w_use_db = $gBitSystem->getPreference('w_use_db', 'y');
+$w_use_dir = $gBitSystem->getPreference('w_use_dir', '');
+$gBitSystem->add_wiki_attachment_hit($_REQUEST["att_id"]);
+$type = &$info["filetype"];
+$file = &$info["filename"];
+$content = &$info["data"];
+//print("File:$file<br/>");
+//die;
+header ("Content-type: $type");
+//header( "Content-Disposition: attachment; filename=$file" );
+header ("Content-Disposition: inline; filename=\"$file\"");
+if ($info["path"]) {
+	readfile ($w_use_dir . $info["path"]);
+} else {
+	echo "$content";
+}
+?>
