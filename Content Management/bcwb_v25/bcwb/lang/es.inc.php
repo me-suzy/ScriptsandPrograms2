fa6f5cddb4e226f01b3d0463b443105006c94cdb
me-suzy <me.suzana@gmail.com>
2025-08-11 18:00:34 +0300

    Add Content Management/bcwb_v25/bcwb/lang/es.inc.php

---
+++ Content Management/bcwb_v25/bcwb/lang/es.inc.php
@@ -0,0 +1,77 @@
+﻿<?PHP
+// vim: set expandtab tabstop=4 shiftwidth=4: 
+// +----------------------------------------------------------------------+
+// | BCWB: Business Card Web Builder                                      |
+// +----------------------------------------------------------------------+
+// | Author:  Dmitry Sheiko <sheiko@cmsdevelopment.com>	                  |
+// | Copyright (c) 2004 Dmitry Sheiko                                     |
+// | http://bcwb.cmsdevelopment.com                                       |
+// +----------------------------------------------------------------------+
+// | This source file is free software; you can redistribute it and/or    |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This source file is distributed in the hope that it will be useful,  |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// +----------------------------------------------------------------------+
+// Release: 23.02.04 (d/m/y)
+// $Id$
+
+$default_charset = "UTF-8";
+
+// Spanish language
+// Translation powered by http://www.systransoft.com/ 
+
+$lang=array(
+
+	"Icon" => "Icon",
+	"CreateIcon" => "Create icon",
+	"Width" => "Width",
+	"Height" => "Height",
+
+	"Need_set_permissions" => "Need set permissions attributes to files/folders",
+	"Run" => "Run",
+	"Archive" => "Archive",
+	"Moved" => "Moved",
+	"Restore_backup" => "Restore backup",
+	"Create_backup" => "Create backup",
+	"Need_authorization" => "Here is a need to authorise",
+	"System_instaled" => "System was installed",
+	"Reports" => "Reports",
+	"Time" => "Time",
+	"Date" => "Date",
+	"Reffer" => "Reffers",	
+	"Visitor" => "Visitor",
+	"Statistic" => "Statistic",
+	"Status" => "Status",
+	"Installations" => "Installations",
+	"Enable" => "Enable",
+	"Disable" => "Disable (recommended)",
+	"URI" => "URI (HTTP-address)",
+	"Admin_area_folder" => "Administrative area folder",
+"Save" => "Conservar", "Cancel" => "Anular", "Edit" => "Redactar", "Edit_page" => "Redactar", "Delete" => "Quitar", "View_page" => " Ver la pбgina ", 
+"Structure" => "la Estructura", "Add_item" => " Aсadir el reparto ", 
+"Create_subitem" => " Crear la subsecciуn ", "Logout" => "la_Salida", 
+"Template_select" => "el Modelo", "Helpdesk_window" => " la Ventana de la ayuda informativa ", 
+"Close_window" => " Cerrar la ventana ", 
+"NoUrl" => " Hace falta llenar el campo URL ", "You_are_sure" => " sois asegurados ", "Page_not_found" => " Es imposible encontrar la pбgina ", "Page_not_found_desc" => " Es posible, esta pбgina fue quitado, es cambiado de nombre, o ella es temporalmente inaccesible. ", "Authorization_in_admin_area" => "..  en el campo de la administraciуn ", "Insert_Picture" => " Insertar la imagen ", "Browse_Picture_Source" => " Escoger el fichero de la imagen ", "Preview" => "la Presentaciуn", "Alternate_Text" => " el texto De acompaсamiento ", "Layout" => "la Instalaciуn", "Alignment" => "la Alineaciуn", 
+"Middle" => "el Medio", "Bottom" => "Hacia abajo", "Top" => "Hacia arriba", "Right" => "el Derecho", "Left" => "es izquierda", "Image" => "la Imagen", "Font" => "la Letra", "Justify" => "el Centrado", "Can_not_save_uploaded_file" => " no ha conseguido cargar el fichero ", "Border_Thickness" => " el Espesor ... ", 
+"Spacing" => "los Apartes", "Horizontal" => "Horizontal", "Vertical" => "Vertical", 
+"Cancel" => "la Anulaciуn", "Insert_Picture" => " Insertar la imagen ", 
+"Insert_File" => " Insertar el fichero ", "cleanHTML" => " Limpiar HTML ", 
+"Indent" => "el Aparte", "List" => "List", "File" => "el Fichero", 
+"Page" => "la Pбgina", "Show" => "Representar", 
+"Link" => "la Referencia", "Insert_Link" => " Insertar la referencia ", 
+"Link" => "la Referencia", "Page_name" => " el Nombre de la pбgina ", 
+"Adding_item" => " la Adiciуn del reparto ", 
+"Creating_subitem" => " la Creaciуn de la subsecciуn metida ", 
+"Is_it_necessary_fill_fileld" => " Hace falta llenar los campos de las propiedades de la pбgina, conservar los cambios y pasar a la redacciуn de la pбgina nueva. ", 
+"XHTML_error" => " el Convertidor HTML2XHTML no el smog tratar el mantenimiento de la pбgina. La pбgina serб quitada. ", "send_message" => " el Mensaje es con йxito entregado ", 
+"no_send_message" => " no ha conseguido enviar el mensaje ", 
+"Not_found_content_pointer" => " no es encontrado ni un contenidor del mantenimiento. Comprueben el xslt-fichero con el fin de las construcciones siguientes: ", "Url" => array ("title" => " Paul URL del documento ", "description" => " la Variable de la HTTP-direcciуn del documento. Por ejemplo, about "), "Title" => array ("title" => " Paul del meta-encabezamiento del documento ", "description" => " el Encabezamiento de la ventana del documento. Tiene el significado especial para las mбquinas de prospecciуn. "), "Template" => array ("title" => " Paul del modelo del documento ", "description" => " el Modelo de la representaciуn de dados el documento en el formato XSLT "),
+
+);
+?>
