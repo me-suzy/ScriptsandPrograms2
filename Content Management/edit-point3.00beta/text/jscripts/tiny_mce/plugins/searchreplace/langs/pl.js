3dbe51b49f37477160e8e787ee6848e2c3c4069f
me-suzy <me.suzana@gmail.com>
2025-08-12 02:46:01 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/searchreplace/langs/pl.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/searchreplace/langs/pl.js
@@ -0,0 +1,21 @@
+// PL lang variables
+
+tinyMCE.addToLang('',{
+searchreplace_search_desc : 'Znajd',
+searchreplace_searchnext_desc : 'Znajd ponownie',
+searchreplace_replace_desc : 'Znajd/Zast¹p',
+searchreplace_notfound : 'Ukoñczono wyszukiwanie. Poszukiwana fraza nie zosta³a odnaleziona.',
+searchreplace_search_title : 'Znajd',
+searchreplace_replace_title : 'Znajd/Zast¹p',
+searchreplace_allreplaced : 'Wszystkie wyst¹pienia poszukiwanej frazy zosta³y zast¹pione. ',
+searchreplace_findwhat : 'Znajd',
+searchreplace_replacewith : 'Zast¹p',
+searchreplace_direction : 'Kierunek',
+searchreplace_up : 'Do góry',
+searchreplace_down : 'Do do³u',
+searchreplace_case : 'Wielkoæ liter',
+searchreplace_findnext : 'Znajd&nbsp;nastêpny',
+searchreplace_replace : 'Zast¹p',
+searchreplace_replaceall : 'Zast¹p&nbsp;wszystkie',
+searchreplace_cancel : 'Wyjd'
+});
