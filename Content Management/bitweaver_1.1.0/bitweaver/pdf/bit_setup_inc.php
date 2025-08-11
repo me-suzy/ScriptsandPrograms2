e0b4366392c028eabb7376be550536dadc774842
me-suzy <me.suzana@gmail.com>
2025-08-11 18:17:09 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/pdf/bit_setup_inc.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/pdf/bit_setup_inc.php
@@ -0,0 +1,17 @@
+<?php
+global $gBitSystem;
+$gBitSystem->registerPackage( 'pdf', dirname( __FILE__ ).'/', TRUE, LIBERTY_SERVICE_DOCUMENT_GENERATION );
+if( $gBitSystem->isPackageActive( 'pdf' ) ) {
+	$gLibertySystem->registerService( LIBERTY_SERVICE_DOCUMENT_GENERATION, PDF_PKG_NAME, array(
+		'content_icon_tpl' => 'bitpackage:pdf/pdf_service_icons.tpl',
+	) );
+	if( !empty( $_REQUEST['style'] ) ) {
+		global $gPreviewStyle;
+		$gPreviewStyle = $_REQUEST['style'];
+	}
+	if( !empty( $_REQUEST['no_force'] ) ) {
+		global $gNoForceStyle;
+		$gNoForceStyle = TRUE;
+	}
+}
+?>
