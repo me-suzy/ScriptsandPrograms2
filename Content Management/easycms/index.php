7888eedfbea98d260f6ac1adb556eaf401cf6f86
me-suzy <me.suzana@gmail.com>
2025-08-11 20:14:30 +0300

    Add Content Management/easycms/index.php

---
+++ Content Management/easycms/index.php
@@ -0,0 +1,62 @@
+<?php include 'top.php' ?>
+
+<table border=0 cellpadding=0 cellspacing=0 bgcolor="#003366" width="150" >
+
+	<tr>
+
+	<td>
+
+ <table border=0 cellpadding=1 cellspacing=1 bgcolor="#003366" width="400" >
+
+				<tr>
+
+    			<td>
+
+			  		   <table border=0 cellpadding=1 cellspacing=1 background="images/back.gif" width=100%>
+
+			  		   <tr>
+
+    		  		   <td align="left"><b>Easy Content Management System </b></td>
+
+			  		   </tr>
+
+			  		   </table>
+
+					   <table border=0 cellpadding=1 cellspacing=1 bgcolor="#ffffff" width=100%>
+
+			  		   <tr>
+
+    		  		   <td align="left">
+
+		   <p class="inner"> This is as it's name implies, an easy to use content management system.
+It let's you manage a completely template driven site.  For more please see the demo.
+
+</p>
+
+					    </td>
+
+			  		   </tr>
+
+			  		   </table>
+
+					  
+
+			  </td>
+
+			  </tr>
+
+			  </table>
+
+ </td>
+
+			  </tr>
+
+			  </table>
+
+
+
+
+
+
+
+<?php include 'bottom.php' ?>
