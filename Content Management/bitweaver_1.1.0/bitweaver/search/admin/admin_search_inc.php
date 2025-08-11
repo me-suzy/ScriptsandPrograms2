04441f6fe336796eeb98582362c85eca338a0608
me-suzy <me.suzana@gmail.com>
2025-08-11 18:31:25 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/search/admin/admin_search_inc.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/search/admin/admin_search_inc.php
@@ -0,0 +1,65 @@
+<?php
+
+// $Header: /cvsroot/bitweaver/_bit_search/admin/admin_search_inc.php,v 1.1.1.1.2.2 2005/10/30 09:27:43 lsces Exp $
+
+// Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+$formSearchToggles = array(
+	'feature_search_fulltext' => array(
+		'label' => 'Fulltext Search',
+		'note' => 'Enable Fulltext Search of all content. This enables users to serach the content of wiki pages, articles, blogs and other similar content.',
+//		'page' => 'FullTextSearch',
+	),
+	'feature_search_stats' => array(
+		'label' => 'Search Statistics',
+		'note' => 'Record searches made and their frequency.',
+//		'page' => 'SearchStats',
+	),
+);
+$formSearchInts = array(
+	'search_refresh_rate' => array(
+		'label' => 'Search Refresh Rate',
+		'note' => 'Varies the rate at which updates to the search index are made, 1 = every page read, while rate>1 will introduce a random chance of a refresh every "rate" pages',
+	),
+	'search_min_wordlength' => array(
+		'label' => 'Minimum number of letters for search words',
+		'note' => 'By settings this value to 3, you can ignore search words such as "a" or "or", however searches for a number like "13" will be ignored as well.',
+	),
+	'search_max_syllwords' => array(
+		'label' => 'Maximum number of words',
+		'note' => 'The maximum number of words containing a syllable that can be serached for in any one search.',
+	),
+	'search_syll_age' => array(
+		'label' => 'Age in hours of search cache',
+		'note' => 'Define the Maximum age of cached search results for any given syllable. The results cache will be used to provide a search result if it is available, and will be cleared after either the age, or when the results cache reaches it\'s limit',
+	),
+	'search_lru_purge_rate' => array(
+		'label' => 'Least Recently Used (LRU) list purging rate',
+		'note' => 'Purge the results cache every "rate" pages. This will keep space available in the cache for new seach results',
+	),
+	'search_lru_length' => array(
+		'label' => 'Least Recently Used (LRU) list length',
+		'note' => 'Limit the results cache to this number of entries',
+	),
+);
+if (isset($_REQUEST["searchprefs"])) {
+	
+	foreach( $formSearchInts as $item => $data ) {
+		simple_set_int( $item );
+		$formSearchInts[$item]['value'] = $_REQUEST[$item];
+	}
+
+	foreach( $formSearchToggles as $item => $data ) {
+		simple_set_toggle( $item );
+	}
+} else {
+	foreach( $formSearchInts as $item => $data ) {
+		$formSearchInts[$item]['value'] = $gBitSystem->mPrefs[$item];
+	}
+}
+$gBitSmarty->assign( 'formSearchToggles',$formSearchToggles );
+$gBitSmarty->assign( 'formSearchInts',$formSearchInts );
+
+
+?>
