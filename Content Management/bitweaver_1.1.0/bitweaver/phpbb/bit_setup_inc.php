62746d627781d9a477fce347c33375656c07b57f
me-suzy <me.suzana@gmail.com>
2025-08-11 18:18:00 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/phpbb/bit_setup_inc.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/phpbb/bit_setup_inc.php
@@ -0,0 +1,11 @@
+<?php
+global $gBitSystem;
+
+$gBitSystem->registerPackage( 'phpbb', dirname( __FILE__ ).'/' );
+if( $gBitSystem->isPackageActive( 'phpbb' ) ) {
+	$gBitSystem->registerAppMenu( 'phpbb', 'Forums', PHPBB_PKG_URL.'index.php', 'bitpackage:phpbb/menu_phpbb.tpl' );
+	if( file_exists( PHPBB_PKG_PATH.'config.php' ) ) {
+		require_once( PHPBB_PKG_PATH.'config.php' );
+	}
+}
+?>
