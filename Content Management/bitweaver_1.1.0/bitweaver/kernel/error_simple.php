55bd24c0d8f483bb6b7532430cee2e0e7743eb83
me-suzy <me.suzana@gmail.com>
2025-08-11 18:08:22 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/kernel/error_simple.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/kernel/error_simple.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_kernel/error_simple.php,v 1.1.1.1.2.3 2005/07/12 11:54:34 spiderr Exp $
+ * @package kernel
+ * @subpackage functions
+ */
+
+echo '<html><body><pre><p>';
+if (isset($_REQUEST['error']) and !is_null($_REQUEST['error'])) {
+	echo strip_tags($_REQUEST['error']);
+} else {
+	echo 'There was an unspecified error.  Please go back and try again.';
+}
+?>
+</p>
+<form name="loginbox" action="<?=USERS_PKG_URL?>validate.php" method="post">
+user: <input type="text" name="user"  size="20" /><br />
+pass: <input type="password" name="pass" size="20" /><br />
+<input type="submit" name="login" value="login" /></form>
+
+<p><a href="javascript:history.back()">Go back</a></p></pre></body></html>
+
