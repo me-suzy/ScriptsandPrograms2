d6cfb0e043bb95dcc536f347c15455c731df62e5
me-suzy <me.suzana@gmail.com>
2025-08-12 02:53:30 +0300

    Add Content Management/encapscms-0.3.6/encapscms-0.3.6/admin/blogs.php

---
+++ Content Management/encapscms-0.3.6/encapscms-0.3.6/admin/blogs.php
@@ -0,0 +1,15 @@
+<?
+$page = "blogs";
+include("blogs_head.php");
+?><table border="0"><tr><td valign="top"><?
+
+$cats->show('cats_manage.html');
+
+?></td><td><?
+
+include("html/bloks.html");
+
+?></td></tr></TABLE><?
+
+include("common_foot.php");
+?>
