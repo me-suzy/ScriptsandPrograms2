0d373c2c10069e7b3af9f46dbcf3002cc8211b5b
me-suzy <me.suzana@gmail.com>
2025-08-11 18:06:42 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/hotwords/hotword_lib.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/hotwords/hotword_lib.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_hotwords/hotword_lib.php,v 1.1.1.1.2.5 2005/08/07 16:23:23 lsces Exp $
+ * @package hotwords
+ */
+
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_hotwords/hotword_lib.php,v 1.1.1.1.2.5 2005/08/07 16:23:23 lsces Exp $
+ * @package hotwords
+ */
+class HotwordsLib extends BitBase {
+	function HotwordsLib() {				
+	BitBase::BitBase();
+	}
+	/**
+	 * List hotwords
+	 * @return Data array of hotwords
+	 * [data] is the actual array of words
+	 * [cant] is a count of the number of entries in [data]
+	 */
+	function list_hotwords($offset = 0, $maxRecords = -1, $sort_mode = 'word_desc', $find = '') {
+
+		if ($find) {
+			$findesc = $this->mDb->qstr('%' . strtoupper( $find ) . '%');
+			$mid = " where UPPER(`word`) like ?";
+			$bindvars = array($findesc);
+		} else {
+			$mid = '';
+			$bindvars = array();
+		}
+
+		$query = "select * from `".BIT_DB_PREFIX."tiki_hotwords` $mid order by ".$this->mDb->convert_sortmode($sort_mode);
+		$query_cant = "select count(*) from `".BIT_DB_PREFIX."tiki_hotwords` $mid";
+		$result = $this->mDb->query($query,$bindvars,$maxRecords,$offset);
+		$cant = $this->mDb->getOne($query_cant,$bindvars);
+		$ret = array();
+
+		while ($res = $result->fetchRow()) {
+			$ret[] = $res;
+		}
+
+		$retval = array();
+		$retval["data"] = $ret;
+		$retval["cant"] = $cant;
+		return $retval;
+	}
+
+	/**
+	 * Add hotword
+	 * 
+	 * @param word		Word to be replaced by link
+	 * @param url		Url to be used with that word 
+	 */
+	function add_hotword($word, $url) {
+		$word = addslashes($word);
+
+		$url = addslashes($url);
+		$query = "delete from `".BIT_DB_PREFIX."tiki_hotwords` where `word`=?";
+		$result = $this->mDb->query($query,array($word));
+		$query = "insert into `".BIT_DB_PREFIX."tiki_hotwords`(`word`,`url`) values(?,?)";
+		$result = $this->mDb->query($query,array($word,$url));
+		return true;
+	}
+
+	/**
+	 * Remove hotword
+	 * 
+	 * @param word		Word to be removed
+	 */
+	function remove_hotword($word) {
+		$query = "delete from `".BIT_DB_PREFIX."tiki_hotwords` where `word`=?";
+		$result = $this->mDb->query($query,array($word));
+	}
+
+	/**
+	 * Replace hotword
+	 *
+	 * @param line		Text to be modified by adding links
+	 * @param words		Words to be replaced by links 
+	 * @return String with hotword link
+	 */
+	function replace_hotwords($line, $words) {
+		global $gBitSystem;
+		$hotw_nw = ($gBitSystem->isFeatureActive( 'feature_hotwords_nw' )) ? "onkeypress='popUpWin(this.href,'fullScreen',0,0);' onclick='popUpWin(this.href,'fullScreen',0,0);return false;'" : '';
+
+		// Replace Hotwords
+		foreach ($words as $word => $url) {
+			// \b is a word boundary, \s is a space char
+			$line = preg_replace("/^$word(\b)/i","<a href=\"$url\" $hotw_nw>$word</a>$1",$line);
+			$line = preg_replace("/\s$word(\b)/i"," <a href=\"$url\" $hotw_nw>$word</a>$1",$line);
+		}
+
+		return $line;
+	}
+
+	/**
+	 * Get hotwords
+	 *
+	 * @return Array of hotwords
+	 */
+	function get_hotwords() {
+		static $retHotwords = NULL;
+		if( !isset( $retHotwords ) ) {
+			$query = "select * from `".BIT_DB_PREFIX."tiki_hotwords`";
+			$result = $this->mDb->query($query, array());
+			$retHotwords = array();
+			while ($res = $result->fetchRow()) {
+				$retHotwords[$res["word"]] = $res["url"];
+			}
+		}
+		return $retHotwords;
+	}
+
+}
+
+/**
+ * @global HotwordsLib Hotwords library
+ */
+global $hotwordlib;
+$hotwordlib = new HotwordsLib();
+
+?>
