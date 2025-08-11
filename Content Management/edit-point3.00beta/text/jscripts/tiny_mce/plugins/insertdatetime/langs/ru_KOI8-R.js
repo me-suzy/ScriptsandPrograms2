cf6915545e112a2544237132808d69415496d2f3
me-suzy <me.suzana@gmail.com>
2025-08-12 02:41:54 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/insertdatetime/langs/ru_KOI8-R.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/insertdatetime/langs/ru_KOI8-R.js
@@ -0,0 +1,12 @@
+// RU lang variables KOI8-R
+
+tinyMCE.addToLang('',{
+insertdate_def_fmt : '%d %m %Y',
+inserttime_def_fmt : '%H:%M:%S',
+insertdate_desc : '÷ÓÔÁ×ÉÔØ ÄÁÔÕ',
+inserttime_desc : '÷ÓÔÁ×ÉÔØ ×ÒÅÍÑ',
+inserttime_months_long : new Array("ÑÎ×ÁÒÑ", "ÆÅ×ÒÁÌÑ", "ÍÁÒÔÁ", "ÁÐÒÅÌÑ", "ÍÁÑ", "ÉÀÎÑ", "ÉÀÌÑ", "Á×ÇÕÓÔÁ", "ÓÅÎÔÑÂÒÑ", "ÏËÔÑÂÒÑ", "ÎÏÑÂÒÑ", "ÄÅËÁÂÒÑ"),
+inserttime_months_short : new Array("ÑÎ×", "ÆÅ×", "ÍÁÒÔ", "ÁÐÒ", "ÍÁÊ", "ÉÀÎØ", "ÉÀÌØ", "Á×Ç", "ÓÅÎÔ", "ÏËÔ", "ÎÂÒ", "ÄÅË"),
+inserttime_day_long : new Array("×ÏÓËÒÅÓÅÎØÅ", "ÐÏÎÅÄÅÌØÎÉË", "×ÔÏÒÎÉË", "ÓÒÅÄÁ", "ÞÅÔ×ÅÒÇ", "ÐÑÔÎÉÃÁ", "ÓÕÂÂÏÔÁ", "×ÏÓËÒÅÓÅÎØÅ"),
+inserttime_day_short : new Array("×ÓË", "ÐÎ", "×Ô", "ÓÒ", "ÞÔ×", "ÐÔÎ", "ÓÂ", "×ÓË")
+});
