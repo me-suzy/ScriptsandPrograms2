4dad83ba27124a7654a20006bee923053a92c909
me-suzy <me.suzana@gmail.com>
2025-08-11 18:00:47 +0300

    Add Content Management/bcwb_v25/bcwb/lang/kr.inc.php

---
+++ Content Management/bcwb_v25/bcwb/lang/kr.inc.php
@@ -0,0 +1,114 @@
+﻿<?PHP
+// vim: set expandtab tabstop=4 shiftwidth=4: 
+// +----------------------------------------------------------------------+
+// | BCWB: Business Card Web Builder                                      |
+// +----------------------------------------------------------------------+
+// | Author:  Dmitry Sheiko <sheiko@cmsdevelopment.com>	                  |
+// | Copyright (c) 2004 Dmitry Sheiko                                     |
+// | http://bcwb.cmsdevelopment.com                                       |
+// +----------------------------------------------------------------------+
+// | This source file is free software; you can redistribute it and/or    |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This source file is distributed in the hope that it will be useful,  |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// +----------------------------------------------------------------------+
+// Release: 23.02.04 (d/m/y)
+// $Id$
+
+$default_charset = "UTF-8";
+
+// Korean language
+// Translation powered by http://www.systransoft.com/ 
+
+$lang=array(
+
+	"Icon" => "Icon",
+	"CreateIcon" => "Create icon",
+	"Width" => "Width",
+	"Height" => "Height",
+
+	"Need_set_permissions" => "Need set permissions attributes to files/folders",
+	"Run" => "Run",
+	"Archive" => "Archive",
+	"Moved" => "Moved",
+	"Restore_backup" => "Restore backup",
+	"Create_backup" => "Create backup",
+	"Need_authorization" => "Here is a need to authorise",
+	"System_instaled" => "System was installed",
+	"Reports" => "Reports",
+	"Time" => "Time",
+	"Date" => "Date",
+	"Reffer" => "Reffers",	
+	"Visitor" => "Visitor",
+	"Statistic" => "Statistic",
+	"Status" => "Status",
+	"Installations" => "Installations",
+	"Enable" => "Enable",
+	"Disable" => "Disable (recommended)",
+	"URI" => "URI (HTTP-address)",
+	"Admin_area_folder" => "Administrative area folder",
+
+"Save" => "﻿를 제외하고",
+"Cancel" => "취소",
+"Edit" => "편집하십시요",
+"Edit page" => "페이지를 편집하십시요",
+"Delete" => "감소",
+"View page" => "전망 페이지",
+"Structure" => "구조",
+"Add section" => "단면도를 추가하십시요",
+"Create_subsection" => "일부를 창조하십시요",
+"Logout" => "로그아웃",
+"Template" => "템플렛",
+"Help window" => "도움말 윈도우",
+"Close window" => "가까운 창",
+"This field should be filled" => "이 분야는 채워야 한다",
+"Are you sure" => "너는 확실히 이다",
+"The page was not found" => "페이지는 발견하지 않았다",
+"This page may have been moved, or may no longer exist" => "이 페이지는 이동할지도 모르고지 않, 또는 더이상 존재할지도 모른다",
+"Authorization in administration area" => "행정 지역안에 허가",
+"Insert Picture" => "삽입 그림",
+"Browse Picture Source" => "그림 근원을 찾아보십시요",
+"Preview" => "시사",
+"Accompanying Text" => "원본을 부대한",
+"Layout" => "배치",
+"Alignment" => "줄맞춤",
+"Middle" => "중간",
+"Bottom" => "바닥",
+"Top" => "정상",
+"Right" => "권리",
+"Left" => "좌",
+"Image" => "심상",
+"Font" => "글꼴",
+"Centering" => "중심에 둠",
+"Can not upload file" => "파일을 올려주기한 할 수 있지 않는다",
+"Border thickness" => "간격을 접경하십시요",
+"Spacing" => "간격",
+"Horizontal" => "수평한",
+"Vertical" => "수직",
+"Insert File" => "삽입 파일",
+"Clean HTML" => "HTML을 청소하십시요",
+"Kibe spacing" => "Kibe간격",
+"List" => "명부",
+"File" => "파일",
+"Page" => "페이지",
+"Show" => "쇼",
+"Link" => "연결",
+"Insert Link" => "삽입 연결",
+"Page name" => "페이지 이름",
+"Add section" => "단면도를 추가하십시요",
+"Creating subsection" => "일부를 창조한",
+"Fill in page properties fields, save changes and proceed to editing new page" => "안으로 변화를 제외하고 페이지 재산 분야를, 채우고 새로운 페이지를 편집하기에 진행하십시요",
+"Convertor XHTML could not process the page contents. The page will be removed" => "변환기XHTML은 페이지 내용을 가공할 수 없었다. 페이지는 제거될 것이다",
+"Container of contents was not found. Check xslt-file for the following structures: " => "내용의 콘테이너는 발견하지 않았다. 뒤에 오는 구조를 위해 체크xslt파일: ",
+	
+		"Url" => array( "title"=> "문서의 분야ULR", "description" => "문서의HTTP주소에서 가변. 예를 들면 묘사, 대략" ),
+		"Title" => array( "title"=> "문서의 분야 메타산 표제", "description" => "문서 창의 표제. 그것의 수색 기계를 위해 특별한 가치를 있는다."),
+		"Template" => array("title"=> "본의 분야", "description" => "XSLT체재안에 문서 자료 발표의 본."),
+
+);
+?>
