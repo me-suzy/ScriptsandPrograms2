af7db7d05a30ee58b9a3304cd661dcca414a02d1
me-suzy <me.suzana@gmail.com>
2025-08-11 17:39:04 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/class/DO/Be_card2petition.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/class/DO/Be_card2petition.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Table Definition for be_card2petition
+ */
+global $_PSL;
+require_once($_PSL['classdir'] . '/BE_DataObject.class');
+
+class DO_Be_card2petition extends BE_DataObject 
+{
+    ###START_AUTOCODE
+    /* the code below is auto generated do not remove the above tag */
+
+    var $__table = 'be_card2petition';                // table name
+    var $cardID;                          // int(11)  not_null primary_key unsigned
+    var $petitionID;                      // int(11)  not_null primary_key unsigned
+
+    /* ZE2 compatibility trick*/
+    function __clone() { return $this;}
+
+    /* Static get */
+    function staticGet($k,$v=NULL) { return DB_DataObject::staticGet('DO_Be_card2petition',$k,$v); }
+
+    /* the code above is auto generated do not remove the tag below */
+    ###END_AUTOCODE
+}
