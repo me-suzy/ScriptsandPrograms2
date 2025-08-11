dd2e2ade7e9a437d4d55be74e456f50c65ae68fd
me-suzy <me.suzana@gmail.com>
2025-08-11 17:42:49 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/class/locale/no.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/class/locale/no.php
@@ -0,0 +1,412 @@
+<?php
+/*
+*  no ( Norwegian) Language file for PHPSlash
+*  translations named using POSIX locale conventions ( en, es, de, etc)
+*
+*  Please don't use HTML code here! 
+*/
+   $pslstrings = array(
+
+      // about.php
+      // article.php
+      // backend.php
+      // comment.php
+      "No Subject Given" =>
+      "Tittel ikke spesifisert",
+
+      "Re: " =>
+      "Ad: ",
+
+      "Anonymous" =>
+      "Anonym",
+
+      "Comments" =>
+      "Kommentarer",
+
+      "You didn't supply a good submit value" =>
+      "Du oppga ikke en gyldig verdi",
+
+      // commentShow.php
+      // commentSubmit.php
+      // config.php
+      
+      "Login" =>
+      "Log inn",
+
+      "Logout" =>
+      "Log ut",
+      
+      "Frontpage" =>
+      "Forsiden",
+
+      "Home" =>
+      "Hjem",
+
+      "Add Story" =>
+      "Bidra med sak",
+
+      "Archives" =>
+      "Arkiv",
+
+      "Polls" =>
+      "Avstemninger",
+
+      "About" =>
+      "Om PDA-Nytt",
+
+      "Topic" =>
+      "Områder",
+
+      "Story" =>
+      "Sak",
+
+      "Submission" =>
+      "Bidrag",
+
+      "Section" =>
+      "Seksjoner", 
+
+      "Block" =>
+      "Blokker",
+
+      "Poll" =>
+      "Avstemming",
+
+      "Author" =>
+      "Forfattere",
+
+      "Glossary" =>
+      "Ordbok",
+
+      "Mailing List" =>
+      "E-postliste",
+
+      "Variable" =>
+      "variabel",
+
+      "Logging" =>
+      "Logging",
+
+      // functions.inc
+      "NO TITLE" =>
+      "INGEN TITTEL",
+
+      "@!#$" =>
+      "@!#$",
+
+      // glossary.php
+      // index.php
+      // indextpl3col.php
+      // login.php
+      "%s logged out." =>
+      "%s logget ut.",
+      // mailinglist.php
+      // poll.php
+      "VOTE: " =>
+      "AVSTEMNING: ",
+
+      "View Pollbooth" =>
+      "Se avstemning",
+
+       "%s Poll" =>
+       "%s Avstemning",
+      // pollBooth.php
+      // search.php
+      "Searching %s" =>
+      "Søker %s",
+
+      "All Topics" =>
+      "Alle områder",
+
+      "All Sections" =>
+      "Alle Seksjoner",
+
+      "All Authors" =>
+      "Alle Forfattere",
+      // submisson.php
+      "Thanks for this submission.  We have it and will set our fearless editorial staff upon it right this second." =>
+      "Takk for innleveringen. Vi har den, og vil sette vårt uredde reporter-team på saken med en gang.",
+      
+      "Something broke, I'm not sure what though??" =>
+      "Noe gikk i stykker, men jeg aner ikke hva...",
+
+      "New Submission" =>
+      "Ny Innlevering",
+      
+      // submissionAdmin.php
+      // /admin files
+      // authorAdmin
+      "Error! Invalid Privileges" => 
+      "Feil! Ugyldig Privilegie",
+      
+      "Sorry. You do not have the necessary privilege to view this page." =>
+      "Beklager. Du har ikke de nødvendige tilgangene til å se denne siden.",
+
+      // blockadmin.php
+      // commentAdmin.php
+      // glossaryAdmin.php
+      // infologAdmin.php
+      // mailinglistAdmin.php
+      // pollAdmin.php
+      // sectionAdmin.php
+      // storyAdmin.php
+      // submissionAdmin.php
+      // topicAdmin.php
+      // variableAdmin.php
+      
+      // /class files
+     
+      // Author.class
+      // Block.class
+      // Block_i.class
+      // Block_render_cal.class
+      "January" =>
+      "januar",
+
+      "February" =>
+      "februar",
+ 
+      "March" =>
+      "mars",
+
+      "April" =>
+      "april",
+
+      "May" =>
+      "mai",
+
+      "June" =>
+      "juni",
+      
+      "July" =>
+      "juli",
+
+      "August" =>
+      "august",
+
+      "September" =>
+      "september",
+
+      "October" =>
+      "oktober",
+
+      "November" =>
+      "november",
+
+      "December" =>
+      "desember",
+      
+      // Block_render_html.class
+      // Block_render_poll.class
+      // Block_render_query.class
+      // Block_render_quote.class
+      // Block_render_rss.class
+      // Block_render_section.class
+      // Block_render_topic.class
+      // Block_render_url.class
+      // Comment.class
+      "Nested" => 
+      "Nested",
+      
+      "Threaded" => 
+      "Threaded",
+      
+      "Flat" => 
+      "Flat",
+      
+      "Index" => 
+      "Indeks",
+      
+      "Bulk Delete" => 
+      "Bulk-sletting",
+      
+      "Show Pending" => 
+      "Vis gjenstående",
+      
+      // Glossary.class
+      "Add a new Term" =>
+      "Legg til nytt ord",
+      
+      // Infolog.class
+      // Mailinglist.class
+      "Email Headline Updates" =>
+      "Email overskriftsoppdateringer",
+
+      "You need to enter an email address" =>
+      "Du må fylle inn en e-postadresse",
+
+      "There was an error inserting your subscription information into the database." =>
+      "Det oppsto en feil når informasjonen din skulle legges til i databasen.",
+
+      "There was an error inserting your frequency data." =>
+      "Det oppsto en feil når frekvensinformasjonen din skulle legges til i databasen.",
+
+      
+      "Thank you. You will receive a confirmation email soon to verify your subscription." =>
+      "Takk! Du vil motta en bekreftende e-post snart for å verifisere abonnementet ditt.",
+
+      "You have been successfully subscribed, but there was a problem sending your confirmation email. You should, however receive your newsletter soon." =>
+      "Du er nå en abonnent, men det oppsto en feil ved sending av bekreftelsen. Du bør, dog, motta nyhetsbrevet snart.",
+
+      "The email address '%s' is not found" =>
+      "E-postadressen '%s' ble ikke funnet",
+
+      "Email: '%s' is not found" =>
+      "E-post: '%s' ble ikke funnet",
+
+      "Email: '%s' has been removed" =>
+      "E-post: '%s' er nå fjernet",
+
+      "Thank you. for your subscription to our site. If there is anything we can do to improve our service,  please let us know!" =>
+      "Takk for at du abonnerer på vårt nyhetsbrev. Hvis det er noe vi kan gjøre for å forbedre servicen vår håper vi å få beskjed!",
+
+      "You've been removed from the mailing list, but there was a problem sending out the final email." =>
+      "Du har blitt fjernet fra e-postlisten, men det oppsto et problem med å sende ut den endelige e-posten.",
+
+      "Unsubscribing all Mailing List Members" =>
+      "Fjerner alle abonnenter",
+
+      "Now ignoring abort calls..." =>
+      "Overser nå forsøk på å avbryte...",
+
+      "Sending mail to the following users:" =>
+      "Sender e-post til følgende brukere:",
+
+      "Mail Sent Successfully!" =>
+      "E-post sendt uten problemer!",
+
+      "Error Sending Email!" =>
+      "Feil under sending av e-post!",
+
+      "You didn't give a email subject!  Halting." =>
+      "Du oppga ikke en tittel! Avbryter.",
+      
+      // Navbar.class
+      "Login" =>
+      "Log inn",
+      
+      "Logout %s" =>
+      "Log ut %s",
+      
+      // Poll.class
+      "Your vote has been registered" =>
+      "Din stemme har blitt registrert",
+
+      "Your vote hasn't been registered because there was no remote address:  %s" =>
+      "Din stemme ble ikke registrert fordi adressen din kunne ikke finnes:  %s",
+      "Your vote hasn't been registered because you (or someone from your computer) has already voted." =>
+      "Din stemme ble ikke registrert fordi du (eller noen på din maskin) har allerede stemt.",
+
+
+      // Section.class
+      // Story.class
+      "More" =>
+      "Mer",
+
+      "Related Links" =>
+      "Relaterte Lenker",
+
+      "Reply" =>
+      "Svar",
+      
+      "Also filed under" =>
+      "Også lagret under",
+
+      "comments" =>
+      "kommentarer",
+ 
+      "comment" =>
+      "kommentar",
+      
+      "Comments?" =>
+      "Kommentarer?",
+      
+      // Submission.class
+      "Anonymous Coward" =>
+      "Anonym Feiging",
+
+      // Topic.class
+      // TopicBar.class
+      // Variable.class
+      // cronmail.php
+      // "Story" =>
+      // "Sak",
+
+      "Written" =>
+      "Skrevet",
+
+      "Intro" =>
+      "Innledning",
+
+      "To add or remove yourself go to" =>
+      "For å fjerne deg selv fra listen kan du gå til",
+
+      // Titles in different php-files
+      "About Page" =>
+      "Beskrivelse",
+
+      "Articles" =>
+      "Artikler",
+
+      "Comment Display" =>
+      "Kommentarvisning",
+
+      "Page not found" =>
+      "Siden kunne ikke finnes",
+
+      "Error document" =>
+      "Feil-side",
+
+      "Glossary Administration" =>
+      "Administrasjon av ordbok",
+
+      "Administration" =>
+      "Administrasjon",
+
+      "Home page" =>
+      "Forsiden",
+
+      "Administration Page" =>
+      "Administrasjonsside",
+
+      "Mailing List Admin" =>
+      "Administrasjon av e-postliste",
+
+      "Poll Booth" =>
+      "Stemmebås",
+
+      "Search" =>
+      "Søk",
+
+      "Search Page" =>
+      "Søkeside",
+
+      "Submission" =>
+      "Innlevering",
+
+      "Author Administration" =>
+      "Administrasjon av forfattere",
+
+      "Block Administration" =>
+      "Administrasjon av blokker",
+
+      "Comment Administration" =>
+      "Administrasjon av kommentarer",
+
+      "Glossary Administration" =>
+      "Administrasjon av ordbok",
+
+      "Poll Administration" =>
+      "Administrasjon av avstemninger",
+
+      "Story Administration" =>
+      "Administrasjon av saker",
+
+      "Submission Administration" =>
+      "Administrasjon av innleveringer",
+
+      "Topic Administration" =>
+      "Administrasjon av områder" 
+   );
+   include_once($_PSL['localedir'] . "/BE_no.php");
+?>
