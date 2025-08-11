ebf47f004bf8ce8e3e29c9308b5ca7ba2f89d574
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:10 +0300

    Add Content Management/clanweb-1.3.4/lang/en/faq.php

---
+++ Content Management/clanweb-1.3.4/lang/en/faq.php
@@ -0,0 +1,49 @@
+<?php
+/*********************************************************
+								faq.php
+								-----------
+					$Copyright: (c) ClanAdmin Tools 2003, 2004
+					$Created: 2004-07-14 by ArreliuS
+					$email: support@clanadmintools.com
+
+	   ClanAdmin Tools is free software; you can redistribute it and/or modify
+	   it under the terms of the GNU General Public License as published by
+	   the Free Software Foundation; either version 2 of the License, or
+	   (at your option) any later version.
+
+**********************************************************/
+/*********************************************************
+	Language file to (c)ClanAdmin Tools(CAT): Version 1.3.x
+ 	Language: english
+	By: ArreliuS
+	Mail: arrelius@error-404.se
+
+	Last modified: 2004-08-28
+**********************************************************/
+// To add a own faq item just write the which name its going to
+// be called by and what text it's going to contain.
+// ex. 'foo' => 'boo',
+
+//FAQ
+$faq = array(
+
+'Installation' => 'Installation',
+'Specs' => '<li>What do I need to run clanadmin tools?</li>- You need to have a webserver that support PHP - 4.0.5 or better and MySQL - 3.23.32 or better.',
+
+'Misc' => 'Misc. Questions',
+'How_to_add' => '<li>How do I add news, games etc?</li>- This is very simple. Just click on "Add news" and fill in the news post in the form and save.',
+'How_to_edit' => '<li>How do I edit news, games etc?</li>- Just click on the edit icon next to the post you want to edit.',
+'Who' => '<li>Who has made ClanAdmin Tools?</li>- CAT is created, coded and maintained by Error 404.',
+'Purpose' => '<li>What can I do with ClanAdmin Tools?</li>- With CAT can you with ease admin your clans site. You can add, edit and remove news, members and games easily.',
+
+'Bugreport' => 'Bug report',
+'What_to_do' => '<li>I have recivied a error message. What do I do to solve this?</li>- Read the readme file on this matter.',
+'Mysql_prob' => '<li>I have installed everything correct but nothing gets saved to the database.</li>-First check if you can reach your database via some other application. If you can then check if the info in cfg.php is correct.',
+
+'Develope' => 'Develope',
+'Idea' => '<li>I have a couple of ideas for the system. Want to hear them?</li>- Ideas and feedback is always welcome. Mail them to cat@error-404.se or write in our forums.',
+
+);
+// EoF
+
+?>
