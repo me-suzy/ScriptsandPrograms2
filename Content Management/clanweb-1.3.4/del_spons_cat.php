05807e91fd647eafe54273b2dbef66b33f685960
me-suzy <me.suzana@gmail.com>
2025-08-11 19:48:04 +0300

    Add Content Management/clanweb-1.3.4/del_spons_cat.php

---
+++ Content Management/clanweb-1.3.4/del_spons_cat.php
@@ -0,0 +1,48 @@
+<?php
+/********************************************************
+								del_spons_cat.php
+								-----------
+					$Copyright: (c) ClanAdmin Tools 2003-2005
+					$Last modified: 2005-01-25
+					$email: support@clanadmintools.com
+
+	   ClanAdmin Tools is free software; you can redistribute it and/or modify
+	   it under the terms of the GNU General Public License as published by
+	   the Free Software Foundation; either version 2 of the License, or
+	   (at your option) any later version.
+
+**********************************************************/
+      require ('auth.php');
+	  require ('_inc/top.inc.php');
+
+	  // Content 
+	  echo"<div class=\"welcome\">";
+
+				$sql = "DELETE 
+						FROM ".$db_prefix. "spons_cat 
+						WHERE spons_cat = '".$_GET['id']."'";
+				
+				$db->query($sql) or exit('An error occured while deleting data.<br/><strong>Line:</strong>'. __LINE__ .'<br/><strong>File:</strong>'. __FILE__ .'<br/><strong>Cause:</strong>'.$db->error().'');
+			
+				$sql = "DELETE 
+						FROM ".$db_prefix. "spons 
+						WHERE spons_cat = '".$_GET['id']."'";
+				
+				$db->query($sql) or exit('An error occured while deleting data.<br/><strong>Line:</strong>'. __LINE__ .'<br/><strong>File:</strong>'. __FILE__ .'<br/><strong>Cause:</strong>'.$db->error().'');
+				
+				// To prevent getting a lot of overhead bytes in the database we optimize
+				// the tables after we've removed the data. 
+				// Going to fix this issue later on.
+				$sql = "OPTIMIZE TABLE ".$db_prefix."spons";
+				$db->query($sql) or exit('An error occured while optimizing table.<br/><strong>Line:</strong>'. __LINE__ .'<br/><strong>File:</strong>'. __FILE__ .'<br/><strong>Cause:</strong>'.$db->error().'');
+				 
+				$sql = "OPTIMIZE TABLE ".$db_prefix."spons_cat";
+				$db->query($sql) or exit('An error occured while optimizing table.<br/><strong>Line:</strong>'. __LINE__ .'<br/><strong>File:</strong>'. __FILE__ .'<br/><strong>Cause:</strong>'.$db->error().'');
+				
+			echo "<h3>Category removed</h3>";
+			echo"</div>";
+	  // bottom.inc
+	  require ('_inc/bottom.inc.php');
+
+?>				  
+				  
