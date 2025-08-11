46535c246137e88f595b09a49245a81f1f129ecc
me-suzy <me.suzana@gmail.com>
2025-08-11 19:37:15 +0300

    Add Content Management/chakraweb-1.01a/ChakraWeb/setup/index.php

---
+++ Content Management/chakraweb-1.01a/ChakraWeb/setup/index.php
@@ -0,0 +1,11 @@
+<?php 
+// ----------------------------------------------------------------------
+// ModName: index.php
+// Purpose: jump to page.php
+// Author:  Mitra Dwi Wiyono (mdwiyono@quick4all.com)
+// ----------------------------------------------------------------------
+
+
+Header("Location: /setup/page.php");
+
+?>
