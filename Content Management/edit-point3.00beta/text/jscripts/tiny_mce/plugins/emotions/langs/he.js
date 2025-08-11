9718cd3ab942c3b501ec7339cd1367b705e081ce
me-suzy <me.suzana@gmail.com>
2025-08-12 02:36:12 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/emotions/langs/he.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/emotions/langs/he.js
@@ -0,0 +1,22 @@
+// HE lang variables
+
+tinyMCE.addToLang('emotions',{
+title : 'äëðñ øâùåï',
+desc : 'øâùåðéí',
+cool : 'øâåò',
+cry : 'áåëä',
+embarassed : 'ðáåê',
+foot_in_mouth : 'òí äøâì áôä',
+frown : 'æåòó',
+innocent : 'úîéí',
+kiss : 'ðùé÷ä',
+laughing : 'öåç÷',
+money_mouth : 'ôä ùì ëñó',
+sealed : 'àèåí',
+smile : 'çéåê',
+surprised : 'îåôúò',
+tongue_out : 'ìùåï áçåõ',
+undecided : 'áìúé äçìèé',
+wink : '÷øéöä',
+yell : 'öò÷ä'
+});
