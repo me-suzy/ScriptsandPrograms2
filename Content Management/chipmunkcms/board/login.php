3bd5843cbd1278157287fb17784c0dc3d669e731
me-suzy <me.suzana@gmail.com>
2025-08-11 19:44:51 +0300

    Add Content Management/chipmunkcms/board/login.php

---
+++ Content Management/chipmunkcms/board/login.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="style.css" type="text/css">
+<center>
+<table class='maintable'>
+<tr class='headline'><td><center>Log In</center></td></tr>
+<tr class='forumrow'><td><center>
+<table border='0'>
+<form method="POST" action="authenticate.php">
+<tr class='forumrow'><td><b>Username:</b></td><td><input type="text" name="user" size="15"></td></tr>
+<tr class='forumrow'><td><b>Password:</b></td><td><input type="password" name="password" size="15"></td></tr>
+<tr class='forumrow'><td><b>Remember me?</b></td><td><input type='checkbox' name='remember'></td></tr>
+<tr class='forumrow'><td></td><td><input type="submit" value="submit" name="submit"></td></tr>
+</form>
+<tr class='forumrow'><td></td><td><A href='getpassword.php'>Forgot Password?</a>
+</td></tr></table></center></td></tr></table>
+</center>
+</html>
