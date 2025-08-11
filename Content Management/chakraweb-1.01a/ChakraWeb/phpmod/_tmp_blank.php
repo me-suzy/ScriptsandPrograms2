66e510bcb56260e8f54ed6f818a9c85a9e251b19
me-suzy <me.suzana@gmail.com>
2025-08-11 19:37:12 +0300

    Add Content Management/chakraweb-1.01a/ChakraWeb/phpmod/_tmp_blank.php

---
+++ Content Management/chakraweb-1.01a/ChakraWeb/phpmod/_tmp_blank.php
@@ -0,0 +1,13 @@
+<?php 
+// ----------------------------------------------------------------------
+// ModName: 
+// Purpose: 
+// Author:  Mitra Dwi Wiyono (mdwiyono@quick4all.com)
+// ----------------------------------------------------------------------
+
+require_once("../_files/library/_config.php");
+
+
+
+
+?>
