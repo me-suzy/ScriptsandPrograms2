3f6dd102c0f762fdde624c46d7c1b3f630412ffb
me-suzy <me.suzana@gmail.com>
2025-08-11 20:14:58 +0300

    Add Content Management/easycms/top.php

---
+++ Content Management/easycms/top.php
@@ -0,0 +1,99 @@
+<html>
+
+<head>
+
+<title>NGK Soft</title>
+
+<link rel="stylesheet" type="text/css"
+
+href="style.css" />
+
+</head>
+
+<body onload="changeurl()">
+
+<script type="text/javascript">
+
+if (location!="http://www.ngksoft.com/") 
+
+{
+
+location="http://www.ngksoft.com/"}
+
+</script>
+
+
+
+<!-- -->
+
+<table>
+
+<tr>
+
+       <td><img src="images/logo.gif"  alt="logo" border="0">
+
+<br>
+
+</td>
+
+       <td>
+
+	   	   		
+
+	   
+
+	   
+
+	   </td>
+
+       <td></td>
+
+</tr>
+
+<tr>
+
+       <td valign="top" ><table border=0 cellpadding=1 cellspacing=1 bgcolor="#003366" width="150" valign="top">
+
+				<tr>
+
+    			<td>
+
+			  		   <table border=0 cellpadding=1 cellspacing=1 background="images/back.gif" width=100%>
+
+			  		   <tr>
+
+    		  		   <td align="center"><b>Demo Features</b></td>
+
+			  		   </tr>
+
+			  		   </table>
+
+					   <table border=0 cellpadding=1 cellspacing=1 bgcolor="#ffffff" width=100%>
+
+			  		   <tr>
+
+    		  		   <td align="center"><b> <a href="easycms/adminlog.php" target="_blank">Demo</a><br>
+
+					   <a href="easycms/easycms.zip">Download</a><br>
+
+					   <a href="instructions.php">Instructions</a><br></b>
+
+					   <br>
+
+					   </td>
+
+			  		   </tr>
+
+			  		   </table>
+
+					  
+
+			  </td>
+
+			  </tr>
+
+			  </table></td>
+
+       <td rowspan=2 valign="top">
+
+	   <!--Here is the top end-->
