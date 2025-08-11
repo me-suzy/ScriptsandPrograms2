826587c30e1504b4e2bbac5812a21497572cc8c4
me-suzy <me.suzana@gmail.com>
2025-08-12 02:42:42 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/paste/langs/he.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/paste/langs/he.js
@@ -0,0 +1,10 @@
+// HE lang variables by Liron Newman, http://eesh.net
+
+tinyMCE.addToLang('',{
+paste_text_desc : 'äãá÷ ëè÷ñè øâéì',
+paste_text_title : 'äùúîù á-CTRL+V ùòì äî÷ìãú ùìê ëãé ìäãáé÷ àú äè÷ñè ìçìåï.',
+paste_text_linebreaks : 'ùîåø òì îòáøé ùåøä',
+paste_word_desc : 'äãá÷ î-Word',
+paste_word_title : 'äùúîù á-CTRL+V ùòì äî÷ìãú ùìê ëãé ìäãáé÷ àú äè÷ñè ìçìåï.',
+selectall_desc : 'áçø äëì'
+});
