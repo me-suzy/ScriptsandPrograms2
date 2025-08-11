d31a6cfbf14aa2e43f2aa5c06845b0b09f68faa3
me-suzy <me.suzana@gmail.com>
2025-08-11 18:56:13 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/paste/langs/zh_cn.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/paste/langs/zh_cn.js
@@ -0,0 +1,9 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+tinyMCELang['lang_paste_text_desc'] = '×÷Îª´¿ÎÄ±¾Õ³Ìù';
+tinyMCELang['lang_paste_text_title'] = 'Ê¹ÓÃ¿ì½Ý¼ü CTRL+V ½«ÎÄ±¾Õ³Ìùµ½ÒÔÏÂ´°¿ÚÖÐ.';
+tinyMCELang['lang_paste_text_linebreaks'] = '±£Áô»»ÐÐ·û';
+tinyMCELang['lang_paste_word_desc'] = '´ÓWordÕ³Ìù';
+tinyMCELang['lang_paste_word_title'] = 'Ê¹ÓÃ¿ì½Ý¼ü CTRL+V ½«ÎÄ±¾Õ³Ìùµ½ÒÔÏÂ´°¿ÚÖÐ.';
+tinyMCELang['lang_selectall_desc'] = 'È«Ñ¡';
