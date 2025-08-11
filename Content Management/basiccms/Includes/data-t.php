cdd09b4afbf35ed40989d118a2c5d52ec2b917d9
me-suzy <me.suzana@gmail.com>
2025-08-11 17:59:12 +0300

    Add Content Management/basiccms/Includes/data-t.php

---
+++ Content Management/basiccms/Includes/data-t.php
@@ -0,0 +1,3 @@
+<p><small>powered by 
+<a name="content management software" href="http://www.content-management-software.us" title="content management software" target="_blank">content management software</a></small>
+</p>
