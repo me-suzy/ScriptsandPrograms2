9c2e73347bb8c330b45d57137a3eb50e834b74d6
me-suzy <me.suzana@gmail.com>
2025-08-11 17:52:14 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/public_html/logging_mod/gif11.php

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/public_html/logging_mod/gif11.php
@@ -0,0 +1,27 @@
+
+<?php 
+/*
+Zircote Web Management
+Copyright (C) 1999  Robert D. Allen <allenb@home-networking.org>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+email: allenb@home-networking.org
+*/
+Header(  "Content-type:  image/gif"); 
+passthru(  "cat clear.gif" );  //send a 1x1 gif 
+include( "./logit.inc");   //Exanded below 
+
+?>
