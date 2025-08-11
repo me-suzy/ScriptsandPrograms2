956f5d5475bd474836cfee02d77de25abc72d2ee
me-suzy <me.suzana@gmail.com>
2025-08-12 02:37:07 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/flash/langs/he.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/flash/langs/he.js
@@ -0,0 +1,11 @@
+// HE lang variables
+
+tinyMCE.addToLang('flash',{
+title : 'äëðñ/òøåê ñøèåï Flash',
+desc : 'äëðñ/òøåê ñøèåï Flash',
+file : '÷åáõ Flash (.swf)',
+size : 'âåãì',
+list : '÷áöé Flash',
+props : 'îàôééðé Flash',
+general : 'ëììé'
+});
