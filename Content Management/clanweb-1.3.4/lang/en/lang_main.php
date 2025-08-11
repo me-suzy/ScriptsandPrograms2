905f944944e6610be1b54d2d0612abe549b9636d
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:15 +0300

    Add Content Management/clanweb-1.3.4/lang/en/lang_main.php

---
+++ Content Management/clanweb-1.3.4/lang/en/lang_main.php
@@ -0,0 +1,138 @@
+<?php
+/*********************************************************
+								lang_main.php
+								-------------
+					$Copyright: (c) ClanAdmin Tools 2003, 2004
+					$Created: 2004-07-14 by ArreliuS
+					$email: support@clanadmintools.com
+
+	ClanAdmin Tools is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+**********************************************************/
+/*********************************************************
+	Language file to (c)ClanAdmin Tools(CAT): Version 1.3.2
+ 	Language: English
+	By: ArreliuS
+	Mail: arrelius@error-404.se
+
+	Last modified: 2004-08-28
+**********************************************************/
+
+//Menu
+$lang_main = "Main";
+$lang_news = "News";
+$lang_crew = "Members";
+$lang_reported = "Reported Comments";
+$lang_admins = "Admins";
+$lang_match = "Games";
+$lang_support = "Support CAT";
+$lang_faq = "FAQ";
+$lang_upload = "Upload file";
+$lang_browse_files = "Browse files";
+$lang_motd ="MOTD";
+$lang_logout = "Logout";
+
+//Functions
+$lang_login = "Login";
+$lang_add_news = "Add news";
+$lang_add_member = "Add member";
+$lang_add_match = "Add match";
+$lang_add_user = "Add user";
+$lang_add_motd = "Add MOTD";
+$lang_add_admin ="Add admin";
+$lang_edit_admin = "Edit admin";
+$lang_edit_news = "Edit news";
+$lang_edit_member = "Edit member";
+$lang_edit_members = "Edit members";
+$lang_edit_match = "Edit match";
+$lang_edit_user = "Edit users";
+$lang_delete_news = "Delete news";
+$lang_delete_member = "Delete member";
+$lang_delete_match = "Delete match";
+$lang_delete_user = "Delete user";
+$lang_delete_admin = "Delete admin";
+$lang_delete_comment = "Delete comment";
+$lang_headadmin = "Head admin";
+$lang_upload_file = "Upload file";
+$lang_file_uploaded = "The file has been uploaded";
+$lang_deleted = "Deleted";
+$lang_comment_deleted = "Comment deleted by admin";
+
+$lang_username = "Username";
+$lang_password = "Password";
+$lang_profile = "Profile";
+$lang_name = "Name";
+$lang_picture ="Picture";
+$lang_age = "Age";
+$lang_quote = "Quote";
+$lang_sex = "Sex";
+$lang_work = "Occupation";
+$lang_location = "Resident";
+$lang_position = "Position";
+$lang_cpu = "CPU";
+$lang_mouse ="Mouse";
+$lang_mousepad ="Mousepad";
+$lang_screen = "Screen";
+$lang_os = "Operating system";
+$lang_gfx ="Graphic card";
+$lang_memory = "Memory";
+$lang_hdd = "Harddrive";
+$lang_resolution = "Resolution";
+$lang_email = "E-mail";
+
+$lang_team_one = "Team One";
+$lang_team_two = "Team Two";
+$lang_type = "Gametype";
+$lang_point_one = "Points for Team One";
+$lang_point_two = "Points for Team Two";
+$lang_map ="Map(s)";
+$lang_lineup = "Lineup";
+$lang_report = "Report";
+
+$lang_topic ="Topic";
+$lang_enter_news = "Enter newspost here";
+$lang_date = "Date";
+$lang_approve = "Approve";
+$lang_delete = "Delete";
+
+$lang_member_deleted = "The member is now deleted!";
+$lang_news_deleted = "The newspost is now deleted!";
+$lang_match_deleted = "The match is now deleted!";
+$lang_user_deleted = "The user is now deleted!";
+$lang_comment_removed = "The comment have been removed.";
+
+$lang_member_updated = "The member is now updated";
+$lang_news_updated = "The newspost is now updated";
+$lang_match_updated = "The match is now updated";
+$lang_user_updated = "The user is now updated";
+
+$lang_member_added = "The member is now added";
+$lang_news_added = "The newspost is now added";
+$lang_match_added = "The match is now added";
+$lang_user_added = "The user is now added";
+$lang_motd_added = "The motd has been added";
+$lang_comment_approved = "The comment is now approved";
+
+// Error messages
+$lang_wrong = "You entered a invalid username and/or password. Please try again.";
+$lang_error = "A error occured. Please try again.";
+$lang_denied = "You don't have permission to access this page!";
+
+//Information
+$lang_contact = "Send questions, comments and ideas to <a href=\"mailto:support@clanadmintools.com\" target=\"_blank\">support@clanadmintools.com</a>";
+$lang_copy = "ClanAdmin Tools ,(CAT), is free to use, modify and redistribute as long as the copyright code stays intact. \nPlease respect this. ClanAdmin Tools ,(CAT), is created by ArreliuS for e-Sport clans over the world. \nUse this script to whatever purpose as long as the (c)-code stays intact and that you don't violate the GPL license.\nClanAdmin Tools ,(CAT), is being distributed by Error 404. Bugreports can be send to support@clanadmintools.com, to download the \nlatest version of ClanAdmin Tools please visit http://www.clanadmintools.com/. All form of support/ideas/comments etc is done by mail or in the forum.";
+$lang_publish_info = "<i>Coded by ArreliuS 2003. Note that when you push the \"Add member\"-button, you automatically accepts that the information is going to be published on the net. If you feel that you can't accept this please contact an admin. I(ArreliuS) can't be held responsible for what this script is being used for. So if you discover that this script is being used for illegal activities please contact your local police. It won't help mailing me. </i>"; 
+$lang_loggedout = "You have now logged out. Welcome back!"; 
+$lang_motd_title ="Message of the day";
+$lang_admin_notice = "Only check this box if you want to give this user the permission to add, edit and remove other users.";
+
+//Install
+$lang_install = "Install";
+$lang_update = "Update";
+
+// EoF
+
+?>
