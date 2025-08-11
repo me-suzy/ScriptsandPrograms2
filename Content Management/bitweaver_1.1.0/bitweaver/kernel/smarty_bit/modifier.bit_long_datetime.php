6e694e42860879ae051c7b5ce59f33437ffc9b75
me-suzy <me.suzana@gmail.com>
2025-08-11 18:11:11 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/kernel/smarty_bit/modifier.bit_long_datetime.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/kernel/smarty_bit/modifier.bit_long_datetime.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+/**
+ * required setup
+ */
+global $gBitSmarty;
+require_once $gBitSmarty->_get_plugin_filepath('modifier','bit_date_format');
+
+/**
+ * smarty_modifier_bit_long_datetime
+ */
+function smarty_modifier_bit_long_datetime($string)
+{
+	global $gBitSystem;
+	return smarty_modifier_bit_date_format($string, $gBitSystem->get_long_datetime_format(), null, "%A %d ".tra('of')." %B, %Y[%H:%M:%S %Z]");
+}
+
+/* vim: set expandtab: */
+
+?>
