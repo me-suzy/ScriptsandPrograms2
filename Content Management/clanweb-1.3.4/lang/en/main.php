a8f4bb37596008d1a497640ca04ed6ffa9216188
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:17 +0300

    Add Content Management/clanweb-1.3.4/lang/en/main.php

---
+++ Content Management/clanweb-1.3.4/lang/en/main.php
@@ -0,0 +1,9 @@
+Welcome to ClanAdmin Tools <b><?=$script?></b>! You have succesfully logged in!<br/><br/>From
+here you can admin your clans homepage on a couple of different ways. I hope
+that your clan finds it easy and effective to use this admin system. If you
+have any questions, comments or ideas about how this system can become better,
+then please mail them to <a href="mailto:support@clanadmintools.com">support@clanadmintools.com</a>
+ and share your thoughts.
+ <br/><br/>
+ <strong>You can edit this text by opening main.php in your choosen language folder.</strong>
+ <pre>Your folder: <?php echo "$lang/main.php"; ?></pre>
