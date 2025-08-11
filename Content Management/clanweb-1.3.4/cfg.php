6419e27df1ba61710926549639b2269729ad25eb
me-suzy <me.suzana@gmail.com>
2025-08-11 19:47:46 +0300

    Add Content Management/clanweb-1.3.4/cfg.php

---
+++ Content Management/clanweb-1.3.4/cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_prefix = 'demo_';
+$dbname = 'clanadmintools_com';
+$dbuser = 'arrelius';
+$dbpw = 'es17rr';
+$dbhost = 'localhost';
+$lang = 'lang/en';
+$lang_file = 'lang_main.php';
+
+$script = '1.3.4 pr1';
+
+require("$lang/$lang_file");
+require ('db/mysql.php');
+?>
