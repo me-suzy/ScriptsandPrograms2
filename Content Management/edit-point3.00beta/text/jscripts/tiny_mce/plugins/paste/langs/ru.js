d7e47a530cabbb31c5b5017608baac60d5de55dc
me-suzy <me.suzana@gmail.com>
2025-08-12 02:42:51 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/paste/langs/ru.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/paste/langs/ru.js
@@ -0,0 +1,10 @@
+// RU cp1251 lang variables
+
+tinyMCE.addToLang('',{
+paste_text_desc : 'Âñòàâèòü êàê ïðîñòîé òåêñò',
+paste_text_title : 'Èñïîëüçóéòå CTRL+V äëÿ âñòàâêè òåêñòà â îêîøêî.',
+paste_text_linebreaks : 'Ñîõðàíèòü ïåðåíîñû ñòðîê',
+paste_word_desc : 'Âñòàâèòü èç Word',
+paste_word_title : 'Èñïîëüçóéòå CTRL+V äëÿ âñòàâêè òåêñòà â îêîøêî.',
+selectall_desc : 'Âûäåëèòü âñ¸'
+});
