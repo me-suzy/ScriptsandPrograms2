65f532ca7db08bc1d79a17b09586666a3f310919
me-suzy <me.suzana@gmail.com>
2025-08-12 02:45:53 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/searchreplace/langs/he.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/searchreplace/langs/he.js
@@ -0,0 +1,21 @@
+// HE lang variables by Liron Newman, http://eesh.net
+
+tinyMCE.addToLang('',{
+searchreplace_search_desc : 'çôù',
+searchreplace_searchnext_desc : 'çôù ùåá',
+searchreplace_replace_desc : 'çôù/äçìó',
+searchreplace_notfound : 'äçéôåù äåùìí. îçøåæú äçéôåù ìà ðîöàä.',
+searchreplace_search_title : 'çôù',
+searchreplace_replace_title : 'çôù/äçìó',
+searchreplace_allreplaced : 'îçøåæú äçéôåù äåçìôä áëì î÷åí áå äéà ðîöàä.',
+searchreplace_findwhat : 'îä ìçôù',
+searchreplace_replacewith : 'ìäçìéó á-',
+searchreplace_direction : 'ëéååï',
+searchreplace_up : 'ìîòìä',
+searchreplace_down : 'ìîèä',
+searchreplace_case : 'äúàí âåãì àåúéåú',
+searchreplace_findnext : 'îöà&nbsp;àú&nbsp;äáà',
+searchreplace_replace : 'äçìó',
+searchreplace_replaceall : 'äçìó&nbsp;äëì',
+searchreplace_cancel : 'áèì'
+});
