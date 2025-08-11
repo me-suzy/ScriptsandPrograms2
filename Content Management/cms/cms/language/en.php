70f874dab046bd1a5e0269605975cd798882f528
me-suzy <me.suzana@gmail.com>
2025-08-11 20:03:17 +0300

    Add Content Management/cms/cms/language/en.php

---
+++ Content Management/cms/cms/language/en.php
@@ -0,0 +1,153 @@
+<?php
+// ----------------------------------------------------------------------
+// Khaled Content Management System
+// Copyright (C) 2004 by Khaled Al-Shamaa.
+// http://www.al-shamaa.com/
+// ----------------------------------------------------------------------
+// LICENSE
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Filename: en.php
+// Original  Author(s): Khaled Al-Sham'aa khaled@al-shamaa.com
+// Purpose:  Define system messages in English language
+// ----------------------------------------------------------------------
+
+define("CHARSET", "windows-1256");
+define("DIRECTION", "LTR");
+define("ALIGN", "Left");
+define("LBL_LANG", "Languages");
+
+define("MISS_USERID", "Please enter user ID");
+define("MISS_PW", "Please enter password");
+define("LBL_USERID", "User ID");
+define("LBL_PW", "Password");
+define("LBL_REM", "Remember me");
+define("LBL_LOGIN", "Login");
+define("LBL_LOGOUT", "Logout");
+define("LBL_INCORRECT", "Incorrect user ID or password");
+define("LBL_EDIT_LINK", "Click here to edit this page");
+define("LBL_ADDPAGE", "Add new page linked to this one");
+define("LBL_ADD", "Add");
+define("LBL_EDIT", "Edit");
+define("LBL_EDIT_ACC", "Edit Your Account");
+define("LBL_TITLE", "Page Title");
+define("LBL_CONTENT", "Page Content");
+define("LBL_BACK", "Back to");
+define("LBL_FULLNAME", "Full Name");
+define("LBL_HOME", "Home Page");
+define("LBL_VIEWPAGE", "View This Page");
+define("INVALID_MSG", "Invalid Field");
+define("LBL_ACTIVE", "Active This Page");
+define("LBL_DEACTIVE", "Deactive This Page");
+define("LBL_DELETE", "Delete This Page");
+define("LBL_SCHADUAL", "Schedule This Page");
+define("LBL_LINKS", "Links List");
+define("CONFIRM_DEL_MSG", "Are you sure that you want to delete this page?");
+define("CONFIRM_DEL_LBL", "Confirm Delete");
+define("SITE_TITLE", "Rama CMS");
+define("TELL_SUBJECT", "You may like this one!");
+define("TELL_BODY", "You may like to browse this page: ");
+define("LBL_FAV", "Add to favorites");
+define("LBL_TELL", "Send this page to a friend");
+define("LBL_PRINT", "Print this page");
+define("LBL_PDF", "PDF Version");
+define("LBL_VIEWS1", "This page visited for ");
+define("LBL_VIEWS2", " times.");
+define("LBL_MAP", "Site Map");
+define("LBL_SEARCH", "Search");
+define("LBL_SEARCH_CMD", "Go");
+define("LBL_TERM", "Search results of ");
+define("LBL_UNDER", "This page still under construction.");
+define("EMAIL_THANKS", "Thanks to contact us, we will replay you soon");
+define("EMAIL_SORRY", "Sorry, your submit does not sent for a technical problem in the website, please try it later");
+define("EMAIL_ALERT", "Please check your email address format");
+define("EMAIL_CONFIRM", "Are you sure about your email address");
+define("EMAIL_NAME", "Name");
+define("EMAIL_ADDRESS", "Email Address");
+define("EMAIL_SUBJECT", "Subject");
+define("EMAIL_BODY", "Body");
+define("EMAIL_SEND", "Send");
+define("EMAIL_LINK", "Contact us");
+define("LBL_RESET", "Undo");
+define("LBL_META", "META Tags");
+define("LBL_FROM", "From");
+define("LBL_TO", "To");
+define("ALT_CALENDAR", "Pick a date");
+define("INVALID_DATE", "Incorrect date (dd/mm/yyyy)");
+define("LBL_ORD_UP", "Move This Link Up");
+define("LBL_ORD_DOWN", "Move This Link Down");
+
+define("LBL_TO_SELL", "Sell this item");
+define("LBL_PRICE", "Price");
+define("LBL_CART", "My shopping cart");
+define("LBL_CART_ADD", "Add to your shopping cart");
+define("LBL_CART_DEL", "Delete this item");
+
+define("IN_CART1", "There are");
+define("IN_CART2", "item(s) in your cart");
+define("EMPTY_CART_CONFIRM", "Are you sure that you want to empty your shopping cart?");
+define("EMPTY_CART", "Empty the shopping cart");
+define("CART_ITEM_TITLE", "Item");
+define("CART_ITEM_QUANTITY", "Quantity");
+define("CART_ITEM_PRICE", "Price");
+define("CART_ITEM_SUBTOTAL", "Sub Total");
+define("DEL_CART_CONFIRM", "Are you sure that you want to remove this item?");
+define("CART_TOTAL", "Total");
+define("CART_CLOSE", "Continue Shopping");
+define("CART_CHECKOUT", "Check Out");
+define("LBL_PHONE", "Phone");
+define("LBL_NAME", "Full Name");
+define("LBL_ADDRESS", "Address");
+define("LBL_CITY", "City");
+define("LBL_STATE", "State");
+define("LBL_ZIP", "P.O. Box");
+define("LBL_COUNTRY", "Country");
+define("LBL_SELECT", "Please Select");
+define("LBL_BUY", "Buy Now!");
+define("CHECKDONE", "You made a payment, we will contact you soon. You can check your invoice by click on the following URL (please save it for your records):");
+define("LBL_MARQUEE", "Marquee");
+
+define("LBL_BACKUP", "Backup");
+define("LBL_RESTORE", "Restore");
+define("LBL_COPY", "Copy to Clipboard");
+define("LAST_UPDATED_AT", "Last updated at");
+define("LAST_UPDATED_BY", "by");
+
+define("LBL_INTERACTIVE", "Interactive mode");
+define("MODE_NONE", "none (default)");
+define("MODE_ADMIN", "Viewed by admin only");
+define("MODE_MODIRATED", "Viewed by users (not modirated)");
+define("MODE_OPEN", "Viewed by users (modirated)");
+define("MODE_EMAIL", "Emailed form");
+define("INTERAVTIVE_ADD", "New Post");
+define("ENTRY_NAME", "Name");
+define("ENTRY_EMAIL", "Email");
+define("ENTRY_CONTENT", "Content");
+define("ENTRY_ADD", "Add");
+define("ENTRY_DEL", "Delete this entry");
+define("ENTRY_SHOW", "Show this entry");
+define("ENTRY_HIDE", "Hide this entry");
+define("ENTRY_DEL_MSG", "Are you sure that you want to delete this entry?");
+
+define("NO_PRIVILEGE", "Sorry, you have no privilege to do this action!");
+define("LBL_UPLOAD", "Upload File to Website");
+define("CMD_UPLOAD", "Upload");
+define("ERR_FILE_EXIST", "There is another file online has the same name");
+define("ERR_FILE_BIG", "Size of the file you try to upload exceeded maximum allowed size");
+define("ERR_FILE_FAILD", "Uploading process has been failed, please try to upload your file again");
+define("ERR_FILE_BLOCKED", "Sorry, but you can't upload file has that extension!");
+define("F_NAME", "File Name");
+define("F_SIZE", "File Size");
+define("F_TIME", "Upload Time");
+?>
