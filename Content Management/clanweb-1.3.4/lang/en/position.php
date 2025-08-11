57fdcc76964782312018a90a5a9b33289e052291
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:19 +0300

    Add Content Management/clanweb-1.3.4/lang/en/position.php

---
+++ Content Management/clanweb-1.3.4/lang/en/position.php
@@ -0,0 +1,11 @@
+<select name="position" class="textfelt">
+                <option selected>-----------------------</option>
+                <option>Clanleader</option>
+                <option>Match-manager</option>
+                <option>Manager</option>
+                <option>Server-manager</option>
+                <option>Active player</option>
+                <option>Inactive player</option>
+                <option>Reserve</option>
+                <option>Webmaster</option>
+              </select>
