c8135deb1e89b1326f976d508d62a11da9a3cccb
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:40 +0300

    Add Content Management/clanweb-1.3.4/lang/se/select.php

---
+++ Content Management/clanweb-1.3.4/lang/se/select.php
@@ -0,0 +1,8 @@
+<?php // Enter your clans newstypes ?>
+              <select name="newstype" class="textfelt">
+                <option selected>Nyhetstyp //</option>
+                <option>Allm&auml;nt</option>
+                <option>Scriptnyhet</option>
+                <option>FAQ</option>
+                <option>Bugg</option>
+              </select>
