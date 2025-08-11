fe8357f601383c4573be5cd5e0b1579d6302ae98
me-suzy <me.suzana@gmail.com>
2025-08-11 19:57:18 +0300

    Add Content Management/cms/cms/entrystatus.php

---
+++ Content Management/cms/cms/entrystatus.php
@@ -0,0 +1,66 @@
+<?php
+// ----------------------------------------------------------------------
+// Khaled Content Management System
+// Copyright (C) 2004 by Khaled Al-Shamaa.
+// GSIBC.net stands behind the software with support, training, certification and consulting.
+// http://www.al-shamaa.com/
+// ----------------------------------------------------------------------
+// LICENSE
+
+// This program is open source product; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Filename: entrystatus.php
+// Original  Author(s): Khaled Al-Sham'aa khaled@al-shamaa.com
+// Purpose:  Update interactive entry status
+// ----------------------------------------------------------------------
+
+session_start();
+?>
+<?php include_once ("db.php") ?>
+<?php include_once ("config.php") ?>
+<?php include_once ("lang.php") ?>
+<?php include_once ("security.inc.php") ?>
+<?php if (@$_SESSION["status"] <> "login" || ($useSSL && $_SERVER['HTTPS'] != 'on')) header("Location: login.php") ?>
+<?php if ($_SESSION["ip"] != getip()) header("Location: login.php") ?>
+<?php
+if(is_numeric($_GET["key"])){ $key = @$_GET["key"]; }
+if(is_numeric($_GET["status"])){ $newstatus = @$_GET["status"]; }
+if(is_numeric($_GET["back_id"])){ $back_id = @$_GET["back_id"]; }
+
+// create an object instance
+// configure library for a MySQL connection
+$db = NewADOConnection(DBTYPE);
+
+// open connection to database
+$db->Connect(HOST, USER, PASS, DB) or die("Unable to connect!");
+
+// get resultset as associative array
+$ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
+
+$tkey = "" . $key . "";
+
+$strsql = "SELECT `page_id` FROM `interactive` WHERE `id`=".$tkey;
+$rs = $db->Execute($strsql) or die("Error in query: $strsql. " . $db->ErrorMsg());
+if (!$rs->EOF) { $page_id = $rs->fields["page_id"]; }
+
+if(!($_SESSION["privilege"] & 16) || !inTree($db, $_SESSION["rootpage"], $page_id)){
+   $db->Close();
+   noPrivilege();
+}
+
+// update
+$updateSQL = "UPDATE `interactive` SET status=$newstatus WHERE `id`=".$tkey;
+$db->Execute($updateSQL) or die("Error in query: $updateSQL. " . $db->ErrorMsg());
+$db->Close();
+header("Location: page-$back_id.html");
+?>
