0d49288d56ba436b8e2aaa5bcb15dd6029eaa385
me-suzy <me.suzana@gmail.com>
2025-08-11 19:17:09 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/users/my_files.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/users/my_files.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * $Header: /cvsroot/bitweaver/_bit_users/my_files.php,v 1.1.1.1.2.2 2005/07/26 15:50:30 drewslater Exp $
+ *
+ * Copyright (c) 2004 bitweaver.org
+ * Copyright (c) 2003 tikwiki.org
+ * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ *
+ * $Id: my_files.php,v 1.1.1.1.2.2 2005/07/26 15:50:30 drewslater Exp $
+ * @package users
+ * @subpackage functions
+ */
+
+/**
+ * required setup
+ */
+require_once("../bit_setup_inc.php");
+global $gBitSystem;
+
+if (!$gBitUser->mUserId) {
+	$gBitSmarty->assign('msg', tra("You are not logged in"));
+	$gBitSystem->display( 'error.tpl' );
+	die;
+}
+
+$userFiles = $gBitUser->getUserFiles();
+$gBitSmarty->assign_by_ref('userFiles', $userFiles['files']);
+$gBitSmarty->assign('numUserFiles', count($userFiles['files']));
+$gBitSmarty->assign('diskUsage', $userFiles['diskUsage']);
+
+if (!empty($_REQUEST['deleteAttachment'])) {
+	$attachmentId = $_REQUEST['deleteAttachment'];
+}
+
+$gBitSystem->display('bitpackage:users/my_files.tpl');
+?>
