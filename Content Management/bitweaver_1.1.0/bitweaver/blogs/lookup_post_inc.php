05a88811df6785057aad3be63f490defa62788e5
me-suzy <me.suzana@gmail.com>
2025-08-11 18:02:01 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/blogs/lookup_post_inc.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/blogs/lookup_post_inc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package blogs
+ * @subpackage functions
+ */
+
+	global $gContent, $gBitSmarty;
+
+	if( empty( $gContent ) || !is_object( $gContent ) || !$gContent->isValid() ) {
+		$postId = !empty( $_REQUEST['post_id'] ) ? $_REQUEST['post_id'] : NULL;
+		$conId = !empty( $_REQUEST['content_id'] ) ? $_REQUEST['content_id'] : NULL;
+		$gContent = new BitBlogPost( $postId, $conId );
+		$gContent->load();
+		$comments_return_url = $_SERVER['PHP_SELF']."?post_id=$postId";
+		$gBitSmarty->assign_by_ref( 'gContent', $gContent );
+	}
+?>
