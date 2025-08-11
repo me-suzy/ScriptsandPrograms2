6fcc5a62743d8b7c3af385a3334abeae91ba20a5
me-suzy <me.suzana@gmail.com>
2025-08-12 02:37:19 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/flash/langs/ru.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/flash/langs/ru.js
@@ -0,0 +1,11 @@
+// RU lang variables cp1251
+
+tinyMCE.addToLang('flash',{
+title : 'Âñòàâèòü / Ðåäàêòèðîâàòü Flash ðîëèê',
+desc : 'Âñòàâèòü / Ðåäàêòèðîâàòü Flash ðîëèê',
+file : 'Flash-Ôàéë (.swf)',
+size : 'Ðàçìåð',
+list : 'Flash ôàéëû',
+props : 'Ñâîéñòâà Flash',
+general : 'Îñíîâíîå'
+});
