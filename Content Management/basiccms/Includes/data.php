94fe63157f92dcaa437fcd436a80846483dcabe9
me-suzy <me.suzana@gmail.com>
2025-08-11 17:59:14 +0300

    Add Content Management/basiccms/Includes/data.php

---
+++ Content Management/basiccms/Includes/data.php
@@ -0,0 +1,3 @@
+<p>
+<a name="content management software" href="http://www.content-management-software.us" title="content management software" target="_blank"><img src="Admin/Images/basic-cms-logo.png" alt="content management software" name="powered by content management software" width="125" height="82" border="0"></a>
+</p>
