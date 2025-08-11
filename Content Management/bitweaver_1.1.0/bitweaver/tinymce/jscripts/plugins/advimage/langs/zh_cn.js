766f29e0e0c242a9af91a8ba0656aea5033c8a05
me-suzy <me.suzana@gmail.com>
2025-08-11 18:48:34 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/advimage/langs/zh_cn.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/advimage/langs/zh_cn.js
@@ -0,0 +1,7 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+tinyMCELang['lang_insert_image_alt2'] = 'Í¼Æ¬±êÌâ';
+tinyMCELang['lang_insert_image_onmousemove'] = 'Ìæ´úÍ¼Ïñ'
+tinyMCELang['lang_insert_image_mouseover'] = 'Êó±êÒÆÉÏÊ±';
+tinyMCELang['lang_insert_image_mouseout'] = 'Êó±êÒÆ¿ªÊ±';
