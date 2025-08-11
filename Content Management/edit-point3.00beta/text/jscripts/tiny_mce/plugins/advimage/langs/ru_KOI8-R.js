5398e9111db73ade9f178b3a3f8ee81cc7e80d98
me-suzy <me.suzana@gmail.com>
2025-08-12 02:33:35 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/advimage/langs/ru_KOI8-R.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/advimage/langs/ru_KOI8-R.js
@@ -0,0 +1,26 @@
+// RU KOI8-R lang variables
+
+tinyMCE.addToLang('advimage',{
+tab_general : 'ïÓÎÏ×ÎÙÅ',
+tab_appearance : '÷ÉÚÕÁÌÉÚÁÃÉÑ',
+tab_advanced : 'òÁÓÛÉÒÅÎÎÙÅ',
+general : 'çÌÁ×ÎÙÅ',
+title : 'úÁÇÏÌÏ×ÏË',
+preview : 'ðÒÅÄ×ÁÒÉÔÅÌØÎÙÊ ÐÒÏÓÍÏÔÒ',
+constrain_proportions : 'óÏÈÒÁÎÉÔØ ÐÒÏÐÏÒÃÉÉ',
+langdir : 'ïÐÉÓÁÎÉÅ ÑÚÙËÁ',
+langcode : 'ëÏÄ ÑÚÙËÁ',
+long_desc : 'ðÏÌÎÏÅ ÏÐÉÓÁÎÉÅ',
+style : 'óÔÉÌÉ',
+classes : 'ëÌÁÓÓÙ',
+ltr : 'óÌÅ×Á ÎÁÐÒÁ×Ï',
+rtl : 'óÐÒÁ×Á ÎÁÌÅ×Ï',
+id : 'Id',
+image_map : 'ëÁÒÔÁ ÉÚÏÂÒÁÖÅÎÉÑ',
+swap_image : 'ðÏÄÍÅÎÁ ËÁÒÔÉÎËÉ',
+alt_image : 'áÌØÔÅÒÎÁÔÉ×ÎÏÅ ÉÚÏÂÒÁÖÅÎÉÅ',
+mouseover : 'ÐÒÉ ÎÁ×ÅÄÅÎÉÉ ÍÙÛÉ',
+mouseout : 'ËÏÇÄÁ Õ×ÏÄÉÔÅ ÍÙÛØ',
+misc : 'ðÒÏÞÅÅ',
+example_img : '÷ÉÚÕÁÌÉÚÁÃÉÑ&nbsp;ÒÁÓÐÏÌÏÖÅÎÉÑ&nbsp;ËÁÒÔÉÎËÉ'
+});
