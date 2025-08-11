419741e0915a93c4360365ee8d4e4a1172f7294c
me-suzy <me.suzana@gmail.com>
2025-08-11 18:00:28 +0300

    Add Content Management/bcwb_v25/bcwb/lang/cn.inc.php

---
+++ Content Management/bcwb_v25/bcwb/lang/cn.inc.php
@@ -0,0 +1,114 @@
+﻿<?PHP
+// vim: set expandtab tabstop=4 shiftwidth=4: 
+// +----------------------------------------------------------------------+
+// | BCWB: Business Card Web Builder                                      |
+// +----------------------------------------------------------------------+
+// | Author:  Dmitry Sheiko <sheiko@cmsdevelopment.com>	                  |
+// | Copyright (c) 2004 Dmitry Sheiko                                     |
+// | http://bcwb.cmsdevelopment.com                                       |
+// +----------------------------------------------------------------------+
+// | This source file is free software; you can redistribute it and/or    |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This source file is distributed in the hope that it will be useful,  |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// +----------------------------------------------------------------------+
+// Release: 23.02.04 (d/m/y)
+// $Id$
+
+$default_charset = "UTF-8";
+
+// Chinese language
+// Translation powered by http://www.systransoft.com/ 
+
+$lang=array(
+
+	"Icon" => "Icon",
+	"CreateIcon" => "Create icon",
+	"Width" => "Width",
+	"Height" => "Height",
+
+	"Need_set_permissions" => "Need set permissions attributes to files/folders",
+	"Run" => "Run",
+	"Archive" => "Archive",
+	"Moved" => "Moved",
+	"Restore_backup" => "Restore backup",
+	"Create_backup" => "Create backup",
+	"Need_authorization" => "Here is a need to authorise",
+	"System_instaled" => "System was installed",
+	"Reports" => "Reports",
+	"Time" => "Time",
+	"Date" => "Date",
+	"Reffer" => "Reffers",	
+	"Visitor" => "Visitor",
+	"Statistic" => "Statistic",
+	"Status" => "Status",
+	"Installations" => "Installations",
+	"Enable" => "Enable",
+	"Disable" => "Disable (recommended)",
+	"URI" => "URI (HTTP-address)",
+	"Admin_area_folder" => "Administrative area folder",
+
+"Save" => "﻿之外",
+"Cancel" => "取消",
+"Edit" => "编辑",
+"Edit page" => "编辑页",
+"Delete" => "删除",
+"View page" => "看法页",
+"Structure" => "结构",
+"Add section" => "增加部分",
+"Create_subsection" => "创造分部",
+"Logout" => "注销",
+"Template" => "模板",
+"Help window" => "帮助窗口",
+"Close window" => "接近的窗口",
+"This field should be filled" => "这个领域应该被填装",
+"Are you sure" => "肯定是您",
+"The page was not found" => "页未被发现",
+"This page may have been moved, or may no longer exist" => "这页也许被移动了, 或也许不再存在",
+"Authorization in administration area" => "授权在管理区域",
+"Insert Picture" => "插入图片",
+"Browse Picture Source" => "浏览图片来源",
+"Preview" => "预览",
+"Accompanying Text" => "伴随文本",
+"Layout" => "布局",
+"Alignment" => "对准线",
+"Middle" => "中间",
+"Bottom" => "底部",
+"Top" => "上面",
+"Right" => "权利",
+"Left" => "左",
+"Image" => "图象",
+"Font" => "字体",
+"Centering" => "集中",
+"Can not upload file" => "不能上装文件",
+"Border thickness" => "毗邻厚度",
+"Spacing" => "间距",
+"Horizontal" => "水平",
+"Vertical" => "垂直",
+"Insert File" => "插入文件",
+"Clean HTML" => "清洗HTML",
+"Kibe spacing" => "Kibe 间距",
+"List" => "名单",
+"File" => "文件",
+"Page" => "页",
+"Show" => "展示",
+"Link" => "链接",
+"Insert Link" => "插入链接",
+"Page name" => "呼叫名字",
+"Add section" => "增加部分",
+"Creating subsection" => "创造分部",
+"Fill in page properties fields, save changes and proceed to editing new page" => "填装页物产领域, 之外变动和进行对编辑新页",
+"Convertor XHTML could not process the page contents. The page will be removed" => "交换器XHTML 不能处理页内容。页将被去除",
+"Container of contents was not found. Check xslt-file for the following structures: " => "容器内容未被发现。检查xslt 文件以下结构: ",
+	
+		"Url" => array( "title"=> "领域ULR 的文件", "description" => "可变物从文件HTTP 地址。例如描述," ),
+		"Title" => array( "title"=> "文件领域阶标题", "description" => "文件视窗标题。它有特别价值为查寻机器。"),
+		"Template" => array("title"=> "样式的领域", "description" => "文件数据介绍的样式在XSLT 格式。"),
+
+); 
+?>
