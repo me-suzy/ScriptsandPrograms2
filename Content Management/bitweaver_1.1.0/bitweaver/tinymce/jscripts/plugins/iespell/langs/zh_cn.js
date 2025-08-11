85db13afa9f36e0578d5947c76e0a628c33ebb4a
me-suzy <me.suzana@gmail.com>
2025-08-11 18:53:55 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/iespell/langs/zh_cn.js

---
+++ Content Management/bitweaver_1.1.0/bitweaver/tinymce/jscripts/plugins/iespell/langs/zh_cn.js
@@ -0,0 +1,5 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+tinyMCELang['lang_iespell_desc'] = 'ÔËÐÐÆ´Ð´¼ì²é';
+tinyMCELang['lang_iespell_download'] = "Î´¼ì²âµ½ieSpellÆ´Ð´¼ì²é£¬µã»÷ OK Ç°ÍùÏÂÔØÒ³Ãæ¡£"
