39d0a821eece770ef3a37eea4c340803c31c2900
me-suzy <me.suzana@gmail.com>
2025-08-11 20:14:53 +0300

    Add Content Management/easycms/settings.php

---
+++ Content Management/easycms/settings.php
@@ -0,0 +1,11 @@
+<?php 
+ //This page contains the database settings for your website 
+$username='kilodod_admin';
+$password='control';
+$hostname='localhost';
+$databasename='kilodod_gallery';
+$create='off';
+$prefix='gs_';
+$admin='admin';
+$adpass='control';
+?>
