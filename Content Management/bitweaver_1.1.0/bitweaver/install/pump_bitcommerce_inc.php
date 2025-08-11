7e8b7c019e314b8443b0a1ee7668260ce83b712a
me-suzy <me.suzana@gmail.com>
2025-08-11 18:07:35 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/install/pump_bitcommerce_inc.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/install/pump_bitcommerce_inc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_install/pump_bitcommerce_inc.php,v 1.1.2.2 2005/08/03 16:54:00 lsces Exp $
+ * @package install
+ * @subpackage pumps
+ */
+
+/**
+ * Required files
+ */
+	require_once( BITCOMMERCE_PKG_PATH.'includes/common_inc.php' );
+	reset_bitcommerce_layout();
+
+	$pumpedData['Bitcommerce'][] = 'Created Commerce Layout';
+
+?>
