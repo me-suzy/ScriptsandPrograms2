f8f27b567a1ae8d19b6df3fd9edaf303d1f251a0
me-suzy <me.suzana@gmail.com>
2025-08-11 18:03:13 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/categories/modules/mod_whats_related.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/categories/modules/mod_whats_related.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * $Header: /cvsroot/bitweaver/_bit_categories/modules/mod_whats_related.php,v 1.1.1.1.2.5 2005/10/03 07:45:22 wolff_borg Exp $
+ *
+ * Copyright (c) 2004 bitweaver.org
+ * Copyright (c) 2003 tikwiki.org
+ * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+ * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
+ *
+ * $Id: mod_whats_related.php,v 1.1.1.1.2.5 2005/10/03 07:45:22 wolff_borg Exp $
+ * @package categories
+ * @subpackage modules
+ */
+global $gQueryUserId, $module_rows, $module_params, $categlib, $gContent;
+/**
+ * required setup
+ */
+if (isset($gContent)) {
+	require_once( CATEGORIES_PKG_PATH.'categ_lib.php');
+
+	$categories = $categlib->get_object_categories( $gContent->getContentType(), $gContent->getContentId() );
+	foreach($categories as $cat)
+		$cats[] = $cat["category_id"];
+
+	$content = $categlib->get_related($cats, $module_rows);
+	foreach($content as $con) {
+		$tmp = $gContent->getLibertyObject( $con );
+		$whatsRelated[] = $tmp->getDisplayLink($tmp->getTitle(), $tmp->mInfo);
+	}
+	$gBitSmarty->assign_by_ref('whatsRelated', $whatsRelated);
+}
+?>
