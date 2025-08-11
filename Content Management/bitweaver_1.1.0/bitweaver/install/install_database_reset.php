24e8492b9e5ae1f11b4c2a2201b456ff30f28e15
me-suzy <me.suzana@gmail.com>
2025-08-11 18:07:12 +0300

    Add Content Management/bitweaver_1.1.0/bitweaver/install/install_database_reset.php

---
+++ Content Management/bitweaver_1.1.0/bitweaver/install/install_database_reset.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @version $Header: /cvsroot/bitweaver/_bit_install/install_database_reset.php,v 1.1.1.1.2.2 2005/07/26 15:50:07 drewslater Exp $
+ * @package install
+ * @subpackage functions
+ */
+
+// Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+
+// assign next step in installation process
+$gBitSmarty->assign( 'next_step',$step );
+//vd($_REQUEST);
+if( isset( $_REQUEST['continue_install'] ) ) {
+	header( 'Location: '.INSTALL_PKG_URL.'install.php?step='.( $step + 1 ) );
+} elseif( isset( $_REQUEST['reset_config_inc'] ) ) {
+	$fw = fopen($config_file, 'w' );
+	if( isset( $fw ) ) {
+        fwrite( $fw, '');
+		fclose( $fw );
+	}
+	header( 'Location: '.INSTALL_PKG_URL.'install.php' );
+}
+?>
