2fc9520f7cca1cb881fb5d4eed4367cee91f7887
me-suzy <me.suzana@gmail.com>
2025-08-11 19:45:02 +0300

    Add Content Management/chipmunkcms/board/register.php

---
+++ Content Management/chipmunkcms/board/register.php
@@ -0,0 +1,23 @@
+<html>
+<link rel="stylesheet" href="style.css" type="text/css">
+<center>
+<table class='maintable'>
+<tr class='headline'><td><center>Register</center></td></tr>
+<tr class='forumrow'><td><center>
+<table border='0'><tr class='forumrow'><td>
+<form method="post" action="reguser.php">
+Type Username Here*:</td><td><input type="text" name="username" size="15"></td></tr>
+<tr class='forumrow'><td>Type Password Here*:</td><td><input type="password" name="password" size="15"></td></tr>
+<tr class='forumrow'><td>Retype password*:</td><td><input type="password" name="pass2" size="15"><br></td></tr>
+<tr class='forumrow'><td>E-mail Address*:</td><td><input type="text" name="email" size="15"><br></td></tr>
+<tr class='forumrow'><td>Location</td><td><input type='text' name='location' size='15'><br></td></tr>
+<tr class='forumrow'><td>AIM</td><td><input type='text' name='aim' size='15'><br></td></tr>
+<tr class='forumrow'><td>ICQ</td><td><input type='text' name='icq' size='15'><br></td></tr>
+<tr class='forumrow'><td>Show Profile?</td><td><select name='showprofile'><option value='1'>Yes</option><option value='0'>No</option></select></td></tr>
+<tr class='forumrow'><td valign='top'>Signature(255 chars max)<br>HTML disable, BBCODE on.</td><td><textarea name='signature' rows='5' cols='30'></textarea></td></tr>
+<tr class='forumrow'><td></td><td><input type="submit" value="submit">
+</center>
+</form>
+</td></tr></table>
+
+</html>
