3888bcb1f991fdfbee081f8fd1b7f77ca6919ba5
me-suzy <me.suzana@gmail.com>
2025-08-11 18:00:45 +0300

    Add Content Management/bcwb_v25/bcwb/lang/jp.inc.php

---
+++ Content Management/bcwb_v25/bcwb/lang/jp.inc.php
@@ -0,0 +1,112 @@
+﻿<?PHP
+// vim: set expandtab tabstop=4 shiftwidth=4: 
+// +----------------------------------------------------------------------+
+// | BCWB: Business Card Web Builder                                      |
+// +----------------------------------------------------------------------+
+// | Author:  Dmitry Sheiko <sheiko@cmsdevelopment.com>	                  |
+// | Copyright (c) 2004 Dmitry Sheiko                                     |
+// | http://bcwb.cmsdevelopment.com                                       |
+// +----------------------------------------------------------------------+
+// | This source file is free software; you can redistribute it and/or    |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This source file is distributed in the hope that it will be useful,  |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// +----------------------------------------------------------------------+
+// Release: 23.02.04 (d/m/y)
+// $Id$
+
+$default_charset = "UTF-8";
+
+// Jananese language
+// Translation powered by http://www.systransoft.com/ 
+
+$lang=array(
+	"Icon" => "Icon",
+	"CreateIcon" => "Create icon",
+	"Width" => "Width",
+	"Height" => "Height",
+
+	"Need_set_permissions" => "Need set permissions attributes to files/folders",
+	"Run" => "Run",
+	"Archive" => "Archive",
+	"Moved" => "Moved",
+	"Restore_backup" => "Restore backup",
+	"Create_backup" => "Create backup",
+	"Need_authorization" => "Here is a need to authorise",
+	"System_instaled" => "System was installed",
+	"Reports" => "Reports",
+	"Time" => "Time",
+	"Date" => "Date",
+	"Reffer" => "Reffers",	
+	"Visitor" => "Visitor",
+	"Statistic" => "Statistic",
+	"Status" => "Status",
+	"Installations" => "Installations",
+	"Enable" => "Enable",
+	"Disable" => "Disable (recommended)",
+	"URI" => "URI (HTTP-address)",
+	"Admin_area_folder" => "Administrative area folder",
+
+"Save" => "﻿を除けば",
+"Cancel" => "取り消し",
+"Edit" => "編集しなさい",
+"Edit page" => "ページを編集しなさい",
+"Delete" => "削除",
+"View page" => "眺めのページ",
+"Structure" => "構造",
+"Add section" => "セクションを加えなさい",
+"Create_subsection" => "部分を作成しなさい",
+"Logout" => "ログアウト",
+"Template" => "型板",
+"Help window" => "ヘルプ・スクリーン",
+"Close window" => "近い窓",
+"This field should be filled" => "この分野は満ちているべきである",
+"Are you sure" => "本当にある",
+"The page was not found" => "ページは見つけられなかった",
+"This page may have been moved, or may no longer exist" => "このページは動くかもしれなかったりまたはもはや存在するかもしれない",
+"Authorization in administration area" => "管理区域の承認",
+"Insert Picture" => "挿入物映像",
+"Browse Picture Source" => "映像の源を拾い読みしなさい",
+"Preview" => "下検分",
+"Accompanying Text" => "テキストに伴うこと",
+"Layout" => "レイアウト",
+"Alignment" => "直線",
+"Middle" => "中間",
+"Bottom" => "底",
+"Top" => "上",
+"Right" => "権利",
+"Left" => "左",
+"Image" => "イメージ",
+"Font" => "壷",
+"Centering" => "集中",
+"Can not upload file" => "ファイルをアップロードするできない",
+"Border thickness" => "厚さに接しなさい",
+"Spacing" => "間隔",
+"Horizontal" => "横",
+"Vertical" => "縦",
+"Insert File" => "挿入物ファイル",
+"Clean HTML" => "HTML をきれいにしなさい",
+"Kibe spacing" => "Kibe の間隔",
+"List" => "リスト",
+"File" => "ファイル",
+"Page" => "ページ",
+"Show" => "ショー",
+"Link" => "リンク",
+"Insert Link" => "挿入物リンク",
+"Page name" => "ページの名前",
+"Add section" => "セクションを加えなさい",
+"Creating subsection" => "部分の作成",
+"Fill in page properties fields, save changes and proceed to editing new page" => "変更を除けばページの特性分野を、記入し、新しいページの編集に進みなさい",
+"Convertor XHTML could not process the page contents. The page will be removed" => "コンバーターXHTML はページの内容を処理できなかった。ページは取除かれる",
+"Container of contents was not found. Check xslt-file for the following structures: " => "内容の容器は見つけられなかった。次の構造のための点検xslt ファイル: ",
+	
+		"Url" => array( "title"=> "文書の分野ULR", "description" => "文書のHTTP の住所からの変数。例えば記述、約" ),
+		"Title" => array( "title"=> "文書の分野のメタヘッディング", "description" => "文書窓のヘッディング。その調査機械のための特別な価値を持っている。"),
+		"Template" => array("title"=> "パターンの分野", "description" => "XSLT のフォーマットの文書データ提示のパターン。"),
+);
+?>
