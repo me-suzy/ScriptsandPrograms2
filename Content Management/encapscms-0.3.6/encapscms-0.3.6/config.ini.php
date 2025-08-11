139ee76b9aa237a5a948abc9aea67ebc6fa12020
me-suzy <me.suzana@gmail.com>
2025-08-12 02:53:17 +0300

    Add Content Management/encapscms-0.3.6/encapscms-0.3.6/config.ini.php

---
+++ Content Management/encapscms-0.3.6/encapscms-0.3.6/config.ini.php
@@ -0,0 +1,17 @@
+<?php
+// database section
+$config["db_host"] = "localhost";
+$config["db_user"] = "root";
+$config["db_pass"] = "";
+$config["db_name"] = "test";
+$config["db_type"] = "mysql";
+//common misc params
+$config["debug"] = "0";
+$config["demo"] = "0";
+$config["path"] = "html/raym/";
+//gallery
+$config["pager_items_per_page"] = "9";
+//admin security, is not yet implemented
+//$config["admin_uname"] = "";
+//$config["admin_pass"] = "";
+?>
