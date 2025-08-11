9827f49568abc8cfc443eeed4f0ca1528b7c7637
me-suzy <me.suzana@gmail.com>
2025-08-12 02:46:05 +0300

    Add Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/searchreplace/langs/ru.js

---
+++ Content Management/edit-point3.00beta/text/jscripts/tiny_mce/plugins/searchreplace/langs/ru.js
@@ -0,0 +1,22 @@
+// RU cp1251 lang variables
+
+tinyMCE.addToLang('',{
+searchreplace_search_desc : 'Íàéòè',
+searchreplace_searchnext_desc : 'Íàéòè îïÿòü',
+searchreplace_replace_desc : 'Íàéòè/Çàìåíèòü',
+searchreplace_notfound : 'Ïîèñê çàâåðø¸í. Èñêîìîå âûðàæåíèå íå íàéäåíî.',
+searchreplace_search_title : 'Íàéòè',
+searchreplace_replace_title : 'Íàéòè/Çàìåíèòü',
+searchreplace_allreplaced : 'Çàìåíà áûëà âûïîëíåíà âî âñåõ ñëó÷àÿõ ñîâïàäåíèÿ èñêîìîãî âûðàæåíèÿ.',
+searchreplace_findwhat : 'Íàéòè ÷òî',
+searchreplace_replacewith : 'Çàìåíèòü ÷åì',
+searchreplace_direction : 'Íàïðàâëåíèå',
+searchreplace_up : 'Ââåðõ',
+searchreplace_down : 'Âíèç',
+searchreplace_case : 'Ñ ó÷¸òîì ðåãèñòðà',
+searchreplace_findnext : 'Íàéòè&nbsp;ñëåäóþùåå',
+searchreplace_replace : 'Çàìåíèòü',
+searchreplace_replaceall : 'Çàìåíèòü&nbsp;âåçäå',
+searchreplace_cancel : 'Îòìåíèòü',
+searchreplace_replace_delta_width : 150
+});
