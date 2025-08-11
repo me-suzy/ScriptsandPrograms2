a0d16fb1e3c9bfc8df978716617cddc05d580768
me-suzy <me.suzana@gmail.com>
2025-08-11 18:00:54 +0300

    Add Content Management/bcwb_v25/bcwb/lang/vi.inc.php

---
+++ Content Management/bcwb_v25/bcwb/lang/vi.inc.php
@@ -0,0 +1,141 @@
+﻿<?PHP
+// vim: set expandtab tabstop=4 shiftwidth=4: 
+// +----------------------------------------------------------------------+
+// | BCWB: Business Card Web Builder                                      |
+// +----------------------------------------------------------------------+
+// | Author:  Dmitry Sheiko <sheiko@cmsdevelopment.com>	                  |
+// | Copyright (c) 2004 Dmitry Sheiko                                     |
+// | http://bcwb.cmsdevelopment.com                                       |
+// +----------------------------------------------------------------------+
+// | This source file is free software; you can redistribute it and/or    |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This source file is distributed in the hope that it will be useful,  |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// +----------------------------------------------------------------------+
+// Release: 23.02.04 (d/m/y)
+// $Id$
+
+$default_charset="UTF-8";
+
+$lang=array(
+
+	"Icon" => "Icon",
+	"CreateIcon" => "Create icon",
+	"Width" => "Width",
+	"Height" => "Height",
+
+	"Need_set_permissions" => "Need set permissions attributes to files/folders",
+	"Run" => "Run",
+	"Archive" => "Archive",
+	"Moved" => "Moved",
+	"Restore_backup" => "Restore backup",
+	"Create_backup" => "Create backup",
+	"Need_authorization" => "Here is a need to authorise",
+	"System_instaled" => "System was installed",
+	"Reports" => "Reports",
+	"Time" => "Time",
+	"Date" => "Date",
+	"Reffer" => "Reffers",	
+	"Visitor" => "Visitor",
+	"Statistic" => "Statistic",
+	"Status" => "Status",
+	"Installations" => "Installations",
+	"Enable" => "Enable",
+	"Disable" => "Disable (recommended)",
+	"URI" => "URI (HTTP-address)",
+	"Admin_area_folder" => "Administrative area folder",
+	
+
+
+	"Run" => "Thực hiện",
+	"Archive" => "Lưu trữ",
+	"Moved" => "Đă di chuyển",
+	"Restore_backup" => "Khôi phục sao lưu",
+	"Create_backup" => "Tạo bản sao lưu",
+	"Need_authorization" => "Không phận sự miễn vào :-)",
+	"System_instaled" => "Hệ thống đă được cài đặt",
+	"Reports" => "Các báo cáo",
+	"Time" => "Thời gian",
+	"Date" => "Ngày",
+	"Reffer" => "Reffers",	
+	"Visitor" => "Khách viếng thăm",
+	"Statistic" => "Thống kê",
+	"Status" => "Trạng thái",
+	"Installations" => "Các thông số cài đặt",
+	"Enable" => "Sẵn sàng",
+	"Disable" => "Không có (khuyến nghị nên dùng)",
+	"URI" => "URI (Địa chỉ HTTP)",
+	"Admin_area_folder" => "Khu vực dàng cho quản trị viên",
+	
+		"Save" => "Lưu",
+		"Cancel" => "Hủy bỏ",
+		"Edit" => "Chỉnh sửa",
+		"Edit_page" => "Chỉnh sửa trang",
+		"Delete" => "Xoá bỏ", 
+		"View_page" => "Xem trang",
+		"Structure" => "Cấu trúc",
+		"Add_item" => "Thêm đoạn",
+		"Create_subitem" => "Tạo đoạn con",
+		"Logout" => "Đăng xuất",
+		"Template_select" => "Khuân hình",
+		"Helpdesk_window" =>"Của sổ trợ giúp",
+		"Close_window" => "Đóng của sổ",
+		"NoUrl" => "Phải điền vào trường này",
+		"You_are_sure" => "Bạn có chắc!",
+		"Page_not_found" => "Không tìm thấy trang",
+		"Page_not_found_desc" => "Trang này có thể bị di chuyển hoặc cũng có thể không tồn tại ",
+		"Authorization_in_admin_area" => "Cho phép trong khu vực quản trị",
+		"Insert_Picture" => "Chèn ảnh",
+		"Browse_Picture_Source" =>"Duyệt hình ảnh nguồn",
+		"Preview" => "Xem trước",
+		"Alternate_Text" => "Văn bản đi kèm", 
+		"Layout" => "Bố cục",
+		"Alignment" => "Sắp thẳng",
+		"Middle" => "Giữa",
+		"Bottom" => "Đáy",
+		"Top" => "Đỉnh",
+		"Right" => "Phải",
+		"Left" => "Trái",
+		"Image" => "Hình ảnh",
+		"Font" => "Phôn chữ",
+		"Justify" => "Trung tâm",
+		"Can_not_save_uploaded_file" => "Không thể tải tệp tin lên",
+		"Border_Thickness" => "Độ dày của viền",
+		"Spacing" => "Khoảng trống",
+		"Horizontal" => "Ngang",
+		"Vertical" =>"Dọc",
+		"Cancel" => "Huỷ bỏ",
+		"Insert_Picture" => "Chèn hình",
+		"Insert_File" =>"Chèn tệp tin",
+		"cleanHTML" => "Thuần HTML",
+		"Indent" => "Khoảng đầu dòng",
+		"List" => "Danh sách",
+		"File" => "Tệp tin",
+		"Page" => "Trang",
+		"Show" => "Hiển thị",
+		"Link" => "Liên kết",
+		"Insert_Link" => "Chèn liên kết",
+		"Link" => "Liên kết",
+		"Page_name" => "Tên trang",
+		"Adding_item" => "Thêm đoạn",
+		"Creating_subitem" => "Tạo đoạn con",
+		"Is_it_necessary_fill_fileld" => "Điền vào trường thuộc tính trang, lưu các thay đổi và tiếp tục chỉnh sửa trang khác",
+		"XHTML_error" => "Bộ chuyển đổi XHTML không thể xử lý nội dung trang. Trang sẽ bị loại bỏ",
+		"send_message" => "Thông điệp được gửi đi",
+		"no_send_message" => "Không thể gửi thông điệp",
+		
+		
+		"Not_found_content_pointer" => "Không tìm thấy nơi chứa nội dung. Kiểm tra tệp tin xslt cho các cấu trúc sau: ",
+		
+		
+		"Url" => array( "title"=> "Trường địa chỉ URL của tài liệu", "description" => "Biến từ địa chỉ HTTP của tài liệu, ví dụ nhu: mô tả, thông tin về tài liệu" ),
+		"Title" => array( "title"=> "Trường tiêu đề chung của tài liệu", "description" => "Tiêu đề của cửa sổ trình bầy tài liệu. Nó chứa các giá trị thích hợp cho các máy tìm kiếm."),
+		"Template" => array("title"=> "Trường mẫu", "description" => "Mẫu dữ liệu mô tả tài liệu theo định dạng XSLT."),
+
+);
+?>
