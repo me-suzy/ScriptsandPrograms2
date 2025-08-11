7ea094b5715e968d11c126178fe35ab0ade8a71c
me-suzy <me.suzana@gmail.com>
2025-08-11 20:14:12 +0300

    Add Content Management/easycms/bottom.php

---
+++ Content Management/easycms/bottom.php
@@ -0,0 +1,138 @@
+<!--Here begins the bottom-->
+
+	   </td>
+
+       <td valign="top" rowspan=2><table border=0 cellpadding=1 cellspacing=1 bgcolor="white" width="150" >
+
+				<tr>
+
+    			<td>
+
+			  		   <table border=0 cellpadding=1 cellspacing=1  width=100%>
+
+			  		   <tr>
+
+    		  		   <td align="center"></td>
+
+			  		   </tr>
+
+			  		   </table>
+
+					   <table border=0 cellpadding=1 cellspacing=1 bgcolor="#ffffff" width=100%>
+
+			  		   <tr>
+
+    		  		   <td align="center">
+
+					   <script type="text/javascript"><!--
+
+google_ad_client = "pub-8911150083511205";
+
+google_ad_width = 160;
+
+google_ad_height = 600;
+
+google_ad_format = "160x600_as";
+
+google_ad_channel ="7020942847";
+
+google_color_border = "FFFFFF";
+
+google_color_bg = "FFFFFF";
+
+google_color_link = "000066";
+
+google_color_url = "000066";
+
+google_color_text = "000066";
+
+//--></script>
+
+<script type="text/javascript"
+
+  src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+
+</script>					   </td>
+
+			  		   </tr>
+
+			  		   </table>
+
+					  
+
+			  </td>
+
+			  </tr>
+
+			  </table>
+
+	   
+
+	   </td>
+
+</tr>
+
+<tr>
+
+<td valign="top"><table border=0 cellpadding=1 cellspacing=1 bgcolor="#003366" width="150" >
+
+				<tr>
+
+    			<td>
+
+			  		   <table border=0 cellpadding=1 cellspacing=1 background="images/back.gif" width=102%>
+
+			  		   <tr>
+
+    		  		   <td align="center"><b>Other Programs</b></td>
+
+			  		   </tr>
+
+			  		   </table>
+
+					   <table border=0 cellpadding=1 cellspacing=1 bgcolor="#ffffff" width=102%>
+
+			  		   <tr>
+
+    		  		   <td ><font size="3"><b>
+
+					  <a href="sp/index.html">Sigma Player</a><br></b>
+
+					  Multimedia Player with a lot of features...
+
+					   </font>
+
+					   
+
+					    </td>
+
+			  		   </tr>
+
+			  		   </table>
+
+					  
+
+			  </td>
+
+<td></td>
+
+
+
+
+
+</tr>
+
+
+
+
+
+
+
+</table>
+
+
+
+</body>
+
+</html>
+
