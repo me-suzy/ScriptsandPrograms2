8ebd01c436e4b73f32478fdac195e828b927073c
me-suzy <me.suzana@gmail.com>
2025-08-11 17:58:37 +0300

    Add Content Management/back-end0.7.2.1/back-end0.7.2.1/tables/PET_petition/drop_petition_tables.sql

---
+++ Content Management/back-end0.7.2.1/back-end0.7.2.1/tables/PET_petition/drop_petition_tables.sql
@@ -0,0 +1,16 @@
+#
+# Table structure for table `pet_alert`
+#
+
+DROP TABLE IF EXISTS `pet_alert`;
+DROP TABLE IF EXISTS `pet_country`;
+DROP TABLE IF EXISTS `pet_data`;
+DROP TABLE IF EXISTS `pet_letters`;
+DROP TABLE IF EXISTS `pet_main`;
+DROP TABLE IF EXISTS `pet_petition`;
+DROP TABLE IF EXISTS `pet_petition2section`;
+DROP TABLE IF EXISTS `pet_petitionText`;
+DROP TABLE IF EXISTS `pet_petition2contact`;
+DROP TABLE IF EXISTS `be_followup`;
+DROP TABLE IF EXISTS `be_followup2contact`;
+DROP TABLE IF EXISTS `be_followup2group`;
