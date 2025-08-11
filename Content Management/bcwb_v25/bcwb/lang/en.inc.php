cc522d2f61aa84b2c6fcb1cff48b027a8728120a
me-suzy <me.suzana@gmail.com>
2025-08-11 18:00:32 +0300

    Add Content Management/bcwb_v25/bcwb/lang/en.inc.php

---
+++ Content Management/bcwb_v25/bcwb/lang/en.inc.php
@@ -0,0 +1,118 @@
+<?PHP
+// vim: set expandtab tabstop=4 shiftwidth=4: 
+// +----------------------------------------------------------------------+
+// | BCWB: Business Card Web Builder                                      |
+// +----------------------------------------------------------------------+
+// | Author:  Dmitry Sheiko <sheiko@cmsdevelopment.com>	                  |
+// | Copyright (c) 2004 Dmitry Sheiko                                     |
+// | http://bcwb.cmsdevelopment.com                                       |
+// +----------------------------------------------------------------------+
+// | This source file is free software; you can redistribute it and/or    |
+// | modify it under the terms of the GNU Lesser General Public           |
+// | License as published by the Free Software Foundation; either         |
+// | version 2.1 of the License, or (at your option) any later version.   |
+// |                                                                      |
+// | This source file is distributed in the hope that it will be useful,  |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+// | Lesser General Public License for more details.                      |
+// +----------------------------------------------------------------------+
+// Release: 23.02.04 (d/m/y)
+// $Id$
+
+$default_charset="ISO-8859-1";
+
+$lang=array(
+	"Icon" => "Icon",
+	"CreateIcon" => "Create icon",
+	"Width" => "Width",
+	"Height" => "Height",
+	
+	"Need_set_permissions" => "Need set permissions attributes to files/folders",
+	"Run" => "Run",
+	"Archive" => "Archive",
+	"Moved" => "Moved",
+	"Restore_backup" => "Restore backup",
+	"Create_backup" => "Create backup",
+	"Need_authorization" => "Here is a need to authorise",
+	"System_instaled" => "System was installed",
+	"Reports" => "Reports",
+	"Time" => "Time",
+	"Date" => "Date",
+	"Reffer" => "Reffers",	
+	"Visitor" => "Visitor",
+	"Statistic" => "Statistic",
+	"Status" => "Status",
+	"Installations" => "Installations",
+	"Enable" => "Enable",
+	"Disable" => "Disable (recommended)",
+	"URI" => "URI (HTTP-address)",
+	"Admin_area_folder" => "Administrative area folder",
+	
+		"Save" => "Save",
+		"Cancel" => "Cancel",
+		"Edit" => "Edit",
+		"Edit_page" => "Edit page",
+		"Delete" => "Delete", 
+		"View_page" => "View page",
+		"Structure" => "Structure",
+		"Add_item" => "Add section",
+		"Create_subitem" => "Create subsection",
+		"Logout" => "Logout",
+		"Template_select" => "Template",
+		"Helpdesk_window" =>"Help window",
+		"Close_window" => "Close window",
+		"NoUrl" => "This field should be filled",
+		"You_are_sure" => "Are you sure",
+		"Page_not_found" => "The page was not found",
+		"Page_not_found_desc" => "This page may have been moved, or may no longer exist",
+		"Authorization_in_admin_area" => "Authorization in administration area",
+		"Insert_Picture" => "Insert Picture",
+		"Browse_Picture_Source" =>"Browse Picture Source",
+		"Preview" => "Preview",
+		"Alternate_Text" => "Accompanying Text", 
+		"Layout" => "Layout",
+		"Alignment" => "Alignment",
+		"Middle" => "Middle",
+		"Bottom" => "Bottom",
+		"Top" => "Top",
+		"Right" => "Right",
+		"Left" => "Left",
+		"Image" => "Image",
+		"Font" => "Font",
+		"Justify" => "Centering",
+		"Can_not_save_uploaded_file" => "Can not upload file",
+		"Border_Thickness" => "Border thickness",
+		"Spacing" => "Spacing",
+		"Horizontal" => "Horizontal",
+		"Vertical" =>"Vertical",
+		"Cancel" => "Cancel",
+		"Insert_Picture" => "Insert Picture",
+		"Insert_File" =>"Insert File",
+		"cleanHTML" => "Clean HTML",
+		"Indent" => "Kibe spacing",
+		"List" => "List",
+		"File" => "File",
+		"Page" => "Page",
+		"Show" => "Show",
+		"Link" => "Link",
+		"Insert_Link" => "Insert Link",
+		"Link" => "Link",
+		"Page_name" => "Page name",
+		"Adding_item" => "Add section",
+		"Creating_subitem" => "Creating subsection",
+		"Is_it_necessary_fill_fileld" => "Fill in page properties fields, save changes and proceed to editing new page",
+		"XHTML_error" => "Convertor XHTML could not process the page contents. The page will be removed",
+		"send_message" => "The message is successfully delivered",
+		"no_send_message" => "It was not possible to send the message",
+		
+		
+		"Not_found_content_pointer" => "Container of contents was not found. Check xslt-file for the following structures: ",
+		
+		
+		"Url" => array( "title"=> "Field ULR of document", "description" => "Variable from HTTP address of document. For example description, about" ),
+		"Title" => array( "title"=> "Field meta-heading of document", "description" => "Heading of document window. Its has special value for search machines."),
+		"Template" => array("title"=> "Field of pattern", "description" => "Pattern of document data presentation in XSLT format."),
+
+);
+?>
