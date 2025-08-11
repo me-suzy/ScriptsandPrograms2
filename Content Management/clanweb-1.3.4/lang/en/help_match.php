6cb69d27a0275fa6d25743c9f10681d890541d9c
me-suzy <me.suzana@gmail.com>
2025-08-11 19:56:12 +0300

    Add Content Management/clanweb-1.3.4/lang/en/help_match.php

---
+++ Content Management/clanweb-1.3.4/lang/en/help_match.php
@@ -0,0 +1,9 @@
+
+<ul>
+  <li>Team 1 and 2: The teams which played</li>
+  <li>Type: PCW/CB or tournament</li>
+  <li>Team 1 and team 2 points</li>
+  <li>Maps: Fill in the played map(s). Separate the maps with a &quot;/&quot;.</li>
+  <li>Lineup: Who played? Separate with &quot;,&quot;. Enter all nicks withous clantag.</li>
+  <li>Report: Enter a matchreport. </li>
+</ul>
