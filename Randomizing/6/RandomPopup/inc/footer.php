13db721ea39ff00053ecd18197c846f4fb9d4271
me-suzy <me.suzana@gmail.com>
2025-08-12 23:31:47 +0300

    Add Randomizing/6/RandomPopup/inc/footer.php

---
+++ Randomizing/6/RandomPopup/inc/footer.php
@@ -0,0 +1,7 @@
+<BR><BR>{placeholder for footer html here}</center>
+<BR>
+<Div align=center><A HREF="http://free-php.net">powered by free-php <? echo $scriptname; ?></A></DIV>
+<BR>
+</center>
+</BODY>
+</HTML>
