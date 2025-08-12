4ac7c4d8807e56c0f913a09f6d20f4eba671a221
me-suzy <me.suzana@gmail.com>
2025-08-12 14:33:08 +0300

    Add File Manipulation/free_download_site_v1.0/Free download site/my_downloads/NucleusCMS Plugins/vote_for_nucleus_plugin_collection.php

---
+++ File Manipulation/free_download_site_v1.0/Free download site/my_downloads/NucleusCMS Plugins/vote_for_nucleus_plugin_collection.php
@@ -0,0 +1,26 @@
+<?php
+/*
+    Copyright (C) 2005 Adullam Limited., All Rights Reserved.
+
+    Unless explicitly acquired and licensed from Licensor under the Technical Pursuit License ("TPL") 
+    Version 1.0 or greater, the contents of this file are subject to the Reciprocal Public License 
+    ("RPL") Version 1.1, or subsequent versions as allowed by the RPL, and You may not copy or use 
+    this file in either source code or executable form, except in compliance with the terms and 
+    conditions of the RPL.
+
+    You may obtain a copy of both the TPL and the RPL (the "Licenses") from Technical Pursuit Inc. 
+    at http://www.technicalpursuit.com.
+
+    All software distributed under the Licenses is provided strictly on an "AS IS" basis, WITHOUT 
+    WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND Adullam Limited HEREBY DISCLAIMS ALL 
+    SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+    PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the Licenses for specific 
+    language governing rights and limitations under the Licenses. 
+    
+    Project Name: Download Site System
+    File-Version: 1.0
+*/
+?>
+<p style="text-align:center;"><strong>
+<a href="http://www.hotscripts.com/Detailed/54297.html?RID=N408312">
+Vote for this collection at HotScripts.com</a></strong></p>
