f5b681e14ea20191b772349e1ab40bf51e646eea
me-suzy <me.suzana@gmail.com>
2025-08-13 02:11:54 +0300

    Add Unsorted/createurgetpaid/terms.php

---
+++ Unsorted/createurgetpaid/terms.php
@@ -0,0 +1,30 @@
+<?
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+	// This script is copyrighted to CreateYourGetPaid©       \\
+	// Duplication, selling, or transferring of this script   \\
+	// is a violation of the copyright and purchase agreement.\\
+	// Alteration of this script in any way voids any         \\
+	// responsibility CreateYourGetPaid© has towards the      \\
+	// functioning of the script. Altering the script in an   \\
+	// attempt to unlock other functions of the program that  \\
+	// have not been purchased is a violation of the          \\
+	// purchase agreement and forbidden by CreateYourGetPaid© \\
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+	include "lib/.htconfig.php";
+
+	$tml->RegisterVar("TITLE", _LANG_TERMS_TITLE);
+	
+	$tml->loadFromFile("pages/header");
+	$tml->Parse();
+	
+	$tml->loadFromFile("pages/terms");
+	$tml->Parse();
+	
+	$tml->loadFromFile("pages/footer");
+	$tml->Parse();
+	
+	$tml->Output();
+	
+?>
