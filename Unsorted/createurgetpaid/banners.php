8eefdcb8589467f08b82a977dba6197c7edac361
me-suzy <me.suzana@gmail.com>
2025-08-13 02:11:07 +0300

    Add Unsorted/createurgetpaid/banners.php

---
+++ Unsorted/createurgetpaid/banners.php
@@ -0,0 +1,24 @@
+<?
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+	// This script is copyrighted to CreateYourGetPaid©       \\
+	// Duplication, selling, or transferring of this script   \\
+	// is a violation of the copyright and purchase agreement.\\
+	// Alteration of this script in any way voids any         \\
+	// responsibility CreateYourGetPaid© has towards the      \\
+	// functioning of the script. Altering the script in an   \\
+	// attempt to unlock other functions of the program that  \\
+	// have not been purchased is a violation of the          \\
+	// purchase agreement and forbidden by CreateYourGetPaid© \\
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+	include "lib/.htconfig.php";
+	
+	if(is_numeric($_GET["id"]))
+	{
+		header("Location: " . $banners->GetURL($_GET["id"]));
+	}
+	else
+		header("Location: index.php?sid=" . $session->ID);
+
+?>
