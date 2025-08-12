4869a02926604329fdc4d7eaf46eec314c72aa49
me-suzy <me.suzana@gmail.com>
2025-08-12 11:58:36 +0300

    Add E-Commerce/enterprise3.01/includes/languages/french/modules/payment/paybox.php

---
+++ E-Commerce/enterprise3.01/includes/languages/french/modules/payment/paybox.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  Contribution by Emmanuel Alliel <manu@maboutique.biz>
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_PAYBOX_TEXT_TITLE', 'Carte bancaire (Paybox)');
+  define('MODULE_PAYMENT_PAYBOX_TEXT_DESCRIPTION', 'Module Paybox realise par Emmanuel Alliel<br>Email: manu@maboutique.biz');
+  define('MODULE_PAYMENT_PAYBOX_TEXT_ERROR', 'Erreur de paiement !');
+  define('MODULE_PAYMENT_PAYBOX_TEXT_ERROR_MESSAGE', 'Il y a eu une erreur durant le traitement de votre paiement CB. Merci de recommencer.');
+?>
