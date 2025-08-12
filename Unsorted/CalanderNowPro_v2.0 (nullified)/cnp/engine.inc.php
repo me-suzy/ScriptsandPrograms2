fc3e7d3691e56ddc0d71b7cdaa8012b4eb4eefda
me-suzy <me.suzana@gmail.com>
2025-08-13 01:25:33 +0300

    Add Unsorted/CalanderNowPro_v2.0 (nullified)/cnp/engine.inc.php

---
+++ Unsorted/CalanderNowPro_v2.0 (nullified)/cnp/engine.inc.php
@@ -0,0 +1,13 @@
+<?
+//////////////////////////////////////////////////////////////////////////////                      
+//                                                                          //
+//  Program Name         : Calander Now Pro                                 //
+//  Program version      : 2.0                                              //
+//  Program Author       : Jason VandeBoom                                  //
+//  Supplied by          : drew010                                          //
+//  Nullified by         : CyKuH [WTN]                                      //
+//  Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                          //
+//////////////////////////////////////////////////////////////////////////////                      
+mysql_connect ("localhost","root","");
+mysql_select_db ("cal");?>
