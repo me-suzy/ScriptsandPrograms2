e9f165c0cbd1e8f17db7a1ece5e98dc63f09c350
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:07 +0300

    Add Content Management/metajour2_1/metajour/lang/frame.en.php

---
+++ Content Management/metajour2_1/metajour/lang/frame.en.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Name';
+$LANG['label_pageid'] = 'Document';
+$LANG['name'] = 'Initial pages';
+$LANG['label_standard'] = '';
+?>
