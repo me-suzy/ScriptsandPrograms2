b1da97853dea61533a6beb68109a4bc14d94df01
me-suzy <me.suzana@gmail.com>
2025-08-12 06:15:03 +0300

    Add Content Management/metajour2_1/metajour/extension/search/lang/search.en.php

---
+++ Content Management/metajour2_1/metajour/extension/search/lang/search.en.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['label_name'] = 'Configuration name';
+$LANG['label_pageid_result'] = 'Result page';
+$LANG['label_templateid_search'] = 'Template for seacrh dialog';
+$LANG['label_templateid_result'] = 'Template for search result';
+$LANG['name'] = 'Configuration of Search';
+?>
