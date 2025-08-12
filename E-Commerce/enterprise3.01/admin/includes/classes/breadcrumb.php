164d6bff19138c151fe37a9429c1af8829af47f6
me-suzy <me.suzana@gmail.com>
2025-08-12 11:39:02 +0300

    Add E-Commerce/enterprise3.01/admin/includes/classes/breadcrumb.php

---
+++ E-Commerce/enterprise3.01/admin/includes/classes/breadcrumb.php
@@ -0,0 +1,43 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  class breadcrumb {
+    var $_trail;
+
+    function breadcrumb() {
+      $this->reset();
+    }
+
+    function reset() {
+      $this->_trail = array();
+    }
+
+    function add($title, $link = '') {
+      $this->_trail[] = array('title' => $title, 'link' => $link);
+    }
+
+    function trail($separator = ' - ') {
+      $trail_string = '';
+
+      for ($i=0, $n=sizeof($this->_trail); $i<$n; $i++) {
+        if (isset($this->_trail[$i]['link']) && escs_not_null($this->_trail[$i]['link'])) {
+          $trail_string .= '<a href="' . $this->_trail[$i]['link'] . '" class="breadcrumbs">' . $this->_trail[$i]['title'] . '</a>';
+        } else {
+          $trail_string .= $this->_trail[$i]['title'];
+        }
+
+        if (($i+1) < $n) $trail_string .= $separator;
+      }
+
+      return $trail_string;
+    }
+  }
+?>
