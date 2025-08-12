4a28220d79d6d9c78bcd274ccbf7278156c7e7a5
me-suzy <me.suzana@gmail.com>
2025-08-12 03:39:00 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_themesdel.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_themesdel.php
@@ -0,0 +1,58 @@
+<?php
+
+/***************************************************************************
+
+ m_themesdel.php
+ ----------------
+ copyright : (C) 2005 - The iziContents Development Team
+
+ iziContents version : 1.0
+ fileversion : 1.0.1
+ change date : 23 - 04 - 2005
+ ***************************************************************************/
+
+/***************************************************************************
+ The iziContents Development Team offers no warranties on this script.
+ The owner/licensee of the script is solely responsible for any problems
+ caused by installation of the script or use of the script.
+
+ All copyright notices regarding iziContents and ezContents must remain intact on the scripts and in the HTML for the scripts.
+
+ For more info on iziContents,
+ visit http://www.izicontents.com*/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the License which can be found within the
+ *   zipped package. Under the licence of GPL/GNU.
+ *
+ ***************************************************************************/
+
+include_once ("rootdatapath.php");
+
+$GLOBALS["form"] = 'themes';
+$validaccess = VerifyAdminLogin();
+
+
+if ($GLOBALS["candelete"] == False) {
+   Header("Location: ".BuildLink('adminlogin.php'));
+} else {
+   DeleteTheme();
+   Header("Location: ".BuildLink('m_themes.php')."&page=".$_GET["page"]."&sort=".$_GET["sort"]);
+}
+
+
+function DeleteTheme()
+{
+   global $EZ_SESSION_VARS, $_GET;
+
+   $sqlString = "DROP TABLE ".$EZ_SESSION_VARS["Site"].$_GET["ThemeCode"]."settings";
+   $result = dbExecute($sqlString,true);
+
+   $strQuery = "DELETE FROM ".$GLOBALS["eztbThemes"]." WHERE themecode='".$_GET["ThemeCode"]."'";
+   $result = dbExecute($strQuery,true);
+   dbCommit();
+} // function DeleteTheme()
+
+?>
