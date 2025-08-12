a889ddc5da24bd42342599f9aefeaf8285cc2e54
me-suzy <me.suzana@gmail.com>
2025-08-12 18:48:01 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/deleteaccount.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/deleteaccount.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='frame' colspan='2'><%WARNING%></td></tr>
+<tr><td class='fframe'><%ACCOUNTNO%></td><td><input type='text' name='cid'></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>' onClick='javascript:return confirm("<%AREYOUSURE%>")'></td></tr>
+</table>
