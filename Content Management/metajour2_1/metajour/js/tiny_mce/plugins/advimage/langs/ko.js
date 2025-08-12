b758416b97cae9b1397c48a68c029670cb097021
me-suzy <me.suzana@gmail.com>
2025-08-12 06:26:20 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/advimage/langs/ko.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/advimage/langs/ko.js
@@ -0,0 +1,8 @@
+// KO lang variables
+
+tinyMCE.addToLang('',{
+insert_image_alt2 : '±×¸² Á¦¸ñ',
+insert_image_onmousemove : '´ëÃ¼ ÀÌ¹ÌÁ&ouml;',
+insert_image_mouseover : '¸¶¿ì½º¸¦ ¿Ã·ÈÀ» ¶§',
+insert_image_mouseout : '¸¶¿ì½º¸¦ ³»·ÈÀ» ¶§'
+});
