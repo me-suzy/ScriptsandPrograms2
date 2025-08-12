823f68516a51d93dcffa1935129fa033681856e3
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:31 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_becomearegistereduser_342663.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_becomearegistereduser_342663.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342663;
+function objfile_342663 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Become a registered user";
+$objdata['subname'] = "";
+$objdata['extension'] = "register";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "<P>&nbsp;</P>";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
