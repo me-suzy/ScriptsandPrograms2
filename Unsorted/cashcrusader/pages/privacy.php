75b9ddb6fa95ce38b6db67afdfaf27efac88c44a
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:19 +0300

    Add Unsorted/cashcrusader/pages/privacy.php

---
+++ Unsorted/cashcrusader/pages/privacy.php
@@ -0,0 +1,81 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr> 
+<td> 
+<center>
+<br>
+<b><font size="3">ZZ Respects Your Privacy</font></b></center>
+<br>
+<br>
+<font size="2"><b>Information Collected</b></font><br><br>
+<font size="2">ZZ has created this privacy statement in order to demonstrate 
+our firm commitment to privacy. ZZ uses your IP address to help diagnose problems 
+with our server, and to administer our Web site. Your IP address is used to gather 
+broad demographic information. ZZ uses cookies to deliver content specific to 
+your interests, to save your password so you don't have to reenter it each time 
+you visit our site, and for other purposes. ZZ's registration form requires users 
+to give us contact information (like their name, address, and email address). 
+ZZ uses member contact information from the registration form to send the member 
+information about our company and email advertisements. <br>
+<br>
+Members may opt-out of receiving future mailings; see the choice/opt-out section 
+below. Demographic and profile data is also collected at our site. We use this 
+data to tailor the visitor's experience at our site, showing them content that 
+we think they might be interested in, and displaying the content according to 
+their preferences. We collect personal off-line information for the sole purpose 
+of paying members for earnings from our program. This site contains links to other 
+sites. ZZ is not responsible for the privacy practices or the content of such 
+Web sites. ZZ uses a sign-up form for new members and advertisers. We collect 
+visitor's contact information (like their email address, address, and name). The 
+member's contact information is used to get in touch with the member when necessary. 
+Users may opt-out of receiving future mailings; see the choice/opt-out section 
+below. </font> <font size="2"><br>
+<br>
+<b>Public Forums:</b></font> <font size="2"><br>
+</font> <font size="2">ZZ, in the future, may make chat rooms, forums, message 
+boards, and/or news groups available to its users. Please remember that any information 
+that is disclosed in these areas becomes public information and you should exercise 
+caution when deciding to disclose your personal information. </font> <font size="2"><br>
+<br>
+<b>Security:</b><br>
+</font> <font size="2">ZZ has security measures in place to protect the loss, 
+misuse and alteration of the information under our control. </font> 
+<div align="left"><font size="2"><br>
+<br>
+<b>Children's Guidelines:</b></font> <font size="2"><br>
+</font> <font size="2">We do not collect online contact information without prior 
+parental consent or parental notification, which will include an opportunity for 
+the parent to prevent use of the information and participation in the activity. 
+Without prior parental consent, online information will only be used to respond 
+directly to the child's request and will not be used for other purposes without 
+prior parental consent. We do not collect personally identifiable off-line contact 
+information without prior parental consent. We do not distribute to third parties 
+any personally identifiable information. We do not give the ability to publicly 
+post or otherwise distribute personally identifiable contact information. We do 
+not entice by the prospect of a special game, prize or other activity, to divulge 
+more information than is needed to participate in the activity. <br>
+</font> </div>
+<div align="left"><font size="2"><br>
+<b>Choice/Opt-Out:<br>
+</b></font> <font size="2">ZZ provides users the opportunity to opt-out of receiving 
+communications from us at the point where we request information about the visitor. 
+ZZ gives users the following options for removing their information from our database 
+to not receive future communications or to no longer receive our service. Log 
+in to your account and click the "User Account Info." Link. At the bottom of the 
+page you will find the option to "Cancel Account". You may also <a href="/pages/contact.php">Contact 
+Us</a> and request your membership to be canceled at anytime. <br>
+</font> <font size="2"><br>
+<b>Correct/Update:<br>
+</b></font></div>
+<p><font size="2">ZZ gives users the following options for changing and modifying 
+information previously provided. Members Area - login and change your personal 
+information. Contacting the Web Site If you have any questions about this privacy 
+statement, the practices of this site, or your dealings with ZZ, you can <a href="/pages/contact.php">Contact 
+Us</a>. <br>
+<br>
+</font></p>
+</table>
+<font size="2" face="arial"> 
+<? include("footer.php");?>
+</font>
