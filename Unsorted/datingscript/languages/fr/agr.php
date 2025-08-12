db00e3f839861d0a04ebb0ba5490a0fd8250a44e
me-suzy <me.suzana@gmail.com>
2025-08-13 02:20:35 +0300

    Add Unsorted/datingscript/languages/fr/agr.php

---
+++ Unsorted/datingscript/languages/fr/agr.php
@@ -0,0 +1,63 @@
+<?=C_SNAME?> Terms of Use Agreement 
+
+
+Welcome to <?=C_SNAME?>, the service for adults to meet each other online operated by <?=C_SNAME?>, (<?=C_SNAME?>). By using the <?=C_SNAME?> web site (the Web site) you agree to be bound by these Terms of Use (this "Agreement"), whether or not you register as a member (Member). If you wish to become a Member and communicate with other Members and make use of the <?=C_SNAME?> service (the "Service"), read these Terms of Use and indicate your acceptance of them by following the instructions in the Registration process. 
+
+This Agreement sets out the legally binding terms of your use of the Web site and your membership in the Service and may be modified by <?=C_SNAME?> from time to time, such modifications to be effective upon posting by <?=C_SNAME?> on the Web site. 
+
+ 
+1.  Eligibility. You must be <?=C_AGES?> or over to register as a member of <?=C_SNAME?> or use the Web site. Membership in the Service is void where prohibited. By using the Web site, you represent and warrant that you have the right, authority, and capacity to enter into this Agreement and to abide by all of the terms and conditions of this Agreement. 
+
+ 
+2.  Term. This Agreement will remain in full force and effect while you use the Web site and/or are a Member. You may terminate your membership at any time, for any reason by following the instructions on the Resign pages in My Account, or upon receipt by <?=C_SNAME?> of your written or email notice of termination. <?=C_SNAME?> may terminate your membership for any reason, effective upon sending notice to you at the email address you provide in your application for membership, or such other email address as you may later provide to <?=C_SNAME?>. If <?=C_SNAME?> terminates your membership in the Service because you have breached the Agreement, you will not be entitled to any refund of unused subscription fees. Even after membership is terminated, this Agreement will remain in effect. 
+
+ 
+3. Non Commercial Use by Members. The Web site is for the personal use of individual Members only and may not be used in connection with any commercial endeavors. Organizations, companies, and/or businesses may not become Members and should not use the Service or the Web site for any purpose. Illegal and/or unauthorized uses of the Web site, including collecting usernames and/or email addresses of members by electronic or other means for the purpose of sending unsolicited email and unauthorized framing of or linking to the Web site will be investigated, and appropriate legal action will be taken, including without limitation, civil, criminal, and injunctive redress. 
+
+ 
+4.  Proprietary Rights in Content on <?=C_SNAME?>. <?=C_SNAME?> owns and retains all proprietary rights in the Web site and the Service. The Web site contains the copyrighted material, trademarks, and other proprietary information of <?=C_SNAME?>, and its licensors. Except for that information which is in the public domain or for which you have been given written permission, you may not copy, modify, publish, transmit, distribute, perform, display, or sell any such proprietary information. 
+
+ 
+5.  Content Posted on the Site. 
+ 
+ a.  You understand and agree that <?=C_SNAME?> may review and delete any content, messages, <?=C_SNAME?> Messenger messages, photos or profiles (collectively, "Content") that in the sole judgment of <?=C_SNAME?> violate this Agreement or which might be offensive, illegal, or that might violate the rights, harm, or threaten the safety of Members.  
+ b.  You are solely responsible for the Content that you publish or display (hereinafter, "post") on the Service, or transmit to other Members.  
+ c.  By posting Content to any public area of <?=C_SNAME?>, you automatically grant, and you represent and warrant that you have the right to grant, to <?=C_SNAME?> an irrevocable, perpetual, non-exclusive, fully paid, worldwide license to use, copy, perform, display, and distribute such information and content and to prepare derivative works of, or incorporate into other works, such information and content, and to grant and authorize sublicenses of the foregoing.  
+ d.  The following is a partial list of the kind of Content that is illegal or prohibited on the Web site. <?=C_SNAME?> reserves the right to investigate and take appropriate legal action in its sole discretion against anyone who violates this provision, including without limitation, removing the offending communication from the Service and terminating the membership of such violators. It includes Content that: 
+is patently offensive to the online community, such as Content that promotes racism, bigotry , hatred or physical harm of any kind against any group or individual; 
+harasses or advocates harassment of another person; 
+involves the transmission of "junk mail", "chain letters," or unsolicited mass mailing or "spamming"; 
+promotes information that you know is false, misleading or promotes illegal activities or conduct that is abusive, threatening, obscene, defamatory or libelous; 
+promotes an illegal or unauthorized copy of another persons copyrighted work, such as providing pirated computer programs or links to them, providing information to circumvent manufacture-installed copy-protect devices, or providing pirated music or links to pirated music files; 
+contains restricted or password only access pages, or hidden pages or images (those not linked to or from another accessible page) ; 
+displays pornographic or sexually explicit material of any kind; 
+provides material that exploits people under the age of 18 in a sexual or violent manner, or solicits personal information from anyone under 18; 
+provides instructional information about illegal activities such as making or buying illegal weapons, violating someones privacy, or providing or creating computer viruses; 
+solicits passwords or personal identifying information for commercial or unlawful purposes from other users; and 
+engages in commercial activities and/or sales without our prior written consent such as contests, sweepstakes, barter, advertising, and pyramid schemes. 
+ 
+ e. You must use the Service in a manner consistent with any and all applicable laws and regulations.  
+ f. You may not include in your Member profile any telephone numbers, street addresses, last names, URLs or email addresses.  
+ g. You may not engage in advertising to, or solicitation of, other Members to buy or sell any products or services through the Service. You may not transmit any chain letters or junk email to other Members. Although <?=C_SNAME?> cannot monitor the conduct of its Members off the Web site, it is also a violation of these rules to use any information obtained from the Service in order to harass, abuse, or harm another person, or in order to contact, advertise to, solicit, or sell to any Member without their prior explicit consent. In order to protect our Members from such advertising or solicitation, <?=C_SNAME?> reserves the right to restrict the number of emails which a Member may send to other Members in any 24-hour period to a number which <?=C_SNAME?> deems appropriate in its sole discretion. 
+
+ 
+6. Copyright Policy. You may not post, distribute, or reproduce in any way any copyrighted material, trademarks, or other proprietary information without obtaining the prior written consent of the owner of such proprietary rights. Without limiting the foregoing, if you believe that your work has been copied and posted on the Service in a way that constitutes copyright infringement, please provide our Copyright Agent with the following information: an electronic or physical signature of the person authorized to act on behalf of the owner of the copyright interest; a description of the copyrighted work that you claim has been infringed; a description of where the material that you claim is infringing is located on the Web site; your address, telephone number, and email address; a written statement by you that you have a good faith belief that the disputed use is not authorized by the copyright owner, its agent, or the law; a statement by you, made under penalty of perjury, that the above information in your notice is accurate and that you are the copyright owner or authorized to act on the copyright owner's behalf. 
+
+7. Disclaimers. <?=C_SNAME?> is not responsible for any incorrect or inaccurate Content posted on the Web site or in connection with the Service, whether caused by users of the Web site, Members or by any of the equipment or programming associated with or utilized in the Service. <?=C_SNAME?> is not responsible for the conduct, whether online or offline, of any user of the Web site or Member of the Service. <?=C_SNAME?> assumes no responsibility for any error, omission, interruption, deletion, defect, delay in operation or transmission, communications line failure, theft or destruction or unauthorized access to, or alteration of, user or Member communications. <?=C_SNAME?> is not responsible for any problems or technical malfunction of any telephone network or lines, computer online systems, servers or providers, computer equipment, software, failure of email or players on account of technical problems or traffic congestion on the Internet or at any web site or combination thereof, including injury or damage to users and/or Members or to any other person's computer related to or resulting from participating or downloading materials in connection with the Web and/or in connection with the Service. Under no circumstances will <?=C_SNAME?> be responsible for any loss or damage, including personal injury or death, resulting from anyone's use of the Web site or the Service, any Content posted on the Web site or transmitted to Members, or any interactions between users of the Web site, whether online or offline. The Web site and the Service are provided "AS-IS" and <?=C_SNAME?> expressly disclaims any warranty of fitness for a particular purpose or non-infringement. <?=C_SNAME?> cannot guarantee and does not promise any specific results from use of the Web site and/or the Service. 
+
+ 
+8. Limitation on Liability. Except in jurisdictions where such provisions are restricted, in no event will <?=C_SNAME?> be liable to you or any third person for any indirect, consequential, exemplary, incidental, special or punitive damages, including also lost profits arising from your use of the Web site or the Service, even if <?=C_SNAME?> has been advised of the possibility of such damages. Notwithstanding anything to the contrary contained herein, <?=C_SNAME?>s liability to you for any cause whatsoever, and regardless of the form of the action, will at all times be limited to the amount paid, if any, by you to <?=C_SNAME?> for the Service during the term of membership. 
+
+ 
+9. Indemnity. You agree to indemnify and hold <?=C_SNAME?>, its subsidiaries, affiliates, officers, agents, and other partners and employees, harmless from any loss, liability, claim, or demand, including reasonable attorney's fees, made by any third party due to or arising out of your use of the Service in violation of this Agreement and/or arising from a breach of this Agreement and/or any breach of your representations and warranties set forth above. 
+
+ 
+10. Other. This Agreement, accepted upon use of the Web site and further affirmed by becoming a Member of the Service, contains the entire agreement between you and <?=C_SNAME?> regarding the use of the Web site and/or the Service. If any provision of this Agreement is held invalid, the remainder of this Agreement shall continue in full force and effect. 
+
+ 
+Please Contact us with any questions regarding this agreement. 
+
+I HAVE READ THIS AGREEMENT AND AGREE TO ALL OF THE PROVISIONS CONTAINED ABOVE.
+ 
+
+ 
