c87602ce7034fd74d2fc128fcb5cc95f1408396c
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:31 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_menusettings.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_menusettings.php
@@ -0,0 +1,53 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Men&uuml;einstellungen verwalten';
+
+//  Form Field Headings
+$GLOBALS["tTopMenuBorder"] = 'Kopf-Men&uuml; Rand';
+$GLOBALS["tTopMenuAlign"] = 'Kopf-Men&uuml; Ausrichtung';
+$GLOBALS["tTopMenuRows"] = 'Kopf-Men&uuml; Spalten';
+$GLOBALS["tTopMenuSeparator"] = 'Kopf-Men&uuml; Teiler';
+$GLOBALS["tMenuBorder"] = 'Men&uuml;rand';
+$GLOBALS["tTopDistance"] = 'Abstand von oben';
+$GLOBALS["tBetweenDistance1"] = 'Abstand zwischen Men&uuml; und Untermen&uuml;';
+$GLOBALS["tBetweenDistance2"] = 'Abstand zwischen Untermen&uuml;punkten';
+$GLOBALS["tIndent"] = 'Untermen&uuml;-Einzug';
+$GLOBALS["tPrivateMenus"] = 'gesch&uuml;tzte Men&uuml;s';
+$GLOBALS["tShowDHTML"] = 'Verwende DHTML Kopf-Men&uuml;';
+
+$GLOBALS["tLocked"] = 'gesperrt';
+$GLOBALS["tHidden"] = 'versteckt';
+
+//  Form Block Titles
+$GLOBALS["thTopMenu"] = 'Kopfmen&uuml;-Einstellungen';
+$GLOBALS["thSideMenu"] = 'Seitenmen&uuml;-Einstellungen';
+$GLOBALS["thAccess"] = 'Einstellungen gesch&uuml;tzte Men&uuml; ';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tiMenuTitle"] = 'Men&uuml; Titel';
+
+$GLOBALS["tDetails"] = '|<br />|- Abstand von oben<br />|<br />\'[Men&uuml;punkt]<br />|<br />|- Abstand zwischen Men&uuml; und Untermen&uuml;/Men&uuml;<br />|<br /><table><tr><td width=10></td><td>[Untermen&uuml;punkt]<br />|<br />|- Abstand zwischen Untermen&uuml;s<br />|<br />[Untermen&uuml;punkt]<br />[Untermen&uuml;punkt]<br /></td></tr></table>[Men&uuml;punkt]<br />[Men&uuml;punkt]<br />[Men&uuml;punkt]<br />';
+
+$GLOBALS["hTopMenuBorder"] = 'Die Randst&auml;rke zwischen den Punkten im Top-Men&uuml;.<br />Setzen Sie diesen Wert auf 0, wenn Sie keinen Rand m&ouml;chten.';
+$GLOBALS["hTopMenuAlign"] = 'Sollen Top-Men&uuml;punkte nach links, rechts oder zentriert ausgerichtet werden, oder sollen sie auf der ganzen Breite der Men&uuml;streifens verteilt werden (Blocksatz)?';
+$GLOBALS["hTopMenuRows"] = 'Dies ist die maximale Anzahl an Men&uuml;punkten, die in einer einzelnen Zeile im Top-Men&uuml; angezeigt werden kann.<br />Wenn mehr Men&uuml;punkte angezeigt werden sollen, wird die Zeile umgebrochen.';
+$GLOBALS["hTopMenuSeparator"] = 'W&auml;hlen Sie den Stil der Trennzeichen, die zwischen den einzelnen Men&uuml;punkten im Top-Men&uuml; verwendet werden.';
+$GLOBALS["hMenuBorder"] = 'Die Randst&auml;rke zwischen Men&uuml;gruppen auf dem Seitemen&uuml;.<br />Setzen Sie diesen Wert auf 0, wenn Sie keinen Rand m&ouml;chten.';
+$GLOBALS["hTopDistance"] = 'Der Abstand vom oberen Frame zum ersten Men&uuml;punkt im Seitenmen&uuml;.';
+$GLOBALS["hBetweenDistance1"] = 'Der Abstand zwischen einem Gruppen-Men&uuml;punkt und einem Untermen&uuml;punkt.';
+$GLOBALS["hBetweenDistance2"] = 'Der Abstand zwischen Untermen&uuml;punkten.';
+$GLOBALS["hIndent"] = 'Der Einzug zwischen dem linken Rand vom Men&uuml;frame und dem Untermen&uuml;punkt.';
+$GLOBALS["hPrivateMenus"] = 'Sollen gesch&uuml;tzte Men&uuml;s versteckt oder gesperrt angezeigt werden?';
+$GLOBALS["hShowDHTML"] = 'Dynamisch generiertes DHTML Kopf-Men&uuml; verwenden (Javascript)';
+
+//  Error Messages
+$GLOBALS["eBorder1"] = 'Der Wert f&uuml;r den Topmen&uuml;-Rand enth&auml;lt keine Zahl.';
+$GLOBALS["eRows1"] = 'Der Wert f&uuml;r den Topmen&uuml;-Zeilenwert enth&auml;lt keine Zahl.';
+$GLOBALS["eBorder2"] = 'Der Wert f&uuml;r den Men&uuml;-Rand ist keine Zahl.';
+$GLOBALS["eDistance1"] = 'Der Wert f&uuml;r den Abstand von Topmen&uuml; zum ersten Seitenmen&uuml; ist keine Zahl.';
+$GLOBALS["eDistance2"] = 'Der Wert f&uuml;r den Abstand zwischen Men&uuml; und dem n&auml;chsten Men&uuml;/Untermen&uuml; ist keine Zahl.';
+$GLOBALS["eDistance3"] = 'Der Wert f&uuml;r den Abstand zwischen den Untermen&uuml;s ist keine Zahl.';
+$GLOBALS["eDistance4"] = 'Der Wert f&uuml;r den Untermen&uuml;-Einzug ist keine Zahl.';
+
+?>
