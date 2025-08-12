86fc7ebee8c7a75d9454f9a7ab3a722d01a17409
me-suzy <me.suzana@gmail.com>
2025-08-12 08:35:09 +0300

    Add Counters/stats1/config.php

---
+++ Counters/stats1/config.php
@@ -0,0 +1,8 @@
+<?
+define("STATS_CONNECTION", 1); // 0 If connection is required, 1 if you have made your own connection
+define("STATS_MYSQL_HOSTNAME", "localhost"); //You probably should leave this
+define("STATS_MYSQL_USERNAME", ""); //MySQL Username
+define("STATS_MYSQL_PASSWORD", ""); //MySQL Password
+define("STATS_MYSQL_DATABASE", ""); //MySQL Database Name
+define("STATS_SITE_URL", "radiantfx.com"); //Your site's url dont include http:// or www.
+?>
