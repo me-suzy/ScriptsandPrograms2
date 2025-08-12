711fec93aa57d165e6b3cb6551c5d972c9e62952
me-suzy <me.suzana@gmail.com>
2025-08-12 08:25:56 +0300

    Add Countdowns/countdown/eventlist.php

---
+++ Countdowns/countdown/eventlist.php
@@ -0,0 +1,21 @@
+<?
+/*
+MVW Counter
+===========
+
+By Gary Kertopermono
+
+This credit tag may not be removed.
+*/
+
+$dummy = true;
+
+$eventname = Array();
+$eventdesc = Array();
+
+$eventname[] = "adventchildren";
+$eventdesc[] = "Advent Children DVD release (US)";
+
+$eventname[] = "adventchildrenjp";
+$eventdesc[] = "Advent Children DVD release (JP)";
+?>
