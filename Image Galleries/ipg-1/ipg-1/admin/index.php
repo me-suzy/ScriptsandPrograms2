d6debfd04231ce2016a90a56ca1ab1df2d7f0089
me-suzy <me.suzana@gmail.com>
2025-08-12 17:58:37 +0300

    Add Image Galleries/ipg-1/ipg-1/admin/index.php

---
+++ Image Galleries/ipg-1/ipg-1/admin/index.php
@@ -0,0 +1,20 @@
+<?php
+/******************************************************************************
+* IPG: Instant Photo Gallery                                                  *
+* =========================================================================== *
+* Software Version:             IPG 1.0                                       *
+* Copyright 2005 by:            Verosky Media - Edward Verosky                *
+* Support, News, Updates at:    http://www.instantphotogallery.com            *
+*******************************************************************************
+* This program is free software; you may redistribute it and/or modify it     *
+* under the terms of the GNU General Public License as published by the Free  * 
+* Software Foundation; either version 2 of the License, or (at your option)   *
+* any later version.                                                          *                                                                             *
+* This program is distributed WITHOUT ANY WARRANTIES; without even any        *
+* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    *
+*                                                                             *
+* See www.gnu.org  for details of the GPL license.                            *
+******************************************************************************/
+include('../includes/config.php');
+include('../includes/functions/fns_std.php');
+redirect('portfolio_admin.php'); ?>
