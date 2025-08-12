2ea9ff5ca4d7b900671e7ed7ee2b613ec57c3e4b
me-suzy <me.suzana@gmail.com>
2025-08-12 23:16:19 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/menu/webring.inc.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/menu/webring.inc.php
@@ -0,0 +1,47 @@
+<?php
+/*******************************************************************************
+ * Copyright (C) 2002 CLAIRE Cédric cedric.claire@safari-msi.com
+ * http://www.portailphp.com/
+ *
+ * Modifié par Martineau Emeric Copyright (C) 2004
+ *
+ * Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le
+ * modifier conformément aux dispositions de la Licence Publique Générale GNU,
+ * telle que publiée par la Free Software Foundation ; version 2 de la licence,
+ * ou encore (à votre choix) toute version ultérieure.
+ *
+ * Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE
+ * GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou
+ * D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence
+ * Publique Générale GNU .
+ *
+ * Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en
+ * même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free
+ * Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+ *
+ * Portail PHP
+ * La présente Licence Publique Générale n'autorise pas le concessionnaire à
+ * incorporer son programme dans des programmes propriétaires. Si votre programme
+ * est une bibliothèque de sous-programmes, vous pouvez considérer comme plus
+ * intéressant d'autoriser une édition de liens des applications propriétaires
+ * avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non
+ * pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+ ***********************************************************************************/
+?>
+<table width='100%'>
+  <tr>
+    <td width='100%' class='tabTitre'  valign='middle' align='center'>
+      <strong><?php echo $Lib_Rub_WebRing ; ?></strong>
+    </td>
+  </tr>
+  <tr>
+    <td width='100%' class='tabMenu'  valign='middle' align='center'>
+      <a href='http://www.toutestfacile.com/phpinit.php?tef_site=php&common=webring' target='_blank'><img src='<?php echo $chemin ; ?>/images/img_webring.gif' border='0' /></a><br />
+      <font size='1'>
+      <a href='http://nav.webring.org/cgi-bin/navcgi?ring=phpfrancophone;random' target='_blank'>Site au hasard</a>
+      <a href='http://nav.webring.org/cgi-bin/navcgi?ring=phpfrancophone;list' target='_blank'>Sites du webring</a><br />
+      <a href='http://www.toutestfacile.com/phpinit.php?tef_site=php&common=webring' target='_blank'>Rejoindre le webring</a><br />
+      </font>
+    </td>
+  </tr>
+</table>
