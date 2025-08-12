235148fd8ebfdb9df3270bd0a61c15ba2c3b3b04
me-suzy <me.suzana@gmail.com>
2025-08-12 08:47:36 +0300

    Add Customer Support/helpdeskfinal/helpdeskfinal/admin_index.php

---
+++ Customer Support/helpdeskfinal/helpdeskfinal/admin_index.php
@@ -0,0 +1,11 @@
+<?php
+//
+// Project: Help Desk support system
+// Description: Admin Main page
+//
+
+// File to redirect to
+$index_page = "admin_summary.php";
+header("Location: $index_page");
+
+?>
