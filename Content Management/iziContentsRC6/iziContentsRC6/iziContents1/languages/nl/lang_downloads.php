25ae5249add344d902e25a48835609c31fc9a043
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:28 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_downloads.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_downloads.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] 		= 'Beheer downloads';
+$GLOBALS["tFormTitle1"] 	= 'Upload download bestanden/creëeren download subdirectories';
+$GLOBALS["tFormTitle2"] 	= 'Upload download bestanden';
+$GLOBALS["tFormTitle3"] 	= 'Creëren subdirectory';
+
+//  List Functions
+$GLOBALS["tAddNewDownload"] 	= 'Nieuwe download toevoegen';
+$GLOBALS["tDeleteDownload"] 	= 'Verwijder download';
+$GLOBALS["tDeleteDownloadDir"] 	= 'Verwijder download directory';
+
+//  List Headings
+$GLOBALS["tDownloadName"] 	= 'Download naam';
+
+//  Form Fields
+$GLOBALS["tDownloadFilename"] 	= 'Download bestandsnaam';
+$GLOBALS["tSubdirectoryname"] 	= 'Directory naam';
+
+//  Form Buttons
+$GLOBALS["bUpload"] 		= 'Upload!';
+$GLOBALS["bCreateDir"] 		= 'Creeëren!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] 		= 'U moet een bestand selecteren om te kunnen uploaden.';
+$GLOBALS["eNoDir"] 		= 'U moet een directory naam opgeven.';
+$GLOBALS["eDirAlreadyExists"]   = 'Deze subdirectory bestaat al.';
+$GLOBALS["eZeroByteFile"] 	= 'Bestand is 0 bytes in grootte';
+$GLOBALS["eNoFileUpload"] 	= 'Het bestand kon niet worden geupload.';
+$GLOBALS["eInvalidFileSize"] 	= 'Bestand is te groot voor uploaden.';
+$GLOBALS["eInvalidFileType"] 	= 'Dit bestandstype is bij ons niet bekend.';
+$GLOBALS["eInvalidMimeType"] 	= 'MIME type van dit bestand is niet bekend.';
+
+?>
