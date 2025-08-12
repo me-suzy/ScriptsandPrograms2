0ff407ac6cd3eeaefea40871a393397bfb6d5376
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:55 +0300

    Add Content Management/metajour2_1/metajour/lang/comment.en.php

---
+++ Content Management/metajour2_1/metajour/lang/comment.en.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Name';
+$LANG['label_subject'] = 'Subject';
+$LANG['label_content'] = 'Content';
+$LANG['name'] = 'Comments';
+?>
