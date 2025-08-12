9fddb9bae5050bebec54668c9c4178a64c69a8a1
me-suzy <me.suzana@gmail.com>
2025-08-12 11:43:38 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/espanol/affiliate_contact.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/espanol/affiliate_contact.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Enviar Email a Afiliados');
+
+define('TEXT_AFFILIATE', 'Afiliado:');
+define('TEXT_SUBJECT', 'Asunto:');
+define('TEXT_FROM', 'Desde:');
+define('TEXT_MESSAGE', 'Mensaje:');
+define('TEXT_SELECT_AFFILIATE', 'Seleccionar Afiliado');
+define('TEXT_ALL_AFFILIATES', 'Todos los Afiliados');
+define('TEXT_NEWSLETTER_AFFILIATES', 'Todos los Suscritos');
+
+define('NOTICE_EMAIL_SENT_TO', 'Aviso: Email enviado a: %s');
+define('ERROR_NO_AFFILIATE_SELECTED', 'Error: No ha seleccionado Afiliado.');
+?>
