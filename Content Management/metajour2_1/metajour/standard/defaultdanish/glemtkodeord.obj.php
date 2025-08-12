11fc0d9df69a3b79d5944d54efbf39990b513b5d
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:40 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/glemtkodeord.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/glemtkodeord.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 30;
+$_objchilds[] = '_child_glemtkodeord_31';
+function objfile_30 () {
+$obj = owNew('document');
+$objdata['name'] = "Glemt kodeord";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
