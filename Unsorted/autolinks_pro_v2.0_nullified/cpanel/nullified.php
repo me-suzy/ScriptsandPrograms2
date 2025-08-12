2e85f2cc5e270bd5a0e8613d26af207762f0648f
me-suzy <me.suzana@gmail.com>
2025-08-13 00:59:29 +0300

    Add Unsorted/autolinks_pro_v2.0_nullified/cpanel/nullified.php

---
+++ Unsorted/autolinks_pro_v2.0_nullified/cpanel/nullified.php
@@ -0,0 +1,109 @@
+<?
+/////////////////////////////////////////////////////////////
+// Program Name         : Autolinks Professional            
+// Program Version      : 2.0                               
+// Program Author       : ScriptsCenter                     
+// Supplied by          : CyKuH [WTN] , Stive [WTN]         
+// Nullified by         : CyKuH [WTN]                       
+// Distribution         : via WebForum and Forums File Dumps
+//                   (c) WTN Team `2002
+/////////////////////////////////////////////////////////////
+  include( "cp_initialize.php" );
+?>
+
+<html>
+<head>
+<link rel="stylesheet" href="main.css">
+</head>
+<body>
+<? showmessage(); ?>
+  <table cellpadding='0' cellspacing='0' border='0' width='100%' bgcolor="#9999CC">
+  <tr>
+    <td>
+        <table cellpadding='4' cellspacing='1' border='0' width='100%'>
+          <tr>
+            <td colspan='2'><font color="#FFFFFF" size="1"><b>Nullification Info</b></font></td>
+          </tr>
+          <tr bgcolor="#F5F5F5"><td width="50%">
+              <p><b>Program Name</b><br>
+            </td><td width="50%">
+             Autolinks Professional
+          </td></tr>
+          <tr bgcolor="#F5F5F5">
+            <td width="50%">
+              <p><b>Program Version</b><br>
+            </td>
+            <td width="50%">
+              v2.0
+          </td></tr>
+          <tr bgcolor="#F5F5F5">
+            <td width="50%">
+              <p><b>Program Author</b><br>
+            </td>
+            <td width="50%">
+             ScriptsCenter
+          </td></tr>
+          <tr bgcolor="#F5F5F5">
+            <td width="50%">
+              <p><b>Home Page</b><br>
+            </td>
+            <td width="50%">
+              http://scriptscenter.com
+          </td></tr>
+          <tr bgcolor="#F5F5F5">
+            <td width="50%">
+              <p><b>Retail Price</b><br>
+            </td>
+            <td width="50%">
+              $119.00 United States Dollars
+          </td></tr>
+          <tr bgcolor="#F5F5F5">
+            <td width="50%">
+              <p><b>WebForum Price</b><br>
+            </td>
+            <td width="50%">
+              $000.00 Always 100% Free
+          </td></tr>
+          <tr bgcolor="#F5F5F5">
+            <td width="50%">
+              <p><b>xCGI Price</b><br>
+            </td>
+            <td width="50%">
+              $000.00 Always 100% Free
+          </td></tr>
+
+          <tr bgcolor="#F5F5F5">
+            <td width="50%">
+              <p><b>Supplied by</b><br>
+            </td>
+            <td width="50%">
+              CyKuH [WTN] , Stive [WTN]
+          </td></tr>
+          <tr bgcolor="#F5F5F5">
+            <td width="50%">
+              <p><b>Nullified by</b><br>
+            </td>
+            <td width="50%">
+              CyKuH [WTN]
+          </td></tr>
+          <tr bgcolor="#F5F5F5">
+            <td width="50%">
+              <p><b>Distribution</b><br>
+            </td>
+            <td width="50%">
+              via WebForum and Forums File Dumps
+          </td></tr>
+          <tr bgcolor="#F5F5F5">
+            <td width="50%">
+              <p><b>Nullification Note</b><br>
+              <font size="1">Removed all live clickable links back to the vendors website. That's it !!! There was nothing else malicious found #:)</font></p>
+            </td>
+          <td width="50%">
+          Thanks All WTN Members.
+          </td></tr>
+
+</table>
+<center> &copy WTN Team `2002
+</form>
+</body>
+</html>
