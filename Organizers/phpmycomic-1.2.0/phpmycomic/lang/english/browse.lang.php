bd491b256a37d639b35a0597f2c67575d4ea0e2b
me-suzy <me.suzana@gmail.com>
2025-08-12 21:32:48 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/browse.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/browse.lang.php
@@ -0,0 +1,41 @@
+<?php
+
+// Variables
+$lang_option_edit = "Edit";
+$lang_option_delete = "Delete";
+$lang_option_none = "None";
+$lang_page_next = "Next Page";
+$lang_page_prev = "Previous Page";
+
+$lang_type_inker = "Inker";
+$lang_type_penciler = "Penciler";
+$lang_type_colorist = "Colorist";
+$lang_type_writer = "Writer";
+$lang_type_letterer = "Letterer";
+$lang_type_cover = "Coverartist";
+$lang_type_publisher = "Publisher";
+$lang_type_genre = "Genre";
+
+// Head titles
+$tpl->assign("lang_head_letters", "Series By Alphabet");
+$tpl->assign("lang_head_series", "Series List");
+$tpl->assign("lang_head_comiclist", "Comic List");
+$tpl->assign("lang_head_lists", "List");
+$tpl->assign("lang_head_by_alphabet", "By Alphabet");
+
+// Lists titles
+$tpl->assign("lang_listall", "List All");
+$tpl->assign("lang_check_all", "Check All");
+$tpl->assign("lang_check_none", "UnCheck All");
+$tpl->assign("lang_list_name", "Name");
+$tpl->assign("lang_list_volume", "Volume");
+$tpl->assign("lang_list_type", "Type");
+$tpl->assign("lang_list_year", "Year");
+$tpl->assign("lang_list_publisher", "Publisher");
+$tpl->assign("lang_list_option", "Options");
+$tpl->assign("lang_list_story", "Story Title");
+$tpl->assign("lang_list_issue", "Issue");
+$tpl->assign("lang_list_issues", "Issues");
+$tpl->assign("lang_list_variation", "Variation");
+
+?>
