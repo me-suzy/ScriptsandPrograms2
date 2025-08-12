e0fa99195d1fd470cd12d5658bf4e41c45d35537
me-suzy <me.suzana@gmail.com>
2025-08-12 18:32:20 +0300

    Add Image Galleries/TinyWebGallery13c/TinyWebGallery13/language/language_en.php

---
+++ Image Galleries/TinyWebGallery13c/TinyWebGallery13/language/language_en.php
@@ -0,0 +1,222 @@
+<?php 
+/*************************
+  Copyright (c) 2004-2005 TinyWebGallery
+  written by Michael Dempfle
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  TWG version: 1.3c
+  $Date: 2005/11/15 09:02 $
+**********************************************/
+// language file - version 1.3 - if you translate a language file where already some parts are translated: Please don't remove the former translator completely - insert him/her behind the lang_translator as a comment
+$lang_translator = "&nbsp;";
+$lang_optionen = "Options";
+$lang_menu_titel = "Caption";
+$lang_kommentar = "Comment";
+
+$lang_titel = "";
+$lang_titel_no = "No galleries are uploaded yet.";
+$lang_select_gallery = "Please select a gallery.";
+$lang_help = "Help";
+$lang_new_window = "New&nbsp;window";
+$lang_login = "Login";
+$lang_logout = "Logout";
+$lang_visitor = "Visits:";
+$lang_total = "Total:";
+$lang_today = "Today:";
+$lang_galleries = "Galleries";
+$lang_picture = "Image";
+$lang_pictures = "Images";
+$lang_of = "/";
+
+$lang_overview = "To the overview";
+$lang_forward = "Next";
+$lang_back = "Last";
+$lang_rotate_left = "Rotate left";
+$lang_rotate_right = "Rotate right";
+$lang_start_slideshow = "Start slideshow";
+$lang_stop_slideshow = "Stop slideshow";
+$lang_thumb_forward = "&gt;"; // = '>'
+$lang_thumb_back="&lt;"; // = '<'
+$lang_views = "&nbsp;views";
+
+$lang_login_php_enter = "Please insert the password to unlock the extended features of the gallery.";
+$lang_login_php_enter_again = "Wrong password<br />Please enter again:";
+
+$lang_kommenar_php_both_fields = "Please fill out both fields.<br />";
+$lang_kommenar_php_enter_name = "Please enter a name<br />(max. 30 Zeichen):";
+$lang_kommenar_php_enter_comment = "Please enter a comment:";
+$lang_kommenar_php_name = "Name:";
+$lang_kommenar_php_comment = "Comment (max. 150 characters):";
+$lang_kommenar_php_speichern = "Save";
+
+$lang_privatelogin_php_wrong_password = "Wrong password<br />Please enter again:";
+$lang_privatelogin_php_password = "Please enter the Password for the gallery<br />'%s':";
+$lang_privatelogin_php_login = "Login";
+
+$lang_titel_php_titel = "Please enter a caption<br />(max. 150 characters):";
+$lang_titel_php_save = "Save";
+
+$lang_slideshowid_php_loading = "Slideshow is loading ...";
+
+$lang_opionen_php_yes = "Yes";
+$lang_opionen_php_no = "No";
+$lang_opionen_php_new_window = "Open Gallery in new window";
+$lang_opionen_php_opt_slideshow = "Optimized slideshow";
+$lang_opionen_php_slideshowintervall = "Slideshow interval (sec.)";
+$lang_opionen_php_ok = "OK";
+$lang_opionen_php_top_nav = "Show only top navigation";
+
+$lang_back_topx = "back&nbsp;to&nbsp;the&nbsp;Top&nbsp;%s";
+$lang_topx = "Top&nbsp;%s";
+$lang_main_topx = "Back to the galleries";
+// new in 1.1
+$lang_loading = "loading...";
+$lang_dhtml_navigation = "Use DHTML navigation";
+$lang_optionen_slideshow = "Type of slideshow";
+$lang_optionen_slideshow_fullscreen = "Maximized";
+$lang_optionen_slideshow_optimized = "Cross fade";
+$lang_optionen_slideshow_normal = "Normal";
+$lang_lowbandwidth = "TinyWebGallery is optimized for slow connections.";
+$lang_highbandwidth = "TinyWebGallery is optimized for fast connections.";
+// new in 1.2
+$lang_email_menu_user = "Email notification";
+$lang_email_menu_admin = "Send email";
+// Sorrymessage for failed subscription
+$lang_email_sorrysignmessage = "Sorry, the email %s is already registered.";
+// Sorrymessage for blank email
+$lang_email_sorryblankmailmessage = "Please enter a email address.";
+// Sorrymessage for invalid emails
+$lang_email_sorryoddmailmessage = "Sorry, The email \"%s\" you entered does not look like a valid email address.";
+// Sorrymessage if someone entered your own mail
+$lang_email_sorryownmailmessage = "Sorry, but I don't really want to get my own newsletter!";
+// Title of the newsletter, will be displayed in the FROM field of the mail client
+$lang_email_subscribemail_subject = "New images are available.";
+// Subscribemessage, will be shown when someone subscribes.
+$lang_email_subscribemessage = "Thanks you for the registration. A confirmation email is sent to your email address.";
+// Subscribemail, will be sent when someone subscribes.
+$lang_email_subscribemail = "You were successful added to the notification newsletter";
+// Unsubscribemessage for deletion, will be followed by the email!
+$lang_email_unsubscribemessage = "The email '%s' was removed.";
+// Unsubscribemessage for failed deletion, will be followed by the email!
+$lang_email_failedunsubscriptionmessage = "Email %s not found.";
+// the welcometext
+$lang_email_welcomemessage = "You can register and cancel the registration for the email notification.<br/>You get informed by the gallery administrator if new images are added to the gallery.";
+// email could not be sent.
+$lang_email_error_send_mail = "The confirmation email could not be sent.<br/>The email server is probably not configured properly. Please contact the gallery administrator.";
+
+$lang_email_add = "Add";
+$lang_email_remove = "Remove";
+$lang_email_send = "Send";
+
+$lang_email_admin_welcomemessage_send = 'You can inform here all registered user (%s) about changes of the gallery.';
+$lang_email_admin_sorryoddsendermailmessage = "Sorry, The sender email \"%s\" you entered does not look like a valid email address.";
+$lang_email_admin_sendermail = "Sender address (your email)";
+$lang_email_admin_subject = "Subject";
+$lang_email_admin_message = "Message";
+$lang_email_admin_sendbutton = "Send";
+$lang_email_admin_sent = "The following message was sent:";
+$lang_email_admin_from = "From";
+$lang_email_admin_notloggedin = "You are not logged in. Please go to the main page and log in properly.";
+
+$lang_visitor_30 = "30 days history";
+
+$lang_login_php_enter = "Please insert the password to unlock the extended features of the gallery.";
+$lang_opionen_php_zoom = "Maximized view";
+$lang_administration = "Administration";
+$lang_opionen_php_zoom_message = "Attention: The \'Maximized view\' is slower than the normal view, because the images are generated on the fly. The images are larger too and therefore they take longer to transfer.\\nPlease use this option only if you have a fast internet connection!";
+$lang_first = "First";
+$lang_last = "Last";
+$lang_no_session = "Session is not available. Login to private galleries is not enabled.";
+$lang_not_loggedin = "You are not logged in.<br/>Please go to the main page and unlock this gallery.";
+$lang_no_topx_images = "No images could be found for this view. <br / >Protected galleries are only included if you unlock them first.";
+
+$lang_tips_overview = array('Tip: Please click on the help link, if you want to know what you can do with the gallery.');
+$lang_tips_thumb = array('Tip: On the small navigation in the left upper corner you can directly jump to all the parent levels.','Tip: The Top 13 on this page does only show the most viewed images of THIS directory.');
+$lang_tips_image = array('Tip: You can use the left/right arrow key to navigate to the next/last image.','Tip: If you login (right upper corner) you can enter captions, delete comments and rotate images permanently.','Tip: You can use smiles like :) or ;) in titles and comments.');
+
+// new 1.3
+$lang_dl_as_zip1 = "You can download all images of this directory in a single archive.<br />Do you want to download the whole directory or continue to the actual image?";
+$lang_dl_as_zip2 = "Actual image";
+$lang_dl_as_zip3 = "All";
+$lang_dl_as_zip4 = "Do not ask again in this session."; // we store this is the session
+
+$lang_download_counter = "Downloads";
+$lang_download_counter_short = "dl";
+// not used in 1.3 yet!
+$lang_email_subscribemail_2_subject = "Please confirm your registration at the TinyWebGallery";
+$lang_email_subscribemail_2 = "Dear TinyWebGallery user:\n\nYou have registered to the notification list of TinyWebGallery. To complete the registration please klick on the link below:\n%s\n\nYour TinyWebGallery";
+// --
+$lang_rating = "Vote";
+$lang_rating1 = "1";
+$lang_rating2 = "2";
+$lang_rating3 = "3";
+$lang_rating4 = "4";
+$lang_rating5 = "5";
+$lang_rating_button = "Rate";
+$lang_rating_text = "Please rate the actual image:";
+$lang_rating_new="New vote: ";
+$lang_rating_vote="Votes";
+
+$lang_rating_security="Please enter the 4 characters<br />of the security code [A-F,0-9]:";
+$lang_rating_help="Please click on the image,<br />if you cannot see the code.";
+$lang_rating_message1="You have already voted.";
+$lang_rating_message2="please close this window. The display of the rating is updated after the next action";
+$lang_rating_message3="Thanks for voting.";
+$lang_rating_message4="You have entered a wrong code.";
+$lang_rating_send="Send";
+
+$lang_fileinfo="Info";
+$lang_fileinfo_name = "File name";
+$lang_fileinfo_date = "Date";
+$lang_fileinfo_size = "Size";
+$lang_fileinfo_resolution = "Resolution";
+$lang_fileinfo_views = "Views";
+$lang_fileinfo_dl = "Downloads";
+$lang_fileinfo_rating = "Rating";
+$lang_fileinfo_not_available = " - (remote)";
+$lang_exif_not_available = " - ";
+// please check the constants in the file inc/exifReader.inc.php for exif entries you can try - not all are available on all cameras! sometimes the constants don't work - try to google for the real exif field name!
+$lang_exif_info = array("Camera Model" => "model", "Exif Date" => "DateTime", "Focal Length" => 
+"focalLength", "f/Stop" => "fnumber", "Shutter Speed" => "exposureTime", "ISO" => "isoEquiv");
+
+$lang_comments = "Comments";
+$lang_last_comments="Newest comments";
+$lang_show_kommentar="Show comments";
+$lang_add_kommentar="Add comment";
+$lang_close_fullscreen="Back to the normal view";
+
+$lang_search_results="Search resuls";
+$lang_search_back="Back to the search results.";
+$lang_search="Search";
+$lang_search_text="Please enter the search text:";
+$lang_search_button="Search";
+$lang_search_where="Search in";
+$lang_search_max="Hits shown per page";
+$lang_search_all="All";
+$lang_search_hits="Found %s hits.";
+
+// height of the i_frames - please check if the settings match for your language!!
+$lang_height_comment = 150;
+$lang_height_caption = 90;
+$lang_height_login = 80;
+$lang_height_private = 80;
+$lang_height_options = 205;
+if (!$show_new_window) { $lang_height_options -= 30; }
+if (!$show_slideshow)  { $lang_height_options -= 60; }
+if (!$enable_maximized_view)  { $lang_height_options -= 30; }
+$lang_height_email_user = 180;
+$lang_height_email_admin = 280;
+$lang_height_info = 150;
+if (!$show_download_counter || !$enable_download_counter) { $lang_height_info -= 17; }
+if (!$show_count_views) { $lang_height_info -= 17; }
+if (!$show_image_rating) { $lang_height_info -= 17; }
+if ($show_exif_info) { $lang_height_info += (count($lang_exif_info) * 17);}
+$lang_height_rating = 150;
+$lang_height_dl_manager=150;
+$lang_height_search=175;
+$lang_xpos_lang_dropdown=200; // this is only needed if you include twg and ie does not position the language dropdown correct because you include twg in several divs - in a fixed layout you can adjust this here - if you don't have a fix layout don't use the dropdown - use the normal setting! 
+?>
