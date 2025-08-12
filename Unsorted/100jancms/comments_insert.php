2e86b1bc95d6e15a0528f5a35ecf0c34b2fa6d29
me-suzy <me.suzana@gmail.com>
2025-08-13 00:08:35 +0300

    Add Unsorted/100jancms/comments_insert.php

---
+++ Unsorted/100jancms/comments_insert.php
@@ -0,0 +1,79 @@
+<?php
+/***************************************\
+|					|
+|	    100janCMS v1.01		|
+|    ------------------------------	|
+|    Supplied & Nulled by GTT '2004	|
+|					|
+\***************************************/
+// do not cache
+header ("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    	        // Date in the past
+header ("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
+header ("Cache-Control: no-cache, must-revalidate");  	        // HTTP/1.1
+header ("Pragma: no-cache");                          	        // HTTP/1.0
+ 
+// Restrict acces to this page
+
+//this page clearance
+$arr = array (  
+  '0' => 'ADMIN',
+  '1' => 'COMMENTS_MASTER',  
+  );
+define('CONSTANT_ARRAY',serialize($arr));
+
+// check
+include 'restrict_access.php';
+
+//configuration file
+include 'config_inc.php'; 
+
+
+//receive posted data
+$id=$_POST["id"];
+$text=$_POST["text"];
+
+?>
+<html>
+<head>
+<title>100janCMS Articles Control</title>
+<?php echo "$text_encoding"; ?>
+<link href="cms_style.css" rel="stylesheet" type="text/css">
+<meta http-equiv="refresh" content="3;URL=comments_search.php">
+
+<style type="text/css">
+body
+{
+background-image: 
+url("images/app/page_bg.jpg");
+background-repeat: 
+repeat-y;
+background-attachment: 
+fixed
+}
+</style>
+
+</head>
+
+<body bgcolor="#FFFFFF" leftmargin="22" rightmargin="0"  topmargin="0" marginwidth="0" marginheight="0" class="maintext">
+<table width="100%" height="60" border="0" cellpadding="0" cellspacing="0" class="mainsectiopntable" >
+  <tr> 
+    <td class="titletext0">Comments: View/Edit Comments:<span class="titletext0blue"> 
+      Edit Comment</span></td>
+  </tr>
+</table>
+  
+<br>
+<br>
+
+<strong>Status:</strong> &nbsp;&nbsp;Comment has been saved! &nbsp;<img src="images/app/all_good.jpg" width="16" height="16" align="absmiddle"><br>
+  <?php 
+
+$text=htmlspecialchars($text ,ENT_QUOTES);
+
+		$query = "UPDATE ".$db_table_prefix."comments SET text='$text' WHERE idComm='".$id."'";
+		mysql_query($query);
+
+?>
+</p>
+</body>
+</html>
