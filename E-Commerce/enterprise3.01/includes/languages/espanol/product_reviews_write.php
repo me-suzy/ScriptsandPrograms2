9bdddf4df9d1d77785af8456b9dde0f7ec84b4f1
me-suzy <me.suzana@gmail.com>
2025-08-12 11:57:27 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/product_reviews_write.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/product_reviews_write.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Comentarios');
+
+define('SUB_TITLE_FROM', 'De:');
+define('SUB_TITLE_REVIEW', 'Comentario:');
+define('SUB_TITLE_RATING', 'Evaluaci&oacute;n:');
+
+define('TEXT_NO_HTML', '<small><font color="#ff0000"><b>NOTA:</b></font></small>&nbsp;No se traducir&aacute; el codigo HTML!');
+define('TEXT_BAD', '<small><font color="#ff0000"><b>MALO</b></font></small>');
+define('TEXT_GOOD', '<small><font color="#ff0000"><b>BUENO</b></font></small>');
+
+define('TEXT_CLICK_TO_ENLARGE', 'Haga Click para agrandar');
+?>
