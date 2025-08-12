deeba0efdf6db2c3df8e9fb136876d2829465079
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:39 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_serversettings.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_serversettings.php
@@ -0,0 +1,50 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Servereinstellungen verwalten';
+
+//  Form Field Headings
+$GLOBALS["tGzipSetting"] = 'gzip Komprimierung verwenden';
+$GLOBALS["tSecureServer"] = 'Secure server';
+$GLOBALS["tGzipTest"] = 'Komprimierung testen';
+$GLOBALS["tGzipSupported"] = 'Ihr Server unterst&uuml;tzt die GZIP Komprimierung.';
+$GLOBALS["tGzipUnsupported"] = 'Ihr Server unterst&uuml;tzt keine GZIP Komprimierung.';
+$GLOBALS["tMultiSite"] = 'Mehrfach-Seiten-Modus eingeschaltet';
+$GLOBALS["tMultiSiteAuthors"] = 'Multi-Site mit gew&ouml;hnlichen Usern';
+$GLOBALS["tMultiLanguage"] = 'Mehrfach-Sprachen eingeschaltet';
+$GLOBALS["tMultiTheme"] = 'Mehrfach-Theme eingeschaltet';
+$GLOBALS["tPageTimer"] = 'Seitenerstellungs-Zeit anzeigen';
+$GLOBALS["tDefaultLanguage"] = 'Standard Sprache';
+$GLOBALS["tDateFormat"] = 'Datumsformatierung';
+$GLOBALS["tTimezone"] = 'Server Zeitzone';
+$GLOBALS["tFrameSetting"] = 'Frames/keine Frames';
+$GLOBALS["tVisitorStats"] = 'Zugriffs-Statistiken einschalten';
+
+//  Form Block Titles
+$GLOBALS["thServerOptions"] = 'Server Optionen';
+$GLOBALS["thezContentsOptions"] = 'Wwebsite Optionen';
+
+//  Form Field Options
+$GLOBALS["tGzipCompression"] = 'Text komprimieren';
+$GLOBALS["tGzipNoCompression"] = 'nicht komprimieren';
+$GLOBALS["tTimerDisplay"] = 'Timer anzeigen';
+$GLOBALS["tTimerNoDisplay"] = 'Timer nicht anzeigen';
+$GLOBALS["tFrames"] = 'Frames';
+$GLOBALS["tNoFrames"] = 'keine frames';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'In diesem Formular k&ouml;nnen Sie einige globale Werte festlegen, die das Aussehen Ihrer Webseite steuern.';
+$GLOBALS["hGzipSetting"] = 'Setzen Sie diesen Haken, um die GZIP Komprimierung einzuschalten, falls Ihr Server dies unterst&uuml;tzt.';
+$GLOBALS["hSecureServer"] = 'Wenn Ihr Server https unterst&uuml;tzt, k&ouml;nnen Sie hier eine Markierung setzten, um ein sicheres Login zu erm&ouml;glichen.<br /><br /><B>Achtung:-</B> Diese Funktionalit&auml;t ist noch nicht eingebaut.';
+$GLOBALS["hMultiSite"] = 'Setzen Sie diesen Wert auf \'Ja\' wenn Sie vorhaben, mehr als eine ezContents Webseite auf diesem Server zu installieren.';
+$GLOBALS["hMultiSiteAuthors"] = 'Setzen Sie diesen Wert auf  \'Ja\' wenn Sie Usern Zugang zu allen Seiten auf diesem Server im Mehrfach-Seiten-Modus zu gew&auml;hren.';
+$GLOBALS["hMultiLanguage"] = 'Setzen Sie diesen Wert auf  \'Ja\' wenn Sie vorhaben, Ihre Seite in mehr als einer Sprache verwalten wollen.';
+$GLOBALS["hMultiTheme"] = 'Setzen Sie diesen Wert auf  \'Ja\' wenn Sie alternative Themes (Layout/Aussehen) f&uuml;r Ihre Seite einschalten wollen.';
+$GLOBALS["hPageTimer"] = 'Schalten Sie den Timer ein, um die Zeit anzuzeigen, die ben&ouml;tigt wird, um die Seite vom Server erstellen zu lassen.<br />Dies ist in erster Linie ein Diagnose-Tool f&uuml;r die Entwicklung, um die Effizienz des Codes von ezContents zu &uuml;berpr&uuml;fen.';
+$GLOBALS["hDefaultLanguage"] = 'Setzen Sie hier die Standard-Sprache f&uuml;r Ihre Seite fest.';
+$GLOBALS["hDateFormat"] = 'Formatierung des angezeigten Datums.<br /><br />Code, der verwendet werden kann, basiert auf dem Open Group standard, und schliesst ein:<br /><ul><li><b>%a</b> = Tag der Woche (3 Buchstaben)<br />z.B. Fri<li><b>%A</b> = Tag der Woche (lang)<br />z.B. Freitag<li><b>%d</b> = Tag des Monats (2 Ziffern)<br />z.B. 01<li><b>%e</b> = Tag des Monats (1 or 2 Ziffern)<br />z.B. 1<br />(Scheint nicht immer zu funktionieren, obwohl vom Open Group Standard definiert.)<li><b>%b</b> = Monat (Text, 3 Buchstaben)<br />z.B. Mai<li><b>%B</b> = Monat (Text, lang<br />z.B. M&auml;rz<li><b>%m</b> = Monat (2 Ziffern)<br />z.B. 03<li><b>%Y</b> = Jahr (4 Ziffern)<br />z.B. 2003<li><b>%y</b> = Jahr (2 Ziffern)<br />z.B. 02<br /><br /><li><b>%x</b> Standard Format f&uuml;r locale<br />(h&auml;ngt vom Server ab)</ul>';
+$GLOBALS["hTimezone"] = 'Zeitzone zur Anzeige in Datum/Zeit-Anzeigen.<br />z.B. \"UST\", \"UST+1\"<br />Wenn Ihre <i>Datumsformatierung</i> die Zeit einschliesst, kann dies Lesern in der Welt eine Idee von der Zeitverschiebung zu ihrer eigenen Zeitzone geben; ansonsten lassen Sie es einfach leer.';
+$GLOBALS["hFrameSetting"] = 'Die Seite verwendet Frames (feststehende Rahmen f&uuml;r die Navigation und den Kopf) oder wird im Tabellen-Layout erstellt, ohne Frames.';
+$GLOBALS["hVisitorStats"] = 'M&ouml;chten Sie die Zugriffsstatistiken auf Ihrer Seite verwalten? Sie k&ouml;nnen dann unter dem Punkt Statistiken einfach die Besucherzahlen  ersehen';
+
+?>
