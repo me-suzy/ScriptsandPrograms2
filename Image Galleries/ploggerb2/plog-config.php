7837bfa258862843d460d76b98e4a9c9969c60d4
me-suzy <me.suzana@gmail.com>
2025-08-12 18:17:03 +0300

    Add Image Galleries/ploggerb2/plog-config.php

---
+++ Image Galleries/ploggerb2/plog-config.php
@@ -0,0 +1,8 @@
+<?php
+// this is the file used to connect to your database.
+// you must change these values in order to run the gallery.
+define("PLOGGER_DB_HOST","");
+define("PLOGGER_DB_USER","");
+define("PLOGGER_DB_PW","");
+define("PLOGGER_DB_NAME","");
+?>
