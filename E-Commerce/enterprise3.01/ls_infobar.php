f99ba3f837bf578da8ab0803f7a4fa1a92aae2ec
me-suzy <me.suzana@gmail.com>
2025-08-12 11:27:51 +0300

    Add E-Commerce/enterprise3.01/ls_infobar.php

---
+++ E-Commerce/enterprise3.01/ls_infobar.php
@@ -0,0 +1,50 @@
+<?
+/*
+
+	Live Support v 1.2b
+	Author: Mike Lessar <mlessar@bluetruck.net>
+
+	for use with:
+	Enterprise Shopping Cart
+	http://www.enterprisecart.com
+
+	Released under the GNU General Public License
+
+*/
+
+?>
+<!doctype HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<style type=text/css>TD.main {
+	FONT-SIZE: 8pt; COLOR: #000000; FONT-FAMILY: arial
+}
+TD.header {
+	FONT-SIZE: 8pt; COLOR: #ffffff; FONT-FAMILY: arial
+}
+TD.hidden {
+	FONT-SIZE: 0pt; COLOR: #ffffff; FONT-FAMILY: arial
+}
+A.bluelink {
+	FONT-SIZE: 8pt; COLOR: #0000ff; FONT-FAMILY: arial; TEXT-DECORATION: none
+}
+A.bluelink:hover {
+	TEXT-DECORATION: underline
+}
+A.whitelink {
+	FONT-SIZE: 8pt; COLOR: #ffffff; FONT-FAMILY: arial; TEXT-DECORATION: none
+}
+A.bluelink:hover {
+	TEXT-DECORATION: underline
+}
+.TextBox {
+	BORDER-RIGHT: #000000 1px inset; BORDER-TOP: #000000 1px inset; FONT-SIZE: 12px; BACKGROUND-ATTACHMENT: fixed; BACKGROUND-IMAGE: url(images/box_shadow.gif); BORDER-LEFT: #000000 1px inset; COLOR: #000000; BORDER-BOTTOM: #000000 1px inset; FONT-FAMILY: Verdana, Arial, Helvetica, sans-serif
+}
+.Button {
+	BORDER-RIGHT: #000000 1px inset; BORDER-TOP: #000000 1px inset; FONT-SIZE: 11px; BORDER-LEFT: #000000 1px inset; COLOR: #000000; BORDER-BOTTOM: #000000 1px inset; FONT-FAMILY: Verdana, Arial, Helvetica, sans-serif
+}
+</style>
+</head>
+<body style="MARGIN: 0px" leftMargin=0 topMargin=0 rightMargin=0 marginheight="0" marginwidth="0">
+</body>
+</html>
