dbf3b172db66e27f7a92cc455ed93cf550419395
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:28 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/account_newsletters.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/account_newsletters.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Mi Cuenta');
+define('NAVBAR_TITLE_2', 'Subscripci&oacute;n a Boletines');
+
+define('HEADING_TITLE', 'Subscripci&oacute;n a Boletines');
+
+define('MY_NEWSLETTERS_TITLE', 'Mis Subscripciones');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER', 'Bolet&iacute;n General');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER_DESCRIPTION', 'Incluye noticias, nuevos productos, ofertas especiales y otros anuncios promocionales.');
+
+define('SUCCESS_NEWSLETTER_UPDATED', 'Se han actualizado sus subscrictiones correctamente.');
+?>
