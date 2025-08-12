05a9fa9ec07238a100cda22fd456630d09e0169c
me-suzy <me.suzana@gmail.com>
2025-08-12 12:40:34 +0300

    Add E-Commerce/shop-script-free/includes/admin/sub/conf_general.php

---
+++ E-Commerce/shop-script-free/includes/admin/sub/conf_general.php
@@ -0,0 +1,40 @@
+<?php
+/*****************************************************************************
+ *                                                                           *
+ * Shop-Script FREE                                                          *
+ * Copyright (c) 2005 WebAsyst LLC. All rights reserved.                     *
+ *                                                                           *
+ ****************************************************************************/
+
+	//general settings
+
+	if (!strcmp($sub, "general"))
+	{
+
+		if (isset($_GET["save_successful"])) //show successful save confirmation message
+		{
+			$smarty->assign("configuration_saved", 1);
+		}
+
+		if (isset($_POST["save_general"])) //save system settings
+		{
+
+			//general settings
+			$f = fopen("./cfg/general.inc.php","w");
+			fputs($f,"<?php\n\tdefine('CONF_SHOP_NAME', '".str_replace("'","\'",stripslashes($_POST["shop_name"]))."');\n");
+			fputs($f,"\tdefine('CONF_SHOP_URL', '".str_replace("'","\'",stripslashes($_POST["shop_url"]))."');\n");
+			fputs($f,"\tdefine('CONF_GENERAL_EMAIL', '".str_replace("'","\'",stripslashes($_POST["general_email"]))."');\n");
+			fputs($f,"\tdefine('CONF_ORDERS_EMAIL', '".str_replace("'","\'",stripslashes($_POST["orders_email"]))."');\n");
+			fputs($f,"\tdefine('CONF_PAYPAL_EMAIL', '".str_replace("'","\'",stripslashes($_POST["paypal_email"]))."');\n");
+			fputs($f,"\tdefine('CONF_PAYPAL_INTEGRATION', ".((int)$_POST["paypal_enabled"]).");\n");
+			fputs($f,"?>");
+			fclose($f);
+
+			header("Location: admin.php?dpt=conf&sub=general&save_successful=yes");
+		}
+
+		//set sub-department template
+		$smarty->assign("admin_sub_dpt", "conf_general.tpl.html");
+	}
+
+?>
