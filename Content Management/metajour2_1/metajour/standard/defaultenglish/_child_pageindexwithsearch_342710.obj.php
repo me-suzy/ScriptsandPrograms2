388d14ed1947e8bb5e53e3c786bde1b8b8d55377
me-suzy <me.suzana@gmail.com>
2025-08-12 07:15:10 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_pageindexwithsearch_342710.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_pageindexwithsearch_342710.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342710;
+function objfile_342710 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Page index with search";
+$objdata['subname'] = "";
+$objdata['extension'] = "indexadv";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
