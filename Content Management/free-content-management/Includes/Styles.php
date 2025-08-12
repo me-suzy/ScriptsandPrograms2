4208d9a5a55dc18fd6a41efd11f6535dd3255b87
me-suzy <me.suzana@gmail.com>
2025-08-12 03:34:21 +0300

    Add Content Management/free-content-management/Includes/Styles.php

---
+++ Content Management/free-content-management/Includes/Styles.php
@@ -0,0 +1,43 @@
+<STYLE TYPE="text/css">
+  BODY {background-color:#FFFFFF}
+  #tableback {
+	border-color:#CCFF99;
+	border-style: solid dotted;
+  }
+.button {
+	BORDER-RIGHT: #666666 1px solid;
+		BORDER-TOP: #666666 1px solid;
+		FONT-SIZE: 11px;
+		BORDER-LEFT: #666666 1px solid;
+		WIDTH: 90px;
+		CURSOR: hand;
+		COLOR: #555555;
+		BORDER-BOTTOM: #666666 1px solid;
+		FONT-FAMILY: Verdana,  Arial, Helvetica, sans-serif;
+		BACKGROUND-COLOR: #dadeeb
+}
+.buttonover {
+	BORDER-RIGHT: #666666 1px solid;
+	BORDER-TOP: #666666 1px solid;
+	FONT-SIZE: 11px;
+	BACKGROUND: #b0c4de;
+	BORDER-LEFT: #666666 1px solid;
+	WIDTH: 90px;
+	CURSOR: hand;
+	COLOR: #555555;
+	BORDER-BOTTOM: #666666 1px solid;
+	FONT-FAMILY: Verdana,  Arial, Helvetica, sans-serif
+}
+.smbutton {
+	BORDER-RIGHT: #666666 1px solid;
+	BORDER-TOP: #666666 1px solid;
+	FONT-SIZE: 8pt;
+	BORDER-LEFT: #666666 1px solid;
+	CURSOR: hand;
+	COLOR: #000000;
+	BORDER-BOTTOM: #666666 1px solid;
+	FONT-FAMILY: Verdana,  Arial, Helvetica, sans-serif;
+	BACKGROUND-COLOR: #d4d0c8
+}
+
+</STYLE>
