4e064a9f7910bb72e745a7ef350273983ba047bc
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:54 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/sgningpwebsite.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/sgningpwebsite.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 64;
+$_objchilds[] = '_child_sgningpwebsitet_65';
+function objfile_64 () {
+$obj = owNew('document');
+$objdata['name'] = "Søgning på website";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
