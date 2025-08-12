6699badb4230a8b2820475f315837d2e5db5e87f
me-suzy <me.suzana@gmail.com>
2025-08-12 08:32:16 +0300

    Add Counters/DBCounter/count.php

---
+++ Counters/DBCounter/count.php
@@ -0,0 +1,17 @@
+<?php
+
+print "<font face='verdana' size='1'>";
+
+If (file_exists( "counter.txt" )){
+	include_once "read.php";
+
+	$count++;
+
+	include_once "write.php";
+} else {
+	print "<font color='red'><b>Error</b>: Cannot find Counter Data File!</font>";
+}
+
+print "</font>";
+
+?>
