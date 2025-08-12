c7d0a1989aa6254b22afa98377ad364579c8910a
me-suzy <me.suzana@gmail.com>
2025-08-12 11:53:05 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/down_for_maintenance.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/down_for_maintenance.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  Created by: Linda McGrath osCOMMERCE@WebMakers.com
+
+  Update by: fram 05-05-2003
+  Updated by: Donald Harriman - 08-08-2003 - MS2
+
+  down_for_maintenance.php v1.1
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Down for Maintenance');
+define('HEADING_TITLE', 'Down for Maintenance ...');
+define('DOWN_FOR_MAINTENANCE_TEXT_INFORMATION', 'The site is currently down for maintenance. Please excuse the dust, and try back later.');
+define('TEXT_MAINTENANCE_ON_AT_TIME', 'The Admin / Webmaster has enabled maintenance at : ');
+define('TEXT_MAINTENANCE_PERIOD', 'Maintenance period : ');
+define('DOWN_FOR_MAINTENANCE_STATUS_TEXT', 'To verify the site status ... Click here:');
+?>
