14a68b892df9005ffe1218fe322af355cdb39650
me-suzy <me.suzana@gmail.com>
2025-08-12 08:20:08 +0300

    Add Content Management/wizard/wizard/admin/webmaster_auth.php

---
+++ Content Management/wizard/wizard/admin/webmaster_auth.php
@@ -0,0 +1,17 @@
+<?php
+
+	
+if (!user_isloggedin()) {
+	    $message = NOTLOGGED; //You are not logged in
+        $location = CMS_WWW . "/templates/forms/login.php?message=$message&pageid=2";
+		header("Location: $location");
+		 
+    }
+	if (!is_memberof(1)) {
+	    
+	    $message = WEBMASTERONLY; //Access restricted to webmaster.
+		echo "<tr><td><table border=\"1\" width=\"100%\" bgcolor=\"#FFFFFF\"><tr><td><p>&nbsp;</p><p align=\"center\"><img alt=\"restricted\" src=\"admin/images/restricted.gif\" height=\"32\" width=\"32\" /></p><p align=\"center\">$message</p></td></tr></table>";
+        
+		exit();
+	}
+?>
