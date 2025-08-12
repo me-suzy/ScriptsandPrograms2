faced97910998662b9aff00ab94a807956e0c7cb
me-suzy <me.suzana@gmail.com>
2025-08-12 11:56:10 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_sales.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_sales.php
@@ -0,0 +1,32 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Affiliate Program');
+define('HEADING_TITLE', 'Affiliate Program: Sales');
+
+define('TABLE_HEADING_DATE', 'Date');
+define('TABLE_HEADING_SALES', 'Affiliate Earnings (excl.)');
+define('TABLE_HEADING_VALUE', 'Sale Value (excl.)');
+define('TABLE_HEADING_PERCENTAGE','Commission Rate');
+define('TABLE_HEADING_STATUS', 'Sale Status');
+
+define('TEXT_DELETED_ORDER_BY_ADMIN', 'Deleted (Admin)');
+define('TEXT_INFORMATION_SALES_TOTAL', 'Your current earnings amount (excl.) to:');
+define('TEXT_INFORMATION_SALES_TOTAL2', '<br>Only delivered sales are counted!');
+define('TEXT_NO_SALES', 'No sales have been recorded yet.');
+define('TEXT_DISPLAY_NUMBER_OF_SALES', 'Displaying <b>%d</b> to <b>%d</b> (of <b>%d</b> sales)');
+
+define('TEXT_AFFILIATE_HEADER', 'Sales from your website:');
+?>
