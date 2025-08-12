806a2dc104e56650912de4879f686d6b0e868ac7
me-suzy <me.suzana@gmail.com>
2025-08-12 03:34:20 +0300

    Add Content Management/free-content-management/Includes/PortalConection.php

---
+++ Content Management/free-content-management/Includes/PortalConection.php
@@ -0,0 +1,13 @@
+<?php
+define("HOSTM", "localhost");
+define("dbUser", "root");
+define("dbPassword", "");
+define("dbToUse", "CMS");
+define("FileUploadPath", "../HTMLFiles/");
+define("FileUploadPathRelative", "HTMLFiles/");
+define("ImageUploadPath", "../Images/");
+define("ImageUploadPathRelative", "Images/");
+define("FromEMail", "test@example.com");
+define("PortalTitle", "Public user's Page Title should go here");
+?>
+
