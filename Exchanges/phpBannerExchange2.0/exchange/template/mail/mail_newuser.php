22d1f8d167c73c1020d00f696e7dc0e14f8d4032
me-suzy <me.suzana@gmail.com>
2025-08-12 14:01:22 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/template/mail/mail_newuser.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/template/mail/mail_newuser.php
@@ -0,0 +1,4 @@
+<?
+$usrsubject = "Welcome to ".$exchangename."!";
+$usrcontent = "You are recieving this email because someone used this email account to sign up for ".$exchangename.".  If you did not sign up for this service or did not request this information, please accept our apologies.\n\nYour login ID is: $login\nYour Password is: $pass\n\nYou may log on to check your stats, get your HTML code, add and remove banners, and other administrative functions at any time by going to:\n   $baseurl\n\nThank you for your interest!\n\n$adminname."; 
+?>
