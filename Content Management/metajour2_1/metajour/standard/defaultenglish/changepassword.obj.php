71682b1a3e8f2971659210e408e85cc5f1f020d8
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:07 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/changepassword.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/changepassword.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342713;
+$_objchilds[] = '_child_changepassword_342714';
+function objfile_342713 () {
+$obj = owNew('document');
+$objdata['name'] = "Change password";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
