a299543edd9d50dc7cfce871cb1dccef3a6f5573
me-suzy <me.suzana@gmail.com>
2025-08-13 01:47:50 +0300

    Add Unsorted/cms master/cms-admin/templates/system-menu.php

---
+++ Unsorted/cms master/cms-admin/templates/system-menu.php
@@ -0,0 +1,5 @@
+<table cellpadding=0 cellspacing=0 border=0><tr><td colspan=3><img src=images/trans.gif width=1 height=10 border=0></td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top><li></td><td><a href="system.php"><?php echo $Lang_system->menu_system_stat ?></a></td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top>&nbsp;</td><td>&nbsp;</td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top><li></td><td><a href="system.php?mode=edit_user"><?php echo $Lang_system->menu_edit_user ?></a></td></tr>
+</table>
