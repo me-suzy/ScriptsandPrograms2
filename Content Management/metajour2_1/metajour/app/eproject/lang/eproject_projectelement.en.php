d515f1b25c0c760cd3129c74672dcf9bbb2469b9
me-suzy <me.suzana@gmail.com>
2025-08-12 06:00:35 +0300

    Add Content Management/metajour2_1/metajour/app/eproject/lang/eproject_projectelement.en.php

---
+++ Content Management/metajour2_1/metajour/app/eproject/lang/eproject_projectelement.en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eProject
+ * @subpackage lang
+ * $Id: eproject_projectelement.en.php,v 1.1 2005/02/16 05:04:16 jan Exp $
+ */
+
+$LANG['name'] = 'Project elements';
+$LANG['label_name'] = 'Department';
+$LANG['label_content'] = 'Description';
+$LANG['label_comment'] = 'Comment';
+$LANG['label_dato1'] = 'Expected finished';
+$LANG['label_dato2'] = 'Finished';
+$LANG['label_messageto'] = 'Message to';
+?>
