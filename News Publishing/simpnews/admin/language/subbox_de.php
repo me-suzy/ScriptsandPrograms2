82e43e52f9fd317d1a7b5bce49e8e340408d407f
me-suzy <me.suzana@gmail.com>
2025-08-12 20:36:51 +0300

    Add News Publishing/simpnews/admin/language/subbox_de.php

---
+++ News Publishing/simpnews/admin/language/subbox_de.php
@@ -0,0 +1,10 @@
+<?php
+/***************************************************************************
+ * (c)2002-2004 Boesch IT-Consulting (info@boesch-it.de)
+ ***************************************************************************/
+$l_sub_email = "E-Mail";
+$l_sub_mailtype = "E-Mailart";
+$l_sub_mailtypes = array("HTML","nur Text");
+$l_sub_subscribe = "bestellen";
+$l_sub_unsubscribe = "abbestellen";
+?>
