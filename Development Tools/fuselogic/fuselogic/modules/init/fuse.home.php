b5282e65dee54e03946ff91a84e5ef1efe90b606
me-suzy <me.suzana@gmail.com>
2025-08-12 09:24:03 +0300

    Add Development Tools/fuselogic/fuselogic/modules/init/fuse.home.php

---
+++ Development Tools/fuselogic/fuselogic/modules/init/fuse.home.php
@@ -0,0 +1,9 @@
+<?php
+//Location(index().'classifieds/category/');
+//echo 'Test';
+//echo phpinfo();
+echo '<table summary="" bgcolor="#ffff66" border="1"><tr><td>';
+echo 'Hi, I am Fuse <b>"'.module().'/'.subModule().'"</b>';
+echo '<br>from __FILE__ = '.__FILE__;
+echo '</td></tr></table>';
+?>
