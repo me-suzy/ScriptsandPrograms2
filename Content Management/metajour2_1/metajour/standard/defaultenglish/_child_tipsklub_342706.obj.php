ef95a1a7c543047f1662a7963ba181366929bc08
me-suzy <me.suzana@gmail.com>
2025-08-12 07:15:17 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_tipsklub_342706.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_tipsklub_342706.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342706;
+function objfile_342706 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Tipsklub";
+$objdata['subname'] = "Jan H. Andersen";
+$objdata['extension'] = "";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "Vi er nogle der vil oprette en tipsklus - nogen interesserede ";
+$obj->createObject($objdata);
+$obj->moveTo(6);
+return $obj;
+}
+?>
