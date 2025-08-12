56227665afccc6fd27d368923ebde1becb1f46e0
me-suzy <me.suzana@gmail.com>
2025-08-12 05:55:03 +0300

    Add Content Management/metajour2_1/metajour/basic_view_access.php

---
+++ Content Management/metajour2_1/metajour/basic_view_access.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('basic_view.php');
+require_once('basic_field.php');
+
+class basic_view_access extends basic_view {
+
+	function loadLanguage() {
+		parent::loadLanguage();
+		$this->loadLangFile('basic_view_access');
+	}
+	
+	function view() {
+		$obj = owRead($this->objectid[0]);
+		$this->context->clearall(); #TODO
+		?>
+		<html>
+		<head>
+		<title><?php echo $this->gl('text_title') ?></title>
+		<style type="text/css">
+			body { background-color: buttonface; font-family: Tahoma; font-size: 8pt; }
+			select { font-family: Tahoma; font-size: 8pt; }
+			input { font-family: Tahoma; font-size: 8pt; }
+			td { font-family: Tahoma; font-size: 8pt; }
+		</style>
+		<script language="JavaScript" FOR="CANCEL" event="onclick">
+			window.close();
+		</script>
+		<script language="JavaScript" FOR="OK" event="onclick">
+			myForm.submit();
+		</script>
+		</head>
+		<body>
+		<table style="margin: 10px">
+		<tr>
+		<td valign="top">
+	    	<form name="myForm"  method="post">
+	    	<?php echo $this->returnviewpost('access'); ?>
+			<table border=0 width="100%" style="border: 1px solid #cccccc">
+				<tr>
+				<td><strong><?php echo $this->gl('text_1') ?></strong></td>
+				</tr>
+				<tr>
+				<td>		
+				<?php
+				$field = new basic_field($this);
+				echo $field->webAccessSelection($this->objectid[0],'__webaccess__');
+				?>
+				</td>
+				</tr>
+				<tr>
+				<td>		
+				<br>
+				</td>
+				</tr>
+				<tr>
+				<td><strong><?php echo $this->gl('text_2') ?></strong></td>
+				</tr>
+				<tr>
+				<td>		
+				<?php
+				$field = new basic_field($this);
+				echo $field->sysAccessSelection($this->objectid[0],'__sysaccess__');
+				?>
+				</td>
+				</tr>
+				<tr>
+				<td><i><?php echo $this->gl('text_3') ?></i></td>
+				</tr>
+			</table>
+	      	<input type="hidden" name="objectid" value="<?php echo $this->objectidstring($this->objectid) ?>">
+	      	<input type="hidden" name="cmd" value="access">
+	    	</form>
+		</td>
+		<td valign="top">
+			<table cellpadding=2>
+				<tr>
+				<td><input type="button" id="OK" value="<?php echo $this->gl('button_ok') ?>" style="width: 80px; height: 22px" default></td>
+				</tr>
+				<tr>
+				<td><input type="button" id="CANCEL" value="<?php echo $this->gl('button_cancel') ?>" style="width: 80px; height: 22px"></td>
+				</tr>
+			</table>
+		</td>
+		</tr>
+		</table>
+		</body>
+		</html>
+		<?php
+	}
+}
+
+?>
