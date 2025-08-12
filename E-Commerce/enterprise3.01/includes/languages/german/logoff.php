fcd4c26995e253b155ce87513618a3c9f299d263
me-suzy <me.suzana@gmail.com>
2025-08-12 12:00:28 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/logoff.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/logoff.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Abmeldung');
+define('NAVBAR_TITLE', 'Abmeldung');
+define('TEXT_MAIN', 'Sie wurden soeben sicher abgemeldet, so dass niemand mehr Ihre Daten einsehen kann.<br><br>Ihr Warenkorb wurde zwischengespeichert und steht Ihnen nach der n&auml;chsten Anmeldung wieder zur Verf&uuml;gung.');
+?>
