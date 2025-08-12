b43adcfa3a7c216815e0f76d798009836554f1b4
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:33 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/affiliate_signup_ok.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/affiliate_signup_ok.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Partnerprogramm');
+define('HEADING_TITLE', 'Partnerkonto mit Erfolg er&ouml;ffnet!');
+define('TEXT_ACCOUNT_CREATED', 'Vielen Dank! Ihr neues Partner Konto wurde erfolgreich er&ouml;ffnet!<br><br>Eine Best&auml;tigung &uuml;ber Ihr neues Konto wird Ihnen zugesendet. Falls Sie dieses eMail nicht innerhalb einer Stunde erhalten, <a href="' . escs_href_link(FILENAME_AFFILIATE_CONTACT) . '">kontaktieren Sie uns</a> bitte.<br><br>Wenn Sie Fragen haben, <a href="' . escs_href_link(FILENAME_AFFILIATE_CONTACT) . '">kontaktieren Sie uns</a> bitte.');
+?>
