350e6dd4f6ba97df1299785f95322361faacd5ed
me-suzy <me.suzana@gmail.com>
2025-08-12 12:03:38 +0300

    Add E-Commerce/enterprise3.01/install/index.php

---
+++ E-Commerce/enterprise3.01/install/index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  require('includes/application.php');
+
+  $page_file = 'index.php';
+  $page_title = 'Welcome';
+  $page_contents = 'index.php';
+
+  require('templates/main_page.php');
+?>
