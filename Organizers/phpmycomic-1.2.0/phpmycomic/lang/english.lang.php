ddca0a760d14889575f9772c570540ba905f1938
me-suzy <me.suzana@gmail.com>
2025-08-12 21:32:17 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english.lang.php
@@ -0,0 +1,18 @@
+<?php
+
+//////////////////////////////////////////////////////////////
+//
+//     Language:      Norwegian
+//     Version:       1.2.0
+//     Made by:       Endre Myrvang
+//     EMail          bstar@dustrium.net
+//
+//////////////////////////////////////////////////////////////
+
+$langname     = "English";
+
+$langauthor   = "Endre Myrvang";
+
+$langmail	  = "bstar@dustrium.net";
+
+?>
