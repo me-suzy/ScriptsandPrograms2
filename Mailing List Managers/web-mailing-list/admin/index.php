dbc305b401e67524d57f0062446b8ed3777e46e9
me-suzy <me.suzana@gmail.com>
2025-08-12 19:33:12 +0300

    Add Mailing List Managers/web-mailing-list/admin/index.php

---
+++ Mailing List Managers/web-mailing-list/admin/index.php
@@ -0,0 +1,11 @@
+<?php
+include("connect.php");
+?>
+<html>
+<head><title>Admin Panel</title></head>
+<body>
+<a href="addnews.php">Add News</a><br />
+<a href="show.php">Show News</a><br />
+<a href="viewusers.php">View Users</a><br />
+</body>
+</html>
