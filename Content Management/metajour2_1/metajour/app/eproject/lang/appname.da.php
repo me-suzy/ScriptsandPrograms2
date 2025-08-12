7914e6dd5e595be2304b4584a05277723468783b
me-suzy <me.suzana@gmail.com>
2025-08-12 06:00:18 +0300

    Add Content Management/metajour2_1/metajour/app/eproject/lang/appname.da.php

---
+++ Content Management/metajour2_1/metajour/app/eproject/lang/appname.da.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eProject
+ * @subpackage lang
+ * $Id: appname.da.php,v 1.1 2005/01/27 10:13:58 jan Exp $
+ */
+
+$LANG['appname'] = 'IPW eProjekt';
+?>
