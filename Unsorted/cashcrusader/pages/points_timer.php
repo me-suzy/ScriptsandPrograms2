a44eb4fc600cbd7bd8bd5e159ca603fe8d3481c1
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:17 +0300

    Add Unsorted/cashcrusader/pages/points_timer.php

---
+++ Unsorted/cashcrusader/pages/points_timer.php
@@ -0,0 +1,12 @@
+<? include("setup.php");?>
+<html><body><center>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<tr align="center"> 
+<td width="40%"><font size=2 face=arial> Thank you for supporting our sponsors!<br>You will receive <? echo $VA;?> point(s) once you have<br>visited this sponsor's site for at least <? echo $TI;?> seconds</font></td>
+<td width="60%"> 
+<? getad('Main');?>
+</td>
+</tr>
+</table>
+</center>
+</body></html>
