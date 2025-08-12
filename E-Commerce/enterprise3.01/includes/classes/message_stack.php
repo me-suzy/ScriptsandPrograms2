e4db1fef1c5adcbe570d9ce63515118b2609512b
me-suzy <me.suzana@gmail.com>
2025-08-12 11:48:54 +0300

    Add E-Commerce/enterprise3.01/includes/classes/message_stack.php

---
+++ E-Commerce/enterprise3.01/includes/classes/message_stack.php
@@ -0,0 +1,88 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+
+  Example usage:
+
+  $messageStack = new messageStack();
+  $messageStack->add('general', 'Error: Error 1', 'error');
+  $messageStack->add('general', 'Error: Error 2', 'warning');
+  if ($messageStack->size('general') > 0) echo $messageStack->output('general');
+*/
+
+  class messageStack extends tableBox {
+
+// class constructor
+    function messageStack() {
+      global $messageToStack;
+
+      $this->messages = array();
+
+      if (escs_session_is_registered('messageToStack')) {
+        for ($i=0, $n=sizeof($messageToStack); $i<$n; $i++) {
+          $this->add($messageToStack[$i]['class'], $messageToStack[$i]['text'], $messageToStack[$i]['type']);
+        }
+        escs_session_unregister('messageToStack');
+      }
+    }
+
+// class methods
+    function add($class, $message, $type = 'error') {
+      if ($type == 'error') {
+        $this->messages[] = array('params' => 'class="messageStackError"', 'class' => $class, 'text' => escs_image(DIR_WS_ICONS . 'error.gif', ICON_ERROR) . '&nbsp;' . $message);
+      } elseif ($type == 'warning') {
+        $this->messages[] = array('params' => 'class="messageStackWarning"', 'class' => $class, 'text' => escs_image(DIR_WS_ICONS . 'warning.gif', ICON_WARNING) . '&nbsp;' . $message);
+      } elseif ($type == 'success') {
+        $this->messages[] = array('params' => 'class="messageStackSuccess"', 'class' => $class, 'text' => escs_image(DIR_WS_ICONS . 'success.gif', ICON_SUCCESS) . '&nbsp;' . $message);
+      } else {
+        $this->messages[] = array('params' => 'class="messageStackError"', 'class' => $class, 'text' => $message);
+      }
+    }
+
+    function add_session($class, $message, $type = 'error') {
+      global $messageToStack;
+
+      if (!escs_session_is_registered('messageToStack')) {
+        escs_session_register('messageToStack');
+        $messageToStack = array();
+      }
+
+      $messageToStack[] = array('class' => $class, 'text' => $message, 'type' => $type);
+    }
+
+    function reset() {
+      $this->messages = array();
+    }
+
+    function output($class) {
+      $this->table_data_parameters = 'class="messageBox"';
+
+      $output = array();
+      for ($i=0, $n=sizeof($this->messages); $i<$n; $i++) {
+        if ($this->messages[$i]['class'] == $class) {
+          $output[] = $this->messages[$i];
+        }
+      }
+
+      return $this->tableBox($output);
+    }
+
+    function size($class) {
+      $count = 0;
+
+      for ($i=0, $n=sizeof($this->messages); $i<$n; $i++) {
+        if ($this->messages[$i]['class'] == $class) {
+          $count++;
+        }
+      }
+
+      return $count;
+    }
+  }
+?>
