b64f15e156e95853241533ff505e662479a2d40f
me-suzy <me.suzana@gmail.com>
2025-08-12 03:40:00 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/excelexport/install.sql

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/excelexport/install.sql
@@ -0,0 +1,7 @@
+INSERT INTO `functions` VALUES ('excelexport', 'access', 3, NULL, NULL, NULL);
+
+INSERT INTO `userprivileges` VALUES (200, 'administrator', 'excelexport', 'Y', 'X', 'X', 'X', 'X');
+INSERT INTO `userprivileges` VALUES (201, 'probationer', 'excelexport', '', 'X', 'X', 'X', 'X');
+INSERT INTO `userprivileges` VALUES (202, 'contributor', 'excelexport', '', 'X', 'X', 'X', 'X');
+INSERT INTO `userprivileges` VALUES (203, 'translator', 'excelexport', '', 'X', 'X', 'X', 'X');
+INSERT INTO `userprivileges` VALUES (204, 'member', 'excelexport', '', 'X', 'X', 'X', 'X');
