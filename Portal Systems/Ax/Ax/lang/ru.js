d941c6b1586967552a2a1a0fbaa44cb50d994566
me-suzy <me.suzana@gmail.com>
2025-08-12 22:03:08 +0300

    Add Portal Systems/Ax/Ax/lang/ru.js

---
+++ Portal Systems/Ax/Ax/lang/ru.js
@@ -0,0 +1,36 @@
+// I18N constants
+
+HTMLArea.I18N = {
+
+	// the following should be the filename without .js extension
+	// it will be used for automatically load plugin language.
+	lang: "ru",
+
+	tooltips: {
+		bold:           "Æèðíûé",
+		italic:         "Íàêëîííûé",
+		underline:      "Ïîä÷åðêíóòûé",
+		strikethrough:  "Ïåðå÷åðêíóòûé",
+		subscript:      "Íèæíèé èíäåêñ",
+		superscript:    "Âåðõíèé èíäåêñ",
+		justifyleft:    "Âûðàâíèâàíèå ïî ëåâîìó êðàþ",
+		justifycenter:  "Âûðàâíèâàíèå ïî öåíòðó",
+		justifyright:   "Âûðàâíèâàíèå ïî ïðàâîìó êðàþ",
+		justifyfull:    "Ðàñòÿíóòûé òåêñò",
+		orderedlist:    "Íóìåðîâàííûé ñïèñîê",
+		unorderedlist:  "Ìàðêèðîâàííûé ñïèñîê",
+		outdent:        "Ñäâèã â ëåâî",
+		indent:         "Ñäâèã â ïðàâî",
+		forecolor:      "Öâåò øðèôòà",
+		backcolor:      "Öâåò ôîíà",
+		horizontalrule: "Ãîðèçîíòàëüíàÿ ëèíèÿ",
+		createlink:     "Âñòàâèòü ññûëêó",
+		insertimage:    "Âñòàâèòü êàðòèíêó",
+		inserttable:    "Âñòàâèòü òàáëèöó",
+		htmlmode:       "Âèäåòü HTML êîä",
+		popupeditor:    "Óâåëè÷èòü ðåäàêòîð",
+		about:          "Î ðåäàêòîðå",
+		help:           "Ïîìîùü â èñïîëüçîâàíèè",
+		textindicator:  "Äàííûé ñòèëü"
+	}
+};
