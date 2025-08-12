89b81c16a344a719723d9cfc89757d1a3c1ca285
me-suzy <me.suzana@gmail.com>
2025-08-13 01:00:05 +0300

    Add Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/install_finish.php

---
+++ Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/install_finish.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<link rel="stylesheet" href="main.css">
+</head>
+<body>
+<p>Nullified Version &copy WTN Team `2002</p>
+All the tables and data have been successfully installed! Please delete the /install/ subdirectory. You may now install Autolinks on your site(s) AutoLinks by going to the <a href="../index.php">control panel</a> and click on the 'Add New' link. We also invite you to navigate through the control panel to change the default settings, add new categories, add new tags and many other things.
+</body>
+</html>
