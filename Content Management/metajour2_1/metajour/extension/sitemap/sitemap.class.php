73b9c14040a68b238f8fc6fc96f756be402bb37b
me-suzy <me.suzana@gmail.com>
2025-08-12 06:15:38 +0300

    Add Content Management/metajour2_1/metajour/extension/sitemap/sitemap.class.php

---
+++ Content Management/metajour2_1/metajour/extension/sitemap/sitemap.class.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+require_once($system_path . "extension/basicextension.class.php");
+
+class ext_sitemap extends basicextension {
+
+	function ext_sitemap() {
+		$this->basicextension();
+		$this->extname = 'sitemap';
+		$this->addextparam('templatename');
+		$this->addextparam('templateid');
+		$this->addextparam('structureid');
+	}
+
+	function _do() {
+		$this->useTemplate('templatename','templateid','standard_sitemap');
+	}
+
+}
+?>
