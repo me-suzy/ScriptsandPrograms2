1dee21c7b4d6c6924c7629e4c7d45465c46f0d9f
me-suzy <me.suzana@gmail.com>
2025-08-12 14:16:11 +0300

    Add File Manipulation/dl4lite/UPLOAD/lang/1/lang_ad_lang.php

---
+++ File Manipulation/dl4lite/UPLOAD/lang/1/lang_ad_lang.php
@@ -0,0 +1,36 @@
+<?php
+/*********************************************************
+* Name: lang_ad_skins.php
+* Author: RW::Download 4.0
+* Contact: realworld@blazefans.com
+* Description: Language variables
+* Version: 4.00
+*********************************************************/
+$lang = array(
+
+'langupdated' => "Language file successfully updated",
+'no_lang_directory' => "Could not create new language directory. Please make sure that the \"lang\" folder in your script root is set with write permissions [0666/0777]",
+'invalid_lang' => "The seleced language pack could not be found",
+'lang_created' => "New language pack created",
+'import_successful' => "Import successful",
+'nodatafile' => "Unable to write/create the language data file for exporting this language pack. Please ensure the language directory and all subdirectories are world writeable [0666/0777]",
+'no_lang_files' => "Unable to save/create language files. Please make sure the following file and all other files in this directory are writable [chmod 0666/0777]",
+'langsetmissing' => "Lang set could not be found.",
+'skin_tag' => "Skin Tag",
+'save_changes' => "Save Changes",
+'langpack' => "Language Pack",
+'langname' => "Language Name",
+'langdeleted' => "Language set successfully removed from script",
+'langdelfail' => "Failed to remove language files from server. They may have already been removed or the folder may not have the correct permissions",
+'select_lang' => "Select Language",
+'export_complete' => "Language pack exported to /archive_out/",
+'click_download' => "Click to download now",
+'lang_import' => "Select language to import",
+'no_imports' => "No langauge sets were found in the /archive_in/ folder",
+'baseskin' => "Base language on",
+'content' => "Content",
+'edit_details' => "Edit Details",
+'details_saved' => "Details have been saved",
+)
+
+?>
