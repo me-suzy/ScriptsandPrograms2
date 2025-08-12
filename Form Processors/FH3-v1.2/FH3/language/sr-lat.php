19eba743d449c1396ba507b5ddaa57149dc41458
me-suzy <me.suzana@gmail.com>
2025-08-12 16:10:59 +0300

    Add Form Processors/FH3-v1.2/FH3/language/sr-lat.php

---
+++ Form Processors/FH3-v1.2/FH3/language/sr-lat.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * ?? language file
+ *
+ * @author Isidor Stankov
+ * @package FormHandler
+ */
+
+$fh_lang = array(
+  1  => 'Januar',
+  2  => 'Februar',
+  3  => 'Mart',
+  4  => 'April',
+  5  => 'Maj',
+  6  => 'Jun',
+  7  => 'Jul',
+  8  => 'Avgust',
+  9  => 'Septembar',
+  10 => 'Octobar',
+  11 => 'Novembar',
+  12 => 'Decembar',
+  13 => 'UneÅ¡en datum nije validan!',
+  14 => 'Niste uneli ispravnu vrednost za polje!',
+  15 => 'Lozinke nisu iste!',
+  16 => 'Ova polja su neophodna!',
+  17 => 'Lozinka mora da bude duÅ¾a od %d karaktera!',
+  18 => 'Lozinka sadrÅ¾i nedozvoljene karaktere!',
+  19 => 'Menjajte ovo polje jedino ako Å¾elite da promenite trenutni sadrÅ¾aj(<a href="%path%%filename%" target="_blank">%filename%</a>)<br />',
+  20 => 'Dozvoljene su samo sledeÄe ekstenzije: %s.',
+  21 => 'Fajl koji prenosite veÄ postoji!',
+  22 => 'Morate da prenesete fajl',
+  23 => 'PremaÅ¡ili ste limit veliÄ?ine fajla od %s kb ',
+  24 => 'PokuÅ¡ajte ponovo!',
+  25 => '<small>Menjajte ova polja jedino ako Å¾elite da promenite svoju trenutnu lozinku</small><br />',
+  26 => 'Prosledi',
+  27 => 'ObriÅ¡i',
+  28 => 'OtkaÅ¾i',
+  29 => 'Dostupno',
+  30 => 'Izabrano',
+  31 => 'PreneÅ¡eni fajl je neodgovarajuÄeg tipa!',
+  32 => 'VeliÄ?ina slike moÅ¾e biti %d x %d ili manje. PreneÅ¡ena slika ima dimenzije %d x %d!',
+  33 => 'Forma nije ispravna i fajl <b>%filename%</b> mora biti izabran ponovo ako Å¾elite da ga prosledite ovom formom!<br />',
+  34 => 'Izaberite podatak da bi ga preneli u formu %s, ili dvostruko kliknite da bi obrisali sve podatke',
+  35 => 'Ovo polje mora biti jedinstveno. Vrednost "%s" veÄ postoji u bazi!',
+  36 => '<b>%d</b> characters remaining on your input limit',
+  37 => 'You have to supply a file with an extension!',
+  38 => 'Back',
+  39 => 'The combination of the fields "%s" has to be unique. This combination already exists in the database! (Violation of index "%s")'
+);
+
+?>
