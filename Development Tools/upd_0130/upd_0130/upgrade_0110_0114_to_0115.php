6148e928111dec4fe17c39e585396d81de019fc4
me-suzy <me.suzana@gmail.com>
2025-08-12 10:17:51 +0300

    Add Development Tools/upd_0130/upd_0130/upgrade_0110_0114_to_0115.php

---
+++ Development Tools/upd_0130/upd_0130/upgrade_0110_0114_to_0115.php
@@ -0,0 +1,16 @@
+<?php
+require('../config.php');
+require('./functions.php');
+?>
+<html><body>
+<div align="center"><h3>ProgSys: Upgrade from 0.110-0.114 to 0.115</h3></div>
+<br>
+<?php
+$sql = "ALTER TABLE ".$tableprefix."_layout ";
+$sql.= "add admstorefilter tinyint(1) unsigned default '0'";
+if(!$result = mysql_query($sql, $db))
+	die("Unable to upgrade table ".$tableprefix."_layout");
+?>
+<br><div align="center">Upgrade done.<br>Please remove install.php, upgrade*.php, mkconfig.php and fill_*.php from server</div>
+<div align="center">Now you can login to the <a href="index.php">admininterface</a></div>
+</body></html>
