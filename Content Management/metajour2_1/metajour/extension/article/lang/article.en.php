daac85c377ef3cd4e374b336dc1d3dc52b8968df
me-suzy <me.suzana@gmail.com>
2025-08-12 06:12:40 +0300

    Add Content Management/metajour2_1/metajour/extension/article/lang/article.en.php

---
+++ Content Management/metajour2_1/metajour/extension/article/lang/article.en.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['name'] = 'Configuration of articlelist';
+$LANG['label_name'] = 'Name';
+$LANG['label_title'] = 'Title';
+$LANG['label_categoryid'] = 'Category';
+$LANG['label_sortorder'] = 'Sort';
+$LANG['option_sortorder'] = array('Oldest first', 'Newest first');
+$LANG['label_excerptlength'] = 'Excerptlength';
+$LANG['label_timelimit'] = 'Show days';
+$LANG['label_numlimit'] = 'Show number';
+$LANG['label_showheader'] = 'Show header';
+$LANG['label_showsubheader'] = 'Show subheader';
+$LANG['label_showdate'] = 'Show date';
+$LANG['label_showowner'] = 'Show owner';
+$LANG['label_showexcerpt'] = 'Show excerpt';
+$LANG['label_templateid'] = 'Template';
+?>
