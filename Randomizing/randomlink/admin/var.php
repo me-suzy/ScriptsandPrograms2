dcbf70d4e1d45e19bedabf77b60d9a31a90629a0
me-suzy <me.suzana@gmail.com>
2025-08-12 23:33:37 +0300

    Add Randomizing/randomlink/admin/var.php

---
+++ Randomizing/randomlink/admin/var.php
@@ -0,0 +1,7 @@
+<?PHP
+$submitlink='Yes'; //let user submit links, Yes or No
+$numlinks=5; //how many links do you want it to show
+$titlecolor='#808080'; //title color of the title random five links text
+$barcolor='#246FF8'; //title of site title and rank bar
+$rows='#C0C0C0'; //link table row color
+?>
