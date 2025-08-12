9a1a768c2d431a4c989a4c8645dd6c2635be4897
me-suzy <me.suzana@gmail.com>
2025-08-12 18:47:12 +0300

    Add Interactive Stories/FicHive_v1/skins/1/moderator.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/moderator.tmpl.php
@@ -0,0 +1,10 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%DELETESTORY%></td><td>
+<select name='moddel'><option value=''><%CHOOSESTORY%><%DELETESTORY_VAL%></select></td></tr>
+<tr><td class='fframe' rowspan='2'><%MOVESTORY%></td>
+<td><select name='modmove'><option value=''><%CHOOSESTORY%><%MOVESTORY_VAL%></select></td></tr>
+<tr><td><select name='ncid'><%CATEGORIES%></select></td></tr>
+<tr><td colspan='2' class='frame'><input type='submit' value='<%GO%>' onClick='javascript:return confirm("<%AREYOUSURE%>")'></td></tr>
+<tr><td>
+</table>
+<%APPROVE%>
