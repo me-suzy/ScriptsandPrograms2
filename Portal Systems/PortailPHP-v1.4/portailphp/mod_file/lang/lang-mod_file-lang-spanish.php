e3b34646617a0f9c63bdcce959d52e3370d10556
me-suzy <me.suzana@gmail.com>
2025-08-12 23:16:50 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_file/lang/lang-mod_file-lang-spanish.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_file/lang/lang-mod_file-lang-spanish.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE FILE
+$Mod_File_Taille="2000000";// Taille maximum des fichiers en Upload
+$Mod_File_Etiquette="Fichero que transferar";
+$Mod_File_Etiquette2="Categoría";
+$Mod_File_Etiquette3="Título";
+$Mod_File_Etiquette4="Nombre";
+$Mod_File_Etiquette5="Email";
+$Mod_File_inde_Date="Fecha";
+$Mod_File_inde_Cat="Categorías";
+$Mod_File_inde_Titre="Titulo";
+$Mod_File_inde_Fichiers="Ficheros";
+$Mod_File_inde_Clics="Clics";
+$Mod_File_PostPar="Enviado por";
+$Mod_File_Le="El";
+$Mod_File_Telecharger="Descargar";
+$Mod_File_PlRecent="Las 10 Descargas (las + recientes)";
+$Mod_File_PlLu="Las 10 Descargas (las + clics)";
+$Mod_File_JS_Nom="El campo Nombre tiene que ser rellenado";
+$Mod_File_JS_Email="El campo Email tiene que ser rellenado";
+$Mod_File_JS_Monfichier="El campo Fichero tiene que ser rellenado";
+$Mod_File_JS_Titre="El campo Título tiene que ser rellenado";
+$Mod_File_Upload_OK="El fichero ha sido transferado correctamente";
+
+?>
