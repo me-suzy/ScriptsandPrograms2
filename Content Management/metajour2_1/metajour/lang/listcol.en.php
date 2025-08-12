22a4e14d269c78ef50099262fbc931957e5b66a7
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:24 +0300

    Add Content Management/metajour2_1/metajour/lang/listcol.en.php

---
+++ Content Management/metajour2_1/metajour/lang/listcol.en.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Column setup';
+$LANG['label_name'] = 'Table';
+$LANG['label_pname'] = 'Name';
+$LANG['label_makedefault'] = 'Set as default';
+$LANG['text_1'] = 'Field';
+$LANG['text_2'] = 'Sorting';
+$LANG['text_3'] = 'Column';
+$LANG['text_4'] = 'Delete';
+$LANG['text_5'] = 'Add field';
+
+?>
