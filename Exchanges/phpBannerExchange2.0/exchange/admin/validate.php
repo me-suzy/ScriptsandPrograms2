82990762da33913d625c4018ec291d3dbf580447
me-suzy <me.suzana@gmail.com>
2025-08-12 13:56:58 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/admin/validate.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/admin/validate.php
@@ -0,0 +1,101 @@
+<?
+$file_rev="041305";
+////////////////////////////////////////////////////////
+//                 phpBannerExchange                  //
+//                   by: Darkrose                     //
+//              (darkrose@eschew.net)                 //
+//                                                    //
+// You can redistribute this software under the terms //
+// of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of  //
+// the License, or (at your option) any later         //
+// version.                                           //
+//                                                    //
+// You should have received a copy of the GNU General //
+// Public License along with this program; if not,    //
+// write to the Free Software Foundation, Inc., 59    //
+// Temple Place, Suite 330, Boston, MA 02111-1307 USA //
+//                                                    //
+//     Copyright 2004 by eschew.net Productions.      //
+//   Please keep this copyright information intact.   //
+////////////////////////////////////////////////////////
+
+include("../config.php");
+include("../css.php");
+include("../lang/admin.php");
+require_once('../lib/template_class.php');
+
+if($use_gzhandler==1){
+	ob_start("ob_gzhandler");
+}
+
+// Begin loginstuff
+if(!$db=@mysql_connect("$dbhost","$dbuser","$dbpass")){
+	include("../lang/errors.php");
+	$err="1";
+	$error.="$LANG_error_header<p>";
+	$error.="$LANG_error_mysqlconnect ";
+	$error.=mysql_error();
+}
+
+@mysql_select_db($dbname,$db);
+
+session_start();
+header("Cache-control: private"); //IE 6 Fix 
+$session=session_id(); 
+$login = $_SESSION['login'];
+$pass = $_SESSION['pass'];
+
+$result = mysql_query("select * from banneradmin where adminuser='$login' AND adminpass='$pass'");
+$get_userinfo=mysql_fetch_array($result);
+$login=$get_userinfo[adminuser];
+$pass=$get_userinfo[adminpass];
+
+    if($login=="" AND $pass=="" OR $pass=="" OR $err=="1") {
+		include("../lang/errors.php");
+		$error.="$LANG_error_header<p>";
+		$error.="$LANG_login_error";
+
+		$page = new Page('../template/admin_error.php');
+		$page->replace_tags(array(
+			'css' => "$css",
+			'session' => "$session",
+			'baseurl' => "$baseurl",
+			'title' => "$exchangename - $LANG_login_error_title",
+			'shorttitle' => "$LANG_login_error_title",
+			'error' => "$error",
+			'menu' => "$menu",
+			'footer' => '../footer.php'));
+		$page->output();
+	session_destroy();
+
+	}else{ 
+		$pending = mysql_query("select banneruser.id, banneruser.login from banneruser left join bannerstats on banneruser.id=bannerstats.uid where approved='0' order by login asc");
+		$num=mysql_num_rows($pending);
+		if($num > 0){
+			while ($get_rows=@mysql_fetch_array($pending)){	
+				$total_found=mysql_num_rows($pending);	
+				$found=1;	
+				$uid=$get_rows[id];	
+				$ulogin=$get_rows[login];
+				$html.="<b><a href=\"edit.php?SID=$session&uid=$uid\">$ulogin</a></b><br>";	
+				}		
+				$num= "<p>$num $LANG_val_awaiting";	
+			}else{
+					$num= "$LANG_val_noaccts";	
+		}
+		$page = new Page('../template/admin_validate.php');
+		$page->replace_tags(array(
+			'css' => "$css",
+			'session' => "$session",
+			'baseurl' => "$baseurl",
+			'title' => "$exchangename - $LANG_menu_valacct",
+			'shorttitle' => "$LANG_menu_valacct",
+			'msg' => "$LANG_val_instructions",
+			'html' => "$html",
+			'num' => "$num",
+			'menu' => 'admin_menuing.php',
+			'footer' => '../footer.php'));
+		$page->output();
+	}
+	?>
