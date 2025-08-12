5ee73694b3dee4c2b4d879522e654f09b824ff63
me-suzy <me.suzana@gmail.com>
2025-08-12 21:08:36 +0300

    Add News Publishing/zomplog34/zomplog34/language/italian.php

---
+++ News Publishing/zomplog34/zomplog34/language/italian.php
@@ -0,0 +1,356 @@
+<?
+
+
+
+// Questa Ë la versione in italiano
+
+
+
+//----------------- Italiano -----------------------//
+
+
+
+// Front end -> get.php
+
+$lang_number_comments = "commenti"; //number of comments under posts
+
+$lang_no_results = "nessun risultato trovato.";
+
+$lang_read_more = "continua...";
+
+$lang_listen = "ascoltare il mp3...";
+
+$lang_view = "vedere filmato...";
+
+$lang_searched_for = "Hai cercato";
+
+$lang_posts_by = "Articoli da";
+
+
+
+// Front end -> sidemenu.php
+
+$lang_search = "Cerca";
+
+$lang_pages = "Pagine";
+
+$lang_recent_posts = "Ultimi articoli";
+
+$lang_categories = "Categorie";
+
+$lang_authors = "Autori";
+
+$lang_rss_feed = "Rss feed";
+
+$lang_admin = "Administrator";
+
+$lang_login = "Log in";
+
+$lang_register = "Iscriviti";
+
+$lang_powered = "Powered by";
+
+$lang_archive = "Archive";
+
+
+
+// Front end -> detail.php
+
+$lang_write_comment = "Scrivere un commento";
+
+$lang_name = "nome";
+
+$lang_comment = "commento";
+
+$lang_comments = "Commenti";
+
+$lang_no_comments_found = "nessun commento trovato";
+
+$lang_on = "dal"; // as in "comment posted on date"
+
+$lang_error_name = "Non hai riempito il campo nome";
+
+$lang_error_comment = "Non hai scritto un commento";
+
+$lang_error_comment_success = "Grazie. Il tuo commento è stato aggiunto.";
+
+
+
+// Front end -> page.php
+
+
+
+$lang_contact = "Contattare";
+
+$lang_your_email_sent = "Il tuo messaggio è stato spedito!";
+
+$lang_name_mail = "Nome";
+
+$lang_email_mail = "Email";
+
+$lang_additional_remarks = "Altri commenti";
+
+
+
+// Back end -> menu.php
+
+$lang_entries = "Articoli";
+
+$lang_newentry = "nuovo articolo";
+
+$lang_edit_delete = "modificare / eliminare articoli";
+
+$lang_profile = "Profilo Personale";
+
+$lang_editprofile = "modificare il profilo personale";
+
+$lang_controlpanel = "Pannello di Controllo Admin";
+
+$lang_settings = "settaggi del sistema";
+
+$lang_moblog_settings = "settaggi moblog";
+
+$lang_content_management = "gestione contenuti";
+
+$lang_manage_users = "gestione utenti";
+
+$lang_manage_categories = "gestione categorie";
+
+$lang_manage_comments = "gestione commenti";
+
+$lang_pages = "Pagine";
+
+$lang_newpage = "nuova pagina";
+
+$lang_edit_delete_page = "modificare / eliminare pagine";
+
+$lang_upload_images = "upload immagini";
+
+$lang_custom_upload_tool = "controllo upload customizzato";
+
+
+// Back end -> members.php
+
+$lang_total_number_posts = "Numero totale di articoli";
+
+$lang_total_number_pages = "Numero totale di pagine";
+
+$lang_total_number_users = "Numero totale di utenti";
+
+$lang_total_number_categories = "Numero totali di categorie";
+
+$lang_total_number_comments = "Numero totali di commenti";
+
+$lang_lastpost = "Il tuo ultimo articolo era";
+
+$lang_site_statistics = "Statistiche del sito";
+
+$lang_your_statistics = "Statistiche personale";
+
+
+
+// Back end -> header.php
+
+
+
+$lang_members_area = "dashboard";
+
+$lang_view_site = "vai al sito";
+
+$lang_logout = "log out";
+
+
+
+// Back end -> entries (newentry.php, editor.php, entry.php)
+
+
+
+$lang_title = "Titolo";
+
+$lang_text = "Testo";
+
+$lang_extended = "Esteso (facoltativa)";
+
+$lang_upload_image = "Upload imagine";
+
+$lang_full_width = "Mostrare largezza intera";
+
+$lang_category = "Categoria";
+
+$lang_choosemaincat = "Scegliere una categoria";
+
+$lang_replace_image = "Sostituire imagine";
+
+$lang_media = "URL del file media";
+
+$lang_choose_type = "Scegliere un tipo di media";
+
+$lang_entries_by_others = "Articoli da altre persone";
+
+$lang_your_entries = "I tuoi articoli";
+
+$lang_order_by = "Ordinare per";
+
+$lang_date = "data";
+
+$lang_number_of_images = "Number of images:";
+
+
+// Back end -> edit/delete
+
+$lang_edit = "modificare";
+
+$lang_delete = "eliminare";
+
+
+
+// Back end -> User Management (profile.php, users.php)
+
+$lang_username = "nome utente";
+
+$lang_password = "password";
+
+$lang_retype_password = "riscrivere password";
+
+$lang_email = "email";
+
+$lang_use_email_moblog = "usare questo indirizzo email per il moblogging";
+
+$lang_what_is_moblogging = "Che cos'&egrave; moblogging?";
+
+$lang_about_me = "chi sono";
+
+$lang_administrator = "administrator";
+
+$lang_user = "utente";
+
+$lang_create_user = "creare un nuovo utente";
+
+$lang_current_users = "utente attuale";
+
+
+
+// Back end -> Settings (settings.php)
+
+$lang_system_settings = "Settaggi del Sistema";
+
+$lang_main_config = "Configurazioni principali";
+
+$lang_weblog_title = "titolo del weblog";
+
+$lang_comments_on = "commenti attivati";
+
+$lang_categories_on = "categorie attivate";
+
+
+
+$lang_mediafile_on = "mediafile attivati";
+
+$lang_activate_site_parts = "Attivare le parti del sito";
+
+$lang_admin_welcome_message = "Messaggio di benvenuto pannello di controllo";
+
+$lang_site_welcome_message = "Messaggio di benvenuto al sito";
+
+$lang_messages = "Messaggi di benvenuto ";
+
+$lang_dashboard_rss_feed = "URL del RSS feed per il dashboard";
+
+$lang_allow_register = "tutti si possono iscrivere";
+
+
+
+$lang_choose_language = "scegliere una lingua";
+
+$lang_choose_skin = "scegliere uno skin";
+
+$lang_posts_per_page = "articoli per pagina";
+
+$lang_scroll = "scroll";
+
+$lang_scroll_explained = "(il numero di pagine nel precedente-prossima parte)";
+
+$lang_upload_on = "upload attivato";
+
+$lang_max_upload = "dimensione max upload";
+
+$lang_pages_admin = "chi può creare/modificare pagine?";
+
+$lang_pages_admin_only = "solo admin";
+
+$lang_date_time = "Data/Ora";
+
+$lang_date_time_format = "formato data/ora";
+
+
+$lang_thumbnail_width = "largezza thumbnail";
+$lang_thumbnail_fullwidth = "largezza massima";
+
+
+// Back end -> category.php
+
+$lang_new_category = "Creare una nuova categoria";
+
+$lang_category_name = "nome della categoria";
+
+$lang_edit_category = "modificare la categoria";
+
+$lang_choose_category = "scegliere una categoria";
+
+$lang_edit_category_name = "modificare il nome della categoria";
+
+$lang_delete_category = "Eliminare la categoria";
+
+
+
+// Back end -> comments_editor.php
+
+$lang_edit_comment = "Modificare commento";
+
+$lang_comment_name = "nome";
+
+$lang_comment = "commento";
+
+$lang_ban = "ban";
+
+
+
+// Back end -> newpage.php
+
+$lang_add_contact_form = "aggiungere modulo contact?";
+
+$lang_form_email = "modulo email";
+
+
+
+//errors, well actually the messages if an action was succesful...
+
+$lang_error1 = "L'articolo &egrave; stato aggiunto con successo!";
+
+$lang_error2 = "L'articolo &egrave; stato aggiornato con successo!";
+
+$lang_error3 = "La data &egrave; stato eliminato con successo.";
+
+$lang_error4 = "Il profilo utente &egrave; stato aggiornato con successo!";
+
+$lang_error5 = "I settaggi sono stati aggiornati con successo!";
+
+$lang_error6 = "La categoria &egrave; stata aggiunta con successo!";
+
+$lang_error7 = "La categoria &egrave; stata modificata con successo!";
+
+$lang_error8 = "Il commento &egrave; stato modificato con successso!";
+
+$lang_error9 = "L'utente &egrave; stato creato!";
+
+$lang_error10 = "La pagina &egrave; stata aggiunta con successo!";
+
+$lang_error11 = "La pagina &egrave; stata aggiornata con successo!";
+
+$lang_error12 = "L'Ip-address &egrave; stato bloccato!";
+
+$lang_error13 = "L'Ip-address &egrave; stato abilitato!";
+
+
+
+?>
