7d45db2bfabddb4fe03a10cfdb2f8e81e85442c8
me-suzy <me.suzana@gmail.com>
2025-08-12 10:27:59 +0300

    Add Discussion Boards/board2/ukiboard/function/menuroot.php

---
+++ Discussion Boards/board2/ukiboard/function/menuroot.php
@@ -0,0 +1,6 @@
+<TABLE cellspacing="5" cellpadding="0">
+<TR>
+<TD><TABLE border="2" cellspacing="0" cellpadding="5">
+<TR><TD width="70" class="vivo" align="center" onMouseOver="MenuOver(this);" onMouseOut="MenuOut(this);"><A HREF="pages/search.php" class="seed"><?php echo $lang[Search]; ?></A>
+</TABLE>
+</TABLE>
