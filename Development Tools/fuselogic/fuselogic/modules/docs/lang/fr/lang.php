c1ec7f202aa6581c1182d1368fa472695f7e250d
me-suzy <me.suzana@gmail.com>
2025-08-12 09:20:16 +0300

    Add Development Tools/fuselogic/fuselogic/modules/docs/lang/fr/lang.php

---
+++ Development Tools/fuselogic/fuselogic/modules/docs/lang/fr/lang.php
@@ -0,0 +1,73 @@
+<?
+$lang['encoding']   = 'iso-8859-15';
+$lang['locales']    = array(
+                        'fra',
+                        'fr_FR@euro',
+                        'fr_FR',
+                        'fr',
+                        'french',
+                      );
+
+$lang['btn_edit']   = 'Editer cette page';
+$lang['btn_source'] = 'Montrer la page originale';
+$lang['btn_show']   = 'Montrer la page';
+$lang['btn_create'] = 'Créer cette page';
+$lang['btn_search'] = 'Recherche';
+$lang['btn_save']   = 'Sauver';
+$lang['btn_preview']= 'Apercu';
+$lang['btn_top']    = 'Haut de page';
+$lang['btn_revs']   = 'Anciennes révisions';
+$lang['btn_recent'] = 'Derniers changements';
+$lang['btn_upload'] = 'Envoyer';
+$lang['btn_cancel'] = 'Annuler';
+$lang['btn_index']  = 'Index';
+$lang['btn_secedit']= 'Editer';
+
+$lang['txt_upload'] = 'Sélectionnez un fichier à envoyer';
+$lang['txt_filename'] = 'Donnez un "wikiname" (optionnel)';
+$lang['lockedby']     = 'Actuellement bloqué par';
+$lang['lockexpire']   = 'Blocage expire à';
+$lang['willexpire']   = 'Votre blocage pour modifier cette page expire dans une minute.\nPour éviter les conflits, utiliser le bouton Apercu pour réinitialiser le minuteur.';
+
+$lang['notsavedyet'] = 'Les changements non enregistrés seront perdus.\nVoulez-vous vraiment continuer?';
+$lang['rssfailed']   = 'Une erreur s\'est produite en cherchant ce champs: ';
+$lang['nothingfound']= 'Pas de réponse.';
+
+$lang['mediaselect'] = 'Sélection de fichier';
+$lang['fileupload']  = 'Envoi de fichier';
+$lang['uploadsucc']  = 'Envoi réussi.';
+$lang['uploadfail']  = 'L\'envoi n\'a pas réussi. Veuillez contacter l\'administrateur';
+
+$lang['hits']       = 'Occurrences retrouvées';
+$lang['quickhits']  = 'Pages retrouvées';
+$lang['toc']        = 'Table des Matières';
+$lang['current']    = 'version actuelle';
+$lang['yours']      = 'votre version';
+$lang['diff']       = 'différences avec la version actuelle';
+$lang['line']       = 'Ligne';
+$lang['breadcrumb'] = 'Piste';
+$lang['lastmod']    = 'Dernier changement';
+
+$lang['mail_newpage'] = '[DokuWiki] page ajoutée:';
+$lang['mail_changed'] = '[DokuWiki] page changée:';
+
+$lang['nosmblinks'] = 'Les liens vers les disques partagés de Windows ne fonctionnent qu\'avec Microsoft Internet Explorer.\nVou pouvez toujours faire un copier/coller du lien.';
+
+$lang['qb_alert']   = 'SVP, entrer le texte que vous souhaiter formater.\nIl sera ajouté à la fin du document.';
+$lang['qb_bold']    = 'Gras';
+$lang['qb_italic']  = 'Italic';
+$lang['qb_underl']  = 'Souligné';
+$lang['qb_code']    = 'Code';
+$lang['qb_h1']      = 'En-tête 1';
+$lang['qb_h2']      = 'En-tête 2';
+$lang['qb_h3']      = 'En-tête 3';
+$lang['qb_h4']      = 'En-tête 4';
+$lang['qb_h5']      = 'En-tête 5';
+$lang['qb_link']    = 'Lien interne';
+$lang['qb_extlink'] = 'Lien externe';
+$lang['qb_hr']      = 'Ligne horizontale';
+$lang['qb_ol']      = 'Liste numérotée';
+$lang['qb_ul']      = 'Liste libre';
+$lang['qb_media']   = 'Ajouter des images et autres fichiers';
+
+?>
