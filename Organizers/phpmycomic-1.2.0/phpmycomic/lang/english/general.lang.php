341d164073e3f57f57fa2629e42cde8cdf10f813
me-suzy <me.suzana@gmail.com>
2025-08-12 21:33:19 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/general.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/general.lang.php
@@ -0,0 +1,49 @@
+<?php
+
+// Main Menu
+$tpl->assign("lang_menu_index", "Index");
+$tpl->assign("lang_menu_search", "Search");
+$tpl->assign("lang_menu_add", "Add");
+$tpl->assign("lang_menu_browse", "Browse");
+$tpl->assign("lang_menu_addcomic", "Add Comic");
+$tpl->assign("lang_menu_addartist", "Add Artist");
+$tpl->assign("lang_menu_addmulti", "Multiple Comics");
+$tpl->assign("lang_menu_tools", "Tools");
+$tpl->assign("lang_menu_login", "Login");
+$tpl->assign("lang_menu_logout", "Logout");
+$tpl->assign("lang_menu_about", "About");
+$tpl->assign("lang_menu_upload", "Upload Image");
+$tpl->assign("lang_menu_import", "Import Comic");
+$tpl->assign("lang_menu_checklist", "Issue Checklist");
+$tpl->assign("lang_menu_creport", "Comic Report");
+
+// Browse Menu
+$tpl->assign("lang_menu_series", "Series");
+$tpl->assign("lang_menu_writer", "Writers");
+$tpl->assign("lang_menu_inker", "Inkers");
+$tpl->assign("lang_menu_publisher", "Publishers");
+$tpl->assign("lang_menu_genre", "Genres");
+$tpl->assign("lang_menu_penciler", "Pencilers");
+$tpl->assign("lang_menu_colorist", "Colorists");
+$tpl->assign("lang_menu_letterer", "Letterers");
+$tpl->assign("lang_menu_cover", "Cover Artists");
+
+// Buttons
+$tpl->assign("lang_button_upload", "Upload");
+$tpl->assign("lang_button_import", "Import");
+$tpl->assign("lang_button_search", "Search");
+$tpl->assign("lang_button_submit", "Submit");
+$tpl->assign("lang_button_ok", "OK");
+$tpl->assign("lang_button_reset", "Reset");
+$tpl->assign("lang_button_calender", "Calender");
+$tpl->assign("lang_button_login", "Login");
+$tpl->assign("lang_button_cancel", "Cancel");
+$tpl->assign("lang_button_export", "Export");
+$tpl->assign("lang_button_delete", "Delete");
+$tpl->assign("lang_button_checkall", "Check All");
+$tpl->assign("lang_button_checknone", "Uncheck All");
+
+// Other Defaults
+$tpl->assign("lang_option", "Option");
+
+?>
