cba826070c3645dab47a8d1ff819af5e800b3a0c
me-suzy <me.suzana@gmail.com>
2025-08-12 06:15:02 +0300

    Add Content Management/metajour2_1/metajour/extension/search/lang/search.da.php

---
+++ Content Management/metajour2_1/metajour/extension/search/lang/search.da.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['label_name'] = 'Konfigurationsnavn';
+$LANG['label_pageid_result'] = 'Resultatside';
+$LANG['label_templateid_search'] = 'Skabelon for søgedialog';
+$LANG['label_templateid_result'] = 'Skabelon for søgeresultat';
+$LANG['name'] = 'Konfiguration af Søgning på website';
+?>
