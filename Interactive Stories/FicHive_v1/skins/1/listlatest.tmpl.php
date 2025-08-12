5341b51b713347d750b004ae76cb56fa81b23871
me-suzy <me.suzana@gmail.com>
2025-08-12 18:46:40 +0300

    Add Interactive Stories/FicHive_v1/skins/1/listlatest.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/listlatest.tmpl.php
@@ -0,0 +1,3 @@
+<b><%TITLE%></b><br>
+<%DESCRIPTION%><br>
+<span class='small'><%WIP%> <%AUTHOR%> &bull; <%CATEGORY%> &bull; <%RATING%> &bull; <%CHAPTERS%> &bull; <%WORDS%> &bull; <%REVIEWS%> &bull; <%HITS%> &bull; <%UPLOADED%> &bull; <%UPDATED%></span>
