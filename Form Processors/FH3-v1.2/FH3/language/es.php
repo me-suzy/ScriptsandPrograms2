934c32ab90420890dd375a045f680149a5585550
me-suzy <me.suzana@gmail.com>
2025-08-12 16:10:09 +0300

    Add Form Processors/FH3-v1.2/FH3/language/es.php

---
+++ Form Processors/FH3-v1.2/FH3/language/es.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Spanish language file
+ *
+ * @author Javi, CMS | cmendez21@gmail.com
+ * @package FormHandler
+ * @since 20/07/2005 (update by Javi)
+ */
+
+$fh_lang = array(
+  1  => 'Enero',
+  2  => 'Febrero',
+  3  => 'Marzo',
+  4  => 'Abril',
+  5  => 'Mayo',
+  6  => 'Junio',
+  7  => 'Julio',
+  8  => 'Agosto',
+  9  => 'Septiembre',
+  10 => 'Octubre',
+  11 => 'Noviembre',
+  12 => 'Diciembre',
+  13 => 'La fecha dada no es v&aacute;lida!',
+  14 => '&iexcl;No ingres&oacute; un valor correcto para este campo!',
+  15 => '&iexcl;La contrase&ntilde;a no es la misma!',
+  16 => '&iexcl;Estos campos son necesarios!',
+  17 => '&iexcl;La contrase&ntilde;a tiene que ser al menos de %d caracteres!',
+  18 => '&iexcl;La contrase&ntilde;a contiene caracteres inv&aacute;lidos!',
+  19 => 'Ingrese solo este campo si quiere sobreescribir el valor actual (<a href="%path%%filename%" target="_blank">%filename%</a>)<br />',
+  20 => 'Solo las siguientes extensiones están permitidas: %s.',
+  21 => 'E&iexcl;l archivo que intent&oacute; subir ya existe...!',
+  22 => '&iexcl;Tiene que subir un archivo...!',
+  23 => 'Tama&ntilde   de archivo m&aacute;ximo excedido: %s kb',
+  24 => '&iexcl;Ocurri&oacute; un problema. Int&eacute;ntelo otra vez...!',
+  25 => '<small>Ingrese solo estos campos si quiere cambiar su contrase&ntilde;a actual</small><br />',
+  26 => 'Enviar',
+  27 => 'Borrar',
+  28 => 'Cancelar',
+  29 => 'Disponible',
+  30 => 'Seleccionado',
+  31 => '&iexcl;El archivo subido no es de un tipo v&aacute;lido!',
+  32 => 'La dimensión de la imagen puede ser %d x %d o menos. El archivo subido tiene una dimensi&oacute;n de of %d x %d!',
+  33 => '&iexcl;Porque el formulario no es v&aacute;lido, el archivo <b>%filename%</b> tiene que ser seleccionado otra vez si quiere enviarlo con este formulario!<br />',
+  34 => 'Selecciona un item para mover a la %s caja o haga doble click para mover todos los items',
+  35 => '&iexcl;Este campo tiene que ser &uacute;nico. El valor "%s" ya existe en la base de datos!',
+  36 => '<b>%d</b> characters remaining on your input limit',
+  37 => 'You have to supply a file with an extension!',
+  38 => 'Back',
+  39 => 'The combination of the fields "%s" has to be unique. This combination already exists in the database! (Violation of index "%s")'
+);
+?>
