0aa6c1ba69711adda3742d76f100806b8cfc847e
me-suzy <me.suzana@gmail.com>
2025-08-12 18:21:29 +0300

    Add Image Galleries/singapore-0.9.11/singapore-0.9.11/templates/admin_default/confirm.tpl.php

---
+++ Image Galleries/singapore-0.9.11/singapore-0.9.11/templates/admin_default/confirm.tpl.php
@@ -0,0 +1,11 @@
+<h1><?php echo $confirmTitle ?></h1>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+<?php 
+  foreach($_REQUEST as $name => $value)
+    echo "<input type=\"hidden\" name=\"$name\" value=\"$value\" />\n";
+?>
+<p><?php echo $confirmMessage ?></p>
+<p><input type="submit" class="button" name="confirmed" value="<?php /*"*/ echo $sg->i18n->_g("confirm|OK") ?>">
+<input type="submit" class="button" name="confirmed" value="<?php /*"*/ echo $sg->i18n->_g("confirm|Cancel") ?>"></p>
+</form>
