ef8a1c4ef66fa86e9978e986572cde5e52d2edd2
me-suzy <me.suzana@gmail.com>
2025-08-12 11:58:45 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/account_history.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/account_history.php
@@ -0,0 +1,27 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Ihr Konto');
+define('NAVBAR_TITLE_2', 'Ihre get&auml;tigten Bestellungen');
+
+define('HEADING_TITLE', 'Ihre get&auml;tigten Bestellungen');
+
+define('TEXT_ORDER_NUMBER', 'Bestellnummer:');
+define('TEXT_ORDER_STATUS', 'Bestellstatus:');
+define('TEXT_ORDER_DATE', 'Bestelldatum:');
+define('TEXT_ORDER_SHIPPED_TO', 'Lieferanschrift:');
+define('TEXT_ORDER_BILLED_TO', 'Rechnungsanschrift:');
+define('TEXT_ORDER_PRODUCTS', 'Artikel:');
+define('TEXT_ORDER_COST', 'Summe:');
+define('TEXT_VIEW_ORDER', 'Bestellung ansehen');
+
+define('TEXT_NO_PURCHASES', 'Sie haben noch keine Bestellungen get&auml;tigt.');
+?>
