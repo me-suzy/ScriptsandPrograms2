dff0ff3551a038fadba9e062290a803d543c15ac
me-suzy <me.suzana@gmail.com>
2025-08-12 12:39:22 +0300

    Add E-Commerce/shop-script-free/includes/search_adv.php

---
+++ E-Commerce/shop-script-free/includes/search_adv.php
@@ -0,0 +1,16 @@
+<?php
+/*****************************************************************************
+ *                                                                           *
+ * Shop-Script FREE                                                          *
+ * Copyright (c) 2005 WebAsyst LLC. All rights reserved.                     *
+ *                                                                           *
+ ****************************************************************************/
+
+	// advanced search
+
+	if (isset($_GET["adv_search"])) //advanced search form
+	{
+		$smarty->assign("main_content_template", "notavailable.tpl.html");
+	}
+
+?>
