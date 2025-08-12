208831f04f0fe16bdf244c065c58d60cfd3deaa3
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:50 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_setregister.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_setregister.php
@@ -0,0 +1,55 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Paramétre d\'Enregistrement';
+
+//  Form Block Titles
+$GLOBALS["thActiveFields"] = 'Activer fields';
+$GLOBALS["thMailSettings"] = 'Courrier paramétres';
+$GLOBALS["thExtraSettings"] = 'Extra paramétres';
+
+//  Form Field Headings
+$GLOBALS["tPhoneNumber"] = 'Numéro de téléphone';
+$GLOBALS["tFaxNumber"] = 'Numéro de FAX';
+$GLOBALS["tAddress"] = 'Addresse';
+$GLOBALS["tCity"] = 'Ville';
+$GLOBALS["tState"] = 'Etat';
+$GLOBALS["tZip"] = 'Zip';
+$GLOBALS["tCountry"] = 'Pays';
+$GLOBALS["tLanguage"] = 'Langue';
+$GLOBALS["tWebsite"] = 'Site web';
+$GLOBALS["tComments"] = 'Commentaire';
+$GLOBALS["tNewsletter"] = 'Bulletin';
+$GLOBALS["tSendConfMail"] = 'Confirmation e-mail';
+$GLOBALS["tServerUserEmail"] = 'Le compte d\'utilisateur du serveur';
+$GLOBALS["tErrFormFontColor"] = 'Message d\'erreur couleur de police';
+$GLOBALS["tShowHelptexts"] = 'Afficher texte aide';
+$GLOBALS["tHelpTextSize"] = 'Texte aide largeur de police';
+$GLOBALS["tHelpTextColor"] = 'Texte aide police couleur';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'Ici vous pouvez ajuster l\'enregistrement et les paramétre d\'envoi de courrier.';
+
+$GLOBALS["hPhoneNumber"] = 'Les expositions ou rend infirme le numéro de téléphone sur la forme d\'enregistrement d\'utilisateur. Quand montré, le champs peut être obligatoire ou facultatif.';
+$GLOBALS["hFaxNumber"] = 'Les expositions ou rend infirme le numéro de fax sur la forme d\'enregistrement d\'utilisateur. Quand montré, le champs peut être obligatoire ou facultatif.';
+$GLOBALS["hAddress"] = 'Les expositions ou rend infirme le champs d\'adresse sur la forme d\'enregistrement d\'utilisateur. Quand montré, le champs peut être obligatoire ou facultatif.';
+$GLOBALS["hCity"] = 'Les expositions ou rend infirme le champs de ville sur la forme d\'enregistrement d\'utilisateur. Quand montré, le champs peut être obligatoire ou facultatif.';
+$GLOBALS["hState"] = 'Les expositions ou rend infirme le champs de l\'état sur la forme d\'enregistrement d\'utilisateur. Quand montré, le champs peut être obligatoire ou facultatif.';
+$GLOBALS["hZip"] = 'Les expositions ou rend infirme le champs de Fermeture éclair sur la forme d\'enregistrement d\'utilisateur. Quand montré, le champs peut être obligatoire ou facultatif.';
+$GLOBALS["hCountry"] = 'Les expositions ou rend infirme le champs de pays sur la forme d\'enregistrement d\'utilisateur. Quand montré, le champs peut être obligatoire ou facultatif.';
+$GLOBALS["hLanguage"] = 'Les expositions ou rend infirme le champs de langue sur la forme d\'enregistrement d\'utilisateur. Quand montré, le champs peut être obligatoire ou facultatif.';
+$GLOBALS["hWebsite"] = 'Les expositions ou rend infirme le champs de Site web sur la forme d\'enregistrement d\'utilisateur. Quand montré, le champs peut être obligatoire ou facultatif. ';
+$GLOBALS["hComments"] = 'Les expositions ou rend infirme le champs de description sur la forme d\'enregistrement d\'utilisateur. Quand montré, le champs peut être obligatoire ou facultatif.';
+$GLOBALS["hNewsletter"] = 'Les expositions ou rend infirme l\'option pour s\'abonner pour un bulletin sur la forme d\'enregistrement d\'utilisateur.';
+$GLOBALS["hSendConfMail"] = 'La confirmation d\'e-mail avec l\'enregistrement détaille sera envoie à l\'a enregistré l\'utilisateur.';
+$GLOBALS["hServerUserEmail"] = 'Parfois il est eu besoin d\'envoyer l\'e-mail avec le compte d\'utilisateur de serveur. Si vous partez ce champs vide l\'adresse électronique de l\'utilisateur administratif sera utilisée dans le du champs. Vous pouvez overrule ce.';
+$GLOBALS["hErrFormFontColor"] = 'La couleur de jeu de caractères sur la forme d\'enregistrement pour les messages d\'erreur de validation.';
+$GLOBALS["hShowHelptexts"] = 'Montrer les textes d\'aide à côté des champs d\'enregistrement.';
+$GLOBALS["hHelpTextSize"] = 'Ceci règle la taille de jeu de caractères des textes d\'aide qui apparaissent à côté des champs d\'enregistrement.';
+$GLOBALS["hHelpTextColor"] = 'Ceci règle la couleur de jeu de caractères des textes d\'aide qui apparaissent à côté des champs d\'enregistrement.';
+
+// Error messages
+$GLOBALS["eErrorColorWrong"] = 'La couleur de jeu de caractères de message d\'erreur a la mauvaise valeur.';
+$GLOBALS["eHelptextColorWrong"] = 'Aider le jeu de caractères de texte colore a mauvais estime.';
+
+?>
