a7f783e7445b4537726c285c37d584d05eae55ea
me-suzy <me.suzana@gmail.com>
2025-08-12 18:13:15 +0300

    Add Image Galleries/PHP_JK_Aug_01_2005/Includes/Nav/PHP_JK_CSS.php

---
+++ Image Galleries/PHP_JK_Aug_01_2005/Includes/Nav/PHP_JK_CSS.php
@@ -0,0 +1,152 @@
+<STYLE>
+	td, form {
+		font-size: 12px;
+		color: #<?=$TextColor1?>;
+		font-family: Verdana, Arial, Sans-Serif;
+	}
+	
+	.SmallContent {
+		font-size: 10px;
+		font-family: Verdana, Arial, Sans-Serif;
+	}
+
+	a.LargeNavPage {
+		font-size: 16px;
+		color: #<?=$PageText?>;
+		font-weight: bold;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+	}
+	a.LargeNavPage:link{color: <?=$PageLinkColor?>;}
+	a.LargeNavPage:hover{color: <?=$PageLinkColor?>;}
+	a.LargeNavPage:visited{color: <?=$PageVLinkColor?>;}
+	a.LargeNavPage:active{color: <?=$PageALinkColor?>;}
+	
+	a.MediumNavPage {
+		font-size: 12px;
+		color: #<?=$PageText?>;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+		font-weight: bold;
+	}
+	a.MediumNavPage:link{color: <?=$PageLinkColor?>;}
+	a.MediumNavPage:hover{color: <?=$PageLinkColor?>;}
+	a.MediumNavPage:visited{color: <?=$PageVLinkColor?>;}
+	a.MediumNavPage:active{color: <?=$PageALinkColor?>;}
+	
+	a.SmallNavPage {
+		font-size: 10px;
+		color: #<?=$PageText?>;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+	}
+	a.SmallNavPage:link{color: <?=$PageLinkColor?>;}
+	a.SmallNavPage:hover{color: <?=$PageLinkColor?>;}
+	a.SmallNavPage:visited{color: <?=$PageVLinkColor?>;}
+	a.SmallNavPage:active{color: <?=$PageALinkColor?>;}
+	
+	a.LargeNav1 {
+		font-size: 16px;
+		color: #<?=$TextColor1?>;
+		font-weight: bold;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+	}
+	a.LargeNav1:link{color: <?=$LinkColor1?>;}
+	a.LargeNav1:hover{color: <?=$LinkColor1?>;}
+	a.LargeNav1:visited{color: <?=$VLinkColor1?>;}
+	a.LargeNav1:active{color: <?=$ALinkColor1?>;}
+	
+	a.MediumNav1 {
+		font-size: 12px;
+		color: #<?=$TextColor1?>;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+		font-weight: bold;
+	}
+	a.MediumNav1:link{color: <?=$LinkColor1?>;}
+	a.MediumNav1:hover{color: <?=$LinkColor1?>;}
+	a.MediumNav1:visited{color: <?=$VLinkColor1?>;}
+	a.MediumNav1:active{color: <?=$ALinkColor1?>;}
+	
+	a.SmallNav1 {
+		font-size: 10px;
+		color: #<?=$TextColor1?>;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+	}
+	a.SmallNav1:link{color: <?=$LinkColor1?>;}
+	a.SmallNav1:hover{color: <?=$LinkColor1?>;}
+	a.SmallNav1:visited{color: <?=$VLinkColor1?>;}
+	a.SmallNav1:active{color: <?=$ALinkColor1?>;}
+	
+	a.LargeNav2 {
+		font-size: 16px;
+		color: #<?=$TextColor2?>;
+		font-weight: bold;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+	}
+	a.LargeNav2:link{color: <?=$LinkColor2?>;}
+	a.LargeNav2:hover{color: <?=$LinkColor2?>;}
+	a.LargeNav2:visited{color: <?=$VLinkColor2?>;}
+	a.LargeNav2:active{color: <?=$ALinkColor2?>;}
+	
+	a.MediumNav2 {
+		font-size: 12px;
+		color: #<?=$TextColor2?>;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+		font-weight: bold;
+	}
+	a.MediumNav2:link{color: <?=$LinkColor2?>;}
+	a.MediumNav2:hover{color: <?=$LinkColor2?>;}
+	a.MediumNav2:visited{color: <?=$VLinkColor2?>;}
+	a.MediumNav2:active{color: <?=$ALinkColor2?>;}
+	
+	a.SmallNav2 {
+		font-size: 10px;
+		color: #<?=$TextColor2?>;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+	}
+	a.SmallNav2:link{color: <?=$LinkColor2?>;}
+	a.SmallNav2:hover{color: <?=$LinkColor2?>;}
+	a.SmallNav2:visited{color: <?=$VLinkColor2?>;}
+	a.SmallNav2:active{color: <?=$ALinkColor2?>;}
+	
+	a.LargeNav3 {
+		font-size: 16px;
+		color: #<?=$TextColor3?>;
+		font-weight: bold;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+	}
+	a.LargeNav3:link{color: <?=$LinkColor3?>;}
+	a.LargeNav3:hover{color: <?=$LinkColor3?>;}
+	a.LargeNav3:visited{color: <?=$VLinkColor3?>;}
+	a.LargeNav3:active{color: <?=$ALinkColor3?>;}
+	
+	a.MediumNav3 {
+		font-size: 12px;
+		color: #<?=$TextColor3?>;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+		font-weight: bold;
+	}
+	a.MediumNav3:link{color: <?=$LinkColor3?>;}
+	a.MediumNav3:hover{color: <?=$LinkColor3?>;}
+	a.MediumNav3:visited{color: <?=$VLinkColor3?>;}
+	a.MediumNav3:active{color: <?=$ALinkColor3?>;}
+	
+	a.SmallNav3 {
+		font-size: 10px;
+		color: #<?=$TextColor3?>;
+		font-family: Verdana, Arial, Sans-Serif;
+		text-decoration: none !important;
+	}
+	a.SmallNav3:link{color: <?=$LinkColor3?>;}
+	a.SmallNav3:hover{color: <?=$LinkColor3?>;}
+	a.SmallNav3:visited{color: <?=$VLinkColor3?>;}
+	a.SmallNav3:active{color: <?=$ALinkColor3?>;}
+</STYLE>
