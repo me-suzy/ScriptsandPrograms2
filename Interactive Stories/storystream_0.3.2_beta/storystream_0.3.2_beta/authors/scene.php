4d28189fdbb87542ed4212bf87999f507a1f136a
me-suzy <me.suzana@gmail.com>
2025-08-12 18:54:39 +0300

    Add Interactive Stories/storystream_0.3.2_beta/storystream_0.3.2_beta/authors/scene.php

---
+++ Interactive Stories/storystream_0.3.2_beta/storystream_0.3.2_beta/authors/scene.php
@@ -0,0 +1,7 @@
+<?php
+// THIS MUST BE THE FIRST LINE OF THE FILE
+$GLOBALS['SCRIPT_ROOT'] = '../';
+require_once ($GLOBALS['SCRIPT_ROOT'].'include/init.inc.php');
+$scene = new SSScenePage;
+$scene->render ();
+?>
