c3372f38c2ccb36b3dd2f34e44fb241fb132f9dd
me-suzy <me.suzana@gmail.com>
2025-08-12 13:56:29 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/admin/faqedit.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/admin/faqedit.php
@@ -0,0 +1,109 @@
+<?
+$file_rev="041305";
+////////////////////////////////////////////////////////
+//                 phpBannerExchange                  //
+//                   by: Darkrose                     //
+//              (darkrose@eschew.net)                 //
+//                                                    //
+// You can redistribute this software under the terms //
+// of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of  //
+// the License, or (at your option) any later         //
+// version.                                           //
+//                                                    //
+// You should have received a copy of the GNU General //
+// Public License along with this program; if not,    //
+// write to the Free Software Foundation, Inc., 59    //
+// Temple Place, Suite 330, Boston, MA 02111-1307 USA //
+//                                                    //
+//     Copyright 2004 by eschew.net Productions.      //
+//   Please keep this copyright information intact.   //
+////////////////////////////////////////////////////////
+
+include("../config.php");
+include("../css.php");
+include("../lang/admin.php");
+require_once('../lib/template_class.php');
+
+if($use_gzhandler==1){
+	ob_start("ob_gzhandler");
+}
+
+// Begin loginstuff
+if(!$db=@mysql_connect("$dbhost","$dbuser","$dbpass")){
+	include("../lang/errors.php");
+	$err="1";
+	$error.="$LANG_error_header<p>";
+	$error.="$LANG_error_mysqlconnect ";
+	$error.=mysql_error();
+}
+
+@mysql_select_db($dbname,$db);
+
+session_start();
+header("Cache-control: private"); //IE 6 Fix 
+$session=session_id(); 
+$login = $_SESSION['login'];
+$pass = $_SESSION['pass'];
+
+$result = mysql_query("select * from banneradmin where adminuser='$login' AND adminpass='$pass'");
+$get_userinfo=mysql_fetch_array($result);
+$login=$get_userinfo[adminuser];
+$pass=$get_userinfo[adminpass];
+
+    if($login=="" AND $pass=="" OR $pass=="" OR $err=="1") {
+		include("../lang/errors.php");
+		$error.="$LANG_error_header<p>";
+		$error.="$LANG_login_error";
+
+		$page = new Page('../template/admin_error.php');
+		$page->replace_tags(array(
+			'css' => "$css",
+			'session' => "$session",
+			'baseurl' => "$baseurl",
+			'title' => "$exchangename - $LANG_login_error_title",
+			'shorttitle' => "$LANG_login_error_title",
+			'error' => "$error",
+			'menu' => "$menu",
+			'footer' => '../footer.php'));
+		$page->output();
+	session_destroy();
+
+	}else{ 
+		$session=$_REQUEST['SID'];
+
+		if($_REQUEST['fid']){
+			$fid=$_REQUEST['fid'];
+			$fid_form= "&fid=$fid";	
+			$faqs=mysql_query("select * from bannerfaq where id='$fid'");	
+			$get_faqs=mysql_fetch_array($faqs);	
+			$question=$get_faqs[question];	
+			$answer=$get_faqs[answer];	
+			$answer=ereg_replace("<p>", "\n\n", $answer);	
+			$answer=ereg_replace("<br>", "\n", $answer);	
+			$answer=ereg_replace("<br />", "", $answer);	
+		}else{	
+			$fid_form="";	
+		}
+
+	$page = new Page('../template/admin_faqedit.php');
+	$page->replace_tags(array(
+		'css' => "$css",
+		'session' => "$session",
+		'baseurl' => "$baseurl",
+		'title' => "$exchangename - $LANG_menu_faqmgr",
+		'fid' => "$fid_form",
+		'shorttitle' => "$LANG_menu_faqmgr",
+		'add' => "$LANG_faq_add",
+		'id' => "$LANG_ID",
+		'question' => "$question",
+		'answer' => "$answer",
+		'question_txt' => "$LANG_question",
+		'answer_txt' => "$LANG_answer",
+		'submit' => "$LANG_submit",
+		'reset' => "$LANG_email_button_reset",
+		'menu' => 'admin_menuing.php',
+		'footer' => '../footer.php'));
+	$page->output();
+	}
+?>
