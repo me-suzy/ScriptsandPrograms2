f0d5b74d9ee7e578efa797035a1f46436ab411af
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:02 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/affiliate_banners.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/affiliate_banners.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Partnerprogramm');
+define('HEADING_TITLE', 'Partnerprogramm: Banner');
+
+define('TEXT_AFFILIATE_NAME', 'Banner Name: ');
+define('TEXT_INFORMATION', 'W&auml;hlen Sie den Banner oder den Link aus den untenhalb angebotenen M&ouml;glichkeiten, den Sie auf Ihrer Webseite publizieren m&ouml;chten.');
+define('TEXT_AFFILIATE_INFO', 'Kopieren Sie den HTML Code und f&uuml;gen Sie diesen in Ihrer Webseite ein.');
+define('TEXT_AFFILIATE_INDIVIDUAL_BANNER', 'BUILD-A-LINK');
+define('TEXT_AFFILIATE_INDIVIDUAL_BANNER_INFO', 'Geben Sie die Produkt-ID (in der URL sichtbar) von dem Produkt ein, das Sie auf Ihrer Webseite anbieten m&ouml;chten und bet&auml;tigen Sie den Button "Build a Link"');
+
+define('IMAGE_BUTTON_BUILD_A_LINK','Link für einen Produkt Banner erzeugen');
+?>
