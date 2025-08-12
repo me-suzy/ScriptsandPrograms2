a3a9e57dcea97f57765b68e77d6c7a66fc20c77c
me-suzy <me.suzana@gmail.com>
2025-08-12 17:02:15 +0300

    Add Groupware Tools/l4w_v0_5_2/install/alter_gacl_table_types.sql

---
+++ Groupware Tools/l4w_v0_5_2/install/alter_gacl_table_types.sql
@@ -0,0 +1,8 @@
+# MySQL-Front Dump 2.5
+#
+# Host: localhost   Database: gacl
+# --------------------------------------------------------
+# adjust gacl tables to be of type InnoDB
+
+ALTER TABLE ###TABLE_PREFIX###gacl_aro_groups TYPE = InnoDB;
+
