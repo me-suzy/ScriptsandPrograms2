3626acf1d0a669e7b2dfda883592c18727f58d91
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:26 +0300

    Add Content Management/metajour2_1/metajour/lang/documentsection.da.php

---
+++ Content Management/metajour2_1/metajour/lang/documentsection.da.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Navn på sektion';
+$LANG['label_subname'] = 'Undernavn';
+$LANG['label_content'] = 'Indhold';
+$LANG['label_extension'] = 'Komponent';
+$LANG['label_configset'] = 'Konfigurationssæt';
+$LANG['label_params'] = 'Parametre';
+$LANG['label_script'] = 'Tillad inline-scripting';
+$LANG['label_file'] = 'Tilknyttet fil';
+$LANG['name'] = 'Dokumentsektioner';
+
+?>
