a1cddde55fd98e0a1a3960bfb4b11ae0481202ba
me-suzy <me.suzana@gmail.com>
2025-08-12 23:18:25 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_membre/lang/lang-mod_membre-lang-dutch.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_membre/lang/lang-mod_membre-lang-dutch.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE MEMBRES
+$Mod_Membres_Accueil_OFF="Herinitialisatie";
+$Mod_Membres_News_Add="Uw artikel werd correct verstuurd";
+$Mod_Membres_Liens_Add="Uw link werd correct verstuurd";
+$Mod_Membres_News_Edit="Uw artikel werd correct verbeterd";
+$Mod_Membres_Info="Bestand bewerken <strong>/include/config.php</strong> om de waarden bij te stellen";
+$Mod_Membres_InfoUser="Bewerk tabel <strong>$BD_Tab_user</strong> om waarden bij te stellen";
+$Mod_Membres_Info_Theme="Thema";
+$Mod_Membres_Info_Langue="Taal";
+$Mod_Membres_Info_Titre="Titel";
+$Mod_Membres_Info_Version="Versie";
+$Mod_Membres_Info_Couleur="Kleur Logo";
+$Mod_Membres_Info_URL="URL";
+$Mod_Membres_Info_UploadLimite="Max. Upload";
+$Mod_Membres_Serveur_Info="Informatie over PHP server";
+$Mod_Membres_Nom="Naam";
+$Mod_Membres_Pseudo="Alias";
+$Mod_Membres_Mail="E-Mail";
+$Mod_Membres_Date="Actief sinds ";
+$Mod_Membres_Droit="Toegang";
+$Mod_Membres_Web="Web";
+$Mod_Membres_Password="Wachtwoord";
+$Mod_Membres_Err_Field="U bent vergeten om een veld in te vullen.";
+$Mod_Membres_Err_Login="Alias en/of wachtwoord niet correct";
+$Mod_Membres_Rub_Accueil="Toegang Admin";
+$Mod_Membres_Rub_Info="Info";
+$Mod_Membres_Rub_News_Cat="Toevoegen 1 artikel aan module Nieuws";
+$Mod_Membres_Rub_News_Add="Toevoegen 1 artikel";
+$Mod_Membres_Rub_News_Edit="Bewerken 1 artikel";
+$Mod_Membres_Rub_News_Del="Verwijderen 1 artikel";
+$Mod_Membres_Rub_News_Del2="Verwijderen";
+$Mod_Membres_Rub_News_Form_Categorie="Naam van de Rubriek";
+$Mod_Membres_Rub_News_JS_Cat="Het veld Rubriek moet ingevuld zijn";
+$Mod_Membres_Rub_News_Cat_OK="Uw rubriek is correct overgezet";
+$Mod_Membres_Rub_File_Cat="Toevoegen 1 rubriek aan module Bestanden";
+$Mod_Membres_Rub_File_Del="Verwijderen 1 Bestanden";
+$Mod_Membres_Rub_File_Form_Categorie="Naam van de rubriek";
+$Mod_Membres_Rub_File_JS_Cat="Het veld Rubriek moet ingevuld zijn";
+$Mod_Membres_Rub_File_Cat_OK="Uw rubriek is correct overgezet";
+$Mod_Membres_Rub_Liens_Cat="Toevoegen 1 rubriek aan de module Links";
+$Mod_Membres_Rub_Liens_Cat_OK="Uw rubriek is correct overgezet";
+$Mod_Membres_Rub_Faq_Form_Categorie="Naam van de rubriek";
+$Mod_Membres_Rub_Faq_Cat="Toevoegen 1 rubriek aan de module FAQ";
+$Mod_Membres_Rub_Faq_Add="Toevoegen 1 FAQ";
+$Mod_Membres_Rub_Faq_Edit="Bewerk 1 FAQ";
+$Mod_Membres_Rub_Faq_Del="Verwijder 1 FAQ";
+$Mod_Membres_Rub_Faq_Add_OK="Uw FAQ is correct overgezet";
+$Mod_Membres_Rub_Faq_Cat_OK="Uw rubriek is correct overgezet";
+$Mod_Membres_Rub_Upload_Photo="Toevoegen 1 foto";
+$Mod_Membres_Rub_Del_Photo="Verwijderen 1 foto";
+$Mod_Membres_Rub_Upload_File = "Toevoegen 1 map";
+$Mod_Membres_Rub_Liens_Edit="Toevoegen 1 link";
+$Mod_Membres_Nom="Naam";
+$Mod_Membres_Regdate="Datum registratie";
+$Mod_Membres_Droit="Toegang";
+$Mod_Membres_Web="Web";
+$Mod_Membres_OK_Login="U bent verbonden";
+$Mod_Membres_OFF_Login="U bent niet meer verbonden";
+$Mod_Membres_Photos_Form_Photo="Foto(JPG)";
+$Mod_Membres_Photos_Form_Vign="Miniatuur(JPG)";
+$Mod_Membres_Photos_Form_Titre="Titel (50)";
+$Mod_Membres_Photos_Form_Date="Datum (jjjj-mm-dd)";
+$Mod_Membres_Photos_Form_Texte="Tekst";
+
+?>
