00cd97ac81af7d0de2f3a7fce5bc78dbb66f24ef
me-suzy <me.suzana@gmail.com>
2025-08-12 12:51:41 +0300

    Add Education/DigitalScribe1.4.1/DigitalScribe1.4.1/DigitalScribe/header2.php

---
+++ Education/DigitalScribe1.4.1/DigitalScribe1.4.1/DigitalScribe/header2.php
@@ -0,0 +1,17 @@
+</head>
+
+<BODY BGCOLOR="#ffffff" TEXT="#000000" LINK="#3333FF" ALINK="#3333FF" VLINK="#3333FF">
+
+
+
+<A HREF=index.php>
+
+<IMG SRC=/DigitalScribe/images/logosmall.gif width=158 height=63 alt="Digital Scribe Logo" border=0></a><P>
+
+
+
+<TABLE WIDTH=90% BORDER="0" CELLPADDING="0" CELLSPACING="0"><TR>
+
+
+
+<TD BGCOLOR="#FFFFFF">
