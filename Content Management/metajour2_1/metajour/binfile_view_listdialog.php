c0a0d60ae048d4a72a8619b209444f5da9a2db1a
me-suzy <me.suzana@gmail.com>
2025-08-12 05:56:42 +0300

    Add Content Management/metajour2_1/metajour/binfile_view_listdialog.php

---
+++ Content Management/metajour2_1/metajour/binfile_view_listdialog.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('binfile_view_list.php');
+
+class binfile_view_listdialog extends binfile_view_list {
+		
+	function ondblclick() {
+		return "
+		var result = new Object;
+		result.id = this.id
+		var nametd = document.getElementById('name-' + this.id);
+		if (nametd) {
+			result.name = nametd.innerText;
+		}
+		window.returnValue = result; window.close(); return false;";
+	}
+
+}
+?>
