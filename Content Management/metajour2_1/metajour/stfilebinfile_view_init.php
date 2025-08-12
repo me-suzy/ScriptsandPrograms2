2cf1d6fcb7db5b2d76afbc47b78f36dbb54c23d1
me-suzy <me.suzana@gmail.com>
2025-08-12 05:58:39 +0300

    Add Content Management/metajour2_1/metajour/stfilebinfile_view_init.php

---
+++ Content Management/metajour2_1/metajour/stfilebinfile_view_init.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('basic_view_split.php');
+
+class stfilebinfile_view_init extends basic_view_split {
+		
+	function view() {
+		return $this->splitView('stfilefolder','stfilebinfile');
+	}
+
+}
+?>
