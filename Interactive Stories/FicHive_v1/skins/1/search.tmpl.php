7be307d87844a0168f21162bce720235605f395d
me-suzy <me.suzana@gmail.com>
2025-08-12 18:47:33 +0300

    Add Interactive Stories/FicHive_v1/skins/1/search.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/search.tmpl.php
@@ -0,0 +1,13 @@
+<%RESULT%>
+<p>
+<table border='0' width='100%'>
+<tr><td class='fframe'><%FOR%></td><td><input type='text' name='for'></td></tr>
+<tr><td class='fframe'><%BOOLEAN%></td><td><select name='st'>
+<option value=1><%STITLE%><option value=2><%SSUMM%><option value=3><%SFULL%></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>'></td></tr>
+</table>
+<p>
+<center><%QUICKNAV%>: <%RANGE%></center>
+<p>
+<a name='boolean'></a>
+<span id='small'><%BOOEXP%></span>
