e9f38f55c35109689665291864683ae5fac4f49e
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:20 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_authors.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_authors.php
@@ -0,0 +1,75 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] 		= 'Beheer gebruikers';
+
+//  List Headings
+$GLOBALS["tLogin"] 		= 'Loginnaam';
+$GLOBALS["tUsername"] 		= 'Gebruikersnaam';
+$GLOBALS["tEMail"] 		= 'E-mail';
+$GLOBALS["tUsergroup"] 		= 'Gebruikersgroep';
+$GLOBALS["tEnabled"] 		= 'Aanzetten';
+
+//  List Functions
+$GLOBALS["tAuthorFilter"] 	= 'Gebruikersgroep filter';
+$GLOBALS["tAddNewAuthor"] 	= 'Nieuwe gebruiker toevoegen';
+$GLOBALS["tEditAuthor"] 	= 'Bewerken gebruiker';
+$GLOBALS["tViewAuthor"] 	= 'Gegevens gebruiker';
+$GLOBALS["tDeleteAuthor"] 	= 'Verwijder gebruiker';
+$GLOBALS["tReleaseUser"] 	= 'Gebruiker aan- of uitzetten';
+$GLOBALS["tConfirmRelease"] 	= 'Weet u zeker dat u deze gebruiker aan of uit wilt zetten?';
+
+//  Form Block Titles
+$GLOBALS["thLogin"] 		= 'Login details';
+$GLOBALS["thUser"] 		= 'Gebruikers details';
+$GLOBALS["thConfiguration"] 	= 'Persoonlijke voorkeuren';
+
+//  Form Field Headings
+$GLOBALS["tPassword"] 		= 'Wachtwoord';
+$GLOBALS["tRetypePassword"] 	= 'Herhaal wachtwoord';
+$GLOBALS["tAuthorname"] 	= 'Scherm naam';
+$GLOBALS["tEMail"] 		= 'E-mailadres';
+$GLOBALS["tPrivateEMail"] 	= 'Verberg e-mailadres';
+$GLOBALS["tCountry"] 		= 'Land';
+$GLOBALS["tLanguage"] 		= 'Taal';
+$GLOBALS["tPhone"] 		= 'Telefoon';
+$GLOBALS["tFax"] 		= 'Fax';
+$GLOBALS["tAddress"] 		= 'Adres';
+$GLOBALS["tCity"] 		= 'Plaats';
+$GLOBALS["tState"] 		= 'Provincie';
+$GLOBALS["tZip"] 		= 'Postcode';
+$GLOBALS["tWebsite"] 		= 'Website';
+$GLOBALS["tComments"] 		= 'Commentaar';
+$GLOBALS["tNewsletter"] 	= 'Nieuwsbrief';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] 		= 'Met dit formulier kunt u gebruikers aanmaken of bewerken die toegang hebben tot de inhoud van de website.';
+$GLOBALS["hLogin"] 		= 'Dit is de loginnaam van de gebruiker, die gebruikt wordt bij het wachtwoord scherm.';
+$GLOBALS["hPassword"] 		= 'Vul het wachtwoord in dat de gebruiker nodig heeft om toegang te krijgen tot iziContents.';
+$GLOBALS["hRetypePassword"] 	= 'Vul opnieuw het wachtwoord in, om er zeker van te zijn dat het goed is ingevuld.';
+$GLOBALS["hAuthorname"] 	= 'Dit veld is de volledige naam van de gebruiker. Deze naam zal gebruikt worden onder de artikelen.';
+$GLOBALS["hUsergroup"] 		= 'De gebruikersgroep waar deze gebruiker toe behoort. De gebruikersgroep bepaalt hoeveel rechten deze gebruiker heeft in het administratie veld.';
+$GLOBALS["hEMail"] 		= 'Het e-mailadres van de gebruiker. Samen met de gebruikersnaam wordt dit een link om te kunnen e-mailen.';
+$GLOBALS["hPrivateEMail"] 	= 'Dit aanvinken als u wilt dat het e-mailadres verborgen moet blijven.';
+$GLOBALS["hCountry"] 		= 'Het land waar de gebruiker vandaan komt (optie).';
+$GLOBALS["hLanguage"] 		= 'Gebruikers tweede taal (optie).<br />Als dit is aangevinkt zal iziContents geheel in een andere taal werken.';
+$GLOBALS["hPhone"] 		= 'Gebruikers telefoonnummer.';
+$GLOBALS["hFax"] 		= 'Gebruikers faxnummer.';
+$GLOBALS["hAddress"] 		= 'Gebruikers adres.';
+$GLOBALS["hCity"] 		= 'De plaats waar de gebruiker woont.';
+$GLOBALS["hState"] 		= 'Gebruikers provincie';
+$GLOBALS["hZip"] 		= 'De postcode van de gebruiker.';
+$GLOBALS["hWebsite"] 		= 'Website adres van de gebruiker.';
+$GLOBALS["hComments"] 		= 'Commentaar van de gebruiker.';
+$GLOBALS["hNewsletter"] 	= 'Gebruiker wil de nieuwsbrief ontvangen.';
+
+//  Error Messages
+$GLOBALS["eLoginExists"] 	= 'Login bestaat al, kies een andere naam.';
+$GLOBALS["eLoginEmpty"] 	= 'Loginnaam kan niet leeg blijven.';
+$GLOBALS["ePasswordError"] 	= 'Het wachtwoord klopt niet met het eerst ingevulde paswoord.';
+$GLOBALS["eNameEmpty"] 		= 'Auteurnaam kan niet leeg blijven.';
+$GLOBALS["eNameExists"] 	= 'Een auteur met deze naam bestaat al, kies een nieuwe.';
+$GLOBALS["eEMailEmpty"] 	= 'U moet een auteurs e-mailadres invullen.';
+$GLOBALS["eEmailIncorrect"]   	= 'Dit e-mailadres klopt niet, probeer het opnieuw.';
+
+?>
