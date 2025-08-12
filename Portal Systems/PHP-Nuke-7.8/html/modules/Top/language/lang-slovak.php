2251912d7f40f22023091cd21894728a874c6a79
me-suzy <me.suzana@gmail.com>
2025-08-12 23:03:28 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Top/language/lang-slovak.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Top/language/lang-slovak.php
@@ -0,0 +1,55 @@
+<?php
+
+/*************************************************************************/
+/* The Slovak language file for module Top                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/*************************************************************************/
+/* The Slovak language file for module Top                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+
+define("_CATEGORY","Kategória");
+define("_LVOTES","hlasovalo");
+define("_HITS"," krát èítané");
+define("_TOPWELCOME","Vitajte na stránke TOP na");
+define("_READSTORIES","najèítanejích èlánkov:");
+define("_COMMENTEDSTORIES","najkomentovanejích èlánkov:");
+define("_ACTIVECAT","najaktívnejích kategórií:");
+define("_READSECTION","najèítanejích èlánkov v iných oblastiach:");
+define("_NEWSSUBMITTERS","najaktívnejích prispievate¾ov:");
+define("_NEWSSENT","poslaných noviniek:");
+define("_VOTEDPOLLS","Najnavtevovanejích ankiet");
+define("_MOSTACTIVEAUTHORS","najaktívnejích autorov");
+define("_NEWSPUBLISHED","publikovaných èlánkov:");
+define("_READREVIEWS","najèítanejích recenzií:");
+define("_DOWNLOADEDFILES","najsahovanejích súborov:");
+define("_LDOWNLOADS","downloads");
+define("_MOSTREADPAGES","Najèítanejie strany");
+define("_MOSTVOTEDSTORIES","Najviac hodnotené èlánky");
+define("_POINTS","body");
+define("_BESTRATEDSTORIES","Najlepie hodnotené èlánky ( minimálne 5 hlasov )");
+
+?>
