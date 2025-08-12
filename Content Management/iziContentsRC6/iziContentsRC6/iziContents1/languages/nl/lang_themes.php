6b02a40145424b944395055167a7d270f71ad05c
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:12 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_themes.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_themes.php
@@ -0,0 +1,38 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Beheer thema\'s';
+
+//  List Headings
+$GLOBALS["tThemeCode"] = 'Thema code';
+$GLOBALS["tThemeName"] = 'Thema naam';
+$GLOBALS["tThemeDescription"] = 'Thema omschrijving';
+$GLOBALS["tThemeEnabled"] = 'Aan';
+
+//  List Functions
+$GLOBALS["tAddNewTheme"] = 'Nieuw thema toevoegen';
+$GLOBALS["tViewTheme"] = 'Bekijk thema bijzonderheden';
+$GLOBALS["tEditTheme"] = 'Bewerk thema bijzonderheden';
+$GLOBALS["tDeleteTheme"] = 'Verwijder thema';
+$GLOBALS["tReleaseTheme"] = 'Aan/Uitzetten van dit thema';
+$GLOBALS["tSelectTheme"] = 'Selecteer dit thema voor beheer';
+
+//  Form Block Titles
+$GLOBALS["thThemeGeneral"] = 'Thema details';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Op deze pagina kunt u thema\'s maken voor de iziContents site.';
+$GLOBALS["hThemeCode"] = 'Dit is een unieke code voor dit thema.<br />Deze code mag geen spaties of speciale karakters bevatten.<br /><br />Deze code mag geen spaties of speciale karakters bevatten.<br /><br />Als de Apache server geconfigureerd is voor het gebruik van de .htaccess file in de iziContents directorie, kunnen de bezoekers direct toegang krijgen tot deze site met een url in dit formaat: http://www.mijnserver.com/ezc_directorie/<themecode>.';
+$GLOBALS["hThemeName"] = 'Deze themanaam wordt gebruikt bij de keuzelijst van thema\'s (gebruikt bij de [themelist] tag).';
+$GLOBALS["hThemeDescription"] = 'Deze omschrijving wordt gebruikt bij de keuzelijst van thema\'s (bij de [themelist] tag).';
+$GLOBALS["hThemeEnabled"] = 'Of dit thema is aangezet of niet.';
+
+//  Error Messages
+$GLOBALS["eNoCode"] 		= 'Dit thema moet een code hebben.';
+$GLOBALS["eInvalidCode"] 	= 'Thema code bevat verkeerde karakters.';
+$GLOBALS["eMasterCode"] 	= 'Deze identificatie code is al in gebruik door het hoofd thema.';
+$GLOBALS["eCodeInUse"] 		= 'Deze identificatie code is al in bebruik door een andere site of thema.';
+$GLOBALS["eNoName"] 		= 'De naam van het thema mag niet leeg zijn.';
+$GLOBALS["eNoDescription"] 	= 'Thema omschrijving mag niet leeg zijn.';
+
+?>
