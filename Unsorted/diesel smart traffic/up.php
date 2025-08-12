66d777f58af85f8abc1bd1648e33d41a9642243c
me-suzy <me.suzana@gmail.com>
2025-08-13 02:35:16 +0300

    Add Unsorted/diesel smart traffic/up.php

---
+++ Unsorted/diesel smart traffic/up.php
@@ -0,0 +1,84 @@
+<?
+	require "conf/sys.conf";
+?>
+
+
+<HTML>
+
+
+<HEAD>
+
+<TITLE>CBSeeker.com - Make Money With Your Own ClickBank® Search Engine!</TITLE>
+<META NAME="AUTHOR" CONTENT="Hits4Profit.com">
+<META NAME="KEYWORDS" CONTENT="Banner Exchange, Free Advertising, Promotion, Web Site Promotions, Register, Banners, Business, Income, Cash, Affiliate Programs, Hosting, Top 10, Links, Url, Promote, Space, Low Cost, opportunity, credits, Increase Hits, Internet, Computer, Home Based, Resources, Get Paid To Surf, Ads, Classifieds, Message Boards, Tell A Friend, Refer, Referral, Mailing List, Join, Login, Software, Create, Impressions, Clicks, Leads, Visitors, 10000, 1:1 ratio, 3368420"> 
+<META NAME="DESCRIPTION" CONTENT="CBSeeker.com - Make Money With Your Own ClickBank® Search Engine!"> 
+<META NAME="GENERATOR" content="Hits4Profit.com">
+<META NAME="ROBOTS" CONTENT="INDEX,FOLLOW">
+
+</HEAD>
+
+<BODY BGCOLOR="#FFFFFF">
+
+
+
+
+
+<table width="420" border="0" cellspacing="0" cellpadding="0"><tr><td colspan="2" width="420">
+
+
+<center><font face="Verdana" size="3"><i>You've Just Discovered An Amazing Program That<br>Can Make You <b>Thousands Of Dollars Per Month!</b></i></font>
+
+
+</td></tr>
+
+
+
+
+
+<tr><td valign="top" width="200"><br>
+
+<a href="http://hop.clickbank.net/?<? echo $CLICKBANK_USER ?>/kocas" target="new"><img src="images/box.jpg" width="200" height="200" alt="Make Money With Your Own ClickBank® Search Engine!" border="0"></a>
+
+</td>
+
+
+
+
+
+<td valign="top" width="220"><br>
+
+<font face="Verdana" size="2">
+
+> Easy Installation - be up &nbsp;&nbsp;&nbsp;and running in 10 minutes!<br>
+
+> Runs on your own server or &nbsp;&nbsp;&nbsp;host!<br>
+
+> Integrates with the look of &nbsp;&nbsp;&nbsp;your own site using templates!<br>
+
+> Your affiliate nickname is &nbsp;&nbsp;&nbsp;embedded into each link!<br>
+
+> Use it as a stand alone site &nbsp;&nbsp;&nbsp;or add it to your current one!<br>
+
+> Just upload, add your &nbsp;&nbsp;&nbsp;affiliate ID, and you're done!<br>
+
+> And Much More...
+
+</font>
+
+</td></tr>
+
+
+
+
+<tr><td colspan="2" align="center"><br>
+
+
+<a href="http://hop.clickbank.net/?<? echo $CLICKBANK_USER ?>/kocas" target="new"><font face="verdana" size="2" color="#0000FF"><i><b>Click Here To Learn More...</b></i></font></a>
+
+
+</td></tr></table>
+
+
+</BODY>
+
+</HTML>
