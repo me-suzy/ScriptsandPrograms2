3caf665e35de6e0db37555ef057d6bf0a30edf21
me-suzy <me.suzana@gmail.com>
2025-08-12 20:05:52 +0300

    Add News Publishing/phpfn/phpfn/Inc/Footer.php

---
+++ News Publishing/phpfn/phpfn/Inc/Footer.php
@@ -0,0 +1,19 @@
+<?php
+
+/*	+--------------------------------------------------------------
+	| PHPFreeNews - News Headlines on your website                |
+	| Developed by Jim Willsher.                                  |
+	| http://www.phpfreenews.co.uk                                |
+	+-------------------------------------------------------------+
+*/
+
+if (! defined('IN_PHPFN'))
+	die('Illegal attempt to access script directly!');
+
+?>
+
+<DIV class="FooterText" align="center">
+	<HR width="740" size="2">
+	News Management System by <A href="http://www.phpfreenews.co.uk" target="_blank">PHPFreeNews</A>, Version V<?= $ScriptVersion ?>
+	<BR>
+</DIV>
