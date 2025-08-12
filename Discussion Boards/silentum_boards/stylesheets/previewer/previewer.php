406e65f261195ab4f30bcd47c157ec8a16d3722f
me-suzy <me.suzana@gmail.com>
2025-08-12 10:53:31 +0300

    Add Discussion Boards/silentum_boards/stylesheets/previewer/previewer.php

---
+++ Discussion Boards/silentum_boards/stylesheets/previewer/previewer.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<!-- previewer.php copyright 2005 "HyperSilence" -->
+<head>
+<link rel="stylesheet" href="../../stylesheets/blue.css" type="text/css" />
+<title>Stylesheet Previewer</title>
+</head>
+<body>
+<object>
+<table align="center" cellspacing="0" style="width: 95%">
+	<tr>
+	<? include("top.php") ?>
+	</tr>
+</table>
+</object><br />
+<object>
+<table align="center" cellspacing="0" style="width: 95%">
+	<tr>
+		<td style="text-align: center"><span class="normal"><strong>Choose a stylesheet above to preview.</strong></span></td>
+	</tr>
+</table>
+</object><br />
+<object>
+<table align="center" cellspacing="0" style="width: 95%">
+	<tr>
+	<? include("bottom.php") ?>
+	</tr>
+</table>
+</object>
+</body>
+</html>
