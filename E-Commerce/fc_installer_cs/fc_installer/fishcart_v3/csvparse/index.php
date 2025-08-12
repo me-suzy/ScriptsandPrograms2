cf0e3583e394fb048b2ae0d70299fb1edcaaaa16
me-suzy <me.suzana@gmail.com>
2025-08-12 12:10:35 +0300

    Add E-Commerce/fc_installer_cs/fc_installer/fishcart_v3/csvparse/index.php

---
+++ E-Commerce/fc_installer_cs/fc_installer/fishcart_v3/csvparse/index.php
@@ -0,0 +1,69 @@
+<?php /*
+FishCart: an online catalog management / shopping system
+Copyright (C) 1997-2002  FishNet, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+USA.
+
+   N. Michael Brennen
+   FishNet(R), Inc.
+   850 S. Greenville, Suite 102
+   Richardson,  TX  75081
+   http://www.fni.com/
+   mbrennen@fni.com
+   voice: 972.669.0041
+   fax:   972.669.8972
+   
+   CSVParse version 2.04 created by Chris Carroll
+   ctcarroll@mindspring.com
+   Completely modified version of CSVParse based on Simon Weller's original work
+*/
+?>
+
+<html>
+<head>
+<title>CSV-Parse</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF">
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr bgcolor="#CCCCCC"> 
+    <td width="98%" height="11">&nbsp;</td>
+    <td colspan="2" height="11" width="2%" bgcolor="#000000">&nbsp;</td>
+  </tr>
+  <tr valign="top"> 
+    <td width="98%" height="62" valign="bottom"><img src="images/cvsparselogo.gif" width="242" height="52"></td>
+    <td colspan="2" height="231" rowspan="2" bgcolor="#CCCCCC" width="2%"> 
+      <p>&nbsp;</p>
+      <p>&nbsp; </p>
+    </td>
+  </tr>
+  <tr valign="top"> 
+    <td width="98%" height="170"> 
+      <p><br>
+        <a href="csvsetup.php">Setup Options</a><br>
+        <a href="csvindex.php">Add Products/Update Products/Delete Products in the Database</a><br>
+        <a href="viewlog.php">View Processing Logs</a><br>
+        <a href="readme.txt">View the Readme file</a><br>
+		<a href="upload.xls">Download the excel template</a> Not updated for this version<br>
+		<a href="upload.csv">Download the CSV template</a> Not updated for this version
+      </p>
+	  <center><b><font face="arial" size="-2">(c)Copyright 2002 by <a href="mailto:ctcarroll@mindspring.com">Chris Carroll</a></b></font>
+	  </center></td>
+  </tr>
+</table>
+</body>
+</html>
