c14974fc085dba5b3661062f361d3ff82cdee5ef
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:05 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view.da.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+/* ELEMENTS FOR BASIC_FIELD */
+/* ELEMENTS FOR BASIC_COLLECTION (and some for hierarchy and list) */
+
+$LANG['context_viewfile'] = 'Vis fil';
+$LANG['context_default'] = 'Sæt til standard';
+$LANG['context_language'] = 'Sprog';
+$LANG['context_createdby'] = 'Oprettet af';
+$LANG['context_checkedby'] = 'Kontrolleret af';
+$LANG['context_changedby'] = 'Redigeret af';
+$LANG['context_created'] = 'Oprettet';
+$LANG['context_checked'] = 'Kontrolleret';
+$LANG['context_changed'] = 'Redigeret';
+$LANG['context_delete'] = 'Slet';
+$LANG['context_publish'] = 'Udgivelse og udløb';
+$LANG['context_createcopy'] = 'Opret kopi';
+$LANG['context_createvariant'] = 'Opret variant';
+$LANG['context_createrevision'] = 'Opret ny revision';
+$LANG['context_requestapproval'] = 'Anmod om godkendelse';
+$LANG['context_approvepublish'] = 'Godkend og udgiv';
+$LANG['context_active'] = 'Aktiver/deaktiver';
+$LANG['context_category'] = 'Tilføj til kategori';
+$LANG['context_filter'] = 'Rapporter';
+$LANG['context_create'] = 'Opret ny';
+$LANG['context_createsub'] = 'Opret underpost';
+$LANG['context_recycle'] = 'Vis Papirkurv';
+$LANG['context_categories'] = 'Kategorier';
+$LANG['context_access'] = 'Rettigheder';
+$LANG['context_advanced'] = 'Avanceret';
+$LANG['context_view'] = 'Vis';
+$LANG['context_edit'] = 'Rediger';
+$LANG['context_properties'] = 'Egenskaber';
+$LANG['context_statistics'] = 'Vis statistik';
+$LANG['context_exportobj'] = 'Eksport til objektpakke';
+$LANG['context_variantcompare'] = 'Fremhæv varianter';
+$LANG['context_preview'] = 'Preview';
+$LANG['select_all'] = 'ALLE';
+$LANG['select_none'] = 'INGEN';
+$LANG['select_owner'] = 'EJER/ADMINISTRATORER/MANAGERE';
+$LANG['select_standard'] = 'STANDARD';
+$LANG['select_category'] = 'Vælg kategori';
+$LANG['select_language'] = 'VÆLG SPROG';
+$LANG['select_country'] = 'VÆLG LAND';
+$LANG['select_table'] = 'VÆLG TABEL';
+$LANG['select_extension'] = 'VÆLG KOMPONENT';
+$LANG['select_file'] = 'VÆLG FIL';
+$LANG['button_cancel'] = 'Fortryd';
+$LANG['button_ok'] = 'OK';
+$LANG['button_save'] = 'Gem';
+$LANG['button_create'] = 'Opret';
+$LANG['button_apply'] = 'Anvend';
+$LANG['_label_category'] = 'Kategorier';
+$LANG['_label_sysaccess'] = 'System Adgang';
+$LANG['_label_webaccess'] = 'Website Adgang';
+$LANG['_label_members'] = 'Medlemmer';
+$LANG['_label_memberof'] = 'Medlem af';
+
+?>
