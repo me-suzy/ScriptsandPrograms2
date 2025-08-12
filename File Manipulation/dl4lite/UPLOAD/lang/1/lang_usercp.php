31c4638a4902d0a5e96b0240fe2d88cc9c2b409f
me-suzy <me.suzana@gmail.com>
2025-08-12 14:16:35 +0300

    Add File Manipulation/dl4lite/UPLOAD/lang/1/lang_usercp.php

---
+++ File Manipulation/dl4lite/UPLOAD/lang/1/lang_usercp.php
@@ -0,0 +1,40 @@
+<?php
+/*********************************************************
+* Name: lang_usercp.php
+* Author: RW::Download 4.0
+* Contact: realworld@blazefans.com
+* Description: Language variables
+* Version: 4.00
+*********************************************************/
+$lang = array(
+
+'home' => "Home",
+'profile' => "Profile",
+'approved' => "Approved Files",
+'unapproved' => "Unapproved Files",
+'limits' => "Your Download Limits",
+'stats' => "Stats",
+'email' => "Email",
+'registered' => "Registered on",
+'group' => "Group",
+'information' => "Information",
+'uploaded' => "Uploaded",
+'downloaded' => "Downloaded",
+'limits_desc' => "This section shows the limits the admin has imposed on you for downloading files. The bandwidth limit is the <b>total</b> size allowed for all of your downloads. The file limits show the total number of files you are allowed to download. The times show how long you have before these limits expire or are reset. If <b>either</b> of your limits reaches zero you will not be able to download files",
+'limits_off' => "Download limits are currently disabled and you can download files without any restrictions",
+'limit_reset_on' => "Reset limits is enabled so when the time counters reach zero your allowed file limits will be reset to the default levels for your user group",
+'limit_reset_off' => "Reset limits is <b>off</b>. When either of your limits reach zero you will not be allowed to download any more files.",
+'bandwidth' => "Remaining bandwidth",
+'bandwidth_time' => "Bandwidth time remaining",
+'filesremain' => "Files remaining",
+'files_time' => "Files time remaining",
+'confirm_email' => "Confirm Email",
+'confirm_password' => "Confirm Password",
+'language' => "Language",
+'skin' => "Skin",
+'your_files' => "Your Files",
+'settingsSaved' => "Your settings have been saved.",
+'filedl' => "File successfully removed. Please remember to add a new download to this file before exiting",
+)
+
+?>
