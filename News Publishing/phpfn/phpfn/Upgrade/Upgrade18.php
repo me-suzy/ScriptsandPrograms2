1c08da3f332d22b1665644eeb055322ca11375ae
me-suzy <me.suzana@gmail.com>
2025-08-12 20:06:56 +0300

    Add News Publishing/phpfn/phpfn/Upgrade/Upgrade18.php

---
+++ News Publishing/phpfn/phpfn/Upgrade/Upgrade18.php
@@ -0,0 +1,83 @@
+<?php
+
+/*	+--------------------------------------------------------------
+	| PHPFreeNews - News Headlines on your website                |
+	| Developed by Jim Willsher.                                  |
+	| http://www.phpfreenews.co.uk                                |
+	+-------------------------------------------------------------+
+*/
+
+require('../Config/Config.php');
+require('../Inc/Functions.php');
+
+$UpgradeScript = $_SERVER['PHP_SELF'];
+$Step = isset($_GET['step']) ? $_GET['step'] : '';
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+	<HEAD>
+		<TITLE><?= $SiteDescription ?> Upgrade to Version 1.8</TITLE>
+			<META name="Author" content="Jim Willsher">
+			<META name="Keywords" content="PHP, News, Headlines, PHPFreeNews">
+			<META name="Description" content="PHP Free News">
+			<LINK rel="stylesheet" href="../Inc/Styles.css" type="text/css" />
+		</HEAD>
+		<BODY>
+			<P class="plaintext"><IMG src="../Inc/Images/<?= $AdminSiteLogo ?>" width="266" height="61"></P>
+			<P class="plaintext">
+
+
+			<?php
+			// =============================================================================================================
+
+			if ($Step == '')
+			{
+				// First Stage
+				?>
+				Welcome to the Version 1.8 Upgrade Script for <B><?= $SiteDescription ?></B>.<BR>
+				<BR>
+				This script will update your database to make it compliant with Version 1.8 of <?= $SiteDescription ?>.
+				<BR>
+				The installer will upgrade database <B>"<?= $db ?>"</B>. Remember to upgrade every database you have.
+				<BR><BR>
+				The MySql User in your Config.php file must have the following permissions: ALTER.<BR>
+				Once the database has been installed you can safely revoke ALTER.
+				<BR>
+				<BR>
+				<A href="<?=$UpgradeScript?>?step=1">Perform Upgrade >></A>
+				<?php
+			}
+
+			// =============================================================================================================
+
+			elseif ($Step == '1')
+			{
+				// Set up the tables...
+				echo ('<br>Updating table: news_posts...');
+				$sql_query = mysql_query("ALTER TABLE news_posts ADD Archived TINYINT DEFAULT '0' NOT NULL");
+				if ($sql_query)
+				{
+					echo ('<b> Done!</b>');
+				}
+				else
+				{
+					echo mysql_error();
+					exit();
+				}
+
+				// ===============================================================================================
+
+				?>
+				<BR>
+				<BR>
+				If there were no errors generated then the Database was updated successfully.<BR>
+				Please now delete this installation file (<?= $_SERVER['PHP_SELF'] ?>) or make it inaccessible. <BR>
+				<BR>
+				<BR>
+				<?php
+			}
+			?>
+		</P>
+	</BODY>
+</HTML>
