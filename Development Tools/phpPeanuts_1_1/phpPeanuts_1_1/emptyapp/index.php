a4d1a6eaa985e0a2883870e58ff3b0a5fa9bd268
me-suzy <me.suzana@gmail.com>
2025-08-12 10:07:34 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/emptyapp/index.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/emptyapp/index.php
@@ -0,0 +1,9 @@
+<?php
+// Copyright (c) the partners of MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+include ("../classes/classSite.php");
+$site = new Site('emptyapp');
+$site->handleRequest();
+	
+?>
