b1739cca8b5775b0afbc169ebbd87ba2d12fe5d8
me-suzy <me.suzana@gmail.com>
2025-08-12 08:23:51 +0300

    Add Contests and Awards/congav1.2.1/congav1.2.1/adminlogin2.php

---
+++ Contests and Awards/congav1.2.1/congav1.2.1/adminlogin2.php
@@ -0,0 +1,76 @@
+<?php
+////////////////////////////////////////////////////////
+// Conga Line Script v1.2.1
+// ©2005 Nathan Bolender www.nathanbolender.com
+// Licensed under the Creative Commons Attribution-NonCommercial-NoDerivs License
+// located at http://creativecommons.org/licenses/by-nc-nd/2.0/
+////////////////////////////////////////////////////////
+
+include ("config.php");
+?>
+<?php include "config.php"; ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Conga Line Script Admin</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+a:link {
+	color: #FF0000;
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+	color: #FF0000;
+}
+a:hover {
+	text-decoration: underline;
+	color: #FF0000;
+}
+a:active {
+	text-decoration: none;
+	color: #FF0000;
+}
+body,td,th {
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: small;
+	color: #000000;
+}
+body {
+	background-color: #FFFFFF;
+}
+-->
+</style></head>
+
+<body>
+<table width="99%"  border="3" align="center" cellpadding="0" cellspacing="0" bordercolor="#000000">
+  <tr>
+    <td bgcolor="#999999"><div align="center"><strong>Conga Line Script Admin</strong></div></td>
+  </tr>
+  <tr>
+    <td height="107" align="center" valign="middle" bgcolor="#CCCCCC"><p>
+	
+	<?php
+	if ($password == $adminpass) {
+	?>
+	<a href="edituser1.php?password=<?php echo "$password"; ?>">Edit users</a>	<br>
+	<br><a href="editsettings1.php?password=<?php echo "$password"; ?>">Edit settings</a>
+	<?php
+	} else {
+	echo "<b>Incorrect password.</b>";
+	}
+	?>
+	
+	
+	
+	
+	</p></td>
+  </tr>
+  <tr>
+    <td bgcolor="#999999"><div align="center">Conga Line Script - &copy;2005 Nathan Bolender - <a href="http://www.nathanbolender.com">nathanbolender.com</a></div></td>
+  </tr>
+</table>
+
+</body>
+</html>
