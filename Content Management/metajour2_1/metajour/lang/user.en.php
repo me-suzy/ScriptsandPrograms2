c880cb7fcd5d3f2413e9dfc9bfdd0fd416d8ed10
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:23 +0300

    Add Content Management/metajour2_1/metajour/lang/user.en.php

---
+++ Content Management/metajour2_1/metajour/lang/user.en.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Users';
+$LANG['label_name'] = 'Name';
+$LANG['label_password'] = 'Password';
+$LANG['label_realname'] = 'Real name';
+$LANG['label_rootdir'] = 'Root directory (obsolete)';
+$LANG['label_profileid'] = 'Profile';
+$LANG['label_objectlanguage'] = 'Record create language';
+$LANG['label_guilanguage'] = 'User interface language';
+$LANG['label_country'] = 'Country';
+$LANG['label_email'] = 'Email';
+$LANG['label_exstr1'] = 'Extra #1';
+$LANG['label_exstr2'] = 'Extra #2';
+$LANG['label_exstr3'] = 'Extra #3';
+$LANG['label_exstr4'] = 'Extra #4';
+$LANG['label_exstr5'] = 'Extra #5';
+$LANG['label_exstr6'] = 'Extra #6';
+$LANG['label_exstr7'] = 'Extra #7';
+$LANG['label_exstr8'] = 'Extra #8';
+$LANG['label_restrictlanguage'] = 'Restrict save to record create language';
+$LANG['label_locale'] = 'Date- and number formats';
+$LANG['label_guilistlanguage'] = 'Language for lists';
+$LANG['label_app'] = 'Initial application';
+$LANG['label_appavail'] = 'Application access';
+$LANG['label_oldeditor'] = 'Use old editor';
+?>
