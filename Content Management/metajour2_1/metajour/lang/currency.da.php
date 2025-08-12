652d7451fc20c84624721f23b346e996ba485e9e
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:05 +0300

    Add Content Management/metajour2_1/metajour/lang/currency.da.php

---
+++ Content Management/metajour2_1/metajour/lang/currency.da.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Valuta';
+$LANG['label_name'] = 'Navn';
+$LANG['label_currency'] = 'Faktor';
+$LANG['label_standard'] = '';
+?>
