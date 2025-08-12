65e34fca6971904a261c68010d48f42909686e99
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:23 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_category.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_category.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Tilføj til kategori';
+$LANG['text_1'] = 'Vælg kategorier som de valgte elementer <br>skal være medlem af:';
+$LANG['text_2'] = 'Hold Ctrl nede for at vælge mere end 1 kategori';
+?>
