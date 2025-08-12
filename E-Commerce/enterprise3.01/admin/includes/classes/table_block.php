f2d0ba1affa0330a0e75a50ad3e8e96831c880ab
me-suzy <me.suzana@gmail.com>
2025-08-12 11:39:44 +0300

    Add E-Commerce/enterprise3.01/admin/includes/classes/table_block.php

---
+++ E-Commerce/enterprise3.01/admin/includes/classes/table_block.php
@@ -0,0 +1,79 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  class tableBlock {
+    var $table_border = '0';
+    var $table_width = '100%';
+    var $table_cellspacing = '0';
+    var $table_cellpadding = '2';
+    var $table_parameters = '';
+    var $table_row_parameters = '';
+    var $table_data_parameters = '';
+
+    function tableBlock($contents) {
+      $tableBox_string = '';
+
+      $form_set = false;
+      if (isset($contents['form'])) {
+        $tableBox_string .= $contents['form'] . "\n";
+        $form_set = true;
+        array_shift($contents);
+      }
+
+      $tableBox_string .= '<table border="' . $this->table_border . '" width="' . $this->table_width . '" cellspacing="' . $this->table_cellspacing . '" cellpadding="' . $this->table_cellpadding . '"';
+      if (escs_not_null($this->table_parameters)) $tableBox_string .= ' ' . $this->table_parameters;
+      $tableBox_string .= '>' . "\n";
+
+      for ($i=0, $n=sizeof($contents); $i<$n; $i++) {
+        $tableBox_string .= '  <tr';
+        if (escs_not_null($this->table_row_parameters)) $tableBox_string .= ' ' . $this->table_row_parameters;
+        if (isset($contents[$i]['params']) && escs_not_null($contents[$i]['params'])) $tableBox_string .= ' ' . $contents[$i]['params'];
+        $tableBox_string .= '>' . "\n";
+
+        if (isset($contents[$i][0]) && is_array($contents[$i][0])) {
+          for ($x=0, $y=sizeof($contents[$i]); $x<$y; $x++) {
+            if (isset($contents[$i][$x]['text']) && escs_not_null(isset($contents[$i][$x]['text']))) {
+              $tableBox_string .= '    <td';
+              if (isset($contents[$i][$x]['align']) && escs_not_null($contents[$i][$x]['align'])) $tableBox_string .= ' align="' . $contents[$i][$x]['align'] . '"';
+              if (isset($contents[$i][$x]['params']) && escs_not_null(isset($contents[$i][$x]['params']))) {
+                $tableBox_string .= ' ' . $contents[$i][$x]['params'];
+              } elseif (escs_not_null($this->table_data_parameters)) {
+                $tableBox_string .= ' ' . $this->table_data_parameters;
+              }
+              $tableBox_string .= '>';
+              if (isset($contents[$i][$x]['form']) && escs_not_null($contents[$i][$x]['form'])) $tableBox_string .= $contents[$i][$x]['form'];
+              $tableBox_string .= $contents[$i][$x]['text'];
+              if (isset($contents[$i][$x]['form']) && escs_not_null($contents[$i][$x]['form'])) $tableBox_string .= '</form>';
+              $tableBox_string .= '</td>' . "\n";
+            }
+          }
+        } else {
+          $tableBox_string .= '    <td';
+          if (isset($contents[$i]['align']) && escs_not_null($contents[$i]['align'])) $tableBox_string .= ' align="' . $contents[$i]['align'] . '"';
+          if (isset($contents[$i]['params']) && escs_not_null($contents[$i]['params'])) {
+            $tableBox_string .= ' ' . $contents[$i]['params'];
+          } elseif (escs_not_null($this->table_data_parameters)) {
+            $tableBox_string .= ' ' . $this->table_data_parameters;
+          }
+          $tableBox_string .= '>' . $contents[$i]['text'] . '</td>' . "\n";
+        }
+
+        $tableBox_string .= '  </tr>' . "\n";
+      }
+
+      $tableBox_string .= '</table>' . "\n";
+
+      if ($form_set == true) $tableBox_string .= '</form>' . "\n";
+
+      return $tableBox_string;
+    }
+  }
+?>
