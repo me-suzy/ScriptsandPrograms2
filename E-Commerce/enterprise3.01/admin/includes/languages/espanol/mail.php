3d7db9303ef1534d6e8def3b0f8ea5beb21bd219
me-suzy <me.suzana@gmail.com>
2025-08-12 11:44:26 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/espanol/mail.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/espanol/mail.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Enviar Email a Cliente(s)');
+
+define('TEXT_CUSTOMER', 'Cliente:');
+define('TEXT_SUBJECT', 'Asunto:');
+define('TEXT_FROM', 'Desde:');
+define('TEXT_MESSAGE', 'Mensaje:');
+define('TEXT_SELECT_CUSTOMER', 'Seleccionar Cliente');
+define('TEXT_ALL_CUSTOMERS', 'Todos los Clientes');
+define('TEXT_NEWSLETTER_CUSTOMERS', 'Todos los Suscritos');
+
+define('NOTICE_EMAIL_SENT_TO', 'Aviso: Email enviado a: %s');
+define('ERROR_NO_CUSTOMER_SELECTED', 'Error: No ha seleccionado ningun cliente.');
+?>
