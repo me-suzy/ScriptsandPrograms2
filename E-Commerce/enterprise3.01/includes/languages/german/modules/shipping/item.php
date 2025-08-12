50d651405d7f3bc273b77ed488ad7c0e93c10839
me-suzy <me.suzana@gmail.com>
2025-08-12 12:01:40 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/modules/shipping/item.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/modules/shipping/item.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_ITEM_TEXT_TITLE', 'Versandkosten pro St&uuml;ck');
+define('MODULE_SHIPPING_ITEM_TEXT_DESCRIPTION', 'Versandkosten pro St&uuml;ck');
+define('MODULE_SHIPPING_ITEM_TEXT_WAY', '');
+?>
