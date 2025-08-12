ed72cdaaf6e5487bdb388d1fe153f0979f257ca8
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:37 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_imageformats.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_imageformats.php
@@ -0,0 +1,39 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] 		= 'Beheer afbeelding templates';
+
+//  List Headings
+$GLOBALS["tName"] 		= 'Naam';
+$GLOBALS["tBorder"] 		= 'Rand';
+$GLOBALS["tAlignment"] 		= 'Uitlijnen';
+$GLOBALS["tColour"] 		= 'Kleur';
+
+//  List Functions
+$GLOBALS["tAddNewIFTemplate"] = 'Nieuw afbeelding template toevoegen';
+$GLOBALS["tViewIFTemplate"] 	= 'Toon afbeelding template';
+$GLOBALS["tEditIFTemplate"] 	= 'Bewerk afbeelding template';
+$GLOBALS["tDeleteIFTemplate"] = 'Verwijder afbeelding template';
+
+//  Form Block Titles
+$GLOBALS["thIFGeneral"] 	= 'Afbeelding template Details';
+
+//  Form Field Headings
+$GLOBALS["tIFName"] 		= 'Naam afbeelding template';
+$GLOBALS["tIFAlign"] 		= 'Uitlijning';
+$GLOBALS["tIFBorder"] 		= 'Rand dikte';
+$GLOBALS["tIFBackground"] 	= 'Achtergrondkleur';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] 		= 'Met dit formulier kunt u templates maken voor uw afbeelding.';
+$GLOBALS["hIFName"] 		= 'De naam die gebruikt wordt in de [image] tag.';
+$GLOBALS["hIFAlign"] 		= 'De positie waar de afbeelding geplaatst wordt: Links, Rechts of Midden.';
+$GLOBALS["hIFBorder"] 		= 'De grootte van de rand die gebruikt wordt.';
+$GLOBALS["hIFBackground"] 	= 'Achtergrond kleur voor de afbeelding.';
+
+//  Error Messages
+$GLOBALS["eNoName"] 		= 'Afbeelding template moet een naam hebben.';
+$GLOBALS["eNameExists"] 	= 'Een afbeelding template met deze naam bestaat al, kies a.u.b. een andere.';
+$GLOBALS["eColourWrong"] 	= 'Achtergrondkleur is niet in orde.';
+
+?>
