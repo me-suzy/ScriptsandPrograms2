4dc257d072d4c0548cfc925148105422f6f68bdf
me-suzy <me.suzana@gmail.com>
2025-08-12 19:51:07 +0300

    Add News Publishing/NewMan147/NewMan147/newman/languages/lang-norwegian.php

---
+++ News Publishing/NewMan147/NewMan147/newman/languages/lang-norwegian.php
@@ -0,0 +1,119 @@
+<?php
+  //******************************************************************************************
+  //**                                                                                      **
+  //** phpNewsManager v1.30                                                                 **
+  //** contact: gregor@klevze.si                                                            **
+  //** Last edited: 10th.June,2002                                                          **
+  //**                                                                                      **
+  //** Language module: English                                                             **
+  //******************************************************************************************
+
+define("_CHARSET","ISO-8859-1");
+define("_STATISTICS","Statestikk");
+define("_ACTUAL","Faktisk");
+define("_LASTVISIT","Siste besøkende");
+define("_NEW","Ny");
+define("_ONLINENEWS","Nyheter Online");
+define("_WEEKLYPOLLQ","Ukas Spørsmål");
+define("_WEEKLYPOLLA","Ukas Resultat");
+define("_PUBLICNEWS","Åpne Nyheter");
+define("_REGISTEREDUSERS","Registrerte brukere");
+define("_NEWSCOMMENTS","Nyhetskommentarer");
+define("_ADDNEWS","Legg til nyheter");
+define("_SUBMITEDNEWS","Innsendte nyheter");
+define("_OPTIONS","Valgmuligheter");
+define("_NEWSHEADLINE","Hovednyhet");
+define("_AUTHOR","Forfatter");
+define("_DATE","Dato");
+define("_SECTION","Seksjon");
+define("_PREVIEW","Forhåndsvisning");
+define("_MESSAGE","Melding");
+define("_SUBMIT","Send inn");
+define("_HOME","Hjem");
+define("_NEWSMANAGER","Nyhetsmeny");
+define("_MODIFYNEWS","Endre Nyhet");
+define("_ADDNEWS","Legg til Nyhet");
+define("_ADDPARTNERS","Legg til Partner");
+define("_UPLOADLOGO","Last opp logo");
+define("_IMAGE","Bilde");
+define("_NAME","Navn");
+define("_NUMBER","#");define("_","#");
+define("_CATEGORYNAME","Kategorinavn");
+define("_MAINMENU","Hovedmeny");
+define("_KEEPIT","Lagre det!");
+define("_DELETEIT","Kaste det!");
+define("_EDITCATEGORY","Andre Kategorier");
+define("_UPLOADPICTURE","Laste opp bilde");
+define("_ADDCATEGORY","Legg til kategori");
+define("_OURPARTNERS","Våre Partnere");
+define("_SELECTPAGE","Velg side");
+define("_LINK","Lenke");
+define("_IN","In");
+define("_OUT","Ut");
+define("_AFFILIATES","Samarbeidspartner");
+define("_GFX","Grafikk");
+define("_PREVIOUS","Forrige");
+define("_NEXT","Neste");
+define("_DESCRIPTION","Beskrivelse");
+define("_MODIFYPARTNERS","Endre Partnere");
+define("_RSSSETTINGS","RSS Instillinger");
+define("_NAMEOFRSSFILE","RSS filens navn");
+define("_NUMBEROFNEWS","Antall nyheter");
+define("_RSSTITLE","RSS Tittel");
+define("_RSSLINK","RSS Lenke");
+define("_RSSDESCRIPTION","RSS Beskrivelse");
+define("_AUTOCREATERSS","Lage en RSS fil automatiskt etter hver nyhetsendring");
+define("_OPTIMIZED","Optimisert");
+define("_OPTIMIZEDATABASE","Optimisere DB");
+define("_ADDSMILEY","Legg til Smiley");
+define("_UPLOADSMILEY","Laste opp Smiley");
+define("_SMILEYEMOTION","Følelse");
+define("_SMILEYEDITOR","Smiley Editor");
+define("_SMILEYCODE","Kode");
+define("_EDITSMILEY","Endre Smiley");
+define("_DELETESMILEY","Fjern Smiley");
+define("_NEWS","Nyheter");
+define("_CATEGORY","Kategori");
+define("_PARTNERS","Partnere");
+define("_WEEKLYPOLL","Ukentlig Poll");
+define("_SMILEYS","Smileys");
+define("_BROWSENEWS","Se gjennom nyheter");
+define("_GROUPS","Grupper");
+define("_ADMINS","Admins");
+define("_USERS","Brukere");
+define("_USERINFO","Bruker Info");
+define("_ADMINISTRATION","Administrasjon");
+define("_YOULOGOUT","Du har akkurat logget ut.");
+define("_LOGIN","Logg inn");
+define("_PASSWORD","Passord");
+define("_USERNAME","Brukernavn");
+define("_WEBCONTROLPANEL","Web Kontrollpanel");
+define("_ADDUSER","Legg til Bruker");
+define("_EMAIL","E-post");
+define("_PRIVILEGES","Fordeler");
+define("_EMAIL","E-post");
+define("_EDITUSER","Endr Bruker");
+define("_DELETEUSER","Slett Bruker");
+define("_INFO","Info");
+define("_ADDGROUP","Legg til Gruppe");
+define("_ADMIN","Admin");
+define("_RSS","RSS");
+define("_MODIFY","Endr");
+define("_ADD","Legg til");
+define("_EDIT","Rediger");
+define("_DELETE","Slett");
+define("_UPLOAD","Last opp");
+define("_GROUPNAME","Gruppenavn");
+define("_SELECT","Velg");
+define("_ADDWEEKLYPOLL","Legg til Ukentlig Poll");
+define("_SUBMITEDPOLLS","Innsendte Polls");
+define("_QUESTION","Spørsmål");
+define("_ANSWERS","Svar");
+define("_SEPERATEANSWERSWITH","Skill svar med");
+define("_EDITWEEKLYPOLL","Rediger Ukentlig Poll");
+define("_SUCCESS","Suksess");
+define("_DELETENEWS","Slettt Nyhet");
+define("_NOTENOUGHPRIV","Du har ikke tilgang nok!");
+define("_LANGUAGE","Språk");
+define("_CONTENT","Innhold");
+?>
