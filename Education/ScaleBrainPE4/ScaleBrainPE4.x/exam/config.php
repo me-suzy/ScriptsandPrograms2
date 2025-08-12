f7127ad81eacd65375cf7ac5d0c720e5ebb3f03c
me-suzy <me.suzana@gmail.com>
2025-08-12 13:00:03 +0300

    Add Education/ScaleBrainPE4/ScaleBrainPE4.x/exam/config.php

---
+++ Education/ScaleBrainPE4/ScaleBrainPE4.x/exam/config.php
@@ -0,0 +1,28 @@
+<?
+@extract($_COOKIE);
+@extract($_ENV);
+@extract($_REQUEST);
+@extract($_FILES);
+@extract($_GET);
+@extract($_POST);
+@extract($_SERVER);
+@extract($_SESSION);
+
+$apppath="/exampack/personal";
+$basepath = "E:/Documents and Settings/pawan/Desktop/web";
+$webpath="http://localhost/pawan";
+$webpath1="http://www.localhost/pawan";
+
+$DBHost = "localhost";
+$DBUser = "pawan";
+$DBPassword = "pawan";
+
+$DBName = "test_personal";
+$db=mysql_connect("$DBHost", "$DBUser", "$DBPassword");
+$result = mysql_select_db ("$DBName");
+
+session_start();
+
+require_once($basepath.$apppath."/cfgn/defines.php");
+require_once($basepath.$apppath."/cfgn/includes.php");
+?>
