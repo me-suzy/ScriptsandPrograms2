23b1599b1eceb6b479bcfe80405bcb4ca2e7630c
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:32 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_preferences.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_preferences.php
@@ -0,0 +1,80 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = '&Auml;ndern Sie Ihre Voreinstellungen';
+$GLOBALS["tFormTitle2"] = 'Geben Sie Registrierungsdetails ein';
+
+//  List Headings
+$GLOBALS["tUsername"] = 'Username';
+$GLOBALS["tEMail"] = 'E-Mail Addresse';
+
+//  Form Field Headings
+$GLOBALS["tLoginName"] = 'Login Name';
+$GLOBALS["tRetainCookie"] = 'Cookie speichern';
+$GLOBALS["tPassword"] = 'Passwort';
+$GLOBALS["tRetypePassword"] = 'Passwort wiederholen';
+$GLOBALS["tAuthorname"] = 'Ihr Name';
+$GLOBALS["tUsergroup"] = 'User Gruppe';
+$GLOBALS["tEMail"] = 'E-mail Addresse';
+$GLOBALS["tCountry"] = 'Land';
+$GLOBALS["tLanguage"] = 'Sprache';
+$GLOBALS["tPhone"] = 'Telefon';
+$GLOBALS["tFax"] = 'Fax';
+$GLOBALS["tAddress"] = 'Addresse';
+$GLOBALS["tCity"] = 'Stadt';
+$GLOBALS["tState"] = 'Bundesland';
+$GLOBALS["tZip"] = 'PLZ';
+$GLOBALS["tWebsite"] = 'Website';
+$GLOBALS["tNewsletter"] = 'Newsletter';
+$GLOBALS["tComments"] = 'Kommentare';
+$GLOBALS["tRegisterIntroText"] = 'Um Zugang zu den gesch&uuml;tzten Bereichen der Webseite zu erhalten, m&uuml;ssen Sie sich registrieren.';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["hLoginName"] = 'Dies ist Ihr Login-Name, der bei der Passwort-Abfrage verwendet wird.';
+$GLOBALS["hRetainCookie"] = 'Wenn Sie diesen Haken setzen, wird ein Cookie mit Ihrem Login-Namen auf Ihrem Computer gespeichert, so dass Sie in Zukunft nur noch das Passwort eingeben m&uuml;ssen. Das Cookie hat eine G&uuml;ltigkeit von 1 Jahr. Danach muss es erneuert werden, in dem Sie wieder Ihre Voreinstellungen &auml;ndern und diese Box erneut anhaken.';
+$GLOBALS["hPassword"] = 'Geben Sie das Passwort ein, mit dem Sie sich in ezContents einloggen werden.';
+$GLOBALS["hRetypePassword"] = 'Wiederholen Sie das Passwort, um Schreibfehler auszuschliessen.';
+$GLOBALS["hAuthorname"] = 'Dieses Feld kann einen vollen Namen oder einen Nicknamen enthalten. Dieser wird unter jedem ver&ouml;ffentlichten Atikel angezeigt, der von Ihnen geschrieben wurde.';
+$GLOBALS["hUsergroup"] = 'Die User-Gruppe, der Sie angeh&ouml;ren. (Verschiedene Berechtigungen k&ouml;nnen hiermit gesteuert werden)';
+$GLOBALS["hEMail"] = 'Ihre Email-Adresse. Zusammen mit Ihrem Namen wird damit ein Link erstellt, mit dem Besucher der Seite Ihnen eine Nachricht zur Artikeln senden k&ouml;nnen, die Sie erstellt haben.';
+$GLOBALS["hCountry"] = 'Das Land, in dem Sie leben.';
+$GLOBALS["hLanguage"] = 'Ihre Muttersprache (optional).<br />Wenn diese gesetzt wurde, k&ouml;nnen Sie Men&uuml;-Optionen, Feldnamen, Hilfe, etc. in dieser Sprache sehen, anstatt in der Standard-Sprache f&uuml;r die Webseite.';
+$GLOBALS["hPhone"] = 'Ihre Telefon-Nummer';
+$GLOBALS["hFax"] = 'Ihre Faxnummer';
+$GLOBALS["hAddress"] = 'Ihre Mailing-Adresse';
+$GLOBALS["hCity"] = 'Die Stadt, in der Sie leben.';
+$GLOBALS["hState"] = 'Das Bundesland, in dem Sie leben';
+$GLOBALS["hZip"] = 'Ihre PLZ.';
+$GLOBALS["hWebsite"] = 'Link auf Ihre Webseite';
+$GLOBALS["hNewsletter"] = 'M&ouml;chten Sie den Newsletter erhalten?';
+$GLOBALS["hComments"] = 'Wenn Sie Kommentare zum Inhalt haben, k&ouml;nnen Sie diese hier mit anderen Usern und den Administratoren f&uuml;r diese Seite teilen und hier hineinschreiben.';
+
+//  Error Messages
+$GLOBALS["eLoginExists"] = 'Der Login-Name ist bereits vorhanden. Bitte w&auml;hlen Sie einen anderen.';
+$GLOBALS["eLoginEmpty"] = 'Der Login-Name kann nicht leer gelassen werden.';
+$GLOBALS["ePasswordError"] = 'Das wiederholte Passwort stimmt nicht mit dem ersten &uuml;berein. Bitte versuchen Sie es nochmal.';
+$GLOBALS["eNameEmpty"] = 'Ihr Name darf nicht leer gelassen werden.';
+$GLOBALS["eEMailEmpty"] = 'Ihre Email-Adresse ist nicht eingegeben oder nicht korrekt.';
+$GLOBALS["ePhoneEmpty"] = 'Das Telefon-Feld darf nicht leer gelassen werden.';
+$GLOBALS["eCountryEmpty"] = 'Es wurde kein Land gew&auml;hlt.';
+$GLOBALS["eFaxEmpty"] = 'In das Fax-Feld wurde nichts eingetragen.';
+$GLOBALS["eAddressEmpty"] = 'Address-Feld darf nicht leer gelassen werden.';
+$GLOBALS["eCityEmpty"] = 'Das Stadt-Feld darf nicht leer gelassen werden.';
+$GLOBALS["eStateEmpty"] = 'Bundesland-Feld darf nicht leer gelassen werden.';
+$GLOBALS["eZipEmpty"] = 'PLZ-Feld darf nicht leer gelassen werden.';
+$GLOBALS["eWebsiteEmpty"] = 'Das Webseite-Feld ist verbindlich.';
+$GLOBALS["eCommentsEmpty"] = 'Das Kommentare-Feld ist verbindlich.';
+
+
+$GLOBALS["tPrefsUpdated"] = 'Ihre User-Einstellungen wurden aktualisiert.';
+$GLOBALS["tPrefsRegistered"] = 'Sie wurden als neuer User registriert.';
+$GLOBALS["tReturnMailSubjest"] = 'Ihre Registrierung';
+$GLOBALS["tReturnMailBody"] = 'Vielen Dank f&uuml;r Ihre Registrierung auf unserer ezContents-administrierten Webseite. Sie habe nun Zugang zu den gesch&uuml;tzten Teilen der Seite, die einen Usernamen und ein Passwort ben&ouml;tigen.';
+$GLOBALS["tServerUserEmail"] = 'E-mail Server User';
+$GLOBALS["tRegister"] = 'Registrieren';
+$GLOBALS["tUpdate"] = 'Aktualisieren';
+
+$GLOBALS["tReturnMailSubject"] = 'Vielen Dank f&uuml;r Ihre Registrierung';
+$GLOBALS["tReturnMailBody"] = 'Unten sehen Sie Ihre Informationen zur Registrierung.';
+
+?>
