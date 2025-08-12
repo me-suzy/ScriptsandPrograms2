586357ffcedaf7cd095f11df81da5f902eeb64af
me-suzy <me.suzana@gmail.com>
2025-08-12 16:09:50 +0300

    Add Form Processors/FH3-v1.2/FH3/language/cs.php

---
+++ Form Processors/FH3-v1.2/FH3/language/cs.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * ?? language file
+ *
+ * @author Vojtech Draxl
+ * @package FormHandler
+ */
+
+// Thanks for translating
+$fh_lang = array(
+  1  => 'Leden',
+  2  => 'Ãnor',
+  3  => 'BÅezen',
+  4  => 'Duben',
+  5  => 'KvÄten',
+  6  => 'Äerven',
+  7  => 'Äervenec',
+  8  => 'Srpen',
+  9  => 'ZÃ¡ÅÃ­',
+  10 => 'ÅÃ­jen',
+  11 => 'Listopad',
+  12 => 'Prosinec',
+  13 => 'ZadanÃ© datum nenÃ­ platnÃ©!',
+  14 => 'Nezadal/a jste platnou hodnotu pro toto pole!',
+  15 => 'Hesla navzÃ¡jem nesouhlasÃ­!',
+  16 => 'Tato pole je nutnÃ© vyplnit!',
+  17 => 'Hesla musÃ­ bÃ½t aspoÅ %d znakÅ¯ dlouhÃ¡!',
+  18 => 'Heslo obsahuje nepovolenÃ© znaky!',
+  19 => 'Toto pole vyplÅte pouze pokud chcete zmÄnit zadanou hodnotu (<a href="%path%%filename%" target="_blank">%filename%</a>)<br />',
+  20 => 'Pouze nÃ¡sledujÃ­cÃ­ pÅÃ­pony jsou povoleny: %s.',
+  21 => 'Soubor, kterÃ½ se snaÅ¾Ã­te nahrÃ¡t, jiÅ¾ existuje!',
+  22 => 'MusÃ­te nahrÃ¡t soubor!',
+  23 => 'MaximÃ¡lnÃ­ velikost souboru %s kb pÅekroÄ?ena',
+  24 => 'NÄco se pÅihodilo. Zkuste to, prosÃ­m, znovu!',
+  25 => '<small>Tato pole pouÅ¾ij pouze pokud chceÅ¡ zmÄnit svÃ© heslo</small><br />',
+  26 => 'Odeslat',
+  27 => 'Vymazat',
+  28 => 'ZruÅ¡it',
+  29 => 'PÅÃ­stupnÃ©',
+  30 => 'VybranÃ©',
+  31 => 'Typ nahrÃ¡vanÃ©ho souboru je neplatnÃ½!',
+  32 => 'RozmÄry obrÃ¡zku mohou bÃ½t %d x %d Ä?i mÃ©nÄ. NahranÃ½ obrÃ¡zek mÃ¡ rozmÄry %d x %d!',
+  33 => 'ProtoÅ¾e formulÃ¡Å nenÃ­ platnÃ½, soubor <b>%filename%</b> musÃ­ bÃ½t vybrÃ¡n znovu jestli mÃ¡ bÃ½t poslÃ¡n s formulÃ¡Åem!<br />',
+  34 => 'Vyber prvek k pÅesunutÃ­ do %s boxu nebo klikni dvakrÃ¡t k pÅesunutÃ­ vÅ¡ech prvkÅ¯',
+  35 => 'Toto pole musÃ­ bÃ½t jedineÄ?nÃ©. Hodnota "%s" jiÅ¾ existuje v databÃ¡zi!',
+  36 => '<b>%d</b> characters remaining on your input limit',
+  37 => 'You have to supply a file with an extension!',
+  38 => 'Back',
+  39 => 'The combination of the fields "%s" has to be unique. This combination already exists in the database! (Violation of index "%s")'
+);
+
+?>
