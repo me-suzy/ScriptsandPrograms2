7d1a440797b6418ef5f0569dbbdb436de743bdd1
me-suzy <me.suzana@gmail.com>
2025-08-12 08:20:25 +0300

    Add Content Management/wizard/wizard/admin/menuSitemap/rebuildMenuPro.php

---
+++ Content Management/wizard/wizard/admin/menuSitemap/rebuildMenuPro.php
@@ -0,0 +1,40 @@
+<?php
+
+/*  
+   Rebuild Admin Menu Selector
+   (c) 2005 Philip Shaddock, www.wizardinteractive.com
+	This file is part of the Wizard Site Framework.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    It is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the Wizard Site Framework; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+include '../../inc/config_cms/configuration.php';	
+include '../../inc/db/db.php';	
+
+
+/*
+*  Builds new menu arrays and stores them in _menuData database tables
+*/
+
+include '../../inc/functions/store_menu_array.php';
+
+
+
+
+$message = "The menu was rebuilt.";
+$location = CMS_WWW . "/admin.php?id=2&item=19&sub=20&message=$message";
+header("Location: $location");
+
+?>
