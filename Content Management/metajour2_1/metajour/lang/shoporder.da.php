43fd265255db640c7c6fc5315f10f689eae5c7e1
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:00 +0300

    Add Content Management/metajour2_1/metajour/lang/shoporder.da.php

---
+++ Content Management/metajour2_1/metajour/lang/shoporder.da.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Kundeordrer (webshop)';
+$LANG['label_name'] = 'Navn';
+$LANG['label_shippingid'] = 'Fragt';
+$LANG['label_paymentid'] = 'Betaling';
+$LANG['label_customerid'] = 'Kunde';
+$LANG['label_userid'] = '';
+$LANG['label_totalsum'] = '';
+$LANG['label_totaldiscsum'] = '';
+$LANG['label_totalactsum'] = 'Excl moms';
+$LANG['label_totalsumvat'] = '';
+$LANG['label_totaldiscsumvat'] = '';
+$LANG['label_totalactsumvat'] = 'Incl moms';
+$LANG['label_total'] = '';
+$LANG['label_totalvat'] = '';
+$LANG['label_shippingprice'] = '';
+$LANG['label_shippingpricevat'] = '';
+$LANG['label_paymentprice'] = '';
+$LANG['label_paymentpricevat'] = '';
+$LANG['label_totalsumcur'] = '';
+$LANG['label_totaldiscsumcur'] = '';
+$LANG['label_totalactsumcur'] = '';
+$LANG['label_totalsumcurvat'] = '';
+$LANG['label_totaldiscsumcurvat'] = '';
+$LANG['label_totalactsumcurvat'] = '';
+$LANG['label_totalcur'] = '';
+$LANG['label_totalcurvat'] = '';
+$LANG['label_shippingpricecur'] = '';
+$LANG['label_shippingpricecurvat'] = '';
+$LANG['label_paymentpricecur'] = '';
+$LANG['label_paymentpricecurvat'] = '';
+?>
