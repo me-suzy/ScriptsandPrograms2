cd9d00570a06266b304f42c0661e90246d45069e
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:21 +0300

    Add Content Management/metajour2_1/metajour/lang/user.da.php

---
+++ Content Management/metajour2_1/metajour/lang/user.da.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Brugere';
+$LANG['label_name'] = 'Brugernavn';
+$LANG['label_password'] = 'Kodeord';
+$LANG['label_realname'] = 'Rigtigt navn';
+$LANG['label_rootdir'] = 'Root bibliotek (udgår)';
+$LANG['label_profileid'] = 'Profil';
+$LANG['label_objectlanguage'] = 'Post-oprettelses sprog';
+$LANG['label_guilanguage'] = 'Brugerflade sprog';
+$LANG['label_country'] = 'Land';
+$LANG['label_email'] = 'Email';
+$LANG['label_exstr1'] = 'Ekstra #1';
+$LANG['label_exstr2'] = 'Ekstra #2';
+$LANG['label_exstr3'] = 'Ekstra #3';
+$LANG['label_exstr4'] = 'Ekstra #4';
+$LANG['label_exstr5'] = 'Ekstra #5';
+$LANG['label_exstr6'] = 'Ekstra #6';
+$LANG['label_exstr7'] = 'Ekstra #7';
+$LANG['label_exstr8'] = 'Ekstra #8';
+$LANG['label_restrictlanguage'] = 'Begræns gem til poster på post-oprettelsessprog';
+$LANG['label_locale'] = 'Dato- og talformater';
+$LANG['label_guilistlanguage'] = 'Sprog for liste-visninger';
+$LANG['label_app'] = 'Start applikation';
+$LANG['label_appavail'] = 'Applikationsadgang';
+$LANG['label_oldeditor'] = 'Brug gammel editor';
+?>
