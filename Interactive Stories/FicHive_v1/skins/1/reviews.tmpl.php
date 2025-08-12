74cf13461551acb5278c1789658cd8013a1cf1b6
me-suzy <me.suzana@gmail.com>
2025-08-12 18:47:29 +0300

    Add Interactive Stories/FicHive_v1/skins/1/reviews.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/reviews.tmpl.php
@@ -0,0 +1,4 @@
+<table border='0' width='100%'>
+<tr><td width='70%' class='frame'><%RNAME%></td><td class='fframe'><%RDATE%></td><td width='5%'><div align='right'># <%RID%></div></td></tr>
+<tr><td colspan='3' class='frame'><%RCOMMENT%></td></tr>
+</table>
