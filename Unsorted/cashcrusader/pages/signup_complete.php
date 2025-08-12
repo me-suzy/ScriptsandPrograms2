16dcb10025db48abecf53d0b06048e8728616699
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:33 +0300

    Add Unsorted/cashcrusader/pages/signup_complete.php

---
+++ Unsorted/cashcrusader/pages/signup_complete.php
@@ -0,0 +1,15 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr> 
+<td align="center"> <font size="5"><br>
+<br>
+<font size="4">You have completed your signup.<br>
+You may now log into your account.<br>
+</font></font><a href=/pages/enter.php><b>Click here to login</b></a><br>
+<br>
+</td>
+</tr>
+</table>
+<br>
+<? include("footer.php");?>
