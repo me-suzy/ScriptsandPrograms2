bbc8170253d483bcc6253fe81d65fee671131e69
me-suzy <me.suzana@gmail.com>
2025-08-12 23:28:18 +0300

    Add Postcards/webcards_1_final_fixed/webcards_1_final_fixed/upload/lang/English/admin/ad_ban.php

---
+++ Postcards/webcards_1_final_fixed/webcards_1_final_fixed/upload/lang/English/admin/ad_ban.php
@@ -0,0 +1,25 @@
+<?php
+$lang['update_email_ban'] = "Update Email Ban Filters";
+$lang['ban_exp_1'] = "Any email addresses that you enter here will not be allowed to recieve a Webcard.";
+$lang['ban_exp_2'] = "This system should be used to block both people who have abused the Webcard system and people who have requested to have their email addresses blocked.";
+$lang['enter_one_email'] = "Enter one email address on each line with no whitespace";
+$lang['update_filter'] = "Update Filter";
+$lang['update_ip_ban'] = "Update IP Ban Filters";
+$lang['ip_filter'] = "IP Filter";
+$lang['ip_exp_1'] = "Any IP adresses you enter here will be prevented from sending or recieving a WebCard.";
+$lang['ip_exp_2'] = "If a user with a banned IP address is sent a WebCard, they will not be able to retrieve it.";
+$lang['ip_exp_3'] = "Enter one IP address on each line with no whitespace";
+$lang['ip_tools'] = "IP Address Tools";
+$lang['ip_tools_exp_1'] = "The following tool can be used to track down malicious users and people who spam using your Webcards system. Use it in conjunction with the";
+$lang['ip_tools_exp_2'] = "Fill in the box below to locate WebCards composed by a user with the IP address you specify";
+$lang['use_star'] = "You can use * as a wildcard at the end of the address to search for.";
+$lang['search'] = "Search";
+$lang['no_ip_entered'] = "You did not enter an IP address to search";
+$lang['back_enter_ip'] = "Go back and enter an IP";
+$lang['matches_shown_below'] = "The cards listed below were all sent by the IP address you specified to search for:";
+$lang['date_sent'] = "Date Sent";
+$lang['sender'] = "Sender";
+$lang['sender_ip'] = "Sender IP";
+$lang['card_title'] = "Card Title";
+$lang['actions'] = "actions";
+?>
