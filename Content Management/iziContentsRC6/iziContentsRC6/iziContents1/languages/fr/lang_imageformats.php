8e5ad5ac779c31f647aefadfa6f1ecf72fd1778c
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:31 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_imageformats.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_imageformats.php
@@ -0,0 +1,34 @@
+<?
+
+// Form Title
+$GLOBALS["tFormTitle"]		= 'Gestion des templates du format des images';
+
+// List Headings
+$GLOBALS["tName"]		= 'Nom';
+$GLOBALS["tBorder"]		= 'Bordure';
+$GLOBALS["tAlignment"]		= 'Alignement';
+$GLOBALS["tColour"]		= 'Couleur';
+
+// List Functions
+$GLOBALS["tAddNewIFTemplate"]	= 'Ajouter nouveau template image';
+$GLOBALS["tEditIFTemplate"]	= 'Editer template image';
+$GLOBALS["tDeleteIFTemplate"]	= 'Effacer template image';
+
+// Form Field Headings
+$GLOBALS["tIFName"]		= 'Nom du template';
+$GLOBALS["tIFAlign"]		= 'Alignement';
+$GLOBALS["tIFBorder"]		= 'Taille de la Bordure';
+$GLOBALS["tIFBackground"]	= 'Couleur de Fond';
+
+// Form Detail Comments and Help Texts
+$GLOBALS["tDetails"]		 = 'Ce formulaire vous permet de construire des templates pour vos images dans votre contenu.';
+$GLOBALS["hIFName"]		 = 'Le nom sous lequel l\'image sera référencée dans le tag [image].';
+$GLOBALS["hIFAlign"]		 = 'Côté de la page sur lequel l\'imagee apparaitra, '.$GLOBALS["tLeft"].' ou '.$GLOBALS["tRight"].'.';
+$GLOBALS["hIFBorder"]		 = 'Taille de la bordure de l\'image.';
+$GLOBALS["hIFBackground"]	 = 'Couleur de Fond de l\'image.';
+
+// Error Messages
+$GLOBALS["eNoName"]		= 'Le template image doit avoir un Nom.';
+$GLOBALS["eColourWrong"]	= 'La valeur de la couleur de fond est erronnée.';
+
+?>
