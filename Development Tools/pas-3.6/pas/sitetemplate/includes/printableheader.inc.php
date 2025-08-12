aa9184f22e9338580f04a52cf864dcba7d9931a9
me-suzy <me.suzana@gmail.com>
2025-08-12 10:00:16 +0300

    Add Development Tools/pas-3.6/pas/sitetemplate/includes/printableheader.inc.php

---
+++ Development Tools/pas-3.6/pas/sitetemplate/includes/printableheader.inc.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>SQLFusion - <?php echo $pageTitle;?></title>
+    <LINK REL=stylesheet TYPE="text/css" HREF="includes/style.css">
+    <META name="author" content="Philippe Lewicki">
+    <META name="copyright" content="SQLFusion">
+  </head>
+  <BODY BGCOLOR="white">
+
