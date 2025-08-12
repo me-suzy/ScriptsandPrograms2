de7ebf378c4b4a95139a8e4832b9995815cc4434
me-suzy <me.suzana@gmail.com>
2025-08-12 03:39:06 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_topgroupsdel.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_topgroupsdel.php
@@ -0,0 +1,60 @@
+<?php
+
+/***************************************************************************
+
+ m_topgroupsdel.php
+ -------------------
+ copyright : (C) 2005 - The iziContents Development Team
+
+ iziContents version : 1.0
+ fileversion : 1.0.1
+ change date : 23 - 04 - 2005
+ ***************************************************************************/
+
+/***************************************************************************
+ The iziContents Development Team offers no warranties on this script.
+ The owner/licensee of the script is solely responsible for any problems
+ caused by installation of the script or use of the script.
+
+ All copyright notices regarding iziContents and ezContents must remain intact on the scripts and in the HTML for the scripts.
+
+ For more info on iziContents,
+ visit http://www.izicontents.com*/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the License which can be found within the
+ *   zipped package. Under the licence of GPL/GNU.
+ *
+ ***************************************************************************/
+
+include_once ("rootdatapath.php");
+
+$GLOBALS["form"] = 'topgroups';
+$validaccess = VerifyAdminLogin();
+
+
+if ($GLOBALS["candelete"] == False) {
+   Header("Location: ".BuildLink('adminlogin.php'));
+} else {
+   DeleteTopGroups();
+   Header("Location: ".BuildLink('m_topgroups.php')."&page=".$_GET["page"]);
+}
+
+
+function DeleteTopGroups()
+{
+   global $_GET;
+
+   // Detach groups from this topgroup
+   $strQuery = "UPDATE ".$GLOBALS["eztbGroups"]." SET topgroupname='' WHERE topgroupname='".$_GET["TopGroupName"]."'";
+   $result = dbExecute($strQuery,true);
+
+   $strQuery = "DELETE FROM ".$GLOBALS["eztbTopgroups"]." WHERE topgroupname='".$_GET["TopGroupName"]."'";
+   $result = dbExecute($strQuery,true);
+   dbCommit();
+} // function DeleteTopGroups()
+
+?>
+
