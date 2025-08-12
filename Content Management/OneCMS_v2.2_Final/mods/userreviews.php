9b8ae6f5e1616ecaaf5e3ea3867cd5547b6aa842
me-suzy <me.suzana@gmail.com>
2025-08-12 07:40:14 +0300

    Add Content Management/OneCMS_v2.2_Final/mods/userreviews.php

---
+++ Content Management/OneCMS_v2.2_Final/mods/userreviews.php
@@ -0,0 +1,13 @@
+<?php
+$name = "User Reviews";
+$sql = "install_userreviews.sql";
+$version = "1.0";
+$readme = "";
+$url = "";
+$url2 = "userreviews.php";
+$status = "On";
+$installed = "";
+$table3name = "onecms_userreviews";
+$template_name = "mod-userreviews";
+$template_list = "list-userreviews";
+?>
