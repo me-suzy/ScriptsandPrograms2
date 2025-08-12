c01e0392a58f5a2ecceb00a844c541fd7aeeab36
me-suzy <me.suzana@gmail.com>
2025-08-12 05:55:54 +0300

    Add Content Management/metajour2_1/metajour/basic_view_jstopmenureload.php

---
+++ Content Management/metajour2_1/metajour/basic_view_jstopmenureload.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('basic_view.php');
+
+class basic_view_jstopmenureload extends basic_view {
+
+	function view() {
+		return '
+		<SCRIPT LANGUAGE="JavaScript">
+		if (top.topmenu) top.topmenu.location.reload();
+		</script>';
+	}
+	
+}
+
+?>
