6d42d642904c78ed9e3bf5818a11058c6a1d31ee
me-suzy <me.suzana@gmail.com>
2025-08-12 08:03:19 +0300

    Add Content Management/ssphp105/ssphp105/config.php

---
+++ Content Management/ssphp105/ssphp105/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    $dbhost = "";   //the host of your mysql server, ie 'localhost'
+    $dbname = "";   //the actual database name that SSPHP will be installed to
+    $dbuser = "";   //the username to connect to the mysql server
+    $dbpass = "";   //the password to connect to the mysql server
+    
+?>
