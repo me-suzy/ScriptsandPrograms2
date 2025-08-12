f5917956a61152372be2cd72a07f61d2a3eca694
me-suzy <me.suzana@gmail.com>
2025-08-12 05:59:40 +0300

    Add Content Management/metajour2_1/metajour/app/edocument/lang/edocument_basic_view_menu.en.php

---
+++ Content Management/metajour2_1/metajour/app/edocument/lang/edocument_basic_view_menu.en.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ * $Id: edocument_basic_view_menu.en.php,v 1.1 2005/02/16 05:03:36 jan Exp $
+ */
+
+$LANG['menu_edocnonapperror'] = 'Non-approved error reports';
+$LANG['menu_edocapperror'] = 'Approved error reports';
+$LANG['menu_edocstat'] = 'Statistics on error reports';
+?>
