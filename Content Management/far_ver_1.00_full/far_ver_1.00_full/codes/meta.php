e01d606f5f5408d3944e8b3666e6257e5d259ab3
me-suzy <me.suzana@gmail.com>
2025-08-12 03:29:43 +0300

    Add Content Management/far_ver_1.00_full/far_ver_1.00_full/codes/meta.php

---
+++ Content Management/far_ver_1.00_full/far_ver_1.00_full/codes/meta.php
@@ -0,0 +1,56 @@
+<?php
+/* =====================================================================
+*	Pagina meta.php (parte din modulul de teme)
+*	Creat de Birkoff pentru proiectul FAR-PHP
+*	Versiune: 1.0
+*	Copyright: (C) 2004 - 2005 The FAR-PHP Group
+*	E-mail: contact@far-php.ro
+*	Data inceperii paginii: 28-12-2004
+*	Ultima modificare: 04-05-2005
+*
+*	Acest program este gratuit pentru utilizare necomerciala (non profit)
+*	si este distribuit sub termenii licentei GNU General Public License
+*	asa cum sunt publicati de Free Software Foundation; versiunea 2 a licentei,
+*	sau (la alegerea dvs) orice versiune ulterioara.
+*
+*	This programs it is for non-comercial use (non-profit)
+*	and is share on GNU GPL licence agreement
+*	publish by Free Software Foundation; version 2,
+*	or (your option) any later version.
+* ======================================================================== */
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title><?php echo $mesaj[1]; ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<META content="text/html; charset=iso-8859-1" http-equiv=Content-Type>
+<META name="keywords" content="<?php echo $mesaj[331]; ?>">
+<META name="Description" content="<?php echo $mesaj[332]; ?>">
+<META content="<?php echo $mesaj[333]; ?>" name="author">
+<META content="<?php echo $mesaj[334]; ?>" name="publisher">
+<META content="<?php echo $mesaj[335]; ?>" name="revisit-after">
+<META content="<?php echo $mesaj[336]; ?>" name="robots">
+<META content="<?php echo $mesaj[337]; ?>" name="audience">
+<META content="<?php echo $mesaj[338]; ?>" name="Copyright">
+<LINK REL="Bookmark" HREF="<?php echo "http://".$_SERVER['HTTP_HOST']."/content/images/favicon.ico"; ?>">
+<LINK REL="SHORTCUT ICON" HREF="<?php echo "http://".$_SERVER['HTTP_HOST']."/content/images/favicon.ico"; ?>">
+<!-- Copyright FAR-PHP 2004-2005, www.far-php.ro, contact@far-php.ro --> 
+
+<SCRIPT LANGUAGE="JavaScript">
+// codul de incarcare a paginii
+// CopyRight 2004 DMBC http://seo-cms.com 
+// create a new Date object then get the current time
+var start = new Date();
+var startsec = start.getTime();
+// run a loop counting up to 250,000
+var num = 0;
+for( var i = 0; i < 250000; i++ )
+{
+  num++;
+}
+var stop  = new Date();
+var stopsec = stop.getTime();
+var loadtime = ( stopsec - startsec ) / 1000;
+</script>
+</head>
