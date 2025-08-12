7e4174c4b5d3087f031717856bc8f0b3a74725a0
me-suzy <me.suzana@gmail.com>
2025-08-12 10:44:37 +0300

    Add Discussion Boards/orcaforum_4.3c/forum.php

---
+++ Discussion Boards/orcaforum_4.3c/forum.php
@@ -0,0 +1,25 @@
+<?php
+
+require "orca/of_lang_en.php";
+include "orca/of_head.php";
+
+?><html>
+<head>
+  <title><?php echo $fData['pagetitle']; ?></title>
+  <link rel="stylesheet" type="text/css" href="orca/of_style.css" />
+
+  <style type="text/css">
+
+body {
+  background-color:#ffffff;
+  font:normal 100% Arial,sans-serif;
+}
+
+  </style>
+</head>
+<body>
+
+<?php include "orca/of_body.php"; ?>
+
+</body>
+</html>
