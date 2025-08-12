857afcc1e15460a1e51aa39a639ef0347878d51f
me-suzy <me.suzana@gmail.com>
2025-08-12 09:02:36 +0300

    Add Database Tools/plug/plug/banned.php

---
+++ Database Tools/plug/plug/banned.php
@@ -0,0 +1,13 @@
+<?php
+
+if($url == "http://www.outpimp.com" || $url == "http://outpimp.com") { 
+
+die("<font size='2' face='$face'>Your Domain has been banned<br><a href='http://$website/plug.php'>Go Back</a></font>"); }
+
+// To ban more domains add - $url == "http://domain.com"  and  $url == "http://www.domain.com", between the brakets above.
+
+?>
+
+
+
+
