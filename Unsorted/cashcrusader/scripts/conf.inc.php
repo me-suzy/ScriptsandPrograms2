a2cca9d4091e60a8a4ed374e509f3f2d056b520e
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:39 +0300

    Add Unsorted/cashcrusader/scripts/conf.inc.php

---
+++ Unsorted/cashcrusader/scripts/conf.inc.php
@@ -0,0 +1,45 @@
+<?
+$mysql_hostname='localhost';
+$mysql_port='';
+$mysql_user='UserID';
+$mysql_password='Password';
+$mysql_database='databasenamehere';
+$mysql_table_prefix='';
+$mysql_pconnect="NO";
+$pages_dir='/home/UserID/www/pages/';
+$pages_url='http://www.xx/pages/';
+$scripts_dir='/home/UserID/www/scripts/';
+$scripts_url='http://www.xx/scripts/';
+$admin_cash_factor=100;
+$popup_width=640;
+$popup_height=60;
+$frame_size=80;
+$domain="xx";
+$login_info_subject="Here is your login info for ZZ";
+$login_info_message="
+Your login username is <USERNAME>
+Your new password is <PASSWORD>
+http://www.xx/pages/enter.php
+";
+$confirm_info_subject="ZZ signup URL";
+$confirm_info_message="
+To complete your signup with ZZ please follow this url:
+
+http://www.xx/pages/signup.php?CO=<CODE>&EM=<EMAIL>
+
+<a href=http://www.xx/pages/signup.php?CO=<CODE>&EM=<EMAIL>>AOL Users</a>
+";
+$signup_info_subject="ZZ Member Info";
+$signup_info_message="
+Welcome to ZZ 
+Your login username is <USERNAME>
+Your login password is <PASSWORD>
+
+You can login by going to 
+
+http://www.xx/pages/enter.php
+
+if you need help email
+
+support@xx
+";
