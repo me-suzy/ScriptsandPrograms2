a9be19f2597d05050089b30b7601c5aa2bb5be7d
me-suzy <me.suzana@gmail.com>
2025-08-12 10:23:46 +0300

    Add Discussion Boards/awebBB/awebBB/switcharray.php

---
+++ Discussion Boards/awebBB/awebBB/switcharray.php
@@ -0,0 +1,24 @@
+<?
+$smiliearray = array(
+  'JavaScript',
+  'javascript',
+  'JAVASCRIPT',
+  ':)',
+  ';)',
+  ':-p',
+  '>:o',
+  ':(',
+  ':-D',
+);
+$imagearray = array(
+  '>javascript<',
+  '>javascript<',
+  '>javascript<',
+  '<img src="smilies/smile.gif" border="0">',
+  '<img src="smilies/wink.gif" border="0">',
+  '<img src="smilies/tongue.gif" border="0">',
+  '<img src="smilies/angry.gif" border="0">',
+  '<img src="smilies/sad.gif" border="0">',
+  '<img src="smilies/laughing.gif" border="0">',
+); 
+?>
