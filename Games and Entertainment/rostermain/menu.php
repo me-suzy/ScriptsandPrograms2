c5b12f7e0f304be0b3884024a35f7c913c2b199a
me-suzy <me.suzana@gmail.com>
2025-08-12 16:33:40 +0300

    Add Games and Entertainment/rostermain/menu.php

---
+++ Games and Entertainment/rostermain/menu.php
@@ -0,0 +1,12 @@
+<br /><br />
+<?
+if (isset($_SESSION['valid_user']))
+{
+	echo '<a href="index.php?page=profile" />My Profile</a>';
+}
+?>
+<br />
+<a href="index.php?page=roster" />Roster</a><br />
+
+
+
