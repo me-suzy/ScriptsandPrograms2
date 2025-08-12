11af94fd21453b0f6bc4f6caaba656ebc5fbb51f
me-suzy <me.suzana@gmail.com>
2025-08-13 01:26:58 +0300

    Add Unsorted/cashcrusader/pages/good_bye.php

---
+++ Unsorted/cashcrusader/pages/good_bye.php
@@ -0,0 +1,12 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr>
+<td align="center"> <br>
+<br>
+<font size="4">Sorry to see you go.</font><br>
+<br>
+</td>
+</tr>
+</table>
+<? include("footer.php");?>
