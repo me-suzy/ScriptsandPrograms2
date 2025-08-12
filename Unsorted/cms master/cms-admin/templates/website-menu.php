9e694d915a4b04fc0c54a2174f831810af46ebb6
me-suzy <me.suzana@gmail.com>
2025-08-13 01:48:00 +0300

    Add Unsorted/cms master/cms-admin/templates/website-menu.php

---
+++ Unsorted/cms master/cms-admin/templates/website-menu.php
@@ -0,0 +1,7 @@
+<table cellpadding=0 cellspacing=0 border=0><tr><td colspan=3><img src=images/trans.gif width=1 height=10 border=0></td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top><li></td><td><a href="website.php"><?php echo $Lang_website->menu_current_options ?></a></td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top>&nbsp;</td><td>&nbsp;</td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top><li></td><td><a href="website.php?mode=edit_title"><?php echo $Lang_website->menu_edit_title ?></a></td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top><li></td><td><a href="website.php?mode=edit_keywords"><?php echo $Lang_website->menu_edit_keywords ?></a></td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top><li></td><td><a href="website.php?mode=edit_description"><?php echo $Lang_website->menu_edit_description ?></a></td></tr>
+</table>
