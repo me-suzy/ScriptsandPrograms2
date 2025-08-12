ee86000e03204409e8d5cead52203da98d02b22b
me-suzy <me.suzana@gmail.com>
2025-08-12 20:24:02 +0300

    Add News Publishing/simpnews/masssub.php

---
+++ News Publishing/simpnews/masssub.php
@@ -0,0 +1,29 @@
+<?php
+/***************************************************************************
+ * (c)2002-2004 Boesch IT-Consulting (info@boesch-it.de)
+ ***************************************************************************/
+require_once('./config.php');
+require_once('./functions.php');
+if(!isset($$langvar) || !$$langvar)
+	$act_lang=$default_lang;
+else
+	$act_lang=$$langvar;
+include_once('language/lang_'.$act_lang.'.php');
+require_once('./includes/get_settings.inc');
+require_once('./includes/block_leacher.inc');
+if($heading)
+	$pageheading=$heading;
+else
+	$pageheading=$l_news;
+if(isset($mode))
+{
+	if($mode=="subscribe")
+	{
+		include_once("./includes/msub_subscribe.inc");
+	}
+}
+else
+{
+	include_once("./includes/msub_main.inc");
+}
+?>
