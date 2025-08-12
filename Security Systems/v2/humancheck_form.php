0db7e12d806d25c76a11cc3e256532384f79760a
me-suzy <me.suzana@gmail.com>
2025-08-12 23:48:50 +0300

    Add Security Systems/v2/humancheck_form.php

---
+++ Security Systems/v2/humancheck_form.php
@@ -0,0 +1,40 @@
+<?PHP
+#############################################
+#	Project:	HumanCheck 2
+#	file:		humancheck_config.php
+#	company:	Horobey Freelance & Telecommuting (http://horobey.com/)
+#	author:		Yuriy Horobey (yuriy@horobey.com)
+#	purpose: 
+#	shows form with image tag with slight editing can be included into existing form in a html file
+#	
+#
+#############################################
+
+$sid	=	session_id();
+if(!$sid){
+	session_start();
+	$sid	=	session_id();
+}
+require(dirname(__FILE__)."/humancheck_config.php");
+//1) lets generate the code
+$noautomationcode = "";
+for($i=0; $i<$config_max_digits;$i++)	$noautomationcode = $noautomationcode.rand(0,9);
+//save it in session
+$HTTP_SESSION_VARS["noautomationcode"] = $noautomationcode;
+
+//show form
+################################################### HERE YOU MAY EDIT #######################################
+$html_form =<<< EHTML
+<!-- the secret image -->
+<img src='humancheck_showcode.php?sid=$sid'>
+<!-- the form -->
+<form action='humancheck_check.php' method='post'>
+	<input type='hidden' name='sid' value='$sid'>
+
+	<input type='text' name='code'>
+	<input type='Submit' name='Submit'>
+</form>
+EHTML;
+#############################################################################################################
+echo($html_form);
+?>
