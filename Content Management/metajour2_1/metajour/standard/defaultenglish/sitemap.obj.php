9fe1eeca0d83dde00eea85b22ca7987b699227f1
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:24 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/sitemap.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/sitemap.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342711;
+$_objchilds[] = '_child_sitemap_342712';
+function objfile_342711 () {
+$obj = owNew('document');
+$objdata['name'] = "Sitemap";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
