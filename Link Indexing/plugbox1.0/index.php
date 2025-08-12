50c36a8174bd1c5aaf5fa41f4da1fc57b58b5935
me-suzy <me.suzana@gmail.com>
2025-08-12 19:28:07 +0300

    Add Link Indexing/plugbox1.0/index.php

---
+++ Link Indexing/plugbox1.0/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Plugbox 1.0</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>
+<body style="background-color: transparent;">
+
+<?php include('plug.php'); ?>
+
+</body>
+</html>
