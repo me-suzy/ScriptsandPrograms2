1d5f2c4a114396bc4e96880654733807adf53d2a
me-suzy <me.suzana@gmail.com>
2025-08-12 10:03:32 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/scriptMakeSettings.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/scriptMakeSettings.php
@@ -0,0 +1,15 @@
+<?php
+//you need to replace these settings 
+		$this->dbUser="yourDatabaseUser";	
+		$this->dbPwd="yourDatabaseUsersPassword";
+		$this->dbAddress="localhost";
+		$this->dbPort="3306";
+		$this->dbName="yourpeanutsdatabase";
+
+//these settings are optional - defaults are shown below
+//		$this->baseUrl = null; //may be set to: 'http://yourServer/yourPeanutsPath/'; 
+//	 	$this->debugMode = 'short'; //options: '', 'short', 'verbose'
+//		$this->funkyUrls = false;
+
+
+?>
