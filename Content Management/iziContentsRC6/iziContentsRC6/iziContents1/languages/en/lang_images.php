7d0350d64a78aad1a3c95fd458e2f2ba38ac211b
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:31 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_images.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_images.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'maintain images';
+$GLOBALS["tFormTitle1"] = 'upload images/create image subdirectories';
+$GLOBALS["tFormTitle2"] = 'Upload image';
+$GLOBALS["tFormTitle3"] = 'Create Subdirectory';
+
+//  List Functions
+$GLOBALS["tAddNewImage"] = 'Add new image';
+$GLOBALS["tDeleteImage"] = 'Delete image';
+$GLOBALS["tDeleteImageDir"] = 'Delete image subdirectory';
+
+//  List Headings
+$GLOBALS["tImageName"] = 'Image name';
+
+//  Form Fields
+$GLOBALS["tImageFilename"] = 'Image filename';
+$GLOBALS["tSubdirectoryname"] = 'Subdirectory name';
+
+//  Form Buttons
+$GLOBALS["bUpload"] = 'Upload!';
+$GLOBALS["bCreateDir"] = 'Create!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] = 'You need to select a file to upload.';
+$GLOBALS["eNoDir"] = 'You need to enter a directory name.';
+$GLOBALS["eDirAlreadyExists"] = 'subdirectory already exists';
+$GLOBALS["eZeroByteFile"] = 'File is 0 bytes in length';
+$GLOBALS["eNoFileUpload"] = 'File could not be uploaded successfully.';
+$GLOBALS["eInvalidFileSize"] = 'File is too large for upload';
+$GLOBALS["eInvalidFileType"] = 'File is not a valid image file';
+$GLOBALS["eInvalidMimeType"] = 'MIME type of File is not a valid image type';
+
+?>
