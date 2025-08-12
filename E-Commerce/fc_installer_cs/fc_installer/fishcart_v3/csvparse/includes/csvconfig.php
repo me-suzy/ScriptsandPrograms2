d5b8d37a1db90ce9f31622a028fd1434bb7d4d61
me-suzy <me.suzana@gmail.com>
2025-08-12 12:10:57 +0300

    Add E-Commerce/fc_installer_cs/fc_installer/fishcart_v3/csvparse/includes/csvconfig.php

---
+++ E-Commerce/fc_installer_cs/fc_installer/fishcart_v3/csvparse/includes/csvconfig.php
@@ -0,0 +1,21 @@
+<?
+/* this is the csv-parse config file */
+
+
+
+
+//Your directory root for fishcartsql
+$droot="/path/to/your/catalog/directory";
+
+
+
+
+//Your custID as supplied in your db.cust you created during the setup of
+// your catalog.
+$custID="fish";
+
+
+
+
+/* end of csvconfig */
+?>
