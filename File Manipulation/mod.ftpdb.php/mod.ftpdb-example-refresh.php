686028eadddae3856bce04b899b57357e52905d4
me-suzy <me.suzana@gmail.com>
2025-08-12 14:37:50 +0300

    Add File Manipulation/mod.ftpdb.php/mod.ftpdb-example-refresh.php

---
+++ File Manipulation/mod.ftpdb.php/mod.ftpdb-example-refresh.php
@@ -0,0 +1,28 @@
+<?
+/* set this to prevent this page from being cached */
+header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+header("Pragma: no-cache"); // HTTP/1.0
+?>
+
+<html>
+<head>
+  <style type="text/css">
+    body, th, td         { font-family: arial; font-size: 8pt; }
+  </style>
+</head>
+<body bgcolor=#d4b488 text=#000000>
+<div align=center>
+
+
+<br>
+<?
+require("mod.ftpdb.php");
+$ftpdb = new ftpdb();
+
+$ftpdb->refresh();
+?>
+
+
+</div>
+</body>
+</html>
