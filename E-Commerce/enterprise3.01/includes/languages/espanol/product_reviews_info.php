b77689a574af42220de828054d7c5b7214e2b426
me-suzy <me.suzana@gmail.com>
2025-08-12 11:57:25 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/product_reviews_info.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/product_reviews_info.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Comentarios');
+define('HEADING_TITLE', ' %s Comentarios');
+define('SUB_TITLE_PRODUCT', 'Producto:');
+define('SUB_TITLE_FROM', 'De:');
+define('SUB_TITLE_DATE', 'Fecha:');
+define('SUB_TITLE_REVIEW', 'Comentario:');
+define('SUB_TITLE_RATING', 'Evaluaci&oacute;n:');
+define('TEXT_OF_5_STARS', '%s de 5 Estrellas!');
+define('TEXT_CLICK_TO_ENLARGE', 'Haga Click para agrandar');
+?>
