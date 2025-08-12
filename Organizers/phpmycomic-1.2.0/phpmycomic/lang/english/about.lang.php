71d3bd048cdfd8cbb68d585223fc854b70a44986
me-suzy <me.suzana@gmail.com>
2025-08-12 21:32:23 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/about.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/about.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$tpl->assign("lang_head_about", "About");
+$tpl->assign("lang_contact", "Contact Author");
+$tpl->assign("lang_license", "PhpMyComic License");
+$tpl->assign("lang_theme", "Current Theme");
+$tpl->assign("lang_version", "version");
+
+?>
