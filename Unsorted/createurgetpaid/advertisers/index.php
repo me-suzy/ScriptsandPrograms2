4d94d7fe2c7373c7dcd044366f4648127daa981c
me-suzy <me.suzana@gmail.com>
2025-08-13 02:12:57 +0300

    Add Unsorted/createurgetpaid/advertisers/index.php

---
+++ Unsorted/createurgetpaid/advertisers/index.php
@@ -0,0 +1,37 @@
+<?
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+	// This script is copyrighted to CreateYourGetPaid©       \\
+	// Duplication, selling, or transferring of this script   \\
+	// is a violation of the copyright and purchase agreement.\\
+	// Alteration of this script in any way voids any         \\
+	// responsibility CreateYourGetPaid© has towards the      \\
+	// functioning of the script. Altering the script in an   \\
+	// attempt to unlock other functions of the program that  \\
+	// have not been purchased is a violation of the          \\
+	// purchase agreement and forbidden by CreateYourGetPaid© \\
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\\
+
+	include "../lib/.htconfig.php";
+	
+	$tml->RegisterVar("TITLE", _LANG_ADVERTISERS_TITLE);
+
+	if(!$user->IsAdvertiser())
+		exit($error->Report(_LANG_ADVERTISERS_TITLE, _LANG_ERROR_NOADVERTISER));
+	
+	$tml->RegisterVar("FNAME", $user->Get("fname"));
+	$tml->RegisterVar("SNAME", $user->Get("sname"));
+	$tml->RegisterVar("EMAIL", $user->Get("email"));
+	
+	$tml->loadFromFile("pages/header");
+	$tml->Parse();
+	
+	$tml->loadFromFile("pages/advertisers/index");
+	$tml->Parse();
+	
+	$tml->loadFromFile("pages/footer");
+	$tml->Parse();
+	
+	$tml->Output();
+
+?>
