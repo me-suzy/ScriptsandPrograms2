515935c609a9a14dd522f18d016daa0379e13605
me-suzy <me.suzana@gmail.com>
2025-08-12 06:39:20 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/iespell/langs/ko.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/iespell/langs/ko.js
@@ -0,0 +1,7 @@
+// KO lang variables
+
+tinyMCE.addToLang('',{
+iespell_desc : '¸ÂÃã¹ý °Ë»ç ½ÇÇà',
+iespell_download : "ieSpellÀ» Ã£À» ¼&ouml; ¾ø½À´Ï´Ù. OK¸¦ ´©¸£¸é ´Ù¿î·Îµ&aring; Æ&auml;ÀÌÁ&ouml;·Î ÀÌµ¿ÇÕ´Ï´Ù."
+});
+
