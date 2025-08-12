d6683e56d1911420cf7c6ce732506005ff01b686
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:05 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_usergroups.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_usergroups.php
@@ -0,0 +1,19 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Gruppenberechtigungs-Namen &uuml;bersetzen';
+
+//  List Headings
+$GLOBALS["tUsergroupname"] = 'Usergruppen-Name';
+
+//  List Functions
+$GLOBALS["tViewPrivilege"] = 'Berechtigungsgruppe ansehen';
+$GLOBALS["tEditPrivilege"] = 'Berechtigungsgruppe &uuml;bersetzen';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'In diesem Formular k&ouml;nnen Sie die Bezeichnungen f&uuml;r die Berechtigungen &uuml;bersetzen.';
+
+//  Error Messages
+$GLOBALS["eNoUsergroupName"] = 'Sie m&uuml;ssen f&uuml;r jede Sprache eine &Uuml;bersetzung eingeben.';
+
+?>
