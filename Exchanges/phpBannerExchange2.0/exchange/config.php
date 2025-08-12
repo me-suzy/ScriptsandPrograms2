064b46221fa3071be0e97ab31e5204803b1d150d
me-suzy <me.suzana@gmail.com>
2025-08-12 13:54:45 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/config.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/config.php
@@ -0,0 +1,12 @@
+<?php
+
+//
+// phpBannerExchange auto-generated config file
+// Change this file at your own risk!
+//
+
+// DO NOT CHANGE THIS OR YOUR HANDS WILL FALL OFF
+// (and it could screw up future installation processes)
+
+
+?>
