08ba7401532809b5fd1ec9d9c7c8ce27fe0d2a77
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:40 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/address_book.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/address_book.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Mi Cuenta');
+define('NAVBAR_TITLE_2', 'Mis Direcciones');
+
+define('HEADING_TITLE', 'Mis Direcciones Postales');
+
+define('PRIMARY_ADDRESS_TITLE', 'Direcci&oacute;n Principal');
+define('PRIMARY_ADDRESS_DESCRIPTION', 'Esta direcci&oacute;n se seleccionara por defecto para el envio y para la facturaci&oacute;n de sus pedidos.<br><br>Esta direcci&oacute;n tambi&eacute;n se utiliz&aacute; para calcular los impuestos que le corresponden.');
+
+define('ADDRESS_BOOK_TITLE', 'Direcciones');
+
+define('PRIMARY_ADDRESS', '(direcci&oacute;n principal)');
+
+define('TEXT_MAXIMUM_ENTRIES', '<font color="#ff0000"><b>NOTA:</b></font> Se permiten un m&aacute;ximo de %s direcciones.');
+?>
