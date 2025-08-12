32346d467c8dd409fa19713461cafe1ec7727913
me-suzy <me.suzana@gmail.com>
2025-08-12 19:55:48 +0300

    Add News Publishing/news_engine_V1.5.0/news_engine_V1.5.0/news_engine/admin/includes/bbcode/bbcode_language_us.js

---
+++ News Publishing/news_engine_V1.5.0/news_engine_V1.5.0/news_engine/admin/includes/bbcode/bbcode_language_us.js
@@ -0,0 +1,47 @@
+b_text = "Insert BOLD text";
+i_text = "Insert ITALIC text";
+u_text = "Insert UNDERLINED text";
+p_text = "Insert paragraph";
+hr_text = "Insert horizontal line";
+sub_text = "Insert subscriped text";
+sup_text = "Insert superscriped text";
+icq_text = "Insert ICQ address";
+
+size_text = "Alter the SIZE of your text";
+font_text = "Alter the FONT of your text";
+color_text = "Alter the COLOR of your text";
+align_text = "Alter the ALIGNEMENT of your text"; 
+
+url_text = "Insert a hyperlink into your message";
+url2_text = "Insert an external hyperlink into your message";
+email_text = "Insert an email-link into your message";
+img_text = "Insert an image into your message";
+
+code_text = "Insert monospace text into your message";
+php_text = "Insert text with PHP syntax highlighting";
+list_text = "Insert an ordered list into your message";
+quote_text = "Insert a quote into your message";
+
+norm_text = "Normal Mode";
+enha_text = "Enhanced Mode";
+
+closecurrent_text = "Close the current tag";
+closeall_text = "Close all opentags";
+
+enhanced_only_text = "<< This is only available in Enhanced Mode >>";
+no_tags_text = "<< No opened BBcode tags detected. >>";
+already_open_text = "<< You already have an opened tag of this type >>";
+
+HELPTEXT = "Simple insertion of BBCode and Smilies.\n\nBBCode itself is similar in style to HTML, tags are enclosed in square braces [ and ] rather than < and >\nand it offers greater control over what and how something is displayed. \n\nDepending on the template you are using you may find adding BBCode to your posts is made much easier through a clickable \ninterface above the message area on the posting form.\n\nIf you want to add a smilie, please click on it if they are activated.";
+
+tag_prompt = "Enter the text you want to format:";
+img_prompt = "Insert the url to the picture:";
+
+font_formatter_prompt = "Enter the text you want to format - ";
+
+link_text_prompt = "Enter the text to be displayed for the link (optional)";
+link_url_prompt = "Enter the full URL for the link";
+link_email_prompt = "Enter the email address for the link";
+
+list_type_prompt = "What type of list do you want? Enter 'ol' for a numbered list or leave blank for a list with bullet points.";
+list_item_prompt = "Enter a list item.\nLeave the box empty or press 'Cancel' to complete the list.";
