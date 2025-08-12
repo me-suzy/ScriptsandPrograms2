c00bf51e116c2dc43f2be64c3a9d2e63d0b82bfe
me-suzy <me.suzana@gmail.com>
2025-08-12 11:42:22 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/define_language.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/define_language.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Define Language');
+
+define('TEXT_FILE_DOES_NOT_EXIST', 'File does not exist.');
+
+define('ERROR_FILE_NOT_WRITEABLE', 'Error: I can not write to this file. Please set the right user permissions on: %s');
+?>
