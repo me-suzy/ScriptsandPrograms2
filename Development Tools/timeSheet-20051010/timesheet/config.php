62d30d39083ae99ee53d7a32fe36b81d4c212cf2
me-suzy <me.suzana@gmail.com>
2025-08-12 10:13:57 +0300

    Add Development Tools/timeSheet-20051010/timesheet/config.php

---
+++ Development Tools/timeSheet-20051010/timesheet/config.php
@@ -0,0 +1,30 @@
+<?php
+/**************************/
+/* Private Label Config   */
+/**************************/
+$config['company_email']	= 'matt@ultrize.com';
+$config['company_name']		= "Ultrize Internet Technology, LLC";
+$config['demo'] 			= 0;
+
+/**************************/
+/* Database Configuration */
+/**************************/
+$config['db_host']			= 'localhost';
+$config['db_name']			= 'ultrize_secureTime';
+$config['db_user']			= 'timesheet';
+$config['db_pass']   		= 'timesheet';
+
+/************/
+/* Sendmail */
+/************/
+$config['sendmail']			= '/usr/lib/sendmail -t';  // Path and command to sendmail
+
+/**********************/
+/* Path configuration */
+/**********************/
+$config['template_dir']		= 'templates/';
+$config['page_dir']			= 'pages/';
+$config['action_dir']		= 'actions/';
+$config['include_dir']		= 'include/';
+
+?>
