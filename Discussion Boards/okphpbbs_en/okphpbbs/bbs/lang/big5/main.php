02cdca0248fe3885cad79fb0ceed09893340b9a2
me-suzy <me.suzana@gmail.com>
2025-08-12 10:43:21 +0300

    Add Discussion Boards/okphpbbs_en/okphpbbs/bbs/lang/big5/main.php

---
+++ Discussion Boards/okphpbbs_en/okphpbbs/bbs/lang/big5/main.php
@@ -0,0 +1,262 @@
+<?php
+/*
+ ***********************************************************************
+ * Copyright (c) 2003-2004 Okphp Group
+ * http://www.okphp.com
+ *   
+ ***********************************************************************
+ */
+$indications['yes'] = "¬O"; //*
+$indications['no'] = "§_";//*
+$indications['1'] = "<font color=green>¤¹³\</font>"; //*
+$indications['0'] = "<font color=red>¸T¤î</font>";//*
+$indications['delete'] = "§R°£";//*
+$indications['add'] = "²K¥[";//*
+$indications['close'] = "Ãö³¬";//*
+$indications['announcements'] = "¤½§i";//*
+
+$indications['sys_message'] = "¨t²Î´£¥Ü";
+$indications['page_will'] = "­¶­±±N¦Û°ÊÂà¨ì¥H¤Uºô§}";
+$indications['submit'] = " ½T©w ";
+$indications['content'] = "¤º®e";
+$indications['forum'] = "½×¾Â";
+$indications['posts'] = "©«¤l";
+$indications['lastpdate_'] = "³Ì«áµoªí";
+$indications['auth'] = "±ÂÅv";
+$indications['read_auth'] = "¾\ÅªÅv­­";
+$indications['reply_auth'] = "¦^¶KÅv­­";
+$indications['new_auth'] = "µo¥DÃDÅv­­";
+
+$indications['manage'] = "ºÞ²z";
+$indications['manage_score'] = "¥[´î¤À¡C¥u¦³ºÞ²z­û/ª©¥D¦³¦¹¥\¯à";
+$indications['moderator'] = "ª©¥D";
+
+$indications['d_o_settings'] = "Àq»{¾Þ§@¥\¯à³]©w";//*
+$indications['charset_'] = "¦r²Å½s½X";//*
+
+$indications['view_itvl'] = "¨ê·s­¶­±³Ìµu¶¡¹j¡]¬í¡^"; //*
+$indications['exceed_allow'] = "¤¹³\¶W¹L¦¸¼Æ";//*
+$indications['input_itvl'] = "´£¥æªí³æ³Ìµu¶¡¹j¡]¬í¡^";//*
+$indications['unlimited'] = "µL­­¨î";//*
+
+// Users
+$indications['member_area'] = "·|­û°Ï"; //*
+$indications['reg_time_'] = "µù¥U";
+$indications['post_num_'] = "©«¤l";
+$indications['score_'] = "¿n¤À";
+$indications['login'] = "µn³°";
+$indications['login'] = "µn³°";
+$indications['login'] = "µn³°";
+$indications['logout'] = "°h¥X";
+$indications['remember_me'] = "°O¦íµn³°";
+$indications['register'] = "·|­ûµù¥U";
+$indications['user_info'] = "¥Î¤á«H®§";
+$indications['modify_info'] = "­×§ï«H®§";
+$indications['choose_info'] = "¿ï¶ñ«H®§";
+$indications['username_'] = "¥Î¤á¦W";
+$indications['password_'] = "±K½X";
+$indications['repeat'] = "­«½Æ¿é¤J";
+$indications['email_'] = "¹q¤l¶l¥ó";
+$indications['g_offered'] = "¥i¿ï¾Üªº·|­û²Õ";
+$indications['g_belong'] = "©ÒÄÝ·|­û²Õ";
+$indications['headpic_'] = "ÀY¹³¦a§}";
+$indications['sig_'] = "Ã±¦W";
+$indications['userintr_'] = "¥Î¤áÂ²¤¶";
+$indications['g_alert'] = "ª`·N¡I¥[«G¦rÅéªí¥Ü¸Ó²Õ¤£¤¹³\·|­û¦Û¤v¥[¤J¡A±z°h¥X¸Ó²Õ«á±NµLªk¦Û¤v¦^¨ì¸Ó²Õ";
+$indications['agreement'] = "¥Î¤á¨óÄ³";
+$indications['accept'] = "¦P·N";
+$indications['not_accept'] = "¤£¦P·N";
+$indications['agreement_required'] = "¹ï¤£°_¡A¥Ñ©ó±z¤£¦P·N§Ú­Ìªº¥Î¤á¨óÄ³¡A§Ú­ÌµLªkÀ°±z§¹¦¨µù¥U¡C";
+$indications['h_height_'] = "ªø«×";
+$indications['h_width_'] = "¼e«×";
+$indications['h_max_'] = "³Ì¤j­­¨î";
+
+// PM
+$indications['send_pm'] = "µo°eµu«H";
+$indications['view_pm'] = "ÂsÄýµu«H";
+$indications['has_pm'] = "<font color=green>(·s)</font>";
+$indications['reply_pm'] = "¦^´_µu«H";
+$indications['del_pm'] = "§R°£µu«H";
+$indications['from_'] = "¨Ó¦Û:";
+$indications['date_'] = "®É¶¡:";
+
+// Threads
+$indications['top_info'] = "<font color=green>¸m³»:</font>";
+								// used in the threads list
+$indications['stress_it'] = "[¥[«G]"; //*
+$indications['unstress_it'] = "[¨ú®ø¥[«G]"; //*
+$indications['top_id'] = "[¸m³»]";
+$indications['untop_id'] = "[¨ú®ø¸m³»]";
+$indications['subject_'] = "¥DÃD";
+$indications['author_'] = "§@ªÌ";
+$indications['postdate_'] = "µo§G®É¶¡";
+$indications['reply'] = "¦^´_";
+$indications['view'] = "ÂsÄý";
+$indications['new_thread'] = "µo§G¥DÃD";
+$indications['new_post'] = "µoªí¦^´_";
+$indications['edit_post'] = "½s¿è©«¤l";
+$indications['topic_name'] = "±`³W°Ï";
+$indications['faq_name'] = "FAQ°Ï";
+$indications['cream_name'] = "ºëµØ°Ï";
+$indications['recovery_name'] = "¦^¦¬¯¸";
+$indications['page_down'] = "¤U­¶&gt;";
+$indications['page_up'] = "&lt;¤W­¶";
+$indications['page_end'] = "¥½­¶&gt;|";
+$indications['page_begin'] = "|&lt;­º­¶";
+$indications['all_page'] = "Á`­¶¼Æ¡G";
+$indications['current_page'] = "¡A·í«e­¶¡G";
+$indications['page_jump'] = "¸õÂà¨ì";
+$indications['fp_name'] = "±ÂÅv";
+$indications['fp_view'] = "¾\Åª";
+$indications['fp_post'] = "µo§G¥DÃD";
+$indications['fp_reply'] = "µoªí¦^´_";
+$indications['fp_all'] = "¶}©ñ";
+$indications['fp_users'] = "©Ò¦³·|­û";
+$indications['fp_special'] = "¯S©w²Õ·|­û";
+$indications['confirm_'] = "ÅçÃÒ½X";
+$indications['confirm_ex'] = "½Ð¿é¤J±z¦b¹Ï¤ù¤¤¬Ý¨ìªº¼Æ¦r";
+$indications['no_mod'] = "¼ÈµL";
+$indications['all_online'] = "BBS¦b½u";
+$indications['type_online'] = "¥»ª©¦b½u";
+$indications['whois_online'] = "¦b½u¦Cªí";
+
+$indications['lock_it'] = "[Âê©w¥DÃD]";
+$indications['unlock_it'] = "[¨ú®øÂê©w]";
+
+$indications['this_t_m'] = "±N¦¹¥DÃD"; //*
+$indications['forum_jump'] = "½×¾Â¸õÂà"; //*
+
+// poll
+$indications['poll_info'] = "<font color=green>Poll:</font>"; //*
+$indications['poll'] = "§ë²¼"; //*
+$indications['set_poll'] = "³]©w§ë²¼"; //*
+$indications['poll_question'] = "§ë²¼°ÝÃD";//*
+$indications['allow_multi'] = "¤¹³\¦h¿ï¶Ü¡H";//*
+$indications['poll_timeout'] = "¦³®Ä´Á";//*
+$indications['days'] = "¤Ñ";
+$indications['no_limit'] = "µL­­¨î";//*
+$indications['poll_option'] = "¿ï¶µ";//*
+$indications['vote'] = "§ë²¼¡I";//*
+
+// attachment
+$indications['attachment'] = "ªþ¥ó";//*
+$indications['set_attach'] = "³]©wªþ¥ó";//*
+$indications['size'] = "¤å¥ó¤j¤p";//*
+$indications['attach_max_size'] = "¤å¥ó¤j¤p­­¨î";//*
+$indications['attach_num_post'] = "¨C¶K¤å¥ó¼Æ­­¨î";//*
+$indications['attach_num_day'] = "¨C¤Ñ¤å¥ó¼Æ­­¨î";//*
+
+$indications['required_info'] = "¥²¶ñ«H®§";
+
+// added 2.0beta =================================================================
+// online users count
+$indications['bb_stats'] = "½×¾Â«H®§";
+$indications['users'] = "·|­û";
+$indications['guests'] = "¹C«È";
+$indications['count_topic_'] = "¥DÃD¼Æ";
+$indications['count_post_'] = "©«¤l¼Æ";
+$indications['count_user_'] = "·|­û¼Æ";
+$indications['new_user'] = "Åwªï·s·|­û";
+$indications['bb_count'] = "½×¾Â²Î­p";
+$indications['basic_setting'] = "°ò¥»³]©w";
+
+$indications['my_posts'] = "±z¦n¡I±z¦b¥»½×¾Âµoªíªº©«¤l¼Æ";
+$indications['my_score'] = "±zªº¿n¤À";
+$indications['my_info'] = "±zªº«H®§";
+$indications['my_reg'] = "±zµù¥U©ó";
+$indications['maxol'] = "³Ì°ª¦b½u°O¿ý¬O";
+
+// new thread
+$indications['emoticons'] = "ªí±¡¹Ï¼Ð";
+$indications['post_option'] = "µo§G¿ï¶µ";
+$indications['disable_bbcode'] = "¸T¥Î½×¾Â¼ÐÅÒ¥N½X(BBCODE)";
+$indications['disable_emote'] = "¸T¥Îªí±¡¹Ï¼Ð";
+$indications['disable_url'] = "¸T¥ÎÃì±µ¦Û°ÊÃÑ§O";
+
+$indications['nfolder'] = "±`³W¥DÃD";
+$indications['hotfolder'] = "¦^´_¸û¦hªº¥DÃD";
+$indications['lockfolder'] = "³QÂê©wªº¥DÃD¡]¤£¯à¦^´_©M½s¿è¡^";
+$indications['topfolder'] = "¸m³»ªº¥DÃD";
+$indications['pollfolder'] = "§ë²¼¥DÃD";
+
+$indications['search_content'] = "·j¯Áµ²ªG";
+$indications['search'] = "·j¯Á";
+$indications['no_new_posts'] = "µL·s©«ªº½×¾Â";
+$indications['new_posts'] = "¦³·s©«ªº½×¾Â";
+
+// BLOG
+$indications['blog'] = "BLOG";
+$indications['blog_name'] = "BLOG¦WºÙ";
+$indications['blog_about'] = "BLOGÂ²¤¶";
+$indications['blog_articles'] = "BLOG¤å³¹¼Æ";
+$indications['blog_info'] = "BLOG«H®§";
+$indications['my_blog'] = "§ÚªºBLOG";
+// ######################################  messages
+
+$messages['access_denied'] = "½Ð¨D³Q©Úµ´¡A¥i¯à±zªºÅv­­¤£°÷©Î¾Þ§@¿ù»~";
+$messages['view_forum_fail'] = "ÂsÄý½×¾Â¥¢±Ñ¡A¥i¯à¬O±zªºÅv­­¤£°÷©ÎªÌ¾Þ§@¿ù»~";
+$messages['new_post_fail'] = "µo§G©«¤l¡I¥i¯à¬O±zªºÅv­­¤£°÷©ÎªÌ¾Þ§@¿ù»~";
+$messages['new_post_ok'] = "µoªí©«¤l¦¨¥\¡I";
+$messages['opration_denied'] ="¾Þ§@¥¢±Ñ!"; //*
+$messages['info_not_enough'] = "±zªº¿é¤J«H®§¤£¥þ¡I";
+$messages['edit_post_fail'] = "½s¿è©«¤l¥¢±Ñ¡A¥i¯à¬O±zªºÅv­­¤£°÷©ÎªÌ¾Þ§@¿ù»~";
+
+// Users
+$messages['login_ok'] = "µn³°¦¨¥\¡I";
+$messages['login_fail'] = "µn³°¥¢±Ñ¡I¨t²Î¤¹³\±zµn³°¥¢±Ñªº¦¸¼Æ¬°¡G";
+$messages['not_login'] = "±zµLÅv¶i¦æ¦¹¾Þ§@¡A½Ð¥ýµn³°¡I";
+$messages['pwd_not_match'] = "¨â­Ó±K½X¤£¬Û¦P";
+$messages['email_wrong'] = "¢Ó¢Û¢Ï¢×¢Ú®æ¦¡¿ù»~";
+$messages['email_exist'] = "±z¿ï¾Üªº¢Ó¢Û¢Ï¢×¢Ú¤w¸g³Q¦û¥Î";
+$messages['sig_too_lang'] = "Ã±¦W¹Lªø";
+$messages['no_such_user'] = "¨S¦³³o­Ó¥Î¤á ";
+$messages['user_exist']	     = "±z¿ï¾Üªº¥Î¤á¦W¤w¸g³Q¦û¥Î";
+$messages['reg_ok'] = "µù¥U¦¨¥\¡I½Ðµn³°";
+$messages['not_img'] = "¤£¬O¦Xªkªº¹Ï¤ù®æ¦¡";
+$messages['img2big'] = "¹Ï¤ù®e¶q¹L¤j";
+$messages['only_az09']		 = "¹ï¤£°_¡I¥Î¤á¦W¥u¯à¥Ñ­^¤å¦r¥À»P¼Æ¦r²Õ¦¨";
+$messages['forbid_s']		 = "¹ï¤£°_¡I±zªº¥Î¤á¦W¤¤¥]§t§Ú­Ì¸T¤îªº¦r²Å";
+$messages['username_2long']  = "¹ï¤£°_¡I±zªº¥Î¤á¦W¤Óªø";
+
+// search
+$messages['s_forbidden']	 = "¹ï¤£°_¡I·j¯Á¥\¯à³QÃö³¬";
+$messages['result_num']		 = "§ä¨ìªº°O¿ýÁ`¼Æ¡G";
+$messages['no_match']		 = "¨S¦³§ä¨ì²Å¦X±ø¥óªº°O¿ý";
+
+// input control
+$messages['msg_too_long']	 = "±z¿é¤Jªº«H®§¤Óªø¡I";
+$messages['input_crigger']	 = "±zµo¨¥ªºÀW²v¹L§Ö¡I½Ðµy­Ô¦A¸Õ";
+
+// PM
+$messages['pm_overload']	 = "¹ï¤£°_¡I¸Ó¥Î¤áªºµu«H®§¤wº¡¡AµLªk±µ¦¬·s¶l¥ó";
+
+// Others
+$messages['confirm_fail'] = "ÅçÃÒ½X¿ù»~";
+$messages['modify_ok'] = "­×§ï¦¨¥\¡I";
+$messages['search_find'] = "²Å¦X±ø¥óªº°O¿ý¼Æ¥Ø";
+$messages['send_ok'] = "µo°e¦¨¥\¡I";
+$messages['del_ok'] = "§R°£¦¨¥\¡I";
+$messages['is_locked'] = "¹ï¤£°_¡I¥»¥DÃD¤w¸g³QÂê©w¡AµLªk½s¿è©Î¦^´_©«¤l";
+$messages['info_added'] = "«H®§µo°e¦¨¥\¡I";
+$messages['process_ok'] = "¾Þ§@¦¨¥\¡I";
+$messages['process_fail'] = "¾Þ§@¥¢±Ñ!"; //*
+
+// poll
+$messages['set_poll_wrong'] = "³]©w§ë²¼¥¢±Ñ¡I ½ÐÀË¬d±zªº«H®§¿ý¤J¬O§_¥¿½T¡C"; //*
+$messages['cannot_vote'] = "¹ï¤£°_¡I§ë²¼¥¢±Ñ¡C¥i¯à±z¤w¸g§ë¹L¤@¦¸²¼¡A©ÎªÌ¸Ó§ë²¼¤w¸g¹L´Á¡C"; //*
+// attachment
+$messages['unknow_filetype'] = "¤£¦Xªkªº¤å¥óÃþ«¬¡C"; //*
+$messages['attach_num_exceed'] = "¹ï¤£°_¡I±z¤W¶Çªº¤å¥ó¼Æ¶q¶W¹L¤F­­¨î"; //*
+$messages['attach_size_exceed'] = "¹ï¤£°_¡I±z¤W¶Çªº¤å¥ó¤j¤p¶W¹L¤F­­¨î"; //*
+$messages['headpic_size_exceed'] = "¹ï¤£°_¡I±z³]¸mªº¹Ï¤ù°ª©Î¼e¶W¹L¤F­­¨î";
+
+// 2.0 ---------
+$messages['opration_ok'] = "¾Þ§@¦¨¥\¡I";
+
+// 3
+$messages['badlogin_locked'] = "¥Ñ©ó±zµn³°¥¢±Ñªº¦¸¼Æ¤Ó¦h¡A¨t²Î¼È®É«Ì½ª¤F±zªºIP¡A½Ð¹L´X¤ÀÄÁ¦A¹Á¸Õµn³°";
+
+// BLOG
+$messages['blog_name_exist'] = "¹ï¤£°_¡I±z¿ï¾ÜªºBLOG¦WºÙ¤w¸g³Q¦û¥Î";
+$messages['blogname_2long'] = "±zªºBLOG¦WºÙ¹Lªø";
+?>
