7b580447c1561ac7db0eb3fde79d6298cdf64760
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:14 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_active.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_active.en.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Activate';
+$LANG['text_1'] = 'Activate or deactivate the selected records';
+$LANG['button_activate'] = 'Activate';
+$LANG['button_deactivate'] = 'Deactivate';
+?>
