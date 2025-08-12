aa95ce08da8aa49eb7e3bd40e1353020198018fd
me-suzy <me.suzana@gmail.com>
2025-08-12 06:15:42 +0300

    Add Content Management/metajour2_1/metajour/extension/sitemap/lang/sitemap.da.php

---
+++ Content Management/metajour2_1/metajour/extension/sitemap/lang/sitemap.da.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['label_name'] = 'Konfigurationsnavn';
+$LANG['label_templateid'] = 'Skabelon';
+$LANG['label_structureid'] = 'Struktur';
+$LANG['name'] = 'Konfiguration af Sitemap';
+?>
