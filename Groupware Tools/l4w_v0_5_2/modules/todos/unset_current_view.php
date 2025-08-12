aa87315c0efc8e14d82fc1731eb5111a5c05df21
me-suzy <me.suzana@gmail.com>
2025-08-12 17:12:15 +0300

    Add Groupware Tools/l4w_v0_5_2/modules/todos/unset_current_view.php

---
+++ Groupware Tools/l4w_v0_5_2/modules/todos/unset_current_view.php
@@ -0,0 +1,33 @@
+<?php
+
+  /**
+    * $Id: unset_current_view.php,v 1.3 2005/07/31 09:20:36 carsten Exp $
+    *
+    * unset current view
+    *
+    * @author       Carsten Graef <evandor@gmx.de>
+    * @copyright    evandor media 2004
+    * @package notes
+    */
+
+	// === Base Configuration ==========================================
+    include_once ("../../config/config.inc.php");
+    if (!defined('EASY_FRAMEWORK_DIR'))
+        define ("EASY_FRAMEWORK_DIR", substr (dirname (__FILE__),0,-30)."/inc/easy_framework");   
+	require_once (EASY_FRAMEWORK_DIR."/easy_framework.inc.php");
+    //$logger->set_logging_level ($LOGGING_DEFAULT_LEVEL);
+    //$logger->set_ident ("notes [User #".$_SESSION['user_id']."]");
+    
+	// === Inclusion of model and controler ============================
+	require_once ("../common/leads4web_model.php");
+
+    // === Application Includes ========================================
+    include ("../../inc/functions.inc.php");
+
+    $affected = l4w_model::unlockEntry ('note', $_REQUEST['entry_id']);
+	
+    
+?>
+unset note<br><?=$_REQUEST['entry_id']?> (<?=$affected?>)
+</body>
+</html>
