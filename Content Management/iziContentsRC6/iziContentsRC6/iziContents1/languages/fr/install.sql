2cee937e9987963822fe143097a1064bdf0aa79e
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:11 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/install.sql

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/install.sql
@@ -0,0 +1,3 @@
+INSERT INTO languages (languagecode, languagename, charset, enabled, direction, translationby) 
+VALUES ('fr', 'Fran&ccedil;ais', 'iso-8859-1, windows-1252', 'Y', 'ltr', 'Diablo');
+
