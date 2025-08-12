1cd19f197d62c318cb1d88d82542f5276af661cb
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:14 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_downloads.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_downloads.php
@@ -0,0 +1,36 @@
+<?php
+
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'Downloads verwalten';
+$GLOBALS["tFormTitle1"] = 'Download-Daten hochladen/Unterverzeichnis erstellen';
+$GLOBALS["tFormTitle2"] = 'Download-Daten hochladen';
+$GLOBALS["tFormTitle3"] = 'Unterverzeichnis erstellen';
+
+//  Funktionen auflisten
+$GLOBALS["tAddNewDownload"] = 'Download hinzuf&uuml;gen';
+$GLOBALS["tDeleteDownload"] = 'Download l&ouml;schen';
+$GLOBALS["tDeleteDownloadDir"] = 'Download Unterverzeichnis l&ouml;schen';
+
+//  List Headings
+$GLOBALS["tDownloadName"] = 'Download Name';
+
+//  Formularfelder
+$GLOBALS["tDownloadFilename"] = 'Download Dateiname';
+$GLOBALS["tSubdirectoryname"] = 'Unterverzeichnis Name';
+
+//  Formular Buttons
+$GLOBALS["bUpload"] = 'hochladen!';
+$GLOBALS["bCreateDir"] = 'erstellen!';
+
+//  Fehlernachrichten
+$GLOBALS["eNoFile"] = 'Sie m&uuml;ssen zum hochladen eine Datei ausw&auml;hlen.';
+$GLOBALS["eNoDir"] = 'Sie m&uuml;ssen einen Verzeichnisnamen eingeben.';
+$GLOBALS["eDirAlreadyExists"] = 'Dieses Verzeichnis besteht bereits';
+$GLOBALS["eZeroByteFile"] = 'Die Datei hat eine L&auml;nge von 0 bytes';
+$GLOBALS["eNoFileUpload"] = 'Die Dateil konnte nicht erfolgreich hochgeladen werden!';
+$GLOBALS["eInvalidFileSize"] = 'Die Datei ist zu gross zum hochladen';
+$GLOBALS["eInvalidFileType"] = 'Die Datei hat kein g&uuml;ltiges Download-Dateiformat';
+$GLOBALS["eInvalidMimeType"] = 'MIME Typ der Datei ist kein g&uuml;ltiger Download-Typ';
+
+?>
