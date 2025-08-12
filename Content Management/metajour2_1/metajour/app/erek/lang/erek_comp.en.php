c42a51f2583205d8bbb1e59e025cce04328bda3a
me-suzy <me.suzana@gmail.com>
2025-08-12 06:01:27 +0300

    Add Content Management/metajour2_1/metajour/app/erek/lang/erek_comp.en.php

---
+++ Content Management/metajour2_1/metajour/app/erek/lang/erek_comp.en.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eReklamation
+ * @subpackage lang
+ * $Id: erek_comp.en.php,v 1.1 2005/02/16 05:04:58 jan Exp $
+ */
+
+$LANG['name'] = 'Complaints';
+$LANG['label_name'] = 'Item no';
+$LANG['label_itemtext'] = 'Item description';
+$LANG['label_itemnum'] = 'Item number';
+$LANG['label_compunitid'] = 'Unit';
+$LANG['label_compcauseid'] = 'Cause';
+$LANG['label_description'] = 'Description';
+$LANG['label_cno'] = 'Customer no.';
+$LANG['label_cname'] = 'Customer name';
+$LANG['label_caddress'] = 'Address';
+$LANG['label_cpostalcode'] = 'Postal';
+$LANG['label_ccity'] = 'City';
+$LANG['label_compcountryid'] = 'Country';
+$LANG['label_ccontact'] = 'Contact';
+$LANG['label_cemail'] = 'Email';
+$LANG['label_messageto'] = 'Message to';
+$LANG['label_compsolutionid'] = 'Solution';
+$LANG['label_compdecisionid'] = 'Decision';
+$LANG['label_credit'] = 'Refund';
+$LANG['label_cost'] = 'Cost';
+$LANG['label_comment'] = 'Comments';
+$LANG['label_compdepartmentid'] = 'Department';
+$LANG['label_comment1'] = 'Closing comments';
+$LANG['label_status'] = 'Status';
+$LANG['label_closedtime'] = 'Closed time';
+$LANG['label_closedby'] = 'Closed by';
+$LANG['label_'] = '';
+?>
