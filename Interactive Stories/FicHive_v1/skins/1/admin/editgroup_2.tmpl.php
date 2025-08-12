ab0f44a450a679d7c85a852c1c23adbd02161c70
me-suzy <me.suzana@gmail.com>
2025-08-12 18:48:45 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/editgroup_2.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/editgroup_2.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%GROUPNAME%></td><td><input type='text' name='gname' value='<%GROUPNAME_VAL%>'></td></tr>
+<tr><td class='fframe'><%GROUPCOLOR%></td><td><input type='text' name='gcolor' value='<%GROUPCOLOR_VAL%>'></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>'></td></tr>
+</table>
