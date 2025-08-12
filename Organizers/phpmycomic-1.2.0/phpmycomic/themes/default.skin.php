b29d3c4516d184945e412d6f26a1249be0438694
me-suzy <me.suzana@gmail.com>
2025-08-12 21:34:01 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/themes/default.skin.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/themes/default.skin.php
@@ -0,0 +1,24 @@
+<?php
+
+//////////////////////////////////////////////////////////////
+//
+//     Theme:         Default
+//     Version:       1.1.6
+//     Made by:       Endre Myrvang
+//     EMail          bstar@dustrium.net
+//
+//     NOTES
+//     This is the default theme for PhpMyComic. This theme
+//     is included with all copies of the program
+//
+//////////////////////////////////////////////////////////////
+
+$themename     = "Default";
+
+$skinversion   = "1.1.6";
+
+$themeauthor   = "Endre Myrvang";
+
+$authormail	   = "bstar@dustrium.net";
+
+?>
