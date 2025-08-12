d58d452e5a1c1a4a5a72843feb0ae667358967a5
me-suzy <me.suzana@gmail.com>
2025-08-12 18:12:48 +0300

    Add Image Galleries/PHP_JK_Aug_01_2005/Includes/Colors/Aqua.php

---
+++ Image Galleries/PHP_JK_Aug_01_2005/Includes/Colors/Aqua.php
@@ -0,0 +1,32 @@
+<?php
+	$PageText = "000000";
+	$PageBGColor = "FFFFFF";
+	$PageBorder = "008D88";
+	$PageLinkColor = "005552";
+	$PageALinkColor = "005552";
+	$PageVLinkColor = "005552";
+	
+	$TextColor1 = "000000";
+	$BGColor1 = "DBEDEC";
+	$BorderColor1 = "008D88";
+	$LinkColor1 = "005552";
+	$ALinkColor1 = "005552";
+	$VLinkColor1 = "005552";
+	
+	$TextColor2 = "000000";
+	$BGColor2 = "95CCCA";
+	$BorderColor2 = "005552";
+	$LinkColor2 = "005552";
+	$ALinkColor2 = "005552";
+	$VLinkColor2 = "005552";
+	
+	$TextColor3 = "000000";
+	$BGColor3 = "42908D";
+	$BorderColor3 = "005552";
+	$LinkColor3 = "0A2E2D";
+	$ALinkColor3 = "0A2E2D";
+	$VLinkColor3 = "0A2E2D";
+	
+	$ErrorColor = "FF6666";
+	$SuccessColor = "004544";
+?>
