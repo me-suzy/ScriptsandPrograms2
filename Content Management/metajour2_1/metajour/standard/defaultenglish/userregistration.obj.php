f6fd3e411a8be196eaa31cfb418dbb0d149b0f3b
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:27 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/userregistration.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/userregistration.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342662;
+$_objchilds[] = '_child_becomearegistereduser_342663';
+function objfile_342662 () {
+$obj = owNew('document');
+$objdata['name'] = "User registration";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
