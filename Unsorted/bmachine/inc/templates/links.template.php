3dc8800c08a6c04e8775ddceae058dbac39a3fc1
me-suzy <me.suzana@gmail.com>
2025-08-13 01:17:57 +0300

    Add Unsorted/bmachine/inc/templates/links.template.php

---
+++ Unsorted/bmachine/inc/templates/links.template.php
@@ -0,0 +1,29 @@
+<table border="0" cellpadding="2" cellspacing="0" width="126">
+<tr>
+<td width="18" height="19">
+</td>
+<td width="108" height="19"><p><b>Links</b></p>
+</td>
+</tr>
+<tr>
+<td width="18">
+<p align="center"><img src="images/box1.gif"></p>
+</td>
+<td width="108"><p><a href="http://boastology.com" target="_blank">bMachine</a></p>
+</td>
+</tr>
+<tr>
+<td width="18">
+<p align="center"><img src="images/box1.gif"></p>
+</td>
+<td width="108"><p><a href="http://bnsoft.net" target="_blank">BN Soft</a></p>
+</td>
+</tr>
+<tr>
+<td width="18">
+<p align="center"><img src="images/box1.gif"></p>
+</td>
+<td width="108"><p><a href="http://fly2.cc" target="_blank">Fly2.cc</a></p>
+</td>
+</tr>
+</table><br>
