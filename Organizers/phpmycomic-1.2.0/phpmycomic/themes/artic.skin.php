ebe410a41ab1dedc074b67ce5a5a4ecd3e7d76f5
me-suzy <me.suzana@gmail.com>
2025-08-12 21:33:55 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/themes/artic.skin.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/themes/artic.skin.php
@@ -0,0 +1,24 @@
+<?php
+
+//////////////////////////////////////////////////////////////
+//
+//     Theme:         Artic Theme
+//     Version:       1.2.0
+//     Made by:       Endre Myrvang
+//     EMail          bstar@dustrium.net
+//
+//     NOTES
+//     This is the new Artic theme based on the 
+//	   the default theme.
+//
+//////////////////////////////////////////////////////////////
+
+$themename     = "Artic Theme";
+
+$skinversion   = "1.2.0";
+
+$themeauthor   = "Endre Myrvang";
+
+$authormail	   = "bstar@dustrium.net";
+
+?>
