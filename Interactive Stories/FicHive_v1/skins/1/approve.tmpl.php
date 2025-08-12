238c40b52cb77c70fbf05b036fc234fc22b5aed3
me-suzy <me.suzana@gmail.com>
2025-08-12 18:46:08 +0300

    Add Interactive Stories/FicHive_v1/skins/1/approve.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/approve.tmpl.php
@@ -0,0 +1,7 @@
+<a name='<%FICTITLE%>'></a>
+<table border='0' width='100%'>
+<tr><td colspan='2'><b><%FICTITLE%></b><td></tr>
+<%CHAPTERS%>
+<tr><td colspan='2'><input type='submit' name='approve' value='<%APPROVE%>' class='mid'>
+<input type='submit' name='reject' value='<%REJECT%>' onClick='return confirm("<%AREYOUSURE%>")' class='mid'></td></tr>
+</table>
