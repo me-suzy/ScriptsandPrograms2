21629ac060a356eac5883f0ccba2b1564bfde1f3
me-suzy <me.suzana@gmail.com>
2025-08-13 01:26:11 +0300

    Add Unsorted/calendar/config.inc.php

---
+++ Unsorted/calendar/config.inc.php
@@ -0,0 +1,6 @@
+<?
+  $config_databasehost = 'localhost';  /* This is the mysql server name */
+  $config_databasename = 'calendar'; /* Name of the database used */
+  $config_databaseuser = 'sa'; /* Name used to connect to the server database.  Must have read/write access */
+  $config_databasepassword = ''; /* Password used to connect to the server database */
+?>
