3fdae2e00b23aac2459c6935490a25103ebb9f9e
me-suzy <me.suzana@gmail.com>
2025-08-13 01:26:54 +0300

    Add Unsorted/cashcrusader/pages/footer.php

---
+++ Unsorted/cashcrusader/pages/footer.php
@@ -0,0 +1,101 @@
+    <!--End Body -->
+</font></td>
+</tr>
+</table>
+</td>
+<td width="0%" align="center" valign="top" bgcolor="#EEEEEE"><table width="120" border="0" cellpadding="0" cellspacing="0">
+<tr bordercolor="#990000">
+<td height="15" align="center" valign="top" bordercolor="#0066CC" bgcolor="#990000"><font color="#EEEEEE" size="1" face="Verdana, Arial, Helvetica, sans-serif"><b>Monthly
+Links</b></font></td>
+</tr>
+<tr>
+<td valign="top"><font size="1" face="Verdana, Arial, Helvetica, sans-serif">
+<!--Begin monthly links-->
+<br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<br>
+<!--End  monthly links-->
+</font></td>
+</tr>
+<tr bordercolor="#990000">
+<td bgcolor="#990000" height="15" align="center"><font color="#EEEEEE" size="1" face="Verdana, Arial, Helvetica, sans-serif"><b>Weekly
+Links</b></font></td>
+</tr>
+<tr>
+<td valign="top"><font size="1" face="Verdana, Arial, Helvetica, sans-serif">
+<!--Begin Weekly Links-->
+<br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here </a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your
+
+
+
+link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your
+link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your
+link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your
+link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your
+link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your
+link here</a><br>
+<!--End Weekly  Links-->
+<br>
+</font></td>
+</tr>
+<tr bordercolor="#990000">
+<td bgcolor="#990000" align="center" height="15"><font color="#EEEEEE" size="1" face="Verdana, Arial, Helvetica, sans-serif"><b>Perma
+Links</b></font></td>
+</tr>
+<tr bordercolor="#990000">
+<td valign="top"><font size="1" face="Verdana, Arial, Helvetica, sans-serif">            
+<!--Begin Perma Links-->
+<br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here </a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php"> Your link here</a><br>
+<!--End Perma Links-->
+<br>
+</font></td></tr>
+</table>
+</td>
+</tr>
+<tr bgcolor="#000033">
+<td colspan="3"><font size="1" face="Verdana, Arial, Helvetica, sans-serif"><img src="/images/spacer.gif" width="125" height="8"></font></td>
+</tr>
+</table>
+<center>
+<font color="#FFFFFF" size="1" face="Verdana, Arial, Helvetica, sans-serif">&copy; All
+Rights Reserved 2003 yourdomainname</font><br>
+<font size="1" face="Verdana, Arial, Helvetica, sans-serif"><a href="http://www.victoriasgiftshop.com/design/index.php" target="_blank"><font color="#FFFFFF">&reg;Created by &quot;DesignsbyVictoria&quot;&#153;</font></a></font>
+</center>
+</body>
+</html>
