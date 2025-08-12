4d6c4815210af316cf1e0457afb0c9934d55744a
me-suzy <me.suzana@gmail.com>
2025-08-12 16:40:57 +0300

    Add Graphs and Charts/advgraphtrial/tutorials/stackedverticalbargraph/config.php

---
+++ Graphs and Charts/advgraphtrial/tutorials/stackedverticalbargraph/config.php
@@ -0,0 +1,69 @@
+<?php
+
+/* ------------------------------------------------------------------ */
+/*                                                                    */
+/* This config script is a very simply little script which            */
+/* outputs the configuration data for the graph.                      */
+/*                                                                    */
+/* ------------------------------------------------------------------ */
+
+
+print "width: 300\n";
+print "height: 300\n";
+
+print "outline: true\n";
+print "gradientfill: true\n";
+print "displaybarvalues: true\n";
+
+print "backgroundcolor: #CCFFDD\n";
+print "grid: true\n";
+print "axis: true\n";
+print "nrows: 5\n";
+
+print "3d: true\n";
+print "depth3d: 10\n";
+
+print "ndecplaces: 0\n";
+
+print "gridbgcolor: #227755\n";
+print "axiscolor: white\n";
+print "floorcolor: #225533\n";
+print "gridcolor: white\n";
+print "gridstyle: dotted\n";
+
+print "series1: 0,128,255|Profit|\n";
+print "series2: 247,147,4|Sales|\n";
+
+print "ylables: true\n";
+print "yfont: medium\n";
+print "ylabelcolor: #202050\n";
+print "ylabelpre: $\n";
+print "ylabelpost: \n";
+
+print "titletext: Sales vs Profit by Month\n";
+print "titlefont: medium bold\n";
+print "titlecolor: #202050\n";
+
+print "xtitletext: Month\n";
+print "xtitlefont: medium bold\n";
+print "xtitlecolor: #202050\n";
+
+print "ytitletext: Value\n";
+print "ytitlefont: medium bold\n";
+print "ytitlecolor: #202050\n";
+
+print "xlabels: January|February|March|April|May|June|\n";
+print "xlabelorientation: up angle\n";
+print "xlabeloffset: 0\n";
+print "xfont: medium\n";
+print "xlabelcolor: #202050\n";
+
+print "legend: true\n";
+print "legendstyle: horizontal\n";
+print "legendbgcolor: #AAFFCC\n";
+print "legendbordercolor: #227755\n";
+print "legendtextcolor: #202050\n";
+print "legendfont: medium bold\n";
+
+
+?>
