b9aa0e50164b7087cdc87df1370c3373cf7ebc80
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:10 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/initenglish.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/initenglish.obj.php
@@ -0,0 +1,12 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342723;
+function objfile_342723 () {
+$obj = owNew('frame');
+$objdata['name'] = "initenglish";
+$objdata['pageid'] = "342676";
+$obj->createObject($objdata);
+$obj->setDefault();
+return $obj;
+}
+?>
