3fc8e0de0ed2b061ca7374b17ebef4efaf8c29cb
me-suzy <me.suzana@gmail.com>
2025-08-13 02:22:29 +0300

    Add Unsorted/datingscript/templates/love/sfooter.php

---
+++ Unsorted/datingscript/templates/love/sfooter.php
@@ -0,0 +1,22 @@
+<?
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.1.1                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 04/05/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# Template Name           Love                             #
+# Author                  AzDG <support@azdg.com>          #
+############################################################
+# File name               footer.php                       #
+# File purpose            Footer for love template         #
+# File created by         AzDG <support@azdg.com>          #
+############################################################
+?>
+
+<!-- !!!Don`t Remove CopyRights (link to azdg.com) !!! -->
+
+<center><span class=desc><?=$w[107]?><br></span></center>
+</body></html>
