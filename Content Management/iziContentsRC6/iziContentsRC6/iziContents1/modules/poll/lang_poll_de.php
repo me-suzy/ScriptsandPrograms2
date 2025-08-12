f97cb25a4b2da8a0fa81a9d7317240da958bf89b
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:57 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/poll/lang_poll_de.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/poll/lang_poll_de.php
@@ -0,0 +1,51 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'Umfragen verwalten';
+$GLOBALS["tSubmitFormTitle"] = 'Neue Umfrage';
+
+//  List Headings
+$GLOBALS["tQuestion"] = 'Frage';
+$GLOBALS["tOption"] = 'Option';
+$GLOBALS["tPostedBy"] = 'Gesendet von';
+$GLOBALS["tPollType"] = 'Umfrage Typ';
+$GLOBALS["tPublishDate"] = 'Datum Start';
+$GLOBALS["tExpiryDate"] = 'Datum Ende';
+$GLOBALS["tStatus"] = 'Status';
+
+//  List Functions
+$GLOBALS["tAddNewPoll"] = 'Umfrage hinzufügen';
+$GLOBALS["tEditPoll"] = 'Umfrage bearbeiten';
+$GLOBALS["tDeletePoll"] = 'Umfrage löschen';
+$GLOBALS["tReleasePoll"] = 'Umfrage freigeben';
+$GLOBALS["tEditOptions"] = 'Umfrage-Optionen bearbeiten';
+
+$GLOBALS["tAddNewPollOption"] = 'Umfrage Option hinzufügen';
+$GLOBALS["tEditPollOption"] = 'Umfrage Option bearbeiten';
+$GLOBALS["tDeletePollOption"] = 'Umfrage Option löschen';
+
+//  Form Field Headings
+$GLOBALS["tCategory"] = 'Kategorie';
+
+//  Poll View Texts
+$GLOBALS["tMultiVotes"] = 'Multi-Vote';
+$GLOBALS["tSingleVote"] = 'Single-Vote';
+
+$GLOBALS["tSubmitPoll"] = 'Umfrage senden';
+$GLOBALS["tContact"] = 'Kontakt';
+$GLOBALS["tEMail"] = 'E-Mail';
+$GLOBALS["tWebPage"] = 'Homepage';
+$GLOBALS["tVoteButton"] = 'Abstimmen';
+
+//  Poll Submission Texts
+$GLOBALS["tThankYou"]		= 'Herzlichen Dank für das Eintragen der Umfrage.';
+$GLOBALS["tReleaseRequired"]	= 'Beachten Sie das die Umfrage erst nach Durchsicht des Administrators freigeschaltet wird.';
+$GLOBALS["tClickToReturn"]	= 'Hier gelangen Sie zurück zur';
+$GLOBALS["tPollPage"]		= 'Umfrage';
+
+//  Security Texts
+$GLOBALS["tMustLogin"]		= 'Sie müssen sich';
+$GLOBALS["tLogin"]		= 'anmelden';
+$GLOBALS["tToSubmit"]		= 'um eine Umfrage einzutragen';
+
+?>
