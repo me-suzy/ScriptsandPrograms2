fc6fee67eca507ea58b8ae1e5949cb97838a58c4
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:32 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_checked.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_checked.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Kontroltidspunkt';
+$LANG['text_1'] = 'Angiv kontroltidspunkt for de markerede poster';
+$LANG['text_2'] = 'Kontrolleret: ';
+?>
