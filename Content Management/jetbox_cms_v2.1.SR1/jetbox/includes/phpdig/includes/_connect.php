2c6c5b3d545d492d198c891412d650a38c571a25
me-suzy <me.suzana@gmail.com>
2025-08-12 05:07:48 +0300

    Add Content Management/jetbox_cms_v2.1.SR1/jetbox/includes/phpdig/includes/_connect.php

---
+++ Content Management/jetbox_cms_v2.1.SR1/jetbox/includes/phpdig/includes/_connect.php
@@ -0,0 +1,29 @@
+<?php
+/*
+--------------------------------------------------------------------------------
+PhpDig 1.6.x
+This program is provided under the GNU/GPL license.
+See LICENSE file for more informations
+All contributors are listed in the CREDITS file provided with this package
+
+PhpDig Website : http://phpdig.toiletoine.net/
+Contact email : phpdig@toiletoine.net
+Author and main maintainer : Antoine Bajolet (fr) bajolet@toiletoine.net
+--------------------------------------------------------------------------------
+*/
+
+// Connection configuration
+define('PHPDIG_DB_PREFIX','<dbprefix>');
+define('PHPDIG_DB_HOST','<host>');
+define('PHPDIG_DB_USER','<user>');
+define('PHPDIG_DB_PASS','<pass>');
+define('PHPDIG_DB_NAME','<database>');
+
+//connection to the MySql server
+$id_connect = @mysql_connect (PHPDIG_DB_HOST,PHPDIG_DB_USER,PHPDIG_DB_PASS);
+if (!$id_connect) {
+die("Unable to connect to database : Check the connection script.\n");
+}
+//Select DataBase
+@mysql_select_db(PHPDIG_DB_NAME,$id_connect);
+?>
