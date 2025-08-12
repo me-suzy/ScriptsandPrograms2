f9eda7378d9e299499951b7d94702c79a969659c
me-suzy <me.suzana@gmail.com>
2025-08-12 23:50:23 +0300

    Add Site Navigation/folderlist-latest/Folder List/classes/config.inc.php

---
+++ Site Navigation/folderlist-latest/Folder List/classes/config.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+/* FolderList v1.1 - by Bruno_Funny $$ classes/config.inc.php */
+
+$ver = "FolderList v1.1";
+$by = "by Bruno_Funny";
+
+
+// Sets /////////////////////////////////////////
+
+$g 		= $_GET; // Set $g like the global var $_GET
+$url_to_domain 	= "http://www.yoursite.com/url/to/script"; // Set the url where this script is
+$url_to_files	= "http://www.yoursite.com/url/to/files/"; // Don't forget the / (slash) in the end of url
+$path_to_files	= "/real/path/to/your/files/into/your/server/"; // LINUX/BSD - Don't forget the / (slash) in the end of path
+// $path_to_files	= "c:\windows\path\style\"; // WINDOWS - Don't forget the / (slash) in the end of path
+
+// Colors /////////////////////////////////////////////////
+
+$bg					= "#D0CDC4";
+$alert				= "#000000";
+$link_color			= "#A69E8E";
+$list_bg  			= "#A69E8E";
+$list_bg2  			= "#F1F0ED";
+$list_header		= "#A69E8E";
+$list_header_txt	= "#FFFFFF";
+$list_bg_folders	= "#BBB5A8";
+$list_bg_files		= "#D4D0C8";
+$list_files			= "#000000";
+$list_folders		= "#000000";
+$alltextaround		= "#000000";
+
+?>
