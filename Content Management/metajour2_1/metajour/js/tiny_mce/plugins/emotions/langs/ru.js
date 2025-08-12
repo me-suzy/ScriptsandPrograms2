ba0251ca0eec17a2dcc6c43f449ca4f48fe6e3b3
me-suzy <me.suzana@gmail.com>
2025-08-12 06:33:59 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/emotions/langs/ru.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/emotions/langs/ru.js
@@ -0,0 +1,22 @@
+// RU lang variables cp1251
+
+tinyMCE.addToLang('emotions',{
+title : 'Âñòàâèòü ñìàéëèê',
+desc : 'Ñìàéëèêè',
+cool : 'Cool',
+cry : 'Ïëà÷',
+embarassed : 'Embarassed',
+foot_in_mouth : 'Foot in mouth',
+frown : 'Íàõìóðåííîñòü',
+innocent : 'Ñâÿòîé',
+kiss : 'Ïîöåëóé',
+laughing : 'Ñìåõ',
+money_mouth : 'Money mouth',
+sealed : 'Çàêëååííûé',
+smile : 'Óëûáêà',
+surprised : 'Ñþðïðèç',
+tongue_out : 'Âûñóíóòûé ÿçûê',
+undecided : 'Undecided',
+wink : 'Wink',
+yell : 'Yell'
+});
