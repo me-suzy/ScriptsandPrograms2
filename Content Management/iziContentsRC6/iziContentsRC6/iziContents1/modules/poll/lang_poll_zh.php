f48713fe323f18e20d698ef362aff4c163bbc3b0
me-suzy <me.suzana@gmail.com>
2025-08-12 04:01:02 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/poll/lang_poll_zh.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/poll/lang_poll_zh.php
@@ -0,0 +1,50 @@
+<?php
+
+
+//  ªí³æ¼ÐÃD
+$GLOBALS["tFormTitle"] = '§ë²¼ºÞ²z';
+$GLOBALS["tSubmitFormTitle"] = 'µo°_§ë²¼';
+
+//  ¼ÐÃD¦Cªí
+$GLOBALS["tQuestion"] = '°ÝÃD';
+$GLOBALS["tOption"] = '¿ï¶µ';
+$GLOBALS["tPostedBy"] = 'µo°_¤H';
+$GLOBALS["tPollType"] = '§ë²¼§Î¦¡';
+$GLOBALS["tPublishDate"] = 'µo°_¤é';
+$GLOBALS["tExpiryDate"] = '¨ì´Á¤é';
+$GLOBALS["tStatus"] = 'ª¬ºA';
+
+//  ¤u§@¦Cªí
+$GLOBALS["tAddNewPoll"] = '·s¼W§ë²¼';
+$GLOBALS["tEditPoll"] = '½s¿è§ë²¼';
+$GLOBALS["tDeletePoll"] = '§R°£§ë²¼';
+$GLOBALS["tReleasePoll"] = '¶}©ñ§ë²¼';
+$GLOBALS["tEditOptions"] = '½s¿è§ë²¼¿ï¶µ';
+$GLOBALS["tAddNewPollOption"] = '·s¼W§ë²¼¿ï¶µ';
+$GLOBALS["tEditPollOption"] = '½s¿è§ë²¼¿ï¶µ';
+$GLOBALS["tDeletePollOption"] = '§R°£§ë²¼¿ï¶µ';
+
+//  ªí³æÄæ¦ì¼ÐÃD
+$GLOBALS["tCategory"] = 'Ãþ§O';
+
+//  §ë²¼ÀËµø¤å¦r
+$GLOBALS["tMultiVotes"] = '¦h¿ï';
+$GLOBALS["tSingleVote"] = '³æ¿ï';
+$GLOBALS["tSubmitPoll"] = 'µo°_§ë²¼';
+$GLOBALS["tContact"] = 'ÁpÃ´';
+$GLOBALS["tEMail"] = 'E-Mail';
+$GLOBALS["tWebPage"] = 'ºô­¶';
+$GLOBALS["tVoteButton"] = '§ë²¼';
+
+//  §ë²¼µo°_¤å¦r
+$GLOBALS["tThankYou"] = '·PÁÂ±zµo°_§ë²¼.';
+$GLOBALS["tReleaseRequired"] = 'ª`·N¥²¶·µ¥¨ì¥»ª©½s¿è³\\¥i«á¤~·|Åã¥Ü¦b§ë²¼ºô­¶¤W.';
+$GLOBALS["tClickToReturn"] = 'ÂIÀ»³o¸Ì¥Hªð¦^';
+$GLOBALS["tPollPage"] = '§ë²¼ºô­¶';
+
+//  ¦w¥þ©Ê¤å¦r
+$GLOBALS["tMustLogin"] = '±z¥²¶·';
+$GLOBALS["tLogin"] = 'µn¤J';
+$GLOBALS["tToSubmit"] = '¥Hµo°_§ë²¼';
+
+?>
