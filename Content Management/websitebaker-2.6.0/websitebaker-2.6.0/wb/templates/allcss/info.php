538b66e86be735903412ca78be8814694c0ce90c
me-suzy <me.suzana@gmail.com>
2025-08-12 08:19:27 +0300

    Add Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/templates/allcss/info.php

---
+++ Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/templates/allcss/info.php
@@ -0,0 +1,34 @@
+<?php
+
+// $Id: info.php 138 2005-09-18 23:45:07Z ryan $
+
+/*
+
+ Website Baker Project <http://www.websitebaker.org/>
+ Copyright (C) 2004-2005, Ryan Djurovich
+
+ Website Baker is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Website Baker is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Website Baker; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+$template_directory = 'allcss';
+$template_name = 'All CSS';
+$template_version = '2.6';
+$template_platform = '2.6.x';
+$template_author = 'Ryan Djurovich';
+$template_license = 'GNU General Public License';
+$template_description = 'This template is designed with one goal in mind: to completely control lay-out with css. In case you were wondering, that is where the name came from.';
+
+?>
