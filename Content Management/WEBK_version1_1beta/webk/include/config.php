8aa652cf94a191854dc334b2bd67ec4fe90569e2
me-suzy <me.suzana@gmail.com>
2025-08-12 08:06:08 +0300

    Add Content Management/WEBK_version1_1beta/webk/include/config.php

---
+++ Content Management/WEBK_version1_1beta/webk/include/config.php
@@ -0,0 +1,49 @@
+<?php
+//---------------------------------------------------------------------------//
+// author: Yanick Bourbeau <ybourbeau@mrgtech.ca> 
+// date:   2005.09.30
+// web:    http://mrgibson.zapto.org
+// info:   WEBK
+//---------------------------------------------------------------------------//
+// copyleft license
+//
+// this software is provided 'as-is', without any express or implied
+// warranty. in no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions: 
+//
+// 1. the origin of this software must not be misrepresented;
+//	  you must not claim that you wrote the original software. 
+//	  if you use this software in a product, an acknowledgment 
+//	  in the product documentation would be appreciated but is not required.
+//
+// 2. altered source versions must be plainly marked as such, 
+//	  and must not be misrepresented as being the original software.
+//
+// 3. mail about the fact of using this class in production 
+//	  would be very appreciated.
+//
+// 4. this notice may not be removed or altered from any source distribution.
+//
+//---------------------------------------------------------------------------//
+
+	// PHP Directives
+	
+	error_reporting(E_ALL);
+	ini_set("display_errors",true);
+
+	// webk theme
+	define("WEBK_THEME","default");
+	define("WEBK_LEFT_WIDTH","0");
+	define("WEBK_RIGHT_WIDTH","160");	
+
+	define ("PATH_WEB","/www/vhosts/webk.zapto.org/htdocs/");
+	define ("PATH_ROOT","/www/vhosts/webk.zapto.org/webk/root/");
+	define ("PATH_TEMPLATE","/www/vhosts/webk.zapto.org/webk/template/default/");
+	define ("PATH_INCLUDE","/www/vhosts/webk.zapto.org/webk/include/");
+
+
+?>
