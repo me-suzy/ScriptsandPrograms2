71d473cd40cdb27b9790353afc152a843b7b5076
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:14 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/labelsavery7163.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/labelsavery7163.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342680;
+function objfile_342680 () {
+$obj = owNew('filter');
+$objdata['name'] = "Labels, Avery 7163";
+$objdata['datatype'] = "contact";
+$objdata['content'] = "";
+$objdata['mimetype'] = "application/msword";
+$objdata['classtype'] = "";
+$objdata['filtertype'] = "5";
+$objdata['filterfiletype'] = "rtf";
+$objdata['binfileid'] = "Avery7163.rtf";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
