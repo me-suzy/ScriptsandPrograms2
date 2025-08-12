f0455b40d6fdb0da8e77dee2bcb300fd7e8d4d99
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:19 +0300

    Add Content Management/metajour2_1/metajour/lang/letter.da.php

---
+++ Content Management/metajour2_1/metajour/lang/letter.da.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Breve';
+$LANG['label_contactid'] = 'Kontaktperson';
+$LANG['label_name'] = 'Navn';
+$LANG['label_content'] = 'Indhold';
+?>
