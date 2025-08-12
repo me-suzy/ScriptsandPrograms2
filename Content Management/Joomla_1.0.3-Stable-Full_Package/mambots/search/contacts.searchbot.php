e444684d443b909f68e1b8969cc35b197cfc21c6
me-suzy <me.suzana@gmail.com>
2025-08-12 05:37:15 +0300

    Add Content Management/Joomla_1.0.3-Stable-Full_Package/mambots/search/contacts.searchbot.php

---
+++ Content Management/Joomla_1.0.3-Stable-Full_Package/mambots/search/contacts.searchbot.php
@@ -0,0 +1,77 @@
+<?php
+/**
+* @version $Id: contacts.searchbot.php 85 2005-09-15 23:12:03Z eddieajau $
+* @package Joomla
+* @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* Joomla! is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details.
+*/
+
+// no direct access
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
+
+$_MAMBOTS->registerFunction( 'onSearch', 'botSearchContacts' );
+
+/**
+* Contacts Search method
+*
+* The sql must return the following fields that are used in a common display
+* routine: href, title, section, created, text, browsernav
+* @param string Target search string
+* @param string mathcing option, exact|any|all
+* @param string ordering option, newest|oldest|popular|alpha|category
+*/
+function botSearchContacts( $text, $phrase='', $ordering='' ) {
+	global $database, $my;
+
+	 $text = trim( $text );
+	if ($text == '') {
+		return array();
+	}
+
+	$section = _CONTACT_TITLE;
+
+	switch ( $ordering ) {
+		case 'alpha':
+			$order = 'a.name ASC';
+			break;
+		case 'category':
+			$order = 'b.title ASC, a.name ASC';
+			break;
+		case 'popular':
+		case 'newest':
+		case 'oldest':
+		default:
+			$order = 'a.name DESC';
+	}
+
+	$query = "SELECT a.name AS title,"
+	. "\n CONCAT_WS( ', ', a.name, a.con_position, a.misc ) AS text,"
+	. "\n '' AS created,"
+	. "\n CONCAT_WS( ' / ', '$section', b.title ) AS section,"
+	. "\n '2' AS browsernav,"
+	. "\n CONCAT( 'index.php?option=com_contact&task=view&&contact_id=', a.id ) AS href"
+	. "\n FROM #__contact_details AS a"
+	. "\n INNER JOIN #__categories AS b ON b.id = a.catid AND b.access <= '$my->gid'"
+	. "\n WHERE ( a.name LIKE '%$text%'"
+	. "\n OR a.misc LIKE '%$text%'"
+	. "\n OR a.con_position LIKE '%$text%'"
+	. "\n OR a.address LIKE '%$text%'"
+	. "\n OR a.suburb LIKE '%$text%'"
+	. "\n OR a.state LIKE '%$text%'"
+	. "\n OR a.country LIKE '%$text%'"
+	. "\n OR a.postcode LIKE '%$text%'"
+	. "\n OR a.telephone LIKE '%$text%'"
+	. "\n OR a.fax LIKE '%$text%' )"
+	. "\n AND a.published = 1"
+	. "\n ORDER BY $order"
+	;
+	$database->setQuery( $query );
+	$rows = $database->loadObjectList();
+	return $rows;
+}
+?>
