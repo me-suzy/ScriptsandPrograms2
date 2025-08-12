d0be4fcc0b893453f5b006996fd6034cc3fbaf8c
me-suzy <me.suzana@gmail.com>
2025-08-12 23:52:05 +0300

    Add Site Recommendation/phpfaber_cms_module_RecommendUs/RecommendUs/mod_config.php

---
+++ Site Recommendation/phpfaber_cms_module_RecommendUs/RecommendUs/mod_config.php
@@ -0,0 +1,33 @@
+<?php
+
+// -----------------------------------------------------------------------------
+//
+// phpFaber CMS v.1.0
+// Copyright(C), phpFaber LLC, 2004-2005, All Rights Reserved.
+// E-mail: products@phpfaber.com
+//
+// All forms of reproduction, including, but not limited to, internet posting,
+// printing, e-mailing, faxing and recording are strictly prohibited.
+// One license required per site running phpFaber CMS.
+// To obtain a license for using phpFaber CMS, please register at
+// http://www.phpfaber.com/i/products/cms/
+//
+// 10:51 PM 09/04/2005
+//
+// -----------------------------------------------------------------------------
+
+if (!defined('INDEX_INCLUDED')) die('<code>You cannot access this file directly..</code>');
+
+$_MOD = array(
+  'name'  => 'Recommend Us',
+  'descr' => 'This pretty simple module allows your visitors tell theirs friends about your site!',
+);
+
+$_MOD_CFG = array(
+  'EN_M_TF' => 1,
+  'FR_NUM' => 5,
+  'SYMBOL_LIMIT' => 255,
+
+);
+
+?>
