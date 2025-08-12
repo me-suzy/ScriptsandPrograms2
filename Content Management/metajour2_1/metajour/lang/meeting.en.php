3197fe6747770e8f512d44f2710d3866cfdd5207
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:31 +0300

    Add Content Management/metajour2_1/metajour/lang/meeting.en.php

---
+++ Content Management/metajour2_1/metajour/lang/meeting.en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Meetings';
+$LANG['label_companyid'] = 'Company';
+$LANG['label_name'] = 'Name';
+$LANG['label_visitdate'] = 'Date';
+$LANG['label_content'] = 'Content';
+?>
