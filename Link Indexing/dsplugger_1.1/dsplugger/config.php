45a5d931a630d3796170a12f1db7111f90c4bb34
me-suzy <me.suzana@gmail.com>
2025-08-12 19:15:28 +0300

    Add Link Indexing/dsplugger_1.1/dsplugger/config.php

---
+++ Link Indexing/dsplugger_1.1/dsplugger/config.php
@@ -0,0 +1,20 @@
+<?php
+// MySQL host -- "localhost" is right 99% of the time
+$db['host'] = "localhost";
+
+// MySQL username -- enter your mysql username between the double quotes
+$db['user'] = "username";
+
+// MySQL database -- enter your mysql database name between the double quotes
+$db['database'] = "database";
+
+// MySQL password -- enter your mysql password between the double quotes
+$db['password'] = "password";
+
+
+// DS Plugger admin username -- this is the username to access the admin panel
+$admin['user'] = "admin";  // CHANGE THIS!!
+
+// DS Plugger admin password -- this is the password to access the admin panel
+$admin['password'] = "password";  // CHANGE THIS!!
+?>
