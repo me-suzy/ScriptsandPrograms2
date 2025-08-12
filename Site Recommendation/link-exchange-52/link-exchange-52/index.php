06d089bdff8054d987312ea413073f6e73cd6cce
me-suzy <me.suzana@gmail.com>
2025-08-12 23:51:49 +0300

    Add Site Recommendation/link-exchange-52/link-exchange-52/index.php

---
+++ Site Recommendation/link-exchange-52/link-exchange-52/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="refresh" content="0;URL=http:free_link_exchange.php">
+</head>
+<body>
+If your browser does not automatically redirect you in 5 seconds,
+click <a href="free_link_change.php">here</a> to go to the new site.
+</body>
+</html>
