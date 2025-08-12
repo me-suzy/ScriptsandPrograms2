8b3f6d34b0f2d6e12ea399e270251beb9eda69c7
me-suzy <me.suzana@gmail.com>
2025-08-12 14:32:42 +0300

    Add File Manipulation/free_download_site_v1.0/Free download site/my_downloads/root.php

---
+++ File Manipulation/free_download_site_v1.0/Free download site/my_downloads/root.php
@@ -0,0 +1,32 @@
+<?php
+/*
+    Copyright (C) 2005 Adullam Limited., All Rights Reserved.
+
+    Unless explicitly acquired and licensed from Licensor under the Technical Pursuit License ("TPL") 
+    Version 1.0 or greater, the contents of this file are subject to the Reciprocal Public License 
+    ("RPL") Version 1.1, or subsequent versions as allowed by the RPL, and You may not copy or use 
+    this file in either source code or executable form, except in compliance with the terms and 
+    conditions of the RPL.
+
+    You may obtain a copy of both the TPL and the RPL (the "Licenses") from Technical Pursuit Inc. 
+    at http://www.technicalpursuit.com.
+
+    All software distributed under the Licenses is provided strictly on an "AS IS" basis, WITHOUT 
+    WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND Adullam Limited HEREBY DISCLAIMS ALL 
+    SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+    PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the Licenses for specific 
+    language governing rights and limitations under the Licenses. 
+    
+    Project Name: Download Site System
+    File: root.php
+    File-Version: 1.0
+    Details: this is a demo of adding extra content.
+*/
+?>
+<hr />
+<p>This is the "root" or general folder from here you can 
+get ever more specialised to a depth of 6 folders deep.  
+Advanced users will be able to change this value at thier 
+own risk in the index file for all download folders.</p>
+<p>This message has been added by adding a file that ends .php
+you can remove it and the message will be gone.</p>
