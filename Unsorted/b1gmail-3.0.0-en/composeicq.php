e58e2780e070018e4842fe187087e946c6f292a6
me-suzy <me.suzana@gmail.com>
2025-08-13 01:10:09 +0300

    Add Unsorted/b1gmail-3.0.0-en/composeicq.php

---
+++ Unsorted/b1gmail-3.0.0-en/composeicq.php
@@ -0,0 +1,26 @@
+<? header("Cache-Control: no-cache"); 
+ session_start();
+ include ("config.inc.php");
+
+$nmail = 0;
+
+
+$usermail = strtolower($user."@".$sdomain);
+
+
+
+ $filename = "templates/${template}/composeicq.htm";
+ $fd = fopen ($filename, "r");
+  $tmpl = fread ($fd, filesize ($filename));
+ fclose ($fd); 
+
+ $dat = date("d.m.Y");
+
+  $output = str_replace ( "%VON%", "$usermail", $tmpl);
+  $output = str_replace ( "%DATUM%", "$dat", $output);
+  $output = str_replace ( "%COPYRIGHT%", "$copyright", $output);
+
+  $output = stripslashes ($output);
+ 
+  echo ($output);
+?>
