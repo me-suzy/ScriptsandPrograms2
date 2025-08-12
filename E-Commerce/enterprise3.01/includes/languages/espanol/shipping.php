22da4b9e23ba1ddc0b78c8e97c2fdf0430b69556
me-suzy <me.suzana@gmail.com>
2025-08-12 11:57:31 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/shipping.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/shipping.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Env&iacute;os y Devoluciones');
+define('HEADING_TITLE', 'Env&iacute;os y Devoluciones');
+
+define('TEXT_INFORMATION', 'Ponga aqui informaci&oacute;n sobre los Env&iacute;os y Devoluciones');
+?>
