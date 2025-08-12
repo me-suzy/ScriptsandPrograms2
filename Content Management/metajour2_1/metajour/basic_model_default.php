9bdf371efa1c73a87ead57bbd8d8b129d27c09bf
me-suzy <me.suzana@gmail.com>
2025-08-12 05:54:34 +0300

    Add Content Management/metajour2_1/metajour/basic_model_default.php

---
+++ Content Management/metajour2_1/metajour/basic_model_default.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage model
+ */
+
+require_once('basic_model.php');
+
+class basic_model_default extends basic_model {
+
+	function model() {
+		foreach($this->objectid as $curid) {
+			$obj = owRead($curid);
+			$obj->setdefault();
+		}
+	}
+	
+}
+
+?>
