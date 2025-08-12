f203b035230f1d5296f612e22a289176bab7ebe7
me-suzy <me.suzana@gmail.com>
2025-08-13 00:07:43 +0300

    Add Unsorted/100jancms/access_denied.php

---
+++ Unsorted/100jancms/access_denied.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Access denied!</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link href="cms_style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body class="maintext" scroll="auto">
+<script language="JavaScript" type="text/JavaScript">
+setTimeout("top.location = 'index.php'", 3000);
+</script>
+<br>
+<center>
+<span class="red">Access denied! <img src="images/app/no_good.jpg" width="16" height="16" align="absmiddle"></span>
+</center>
+</body>
+</html>
