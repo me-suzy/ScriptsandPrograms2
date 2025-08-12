9bab62ddf0ec59271f4e2697be414cdd1b77d023
me-suzy <me.suzana@gmail.com>
2025-08-12 12:37:22 +0300

    Add E-Commerce/shop-script-free/cfg/appearence.inc.php

---
+++ E-Commerce/shop-script-free/cfg/appearence.inc.php
@@ -0,0 +1,9 @@
+<?php
+	define('CONF_PRODUCTS_PER_PAGE', '8');
+	define('CONF_COLUMNS_PER_PAGE', '2');
+	define('CONF_SHOW_ADD2CART', '1');
+	define('CONF_SHOW_BEST_CHOICE', '1');
+	define('CONF_DARK_COLOR', 'CD8181');
+	define('CONF_MIDDLE_COLOR', 'F0B5B5');
+	define('CONF_LIGHT_COLOR', 'FFEEEE');
+?>
