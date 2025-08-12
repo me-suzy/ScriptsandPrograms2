95a5055636772f4d87b97879b5c1c8449216a614
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:08 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_tagcategories.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_tagcategories.php
@@ -0,0 +1,21 @@
+<?php
+
+// 18-9-2003 23:04
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Vertaal tag categorieën';
+
+//  List Headings
+$GLOBALS["tCatDesc"] = 'Categorie naam';
+
+//  List Functions
+$GLOBALS["tViewCategory"] = 'Bekijk tag categorie';
+$GLOBALS["tEditCategory"] = 'Vertaal tag categorie';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Met dit formulier kun u tag categorie namen vertalen.';
+
+//  Error Messages
+$GLOBALS["eNoTagcategoryDesc"] = 'U moet een vertaling geven voor elke taal';
+
+?>
