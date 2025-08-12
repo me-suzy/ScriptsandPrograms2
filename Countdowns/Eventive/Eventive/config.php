13520afc7bfd16a269514d2ff6ef69bc664c5b7f
me-suzy <me.suzana@gmail.com>
2025-08-12 08:26:11 +0300

    Add Countdowns/Eventive/Eventive/config.php

---
+++ Countdowns/Eventive/Eventive/config.php
@@ -0,0 +1,22 @@
+<?PHP
+
+//---------------------------------------------------------
+// EVENTIVE
+// Version v0.1
+//
+// Written by Andrew Whitehead
+// (c) Andrew Whitehead 2004
+//
+// THIS TAG MUST NOT BE REMOVED
+//---------------------------------------------------------
+
+//Change the below variables to your liking
+
+$dbhost = "localhost";                                       	//Usually localhost
+$dbname = "eventive";                                        	//Name of the database
+$dbuser = "user";                                     		//Name of the database user
+$dbpass = "pass";                                          	//Password of the user
+
+$defaultformat = "0";                                        	//The default layout for the countdown
+
+?>
