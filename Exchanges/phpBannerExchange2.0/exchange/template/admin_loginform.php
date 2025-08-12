5b1cd35bddfaa39cda0bb73b61d22fe8e4c65f34
me-suzy <me.suzana@gmail.com>
2025-08-12 13:59:37 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/template/admin_loginform.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/template/admin_loginform.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>{title}</title>
+<link rel="stylesheet" href="{baseurl}/template/css/{css}" type="text/css">
+</head>
+<body leftmargin="0" topmargin="0" marginwidth="0" 
+  marginheight="0" >
+<div id="content">
+<div class="main">
+<table border="0" cellpadding="1" width="650" cellspacing="0">
+<tr>
+<td>
+<table cellpadding="5" border="1" width="100%" cellspacing="0">
+<tr>
+<td colspan="2" class="tablehead"><center><div class="head">{title}</center></div></td>
+</tr>
+<td class="tablebody" colspan="2">
+<div class="mainbody">
+<table border="0" cellpadding="1" cellspacing="1" style="border-collapse: collapse"  width="90%">
+  <tr>
+<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" width="90%" >
+<tr>
+<form action="{baseurl}/admin/stats.php" method="post">
+<center><hr width="550" size="1"></center>
+<table border="0" cellpadding="0" width="200" align="center" cellspacing="0">
+<tr><td class="tablehead" colspan="2"><div class="littlehead">{shorttitle}</div></td></tr>
+    <tr><td class="tablebody">{login}</td><td class="tablebody"><input class="formbox" type="text" name="login" size="12" maxlength="20"></td></tr>
+    <tr><td td class="tablebody">{pass}</td><td class="tablebody"><input class="formbox" type="password" name="pass" size="12" maxlength="20"></td></tr>
+    <tr><td colspan=2 class="tablebody"><center>
+	<input class="button" type="submit" value="{login}"></center></form>
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<div class="footer">
+{footer}
+</div>
+</div>
