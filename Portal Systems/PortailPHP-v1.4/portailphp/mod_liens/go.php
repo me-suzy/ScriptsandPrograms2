35fb2a00988e102cf38329bf1edbff7a69add80e
me-suzy <me.suzana@gmail.com>
2025-08-12 23:17:24 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_liens/go.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_liens/go.php
@@ -0,0 +1,42 @@
+<?php
+/*******************************************************************************
+ * Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr
+ * http://www.yoopla.net/portailphp/
+ *
+ * Modifié par Martineau Emeric Copyright (C) 2004
+ *
+ * Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le
+ * modifier conformément aux dispositions de la Licence Publique Générale GNU,
+ * telle que publiée par la Free Software Foundation ; version 2 de la licence,
+ * ou encore (à votre choix) toute version ultérieure.
+ *
+ * Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE
+ * GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou
+ * D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence
+ * Publique Générale GNU .
+ *
+ * Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en
+ * même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free
+ * Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+ *
+ * Portail PHP
+ * La présente Licence Publique Générale n'autorise pas le concessionnaire à
+ * incorporer son programme dans des programmes propriétaires. Si votre programme
+ * est une bibliothèque de sous-programmes, vous pouvez considérer comme plus
+ * intéressant d'autoriser une édition de liens des applications propriétaires
+ * avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non
+ * pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+ ***********************************************************************************/
+session_start() ;
+$chemin = ".." ;
+include "$chemin/include/config.php" ;
+include "$chemin/include/fonctions.php" ;
+include "$chemin/include/" . $_SESSION["App_Langue"] ;
+
+$sql_id = sql_connect($BD_host, $BD_name, $BD_user, $BD_pass) ;
+$res = sql_query("UPDATE $BD_Tab_liens SET LI_clic=LI_clic + 1 WHERE LI_uid ='" . $_GET["id"] . "'", $sql_id) or die("$Err_Modif") ;
+@mysql_close() ;
+
+
+die(header("Location: " . $_GET["URL"])) ;
+?>
