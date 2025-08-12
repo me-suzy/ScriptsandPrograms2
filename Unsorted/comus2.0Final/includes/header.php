bca6f47da866fbb1fee8aba7d3d9814619ed08ce
me-suzy <me.suzana@gmail.com>
2025-08-13 01:51:24 +0300

    Add Unsorted/comus2.0Final/includes/header.php

---
+++ Unsorted/comus2.0Final/includes/header.php
@@ -0,0 +1,9 @@
+<HTML>
+<HEAD>
+
+<BASE HREF="http://your_domain.com">
+
+<TITLE>Comus TGP Script</TITLE>
+<LINK REL="stylesheet" HREF="includes/gnat-tgp-style.css" TYPE="text/css">
+</HEAD>
+<BODY LEFTMARGIN="0" RIGHTMARGIN="0" TOPMARGIN="0" BOTTOMMARGIN="0">
