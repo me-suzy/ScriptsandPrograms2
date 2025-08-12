da80610003ac766cca650a49d309c9ea50ff92eb
me-suzy <me.suzana@gmail.com>
2025-08-12 03:47:09 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/include/tinymce/plugins/filemanager/InsertFile/lang/lang-de.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/include/tinymce/plugins/filemanager/InsertFile/lang/lang-de.php
@@ -0,0 +1,78 @@
+<?php
+// $Id: lang-de.php,v 1.1.1.1 2005/05/11 11:02:09 saschat Exp $
+/**
+* HTMLArea3 XTD addon - FileManager
+* Based on AlRashid's FileManager
+* @package Mambo Open Source
+* @Copyright © 2004 Bernhard Pfeifer aka novocaine
+* @ All rights reserved
+* @ Mambo Open Source is Free Software
+* @ Released under GNU/GPL License : http://www.gnu.org/copyleft/gpl.html
+* @version $Revision: 1.1.1.1 $
+**/
+$MY_MESSAGES = array();
+$MY_MESSAGES['extmissing'] = 'Bitte laden Sie eine Datei mit einer Dateiendung hoch, z. B. "bild.jpg".';
+$MY_MESSAGES['loading'] = 'Lade Dateien...';
+$MY_MESSAGES['uploading'] = 'Lade hoch...';
+$MY_MESSAGES['nopermtodeletefile'] = 'Keine ausreichende Berechtigung zum Löschen der Datei.';
+$MY_MESSAGES['filenotfound'] = 'Datei nicht gefunden.';
+$MY_MESSAGES['unlinkfailed'] = 'Löschen fehlgeschlagen.';
+$MY_MESSAGES['rmdirfailed'] = 'Verzeichnis löschen fehlgeschlagen.';
+$MY_MESSAGES['nopermtodeletefolder'] = 'Keine ausreichende Berechtigung zum Löschen des Verzeichnisses.';
+$MY_MESSAGES['foldernotfound'] = 'Verzeichnis nicht gefunden.';
+$MY_MESSAGES['foldernotempty'] = 'Verzeichnis ist nicht leer.\nBitte löschen Sie zuerst alle darin enthaltenen Dateien.';
+$MY_MESSAGES['nopermtocreatefolder'] = 'Keine ausreichende Berechtigung zum Erstellen eines Verzeichnisses.';
+$MY_MESSAGES['pathnotfound'] = 'Pfad nicht gefunden.';
+$MY_MESSAGES['foldernamemissing'] = 'Verzeichnisname fehlt.';
+$MY_MESSAGES['folderalreadyexists'] = 'Verzeichnis existiert bereits.';
+$MY_MESSAGES['mkdirfailed'] = 'Verzeichnis erstellen fehlgeschlagen.';
+$MY_MESSAGES['nopermtoupload'] = 'Keine ausreichende Berechtigung zum Hochladen von Dateien.';
+$MY_MESSAGES['extnotallowed'] = 'Dateien mit dieser Dateiendung sind nicht erlaubt.';
+$MY_MESSAGES['filesizeexceedlimit'] = 'Datei überschreitet die zulässige Kapazität.';
+$MY_MESSAGES['filenotuploaded'] = 'Datei wurde nicht hochgeladen.';
+$MY_MESSAGES['nofiles'] = 'Keine Dateien...';
+$MY_MESSAGES['configproblem'] = 'Konfigurationsproblem ';
+$MY_MESSAGES['deletefile'] = 'Datei löschen';
+$MY_MESSAGES['deletefolder'] = 'Verzeichnis löschen';
+$MY_MESSAGES['refresh'] = 'Aktualisieren';
+$MY_MESSAGES['folder'] = 'Verzeichnis';
+$MY_MESSAGES['type'] = 'Typ';
+$MY_MESSAGES['name'] = 'Name';
+$MY_MESSAGES['size'] = 'Grösse';
+$MY_MESSAGES['datemodified'] = 'Geändert am';
+$MY_MESSAGES['url'] = 'Pfad';
+$MY_MESSAGES['comment'] = 'Kommentar';
+$MY_MESSAGES['caption'] = 'Text';
+$MY_MESSAGES['upload'] = 'Hochladen';
+$MY_MESSAGES['insertfile'] = "Datei einfügen";
+$MY_MESSAGES['filemanager'] = "Datei Manager";
+$MY_MESSAGES['directory'] = "Verzeichnis";
+$MY_MESSAGES['enterurl'] = "Bitte geben Sie eine URL ein.";
+$MY_MESSAGES['entercaption'] = 'Bitte geben Sie einen Text für die Datei ein.';
+$MY_MESSAGES['inserticon'] = 'Icon für Dateityp hinzufügen';
+$MY_MESSAGES['insertsize'] = 'Angaben über Dateigrösse hinzufügen';
+$MY_MESSAGES['insertdate'] = 'Angaben über Änderungsdatum hinzufügen';
+$MY_MESSAGES['newfolder'] = 'Name des Verzeichnisses:';
+$MY_MESSAGES['cancel'] = 'Abbrechen';
+$MY_MESSAGES['inserticon'] = 'Icon Dateityp einfügen';
+$MY_MESSAGES['insertsize'] = 'Dateigrösse anzeigen';
+$MY_MESSAGES['insertdate'] = 'Einfügedatum anzeigen';
+$MY_MESSAGES['cancel'] = 'Abbrechen';
+$MY_MESSAGES['ok'] = 'Einfügen';
+$MY_MESSAGES['reset'] = 'Zurücksetzen';
+$MY_MESSAGES['delete'] = '';
+$MY_MESSAGES['folders'] = 'Ordner löschen ';
+$MY_MESSAGES['files'] = 'Datei löschen ';
+$MY_MESSAGES['rename'] = 'Umbenennen';
+$MY_MESSAGES['renamefailed'] = 'Umbenennen fehlgeschlagen';
+$MY_MESSAGES['move'] = 'Verschieben';
+$MY_MESSAGES['nopermtomove'] = 'Keine Berechtigung zum Verschieben.';
+$MY_MESSAGES['newfolder'] = 'Neuer Ordner';
+$MY_MESSAGES['newfoldernamemissing'] = 'Neuer Ordnername fehlt!';
+$MY_MESSAGES['renamefolder'] = 'Neuer Ordnername:';
+$MY_MESSAGES['renamewarning'] = 'Warning!\n Renaming or moving folders and files will break existing links in your documents. Continue?';
+$MY_MESSAGES['renamefile'] = 'Neuer Dateiname:';
+$MY_MESSAGES['nopermtorename'] = 'Keine Berechtigung zum Umbenennen.';
+$MY_MESSAGES['newfilenamemissing'] = 'Neuer Dateiname fehlt!';
+
+?>
