dba0dd8d3dc9580299a56fd1e712d5adfab1f034
me-suzy <me.suzana@gmail.com>
2025-08-12 12:00:53 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/shipping.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/shipping.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Liefer- und Versandkosten');
+define('HEADING_TITLE', 'Liefer- und Versandkosten');
+
+define('TEXT_INFORMATION', 'F&uuml;gen Sie hier Ihre Informationen &uuml;ber Liefer- und Versandkosten ein.');
+?>
