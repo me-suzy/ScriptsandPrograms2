257960f214004d26af62d337541b72cef72c2832
me-suzy <me.suzana@gmail.com>
2025-08-12 16:45:26 +0300

    Add Groupware Tools/CuteFlow_bin_v150/pages/deleteslot.php

---
+++ Groupware Tools/CuteFlow_bin_v150/pages/deleteslot.php
@@ -0,0 +1,65 @@
+<?php
+	/** Copyright (c) 2003, 2004 EMEDIA OFFICE GmbH. All rights reserved.
+	*
+	* Redistribution and use in source and binary forms, with or without 
+	* modification, are permitted provided that the following conditions are met:
+	* 
+	*  o Redistributions of source code must retain the above copyright notice, 
+	*    this list of conditions and the following disclaimer. 
+	*     
+	*  o Redistributions in binary form must reproduce the above copyright notice, 
+	*    this list of conditions and the following disclaimer in the documentation 
+	*    and/or other materials provided with the distribution. 
+	*     
+	*  o Neither the name of EMEDIA OFFICE GmbH nor the names of 
+	*    its contributors may be used to endorse or promote products derived 
+	*    from this software without specific prior written permission. 
+	*     
+	* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+	* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+	* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+	* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+	* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+	* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+	* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+	* OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+	* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+	* OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+	* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	*/
+	
+	//--- write user to database
+	include ("../config/config.inc.php");
+	include ("../language_files/".$_REQUEST["language"]."/gui.inc.php");
+
+	//--- open database
+	$nConnection = mysql_connect($DATABASE_HOST, $DATABASE_UID, $DATABASE_PWD);
+	
+	if ($nConnection)
+	{
+		//--- get maximum count of users
+		if (mysql_select_db($DATABASE_DB, $nConnection))
+		{
+			$query = "DELETE FROM cf_formslot WHERE nID=".$_REQUEST["slotid"];
+			mysql_query($query, $nConnection);
+			
+			$query = "DELETE FROM cf_slottofield WHERE nSlotId=".$_REQUEST["slotid"];
+			mysql_query($query, $nConnection);
+		}
+	}
+?>
+
+<html>
+	<head>
+		<script language="Javascript">
+		<!--
+			function loadNext()
+			{
+				document.location.href="edittemplate_step2.php?language=<?php echo $_REQUEST["language"];?>&start=<?php echo $_REQUEST["start"];?>&sortby=<?php echo $_REQUEST["sortby"];?>&templateid=<?php echo $_REQUEST["tamplateid"];?>&reload=1";
+			}
+		//-->
+		</script>
+	</head>
+	<body onload="window.setTimeout('loadNext()',1000);">
+	</body>
+</html>
