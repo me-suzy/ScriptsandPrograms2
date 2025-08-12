997bc09076cdd2d33861a90828069ce95a35ff1e
me-suzy <me.suzana@gmail.com>
2025-08-12 15:09:09 +0300

    Add Financial Tools/echeckbook/echeckbook/config.php

---
+++ Financial Tools/echeckbook/echeckbook/config.php
@@ -0,0 +1,17 @@
+<?
+
+// READ THE READ-ME.TXT IF YOU HAVEN'T ALREADY
+
+// MySQL Host
+$sql_host = "localhost";
+
+// MySQL Database
+$sql_db = "DATABASE";
+
+// MySQL Username
+$sql_user = "USERNAME";
+
+// MySQL Password
+$sql_pass = "PASSWORD";
+
+?>
