2d7f0c22f106aa05ebf3df201ec787019e74b1fe
me-suzy <me.suzana@gmail.com>
2025-08-12 19:47:51 +0300

    Add News Publishing/GENU-2.2/GENU-2.2/sql/smilies.sql

---
+++ News Publishing/GENU-2.2/GENU-2.2/sql/smilies.sql
@@ -0,0 +1,30 @@
+# --------------------------------------------------------------
+#
+# $Id: smilies.sql,v 1.1 2005/03/28 12:54:25 raoul Exp $
+#
+# Copyright:	(C) 2003-2005 Raoul Proença <raoul@genu.org>
+# License:	GNU GPL (see COPYING)
+# Website:	http://genu.org/
+#
+# --------------------------------------------------------------
+
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':)', './../images/smilies/smile.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':(', './../images/smilies/sad.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':D', './../images/smilies/biggrin.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (';)', './../images/smilies/wink.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':o', './../images/smilies/surprised.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES ('8)', './../images/smilies/cool.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':|', './../images/smilies/neutral.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':p', './../images/smilies/razz.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':lol:', './../images/smilies/lol.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':confused:', './../images/smilies/confused.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':rolleyes:', './../images/smilies/rolleyes.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':mad:', './../images/smilies/mad.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':shock:', './../images/smilies/shock.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':cry:', './../images/smilies/cry.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':redface:', './../images/smilies/redface.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':evil:', './../images/smilies/evil.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':!:', './../images/smilies/exclaim.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':?:', './../images/smilies/question.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':idea:', './../images/smilies/idea.gif');
+INSERT INTO genu_smilies (smiley_code, smiley_image) VALUES (':arrow:', './../images/smilies/arrow.gif');
