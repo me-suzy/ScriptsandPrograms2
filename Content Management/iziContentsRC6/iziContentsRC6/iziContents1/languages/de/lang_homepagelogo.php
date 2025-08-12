5f91c7caf82410c8aa60573ef379ddadd0b6994d
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:22 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_homepagelogo.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_homepagelogo.php
@@ -0,0 +1,12 @@
+<?
+
+// Form Title (Formular-Titel)
+$GLOBALS["tFormTitle"]		= 'Webpr&auml;senz Logobild einstellen';
+
+// Form Field Headings (Formular-Feld Beschriftungen)
+$GLOBALS["tHomepageLogo"]	= 'Webpr&auml;senz Logobild';
+
+// Form Text Description (Formular-Text Beschreibungen)
+$Globals["tDetails"]		= 'Hier k&ouml;nnen Sie das Logobild ihrer Webpr&auml;senz einstellen.<br> Bitte beachten Sie, das das Bild zun&auml;chst mit der Bild-Hochlade-Funktion in ezContents abgespeichert werden mu&szlig;.';
+
+?>
