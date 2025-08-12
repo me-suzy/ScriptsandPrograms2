7602335c95c481aa730b371ca759cb4b38c7f119
me-suzy <me.suzana@gmail.com>
2025-08-12 18:46:16 +0300

    Add Interactive Stories/FicHive_v1/skins/1/category.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/category.tmpl.php
@@ -0,0 +1,9 @@
+<center><%IMG%></center>
+<%SUBCATS%>
+<table border='0' width='100%'><tr>
+<td width='50%'><%MODERATORS%></td>
+<td width='50%'><div align='right'><%PAGES%></div></td>
+</tr><tr>
+<td colspan='2'><center><%SORTER%> <%GENRE%> <%CHARACTER%></div></td></tr></table>
+<table border='0' width='100%'><tr><td class='frame'><%STORIES%></td></tr></table>
+<%MODERATOROPTIONS%>
