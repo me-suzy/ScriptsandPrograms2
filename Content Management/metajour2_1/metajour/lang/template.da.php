9489ef8f7fa4320ee4c834c2355ab51f0d01ffbe
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:13 +0300

    Add Content Management/metajour2_1/metajour/lang/template.da.php

---
+++ Content Management/metajour2_1/metajour/lang/template.da.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Navn på skabelon';
+$LANG['label_templatefile'] = 'Fil';
+$LANG['label_content'] = 'Skabelon indhold';
+$LANG['label_param'] = 'Parametre';
+$LANG['label_setting'] = 'Parameterværdier';
+$LANG['label_tpltype'] = 'Skabelon type';
+$LANG['option_tpltype']['0'] = 'Sideskabelon';
+$LANG['option_tpltype']['1'] = 'Delskabelon';
+$LANG['option_tpltype']['2'] = 'Komponentskabelon';
+$LANG['name'] = 'Skabeloner';
+$LANG['label_header'] = 'Header';
+$LANG['label_style'] = 'Typografi CSS';
+$LANG['label_doctype'] = 'Doctype';
+$LANG['label_config'] = 'Konfigurering';
+$LANG['label_htmledit'] = 'HTML redigering';
+?>
