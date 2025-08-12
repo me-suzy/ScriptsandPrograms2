be9292ad2185f19e2cca008af7aa58ab544eee5d
me-suzy <me.suzana@gmail.com>
2025-08-12 19:52:24 +0300

    Add News Publishing/newspro115/1.1.5/upload/footer.php

---
+++ News Publishing/newspro115/1.1.5/upload/footer.php
@@ -0,0 +1,20 @@
+<br />
+<center>
+<div align="center" class="fbox smallfont">
+<!-- Copyright -->
+Powered By: <a href="about.php">Utopia News Pro</a> <?php echo $version ?><br />
+Copyright &copy;2003 - 2005, UtopiaSoft, UtopiaSoftware.net
+<!-- Copyright -->
+</div></center>
+<?php
+if (DEV_BUILD == true)
+{
+?>
+<br />
+<center><div align="center" class="qbox smallfont">
+Page generated using <?php echo $query_count ?> queries</div></center>
+<?php
+}
+?>
+</body>
+</html>
