5dd22f3d7ffe0c9dfc1491cf66debc92eb41326d
me-suzy <me.suzana@gmail.com>
2025-08-12 23:28:35 +0300

    Add Postcards/webcards_1_final_fixed/webcards_1_final_fixed/upload/lang/English/admin/ad_users.php

---
+++ Postcards/webcards_1_final_fixed/webcards_1_final_fixed/upload/lang/English/admin/ad_users.php
@@ -0,0 +1,27 @@
+<?php
+$lang['add_admin'] = "Add An Administrator";
+$lang['select_default_lang'] = "Select a default language for this administrator";
+$lang['fill_admin_details'] = "Fill in the new administrator details below";
+$lang['add_user'] = "Add this user";
+$lang['new_user_ok'] = "New user addedd successfully";
+$lang['no_open_lang_dir'] = "Unable to open the lang directory";
+$lang['no_enter_username'] = "You did not enter a username for the new admin.";
+$lang['back_enter_username'] = "Go back and enter a username.";
+$lang['no_pw1'] = "You did not fill in the first password field.";
+$lang['back_enter_pw1'] = "Go back and fill in the first password field.";
+$lang['no_pw2'] = "You did not fill in the second password field.";
+$lang['back_enter_pw2'] = "Go back and fill in the second password field.";
+$lang['pw_no_match'] = "The two password fields do not match.";
+$lang['back_enter_matching_pw'] = "Go back and ensure you enter the same password in both password fields";
+$lang['choose_admin'] = "Choose an administrator";
+$lang['del_admin'] = "Delete An Administrator";
+$lang['choose_admin_del'] = "Choose an admininstrator to delete.";
+$lang['confirm_del_admin'] = "Are you sure you wish to delete this administrator?";
+$lang['no_admin_db'] = "There are currently no administrators in the database.";
+$lang['add_some_admins'] = "Add some administrators to the database.";
+$lang['no_admin_spec'] = "You did not specify an administrator to delete.";
+$lang['back_choose_admin'] = "Go back and choose an administrator.";
+$lang['del_base_admin'] = "You have chosen to delete the base administrator. The base administrator cannot be deleted.";
+$lang['choose_other_admin'] = "Select another administrator to delete.";
+$lang['admin_deleted'] = "Administrator was sucessfully deleted.";
+?>
