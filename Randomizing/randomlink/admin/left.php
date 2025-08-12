dffdeb84bf05c800394282037e78ee110384e9f4
me-suzy <me.suzana@gmail.com>
2025-08-12 23:33:24 +0300

    Add Randomizing/randomlink/admin/left.php

---
+++ Randomizing/randomlink/admin/left.php
@@ -0,0 +1,10 @@
+<table border='2' width='100%' bordercolor='red'>
+<tr>
+<td bgcolor='red'><font color='white'><b>Admin Options</b></font></td></tr>
+<tr><td bgcolor='white'>
+<A href='addlink.php'>Add link</a><br>
+<A href='validate.php'>Validate Links</a><br>
+<A href='deletelink.php'>Delete link</a><br>
+<A href='search.php'>Search links</a><br>
+<A href='prunestats.php'>Prune stats</a><br>
+</td></tr></table>
