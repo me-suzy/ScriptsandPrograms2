8e5ea964d3bc3905a486092712a3253b658a6630
me-suzy <me.suzana@gmail.com>
2025-08-12 12:04:12 +0300

    Add E-Commerce/enterprise3.01/install/templates/main_page/header.php

---
+++ E-Commerce/enterprise3.01/install/templates/main_page/header.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+?>
+<!-- header //-->
+
+<p>&nbsp;</p>
+
+<table width="75%" cellpadding="0" cellspacing="0" border="0" align="center">
+  <tr>
+    <td><h2>Enterprise Shopping Cart Software Installer</h2>
+<!-- header_eof //-->
