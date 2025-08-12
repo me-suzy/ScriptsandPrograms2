7a97e93219038200efb7acc7498a708288533d56
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:36 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/moduleref.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/moduleref.php
@@ -0,0 +1,15 @@
+<?php
+
+$GLOBALS["ModuleRef"]  = 'links';
+
+$GLOBALS["RestrictTags"]	= 'C';
+$GLOBALS["PermittedTags"]	= array( 'code',		//	pre-compiled tags
+									 'link',
+									 'ilink',
+									 'hovernote',
+									 'download',
+									 'email',
+									 'year'			//	run-time tags
+								   ) ;
+
+?>
