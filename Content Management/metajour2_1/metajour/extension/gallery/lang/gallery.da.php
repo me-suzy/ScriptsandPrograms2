bd3dc157579bda9aa00c2dc34d2fbfe719e21b06
me-suzy <me.suzana@gmail.com>
2025-08-12 06:13:41 +0300

    Add Content Management/metajour2_1/metajour/extension/gallery/lang/gallery.da.php

---
+++ Content Management/metajour2_1/metajour/extension/gallery/lang/gallery.da.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['label_name'] = 'Konfigurationsnavn';
+$LANG['label_title'] = 'Titel';
+$LANG['label_folderid'] = 'Mappe med billeder';
+$LANG['label_xnum'] = 'Antal billeder horisontalt';
+$LANG['label_ynum'] = 'Antal billeder vertikalt';
+$LANG['label_thumbsize'] = 'Max størrelse for miniaturer (pixels)';
+$LANG['label_mediumsize'] = 'Max størrelse for medium-billeder (pixels)';
+$LANG['label_usemedium'] = 'Vis medium-billeder';
+$LANG['label_usefull'] = 'Vis original-billeder';
+$LANG['label_linktofull'] = 'Link til originalbilleder';
+$LANG['label_fullnewwindow'] = 'Vis original-billeder i nyt vindue';
+$LANG['label_mediumnewwindow'] = 'Vis medium-billeder i nyt vindue';
+
+$LANG['label_addcomment'] = 'Tillad tilføjelse af kommentarer';
+$LANG['label_listcomment'] = 'Tillad visning af kommentarer';
+$LANG['label_commentpos'] = 'Placering af kommentarer';
+$LANG['option_commentpos'][0] = 'Under billede';
+
+$LANG['label_templateid_index'] = 'Skabelon for index-visning';
+$LANG['label_templateid_medium'] = 'Skabelon for medium-visning';
+$LANG['label_templateid_full'] = 'Skabelon for original-visning';
+
+$LANG['name'] = 'Konfiguration af Billedgalleri';
+?>
