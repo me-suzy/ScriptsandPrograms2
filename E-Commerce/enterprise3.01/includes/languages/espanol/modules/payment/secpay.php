171ad3bc2bdd4b658cf00ef67752c6e49aaaecc1
me-suzy <me.suzana@gmail.com>
2025-08-12 11:58:16 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/modules/payment/secpay.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/modules/payment/secpay.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_SECPAY_TEXT_TITLE', 'SECPay');
+  define('MODULE_PAYMENT_SECPAY_TEXT_DESCRIPTION', 'Tarjeta de Cr&eacute;dito para Pruebas:<br><br>Numero: 4444333322221111<br>Caducidad Cualquiera');
+  define('MODULE_PAYMENT_SECPAY_TEXT_ERROR', 'Error de Tarjeta de Cr&eacute;dito!');
+  define('MODULE_PAYMENT_SECPAY_TEXT_ERROR_MESSAGE', 'Ha ocurrido un error procesando su tarjeta de cr&eacute;dito. Por favor, intentelo de nuevo.');
+?>
