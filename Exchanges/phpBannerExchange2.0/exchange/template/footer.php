ed5f1ca242e9226fc21c69c6cd3562a96284385e
me-suzy <me.suzana@gmail.com>
2025-08-12 14:00:48 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/template/footer.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/template/footer.php
@@ -0,0 +1,7 @@
+<center>
+<p>&nbsp;
+<p>&nbsp;
+<p>&nbsp;
+<p>&nbsp;
+<a href="http://www.eschew.net/scripts/">phpBannerExchange 2.0</a><br>Administered by <a href="mailto:{ownermail}">{adminname}</a>
+</a>
