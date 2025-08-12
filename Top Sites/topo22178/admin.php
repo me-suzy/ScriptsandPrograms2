915ec67049182e683191fa9c536075c00f55f0e8
me-suzy <me.suzana@gmail.com>
2025-08-13 00:01:58 +0300

    Add Top Sites/topo22178/admin.php

---
+++ Top Sites/topo22178/admin.php
@@ -0,0 +1,13 @@
+<?php
+//
+//  admin.php
+//  rev001
+//  PHP v4.2+
+//
+
+//----------------------------------------------------------------
+// REDIRECCION A modes/admin/html.php
+//----------------------------------------------------------------
+echo "<script>document.location.href='index.php?m=admin&s=html';</script>";
+
+?>
