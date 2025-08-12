3e195934d0d570b013c0a6cfece61b3b5550abd8
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:55 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/poll/lang_poll_da.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/poll/lang_poll_da.php
@@ -0,0 +1,51 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'Vedligehold afstemninger';
+$GLOBALS["tSubmitFormTitle"] = 'Send afstemning';
+
+//  List Headings
+$GLOBALS["tQuestion"] = 'Spørgsmål';
+$GLOBALS["tOption"] = 'Svarmulighed';
+$GLOBALS["tPostedBy"] = 'Indsendt af';
+$GLOBALS["tPollType"] = 'Afstemningstype';
+$GLOBALS["tPublishDate"] = 'Startdato';
+$GLOBALS["tExpiryDate"] = 'Slutdato';
+$GLOBALS["tStatus"] = 'Status';
+
+//  List Functions
+$GLOBALS["tAddNewPoll"] = 'Tilføj en afstemning';
+$GLOBALS["tEditPoll"] = 'Ret er afstemning';
+$GLOBALS["tDeletePoll"] = 'Slet en afstemning';
+$GLOBALS["tReleasePoll"] = 'Frigiv en afstemning';
+$GLOBALS["tEditOptions"] = 'Ret afstemnings-muligheder';
+
+$GLOBALS["tAddNewPollOption"] = 'Tilføj en afstemnings-mulighed';
+$GLOBALS["tEditPollOption"] = 'Ret en afstemnings-mulighed';
+$GLOBALS["tDeletePollOption"] = 'Slet en afstemnings-mulighed';
+
+//  Form Field Headings
+$GLOBALS["tCategory"] = 'Karegori';
+
+//  Poll View Texts
+$GLOBALS["tMultiVotes"] = 'Afgiv flere stemmer';
+$GLOBALS["tSingleVote"] = 'Afgiv kun en stemme';
+
+$GLOBALS["tSubmitPoll"] = 'Indsend en afstemning';
+$GLOBALS["tContact"] = 'Kontakt';
+$GLOBALS["tEMail"] = 'E-Mail';
+$GLOBALS["tWebPage"] = 'Web Side';
+$GLOBALS["tVoteButton"] = 'Stemme';
+
+//  Poll Submission Texts
+$GLOBALS["tThankYou"] = 'Tak fordi du indsendte en afstemning.';
+$GLOBALS["tReleaseRequired"] = 'Bemærk at afstemningen først bliver aktiv når redaktøren har godkendt/frigivet denne.';
+$GLOBALS["tClickToReturn"] = 'Klik her for at returnere til';
+$GLOBALS["tPollPage"] = 'afstemning Siden';
+
+//  Security Texts
+$GLOBALS["tMustLogin"] = 'Du skal være';
+$GLOBALS["tLogin"] = 'logget ind';
+$GLOBALS["tToSubmit"] = 'for at indesende en afstemning';
+
+?>
