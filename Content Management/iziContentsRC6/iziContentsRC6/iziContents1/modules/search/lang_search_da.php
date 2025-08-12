bfba2843c669f1492691a8b97f151f597d333828
me-suzy <me.suzana@gmail.com>
2025-08-12 04:01:29 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_da.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_da.php
@@ -0,0 +1,16 @@
+<?php
+
+$GLOBALS["module_title"] = 'Søg denne hjemmeside';
+$GLOBALS["search_keywords"] = 'Søgeord der skal søges efter';
+$GLOBALS["search_OR"] = 'Alle kombinationer af søgeord';
+$GLOBALS["search_AND"] = 'Alle søgeord';
+$GLOBALS["search_titles"] = 'Søge titel';
+$GLOBALS["search_teasers"] = 'Smagsprøve';
+$GLOBALS["search_contents"] = 'Indhold';
+$GLOBALS["search_button"] = 'Søg';
+
+$GLOBALS["eEnterKeywords"] = 'Indtast søgeord.';
+$GLOBALS["eEnterElements"] = 'Indtast et sideelement som du vil søge.';
+$GLOBALS["eNoResults"] = 'Intet fundet.';
+
+?>
