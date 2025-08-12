f470265d650962c39f118985be2fc8084d2e4eae
me-suzy <me.suzana@gmail.com>
2025-08-12 16:40:29 +0300

    Add Graphs and Charts/advgraphtrial/tutorials/stackedhorizontalbargraph/config.php

---
+++ Graphs and Charts/advgraphtrial/tutorials/stackedhorizontalbargraph/config.php
@@ -0,0 +1,67 @@
+<?php
+
+/* ------------------------------------------------------------------ */
+/*                                                                    */
+/* This config script is a very simply little script which            */
+/* outputs the configuration data for the graph.                      */
+/*                                                                    */
+/* ------------------------------------------------------------------ */
+
+
+print "width: 300\n";
+print "height: 300\n";
+
+print "outline: true\n";
+print "gradientfill: true\n";
+print "displaybarvalues: false\n";
+
+print "backgroundcolor: light grey\n";
+print "grid: true\n";
+print "axis: true\n";
+print "nrows: 5\n";
+
+print "3d: true\n";
+print "depth3d: 10\n";
+
+print "ndecplaces: 0\n";
+
+print "gridbgcolor: #000000\n";
+print "axiscolor: white\n";
+print "floorcolor: dark grey\n";
+print "gridcolor: white\n";
+print "gridstyle: dotted\n";
+
+print "series1: 0,128,255|Profit|bottom\n";
+print "series2: 247,147,4|Sales|bottom\n";
+
+print "xlables: true\n";
+print "xfont: medium\n";
+print "xlabelcolor: #202050\n";
+print "xlabelpre: $\n";
+print "xlabelpost: \n";
+
+print "titletext: Sales vs Profit by Month\n";
+print "titlefont: medium bold\n";
+print "titlecolor: #202050\n";
+
+print "ytitletext: Month\n";
+print "ytitlefont: medium bold\n";
+print "ytitlecolor: #202050\n";
+
+print "xtitletext: Value\n";
+print "xtitlefont: medium bold\n";
+print "xtitlecolor: #202050\n";
+
+print "ylabels: January|February|March|April|May|June|\n";
+print "yfont: medium\n";
+print "ylabelcolor: #202050\n";
+
+print "legend: true\n";
+print "legendstyle: horizontal\n";
+print "legendbgcolor: #DDDDDD\n";
+print "legendbordercolor: #9999FF\n";
+print "legendtextcolor: #202050\n";
+print "legendfont: medium bold\n";
+
+
+?>
