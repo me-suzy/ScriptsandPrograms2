645837d2389b2c487d4d1357c81eec8e270e45f3
me-suzy <me.suzana@gmail.com>
2025-08-12 10:47:49 +0300

    Add Discussion Boards/phpp/forum/languages/english.php

---
+++ Discussion Boards/phpp/forum/languages/english.php
@@ -0,0 +1,375 @@
+<?
+// Menu links
+$txt_mview = "view forums";
+$txt_msearch = "search";
+$txt_mmembers = "members";
+$txt_mprofile = "edit profile";
+$txt_mpms = "pms";
+$txt_mregister = "register";
+$txt_mlogin = "log in";
+
+
+// Titles
+$txt_add = "Add new topic";
+$txt_admincentre = "Admin centre";
+$txt_checkstatus = "Check status";
+$txt_edit = "Edit";
+$txt_editmsg = "Edit message";
+$txt_editprofile = "Edit profile";
+$txt_editrights = "Edit rights";
+$txt_forumadmin = "Forum administration";
+$txt_reply = "Reply to message";
+$txt_inbox = "Inbox";
+$txt_ip = "IP address";
+$txt_ipstatus = "IP status";
+$txt_search = "Search";
+$txt_siteadmin = "Site administration";
+$txt_statuschanged = "Status changed";
+$txt_useradmin = "User administration";
+
+// General text
+$txt_accept = "Accept";
+$txt_and = "and";
+$txt_announcement = "Announcement";
+$txt_avatar = "Avatar";
+$txt_avatardelete = "Delete avatar";
+$txt_avatars = "Avatars";
+$txt_avatarrules = "Max size 80x80; max filesize 10KB";
+$txt_avatarsshow = "Show avatars?";
+$txt_avatarupload = "Upload new avatar";
+$txt_at = "at";
+$txt_ban = "Ban";
+$txt_banned = "BANNED";
+$txt_by = "by";
+$txt_changessaved = "Changes have been saved.";
+$txt_closewindow = "Close window";
+$txt_del = "Delete";
+$txt_forumlocked = "(forum locked)";
+$txt_forumreturn = "Return to forum";
+$txt_found = "Found";
+$txt_fromuc = "From";
+$txt_from = "from";
+$txt_hot = "(hot topic)";
+$txt_in = "in";
+$txt_jumpto = "Jump to:";
+$txt_key = "Key";
+$txt_language = "Language";
+$txt_lastpost = "Last post";
+$txt_lastvisit = "Your last visit was at";
+$txt_lockedt = "(locked)";
+$txt_loggedas = "Logged in as";
+$txt_login = "Log in";
+$txt_logout = "Log out";
+$txt_message = "Message";
+$txt_moveup = "Move up";
+$txt_movedown = "Move down";
+$txt_newposts = "New posts";
+$txt_next = "Next";
+$txt_no = "No";
+$txt_none = "None";
+$txt_nonewposts = "No new posts";
+$txt_normal = "Normal";
+$txt_noaccess = "No access";
+$txt_notifyreps = "Notify me of replies";
+$txt_notopics = "No topics";
+$txt_or = "or";
+$txt_page = "Page";
+$txt_password = "Password";
+$txt_post = "post";
+$txt_posts = "posts";
+$txt_posttype = "Post type";
+$txt_prev = "Prev";
+$txt_reject = "Reject";
+$txt_quote = "Quote";
+$txt_quoting = "Quoting";
+$txt_register = "Register";
+$txt_registerlc = "register";
+$txt_registertm = "Registered";
+$txt_rememberme = "Remember me";
+$txt_result = "result";
+$txt_results = "results";
+$txt_said = "said";
+$txt_savechanges = "Save changes";
+$txt_sticky = "Sticky";
+$txt_subject = "Subject";
+$txt_time = "Time";
+$txt_to = "To";
+$txt_topicuc = "Topic";
+$txt_topic = "topic";
+$txt_topics = "Topics";
+$txt_topicslc = "topics";
+$txt_unban = "Unban";
+$txt_userguest = "User is a guest so does not have a profile.";
+$txt_username = "Username";
+$txt_userposted = "User posted image";
+$txt_userson = "Users online";
+$txt_warning = "Warning";
+$txt_with = "with";
+$txt_yes = "Yes";
+
+// Administration
+$txt_adminonly = "Sorry, only the site's administrator can access this page.";
+$txt_adminwelcome = "Welcome to the admin centre. Please choose an option from the list below.";
+
+// Editing
+$txt_editedby = "Last edit by";
+$txt_edited = "Edited by $logincookie[user]";
+$txt_postadd = "added";
+$txt_postedit = "edited";
+$txt_posthasbeen = "Post has been";
+$txt_preview = "Preview";
+
+// Error messages
+$txt_banneduser = "You have tried to post as a guest with a username which is banned from this board.";
+$txt_cantdisplay = "Sorry, there is a problem with displaying that post. Please try again.";
+$txt_catexists = "Sorry, a category with that name already exists.";
+$txt_choosefile = "Please choose a file";
+$txt_errnotif = "Error sending notification e-mail(s)";
+$txt_error = "Error";
+$txt_filenotrec = "File type not recognised - must be .gif or .jpg";
+$txt_filenotup = "File could not be uploaded";
+$txt_forumexists = "Sorry, a forum with that name already exists in that category.";
+$txt_forumnoexist = "Sorry, the forum you have selected does not exist.";
+$txt_forumnorights = "Sorry, you do not have the rights to access this forum. If you believe this is incorrect, please contact the forum administrator.";
+$txt_forumnosorry = "Sorry, the forum you have selected does not exist.";
+$txt_forumnotexist = "Forum does not exist";
+$txt_installonserver = "The <i>install.php</i> file is still on the server. It must be removed before you can access the forum.<br/>If the installer has not yet been run, click <a href=\"install.php\">here</a> to load it.";
+$txt_ipban = "IP banned";
+$txt_ipbanmsg = "The IP address you are accessing the site from has been banned from this board.";
+$txt_islocked = "is locked.";
+$txt_it = "it";
+$txt_norights = "You do not have the rights to edit this message.";
+$txt_norightsforum = "Sorry, you do not have the rights to access this forum. If you believe this is incorrect, please contact the forum administrator.";
+$txt_sorry = "Sorry, you cannot post to this topic because";
+$txt_subjmsg = "You must enter a subject and a message.";
+$txt_templatenotexist = "Template <i>$template</i> does not exist. Board cannot load. Please contact the <a href=\"mailto:$siteemail\">site administrator</a>.";
+$txt_theforum = "the forum";
+$txt_userbanmsg = "The username you are signed in with has been banned from this board.";
+$txt_userbanned = "User banned";
+$txt_userregd = "You have tried to post as a guest with a username which is registered.<br/>If this is your username, please <a href=\"login.php\">log in</a>.<br/>Otherwise, please go <a href=\"javascript:history.back(-1)\">back</a> and try a different username.";
+
+// Forum administration
+$txt_addcat = "Add category";
+$txt_addforum = "Add forum";
+$txt_cat = "Category";
+$txt_catdel = "Category has been deleted.";
+$txt_catcreate = "Create category";
+$txt_catcreated = "Category has been created.";
+$txt_editcat = "Edit category";
+$txt_editforum = "Edit forum";
+$txt_catedited = "Category has been edited.";
+$txt_catmoved = "Category moved";
+$txt_catname = "Category name";
+$txt_catremwarn = "Removing a category will also remove all forums and posts linked to it. This is not a recoverable operation.<br/>Are you sure this is what you want to do?";
+$txt_createforum = "Create forum";
+$txt_forumcreated = "Forum has been created.";
+$txt_forumedited = "Forum has been edited.";
+$txt_forumdel = "Forum has been deleted.";
+$txt_forumdesc = "Forum description";
+$txt_forummoved = "Forum moved";
+$txt_forumname = "Forum name";
+$txt_forumremwarn = "Removing a forum will also remove all posts linked to it. This is not a recoverable operation.<br/>Are you sure this is what you want to do?";
+$txt_restaccessq = "Restricted access?";
+
+// Index
+$txt_birthday = "Happy birthday to:";
+$txt_guestuc = "Guest";
+$txt_guest = "guest";
+$txt_guests = "guests";
+$txt_lastreg = "Last registered user:";
+$txt_markallread = "Mark all posts as read";
+$txt_member = "member";
+$txt_members = "members";
+$txt_newpm = "new private message";
+$txt_newpms = "new private messages";
+$txt_registered = "registered";
+$txt_regusers = "Registered users:";
+$txt_reporteddeal = "There are reported posts which have not yet been dealt with.";
+$txt_reportedview = "View reported posts";
+$txt_thereare = "There are";
+$txt_thereis = "There is";
+$txt_usersonline = "users online";
+$txt_useronline = "user online";
+$txt_wehave = "We have a total of";
+$txt_welcomeback = "Welcome back to";
+$txt_youhave = "You have";
+
+// Links
+$txt_goback = "Go <a href=\"javascript:history.back(-1)\">back</a> and try again.";
+$txt_viewindex = "View forum index";
+$txt_viewtopic = "View topic";
+
+// Logging in and out
+$txt_cont = "Click <a href=\"index.php\">here</a> to continue.";
+$txt_loggedin = "Logged in";
+$txt_loginagain = "Click <a href=\"login.php\">here</a> to log in again.";
+$txt_loginwrong = "Sorry, that username or password is not recognised.";
+$txt_loggedout = "Logged out";
+$txt_logoutmsg = "You have been logged out.";
+$txt_pleaselogin = "Please log in to add and edit messages.";
+$txt_thanks = "Thank you for logging in,";
+
+// Member list
+$txt_asc = "Ascending";
+$txt_desc = "Descending";
+$txt_member = "member";
+$txt_memberlist = "Member list";
+$txt_members = "members";
+$txt_notspec = "Not specified";
+$txt_private = "Private";
+$txt_sort = "Sort";
+$txt_sortby = "Sort by";
+$txt_totposts = "Total posts";
+
+// Moderation
+$txt_adminlockforums = "Sorry, you have to be an administrator to lock forums.";
+$txt_adminunlockforums = "Sorry, you have to be an administrator to unlock forums.";
+$txt_allfromip = "All posts from IP";
+$txt_allfromuser = "All posts from user";
+$txt_bancontrol = "Ban control";
+$txt_bannedalready = "has already been banned.";
+$txt_banip = "Ban IP";
+$txt_banuser = "Ban user";
+$txt_cantbanadmin = "User cannot been banned as he/she is a site administrator.<p/>If you are the overall administrator and wish to ban this user, you must log in to the admin control panel.";
+$txt_delnorights = "Sorry, you don't have the rights to delete this.";
+$txt_deltopic = "Delete topic";
+$txt_forumlocked = "Forum has been locked.";
+$txt_forumunlocked = "Forum has been unlocked.";
+$txt_fromip = "Posts from IP address";
+$txt_iplookup = "IP Lookup";
+$txt_ipused = "IP addresses used by user";
+$txt_lockforum = "Lock forum";
+$txt_locktopic = "Lock topic";
+$txt_movenorights = "You don't have the rights to move this post to this forum.";
+$txt_movetopic = "Move topic";
+$txt_mustbeadmin = "You must be an administrator to ban a user or an IP.";
+$txt_norightsaccess = "Sorry, you don't have the rights to access this page.";
+$txt_norightslock = "You don't have the rights to lock this topic.";
+$txt_norightsunlock = "You don't have the rights to unlock this topic.";
+$txt_selectforum = "Please select the forum you want to move this topic to from the box below.";
+$txt_postedby = "posted by";
+$txt_postmoved = "Post has been moved.";
+$txt_postnotexist = "Sorry, that post doesn't exist in the original forum.";
+$txt_topicdel = "Topic has been deleted.";
+$txt_topiclocked = "Topic has been locked.";
+$txt_topicunlocked = "Topic has been unlocked.";
+$txt_topicreturn = "Return to topic";
+$txt_unlockforum = "Unlock forum";
+$txt_unlocktopic = "Unlock topic";
+$txt_viewpost = "View post";
+
+// Private messages
+$txt_errsending = "Sorry, your message could not be sent.";
+$txt_fromemail = "From e-mail address";
+$txt_pmdel = "Message has been deleted.";
+$txt_pmnoexist = "does not exist!";
+$txt_pmnone = "No private messages";
+$txt_pmsend = "Send message";
+$txt_pmsent = "Message has been sent.";
+$txt_sendpm = "Send private message";
+
+// Registration
+$txt_confpass = "Confirm password";
+$txt_enteruser = "You must enter a username.";
+$txt_passlen = "Password must be between 6 and 15 characters.";
+$txt_noamp = "Your username cannot contain the &amp; character.";
+$txt_nospaces = "Password cannot contain spaces.";
+$txt_oncein = "Once you are logged in, please click &quot;profile&quot; on the menu to add more settings to your profile.";
+$txt_passnomatch = "Passwords do not match.";
+$txt_reqfield = "Required field";
+$txt_resuser = "Sorry, you have chosen a reserved username.";
+$txt_useradded = "User has been added.";
+$txt_userexists = "A user with that username already exists.";
+$txt_validemail = "You must enter a valid e-mail address.";
+
+// Reported posts
+$txt_alreadygone = "Sorry, the post this referred to has already been deleted.";
+$txt_delreport = "Delete report";
+$txt_mustbemod = "Sorry, you must be a moderator to access this function.";
+$txt_norepposts = "There are currently no reported posts which have not been dealt with.";
+$txt_origmsg = "Original message";
+$txt_repdesc = "If you wish, you may enter a message explaining why you wish to report this post. When you are ready, click &quot;Report&quot;.";
+$txt_repmsgsent = "Message sent by reporter";
+$txt_repnorights = "Sorry, the only posts that have been reported are for forums where you aren't a moderator.";
+$txt_reportby = "Reported by";
+$txt_reportpost = "Report post";
+$txt_reportedposts = "Reported posts";
+$txt_repremoved = "Report has been removed from database.";
+$txt_repsubmitted = "Your report has been submitted for the site's administrators and moderators to examine.";
+
+// Searching
+$txt_sall = "all users";
+$txt_sallforums = "all forums";
+$txt_sallterms = "all terms";
+$txt_schoose = "Choose the terms you wish to search for below.";
+$txt_sexact = "as exact phrase";
+$txt_sexactq = "Exact phrase?";
+$txt_sfor = "Searching for:";
+$txt_sgotopic = "Go to topic";
+$txt_smsg = "messages";
+$txt_sposted = "Posted at";
+$txt_sresults = "results found.";
+$txt_ssubmsg = "subjects and messages";
+$txt_stext = "Text";
+$txt_stoshort = "Sorry, your search term must be at least three characters long.";
+
+// Site administration
+$txt_adminemail = "Admin e-mail address";
+$txt_dateformat = "Date format";
+$txt_forumurl = "Forum URL";
+$txt_gdver = "GD version";
+$txt_guestspost = "Allow guests to post?";
+$txt_menutemplate = "Menu style";
+$txt_postshot = "Posts for hot topic";
+$txt_postspage = "Posts per page";
+$txt_sitename = "Site name";
+$txt_splitter = "Splitter character";
+$txt_template = "Template";
+$txt_timedef = "Default time zone";
+$txt_timeformat = "Time format";
+$txt_topicspage = "Topics per page";
+
+// User administration
+$txt_accessrights = "Access rights (restricted forums)";
+$txt_address = "Address";
+$txt_allowedacc = "is currently allowed to access this board.";
+$txt_aol = "AOL ID";
+$txt_banned = "has been banned";
+$txt_bannedacc = "is currently banned from accessing this board.";
+$txt_currenttime = "Current server time:";
+$txt_dob = "Date of birth";
+$txt_dispemail = "Display e-mail address?";
+$txt_email = "E-mail address";
+$txt_enterip = "Alternatively, use the box below to enter an IP address.";
+$txt_enterusername = "Please enter either the whole or part of the username you wish to access.";
+$txt_female = "Female";
+$txt_hasbeenbanned = "has been banned from this site.";
+$txt_homepage = "Home page";
+$txt_icq = "ICQ ID";
+$txt_imgsig = "Display images in signatures?";
+$txt_location = "Location";
+$txt_male = "Male";
+$txt_msn = "MSN ID";
+$txt_moderator = "is a moderator in the following forums:";
+$txt_modrights = "Moderator rights";
+$txt_overalladmin = "is overall admin for this site.";
+$txt_pmnotify = "E-mail notify for new PM?";
+$txt_postedfromip = "The following users have posted from this IP address:";
+$txt_profile = "Profile";
+$txt_realname = "Real name";
+$txt_restaccess = "has been given access to the following restricted forums:";
+$txt_sex = "Sex";
+$txt_signature = "Signature";
+$txt_issiteadmin = "is a site administrator.";
+$txt_siteadminq = "Site administrator?";
+$txt_timezone = "Time offset";
+$txt_unbanned = "has been unbanned";
+$txt_yahoo = "Yahoo! ID";
+
+// View page text
+$txt_announcement = "Announcement";
+$txt_go = "Go";
+$txt_replies = "Replies";
+$txt_topicstarter = "Topic starter";
+$txt_sticky = "Sticky topic";
+?>
