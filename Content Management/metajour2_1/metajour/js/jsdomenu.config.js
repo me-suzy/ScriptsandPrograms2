b2da2b1044a6cdbdb07e080e081fd722a21922ec
me-suzy <me.suzana@gmail.com>
2025-08-12 06:16:55 +0300

    Add Content Management/metajour2_1/metajour/js/jsdomenu.config.js

---
+++ Content Management/metajour2_1/metajour/js/jsdomenu.config.js
@@ -0,0 +1,34 @@
+/*
+Default values of public properties:
+allExceptFilter = new Array("A.*", 
+                            "BUTTON.*", 
+                            "IMG.*", 
+                            "INPUT.*", 
+                            "OBJECT.*", 
+                            "OPTION.*", 
+                            "SELECT.*", 
+                            "TEXTAREA.*");
+noneExceptFilter = new Array();
+menuClass = "jsdomenudiv";
+itemClass = "jsdomenuitem";
+itemClassOver = "jsdomenuitemover";
+sepClass = "jsdomenusep";
+imgSrc = "js/arrow.png";
+imgSrcOver = "js/arrow_o.png";
+imgOffset = 5;
+menuBorderWidth = 2;
+menuDelay = 0;
+*/
+
+// Uncomment any of the following public properties to override the default value.
+//var allExceptFilter = 
+//var noneExceptFilter = 
+//var menuClass = 
+//var itemClass = 
+//var itemClassOver = 
+//var sepClass = 
+//var imgSrc = 
+//var imgSrcOver = 
+//var imgOffset = 
+//var menuBorderWidth = 
+//var menuDelay = 
