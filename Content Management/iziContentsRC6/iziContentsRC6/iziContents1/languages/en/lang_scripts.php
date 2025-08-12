3e3cdecac091896cec6291e158e1c326b76f1345
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:42 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_scripts.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_scripts.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'maintain scripts';
+$GLOBALS["tFormTitle1"] = 'upload scripts/create script subdirectories';
+$GLOBALS["tFormTitle2"] = 'Upload script';
+$GLOBALS["tFormTitle3"] = 'Create Subdirectory';
+
+//  List Functions
+$GLOBALS["tAddNewScript"] = 'Add new script';
+$GLOBALS["tDeleteScript"] = 'Delete script';
+$GLOBALS["tDeleteScriptDir"] = 'Delete script subdirectory';
+
+//  List Headings
+$GLOBALS["tScriptName"] = 'Script name';
+
+//  Form Fields
+$GLOBALS["tScriptFilename"] = 'Script filename';
+$GLOBALS["tSubdirectoryname"] = 'Subdirectory name';
+
+//  Form Buttons
+$GLOBALS["bUpload"] = 'Upload!';
+$GLOBALS["bCreateDir"] = 'Create!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] = 'You need to select a file to upload.';
+$GLOBALS["eNoDir"] = 'You need to enter a directory name.';
+$GLOBALS["eDirAlreadyExists"] = 'subdirectory already exists';
+$GLOBALS["eZeroByteFile"] = 'File is 0 bytes in length';
+$GLOBALS["eNoFileUpload"] = 'File could not be uploaded successfully.';
+$GLOBALS["eInvalidFileSize"] = 'File is too large for upload';
+$GLOBALS["eInvalidFileType"] = 'File is not a valid script file';
+$GLOBALS["eInvalidMimeType"] = 'MIME type of File is not a valid script type';
+
+?>
