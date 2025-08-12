d86f385e38b0dbdb0164d1c2f7b6499ecd7279ef
me-suzy <me.suzana@gmail.com>
2025-08-13 00:12:54 +0300

    Add Unsorted/12-all.v3.8.2.0.1/send_app3.php

---
+++ Unsorted/12-all.v3.8.2.0.1/send_app3.php
@@ -0,0 +1,17 @@
+<body onLoad="window.resizeTo(400,375)" bgcolor="#FFFFFF" text="#000000">
+<?PHP require("lang_select.php"); ?>
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=<?PHP print $lang_char; ?>">
+
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<div align="center"> 
+  <p><font size="4" face="Arial, Helvetica, sans-serif"><b> <font color="#990000"><?PHP print $lang_326; ?><br>
+    </font><font size="2"><br>
+    </font></b></font><b><font face="Arial, Helvetica, sans-serif" size="2"><?PHP print $lang_327; ?></font></b></p>
+  </div>
+</body>
+</html>
