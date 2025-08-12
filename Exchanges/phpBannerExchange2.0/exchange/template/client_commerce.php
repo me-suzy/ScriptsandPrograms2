9bcbd7a31156a62f4dd8c8456aad282ba53a4552
me-suzy <me.suzana@gmail.com>
2025-08-12 14:00:13 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/template/client_commerce.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/template/client_commerce.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+  <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>{title}</title>
+<link rel="stylesheet" href="{baseurl}/template/css/{css}" type="text/css">
+</head>
+<body leftmargin="0" topmargin="0" marginwidth="0" 
+  marginheight="0" >
+<div id="content">
+<div class="main">
+<table border="0" cellpadding="1" width="650" cellspacing="0">
+<tr>
+<td>
+<table cellpadding="5" border="1" width="100%" cellspacing="0">
+<tr>
+<td colspan="2" class="tablehead"><center><div class="head">{title}</center></div></td>
+</tr>
+<td class="tablebody" colspan="2">
+<div class="mainbody">
+<table border="0" cellpadding="1" cellspacing="1" style="border-collapse: collapse"  width="90%">
+  <tr>
+<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" width="90%" >
+<tr>
+    <td align="center"></td>
+<table border="1" cellpadding="2" cellspacing="2" style="border-collapse: collapse" width="100%" >
+<tr>
+	<td class="tablehead"><b>{name}</b></td>
+	<td class="tablehead"><b>{credits}</b></td>
+	<td class="tablehead"><b>{price}</b></td>
+	<td class="tablehead"><b>{purchase}</b></td>
+{msg}
+</td>
+</tr>
+</table>
+<p>
+{couperror}
+<center>{couponcode}</center>
+<p>
+{histcode}
+{history}
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table><p>
+</div>
+</div>
+<div class="footer">
+{footer}
+</div>
+</div>
+{menu}
