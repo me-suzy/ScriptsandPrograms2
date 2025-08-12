534aa647ea674771eaaff30562db96fa9309f71d
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:26 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_menu.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_menu.da.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['menu_website'] = 'Website';
+$LANG['menu_file'] = 'Filstyring';
+$LANG['menu_access'] = 'Adgang';
+$LANG['menu_ebusiness'] = 'Ebusiness';
+$LANG['menu_intranet'] = 'CRM';
+$LANG['menu_advanced'] = 'Funktioner';
+$LANG['menu_extension'] = 'Komponenter';
+$LANG['menu_statistics'] = 'Besøgsstatistik';
+$LANG['menu_filestatistics'] = 'Filstatistik';
+$LANG['menu_changeguilanguage'] = 'Skift brugerfladesprog';
+$LANG['menu_createsite'] = 'Opret ny virksomhed / site';
+$LANG['menu_installsite'] = 'Installer prædefineret site';
+$LANG['menu_development'] = 'Avanceret';
+$LANG['menu_customform'] = 'Blanketter';
+$LANG['menu_customform_generator'] = 'Blanket generator';
+$LANG['menu_customform_form'] = 'Blanket';
+$LANG['menu_customform_list'] = 'Liste';
+$LANG['menu_customform_stats'] = 'Statistik';
+$LANG['menu_import'] = 'Import';
+$LANG['menu_package'] = 'Importer objektpakke';
+$LANG['menu_emptytrashcan'] = 'Tøm papirkurv';
+$LANG['menu_exit'] = 'Afslut';
+$LANG['dialog_terminate'] = 'Vil du afslutte programmet ?';
+?>
