1742198c838fbd85049893ee15050327b24e2f7c
me-suzy <me.suzana@gmail.com>
2025-08-12 11:42:45 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/live_support.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/live_support.php
@@ -0,0 +1,22 @@
+<?php
+/*
+
+	Live Support v 1.0
+	Author: Mike Lessar <mlessar@bluetruck.net>
+
+	for use with:
+	Enterprise Shopping Cart Software
+	http://www.enterprisecart.com
+
+	Released under the GNU General Public License
+
+*/
+
+define('HEADING_TITLE', 'Live Support');
+
+define('TABLE_HEADING_ATTRIBUTES', 'Atribute');
+define('TABLE_HEADING_STATUS', 'Choice');
+define('TABLE_HEADING_ACTION', 'Action');
+
+define('TEXT_NUMBER_OF_CUSTOMERS', 'Currently there are %s customers online');
+?>
