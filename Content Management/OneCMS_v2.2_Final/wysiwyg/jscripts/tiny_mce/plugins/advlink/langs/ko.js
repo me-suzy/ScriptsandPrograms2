a2e7276590b4a72fd4ba445be1c745991216f479
me-suzy <me.suzana@gmail.com>
2025-08-12 07:42:44 +0300

    Add Content Management/OneCMS_v2.2_Final/wysiwyg/jscripts/tiny_mce/plugins/advlink/langs/ko.js

---
+++ Content Management/OneCMS_v2.2_Final/wysiwyg/jscripts/tiny_mce/plugins/advlink/langs/ko.js
@@ -0,0 +1,19 @@
+// KO lang variables
+
+tinyMCELang['lang_insert_link_target_same'] = '°°Àº Ã¢¿¡¼­ ¸µÅ©/ÇÁ·¹ÀÓ ¿­±â';
+tinyMCELang['lang_insert_link_target_parent'] = 'ºÎ¸ð Ã¢¿¡¼­ ¸µÅ©/ÇÁ·¹ÀÓ ¿­±â';
+tinyMCELang['lang_insert_link_target_top'] = 'TOP ÇÁ·¹ÀÓ¿¡¼­ ¿­±â (¸ðµç ÇÁ·¹ÀÓÀ» ´ëÃ¼)';
+tinyMCELang['lang_insert_link_target_blank'] = '»õ Ã¢¿¡¼­ ¿­±â';
+tinyMCELang['lang_insert_link_target_named'] = '»õ Ã¢¿¡¼­ ¿­±â';
+tinyMCELang['lang_insert_link_popup'] = 'JS-Popup';
+tinyMCELang['lang_insert_link_popup_url'] = 'ÆË¾÷ URL';
+tinyMCELang['lang_insert_link_popup_name'] = 'Ã¢ ÀÌ¸§';
+tinyMCELang['lang_insert_link_popup_return'] = '\'return false\' ³Ö±â';
+tinyMCELang['lang_insert_link_popup_scrollbars'] = '½ºÅ©·Ñ¹Ù¸¦ º¸¿©ÁÜ';
+tinyMCELang['lang_insert_link_popup_statusbar'] = '»óÅÂÃ¢À» º¸¿©ÁÜ';
+tinyMCELang['lang_insert_link_popup_toolbar'] = 'Åø¹Ù¸¦ º¸¿©ÁÜ';
+tinyMCELang['lang_insert_link_popup_menubar'] = '¸Þ´º¹Ù¸¦ º¸¿©ÁÜ';
+tinyMCELang['lang_insert_link_popup_location'] = 'À§Ä¡ Ç¥½ÃÁÙÀ» º¸¿©ÁÜ';
+tinyMCELang['lang_insert_link_popup_resizable'] = 'Å©±â Á¶Àý °¡´ÉÇÑ À©µµ¿ì »ý¼º';
+tinyMCELang['lang_insert_link_popup_size'] = 'Å©±â';
+tinyMCELang['lang_insert_link_popup_position'] = 'À§Ä¡ (X/Y)';
