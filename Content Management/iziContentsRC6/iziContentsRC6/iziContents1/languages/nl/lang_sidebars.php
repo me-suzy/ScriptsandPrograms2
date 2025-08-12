33e3f763ea1d7ab47955dca74f77187f3c01e963
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:57 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_sidebars.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_sidebars.php
@@ -0,0 +1,39 @@
+<?
+
+// 18-9-2003 22:49
+
+// Form Title
+$GLOBALS["tFormTitle"]		= 'Beheer zijkolommen templates';
+
+// List Headings
+$GLOBALS["tName"]			= 'Naam';
+$GLOBALS["tBorder"]		= 'Randdikte';
+$GLOBALS["tAlignment"]		= 'Uitlijning';
+$GLOBALS["tColour"]		= 'Kleur';
+$GLOBALS["tWidth"]		= 'Breedte';
+
+// List Functions
+$GLOBALS["tAddNewSidebar"]	= 'Nieuwe zijkolom toevoegen';
+$GLOBALS["tEditSidebar"]	= 'Bewerken zijkolom';
+$GLOBALS["tDeleteSidebar"]	= 'Verwijderen zijkolom';
+
+// Form Field Headings
+$GLOBALS["tSBName"]		= 'Zijkolom naam';
+$GLOBALS["tSBAlign"]		= 'Zijkolom uitlijning';
+$GLOBALS["tSBBorder"]		= 'Randdikte kantlijn';
+$GLOBALS["tSBWidth"]		= 'Zijkolom breedte';
+$GLOBALS["tSBBackground"]	= 'Zijkolom achtergrondkleur';
+
+// Form Detail Comments and Help Texts
+$GLOBALS["tDetails"]		 = 'Op deze pagina is het mogelijk om templates voor zijkolommen te maken en aan te passen.';
+$GLOBALS["hSBName"]		 = 'De naam waarnaar de zijkolom zal refereren in de [sidebar] tag.';
+$GLOBALS["hSBAlign"]		 = 'Kant van de pagina dat de zijkolom zal verschijnen: '.$GLOBALS["tLeft"].' of '.$GLOBALS["tRight"].'.';
+$GLOBALS["hSBBorder"]		 = 'Dikte van de zijkolom kantlijn.';
+$GLOBALS["hSBWidth"]		 = 'De maximale breedte (in percentages) van de zijkolom.';
+$GLOBALS["hSBBackground"]	 = 'Achtergrond kleur van de zijkolom.';
+
+// Error Messages
+$GLOBALS["eNoName"]		= 'Zijkolom template moet een naam bevatten.';
+$GLOBALS["eColourWrong"]	= 'Waarde van de achtergrondkleur is verkeerd.';
+
+?>
