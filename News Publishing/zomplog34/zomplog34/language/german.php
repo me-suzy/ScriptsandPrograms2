c0aa9bdb74ca534b5f69712b3e4ac85081d617df
me-suzy <me.suzana@gmail.com>
2025-08-12 21:08:29 +0300

    Add News Publishing/zomplog34/zomplog34/language/german.php

---
+++ News Publishing/zomplog34/zomplog34/language/german.php
@@ -0,0 +1,181 @@
+<?
+
+// This is the German language file
+
+//----------------- German -----------------------//
+
+// Front end -> get.php
+$lang_number_comments = "Kommentar(e)"; //number of comments under posts
+$lang_no_results = "Keine Ergebnisse gefunden.";
+$lang_read_more = "mehr lesen...";
+$lang_listen = "MP3 anh&ouml;ren...";
+$lang_view = "Film anschauen...";
+$lang_searched_for = "Sie haben gesucht nach";
+$lang_posts_by = "Eintr&auml;ge von";
+
+// Front end -> sidemenu.php
+$lang_search = "Suche";
+$lang_pages = "Seiten";
+$lang_recent_posts = "Aktuelle Eintr&auml;ge";
+$lang_categories = "Kategorien";
+$lang_authors = "Autoren";
+$lang_rss_feed = "RSS Feed";
+$lang_admin = "Administrator";
+$lang_login = "Einloggen";
+$lang_register = "Registrieren";
+$lang_powered = "Powered by";
+$lang_archive = "Archiv";
+
+// Front end -> detail.php
+$lang_write_comment = "Einen Kommentar schreiben";
+$lang_name = "Name";
+$lang_comment = "Kommentar";
+$lang_comments = "Kommentare";
+$lang_no_comments_found = "Keine Kommentare gefunden";
+$lang_on = "am"; // as in "comment posted on date"
+$lang_error_name = "Sie haben Ihren Namen nicht angegeben.";
+$lang_error_comment = "Sie haben keinen Kommentar angegeben.";
+$lang_error_comment_success = "Sie haben erfolgreich einen Kommentar hinzugef&uuml;gt.";
+
+// Front end -> page.php
+
+$lang_contact = "Kontakt";
+$lang_your_email_sent = "Ihre e-Mail wurde versendet!";
+$lang_name_mail = "Name";
+$lang_email_mail = "eMail";
+$lang_additional_remarks = "Zus&auml;tzliche Anmerkungen";
+
+// Back end -> menu.php
+$lang_entries = "Eintr&auml;ge";
+$lang_newentry = "Neuer Eintrag";
+$lang_edit_delete = "Eintrag bearbeiten / l&ouml;schen";
+$lang_profile = "Pers&ouml;nliches Profil";
+$lang_editprofile = "Pers&ouml;nliches Profil bearbeiten";
+$lang_controlpanel = "Admin Kontrollpanel";
+$lang_settings = "Systemeinstellungen";
+$lang_moblog_settings = "moblog Einstellungen";
+$lang_content_management = "Content Management";
+$lang_manage_users = "Mitglieder verwalten";
+$lang_manage_categories = "Kategorien verwalten";
+$lang_manage_comments = "Kommentare verwalten";
+$lang_pages = "Seiten";
+$lang_newpage = "Neue Seite";
+$lang_edit_delete_page = "Seite bearbeiten / l&ouml;schen";
+$lang_upload_images = "Bilder hochladen";
+$lang_custom_upload_tool = "Benutzer Upload-Hilfsprogramm";
+
+// Back end -> members.php
+$lang_total_number_posts = "Komplettanzahl Einträge";
+$lang_total_number_pages = "Komplettanzahl Seiten";
+$lang_total_number_users = "Komplettanzahl Mitglieder";
+$lang_total_number_categories = "Komplettanzahl Kategorien";
+$lang_total_number_comments = "Komplettanzahl Kommentare";
+$lang_lastpost = "Ihr letzter Eintrag war";
+$lang_site_statistics = "Seiten Statistik";
+$lang_your_statistics = "Ihre Statistik";
+
+// Back end -> header.php
+
+$lang_members_area = "Dashboard";
+$lang_view_site = "Hauptseite";
+$lang_logout = "Ausloggen";
+
+// Back end -> entries (newentry.php, editor.php, entry.php)
+
+$lang_title = "Titel";
+$lang_text = "Text";
+$lang_extended = "Erweitert (optional)";
+$lang_upload_image = "Bild hochladen";
+$lang_full_width = "Bild(er) in Originalgr&ouml;&szlig;e darstellen";
+$lang_category = "Kategorie";
+$lang_choosemaincat = "Hauptkategorie ausw&auml;hlen";
+$lang_replace_image = "Bild ersetzen";
+$lang_media = "URL vom der Mediendatei";
+$lang_choose_type = "Medientyp auswählen";
+$lang_entries_by_others = "Einträge von Anderen";
+$lang_your_entries = "Ihre Einträge";
+$lang_order_by = "Ordnen nach";
+$lang_date = "Datum";
+$lang_number_of_images = "Anzahl der Bilder:";
+
+// Back end -> edit/delete
+$lang_edit = "bearbeiten";
+$lang_delete = "l&ouml;schen";
+
+// Back end -> User Management (profile.php, users.php)
+$lang_username = "Benutzername";
+$lang_password = "Passwort";
+$lang_retype_password = "Passwort wiederholen";
+$lang_email = "e-Mail";
+$lang_use_email_moblog = "Diese e-Mail Adresse f&uuml;r moblogging nutzen.";
+$lang_what_is_moblogging = "Was ist moblogging?";
+$lang_about_me = "&uuml;ber mich";
+$lang_administrator = "Administrator";
+$lang_user = "Mitglied";
+$lang_create_user = "Einen neuen Benutzer anlegen";
+$lang_current_users = "Aktuelle Mitglieder";
+
+// Back end -> Settings (settings.php)
+$lang_system_settings = "Systemeinstellungen";
+$lang_main_config = "Hauptkonfiguration";
+$lang_weblog_title = "weblog Titel";
+$lang_comments_on = "Kommentare ein";
+$lang_categories_on = "Kategorien ein";
+
+$lang_mediafile_on = "Mediendateien ein";
+$lang_activate_site_parts = "Aktive Seitenteile";
+$lang_admin_welcome_message = "Willkommens Nachricht Mitgliederbereich";
+$lang_site_welcome_message = "Willkommens Nachricht Website";
+$lang_messages = "Willkommens Nachricht";
+$lang_dashboard_rss_feed = "RSS Feed URL für das Dashboard";
+$lang_allow_register = "Jeder kann sich registrieren";
+
+$lang_choose_language = "Sprache ausw&auml;hlen";
+$lang_choose_skin = "Aussehen ausw&auml;hlen";
+$lang_posts_per_page = "Eintr&auml;ge pro Seite";
+$lang_scroll = "Seitenanzahl";
+$lang_scroll_explained = "(Die Anzahl der Seiten im Vorw&auml;rts / R&uuml;ckw&auml;rts Abschnitt)";
+$lang_upload_on = "Hochladen ein";
+$lang_max_upload = "Gr&ouml;&szlig;e Hochladen";
+$lang_pages_admin = "Wer kann Seiten erstellen / bearbeiten?";
+$lang_pages_admin_only = "nur der Administrator";
+$lang_date_time = "Datum / Uhrzeit";
+$lang_date_time_format = "Datum / Uhrzeit Format";
+
+$lang_thumbnail_width = "Bildweite";
+$lang_thumbnail_fullwidth = "Maximale Weite";
+
+// Back end -> category.php
+$lang_new_category = "Neue Kategorie anlegen";
+$lang_category_name = "Kategoriename";
+$lang_edit_category = "Kategorie bearbeiten";
+$lang_choose_category = "Kategorie ausw&auml;hlen";
+$lang_edit_category_name = "Kategoriename bearbeiten";
+$lang_delete_category = "Kategorie l&ouml;schen";
+
+// Back end -> comments.php, comments_editor.php
+$lang_edit_comment = "Kommentar bearbeiten";
+$lang_comment_name = "Name";
+$lang_comment = "Kommentar";
+$lang_ban = "Gebannt";
+
+// Back end -> newpage.php
+$lang_add_contact_form = "Kontaktformular hinzuf&uuml;gen?";
+$lang_form_email = "Kontakt e-Mail";
+
+//errors, well actually the messages if an action was succesful...
+$lang_error1 = "Der Eintrag wurde erfolgreich hinzugef&uuml;gt!";
+$lang_error2 = "Der Eintrag wurde erfolgreich aktualisiert!";
+$lang_error3 = "Die Daten wurden erfolgreich gel&ouml;scht.";
+$lang_error4 = "Das Benutzerprofil wurde erfolgreich aktualisiert!";
+$lang_error5 = "Die Einstellungen wurden erfolgreich aktualisiert!";
+$lang_error6 = "Die Kategorie wurde erfolgreich hinzugef&uuml;gt!";
+$lang_error7 = "Die Kategorie wurde erfolgreich ge&auml;ndert!";
+$lang_error8 = "Der Kommentar wurde erfolgreich ge&auml;ndert!";
+$lang_error9 = "Der Benutzer wurde erfolgreich angelegt!";
+$lang_error10 = "Die Seite wurde erfolgreich hinzugef&uuml;gt!";
+$lang_error11 = "Die Seite wurde erfolgreich aktualisiert!";
+$lang_error12 = "Die IP-Adresse wurde erfolgreich gebannt!";
+$lang_error13 = "Die IP-Adresse wurde erfolgreich freigeschaltet!";
+
+?>
