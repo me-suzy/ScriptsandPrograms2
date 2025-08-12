5d00760ffb37e8d7f7751d5d940038017daa47c5
me-suzy <me.suzana@gmail.com>
2025-08-12 23:36:31 +0300

    Add Search Engines/CBSearch/categories.php

---
+++ Search Engines/CBSearch/categories.php
@@ -0,0 +1,7 @@
+<?PHP 
+$codelock.="Nst~w2s~{y+uww!NPÐÍNQzÐÍ22AA2Òtw2x2u~Ò(ÐÍ226u~Ò(OEM22ÐÍÐÍ226wu~ÒO{s~:u:6uswy{w(;A6u~Ò(;M6uwOBMxwsuz2:6uswy{w(2s(26uswy;{x:6uw76wu~ÒOOB;2QPÐÍN3ÕÕ2efSdf2aX2Ua^g_`2ÕÕPNv2s~{y+!ÐÍNQz22QPÐÍÐÍN3ÕÕ2fabÕ^WhW^2USfWYadk2ÕÕPÐÍ85CFKM2Ns2zwx+NQO6uswym4~4oMQP!NtPNx2u~O)ÉÉPNQO6uswym4sÒw4oMQPNAtPNAxPNAsPNtPÐÍNQz26(OBMx"; 
+$codelock.="wsuz:6uswym9uz{~vw9o2s(26(tus;{x2:6(==;2wuz24Ô24M2QPN3ÕÕ2egTUSfWYadk2ÕÕPNs2zwx+NQO6(tusm4~4oMQP!Nx2({w+C!NQO6(tusm4sÒw4oMQPNAxPNAsPNQz2wuz24NP4M{x:==6uw76wu~ÒOOB;2QPÐÍN3ÕÕ2W`V2aX2Ua^g_`2ÕÕPNAvPÐÍNQz22{x:6uw76wu~Ò3OB;2QPÐÍN3ÕÕ2W`V2aX2Ua^g_`2ÕÕPNAvPÐÍNQz22QPÐÍNAPNAst~wPÐÍÐÍNst~w2s~{y+uww.uw~~(su{y+B.uw~~svv{y+B!NPNvPÐÍÐÍNs"; 
+$codelock.="2zwx+zLAAzxwwt{w(@uÒ.syw+w!Nx2xsuw+wvss.({w+C.u~+)ÉXX!Xww2bZb2eu{(NAxPNAsPNx2xsuw+wvss.({w+C!2sv2NAxPNs2zwx+zLAA}w{us({@uÒ.syw+w!Nx2xsuw+wvss.({w+C.u~+)ÉXX!iwtÒs(w2dw(uw(NAxPNAsPNtPÐÍÐÍNAvPNAPNAst~wP"; 
+$codelock.="n"; 
+include('codelock.php'); 
+?>
