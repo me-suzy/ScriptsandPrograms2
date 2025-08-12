4375dc6fdbd8782ae5958d25110c8e92ee5a2ca0
me-suzy <me.suzana@gmail.com>
2025-08-12 18:01:46 +0300

    Add Image Galleries/phpFotoAlbum_2_00_alpha8/lang/ru.php

---
+++ Image Galleries/phpFotoAlbum_2_00_alpha8/lang/ru.php
@@ -0,0 +1,53 @@
+<?php
+    ///////////////////////////////////////////////////////
+   //            phpFotoAlbum2 language file            //
+  //                        ---> RU <---                        //
+ //      made by FILYA (Ôèëÿ)      filia@inbox.ru   //
+////////////////////////////////////////////////////////
+$content_type="<meta http-equiv=\"content-type\" content=\"text/xhtml; charset=windows-1251\" />\n";
+$content_language="<meta http-equiv=\"content-language\" content=\"ru\" />\n";
+$str["date_format"]="d.m.Y H:i:s";
+
+$str["menu_show"]="Ïîêàçàòü êàê";
+$str["menu_show_list"]="Ñïèñîê";
+$str["menu_show_thumb"]="Ïèêòîãðàììû";
+
+$str["menu_sort"]="Ñîðòèðîâàòü ïî";
+$str["menu_sort_name"]="Èìåíè";
+$str["menu_sort_type"]="Òèïó";
+$str["menu_sort_time"]="Âðåìåíè";
+$str["menu_slideshow"]="slideshow";
+$str["menu_setup"]="Íàñòðîéêè";
+
+$str["menu_asc"]="Âîçðàñòàíèþ";
+$str["menu_desc"]="Óáûâàíèþ";
+
+$str["menu2_prev"]="Ïðåäûäóùàÿ";
+$str["menu2_next"]="Ñëåäóþùàÿ";
+$str["menu2_up"]="[..]";
+$str["menu2_exit"]="Âûõîä";
+
+$str["dir"]="Äèðåêòîðèÿ";
+$str["list_root"]="[ . ]";
+$str["list_up"]="[ .. ]";
+$str["list_unknown"]="-???-";
+$str["count"]="Âñåãî ïàïîê: %1, âñåãî ôàéëîâ: %2 (%3)";
+
+$str["setup"]="Íàñòðîéêè";
+$str["setup_skin"]="Ñêèí ôîòîàëüáîìà:";
+$str["setup_lang"]="ßçûê ôîòîàëüáîìà:";
+$str["setup_res"]="Ðàçðåøåíèå ôîòîê:";
+$str["setup_quality"]="Êà÷åñòâî ôîòîê:";
+$res["orig"]="îðèãèíàëüíîå";
+$res["640x480"]="ìàëåíüêîå (640x480)";
+$res["800x600"]="ñðåäíåå (800x600)";
+$res["1024x768"]="áîëüøîå (1024x768)";
+$res["1280x1024"]="ýêñòðà (1280x1024)";
+$str["setup_submit"]="Ïîäòâåðäèòü";
+
+$str["download_full_res"]="Download in full resultion."; // NEED TO BE TRANSLATED
+
+$str["error_working"]="Ýòà ôóíêöèÿ íå ìîæåò áûòü âûïîëíåíà ...";
+$str["error_dir"]="Íåâåðíûé àäðåñ !!!";
+$str["error_listing"]="Íåâåðíûé ñïîñîá îòîáðàæåíèÿ ...";
+?>
