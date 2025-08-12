d540ee07543de804a69c2c4cc055650441988b57
me-suzy <me.suzana@gmail.com>
2025-08-12 07:22:28 +0300

    Add Content Management/mysource_2.10.2/mysource_2.10.2/squizlib/wysiwyg/pop_ups/blank.php

---
+++ Content Management/mysource_2.10.2/mysource_2.10.2/squizlib/wysiwyg/pop_ups/blank.php
@@ -0,0 +1,33 @@
+<?  ##############################################
+   ### SQUIZLIB ------------------------------###
+  ##- Bodycopy Editor ---- PHP4 --------------##
+ #-- Copyright Squiz.net ---------------------#
+##############################################
+## This file is subject to version 1.0 of the
+## MySource License, that is bundled with
+## this package in the file LICENSE, and is
+## available at through the world-wide-web at
+## http://mysource.squiz.net/
+## If you did not receive a copy of the MySource
+## license and are unable to obtain it through
+## the world-wide-web, please contact us at
+## mysource@squiz.net so we can mail you a copy
+## immediately.
+##
+## $Source: /home/cvsroot/squizlib/wysiwyg/pop_ups/blank.php,v $
+## $Revision: 1.1 $
+## $Author: blair $
+## $Date: 2002/03/27 08:42:33 $
+#######################################################################
+?>
+<html>
+<head>
+<style type="text/css">
+	body { 
+		background-color: #880088;
+	}
+</style>
+<body>
+&nbsp;
+</body>
+</html>
