c57cb20ce53c74bc5f163d8ff9d44484666bf407
me-suzy <me.suzana@gmail.com>
2025-08-12 19:51:15 +0300

    Add News Publishing/NewMan147/NewMan147/newman/languages/lang-swedish.php

---
+++ News Publishing/NewMan147/NewMan147/newman/languages/lang-swedish.php
@@ -0,0 +1,119 @@
+<?php
+  //******************************************************************************************
+  //**                                                                                      **
+  //** phpNewsManager v1.30                                                                 **
+  //** contact: gregor@klevze.si                                                            **
+  //** Last edited: 10th.June,2002                                                          **
+  //**                                                                                      **
+  //** Language module: English                                                             **
+  //******************************************************************************************
+
+define("_CHARSET","ISO-8859-1");
+define("_STATISTICS","Statistik");
+define("_ACTUAL","Faktisk");
+define("_LASTVISIT","Senast besökt");
+define("_NEW","Ny");
+define("_ONLINENEWS","Nyheter Online");
+define("_WEEKLYPOLLQ","Vecko Frågeenkät");
+define("_WEEKLYPOLLA","Vecko Svarsenkät");
+define("_PUBLICNEWS","Publika Nyheter");
+define("_REGISTEREDUSERS","Registrerade användare");
+define("_NEWSCOMMENTS","Nyhetskommentarer");
+define("_ADDNEWS","Lägg till nyheter");
+define("_SUBMITEDNEWS","Inskickade nyheter");
+define("_OPTIONS","Valmöjligheter");
+define("_NEWSHEADLINE","Huvudnyheter");
+define("_AUTHOR","Författare");
+define("_DATE","Datum");
+define("_SECTION","Avdelning");
+define("_PREVIEW","Förhandsgranska");
+define("_MESSAGE","Meddelande");
+define("_SUBMIT","Skicka in");
+define("_HOME","Hem");
+define("_NEWSMANAGER","Nyhetsmeny");
+define("_MODIFYNEWS","Modifiera Nyhet");
+define("_ADDNEWS","Lägg till Nyhet");
+define("_ADDPARTNERS","Lägg till Partner");
+define("_UPLOADLOGO","Ladda upp Logo");
+define("_IMAGE","Bild");
+define("_NAME","Namn");
+define("_NUMBER","#");define("_","#");
+define("_CATEGORYNAME","Kategorinamn");
+define("_MAINMENU","Huvudmeny");
+define("_KEEPIT","Spara det!");
+define("_DELETEIT","Kasta det!");
+define("_EDITCATEGORY","Ändra Kategori");
+define("_UPLOADPICTURE","Ladda upp bild");
+define("_ADDCATEGORY","Lägg till kategori");
+define("_OURPARTNERS","Våra Partners");
+define("_SELECTPAGE","Välj sida");
+define("_LINK","Länk");
+define("_IN","In");
+define("_OUT","Ut");
+define("_AFFILIATES","Affiliates");
+define("_GFX","Gfx");
+define("_PREVIOUS","Föregående");
+define("_NEXT","Next");
+define("_DESCRIPTION","Beskrivning");
+define("_MODIFYPARTNERS","Ändra Partners");
+define("_RSSSETTINGS","RSS Inställningar");
+define("_NAMEOFRSSFILE","RSS filens namn");
+define("_NUMBEROFNEWS","Number of news");
+define("_RSSTITLE","RSS Titel");
+define("_RSSLINK","RSS Länk");
+define("_RSSDESCRIPTION","RSS Beskrivning");
+define("_AUTOCREATERSS","Skapa en RSS fil automatiskt efter varje nyhetsändring");
+define("_OPTIMIZED","Optimerad");
+define("_OPTIMIZEDATABASE","Optimera Databas");
+define("_ADDSMILEY","Lägg till Smiley");
+define("_UPLOADSMILEY","Ladda upp Smiley");
+define("_SMILEYEMOTION","Känsla");
+define("_SMILEYEDITOR","Smiley Editor");
+define("_SMILEYCODE","Kod");
+define("_EDITSMILEY","Editera Smiley");
+define("_DELETESMILEY","Ta bort Smiley");
+define("_NEWS","Nyheter");
+define("_CATEGORY","Kategori");
+define("_PARTNERS","Partners");
+define("_WEEKLYPOLL","Veckofråga");
+define("_SMILEYS","Smileys");
+define("_BROWSENEWS","Bläddra nyheter");
+define("_GROUPS","Grupper");
+define("_ADMINS","Administratörer");
+define("_USERS","Användare");
+define("_USERINFO","Användarinfo");
+define("_ADMINISTRATION","Administration");
+define("_YOULOGOUT","Du har precis loggat ut.");
+define("_LOGIN","Logga in");
+define("_PASSWORD","Lösenord");
+define("_USERNAME","Användarnamn");
+define("_WEBCONTROLPANEL","WEB CONTROL PANEL");
+define("_ADDUSER","Lägg till användare");
+define("_EMAIL","Email");
+define("_PRIVILEGES","Behörigheter");
+define("_EMAIL","Email");
+define("_EDITUSER","Editera användare");
+define("_DELETEUSER","Radera användare");
+define("_INFO","Info");
+define("_ADDGROUP","Lägg till grupp");
+define("_ADMIN","Administration");
+define("_RSS","RSS");
+define("_MODIFY","Modifiera");
+define("_ADD","Lägg till");
+define("_EDIT","Ändra");
+define("_DELETE","Radera");
+define("_UPLOAD","Ladda upp");
+define("_GROUPNAME","Gruppnamn");
+define("_SELECT","Välj");
+define("_ADDWEEKLYPOLL","Lägg till Veckofråga");
+define("_SUBMITEDPOLLS","Inskickade frågor");
+define("_QUESTION","Fråga");
+define("_ANSWERS","Svar");
+define("_SEPERATEANSWERSWITH","Separera svar med");
+define("_EDITWEEKLYPOLL","Ändra Veckofråga");
+define("_SUCCESS","Success");
+define("_DELETENEWS","Radera Nyheter");
+define("_NOTENOUGHPRIV","Du har inte tillräcklig behörighet!");
+define("_LANGUAGE","Språk");
+define("_CONTENT","Innehåll");
+?>
