b1ce216454c4834545ceef08ff7ca8bd9ab65dc1
me-suzy <me.suzana@gmail.com>
2025-08-12 06:02:50 +0300

    Add Content Management/metajour2_1/metajour/core/itemgroupclass.php

---
+++ Content Management/metajour2_1/metajour/core/itemgroupclass.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage core
+ */
+
+require_once('basicclass.php');
+
+class itemgroup extends basic {
+
+	function itemgroup() {
+		$this->basic();
+		$this->addcolumn('name',0,UI_STRING);
+	
+		$this->removeview('createfuture');
+		$this->removeview('approvepublish');
+		$this->removeview('requestapproval');
+	}
+
+}
