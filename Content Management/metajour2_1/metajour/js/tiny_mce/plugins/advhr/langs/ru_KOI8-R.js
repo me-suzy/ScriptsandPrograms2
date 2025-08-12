e23400aa4b7659dfc31c07f57a34ff85481b2fb8
me-suzy <me.suzana@gmail.com>
2025-08-12 06:24:23 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/advhr/langs/ru_KOI8-R.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/advhr/langs/ru_KOI8-R.js
@@ -0,0 +1,8 @@
+// RU KOI8-R lang variables
+
+tinyMCE.addToLang('',{
+insert_advhr_desc : '÷ÓÔÁ×ÉÔØ / ÒÅÄÁËÔÉÒÏ×ÁÔØ ÇÏÒÉÚÏÎÔÁÌØÎÙÊ ÒÁÚÄÅÌÉÔÅÌØ',
+insert_advhr_width : 'ûÉÒÉÎÁ',
+insert_advhr_size : '÷ÙÓÏÔÁ',
+insert_advhr_noshade : 'âÅÚ ÔÅÎÉ'
+});
