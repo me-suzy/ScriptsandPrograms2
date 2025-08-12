99e6e7acf9bdf7ce2a1570b71385a52d2669ee46
me-suzy <me.suzana@gmail.com>
2025-08-12 18:47:02 +0300

    Add Interactive Stories/FicHive_v1/skins/1/mainpage_news.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/mainpage_news.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='navigation'><%DATE%> (<%POSTER%>)</td></tr>
+<tr><td colspan='2' class='frame'><%NEWS%></td></tr>
+<tr><td colspan='2' class='fframe'><%COMMENTS%></td></tr>
+</table>
