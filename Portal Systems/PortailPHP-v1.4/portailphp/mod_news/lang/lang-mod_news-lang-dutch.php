9633cb3c784e0cd656178dd5fd26967e45e16a79
me-suzy <me.suzana@gmail.com>
2025-08-12 23:18:38 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_news/lang/lang-mod_news-lang-dutch.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_news/lang/lang-mod_news-lang-dutch.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE NEWS
+$Mod_News_PostPar="Gepost door";
+$Mod_News_Le="op";
+$Mod_News_PlRecent="10 meest recente artikels";
+$Mod_News_PlLu="10 meest gelezen artikels";
+$Mod_News_inde_Date="Datum";
+$Mod_News_inde_Sujets="Onderwerp";
+$Mod_News_inde_Auteurs="Auteur";
+$Mod_News_inde_Lectures="Treffers";
+$Mod_News_inde_Categorie="Rubriek";
+$Mod_News_Form_Categorie="Rubriek";
+$Mod_News_Form_Sujet="Onderwerp";
+$Mod_News_Form_Date="Datum";
+$Mod_News_Form_Corps="Inhoud (HTML)";
+$Mod_News_JS_Categorie="Het veld Rubriek moet ingevuld worden";
+$Mod_News_JS_Sujet="Het veld Onderwerp moet ingevuld worden";
+$Mod_News_JS_Contenu="Het veld Onderwerp moet ingevuld worden";
+
+?>
