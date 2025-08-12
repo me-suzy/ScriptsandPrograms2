b46db1d8a5744ade344cd93a735f747a49d6047a
me-suzy <me.suzana@gmail.com>
2025-08-12 21:33:31 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/loan.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/loan.lang.php
@@ -0,0 +1,16 @@
+<?php
+
+$tpl->assign("lang_head_loan", "Loan This Comic");
+$tpl->assign("lang_comic_info", "Comic Info");
+$tpl->assign("lang_loan_info", "Loan Info");
+
+$tpl->assign("lang_comic_name", "Comic title and issue number");
+$tpl->assign("lang_comic_story", "Story title");
+$tpl->assign("lang_lend_name", "Loan this comic to");
+$tpl->assign("lang_lend_notes", "Other notes");
+$tpl->assign("lang_lend_date", "Loan date");
+$tpl->assign("lang_lend_due", "Due return date");
+
+$tpl->assign("lang_button_loan", "Loan");
+
+?>
