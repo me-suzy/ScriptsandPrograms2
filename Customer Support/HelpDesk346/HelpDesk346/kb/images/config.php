cc7ba70bb915cca24840b2185f1244c8cab8d084
me-suzy <me.suzana@gmail.com>
2025-08-12 08:46:25 +0300

    Add Customer Support/HelpDesk346/HelpDesk346/kb/images/config.php

---
+++ Customer Support/HelpDesk346/HelpDesk346/kb/images/config.php
@@ -0,0 +1,15 @@
+<?php
+ 
+				$server =  'db66b.pair.com';
+
+				$database = 'appstest_11';
+
+				$databaseName = 'appstest_test';
+
+				$databasePassword = 'Just4today!';
+
+				$databasePrefix = 'jdjgreen_';
+
+				define('DB_PREFIX', 'jdjgreen_');
+
+?>
