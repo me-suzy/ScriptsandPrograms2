7ca2def91ea322fc929b976d93ed4494b0b9c8e9
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:33 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/lang_links_nl.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/lang_links_nl.php
@@ -0,0 +1,38 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'Onderhoud links';
+$GLOBALS["tSubmitFormTitle"] = 'Toevoegen link';
+
+//  List Headings
+$GLOBALS["tPostedBy"] = 'Geplaatst door';
+$GLOBALS["tPublishDate"] = 'Publiseer datum';
+$GLOBALS["tStatus"] = 'Status';
+
+// List Functions
+$GLOBALS["tAddNewLink"]	= 'Link toevoegen';
+$GLOBALS["tEditLink"]	= 'Bewerk link';
+$GLOBALS["tDeleteLink"]	= 'Verwijder link';
+$GLOBALS["tReleaseLink"]	= 'aan/uitzetten link';
+$GLOBALS["tEditExtraSettings"] = "Bewerk extra link instellingen";
+
+// Form Field Headings
+$GLOBALS["tCategory"] 		= 'Categorie';
+$GLOBALS["tWebURL"]			= 'Website URL';
+$GLOBALS["tDescription"]	= 'Omschrijving';
+$GLOBALS["tSubmitLink"]		= 'Voeg link toe';
+
+// Link Submission Texts
+$GLOBALS["tThankYou"]		= 'Bedankt voor het plaatsen van een link.';
+$GLOBALS["tReleaseRequired"]	= 'De link wordt geplaatst als de beheerder de link heeft vrijgegeven';
+$GLOBALS["tClickToReturn"]	= 'Klik hier om terug te gaan naar de';
+$GLOBALS["tLinksPage"]	= 'links pagina';
+
+// Security Texts
+$GLOBALS["tMustLogin"]		= 'Je moet';
+$GLOBALS["tLogin"]		= 'inloggen';
+$GLOBALS["tToSubmit"]		= 'om een link te kunnen plaatsen';
+
+//Validation
+$GLOBALS["eWebURLEmpty"]	= "Het link veld mag niet leeg zijn";
+?>
