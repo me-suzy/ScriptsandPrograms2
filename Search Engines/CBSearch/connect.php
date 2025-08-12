84053b4977e10e330828ce7a6edda172d1f8f51b
me-suzy <me.suzana@gmail.com>
2025-08-12 23:36:34 +0300

    Add Search Engines/CBSearch/connect.php

---
+++ Search Engines/CBSearch/connect.php
@@ -0,0 +1,5 @@
+<?PHP 
+$codelock.="NQzÐÍ22Ò(~quwu:4~us~z(4Ô4vtq(wqsÒw4Ô4(wqs((4;MÐÍ22Ò(~q(w~wuqvt:4vtqsÒw4;MÐÍ226sxx{v+ut(wsuzCDE4MÐÍ226s((+ut(wsuzCDE4MÐÍQP"; 
+$codelock.="n"; 
+include('codelock.php'); 
+?>
