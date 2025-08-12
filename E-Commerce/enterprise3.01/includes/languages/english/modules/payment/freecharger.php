84f16098d4725c01ae858d4280686d053a33c50b
me-suzy <me.suzana@gmail.com>
2025-08-12 11:54:30 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/modules/payment/freecharger.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/modules/payment/freecharger.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  WebMakers.com Added: Free Payments and Shipping
+  Written by Linda McGrath osCOMMERCE@WebMakers.com
+  http://www.thewebmakerscorner.com
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_FREECHARGER_TEXT_TITLE', 'The WebMakers Corner FREE CHARGE CARD');
+  define('MODULE_PAYMENT_FREECHARGER_TEXT_DESCRIPTION', 'Used for purchase of FREE CHARGE ONLY');
+  define('MODULE_PAYMENT_FREECHARGER_TEXT_EMAIL_FOOTER', 'Used with purchases via Free Charge');
+?>
