ff85b57f9107647f8f6ba9379452fddac8ec0351
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:55 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_sideoversigt_67.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_sideoversigt_67.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 67;
+function objfile_67 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Sideoversigt";
+$objdata['subname'] = "";
+$objdata['extension'] = "index";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
