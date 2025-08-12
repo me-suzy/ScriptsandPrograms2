86d3770791a75a7a418d8078705a3f56f0f9c6b2
me-suzy <me.suzana@gmail.com>
2025-08-12 14:16:33 +0300

    Add File Manipulation/dl4lite/UPLOAD/lang/1/lang_search.php

---
+++ File Manipulation/dl4lite/UPLOAD/lang/1/lang_search.php
@@ -0,0 +1,15 @@
+<?php
+/*********************************************************
+* Name: lang_search.php
+* Author: RW::Download 4.0
+* Contact: realworld@blazefans.com
+* Description: Language variables
+* Version: 4.00
+*********************************************************/
+$lang = array(
+
+'search_terms' => "Search Terms",
+'search_results' => "Search Results",
+)
+
+?>
