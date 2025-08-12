cd2414b376cc05ab7620b0cb5a926ea10b723660
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:55 +0300

    Add Content Management/metajour2_1/metajour/lang/extradata.da.php

---
+++ Content Management/metajour2_1/metajour/lang/extradata.da.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Navn';
+$LANG['name'] = 'Ekstra data';
+$LANG['text_1'] = 'Type';
+$LANG['text_2'] = 'Beskrivelse';
+$LANG['text_3'] = 'Sortering';
+$LANG['text_4'] = 'Feltnavn';
+$LANG['text_5'] = 'Slet';
+$LANG['text_6'] = 'Felt';
+$LANG['text_7'] = 'Tilføj felt';
+$LANG['text_8'] = 'Tabel';
+?>
