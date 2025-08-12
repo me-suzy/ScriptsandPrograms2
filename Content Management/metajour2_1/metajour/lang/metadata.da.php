aab0ecb699d775205aae9011ca0fbe3347b45a71
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:36 +0300

    Add Content Management/metajour2_1/metajour/lang/metadata.da.php

---
+++ Content Management/metajour2_1/metajour/lang/metadata.da.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Navn på metadata';
+$LANG['label_description'] = 'Beskrivelse';
+$LANG['label_keyword'] = 'Nøgleord';
+$LANG['label_copyright'] = 'Ophavsret';
+$LANG['label_publisher'] = 'Udgiver';
+$LANG['name'] = 'Metadata';
+
+?>
