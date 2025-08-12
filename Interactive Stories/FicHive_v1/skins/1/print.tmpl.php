033257e620e0bdf5da6640d723f4e0b0251fabf1
me-suzy <me.suzana@gmail.com>
2025-08-12 18:47:19 +0300

    Add Interactive Stories/FicHive_v1/skins/1/print.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/print.tmpl.php
@@ -0,0 +1,13 @@
+<html>
+<head><title><%TITLE%></title></head>
+<body>
+<center>
+<table border='0' width='600' cellspacing='0'><tr><td>
+<font size=2 face='verdana'>
+<b><%TITLE%></b><br>
+<%AUTHOR%>
+<hr><%CHAPTER%><hr>
+</td></tr></table>
+</center>
+</body>
+</html>
