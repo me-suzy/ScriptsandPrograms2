986dbd60a23fce160cc1c18ceef4b47546a5ccf2
me-suzy <me.suzana@gmail.com>
2025-08-12 12:28:48 +0300

    Add E-Commerce/ordersys/ordersys/ordering/interface_creator/footer_admin.php

---
+++ E-Commerce/ordersys/ordersys/ordering/interface_creator/footer_admin.php
@@ -0,0 +1,13 @@
+﻿<p>
+<?php
+if($parentsite_name != '')
+{echo ('<a href="'.$parentsite_url.'">'.$parentsite_name.'</a> | ');}
+
+echo ('<a href="'.$dadabik_main_file.'">'.$site_name.' - data browser</a> | <a href="admin.php">'.$site_name.' - administration home page</a> | <a href="help.htm" onclick="return popitup(\'help.htm\')">Administration help</a> | ');
+
+if($mainsite_name != '')
+{echo ('<a href="'.$mainsite_url.'">'.$mainsite_name.'</a>');}
+?>
+</p>
+</body>
+</html>
