f5a6bc2955222594576d1384c0447e769286f440
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:02 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_webshop_87.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_webshop_87.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 87;
+function objfile_87 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Webshop";
+$objdata['subname'] = "";
+$objdata['extension'] = "shop";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
