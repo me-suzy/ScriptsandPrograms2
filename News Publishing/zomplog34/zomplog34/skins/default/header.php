5b6c761459cbef731fd18879cf99383dc18b991b
me-suzy <me.suzana@gmail.com>
2025-08-12 21:09:44 +0300

    Add News Publishing/zomplog34/zomplog34/skins/default/header.php

---
+++ News Publishing/zomplog34/zomplog34/skins/default/header.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+<title>Zomplog</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+<!-- leave this in place if you're changing the skin -->
+<link rel="stylesheet" href="skins/default/style.css" type="text/css" />
+<script type="text/javascript" src="./popup/scripts/thumbnail.js"></script>
+  
+  <style type="text/css">
+   @import "./popup/css/popups_all.css";
+  </style>
+  <!--[if IE]>
+   <link href="./popup/css/popups_ie.css" rel="stylesheet" type="text/css" media="screen">
+   <script type="text/javascript"> onload = function() { document.focus() }</script>
+  <![endif]-->
+
+
+<script language="javascript" type="text/javascript">
+function OpenLarge (c) {
+    window.open(c,
+                    'large',
+                    'width=500,height=370,scrollbars=yes,status=yes');
+}
+</script> 
+
+<!-- you can change anything below -->
+
+<style type="text/css">
+<!--
+body {
+	background-image: url(skins/default/images/back.gif);
+}
+-->
+</style></head>
+<body>
+
+<table width="650" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF" style='border: #FFFFFF solid; border-width: 1px 1px 1px 1px; padding: 15px 15px 15px 15px;' class="text">
+  <tr>
+    <td colspan="3" background="skins/default/images/top.jpg">
+      <p>&nbsp;</p>
+	    <p>&nbsp;</p>
+      <p><div align="center"><a href="index.php" class="weblog_title"><? echo "$settings[weblog_title]"; ?></a></div></p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+    </td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><table width="100%"  border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td>
