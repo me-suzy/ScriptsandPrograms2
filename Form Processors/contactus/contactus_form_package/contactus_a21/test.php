e871ad21c8da78ab8b83f15e233e713b52272282
me-suzy <me.suzana@gmail.com>
2025-08-12 15:32:47 +0300

    Add Form Processors/contactus/contactus_form_package/contactus_a21/test.php

---
+++ Form Processors/contactus/contactus_form_package/contactus_a21/test.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Contact us</title>
+</head>
+<body>
+<b>This is your contact us page.</b><br> You should insert the following line into the BODY section. <br> Also you should modify "ssconfig.php" file with your own settings.<br> See "ssconfig.php".<br><br>
+
+<? include "contactus_a21.php";?>
+
+</body>
+</html>
