fa06f16b1cfa8eb1640219931bafa9e22c94b1bb
me-suzy <me.suzana@gmail.com>
2025-08-12 11:54:06 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/modules/order_total/ot_gv.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/modules/order_total/ot_gv.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ORDER_TOTAL_GV_TITLE', 'Gift Vouchers');
+  define('MODULE_ORDER_TOTAL_GV_HEADER', 'Gift Vouchers/Discount Coupons');
+  define('MODULE_ORDER_TOTAL_GV_DESCRIPTION', 'Gift Vouchers');
+  define('SHIPPING_NOT_INCLUDED', ' [Shipping not included]');
+  define('TAX_NOT_INCLUDED', ' [Tax not included]');
+  define('MODULE_ORDER_TOTAL_GV_USER_PROMPT', 'Tick to use Gift Voucher account balance ->&nbsp;');
+  define('TEXT_ENTER_GV_CODE', 'Enter Redeem Code&nbsp;&nbsp;');
+?>
