8a198256d1b61e326611a7ae5b1d77cb43de904c
me-suzy <me.suzana@gmail.com>
2025-08-12 11:58:09 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/modules/payment/paypalipn.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/modules/payment/paypalipn.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $Id: paypal_notify.php,v 0.981 2003-16-07 10:57:31 pablo_pasqualino Exp pablo_pasqualino $
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Paypal IPN v0.981 for Milestone 2
+  Copyright (c) 2003 Pablo Pasqualino
+  pablo_osc@osmosisdc.com
+  http://www.osmosisdc.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_PAYPALIPN_TEXT_TITLE', 'PayPal IPN');
+  define('MODULE_PAYMENT_PAYPALIPN_TEXT_DESCRIPTION', 'PayPal IPN');
+  define('MODULE_PAYMENT_PAYPALIPN_TEXT_CURL', 'cURL Activado');
+?>
