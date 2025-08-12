2c2f1fcce08f31ce9cb99c3e18bb87fd85446fa4
me-suzy <me.suzana@gmail.com>
2025-08-12 14:12:29 +0300

    Add File Manipulation/cep/cep/all.php

---
+++ File Manipulation/cep/cep/all.php
@@ -0,0 +1,32 @@
+<?php
+/*------------------------------------------------------------------------------
+CJG EXPLORER PRO v3.2 - WEB FILE MANAGEMENT - Copyright (C) 2003 CARLOS GUERLLOY
+CJGSOFT Software
+cjgexplorerpro@guerlloy.com
+guerlloy@hotmail.com
+carlos@weinstein.com.ar
+Buenos Aires, Argentina
+--------------------------------------------------------------------------------
+This program is free software; you can  redistribute it and/or  modify it  under
+the terms   of the   GNU General   Public License   as published   by the   Free
+Software Foundation; either  version 2   of the  License, or  (at  your  option)
+any  later version. This program  is  distributed in  the hope that  it  will be
+useful,  but  WITHOUT  ANY  WARRANTY;  without  even  the   implied  warranty of
+MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR  PURPOSE.  See the  GNU   General
+Public License for   more details. You  should have received  a copy of  the GNU
+General Public License along  with this  program; if   not, write  to the   Free
+Software  Foundation, Inc.,  59 Temple Place,  Suite 330, Boston,  MA 02111-1307
+USA
+------------------------------------------------------------------------------*/
+
+header("Expires: Mon, 4 Jul 1967 12:00:00 GMT");
+#header("Last-Modified: ".gmdate("D, d M Y H:i:s") . " GMT"); 
+#header("Cache-Control: no-store, no-cache, must-revalidate");
+#header("Cache-Control: post-check=0, pre-check=0", false);
+#header("Pragma: no-cache");
+
+foreach($HTTP_GET_VARS as $k=>$v) {
+if(strstr($v,"..")) die("Invalid Path");
+}
+
+?>
