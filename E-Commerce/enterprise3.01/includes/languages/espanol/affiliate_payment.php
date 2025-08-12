9d94432a36a52d45bd35c608a826b78d87360f9b
me-suzy <me.suzana@gmail.com>
2025-08-12 11:56:08 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_payment.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_payment.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Affiliate Program');
+define('HEADING_TITLE', 'Affiliate Program: Payment');
+
+define('TEXT_AFFILIATE_HEADER', 'Your Payments:');
+
+define('TABLE_HEADING_DATE', 'Payment Date');
+define('TABLE_HEADING_PAYMENT', 'Affiliate Earnings');
+define('TABLE_HEADING_STATUS', 'Payment Status');
+define('TABLE_HEADING_PAYMENT_ID','Payment-ID');
+define('TEXT_DISPLAY_NUMBER_OF_PAYMENTS', 'Displaying <b>%d</b> to <b>%d</b> (of <b>%d</b> payments)');
+define('TEXT_INFORMATION_PAYMENT_TOTAL', 'Your current earnings amount to:');
+define('TEXT_NO_PAYMENTS', 'Up to now still no payments have been recorded.');
+?>
