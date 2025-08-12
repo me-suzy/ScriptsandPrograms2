c1f188f2f273f110ec36a9c9046f5e2fddc7f88b
me-suzy <me.suzana@gmail.com>
2025-08-12 14:21:54 +0300

    Add File Manipulation/download_engine_V1.4.1/download_engine_V1.4.1/download_engine/admin/includes/spaw/lib/lang/zh-big5/zh-big5_lang_data.inc.php

---
+++ File Manipulation/download_engine_V1.4.1/download_engine_V1.4.1/download_engine/admin/includes/spaw/lib/lang/zh-big5/zh-big5_lang_data.inc.php
@@ -0,0 +1,293 @@
+<?php
+// ================================================
+// SPAW PHP WYSIWYG editor control
+// ================================================
+// Chinese Big5 language file 
+// ================================================
+// Developed: Alan Mendelevich, alan@solmetra.lt
+// Chinese translation: aman@wealthgrp.com.tw;aman@516888.com;aman77@pchome.com.tw
+// Copyright: Solmetra (c)2003 All rights reserved.
+// ------------------------------------------------
+//                                www.solmetra.com
+// ================================================
+// v.1.0, 2003-03-20
+// ================================================
+
+// charset to be used in dialogs
+$spaw_lang_charset = 'big5';
+
+// language text data array
+// first dimension - block, second - exact phrase
+// alternative text for toolbar buttons and title for dropdowns - 'title'
+
+$spaw_lang_data = array(
+  'cut' => array(
+    'title' => '°Å¤U'
+  ),
+  'copy' => array(
+    'title' => '´_¨î'
+  ),
+  'paste' => array(
+    'title' => '¶K¤W'
+  ),
+  'undo' => array(
+    'title' => '´_­ì'
+  ),
+  'redo' => array(
+    'title' => '¤Ï´_­ì'
+  ),
+  'hyperlink' => array(
+    'title' => '¶W³sµ²'
+  ),
+  'image_insert' => array(
+    'title' => '´¡¤J¹Ï¤ù',
+    'select' => '¿ï¨ú',
+    'cancel' => '¨ú®ø',
+    'library' => '¸ê®Æ§¨',
+    'preview' => '¹wÄý',
+    'images' => '¹Ï¤ù',
+    'upload' => '¤W¶Ç¹Ï¤ù',
+    'upload_button' => '¤W¶Ç',
+    'error' => '¿ù»~',
+    'error_no_image' => '½Ð¿ï©w¹Ï¤ù',
+    'error_uploading' => 'ÀÉ®×¤W¶Çµo¥Í¿ù»~. ½Ðµy«á­«¶Ç',
+    'error_wrong_type' => 'ÀÉ®×«¬ºA¤£²Å',
+    'error_no_dir' => '§ä¤£¨ì¸ê®Æ§¨',
+  ),
+  'image_prop' => array(
+    'title' => '¹ÏÀÉÄÝ©Ê',
+    'ok' => '   ½T©w   ',
+    'cancel' => '¨ú®ø',
+    'source' => '¨Ó·½',
+    'alt' => '¤å¦r´£¥Ü',
+    'align' => '¹ï»ô',
+    'left' => '¥ª',
+    'right' => '¥k',
+    'top' => '¤W',
+    'middle' => '¤¤',
+    'bottom' => '¤U',
+    'absmiddle' => 'µ´¹ï¤¤¥¡',
+    'texttop' => '¤å¦r³»ºÝ',
+    'baseline' => '°ò·Ç½u',
+    'width' => '¼e«×',
+    'height' => '°ª«×',
+    'border' => 'Ãä®Ø¼e«×',
+    'hspace' => '¤ô¥­¶¡¶Z',
+    'vspace' => '««ª½¶¡¶Z',
+    'error' => '¿ù»~',
+    'error_width_nan' => '¼e«×¤£¬O¼Æ¦r',
+    'error_height_nan' => '°ª«×¤£¬O¼Æ¦r',
+    'error_border_nan' => 'Ãä®Ø¼e«×¤£¬O¼Æ¦r',
+    'error_hspace_nan' => '¤ô¥­¶¡¶Z¤£¬O¼Æ¦r',
+    'error_vspace_nan' => '««ª½¶¡¶Z¤£¬O¼Æ¦r',
+  ),
+  'hr' => array(
+    'title' => '¤ô¥­³W½u'
+  ),
+  'table_create' => array(
+    'title' => '·s¼Wªí®æ'
+  ),
+  'table_prop' => array(
+    'title' => 'ªí®æÄÝ©Ê',
+    'ok' => '   ½T©w   ',
+    'cancel' => '¨ú®ø',
+    'rows' => '¦C¼Æ',
+    'columns' => '¦æ¼Æ',
+    'width' => '¼e«×',
+    'height' => '°ª«×',
+    'border' => 'Ãä®Ø¼e«×',
+    'pixels' => 'px',
+    'cellpadding' => '¤å®Ø¶¡¶Z',
+    'cellspacing' => '®Ø½u¶¡¶Z',
+    'bg_color' => '­I´ºÃC¦â',
+    'error' => '¿ù»~',
+    'error_rows_nan' => '¦C¼Æ¤£¬O¼Æ¦r',
+    'error_columns_nan' => '¦æ¼Æ¤£¬O¼Æ¦r',
+    'error_width_nan' => '¼e«×¤£¬O¼Æ¦r',
+    'error_height_nan' => '°ª«×¤£¬O¼Æ¦r',
+    'error_border_nan' => 'Ãä®Ø¼e«×¤£¬O¼Æ¦r',
+    'error_cellpadding_nan' => '¤å®Ø¶¡¶Z¤£¬O¼Æ¦r',
+    'error_cellspacing_nan' => '®Ø½u¶¡¶Z¤£¬O¼Æ¦r',
+  ),
+  'table_cell_prop' => array(
+    'title' => 'Àx¦s®æÄÝ©Ê',
+    'horizontal_align' => '¤ô¥­¹ï»ô',
+    'vertical_align' => '««ª½¹ï»ô',
+    'width' => '¼e«×',
+    'height' => '°ª«×',
+    'css_class' => 'CSS class',
+    'no_wrap' => '¤å¦r¤£Âà¦æ',
+    'bg_color' => '­I´ºÃC¦â',
+    'ok' => '   ½T©w   ',
+    'cancel' => '¨ú®ø',
+    'left' => '¥ª',
+    'center' => '¤¤',
+    'right' => '¥k',
+    'top' => '³»',
+    'middle' => '¤¤¥¡',
+    'bottom' => '©³',
+    'baseline' => '°ò·Ç½u',
+    'error' => '¿ù»~',
+    'error_width_nan' => '¼e«×¤£¬O¼Æ¦r',
+    'error_height_nan' => '°ª«×¤£¬O¼Æ¦r',
+    
+  ),
+  'table_row_insert' => array(
+    'title' => '´¡¤J¾î¦C'
+  ),
+  'table_column_insert' => array(
+    'title' => '´¡¤Jª½¦æ'
+  ),
+  'table_row_delete' => array(
+    'title' => '§R°£¾î¦C'
+  ),
+  'table_column_delete' => array(
+    'title' => '§R°£ª½¦æ'
+  ),
+  'table_cell_merge_right' => array(
+    'title' => '¦X¨Ã¥k°¼'
+  ),
+  'table_cell_merge_down' => array(
+    'title' => '¦X¨Ã¤U¤è'
+  ),
+  'table_cell_split_horizontal' => array(
+    'title' => '¤ô¥­¤À³Î'
+  ),
+  'table_cell_split_vertical' => array(
+    'title' => '««ª½¤À³Î'
+  ),
+  'style' => array(
+    'title' => 'Style'
+  ),
+  'font' => array(
+    'title' => '¦rÅé'
+  ),
+  'fontsize' => array(
+    'title' => '¦r¸¹'
+  ),
+  'paragraph' => array(
+    'title' => 'Paragraph'
+  ),
+  'bold' => array(
+    'title' => '²ÊÅé'
+  ),
+  'italic' => array(
+    'title' => '±×Åé'
+  ),
+  'underline' => array(
+    'title' => '¥[©³½u'
+  ),
+  'ordered_list' => array(
+    'title' => '§Ç¸¹ªí¦C'
+  ),
+  'bulleted_list' => array(
+    'title' => 'ÂI¸¹ªí¦C'
+  ),
+  'indent' => array(
+    'title' => '¼W¥[ÁY±Æ'
+  ),
+  'unindent' => array(
+    'title' => '´î¤ÖÁY±Æ'
+  ),
+  'left' => array(
+    'title' => '¾a¥ª¤Á»ô'
+  ),
+  'center' => array(
+    'title' => '¸m¤¤¹ï»ô'
+  ),
+  'right' => array(
+    'title' => '¾a¥k¤Á»ô'
+  ),
+  'fore_color' => array(
+    'title' => '¦rÅéÃC¦â'
+  ),
+  'bg_color' => array(
+    'title' => '­I´ºÃC¦â'
+  ),
+  'design_tab' => array(
+    'title' => '¤Á´« WYSIWYG (ª½Ä±) ¼Ò¦¡'
+  ),
+  'html_tab' => array(
+    'title' => '¤Á´« HTML (·½½X) ¼Ò¦¡'
+  ),
+  'colorpicker' => array(
+    'title' => '½Õ¦â½L',
+    'ok' => '   ½T©w   ',
+    'cancel' => '¨ú®ø',
+  ),
+  // <<<<<<<<< NEW >>>>>>>>>
+  'cleanup' => array(
+    'title' => '²M°£HTML (²¾°£ºô­¶®æ¦¡)',
+    'confirm' => '³o­Ó°Ê§@±N·|²M°£©Ò¦³ªººô­¶®æ¦¡¡A½Ðª`·N.',
+    'ok' => '   ½T©w   ',
+    'cancel' => '¨ú®ø',
+  ),
+  'toggle_borders' => array(
+    'title' => '¤Á´«Ãä½u',
+  ),
+  'hyperlink' => array(
+    'title' => '¶W³sµ²',
+    'url' => 'ºô§}',
+    'name' => '¦WºÙ',
+    'target' => '¥Ø¼Ð',
+    'title_attr' => '¥DÃD',
+    'ok' => '   ½T©w   ',
+    'cancel' => '¨ú®ø',
+  ),
+  'table_row_prop' => array(
+    'title' => '¾î¦CÄÝ©Ê',
+    'horizontal_align' => '¤ô¥­¹ï»ô',
+    'vertical_align' => '««ª½¹ï»ô',
+    'css_class' => 'CSS class',
+    'no_wrap' => '¤£´«¦æ',
+    'bg_color' => '­I´ºÃC¦â',
+    'ok' => '   ½T©w   ',
+    'cancel' => '¨ú®ø',
+    'left' => '¥ª',
+    'center' => '¤¤',
+    'right' => '¥k',
+    'top' => '³»',
+    'middle' => '¤¤¥¡',
+    'bottom' => '©³³¡',
+    'baseline' => '°ò½u',
+  ),
+  'symbols' => array(
+    'title' => '¯S®í²Å¸¹',
+    'ok' => '   ½T©w   ',
+    'cancel' => '¨ú®ø',
+  ),
+  'symbols' => array(
+    'title' => '¯S®í²Å¸¹',
+    'ok' => '   ½T©w   ',
+    'cancel' => '¨ú®ø',
+  ),
+  'templates' => array(
+    'title' => '¼ÒªO',
+  ),
+  'page_prop' => array(
+    'title' => 'ºô­¶ÄÝ©Ê',
+    'title_tag' => '¥DÃD',
+    'charset' => '¤å¦r½s½X',
+    'background' => '­I´º¹Ï¤ù',
+    'bgcolor' => '­I´ºÃC¦â',
+    'text' => '¤å¦rÃC¦â',
+    'link' => '³sµ²ÃC¦â',
+    'vlink' => '°ÑÆ[¹Lªº³sµ²ÃC¦â',
+    'alink' => '¥¿¦b°õ¦æªº³sµ²ÃC¦â',
+    'leftmargin' => '¥ªÃä¬É',
+    'topmargin' => '¤W¤èÃä¬É',
+    'css_class' => 'CSS class',
+    'ok' => '   ½T©w   ',
+    'cancel' => '¨ú®ø',
+  ),
+  'preview' => array(
+    'title' => '¹wÄý',
+  ),
+  'image_popup' => array(
+    'title' => '¹Ï¤ù¼u¥X',
+  ),
+  'zoom' => array(
+    'title' => 'Zoom',
+  ),
+);
+?>
