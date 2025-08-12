5cb8561f9c234f70fe2ce65952d917197d1b74e6
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:21 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_approvepublish.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_approvepublish.en.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Approve and publish';
+$LANG['text_1'] = 'Are you sure that you want to approve and publish the selected records?';
+?>
