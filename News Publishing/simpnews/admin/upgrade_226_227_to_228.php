8c2fed9c9a476a8fc81207898072d89b61e11aa5
me-suzy <me.suzana@gmail.com>
2025-08-12 20:28:51 +0300

    Add News Publishing/simpnews/admin/upgrade_226_227_to_228.php

---
+++ News Publishing/simpnews/admin/upgrade_226_227_to_228.php
@@ -0,0 +1,20 @@
+<?php
+/***************************************************************************
+ * (c)2002-2005 Boesch IT-Consulting (info@boesch-it.de)
+ ***************************************************************************/
+require('../config.php');
+?>
+<html><body>
+<div align="center"><h3>SimpNews: Upgrade v2.26/2.27 to v2.28</h3></div>
+<br>
+<?php
+echo "Upgrading tables...<br>";
+flush();
+$sql = "ALTER TABLE ".$tableprefix."_layout ";
+$sql.= "add hnnolinking tinyint(1) unsigned NOT NULL default '0'";
+if(!$result = mysql_query($sql, $db))
+	die("Unable to upgrade table ".$tableprefix."_layout");
+?>
+<br><div align="center">Upgrade done.<br>Please remove install.php, upgrade*.php and fill_freemailer.php from server</div>
+<div align="center">Now you can login to the <a href="index.php">admininterface</a></div>
+</body></html>
