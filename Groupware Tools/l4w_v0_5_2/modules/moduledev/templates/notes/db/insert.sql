be654399659f1ba9f93ac441a99ff737e68ddae0
me-suzy <me.suzana@gmail.com>
2025-08-12 17:10:28 +0300

    Add Groupware Tools/l4w_v0_5_2/modules/moduledev/templates/notes/db/insert.sql

---
+++ Groupware Tools/l4w_v0_5_2/modules/moduledev/templates/notes/db/insert.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO tree VALUES(###id###,         "1",       "###name###",     "",                                                                          "",         "notes.gif", "", ###ordernr###,  NULL, "1", "1", "0", "1");
+INSERT INTO tree VALUES(###idplusone###,  ###id###,  "new ###name###", "../../modules/###name###_###version###/index.php?command=add_contact_view", "l4w_main", "",          '', "1",            NULL, "1", "1", "0", "1");
+INSERT INTO tree VALUES(###idplustwo###,  ###id###,  "browse",         "../../modules/###name###_###version###/index.php?command=show_entries",     "l4w_main", "",          '', "2",            NULL, "1", "1", "0", "1");
