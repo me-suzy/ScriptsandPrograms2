a693fd10c36304bcd2086d11276034aaffa769d4
me-suzy <me.suzana@gmail.com>
2025-08-12 11:45:17 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/german/affiliate_contact.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/german/affiliate_contact.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'eMail an Partnerprogramm Teilnehmer versenden');
+
+define('TEXT_AFFILIATE', 'Partner:');
+define('TEXT_SUBJECT', 'Betreff:');
+define('TEXT_FROM', 'Absender:');
+define('TEXT_MESSAGE', 'Nachricht:');
+define('TEXT_SELECT_AFFILIATE', 'Partner ausw&auml;hlen');
+define('TEXT_ALL_AFFILIATES', 'Alle Partner');
+define('TEXT_NEWSLETTER_AFFILIATES', 'An alle Affiliate Newsletter-Abonnenten');
+
+define('NOTICE_EMAIL_SENT_TO', 'Hinweis: eMail wurde versendet an: %s');
+define('ERROR_NO_AFFILIATE_SELECTED', 'Fehler: Es wurde kein Partner ausgew&auml;hlt.');
+?>
