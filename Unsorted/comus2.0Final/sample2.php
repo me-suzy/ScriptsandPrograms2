735f11b385f81383202da23fc26085ab6ab5b9c1
me-suzy <me.suzana@gmail.com>
2025-08-13 01:50:52 +0300

    Add Unsorted/comus2.0Final/sample2.php

---
+++ Unsorted/comus2.0Final/sample2.php
@@ -0,0 +1,435 @@
+<html>
+<head>
+</head>
+<body bgcolor="#FFFFFF" text="#000000">
+<div align="center"><font size="7">Sample Page #2</font><br>
+  <br>
+</div>
+<table width="100%" border="0" cellspacing="5" cellpadding="5" align="center">
+  <tr>
+    <td height="545" valign="top"> 
+      <table width="100%" border="0" cellspacing="3" cellpadding="10" align="center">
+        <tr> 
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>AMATEURS</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff">
+                          <?
+	   
+	   $onlycat = "Amateurs";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>BABES</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff">
+                          <?	   
+	   $onlycat = "Babes";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>TEENS</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "Teens";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+        </tr>
+      </table>
+      <table width="100%" border="0" cellspacing="3" cellpadding="10" align="center">
+        <tr> 
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>ANAL</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "Anal";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>CUMSHOTS</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff">
+                          <?
+	   	   
+	   $onlycat = "Cumshots";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>FOOT FETISH</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "Foot Fetish";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+        </tr>
+      </table>
+      <table width="100%" border="0" cellspacing="3" cellpadding="10" align="center">
+        <tr> 
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>BBW</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "BBW";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+          <td valign="top" width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="#0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>TOP LIST</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff">&nbsp; </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>GAY</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "Gay";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+        </tr>
+      </table>
+      <table width="100%" border="0" cellspacing="3" cellpadding="10" align="center">
+        <tr> 
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>INTERRACIAL</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "Interracial";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>ASIAN</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "Asian";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>MATURE</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "Mature";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+        </tr>
+      </table>
+      <table width="100%" border="0" cellspacing="3" cellpadding="10" align="center">
+        <tr> 
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>TRANNY</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "Tranny";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="250" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>LESBIAN</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "Lesbians";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+          <td width="33%"> 
+            <div align="center"> 
+              <table width="250" border="0" cellspacing="0" cellpadding="0">
+                <tr> 
+                  <td valign="top" bgcolor="0066cc"> 
+                    <table width="269" border="1" cellspacing="1" cellpadding="3" bordercolor="0066cc">
+                      <tr> 
+                        <td> 
+                          <div align="center"><font color="ffebcd"><b>FETISH / 
+                            BIZARRE</b></font></div>
+                        </td>
+                      </tr>
+                      <tr> 
+                        <td bgcolor="ffffff"> 
+                          <?
+	   	   
+	   $onlycat = "Fetishbizarre";
+	   include($DOCUMENT_ROOT . "/includes/tgpinc.php");
+	   ?>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </td>
+        </tr>
+      </table>
+      <table width="100%" border="0" cellspacing="3" cellpadding="10" align="center">
+        <tr> 
+          <td>&nbsp;</td>
+          <td> 
+            <div align="center"></div>
+          </td>
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+      <p align="center">&nbsp;</p>
+    </td>
+  </tr>
+</table>
+<br>
+<div align="center"> 
+  <p> 
+
+  </p>
+</div>
+</body>
+</html>
