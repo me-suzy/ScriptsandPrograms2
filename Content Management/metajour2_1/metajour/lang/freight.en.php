a3fa6cdb3b355423f7bcf84cc38159dcd1eab403
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:10 +0300

    Add Content Management/metajour2_1/metajour/lang/freight.en.php

---
+++ Content Management/metajour2_1/metajour/lang/freight.en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Freight';
+$LANG['label_name'] = 'Name';
+$LANG['label_init'] = 'Initial price';
+$LANG['label_perweight'] = 'Price per weight';
+$LANG['label_vatid'] = 'VAT';
+?>
