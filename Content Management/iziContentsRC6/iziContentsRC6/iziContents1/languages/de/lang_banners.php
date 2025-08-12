6f71c2b6b748dff6cac871f0cc7fa0d815dfb4fd
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:09 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_banners.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_banners.php
@@ -0,0 +1,53 @@
+<?php
+
+
+//  Formular Titel
+$GLOBALS["tFormTitle"] = 'Banner verwalten';
+
+//  Überschriften listen
+$GLOBALS["tURL"] = 'URL';
+$GLOBALS["tImpressions"] = 'Impressions';
+$GLOBALS["tClicks"] = 'Clicks';
+
+//  Funktionen listen
+$GLOBALS["tAddNewBanner"] = 'Neues Banner hinzuf&uuml;gen';
+$GLOBALS["tViewBanner"] = 'Banner ansehen';
+$GLOBALS["tEditBanner"] = 'Banner &auml;ndern';
+$GLOBALS["tDeleteBanner"] = 'Banner l&ouml;schen ';
+
+//  Formular Block Titel
+$GLOBALS["thDetails"] = 'Banner Anzeige-Details';
+$GLOBALS["thStatus"] = 'Status';
+$GLOBALS["thLog"] = 'Logging';
+
+//  Formularfeld Überschriften
+$GLOBALS["tTarget"] = 'Remote URL';
+$GLOBALS["tAltText"] = 'Bild Alternativtext';
+$GLOBALS["tPublishDate"] = 'Ver&ouml;ffentlichungs-Datum';
+$GLOBALS["tExpireDate"] = 'Verfalls-Datum';
+$GLOBALS["tBannerImage"] = 'Banner Bild';
+$GLOBALS["tBannerHTML"] = 'Banner HTML';
+
+//  Andere Formular Überschriften
+$GLOBALS["tShowBanner"] = 'Banner zeigen';
+
+//  Formular Detail Kommentare und Hilfetexte
+$GLOBALS["tDetails"] = 'Dieses Formular wird f&uuml;r die Verwaltung von Bannern verwendet, die auf Ihrer Webseite erscheinen sollen.';
+$GLOBALS["hTarget"] = 'Die Ziel-URL nach einem Klick auf das Banner.<br />(Schliesst immer ein: \"http:{groups';
+$GLOBALS["hAltText"] = 'Alt Text f&uuml;r das Banner Bild.';
+$GLOBALS["hPublishDate"] = 'Ver&ouml;ffentlichungs-Datum f&uuml;r das Banner. Es wird vor diesem Datum nicht angezeigt.';
+$GLOBALS["hExpireDate"] = 'Verfallsdatum f&uuml;r das Banner. Es wird nach diesem Datum nicht mehr angezeigt. ';
+$GLOBALS["hEnabled"] = 'Wenn dies auf \"Nein\" gesetzt ist, wird das Banner nie zur Anzeige ausgew&auml;hlt.';
+$GLOBALS["hImpressions"] = 'Die Anzahl, wie h&auml;ufig dieses Banner angezeigt wurde. Benutzen Sie dieses Feld, um den Wert zur&uuml;ckzusetzen.';
+$GLOBALS["hClicks"] = 'Die Anzahl, wie h&auml;ufig jemand auf das Banner geklickt hat. Benutzen Sie dieses Feld, um die Zahl zur&uuml;ckzusetzen.';
+$GLOBALS["hBannerImage"] = 'Geben Sie hier den Dateinamen f&uuml;r das Banner-Bild an.<br />Alternativ dazu k&ouml;nnen Sie auch HTML-Code im Banner HTML-Feld hinterlegen, allerdings nicht beides.';
+$GLOBALS["hBannerHTML"] = 'Hier k&ouml;nnen Sie HTML-Code zu einem Bild oder einer Quelle im Web angeben, anstatt ein Bild hochzuladen, z.B. <br />&lt;A HREF=&quot;http:{groups';
+
+//  Fehlernachrichten
+$GLOBALS["eNoURL"] = 'Die Ziel-URL kann nicht leer gelassen werden.';
+$GLOBALS["eImpressionsNum"] = 'Impressions Wert muss numerisch sein.';
+$GLOBALS["eClicksNum"] = 'Clicks Wert muss numerisch sein.';
+$GLOBALS["eNoImage"] = 'Ein Bild oder eine Bild-URL muss angegeben werden.';
+$GLOBALS["eBothImageHTML"] = 'Geben Sie nur das Bild oder das HTML-Banner an, nicht beides.';
+
+?>
