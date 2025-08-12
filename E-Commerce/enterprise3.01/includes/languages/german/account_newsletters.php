6269533ee4948abd88380672fc2100602abac432
me-suzy <me.suzana@gmail.com>
2025-08-12 11:58:49 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/account_newsletters.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/account_newsletters.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Ihr Konto');
+define('NAVBAR_TITLE_2', 'Newsletter Abonnements');
+
+define('HEADING_TITLE', 'Newsletter Abonnements');
+
+define('MY_NEWSLETTERS_TITLE', 'Meine Newsletter Abonnements');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER', 'Allgmeiner Newsletter');
+define('MY_NEWSLETTERS_GENERAL_NEWSLETTER_DESCRIPTION', 'Beinhaltet allgemeine Nachrichten &uuml;nseren Shop, Informationen &uuml;ber neue Produkte, Sonderangebote und andere Informationen von allgemeinem Interesse.');
+
+define('SUCCESS_NEWSLETTER_UPDATED', 'Ihre Newsletter Abonnements wurden erfolgreich aktualisiert!');
+?>
