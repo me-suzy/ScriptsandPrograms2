0dc5273c29c0157b0f4572d90328c951961a2631
me-suzy <me.suzana@gmail.com>
2025-08-12 11:51:51 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/address_book.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/address_book.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'My Account');
+define('NAVBAR_TITLE_2', 'Address Book');
+
+define('HEADING_TITLE', 'My Personal Address Book');
+
+define('PRIMARY_ADDRESS_TITLE', 'Primary Address');
+define('PRIMARY_ADDRESS_DESCRIPTION', 'This address is used as the pre-selected shipping and billing address for orders placed on this store.<br><br>This address is also used as the base for product and service tax calculations.');
+
+define('ADDRESS_BOOK_TITLE', 'Address Book Entries');
+
+define('PRIMARY_ADDRESS', '(primary address)');
+
+define('TEXT_MAXIMUM_ENTRIES', '<font color="#ff0000"><b>NOTE:</b></font> A maximum of %s address book entries allowed.');
+?>
