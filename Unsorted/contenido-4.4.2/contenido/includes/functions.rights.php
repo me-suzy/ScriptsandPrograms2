2c71c7b8c72d74af65aac4e4768a096097d9d145
me-suzy <me.suzana@gmail.com>
2025-08-13 01:59:27 +0300

    Add Unsorted/contenido-4.4.2/contenido/includes/functions.rights.php

---
+++ Unsorted/contenido-4.4.2/contenido/includes/functions.rights.php
@@ -0,0 +1,16 @@
+<?php
+/******************************************
+* File      :   functions.rights.php
+* Project   :   Contenido
+* Descr     :   Defines the 'rights' related
+*               functions
+*
+* Author    :   Timo A. Hummel
+* Created   :   30.04.2002
+* Modified  :   30.04.2003
+*
+* © four for business AG
+*****************************************/
+
+
+?>
