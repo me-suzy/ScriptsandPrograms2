d74dbdb1f1a374bb8376cc41119449940a8e9d90
me-suzy <me.suzana@gmail.com>
2025-08-12 11:43:14 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/stats_ad_results.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/stats_ad_results.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $Id: stats_ad_results.php,v 1.2 $
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Ad Results');
+define('TABLE_HEADING_NUMBER', 'No.');
+define('TABLE_HEADING_ADS','Ad');
+define('TABLE_HEADING_TOTAL_AMOUNT','Amount Earned');
+define('TEXT_AD_RANGE_START','Start Date');
+
+
+
+?>
