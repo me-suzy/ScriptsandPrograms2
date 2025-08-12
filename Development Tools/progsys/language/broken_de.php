795ccc8066a4ddbaa3284485b704e4b574790858
me-suzy <me.suzana@gmail.com>
2025-08-12 10:12:33 +0300

    Add Development Tools/progsys/language/broken_de.php

---
+++ Development Tools/progsys/language/broken_de.php
@@ -0,0 +1,27 @@
+<?php
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+/***************************************************************************
+ * Created by: Boesch IT-Consulting (info@boesch-it.de)
+ * (c)2002-2005 Boesch IT-Consulting
+ * *************************************************************************/
+$l_bm_mailstart = "Ein Benutzer hat einen defekten Referenzlink gemeldet:\r\n";
+$l_bm_contact = "Kontaktperson";
+$l_bm_reporttime = "Meldezeitpunkt";
+$l_bm_reportermail = "Email des Melders";
+$l_bm_subject = "Defekter Referenzlink";
+$l_bm_sitename = "Name der Website";
+$l_bm_url = "URL";
+$l_bm_language = "Sprache";
+$l_bm_reason = "Grund";
+$l_bm_program = "Programm";
+$l_bm_reasons = array("DNS: Name nicht gefunden","keine Antwort (keine Verbindung)","Verschoben","401 Authorization Required","404 Not Found","500 Server Error","Sonstiges");
+$l_bm_email = "Email";
+$l_bm_reporterip = "IP des Melders";
+?>
