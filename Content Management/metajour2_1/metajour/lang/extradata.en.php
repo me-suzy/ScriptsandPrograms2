1e6ea6430071ec988eb62bc22ce176b3b1b00b52
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:57 +0300

    Add Content Management/metajour2_1/metajour/lang/extradata.en.php

---
+++ Content Management/metajour2_1/metajour/lang/extradata.en.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Name';
+$LANG['name'] = 'Extra data';
+$LANG['text_1'] = 'Type';
+$LANG['text_2'] = 'Description';
+$LANG['text_3'] = 'Sorting';
+$LANG['text_4'] = 'Fieldname';
+$LANG['text_5'] = 'Delete';
+$LANG['text_6'] = 'Delete';
+$LANG['text_7'] = 'Add field';
+$LANG['text_8'] = 'Table';
+?>
