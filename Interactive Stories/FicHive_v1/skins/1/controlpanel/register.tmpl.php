bd117aa421c881e8920f3e5ced3dbb92fc7920c7
me-suzy <me.suzana@gmail.com>
2025-08-12 18:50:36 +0300

    Add Interactive Stories/FicHive_v1/skins/1/controlpanel/register.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/controlpanel/register.tmpl.php
@@ -0,0 +1,7 @@
+<table border='0' width='100%'>
+<tr><td colspan='2' class='chapter'><%TAC%></td></tr>
+<tr><td class='fframe'><%USERNAME%></td><td><input type='text' name='u_name'></td></tr>
+<tr><td class='fframe'><%EMAIL%></td><td><input type='text' name='u_email'></td></tr>
+<tr><td class='fframe'><input type='checkbox' name='agree' value='1'></td><td><%AGREES%></td></tr>
+<tr><td colspan='2' class='frame'><input type='submit' value='<%GO%>'></td></tr>
+</table>
