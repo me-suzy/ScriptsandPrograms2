193dd911aad1edf1659fbc2cec7ac88a1efa52f4
me-suzy <me.suzana@gmail.com>
2025-08-12 07:35:43 +0300

    Add Content Management/netious-cms-serv-0.3/cms/delnewsResponse.php

---
+++ Content Management/netious-cms-serv-0.3/cms/delnewsResponse.php
@@ -0,0 +1,90 @@
+<?
+require("../db.php");
+require("include.php");
+DBinfo();
+
+mysql_connect("$DBHost","$DBUser","$DBPass");
+mysql_select_db("$DBName");
+
+
+
+$SUID=f_ip2dec($REMOTE_ADDR);
+if (!session_id($SUID))
+session_start();
+
+$username=$_SESSION['uname'];
+$password=$_SESSION['pass'];
+
+$result=mysql_query("SELECT AdminId FROM mycmsadmin WHERE username='$username' and password='".sha1($password)."'");
+$row=mysql_fetch_row($result);
+$num_rows = mysql_num_rows($result);
+$id=$row[0];
+
+
+
+if ($_SESSION['signed_in']!='indeed' || $num_rows!=1 || $id!=1){
+Header( "Location: index.php?action=2");
+
+}else{
+
+/* Remove from the news page from the db */
+
+
+mysql_query("DELETE FROM news WHERE NewsId='$newsid'");
+
+
+/* Remove the news directory */
+
+$directory="../news/$newsid/files";
+
+$dh=opendir($directory);
+while ($file=readdir($dh))	
+	{if ($file!=".." && $file !=".") unlink("$directory/$file");}
+
+closedir($dh);
+rmdir($directory);
+
+
+$directory="../news/$newsid/images";
+
+$dh=opendir($directory);
+while ($file=readdir($dh))	
+	{if ($file!=".." && $file !=".") unlink("$directory/$file");}
+
+closedir($dh);
+rmdir($directory);
+
+rmdir("../news/$newsid");
+
+/* Read the channel and remove the RSS item from the DB */
+
+$result=mysql_query("SELECT RefId FROM rssitem WHERE ItemId='$itemid'");
+$row=mysql_fetch_row($result);
+$refid=$row[0];
+
+
+mysql_query("DELETE FROM rssitem WHERE ItemId='$itemid'");
+
+/* And the item's directory */
+
+$result=mysql_query("SELECT Name FROM rsschannel WHERE RssId='$refid'");
+$row=mysql_fetch_row($result);
+$channelname=$row[0];
+
+$directory="../rss/$channelname/$itemid";
+
+if (file_exists($directory))
+	{$dh=opendir($directory);
+		while($file=readdir($dh))
+			{if ($file!="." && $file!="..") unlink("$directory/$file");
+			}
+		closedir($dh);
+		rmdir($directory);
+	}
+
+
+Header("Location:makerss.php?rssid=$refid&from=news");
+
+
+}
+?>
