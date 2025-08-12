cd996ebc35e0375a3d5d236cf257b8227fe2453e
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:10 +0300

    Add Unsorted/cashcrusader/pages/invalid_paid_mail.php

---
+++ Unsorted/cashcrusader/pages/invalid_paid_mail.php
@@ -0,0 +1,14 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr> 
+<td align="center"><br>
+<br>
+<font size="4"> Error!<br>
+Invalid paid mail ID.<br>
+Sponsor not found.</font><br>
+<b> </b> <br>
+</td>
+</tr>
+</table>
+<? include("footer.php");?>
