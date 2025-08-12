3657f9930c3b89f39fc764cb112a35ba77f380a9
me-suzy <me.suzana@gmail.com>
2025-08-12 23:36:27 +0300

    Add Reviews and Ratings/sayop1.3/sayop1.3/inc/auth.php

---
+++ Reviews and Ratings/sayop1.3/sayop1.3/inc/auth.php
@@ -0,0 +1,30 @@
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////
+//Please modify the values below to configure the control panel access and script functionality.//
+/////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////
+//Username
+
+$user = "Username";
+
+///////////////////////////////////////////
+//Password
+
+$pass = "Password";
+
+///////////////////////////////////////////
+//The full URL to the script directory: (NO trailing slash)     
+
+$fullurl = "http://www.yourdomain.com/comments";
+
+///////////////Optional below////////////////////
+//Email alerts
+//Set $mail_posts to "1" (one)  if you want to receive an email alert when somone makes a comment, othervise leave it "0" (zero)
+
+$mail_posts = "0";
+
+//If the above is set to "1", specify the email address you want to use for receiving the alerts.
+
+$alerts_email_addy = "foo@bar.com";
+?>
