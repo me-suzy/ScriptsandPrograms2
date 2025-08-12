c5f9106f93e7a81b4008744cf66bee0db81ec788
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:05 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_delete.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_delete.en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Delete';
+$LANG['text_1'] = 'Are you sure that you want to delete the selected records?';
+$LANG['text_cant'] = "This record can't be deleted as it is referenced by other records.";
+?>
