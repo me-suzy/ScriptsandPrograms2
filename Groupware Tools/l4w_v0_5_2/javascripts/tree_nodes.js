5b16b8503258d3f96671c7caf047923d4d50b5b7
me-suzy <me.suzana@gmail.com>
2025-08-12 17:03:43 +0300

    Add Groupware Tools/l4w_v0_5_2/javascripts/tree_nodes.js

---
+++ Groupware Tools/l4w_v0_5_2/javascripts/tree_nodes.js
@@ -0,0 +1,41 @@
+var TREE_NODES = [
+
+	['leads4web 3.0', null, null,
+		['<img src="img/contacts.gif" align="absmiddle" border=0> Kontakte', 'contacts.php', 'main',
+			['Neuer Kontakt', 'subitem2.html', null],
+			['Durchsuchen', 'subitem2.html', null],
+			['Erweiterte Suche', 'subitem2.html', null]
+		],
+		['<img src="img/contacts.gif" align="absmiddle" border=0> Firmen', null, null,
+			['Neuer Kontakt', 'subitem2.html', null],
+			['Durchsuchen', 'subitem2.html', null],
+			['Erweiterte Suche', 'subitem2.html', null]
+		],
+		['<img src="img/todo.gif" align="absmiddle" border=0> Aufgaben', null, null,
+			['Aktuell', 'subitem2.html', null,
+				  ['Neuer Kontakt', 'subitem2.html', null],
+				  ['Durchsuchen', 'subitem2.html', null],
+				  ['Erweiterte Suche', 'subitem2.html', null]
+			],
+			['Durchsuchen', 'subitem2.html', null],
+			['Neue Aufgabe', 'subitem2.html', null]
+		],
+		['<img src="img/notes.gif" align="absmiddle" border=0> Notizen', null, null,
+			['Sub Item 2', 'subitem2.html', null]
+		],
+		['<img src="img/notes.gif" align="absmiddle" border=0> Vorlagen', null, null,
+			['Sub Item 2', 'subitem2.html', null]
+		],
+		['<img src="img/notes.gif" align="absmiddle" border=0> Einstellungen', null, null,
+			['Skins', null, null]
+		],
+		['<img src="img/calendar.gif" align="absmiddle" border=0> Termine', null, null,
+			['Sub Item 2', 'subitem2.html', null]
+		]
+	],
+	['&copy; evandor 2005', null, null,
+		['show4web', null, null]
+	]
+];
+
+
