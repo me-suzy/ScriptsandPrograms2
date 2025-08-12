f6f873f76607a339f54638d26b9917947aa315f2
me-suzy <me.suzana@gmail.com>
2025-08-12 08:52:26 +0300

    Add Database Tools/discoman 1.4/discoman 1.4/bottom.php

---
+++ Database Tools/discoman 1.4/discoman 1.4/bottom.php
@@ -0,0 +1,12 @@
+<?php
+
+//CREDIT SUR 3EME FRAME
+
+require("presentation.inc.php");
+HAUTPAGEWEB('www.the-mirror-of-dreams.com');
+CREDIT();
+
+echo "<div align=\"center\">DiscoMan &copy; 2004 E.R. - <a href=\"http://www.the-mirror-of-dreams.com\" target=\"_blank\">www.the-mirror-of-dreams.com</a>";
+
+BASPAGEWEB();
+?>
