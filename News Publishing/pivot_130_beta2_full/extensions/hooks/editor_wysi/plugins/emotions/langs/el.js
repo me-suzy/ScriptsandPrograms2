c205cbce6aec8699ce7823a115d8185d344fe219
me-suzy <me.suzana@gmail.com>
2025-08-12 20:09:06 +0300

    Add News Publishing/pivot_130_beta2_full/extensions/hooks/editor_wysi/plugins/emotions/langs/el.js

---
+++ News Publishing/pivot_130_beta2_full/extensions/hooks/editor_wysi/plugins/emotions/langs/el.js
@@ -0,0 +1,6 @@
+// Greek lang variables by Jacaranda Bill
+
+tinyMCE.addToLang('',{
+insert_emotions_title : '&Aring;éóáãùãÞ &aring;íüò &aring;éêïíé&auml;ßïõ emoticon',
+emotions_desc : '&Aring;éêïíß&auml;éá emoticons'
+});
