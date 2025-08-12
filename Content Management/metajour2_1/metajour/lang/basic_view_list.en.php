983c71f510b23a9ab84af30f1f92d1f91afe09aa
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:25 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_list.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_list.en.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['title'] = 'List';
+
+$LANG['img_search'] = 'Search';
+$LANG['img_create'] = 'Create';
+$LANG['img_listall'] = 'Show all';
+$LANG['img_hierarchy'] = 'Tree view';
+
+$LANG['footer_count'] = 'COUNT:';
+
+$LANG['label_objectid'] = 'Objectid';
+$LANG['label_createdbyname'] = 'Created by';
+$LANG['label_changed'] = 'Changed';
+$LANG['label_language'] = 'Language';
+
+$LANG['tool_field'] = 'Field';
+$LANG['tool_like'] = 'Contains';
+$LANG['tool_likestart'] = 'Starts with';
+$LANG['tool_likeend'] = 'Ends with';
+$LANG['tool_standardcol'] = 'Show standard columns';
+$LANG['tool_standardsort'] = 'Standard sorting';
+
+$LANG['alt_element'] = 'Doubleclick to edit record, rightclick for options';
+$LANG['alt_inactive'] = 'Record is inactive';
+$LANG['alt_default'] = 'Record is set as default';
+$LANG['alt_access'] = 'Record is protected';
+$LANG['alt_variant'] = 'Record has variants';
+$LANG['alt_islocked'] = 'Record is locked';
+$LANG['alt_preview'] = 'Click to see preview';
+
+?>
