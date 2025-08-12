7bd816961669ea789f83ac49b71e0d55c5e01459
me-suzy <me.suzana@gmail.com>
2025-08-12 20:10:52 +0300

    Add News Publishing/pivot_130_beta2_full/extensions/hooks/editor_wysi/plugins/iespell/langs/zh_cn.js

---
+++ News Publishing/pivot_130_beta2_full/extensions/hooks/editor_wysi/plugins/iespell/langs/zh_cn.js
@@ -0,0 +1,7 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµ&Auml;&Ouml;ÐÎ&Auml;&Ouml;§³&Ouml;
+
+tinyMCE.addToLang('',{
+iespell_desc : 'ÔËÐÐÆ´Ð´¼ì²é',
+iespell_download : "Î´¼ì²âµ½ieSpellÆ´Ð´¼ì²é£¬µã»÷ OK Ç°ÍùÏÂÔØÒ³Ãæ¡£"
+});
