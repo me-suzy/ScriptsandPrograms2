8bc7afeca7acfaa26d6a3fb56e329609eebdb065
me-suzy <me.suzana@gmail.com>
2025-08-12 13:54:35 +0300

    Add Exchanges/mybannerexchange/index.php

---
+++ Exchanges/mybannerexchange/index.php
@@ -0,0 +1,10 @@
+
+<p><font face="Verdana, Arial, Helvetica, sans-serif"><font face="Verdana, Arial, Helvetica, sans-serif"><b>MyBannerExchange 
+    </b></font><br>
+    <font size="-1">This index page should service only as an EXAMPLE. Nothing 
+    in the config file applies here. This page is here only to show you what files 
+    to link to for the signup and login pages.</font></font></p>
+  <p align="center"><font face="Verdana, Arial, Helvetica, sans-serif"><b><a href="create.php">Create 
+    Account</a><br>
+    <a href="edit.php">Log In</a></b></font></p>
+  <p>
