179f8acd8928ce9e8dae3bcbe42db78f5f396043
me-suzy <me.suzana@gmail.com>
2025-08-12 18:21:42 +0300

    Add Image Galleries/singapore-0.9.11/singapore-0.9.11/templates/admin_default/footer.tpl.php

---
+++ Image Galleries/singapore-0.9.11/singapore-0.9.11/templates/admin_default/footer.tpl.php
@@ -0,0 +1,10 @@
+
+<div id="footer"><p>
+  <?php echo $sg->allRightsReserved() ?>
+  <br />
+  <?php echo $sg->poweredByVersion() ?> 
+  <?php echo $sg->scriptExecTimeText() ?>
+</p></div>
+
+</body>
+</html>
