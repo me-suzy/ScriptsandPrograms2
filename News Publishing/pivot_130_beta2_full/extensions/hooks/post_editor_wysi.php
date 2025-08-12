46c32f8728c9e427784fa2a80e54becf7d50139c
me-suzy <me.suzana@gmail.com>
2025-08-12 20:07:38 +0300

    Add News Publishing/pivot_130_beta2_full/extensions/hooks/post_editor_wysi.php

---
+++ News Publishing/pivot_130_beta2_full/extensions/hooks/post_editor_wysi.php
@@ -0,0 +1,30 @@
+<?php
+// Name: TinyMCE editor extension.
+// Version: 0.1
+// Author: Pivot Development Team
+// License: GPL 2.0
+
+// See pre_editor_wysi for instructions.
+
+
+/**
+ * 
+ *
+ */
+function post_editor_wysi_init() {
+
+		
+}
+
+
+/**
+ * 
+ *
+ */
+function post_editor_wysi() {
+
+	
+}
+
+
+?>
