b953c3a07831cbf1b77a81cc0fff791688b6aabc
me-suzy <me.suzana@gmail.com>
2025-08-13 02:22:23 +0300

    Add Unsorted/datingscript/templates/love/config.php

---
+++ Unsorted/datingscript/templates/love/config.php
@@ -0,0 +1,49 @@
+<?
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.1.1                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 04/05/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# Template Name           Love                             #
+# Author                  AzDG <support@azdg.com>          #
+############################################################
+# File name               config.php                       #
+# File purpose            Config for love template         #
+# File created by         AzDG <support@azdg.com>          #
+############################################################
+###### Templates
+### Colors and Borders
+define('COLOR1','#FECDDE'); // Color for add form, search, email and other
+define('COLOR2','#FECBDD'); 
+define('COLOR3','#FEB8D0'); 
+define('COLOR4','#FEC7DA'); 
+define('COLORH','#FEBAC6'); // Head color
+define('C_BORDER','2'); // Table border (external)
+define('C_IBORDER','1'); // Table border (internal)
+define('C_BWIDTH','740'); // Table width (Big width) 
+define('C_WIDTHL','189'); // Table width left 
+define('C_WIDTHC','360'); // Table width center 
+define('C_WIDTHR','189'); // Table width right 
+define('C_WIDTH','500'); // Table width (Medium forms) 
+define('C_SWIDTH','350'); // Table width (Small forms) 
+define('C_ALIGN','left'); // Align td
+define('C_CELLP','5'); // Table CellPadding
+define('C_TBCOLOR','#B00B76'); // Table border color
+define('C_GRAPHIC','250'); // Graphic max width
+define('C_WIN_WIDTH','400'); // Window width (bad profile, add to bedroom)
+define('C_WIN_HEIGHT','300'); // Window height 
+define('C_IMG_BRDR','2'); // Image Border 
+
+### Error and succesfull messages templates
+define('C_ETOP','<center><br><br><br><br><Table Border=1 CellSpacing=0 CellPadding=4 bordercolor=black width=600><Tr><Td Width=600 align=center class=head bgcolor='.COLOR1.'><br><br>');
+
+define('C_EBOT','<br><br><input class=button type=button OnClick=\'history.back()\' value=<<<<<><br></Td></Tr></table><br><br><br><br></center>');
+
+define('C_SBOT','<br><br><br></Td></Tr></table><br><br><br><br></center>');
+
+
+
+?>
