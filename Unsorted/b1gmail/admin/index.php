0dbc6240a3a6bb4ca809b6389616677b7b996926
me-suzy <me.suzana@gmail.com>
2025-08-13 01:08:27 +0300

    Add Unsorted/b1gmail/admin/index.php

---
+++ Unsorted/b1gmail/admin/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Admin</title>
+</head>
+<frameset rows="50,*" border="0">
+ <frame src="header.php" scrolling="no" name="header" noresize>
+ <frame src="welcome.php" scrolling="auto" name="main" noresize>
+</frameset>
+</html>
