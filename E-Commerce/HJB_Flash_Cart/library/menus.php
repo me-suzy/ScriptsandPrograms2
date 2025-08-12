8d3bd3ebc15ceead6e3b1f2bf36705afb802873b
me-suzy <me.suzana@gmail.com>
2025-08-12 12:22:59 +0300

    Add E-Commerce/HJB_Flash_Cart/library/menus.php

---
+++ E-Commerce/HJB_Flash_Cart/library/menus.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+***************************************************************************************************************************
+*****************************************COPYRIGHT 2005 YOU MAY NOT USE THIS WITHOUT PERMISSION****************************
+
+HJB IS PROVIDED "As Is" FOR USE ON WEBSITES WHERE A LICENSE FOR SUCH USE WAS PURCHASED.  IT MAY ONLY BE USED ON ONE SITE PER LICENSING
+FEE.  IN ORDER TO USE ON ADDITIONAL SITES, ADDITIONAL LICENSES MUST BE PURCHASED.  
+
+
+THE PHP SCRIPTS MAY BE ALTERED, AS LONG AS THE CREDIT LINE AND LINKS AT THE BOTTOM OF EACH PAGE REMAIN. THE FLASH MAY NOT IN ANY
+WAY BE CHANGED OR ALTERED.  ANY VIOLATION OF THESE TERMS WILL RESULT IN THE FORFEITING OF YOUR RIGHT TO USE THIS SOFTWARE.
+
+NationWideShelving.com does not guarantee this software in anyway.  You use this at your own risk.  NationWideShelving or any of its
+employees or subsidiaries are not responsible for any damage, and / or loss of business, reputation, or other damages of any kind
+which are caused whether actual or not, by the use of this product.  By using this product you agree to hold NationWideShelving, its
+employees, and all subsidiaries harmless for any and all reasons associated with your use of this product.
+
+Your installation of this software consititues an agreement to these terms.
+
+****************************************************************************************************************************
+	*/
+
+function menu_shopping_cart(){
+?>
+<style type="text/css">
+<!--
+.linker {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-size: 10px;
+	color: #000000;
+	text-decoration: none;
+}
+-->
+</style>
+<table width="100%"  border="0" cellspacing="4" cellpadding="4">
+              <tr>
+                <td><div align="center"><span class="style1 style6"><a href="/update/index.php"><img src="/images/buttons/cp_home.gif" width="198" height="40" border="0"></a></span></div></td>
+              </tr>
+           
+              <tr>
+                <td><div align="center" class="style1 style6"><span class="style7"><a href="/update/shopping_cart/categories.php?siteID=1"><img src="/images/buttons/manage_sites_cp.gif" width="198" height="40" border="0"></a></span></div></td>
+              </tr>
+			 
+              <tr>
+                <td><div align="center" class="style7"><a href="../update/shopping_cart/manage_products.php"><img src="/images/buttons/manage_products_cp.gif" width="198" height="40" border="0"></a></div></td>
+              </tr>
+              <tr>
+                <td><div align="center"><span class="style7"><a href="/library/logout.php"><img src="/images/buttons/logout.gif" width="198" height="40" border="0"></a></span></div></td>
+              </tr>
+              
+              
+             
+</table>
+<?php
+}
+
+
+
+?>
