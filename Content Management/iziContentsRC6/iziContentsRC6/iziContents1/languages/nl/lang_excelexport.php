fbe3f75504cedec3cd76a6cc6b50271232baabaf
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:30 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_excelexport.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_excelexport.php
@@ -0,0 +1,18 @@
+<?php
+$GLOBALS["texFormTitle"] = 'Excel-Export';
+$GLOBALS["texTitel"] = 'Zoek naar gebruikers om te exporteren naar een excel-file';
+$GLOBALS["texSubmit"] = 'Zoek';
+$GLOBALS["texSearchAll"] = 'ALLES';
+$GLOBALS["texName"] = 'Naam';
+$GLOBALS["texPLZ"] = 'Postcode';
+$GLOBALS["texOrt"] = 'Stad';
+$GLOBALS["texCountry"] = 'Land';
+$GLOBALS["texUsergroup"] = 'Gebruikersgroep';
+$GLOBALS["texSearchKriteria"] = 'Zoek criteria';
+$GLOBALS["texNoKriteria"] = 'geen';
+$GLOBALS["texNoResults"] = 'Niets gevonden!';
+$GLOBALS["texFoundResults"] = 'Gevonden resultaat';
+$GLOBALS["texDlExcel"] = 'Download Excel-File';
+$GLOBALS["texPhone"] = 'Telefoon';
+$GLOBALS["texAdresse"] = 'ADRES';
+?>
