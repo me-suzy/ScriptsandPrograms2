127b7f6675cc2f5f4f36ccc7f65af9d09cbe0882
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:29 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_imageformats.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_imageformats.php
@@ -0,0 +1,39 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintain image format templates';
+
+//  List Headings
+$GLOBALS["tName"] = 'Name';
+$GLOBALS["tBorder"] = 'Border';
+$GLOBALS["tAlignment"] = 'Alignment';
+$GLOBALS["tColour"] = 'Colour';
+
+//  List Functions
+$GLOBALS["tAddNewIFTemplate"] = 'Add new image template';
+$GLOBALS["tViewIFTemplate"] = 'View image template';
+$GLOBALS["tEditIFTemplate"] = 'Edit image template';
+$GLOBALS["tDeleteIFTemplate"] = 'Delete image template';
+
+//  Form Block Titles
+$GLOBALS["thIFGeneral"] = 'Image Format Template Details';
+
+//  Form Field Headings
+$GLOBALS["tIFName"] = 'Image template name';
+$GLOBALS["tIFAlign"] = 'Alignment';
+$GLOBALS["tIFBorder"] = 'Border size';
+$GLOBALS["tIFBackground"] = 'Background colour';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'This form lets you build templates for images in your content.';
+$GLOBALS["hIFName"] = 'The name that the image template will be referred to in the [image] tag.';
+$GLOBALS["hIFAlign"] = 'Side of the page that the image will appear on, Left or Right.';
+$GLOBALS["hIFBorder"] = 'Size of the displayed image border.';
+$GLOBALS["hIFBackground"] = 'Background color for the image.';
+
+//  Error Messages
+$GLOBALS["eNoName"] = 'Image template must have a name.';
+$GLOBALS["eNameExists"] = 'An image template with this name already exists, please choose another.';
+$GLOBALS["eColourWrong"] = 'Background color value is wrong.';
+
+?>
