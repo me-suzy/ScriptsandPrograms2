ad17c13422f8e8163479c470e2f0033a12365dbc
me-suzy <me.suzana@gmail.com>
2025-08-12 23:17:39 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_liens/lang/lang-mod_liens-lang-spanish.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_liens/lang/lang-mod_liens-lang-spanish.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE LIENS
+$Mod_Liens_PlRecent="Los 10 Vínculos (los + recientes)";
+$Mod_Liens_Top="El top 10 de los vínculos";
+$Mod_Liens_form_Auteur="Autor";
+$Mod_Liens_form_Mail="EMail";
+$Mod_Liens_form_Rubrique="Rúbrica";
+$Mod_Liens_form_Titre="Título";
+$Mod_Liens_form_Lien="Vínculo";
+$Mod_Liens_form_Desc="Descripción";
+$Mod_Liens_JS_Auteur="El campo Autor tiene que ser rellenado";
+$Mod_Liens_JS_mail="El campo EMail tiene que ser rellenado";
+$Mod_Liens_JS_Rubrique="El campo Rúbrica tiene que ser rellenado";
+$Mod_Liens_JS_Titre="El campo Título tiene que ser rellenado";
+$Mod_Liens_JS_Lien="El campo Vínculo tiene que ser rellenado";
+$Mod_Liens_JS_Desc="El campo Descripción tiene que ser rellenado";
+$Mod_Lien_inde_Categorie="Typo";
+$Mod_Liens_inde_Date="Fecha";
+$Mod_Liens_inde_Titre="Titulo";
+$Mod_Liens_inde_Auteur="Autore";
+$Mod_Liens_inde_Clics="Clics";
+
+?>
