d609565a7eacdc2d30342433af7ced73df841921
me-suzy <me.suzana@gmail.com>
2025-08-12 18:49:51 +0300

    Add Interactive Stories/FicHive_v1/skins/1/controlpanel/deletereviews.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/controlpanel/deletereviews.tmpl.php
@@ -0,0 +1,4 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%REVIEWNO%></td><td><input type='text' name='rid'></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>' onClick='javascript:return confirm("<%AREYOUSURE%>")'></td></tr>
+</table>
