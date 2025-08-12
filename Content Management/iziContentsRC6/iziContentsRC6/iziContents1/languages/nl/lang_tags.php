bdacde0e0e5a09c9c42504499d2bb0a905202bfd
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:10 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_tags.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_tags.php
@@ -0,0 +1,27 @@
+<?
+
+// Form Title
+$GLOBALS["tFormTitle"]		= 'Beheer tags';
+
+// List Headings
+$GLOBALS["tCategory"] = 'Categorie';
+$GLOBALS["tTag"]		= 'Tag';
+$GLOBALS["tTranslation"]	= 'Vertaling';
+
+// List Functions
+$GLOBALS["tAddNewTag"]		= 'Nieuwe tag toevoegen';
+$GLOBALS["tViewTag"] = 'Bekijk tag';
+$GLOBALS["tEditTag"]		= 'Bewerk tag';
+$GLOBALS["tDeleteTag"]		= 'Verwijder tag';
+
+// Form Detail Comments and Help Texts
+$GLOBALS["tDetails"]		 = 'Met dit formulier kun je tags definiëren voor de inhoud van uw website.';
+$GLOBALS["hCategory"] = 'Selecteer de categorie die overeen komt met de groep waartoe de tag behoort.';
+$GLOBALS["hTag"]		 = 'Dit is de tagcode die gebruikt wordt binnen de inhoud, bijv. een artikel.<br>Binnen de content, moet de tag tussen teksthaken staan <b>[tag]</b> om herkend te worden door het script; </ br>Geef de tag een naam , bijv. <b>tag</b>.<br>Als je een open- en sluittag nodig hebt, bijvoorbeeld "vet" (<b>[b]</b> en <b>[/b]</b>), </ br>moet je allebei definiëren, bijv. <b>tag</b> en <b>/tag</b>.';
+$GLOBALS["hTranslation"]	 = 'Dit is de vertaling van je tag in "straight" HTML.';
+
+// Error Messages
+$GLOBALS["eNoTag"]		= 'Tag naam mag niet leeg zijn.';
+$GLOBALS["eNoTranslation"]	= 'Vertaling mag niet leeg zijn.';
+
+?>
