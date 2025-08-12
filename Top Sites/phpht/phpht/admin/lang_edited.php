c5b759dfdc0f8bac0c2cc86c0511d9dc7f756744
me-suzy <me.suzana@gmail.com>
2025-08-13 00:01:02 +0300

    Add Top Sites/phpht/phpht/admin/lang_edited.php

---
+++ Top Sites/phpht/phpht/admin/lang_edited.php
@@ -0,0 +1,96 @@
+<?php
+/****************************************************************/
+/*                       phpht Topsites                         */
+/*                 admin/lang_edited.php file                   */
+/*                      (c)copyright 2003                       */
+/*                       By hinton design                       */
+/*                 http://www.hintondesign.org                  */
+/*                  support@hintondesign.org                    */
+/*                                                              */
+/* This program is free software. You can redistrabute it and/or*/
+/* modify it under the terms of the GNU General Public Licence  */
+/* as published by the Free Software Foundation; either version */
+/* 2 of the license.                                            */
+/*                                                              */
+/****************************************************************/
+$phpht_real_path = "./../";
+include($phpht_real_path . 'common.php');
+
+if($HTTP_COOKIE_VARS['loged'] == 'yes') {
+   if($HTTP_COOKIE_VARS['user_level'] == '1') {
+      if(!$HTTP_POST_VARS['name']) {
+         include("page_header_admin.php");
+         $display = "Please fill in the name field.";
+         $template->getFile(array(
+                            'error' => 'admin/error.tpl')
+         );
+         $template->add_vars(array(
+                            'L_ERROR' => $lang['error'],
+                            'DISPLAY' => $display)
+         );
+         $template->parse("error");
+         include("page_footer_admin.php");
+         exit();
+      }
+
+      $sql = "UPDATE ".$prefix."_lang SET name='$HTTP_POST_VARS[name]' WHERE id='$HTTP_POST_VARS[id]'";
+      $result = $db->query($sql);
+
+      if(!$result) {
+         include("page_header_admin.php");
+         $display = "Could not edit the language.";
+         $template->getFile(array(
+                            'error' => 'admin/error.tpl')
+         );
+         $template->add_vars(array(
+                            'L_ERROR' => $lang['error'],
+                            'DISPLAY' => $display)
+         );
+         $template->parse("error");
+         include("page_footer_admin.php");
+         exit();
+      } else {
+         include("page_header_admin.php");
+         $display = "The language has been edited.";
+         $link = "lang.php";
+         $template->getFile(array(
+                            'success' => 'admin/success.tpl')
+         );
+         $template->add_vars(array(
+                            'L_SUCCESS' => $lang['success'],
+                            'DISPLAY' => $display,
+                            'LINK' => $link)
+         );
+         $template->parse("success");
+         include("page_footer_admin.php");
+         exit();
+      }
+   } else {
+      include('page_header_admin.php');
+      $display = "You do not have permission to view this page";
+      $template->getFile(array(
+                         'error' => 'admin/error.tpl')
+      );
+      $template->add_vars(array(
+                         'L_ERROR' => $lang['error'],
+                         'DISPLAY' => $display)
+      );
+      $template->parse("error");
+      include('page_footer_admin.php');
+      exit();
+   }
+} else {
+      include('page_header_admin.php');
+      $display = "You are not logged in Please do so <a href=\"../admin.php\">Here</a>.";
+      $template->getFile(array(
+                         'error' => 'admin/error.tpl')
+      );
+      $template->add_vars(array(
+                         'L_ERROR' => $lang['error'],
+                         'DISPLAY' => $display)
+      );
+      $template->parse("error");
+      include('page_footer_admin.php');
+      exit();
+}
+?>
