4f9866e9bc6fd233d5a5993d69d83d0c3d4fac0a
me-suzy <me.suzana@gmail.com>
2025-08-13 01:26:51 +0300

    Add Unsorted/cashcrusader/pages/expired_paid_mail.php

---
+++ Unsorted/cashcrusader/pages/expired_paid_mail.php
@@ -0,0 +1,15 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr> 
+<td align="center"><font size="4"><br>
+<br>
+Sorry, this link has expired.<br>
+<br>
+<br>
+</font></td>
+</tr>
+</table>
+<b><font size="5" face="arial"><br>
+</font></b> 
+<? include("footer.php");?>
