b78265c5afa988c93b30a9572576ed9e6cdc7f5d
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:15 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_userdata.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_userdata.php
@@ -0,0 +1,27 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Beheer van gebruikersdata frame layout';
+
+//  Edit Function Texts
+$GLOBALS["tToggleUserdata"] = 'Toon gebruikersdata onderdeel';
+
+//  List Headings
+$GLOBALS["tUserdataname"] = 'Gebruikersdata onderdeel';
+$GLOBALS["tEnabled"] = 'Toegelaten';
+
+//  Userdata Elements
+$GLOBALS["tdloginmessage"] = 'Inlog bericht';
+$GLOBALS["tdloginlogout"] = 'In- en uitloggen';
+$GLOBALS["tdadminfunctions"] = 'Admin functies';
+$GLOBALS["tdpreferences"] = 'Registratie van nieuwe gebruiker/Beheer voorkeuren gebruiker';
+$GLOBALS["tdmemberlist"] = 'Ledenlijst';
+$GLOBALS["tdlanguage"] = 'Taal';
+$GLOBALS["tdselectlanguage"] = 'Taalselectie';
+$GLOBALS["tdsite"] = 'Toon site';
+$GLOBALS["tdselectsite"] = 'Selecteer site';
+$GLOBALS["tdtheme"] = 'Thema';
+$GLOBALS["tdselecttheme"] = 'Selecteer thema';
+$GLOBALS["tdhomesite"] = 'Homesite link';
+
+?>
