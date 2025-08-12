698d5867809ba21f397cf452f19e3539e1b158fb
me-suzy <me.suzana@gmail.com>
2025-08-12 23:31:32 +0300

    Add Randomizing/6/RandomPopup/example.php

---
+++ Randomizing/6/RandomPopup/example.php
@@ -0,0 +1,28 @@
+<?
+#########################################################
+# Random Popup                                          #
+#########################################################
+#                                                       #
+# Author: Doni Ronquillo                                #
+#                                                       #
+# This script and all included functions, images,       #
+# and documentation are copyright 2003                  #
+# free-php.net (http://free-php.net) unless             #
+# otherwise stated in the module.                       #
+#                                                       #
+# Any copying, distribution, modification with          #
+# intent to distribute as new code will result          #
+# in immediate loss of your rights to use this          #
+# program as well as possible legal action.             #
+#                                                       #
+#########################################################
+
+	include('inc/header.php');
+
+    # include popup.php like below in the pages you wish
+    # to have the popup display
+    include('popup.php');
+    
+ 	include('inc/footer.php');
+    
+?>
