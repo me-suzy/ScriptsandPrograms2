c7145df1943efbab5184a08568b02f3abbaddf5f
me-suzy <me.suzana@gmail.com>
2025-08-12 13:52:50 +0300

    Add Email Systems/myAgenda-v.1.1/myAgenda-v.1.1/logout.php

---
+++ Email Systems/myAgenda-v.1.1/myAgenda-v.1.1/logout.php
@@ -0,0 +1,15 @@
+<?php
+#############################################################################
+# myAgenda v1.1																#
+# =============																#
+# Copyright (C) 2002  Mesut Tunga - mesut@tunga.com							#
+# http://www.tunga.com														#
+#																			#
+# This program is free software. You can redistribute it and/or modify		#
+# it under the terms of the GNU General Public License as published by 		#
+# the Free Software Foundation; either version 2 of the License.       		#
+#############################################################################
+	setcookie("asID",0,0,"/");
+	setcookie("auID",0,0,"/");
+	header("Location: ./");
+?>
