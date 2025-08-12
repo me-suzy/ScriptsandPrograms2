d0b9b6303657f0e017a3bd9ec1ba8cd026fa1f76
me-suzy <me.suzana@gmail.com>
2025-08-12 10:03:30 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/generalFunctions.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/generalFunctions.php
@@ -0,0 +1,12 @@
+<?php
+
+/** Collection of general purpose functions that may be used by all phpPeanuts classes 
+*/
+
+/** Tries to include a file once. Returns wheater the file 
+* was included or included before.  Does not trigger warnings.
+*/
+require_once('../classes/pnt/generalFunctions.php');
+
+
+?>
