9b195804999ea9d7d39a8f6be4abf8b211e804f0
me-suzy <me.suzana@gmail.com>
2025-08-13 01:00:50 +0300

    Add Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/upg_img.sql

---
+++ Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/upg_img.sql
@@ -0,0 +1,12 @@
+ALTER TABLE al_img RENAME al_oldimg;
+
+CREATE TABLE al_img (
+  id smallint(5) unsigned NOT NULL auto_increment,
+  type varchar(10) NOT NULL default '',
+  login varchar(16) NOT NULL default '',
+  format varchar(10) NOT NULL default '',
+  extension char(3) NOT NULL default '',
+  updated datetime NOT NULL default '0000-00-00 00:00:00',
+  rawdata mediumblob NOT NULL,
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
