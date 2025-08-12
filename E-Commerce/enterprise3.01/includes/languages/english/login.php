d1723f1594bcee51d6124159f8d5659528532481
me-suzy <me.suzana@gmail.com>
2025-08-12 11:53:18 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/login.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/login.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Login');
+define('HEADING_TITLE', 'Welcome, Please Sign In');
+
+define('HEADING_NEW_CUSTOMER', 'Click Continue If You Have Not Shopped With Us Previously');
+define('TEXT_NEW_CUSTOMER', '');
+define('TEXT_NEW_CUSTOMER_INTRODUCTION', 'Please click continue to create an account so that you can finish your purchase.  Thank you.');
+
+define('HEADING_RETURNING_CUSTOMER', 'Returning Customer');
+define('TEXT_RETURNING_CUSTOMER', 'I am a returning customer.');
+
+define('TEXT_PASSWORD_FORGOTTEN', 'Password forgotten? Click here.');
+
+define('TEXT_LOGIN_ERROR', 'Error: No match for E-Mail Address and/or Password.');
+define('TEXT_VISITORS_CART', '<font color="#ff0000"><b>Note:</b></font> Your &quot;Visitors Cart&quot; contents will be merged with your &quot;Members Cart&quot; contents once you have logged on. <a href="javascript:session_win();">[More Info]</a>');
+?>
