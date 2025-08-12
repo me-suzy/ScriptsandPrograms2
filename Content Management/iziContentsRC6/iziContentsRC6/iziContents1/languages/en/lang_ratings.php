6cfe4acbbde35e367cec4b34cdf21ae53318f695
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:41 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_ratings.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_ratings.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintain content settings';
+
+//  Form Field Headings
+$GLOBALS["tAllowRatings"] = 'Allow Article Ratings';
+$GLOBALS["tAllowComments"] = 'Allow Article Comments';
+$GLOBALS["tRatingMin"] = 'Rating Minimum Value';
+$GLOBALS["tRatingMax"] = 'Rating Maximum Value';
+$GLOBALS["tRatingImage1"] = 'Star Rating Image';
+$GLOBALS["tRatingImage2"] = 'Star Rating Image (unrated)';
+$GLOBALS["tVetComments"] = 'Vet Comments before release';
+$GLOBALS["tPrintFriendly"] = 'Printer-Friendly Icon Display';
+$GLOBALS["tTeaserWithDetails"] = 'Teaser Text on Detail Page';
+
+//  Form Block Titles
+$GLOBALS["thPrinterFriendly"] = 'Printer Friendly Options';
+$GLOBALS["thContentRatings"] = 'Content Rating Options';
+$GLOBALS["thContentComments"] = 'Content Comment Options';
+$GLOBALS["thContentSettings"] = 'Other Content Options';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'This form allows you to set some global values for content rating on your site.';
+$GLOBALS["hAllowRatings"] = 'Do you wish to allow members to rate articles of content?';
+$GLOBALS["hAllowComments"] = 'Do you wish to allow members to comment on articles?';
+$GLOBALS["hRatingMin"] = 'Select the minimum rating value.';
+$GLOBALS["hRatingMax"] = 'Select the maximum rating value.';
+$GLOBALS["hRatingImage1"] = 'Select the image to be displayed for ratings.';
+$GLOBALS["hRatingImage2"] = 'Select the image to be displayed for ratings.';
+$GLOBALS["hVetComments"] = 'Do you want to vet comments before they are visible on the page?<br />Note that this function is not yet available.';
+$GLOBALS["hPrintFriendly"] = 'This is the default for the Printer-Friendly content option.';
+$GLOBALS["hTeaserWithDetails"] = 'Show the teaser text on detail pages as well as the actual content detail.';
+
+?>
