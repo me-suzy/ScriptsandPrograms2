2d378d770af8b1d491a9c64c5bc15a627844cb6d
me-suzy <me.suzana@gmail.com>
2025-08-12 18:01:52 +0300

    Add Image Galleries/phpFotoAlbum_2_00_alpha8/skin/default/colors.php

---
+++ Image Galleries/phpFotoAlbum_2_00_alpha8/skin/default/colors.php
@@ -0,0 +1,10 @@
+<?php
+$table_color[0]="#cccccc";
+$table_color[1]="#dddddd";
+$thumb_dir_color["r"]=204;
+$thumb_dir_color["g"]=204;
+$thumb_dir_color["b"]=204;
+$thumb_text_color["r"]=100;
+$thumb_text_color["g"]=10;
+$thumb_text_color["b"]=10;
+?>
