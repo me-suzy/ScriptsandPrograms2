7af23cb6b62d24fa366b10b47e103e4c8d4edd7e
me-suzy <me.suzana@gmail.com>
2025-08-12 14:00:43 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/template/common_menuing.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/template/common_menuing.php
@@ -0,0 +1,17 @@
+<div id="llinks">
+<div class="sidetitle">{menu_head}</div>
+  <div class="side">
+<a href="{baseurl}/index.php?SID={session}">{home}</a><br>
+<a href="{baseurl}/recoverpw.php?SID={session}">{lostpw}</a><br>
+<a href="{baseurl}/faq.php?SID={session}">{faq}</a><br>
+<a href="{baseurl}/signup.php?SID={session}">{signup}</a><br>
+<a href="{baseurl}/rules.php?SID={session}">{rules}</a><br>
+<a href="{baseurl}/cou.php?SID={session}">{cou}</a><br>
+  </div>
+
+<div class="sidetitle">{extras}</div>
+  <div class="side">
+<a href="top.php?SID={session}">{topbanns}</a><br>
+<a href="overall.php?SID={session}">{overallstats}</a><br>
+  </div>
+  </div>
