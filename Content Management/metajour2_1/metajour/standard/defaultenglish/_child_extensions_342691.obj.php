ead6a341c76760793967a14de2297b456bb972fa
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:39 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_extensions_342691.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_extensions_342691.obj.php
@@ -0,0 +1,34 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342691;
+$_objchilds[] = '_child_objfile_342692';
+$_objchilds[] = '_child_objfile_342693';
+$_objchilds[] = '_child_objfile_342694';
+$_objchilds[] = '_child_objfile_342695';
+$_objchilds[] = '_child_objfile_342696';
+$_objchilds[] = '_child_objfile_342697';
+$_objchilds[] = '_child_objfile_342698';
+$_objchilds[] = '_child_objfile_342699';
+$_objchilds[] = '_child_objfile_342700';
+$_objchilds[] = '_child_objfile_342701';
+$_objchilds[] = '_child_objfile_342702';
+function objfile_342691 () {
+$obj = owNew('structureelement');
+$objdata['name'] = "Extensions";
+$objdata['pageid'] = "0";
+$objdata['url'] = "";
+$objdata['binfile1'] = "0";
+$objdata['binfile2'] = "0";
+$objdata['binfile3'] = "0";
+$objdata['showtype'] = "0";
+$objdata['target'] = "";
+$objdata['image1'] = "";
+$objdata['image2'] = "";
+$objdata['image3'] = "";
+$objdata['templateid'] = "0";
+$objdata['structureid'] = "0";
+$obj->createObject($objdata);
+$obj->moveTo(46);
+return $obj;
+}
+?>
