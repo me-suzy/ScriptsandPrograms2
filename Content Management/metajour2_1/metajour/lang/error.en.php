2bebbfd70ae22096d0a82f64cc5c71e5127ff19d
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:50 +0300

    Add Content Management/metajour2_1/metajour/lang/error.en.php

---
+++ Content Management/metajour2_1/metajour/lang/error.en.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['error_unexpected'] = 'Unexpected error occured';
+$LANG['error_owread_notfound'] = 'Cannot locate the requested record';
+$LANG['error_ownew_classnotfound'] = 'Cannot locate the requested table';
+$LANG['error_php_error'] = 'A PHP error occured';
+$LANG['error_basic_recordnotfound'] = 'Cannot locate the requested record';
+$LANG['error_basic_noaccess'] = 'You are not authorized to access the requested record';
+
+$LANG['basic_duplicate'] = 'Duplicate names are not allowed';
+
+?>
