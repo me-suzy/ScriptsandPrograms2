3aebcdba49f581e8d96478ece55dc2331191130a
me-suzy <me.suzana@gmail.com>
2025-08-12 16:30:19 +0300

    Add Games and Entertainment/online-dice/config.php

---
+++ Games and Entertainment/online-dice/config.php
@@ -0,0 +1,10 @@
+<?php
+	// The dice style, choose from 1,2 or 3 (browse through styles directory to preview)
+	$dice_style = 3;
+	
+	// The number of dice to use
+	$dice_num = 5;
+	
+	// The absolute URL of your site, for instance, "www.mywebsite.com"
+	$dice_absolute_url = 'www.mysite.com';
+?>
