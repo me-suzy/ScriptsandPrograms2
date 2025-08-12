f44903298508676b694d7daa4f22b48696bbf925
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:21 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/affiliate_info.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/affiliate_info.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Partnerprogramm');
+define('HEADING_TITLE', 'Partnerprogramm');
+
+define('HEADING_AFFILIATE_PROGRAM_TITLE', 'Das ' . STORE_NAME . ' Partnerprogramm');
+define('TEXT_INFORMATION', 'F&uuml;gen Sie hier Ihre Informationen zum Partnerprogramm ein.');
+?>
