edb8365dce8d03c52116cbf91c8bac23ab223287
me-suzy <me.suzana@gmail.com>
2025-08-12 11:42:49 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/logoff.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/logoff.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id $
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Log Off');
+define('NAVBAR_TITLE', 'Log Off');
+define('TEXT_MAIN', 'You have been logged-off from <b>Admin</b> area. It is safe to leave the computer now. Click back to relogin');
+define('TEXT_RELOGIN', 're-login');
+?>
