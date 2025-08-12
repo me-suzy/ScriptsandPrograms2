7736ba57c8a8db8fd125de222fa77428cea2a7fa
me-suzy <me.suzana@gmail.com>
2025-08-12 18:20:19 +0300

    Add Image Galleries/singapore-0.9.11/singapore-0.9.11/secret.ini.php

---
+++ Image Galleries/singapore-0.9.11/singapore-0.9.11/secret.ini.php
@@ -0,0 +1,14 @@
+; singapore secret configuration file <http://singapore.sourceforge.net>
+;
+; <?php die("The contents of this file are hidden"); ?>
+;
+; these options are hidden from web users
+
+[SQL]
+;
+; settings relevant to all the SQL backends. 
+; You may ignore these if you are not using this backend.
+;
+
+sql_user = 
+sql_pass = 
