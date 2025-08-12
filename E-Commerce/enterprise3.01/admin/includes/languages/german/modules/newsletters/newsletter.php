03cdd822c57c4a4f547c53eeead5ebc75b7c3832
me-suzy <me.suzana@gmail.com>
2025-08-12 11:46:42 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/german/modules/newsletters/newsletter.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/german/modules/newsletters/newsletter.php
@@ -0,0 +1,13 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('TEXT_COUNT_CUSTOMERS', 'Anzahl der Kunden, welche das Rundschreiben erhalten: %s');
+?>
