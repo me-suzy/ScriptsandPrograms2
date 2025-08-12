1520563bc14844cd1087014b70e8a050aac75e86
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:21 +0300

    Add Content Management/metajour2_1/metajour/lang/letter.en.php

---
+++ Content Management/metajour2_1/metajour/lang/letter.en.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Letters';
+$LANG['label_contactid'] = 'Contact';
+$LANG['label_name'] = 'Name';
+$LANG['label_content'] = 'Content';
+?>
