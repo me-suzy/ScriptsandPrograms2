b7a30e94ace1d4c797b2e99956c35c40d446a0fe
me-suzy <me.suzana@gmail.com>
2025-08-12 22:36:49 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Downloads/language/lang-slovak.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Downloads/language/lang-slovak.php
@@ -0,0 +1,213 @@
+<?php
+
+/*************************************************************************/
+/* The Slovak language file for module Downloads                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/*************************************************************************/
+/* The Slovak language file for module Downloads                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+
+define("_URL","URL");
+define("_PREVIOUS","Predchádzajúca strana");
+define("_NEXT","Nasledujúca strana");
+define("_CATEGORY","Kategória");
+define("_CATEGORIES","Kategórie");
+define("_LVOTES","hlasovalo");
+define("_TOTALVOTES","Celkom hlasovalo:");
+define("_THEREARE","V naej databáze sa nachádza");
+define("_NOMATCHES","Nenaiel sa iadny výsledok");
+define("_SCOMMENTS","Komentáre");
+define("_UNKNOWN","Neidentifikované");
+define("_AUTHORNAME","Meno autora");
+define("_AUTHOREMAIL","email autora");
+define("_DOWNLOADNAME","Názov programu");
+define("_ADDTHISFILE","Prida tento súbor");
+define("_INBYTES","v bajtoch");
+define("_FILESIZE","Ve¾kos súboru");
+define("_VERSION","Verzia");
+define("_DESCRIPTION","Popis");
+define("_AUTHOR","Autor");
+define("_HOMEPAGE","Domovská stránka");
+define("_DOWNLOADNOW","Stiahnite si tento súbor!");
+define("_RATERESOURCE","Ohodnoti zdroj");
+define("_FILEURL","Adresa súboru");
+define("_ADDDOWNLOAD","Prida download");
+define("_DOWNLOADSMAIN","Hlavné kategórie downloadu");
+define("_DOWNLOADCOMMENTS","Komentáre k downloadom");
+define("_DOWNLOADSMAINCAT","Hlavné kategórie downloadu");
+define("_ADDADOWNLOAD","Prida nový download");
+define("_DSUBMITONCE","Vystavi unikátny download len raz.");
+define("_DPOSTPENDING","Vetky odoslané downloady budú preverené.");
+define("_RESSORTED","zoradené pod¾a");
+define("_DOWNLOADSNOTUSER1","Nie ste registrovaný uívate¾ alebo nie ste prihlásený.");
+define("_DOWNLOADSNOTUSER2","Ak sa zaregistrujete, môete pridáva downloady na tejto stránke.");
+define("_DOWNLOADSNOTUSER3","Sta sa registrovaným uívate¾om je krátky a ¾ahký proces.");
+define("_DOWNLOADSNOTUSER4","Preèo vyadujeme registráciu pre prístup k istým funkciám?");
+define("_DOWNLOADSNOTUSER5","Pretoe  chceme poskytnú len najkvalitnejie sluby,");
+define("_DOWNLOADSNOTUSER6","kadá poloka je individuálne skontrolovaná naim tímom.");
+define("_DOWNLOADSNOTUSER7","Dúfame, e Vám poskytneme len hodnotné informácie.");
+define("_DOWNLOADSNOTUSER8","<a href=\"modules.php?name=Your_Account\">Zaregisterujte sa</a>");
+define("_DOWNLOADALREADYEXT","CHYBA: Toto URL je u v databáze!");
+define("_DOWNLOADNOTITLE","CHYBA: Treba uvies názov súboru!");
+define("_DOWNLOADNOURL","CHYBA: Treba uvies URL adresu súboru!");
+define("_DOWNLOADNODESC","CHYBA: Treba uvies popis súboru na stiahnutie!");
+define("_DOWNLOADRECEIVED","Vïaka za poloku pre download.");
+define("_NEWDOWNLOADS","nových súborov");
+define("_TOTALNEWDOWNLOADS","nových súborov");
+define("_DTOTALFORLAST","nových súborov za posledných");
+define("_DBESTRATED","najlepie hodnotené súbory - Top");
+define("_TRATEDDOWNLOADS","spolu hodnotených súborov");
+define("_SORTDOWNLOADSBY","Trieïi pod¾a");
+define("_DCATNEWTODAY","Nové downloady v tejto kategórii pridané dnes");
+define("_DCATLAST3DAYS","Nové downloady v tejto kategórii pridané v posledných 3 dòoch");
+define("_DCATTHISWEEK","Nové downloady v tejto kategórii pridané za posledný týdeò");
+define("_DDATE1","Dátum (staré downloady najprv)");
+define("_DDATE2","Dátum (nové downloady najprv)");
+define("_DOWNLOADS","downloadov");
+define("_DOWNLOADPROFILE","Profil downloadov");
+define("_DOWNLOADRATINGDET","Detaily hodnotenia");
+define("_EDITTHISDOWNLOAD","Zmeni tento download");
+define("_DOWNLOADRATING","Hodnotenie");
+define("_DOWNLOADVOTE","Hlasova!");
+define("_DONLYREGUSERSMODIFY","Poadova zmenu informácií pre download môu len registrovaní uívatelia. <a href=\"modules.php?name=Your_Account\">Prihláste sa</a>.");
+define("_REQUESTDOWNLOADMOD","Vyiada zmenu downloadu");
+define("_DOWNLOADID","Download ID");
+define("_DLETSDECIDE","Pomôte ostatným uívate¾om rozhodnú sa na ktorý download je najlepie kliknú");
+define("_DRATENOTE4","Môete si pozrie <a href=\"modules.php?name=Downloads&d_op=TopRated\">najlepie hodnotené zdroje");
+define("_DATE","Dátum");
+define("_TO","pre");
+define("_NEW","Nové");
+define("_POPULAR","Ob¾úbené");
+define("_TOPRATED","Vysoko hodnotené");
+define("_ADDITIONALDET","Detaily");
+define("_EDITORREVIEW","Kontroloval");
+define("_REPORTBROKEN","Správa o nesprávnom odkaze");
+define("_AND","a");
+define("_INDB","v naej databáze");
+define("_INSTRUCTIONS","Intrukcie");
+define("_USERANDIP","Meno uívate¾a a IP adresa sú zaznamenané, prosím nezneuíva tento systém. Ak chcete prida kategóriu, resp. podkategóriu, zriadime.");
+define("_LDESCRIPTION","Popis: (max. 255 znakov)");
+define("_CHECKFORIT","Nezadali ste email. Vá odkaz aj tak preveríme èo najskôr.");
+define("_LASTWEEK","posledný týdeò");
+define("_LAST30DAYS","Posledných 30 dní");
+define("_1WEEK","1 týdeò");
+define("_2WEEKS","2 týdne");
+define("_30DAYS","30 dní");
+define("_SHOW","Ukáza");
+define("_DAYS","dní");
+define("_ADDEDON","pridané");
+define("_RATING","Hodnotenie");
+define("_DETAILS","Detaily");
+define("_OF","z");
+define("_TVOTESREQ","je minimum potrebných hlasov");
+define("_SHOWTOP","Zobrazi Top");
+define("_MOSTPOPULAR","Najob¾úbenejie odkazy - Top");
+define("_OFALL","zo vetkých");
+define("_POPULARITY","Ob¾úbené");
+define("_SELECTPAGE","Vybra stranu");
+define("_MAIN","Hlavné");
+define("_NEWTODAY","Dnes nové");
+define("_NEWLAST3DAYS","Nové za posledné 3 dni");
+define("_NEWTHISWEEK","Nové za tento týdeò");
+define("_POPULARITY1","Ob¾úbenos (od menej po viac kliknutí)");
+define("_POPULARITY2","Ob¾úbenos (od viac po menej kliknutí)");
+define("_TITLEAZ","Názov (od A po Z)");
+define("_TITLEZA","Názov (od Z po A)");
+define("_RATING1","Hodnotenie (od najniieho po najvyie skóre)");
+define("_RATING2","Hodnotenie (od najvyieho po najniie skóre)");
+define("_SEARCHRESULTS4","Výsledky vyh¾adávania pre heslo");
+define("_USUBCATEGORIES","Podkategórie");
+define("_TRY2SEARCH","Skúsi vyh¾ada");
+define("_INOTHERSENGINES","v iných vyh¾adávaèoch");
+define("_EDITORIAL","Úvodník");
+define("_EDITORIALBY","Úvodník napísal");
+define("_NOEDITORIAL","Pre tento web nie je dostupný iadny komentár.");
+define("_RATETHISSITE","Hodnoti túto webstránku");
+define("_ISTHISYOURSITE","Je to Vá výtvor?");
+define("_ALLOWTORATE","Umoni ostatným uívate¾om hodnoti priamo z ich webstránky!");
+define("_OVERALLRATING","Celkové hodnotenie");
+define("_TOTALOF","Celkom z");
+define("_USER","uívate¾");
+define("_USERAVGRATING","Priemerné uívate¾ské hodnotenie");
+define("_NUMRATINGS","# z hodnotených");
+define("_REGISTEREDUSERS","Registrovaní uívatelia");
+define("_NUMBEROFRATINGS","Poèet hodnotených");
+define("_NOREGUSERSVOTES","iadne hodnotenia registrovaných uívate¾ov.");
+define("_BREAKDOWNBYVAL","Chyba v hodnotení u");
+define("_LTOTALVOTES","hlasov celkom");
+define("_HIGHRATING","Vysoké hodnotenie");
+define("_LOWRATING","Nízke hodnotenie");
+define("_NUMOFCOMMENTS","Poèet komentárov");
+define("_WEIGHNOTE","Poznámka: Táto webstránka rozliuje registrovaných a neregistrovaných uívate¾ov.");
+define("_NOUNREGUSERSVOTES","iadne hlasy od neregistrovaných uívate¾ov");
+define("_WEIGHOUTNOTE","Poznámka: Táto webstránka rozliuje hodnotenie registrovaných a uívate¾ov z vonku.");
+define("_NOOUTSIDEVOTES","iadne hlasy z vonku");
+define("_OUTSIDEVOTERS","Hlasy z vonku");
+define("_UNREGISTEREDUSERS","Neregistrovaní uívatelia");
+define("_PROMOTEYOURSITE","Urobte reklamu Vaej stránke");
+define("_PROMOTE01","Mono Vás bude zaujíma nieko¾ko moností hodnotenia Vaej webstránky.");
+define("_TEXTLINK","Textový odkaz");
+define("_PROMOTE02","Jeden zo spôsobov ako sa dosta k hodnotiacemu formuláru je cez jednoduchý textový link:");
+define("_HTMLCODE1","HTML kód, ktorý v tomto prípade môete poui, je nasledujúci:");
+define("_THENUMBER","Èíslo");
+define("_IDREFER","v tomto HTML kóde znamená ID èíslo Vaej stránky v databáze $sitename . Uistite sa, e skutoène odkazuje na Vau stránku.");
+define("_BUTTONLINK","Tlaèidlo");
+define("_PROMOTE03","Ak chcete nieèo viac, ako základný textový link, môete poui malé downloadové tlaèidlo:");
+define("_RATEIT","Hodnoti túto stránku!");
+define("_HTMLCODE2","Zdrojový kód pre takéto tlaèidlo je:");
+define("_REMOTEFORM","Vzdialený formulár hodnotenia");
+define("_PROMOTE04","Ak podvádzate, vymaeme Vá download. Toto je aktuálne hodnotenie:");
+define("_VOTE4THISSITE","Hlasujte za túto stránku!");
+define("_HTMLCODE3","Tento formulár dovoluje uívate¾om hodnoti zadaný link priamo z Vaej stránky a hodnotenie sa zaznamená sem. Zdrojový kód formulára:");
+define("_PROMOTE05","Vïaka! A ve¾a astia v hodnotení!");
+define("_STAFF","tím");
+define("_THANKSBROKEN","Ïakujeme za pomoc pri starostlivosti o celistvos adresára.");
+define("_SECURITYBROKEN","Z dôvodu bezpeènosti bude Vae meno a IP adresa doèasne zaznamenaná.");
+define("_THANKSFORINFO","Vïaka za informáciu.");
+define("_LOOKTOREQUEST","Vau poiadavku vybavíme èo najskôr.");
+define("_SENDREQUEST","Odosla");
+define("_THANKSTOTAKETIME","Vïaka, e ste si nali èas na ohodnotenie stránky");
+define("_RETURNTO","Návrat na");
+define("_RATENOTE1","Prosím, nehlasujte za tú istú webstránku viac ako raz.");
+define("_RATENOTE2","Stupnica hodnotenia je 1 a 10, prièom 1 = úbohá a 10 = výborná úroveò.");
+define("_RATENOTE3","Prosím, buïe pri hlasovaní objektívni, ak kadý dostane 1 alebo 10, hodnotenie nebude ve¾mi pouite¾né.");
+define("_RATENOTE5","Nehlasujte za svoje vlastné príspevky.");
+define("_YOUAREREGGED","Ste registrovaný uívate¾ a ste prihlásený.");
+define("_FEELFREE2ADD","Neváhajte prida komentár k týmto webovým stránkam.");
+define("_YOUARENOTREGGED","Nie ste registrovaný uívate¾ alebo nie ste prihlásený.");
+define("_IFYOUWEREREG","Ako registrovaný uívate¾ môete prida vlastné komentáre k tejto stránke.");
+define("_TITLE","Názov");
+define("_MODIFY","Zmeni");
+define("_COMPLETEVOTE1","Ïakujeme za hlas.");
+define("_COMPLETEVOTE2","¡Pred $anonwaitdays dòami ste u hlasovali za tento zdroj.");
+define("_COMPLETEVOTE3","Hlasujte, prosím, iba raz.<br>Vetky hlasy sú zaznamenané a skontrolované.");
+define("_COMPLETEVOTE4","Nemôete hlasova za link, ktorý ste sami pridali.<br>Vetky hlasy sú zaznamenané a skontrolované.");
+define("_COMPLETEVOTE5","Nevybrali ste iadnu monos - hlas nebol uznaný");
+define("_COMPLETEVOTE6","Z jednej IP adresy môete hlasova iba raz za $outsidewaitdays dní.");
+define("_LINKSDATESTRING","%d. %b. %Y");
+
+?>
