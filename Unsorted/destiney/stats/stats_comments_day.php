882486bca127efdce5708908351ca3dfa4f9c267
me-suzy <me.suzana@gmail.com>
2025-08-13 02:28:39 +0300

    Add Unsorted/destiney/stats/stats_comments_day.php

---
+++ Unsorted/destiney/stats/stats_comments_day.php
@@ -0,0 +1,93 @@
+<?php
+include("../admin/config.php");
+include("$include_path/common.php");
+include("$include_path/$table_file");
+include("$include_path/doc_head.php");
+include("$include_path/styles.php");
+
+$final_output .= <<<FO
+</head>
+<body bgcolor="$page_bg_color">
+<table border="0" cellpadding="0" cellspacing="0" width="$total_width" align="center">
+<tr>
+	<td colspan="3" width="100%" valign="bottom">
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+	<tr>
+		<td valign="middle" class="dc">$title_image</td>
+		<td align="right" valign="bottom">
+FO;
+
+include("$include_path/logged_status.php");
+
+$final_output .= <<<FO
+		</td>
+	</tr>
+	</table>
+	</td>
+</tr>
+<tr>
+<td width="$left_col_width" valign="top">
+FO;
+
+include("$include_path/left.php");
+
+$final_output .= <<<FO
+</td>
+<td width="$main_col_width" valign="top">
+<table cellpadding="0" cellspacing="0" border="0" width="100%" align="center">
+FO;
+
+$final_output .= <<<FO
+<tr>
+<td align="left" valign="top">
+FO;
+
+$stats = <<<EOF
+<table cellpadding="2" cellspacing="0" border="0" width="100%">
+<tr>
+	<td class="bold">&nbsp;<a class="bold" href="$base_url/">$site_title</a> >> <a class="bold" href="$base_url/stats/">Site Stats</a> >> Comments Past Day</td>
+</tr>
+</table>
+<table cellpadding="5" cellspacing="0" border="0" width="100%">
+<tr>
+	<td class="regular"><img src="stats_im_comments_day.php" border="0" width="640" height="480" alt="Comments Past Day" title="Comments Past Day"></td>
+</tr>
+<tr>
+<td class="regular"></td>
+</tr>
+</table>
+EOF;
+
+$final_output .= table("Stats", $stats);
+
+$final_output .= <<<FO
+</td>
+</tr>
+FO;
+
+$final_output .= <<<FO
+</table>
+FO;
+
+include("$include_path/copyright.php");
+
+$final_output .= <<<FO
+</td>
+<td width="$right_col_width" valign="top">
+FO;
+
+include("$include_path/right.php");
+
+$final_output .= <<<FO
+</td>
+</tr>
+</table>
+</body>
+</html>
+FO;
+
+$final_output = final_output($final_output);
+
+echo $final_output;
+
+?>
