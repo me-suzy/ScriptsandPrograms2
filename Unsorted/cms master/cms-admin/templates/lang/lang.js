10785d090467941fea5f6cc09c823d75aa585e0d
me-suzy <me.suzana@gmail.com>
2025-08-13 01:48:03 +0300

    Add Unsorted/cms master/cms-admin/templates/lang/lang.js

---
+++ Unsorted/cms master/cms-admin/templates/lang/lang.js
@@ -0,0 +1,47 @@
+HTMLArea.I18N = {
+
+	lang: "en",
+
+	tooltips: {
+		bold:           "Bold",
+		italic:         "Italic",
+		underline:      "Underline",
+		strikethrough:  "Strikethrough",
+		subscript:      "Subscript",
+		superscript:    "Superscript",
+		justifyleft:    "Align left",
+		justifycenter:  "Center",
+		justifyright:   "Align right",
+		justifyfull:    "Justify",
+		orderedlist:    "Numbering",
+		unorderedlist:  "Bullets",
+		outdent:        "Decrease indent",
+		indent:         "Increase indent",
+		forecolor:      "Text color",
+		hilitecolor:    "Background color",
+		horizontalrule: "Horizontal line",
+		createlink:     "Insert link",
+		insertimage:    "Insert image",
+		inserttable:    "Insert table",
+		htmlmode:       "Switch to HTML",
+		popupeditor:    "Enlarge",
+		about:          "About",
+		showhelp:       "Help",
+		textindicator:  "Current style",
+		undo:           "Undo",
+		redo:           "Redo",
+		cut:            "Cut",
+		copy:           "Copy",
+		paste:          "Paste"
+	},
+
+	buttons: {
+		"ok":           "OK",
+		"cancel":       "Cancel"
+	},
+
+	msg: {
+		"Path":         "Path",
+		"TEXT_MODE":    "You are in TEXT MODE.  Use the [<>] button to switch back to WYSIWIG."
+	}
+};
