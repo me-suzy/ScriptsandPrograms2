6f9a469dec2fa47d3f823fede2de7141d056122e
me-suzy <me.suzana@gmail.com>
2025-08-12 23:17:16 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_forum/lang/lang-mod_forum-lang-french.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_forum/lang/lang-mod_forum-lang-french.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE FORUM
+$Mod_Forum_Ajouter="Ajouter un message";
+$Mod_Forum_Search="Rechercher";
+$Mod_Forum_Auteurs="Auteurs";
+$Mod_Forum_Post="Posté le";
+$Mod_Forum_Rep = "R&eacute;ponse" ;
+$Mod_Forum_Rep1 = " et les r&eacute;ponses apport&eacute;es" ;
+$Mod_Forum_Rep2 = " et la r&eacute;ponse apport&eacute;e" ;
+$Mod_Forum_ResultatRecherche = "R&eacute;sultat de la recherche" ;
+$Mod_Forum_valider= "Valider" ;
+$Mod_Forum_DeleteOK = "Message supprim&eacute; avec succ&egrave;s." ;
+$Mod_Forum_DeleteNOK = "Impossible de supprimer le message. Les commentaires associ&eacute;s n'ont pas &eacute;t&eacute; supprim&eacute;s." ;
+$Mod_Forum_Query = "Êtes-vous sur de vouloir supprimer le message et ses commentaires associés ou le message ?" ;
+$Mod_Forum_Delete_com_OK = "R&eacute;ponses associ&eacute;es au message supprim&eacute;es avec succ&egrave;s." ;
+$Mod_Forum_Delete_com_NOK = "Impossible de supprimer les r&eacute;ponses associ&eacute;es au message ayant pour reponse_a_id=" ;
+?>
