f0ab60c79f548dfba06fd77d4ee88f8d76838e7e
me-suzy <me.suzana@gmail.com>
2025-08-12 10:33:48 +0300

    Add Discussion Boards/dot_premodded_1.0.7_162/dot_premodded_1.0.7/language/lang_english/lang_admin_rebuild_search.php

---
+++ Discussion Boards/dot_premodded_1.0.7_162/dot_premodded_1.0.7/language/lang_english/lang_admin_rebuild_search.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['Rebuild_search'] = 'Reindexeer Zoeken';
+$lang['Rebuild_search_desc'] = 'Door deze functie zal ieder bericht op je .premodded/phpBB geindexeerd worden, door de zoektabellen te herbouwen. Dit proces kan lang in beslag nemen, ga in de tussentijd NIET weg van/uit deze pagina, totdat dit proces voltooid is.';
+$lang['Post_limit'] = 'Bericht limiet';
+$lang['Time_limit'] = 'Tijd limiet';
+$lang['Refresh_rate'] = 'Vernieuw mate';
+
+$lang['Next'] = 'Volgende';
+$lang['Finished'] = 'Voltooien';
+
+?>
