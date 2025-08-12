5a2740c251a3ec67a57ba76463237147a16c9f92
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:53 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_createdby.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_createdby.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Ejer';
+$LANG['text_1'] = 'Angiv ejer (opretter) for de markerede poster';
+$LANG['text_2'] = 'Oprettet af: ';
+?>
