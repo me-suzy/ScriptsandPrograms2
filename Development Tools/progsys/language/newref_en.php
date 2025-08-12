41ed786fc86d8306ce65d2b2c8e97add36622e81
me-suzy <me.suzana@gmail.com>
2025-08-12 10:12:42 +0300

    Add Development Tools/progsys/language/newref_en.php

---
+++ Development Tools/progsys/language/newref_en.php
@@ -0,0 +1,18 @@
+<?php
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+/***************************************************************************
+ * Created by: Boesch IT-Consulting (info@boesch-it.de)
+ * (c)2002-2005 Boesch IT-Consulting
+ * *************************************************************************/
+$l_newrefmail = "A reference site for '$progname' has been added\r\n
+Sitename: $sitename\r\nURL: ".$prot."://".$input_url."\r\nEmail: $contactmail\r\ncontact: $contactname\r\n
+seen on: $heardfrom\r\nnote: $note\r\n\r\n";
+$l_newrefsubject = "new reference site";
+?>
