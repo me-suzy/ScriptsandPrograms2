cda3b9c22b8adea3f86f3a3655be7eadf45573dd
me-suzy <me.suzana@gmail.com>
2025-08-12 09:23:20 +0300

    Add Development Tools/fuselogic/fuselogic/modules/form1/form.register_lib.php

---
+++ Development Tools/fuselogic/fuselogic/modules/form1/form.register_lib.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once(getModulePath('form').'/class.validator.php');
+require_once('class.validator_email.php');
+require_once('class.validator_user.php');
+require_once('class.validator_password.php');
+
+
+?>
