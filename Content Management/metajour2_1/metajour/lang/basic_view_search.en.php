4d09e9b490ac1a8c177aeac7fd3ae073cb0ff184
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:38 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_search.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_search.en.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['sttext_1'] = 'Search criteria #1';
+$LANG['sttext_2'] = 'Search criteria #2';
+$LANG['sttext_3'] = 'Search criteria #3';
+$LANG['sttext_4'] = 'Search criteria #4';
+$LANG['sttext_5'] = 'Search criteria #5';
+$LANG['sttext_6'] = 'Show columns';
+$LANG['sttext_7'] = 'Created from';
+$LANG['sttext_8'] = 'Created to';
+?>
