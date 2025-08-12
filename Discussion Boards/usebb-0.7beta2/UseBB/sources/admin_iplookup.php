e524eeb8ff0465c5ff932b30809b739ffc45b0cc
me-suzy <me.suzana@gmail.com>
2025-08-12 10:55:35 +0300

    Add Discussion Boards/usebb-0.7beta2/UseBB/sources/admin_iplookup.php

---
+++ Discussion Boards/usebb-0.7beta2/UseBB/sources/admin_iplookup.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+	Copyright (C) 2003-2005 UseBB Team
+	http://www.usebb.net
+	
+	$Header: /cvsroot/usebb/UseBB/sources/admin_iplookup.php,v 1.9 2005/09/15 15:46:45 pc_freak Exp $
+	
+	This file is part of UseBB.
+	
+	UseBB is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	UseBB is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with UseBB; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+/**
+ * ACP IP address lookup
+ *
+ * Gives an interface to do IP address to hostname lookups.
+ *
+ * @author	UseBB Team
+ * @link	http://www.usebb.net
+ * @license	GPL-2
+ * @version	$Revision: 1.9 $
+ * @copyright	Copyright (C) 2003-2005 UseBB Team
+ * @package	UseBB
+ * @subpackage	ACP
+ */
+
+//
+// Die when called directly in browser
+//
+if ( !defined('INCLUDED') )
+	exit();
+
+$content = '<p>'.$lang['IPLookupInfo'].'</p>';
+
+$content .= '<form action="'.$functions->make_url('admin.php', array('act' => 'iplookup')).'" method="post"><p>'.$lang['IPAddress'].': <input type="text" name="ip" id="ip" size="15" maxlength="15" /> <input type="submit" value="'.$lang['Search'].'" /></p></form>';
+
+if ( !empty($_REQUEST['ip']) && preg_match('#^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$#', $_REQUEST['ip']) ) {
+	
+	$hostname = @gethostbyaddr($_REQUEST['ip']);
+	
+	if ( !empty($hostname) && $_REQUEST['ip'] != $hostname )
+		$content .= '<p>'.sprintf($lang['IPLookupResult'], '<em>'.$_REQUEST['ip'].'</em>', '<em>'.$hostname.'</em>').'</p>';
+	else
+		$content .= '<p>'.sprintf($lang['IPLookupNotFound'], '<em>'.$_REQUEST['ip'].'</em>').'</p>';
+	
+}
+
+$admin_functions->create_body('iplookup', $content);
+$template->set_js_onload("set_focus('ip')");
+
+?>
