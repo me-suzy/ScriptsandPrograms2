0f91e05f8752c20537b6268bda5d51be09b7dbeb
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:17 +0300

    Add Content Management/metajour2_1/metajour/lang/stfilebinfile.da.php

---
+++ Content Management/metajour2_1/metajour/lang/stfilebinfile.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Statiske filer (gammel)';
+$LANG['label_name'] = 'Navn';
+$LANG['label_description'] = 'Beskrivelse';
+?>
