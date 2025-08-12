9dca47b44019328850430e13dce5875b6f16962e
me-suzy <me.suzana@gmail.com>
2025-08-12 18:28:00 +0300

    Add Image Galleries/TinyWebGallery13c/TinyWebGallery13/inc/fixfont.inc.php

---
+++ Image Galleries/TinyWebGallery13c/TinyWebGallery13/inc/fixfont.inc.php
@@ -0,0 +1,29 @@
+<?php
+/*************************
+  Copyright (c) 2004-2005 TinyWebGallery
+  written by Michael Dempfle
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  TWG version: 1.3c
+  $Date: 2005/11/15 09:02 $
+**********************************************/
+$lang_height_comment = ceil($fontscale * $lang_height_comment);
+$lang_height_caption = ceil($fontscale * $lang_height_caption);
+$lang_height_login = ceil($fontscale * $lang_height_login);
+$lang_height_private = ceil($fontscale * $lang_height_private);
+$lang_height_options = ceil($fontscale * $lang_height_options);
+$lang_height_email_user = ceil($fontscale * $lang_height_email_user);
+$lang_height_email_admin = ceil($fontscale * $lang_height_email_admin);
+$lang_height_info = ceil($fontscale * $lang_height_info);
+$lang_height_rating = ceil($fontscale * $lang_height_rating);
+$lang_height_dl_manager = ceil($fontscale * $lang_height_dl_manager);
+$lang_height_search = ceil($fontscale * $lang_height_search);
+if ($fontscale > 1) {
+  $detailswidth = ceil($fontscale* 1.1 * $detailswidth);
+}
+
+?>
