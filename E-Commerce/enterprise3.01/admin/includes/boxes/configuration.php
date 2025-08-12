2e2b0bb1f3c53c7bc0c2b71673dee2a6ec520bf6
me-suzy <me.suzana@gmail.com>
2025-08-12 11:38:42 +0300

    Add E-Commerce/enterprise3.01/admin/includes/boxes/configuration.php

---
+++ E-Commerce/enterprise3.01/admin/includes/boxes/configuration.php
@@ -0,0 +1,37 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+?>
+<!-- configuration //-->
+          <tr>
+            <td>
+<?php
+  $heading = array();
+  $contents = array();
+
+  $heading[] = array('text'  => BOX_HEADING_CONFIGURATION,
+                     'link'  => escs_href_link(FILENAME_CONFIGURATION, 'gID=1&selected_box=configuration'));
+
+  if ($selected_box == 'configuration') {
+    $cfg_groups = '';
+    $configuration_groups_query = escs_db_query("select configuration_group_id as cgID, configuration_group_title as cgTitle from " . TABLE_CONFIGURATION_GROUP . " where visible = '1' order by sort_order");
+    while ($configuration_groups = escs_db_fetch_array($configuration_groups_query)) {
+      $cfg_groups .= '<a href="' . escs_href_link(FILENAME_CONFIGURATION, 'gID=' . $configuration_groups['cgID'], 'NONSSL') . '" class="menuBoxContentLink">' . $configuration_groups['cgTitle'] . '</a><br>';
+    }
+
+    $contents[] = array('text'  => $cfg_groups);
+  }
+
+  $box = new box;
+  echo $box->menuBox($heading, $contents);
+?>
+            </td>
+          </tr>
+<!-- configuration_eof //-->
