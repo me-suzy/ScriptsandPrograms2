77252797e26b0b16f4dc23a90cc55735fa1b38af
me-suzy <me.suzana@gmail.com>
2025-08-12 13:49:01 +0300

    Add Email Systems/DSNewsletter/12_.php

---
+++ Email Systems/DSNewsletter/12_.php
@@ -0,0 +1,30 @@
+<?
+include("header.php");
+?>
+<?php
+
+include("config.php");
+$curr = "12_";
+echo "<font size=6><u>$name Isssue Code $curr</u></font><br><br>";
+// $files = array();
+$dir = "perm";
+$dh = opendir($dir);
+
+while (false !== ($filename = readdir($dh))) {
+
+    if ($filename == '.' || $filename == '..') {
+        continue; // skip these
+    }
+
+    if (0 === strpos($filename, $curr)) { // if the prefix exists at the very beggining of the filename
+        $files = "$dir/$filename";
+		$str = file_get_contents("$files");
+		echo "$str <br><br>";
+    }
+}
+
+ echo "";
+ ?>
+ <?
+include("footer.php");
+?>
