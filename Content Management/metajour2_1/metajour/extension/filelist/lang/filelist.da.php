9b852f14da06ebc0762e2d455de4dc2809a4916a
me-suzy <me.suzana@gmail.com>
2025-08-12 06:13:04 +0300

    Add Content Management/metajour2_1/metajour/extension/filelist/lang/filelist.da.php

---
+++ Content Management/metajour2_1/metajour/extension/filelist/lang/filelist.da.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['label_name'] = 'Konfigurationsnavn';
+$LANG['label_title'] = 'Titel';
+$LANG['label_folderid'] = 'Udgivet mappe';
+$LANG['label_thumbnail'] = 'Vis miniaturer';
+$LANG['label_templateid'] = 'Skabelon';
+$LANG['name'] = 'Konfiguration af Filudgivelse';
+?>
