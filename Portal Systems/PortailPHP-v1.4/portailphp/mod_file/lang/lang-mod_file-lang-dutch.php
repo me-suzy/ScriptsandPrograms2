89005d15c9eac29816a2cbe66d6562eaa837e081
me-suzy <me.suzana@gmail.com>
2025-08-12 23:16:41 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_file/lang/lang-mod_file-lang-dutch.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_file/lang/lang-mod_file-lang-dutch.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE FILE
+$Mod_File_Taille="2000000";// maximum bestandsgrootte om te uploaden
+$Mod_File_Etiquette="Bestand om te uploaden";
+$Mod_File_Etiquette2="Rubriek";
+$Mod_File_Etiquette3="Titel";
+$Mod_File_Etiquette4="Naam";
+$Mod_File_Etiquette5="E-mail";
+$Mod_File_inde_Date="Datum";
+$Mod_File_inde_Cat="Rubriek";
+$Mod_File_inde_Titre="Naam";
+$Mod_File_inde_Fichiers="Bestanden";
+$Mod_File_inde_Clics="Treffers";
+$Mod_File_PostPar="Gepost door";
+$Mod_File_Le="op";
+$Mod_File_Telecharger="Downloaden";
+$Mod_File_PlRecent="10 meest recente downloads";
+$Mod_File_PlLu="Top 10 downloads";
+$Mod_File_JS_Nom="Het veld Naam moet ingevuld worden";
+$Mod_File_JS_Email="Het veld E-mail moet ingevuld worden";
+$Mod_File_JS_Monfichier="Het veld Bestanden moet ingevuld worden";
+$Mod_File_JS_Titre="Het veld Titel moet ingevuld worden";
+$Mod_File_Upload_OK="Uw bestand werd correct verstuurd";
+
+?>
