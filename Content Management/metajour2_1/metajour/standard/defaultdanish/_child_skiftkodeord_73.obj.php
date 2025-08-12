baed8d36dc751dd806a1cf20b659c54f48382891
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:58 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_skiftkodeord_73.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_skiftkodeord_73.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 73;
+function objfile_73 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Skift kodeord";
+$objdata['subname'] = "";
+$objdata['extension'] = "changepassword";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
