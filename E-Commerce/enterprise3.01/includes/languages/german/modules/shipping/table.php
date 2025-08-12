47cf3ea46834a855bef4d7018576652425b3f127
me-suzy <me.suzana@gmail.com>
2025-08-12 12:01:42 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/modules/shipping/table.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/modules/shipping/table.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_TABLE_TEXT_TITLE', 'Tabellarische Versandkosten');
+define('MODULE_SHIPPING_TABLE_TEXT_DESCRIPTION', 'Tabellarische Versandkosten');
+define('MODULE_SHIPPING_TABLE_TEXT_WAY', '');
+define('MODULE_SHIPPING_TABLE_TEXT_WEIGHT', 'Gewicht');
+define('MODULE_SHIPPING_TABLE_TEXT_AMOUNT', 'Menge');
+?>
