4c89075e010701df32c6d5373ba41a8ce482c970
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:19 +0300

    Add Content Management/metajour2_1/metajour/lang/template.en.php

---
+++ Content Management/metajour2_1/metajour/lang/template.en.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Templates';
+$LANG['label_name'] = 'Name';
+$LANG['label_templatefile'] = 'File';
+$LANG['label_content'] = 'Content';
+$LANG['label_param'] = 'Parameters';
+$LANG['label_setting'] = 'Parameter values';
+$LANG['label_tpltype'] = 'Template type';
+$LANG['option_tpltype']['0'] = 'Page template';
+$LANG['option_tpltype']['1'] = 'Partial template';
+$LANG['option_tpltype']['2'] = 'Extension template';
+$LANG['label_header'] = 'Header';
+$LANG['label_style'] = 'Stylesheet CSS';
+$LANG['label_doctype'] = 'Doctype';
+$LANG['label_config'] = 'Configuration';
+$LANG['label_htmledit'] = 'HTML editing';
+?>
