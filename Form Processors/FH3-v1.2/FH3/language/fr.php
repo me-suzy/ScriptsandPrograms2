ae6d581d35d9ed5fb4d82011f206bc057677ab34
me-suzy <me.suzana@gmail.com>
2025-08-12 16:10:16 +0300

    Add Form Processors/FH3-v1.2/FH3/language/fr.php

---
+++ Form Processors/FH3-v1.2/FH3/language/fr.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Frence language file
+ *
+ * @package FormHandler
+ * @author ??
+ */
+
+$fh_lang = array(
+  1  => 'Janvier',
+  2  => 'Février',
+  3  => 'Mars',
+  4  => 'Avril',
+  5  => 'Mai',
+  6  => 'Juin',
+  7  => 'Juillet',
+  8  => 'Août',
+  9  => 'Septembre',
+  10 => 'Octobre',
+  11 => 'Novembre',
+  12 => 'Décembre',
+  13 => 'La date indiquée n\'est pas valable!',
+  14 => 'La valeur donnée est non-valable!',
+  15 => 'Les mots de garde ne sont pas la même chose!',
+  16 => 'Ces terrains sont obligatoires!',
+  17 => 'Le mot de garde doit être au moins %d signes long!',
+  18 => 'Le mot de garde comprend les signes non-valables!',
+  19 => 'Ce terrain remplit uniquement si vous voulez transférer la valeur existante (<a href="%path%%filename%" target="_blank">%filename%</a>)<br />',
+  20 => 'Le suivant extensive a uniquement été autorisé: %s.',
+  21 => 'Le geuploade fichier existe déjà!',
+  22 => 'Ce terrain est obligatoire!',
+  23 => 'La hauteur de fichier maximale de %s kb a été dépassée!',
+  24 => 'Il y a quelque chose erreur est allée, essaye de nouveau!',
+  25 => '<small>Ces terrains remplissent uniquement si vous voulez modifier votre vieux mot de garde</small>',
+  26 => 'Submit',
+  27 => 'Reset',
+  28 => 'Annulent',
+  29 => 'Disponible',
+  30 => 'Sélectionnés',
+  31 => 'Le geuploade fichier a un type de fichier non-valable!',
+  32 => 'Le format de la plaquette peut les dimensions %d x %d avoir maximalement ! Le geuploade fichier a les dimensions %d x %d.',
+  33 => 'Parce que un terrain est non- bien rempli, vous devez de nouveau sélectionner le fichier <b>%filename%</b> pour envoyer le!<br />',
+  34 => 'Sélectionner un item afin de déplacer vers le %s le terrain ou doublement le clic pour déplacer tous items',
+  35 => 'Ce champ doit être unique. La valeur "%s" allready existe dans la base de données!',
+  36 => '<b>%d</b> characters remaining on your input limit',
+  37 => 'You have to supply a file with an extension!',
+  38 => 'Back',
+  39 => 'The combination of the fields "%s" has to be unique. This combination already exists in the database! (Violation of index "%s")'
+);
+
+?>
