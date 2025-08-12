b12ad7e5f3e9cb300f96785b9d431ecd667118ba
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:01 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_sitesettings.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_sitesettings.php
@@ -0,0 +1,94 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] 		= 'Beheer website instellingen';
+
+//  Form Block Titles
+$GLOBALS["thTitles"] 		= 'Website titel';
+$GLOBALS["thSecurity"] 		= 'Beveiliging';
+$GLOBALS["thTopFrame"] 		= 'Kop frame instellingen';
+$GLOBALS["thUserdataFrame"] 	= 'Gebruikersdata frame voorkeuren';
+$GLOBALS["thMenus"] 		= 'Menu instelllingen';
+$GLOBALS["thContents"] 		= 'Inhoud instellingen';
+$GLOBALS["thBanners"] 		= 'Reclameblok instellingen';
+$GLOBALS["thFooter"] 		= 'Voet instellingen';
+
+//  Form Field Headings
+$GLOBALS["tSiteTitle"] 		= 'Website titel';
+$GLOBALS["tSiteDescr"] 		= 'Website omschrijving';
+$GLOBALS["tSiteKeywords"] 	= 'Website trefwoorden';
+$GLOBALS["tSiteWidth"] 		= 'Website breedte';
+$GLOBALS["tSiteHeight"] 	= 'Website hoogte';
+$GLOBALS["tSectionSecurity"] 	= 'Onderdeel-level beveiliging';
+$GLOBALS["tTopFrameHeight"] 	= 'Kop frame hoogte';
+$GLOBALS["tTopHTML"] 		= 'HTML voor kopframe';
+$GLOBALS["tTopMenu"] 		= 'Topmenu in de kop';
+$GLOBALS["tTopMenuHeight"] 	= 'Topmenu frame hoogte';
+$GLOBALS["tUserdata"] 		= 'Tonen gebruikersdata frame';
+$GLOBALS["tUserdataWidth"] 	= 'Gebruikersdata frame breedte';
+$GLOBALS["tMenuWidth"] 		= 'Menu frame breedte';
+$GLOBALS["tMenuFrameAlign"] 	= 'Uitlijning menu frame';
+$GLOBALS["tMenuExpand"] 	= 'Uitvouwen van menu\'s als u erop klikt';
+$GLOBALS["tMenuCollapse"] 	= 'Opvouwen van menu\'s als u erop klikt';
+$GLOBALS["tMenuHover"] 		= 'Wilt u een zweef tekst bij uw knoppen';
+$GLOBALS["tLRContent"] 		= 'Zowel linker als rechterkolom';
+$GLOBALS["tRightWidth"] 	= 'Rechter kolom breedte';
+$GLOBALS["tColBreak"] 		= 'Afbeelding voor kolomscheiding rechter- en linkerkolom';
+$GLOBALS["tBreadcrumb"] 	= 'Tonen van kruimelpad';
+$GLOBALS["tBreadcrumbSeparator"] = 'Scheidingsteken kruimelpad';
+$GLOBALS["tBookmark"] 		= 'Tonen bookmarks';
+$GLOBALS["tBanners"] 		= 'Tonen reclameblokken';
+$GLOBALS["tFooter"] 		= 'Opnemen voetframe';
+$GLOBALS["tFooterHeight"] 	= 'Voetframe hoogte';
+$GLOBALS["tFooterText"] 	= 'Voettekst';
+
+//  Form Field Options
+$GLOBALS["tHeaderPanel"] = 'In kopframe';
+$GLOBALS["tMenuPanel"] = 'In menuframe';
+$GLOBALS["tNoUserdata"] = 'Geen frame';
+
+$GLOBALS["tNoBanners"] = 'Laat geen Reclameblokken zien';
+$GLOBALS["tBannersT"] = 'Reclameblokken in de kop';
+$GLOBALS["tBannersB"] = 'Reclameblokken in de voet';
+$GLOBALS["tBannersTB"] = 'Reclameblokken zowel in de kop als in de voet';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'Met dit formulier kunt de de algemene instellingen van uw website beheren.';
+$GLOBALS["hSiteTitle"] = 'De titel van de website zoals die in de titel balkt getoond wordt.<br />';
+$GLOBALS["hSiteDescr"] = 'Specifies the HTML meta tag of type description.<br />';
+$GLOBALS["hSiteDescr"] = 'Useful for indexing in searching engines.';
+$GLOBALS["hSiteKeywords"] = 'Dit zijn de trefwoorden van de site; deze worden opgenomen in de HTML meta tag van het type keywords.<br />Deze trefwoorden worden gebruikt door de zoekmachines om uw website te indexeren; voer hier uw trefwoorden in gescheiden door komma\'s.';
+$GLOBALS["hSiteWidth"] = 'Als u wilt dat de website geopend wordt in een gecentreerd venster in de browser, dan geeft deze instelling de breedte aan van het venster.';
+$GLOBALS["hSiteHeight"] = 'Als u wilt dat de website geopend wordt in een gecentreerd venster in de browser, dan geeft deze instelling de hoogte aan van het venster.';
+$GLOBALS["hSectionSecurity"] = 'Indien de onderdeel-level beveiling op \'Ja\' wordt gezet, dan kunt u auteurs en anderen die bijdragen leveren aan de site beperken in hun toegang tot uw site en alleen toegang geven tot specifieke delen van de site.';
+$GLOBALS["hTopFrameHeight"] = 'Dit stelt de hoogte in van het kopframe.';
+$GLOBALS["hTopHTML"] = 'Hier kunt u additonele HTML code invoeren voor het topframe.';
+$GLOBALS["hTopMenu"] = 'Dit geeft aan of een een topmenu in de kop getoond wordt.<br />Wanneer u deze eigenschap aanzet moet er ook een waarde ingegeven worden voor Topmenu frame hoogte.<br /><br />Indien u deze instelling veranderd en ook bewaard hebt, is het aan te raden om het scherm van het admin menu te verversen. Op deze manier worden ook de beschikbare menu opties van het admin menu aangepast.';
+$GLOBALS["hTopMenuHeight"] = 'Dit stelt de hoogte in van de topmenu balk (indien er een topmenu getoond wordt).<br />Deze hoogte komt bij de hoogte die is ingesteld voor het topmenu frame.';
+$GLOBALS["hUserdata"] = 'Dit geeft aan of het Gebruikersdata frame getoond moet worden; in dit frame staan o.a. login gegevens van de bezoeker. Dit Gebruikersdata frame kan weergegeven worden in het kop frame of in het menu fram.<br />Indien het Gebruikersdata frame getoond wordt, moet ook de breedte van dit frame worden aangegeven.';
+$GLOBALS["hUserdataWidth"] = 'Geeft de breedte aan van het Gebruikersdata fram; alleen van belang indien u aangeeft dat het Gebruikersdata frame getoond moet worden.';
+$GLOBALS["hMenuWidth"] = 'Dit stelt de breedte in van het menu frame.';
+$GLOBALS["hMenuFrameAlign"] = 'Deze instelling geeft hoe het menu frame uitgelijnd wordt: het menu frame kan aan de linkerzijde of aan de rechterzijde van het venster getoond worden.<br /> De optie \'Automatisch\' toont het menu aan de linkerzijde indien de gekozen taal voor de website van links-naar-rechts getoond wordt; en het geeft het menu frame aan de linkerzijde weer wanneer de taal van de website is van rechts-naar-links.';
+$GLOBALS["hMenuExpand"] = 'Indien u ervoor kiest om de menu\'s te kunnen uitvouwen, dan wordt alleen één menu uitgevouwen met de bijbehorende submenu\'s.<br />Als deze instelling op \'Nee\' is ingesteld zullen <B><I>alle</I></B> menu\'s uitgevouwen getoond worden.';
+$GLOBALS["hMenuCollapse"] = 'Indien u kiest voor het opvouwen van menu\'s, dan wordt een uitgevouwen menu weer ingevouwen als u erop klikt.<br /> N.B.: Het uitvouwen van menu\'s dient op Ja te staan om van deze instelling gebruik te kunnen maken. Ook wordt er slechts één menu tegelijkertijd uitgevouwen.';
+$GLOBALS["hMenuHover"] = 'Deze instelling geeft aan of er een zweeftekst getoond wordt als de muis over een menu link gaat. De zweeftekst zelf kunt u bij de betreffende menu link ingeven.';
+$GLOBALS["hLRContent"] = 'Dit bepaalt of de inhoudspagina\'s gesplitst worden in een linker- en een rechterkolom. Als er een linker- en rechterkolom is, kan van elk artikel worden aangegeven of het in de linkerkolom of de rechterkolom getoond zal worden<br />Indien ook rechterkolommen getoond worden, moet u ook een waarde opgeven voor de breedte van de kolommen.';
+$GLOBALS["hRightWidth"] = 'Dit is de breedte van de rechterkolom\; is alleen van belang als ook de rechterkolom getoond wordt.';
+$GLOBALS["hColBreak"] = 'Als zowel linker- als rechterkolommen getoond worden, kunt u hier een afbeelding opgeven, die als afscheiding getoond wordt tussen de linker- en de rechterkolom.';
+$GLOBALS["hBreadcrumb"] = 'Wanneer u het \'kruimelpad\' aanzet, wordt boven in het inhoudsscherm een link getoond die het menu en submenu\'s toont waar u op moment bent.';
+$GLOBALS["hBreadcrumbSeparator"] = 'Als het \'kruimelpad\' wordt getoond, zijn dit de scheidingstekens tussen de verschillende niveaus van de menu\'s en submenu\'s.';
+$GLOBALS["hBookmark"] = 'Bepaalt of het mogelijk is om de pagina te \'bookmarken\' (d.w.z. toe te voegen aan de Favorieten lijst in Internet Explorer.';
+$GLOBALS["hBanners"] = 'Dit geeft aan of u reclameblokken wilt weergeven op uw website.<br />Ook kunt u aangeven of de reclameblokken getoond worden in de kop en/of in de voet van de pagina.';
+$GLOBALS["hFooter"] = 'Vul \'Ja\' als een voet frame wilt toevoegen onderaan een pagina. Indien u dit doet, dient u ook de hoogte aan te geven van het voet frame. Tevens kunt u een tekst opgeven die in het voet frame verschijnt.';
+$GLOBALS["hFooterHeight"] = 'Dit geeft de hoogte aan van het voetframe; alleen relevent indien het voetframe getoond wordt.';
+$GLOBALS["hFooterText"] = 'Hier kunt u de inhoud opstellen voor het voetframe; dit kan tekts zijn, maar ook HTML.';
+
+//  Error Messages
+$GLOBALS["eTopFrame"] = 'De hoogte van het top frame is geen getal.';
+$GLOBALS["eTopmenuFrame"] = 'De hoogte van het topmenu frame is geen getal.';
+$GLOBALS["eUserdataFrame"] = 'De breedte van het gebruikersdata frame is geen getal.';
+$GLOBALS["eMenuFrame"] = 'De breedte van het menu frame is geen getal.';
+$GLOBALS["eRightFrame"] = 'De breedte van de rechterkolom is geen getal.';
+$GLOBALS["eBottomFrame"] = 'De hoogte van het voet frame is geen getal.';
+
+?>
