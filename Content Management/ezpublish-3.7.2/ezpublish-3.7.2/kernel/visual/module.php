f52f5fd40ec6516040ccd2835eee6205e46858e7
me-suzy <me.suzana@gmail.com>
2025-08-12 03:12:00 +0300

    Add Content Management/ezpublish-3.7.2/ezpublish-3.7.2/kernel/visual/module.php

---
+++ Content Management/ezpublish-3.7.2/ezpublish-3.7.2/kernel/visual/module.php
@@ -0,0 +1,101 @@
+<?php
+//
+// Created on: <17-Apr-2002 11:05:08 amos>
+//
+// Copyright (C) 1999-2005 eZ systems as. All rights reserved.
+//
+// This source file is part of the eZ publish (tm) Open Source Content
+// Management System.
+//
+// This file may be distributed and/or modified under the terms of the
+// "GNU General Public License" version 2 as published by the Free
+// Software Foundation and appearing in the file LICENSE included in
+// the packaging of this file.
+//
+// Licencees holding a valid "eZ publish professional licence" version 2
+// may use this file in accordance with the "eZ publish professional licence"
+// version 2 Agreement provided with the Software.
+//
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING
+// THE WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE.
+//
+// The "eZ publish professional licence" version 2 is available at
+// http://ez.no/ez_publish/licences/professional/ and in the file
+// PROFESSIONAL_LICENCE included in the packaging of this file.
+// For pricing of this licence please contact us via e-mail to licence@ez.no.
+// Further contact information is available at http://ez.no/company/contact/.
+//
+// The "GNU General Public License" (GPL) is available at
+// http://www.gnu.org/copyleft/gpl.html.
+//
+// Contact licence@ez.no if any conditions of this licencing isn't clear to
+// you.
+//
+
+$Module = array( "name" => "eZVisual",
+                 "variable_params" => true,
+                 'ui_component_match' => 'view' );
+
+$ViewList = array();
+$ViewList["toolbarlist"] = array(
+    "script" => "toolbarlist.php",
+    "default_navigation_part" => 'ezvisualnavigationpart',
+    "params" => array( 'SiteAccess' ) );
+
+$ViewList["toolbar"] = array(
+    "script" => "toolbar.php",
+    'ui_context' => 'edit',
+    "default_navigation_part" => 'ezvisualnavigationpart',
+    'post_actions' => array( 'BrowseActionName' ),
+    'single_post_actions' => array( 'BackToToolbarsButton' => 'BackToToolbars',
+                                    'NewToolButton' => 'NewTool',
+                                    'UpdatePlacementButton' => 'UpdatePlacement',
+                                    'BrowseButton' => 'Browse',
+                                    'RemoveButton' => 'Remove',
+                                    'StoreButton' => 'Store' ),    
+    "params" => array( 'SiteAccess', 'Position' ) );
+
+$ViewList["menuconfig"] = array(
+    "script" => "menuconfig.php",
+    'default_navigation_part' => 'ezvisualnavigationpart',
+    'single_post_actions' => array( 'StoreButton' => 'Store',
+                                    'SelectCurrentSiteAccessButton' => 'SelectCurrentSiteAccess' ),
+    "params" => array() );
+
+$ViewList["templatelist"] = array(
+    "script" => "templatelist.php",
+    "default_navigation_part" => 'ezvisualnavigationpart',
+    "params" => array( ),
+    "unordered_params" => array( "offset" => "Offset" ) );
+
+$ViewList["templateview"] = array(
+    "script" => "templateview.php",
+    "default_navigation_part" => 'ezvisualnavigationpart',
+    'single_post_actions' => array( 'SelectCurrentSiteAccessButton' => 'SelectCurrentSiteAccess',
+                                    'RemoveOverrideButton' => 'RemoveOverride',
+                                    'UpdateOverrideButton' => 'UpdateOverride',
+                                    'NewOverrideButton' => 'NewOverride' ),
+    "params" => array( ) );
+
+$ViewList['templateedit'] = array(
+    'script' => 'templateedit.php',
+    'ui_context' => 'edit',
+    'default_navigation_part' => 'ezvisualnavigationpart',
+    'single_post_actions' => array( 'SaveButton' => 'Save',
+                                    'DiscardButton' => 'Discard' ),
+    'params' => array( ),
+    'unordered_params' => array( 'siteAccess' => 'SiteAccess' ) );
+
+$ViewList['templatecreate'] = array(
+    'script' => 'templatecreate.php',
+    'ui_context' => 'edit',
+    'default_navigation_part' => 'ezvisualnavigationpart',
+    'single_post_actions' => array( 'CreateOverrideButton' => 'CreateOverride',
+                                    'CancelOverrideButton' => 'CancelOverride' ),
+    'params' => array( ),
+    'unordered_params' => array( 'siteAccess' => 'SiteAccess',
+                                 'classID' => 'ClassID',
+                                 'nodeID' => 'NodeID' ) );
+
+?>
