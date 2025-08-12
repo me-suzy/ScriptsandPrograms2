101920b057980ac75aff0c6be862f6fa703bd752
me-suzy <me.suzana@gmail.com>
2025-08-12 04:01:00 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/poll/lang_poll_ru.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/poll/lang_poll_ru.php
@@ -0,0 +1,51 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] 		= '®«®á®¢ ­¨ï';
+$GLOBALS["tSubmitFormTitle"] 	= '®§¤ âì £®«®á®¢ ­¨¥';
+
+//  List Headings
+$GLOBALS["tQuestion"] 		= '®¯à®á';
+$GLOBALS["tOption"] 		= 'ã­ªâ ¤«ï ¢ë¡®à ';
+$GLOBALS["tPostedBy"] 		= 'â¯à ¢«¥­®';
+$GLOBALS["tPollType"] 		= '¨¯ £®«®á®¢ ­¨ï';
+$GLOBALS["tPublishDate"] 	= ' â  ¯ã¡«¨ª æ¨¨';
+$GLOBALS["tExpiryDate"] 	= ' â  ®ª®­ç ­¨ï';
+$GLOBALS["tStatus"] 		= 'â âãá';
+
+//  List Functions
+$GLOBALS["tAddNewPoll"] 	= '®¡ ¢¨âì £®«®á®¢ ­¨¥';
+$GLOBALS["tEditPoll"] 		= 'à ¢¨âì £®«®á®¢ ­¨¥';
+$GLOBALS["tDeletePoll"] 	= '¤ «¨âì £®«®á®¢ ­¨¥';
+$GLOBALS["tReleasePoll"] 	= '®¤â¢¥à¤¨âì £®«®á®¢ ­¨¥';
+$GLOBALS["tEditOptions"] 	= 'à ¢¨âì ­ áâà®©ª¨ £®«®á®¢ ­¨ï';
+
+$GLOBALS["tAddNewPollOption"] 	= '®¡ ¢¨âì ¯ã­ªâ';
+$GLOBALS["tEditPollOption"] 	= 'à ¢¨âì ¯ã­ªâ';
+$GLOBALS["tDeletePollOption"] 	= '¤ «¨âì ¯ã­ªâ';
+
+//  Form Field Headings
+$GLOBALS["tCategory"] 		= ' â¥£®à¨ï';
+
+//  Poll View Texts
+$GLOBALS["tMultiVotes"] 	= '­®¦¥áâ¢¥­­ë© ¢ë¡®à';
+$GLOBALS["tSingleVote"] 	= '¤¨­áâ¢¥­­ë© ¢ë¡®à';
+
+$GLOBALS["tSubmitPoll"] 	= 'â¯à ¢¨âì £®«®á®¢ ­¨¥';
+$GLOBALS["tContact"] 		= '®­â ªâ';
+$GLOBALS["tEMail"] 		= 'E-Mail';
+$GLOBALS["tWebPage"] 		= 'Web âà ­¨æ ';
+$GLOBALS["tVoteButton"] 	= 'à®£®«®á®¢ âì';
+
+//  Poll Submission Texts
+$GLOBALS["tThankYou"]		= '¯ á¨¡® §  ®â¯à ¢ªã £®«®á®¢ ­¨ï.';
+$GLOBALS["tReleaseRequired"] 	= '¡à â¨â¥ ¢­¨¬ ­¨¥, çâ® £®«®á®¢ ­¨¥ ­¥ ¡ã¤¥â ¯®ª §ë¢ âìáï, ¯®ª  ­¥ ¡ã¤¥â ¯®¤â¢¥à¦¤¥­   ¤¬¨­¨áâà â®à®¬.';
+$GLOBALS["tClickToReturn"] 	= '¥«ª­¨â¥ §¤¥áì, çâ®¡ë ¢¥à­ãâìáï ­  ';
+$GLOBALS["tGalleryPage"] 	= 'áâà ­¨æã £®«®á®¢ ­¨©';
+
+// Security Texts
+$GLOBALS["tMustLogin"] 		= 'ë ¤®«¦­ë';
+$GLOBALS["tLogin"] 		= '¢®©â¨ ¢ á¨áâ¥¬ã';
+$GLOBALS["tToSubmit"] 		= '¤«ï ®â¯à ¢ª¨ £®«®á®¢ ­¨ï';
+
+?>
