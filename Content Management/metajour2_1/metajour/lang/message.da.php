7c49543f97f49cee5b10e88cc398227528b1368b
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:33 +0300

    Add Content Management/metajour2_1/metajour/lang/message.da.php

---
+++ Content Management/metajour2_1/metajour/lang/message.da.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Beskeder';
+$LANG['label_name'] = 'Emne';
+$LANG['label_touser'] = '';
+$LANG['label_fromuser'] = '';
+$LANG['label_content'] = '';
+$LANG['label_readstatus'] = '';
+$LANG['label_messagetype'] = '';
+?>
