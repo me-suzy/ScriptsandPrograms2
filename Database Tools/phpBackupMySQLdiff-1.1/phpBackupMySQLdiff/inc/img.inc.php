e0a8599846cf49f1876fe4eb5c344e7a7d167c3c
me-suzy <me.suzana@gmail.com>
2025-08-12 09:00:17 +0300

    Add Database Tools/phpBackupMySQLdiff-1.1/phpBackupMySQLdiff/inc/img.inc.php

---
+++ Database Tools/phpBackupMySQLdiff-1.1/phpBackupMySQLdiff/inc/img.inc.php
@@ -0,0 +1,16 @@
+<?
+/**universal
+ * Fichier contenant les définitions des images
+ * @author Thomas Pequet
+ * @version 1.0 
+ */
+
+// Images communes à toutes les pages
+$imageLogo = '<IMG SRC="'.$rep_par_rapport_racine.'img/logo.png" WIDTH="320" HEIGHT="70" ALT="'.$nomSite.'">';
+	
+// Images de la page "index.xxx"
+if ($page=="1") {
+	$imageFleur = '<IMG SRC="'.$rep_par_rapport_racine.'img/1.gif" WIDTH="40" HEIGHT="40" ALT="Une production Thomas Pequet">';
+	$imagePx = '<IMG SRC="'.$rep_par_rapport_racine.'img/2.gif" ';
+}
+?>
