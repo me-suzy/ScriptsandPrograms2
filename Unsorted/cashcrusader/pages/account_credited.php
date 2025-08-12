1e8755e67cd088e9433087c65bb713243785b7eb
me-suzy <me.suzana@gmail.com>
2025-08-13 01:26:31 +0300

    Add Unsorted/cashcrusader/pages/account_credited.php

---
+++ Unsorted/cashcrusader/pages/account_credited.php
@@ -0,0 +1,13 @@
+<? include("setup.php");?>
+<html><body><center>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+<tr align="center"> 
+<td width="40%" valign="top"><font face=arial size=2>Your account has been credited.<br>
+Thank You!</font> </td>
+<td width="60%" align="center" valign="top"> 
+<? getad('Main');?>
+</td>
+</tr>
+</table>
+</center>
+</body></html>
