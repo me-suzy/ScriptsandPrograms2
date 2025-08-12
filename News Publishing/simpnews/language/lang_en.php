ec46720421aa29e5bb9db6d8c0ef19ddd5ca1709
me-suzy <me.suzana@gmail.com>
2025-08-12 20:48:14 +0300

    Add News Publishing/simpnews/language/lang_en.php

---
+++ News Publishing/simpnews/language/lang_en.php
@@ -0,0 +1,316 @@
+<?php
+/***************************************************************************
+ * (c)2002-2004 Boesch IT-Consulting (info@boesch-it.de)
+ ***************************************************************************/
+$l_poster = "poster";
+$l_functiondisabled = "function disabled";
+$l_subscriptionprelude = "subscribe news";
+$l_email = "email";
+$l_nofreemailer = "(no freemailers allowed)";
+$l_subscribe = "subscribe";
+$l_noemail = "You must provide an email";
+$l_back = "back";
+$l_novalidemail = "Please provide a valid email";
+$l_forbidden_freemailer = "An email at this freemailer is not allowed for subscribers";
+$l_hours = "hours";
+$l_subscriptionconfirmmail = "Hello,\nYou have requested to subscribe news at {sitename}.\n
+To ensure the request really was posted by you, this confirmation email has been sent.\n
+To activate subscription, please go to this URL within the next {confirmtime}:\n{confirmurl}\n
+If you haven't requested subscription, you need not to do anything.";
+$l_subscriptionconfirmmail_html = "<font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"2\">Hello,\nYou have requested to subscribe news at {sitename}.\n
+To ensure the request really was posted by you, this confirmation email has been sent.\n
+To activate subscription, please go to this URL within the next {confirmtime}:\n<a href=\"{confirmurl}\"><font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"1\">{confirmurl}</font></a>\n
+If you haven't requested subscription, you need not to do anything.</font>";
+$l_subscriptionconfirmsubject = "News subscription ({sitename}) - confirmation request";
+$l_subscriptiondone = "Thank you for subscribing to our news";
+$l_allready_subscribed = "A subscription for this email already exists.";
+$l_allready_pending = "There already is an unconfirmed subscription request for this email.<br>Please wait for confirmation request beeing sent to you by email.";
+$l_missingemail = "No email submitted";
+$l_missingid = "No ID submitted";
+$l_noconfirmentry = "No subscription request for confirmation found.<br>Maybe the maximum amount of time to confirm your request has been passed<br>or you already confirmed the request.";
+$l_subscriptionconfirmed = "Your news subscription has been actived.";
+$l_noremoveentry = "No subscription for removing found.";
+$l_unsubscribed = "subscription removed.";
+$l_emailtype = "Type for email";
+$l_htmlmail = "HTML";
+$l_ascmail = "plain text";
+$l_subscriptionremoveprelude = "Do you really want to remove subscription for {email}?";
+$l_yes = "yes";
+$l_subscribe = "subscribe news";
+$l_ok = "Ok";
+$l_unsubscribe = "unsubscribe news";
+$l_unsubscriptionconfirmmail = "Hello,\nyou have requested to unsubscribe news at {sitename}.\n
+To ensure the request really was posted by you, this confirmation request has been sent.\n
+To remove your subscription, please go to this URL:\n{confirmurl}\n
+If you haven't requested to unsubscribe, you need not to do anything.";
+$l_unsubscriptionconfirmmail_html = "<font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"2\">Hello,\nyou have requeste to unsubscribe news at {sitename}.\n
+To ensure the request really was posted by you, this confirmation request has been sent.\n
+To remove your subscription, please go to this URL:\n<a href=\"{confirmurl}\"><font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"1\">{confirmurl}</font></a>\n
+If you haven't requested to unsubscribe, you need not to do anything.</font>";
+$l_unsubscriptionconfirmsubject = "News subscription ({sitename}) - unsubscribe request";
+$l_unsubscribesent = "An email containing the URL to confirm your unsubscription has been sent.<br>Please follow the instructions in this email to remove your subscription.";
+$l_page = "page";
+$l_entries = "entries";
+$l_of = "of";
+$l_writecomment = "write comment";
+$l_name = "Name";
+$l_comment = "Comment";
+$l_noname = "You have to provide a name";
+$l_nocomment = "You have to provide a comment";
+$l_callingerror = "calling error";
+$l_commentposted = "comment saved";
+$l_news = "news";
+$l_comments = "comments";
+$l_allnews = "all news";
+$l_timeonserver = "actual time on server";
+$l_search="search";
+$l_dosearch = "search";
+$l_result = "results";
+$l_page_forward = "next page";
+$l_page_back = "previous page";
+$l_page_last = "last page";
+$l_page_first = "first page";
+$l_gotop = "goto top of page";
+$l_newnews = "new news entries";
+$l_nonewnews = "no new news entries";
+$l_help = "help";
+$l_noentriesfound = "no matching entries found";
+$l_attachement = "download attachment";
+$l_weekday = array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+$l_monthname = array("January","February","March","April","May","June","July","August","September","October","November","December");
+$l_prevmonth = "previous Month";
+$l_nextmonth = "next Month";
+$l_newsforthisdate = "News for this date";
+$l_noentries = "no entries";
+$l_nosuchentry = "no such entry";
+$l_noeventstoday = "no events today";
+$l_new = "new";
+$l_more = "more";
+$l_eventsfor = "Events for";
+$l_redirected = "You will be forwarded to the news shortly.";
+$l_text = "Text";
+$l_betweendate = "in the following Timeframe";
+$l_startdate = "start date";
+$l_enddate = "end date";
+$l_dateselformat = array("month","day","year");
+$l_day = "Day";
+$l_month = "Month";
+$l_year = "Year";
+$l_morenews = "more news";
+$l_general = "General";
+$l_events = "Events";
+$l_showevents = "show events";
+$l_showcategory = "only show this category";
+$l_newslist = "newslist";
+$l_showentry = "display entry";
+$l_temp_unavail = "service temporary unavailable";
+$l_catlist = "Categories";
+$l_content = "Content";
+$l_generated_with = "Powered by";
+$l_propose_news = "propose news entry";
+$l_heading = "Heading";
+$l_notext = "You have to provide a text";
+$l_noheading = "You have to provide a heading";
+$l_proposeposted = "Your proposal has been saved.";
+$l_jumpto = "jump to";
+$l_noevents = "no events";
+$l_propose_event = "propose event";
+$l_date = "Date";
+$l_novaliddate = "{month}/{day}/{year} is not a valid date";
+$l_novaliddate2 = "Please select a valid date.";
+$l_redirected_events = "You will be forwarded to the events shortly.";
+$l_pastdate = "You can't enter a day in the past";
+$l_all_cats = "all categories";
+$l_category = "Category";
+$l_notsubj = "new entry proposal ({ptype}, {sitename})";
+$l_notmsg = "A new entry proposal was posted in {ptype},{category}";
+$l_print = "printerfriendly version";
+$l_withattachement = "Attachment";
+$l_eventsforthisdate = "Events on this date";
+$l_storeuserdata = "Store userdata for later reuse";
+$l_goto_actdate = "go to actual date";
+$l_csvexport = "CSV export";
+$l_language = "Language";
+$l_asclist = "List as ASCII";
+$l_editownproposals = "edit own proposals";
+$l_proposed_news = "proposed news";
+$l_proposelistprelude = "Please enter email You used on submitting proposal.";
+$l_back2news = "back to news";
+$l_transferrednews = "proposals already transferred to news";
+$l_edit = "edit";
+$l_delete = "delete";
+$l_proposals = "Proposals";
+$l_password = "Password";
+$l_requestpw = "request password";
+$l_nopassword = "You have to provide a password";
+$l_requestpwprelude = "Request password for {email}";
+$l_passwordconfirm = "confirm password";
+$l_passwordmismatch = "Passwords don't match";
+$l_noentryforemail = "No entry for this email";
+$l_pwconfirmmail = "Hello,\nYou have requested a password to manage your porposed news/events on {sitename}.
+To enable the password, please visit this link: {confirmurl}";
+$l_pwconfirmmail_html = "<font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"2\">Hello,\nYou have requested a password to manage your porposed news/events on {sitename}.
+To enable the password, please visit this link:</font> <a href=\"{confirmurl}\"><font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"1\">{confirmurl}</font></a>";
+$l_pwconfirmsubject = "Passwordrequest ({sitename})";
+$l_proppw = "Password for managing proposed news/events";
+$l_pwrequested = "Password request done.<br>An email containing instructions on activating the password was sent to {email}.";
+$l_pwallreadydefined = "Password for this email already defined";
+$l_pwconfirmed = "Password confirmed and active.";
+$l_erroronlogin = "Error logging in (wrong email/password)";
+$l_loggedinas = "Loggedin as";
+$l_logout = "logout";
+$l_logged_out = "Your loggedin now";
+$l_login = "login";
+$l_loginfirst = "You have to login first";
+$l_edit_proposed_news = "Edit proposed news";
+$l_entryupdated = "entry updated";
+$l_delmsg = "Really delete entry {entrynr}?";
+$l_bbbuttons = array("liststart"=>"list start","listend"=>"list end","listitem"=>"list item","font"=>"font","fontsize"=>"fontsize","fontcolor"=>"fontcolor","alignment"=>"alignment","bold"=>"bold","italic"=>"italic","typewriter"=>"typewriter","center"=>"center","sub"=>"subscript","sup"=>"superscript","code"=>"code","quote"=>"quote","url"=>"URL","email"=>"email","pic"=>"picture","strike"=>"strike through","pic2"=>"choose/upload image","underline"=>"underline");
+$l_availfonts = array("Andale Mono","Arial","Arial Black","Book Antiqua","Century Gothic","Comic Sans MS","Courier New","Georgia","Impact","Tahoma","Times New Roman","Script MT Bold","Verdana","Lucida Console");
+$l_availcolors = array("White","Black","Red","Yellow","Pink","Green","Orange","Purple","Blue","Beige","Brown","Teal","Navy","Maroon","Limegreen");
+$l_enterfont = "Please enter font";
+$l_entertextsize = "Please enter fontsize";
+$l_entercolor = "Please enter fontcolor";
+$l_alignments = array("Left","Right","Center");
+$l_alignment = "Alignment";
+$l_enterlink = "Please enter link";
+$l_enteremail = "Please enter email";
+$l_enterurl = "Please enter URL";
+$l_entertext = "Enter text to display";
+$l_bbcode_helps = array(
+	"bold text: [b]Text[/b]",
+	"italic text: [i]Text[/i]",
+	"Typewriter: [tt]Text[/tt]",
+	"strike through: [s]Text[/s]",
+	"center: [center]Text[/center]",
+	"subscript: [sub]Text[/sub]",
+	"superscript: [sup]Text[/sup]",
+	"Code: [code]Text[/code]",
+	"Quote: [quote]Text[/quote]",
+	"Liststart: [list]",
+	"Listitem: [*]",
+	"Listend: [/list]",
+	"Email: [email]test@foo.bar[/email]",
+	"URL: [url]http://www.foo.bar[/url]",
+	"Image: [img]http://www.foo.bar/bild.gif[/img]",
+	"choose/upload image",
+	"Font: [font=Arial]Text[/font]",
+	"Fontsize: [size=2]Text[size]",
+	"Textalignment: [align=right]Text[/align]",
+	"Fontcolor: [color=red]Text[/color]"
+);
+$l_fontcolor = "fontcolor";
+$l_emoticonlist = "List of smilies";
+$l_close = "close";
+$l_code = "Code";
+$l_emotion = "Emotion";
+$l_bbcodehelp = "BBCode help";
+$l_options = "Options";
+$l_urlautoencode = "automatically encode URLs";
+$l_enablespcode = "activate BBTags";
+$l_disableemoticons = "disable smilies";
+$l_entrydeleted = "entry deleted";
+$l_delnewsmail = "{email} has requested to delete news entry #{newsnr}.";
+$l_delnewssubj = "Request for deleting a news entry";
+$l_proposed_events = "proposed Events";
+$l_back2events = "Back to events";
+$l_transferredevents = "proposals already transferred to Events";
+$l_deleventmail = "{email} has requested to delete event entry #{eventnr}.";
+$l_deleventsubj = "Request for deleteing an event";
+$l_edit_proposed_events = "Edit proposed event";
+$l_announcements = "Announcements";
+$l_announcement = "Announcement";
+$l_global_announcement = "global announcement";
+$l_back2search = "back to search";
+$l_newsubsubj = "new subscriber ({sitename})";
+$l_newsubmsg = "{email} has subscribed to newsletter for {category} on {sitename}";
+$l_remsubsubj = "removed subscriber ({sitename})";
+$l_remsubmsg = "{email} has unsubscribed from newsletter for {category} on {sitename}";
+$l_attachfile = "attach file";
+$l_noeventstoday2 = "no events today";
+$l_subscriptionconfirminfo = "To ensure you are the owner of the entered email<br>a confirmation request has been sent to this email.<br>
+Please follow the instructions in the email to activate your subscription.";
+$l_edituserentry = "edit user entry";
+$l_newproposal = "post new proposal";
+$l_newpassword = "new password";
+$l_emailentry = "send entry by email";
+$l_mailentrysubj = "Newsentry";
+$l_subject = "Subject";
+$l_sendermail = "email of sender";
+$l_receivermail = "email of receiver";
+$l_nosendermail = "You have to provide a sender email";
+$l_noreceivermail = "You have to provide a receiver email";
+$l_novalidsendermail = "Invalid email for sender";
+$l_novalidreceivermail = "Invalid email for receiver";
+$l_entrymailed = "Entry sent by email";
+$l_ratingquestion = "Should there be more news like this";
+$l_ratings = array(1=>"Yes",0=>"No");
+$l_ratingadded = "Your rating has been added";
+$l_timezone_note = "all times are";
+$l_by = "by";
+$l_on = "on";
+$l_newcommentsubj = "new comment ({sitename})";
+$l_newcommentmsg = "{email} has posted a new comment on  entry #{entry} on {sitename}";
+$l_nocatselected = "You have to select at least one category";
+$l_unknown = "unknown";
+$l_msubscriptionconfirmmail = "Hello,\nYou have requested to subscribe news for '{catname}' at {sitename}.\n
+To ensure the request really was done by you, this confirmation request has been sent by us.\n
+To activate subscription, please go to this URL within the next {confirmtime}:\n{confirmurl}\n
+If you haven't requested subscription, you need not to do anything.";
+$l_msubscriptionconfirmmail_html = "<font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"2\">Hello,\nYou have requested to subscribe news for <i>{catname}</i> at {sitename}.\n
+To ensure the request really was done by you, this confirmation request has been sent by us.\n
+To activate subscription, please go to this URL within the next {confirmtime}:\n<a href=\"{confirmurl}\"><font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"1\">{confirmurl}</font></a>\n
+If you haven't requested subscription, you need not to do anything.</font>";
+$l_subscriptionmaint = "maintain subscriptions";
+$l_listsubs = "list subscriptions";
+$l_subscriptionsfor = "subscriptions for";
+$l_munsubscriptionconfirmmail = "Hello,\nyou have requeste to unsubscribe news for {catname} at {sitename}.\n
+To ensure the request really was done by you, this confirmation request has been sent by us.\n
+To remove your subscription, please go to this URL:\n{confirmurl}\n
+If you haven't requested tu unsubscribe, you need not to do anything.";
+$l_munsubscriptionconfirmmail_html = "<font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"2\">Hello,\nyou have requeste to unsubscribe news for <i>{catname}</i> at {sitename}.\n
+To ensure the request really was done by you, this confirmation request has been sent by us.\n
+To remove your subscription, please go to this URL:\n<a href=\"{confirmurl}\"><font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"1\">{confirmurl}</font></a>\n
+If you haven't requested tu unsubscribe, you need not to do anything.</font>";
+$l_lastupdated = "last updated";
+$l_next_entry = "next entry";
+$l_gotostart = "goto start";
+$l_prev_entry = "previous entry";
+$l_goto_news = "goto news";
+$l_goto_events = "goto events";
+$l_events_for_today = "Events for today";
+$l_events_for_days = "Events within the next %d days";
+$l_numentries = "number of entries";
+$l_goto_announcements = "goto announcements";
+$l_search_events = "search events";
+$l_do_search = "do search";
+$l_search_results = "Search results for %s (%d)";
+$l_restart_search = "restart search";
+$l_events_for = "events for the next";
+$l_days = "days";
+$l_wap_evs_dayrange = "days +/-";
+$l_only_selday = "only selected day";
+$l_search_results2 = "Search results for %s, +/- %d Tage (%d)";
+$l_nextpage = "next page";
+$l_prevpage = "previous page";
+$l_searchtext = "search words";
+$l_search_results3 = "Search results for %s - %s (%d)";
+$l_listofcats = "list of categories";
+$l_collapse = "collapse entry";
+$l_expand = "expand entry";
+$l_mode = "Mode";
+$l_file = "File";
+$l_description = "Description";
+$l_rss_newsfeeds = "RSS Newsfeeds";
+$l_unallowedfiletype = "Files of this type (.%s) are not allowed to be used as attachments";
+$l_catundef = "category not defined";
+$l_wholestory = "[read more]";
+$l_noannouncements = "no announcements";
+$l_searchby = "search by";
+$l_pwset = "password has been set";
+$l_noflooding = "an entry only can be submitted once";
+// If you want an info stating you have translated the language file in generated HTML pages
+// provide the text within this variable
+// e.g. $l_translationnote = "translation to greek language done by sokrates"
+$l_translationnote = ""
+?>
