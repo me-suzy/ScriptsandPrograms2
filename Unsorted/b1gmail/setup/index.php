e1f414cf2b0b6f6336be3fdc87ac9608a31413db
me-suzy <me.suzana@gmail.com>
2025-08-13 01:08:49 +0300

    Add Unsorted/b1gmail/setup/index.php

---
+++ Unsorted/b1gmail/setup/index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<title>b1gMail Setup</title>
+</head>
+<body bgcolor="#ffffff">
+<center>
+ <table width="100%" height="100%" cellspacing="0" cellpadding="0">
+  <tr>
+   <td width="100%" height="100%" valign="middle" align="center">
+    <table cellspacing="1" bgcolor="#000000" cellpadding="0" width="65%">
+     <tr>
+      <td bgcolor="#003366" background="table.gif" align="center">
+       <font face="verdana" size="2" color="#ffffff">
+        b1gMail Setup
+       </font>
+      </td>
+     </tr>
+     <tr>
+      <td bgcolor="#D4D0C8" align="center">
+       <table width="100%">
+        <tr>
+         <td width="150" valign="middle" align="center">
+          <img src="mbox.gif" border="0">
+         </td>
+         <td valign="top" align="center">
+           <font face="verdana" size="2" color="#000000">
+             <b><u>b1gMail Setup</u></b>
+             <br><br>
+             Welcome to the b1gMail Setup. This wizard will guide you throgh the setup process.<br><br>Hit "Weiter" to continue.
+           </font>
+         </td>
+        </tr>
+        <tr>
+         <td align="center"><font face="verdana" size="1" color="#000000">&copy; 2002 by www.b1g.de</font></td>
+         <td align="right"><font face="verdana" size="2" color="#000000"><a href="step2.php"><img src="next.gif" border="0"></a></font></td>
+        <tr>
+       </table>
+      </td>
+     </tr>
+    </table>
+   </td>
+  </tr>
+ </table>
+</center>
+</body>
+</html>
