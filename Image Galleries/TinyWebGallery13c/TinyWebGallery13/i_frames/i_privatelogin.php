fe51a93213c40b123bab77cc08c60ee278d52764
me-suzy <me.suzana@gmail.com>
2025-08-12 18:28:44 +0300

    Add Image Galleries/TinyWebGallery13c/TinyWebGallery13/i_frames/i_privatelogin.php

---
+++ Image Galleries/TinyWebGallery13c/TinyWebGallery13/i_frames/i_privatelogin.php
@@ -0,0 +1,87 @@
+<?php
+/*************************
+  Copyright (c) 2004-2005 TinyWebGallery
+  written by Michael Dempfle
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  TWG version: 1.3c
+  $Date: 2005/11/15 09:02 $
+**********************************************/
+require  "../config.php";
+include "../inc/mysession.inc.php";
+include "../inc/filefunctions.inc.php";
+include "i_parserequest.inc.php";
+
+$relativepath = "../";
+include "../inc/checkprivate.inc.php";
+
+$passwort = false;
+
+if (isset($_GET['twg_passwort'])) {
+		$passwort = urlencode($_GET['twg_passwort']);
+    if ($encrypt_passwords) {
+    	if (function_exists("sha1") && $use_sha1_for_password) {
+		      $passwort = sha1($passwort);
+			} else {
+			    $passwort = sha2($passwort);
+	 		}
+    }
+    if (in_array($passwort,$passwd)) {
+        $_SESSION["privategallogin"] =  $passwort;   
+    } 
+} 
+
+$logout = false;
+if (isset($_GET['twg_logout'])) {
+    session_unregister("privategallogin");
+    $logout = true;
+} 
+
+require "../language/language_" . $default_language . ".php";
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+ "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>TinyWebGallery</title>
+<meta name="author" content="mid" />
+<link rel="stylesheet" type="text/css" href="iframe.css" />
+<script type="text/javaScript">reload = false;</script>
+<script type="text/javaScript" src="../js/twg_image.js"></script>
+</head>
+<body>
+ <form action="<?php print $_SERVER['PHP_SELF']; ?>" method="get">
+<table summary='' style="width: 100%; height:100%" cellpadding='0' cellspacing='0'><tr><td class="closebutton"> 
+<img name="imageField" onClick="closeiframe()" alt='' align="right" src="../buttons/close.gif" width="12" height="12" border="0" />
+</td></tr><tr><td> 
+ <input name="twg_album" type="hidden" value="<?php echo encodespace($twg_album); ?>"/>
+ <input name="twg_lang" type="hidden" value="<?php echo $default_language; ?>"/>
+ <?php echo $hiddenvals; ?>
+<?php
+$closescript = "<script>closeiframe(); if (reload) { parent.location='" . urldecode($twg_root) ."' + location.search.substring(0,location.search.indexOf('twg_passwort')-1);  }</script>";
+if ($logout) {
+    echo $closescript;
+} else if ($passwort == false) {
+    echo sprintf($lang_privatelogin_php_password, $twg_album);
+} else if (!in_array($passwort,$passwd)) {
+    echo $lang_privatelogin_php_wrong_password;
+} else {
+    echo $closescript;
+} 
+
+?>
+  <br/><img alt='' src='../buttons/1x1.gif' height='4' /><br/><input name="twg_passwort" type="password" size="20"/>
+  &nbsp;
+  <input type="submit" name="Submit" value="<?php echo $lang_privatelogin_php_login ?>" />
+</td></tr></table>
+</form>
+
+<?php include "i_bottom.inc.php"; ?>
+</body>
+</html>
