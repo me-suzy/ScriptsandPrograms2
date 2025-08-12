ef6ec8a0846dd87525010d4eb3d1154b04c658f5
me-suzy <me.suzana@gmail.com>
2025-08-13 01:26:53 +0300

    Add Unsorted/cashcrusader/pages/expired_ptc_ad.php

---
+++ Unsorted/cashcrusader/pages/expired_ptc_ad.php
@@ -0,0 +1,16 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr> 
+<td align="center"> 
+<p><font size="4"><br>
+<br>
+Sorry, this link just expired</font></p>
+<font size="4"><br>
+<br>
+<br>
+</font></td>
+</tr>
+</table>
+<br>
+<? include("footer.php");?>
