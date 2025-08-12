34f58156183a7d5ecf2d6cb47597f495c67fe9b1
me-suzy <me.suzana@gmail.com>
2025-08-12 03:37:02 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_downloads.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_downloads.php
@@ -0,0 +1,330 @@
+<?php
+
+/***************************************************************************
+
+ m_downloads.php
+ ----------------
+ copyright : (C) 2005 - The iziContents Development Team
+
+ iziContents version : 1.0
+ fileversion : 1.0.1
+ change date : 23 - 04 - 2005
+ ***************************************************************************/
+
+/***************************************************************************
+ The iziContents Development Team offers no warranties on this script.
+ The owner/licensee of the script is solely responsible for any problems
+ caused by installation of the script or use of the script.
+
+ All copyright notices regarding iziContents and ezContents must remain intact on the scripts and in the HTML for the scripts.
+
+ For more info on iziContents,
+ visit http://www.izicontents.com*/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the License which can be found within the
+ *   zipped package. Under the licence of GPL/GNU.
+ *
+ ***************************************************************************/
+include_once ("rootdatapath.php");
+
+$GLOBALS["form"] = 'downloads';
+$GLOBALS["validaccess"] = VerifyAdminLogin();
+include_once ($GLOBALS["rootdp"]."include/filefunctions.php");
+validatefiletypes('Download');
+
+includeLanguageFiles('admin','downloads');
+
+
+if (isset($_GET["subdir"])) {
+	$_GET["subdir"] = str_replace('../', '', $_GET["subdir"]);
+} else {
+	$_GET["subdir"] = '';
+}
+
+if (($_GET["mode"] == 'delete') || ($_GET["mode"] == 'deletedir')) {
+	if ($GLOBALS["candelete"] == False) {
+		Header("Location: ".BuildLink('adminlogin.php'));
+	} else {
+		if ($_GET["mode"] == 'delete') {
+			$delete = DeleteDownload();
+		} else {
+			$delete = DeleteDownloadDir();
+		}
+	}
+}
+
+
+force_page_refresh();
+frmDownloads();
+
+
+function frmDownloads()
+{
+	global $_GET, $EzAdmin_Style;
+
+	adminheader();
+	if ($GLOBALS["ShowFilePermissions"] != 'N') {
+		if ($GLOBALS["OS"] != "Windows") { $colcount = 7;
+		} else { $colcount = 5; }
+	} else { $colcount = 4; }
+
+	admintitle($colcount,$GLOBALS["tFormTitle"]);
+	adminbuttons('',$GLOBALS["tAddNewDownload"],$GLOBALS["tEditDownload"],$GLOBALS["tDeleteDownload"]);
+	$GLOBALS["iOpenFolder"] = imagehtmltag($GLOBALS["theme_home"],$EzAdmin_Style["FolderIcon"],'',0,'');
+	if ($GLOBALS["iOpenFolder"] == '') $GLOBALS["iOpenFolder"] = imagehtmltag($GLOBALS["style_home"],$EzAdmin_Style["FolderIcon"],'',0,'');
+
+	$nCurrentPage = 0;
+	if ($_GET["page"] != "") { $nCurrentPage = $_GET["page"]; }
+	$lRecCount = lCountFiles($GLOBALS["downloads_home"],'download');
+	$nPages = intval(($lRecCount - 0.5) / $GLOBALS["RECORDS_PER_PAGE"]) + 1;
+	if ($nCurrentPage >= $nPages) { $nCurrentPage = 0; }
+	$lStartRec = $nCurrentPage * $GLOBALS["RECORDS_PER_PAGE"];
+
+	frmDisplayDir($colcount,$GLOBALS["downloads_home"]);
+	safeModeWarning($colcount);
+
+
+	frmDownloadsHdFt($colcount,$nCurrentPage,$nPages);
+	?>
+	<tr class="teaserheadercontent"><?php
+		adminlistitem(10,$GLOBALS["tDel"],'');
+		adminlistitem(6,'&nbsp;','');
+		if ($GLOBALS["ShowFilePermissions"] != 'N') {
+			if ($GLOBALS["OS"] != "Windows") { 
+				adminlistitem(42,$GLOBALS["tDownloadName"],'');
+				adminlistitem(10,'Permissions','');
+				adminlistitem(10,'Owner','');
+				adminlistitem(10,'Group','');
+			} else {
+				adminlistitem(62,$GLOBALS["tDownloadName"],'');
+				adminlistitem(10,'Permissions','');
+			}
+		} else {
+			adminlistitem(72,$GLOBALS["tDownloadName"],'');
+		}
+		adminlistitem(12,'&nbsp;','');
+	?>
+	</tr>
+	<?php
+
+	$nDownloadNr = 0;
+	$nDownloadShowed = 0;
+
+	if (isset($GLOBALS["files"])) {
+		sort($GLOBALS["files"]);
+		while (list($i,$val) = each($GLOBALS["files"])) {
+			if ($GLOBALS["files"][$i]["filetype"] == 'download') {
+				if ($nDownloadNr >= $lStartRec && $nDownloadShowed < $GLOBALS["RECORDS_PER_PAGE"]) {
+					?>
+					<tr class="teasercontent">
+						<td align="center" valign="middle" class="content">
+							<?php
+							echo $GLOBALS["iBlank"].'&nbsp;';
+							if ($GLOBALS["candelete"] === False) { echo $GLOBALS["iBlank"];
+							} else {
+								?><a href="javascript:DelDownload('subdir=<?php echo $_GET["subdir"]; ?>&Download=<?php echo $GLOBALS["files"][$i]["filename"]; ?>&page=<?php echo $GLOBALS["page"]; ?>')" <?php echo BuildLinkMouseOver($GLOBALS["tDeleteDownload"]); ?>><?php echo $GLOBALS["iDelete"]; ?></a><?php
+							}
+							?>
+						</td>
+						<td align="center" valign="top" class="content">
+							<?php
+							if ($GLOBALS["files"][$i]["fileicon"] != '') {
+								echo '<img src="'.$GLOBALS["rootdp"].$GLOBALS["image_home"].$GLOBALS["files"][$i]["fileicon"].'">';
+							}
+							?>
+						</td>
+						<td valign="top" class="content">
+							<a href="javascript:ShowDownload('<?php echo $GLOBALS["rootdp"].$GLOBALS["downloads_home"].$_GET["subdir"].$GLOBALS["files"][$i]["filename"]; ?>')"><?php echo $GLOBALS["files"][$i]["filename"]; ?></a>
+						</td>
+						<?php
+						if ($GLOBALS["ShowFilePermissions"] != 'N') {
+							?>
+							<td align="center" valign="top" class="content">
+								<?php echo $GLOBALS["files"][$i]["fileperms"]; ?>
+							</td>
+							<?php
+							if ($GLOBALS["OS"] != "Windows") { 
+								?>
+								<td align="center" valign="top" class="content">
+									<?php echo $GLOBALS["files"][$i]["fileowner"]; ?>
+								</td>
+								<td align="center" valign="top" class="content">
+									<?php echo $GLOBALS["files"][$i]["filegroup"]; ?>
+								</td>
+							<?php
+							}
+						}
+						?>
+						<td align="<?php echo $GLOBALS["right"]; ?>" valign="top" class="content">
+							<?php echo $GLOBALS["files"][$i]["filesize"]; ?>
+						</td>
+					</tr>
+					<?php
+					$nDownloadShowed++;
+				}
+				$nDownloadNr++;
+			} else {
+				if ((($_GET["subdir"] != '') && ($GLOBALS["files"][$i]["filename"] != '.')) || (($_GET["subdir"] == '') && ($GLOBALS["files"][$i]["filename"] != '.') && ($GLOBALS["files"][$i]["filename"] != '..'))) {
+					if ($nDownloadNr >= $lStartRec && $nDownloadShowed < $GLOBALS["RECORDS_PER_PAGE"]) {
+						if ($GLOBALS["files"][$i]["filename"] != '..') {
+							$newsubdir = $_GET["subdir"].$GLOBALS["files"][$i]["filename"].'/';
+						} else {
+							$dirbreakdown = explode('/',$_GET["subdir"]);
+							array_pop($dirbreakdown); array_pop($dirbreakdown);
+							$newsubdir = implode('/',$dirbreakdown).'/';
+							if ($newsubdir == '/') { $newsubdir = ''; }
+						}
+						?>
+						<tr class="teasercontent">
+							<td align="center" valign="middle" class="content">
+								<?php
+								if ($GLOBALS["files"][$i]["filename"] != '..') {
+									?><a href="<?php echo BuildLink('m_downloads.php'); ?>&subdir=<?php echo $newsubdir; ?>"><?php echo $GLOBALS["iOpenFolder"]; ?></a>&nbsp;<?php
+									if ($GLOBALS["candelete"] === False) { echo $GLOBALS["iBlank"];
+									} else {
+										?><a href="javascript:DelDir('subdir=<?php echo $_GET["subdir"]; ?>&folder=<?php echo $GLOBALS["files"][$i]["filename"]; ?>&page=<?php echo $_GET["page"]; ?>')" <?php echo BuildLinkMouseOver($GLOBALS["tDeleteDownloadDir"]); ?>><?php echo $GLOBALS["iDelete"]; ?></a><?php
+									}
+								}
+								?>
+							</td>
+							<td align="center" valign="top" class="content">
+								&nbsp;
+							</td>
+							<td valign="top" class="content">
+								<a href="<?php echo BuildLink('m_downloads.php'); ?>&subdir=<?php echo $newsubdir; ?>"><?php echo $GLOBALS["files"][$i]["filename"]; ?></a>
+							</td>
+							<?php
+							if ($GLOBALS["ShowFilePermissions"] != 'N') {
+								?>
+								<td align="center" valign="top" class="content">
+									<?php echo $GLOBALS["files"][$i]["fileperms"]; ?>
+								</td>
+								<?php
+								if ($GLOBALS["OS"] != "Windows") { 
+									?>
+									<td align="center" valign="top" class="content">
+										<?php echo $GLOBALS["files"][$i]["fileowner"]; ?>
+									</td>
+									<td align="center" valign="top" class="content">
+										<?php echo $GLOBALS["files"][$i]["filegroup"]; ?>
+									</td>
+								<?php
+								}
+							}
+							?>
+							<td align="center" valign="middle" class="content">
+								&nbsp;
+							</td>
+						</tr>
+						<?php
+						$nDownloadShowed++;
+					}
+					$nDownloadNr++;
+				}
+			}
+		}
+	}
+
+	frmDownloadsHdFt($colcount,$nCurrentPage,$nPages);
+	frmFreeSpace($colcount,$GLOBALS["downloads_home"]);
+	?>
+	</table>
+	</body>
+	</html>
+	<?php
+} // function frmDownloads()
+
+
+function frmDownloadsHdFt($colspan,$nCurrentPage,$nPages)
+{
+	global $_GET;
+
+	$pLink = BuildLink('m_downloads.php');
+	$linkmod = '&subdir='.$_GET["subdir"];
+	$hlink = '<a href="javascript:UploadDownload();" '.BuildLinkMouseOver($GLOBALS["tAddNew"]).'>';
+	echo '<form name="PagingForm" action="'.$pLink.'" method="GET">';
+	?>
+	<tr class="topmenuback">
+		<td colspan="<?php echo $colspan; ?>" align="<?php echo $GLOBALS["left"]; ?>">
+			<table height="100%" width="100%" cellspacing="0" cellpadding="0">
+				<tr><?php
+					if (($GLOBALS["canadd"] === True) && ($GLOBALS["file_uploads"])) {
+						?><td align="<?php echo $GLOBALS["left"]; ?>" valign="bottom"><?php
+						echo displaybutton('addbutton','downloads',$GLOBALS["tAddNew"].'...',$hlink);
+						?></td><?php
+					}
+					?>
+					<td align="<?php echo $GLOBALS["right"]; ?>" valign="bottom"><?php
+						if ($nCurrentPage != 0) { ?><a href="<?php echo $pLink.$linkmod; ?>&page=0" <?php echo BuildLinkMouseOver($GLOBALS["tFirstPage"]); ?>><?php echo $GLOBALS["iFirst"]; ?></a><?php } else { echo $GLOBALS["iFirst"]; }
+						echo '&nbsp;';
+						if ($nCurrentPage != 0) { ?><a href="<?php echo $pLink.$linkmod; ?>&page=<?php echo $nCurrentPage - 1; ?>" <?php echo BuildLinkMouseOver($GLOBALS["tPrevPage"]); ?>><?php echo $GLOBALS["iPrev"]; ?></a><?php } else { echo $GLOBALS["iPrev"]; }
+						$nCPage = $nCurrentPage + 1;
+						echo RenderPageList($nCPage,$nPages,'m_downloads.php',$linkmod);
+						if ($nCurrentPage + 1 != $nPages) { ?><a href="<?php echo $pLink.$linkmod; ?>&page=<?php echo $nCurrentPage + 1; ?>" <?php echo BuildLinkMouseOver($GLOBALS["tNextPage"]); ?>><?php echo $GLOBALS["iNext"]; ?></a><?php } else { echo $GLOBALS["iNext"]; }
+						echo '&nbsp;';
+						if ($nCurrentPage + 1 != $nPages) { ?><a href="<?php echo $pLink.$linkmod; ?>&page=<?php echo $nPages - 1; ?>" <?php echo BuildLinkMouseOver($GLOBALS["tLastPage"]); ?>><?php echo $GLOBALS["iLast"]; ?></a><?php } else { echo $GLOBALS["iLast"]; } ?>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<?php
+	echo '</form>';
+} // function frmDownloadsHdFt()
+
+
+?>
+<script language="Javascript" type="text/javascript">
+	<!-- Begin
+	function DelDownload(sParams) {
+		if (window.confirm('<?php echo $GLOBALS["tConfirmDeletion"]; ?>')) {
+			location.href='<?php echo BuildLink('m_downloads.php'); ?>&mode=delete&' + sParams;
+		}
+	}
+
+	function DelDir(sParams) {
+		if(window.confirm('<?php echo $GLOBALS["tConfirmDeletion"]; ?>')) {
+			location.href='<?php echo BuildLink('m_downloads.php'); ?>&mode=deletedir&' + sParams;
+		}
+	}
+
+	function ShowDownload(sDownloadName) {
+		window.open(sDownloadName, "Download", "width=500,height=400,status=no,resizable=yes,scrollbars=yes");
+	}
+
+	function UploadDownload() {
+		window.open("<?php echo BuildLink('uploaddownload.php'); ?>&subdir=<?php echo $_GET["subdir"]; ?>", "UploadDownload", "width=600,height=310,status=no,resizable=no,scrollbars=no");
+	}
+	//  End -->
+</script>
+<?php
+
+
+function DeleteDownload()
+{
+	global $_GET;
+
+	$path = $GLOBALS["rootdp"].$GLOBALS["downloads_home"].$_GET["subdir"];
+	$ffile = $_GET["Download"];
+	$delete = DeleteFile($path,$ffile);
+
+	return $delete;
+} // function DeleteDownload()
+
+
+function DeleteDownloadDir()
+{
+	global $_GET;
+
+	$path = $GLOBALS["rootdp"].$GLOBALS["downloads_home"].$_GET["subdir"];
+	$ffile = $_GET["folder"];
+	$delete = DeleteDir($path,$ffile);
+
+	return $delete;
+} // function DeleteDownloadDir()
+
+?>
+
