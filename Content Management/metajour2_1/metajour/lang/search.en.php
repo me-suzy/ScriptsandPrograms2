ec41cc0e2d5be962119ee2f98f85bc44432c567a
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:58 +0300

    Add Content Management/metajour2_1/metajour/lang/search.en.php

---
+++ Content Management/metajour2_1/metajour/lang/search.en.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'name';
+$LANG['label_pageid_result'] = 'pageid_result';
+$LANG['label_templateid_search'] = 'templateid_search';
+$LANG['label_templateid_result'] = 'templateid_result';
+?>
