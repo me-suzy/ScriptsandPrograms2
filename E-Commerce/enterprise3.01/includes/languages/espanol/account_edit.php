85a6c4b1fd855dc7b4076134b3a1eacfc05cdd46
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:23 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/account_edit.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/account_edit.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Mi Cuenta');
+define('NAVBAR_TITLE_2', 'Editar Cuenta');
+
+define('HEADING_TITLE', 'Datos de Mi Cuenta');
+
+define('MY_ACCOUNT_TITLE', 'Mi Cuenta');
+
+define('SUCCESS_ACCOUNT_UPDATED', 'Se ha actualizado su cuenta correctamente.');
+?>
