5acd03d504bf86b4a7689920e8d4bea5b0476065
me-suzy <me.suzana@gmail.com>
2025-08-12 17:29:49 +0300

    Add Guestbooks/guestbook/guestbook/admin/delete_theme.php

---
+++ Guestbooks/guestbook/guestbook/admin/delete_theme.php
@@ -0,0 +1,81 @@
+<?php
+/****************************************************************/
+/*                       phphg Guestbook                        */
+/*                    admin/delete_theme.php file               */
+/*                      (c)copyright 2003                       */
+/*                       By hinton design                       */
+/*                 http://www.hintondesign.org                  */
+/*                  support@hintondesign.org                    */
+/*                                                              */
+/* This program is free software. You can redistrabute it and/or*/
+/* modify it under the terms of the GNU General Public Licence  */
+/* as published by the Free Software Foundation; either version */
+/* 2 of the license.                                            */
+/*                                                              */
+/****************************************************************/
+$phphg_real_path = "./../";
+include($phphg_real_path . 'common.php');
+
+if($_COOKIE['loged'] == 'yes') {
+   if($_COOKIE['user_level'] == '1') {
+      $sql = "DELETE FROM ".$prefix."_themes WHERE id='$HTTP_GET_VARS[id]'";
+      $result = $db->query($sql);
+
+      if(!$result) {
+         include("page_header.php");
+         $display = "Could not delete the theme.";
+         $template->getFile(array(
+                            'error' => 'admin/error.tpl')
+         );
+         $template->add_vars(array(
+                            'L_ERROR' => $lang['error'],
+                            'DISPLAY' => $display)
+         );
+         $template->parse("error");
+         include("page_footer.php");
+         exit();
+      } else {
+         include("page_header.php");
+         $display = "The Theme has been deleted.";
+         $link = "theme.php";
+         $template->getFile(array(
+                            'success' => 'admin/success.tpl')
+         );
+         $template->add_vars(array(
+                            'L_SUCCESS' => $lang['success'],
+                            'DISPLAY' => $display,
+                            'LINK' => $link)
+         );
+         $template->parse("success");
+         include("page_footer.php");
+         exit();
+      }
+   } else {
+      include('page_header.php');
+      $display = "You do not have permission to view this page.";
+      $template->getFile(array(
+                         'error' => 'admin/error.tpl')
+      );
+      $template->add_vars(array(
+                         'L_ERROR' => $lang['error'],
+                         'DISPLAY' => $display)
+      );
+      $template->parse("error");
+      include('page_footer.php');
+      exit();
+   }
+} else {
+      include('page_header.php');
+      $display = "You are not logged in. Please do so <a href=\"../admin.php\">Here</a>.";
+      $template->getFile(array(
+                         'error' => 'admin/error.tpl')
+      );
+      $template->add_vars(array( 
+                         'L_ERROR' => $lang['error'],
+                         'DISPLAY' => $display)
+      );
+      $template->parse("error");
+      include('page_footer.php');
+      exit();
+}
+?>
