3509253628378eb96d5df8333f29d988f5a79080
me-suzy <me.suzana@gmail.com>
2025-08-12 17:31:08 +0300

    Add Guestbooks/guestbook/guestbook/admin/message_edited.php

---
+++ Guestbooks/guestbook/guestbook/admin/message_edited.php
@@ -0,0 +1,110 @@
+<?php
+/****************************************************************/
+/*                       phphg Guestbook                        */
+/*                admin/message_edited.php file                 */
+/*                      (c)copyright 2003                       */
+/*                       By hinton design                       */
+/*                 http://www.hintondesign.org                  */
+/*                  support@hintondesign.org                    */
+/*                                                              */
+/* This program is free software. You can redistrabute it and/or*/
+/* modify it under the terms of the GNU General Public Licence  */
+/* as published by the Free Software Foundation; either version */
+/* 2 of the license.                                            */
+/*                                                              */
+/****************************************************************/
+$phphg_real_path = "./../";
+include($phphg_real_path . 'common.php');
+
+if($HTTP_COOKIE_VARS['loged'] == 'yes') {
+   if($HTTP_COOKIE_VARS['user_level'] == '1') {
+      if((!$HTTP_POST_VARS['email']) || (!$HTTP_POST_VARS['location']) || (!$HTTP_POST_VARS['message'])) {
+          include("page_header.php");
+          $display = "You didnt fill in the required fields.";
+          $template->getFile(array(
+                             'error' => 'admin/error.tpl')
+          );
+          $template->add_vars(array(
+                             'L_ERROR' => $lang['error'],
+                             'DISPLAY' => $display)
+          );
+          $template->parse("error");
+          include("page_footer.php");
+          exit();
+      }
+
+      if(!eregi("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,3})$", $HTTP_POST_VARS[email])) {
+         include("page_header.php");
+         $display = "That is not a valid email address.";
+         $template->getFile(array(
+                            'error' => 'admin/error.tpl')
+         );
+         $template->add_vars(array(
+                            'L_ERROR' => $lang['error'],
+                            'DISPLAY' => $display)
+         );
+         $template->parse("error");
+         include("page_footer.php");
+         exit();
+      }
+
+      $sql = "UPDATE ".$prefix."_message SET email='$HTTP_POST_VARS[email]', website='$HTTP_POST_VARS[website]', location='$HTTP_POST_VARS[location]', message='$HTTP_POST_VARS[message]' WHERE id='$HTTP_POST_VARS[id]'";
+      $result = $db->query($sql);
+
+      if(!$result) {
+         include("page_header.php");
+         $display = "Could not update the message";
+         $template->getFile(array(
+                            'error' => 'admin/error.tpl')
+         );
+         $template->add_vars(array(
+                            'L_ERROR' => $lang['error'],
+                            'DISPLAY' => $display)
+         );
+         $template->parse("error");
+         include("page_footer.php");
+         exit();
+      } else {
+         include("page_header.php");
+         $display = "The message has been updated.";
+         $link = "message.php";
+         $template->getFile(array(
+                            'success' => 'admin/success.tpl')
+         );
+         $template->add_vars(array(
+                            'L_SUCCESS' => $lang['success'],
+                            'DISPLAY' => $display,
+                            'LINK' => $link)
+         );
+         $template->parse("success");
+         exit();
+      }
+   } else {
+      include('page_header.php');
+      $display = "You do not have permission to view this page.";
+      $template->getFile(array(
+                         'error' => 'admin/error.tpl')
+      );
+      $template->add_vars(array(
+                         'L_ERROR' => $lang['error'],
+                         'DISPLAY' => $display)
+      );
+      $template->parse("error");
+      include('page_footer.php');
+      exit();
+   }
+} else {
+      include('page_header.php');
+      $display = "You are not logged in. Please do so <a href=\"../admin.php\">Here</a>.";
+      $template->getFile(array(
+                         'error' => 'admin/error.tpl')
+      );
+      $template->add_vars(array(
+                         'L_ERROR' => $lang['error'],
+                         'DISPLAY' => $display)
+      );
+      $template->parse("error");
+      include('page_footer.php');
+      exit();
+}
+?>    
