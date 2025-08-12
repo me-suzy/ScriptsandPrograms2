a21dba0167c26bf7c99beed7ac6111dc34b8303e
me-suzy <me.suzana@gmail.com>
2025-08-13 00:37:36 +0300

    Add Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/style/xpsilver1024/template/at_ctrl.php

---
+++ Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/style/xpsilver1024/template/at_ctrl.php
@@ -0,0 +1,86 @@
+<?php
+$top=<<<TOP
+<table width=975 border=0 cellspacing=0 cellpadding=0 align=center class=bgcol>
+
+<tr height=29>
+<td width=7><img width=7 height=29 border=0 src="%%RF%%style/%%STYLE%%/image/ttopl.gif"></td>
+<td width=961 background="%%RF%%style/%%STYLE%%/image/ttop.gif">
+<table width=961 border=0 cellspacing=0 cellpadding=0>
+<tr>
+<td><div class=tframe>%%HEADER%%</div></td>
+
+TOP;
+
+$button=<<<BUTTON
+<td width=21><div class=vstext><a href="%%FOLDER%%"><img width=21 height=21 src="%%RF%%style/%%STYLE%%/image/%%MODULE%%.gif" title="%%TITLE%%" border=0></a></div></td>
+BUTTON;
+
+
+$top2=<<<TOP2
+</tr>
+</table>
+</td>
+<td width=7><img width=7 height=29 border=0 src="%%RF%%style/%%STYLE%%/image/ttopr.gif"></td>
+</tr>
+
+<tr>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenl.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+<td width=961><div class=tsubtitle>%%CTIME%%</div></td>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenr.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+</tr>
+
+<tr height=20 valign=middle>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenl.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+<td width=961>
+<div class=tareawsubt>
+<div class=tborder>
+<table border=0 cellspacing=0 cellpadding=0 class=astable>
+<tr>
+<td>%%NAME%%</td>
+<td><select TABINDEX=1 name="act" class=listarea>
+
+TOP2;
+
+$opt=<<<OPT
+<option value=%%VALUE%%%%SELECTED%%>%%NAME%%</option>
+
+OPT;
+
+$bottom=<<<BOTTOM
+</select>
+</td>
+</tr>
+</table>
+</div>
+</div>
+</td>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenr.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+</tr>
+
+<tr height=21 valign=middle>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenl.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+<td>
+<div class=tbarea>
+<table border=0 cellspacing=0 cellpadding=0 align=center>
+<tr height=21>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bleft.gif"></td>
+<td background="%%RF%%style/%%STYLE%%/image/bcen.gif"><div class=tbutton><a TABINDEX=2 href="%%SCRIPT%%.php" onclick='return false'><span onclick='CtrlSel(%%SCRIPT%%)'>%%SUBMIT%%</span></a></div></td>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bright.gif"></td>
+</tr>
+</table>
+</div>
+</td>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenr.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+</tr>
+
+<tr height=3>
+<td width=7><img width=7 height=3 border=0 src="%%RF%%style/%%STYLE%%/image/tbotl.gif"></td>
+<td height=3 width=961 background="%%RF%%style/%%STYLE%%/image/tbot.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=3></td>
+<td width=7><img width=7 height=3 border=0 src="%%RF%%style/%%STYLE%%/image/tbotr.gif"></td>
+</tr>
+
+</table>
+<br>
+
+BOTTOM;
+?>
