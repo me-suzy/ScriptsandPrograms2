d80b4561d5b03b4d51145f7721dde2a711ffb54a
me-suzy <me.suzana@gmail.com>
2025-08-12 11:19:31 +0300

    Add E-Commerce/coupon_demo/coupon/admin/index.php

---
+++ E-Commerce/coupon_demo/coupon/admin/index.php
@@ -0,0 +1,4 @@
+<?PHP 
+/* WARNING: This script is protected. Any attempt to reverse engineer, debug or de-code this file or its dependent files is strictly prohibited */
+$codelock_enc="admin_l.php"; $codelock_file="index.php"; $codelock_filed=dirname(__FILE__); include("admin_l.php"); return; ?>
+C3CxSE8MN81K8gjLVgl2zIwMDypJDDcpTXbPKPfMKk4PqDKxBQA=
