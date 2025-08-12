e465d84c29c835aa43dff0060abdfe91604ac618
me-suzy <me.suzana@gmail.com>
2025-08-12 19:51:04 +0300

    Add News Publishing/NewMan147/NewMan147/newman/languages/lang-french.php

---
+++ News Publishing/NewMan147/NewMan147/newman/languages/lang-french.php
@@ -0,0 +1,119 @@
+<?php
+  //******************************************************************************************
+  //**                                                                                      **
+  //** phpNewsManager v1.30                                                                 **
+  //** contact: gregor@klevze.si                                                            **
+  //** Last edited: 10th.June,2002                                                          **
+  //**                                                                                      **
+  //** Language module: French                                                             **
+  //******************************************************************************************
+
+define("_CHARSET","ISO-8859-1");
+define("_STATISTICS","Statistiques");
+define("_ACTUAL","Actuel");
+define("_LASTVISIT","Dernière visite");
+define("_NEW","Nouveau");
+define("_ONLINENEWS","News En Ligne");
+define("_WEEKLYPOLLQ","Questions aux Sondages");
+define("_WEEKLYPOLLA","Réponses aux Sondages");
+define("_PUBLICNEWS","News Publiques");
+define("_REGISTEREDUSERS","Utilisateurs Enregistrés");
+define("_NEWSCOMMENTS","Commentaires");
+define("_ADDNEWS","Ajouter une News");
+define("_SUBMITEDNEWS","News Proposées");
+define("_OPTIONS","Options");
+define("_NEWSHEADLINE","Ligne de Titre");
+define("_AUTHOR","Auteur");
+define("_DATE","Date");
+define("_SECTION","Section");
+define("_PREVIEW","Prévisualisation");
+define("_MESSAGE","Message");
+define("_SUBMIT","Envoyer");
+define("_HOME","Accueil");
+define("_NEWSMANAGER","Menu des News");
+define("_MODIFYNEWS","Modifier la News");
+define("_ADDNEWS","Ajouter une News");
+define("_ADDPARTNERS","Ajouter un Partenaire");
+define("_UPLOADLOGO","Uploader un Logo");
+define("_IMAGE","Image");
+define("_NAME","Nom");
+define("_NUMBER","#");define("_","#");
+define("_CATEGORYNAME","Nom de Catégorie");
+define("_MAINMENU","Menu Principal");
+define("_KEEPIT","Garder!");
+define("_DELETEIT","Supprimer!");
+define("_EDITCATEGORY","Editer la Catégorie");
+define("_UPLOADPICTURE","Envoyer une Image");
+define("_ADDCATEGORY","Ajouter une Catégorie");
+define("_OURPARTNERS","Nos Partenaires");
+define("_SELECTPAGE","Sélectionner la Page");
+define("_LINK","Lien");
+define("_IN","Entrées");
+define("_OUT","Sorties");
+define("_AFFILIATES","Affiliés");
+define("_GFX","Gfx");
+define("_PREVIOUS","Précédent");
+define("_NEXT","Suivant");
+define("_DESCRIPTION","Description");
+define("_MODIFYPARTNERS","Modifier les Partenaires");
+define("_RSSSETTINGS","Configuration RSS");
+define("_NAMEOFRSSFILE","Nom du fichier RSS");
+define("_NUMBEROFNEWS","Nombre de news");
+define("_RSSTITLE","Titre du RSS");
+define("_RSSLINK","Lien du RSS");
+define("_RSSDESCRIPTION","Description du RSS");
+define("_AUTOCREATERSS","Créer automatiquement un fichier RSS, après chaque modification des news");
+define("_OPTIMIZED","Optimisé");
+define("_OPTIMIZEDATABASE","Optimiser la Base de Données");
+define("_ADDSMILEY","Ajouter un Smiley");
+define("_UPLOADSMILEY","Uploader un Smiley");
+define("_SMILEYEMOTION","Expression");
+define("_SMILEYEDITOR","Editeur de Smiley");
+define("_SMILEYCODE","Code");
+define("_EDITSMILEY","Editer le Smiley");
+define("_DELETESMILEY","Supprimer le Smiley");
+define("_NEWS","News");
+define("_CATEGORY","Catégories");
+define("_PARTNERS","Partenaires");
+define("_WEEKLYPOLL","Sondages");
+define("_SMILEYS","Smileys");
+define("_BROWSENEWS","Parcourir les News");
+define("_GROUPS","Groupes");
+define("_ADMINS","Admins");
+define("_USERS","Utilisateurs");
+define("_USERINFO","Info de l'Utilisateur");
+define("_ADMINISTRATION","Administration");
+define("_YOULOGOUT","Vous venez de vous déconnecter.");
+define("_LOGIN","Connexion");
+define("_PASSWORD","Mot de Passe");
+define("_USERNAME","Utilisateur");
+define("_WEBCONTROLPANEL","PANNEAU DE CONFIGURATION");
+define("_ADDUSER","Ajouter un Utilisateur");
+define("_EMAIL","Email");
+define("_PRIVILEGES","Privilèges");
+define("_EMAIL","Email");
+define("_EDITUSER","Editer l'Utilisateur");
+define("_DELETEUSER","Supprimer l'Utilisateur");
+define("_INFO","Info");
+define("_ADDGROUP","Ajouter un Groupe");
+define("_ADMIN","Admin");
+define("_RSS","RSS");
+define("_MODIFY","Modifier");
+define("_ADD","Ajouter");
+define("_EDIT","Editer");
+define("_DELETE","Supprimer");
+define("_UPLOAD","Uploader");
+define("_GROUPNAME","Nom de Goupe");
+define("_SELECT","Sélectionner");
+define("_ADDWEEKLYPOLL","Ajouter un Sondage");
+define("_SUBMITEDPOLLS","Sondages proposés");
+define("_QUESTION","Question");
+define("_ANSWERS","Réponses");
+define("_SEPERATEANSWERSWITH","Séparer les réponses par ");
+define("_EDITWEEKLYPOLL","Editer le Sondage");
+define("_SUCCESS","Réussite");
+define("_DELETENEWS","Supprimer la News");
+define("_NOTENOUGHPRIV","Vous n'avez pas assez de permissions!");
+define("_LANGUAGE","Langage");
+define("_CONTENT","Contenu");
+?>
