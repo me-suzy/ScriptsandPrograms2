64b9a9462bf18e0d248cf87f930fdebacf641478
me-suzy <me.suzana@gmail.com>
2025-08-12 13:18:23 +0300

    Add Education/tcexam-demo/index.php

---
+++ Education/tcexam-demo/index.php
@@ -0,0 +1,25 @@
+<?php
+//============================================================+
+// File name   : index.php                                 
+// Begin       : 2002-01-11                                    
+// Last Update : 2003-04-24                                    
+//                                                             
+// Description : Redirect browser to main page     
+//                                                             
+// Author: Nicola Asuni                                        
+//                                                             
+// (c) Copyright:                                              
+//               Tecnick.com S.r.l.                            
+//               Via Ugo Foscolo n.19                          
+//               09045 Quartu Sant'Elena (CA)                  
+//               ITALY                                         
+//               www.tecnick.com                               
+//               info@tecnick.com                              
+//============================================================+
+
+header("Location: public/code/index.php"); //redirect browser to public main page
+
+//============================================================+
+// END OF FILE                                                 
+//============================================================+
+?>
