394a696e59f24bd5314562746bbab51d5b40ac10
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:31 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/lang_links_en.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/lang_links_en.php
@@ -0,0 +1,38 @@
+<?php
+
+// Form Titles
+$GLOBALS["tFormTitle"]		= 'Maintain Links';
+$GLOBALS["tSubmitFormTitle"]	= 'Submit Link';
+
+// List Headings
+$GLOBALS["tPostedBy"]		= 'Posted by';
+$GLOBALS["tPublishDate"]	= 'Publish Date';
+$GLOBALS["tStatus"]		= 'Status';
+
+// List Functions
+$GLOBALS["tAddNewLink"]	= 'Add link';
+$GLOBALS["tEditLink"]	= 'Edit link';
+$GLOBALS["tDeleteLink"]	= 'Delete link';
+$GLOBALS["tReleaseLink"]	= 'Release link';
+$GLOBALS["tEditExtraSettings"] = "Edit extra link settings";
+
+// Form Field Headings
+$GLOBALS["tCategory"] 		= 'Category';
+$GLOBALS["tWebURL"]			= 'Website URL';
+$GLOBALS["tDescription"]	= 'Description';
+$GLOBALS["tSubmitLink"]		= 'Submit Link';
+
+// Link Submission Texts
+$GLOBALS["tThankYou"]		= 'Thank you for submitting your link.';
+$GLOBALS["tReleaseRequired"]	= 'Note that it will not be displayed on the links page until it has been released by the board editor.';
+$GLOBALS["tClickToReturn"]	= 'Click here to return to the';
+$GLOBALS["tLinksPage"]	= 'links page';
+
+// Security Texts
+$GLOBALS["tMustLogin"]		= 'You must';
+$GLOBALS["tLogin"]		= 'log in';
+$GLOBALS["tToSubmit"]		= 'to submit a Link';
+
+//Validation
+$GLOBALS["eWebURLEmpty"]	= "The link field can not be left empty";
+?>
