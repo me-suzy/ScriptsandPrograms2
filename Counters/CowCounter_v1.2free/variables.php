5910659c36acc133b23f2b89778f1a042b8324c7
me-suzy <me.suzana@gmail.com>
2025-08-12 08:31:54 +0300

    Add Counters/CowCounter_v1.2free/variables.php

---
+++ Counters/CowCounter_v1.2free/variables.php
@@ -0,0 +1,9 @@
+<?
+
+//This variable tells CowCounter what Counter Names are avalible and what are not.  This is here so that nobody comes and starts to create their own counters using your site.  Make sure that you say here what names are valid so that CowCounter knows that its real.  These names are CASE INSENSITIVE.
+
+$Enabled_Counters = array("CowMail", "MainPage");
+
+
+
+?>
