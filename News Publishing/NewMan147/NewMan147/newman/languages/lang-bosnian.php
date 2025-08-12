b56d4ec95460a541c3d963c8d5cbe7b6a0f699b6
me-suzy <me.suzana@gmail.com>
2025-08-12 19:50:55 +0300

    Add News Publishing/NewMan147/NewMan147/newman/languages/lang-bosnian.php

---
+++ News Publishing/NewMan147/NewMan147/newman/languages/lang-bosnian.php
@@ -0,0 +1,119 @@
+<?php
+  //******************************************************************************************
+  //**                                                                                      **
+  //** phpNewsManager v1.30                                                                 **
+  //** contact: gregor@klevze.si                                                            **
+  //** Last edited: 10th.June,2002                                                          **
+  //**                                                                                      **
+  //** Language module: Bosnian/Croatia/Serbian                                             **
+  //******************************************************************************************
+
+define("_CHARSET","ISO-8859-1");
+define("_STATISTICS","Statistike");
+define("_ACTUAL","Actual");
+define("_LASTVISIT","Posljednja posjeta");
+define("_NEW","Novo");
+define("_ONLINENEWS","Online Vijesti");
+define("_WEEKLYPOLLQ","Sedmicna pitanja za Anketu");
+define("_WEEKLYPOLLA","Sedmicni odgovori za Anketu");
+define("_PUBLICNEWS","Javne vijesti");
+define("_REGISTEREDUSERS","Registrvanih korisnika");
+define("_NEWSCOMMENTS","Komentari na vijesti");
+define("_ADDNEWS","Dodaj vijest");
+define("_SUBMITEDNEWS","Poslana vijest");
+define("_OPTIONS","Opcije");
+define("_NEWSHEADLINE","Naslov vijesti");
+define("_AUTHOR","Autor");
+define("_DATE","Datum");
+define("_SECTION","Sekcija");
+define("_PREVIEW","Pregled");
+define("_MESSAGE","Poruka");
+define("_SUBMIT","Salji");
+define("_HOME","Home");
+define("_NEWSMANAGER","Meni za vijesti");
+define("_MODIFYNEWS","Modifikujte vijest");
+define("_ADDNEWS","Dodaj vijest");
+define("_ADDPARTNERS","Dodaj Partnera");
+define("_UPLOADLOGO","Salji logo");
+define("_IMAGE","Slika");
+define("_NAME","Ime");
+define("_NUMBER","#");define("_","#");
+define("_CATEGORYNAME","Ime kategorije");
+define("_MAINMENU","Glavni meni");
+define("_KEEPIT","Sacuvaj!");
+define("_DELETEIT","Izbrisi!");
+define("_EDITCATEGORY","Uredi kategoriju");
+define("_UPLOADPICTURE","Salji sliku");
+define("_ADDCATEGORY","Dodaj kategoriju");
+define("_OURPARTNERS","Nasi Partneri");
+define("_SELECTPAGE","Odaberi stranicu");
+define("_LINK","Link");
+define("_IN","U");
+define("_OUT","Van");
+define("_AFFILIATES","Clanovi");
+define("_GFX","Gfx");
+define("_PREVIOUS","Prethodni");
+define("_NEXT","Sljedeci");
+define("_DESCRIPTION","Opis");
+define("_MODIFYPARTNERS","Uredi Partnere");
+define("_RSSSETTINGS","RSS Namjestanja");
+define("_NAMEOFRSSFILE","Ime RSS datoteke");
+define("_NUMBEROFNEWS","Broj vijesti");
+define("_RSSTITLE","RSS Naslov");
+define("_RSSLINK","RSS Link");
+define("_RSSDESCRIPTION","RSS Opis");
+define("_AUTOCREATERSS","Automatski kreiraj RSS datoteku, nakon uredzivanja svake vijesti");
+define("_OPTIMIZED","Optimizirano");
+define("_OPTIMIZEDATABASE","Optimiziraj bazu podataka");
+define("_ADDSMILEY","Dodaj Smiley(Smjesak)");
+define("_UPLOADSMILEY","Posalji Smiley(Smjesak)");
+define("_SMILEYEMOTION","Emocija");
+define("_SMILEYEDITOR","Smiley(Smjesak) uredzivac");
+define("_SMILEYCODE","Kod");
+define("_EDITSMILEY","Uredi Smiley(Smjesak)");
+define("_DELETESMILEY","Izbrisi Smiley(Smjesak)");
+define("_NEWS","Vijesti");
+define("_CATEGORY","Kategorije");
+define("_PARTNERS","Partneri");
+define("_WEEKLYPOLL","Sedmicna anketa");
+define("_SMILEYS","Smileyi(Smjesci)");
+define("_BROWSENEWS","Pregledaj vijesti");
+define("_GROUPS","Grupe");
+define("_ADMINS","Administratori");
+define("_USERS","Korisnici");
+define("_USERINFO","Informacije o korisniku");
+define("_ADMINISTRATION","Administracija");
+define("_YOULOGOUT","Upravo ste izasli sa vaseg racuna.");
+define("_LOGIN","Ulaz");
+define("_PASSWORD","Sifra");
+define("_USERNAME","Korisnicko ime");
+define("_WEBCONTROLPANEL","WEB KONTROLNO SUCELJE");
+define("_ADDUSER","Dodaj korisnika");
+define("_EMAIL","Email");
+define("_PRIVILEGES","Privilegije");
+define("_EMAIL","Email");
+define("_EDITUSER","Uredi korisnika");
+define("_DELETEUSER","Izbrisi korisnika");
+define("_INFO","Informacije");
+define("_ADDGROUP","Dodaj Grupu");
+define("_ADMIN","Administrator");
+define("_RSS","RSS");
+define("_MODIFY","Modifikujte");
+define("_ADD","Dodaj");
+define("_EDIT","Uredi");
+define("_DELETE","Izbrisi");
+define("_UPLOAD","Posalji");
+define("_GROUPNAME","GIme grupe");
+define("_SELECT","Odaberi");
+define("_ADDWEEKLYPOLL","Dodaj sedmicnu anketu");
+define("_SUBMITEDPOLLS","Dosadasnje ankete");
+define("_QUESTION","Pitanje");
+define("_ANSWERS","Odgovor");
+define("_SEPERATEANSWERSWITH","Odvoji odgovore sa");
+define("_EDITWEEKLYPOLL","Uredi sedmicnu anketu");
+define("_SUCCESS","Uspjesno");
+define("_DELETENEWS","Izbrisi vijest");
+define("_NOTENOUGHPRIV","Vi nemate ovlastenje za to!");
+define("_LANGUAGE","Jezik");
+define("_CONTENT","Sadrzaj");
+?>
