d037a026eea615902adef1c94a5047bf7b30a7bc
me-suzy <me.suzana@gmail.com>
2025-08-12 10:37:55 +0300

    Add Discussion Boards/forum/forum/admin_.php

---
+++ Discussion Boards/forum/forum/admin_.php
@@ -0,0 +1,18 @@
+<?php
+
+
+echo ("<form method=\"post\" action=\"\">");
+table_header("");
+  echo ("
+<table border=\"0\" cellspacing=\"1\" cellpadding=\"3\" width=\"$fwidth\" align=\"center\" bgcolor=\"$tborder_color2\">
+  <tr>
+     <td  colspan=\"1\" width=\"100%\"><font size=\"$fsmall\" color=\"$fcolor\" face=\"$fface\">
+      content here
+    </td>
+  </tr>
+</table>");
+table_footer();
+
+echo ("</form>");
+
+?>
