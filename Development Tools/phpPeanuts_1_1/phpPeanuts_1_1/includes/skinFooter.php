01f4a87282fb91e79be16967eb776dfea0b5e07a
me-suzy <me.suzana@gmail.com>
2025-08-12 10:07:51 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/includes/skinFooter.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/includes/skinFooter.php
@@ -0,0 +1,30 @@
+  </td>
+</tr>
+
+<tr height=1 bgcolor=purple>
+  <td valign=top height=1>   
+  </td>
+</tr>
+<tr height=1 bgcolor=#FF9933>
+  <td valign=top height=1>   
+  </td>
+</tr>
+<tr>
+  <td valign=bottom height=10> <div align=right><a class=pntFooter href='http://www.phppeanuts.org'>powered by peanuts</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div> 
+  </td>
+</tr>
+<tr height=1 bgcolor=#FF9933>
+  <td valign=top height=1>   
+  </td>
+</tr>
+<tr height=1 bgcolor=purple>
+  <td valign=top height=1>   
+  </td>
+</tr>
+
+</table>
+<?php global $queryCount;
+	print ("<!-- $queryCount queries executed, page executed in ".$this->getRequestDuration()." seconds -->"); ?>
+
+</body>
+</html>
