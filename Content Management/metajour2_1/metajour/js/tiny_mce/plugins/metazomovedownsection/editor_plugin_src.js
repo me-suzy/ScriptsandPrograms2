f609cba97ad8ec5732b16058fda45952e51e8bb1
me-suzy <me.suzana@gmail.com>
2025-08-12 06:44:37 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/metazomovedownsection/editor_plugin_src.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/metazomovedownsection/editor_plugin_src.js
@@ -0,0 +1,44 @@
+/* Import plugin specific language pack */
+tinyMCE.importPluginLanguagePack('metazomovedownsection', 'en');
+
+/**
+ * Returns the HTML contents of the save control.
+ */
+function TinyMCE_metazomovedownsection_getControlHTML(control_name) {
+	switch (control_name) {
+		case "metazomovedownsection":
+			return '<img id="{$editor_id}_metazomovedownsection" src="{$pluginurl}/images/metazomovedownsection.gif" title="{$lang_metazomovedownsection_desc}" width="20" height="20" class="mceButtonNormal" onmouseover="tinyMCE.switchClass(this,\'mceButtonOver\');" onmouseout="tinyMCE.switchClass(this,\'mceButtonNormal\');" onmousedown="tinyMCE.switchClass(this,\'mceButtonDown\');" onclick="tinyMCE.execInstanceCommand(\'{$editor_id}\',\'mceMetazoMoveDownSection\');" />';
+	}
+	return "";
+}
+
+/**
+ * Executes the save command.
+ */
+function TinyMCE_metazomovedownsection_execCommand(editor_id, element, command, user_interface, value) {
+	// Handle commands
+	switch (command) {
+		case "mceMetazoMoveDownSection":
+			var formObj = tinyMCE.selectedInstance.formElement.form;
+
+			if (formObj) {
+				tinyMCE.triggerSave();
+
+				// Disable all UI form elements that TinyMCE created
+				for (var i=0; i<formObj.elements.length; i++) {
+					var elementId = formObj.elements[i].name ? formObj.elements[i].name : formObj.elements[i].id;
+
+					if (elementId.indexOf('mce_editor_') == 0)
+						formObj.elements[i].disabled = true;
+				}
+
+				formObj.cmd.value = formObj.cmd.value + ',movedown';
+				tinyMCE.selectedInstance.formElement.form.submit();
+			} else
+				alert("Error: No form element found.");
+
+			return true;
+	}
+	// Pass to next handler in chain
+	return false;
+}
