fe38aacb08230017c4f166d0548abfabd50f6cae
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:57 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_sitemap_71.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_sitemap_71.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 71;
+function objfile_71 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Sitemap";
+$objdata['subname'] = "";
+$objdata['extension'] = "sitemap";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
