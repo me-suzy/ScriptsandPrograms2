db14f40dda3d98bd87c5eb512ea0cf2dc395032c
me-suzy <me.suzana@gmail.com>
2025-08-12 21:32:29 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/add.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/add.lang.php
@@ -0,0 +1,62 @@
+<?php
+
+// Head titles
+$tpl->assign("lang_head_artist", "Artist Information");
+$tpl->assign("lang_head_comic", "Comic Information");
+$tpl->assign("lang_head_addartist", "Add Artist");
+$tpl->assign("lang_head_addseries", "Add Series");
+$tpl->assign("lang_head_addpublisher", "Add Publisher");
+$tpl->assign("lang_head_addgenre", "Add Genre");
+$tpl->assign("lang_head_other", "Other Information");
+$tpl->assign("lang_head_storyline", "Comic Storyline");
+$tpl->assign("lang_head_addmulti", "Add Multiple Issues");
+
+// Artist Types
+$tpl->assign("lang_type_writer", "Writer");
+$tpl->assign("lang_type_letterer", "Letterer");
+$tpl->assign("lang_type_colorist", "Colorist");
+$tpl->assign("lang_type_penciler", "Penciler");
+$tpl->assign("lang_type_inker", "Inker");
+$tpl->assign("lang_type_cover", "Cover Artist");
+
+// Add artist
+$tpl->assign("lang_add_artist", "Name of new artist");
+$tpl->assign("lang_add_publisher", "Name of new publisher");
+$tpl->assign("lang_add_genre", "Name of new genre");
+$tpl->assign("lang_add_year", "Year");
+$tpl->assign("lang_add_series", "Name of new series");
+$tpl->assign("lang_url_series", "URL to new series");
+$tpl->assign("lang_url_publisher", "URL to new publisher");
+$tpl->assign("lang_select_artisttype", "Select one or more artist roles");
+
+// Add Comic
+$tpl->assign("lang_name_series", "Name of series");
+$tpl->assign("lang_publisher", "Publisher");
+$tpl->assign("lang_story", "Story title");
+$tpl->assign("lang_quantity", "Quantity");
+$tpl->assign("lang_issue_volume", "Comic issue & volume");
+$tpl->assign("lang_add_volume", "Volume");
+$tpl->assign("lang_miniseries", "Number of issues in miniseries");
+$tpl->assign("lang_type", "Comic type");
+$tpl->assign("lang_format", "Comic Format");
+$tpl->assign("lang_condition", "Issue condition");
+$tpl->assign("lang_variation", "Issue Variation");
+$tpl->assign("lang_genre", "Comic genre");
+$tpl->assign("lang_pubdate", "Publication Date");
+$tpl->assign("lang_currency", "Choose a currency for this issue");
+$tpl->assign("lang_price_value", "Issue cover price and value");
+$tpl->assign("lang_language", "Issue language");
+$tpl->assign("lang_translator", "Language translator");
+$tpl->assign("lang_translator_nfo", "If any, or else leave blank");
+$tpl->assign("lang_useroption", "User option");
+$tpl->assign("lang_userinput", "User input");
+$tpl->assign("lang_coverimage", "Cover image");
+$tpl->assign("lang_coverimage_nfo", "Use the whole filename; Ex: image.jpg");
+$tpl->assign("lang_ebay_link", "Ebay link to this comic");
+$tpl->assign("lang_enable_ebay", "Add an ebay link?");
+$tpl->assign("lang_multi_startend", "Enter start and end issue");
+$tpl->assign("lang_multi_startend_nfo", "Existing issues will be skipped");
+$tpl->assign("lang_ebay_yes", "Yes");
+$tpl->assign("lang_ebay_no", "No");
+
+?>
