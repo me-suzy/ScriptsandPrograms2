a6919ea374bc8a4e1d27625ad9d95215b9d7043c
me-suzy <me.suzana@gmail.com>
2025-08-12 18:10:48 +0300

    Add Image Galleries/PHP_JK_Aug_01_2005/fckeditor/editor/_source/globals/fckeditorapi.js

---
+++ Image Galleries/PHP_JK_Aug_01_2005/fckeditor/editor/_source/globals/fckeditorapi.js
@@ -0,0 +1,43 @@
+﻿/*
+ * fckeditor - The text editor for internet
+ * Copyright (C) 2003-2005 Frederico Caldeira Knabben
+ * 
+ * Licensed under the terms of the GNU Lesser General Public License:
+ * 		http://www.opensource.org/licenses/lgpl-license.php
+ * 
+ * For further information visit:
+ * 		http://www.fckeditor.net/
+ * 
+ * File Name: fckeditorapi.js
+ * 	Create the fckeditorAPI object that is available as a global object in
+ * 	the page where the editor is placed in.
+ * 
+ * File Authors:
+ * 		Frederico Caldeira Knabben (fredck@fckeditor.net)
+ */
+
+var fckeditorAPI ;
+
+function fckeditorAPI_GetInstance( instanceName )
+{
+	return this.__Instances[ instanceName ] ;
+}
+
+if ( !window.parent.fckeditorAPI )
+{
+	// Make the fckeditorAPI object available in the parent window.
+	fckeditorAPI = window.parent.fckeditorAPI = new Object() ;
+	fckeditorAPI.__Instances = new Object() ;
+
+	// Set the current version.
+	fckeditorAPI.Version = '2.0 FC' ;
+
+	// Function used to get a instance of an existing editor present in the 
+	// page.
+	fckeditorAPI.GetInstance = fckeditorAPI_GetInstance ;
+}
+else
+	fckeditorAPI = window.parent.fckeditorAPI ;
+
+// Add the current instance to the fckeditorAPI's instances collection.
+fckeditorAPI.__Instances[ FCK.Name ] = FCK ;
