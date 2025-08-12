e3a04454a90c29b0f2db31bf30262382131e6fc3
me-suzy <me.suzana@gmail.com>
2025-08-12 03:38:17 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_siterel.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_siterel.php
@@ -0,0 +1,54 @@
+<?php
+
+/***************************************************************************
+
+ m_siterel.php
+ --------------
+ copyright : (C) 2005 - The iziContents Development Team
+
+ iziContents version : 1.0
+ fileversion : 1.0.1
+ change date : 23 - 04 - 2005
+ ***************************************************************************/
+
+/***************************************************************************
+ The iziContents Development Team offers no warranties on this script.
+ The owner/licensee of the script is solely responsible for any problems
+ caused by installation of the script or use of the script.
+
+ All copyright notices regarding iziContents and ezContents must remain intact on the scripts and in the HTML for the scripts.
+
+ For more info on iziContents,
+ visit http://www.izicontents.com*/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the License which can be found within the
+ *   zipped package. Under the licence of GPL/GNU.
+ *
+ ***************************************************************************/
+
+include_once ("rootdatapath.php");
+
+$GLOBALS["form"] = 'sites';
+$validaccess = VerifyAdminLogin();
+
+
+if ($GLOBALS["canedit"] == False) {
+   Header("Location: ".BuildLink('adminlogin.php'));
+} else {
+   ReleaseSite();
+   Header("Location: ".BuildLink('m_sites.php')."&page=".$_GET["page"]."&sort=".$_GET["sort"]);
+}
+
+function ReleaseSite()
+{
+   global $_GET;
+
+   $strQuery = "UPDATE ".$GLOBALS["eztbSites"]." SET siteenabled = 1-siteenabled WHERE sitecode='".$_GET["SiteCode"]."'";
+   $result = dbExecute($strQuery,true);
+   dbCommit();
+} // function ReleaseSite()
+
+?>
