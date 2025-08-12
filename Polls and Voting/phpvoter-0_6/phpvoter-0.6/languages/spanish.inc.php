6222f0f57100c0dbc19e12a44c2bef4c7d2ecaa7
me-suzy <me.suzana@gmail.com>
2025-08-12 22:00:13 +0300

    Add Polls and Voting/phpvoter-0_6/phpvoter-0.6/languages/spanish.inc.php

---
+++ Polls and Voting/phpvoter-0_6/phpvoter-0.6/languages/spanish.inc.php
@@ -0,0 +1,86 @@
+<?php
+
+/* ***************************** */
+/* LANGUAGE STRINGS FOR PHPVOTER */
+/* ***************************** */
+
+$lang['vote_button'] = "Vota";
+$lang['onevote'] = "voto";
+$lang['votes'] = "votos";
+$lang['result_saved'] = "Tu voto ha sido guardado.<br />\n";
+$lang['createvote'] = "Crea/Cambia la encuesta";
+$lang['question'] = "La encuesta";
+$lang['alreadyvoted'] = "Ya habias votado en esta encuesta.";
+$lang['viewresults'] = "Mostrar resultados";
+$lang['noip'] = "Tu navegador no ha establecido el REFERER asi que tu voto no ha sido guardado.<br />\n";
+$lang['notactive'] = "<h1>¡Lo sentimos!</h1>\nEsta encuesta ya no esta activa y no puede ser votada.\n";
+$lang['activequestion'] = "<b>Encuesta actual:</b><br />\n";
+$lang['totalvoters'] = "<br />Numero de votos: ";
+$lang['info'] = <<<ENDSTRING
+Estas encuestas no son cientificas en modo alguno y los resultados solo pueden ser
+interpretados como indicaciones y no como hechos reales. Si tienes peticiones para encuestas
+que podriamos incluir, envianos un <a href="mailto:oscar@putalocura.com">email</a>.
+<br /><br />
+ENDSTRING;
+$lang['sorry'] = "Lo sentimos";
+$lang['no_votes'] = "No hay votos";
+$lang['no_votes_in_db'] = "No hay encuestas en la base de datos.";
+$lang['no_active_vote'] = "No hay encuesta activa.";
+$lang['error_header'] = "A ocurrido un error";
+$lang['admin_menu'] = "Administracion del PHPVoter";
+$lang['admin_list'] = "Mostrar todas las encuestas";
+$lang['admin_create'] = "Crear una nueva encuesta";
+$lang['admin_list_header'] = "Lista de encuentas";
+$lang['voteid'] = "ID";
+$lang['question'] = "Pregunta";
+$lang['date'] = "Fecha";
+$lang['state'] = "Estado";
+$lang['answers'] = "Opciones";
+$lang['voters'] = "Votos";
+$lang['function'] = "Funcion";
+$lang['inactive'] = "Inactivo";
+$lang['active'] = "Activo";
+$lang['unfinished'] = "Inedito";
+$lang['deleted'] = "Borrado";
+$lang['showvote'] = "Mostrar/Editar";
+$lang['admin_show_vote'] = "Mostrar/Editar una encuesta";
+$lang['admin_voter_list'] = "Lista de votos";
+$lang['IP'] = "IP";
+$lang['voterid'] = "ID";
+$lang['answer'] = "Voto";
+$lang['delete'] = "Quitar";
+$lang['change'] = "Cambiar";
+$lang['choice_id'] = "ID";
+$lang['admin_delete_question'] = "Quitar esta encuesta";
+$lang['admin_change_question'] = "Cambiar esta encuesta";
+$lang['admin_vote_question'] = "Votar en esta encuesta";
+$lang['admin_set_this_active'] = "Poner activa esta encuesta";
+$lang['admin_return'] = "Volver a la administracion";
+$lang['identifier_deleted'] = "IP borrada";
+$lang['votes_updated'] = "El numero de votos ha sido actualizado.";
+$lang['votes_not_updated'] = "El numero de votos no ha sido actualizado puesto que las respuestas no estan guardadas en la base de datos.";
+$lang['vote_activated'] = "La encuesta ha sido activada.";
+$lang['one_active_vote'] = "Solo se permite una encuesta activa, el resto quedan inactivas.";
+$lang['question_deleted'] = "La encuesta seleccionada ha sido borrada.";
+$lang['voters_deleted'] = "La lista de votantes ha sido borrada.";
+$lang['comment'] = "Comentario a la votacion (sera mostrado en lista completa y en la pagina de resultados)";
+$lang['savequestion'] = "Guardar votacion / Añadir nueva opcion";
+$lang['new_answer'] = "Nuevo";
+$lang['save_question'] = "Guardar/Cambiar encuesta";
+$lang['answer_deleted'] = "La opcion ha sido borrada";
+$lang['question_updated'] = "La votacio ha sido actualizada.";
+$lang['error_incorrect_values'] = "Uno o mas valores han sido introducidos incorrectamente o se han perdido, nada ha sido guardado.";
+$lang['error_access_denied'] = "¡Acceso denegado!";
+$lang['publish'] = "Fecha de publicación";
+$lang['information'] = "Información";
+$lang['admin_menu'] = "Menu";
+$lang['no_voters'] = "Nadie ha votado aun en esta encuesta.";
+$lang['question_state'] = "Estado de la votación: ";
+$lang['question_comment'] = "Comentario a la votación: ";
+$lang['vote_on_question'] = "Votar en una encuesta";
+$lang['set_active_vote'] = "Encuesta activada";
+$lang['error_sql_query'] = "SQL query";
+$lang['error_sql'] = "A SQL error occured";
+$lang['error_notemplate'] = "Couldn't find the required template file.";
+
+?>
