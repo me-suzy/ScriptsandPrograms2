810887d75935d92245da1c795005d3f9863f4483
me-suzy <me.suzana@gmail.com>
2025-08-12 14:16:16 +0300

    Add File Manipulation/dl4lite/UPLOAD/lang/1/lang_ad_nav.php

---
+++ File Manipulation/dl4lite/UPLOAD/lang/1/lang_ad_nav.php
@@ -0,0 +1,13 @@
+<?php
+/*********************************************************
+* Name: lang_ad_main.php
+* Author: RW::Download 4.0
+* Contact: realworld@blazefans.com
+* Description: Language variables
+* Version: 4.00
+*********************************************************/
+$lang = array(
+
+)
+
+?>
