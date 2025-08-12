6b827231ce3fcca50203b8f42b3a21984493a367
me-suzy <me.suzana@gmail.com>
2025-08-13 02:53:59 +0300

    Add Unsorted/EasyBannerPro/join.php

---
+++ Unsorted/EasyBannerPro/join.php
@@ -0,0 +1,25 @@
+<?PHP
+
+#################################################
+##                                             ##
+##              Easy Banner Pro                ##
+##       http://www.phpwebscripts.com/         ##
+##       e-mail: info@phpwebscripts.com        ##
+##                                             ##
+##                 Version 2.8                 ##
+##             copyright (c) 2003              ##
+##                                             ##
+##  This script is not freeware nor shareware  ##
+##    Please do no distribute it by any way    ##
+##                                             ##
+#################################################
+
+include('./functions.php');
+include_once("$s[phppath]/data/messages.php");
+$s[sponsor] = 0;
+if ($HTTP_POST_VARS) user_joined($HTTP_POST_VARS);
+else page_from_template('u_join.html',$s);
+
+#####################################################################
+
+?>
