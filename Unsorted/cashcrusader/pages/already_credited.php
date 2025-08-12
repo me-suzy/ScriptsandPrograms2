eca1d0e5e5cc97144a0dd930883d06e5336421de
me-suzy <me.suzana@gmail.com>
2025-08-13 01:26:37 +0300

    Add Unsorted/cashcrusader/pages/already_credited.php

---
+++ Unsorted/cashcrusader/pages/already_credited.php
@@ -0,0 +1,31 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr> 
+<td> 
+<center>
+<b><font size="3"><br>
+<br>
+You have already visited this sponsor and <br>
+your account was credited at that time. </font> </b> 
+<form method=post>
+<center>
+<b><font size="3"> 
+<input type=hidden name=username value=LOGOUT>
+<input type=hidden name=password value='LOGOUT'>
+<input type=submit value='Log off' name="submit">
+</font> </b> 
+</center>
+</form>
+</center>
+<center>
+<b><font size="3"><br>
+<br>
+</font> </b> 
+</center>
+</td>
+</tr>
+</table>
+<br>
+<? include("footer.php");?>
+
