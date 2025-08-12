0b183844e35e23c160928c87af94075ff43616a1
me-suzy <me.suzana@gmail.com>
2025-08-12 06:13:20 +0300

    Add Content Management/metajour2_1/metajour/extension/forum/lang/forum.da.php

---
+++ Content Management/metajour2_1/metajour/extension/forum/lang/forum.da.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['label_name'] = 'Konfigurationsnavn';
+$LANG['name'] = 'Konfiguration af Forum';
+?>
