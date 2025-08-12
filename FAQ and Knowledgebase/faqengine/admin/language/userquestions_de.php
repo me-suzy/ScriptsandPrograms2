e21adad26a26f6cddae170e959d360fbfde69714
me-suzy <me.suzana@gmail.com>
2025-08-12 14:09:02 +0300

    Add FAQ and Knowledgebase/faqengine/admin/language/userquestions_de.php

---
+++ FAQ and Knowledgebase/faqengine/admin/language/userquestions_de.php
@@ -0,0 +1,12 @@
+<?php
+/***************************************************************************
+ * (c)2001-2005 Boesch IT-Consulting (info@boesch-it.de)
+ ***************************************************************************/
+$l_uq_answer_prelude = "Am {date} haben Sie folgende Frage gestellt:";
+$l_uq_answer_subject = "Antwort auf FAQ-Anfrage (#{refid})";
+$l_uq_answer_is = "Die Antwort darauf lautet:";
+$l_uq_sc_subject = "Statuswechsel der FAQ-Anfrage (#{refid})";
+$l_uq_sc_states = array("neu","in Bearbeitung","zurückgestellt","bearbeitet");
+$l_uq_sc_body = "Der Status Ihrer FAQ-Anfrage (#{refid}) vom {date} hat sich geändert.\nDer neue Status ist: {state}";
+$l_uq_dateformat = "d.m.Y H:i:s";
+?>
