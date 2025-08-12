e6659b7aa7857d42ee74f7c349d68e217ad8df32
me-suzy <me.suzana@gmail.com>
2025-08-12 11:41:41 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/affiliate_invoice.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/affiliate_invoice.php
@@ -0,0 +1,32 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Affiliate Invoice');
+
+define ('TEXT_AFFILIATE','Affiliate');
+define ('TEXT_AFFILIATE_PAYMENT','Payment');
+define ('TEXT_AFFILIATE_BILLED','Billing Day');
+define('TABLE_HEADING_ORDER_ID', 'Order ID');
+define('TABLE_HEADING_ORDER_DATE', 'Date');
+define('TABLE_HEADING_ORDER_VALUE', 'Order Value');
+define('TABLE_HEADING_COMMISSION_RATE', 'Commission Rate');
+define('TABLE_HEADING_COMMISSION_VALUE', 'Commission Value');
+define('TABLE_HEADING_TOTAL', 'Total');
+
+define('TEXT_SUB_TOTAL', 'Sub-Total');
+define('TEXT_TAX', 'Tax');
+define('TEXT_TOTAL', 'Total');
+define('ENTRY_PERCENT', '%');
+?>
