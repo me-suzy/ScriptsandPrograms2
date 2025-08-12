52c675271877c7a7737090510e69828fad344761
me-suzy <me.suzana@gmail.com>
2025-08-12 03:30:32 +0300

    Add Content Management/far_ver_1.00_full/far_ver_1.00_full/themes/blue/top.php

---
+++ Content Management/far_ver_1.00_full/far_ver_1.00_full/themes/blue/top.php
@@ -0,0 +1,11 @@
+<table width="100%"  border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td width="328" valign="top"><img src="themes/blue/images/logo.jpg" width="328" height="125"></td>
+    <td>
+<?php 
+body_far("banner");
+?>
+</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
