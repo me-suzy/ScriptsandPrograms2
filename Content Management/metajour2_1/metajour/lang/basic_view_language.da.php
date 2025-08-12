56b059b56320aabcb37c7b6e8c4ca8f5783b12c0
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:20 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_language.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_language.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Sprog';
+$LANG['text_1'] = 'Angiv sprog for de markerede poster';
+$LANG['text_2'] = 'Sprog';
+?>
