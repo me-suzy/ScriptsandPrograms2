303f4109ab954556b48f3552c2ca4ac0c54999b1
me-suzy <me.suzana@gmail.com>
2025-08-12 14:06:07 +0300

    Add FAQ and Knowledgebase/faqengine/admin/upgrade_408_to_409.php

---
+++ FAQ and Knowledgebase/faqengine/admin/upgrade_408_to_409.php
@@ -0,0 +1,20 @@
+<?php
+/***************************************************************************
+ * (c)2001-2005 Boesch IT-Consulting (info@boesch-it.de)
+ ***************************************************************************/
+require('../config.php');
+?>
+<html><body>
+<div align="center"><h3>FAQEngine: Upgrade v4.08 to v4.09</h3></div>
+<br>
+<?php
+echo "Upgrading tables...<br>";
+flush();
+$sql="ALTER TABLE ".$tableprefix."_settings ";
+$sql.="add lhide tinyint(1) unsigned NOT NULL default '0'";
+if(!$result = mysql_query($sql, $db))
+	die("Unable to upgrade table ".$tableprefix."_settings ".mysql_error());
+?>
+<br><div align="center">Upgrade done.<br>Please remove install.php, upgrade*.php, mkconfig.php and fill_*.php from server</div>
+<div align="center">Now you can login to the <a href="index.php">admininterface</a></div>
+</body></html>
