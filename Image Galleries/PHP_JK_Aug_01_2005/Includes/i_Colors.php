6f075861e0f0505b0c196224ee3b7c8ac3443229
me-suzy <me.suzana@gmail.com>
2025-08-12 18:12:35 +0300

    Add Image Galleries/PHP_JK_Aug_01_2005/Includes/i_Colors.php

---
+++ Image Galleries/PHP_JK_Aug_01_2005/Includes/i_Colors.php
@@ -0,0 +1,24 @@
+<?
+	switch ($iColorScheme) {
+		case 1:
+			require("Colors/Black.php");
+			break;
+		case 2:
+			require("Colors/White.php");
+			break;
+		case 3:
+			require("Colors/Aqua.php");
+			break;
+		case 4:
+			require("Colors/Flat_Black.php");
+			break;
+		case 5:
+			require("Colors/Flat_White.php");
+			break;
+		case 6:
+			require("Colors/Flat_Aqua.php");
+			break;
+		/* add new color scheme entries here */
+	}
+
+?>
