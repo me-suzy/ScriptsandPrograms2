dc275e28067ca04f3ea59366f9e0a7c7670696f8
me-suzy <me.suzana@gmail.com>
2025-08-12 10:39:34 +0300

    Add Discussion Boards/forumhost/index.php

---
+++ Discussion Boards/forumhost/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>ForumHost Temporary Index Page</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<div align="center"><font size="+3" face="Verdana, Arial, Helvetica, sans-serif"><br>
+  <br>
+  <br>
+  <a href="signup.php">Create Account</a><br>
+  <a href="members.php">Log In</a></font> </div>
+</body>
+</html>
