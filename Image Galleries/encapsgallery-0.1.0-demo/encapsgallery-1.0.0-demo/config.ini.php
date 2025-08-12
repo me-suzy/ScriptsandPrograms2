1bdd462666eb34f0729ec9aafc9559033f79640f
me-suzy <me.suzana@gmail.com>
2025-08-12 17:48:17 +0300

    Add Image Galleries/encapsgallery-0.1.0-demo/encapsgallery-1.0.0-demo/config.ini.php

---
+++ Image Galleries/encapsgallery-0.1.0-demo/encapsgallery-1.0.0-demo/config.ini.php
@@ -0,0 +1,12 @@
+<?php
+$config["db_host"] = "localhost";
+$config["db_name"] = "test";
+$config["db_user"] = "root";
+$config["db_pass"] = "";
+$config["db_type"] = "mysql";
+$config["theme"] = "acd";
+$config["pager_items_per_page"] = "10";
+$config["sort"] = "new";
+$config["upl_def_cat"] = "1";
+$config["action"] = "config_update";
+?>
