bb4968834e5686b16ef41b125979fbf31507b975
me-suzy <me.suzana@gmail.com>
2025-08-12 06:02:06 +0300

    Add Content Management/metajour2_1/metajour/core/badipclass.php

---
+++ Content Management/metajour2_1/metajour/core/badipclass.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage core
+ */
+
+require_once('basicclass.php');
+
+class badip extends basic {
+
+	function badip() {
+		$this->basic();
+		$this->addcolumn('name',0,UI_STRING);
+	}
+
+}
