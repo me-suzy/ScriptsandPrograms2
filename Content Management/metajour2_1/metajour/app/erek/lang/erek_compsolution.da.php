e815bc0ccde5c3a79c0314661e7e53ef59dd6c81
me-suzy <me.suzana@gmail.com>
2025-08-12 06:01:42 +0300

    Add Content Management/metajour2_1/metajour/app/erek/lang/erek_compsolution.da.php

---
+++ Content Management/metajour2_1/metajour/app/erek/lang/erek_compsolution.da.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eReklamation
+ * @subpackage lang
+ * $Id: erek_compsolution.da.php,v 1.2 2005/01/12 03:23:22 jan Exp $
+ */
+
+$LANG['name'] = 'Løsninger';
+$LANG['label_name'] = 'Tekst';
+?>
