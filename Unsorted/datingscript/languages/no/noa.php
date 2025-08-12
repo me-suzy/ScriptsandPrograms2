fcf2740c1a5a89d680f1dda32c5014dfd838a85e
me-suzy <me.suzana@gmail.com>
2025-08-13 02:21:36 +0300

    Add Unsorted/datingscript/languages/no/noa.php

---
+++ Unsorted/datingscript/languages/no/noa.php
@@ -0,0 +1,122 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.0.3                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 11/01/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               dka.php                          #
+# File purpose            Additional langfile for admin    #
+# File created by         Knut Saglien <knut@witoweb.com>  #
+############################################################
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Feil ID eller passord - prøv igjen',//1
+'Administrator område',//2
+'Admin login',//3
+'Admin passord',//4
+'Enter',//5
+'Kun adgang for administratorer',//6
+'Hovedside',//7
+'Slett hits',//8
+'Reparer&optimer',//9
+'Bruger administration',//10
+'FAQ Administration',//11
+'Mailliste',//12
+'Avslutt',//13
+'du er nå logget ut fra Admint<br>',//14
+'Totale mailer',//15
+'Sendte maile',//16
+'Den følgende del',//17
+'{0} maile er sendt til brukere',//18
+'Du har uleste mail i {0}',//19 {0} - SiteName (C_SNAME)
+'Fant {0} medlemmer',//20
+'Maillist administration',//21
+'Med status:',//22
+'Mail Muligheter og felter:',//23
+'Mail type',//24
+'Fra Mail',//25
+'Mail emne',//26
+'Mail innhold',//27
+'Bekreft om resultater',//28
+'Tilbake',//29
+'Send mailer',//30
+'De usendte sendingene - {0}',//31
+'Emne : {0} [Sendt til {1} brukere] :::>',//32
+'Avslutt sending',//33
+'Tekst',//34
+'HTML',//35
+'Forvisning',//36
+'Brukere med uleste beskjeder - ',//37
+'Totalt uleste beskjeder - ',//38
+'Siste beskjed id - ',//39
+'Send bekreftelser til brukere med uleste beskjeder',//40
+'Profil oppdatert',//41
+'FAQ Er opprettet',//42
+'Oppret ny',//43
+'Skriv spørsmål',//44
+'Skriv svar',//45
+'FAQ Administration',//46
+'Oppret ny FAQ Katagori',//47
+'Endring av FAQ Katagori',//48
+'Slett FAQ Katagori',//49
+'Opprett FAQ',//50
+'Velg FAQ Katagori',//51
+'FAQ Spørssmål',//52
+'FAQ svar',//53
+'Først skal du opprette FAQ katagorier',//54
+'Skriv katagori navn',//55
+'FAQ Katagori opprettelse',//56
+'Ny katagori opprette:',//57
+'FAQ katagori navn',//58
+'FAQ kategori edit',//59
+'Se på',//60
+'Kategorien er endret',//61
+'FAQ katagori endring',//62
+'FAQ er slettet',//63
+'FAQ Katagori slett',//64
+'Note : OBS. Når du sletter en katagori, vil du slette alle spørsmål i denne katagori.',//65
+'Slett',//66
+'0 - Vent email sjekk<br>1 - Vent. sjekk etter registrering<br>2 - Vent. Sjekk etter oppdatering<br>3,8 - Vent på tillatelse til å slette(Uten,med vis)<br>7 - Bruker er aktiv<br>',//67
+'Database er repareret og optimeret',//68
+'Bruker med ID#={0} er slettet fra databasen',//69
+'Hits er fjernet',//70
+'Tillatelse til bruker med id={0}',//71
+'IP',//72
+'Krav [0 - 255]',//73
+'Status - (PAS P)',//74
+'Tillate',//75
+'Bruker administration',//76
+'Total aktive brukere :',//77
+'Bruker venter på profil aktivering etter registrering',//78
+'Bruker venter på profil aktivering etter oppdatering',//79
+'Bruker venter på sletting',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
