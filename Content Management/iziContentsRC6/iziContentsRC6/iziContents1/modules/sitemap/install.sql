73b8e9ebac6cf03dbc0407e582cb5478a8a199c2
me-suzy <me.suzana@gmail.com>
2025-08-12 04:01:51 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/sitemap/install.sql

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/sitemap/install.sql
@@ -0,0 +1,5 @@
+# --------------------------------------------------------
+#
+# Data for table `modules`
+#
+INSERT INTO modules (modulename, extin, moduledirectory, modulescript, modulesubmit, hascats) VALUES ('Site Map', 'E', 'sitemap', 'sitemap.php', 'N', 'N');
