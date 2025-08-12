c7ef8173ba5c1175421c412c00a01a2ee0fc1bf0
me-suzy <me.suzana@gmail.com>
2025-08-12 06:20:18 +0300

    Add Content Management/metajour2_1/metajour/js/calendar/lang/calendar-zh.js

---
+++ Content Management/metajour2_1/metajour/js/calendar/lang/calendar-zh.js
@@ -0,0 +1,45 @@
+// ** Translated by ATang ** I18N
+Calendar._DN = new Array
+("ÐÇÆÚÈÕ",
+ "ÐÇÆÚÒ»",
+ "ÐÇÆÚ¶þ",
+ "ÐÇÆÚÈý",
+ "ÐÇÆÚËÄ",
+ "ÐÇÆÚÎå",
+ "ÐÇÆÚÁù",
+ "ÐÇÆÚÈÕ");
+Calendar._MN = new Array
+("Ò»ÔÂ",
+ "¶þÔÂ",
+ "ÈýÔÂ",
+ "ËÄÔÂ",
+ "ÎåÔÂ",
+ "ÁùÔÂ",
+ "ÆßÔÂ",
+ "°ËÔÂ",
+ "¾ÅÔÂ",
+ "Ê®ÔÂ",
+ "Ê®Ò»ÔÂ",
+ "Ê®¶þÔÂ");
+
+// tooltips
+Calendar._TT = {};
+Calendar._TT["TOGGLE"] = "ÇÐ»»ÖÜ¿ªÊ¼µÄÒ»Ìì";
+Calendar._TT["PREV_YEAR"] = "ÉÏÒ»Äê (°´×¡³ö²Ëµ¥)";
+Calendar._TT["PREV_MONTH"] = "ÉÏÒ»ÔÂ (°´×¡³ö²Ëµ¥)";
+Calendar._TT["GO_TODAY"] = "µ½½ñÈÕ";
+Calendar._TT["NEXT_MONTH"] = "ÏÂÒ»ÔÂ (°´×¡³ö²Ëµ¥)";
+Calendar._TT["NEXT_YEAR"] = "ÏÂÒ»Äê (°´×¡³ö²Ëµ¥)";
+Calendar._TT["SEL_DATE"] = "Ñ¡ÔñÈÕÆÚ";
+Calendar._TT["DRAG_TO_MOVE"] = "ÍÏ¶¯";
+Calendar._TT["PART_TODAY"] = " (½ñÈÕ)";
+Calendar._TT["MON_FIRST"] = "Ê×ÏÈÏÔÊ¾ÐÇÆÚÒ»";
+Calendar._TT["SUN_FIRST"] = "Ê×ÏÈÏÔÊ¾ÐÇÆÚÈÕ";
+Calendar._TT["CLOSE"] = "¹Ø±Õ";
+Calendar._TT["TODAY"] = "½ñÈÕ";
+
+// date formats
+Calendar._TT["DEF_DATE_FORMAT"] = "y-mm-dd";
+Calendar._TT["TT_DATE_FORMAT"] = "D, M d";
+
+Calendar._TT["WK"] = "ÖÜ";
