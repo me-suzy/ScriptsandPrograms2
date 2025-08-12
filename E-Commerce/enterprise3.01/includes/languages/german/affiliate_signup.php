539592b6a8e89db165d9c319225d001cef0b4348
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:31 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/affiliate_signup.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/affiliate_signup.php
@@ -0,0 +1,35 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Partnerprogramm');
+define('HEADING_TITLE', 'Anmeldung zu unserem Partnerprogramm');
+
+define('MAIL_AFFILIATE_SUBJECT', 'Willkommen zum Partnerprogramm von' . STORE_NAME);
+define('MAIL_AFFILIATE_HEADER', 'Verehrter Partner
+
+Vielen Dank für Ihre Anmeldung bei unserem Partnerprogramm.
+
+Ihre Anmeldeinformationen:
+**************************
+
+');
+define('MAIL_AFFILIATE_ID', 'Ihre Partner-ID ist: ');
+define('MAIL_AFFILIATE_USERNAME', 'Ihr Benutzername ist: ');
+define('MAIL_AFFILIATE_PASSWORD', 'Ihr Passwort ist: ');
+define('MAIL_AFFILIATE_LINK', 'Melden Sie sich hier an: ');
+define('MAIL_AFFILIATE_FOOTER', 'Wir freuen uns auf eine gute Zusammenarbeit mit Ihnen!
+
+Ihr Partnerprogramm Team');
+?>
