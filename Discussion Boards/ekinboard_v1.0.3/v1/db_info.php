9a63fd22302560e356511ddc12a6d0061cdf26cc
me-suzy <me.suzana@gmail.com>
2025-08-12 10:36:31 +0300

    Add Discussion Boards/ekinboard_v1.0.3/v1/db_info.php

---
+++ Discussion Boards/ekinboard_v1.0.3/v1/db_info.php
@@ -0,0 +1,16 @@
+<?PHP
+// You will need to edit the 4 connection variables if the installer doesn't automatically do it for you.  Replace everything inside the ' ' with the value.
+
+// Your mySQL database host (usually 'localhost')
+$db_host = '<{$db_host}>';
+
+// Your mySQL database username
+$db_user = '<{$db_user}>';
+
+// Your mySQL database password
+$db_pass = '<{$db_pass}>';
+
+// Your mySQL database name
+$db_name = '<{$db_name}>';
+
+?>
