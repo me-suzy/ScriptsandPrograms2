bdfcecddc1ff71c7498758a83d81c793765decad
me-suzy <me.suzana@gmail.com>
2025-08-12 18:45:41 +0300

    Add Interactive Stories/FicHive_v1/language/1/system.lang.php

---
+++ Interactive Stories/FicHive_v1/language/1/system.lang.php
@@ -0,0 +1,28 @@
+<?php
+define( "LANG_SYS_CLOSED" , "Sorry, " . $conf['title'] . " is currently closed" );
+define( "LANG_SYS_UNAUTHORISED" , "You are not authorised to access this zone" );
+define( "LANG_SYS_FNF" , "Cannot find file: ");
+
+define( "LANG_SYS_SUBMIT" , "Go" );
+define( "LANG_SYS_AREYOUSURE" , "Are you sure?" );
+
+define( "LANG_SYS_QUICKNAV" , "Quick Navigation" );
+
+$lang['sys']['nav']['fiction'] = "Fiction";
+$lang['sys']['nav']['search'] = "Search";
+$lang['sys']['nav']['latest'] = "Latest";
+$lang['sys']['nav']['cpanel'] = "ControlPanel";
+$lang['sys']['nav']['apanel'] = "Admin";
+
+define( "LANG_SYS_STATS_AUTHORS" , "Authors" );
+define( "LANG_SYS_STATS_FICTION" , "Stories" );
+define( "LANG_SYS_STATS_REVIEWS" , "Reviews" );
+
+define( "LANG_SYS_MP_LATEST" , "Latest" );
+define( "LANG_SYS_MP_STATISTICS" , "Statistics" );
+
+define( "LANG_SYS_WELCOME" , "This is the placeholder for your welcome message. To edit it, you should go to the Admin area after logging in, press 'Edit Language' and then edit the 'system.lang.php' file.");
+
+define( "LANG_SYS_NEWS_COMMENT" , "Comment" );
+define( "LANG_SYS_NEWS_COMMENTS" , "Comments (<%COMS%>)" );
+?>
