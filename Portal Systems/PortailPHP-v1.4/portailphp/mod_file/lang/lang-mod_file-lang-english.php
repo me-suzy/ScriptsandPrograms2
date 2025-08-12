d070fc57a9cbcb3d98c60d9fb438e5c0755e9aac
me-suzy <me.suzana@gmail.com>
2025-08-12 23:16:43 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_file/lang/lang-mod_file-lang-english.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_file/lang/lang-mod_file-lang-english.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE FILE
+$Mod_File_Taille="2000000";// maximum file size when upload
+$Mod_File_Etiquette="File to upload";
+$Mod_File_Etiquette2="Heading";
+$Mod_File_Etiquette3="Title";
+$Mod_File_Etiquette4="Name";
+$Mod_File_Etiquette5="E-mail";
+$Mod_File_inde_Date="Date";
+$Mod_File_inde_Cat="Headings";
+$Mod_File_inde_Titre="Name";
+$Mod_File_inde_Fichiers="Files";
+$Mod_File_inde_Clics="Hits";
+$Mod_File_PostPar="Posted by";
+$Mod_File_Le="the";
+$Mod_File_Telecharger="Download";
+$Mod_File_PlRecent="10 most recent downloads";
+$Mod_File_PlLu="10 most clicked downloads";
+$Mod_File_JS_Nom="Name field must be filled";
+$Mod_File_JS_Email="E-mail field must be filled";
+$Mod_File_JS_Monfichier="File field must be filled";
+$Mod_File_JS_Titre="Title field must be filled";
+$Mod_File_Upload_OK="File upload OK";
+$Mod_File_Upload_OK="File could not upload.";
+?>
