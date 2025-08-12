a0842045d442c4a44b23fd46a07ef50efa418b0d
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:25 +0300

    Add Content Management/metajour2_1/metajour/lang/usergroup.da.php

---
+++ Content Management/metajour2_1/metajour/lang/usergroup.da.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Brugergruppe navn';
+$LANG['label_selection'] = 'Tildeling af medlemmer';
+$LANG['name'] = 'Brugergrupper';
+$LANG['label_level'] = '';
+?>
