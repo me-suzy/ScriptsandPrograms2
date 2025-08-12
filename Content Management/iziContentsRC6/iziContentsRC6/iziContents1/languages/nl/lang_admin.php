2e43ce5f53a27fdeb83e61a18bc44243cb9d4257
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:16 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_admin.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_admin.php
@@ -0,0 +1,177 @@
+﻿<?php
+
+$GLOBALS["tPage"] 		= 'Pagina';
+$GLOBALS["tOf"] 		    	= 'van';
+$GLOBALS["tEditDel"] 		= 'Bewerk/Verwijder';
+$GLOBALS["tEditDelRel"] 	= 'Bew/Ver/Pub';
+$GLOBALS["tDel"] 			= 'Verwijder';
+$GLOBALS["tAddNew"] 		= 'Nieuwe toevoegen';
+$GLOBALS["tEdit"] 		= 'Bewerken';
+$GLOBALS["tView"] 		= 'Voorbeeld';
+$GLOBALS["tDelete"] 		= 'Verwijderen';
+$GLOBALS["tConfirmDeletion"]	= 'Bevestig om te verwijderen';
+$GLOBALS["tToggle"] 		= 'Schakel gepubliceerd/In behandeling';
+$GLOBALS["tShowAll"] 		= 'Toon alles';
+
+$GLOBALS["tMoveUp"] 		= 'Omhoog';
+$GLOBALS["tMoveDown"] 		= 'Beneden';
+
+$GLOBALS["tReleased"] 		= 'Gepubliceerd';
+$GLOBALS["tPending"] 		= 'In behandeling';
+
+//  Bladzijde lijsten
+$GLOBALS["tFirstPage"] 		= 'Eerste pagina';
+$GLOBALS["tNextPage"] 		= 'Volgende pagina';
+$GLOBALS["tPrevPage"] 		= 'Vorige pagina';
+$GLOBALS["tLastPage"] 		= 'Laatste pagina';
+$GLOBALS["tSort"] 		= 'Sorteer op';
+
+//  Actie knoppen
+$GLOBALS["tSave"] 		= 'Bewaren';
+$GLOBALS["tReset"] 		= 'Terugzetten';
+$GLOBALS["tCancel"] 		= 'Annuleer';
+$GLOBALS["tTranslate"] 		= 'Vertaal';
+
+$GLOBALS["tMonth_Array"] 	= array('', 'januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+$GLOBALS["tWeek_Array"] 	= array('zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag');
+
+$GLOBALS["tYes"] 		= 'Ja';
+$GLOBALS["tNo"] 		= 'Nee';
+
+$GLOBALS["tLeft"] 		= 'Links';
+$GLOBALS["tCentre"] 		= 'Midden';
+$GLOBALS["tRight"] 		= 'Rechts';
+$GLOBALS["tJustify"] 		= 'Aanpassen';
+
+$GLOBALS["tAscending"] 		= 'Oplopend';
+$GLOBALS["tDescending"] 	= 'Aflopend';
+
+$GLOBALS["toOrderID"] 		= 'Sorteer ID';
+$GLOBALS["toPublished"] 	= 'Datum gepubliceerd';
+$GLOBALS["toModified"] 		= 'Datum aanpassing';
+$GLOBALS["toAlphabetic"] 	= 'Titel (Alfabetisch)';
+
+$GLOBALS["tAlways"] 		= 'Altijd';
+$GLOBALS["tAutomatic"] 		= 'Automatisch';
+$GLOBALS["tNever"] 		= 'Nooit';
+
+$GLOBALS["tFilter"] 		= 'Filter';
+$GLOBALS["tSelect"] 		= 'Selecteer';
+$GLOBALS["tNoImage"] 		= 'Geen afbeeldingen';
+$GLOBALS["tNoParent"] 		= 'Geen hoofdmenu\'s';
+$GLOBALS["tAllParents"] 	= 'Alle hoofdmenu\'s';
+$GLOBALS["tShowImage"] 		= 'Toon plaatje';
+$GLOBALS["tPublish"] 		= 'Publiceren';
+$GLOBALS["tExpire"] 		= 'Verlopen';
+
+$GLOBALS["tExpired"] 		= 'Is verlopen';
+$GLOBALS["tPending"] 		= 'Wachten';
+$GLOBALS["tActive"] 		= 'Actief';
+$GLOBALS["tEnabled"] 		= 'Aanzetten';
+$GLOBALS["tDisabled"] 		= 'Uitzetten';
+$GLOBALS["tVisible"] 		= 'Zichtbaar';
+$GLOBALS["tHidden"] 		= 'Verborgen';
+
+//  Menu Tekst
+$GLOBALS["tsTitle"] 		= 'iziContents beheer ';
+$GLOBALS["tmWelcome"] 		= 'Welkom';
+$GLOBALS["tmInGroup"] 		= 'U bent in de groep';
+$GLOBALS["tsWelcome"] 		= 'Welkom bij iziContents Administratie Beheer, ';
+$GLOBALS["tsMessage"] 		= 'Hier kunt u uw website beheren.';
+
+$GLOBALS["tmSubContent"] 	= 'Door gebruikers aangeleverde inhoud';
+$GLOBALS["tmLogin"] 		= 'Inloggen';
+$GLOBALS["tmLogout"] 		= 'Uitloggen';
+
+//  Login Tekst
+$GLOBALS["tLoginTitle"] 	= 'Inloggen gebruiker';
+$GLOBALS["tInvalidLogin"] 	= 'Toegang geweigerd';
+$GLOBALS["tUsernameText"] 	= 'Gebruikersnaam';
+$GLOBALS["tPasswordText"] 	= 'Wachtwoord';
+$GLOBALS["tLoginText"] 		= 'Inloggen';
+
+$GLOBALS["tMustLogin"] 		= 'U bent al een geregistreerde gebruiker, maar u moet';
+$GLOBALS["tMustRegister"] 	= 'U moet registreren en/of';
+$GLOBALS["tLogin"] 		= 'Log in';
+$GLOBALS["tHigherPriv"] 	= 'met een geschikte beheersrechten';
+$GLOBALS["tToUpdateDetails"] 	= 'om uw gebruikers te bewerken';
+$GLOBALS["tToAccessPrivate"] 	= 'om toegang te krijgen tot prive gedeelten van deze website';
+$GLOBALS["eNoLogin"] 		= 'U moet uw loginnaam ingeven';
+$GLOBALS["eNoPassword"] 	= 'U moet een wachtwoord ingeven';
+$GLOBALS["tLoginSuccessful"] 	= 'U bent met success ingelogd';
+
+$GLOBALS["tFramesRequired"] 	= 'Deze pagina gebruikt frames, maar uw browser ondersteunt dit niet.';
+
+//  Website en Thema's
+$GLOBALS["tCurrentSite"] 	= 'Huidige website';
+$GLOBALS["tMasterSite"] 	= 'Hoofd website';
+$GLOBALS["tCurrentTheme"] 	= 'Huidig thema';
+$GLOBALS["tDefaultTheme"] 	= 'Standaard thema';
+$GLOBALS["tAdminStyle"] = 'Administratie thema';
+
+//  Vertalingen
+$GLOBALS["tLangFilter"] 	= 'Taal filter';
+$GLOBALS["tTranslated"] 	= 'Onderdeel is vertaald';
+$GLOBALS["tNotTranslated"] 	= 'Onderdeel is niet vertaald';
+
+//  Help
+$GLOBALS["tOnlineHelp"] 	= 'iziContents Online Help';
+$GLOBALS["tHelpText"] 		= 'Klik op een veldnaam om het helpsysteem te activeren';
+$GLOBALS["tCloseHelp"] 		= 'Sluit help venster';
+$GLOBALS["tBabelTranslation"] 	= 'Babelfish vertalingen';
+$GLOBALS["tCloseWindow"] 	= 'Sluit venster';
+
+//  Over Tekst
+$GLOBALS["tAbout"] 						= 'Over iziContents '.$GLOBALS["Version"];
+$GLOBALS["tAboutez"] 					= 'iziContents is vanaf 2005 een OpenSource CMS dat gebaseerd is op ezContents van VisualShapers<br>ezContents werd door Marek Lyczba, Mark Baker, Wilbert Claessens geschreven.';
+$GLOBALS["tViewEzContents"] 	= 'Laat iziContents zien';
+$GLOBALS["tWrittenBy"] 				= 'Geschreven door';
+$GLOBALS["tAdditionalCode"] 	= 'Bijkomende code door';
+$GLOBALS["tTranslatedBy"] 		= 'Vertalingen door';
+$GLOBALS["tBetaTestLead"] 		= 'QA Lead';
+$GLOBALS["tBetaTesters"] 			= 'Speciale dank gaat uit naar de bèta testers';
+$GLOBALS["tBetaTesterThanks"] = 'en iedereen die op enige manier geholpen heeft aan de ontwikkeling van dit systeem.';
+$GLOBALS["tDeleteInstalDir"] 	= 'De izi_install directory is nog niet verijderd, doe dit om beveiligingsredenen NU.';
+$GLOBALS["tDeleteInstalDirButton"] = 'Verwijder izi_install directory';
+
+$GLOBALS["tModules"] 		= 'Modules';
+$GLOBALS["tVersion"] 		= 'Versie';
+
+$GLOBALS["tDBClient"] 		= 'Klant';
+$GLOBALS["tDBServer"] 		= 'Server';
+
+$GLOBALS["tRegisteredTo"] 	= 'Geregistreerd voor';
+$GLOBALS["tRegister"] 		= 'Vul hier uw registratie gegevens in';
+$GLOBALS["tUnregistered"] 	= 'Niet geregistreerd Kopie';
+$GLOBALS["tLicenseExpired"] = 'LICENTIE VERLOPEN';
+$GLOBALS["tRenewLicense"] = 'Vernieuw je Registratie';
+
+$GLOBALS["tRegistrationHash"] 	= 'Registratie hash code';
+$GLOBALS["tRegistrationBlock"] 	= 'Registratie block';
+$GLOBALS["tRegistrationDate"] 	= 'Verloop datum registratie';
+$GLOBALS["hRegistrationHash"] 	= 'Voer de Hash Code in dat u gegeven werd als deel van je inschrijving. <br /><b>Let op:-</b> in hoofdletters.';
+$GLOBALS["hRegistrationBlock"] 	= 'Voer hier het Registratie Blok in dat u gegeven is, precies zo als het toegezonden is, inclusief de <-- BEGIN REGISTRATION BLOCK --> en <-- END REGISTRATION BLOCK --> tags.';
+$GLOBALS["hRegistrationDate"] 	= 'Voer hier de datum in, waarop de licentie zal verlopen.';
+
+
+//  Aanvullende inhoud
+$GLOBALS["tThereIs"] 		= 'Er is';
+$GLOBALS["tThereAre"] 		= 'Er zijn';
+$GLOBALS["tEntryPending"] 	= 'toevoeging in behandeling';
+$GLOBALS["tEntriesPending"] 	= 'toevoegingen in behandeling';
+$GLOBALS["tRet_SubContent"] 	= 'Terugkeer naar voorgelegde inhoud module lijst';
+
+// Bestanden
+$GLOBALS["tFreeSpace"] 		= 'Vrije ruimte';
+$GLOBALS["eSafeModeWarning"] 	= '*** WAARSCHUWING: PHP \'Safe Mode\' zou beperking kunnen zijn van een aantal functies ***';
+
+// Browser/Connection Details
+$GLOBALS["Browser"] 		= 'Browser';
+$GLOBALS["tProxyServer"] 	= 'Proxy Server';
+$GLOBALS["tProxyPort"] 		= 'Poort';
+$GLOBALS["tProxyUserID"] 	= 'Proxy Gebruikers ID';
+$GLOBALS["tProxyPassword"] 	= 'Wachtwoord';
+
+$GLOBALS["tUnSupported"] 	= 'Niet ondersteund';
+
+?>
