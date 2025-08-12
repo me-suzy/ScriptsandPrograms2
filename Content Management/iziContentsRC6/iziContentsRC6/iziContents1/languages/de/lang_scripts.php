1a258636c8b2bd427f0dc29baf5af9dc1176fe82
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:38 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_scripts.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_scripts.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'Scripte verwalten';
+$GLOBALS["tFormTitle1"] = 'Scripte hochloden/Script Unterverzeichnisse erstellen';
+$GLOBALS["tFormTitle2"] = 'Script hochladen';
+$GLOBALS["tFormTitle3"] = 'Unterverzeichnis erstellen';
+
+//  List Functions
+$GLOBALS["tAddNewScript"] = 'Script hinzuf&uuml;gen';
+$GLOBALS["tDeleteScript"] = 'Script l&ouml;schen';
+$GLOBALS["tDeleteScriptDir"] = 'Script Unterverzeichnis l&ouml;schen';
+
+//  List Headings
+$GLOBALS["tScriptName"] = 'Script Name';
+
+//  Form Fields
+$GLOBALS["tScriptFilename"] = 'Script Dateiname';
+$GLOBALS["tSubdirectoryname"] = 'Unterverzeichnis Name';
+
+//  Form Buttons
+$GLOBALS["bUpload"] = 'hochladen!';
+$GLOBALS["bCreateDir"] = 'erstellen!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] = 'Sie m&uuml;ssen zum hochladen eine Datei ausw&auml;hlen.';
+$GLOBALS["eNoDir"] = 'Sie m&uuml;ssen einen Verzeichnisnamen eingeben.';
+$GLOBALS["eDirAlreadyExists"] = 'Dieses Unterverzeichnis ist bereits vorhanden.';
+$GLOBALS["eZeroByteFile"] = 'Die Datei hat eine L&auml;nge von 0 bytes.';
+$GLOBALS["eNoFileUpload"] = 'Die Datei konnt leider nicht erfolgreich hochgeladen werden.';
+$GLOBALS["eInvalidFileSize"] = 'Die Datei ist zu gross zum hochladen.';
+$GLOBALS["eInvalidFileType"] = 'Die Datei ist keine g&uuml;ltige Scriptdatei';
+$GLOBALS["eInvalidMimeType"] = 'MIME-Typ der Datei ist kein g&uuml;ltiger Scripttyp.';
+
+?>
