558cc3e273ba287be35acc949d81dae6fe930c5d
me-suzy <me.suzana@gmail.com>
2025-08-12 11:58:19 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/modules/shipping/item.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/modules/shipping/item.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_ITEM_TEXT_TITLE', 'Por artículo');
+define('MODULE_SHIPPING_ITEM_TEXT_DESCRIPTION', 'Por artículo');
+define('MODULE_SHIPPING_ITEM_TEXT_WAY', 'La Mejor Opcion');
+?>
