b24e4f4f49c54fd2050c988912e9f39175c349fc
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:02 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_admin.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_admin.php
@@ -0,0 +1,153 @@
+<?php
+
+$GLOBALS["tPage"] = 'Seite';
+$GLOBALS["tOf"] = 'von';
+$GLOBALS["tEditDel"] = '&Auml;ndern/Löschen';
+$GLOBALS["tEditDelRel"] = 'Bearbeiten/L&ouml;schen/Freigeben';
+$GLOBALS["tDel"] = 'l&ouml;schen';
+$GLOBALS["tAddNew"] = 'hinzuf&uuml;gen';
+$GLOBALS["tEdit"] = '&auml;ndern';
+$GLOBALS["tView"] = 'ansehen';
+$GLOBALS["tDelete"] = 'l&ouml;schen';
+$GLOBALS["tConfirmDeletion"] = 'L&ouml;schen best&auml;tigen';
+$GLOBALS["tToggle"] = 'Umschalten Freigeben/Wartend';
+$GLOBALS["tShowAll"] = 'alle anzeigen';
+$GLOBALS["tMoveUp"] = 'aufw&auml;rts';
+$GLOBALS["tMoveDown"] = 'abw&auml;rts';
+$GLOBALS["tReleased"] = 'ver&ouml;ffentlicht';
+$GLOBALS["tPending"] = 'Wartet';
+
+//  List Paging
+$GLOBALS["tFirstPage"] = 'Erste Seite';
+$GLOBALS["tNextPage"] = 'n&auml;chste Seite';
+$GLOBALS["tPrevPage"] = 'vorherige Seite';
+$GLOBALS["tLastPage"] = 'letzte Seite';
+$GLOBALS["tSort"] = 'sortiert nach';
+
+//  Action Buttons
+$GLOBALS["tSave"] = 'sichern';
+$GLOBALS["tReset"] = 'zur&uuml;cksetzen';
+$GLOBALS["tCancel"] = 'abbrechen';
+$GLOBALS["tTranslate"] = '�bersetzen';
+$GLOBALS["tMonth_Array"] = array('', 'Januar', 'Februar', 'M&auml;rz', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember');
+$GLOBALS["tWeek_Array"] = array('Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag', 'Sonntag');
+$GLOBALS["tYes"] = 'Ja';
+$GLOBALS["tNo"] = 'Nein';
+$GLOBALS["tLeft"] = 'links';
+$GLOBALS["tCentre"] = 'mittig';
+$GLOBALS["tRight"] = 'rechts';
+$GLOBALS["tJustify"] = 'Blocksatz';
+$GLOBALS["tAscending"] = 'aufsteigend';
+$GLOBALS["tDescending"] = 'absteigend';
+$GLOBALS["toOrderID"] = 'Order ID';
+$GLOBALS["toPublished"] = 'Datum ver&ouml;ffentlicht';
+$GLOBALS["toModified"] = 'Datum ge&auml;ndert';
+$GLOBALS["toAlphabetic"] = 'Titel (Alphabetisch)';
+$GLOBALS["tAlways"] = 'immer';
+$GLOBALS["tAutomatic"] = 'automatisch';
+$GLOBALS["tNever"] = 'nie';
+$GLOBALS["tFilter"] = 'Filter';
+$GLOBALS["tSelect"] = 'Auswahl';
+$GLOBALS["tNoImage"] = 'Kein Bild';
+$GLOBALS["tNoParent"] = 'Keine Eltern';
+$GLOBALS["tAllParents"] = 'Alle Eltern';
+$GLOBALS["tShowImage"] = 'Bild anzeigen';
+$GLOBALS["tPublish"] = 'ver&ouml;ffentlichen';
+$GLOBALS["tExpire"] = 'ablaufen';
+$GLOBALS["tExpired"] = 'abgelaufen';
+$GLOBALS["tPending"] = 'Pending';
+$GLOBALS["tActive"] = 'aktiv';
+$GLOBALS["tEnabled"] = 'eingeschaltet';
+$GLOBALS["tDisabled"] = 'ausgeschaltet';
+$GLOBALS["tVisible"] = 'sichtbar';
+$GLOBALS["tHidden"] = 'unsichtbar';
+
+//  Men� Texts
+$GLOBALS["tsTitle"] = 'Homepage Administrationsbereich';
+$GLOBALS["tmWelcome"] = 'Willkommen';
+$GLOBALS["tmInGroup"] = 'Sie sind in der Gruppe';
+$GLOBALS["tsWelcome"] = 'Willkommen in der Administration ';
+$GLOBALS["tsMessage"] = 'Hier k&ouml;nnen Sie Ihre Webseite pflegen.';
+$GLOBALS["tmSubContent"] = 'durch User &uuml;bermittelte Inhalte';
+$GLOBALS["tmLogin"] = 'Login';
+$GLOBALS["tmLogout"] = 'Logout';
+
+//  Login Texte
+$GLOBALS["tLoginTitle"] = 'Benutzer Anmeldung';
+$GLOBALS["tInvalidLogin"] = 'Zugriff verweigert';
+$GLOBALS["tUsernameText"] = 'Benutzername';
+$GLOBALS["tPasswordText"] = 'Passwort';
+$GLOBALS["tLoginText"] = 'Login';
+$GLOBALS["tMustLogin"] = 'Sie haben bereits einmal Ihre Daten ge&auml;ndert, Sie m&uuml;ssen sich neu ';
+$GLOBALS["tMustRegister"] = 'Sie m&uuml;ssen sich';
+$GLOBALS["tLogin"] = 'einloggen';
+$GLOBALS["tHigherPriv"] = 'mit geeigneten Benutzer-Rechten';
+$GLOBALS["tToUpdateDetails"] = 'um Ihre Benutzer-Details erneut zu &auml;ndern';
+$GLOBALS["tToAccessPrivate"] = 'um gesch&uuml;tzte Bereiche zu betreten';
+$GLOBALS["eNoLogin"] = 'Sie m&uuml;ssen Ihren Benutzernamen eingeben';
+$GLOBALS["eNoPassword"] = 'Sie m&uuml;ssen Ihr Passwort eingeben';
+$GLOBALS["tLoginSuccessful"] = 'Sie haben sich erfolgreich eingeloggt';
+$GLOBALS["tFramesRequired"] = 'Diese Seite benutzt Frames, aber leider unterst&uuml;tzt Ihr Browser keine Frames.';
+$GLOBALS["tLoginAnforderung"] = '<b>Login anfordern</b><br>Sie haben noch keine Login-Daten bei uns angefordert, m&ouml;chten das Angebot aber nutzen? <a href="showcontents.php?&topgroupname=kontakt&groupname=anfrageformular&sel=Login">Fordern Sie hier Ihren KOSTENLOSEN Login an!</a>';
+$GLOBALS["tLoginPasswortvergessen"] = '<b>Passwort vergessen</b><br>Wenn Sie Ihr pers&ouml;nliches Passwort vergessen haben, schicken Sie uns bitte <a href="mailto:passwort-hp@rottner-tresor.at?subject=Passwort vergessen">eine Email</a> mit Ihrem Benutzernamen und Ihrer Kundennummer. Wir lassen Ihnen umgehend ein neues Passwort per Email zukommen.';
+
+//  Sites und Themes
+$GLOBALS["tCurrentSite"] = 'derzeitige Seite';
+$GLOBALS["tMasterSite"] = 'Master Site';
+$GLOBALS["tCurrentTheme"] = 'Derzeitiges Theme';
+$GLOBALS["tDefaultTheme"] = 'Standard Theme';
+
+//  �bersetzungen
+$GLOBALS["tLangFilter"] = 'Sprachfilter';
+$GLOBALS["tTranslated"] = 'Nachricht wurde &uuml;bersetzt';
+$GLOBALS["tNotTranslated"] = 'Nachricht wurde nicht &uuml;bersetzt';
+
+//  Hilfe
+$GLOBALS["tOnlineHelp"] = 'Online Hilfe';
+$GLOBALS["tHelpText"] = 'Klicken Sie auf einen Feldnamen f&uuml;r eine Erkl&auml;rung zum jeweiligen Feld';
+$GLOBALS["tCloseHelp"] = 'Hilfe-Fenster schliessen';
+$GLOBALS["tBabelTranslation"] = 'Babelfish &Uuml;bersetzung';
+$GLOBALS["tCloseWindow"] = 'Fenster schliessen';
+
+//  About Texts
+$GLOBALS["tAbout"] = '&Uuml;ber iziContents '.$GLOBALS["Version"];
+$GLOBALS["tAboutez"] = 'iziContents ist seit 2005 ein OpenSource CMS basierend auf ezContents der Firma VisualShaper <br> ezContents wurde von Marek Lyczba, Mark Baker, Wilbert Claessens geschrieben.';
+$GLOBALS["tViewEzContents"] = 'View ezContents';
+$GLOBALS["tWrittenBy"] = 'geschrieben von ';
+$GLOBALS["tAdditionalCode"] = 'zus&auml;tzlicher Code von ';
+$GLOBALS["tTranslatedBy"] = 'Sprach&uuml;bersetzungen';
+$GLOBALS["tBetaTestLead"] = 'Qualit&auml;tssicherung';
+$GLOBALS["tBetaTesters"] = 'Mit Dank an unsere Beta-Tester';
+$GLOBALS["tModules"] = 'Module';
+$GLOBALS["tVersion"] = 'Version';
+$GLOBALS["tDBClient"] = 'Client';
+$GLOBALS["tDBServer"] = 'Server';
+$GLOBALS["tRegisteredTo"] = 'Registriert auf ';
+$GLOBALS["tRegister"] = 'Registrierungs-Details eingeben';
+$GLOBALS["tUnregistered"] = 'unregistrierte Kopie';
+$GLOBALS["tRegistrationHash"] = 'Registrierungs Hash Code';
+$GLOBALS["tRegistrationBlock"] = 'Registrierungs Block';
+$GLOBALS["hRegistrationHash"] = 'Geben Sie bitte den Hash Code ein, den Sie als Teil Ihrer Registrierung erhalten haben.<br /><b>Achtung:-</b> Dieser Wert ist case sensitive.';
+$GLOBALS["hRegistrationBlock"] = 'Geben Sie bitte den Hash Code ein, den Sie als Teil Ihrer Registrierung erhalten haben, und zwar exakt so wie er gesendet wurde, einschliesslich der <-- BEGIN REGISTRATION BLOCK --> und <-- END REGISTRATION BLOCK --> tags.';
+
+//  Submitted Content
+$GLOBALS["tThereIs"] = 'Es sind';
+$GLOBALS["tThereAre"] = 'Es sind';
+$GLOBALS["tEntryPending"] = 'Eintr&auml;ge wartend';
+$GLOBALS["tEntriesPending"] = 'Eintr&auml;ge wartend';
+$GLOBALS["tRet_SubContent"] = 'Zur&uuml;ck zur Modulliste f&uuml;r &Uuml;bermittelte Inhalte';
+
+//  Dateien
+$GLOBALS["tFreeSpace"] = 'Freier Speicher';
+$GLOBALS["eSafeModeWarning"] = '*** WARNUNG: PHP \'Safe Mode\' restriction kann die Funktionalit&auml;t beeintr&auml;chtigen ***';
+
+//  Browser/Connection Details
+$GLOBALS["Browser"] = 'Browser';
+$GLOBALS["tProxyServer"] = 'Proxy Server';
+$GLOBALS["tProxyPort"] = 'Port';
+$GLOBALS["tProxyUserID"] = 'Proxy User ID';
+$GLOBALS["tProxyPassword"] = 'Passwort';
+$GLOBALS["tUnSupported"] = 'nicht unterst&uuml;tzt';
+
+
+?>
