8cc78b7b6be67da647f39f7a07edd3d9ac9ac33e
me-suzy <me.suzana@gmail.com>
2025-08-12 10:28:08 +0300

    Add Discussion Boards/board2/ukiboard/languages/czech-win1250.php

---
+++ Discussion Boards/board2/ukiboard/languages/czech-win1250.php
@@ -0,0 +1,56 @@
+<?php
+
+$lang['Encoding'] = "Windows-1250";
+$lang['Administrator'] = "Administrátor";
+$lang['Home'] = "Domù";
+$lang['Settings'] = "Nastavení";
+$lang['Language'] = "Jazyk";
+$lang['Theme'] = "Vzhled";
+$lang['Adminemail'] = "E-mail administrátora";
+$lang['Perpage'] = "Poèet pøíspìvkù na stránku";
+
+$lang['Category'] = "Kategorie";
+$lang['Posts'] = "Pøíspìvky";
+$lang['Page'] = "Strana";
+
+$lang['Search'] = "Hledání";
+$lang['Admin'] = "Administrace";
+
+$lang['Name'] = "Jméno";
+$lang['Email'] = "E-mail";
+$lang['Post_subject'] = "Pøedmìt";
+$lang['Password'] = "Heslo";
+
+$lang['Passwordold'] = "Staré heslo";
+$lang['Passwordnew'] = "Nové heslo";
+$lang['Passwordcon'] = "Potvrï nové heslo";
+
+$lang['Fill'] = "Nutno zadat";
+$lang['Confirm'] = "Potvrdit";
+$lang['Send'] = "Odeslat";
+$lang['Post'] = "Pøíspìvek";
+$lang['Searching'] = "Hledej";
+$lang['Searchenter'] = "Zadej hledaný øetìzec";
+
+$lang['Author'] = "Autor";
+$lang['Date'] = "Datum";
+$lang['Back'] = "Zpìt";
+
+$lang['Search_keyword'] = "Hledaný výraz";
+$lang['Where_search'] = "Kde hledat";
+$lang['Topics_found'] = "Poèet nalezených pøíspìvkù";
+
+$lang['Delete'] = "Smazat";
+$lang['Edit'] = "Editovat";
+$lang['Create'] = "Vytvoøit";
+$lang['Order'] = "Poøadí";
+$lang['Yes'] = "Ano";
+
+$lang['Title'] = "Název";
+$lang['Description'] = "Popis";
+
+$lang['Deletesure'] = "Chce to opravdu smazat ?";
+$lang['Changename'] = "Zmìna jména";
+$lang['Changepassword'] = "Zmìna hesla";
+
+?>
