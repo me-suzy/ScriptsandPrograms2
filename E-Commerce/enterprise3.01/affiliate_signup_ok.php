fe2ef8ebd390a41099cac5627331814ffe9aaa93
me-suzy <me.suzana@gmail.com>
2025-08-12 11:24:23 +0300

    Add E-Commerce/enterprise3.01/affiliate_signup_ok.php

---
+++ E-Commerce/enterprise3.01/affiliate_signup_ok.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+  OSC-Affiliate
+
+
+
+  Contribution based on:
+
+
+
+  osCommerce, Open Source E-Commerce Solutions
+
+  http://www.oscommerce.com
+
+
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+
+
+  Released under the GNU General Public License
+
+*/
+
+
+
+  require('includes/application_top.php');
+
+
+
+  require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_AFFILIATE_SIGNUP_OK);
+
+
+
+  $breadcrumb->add(NAVBAR_TITLE);
+
+
+
+  $content = affiliate_signup_ok;
+
+
+
+  require(DIR_WS_TEMPLATES . TEMPLATENAME_MAIN_PAGE);
+
+
+
+  require(DIR_WS_INCLUDES . 'application_bottom.php');
+
+?>
+
