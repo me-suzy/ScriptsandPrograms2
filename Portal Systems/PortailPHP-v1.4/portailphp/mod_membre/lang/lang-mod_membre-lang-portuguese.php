be3950684327e82cc9cf5c8e30f85bf8e964a130
me-suzy <me.suzana@gmail.com>
2025-08-12 23:18:32 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_membre/lang/lang-mod_membre-lang-portuguese.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_membre/lang/lang-mod_membre-lang-portuguese.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE MEMBRES
+$Mod_Membres_Accueil_OFF="Sair";
+$Mod_Membres_News_Add="Notícia adicionada";
+$Mod_Membres_Liens_Add="Link adicionado";
+$Mod_Membres_News_Edit="Edit Notícia";
+$Mod_Membres_Info="Edite o arquivo <strong>/include/config.php</strong> para modificar estes valores";
+$Mod_Membres_InfoUser="Edite a tabela <strong>$BD_Tab_user</strong> para modificar estes valores";
+$Mod_Membres_Info_Theme="Tema";
+$Mod_Membres_Info_Langue="Idioma";
+$Mod_Membres_Info_Titre="Título";
+$Mod_Membres_Info_Version="Versão";
+$Mod_Membres_Info_Couleur="Cor do Logo";
+$Mod_Membres_Info_URL="URL";
+$Mod_Membres_Info_UploadLimite="Max. Upload";
+$Mod_Membres_Serveur_Info="Informações sobre o servidor";
+$Mod_Membres_Nom="Nome";
+$Mod_Membres_Pseudo="Login";
+$Mod_Membres_Mail="Email";
+$Mod_Membres_Date="Ativo desde ";
+$Mod_Membres_Droit="Permissões";
+$Mod_Membres_Web="Web";
+$Mod_Membres_Password="Senha";
+$Mod_Membres_Err_Field="ocê deixou de preencher um campo.";
+$Mod_Membres_Err_Login="Login ou Senha incorretos";
+$Mod_Membres_Rub_Accueil="Administração";
+$Mod_Membres_Rub_Info="Informações";
+$Mod_Membres_Rub_News_Cat="Adicionar Categoria - Notícias";
+$Mod_Membres_Rub_News_Add="Adicionar Notícia";
+$Mod_Membres_Rub_News_Edit="Modificar Notícia";
+$Mod_Membres_Rub_News_Del="Excluir Notícia";
+$Mod_Membres_Rub_News_Del2="Excluir";
+$Mod_Membres_Rub_News_Form_Categorie="nome da categoria";
+$Mod_Membres_Rub_News_JS_Cat="O campo Categoria deve ser preenchido";
+$Mod_Membres_Rub_News_Cat_OK="Categoria atualizada";
+$Mod_Membres_Rub_File_Cat="Modificar Notícia";
+$Mod_Membres_Rub_File_Del="Excluir 1 Notícia";
+$Mod_Membres_Rub_File_Form_Categorie="Categoria";
+$Mod_Membres_Rub_File_JS_Cat="O campo Categoria deve ser preenchido";
+$Mod_Membres_Rub_File_Cat_OK="Categoria atualizada";
+$Mod_Membres_Rub_Liens_Cat="Adicionar Categoria - Links";
+$Mod_Membres_Rub_Liens_Cat_OK="Categoria atualizada";
+$Mod_Membres_Rub_Faq_Form_Categorie="Categoria";
+$Mod_Membres_Rub_Faq_Cat="Adicionar Categoria - FAQ";
+$Mod_Membres_Rub_Faq_Add="Adicionar FAQ";
+$Mod_Membres_Rub_Faq_Edit="Modificar FAQ";
+$Mod_Membres_Rub_Faq_Del="Excluir FAQ";
+$Mod_Membres_Rub_Faq_Add_OK="FAQ adicionado";
+$Mod_Membres_Rub_Faq_Cat_OK="Categoria atualizada";
+$Mod_Membres_Rub_Upload_Photo="Adicionar Foto";
+$Mod_Membres_Rub_Del_Photo="Excluir 1 foto";
+$Mod_Membres_Rub_Upload_File="Adicionar Arquivo";
+$Mod_Membres_Rub_Liens_Edit="Adicionar Link";
+$Mod_Membres_Nom="Nome";
+$Mod_Membres_Regdate="Última alteração";
+$Mod_Membres_Droit="Permissão";
+$Mod_Membres_Web="Web";
+$Mod_Membres_OK_Login="Você entrou no sistema";
+$Mod_Membres_OFF_Login="Você saiu do sistema";
+$Mod_Membres_Photos_Form_Photo="Foto (JPG)";
+$Mod_Membres_Photos_Form_Vign="Miniatura (JPG, 90 largura)";
+$Mod_Membres_Photos_Form_Titre="Título (50)";
+$Mod_Membres_Photos_Form_Date="Data (aaaa-mm-dd)";
+$Mod_Membres_Photos_Form_Texte="Texto";
+
+?>
