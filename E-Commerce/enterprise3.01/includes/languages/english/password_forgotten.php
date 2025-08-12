e0eeefa5e88186161e811743ee8408212981779e
me-suzy <me.suzana@gmail.com>
2025-08-12 11:53:25 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/password_forgotten.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/password_forgotten.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Login');
+define('NAVBAR_TITLE_2', 'Password Forgotten');
+
+define('HEADING_TITLE', 'I\'ve Forgotten My Password!');
+
+define('TEXT_MAIN', 'If you\'ve forgotten your password, enter your e-mail address below and we\'ll send you an e-mail message containing your new password.');
+
+define('TEXT_NO_EMAIL_ADDRESS_FOUND', 'Error: The E-Mail Address was not found in our records, please try again.');
+
+define('EMAIL_PASSWORD_REMINDER_SUBJECT', STORE_NAME . ' - New Password');
+define('EMAIL_PASSWORD_REMINDER_BODY', 'A new password was requested from ' . $REMOTE_ADDR . '.' . "\n\n" . 'Your new password to \'' . STORE_NAME . '\' is:' . "\n\n" . '   %s' . "\n\n");
+
+define('SUCCESS_PASSWORD_SENT', 'Success: A new password has been sent to your e-mail address.');
+?>
