38d383dd49e559719ea6ab6e0af13cbeb8e2dad1
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:36 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_menusettings.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_menusettings.php
@@ -0,0 +1,55 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintain menu settings';
+
+//  Form Field Headings
+$GLOBALS["tTopMenuBorder"] = 'Header bar menu border';
+$GLOBALS["tTopMenuAlign"] = 'Header bar menu alignment';
+$GLOBALS["tTopMenuRows"] = 'Header bar menu columns';
+$GLOBALS["tTopMenuSeparator"] = 'Header bar menu separators';
+$GLOBALS["tMenuBorder"] = 'Menu border';
+$GLOBALS["tTopDistance"] = 'Distance from the top';
+$GLOBALS["tBetweenDistance1"] = 'Distance between menu and sub-menu/menu';
+$GLOBALS["tBetweenDistance2"] = 'Distance between sub-menus';
+$GLOBALS["tIndent"] = 'Sub menu indent';
+$GLOBALS["tPrivateMenus"] = 'Private menus';
+$GLOBALS["tShowDHTML"] = 'Use DHTML header bar';
+
+
+$GLOBALS["tLocked"] = 'Locked';
+$GLOBALS["tHidden"] = 'Hidden';
+
+//  Form Block Titles
+$GLOBALS["thTopMenu"] = 'Header Bar Menu Settings';
+$GLOBALS["thSideMenu"] = 'Sidebar Menu Settings';
+$GLOBALS["thAccess"] = 'Private Menu Settings';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tiMenuTitle"] = 'Menu Title';
+
+$GLOBALS["tDetails"] = '|<br />|- Distance from the top<br />|<br />\'[menu]<br />|<br />|- Distance between menu and sub-menu/menu<br />|<br /><table><tr><td width=10></td><td>[submenu]<br />|<br />|- Distance between sub-menus<br />|<br />[submenu]<br />[submenu]<br /></td></tr></table>[menu]<br />[menu]<br />[menu]<br />';
+
+$GLOBALS["hTopMenuBorder"] = 'The thickness of border between items on the top header menu.<br />Set this value to 0 if you don\\t want a border.';
+$GLOBALS["hTopMenuAlign"] = 'Should header menu entries be aligned Centred, Left, Right or spread across the full width of the menu bar (Justified)?';
+$GLOBALS["hTopMenuRows"] = 'This is the maximum number of menu items to display in a single row on the top menu.<br />If the number of items to display is greater, the menu will wrap onto another row.';
+$GLOBALS["hTopMenuSeparator"] = 'Select the style of separators to use between menu items in the top menu bar.';
+$GLOBALS["hMenuBorder"] = 'The thickness of border between menu groups on the sidebar menu.<br />Set this value to 0 if you don\\t want a border.';
+$GLOBALS["hTopDistance"] = 'The distance from the top of the frame to the first item in the sidebar menu.';
+$GLOBALS["hBetweenDistance1"] = 'The distance between a group menu item and any subgroup menu items.';
+$GLOBALS["hBetweenDistance2"] = 'The distance between sub-menu items.';
+$GLOBALS["hIndent"] = 'The indent between the left border of the menu frame and sub-group menu items.';
+$GLOBALS["hPrivateMenus"] = 'Should private menus be displayed Locked or Hidden?';
+$GLOBALS["hShowDHTML"] = 'Show dynamic generated javascript DHTML header bar';
+
+
+//  Error Messages
+$GLOBALS["eBorder1"] = 'Top menu border is not a number.';
+$GLOBALS["eRows1"] = 'Top menu rows value is not a number.';
+$GLOBALS["eBorder2"] = 'Menu border is not a number.';
+$GLOBALS["eDistance1"] = 'Distance from the top to first menu is not a number.';
+$GLOBALS["eDistance2"] = 'Distance between menu and next menu/sub-menu is not a number.';
+$GLOBALS["eDistance3"] = 'Distance between sub-menus is not a number.';
+$GLOBALS["eDistance4"] = 'Sub menu indent is not a number.';
+
+?>
