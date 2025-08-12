d0fb6cc6a2981dde99ce779cf3e67a3cd34a77e5
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:55 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_subgroups.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_subgroups.php
@@ -0,0 +1,70 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Untermen&uuml;s verwalten';
+$GLOBALS["tFormTitle2"] = 'Untermen&uuml;s &Uuml;bersetzung verwalten';
+
+//  List Headings
+$GLOBALS["tSubmenuTitle"] = 'Untermen&uuml;-Titel';
+$GLOBALS["tMenuTitle"] = 'Men&uuml;titel';
+
+//  List Functions
+$GLOBALS["tMenuFilter"] = 'Men&uuml;filter';
+$GLOBALS["tViewSubmenu"] = 'Untermen&uuml; ansehen';
+$GLOBALS["tAddNewSubmenu"] = 'Untermen&uuml;punkt hinzuf&uuml;gen';
+$GLOBALS["tEditSubmenu"] = 'Untermen&uuml;punkt &auml;ndern';
+$GLOBALS["tDeleteSubmenu"] = 'Untermen&uuml;punkt l&ouml;schen';
+$GLOBALS["tTranslate"] = 'Men&uuml;punkt &uuml;bersetzen';
+$GLOBALS["tViewTranslation"] = '&Uuml;bersetzung ansehen';
+$GLOBALS["tEditTranslation"] = '&Uuml;bersetzung &auml;ndern';
+
+//  Form Block Titles
+$GLOBALS["thGeneral"] = 'Generelle Referenz';
+$GLOBALS["thGraphics"] = 'Grafiken';
+$GLOBALS["thLinks"] = 'Module und Links';
+$GLOBALS["thSequence"] = 'Men&uuml;reihenfolge';
+$GLOBALS["thAccess"] = 'Zugriffssicherheit';
+
+//  Form Field Headings
+$GLOBALS["tMenuRef"] = 'Interner Name';
+$GLOBALS["tSubmenuTitle"] = 'Untermen&uuml;-Titel';
+$GLOBALS["tParentMenu"] = 'Men&uuml;';
+$GLOBALS["tMenuLink"] = 'Untermen&uuml; Link';
+$GLOBALS["tOpenMenuLink"] = 'Öffne Untermen&uuml;punkt innerhalb der Seite';
+$GLOBALS["tMenuImage1"] = 'Untermen&uuml; Bild';
+$GLOBALS["tMenuImage2"] = 'Untermen&uuml; Bild mouse over';
+$GLOBALS["tMenuImage3"] = 'Untermen&uuml; Bild ausgew&auml;hlt';
+$GLOBALS["tMenuImage4"] = 'Untermen&uuml; Bild ausgew&auml;hlt - mouse over';
+$GLOBALS["tMenuHover"] = 'Untermen&uuml; hover text';
+$GLOBALS["tShowMenu"] = 'Untermen&uuml; anzeigen';
+$GLOBALS["tOrderBy"] = 'Inhalte sortiert nach';
+$GLOBALS["tOrderDir"] = 'Sortierrichtung';
+$GLOBALS["tMLoginReq"] = 'Login notwendig';
+$GLOBALS["tUsergroups"] = 'Usergruppen';
+$GLOBALS["tAuthor"] = 'Untermen&uuml;-Eigent&uuml;mer';
+
+//  Form Text Descriptions and Help Messages
+$GLOBALS["tDetails"] = 'In diesem Formular k&ouml;nne Sie Untermen&uuml;-Gruppen erstellen oder &auml;ndern, um in den Inhalten der Webseite zu navigieren';
+$GLOBALS["hMenuRef"] = 'Dies ist der interne Bezeichner, den ezContents verwendet, um diesen Untermen&uuml;punkt zu referenzieren.<br />Wenn Sie dieses Feld leer lassen, wird automatisch eine fortlaufende Nummer als Referenz vergeben.';
+$GLOBALS["hSubmenuTitle"] = 'Der Name der Men&uuml;gruppe. Dies wird ein Link zu den Inhalten von diesem Men&uuml;.';
+$GLOBALS["hParentMenu"] = 'Der Obermen&uuml;punkt zu diesem Untermen&uuml;.';
+$GLOBALS["hMenuLink"] = 'Wenn dieses Feld ausgef&uuml;llt ist, wird ein absoluter Link zu dem speziellen Link in diesem Men&uuml; erstellt, z.B. mit http://www.altavista.com wird die Altavista-Seite im Inhaltsframe dieser Seite anzeigen. Es wird nicht m&ouml;glich sein, anderen Inhalt mit dieser Gruppe zu verkn&uuml;pfen.<br />Auf diese Weise k&ouml;nnen Sie benutzerdefinierte Links erstellen.<br /><br />ezContents enth&auml;lt Plug-in Module, die Sie verwenden k&ouml;nnen f&uuml;r  \"Suche\", \"Neues\" oder andere Add-Ins.<ul><li>F&uuml;r eine  \"Suche\" Seite erstellen Sie folgenden Link: modules/search/search.php<li>F&uuml;r die \"Neues\" Seite erstellen Sie diesen Link: modules/whatsnew/whatsnew.php</ul>';
+$GLOBALS["hOpenMenuLink"] = 'Men&uuml;links &ouml;ffnen sich in einem neuen Browserfenster, wenn ezContents im Nicht-Rahmen-Modus eingerichtet ist. <br />Wenn Sie diese Markierung setzen, wird ein externer Link innerhalb der Inhaltsseiten im ezContents Fenster. Dies wird nur korrekt funktionieren, wenn das externe Seitenscript so programmiert wurde, dass es mit ezContents zusammenarbeitet.';
+$GLOBALS["hMenuImage1"] = 'Das Bild, das den Men&uuml;titel ersetzen soll. (Es sollte kleiner als die definierte Breite Ihrer linke Spalte sein.)';
+$GLOBALS["hMenuImage2"] = 'Das Bild, das angezeigt wird, wenn der Mauscursor &uuml;ber das Men&uuml;bild bewegt wird (Mouse-over-Effekt).<br />(Es sollte kleiner als die definierte Breite Ihrer linke Spalte sein, zus&auml;tzlich zum Einzug, der in den Men&uuml;einstellungen festgelegt ist.)';
+$GLOBALS["hMenuImage3"] = 'Das Bild, das angezeigt wird, wenn der Men&uuml;punkt gerade ausgew&auml;hlt ist.<br />(Es sollte kleiner als die definierte Breite Ihrer linke Spalte sein, zus&auml;tzlich zum Einzug, der in den Men&uuml;einstellungen festgelegt ist.)';
+$GLOBALS["hMenuImage4"] = 'Das Bild, das angezeigt wird, wenn der Men&uuml;punkt gerade ausgew&auml;hlt ist und der Mauszeiger dar&uuml;ber bewegt wird.';
+$GLOBALS["hMenuHover"] = 'Der Text, der angezeigt wird, wenn der Mauszeiger &uuml;ber dem Men&uuml;punkt schwebt (Alt-Tag), wenn Sie dieses Feature eingeschaltet haben.';
+$GLOBALS["hShowMenu"] = 'Legt fest, ob das Men&uuml; angezeigt wird, oder nicht.';
+$GLOBALS["hOrderBy"] = 'Hier wird die Reihenfolge gew&auml;hlt, nach der die Inhalte des Men&uuml;s sortiert werden.';
+$GLOBALS["hOrderDir"] = 'Legt fest, in welcher Richtung die Sortierung erfolgt (aufsteigend oder absteigend).';
+$GLOBALS["hMLoginReq"] = 'Wenn diese Markierung gesetzt ist, dann wird ein Login vom User ben&ouml;tigt, um auf diese Men&uuml;option zuzugreifen.';
+$GLOBALS["hUsergroups"] = 'Der Login ist auf Mitglieder aus diesen gew&auml;hlten Usergruppen beschr&auml;nkt.';
+$GLOBALS["hAuthor"] = 'Dies ist der  \'Eigent&uuml;mer\' von diesem Untermen&uuml;. Wenn Abschnitte der Webseite mit Sicherheitsbeschr&auml;nkungen versehen sind, kann nur der Eigent&uuml;mer Artikel zu diesem Men&uuml; hinzuf&uuml;gen.';
+
+//  Error Messages
+$GLOBALS["eMenuExists"] = 'Ein Men&uuml; mit diesem Namen ist bereits vorhanden, bitte w&auml;hlen Sie einen anderen Namen.';
+$GLOBALS["eInvalidName"] = 'Der Untermen&uuml;-Name enth&auml;lt unzul&auml;ssige Zeichen. Bitte verwenden Sie keine Sonderzeichen.';
+$GLOBALS["eTitleEmpty"] = 'Der Untermen&uuml;-Titel muss ausgef&uuml;llt werden.';
+
+?>
