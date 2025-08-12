f59e7c94a743542463692f611722ec6aaf5f3633
me-suzy <me.suzana@gmail.com>
2025-08-12 12:13:15 +0300

    Add E-Commerce/fc_installer_cs/fc_installer/fishcart_v3/languages/lang_nor.php

---
+++ E-Commerce/fc_installer_cs/fc_installer/fishcart_v3/languages/lang_nor.php
@@ -0,0 +1,395 @@
+<?php
+//Norwegian
+//ISO 639/2 code: nor
+//Translated by Frode Vasstein at frode@go-there.com
+
+$salutearray[] = 'Herr';
+$salutearray[] = 'Fru';
+$salutearray[] = 'Frue';
+$salutearray[] = 'Dr.';
+$salutearray[] = 'Rev.';
+
+$fc_prompt = array(
+'welcome' =>
+	'Velkommen til FishCart, premieren open source multi-nasjonal e-commerce system. Du kan laste ned FishCart fra <a href="http://fishcart.org">fishcart.org</a>.',
+'outofservice' =>
+	'Butikk programmet er for øyeblikket ute av service. Vær vennlig å prøv ogjen om noen få minutter; Vi beklager dette.',
+'choosegeo' =>
+	'<h2 align=center>Geografi Valg</h2><i>Velg området du bor i<br>salgs moms og utsendings gebyr blir kalkulert.</i>',
+'invalidfield' =>
+	'</center><p><b>Et nødvendig felt er ikke skrevet.<br> Vær vennlig å klikk på "tilbake" knappen på din browser<br>og pass på at alle felt er skrevet. Tusen takk.</b><br>',
+
+'tcnotapproved' =>
+        '<p><b>The Terms and Conditions were not approved; your order cannot be '.
+        'completed without this approval.  Please click the &quot;Back&quot; button '.
+        'on your browser to approve them, or click on the link below to abandon your '.
+        'order and return to the front page.  Thank you</b></p>',
+
+'contribblank'  =>
+        '<p><b>The contribution amount was left blank.  Please click '.
+        'the &quot;Back&quot; button on your browser and enter an amount. '.
+        'Thank you!</b></p>',
+
+'payamblank'  =>
+        '<p><b>The payment amount was left blank.  Please click '.
+        'the &quot;Back&quot; button on your browser and enter an amount. '.
+        'Thank you!</b></p>',
+
+'invalidemail' =>
+	'</center><p><b>Det ser ut til at din epost adresse er skrevet feil.<br> Vær vennlig å klikk på "Tilbake" knappen på din browser<br> og sjekk om det er riktig. Tusen takk.<b><br>',
+'invalidoffon' =>
+	'</center><p><b>Verken Online eller Offline ordre metode er valgt.<br>Vær vennlig å klikk på "Tilbake" knappen på din browser og velg en. Tusen takk.</b><br>',
+'invalidccfld' =>
+	'</center><p><b>Et nødvendig kreditt kort felt er blank. Vær vennlig å klikk på "Tolbake" knappen på din browser og vær sikker på at det er riktig utfylt. Tusen takk.</b>',
+'invalidccard' =>
+	'</center><br><b>Kreditt kort nummeret som vart nedskrevet, ser ut til ikke å være riktig<br> skrevet. Vær vennlig å klikk på "Tilbake" knappen på din browser<br>og sjekk om det er riktig skrevet. For å hjelpe deg å gjøre nummerene<br> bedre leslig, kan du sepparere kreditt kort nummerene i grupper<br>med blanke eller mellomrom som viser i eksempelet nedenfor. Tusen Takk.</b><br><pre>1111 2222 3333 4444 /Visa / Mastercard) 1111 22222 333333 (American Express)</pre>',
+'invalidcctype' =>
+	'</center><p><b>Vær vennlig å klikk på "Tilbake" knappen på din browser og velg typen av kreditt kort. Tusen takk.</b>',
+'invalidccyr' =>
+	'</center><p><b>Vær vennlig å klikk på "Tilbake" knappen på din browser og<br> skriv inn riktig kreditt kort utgår år. Tusen takk.</b>',
+'invalidccmo' =>
+	'</center><p><b>Vær vennlig å klikk på "Tilbake" knappen på din browser og<br> skriv inn riktig kreditt kort utgår måned. Tusen takk.</b>',
+'invalidorder' =>
+	'<h2 align=center>Feil eller ferdig ordre</h2>
+Denne ordren ser ut til enten å være feil eller er blitt helt ferdig. Du vil motta denne beskjeden om du klikker på "Tilbake" knappen på din browser etter at du er fullstendig ferdig med din ordre. Om du har plassert en ordre online, vil du vite med sikkerhet at det vart fullstendig gjennomført når du har mottatt en epost som bekrefter ordren. Ingen e-post blir sendt til offline ordrer.<p>For å fortsette å se FIRMAETS Webside, vær vennlig å klikk på <a href="/">tilbake til hjemmesiden.</a> Tusen takk!',
+'pwexp' =>
+	'Denne elektroniske utsalgs konto er utgått. Vær vennlig å kontakte vår Kunde Service for mer assistanse. Tusen takk.',
+'orderfinal' =>
+	'Din ordre er plassert! En detaljert bekreftelse har blitt sendt til din e-post adresse som du skrev inn i butikkprogrammet.<p>Vær vennlig å IKKE klikk på "Tilbake" knappen på din browser. For å fortsette, klikk på linken nedenfor for å gå tilbake til FIRMAETS hjemmeside. Tusen takk igjen for ordren!',
+'emptysearch' =>
+	'Enten vart ingen produkter og/eller søke ord vart spesifisert, eller ditt søk vart returnert uten resultater. Vær vennlig å gå tilbake til produkt valg siden og velg en kategori eller skriv et søke ord. Tusen takk.',
+'back2select' =>
+	'Klikk her for å gå tilbake til produkt valg siden.',
+
+'click2select'	=>
+	'<i>Click on the picture for more product detail</i>',
+
+'click2prodname'=>
+	'<i>Click on product name for more detail</i>',
+
+'click2select2' =>
+        '<i>Click here for more product detail</i>',
+
+'back2cat'=>
+	'&#171Back',
+
+'shipinfo' =>
+	'Utsendings Informasjon<br><i>(om det er forskjellig fra Betalings Informasjonen)</i>',
+'onlinetext' =>
+	'Online ordren vart plassert ved å bruke ditt kreditt kort. Ordren er høyt kryptert for å sikre dine finansielle informasjoner.',
+'offlinetext' =>
+	'Du kan skrive ut ordren og sende det til telefonen, faksen eller epost. Du kan betale med kreditt kort eller postordre.',
+'noshipcalc' =>
+	'Utsendings kalkulatoren vart ikke funnet.',
+'cartcontents' =>
+	'Butikk Innhold',
+'cartempty' =>
+	'Din kurv er tom!',
+'cartmodify' =>
+	'<i>For å modifisere en vare, skriv inn antallet og klikk "Oppdater Din Ordre".</i><br><i>For å slette en vare, skriv in 0 antall og klikk på "Oppdater Din Ordre".</i><br>',
+'cartsubmit' =>
+	'Oppdater din Ordre',
+'cartinvmax' =>
+	'<i>*** antallet har overstiget tilgjengelig inventar</i>',
+'esdnotrans' =>
+	'Ingen produkt vart gitt for nedlasting.<br>',
+'esdnodl' =>
+	'Enten er nedlasting informasjonen feil eller så er passer ikke varen denne beskrivelsen.<br>',
+'esddlmax' =>
+	'Den nedlastbare telleren i denne filen har overskredet.<br>',
+'esdnofile' =>
+	'Filen som skal lastes ned vart ikke funnet.',
+'custsvc' =>
+	'Vær vennlig å kontakte FIRMAETS kunde Service for mer hjelp.<br>',
+'donatetext' =>
+	'Om du vil gi litt penger til forskjellige FIRMAER, skriv inn beløpet nedenfor og det vil bli lagt til i total beløpet til din kort. Tusen takk.',
+'optviolation' =>
+	'<b><i>Et nødvendig felt for dette produktet vart ikke valgt. Vær vennlig å skriv inn igjen ditt produkt valg; nødvendige valg er merket med en <font color="#ff0000"<b>*</b></font>. Tusen takk.</i></b>',
+'optreqtext' =>
+	'<font color="#ff0000"><b>*</b> <i>= nødvendig valg</i></font>',
+'reqtext' =>
+	'<font color="#ff0000"><b>*</b> <i>= nødvendig</i></font>',
+'reqflag' =>
+	'<font color="#ff0000"><b>*</b></font>',
+'choosezone' =>
+	'Velg en Katalog',
+'chooselang' =>
+	'Velg et Språk',
+'choosecat' =>
+	'Se Kategorien',
+'selectcat' =>
+	'[velg en kategori]',
+'choosekey' =>
+	'Nøkkelord Søk',
+'newitems' =>
+	'Nye Innlegg!',
+'vieworder' =>
+	'Se din gjeldene Ordre',
+'contactinfo' =>
+	'Kontakt Informasjon',
+'supportinfo' =>
+	'Kunde Behandling',
+'titletag' =>
+	'Online Butikk',
+'submitgeo' =>
+	'Velg Ditt Betalings Område',
+'prodinfo' =>
+	'Produkt Kjøp',
+'orderinfo' =>
+	'Kunde Ordre Informasjon',
+'billinfo' =>
+	'Betalings Informasjon',
+'creditinfo' =>
+	'Kreditt kort Informasjon',
+'proddesc' =>
+	'Produkt Beskrivelse',
+'couponid' =>
+	'Kupong ID:',
+'coupondisc' =>
+	'Kupong Rabatt:',
+'unitprice' =>
+	'Pris enhet:',
+'baseprice' =>
+	'Pris Base:',
+'option' =>
+	'Valg:',
+'setup' =>
+	'Setup:',
+'setupfee' =>
+	'Setup kostnad:',
+'setupfees' =>
+	'Setup kostnader:',
+'shipfee' =>
+	'Utsendings kostnad:',
+'salestax' =>
+	'Salgs Moms:',
+'thankyou' =>
+	'Tusen Takk!',
+'voluntary' =>
+	'Frivillig Tilleggs Donasjon:',
+'subtotal' =>
+	'Sum totalt:',
+'psubtotal' => 'psubtotal: to be translated',
+'product'=>
+	'product: to be translated',
+'total' =>
+	'Totlat:',
+'longadd' =>
+	'Legg til din Ordre',
+'shortadd' =>
+	'Legg til',
+'quantity' =>
+	'Antall',
+'qty' =>
+	'Ant.',
+'home' =>
+	'Hjem',
+'subcats' =>
+	'Subkategori',
+'previous' =>
+	'Forrige',
+'next' =>
+	'Neste',
+'searchresult' =>
+	'Søke Resultater:',
+'sku' =>
+	'Antall #:',
+'download' =>
+	'Last ned',
+'downloadrem' =>
+	'nedlastbart igjen',
+'downloadmax' =>
+	'maksimum nedlastbart er nådd',
+'onsale' =>
+	'Spesial!',
+'price' =>
+	'Pris:',
+'retailprice'	=> 'retailprice: to be translated',
+'nocharge' =>
+	'IKKE TILGJENGELIG',
+'audiosample' =>
+	'Audio klipp',
+'videosample' =>
+	'Video klipp',
+'homepage' =>
+	'Hjemmeside',
+'zonehome' =>
+	'Katalog Forside',
+'returnpage' =>
+	'Returner til forrige side',
+'returnprod' =>
+	'Returner til Produkt Siden',
+'selectctry' => 'To be translated: Select Country',
+'selectgeo' =>
+	'Velg Geografi Område',
+'checkout' =>
+	'Sjekk ut',
+'contribution' =>
+	'Online Donasjon',
+'contribamount' =>
+	'Donasjons mengde',
+'emailaddr' =>
+	'E-Post Adresse',
+'salutation' =>
+	'Tittel<br><i>valgfritt</i>',
+'saluteopt' =>
+	'[valfri tittel]',
+'firstname' =>
+	'For namn',
+'miname' =>
+	'M.I.',
+'lastname' =>
+	'Etter namn',
+'address' =>
+	'Adresse',
+'city' =>
+	'Sted',
+'state' =>
+	'Fylke',
+'zip' =>
+	'Postnr.',
+'country' =>
+	'Land',
+'dayphone' =>
+	'Dagtid telefon # (nummer)',
+'ccname' =>
+	'Kunde namn på Kreditt kortet',
+'ccnumber' =>
+	'Kreditt kort Nummer',
+'cctype' =>
+	'Kreditt kort Type',
+'ccexpire' =>
+	'Kreditt kortet Utgår',
+'cvvnumber' =>
+	'CVV2',
+'month' =>
+	'Måned',
+'year' =>
+	'År',
+'ordermethod' =>
+	'Ordre Metode',
+'ordersubmit' =>
+	'Submitt Din Ordre',
+'contribsubmit' =>
+	'Submitt Din Donasjon',
+'clearform' =>
+	'Reset',
+'online' =>
+	'Online',
+'offline' =>
+	'Offline',
+'ordersubj' =>
+	'Ordre Bekreftelse',
+'contribsubj' =>
+	'Donasjons Bekreftelse',
+'orderconf' =>
+	'Dette er din Bekreftelses kopi av din ordre på FIRMA',
+'contribconf' =>
+	'Dette er din Bekreftelses kopi av din donasjon til FIRMA.',
+'contribconf'	=> 
+	"This is the confirmation copy of your contribution to\nCOMPANY.",
+'paymentconf'	=> 
+	"This is the confirmation copy of your payment to\nCOMPANY.",
+'promoemail'	=> 
+	'Yes, please send me product announcements and promotions.',
+'retain_addr'	=> 
+	'Remember the above address information on this computer.',
+'approvetc'	=> 
+	'Yes, I have read the Terms and Conditions, and I accept them.',
+'orderorigin'	=> 
+	"\nThis order was processed by FishCart(r), FishNet(r)'s Open Source\n".
+	"e-commerce software.  For information regarding support, upgrade and\n".
+	"feature development services, please visit http://www.fishcart.org/\n".
+	"or http://www.fishnetinc.com/.\n",
+
+'jsapprovetc'	=>
+	'Please indicate that you accept the Terms and Conditions.  Thank you!',
+'jsonoff' =>
+	'Vær vennlig å velg Online eller Offline. Tusen takk!',
+'jscontrib' =>
+	'Vær vennlig å skriv inn donasjons beløpet. Tusen takk!',
+'jscountry' =>
+	'Vær vennlig å velg land. Tusen takk!',
+'jsbcountry' =>
+	'Vær vennlig å velg betalings land. Tusen takk!',
+'jsscountry' =>
+	'Vær vennlig å velg utsendings landet. Tusen takk!',
+'jsccname' =>
+	'Vær vennlig å skriv inn namnet på kreditt kortet- Tusen takk!',
+'jsccnum' =>
+	'Vær vennlig å skriv inn et kreditt kort nummer. Tusen tak!',
+'jscctype' =>
+	'Vær vennlig  å skriv inn et krditt ort type. Tusen takk!',
+'jsccexp' =>
+	'Vær vennlig å skriv inn riktig kreditt kort utgått dato. Tusen takk!',
+'jsbemail' =>
+	'Vær vennlig å skriv inn betalings e-post adresse. Tusen takk!',
+'jsbfname' =>
+	'Vær vennlig å skriv inn betalings fornamn. Tusen takk!',
+'jsblname' =>
+	'Vær vennlig å skriv inn betalings etternamn. Tusen takk!',
+'jsbaddr' =>
+	'Vær vennlig å skriv inn betalings adresse. Tusen takk!',
+'jsbcity' =>
+	'Vær vennlig å skriv inn betalings sted. Tusen takk!',
+'jsbstate' =>
+	'Vær vennlig å skriv inn betalings fylke. Tusen takk!',
+'jsbzip' =>
+	'Vær vennlig å skriv inn betalings postnr. Tusen takk!',
+'jspickone' =>
+	'Vær vennlig å velg enten en kategori eller skriv inn et søke nøkkelord. Tusen takk!',
+'jsplaced' =>
+	'Tusen takk! Din ordre er plassert; Vær tolmodig, denne prosessen vil ta noen få sekunder. Tusen takk igjen for din ordre!',
+'itemcapfix' =>
+	'VARE',
+'qtycapfix' =>
+	'ANT',
+'pricecapfix' =>
+	'PRIS',
+'productcapfix' =>
+	'BESKRIVELSE',
+'basepricefix' =>
+	'Pris Base:',
+'optionfix' =>
+	'Valg:',
+'qtyfix' =>
+	'Ant:',
+'totalfix' =>
+	'Totalt:',
+'setupfix' =>
+	'Setup:',
+'setuptotalfix' =>
+	'Setup Totalt:',
+'discountfix' =>
+	'Rabatt:',
+'subtotalfix' =>
+	'Subtotalt:',
+'psubtotalfix' => 'psubtotalfix: to be translated',
+'shippingfix' =>
+	'Utsending:',
+'salestaxfix' =>
+	'MVA:',
+'contributefix' =>
+	'Gitt av:',
+'ordertotalfix' =>
+	'Ordre Totalt:',
+'billinfofix' =>
+	'Betalings Informasjon:',
+'emailaddrfix' =>
+	'E-Post Adresse:',
+'shipaddrfix' =>
+	'Utsendings Adresse:',
+'dlusernamefix' =>
+	'Nedlastbar bRukernamn:',
+'dlpasswordfix' =>
+	'Nedlastbar Passord:',
+'coupon' =>
+	'Kupong:',
+'orderid' =>
+	'Ordre ID:',
+'phone' =>
+	'Telefon:',
+'fax' =>
+	'FAKS:'
+);
+?>
