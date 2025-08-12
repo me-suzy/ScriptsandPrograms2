fbeb37d0b7525b2899ed55f61ad40c154a496153
me-suzy <me.suzana@gmail.com>
2025-08-12 07:34:24 +0300

    Add Content Management/n8cms_v1.12_install/n8cms_v1.12_install/incl/footer.php

---
+++ Content Management/n8cms_v1.12_install/n8cms_v1.12_install/incl/footer.php
@@ -0,0 +1,7 @@
+<sub>2005</sub>
+&copy;
+<sub><a class="int_link" target="_new" >N8's Content Management System</a></sub>
+This is the footer file located in "incl/footer.php"<br>
+<a href="exit.php" accesskey="l" ></a>
+<? mysql_close(); ?>
+</td></tr></table></body></html>
