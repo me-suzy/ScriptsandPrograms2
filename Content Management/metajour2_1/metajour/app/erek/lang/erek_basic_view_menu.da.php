8f954cc7d21cb2258de657c811933f859523ea13
me-suzy <me.suzana@gmail.com>
2025-08-12 06:01:22 +0300

    Add Content Management/metajour2_1/metajour/app/erek/lang/erek_basic_view_menu.da.php

---
+++ Content Management/metajour2_1/metajour/app/erek/lang/erek_basic_view_menu.da.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ * $Id: erek_basic_view_menu.da.php,v 1.1 2005/02/16 05:04:58 jan Exp $
+ */
+
+$LANG['menu_ecompcreate'] = 'Opret ny';
+$LANG['menu_ecompactive'] = 'Aktive';
+$LANG['menu_ecompawait'] = 'Parkerede';
+$LANG['menu_ecompdone'] = 'Til godkendelse';
+$LANG['menu_ecompclosed'] = 'Afsluttede';
+$LANG['menu_ecompstat'] = 'Statistik';
+?>
