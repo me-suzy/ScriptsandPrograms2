55fac3c1a1fbfdcf2461ccf2d7948b0dde0640a4
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:03 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/modules/shipping/freeshipper.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/modules/shipping/freeshipper.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  WebMakers.com Added: Free Payments and Shipping
+  Written by Linda McGrath osCOMMERCE@WebMakers.com
+  http://www.thewebmakerscorner.com
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_FREESHIPPER_TEXT_TITLE', 'FREE SHIPPING!');
+define('MODULE_SHIPPING_FREESHIPPER_TEXT_DESCRIPTION', 'FREE SHIPPING');
+define('MODULE_SHIPPING_FREESHIPPER_TEXT_WAY', 'Free Shipping Only');
+?>
