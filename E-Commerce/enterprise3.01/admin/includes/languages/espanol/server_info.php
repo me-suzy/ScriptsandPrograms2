88fc4f61ac05263f76b601e0530ec179ec7acdd6
me-suzy <me.suzana@gmail.com>
2025-08-12 11:44:48 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/espanol/server_info.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/espanol/server_info.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Informaci&oacute;n del Servidor');
+
+define('TITLE_SERVER_HOST', 'Nombre del Servidor:');
+define('TITLE_SERVER_OS', 'Sistema Operativo del Servidor:');
+define('TITLE_SERVER_DATE', 'Fecha en el Servidor:');
+define('TITLE_SERVER_UP_TIME', 'Encendido desde:');
+define('TITLE_HTTP_SERVER', 'Servidor HTTP:');
+define('TITLE_PHP_VERSION', 'Version PHP:');
+define('TITLE_ZEND_VERSION', 'Zend:');
+define('TITLE_DATABASE_HOST', 'Servidor de Base de Datos:');
+define('TITLE_DATABASE', 'Base de Datos:');
+define('TITLE_DATABASE_DATE', 'Fecha de la Base de Datos:');
+?>
