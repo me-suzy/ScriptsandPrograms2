9fa3c7c4ca9bc0d64b69902e4f59ab9d7abe330e
me-suzy <me.suzana@gmail.com>
2025-08-12 06:03:39 +0300

    Add Content Management/metajour2_1/metajour/core/vatclass.php

---
+++ Content Management/metajour2_1/metajour/core/vatclass.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage core
+ */
+
+require_once('basicclass.php');
+
+class vat extends basic {
+
+	function vat() {
+		$this->basic();
+		$this->addcolumn('name',0,UI_STRING);
+		$this->addcolumn('vat',0,UI_STRING);
+
+		$this->addview('default');
+		$this->removeview('createvariant');
+		$this->removeview('createfuture');
+		$this->removeview('approvepublish');
+		$this->removeview('requestapproval');
+	}
+
+}
