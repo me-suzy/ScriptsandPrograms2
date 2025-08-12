5d9ba76eac79a777bd46918607c5c5e11fa86b49
me-suzy <me.suzana@gmail.com>
2025-08-12 14:22:48 +0300

    Add File Manipulation/download_engine_V1.4.1/download_engine_V1.4.1/download_engine/lang/english/phpmailer.lang-en.php

---
+++ File Manipulation/download_engine_V1.4.1/download_engine_V1.4.1/download_engine/lang/english/phpmailer.lang-en.php
@@ -0,0 +1,52 @@
+<?php
+/*
++--------------------------------------------------------------------------
+|   Alex Picture Engine
+|   ========================================
+|   by Alex Höntschel
+|   (c) 2002 AlexScriptEngine
+|   http://www.alexscriptengine.de
+|   ========================================
+|   Web: http://www.alexscriptengine.de
+|   Email: info@alexscriptengine.de
++---------------------------------------------------------------------------
+|
+|   > Beschreibung
+|   > Email-Klasse (PHP-Mailer)Sprachdatei
+|
+|	> Dieses Script ist KEINE Freeware. Bitte lesen Sie die Lizenz-
+|	> bedingungen (read-me.html) für weitere Informationen.
+|   > Diese Klasse steht unter LGPL-Lizenz, Original von Brent R. Matzelle
+|	>-----------------------------------------------------------------
+|	> This script is NOT freeware! Please read the Copyright Notice
+|	> (read-me_eng.html) for further information. 
+|   > This class was published under LGPL-Licence, original from Brent R. Matzelle
+|
+|	> $Id: phpmailer.lang-en.php 6 2005-10-08 10:12:03Z alex $
+|
++--------------------------------------------------------------------------
+*/
+
+/**
+ * PHPMailer language file.  
+ * English Version
+ */
+
+$PHPMAILER_LANG = array();
+
+$PHPMAILER_LANG["provide_address"] = 'You must provide at least one ' .
+                                     'recipient email address.';
+$PHPMAILER_LANG["mailer_not_supported"] = ' mailer is not supported.';
+$PHPMAILER_LANG["execute"] = 'Could not execute: ';
+$PHPMAILER_LANG["instantiate"] = 'Could not instantiate mail function.';
+$PHPMAILER_LANG["authenticate"] = 'SMTP Error: Could not authenticate.';
+$PHPMAILER_LANG["from_failed"] = 'The following From address failed: ';
+$PHPMAILER_LANG["recipients_failed"] = 'SMTP Error: The following ' .
+                                       'recipients failed: ';
+$PHPMAILER_LANG["data_not_accepted"] = 'SMTP Error: Data not accepted.';
+$PHPMAILER_LANG["connect_host"] = 'SMTP Error: Could not connect to SMTP host.';
+$PHPMAILER_LANG["file_access"] = 'Could not access file: ';
+$PHPMAILER_LANG["file_open"] = 'File Error: Could not open file: ';
+$PHPMAILER_LANG["encoding"] = 'Unknown encoding: ';
+?>
+
