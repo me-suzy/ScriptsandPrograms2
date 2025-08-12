0ceccf25bef327fe77ca27205123f9771aa9d1e0
me-suzy <me.suzana@gmail.com>
2025-08-12 18:14:01 +0300

    Add Image Galleries/PHP_JK_Aug_01_2005/SubscribeGallery/index.php

---
+++ Image Galleries/PHP_JK_Aug_01_2005/SubscribeGallery/index.php
@@ -0,0 +1,73 @@
+<?php
+	Require("../Includes/i_Includes.php");
+	Require("../Templates/" . $sTemplates . "/Nav/PHP_JK_GALLERIES_OPEN.php");
+	Main();
+	Require("../Templates/" . $sTemplates . "/Nav/PHP_JK_GALLERIES_CLOSE.php");
+	
+	
+	//************************************************************************************
+	//*																					*
+	//*																					*
+	//*																					*
+	//************************************************************************************
+	Function Main()
+	{
+		Global $iLoginAccountUnq;
+		
+		$iGalleryUnq = Trim(Request("iGalleryUnq"));
+
+		If ( $iGalleryUnq != "" )
+		{
+			If ( G_ADMINISTRATION_HasGallerySubscription($iLoginAccountUnq, $iGalleryUnq) ) {
+				$sMessage = "You are already subscribed to this gallery.";
+			}Else{
+				DB_Insert ("INSERT INTO IG_Subscriptions VALUES (-1, " . $iGalleryUnq . ", " . $iLoginAccountUnq . ", 'N')");
+				$sMessage = "You are now subscribed to this gallery and will receive an email when new images are added to this gallery.<br><br>To unsubscribe, return to the gallery page and click the \"Unsubscribe Gallery\" button.";
+			}
+		}Else{
+			$sMessage = "Unable to subscribe to the gallery -- missing GalleryUnq.";
+		}
+		WriteForm($sMessage);
+	}
+	//************************************************************************************
+
+	
+	//************************************************************************************
+	//*																					*
+	//*																					*
+	//*																					*
+	//************************************************************************************
+	Function WriteForm($sMessage)
+	{
+		Global $iTableWidth;
+		
+		$sReturnPage	= "";
+		
+		If ( isset($_SERVER['HTTP_REFERER_http']) )
+		{
+			$sReturnPage		= $_SERVER["HTTP_REFERER_http"];
+		}ElseIf ( isset($_SERVER['HTTP_REFERER']) )
+		{
+			$sReturnPage		= $_SERVER["HTTP_REFERER"];
+		}
+		
+		Echo "<BR>";
+		G_STRUCTURE_HeaderBar("SubscribeGalHead.gif", "", "", "Galleries");
+		?>
+		<table width=<?=$iTableWidth?> cellpadding = 10 cellspacing = 0 border = 0 class='TablePage_Boxed'>
+			<tr>
+				<td valign=top>
+					<br><?=$sMessage?><br><br>
+				</td>
+			</tr>
+		</table>
+		<br>
+		<table width=<?=$iTableWidth?> cellpadding = 0 cellspacing = 0 border = 0 class='TablePage'><tr><td>
+		<a href='<?=$sReturnPage?>'><img src='<?=G_STRUCTURE_DI("Return.gif", $GLOBALS["SCHEMEBASED"])?>' border=0></a>
+		</TD></TR></TABLE>
+		<br>
+		<?php 
+
+	}
+	//************************************************************************************
+?>
