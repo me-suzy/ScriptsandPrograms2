af68b75a473f080dad3b9e916e8ce3c4a7e36bd9
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:51 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_statistics.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_statistics.php
@@ -0,0 +1,55 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"]  = 'Besucher-Statistiken';
+$GLOBALS["tFormTitle0"] = '&Uuml;bersicht';
+$GLOBALS["tFormTitle1"] = 'Wann';
+$GLOBALS["tFormTitle2"] = 'Wer';
+$GLOBALS["tFormTitle3"] = 'Wo';
+$GLOBALS["tFormTitle4"] = 'Wie';
+
+//  Headings
+$GLOBALS["tVisitors"] = 'Besucher';
+$GLOBALS["tHits"] = 'Hits';
+$GLOBALS["tPercentage"] = 'Prozent';
+$GLOBALS["tReferer"] = 'Referer';
+$GLOBALS["stBrowser"] = 'Browser';
+$GLOBALS["stOS"] = 'Betriebssystem';
+$GLOBALS["tpToday"] = 'Heute';
+$GLOBALS["tpThisWeek"] = 'Diese Woche';
+$GLOBALS["tpThisMonth"] = 'Dieser Monat';
+$GLOBALS["tpThisYear"] = 'Dieses Jahr';
+$GLOBALS["tpAllTime"] = 'Alle Eintr&auml;ge';
+
+//  Summary Page
+$GLOBALS["stStartDate"] = 'gemessen seit';
+$GLOBALS["stTodayCount"] = 'Heute sind insgesamt';
+$GLOBALS["stTotalCount"] = 'Besucher total';
+$GLOBALS["stAverageCount"] = 'Durchschnittliche Besucher pro Tag';
+$GLOBALS["stTopCount"] = 'Das Datum mit der h&ouml;chsten Besucherrate war';
+$GLOBALS["tWith"] = 'mit';
+
+//  When Page
+$GLOBALS["otHour"] = 'Stunde';
+$GLOBALS["otHours"] = 'Stunden';
+$GLOBALS["otWeekday"] = 'Wochentag';
+$GLOBALS["otWeekdays"] = 'Wochentage';
+$GLOBALS["otDate"] = 'Datum';
+$GLOBALS["otDates"] = 'Daten';
+$GLOBALS["otWeek"] = 'Woche';
+$GLOBALS["otWeeks"] = 'Wochen';
+$GLOBALS["otMonth"] = 'Monat';
+$GLOBALS["otMonths"] = 'Monate';
+$GLOBALS["otYear"] = 'Jahr';
+$GLOBALS["otYears"] = 'Jahre';
+
+//  Who Page
+$GLOBALS["tDateTime"] = 'Datum/Zeit';
+$GLOBALS["tCountry"] = 'Land';
+
+//  Where Page
+
+//  How Page
+$GLOBALS["stAgent"] = 'HTTP User Agent';
+
+?>
