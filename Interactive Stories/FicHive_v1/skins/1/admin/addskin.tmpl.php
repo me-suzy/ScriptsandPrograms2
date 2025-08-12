d09fd2013ff46b457cd9b1b9e91e56f94dc97b39
me-suzy <me.suzana@gmail.com>
2025-08-12 18:47:58 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/addskin.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/addskin.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%BASEON%></td><td><select name='skid'><%SKINLIST%></select></td></tr>
+<tr><td class='fframe'><%SKINNAME%></td><td><input type='text' name='skname'></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>'></td></tr>
+</table>
