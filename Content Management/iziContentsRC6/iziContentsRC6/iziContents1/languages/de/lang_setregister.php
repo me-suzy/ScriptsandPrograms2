4da85302dec2df8f8a81006c5e08624c804ee410
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:45 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_setregister.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_setregister.php
@@ -0,0 +1,55 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Registrierungeinstellungen';
+
+//  Form Block Titles
+$GLOBALS["thActiveFields"] = 'Aktive Felder';
+$GLOBALS["thMailSettings"] = 'Mail Einstellungen';
+$GLOBALS["thExtraSettings"] = 'Extra Einstellungen';
+
+//  Form Field Headings
+$GLOBALS["tPhoneNumber"] = 'Telefonnummer';
+$GLOBALS["tFaxNumber"] = 'Faxnummer';
+$GLOBALS["tAddress"] = 'Addresse';
+$GLOBALS["tCity"] = 'Stadt';
+$GLOBALS["tState"] = 'Bundesland';
+$GLOBALS["tZip"] = 'PLZ';
+$GLOBALS["tCountry"] = 'Land';
+$GLOBALS["tLanguage"] = 'Sprache';
+$GLOBALS["tWebsite"] = 'Webseite';
+$GLOBALS["tComments"] = 'Kommentare';
+$GLOBALS["tNewsletter"] = 'Newsletter';
+$GLOBALS["tSendConfMail"] = 'Best&auml;tigungs-E-mail';
+$GLOBALS["tServerUserEmail"] = 'Server User Konto';
+$GLOBALS["tErrFormFontColor"] = 'Schriftfarbe f&uuml;r Fehlermeldungen';
+$GLOBALS["tShowHelptexts"] = 'Hilfetexte anzeigen';
+$GLOBALS["tHelpTextSize"] = 'Schriftgr&ouml;sse der Hilfetexte';
+$GLOBALS["tHelpTextColor"] = 'Schriftfarbe der Hilfetexte';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'Hier kann die Registrierung angepasst und das Versenden von Emails eingestellt werden.';
+
+$GLOBALS["hPhoneNumber"] = 'Zeigt oder versteckt die Telefonnummer im User-Registrierungsformular. Wenn angezeigt, kann das Feld optional oder zwingend notwendig sein.';
+$GLOBALS["hFaxNumber"] = 'Zeigt oder versteckt die Faxnummer im User-Registrierungsformular. Wenn angezeigt, kann das Feld optional oder zwingend notwendig sein.';
+$GLOBALS["hAddress"] = 'Zeigt oder versteckt das Adressfeld im User-Registrierungsformular. Wenn angezeigt, kann das Feld optional oder zwingend notwendig sein.';
+$GLOBALS["hCity"] = 'Zeigt oder versteckt das Eingabefeld f&uuml;r die Stadt im User-Registrierungsformular. Wenn angezeigt, kann das Feld optional oder zwingend notwendig sein.';
+$GLOBALS["hState"] = 'Zeigt oder versteckt das Feld f&uuml;r das Bundesland im User-Registrierungsformular. Wenn angezeigt, kann das Feld optional oder zwingend notwendig sein.';
+$GLOBALS["hZip"] = 'Zeigt oder versteckt das Feld f&uuml;r die PLZ im User-Registrierungsformular. Wenn angezeigt, kann das Feld optional oder zwingend notwendig sein.';
+$GLOBALS["hCountry"] = 'Zeigt oder versteckt das Feld f&uuml;r das Land im User-Registrierungsformular. Wenn angezeigt, kann das Feld optional oder zwingend notwendig sein.';
+$GLOBALS["hLanguage"] = 'Zeigt oder versteckt das Feld f&uuml;r das Land (Staat) im User-Registrierungsformular. Wenn angezeigt, kann das Feld optional oder zwingend notwendig sein.';
+$GLOBALS["hWebsite"] = 'Zeigt oder versteckt das Feld f&uuml;r die eigene Webseite im User-Registrierungsformular. Wenn angezeigt, kann das Feld optional oder zwingend notwendig sein.';
+$GLOBALS["hComments"] = 'Zeigt oder versteckt das Beschreibungsfeld im User-Registrierungsformular. Wenn angezeigt, kann das Feld optional oder zwingend notwendig sein.';
+$GLOBALS["hNewsletter"] = 'Zeigt oder versteckt die Option, sich f&uuml;r einen Newsletter einzutragen im Registrierungsformular.';
+$GLOBALS["hSendConfMail"] = 'Eine Best&auml;tigungs-E-mail mit den Registrierungsinformationen wird an den neu registrierten User versendet.';
+$GLOBALS["hServerUserEmail"] = 'Eventuell ist es n&ouml;tig, eine E-Mail mit den Server User Konto zu versenden. Wenn Sie dieses Feld leer lassen, wird die Email-Adresse des Administrators im Absender-("Von")Feld benutzt. Sie k&ouml;nnen diese Regel hier &uuml;berschreiben.';
+$GLOBALS["hErrFormFontColor"] = 'Die Schriftfarbe f&uuml;r Pr&uuml;fungsfehler im Registrierungsformular.';
+$GLOBALS["hShowHelptexts"] = 'Zeigt die Hilfetexte neben den einzelnen Feldern im Registrierungsformular.';
+$GLOBALS["hHelpTextSize"] = 'Dies regelt die Schriftgr&ouml;sse der Hilfetexte neben den Eingabefeldern in der Registrierung.';
+$GLOBALS["hHelpTextColor"] = 'Dies steuert die Schriftfarbe der Hilfetexte.';
+
+// Error messages
+$GLOBALS["eErrorColorWrong"] = 'Die Schriftfarbe f&uuml;r Fehlermeldungen hat einen falschen Wert.';
+$GLOBALS["eHelptextColorWrong"] = 'Die Textfarbe f&uuml;r Hilfetexte hat einen falschen Wert.';
+
+?>
