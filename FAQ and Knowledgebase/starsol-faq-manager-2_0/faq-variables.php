bf90514df8b23b37a2247cd62485ac7c305286ae
me-suzy <me.suzana@gmail.com>
2025-08-12 14:12:12 +0300

    Add FAQ and Knowledgebase/starsol-faq-manager-2_0/faq-variables.php

---
+++ FAQ and Knowledgebase/starsol-faq-manager-2_0/faq-variables.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_location = 'localhost';
+$db_username = 'username';
+$db_password = 'password';
+$db_database = 'database';
+$db_prefix = 'starsol_faq_';
+
+$site_name = 'Starsol Scripts';
+$site_domain = 'starsol.co.uk';
+
+$rating_switch = '1';
+
+$admin_username = 'admin_u';
+$admin_password = 'admin_p';
+
+?>
