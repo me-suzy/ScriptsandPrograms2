bd3078a380cf6ce26a5260bf772c8aa4f297323a
me-suzy <me.suzana@gmail.com>
2025-08-12 11:54:46 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/modules/payment/paypal.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/modules/payment/paypal.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_PAYPAL_TEXT_TITLE', 'PayPal');
+  define('MODULE_PAYMENT_PAYPAL_TEXT_DESCRIPTION', 'PayPal');
+?>
