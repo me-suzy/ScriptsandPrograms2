5daeacf6f759f24c62f78625ed5d76ca0a323645
me-suzy <me.suzana@gmail.com>
2025-08-12 11:29:18 +0300

    Add E-Commerce/enterprise3.01/specials.php

---
+++ E-Commerce/enterprise3.01/specials.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  require('includes/application_top.php');
+
+  require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_SPECIALS);
+
+  $breadcrumb->add(NAVBAR_TITLE, escs_href_link(FILENAME_SPECIALS));
+
+  $content = CONTENT_SPECIALS;
+
+  require(DIR_WS_TEMPLATES . TEMPLATENAME_MAIN_PAGE);
+
+  require(DIR_WS_INCLUDES . 'application_bottom.php');
+?>
