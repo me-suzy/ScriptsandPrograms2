4c44215c14f7b7bd7d8a1d85d1548f19d9c25ea2
me-suzy <me.suzana@gmail.com>
2025-08-12 14:32:30 +0300

    Add File Manipulation/free_download_site_v1.0/Free download site/top.inc.php

---
+++ File Manipulation/free_download_site_v1.0/Free download site/top.inc.php
@@ -0,0 +1,56 @@
+<?php
+/*
+    Copyright (C) 2005 Adullam Limited., All Rights Reserved.
+
+    Unless explicitly acquired and licensed from Licensor under the Technical Pursuit License ("TPL") 
+    Version 1.0 or greater, the contents of this file are subject to the Reciprocal Public License 
+    ("RPL") Version 1.1, or subsequent versions as allowed by the RPL, and You may not copy or use 
+    this file in either source code or executable form, except in compliance with the terms and 
+    conditions of the RPL.
+
+    You may obtain a copy of both the TPL and the RPL (the "Licenses") from Technical Pursuit Inc. 
+    at http://www.technicalpursuit.com.
+
+    All software distributed under the Licenses is provided strictly on an "AS IS" basis, WITHOUT 
+    WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND Adullam Limited HEREBY DISCLAIMS ALL 
+    SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+    PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the Licenses for specific 
+    language governing rights and limitations under the Licenses. 
+    
+    Project Name: Download Site System
+    File: top.inc.php
+    File-Version: 1.0
+    Details: this file contains everything that goes BEFORE the download listings.
+*/
+?>
+<!-- HTML top area -->
+<!-- SIDE BAR -->
+<div style="float:right; margin-left:1em; margin-bottom:0.5em;">
+<p><strong>Thank you for your<br />interest in<br />our  files</strong></p>
+<p>You are free<br />to download<br />
+them or return<br />to: <a href="http://mysite.com"></a>...</p><br />
+
+PERFECT<br />PLACE<br />FOR A<br />SKYSCRAPER<br />ADVERT
+
+<!-- COL WIDTH IS CHOSEN BY THE WIDEST ELIMENT -->
+
+</div>
+
+<!-- MAIN AREA -->
+<!-- This will go at THE VERY TOP of every page -->
+<div id="headings" style="text-align:center;">
+<h1>Download Area</h1>
+<h3>download page: Something smart about you site!</h3>
+
+this is a good place for the: 468x15 link ad from google
+</div>
+<p>Now for this page to bring in ads that bring in bucks you need to say a few words on the subject of these downloads
+...</p><p>Some files are ...</p>
+
+<!-- This line is always good for related content -->
+<p>Many files are ZIPped which means you will need a zip program like winzip or PKZip to unpack them</p>
+
+
+
+<!-- If you want to link to particular folders to draw attention to them this is the perfect file to do this in. -->
+<!-- End Top area -->
