4f2e8a072b6a32c42539d718b0e811df64df3899
me-suzy <me.suzana@gmail.com>
2025-08-13 01:26:32 +0300

    Add Unsorted/cashcrusader/pages/account_menu.php

---
+++ Unsorted/cashcrusader/pages/account_menu.php
@@ -0,0 +1,11 @@
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr>
+<td align="center"> <font size="2"><strong><a href=/pages/enter.php>Earnings Stats</a>
+| <a href=/pages/adstats.php> Advertising Stats</a> | <a href=/pages/reflinks.php>Referral
+Links</a><br>
+<a href=/pages/userinfo.php>User Account Info</a> | <a href=/pages/gold.php>Gold
+Membership</a> | <a href=/pages/ptc.php>Paid2Click</a> | <a href=/pages/index.php?username=LOGOUT&password=LOGOUT>Log-Out</a>
+</strong><br>
+</font></td>
+</tr>
+</table>
