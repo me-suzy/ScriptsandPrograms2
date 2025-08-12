7d1855b23f245a812f6404cd28452b1759702cd7
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:00 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_default.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_default.da.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Sæt til standard';
+$LANG['text_1'] = 'Er du sikker på at du vil sætte den markerede post til standard?';
+?>
