fe9f50248e6e716e9240174d204fff9b3f8b73f8
me-suzy <me.suzana@gmail.com>
2025-08-12 11:52:04 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/affiliate_contact.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/affiliate_contact.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Affiliate Program');
+define('HEADING_TITLE', 'Affiliate Program - Contact Form');
+
+define('TEXT_SUCCESS', 'Your message has been successfully sent to the Affiliate Program Team');
+
+define('EMAIL_SUBJECT', 'Affiliate Program');
+define('ENTRY_NAME', 'Your Full Name:');
+define('ENTRY_EMAIL', 'Your E-Mail:');
+define('ENTRY_ENQUIRY', 'Your Message:');
+?>
