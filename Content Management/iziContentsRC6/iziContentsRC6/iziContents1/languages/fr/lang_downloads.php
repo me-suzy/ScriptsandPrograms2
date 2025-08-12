c7099d8493fe613800989c8006e553b420618f80
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:25 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_downloads.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_downloads.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'maintenir les téléchargements';
+$GLOBALS["tFormTitle1"] = 'télécharger créer sous-directorie';
+$GLOBALS["tFormTitle2"] = 'Télécharger le fichier';
+$GLOBALS["tFormTitle3"] = 'Crée Sous-Directorie';
+
+//  List Functions
+$GLOBALS["tAddNewDownload"] = 'Ajouter le nouveau téléchargement';
+$GLOBALS["tDeleteDownload"] = 'Effacer le téléchargement';
+$GLOBALS["tDeleteDownloadDir"] = 'Effacer téléchargement sous-directorie';
+
+//  List Headings
+$GLOBALS["tDownloadName"] = 'Téléchargement nom';
+
+//  Form Fields
+$GLOBALS["tDownloadFilename"] = 'Téléchargement nom de fichier';
+$GLOBALS["tSubdirectoryname"] = 'Nom du Sous-directorie';
+
+//  Form Buttons
+$GLOBALS["bUpload"] = 'Télécharger!';
+$GLOBALS["bCreateDir"] = 'Créer!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] = 'Vous avez besoin de choisir un fichier pour télécharger.';
+$GLOBALS["eNoDir"] = 'Vous avez besoin entrer un nom de directorie.';
+$GLOBALS["eDirAlreadyExists"] = 'subdirectory existe déjà';
+$GLOBALS["eZeroByteFile"] = 'Le fichier est 0 octets dans la longueur';
+$GLOBALS["eNoFileUpload"] = 'Fichier ne pourrait pas être téléchargé avec succès.';
+$GLOBALS["eInvalidFileSize"] = 'Le fichier est trop grand pour télécharger';
+$GLOBALS["eInvalidFileType"] = 'Le fichier n est pas un fichier de chargement valide';
+$GLOBALS["eInvalidMimeType"] = 'Le type de MIME de Fichier n est pas un valide charge le type';
+
+?>
