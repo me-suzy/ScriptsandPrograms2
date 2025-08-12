47b2b7ca4121f8bc967faea14ca0dbfc2e31149f
me-suzy <me.suzana@gmail.com>
2025-08-12 19:53:38 +0300

    Add News Publishing/News_Content_Publisher/GateQuest/News_Content_Publisher_2.0/edit/logout.php

---
+++ News Publishing/News_Content_Publisher/GateQuest/News_Content_Publisher_2.0/edit/logout.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+	<title>GateQuest php News/Content Publisher</title>
+	<link rel="stylesheet" href="../docs/misc/styles.css" type="text/css">
+</head>
+
+<body>
+
+<table style="height: 100%; text-align: center; width: 90%">
+	<tr>
+		<td style="height: 65px; padding: 10px 0px 0px 10px; text-align: left"><a href="http://www.gatequest.net/products/News_Content_Publisher/"><img alt="" border="0" width="100" height="50" src="../docs/misc/gfx/gq_logo.gif"></a></td>
+	</tr>
+	<tr>
+		<td style="vertical-align: middle">
+		<p>Thanks for your visit!
+		<p>Click <a href="index.php">here</a> to login again.</p></td>
+	</tr>
+</table>
+
+</body>
+</html>
