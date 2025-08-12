93d759b43ec84ce35f62329fd6f9ff97e3672c1a
me-suzy <me.suzana@gmail.com>
2025-08-13 01:55:40 +0300

    Add Unsorted/contenido-4.4.2/contenido/external/frontend/config.php

---
+++ Unsorted/contenido-4.4.2/contenido/external/frontend/config.php
@@ -0,0 +1,20 @@
+<?php
+
+//path to contenido, for all inclusions
+$contenido_path = "!PATH!";
+$errorfont = "font3";
+//fo the language file
+$language="de";
+//optional    if it isnt set the first language of the client is choose
+$load_lang = "!LANG!";
+$load_client = "!CLIENT!";
+
+/* Various debugging options */
+$frontend_debug["container_display"] = false;
+$frontend_debug["module_display"] = false;
+$frontend_debug["module_timing"] = false;
+
+/* Set to 1 to brute-force module regeneration */
+$force = 0;
+
+?>
