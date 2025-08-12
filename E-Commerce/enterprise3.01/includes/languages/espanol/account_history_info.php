b49d2062f88f34a0527057846af86a5ccdf0c3fe
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:26 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/account_history_info.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/account_history_info.php
@@ -0,0 +1,40 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Mi Cuenta');
+define('NAVBAR_TITLE_2', 'Historial');
+define('NAVBAR_TITLE_3', 'Pedido Num. %s');
+
+define('HEADING_TITLE', 'Datos del Pedido');
+
+define('HEADING_ORDER_NUMBER', 'Pedido Num. %s');
+define('HEADING_ORDER_DATE', 'Fecha del Pedido:');
+define('HEADING_ORDER_TOTAL', 'Total del Pedido:');
+
+define('HEADING_DELIVERY_ADDRESS', 'Direcci&oacute;n de Entrega');
+define('HEADING_SHIPPING_METHOD', 'Gastos de Env&iacute;o');
+
+define('HEADING_PRODUCTS', 'Producto');
+define('HEADING_TAX', 'Impuestos');
+define('HEADING_TOTAL', 'Total');
+
+define('HEADING_BILLING_INFORMATION', 'Datos de Facturaci&oacute;n');
+define('HEADING_BILLING_ADDRESS', 'Direcci&oacute;n de Facturaci&oacute;n');
+define('HEADING_PAYMENT_METHOD', 'Forma de Pago');
+
+define('HEADING_ORDER_HISTORY', 'Mis Pedidos');
+define('HEADING_COMMENT', 'Comentarios');
+define('TEXT_NO_COMMENTS_AVAILABLE', 'No hay comentarios.');
+
+define('TABLE_HEADING_DOWNLOAD_DATE', 'Fecha Caducidad: ');
+define('TABLE_HEADING_DOWNLOAD_COUNT', ' descargas restantes');
+define('HEADING_DOWNLOAD', 'Descarga tus productos aqui');
+?>
