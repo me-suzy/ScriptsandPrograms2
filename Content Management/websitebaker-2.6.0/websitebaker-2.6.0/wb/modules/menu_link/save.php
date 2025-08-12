7a0feadddc1e7090ea6d6ead38fc283f467cd0ca
me-suzy <me.suzana@gmail.com>
2025-08-12 08:17:53 +0300

    Add Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/modules/menu_link/save.php

---
+++ Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/modules/menu_link/save.php
@@ -0,0 +1,55 @@
+<?php
+
+// $Id: save.php 40 2005-09-07 19:22:34Z stefan $
+
+/*
+
+ Website Baker Project <http://www.websitebaker.org/>
+ Copyright (C) 2004-2005, Ryan Djurovich
+
+ Website Baker is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Website Baker is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Website Baker; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+require('../../config.php');
+
+// Include WB admin wrapper script
+$update_when_modified = true; // Tells script to update when this page was last updated
+require(WB_PATH.'/modules/admin.php');
+
+$database = new database();
+
+// Update the mod_menu_links table with the link
+if(isset($_POST['link'])) {
+	// Update link and target
+	$link = $admin->add_slashes($_POST['link']);
+	$target = $_POST['target'];
+	$query = "UPDATE ".TABLE_PREFIX."pages SET link = '$link', target = '$target' WHERE page_id = '$page_id'";
+	$database->query($query);
+} else {
+	$admin->print_error('Error in wb/modules/menu_link/save.php at line 35', $js_back);
+}
+
+// Check if there is a database error, otherwise say successful
+if($database->is_error()) {
+	$admin->print_error($database->get_error(), $js_back);
+} else {
+	$admin->print_success($MESSAGE['PAGES']['SAVED'], ADMIN_URL.'/pages/modify.php?page_id='.$page_id);
+}
+
+// Print admin footer
+$admin->print_footer();
+
+?>
