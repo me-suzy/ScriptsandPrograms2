ba8fb101a27b23d2192ef15041dd1c6d2ba5016b
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:16 +0300

    Add Content Management/metajour2_1/metajour/lang/customformelement.da.php

---
+++ Content Management/metajour2_1/metajour/lang/customformelement.da.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Navn (a-z,fast)';
+$LANG['label_label'] = 'Beskrivende tekst';
+$LANG['label_fieldtype'] = 'Felttype';
+$LANG['label_fieldrelation'] = 'Relation til';
+$LANG['label_fieldwidth'] = 'Bredde';
+$LANG['label_fieldheight'] = 'Højde';
+$LANG['label_groupwithprev'] = 'Hold sammen med forrige';
+$LANG['label_keeptogether'] = 'På linie med forrige';
+$LANG['label_fieldwidthpx'] = 'Bredde (præcis)';
+$LANG['label_onlist'] = 'Medtag på liste';
+$LANG['name'] = 'Blanketfelter';
+
+$LANG['option_fieldwidth'][0] = 'Normal';
+$LANG['option_fieldwidth'][1] = 'Smal';
+$LANG['option_fieldwidth'][2] = 'Bred';
+
+$LANG['option_fieldtype']['UI_STRING'] = 'Tekststreng';
+$LANG['option_fieldtype']['UI_TEXT_WRAP'] = 'Tekstfelt';
+$LANG['option_fieldtype']['UI_DATE'] = 'Datofelt';
+$LANG['option_fieldtype']['UI_RELATION'] = 'Relation til tabel';
+$LANG['option_fieldtype']['UI_RELATION_MULTIPLE'] = 'Multipel relation til tabel';
+$LANG['option_fieldtype']['UI_LISTDIALOG'] = 'Relation til tabel (som popup)';
+
+?>
