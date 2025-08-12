799d8368e2c9310ebf08b12d9014f3cdde911b06
me-suzy <me.suzana@gmail.com>
2025-08-12 19:14:26 +0300

    Add Link Indexing/cncat/cncat/lang/lang_rus.php

---
+++ Link Indexing/cncat/cncat/lang/lang_rus.php
@@ -0,0 +1,188 @@
+<?
+$LANG=Array();
+$LANG["charset"]="windows-1251";
+
+$LANG["linksnotfound"]="Ññûëîê â äàííîé êàòåãîðèè íå íàéäåíî";
+$LANG["sortby"]="Ñîðòèðîâàòü ïî";
+$LANG["popuarity"]="Ïîïóëÿðíîñòè";
+$LANG["title"]="Íàçâàíèþ";
+$LANG["modervote"]="Îöåíêå ìîäåðàòîðà";
+$LANG["sortbyin"]="Ïî äàòå ïîñòóïëåíèÿ";
+$LANG["main"]="Êàòàëîã";
+$LANG["linkcount"]="Ññûëîê â êàòåãîðèè";
+$LANG["addlink"]="Äîáàâèòü ññûëêó";
+$LANG["moderators"]="Ìîäåðàòîðàì";
+$LANG["category"]="Êàòåãîðèÿ";
+$LANG["notselected"]="Íå âûáðàíà";
+$LANG["sitetitle"]="Íàçâàíèå ñàéòà";
+$LANG["siteurl"]="URL ñàéòà";
+$LANG["sitedescription"]="Îïèñàíèå";
+$LANG["email"]="E-Mail";
+$LANG["submit"]="Äîáàâèòü";
+$LANG["newlinks"]="Íîâûå ññûëêè";
+$LANG["groupop"]="Ãðóïïîâûå îïåðàöèè";
+
+$LANG["errorsfound"]="Ïðîèçîøëè ñëåäóþùèå îøèáêè";
+$LANG["mustbecat"]="Äîëæíà áûòü óêàçàíà êàòåãîðèÿ.";
+$LANG["mustbeemail"]="Äîëæåí áûòü óêàçàí E-Mail.";
+$LANG["mustbeurl"]="Äîëæåí áûòü óêàçàí URL ñàéòà.";
+$LANG["mustbetitle"]="Äîëæíî áûòü óêàçàíî íàçâàíèå ñàéòà.";
+$LANG["mustbedescription"]="Äîëæíî áûòü ââåäåíî îïèñàíèå. ";
+
+$LANG["administration"]="Àäìèíèñòðèðîâàíèå";
+$LANG["no"]="íåò";
+$LANG["delete"]="Óäàëèòü";
+$LANG["asubmit"]="Ïîäòâåðäèòü";
+$LANG["tonew"]="Â íîâûå";
+$LANG["change"]="Èçìåíèòü";
+$LANG["total"]="Âñåãî";
+$LANG["new"]="Íîâûå";
+$LANG["submited"]="Ðàáî÷èå";
+$LANG["deleted"]="Óäàëåííûå";
+$LANG["editcats"]="Ðåäàêòèðîâàòü ðàçäåëû";
+$LANG["duplinks"]="Äóáëèðóþùèåñÿ ññûëêè";
+$LANG["edit"]="Ðåäàêòèðîâàòü";
+
+$LANG["search"]="Èñêàòü";
+$LANG["notfound"]="Èñêîìàÿ êîìáèíàöèÿ ñëîâ íèãäå íå âñòðå÷àåòñÿ";
+$LANG["pagesfound"]="Íàéäåíî ñòðàíèö";
+$LANG["emptyquery"]="Çàäàí ïóñòîé ïîèñêîâûé çàïðîñ";
+
+$LANG["createcategory"]="Ñîçäàòü ðàçäåë";
+$LANG["createcategoryinsubcat"]="Ñîçäàòü ïîäðàçäåë â ðàçäåëå";
+$LANG["rootcat"]="Êîðíåâîé ðàçäåë";
+$LANG["categoryname"]="Íàçâàíèå ðàçäåëà";
+$LANG["add"]="Äîáàâèòü";
+$LANG["rename"]="Ïåðåèìåíîâàòü";
+$LANG["deletecategory"]="Óäàëèòü ðàçäåë";
+$LANG["renamecategory"]="Ïåðåèìåíîâàòü ðàçäåë";
+$LANG["import"]="Èìïîðò";
+
+$LANG["brokenlinks"]="Áèòûå ññûëêè";
+$LANG["alreadyexists"]="Ýòà ññûëêà óæå åñòü â êàòàëîãå";
+$LANG["brokensure"]="Âû óâåðåíû, ÷òî ýòî ññûëêà íå ðàáîòàåò ?";
+$LANG["yes"]="äà";
+$LANG["no"]="íåò";
+$LANG["resetbroken"]="Ñáðîñèòü ñ÷åò÷èê";
+$LANG["broken"]="<font color=red>x</font>";
+
+$LANG["placelink"]="Ïîñòàâüòå ññûëêó íà íàø êàòàëîã";
+$LANG["linkwasadded"]="Ññûëêà äîáàâëåíà";
+$LANG["thx"]="<P>Ñïàñèáî, âàøà ññûëêà áûëà äîáàâëåíà â íàøó áàçó äàííûõ, è â òå÷åíèè 24 ÷àñîâ áóäåò ðàññìîòðåíà ìîäåðàòîðîì. </P>";
+
+/* new in CNCat 1.3 */
+$LANG["changelink"]="Èçìåíèòü ññûëêó";
+$LANG["idanddate"]="ID è äàòà";
+$LANG["fromsite"]="Ïåðåõîäîâ ñ ñàéòà";
+$LANG["tosite"]="Ïåðåõîäîâ íà ñàéò";
+$LANG["back"]="Íàçàä";
+
+/* new in CNCat 1.3.1 */
+$LANG["sync"]="Ñèíõðîíèçàöèÿ";
+$LANG["syncz"]="Ñèíõðîíèçèðîâàòü";
+$LANG["syncdelall"]="Ôèçè÷åñêè óäàëèòü âñå ññûëêè â ðàçäåëå &quot;óäàëåííûå&quot;";
+$LANG["synccount"]="Ñèíõðîíèçàöèÿ êîëè÷åñòâà ññûëîê è íàçâàíèé ïîäðàçäåëîâ";
+$LANG["syncjumps"]="Ñ÷åò÷èêè ïåðåõîäîâ";
+$LANG["syncgin"]="Êîëè÷åñòâî ïåðåõîäîâ â êàòàëîã";
+$LANG["syncgout"]="Êîëè÷åñòâî ïåðåõîäîâ èç êàòàëîãà";
+$LANG["syncreset"]="Îáíóëèòü";
+$LANG["syncavgrowisize"]="Ñðåäíÿÿ äëèíà ðÿäà";
+$LANG["syncdelrows"]="Óäàëåííûõ ðÿäîâ";
+$LANG["syncfrom"]="èç";
+$LANG["syncmustfree"]="Ïðè óäàëåíèè îñâîáîäèòñÿ";
+$LANG["bytes"]="áàéò(à)";
+
+/* new in CNCat 1.4 */
+$LANG["sortbyin"]="Äàòå";
+$LANG["remove_install"]="Óäàëèòå install.php ïðåæäå ÷åì èñïîëüçîâàòü CNCat.";
+$LANG["plugins"]="Ïëàãèíû";
+$LANG["plugin_next"]="Äàëåå";
+$LANG["plugin_example"]="Ïðèìåð ïðîñòîãî ïëàãèíà è ñòðóêòóðó áàçû äàííûõ âû ìîæåòå ïîñìîòðåòü â <a href=../manual/manual.html#plugin>äîêóìåíòàöèè</a>.";
+$LANG["plugin_phpnuke"]="Èìïîðò ññûëîê èç êàòàëîãà PHPNuke";
+$LANG["plugin_phpnuke_sync"]="Ñèíõðîíèçàöèÿ";
+$LANG["plugin_phpnuke_finished"]="Çàêîí÷åíî";
+$LANG["plugin_phpnuke_cat"]="Èìïîðòèðîâàíèå êàòåãîðèé...";
+$LANG["plugin_phpnuke_subcat"]="Èìïîðòèðîâàíèå ïîäêàòåãîðèé...";
+$LANG["plugin_phpnuke_links"]="Èìïîðòèðîâàíèå ññûëîê...";
+$LANG["plugin_export_rubrics"]="Ýêñïîðòèðîâàòü ðóáðèêè";
+$LANG["plugin_export_links"]="Ýêñïîðòèðîâàòü ññûëêè";
+$LANG["plugin_export_design"]="Ýêñïîðòèðîâàòü îôîðìëåíèå";
+$LANG["plugin_export"]="Ýêñïîðò â XML";
+$LANG["plugin_export_download"]="Ñêà÷àòü XML";
+$LANG["plugin_import"]="Èìïîðòèðîâàíèå èç XML";
+$LANG["plugin_import_do"]="Èìïîðòèðîâàòü";
+$LANG["plugin_import_rubrics"]="Ôàéë ñ ðóáðèêàìè (cncat_rubrics.xml)";
+$LANG["plugin_import_links"]="Ôàéë ñ ññûëêàìè (cncat_links.xml)";
+$LANG["plugin_import_skip"]="Ïðîïóñòèòü, åñëè ññûëêà ñ òàêèì ID óæå ñóùåñòâóåò";
+$LANG["plugin_import_overwrite"]="Ïåðåçàïèñàòü, åñëè ññûëêà ñ òàêèì ID óæå ñóùåñòâóåò";
+$LANG["plugin_import_delete"]="Î÷èñòèòü êàòàëîã ïåðåä èìïîðòîì<br>(ôèçè÷åñêè óäàëÿþòñÿ âñå çàïèñè è èìïîðòèðîâàíèå ññûëîê ïðîèçâîäèòñÿ â ïóñòîé êàòàëîã)";
+$LANG["plugin_import_finished"]="Èìïîðòèðîâàíèå óñïåøíî çàâåðøåíî.";
+$LANG["plugin_import_ed_links"]="Èìïîðòèðîâàíî ññûëîê";
+$LANG["plugin_import_ed_rubrics"]="Èìïîðòèðîâàíî ðóáðèê";
+$LANG["plugin_multiadd"]="Ìàññîâîå äîáàâëåíèå ññûëîê";
+$LANG["plugin_multiadd_count"]="Äîáàâëÿòü ññûëîê çà îäèí ðàç";
+$LANG["plugin_multiadd_attention"]="åñëè ïîëå íå çàïîëíåíî, òî ññûëêà äîáàâëåíà íå áóäåò";
+$LANG["plugin_multiadd_modervote"]="Îöåíêà ìîäåðàòîðà";
+$LANG["plugin_multiadd_add"]="Äîáàâëåíèå ññûëêè ¹";
+
+$LANG["plugin_cy"]="Ñîðòèðîâàòü ïî èíäåêñó öèòèðóåìîñòè ßíäåêñà.";
+$LANG["plugin_cy_order"]="Ñîðòèðîâî÷íûå äàííûå óñïåøíî îáíîâëåíû.";
+$LANG["plugin_cy_perpage"]="Îáðàáàòûâàòü ññûëîê çà ñòðàíèöó";
+$LANG["plugin_cy_notice"]="<P>Âíèìàíèå! Ýòîò ïëàãèí çàìåíÿåò êîëè÷åñòâî ïåðåõîäîâ ñ ñàéòà íà <a href=http://www.yandex.ru/cy/ target=_blank>èíäåêñ öèòèðóåìîñòè ßíäåêñà</a>. </P>\nÂñå äàííûå ïîëó÷åííûå ñ ßíäåêñà, ÿâëÿþòñÿ èõ ñîáñòâåííîñòüþ. ß íå íåñó íèêàêîé îòâåòñòâåííîñòè, è âû èñïîëüçóåòå ýòî ïëàãèí íà ñâîé ñòðàõ è ðèñê. </P>";
+$LANG["plugin_cy_wait"]="Ïîæàëóéñòà ïîäîæäèòå. Ñ÷èòûâàåòñÿ ÑY.";
+
+$LANG["secretcodeerror"]="×èñëî óêàçàííîå íà êàðòèíêå ââåäåíî íå âåðíî";
+$LANG["secretcode"]="Ââåäèòå ÷èñëî, êîòîðîå âû âèäèòå íà êàðòèíêå";
+
+/* New in 1.5 */
+
+$LANG["mail_desc_1"]="Ïèñüìî îòïðàâëÿåìîå, ïðè äîáàâëåíèè ññûëêè â êàòàëîã.";
+$LANG["mail_desc_2"]="Ïèñüìî îòïðàâëÿåìîå, ïðè óòâåðæäåíèè ññûëêè ìîäåðàòîðîì.";
+$LANG["mail_desc_3"]="Ïèñüìî îòïðàâëÿåìîå, ïðè îòêëîíåíèè ññûëêè ìîäåðàòîðîì.";
+$LANG["mail_headers"]="Äîïîëíèòåëüíûå çàãîëîâêè";
+$LANG["mail_body"]="Òåëî ïèñüìà";
+$LANG["mail_from"]="Îò";
+$LANG["mail_subject"]="Òåìà";
+$LANG["mail_send"]="Îòïðàâëÿòü ïèñüìî";
+$LANG["mail_menu_add"]="Äîáàâëåíèå";
+$LANG["mail_menu_submit"]="Ïîäòâåðæäåíèå";
+$LANG["mail_menu_delete"]="Îòêàç";
+$LANG["mail_title"]="Ïèñüìà";
+
+$LANG["tmpl"]="Øàáëîíû";
+$LANG["tmpl_bmenu"]="Ìåíþ";
+$LANG["tmpl_catname"]="Íàçâàíèå êàòàëîãà è ïóòü";
+$LANG["tmpl_linkstop"]="×àñòü HTML ñòðàíèöû ïåðåä ñïèñêîì ññûëîê";
+$LANG["tmpl_linksmiddle"]="×àñòü HTML ñòðàíèöû ìåæäó ëó÷øèìè è îáûêíîâåííûìè ññûëêàìè";
+$LANG["tmpl_linksbottom"]="×àñòü HTML ñòðàíèöû ïîñëå ñïèñêà ññûëîê";
+$LANG["tmpl_linksbit"]="Îäíà ññûëêà";
+$LANG["tmpl_bestlinksbit"]="Îäíà ëó÷øàÿ ññûëêà";
+$LANG["tmpl_newlinkstop"]="Âåðõíÿÿ ÷àñòü áëîêà \"Íîâûå ññûëêè\"";
+$LANG["tmpl_newlinkstbottom"]="Íèæíÿÿ ÷àñòü áëîêà ññûëêè \"Íîâûå ññûëêè\"";
+$LANG["tmpl_newlinkstbit"]="Íîâàÿ ññûëêà";
+$LANG["tmpl_partstop"]="Âåðõíÿÿ ÷àñòü áëîêà \"ðóáðèêè\"";
+$LANG["tmpl_partsdelimtop"]="Íà÷àëî íîâîé ñòðîêè áëîêà \"ðóáðèêè\"";
+$LANG["tmpl_partsdelimbottom"]="Êîíåö ñòðîêè áëîêà \"ðóáðèêè\"";
+$LANG["tmpl_partsbottom"]="Íèæíÿÿ ÷àñòü  áëîêà \"ðóáðèêè\"";
+$LANG["tmpl_partsbit"]="Íàçâàíèå êàòåãîðèè";
+$LANG["tmpl_searchform"]="Ïîèñêîâàÿ ôîðìà";
+$LANG["tmpl_searchtop"]="×àñòü HTML ñòðàíèöû ïåðåä ñïèñêîì ññûëîê";
+$LANG["tmpl_searchbottom"]="×àñòü HTML ñòðàíèöû ïîñëå ñïèñêà ññûëîê";
+$LANG["tmpl_sortby"]="Ñîðòèðîâêà";
+$LANG["tmpl_brokenscript"]="Ñêðèïò \"Íåðàáîòàþùèå ññûëêè\"";
+$LANG["tmpl_title_1"]="Ñïèñîê ññûëîê";
+$LANG["tmpl_title_2"]="Áëîê \"Íîâûå ññûëêè\"";
+$LANG["tmpl_title_3"]="Ñïèñîê êàòåãîðèé";
+$LANG["tmpl_title_4"]="Ïîèñê";
+$LANG["tmpl_title_5"]="Ðàçíîå";
+$LANG["tmpl_about"]="Âåðõíþþ è íèæíþþ ÷àñòü äîêóìåíòà âû ìîæåòå èçìåíèòü â ôàéëàõ _top.php è _bottom.php.<br>Â íèõ âû ìîæåòå èñïîëüçîâàòü PHP ñêðèïòû.";
+
+$LANG["links"]="Ññûëêè";
+$LANG["other"]="Ðàçíîå";
+$LANG["plugin_import_templates"]="Ôàéë ñ îôîðìëåíèåì (cncat_design.xml)";
+
+$LANG["wrong_password"]="Ïàðîëü ââåäåí íåâåðíî";
+$LANG["wrong_login"]="Ëîãèí ââåäåí íåâåðíî";
+$LANG["logout"]="Logout";
+$LANG["nobroken"]="Îòçûâîâ î áèòûõ ññûëêàõ íå íàéäåíî.";
+?>
