5ba6976203b06a86f8aa3b043c6c97ef156b6f2e
me-suzy <me.suzana@gmail.com>
2025-08-12 21:33:25 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/index.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/index.lang.php
@@ -0,0 +1,30 @@
+<?php
+$lang_head_latest = "Latest Added Comics";
+$lang_head_favs = "Latest Favorite Comics";
+
+$tpl->assign("lang_head_stats", "Statistics");
+$tpl->assign("lang_head_price", "Collection Price & Values");
+$tpl->assign("lang_artist_number", "Artist Numbers");
+
+$tpl->assign("lang_list_title", "Title");
+$tpl->assign("lang_list_story", "Story");
+$tpl->assign("lang_list_issue", "Issue");
+$tpl->assign("lang_list_volume", "Vol");
+$tpl->assign("lang_list_added", "Added Date");
+
+$tpl->assign("lang_titles", "Total titles");
+$tpl->assign("lang_writers", "Total Writers");
+$tpl->assign("lang_inkers", "Total Inkers");
+$tpl->assign("lang_pencilers", "Total Pencilers");
+$tpl->assign("lang_colorists", "Total Colorists");
+$tpl->assign("lang_letterers", "Total Letterers");
+$tpl->assign("lang_covers", "Total Coverartists");
+$tpl->assign("lang_publishers", "Total Publishers");
+$tpl->assign("lang_current_lang", "Current Language");
+$tpl->assign("lang_current_theme", "Current Theme");
+$tpl->assign("lang_comics", "Total comics");
+$tpl->assign("lang_version", "Program Version");
+$lang_price_in = "Price In";
+$lang_value_in = "Value In";
+
+?>
