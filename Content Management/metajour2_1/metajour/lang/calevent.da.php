09c95fea6ed3b7e0d5fdc256b9819b9ee36eb98b
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:43 +0300

    Add Content Management/metajour2_1/metajour/lang/calevent.da.php

---
+++ Content Management/metajour2_1/metajour/lang/calevent.da.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Aktiviteter';
+$LANG['label_caleventtypeid'] = 'Aktivitetstype';
+$LANG['label_name'] = 'Overskrift';
+$LANG['label_content'] = 'Beskrivelse';
+$LANG['label_begindate'] = 'Start';
+$LANG['label_enddate'] = 'Slut';
+$LANG['label_hidedate'] = 'Skjul dato';
+$LANG['label_documentid'] = 'Link til dokument';
+?>
