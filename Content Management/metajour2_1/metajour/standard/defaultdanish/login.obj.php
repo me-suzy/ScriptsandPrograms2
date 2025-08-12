a857c7da6268be90147b3a8b63457a455e022b00
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:49 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/login.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/login.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 38;
+$_objchilds[] = '_child_login_39';
+function objfile_38 () {
+$obj = owNew('document');
+$objdata['name'] = "Login";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
