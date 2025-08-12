3c5dd7146b4a5f8a8f0cc43bedbe2b714e8d8168
me-suzy <me.suzana@gmail.com>
2025-08-12 05:59:45 +0300

    Add Content Management/metajour2_1/metajour/app/edocument/lang/edocument_edocform.da.php

---
+++ Content Management/metajour2_1/metajour/app/edocument/lang/edocument_edocform.da.php
@@ -0,0 +1,22 @@
+<?php
+$LANG['name'] = 'Afvigelsesrapport';
+$LANG['label_errorcodeid'] = 'Fejlkode';
+$LANG['label_correctionid'] = 'Rettelseskode';
+$LANG['label_responsibleid'] = 'Primært ansvar';
+$LANG['label_name'] = 'Kundenavn';
+$LANG['label_cno'] = 'Kundenr';
+$LANG['label_cname'] = 'Kontaktperson';
+$LANG['label_caddress'] = 'Adresse';
+$LANG['label_cpostalcode'] = 'Postnr';
+$LANG['label_ccity'] = 'By';
+$LANG['label_ccontact'] = 'Land';
+$LANG['label_cphone'] = 'Telefon';
+$LANG['label_cemail'] = 'Email';
+$LANG['label_comment1'] = 'Det har jeg gjort';
+$LANG['label_comment2'] = 'Supplerende bemærkninger';
+$LANG['label_comment3'] = 'Korrigerende handling';
+$LANG['label_ccountryid'] = 'Land';
+$LANG['label_status'] = 'Statuskode';
+$LANG['label_infoid'] = 'Information til';
+$LANG['label_handleid'] = 'Behandles af';
+?>
