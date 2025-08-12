bd0b1aa8755ce227506194433a1fd19ee0a10f37
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:11 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_usergroups.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_usergroups.php
@@ -0,0 +1,19 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'translation privilege groupe noms';
+
+//  List Headings
+$GLOBALS["tUsergroupname"] = 'Groupe d\'utilisateur nom';
+
+//  List Functions
+$GLOBALS["tViewPrivilege"] = 'Affichage privilège group';
+$GLOBALS["tEditPrivilege"] = 'Translation privilège group';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Ce formulaire vous permez de traduire les noms de groupe de privilège.';
+
+//  Error Messages
+$GLOBALS["eNoUsergroupName"] = 'Vous devez entrer une traduction pour chaque langue';
+
+?>
