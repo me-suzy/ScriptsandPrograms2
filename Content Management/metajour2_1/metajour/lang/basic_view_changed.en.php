d6fdf3d81f068de2dd82f8e88863f7bd3c7e3445
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:28 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_changed.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_changed.en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Changed';
+$LANG['text_1'] = 'Enter time and date of change for the selected records';
+$LANG['text_2'] = 'Changed: ';
+?>
