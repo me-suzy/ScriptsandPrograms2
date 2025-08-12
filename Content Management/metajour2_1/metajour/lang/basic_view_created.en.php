77f4ebbc36461e41988dc9aa11fd2afd7dc5e234
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:51 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_created.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_created.en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Created';
+$LANG['text_1'] = 'Enter time and date of creation for the selected records';
+$LANG['text_2'] = 'Created: ';
+?>
