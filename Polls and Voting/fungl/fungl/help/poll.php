2481e4d44ae347c84532e2699206bfabef95ad98
me-suzy <me.suzana@gmail.com>
2025-08-12 21:55:44 +0300

    Add Polls and Voting/fungl/fungl/help/poll.php

---
+++ Polls and Voting/fungl/fungl/help/poll.php
@@ -0,0 +1,23 @@
+<div class="rightbox">
+	<h2>Help</h2>
+	<h2>Create poll</h2>
+	Click the <b>Create&nbsp;poll</b> link just below the headline to create a new poll.<br/>
+	<br/>
+	<b>Title:</b> The name of the poll, this is the headline shown to users.<br/>
+	<b>Vote button text:</b> The text on the vote button in the poll.<br/>
+	
+	<h2>Polls</h2>
+	<b>Action links</b><br/>
+	<b>Edit:</b> edit the info in the project.<br/>
+	<b>Edit questions:</b> edit the questions assosiated with the poll.<br/>
+	<b>Delete:</b> delete the poll, this will also delete all assosiated questions<br/>
+	
+	<h2>Time</h2>
+	There is two ways to decide when to show the polls to users.<br/>
+	<b>Interval</b><br/>
+	You select a start date and an end date for when a poll is to be shown.
+	This will override any polls that is to be displayed periodic.
+	
+	<b>Periodic</b><br/>
+	If this mode is selected the poll will only be shown on the weekday selected.
+</div>
