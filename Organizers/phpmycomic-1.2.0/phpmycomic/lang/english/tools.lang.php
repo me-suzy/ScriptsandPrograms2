939fa5c552946182bc269eff351ec3d92d6b2318
me-suzy <me.suzana@gmail.com>
2025-08-12 21:33:49 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/tools.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/tools.lang.php
@@ -0,0 +1,29 @@
+<?php
+
+// Head titles
+$tpl->assign("lang_head_tools", "Program Tools");
+$tpl->assign("lang_head_upload", "Image Upload");
+$tpl->assign("lang_head_import", "Comic Import");
+$tpl->assign("lang_head_checklist", "Issue Checklist");
+$tpl->assign("lang_head_comicreport", "Custom Comic Report");
+
+// Upload And Import
+$tpl->assign("lang_upload_text", "Upload the cover scans of your comics and use them when adding comics or when editing. Browse your system for images (jpg, jpeg, gif & png)!");
+$tpl->assign("lang_import_text", "This will allow you to import comics that you have exported from other phpmycomic databases! Browse your system for import files (pmc)!");
+$tpl->assign("lang_upload_file", "Image file:");
+$tpl->assign("lang_import_file", "Import file:");
+
+// Custom Comic Report
+$tpl->assign("lang_option_1", "Choose Field 1");
+$tpl->assign("lang_option_2", "Choose Field 2");
+$tpl->assign("lang_option_3", "Choose Field 3");
+$tpl->assign("lang_option_4", "Choose Field 4");
+$tpl->assign("lang_option_5", "Choose Field 5");
+
+// Issue Checklist
+$tpl->assign("lang_select_name", "Select Series");
+$tpl->assign("lang_select_vol", "Choose Series Volume");
+$tpl->assign("lang_select_start", "Enter Start Issue");
+$tpl->assign("lang_select_end", "Enter End Issue");
+
+?>
