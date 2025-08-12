810bd2fe4670f60560c926833a350ab91c95c10f
me-suzy <me.suzana@gmail.com>
2025-08-12 19:25:55 +0300

    Add Link Indexing/phpOnDirectory/phpOnDirectory/Templates/maintemplate.footer.inc.php

---
+++ Link Indexing/phpOnDirectory/phpOnDirectory/Templates/maintemplate.footer.inc.php
@@ -0,0 +1,13 @@
+    </td>
+  </tr>
+</table>
+<table width="770" border="0" align="center" cellpadding="5" cellspacing="0">
+  <tr>
+    <td align="center"  class="foot">&nbsp;</td>
+  </tr>
+  <tr>
+    <td align="center" bgcolor="#FFFFFF"  class="small">Copyright &copy; 1999 - 2005 Interactive Arts Ltd</td>
+  </tr>
+</table>
+</body>
+</html>
