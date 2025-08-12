9ec959cf33d3d9efda632fffd620ea6d78fef500
me-suzy <me.suzana@gmail.com>
2025-08-12 11:53:58 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/xsell_products.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/xsell_products.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id: xsell_products.php, v1
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.enterprisecart.com
+
+  Released under the GNU General Public License
+*/
+
+  define('TEXT_BUY', 'Buy 1 \'');
+  define('TEXT_NOW', '\' now');
+
+?>
