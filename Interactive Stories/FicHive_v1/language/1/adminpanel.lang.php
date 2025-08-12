f1fc79c526b69588ba3c0202af5612176660f3fd
me-suzy <me.suzana@gmail.com>
2025-08-12 18:45:26 +0300

    Add Interactive Stories/FicHive_v1/language/1/adminpanel.lang.php

---
+++ Interactive Stories/FicHive_v1/language/1/adminpanel.lang.php
@@ -0,0 +1,168 @@
+<?php
+$lang['nav']['admin'][0]['main'] = "General Settings";
+$lang['nav']['admin'][1]['main'] = "Manage Categories";
+$lang['nav']['admin'][1]['sub'][] = "Add Category";
+$lang['nav']['admin'][1]['sub'][] = "Edit Category";
+$lang['nav']['admin'][1]['sub'][] = "Delete Category";
+$lang['nav']['admin'][1]['sub'][] = "Order Categories";
+$lang['nav']['admin'][2]['main'] = "Manage Users";
+$lang['nav']['admin'][2]['sub'][] = "Edit Account";
+$lang['nav']['admin'][2]['sub'][] = "Delete Account";
+$lang['nav']['admin'][3]['main'] = "Manage Groups";
+$lang['nav']['admin'][3]['sub'][] = "Add Group";
+$lang['nav']['admin'][3]['sub'][] = "Edit Group";
+$lang['nav']['admin'][3]['sub'][] = "Delete Group";
+$lang['nav']['admin'][4]['main'] = "Manage Skins";
+$lang['nav']['admin'][4]['sub'][] = "Create Skin";
+$lang['nav']['admin'][4]['sub'][] = "Edit Skin";
+$lang['nav']['admin'][4]['sub'][] = "Delete Skin";
+$lang['nav']['admin'][5]['main'] = "Manage Languages";
+$lang['nav']['admin'][5]['sub'][] = "Create Language";
+$lang['nav']['admin'][5]['sub'][] = "Edit Language";
+$lang['nav']['admin'][5]['sub'][] = "Delete Language";
+$lang['nav']['admin'][6]['main'] = "Manage News";
+$lang['nav']['admin'][6]['sub'][] = "Add News";
+$lang['nav']['admin'][6]['sub'][] = "Edit News";
+$lang['nav']['admin'][6]['sub'][] = "Delete News";
+
+define( "LANG_AP_YES" , "Yes" );
+define( "LANG_AP_NO" , "No" );
+
+define( "LANG_AP_GS_DATABASE" , "Settings: Database" );
+define( "LANG_AP_GS_DBSERVER" , "Server" );
+define( "LANG_AP_GS_DBNAME" , "Name" );
+define( "LANG_AP_GS_DBUSER" , "Username" );
+define( "LANG_AP_GS_DBPASS" , "Password" );
+define( "LANG_AP_GS_DBPRE" , "Prefix" );
+define( "LANG_AP_GS_SITE" , "Settings: Site" );
+define( "LANG_AP_GS_TITLE" , "Title" );
+define( "LANG_AP_GS_URL" , "Url" );
+define( "LANG_AP_GS_BOT" , "Auto Mailer" );
+define( "LANG_AP_GS_PATH" , "Path" );
+define( "LANG_AP_GS_STATUS" , "Status" );
+define( "LANG_AP_GS_OPEN" , "Open" );
+define( "LANG_AP_GS_CLOSED" , "Closed" );
+define( "LANG_AP_GS_GENERAL" , "Settings: General" );
+define( "LANG_AP_GS_MAGE" , "Minimum Age" );
+define( "LANG_AP_GS_TIME" , "Time Difference (+-)" );
+define( "LANG_AP_GS_TIMEFORMAT" , "Time Format" );
+define( "LANG_AP_GS_TIMEFORMATNEWS" , "Time Format - News" );
+define( "LANG_AP_GS_ALLOWEDHTML" , "Allowed HTML" );
+define( "LANG_AP_GS_PASSWORDSALT" , "Crypt key" );
+define( "LANG_AP_GS_COOKIES" , "Settings: Cookies" );
+define( "LANG_AP_GS_COOKIEPRE" , "Prefix" );
+define( "LANG_AP_GS_COOKIEPATH" , "Path" );
+define( "LANG_AP_GS_COOKIEDOMAIN" , "Domain" );
+define( "LANG_AP_GS_SEPARATORS" , "Settings: Separators" );
+define( "LANG_AP_GS_SEPTITLE" , "Title" );
+define( "LANG_AP_GS_SEPCRUMB" , "Crumb" );
+define( "LANG_AP_GS_SEPNAVIG" , "Navigation" );
+define( "LANG_AP_GS_SEPMISC" , "Miscellaneous" );
+define( "LANG_AP_GS_LAYOUT" , "Settings: Layout" );
+define( "LANG_AP_GS_LAYCOLUMNS" , "Columns" );
+define( "LANG_AP_GS_LAYPPAGE" , "Stories per page" );
+define( "LANG_AP_GS_LATEST" , "Settings: Latest" );
+define( "LANG_AP_GS_LATESTLIMIT" , "Latest limit" );
+define( "LANG_AP_GS_SEARCH" , "Settings: Search" );
+define( "LANG_AP_GS_SEARCHLIMIT" , "Search limit" );
+define( "LANG_AP_GS_SEARCHWAIT" , "Seconds between searches" );
+define( "LANG_AP_GS_USERS" , "Settings: Users" );
+define( "LANG_AP_GS_USERNAMELIMIT" , "Max pen name length" );
+define( "LANG_AP_GS_USERPASSLIMIT" , "Max password length" );
+define( "LANG_AP_GS_USERAVAHEIGHT" , "Max avatar height (px)" );
+define( "LANG_AP_GS_USERAVAWIDTH" , "Max avatar width (px)" );
+define( "LANG_AP_GS_FICTION" , "Settings: Fiction" );
+define( "LANG_AP_GS_FICWORDS" , "Max words per chapter" );
+define( "LANG_AP_GS_FICUPLOAD" , "Allow chapter uploads" );
+define( "LANG_AP_GS_FICTYPES" , "Allowed upload types" );
+define( "LANG_AP_GS_DEFAULTS" , "Settings: Defaults" );
+define( "LANG_AP_GS_DEFAULTSKIN" , "Default skin" );
+define( "LANG_AP_GS_DEFAULTLANG" , "Default language" );
+define( "LANG_AP_GS_DEFAULTGROUP" , "Group on registration" );
+define( "LANG_AP_GS_MAILER" , "Settings: Send Mail Alerts On " );
+define( "LANG_AP_GS_MAILUPDATEALERT" , "Update On Favourite Story" );
+define( "LANG_AP_GS_MAILREVIEWALERT" , "New Review" );
+
+define( "LANG_AP_GS_SUCC1" , "Your configuration has been altered" );
+
+define( "LANG_AP_CAT_ERR1" , "The category you are deleting cannot be the same as the category you are moving content to" );
+
+define( "LANG_AP_CAT_SUCC1" , "The category has been created successfully" );
+define( "LANG_AP_CAT_SUCC2" , "The category has been edited successfully" );
+define( "LANG_AP_CAT_SUCC3" , "The category has been deleted and the content moved successfully" );
+define( "LANG_AP_CAT_SUCC4" , "The categories have been reordered successfully" );
+
+define( "LANG_AP_CAT_NAME" , "Name" );
+define( "LANG_AP_CAT_DESC" , "Description" );
+define( "LANG_AP_CAT_PARENT" , "Parent" );
+define( "LANG_AP_CAT_TOPLEVEL" , "Top Level" );
+define( "LANG_AP_CAT_SELIMG" , "Select Image" );
+define( "LANG_AP_CAT_OPEN" , "Open" );
+define( "LANG_AP_CAT_APPROVAL" , "Require Approval" );
+define( "LANG_AP_CAT_PERMISSIONS" , "Permissions" );
+define( "LANG_AP_CAT_READPERM" , "Can Read" );
+define( "LANG_AP_CAT_WRITEPERM" , "Can Post" );
+define( "LANG_AP_CAT_MODERATORS" , "Moderated By" );
+define( "LANG_AP_CAT_DELETE" , "Delete" );
+define( "LANG_AP_CAT_CHANGETO" , "Move content to" );
+define( "LANG_AP_CAT_CHARACTERS" , "Characters<br>(one option per line)" );
+
+
+define( "LANG_CP_U_ERR1" , "Cannot find account, please try again" );
+define( "LANG_CP_U_ERR2" , "The pen name you've chosen is already in use" );
+define( "LANG_CP_U_ERR3" , "The password must be between 3 and ".$conf['password_length']." characters long and alphanumeric only" );
+define( "LANG_CP_U_ERR4" , "You must provide a valid email address" );
+define( "LANG_CP_U_ERR5" , "The penname must be between 3 and ".$conf['penname_length']." letters long and contain letters, numbers and spaces only" );
+
+define( "LANG_CP_U_SUCC1" , "The user account has been successfully edited" );
+define( "LANG_CP_U_SUCC2" , "The user account has been successfully deleted" );
+
+define( "LANG_CP_U_WARN1" , "Deleting a user account will also delete all stories and reviews associated with it" );
+
+define( "LANG_CP_U_ACCOUNTNO" , "Account N&ordm;" );
+define( "LANG_CP_U_PENNAME" , "PenName" );
+define( "LANG_CP_U_PASSWORD" , "Password" );
+define( "LANG_CP_U_EMAIL" , "Email" );
+define( "LANG_CP_U_LANGUAGE" , "Language" );
+define( "LANG_CP_U_SKIN" , "Skin" );
+define( "LANG_CP_U_BIO" , "About" );
+define( "LANG_CP_U_GROUP" , "Group" );
+define( "LANG_CP_U_IP" , "IP" );
+define( "LANG_CP_U_REGISTERED" , "Registered" );
+
+define( "LANG_CP_GRP_SUCC1" , "The group has been successfully created" );
+define( "LANG_CP_GRP_SUCC2" , "The group has been successfully edited" );
+define( "LANG_CP_GRP_SUCC3" , "The group has been deleted and the accounts moved successfully" );
+
+define( "LANG_CP_GRP_ERR1" , "The group you are deleting cannot be the same as the group you are moving accounts to" );
+
+define( "LANG_CP_GRP_GROUPNAME" , "Group Name" );
+define( "LANG_CP_GRP_GROUPCOLOR" , "Group Color" );
+define( "LANG_CP_GRP_DELETEGROUP" , "Delete" );
+define( "LANG_CP_GRP_CHANGETO" , "Move accounts to" );
+
+define( "LANG_CP_SKIN_ADDBASEON" , "Base On" );
+define( "LANG_CP_SKIN_ADDSKINNAME" , "Skin Name" );
+define( "LANG_CP_SKIN_DELETESKIN" , "Delete Skin" );
+define( "LANG_CP_SKIN_TRANSFERTO" , "Transfer users to" );
+define( "LANG_CP_SKIN_ERR1" , "You cannot delete the default skin" );
+define( "LANG_CP_SKIN_ERR2" , "The skin you are deleting cannot be the same as the skin you are moving users to" );
+define( "LANG_CP_SKIN_SUCC1" , "The skin has been successfully created" );
+define( "LANG_CP_SKIN_SUCC2" , "The skin file has been successfully edited" );
+define( "LANG_CP_SKIN_SUCC3" , "The skin has been successfully deleted" );
+
+define( "LANG_CP_LANG_ADDBASEON" , "Base On" );
+define( "LANG_CP_LANG_ADDLANGNAME" , "Language Name" );
+define( "LANG_CP_LANG_DELETELANG" , "Delete Language" );
+define( "LANG_CP_LANG_TRANSFERTO" , "Transfer users to" );
+define( "LANG_CP_LANG_ERR1" , "You cannot delete the default language" );
+define( "LANG_CP_LANG_ERR2" , "The language you are deleting cannot be the same as the language you are moving users to" );
+define( "LANG_CP_LANG_SUCC1" , "The language has been successfully created" );
+define( "LANG_CP_LANG_SUCC2" , "The language file has been successfully edited" );
+define( "LANG_CP_LANG_SUCC3" , "The language has been successfully deleted" );
+
+define( "LANG_AP_NEWS_SUCC1" , "The news item has been added" );
+define( "LANG_AP_NEWS_SUCC2" , "The news item has been edited" );
+define( "LANG_AP_NEWS_SUCC3" , "The news item has been deleted" );
+define( "LANG_AP_NEWS_DATE" , "Date" );
+?>
