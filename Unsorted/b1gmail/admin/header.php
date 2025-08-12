74f00bbe34c803116bb151f7426cf83ed900b5a0
me-suzy <me.suzana@gmail.com>
2025-08-13 01:08:24 +0300

    Add Unsorted/b1gmail/admin/header.php

---
+++ Unsorted/b1gmail/admin/header.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+ <title>Adminbereich</title>
+ <SCRIPT language=javascript>
+  <!--
+
+   function LmOver(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+   function LmOut(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+   function LmDown(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+  //-->
+ </SCRIPT>
+ <style> 
+  <!--  
+   a:link     { text-decoration: none      }
+   a:hover    { text-decoration: underline }
+   a:active   { text-decoration: none      }
+   a:visited  { text-decoration: none      }
+
+   a.dl:link     { color: #ffffff; text-decoration: none      }
+   a.dl:hover    { color: #ffffff; text-decoration: underline }
+   a.dl:active   { color: #ffffff; text-decoration: none      }
+   a.dl:visited  { color: #ffffff; text-decoration: none      }
+  -->
+ </style>
+ <base target="main">
+</head>
+
+<body bgcolor="#ffffff">
+<center>
+ <table width="90%" bgcolor="#003366" cellspacing="1">
+  <tr>
+    <td width="14%" bgcolor="#1f487f" onMouseover="LmOver(this, '#336699')" onMouseout="LmOut(this, '#1f487f')" onMouseDown="LmDown(this, '#336699')" align="center"><font face="arial" size="2" color="#ffffff"><b><a class="dl" href="welcome.php">Start</a></b></font></td>
+    <td width="14%" bgcolor="#1f487f" onMouseover="LmOver(this, '#336699')" onMouseout="LmOut(this, '#1f487f')" onMouseDown="LmDown(this, '#336699')" align="center"><font face="arial" size="2" color="#ffffff"><b><a class="dl" href="user.php">User</a></b></font></td>
+    <td width="14%" bgcolor="#1f487f" onMouseover="LmOver(this, '#336699')" onMouseout="LmOut(this, '#1f487f')" onMouseDown="LmDown(this, '#336699')" align="center"><font face="arial" size="2" color="#ffffff"><b><a class="dl" href="banner.php">Ads</a></b></font></td>
+    <td width="14%" bgcolor="#1f487f" onMouseover="LmOver(this, '#336699')" onMouseout="LmOut(this, '#1f487f')" onMouseDown="LmDown(this, '#336699')" align="center"><font face="arial" size="2" color="#ffffff"><b><a class="dl" href="version.php">Versions</a></b></font></td>
+    <td width="14%" bgcolor="#1f487f" onMouseover="LmOver(this, '#336699')" onMouseout="LmOut(this, '#1f487f')" onMouseDown="LmDown(this, '#336699')" align="center"><font face="arial" size="2" color="#ffffff"><b><a class="dl" href="passwort.php">Password</a></b></font></td>
+    <td width="14%" bgcolor="#1f487f" onMouseover="LmOver(this, '#336699')" onMouseout="LmOut(this, '#1f487f')" onMouseDown="LmDown(this, '#336699')" align="center"><font face="arial" size="2" color="#ffffff"><b><a class="dl" href="newsletter.php">Newsletter</a></b></font></td>
+    <td width="14%" bgcolor="#1f487f" onMouseover="LmOver(this, '#336699')" onMouseout="LmOut(this, '#1f487f')" onMouseDown="LmDown(this, '#336699')" align="center"><font face="arial" size="2" color="#ffffff"><b><a class="dl" href="support.php">Support</a></b></font></td>
+  </tr>
+ </table>
+</center>
+</body>
+</html>
