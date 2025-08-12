e6591a8be9150652c1ce656ce09001a2d892fef5
me-suzy <me.suzana@gmail.com>
2025-08-12 04:01:32 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_en.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_en.php
@@ -0,0 +1,16 @@
+<?php
+
+$GLOBALS["module_title"] = 'Search Website';
+$GLOBALS["search_keywords"] = 'Keywords to search for';
+$GLOBALS["search_OR"] = 'Any combination of keywords';
+$GLOBALS["search_AND"] = 'All keywords';
+$GLOBALS["search_titles"] = 'Search title';
+$GLOBALS["search_teasers"] = 'Teaser text';
+$GLOBALS["search_contents"] = 'Content';
+$GLOBALS["search_button"] = 'Search';
+
+$GLOBALS["eEnterKeywords"] = 'Please enter Search keywords.';
+$GLOBALS["eEnterElements"] = 'Please enter the page elements you would like to search.';
+$GLOBALS["eNoResults"] = 'No results found.';
+
+?>
