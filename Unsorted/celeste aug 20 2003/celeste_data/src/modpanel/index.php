411c04da37b7fe5a85a69fe098c0f4b666e6fe33
me-suzy <me.suzana@gmail.com>
2025-08-13 01:33:08 +0300

    Add Unsorted/celeste aug 20 2003/celeste_data/src/modpanel/index.php

---
+++ Unsorted/celeste aug 20 2003/celeste_data/src/modpanel/index.php
@@ -0,0 +1,49 @@
+<?php
+
+mod_header("Welcome to Celeste - Moderator Control Panel");
+
+?>
+<br>
+Note: Please Log out when you leave.<br>
+<br>
+
+<br>
+
+<br><br>
+
+<table width=95% cellpadding=1 cellspacing=0 bgcolor=666666 align=center>
+<tr><td><table width=100% cellpadding=4 cellspacing=2 bgcolor=FFFFFF>
+<tr><td height=22 background="images/mod/tdbg.gif" align=center>
+<b>Quick Links</b>
+</td></tr>
+<tr><td>
+<a href="http://www.fastboard.org" target=_blank><b>Celeste Home</b></a><br>
+Find more about this forum and its dev team.<br><br>
+
+<a href="http://www.php.net" target=_blank><b>PHP Home</b></a><br>
+Find more about PHP.<br><br>
+
+<a href="http://www.mysql.com" target=_blank><b>Mysql Home</b></a><br>
+Find more about MySQL.<br><br>
+
+</td></tr>
+</table>
+</table>
+
+<br><br>
+
+<table width=95% cellpadding=1 cellspacing=0 bgcolor=666666 align=center>
+<tr><td><table width=100% cellpadding=4 cellspacing=2 bgcolor=FFFFFF>
+<tr><td height=22 background="images/mod/tdbg.gif" align=center>
+<b>Celeste Dev Team</b>
+</td></tr>
+<tr><td>
+<b>Lvxing , Y10k</b>
+</td></tr>
+</table>
+</table>
+
+
+<?php
+
+mod_footer();
