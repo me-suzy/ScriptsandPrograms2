61dc662327af05328245e37f8d65aad61e95ae76
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:12 +0300

    Add Content Management/metajour2_1/metajour/lang/item.da.php

---
+++ Content Management/metajour2_1/metajour/lang/item.da.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Produkter';
+$LANG['label_name'] = 'Varenr';
+$LANG['label_content1'] = 'Beskrivelse 1';
+$LANG['label_content2'] = 'Beskrivelse 2';
+$LANG['label_content3'] = 'Beskrivelse 3';
+$LANG['label_price'] = 'Pris';
+$LANG['label_vatid'] = 'Momssats';
+$LANG['label_image1'] = 'Billede 1';
+$LANG['label_image2'] = 'Billede 2';
+$LANG['label_image3'] = 'Billede 3';
+$LANG['label_exstr1'] = 'Ekstra #1';
+$LANG['label_exstr2'] = 'Ekstra #2';
+$LANG['label_exstr3'] = 'Ekstra #3';
+$LANG['label_exstr4'] = 'Ekstra #4';
+$LANG['label_exstr5'] = 'Ekstra #5';
+$LANG['label_exstr6'] = 'Ekstra #6';
+$LANG['label_exstr7'] = 'Ekstra #7';
+$LANG['label_exstr8'] = 'Ekstra #8';
+?>
