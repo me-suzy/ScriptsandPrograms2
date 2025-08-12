eb02232cdea6dedb8d4267c445da5a089464517c
me-suzy <me.suzana@gmail.com>
2025-08-12 08:51:30 +0300

    Add Database Tools/csv_importer/csv_importer/includes/javascript/elements.js

---
+++ Database Tools/csv_importer/csv_importer/includes/javascript/elements.js
@@ -0,0 +1,23 @@
+// ELEMENTS
+var doc = document;
+
+// Form
+var _input = doc.createElement("input");
+var _select = doc.createElement("select");
+
+// Table
+var _table = doc.createElement("table");
+	_table.align = "center";
+	_table.border = 0;
+	_table.cellPadding = 4;
+	_table.cellSpacing = 1;
+var _tbody = doc.createElement("tbody");
+var _tr = doc.createElement("tr");
+var _td = doc.createElement("td");
+
+// Other
+var _a = doc.createElement("a");
+var _br = doc.createElement("br");
+var _div = doc.createElement("div");
+var _img = doc.createElement("img");
+var _p = doc.createElement("p");
