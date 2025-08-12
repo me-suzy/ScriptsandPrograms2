ba06401a8a7a8df481fd07443e531e78edbcff61
me-suzy <me.suzana@gmail.com>
2025-08-12 13:50:12 +0300

    Add Email Systems/fusioncontact_1_Final/fusioncontact_1_Final/fusion contact/inc/subject.db.php

---
+++ Email Systems/fusioncontact_1_Final/fusioncontact_1_Final/fusion contact/inc/subject.db.php
@@ -0,0 +1,22 @@
+<?php
+$sub1="Question";
+$sub2="Request";
+$sub3="Staff Application";
+$sub4="";
+$sub5="";
+$sub6="";
+$sub7="";
+$sub8="";
+$sub9="";
+$sub10="";
+$sv1="question";
+$sv2="request";
+$sv3="staff app";
+$sv4="";
+$sv5="";
+$sv6="";
+$sv7="";
+$sv8="";
+$sv9="";
+$sv10="";
+?>
