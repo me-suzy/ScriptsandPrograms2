bed02c14140dc8f1ae4670d4f4d67e7e73d702de
me-suzy <me.suzana@gmail.com>
2025-08-12 13:55:51 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/admin/delcat.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/admin/delcat.php
@@ -0,0 +1,106 @@
+<?
+$file_rev="041305";
+////////////////////////////////////////////////////////
+//                 phpBannerExchange                  //
+//                   by: Darkrose                     //
+//              (darkrose@eschew.net)                 //
+//                                                    //
+// You can redistribute this software under the terms //
+// of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of  //
+// the License, or (at your option) any later         //
+// version.                                           //
+//                                                    //
+// You should have received a copy of the GNU General //
+// Public License along with this program; if not,    //
+// write to the Free Software Foundation, Inc., 59    //
+// Temple Place, Suite 330, Boston, MA 02111-1307 USA //
+//                                                    //
+//     Copyright 2004 by eschew.net Productions.      //
+//   Please keep this copyright information intact.   //
+////////////////////////////////////////////////////////
+
+include("../config.php");
+include("../css.php");
+include("../lang/admin.php");
+require_once('../lib/template_class.php');
+
+if($use_gzhandler==1){
+	ob_start("ob_gzhandler");
+}
+
+// Begin loginstuff
+if(!$db=@mysql_connect("$dbhost","$dbuser","$dbpass")){
+	include("../lang/errors.php");
+	$err="1";
+	$error.="$LANG_error_header<p>";
+	$error.="$LANG_error_mysqlconnect ";
+	$error.=mysql_error();
+}
+
+@mysql_select_db($dbname,$db);
+
+session_start();
+header("Cache-control: private"); //IE 6 Fix 
+$session=session_id(); 
+$login = $_SESSION['login'];
+$pass = $_SESSION['pass'];
+
+$result = mysql_query("select * from banneradmin where adminuser='$login' AND adminpass='$pass'");
+$get_userinfo=mysql_fetch_array($result);
+$login=$get_userinfo[adminuser];
+$pass=$get_userinfo[adminpass];
+
+    if($login=="" AND $pass=="" OR $pass=="" OR $err=="1") {
+		include("../lang/errors.php");
+		$error.="$LANG_error_header<p>";
+		$error.="$LANG_login_error";
+
+		$page = new Page('../template/admin_error.php');
+		$page->replace_tags(array(
+			'css' => "$css",
+			'session' => "$session",
+			'baseurl' => "$baseurl",
+			'title' => "$exchangename - $LANG_login_error_title",
+			'shorttitle' => "$LANG_login_error_title",
+			'error' => "$error",
+			'menu' => "$menu",
+			'footer' => '../footer.php'));
+		$page->output();
+	session_destroy();
+
+	}else{ 
+
+	$catid=$_REQUEST[catid];
+
+		$get_count=="0";	
+		$check=mysql_query("select * from bannerstats where category=$catid");
+		$get_count=@mysql_num_rows($check);	
+		
+		if($catid == 0){
+			$msg= "$LANG_delcat_defaultcat";
+		}else{
+			if($get_count > 0){	
+				$msg= "$LANG_delcat_acctexist";	
+				$siteyn="Y";
+			}else{	
+					$msg= "$LANG_delcat_sure";	
+					$siteyn="N";
+			}
+		}	
+			
+			$page = new Page('../template/admin_delcat.php');
+			$page->replace_tags(array('session' => "$session",
+				'baseurl' => "$baseurl",
+				'css' => "$css",
+				'title' => "$exchangename - $LANG_delcat_title",
+				'shorttitle' => "$LANG_delcat_title",
+				'catid' => "$catid",
+				'siteyn' => "$siteyn",
+				'button' => "$LANG_delcat_button",
+				'msg' => "$msg",
+				'menu' => 'admin_menuing.php',
+				'footer' => '../footer.php'));
+			$page->output();
+	
+	}
