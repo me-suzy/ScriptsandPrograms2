50bc8fb7e48a6b1215b23859dc7eaf5ebe9d8ea1
me-suzy <me.suzana@gmail.com>
2025-08-13 01:47:44 +0300

    Add Unsorted/cms master/cms-admin/templates/pages-menu.php

---
+++ Unsorted/cms master/cms-admin/templates/pages-menu.php
@@ -0,0 +1,8 @@
+<table cellpadding=0 cellspacing=0 border=0><tr><td colspan=3><img src=images/trans.gif width=1 height=10 border=0></td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top><li></td><td><a href="pages.php"><?php echo $Lang_pages->menu_page_list ?></a></td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top>&nbsp;</td><td>&nbsp;</td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top><li></td><td><a href="pages.php?mode=add_new_page"><?php echo $Lang_pages->menu_add_new_page ?></a></td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top>&nbsp;</td><td>&nbsp;</td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top><li></td><td><a href=# onClick="window.open('templates/pages-edit.php?id=0', 'page_edit', 'width=730,height=480,toolbar=0'); return false;"><?php echo $Lang_pages->menu_edit_home_page ?></a></td></tr>
+<tr><td width=10><img src=images/trans.gif width=10 height=1 border=0></td><td valign=top><li></td><td><a href="pages.php?mode=edit_page_options&id=0"><?php echo $Lang_pages->menu_edit_home_page_option ?></a></td></tr>
+</table>
