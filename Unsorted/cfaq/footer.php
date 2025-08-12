1be4197fc486da081ff89be3f6aef50fec3b284c
me-suzy <me.suzana@gmail.com>
2025-08-13 01:35:34 +0300

    Add Unsorted/cfaq/footer.php

---
+++ Unsorted/cfaq/footer.php
@@ -0,0 +1,11 @@
+</body>
+</html>
+
+<!-- 
+	Put any HTML code that should appear below the FAQ output here.  If you
+	already have a footer file you can include it by using the following line
+	(just make sure you take out the exclamation point).  You can include an
+	HTML or PHP file, just change the name to match your current footer file.
+	
+	< ! ?php include ("footer.php") ?>
+ -->
