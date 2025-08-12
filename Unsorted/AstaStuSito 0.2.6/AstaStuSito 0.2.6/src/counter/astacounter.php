2b3c626845125cbafb635131fcc2fe410059b071
me-suzy <me.suzana@gmail.com>
2025-08-13 00:52:08 +0300

    Add Unsorted/AstaStuSito 0.2.6/AstaStuSito 0.2.6/src/counter/astacounter.php

---
+++ Unsorted/AstaStuSito 0.2.6/AstaStuSito 0.2.6/src/counter/astacounter.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+AstaStuSito ver 0.2.6
+Copyright (C) 2001-2002 isazi <isazi@olografix.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+# Include
+
+include("counter-conf.php");
+
+# If the counter is active, increment it, else begin from "0"
+
+if (file_exists("$Visitor_File")) {	
+
+	$File = fopen("$Visitor_File","r");
+	$Counter = fgets($File,4096);
+	settype($Counter, "integer");
+	fclose($File);
+	unlink("$Visitor_File");
+}
+
+else {
+	$Counter = 0;
+}
+
+$File = fopen("$Visitor_File","w");
+flock($File,2);
+$Counter += 1;
+fputs($File,$Counter);
+flock($File,3);
+fclose($File);
+
+?>
