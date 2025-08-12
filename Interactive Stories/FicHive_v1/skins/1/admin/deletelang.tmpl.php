d190233d8e06ebe3ea21adeae0b1f202340596ff
me-suzy <me.suzana@gmail.com>
2025-08-12 18:48:14 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/deletelang.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/deletelang.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%DELETELANG%></td><td><select name='lgida'><%LANGLIST%></select></td></tr>
+<tr><td class='fframe'><%CHANGETO%></td><td><select name='lgidb'><%LANGLIST%></select></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>' onClick='javascript:return confirm("<%AREYOUSURE%>")'></td></tr>
+</table>
