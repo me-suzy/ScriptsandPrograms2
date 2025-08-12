7911d838327a2f9b188b590e0b60d0b0af075c1b
me-suzy <me.suzana@gmail.com>
2025-08-12 13:39:46 +0300

    Add Education/tcexam-demo/admin/phpPgAdmin/lang/chinese-sim.php

---
+++ Education/tcexam-demo/admin/phpPgAdmin/lang/chinese-sim.php
@@ -0,0 +1,374 @@
+<?php
+
+/**
+* @maintainer He Wei Ping [laser@zhengmai.com.cn]
+*/
+
+// Language and character set
+$lang['applang'] = '¼òÌåÖÐÎÄ£¨Í³Ò»Âë£©';
+$lang['appcharset'] = 'gb2312';
+$lang['applocale'] = 'zh_CN';
+$lang['appdbencoding'] = 'EUC_CN';
+$lang['applangdir'] = 'ltr';
+
+// Basic strings
+$lang['strintro'] = 'Ó­Ê¹ÓÃ phpPgAdmin¡£';
+$lang['strlogin'] = 'µÇÂ¼';
+$lang['strloginfailed'] = 'µÇÂ¼Ê§°Ü';
+$lang['strserver'] = '·þÎñÆ÷';
+$lang['strlogout'] = '×¢Ïú';
+$lang['strowner'] = 'ËùÊôÈË';
+$lang['straction'] = '¹¦ÄÜ';
+$lang['stractions'] = '¹¦ÄÜ';
+$lang['strname'] = 'Ãû×Ö';
+$lang['strdefinition'] = '¶¨Òå';
+$lang['strsequence'] = 'ÐòÁÐ';
+$lang['strsequences'] = 'ÐòÁÐ';
+$lang['stroperators'] = '²Ù×÷';
+$lang['strtypes'] = 'ÀàÐÍ';
+$lang['straggregates'] = '¾Û¼¯';
+$lang['strproperties'] = 'ÊôÐÔ';
+$lang['strbrowse'] = 'ä¯ÀÀ';
+$lang['strdrop'] = 'É¾³ý';
+$lang['strdropped'] = 'ÒÑÉ¾³ý';
+$lang['strnull'] = '¿Õ';
+$lang['strnotnull'] = '·Ç¿Õ';
+$lang['strprev'] = 'ÉÏÒ»¸ö';
+$lang['strnext'] = 'ÏÂÒ»¸ö';
+$lang['strfailed'] = 'Ê§°Ü';
+$lang['strcreate'] = '´´½¨';
+$lang['strcomment'] = '×¢ÊÍ';
+//$lang['strnext'] = 'Next';
+$lang['strlength'] = '³¤¶È';
+$lang['strdefault'] = 'Ä¬ÈÏ';
+$lang['stralter'] = '¸ü¸Ä';
+$lang['strcancel'] = 'È¡Ïû';
+$lang['strsave'] = '´æ´¢';
+$lang['strinsert'] = '²åÈë';
+$lang['strselect'] = 'Ñ¡È¡';
+$lang['strdelete'] = 'É¾³ý';
+$lang['strupdate'] = '¸üÐÂ';
+$lang['strreferences'] = '²Î¿¼';
+$lang['stryes'] = 'ÊÇ';
+$lang['strno'] = '·ñ';
+$lang['stredit'] = '±à¼­';
+$lang['strrows'] = 'ÐÐ';
+$lang['strexample'] = 'Èç£º';
+$lang['strback'] = '·µ»Ø';
+$lang['strqueryresults'] = '²éÑ°½á¹û';
+$lang['strshow'] = 'ÏÔÊ¾';
+$lang['strempty'] = '¿Õ';
+$lang['strlanguage'] = 'ÓïÑÔ';
+
+// Error handling
+$lang['strnoframes'] = 'Äú±ØÊ¹ÓÃÖ§³Ö¿ò¼ÜµÄä¯ÀÀÆ÷ä¯ÀÀ±¾³ÌÐò¡£';
+$lang['strbadconfig'] = 'ÄúµÄ config.inc.php ÒÑÊ§Ð§¡£ÄúÐèÒª×ÔÐÐÍ¨¹ý config.inc.php-ist ÐÞ¸Ä¡£';
+$lang['strnotloaded'] = 'ÄúµÄ PHP ÖÐÃ»ÓÐÍêÕûµÄÊý¾Ý¿âÖ§³Ö¡£';
+$lang['strsqlerror'] = 'SQL:´íÎó';
+$lang['strinstatement'] = 'In statement£º';
+$lang['strinvalidparam'] = 'ÎÞÐ§µÄ½Å±¾²ÎÊý';
+$lang['strnodata'] = '²éÎÞ´ËÐÐ¡£';
+
+// Tables
+$lang['strnotables'] = '²éÎÞ´Ë±í¡£';
+$lang['strnotable'] = '²éÎÞ´Ë±í¡£';
+$lang['strtable'] = 'Êý¾Ý±í';
+$lang['strtables'] = 'Êý¾Ý±í';
+$lang['strtablecreated'] = '½¨±íÍê³É¡£';
+$lang['strtablecreatedbad'] = '½¨±íÊ§°Ü';
+$lang['strtableneedsfield'] = 'ÖÁÉÙÐèÒªÒ»¸öÊý¾Ý¶Î¡£';
+$lang['strinsertrow'] = '²åÈëÐÐ';
+$lang['strrowinserted'] = '²åÈëÐÐÍê³É¡£';
+$lang['strrowinsertedbad'] = 'ÏÈ²åÈëÐÐ¡£';
+$lang['streditrow'] = '¸ü¸ÄÐÐ';
+$lang['strrowupdated'] = 'Íê³ÉÐÐ¸üÐÂ¡£';
+$lang['strrowupdatedbad'] = '¸üÐÂÐÐÊ§°Ü¡£';
+$lang['strdeleterow'] = 'É¾³ýÐÐ';
+$lang['strconfdeleterow'] = 'ÕæµÄÒª³ýËùÓÐµÄÐÐ£¿';
+$lang['strrowdeleted'] = 'É¾³ý³ýÐÐÍê³É¡£';
+$lang['strrowdeletedbad'] = '³ýÐÐÊ§°Ü¡£';
+$lang['strsaveandrepeat'] = 'ÖØ¸´´æ´¢';
+$lang['strconfemptytable'] = 'ÕæµÄÒªÇå¿Õ"%s"Êý¾Ý±í£¿';
+$lang['strtableemptied'] = 'Êý¾Ý±íÇå¿ÕÍê³É¡£';
+$lang['strtableemptiedbad'] = 'Êý¾Ý±íÇå¿ÕÊ§°Ü¡£';
+$lang['strconfdroptable'] = 'ÕæµÄÒªÉ¾³ý³ý"%s"Êý¾Ý±í£¿';
+$lang['strtabledropped'] = 'ÉÆ³ýÊý¾Ý±íÍê³É¡£';
+$lang['strtabledroppedbad'] = 'É¾³ýÊý¾Ý±íÊ§°Ü¡£';
+
+// Users
+$lang['struseradmin'] = 'ÓÃ»§¹ÜÀí';
+$lang['struser'] = 'ÓÃ»§';
+$lang['strusers'] = 'ÓÃ»§';
+$lang['strusername'] = 'ÓÃÃû';
+$lang['strpassword'] = 'ÃÜÂë';
+$lang['strsuper'] = '³¬¼¶ÓÃ»§';
+$lang['strcreatedb'] = '½¨¿â';
+$lang['strexpires'] = '¹ýÆÚ';
+$lang['strnousers'] = '²éÎÞ´ËÓÃ»§';
+
+// Groups
+$lang['strgroupadmin'] = '×é¹ÜÀí';
+$lang['strgroup'] = '×é';
+$lang['strgroups'] = 'Èº×é';
+$lang['strnogroups'] = '²éÎÞÈº×é¡£';
+$lang['strcreategroup'] = '´´½¨×é';
+$lang['strshowallgroups'] = 'ÏÔÊ¾ËùÓÐÈº×é';
+$lang['strgroupneedsname'] = 'Äã±Ø¸øÄú×éÃüÃû¡£';
+$lang['strgroupcreated'] = '½¨×éÍê³É¡£';
+$lang['strgroupcreatedbad'] = '½¨×éÊ§°Ü¡£';
+$lang['strconfdropgroup'] = 'ÕæµÄÒªÉ¾³ý"%s"×é£¿';
+$lang['strgroupdropped'] = 'É¾³ý×éÍê³É¡£';
+$lang['strgroupdroppedbad'] = 'É¾³ý×éÊ§°Ü¡£';
+$lang['strmembers'] = '³ÉÔ±';
+
+// Privilges
+$lang['strprivileges'] = 'ÌØÈ¨';
+$lang['strgrant'] = '¸³Óè';
+$lang['strrevoke'] = '³·»Ø';
+
+// Databases
+$lang['strdatabase'] = 'Êý¾Ý¿â';
+$lang['strdatabases'] = 'Êý¾Ý¿â';
+$lang['strnodatabases'] = '²éÎÞ´ËÊý¾Ý¿â¡£';
+$lang['strdatabaseneedsname'] = 'Äã±ØÐë¸øÄúµÄÊý¾Ý¿âÃüÃû¡£';
+
+// Views
+$lang['strviewneedsname'] = 'Äã±ØÐë¸øÄúµÄÊÓÍ¼ÃüÃû¡£';
+$lang['strviewneedsdef'] = 'Äã±ØÐë¶¨ÒåÄúµÄÊÓÍ¼¡£';
+$lang['strcreateview'] = '½¨Á¢ÊÓÍ¼';
+$lang['strnoviews'] = '²éÎÞÊÓÍ¼¡£';
+$lang['strview'] = 'ÊÓÍ¼';
+$lang['strviews'] = 'ÊÓÍ¼';
+
+// Sequences
+$lang['strnosequences'] = '²éÎÞÐòÁÐ¡£';
+$lang['strsequencename'] = 'ÐòÁÐÃû³Æ';
+$lang['strlastvalue'] = '×îºóµÄÊýÄ¿';
+$lang['strincrementby'] = 'ÔöÁ¿£¨¼Ó£¯¼õ£©';
+$lang['strmaxvalue'] = '×î´óÖµ';
+$lang['strminvalue'] = '×îÐ¡Öµ';
+$lang['strcachevalue'] = 'cache_value';
+$lang['strlogcount'] = 'log_cnt';
+$lang['striscycled'] = 'is_cycled';
+$lang['striscalled'] = 'is_called';
+$lang['strreset'] = 'ÖØÖÃ';
+
+// Indexes
+$lang['strindexes'] = 'Ë÷Òý';
+$lang['strindexname'] = 'Ë÷ÒýÃû';
+$lang['strtabname'] = 'Tab Name';
+$lang['strcolumnname'] = 'Column Name';
+$lang['struniquekey'] = 'Î¨Ò»¼ü';
+$lang['strprimarykey'] = 'Ö÷¼ü';
+$lang['strshowallindexes'] = 'ÏÔÊ¾ËùÓÐË÷Òý';
+$lang['strcreateindex'] = '´´½¨Ë÷Òý';
+$lang['strindexneedsname'] = 'Äã±ØÐë¸øÄúµÄË÷ÒýÃüÃû¡£';
+$lang['strindexneedscols'] = 'Äã±ØÐë¸øÄãµÄ×Ö¶Î¸³ÓèÒ»¸öÕýÕûÊý¡£';
+$lang['strindexcreated'] = '´´½¨Ë÷ÒýÍê³É';
+$lang['strindexcreatedbad'] = '´´½¨Ë÷ÒýÊ§°Ü.';
+$lang['strconfdropindex'] = 'ÕæµÄÒªÉ¾³ý"%s"Ë÷Òý£¿';
+$lang['strindexdropped'] = 'É¾³ýË÷ÒýÍê³É¡£';
+$lang['strindexdroppedbad'] = 'É¾³ý³ýË÷ÒýÊ§°Ü¡£';
+
+// Rules
+$lang['strrules'] = '¹æÔò';
+$lang['strrule'] = '¹æÔò';
+$lang['strnorules'] = '²éÎÞ´Ë¹æÔò';
+$lang['strcreaterule'] = '´´½¨¹æÔò';
+
+// Tables
+$lang['strfield'] = 'ÁÐ';
+$lang['strfields'] = 'ÁÐ';
+$lang['strtype'] = 'ÀàÐÍ';
+$lang['strvalue'] = 'Öµ';
+$lang['strshowalltables'] = 'Ê¾ËùÓÐ±í¡£';
+$lang['strunique'] = 'Î¨Ò»';
+$lang['strprimary'] = 'Ö÷';
+$lang['strkeyname'] = '¼üÃû';
+$lang['strnumfields'] = 'ÁÐÊý';
+$lang['strcreatetable'] = '´´½¨±í';
+$lang['strtableneedsname'] = 'Äã±ØÄúµÄË÷ÒýÃüÃû¡£';
+$lang['strtableneedscols'] = 'Äã±ØÐë¸øÄãµÄ×Ö¶Î¸³ÓèÒ»¸öÕýÕûÊý¡£';
+$lang['strexport'] = 'µ¼³ö';
+$lang['strconstraints'] = 'Ç¿ÖÆ';
+$lang['strcolumns'] = 'ÁÐ';
+
+// Functions
+$lang['strnofunctions'] = '²éÎÞ´Ëº¯Êý';
+$lang['strfunction'] = 'º¯Êý';
+$lang['strfunctions'] = 'º¯Êý';
+$lang['strreturns'] = 'Returns';
+$lang['strarguments'] = '²ÎÊý';
+$lang['strproglanguage'] = 'ÓïÑÔ';
+$lang['strfunctionneedsname'] = 'Äã±ØÐë¸øÄúµÄº¯ÊýÃüÃû¡£';
+$lang['strfunctionneedsdef'] = 'Äã±ØÐë¶¨ÒåÄúµÄº¯Êý¡£';
+
+// Triggers
+$lang['strtrigger'] = '´¥·¢Æ÷';
+$lang['strtriggers'] = '´¥·¢Æ÷';
+$lang['strnotriggers'] = '²éÎÞ´Ë´¥·¢Æ÷¡£';
+$lang['strcreatetrigger'] = '´´½¨´¥·¢Æ÷';
+
+// Types
+$lang['strtype'] = 'ÀàÐÍ';
+$lang['strtypes'] = 'ÀàÐÍ';
+$lang['strnotypes'] = '²éÎÞ´ËÀàÐÍ¡£';
+$lang['strcreatetype'] = '´´½¨ÀàÐÍ';
+$lang['strconfdroptype'] = 'ÕæµÄÒªÉ¾³ý"%s"ÀàÐÍ£¿';
+$lang['strtypedropped'] = 'É¾³ýÀàÐÍÍê³É¡£';
+$lang['strtypedroppedbad'] = 'É¾³ýÀàÐÍÊ§°Ü¡£';
+$lang['strtypecreated'] = '´´½¨ÀàÐÍÍê³É¡£';
+$lang['strtypecreatedbad'] = '½¨ÐÍÊ§°Ü¡£';
+$lang['strshowalltypes'] = 'ÏÔÊ¾ËùÓÐµÄÀàÐÍ';
+$lang['strinputfn'] = 'ÊäÈë¹¦ÄÜ';
+$lang['stroutputfn'] = 'Êä³ö¹¦ÄÜ';
+$lang['strpassbyval'] = 'Passed by val£¿';
+$lang['stralignment'] = 'Alignment';
+$lang['strelement'] = 'ÔªËØ';
+$lang['strdelimiter'] = '·Ö¸ô·û';
+$lang['strstorage'] = '´ÅÅÌ´æ´¢';
+$lang['strtypeneedsname'] = 'Äã±Ø¸øÄúµÄÀàÐÍÃüÃû¡£';
+$lang['strtypeneedslen'] = 'Äã±Ø¸øÄúµÄÀàÐÍ¶¨ÒåÒ»¸ö³¤¶È¡£';
+
+// Schemas
+$lang['strschema'] = 'Ä£Ê½';
+$lang['strschemas'] = 'Ä£Ê½';
+$lang['strcreateschema'] = '´´½¨Ä£Ê½';
+$lang['strnoschemas'] = 'Ã»ÓÐ´ËÄ£Ê½';
+$lang['strconfdropschema'] = 'ÄãÈ·¶¨ÒªÉ¾³ý"%s"Ä£Ê½Ã´£¿';
+$lang['strschemadropped'] = 'Ä£Ê½ÒÑÉ¾³ý';
+$lang['strschemadroppedbad'] = 'Ä£Ê½É¾³ýÊ§°Ü';
+$lang['strschemacreated'] = 'Ä£Ê½ÒÑ½¨Á¢';
+$lang['strschemacreatedbad'] = '´´½¨Ä£Ê½Ê§°Ü';
+$lang['strshowallschemas'] = 'ÏÔÊ¾ËùÓÐÄ£Ê½?';
+$lang['strschemaneedsname'] = '±ØÐë¸øÄ£Ê½ÃüÃû';
+
+$lang['strcreated'] = '´´½¨Íê³É';
+$lang['strok'] = 'Íê³É';
+$lang['strencoding'] = '±àÂë';
+$lang['strsql'] = 'SQLÂë';
+$lang['stradmin'] = '¹ÜÀí';
+$lang['strvacuum'] = 'Çå¿Õ';
+$lang['stranalyze'] = '·ÖÎö';
+$lang['strreindex'] = 'ÖØ½¨Ë÷Òý';
+$lang['strrun'] = 'Ö´ÐÐ';
+$lang['stradd'] = '´´½¨';
+$lang['strformat'] = '¸ñÊ½»¯';
+$lang['strtablename'] = '×ÊÁÏ±íÃû³Æ';
+$lang['strfieldneedsname'] = 'Äã±ØÐë¸øÄúµÄÊý¾Ý¶ÎÃüÃû';
+$lang['struserupdated'] = 'ÓÃ»§¸üÐÂ';
+$lang['struserupdatedbad'] = 'ÓÃ»§¸üÐÂ';
+$lang['strshowallusers'] = 'ÏÔÊ¾ËùÓÐÓÃ»§';
+$lang['strcreateuser'] = '´´½¨ÓÃ»§';
+$lang['strusercreated'] = '´´½¨ÓÃ»§Íê³É';
+$lang['strusercreatedbad'] = '´´½¨ÓÃ»§Ê§°Ü';
+$lang['strconfdropuser'] = 'ÄúÈ·¶¨ÒªÉ¾³ýÓÃ»§"%s"Ã´£¿';
+$lang['struserdropped'] = 'ÓÃ»§É¾³ýÍê³É';
+$lang['struserdroppedbad'] = 'É¾³ýÓÃ»§Ê§°Ü';
+$lang['strnogroup'] = '²éÎÞ´ËÈº×é¡£';
+$lang['strprivilege'] = 'ÌØÈ¨';
+$lang['strgranted'] = 'ÌØÈ¨¸³ÓèÍê³É';
+$lang['strgrantfailed'] = 'ÌØÈ¨¸³ÓèÊ§°Ü';
+$lang['strgrantuser'] = 'ÌØÈ¨¸³ÓèÓÃ»§';
+$lang['strgrantgroup'] = 'ÌØÈ¨¸³Óè×é';
+$lang['strshowalldatabases'] = 'ÏÔÊ¾ËùÓÐÊý¾Ý¿â';
+$lang['strnodatabase'] = '²éÎÞ´ËÊý¾Ý¿â¡£';
+$lang['strcreatedatabase'] = '´´½¨Êý¾Ý¿â';
+$lang['strdatabasename'] = 'Êý¾Ý¿âÃû³Æ';
+$lang['strdatabasecreated'] = '´´½¨Êý¾Ý¿âÍê³É';
+$lang['strdatabasecreatedbad'] = '´´½¨Êý¾Ý¿âÊ§°Ü';
+$lang['strconfdropdatabase'] = 'ÄúÈ·¶¨ÒªÉ¾³ýÊý¾Ý¿â"%s"Ã´£¿';
+$lang['strdatabasedropped'] = 'Êý¾Ý¿âÉ¾³ýÍê³É';
+$lang['strdatabasedroppedbad'] = 'É¾³ýÊý¾Ý¿âÊ§°Ü';
+$lang['strentersql'] = 'ÇëÔÚÏÂ·½ÊäÈëÒªÖ´ÐÐµÄSQLÂë£º';
+$lang['strvacuumgood'] = 'Çå¿ÕÍê³É';
+$lang['strvacuumbad'] = 'Çå¿ÕÊ§°Ü';
+$lang['stranalyzegood'] = '·ÖÎöÍê³É';
+$lang['stranalyzebad'] = '·ÖÎöÊ§°Ü';
+$lang['strshowallviews'] = 'ÏÔÊ¾ËùÊÓÍ¼';
+$lang['strnoview'] = '²éÎÞ´ËÊÓÍ¼';
+$lang['strviewname'] = 'ÊÓÍ¼Ãû³Æ';
+$lang['strviewcreated'] = '´´½¨ÊÓÍ¼Íê³É';
+$lang['strviewcreatedbad'] = '´´½¨ÊÓÍ¼Ê§°Ü';
+$lang['strconfdropview'] = 'ÄúÈ·¶¨ÒªÉ¾³ýÊÓÍ¼"%s"Ã´£¿';
+$lang['strviewdropped'] = 'ÊÓÍ¼É¾³ýÍê³É';
+$lang['strviewdroppedbad'] = 'É¾³ýÊÓÍ¼Ê§°Ü';
+$lang['strviewupdated'] = 'ÊÓÍ¼¸üÐÂÍê³É';
+$lang['strviewupdatedbad'] = 'ÊÓÍ¼¸üÐÂÊ§°Ü';
+$lang['strshowallsequences'] = 'ÏÔÊ¾ËùÓÐÐòÁÐ';
+$lang['strnosequence'] = '²éÎÞ´ËÐòÁÐ';
+$lang['strcreatesequence'] = '´´½¨ÐòÁÐ';
+$lang['strstartvalue'] = 'ÆðÊ¼Öµ';
+$lang['strsequenceneedsname'] = 'Äã±ØÐë¸øÄúµÄÐòÁÐÃüÃû';
+$lang['strsequencecreated'] = '´´½¨ÐòÁÐÍê³É';
+$lang['strsequencecreatedbad'] = '´´½¨ÐòÁÐÊ§°Ü';
+$lang['strconfdropsequence'] = 'ÄúÈ·¶¨ÒªÉ¾³ýÐòÁÐ"%s"Ã´£¿';
+$lang['strsequencedropped'] = 'ÐòÁÐÉ¾³ýÍê³É';
+$lang['strsequencedroppedbad'] = 'É¾³ýÐòÁÐÊ§°Ü';
+$lang['strnoindex'] = '²éÎÞ´ËË÷Òý';
+$lang['strnoindexes'] = '²éÎÞ´ËË÷Òý';
+$lang['strindextype'] = 'Ë÷ÒýµÄÀàÐÍ';
+$lang['strshowallrules'] = 'ÏÔÊ¾ËùÓÐ¹æÔò';
+$lang['strnorule'] = '²éÎÞ´Ë¹æÔò';
+$lang['strrulename'] = '¹æÔòÃû³Æ';
+$lang['strruleneedsname'] = 'Äã±ØÐë¸øÄúµÄ¹æÔòÃüÃû';
+$lang['strrulecreated'] = '´´½¨¹æÔòÍê³É';
+$lang['strrulecreatedbad'] = '´´½¨¹æÔòÊ§°Ü';
+$lang['strconfdroprule'] = 'ÄúÈ·¶¨Òª°Ñ¹æÔò"%s"ÔÚ×ÊÁÏ¿â"%s"ÖÐÉ¾³ýÃ´£¿';
+$lang['strruledropped'] = '¹æÔòÉ¾³ýÍê³É';
+$lang['strruledroppedbad'] = 'É¾³ý¹æÔòÊ§°Ü';
+$lang['strshowallconstraints'] = 'ÏÔÊ¾ËùÓÐÇ¿ÖÆ';
+$lang['strnoconstraints'] = '²éÎÞ´ËÇ¿ÖÆ';
+$lang['strcreateconstraint'] = '´´½¨Ç¿ÖÆ';
+$lang['strconstraintcreated'] = '´´½¨Ç¿ÖÆÍê³É';
+$lang['strconstraintcreatedbad'] = '´´½¨Ç¿ÖÆÊ§°Ü';
+$lang['strconfdropconstraint'] = 'ÄúÈ·¶¨Òª°ÑÇ¿ÖÆ"%s"ÔÚ×ÊÁÏ¿â"%s"ÖÐÉ¾³ýÃ´£¿';
+$lang['strconstraintdropped'] = 'É¾³ýÇ¿ÖÆÍê³É';
+$lang['strconstraintdroppedbad'] = 'É¾³ýÇ¿ÖÆÊ§°Ü';
+$lang['straddpk'] = 'ÐÂÔöÖ÷¼ü';
+$lang['strpkneedscols'] = 'Ö÷¼üÐëÒªÖÁÉÙÒ»ÁÐ';
+$lang['strpkadded'] = 'Ö÷¼üÐÂÔöÍê³É';
+$lang['strpkaddedbad'] = 'ÐÂÔöÖ÷¼üÊ§°Ü';
+$lang['strshowallfunctions'] = 'ÏÔÊ¾ËùÓÐº¯Êý';
+$lang['strnofunction'] = '²éÎÞ´Ëº¯Êý';
+$lang['strcreatefunction'] = '´´½¨º¯Êý';
+$lang['strfunctionname'] = 'º¯ÊýÃû³Æ';
+$lang['strfunctioncreated'] = '´´½¨º¯ÊýÍê³É';
+$lang['strfunctioncreatedbad'] = '´´½¨º¯ÊýÊ§°Ü';
+$lang['strconfdropfunction'] = 'ÄúÈ·¶¨ÒªÉ¾³ýº¯Êý"%s"Ã´£¿';
+$lang['strfunctiondropped'] = 'É¾³ýº¯ÊýÍê³É.';
+$lang['strfunctiondroppedbad'] = 'É¾³ýº¯ÊýÊ§°Ü';
+$lang['strfunctionupdated'] = '¸üÐÂº¯ÊýÍê³É.';
+$lang['strfunctionupdatedbad'] = '¸üÐÂº¯ÊýÊ§°Ü';
+$lang['strshowalltriggers'] = 'ÏÔÊ¾ËùÓÐ´¥·¢Æ÷';
+$lang['strnotrigger'] = '²éÎÞ´Ë´¥·¢Æ÷';
+$lang['strtriggerneedsname'] = 'Äã±ØÐë¸øÄúµÄ´¥·¢Æ÷ÃüÃû';
+$lang['strtriggerneedsfunc'] = 'Äã±ØÐë¸øÄúµÄ´¥·¢Æ÷Ò»¸öº¯Êý';
+$lang['strtriggercreated'] = '´´½¨´¥·¢Æ÷Íê³É';
+$lang['strtriggercreatedbad'] = '´´½¨´¥·¢Æ÷Ê§°Ü';
+$lang['strconfdroptrigger'] = 'ÄúÈ·¶¨Òª°Ñ´¥·¢Æ÷"%s"ÔÚ×ÊÁÏ¿â"%s"ÖÐÉ¾³ýÃ´£¿';
+$lang['strtriggerdropped'] = 'É¾³ý´¥·¢Æ÷Íê³É.';
+$lang['strtriggerdroppedbad'] = 'É¾³ý´¥·¢Æ÷Ê§°Ü';
+$lang['strnotype'] = '²éÎÞ´ËÀàÐÍ';
+$lang['strtypename'] = 'ÀàÐÍÃû³Æ';
+$lang['strnoschema'] = '²éÎÞ´ËÄ£Ê½';
+$lang['strschemaname'] = 'Ä£Ê½Ãû³Æ';
+$lang['strreport'] = '±¨±í';
+$lang['strreports'] = '±¨±í';
+$lang['strshowallreports'] = 'ÏÔÊ¾ËùÓÐ±¨±í';
+$lang['strnoreports'] = '²éÎÞ´Ë±¨±í';
+$lang['strcreatereport'] = '´´½¨±¨±í';
+$lang['strreportdropped'] = '´´½¨±¨±íÍê³É.';
+$lang['strreportdroppedbad'] = 'É¾³ý±¨±íÊ§°Ü';
+$lang['strconfdropreport'] = 'ÄúÈ·¶¨ÒªÉ¾³ý±¨±í"%s"Ã´£¿';
+$lang['strreportneedsname'] = 'Äã±ØÐë¸øÄúµÄ±¨±íÃüÃû';
+$lang['strreportcreated'] = '´¢´æ±¨±íÍê³É';
+$lang['strreportcreatedbad'] = '´¢´æ±¨±íÊ§°Ü';
+
+
+
+// Miscellaneous
+$lang['strtopbar'] = '%s ¼ÜÓÚ %s£º%s £­ ÄúÊÇ "%s"£¬ %s';
+$lang['strtimefmt'] = 'jS M, Y g:iA';
+
+?>
