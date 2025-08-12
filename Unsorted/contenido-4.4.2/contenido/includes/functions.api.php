f01ebd4e75e80fe7c142c9bd51e643d37712ed6a
me-suzy <me.suzana@gmail.com>
2025-08-13 01:59:06 +0300

    Add Unsorted/contenido-4.4.2/contenido/includes/functions.api.php

---
+++ Unsorted/contenido-4.4.2/contenido/includes/functions.api.php
@@ -0,0 +1,25 @@
+<?php
+
+/*****************************************
+* File      :   $RCSfile: functions.api.php,v $
+* Project   :   Contenido
+* Descr     :   Contenido API loader
+*
+* Author    :   $Author: jan $
+*               
+* Created   :   08.08.2003
+* Modified  :   $Date: 2003/10/08 15:11:13 $
+*
+* © four for business AG, www.4fb.de
+*
+* $Id: functions.api.php,v 1.4 2003/10/08 15:11:13 jan Exp $
+******************************************/
+
+cInclude('includes', 'functions.api.category.php');
+cInclude('includes', 'functions.api.string.php');
+cInclude('includes', 'functions.api.images.php');
+cInclude('classes',  'class.templateconfig.php');
+cInclude('classes',  'class.article.php');
+
+
+?>
