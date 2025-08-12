ac622d19048a800e6cefd9ecb9d92813c44c9970
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:59 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/conditions.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/conditions.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Allgemeine Gesch&auml;ftsbedingungen');
+define('HEADING_TITLE', 'Allgemeine Gesch&auml;ftsbedingungen');
+
+define('TEXT_INFORMATION', 'F&uuml;gen Sie hier Ihre allgemeinen Gesch&auml;ftsbedingungen ein.');
+?>
