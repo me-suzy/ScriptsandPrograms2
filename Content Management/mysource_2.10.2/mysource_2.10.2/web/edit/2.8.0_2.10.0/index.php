de4c55d89e3127869215747cb9d54ba55935a279
me-suzy <me.suzana@gmail.com>
2025-08-12 07:24:15 +0300

    Add Content Management/mysource_2.10.2/mysource_2.10.2/web/edit/2.8.0_2.10.0/index.php

---
+++ Content Management/mysource_2.10.2/mysource_2.10.2/web/edit/2.8.0_2.10.0/index.php
@@ -0,0 +1,103 @@
+<?
+$files_to_ignore = array('blank.php', 'index.php', 'upgrade_functions.php', 'upgrade_template.php');
+
+require_once('../../init.php');
+
+$GLOBALS['UPGRADE_FROM'] = '2.8.0';
+$GLOBALS['UPGRADE_TO']   = '2.10.0';
+
+$dir = dirname(__FILE__);
+if (!is_writable($dir) || !is_dir($dir)) {
+    report_error(__FILE__, __LINE__, 'Unable to run upgrade the following directory is not writable : '.$dir);
+}
+
+error_reporting(5);
+ ####################################################################
+# tell anyone who isn't root to go away
+if (!$SESSION->logged_in()) {
+	$SESSION->login_screen("Upgrade to $UPGRADE_TO", "You must be logged in.");
+	exit();
+} else if (!user_root()) {
+	$SESSION->login_screen("Upgrade to $UPGRADE_TO", "You must be a <b>root</b> to upgrade the system.",$SESSION->user->login);
+	exit();
+}
+
+?>
+<html>
+<head>
+<title>Upgrade MySource Version</title>
+<body>
+<?
+
+if (!$_REQUEST['system_backed_up']) {
+	?>
+	<div style="font-family: verdana">
+	Upgrading MySource <?=$UPGRADE_FROM?> to <?=$UPGRADE_TO?>...<br><br>
+	<form method="post" action="<?=$_SERVER['PHP_SELF']?>">
+	<b>Are you sure that you have backed up your system? You only need to make copies of the <b>data</b> directory and dump you MySource database(s).</b><p>
+	<input type="hidden" name="time" value="<?=microtime()?>">
+	<input type="submit" name="system_backed_up" value="&nbsp;&nbsp;&nbsp;Yes&nbsp;&nbsp;&nbsp;">
+	<input type="button" value="&nbsp;&nbsp;&nbsp;No&nbsp;&nbsp;&nbsp;" onClick="javascript: alert('Well get on with it :)');">
+	</form>
+	</div>
+	<?
+	exit;
+}
+
+?>
+These are the upgrade scripts that need to run to upgrade MySource from version <?=$GLOBALS['UPGRADE_FROM']?> to version <?=$GLOBALS['UPGRADE_TO']?>.<br />
+<?
+$upgrade_files = array();
+$dir = opendir(dirname(__FILE__));
+
+while(($file = readdir($dir)) !== FALSE) {
+	if ($file == '.' || $file == '..') continue;
+	if (is_dir($file)) continue;
+	$ext = end(explode('.',$file));
+	if ($ext == 'php' && !in_array($file, $files_to_ignore)) $upgrade_files[] = $file;
+}
+closedir($dir);
+
+function cmp_sort_upgrade_scripts($a, $b) {
+	static $re = '/^([^0-9]*)([0-9.]+).*$/';
+	if (preg_match($re, $a, $a_matches) && preg_match($re, $b, $b_matches)) {
+		if ($a_matches[1] == $b_matches[1]) {
+			return version_no_compare($a_matches[2], $b_matches[2]);
+
+		}
+
+	} 
+	// otherwise just default to the string compare
+	return strcmp($a, $b);
+}
+
+usort($upgrade_files, 'cmp_sort_upgrade_scripts');
+
+$upgrades_done = array();
+
+foreach($upgrade_files as $file) {
+	if (is_file($file . '.success')) {
+		$upgrades_done[] = $file;
+		?><span class="success">Upgrade <?=$file?> has been run successfully.</span><br /><?
+	} elseif (is_file($file . '.failure')) {
+		?><span class="failure">Upgrade <a href="<?=$file?>" target="upgrade_bottom"><?=$file?></a> has been run, but failed.</span><br /><?
+	} elseif (is_file($file . '.ignore')) {
+		$upgrades_done[] = $file;
+		?><span class="ignore">Upgrade <?=$file?> doesn't need to run.</span><br /><?
+	} else {
+		?><a href="<?=$file?>" target="upgrade_bottom"><?=$file?></a><br /><?
+	}
+}
+
+sort($upgrades_done);
+
+if (equal_arrays($upgrades_done, $upgrade_files)) {
+	?>
+	All Upgrades Complete!<br />
+	<a href="../index.php">Go back to site.</a><br />
+	<?
+}
+
+?>
+</body>
+</html>
