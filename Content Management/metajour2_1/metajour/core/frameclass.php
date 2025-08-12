30ebfe13c23d093328dccf0ff5d9751a8dbeb16e
me-suzy <me.suzana@gmail.com>
2025-08-12 06:02:45 +0300

    Add Content Management/metajour2_1/metajour/core/frameclass.php

---
+++ Content Management/metajour2_1/metajour/core/frameclass.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage core
+ */
+
+require_once('basicclass.php');
+
+class frame extends basic {
+
+	function frame() {
+		$this->basic();
+		$this->addcolumn('name',0,UI_STRING);
+		$this->addcolumn('pageid',0,UI_RELATION,'document');
+	
+		$this->removeview('createvariant');
+		$this->removeview('createfuture');
+		$this->removeview('approvepublish');
+		$this->removeview('requestapproval');
+		$this->addview('default');
+	}
+
+}
