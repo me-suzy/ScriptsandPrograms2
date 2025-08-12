4ceaee2e3801499c23981d17c1b437b654c6393b
me-suzy <me.suzana@gmail.com>
2025-08-12 23:50:17 +0300

    Add Site Navigation/cm/cjgmenu/w.php

---
+++ Site Navigation/cm/cjgmenu/w.php
@@ -0,0 +1,32 @@
+<?php
+/*------------------------------------------------------------------------------
+CJG MENU v1.0 - Html Tree Menu Structure - Copyright (C) 2002 CARLOS GUERLLOY  
+cjgmenu@guerlloy.com
+guerlloy@hotmail.com
+carlos@weinstein.com.ar
+Buenos Aires, Argentina
+--------------------------------------------------------------------------------
+This program is free software; you can  redistribute it and/or  modify it  under
+the terms   of the   GNU General   Public License   as published   by the   Free
+Software Foundation; either  version 2   of the  License, or  (at  your  option)
+any  later version. This program  is  distributed in  the hope that  it  will be
+useful,  but  WITHOUT  ANY  WARRANTY;  without  even  the   implied  warranty of
+MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR  PURPOSE.  See the  GNU   General
+Public License for   more details. You  should have received  a copy of  the GNU
+General Public License along  with this  program; if   not, write  to the   Free
+Software  Foundation, Inc.,  59 Temple Place,  Suite 330, Boston,  MA 02111-1307
+USA
+------------------------------------------------------------------------------*/
+?>
+
+<HTML>
+<HEAD>
+<META http-equiv="Expires" content="MON, 4 JUL 1967 12:00:00 GMT">
+</HEAD>
+<BODY>
+<HR>
+<H1><?php echo $p; ?></H1>
+<HR>
+</BODY>
+</HTML>
+
