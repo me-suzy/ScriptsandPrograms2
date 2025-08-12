82a297b763e40662c6771c3d88e4b5b6eff90b96
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:36 +0300

    Add Content Management/metajour2_1/metajour/lang/structureelement.en.php

---
+++ Content Management/metajour2_1/metajour/lang/structureelement.en.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_pageid'] = 'Attached document';
+$LANG['label_name'] = 'Text';
+$LANG['label_showtype'] = 'Type';
+$LANG['label_url'] = 'External URL';
+$LANG['label_templateid'] = 'Template';
+$LANG['label_binfile1'] = 'File';
+$LANG['label_binfile2'] = 'File #2';
+$LANG['label_binfile3'] = 'File #3';
+$LANG['label_image1'] = 'Design image #1';
+$LANG['label_image2'] = 'Design image #2';
+$LANG['label_image3'] = 'Design image #3';
+$LANG['label_structureid'] = 'Structure attachment';
+$LANG['label_target'] = 'Target';
+$LANG['option_showtype'][0] = 'Normal';
+$LANG['option_showtype'][1] = 'Open in new window';
+$LANG['name'] = 'Structure records';
+
+?>
