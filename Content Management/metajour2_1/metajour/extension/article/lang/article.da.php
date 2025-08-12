e204fd411d2e0be9766badd22a1e429ad795863e
me-suzy <me.suzana@gmail.com>
2025-08-12 06:12:38 +0300

    Add Content Management/metajour2_1/metajour/extension/article/lang/article.da.php

---
+++ Content Management/metajour2_1/metajour/extension/article/lang/article.da.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['name'] = 'Konfiguration af Artikelliste';
+$LANG['label_name'] = 'Navn';
+$LANG['label_title'] = 'Titel';
+$LANG['label_categoryid'] = 'Kategori';
+$LANG['label_sortorder'] = 'Sortering';
+$LANG['option_sortorder'] = array('Ældste først', 'Nyeste først');
+$LANG['label_excerptlength'] = 'Længde af uddrag';
+$LANG['label_timelimit'] = 'Vis dage';
+$LANG['label_numlimit'] = 'Vis antal';
+$LANG['label_showheader'] = 'Vis overskrift';
+$LANG['label_showsubheader'] = 'Vis underoverskrift';
+$LANG['label_showdate'] = 'Vis dato';
+$LANG['label_showowner'] = 'Vis ejer';
+$LANG['label_showexcerpt'] = 'Vis uddrag';
+$LANG['label_templateid'] = 'Skabelon';
+?>
