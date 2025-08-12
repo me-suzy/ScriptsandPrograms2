95c26ee197a6f5e03f7f4a524359af1717af5833
me-suzy <me.suzana@gmail.com>
2025-08-12 11:57:09 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/logoff.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/logoff.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id $
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Salir');
+define('NAVBAR_TITLE', 'Salir');
+define('TEXT_MAIN', 'Ha solicitado salir de su cuenta. Ahora es seguro abandonar el ordenador.<br><br>Su carrito de la compra ha sido guardado y el contenido del mismo ser&aacute; restaurado cuando vuelva a entrar en su cuenta.');
+?>
