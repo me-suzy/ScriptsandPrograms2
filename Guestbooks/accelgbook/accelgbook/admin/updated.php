868302d5f66952772f0dfe3cd13f77e1370e13bc
me-suzy <me.suzana@gmail.com>
2025-08-12 17:27:40 +0300

    Add Guestbooks/accelgbook/accelgbook/admin/updated.php

---
+++ Guestbooks/accelgbook/accelgbook/admin/updated.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>deleted</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="../forText.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+<!--
+.style1 {
+	color: #FF3300;
+	font-size: 12px;
+}
+-->
+</style>
+</head>
+
+<body>
+<table width="600" border="0" cellspacing="1" cellpadding="2">
+  <tr>
+    <td height="30" class="forTableBgLeft"><div align="center">Modified Successfully</div></td>
+  </tr>
+  <tr>
+    <td class="forTableBgRight"><div align="center">Record ID: <?php echo $_GET['modifyID']; ?> has been modified</div></td>
+  </tr>
+</table>
+</body>
+</html>
