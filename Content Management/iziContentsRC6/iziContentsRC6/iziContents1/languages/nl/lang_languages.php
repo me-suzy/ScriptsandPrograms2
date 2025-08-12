c7f68c2fc5cb4ebbc696a7141ba56d8879fdf338
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:40 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_languages.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_languages.php
@@ -0,0 +1,56 @@
+<?php
+
+// 21-9-2005 19:24, BD
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Beheer talen';
+$GLOBALS["tFormTitle2"] = 'Voeg een nieuwe taal toe';
+
+//  List Headings
+$GLOBALS["tLanguageCode"] = 'Taal code';
+$GLOBALS["tLanguage"] = 'Taal';
+$GLOBALS["tCharSet"] = 'Karakter set';
+$GLOBALS["tDirection"] = 'Richting';
+$GLOBALS["tEnabled"] = 'Aanzetten';
+$GLOBALS["tFileName"] = 'Taal bestandsnaam';
+$GLOBALS["tFileNameM"] = 'Module bestandsnaam';
+
+//  List Functions
+$GLOBALS["tAddNew"] = 'Zet taal aan';
+$GLOBALS["tViewLanguage"] = 'Bekijk taal instellingen';
+$GLOBALS["tEditLanguage"] = 'Bewerk taal instellingne';
+$GLOBALS["tViewFiles"] = 'Bekijk taal bestanden';
+$GLOBALS["tEditFiles"] = 'Bewerk taal bestanden';
+$GLOBALS["tViewFile"] = 'Bekijk taal bestand';
+$GLOBALS["tEditFile"] = 'Bewerk taal bestand';
+$GLOBALS["tRet_Languages"] = 'Terug naar de lijst met talen';
+$GLOBALS["tToggleLanguage"] = 'activate/deactivate Language ';
+$GLOBALS["defaultLangError"] = 'Language cannot be disabled because they are set to DEFAULT';
+
+//  Form Block Titles
+$GLOBALS["thLanguageGeneral"] = 'Taal details';
+$GLOBALS["thMainFiles"] = 'iziContents hoofd bestanden';
+$GLOBALS["thModuleFiles"] = 'Module bestanden';
+$GLOBALS["thJumpRef"] = '-- Selecteer blok --';
+$GLOBALS["thJumptoBottom"] = 'Naar onderzijde formulier';
+$GLOBALS["thJumptoTop"] = 'Naar bovenzijde formulier';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Dit formulier laat je talen definieren en karakter sets.';
+$GLOBALS["hLanguage"] = 'Dit is de naam van de taal.';
+$GLOBALS["hCharSet"] = 'Dit is de karakter set voor deze taal.';
+$GLOBALS["hFileName"] = 'Selecteer de taal om te laden.';
+$GLOBALS["hDirection"] = 'Is deze taal geschreven van links naar rechts net als Nederlands, of van rechts naar links net als Arabisch?';
+
+$GLOBALS["tRtL"] = 'Recht naar links';
+$GLOBALS["tLtR"] = 'Links naar rechts';
+$GLOBALS["tRtLs"] = 'rnl';
+$GLOBALS["tLtRs"] = 'lnr';
+
+//  Error Messages
+$GLOBALS["eNoLanguage"] = 'Taal-naam mag niet leeg gelaten worden.';
+$GLOBALS["eNoFile"] = 'geef een bestandsnaam voor de taal.';
+$GLOBALS["eLanguageExists"] = 'Deze taal bestaat reeds.';
+$GLOBALS["eNoEnableReady"] = 'Er zijn geen taalbestanden klaar om ingeschakeld te worden.';
+
+?>
