686c5876d29c88d58f9ea61ab6aed1cfedf0908d
me-suzy <me.suzana@gmail.com>
2025-08-12 04:01:44 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_zh.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_zh.php
@@ -0,0 +1,15 @@
+<?php
+
+$GLOBALS["module_title"] = '·j´Mºô¯¸';
+$GLOBALS["search_keywords"] = 'ÃöÁä¦r';
+$GLOBALS["search_OR"] = '¥ô¦óÃöÁä¦rªº²Õ¦X';
+$GLOBALS["search_AND"] = '©Ò¦³ÃöÁä¦r';
+$GLOBALS["search_titles"] = '·j´M¼ÐÃD';
+$GLOBALS["search_teasers"] = 'ºK­n¤å¦r';
+$GLOBALS["search_contents"] = '¤º®e';
+$GLOBALS["search_button"] = '·j´M';
+$GLOBALS["eEnterKeywords"] = '½Ð¿é¤JÃöÁä¦r.';
+$GLOBALS["eEnterElements"] = '½Ð¿é¤J±z©Ò­n·j´Mªººô­¶¤¸¯À.';
+$GLOBALS["eNoResults"] = '¨S¦³µ²ªG.';
+
+?>
