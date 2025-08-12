9d7c64db54a5b98d527b41e0b45254f534d4fae1
me-suzy <me.suzana@gmail.com>
2025-08-12 19:50:06 +0300

    Add News Publishing/NewMan147/NewMan147/newman/footer.php

---
+++ News Publishing/NewMan147/NewMan147/newman/footer.php
@@ -0,0 +1,14 @@
+</td>
+</tr>
+</table>
+<table width="796" cellspacing="0" cellpadding="0" border="0">
+ <tr><td bgcolor="#000000" height="1"></td></tr>
+ <tr>
+  <td class="SmallT" bgcolor="#9EC5E4" align="center">
+   Powered by phpNewMan Version <?=$newman_ver;?><br />
+   Copyright &copy; 2000 - 2003 The <a href="http://skintech.skinbase.org">SkinTech</a> Group 
+  </td>
+ </tr>
+</table>
+</body>
+</html>
