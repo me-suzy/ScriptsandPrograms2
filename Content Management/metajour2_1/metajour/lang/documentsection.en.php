8e50cbbff1803218dd074270bc81858703398b1d
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:28 +0300

    Add Content Management/metajour2_1/metajour/lang/documentsection.en.php

---
+++ Content Management/metajour2_1/metajour/lang/documentsection.en.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Section name';
+$LANG['label_subname'] = 'Subname';
+$LANG['label_content'] = 'Content';
+$LANG['label_extension'] = 'Extension';
+$LANG['label_configset'] = 'Configuration';
+$LANG['label_params'] = 'Parameters';
+$LANG['label_script'] = 'Allow inline scripting';
+$LANG['label_file'] = 'File';
+$LANG['name'] = 'Documentsections';
+
+?>
