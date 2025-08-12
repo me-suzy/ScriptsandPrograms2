dd11349eb10dede771944ac48066b61e36edd5e8
me-suzy <me.suzana@gmail.com>
2025-08-12 19:20:26 +0300

    Add Link Indexing/phpOnDirectory/phpOnDirectory/aboutus.php

---
+++ Link Indexing/phpOnDirectory/phpOnDirectory/aboutus.php
@@ -0,0 +1,55 @@
+<?php
+# choose a banner
+include_once('includes/db_connect.php');
+include_once('banner.inc.php');
+include('Templates/maintemplate.header.inc.php');
+?>
+        <table border="0" cellpadding="0" cellspacing="0" width="95%">
+          <tr>
+            <td width="100%" height="30" colspan="3" >
+              <h1>About Us</h1></td>
+          </tr>
+          <tr>
+            <td width="100%" valign="top" align="left" colspan="3">
+            </td>
+          </tr>
+          <tr>
+            <td width="100%" colspan="3">
+              <b>With the mass of dating sites available to choose from, <?php echo $CONST_LINK_SITE?> tries to help consumers find sites specific to their interest groups or geographic region by promoting not only the larger sites, but also some of the small and medium sized niche sites. We try to maintain quality by hand checking each site submitted. We observe the following guidelines: </b>
+              <p>No affiliate sites are permitted so that you don't get directed to
+        the same site time and again. All sites must provide their own unique
+        content.</p>
+        <p>Each site must be within it's own domain. We do not permit sites that
+        are unprofessional and hosted by such providers as geocities or tripod.</p>
+        <p>Sites must be what they say they are. Sites claiming to be 100% free
+        but are not are not listed in that category. So many sites claim to be
+        free and this unfair to you the user.</p>
+        <p>Poor design, multiple pop-ups, poor performance (slow sites), sites
+        that won't let you leave, sites that update your browser to make their
+        page your home page, sites that try to install diallers or cheat you in
+        any way are not listed.</p>
+        <p><i>If you think that any of the sites listed on this site do not meet
+        this criteria then please mail us and let us know the site in question
+        and we will investigate it.</i></p>
+        <p><?php echo $CONST_LINK_SITE ?> was established to provide a focused listing
+        forum for dating services and dating related resources. The site is free
+        to use for both the dating service and the browser.</p>
+        <p>Some sites that we consider to be exceptional are listed under our
+        Top Dating Sites category. Other categories Matchmaking sites,
+        International dating sites, Regional sites, Adult services etc. We have excluded
+        affiliate sites (such as the one-and-only network) and have only listed online dating services that
+        provide you with unique content and that we feel
+        meet the quality of service that you should expect. </p>
+        <p> Some of these sites
+        in our dating directory are 100% free and others require payment but
+        have many free aspects to their dating service that mean you can review
+        the personals before deciding whether to become a full member of the
+        site and take advantage of the free facilities they offer.</p>
+        <p>We plan to expand this directory to include written reviews of each
+        service on offer.</p>
+       <p align="left">
+        </p>
+            </td>
+          </tr>
+        </table>
+<?include('Templates/maintemplate.footer.inc.php');?>
