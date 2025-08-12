955a4a5781ef7f45b1f30d85b6dc427ba0845b7c
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:37 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/ebusinessipwedokument.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/ebusinessipwedokument.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 20;
+$_objchilds[] = '_child_ipwedokument_21';
+function objfile_20 () {
+$obj = owNew('document');
+$objdata['name'] = "Ebusiness: IPW eDokument";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
