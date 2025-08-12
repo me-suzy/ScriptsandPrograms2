237b8ecdfaa4e9dea5486297a4920b67786033fd
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:01 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_tags.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_tags.php
@@ -0,0 +1,30 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintain tags';
+
+//  List Headings
+$GLOBALS["tCategory"] = 'Category';
+$GLOBALS["tTag"] = 'Tag';
+$GLOBALS["tTranslation"] = 'Translation';
+
+//  List Functions
+$GLOBALS["tAddNewTag"] = 'Add new tag';
+$GLOBALS["tViewTag"] = 'View tag';
+$GLOBALS["tEditTag"] = 'Edit tag';
+$GLOBALS["tDeleteTag"] = 'Delete tag';
+
+//  Form Block Titles
+$GLOBALS["thTagGeneral"] = 'Tag Details';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'This form lets you define custom tag codes for use within the body of your content.';
+$GLOBALS["hCategory"] = 'Select the category that most closely suits the group to which this tag belongs.';
+$GLOBALS["hTag"] = 'This is the tag code that you will use within your content.<br />Within the content, the tag should be within square braces <b>[tag]</b> to distinguish it; here, simply identify it by name, e.g. <b>tag</b>.<br />If you need both opening and closing tags, such as those used for bold text (<b>[b]</b> and <b>[/b]</b>), you will need to define both, e.g. <b>tag</b> and <b>/tag</b>.';
+$GLOBALS["hTranslation"] = 'This is the translation of your tag into straight HTML.';
+
+//  Error Messages
+$GLOBALS["eNoTag"] = 'Tag name can not be left empty.';
+$GLOBALS["eNoTranslation"] = 'Translation can not be left empty.';
+
+?>
