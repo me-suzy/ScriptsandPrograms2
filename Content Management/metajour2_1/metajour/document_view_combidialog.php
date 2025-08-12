e0026ce5f8fee23e6e5d3a6ed4d0c5a5da623067
me-suzy <me.suzana@gmail.com>
2025-08-12 05:57:31 +0300

    Add Content Management/metajour2_1/metajour/document_view_combidialog.php

---
+++ Content Management/metajour2_1/metajour/document_view_combidialog.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('basic_view_combi.php');
+
+class document_view_combidialog extends basic_view_combi {
+
+/*	function combiButtonBar() {
+		return '';
+	}*/
+
+}
