aba8ad184e4f472988a333703012f753e1b90046
me-suzy <me.suzana@gmail.com>
2025-08-12 11:42:35 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/gv_queue.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/gv_queue.php
@@ -0,0 +1,34 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Gift Voucher System v1.0
+  Copyright (c) 2001,2002 Ian C Wilson
+  http://www.phesis.org
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Gift Voucher Release Queue');
+
+define('TABLE_HEADING_CUSTOMERS', 'Customers');
+define('TABLE_HEADING_ORDERS_ID', 'Order-No.');
+define('TABLE_HEADING_VOUCHER_VALUE', 'Voucher Value');
+define('TABLE_HEADING_DATE_PURCHASED', 'Date Purchased');
+define('TABLE_HEADING_ACTION', 'Action');
+
+define('TEXT_REDEEM_COUPON_MESSAGE_HEADER', 'You recently purchased a Gift Voucher from our online store.' . "\n"
+                                          . 'For security reasons this was not made immediately available to you.' . "\n"
+                                          . 'However this amount has now been released. You can now visit our store' . "\n"
+                                          . 'and sent the value via email to someone else' . "\n\n");
+
+define('TEXT_REDEEM_COUPON_MESSAGE_AMOUNT', 'The Gift Voucher(s) you purchased are worth %s' . "\n\n");
+
+define('TEXT_REDEEM_COUPON_MESSAGE_BODY', '');
+define('TEXT_REDEEM_COUPON_MESSAGE_FOOTER', '');
+define('TEXT_REDEEM_COUPON_SUBJECT', 'Gift Voucher Purchase');
+?>
