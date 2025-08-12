b5f73d99eb7ba64b5462fd3f1fe4c8025739375c
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:33 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_requestapproval.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_requestapproval.da.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Anmod om godkendelse';
+$LANG['text_1'] = 'Er du sikker på at du vil anmode om godkendelse?';
+?>
