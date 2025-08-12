c381681cc9fc7c77a552a73de6066977a3b38e9a
me-suzy <me.suzana@gmail.com>
2025-08-12 06:12:45 +0300

    Add Content Management/metajour2_1/metajour/extension/bulletinboard/bulletinboard.class.php

---
+++ Content Management/metajour2_1/metajour/extension/bulletinboard/bulletinboard.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+require_once($system_path . "extension/basicextension.class.php");
+
+class ext_bulletinboard extends basicextension {
+
+	function ext_bulletinboard() {
+		$this->basicextension();
+		$this->extname = 'bulletinboard';
+		$this->addextparam('templatename');
+		$this->addextparam('templateid');
+	}
+
+	function _do() {
+		$this->useTemplate('templatename','templateid','standard_bulletinboard_list');
+	}
+
+}
+?>
