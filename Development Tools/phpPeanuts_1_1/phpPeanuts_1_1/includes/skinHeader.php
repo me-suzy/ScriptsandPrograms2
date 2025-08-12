1a28c8223dd317fb773d11f9a37c4e169bd9ee2e
me-suzy <me.suzana@gmail.com>
2025-08-12 10:07:57 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/includes/skinHeader.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/includes/skinHeader.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<title>
+<?php print $this->getLabel() ?>
+</title>
+<link rel="stylesheet" href="../style/phppeanuts.css">
+<META name="generator" content="powered by phpPeanuts">
+
+<script src="../scripts/general.js"></script>
+<script src="../scripts/popup.js"></script>
+<script src="../scripts/specific.js"></script>
+
+</head>
+<body <?php $this->printBodyTagIeExtraPiece() ?> BODYMARGIN="0" LEFTMARGIN="0" TOPMARGIN="0" MARGINHEIGHT="0" MARGINWIDTH="0"> 
+<table class=maintable height=100% width=100% cellpadding=0 cellspacing=0>
+<tr height=1 bgcolor=#FF9933>
+  <td valign=top height=1>   
+  </td>
+</tr>
+<tr height=10>
+  <td valign=top height=10>   
+  </td>
+</tr>
+<tr height=1 bgcolor=#FF9933>
+  <td valign=top height=1>   
+  </td>
+</tr>
+<tr height=1 bgcolor=purple>
+  <td valign=top height=1>   
+  </td>
+</tr>
+<tr height=40>
+  <td class=pntPageTitle>  
+  <div align=center id=pntPageLabel>
+  	<?php print $this->getLabel() ?>
+  </div> 
+  </td>
+</tr>
+<tr height=2 bgcolor=#FF9933>
+  <td valign=top height=1>   
+  </td>
+</tr>
+<tr height=1 bgcolor=purple>
+  <td valign=top height=1>   
+  </td>
+</tr>
+<tr height=100%>
+  <td valign=top class=pntGroupContent>
+  
