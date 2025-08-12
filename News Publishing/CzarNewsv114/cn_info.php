228e564295ea6b8a2ed595d3672d71d1e1f24b0d
me-suzy <me.suzana@gmail.com>
2025-08-12 19:43:55 +0300

    Add News Publishing/CzarNewsv114/cn_info.php

---
+++ News Publishing/CzarNewsv114/cn_info.php
@@ -0,0 +1,28 @@
+<?
+$pagetitle = "Information/About";
+include("cn_auth.php");
+include("cn_head.php");
+?>
+
+<ul type="disc">
+	<li><b>About the Author</b><br>
+		CzarNews was made by Vance Lucas, who goes by the internet alias of "Czaries".  I am currrently enrolled as a full-time college student at a university in Oklahoma in the United States.  In my free time, I design and create websites, graphics, logos, and do some scripting in PHP.  CzarNews and the rest of the scripts I offer on my website were made in my free time as a hobby of mine, and are more or less simply projects of mine so that I can see what I can accomplish when I really apply myself.<br><br>
+	<li><b>Why was CzarNews made?</b><br>
+		CzarNews was made primarily due to my own frustration of the existing news scripts available at the time.  I needed a script that would be fast and simple, yet at the same time be flexible enough for me to have complete control over.  None of the free news scripts available allowed you to include the news on your own page, in your own layout template, or were messy and very confusing.  Additionally, they also provided you with little control over the format in which the news was displayed.  Another problem I kept running into was that many of the "good" scripts out there had a complicated install process that would easily lose new users.  I finally just got fed up with everything, and set out to make my own script.  The goal at first was to make as simple of a script as possible, and make it very easy to set up without lots of confusing options and obscure unexplained settings.  It had to be very user-friendly, and put together in a way that would appear attractive and organized.  I knew I myself hated dealing with template systems, so I decided to go with a much simpler and much more versitile include system.  CzarNews later expanded into a more robust news script with all of the commonly used features, and none of the excess 'fluff'.<br><br>
+	<li><b>What can I do to help?</b><br>
+	Probably the most helpful thing you can do as a user of CzarNews is give me your honest feedback.  What did you like about the script?  What do you not like about it?  What features do you want in the next version, etc.  Another major help is submitting bugs or security holes.  Any comments, feedback, suggestions, bug reports, or anything else related to this script can be emailed to: <a href="mailto:czaries@czaries.net">czaries@czaries.net</a>.  All comments and questions are welcome.  You may also post a problem on the <a href="http://forum.czaries.net" target="_blank">Support Forums</a> under "Script Troubleshooting".<br><br>
+If you have no input on the script for future releases, but would still like to contribute something you may donate a small amount of money through PayPal.  Please note this script is and will always be free, so do not feel like you are "stealing" it by not paying for it.  The PayPal donation option is simply for those who wish to be helpful, or those who feel compelled to give something for my work.<br><br>
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_xclick">
+<input type="hidden" name="business" value="czaries@hotmail.com">
+<input type="hidden" name="item_name" value="Czaries.net Scripts">
+<input type="hidden" name="no_note" value="1">
+<input type="hidden" name="currency_code" value="USD">
+<input type="hidden" name="tax" value="0">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but04.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+</form>
+</ul>
+
+<?
+include("cn_foot.php");
+?>
