0f72e4294d31f608afdd78e13a7649d614537620
me-suzy <me.suzana@gmail.com>
2025-08-12 12:51:34 +0300

    Add Education/DigitalScribe1.4.1/DigitalScribe1.4.1/DigitalScribe/footer.php

---
+++ Education/DigitalScribe1.4.1/DigitalScribe1.4.1/DigitalScribe/footer.php
@@ -0,0 +1,13 @@
+</td></tr>
+
+<TR>
+<TD>
+<BR>
+<A HREF=index.php>Home</a> | 
+
+<A HREF=viewhw1.php>Homework</a> |  <A HREF=announcements.php>Announcements</a> | <A HREF=addstuwork.php>Add Student Work</a> | <A HREF=/DigitalScribe/login.php>Login</a> | <A HREF=register.php>Register</a>
+
+<BR><SPAN CLASS=legal>Copyright 2005 &copyl <A HREF=http://www.digital-scribe.org>Digital Scribe v.1.4.1</a> - All Rights Reserved</span>
+</td>
+</tr></table>
+</body>
