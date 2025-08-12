c9546ca5cddf3d147674be5f4eeca72c59a4148e
me-suzy <me.suzana@gmail.com>
2025-08-12 14:12:37 +0300

    Add File Manipulation/cep/cep/cjgmenuconfig.php

---
+++ File Manipulation/cep/cep/cjgmenuconfig.php
@@ -0,0 +1,61 @@
+<?php
+/*------------------------------------------------------------------------------
+CJG EXPLORER PRO v3.2 - WEB FILE MANAGEMENT - Copyright (C) 2003 CARLOS GUERLLOY
+CJGSOFT Software
+cjgexplorerpro@guerlloy.com
+guerlloy@hotmail.com
+carlos@weinstein.com.ar
+Buenos Aires, Argentina
+--------------------------------------------------------------------------------
+This program is free software; you can  redistribute it and/or  modify it  under
+the terms   of the   GNU General   Public License   as published   by the   Free
+Software Foundation; either  version 2   of the  License, or  (at  your  option)
+any  later version. This program  is  distributed in  the hope that  it  will be
+useful,  but  WITHOUT  ANY  WARRANTY;  without  even  the   implied  warranty of
+MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR  PURPOSE.  See the  GNU   General
+Public License for   more details. You  should have received  a copy of  the GNU
+General Public License along  with this  program; if   not, write  to the   Free
+Software  Foundation, Inc.,  59 Temple Place,  Suite 330, Boston,  MA 02111-1307
+USA
+------------------------------------------------------------------------------*/
+
+$def="cjgmenu.def";			// Menu definition file
+
+$showfoldericon=1;			// Show/Hide folder icons
+$showfileicon=1;			// Show/Hide file icons
+$shownodelines=1;			// Show/Hide lines and plus/minus icons
+$showroot=0;				// Show/Hide root icon
+
+$imgwidth=19;				// Width for all icons and lines
+$imgheight=16;				// Height for all icons and lines
+
+$folderimages="cm/img";			// Location for images
+$foldericons="cm/ico";			// Location for custom file icons
+
+// All these icons must be located under $folderimages folder
+$imgroot="folderopen.gif";		// Icon for root folder
+$imgfolderclose="folderclose.gif";	// Icon for closed folder
+$imgfolderopen="folderopen.gif";	// Icon for opened folder
+$imgfolderwait="folderwait.gif";	// Icon for waiting folder
+$imgplustop="plustop.gif";		// Icon for top plus mark
+$imgplustopbot="plustopbot.gif";	// Icon for unique plus mark
+$imgplusmiddle="plusmiddle.gif";	// Icon for middle plus mark
+$imgplusbottom="plusbottom.gif";	// Icon for bottom plus mark
+$imgminustop="minustop.gif";		// Icon for top minus mark
+$imgminustopbot="minustopbot.gif";	// Icon for unique minus mark
+$imgminusmiddle="minusmiddle.gif";	// Icon for middle minus mark
+$imgminusbottom="minusbottom.gif";	// Icon for bottom minus mark
+$imgjointop="jointop.gif";		// Icon for top join line
+$imgjointopbot="jointopbot.gif";	// Icon for unique join line
+$imgjoinmiddle="joinmiddle.gif";	// Icon for middle join line
+$imgjoinbottom="joinbottom.gif";	// Icon for bottom join line
+$imglinemiddle="linemiddle.gif";	// Icon for line
+$imglinebottom="linebottom.gif";	// Icon for bottom line (space)
+$imgfiledefault="folderclose.gif";	// Icon for files
+
+
+
+
+?>
+
+
