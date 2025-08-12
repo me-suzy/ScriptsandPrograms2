3414b412c576b10a40c645d067ca9e515c928bd4
me-suzy <me.suzana@gmail.com>
2025-08-12 18:34:19 +0300

    Add Image Galleries/TinyWebGallery13c/TinyWebGallery13/language_extra/language_tr.php

---
+++ Image Galleries/TinyWebGallery13c/TinyWebGallery13/language_extra/language_tr.php
@@ -0,0 +1,222 @@
+<?php
+/*************************
+  Copyright (c) 2004-2005 TinyWebGallery
+  written by Michael Dempfle
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  TWG version: 1.3c
+  $Date: 2005/11/15 09:02 $
+**********************************************/
+// language file - version 1.3 - if you translate a langauge file where already some parts are translated: Please don't remove the former translator completely - insert him/her behind the lang_translator as a comment
+$lang_translator = "&nbsp;";
+$lang_optionen = "Ayarlar";
+$lang_menu_titel = "Ba&thorn;l&yacute;k";
+$lang_kommentar = "Yorumlar";
+
+$lang_titel = "";
+$lang_titel_no = "Hen&uuml;z resim y&uuml;kl&uuml; de&eth;il.";
+$lang_select_gallery = "L&uuml;tfen Galeriyi Se&ccedil;iniz.";
+$lang_help = "Yard&yacute;m";
+$lang_new_window = "Yeni Pencere";
+$lang_login = "Giri&thorn;";
+$lang_logout = "&Ccedil;&yacute;k&yacute;&thorn;";
+$lang_visitor = "Ziyaret:";
+$lang_total = "Toplam:";
+$lang_today = "Bug&uuml;n:";
+$lang_galleries = "Galeriler";
+$lang_picture = "Resim";
+$lang_pictures = "Resimler";
+$lang_of = "";
+
+$lang_overview = "Genel G&ouml;r&uuml;n&uuml;m";
+$lang_forward = "Sonraki";
+$lang_back = "&Ouml;nceki";
+$lang_rotate_left = "Saat y&ouml;n&uuml;n&uuml;n tersine &ccedil;evir";
+$lang_rotate_right = "Saat y&ouml;n&uuml;nde &ccedil;evir";
+$lang_start_slideshow = "Sunumu ba&thorn;lat";
+$lang_stop_slideshow = "Sunumu bitir";
+$lang_thumb_forward = "&gt;"; // = '>'
+$lang_thumb_back="&lt;"; // = '<'
+$lang_views = "x";
+
+$lang_login_php_enter = "Galerideki geli&thorn;mi&thorn; &ouml;zellikleri kullanmak i&ccedil;in l&uuml;tfen giri&thorn; yap&yacute;n&yacute;z.";
+$lang_login_php_enter_again = "Yanl&yacute;&thorn; parola<br />l&uuml;tfen tekrar giriniz:";
+
+$lang_kommenar_php_both_fields = "L&uuml;tfen her iki bo&thorn;lu&eth;uda doldurunuz.<br />";
+$lang_kommenar_php_enter_name = "&Yacute;sim giriniz<br />(Azami. 30 karakter):";
+$lang_kommenar_php_enter_comment = "Yorum giriniz<br />(Azami. 150 karakter):";
+$lang_kommenar_php_name = "&Yacute;sim:";
+$lang_kommenar_php_comment = "Yorumlar (Azami. 150 karakter):";
+$lang_kommenar_php_speichern = "Kaydet";
+
+$lang_privatelogin_php_wrong_password = "Yanl&yacute;&thorn; parola<br />l&uuml;tfen tekrar giriniz:";
+$lang_privatelogin_php_password = "Galeri i&ccedil;in parolay&yacute; giriniz<br />'%s':";
+$lang_privatelogin_php_login = "Giri&thorn;";
+
+$lang_titel_php_titel = "L&uuml;tfen bir ba&thorn;l&yacute;k giriniz.<br />(Azami 150 karakter):";
+$lang_titel_php_save = "Kaydet";
+
+$lang_slideshowid_php_loading = "Sunum y&uuml;kleniyor ...";
+
+$lang_opionen_php_yes = "Evet";
+$lang_opionen_php_no = "Hay&yacute;r";
+$lang_opionen_php_new_window = "Galeriyi yeni pencerede a&ccedil;";
+$lang_opionen_php_opt_slideshow = "Sunumu de&eth;i&thorn;me i&ccedil;in ayarla";
+$lang_opionen_php_slideshowintervall = "Sunum aral&yacute;&eth;&yacute;(saniye)";
+$lang_opionen_php_ok = "Tamam";
+$lang_opionen_php_top_nav = "Sadece &uuml;stte g&ouml;ster";
+
+$lang_back_topx = "Ba&thorn;a&nbsp;geri&nbsp;d&ouml;n %s.";
+$lang_topx = "Ba&thorn;; %s";
+$lang_main_topx = "Galerilere&nbsp;geri&nbsp;d&ouml;n";
+// new 1.1
+$lang_loading = "y&uuml;kleniyor...";
+$lang_dhtml_navigation = "DHTML dola&thorn;&yacute;m&yacute; kullan";
+$lang_optionen_slideshow = "Sunum tipi";
+$lang_optionen_slideshow_fullscreen = "Tam Ekran";
+$lang_optionen_slideshow_optimized = "Crossfade";
+$lang_optionen_slideshow_normal = "Normal";
+$lang_lowbandwidth = "TinyWebGallery yava&thorn; ba&eth;lant&yacute;lar i&ccedil;in ayarlanm&yacute;&thorn;t&yacute;r.";
+$lang_highbandwidth = "TinyWebGallery h&yacute;zl&yacute; ba&eth;lant&yacute;lar i&ccedil;in ayarlanm&yacute;&thorn;t&yacute;r.";
+// new in 1.2
+$lang_email_menu_user = "E-posta duyurusu";
+$lang_email_menu_admin = "e-posta yolla"; 
+// Sorrymessage for failed subscription
+$lang_email_sorrysignmessage = "Yazd&yacute;&eth;&yacute;n&yacute;z '%s' e-posta adresi sistemimize kay&yacute;tl&yacute;d&yacute;r."; 
+// Sorrymessage for blank email
+$lang_email_sorryblankmailmessage = "L&uuml;tfen e-posta adresinizi giriniz."; 
+// Sorrymessage for invalid emails
+$lang_email_sorryoddmailmessage = "Yazd&yacute;&eth;&yacute;n&yacute;z '%s' e-posta adresi ilk yaz&yacute;d&yacute;&eth;&yacute;n&yacute;zla ayn&yacute; de&eth;ildir."; 
+// Sorrymessage if someone entered your own mail
+$lang_email_sorryownmailmessage = "&Ouml;z&uuml;r dilerim, ama ben kendi haber grubumun postalar&yacute;n&yacute; almak istemiyorum!"; 
+// Title of the newsletter, will be displayed in the FROM field of the mailclient
+$lang_email_subscribemail_subject = "Yeni resmler mevcuttur."; 
+// Subscribemessage, will be shown when someone subscribes.
+$lang_email_subscribemessage = "Kay&yacute;t i&ccedil;in te&thorn;ekk&uuml;rler. E-posta adresinize onay postas&yacute; yollanacakt&yacute;r."; 
+// Subscribemail, will be sent when someone subscribes.
+$lang_email_subscribemail = "Haber grubuna e-posta adresiniz ba&thorn;ar&yacute;yla eklenmi&thorn;tir."; 
+// Unsubscribemessage for deletion, will be followed by the email!
+$lang_email_unsubscribemessage = "E-posta '%s' adresiniz sitemize art&yacute;k kay&yacute;tl&yacute; de&eth;ildir."; 
+// Unsubscribemessage for failed deletion, will be followed by the email!
+$lang_email_failedunsubscriptionmessage = "E-posta '%s' bulunamad&yacute;."; 
+// the welcometext
+$lang_email_welcomemessage = "E-posta bildirgesi i&ccedil;in kay&yacute;t olabilir ve You can register and cancel the registation for the email notification.<br/>Galeriye yeni resimler eklendi&eth;inde galeri y&ouml;neticisi taraf&yacute;ndan bilgilendirileceksiniz."; 
+// email could not be sent.
+$lang_email_error_send_mail = "Onay postas&yacute; yollanamad&yacute;.<br/>E-posta sunucusu d&uuml;zg&uuml;n &thorn;ekilde ayarlanmam&yacute;&thorn;. L&uuml;tfen galeri y&ouml;neticisi ile irtibata ge&ccedil;iniz.";
+
+$lang_email_add = "Ekle";
+$lang_email_remove = "Kald&yacute;r";
+$lang_email_send = "Yolla";
+
+$lang_email_admin_welcomemessage_send = 'Galerideki t&uuml;m de&eth;i&thorn;ikler hakk&yacute;nda kay&yacute;tl&yacute; t&uuml;m kullan&yacute;c&yacute;lar&yacute; (%s) bildirebilirsiniz.';
+$lang_email_admin_sorryoddsendermailmessage = "Girdi&eth;iniz '%s' mail adresi ge&ccedil;erli de&eth;ildir.";
+$lang_email_admin_sendermail = "G&ouml;nderenin e-posta adresi";
+$lang_email_admin_subject = "Konu";
+$lang_email_admin_message = "Mesaj";
+$lang_email_admin_sendbutton = "Yolla";
+$lang_email_admin_sent = "Mesaj&yacute;n&yacute;z yollanm&yacute;&thorn;t&yacute;r:";
+$lang_email_admin_from = "Kimden";
+$lang_email_admin_notloggedin = "Sisteme girmediniz. Anasayfaya giderek sisteme giri&thorn; yap&yacute;n&yacute;z.";
+
+$lang_visitor_30 = "30 g&uuml;nl&uuml;k ziyaret";
+
+$lang_login_php_enter = "Galerideki geli&thorn;mi&thorn; &ouml;zellikleri kullanmak i&ccedil;in l&uuml;tfen giri&thorn; yap&yacute;n&yacute;z.";
+$lang_opionen_php_zoom = "Maximized view";
+$lang_administration = "Administration";
+$lang_opionen_php_zoom_message = "Attention: The \'Maximized view\' is slower than the normal view, because the images are generated on the fly. The images are larger too and therefore they take longer to transfer.\\nPlease use this option only if you have a fast internet connection!";
+$lang_first = "First";
+$lang_last = "Last";
+$lang_no_session = "Session is not available. Login to private galleries is not enabled.";
+$lang_not_loggedin = "You are not logged in.<br/>Please go to the main page and unlock this gallery.";
+$lang_no_topx_images = "No images could be found for this view. <br / >Protected galleries are only included if you unlock them first."; 
+
+$lang_tips_overview = array('Tip: Please click on the help link, if you want to know what you can do with the gallery.');
+$lang_tips_thumb = array('Tip: On the small navigation in the left upper corner you can directly jump to all the parent levels.','Tip: The Top 13 on this page does only show the most viewed images of THIS directory.');
+$lang_tips_image = array('Tip: You can use the left/right arrow key to navigate to the next/last image.','Tip: If you login (right upper corner) you can enter captions, delete comments and rotate images permanently.','Tip: You can use smilies like :) or ;) in titles and comments.');
+
+// new 1.3
+$lang_dl_as_zip1 = "You can download all images of this directory in a single archive.<br />Do you want to download the whole directory or continue to the actual image?";
+$lang_dl_as_zip2 = "Actual image";
+$lang_dl_as_zip3 = "All";
+$lang_dl_as_zip4 = "Do not ask again in this session."; // we store this is the session
+
+$lang_download_counter = "Downloads";
+$lang_download_counter_short = "dl";
+// not used in 1.3 yet!
+$lang_email_subscribemail_2_subject = "Please confirm your registration at the TinyWebGallery";
+$lang_email_subscribemail_2 = "Dear TinyWebGallery user:\n\nYou have registered to the notification list of TinyWebGallery. To complete the registration please klick on the link below:\n%s\n\nYour TinyWebGallery";
+// --
+$lang_rating = "Vote";
+$lang_rating1 = "1";
+$lang_rating2 = "2";
+$lang_rating3 = "3";
+$lang_rating4 = "4";
+$lang_rating5 = "5";
+$lang_rating_button = "Rate";
+$lang_rating_text = "Please rate the actual image:";
+$lang_rating_new="New vote: ";
+$lang_rating_vote="Votes";
+
+$lang_rating_security="Please enter the 4 characters<br />of the security code [A-F,0-9]:";
+$lang_rating_help="Please click on the image,<br />if you cannot see the code.";
+$lang_rating_message1="You have already voted.";
+$lang_rating_message2="please close this window.";
+$lang_rating_message3="Thanks for voting.";
+$lang_rating_message4="You have entered a wrong code.";
+$lang_rating_send="Send";
+
+$lang_fileinfo="Info";
+$lang_fileinfo_name = "File name";
+$lang_fileinfo_date = "Date";
+$lang_fileinfo_size = "Size";
+$lang_fileinfo_resolution = "Resolution";
+$lang_fileinfo_views = "Views";
+$lang_fileinfo_dl = "Downloads";
+$lang_fileinfo_rating = "Rating";
+$lang_fileinfo_not_available = " - (remote)";
+$lang_exif_not_available = " - ";
+// please check the constants in the file inc/exifReader.inc.php for exif entries you can try - not all are available on all cameras! sometimes the constants don't work - try to google for the real exif field name!
+$lang_exif_info = array("Camera Model" => "model", "Exif Date" => "DateTime", "Focal Length" => 
+"focalLength", "f/Stop" => "fnumber", "Shutter Speed" => "exposureTime", "ISO" => "isoEquiv");
+
+$lang_comments = "Comments";
+$lang_last_comments="Newest comments";
+$lang_show_kommentar="Show comments";
+$lang_add_kommentar="Add comment";
+$lang_close_fullscreen="Back to the normal view";
+
+$lang_search_results="Search resuls";
+$lang_search_back="Back to the search results.";
+$lang_search="Search";
+$lang_search_text="Please enter the search text:";
+$lang_search_button="Search";
+$lang_search_where="Search in";
+$lang_search_max="Hits shown per page";
+$lang_search_all="All";
+$lang_search_hits="Found %s hits.";
+
+// height of the i_frames - please check if the settings match for your language!!
+$lang_height_comment = 150;
+$lang_height_caption = 90;
+$lang_height_login = 80;
+$lang_height_private = 80;
+$lang_height_options = 205;
+if (!$show_new_window) { $lang_height_options -= 30; }
+if (!$show_slideshow)  { $lang_height_options -= 60; }
+if (!$enable_maximized_view)  { $lang_height_options -= 30; }
+$lang_height_email_user = 180;
+$lang_height_email_admin = 280;
+$lang_height_info = 150;
+if (!$show_download_counter || !$enable_download_counter) { $lang_height_info -= 17; }
+if (!$show_count_views) { $lang_height_info -= 17; }
+if (!$show_image_rating) { $lang_height_info -= 17; }
+if ($show_exif_info) { $lang_height_info += (count($lang_exif_info) * 17);}
+$lang_height_rating = 150;
+$lang_height_dl_manager=150;
+$lang_height_search=175;
+$lang_xpos_lang_dropdown="200"; // this is only needed if you include twg and ie does not position the language dropdown correct because you include twg in several divs - in a fixed layout you can adjust this here - if you don't have a fix layout don't use the dropdown - use the normal setting! 
+?>
