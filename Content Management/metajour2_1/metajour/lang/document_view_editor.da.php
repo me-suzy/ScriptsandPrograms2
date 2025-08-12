ad0ddbcdd107bc7be14360dfb1ec5b327b5df274
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:33 +0300

    Add Content Management/metajour2_1/metajour/lang/document_view_editor.da.php

---
+++ Content Management/metajour2_1/metajour/lang/document_view_editor.da.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['menu_file'] = 'Filer';
+$LANG['menu_filesave'] = 'Gem';
+$LANG['menu_documentsection_properties'] = 'Sektionsegenskaber...';
+$LANG['menu_document_properties'] = 'Dokumentegenskaber...';
+
+$LANG['menu_edit'] = 'Rediger';
+$LANG['menu_undo'] = 'Fortryd';
+$LANG['menu_redo'] = 'Genskab';
+$LANG['menu_cut'] = 'Klip';
+$LANG['menu_copy'] = 'Kopier';
+$LANG['menu_paste'] = 'Indsæt';
+$LANG['menu_delete'] = 'Slet';
+$LANG['menu_selectall'] = 'Marker alt';
+$LANG['menu_findtext'] = 'Søg...';
+
+$LANG['menu_view'] = 'Vis';
+$LANG['menu_toolbar'] = 'Værktøjslinier';
+$LANG['menu_tbstd'] = 'Standard';
+$LANG['menu_tbfmt'] = 'Formatering';
+$LANG['menu_tbabs'] = 'Absolut placering';
+$LANG['menu_tbtable'] = 'Tabel';
+$LANG['menu_tbadvanced'] = 'Avanceret';
+$LANG['menu_tbsection'] = 'Sektioner';
+
+$LANG['menu_format'] = 'Formater';
+$LANG['menu_font'] = 'Skrifttype';
+$LANG['menu_bold'] = 'Fed';
+$LANG['menu_italic'] = 'Kursiv';
+$LANG['menu_underline'] = 'Understreget';
+$LANG['menu_forecolor'] = 'Skriftfarve';
+$LANG['menu_backcolor'] = 'Baggrundsfarve';
+$LANG['menu_justifyleft'] = 'Venstrestil';
+$LANG['menu_justifycenter'] = 'Centrer';
+$LANG['menu_justifyright'] = 'Højrestil';
+
+$LANG['menu_html'] = 'HTML';
+$LANG['menu_hyperlink'] = 'Hyperlink...';
+$LANG['menu_anchor'] = 'Bogmærke...';
+$LANG['menu_image'] = 'Billede...';
+$LANG['menu_visibleborders'] = 'Synlige rammer';
+$LANG['menu_showdetails'] = 'Vis detaljer';
+$LANG['menu_makeabs'] = 'Placer frit';
+$LANG['menu_lock'] = 'Lås';
+$LANG['menu_zorder'] = 'Rækkefølge';
+
+$LANG['menu_bringfront'] = 'Placer forrest';
+$LANG['menu_sendback'] = 'Placer bagerst';
+$LANG['menu_bringforward'] = 'Flyt en frem';
+$LANG['menu_sendbackward'] = 'Flyt en tilbage';
+$LANG['menu_belowtext'] = 'Bag tekst';
+$LANG['menu_abovetext'] = 'Foran tekst';
+$LANG['menu_snaptogrid'] = 'Lås til gitter';
+$LANG['menu_customhtml'] = 'Brugerdefineret HTML';
+
+$LANG['menu_orderlist'] = 'Nummereret liste';
+$LANG['menu_unorderlist'] = 'Punktliste';
+$LANG['menu_indent'] = 'Forøg indrykning';
+$LANG['menu_outdent'] = 'Formindsk indrykning';
+
+$LANG['menu_inserttable'] = 'Indsæt tabel';
+$LANG['menu_insertrow'] = 'Indsæt række';
+$LANG['menu_deleterows'] = 'Slet rækker';
+$LANG['menu_insertcol'] = 'Indsæt kolonne';
+$LANG['menu_deletecols'] = 'Slet kolonner';
+$LANG['menu_insertcell'] = 'Indsæt celle';
+$LANG['menu_deletecells'] = 'Slet celler';
+$LANG['menu_mergecells'] = 'Flet celler';
+$LANG['menu_splitcell'] = 'Opdel celle';
+
+$LANG['menu_showhtml'] = 'Vis HTML';
+
+$LANG['menu_newvariant'] = 'Ny variant';
+$LANG['menu_newsection'] = 'Ny sektion';
+$LANG['menu_newsectionbelow'] = 'Ny sektion herunder';
+$LANG['menu_moveupsection'] = 'Flyt sektion op';
+$LANG['menu_movedownsection'] = 'Flyt sektion ned';
+$LANG['menu_deletesection'] = 'Slet sektion';
+$LANG['menu_requestapproval'] = 'Anmod om godkendelse';
+$LANG['menu_approvepublish'] = 'Godkend og udgiv';
+$LANG['menu_preview'] = 'Preview';
+
+$LANG['color_title'] = 'Vælg farve';
+$LANG['color_lable'] = 'Farve: ';
+
+$LANG['table_inserttitle'] = 'Indsæt tabel';
+$LANG['table_edittitle'] = 'Rediger tabel';
+$LANG['table_dimension'] = 'Dimensioner';
+$LANG['table_rows'] = 'Rækker:';
+$LANG['table_width'] = 'Bredde:';
+$LANG['table_columns'] = 'Kolonner:';
+$LANG['table_borderwidth'] = 'Kanttykkelse:';
+$LANG['table_colors'] = 'Farver';
+$LANG['table_bordercolor'] = 'Kantfarve:';
+$LANG['table_bgcolor'] = 'Baggrundsfarve:';
+$LANG['table_spacing'] = 'Afstand';
+$LANG['table_cellpadding'] = 'Indre celleafstand:';
+$LANG['table_cellspacing'] = 'Ydre celleafstand:';
+
+$LANG['table_select'] = 'Vælg';
+
+?>
