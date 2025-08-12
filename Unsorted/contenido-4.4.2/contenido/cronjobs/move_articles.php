0c47d94f7554b69866d2444b0512210c6118f861
me-suzy <me.suzana@gmail.com>
2025-08-13 01:55:32 +0300

    Add Unsorted/contenido-4.4.2/contenido/cronjobs/move_articles.php

---
+++ Unsorted/contenido-4.4.2/contenido/cronjobs/move_articles.php
@@ -0,0 +1,43 @@
+<?php
+
+/*****************************************
+* File      :   $RCSfile: move_articles.php,v $
+* Project   :   Contenido
+* Descr     :   Cron Job to implement the move article feature
+*
+* Author    :   $Author: timo.hummel $
+*               
+* Created   :   26.05.2003
+* Modified  :   $Date: 2003/08/30 16:15:38 $
+*
+* © four for business AG, www.4fb.de
+*
+* $Id: move_articles.php,v 1.5 2003/08/30 16:15:38 timo.hummel Exp $
+******************************************/
+
+include_once ('../includes/config.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.user.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.plugin.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.xml.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.navigation.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.template.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.backend.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.table.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.notification.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.area.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.module.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.layout.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.client.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.cat.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["classes"] . 'class.treeitem.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["includes"] . 'cfg_sql.inc.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["includes"] . 'cfg_language_de.inc.php');
+include_once ($cfg['path']['contenido'].$cfg["path"]["includes"] . 'functions.general.php');
+include_once ('../includes/functions.con.php');
+
+$db = new DB_Contenido;
+conFlagOnOffline();
+conMoveArticles();
+
+
+?>
