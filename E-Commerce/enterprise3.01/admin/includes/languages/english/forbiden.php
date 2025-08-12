b235e7dbfc26f973444d479415933239a7470493
me-suzy <me.suzana@gmail.com>
2025-08-12 11:42:30 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/forbiden.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/forbiden.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id $
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Access Denied');
+define('NAVBAR_TITLE', 'No Right Permission Access');
+define('TEXT_MAIN', '&nbsp;Please contact your <b>Web Administrator</b> to request <br>&nbsp;more access or if you found any problem.<br>&nbsp;');
+define('TEXT_BACK', 'back');
+?>
