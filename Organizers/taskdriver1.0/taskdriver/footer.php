c672815bfaefdeea43274133ef9ff064ee4ff61f
me-suzy <me.suzana@gmail.com>
2025-08-12 21:41:16 +0300

    Add Organizers/taskdriver1.0/taskdriver/footer.php

---
+++ Organizers/taskdriver1.0/taskdriver/footer.php
@@ -0,0 +1,7 @@
+<?php
+include 'config.php';
+echo "<table cellspacing=\"0\" class=\"black\" width=\"100%\">";
+echo "<tr><td align=\"center\" width=\"100%\"></td></tr>";
+echo "<tr><td align=\"center\" width=\"100%\">TaskDriver $version</td></tr>";
+echo "</table>";
+?>
