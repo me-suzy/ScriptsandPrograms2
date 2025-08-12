750dff7d7e228b87b39caa461f3ad1b0dbb1164a
me-suzy <me.suzana@gmail.com>
2025-08-12 11:43:55 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/espanol/cache.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/espanol/cache.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Control de Cach&eacute;');
+
+define('TABLE_HEADING_CACHE', 'Paneles');
+define('TABLE_HEADING_DATE_CREATED', 'Fecha de Creaci&oacute;n');
+define('TABLE_HEADING_ACTION', 'Acci&oacute;n');
+
+define('TEXT_FILE_DOES_NOT_EXIST', 'No existe fichero');
+define('TEXT_CACHE_DIRECTORY', 'Directorio para la Cach&eacute;: ');
+
+define('ERROR_CACHE_DIRECTORY_DOES_NOT_EXIST', 'Error: No existe el directorio para la cach&eacute;. Especifique un directorio valido en Configuraci&oacute;n->Cach&eacute;.');
+define('ERROR_CACHE_DIRECTORY_NOT_WRITEABLE', 'Error: No hay permiso de escritura en el directorio para la caché.');
+?>
