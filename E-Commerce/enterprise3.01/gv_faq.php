13d6cb40783b94ab780e2d3bfd0d805fb15f1614
me-suzy <me.suzana@gmail.com>
2025-08-12 11:26:43 +0300

    Add E-Commerce/enterprise3.01/gv_faq.php

---
+++ E-Commerce/enterprise3.01/gv_faq.php
@@ -0,0 +1,27 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Gift Voucher System v1.0
+  Copyright (c) 2001, 2002 Ian C Wilson
+  http://www.phesis.org
+
+  Released under the GNU General Public License
+*/
+
+  require('includes/application_top.php');
+
+  require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_GV_FAQ);
+
+  $breadcrumb->add(NAVBAR_TITLE);
+
+  $content = CONTENT_GV_FAQ;
+
+  require(DIR_WS_TEMPLATES . TEMPLATENAME_MAIN_PAGE);
+
+  require(DIR_WS_INCLUDES . 'application_bottom.php');
+  ?>
