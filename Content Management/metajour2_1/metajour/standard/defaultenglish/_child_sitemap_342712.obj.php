25775029e9e1762f65349d1336c5400298c7cb52
me-suzy <me.suzana@gmail.com>
2025-08-12 07:15:14 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_sitemap_342712.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_sitemap_342712.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342712;
+function objfile_342712 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Sitemap";
+$objdata['subname'] = "";
+$objdata['extension'] = "sitemap";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
