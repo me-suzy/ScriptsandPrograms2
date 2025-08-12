72dfa7213e95678326a501cb83cfa8004abb340e
me-suzy <me.suzana@gmail.com>
2025-08-12 14:00:04 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/template/client_banners.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/template/client_banners.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+  <head>
+  <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE">
+<title>{title}</title>
+<link rel="stylesheet" href="{baseurl}/template/css/{css}" type="text/css">
+</head>
+<body leftmargin="0" topmargin="0" marginwidth="0" 
+  marginheight="0" >
+<div id="content">
+<div class="main">
+<table border="0" cellpadding="1" width="650" cellspacing="0">
+<tr>
+<td>
+<table cellpadding="5" border="1" width="100%" cellspacing="0">
+<tr>
+<td colspan="2" class="tablehead"><center><div class="head">{title}</center></div></td>
+</tr>
+<td class="tablebody" colspan="2">
+<div class="mainbody">
+<table border="0" cellpadding="1" cellspacing="1" style="border-collapse: collapse"  width="90%">
+  <tr>
+<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" width="90%" >
+<tr>
+<table cellpadding="4" cellspacing="4" width="100%"><tr><td width="100%">
+{instructions}<p><hr>
+<center>{code}</center><p>
+<center>{foundnum}<br>
+    <tr>
+      <td width="100%" colspan="2"><center><a class="littleheadplain"><b>{addbanner}</b></a></td>
+    </tr>
+    <tr>
+      <td width="84%"><CENTER>{msg}
+		{banner_form}<p>
+</div>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<div class="footer">
+{footer}
+</div>
+</div>
+{menu}
