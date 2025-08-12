3b37bb29d79ae8ad481576935a8eab5506d672f1
me-suzy <me.suzana@gmail.com>
2025-08-12 10:03:07 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/classObjectEditDetailsPage.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/classObjectEditDetailsPage.php
@@ -0,0 +1,23 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+includeClass('PntObjectEditDetailsPage', 'pnt/web/pages');
+
+/** Page showing property labels and editing property values of a single object
+* By default shows properties specified by  getUiFieldPaths method 
+* on the class of the shown object. Layout can be specialized, 
+* @see http://www.phppeanuts.org/site/index_php/Pagina/150
+*
+* This concrete subclass is here to keep de application developers
+* code (including localization overrides) separated from the framework code.
+* @see http://www.phppeanuts.org/site/index_php/Menu/178
+* Framework code is in superclass. 
+* This class may be copied to an application folder to
+* make application specific overrides.
+* @see http://www.phppeanuts.org/site/index_php/Pagina/64
+*/
+class ObjectEditDetailsPage extends PntObjectEditDetailsPage {
+	
+}
+?>
