14c3b0535e60ce172caca382ebcc797834b0a0c7
me-suzy <me.suzana@gmail.com>
2025-08-12 14:10:42 +0300

    Add FAQ and Knowledgebase/faqengine/language/emails_en.php

---
+++ FAQ and Knowledgebase/faqengine/language/emails_en.php
@@ -0,0 +1,10 @@
+<?php
+/***************************************************************************
+ * (c)2001-2005 Boesch IT-Consulting (info@boesch-it.de)
+ ***************************************************************************/
+$l_email_new_com_subject = "FAQEngine - new comment";
+$l_email_new_com_mail = "{date}:\r\nNew user comment on FAQ #{faqnr}\r\n\r\n";
+$l_email_new_userquestion_subject = "FAQEngine - new user question";
+$l_email_new_userquestion_ascmail= "New user question #{qnr}\r\nClick on this link to see:\r\n{qlink}\r\n\r\n";
+$l_email_new_userquestion_htmlmail= "New user question #{qnr}<br>Click on this link to see:<br><a href=\"{qlink}\">{qlink}</a>";
+?>
