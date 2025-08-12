9b278b696d9baa521e7c0d2192d015e1914450ee
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:52 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_sidebars.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_sidebars.php
@@ -0,0 +1,37 @@
+<?
+
+// Form Title
+$GLOBALS["tFormTitle"]		= 'Gestion des Gabarits des Sidebars';
+
+// List Headings
+$GLOBALS["tName"]		= 'Nom';
+$GLOBALS["tBorder"]		= 'Bordure';
+$GLOBALS["tAlignment"]		= 'Alignement';
+$GLOBALS["tColour"]		= 'Couleur';
+$GLOBALS["tWidth"]		= 'Largeur';
+
+// List Functions
+$GLOBALS["tAddNewSidebar"]	= 'Ajouter nouvelle barre';
+$GLOBALS["tEditSidebar"]	= 'Editer barre';
+$GLOBALS["tDeleteSidebar"]	= 'Effacer barre';
+
+// Form Field Headings
+$GLOBALS["tSBName"]		= 'Nom de la Barre';
+$GLOBALS["tSBAlign"]		= 'Alignement de la Barre';
+$GLOBALS["tSBBorder"]		= 'Taille de la Bordure';
+$GLOBALS["tSBWidth"]		= 'Largeur de la Barre';
+$GLOBALS["tSBBackground"]	= 'Couleur de Fond de la Barre';
+
+// Form Detail Comments and Help Texts
+$GLOBALS["tDetails"]		 = 'Ce formulaire vous permet de construire des templates pour vos Sidebars dans votre contenu.';
+$GLOBALS["hSBName"]		 = 'Le nom sous lequel la barre sera référencée dans le tag [sidebar].';
+$GLOBALS["hSBAlign"]		 = 'Côté de la page sur lequel la barre apparaitra, '.$GLOBALS["tLeft"].' ou '.$GLOBALS["tRight"].'.';
+$GLOBALS["hSBBorder"]		 = 'Taille de la bordure de la barre.';
+$GLOBALS["hSBWidth"]		 = 'Le pourcentage maximum en largeur de la barre dans la frame du contenu.';
+$GLOBALS["hSBBackground"]	 = 'Couleur de Fond de la Barre.';
+
+// Error Messages
+$GLOBALS["eNoName"]		= 'Le template de la Barre doit avoir un Nom.';
+$GLOBALS["eColourWrong"]	= 'La valeur de la couleur de fond est erronnée.';
+
+?>
