3e2d00ad9929c7cffac198afca130fc971ada335
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:45 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/ipwmetazo.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/ipwmetazo.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 35;
+$_objchilds[] = '_child_ipwmetazo21_36';
+function objfile_35 () {
+$obj = owNew('document');
+$objdata['name'] = "IPW METAZO";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
