d747ebad0f2fa1dcd54c57a836842d94d4ff7b16
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:27 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_languages.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_languages.php
@@ -0,0 +1,54 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Sprachen verwalten';
+$GLOBALS["tFormTitle2"] = 'Neue Sprache hinzuf&uuml;gen';
+
+//  List Headings
+$GLOBALS["tLanguageCode"] = 'Sprachcode';
+$GLOBALS["tLanguage"] = 'Sprache';
+$GLOBALS["tCharSet"] = 'Zeichensatz';
+$GLOBALS["tDirection"] = 'Leserichtung';
+$GLOBALS["tEnabled"] = 'eingeschaltet';
+$GLOBALS["tFileName"] = 'Sprachdatei-Name';
+$GLOBALS["tFileNameM"] = 'Modul-Dateiname';
+
+//  List Functions
+$GLOBALS["tAddNew"] = 'Sprache einschalten';
+$GLOBALS["tViewLanguage"] = 'Spracheinstellungen ansehen';
+$GLOBALS["tEditLanguage"] = 'Spracheinstellungen &auml;ndern';
+$GLOBALS["tToggleLanguage"] = 'Sprache aktivieren/deaktivieren';
+$GLOBALS["tViewFiles"] = 'Sprachdateien ansehen';
+$GLOBALS["defaultLangError"] = 'Sprache kann nicht deaktiviert werden da sie DEFAULT ist';
+$GLOBALS["tEditFiles"] = 'Sprachdateien &auml;ndern';
+$GLOBALS["tViewFile"] = 'Sprachdatei ansehen';
+$GLOBALS["tEditFile"] = 'Sprachdatei &auml;ndern';
+$GLOBALS["tRet_Languages"] = 'zur&uuml;ck zur Sprachliste';
+
+//  Form Block Titles
+$GLOBALS["thLanguageGeneral"] = 'Sprachdetails';
+$GLOBALS["thMainFiles"] = 'Website Hauptdateien';
+$GLOBALS["thModuleFiles"] = 'Modul-Dateien';
+$GLOBALS["thJumpRef"] = '-- Block ausw&auml;hlen --';
+$GLOBALS["thJumptoBottom"] = 'nach unten springen';
+$GLOBALS["thJumptoTop"] = 'nach oben springen';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'In diesem Formular k&ouml;nnen Sie Sprachnamen und Zeichens&auml;tze festlegen.';
+$GLOBALS["hLanguage"] = 'Dies ist der Name der Sprache.';
+$GLOBALS["hCharSet"] = 'Dies ist der Zeichensatz f&uuml;r diese Sprache.';
+$GLOBALS["hFileName"] = 'W&auml;hlen Sie eine Sprachdatei zum laden.';
+$GLOBALS["hDirection"] = 'Wird diese Sprache von links-nach-rechts geschrieben, wie Englisch oder Deutsch, oder von rechts-nach-links wie Arabisch oder Hebr&auml;isch?';
+
+$GLOBALS["tRtL"] = 'rechts nach links';
+$GLOBALS["tLtR"] = 'links nach rechts';
+$GLOBALS["tRtLs"] = 'rnl';
+$GLOBALS["tLtRs"] = 'lnr';
+
+//  Error Messages
+$GLOBALS["eNoLanguage"] = 'Der Sprachname muss eingegeben werden.';
+$GLOBALS["eNoFile"] = 'Sie m&uuml;ssen eine Sprachdatei zuweisen.';
+$GLOBALS["eLanguageExists"] = 'Diese Sprache ist bereits vorhanden.';
+$GLOBALS["eNoEnableReady"] = 'Es liegen keine fertigen Sprachdateien vor, die aktiviert werden k&ouml;nnten.';
+
+?>
