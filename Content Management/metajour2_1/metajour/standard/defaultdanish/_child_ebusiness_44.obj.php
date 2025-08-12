2fb97cd2aefee8a4fce93a8446f035e6c93202d2
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:09 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ebusiness_44.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ebusiness_44.obj.php
@@ -0,0 +1,25 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 44;
+$_objchilds[] = '_child_ipwereklamation_45';
+$_objchilds[] = '_child_ipwedokument_47';
+function objfile_44 () {
+$obj = owNew('structureelement');
+$objdata['name'] = "Ebusiness";
+$objdata['pageid'] = "0";
+$objdata['url'] = "";
+$objdata['binfile1'] = "0";
+$objdata['binfile2'] = "0";
+$objdata['binfile3'] = "0";
+$objdata['showtype'] = "0";
+$objdata['target'] = "";
+$objdata['image1'] = "";
+$objdata['image2'] = "";
+$objdata['image3'] = "";
+$objdata['templateid'] = "0";
+$objdata['structureid'] = "0";
+$obj->createObject($objdata);
+$obj->moveTo(29);
+return $obj;
+}
+?>
