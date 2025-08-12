1602ec22debb005092e46f9b27cf9e6a663433c9
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:02 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_tagcategories.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_tagcategories.php
@@ -0,0 +1,19 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'traduire les catégories tag';
+
+//  List Headings
+$GLOBALS["tCatDesc"] = 'Nom de catégorie';
+
+//  List Functions
+$GLOBALS["tViewCategory"] = 'Afficher la catégorie tag';
+$GLOBALS["tEditCategory"] = 'Traduire la catégorie tag';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Cette forme vous permettez de traduit les noms de catégorie d\'étiquette.';
+
+//  Error Messages
+$GLOBALS["eNoTagcategoryDesc"] = 'Vous devez entrer une traduction pour chaque langue';
+
+?>
