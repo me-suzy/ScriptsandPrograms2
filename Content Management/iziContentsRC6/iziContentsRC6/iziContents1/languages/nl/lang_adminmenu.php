ee1bab0186812691355964c897cec111f0aad72d
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:18 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_adminmenu.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_adminmenu.php
@@ -0,0 +1,59 @@
+<?php
+
+//  Groep Titels en beschrijvingen
+$GLOBALS["tgaccess"] 		= 'Beveiliging';
+$GLOBALS["tgdaccess"] 		= 'Beheer van de gebruikers die toegang hebben tot het admin gedeelte, en andere functies.';
+$GLOBALS["tgmenus"] 		= 'Menu\'s';
+$GLOBALS["tgdmenus"]		= 'Beheer van de menu\'s opbouw van de website.';
+$GLOBALS["tgtemplates"] 	= 'Templates';
+$GLOBALS["tgdtemplates"] 	= 'Beheer van de templates en de standaard formaten voor uw inhoud.';
+$GLOBALS["tgcontents"] 		= 'Inhoud';
+$GLOBALS["tgdcontents"] 	= 'Beheer van de artikelen, pagina\'s en afbeeldingen.';
+$GLOBALS["tglanguages"] 	= 'Vertalingen';
+$GLOBALS["tgdlanguages"] 	= 'Vertaal uw website voor andere talen.';
+$GLOBALS["tgstyles"] 		= 'Layout';
+$GLOBALS["tgdstyles"] 		= 'Pas hier het uiterlijk van uw website aan.';
+$GLOBALS["tgstatistics"] 	= 'Statistieken';
+$GLOBALS["tgdstatistics"] 	= 'Laat de statistieken zien van uw website.';
+$GLOBALS["tgbanners"] 		= 'Reclameblokken';
+$GLOBALS["tgdbanners"] 		= 'Beheer hier de reclameblokken van uw website.';
+
+//  Functie Titels
+$GLOBALS["tfprivileges"] 	= 'Beheer gebruikersgroepen';
+$GLOBALS["tfauthors"] 		= 'Beheer gebruikers';
+$GLOBALS["tfsetregister"] 	= 'Registratie instellingen';
+$GLOBALS["tfbackup"] 		= 'Database Backup/Terugzetten';
+$GLOBALS["tfsites"] 		= 'Beheer sites';
+$GLOBALS["tffiletypes"] 	= 'Beheer bestandstype';
+$GLOBALS["tfuserdata"] 		= 'Gebruikersdata frame layout';
+$GLOBALS["tfmenusettings"] 	= 'Beheer menu instellingen';
+$GLOBALS["tftopgroups"] 	= 'Beheer topmenu\'s';
+$GLOBALS["tfgroups"] 		= 'Beheer menu\'s';
+$GLOBALS["tfsubgroups"] 	= 'Beheer submenu\'s';
+$GLOBALS["tftags"] 		= 'Beheer tags';
+$GLOBALS["tfsidebars"] 		= 'Beheer zijkolommen';
+$GLOBALS["tfimageformats"] 	= 'Beheer afbeeldingen';
+$GLOBALS["tfratings"] 		= 'Beheer instellingen inhoud';
+$GLOBALS["tfcontent"] 		= 'Beheer inhoud';
+$GLOBALS["tfsubcontent"] 	= 'Beheer modules';
+$GLOBALS["tfimages"] 		= 'Beheer afbeeldingen';
+$GLOBALS["tfscripts"] 		= 'Beheer scripts';
+$GLOBALS["tfdownloads"] 	= 'Beheer downloads';
+$GLOBALS["tflanguages"] 	= 'Beheer talen';
+$GLOBALS["tftusergroups"] 	= 'Vertaal gebruikersgroep namen';
+$GLOBALS["tfttagcategories"] 	= 'Vertaal tag categorien';
+$GLOBALS["tfttopgroups"] 	= 'Vertaal topmenu';
+$GLOBALS["tftgroups"] 		= 'Vertaal menu\'s';
+$GLOBALS["tftsubgroups"] 	= 'Vertaal submenu\'s';
+$GLOBALS["tftcontent"] 		= 'Vertaal inhoud';
+$GLOBALS["tfserversettings"] 	= 'Server instellingen';
+$GLOBALS["tfsitesettings"] 	= 'Website instellingen';
+$GLOBALS["tfcolorsform"] 	= 'Instellingen van kleuren en lettertype';
+$GLOBALS["tfsetgraphics"] 	= 'Grafische instellingen';
+$GLOBALS["tfthemes"] 		= 'Beheer thema\'s';
+$GLOBALS["tfviewstatistics"] 	= 'Laat de statistieken zien';
+$GLOBALS["tfprunestatistics"] 	= 'Samenvatting archief statistieken';
+$GLOBALS["tfbanners"] 		= 'Beheer reclame blokken';
+$GLOBALS["tfexcelexport"] = 'Excel-Export';
+
+?>
