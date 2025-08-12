1ec49d8f8a2f604d1c381fc6d662dc739700b11a
me-suzy <me.suzana@gmail.com>
2025-08-12 05:55:44 +0300

    Add Content Management/metajour2_1/metajour/basic_view_init.php

---
+++ Content Management/metajour2_1/metajour/basic_view_init.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('basic_view_list.php');
+
+class basic_view_init extends basic_view_list {
+}
+
+?>
