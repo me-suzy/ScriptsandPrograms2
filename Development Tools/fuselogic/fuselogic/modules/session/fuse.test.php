388ab36b713639f53906d7be9488783762a49dbf
me-suzy <me.suzana@gmail.com>
2025-08-12 09:25:38 +0300

    Add Development Tools/fuselogic/fuselogic/modules/session/fuse.test.php

---
+++ Development Tools/fuselogic/fuselogic/modules/session/fuse.test.php
@@ -0,0 +1,23 @@
+<?php
+
+ini_set('session.save_path',dirname($_SERVER['DOCUMENT_ROOT']).'/www_session');
+ini_set('session.cookie_lifetime',300);
+require_once 'function.presistence_object.php';
+
+presistence_object('test1_object','counter');
+echo '<br>test1_object->count() = '.$test1_object->count();
+
+echo '<br>';
+
+presistence_object('test2_object','gender');
+echo '<br>test2_object->gender() = '.$test2_object->check();
+
+echo '<h2>Presistence Object Info</h2>';
+foreach($presistence_copy->list as $key=>$value){
+   echo 'Instance Name = '.$key.'<br>';
+	 echo 'Object Name = '.$value->object_name.'<br>';	
+	 echo 'Object File = '.$value->object_file.'<br>';	
+	 echo '<br>';
+}
+
+?>
