4803ea5d8f14daf1fc060379807173f21e87e75d
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:50 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_scripts.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_scripts.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'Beheer scripts';
+$GLOBALS["tFormTitle1"] = 'Upload scripts/maak script subdirectories';
+$GLOBALS["tFormTitle2"] = 'Upload script';
+$GLOBALS["tFormTitle3"] = 'Creeëren subdirectory';
+
+//  List Functions
+$GLOBALS["tAddNewScript"] = 'Nieuw script toevoegen';
+$GLOBALS["tDeleteScript"] = 'Verwijder script';
+$GLOBALS["tDeleteScriptDir"] = 'Verwijder script subdirectory';
+
+//  List Headings
+$GLOBALS["tScriptName"] = 'Script naam';
+
+//  Form Fields
+$GLOBALS["tScriptFilename"] = 'Script bestandsnaam';
+$GLOBALS["tSubdirectoryname"] = 'Subdirectory naam';
+
+//  Form Buttons
+$GLOBALS["bUpload"] = 'Upload!';
+$GLOBALS["bCreateDir"] = 'Creeëren!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] = 'Selecteer een bestand voor om te uploaden.';
+$GLOBALS["eNoDir"] = 'Voor een naam in voor de directory.';
+$GLOBALS["eDirAlreadyExists"] = 'subdirectory bestaat al';
+$GLOBALS["eZeroByteFile"] = 'Bestand is 0 bytes lang';
+$GLOBALS["eNoFileUpload"] = 'Upload van het bestand is niet gelukt.';
+$GLOBALS["eInvalidFileSize"] = 'Bestand is te groot om te uploaden';
+$GLOBALS["eInvalidFileType"] = 'Dit bestand is geen geldig script';
+$GLOBALS["eInvalidMimeType"] = 'MIME soort van dit script is geen geldige scriptsoort';
+
+?>
