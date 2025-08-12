a578ed3de0f9874e6ffd3887d86da9daf9ad5c19
me-suzy <me.suzana@gmail.com>
2025-08-12 14:16:36 +0300

    Add File Manipulation/dl4lite/UPLOAD/lang/1/lang_warn.php

---
+++ File Manipulation/dl4lite/UPLOAD/lang/1/lang_warn.php
@@ -0,0 +1,30 @@
+<?php
+/*********************************************************
+* Name: lang_warn.php
+* Author: RW::Download 4.0
+* Contact: realworld@blazefans.com
+* Description: Language variables
+* Version: 4.00
+*********************************************************/
+$lang = array(
+
+'warn_alreadyrated' => "The file could not be rated as you have already rated this file.",
+'warn_missing' => "You did not fill in all the required fields. Please click back",
+'warn_catdel' => "Are you sure you want to delete this category? All downloads in this category will be deleted! This can not be undone",
+'warn_dldel' => "Are you sure you want to delete this link from the database? This can not be undone!",
+'warn_commentdel' => "Are you sure you want to delete this comment from the database? This can not be undone!",
+'warn_fields' => "The following fields are missing",
+'warn_copyright' => "Copyright tag removed. You may not remove this tag. RW::Download will not function until this has been added",
+'warn_notloggedin' => "You are not logged in. You must register to access this page.",
+'warn_useforum' => "You have chosen to use an external database for the user table. You must use the forum you selected to edit members.",
+'warn_report' => "Are you sure you want to report this download as broken?",
+'warn_delcustom' => "Are you sure you want to remove this custom entry? All downloads using this entry will no longer display this value",
+'warn_imgdel' => "Are you sure you want to remove the image from this download? The image will also be removed from the server",
+'warn_nocatshere' => "You do not have permission to view any categories here. You may need to register and login. If you are already logged in and think you should have access to this area, please contact the admin.",
+'warn_unapp' => "This file has not been approved. Only admins and the owner of this file can see this page until it has been approved by an admin",
+'warn_nouser' => "Could not find a user by that name",
+'warn_upload' => "Notice: All files will be uploaded when you select 'preview'. Depending on the size of the files and your internet connection, this could take some time.",
+'ext_warn' => "Warning: You are using a non default user database. RW::Download will restrict the options on this page to avoid potential corruption of external database data. Please use the external database's admin section to change user details not listed on this page",
+)
+
+?>
