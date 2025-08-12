7a55dd1595d3298017be46e82765884d2f509102
me-suzy <me.suzana@gmail.com>
2025-08-13 01:30:06 +0300

    Add Unsorted/celeste aug 20 2003/celeste_data/settings/config.panel.php

---
+++ Unsorted/celeste aug 20 2003/celeste_data/settings/config.panel.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Project Source File
+ * Celeste 2003 1.00 Build 0011
+ * Jul 19, 2003
+ * Celeste Dev Team - Lvxing / Y10k
+ *
+ * Copyright (C) 2003 CelesteSoft.com. All rights reserved.
+ *
+ * This software is the proprietary information of celeste Team.
+ * Use is subject to license terms.
+ */
+
+$acp_root_action = array();
+
+
+define('SET_PANEL_MODE',1);
+// 1 = advanced, 0 = simple
+
+define('SET_PANEL_MAIL_PER_BATCH', 50);
