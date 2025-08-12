d1df572456d7694732ab3d8019807a786ff4878f
me-suzy <me.suzana@gmail.com>
2025-08-12 03:34:40 +0300

    Add Content Management/icedl/menu.php

---
+++ Content Management/icedl/menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Untitled Document</title>
+</head>
+
+<body>
++ <a href="protected.php">Home</a><br>
++ <a href="add.php">Add<br>
+</a>+ <a href="edit.php">Edit</a><br>
++ <a href="show.php">Show</a><br>
++ <a href="logout.php">Logout</a>
+</body>
+</html>
