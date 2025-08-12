707140fb3a9429de76e6c27222e6d2696de752bd
me-suzy <me.suzana@gmail.com>
2025-08-12 21:32:54 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/comic.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/comic.lang.php
@@ -0,0 +1,54 @@
+<?php
+
+// Head titles
+$tpl->assign("lang_head_details", "Comic Details");
+$tpl->assign("lang_head_issuedetails", "Issue Details");
+$tpl->assign("lang_head_comicdetails", "Comic Details");
+$tpl->assign("lang_head_artistdetail", "Artist Details");
+$tpl->assign("lang_head_about", "About This Issue");
+$tpl->assign("lang_head_story", "Story Details");
+
+// Comic details menu
+$lang_comic_edit = "Edit";
+$lang_comic_delete = "Delete";
+$lang_comic_comurl = "Link Comic";
+$lang_comic_puburl = "Link Publisher";
+$lang_comic_pdf = "PDF";
+$lang_comic_print = "Printer Friendly";
+$lang_comic_loan = "Lend Comic";
+$lang_comic_fav = "Add To Favorites";
+$lang_comic_export = "Export";
+$lang_comic_borrowed = "Lent Out";
+$lang_no_ebaylink = "No link added";
+$lang_ebay_click = "Click to see this comic at Ebay";
+$lang_next_issue = "Next Issue";
+$lang_prev_issue = "Previous Issue";
+$lang_part_miniseries = "Of";
+$lang_no_pubdate = "No date added";
+$lang_no_plot = "No plot has been written";
+$lang_user_def1 = "User Defined";
+$lang_user_def2 = "No Entry";
+
+// Comic details
+$tpl->assign("lang_issue", "Issue:");
+$tpl->assign("lang_volume", "Volume:");
+$tpl->assign("lang_issuecost", "Issue Cost");
+$tpl->assign("lang_issuevalue", "Current Value");
+$tpl->assign("lang_publisher", "Publisher");
+$tpl->assign("lang_format", "Comic Format");
+$tpl->assign("lang_type", "Comic Type");
+$tpl->assign("lang_condition", "Issue Condition");
+$tpl->assign("lang_ebayoption", "Ebay link");
+$tpl->assign("lang_genre", "Genre");
+$tpl->assign("lang_pubdate", "Publication Date");
+$tpl->assign("lang_variation", "Issue Variation");
+$tpl->assign("lang_language", "Language");
+$tpl->assign("lang_writer", "Writer");
+$tpl->assign("lang_inker", "Inker");
+$tpl->assign("lang_penciler", "Penciler");
+$tpl->assign("lang_colorist", "Colorist");
+$tpl->assign("lang_coverartist", "Cover Artist");
+$tpl->assign("lang_letterer", "Letterer");
+$tpl->assign("lang_quantity", "Quantity");
+
+?>
