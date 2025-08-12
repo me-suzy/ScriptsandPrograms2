8dd831c20ed3e604a0f91bf43d77b63f8bde4643
me-suzy <me.suzana@gmail.com>
2025-08-12 12:04:05 +0300

    Add E-Commerce/enterprise3.01/install/templates/main_page.php

---
+++ E-Commerce/enterprise3.01/install/templates/main_page.php
@@ -0,0 +1,38 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+
+<head>
+
+<title>Enterprise Shopping Cart Software</title>
+
+<meta name="ROBOTS" content="NOFOLLOW">
+
+<link rel="stylesheet" type="text/css" href="templates/main_page/stylesheet.css">
+
+<script language="javascript" src="templates/main_page/javascript.js"></script>
+
+</head>
+
+<body text="#000000" bgcolor="#ffffff" leftmargin="0" topmargin="0" marginheight="0" marginwidth="0">
+
+<?php require('templates/main_page/header.php'); ?>
+
+<?php require('templates/pages/' . $page_contents); ?>
+
+<?php require('templates/main_page/footer.php'); ?>
+
+</body>
+
+</html>
