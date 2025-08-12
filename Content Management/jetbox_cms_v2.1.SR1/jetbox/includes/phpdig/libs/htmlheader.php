f5e614695c171e2ee130aacaa972a51abf6c981a
me-suzy <me.suzana@gmail.com>
2025-08-12 05:07:53 +0300

    Add Content Management/jetbox_cms_v2.1.SR1/jetbox/includes/phpdig/libs/htmlheader.php

---
+++ Content Management/jetbox_cms_v2.1.SR1/jetbox/includes/phpdig/libs/htmlheader.php
@@ -0,0 +1,3 @@
+<?php print '<?xml version="1.0" encoding="'.PHPDIG_ENCODING.'" ?>'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
