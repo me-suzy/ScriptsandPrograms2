6be1035f0d2de60709871c7ce4a5256dad0c306e
me-suzy <me.suzana@gmail.com>
2025-08-13 02:21:17 +0300

    Add Unsorted/datingscript/languages/it/ita.php

---
+++ Unsorted/datingscript/languages/it/ita.php
@@ -0,0 +1,123 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.1.1                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 04/05/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               it.php                           #
+# File purpose            Italian language file            #
+# File created by         D.Bianco <webmaster@9euro.com>   #
+############################################################
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Login o password errata - prova ancora',//1
+'Area amministrazione',//2
+'Admin Login',//3
+'Admin Password',//4
+'Invio',//5
+'Accesso inibito ai non amministratori',//6
+'Pagina principale',//7
+'Cancella richieste',//8
+'Ripara&Ottimizza',//9
+'Gestione utente',//10
+'Gestione FAQ',//11
+'Maillist',//12
+'Esci',//13
+'Sei uscito dall area amministrativa<br><br>Grazie per aver utilizzato lo script',//14
+'Messaggi totali',//15
+'Messaggi inviati',//16
+'La parte seguente',//17
+'{0} messaggi sono stati inviati a ',//18
+'Tu hai messaggi non letti in {0}',//19 {0} - SiteName (C_SNAME)
+'Trovati {0} membri',//20
+'Gestore Maillist',//21
+'Con stato:',//22
+'Opzioni e campi messaggio:',//23
+'Tipo messaggio',//24
+'Da',//25
+'Oggetto',//26
+'Corpo messaggio',//27
+'Conferma sui risultati',//28
+'Torna indietro',//29
+'Invia messaggi',//30
+'The unfinished dispatches - {0}',//31
+'Subject : {0} [Sended to {1} users] :::>',//32
+'Finish this dispatch',//33
+'Text',//34
+'HTML',//35
+'Preview',//36
+'Utenti con messaggi non letti - ',//37
+'Messaggi totali non letti - ',//38
+'Ultimo id messaggio - ',//39
+'Invia messaggio conferma ad utenti con messaggi non letti',//40
+'Profilo aggiornato',//41
+'FAQ creata',//42
+'Crea nuova',//43
+'Inserisci domanda',//44
+'Inserisci risposta',//45
+'Gestione FAQ',//46
+'Crea nuova categoria FAQ',//47
+'Modifica categoria FAQ',//48
+'Elimina categoria FAQ',//49
+'Crea FAQ',//50
+'Seleziona categoria FAQ',//51
+'FAQ domanda',//52
+'FAQ risposta',//53
+'Prima di tutto devi creare una categoria FAQ',//54
+'Inserisci nome categoria',//55
+'Categoria FAQ creata',//56
+'Nuova categoria creata:',//57
+'Nome categoria FAQ',//58
+'Modifica categoria FAQ',//59
+'Modifica',//60
+'Categoria modificata',//61
+'Modifica categoria FAQ',//62
+'FAQ eliminata',//63
+'FAQ categoria eliminata',//64
+'Nota: stai attento! Eliminando una categoria, eliminerai anche tutte le domande di quella categoria.',//65
+'Elimina',//66
+'0 - Attendi controllo email<br>1 - Attendi controllo dopo registrazione<br>2 - Attendi controllo dopo aggiornamento<br>3,8 - Attendi autorizzazione a rimuovere(senza, con mostra)<br>7 - Utente attivo<br>',//67
+'Database riparato ed aggiornato',//68
+'Utente con ID={0} eliminato dal database',//69
+'Contatti resettati',//70
+'Autorizzato user con id={0}',//71
+'IP',//72
+'Richiesti [0 - 255]',//73
+'Stato - (STAI ATTENTO)',//74
+'Consenti',//75
+'Gestione utenti',//76
+'Totale utenti attivi :',//77
+'Utenti in attesa di autorizzazione dopo registrazione',//78
+'Utenti in attesa di autorizzazione dopo variazione',//79
+'Utenti in attesa di rimozione',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
+
