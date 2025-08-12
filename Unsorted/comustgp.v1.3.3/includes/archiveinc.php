20ec4aad664cbed929c8ce9ec758853d6d73a603
me-suzy <me.suzana@gmail.com>
2025-08-13 01:52:32 +0300

    Add Unsorted/comustgp.v1.3.3/includes/archiveinc.php

---
+++ Unsorted/comustgp.v1.3.3/includes/archiveinc.php
@@ -0,0 +1,81 @@
+<?
+#######################################
+###         ComusTGP version 1.3.3  ###
+###         nibbi@nibbi.net         ###
+###         Copyright 2002          ###
+#######################################
+?>
+<?php
+// Include Configuration file
+include_once($DOCUMENT_ROOT . "/includes/config.inc.php");
+
+if ($cjultra == Yes){
+   $cjstring = $cjstring;
+   $cjstring2 = $cjstring2;
+}else{
+   $cjstring = "";
+   $cjstring2 = "";
+}
+
+/* GET AND DISPLAY ALL POSTS ACCORDING TO CATEGORY SELECTED */
+
+   $query = "SELECT * FROM tblCategories ORDER BY Category";
+   $result = mysql_query ($query)
+        or die ("Query failed");
+
+if ($result) {
+      echo"<table width=95%>
+                <tr valign=top> 
+                  <td height=77> 
+                    <p><a href=\"javascript:bookmark();\">Bookmark Us Now</a> (Ctrl-D 
+                      for Netscape users)</p>
+                    <p align=center>";
+
+   while ($r = mysql_fetch_array($result)) { 
+
+   $Category = $r["Category"];
+
+ echo "<b><a href=\"archive.php?choice=$Category\">$Category</a> - </b>";
+ 
+   }
+}                   
+
+   echo "</p></td>
+          </tr>
+        </table>";
+
+
+// See if they selected anything
+
+if ($choice) {
+   $query = "select * from tblTgp WHERE category='$choice' AND accept='yes' ORDER BY id DESC LIMIT $lim";
+
+$result = mysql_query ($query)
+        or die ("Query failed");
+
+if ($result) {  
+echo "<table width=95% border=1 cellpadding=10>
+                <tr bgcolor=$bgcolor valign=top> 
+                  <td height=100%> 
+                    <div align=left> 
+                      <table width=100% border=0 cellspacing=0 cellpadding=0>
+                        <tr>";
+while ($r = mysql_fetch_array($result)) { 
+
+$cat = $r["category"]; 
+$url = $r["url"]; 
+$desc = $r["description"]; 
+$date = $r["date"]; 
+
+echo "<a href=\"$cjstring$url$cjstring2\"><b>$cat</b></font>&nbsp&nbsp<font color=#0033CC>$desc</font></a><BR>"; 
+      } 
+   }
+echo "</table>                    
+            </div>
+               </td>
+              </tr>
+             </table>"; 
+} else { 
+echo "<p>&nbsp;</p><p>&nbsp;</p><font color=red><h3><b>Please select the catagory above that you would like to view.</b></h3></font>"; 
+} 
+?>
