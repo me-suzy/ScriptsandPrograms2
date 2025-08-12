775d1b517a30b3b3110cadbdbadd46f76055c048
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:45 +0300

    Add Content Management/metajour2_1/metajour/lang/payment.en.php

---
+++ Content Management/metajour2_1/metajour/lang/payment.en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Payment';
+$LANG['label_name'] = 'Name';
+$LANG['label_init'] = 'Initial fee';
+$LANG['label_percentage'] = 'Fee by procent';
+$LANG['label_vatid'] = 'VAT';
+?>
