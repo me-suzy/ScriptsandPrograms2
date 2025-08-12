4f43eacc15599c2c0b900ac97c173e62f5c28b67
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:20 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_content.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_content.php
@@ -0,0 +1,92 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintain articles';
+$GLOBALS["tFormTitle2"] = 'maintain article translations';
+
+//  List Headings
+$GLOBALS["tMenu"] = 'Menu';
+$GLOBALS["tPageRef"] = 'Article Name';
+$GLOBALS["tSubmenu"] = 'Sub-menu';
+$GLOBALS["tTitle"] = 'Title';
+
+//  List Functions
+$GLOBALS["tMenuFilter"] = 'Menu filter';
+$GLOBALS["tAllMenus"] = 'All Menus';
+$GLOBALS["tAddNewContent"] = 'Add new article';
+$GLOBALS["tEditContent"] = 'Edit article';
+$GLOBALS["tDeleteContent"] = 'Delete article';
+$GLOBALS["tTranslate"] = 'Translate article';
+$GLOBALS["tViewTranslation"] = 'View translation';
+$GLOBALS["tEditTranslation"] = 'Edit translation';
+$GLOBALS["tRSSen"] 		= 'RSS';
+$GLOBALS["tSearchen"] 		= 'Search';
+
+//  Form Block Titles
+$GLOBALS["thContentLinks"] = 'Article Parent Details';
+$GLOBALS["thDates"] = 'Dates for Display';
+$GLOBALS["thHeader"] = 'Header';
+$GLOBALS["thTeaser"] = 'Teaser';
+$GLOBALS["thBodyContent"] = 'Body Content';
+$GLOBALS["thFooter"] = 'Footer';
+$GLOBALS["thPosition"] = 'Position on Page';
+$GLOBALS["thRatings"] = 'Rating Options';
+$GLOBALS["thRSS"]	= 'RSS / Search';
+
+//  Form Field Headings
+$GLOBALS["tPublishDate"] = 'Publish date';
+$GLOBALS["tExpiryDate"] = 'Expiry date';
+$GLOBALS["tHeader"] = 'Header title';
+$GLOBALS["tHeaderImage"] = 'Header image';
+$GLOBALS["tTeaser"] = 'Teaser';
+$GLOBALS["tBodyText"] = 'Body text';
+$GLOBALS["tShowHeader"] = 'Show header';
+$GLOBALS["tShowAuthor"] = 'Show author';
+$GLOBALS["tShowUpdate"] = 'Show update date';
+$GLOBALS["tTeaserImage"] = 'Teaser image';
+$GLOBALS["tTeaserImageAlign"] = 'Align teaser image';
+$GLOBALS["tDetailImage"] = 'Image on details page';
+$GLOBALS["tDetailImageAlign"] = 'Align details page image';
+$GLOBALS["tLeftRight"] = 'Left/Right column';
+$GLOBALS["tCanRate"] = 'Allow article rating';
+$GLOBALS["tCanComment"] = 'Allow article comments';
+$GLOBALS["tPrinterFriendly"] = 'Allow "printer friendly" display';
+$GLOBALS["tPDFPrint"] = 'Allow PDF display';
+$GLOBALS["tTellFriend"] = 'Allow Tell-a-Friend display';
+$GLOBALS["tAuthor"] = 'Article author';
+$GLOBALS["tRSS"] 		= 'Provide content for RSS';
+$GLOBALS["tSearch"]	= 'Provide content when searching';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'This form lets you edit the content of your articles.<br /><br />1. If the teaser text is left empty then the body will be shown on the contents page.<br />2. If the teaser text is filled and the body text is empty no "Read me more..." will be shown.<br />3. If teaser and body text are entered, then the teaser text will be shown on the contents page,<br />and the "Read me more..." will link to the body text on the details page.<br /><br />If you use HTML code it will be encoded so the contents will be visible as entered. <br />You can use custom tags. These include:<br />';
+
+$GLOBALS["hMenu"] = 'This is the menu against alongside which this content page will be displayed.';
+$GLOBALS["hSubmenu"] = 'This is the sub-group menu against alongside which this content page will be displayed.';
+$GLOBALS["hPublishDate"] = 'The publication date of the content, before which the page will not be displayed.';
+$GLOBALS["hExpiryDate"] = 'the expiry date for the content, after which the page will not be displayed.';
+$GLOBALS["hHeader"] = 'The title that will appear in the title bar for this page if the "Show Header" flag is enabled.';
+$GLOBALS["hHeaderImage"] = 'An image that will appear as an alternative to the text title in the title bar for this page if the "Show Header" flag is enabled.';
+$GLOBALS["hTeaser"] = 'Teaser text.<br />If you enter text in this field, it will appear on the page instead of body text, with a "Read More" link to the main body of the content.';
+$GLOBALS["hBodyText"] = 'This is the main body text of your content.';
+$GLOBALS["hShowHeader"] = 'If this flag is enabled, the header text or image will be displayed in a title bar above the teaser or body text of your content.';
+$GLOBALS["hShowAuthor"] = 'If this flag is set, a footer to your content will show the name (with an e-mail link) of the author who posted this content.';
+$GLOBALS["hShowUpdate"] = 'If this flag is set, a footer to your content will show the update date when it was originally posted, or last edited.';
+$GLOBALS["hTeaserImage"] = 'Here, you can specify an image to display at the top of the teaser display.';
+$GLOBALS["hTeaserImageAlign"] = 'If you have selected a teaser image, you can display it to the Left or Right of any teaser text.';
+$GLOBALS["hDetailImage"] = 'Here, you can specify an image to display at the top of the main body text display.';
+$GLOBALS["hDetailImageAlign"] = 'If you have selected an image for the main body text, you can display it to the Left or Right of the body text.';
+$GLOBALS["hLeftRight"] = 'This flag determines whether this content page appears in the Left or Right column of a split content display.';
+$GLOBALS["hCanRate"] = 'Check this flag to allow rating of this article.';
+$GLOBALS["hCanComment"] = 'Check this flag to allow viewers to comment on this article.';
+$GLOBALS["hPrinterFriendly"] = 'Check this box to display the Printer Friendly icon with this article.';
+$GLOBALS["hPDFPrint"] = 'Check this box to display the PDF icon with the article';
+$GLOBALS["hTellFriend"] = 'Check this box to display the Mail icon with the article';
+$GLOBALS["hAuthor"] = 'The author of this article.';
+$GLOBALS["hRSS"] 					= 'Provide this content for RSS.';
+$GLOBALS["hSearch"] 			= 'Provide this content for the search function.';
+
+//  Error Messages
+$GLOBALS["eArticleExists"] = 'An article with this name already exists, please choose another.';
+$GLOBALS["eInvalidName"] = 'Article name contains invalid characters.';
+
+?>
