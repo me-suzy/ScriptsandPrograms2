e301218e64c324e2a31eeb86bd9dff375c3c28d2
me-suzy <me.suzana@gmail.com>
2025-08-12 13:57:33 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/client/logout.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/client/logout.php
@@ -0,0 +1,51 @@
+<?
+$file_rev="041305";
+////////////////////////////////////////////////////////
+//                 phpBannerExchange                  //
+//                   by: Darkrose                     //
+//              (darkrose@eschew.net)                 //
+//                                                    //
+// You can redistribute this software under the terms //
+// of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of  //
+// the License, or (at your option) any later         //
+// version.                                           //
+//                                                    //
+// You should have received a copy of the GNU General //
+// Public License along with this program; if not,    //
+// write to the Free Software Foundation, Inc., 59    //
+// Temple Place, Suite 330, Boston, MA 02111-1307 USA //
+//                                                    //
+//     Copyright 2004 by eschew.net Productions.      //
+//   Please keep this copyright information intact.   //
+////////////////////////////////////////////////////////
+
+include("../config.php");
+include("../css.php");
+
+if($use_gzhandler==1){
+ob_start("ob_gzhandler");
+}
+
+require_once('../lib/template_class.php');
+include("../lang/client.php");
+
+	session_start();
+	$session=session_id();
+	$login=$_SESSION['login'];
+	$pass=$_SESSION['pass'];
+	$id=$_SESSION['id'];
+	session_destroy();
+
+	$page = new Page('../template/client_logout.php');
+	$page->replace_tags(array(
+	'css' => "$css",
+	'session' => "$session",
+	'baseurl' => "$baseurl",
+	'title' => "$exchangename - $LANG_logout_title",
+	'msg' => "$LANG_logout_message",
+	'footer' => '../footer.php',
+	'menu' => 'client_menuing.php'));
+
+	$page->output();
+?>
