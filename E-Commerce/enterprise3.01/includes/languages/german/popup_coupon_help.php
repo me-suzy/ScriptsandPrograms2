ab3615db0c2ec39c8a797152771b19c4cd5895a4
me-suzy <me.suzana@gmail.com>
2025-08-12 12:00:35 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/popup_coupon_help.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/popup_coupon_help.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_COUPON_HELP', 'Discount Coupon Help');
+define('TEXT_CLOSE_WINDOW', 'Close Window [x]');
+define('TEXT_COUPON_HELP_HEADER', 'Congratulations, you have redeemed a Discount Coupon.');
+define('TEXT_COUPON_HELP_NAME', '<br><br>Coupon Name : %s');
+define('TEXT_COUPON_HELP_FIXED', '<br><br>The coupon is worth %s discount against your order');
+define('TEXT_COUPON_HELP_MINORDER', '<br><br>You need to spend %s to use this coupon');
+define('TEXT_COUPON_HELP_FREESHIP', '<br><br>This coupon gives you free shipping on your order');
+define('TEXT_COUPON_HELP_DESC', '<br><br>Coupon Description : %s');
+define('TEXT_COUPON_HELP_DATE', '<br><br>The coupon is valid between %s and %s');
+define('TEXT_COUPON_HELP_RESTRICT', '<br><br>Product/Category Restrictions');
+define('TEXT_COUPON_HELP_CATEGORIES', 'Category');
+define('TEXT_COUPON_HELP_PRODUCTS', 'Product');
+?>
