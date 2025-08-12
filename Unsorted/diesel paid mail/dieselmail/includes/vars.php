18a393afd9029ed469f6697f025199af37ad9efe
me-suzy <me.suzana@gmail.com>
2025-08-13 02:32:49 +0300

    Add Unsorted/diesel paid mail/dieselmail/includes/vars.php

---
+++ Unsorted/diesel paid mail/dieselmail/includes/vars.php
@@ -0,0 +1,34 @@
+<?
+       $admin_mail_id="webmaster@YourSiteName.com";   //Site Admin's email adds. for contact
+
+       //paths to folders
+       $url="http://www.YourSiteName.com/dieselmail";
+       $path="/home/e-mailc/public_html/dieselmail";
+
+       $site_url="$url/site";   //virtual path of the site folder       
+       $site_path="$path/site";   //physical path of the site folder  
+       $siteadds="www.YourSiteName.com";   //site address
+       $sitename="YourSiteName";   //site name
+
+       $dbhost = "localhost";   //Host of the DataBase
+       $dbname = "11111";   //DB Name
+       $dbuser = "11111";   //DB user name
+       $dbpwd = "11111";    //DB user password
+
+       $admin_url="$url/admin";   //virtual path of the admin folder
+       $admin_path="$path/admin";  //physical path of the admin folder
+       $include_path="$path/includes";  //physical path of the includes folder
+
+       $admin_html_path="$admin_path/html";  //virtual path of the admin html folder
+       $site_html_path="$site_path/html";   //virtual path of the site html folder
+       $banner_url="$url/images";   //virtual path of the images folder
+
+       $advt_regn_email_subject="Advertisement Purchased";  //Advertiser Registeration email message subject
+       $email_regn_subject="Login Details for $sitename.com"; //Member Registeration email message subject
+       $email_signup_subject="Signup Confirmation Email from $sitename.com"; //Sign up confirmation email subject
+
+       $email_contactus_subject="$sitename.com Contact Us Message";
+
+       $color_feedback_good = "green";   //normal feedback message
+       $color_feedback_bad = "red";    //error feedback message
+?>
