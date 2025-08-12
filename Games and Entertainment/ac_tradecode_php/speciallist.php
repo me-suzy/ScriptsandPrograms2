0afd19e93c66491ac9f545f1faf1a8362b55b255
me-suzy <me.suzana@gmail.com>
2025-08-12 16:26:12 +0300

    Add Games and Entertainment/ac_tradecode_php/speciallist.php

---
+++ Games and Entertainment/ac_tradecode_php/speciallist.php
@@ -0,0 +1,13 @@
+<?php
+
+$speciallist[0x0900] = "Does not yet work";
+$speciallist[0x1dc4] = "NES Contest only";
+$speciallist[0x1dc8] = "NES Contest only";
+$speciallist[0x1dcc] = "NES Contest only";
+$speciallist[0x1dd0] = "NES Contest only";
+$speciallist[0x1de4] = "Does not yet work";
+$speciallist[0x1de8] = "Does not yet work";
+$speciallist[0x1dec] = "Does not yet work";
+$speciallist[0x1df0] = "Does not yet work";
+$speciallist[0x2E01] = "K.K. Love Song and two NES games";
+?>
