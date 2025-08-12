c851f1e142eef63dafb8e389175b4adedb920c1c
me-suzy <me.suzana@gmail.com>
2025-08-12 17:31:43 +0300

    Add Guestbooks/guestbook/guestbook/admin/smilie_edited.php

---
+++ Guestbooks/guestbook/guestbook/admin/smilie_edited.php
@@ -0,0 +1,96 @@
+<?php
+/****************************************************************/
+/*                       phphg Guestbook                        */
+/*                 admin/smilie_edited.php file                 */
+/*                      (c)copyright 2003                       */
+/*                       By hinton design                       */
+/*                 http://www.hintondesign.org                  */
+/*                  support@hintondesign.org                    */
+/*                                                              */
+/* This program is free software. You can redistrabute it and/or*/
+/* modify it under the terms of the GNU General Public Licence  */
+/* as published by the Free Software Foundation; either version */
+/* 2 of the license.                                            */
+/*                                                              */
+/****************************************************************/
+$phphg_real_path = "./../";
+include($phphg_real_path . 'common.php');
+
+if($HTTP_COOKIE_VARS['loged'] == 'yes') {
+   if($HTTP_COOKIE_VARS['user_level'] == '1') {
+      if((!$HTTP_POST_VARS['name']) || (!$HTTP_POST_VARS['url']) || (!$HTTP_POST_VARS['code'])) {
+          include("page_header.php");
+          $display = "You didnt fill in some required fields.";
+          $template->getFile(array(
+                             'error' => 'admin/error.tpl')
+          );
+          $template->add_vars(array(
+                             'L_ERROR' => $lang['error'],
+                             'DISPLAY' => $display)
+          );
+          $template->parse("error");
+          include("page_footer.php");
+          exit();
+      }
+
+      $sql = "UPDATE ".$prefix."_smilies SET name='$HTTP_POST_VARS[name]', url='$HTTP_POST_VARS[url]', code='$HTTP_POST_VARS[code]' WHERE id='$HTTP_POST_VARS[id]'";
+      $result = $db->query($sql);
+
+      if(!$result) {
+         include("page_header.php");
+         $display = "Could not edit the smilie";
+         $template->getFile(array(
+                            'error' => 'admin/error.tpl')
+         );
+         $template->add_vars(array(
+                            'L_ERROR' => $lang['error'],
+                            'DISPLAY' => $display)
+         );
+         $template->parse("error");
+         include("page_footer.php");
+         exit();
+      } else {
+         include("page_header.php");
+         $display = "The smilie has been edited.";
+         $link = "smilie.php";
+         $template->getFile(array(
+                            'success' => 'admin/success.tpl')
+         );
+         $template->add_vars(array(
+                            'L_SUCCESS' => $lang['success'],
+                            'DISPLAY' => $display,
+                            'LINK' => $link)
+         );
+         $template->parse("success");
+         include("page_footer.php");
+         exit();
+      }
+   } else {
+      include('page_header.php');
+      $display = "You do not have permission to view this page.";
+      $template->getFile(array(
+                         'error' => 'admin/error.tpl')
+      );
+      $template->add_vars(array(
+                         'L_ERROR' => $lang['error'],
+                         'DISPLAY' => $display)
+      );
+      $template->parse("error");
+      include('page_footer.php');
+      exit();
+   }
+} else {
+      include('page_header.php');
+      $display = "You are not logged in. Please do so <a href=\"../admin.php\">Here</a>.";
+      $template->getFile(array(
+                         'error' => 'admin/error.tpl')
+      );
+      $template->add_vars(array(
+                         'L_ERROR' => $lang['error'],
+                         'DISPLAY' => $display)
+      );
+      $template->parse("error");
+      include('page_footer.php');
+      exit();
+}
+?>    
