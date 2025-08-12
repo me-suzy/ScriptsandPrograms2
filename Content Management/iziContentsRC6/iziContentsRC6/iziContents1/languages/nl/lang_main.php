2997240c36dcc54af1d7eeb859761589a3abffee
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:42 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_main.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_main.php
@@ -0,0 +1,68 @@
+<?php
+
+$GLOBALS["tLoggedInAs"] = 'U bent ingelogd als';
+$GLOBALS["tNotLoggedIn"] = 'U bent niet ingelogd';
+$GLOBALS["tAdminFunctions"] = 'Admin functies';
+$GLOBALS["tPreferences"] = 'Voorkeuren';
+$GLOBALS["tMemberList"] = 'Ledenlijst';
+$GLOBALS["tRegister"] = 'Registreer';
+
+$GLOBALS["tReadMore"] = 'Lees verder...';
+$GLOBALS["tReturn"] = 'Terug';
+$GLOBALS["tArticlePostedOn"] = 'Geplaatst op';
+$GLOBALS["tArticleUpdatedOn"] = 'Bijgewerkt op';
+$GLOBALS["tArticleBy"] = 'door';
+$GLOBALS["tArticlePostedBy"] = 'Geplaatst door';
+
+$GLOBALS["tFramesUsed"]		= 'Deze website gebruikt frames.';
+$GLOBALS["tNonFramesClick"]	= 'Klik hier voor een non-frames versie van de site.';
+$GLOBALS["tNonFramesWarning"]	= 'Opmerking: de website maakt gebruikt van geneste tabellen, cookies en javascript. Als uw browser dit niet ondersteund, of u hebt deze uitgeschakeld, kunnen wij niet instaan voor de resultaten.';
+
+$GLOBALS["tLanguage"] = 'Taal';
+$GLOBALS["tSite"] = 'Site';
+$GLOBALS["tMasterSite"] = 'Hoofd site';
+$GLOBALS["tTheme"] = 'Thema';
+$GLOBALS["tMasterTheme"] = 'Standaard thema';
+$GLOBALS["tBookmarkPage"] = 'Bookmark deze pagina';
+
+$GLOBALS["tPrivateOption"] = 'Persoonlijke optie';
+$GLOBALS["tPrivateEMail"] = '*** PERSOONLIJK ***';
+$GLOBALS["tPrinterFriendly"] = ' Print vriendelijke versie';
+$GLOBALS["tPDFPrint"] = 'PDF versie';
+$GLOBALS["tTellFriend"] = 'Vertel-een-vriend versie';
+$GLOBALS["tUnrated"] = 'Niet beoordeeld';
+$GLOBALS["tRate"] = 'Bekijk beoordelingen';
+$GLOBALS["tComment"] = 'Geef commentaar op dit artikel';
+$GLOBALS["tViewComments"] = 'Bekijk commentaar';
+$GLOBALS["tComments"] = 'Ander commentaar op dit artikel';
+$GLOBALS["tYourComments"] = 'Uw commentaar op dit artikel';
+$GLOBALS["tRating"] = 'Beoordeling';
+$GLOBALS["tRatings"] = 'Beoordelingen voor dit artikel';
+$GLOBALS["tYourRating"] = 'Uw beoordeling voor dit artikel';
+$GLOBALS["tScore"] = 'Score';
+$GLOBALS["tVoters"] = 'Aantal stemmers';
+$GLOBALS["tVotersPercent"] = '%age of stemmer';
+$GLOBALS["tRatingMessage1"] = 'lezers hebben dit artikel beoordeeld met een score van';
+$GLOBALS["tRatingMessage2"] = 'lezers hebben dit artikel beoordeeld met een gemiddelde score van';
+$GLOBALS["tRatingMessage3"] = 'op een schaal van';
+$GLOBALS["tRatingMessage4"] = 'tot';
+$GLOBALS["tRatingBad"] = 'Zeer slecht';
+$GLOBALS["tRatingGood"] = 'Uitstekend';
+
+$GLOBALS["tAllCategories"] = 'Alle categorieën';
+
+$GLOBALS["editorTags"]  = array('html', 'include', 'ilink', 'pagelink', 'email', 'file', 'contentlist', 'grouplist', 'image', 'sidebar', 'b', 'ol', 'ul');
+$GLOBALS["editorParms"] = array('', '', '', '', '', '', '', '', 'logo.gif, rightborder, het iziContents logo', 'sbright50, Voorbeeld van een Sidebar', '', '', '');
+$GLOBALS["editorTexts"] = array('HTML Tag', 'Include Tag', 'Internal Link Tag', 'Page Link Tag', 'Email Tag', 'File Tag', 'Content List Tag', 'Group List', 'Image Tag', 'Sidebar Tag', 'Bold Style', 'Ordered List', 'Unordered List');
+$GLOBALS["editorIcons"] = array('html', 'include', 'ilink', 'pagelink', 'email', 'file', 'contentlist', 'grouplist', 'img', 'sidebar', 'bold', 'list', 'ulist');
+
+//tell a friend
+$GLOBALS["tMailTo"] = 'Ontvanger e-mailadres';
+$GLOBALS["tMailFromName"] = 'Naam verzender';
+$GLOBALS["tMailFromEMail"] = 'Van e-mailadres';
+$GLOBALS["tMailSubject"] = 'Onderwerp';
+$GLOBALS["tMailMessage"] = 'Bericht';
+$GLOBALS["tMailSend"] = 'Verzeden';
+$GLOBALS["tMailReset"] = 'Herstel';
+
+?>
