c12f64f6e15bd463e58f111e6bf3f310c396c188
me-suzy <me.suzana@gmail.com>
2025-08-12 11:53:51 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/site_map.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/site_map.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id: site_map.php,v 1  English   2003/09/19
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2003 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+
+define('NAVBAR_TITLE', 'All Products');
+define('HEADING_TITLE', 'All Products');
+
+?>
