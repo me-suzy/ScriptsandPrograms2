80b308196a46c47aa3a24f73f021fbd2e1dafece
me-suzy <me.suzana@gmail.com>
2025-08-12 21:55:49 +0300

    Add Polls and Voting/fungl/fungl/help/question.php

---
+++ Polls and Voting/fungl/fungl/help/question.php
@@ -0,0 +1,11 @@
+<div class="rightbox">
+	<h2>Help</h2>
+	<h2>Create question</h2>
+	Click the <b>Create&nbsp;question</b> link just below the headline to create a new question.<br/>
+	<br/>
+
+	<h2>Polls</h2>
+	<b>Action links</b><br/>
+	<b>Edit:</b> edit the question text.<br/>
+	<b>Delete:</b> delete the question.<br/>	
+</div>
