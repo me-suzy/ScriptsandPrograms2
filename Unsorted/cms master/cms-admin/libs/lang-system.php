c87a3b14917841d46ef2521823fd93649bb80a46
me-suzy <me.suzana@gmail.com>
2025-08-13 01:47:06 +0300

    Add Unsorted/cms master/cms-admin/libs/lang-system.php

---
+++ Unsorted/cms master/cms-admin/libs/lang-system.php
@@ -0,0 +1,46 @@
+<?php
+//=============================================================================
+//
+//     CMS Master :: Content management system
+//     File: /cms-admin/libs/lang-system.php
+//     Version: 1.0
+//     Created date: 28 September 2003
+//     Modification date: 28 September 2003
+//
+//============================================================================
+                                                                                     
+class LangSystem {
+
+    var $header_system_stat = "System statistics";
+    var $header_edit_user = "Edit user information";
+    
+    var $menu_system_stat = "System statistics";
+    var $menu_system_check = "System check";
+    var $menu_edit_user = "Edit user information";
+    
+    var $table_system_pages = "Pages";
+    var $table_system_images = "Images";
+    var $table_system_files = "Files";
+    
+    var $field_count_pages = "Total pages";
+    var $field_count_visible_pages = "Visible pages";
+    var $field_count_hidden_pages = "Invisible pages";
+    var $field_count_images = "Total images";
+    var $field_images_size = "Total image size (bytes)";
+    var $field_count_files = "Total files";
+    var $field_files_size = "Total file size (bytes)";
+    var $field_login = "Login";
+    var $field_password = "Password";
+    var $field_retype_password = "Retype password";
+    var $field_email = "E-mail";
+    
+    var $button_update = "Update";
+    
+    var $msg_update_user_ok = "User information updated successfully";
+    
+    var $error_password_incorrect = "Error. Your password entries do not match.";
+    
+    var $notes_edit_user = "Place for notes.";
+}
+
+?>
