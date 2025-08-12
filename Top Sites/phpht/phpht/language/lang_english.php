4755bb3e1798b9fb503852cdcec9e9127c2c85c3
me-suzy <me.suzana@gmail.com>
2025-08-13 00:01:45 +0300

    Add Top Sites/phpht/phpht/language/lang_english.php

---
+++ Top Sites/phpht/phpht/language/lang_english.php
@@ -0,0 +1,79 @@
+<?php
+/****************************************************************/
+/*                       phpht Topsites                         */
+/*                     lang_main.php file                       */
+/*                      (c)copyright 2003                       */
+/*                       By hinton design                       */
+/*                 http://www.hintondesign.org                  */
+/*                  support@hintondesign.org                    */
+/*                                                              */
+/* This program is free software. You can redistrabute it and/or*/
+/* modify it under the terms of the GNU General Public Licence  */
+/* as published by the Free Software Foundation; either version */
+/* 2 of the license.                                            */
+/*                                                              */
+/****************************************************************/
+//Universal variables used on any page.
+$lang['site'] = 'Site Name';
+$lang['hits_in'] = 'Hits In';
+$lang['hits_out'] = 'Hits Out';
+$lang['login'] = 'Login';
+$lang['username'] = 'Username';
+$lang['password'] = 'Password';
+$lang['home'] = 'Home';
+$lang['admin'] = 'Admin CP';
+$lang['link'] = 'Add Link';
+$lang['website'] = 'Website';
+$lang['url'] = 'Website URL';
+$lang['banner'] = 'Banner URL';
+$lang['width'] = 'Banner Width(max 468)';
+$lang['height'] = 'Banner Height(max 60)';
+$lang['password2'] = 'Password Again';
+$lang['desc'] = 'Website Description';
+$lang['email'] = 'Email Address';
+$lang['admin'] = 'Admin Login';
+$lang['error'] = 'Error';
+$lang['success'] = 'Success';
+$lang['edit_link'] = 'Edit Website';
+$lang['delete'] = 'Delete';
+$lang['navigation'] = 'Navigation';
+$lang['web_name'] = 'Website Name';
+$lang['activated'] = 'Activated';
+$lang['action'] = 'Action';
+$lang['vote_url'] = 'Vote URL';
+
+//Admin links page
+$lang['add_link'] = 'Add Link';
+$lang['edit_link'] = 'Edit Link';
+
+//Admin Admins page
+$lang['add_admin'] = 'Add Admin';
+$lang['edit_admin'] = 'Edit Admin';
+
+//Admin Reset Stats Page
+$lang['reset'] = 'Reset Stats';
+$lang['reset_mess'] = 'To reset the hits in and hits out for the links click the button below. Caution this will reset those stats back to 0.';
+
+// Admin Settings Page
+$lang['board'] = 'Board Settings';
+$lang['site_title'] = 'Site Title';
+$lang['site_email'] = 'Site Email';
+$lang['domain'] = 'Domain';
+$lang['script_path'] = 'Script Path';
+$lang['default_theme'] = 'Default Theme';
+$lang['default_lang'] = 'Default Language';
+$lang['activation'] = 'Activation';
+$lang['mail_func'] = 'Enable php mail function';
+$lang['limit'] = 'Website limit Per page';
+$lang['wel_mess'] = 'Welcome Message';
+
+//Admin themes page
+$lang['theme_name'] = 'Theme Name';
+$lang['add_theme'] = 'Add Theme';
+$lang['edit_theme'] = 'Edit Theme';
+
+//Admin Language page
+$lang['lang_name'] = 'Language Name';
+$lang['add_lang'] = 'Add Language';
+$lang['edit_lang'] = 'Edit Language';
+?>
