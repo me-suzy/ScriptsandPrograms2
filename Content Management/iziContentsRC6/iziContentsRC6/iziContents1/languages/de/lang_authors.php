ac2cc67d2362afce7cf3b3759b7a4aba83992acd
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:05 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_authors.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_authors.php
@@ -0,0 +1,80 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Benutzer';
+
+//  List Headings
+$GLOBALS["tLogin"] = 'Login Name';
+$GLOBALS["tUsername"] = 'Username';
+$GLOBALS["tEMail"] = 'E-Mail';
+$GLOBALS["tUsergroup"] = 'Usergruppe';
+$GLOBALS["tEnabled"] = 'eingeschaltet';
+
+//  List Functions
+$GLOBALS["tAuthorFilter"] = 'Usergruppenfilter';
+$GLOBALS["tAddNewAuthor"] = 'Neuen User hinzuf&uuml;gen';
+$GLOBALS["tEditAuthor"] = 'User &auml;ndern';
+$GLOBALS["tViewAuthor"] = 'User ansehen';
+$GLOBALS["tDeleteAuthor"] = 'User l&ouml;schen';
+$GLOBALS["tReleaseUser"] = 'User zulassen/verbieten';
+$GLOBALS["tConfirmRelease"] = 'Sind Sie sich sicher, dass Sie diesen User zulassen/verbieten wollen?';
+
+//  Form Block Titles
+$GLOBALS["thLogin"] = 'Login Details';
+$GLOBALS["thUser"] = 'User Details';
+$GLOBALS["thConfiguration"] = 'pers&ouml;nliche Einstellungen';
+$GLOBALS["thVertriebspartner"] = 'Vertriebspartner';
+
+//  Form Field Headings
+$GLOBALS["tPassword"] = 'Passwort';
+$GLOBALS["tRetypePassword"] = 'Passwort wiederholen';
+$GLOBALS["tAuthorname"] = 'Name anzeigen';
+$GLOBALS["tEMail"] = 'E-mail Addresse';
+$GLOBALS["tPrivateEMail"] = 'E-mail Addresse verstecken';
+$GLOBALS["tCountry"] = 'Land';
+$GLOBALS["tLanguage"] = 'Sprache';
+$GLOBALS["tPhone"] = 'Telefon';
+$GLOBALS["tFax"] = 'Fax';
+$GLOBALS["tAddress"] = 'Addresse';
+$GLOBALS["tCity"] = 'Stadt';
+$GLOBALS["tState"] = 'Bundesland';
+$GLOBALS["tZip"] = 'PLZ';
+$GLOBALS["tWebsite"] = 'Website <font color=red>(mit http://)</font>';
+$GLOBALS["tComments"] = 'Kommentare';
+$GLOBALS["tNewsletter"] = 'Newsletter';
+$GLOBALS["tSalescode"] = 'Salescode';
+$GLOBALS["tSalessite"] = 'Salesseite';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'In diesem Formular erstellen oder &auml;ndern Sie neue User, die die Inhalte der Webseite anlegen und &auml;ndern k&ouml;nnen.';
+$GLOBALS["hLogin"] = 'Dies ist der Login-Name des Users, der bei der Passwort-Abfrage verwendet wird.';
+$GLOBALS["hPassword"] = 'Geben Sie das Passwort ein, dass dieser User beim Einloggen in den Administrations-Bereich benutzen wird.';
+$GLOBALS["hRetypePassword"] = 'Wiederholen Sie das User-Passwort, um Fehler auszuschliessen.';
+$GLOBALS["hAuthorname"] = 'Dieses Feld ist f&uuml;r den vollen Namen oder den Nicknamen des Users. Dieser wird unter allen Artikeln angezeigt, die von diesem User erstellt wurden, wenn das entsprechende Feld im Artikel-Formular angehakt ist.';
+$GLOBALS["hUsergroup"] = 'The user group that this user belongs to. User group will determine the admin options available to this user.';
+$GLOBALS["hEMail"] = 'Die E-mail Addresse des Users. Zusammen mit dem Usernamen wird daraus der Hyperlink f&uuml;r einen Email-Kontakt erstellt.';
+$GLOBALS["hPrivateEMail"] = 'Markieren Sie dieses K&auml;stchen, wenn Ihre Email-Adresse nicht angezeigt werden soll.';
+$GLOBALS["hCountry"] = 'Das Land, in dem der User lebt. (optional).';
+$GLOBALS["hLanguage"] = 'Muttersprache (optional).<br />Wenn diese eingestellt wurde, wird der User die Men&uuml;optionen, Feldbezeichnungen, Hilfe, etc. in dieser Sprache angezeigt bekommen, im Unterschied zur Standard-Sprache der Seite.';
+$GLOBALS["hPhone"] = 'Telefonnummer des Users';
+$GLOBALS["hFax"] = 'Faxnummer des Users';
+$GLOBALS["hAddress"] = 'Adresse des Users';
+$GLOBALS["hCity"] = 'die Stadt, in der der User lebt';
+$GLOBALS["hState"] = 'Bundesland des Users';
+$GLOBALS["hZip"] = 'PLZ des Users';
+$GLOBALS["hWebsite"] = 'Webseite des Users';
+$GLOBALS["hComments"] = 'Kommentare des Users';
+$GLOBALS["hNewsletter"] = 'User erh&auml;lt den Newsletter.';
+$GLOBALS["hSalescode"] = 'Salescode des Benutzers';
+$GLOBALS["hSalessite"] = 'Salesseite des Benutzers';
+
+//  Error Messages
+$GLOBALS["eLoginExists"] = 'Dieser Login Name ist bereits vorhanden, bitte w&auml;hlen Sie einen anderen.';
+$GLOBALS["eLoginEmpty"] = 'Der Login Name kann nicht leer gelassen werden.';
+$GLOBALS["ePasswordError"] = 'Das wiederholte Passwort ist nicht das gleiche wie im Passwort-Feld.';
+$GLOBALS["eNameEmpty"] = 'Der angezeigte Autoren-Name darf nicht leer gelassen werden.';
+$GLOBALS["eNameExists"] = 'Ein Autor mit diesem angezeigten Namen ist bereits vorhanden.';
+$GLOBALS["eEMailEmpty"] = 'Email-Adresse des Autors darf nicht leer gelassen werden.';
+$GLOBALS["eEmailIncorrect"]   = 'Die Email-Adresse hat ein ung&uuml;ltiges Format.';
+
+?>
