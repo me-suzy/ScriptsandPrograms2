4fad3e6853f04af9798439597f9c8a9491e78407
me-suzy <me.suzana@gmail.com>
2025-08-13 02:30:39 +0300

    Add Unsorted/DEVShopStop/admin/links/links.shopstop.php

---
+++ Unsorted/DEVShopStop/admin/links/links.shopstop.php
@@ -0,0 +1,22 @@
+<?php
+/************************************************************************/
+/*                                                                      */
+/* Copyright (c) 2001-2002 by CrossWalkCentral                          */
+/* http://www.crosswalkcentral.net                                      */
+/*                                                                      */
+/* CrossWalkCentral                                                     */
+/* You Web Hosting Community!                                           */
+/*                                                                      */
+/* Let us customize this script for you.                                */
+/*                                                                      */
+/* Please let us know what you think of this script			*/
+/* at http://www.crosswalkcentral.net/modules.php?name=Forum            */
+/*                                                                      */
+/* 									*/
+/************************************************************************/
+
+if ($radminsuper==1) {
+    adminmenu("admin.php?op=shopstop", "".CWC_Shopping."", "blocks.gif");
+}
+
+?>
