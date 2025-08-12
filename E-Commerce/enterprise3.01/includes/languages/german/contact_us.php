44ee88b550cec058c0f2a2a33db8af8dad4533b3
me-suzy <me.suzana@gmail.com>
2025-08-12 12:00:01 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/contact_us.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/contact_us.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Kontakt');
+define('NAVBAR_TITLE', 'Kontakt');
+define('TEXT_SUCCESS', 'Ihre Anfrage wurde erfolgreich an den Vertrieb gesendet.');
+define('EMAIL_SUBJECT', 'Anfrage von ' . STORE_NAME);
+
+define('ENTRY_NAME', 'Vollst&auml;ndiger Name:');
+define('ENTRY_EMAIL', 'eMail-Adresse:');
+define('ENTRY_ENQUIRY', 'Anfrage:');
+?>
