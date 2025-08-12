b1f0500878cb0b36394fb9c0d45dc8606ad0bcda
me-suzy <me.suzana@gmail.com>
2025-08-12 17:26:38 +0300

    Add Guestbooks/accelgbook/accelgbook/gbook.php

---
+++ Guestbooks/accelgbook/accelgbook/gbook.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<HTML><HEAD><TITLE>Welcome to Our Guestbook</TITLE>
+<META http-equiv=Content-Type content="text/html; CHARSET=UTF-8"><LINK 
+href="style.css" 
+type=text/css rel=stylesheet>
+
+<META content="Microsoft FrontPage 4.0" name=GENERATOR></HEAD>
+<BODY vLink=#333333 aLink=#ffcc33 bgColor=#333333 leftMargin=0 topMargin=0 " 
+marginheight="0" marginwidth="0">
+<TABLE cellSpacing=0 cellPadding=0 width=770 bgColor=#000000 border=0>
+  <TBODY>
+  <TR vAlign=top>
+    <TD vAlign=center width=770>
+      <h1><font color="#EEEEEE">accelGbook</font></h1>
+      <!-- end top nav -->
+    </TD>
+  </TR></TBODY></TABLE>
+<TABLE cellSpacing=0 cellPadding=0 width=770 bgColor=#99cc00 border=0>
+  <TBODY>
+  <TR>
+    <TD>
+      </TD></TR></TBODY></TABLE>
+<TABLE cellSpacing=0 cellPadding=0 width=722 bgColor=#ffffff border=0>
+  <TBODY>
+  <TR>
+    <TD 
+    background="images/bg_grey.gif" 
+    bgColor=#eeeeee colSpan=2 width="828"><IMG height=20 
+      src="images/p.gif" 
+      width=770></TD></TR>
+  <TR>
+    <TD bgColor=#ffcc33 width="25"><IMG height=16 
+      src="images/p.gif" 
+      width=25></TD>
+    <TD vAlign=center bgColor=#ffcc33 width="750">
+      <p align="right"><b><a href="/">Home</a>&nbsp; |&nbsp; <a href="signgbook.php">Sign 
+      Our Guestbook</a>&nbsp;&nbsp;&nbsp;</b></p>
+    </TD>
+  </TR>
+  <TR>
+    <TD width="585" colspan="2"><?php include 'view_accelgbook.php'; ?></TD>
+  </TR>
+  </TBODY></TABLE><!-- begin DHTML layers for global nav menu -->
+<TABLE cellSpacing=0 cellPadding=2 width=770 bgColor=#ffffff border=0>
+  <TBODY>
+  <TR>
+    <TD><IMG height=13 
+      src="images/p.gif" 
+      width=766></TD></TR>
+  <TR>
+    <TD vAlign=center align=middle bgColor=#99cc00>Powered By <a href="http://www.russeldesign.com"> accelGbook</a>  
+</TD></TR></TBODY></TABLE>
+
+</BODY></HTML>  
+
