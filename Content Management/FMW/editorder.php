a1f38f74b585a282512d47e7b924b3c2ffbcde4f
me-suzy <me.suzana@gmail.com>
2025-08-12 03:32:06 +0300

    Add Content Management/FMW/editorder.php

---
+++ Content Management/FMW/editorder.php
@@ -0,0 +1,106 @@
+<?php
+include "header.php";
+session_start();
+if (($_SESSION['perm'] < "5"))  {
+echo "<font color='#$col_text'>"; die ('You are not authorised to access this section');
+}
+$fileId = $_GET['fileId'];
+
+?><font color="#<?php echo $col_text ?>"><?php
+
+$query="SELECT * FROM roles WHERE role_id= '$fileId'";
+$result=mysql_query($query);
+while($row = mysql_fetch_array($result))
+{
+$role_order = $row["role_order"];
+$role_title = $row["role_title"];
+
+
+}
+
+
+
+
+if ($_POST['submit'] == 'submit') {
+
+if(!$_POST['role_order']) {
+	?><font color="#<?php echo "$col_text" ?>"><?php die('You must enter a number');
+	}
+
+
+$role_order = $_POST['role_order'];
+$query="UPDATE roles SET role_order = '$role_order' WHERE role_id = $fileId ";
+@mysql_select_db($db_name) or die( "Unable to select database");
+mysql_query($query);
+?> <meta HTTP-EQUIV="Refresh" CONTENT="0; URL=memberrole.php"> <?php
+
+}
+?>
+
+<br>
+
+<html>
+
+<head>
+
+<title>Edit Order</title>
+</head>
+
+<body>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];echo "?fileId="; echo "$fileId";?>">
+
+
+
+<center>
+<table border="0" width="50%" height="30" cellpadding="0" cellspacing="0">
+<tr bgcolor="#<?php echo "$col_back"; ?>">
+<td width="5" align="right" valign="top"><img src="images/theme/<?php echo "$theme_col" ?>/boxcornertopleft.png" width="5" height="25" alt=""></td>
+<td width="100%" align="right" valign="top"><img src="images/theme/<?php echo "$theme_col" ?>/boxbacktop.png" width="100%" height="25" alt=""></td>
+<td width="5" align="right" valign="top"><img src="images/theme/<?php echo "$theme_col" ?>/boxcornertopright.png" width="5" height="25" alt=""></td>
+</tr>
+
+<tr bgcolor="#<?php echo "$col_table_header"; ?>">
+<td width="5"><img src="images/theme/<?php echo "$theme_col" ?>/boxleft.png" width="5" height="100%" alt=""></td>
+<td width="100%" align="right" valign="top"><img src="images/theme/<?php echo "$theme_col" ?>/boxbackbottom.png" width="100%" height="5" alt=""></td>
+<td width="5" align="right" valign="top"><img src="images/theme/<?php echo "$theme_col" ?>/boxright.png" width="5" height="25" alt=""></td>
+</tr>
+
+<tr bgcolor="#<?php echo "$col_table_header"; ?>">
+<td width="5"><img src="images/theme/<?php echo "$theme_col" ?>/boxleft.png" width="5" height="100%" alt=""></td>
+<td width="100%" bgcolor="#<?php echo "$col_table_header"; ?>" align="center">
+ <?php  echo "<center>"; echo "<font color='#$col_table_header_text'>"; ?><b>Edit Display Order <?php echo $row["role_title"]; ?></b><br><br>
+<td width="5"><img src="images/theme/<?php echo "$theme_col" ?>/boxright.png" width="5" height="100%" alt=""></td>
+
+</tr>
+
+<tr bgcolor="#<?php echo "$col_back"; ?>">
+<td width="5" align="left" valign="top"><img src="images/theme/<?php echo "$theme_col" ?>/boxcornerbottomleft.png" width="5" height="5" alt=""></td>
+<td width="100%" align="right" valign="top"><img src="images/theme/<?php echo "$theme_col" ?>/boxbackbottom.png" width="100%" height="5" alt=""></td>
+<td width="5" align="right" valign="top"><img src="images/theme/<?php echo "$theme_col" ?>/boxcornerbottomright.png" width="5" height="5" alt=""></td>
+</table>
+<div align="center">
+  <center>
+<br><br>
+
+Enter new order value for <?php echo "$role_title" ?>
+<br><br>
+
+<input id="role_order" size="3" name="role_order" value="<?php echo "$role_order" ?>"><br>
+
+
+ 
+
+<?php
+
+
+?>
+  </center>
+
+
+
+<center><br><input type="Submit" name="submit" value="submit">
+
+
+</body>
+
+</html>
