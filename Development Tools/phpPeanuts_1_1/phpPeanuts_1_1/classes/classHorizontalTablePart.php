a26f5a95dac514c743570cd3f93322520bd1cde7
me-suzy <me.suzana@gmail.com>
2025-08-12 10:02:53 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/classHorizontalTablePart.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/classHorizontalTablePart.php
@@ -0,0 +1,23 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+includeClass('PntHorizontalTablePart', 'pnt/web/parts');
+
+/* Part that works like a normal TablePart, except for the colums 
+* and rows to be shown exchanged. In this table the properties 
+* are shown in the rows, while each item has a column.
+*
+* This concrete subclass is here to keep de application developers
+* code (including localization overrides) separated from the framework code.
+* @see http://www.phppeanuts.org/site/index_php/Menu/178
+* Framework code is in the superclass. 
+* This class may be copied to an application folder to
+* make application specific overrides.
+* @see http://www.phppeanuts.org/site/index_php/Pagina/65
+*/
+class HorizontalTablePart extends PntHorizontalTablePart {
+
+	
+}
+?>
