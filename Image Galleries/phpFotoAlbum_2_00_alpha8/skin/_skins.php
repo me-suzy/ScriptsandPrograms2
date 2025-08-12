2d2597a6b6c895a063ebddc0081da056a401f460
me-suzy <me.suzana@gmail.com>
2025-08-12 18:01:50 +0300

    Add Image Galleries/phpFotoAlbum_2_00_alpha8/skin/_skins.php

---
+++ Image Galleries/phpFotoAlbum_2_00_alpha8/skin/_skins.php
@@ -0,0 +1,17 @@
+<?php
+/*
+****************************************
+*   DEFINICE SKINù / SKIN DEFINITION   *
+****************************************
+ $skin[$dir] = $name;
+   "$dir"
+    - Adresáø, kde je skin umístìn
+	- Skin directory
+   "$name"
+    - Zobrazované jméno skinu
+	- Skin name
+*****************************************
+*/
+$skin["default"]="default";
+$skin["lite"]="lite";
+?>
