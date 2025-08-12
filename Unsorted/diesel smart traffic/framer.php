b27a6d0437777065edd6c6bb2aa1ec2829e580f2
me-suzy <me.suzana@gmail.com>
2025-08-13 02:34:45 +0300

    Add Unsorted/diesel smart traffic/framer.php

---
+++ Unsorted/diesel smart traffic/framer.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Traffic Exchange</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<frameset rows="60,*" frameborder="NO" border="0" framespacing="0"> 
+  <frame name="topFrame" scrolling="NO" noresize src="tpl/top_frame.php?cmid=<?php echo $cmid; ?>&url=<?php echo $url; ?>&uid=<?php echo $uid; ?>" >
+  <frame name="mainFrame" src="<?php echo $url; ?>">
+</frameset>
+<noframes><body bgcolor="#FFFFFF" text="#000000">
+</body></noframes>
+</html>
