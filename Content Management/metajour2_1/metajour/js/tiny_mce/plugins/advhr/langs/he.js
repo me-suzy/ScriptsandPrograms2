177609a106c918af386c964d761a2e2387584c5e
me-suzy <me.suzana@gmail.com>
2025-08-12 06:23:48 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/advhr/langs/he.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/advhr/langs/he.js
@@ -0,0 +1,8 @@
+// HE lang variables by Liron Newman, http://eesh.net
+
+tinyMCE.addToLang('',{
+insert_advhr_desc : 'äëðñ/òøåê ÷å àåô÷é',
+insert_advhr_width : 'øåçá',
+insert_advhr_size : 'âåáä',
+insert_advhr_noshade : 'ììà öì'
+});
