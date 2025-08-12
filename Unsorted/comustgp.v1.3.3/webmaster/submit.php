3ea87d79a5a626cef602aae32b055dfa4c2df414
me-suzy <me.suzana@gmail.com>
2025-08-13 01:52:44 +0300

    Add Unsorted/comustgp.v1.3.3/webmaster/submit.php

---
+++ Unsorted/comustgp.v1.3.3/webmaster/submit.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body bgcolor="#FFFFFF" topmargin="0">
+
+        <?php include ($DOCUMENT_ROOT . "/webmaster/submit.inc.php"); ?>
+      
+</body>
+</html>
