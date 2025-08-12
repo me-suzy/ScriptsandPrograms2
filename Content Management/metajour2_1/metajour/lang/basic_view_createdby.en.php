6985ee0284d1043886bd63dc38cc279f28656f14
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:54 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_createdby.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_createdby.en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Owner';
+$LANG['text_1'] = 'Select the owner (creator) of the selected records';
+$LANG['text_2'] = 'Created by: ';
+?>
