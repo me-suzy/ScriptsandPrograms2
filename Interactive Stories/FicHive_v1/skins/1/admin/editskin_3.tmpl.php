5917fca30863d0fbbf71a5f433677b87bcd8f618
me-suzy <me.suzana@gmail.com>
2025-08-12 18:49:18 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/editskin_3.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/editskin_3.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%FILENAME%></td></tr>
+<tr><td class='frame'><textarea name='skcontent' style='height:300'><%SKINFILE%></textarea></td></tr>
+<tr><td class='frame'><input type='submit' value='<%GO%>'></td></tr>
+</table>
