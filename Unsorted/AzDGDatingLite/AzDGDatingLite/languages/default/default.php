f8406d243ea6038c0288e61a2a23156119383f25
me-suzy <me.suzana@gmail.com>
2025-08-13 01:06:53 +0300

    Add Unsorted/AzDGDatingLite/AzDGDatingLite/languages/default/default.php

---
+++ Unsorted/AzDGDatingLite/AzDGDatingLite/languages/default/default.php
@@ -0,0 +1,223 @@
+<?php 
+
+$langcharset = "windows-1251";
+
+$langgender[1] = "Male";
+$langgender[2] = "Female";
+
+$langpurposes[1] = "for romantic";
+$langpurposes[2] = "for long relationship";
+$langpurposes[3] = "for friendship";
+$langpurposes[4] = "for sex services";
+
+
+$lang[1] = "Add User";
+$lang[2] = "Edit User";
+$lang[3] = "Search";
+$lang[4] = "Username all ready in use";
+$lang[5] = "Bad Username";
+$lang[6] = "Bad Password";
+$lang[7] = "Bad E-Mail";
+$lang[8] = "Thank you for signing up<br><br><a href='search.php'>Search for that special someone.</a>";
+$lang[9] = "Username";
+$lang[10] = "Password";
+$lang[11] = "Gender";
+$lang[12] = "E-Mail";
+$lang[13] = "Country";
+$lang[14] = "Category";
+$lang[15] = "City";
+$lang[16] = "Your hobbies";
+$lang[17] = "Write about you and about searched people";
+$lang[18] = "Height";
+$lang[19] = "Weight";
+$lang[20] = "Age";
+$lang[21] = "Photo";
+$lang[22] = "Register";
+$lang[23] = "FeedBack";
+$lang[24] = "Statistic";
+$lang[25] = "Main page";
+$lang[26] = "Your search did not return any matches!";
+$lang[27] = "Search results";
+$lang[28] = "Please select";
+$lang[29] = "Top Woman";
+$lang[30] = "Welcome to our site";
+$lang[31] = "Top Man";
+$lang[32] = "Change Info";
+$lang[33] = "Statistic no available, because no user in database";
+$lang[34] = "Admin error logs";
+$lang[35] = "Updating successfull";
+$lang[36] = "Click here to E-mail";
+$lang[37] = "Anket - ";
+$lang[38] = "Email to ";
+$lang[39] = "Congratulations!!!<br><br><br>Your message has been sent";
+$lang[40] = "Your Name";
+$lang[41] = "Your E-Mail";
+$lang[42] = "Your Message";
+$lang[43] = "Send Message";
+$lang[44] = "Congratulations!!!<br><br><br>Your message has been sent to webmaster";
+$lang[45] = "Your password incorrect";
+$lang[46] = "Username does`nt exists";
+$lang[47] = "Upload your photo";
+$lang[48] = "Your photo";
+$lang[49] = "Photo:";
+$lang[50] = "You are don`t select any file for uploading. <br>Please goback and select some imagefile from your computer";
+$lang[51] = "Only with photos";
+$lang[52] = "Languages";
+$lang[53] = "Sorry, Statistic page available only in payed version.<br><br>AzDGDatingPro v1.01<br><br>Thanks for using this is free script!!!";
+$lang[54] = $lang[9]." length must be more than ".$username_s." symbols<br> and must be smaller than ".$username_l." symbols";
+$lang[55] = $lang[10]." length must be more than ".$password_s." symbols<br> and must be smaller than ".$password_l." symbols";
+$lang[56] = "Please goback and select you gender";
+$lang[57] = "You must be male or female for registering :)";
+$lang[58] = "Please enter correct email - its very important part of your data.<br>Don`t worry - your email not displaying for users";
+$lang[59] = "Please enter you country from list!";
+$lang[60] = "Your email length not must be bigger than ".$email_l;
+$lang[61] = "Please enter you city!";
+$lang[62] = "Your city field length must be smaller than ".$city_l;
+$lang[63] = "Be accurate and are attentive at filling<br>All fields (except photo) are required!!!";
+$lang[64] = "Hobby size must be smaller than ".$hobby_l;
+$lang[65] = "Hobby word size must be smaller than ".$hobby_w;
+$lang[66] = "Description size must be smaller than ".$desc_l;
+$lang[67] = "Description word size must be smaller than ".$desc_w;
+$lang[68] = "We know that it`s some difficultly, but you must select you weight and height!";
+$lang[69] = "File extention must be gif or jpg";
+$lang[70] = "File size must be smaller than ".$MaxSize." kb.";
+$lang[71] = "CONGRATULATIONS!!!!!<br><br>Your info has been added in our database.<br>You email are confidiently,<br>and nobody see it.<br><br>You may view your anket here:<br>";
+$lang[72] = "Your Email using by another user. <br> or you are registered there by another name!<br><br>Please return and write another email";
+$lang[73] = "Age must be number!";
+$lang[74] = "Please select category of dating!";
+
+// Welcome message
+$lang[75] = "Dear friend, we welcome you in our site!<br>Please select choice from menu you needed. <br>If you don`t registered please <a href=add.php?l=".$l.">register here</a><br><br>Please don`t worry, nothing can view your email, and you must writed to other users using your login and password. If you already registered, you can change information, view messages or delete your profile <a href=login.php?l=".$l.">here</a><br><br>In this site you can search by many parameters. You can do it <a href=search.php?l=".$l.">here</a><br><br>The page <a href=email.php?l=".$l.">Feedback</a> is needed for contact with us for some questions and problems<br><br>In <a href=stat.php?l=".$l.">Stat page</a> you can find some interesting information about our users";
+
+$lang[76] = "Done installing... <br>Delete install.php file now please for secure reasons";
+$lang[77] = "Forget password?";
+$lang[78] = "Please fill some fields";
+$lang[79] = "You password has been sucessfull sent your email";
+$lang[80] = "Email does`nt exists";
+$lang[81] = "Please fill field that you sure that is right!";
+$lang[82] = "Or your Email";
+$lang[83] = "Send password";
+$lang[84] = "None";
+$lang[85] = "Yes";
+$lang[86] = "Page";
+$lang[87] = "Results on page";
+$lang[88] = "results";
+$lang[89] = "Popularity";
+$lang[90] = "No ID set";
+$lang[91] = "has been deleted from database!";
+$lang[92] = "Edit your picture";
+$lang[93] = "You have no pictures. Upload NOW!";
+$lang[94] = "Remove Profile";
+$lang[95] = "No important";
+$lang[96] = "Statistic";
+$lang[97] = "Description";
+$lang[98] = "Graphic";
+$lang[99] = "Quantity";
+$lang[100] = "Total members";
+$lang[101] = "and";
+$lang[102] = "Optimize tables";
+$lang[103] = "Repair tables";
+$lang[104] = "Clear hits";
+$lang[105] = "Remove members";
+$lang[106] = "Search/Edit";
+$lang[107] = "Send Mail";
+$lang[108] = "Config";
+$lang[109] = "Quit";
+$lang[110] = "Database has been repaired";
+$lang[111] = "Database has been optimized";
+$lang[112] = "Member with ID";
+$lang[113] = "has been deleted";
+$lang[114] = "Administration Area";
+$lang[115] = "Enter member ID";
+$lang[116] = "Hits has been cleared!!!<br>Statistic begin again!";
+$lang[117] = "has been deleted from database!";
+$lang[118] = "Date";
+$lang[119] = "Edit";
+$lang[120] = "Delete";
+$lang[121] = "Send mails to members";
+$lang[122] = "Your mail has been sent to";
+$lang[123] = "selected users";
+$lang[124] = "Enter subject";
+$lang[125] = "Enter message";
+$lang[126] = "Notify me, when emails has been sended to all members!";
+$lang[127] = "ADMIN AREA<br><br>Please select something from menu";
+$lang[128] = "Incorrect permission!<p><p>Your IP:<br>";
+$lang[129] = "<br>has been writed to log file.";
+$lang[130] = "System";
+$lang[131] = "Path";
+$lang[132] = "Admin Log";
+$lang[133] = "<br>System:";
+$lang[134] = "Admin Log file is empty<br><br>No incorrect logins here!";
+$lang[135] = "<br>Date:";
+$lang[136] = "Admin Log file has been reset";
+$lang[137] = "Clear Admin Log";
+$lang[138] = "with photo";
+$lang[139] = "Search in country (by internal cities)";
+$lang[140] = "Info";
+$lang[141] = "Back to edit menu";
+$lang[142] = "Message from";
+$lang[143] = "From";
+$lang[144] = "Subject";
+$lang[145] = "Message";
+$lang[146] = "Use this form to send reply";
+$lang[147] = "RE:"; // Reply to letter, ex. RE: For love!
+$lang[148] = "Notify to my account when";
+$lang[149] = "read this message";
+$lang[150] = "Back to messages";
+$lang[151] = "Send reply";
+
+$lang[152] = "Your message has been readed";
+$lang[153] = "Your message:";
+$lang[154] = "has been readed from";
+$lang[155] = "in";
+
+$lang[156] = "You must enter your subject";
+$lang[157] = "You must enter your message";
+
+$lang[158] = "messages has been deleted!";
+$lang[159] = "Delete selected messages";
+$lang[160] = "You have no messages";
+
+$lang[161] = "ARE YOU SURE, THAT YOU YOU WANT DELETE YOUR PROFILE FROM DATABASE";
+$lang[162] = "Yes";
+$lang[163] = "Cancel";
+
+$lang[164] = "View messages";
+$lang[165] = "Edit Info";
+$lang[166] = "Some Statistic";
+$lang[167] = "symbols";
+$lang[168] = "Your login";
+$lang[169] = "Your password";
+$lang[170] = "You must be registered user for send message";
+$lang[171] = "Notify to my account when";
+$lang[172] = "read this message";
+$lang[173] = "years old";
+$lang[174] = "Popularity";
+$lang[175] = "AzDGDatingLite Administration";
+$lang[176] = "Administration Area";
+$lang[177] = "Admin Login";
+$lang[178] = "Admin Password";
+$lang[179] = "ERROR - no available language in";
+$lang[180] = "Select language";
+$lang[181] = "Send mail to users with unreaded messages";
+$lang[182] = "This procedure you may did every month";
+$lang[183] = "Please write about your hobby";
+$lang[184] = "Please write anout you and about searched people";
+$lang[185] = "Last 15 registered";
+$lang[186] = "Your photo has been succesfull uploaded";
+$lang[187] = "Processing time";
+$lang[188] = "sec.";
+
+// for v. 1.1.0
+$lang[189] = "From";
+$lang[190] = "to";
+$lang[191] = "Age must be >= ".$age_s." and <= ".$age_b;
+
+
+
+$newm = "Your password";
+$body1 = "Hello!<br><br>Your login:"; // after them writes user login
+$body2 = "<br>Password:"; // after them writes user password
+$body3 = "<br><br>With best regards<br>AzDGDatingLite Team";
+
+?>
