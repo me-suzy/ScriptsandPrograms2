2dee652ce049e9e3f72b161a877e75317a04fe7d
me-suzy <me.suzana@gmail.com>
2025-08-12 22:00:26 +0300

    Add Polls and Voting/phpvoter-0_6/phpvoter-0.6/templates/wrnu/footer.tmpl.php

---
+++ Polls and Voting/phpvoter-0_6/phpvoter-0.6/templates/wrnu/footer.tmpl.php
@@ -0,0 +1,34 @@
+<?php
+
+$template_content = <<<ENDSTRING
+        <!-- End of middle field -->
+		</FONT>
+                <BR><BR>
+             </TD>
+             <TD WIDTH=10>
+                <IMG SRC="/images/trans.gif" BORDER=0 WIDTH="10" HEIGHT="2" ALT="">
+             </TD>
+          </TR>
+                </TABLE>
+        </TD>
+
+
+        <TD VALIGN="top" WIDTH="169" HEIGHT="267">
+        
+	<!-- Beginning of right field -->
+        
+	<FONT SIZE="-1" FACE="helvetica, arial">
+        {$tmplarr['oldquestions']}
+	</FONT>
+        
+	<!-- End of right field -->
+        
+	</TD>
+</TR>
+</TABLE>
+
+</BODY>
+</HTML>
+ENDSTRING;
+
+?>
