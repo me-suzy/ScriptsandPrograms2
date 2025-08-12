6007df73b2f2913a37768c5af01ce0b174787015
me-suzy <me.suzana@gmail.com>
2025-08-12 14:32:56 +0300

    Add File Manipulation/free_download_site_v1.0/Free download site/my_downloads/NucleusCMS Plugins/all about NP files.php

---
+++ File Manipulation/free_download_site_v1.0/Free download site/my_downloads/NucleusCMS Plugins/all about NP files.php
@@ -0,0 +1,39 @@
+<?php
+/*
+    Copyright (C) 2005 Adullam Limited., All Rights Reserved.
+
+    Unless explicitly acquired and licensed from Licensor under the Technical Pursuit License ("TPL") 
+    Version 1.0 or greater, the contents of this file are subject to the Reciprocal Public License 
+    ("RPL") Version 1.1, or subsequent versions as allowed by the RPL, and You may not copy or use 
+    this file in either source code or executable form, except in compliance with the terms and 
+    conditions of the RPL.
+
+    You may obtain a copy of both the TPL and the RPL (the "Licenses") from Technical Pursuit Inc. 
+    at http://www.technicalpursuit.com.
+
+    All software distributed under the Licenses is provided strictly on an "AS IS" basis, WITHOUT 
+    WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND Adullam Limited HEREBY DISCLAIMS ALL 
+    SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+    PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the Licenses for specific 
+    language governing rights and limitations under the Licenses. 
+    
+    Project Name: Download Site System
+    File: all about NP files.php
+    File-Version: 1.0
+    Details: infor on NucleusCMS plugins.
+*/
+?><br /><hr />
+<div style="color:#666;">
+<h2>Special Explination</h2>
+<p>This message is to let you know a bit about some of the downloads you can get for free in this section.</p>
+<p><strong>Interlace</strong><br />
+A wiki and BB code mark up system that allows some interwiki style linking in your NucleusCMS posts</p>
+<p><strong>Plugin Plugin</strong><br />
+A plugin for testing your plugins against another users plugins... simple.</p>
+<p><strong>Snap-in Demo</strong><br />
+Snapins or <strong>extends</strong> are a way for 
+plugin writers to extend their plugin functionality or have others do it for them.  This is a demo.</p>
+<p>You can download NucleusCMS (one of the best blog scripts in the world) 
+from <a href="http://nucleuscms.org/download.php">NucleusCMS.org</a></p>
+</div>
+<hr /><br />
