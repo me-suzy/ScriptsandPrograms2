800d881fa03e80182ab50961b66dfd9c7c01acce
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:04 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_adminmenu.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_adminmenu.php
@@ -0,0 +1,64 @@
+<?php
+
+
+//  Gruppen-Titel und Beschreibungen
+$GLOBALS["tgaccess"] = 'Zugriff';
+$GLOBALS["tgdaccess"] = 'Verwalten Sie die Benutzer, die Zugang zur Administration haben, und andere Zugangskontrollen.';
+$GLOBALS["tgmenus"] = 'Menüs';
+$GLOBALS["tgdmenus"] = 'Verwalten Sie die Men&uuml;-Struktur Ihrer Webseite.';
+$GLOBALS["tgtemplates"] = 'Vorlagen';
+$GLOBALS["tgdtemplates"] = 'Verwalten Sie die Vorlagen und Standard-Formate f&uuml;r Ihre Inhalte.';
+$GLOBALS["tgcontents"] = 'Inhalte';
+$GLOBALS["tgdcontents"] = 'Verwalten Sie die Artikel, Seiten und Bilder.';
+$GLOBALS["tglanguages"] = 'Sprachen';
+$GLOBALS["tgdlanguages"] = '&Uuml;bersetzen Sie die Inhalte Ihrer Webseite in andere Sprachen.';
+$GLOBALS["tgstyles"] = 'Layout';
+$GLOBALS["tgdstyles"] = 'Passen Sie den Look Ihrer Webseiten Ihren Bed&uuml;rfnissen an.';
+$GLOBALS["tgstatistics"] = 'Statistik';
+$GLOBALS["tgdstatistics"] = 'Besucher-Statistiken ansehen';
+$GLOBALS["tgbanners"] = 'Banner';
+$GLOBALS["tgdbanners"] = 'Verwalten Sie Werbe-Banner, die auf Ihren Webseiten gezeigt werden.';
+
+//  Funktionen Titel
+$GLOBALS["tfprivileges"] = 'Verwalten von Zugriffs-Rechten';
+$GLOBALS["tfauthors"] = 'Benutzer verwalten';
+$GLOBALS["tfsetregister"] = 'Registrierung-Einstellungen';
+$GLOBALS["tfbackup"] = 'Datenbank Backup/Restore';
+$GLOBALS["tfsites"] = 'Seiten verwalten';
+$GLOBALS["tffiletypes"] = 'Dateitypen verwalten';
+$GLOBALS["tfuserdata"] = 'Userdaten Frame Layout';
+$GLOBALS["tfmenusettings"] = 'Men&uuml;-Einstellungen';
+$GLOBALS["tftopgroups"] = 'Top-Level Men&uuml;';
+$GLOBALS["tfgroups"] = 'Men&uuml;s';
+$GLOBALS["tfsubgroups"] = 'Untermen&uuml;s';
+$GLOBALS["tftags"] = 'Tag-Verwaltung';
+$GLOBALS["tfsidebars"] = 'Sidebars verwalten';
+$GLOBALS["tfimageformats"] = 'Bildformate';
+$GLOBALS["tfratings"] = 'Inhalte Einstellungen';
+$GLOBALS["tfcontent"] = 'Inhalte';
+$GLOBALS["tfsubcontent"] = 'Module verwalten';
+$GLOBALS["tfimages"] = 'Bilder Übersicht';
+$GLOBALS["tfscripts"] = 'Scripts';
+$GLOBALS["tfdownloads"] = 'Dokumente Übersicht';
+$GLOBALS["tflanguages"] = 'Sprach-Einstellungen';
+$GLOBALS["tftusergroups"] = 'Benutzer-Gruppennamen &uuml;bersetzen';
+$GLOBALS["tfttagcategories"] = 'Tag-Kategorien &uuml;bersetzen';
+$GLOBALS["tfttopgroups"] = 'Top-level Men&uuml; &uuml;bersetzen';
+$GLOBALS["tftgroups"] = 'Men&uuml;s &uuml;bersetzen';
+$GLOBALS["tftsubgroups"] = 'Untermen&uuml;s &uuml;bersetzen';
+$GLOBALS["tftcontent"] = 'Inhalte &uuml;bersetzen';
+$GLOBALS["tfserversettings"] = 'Server Einstellungen';
+$GLOBALS["tfsitesettings"] = 'Seiten-Einstellungen';
+$GLOBALS["tfcolorsform"] = 'Farben/Schriften festlegen';
+$GLOBALS["tfsetgraphics"] = 'Grafiken festlegen';
+$GLOBALS["tfthemes"] = 'Themes verwalten';
+$GLOBALS["tfviewstatistics"] = 'Statistiken ansehen';
+$GLOBALS["tfprunestatistics"] = 'Statistiken zusammenfassen';
+$GLOBALS["tfbanners"] = 'Banner verwalten';
+$GLOBALS["tfexcelexport"] = 'Excel-Export';
+$GLOBALS["tStartseiteAdmin"] = 'Home Admin';
+$GLOBALS["tHilfe"] = 'Hilfe';
+$GLOBALS["tHomepage"] = 'Zur Homepage';
+$GLOBALS["tfeledicss"] = 'CSS-Editor';
+
+?>
