2ecec22d2f6aa383c9c701d217db63dd20fc99cb
me-suzy <me.suzana@gmail.com>
2025-08-12 11:52:35 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/allprods.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/allprods.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'All Products');
+define('HEADING_TITLE', 'All Products');
+
+define('BOX_INFORMATION_ALLPRODS', 'View All of Our Products');
+?>
