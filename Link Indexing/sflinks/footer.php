589b504eb73e99fd120546054e1a92cf41d7ba62
me-suzy <me.suzana@gmail.com>
2025-08-12 19:28:49 +0300

    Add Link Indexing/sflinks/footer.php

---
+++ Link Indexing/sflinks/footer.php
@@ -0,0 +1,6 @@
+<p align="center"><h1><a href="addlink.php">Add a Link</a> || <a href="deletelink.php">Delete a Link</a> || <a href="#" onClick="list=window.open('showids.php','list','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=650,height=500'); return false;">Show Links</a></h1></p>
+
+<p align="center"><font size="1">This script brought to you by the letters <a href="http://spoken-for.org" target="_blank">S and F and O</a>.</font></p>
+
+</body>
+</html>
