b920c12025dba770d85cfac0c28e4ca9b11008c4
me-suzy <me.suzana@gmail.com>
2025-08-13 01:00:45 +0300

    Add Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/upgrade_finish.php

---
+++ Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/upgrade_finish.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<link rel="stylesheet" href="main.css">
+</head>
+<body>
+All the tables and data have been successfully upgraded to AutoLinks 2.0. Please delete the /install/ subdirectory. You may now install AutoLinks on your site(s) by going to the <a href="../index.php">control panel</a> and click on the 'Install New Site' link. We also invite you to navigate through the control panel to see all the new options available on the new version.
+</body>
+</html>
