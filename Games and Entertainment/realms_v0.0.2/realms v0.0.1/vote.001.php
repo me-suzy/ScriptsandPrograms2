98cf96a3d2a66f15b7b850b64e1934f27260d7b5
me-suzy <me.suzana@gmail.com>
2025-08-12 16:33:14 +0300

    Add Games and Entertainment/realms_v0.0.2/realms v0.0.1/vote.001.php

---
+++ Games and Entertainment/realms_v0.0.2/realms v0.0.1/vote.001.php
@@ -0,0 +1,9 @@
+<?php
+
+
+
+
+print"<a href=\"http://apexwebgaming.com/in/103\"><img src=\"http://apexwebgaming.com/images/vote_1.gif\" width=\"468\" height=\"60\" alt=\"Vote at Apex Web Gaming\" border=\"0\"></a>
+<BR>
+<SCRIPT language=\"Javascript\" src=\"http://apexwebgaming.com/current_stats.php?id=103\"></SCRIPT>
+<BR>";
