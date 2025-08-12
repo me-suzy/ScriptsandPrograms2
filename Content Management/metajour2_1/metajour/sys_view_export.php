5401e0557334dfeef9ceec66ae6dc5f01d60be3e
me-suzy <me.suzana@gmail.com>
2025-08-12 05:58:54 +0300

    Add Content Management/metajour2_1/metajour/sys_view_export.php

---
+++ Content Management/metajour2_1/metajour/sys_view_export.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('basic_view.php');
+require_once('basic_field.php');
+
+class sys_view_export extends basic_view {
+
+}
+
+?>
