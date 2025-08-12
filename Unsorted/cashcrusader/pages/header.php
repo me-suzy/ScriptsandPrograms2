7c18779238bf24bf90877cd7380f86da3a0c70a9
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:00 +0300

    Add Unsorted/cashcrusader/pages/header.php

---
+++ Unsorted/cashcrusader/pages/header.php
@@ -0,0 +1,131 @@
+<html>
+<head>
+<title>Your Domain Name Here -  Get Paid to Read your eMail.</title>
+<meta name="keywords" content="Get Paid to Surf the Web, Get Paid to Surf, Earn Free Money, paid surfing, viewbars, mlm, make money with web site, cashbars, get paid to search the web, get paid to read emails, referrals, sponsors, affiliate programs, ads, sponsors, associate programs, partner programs, webmaster, revenue, commissions, affiliates, associates, surf for pay, surf for cash, surf to earn, directories, partners, referrals, program, sell, income, money making, business opportunities, get paid to surf the net, free, easy money, cash, money, extra income, online, earn money at home, freebies, internet, money tree, make money, top, top sites, top money sites, mlm, easy, pay, paid, earn, cash, chat, affiliates, internet, www, web, residual, email, xxxx">
+<meta name="Description" content="Get paid to read emails!">
+<meta name="ROBOTS" content="ALL">
+<meta name="distribution" content="global">
+<style type="text/css">
+<!-- 
+a            { color: #0033CC; text-decoration: none; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11px}
+a:visited    { color: #0033CC; text-decoration: none; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11px}
+a:hover      { color: #FF0000; text-decoration: underline; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 11px}
+body {
+font-family: Verdana, Arial, Helvetica, sans-serif;
+margin-top: 0px;
+margin-right: 0px;
+margin-bottom: 0px;
+margin-left: 0px;
+font-size: 12px;
+color: #990000;
+background-color: #990000;
+}
+-->
+</style>
+</head>
+<body>
+<table width="99%"  border="0" cellpadding="2" cellspacing="2">
+<tr>
+<td><table width="180" height="95"  border="0" cellpadding="2" cellspacing="2">
+<tr>
+<th width="161" align="center"><font color="#CCCCCC" size="5" face="Georgia, Times New Roman, Times, serif">Site
+Name </font></th>
+</tr>
+</table>
+</td>
+<td align="center"><? getad("main");?>
+</td>
+</tr>
+</table>
+<table width="100%"  border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr align="center" valign="bottom" bgcolor="#000033">
+<td colspan="3"><font size="1" face="Verdana, Arial, Helvetica, sans-serif">
+<marquee bgcolor="#000033" border="0" align="bottom" scrollamount="3"  scrolldelay="80" behavior="scroll"  width="100%" height="15" style="color: #FFFFFF; font-size: 11">
+<b>Welcome to our Site!</b>
+</marquee>
+</font></td>
+</tr>
+<tr>
+<td width="0%" align="center" valign="top" bgcolor="#EEEEEE"><table width="120" border="0" cellspacing="0" cellpadding="0">
+<tr bordercolor="#990000">
+<td height="15" align="center" valign="top" bgcolor="#990000"><font color="#EEEEEE" size="1" face="Verdana, Arial, Helvetica, sans-serif"><b>Site
+Menu</b></font></td>
+</tr>
+<tr>
+<td valign="top"><font size="1" face="Verdana, Arial, Helvetica, sans-serif">
+  <!--Begin Site Menu -->
+  <br>
+  <strong> &nbsp; ::</strong> <a href="/pages/index.php" class="toplink">Home</a><br>  
+  <strong> &nbsp; ::</strong> <a href="/pages/confirm.php" class="toplink">Sign Up</a><br>
+  <strong> &nbsp; ::</strong> <a href="/pages/help.php" class="toplink">Questions?</a><br>
+  <strong> &nbsp; ::</strong> <a href="/pages/advertise.php" class="toplink">Advertising</a><br>
+  <strong> &nbsp; ::</strong> <a href="/pages/contact.php" class="toplink">Contact Us</a><br>
+  <strong> &nbsp; ::</strong> <a href="/pages/enter.php" class="toplink">View Stats</a><br>
+  <strong> &nbsp; ::</strong> <a href="/pages/privacy.php" class="toplink">Privacy Policy</a><br>
+  <strong> &nbsp; ::</strong> <a href="/pages/terms.php" class="toplink">Member Terms</a><br>
+  <strong> &nbsp; ::</strong> <a href="/pages/index.php" class="toplink">Link</a></font><font size="1"><br>
+  <font face="Verdana, Arial, Helvetica, sans-serif"><strong>&nbsp; ::</strong> <a href="/pages/index.php" class="toplink">Link</a></font><br>  
+  <br>
+  <!--End  Site Menu -->
+  </font></td>
+</tr>
+<tr bordercolor="#990000">
+<td height="15" align="center" bordercolor="#990000" bgcolor="#990000"><font color="#EEEEEE" size="1" face="Verdana, Arial, Helvetica, sans-serif"><b>Advertise
+Here</b></font></td>
+</tr>
+<tr>
+<td valign="top"><font size="1" face="Verdana, Arial, Helvetica, sans-serif">
+<!--Begin   Left Advertise Here Menu-->
+<br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<strong> &nbsp; ::</strong> <a href="/pages/advertise.php">Advertise here</a><br>
+<br>
+<!--End   Left Advertise Here Menu-->
+</font></td>
+</tr>
+<tr bordercolor="#990000">
+<td bgcolor="#990000" align="center" height="15"><font color="#EEEEEE" size="1" face="Verdana, Arial, Helvetica, sans-serif"><b>Members</b></font></td>
+</tr>
+<tr bordercolor="#990000">
+<td align="center" valign="top"><font size="1" face="Verdana, Arial, Helvetica, sans-serif"><br>
+    <b><font color="#000000">Log-into Account</font></b>
+    </font><font face="Verdana, Arial, Helvetica, sans-serif"><form method=post action=/pages/enter.php>
+      <font size="1">      <br>
+      <strong>UserName</strong><br>
+      <input type=text name=username size="10">
+      <br>
+      <br>
+      Password<br>
+      <input type=password name=password size="10">
+      <br>
+      <input type=submit value="Login" name="submit">
+      </font>
+    </form>
+    <b><font size="1">New users</font></b><font size="1"><br>
+    Get a free account.<a href="/pages/confirm.php"><br>
+    Click here</a>.&nbsp;<br>
+    </font><font face="Verdana, Arial, Helvetica, sans-serif"><font size="1"><br>
+    </font></font><font size="1"><br>
+    </font><font size="1"><br>
+Total Members<br>
+<br>
+<? usercount();?>
+<br>
+<br>
+    </font></font></font></td>
+</tr>
+</table>
+</td>
+<td width="100%" valign="top" bgcolor="#FFFFFF"><table width="100%" border="0" cellpadding="3" cellspacing="3">
+<tr>
+<td valign="top"><font size="1" face="Verdana, Arial, Helvetica, sans-serif"><br>
+    <!--Begin Body -->
+	
+	
