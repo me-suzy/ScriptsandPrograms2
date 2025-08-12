f7405aa95bf2d468224f582e57961e0ef278da33
me-suzy <me.suzana@gmail.com>
2025-08-12 17:26:52 +0300

    Add Guestbooks/accelgbook/accelgbook/login.php

---
+++ Guestbooks/accelgbook/accelgbook/login.php
@@ -0,0 +1,19 @@
+<form method="POST" name="login" id="login">
+  <table width="400" border="0" align="center" cellpadding="3" cellspacing="1">
+    <tr>
+      <td width="100" class="forTableBgLeft">Username:</td>
+      <td class="forTableBgRight">
+      <input name="username" type="text" class="forForm" id="username">      </td>
+    </tr>
+    <tr>
+      <td width="100" class="forTableBgLeft">Password:</td>
+      <td class="forTableBgRight"><input name="password" type="password" class="forForm" id="password"></td>
+    </tr>
+    <tr>
+      <td width="100" class="forTableBgLeft">&nbsp;</td>
+      <td class="forTableBgRight"><input name="Submit" type="submit" class="forButton" value="Login"></td>
+    </tr>
+  </table>
+</form>
+
+
