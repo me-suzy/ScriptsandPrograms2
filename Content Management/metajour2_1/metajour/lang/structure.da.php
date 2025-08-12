30e291b007f1020321ce31b1efd98a041ebc6cf7
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:31 +0300

    Add Content Management/metajour2_1/metajour/lang/structure.da.php

---
+++ Content Management/metajour2_1/metajour/lang/structure.da.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Strukturer';
+$LANG['label_name'] = 'Navn';
+$LANG['label_templateid'] = 'Skabelon';
+$LANG['label_cacheallowed'] = 'Tillad caching';
+$LANG['label_cachelifetime'] = 'Cache levetid (sekunder)';
+?>
