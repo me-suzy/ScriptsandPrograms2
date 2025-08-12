20a6796f52c74265340b0ed4d12af095f256f11d
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:04 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_tags.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_tags.php
@@ -0,0 +1,30 @@
+<?
+
+// Form Title
+$GLOBALS["tFormTitle"]		= 'Gestion des tags';
+
+// List Headings
+$GLOBALS["tCategory"] = 'Category';
+$GLOBALS["tTag"]		= 'Tag';
+$GLOBALS["tTranslation"]	= 'Traduction';
+
+
+// List Functions
+$GLOBALS["tAddNewTag"]		= 'Ajouter nouveau tag';
+$GLOBALS["tEditTag"]		= 'Editer tag';
+$GLOBALS["tDeleteTag"]		= 'Effacer tag';
+
+// Form Detail Comments and Help Texts
+$GLOBALS["tDetails"]		 = 'Ce formulaire vous permet de définir des tags persnnalisés à utiliser dans le corps de votre contenu.';
+$GLOBALS["hTag"]		 = 'Ceci est le code du tag que vous utiliserez dans votre contenu.<br>';
+$GLOBALS["hTag"]		.= 'Dans le contenu, le tag devra être entre des accolades carrées  <b>[tag]</b> pour le distinguer; ';
+$GLOBALS["hTag"]		.= 'ici, simplement identifiez le par son nom, ex: <b>tag</b>.<br>';
+$GLOBALS["hTag"]		.= 'Si vous avez besoin à la fois des deux tags ouvrant et fermant, tels ceux utilisés pour le texte en gras (<b>[b]</b> et <b>[/b]</b>), ';
+$GLOBALS["hTag"]		.= 'vous devrez définir les deux, ex: <b>tag</b> et <b>/tag</b>.';
+$GLOBALS["hTranslation"]	 = 'Ceci est la traduction de votre tag en HTML pur.';
+
+// Error Messages
+$GLOBALS["eNoTag"]		= 'Le Nom du Tag ne peut pas être vide.';
+$GLOBALS["eNoTranslation"]	= 'La Traduction ne peut pas être vide.';
+
+?>
