e857f70f03d1f11f3911a9b81eae9d97b225436f
me-suzy <me.suzana@gmail.com>
2025-08-13 02:19:55 +0300

    Add Unsorted/datingscript/languages/de/dea.php

---
+++ Unsorted/datingscript/languages/de/dea.php
@@ -0,0 +1,123 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.0.3                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 05/02/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               dea.php                          #
+# File purpose            Additional langfile for admin    #
+# File created by Sebastian Karkus <Sebastian@Karkus.de>   #
+############################################################
+
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Falscher loginname oder Passwort - bitte erneut versuchen',//1
+'Administration',//2
+'Admin Login',//3
+'Admin Password',//4
+'Enter',//5
+'Zugang fuer nicht Administratoren gesperrt',//6
+'Hauptseite',//7
+'Treffer zuruecksetzen',//8
+'Reparieren und Optimieren',//9
+'User Management',//10
+'FAQ Management',//11
+'Mailliste',//12
+'Ausgang',//13
+'Du hast den Admin-Bereich verlassen.<br><br>Danke für das Benutzen von diesem Script',//14
+'Insgesamt Nachrichten',//15
+'Gesendete Nachrichten',//16
+'Der folgende Teil',//17
+'{0} Nachrichten wurden zu den Teilnehmern verschickt',//18
+'Du hast ungelesende Nachrichten in {0}',//19 {0} - SiteName (C_SNAME)
+'Gefunden {0} Mitglieder',//20
+'Maillist manager',//21
+'Status:',//22
+'Nachrichtenoptionen und Felder:',//23
+'Nachrichttyp',//24
+'Nachricht von',//25
+'Nachricht-Betreff',//26
+'Nachricht-Inhalt',//27
+'Resultate bestätigen',//28
+'Zurueck',//29
+'Nachrichten senden',//30
+'Unfertige Berichte - {0}',//31
+'Betreff : {0} [Gesended zu {1} Mitgliedern] :::>',//32
+'Beende diesen Bericht',//33
+'Text',//34
+'HTML',//35
+'Vorschau',//36
+'Mitglieder mit ungelesenen Nachrichten - ',//37
+'Insgesamt ungelesene Nachrichten - ',//38
+'Letzte Nachricht id - ',//39
+'Sende Bestätigungsnachricht zu Mitgliedern mit ungelesenen Nachrichten',//40
+'Profil erfolgreich aktualisiert',//41
+'FAQ Frage wurde erfolgreich erstellt',//42
+'Neu erstellen',//43
+'Bitte Frage eingeben',//44
+'Bitte Antwort eingeben',//45
+'FAQ Management',//46
+'Neue FAQ Kategorie erstellen',//47
+'FAQ Kategorie editieren',//48
+'FAQ Kategorie löschen',//49
+'FAQ erstellen',//50
+'FAQ Kategorie erstellen',//51
+'FAQ Frage',//52
+'FAQ Antwort',//53
+'Zuerst musst Du FAQ-Kategorien erstellen',//54
+'Bitte Namen der Kategorie eingeben',//55
+'FAQ Kategorie erstellen',//56
+'Neue Kategorie erstellt:',//57
+'FAQ Kategorie Name',//58
+'FAQ Kategorie editieren',//59
+'Editieren',//60
+'Kategorie wurde veraendert',//61
+'FAQ Kategorie editieren',//62
+'FAQ wurde gelöscht',//63
+'FAQ Kategorie loeschen',//64
+'Achtung : Mit dem Löschen der Kategorie löscht man auch alle Fragen in dieser Kategorie.',//65
+'Loeschen',//66
+'0 - Warte auf den email check<br>1 - Warte den check nach dem Registrieren<br>2 - Warte den check nach dem Update<br>3,8 - Warte die Erlaubnis zum Löschen ab (ohne/mit Anzeige)<br>7 - Mitglied ist aktiv<br>',//67
+'Datenbank wurde repariert und optimiert',//68
+'Mitglied mit der ID#={0} wurde erfolgreich aus der Datenbank entfernt',//69
+'Treffer geloescht',//70
+'Mitglied erlaubt mit der ID={0}',//71
+'IP',//72
+'Verlangt [0 - 255]',//73
+'Status - (ACHTUNG!)',//74
+'Erlaube',//75
+'User management',//76
+'Insgesamt aktive Mitglieder :',//77
+'Mitglieder auf die Aktivierung nach dem Registrieren wartend',//78
+'Mitglieder auf die Aktivierung wartend nach dem Update',//79
+'Mitglieder auf das Löschen wartend',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
