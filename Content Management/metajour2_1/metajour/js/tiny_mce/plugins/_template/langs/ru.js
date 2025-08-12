11e32e56598ae14180264a1cc03ab7476e2bd420
me-suzy <me.suzana@gmail.com>
2025-08-12 06:59:22 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/_template/langs/ru.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/_template/langs/ru.js
@@ -0,0 +1,8 @@
+// RU cp1251 lang variables
+
+/* Remember to namespace the language parameters lang_<your plugin>_<some name> */
+
+tinyMCE.addToLang('',{
+template_title : 'Ýòî øàáëîí äëÿ popup',
+template_desc : 'Ýòî øàáëîí äëÿ êíîïêè'
+});
