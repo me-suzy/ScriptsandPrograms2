36ad4d257e410fba075e3fe0189d8665f9768ca8
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:17 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/main.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/main.obj.php
@@ -0,0 +1,15 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342683;
+$_objchilds[] = '_child_general_342684';
+$_objchilds[] = '_child_extensions_342691';
+function objfile_342683 () {
+$obj = owNew('structure');
+$objdata['name'] = "main";
+$objdata['templateid'] = "0";
+$objdata['cacheallowed'] = "0";
+$objdata['cachelifetime'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
