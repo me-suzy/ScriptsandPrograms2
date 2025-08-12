72a6b04cbd64fab690c01021fc287c4b770658bc
me-suzy <me.suzana@gmail.com>
2025-08-12 12:37:32 +0300

    Add E-Commerce/shop-script-free/cfg/connect.inc.php

---
+++ E-Commerce/shop-script-free/cfg/connect.inc.php
@@ -0,0 +1,29 @@
+<?php
+
+	//database connection settings
+
+
+
+	$not_installed = 1;
+
+	define('DB_HOST', 'localhost'); // database host
+
+	define('DB_USER', ''); // username
+
+	define('DB_PASS', ''); // password
+
+	define('DB_NAME', ''); // database name
+
+	define('ADMIN_LOGIN', ''); //administrator's login
+
+	define('ADMIN_PASS', ''); //administrator's login
+
+
+
+	//database tables
+
+	include("./cfg/tables.inc.php");
+
+
+
+?>
