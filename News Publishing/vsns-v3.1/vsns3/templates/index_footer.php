f5f442e2fcea4d4647d80db5bef51ee267ec731d
me-suzy <me.suzana@gmail.com>
2025-08-12 20:54:01 +0300

    Add News Publishing/vsns-v3.1/vsns3/templates/index_footer.php

---
+++ News Publishing/vsns-v3.1/vsns3/templates/index_footer.php
@@ -0,0 +1,39 @@
+<?php
+/***************************************************************************
+*	Very Simple News System
+*	Version: 3.1.1
+*	Filename: index_footer.php
+*	Description: Default footer file
+****************************************************************************
+*	Build Date: October 14, 2005
+*	Author: Tachyon
+*	Website: http://tachyondecay.net/
+****************************************************************************
+*	Copyright © 2005 by Tachyon
+*
+*	This program is free software; you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation; either version 2 of the License, or
+*   (at your option) any later version.  A copy of the GPL version 2 is
+*	included with this package in the file "COPYING.TXT"
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program; if not, write to the Free Software
+*   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+****************************************************************************/
+?>
+</div>
+
+<div id="footer">
+	<p>Powered by VSNS Lemon <?php echo $version; ?> &copy;2005 by <a href="http://tachyondecay.net/">Tachyon</a>.  All rights reserved.<br />
+<a href="<?php echo $path."admin.php";?>">Admin CP</a></p>
+</div>
+</div>
+
+</body>
+</html>
