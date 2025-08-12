3688187dfa753e0abd159e2276214b1152bc6f6c
me-suzy <me.suzana@gmail.com>
2025-08-12 17:59:08 +0300

    Add Image Galleries/ipg-1/ipg-1/admin/templates/footer.php

---
+++ Image Galleries/ipg-1/ipg-1/admin/templates/footer.php
@@ -0,0 +1,15 @@
+</div>
+</td>
+  </tr>
+</table> 
+<table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#000000" height="71">
+  <tr> 
+    <td align="center" valign="top"><a href="./model_admin/index.php" class="menu"><span class="menu_dividers"><br>
+      </span></a></td>
+  </tr>
+</table>
+<script language="JavaScript" type="text/javascript" src="../includes/js/wz_tooltip.js"></script>
+</body>
+</html>
+
+
