cdfab58b2c072aa89c6da0342448db1d7fc652c1
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:08 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_edit.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_edit.en.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['title'] = 'Edit';
+
+$LANG['img_create'] = 'Create new';
+$LANG['img_list'] = 'List';
+$LANG['img_hierarchy'] = 'Tree list';
+$LANG['img_createvariant'] = 'Create variant';
+$LANG['img_delete'] = 'Delete';
+$LANG['img_previous'] = 'Previous';
+$LANG['img_next'] = 'Next';
+
+$LANG['button_save'] = 'Save';
+?>
