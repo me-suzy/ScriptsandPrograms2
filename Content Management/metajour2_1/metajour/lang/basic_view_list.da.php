9ece1efa57a184ed255e97a5e15abdb6d18d75b1
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:23 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_list.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_list.da.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['title'] = 'Liste';
+
+$LANG['img_search'] = 'Søg';
+$LANG['img_create'] = 'Opret';
+$LANG['img_listall'] = 'Vis alle';
+$LANG['img_hierarchy'] = 'Hierarkisk oversigt';
+
+$LANG['footer_count'] = 'ANTAL:';
+
+$LANG['label_objectid'] = 'Objectid';
+$LANG['label_createdbyname'] = 'Oprettet af';
+$LANG['label_changed'] = 'Ændret';
+$LANG['label_language'] = 'Sprog';
+
+$LANG['tool_field'] = 'Feltet';
+$LANG['tool_like'] = 'Indeholder';
+$LANG['tool_likestart'] = 'Starter med';
+$LANG['tool_likeend'] = 'Slutter med';
+$LANG['tool_standardcol'] = 'Vis standard kolonner';
+$LANG['tool_standardsort'] = 'Standard sortering';
+
+$LANG['alt_element'] = 'Klik for at aktivere, dobbeltklik for at redigere, højreklik for muligheder';
+$LANG['alt_inactive'] = 'Objektet er inaktivt';
+$LANG['alt_default'] = 'Objektet er valgt som standard';
+$LANG['alt_access'] = 'Objektet er beskyttet med rettigheder';
+$LANG['alt_variant'] = 'Objektet har varianter';
+$LANG['alt_islocked'] = 'Objektet er låst mod redigering';
+$LANG['alt_preview'] = 'Klik her for at se element som preview';
+
+?>
