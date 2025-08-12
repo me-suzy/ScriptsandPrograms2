d5615175585539cd3ea302de8215f0beba2cc5e1
me-suzy <me.suzana@gmail.com>
2025-08-13 01:08:42 +0300

    Add Unsorted/b1gmail/admin/support.php

---
+++ Unsorted/b1gmail/admin/support.php
@@ -0,0 +1,66 @@
+<? include("../config.inc.php"); ?>
+<html>
+<head>
+ <title>Adminbereich</title>
+ <SCRIPT language=javascript>
+  <!--
+
+   function LmOver(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+   function LmOut(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+   function LmDown(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+  //-->
+ </SCRIPT>
+ <style> 
+  <!--  
+   a:link     { text-decoration: none      }
+   a:hover    { text-decoration: underline }
+   a:active   { text-decoration: none      }
+   a:visited  { text-decoration: none      }
+
+   a.dl:link     { color: #ffffff; text-decoration: none      }
+   a.dl:hover    { color: #ffffff; text-decoration: underline }
+   a.dl:active   { color: #ffffff; text-decoration: none      }
+   a.dl:visited  { color: #ffffff; text-decoration: none      }
+  -->
+ </style>
+</head>
+
+<body bgcolor="#ffffff">
+<center>
+ <table width="60%" bgcolor="#003366" cellspacing="1">
+  <tr>
+    <td background="table.gif" width="100%"><font face="arial" size="2" color="#ffffff"><b>&nbsp;Support</b></font></td>
+  </tr>
+  <tr>
+    <td bgcolor="#336699" width="100%" align="center"><font face="arial" size="2" color="#ffffff"><br>
+<table cellspacing="0" border="0"><form action="asupport.php" method="post">
+<tr>
+<td align="right" valign="top"><font face="arial" size="2" color="#ffffff">Name:</font></td>
+<td  valign="top"><font face="arial" size="2" color="#ffffff"><input type="text" name="name"></font></td>
+</tr>
+<tr>
+<td align="right" valign="top"><font face="arial" size="2" color="#ffffff">E-Mail:</font></td>
+<td  valign="top"><font face="arial" size="2" color="#ffffff"><input type="text" name="email"></font></td>
+</tr>
+<tr>
+<td align="right" valign="top"><font face="arial" size="2" color="#ffffff">Question/Problem:</font></td>
+<td  valign="top"><font face="arial" size="2" color="#ffffff"><textarea name="text" style="width:320;height:240"></textarea></font></td>
+</tr>
+<tr>
+<td align="right" valign="top"><font face="arial" size="2" color="#ffffff">&nbsp;</font></td>
+<td  valign="top"><font face="arial" size="2" color="#ffffff"><input type="submit" value="Submit"></font></td>
+</tr></form>
+</table>
+<br>
+</font></td>
+  </tr>
+ </table>
+</center>
+</body>
+</html>
