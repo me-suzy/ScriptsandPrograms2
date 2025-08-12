6ff01c0b0fd834ee28b3e1062250230b9560bc7c
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:24 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_category.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_category.en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Add to category';
+$LANG['text_1'] = 'Select the categories that the selected records<br>should be a member of:';
+$LANG['text_2'] = 'Press Ctrl to select more than 1 category';
+?>
