09c60455a042fc30a0f03335f013dcd7bbce9c70
me-suzy <me.suzana@gmail.com>
2025-08-12 03:55:27 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/izi_install/update.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/izi_install/update.php
@@ -0,0 +1,24 @@
+<?php
+
+/***************************************************************************
+ The iziContents Development Team offers no warranties on this script.
+ The owner/licensee of the script is solely responsible for any problems
+ caused by installation of the script or use of the script.
+
+ All copyright notices regarding iziContents and ezContents must remain intact on the scripts and in the HTML for the scripts.
+
+ For more info on iziContents,
+ visit http://www.izicontents.com*/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the License which can be found within the
+ *   zipped package. Under the licence of GPL/GNU.
+ *
+ ***************************************************************************/
+include ("dboperat.php");
+ 
+
+
+?>
