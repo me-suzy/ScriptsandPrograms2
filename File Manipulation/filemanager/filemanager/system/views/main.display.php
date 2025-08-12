5f8fb9ffbd64d045bfffd8f94453e7e91d06f3ea
me-suzy <me.suzana@gmail.com>
2025-08-12 14:29:54 +0300

    Add File Manipulation/filemanager/filemanager/system/views/main.display.php

---
+++ File Manipulation/filemanager/filemanager/system/views/main.display.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "
+<table width=\"100%\" cellspacing=\"0\" cellpadding=\"3\" border=\"0\" bgcolor=\"#E9EBF4\">
+	<tr>
+		<td><b>Willkommen</b></td>
+	</tr>
+</table>
+<br>
+Klicken Sie auf eine Kategorie auf der linken Seite um deren Inhalt anzuzeigen..
+<br><br>
+";
+
+?>
