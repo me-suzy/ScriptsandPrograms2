2a4d9c10a27d19311140dfce80bb57c28a1c56d3
me-suzy <me.suzana@gmail.com>
2025-08-12 18:48:07 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/deletecategory.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/deletecategory.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%DELETECATEGORY%></td><td><select name='cida'><%CATEGORYLIST%></td></tr>
+<tr><td class='fframe'><%CHANGETO%></td><td><select name='cidb'><option value='0'><%TOPLEVEL%><%CATEGORYLIST%></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>' onClick='javascript:return confirm("<%AREYOUSURE%>")'></td></tr>
+</table>
