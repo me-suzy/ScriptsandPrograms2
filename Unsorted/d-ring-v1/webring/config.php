69286f8e5786216b787cf2baa100e41fe1d49061
me-suzy <me.suzana@gmail.com>
2025-08-13 02:18:38 +0300

    Add Unsorted/d-ring-v1/webring/config.php

---
+++ Unsorted/d-ring-v1/webring/config.php
@@ -0,0 +1,16 @@
+<?
+// The Name of your Web Ring.
+$ring_name = "Web Ring";
+
+// The URL to the webring home.
+$url = "http://www.kellishaver.com/webring";
+
+// The admin email address.
+$adminmail = "webring@kellishaver.com";
+
+// The directory where new submissions are stored.
+$submitdir = $DOCUMENT_ROOT . '/webring/incoming';
+
+// The directory where member sites info is stored.
+$memdir = $DOCUMENT_ROOT . '/webring/members';
+?>
