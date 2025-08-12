729d2ebd21cdb666f2784d79fbf43bda0349191a
me-suzy <me.suzana@gmail.com>
2025-08-12 18:01:43 +0300

    Add Image Galleries/phpFotoAlbum_2_00_alpha8/lang/cz.php

---
+++ Image Galleries/phpFotoAlbum_2_00_alpha8/lang/cz.php
@@ -0,0 +1,48 @@
+<?php
+//////////////////////////////////
+// phpFotoAlbum2 language file  //
+//         ---> CZ <---         //
+//////////////////////////////////
+$content_type="<meta http-equiv=\"content-type\" content=\"text/xhtml; charset=windows-1250\" />\n";
+$content_language="<meta http-equiv=\"content-language\" content=\"cs\" />\n";
+$str["date_format"]="d.m.Y H:i:s";
+
+$str["menu_show"]="Zobrazení";
+$str["menu_show_list"]="seznam";
+$str["menu_show_thumb"]="náhledy";
+$str["menu_sort"]="øadit podle";
+$str["menu_sort_name"]="jména";
+$str["menu_sort_type"]="pøípony";
+$str["menu_sort_time"]="èasu";
+$str["menu_slideshow"]="slideshow";
+$str["menu_setup"]="-nastavení-";
+
+$str["menu2_prev"]="pøedelý";
+$str["menu2_next"]="následující";
+$str["menu2_up"]="[ ZPÌT ]";
+$str["menu2_exit"]="zavøít";
+
+$str["dir"]="adresáø";
+$str["list_root"]="[ . ]";
+$str["list_up"]="[ .. ]";
+$str["list_unknown"]="-???-";
+$str["count"]="Poèet adresáøù: %1, poèet souborù: %2 (%3)";
+
+$str["setup"]="Nastavení";
+$str["setup_skin"]="Zvolte si skin phpFotoAlba:";
+$str["setup_lang"]="Zvolte jazyk phpFotoAlba:";
+$str["setup_res"]="Zvolte rozliení pro prohlíení obrázkù: ";
+$str["setup_quality"]="Zvolte kvalitu pro prohlíení obrázkù: ";
+$res["orig"]="pùvodní rozliení";
+$res["640x480"]="malé (640x480)";
+$res["800x600"]="støední (800x600)";
+$res["1024x768"]="velké (1024x768)";
+$res["1280x1024"]="extra (1280x1024)";
+$str["setup_submit"]="uloit";
+
+$str["download_full_res"]="Stáhnout v pùvodním rozliení.";
+
+$str["error_working"]="Tato funkce zatím není implementována....";
+$str["error_dir"]="NEPLATNÝ ADRESÁØ!!!";
+$str["error_listing"]="NEPODPOROVANÝ ZPùSOB ZOBRAZENÍ...";
+?>
