1affb6c2a6de7496b4e5e244db7fd29db5eedd43
me-suzy <me.suzana@gmail.com>
2025-08-12 19:32:54 +0300

    Add Mailing List Managers/web-mailing-list/index.php

---
+++ Mailing List Managers/web-mailing-list/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<title>Public Page</title>
+</head>
+<body>
+<a href="register.php">Sign Up</a><br />
+<a href="unsubscribe.php">Remove Email From List</a>
+</body>
+</html>
