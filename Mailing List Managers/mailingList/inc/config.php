92c3de1725c461d233a70d5b4787988c176ae81d
me-suzy <me.suzana@gmail.com>
2025-08-12 19:32:13 +0300

    Add Mailing List Managers/mailingList/inc/config.php

---
+++ Mailing List Managers/mailingList/inc/config.php
@@ -0,0 +1,8 @@
+<?php
+$host ='localhost';
+$username = '';
+$pass = '';
+$db = '';
+$homePageUrl = ''; /// enter your site name
+$installationUrl = ''; /// full path to installation. Dont add tralling slash at the end
+?>
