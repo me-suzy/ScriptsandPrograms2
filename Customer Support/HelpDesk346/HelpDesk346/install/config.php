5c61c8791f6a4c29b60bc1c7511948ef477d3b4a
me-suzy <me.suzana@gmail.com>
2025-08-12 08:46:08 +0300

    Add Customer Support/HelpDesk346/HelpDesk346/install/config.php

---
+++ Customer Support/HelpDesk346/HelpDesk346/install/config.php
@@ -0,0 +1,22 @@
+<?php
+ 
+				$server =  '';
+
+				$database = '';
+
+				$databaseName = '';
+
+				$databasePassword = '';
+
+				$databasePrefix = '';
+
+				define('DB_PREFIX', '');
+
+				define('DB_HOST', '');
+
+				define('DB_UNAME', '');
+
+				define('DB_DBNAME', '');
+
+				define('DB_PASS', '');
+			?>
