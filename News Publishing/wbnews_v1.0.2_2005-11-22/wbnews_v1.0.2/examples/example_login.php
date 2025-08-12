7c1cbf3950db9e27c784baa08fdd2657b274d119
me-suzy <me.suzana@gmail.com>
2025-08-12 20:57:18 +0300

    Add News Publishing/wbnews_v1.0.2_2005-11-22/wbnews_v1.0.2/examples/example_login.php

---
+++ News Publishing/wbnews_v1.0.2_2005-11-22/wbnews_v1.0.2/examples/example_login.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+
+	Can be used best when using Send to friend to link to this page. 
+
+*/
+define('wbnews',true);
+
+include "../global.php";
+include $config['installdir']."/users.php";
+
+$user = new user($newsConfig);
+if ($user->protectedArea()!=true)
+    $user->logIn();
+else
+    echo "Logged In";
+
+?>
