45444307e7e10c5ac14f1800ead2a50506f1fa68
me-suzy <me.suzana@gmail.com>
2025-08-12 08:53:04 +0300

    Add Database Tools/discoman 1.4/discoman 1.4/meta.php

---
+++ Database Tools/discoman 1.4/discoman 1.4/meta.php
@@ -0,0 +1,14 @@
+<?
+echo"
+<meta http-equiv=\'Content-Type\' content=\'text/html; charset=iso-8859-1\'>
+
+<meta name=\'description\' content=\'A COMPLETER\'>
+
+<meta name=\'Keywords\' content=\'A COMPLETER\'>
+
+<meta http-equiv=\'expires\' content=\'0\'>
+
+<META NAME=\'robots\' CONTENT=\'index, follow\'>
+
+<meta name=\'revisit-after\' content=\'7 days\'>";
+?>
