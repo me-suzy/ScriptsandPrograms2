2b21c7fd6b3ffdb8e5ac9bafd12ed39563e236dc
me-suzy <me.suzana@gmail.com>
2025-08-12 23:15:28 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/licence.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/licence.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric cedric.claire@voila.fr http://www.portailphp.com/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/ 
+
+// Vous n'êtes pas authorisé a modifier les logos ni les liens ci dessous !
+echo("<br /><font face='Arial' size='1'>");
+echo("<a href='http://www.portailphp.com' target='_blank'><img border='0' src='images/logo-pphp.gif' /></a> ");
+echo("<a href='http://www.apache.org' target='_blank'><img border='0' src='images/logo-apache.gif' /></a> ");
+echo("<a href='http://www.php.net' target='_blank'><img border='0' src='images/logo-php.gif' /></a><br />");
+echo("<strong>$App_Me_Titre</strong> a &eacute;t&eacute; construit avec <a href='http://www.portailphp.com' target='_blank'>Portail PHP</a>, distribué librement sous licence <a href='http://www.gnu.org/home.fr.html' target='_blank'>GNU</a>/<a href='http://www.april.org/gnu/gpl_french.html' target='_blank'>GPL</a>.");
+echo("<a href='http://www.portailphp.com' target='_blank'>Portail PHP</a> © 08/2002-11/2005 - <a href='mailto:cedric.claire@voila.fr' target='_blank'>Cedric CLAIRE</a>.");
+echo("</font><br />");
+?>
