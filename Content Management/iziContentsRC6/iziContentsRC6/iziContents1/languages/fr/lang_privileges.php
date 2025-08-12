d27cab8c81575e130c92581b5da84ffeb45554b4
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:42 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_privileges.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_privileges.php
@@ -0,0 +1,37 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Maintenir les Groupes de Privilège';
+
+//  List Headings
+$GLOBALS["tUsergroupname"] = 'Nom du groupe d usager';
+$GLOBALS["tDefaultGroup"] = 'Groupe d usager par default';
+$GLOBALS["tAdminGroup"] = 'Groupe administrateur';
+
+//  List Functions
+$GLOBALS["tViewPrivilege"] = 'Affichage du groupe d usager';
+$GLOBALS["tAddNewPrivilege"] = 'Ajouter un nouveau groupe d usager';
+$GLOBALS["tEditPrivilege"] = 'Editer le groupe d usager';
+$GLOBALS["tDeletePrivilege"] = 'Effacer le groupe d usager';
+$GLOBALS["tMakeDefault"] = 'Faire le groupe implicite pour les nouveaux utilisateurs';
+$GLOBALS["tDefaultSet"] = 'Le groupe implicite de privilège pour les nouveaux utilisateurs';
+$GLOBALS["tMakeAdmin"] = 'Faire le groupe de admininstrator';
+$GLOBALS["tAdminSet"] = 'Le groupe de privilège d administrateur';
+
+//  Form Field Headings
+$GLOBALS["tPrivilegesFor"] = 'Les privilèges pour le code du groupe d usager';
+$GLOBALS["tView"] = 'Affichage';
+$GLOBALS["tEdit"] = 'Editer';
+$GLOBALS["tAdd"] = 'Ajouter';
+$GLOBALS["tDelete"] = 'Effacer';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Cette forme vous permettez d éditer ou créer un nouveau groupe d usager.';
+
+//  Error Messages
+$GLOBALS["eNoUsergroupCode"] = 'Vous devez entrer un code de groupe d usager.';
+$GLOBALS["eNoUsergroupName"] = 'Vous devez entrer un nom du groupe d usager.';
+$GLOBALS["eCodeInUse"] = 'Ce groupe de privilège existe déjà.';
+$GLOBALS["eUsersExist"] = 'Ne peut pas effacer parce que les utilisateurs suivants sont calmes assigné à ce groupe de privilège:';
+
+?>
