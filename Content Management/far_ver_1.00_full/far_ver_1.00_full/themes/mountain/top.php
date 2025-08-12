9d68e11b81c9b15afc63a90772310b6cf4a37ef7
me-suzy <me.suzana@gmail.com>
2025-08-12 03:31:03 +0300

    Add Content Management/far_ver_1.00_full/far_ver_1.00_full/themes/mountain/top.php

---
+++ Content Management/far_ver_1.00_full/far_ver_1.00_full/themes/mountain/top.php
@@ -0,0 +1,17 @@
+<table width="100%"  border="0" cellspacing="1" cellpadding="0">
+      <tr>
+        <td width="250">
+          <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,29,0" width="250" height="70">
+            <param name="movie" value="themes/mountain/images/logo.swf">
+            <param name="quality" value="high">
+			<param name="devicefont" value="true" /><param name="LOOP" value="false">
+            <embed src="themes/mountain/images/logo.swf" width="250" height="70" loop="false" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash"></embed>
+          </object></td>
+        <td>
+<?php 
+body_far("banner");
+?>
+</td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
