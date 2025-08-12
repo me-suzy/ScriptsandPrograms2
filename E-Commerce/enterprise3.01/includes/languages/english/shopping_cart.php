61a3798a14f513726b995c22c631242771266a32
me-suzy <me.suzana@gmail.com>
2025-08-12 11:53:48 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/shopping_cart.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/shopping_cart.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Shopping Cart Contents');
+define('HEADING_TITLE', 'Shopping Cart Contents');
+define('TABLE_HEADING_REMOVE', 'Remove');
+define('TABLE_HEADING_QUANTITY', 'Qty.');
+define('TABLE_HEADING_MODEL', 'Model');
+define('TABLE_HEADING_PRODUCTS', 'Product(s)');
+define('TABLE_HEADING_TOTAL', 'Total');
+define('TEXT_CART_EMPTY', 'Your Shopping Cart is empty!');
+define('SUB_TITLE_SUB_TOTAL', 'Sub-Total:');
+define('SUB_TITLE_TOTAL', 'Total:');
+
+define('OUT_OF_STOCK_CANT_CHECKOUT', 'Products marked with ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' doesn\'t exist in desired quantity in our stock.<br>Please alter the quantity of products marked with (' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . '), Thank you');
+define('OUT_OF_STOCK_CAN_CHECKOUT', 'Products marked with ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' doesn\'t exist in desired quantity in our stock.<br>You can buy them anyway and check the quantity we have in stock for immediate deliver in the checkout process.');
+?>
