1073c0b84b60f74883e03e5431610bfd0a538277
me-suzy <me.suzana@gmail.com>
2025-08-12 20:07:12 +0300

    Add News Publishing/pivot_130_beta2_full/extensions/bbclone_tools/bbclone_lang.php

---
+++ News Publishing/pivot_130_beta2_full/extensions/bbclone_tools/bbclone_lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Language strings for use with the bbclone extensions.
+ */
+
+$lang['bbclone'] = array(
+
+	'total_visits'   => 'Total visits',
+	'unique_visits'  => 'Unique visitors',
+	'this_month'  => 'This month',
+	'this_week'  => 'This week',
+	'today'  => 'Today',
+
+);
+
+?>
