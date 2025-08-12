5b634d64984900702aa21cc99603036556b22129
me-suzy <me.suzana@gmail.com>
2025-08-12 06:15:18 +0300

    Add Content Management/metajour2_1/metajour/extension/shop/standard/standard_shop_finish.obj.php

---
+++ Content Management/metajour2_1/metajour/extension/shop/standard/standard_shop_finish.obj.php
@@ -0,0 +1,43 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 314147;
+function objfile_314147 () {
+$obj = owNew('template');
+$objdata['name'] = "standard_shop_finish";
+$objdata['content'] = "Tak for din bestilling. Din ordre vil blive behandlet snarest muligt.";
+$objdata['tpltype'] = "2";
+$objdata['htmledit'] = "0";
+$objdata['header'] = "";
+$objdata['style'] = ".extcolorcell {
+	PADDING-RIGHT: 4px; PADDING-LEFT: 4px; PADDING-BOTTOM: 4px; PADDING-TOP: 4px; BACKGROUND-COLOR: #dce1e5
+}
+.extlightcell {
+        PADDING-RIGHT: 4px; PADDING-LEFT: 4px; PADDING-BOTTOM: 4px; PADDING-TOP: 4px; BACKGROUND-COLOR: #ececec
+}
+.extdarkcell {
+	PADDING-RIGHT: 4px; PADDING-LEFT: 4px; PADDING-BOTTOM: 4px; PADDING-TOP: 4px; BACKGROUND-COLOR: #c0c8d0
+}
+.extheader {
+	PADDING-RIGHT: 4px; PADDING-LEFT: 4px; FONT-WEIGHT: bold; FONT-SIZE: 85%; BACKGROUND-IMAGE: url(img/extbg.gif); COLOR: #ffffff; WHITE-SPACE: nowrap; HEIGHT: 24px; BACKGROUND-COLOR: #006699
+}
+.exttablebg {
+	BACKGROUND-COLOR: #a9b8c2
+}
+.extmsgbody {
+	LINE-HEIGHT: 140%
+}
+.extbutton {
+background-color: #c0c8d0; font-style: bold; font-size: 80%; font-weight: bold;
+}";
+$objdata['param'] = "";
+$objdata['setting'] = "";
+$objdata['config'] = "text = \"Thank you. Your order is now registered.\"
+
+[DA]
+text = \"Tak for din bestilling. Din ordre vil blive behandlet snarest muligt.\"
+";
+$objdata['doctype'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
