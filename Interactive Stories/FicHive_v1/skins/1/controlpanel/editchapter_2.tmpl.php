6ee5e86e7aed9cab71df1a66c0d5953fffa6561c
me-suzy <me.suzana@gmail.com>
2025-08-12 18:49:57 +0300

    Add Interactive Stories/FicHive_v1/skins/1/controlpanel/editchapter_2.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/controlpanel/editchapter_2.tmpl.php
@@ -0,0 +1,4 @@
+<table border='0' width='100%'>
+<tr><td class='frame'><select name='chid' size='17'><%CHAPTERLIST%></select></td></tr>
+<tr><td class='frame'><input type='submit' value='<%GO%>'></td></tr>
+</table>
