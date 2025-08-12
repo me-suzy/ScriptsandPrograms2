063930d8394645566b2392316e8a707f32b9329f
me-suzy <me.suzana@gmail.com>
2025-08-12 12:37:49 +0300

    Add E-Commerce/shop-script-free/cfg/general.inc.php

---
+++ E-Commerce/shop-script-free/cfg/general.inc.php
@@ -0,0 +1,8 @@
+<?php
+	define('CONF_SHOP_NAME', 'Shop-Script FREE');
+	define('CONF_SHOP_URL', 'www.shop-script.com');
+	define('CONF_GENERAL_EMAIL', 'your@email.here');
+	define('CONF_ORDERS_EMAIL', 'your@email.here');
+	define('CONF_PAYPAL_EMAIL', 'paypal@webasyst.net');
+	define('CONF_PAYPAL_INTEGRATION', 1);
+?>
