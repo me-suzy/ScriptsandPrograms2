2e23f6a3a6aa0d12fe4b46c311c7e66162caf75d
me-suzy <me.suzana@gmail.com>
2025-08-12 18:48:11 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/deletegroup.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/deletegroup.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%DELETEGROUP%></td><td><select name='gida'><%GROUPLIST%></select></td></tr>
+<tr><td class='fframe'><%CHANGETO%></td><td><select name='gidb'><%GROUPLIST%></select></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>' onClick='javascript:return confirm("<%AREYOUSURE%>")'></td></tr>
+</table>
