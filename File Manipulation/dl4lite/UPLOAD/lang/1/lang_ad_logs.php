5afb9f8d68978cf5279f14f5227ed4fe702be74b
me-suzy <me.suzana@gmail.com>
2025-08-12 14:16:13 +0300

    Add File Manipulation/dl4lite/UPLOAD/lang/1/lang_ad_logs.php

---
+++ File Manipulation/dl4lite/UPLOAD/lang/1/lang_ad_logs.php
@@ -0,0 +1,18 @@
+<?php
+/*********************************************************
+* Name: lang_ad_files.php
+* Author: RW::Download 4.0
+* Contact: realworld@blazefans.com
+* Description: Language variables
+* Version: 4.00
+*********************************************************/
+$lang = array(
+
+'log_file' => "File",
+'log_refer' => "Referer",
+'log_time' => "Time",
+'log_ip' => "IP",
+
+)
+
+?>
