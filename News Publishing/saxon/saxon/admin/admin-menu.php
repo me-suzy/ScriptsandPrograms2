416891b7362764d624f08f08cb4b8e895f8b703d
me-suzy <me.suzana@gmail.com>
2025-08-12 20:22:21 +0300

    Add News Publishing/saxon/saxon/admin/admin-menu.php

---
+++ News Publishing/saxon/saxon/admin/admin-menu.php
@@ -0,0 +1,15 @@
+<div id="menu">
+<ul>
+<li><a href="list-users.php">List Users</a></li>
+<li><a href="add-user.php">Add User</a></li>
+<li><a href="delete-user.php">Delete User</a></li>
+<li><a href="change-user-pwd.php">Change User Password</a></li>
+<li><a href="amend-user-status.php">Amend User Status</a></li>
+<li><a href="../add.php">Main Menu</a></li>
+</ul>
+</div>
+
+<!-- start content -->
+
+<div id="content">
+<a id="main-content" href="#main-content"></a>
