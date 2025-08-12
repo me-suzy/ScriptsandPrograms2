7cd47d961013d66b52dee954a122708e850f869a
me-suzy <me.suzana@gmail.com>
2025-08-12 08:35:31 +0300

    Add Counters/text_hit_counter_v1_0/root/counter/config.php

---
+++ Counters/text_hit_counter_v1_0/root/counter/config.php
@@ -0,0 +1,8 @@
+<?php
+// Text hit counter v1.0
+$justify ="center"; //Where the script should appear on your page
+$font_face = "verdana"; //The font face you want to use
+$font_size = "1";    //The font size you want to use
+$text_before_the_hit_number = "There have been"; //The text that appears before the number of hits
+$text_after_the_hit_number = "visitors to this page"; //The text that appears after the number of hits
+?>
