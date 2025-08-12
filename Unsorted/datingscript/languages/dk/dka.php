b0f1bf465e06b36538d1d8cd27fe918b81d9a0de
me-suzy <me.suzana@gmail.com>
2025-08-13 02:20:13 +0300

    Add Unsorted/datingscript/languages/dk/dka.php

---
+++ Unsorted/datingscript/languages/dk/dka.php
@@ -0,0 +1,122 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.0.3                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 11/01/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               dka.php                          #
+# File purpose            Additional langfile for admin    #
+# File created by         Søren Egelund <sde@tdcadsl.dk>   #
+############################################################
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Forkert login eller kodeord - prv igen',//1
+'Administrator omrde',//2
+'Admin login',//3
+'Admin kodeord',//4
+'Enter',//5
+'Kun adgang for administratorer',//6
+'Hovedside',//7
+'Slet hits',//8
+'Reparer&optimer',//9
+'Bruger administration',//10
+'FAQ Administration',//11
+'Mail liste',//12
+'Afslut',//13
+'Du har forladt administrations omrdet<br><br>Tak fordi du benyttede vores script',//14
+'Totale mails',//15
+'Afsendte mails',//16
+'Den flgende del',//17
+'{0} mails er afsendt til brugere',//18
+'Du har ulste mails i {0}',//19 {0} - SiteName (C_SNAME)
+'Fandt {0} medlemmer',//20
+'Mail list administration',//21
+'Med status:',//22
+'Mail Muligheder og felter:',//23
+'Mail type',//24
+'Fra Mail',//25
+'Mail emne',//26
+'Mail indhold',//27
+'Bekrft om resultater',//28
+'Tilbage',//29
+'Send mails',//30
+'De ufuldstendte afsendelser - {0}',//31
+'Emne : {0} [Sendt til {1} brugere] :::>',//32
+'Afslut afsendelse',//33
+'Tekst',//34
+'HTML',//35
+'Preview',//36
+'Brugere med ulste beskeder - ',//37
+'Totalt ulste beskeder - ',//38
+'Sidste besked id - ',//39
+'Send bekrftelser til brugere med ulste beskeder',//40
+'Profil opdateret',//41
+'FAQ Er oprettet',//42
+'Opret ny',//43
+'Skriv sprgsml',//44
+'Skriv svar',//45
+'FAQ Administration',//46
+'Opret ny FAQ Katagori',//47
+'ndring af FAQ Katagori',//48
+'Slet FAQ Katagori',//49
+'Opret FAQ',//50
+'Vlg FAQ Katagori',//51
+'FAQ Sprgsml',//52
+'FAQ svar',//53
+'Frst skal du oprette FAQ katagorier',//54
+'Skriv katagori navn',//55
+'FAQ Katagori oprettelse',//56
+'Ny katagori oprette:',//57
+'FAQ katagori navn',//58
+'FAQ category edit',//59
+'Edit',//60
+'Category has been changed',//61
+'FAQ katagori ndring',//62
+'FAQ er blevet slettet',//63
+'FAQ Katagori slet',//64
+'Note : Pas p. Nr du sletter en katagori, Vil du slette alle sprgsml i denne katagori.',//65
+'Slet',//66
+'0 - Vent email check<br>1 - Vent check efter registrering<br>2 - Vent check efter opdatering<br>3,8 - Vent p tilladelse til at slette(Uden,med vis)<br>7 - Bruger er aktiv<br>',//67
+'Database er repareret og optimeret',//68
+'Bruger med ID#={0} er blevet slettet fra databasen',//69
+'Hits er blevet fjernet',//70
+'Tilladelse til bruger med id={0}',//71
+'IP',//72
+'Krav [0 - 255]',//73
+'Status - (PAS P)',//74
+'Tillad',//75
+'Bruger administration',//76
+'Total aktive brugere :',//77
+'Bruger venter p profil aktivering after registrering',//78
+'Bruger venter p profil aktivering efter opdatering',//79
+'Bruger venter p sletning',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
