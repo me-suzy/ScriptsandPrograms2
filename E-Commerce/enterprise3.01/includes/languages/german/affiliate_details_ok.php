683f6d70c826b58629d9ba0aa7a6682c9ab2c60d
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:14 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/affiliate_details_ok.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/affiliate_details_ok.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Partnerprogramm - Ihre Kontodaten');
+define('HEADING_TITLE', 'Ihre Kontodaten wurden ge&auml;ndert!');
+?>
