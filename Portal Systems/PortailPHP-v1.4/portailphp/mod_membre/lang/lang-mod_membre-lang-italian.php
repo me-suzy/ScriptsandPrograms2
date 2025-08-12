4a58018d6088968e865f6d58910c65f0e9de949d
me-suzy <me.suzana@gmail.com>
2025-08-12 23:18:30 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_membre/lang/lang-mod_membre-lang-italian.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_membre/lang/lang-mod_membre-lang-italian.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE MEMBRES
+$Mod_Membres_Accueil_OFF="Sconnessione";
+$Mod_Membres_News_Add="Il vostro articolo è stato trasferito correttamente";
+$Mod_Membres_Liens_Add="Il vostro link è stato trasferito correttamente";
+$Mod_Membres_News_Edit="Il vostro articolo è stato modificato correttamente";
+$Mod_Membres_Info="Pubblichiate la lima <strong>/include/config.php</strong> per modificare i valori";
+$Mod_Membres_InfoUser="Pubblichiate la tabella <strong>$BD_Tab_user</strong> per modificare i valori";
+$Mod_Membres_Info_Theme="Template";
+$Mod_Membres_Info_Langue="Lingua";
+$Mod_Membres_Info_Titre="Titolo";
+$Mod_Membres_Info_Version="Versione";
+$Mod_Membres_Info_Couleur="Colore del logo";
+$Mod_Membres_Info_URL="URL";
+$Mod_Membres_Info_UploadLimite="Max. Upload";
+$Mod_Membres_Serveur_Info="Le informazioni del server PHP";
+$Mod_Membres_Nom="Nome";
+$Mod_Membres_Pseudo="Username";
+$Mod_Membres_Mail="Mail";
+$Mod_Membres_Date="Registrato nel";
+$Mod_Membres_Droit="Diritti";
+$Mod_Membres_Web="Web";
+$Mod_Membres_Password="Password";
+$Mod_Membres_Err_Field="Vi siete dimenticati di riempire un campo.";
+$Mod_Membres_Err_Login="Username e/o password errato";
+$Mod_Membres_Rub_Accueil="Principale Admin";
+$Mod_Membres_Rub_Info="Informazioni";
+$Mod_Membres_Rub_News_Cat="Aggiunga 1 Categoria per gli articoli";
+$Mod_Membres_Rub_News_Add="Aggiunga 1 articolo";
+$Mod_Membres_Rub_News_Edit="Modificare 1 articolo";
+$Mod_Membres_Rub_News_Del="Rimuovere 1 articolo";
+$Mod_Membres_Rub_News_Del2="Rimuovere";
+$Mod_Membres_Rub_News_Form_Categorie="Nome della Catagoria";
+$Mod_Membres_Rub_News_JS_Cat="Il campo Categoria deve essere riempito";
+$Mod_Membres_Rub_News_Cat_OK="La vostra categoria è stata trasferita correttamente";
+$Mod_Membres_Rub_File_Cat="Aggiunga 1 Categoria per le lime";
+$Mod_Membres_Rub_File_Del="Rimuovere 1 lime";
+$Mod_Membres_Rub_File_Form_Categorie="Nome della Catagoria";
+$Mod_Membres_Rub_File_JS_Cat="Il campo Categoria deve essere riempito";
+$Mod_Membres_Rub_File_Cat_OK="La vostra categoria è stata trasferita correttamente";
+$Mod_Membres_Rub_Liens_Cat="Aggiunga 1 Categoria per gli altrisiti";
+$Mod_Membres_Rub_Liens_Cat_OK="La vostra categoria è stata trasferita correttamente";
+$Mod_Membres_Rub_Faq_Form_Categorie="Nome della Catagoria";
+$Mod_Membres_Rub_Faq_Cat="Aggiunga 1 Categoria per la FAQ";
+$Mod_Membres_Rub_Faq_Add="Aggiunga 1 FAQ";
+$Mod_Membres_Rub_Faq_Edit="Modificare 1 FAQ";
+$Mod_Membres_Rub_Faq_Del="Rimuovere 1 FAQ";
+$Mod_Membres_Rub_Faq_Add_OK="La vostra FAQ è stata trasferita correttamente";
+$Mod_Membres_Rub_Faq_Cat_OK="La vostra categoria è stata trasferita correttamente";
+$Mod_Membres_Rub_Upload_Photo="Aggiunga 1 foto";
+$Mod_Membres_Rub_Del_Photo="Rimuovere 1 foto";
+$Mod_Membres_Rub_Upload_File = "Aggiunga 1 lima";
+$Mod_Membres_Rub_Liens_Edit="Aggiunga 1 link";
+$Mod_Membres_Nom="Nome";
+$Mod_Membres_Regdate="data del registro";
+$Mod_Membres_Droit="Diritti";
+$Mod_Membres_Web="Web";
+$Mod_Membres_OK_Login="Siete collegati";
+$Mod_Membres_OFF_Login="Siete staccati";
+$Mod_Membres_Photos_Form_Photo="Foto (JPG)";
+$Mod_Membres_Photos_Form_Vign="thumbnail (JPG)";
+$Mod_Membres_Photos_Form_Titre="Titolo (50)";
+$Mod_Membres_Photos_Form_Date="Data (aaaa-mm-jj)";
+$Mod_Membres_Photos_Form_Texte="Testo";
+
+?>
