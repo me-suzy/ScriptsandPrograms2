7e3df62cfd612bedb35002a1c90b1f31223f7369
me-suzy <me.suzana@gmail.com>
2025-08-12 19:34:04 +0300

    Add Multimedia/djstatusv1.8.2/admin/install/2.sql

---
+++ Multimedia/djstatusv1.8.2/admin/install/2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `currentdj_settings` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` text NOT NULL,
+  `setting` text NOT NULL,
+  PRIMARY KEY  (`id`)
+) TYPE=MyISAM AUTO_INCREMENT=1 ;
