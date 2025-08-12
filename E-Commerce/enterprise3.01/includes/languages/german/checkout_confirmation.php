fc8b3b4c5c17a9ea4e8ec8eba179ba85649390d8
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:40 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/checkout_confirmation.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/checkout_confirmation.php
@@ -0,0 +1,29 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Kasse');
+define('NAVBAR_TITLE_2', 'Best&auml;tigung');
+
+define('HEADING_TITLE', 'Bestellung aufgeben:');
+
+define('HEADING_DELIVERY_ADDRESS', 'Versandadresse');
+define('HEADING_SHIPPING_METHOD', 'Versandart');
+define('HEADING_PRODUCTS', 'Produkte');
+define('HEADING_TAX', 'MwSt.');
+define('HEADING_TOTAL', 'Summe');
+define('HEADING_BILLING_INFORMATION', 'Rechnungsinformationen');
+define('HEADING_BILLING_ADDRESS', 'Rechnungsadresse');
+define('HEADING_PAYMENT_METHOD', 'Zahlungsweise');
+define('HEADING_PAYMENT_INFORMATION', 'Zahlungsinformationen');
+define('HEADING_ORDER_COMMENTS', 'Anmerkung zu Ihrer Bestellung');
+
+define('TEXT_EDIT', 'Bearbeiten');
+?>
