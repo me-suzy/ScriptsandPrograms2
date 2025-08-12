4755965780ae285de82fbd064a8a6a8c339015d2
me-suzy <me.suzana@gmail.com>
2025-08-12 11:56:04 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_logout.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_logout.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'The Affiliate Program');
+define('HEADING_TITLE', 'The Affiliate Program');
+
+define('TEXT_INFORMATION', 'You were logged out');
+define('TEXT_INFORMATION_ERROR_1', 'You could not be logged out.');
+define('TEXT_INFORMATION_ERROR_2', 'You were not logged in and can therefore not be logged out.');
+?>
