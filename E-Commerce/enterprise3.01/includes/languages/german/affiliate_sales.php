50099c3be4669780b28fdd0495b97921b6f98bd8
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:29 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/affiliate_sales.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/affiliate_sales.php
@@ -0,0 +1,32 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Partnerprogramm');
+define('HEADING_TITLE', 'Partnerprogramm: Verk&auml;ufe');
+
+define('TABLE_HEADING_DATE', 'Datum');
+define('TABLE_HEADING_SALES', 'Provision (exkl.)');
+define('TABLE_HEADING_VALUE', 'Bestellwert (exkl.)');
+define('TABLE_HEADING_PERCENTAGE','% Provision');
+define('TABLE_HEADING_STATUS', 'Status');
+
+define('TEXT_DELETED_ORDER_BY_ADMIN', 'Deleted (Admin)');
+define('TEXT_INFORMATION_SALES_TOTAL', 'Ihre momentane Provision (exkl.) betr&auml;gt:');
+define('TEXT_INFORMATION_SALES_TOTAL2', '<br>Es werden nur versandte Verk&auml;ufe summiert!');
+define('TEXT_NO_SALES', 'Bis jetzt wurden noch keine Verk&auml;ufe &uuml;ber Ihre Webseite get&auml;tigt.');
+define('TEXT_DISPLAY_NUMBER_OF_SALES', 'angezeigte Verk&auml;ufe <b>%d</b> bis <b>%d</b> (von <b>%d</b> insgesamt)');
+
+define('TEXT_AFFILIATE_HEADER', 'Verk&auml;ufe &uuml;ber Ihre Webseite:');
+?>
