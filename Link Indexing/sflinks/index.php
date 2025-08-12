11e0fba31b99c287973a56364d829f6a62ed008b
me-suzy <me.suzana@gmail.com>
2025-08-12 19:28:55 +0300

    Add Link Indexing/sflinks/index.php

---
+++ Link Indexing/sflinks/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>nope.</title>
+</head>
+
+<body>
+<style type="text/css">
+body, html {background-color: #fff; color: #000; scrollbar-base-color: #fff; font-family: verdana, arial, sans-serif; font-size: 9px;</style>
+
+you're not supposed to be here.
+
+</body>
+</html>
