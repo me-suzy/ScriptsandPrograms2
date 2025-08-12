0819f4b036d593e4687450c922a791df2a83df0a
me-suzy <me.suzana@gmail.com>
2025-08-12 07:53:32 +0300

    Add Content Management/php-mysql-simple-cms/admin/index2.php

---
+++ Content Management/php-mysql-simple-cms/admin/index2.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body background="img/int1_back.gif" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<table width="970" border="0" align="center" class="hoofdtabel">
+</table>
+
+</body>
+</html>
