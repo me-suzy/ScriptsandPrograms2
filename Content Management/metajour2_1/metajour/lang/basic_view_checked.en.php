b374cd3fa09e34285b17a87ce6b1f4fa27053152
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:34 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_checked.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_checked.en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Checked';
+$LANG['text_1'] = 'Enter the time and date of last check for the selected records';
+$LANG['text_2'] = 'Checked: ';
+?>
