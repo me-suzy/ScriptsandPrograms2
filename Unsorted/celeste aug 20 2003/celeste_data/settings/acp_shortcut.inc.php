d27cf39d8991796cb37830cb14c31185ad4c9090
me-suzy <me.suzana@gmail.com>
2025-08-13 01:29:55 +0300

    Add Unsorted/celeste aug 20 2003/celeste_data/settings/acp_shortcut.inc.php

---
+++ Unsorted/celeste aug 20 2003/celeste_data/settings/acp_shortcut.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$acpShortcuts=array();
+$acpShortcuts['forum|1|forum::man'] = 1;
+$acpShortcuts['forum|2|forum::update'] = 1;
+$acpShortcuts['forum|4|per::view'] = 1;
+$acpShortcuts['user|1|user::list'] = 1;
+$acpShortcuts['user|2|user::mail'] = 1;
+$acpShortcuts['user|6|pm::view'] = 1;
