157c2a92c16b7131a7286380a79a7fe0a39706a2
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:42 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/initdansk.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/initdansk.obj.php
@@ -0,0 +1,12 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 32;
+function objfile_32 () {
+$obj = owNew('frame');
+$objdata['name'] = "initdansk";
+$objdata['pageid'] = "33";
+$obj->createObject($objdata);
+$obj->setDefault();
+return $obj;
+}
+?>
