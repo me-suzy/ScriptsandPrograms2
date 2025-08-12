b4553ae1866e0e2f93e81eae78615725615e6e1c
me-suzy <me.suzana@gmail.com>
2025-08-13 02:21:06 +0300

    Add Unsorted/datingscript/languages/hr/hra.php

---
+++ Unsorted/datingscript/languages/hr/hra.php
@@ -0,0 +1,122 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.0.5                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 20/03/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               defaulta.php                     #
+# File purpose            Additional langfile for admin    #
+# File created by         AzDG <support@azdg.com>          #
+############################################################
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Krivo korisnicko ime ili lozinka - pokuaj ponovno',//1
+'Administrator',//2
+'Admin Login',//3
+'Admin Password',//4
+'Ulaz',//5
+'Onemogucen ulaz normalnim korisnicima',//6
+'Pocetna stranica',//7
+'Pocisti klikove',//8
+'Popravi i optimiziraj',//9
+'Uredivanje korisnika',//10
+'FAQ administracija',//11
+'Maillist',//12
+'Izlaz',//13
+'Uspjeno ste odlogirani<br><br>Dodite opet',//14
+'Ukupno mailova',//15
+'Poslano mailova',//16
+'Sljedeci dio',//17
+'{0} mailova je poslano korisnicima',//18
+'Imate neprocitane poruke u {0}',//19 {0} - SiteName (C_SNAME)
+'Pronadeno {0} korisnika',//20
+'Maillist administracija',//21
+'Status:',//22
+'Mail opcije:',//23
+'Vrsta maila',//24
+'Od Maila',//25
+'Mail naslov',//26
+'Sadraj maila',//27
+'Potvrdi rezultate',//28
+'Povratak',//29
+'Poalji mailove',//30
+'Nedovrena isporuka - {0}',//31
+'Tema : {0} [Sended to {1} users] :::>',//32
+'Isporuceno',//33
+'Text',//34
+'HTML',//35
+'Pogled',//36
+'Korisnici sa neprocitanim porukama - ',//37
+'Ukupno neprocitanih poruka - ',//38
+'Posljednja poruka ID - ',//39
+'alji mail protvrde korisnicima sa neprocitanim porukama',//40
+'Profajl uspjeko editiran',//41
+'FAQ pitanje uspjeno kreirano',//42
+'Kreiraj novi',//43
+'Unesite pitanje',//44
+'Unesite odgovor',//45
+'FAQ administracija',//46
+'Dodaj novu FAQ kategoriju',//47
+'Editiraj FAQ kategoriju',//48
+'Obrii FAQ kategorju',//49
+'Kreiraj FAQ',//50
+'Odaberi FAQ kategoriju',//51
+'FAQ pitanje',//52
+'FAQ odgovor',//53
+'Prvo morate kreirati FAQ kategoriju',//54
+'Upiite ime kategorije',//55
+'FAQ kreirana kategorija',//56
+'Kreiranje nove kategorije:',//57
+'FAQ ime kategrije',//58
+'FAQ promjena kategorije',//59
+'Promjeni',//60
+'Promjena je spremljena',//61
+'FAQ editiranje kategorija',//62
+'FAQ obrisan',//63
+'FAQ kategorija obrisana',//64
+'Biljeka : Budi oprezan kod brisanja kategorija, obrisati cete sva pitanja i dogovore u kategoriji.',//65
+'Obrii',//66
+'0 - Wait email check<br>1 - Wait check after register<br>2 - Wait check after update<br>3,8 - Wait allow to remove(without,with show)<br>7 - User is active<br>',//67
+'Databaza je popravljen i optimizirana',//68
+'Korisnik sa ID#={0} je izbrisan iz baze',//69
+'Klikovi su pobrisani',//70
+'Uspjeno dodan korisnik sa id={0}',//71
+'IP',//72
+'Potrebno [0 - 255]',//73
+'Status - (BE CAREFULL)',//74
+'Dozvoliti',//75
+'Promjena korisnika',//76
+'Aktivnih korisnika :',//77
+'Korisnici cekaju aktivaciju nakon registracije',//78
+'Korisnici cekaju aktivaciju nakon promjene profila',//79
+'Korisnici cekaju brisanje',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
