c819c096c07212471d9d353d97f92a557218a2e8
me-suzy <me.suzana@gmail.com>
2025-08-12 10:28:43 +0300

    Add Discussion Boards/board2/ukiboard/themes/greenstyle.php

---
+++ Discussion Boards/board2/ukiboard/themes/greenstyle.php
@@ -0,0 +1,20 @@
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+
+function MenuOver(element)
+{
+	if(element.style){
+		element.style.backgroundColor='#996699';
+		element.style.cursor='hand';
+	};
+};
+
+function MenuOut(element)
+{
+	if(element.style) element.style.backgroundColor='#660099';
+};
+
+window.defaultStatus="UKiBoard";
+
+//-->
+</SCRIPT>
