8dc2c3e817678c365140e4c0ce2e41903bac368c
me-suzy <me.suzana@gmail.com>
2025-08-13 02:45:03 +0300

    Add Unsorted/downloadcounter/config.php

---
+++ Unsorted/downloadcounter/config.php
@@ -0,0 +1,23 @@
+<?php
+// MySQL settings:
+$db_pass="pass";
+$db_user="user";
+$db_name="name";
+$db_host="mj-smile.com";
+
+// Main table name:
+$table="download_titles";
+
+// Admin Panel settings
+
+$pass="pass";
+$user="user";
+
+/*
+How do u want to show the counter
+type SSI to use ssi and anything else to use Javascript
+*/
+
+$type="SSI";
+
+?>
