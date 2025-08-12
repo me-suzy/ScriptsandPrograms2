705ae662fb4629da42738741f043cca3e5b9c168
me-suzy <me.suzana@gmail.com>
2025-08-12 22:56:46 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Search/language/lang-slovak.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Search/language/lang-slovak.php
@@ -0,0 +1,78 @@
+<?php
+
+/*************************************************************************/
+/* The Slovak language file for module Search                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/*************************************************************************/
+/* The Slovak language file for module Search                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+
+define("_ALLTOPICS","Vetky témy");
+define("_NOCOMMENTS","Bez komentára");
+define("_UCOMMENT","Komentár");
+define("_REVIEWS","Recenzie");
+define("_SEARCHUSERS","H¾ada v databáze uívate¾ov");
+define("_SEARCHSECTIONS","H¾ada v záujmových oblastiach");
+define("_SEARCHREVIEWS","H¾ada v recenziách");
+define("_SEARCHIN","H¾ada v téme:");
+define("_ARTICLES","Èlánky");
+define("_ALLAUTHORS","Vetkých autorov");
+define("_ALL","Vetko");
+define("_WEEK","týdeò");
+define("_WEEKS","týdne");
+define("_MONTH","mesiac");
+define("_MONTHS","mesiace");
+define("_SEARCHON","H¾adaj v:");
+define("_SSTORIES","Èlánky");
+define("_SSECTIONS","Záujmové oblasti");
+define("_SUSERS","uívatelia");
+define("_NOMATCHES","Nenaiel sa iadny výsledok");
+define("_PREVMATCHES","predchádzajúcich záznamov");
+define("_NEXTMATCHES","ïalie nájdené záznamy");
+define("_INSECTION","Èlánok v oblasti");
+define("_NONAME","Nebolo zadané iadne meno");
+define("_SCOMMENTS","Komentáre");
+define("_SEARCHRESULTS","Výsledky vyh¾adávania");
+define("_CONTRIBUTEDBY","Príspevok poslal");
+define("_UCOMMENTS","Komentáre");
+define("_MATCHTITLE","Porovna nadpisy");
+define("_MATCHTEXT","Porovna texty èlánkov");
+define("_MATCHBOTH","Porovna nadpisy a texty èlánkov");
+define("_SREPLY","Odpoveï");
+define("_SREPLIES","Odpovede");
+define("_ATTACHART","Prídavky k èlánku");
+define("_PAGES","Stránky");
+define("_REVIEWSCORE","Skóre pre túto recenziu");
+define("_PAGE","Strana");
+define("_DOWNLOADS","Downloady");
+define("_WEBLINKS","Web linky");
+define("_FINDMORE",".. viac");
+define("_DIDNOTFIND","Nenali ste, èo ste chceli?");
+define("_ENCYCLOPEDIA","Encyklopédia");
+define("_SEARCHINSTORY","Preh¾adáva vetky komentáre v èlánku:");
+
+?>
