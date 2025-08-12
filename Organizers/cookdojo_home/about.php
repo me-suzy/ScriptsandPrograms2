1ad40a1b86609fc62ce998eaad4e70672a7ba545
me-suzy <me.suzana@gmail.com>
2025-08-12 21:18:18 +0300

    Add Organizers/cookdojo_home/about.php

---
+++ Organizers/cookdojo_home/about.php
@@ -0,0 +1,5 @@
+<?php
+	include ('include/header.php');
+	echo "<br><br><iframe id='datamain' src='about_s.php' width=755 height=195 marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=0 scrolling=no></iframe>";
+	include ('include/footer.php');
+?>
