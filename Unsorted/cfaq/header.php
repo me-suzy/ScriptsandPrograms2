8386a2846ef37c454a936fa6085846ce0c21ef8b
me-suzy <me.suzana@gmail.com>
2025-08-13 01:35:39 +0300

    Add Unsorted/cfaq/header.php

---
+++ Unsorted/cfaq/header.php
@@ -0,0 +1,30 @@
+<?php
+	/*
+		Modify the HTML below to match the look and feel of your site.
+		The file should contain all the HTML code that would appear BEFORE
+		the spot the FAQ appears.  
+		
+		You can use CSS to further control the look of the site, by setting
+		font sizes, families, etc for the DL, DT, LI, UL, DD, blockquote and 
+		P elements.\
+		
+		If you already have a header file for your site, you can include that
+		instead by uncommenting out the line below, changing the "myheader.php"
+		to match the name of your header file, and deleting the HTML code
+		that follows the PHP.
+	*/
+	
+	// 		include ("myheader.php")
+
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+	<title><?php print $faqname ?></title>
+	<meta NAME="REVISIT-AFTER" CONTENT="7 days">
+	<meta HTTP-EQUIV="pragma" CONTENT="no-cache">
+</head>
+
+<body topmargin=5 leftmargin=5 bgcolor="#FFFFFF" text="#000000" link="#0000FF" vlink="#800080" alink="#FF0000">
