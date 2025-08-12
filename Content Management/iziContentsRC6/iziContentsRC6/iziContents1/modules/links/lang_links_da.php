d0474a18214ea231591cc4cc8174a3f9e96cc97b
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:27 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/lang_links_da.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/lang_links_da.php
@@ -0,0 +1,38 @@
+<?php
+
+// Form Titles
+$GLOBALS["tFormTitle"]		= 'Vedligehold Links';
+$GLOBALS["tSubmitFormTitle"]	= 'Indsend et Link';
+
+// List Headings
+$GLOBALS["tPostedBy"]		= 'Indsendt af';
+$GLOBALS["tPublishDate"]	= 'Udsendt dato';
+$GLOBALS["tStatus"]		= 'Status';
+
+// List Functions
+$GLOBALS["tAddNewLink"]	= 'Tilføj link';
+$GLOBALS["tEditLink"]	= 'Ret link';
+$GLOBALS["tDeleteLink"]	= 'Slet link';
+$GLOBALS["tReleaseLink"]	= 'Frigiv link';
+$GLOBALS["tEditExtraSettings"] = "Ret eksta link indstillinger";
+
+// Form Field Headings
+$GLOBALS["tCategory"] 		= 'Kategori';
+$GLOBALS["tWebURL"]			= 'Website URL';
+$GLOBALS["tDescription"]	= 'Beskrivelse';
+$GLOBALS["tSubmitLink"]		= 'Indsend linket';
+
+// Link Submission Texts
+$GLOBALS["tThankYou"]		= 'Tak for det indsendte link.';
+$GLOBALS["tReleaseRequired"]	= 'Bemærk at linket først bliver vist når dette er godkendt/frigivet af redaktøren.';
+$GLOBALS["tClickToReturn"]	= 'Klik her for at returnere til';
+$GLOBALS["tLinksPage"]	= 'link-siden';
+
+// Security Texts
+$GLOBALS["tMustLogin"]		= 'Du skal være';
+$GLOBALS["tLogin"]		= 'logget ind';
+$GLOBALS["tToSubmit"]		= 'for at indsende et link';
+
+//Validation
+$GLOBALS["eWebURLEmpty"]	= "Link feltet kan ikke være tomt";
+?>
