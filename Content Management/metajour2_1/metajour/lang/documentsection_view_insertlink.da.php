598f5fde5e065c4dad1a26fa286a34d82708880c
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:30 +0300

    Add Content Management/metajour2_1/metajour/lang/documentsection_view_insertlink.da.php

---
+++ Content Management/metajour2_1/metajour/lang/documentsection_view_insertlink.da.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['title'] = 'Hyperlink';
+$LANG['text_1'] = 'Type:';
+$LANG['text_2'] = 'Side';
+$LANG['text_3'] = 'URL';
+$LANG['text_4'] = 'Fil';
+$LANG['text_5'] = 'Bogmærke';
+$LANG['text_6'] = 'Side:';
+$LANG['text_7'] = 'VÆLG SIDE';
+$LANG['text_7'] = 'VÆLG FIL';
+
+?>
