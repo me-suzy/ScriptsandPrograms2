95f28b439a3d5639bc2fa34f3e9e42bd3521862a
me-suzy <me.suzana@gmail.com>
2025-08-12 03:36:41 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_backupdel.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_backupdel.php
@@ -0,0 +1,63 @@
+<?php
+
+/***************************************************************************
+
+ m_backupdel.php
+ ----------------
+ copyright : (C) 2005 - The iziContents Development Team
+
+ iziContents version : 1.0
+ fileversion : 1.0.1
+ change date : 23 - 04 - 2005
+ ***************************************************************************/
+
+/***************************************************************************
+ The iziContents Development Team offers no warranties on this script.
+ The owner/licensee of the script is solely responsible for any problems
+ caused by installation of the script or use of the script.
+
+ All copyright notices regarding iziContents and ezContents must remain intact on the scripts and in the HTML for the scripts.
+
+ For more info on iziContents,
+ visit http://www.izicontents.com*/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the License which can be found within the
+ *   zipped package. Under the licence of GPL/GNU.
+ *
+ ***************************************************************************/
+
+$GLOBALS["rootdp"] = '../';
+
+$GLOBALS["form"] = 'backup';
+$validaccess = VerifyAdminLogin();
+
+
+if ($GLOBALS["candelete"] == False)
+{
+   Header("Location: ".BuildLink('adminlogin.php'));
+}
+else
+{
+   DeleteBackup();
+   Header("Location: ".BuildLink('m_backup.php')."&page=".$_GET["page"]."&subdir=".$_GET["subdir"]);
+}
+
+
+function DeleteBackup()
+{
+   global $_GET;
+
+   $savedir = getcwd();
+   chdir($GLOBALS["rootdp"].$GLOBALS["backup_home"].$_GET["subdir"]);
+   $ffile = $_GET["Backup"];
+   if (file_exists("$ffile"))
+   {
+      unlink ("$ffile");
+   };
+   chdir($savedir);
+} // function DeleteBackup()
+
+?>
