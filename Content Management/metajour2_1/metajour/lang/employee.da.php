21b8c90354bf5b77894e7db5b9fbe2f605e0aa35
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:47 +0300

    Add Content Management/metajour2_1/metajour/lang/employee.da.php

---
+++ Content Management/metajour2_1/metajour/lang/employee.da.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Medarbejdere';
+$LANG['label_departmentid'] = 'Afdeling';
+$LANG['label_name'] = 'Navn';
+$LANG['label_jobtitle'] = 'Stilling';
+$LANG['label_binfile1'] = 'Billede';
+$LANG['label_address1'] = 'Adresse 1';
+$LANG['label_address2'] = 'Adresse 2';
+$LANG['label_address3'] = 'Adresse 3';
+$LANG['label_address4'] = 'Adresse 4';
+$LANG['label_postalcode'] = 'Postnr';
+$LANG['label_city'] = 'By';
+$LANG['label_state'] = 'Stat/Region';
+$LANG['label_country'] = 'Land';
+$LANG['label_telephone1'] = 'Telefon 1';
+$LANG['label_telephone2'] = 'Telefon 2';
+$LANG['label_telephone3'] = 'Telefon 3';
+$LANG['label_telefax'] = 'Telefax';
+$LANG['label_website'] = 'Website';
+$LANG['label_email1'] = 'Email 1';
+$LANG['label_email2'] = 'Email 2';
+$LANG['label_email3'] = 'Email 3';
+$LANG['label_timezone'] = 'Tidszone';
+$LANG['label_birthday'] = 'Fødselsdag';
+$LANG['label_specialdate1'] = 'Mærkedag';
+$LANG['label_specialtext1'] = 'Mærkedag tekst';
+$LANG['label_comment'] = 'Kommentar';
+?>
