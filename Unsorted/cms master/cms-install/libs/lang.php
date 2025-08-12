07c2391c82fcd115bb1374ded61704ef01d0edb3
me-suzy <me.suzana@gmail.com>
2025-08-13 01:48:27 +0300

    Add Unsorted/cms master/cms-install/libs/lang.php

---
+++ Unsorted/cms master/cms-install/libs/lang.php
@@ -0,0 +1,18 @@
+<?php
+//=============================================================================
+//
+//     CMS Master :: Content management system
+//     File: /cms-install/lang.php
+//     Version: 1.0
+//     Created date: 28 September 2003
+//     Modification date: 28 September 2003
+//
+//============================================================================
+                                                                                     
+class Lang {
+
+    var $title = "CMS Master :: Content management system";
+
+}
+
+?>
