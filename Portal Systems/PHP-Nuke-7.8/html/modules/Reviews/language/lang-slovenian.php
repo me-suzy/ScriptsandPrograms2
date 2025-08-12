09a83b0a08ff146acb25d784d9c5a9ad0da1869c
me-suzy <me.suzana@gmail.com>
2025-08-12 22:55:43 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Reviews/language/lang-slovenian.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Reviews/language/lang-slovenian.php
@@ -0,0 +1,99 @@
+<?php
+
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+
+define("_PREVIOUS","Prejnja stran");
+define("_NEXT","Naslednja stran");
+define("_YOURNAME","Vae ime");
+define("_SORTASC","Sortiraj od A do ");
+define("_SORTDESC","Sortiraj od  do A");
+define("_CANCEL","Izbrii");
+define("_YES","Da");
+define("_NO","Ne");
+define("_SCORE","Ocena:");
+define("_REPLYMAIN","Komentiraj");
+define("_ALLOWEDHTML","HTML koda je dovoljena:");
+define("_POSTANON","Polji anonimno");
+define("_WRITEREVIEW","Napii recenzijo");
+define("_WRITEREVIEWFOR","Napii recenzijo za");
+define("_ENTERINFO","Prosimo Vas, da vnesete informacije glede na specifikacijo");
+define("_PRODUCTTITLE","Naziv");
+define("_NAMEPRODUCT","Naziv izdelka za katerega piete recenzijo.");
+define("_REVIEW","Recenzija");
+define("_CHECKREVIEW","Vaa recenzija. Prosimo Vas, da ste pozorni na pravopis! Napiite najmanj 100 besed, OK? Prav tako lahko uporabljate HTML, èe seveda znate.");
+define("_FULLNAMEREQ","Vae polno ime. Obvezno.");
+define("_REMAIL","Va E-mail");
+define("_REMAILREQ","Va E-mail naslove. Obvezeno.");
+define("_SELECTSCORE","Vaa ocena izdelka");
+define("_RELATEDLINK","Vezana povezava");
+define("_PRODUCTSITE","Slubena stran izdelka. Pazite da se URL zaène z \"http://\"");
+define("_LINKTITLE","Naslov povezave");
+define("_LINKTITLEREQ","Potrebno, èe imate vezano povezavo, drugaèe ne.");
+define("_RIMAGEFILE","Naziv datoteke slike");
+define("_RIMAGEFILEREQ","Ime slike z naslovnice, nameèene v images/reviews/. Ni obvezno.");
+define("_CHECKINFO","Prosim preverite èe so vneeni podatki 100% toèni in brez gramatiènih napak, s pravilno uporabo velikih/malih èrk. Npr., prosimo, da ne piete cel tekst z uporabo velikih èrk (vpitje) saj bo izbrisan.");
+define("_INVALIDTITLE","Nepravilen naslov... ne more biti prazno");
+define("_INVALIDSCORE","Nepravilna ocena... mora biti med 1 in 10");
+define("_INVALIDTEXT","Nepravilen tekst recenzije... ne more biti prazno");
+define("_INVALIDHITS","Hits mora biti pozitivna vrednost");
+define("_CHECKNAME","Morate vpisati Vae ime in e-mail");
+define("_INVALIDEMAIL","Nepravilen e-mail (npr: vi@nekaj.com)");
+define("_INVALIDLINK","Morate vpisati oboje, naslov povezave in vezano povezavo, ali pa pustiti oboje prazno");
+define("_ADDED","Dodano:");
+define("_REVIEWER","Recenzijo je napisal:");
+define("_REVIEWID","ID recenzije");
+define("_HITS","Prebrano");
+define("_LOOKSRIGHT","Ali izgleda pravilno?");
+define("_RMODIFIED","spremenjeno");
+define("_RADDED","dodano");
+define("_ADMINLOGGED","Trenutno ste vpisani kot administrator... recenzija bo takoj");
+define("_RTHANKS","Hvala za Vao poslano recenzijo");
+define("_MODIFICATION","sprememba");
+define("_ISAVAILABLE","Sedaj je dostopen v bazi recenzij.");
+define("_EDITORWILLLOOK","Vao recenziju bo pregledal administrator. Najverjetneje bo kmalu dostopna!");
+define("_RBACK","Nazaj na kazalo recenzij");
+define("_RWELCOME","Dobrodoli na oddelku recenzij");
+define("_10MOSTPOP","10 najpopularnejih recenzij");
+define("_10MOSTREC","10 najnovejih recenzij");
+define("_THEREARE","Trenutno se nahaja");
+define("_REVIEWSINDB"," recenzij v nai bazi podatkov");
+define("_REVIEWS","Recenzije");
+define("_REVIEWSLETTER","Recenzije na");
+define("_NOREVIEWS","Nobene recenzije ni na èrko");
+define("_TOTALREVIEWS","Skupno je bilo najedenih recenzij.");
+define("_RETURN2MAIN","Nazaj na glavno kazalo");
+define("_REVIEWCOMMENT","Komentarji na recenzijo:");
+define("_YOURNICK","Va nadimek:");
+define("_RCREATEACCOUNT","<a href=modules.php?name=Your_Account>Izdelajte</a> raèun");
+define("_YOURCOMMENT","Va komentar:");
+define("_MYSCORE","Moja ocena:");
+define("_REVIEWMOD","Sprememba recenzije");
+define("_RDATE","Datum:");
+define("_RTITLE","Naslov:");
+define("_RTEXT","Tekst:");
+define("_REVEMAIL","E-mail:");
+define("_RLINK","Povezava:");
+define("_RLINKTITLE","Naslov povezave:");
+define("_COVERIMAGE","Slika naslovnice:");
+define("_PREMODS","Pregled spremembe");
+define("_PAGE","Strani");
+define("_PAGEBREAK","Èe elite èlanek na veè straneh napiite <b>&lt;!--pagebreak--&gt;</b> tam kjer elite prelom strani.");
+define("_PREVIEW","Pregled");
+define("_LANGUAGE","Jezik");
+
+?>
