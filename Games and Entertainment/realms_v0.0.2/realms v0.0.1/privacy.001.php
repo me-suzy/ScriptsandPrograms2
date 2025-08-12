8a00f5f74c54de9f24840a52880392330f8b8635
me-suzy <me.suzana@gmail.com>
2025-08-12 16:32:30 +0300

    Add Games and Entertainment/realms_v0.0.2/realms v0.0.1/privacy.001.php

---
+++ Games and Entertainment/realms_v0.0.2/realms v0.0.1/privacy.001.php
@@ -0,0 +1,20 @@
+<P><h1>Privacy Policy Statement</h1><BR>
+
+<P>
+For each visitor to our Web page, our Web server automatically recognizes the consumer's domain name and e-mail address (where possible).</P>
+
+<P>We collect the domain name and e-mail address (where possible) of visitors to our Web page, the e-mail addresses of those who post messages to our bulletin board, the e-mail addresses of those who communicate with us via e-mail, the e-mail addresses of those who make postings to our chat areas, aggregate information on what pages consumers access or visit, user-specific information on what pages consumers access or visit, information volunteered by the consumer, such as survey information and/or site registrations.</P>
+
+<P>The information we collect is used to customize the content and/or layout of our page for each individual visitor, used to notify consumers about updates to our Web site, not shared with other organizations for commercial purposes.</P>
+
+<P>With respect to cookies: We use cookies to store visitors preferences, customize Web page content based on visitors' browser type or other information that the visitor sends.</P>
+
+<P>If you do not want to receive e-mail from us in the future, please let us know by sending us e-mail at the above address, calling us at the above telephone number, writing to us at the above address or Tell us ingame.</P>
+
+<P>With respect to Ad Servers: To try and bring you offers that are of interest to you, we have relationships with other companies that we allow to place ads on our Web pages. As a result of your visit to our site, ad server companies may collect information such as your domain type, your IP address and clickstream information. For further information, consult the privacy policies of:<BR>
+http://www.google.com/privacy.html</P>
+
+<P>Upon request we provide site visitors with access to all information [including proprietary information] that we maintain about them, unique identifier information (e.g., customer number or password) that we maintain about them, communications that the consumer/visitor has directed to our site (e.g., e-mails, customer inquiries), contact information (e.g., name, address, phone number) that we maintain about them , a description of information that we maintain about them.<BR>
+Consumers can access this information by e-mail us at the above address, write to us at the above address, writing to us at the above address or asking ingame. Consumers can have this information corrected by sending us e-mail at the above address, calling us at the above telephone number, writing to us at the above address or asking ingame. </P>
+
+<P>If you feel that this site is not following its stated information policy, you may contact us at the above addresses or phone number.</P>
