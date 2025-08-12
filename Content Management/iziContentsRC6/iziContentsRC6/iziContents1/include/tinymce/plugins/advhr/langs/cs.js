d469fc31f0ecdfbcde9837e7be8146e585f6e18d
me-suzy <me.suzana@gmail.com>
2025-08-12 03:45:33 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/include/tinymce/plugins/advhr/langs/cs.js

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/include/tinymce/plugins/advhr/langs/cs.js
@@ -0,0 +1,6 @@
+// UK lang variables
+
+tinyMCELang['lang_insert_advhr_desc']    = 'Vloit/editovat vodorovný oddìlovaè'
+tinyMCELang['lang_insert_advhr_width']   = 'íøka';
+tinyMCELang['lang_insert_advhr_size']    = 'Výka';
+tinyMCELang['lang_insert_advhr_noshade'] = 'Nestínovat';
