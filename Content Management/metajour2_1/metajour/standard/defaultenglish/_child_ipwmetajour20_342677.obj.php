ad805328a7c271cca7d7de77311c50b5514d920b
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:44 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_ipwmetajour20_342677.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_ipwmetajour20_342677.obj.php
@@ -0,0 +1,33 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342677;
+function objfile_342677 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "IPW METAjour 2.1";
+$objdata['subname'] = "";
+$objdata['extension'] = "";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "<P>IPW Systems, Inc. is proud to announce the final release of IPW METAjour version 2.1.</P>
+<P>Some of the key features of IPW METAjour v2.1 include:</P>
+<UL>
+<LI>Windows-like user interface </LI>
+<LI>100% server based web application</LI>
+<LI>Based on PHP and MySQL</LI>
+<LI>New document editor based on TinyMCE, with no need for ActiveX</LI>
+<LI>Advanced template scripting with Smarty based template engine</LI>
+<LI>User and usergroup architecture for handling access restrictions for users and editors</LI>
+<LI>Profile management for detailed control of available operations</LI>
+<LI>Easy handling of large multi-lingual websites</LI>
+<LI>Event handling for automated operations invoked by predefined actions </LI>
+<LI>Report generator for rendering selected records in external document formats </LI>
+<LI>Easy installation</LI>
+<LI>Predefined english and danish websites demo-websites</LI>
+<LI>Numerous out-of-the-box website extensions included, such as webshop, bulletin board, site search, sitemap, user registration, login etc. </LI>
+<LI>Based 100% on our IPW METAZO v2.1 framework, a Model-View-Controller architecture for easy maintenance and extensibility of our object oriented applications</LI></UL>";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
