173519354a24e6f9ff01dac6b684e1c8b68a0869
me-suzy <me.suzana@gmail.com>
2025-08-12 08:40:22 +0300

    Add Customer Support/current_craftysyntax/craftysyntax2.12.7/prefs.php

---
+++ Customer Support/current_craftysyntax/craftysyntax2.12.7/prefs.php
@@ -0,0 +1,55 @@
+<?php
+//===========================================================================
+//* --    ~~                Crafty Syntax Live Help                ~~    -- *
+//===========================================================================
+//           URL:   http://www.craftysyntax.com/    EMAIL: ericg@craftysyntax.com
+//         Copyright (C) 2003-2005 Eric Gerdes   (http://www.craftysyntax.com )
+// --------------------------------------------------------------------------
+// $              CVS will be released with version 3.1.0                   $
+// $    Please check http://www.craftysyntax.com/ or REGISTER your program for updates  $
+// --------------------------------------------------------------------------
+// NOTICE: Do NOT remove the copyright and/or license information any files. 
+//         doing so will automatically terminate your rights to use program.
+//         If you change the program you MUST clause your changes and note
+//         that the original program is Crafty Syntax Live help or you will 
+//         also be terminating your rights to use program and any segment 
+//         of it.        
+// --------------------------------------------------------------------------
+// LICENSE:
+//     This program is free software; you can redistribute it and/or
+//     modify it under the terms of the GNU General Public License
+//     as published by the Free Software Foundation; 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program in a file named LICENSE.txt .
+// --------------------------------------------------------------------------  
+// BIG NOTE:
+//     At the time of the release of this version of CSLH, Version 3.1.0 
+//     which is a more modular, extendable , skinable version of CSLH
+//     was being developed.. please visit http://www.craftysyntax.com to see if it was released! 
+//===========================================================================
+require_once("admin_common.php");
+validate_session($identity);
+
+// get the info of this user.. 
+$query = "SELECT * FROM livehelp_users WHERE sessionid='".$identity['SESSIONID']."'";	
+$people = $mydatabase->query($query);
+$people = $people->fetchRow(DB_FETCHMODE_ASSOC);
+$myid = $people['user_id'];
+$channel = $people['onchannel'];
+$isadminsetting = $people['isadmin'];
+
+$lastaction = date("Ymdhis");
+$startdate =  date("Ymd");
+
+$mydatabase->close_connect();
+?>
+<title>Live help admin</title>
+<frameset rows="40,*" border="0" frameborder="0" framespacing="0" spacing="0">
+<frame src="admin_options.php?tab=prefs" name="topofit" scrolling="no" border="0" marginheight="0" marginwidth="0" NORESIZE=NORESIZE>
+<frame src="prefer.php" name="users" scrolling="AUTO" border="0" marginheight="0" marginwidth="0" NORESIZE=NORESIZE>
+</frameset>
