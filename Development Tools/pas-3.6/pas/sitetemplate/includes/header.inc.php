504a5580a0c47582e23b961625217e8dd064c054
me-suzy <me.suzana@gmail.com>
2025-08-12 10:00:09 +0300

    Add Development Tools/pas-3.6/pas/sitetemplate/includes/header.inc.php

---
+++ Development Tools/pas-3.6/pas/sitetemplate/includes/header.inc.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title><?php echo $pageTitle;?></title>
+    <link rel="stylesheet" type="text/css" href="includes/style.css">
+    <meta name="author" content="<?php echo $Author; ?>">
+    <meta name="keywords" content="<?php echo $Keywords; ?>">
+    <meta name="description" content="<?php echo $Description; ?>">
+  </head>
+  <body bgcolor="<?php echo $background_color;?>" background="<?php echo $background_image?>">
+
