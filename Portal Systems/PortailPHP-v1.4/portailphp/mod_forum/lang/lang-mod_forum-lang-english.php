c182086dafcc32f5bbe60d5bef69489364f80d68
me-suzy <me.suzana@gmail.com>
2025-08-12 23:17:15 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_forum/lang/lang-mod_forum-lang-english.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_forum/lang/lang-mod_forum-lang-english.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE FORUM
+$Mod_Forum_Ajouter="Add a message";
+$Mod_Forum_Search="Seek";
+$Mod_Forum_Auteurs="Authors";
+$Mod_Forum_Post="Posted the";
+$Mod_Forum_Rep = "Anwser" ;
+$Mod_Forum_ResultatRecherche = "Search result" ;
+$Mod_Forum_valider= "Ok" ;
+$Mod_Forum_DeleteOK = "The message was deleted." ;
+$Mod_Forum_DeleteNOK = "Unable to delete the message. Associate comment wasn't deleted." ;
+$Mod_Forum_Query = "Are you sure you want delete this message ?" ;
+$Mod_Forum_Delete_com_OK = "Answer of message was deleted." ;
+$Mod_Forum_Delete_com_NOK = "Unabled to delete answer of message who have reponse_a_id=" ;
+?>
