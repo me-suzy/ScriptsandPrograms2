ea258ba21b6a91a2941fba3202851c3605096630
me-suzy <me.suzana@gmail.com>
2025-08-12 06:00:46 +0300

    Add Content Management/metajour2_1/metajour/app/erek/erek_comp_model_create.php

---
+++ Content Management/metajour2_1/metajour/app/erek/erek_comp_model_create.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eReklamation
+ * @subpackage model
+ * $Id: erek_comp_model_create.php,v 1.1 2005/02/15 12:21:44 jan Exp $
+ */
+
+global $system_path;
+require_once($system_path.'basic_model_createobject.php');
+
+class erek_comp_model_create extends basic_model_createobject {
+
+}
+
+?>
