5a4c035dbfca861ad5b4218e2e220760752c8f7b
me-suzy <me.suzana@gmail.com>
2025-08-12 07:36:46 +0300

    Add Content Management/netious-cms-serv-0.3/cms/manageFile.php

---
+++ Content Management/netious-cms-serv-0.3/cms/manageFile.php
@@ -0,0 +1,162 @@
+<?
+require("../db.php");
+require("include.php");
+DBinfo();
+
+mysql_connect("$DBHost","$DBUser","$DBPass");
+mysql_select_db("$DBName");
+
+
+
+$SUID=f_ip2dec($REMOTE_ADDR);
+if (!session_id($SUID))
+session_start();
+
+$username=$_SESSION['uname'];
+$password=$_SESSION['pass'];
+
+$result=mysql_query("SELECT AdminId FROM mycmsadmin WHERE username='$username' and password='".sha1($password)."'");
+$row=mysql_fetch_row($result);
+$num_rows = mysql_num_rows($result);
+$id=$row[0];
+
+
+
+if ($_SESSION['signed_in']!='indeed' || $num_rows!=1 || $id!=1){
+Header( "Location: index.php?action=2");
+}else{
+
+
+$result=mysql_query("SELECT Name FROM pages WHERE PageId='$pageid'");
+$row=mysql_fetch_row($result);
+$name=$row[0];
+
+if (!isset($action)) $action="0";
+if (!isset($f)) $f="file";
+if (!isset($sf)) $sf="";
+
+$rootdir="../sections/$pageid";
+$imagedir="../sections/$pageid/images";
+$filedir="../sections/$pageid/files";
+
+
+
+commonheader();
+bodybegin();
+logobar($logoname,$textlogo);
+mainmenu($f);
+
+
+echo "
+<br><br>
+<table>
+<tr>
+	<td valign=\"top\" width=\"20%\">";
+
+submenu($f,$sf);
+
+echo "
+	</td>
+	<td>
+	<center><h2>Control panel - file management</h2></center><br /><br />
+	<b>In the table below you you can see the list of currently stored files.</b><br />
+<br /><br />
+<br />
+
+<!-- Dynamically switch the action of the form between delete/rename -->
+
+	 <script language=\"JavaScript\" type=\"text/javascript\">
+	function OnSubmitForm()
+	{
+	  if(document.pressed == 'Delete')
+	  {
+	   document.fileform.action =\"delfile.php\";
+	  }
+	  else
+	  if(document.pressed == 'Rename')
+	  {
+	    document.fileform.action =\"renamefile.php\";
+	  }
+	  return true;
+	}
+	</SCRIPT>
+
+	<center>
+	<div id=\"formular\">
+
+	<table width=\"90%\" border=\"1\" style=\"border-collapse:collapse\" cellpadding=\"5\" cellspacing=\"5\">
+	<form name=\"fileform\" onsubmit=\"return OnSubmitForm();\">
+	<input type=\"hidden\" name=\"pageid\" value=\"$pageid\" />
+	<tr>
+	<td align=\"center\" width=\"50%\">
+	<b>Images in \"$name\"</b>	
+	</td>
+	<td align=\"center\" width=\"50%\">
+	<b>Other files in \"$name\"</b>
+	</tr>
+	<tr>
+	<td align=\"left\">";
+	$dh=opendir($imagedir);
+	while ($file=readdir($dh))
+	{if ($file!="." && $file!=".." && $file!="index.php") echo "<input type=\"radio\" name=\"thefile\" value=\"$imagedir/$file\" /> $file<br />";
+	}
+	echo "
+	</td>
+	<td align=\"left\">";
+	$dh=opendir($filedir);
+	while ($file=readdir($dh))
+	{if ($file!="." && $file!=".." && $file!="index.php") echo "<input type=\"radio\" name=\"thefile\" value=\"$filedir/$file\" /> $file <br />";
+	}
+	echo "
+	</td>
+	</tr>
+	<tr>
+	<td colspan=\"2\" align=\"center\">
+	<input type=\"submit\" value=\"Delete\" name=\"operation\" onclick=\"document.pressed=this.value\" /> <br/>
+	<input type=\"submit\" value=\"Rename\" name=\"operation\" onclick=\"document.pressed=this.value\" />
+	
+	</td>
+	</tr>
+	</form>
+	<tr>
+	<td align=\"left\">
+	<b>Upload image:</b><br />
+	<form enctype=\"multipart/form-data\" action='uploadfileResponse.php' method='POST'>
+	<input type=\"hidden\" name=\"pageid\" value=\"$pageid\" />
+	<input type=\"hidden\" name=\"filetype\" value=\"image\" />
+	<input type=\"file\" name=\"thefile\" />
+	<input type=\"submit\" value=\"Upload\" />
+	</form>
+	</td>
+	<td align=\"left\">
+	<b>Upload file:</b><br />
+	<form enctype=\"multipart/form-data\" action='uploadfileResponse.php' method='POST'>
+	<input type=\"hidden\" name=\"pageid\" value=\"$pageid\" />
+	<input type=\"hidden\" name=\"filetype\" value=\"file\" />
+	<input type=\"file\" name=\"thefile\" />
+	<input type=\"submit\" value=\"Upload\" />
+	</form>
+	</td>
+	</tr>
+	</table>";
+	if ($action=="1") echo "<b>You cannot upload scripts using this forms!</b>";
+	
+	echo "
+	</div>
+	</center>
+
+	<br /><br /><br />
+
+
+	</td>
+</tr>
+</table>
+ ";
+
+bodyend();
+commonfooter();
+
+
+
+}
+?>
