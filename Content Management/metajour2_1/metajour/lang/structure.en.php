380b14150a2bfaddbc5d40d0ad9e999feb6de2a4
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:32 +0300

    Add Content Management/metajour2_1/metajour/lang/structure.en.php

---
+++ Content Management/metajour2_1/metajour/lang/structure.en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Structures';
+$LANG['label_name'] = 'Name';
+$LANG['label_templateid'] = 'Template';
+$LANG['label_cacheallowed'] = 'Allow caching';
+$LANG['label_cachelifetime'] = 'Cache limit (seconds)';
+?>
