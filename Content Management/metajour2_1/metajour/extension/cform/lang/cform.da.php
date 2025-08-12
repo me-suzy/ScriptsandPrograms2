49506b4ed08c974aff55de6838bcebb8249389ad
me-suzy <me.suzana@gmail.com>
2025-08-12 06:12:52 +0300

    Add Content Management/metajour2_1/metajour/extension/cform/lang/cform.da.php

---
+++ Content Management/metajour2_1/metajour/extension/cform/lang/cform.da.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['label_name'] = 'Konfigurationsnavn';
+$LANG['label_otype'] = 'Tabel';
+$LANG['label_formtype'] = 'Formular type';
+$LANG['label_templateid'] = 'Skabelon';
+$LANG['name'] = 'Konfiguration af Brugerdefineret formular';
+$LANG['option_formtype'][0] = 'Opret eller rediger post';
+$LANG['option_formtype'][1] = 'Kun oprettelse';
+$LANG['option_formtype'][3] = 'Opret eller rediger denne brugers egen post';
+?>
