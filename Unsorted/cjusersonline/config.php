5fb2bcabcb91d93d5f8a125ee46a17789009d256
me-suzy <me.suzana@gmail.com>
2025-08-13 01:40:50 +0300

    Add Unsorted/cjusersonline/config.php

---
+++ Unsorted/cjusersonline/config.php
@@ -0,0 +1,8 @@
+<?
+$server = "localhost";				// Your mySQL Server, most cases "localhost"                  
+$db_user = "joe_bloggs";			// Your mySQL Username                                        
+$db_pass = "password";		// Your mySQL Password                                        
+$db = "database1";					// Database Name     
+
+$to_secs = 120;		// time to reset IP address's value in seconds, default here is 120 (2 minutes)
+?>
