e98b2e4cb14ae8fe91edb772b7deaffb304ed4f4
me-suzy <me.suzana@gmail.com>
2025-08-12 17:09:32 +0300

    Add Groupware Tools/l4w_v0_5_2/modules/mandators/models/fields_validations.inc.php

---
+++ Groupware Tools/l4w_v0_5_2/modules/mandators/models/fields_validations.inc.php
@@ -0,0 +1,38 @@
+<?php
+
+  /**
+    * $Id: fields_validations.inc.php,v 1.1 2005/06/02 13:20:01 carsten Exp $
+    *
+    * controls additional field validation 
+    *
+    * @author       Carsten Graef <evandor@gmx.de>
+    * @copyright    evandor media 2005
+    * @package mandators
+    */
+    
+   /**
+    * Additional field validation
+    *
+    * Additional validations (apart from core validation) can be defined here. 
+    *
+    * @author       Carsten Graef <evandor@gmx.de>
+    * @copyright    evandor media 2004
+    * @package mandators
+    */    
+    class fields_validations {
+         
+       /**
+        * Constructor.
+        *
+        * empty
+        * 
+        * @access       public
+        * @since        0.5.1
+        * @version      0.5.1
+        */
+        function fields_validations () {
+        }
+
+    }   
+
+?>
