49b36ca91bc20396324688377317f3ae3a370a06
me-suzy <me.suzana@gmail.com>
2025-08-12 07:15:12 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_pageindex_342708.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_pageindex_342708.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342708;
+function objfile_342708 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Page index";
+$objdata['subname'] = "";
+$objdata['extension'] = "index";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
