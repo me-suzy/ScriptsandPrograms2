692f61579f88fa2b0cbf3d4081ea4866ab470733
me-suzy <me.suzana@gmail.com>
2025-08-12 10:38:20 +0300

    Add Discussion Boards/forum/forum/chat.php

---
+++ Discussion Boards/forum/forum/chat.php
@@ -0,0 +1,15 @@
+<?php
+
+include("header.php");
+
+table_header("Realtime Chat");
+echo ("<table border=\"0\" cellspacing=\"1\" cellpadding=\"3\" bgcolor=\"$tborder_color2\" width=\"$fwidth\" align=\"center\">
+   <tr bgcolor=\"#FFFFFF\">
+      <td width=\"100%\" colspan=\"1\"><font size=\"$fsmall\" face=\"$fface\" color=\"$fcolor\">lol</font></td>
+   </tr>
+</table>");
+table_footer();
+
+include("footer.php");
+
+?>
