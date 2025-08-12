7e9aa9e0d35e613de08882a2b99de15da76e7078
me-suzy <me.suzana@gmail.com>
2025-08-12 17:59:30 +0300

    Add Image Galleries/ipg-1/ipg-1/install/index.php

---
+++ Image Galleries/ipg-1/ipg-1/install/index.php
@@ -0,0 +1,56 @@
+<?php
+/******************************************************************************
+* IPG: Instant Photo Gallery                                                  *
+* =========================================================================== *
+* Software Version:             IPG 1.0                                       *
+* Copyright 2005 by:            Verosky Media - Edward Verosky                *
+* Support, News, Updates at:    http://www.instantphotogallery.com            *
+*******************************************************************************
+* This program is free software; you may redistribute it and/or modify it     *
+* under the terms of the GNU General Public License as published by the Free  * 
+* Software Foundation; either version 2 of the License, or (at your option)   *
+* any later version.                                                          *                                                                             *
+* This program is distributed WITHOUT ANY WARRANTIES; without even any        *
+* implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    *
+*                                                                             *
+* See www.gnu.org  for details of the GPL license.                            *
+******************************************************************************/
+
+?>
+
+<HTML>
+<HEAD>
+<title>Installation</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+<link rel="stylesheet" href="../CSS/main.css" type="text/css">
+
+</head>
+
+<body bgcolor="#CCCCCC" text="#000000">
+<p>&nbsp;</p>
+<div align="center">
+  <table width="75%" border="3" cellspacing="0" cellpadding="20">
+    <tr> 
+      <td bgcolor="#FFFFFF"> 
+        <p><img src="logo.gif" width="273" height="75"></p>
+        <h3>Welcome to the Instant Photo Gallery Installation Page.</h3>
+        <p>Please follow these simple steps to get your Instant Photo Gallery 
+          installed and online. If you have any problems with the installation, 
+          please visit the <a href="http://www.instantphotogallery.com" target="_blank">support</a> 
+          site.</p>
+        <p>By purchasing, installing, and using this product, you agree to the terms of the 
+          <a href="http://www.instantphotogallery.com" target="_blank">license.</a></p>
+        <p>&nbsp;</p>
+        <center>
+          <form name="form1" method="post" action="install_01.php">
+          <input type="submit" name="Submit" value="Continue...">
+        </form></center>
+
+      </td>
+    </tr>
+  </table>
+</div>
+<p>&nbsp; </p>
+</body>
+</html>
