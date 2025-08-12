12ca5473b757b3186906f94137a5f1c37d54eb42
me-suzy <me.suzana@gmail.com>
2025-08-12 04:01:31 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_de.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_de.php
@@ -0,0 +1,16 @@
+<?php
+
+$GLOBALS["module_title"]	= 'Web-Seite durchsuchen';
+$GLOBALS["search_keywords"]	= 'Suche nach folgenden Schlüsselwörtern';
+$GLOBALS["search_OR"]		= 'Beliebige Kombination der Wörter';
+$GLOBALS["search_AND"]		= 'Alle Wörter';
+$GLOBALS["search_titles"]	= 'Durchsuche Titel';
+$GLOBALS["search_teasers"]	= 'Fusstext';
+$GLOBALS["search_contents"]	= 'Inhalt';
+$GLOBALS["search_button"]	= 'Suche';
+
+$GLOBALS["eEnterKeywords"]	= 'Bitte geben Sie Suchbegriffe ein.';
+$GLOBALS["eEnterElements"]	= 'Bitte geben Sie die zu durchsuchenden Elemente an.';
+$GLOBALS["eNoResults"]		= 'Keine Ergebnisse gefunden.';
+
+?>
