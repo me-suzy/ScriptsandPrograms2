c579fcb925908c512da300f977a4790fb293bc72
me-suzy <me.suzana@gmail.com>
2025-08-12 23:53:48 +0300

    Add Tests and Quiz/quiz_manager_09/quiz_manager_09/index.php

---
+++ Tests and Quiz/quiz_manager_09/quiz_manager_09/index.php
@@ -0,0 +1,22 @@
+
+
+
+<b>Quiz Manager</b>
+<ul>
+<li><a href=docs/readme.txt>readme</a>
+<li><a href=docs/tables.txt>tables</a>
+<li><a href=install.php>install</a> 
+<li><a href=admin.php>admin</a> (default password: admin)
+</ul>
+<p>
+
+Including Quiz Form on Another Page:
+<pre>
+	&lt;? include "<?
+
+	print str_replace("index.php", "", $_SERVER[REQUEST_URI]);	
+
+	?>quiz_form.php" ?&gt;
+</pre>
+
+
