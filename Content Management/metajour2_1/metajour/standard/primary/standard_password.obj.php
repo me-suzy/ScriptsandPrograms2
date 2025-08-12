31fbe00d8b9e73c8e9a322fead27557a1d9f9d09
me-suzy <me.suzana@gmail.com>
2025-08-12 07:15:20 +0300

    Add Content Management/metajour2_1/metajour/standard/primary/standard_password.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/primary/standard_password.obj.php
@@ -0,0 +1,19 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 345379;
+function objfile_345379 () {
+$obj = owNew('template');
+$objdata['name'] = "standard_password";
+$objdata['content'] = "{include file=\"__STANDARD__\" _system_force_password=\"yes\"}";
+$objdata['tpltype'] = "1";
+$objdata['htmledit'] = "0";
+$objdata['header'] = "";
+$objdata['style'] = "";
+$objdata['param'] = "";
+$objdata['setting'] = "";
+$objdata['config'] = "";
+$objdata['doctype'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
