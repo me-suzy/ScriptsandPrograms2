a9ed32025113a5436b1a2cc773c6680332564975
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:28 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_menu.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_menu.en.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['menu_website'] = 'Website';
+$LANG['menu_file'] = 'Files';
+$LANG['menu_access'] = 'Users';
+$LANG['menu_ebusiness'] = 'Ebusiness';
+$LANG['menu_intranet'] = 'CRM';
+$LANG['menu_advanced'] = 'Tools';
+$LANG['menu_extension'] = 'Extensions';
+$LANG['menu_statistics'] = 'Website statistics';
+$LANG['menu_filestatistics'] = 'File statistics';
+$LANG['menu_changeguilanguage'] = 'Change language';
+$LANG['menu_createsite'] = 'Create new company / site';
+$LANG['menu_installsite'] = 'Install predefined site';
+$LANG['menu_development'] = 'Advanced';
+$LANG['menu_customform'] = 'Forms';
+$LANG['menu_customform_generator'] = 'Form generator';
+$LANG['menu_customform_form'] = 'Form';
+$LANG['menu_customform_list'] = 'List';
+$LANG['menu_customform_stats'] = 'Statistics';
+$LANG['menu_import'] = 'Import';
+$LANG['menu_package'] = 'Import object archive';
+$LANG['menu_emptytrashcan'] = 'Empty trashcan';
+$LANG['menu_exit'] = 'Exit';
+$LANG['dialog_terminate'] = 'Do you want to exit the program?';
+?>
