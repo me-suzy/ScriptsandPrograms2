71d9789671bb5f74eabca2cdcdff56425fb4dfdd
me-suzy <me.suzana@gmail.com>
2025-08-12 18:48:19 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/deleteskin.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/deleteskin.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%DELETESKIN%></td><td><select name='skida'><%SKINLIST%></select></td></tr>
+<tr><td class='fframe'><%CHANGETO%></td><td><select name='skidb'><%SKINLIST%></select></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>' onClick='javascript:return confirm("<%AREYOUSURE%>")'></td></tr>
+</table>
