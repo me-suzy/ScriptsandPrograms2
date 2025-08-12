88377b6215cd77a91c24992deedf23b6b8221d25
me-suzy <me.suzana@gmail.com>
2025-08-12 11:54:26 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/modules/payment/egold.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/modules/payment/egold.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id: e-gold.php,v 1 2003/07/23
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+
+  e-Gold module Crated by Craig Harrison
+*/
+
+  define('MODULE_PAYMENT_EGOLD_TEXT_TITLE', 'e-Gold');
+  define('MODULE_PAYMENT_EGOLD_TEXT_DESCRIPTION', 'e-Gold');
+?>
