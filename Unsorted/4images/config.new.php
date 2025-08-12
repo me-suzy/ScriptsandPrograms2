89cde1ed6b61e1411fdd65a3c7bd887cd1e0463d
me-suzy <me.suzana@gmail.com>
2025-08-13 00:14:24 +0300

    Add Unsorted/4images/config.new.php

---
+++ Unsorted/4images/config.new.php
@@ -0,0 +1,35 @@
+<?php
+/**************************************************************************
+ *                                                                        *
+ *    4images - A Web Based Image Gallery Management System               *
+ *    ----------------------------------------------------------------    *
+ *                                                                        *
+ *             File: config.php                                           *
+ *        Copyright: (C) 2002 Jan Sorgalla                                *
+ *            Email: jan@4homepages.de                                    *
+ *              Web: http://www.4homepages.de                             *
+ *    Scriptversion: 1.7                                                  *
+ *                                                                        *
+ *    Never released without support from: Nicky (http://www.nicky.net)   *
+ *                                                                        *
+ **************************************************************************
+ *                                                                        *
+ *    Dieses Script ist KEINE Freeware. Bitte lesen Sie die Lizenz-       *
+ *    bedingungen (Lizenz.txt) für weitere Informationen.                 *
+ *    ---------------------------------------------------------------     *
+ *    This script is NOT freeware! Please read the Copyright Notice       *
+ *    (Licence.txt) for further information.                              *
+ *                                                                        *
+ *************************************************************************/
+
+$db_servertype = "mysql";
+$db_host = "";
+$db_name = "";
+$db_user = "";
+$db_password = "";
+
+$table_prefix = "4images_";
+
+define('4IMAGES_ACTIVE', 1);
+
+?>
