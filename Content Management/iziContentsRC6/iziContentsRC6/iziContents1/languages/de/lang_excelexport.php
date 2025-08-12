6c850451c7c131f2b334b2a842f299d11eb04387
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:16 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_excelexport.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_excelexport.php
@@ -0,0 +1,18 @@
+<?php
+$GLOBALS["texFormTitle"] = 'Excel-Export';
+$GLOBALS["texTitel"] = 'Suche nach User, um sie als Excel zu exportieren';
+$GLOBALS["texSubmit"] = 'Suchen';
+$GLOBALS["texSearchAll"] = 'Alle';
+$GLOBALS["texName"] = 'Name';
+$GLOBALS["texPLZ"] = 'PLZ';
+$GLOBALS["texOrt"] = 'Ort';
+$GLOBALS["texCountry"] = 'Land';
+$GLOBALS["texUsergroup"] = 'Usergruppe';
+$GLOBALS["texSearchKriteria"] = 'Suchkriterien';
+$GLOBALS["texNoKriteria"] = 'keine';
+$GLOBALS["texNoResults"] = 'Es wurden keine Datens&auml;tze zu Ihren Suchkriterien gefunden!';
+$GLOBALS["texFoundResults"] = 'Gefundene Datens&auml;tze';
+$GLOBALS["texDlExcel"] = 'Download Excel-Datei';
+$GLOBALS["texPhone"] = 'TELEFON';
+$GLOBALS["texAdresse"] = 'ADRESSE';
+?>
