bbc6b0f327d5371bcff283b34d767b5a38ab1cfc
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:16 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_addcategory.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_addcategory.da.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Tilføj til kategori';
+$LANG['text_1'] = 'Er du sikker på at du vil tilføje kategorien til de valgte poster';
+?>
