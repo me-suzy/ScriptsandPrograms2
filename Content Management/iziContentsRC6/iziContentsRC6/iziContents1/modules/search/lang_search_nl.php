51866b3820d01daee659b43470f23d2cae70bc06
me-suzy <me.suzana@gmail.com>
2025-08-12 04:01:39 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_nl.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_nl.php
@@ -0,0 +1,17 @@
+<?php
+
+$GLOBALS["module_title"]	= 'Zoeken';
+$GLOBALS["search_keywords"]	= 'Zoekwoorden';
+$GLOBALS["search_OR"]		= 'Elke combinatie van de zoekwoorden';
+$GLOBALS["search_AND"]		= 'Alle zoekwoorden';
+$GLOBALS["search_titles"]	= 'Zoek titel';
+$GLOBALS["search_teasers"]	= 'Teaser tekst';
+$GLOBALS["search_contents"]	= 'Content';
+$GLOBALS["search_button"]	= 'Zoek';
+
+$GLOBALS["eEnterKeywords"]	= 'Voer de zoekwoorden in!';
+$GLOBALS["eEnterElements"]	= 'Geef aan welke pagina-elementen je wilt zoeken.';
+$GLOBALS["eNoResults"]		= 'Geen zoekresultaten.';
+
+?>
+
