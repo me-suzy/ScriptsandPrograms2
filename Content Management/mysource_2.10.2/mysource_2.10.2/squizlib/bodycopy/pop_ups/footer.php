c0856042fb60430521e80e743d35d3f45ddf04ef
me-suzy <me.suzana@gmail.com>
2025-08-12 07:20:20 +0300

    Add Content Management/mysource_2.10.2/mysource_2.10.2/squizlib/bodycopy/pop_ups/footer.php

---
+++ Content Management/mysource_2.10.2/mysource_2.10.2/squizlib/bodycopy/pop_ups/footer.php
@@ -0,0 +1,26 @@
+<?  ##############################################
+   ### SQUIZLIB ------------------------------###
+  ##- Bodycopy Editor ---- PHP4 --------------##
+ #-- Copyright Squiz.net ---------------------#
+##############################################
+## This file is subject to version 1.0 of the
+## MySource License, that is bundled with
+## this package in the file LICENSE, and is
+## available at through the world-wide-web at
+## http://mysource.squiz.net/
+## If you did not receive a copy of the MySource
+## license and are unable to obtain it through
+## the world-wide-web, please contact us at
+## mysource@squiz.net so we can mail you a copy
+## immediately.
+##
+## $Source: /home/cvsroot/squizlib/bodycopy/pop_ups/footer.php,v $
+## $Revision: 2.0 $
+## $Author: agland $
+## $Date: 2001/12/18 06:03:03 $
+#######################################################################
+?> 
+	</td></tr>
+</table>
+</body>
+</html>
