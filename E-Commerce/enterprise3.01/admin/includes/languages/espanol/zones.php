399f98b99dfa235b17f2aaae9854b151c776fcf6
me-suzy <me.suzana@gmail.com>
2025-08-12 11:45:03 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/espanol/zones.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/espanol/zones.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Zonas');
+
+define('TABLE_HEADING_COUNTRY_NAME', 'Pa&iacute;s');
+define('TABLE_HEADING_ZONE_NAME', 'Zonas');
+define('TABLE_HEADING_ZONE_CODE', 'C&oacute;digo');
+define('TABLE_HEADING_ACTION', 'Acci&oacute;n');
+
+define('TEXT_INFO_EDIT_INTRO', 'Haga los cambios necesarios');
+define('TEXT_INFO_ZONES_NAME', 'Nombre de la Zona:');
+define('TEXT_INFO_ZONES_CODE', 'C&oacute;digo de la Zona:');
+define('TEXT_INFO_COUNTRY_NAME', 'Pa&iacute;s:');
+define('TEXT_INFO_INSERT_INTRO', 'Introduzca los datos de la nueva zona');
+define('TEXT_INFO_DELETE_INTRO', 'Seguro que desea eliminar esta zona?');
+define('TEXT_INFO_HEADING_NEW_ZONE', 'Nueva Zona');
+define('TEXT_INFO_HEADING_EDIT_ZONE', 'Editar Zone');
+define('TEXT_INFO_HEADING_DELETE_ZONE', 'Eliminar Zone');
+?>
