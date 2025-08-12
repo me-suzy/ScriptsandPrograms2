502a79c50f5a9bed15f06eb41f67741b4c2d2058
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:12 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_active.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_active.da.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Aktiver';
+$LANG['text_1'] = 'Aktiver eller deaktiver de markerede poster';
+$LANG['button_activate'] = 'Aktiver';
+$LANG['button_deactivate'] = 'Deaktiver';
+?>
