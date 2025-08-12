bd798cacea0a141e197abbe55caa214c3795c5d1
me-suzy <me.suzana@gmail.com>
2025-08-12 21:02:58 +0300

    Add News Publishing/zomplog34/zomplog34/image.php

---
+++ News Publishing/zomplog34/zomplog34/image.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Image</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<table width="100%" border="0" align="center" cellspacing="0">
+  <tr>
+    <td><?php echo "<div align='center'><img src='upload/thumbnail.php?gd=2&src=$_GET[image]&maxw=470'></div>"; ?></td>
+  </tr>
+</table>
+</body>
+</html>
