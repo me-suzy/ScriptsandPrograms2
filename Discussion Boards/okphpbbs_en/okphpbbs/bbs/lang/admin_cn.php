11e0cb6973722f3da4a65fb2b4498d6010315312
me-suzy <me.suzana@gmail.com>
2025-08-12 10:43:16 +0300

    Add Discussion Boards/okphpbbs_en/okphpbbs/bbs/lang/admin_cn.php

---
+++ Discussion Boards/okphpbbs_en/okphpbbs/bbs/lang/admin_cn.php
@@ -0,0 +1,99 @@
+<?php
+/*
+ ***********************************************************************
+ * Copyright (c) 2003-2004 Okphp Group
+ * http://www.okphp.com
+ *  
+ ***********************************************************************
+ */
+
+
+//#######################
+// global
+$indications['yes'] = "ÊÇ"; //*
+$indications['no'] = "·ñ"; //*
+
+$indications['root'] = "¸ùÀà±ð";
+$indications['add_forum'] = "Ìí¼ÓÂÛÌ³";
+$indications['add_class'] = "Ìí¼Ó·ÖÀà";
+$indications['add_type'] = "Add Type";
+$indications['manage_comment'] = "Manage Comments";
+$indications['add_file'] = "Add File";
+$indications['back_'] = "Go Back";
+
+// page
+$indications['page_down'] = "ÏÂÒ³&gt;";
+$indications['page_up'] = "&lt;ÉÏÒ³";
+$indications['page_end'] = "Ä©Ò³&gt;|";
+$indications['page_begin'] = "|&lt;Ê×Ò³";
+$indications['all_page'] = "×ÜÒ³Êý£º";
+$indications['current_page'] = "£¬µ±Ç°Ò³£º";
+$indications['page_jump'] = "Ìø×ªµ½";
+
+// 3.0
+$indications['on_use'] = "Ê¹ÓÃÖÐ";
+// #########################
+$messages['process_failed'] = "²Ù×÷Ê§°Ü£¡ÇëÈ·ÈÏ²Ù×÷²½ÖèÕýÈ·¡£";
+$messages['process_ok'] = "²Ù×÷³É¹¦£¡";
+$messages['file_not_exists'] = "¶Ô²»Æð£¡¸ÃÎÄ¼þ²»´æÔÚ";
+$messages['info_not_enough'] = "ÐÅÏ¢ÌîÐ´²»ÍêÈ«£¡";
+$messages['must_files_folder'] = "Äú±ØÐëÑ¡ÔñÒ»¸öÎÄÕÂÀà";
+$messages['not_to_self'] = "ÎÄ¼þÒÑ¾­ÔÚµ±Ç°Ä¿Â¼";
+$messages['no_file_id'] = "ÇëÑ¡ÔñÒª²Ù×÷µÄÎÄ¼þ";
+$messages['no_such_user'] = "Ã»ÓÐ¸Ã»áÔ±";
+$messages['wrong_email'] = "EMAIL¸ñÊ½²»¶Ô";
+$messages['login_failed'] = "µÇÂ½Ê§°Ü£¡ÏµÍ³ÔÊÐíÄúµÇÂ½Ê§°ÜµÄ´ÎÊýÎª£º";
+$messages['access_dennied'] = "È¨ÏÞ²»¹»£¬²Ù×÷Ê§°Ü";
+$messages['not_match_ip'] = "´ËIP¸ñÊ½²»¶Ô£¡ÔÊÐíµÄ¸ñÊ½Îª:123.123.123.123,123.123.123,123.123";
+$messages['no_count'] = "<font size=5px>¶Ô²»Æð£¡ÄúÑ¡ÔñµÄÈÕÆÚ»¹Ã»ÓÐÍ³¼Æ¼ÇÂ¼¡£</font>"; $messages['no_find'] = "Ã»ÓÐÕÒµ½·ûºÏÌõ¼þµÄ¼ÇÂ¼";
+
+
+
+
+// new ##################################
+$messages['fill_all'] = "ÊäÈëµÄÐÅÏ¢²»È«£¡";
+$messages['delete_class'] = "É¾³ýÀà±ð";
+$messages['deteted_ok'] = "É¾³ý³É¹¦£¡";
+$messages['find'] = "ËÑË÷";
+$messages['records'] = "¼ÇÂ¼";
+$messages['no_find'] = "¶Ô²»Æð£¡Ã»ÓÐÕÒµ½·ûºÏÌõ¼þµÄ¼ÇÂ¼";
+$messages['moved_ok'] = "Ä¿±êÒÆ¶¯³É¹¦£¡";
+$messages['modified_ok'] = "Ä¿±êÐÞ¸Ä³É¹¦£¡";
+$messages['added_ok'] = "Ä¿±ê½¨Á¢³É¹¦£¡";
+
+// cache
+$messages['cache_up_ok'] = "»º´æÒÑ¾­±»³É¹¦¸üÐÂ£¡";
+
+// =======
+$indications['top'] = "ÉÏ";
+$indications['bottom'] = "ÏÂ";
+$indications['left'] = "×ó";
+$indications['right'] = "ÓÒ";
+
+// guestbook
+$indications['replied'] = "ÊÇ";
+$indications['not_reply'] = "·ñ";
+
+// log
+$indications['del_group'] = "É¾³ý»áÔ±×é";
+$indications['add_group'] = "Ìí¼Ó»áÔ±×é";
+$indications['modify_group'] = "ÐÞ¸Ä»áÔ±×é";
+$indications['modify_member'] = "ÐÞ¸Ä»áÔ±ÐÅÏ¢";
+$indications['add_member'] = "Ìí¼Ó»áÔ±";
+$indications['success'] = "³É¹¦!";
+$indications['faild'] = "Ê§°Ü";
+
+// login
+$messages['not_req_for_trial'] = "²âÊÔÓÃ»§²»ÓÃÌîÐ´";
+$messages['license_key_stored'] = "ÄúµÄÐí¿ÉÖ¤ÃÜÂëÒÑ¾­Í¨¹ýCOOKIES´¢´æÔÚÄúµÄµçÄÔÖÐ";
+$indications['username'] = "ÓÃ»§Ãû";
+$indications['password'] = "ÃÜÂë";
+$indications['license_key'] = "Ðí¿ÉÖ¤ÃÜÂë";
+
+// confirm code
+$indications['confirm_'] = "ÑéÖ¤Âë";
+$indications['confirm_ex'] = "ÇëÊäÈëÄúÔÚÍ¼Æ¬ÖÐ¿´µ½µÄÊý×Ö";
+
+// 2.1
+$messages['badlogin_locked'] = "ÓÉÓÚÄúµÇÂ½Ê§°ÜµÄ´ÎÊýÌ«¶à£¬ÏµÍ³ÔÝÊ±ÆÁ±ÎÁËÄúµÄIP£¬Çë¹ý¼¸·ÖÖÓÔÙ³¢ÊÔµÇÂ½";
+?>
