0a66370c5d9cd3b2ab83ff36fed8c60299469845
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:49 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_ratings.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_ratings.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Beheer instellingen inhoud ';
+
+//  Form Field Headings
+$GLOBALS["tAllowRatings"] = 'Sta artikel beoordelingen toe';
+$GLOBALS["tAllowComments"] = 'Sta artikel commentaar toe';
+$GLOBALS["tRatingMin"] = 'Minimale waarde beoordeling';
+$GLOBALS["tRatingMax"] = 'Maximale waarde beoordeling';
+$GLOBALS["tRatingImage1"] = 'Afbeelding beoordeling ster';
+$GLOBALS["tRatingImage2"] = 'Afbeelding beoordeling ster (nog niet beoordeeld)';
+$GLOBALS["tVetComments"] = 'Bekijk de commentaren voor vrijgave';
+$GLOBALS["tPrintFriendly"] = 'Toon printer-vriendelijk icoon';
+$GLOBALS["tTeaserWithDetails"] = 'Intro tekst op detail-pagina';
+
+//  Form Block Titles
+$GLOBALS["thPrinterFriendly"] = 'Print-vriendelijke opties';
+$GLOBALS["thContentRatings"] = 'Inhoud beoordeling opties';
+$GLOBALS["thContentComments"] = 'Inhoud commentaar opties';
+$GLOBALS["thContentSettings"] = 'Andere inhoud opties';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'Met dit formulier kunt u algemene waarden instellen voor inhoudbeoordeling voor deze website.';
+$GLOBALS["hAllowRatings"] = 'Wilt u leden in staat stellen inhoud te beoordelen?';
+$GLOBALS["hAllowComments"] = 'Wilt u leden in staat stellen commentaar te geven op de inhoud?';
+$GLOBALS["hRatingMin"] = 'Selecteer de minumumbeoordelings waarde.';
+$GLOBALS["hRatingMax"] = 'Selecteer de maximumbeoordelings waarde.';
+$GLOBALS["hRatingImage1"] = 'Selecteer de afbeelding voor beoordelingen.';
+$GLOBALS["hRatingImage2"] = 'Selecteer de afbeelding voor beoordelingen.';
+$GLOBALS["hVetComments"] = 'Wilt u de commentaren bekijken alvorens ze op de site zichtbaar zijn?<br />Note nog niet beschikbaar.';
+$GLOBALS["hPrintFriendly"] = 'Dit is de standaard voor de print-vriendelijke opties.';
+$GLOBALS["hTeaserWithDetails"] = 'Toon de introtekst zowel op de detailspagina als op de hoofdpagina';
+
+?>
