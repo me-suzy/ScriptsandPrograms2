b6d654694ab723739e289d936627f9b01d596cab
me-suzy <me.suzana@gmail.com>
2025-08-12 11:30:35 +0300

    Add E-Commerce/enterprise3.01/admin/affiliate_sales.php

---
+++ E-Commerce/enterprise3.01/admin/affiliate_sales.php
@@ -0,0 +1,154 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  require('includes/application_top.php');
+
+  require(DIR_WS_CLASSES . 'currencies.php');
+  $currencies = new currencies();
+
+  if ($HTTP_GET_VARS['acID'] > 0) {
+
+    $affiliate_sales_raw = "
+      select asale.*, os.orders_status_name as orders_status, a.affiliate_firstname, a.affiliate_lastname from " . TABLE_AFFILIATE_SALES . " asale
+      left join " . TABLE_ORDERS . " o on (asale.affiliate_orders_id = o.orders_id)
+      left join " . TABLE_ORDERS_STATUS . " os on (o.orders_status = os.orders_status_id and language_id = " . $languages_id . ")
+      left join " . TABLE_AFFILIATE . " a on (a.affiliate_id = asale.affiliate_id)
+      where asale.affiliate_id = '" . $HTTP_GET_VARS['acID'] . "'
+      order by affiliate_date desc
+      ";
+    $affiliate_sales_split = new splitPageResults($HTTP_GET_VARS['page'], MAX_DISPLAY_SEARCH_RESULTS, $affiliate_sales_raw, $affiliate_sales_numrows);
+
+  } else {
+
+    $affiliate_sales_raw = "
+      select asale.*, os.orders_status_name as orders_status, a.affiliate_firstname, a.affiliate_lastname from " . TABLE_AFFILIATE_SALES . " asale
+      left join " . TABLE_ORDERS . " o on (asale.affiliate_orders_id = o.orders_id)
+      left join " . TABLE_ORDERS_STATUS . " os on (o.orders_status = os.orders_status_id and language_id = " . $languages_id . ")
+      left join " . TABLE_AFFILIATE . " a  on (a.affiliate_id = asale.affiliate_id)
+      order by affiliate_date desc
+      ";
+    $affiliate_sales_split = new splitPageResults($HTTP_GET_VARS['page'], MAX_DISPLAY_SEARCH_RESULTS, $affiliate_sales_raw, $affiliate_sales_numrows);
+  }
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
+<title><?php echo TITLE; ?></title>
+<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
+<script type="text/javascript" src="includes/browser.js">/************************************************ Jim's DHTML Menu v5.0- © Jim Salyer (jsalyer@REMOVETHISmchsi.com)* Visit Dynamic Drive: http://www.dynamicdrive.com for script and instructions* This notice must stay intact for use***********************************************/</script><script type="text/javascript" src="config.js"></script></head>
+<body onload="init();" marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+
+<!-- body //-->
+<table border="0" width="100%" cellspacing="2" cellpadding="2">
+  <tr>
+    <td width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+    <td width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
+            <td class="pageHeading" align="right"><?php echo escs_draw_separator('pixel_trans.gif', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
+<?php
+  if ($HTTP_GET_VARS['acID'] > 0) {
+?>
+            <td class="pageHeading" align="right"><?php echo '<a href="' . escs_href_link(FILENAME_AFFILIATE_STATISTICS, escs_get_all_get_params(array('action'))) . '">' . escs_image_button('button_back.gif', IMAGE_BACK) . '</a>'; ?></td>
+<?php
+  } else {
+?>
+            <td class="pageHeading" align="right"><?php echo '<a href="' . escs_href_link(FILENAME_AFFILIATE_SUMMARY, '') . '">' . escs_image_button('button_back.gif', IMAGE_BACK) . '</a>'; ?></td>
+<?php
+  }
+?>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="4">
+          <tr class="dataTableHeadingRow">
+            <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_AFFILIATE; ?></td>
+            <td class="dataTableHeadingContent" align="center"><?php echo TABLE_HEADING_DATE; ?></td>
+            <td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_ORDER_ID; ?></td>
+            <td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_VALUE; ?></td>
+            <td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_PERCENTAGE; ?></td>
+            <td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_SALES; ?></td>
+            <td class="dataTableHeadingContent" align="center"><?php echo TABLE_HEADING_STATUS; ?></td>
+          </tr>
+<?php
+  if ($affiliate_sales_numrows > 0) {
+    $affiliate_sales_values = escs_db_query($affiliate_sales_raw);
+    $number_of_sales = '0';
+    while ($affiliate_sales = escs_db_fetch_array($affiliate_sales_values)) {
+      $number_of_sales++;
+      if (($number_of_sales / 2) == floor($number_of_sales / 2)) {
+        echo '          <tr class="dataTableRowSelected">';
+      } else {
+        echo '          <tr class="dataTableRow">';
+      }
+
+      $link_to = '<a href="orders.php?action=edit&oID=' . $affiliate_sales['affiliate_orders_id'] . '">' . $affiliate_sales['affiliate_orders_id'] . '</a>';
+?>
+            <td class="dataTableContent"><?php echo $affiliate_sales['affiliate_firstname'] . " ". $affiliate_sales['affiliate_lastname']; ?></td>
+            <td class="dataTableContent" align="center"><?php echo escs_date_short($affiliate_sales['affiliate_date']); ?></td>
+            <td class="dataTableContent" align="right"><?php echo $link_to; ?></td>
+            <td class="dataTableContent" align="right">&nbsp;&nbsp;<?php echo $currencies->display_price($affiliate_sales['affiliate_value'], ''); ?></td>
+            <td class="dataTableContent" align="right"><?php echo $affiliate_sales['affiliate_percent'] . "%" ; ?></td>
+            <td class="dataTableContent" align="right">&nbsp;&nbsp;<?php echo $currencies->display_price($affiliate_sales['affiliate_payment'], ''); ?></td>
+            <td class="dataTableContent" align="center"><?php if ($affiliate_sales['orders_status']) echo $affiliate_sales['orders_status']; else echo TEXT_DELETED_ORDER_BY_ADMIN; ?></td>
+<?php
+    }
+  } else {
+?>
+          <tr class="dataTableRowSelected">
+            <td colspan="7" class="smallText"><?php echo TEXT_NO_SALES; ?></td>
+          </tr>
+<?php
+  }
+  if ($affiliate_sales_numrows > 0 && (PREV_NEXT_BAR_LOCATION == '2' || PREV_NEXT_BAR_LOCATION == '3')) {
+?>
+          <tr>
+            <td colspan="7"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td class="smallText" valign="top"><?php echo $affiliate_sales_split->display_count($affiliate_sales_numrows, MAX_DISPLAY_SEARCH_RESULTS, $HTTP_GET_VARS['page'], TEXT_DISPLAY_NUMBER_OF_SALES); ?></td>
+                <td class="smallText" align="right"><?php echo $affiliate_sales_split->display_links($affiliate_sales_numrows, MAX_DISPLAY_SEARCH_RESULTS, MAX_DISPLAY_PAGE_LINKS, $HTTP_GET_VARS['page'], escs_get_all_get_params(array('page', 'info', 'x', 'y'))); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+<?php
+  }
+?>
+        </table></td>
+      </tr>
+    </table></td>
+<!-- body_text_eof //-->
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br>
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php');?>
