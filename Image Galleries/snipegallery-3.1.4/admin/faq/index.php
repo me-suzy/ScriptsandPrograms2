fc61dcd2d146b5080bbba2d998350f5c7106a069
me-suzy <me.suzana@gmail.com>
2025-08-12 18:22:54 +0300

    Add Image Galleries/snipegallery-3.1.4/admin/faq/index.php

---
+++ Image Galleries/snipegallery-3.1.4/admin/faq/index.php
@@ -0,0 +1,37 @@
+<?php 
+/**
+* Frequently Asked Questions Index
+*
+* Provides some frequently asked questions and answers, as well as troubleshooting 
+* tips and links to where to find support.
+*   
+* @package      admin
+* @author       A Gianotto <snipe@snipe.net>
+* @since 3.0
+* @version 3.0
+*/
+$GALLERY_SECTION = "faq";
+$PAGE_TITLE = "FAQ";
+include ("../../inc/config.php");
+
+include ("../layout/admin.header.php");  
+
+?>
+<h3>Frequently Asked Questions</h3>
+<p><b>What is the difference between an &quot;album&quot; and a &quot;gallery&quot;?</b><br>
+&quot;Album&quot; and &quot;Gallery&quot; are simply terms we use to show the difference between top level categories and sub-level categories.  Albums are the top-level categories which can contain individual galleries, allowing you to quickly and easily organize your images.   
+</p>
+
+<p><b>Where can I go for Snipe Gallery Support?</b><br>
+Please visit the Snipe Gallery forums, available at <a href="http://www.snipegallery.com/forums/" target="_new">http://www.snipegallery.com/forums/</a>
+</p>
+
+<p><b>Where can I go for Snipe Gallery Documentation?</b><br>
+Please visit the online documentation, available at <a href="http://snipegallery.com/docs/" target="_new">http://snipegallery.com/docs/</a>
+</p>
+
+
+
+
+	
+<?php include ("../layout/admin.footer.php");   ?>	
