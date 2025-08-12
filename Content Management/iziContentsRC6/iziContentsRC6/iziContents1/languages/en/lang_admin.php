a9a99f1fdbb5dc22b1d8e73cdf8b842f80097140
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:10 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_admin.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_admin.php
@@ -0,0 +1,177 @@
+<?php
+
+$GLOBALS["tPage"] = 'Page';
+$GLOBALS["tOf"] = 'of';
+$GLOBALS["tEditDel"] = 'Edit/Del';
+$GLOBALS["tEditDelRel"] = 'Edit/Del/Rel';
+$GLOBALS["tDel"] = 'Del';
+$GLOBALS["tAddNew"] = 'Add new';
+$GLOBALS["tEdit"] = 'Edit';
+$GLOBALS["tView"] = 'View';
+$GLOBALS["tDelete"] = 'Delete';
+$GLOBALS["tConfirmDeletion"] = 'Confirm deletion';
+$GLOBALS["tToggle"] = 'Toggle Released/Pending';
+$GLOBALS["tShowAll"] = 'Show all';
+
+$GLOBALS["tMoveUp"] = 'Up';
+$GLOBALS["tMoveDown"] = 'Down';
+
+$GLOBALS["tReleased"] = 'Released';
+$GLOBALS["tPending"] = 'Pending';
+
+//  List Paging
+$GLOBALS["tFirstPage"] = 'First page';
+$GLOBALS["tNextPage"] = 'Next page';
+$GLOBALS["tPrevPage"] = 'Previous page';
+$GLOBALS["tLastPage"] = 'Last page';
+$GLOBALS["tSort"] = 'Sort by';
+
+//  Action Buttons
+$GLOBALS["tSave"] = 'Save';
+$GLOBALS["tReset"] = 'Reset';
+$GLOBALS["tCancel"] = 'Cancel';
+$GLOBALS["tTranslate"] = 'Translate';
+
+$GLOBALS["tMonth_Array"] = array('', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+$GLOBALS["tWeek_Array"] = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+
+$GLOBALS["tYes"] = 'Yes';
+$GLOBALS["tNo"] = 'No';
+
+$GLOBALS["tLeft"] = 'Left';
+$GLOBALS["tCentre"] = 'Centre';
+$GLOBALS["tRight"] = 'Right';
+$GLOBALS["tJustify"] = 'Justify';
+
+$GLOBALS["tAscending"] = 'Ascending';
+$GLOBALS["tDescending"] = 'Descending';
+
+$GLOBALS["toOrderID"] = 'Order ID';
+$GLOBALS["toPublished"] = 'Date Published';
+$GLOBALS["toModified"] = 'Date Modified';
+$GLOBALS["toAlphabetic"] = 'Title (Alphabetic)';
+
+$GLOBALS["tAlways"] = 'Always';
+$GLOBALS["tAutomatic"] = 'Automatic';
+$GLOBALS["tNever"] = 'Never';
+
+$GLOBALS["tFilter"] = 'Filter';
+$GLOBALS["tSelect"] = 'Select';
+$GLOBALS["tNoImage"] = 'No image';
+$GLOBALS["tNoParent"] = 'No parent';
+$GLOBALS["tAllParents"] = 'All parents';
+$GLOBALS["tShowImage"] = 'Show image';
+$GLOBALS["tPublish"] = 'Publish';
+$GLOBALS["tExpire"] = 'Expire';
+
+$GLOBALS["tExpired"] = 'Expired';
+$GLOBALS["tPending"] = 'Pending';
+$GLOBALS["tActive"] = 'Active';
+$GLOBALS["tEnabled"] = 'Enabled';
+$GLOBALS["tDisabled"] = 'Disabled';
+$GLOBALS["tVisible"] = 'Visible';
+$GLOBALS["tHidden"] = 'Hidden';
+
+//  Menu Texts
+$GLOBALS["tsTitle"] = 'iziContents admin section';
+$GLOBALS["tmWelcome"] = 'Welcome';
+$GLOBALS["tmInGroup"] = 'You are in the group';
+$GLOBALS["tsWelcome"] = 'Welcome to the iziContents admin section ';
+$GLOBALS["tsMessage"] = 'Here you can maintain your website.';
+
+$GLOBALS["tmSubContent"] = 'User-submitted content';
+$GLOBALS["tmLogin"] = 'Login';
+$GLOBALS["tmLogout"] = 'Logout';
+
+//  Login Texts
+$GLOBALS["tLoginTitle"] = 'USER LOGIN';
+$GLOBALS["tInvalidLogin"] = 'Access denied';
+$GLOBALS["tUsernameText"] = 'Username';
+$GLOBALS["tPasswordText"] = 'Password';
+$GLOBALS["tLoginText"] = 'Login';
+
+$GLOBALS["tMustLogin"] = 'You are already a registered user, but you must';
+$GLOBALS["tMustRegister"] = 'You must register and/or';
+$GLOBALS["tLogin"] = 'log in';
+$GLOBALS["tHigherPriv"] = 'with an appropriate privilege rating';
+$GLOBALS["tToUpdateDetails"] = 'to update your user details';
+$GLOBALS["tToAccessPrivate"] = 'to access private areas';
+$GLOBALS["eNoLogin"] = 'You must enter your login name';
+$GLOBALS["eNoPassword"] = 'You must enter your password';
+$GLOBALS["tLoginSuccessful"] = 'You have logged on successfully';
+
+$GLOBALS["tFramesRequired"] = 'This page uses frames, but your browser doesn\'t support them.';
+
+//  Sites and themes
+$GLOBALS["tCurrentSite"] = 'Current site';
+$GLOBALS["tMasterSite"] = 'Master Site';
+$GLOBALS["tCurrentTheme"] = 'Current theme';
+$GLOBALS["tDefaultTheme"] = 'Default Theme';
+$GLOBALS["tAdminStyle"] = 'Administration style';
+
+//  Translations
+$GLOBALS["tLangFilter"] = 'Language filter';
+$GLOBALS["tTranslated"] = 'Item has been translated';
+$GLOBALS["tNotTranslated"] = 'Item has not been translated';
+
+//  Help
+$GLOBALS["tOnlineHelp"] = 'iziContents Online Help';
+$GLOBALS["tHelpText"] = 'Click on a field name for an explanation of that field';
+$GLOBALS["tCloseHelp"] = 'Close help window';
+$GLOBALS["tBabelTranslation"] = 'Babelfish Translation';
+$GLOBALS["tCloseWindow"] = 'Close window';
+
+//  About Texts
+$GLOBALS["tAbout"] = 'About iziContents '.$GLOBALS["Version"];
+$GLOBALS["tAboutez"] = 'Since 2005 iziContents is an OpenSource CMS based on ezContents from VisualShapers<br>ezContents was created by Marek Lyczba, Mark Baker, Wilbert Claessens.';
+$GLOBALS["tViewEzContents"] = 'View iziContents';
+$GLOBALS["tWrittenBy"] = 'Written by';
+$GLOBALS["tAdditionalCode"] = 'Additional code by';
+$GLOBALS["tTranslatedBy"] = 'Language translations';
+$GLOBALS["tBetaTestLead"] = 'QA Lead';
+$GLOBALS["tBetaTesters"] = 'With thanks to our beta testers';
+$GLOBALS["tBetaTesterThanks"] = 'and everybody else who has supported the development of this system.';
+$GLOBALS["tDeleteInstalDir"] = 'The izi_install directory has not been deleted yet, do this for security issues NOW.';
+$GLOBALS["tDeleteInstalDirButton"] = 'Delete izi_install directory';
+
+$GLOBALS["tModules"] = 'Modules';
+$GLOBALS["tVersion"] = 'Version';
+
+$GLOBALS["tDBClient"] = 'Client';
+$GLOBALS["tDBServer"] = 'Server';
+
+$GLOBALS["tRegisteredTo"] = 'Registered to';
+$GLOBALS["tRegister"] = 'Enter Registration Details';
+$GLOBALS["tUnregistered"] = 'Unregistered Copy';
+$GLOBALS["tLicenseExpired"] = 'LICENSE EXPIRED';
+$GLOBALS["tRenewLicense"] = 'Renew your Registration';
+
+$GLOBALS["tRegistrationHash"] = 'Registration Hash Code';
+$GLOBALS["tRegistrationBlock"] = 'Registration Block';
+$GLOBALS["tRegistrationDate"] = 'License Expiration Date';
+$GLOBALS["hRegistrationHash"] = 'Enter the Hash Code that you were given as part of your registration.<br /><b>Note:-</b> This value is case sensitive.';
+$GLOBALS["hRegistrationBlock"] = 'Enter the Registration Block that you were given as part of your registration exactly as it was sent, including the <-- BEGIN REGISTRATION BLOCK --> and <-- END REGISTRATION BLOCK --> tags.';
+$GLOBALS["hRegistrationDate"] = 'Enter the date when your license is scheduled to expire';
+
+
+//  Submitted Content
+$GLOBALS["tThereIs"] = 'There is';
+$GLOBALS["tThereAre"] = 'There are';
+$GLOBALS["tEntryPending"] = 'entry pending';
+$GLOBALS["tEntriesPending"] = 'entries pending';
+$GLOBALS["tRet_SubContent"] = 'Return to Submitted Content module list';
+
+// Files
+$GLOBALS["tFreeSpace"] = 'Free Space';
+$GLOBALS["eSafeModeWarning"] = '*** WARNING: PHP \'Safe Mode\' restriction may affect functionality ***';
+
+// Browser/Connection Details
+$GLOBALS["Browser"] = 'Browser';
+$GLOBALS["tProxyServer"] = 'Proxy Server';
+$GLOBALS["tProxyPort"] = 'Port';
+$GLOBALS["tProxyUserID"] = 'Proxy User ID';
+$GLOBALS["tProxyPassword"] = 'Password';
+
+$GLOBALS["tUnSupported"] = 'Unsupported';
+
+?>
