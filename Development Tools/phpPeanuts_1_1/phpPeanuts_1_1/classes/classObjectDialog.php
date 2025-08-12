3b883e19aa5c2889b4d619c56d32a2c4d966e482
me-suzy <me.suzana@gmail.com>
2025-08-12 10:03:05 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/classObjectDialog.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/classObjectDialog.php
@@ -0,0 +1,23 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+includeClass('PntObjectDialog', 'pnt/web/dialogs');
+
+/** Dialog with FilterFormPart for searching and selecting an object.
+* Paging buttons are created by a PntPagerButtonsListBuilder, 
+* whose classfolder is pnt/web/helpers.
+*
+* This concrete subclass is here to keep de application developers
+* code (including localization overrides) separated from the framework code.
+* @see http://www.phppeanuts.org/site/index_php/Menu/178
+* Framework code is in superclass. 
+* This class may be copied to an application folder to
+* make application specific overrides.
+* @see http://www.phppeanuts.org/site/index_php/Pagina/66
+*/
+class ObjectDialog extends PntObjectDialog {
+
+
+}	
+?>
