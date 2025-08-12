fbcb6c63c546197d2f1f2bc218c8192c06b2da07
me-suzy <me.suzana@gmail.com>
2025-08-12 21:32:42 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/artistlink.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/artistlink.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$tpl->assign("lang_head_artlink", "Add An Artist To Comic");
+$tpl->assign("lang_comic_info", "Comic Info");
+$tpl->assign("lang_add_artist", "Add");
+
+$tpl->assign("lang_comic_name", "Comic title and issue number");
+$tpl->assign("lang_comic_story", "Story title");
+$tpl->assign("lang_select_artist", "Select an artist");
+
+?>
