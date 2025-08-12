5b8e7dfc6bb6c27fa0bf85e0a4e72319a36b6ce4
me-suzy <me.suzana@gmail.com>
2025-08-12 19:50:58 +0300

    Add News Publishing/NewMan147/NewMan147/newman/languages/lang-danish.php

---
+++ News Publishing/NewMan147/NewMan147/newman/languages/lang-danish.php
@@ -0,0 +1,120 @@
+<?php
+  //******************************************************************************************
+  //**                                                                                      **
+  //** phpNewsManager v1.35                                                                 **
+  //** contact: gregor@klevze.si                                                            **
+  //** Last edited: 15th.June,2002                                                          **
+  //**                                                                                      **
+  //** Language module: Danish
+  //** by linuxnerd@ofir.dk, Michael Danaa                                                             **
+  //******************************************************************************************
+
+define("_CHARSET","ISO-8859-1");
+define("_STATISTICS","Statestik");
+define("_ACTUAL","Faktisk");
+define("_LASTVISIT","Sidste besøg");
+define("_NEW","Ny");
+define("_ONLINENEWS","Nyheder Online");
+define("_WEEKLYPOLLQ","ugens spørgsmål");		//** could be argued 
+define("_WEEKLYPOLLA","Ugens svar");		//** could be argued
+define("_PUBLICNEWS","Publicér Nyheder");		//** could be argued 
+define("_REGISTEREDUSERS","Registrerede brugere");
+define("_NEWSCOMMENTS","Nyhedskommentarer");
+define("_ADDNEWS","Tilføj nyheder");
+define("_SUBMITEDNEWS","Indsendte nyheder");
+define("_OPTIONS","Valgmuligheder");
+define("_NEWSHEADLINE","Hovednyhed");
+define("_AUTHOR","Forfatter");
+define("_DATE","Dato");
+define("_SECTION","Afdelning");
+define("_PREVIEW","Preview");
+define("_MESSAGE","Meddelse");
+define("_SUBMIT","Send ind");
+define("_HOME","Hjem");
+define("_NEWSMANAGER","Nyhedsmenu");
+define("_MODIFYNEWS","Ændre Nyhed");
+define("_ADDNEWS","Tilføj Nyhed");
+define("_ADDPARTNERS","Tilføj Partner");
+define("_UPLOADLOGO","Upload Logo");
+define("_IMAGE","Billede");
+define("_NAME","Navn");
+define("_NUMBER","#");define("_","#");
+define("_CATEGORYNAME","Kategorinavn");
+define("_MAINMENU","Hovedmenu");
+define("_KEEPIT","Gem!");
+define("_DELETEIT","Slet!");
+define("_EDITCATEGORY","Ændre Kategori");
+define("_UPLOADPICTURE","Upload billede");
+define("_ADDCATEGORY","Tilføj kategori");
+define("_OURPARTNERS","Vores Partners");
+define("_SELECTPAGE","Vælg side");
+define("_LINK","Link");
+define("_IN","Ind");
+define("_OUT","Ud");
+define("_AFFILIATES","Affiliates");
+define("_GFX","Gfx");
+define("_PREVIOUS","Foregående");
+define("_NEXT","Næste");
+define("_DESCRIPTION","Beskrivelse");
+define("_MODIFYPARTNERS","Ændre Partners");
+define("_RSSSETTINGS","RSS Indstillninger");
+define("_NAMEOFRSSFILE","RSS filens navn");
+define("_NUMBEROFNEWS","antal nyheder");
+define("_RSSTITLE","RSS Titel");
+define("_RSSLINK","RSS Link");
+define("_RSSDESCRIPTION","RSS Beskrivelse");
+define("_AUTOCREATERSS","Opret en  RSS fil automatiskt efter hver nyhedsændring");
+define("_OPTIMIZED","Optimered");
+define("_OPTIMIZEDATABASE","Optimere Databasen");
+define("_ADDSMILEY","Tilføj Smiley");
+define("_UPLOADSMILEY","Upload Smiley");
+define("_SMILEYEMOTION","Følelse");
+define("_SMILEYEDITOR","Smiley Editor");
+define("_SMILEYCODE","Kode");
+define("_EDITSMILEY","Ændre Smiley");
+define("_DELETESMILEY","Fjern Smiley");
+define("_NEWS","Nyheder");
+define("_CATEGORY","Kategori");
+define("_PARTNERS","Partners");
+define("_WEEKLYPOLL","Ungentlig Poll");
+define("_SMILEYS","Smileys");
+define("_BROWSENEWS","Gennemse Nyheder");
+define("_GROUPS","Grupper");
+define("_ADMINS","Admins");
+define("_USERS","Brugere");
+define("_USERINFO","Bruger info");
+define("_ADMINISTRATION","Administration");
+define("_YOULOGOUT","Du har netop logget ud.");
+define("_LOGIN","Login");
+define("_PASSWORD","Password");
+define("_USERNAME","Brugernavn");
+define("_WEBCONTROLPANEL","WEB KONTROL PANEL");
+define("_ADDUSER","Tilføj Bruger");
+define("_EMAIL","Email");
+define("_PRIVILEGES","Privileges");
+define("_EMAIL","Email");
+define("_EDITUSER","Ændre bruger");
+define("_DELETEUSER","Slet User");
+define("_INFO","Info");
+define("_ADDGROUP","Tilføj Gruppe");
+define("_ADMIN","Admin");
+define("_RSS","RSS");
+define("_MODIFY","Ændre");
+define("_ADD","tilføj");
+define("_EDIT","Edit");
+define("_DELETE","Slet");
+define("_UPLOAD","Upload");
+define("_GROUPNAME","Gruppenavn");
+define("_SELECT","Vælg");
+define("_ADDWEEKLYPOLL","Tilføj Ugentlig Poll");
+define("_SUBMITEDPOLLS","tilføjede Polls");
+define("_QUESTION","Spørgsmål");
+define("_ANSWERS","Svar");
+define("_SEPERATEANSWERSWITH","Adskild spørgsmål med");
+define("_EDITWEEKLYPOLL","Edit ugentlig Poll");
+define("_SUCCESS","Success");
+define("_DELETENEWS","slet Nyheder");
+define("_NOTENOUGHPRIV","Du har ikke tilstrækkeligt med permissions!");   //** could be argued
+define("_LANGUAGE","sprog");
+define("_CONTENT","Indhold");
+?>
