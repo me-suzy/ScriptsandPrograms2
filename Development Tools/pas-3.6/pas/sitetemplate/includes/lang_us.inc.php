abd36755b21edbcc9f870580eb1ce1c224848992
me-suzy <me.suzana@gmail.com>
2025-08-12 10:00:13 +0300

    Add Development Tools/pas-3.6/pas/sitetemplate/includes/lang_us.inc.php

---
+++ Development Tools/pas-3.6/pas/sitetemplate/includes/lang_us.inc.php
@@ -0,0 +1,40 @@
+<?php 
+/* Language File
+ */
+$strWelcome = "Welcome to this new application and thanks for using MyDB 2 Framework." ;
+
+// add record event:
+$strInsertError = "Error while inserting the record ";
+$strInsertOk = "The record has been inserted " ;
+$strAddCancel = "The insertion of the record as been canceled"; 
+$strCancel = "Cancel";
+
+// mydb.check Email
+$strWrongEmail = "The email you entered doesn't exist" ;
+
+// mydb.check Required
+$strRequiredField = "You must fill in all the fields that are required." ;
+
+// mydb.check Username Password
+$strMissingField = "You need a login and password in the form " ;
+$strErrorPasswordNotMatch = "The passwords doesn't mach" ;
+$strErrorLoginAlreadyUsed = "The username is already in use" ;
+
+// mydb Format pricture field 
+$strUnabletoSave = "Was unable to save the file :";
+
+// mydb format time field
+$strWrongTimeFormat = "Wrong time format should be : HH:MM:SS with hours < 24, minutes < 60 and seconds < 60";
+ 
+// mydb manage record
+$strNoActionsFound = "No Action found" ;
+$strMissingArgument = "Error coudn't add the record, missing arguments" ;
+$strConfirm = "Are you sure you want to delete the record" ;
+$strYes = "Yes" ;
+$strNo = "No" ;
+
+// mydb table update record
+ $strInsertError = "An error occured while updating the record";
+ $strUpdateOk = "The record has been updated" ;
+ $strCancel = "Cancel";
+ $strUpdateCancel = "The Update of the record as been canceled"; 
