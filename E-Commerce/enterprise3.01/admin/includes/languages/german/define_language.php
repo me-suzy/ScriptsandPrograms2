ea8b6ec1b788667e7a341226207b2cf7fb9a7944
me-suzy <me.suzana@gmail.com>
2025-08-12 11:45:49 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/german/define_language.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/german/define_language.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Sprache definieren');
+
+define('TEXT_FILE_DOES_NOT_EXIST', 'Datei nicht vorhanden.');
+
+define('ERROR_FILE_NOT_WRITEABLE', 'Fehler: Die Datei ist schreibgesch&uuml;tzt. Bitte korrigieren Sie die Zugriffsrechte f&uuml;r: %s');
+?>
