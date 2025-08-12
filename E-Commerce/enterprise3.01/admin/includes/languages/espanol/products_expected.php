24d1fd801e7cea8955867fb1a6a78a4b8dd54389
me-suzy <me.suzana@gmail.com>
2025-08-12 11:44:44 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/espanol/products_expected.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/espanol/products_expected.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Pr&oacute;ximamente');
+
+define('TABLE_HEADING_PRODUCTS', 'Productos');
+define('TABLE_HEADING_DATE_EXPECTED', 'Fecha Lanzamiento');
+define('TABLE_HEADING_ACTION', 'Acci&oacute;n');
+
+define('TEXT_INFO_DATE_EXPECTED', 'Fecha Lanzamiento:');
+?>
