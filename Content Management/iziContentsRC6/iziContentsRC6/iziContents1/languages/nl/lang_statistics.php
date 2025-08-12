9f523324647f1acc44a9aa4672e907fe2c5fca3c
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:03 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_statistics.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_statistics.php
@@ -0,0 +1,57 @@
+<?php
+
+// versie 0.2 27-9-2003 door Jan Hof
+
+//  Form Titles
+$GLOBALS["tFormTitle"]  = 'Bezoekers statistieken website';
+$GLOBALS["tFormTitle0"] = 'Overzicht';
+$GLOBALS["tFormTitle1"] = 'Wanneer';
+$GLOBALS["tFormTitle2"] = 'Wie';
+$GLOBALS["tFormTitle3"] = 'Waar';
+$GLOBALS["tFormTitle4"] = 'Hoe';
+
+//  Headings
+$GLOBALS["tVisitors"] 	='bezoekers';
+$GLOBALS["tHits"] 	= 'Hits';
+$GLOBALS["tPercentage"] = 'Percentage';
+$GLOBALS["tReferer"] 	= 'Kwam van';
+$GLOBALS["stBrowser"] 	= 'Browser';
+$GLOBALS["stOS"] 		= 'Operating Systeem';
+$GLOBALS["tpToday"] 	= 'Vandaag';
+$GLOBALS["tpThisWeek"] 	= 'Deze week';
+$GLOBALS["tpThisMonth"] = 'Deze maand';
+$GLOBALS["tpThisYear"] 	= 'Dit jaar';
+$GLOBALS["tpAllTime"] 	= 'Alles'; 
+
+//  Summary Page
+$GLOBALS["stStartDate"] = 'Gemeten sinds';
+$GLOBALS["stTodayCount"] = 'Vandaag waren er';
+$GLOBALS["stTotalCount"] = 'Totaal aantal bezoekers';
+$GLOBALS["stAverageCount"] = 'Gemiddeld aantal bezoekers per dag';
+$GLOBALS["stTopCount"] = 'De dag met het hoogst aantal bezoekers was';
+$GLOBALS["tWith"] = 'met';
+
+//  When Page
+$GLOBALS["otHour"] = 'Uur';
+$GLOBALS["otHours"] = 'Uren';
+$GLOBALS["otWeekday"] = 'Weekdag';
+$GLOBALS["otWeekdays"] = 'Weekdagen';
+$GLOBALS["otDate"] = 'Datum';
+$GLOBALS["otDates"] = 'Datums';
+$GLOBALS["otWeek"] = 'Week';
+$GLOBALS["otWeeks"] = 'Weken';
+$GLOBALS["otMonth"] = 'Maand';
+$GLOBALS["otMonths"] = 'Maanden';
+$GLOBALS["otYear"] = 'Jaar';
+$GLOBALS["otYears"] = 'Jaren';
+
+//  Who Page
+$GLOBALS["tDateTime"] = 'Datum/Tijd';
+$GLOBALS["tCountry"] = 'Land';
+
+//  Where Page
+
+//  How Page
+$GLOBALS["stAgent"] = 'HTTP User Agent';
+
+?>
