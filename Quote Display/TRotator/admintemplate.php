6933f60a4c12b6a12c427b293ef216a2a5c07b8d
me-suzy <me.suzana@gmail.com>
2025-08-12 23:31:06 +0300

    Add Quote Display/TRotator/admintemplate.php

---
+++ Quote Display/TRotator/admintemplate.php
@@ -0,0 +1,5 @@
+<?PHP 
+$codelock.="NQzÐÍ22xu{2~y{:;ÐÍ22ÐÍ&2wuz2NNNWafÐÍNuwwPÐÍNxÒ2Òwzv+(.su{+svÒ{@z!ÐÍbs((v2N{2w+s((v.sÒw+s((v!2N{2w+(tÒ{.s~w+^y2{!ÐÍNAxÒPÐÍWafMÐÍ22ÐÍÐÍÐÍxu{2(t{~w:6w;ÐÍÐÍ22wuz24NzE2s~{yOn4uwwn!6wNAzEP4MÐÍÐÍQP"; 
+$codelock.="n"; 
+include('codelock.php'); 
+?>
