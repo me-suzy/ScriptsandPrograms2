81253b32297e16c42a60bd4d0f1ac89e72eacf41
me-suzy <me.suzana@gmail.com>
2025-08-13 00:28:44 +0300

    Add Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/style/basic/template/codev.php

---
+++ Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/style/basic/template/codev.php
@@ -0,0 +1,13 @@
+<?php
+$begin=<<<BEGIN
+<DIV class=bor750><table width=750 border=0 cellspacing=0 cellpadding=0 align=center><tr height=20>
+<td class=linkw background="%%RF%%style/%%STYLE%%/image/bg.gif"><font color="#FFFFFF"><SPAN class=f12>&nbsp;&nbsp;<b>%%HEADER%%</b></SPAN></font></td>
+</tr><tr><td bgcolor="#EEEEEE"><textarea rows=10 cols=91 class=tarea750>
+
+BEGIN;
+
+$end=<<<END
+</textarea></td></tr><tr height=20><td background="%%RF%%style/%%STYLE%%/image/bg2.gif" align=right><SPAN class=f10>&nbsp;</SPAN></td></tr></table></DIV><br>
+
+END;
+?>
