785c2e02f1bc7dbc45be2ea062868f8cdd5d8878
me-suzy <me.suzana@gmail.com>
2025-08-12 22:55:00 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Reviews/language/lang-czech.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Reviews/language/lang-czech.php
@@ -0,0 +1,99 @@
+<?php
+
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+
+define("_PREVIOUS","Pøedchozí stránka");
+define("_NEXT","Dalí stránka");
+define("_YOURNAME","Vae jméno");
+define("_SORTASC","Tøídit vzestupnì");
+define("_SORTDESC","Tøídit sestupnì");
+define("_CANCEL","Cancel");
+define("_YES","Ano");
+define("_NO","Ne");
+define("_SCORE","Skóre:");
+define("_REPLYMAIN","Poslat komentáø");
+define("_ALLOWEDHTML","Povolit HTML:");
+define("_POSTANON","Poslat anonymnì");
+define("_WRITEREVIEW","Napite recenzi");
+define("_WRITEREVIEWFOR","Napite recenzi pro");
+define("_ENTERINFO","Napite, prosím, informace v souladu se skuteèností.");
+define("_PRODUCTTITLE","Název produktu");
+define("_NAMEPRODUCT","Název recenzovaného produktu.");
+define("_REVIEW","Recenze");
+define("_CHECKREVIEW","Vá aktuální názor. Zkuste napsat alespoò 100 slov. Pokud ovládáte HTML, mùete pouít i HTML tagy.");
+define("_FULLNAMEREQ","Vae celé jméno. Vyadováno.");
+define("_REMAIL","Vá e-mail");
+define("_REMAILREQ","Vá e-mail. Vyadováno.");
+define("_SELECTSCORE","Hodnocení produktu (10 je nejlepí 1 nejhorí");
+define("_RELATEDLINK","Související odkaz");
+define("_PRODUCTSITE","Oficiální stránka produktu. Ujistìte se, e URL zaèíná \"http://\"");
+define("_LINKTITLE","Název odkazu");
+define("_LINKTITLEREQ","Vyadováno pouze pokud je zadán související odkaz, v opaèném pøípadì není tøeba vyplòovat.");
+define("_RIMAGEFILE","Obrázkový soubor");
+define("_RIMAGEFILEREQ","Jméno obrázkového souboru, musí být uloen v adresáøi images/reviews/. Není vyadováno.");
+define("_CHECKINFO","Ujistìte se prosím, e jsou zadané informace 100% platné a e jste napsali správnì adresy, jména a velká písmena. Nepouívejte, prosím, k napsání recenze pouze VELKÁ PÍSMENA, taková recenze bude vyøazena.");
+define("_INVALIDTITLE","Není zadán název... Pole nesmí zùstat nevyplnìno");
+define("_INVALIDSCORE","Neplatné hodnocení... Musí být od 1 do 10");
+define("_INVALIDTEXT","Není zadán text recenze... Pole nesmí zùstat nevyplnìno");
+define("_INVALIDHITS","Hits must be a positive integer");
+define("_CHECKNAME","Musíte zadat obojí, jméno i e-mail");
+define("_INVALIDEMAIL","Neplatný e-mail (napø: you@hotmail.com)");
+define("_INVALIDLINK","Musíte zadat obojí, název odkazu i související odkaz, nebo nechat obojí prázdné");
+define("_ADDED","Pøidáno:");
+define("_REVIEWER","Recenzent:");
+define("_REVIEWID","ID recenze");
+define("_HITS","Klinkutí");
+define("_LOOKSRIGHT","Je to tak správnì?");
+define("_RMODIFIED","upraveno");
+define("_RADDED","pøidána");
+define("_ADMINLOGGED","Jste pøihláen jako admin... recenze bude okamitì");
+define("_RTHANKS","Díky za odeslání recenze");
+define("_MODIFICATION","úpravy");
+define("_ISAVAILABLE","Nyní je zaøazena do databáze recenzí.");
+define("_EDITORWILLLOOK","Administrátor prohlédne vá pøíspìvek.");
+define("_RBACK","Zpìt na seznam recenzí");
+define("_RWELCOME","Sekce recenzí");
+define("_10MOSTPOP","10 nejètenìjích recenzí");
+define("_10MOSTREC","10 nejaktuálnìjích recenzí");
+define("_THEREARE","V souèasné dobì je");
+define("_REVIEWSINDB","recenzí v databázi");
+define("_REVIEWS","Recenze");
+define("_REVIEWSLETTER","Recenze produktù zaèínajících na");
+define("_NOREVIEWS","Neexistuje ádná recenze produktu zaèínajícího na");
+define("_TOTALREVIEWS","Vechny nalezené recenze.");
+define("_RETURN2MAIN","Zpìt na seznam recenzí");
+define("_REVIEWCOMMENT","Komentáø k recenzi:");
+define("_YOURNICK","Vae pøezdívka:");
+define("_RCREATEACCOUNT","<a href=modules.php?name=Your_Account>Vytvoøit</a> úèet");
+define("_YOURCOMMENT","Vá komentáø:");
+define("_MYSCORE","Moje hodnocení:");
+define("_REVIEWMOD","Úprava recenze");
+define("_RDATE","Datum:");
+define("_RTITLE","Název produktu:");
+define("_RTEXT","Text:");
+define("_REVEMAIL","Email:");
+define("_RLINK","Odkaz:");
+define("_RLINKTITLE","Název odkazu:");
+define("_COVERIMAGE","Obrázek:");
+define("_PREMODS","Prohlédnout úpravy");
+define("_PAGE","Stránka");
+define("_PAGEBREAK","Pokud chcete napsat vícestupòovou stránku pouijte <b>&lt;!--pagebreak--&gt;</b> v místì, kde ji chcete zalomit.");
+define("_PREVIEW","Prohlédnout");
+define("_LANGUAGE","Language");
+
+?>
