f768f49131593dc6f89d91e8aa1cea7b2ae53cb5
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:53 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_subcontent.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_subcontent.php
@@ -0,0 +1,80 @@
+<?php
+
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'Module verwalten';
+$GLOBALS["tFormTitle2"] = 'Konfiguration des Moduls';
+$GLOBALS["tFormTitle3"] = 'Kategorien des Moduls';
+
+//  List Headings
+$GLOBALS["tEdit"] = 'Ändern';
+$GLOBALS["tAddNew"] = 'Neues Modul hinzufügen';
+$GLOBALS["tAddNewCat"] = 'Neue Kategorie hinzufügen';
+$GLOBALS["tModuleName"] = 'Modulname';
+$GLOBALS["tModuleTitle"] = 'Modultitel';
+$GLOBALS["tAnonPosting"] = 'Anonyme Postings erlaubt';
+$GLOBALS["tValReq"] = '&Uuml;berpr&uuml;fung erforderlich';
+$GLOBALS["tTablename"] = 'Tabellenname';
+$GLOBALS["tCategory"] = 'Kategorie';
+$GLOBALS["tCatReference"] = 'Interne Referenz';
+
+//  List Functions
+$GLOBALS["tConfig"] = 'Einstellungen';
+$GLOBALS["tCategories"] = 'Kategorien';
+
+//  Form Block Titles
+$GLOBALS["thGeneral"] = 'Konfiguration von Modulen';
+$GLOBALS["thSubmissions"] = '&Uuml;bertragungsdetails';
+$GLOBALS["thSubmissionRules"] = '&Uuml;bertragungsregeln';
+$GLOBALS["thDisplay"] = 'Anzeige-Einstellungen';
+
+//  Form Field Headings
+$GLOBALS["tModuleID"] = 'Modul ID';
+$GLOBALS["tCatParent"] = 'Eltern Kategorie';
+$GLOBALS["tSText"] = '&Uuml;bertragungstext';
+$GLOBALS["tSTextDisplay"] = '&Uuml;bertragungstext anzeigen';
+$GLOBALS["tSGraphic"] = '&Uuml;bertragungstext Grafik';
+$GLOBALS["tSGraphicDisplay"] = '&Uuml;bertragungstext Grafik anzeigen';
+$GLOBALS["tRegOnly"] = 'Nur f&uuml;r registrierte User';
+$GLOBALS["tUsergroups"] = 'Usergruppen';
+$GLOBALS["tValReg"] = '&Uuml;berpr&uuml;fung erforderlich';
+$GLOBALS["tUseCategories"] = 'Unterkategorien verwenden';
+$GLOBALS["tPerPage"] = 'pro Seite';
+$GLOBALS["tCatName"] = 'Kategoriename';
+$GLOBALS["tHiddenCat"] = 'versteckte Kategorie';
+$GLOBALS["tOrderBy"] = 'sortiert nach';
+$GLOBALS["tPostedBy"] = 'Anzeige gepostet von';
+$GLOBALS["tPostedDate"] = 'Anzeige Posting-Datum';
+
+// Toggel Modules
+$GLOBALS["tConfirmRelease"] = 'Module aktivieren / deaktivieren';
+$GLOBALS["tReleaseModule"] = 'Module aktivieren / deaktivieren';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Verwenden Sie dieses Formular, um die Konfiguration von User-Mitteilungen in die speziellen Inhaltsmodule (Add-Ins) vorzunehmen.';
+$GLOBALS["hModuleName"] = 'Der Modulname, ein Kurzbefehl, um es intern zu identifizieren.';
+$GLOBALS["hModuleTitle"] = 'Der Modultitel wird auf der Hauptseite des Inhalts f&uuml;r diesen Typ von Modul angezeigt.';
+$GLOBALS["hSText"] = 'Der Text f&uuml;r die Anzeige im Kopf von User-Mitteilungen.<br>Dieser Wert wird nur verwendet, wenn es keinen sprachspezifischen Text in den Sprachdateien des Moduls gibt.';
+$GLOBALS["hSTextDisplay"] = 'Soll &uuml;bertragener Text (entweder aus den Sprachdateien, oder aus dem  \'Display Submission Text\' Feld in diesem Formular) im Kopf angezeigt werden?';
+$GLOBALS["hSGraphic"] = 'Bild f&uuml;r den Kopf von User-Mitteilungen';
+$GLOBALS["hSGraphicDisplay"] = 'Soll die Mitteilungsgrafik im Kopf angezeigt werden?';
+$GLOBALS["hRegOnly"] = 'Sollen nur registrierte Autoren/User Eintr&auml;ge vornehmen k&ouml;nnen? Oder alle Besucher?';
+$GLOBALS["hUsergroups"] = 'Es gibt registrierte User mit Privilegien, um Eintr&auml;ge vorzunehmen. Diese User sind in den hervorgehobenen Usergruppen.<br />Lassen Sie dies leer, wenn alle registrierten User Mitteilungen &uuml;bertragen d&uuml;rfen.';
+$GLOBALS["hValReq"] = 'Hier wird angezeigt, ob eine &Uuml;berpr&uuml;fung ben&ouml;tigt wird, bevor &uuml;bertragene Inhalte ver&ouml;ffentlicht werden, bzw. ob sie auf die Freigabe durch den Administrator warten.';
+$GLOBALS["hPerPage"] = 'Die Anzahl der Eintr&auml;ge, die pro Seite angezeigt wird.';
+$GLOBALS["hUseCategories"] = 'Wenn diese Markierung gesetzt ist, werden die von Usern &uuml;bermittelten Artikel/Texte mit einer Reihe von Unterkategorien verbunden, die der Administrator definieren kann.';
+$GLOBALS["hOrderBy"] = 'Eintr&auml;ge nach Datum oder Kategorie/Datum sortieren (wenn Kategorien verwendet werden).';
+$GLOBALS["hPostedBy"] = 'Den Usernamen anzeigen, der diesen Eintrag erstellt hat';
+$GLOBALS["hPostedDate"] = 'Zeige Datum/Zeit von diesem Eintrag';
+$GLOBALS["hCatName"] = 'Geben Sie bitte eine Textbeschreibung zu dieser Kategorie an.';
+$GLOBALS["hCatParent"] = 'W&auml;hlen Sie bitte eine \'Oberkategorie\' aus der Liste, wenn Sie diese Kategorie zu einer \'Unterkategorie\' machen wollen.';
+$GLOBALS["hHiddenCat"] = 'Setzen Sie hier einen Haken auf \"Ja\", wenn diese Kategorie (und alle dazugeh&ouml;renden Unterkategorien) nicht in der User Liste f&uuml;r Kategorien angezeigt werden soll';
+$GLOBALS["toCategory"] = 'Kategorie/Datum';
+$GLOBALS["toDate"] = 'Datum';
+
+//  Error messages
+$GLOBALS["eNoModules"] = 'Es sind keine Module zur Installation zur Verf&uuml;gung.';
+$GLOBALS["ePerPageNumber"] = 'Der Wert Pro Seite muss eine Zahl sein';
+$GLOBALS["ePerPageZero"] = 'Der Wert Pro Seite muss gr&ouml;sser als 0 sein';
+
+?>
