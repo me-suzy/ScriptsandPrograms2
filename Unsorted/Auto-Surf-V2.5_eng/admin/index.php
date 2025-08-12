b9b6d35c5536d1743ab1fe114637ec8b3c9bb493
me-suzy <me.suzana@gmail.com>
2025-08-13 00:54:10 +0300

    Add Unsorted/Auto-Surf-V2.5_eng/admin/index.php

---
+++ Unsorted/Auto-Surf-V2.5_eng/admin/index.php
@@ -0,0 +1,7 @@
+<?
+include("../templates/admin-header.txt");
+?>
+<center><font size="3" color="red">Welcome to your admin menu!<br>Please select action in the menu bar on the left.
+<?
+include("../templates/admin-footer.txt");
+?>
