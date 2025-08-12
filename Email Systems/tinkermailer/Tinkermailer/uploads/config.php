6bf1c8ca8397f04489b511173c9a1b7b87222e25
me-suzy <me.suzana@gmail.com>
2025-08-12 13:53:27 +0300

    Add Email Systems/tinkermailer/Tinkermailer/uploads/config.php

---
+++ Email Systems/tinkermailer/Tinkermailer/uploads/config.php
@@ -0,0 +1,8 @@
+<?php
+// Edit the Following to fit your needs
+$youremail='your email here';
+$headers = "From: $name <$email>\n";
+$subject= 'subject Line'; 
+// Do not Edit
+$message = "$comments, $siteurl, $sitename";
+?>
