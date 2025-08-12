62f741458d9eb51297b79576554ff2aefc31daba
me-suzy <me.suzana@gmail.com>
2025-08-12 06:00:44 +0300

    Add Content Management/metajour2_1/metajour/app/erek/erek_comp_model_caseopen.php

---
+++ Content Management/metajour2_1/metajour/app/erek/erek_comp_model_caseopen.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eReklamation
+ * @subpackage view
+ * $Id: erek_comp_model_caseopen.php,v 1.4 2005/03/23 04:27:25 jan Exp $
+ */
+
+global $system_path;
+require_once($system_path.'basic_model.php');
+
+class erek_comp_model_caseopen extends basic_model {
+
+	function model() {
+		foreach($this->objectid as $curid) {
+			$obj = owRead($curid);
+			$obj->updateObject(array('status' => CASE_OPEN));
+		}
+	}
+	
+}
+
+?>
