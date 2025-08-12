3877c54d2038ad57c84c6f2fbc43dae38c911bf8
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:12 +0300

    Add Content Management/metajour2_1/metajour/lang/customform.da.php

---
+++ Content Management/metajour2_1/metajour/lang/customform.da.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Blanketter';
+$LANG['label_name'] = 'Navn (a-z,fast)';
+$LANG['label_description'] = 'Beskrivende tekst';
+$LANG['label_eventto'] = 'Hændelsesmail til';
+$LANG['label_createevent'] = 'Opret hændelse';
+$LANG['label_createtemplate'] = 'Opret skabelon';
+$LANG['label_createdocument'] = 'Opret dokument';
+$LANG['label_uploadpageid'] = 'Upload side';
+?>
