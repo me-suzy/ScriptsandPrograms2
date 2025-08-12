46d4eb508189b97757008a565feea7e43a206412
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:39 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_privileges.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_privileges.php
@@ -0,0 +1,37 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintain privilege groups';
+
+//  List Headings
+$GLOBALS["tUsergroupname"] = 'Usergroup name';
+$GLOBALS["tDefaultGroup"] = 'Default group';
+$GLOBALS["tAdminGroup"] = 'Administrator group';
+
+//  List Functions
+$GLOBALS["tViewPrivilege"] = 'View usergroup';
+$GLOBALS["tAddNewPrivilege"] = 'Add new usergroup';
+$GLOBALS["tEditPrivilege"] = 'Edit usergroup';
+$GLOBALS["tDeletePrivilege"] = 'Delete usergroup';
+$GLOBALS["tMakeDefault"] = 'Make default group for new users';
+$GLOBALS["tDefaultSet"] = 'Default privilege group for new users';
+$GLOBALS["tMakeAdmin"] = 'Make admininstrator group';
+$GLOBALS["tAdminSet"] = 'Administrator privilege group';
+
+//  Form Field Headings
+$GLOBALS["tPrivilegesFor"] = 'Privileges for usergroup code';
+$GLOBALS["tView"] = 'View';
+$GLOBALS["tEdit"] = 'Edit';
+$GLOBALS["tAdd"] = 'Add';
+$GLOBALS["tDelete"] = 'Delete';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'This form lets you edit or create new usergroups.';
+
+//  Error Messages
+$GLOBALS["eNoUsergroupCode"] = 'You must enter a usergroup code.';
+$GLOBALS["eNoUsergroupName"] = 'You must enter a usergroup name.';
+$GLOBALS["eCodeInUse"] = 'This privilege group already exists.';
+$GLOBALS["eUsersExist"] = 'Cannot delete because the following users are still assigned to this privilege group:';
+
+?>
