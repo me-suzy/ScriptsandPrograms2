02201c938b7780ff06995a01363cd56d96134515
me-suzy <me.suzana@gmail.com>
2025-08-12 11:56:18 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_terms.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_terms.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Affiliate Program');
+define('HEADING_TITLE', 'Affiliate Program');
+
+define('HEADING_AFFILIATE_PROGRAM_TITLE', 'The ' . STORE_NAME . ' Affiliate Terms');
+define('TEXT_INFORMATION', 'Ponga aqui sus condiciones de Affiliate.');
+?>
