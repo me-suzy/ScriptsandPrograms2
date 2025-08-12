7076e21bbfbc5de2b2066e6f6d24cc1a5f795186
me-suzy <me.suzana@gmail.com>
2025-08-12 10:48:45 +0300

    Add Discussion Boards/scssboard-1.12/scssboard/functions/global/bbcode_table.php

---
+++ Discussion Boards/scssboard-1.12/scssboard/functions/global/bbcode_table.php
@@ -0,0 +1,11 @@
+<br /><br /> <br /><br />
+<b>BBCode</b><br /><br />
+
+[b] <strong>Strong</strong> [/b]<br />
+[i] <em>Emphasis</em> [/i]<br />
+[u] <u>Underline</u> [/u]<br /><br />
+
+[url=#] <a href='http://www.google.com' target='_blank'>Link</a> [/url]<br />
+[img] Image [/img]<br /><br />
+
+[quote] Quote [/quote]
