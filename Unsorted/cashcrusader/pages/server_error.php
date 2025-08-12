c99723480795f44327238772d72cd1f516dcc3ce
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:28 +0300

    Add Unsorted/cashcrusader/pages/server_error.php

---
+++ Unsorted/cashcrusader/pages/server_error.php
@@ -0,0 +1,17 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr> 
+<td align="center"> 
+<center>
+<b><font size="4"><br>
+<br>
+</font></b><font size="4">The MySQL server is off line<br>
+Please try later</font>.<br>
+<br>
+</center>
+</td>
+</tr>
+</table>
+<br>
+<? include("footer.php");?>
