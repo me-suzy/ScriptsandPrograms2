5f4926c4dbdedab4cdbf382f34ee580a826e7a24
me-suzy <me.suzana@gmail.com>
2025-08-12 08:21:46 +0300

    Add Content Management/wizard/wizard/admin/user/userEditDelete.php

---
+++ Content Management/wizard/wizard/admin/user/userEditDelete.php
@@ -0,0 +1,42 @@
+<?php
+/*  
+    User Delete Processor
+    (c) 2005 Philip Shaddock, www.wizardinteractive.com
+	This file is part of the Wizard Site Framework.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    It is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with the Wizard Site Framework; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+include '../../inc/config_cms/configuration.php';	
+include '../../inc/db/db.php';
+
+
+$uid = $_GET['uid']; 
+
+	
+	$db = new DB();
+	$db->query("DELETE FROM ". DB_PREPEND . "users WHERE uid='$uid' ");
+	
+	$message = "User was deleted.";
+	//delete users group membership
+	$db = new DB();
+	$db->query("DELETE FROM ". DB_PREPEND . "groupusers WHERE uid='$uid' ");
+	
+	
+$location = CMS_WWW . "/admin.php?id=2&item=27&sub=28&message=$message";
+header("Location: $location");
+
+
