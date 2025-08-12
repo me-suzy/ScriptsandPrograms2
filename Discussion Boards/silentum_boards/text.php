7c52c8b7a75d93f22ed38bffc526349afd7dd621
me-suzy <me.suzana@gmail.com>
2025-08-12 10:52:16 +0300

    Add Discussion Boards/silentum_boards/text.php

---
+++ Discussion Boards/silentum_boards/text.php
@@ -0,0 +1,127 @@
+<?
+	/*
+	Silentum Boards v1.4.3
+	text.php copyright 2005 "HyperSilence"
+	Modification of this page allowed as long as this notice stays intact
+	*/
+
+	$txt['Pages'] = "(Page %s)";
+
+	$txt['Links']['Board_Index'] = "Return to the %sBoard Index%s.";
+	$txt['Links']['Here'] = "%shere%s";
+	$txt['Links']['Moved_Topic'] = "Go to the %snew location%s of the topic.";
+	$txt['Links']['New_Topic'] = "Go to the %stopic%s.";
+	$txt['Links']['Profile'] = "Go back to your %sUser Preferences%s.";
+	$txt['Links']['Queue'] = "Return to the %sQueue%s.";
+	$txt['Links']['Register_Or_Login'] = "Proceed to %sregister%s or %slogin%s.";
+	$txt['Links']['Topic'] = "Return to the %stopic%s.";
+	$txt['Links']['Topic_Index'] = "Return to the %sTopic Index%s.";
+	$txt['Links']['User_Control_Panel'] = "Return to your %sUser CP%s.";
+	$txt['Links']['View_Profile'] = "View your %snew profile%s.";
+
+	$txt['Navigation']['Account_Closed'][0] = "Account Closed";
+	$txt['Navigation']['Account_Closed'][1] = "<strong>Your account has been closed.</strong>";
+	$txt['Navigation']['Already_Logged_In'][0] = "Already Logged In";
+	$txt['Navigation']['Already_Logged_In'][1] = "<strong>You must first logout if you want to register a new account.</strong>";
+	$txt['Navigation']['Already_Voted'][0] = "Already Voted";
+	$txt['Navigation']['Already_Voted'][1] = "<strong>You have already voted.</strong>";
+	$txt['Navigation']['Banned'][0] = "Error";
+	$txt['Navigation']['Banned'][1] = "<strong>Your account has been banned.</strong>";
+	$txt['Navigation']['Board_Does_Not_Exist'][0] = "Board Does Not Exist";
+	$txt['Navigation']['Board_Does_Not_Exist'][1] = "<strong>The board you are trying to reach does not exist. If you feel this is an error, contact the Host.</strong>";
+	$txt['Navigation']['Closed'][0] = "Error";
+	$txt['Navigation']['Closed'][1] = "<strong>Your account has been closed.</strong>";
+	$txt['Navigation']['Error'][0] = "Error";
+	$txt['Navigation']['Error'][1] = "<strong>An error has occurred.</strong>";
+	$txt['Navigation']['Exceeded_Post_Limit'][0] = "Error";
+	$txt['Navigation']['Exceeded_Post_Limit'][1] = "<strong>You have exceeded your post limit for your user level. Please wait until you gain more Karma.</strong>";
+	$txt['Navigation']['IP_Suspended'][0] = "Error";
+	$txt['Navigation']['IP_Suspended'][1] = "<strong>Your account has been IP suspended.</strong>";
+	$txt['Navigation']['Logged_In_To_Vote'][0] = "Not Logged In";
+	$txt['Navigation']['Logged_In_To_Vote'][1] = "<strong>You must be logged in to vote.</strong>";
+	$txt['Navigation']['Max_Registrations'][0] = "Registration Limit Hit";
+	$txt['Navigation']['Max_Registrations'][1] = "<strong>The maximum number of registrations has been hit.</strong>";
+	$txt['Navigation']['Not_Available'][0] = "Not Available";
+	$txt['Navigation']['Not_Available'][1] = "<strong>This feature is not available.</strong>";
+	$txt['Navigation']['Not_Enough_Karma'][0] = "Error";
+	$txt['Navigation']['Not_Enough_Karma'][1] = "<strong>You do not have enough Karma.</strong>";
+	$txt['Navigation']['Not_Logged_In'][0] = "Not Logged In";
+	$txt['Navigation']['Not_Logged_In'][1] = "<strong>You are not logged in.</strong>";
+	$txt['Navigation']['Poll_Posted'][0] = "Poll Posted";
+	$txt['Navigation']['Poll_Posted'][1] = "<strong>The poll has been posted.</strong>";
+	$txt['Navigation']['Post_Deleted'][0] = "Post Deleted";
+	$txt['Navigation']['Post_Deleted'][1] = "<strong>The post has been deleted.</strong>";
+	$txt['Navigation']['Post_Does_Not_Exist'][0] = "Post Does Not Exist";
+	$txt['Navigation']['Post_Does_Not_Exist'][1] = "<strong>The post you are trying to reach does not exist. It may have been moved or deleted.</strong>";
+	$txt['Navigation']['Post_Edited'][0] = "Post Edited";
+	$txt['Navigation']['Post_Edited'][1] = "<strong>The post has been edited.</strong>";
+	$txt['Navigation']['Post_Removed'][0] = "Post Removed";
+	$txt['Navigation']['Post_Removed'][1] = "<strong>The post has been removed.</strong>";
+	$txt['Navigation']['Post_Reported'][0] = "Post Reported";
+	$txt['Navigation']['Post_Reported'][1] = "<strong>Thank you for reporting this post.</strong><br /><br />Please allow at least 24 hours for the appropriate action to be taken. Reporting a message more than once will <em>not</em> speed up the process.";
+	$txt['Navigation']['Registration_Deactivated'][0] = "Registration Deactivated";
+	$txt['Navigation']['Registration_Deactivated'][1] = "<strong>Registration has been deactivated.</strong>";
+	$txt['Navigation']['Reply_Posted'][0] = "Reply Posted";
+	$txt['Navigation']['Reply_Posted'][1] = "<strong>Your reply has been posted.</strong>";
+	$txt['Navigation']['Restricted'][0] = "Restricted";
+	$txt['Navigation']['Restricted'][1] = "<strong>This page is restricted.</strong>";
+	$txt['Navigation']['Restricted_Board'][0] = "Restricted";
+	$txt['Navigation']['Restricted_Board'][1] = "<strong>This board is restricted. You cannot view the topics on it.</strong>";
+	$txt['Navigation']['Suspended'][0] = "Error";
+	$txt['Navigation']['Suspended'][1] = "<strong>Your account has been suspended. Your account will be restored 72 hours after the suspension took place.</strong>";
+	$txt['Navigation']['Topic_Deleted'][0] = "Topic Deleted";
+	$txt['Navigation']['Topic_Deleted'][1] = "<strong>The topic has been deleted.</strong>";
+	$txt['Navigation']['Topic_Does_Not_Exist'][0] = "Topic Does Not Exist";
+	$txt['Navigation']['Topic_Does_Not_Exist'][1] = "<strong>The topic you are trying to reach does not exist. It may have been moved or deleted.</strong>";
+	$txt['Navigation']['Topic_Is_Locked'][0] = "Topic Locked";
+	$txt['Navigation']['Topic_Is_Locked'][1] = "<strong>The topic has been locked.</strong>";
+	$txt['Navigation']['Topic_Locked'][0] = "Topic Locked";
+	$txt['Navigation']['Topic_Locked'][1] = "<strong>This topic has been locked by a Moderator or an Administrator. No more replies can be posted.</strong>";
+	$txt['Navigation']['Topic_Moved'][0] = "Topic Moved";
+	$txt['Navigation']['Topic_Moved'][1] = "<strong>The topic has been moved.</strong>";
+	$txt['Navigation']['Topic_Posted'][0] = "Topic Posted";
+	$txt['Navigation']['Topic_Posted'][1] = "<strong>The topic has been posted.</strong>";
+	$txt['Navigation']['Topic_Unlocked'][0] = "Topic Unlocked";
+	$txt['Navigation']['Topic_Unlocked'][1] = "<strong>The topic has been unlocked.</strong>";
+	$txt['Navigation']['Unknown_User'][0] = "Unknown User";
+	$txt['Navigation']['Unknown_User'][1] = "<strong>This user does not exist.</strong>";
+	$txt['Navigation']['User_Preferences_Updated'][0] = "User Preferences Updated";
+	$txt['Navigation']['User_Preferences_Updated'][1] = "<strong>Your user preferences have been updated.</strong>";
+
+	$txt['Navigation']['All_Titles_Have_Rights'][0] = "Error";
+	$txt['Navigation']['All_Titles_Have_Rights'][1] = "<strong>For this board, all titles already have rights.</strong>";
+	$txt['Navigation']['No_Titles'][0] = "No Titles Defined";
+	$txt['Navigation']['No_Titles'][1] = "<strong>So far, no titles have been defined.</strong>";
+	$txt['Navigation']['Settings_Saved'][0] = "Settings Saved";
+	$txt['Navigation']['Settings_Saved'][1] = "<strong>The settings were successfully saved.</strong>";
+	$txt['Navigation']['Topics_Purged'][0]  = "Topics Purged";
+	$txt['Navigation']['Topics_Purged'][1]  = "<strong>Approximately %skb has been deleted.</strong>";
+	$txt['Navigation']['User_Edited'][0] = "User Edited";
+	$txt['Navigation']['User_Edited'][1] = "<strong>The user was successfully edited.</strong>";
+
+	$txt['Time_Zone'][0] = array("GMT-1200 - Cape Verde Islands","-1200");
+	$txt['Time_Zone'][1] = array("GMT-1100 - Niue, Samoa","-1100");
+	$txt['Time_Zone'][2] = array("GMT-1000 - Hawaii","-1000");
+	$txt['Time_Zone'][3] = array("GMT-0900 - Alaska","-0900");
+	$txt['Time_Zone'][4] = array("GMT-0800 - Pacific Time","-0800");
+	$txt['Time_Zone'][5] = array("GMT-0700 - Mountain Time","-0700");
+	$txt['Time_Zone'][6] = array("GMT-0600 - Central Time","-0600");
+	$txt['Time_Zone'][7] = array("GMT-0500 - Eastern Time","-0500");
+	$txt['Time_Zone'][8] = array("GMT-0400 - Atlantic Time (Canada)","-0400");
+	$txt['Time_Zone'][9] = array("GMT-0300 - Argentina, Brazil","-0300");
+	$txt['Time_Zone'][10] = array("GMT-0200 - South Georgia","-0200");
+	$txt['Time_Zone'][11] = array("GMT-0100 - Cape Verde, Portugal","-0100");
+	$txt['Time_Zone'][12] = array("GMT - Guinea, Iceland, Spain","+0000");
+	$txt['Time_Zone'][13] = array("GMT+0100 - Denmark, Poland, Sweden","+0100");
+	$txt['Time_Zone'][14] = array("GMT+0200 - Egypt, Finland, Israel","+0200");
+	$txt['Time_Zone'][15] = array("GMT+0300 - Ethiopia, Iraq, Russia","+0300");
+	$txt['Time_Zone'][16] = array("GMT+0400 - United Arab Emirates","+0400");
+	$txt['Time_Zone'][17] = array("GMT+0500 - Indian Ocean Territory","+0500");
+	$txt['Time_Zone'][18] = array("GMT+0600 - Bangledesh","+0600");
+	$txt['Time_Zone'][19] = array("GMT+0700 - Mongolia, Thailand","+0700");
+	$txt['Time_Zone'][20] = array("GMT+0800 - Australia WST","+0800");
+	$txt['Time_Zone'][21] = array("GMT+0900 - Indonesia, Japan, Korea","+0900");
+	$txt['Time_Zone'][22] = array("GMT+1000 - Australia EST","+1000");
+	$txt['Time_Zone'][23] = array("GMT+1100 - Solomon Islands","+1100");
+	$txt['Time_Zone'][24] = array("GMT+1200 - Fiji, New Zealand","+1200");
+?>
