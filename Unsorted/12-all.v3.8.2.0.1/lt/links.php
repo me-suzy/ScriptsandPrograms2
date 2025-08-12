55ec050029d1ad20cfe3b126f712d5caef4d7f88
me-suzy <me.suzana@gmail.com>
2025-08-13 00:14:12 +0300

    Add Unsorted/12-all.v3.8.2.0.1/lt/links.php

---
+++ Unsorted/12-all.v3.8.2.0.1/lt/links.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<font size="2" face="Arial, Helvetica, sans-serif"><b><a href="main.php?page=lt/links_v&nl=<?PHP print $nl; ?>"> 
+</a></b><a href="main.php?page=lt/links_v&nl=<?PHP print $nl; ?>"><?PHP print $lang_411; ?></a> 
+</font> 
+</body>
+</html>
