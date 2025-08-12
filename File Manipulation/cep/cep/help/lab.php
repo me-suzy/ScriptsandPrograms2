20f309c4543c441ae6506134283634afd65e3f97
me-suzy <me.suzana@gmail.com>
2025-08-12 14:14:05 +0300

    Add File Manipulation/cep/cep/help/lab.php

---
+++ File Manipulation/cep/cep/help/lab.php
@@ -0,0 +1,84 @@
+<?php
+/*------------------------------------------------------------------------------
+CJG EXPLORER PRO v3.2 - WEB FILE MANAGEMENT - Copyright (C) 2003 CARLOS GUERLLOY
+CJGSOFT Software
+cjgexplorerpro@guerlloy.com
+guerlloy@hotmail.com
+carlos@weinstein.com.ar
+Buenos Aires, Argentina
+--------------------------------------------------------------------------------
+This program is free software; you can  redistribute it and/or  modify it  under
+the terms   of the   GNU General   Public License   as published   by the   Free
+Software Foundation; either  version 2   of the  License, or  (at  your  option)
+any  later version. This program  is  distributed in  the hope that  it  will be
+useful,  but  WITHOUT  ANY  WARRANTY;  without  even  the   implied  warranty of
+MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR  PURPOSE.  See the  GNU   General
+Public License for   more details. You  should have received  a copy of  the GNU
+General Public License along  with this  program; if   not, write  to the   Free
+Software  Foundation, Inc.,  59 Temple Place,  Suite 330, Boston,  MA 02111-1307
+USA
+------------------------------------------------------------------------------*/
+
+$lab=array(
+'about'=>'About',
+'features'=>'Features',
+'license'=>'License',
+'admin'=>'Administration',
+'prereq'=>'Prerequisites',
+'install'=>'Installation',
+'configure'=>'Configuration',
+'translate'=>'Translate utility',
+'screen'=>'Screen Parts',
+'howto'=>'HowTo',
+'interface'=>'Interface',
+'hideshowpanel'=>'Hide And Show Panels',
+'resize'=>'Resize Frames',
+'hideshowhist'=>'Hide And Show History Details',
+'clear'=>'Clear History',
+'navigation'=>'Navigation',
+'refresh'=>'Refresh Tree',
+'collapse'=>'Collapse Tree',
+'openfolder'=>'Open Folder',
+'changeview'=>'Change View Settings',
+'sort'=>'Change Sort Order',
+'help'=>'Show Help',
+'select'=>'Select Files And Folders',
+'selectitems'=>'Select Items',
+'selectall'=>'Select All Items',
+'unselect'=>'Unselect All Items',
+'selectallfiles'=>'Select All Files',
+'selectallfolders'=>'Select All Folders',
+'reverse'=>'Reverse Selection',
+'filesystem'=>'Filesystem Functions',
+'delete'=>'Delete Items',
+'edit'=>'Edit Items',
+'viewasc'=>'View contents (ascii)',
+'viewhex'=>'View contents (hex)',
+'open'=>'Open Items',
+'move'=>'Move Items To A Folder',
+'rename'=>'Rename An Item',
+'copy'=>'Copy Items',
+'mkdir'=>'Create A Folder',
+'chmod'=>'Change Permissions',
+'archive'=>'Archive Functions',
+'untar'=>'Extract Archive Items (Tar)',
+'untgz'=>'Extract Archive Items (Tgz)',
+'unzip'=>'Extract Compressed Items (Zip)',
+'tar'=>'Archive Items (Tar)',
+'tgz'=>'Archive And Compress Items (Tgz)',
+'zip'=>'Archive And Compress Items (Zip)',
+'search'=>'Item Search',
+'find'=>'Find Items On The Tree',
+'contents'=>'File Contents',
+'contwin'=>'Contents Window',
+'viewfile'=>'View A File',
+'openfile'=>'Open A File',
+'editnewfile'=>'Edit A New File',
+'edittextfile'=>'Edit A Text File',
+'editbinaryfile'=>'Edit A Binary File',
+'transfer'=>'Transfer Functions',
+'download'=>'Download Files',
+'zipdownload'=>'Zip And Download Items',
+'upload'=>'Upload Files',
+'history'=>'Help History'
+);
