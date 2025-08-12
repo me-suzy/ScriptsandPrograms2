d67c4cef1301f94c4c57d82c29b9db02b2aae240
me-suzy <me.suzana@gmail.com>
2025-08-12 21:10:20 +0300

    Add News Publishing/zomplog34/zomplog34/skins/kindakubrick/footer.php

---
+++ News Publishing/zomplog34/zomplog34/skins/kindakubrick/footer.php
@@ -0,0 +1,15 @@
+</td><td>&nbsp;</td>
+        </tr>
+        <tr> 
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr> 
+    <td><img src="skins/kindakubrick/images/top_bottom.gif" width="650" height="59" border="0"></td>
+  </tr>
+</table>
+</body>
+</html>
