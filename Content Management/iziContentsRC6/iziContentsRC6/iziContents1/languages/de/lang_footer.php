413d7882f2a9b8b6c4ec1d9e4a074a7559d1b80d
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:19 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_footer.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_footer.php
@@ -0,0 +1,6 @@
+<?php
+
+$GLOBALS["tFooterRight"] = 'Thern 17 &middot; A - 4880 St.Georgen / Attergau &middot; Tel +43 (0)7667 / 6600 &middot; <A class=footerlink HREF="mailto:office@rottner-tresor.at">office@rottner-tresor.at</A><BR>Ludwig-Zeller-Stra&szlig;e 1 &middot; D - 83395 Freilassing &middot; Tel +49 (0)8654 / 778590 &middot; <A class=footerlink HREF="mailto:vertrieb@rottner-tresor.at">vertrieb@rottner-tresor.at</A><BR>Ravensburgerstra&szlig;e 16 &middot; D - 88677 Markdorf &middot; Tel +49 (0)7544 / 5094 &middot; <A class=footerlink HREF="mailto:melanie.hartleib@rottner-tresor.at">melanie.hartleib@rottner-tresor.at</A>';
+$GLOBALS["tFooterLeft"] = 'ROTTNER TRESOR GmbH:<BR>ROTTNER SECURITY GmbH: <BR>VERKAUFSB&Uuml;RO Deutschland:';
+
+?>
