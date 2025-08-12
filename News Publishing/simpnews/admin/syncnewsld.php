bc9c1e73a30eb477cd48a1fffa683a46880edb70
me-suzy <me.suzana@gmail.com>
2025-08-12 20:28:38 +0300

    Add News Publishing/simpnews/admin/syncnewsld.php

---
+++ News Publishing/simpnews/admin/syncnewsld.php
@@ -0,0 +1,54 @@
+<?php
+/***************************************************************************
+ * (c)2002-2004 Boesch IT-Consulting (info@boesch-it.de)
+ ***************************************************************************/
+require_once('../config.php');
+require_once('./admchk.php');
+if(!isset($$langvar) || !$$langvar)
+	$act_lang=$admin_lang;
+else
+	$act_lang=$$langvar;
+include_once('./language/lang_'.$act_lang.'.php');
+require_once('./auth.php');
+$page_title="$l_synclinkdates ($l_news)";
+require_once('./heading.php');
+?>
+<table align="center" width="80%" CELLPADDING="1" CELLSPACING="0" border="0" valign="top">
+<tr><TD BGCOLOR="#000000">
+<table align="center" width="100%" border="0" cellspacing="1" cellpadding="1">
+<?php
+if($admin_rights < 2)
+{
+	echo "<tr class=\"errorrow\"><td align=\"center\">";
+	die("$l_functionnotallowed");
+}
+$sql = "select * from ".$tableprefix."_data where linknewsnr!=0";
+if(!$result = mysql_query($sql, $db))
+{
+	echo "<tr class=\"errorrow\"><td align=\"center\">";
+    die("Could not connect to the database.");
+}
+while($myrow=mysql_fetch_array($result))
+{
+	$sql2="select * from ".$tableprefix."_data where newsnr=".$myrow["linknewsnr"];
+	if(!$result2 = mysql_query($sql2, $db))
+	{
+		echo "<tr class=\"errorrow\"><td align=\"center\">";
+	    die("Could not connect to the database.");
+	}
+	if($myrow2=mysql_fetch_array($result2))
+	{
+		$sql3="update ".$tableprefix."_data set date='".$myrow2["date"]."' where newsnr=".$myrow["newsnr"];
+		if(!$result3 = mysql_query($sql3, $db))
+		{
+			echo "<tr class=\"errorrow\"><td align=\"center\">";
+			die("Could not connect to the database.");
+		}
+	}
+}
+?>
+<tr class="displayrow"><td align="center"><?php echo $l_datessynced?></td></tr>
+</table></td></tr></table>
+<?php
+include('./trailer.php');
+?>
