593fa50694b087209521b02874d674820af58f7f
me-suzy <me.suzana@gmail.com>
2025-08-12 18:12:41 +0300

    Add Image Galleries/PHP_JK_Aug_01_2005/Includes/i_Globals.php

---
+++ Image Galleries/PHP_JK_Aug_01_2005/Includes/i_Globals.php
@@ -0,0 +1,37 @@
+<?php
+	// CHANGE NONE OF THESE VARIABLES
+	$PHPJK_VERSION			= 1.00;
+	
+	$NONMEMBER_ID			= -1;
+
+	$sPageTitle				= "";
+	$sStyle					= "";
+	$sHeader				= "";
+	$iDomainName			= 1;
+	
+	$iLoginAccountUnq		= 0;
+	$bHasAccount			= False;
+	
+	$iTextScheme			= 1;
+	$aVariables				= array(0=>"",1=>"",2=>"",3=>"",4=>"",5=>"",6=>"",7=>"",8=>"",9=>"",10=>"",11=>"",12=>"",13=>"",14=>"",15=>"",16=>"",17=>"",18=>"",19=>"");
+	$aValues				= array(0=>"",1=>"",2=>"",3=>"",4=>"",5=>"",6=>"",7=>"",8=>"",9=>"",10=>"",11=>"",12=>"",13=>"",14=>"",15=>"",16=>"",17=>"",18=>"",19=>"");
+	$iTableWidth			= "671";
+	$bHasADVRights			= False;	// this is set to TRUE when the user has rights to the ADV after calling ACCNT_ReturnADV, FALSE otherwise
+	
+	$PHPJKConnection		= 0;
+	
+	$iTtlNumItems			= 0;
+	$iDBLoc					= 0;
+	$iNumPerPage			= 10;
+	
+	$PUBLIC_GALLERIES	= 1;
+	$PRIVATE_GALLERIES	= 2;
+	
+	// these two tell which thumbnail conversion component to use
+	$ASPIMAGE			= 1;
+	$GFL				= 2;
+	
+	$COLORBASED			= 1;
+	$SCHEMEBASED		= 2;
+	$ADMINISTRATIVE		= 3;
+?>
