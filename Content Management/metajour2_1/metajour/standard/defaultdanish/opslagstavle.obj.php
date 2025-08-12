d0d663908da30d51d91ba4ec6fb96b1c4bd3fc26
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:52 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/opslagstavle.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/opslagstavle.obj.php
@@ -0,0 +1,20 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 60;
+$_objchilds[] = '_child_opslagstavlen_61';
+$_objchilds[] = '_child_cykeltilsalg_62';
+$_objchilds[] = '_child_tipsklub_63';
+function objfile_60 () {
+$obj = owNew('document');
+$objdata['name'] = "Opslagstavle";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
