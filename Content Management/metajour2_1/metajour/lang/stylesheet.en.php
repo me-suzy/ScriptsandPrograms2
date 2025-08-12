4cbbbc1dcfc5ee18623e9f03749d26c519d67762
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:42 +0300

    Add Content Management/metajour2_1/metajour/lang/stylesheet.en.php

---
+++ Content Management/metajour2_1/metajour/lang/stylesheet.en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Name';
+$LANG['label_content'] = 'Stylesheet CSS';
+$LANG['name'] = 'Stylesheets';
+$LANG['label_standard'] = '';
+$LANG['label_mapping'] = 'Editor stylesheet';
+?>
