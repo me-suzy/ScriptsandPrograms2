bfd28dbb0dcd7431f09463dc7127983bc3b0a31d
me-suzy <me.suzana@gmail.com>
2025-08-12 10:28:36 +0300

    Add Discussion Boards/board2/ukiboard/themes/brownstyle.php

---
+++ Discussion Boards/board2/ukiboard/themes/brownstyle.php
@@ -0,0 +1,20 @@
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+
+function MenuOver(element)
+{
+	if(element.style){
+		element.style.backgroundColor='#FFCC99';
+		element.style.cursor='hand';
+	};
+};
+
+function MenuOut(element)
+{
+	if(element.style) element.style.backgroundColor='#CC9966';
+};
+
+window.defaultStatus="UKiBoard";
+
+//-->
+</SCRIPT>
