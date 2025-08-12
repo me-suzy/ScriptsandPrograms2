01e33e3baf1ee88c978d198c4201880a2c12d6fe
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:29 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/lang_links_de.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/lang_links_de.php
@@ -0,0 +1,41 @@
+<?
+
+// Form Titles
+$GLOBALS["tFormTitle"]		= 'Übersicht Links';
+$GLOBALS["tFormTitleExtra"]	= 'Übersicht Extra Link Einstellungen';
+$GLOBALS["tSubmitFormTitle"]	= 'Link senden';
+
+// List Headings
+$GLOBALS["tPostedBy"]		= 'Posted von';
+$GLOBALS["tPublishDate"]	= 'Datum';
+$GLOBALS["tStatus"]		= 'Status';
+
+// List Functions
+$GLOBALS["tAddNewLink"]	= 'Link hinzufügen';
+$GLOBALS["tEditLink"]	= 'Link bearbeiten';
+$GLOBALS["tDeleteLink"]	= 'Link löschen';
+$GLOBALS["tReleaseLink"]	= 'Link freigeben';
+$GLOBALS["tEditExtraSettings"] = "Extra-Einstellungen bearbeiten";
+
+// Form Field Headings
+$GLOBALS["tCategory"] 		= 'Kategorie';
+$GLOBALS["tPageID"]			= 'Seiten ID';
+$GLOBALS["tWebURL"]			= 'Website URL';
+$GLOBALS["tDescription"]	= 'Beschreibung';
+$GLOBALS["tIntro"]			= 'Intro Text';
+$GLOBALS["tSubmitLink"]		= 'Link schicken';
+
+// Link Submission Texts
+$GLOBALS["tThankYou"]		= 'Danke für das Übermitteln Ihres Link-Vorschlages.';
+$GLOBALS["tReleaseRequired"]	= 'Bitte beachten Sie, dass der Link erst vom Administrator freigegeben wird und vorher nicht erscheint.';
+$GLOBALS["tClickToReturn"]	= 'Klicken Sie hier zum Zurückkehren zur';
+$GLOBALS["tLinksPage"]	= 'Link Seite';
+
+// Security Texts
+$GLOBALS["tMustLogin"]		= 'Sie müssen';
+$GLOBALS["tLogin"]		= 'sich anmelden';
+$GLOBALS["tToSubmit"]		= 'um den Link zu senden';
+
+//Validation
+$GLOBALS["eWebURLEmpty"]	= "Das Link-Feld kann nicht freigelassen werden";
+?>
