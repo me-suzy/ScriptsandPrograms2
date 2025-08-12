eff98e66d9dbcb42050b332e8333d0a626339b87
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:43 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_setgraphics.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_setgraphics.php
@@ -0,0 +1,103 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Seitengrafik einstellen';
+
+//  Form Block Titles
+$GLOBALS["thSiteLogo"] = 'Seiten Logo';
+$GLOBALS["thBackgrounds"] = 'Hintergrund-Bilder';
+$GLOBALS["thPagingIcons"] = 'Seiten-Symbole';
+$GLOBALS["thMenuIcons"] = 'Men&uuml;-Symbole';
+$GLOBALS["thSpecialIcons"] = 'Spezial Symbole';
+
+//  Form Field Headings
+$GLOBALS["tHomepageLogo"] = 'Homepage Logo Bild';
+$GLOBALS["tTopBackground"] = 'Top Hintergrund';
+$GLOBALS["tTopBackgroundRepeat"] = 'Top Hintergrund wiederholen';
+$GLOBALS["tTopBackgroundFixed"] = 'Top Hintergrund fixieren';
+$GLOBALS["tTopBackgroundPosition"] = 'Top Hintergrund Position';
+$GLOBALS["tMainBackground"] = 'Hauptseiten-Hintergrund';
+$GLOBALS["tMainBackgroundRepeat"] = 'Hauptseiten-Hintergrund wiederholen';
+$GLOBALS["tMainBackgroundFixed"] = 'Hauptseiten-Hintergrund fixieren';
+$GLOBALS["tMainBackgroundPosition"] = 'Hauptseiten-Hintergrund Position';
+$GLOBALS["tBorderBackground"] = 'Rahmen-Hintergrund';
+$GLOBALS["tBorderBackgroundRepeat"] = 'Rahmen-Hintergrund wiederholen';
+$GLOBALS["tBorderBackgroundFixed"] = 'Rahmen-Hintergrund fixieren';
+$GLOBALS["tBorderBackgroundPosition"] = 'Rahmen-Hintergrund Position';
+$GLOBALS["tContentBackground"] = 'Inhalts-Kopf Hintergrund';
+$GLOBALS["tContentBackgroundRepeat"] = 'Inhalts-Kopf Hintergrund wiederholen';
+$GLOBALS["tContentBackgroundPosition"] = 'Inhalts-Kopf Hintergrund Position';
+$GLOBALS["tTopMenuBackground"] = 'Top-Men&uuml; Hintergrund';
+$GLOBALS["tTopMenuBackgroundRepeat"] = 'Top-Men&uuml; Hintergrund wiederholen';
+$GLOBALS["tTopMenuBackgroundFixed"] = 'Top-Men&uuml; Hintergrund fixieren';
+$GLOBALS["tTopMenuBackgroundPosition"] = 'Top-Men&uuml; Hintergrund Position';
+$GLOBALS["tMenuBackground"] = 'Men&uuml; Hintergrund';
+$GLOBALS["tMenuBackgroundRepeat"] = 'Men&uuml; Hintergrund wiederholen';
+$GLOBALS["tMenuBackgroundFixed"] = 'Men&uuml; Hintergrund fixieren';
+$GLOBALS["tMenuBackgroundPosition"] = 'Men&uuml; Hintergrund Position';
+$GLOBALS["tFooterBackground"] = 'Fu� Hintergrund';
+$GLOBALS["tFooterBackgroundRepeat"] = 'Fu� Hintergrund wiederholen';
+$GLOBALS["tFooterBackgroundFixed"] = 'Fu� Hintergrund fixieren';
+$GLOBALS["tFooterBackgroundPosition"] = 'Fu� Hintergrund Position';
+$GLOBALS["tFirstPageIcon"] = 'Symbol f&uuml;r Erste Seite';
+$GLOBALS["tPrevPageIcon"] = 'Symbol f&uuml;r vorherige Seite';
+$GLOBALS["tNextPageIcon"] = 'Symbol f&uuml;r n&auml;chste Seite';
+$GLOBALS["tLastPageIcon"] = 'Symbol f&uuml;r letzte Seite';
+$GLOBALS["tListStyleIcon"] = 'Symbol f&uuml;r Aufz&auml;hlungen';
+$GLOBALS["tExpandIcon"] = 'Symbol f&uuml;r Men&uuml; erweitern';
+$GLOBALS["tCollapseIcon"] = 'Symbol f&uuml;r Men&uuml; zusammenklappen';
+$GLOBALS["tNoExpandIcon"] = 'Symbol f&uuml;r Keine Erweiterung des Men&uuml;s ';
+$GLOBALS["tSecureIcon"] = 'Symbol f&uuml;r eingeschr&auml;nktes Men&uuml;';
+$GLOBALS["tPrintIcon"] = 'Symbol f&uuml;r druckerfreundliche Version';
+$GLOBALS["tPDFIcon"] = 'Symbol f&uuml;r PDF Version';
+$GLOBALS["tTellFriendIcon"] = 'Symbol f&uuml;r Tell-a-Friend';
+$GLOBALS["tRatingIcon"] = 'Symbol f&uuml;r Artikelbewertung';
+$GLOBALS["tCommentIcon"] = 'Symbol f&uuml;r Artikelkommentare';
+
+//  Form Text Descriptions
+$GLOBALS["tDetails"] = 'Legen Sie hier Bilder und Hintergr&uuml;nde f&uuml;r Ihre Webseite fest.<br />Vergewissern Sie sich, dass die Bilder, die Sie ben&ouml;tigen, auch hochgeladen wurden. (Funktion Bilder verwalten)';
+
+$GLOBALS["hHomepageLogo"] = 'Dies ist das Hauptlogo f&uuml;r Ihre Seite. Es wird oben links im Browser angezeigt.';
+$GLOBALS["hTopBackground"] = 'Dies setzt den Hintergrund f&uuml;r den Kopfrahmen mit Ihrem Logo und dem Banner, ein Kopf \"userdata\" Rahmen, wenn Sie dies f&uuml;r Ihre Seite konfiguriert haben.';
+$GLOBALS["hTopBackgroundRepeat"] = 'Das Kopf-Hintergrundbild wird wiederholt.';
+$GLOBALS["hTopBackgroundFixed"] = 'Markieren Sie diese Box, wenn Sie m&ouml;chten, dass das Hintergrundbild im Kopf fixiert ist, wenn dies eine Region mit Scrollbalken ist';
+$GLOBALS["hTopBackgroundPosition"] = 'W&auml;hlen Sie die gew&uuml;nschte Einstellung, um das Hintergrundbild zu positionieren, wenn Sie es nicht wiederholt haben m&ouml;chten';
+$GLOBALS["hMainBackground"] = 'Dies legt den Haupthintergrund fest.<br />Wenn Sie die Tabellen-Hintergrund-Farbe und Tabellenrahmen-Farbe auf leer festlegen, wird die Inhaltstabelle (in der der Artikeltext steht) transparent und das Hintergrundbild wird unter der Tabelle sichtbar.';
+$GLOBALS["hMainBackgroundRepeat"] = 'Wiederholt das Haupt-Hintergund-Bild.';
+$GLOBALS["hMainBackgroundFixed"] = 'Setzen Sie hier einen Haken, wenn Sie m&ouml;chten, dass der Hauptseiten-Hintergrund fixiert bleibt, auch wenn dieser zu einer Region mit Scrollbalken geh&ouml;rt.';
+$GLOBALS["hMainBackgroundPosition"] = 'W&auml;hlen Sie die gew&uuml;nschte Einstellung, um das Hintergrundbild zu positionieren, wenn Sie es nicht wiederholt haben m&ouml;chten';
+$GLOBALS["hBorderBackground"] = 'Dies legt den Rahmenhintergrund fest.<br />Wenn Sie die Tabellen-Hintergrund-Farbe und Tabellenrahmen-Farbe auf leer festlegen, wird die Inhaltstabelle (in der der Artikeltext steht) transparent und das Hintergrundbild wird unter der Tabelle sichtbar.';
+$GLOBALS["hBorderBackgroundRepeat"] = 'Wiederholt das Rahmen-Hintergund-Bild.';
+$GLOBALS["hBorderBackgroundFixed"] = 'Setzen Sie hier einen Haken, wenn Sie m&ouml;chten, dass der Rahmen-Hintergrund fixiert bleibt, auch wenn dieser zu einer Region mit Scrollbalken geh&ouml;rt.';
+$GLOBALS["hBorderBackgroundPosition"] = 'W&auml;hlen Sie die gew&uuml;nschte Einstellung, um das Hintergrundbild zu positionieren, wenn Sie es nicht wiederholt haben m&ouml;chten';
+$GLOBALS["hContentBackground"] = 'Das Hintergrundbild f&uuml;r den Inhaltskopf (&Uuml;berschrift oder Titel des Artikels). Es wird f&uuml;r alle Artikel festgelegt, die auf der Seite ver&ouml;ffentlicht werden. <br />Der Vorteil liegt darin, dass Sie Text dar&uuml;ber legen k&ouml;nnen. Wenn Sie den Kopf mit einer Grafik gestalten (in der Inhaltsverwaltung), ben&ouml;tigen Sie f&uuml;r jede/n Titel/&Uuml;berschrift ein separates Bild. Sie k&ouml;nnen auch das Kopfbild und das Hintergrundbild zusammen verwenden.';
+$GLOBALS["hContentBackgroundRepeat"] = 'Wiederholt das Bild f&uuml;r den Inhalts-Hintergrund.';
+$GLOBALS["hContentBackgroundPosition"] = 'W&auml;hlen Sie die gew&uuml;nschte Einstellung, um das Hintergrundbild zu positionieren, wenn Sie es nicht wiederholt haben m&ouml;chten';
+$GLOBALS["hTopMenuBackground"] = 'Dies legt das Bild f&uuml;r das Topmen&uuml; fest. Wenn Sie die Topmen&uuml;-Hintergrundfarbe auf  \"leer\" setzen, wird die Tabelle, die normalerweise um das Men&uuml; herum angezeigt wird, transparent und gibt einem Hintergrundbild ein sch&ouml;neres Aussehen.';
+$GLOBALS["hTopMenuBackgroundRepeat"] = 'Wiederholt das Topmen&uuml; Hintergrundbild.';
+$GLOBALS["hTopMenuBackgroundFixed"] = 'Setzen Sie hier einen Haken, wenn Sie m&ouml;chten, dass der Topmen&uuml;-Hintergrund fixiert bleibt, auch wenn das Topmen&uuml; Scrollbalken bekommt.';
+$GLOBALS["hTopMenuBackgroundPosition"] = 'W&auml;hlen Sie die gew&uuml;nschte Einstellung, um das Hintergrundbild zu positionieren, wenn Sie es nicht wiederholt haben m&ouml;chten';
+$GLOBALS["hMenuBackground"] = 'Hier wird das Bild f&uuml;r den Men&uuml;hintergrund festgelegt. Auch hier: Wenn Sie die Men&uuml;hintergrund-Farbe auf \"leer\" setzen, wird die Tabelle, die normalerweise um das Men&uuml; herum angezeigt wird, transparent dargestellt und bringt auf diese Weise ein Hintergrundbild besser zur Geltung.';
+$GLOBALS["hMenuBackgroundRepeat"] = 'Wiederholt das Men&uuml;hintergrundbild.';
+$GLOBALS["hMenuBackgroundFixed"] = 'Setzen Sie hier einen Haken, wenn Sie m&ouml;chten, das der Men&uuml;hintergrund fixiert bleibt, falls das Men&uuml; einen Scrollbalken bekommt.';
+$GLOBALS["hMenuBackgroundPosition"] = 'W&auml;hlen Sie die gew&uuml;nschte Einstellung, um das Hintergrundbild zu positionieren, wenn Sie es nicht wiederholt haben m&ouml;chten';
+$GLOBALS["hFooterBackground"] = 'Dies legt den Hintergrund f&uuml;r den Fuss-Frame fest, wenn Sie diese Eigenschaft in ezContents eingestellt haben.';
+$GLOBALS["hFooterBackgroundRepeat"] = 'Wiederholt das Fuss-Hintergrundbild.';
+$GLOBALS["hFooterBackgroundFixed"] = 'Setzen Sie hier einen Haken, wenn Sie m&ouml;chten, dass der Fuss-Hintergrund fixiert bleibt, auch wenn dieser Bereich einen Scrollbalken erh&auml;lt.';
+$GLOBALS["hFooterBackgroundPosition"] = 'W&auml;hlen Sie die gew&uuml;nschte Einstellung, um das Hintergrundbild zu positionieren, wenn Sie es nicht wiederholt haben m&ouml;chten';
+$GLOBALS["hFirstPageIcon"] = 'Dieses Symbol wird angezeigt, um bei mehrseitigen Artikeln zur ersten Seite zur&uuml;ckzukehren.';
+$GLOBALS["hPrevPageIcon"] = 'Dieses Symbol wird angezeigt, um bei mehrseitigen Artikeln zur vorherigen Seite zur&uuml;ckzugehen.';
+$GLOBALS["hNextPageIcon"] = 'Dieses Symbol wird angezeigt, um bei mehrseitigen Artikeln zur n&auml;chsten Seite voran zu gehen.';
+$GLOBALS["hLastPageIcon"] = 'Dieses Symbol wird angezeigt, um bei mehrseitigen Artikeln zur letzten Seite voran zu gehen.';
+$GLOBALS["hListStyleIcon"] = 'Dieses Symbol wird bei Aufz&auml;hlungen angezeigt, die in den Inhalten mit <li> definiert sind.';
+$GLOBALS["hExpandIcon"] = 'Dieses Symbol kann neben Men&uuml;eintr&auml;gen angezeigt werden, um auf die Erweiterung dieses Men&uuml;punktes zu einem Untermen&uuml; hinzuweisen.';
+$GLOBALS["hCollapseIcon"] = 'Dieses Symbol erscheint neben einem erweiterten Men&uuml;, um anzuzeigen, das dieses Untermen&uuml; zusammen geklappt werden kann.';
+$GLOBALS["hNoExpandIcon"] = 'Dieses Men&uuml;symbol wird neben Men&uuml;punkten angezeigt, die nicht erweiterbar sind, weil es kein Untermen&uuml; zu diesem Punkt gibt.';
+$GLOBALS["hSecureIcon"] = 'Dieses Symbol wird neben Men&uuml;s mit Zugriffsbeschr&auml;nkung angezeigt, um anzuzeigen, dass der Zugang nur f&uuml;r bestimmte User berechtigt ist.';
+$GLOBALS["hPrintIcon"] = 'Dieses Symbol stellt einen Link f&uuml;r eine druckerfreundliche Version des Artikels zur Verf&uuml;gung.';
+$GLOBALS["hPDFIcon"] = 'Dieses Symbol stellt einen Link f&uuml;r eine PDF Version des Artikels zur Verf&uuml;gung.';
+$GLOBALS["hTellFriendIcon"] = 'Dieses Symbol stellt einen Link f&uuml;r den Mail-Versand des Artikels zur Verf&uuml;gung.';
+$GLOBALS["hRatingIcon"] = 'Dieses Symbol erscheint bei Artikeln, die von den Lesern bewertet werden k&ouml;nnen.';
+$GLOBALS["hCommentIcon"] = 'Dieses Symbol erscheint bei Artikeln, die von den Lesern kommentiert werden k&ouml;nnen.';
+
+?>
