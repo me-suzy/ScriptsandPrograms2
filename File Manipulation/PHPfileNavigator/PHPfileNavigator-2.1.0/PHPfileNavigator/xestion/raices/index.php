a2b3f7e9963624c0396c1609f3c174f9688a7e94
me-suzy <me.suzana@gmail.com>
2025-08-12 14:59:03 +0300

    Add File Manipulation/PHPfileNavigator/PHPfileNavigator-2.1.0/PHPfileNavigator/xestion/raices/index.php

---
+++ File Manipulation/PHPfileNavigator/PHPfileNavigator-2.1.0/PHPfileNavigator/xestion/raices/index.php
@@ -0,0 +1,54 @@
+<?php
+/****************************************************************************
+* xestion/raices/index.php
+*
+* Carga la pantalla para la administración de una raíz
+*
+
+PHPfileNavigator versión 2.0.0
+
+Copyright (C) 2004-2005 Lito <lito@eordes.com>
+
+http://phpfilenavigator.litoweb.net/
+
+Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los
+términos de la Licencia Pública General de GNU según es publicada por la Free
+Software Foundation, bien de la versión 2 de dicha Licencia o bien (según su
+elección) de cualquier versión posterior. 
+
+Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA
+GARANTÍA, incluso sin la garantía MERCANTIL implícita o sin garantizar la
+CONVENIENCIA PARA UN PROPÓSITO PARTICULAR. Véase la Licencia Pública General de
+GNU para más detalles. 
+
+Debería haber recibido una copia de la Licencia Pública General junto con este
+programa. Si no ha sido así, escriba a la Free Software Foundation, Inc., en
+675 Mass Ave, Cambridge, MA 02139, EEUU. 
+*******************************************************************************/
+
+$relativo = '../../';
+
+include ($relativo.'paths.php');
+include_once ($paths['include'].'basicweb.php');
+include_once ($paths['include'].'Xusuarios.php');
+
+PFN_quita_url_SERVER('id_raiz');
+
+session_write_close();
+
+$tempo->rexistra('precarga');
+
+include ($paths['plantillas'].'cab.inc.php');
+include ($paths['xestion'].'Xopcions.inc.php');
+
+$tempo->rexistra('precodigo');
+
+$id_raiz = $vars->get('id_raiz');
+
+include ($paths['xestion'].'raices/index.inc.php');
+include ($paths['plantillas'].'Xraices.inc.php');
+
+$tempo->rexistra('postcodigo');
+
+include ($paths['plantillas'].'pe.inc.php');
+?>
