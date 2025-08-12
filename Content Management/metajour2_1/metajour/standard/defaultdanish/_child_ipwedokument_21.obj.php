9d7d4f8bc32737c549ee3cf30708c183e127d9c8
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:14 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ipwedokument_21.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ipwedokument_21.obj.php
@@ -0,0 +1,45 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 21;
+function objfile_21 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "IPW eDokument";
+$objdata['subname'] = "";
+$objdata['extension'] = "";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "<P>IPW eDokument er vores kvalitet-og miljøstyringssystem hvor du blandt andet kan vedligeholde virksomhedens håndbøger. Yderligere håndterer systemet afvigelser og reklamationer i relation til din kvalitet-og miljøstyring. </P>
+<P>Med IPW eDokument&nbsp;får du en løsning, der giver dig overblik, sikkerhed og sparer på din tid. IPW eDokument er baseret på IPW METAjour, vores udbredte opensource content management system. </P>
+<P>Med IPW eDokument får du:</P>
+<UL>
+<LI>1 system med frit antal brugere, dokumenter, sprog m.v. 
+<LI>Installation af IPW eDokument på kundens server 
+<LI>Tilpasning af standard design til virksomhedens farver og logo 
+<LI>Trykt dansk brugervejledning 
+<LI>Blandt andet følgende funktioner: 
+<UL>
+<LI>Nem Word-lignende dokumentredigering 
+<LI>Redigeringsadgang direkte fra den elektroniske kvalitetshåndbog 
+<LI>Mulighed for brug af video- og lydfiler, foto og grafik til underbygning af tekst 
+<LI>Brugeradministration og uddelegering af dokument vedligeholdelse 
+<LI>E-mailbaseret beskedsystem til afvigelser og dokumentgodkendelser 
+<LI>Workflow system (godkendelsessystem til ændring og udgivelse af dokumenter) 
+<LI>Revisionskontrol på dokumenter, filer og foto 
+<LI>Dokumenter og afvigelser kan ikke slettes fra systemet 
+<LI>Mulighed for sprogversionering af dokumenter 
+<LI>Elektronisk afvigelse med statistik 
+<LI>Overblik over, hvor en afvigelse aktuelt befinder sig 
+<LI>Ordforklaring til dokumenter 
+<LI>Søgefunktion og sitemap 
+<LI>3 side skabeloner som i standarddesign 
+<LI>Printvenlig version 
+<LI>Forum for kvalitetsdebat </LI></UL></LI></UL>
+<P>IPW eDokument kan udbygges med ønskede funktioner og kan integreres med din virksomheds andre ERP-systemer. Løsningen kan således tilpasses lige netop til dine behov. Og naturligvis kan IPW eDokument også håndtere miljøledelse, SQAS, HACCP mv.</P>
+<P>IPW eDokument er et købe-produkt fra <A href=\"http://www.ipwsystems.dk/\" target=_blank>IPW Systems A/S</A>.</P>
+<P>IPW eDokument er et overordentlig fleksibelt system  og dog særdeles nemt og enkelt i brug. Vil du vide mere om IPW eDokument, <A href=\"http://www.ipwsystems.dk/showpage.php?pageid=1509\" target=_blank>kan du læse mere her</A>.</P>";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
