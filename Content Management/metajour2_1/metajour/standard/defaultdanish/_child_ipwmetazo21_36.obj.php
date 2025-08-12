5231a64305f932894a3ba2e242a38351cab6cfa2
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:22 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ipwmetazo21_36.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ipwmetazo21_36.obj.php
@@ -0,0 +1,20 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 36;
+function objfile_36 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "IPW METAZO 2.1";
+$objdata['subname'] = "";
+$objdata['extension'] = "";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "<P>IPW METAZO er ikke et selvstændigt produkt. IPW METAZO er udviklingsplatformen fra IPW Systems A/S, hvorpå alle andre programmer er baseret. Både standardsystemer som IPW METAjour, IPW eDokument og IPW eReklamation, men også alle specialudviklede løsninger.</P>
+<P>IPW METAZO stiller alle de byggeklodser til rådighed, som samtlige af vore programmer er afhængige af. Alt fra brugerhåndtering og skabeloner til brugerflade og databaseintegration varetages af IPW METAZO.</P>
+<P>Den fælles platform for alle systemerne er med til at sikre en ensartet og genkendelig brugerflade på tværs af alle vore programmer, og at sørge for problemfri deling af informationer mellem alle programmer.</P>
+<P>Vil du vide mere om mulighederne i IPW METAZO, <A href=\"http://www.ipwsystems.dk/showpage.php?pageid=1475\" target=_blank>kan du læse mere her</A>.</P>";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
