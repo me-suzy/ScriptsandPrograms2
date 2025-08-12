2688b60c3c8ac2d2014779dabf2b3b222d5e9c9a
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:35 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_languages.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_languages.php
@@ -0,0 +1,55 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintenir des langues';
+$GLOBALS["tFormTitle2"] = 'ajouter une nouvelle langue';
+
+//  List Headings
+$GLOBALS["tLanguageCode"] = 'Code de langue';
+$GLOBALS["tLanguage"] = 'Langue';
+$GLOBALS["tCharSet"] = 'S�rie de caract�re';
+$GLOBALS["tDirection"] = 'Direction';
+$GLOBALS["tEnabled"] = 'Activer';
+$GLOBALS["tFileName"] = 'Nom de fichier de langue';
+$GLOBALS["tFileNameM"] = 'Nom de fichier de module';
+
+//  List Functions
+$GLOBALS["tAddNew"] = 'Activer langue';
+$GLOBALS["tViewLanguage"] = 'Affichage param�tre du language';
+$GLOBALS["tEditLanguage"] = 'Editer param�tre du language';
+$GLOBALS["tViewFiles"] = 'Affichage les fichiers du language';
+$GLOBALS["tEditFiles"] = 'Editer les fichiers du language';
+$GLOBALS["tViewFile"] = 'Affichage le fichier du language';
+$GLOBALS["tEditFile"] = 'Editer le fichier du language';
+$GLOBALS["tRet_Languages"] = 'Le retour la liste des langues';
+$GLOBALS["tToggleLanguage"] = 'activate/deactivate language ';
+$GLOBALS["defaultLangError"] = 'Language cannot be disabled because they are set to DEFAULT';
+
+
+//  Form Block Titles
+$GLOBALS["thLanguageGeneral"] = 'Les D�tailles de la Langue ';
+$GLOBALS["thMainFiles"] = 'Les Fichiers Principaux ezContents';
+$GLOBALS["thModuleFiles"] = 'Fichiers Module';
+$GLOBALS["thJumpRef"] = '-- Selecter le Block --';
+$GLOBALS["thJumptoBottom"] = 'Sauter � la Fin de la Forme';
+$GLOBALS["thJumptoTop"] = 'Sauter au D�but de la Forme';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Cette forme vous permettez de d�finit les noms de langue et les s�ries de caract�re.';
+$GLOBALS["hLanguage"] = 'Ceci est le nom de la langue.';
+$GLOBALS["hCharSet"] = 'Ceci est la s�rie de caract�re pour cette langue.';
+$GLOBALS["hFileName"] = 'Choisir le fichier de langue pour charger.';
+$GLOBALS["hDirection"] = 'Cette langue est-il �crite part-�-la droite comme anglais, ou bon-�-part comme l Arabe? ';
+
+$GLOBALS["tRtL"] = 'Droite � Gauche';
+$GLOBALS["tLtR"] = 'Gauche to Droite';
+$GLOBALS["tRtLs"] = 'rtl';
+$GLOBALS["tLtRs"] = 'ltr';
+
+//  Error Messages
+$GLOBALS["eNoLanguage"] = 'Le nom de langue ne peut pas �tre gauche vide.';
+$GLOBALS["eNoFile"] = 'Vous devez sp�cifier un fichier de langue.';
+$GLOBALS["eLanguageExists"] = 'Cette langue existe d�j�.';
+$GLOBALS["eNoEnableReady"] = 'Aucun type de fichier de la langue est pr�t �tre rendu capable.';
+
+?>
