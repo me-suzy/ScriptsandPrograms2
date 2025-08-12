97702a2af55034994e10c86acf037ce54c5a0167
me-suzy <me.suzana@gmail.com>
2025-08-13 01:50:50 +0300

    Add Unsorted/comus2.0Final/sample1.php

---
+++ Unsorted/comus2.0Final/sample1.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<table width="600" border="0" cellspacing="0" cellpadding="10">
+  <tr>
+    <td bgcolor="#FFFFCC"> 
+      <div align="center"><font size="5" color="#FF0000">My wicked cool tgp site</font></div>
+    </td>
+  </tr>
+  <tr>
+    <td> 
+      <div align="center"><? include($DOCUMENT_ROOT . "/includes/tgpinc.php"); ?></div>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
