2896c44a578b31a61c98f331cfccdacb46e85973
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:51 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_sgningpwebsitet_65.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_sgningpwebsitet_65.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 65;
+function objfile_65 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Søgning på websitet";
+$objdata['subname'] = "";
+$objdata['extension'] = "search";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
