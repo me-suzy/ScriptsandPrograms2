f1ddefd87a4c5143b3d0d6ea0731abab1f673f54
me-suzy <me.suzana@gmail.com>
2025-08-12 05:41:59 +0300

    Add Content Management/littleblisscms-0.8.2/littleblisscms/tiny_mce/plugins/directionality/langs/zh_cn.js

---
+++ Content Management/littleblisscms-0.8.2/littleblisscms/tiny_mce/plugins/directionality/langs/zh_cn.js
@@ -0,0 +1,5 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+tinyMCELang['lang_directionality_ltr_desc'] = '´Ó×óÍùÓÒµÄ·½Ïò'
+tinyMCELang['lang_directionality_rtl_desc'] = '´ÓÓÒÍù×óµÄ·½Ïò';
