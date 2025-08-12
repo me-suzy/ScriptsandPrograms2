12d8be370d98018c869587ba4d409893d26cbd5c
me-suzy <me.suzana@gmail.com>
2025-08-13 01:00:14 +0300

    Add Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/new_cat.sql

---
+++ Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/new_cat.sql
@@ -0,0 +1,37 @@
+CREATE TABLE al_cat (
+  id tinyint(3) unsigned NOT NULL default '0',
+  name varchar(32) NOT NULL default '',
+  selectable tinyint(4) NOT NULL default '0',
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
+
+INSERT INTO al_cat VALUES ('1', 'Default', '1');
+INSERT INTO al_cat VALUES ('2', '', '0');
+INSERT INTO al_cat VALUES ('3', '', '0');
+INSERT INTO al_cat VALUES ('5', '', '0');
+INSERT INTO al_cat VALUES ('7', '', '0');
+INSERT INTO al_cat VALUES ('4', '', '0');
+INSERT INTO al_cat VALUES ('6', '', '0');
+INSERT INTO al_cat VALUES ('8', '', '0');
+INSERT INTO al_cat VALUES ('9', '', '0');
+INSERT INTO al_cat VALUES ('11', '', '0');
+INSERT INTO al_cat VALUES ('10', '', '0');
+INSERT INTO al_cat VALUES ('13', '', '0');
+INSERT INTO al_cat VALUES ('14', '', '0');
+INSERT INTO al_cat VALUES ('12', '', '0');
+INSERT INTO al_cat VALUES ('15', '', '0');
+INSERT INTO al_cat VALUES ('16', '', '0');
+INSERT INTO al_cat VALUES ('17', '', '0');
+INSERT INTO al_cat VALUES ('18', '', '0');
+INSERT INTO al_cat VALUES ('19', '', '0');
+INSERT INTO al_cat VALUES ('20', '', '0');
+INSERT INTO al_cat VALUES ('21', '', '0');
+INSERT INTO al_cat VALUES ('22', '', '0');
+INSERT INTO al_cat VALUES ('23', '', '0');
+INSERT INTO al_cat VALUES ('24', '', '0');
+INSERT INTO al_cat VALUES ('25', '', '0');
+INSERT INTO al_cat VALUES ('26', '', '0');
+INSERT INTO al_cat VALUES ('27', '', '0');
+INSERT INTO al_cat VALUES ('28', '', '0');
+INSERT INTO al_cat VALUES ('29', '', '0');
+INSERT INTO al_cat VALUES ('30', '', '0');
