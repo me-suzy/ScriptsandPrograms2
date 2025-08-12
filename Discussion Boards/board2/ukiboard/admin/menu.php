f5396c996ea79192def59d440af939898dd6c3f7
me-suzy <me.suzana@gmail.com>
2025-08-12 10:27:37 +0300

    Add Discussion Boards/board2/ukiboard/admin/menu.php

---
+++ Discussion Boards/board2/ukiboard/admin/menu.php
@@ -0,0 +1,15 @@
+<TABLE cellspacing="5" cellpadding="0">
+<TR>
+<TD><TABLE border="2" cellspacing="0" cellpadding="5">
+<TR><TD width="70" class="vivo" align="center" onMouseOver="MenuOver(this);" onMouseOut="MenuOut(this);"><A HREF="../index.php" class="seed"><?php echo $lang[Home]; ?></A>
+</TABLE>
+<TD><TABLE border="2" cellspacing="0" cellpadding="5">
+<TR><TD width="70" class="vivo" align="center" onMouseOver="MenuOver(this);" onMouseOut="MenuOut(this);"><A HREF="category.php" class="seed"><?php echo $lang[Category]; ?></A>
+</TABLE>
+<TD><TABLE border="2" cellspacing="0" cellpadding="5">
+<TR><TD width="70" class="vivo" align="center" onMouseOver="MenuOver(this);" onMouseOut="MenuOut(this);"><A HREF="settings.php" class="seed"><?php echo $lang[Settings]; ?></A>
+</TABLE>
+<TD><TABLE border="2" cellspacing="0" cellpadding="5">
+<TR><TD width="70" class="vivo" align="center" onMouseOver="MenuOver(this);" onMouseOut="MenuOut(this);"><A HREF="password.php" class="seed"><?php echo $lang[Password]; ?></A>
+</TABLE>
+</TABLE>
