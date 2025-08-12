ef5fa91366fe8a99764b8b0d2af2d7e4c01ac3db
me-suzy <me.suzana@gmail.com>
2025-08-12 09:30:17 +0300

    Add Development Tools/fuselogic/fuselogic/modules/tools/class/class.test.php

---
+++ Development Tools/fuselogic/fuselogic/modules/tools/class/class.test.php
@@ -0,0 +1,14 @@
+<?php
+class test{  
+    function test(){
+		    $this->true = True;
+				$this->false = False;
+		}
+    function getTrue(){
+		    return True;
+		}
+		function getFalse(){
+		    return False;
+		}
+}
+?>
