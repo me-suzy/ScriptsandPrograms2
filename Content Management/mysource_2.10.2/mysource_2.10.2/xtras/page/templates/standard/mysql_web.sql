b4e9064b2e26583030124aeb3bd0d7a8acb298d6
me-suzy <me.suzana@gmail.com>
2025-08-12 07:26:52 +0300

    Add Content Management/mysource_2.10.2/mysource_2.10.2/xtras/page/templates/standard/mysql_web.sql

---
+++ Content Management/mysource_2.10.2/mysource_2.10.2/xtras/page/templates/standard/mysql_web.sql
@@ -0,0 +1,30 @@
+    ##############################################
+   ### MySource ------------------------------###
+  ##- Page Template Xtra ------ MySQL --------##
+ #-- Copyright Squiz.net ---------------------#
+##############################################
+## This file is subject to version 1.0 of the
+## MySource License, that is bundled with
+## this package in the file LICENSE, and is
+## available at through the world-wide-web at
+## http://mysource.squiz.net/
+## If you did not receive a copy of the MySource
+## license and are unable to obtain it through
+## the world-wide-web, please contact us at
+## mysource@squiz.net so we can mail you a copy
+## immediately.
+##
+## File: xtras/page/templates/standard/database.sql
+## Desc: SQL Statements to add this template to the web system
+## $Source: /home/cvsroot/xtras/page/templates/standard/mysql_web.sql,v $
+## $Revision: 2.2 $
+## $Author: achadszinow $
+## $Date: 2003/05/22 00:24:31 $
+#######################################################################
+
+#---------------------------------------------------------------------#
+
+CREATE TABLE xtra_page_template_standard (
+  pageid                MEDIUMINT(9) UNSIGNED NOT NULL PRIMARY KEY,
+  parameters            LONGTEXT NOT NULL
+);
