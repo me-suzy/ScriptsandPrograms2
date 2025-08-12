eb956f6d210027ed5efc786959fb5320ae2f85ea
me-suzy <me.suzana@gmail.com>
2025-08-13 01:47:08 +0300

    Add Unsorted/cms master/cms-admin/libs/lang-website.php

---
+++ Unsorted/cms master/cms-admin/libs/lang-website.php
@@ -0,0 +1,40 @@
+<?php
+//=============================================================================
+//
+//     CMS Master :: Content management system
+//     File: /cms-admin/libs/lang-website.php
+//     Version: 1.0
+//     Created date: 28 September 2003
+//     Modification date: 28 September 2003
+//
+//============================================================================
+                                                                                     
+class LangWebsite {
+
+    var $header_current_options = "Current website settings";
+    var $header_edit_title = "Edit website title";
+    var $header_edit_keywords = "Edit website keywords";
+    var $header_edit_description = "Edit website description";
+    
+    var $menu_current_options = "Current settings";
+    var $menu_edit_title = "Edit title";
+    var $menu_edit_keywords = "Edit keywords";
+    var $menu_edit_description = "Edit description";
+    
+    var $table_title = "Title";
+    var $table_keywords = "Keywords";
+    var $table_description = "Description";
+    
+    var $button_update = "Update";
+    
+    var $notes_edit_title = "Place for notes";
+    var $notes_edit_keywords = "Place for notes";
+    var $notes_edit_description = "Place for notes";
+    
+    var $msg_update_title_ok = "Website title updated successfully";
+    var $msg_update_keywords_ok = "Website keywords updated successfully";
+    var $msg_update_description_ok = "Website description updated successfully";
+    
+}
+
+?>
