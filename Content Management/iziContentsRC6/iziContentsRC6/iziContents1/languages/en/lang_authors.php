fcef023b3b6e6fc55a72a601a4b7a17a3346a0c3
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:13 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_authors.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_authors.php
@@ -0,0 +1,75 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintain users';
+
+//  List Headings
+$GLOBALS["tLogin"] = 'Login name';
+$GLOBALS["tUsername"] = 'Username';
+$GLOBALS["tEMail"] = 'E-Mail';
+$GLOBALS["tUsergroup"] = 'User group';
+$GLOBALS["tEnabled"] = 'Enabled';
+
+//  List Functions
+$GLOBALS["tAuthorFilter"] = 'Usergroup filter';
+$GLOBALS["tAddNewAuthor"] = 'Add new user';
+$GLOBALS["tEditAuthor"] = 'Edit user';
+$GLOBALS["tViewAuthor"] = 'View user';
+$GLOBALS["tDeleteAuthor"] = 'Delete user';
+$GLOBALS["tReleaseUser"] = 'Enable/Disable user';
+$GLOBALS["tConfirmRelease"] = 'Are you sure you wish to enable/disable this user?';
+
+//  Form Block Titles
+$GLOBALS["thLogin"] = 'Login Details';
+$GLOBALS["thUser"] = 'User Details';
+$GLOBALS["thConfiguration"] = 'Personal Settings';
+
+//  Form Field Headings
+$GLOBALS["tPassword"] = 'Password';
+$GLOBALS["tRetypePassword"] = 'Retype password';
+$GLOBALS["tAuthorname"] = 'Display name';
+$GLOBALS["tEMail"] = 'E-mail address';
+$GLOBALS["tPrivateEMail"] = 'Hide E-mail address';
+$GLOBALS["tCountry"] = 'Country';
+$GLOBALS["tLanguage"] = 'Language';
+$GLOBALS["tPhone"] = 'Phone';
+$GLOBALS["tFax"] = 'Fax';
+$GLOBALS["tAddress"] = 'Address';
+$GLOBALS["tCity"] = 'City';
+$GLOBALS["tState"] = 'State';
+$GLOBALS["tZip"] = 'ZIP';
+$GLOBALS["tWebsite"] = 'Website';
+$GLOBALS["tComments"] = 'Comments';
+$GLOBALS["tNewsletter"] = 'Newsletter';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'This form lets you edit or create new users that will be able to access the contents.';
+$GLOBALS["hLogin"] = 'This is the login name of the user used on the password screen.';
+$GLOBALS["hPassword"] = 'Enter the password that this user will use to log on to ezContents.';
+$GLOBALS["hRetypePassword"] = 'Re-enter the user\'s password, to ensure that you have typed it correctly.';
+$GLOBALS["hAuthorname"] = 'This field is a full name or nickname for the user. It will be visible to viewers beneath any articles entered by this user if the appropriate Content form field has been checked.';
+$GLOBALS["hUsergroup"] = 'The user group that this user belongs to. User group will determine the admin options available to this user.';
+$GLOBALS["hEMail"] = 'The E-mail address of the user. Together with the user\'s name this will create a hyper link for mail.';
+$GLOBALS["hPrivateEMail"] = 'Check this box if you wish to keep your E-mail address private.';
+$GLOBALS["hCountry"] = 'The country where the user lives (optional).';
+$GLOBALS["hLanguage"] = 'User\'s native language (optional).<br />If this is set, then the user will see menu options, fieldnames, help, etc. in that language rather than the default language for the site.';
+$GLOBALS["hPhone"] = 'User\'s phone number.';
+$GLOBALS["hFax"] = 'User\'s fax number.';
+$GLOBALS["hAddress"] = 'User\'s address.';
+$GLOBALS["hCity"] = 'The city where the user lives.';
+$GLOBALS["hState"] = 'User\'s state';
+$GLOBALS["hZip"] = 'User\'s ZIP code.';
+$GLOBALS["hWebsite"] = 'User\'s website address.';
+$GLOBALS["hComments"] = 'User\'s comments.';
+$GLOBALS["hNewsletter"] = 'User is subscribed for newsletter.';
+
+//  Error Messages
+$GLOBALS["eLoginExists"] = 'Login name already exists, please choose another.';
+$GLOBALS["eLoginEmpty"] = 'Login name can not be left empty.';
+$GLOBALS["ePasswordError"] = 'Retyped password is not the same as the given password.';
+$GLOBALS["eNameEmpty"] = 'Author display name can not be left empty.';
+$GLOBALS["eNameExists"] = 'An author with this display name already exists.';
+$GLOBALS["eEMailEmpty"] = 'Author e-mail can not be left empty.';
+$GLOBALS["eEmailIncorrect"]   = 'The e-mail that you entered is an invalid format.';
+
+?>
