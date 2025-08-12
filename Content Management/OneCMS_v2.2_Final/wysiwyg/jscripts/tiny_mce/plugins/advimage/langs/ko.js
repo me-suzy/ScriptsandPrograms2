216ccff79372edf17b286d497b0d8970b8493594
me-suzy <me.suzana@gmail.com>
2025-08-12 07:42:20 +0300

    Add Content Management/OneCMS_v2.2_Final/wysiwyg/jscripts/tiny_mce/plugins/advimage/langs/ko.js

---
+++ Content Management/OneCMS_v2.2_Final/wysiwyg/jscripts/tiny_mce/plugins/advimage/langs/ko.js
@@ -0,0 +1,6 @@
+// KO lang variables
+
+tinyMCELang['lang_insert_image_alt2'] = '±×¸² Á¦¸ñ';
+tinyMCELang['lang_insert_image_onmousemove'] = '´ëÃ¼ ÀÌ¹ÌÁö'
+tinyMCELang['lang_insert_image_mouseover'] = '¸¶¿ì½º¸¦ ¿Ã·ÈÀ» ¶§';
+tinyMCELang['lang_insert_image_mouseout'] = '¸¶¿ì½º¸¦ ³»·ÈÀ» ¶§';
