b3a8484a5ff8a0d48320b7f9a0849605406b0bc1
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:31 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_changedby.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_changedby.en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Editor';
+$LANG['text_1'] = 'Select the editor for the selected records';
+$LANG['text_2'] = 'Changed by: ';
+?>
