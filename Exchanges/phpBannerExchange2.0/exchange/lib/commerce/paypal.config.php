6585ba7b1f9162a5bd9a4823521c7fa3449e03ea
me-suzy <me.suzana@gmail.com>
2025-08-12 13:58:25 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/lib/commerce/paypal.config.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/lib/commerce/paypal.config.php
@@ -0,0 +1,20 @@
+<?php
+$file_rev="041305";
+//
+// phpBannerExchange auto-generated config file
+// Change this file at your own risk!
+//
+
+$businessname="your@paypalaccount.com";
+$ipn_page="ipn.php";
+$propername="PayPal";
+
+$payment_currency="US";
+
+$currency_sign="\$";
+$currency_int="USD";
+$decimal_separator=".";
+$thousands_separator=",";
+$places="2";
+
+?>
