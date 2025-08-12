345cc55feafcf13785d54e63f5f3583c7ebc64f4
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:32 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/brugerregistrering.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/brugerregistrering.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 15;
+$_objchilds[] = '_child_blivregistreretbruger_16';
+function objfile_15 () {
+$obj = owNew('document');
+$objdata['name'] = "Brugerregistrering";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
