84e896fa9474a5ba6ef821de100bdd124d32f5d0
me-suzy <me.suzana@gmail.com>
2025-08-12 14:14:06 +0300

    Add File Manipulation/cep/cep/help/nada.php

---
+++ File Manipulation/cep/cep/help/nada.php
@@ -0,0 +1,32 @@
+<?php
+/*------------------------------------------------------------------------------
+CJG EXPLORER PRO v3.2 - WEB FILE MANAGEMENT - Copyright (C) 2003 CARLOS GUERLLOY
+CJGSOFT Software
+cjgexplorerpro@guerlloy.com
+guerlloy@hotmail.com
+carlos@weinstein.com.ar
+Buenos Aires, Argentina
+--------------------------------------------------------------------------------
+This program is free software; you can  redistribute it and/or  modify it  under
+the terms   of the   GNU General   Public License   as published   by the   Free
+Software Foundation; either  version 2   of the  License, or  (at  your  option)
+any  later version. This program  is  distributed in  the hope that  it  will be
+useful,  but  WITHOUT  ANY  WARRANTY;  without  even  the   implied  warranty of
+MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR  PURPOSE.  See the  GNU   General
+Public License for   more details. You  should have received  a copy of  the GNU
+General Public License along  with this  program; if   not, write  to the   Free
+Software  Foundation, Inc.,  59 Temple Place,  Suite 330, Boston,  MA 02111-1307
+USA
+------------------------------------------------------------------------------*/
+
+?>
+<HTML>
+<HEAD>
+<META NAME="Author" CONTENT="Carlos Guerlloy">
+<META NAME="Description" CONTENT="cjgExplorer Pro v3.2">
+<META http-equiv="Expires" content="MON, 4 JUL 1967 12:00:00 GMT">
+<title>CJG</title>
+<STYLE><?php include("style.php");?></STYLE>
+<BODY>
+</BODY>
+</HTML>
