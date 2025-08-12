5387d9db30ca243cf19dc7d9f490c38126b5b278
me-suzy <me.suzana@gmail.com>
2025-08-13 02:21:10 +0300

    Add Unsorted/datingscript/languages/hr/inst.php

---
+++ Unsorted/datingscript/languages/hr/inst.php
@@ -0,0 +1,120 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.1.1                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 04/05/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               inst.php                         #
+# File purpose            Installation language file       #
+# File created by         AzDG <support@azdg.com>          #
+############################################################
+
+define('C_HTML_DIR','ltr'); // HTML direction for this language
+define('C_CHARSET', 'iso-8859-1'); // HTML charset for this language
+
+$y=array( // $y - installation phrases array
+'<b>Please change short_open_tag variable in your php.ini file to:<br><br>short_open_tag=On</b>', //0
+'OK', //1
+'FALSE', //2
+'INSTALLATION', //3
+'Select installation language', //4
+'Languages', //5
+'No languages in languages/ directory. Please upload them.', //6
+'Accepting License', //7
+'Please upload GPL Lisence for accept it. File:gpl.txt', //8
+'Please fix all errors and continue installation', //9
+'Update Page', //10
+'Don`t Accept', //11
+'Accept', //12
+'STEP', //13 - For example: Step 1
+'Checking for requirement values', //14
+'Requirement', //15
+'Minimal configuration', //16
+'Server configuration', //17
+'Results', //18
+'PHP Version', //19
+'MySQL Version', //20
+'Checking for directories and files for write', //21
+'Checking for write', //22
+'Please fix all errors and continue installation', //23
+'Next Step', //24
+'Creating configuration', //25
+'Please enter url', //26
+'Please enter path', //27
+'Please enter site name', //28
+'Please enter mysql host', //29
+'Please enter mysql user name', //30
+'Please enter mysql password', //31
+'Please enter mysql database name', //32
+'Please enter admin login', //33
+'Please enter admin password', //34
+'Please enter admin email', //35
+'Please check all fields to match your web sites values', //36
+'Script URL', //37
+'Without "/" at end. You must to enter url were script located.', //38
+'Server path to script', //39
+'Without "/" at end. Internal server path. Please ask it from your hosting if it empty.', //40
+'Website Name', //41
+'MySQL Data', //42
+'MySQL hostname', //43
+'Usually', //44
+'MySQL Database Name', //45
+'You must create it, if database does`nt exist', //46
+'MySQL Username', //47
+'MySQL Password', //48
+'Tables prefix', //49
+'Administrator data', //50
+'Admin login name', //51
+'Admin password', //52
+'Admin email', //53
+'Admin language', //54
+'Language for administration area', //55
+'Please return back and correct Script URL variable - don`t write "/" at end please', //56
+'Please return back and correct Server Path variable - don`t write "/" at end please', //57
+'Please return back and correct Server Path variable', //58
+'Can`t connect to server<br>Your mysql login or mysql password is wrong.<br>Check it in config file', //59
+'Can`t connect to server<br>Database don`t exist<br>Or Change Database name in config', //60
+'Installing MySQL tables', //61
+'Table name', //62
+'Install status', //63
+'Created or already exist', //64
+'Don`t created', //65
+'Installation results', //66
+'Successfully', //67
+'Admin page', //68
+'Main page', //69
+'Secure notes', //70
+'Remove install.php from server', //71
+'Manual installation', //72
+'', //73
+'', //74
+'', //75
+'', //76
+'', //77
+'', //78
+'', //79
+'', //80
+'', //81
+'', //82
+'', //83
+'', //84
+'', //85
+'', //86
+'', //87
+'', //88
+'', //89
+'', //90
+'', //91
+'', //92
+'', //93
+'', //94
+'', //95
+'', //96
+'', //97
+'', //98
+'' //99
+);
+?>
