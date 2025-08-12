90d58df068854c6c00d3826ce313f3ea22dd00b8
me-suzy <me.suzana@gmail.com>
2025-08-13 00:52:59 +0300

    Add Unsorted/Auto-Surf-V2.5_eng/index.php

---
+++ Unsorted/Auto-Surf-V2.5_eng/index.php
@@ -0,0 +1,13 @@
+<?php
+require('./prepend.inc.php');
+?>
+
+<?
+include("./templates/main-header.txt");
+?>
+
+<br><font size="3"><? echo "$starttext"; ?>
+
+<?
+include("./templates/main-footer.txt");
+?>
