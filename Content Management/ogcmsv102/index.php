4e0995a785323b5b182041921e47155a3de011d9
me-suzy <me.suzana@gmail.com>
2025-08-12 07:37:49 +0300

    Add Content Management/ogcmsv102/index.php

---
+++ Content Management/ogcmsv102/index.php
@@ -0,0 +1,15 @@
+<?php
+/*-----------------------------------------------------------------------------+
+|                            OG CMS v1.02                         |
++------------------------------------------------------------------------------+
+| This file is component of OG CMS :: web management system                    |
+|                                                                              |
+|                    Please, send any comments, suggestions and bug reports to |
+|                                                      olegu@soemme.no         |
+| Original Author: Vidar Løvbrekke Sømme                                       |
+| Author email   : olegu@soemme.no                                             |
+| Project website: http://www.soemme.no/                                       |
+| Licence Type   : FREE                                                        |
++------------------------------------------------------------------------------+ */
+include 'post_list.php';
+?>
