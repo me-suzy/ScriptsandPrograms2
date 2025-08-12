46b7fb50194bac0134459cd22ce1d19a7f799047
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:23 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/affiliate_logout.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/affiliate_logout.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Partnerprogramm');
+define('HEADING_TITLE', 'Partnerprogramm abmelden');
+
+define('TEXT_INFORMATION', 'Sie wurden abgemeldet.');
+define('TEXT_INFORMATION_ERROR_1', 'Sie konnten nicht abgemeldet werden!');
+define('TEXT_INFORMATION_ERROR_2', 'Sie waren nicht angemeldet und k&ouml;nnen deshalb nicht abgemeldet werden!');
+?>
