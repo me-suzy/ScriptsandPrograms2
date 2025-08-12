8cb9cccee98361fa4244675b1522068e2c518e56
me-suzy <me.suzana@gmail.com>
2025-08-12 13:47:01 +0300

    Add Education/translide/translide.php

---
+++ Education/translide/translide.php
@@ -0,0 +1,157 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>Translide - Translation Solutions</title>
+
+
+  <meta http-equiv="content-type"
+ content="text/html; charset=ISO-8859-1">
+
+
+
+
+
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</head>
+
+<body leftmargin=0 topmargin=0 marginheight="0" marginwidth="0"  >
+
+
+
+
+
+
+<table border="0" cellpadding="0" cellspacing="0">
+<tr>
+	<td><img src="t_11.gif" width="10" height="9" alt="" border="0"></td>
+	<td background="t_13.gif"><img src="t_12.gif" width="6" height="9" alt="" border="0"></td>
+	<td background="t_13.gif" align="right"><img src="t_14.gif" width="6" height="9" alt="" border="0"></td>
+	<td><img src="t_15.gif" width="10" height="9" alt="" border="0"></td>
+</tr>
+<tr valign="top">
+	<td background="t_fon_left.gif"><img src="t_21.gif" width="10" height="6" alt="" border="0"></td>
+	<td rowspan="2" colspan="2">
+
+
+<table><tr>
+<td>
+<img src="globe.jpg" height="50" alt="" border="0">
+</td>
+<td>
+
+<p>Translide - Translation Software
+
+</td></tr></table>
+
+<hr>
+
+
+<form name="form1" method="post" action="translide2.php">
+
+<p>
+<input type="radio" name="lang1" value="EnglishTOChinese" checked>English To Chinese
+<input type="radio" name="lang1" value="EnglishTOFrench">English To French
+<input type="radio" name="lang1" value="EnglishTOGerman">English To German
+<input type="radio" name="lang1" value="EnglishTOJapanese">English To Japanese
+<input type="radio" name="lang1" value="EnglishTOKorean">English To Korean
+<input type="radio" name="lang1" value="EnglishTOPortuguese">English To Portuguese
+<input type="radio" name="lang1" value="EnglishTOSpanish">English To Spanish
+<input type="radio" name="lang1" value="ChineseTOEnglish">Chinese To English
+<input type="radio" name="lang1" value="FrenchTOEnglish">French To English
+<input type="radio" name="lang1" value="FrenchTOGerman">French To German
+<input type="radio" name="lang1" value="GermanTOEnglish">German To English
+<input type="radio" name="lang1" value="GermanTOFrench">German To French
+<input type="radio" name="lang1" value="ItalianTOEnglish">Italian To English
+<input type="radio" name="lang1" value="JapaneseTOEnglish">Japanese To English
+<input type="radio" name="lang1" value="PortugueseTOEnglish">Portuguese To English
+<input type="radio" name="lang1" value="RussianTOEnglish">Russian To English
+<input type="radio" name="lang1" value="SpanishTOEnglish">Spanish To English
+</p>   
+<br><br>    
+
+
+
+
+
+<textarea NAME="sst" COLS="60" ROWS="15">
+</textarea>
+
+
+
+  <input type="submit" name="Submit" value="Translate Now" size="100">
+  
+</p>
+</form>
+
+
+
+
+
+
+<center>
+<p>Designed by <a href="http://www.slidecorp.com" onclick="exit=false">Slidecorp.com</a></p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</td>
+	<td background="t_fon_right.gif"><img src="t_23.gif" width="10" height="6" alt="" border="0"></td>
+</tr>
+<tr valign="bottom">
+	<td background="t_fon_left.gif"><img src="t_31.gif" width="10" height="7" alt="" border="0"></td>
+	<td background="t_fon_right.gif"><img src="t_33.gif" width="10" height="7" alt="" border="0"></td>
+</tr>
+<tr>
+	<td><img src="t_41.gif" width="10" height="10" alt="" border="0"></td>
+	<td background="t_fon_bot.gif"><img src="t_42.gif" width="6" height="10" alt="" border="0"></td>
+	<td background="t_fon_bot.gif" align="right"><img src="t_44.gif" width="6" height="10" alt="" border="0"></td>
+	<td ><img src="t_45.gif" width="10" height="10" alt="" border="0"></td>
+</tr>
+</table>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
