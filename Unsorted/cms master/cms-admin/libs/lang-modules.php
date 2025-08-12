77fc2daeea90fb51b60952a5443db9d2ba657bd3
me-suzy <me.suzana@gmail.com>
2025-08-13 01:47:03 +0300

    Add Unsorted/cms master/cms-admin/libs/lang-modules.php

---
+++ Unsorted/cms master/cms-admin/libs/lang-modules.php
@@ -0,0 +1,25 @@
+<?php
+//=============================================================================
+//
+//     CMS Master :: Content management system
+//     File: /cms-admin/libs/lang-modules.php
+//     Version: 1.0
+//     Created date: 28 September 2003
+//     Modification date: 28 September 2003
+//
+//============================================================================
+                                                                                     
+class LangModules {
+
+    var $header_modules_list = "List of installed modules";
+
+    var $msg_no_modules = "There are no modules installed";
+    
+    var $table_module_name = "Name";
+    var $table_module_cmd = "Action";
+    
+    var $cmd_module_go = "Use module";
+
+}
+
+?>
