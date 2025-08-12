fe05fe31e57e64f99633eb0868c2632145b38601
me-suzy <me.suzana@gmail.com>
2025-08-12 10:40:15 +0300

    Add Discussion Boards/giftlist/install/delgift.php

---
+++ Discussion Boards/giftlist/install/delgift.php
@@ -0,0 +1,25 @@
+<?php
+include "header.php";
+// require('db_connect.php');	// database connect script.
+@mysql_select_db($db_name) or die( "Unable to select database");
+
+
+
+// mysql_query("DELETE FROM gifts WHERE gift_id='$fileId'")
+// or die(mysql_error());
+
+$query="UPDATE gifts SET del_gift='yes' WHERE gift_id='$fileId'";
+@mysql_select_db($db_name) or die( "Unable to select database");
+mysql_query($query);
+
+
+?>
+
+<br><br>
+Record Deleted
+
+
+
+
+
+
