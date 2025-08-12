b5ba66068de389aa6cc1ac5335a4fce17cb68f28
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:29 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_main.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_main.php
@@ -0,0 +1,70 @@
+<?php
+
+$GLOBALS["tLoggedInAs"] = 'Sie sind eingeloggt als';
+$GLOBALS["tNotLoggedIn"] = 'Sie sind nicht eingeloggt.';
+$GLOBALS["tAdminFunctions"] = 'Administrationsfunktionen';
+$GLOBALS["tPreferences"] = 'Voreinstellungen';
+$GLOBALS["tMemberList"] = 'Mitgliederliste';
+$GLOBALS["tRegister"] = 'Registrieren';
+$GLOBALS["tReadMore"] = 'Details';
+$GLOBALS["tReturn"] = '&laquo; zur&uuml;ck';
+$GLOBALS["tArticlePostedOn"] = 'geschrieben am';
+$GLOBALS["tArticleUpdatedOn"] = 'aktualisiert am';
+$GLOBALS["tArticleBy"] = 'von';
+$GLOBALS["tArticlePostedBy"] = 'geschrieben von';
+$GLOBALS["tFramesUsed"] = 'Diese Seite verwendet Frames.';
+$GLOBALS["tNonFramesClick"] = 'Bitte klicken Sie hier f&uuml;r eine Tabellen-Version dieser Seite ohne Frames.';
+$GLOBALS["tNonFramesWarning"] = 'Bitte beachten Sie, dass verschachtelte Tabellen, Cookies und Javascript verwendet werden. Wenn Ihr Browser dies nicht unterst&uuml;tzt oder wenn Sie diese Funktionen abgeschaltet haben, k&ouml;nnte die Seite nicht korrekt dargestellt werden.';
+$GLOBALS["tLanguage"] = 'Sprache';
+$GLOBALS["tSite"] = 'Seite';
+$GLOBALS["tMasterSite"] = 'Hauptseite';
+$GLOBALS["tTheme"] = 'Theme';
+$GLOBALS["tMasterTheme"] = 'Standard Theme';
+$GLOBALS["tBookmarkPage"] = 'Zu den Favoriten';
+$GLOBALS["tPrivateOption"] = 'Private Optionen';
+$GLOBALS["tPrivateEMail"] = '*** PRIVAT ***';
+$GLOBALS["tPrinterFriendly"] = ' Druckerfreundliche Version';
+$GLOBALS["tPDFPrint"] = ' PDF Version';
+$GLOBALS["tTellFriend"] = ' Tell-a-Friend Version';
+$GLOBALS["tUnrated"] = 'nicht bewertet';
+$GLOBALS["tRate"] = 'Bewertungen ansehen';
+$GLOBALS["tComment"] = 'Kommentare zu diesem Artikel';
+$GLOBALS["tViewComments"] = 'Kommentare ansehen';
+$GLOBALS["tComments"] = 'Andere Kommentare zu diesem Artikel';
+$GLOBALS["tYourComments"] = 'Ihr Kommentar zu diesem Artikel';
+$GLOBALS["tRating"] = 'Bewertung';
+$GLOBALS["tRatings"] = 'Bewertungen f&uuml;r diesen Artikel';
+$GLOBALS["tYourRating"] = 'Ihre Bewertung f&uuml;r diesen Artikel';
+$GLOBALS["tScore"] = 'Ergebnis';
+$GLOBALS["tVoters"] = 'Anzahl an Stimmen';
+$GLOBALS["tVotersPercent"] = '% der Stimmen';
+$GLOBALS["tRatingMessage1"] = 'User haben abgestimmt und diesen Artikel so bewertet';
+$GLOBALS["tRatingMessage2"] = 'Users haben diesen Artikel bewertet mit diesem Durchschnitt';
+$GLOBALS["tRatingMessage3"] = 'auf einer Skala von';
+$GLOBALS["tRatingMessage4"] = 'bis';
+$GLOBALS["tRatingBad"] = 'v&ouml;llig uninteressant';
+$GLOBALS["tRatingGood"] = 'sehr interessant';
+$GLOBALS["tAllCategories"] = 'Alle Betriebe';
+
+$GLOBALS["editorTags"]  = array ('html','include','ilink','pagelink','email','file','contentlist','grouplist','image','sidebar','b','ol','ul'); 
+$GLOBALS["editorParms"] = array('','','','','','','','','logo.gif, rightborder, The EzContents logo','sbright50, Example of Sidebar','','',''); 
+$GLOBALS["editorTexts"] = array('HTML Tag','Include Tag','Internal Link Tag','Page Link Tag','Email Tag','File Tag','Content List Tag','Group List','Image Tag','Sidebar Tag','Bold Style','Ordered List','Unordered List'); 
+$GLOBALS["editorIcons"] = array ('html','include','ilink','pagelink','email','file','contentlist','grouplist','img','sidebar','bold','list','ulist'); 
+
+$GLOBALS["tStartseiteAdmin"] = 'Startseite Admin';
+$GLOBALS["tHilfe"] = 'Hilfe';
+$GLOBALS["tHomepage"] = 'Zur Homepage';
+
+$GLOBALS["SearchWebsite"] = 'WEBSITE DURCHSUCHEN';
+$GLOBALS["Startseite"] = 'Startseite';
+
+//tell a friend
+$GLOBALS["tMailTo"] = 'Empf&auml;nger';
+$GLOBALS["tMailFromName"] = 'Absendename';
+$GLOBALS["tMailFromEMail"] = 'Absendeadresse';
+$GLOBALS["tMailSubject"] = 'Betreff';
+$GLOBALS["tMailMessage"] = 'Nachricht';
+$GLOBALS["tMailSend"] = 'Absenden';
+$GLOBALS["tMailReset"] = 'Zur&uuml;cksetzen';
+
+?>
