db6d1d09be08425a996322db42c9fe614a84d1d0
me-suzy <me.suzana@gmail.com>
2025-08-12 15:00:35 +0300

    Add File Manipulation/phpfxp_3/phpfxp_3/config.inc.php

---
+++ File Manipulation/phpfxp_3/phpfxp_3/config.inc.php
@@ -0,0 +1,36 @@
+<?
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        PHP FXP 3.0
+        by Harald Meyer (webmaster@harrym.nu)
+        Feel free to use it, but don't delete these lines.
+        PHP FXP is Freeware but all links to the PHP FXP homepage must not be deleted!
+        If you want to use PHP FXP COMMERCIAL please contact me.
+        Please send me modified versions!
+        If you use it on your page, a link back to its homepage (to PHP FXP's homepage)
+        would be highly appreciated.
+        Homepage: http://fxp.harrym.nu
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+ /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        File: config.inc.php
+        Description: -
+        Last update: 27-09-2002
+        Created by: Harald Meyer
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+//General
+$mirrordir=false;
+
+//URL
+$url="http://www.webspace.domain/fxp";
+$path="/home/myaccount/www/fxp/";
+
+$storepath=$path."store/"; //777
+
+$datapath=$path."data/";
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        END
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+?>
