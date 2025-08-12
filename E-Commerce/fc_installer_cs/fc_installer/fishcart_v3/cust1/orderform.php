9386c7dc1ec6b84f95ace665a838c1a8b5035bea
me-suzy <me.suzana@gmail.com>
2025-08-12 12:11:29 +0300

    Add E-Commerce/fc_installer_cs/fc_installer/fishcart_v3/cust1/orderform.php

---
+++ E-Commerce/fc_installer_cs/fc_installer/fishcart_v3/cust1/orderform.php
@@ -0,0 +1,151 @@
+<?php 
+// Final stop to process an order.
+
+require("cc.php");
+
+$sd=odbc_connect("TCP/IP PORTNUM","USERID","USERPW");
+$lr=odbc_exec($sd,"select langfinl from LANGTABLE where langid=$lid");
+if($lr==""){ echo "Error getting the language zone record.<br>"; exit; }
+$lc=odbc_fetch_row($lr);
+$final=odbc_result($lr,"langfinl");
+odbc_free_result($lr);
+
+if(($onoff!="on")&&($onoff!="off")){
+	echo "</center><p><b>";
+	echo "Please click the &quot;Back&quot; button on your browser and ";
+	echo "select either the Online or Offline order method.  Thank you.</b>\n";
+	exit;
+}
+
+if($billing_first=="" || $billing_last=="" ||
+  ($billing_address1=="" && $billing_address2=="") ||
+  $billing_city=="" || $billing_state=="" ||
+  $billing_zip==""  || $billing_email=="" ||
+  ($onoff=="on"&&$cc_name=="") ||
+  ($onoff=="on"&&$ccexp_month=="") ||
+  ($onoff=="on"&&$ccexp_year=="") ||
+  ($onoff=="on"&&$cc_number=="") ){
+  echo "</center><p><b>A required field has been left blank. ";
+  echo "Please click the &quot;Back&quot; button on your browser ";
+  echo "and make sure they are properly filled in.  Thank you.</b>\n";
+  exit;
+}
+
+if($onoff=="on"||($onoff=="off"&&$cc_number!="")){
+ if($cctype==""){
+  echo "</center><p><b>";
+	echo "Please click the &quot;Back&quot; button on your browser and ";
+	echo "select the type of credit card.  Thank you.</b>\n";
+  exit;
+ }
+ /* verify the cc */
+ $cc_number=ereg_replace(" ","",$cc_number);
+ $cc_number=ereg_replace("-","",$cc_number);	
+ $cctype=strtoupper($cctype);
+ $rv=CC($cctype,$cc_number);
+ if($rv==0){?>
+</center><br><b>
+The credit card number as entered does not appear to be a valid
+number.  Please click the &quot;Back&quot; button on your browser and
+make sure that it is entered correctly.  To help make the number more 
+readable, you may separate the credit card number into groups with blanks
+or spaces as shown by the examples below.  Thank you.</b>
+<pre>
+1111 2222 3333 4444
+  
+  or
+
+1111-2222-3333-4444
+</pre>
+<?php 
+  exit;
+ }
+ 
+ $ccexp_year=(int)ereg_replace(" ","",$ccexp_year);
+ // assume no CCs have expire years over 5 years out
+ $lower=(int)date("Y",time());
+ $upper=5+$lower;
+ $res=(int)ereg("^[0-9]+$", $ccexp_year);
+ if($res==0 || $lower > $ccexp_year || $upper < $ccexp_year){
+  echo "</center><p><b>";
+	echo "Please click the &quot;Back&quot; button on your browser and ";
+	echo "enter a valid credit card expiration year.  Thank you.</b>\n";
+  exit;
+ }
+
+ $ccexp_month=(int)ereg_replace(" ","",$ccexp_month);
+ $res=(int)ereg("^[0-9]+$", $ccexp_month);
+ if($res==0 || $ccexp_month < 1 || $ccexp_month > 12){
+  echo "</center><p><b>";
+	echo "Please click the &quot;Back&quot; button on your browser and ";
+	echo "enter a valid credit card expiration month.  Thank you.</b>\n";
+  exit;
+ }
+
+ if(strlen($ccexp_year)!=4){
+  echo "</center><p><b>";
+	echo "Please click the &quot;Back&quot; button on your browser and ";
+	echo "enter a four digit credit card expiration year.  Thank you.</b>\n";
+  exit;
+ }
+}  // onoff=="on"
+
+$tph=ereg_replace("\(","",$billing_phone);
+$tph=ereg_replace("\)","",$tph);
+$tph=ereg_replace("-","",$tph);
+$tph=ereg_replace("/","",$tph);
+$tph=ereg_replace("\.","",$tph);
+$tph=ereg_replace(" ","",$tph);
+$billing_phone=ereg_replace(" ","",$tph);
+
+$tph=ereg_replace("\(","",$shipping_phone);
+$tph=ereg_replace("\)","",$tph);
+$tph=ereg_replace("-","",$tph);
+$tph=ereg_replace("/","",$tph);
+$tph=ereg_replace("\.","",$tph);
+$tph=ereg_replace(" ","",$tph);
+$shipping_phone=ereg_replace(" ","",$tph);
+
+$billing_zip=ereg_replace("-","",$billing_zip);
+$billing_zip=ereg_replace("_","",$billing_zip);
+$billing_zip=ereg_replace(" ","",$billing_zip);
+$shipping_zip=ereg_replace("-","",$shipping_zip);
+$shipping_zip=ereg_replace("_","",$shipping_zip);
+$shipping_zip=ereg_replace(" ","",$shipping_zip);
+
+$sd=odbc_connect("TCP/IP PORTNUM","USERID","USERPW");
+$vr=odbc_exec($sd,"select * from VENDORTABLE where vendzid=$zid"); 
+$vc=odbc_fetch_row($vr);
+
+/* filter nasty shell escapes from the email address */
+$billing_email=EscapeShellCmd($billing_email);
+
+/* update inventory quantity in a central place */
+$lr=odbc_exec($sd,"select * from ORDERLINE where orderid='$cartid'"); 
+$lc=odbc_fetch_row($lr);
+if($lc>0){
+  while($lc){
+	$sku=odbc_result($lr,"sku");
+	$qty=(int)odbc_result($lr,"qty");
+	$pr=odbc_exec($sd,"select produseinvq,prodinvqty from PRODTABLE ".
+		"where prodzid=$zid and prodsku='$sku'");
+	$pc=odbc_fetch_row($pr);
+	$useq=(int)odbc_result($pr,"produseinvq");
+	if($useq>0){
+	 $iqty=(int)odbc_result($pr,"prodinvqty");
+	 $iqty=$iqty-$qty;
+	 $pr=odbc_exec($sd,"update PRODTABLE set prodinvqty=$iqty ".
+		"where prodzid=$zid and prodsku='$sku'");
+	}
+	odbc_free_result($pr);
+	$lc=odbc_fetch_row($lr);
+  }
+}
+
+if($onoff=="on"){
+	$fd=odbc_result($vr,"vendonline");
+}elseif($onoff=="off"){
+	$fd=odbc_result($vr,"vendofline");
+}
+require("$fd");
+?>
