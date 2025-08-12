0c230b782124dcb56ee6d1981ef638487c988b62
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:58 +0300

    Add Content Management/metajour2_1/metajour/lang/company.en.php

---
+++ Content Management/metajour2_1/metajour/lang/company.en.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Companies';
+$LANG['label_name'] = 'Name';
+$LANG['label_address1'] = 'Address 1';
+$LANG['label_address2'] = 'Address 2';
+$LANG['label_address3'] = 'Address 3';
+$LANG['label_address4'] = 'Address 4';
+$LANG['label_postalcode'] = 'Postal';
+$LANG['label_city'] = 'City';
+$LANG['label_state'] = 'State/Region';
+$LANG['label_country'] = 'Country';
+$LANG['label_telephone1'] = 'Phone 1';
+$LANG['label_telephone2'] = 'Phone 2';
+$LANG['label_telephone3'] = 'Phone 3';
+$LANG['label_telefax'] = 'Telefax';
+$LANG['label_website'] = 'Website';
+$LANG['label_email'] = 'Email';
+$LANG['label_timezone'] = 'Timezone';
+$LANG['label_comment'] = 'Comment';
+?>
