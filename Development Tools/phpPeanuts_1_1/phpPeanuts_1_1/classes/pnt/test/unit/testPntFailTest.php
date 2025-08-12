e165158719798df2f20e6726125372293d745cfd
me-suzy <me.suzana@gmail.com>
2025-08-12 10:05:16 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/pnt/test/unit/testPntFailTest.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/pnt/test/unit/testPntFailTest.php
@@ -0,0 +1,56 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+   	
+require_once('../classes/pnt/unit/classPntTestCase.php');
+require_once('../classes/pnt/test/unit/classObjectToTest.php');
+
+/**
+ * @package pnt/test/unit
+ * @author  Henk Verhoeven, MetaClass <henk@phpPeanuts.org>
+ */
+class PntFailTest extends PntTestCase {
+	
+	var $obj1;
+	var $obj2;
+	
+	function setUp() {
+		$this->obj1 =& new ObjectToTest();
+    	$this->obj2 =& new ObjectToTest();
+    	$this->obj2->var1 = 'value of obj2->var1 explicitly set by testFail';
+		
+	}
+
+    function testFail() {
+
+    	$this->assertEquals('yes', 123, 'mixed');
+    	$this->assertEquals(2, 4, 'with numbers');
+    	$this->assertEquals('yes', 'no', 'with Strings');
+    	$this->assertEquals($this->obj1, $this->obj2, 'with objects');
+
+		$this->assertNotNull(null, 'null');
+		$this->assertNull(123, '123');
+
+		$this->assertSame('12', 12, '12');
+		$this->assertNotSame($this->obj1, $this->obj1, '$this->obj1');
+
+     	$this->assertTrue(false, 'false');
+		$this->assertFalse(true, 'true');
+
+		$this->assertRegExp('~.php~', 'myFile.txt', 'RegExp');
+     	
+		Assert::ofType('boolean', 1);
+		Assert::ofType('string', 123);
+		Assert::ofType('number', '123,45');
+		Assert::ofType('integer', null);
+		Assert::ofType('object', $this->obj1);
+		Assert::ofType('ObjectToTest', $this);
+		
+		Assert::ofAnyType(array('integer', 'boolean'), '12');
+    }
+    
+}
+
+return 'PntFailTest';
+?>
