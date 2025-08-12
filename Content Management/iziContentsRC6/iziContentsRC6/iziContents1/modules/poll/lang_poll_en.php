9fcd05457764286918394de6e0919d3ccdc3f649
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:59 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/poll/lang_poll_en.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/poll/lang_poll_en.php
@@ -0,0 +1,51 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'maintain polls';
+$GLOBALS["tSubmitFormTitle"] = 'Submit Poll';
+
+//  List Headings
+$GLOBALS["tQuestion"] = 'Question';
+$GLOBALS["tOption"] = 'Option';
+$GLOBALS["tPostedBy"] = 'Posted by';
+$GLOBALS["tPollType"] = 'Poll Type';
+$GLOBALS["tPublishDate"] = 'Publish Date';
+$GLOBALS["tExpiryDate"] = 'Expiry Date';
+$GLOBALS["tStatus"] = 'Status';
+
+//  List Functions
+$GLOBALS["tAddNewPoll"] = 'Add a poll';
+$GLOBALS["tEditPoll"] = 'Edit poll';
+$GLOBALS["tDeletePoll"] = 'Delete poll';
+$GLOBALS["tReleasePoll"] = 'Release poll';
+$GLOBALS["tEditOptions"] = 'Edit poll options';
+
+$GLOBALS["tAddNewPollOption"] = 'Add a poll option';
+$GLOBALS["tEditPollOption"] = 'Edit poll option';
+$GLOBALS["tDeletePollOption"] = 'Delete poll option';
+
+//  Form Field Headings
+$GLOBALS["tCategory"] = 'Category';
+
+//  Poll View Texts
+$GLOBALS["tMultiVotes"] = 'Multi-vote';
+$GLOBALS["tSingleVote"] = 'Single-vote';
+
+$GLOBALS["tSubmitPoll"] = 'Submit Poll';
+$GLOBALS["tContact"] = 'Contact';
+$GLOBALS["tEMail"] = 'E-Mail';
+$GLOBALS["tWebPage"] = 'Web Page';
+$GLOBALS["tVoteButton"] = 'Vote';
+
+//  Poll Submission Texts
+$GLOBALS["tThankYou"] = 'Thank you for submitting your poll.';
+$GLOBALS["tReleaseRequired"] = 'Note that it will not be displayed on the polls page until it has been released by the board editor.';
+$GLOBALS["tClickToReturn"] = 'Click here to return to the';
+$GLOBALS["tPollPage"] = 'Poll Page';
+
+//  Security Texts
+$GLOBALS["tMustLogin"] = 'You must';
+$GLOBALS["tLogin"] = 'log in';
+$GLOBALS["tToSubmit"] = 'to submit a Poll';
+
+?>
