4b5827c3e05d11963847b91f441c8f725ef135b0
me-suzy <me.suzana@gmail.com>
2025-08-13 02:28:04 +0300

    Add Unsorted/destiney/include/copyright.php

---
+++ Unsorted/destiney/include/copyright.php
@@ -0,0 +1,13 @@
+<?php
+
+$content = <<<EOF
+<table cellpadding="2" cellspacing="0" border="0" width="100%">
+<tr>
+<td class="smallregular" align="center">Copyright © 2003 <a class="small" href="$base_url/" target="_top">$site_title</a> - All Rights Reserved</td>
+</tr>
+</table>
+EOF;
+
+$final_output .= content_table($content);
+
+?>
