fe67e1c4bbc2f77cf453e297c8464197a4340b76
me-suzy <me.suzana@gmail.com>
2025-08-12 13:47:32 +0300

    Add Education/wordcircle-2.17/s_course.php

---
+++ Education/wordcircle-2.17/s_course.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+
+CLASS
+-----
+COURSE
+
+
+PROPERTIES
+----------
+owner: user_id of person who started course
+name: true or false
+course_id
+key
+public: true or false
+membersArray: members for this course
+
+
+METHODS
+-------
+getMembers()
+register()
+unregister()
+delete()
+create()
+getCourseInfo()
+setCourseInfo()
+getCourses()
+
+
+*/
+
+
+
+?>
