5410e59e4ab3b90c8cdaf4376d814b031937f586
me-suzy <me.suzana@gmail.com>
2025-08-12 23:37:18 +0300

    Add Search Engines/indexer/indexer/project_info.php

---
+++ Search Engines/indexer/indexer/project_info.php
@@ -0,0 +1,11 @@
+project_name;Indexer
+version;0.9
+release_date;2004-11-01
+
+author_name;Oliver Kührig
+author_email;oliver@bitesser.de
+
+homepage_text;bitesser.de
+homepage_link;http://www.bitesser.de
+
+install_protection;disabled
