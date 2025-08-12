b38f76f1cac6b4f917ad9a52c12382c34cbafae1
me-suzy <me.suzana@gmail.com>
2025-08-12 08:22:31 +0300

    Add Content Management/wizard/wizard/inc/languages/english.admin.php

---
+++ Content Management/wizard/wizard/inc/languages/english.admin.php
@@ -0,0 +1,16 @@
+<?php
+/*  
+ *  Language Translation Admin Panel
+
+*/
+
+
+// Home and Login Bar at top
+	define("HOME", "Home" );  
+	define("LOGOUT", "Logout" );
+
+//Authorization
+    define("NOTLOGGED", "You are not logged in.");  
+    define("ADMINONLY", "Access is restricted to administrators.");
+	define("WEBMASTERONLY", "Access is restricted to the webmaster.");
+?>
