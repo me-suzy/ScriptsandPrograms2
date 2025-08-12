83ac4b1103cdf1aad8fde27ae48151a74b85f03c
me-suzy <me.suzana@gmail.com>
2025-08-12 09:28:42 +0300

    Add Development Tools/fuselogic/fuselogic/modules/test3/fuse._layout2.php

---
+++ Development Tools/fuselogic/fuselogic/modules/test3/fuse._layout2.php
@@ -0,0 +1,17 @@
+<table border="1"><tr><td bgcolor="#ffcccc">
+<?php 
+   echo 'Hi, I am Fuse <b>"'.module().'/'.subModule().'"</b>';
+   echo '<br>From __FILE__ = '.__FILE__;
+?>
+<br><br>
+<table summary="" bgcolor="#ffcccc" border="0">    
+    <tr>		    
+        <td>
+            <?php
+            echo getLayout();
+            ?>
+        </td>
+  	</tr>
+</table>
+<br><br>
+</td></tr></table>
