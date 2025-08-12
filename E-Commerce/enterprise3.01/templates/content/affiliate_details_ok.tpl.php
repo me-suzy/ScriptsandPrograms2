5776a13a658c8dea20151b3c8d363d87484b6e64
me-suzy <me.suzana@gmail.com>
2025-08-12 12:05:52 +0300

    Add E-Commerce/enterprise3.01/templates/content/affiliate_details_ok.tpl.php

---
+++ E-Commerce/enterprise3.01/templates/content/affiliate_details_ok.tpl.php
@@ -0,0 +1,11 @@
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+	<tr>
+    	<td class="pageHeading"><?php echo HEADING_TITLE; ?></td>
+        <td class="pageHeading" align="right"></td>
+    </tr>
+</table>
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+	<tr>
+    	<td align="right" class="smallText"><br><?php echo '<a href="' . escs_href_link(FILENAME_AFFILIATE_SUMMARY) . '">' . escs_image_button('button_continue.gif', IMAGE_BUTTON_CONTINUE) . '</a>'; ?></td>
+    </tr>
+</table>
