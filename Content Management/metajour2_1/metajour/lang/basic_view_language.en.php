9c56b989bb012201b163449d1c8fa44c67caa3f4
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:21 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_language.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_language.en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Language';
+$LANG['text_1'] = 'Select language for the selected records';
+$LANG['text_2'] = 'Language';
+?>
