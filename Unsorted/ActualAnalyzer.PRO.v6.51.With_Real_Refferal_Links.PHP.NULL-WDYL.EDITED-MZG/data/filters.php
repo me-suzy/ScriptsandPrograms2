e73c9b3e640ae290c477da550b6b609724522284
me-suzy <me.suzana@gmail.com>
2025-08-13 00:27:22 +0300

    Add Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/data/filters.php

---
+++ Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/data/filters.php
@@ -0,0 +1,55 @@
+<?php
+
+/*------------------------------------------------------------------------------
+Add below this block the IP addresses for which it is necessary to block the
+counting. For example:
+
+$ip_filter[]='192.168.0.1';
+$ip_filter[]='192.168.0.2';
+$ip_filter[]='192.168.0.3';
+
+will block the count for users with IP = 192.168.0.1 or 192.168.0.2 or
+192.168.0.3
+
+It is accessible to set the range of IP addresses for which it is necessary to
+block the counting. For example:
+
+$ip_filter[]='192.168.0.1-192.168.0.3';
+
+will block the count for users with IP = 192.168.0.1 or 192.168.0.2 or
+192.168.0.3
+------------------------------------------------------------------------------*/
+
+
+
+
+
+
+
+/*------------------------------------------------------------------------------
+Add below this block the Proxys' IP addresses for which it is necessary to block
+the counting. For example:
+
+$proxy_filter[]='215.117.0.1';
+$proxy_filter[]='215.117.0.2';
+$proxy_filter[]='215.117.0.3';
+
+will block the count for users with Proxy's IP = 215.117.0.1 or 215.117.0.2 or
+215.117.0.3
+
+It is accessible to set the range of Proxys' IP addresses for which it is
+necessary to block the counting. For example:
+
+$proxy_filter[]='215.117.0.1-215.117.0.3';
+
+will block the count for users with Proxy's IP = 215.117.0.1 or 215.117.0.2 or
+215.117.0.3
+------------------------------------------------------------------------------*/
+
+
+
+
+
+
+
+?>
