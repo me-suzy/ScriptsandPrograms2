60073afc4ddee3ffb1446b17cdbec5d2f0cc87aa
me-suzy <me.suzana@gmail.com>
2025-08-12 10:53:54 +0300

    Add Discussion Boards/tinybb_0_3/config.inc.php

---
+++ Discussion Boards/tinybb_0_3/config.inc.php
@@ -0,0 +1,31 @@
+<?php
+// THIS CONFIG FILE IS COMPATIBLE UP TO tinybb V0.1
+
+$tinybb_title 		= "";			// Website title
+$tinybb_url 		= "";			// Website url
+$tinybb_folder 		= "";			// Name of the folder you have installed to without the leading /
+$tinybb_email 		= "";			// Webmaster email address
+
+$tinybb_header		= "";			// Relative location of your header file
+						// e.g. "../common_files/header.php";
+$tinybb_footer		= "";			// Relative location of your header file
+
+$tinybb_i_index 	= 5;			// Max number of topics to show on homepage
+$tinybb_i_pages 	= 10;			// Max number of pages to show
+$tinybb_chars_index	= 85;			// Max characters for preview of posts
+
+$tinybb_moderators 	= "";			// Usernames of moderators, seperated by
+						// commas, e.g. "user1,user2,user3"
+
+$tinybb_mysql_host 	= "";			// MySQL hostname or IP address
+$tinybb_mysql_user	= "";			// MySQL username
+$tinybb_mysql_password	= "";			// MySQL password
+$tinybb_mysql_db	= "";			// MySQL database
+
+// Allowable HTML tags are stored in the "tags.php" file
+//
+// Emoticons for forum posts are stored in the "smilies.php" file
+//
+// Words now allowed to be posted (swwear words) are stored in the "censorship.php" file
+
+?>
