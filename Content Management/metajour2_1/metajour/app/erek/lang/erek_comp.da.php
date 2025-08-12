c7f7c2d647ca75cba8d7805101083141f6dac1ca
me-suzy <me.suzana@gmail.com>
2025-08-12 06:01:25 +0300

    Add Content Management/metajour2_1/metajour/app/erek/lang/erek_comp.da.php

---
+++ Content Management/metajour2_1/metajour/app/erek/lang/erek_comp.da.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eReklamation
+ * @subpackage lang
+ * $Id: erek_comp.da.php,v 1.2 2005/01/12 03:23:22 jan Exp $
+ */
+
+$LANG['name'] = 'Reklamationer';
+$LANG['label_name'] = 'Varenr';
+$LANG['label_itemtext'] = 'Varebeskrivelse';
+$LANG['label_itemnum'] = 'Antal';
+$LANG['label_compunitid'] = 'Enhed';
+$LANG['label_compcauseid'] = 'Årsag';
+$LANG['label_description'] = 'Beskrivelse';
+$LANG['label_cno'] = 'Kundenr';
+$LANG['label_cname'] = 'Kundenavn';
+$LANG['label_caddress'] = 'Adresse';
+$LANG['label_cpostalcode'] = 'Postnr';
+$LANG['label_ccity'] = 'By';
+$LANG['label_compcountryid'] = 'Land';
+$LANG['label_ccontact'] = 'Kontaktperson';
+$LANG['label_cemail'] = 'Email';
+$LANG['label_messageto'] = 'Besked til';
+$LANG['label_compsolutionid'] = 'Valgt løsning';
+$LANG['label_compdecisionid'] = 'Stillingtagen';
+$LANG['label_credit'] = 'Kreditnota beløb';
+$LANG['label_cost'] = 'Omkostninger';
+$LANG['label_comment'] = 'Bemærkninger';
+$LANG['label_compdepartmentid'] = 'Sag opstået hos';
+$LANG['label_comment1'] = 'Afsluttende bemærkning';
+$LANG['label_status'] = 'Status';
+$LANG['label_closedtime'] = 'Afsluttet tidspunkt';
+$LANG['label_closedby'] = 'Afsluttet af';
+$LANG['label_'] = '';
+?>
