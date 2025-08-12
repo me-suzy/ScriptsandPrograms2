2f81bed5b8fc591b99be42a87aeed2817e2b0504
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:03 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_delete.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_delete.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Slet';
+$LANG['text_1'] = 'Er du sikker på at du vil slette de markerede poster?';
+$LANG['text_cant'] = 'Posten kan ikke slettes, da den benyttes af andre poster!';
+?>
