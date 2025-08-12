c9ac177c553df14cd04cd93b72c1573554172e60
me-suzy <me.suzana@gmail.com>
2025-08-12 06:13:23 +0300

    Add Content Management/metajour2_1/metajour/extension/forum/lang/forumdata.da.php

---
+++ Content Management/metajour2_1/metajour/extension/forum/lang/forumdata.da.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['label_name'] = 'Emne';
+$LANG['label_content'] = 'Besked';
+$LANG['label_uname'] = 'Fra';
+$LANG['label_numread'] = 'Antal visninger';
+$LANG['label_numreply'] = 'Antal svar';
+$LANG['label_lastreply'] = 'Seneste svar';
+$LANG['name'] = 'Forum data';
+?>
