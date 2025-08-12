5bdd9d882c2e6267338dbfdc97e44873e761a327
me-suzy <me.suzana@gmail.com>
2025-08-12 12:59:34 +0300

    Add Education/prm-2.1/settings.php

---
+++ Education/prm-2.1/settings.php
@@ -0,0 +1,15 @@
+<?php 
+
+$GLOBALS['configured'] = false;//WARNING IF THIS IS FALSE SOMEONE CAN RUN THE INSTALL
+ 
+$GLOBALS['dbserver'] = '';
+$GLOBALS['dbname'] = '';
+$GLOBALS['dbuser'] = '';
+$GLOBALS['dbpass'] = '';
+
+$GLOBALS['school'] = '';
+
+$GLOBALS['admin'] = '';
+
+
+?>
