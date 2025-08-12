ecc848bc34eb0e57b18fd50d722e9a841e33f021
me-suzy <me.suzana@gmail.com>
2025-08-12 22:19:23 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/includes/tiny_mce/plugins/insertdatetime/langs/czech.js

---
+++ Portal Systems/PHP-Nuke-7.8/html/includes/tiny_mce/plugins/insertdatetime/langs/czech.js
@@ -0,0 +1,8 @@
+// CS lang variables
+
+tinyMCELang['lang_insertdate_desc'] = 'Vloit datum';
+tinyMCELang['lang_inserttime_desc'] = 'Vloit èas';
+tinyMCELang['lang_inserttime_months_long'] = new Array('Leden','Únor','Bøezen','Duben','Kvìten','Èerven','Èervenec','Srpen','Záøí','Øíjen','Listopad','Prosinec');
+tinyMCELang['lang_inserttime_months_short'] = new Array('Led','Úno','Bøe','Dub','Kvì','Èrv','Èvc','Srp','Záø','Øíj','Lis','Pro');
+tinyMCELang['lang_inserttime_day_long'] = new Array('Nedìle','Pondìlí','Úterý','Støeda','Ètvrtek','Pátek','Sobota','Nedìle');
+tinyMCELang['lang_inserttime_day_short'] = new Array('Ne','Po','Út','St','Èt','Pá','So','Ne');
