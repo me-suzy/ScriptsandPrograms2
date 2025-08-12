bad1af08374f943e11b76904b81a1bddcaf897aa
me-suzy <me.suzana@gmail.com>
2025-08-12 10:07:29 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/widgets/classSelectWidget.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/widgets/classSelectWidget.php
@@ -0,0 +1,22 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+
+includeClass('PntSelectWidget', 'pnt/web/widgets');
+
+/** FormWidget that generates html specifying 
+* a SELECT tag, by default as a dropdown list
+* with values that are options for the property.
+* @see http://www.phppeanuts.org/site/index_php/Pagina/128
+*
+* This concrete subclass is here to keep de application developers
+* code separated from the framework code.
+* @see http://www.phppeanuts.org/site/index_php/Menu/178
+* Framework code is in the superclass.
+* @package widgets
+*/
+class SelectWidget extends PntSelectWidget {
+
+}
+?>
