90ddcd6c128ab13008d57d6df5cdc1e33bce5903
me-suzy <me.suzana@gmail.com>
2025-08-12 11:45:34 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/german/cache.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/german/cache.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Cache Steuerung');
+
+define('TABLE_HEADING_CACHE', 'Cache Bl&ouml;cke');
+define('TABLE_HEADING_DATE_CREATED', 'erstellt am');
+define('TABLE_HEADING_ACTION', 'Aktion');
+
+define('TEXT_FILE_DOES_NOT_EXIST', 'Datei ist nicht vorhanden');
+define('TEXT_CACHE_DIRECTORY', 'Cache Verzeichnis:');
+
+define('ERROR_CACHE_DIRECTORY_DOES_NOT_EXIST', 'Fehler: Das Cache Verzeichnis ist nicht vorhanden. Please set it in Konfiguration / Cache');
+define('ERROR_CACHE_DIRECTORY_NOT_WRITEABLE', 'Fehler: Das Cache Verzeichnis ist schreibgesch&uuml;tzt.');
+?>
