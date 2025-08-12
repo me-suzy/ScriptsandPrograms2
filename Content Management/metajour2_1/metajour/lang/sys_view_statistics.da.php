95c045b2f2dc7099a13f4b1617c1a5d0a4806df9
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:59 +0300

    Add Content Management/metajour2_1/metajour/lang/sys_view_statistics.da.php

---
+++ Content Management/metajour2_1/metajour/lang/sys_view_statistics.da.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['heading'] = 'Website Statistik';
+$LANG['label_sum_pageid'] = 'Antal visninger';
+$LANG['label_sum_sessionid'] = 'Antal sessions';
+$LANG['label_sum_userid'] = 'Antal brugere';
+
+$LANG['label_group_pageid'] = 'Dokumentnavn';
+$LANG['label_group_ip'] = 'IP adresse';
+$LANG['label_group_sessionid'] = 'Session ID';
+$LANG['label_group_userid'] = 'Bruger ID';
+$LANG['label_group_browser'] = 'Browser';
+$LANG['label_group_browserlanguage'] = 'Browser sprog';
+$LANG['label_group_usercreated'] = 'Kendt bruger';
+$LANG['label_group_referer'] = 'Referer';
+$LANG['label_group_useragent'] = 'Useragent';
+$LANG['label_group_host'] = 'Host/Domæne';
+$LANG['label_group_tld'] = 'Land/TopLevelDomæne';
+$LANG['label_group_date'] = 'Dato';
+$LANG['label_group_dayname'] = 'Ugedag';
+$LANG['label_group_dayofmonth'] = 'Dag i måneden';
+$LANG['label_group_monthname'] = 'Måned';
+$LANG['label_group_quarter'] = 'Kvartal';
+$LANG['label_group_year'] = 'År';
+$LANG['label_group_hour'] = 'Klokkeslet';
+
+$LANG['label_date'] = 'Periode: ';
+$LANG['label_from'] = ' fra ';
+$LANG['label_to'] = ' til ';
+$LANG['label_done'] = 'Færdig!';
+$LANG['label_limitto'] = 'Afgrænset til: ';
+$LANG['label_groupby'] = 'Gruppering efter: ';
+$LANG['label_sumby'] = 'Sammentælling af: ';
+$LANG['label_count'] = 'Optæl';
+$LANG['label_grouped'] = 'Opdelt efter';
+$LANG['label_distinct'] = 'ANTAL FORSKELLIGE: ';
+$LANG['label_fromdate'] = 'Fra dato: ';
+$LANG['label_todate'] = 'Til dato: ';
+$LANG['label_generate'] = 'Vis statistik';
+$LANG['label_collecthost'] = 'Indhent host-oplysninger';
+$LANG['label_collecthostwarn'] = 'Dette kan tage lang tid';
+$LANG['label_limitto'] = "Søgning afgrænset til poster hvor ";
+$LANG['label_ulang'] = "Afgræns til sprog";
+
+?>
