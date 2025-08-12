0747451c0dc0dc94656ab7b70add79c432bf90ca
me-suzy <me.suzana@gmail.com>
2025-08-12 08:43:46 +0300

    Add Customer Support/ExoPHPDesk_v1.2/ExoPHPDesk_v1.2/tpl/Lightblue/tpl_info.php

---
+++ Customer Support/ExoPHPDesk_v1.2/ExoPHPDesk_v1.2/tpl/Lightblue/tpl_info.php
@@ -0,0 +1,20 @@
+<?php
+
+// << -------------------------------------------------------------------- >>
+// >> EXO Blue Template Configuration File
+// >>
+// >> TPL_INFO . PHP File - Template Configuration File
+// >> Edited  : 16 December, 2003
+// >>
+// << -------------------------------------------------------------------- >>
+
+// CHAT WINDOW SIZE CONFIGURATION - ADMIN/STAFF
+$cwin_height = "300";
+$cwin_width = "800";
+
+// CHAT WINDOW SIZE CONFIGURATION - Members
+$mwin_height = "500";
+$mwin_width = "500";
+
+
+?>
