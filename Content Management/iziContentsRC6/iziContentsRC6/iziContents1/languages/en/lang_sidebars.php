8561468749fec9355bbe2bc48eba843b20f82fc3
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:49 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_sidebars.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_sidebars.php
@@ -0,0 +1,41 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintain sidebar templates';
+
+//  List Headings
+$GLOBALS["tName"] = 'Name';
+$GLOBALS["tBorder"] = 'Border';
+$GLOBALS["tAlignment"] = 'Alignment';
+$GLOBALS["tColour"] = 'Colour';
+$GLOBALS["tWidth"] = 'Width';
+
+//  List Functions
+$GLOBALS["tAddNewSidebar"] = 'Add new sidebar';
+$GLOBALS["tViewSidebar"] = 'View sidebar';
+$GLOBALS["tEditSidebar"] = 'Edit sidebar';
+$GLOBALS["tDeleteSidebar"] = 'Delete sidebar';
+
+//  Form Block Titles
+$GLOBALS["thSBGeneral"] = 'Sidebar Details';
+
+//  Form Field Headings
+$GLOBALS["tSBName"] = 'Sidebar name';
+$GLOBALS["tSBAlign"] = 'Sidebar alignment';
+$GLOBALS["tSBBorder"] = 'Border size';
+$GLOBALS["tSBWidth"] = 'Sidebar width';
+$GLOBALS["tSBBackground"] = 'Sidebar background colour';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'This form lets you build templates for sidebars in your content.';
+$GLOBALS["hSBName"] = 'The name that the sidebar will be referred to in the [sidebar] tag.';
+$GLOBALS["hSBAlign"] = 'Side of the page that the sidebar will appear on, Left or Right.';
+$GLOBALS["hSBBorder"] = 'Size of the displayed sidebar border.';
+$GLOBALS["hSBWidth"] = 'The maximum percentage width of the sidebar in the content frame.';
+$GLOBALS["hSBBackground"] = 'Background color for the sidebar.';
+
+//  Error Messages
+$GLOBALS["eNoName"] = 'Sidebar template must have a name.';
+$GLOBALS["eColourWrong"] = 'Background colour value is wrong.';
+
+?>
