8702410a87db9d6f87d713eff04e52fe8ff8d0c2
me-suzy <me.suzana@gmail.com>
2025-08-13 01:30:00 +0300

    Add Unsorted/celeste aug 20 2003/celeste_data/settings/censoredword.inc.php

---
+++ Unsorted/celeste aug 20 2003/celeste_data/settings/censoredword.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$CensoredWords[0] = '¹²²úµ³';
+$CensoredWords[1] = '·¨ÂÖ¹¦';
+$CensoredWords[2] = 'f u c k';
+$CensoredWords[3] = 'ËûÂèµÄ';
+$ReplaceToWords[0] = 'G.C.D';
+$ReplaceToWords[1] = 'F.L.G';
+$ReplaceToWords[2] = 'f**k';
+$ReplaceToWords[3] = 'TMD';
