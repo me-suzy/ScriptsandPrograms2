321762d6e441e53d43998c37ec6ed40f2b5afddb
me-suzy <me.suzana@gmail.com>
2025-08-12 11:35:26 +0300

    Add E-Commerce/enterprise3.01/admin/popup_image.php

---
+++ E-Commerce/enterprise3.01/admin/popup_image.php
@@ -0,0 +1,54 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  require('includes/application_top.php');
+
+  reset($HTTP_GET_VARS);
+  while (list($key, ) = each($HTTP_GET_VARS)) {
+    switch ($key) {
+      case 'banner':
+        $banners_id = escs_db_prepare_input($HTTP_GET_VARS['banner']);
+
+        $banner_query = escs_db_query("select banners_title, banners_image, banners_html_text from " . TABLE_BANNERS . " where banners_id = '" . (int)$banners_id . "'");
+        $banner = escs_db_fetch_array($banner_query);
+
+        $page_title = $banner['banners_title'];
+
+        if ($banner['banners_html_text']) {
+          $image_source = $banner['banners_html_text'];
+        } elseif ($banner['banners_image']) {
+          $image_source = escs_image(HTTP_CATALOG_SERVER . DIR_WS_CATALOG_IMAGES . $banner['banners_image'], $page_title);
+        }
+        break;
+    }
+  }
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<title><?php echo $page_title; ?></title>
+<script language="javascript"><!--
+var i=0;
+
+function resize() {
+  if (navigator.appName == 'Netscape') i = 40;
+  window.resizeTo(document.images[0].width + 30, document.images[0].height + 60 - i);
+}
+//--></script>
+<script type="text/javascript" src="includes/browser.js">/************************************************ Jim's DHTML Menu v5.0- © Jim Salyer (jsalyer@REMOVETHISmchsi.com)* Visit Dynamic Drive: http://www.dynamicdrive.com for script and instructions* This notice must stay intact for use***********************************************/</script><script type="text/javascript" src="config.js"></script></head>
+
+<body onload="init();" onload="resize();">
+
+<?php echo $image_source; ?>
+
+</body>
+
+</html>
