ca9ec6f343035cfa36eb244223d4d88f9611e389
me-suzy <me.suzana@gmail.com>
2025-08-12 08:50:28 +0300

    Add Customer Support/rockcontact-0.7.3/rockcontact-0.7.3/languages/en/footer.php

---
+++ Customer Support/rockcontact-0.7.3/rockcontact-0.7.3/languages/en/footer.php
@@ -0,0 +1,31 @@
+<?
+/***************************************************************************
+ *                            footer.php [English]
+ *                            -------------------
+ *   begin                : Sunday, Nov 21, 2004
+ *   copyright            : (C) 2004 Network Rebusnet
+ *   contact              : http://rockcontact.rebusnet.biz/contact/
+ *
+ *   $Id$
+ *
+ ***************************************************************************/
+ 
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
+$msg = array (
+  'COPYRIGHT'        => "Copyright 2002-{NOW_YEAR} Network Rebusnet",
+  'TRANSLATION_INFO' => "[English] translated by Network Rebusnet"
+);
+
+foreach ($msg as $k => $v) {
+  $msg[$k] = htmlentities($v, ENT_QUOTES);
+}
+
+?>
