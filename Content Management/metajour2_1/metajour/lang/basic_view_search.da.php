fffdfd54843c2ab4baed4f04cd430b1010efcbf2
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:37 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_search.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_search.da.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['sttext_1'] = 'Søgekriterie #1';
+$LANG['sttext_2'] = 'Søgekriterie #2';
+$LANG['sttext_3'] = 'Søgekriterie #3';
+$LANG['sttext_4'] = 'Søgekriterie #4';
+$LANG['sttext_5'] = 'Søgekriterie #5';
+$LANG['sttext_6'] = 'Synlige kolonner';
+$LANG['sttext_7'] = 'Oprettet fra';
+$LANG['sttext_8'] = 'Oprettet til';
+$LANG['img_search_save'] = 'Gem søgning';
+$LANG['img_search_load'] = 'Hent søgning';
+?>
