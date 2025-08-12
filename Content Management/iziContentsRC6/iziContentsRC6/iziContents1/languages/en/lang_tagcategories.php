a6bc1f32f6b97e5dedcec366f39ec3a45eceff12
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:59 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_tagcategories.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_tagcategories.php
@@ -0,0 +1,19 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'translate tag categories';
+
+//  List Headings
+$GLOBALS["tCatDesc"] = 'Category Name';
+
+//  List Functions
+$GLOBALS["tViewCategory"] = 'View tag category';
+$GLOBALS["tEditCategory"] = 'Translate tag category';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'This form lets you translate tag category names.';
+
+//  Error Messages
+$GLOBALS["eNoTagcategoryDesc"] = 'You must enter a translation for every language';
+
+?>
