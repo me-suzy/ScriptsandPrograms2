e09e18ad377529cae00e5998dcd57941101a3635
me-suzy <me.suzana@gmail.com>
2025-08-13 01:30:10 +0300

    Add Unsorted/celeste aug 20 2003/celeste_data/settings/title.inc.php

---
+++ Unsorted/celeste aug 20 2003/celeste_data/settings/title.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+$titleSetting[0]['post'] = '0';
+$titleSetting[0]['title'] = '8086';
+$titleSetting[0]['image'] = 'images/title/1.gif';
+$titleSetting[1]['post'] = '10';
+$titleSetting[1]['title'] = '8088';
+$titleSetting[1]['image'] = 'images/title/2.gif';
+$titleSetting[2]['post'] = '50';
+$titleSetting[2]['title'] = '80286';
+$titleSetting[2]['image'] = 'images/title/3.gif';
+$titleSetting[3]['post'] = '100';
+$titleSetting[3]['title'] = '80386';
+$titleSetting[3]['image'] = 'images/title/4.gif';
+$titleSetting[4]['post'] = '500';
+$titleSetting[4]['title'] = '80486';
+$titleSetting[4]['image'] = 'images/title/5.gif';
+$groupLevelImage[1] = 'images/title/admin.gif';
