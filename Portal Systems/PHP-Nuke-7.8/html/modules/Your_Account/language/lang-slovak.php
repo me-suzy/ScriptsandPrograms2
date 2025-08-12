184734fef927656c8b7a6ca2fca516e9cfbf74a4
me-suzy <me.suzana@gmail.com>
2025-08-12 23:10:30 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Your_Account/language/lang-slovak.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Your_Account/language/lang-slovak.php
@@ -0,0 +1,269 @@
+<?php
+
+/*************************************************************************/
+/* The Slovak language file for module Your_Account                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/*************************************************************************/
+/* The Slovak language file for module Your_Account                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+
+define("_EMAIL","E-mail");
+define("_YES","Áno");
+define("_NO","Nie");
+define("_OPTIONAL","(na elanie)");
+define("_REQUIRED","(povinné)");
+define("_SAVECHANGES","Uloi zmeny");
+define("_THRESHOLD","Prah");
+define("_NOCOMMENTS","Bez komentára");
+define("_NESTED","Triedené");
+define("_FLAT","Ako prilo");
+define("_THREAD","V súvislostiach");
+define("_OLDEST","Najprv starie");
+define("_NEWEST","Najprv novie");
+define("_HIGHEST","Najprv s vyím skóre");
+define("_ERRORINVEMAIL","Chyba: Nesprávna e-mailová adresa");
+define("_ERROREMAILSPACES","Chyba: E-mailová adresa obsahuje medzery");
+define("_ERRORINVNICK","Chyba: Nesprávne uívate¾ské meno");
+define("_NICK2LONG","Uívate¾ské meno je pridlhé. Musí ma menej ako 25 znakov");
+define("_NAMERESERVED","Chyba: Toto meno je u rezervované");
+define("_NICKNOSPACES","Chyba: Uívate¾ské meno nesmie obsahova medzery");
+define("_NICKTAKEN","Chyba: Toto uívate¾ské meno je u obsadené");
+define("_EMAILREGISTERED","Chyba: Táto e-mailová adresa je u zaregistrovaná.");
+define("_UUSERNAME","uívate¾ské meno");
+define("_FINISH","Hotovo");
+define("_YOUUSEDEMAIL","Vy, alebo niekto iný pouil vau e-mailovú adresu");
+define("_TOREGISTER","pre vytvorenie konta");
+define("_FOLLOWINGMEM","Tu sú registraèné informácie:");
+define("_UNICKNAME","* Uívate¾ské meno:");
+define("_UPASSWORD","* Heslo:");
+define("_USERPASS4","uívate¾ské heslo pre");
+define("_YOUAREREGISTERED","Registrácia bola úspená. Heslo dostanete e-mailom na adresu, ktorú ste zadali pri registrácii.");
+define("_THISISYOURPAGE","Vaa vstupná stránka.");
+define("_AVATAR","Avatar-ikonka");
+define("_WEBSITE","Vaa domáca webstránka");
+define("_ICQ","ICQ èíslo");
+define("_AIM","AIM èíslo");
+define("_YIM","YIM èíslo");
+define("_MSNM","MSNM èíslo");
+define("_LOCATION","bydlisko");
+define("_OCCUPATION","zamestnanie");
+define("_INTERESTS","záujmy");
+define("_SIGNATURE","Podpis");
+define("_MYHOMEPAGE","Moja domovská stránka:");
+define("_MYEMAIL","Môj e-mail");
+define("_EXTRAINFO","Extra informácie");
+define("_NOINFOFOR","Nie sú k dispozícii iadne informácie o uívate¾ovi");
+define("_LAST10COMMENTS","Posledných 10 komentárov ktoré poslal");
+define("_LAST10SUBMISSIONS","Posledných 10 èlánkov ktoré poslal");
+define("_LOGININCOR","Chyba pri prihlasovaní! Prosím skúste znova...");
+define("_USERLOGIN","Prihlásenie");
+define("_USERREGLOGIN","Registrácia uívate¾a/Prihlásenie");
+define("_REGNEWUSER","Registrácia nového uívate¾a");
+define("_LIST","Zoznam");
+define("_NEWUSER","Nový uívate¾");
+define("_PASSWILLSEND","(Heslo Vám bude zaslané na emailovú adresu, ktorú ste zadali pri registrácii)");
+define("_COOKIEWARNING","Poznámka: uívate¾ské nastavenia vyadujú zapnuté cookies.");
+define("_ASREGUSER","Ako registrovaný uívate¾ môete:");
+define("_ASREG1","Posiela komentáre pod vlastným uívate¾ským menom.");
+define("_ASREG2","Posiela èlánky pod vlastným uívate¾ským menom.");
+define("_ASREG3","Ma k dispozícii osobný box a zvoli si vlastnú úpravu vzh¾adu stránok.");
+define("_ASREG4","Urèova poèet èlánkov na úvodnej stránke.");
+define("_ASREG5","Nastavi zobrazovanie komentárov.");
+define("_ASREG6","Zvoli iný design stránky");
+define("_ASREG7","a ïalie pekné malièkosti... Registráciou nevznikajú zo strany uívate¾a iadne záväzky, ani povinnosti");
+define("_REGISTERNOW","Zaregistrujte sa! Registrácia je bezplatná.");
+define("_WEDONTGIVE","Vae osobné informácie nebudú v iadnom prípade poskytnuté tretím osobám!");
+define("_ALLOWEMAILVIEW","Povo¾ujem ostatným uívate¾om vidie moju e-mailovú adresu");
+define("_OPTION","Nastavenie");
+define("_PASSWORDLOST","Zabudli ste heslo?");
+define("_NOPROBLEM","iadny problém. Zadajte Vae uívate¾ské meno a stlaète tlaèítko Posla heslo. Na adresu, ktorú ste zadali pri registrácii Vám poleme automatický email s potvrdzovacím kódom. Potom musíte znovu napísa Vae uívate¾ské meno a zada potvrdzovací kód a poleme Vám nové heslo.");
+define("_CONFIRMATIONCODE","Potvrdzovací kod");
+define("_SENDPASSWORD","Posla heslo");
+define("_YOUARELOGGEDOUT","Teraz ste odhlásený!");
+define("_SORRYNOUSERINFO","¼utujem, zodpovedajúci uívate¾ nebol nájdený.");
+define("_USERACCOUNT","uívate¾ské konto");
+define("_AT","na");
+define("_HASTHISEMAIL","je viazané na túto e-mailovú adresu.");
+define("_AWEBUSERFROM","uívate¾");
+define("_HASREQUESTED","prave poiadal o poslanie hesla.");
+define("_YOURNEWPASSWORD","Vae nové heslo je:");
+define("_YOUCANCHANGE","Môete ho zmeni, potom èo sa prihlásite znova na");
+define("_IFYOUDIDNOTASK","Ak ste o toto nepoiadali, nebuïte smutný. Vidíte túto správu, nie 'ich'. Ak sa jedná o chybu, prihláste sa znovu - s novým heslom.");
+define("_UPDATEFAILED","Uívate¾ský údaj sa momentálne nedá zmeni. Prosím, kontaktujte administrátora.");
+define("_PASSWORD4","Heslo pre");
+define("_MAILED","odoslané.");
+define("_CODEREQUESTED","poiadal o potvrdzovaci kód pre zmenu hesla.");
+define("_YOURCODEIS","Vá potvrdzovací kód je:");
+define("_WITHTHISCODE","S tymto kódom môete zada nové heslo na:");
+define("_IFYOUDIDNOTASK2","Ak ste o niè neiadali, zmate prosím tento email.");
+define("_CODEFOR","Potvrdzovaci kód pre");
+define("_USERPASSWORD4","uívate¾ské heslo pre");
+define("_UREALNAME","Skutoèné meno");
+define("_UREALEMAIL","Skutoèná e-mailová adresa");
+define("_EMAILNOTPUBLIC","(Táto e-mailová adresa bude neverejná, bude ale potrebná pre zaslanie hesla (pri novej registrácii alebo jeho zabudnutí).");
+define("_UFAKEMAIL","Nepravá e-mailová adresa (pre ostatných uívate¾ov)");
+define("_EMAILPUBLIC","(Táto e-mailová adresa bude publikovaná.)");
+define("_YOURHOMEPAGE","Vaa domáca web stránka");
+define("_YOURAVATAR","Vaa Avatar-ikonka");
+define("_YICQ","Vae ICQ");
+define("_YAIM","Vae AIM");
+define("_YYIM","Vae YIM");
+define("_YMSNM","Vae MSNM");
+define("_YLOCATION","Bydlisko");
+define("_YOCCUPATION","Povolanie");
+define("_YINTERESTS","Vae záujmy");
+define("_255CHARMAX","(max. 255 znakov. Zadajte svoj podpis, môe by aj v HTML.)");
+define("_CANKNOWABOUT","(max. 255 znakov. Zadajte informácie o sebe pre ostatných uivate¾ov.)");
+define("_TYPENEWPASSWORD","(nové heslo zadajte 2-krát pre jeho zmenu)");
+define("_SOMETHINGWRONG","Nieèo Vás vytáèa... Pociujete nenávis?");
+define("_PASSDIFFERENT","Heslá sú rozdielne, musia by rovnaké.");
+define("_CHARLONG","znakov.");
+define("_AVAILABLEAVATARS","Zoznam dostupných Avatar-ikoniek");
+define("_NEWSINHOME","Poèet èlánkov na domovskej stránke");
+define("_MAX127","(max. 127):");
+define("_ACTIVATEPERSONAL","Aktivova osobného menu.");
+define("_CHECKTHISOPTION","(Aktivujte túto monos a na úvodnej stránke budete ma blok, v ktorom môete ma svoj text, alebo vlastné menu.)");
+define("_YOUCANUSEHTML","(Môete poui HTML kód.)");
+define("_SELECTTHEME","Zvo¾te si vzh¾ad webu(theme)");
+define("_THEMETEXT1","Táto monos zmení vzh¾ad celého webu.");
+define("_THEMETEXT2","Tieto zmeny platia iba pre Vás.");
+define("_THEMETEXT3","Kadý uívate¾ si môe nastavi rozdielny vzh¾ad webu.");
+define("_DISPLAYMODE","Zobrazi ako");
+define("_SORTORDER","Zoradi");
+define("_COMMENTSWILLIGNORED","Komentáre s niím poètom bodov ako nastavíte budú ignorované.");
+define("_UNCUT","Nefiltrova");
+define("_EVERYTHING","Skoro vetko");
+define("_FILTERMOSTANON","Filtrova - väèinou anonymy");
+define("_USCORE","Skóre");
+define("_SCORENOTE","Anonymné èlánky zaèínajú na 0, Èlánky registrovaných uívate¾ov na 1. Moderátori môu pridáva a odobera body.");
+define("_NOSCORES","Nezobrazova skóre.");
+define("_HIDDESCORES","(Skryté hodnotenie: bude sa pouíva, len ho neuvidíte)");
+define("_MAXCOMMENT","Max. dåka komentára");
+define("_TRUNCATES","(Skráti dlhé èlánky a pridá k nim odkaz: Èíta ïalej...)");
+define("_BYTESNOTE","bytov (1024 bytov = 1K)");
+define("_USENDPRIVATEMSG","Posla súkromný odkaz pre:");
+define("_THEMESELECTION","Výber vzh¾adu");
+define("_COMMENTSCONFIG","Konfigurácia komentárov");
+define("_HOMECONFIG","Konfigurácia úvodnej stránky");
+define("_PERSONALINFO","Osobné informácie");
+define("_USERSTATUS","Stav aktuálnych uívate¾ov");
+define("_ONLINE","Online");
+define("_OFFLINE","Offline");
+define("_CHANGEYOURINFO","Zmeòi vlastné informácie");
+define("_CONFIGCOMMENTS","Nastavenie komentárov");
+define("_CHANGEHOME","Zmeòi úvodnú stránku");
+define("_LOGOUTEXIT","Odhlásenie/Odchod");
+define("_SELECTTHETHEME","Vybra vzh¾ad");
+define("_PRIVATEMESSAGES","Vae súkromné odkazy");
+define("_EDITUSER","Upravi uívate¾a");
+define("_RECEIVENEWSLETTER","Prijíma Newsletter emailom?");
+define("_NOTSUBSCRIBED","Nie ste prihlásený do náho Newslettru");
+define("_SUBSCRIBED","Prihlásili ste sa do náho Newslettru");
+define("_USERCHECKDATA","Prosím, skontrolujte nasledovné informácie. Ak je vetko uvedené správne, pokraèujte kliknutím na Hotovo, inak kliknite na Návrat a zmeòte èo potrebujete.");
+define("_USERFINALSTEP","Registrácia nového uívate¾a: Posledný krok");
+define("_ACCOUNTCREATED","Konto pre nového uívatela bolo vytvorené!");
+define("_THANKSUSER","Ïakujeme, e ste sa zaregistrovali na stránke ");
+define("_YOUCANLOGIN","Za nieko¾ko sekúnd dostanete od nás email s prideleným heslom. Potom sa môete <a href=\"modules.php?name=Your_Account\">prihlási</a>.Nezabudnite si zmeni heslo a nastavi predvo¾by k Vámu úètu. Dúfame, e budete spokojný s naími slubami.");
+define("_BROWSEUSERS","Prezrie uívate¾ov");
+define("_SEARCHUSERS","H¾ada v databáze uívate¾ov");
+define("_BROADCAST","Rozposla hromadnú správu");
+define("_BROADCASTTEXT","Môete posla <i>hromadnú správu</i> (max. 255 znakov). Táto správa sa zobrazí vetkým online uívate¾om v priebehu 10 minút. Kadý uívate¾, ktorý je online, uvidí Vau správu len raz v èervenom pruhu pod logom stránky. Uívatelia si môu deaktivova túto slubu <a href=\"modules.php?name=Your_Account&amp;op=edithome\">tu</a>. Nezneuívajte túto slubu. HTML kód nie je povolený");
+define("_SEND","Posla");
+define("_MESSAGEACTIVATE","Aktivova hromadné správy?");
+define("_BROADCASTSENT","Vaa hromadná správa bola rozposlaná.");
+define("_BROADCASTNOTSENT","<b>CHYBA:</b> Vaa hromadná správa je prázdna, alebo ste ju u pred chví¾ou poslali. Aby ste mohli posla ïaliu správu musíte poèka okolo 10 minút.");
+define("_RETURNPAGE","Návrat na osobnú stránku");
+define("_MYHEADLINES","Moje titulky");
+define("_SELECTASITE","Vyberte si stránku z ktorej si eláte èíta titulky:");
+define("_SELECTASITE2","Vyberte web stránku ");
+define("_ORTYPEURL"," alebo zadajte url tituliek vo formáte RSS/XML Vaej ob¾úbenej stránky:");
+define("_GO","Choï");
+define("_HEADLINESFROM","Titulky z");
+define("_WEBMAIL", "WebMail");
+define("_MESSAGES","Správy");
+define("_RETURNACCOUNT", "Return to Your Account Page");
+define("_ACCESSTO", "Access to");
+define("_CREATEJOURNAL", "Create your own Journal");
+define("_READHEADLINES", "Read custom headlines");
+define("_ACCESSTO", "Access to");
+define("_CREATEJOURNAL", "Create your own Journal");
+define("_READHEADLINES", "Read custom headlines");
+define("_ACCESSTO", "Access to");
+define("_CREATEJOURNAL", "Create your own Journal");
+define("_READHEADLINES", "Read custom headlines");
+define("_POPMSGACTIVE","Activate Private Message PopUp?");
+define("_READMYJOURNAL","Read My Journal");
+define("_AVATARNOTE","You can select your avatar once you have been registered");
+define("_USRNICKNAME","Nickname/Username");
+define("_ALWAYSSHOWEMAIL","Always show my Email Address");
+define("_HIDEONLINE","Hide your online status");
+define("_REPLYNOTIFY","Always notify me of replies");
+define("_REPLYNOTIFYMSG","Sends an email when someone replies to a topic you have posted in. This can be changed whenever you post");
+define("_PMNOTIFY","Notify on new Private Message by Email");
+define("_POPPM","Pop up window on new Private Message");
+define("_POPPMMSG","Will open a new popup window to inform you when new private messages arrive");
+define("_ATTACHSIG","Always attach my signature");
+define("_ALLOWBBCODE","Always allow BBCode");
+define("_ALLOWHTMLCODE","Always allow HTML");
+define("_ALLOWSMILIES","Always enable Smilies");
+define("_FORUMSTIME","Forums Timezone");
+define("_FORUMSDATE","Forums Date format");
+define("_FORUMSDATEMSG","The syntax used is identical to the PHP <a href=\"http://www.php.net/date\">date()</a> function");
+define("_SECURITYCODE","Security Code");
+define("_TYPESECCODE","Type Security Code Here");
+define("_NEWUSERERROR","New User Creation Error!");
+define("_SECCODEINCOR","Security Code is incorrect, Please go back and type it exactly as given...");
+define("_RETYPEPASSWORD","Re-type Password");
+define("_ALLREQUIRED","All fields are required");
+define("_YOUWILLRECEIVE","You will receive a confirmation email with a link to a page you should visit to activate your account in the next 24 hours.");
+define("_BLANKFORAUTO","Leave blank for auto-generate your password");
+define("_TOFINISHUSER","To finish the registration process you should visit the following link in the next 24 hours to activate your user account, otherwise the information will be automaticaly deleted by the system and you should apply again:");
+define("_YOUPASSMUSTBE","Your Password must be");
+define("_FINISHUSERCONF","Your request for a new account has been processed. You'll receive an email in the next few seconds with a activation link that should be visited in the next 24 hours to be able to activate your account.");
+define("_ACTIVATIONERROR","New User Activation Error");
+define("_ACTIVATIONYES","New User Activation");
+define("_ACTMSG","Your account has been activated. Please login from <a href=\"modules.php?name=Your_Account\">this link</a> using your assigned Nickname and Password.");
+define("_ACTERROR1","New user verification number is invalid.<br><br>Please be sure to click on the link you received by email or apply for a new account <a href=\"modules.php?name=Your_Account\">here</a>.");
+define("_ACTERROR2","There is no user in the database with this information.<br><br>You can register a new user from <a href=\"modules.php?name=Your_Account\">here</a>.");
+define("_ACTIVATIONSUB","New User Account Activation");
+define("_USERKARMA","User Karma:");
+define("_CHANGEKARMA","Change Karma for User");
+define("_ADMINFUNCTIONS","Administration Functions");
+define("_KARMAGOOD","Good Karma");
+define("_KARMAGOODHLP","This is a normal good user");
+define("_KARMAGOODREF","Default value. Users marked with <i>Good Karma</i> can post their comments without any restrictions.");
+define("_KARMALOW","Regular Karma");
+define("_KARMALOWHLP","This user has been marked, requires atention");
+define("_KARMALOWREF","Users marked with <i>Regular Karma</i> should be under observation. Consider this value a <i>flag</i> to have a eye on it.");
+define("_KARMABAD","Bad Karma");
+define("_KARMABADHLP","This user is bad and his comments are moderated");
+define("_KARMABADREF","Users marked with <i>Bad Karma</i> can post comments but the administrator should approve/moderate them from the admin panel.");
+define("_KARMADEVIL","Devil Karma");
+define("_KARMADEVILHLP","This user is a devil and his comments will not be published");
+define("_KARMADEVILREF","Users marked with <i>Devil Karma</i> are very bad. This users are not allowed to post comments. All his content is just ignored and nothing will be published.");
+
+?>
