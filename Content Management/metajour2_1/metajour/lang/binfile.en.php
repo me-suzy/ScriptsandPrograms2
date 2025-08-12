2ccca80cc129fcdf2c21a3a4c92c5dd94e8a8b11
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:42 +0300

    Add Content Management/metajour2_1/metajour/lang/binfile.en.php

---
+++ Content Management/metajour2_1/metajour/lang/binfile.en.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Files';
+$LANG['label_name'] = 'Name';
+$LANG['label_mimetype'] = 'MIME type';
+$LANG['label_description'] = 'Description';
+$LANG['label_revision'] = 'Revision';
+$LANG['label_upload'] = 'Upload';
+$LANG['label_file'] = 'File';
+$LANG['text_1'] = 'Previous revisions';
+?>
