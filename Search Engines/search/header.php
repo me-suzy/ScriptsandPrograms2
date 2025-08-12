7f3921fcdd25f3eda200d803c67d88fdc66b55aa
me-suzy <me.suzana@gmail.com>
2025-08-12 23:39:13 +0300

    Add Search Engines/search/header.php

---
+++ Search Engines/search/header.php
@@ -0,0 +1,8 @@
+<?php
+
+	error_reporting(E_ALL & ~E_NOTICE);
+
+	echo '<html><head><meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=UTF-8"><title>Results for:  ' . stripslashes ($HTTP_GET_VARS['q']) . '</title></head>';
+	echo '<body><BR>';
+
+?>
