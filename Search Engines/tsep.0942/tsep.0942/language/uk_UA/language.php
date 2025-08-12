872c81e306553d8e0f43eb1deab7e088b89cca9e
me-suzy <me.suzana@gmail.com>
2025-08-12 23:46:11 +0300

    Add Search Engines/tsep.0942/tsep.0942/language/uk_UA/language.php

---
+++ Search Engines/tsep.0942/tsep.0942/language/uk_UA/language.php
@@ -0,0 +1,169 @@
+<?php
+
+/* following will be filled automatically by SubVersion!
+
+Do not change by hand!
+
+$LastChangedDate: 2005-06-25 17:39:06 +0200 (Sa, 25 Jun 2005) $
+
+@lastedited $LastChangedBy: olaf $
+
+$LastChangedRevision: 198 $
+*/
+$tsep_lng['above_values'] = <<<_P
+Ð·Ð½Ð°ÑÐµÐ½Ð½Ñ Ð²Ð¸ÑÐµ
+_P;
+
+$tsep_lng['add'] = <<<_P
+Ð´Ð¾Ð´Ð°ÑÐ¸
+_P;
+
+$tsep_lng['all'] = <<<_P
+Ð²ÑÐµ
+_P;
+
+$tsep_lng['assigned_indexingprofiles'] = <<<_P
+Ð½Ð°Ð·Ð½Ð°ÑÐµÐ½Ñ Ð¿ÑÐ¾ÑÑÐ»Ñ ÑÐ½Ð´ÐµÐºÑÑÐ²Ð°Ð½Ð½Ñ
+_P;
+
+$tsep_lng['button_search'] = <<<_P
+ÐÐ¾ÑÑÐº
+_P;
+
+$tsep_lng['click_here_to_open'] = <<<_P
+ÐÐµÑÐµÐ¹Ð´ÑÑÑ Ð½Ð° Ð¿Ð¾ÑÐ¸Ð»Ð°Ð½Ð½Ñ Ð´Ð»Ñ ÑÐ¾Ð³Ð¾ ÑÐ¾Ð± Ð²ÑÐ´ÐºÑÐ¸ÑÐ¸ ÑÑÐ¾ÑÑÐ½ÐºÑ
+_P;
+
+$tsep_lng['close_this_window'] = <<<_P
+ÐÐ°ÐºÑÐ¸Ð²Ð°Ñ ÑÐµ Ð²ÑÐºÐ½Ð¾
+_P;
+
+$tsep_lng['config_absPath'] = <<<_P
+ÐÐ¸Ð·Ð½Ð°ÑÑÑÑ Ð°Ð±ÑÐ¾Ð»ÑÑÐ½Ð¸Ð¹ ÑÐ»ÑÑ Ð´Ð¾ Ð¿ÑÐ¸ÐºÐ»Ð°Ð´ÑÐ² TSEP: ÑÐºÑÐ¾ ÑÐ°Ð¹Ð» search.php ÑÐ¾Ð·Ð¼ÑÑÐµÐ½Ð¾ Ñ /home/www/php/tsepsearch/search.php Ð²ÑÐ½ Ð±ÑÐ´Ðµ Ð²Ð¸Ð³Ð»ÑÐ´Ð°ÑÐ¸ ÑÐº '/home/www/php/tsepsearch'
+_P;
+
+$tsep_lng['config_Color_1'] = <<<_P
+ÐÐµÑÑÐ¸Ð¹ Ð· Ð´Ð²Ð¾Ñ ÐºÐ¾Ð»ÑÐ¾ÑÑÐ² (ÑÑÐ¾ÑÐº) Ð´Ð»Ñ Ð´Ð¾Ð²Ð³Ð¸Ñ ÑÐ¿Ð¸ÑÐºÑÐ²
+_P;
+
+$tsep_lng['config_Color_2'] = <<<_P
+ÐÑÑÐ³Ð¸Ð¹ Ð· Ð´Ð²Ð¾Ñ ÐºÐ¾Ð»ÑÐ¾ÑÑÐ² (ÑÑÐ¾ÑÐº) Ð´Ð»Ñ Ð´Ð¾Ð²Ð³Ð¸Ñ ÑÐ¿Ð¸ÑÐºÑÐ²
+_P;
+
+$tsep_lng['config_Date_Style'] = <<<_P
+Ð¯Ðº Ð²ÑÐ´Ð¾Ð±ÑÐ°Ð¶Ð°ÑÐ¸ Ð´Ð°ÑÑ(Ñ ÑÑÐ¸Ð»Ñ PHP Ð²Ð¸ Ð¼Ð¾Ð¶ÐµÑÐµ Ð²Ð¸ÐºÐ¾ÑÐ¸ÑÑÐ¾Ð²ÑÐ²Ð°ÑÐ¸ D, l, M & F). ÐÐ¸Ð²Ð¾Ð´ Ð±ÑÐ´Ðµ Ð½Ð° Ð¼Ð¾Ð²Ñ Ð²ÐºÐ°Ð·Ð°Ð½ÑÐ¹ Ð½Ð¸Ð¶ÑÐµ. ÐÐ°Ð¿ÑÐ¸ÐºÐ»Ð°Ð´: ÐÐ½Ð³Ð»ÑÐ¹ÑÑÐºÐ¸Ð¹ ÑÑÐ¸Ð»Ñ: 'l, F d Y h:i a' ÐÑÐ¼ÐµÑÑÐºÐ¸Ð¹ ÑÑÐ¸Ð»Ñ: 'l, d. F Y, G:i'
+_P;
+
+$tsep_lng['config_dir_exclude'] = <<<_P
+ÐÐ²ÐµÐ´ÑÑÑ ÐºÐ°ÑÐ°Ð»Ð¾Ð³Ð¸ Ð´Ð»Ñ Ð²Ð¸ÐºÐ»ÑÑÐµÐ½Ð½Ñ
+_P;
+
+$tsep_lng['config_ext_include'] = <<<_P
+Ð Ð¾Ð·ÑÐ¸ÑÐµÐ½Ð½Ñ ÑÐ°Ð¹Ð»ÑÐ² Ð´Ð»Ñ Ð²ÐºÐ»ÑÑÐµÐ½Ð½Ñ
+_P;
+
+$tsep_lng['config_file_exclude'] = <<<_P
+ÐÐ²ÐµÐ´ÑÑÑ ÑÐ°Ð¹Ð»Ð¸ Ð´Ð»Ñ Ð²Ð¸ÐºÐ»ÑÑÐµÐ½Ð½Ñ:
+_P;
+
+$tsep_lng['config_group_general'] = <<<_P
+ÐÐ°Ð³Ð°Ð»ÑÐ½Ðµ
+_P;
+
+$tsep_lng['config_group_lists'] = <<<_P
+Ð¡Ð¿Ð¸ÑÐºÐ¸
+_P;
+
+$tsep_lng['config_group_lists_colors'] = <<<_P
+ÐÐ¾Ð»ÑÐ¾ÑÐ¸ ÑÐ¿Ð¸ÑÐºÑÐ²
+_P;
+
+$tsep_lng['config_group_lists_limits'] = <<<_P
+ÐÐ¾ÑÐ´Ð¾Ð½Ð¸ ÑÐ¿Ð¸ÑÐºÑÐ²
+_P;
+
+$tsep_lng['config_group_logging'] = <<<_P
+Ð¶ÑÑÐ½Ð°Ð»ÑÐ²Ð°Ð½Ð½Ñ
+_P;
+
+$tsep_lng['config_group_visible2enduser'] = <<<_P
+ÐÐ½ÑÐµÑÑÐµÐ¹Ñ ÐºÐ¾ÑÐ¸ÑÑÑÐ²Ð°ÑÐ°
+_P;
+
+$tsep_lng['config_group_visible2enduser_range'] = <<<_P
+ÐÐ¾ÑÐ´Ð¾Ð½Ð¸
+_P;
+
+$tsep_lng['config_group_visible2enduser_results'] = <<<_P
+Ð ÐµÐ·ÑÐ»ÑÑÐ°ÑÐ¸
+_P;
+
+$tsep_lng['config_group_visible2enduser_searchsuggest'] = <<<_P
+ÐÐ¾Ð¿Ð¾Ð¼Ð¾Ð³Ð° Ñ Ð¿Ð¾ÑÑÐºÑ
+_P;
+
+$tsep_lng['config_group_visible2enduser_search'] = <<<_P
+ÐÐ¾ÑÑÐº
+_P;
+
+$tsep_lng['config_Hour_Difference'] = <<<_P
+ÑÑÐ·Ð½Ð¸ÑÑ Ñ ÑÐ°ÑÑ Ð¼ÑÐ¶ ÑÐ°ÑÐ¾Ð¼ ÑÐµÑÐ²ÐµÑÐ° ÑÐ° Ð¼ÑÑÐµÐ²Ð¸Ð¼. ÐÐ¼ÑÐ½ÑÑÑ Ð²ÑÐ´Ð¿Ð¾Ð²ÑÐ´Ð½Ð¾
+_P;
+
+$tsep_lng['config_calc_hits_method'] = <<<_P
+Ð¯ÐºÐ¸Ð¹ ÑÐ¿Ð¾ÑÑÐ± Ð¾Ð±ÑÐ¸ÑÐ»ÐµÐ½Ñ Ð´Ð»Ñ Ð½Ð¾Ð¼ÐµÑÑÐ² ÑÑÐ¾ÑÑÐ½Ð¾Ðº Ð¿Ð¾ÑÑÑÐ±Ð½Ð¾ Ð¿Ð¾Ð²ÐµÑÑÐ°ÑÐ¸?
+_P;
+
+$tsep_lng['config_How_Many_CharsAfter_Hit'] = <<<_P
+Ð¡ÐºÑÐ»ÑÐºÐ¸ ÑÐ¸Ð¼Ð¾Ð»ÑÐ² Ð¿Ð¾ÐºÐ°Ð·ÑÐ²Ð°ÑÐ¸ Ð¿ÑÑÐ»Ñ Ð¿ÐµÑÑÐ¾Ð³Ð¾ Ð½Ð°ÑÐ¸ÑÐºÐ°Ð½Ð½Ñ?
+_P;
+
+$tsep_lng['config_How_Many_CharsBefore_Hit'] = <<<_P
+Ð¡ÐºÑÐ»ÑÐºÐ¸ ÑÐ¸Ð¼Ð¾Ð»ÑÐ² Ð¿Ð¾ÐºÐ°Ð·ÑÐ²Ð°ÑÐ¸ Ð¿ÐµÑÐµÐ´ Ð¿ÐµÑÑÐ¸Ð¼ Ð½Ð°ÑÐ¸ÑÐºÐ°Ð½Ð½ÑÐ¼?
+_P;
+
+$tsep_lng['config_Logging_result_links'] = <<<_P
+ÐÑÑÐ½Ð°Ð» ÐºÐ»ÑÐºÑÐ² Ð½Ð° ÑÐµÐ·ÑÐ»ÑÑÐ°ÑÐ°Ñ Ð¿Ð¾ÑÑÐºÑ?
+_P;
+
+$tsep_lng['config_Logging_search_term'] = <<<_P
+ÐÑÑÐ½Ð°Ð» ÑÐµÑÐ¼ÑÐ½ÑÐ² Ð´Ð»Ñ Ð¿Ð¾ÑÑÐºÑ?
+_P;
+
+$tsep_lng['page_additional_info'] = <<<_P
+ÐÐ¾Ð´Ð°ÑÐºÐ¾Ð²Ð° ÑÐ½ÑÐ¾ÑÐ¼Ð°ÑÑÑ
+_P;
+
+$tsep_lng['ss_search_term'] = <<<_P
+ÐÐ°Ð¿Ð¸Ñ
+_P;
+
+$tsep_lng['ss_search_term_hover'] = <<<_P
+ÐÐ¾Ð¿ÐµÑÐµÐ´Ð½Ñ Ð·Ð°Ð¿Ð¸ÑÐ¸ Ð¿Ð¾ÑÑÐºÑ
+_P;
+
+$tsep_lng['ss_popular'] = <<<_P
+ÐÐ¾Ð¿.
+_P;
+
+$tsep_lng['ss_popular_hover'] = <<<_P
+ÐÑÐ»ÑÐºÑÑÑÑ ÑÐ°Ð·ÑÐ² ÑÐºÑ Ð²Ð¸ÐºÐ¾ÑÐ¸ÑÑÐ¾Ð²ÑÐ²Ð°Ð»Ð¸ Ð·Ð°Ð¿Ð¸Ñ(Ð¿Ð¾Ð¿ÑÐ»ÑÑÐ½ÑÑÑÑ)
+_P;
+
+$tsep_lng['ss_returned_pages'] = <<<_P
+ÐÐ¡
+_P;
+
+$tsep_lng['ss_returned_pages_hover'] = <<<_P
+ÐÑÐ»ÑÐºÑÑÑÑ ÑÑÐ¾ÑÑÐ½Ð¾Ðº, ÑÐºÐ° Ð¿Ð¾Ð²ÐµÑÑÐ°ÑÑÑÑÑ Ð·Ð°Ð¿Ð¸ÑÐ¾Ð¼
+_P;
+
+$tsep_lng['error_index_nothing'] = <<<_P
+ÐÐ¾ÑÐ¾Ð¶Ð½ÑÐ¾(ÑÐ¾Ð±ÑÐ¾ Ð½ÐµÐ¼Ð°Ñ ÑÐ¾ ÑÐ½Ð´ÐµÐºÑÑÐ²Ð°ÑÐ¸):  
+_P;
+
+$tsep_lng['error_empty_files'] = <<<_P
+ÑÐ°Ð¹Ð»Ð¸ Ð¿Ð¾ÑÐ¾Ð¶Ð½Ñ(ÑÐ¾Ð±ÑÐ¾ Ð½ÐµÐ¼Ð°Ñ ÑÐ¾ ÑÐ½Ð´ÐµÐºÑÑÐ²Ð°ÑÐ¸)
+_P;
+
+?>
