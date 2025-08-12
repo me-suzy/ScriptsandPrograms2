059ae2071cf9d110d1226e36b3029bfcd7b52ba7
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:05 +0300

    Add Content Management/metajour2_1/metajour/lang/frame.da.php

---
+++ Content Management/metajour2_1/metajour/lang/frame.da.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Navn';
+$LANG['label_pageid'] = 'Dokument';
+$LANG['name'] = 'Startsider';
+$LANG['label_standard'] = '';
+?>
