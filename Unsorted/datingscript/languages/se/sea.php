7494410c42be005c79c12194880aa35b2bef8524
me-suzy <me.suzana@gmail.com>
2025-08-13 02:21:55 +0300

    Add Unsorted/datingscript/languages/se/sea.php

---
+++ Unsorted/datingscript/languages/se/sea.php
@@ -0,0 +1,122 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.0.3                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 05/02/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               swa.php                          #
+# File purpose            Additional langfile for admin    #
+# File created by         Kaj Merstrand <kaj@kub.se>       #
+############################################################
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Felaktigt login eller lösenord - försök igen',//1
+'Administrations avdelning',//2
+'Admin Login',//3
+'Admin Lösenord',//4
+'Gå in',//5
+'Endast för administratören',//6
+'Huvudsida',//7
+'Rensa hits',//8
+'Reparera&Optimera',//9
+'Användar hantering',//10
+'FAQ hantering',//11
+'E-post lista',//12
+'Logga ut',//13
+'Du är nu ut loggad ur administrations avdelningen.<br><br>Tack',//14
+'Totalt E-post',//15
+'Skickade E-post',//16
+'Den följande del',//17
+'{0} E-post har skickats till användarna',//18
+'Du har olästa meddelande i {0}',//19 {0} - SiteName (C_SNAME)
+'Hittade {0} medlemmar',//20
+'Hantera E-post lista',//21
+'Med status:',//22
+'E-post inställningar och fält:',//23
+'E-post typ',//24
+'Från E-post',//25
+'E-post rubrik',//26
+'E-post innehåll',//27
+'Bekräftelse om resultat',//28
+'Gå tillbaka',//29
+'Skicka E-post',//30
+'Ofullständiga försändelser - {0}',//31
+'Rubrik : {0} [Sended to {1} users] :::>',//32
+'Avluta denna försändelse',//33
+'Text',//34
+'HTML',//35
+'Förhandsgranska',//36
+'Användare med oläst E-post - ',//37
+'Totalt olästa meddelanden - ',//38
+'Senaste meddelande ID - ',//39
+'Skicka bekräftelse till användare med olästa meddelanden',//40
+'Profilen är uppdaterad',//41
+'FAQ är uppdaterad',//42
+'Skapa ny',//43
+'Skriv fråga',//44
+'Skriv svar',//45
+'FAQ hantering',//46
+'Skapa ny FAQ kategori',//47
+'Editera FAQ kategori',//48
+'Radera FAQ kategori',//49
+'Skapa FAQ',//50
+'Välj FAQ kategori',//51
+'FAQ fråga',//52
+'FAQ svar',//53
+'Först av allt måste du skapa FAQ kategorier',//54
+'Skriv kategori namn',//55
+'FAQ kategori skapa',//56
+'Ny kategori är skapad:',//57
+'FAQ kategori namn',//58
+'FAQ kategori editera',//59
+'Edit',//60
+'Kategorin är uppdaterad',//61
+'FAQ kategori editera',//62
+'FAQ har raderats',//63
+'FAQ kategori raderad',//64
+'OBS : Var försiktig. När du raderar en kategori försvinner alla frågor och svar under aktuell kategori.',//65
+'Radera',//66
+'0 - Vänta E-post kontrolleras<br>1 - Vänta kontrollerar efter registrering<br>2 - Vänta kontrollerar efter uppdatering<br>3,8 - Vänta på tillåtelse att radera(utan,med vis)<br>7 - Användare är aktiv<br>',//67
+'Databasen har reparerats och optimerats',//68
+'Användare med ID#={0} har raderats från databasen',//69
+'Hits har nollställts ',//70
+'Tillåtelse till användare med id={0}',//71
+'IP',//72
+'Krav [0 - 255]',//73
+'Status - (BE CAREFULL)',//74
+'Tillåt',//75
+'Hantera användare',//76
+'Totalt activa användare :',//77
+'Användare som väntar på aktivering efter registrering',//78
+'Användare som väntar på aktivering efter uppdatering',//79
+'Användare som väntar på radering',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
