2e69d368399107f3931c6347a4234562090cc3f5
me-suzy <me.suzana@gmail.com>
2025-08-12 10:43:23 +0300

    Add Discussion Boards/okphpbbs_en/okphpbbs/bbs/lang/en/main.php

---
+++ Discussion Boards/okphpbbs_en/okphpbbs/bbs/lang/en/main.php
@@ -0,0 +1,259 @@
+<?php
+/*
+ ***********************************************************************
+ * Copyright (c) 2003-2004 Okphp Group
+ * http://www.okphp.com
+ *  
+ ***********************************************************************
+ */
+$indications['yes'] = "Yes"; //*
+$indications['no'] = "No";//*
+$indications['1'] = "<font color=green>Yes</font>"; //*
+$indications['0'] = "<font color=red>No</font>";//*
+$indications['delete'] = "Delete";//*
+$indications['add'] = "Add";//*
+$indications['close'] = "Close";//*
+$indications['announcements'] = "Announcements";//*
+
+$indications['sys_message'] = "System Message";
+$indications['page_will'] = "This page will be redirected to the following URL:";
+$indications['submit'] = " Submit ";
+$indications['content'] = "Content";
+$indications['forum'] = "Forum";
+$indications['posts'] = "Posts";
+$indications['lastpdate_'] = "Last Post Date";
+$indications['auth'] = "Authority";
+$indications['read_auth'] = "ViewAuth";
+$indications['reply_auth'] = "NewPost";
+$indications['new_auth'] = "NewThread";
+
+$indications['manage'] = "Manage";
+$indications['manage_score'] = "Edit Score";
+$indications['moderator'] = "Moderator";
+$indications['d_o_settings'] = "Default Opration Access Settings";//*
+$indications['charset_'] = "Charset";//*
+
+$indications['view_itvl'] = "Refresh Interval (seconds)"; //*
+$indications['exceed_allow'] = "Exceeded times allowed";//*
+$indications['input_itvl'] = "Form Submition Interval (seconds)";//*
+$indications['unlimited'] = "Unlimited";//*
+
+// Users
+$indications['member_area'] = "Member Area"; //*
+$indications['reg_time_'] = "Register";
+$indications['post_num_'] = "Posts";
+$indications['score_'] = "Scores";
+$indications['login'] = "Login";
+$indications['logout'] = "Logout";
+$indications['remember_me'] = "Remember Me";
+$indications['register'] = "Register";
+$indications['user_info'] = "UserProfile";
+$indications['modify_info'] = "ModifyProfile";
+$indications['choose_info'] = "Choose To Fill";
+$indications['username_'] = "UserName";
+$indications['password_'] = "PassWord";
+$indications['repeat'] = "Repeat";
+$indications['email_'] = "E-mail";
+$indications['g_offered'] = "Groups offered";
+$indications['g_belong'] = "Groups joined";
+$indications['headpic_'] = "Avatar";
+$indications['sig_'] = "Signature";
+$indications['userintr_'] = "Brief introduction";
+$indications['g_alert'] = "Attention!! You can leave highlight groups, but cannot return to join them by yourself!";
+$indications['agreement'] = "Members Agreement";
+$indications['accept'] = "Accept";
+$indications['not_accept'] = "Not Accept";
+$indications['agreement_required'] = "Sorry, you cannot join us if you do not accept our Agreement.";
+$indications['h_height_'] = "Height";
+$indications['h_width_'] = "Width";
+$indications['h_max_'] = "Max";
+
+// PM
+$indications['send_pm'] = "Send PM";
+$indications['view_pm'] = "View PM";
+$indications['reply_pm'] = "Reply";
+$indications['del_pm'] = "Delete";
+$indications['has_pm'] = "<font color=green>(New)</font>";
+$indications['from_'] = "From:";
+$indications['date_'] = "Date:";
+
+// Threads
+$indications['top_info'] = "<font color=green>Sticky:</font>";
+$indications['stress_it'] = "[Stress]"; //*
+$indications['unstress_it'] = "[UnStress]"; //*
+$indications['top_id'] = "[Stick]";
+$indications['untop_id'] = "[UnStick]";
+$indications['subject_'] = "Subject";
+$indications['author_'] = "Author";
+$indications['postdate_'] = "Post Date";
+$indications['reply'] = "Reply";
+$indications['view'] = "View";
+$indications['new_thread'] = "New Thread";
+$indications['new_post'] = "New Post";
+$indications['edit_post'] = "Edit";
+$indications['topic_name'] = "Active";
+$indications['faq_name'] = "FAQ";
+$indications['cream_name'] = "Cream";
+$indications['recovery_name'] = "Recovery";
+$indications['page_down'] = "NextPage&gt;";
+$indications['page_up'] = "&lt;LastPage";
+$indications['page_end'] = "EndPage&gt;|";
+$indications['page_begin'] = "|&lt;FirstPage";
+$indications['all_page'] = "AllPages:";
+$indications['current_page'] = ",CurrentPage:";
+$indications['page_jump'] = "Jump to";
+$indications['fp_name'] = "Authority";
+$indications['fp_view'] = "View";
+$indications['fp_post'] = "New Thread";
+$indications['fp_reply'] = "New Post";
+$indications['fp_all'] = "Open";
+$indications['fp_users'] = "All Users";
+$indications['fp_special'] = "Special Groups";
+
+$indications['confirm_'] = "Confirm Code";
+$indications['confirm_ex'] = "Enter the code exactly as you see it.";
+
+
+$indications['no_mod'] = "-";
+$indications['all_online'] = "Online Users";
+$indications['type_online'] = "Online in this Forum";
+$indications['whois_online'] = "Whois Online";
+
+$indications['lock_it'] = "[Lock]";
+$indications['unlock_it'] = "[Unlock]";
+
+$indications['this_t_m'] = "This thread "; //*
+$indications['forum_jump'] = "Forum Jump "; //*
+
+// poll
+$indications['poll_info'] = "<font color=green>Poll:</font>"; //*
+$indications['poll'] = "Poll"; //*
+$indications['set_poll'] = "Set Poll"; //*
+$indications['poll_question'] = "Poll Question";//*
+$indications['allow_multi'] = "Allow multiple choice?";//*
+$indications['poll_timeout'] = "Poll TimeOut";//*
+$indications['days'] = "days";
+$indications['no_limit'] = "Unlimited";//*
+$indications['poll_option'] = "Option";//*
+$indications['vote'] = "Vote!";//*
+
+// attachment
+$indications['attachment'] = "Attachment";//*
+$indications['set_attach'] = "Set Attachment";//*
+$indications['size'] = "Size";//*
+$indications['attach_max_size'] = "File max size";//*
+$indications['attach_num_post'] = "Files /a post";//*
+$indications['attach_num_day'] = "Files /a day";//*
+
+// added 2.0beta =================================================================
+// online users count
+$indications['bb_stats'] = "BBS stats";
+$indications['users'] = "Members";
+$indications['guests'] = "Guests";
+$indications['count_topic_'] = "Threads";
+$indications['count_post_'] = "Posts";
+$indications['count_user_'] = "Members";
+$indications['new_user'] = "The newest registered user is";
+$indications['bb_count'] = "BBS counts";
+$indications['basic_setting'] = "Basic settings";
+
+$indications['my_posts'] = "Hello! Your posts";
+$indications['my_score'] = "Your score";
+$indications['my_info'] = "Your profile";
+$indications['my_reg'] = "Registered";
+$indications['maxol'] = "Most users ever online was";
+
+// new thread
+$indications['emoticons'] = "Emoticons";
+$indications['post_option'] = "Post options";
+$indications['disable_bbcode'] = "Disable bbcode";
+$indications['disable_emote'] = "Disable emoticons";
+$indications['disable_url'] = "Disable URL auto check";
+
+$indications['nfolder'] = "Normal thread";
+$indications['hotfolder'] = "Popular thread";
+$indications['lockfolder'] = "Locked thread";
+$indications['topfolder'] = "Sticky";
+$indications['pollfolder'] = "Poll";
+
+$indications['search_content'] = "Search result";
+$indications['search'] = "Search";
+$indications['no_new_posts'] = "No new posts";
+$indications['new_posts'] = "New posts";
+
+// BLOG
+$indications['blog'] = "BLOG";
+$indications['blog_name'] = "BLOG Name";
+$indications['blog_about'] = "BLOG Intrduce";
+$indications['blog_articles'] = "Blog Articles";
+$indications['blog_info'] = "BLOG Infomation";
+$indications['my_blog'] = "My BLOG";
+// ######################################  messages
+
+$messages['confirm_fail']	 = "The confirmation code you entered was incorrect";
+$messages['access_denied'] ="Access Denied!";
+$messages['opration_denied'] ="Opration Denied!"; //*
+$messages['view_forum_fail'] = "View Forum Failed. ";
+$messages['new_post_fail'] = "Post failed. ";
+$messages['new_post_ok'] = "Posted successfully!";
+$messages['info_not_enough'] = "Your form infomation is imperfect, please check it and submit again.";
+$messages['edit_post_fail'] = "Edit post failed. ";
+$messages['only_post_active'] = "Sorry! Only threads in ACTIVE could be replied.";
+// Users
+$messages['login_ok']		 = "Login Success!";
+$messages['login_fail']      = "Login failed, You can try a limited times:";
+$messages['not_login']		 = "You cannot do this before login!";
+$messages['pwd_not_match']	 = "Please check your password!";
+$messages['email_wrong']	 = "Wrong E-mail format";
+$messages['email_exist']	 = "It seems that your E-mail address has been used by somebody else.";
+$messages['user_exist']	     = "It seems that your username has been used by somebody else.";
+$messages['sig_too_lang']	 = "Signature is too long!";
+$messages['no_such_user']    = "No such a user ";
+$messages['reg_ok']			 = "Congratulations! You have registered successfully!";
+$messages['not_img'] = "Incorrect image format!";
+$messages['img2big'] = "Image is too big";
+$messages['only_az09']		 = "Sorry! username must be composed of a-z,A-Z,0-9";
+$messages['forbid_s']		 = "Sorry! Your username contains banned charators";
+$messages['username_2long']  = "Sorry! Your username is too long!";
+
+// search
+$messages['s_forbidden']	 = "Sorry! Search-function has been closed.";
+$messages['result_num']		 = "All records found:";
+$messages['no_match']		 = "Sorry! No records was found";
+
+// input control
+$messages['msg_too_long']	 = "Messages is too long";
+$messages['input_crigger']	 = "Sorry! You submit forms too frequently. Please wait a moment before next submitting.";
+
+// PM
+$messages['pm_overload']	 = "Sorry! The user's PM box is overload, he/she cannot recieve your PM";
+
+// Others
+$messages['modify_ok'] = "Target has been Modified successfully!";
+$messages['search_find'] = "Found records ";
+$messages['send_ok'] = "Info has benn Delivered successfully!";
+$messages['del_ok'] = "Target has been Deleted successfully!";
+$messages['is_locked'] = "Sorry! This thread is locked. ";
+$messages['info_added'] = "Info has benn Delivered successfully!";
+$messages['process_ok'] = "Opration Complete!";
+$messages['process_fail'] = "Opration failed!"; //*
+
+// poll
+$messages['set_poll_wrong'] = "Cannot add poll. Please check your poll settings."; //*
+$messages['cannot_vote'] = "Sorry! Voting failed. Perhaps you have voted before, or this Poll is OUTTIME now."; //*
+// attachment
+$messages['unknow_filetype'] = "Unknow file type."; //*
+$messages['attach_num_exceed'] = "Sorry! You have exceeded the max num of attachment files"; //*
+$messages['attach_size_exceed'] = "Sorry! You have exceeded the max size of attachment file"; //*
+$messages['headpic_size_exceed'] = "Sorry! You have exceeded the max size of avatar";
+
+
+// 2.0 ---------
+$messages['opration_ok'] = "Opration Success!";
+
+$messages['badlogin_locked'] = "Your IP has been blocked, please retry later..";
+
+// BLOG
+$messages['blog_name_exist'] = "Sorry! the blog name has been used";
+$messages['blogname_2long'] = "Sorry! your blog name is too long";
+?>
