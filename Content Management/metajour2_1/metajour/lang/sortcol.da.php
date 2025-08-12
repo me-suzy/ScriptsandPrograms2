165992cbb09a56833b846afd2cfb177f8387a256
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:07 +0300

    Add Content Management/metajour2_1/metajour/lang/sortcol.da.php

---
+++ Content Management/metajour2_1/metajour/lang/sortcol.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Sorterings opsætning';
+$LANG['label_name'] = 'Tabel';
+$LANG['label_makedefault'] = 'Gør til forvalgt';
+?>
