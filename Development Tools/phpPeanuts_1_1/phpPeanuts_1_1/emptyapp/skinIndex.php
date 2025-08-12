a5bd9dd52774e6e94df93f3036d138ce43ca8c6a
me-suzy <me.suzana@gmail.com>
2025-08-12 10:07:36 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/emptyapp/skinIndex.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/emptyapp/skinIndex.php
@@ -0,0 +1,22 @@
+<H1>Empty Application</H1>
+
+If this page comes from your own web server you have successfully 
+installed phpPeanuts. Congratulations!<BR>
+<BR>
+This application can be used to create a new application from scratch.<BR>
+Just copy the 'emptyapp' folder from the phpPeanuts folder to become a 
+new subfolder also directly in the phpPeanuts folder.<BR>
+Then change all references to emptyapp in the files in the new folder 
+to the name of the new folder.<BR>
+Then create a new folder with the same name directly in the classes folder.<BR>
+Finally make a new hyperlink and a new printSubmenu line in the 
+'skinMenuPart.php' file in the includes folder referring the new folder.<BR>
+<BR>
+If you get this message properly from requesting the url referencing 
+index.php in your new folder you have successfully created a new application.
+ 
+<H3>Examples</H3>
+
+The examples for this version are now available as a separate 
+<A HREF='http://www.phppeanuts.org/site/index_php/Menu/23/Download.html'>download</A>. See 
+<A HREF='http://www.phppeanuts.org/site/index_php/Pagina/143/How+to+install+the+examples.html'>how to install the examples</A>
