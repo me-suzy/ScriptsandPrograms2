d1fa5e9b846753c1cec3edd838e302bbe756a1d4
me-suzy <me.suzana@gmail.com>
2025-08-12 05:57:19 +0300

    Add Content Management/metajour2_1/metajour/documentsection_view_editor.php

---
+++ Content Management/metajour2_1/metajour/documentsection_view_editor.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('document_view_editor.php');
+
+class documentsection_view_editor extends document_view_editor {
+
+	function view() {
+		parent::view();
+	}
+
+}
