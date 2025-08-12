cc6126c28c56a417358fe46dade21e67eba483d0
me-suzy <me.suzana@gmail.com>
2025-08-12 23:53:46 +0300

    Add Tests and Quiz/quiz_manager_09/quiz_manager_09/config.php

---
+++ Tests and Quiz/quiz_manager_09/quiz_manager_09/config.php
@@ -0,0 +1,15 @@
+<?php
+
+	  $admin_pass = "admin";
+
+
+	  $db_host = "localhost";
+	  $db_name = "quiz";
+	  $db_user = "test";
+	  $db_password = "test";
+
+	  $tempz[form] = "templates/quiz_form_template.html";
+
+	 $email_not_required = 1; 
+	
+?>
