ad9f86ce2fe7ed770b9471f623a8212111865a95
me-suzy <me.suzana@gmail.com>
2025-08-12 12:00:26 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/login.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/login.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Anmelden');
+define('HEADING_TITLE', 'Melden Sie sich an');
+
+define('HEADING_NEW_CUSTOMER', 'Neuer Kunde');
+define('TEXT_NEW_CUSTOMER', 'Ich bin ein neuer Kunde.');
+define('TEXT_NEW_CUSTOMER_INTRODUCTION', 'Durch Ihre Anmeldung bei ' . STORE_NAME . ' sind Sie in der Lage schneller zu bestellen, kennen jederzeit den Status Ihrer Bestellungen und haben immer eine aktuelle &Uuml;bersicht &uuml;ber Ihre bisherigen Bestellungen.');
+
+define('HEADING_RETURNING_CUSTOMER', 'Bereits Kunde');
+define('TEXT_RETURNING_CUSTOMER', 'Ich bin bereits Kunde.');
+
+define('TEXT_PASSWORD_FORGOTTEN', 'Sie haben Ihr Passwort vergessen? Dann klicken Sie <u>hier</u>');
+
+define('TEXT_LOGIN_ERROR', 'Fehler: Keine &Uuml;bereinstimmung der eingebenen eMail-Adresse und/oder dem Passwort.');
+define('TEXT_VISITORS_CART', '<font color="#ff0000"><b>Achtung:</b></font> Ihre Besuchereingaben werden automatisch mit Ihrem Kundenkonto verbunden. <a href="javascript:session_win();">[Mehr Information]</a>');
+?>
