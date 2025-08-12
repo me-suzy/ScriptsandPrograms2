2cad3a26f10b81e1a04a7aeaac1c641ebef9627d
me-suzy <me.suzana@gmail.com>
2025-08-12 20:21:05 +0300

    Add News Publishing/qlitenews/qlitenews/admin/login.php

---
+++ News Publishing/qlitenews/qlitenews/admin/login.php
@@ -0,0 +1,9 @@
+<div>
+  <form method="post" action="loginprocess.php">
+    <strong>Username:</strong><br/>
+    <input type="text" name="qname" maxlength="20" size="40"/><br/>
+    <strong>Password:</strong><br/>
+    <input type="text" name="qpass" size="40"/><br/>
+    <input type="submit" name="qlogin" value="Login"/>
+  </form>
+</div>
