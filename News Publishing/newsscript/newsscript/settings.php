3ff0c9122ab87da42a3517b251507c3954ac1180
me-suzy <me.suzana@gmail.com>
2025-08-12 19:53:14 +0300

    Add News Publishing/newsscript/newsscript/settings.php

---
+++ News Publishing/newsscript/newsscript/settings.php
@@ -0,0 +1,140 @@
+<?
+######################
+# COLOR AND SETTINGS #
+######################
+
+#####COLOR AND SETTINGS FOR NEWS#####
+$news_bgcolor =                         "#80CFE2";
+$news_width =                           "350";
+$news_cellspacing =                     "5";
+$news_cellpadding =                     "2";
+$news_align =                           "left";
+$news_border =                          "0";
+$news_page_bgcolor =					"#E6E6E6";
+
+
+
+######################
+# SETTINGS           #
+######################
+$ns_writtenby = 						"Written by: ";
+$ns_category =							"Category:";
+
+
+
+$ns_newsadded =                         "The news was added";
+
+$ns_categorychanged =                   "The categories were changed";
+
+
+$ns_editnews_thestory =                 "The news";
+$ns_editnews_wasdeleted =               "was deleted";
+$ns_editnews_wasupdated =               "was updated";
+$ns_change_change =						"Change";
+
+
+$ns_upload_error =                      "Error. Could not upload.";
+$ns_upload_errorfile =                  "You did not specify a filename";
+$ns_upload_pictureuploaded =            "You uploaded this picture:";
+
+$ns_picture_deleted =                   "was deleted";
+$ns_picture_delete =                    "Delete";
+$ns_picture_showpicture =               "Show picture";
+
+$ns_user_normal =                       "Normal";
+$ns_user_admin  =                       "admin";
+$ns_user_user =                         "The user";
+$ns_user_added =                        "was added";
+$ns_user_deleted =                      "was deleted";
+$ns_user_adduser =                      "Add user";
+$ns_user_addname =                      "Name:";
+$ns_user_addemail =                     "Email:";
+$ns_user_addusername =                  "Username:";
+$ns_user_addpassword =                  "Password:";
+$ns_user_addlevel =                     "User level";
+$ns_user_deleteuser =                   "Delete user";
+$ns_user_adminonly =                    "Only administrators can change users";
+$ns_user_usersettingsupdated =			"Your user settings was updated <br><br> You have to login again to apply changes.";
+$ns_user_changeaccount =				"Change account";
+$ns_loggedout =                         "You are logged out";
+
+$ns_main_welcome =                      "Welcome to the main menu";
+$ns_main_username =                     "Your username is:";
+$ns_main_email =                        "and your email address is:";
+
+
+$ns_cat_name =                          "Name";
+$ns_cat_filename =                      "Filename";
+$ns_cat_deletecat =                     "Delete category";
+$ns_cat_addcategory =                   "Add category";
+$ns_cat_adminonly =                     "Only administrators can change categories";
+
+
+$ns_change_choose =						"Please choose news to edit";
+$ns_change_nonews =                     "You have not posted any news";
+$ns_change_presentcat =                 "Present category";
+$ns_change_delete =                     "Delete?";
+$ns_change_written_by =                 "Written by:";
+
+$ns_new_doyouwrite = 					"Are you writing for anyone else? (If you don't want to have a 'Written by:....' write: INGEN)";
+$ns_new_nameofauthor =					"Name of author:";
+
+//SITE NAME:
+$name =                                 "SITE NAME";
+
+//Css file
+$css =                                  "temp.css";
+
+//Value for "send" button:
+$send =                                 "Send";
+
+//Headline for archive;
+$arkiv =                                "Archive";
+
+//Link text for "Fullnews"
+$leshele =                              "Read full story";
+
+//open news from the headlines file in frame name, leave blank if none. Main is default
+$target =                               "main";
+
+//make new category:
+$katname =                              "New category:";
+
+
+//"Back" link
+$tilbake =                               "Back";
+
+//"New story"
+$newstory =                              "New story";
+
+//"Headline:"
+$headl = 								"Headline";
+
+//"Introduction:"
+$introl = 								"Introduction";
+
+//"Main text:"
+$hovedl = 								"Full news";
+
+//"Use picture?"
+$bildel = 								"Use picture";
+
+//"URL"
+$urll = 								"URL";
+
+//"Category"
+$katl = 								"Use category";
+
+//Change categories
+$endrekatl = 							"Change categories";
+
+$topbeskr = 							"User: <br>$nsnavn";
+
+
+
+
+
+
+
+
+?>
