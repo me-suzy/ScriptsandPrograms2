47c0fb8ac4053d4595285930216f8fba6890d35c
me-suzy <me.suzana@gmail.com>
2025-08-12 17:46:56 +0300

    Add Image Galleries/cpg1.4.2/cpg142/plugins/sef_urls/configuration.php

---
+++ Image Galleries/cpg1.4.2/cpg142/plugins/sef_urls/configuration.php
@@ -0,0 +1,24 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2005 Coppermine Dev Team
+  v1.1 originaly written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  Coppermine version: 1.4.2
+  $Source: /cvsroot/coppermine/devel/plugins/sef_urls/configuration.php,v $
+  $Revision: 1.3 $
+  $Author: gaugau $
+  $Date: 2005/10/25 01:16:09 $
+**********************************************/
+
+$name = 'Search Engine Friendly URLs';
+$description = 'Makes SEF URLs for index, thumbnails, and displayimage.php.<br />Warning: this plugin is still experimental, there are known issues when using it. Test thoroughly and use at your own risk.';
+$author = 'Coppermine Development Team';
+$version = '1.0';
+?>
