5a0adf7427bf7e9f166bfd7aa4c7e99f03f6a414
me-suzy <me.suzana@gmail.com>
2025-08-12 07:15:15 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_sitesearch_342720.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_sitesearch_342720.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342720;
+function objfile_342720 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Site search";
+$objdata['subname'] = "";
+$objdata['extension'] = "search";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
