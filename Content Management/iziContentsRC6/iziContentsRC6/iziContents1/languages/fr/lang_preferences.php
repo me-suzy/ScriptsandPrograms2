ad8d51c83c7d5b28f718e7ec65ef251554eeaf8b
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:40 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_preferences.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_preferences.php
@@ -0,0 +1,43 @@
+<?
+
+// Form Title
+$GLOBALS["tFormTitle"]		= 'Editer vos Préférences';
+
+// List Headings
+$GLOBALS["tUsername"]		= 'Nom D\'Utilisateur';
+$GLOBALS["tEMail"]		= 'E-Mail';
+
+// Form Field Headings
+$GLOBALS["tLoginName"]		= 'Pseudo de Login';
+$GLOBALS["tPassword"]		= 'Password';
+$GLOBALS["tRetypePassword"]	= 'Retapez password';
+$GLOBALS["tAuthorname"]		= 'Nom Réel';
+$GLOBALS["tUsergroup"]		= 'Groupe D\'Utilisateurs';
+$GLOBALS["tEMail"]		= 'Adresse E-mail';
+$GLOBALS["tCountry"]		= 'Pays';
+$GLOBALS["tLanguage"]		= 'Langue';
+
+// Form Detail Comments and Help Texts
+$GLOBALS["tDetails"]		 = 'Ce formulaire vous permet d\'éditer vos préférences en tant qu\'utilisateur.';
+$GLOBALS["hLoginName"]		 = 'Ceci est votre pseudo de login utilisé dans l\'interface de connexion.';
+$GLOBALS["hPassword"]		 = 'Entrez le password que vous voulez utiliser pour vous connecter à ezContents.';
+$GLOBALS["hRetypePassword"]	 = 'Re-entrez votrepassword, pour vérifier que vous l\'avez tapé correctement.';
+$GLOBALS["hAuthorname"]		 = 'Ce champ représente votre nom complet ou votre surnom. Il sera visible sur tous les articles que vous publierez.';
+$GLOBALS["hUsergroup"]		 = 'Le groupe d\'utilisateurs auquel vous appartenez.';
+$GLOBALS["hEMail"]		 = 'Votre Adresse E-mail. Avec votre nom cela créera un lien hypertexte représentant votre email sur tous les articles que vous publierez.';
+$GLOBALS["hCountry"]		 = 'Le pays où vous vivez (optionnel).';
+$GLOBALS["hLanguage"]		 = 'Votre langue natale (optionnel).<br>';
+$GLOBALS["hLanguage"]		.= 'Si cela est déterminé, alors vous verrez lesoptions du menu, fles champs, l\'aide, etc. dans cette langue à la place du langage par défaut du site.';
+
+// Error Messages
+$GLOBALS["eLoginExists"]	= 'Ce Pseudo de Login existe déjà, svp choisissez en un autre.';
+$GLOBALS["eLoginEmpty"]		= 'Le Pseudo de Login ne peut pas être vide.';
+$GLOBALS["ePasswordError"]	= 'Le password retapé n\'est pas le même que le premier password tapé.';
+$GLOBALS["eNameEmpty"]		= 'Votre Nom ne peut pas être vide.';
+$GLOBALS["eEMailEmpty"]		= 'Votre e-mail ne peut pas être vide.';
+
+
+$GLOBALS["tPrefsUpdated"]	= 'Vos préférences d\'utilisateurs ont été updatées.';
+$GLOBALS["tPrefsRegistered"]	= 'Vous avez été enregistré en tant que nouvel utilisateur.';
+
+?>
