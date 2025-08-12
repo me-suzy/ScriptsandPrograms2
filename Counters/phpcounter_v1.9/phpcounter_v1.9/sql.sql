a81072ae3d2ee40e1e14b707f4e70a57c6784c53
me-suzy <me.suzana@gmail.com>
2025-08-12 08:34:11 +0300

    Add Counters/phpcounter_v1.9/phpcounter_v1.9/sql.sql

---
+++ Counters/phpcounter_v1.9/phpcounter_v1.9/sql.sql
@@ -0,0 +1,17 @@
+# //----------------------------------------//
+# //             Php Counter v1.9           //
+# //             Creator: Tivadar           //
+# //              2005 september            //
+# //                                        //
+# //                                        //
+# // E-mail: info@tivadar.tk                //
+# // MSN: msn@tivadar.tk                    //
+# // Web: www.tivadar.tk                    //
+# //----------------------------------------//
+
+CREATE TABLE `szamlalo` (
+`szam` INT(255) default '0',
+`szam_l` INT(255) default '0'
+);
+
+INSERT INTO `szamlalo` VALUES (0,0);
