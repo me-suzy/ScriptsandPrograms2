8f167204b2781831bc37845d00eb8949c8e25b1a
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:20 +0300

    Add Unsorted/cashcrusader/pages/ptc.php

---
+++ Unsorted/cashcrusader/pages/ptc.php
@@ -0,0 +1,17 @@
+<? include("setup.php");?>
+<? login();?>  
+<? include("header.php");?>
+<? include("account_menu.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr> 
+<td align="center"> <b><br>
+</b> <b><font size="3">Get paid to click on the ads below </font></b> 
+<hr align="center">
+<b><font size="2"><br>
+<? get_ptc_ad('main');?>
+<br>
+<br>
+</font> </b></td>
+</tr>
+</table>
+<? include("footer.php");?>
