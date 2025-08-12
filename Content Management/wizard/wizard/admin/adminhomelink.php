6cc39980372cece7fd949ef0bf9d878b710728eb
me-suzy <me.suzana@gmail.com>
2025-08-12 08:20:05 +0300

    Add Content Management/wizard/wizard/admin/adminhomelink.php

---
+++ Content Management/wizard/wizard/admin/adminhomelink.php
@@ -0,0 +1,5 @@
+<table width="799" border="0" cellpadding="0" cellspacing="0">
+  <tr>
+    <td align="left"><a href="<?php echo CMS_WWW; ?>">Return to: <strong><font face="Arial, Helvetica, sans-serif" style="text-decoration:none">Home</font></strong></a></td>
+  </tr>
+</table>
