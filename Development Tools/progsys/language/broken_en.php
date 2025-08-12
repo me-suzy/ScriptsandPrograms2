1be4d5a0853284f01791f05488c2bfb90f3f97a9
me-suzy <me.suzana@gmail.com>
2025-08-12 10:12:35 +0300

    Add Development Tools/progsys/language/broken_en.php

---
+++ Development Tools/progsys/language/broken_en.php
@@ -0,0 +1,27 @@
+<?php
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+/***************************************************************************
+ * Created by: Boesch IT-Consulting (info@boesch-it.de)
+ * (c)2002-2005 Boesch IT-Consulting
+ * *************************************************************************/
+$l_bm_mailstart = "An user has reported an broken reference link:\r\n";
+$l_bm_contact = "Contact";
+$l_bm_reporttime = "reporting time";
+$l_bm_reportermail = "email of reporter";
+$l_bm_subject = "broken link";
+$l_bm_sitename = "name of website";
+$l_bm_url = "URL";
+$l_bm_language = "language";
+$l_bm_reason = "reason";
+$l_bm_program = "program";
+$l_bm_reasons = array("DNS: Name Not Found","No Reply (Cannot Connect)","Moved","401 Authorization Required","404 Not Found","500 Server Error","Other");
+$l_bm_email = "email";
+$l_bm_reporterip = "ip of reporter";
+?>
