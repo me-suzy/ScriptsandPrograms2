81b7206ebd047551ecae9a58d1d8fd4e78fb9b24
me-suzy <me.suzana@gmail.com>
2025-08-12 11:46:58 +0300

    Add E-Commerce/enterprise3.01/includes/application_bottom_sitemap.php

---
+++ E-Commerce/enterprise3.01/includes/application_bottom_sitemap.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2003 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+// close session (store variables)
+  escs_session_close();
+?>
