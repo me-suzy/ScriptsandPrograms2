3df814673211f7b0abb8250e9ff1ff5f04ce9a6b
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:23 +0300

    Add Content Management/metajour2_1/metajour/lang/listcol.da.php

---
+++ Content Management/metajour2_1/metajour/lang/listcol.da.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Kolonne opsætning';
+$LANG['label_name'] = 'Tabel';
+$LANG['label_pname'] = 'Navn';
+$LANG['label_makedefault'] = 'Gør til forvalgt';
+$LANG['text_1'] = 'Felt';
+$LANG['text_2'] = 'Rækkefølge';
+$LANG['text_3'] = 'Kolonne';
+$LANG['text_4'] = 'Slet';
+$LANG['text_5'] = 'Tilføj felt';
+
+?>
