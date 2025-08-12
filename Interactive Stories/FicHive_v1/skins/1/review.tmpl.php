f6cc463017dfbbc968f7516d7c979e95a2d83ad7
me-suzy <me.suzana@gmail.com>
2025-08-12 18:47:24 +0300

    Add Interactive Stories/FicHive_v1/skins/1/review.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/review.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%RNAMEA%></td><td><%RNAMEB%></td></tr>
+<tr><td colspan='2' class='frame'><textarea name='rreview' class='large'></textarea></td></tr>
+<tr><td colspan='2' class='frame'><input type='submit' value='<%GO%>'></td></tr>
+</table>
