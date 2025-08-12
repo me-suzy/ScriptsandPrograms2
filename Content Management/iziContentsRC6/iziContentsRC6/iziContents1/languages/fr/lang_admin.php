0ea4031a9b504cbabbabec1a5583fdd70f2791fc
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:13 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_admin.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_admin.php
@@ -0,0 +1,169 @@
+<?
+
+$GLOBALS["tPage"] = 'Page';
+$GLOBALS["tOf"] = 'sur';
+$GLOBALS["tEditDel"] = 'Editer/Effacer';
+$GLOBALS["tEditDelRel"] = 'Editer/Effacer/Valider';
+$GLOBALS["tDel"] = 'Effacer';
+$GLOBALS["tAddNew"] = 'Ajouter nouveau';
+$GLOBALS["tEdit"] = 'Editer';
+$GLOBALS["tView"] = 'Voir';
+$GLOBALS["tDelete"] = 'Effacer';
+$GLOBALS["tConfirmDeletion"] = 'Confirmer Effacement';
+$GLOBALS["tToggle"] = 'Inverser Publier/En Attente';
+$GLOBALS["tShowAll"] = 'Voir Tous';
+
+$GLOBALS["tMoveUp"] = 'Haut';
+$GLOBALS["tMoveDown"] = 'En bas';
+
+$GLOBALS["tReleased"] = 'Publier';
+$GLOBALS["tPending"] = 'En attente';
+
+//  Files
+$GLOBALS["tFirstPage"] = 'Premi�re page';
+$GLOBALS["tNextPage"] = 'Page suivante';
+$GLOBALS["tPrevPage"] = 'Page pr�c�dente';
+$GLOBALS["tLastPage"] = 'Derni�re page';
+$GLOBALS["tSort"] = 'Sort by';
+
+//  Action Buttons
+$GLOBALS["tSave"] = 'Sauver';
+$GLOBALS["tReset"] = 'R�initialiser';
+$GLOBALS["tCancel"] = 'Annuler';
+$GLOBALS["tTranslate"] = 'Traduire';
+
+$GLOBALS["tMonth_Array"] = array('', 'Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre');
+$GLOBALS["tWeek_Array"] = array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
+
+$GLOBALS["tYes"] = 'Oui';
+$GLOBALS["tNo"] = 'Non';
+
+$GLOBALS["tLeft"] = 'Gauche';
+$GLOBALS["tCentre"] = 'Centre';
+$GLOBALS["tRight"] = 'Droite';
+$GLOBALS["tJustify"] = 'Justifier';
+
+$GLOBALS["tAscending"] = 'Ascendant';
+$GLOBALS["tDescending"] = 'Descendant';
+
+$GLOBALS["toOrderID"] = 'Ordre ID';
+$GLOBALS["toPublished"] = 'Date de Publication';
+$GLOBALS["toModified"] = 'Date de Modification';
+$GLOBALS["toAlphabetic"] = 'Titre (Alphabetique)';
+
+$GLOBALS["tAlways"] = 'Toujours';
+$GLOBALS["tAutomatic"] = 'Automatique';
+$GLOBALS["tNever"] = 'Jamais';
+
+$GLOBALS["tFilter"] = 'Filtrer';
+$GLOBALS["tSelect"] = 'S�lectionner';
+$GLOBALS["tNoImage"] = 'Aucune image';
+$GLOBALS["tNoParent"] = 'Aucun parent';
+$GLOBALS["tAllParents"] = 'Tous parents';
+$GLOBALS["tShowImage"] = 'Voir image';
+$GLOBALS["tPublish"] = 'Publier';
+$GLOBALS["tExpire"] = 'Expiration';
+
+$GLOBALS["tExpired"] = 'Expir�';
+$GLOBALS["tPending"] = 'Pending';
+$GLOBALS["tActive"] = 'Active';
+$GLOBALS["tEnabled"] = 'Enabled';
+$GLOBALS["tDisabled"] = 'Disabled';
+$GLOBALS["tVisible"] = 'Visible';
+$GLOBALS["tHidden"] = 'Hidden';
+
+//  Menu Texts
+$GLOBALS["tsTitle"] = 'section d\'administration de ezContents';
+$GLOBALS["tmWelcome"] = 'Bienvenue';
+$GLOBALS["tmInGroup"] = 'Vous �tes du groupe';
+$GLOBALS["tsWelcome"] = 'Bienvenue dans la section administration de ezContents ';
+$GLOBALS["tsMessage"] = 'Ici vous pouvez g�rer votre Site Web.';
+
+$GLOBALS["tmSubContent"] = 'User-submitted content';
+$GLOBALS["tmLogin"] = 'Connexion';
+$GLOBALS["tmLogout"] = 'D�connexion';
+
+//  Login Texts
+$GLOBALS["tLoginTitle"] = 'INTERFACE DE CONNEXION';
+$GLOBALS["tInvalidLogin"] = 'Acc�s Refus�';
+$GLOBALS["tUsernameText"] = 'Nom Utilisateur';
+$GLOBALS["tPasswordText"] = 'Password';
+$GLOBALS["tLoginText"] = 'Login';
+
+$GLOBALS["tMustLogin"] = 'Vous �tes d�j� un utilisateur enregistr�, mais vous devez';
+$GLOBALS["tMustRegister"] = 'vous enregistrer et/ou';
+$GLOBALS["tLogin"] = 'vous connecter';
+$GLOBALS["tHigherPriv"] = 'avec les permissions valables';
+$GLOBALS["tToUpdateDetails"] = 'pour updater vos infos personnelles';
+$GLOBALS["tToAccessPrivate"] = 'aux zones priv�es';
+$GLOBALS["eNoLogin"] = 'You must enter your login name';
+$GLOBALS["eNoPassword"] = 'You must enter your password';
+$GLOBALS["tLoginSuccessful"] = 'You have logged on successfully';
+
+$GLOBALS["tFramesRequired"] = 'Cette page utilise des cadres, mais votre navigateur ne les supportent pas.';
+
+//  Sites and themes
+$GLOBALS["tCurrentSite"] = 'Current site';
+$GLOBALS["tMasterSite"] = 'Master Site';
+$GLOBALS["tCurrentTheme"] = 'Current theme';
+$GLOBALS["tDefaultTheme"] = 'Default Theme';
+
+
+//  Translations
+$GLOBALS["tLangFilter"] = 'Language filtre';
+$GLOBALS["tTranslated"] = 'Item has been translated';
+$GLOBALS["tNotTranslated"] = 'Item has not been translated';
+
+//  Help
+$GLOBALS["tOnlineHelp"] = 'Aide en Ligne de ezContents ';
+$GLOBALS["tHelpText"] = 'Cliquez sur le nom d\'un champ pour avoir son explication';
+$GLOBALS["tCloseHelp"] = 'Fermez la Fen�tre d\'aide';
+$GLOBALS["tBabelTranslation"] = 'Babelfish Translation';
+$GLOBALS["tCloseWindow"] = 'Close window';
+
+//  About Texts
+$GLOBALS["tAbout"] = 'A propos d\'iziContents '.$GLOBALS["Version"];
+$GLOBALS["tAboutez"] = 'iziContents est, depuis 2005, une OpenSource CMS se basant sur ezContents de l\'entreprise VisualShaper<br> ezContents a été écrite par Marek Lyczba, Mark Baker, Wilbert Claessens.';
+$GLOBALS["tViewEzContents"] = 'View ezContents';
+$GLOBALS["tWrittenBy"] = 'Ecrit par';
+$GLOBALS["tAdditionalCode"] = 'Additional code by';
+$GLOBALS["tTranslatedBy"] = 'Traductions additionnelles par';
+$GLOBALS["tBetaTestLead"] = 'QA Lead';
+$GLOBALS["tBetaTesters"] = 'Et merci � nos b�ta testeurs';
+$GLOBALS["tModules"] = 'Modules';
+$GLOBALS["tVersion"] = 'Version';
+
+$GLOBALS["tDBClient"] = 'Client';
+$GLOBALS["tDBServer"] = 'Server';
+
+$GLOBALS["tRegisteredTo"] = 'Registered to';
+$GLOBALS["tRegister"] = 'Enter Registration Details';
+$GLOBALS["tUnregistered"] = 'Unregistered Copy';
+
+$GLOBALS["tRegistrationHash"] = 'Registration Hash Code';
+$GLOBALS["tRegistrationBlock"] = 'Registration Block';
+$GLOBALS["hRegistrationHash"] = 'Enter the Hash Code that you were given as part of your registration.<br /><b>Note:-</b> This value is case sensitive.';
+$GLOBALS["hRegistrationBlock"] = 'Enter the Registration Block that you were given as part of your registration exactly as it was sent, including the <-- BEGIN REGISTRATION BLOCK --> and <-- END REGISTRATION BLOCK --> tags.';
+
+
+//  Submitted Content
+$GLOBALS["tThereIs"] = 'il y �';
+$GLOBALS["tThereAre"] = 'il y �';
+$GLOBALS["tEntryPending"] = 'en attente de validation';
+$GLOBALS["tEntriesPending"] = 'en attente de validation';
+$GLOBALS["tRet_SubContent"] = 'Return to Submitted Content module list';
+
+//  Files
+$GLOBALS["tFreeSpace"] = 'Free Space';
+$GLOBALS["eSafeModeWarning"] = '*** WARNING: PHP \'Safe Mode\' restriction may affect functionality ***';
+
+// Browser/Connection Details
+$GLOBALS["Browser"] = 'Browser';
+$GLOBALS["tProxyServer"] = 'Proxy Server';
+$GLOBALS["tProxyPort"] = 'Port';
+$GLOBALS["tProxyUserID"] = 'Proxy User ID';
+$GLOBALS["tProxyPassword"] = 'Password';
+
+$GLOBALS["tUnSupported"] = 'Unsupported';
+
+?>
