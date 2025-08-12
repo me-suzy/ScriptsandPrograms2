96def6388b97c55b4cc216b7c7b56ec587de17be
me-suzy <me.suzana@gmail.com>
2025-08-12 22:00:15 +0300

    Add Polls and Voting/phpvoter-0_6/phpvoter-0.6/languages/swedish.inc.php

---
+++ Polls and Voting/phpvoter-0_6/phpvoter-0.6/languages/swedish.inc.php
@@ -0,0 +1,92 @@
+<?php
+
+/* ***************************** */
+/* LANGUAGE STRINGS FOR PHPVOTER */
+/* ***************************** */
+
+$lang['vote_button'] = "Rösta";
+$lang['onevote'] = "röst";
+$lang['votes'] = "röster";
+$lang['result_saved'] = "Ditt resultat har nu sparats.<br />\n";
+$lang['createvote'] = "Skapa/ändra en omröstning";
+$lang['question'] = "Frågan";
+$lang['alreadyvoted'] = "Du har redan röstat på den aktuella frågan.";
+$lang['viewresults'] = "Visa resultat";
+$lang['noip'] = "Din browser skickade inte med IP-nummer så din röst har inte sparats.<br />\n";
+$lang['notactive'] = "<h1>Tyvärr!</h1>\nDen frågan är inte längre aktiv så det går inte att rösta på den.\n";
+$lang['activequestion'] = "<b>Aktuell fråga:</b><br />\n";
+$lang['totalvoters'] = "<br />Totalt antal röstande: ";
+$lang['info'] = <<<ENDSTRING
+Dessa omröstningar är synnerligen ovetenskapliga och dess enda uppgift
+är att ge en indikation på vad besökarna på www.rollspel.nu tycker i
+olika frågor. Alla får skicka in frågor till <a
+href="mailto:red@rollspel.nu">oss</a> så lägger vi upp nya med jämna
+mellanrum. Tycker ni inte om de alternativ som finns att välja på så
+är det inte mycket att göra åt förutom att ni kan skicka in egna frågor. 
+<br /><br />
+Vi förbehåller oss rätten att refusera inskickade frågor utan att
+behöva motivera orsaken.
+<br /><br />
+ENDSTRING;
+$lang['sorry'] = "Tyvärr";
+$lang['no_votes'] = "Inga röster";
+$lang['no_votes_in_db'] = "Det finns inga röster i databasen.";
+$lang['no_active_vote'] = "Det finns ingen aktiv omröstning.";
+$lang['error_header'] = "Ett fel har uppstått";
+$lang['admin_menu'] = "PHPVoter Administrationsmeny";
+$lang['admin_list'] = "Lista alla omröstningar";
+$lang['admin_create'] = "Skapa en ny omröstning";
+$lang['admin_list_header'] = "Lista över omröstningar";
+$lang['voteid'] = "ID";
+$lang['question'] = "Fråga";
+$lang['date'] = "Datum";
+$lang['state'] = "Typ";
+$lang['answers'] = "Frågor";
+$lang['voters'] = "Röster";
+$lang['function'] = "Funktion";
+$lang['inactive'] = "Inaktiv";
+$lang['active'] = "Aktiv";
+$lang['unfinished'] = "Opublicerad";
+$lang['deleted'] = "Raderad";
+$lang['showvote'] = "Visa/Ändra";
+$lang['admin_show_vote'] = "Visa/Ändra en omröstning";
+$lang['admin_voter_list'] = "Lista över röster";
+$lang['IP'] = "IP";
+$lang['voterid'] = "ID";
+$lang['answer'] = "Svar";
+$lang['delete'] = "Ta bort";
+$lang['change'] = "Ändra";
+$lang['choice_id'] = "ID";
+$lang['admin_delete_question'] = "Ta bort denna fråga";
+$lang['admin_change_question'] = "Ändra denna fråga";
+$lang['admin_vote_question'] = "Rösta på denna fråga";
+$lang['admin_set_this_active'] = "Gör denna fråga aktiv";
+$lang['admin_return'] = "Tillbaka till administrationsmenyn";
+$lang['identifier_deleted'] = "IP borttaget";
+$lang['votes_updated'] = "Antalet röster har justerats.";
+$lang['votes_not_updated'] = "Röstantalet har inte justerats då typ av röst inte sparas i databasen.";
+$lang['vote_activated'] = "Denna omröstning har aktiverats.";
+$lang['one_active_vote'] = "Endast en aktiv omröstning är tillåten, alla andra aktiva omröstningar har gjorts inaktiva.";
+$lang['question_deleted'] = "Den valda frågan har tagits bort.";
+$lang['voters_deleted'] = "Listan över röstare har också tagits bort.";
+$lang['comment'] = "Kommentar till frågan (visas vid komplett lista och på resultatsidan)";
+$lang['savequestion'] = "Spara fråga / Lägg till extra svar";
+$lang['new_answer'] = "Ny";
+$lang['save_question'] = "Spara/Ändra omröstningen";
+$lang['answer_deleted'] = "Svaret har raderats";
+$lang['question_updated'] = "Omröstningen har uppdaterats";
+$lang['error_incorrect_values'] = "Ett eller flera värden var felaktigt ifyllda, inget har sparats.";
+$lang['error_access_denied'] = "Tillträde nekas!";
+$lang['publish'] = "Publiceringsdatum";
+$lang['information'] = "Information";
+$lang['admin_menu'] = "Meny";
+$lang['no_voters'] = "Det är ingen som har svarat på denna fråga ännu.";
+$lang['question_state'] = "Frågans status: ";
+$lang['question_comment'] = "Kommentar till frågan: ";
+$lang['vote_on_question'] = "Rösta på en fråga";
+$lang['set_active_vote'] = "Fråga aktiverad";
+$lang['error_sql_query'] = "SQL-fråga";
+$lang['error_sql'] = "Det blev ett SQL-fel";
+$lang['error_notemplate'] = "Kunde inte hitta mallfilen till denna sida.";
+
+?>
