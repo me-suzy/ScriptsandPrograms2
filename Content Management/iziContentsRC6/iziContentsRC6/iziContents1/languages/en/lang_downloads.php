44144c8aabc505d82c31c2b93b246dc4d2710bca
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:22 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_downloads.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_downloads.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'maintain downloads';
+$GLOBALS["tFormTitle1"] = 'upload download files/create download subdirectories';
+$GLOBALS["tFormTitle2"] = 'Upload download file';
+$GLOBALS["tFormTitle3"] = 'Create Subdirectory';
+
+//  List Functions
+$GLOBALS["tAddNewDownload"] = 'Add new download';
+$GLOBALS["tDeleteDownload"] = 'Delete download';
+$GLOBALS["tDeleteDownloadDir"] = 'Delete download subdirectory';
+
+//  List Headings
+$GLOBALS["tDownloadName"] = 'Download name';
+
+//  Form Fields
+$GLOBALS["tDownloadFilename"] = 'Download filename';
+$GLOBALS["tSubdirectoryname"] = 'Subdirectory name';
+
+//  Form Buttons
+$GLOBALS["bUpload"] = 'Upload!';
+$GLOBALS["bCreateDir"] = 'Create!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] = 'You need to select a file to upload.';
+$GLOBALS["eNoDir"] = 'You need to enter a directory name.';
+$GLOBALS["eDirAlreadyExists"] = 'subdirectory already exists';
+$GLOBALS["eZeroByteFile"] = 'File is 0 bytes in length';
+$GLOBALS["eNoFileUpload"] = 'File could not be uploaded successfully.';
+$GLOBALS["eInvalidFileSize"] = 'File is too large for upload';
+$GLOBALS["eInvalidFileType"] = 'File is not a valid download file';
+$GLOBALS["eInvalidMimeType"] = 'MIME type of File is not a valid download type';
+
+?>
