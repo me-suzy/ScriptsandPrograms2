a3b8e3b87783718b812539c385a586bb5a7c13ad
me-suzy <me.suzana@gmail.com>
2025-08-12 17:08:55 +0300

    Add Groupware Tools/l4w_v0_5_2/modules/emails/models/fields_validations.inc.php

---
+++ Groupware Tools/l4w_v0_5_2/modules/emails/models/fields_validations.inc.php
@@ -0,0 +1,55 @@
+<?php
+
+  /**
+    * $Id: fields_validations.inc.php,v 1.2 2005/05/30 06:25:16 carsten Exp $
+    *
+    * controls additional field validation 
+    *
+    * @author       Carsten Graef <evandor@gmx.de>
+    * @copyright    evandor media 2004
+    * @package      notes
+    */
+    
+   /**
+    * Additional field validation
+    *
+    * Additional validations (apart from core validation) can be defined here. 
+    *
+    * @author       Carsten Graef <evandor@gmx.de>
+    * @copyright    evandor media 2004
+    * @package docs
+    */    
+    class email_fields_validations {
+         
+       /**
+        * Constructor.
+        *
+        * empty
+        * 
+        * @access       public
+        * @since        0.4.0
+        * @version      0.4.4
+        */
+        function email_fields_validations () {
+        }
+
+       /**
+        * User defined validation.
+        *
+        * example function
+        * minLength returns corresponding message if the lastname field is shorter than 3 letters
+        * 
+        * @access       public
+        * @since        0.4.0
+        * @version      0.4.4
+        */
+		function minLength (&$params) {
+			$data = $params->entry['lastname']->data; // cannot use get here (get is calling this line actually!)
+			if (strlen ($data) < 3) { 
+				return "LASTNAME TOO SHORT";
+			}
+			return '';
+		}	
+    }   
+
+?>
