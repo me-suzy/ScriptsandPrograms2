b17c3be60c7a9e9e1fa4e3867f6719bb3074d6ad
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:34 +0300

    Add Content Management/metajour2_1/metajour/lang/structureelement.da.php

---
+++ Content Management/metajour2_1/metajour/lang/structureelement.da.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_pageid'] = 'Tilknyttet dokument';
+$LANG['label_name'] = 'Tekst';
+$LANG['label_showtype'] = 'Visningstype';
+$LANG['label_url'] = 'Ekstern URL';
+$LANG['label_templateid'] = 'Skabelon';
+$LANG['label_binfile1'] = 'Tilknyttet fil';
+$LANG['label_binfile2'] = 'Tilknyttet fil #2';
+$LANG['label_binfile3'] = 'Tilknyttet fil #3';
+$LANG['label_image1'] = 'Design billede #1';
+$LANG['label_image2'] = 'Design billede #2';
+$LANG['label_image3'] = 'Design billede #3';
+$LANG['label_structureid'] = 'Struktur sammenkædning';
+$LANG['label_target'] = 'Destinationsramme';
+$LANG['option_showtype'][0] = 'Normal';
+$LANG['option_showtype'][1] = 'Visning i nyt vindue';
+$LANG['name'] = 'Strukturelementer';
+
+?>
