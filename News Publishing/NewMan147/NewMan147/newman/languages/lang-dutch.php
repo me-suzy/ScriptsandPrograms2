68ad61d62516e448290c3ec6bd87a8b1429894de
me-suzy <me.suzana@gmail.com>
2025-08-12 19:51:00 +0300

    Add News Publishing/NewMan147/NewMan147/newman/languages/lang-dutch.php

---
+++ News Publishing/NewMan147/NewMan147/newman/languages/lang-dutch.php
@@ -0,0 +1,119 @@
+<?php
+  //******************************************************************************************
+  //**                                                                                      **
+  //** phpNewsManager v1.30                                                                 **
+  //** contact: gregor@klevze.si                                                            **
+  //** Last edited: 10th.June,2002                                                          **
+  //**                                                                                      **
+  //** Language module: English                                                             **
+  //******************************************************************************************
+
+define("_CHARSET","ISO-8859-1");
+define("_STATISTICS","Statistieken");
+define("_ACTUAL","Actueel");
+define("_LASTVISIT","Laatste bezoek");
+define("_NEW","Nieuw");
+define("_ONLINENEWS","Online Nieuws");
+define("_WEEKLYPOLLQ","Vragen Wekelijkse Peiling");
+define("_WEEKLYPOLLA","Antwoorden Wekelijkse Peiling");
+define("_PUBLICNEWS","Algemeen Nieuws");
+define("_REGISTEREDUSERS","Geregistreerde gebruiker");
+define("_NEWSCOMMENTS","Nieuws commentaren");
+define("_ADDNEWS","Nieuws toevoegen");
+define("_SUBMITEDNEWS","Toegevoegd nieuws");
+define("_OPTIONS","Opties");
+define("_NEWSHEADLINE","Nieuwskoppen");
+define("_AUTHOR","Auteur");
+define("_DATE","Datum");
+define("_SECTION","Categorie");
+define("_PREVIEW","Inzage");
+define("_MESSAGE","Bericht");
+define("_SUBMIT","Verzenden");
+define("_HOME","Home");
+define("_NEWSMANAGER","Nieuws Menu");
+define("_MODIFYNEWS","Aanpassen Nieuws");
+define("_ADDNEWS","Nieuws toevoegen");
+define("_ADDPARTNERS","Partner toevoegen");
+define("_UPLOADLOGO","Uploaden Logo");
+define("_IMAGE","Plaatje");
+define("_NAME","Naam");
+define("_NUMBER","#");define("_","#");
+define("_CATEGORYNAME","Categorie Naam");
+define("_MAINMENU","Hoofdmenu");
+define("_KEEPIT","Houden!");
+define("_DELETEIT","Verwijderen!");
+define("_EDITCATEGORY","Aanpassen Categorie");
+define("_UPLOADPICTURE","Uploaden Plaatje");
+define("_ADDCATEGORY","Toevoegen Categorie");
+define("_OURPARTNERS","Onze Partners");
+define("_SELECTPAGE","Kies pagina");
+define("_LINK","Link");
+define("_IN","In");
+define("_OUT","Uit");
+define("_AFFILIATES","Leden");
+define("_GFX","Gfx");
+define("_PREVIOUS","Vorige");
+define("_NEXT","Volgende");
+define("_DESCRIPTION","Beschrijving");
+define("_MODIFYPARTNERS","Modify Partners");
+define("_RSSSETTINGS","RSS Instellingen");
+define("_NAMEOFRSSFILE","Naam van het RSS bestand");
+define("_NUMBEROFNEWS","Aantal nieuws items");
+define("_RSSTITLE","RSS Titel");
+define("_RSSLINK","RSS Link");
+define("_RSSDESCRIPTION","RSS Beschrijving");
+define("_AUTOCREATERSS","Automatisch een RSS bestand maken, na elke nieuws aanpassing");
+define("_OPTIMIZED","Geoptimalizeerd");
+define("_OPTIMIZEDATABASE","Database optimalizeren");
+define("_ADDSMILEY","Smiley toevoegen");
+define("_UPLOADSMILEY","Smiley uploaden");
+define("_SMILEYEMOTION","Emotie");
+define("_SMILEYEDITOR","Smiley Bewerker");
+define("_SMILEYCODE","Code");
+define("_EDITSMILEY","Smiley bewerken");
+define("_DELETESMILEY","Smiley verwijderen");
+define("_NEWS","Nieuws");
+define("_CATEGORY","Categorie");
+define("_PARTNERS","Partners");
+define("_WEEKLYPOLL","Wekelijkse Peiling");
+define("_SMILEYS","Smileys");
+define("_BROWSENEWS","Rondkijken in het nieuws");
+define("_GROUPS","Groepen");
+define("_ADMINS","Admins");
+define("_USERS","Gebruikers");
+define("_USERINFO","User Info");
+define("_ADMINISTRATION","Administratie");
+define("_YOULOGOUT","U bent zojuist uitgelogd.");
+define("_LOGIN","Login");
+define("_PASSWORD","Paswoord");
+define("_USERNAME","Gebruikersnaam");
+define("_WEBCONTROLPANEL","WEB CONTROL PANEL");
+define("_ADDUSER","Gebruiker toevoegen");
+define("_EMAIL","Email");
+define("_PRIVILEGES","Rechten");
+define("_EMAIL","Email");
+define("_EDITUSER","Gebruiker bewerken");
+define("_DELETEUSER","Gebruiker schrappen");
+define("_INFO","Informatie");
+define("_ADDGROUP","Groep toevoegen");
+define("_ADMIN","Admin");
+define("_RSS","RSS");
+define("_MODIFY","Veranderen");
+define("_ADD","Toevoegen");
+define("_EDIT","Bewerken");
+define("_DELETE","Schrappen");
+define("_UPLOAD","Uploaden");
+define("_GROUPNAME","Groupname");
+define("_SELECT","Selecteren");
+define("_ADDWEEKLYPOLL","Wekelijkse peiling toevoegen");
+define("_SUBMITEDPOLLS","Geplaatste peilingen");
+define("_QUESTION","Vraag");
+define("_ANSWERS","Antwoord");
+define("_SEPERATEANSWERSWITH","Antwoorden scheiden door");
+define("_EDITWEEKLYPOLL","Wekelijkse peiling bewerken");
+define("_SUCCESS","Succes");
+define("_DELETENEWS","Nieuws schrappen");
+define("_NOTENOUGHPRIV","U heeft niet genoeg rechten!");
+define("_LANGUAGE","Taal");
+define("_CONTENT","Inhoud");
+?>
