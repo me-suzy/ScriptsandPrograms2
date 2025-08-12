41f0db8d59c28af157b91a4e15ad6b9bb056a3b1
me-suzy <me.suzana@gmail.com>
2025-08-13 00:57:41 +0300

    Add Unsorted/Auto-Surf-V2_5_eng/Auto-Surf-V2.5_eng/admin/usersp.php

---
+++ Unsorted/Auto-Surf-V2_5_eng/Auto-Surf-V2.5_eng/admin/usersp.php
@@ -0,0 +1,30 @@
+<?
+include("../templates/admin-header.txt");
+?>
+<center><table border="0" bordercolor="#00FF00">
+              <tr>
+                <td rowspan="6" width="483" valign="top">
+                  <div align="left">
+
+                    <form name="form1" method="post" action="usersp2.php">
+                      <table width="70%" border="0" align="center">
+                        <tr>
+                          <td width="50%">User's e-mail</td>
+                          <td width="77%">
+                            <input type="text" name="email" size="20" maxlength="50">
+                          </td>
+                        </tr>
+                        <tr>
+                          <td colspan="2">
+                            <div align="center">
+                              <input type="submit" name="Submit" value="Lock">
+                            </div>
+                          </td>
+                        </tr>
+                      </table>
+                    </form>
+                    <p align="center">&nbsp;</p>
+    </div>
+                </td>
+              </tr>
+            </table></center>
