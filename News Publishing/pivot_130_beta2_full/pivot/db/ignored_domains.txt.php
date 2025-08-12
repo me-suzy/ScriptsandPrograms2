09b481405bd9640628cad9d56de61005bcdf7ee2
me-suzy <me.suzana@gmail.com>
2025-08-12 20:17:20 +0300

    Add News Publishing/pivot_130_beta2_full/pivot/db/ignored_domains.txt.php

---
+++ News Publishing/pivot_130_beta2_full/pivot/db/ignored_domains.txt.php
@@ -0,0 +1,118 @@
+4u
+acyclovir
+adipex
+adult *
+affiliate *
+aldara
+ambien
+bestiality
+blackjack
+black-jack
+blowjob *
+blow-job *
+cams
+carisoprodol
+casino *
+-casino
+celebrex
+cheap-
+cialis
+clothing *
+commercial-
+condylox
+craps *
+credit *
+credit-card *
+cruises *
+cyclobenzaprine
+denavir
+diet-
+diflucan
+discount *
+drugs *
+ejacula
+ejaculation
+estradiol
+famvir
+fatties *
+finance *
+fioricet
+fluoxetine
+gambling *
+gay *
+goodcounter
+health *
+hentai
+holdem
+hold-em
+hydrocodone
+imitrex
+incest *
+insurance *
+ionamin
+lesbian *
+levitra
+lipitor
+lolita
+loan *
+medicine *
+meridia
+mortgage *
+-online
+online-
+order-
+paxil
+paying *
+penis *
+porn *
+pharmacie
+pharmacy
+phentermine
+-pics
+pills *
+poker *
+-poker
+porn *
+premature *
+prescription *
+propecia
+prozac
+putane
+-rated *
+-rape
+rape-
+replica *
+retin-a
+roulette
+scopate
+seeya *
+semen
+sesso
+-sex
+sex-
+skelaxin
+slot-
+slotmachines
+sodomy
+sperm
+tamiflu
+-teen
+teen-
+tette
+texas-
+tramadol
+triphasil
+ultracet
+ultram
+valium
+valtrex
+voyeur *
+watches *
+weight-
+xanax
+xenical
+xxx
+zanaflex
+zoloft
+zovirax
+zyban
