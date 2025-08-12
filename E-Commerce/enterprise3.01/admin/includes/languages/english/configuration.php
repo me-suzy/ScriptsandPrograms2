b722d60eae4b541d153408a2736f77f70e1a1050
me-suzy <me.suzana@gmail.com>
2025-08-12 11:42:01 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/configuration.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/configuration.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('TABLE_HEADING_CONFIGURATION_TITLE', 'Title');
+define('TABLE_HEADING_CONFIGURATION_VALUE', 'Value');
+define('TABLE_HEADING_ACTION', 'Action');
+
+define('TEXT_INFO_EDIT_INTRO', 'Please make any necessary changes');
+define('TEXT_INFO_DATE_ADDED', 'Date Added:');
+define('TEXT_INFO_LAST_MODIFIED', 'Last Modified:');
+?>
