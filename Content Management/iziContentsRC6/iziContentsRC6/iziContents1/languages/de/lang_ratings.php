2a81aae8de1fd11181b9a9f460616a3c3833d8b0
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:36 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_ratings.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_ratings.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Inhaltseinstellungen verwalten';
+
+//  Form Field Headings
+$GLOBALS["tAllowRatings"] = 'Artikel-Bewertungen erlauben';
+$GLOBALS["tAllowComments"] = 'Artikel-Kommentare erlauben';
+$GLOBALS["tRatingMin"] = 'Bewertungs-Minimum-Wert';
+$GLOBALS["tRatingMax"] = 'Bewertungs-Maximum-Wert';
+$GLOBALS["tRatingImage1"] = 'Stern Bewertungs-Bild';
+$GLOBALS["tRatingImage2"] = 'Stern Bewertungs-Bild (nicht bewertet)';
+$GLOBALS["tVetComments"] = 'Freigabe f&uuml;r Kommentare vor Ver&ouml;ffentlichung';
+$GLOBALS["tPrintFriendly"] = 'Symbol f&uuml;r Druckerfreundlich anzeigen';
+$GLOBALS["tTeaserWithDetails"] = 'Reizartikel-Text (Teaser) auf der Detailseite anzeigen';
+
+//  Form Block Titles
+$GLOBALS["thPrinterFriendly"] = 'druckerfreundliche Optionen';
+$GLOBALS["thContentRatings"] = 'Bewertungsoptionen f&uuml;r den Inhalt';
+$GLOBALS["thContentComments"] = 'Kommentaroptionen f&uuml;r den Inhalt';
+$GLOBALS["thContentSettings"] = 'Andere Optionen f&uuml;r den Inhalt';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'Diese Formular erm&ouml;glicht es, einige globale Werte f&uuml;r die Bewertungen von Inhalten auf Ihrer Seite zu setzen.';
+$GLOBALS["hAllowRatings"] = 'M&ouml;chten Sie Mitgliedern erlauben, Artikel zu bewerten?';
+$GLOBALS["hAllowComments"] = 'M&ouml;chten Sie Mitgliedern erlauben, Kommentare zu Artikeln zu schreiben?';
+$GLOBALS["hRatingMin"] = 'W&auml;hlen Sie den Wert f&uuml;r die kleinste Bewertung.';
+$GLOBALS["hRatingMax"] = 'W&auml;hlen Sie den Wert f&uuml;r die h&ouml;chste Bewertung.';
+$GLOBALS["hRatingImage1"] = 'W&auml;hlen Sie das Bild, welches f&uuml;r die Bewertungen angezeigt werden soll.';
+$GLOBALS["hRatingImage2"] = 'W&auml;hlen Sie das Bild, welches f&uuml;r die Bewertungen angezeigt werden soll.';
+$GLOBALS["hVetComments"] = 'M&ouml;chten Sie Kommentare erst lesen und freigeben, bevor sie auf der Webseite erscheinen?<br />Achtung, diese Funktion ist noch nicht verf&uuml;gbar.';
+$GLOBALS["hPrintFriendly"] = 'Dies ist die Standard-Option f&uuml;r druckerfreundlichen Inhalt.';
+$GLOBALS["hTeaserWithDetails"] = 'Soll der Teaser Text auf der Detailseite gezeigt werden, gemeinsam mit dem zus&auml;tzlich geschriebenen Text im Inhalt.';
+
+?>
