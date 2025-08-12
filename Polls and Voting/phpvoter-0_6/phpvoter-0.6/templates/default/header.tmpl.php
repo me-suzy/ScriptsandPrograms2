a812a82a5d072114dfef22e982ed0c37cacf5181
me-suzy <me.suzana@gmail.com>
2025-08-12 22:00:19 +0300

    Add Polls and Voting/phpvoter-0_6/phpvoter-0.6/templates/default/header.tmpl.php

---
+++ Polls and Voting/phpvoter-0_6/phpvoter-0.6/templates/default/header.tmpl.php
@@ -0,0 +1,14 @@
+<?php
+
+$template_content = <<<ENDSTRING
+<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">
+<html>
+<head>
+	<title>{$config['longversion']}</title>
+</head>
+
+<body>
+{$template['fontstring']}
+ENDSTRING;
+
+?>
