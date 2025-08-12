5096aaa4b0940b7e5ec305ce49a7dd39e1005d1c
me-suzy <me.suzana@gmail.com>
2025-08-12 23:28:16 +0300

    Add Postcards/webcards_1_final_fixed/webcards_1_final_fixed/upload/lang/English/pickup.php

---
+++ Postcards/webcards_1_final_fixed/webcards_1_final_fixed/upload/lang/English/pickup.php
@@ -0,0 +1,15 @@
+<?php
+$lang['no_code'] = "You did not enter a pickup code";
+$lang['invalid_code'] = "You entered an invalid code";
+$lang['card_id_invalid'] = "The card ID you entered was invalid. The card may have expired or never existed at all.";
+$lang['fill_in_id'] = "Fill in the pickup ID you recieved in the email.";
+$lang['go'] = "Go";
+$lang['forgot_code'] = "Forgotten your pickup code?";
+$lang['click_forgot'] = "Click here to resend your code.";
+$lang['no_resends'] = "Sorry! The administrator has disabled re-sending of WebCards pickup codes.";
+$lang['explain_pickup_procedure'] = "Type in your email address. The ID for the newest WebCard that is present in the database will be sent to your email address.<br />It is not possible to recover older WebCards.";
+$lang['no_email'] = "You did not enter an email address";
+$lang['reminder_sent'] = "Your email address was found in the database and your pickup code has been sucessfully sent.";
+$lang['no_cards_found'] = "No WebCards were found that were sent to your email address ";
+$lang['webcards_may_be_deleted'] = "The WebCard may have been deleted or may have never existed at all.<br /><br />Go back and check your email address.";
+?>
