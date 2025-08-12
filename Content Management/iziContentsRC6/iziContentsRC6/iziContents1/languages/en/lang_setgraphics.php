18abb70dac51cc2f3b9b8956e6ded26585c807c0
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:46 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_setgraphics.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_setgraphics.php
@@ -0,0 +1,103 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'set site graphics';
+
+//  Form Block Titles
+$GLOBALS["thSiteLogo"] = 'Site Logo';
+$GLOBALS["thBackgrounds"] = 'Background Images';
+$GLOBALS["thPagingIcons"] = 'Paging Icons';
+$GLOBALS["thMenuIcons"] = 'Menu Icons';
+$GLOBALS["thSpecialIcons"] = 'Special Icons';
+
+//  Form Field Headings
+$GLOBALS["tHomepageLogo"] = 'Homepage logo image';
+$GLOBALS["tTopBackground"] = 'Top background';
+$GLOBALS["tTopBackgroundRepeat"] = 'Repeat top background';
+$GLOBALS["tTopBackgroundFixed"] = 'Fixed top background';
+$GLOBALS["tTopBackgroundPosition"] = 'Position top background';
+$GLOBALS["tMainBackground"] = 'Main page background';
+$GLOBALS["tMainBackgroundRepeat"] = 'Repeat main page background';
+$GLOBALS["tMainBackgroundFixed"] = 'Fixed main page background';
+$GLOBALS["tMainBackgroundPosition"] = 'Position main page background';
+$GLOBALS["tBorderBackground"] = 'Border background';
+$GLOBALS["tBorderBackgroundRepeat"] = 'Repeat border background';
+$GLOBALS["tBorderBackgroundFixed"] = 'Fixed border background';
+$GLOBALS["tBorderBackgroundPosition"] = 'Position border background';
+$GLOBALS["tContentBackground"] = 'Content header background';
+$GLOBALS["tContentBackgroundRepeat"] = 'Repeat content header background';
+$GLOBALS["tContentBackgroundPosition"] = 'Position content header background';
+$GLOBALS["tTopMenuBackground"] = 'Top Menu background';
+$GLOBALS["tTopMenuBackgroundRepeat"] = 'Repeat top menu background';
+$GLOBALS["tTopMenuBackgroundFixed"] = 'Fixed top menu background';
+$GLOBALS["tTopMenuBackgroundPosition"] = 'Position top menu background';
+$GLOBALS["tMenuBackground"] = 'Menu background';
+$GLOBALS["tMenuBackgroundRepeat"] = 'Repeat menu background';
+$GLOBALS["tMenuBackgroundFixed"] = 'Fixed menu background';
+$GLOBALS["tMenuBackgroundPosition"] = 'Position menu background';
+$GLOBALS["tFooterBackground"] = 'Footer background';
+$GLOBALS["tFooterBackgroundRepeat"] = 'Repeat footer background';
+$GLOBALS["tFooterBackgroundFixed"] = 'Fixed footer background';
+$GLOBALS["tFooterBackgroundPosition"] = 'Position footer background';
+$GLOBALS["tFirstPageIcon"] = 'First Page icon';
+$GLOBALS["tPrevPageIcon"] = 'Previous Page icon';
+$GLOBALS["tNextPageIcon"] = 'Next Page icon';
+$GLOBALS["tLastPageIcon"] = 'Last Page icon';
+$GLOBALS["tListStyleIcon"] = 'List Style icon';
+$GLOBALS["tExpandIcon"] = 'Expand menu icon';
+$GLOBALS["tCollapseIcon"] = 'Collapse menu icon';
+$GLOBALS["tNoExpandIcon"] = 'No-expansion menu icon';
+$GLOBALS["tSecureIcon"] = 'Restricted menu icon';
+$GLOBALS["tPrintIcon"] = 'Printerfriendly icon';
+$GLOBALS["tPDFIcon"] = 'PDF icon';
+$GLOBALS["tTellFriendIcon"] = 'Tell-a-Friend';
+$GLOBALS["tRatingIcon"] = 'Article Rating icon';
+$GLOBALS["tCommentIcon"] = 'Article Comments icon';
+
+//  Form Text Descriptions
+$GLOBALS["tDetails"] = 'Here you can set images and backgrounds for your website.<br />Make sure the images that you need are uploaded with the image upload maintenance function.';
+
+$GLOBALS["hHomepageLogo"] = 'This is the main logo of your site, displayed on the top-left of the browser window.';
+$GLOBALS["hTopBackground"] = 'This will set the background for the header frame with your logo and the banner, a header \"userdata\" frame if you have configured your site for this.';
+$GLOBALS["hTopBackgroundRepeat"] = 'Repeat (tile) the header background image.';
+$GLOBALS["hTopBackgroundFixed"] = 'Check this box if you want the header frame background to remain fixed if it is in a scrolling region';
+$GLOBALS["hTopBackgroundPosition"] = 'Select the desired setting for the position of the background, if it is not repeated';
+$GLOBALS["hMainBackground"] = 'This will set the main background.<br />If you set \"Table background color\" and \"Table border color\" to \'empty\' it will make the content table transparent and the background will be also put \'under\' the table.';
+$GLOBALS["hMainBackgroundRepeat"] = 'Repeat (tile) the main background image.';
+$GLOBALS["hMainBackgroundFixed"] = 'Check this box if you want the main page background to remain fixed if it is in a scrolling region';
+$GLOBALS["hMainBackgroundPosition"] = 'Select the desired setting for the position of the background, if it is not repeated';
+$GLOBALS["hBorderBackground"] = 'This will set the border background.<br />If you set \"Table background color\" and \"Table border color\" to \'empty\' it will make the content table transparent and the background will be also put \'under\' the table.';
+$GLOBALS["hBorderBackgroundRepeat"] = 'Repeat (tile) the border background image.';
+$GLOBALS["hBorderBackgroundFixed"] = 'Check this box if you want the border background to remain fixed if it is in a scrolling region';
+$GLOBALS["hBorderBackgroundPosition"] = 'Select the desired setting for the position of the background, if it is not repeated';
+$GLOBALS["hContentBackground"] = 'The background image for the content header. It is set for all the articles published on the site.<br />The advantage of this is that you can place text on top of this. When you make the header an image (in the content management) you will need separate images for each article. You can also use the header image and background image together.';
+$GLOBALS["hContentBackgroundRepeat"] = 'Repeat (tile) the content background image.';
+$GLOBALS["hContentBackgroundPosition"] = 'Select the desired setting for the position of the background, if it is not repeated';
+$GLOBALS["hTopMenuBackground"] = 'This will set the top menu background image. If you set the top menu background color to \"empty\" the table that is usually displayed around the menu will disappear giving a better effect when a background is set up.';
+$GLOBALS["hTopMenuBackgroundRepeat"] = 'Repeat (tile) the top menu background image.';
+$GLOBALS["hTopMenuBackgroundFixed"] = 'Check this box if you want the top menu frame background to remain fixed if it is in a scrolling region';
+$GLOBALS["hTopMenuBackgroundPosition"] = 'Select the desired setting for the position of the background, if it is not repeated';
+$GLOBALS["hMenuBackground"] = 'This will set the menu background image. If you set the menu background color to \"empty\" the table that is usually displayed around the menu will disappear giving a better effect when a background is set up.';
+$GLOBALS["hMenuBackgroundRepeat"] = 'Repeat (tile) the menu background image.';
+$GLOBALS["hMenuBackgroundFixed"] = 'Check this box if you want the menu frame background to remain fixed if it is in a scrolling region';
+$GLOBALS["hMenuBackgroundPosition"] = 'Select the desired setting for the position of the background, if it is not repeated';
+$GLOBALS["hFooterBackground"] = 'This will set the background for the footer frame if you have enabled that feature of ezContents.';
+$GLOBALS["hFooterBackgroundRepeat"] = 'Repeat (tile) the footer background image.';
+$GLOBALS["hFooterBackgroundFixed"] = 'Check this box if you want the footer frame background to remain fixed if it is in a scrolling region';
+$GLOBALS["hFooterBackgroundPosition"] = 'Select the desired setting for the position of the background, if it is not repeated';
+$GLOBALS["hFirstPageIcon"] = 'This is the icon displayed for paged articles to return to the first page';
+$GLOBALS["hPrevPageIcon"] = 'This is the icon displayed for paged articles to return to the previous page';
+$GLOBALS["hNextPageIcon"] = 'This is the icon displayed for paged articles to move on to the next page';
+$GLOBALS["hLastPageIcon"] = 'This is the icon displayed for paged articles to move on to the last page';
+$GLOBALS["hListStyleIcon"] = 'This is the icon displayed for unordered lists which are defined in contents with <li>';
+$GLOBALS["hExpandIcon"] = 'This is the icon that will be displayed alongside menu entries that can be expanded to reveal a submenu.';
+$GLOBALS["hCollapseIcon"] = 'This icon will appear alongside an expanded menu to show that the sub-menu can be collapsed again.';
+$GLOBALS["hNoExpandIcon"] = 'This menu icon will appear alongside menu items that cannot be expanded or collapsed because there is no sub-menu to expand.';
+$GLOBALS["hSecureIcon"] = 'This icon will appear alongside restricted menu items to show that they are restricted.';
+$GLOBALS["hPrintIcon"] = 'This icon provides a link to a printer friendly version of an article.';
+$GLOBALS["hPDFIcon"] = 'This icon provides a link to a pdf version of an article.';
+$GLOBALS["hTellFriendIcon"] = 'This icon provides a link to the tell-a-friend file.';
+$GLOBALS["hRatingIcon"] = 'This icon appears on articles that can be rated by readers.';
+$GLOBALS["hCommentIcon"] = 'This icon appears on articles where readers can make comments.';
+
+?>
