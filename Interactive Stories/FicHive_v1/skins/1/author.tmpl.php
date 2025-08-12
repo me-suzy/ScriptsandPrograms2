9d9c004fb2d9a679ea0a161b6534cf16e6ceea91
me-suzy <me.suzana@gmail.com>
2025-08-12 18:46:12 +0300

    Add Interactive Stories/FicHive_v1/skins/1/author.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/author.tmpl.php
@@ -0,0 +1,9 @@
+<table border='0' width='100%'>
+<tr><td valign='top' class='fframe'><center><%AVATAR%></center></td>
+<td class='chapter'><b><%AUTHORNAME%></b><br>
+<%STARTED%>: <%STARTED_VAL%><br>
+<%GROUP%>: <%GROUP_VAL%></td></tr>
+<tr><td class='chapter' colspan='2'><%AUTHORBIOGRAPHY%></td></tr>
+<tr><td class='chapter' colspan='2'><%WRITTEN%></td></tr>
+<tr><td class='chapter' colspan='2'><%FAVORITES%></td></tr>
+</table>
