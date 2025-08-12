a614987132e1e81a94a7cc02c17c308caa11d005
me-suzy <me.suzana@gmail.com>
2025-08-12 11:10:27 +0300

    Add Documents/simpledoc-0.7.0/simpledoc/www/config.php

---
+++ Documents/simpledoc-0.7.0/simpledoc/www/config.php
@@ -0,0 +1,8 @@
+<?php
+$CONFIG['username'] = 'admin';
+$CONFIG['password'] = 'admin';
+$CONFIG['encoding'] = 'iso-8859-1';
+$CONFIG['editor-width'] = '600';
+$CONFIG['editor-height'] = '350';
+$CONFIG['publish_dir'] = 'publish';
+?>
