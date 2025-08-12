13bef4c58ccfd69dcc019b16220e305d22e66301
me-suzy <me.suzana@gmail.com>
2025-08-12 17:27:28 +0300

    Add Guestbooks/accelgbook/accelgbook/admin/index.php

---
+++ Guestbooks/accelgbook/accelgbook/admin/index.php
@@ -0,0 +1,15 @@
+<?php require_once("adminOnly.php");?>
+<?
+$URL="frame.html";
+header ("Location: $URL");
+?>
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+
+</body>
+</html>
