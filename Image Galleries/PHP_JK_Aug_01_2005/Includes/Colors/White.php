b662a8369a39d209e4aafb1c3e754c0b1cd051e8
me-suzy <me.suzana@gmail.com>
2025-08-12 18:12:56 +0300

    Add Image Galleries/PHP_JK_Aug_01_2005/Includes/Colors/White.php

---
+++ Image Galleries/PHP_JK_Aug_01_2005/Includes/Colors/White.php
@@ -0,0 +1,32 @@
+<?php
+	$PageText = "000000";
+	$PageBGColor = "FFFFFF";
+	$PageBorder = "CCCCCC";
+	$PageLinkColor = "884488";
+	$PageALinkColor = "448844";
+	$PageVLinkColor = "444488";
+	
+	$TextColor1 = "000000";
+	$BGColor1 = "F5F5F5";
+	$BorderColor1 = "CCCCCC";
+	$LinkColor1 = "884488";
+	$ALinkColor1 = "448844";
+	$VLinkColor1 = "444488";
+	
+	$TextColor2 = "000000";
+	$BGColor2 = "EEEEEE";
+	$BorderColor2 = "000000";
+	$LinkColor2 = "884488";
+	$ALinkColor2 = "448844";
+	$VLinkColor2 = "444488";
+	
+	$TextColor3 = "000000";
+	$BGColor3 = "DDDDDD";
+	$BorderColor3 = "000000";
+	$LinkColor3 = "884488";
+	$ALinkColor3 = "448844";
+	$VLinkColor3 = "444488";
+	
+	$ErrorColor = "FF6666";
+	$SuccessColor = "444488";
+?>
