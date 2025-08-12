f54b686cfd1f723049f8532c2c58f47749776c03
me-suzy <me.suzana@gmail.com>
2025-08-12 06:43:56 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/insertdatetime/langs/zh_cn.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/insertdatetime/langs/zh_cn.js
@@ -0,0 +1,11 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµ&Auml;&Ouml;ÐÎ&Auml;&Ouml;§³&Ouml;
+
+tinyMCE.addToLang('',{
+insertdate_desc : '²&aring;Èëµ±Ç°ÈÕÆÚ',
+inserttime_desc : '²&aring;Èëµ±Ç°Ê±¼&auml;',
+inserttime_months_long : new Array("Ò»ÔÂ·Ý", "¶þÔÂ·Ý", "ÈýÔÂ·Ý", "Ë&Auml;ÔÂ·Ý", "Î&aring;ÔÂ·Ý", "ÁùÔÂ·Ý", "ÆßÔÂ·Ý", "°ËÔÂ·Ý", "¾&Aring;ÔÂ·Ý", "Ê®ÔÂ·Ý", "Ê®Ò»ÔÂ·Ý", "Ê®¶þÔÂ·Ý"),
+inserttime_months_short : new Array("Ò»ÔÂ", "¶þÔÂ", "ÈýÔÂ", "Ë&Auml;ÔÂ", "Î&aring;ÔÂ", "ÁùÔÂ", "ÆßÔÂ", "°ËÔÂ", "¾&Aring;ÔÂ", "Ê®ÔÂ", "Ê®Ò»ÔÂ", "Ê®¶þÔÂ"),
+inserttime_day_long : new Array("ÐÇÆÚÈÕ", "ÐÇÆÚÒ»", "ÐÇÆÚ¶þ", "ÐÇÆÚÈý", "ÐÇÆÚË&Auml;", "ÐÇÆÚÎ&aring;", "ÐÇÆÚÁù", "ÐÇÆÚÈÕ"),
+inserttime_day_short : new Array("&Ouml;ÜÈÕ", "&Ouml;ÜÒ»", "&Ouml;Ü¶þ", "&Ouml;ÜÈý", "&Ouml;ÜË&Auml;", "&Ouml;ÜÎ&aring;", "&Ouml;ÜÁù", "&Ouml;ÜÈÕ")
+});
