420ee12a2d0cf4f0dd0acf3c945118e41bcd3b58
me-suzy <me.suzana@gmail.com>
2025-08-12 10:07:32 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/widgets/classTextWidget.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/widgets/classTextWidget.php
@@ -0,0 +1,19 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+
+includeClass('PntTextWidget', 'pnt/web/widgets');
+
+/** FormWidget that generates html specifying an input type=Text.
+*
+* This concrete subclass is here to keep de application developers
+* code separated from the framework code.
+* @see http://www.phppeanuts.org/site/index_php/Menu/178
+* Framework code is in the superclass.
+* @package widgets
+*/
+class TextWidget extends PntTextWidget {
+
+}
+?>
