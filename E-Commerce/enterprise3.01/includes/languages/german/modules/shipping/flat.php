4ca5a5d3ab57398d1888da200e192e05d92fb2ce
me-suzy <me.suzana@gmail.com>
2025-08-12 12:01:38 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/modules/shipping/flat.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/modules/shipping/flat.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_FLAT_TEXT_TITLE', 'Pauschale Versandkosten');
+define('MODULE_SHIPPING_FLAT_TEXT_DESCRIPTION', 'Pauschale Versandkosten');
+define('MODULE_SHIPPING_FLAT_TEXT_WAY', '');
+?>
