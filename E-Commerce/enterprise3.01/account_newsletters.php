5953ae4541bccec5e5bb19fe7c698a3804da0d87
me-suzy <me.suzana@gmail.com>
2025-08-12 11:21:31 +0300

    Add E-Commerce/enterprise3.01/account_newsletters.php

---
+++ E-Commerce/enterprise3.01/account_newsletters.php
@@ -0,0 +1,52 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  require('includes/application_top.php');
+
+  if (!escs_session_is_registered('customer_id')) {
+    $navigation->set_snapshot();
+    escs_redirect(escs_href_link(FILENAME_LOGIN, '', 'SSL'));
+  }
+
+// needs to be included earlier to set the success message in the messageStack
+  require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_ACCOUNT_NEWSLETTERS);
+
+  $newsletter_query = escs_db_query("select customers_newsletter from " . TABLE_CUSTOMERS . " where customers_id = '" . (int)$customer_id . "'");
+  $newsletter = escs_db_fetch_array($newsletter_query);
+
+  if (isset($HTTP_POST_VARS['action']) && ($HTTP_POST_VARS['action'] == 'process')) {
+    if (isset($HTTP_POST_VARS['newsletter_general']) && is_numeric($HTTP_POST_VARS['newsletter_general'])) {
+      $newsletter_general = escs_db_prepare_input($HTTP_POST_VARS['newsletter_general']);
+    } else {
+      $newsletter_general = '0';
+    }
+
+    if ($newsletter_general != $newsletter['customers_newsletter']) {
+      $newsletter_general = (($newsletter['customers_newsletter'] == '1') ? '0' : '1');
+
+      escs_db_query("update " . TABLE_CUSTOMERS . " set customers_newsletter = '" . (int)$newsletter_general . "' where customers_id = '" . (int)$customer_id . "'");
+    }
+
+    $messageStack->add_session('account', SUCCESS_NEWSLETTER_UPDATED, 'success');
+
+    escs_redirect(escs_href_link(FILENAME_ACCOUNT, '', 'SSL'));
+  }
+
+  $breadcrumb->add(NAVBAR_TITLE_1, escs_href_link(FILENAME_ACCOUNT, '', 'SSL'));
+  $breadcrumb->add(NAVBAR_TITLE_2, escs_href_link(FILENAME_ACCOUNT_NEWSLETTERS, '', 'SSL'));
+
+  $content = CONTENT_ACCOUNT_NEWSLETTERS;
+  $javascript = $content . '.js';
+
+  require(DIR_WS_TEMPLATES . TEMPLATENAME_MAIN_PAGE);
+
+  require(DIR_WS_INCLUDES . 'application_bottom.php');
+?>
