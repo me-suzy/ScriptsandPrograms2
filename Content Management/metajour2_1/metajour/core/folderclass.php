445ccea1f0c0825046e6f0db27c9b546af8b8948
me-suzy <me.suzana@gmail.com>
2025-08-12 06:02:43 +0300

    Add Content Management/metajour2_1/metajour/core/folderclass.php

---
+++ Content Management/metajour2_1/metajour/core/folderclass.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage core
+ */
+
+require_once('basicclass.php');
+
+class folder extends basic {
+
+	function folder() {
+		$this->basic();
+		$this->addcolumn('name',0,UI_STRING);
+	
+		$this->removeview('createvariant');
+	}
+
+}
