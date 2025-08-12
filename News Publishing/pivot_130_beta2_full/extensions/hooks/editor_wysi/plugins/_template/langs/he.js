8b92c529071a2ba4441aa183e622855947107372
me-suzy <me.suzana@gmail.com>
2025-08-12 20:11:06 +0300

    Add News Publishing/pivot_130_beta2_full/extensions/hooks/editor_wysi/plugins/_template/langs/he.js

---
+++ News Publishing/pivot_130_beta2_full/extensions/hooks/editor_wysi/plugins/_template/langs/he.js
@@ -0,0 +1,8 @@
+// HE lang variables
+
+/* Remember to namespace the language parameters lang_<your plugin>_<some name> */
+
+tinyMCE.addToLang('',{
+template_title : 'æä ñúí çìåï ÷åôõ ùì äúáðéú',
+template_desc : 'æä ñúí ëôúåø ùì äúáðéú'
+});
