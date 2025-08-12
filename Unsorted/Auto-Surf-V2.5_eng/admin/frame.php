5fa42b359272c35da04686fd6c9b21e4007dd2d2
me-suzy <me.suzana@gmail.com>
2025-08-13 00:54:02 +0300

    Add Unsorted/Auto-Surf-V2.5_eng/admin/frame.php

---
+++ Unsorted/Auto-Surf-V2.5_eng/admin/frame.php
@@ -0,0 +1,6 @@
+<html><head><title>Admin Surf</title></head>
+<frameset rows="40,*" frameborder="NO" border="0" framespacing="0"> 
+  <frame name="top" scrolling="NO" noresize src="./frame_top.php?url=<?php echo $url; ?>" >
+  <frame name="bottom" src="<?php echo $url; ?>">
+</frameset>
+</html>
