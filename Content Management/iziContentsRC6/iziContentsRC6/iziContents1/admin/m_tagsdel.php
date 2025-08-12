fd187b448f96eee5033a70aea67fb10e3c55cb65
me-suzy <me.suzana@gmail.com>
2025-08-12 03:38:49 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_tagsdel.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_tagsdel.php
@@ -0,0 +1,56 @@
+<?php
+
+/***************************************************************************
+
+ m_tagsdel.php
+ --------------
+ copyright : (C) 2005 - The iziContents Development Team
+
+ iziContents version : 1.0
+ fileversion : 1.0.1
+ change date : 23 - 04 - 2005
+ ***************************************************************************/
+
+/***************************************************************************
+ The iziContents Development Team offers no warranties on this script.
+ The owner/licensee of the script is solely responsible for any problems
+ caused by installation of the script or use of the script.
+
+ All copyright notices regarding iziContents and ezContents must remain intact on the scripts and in the HTML for the scripts.
+
+ For more info on iziContents,
+ visit http://www.izicontents.com*/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the License which can be found within the
+ *   zipped package. Under the licence of GPL/GNU.
+ *
+ ***************************************************************************/
+
+include_once ("rootdatapath.php");
+
+$GLOBALS["form"] = 'tags';
+$validaccess = VerifyAdminLogin();
+
+
+if ($GLOBALS["candelete"] == False)
+{
+   Header("Location: ".BuildLink('adminlogin.php'));
+} else {
+   DeleteTag();
+   Header("Location: ".BuildLink('m_tags.php')."&page=".$_GET["page"]."&sort=".$_GET["sort"]);
+}
+
+
+function DeleteTag()
+{
+   global $_GET;
+
+   $strQuery = "DELETE FROM ".$GLOBALS["eztbTags"]." WHERE tagid='".$_GET["TagID"]."' AND candelete='Y'";
+   $result = dbExecute($strQuery,true);
+   dbCommit();
+} // function DeleteTag()
+
+?>
