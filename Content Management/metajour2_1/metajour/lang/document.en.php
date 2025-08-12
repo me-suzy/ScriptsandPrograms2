f58213a1a3d338411ca849f2a867dab6d088cc7f
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:24 +0300

    Add Content Management/metajour2_1/metajour/lang/document.en.php

---
+++ Content Management/metajour2_1/metajour/lang/document.en.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Documents';
+$LANG['label_name'] = 'Document name';
+$LANG['label_templateid'] = 'Template';
+$LANG['label_stylesheetid'] = 'Stylesheet';
+$LANG['label_metadataid'] = 'Metadata';
+$LANG['label_alias'] = 'Subname';
+$LANG['label_structureid'] = 'Structure';
+$LANG['label_nolist'] = 'Exclude from website lists';
+$LANG['label_nosearch'] = 'Exclude from website search';
+?>
