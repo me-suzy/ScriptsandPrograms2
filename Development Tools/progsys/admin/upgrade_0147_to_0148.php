934486c6fcf67ad9f6b1691ae81380cc49ce10cd
me-suzy <me.suzana@gmail.com>
2025-08-12 10:10:44 +0300

    Add Development Tools/progsys/admin/upgrade_0147_to_0148.php

---
+++ Development Tools/progsys/admin/upgrade_0147_to_0148.php
@@ -0,0 +1,29 @@
+<?php
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+/***************************************************************************
+ * Created by: Boesch IT-Consulting (info@boesch-it.de)
+ * *************************************************************************/
+require('../config.php');
+require('./functions.php');
+?>
+<html><body>
+<div align="center"><h3>ProgSys: Upgrade from 0.147 to 0.148</h3></div>
+<br>
+<?php
+echo "Upgrading tables...<br>";
+flush();
+$sql = "ALTER TABLE ".$tableprefix."_programm ";
+$sql.= "ADD disableref tinyint(1) unsigned NOT NULL default '0'";
+if(!$result = mysql_query($sql, $db))
+	die("Unable to modify table ".$tableprefix."_programm".mysql_error());
+?>
+<br><div align="center">Upgrade done.<br>Please remove install.php, upgrade*.php, mkconfig.php and fill_*.php from server</div>
+<div align="center">Now you can login to the <a href="index.php">admininterface</a></div>
+</body></html>
