6f568ce7748252583110b347ff2036b43fef4e63
me-suzy <me.suzana@gmail.com>
2025-08-13 02:28:28 +0300

    Add Unsorted/destiney/include/styles.php

---
+++ Unsorted/destiney/include/styles.php
@@ -0,0 +1,201 @@
+<?php
+
+$final_output .= <<<EOF
+
+<style type="text/css">
+body {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: normal;
+	color: $base_font_color;
+}
+.dc {
+	font-family: $base_font;
+	font-size: $x_large_font;
+	font-weight: bold;
+	color: $table_title_text_color;
+}
+a {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: normal;
+	color: $base_link_color;
+}
+a:active {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: normal;
+	color: $base_link_color;
+}
+a:visited {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: normal;
+	color: $base_link_color;
+}
+a:hover {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: normal;
+	color: $hover_link_color;
+	background-color: $hover_link_bg_color;
+}
+a.bold {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: bold;
+	color: $base_link_color;
+}
+a:active.bold {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: bold;
+	color: $base_link_color;
+}
+a:visited.bold {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: bold;
+	color: $base_link_color;
+}
+a:hover.bold {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: bold;
+	color: $hover_link_color;
+	background-color: $hover_link_bg_color;
+}
+a.small {
+	font-family: $base_font;
+	font-size: $small_font;
+	font-weight: normal;
+	color: $base_font_color;
+}
+a:active.small {
+	font-family: $base_font;
+	font-size: $small_font;
+	font-weight: normal;
+	color: $base_font_color;
+}
+a:visited.small {
+	font-family: $base_font;
+	font-size: $small_font;
+	font-weight: normal;
+	color: $base_font_color;
+}
+a:hover.small {
+	font-family: $base_font;
+	font-size: $small_font;
+	font-weight: normal;
+	color: $hover_link_color;
+	background-color: $hover_link_bg_color;
+}
+a.smallbold {
+	font-family: $base_font;
+	font-size: $small_font;
+	font-weight: bold;
+	color: $base_font_color;
+}
+a:active.smallbold {
+	font-family: $base_font;
+	font-size: $small_font;
+	font-weight: bold;
+	color: $base_font_color;
+}
+a:visited.smallbold {
+	font-family: $base_font;
+	font-size: $small_font;
+	font-weight: bold;
+	color: $base_font_color;
+}
+a:hover.smallbold {
+	font-family: $base_font;
+	font-size: $small_font;
+	font-weight: bold;
+	color: $hover_link_color;
+	background-color: $hover_link_bg_color;
+}
+a:il.hover {
+	background-color: transparent;
+}
+.alt1 {
+	background-color: $alt1_bgcolor;
+}
+.alt2 {
+	background-color: $alt2_bgcolor;
+}
+.title {
+	font-family: $base_font;
+	font-size: $large_font;
+	font-weight: bold;
+	color: $table_title_text_color;
+}
+.regular {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: normal;
+	color: $base_font_color;
+}
+.selectedNav {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: normal;
+	color: $hover_link_color;
+	background: $hover_link_bg_color;
+}
+.bold {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: bold;
+	color: $base_font_color;
+}
+.bold-rv {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: bold;
+	color: $table_title_text_color;
+	background-color: $hover_link_bg_color;
+}
+.smallbold {
+	font-family: $base_font;
+	font-size: $small_font;
+	font-weight: bold;
+	color: $base_font_color;
+}
+.smallregular {
+	font-family: $base_font;
+	font-size: $small_font;
+	font-weight: normal;
+	color: $base_font_color;
+}
+.error {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: normal;
+	color: $error_font_color;
+}
+.input {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: bold;
+	color: $hover_link_color;
+	background-color: $hover_link_bg_color;
+}
+.button {
+	font-family: $base_font;
+	font-size: $medium_font;
+	font-weight: bold;
+	color: $base_font_color;
+	background-color: $alt1_bgcolor;
+}
+.clearbgtext {
+	font-family: $base_font;
+	font-size: 13px;
+	font-weight: normal;
+	color: black;
+	background-color: transparent;
+}
+</style>
+EOF;
+
+?>
