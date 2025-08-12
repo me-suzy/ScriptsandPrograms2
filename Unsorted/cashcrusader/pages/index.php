cb080f387c992295c3f2f52fe789ed9c66e85348
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:03 +0300

    Add Unsorted/cashcrusader/pages/index.php

---
+++ Unsorted/cashcrusader/pages/index.php
@@ -0,0 +1,74 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<p><font face="arial"></font></p>
+<p>&nbsp;</p>
+<table width="100%" border="0" cellspacing="5" cellpadding="5" height="450" align="center">
+<tr> 
+<td width="50%" valign="top" align="right" height="412"> 
+<table width="255" border="0" cellspacing="1" cellpadding="5" height="320" bgcolor="#333333">
+<tr> 
+<td align="left" bgcolor="#FFFFFF" valign="top"> 
+<h2><font face="Arial, Helvetica, sans-serif">Members</font></h2>
+<font face="Arial, Helvetica, sans-serif"><font face="Arial, Helvetica, sans-serif" size="1">&#149; 
+</font><font size="2"> FREE Membership</font><br><font face="Arial, Helvetica, sans-serif" size="1">&#149; </font><font size="2"> 
+Get Paid for Reading Your eMail</font><br><font face="Arial, Helvetica, sans-serif" size="1">&#149; </font><font size="2"> 
+Refer Others and Get Paid</font><br>
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font><font size="2"> $?
+Dollar Payout</font><br>
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font><font size="2"> $?
+Sign Up Bonus</font><br>
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font><font size="2"> 
+International Members are Welcome!</font><br><font face="Arial, Helvetica, sans-serif" size="1">&#149; </font><font size="2"> 
+Receive eMail on the best&nbsp;products</font><br><font face="Arial, Helvetica, sans-serif" size="1">&#149; </font><font size="2"> 
+Receive eMail on the best opportunities</font><br><font face="Arial, Helvetica, sans-serif" size="1">&#149; </font><font size="2"> 
+Refer Others and Get Paid</font><br><font size="1">&#149; </font><font size="2">6 Ref levels of commission 
+under&nbsp;you</font><br></font> 
+<table width="219" border="0" cellpadding="2" cellspacing="2">
+<tr valign="top"> 
+<td width="98"><font face="Arial, Helvetica, sans-serif" size="2"> Level 1 - 20%<br>
+Level 2 - 15%<br>
+Level 3 - 10%&nbsp;<b> </b></font></td>
+<td width="107"><font face="Arial, Helvetica, sans-serif" size="2"> Level 4 -
+    5%<br>
+Level 5 - 3%<br>
+Level 6 - 1%<br>
+<b> </b></font></td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+<td width="50%" valign="bottom" align="left" height="412"> 
+<table width="255" border="0" cellspacing="1" cellpadding="5" height="320" bgcolor="#333333">
+<tr> 
+<td align="left" valign="top" bgcolor="#FFFFFF"> 
+<h2><font face="Arial, Helvetica, sans-serif">Advertisers </font></h2>
+
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font>
+<font size="2" face="Arial, Helvetica, sans-serif"> The most cost effective advertising</font><br>
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font>
+<font size="2" face="Arial, Helvetica, sans-serif">Guaranteed Clickthru Ad Options</font><br>
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font>
+<font size="2" face="Arial, Helvetica, sans-serif"> eMail Advertising or Banner Advertising</font><br>
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font>
+<font size="2" face="Arial, Helvetica, sans-serif"> Advertise to 100% opt in membership</font><br>
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font>
+<font size="2" face="Arial, Helvetica, sans-serif"> Members are&nbsp;eager to receive your ads.</font><br><font face="Arial, Helvetica, sans-serif" size="1">&#149; </font>
+<font size="2" face="Arial, Helvetica, sans-serif"> Advertise to 1000's or as little as 500</font><br>
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font>
+<font size="2" face="Arial, Helvetica, sans-serif"> We will customize your Ad Campaign</font><br>
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font>
+<font size="2" face="Arial, Helvetica, sans-serif"> Reliable, Friendly Service</font><br>
+<font face="Arial, Helvetica, sans-serif" size="1">&#149; </font>
+<font size="2" face="Arial, Helvetica, sans-serif"> Absolutely no Spam!</font><br>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<font face="arial"></font><br> 
+<p> 
+<? include("footer.php");?>
+</p>
