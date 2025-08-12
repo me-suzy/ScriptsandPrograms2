39087c8e98486a4fd2ba594f1d00c8ba65f6f375
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:29 +0300

    Add Content Management/metajour2_1/metajour/lang/meeting.da.php

---
+++ Content Management/metajour2_1/metajour/lang/meeting.da.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Mødereferater';
+$LANG['label_companyid'] = 'Virksomhed';
+$LANG['label_name'] = 'Navn';
+$LANG['label_visitdate'] = 'Besøgsdato';
+$LANG['label_content'] = 'Indhold';
+?>
