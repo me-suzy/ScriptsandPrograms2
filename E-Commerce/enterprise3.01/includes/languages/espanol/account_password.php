99f2043600d20c13dd0bbd59ebf9ac88697b428a
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:36 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/account_password.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/account_password.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Mi Cuenta');
+define('NAVBAR_TITLE_2', 'Modificar Contrase&ntilde;a');
+
+define('HEADING_TITLE', 'Mi Contrase&ntilde;a');
+
+define('MY_PASSWORD_TITLE', 'Mi Contrase&ntilde;a');
+
+define('SUCCESS_PASSWORD_UPDATED', 'Se ha actualizado su contrase&ntilde;a correctamente.');
+define('ERROR_CURRENT_PASSWORD_NOT_MATCHING', 'Su contrase&ntilde;a actual no coincide. Por favor int&eacute;ntelo de nuevo.');
+?>
