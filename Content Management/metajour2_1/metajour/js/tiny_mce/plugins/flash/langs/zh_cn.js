f7fc4ef59e168235777c145695cd5b4b04c0a2c4
me-suzy <me.suzana@gmail.com>
2025-08-12 06:36:40 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/flash/langs/zh_cn.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/flash/langs/zh_cn.js
@@ -0,0 +1,10 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµ&Auml;&Ouml;ÐÎ&Auml;&Ouml;§³&Ouml;
+
+tinyMCE.addToLang('',{
+insert_flash : '²&aring;Èë/±à¼­ FlashµçÓ°',
+insert_flash_file : 'FlashÎ&Auml;¼þ(.swf)',
+insert_flash_size : '³ß´ç',
+insert_flash_list : 'FlashÎ&Auml;¼þÁÐ±í',
+flash_props : 'FlashÊôÐÔ'
+});
