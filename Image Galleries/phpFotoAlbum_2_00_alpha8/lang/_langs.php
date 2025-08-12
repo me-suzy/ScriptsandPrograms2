e14aa40727efa80e630942ce9661ba3d27803e7b
me-suzy <me.suzana@gmail.com>
2025-08-12 18:01:48 +0300

    Add Image Galleries/phpFotoAlbum_2_00_alpha8/lang/_langs.php

---
+++ Image Galleries/phpFotoAlbum_2_00_alpha8/lang/_langs.php
@@ -0,0 +1,17 @@
+<?php
+/*
+*****************************************
+*          LANGUAGE DEFINITION          *
+*****************************************
+ $lang[$dir] = $name;
+   "$dir"
+    - Soubor,ve kterém je definice jazyka umístìna
+	- Language file name
+   "$name"
+    - Zobrazované jméno
+	- Language name
+******************************************
+*/
+$lang["en"]="English";
+$lang["cz"]="Czech";
+?>
