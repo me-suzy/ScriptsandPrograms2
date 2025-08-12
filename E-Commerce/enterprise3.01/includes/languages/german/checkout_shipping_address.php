7b528dd36957dc289bbba8e0bc7ccb14e2b7f004
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:54 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/checkout_shipping_address.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/checkout_shipping_address.php
@@ -0,0 +1,30 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Kasse');
+define('NAVBAR_TITLE_2', 'Versandadresse &auml;ndern');
+
+define('HEADING_TITLE', 'Informationen zur Versandadresse');
+
+define('TABLE_HEADING_SHIPPING_ADDRESS', 'Versandadresse');
+define('TEXT_SELECTED_SHIPPING_DESTINATION', 'Dies ist die aktuell ausgew&auml;hlte Versandadresse, an die Ihre Bestellung geliefert wird.');
+define('TITLE_SHIPPING_ADDRESS', 'Versandadresse:');
+
+define('TABLE_HEADING_ADDRESS_BOOK_ENTRIES', 'Adressbucheintr&auml;ge');
+define('TEXT_SELECT_OTHER_SHIPPING_DESTINATION', 'Bitte w&auml;hlen Sie die gew&uuml;nschte Versandadresse, an die wir die Auslieferung vornehmen sollen.');
+define('TITLE_PLEASE_SELECT', 'Bitte w&auml;hlen Sie');
+
+define('TABLE_HEADING_NEW_SHIPPING_ADDRESS', 'Neue Versandadresse');
+define('TEXT_CREATE_NEW_SHIPPING_ADDRESS', 'Bitte nutzen Sie dieses Formular, um eine neue Versandadresse f&uuml;r Ihre Bestellung zu erfassen.');
+
+define('TITLE_CONTINUE_CHECKOUT_PROCEDURE', 'Fortsetzung des Bestellvorganges');
+define('TEXT_CONTINUE_CHECKOUT_PROCEDURE', 'zur Auswahl der gew&uuml;nschten Versandart.');
+?>
