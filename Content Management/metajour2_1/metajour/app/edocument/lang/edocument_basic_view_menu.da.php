7b2947fbe44b4c0f95267f5ab20556aef4a86e95
me-suzy <me.suzana@gmail.com>
2025-08-12 05:59:36 +0300

    Add Content Management/metajour2_1/metajour/app/edocument/lang/edocument_basic_view_menu.da.php

---
+++ Content Management/metajour2_1/metajour/app/edocument/lang/edocument_basic_view_menu.da.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ * $Id: edocument_basic_view_menu.da.php,v 1.1 2005/02/16 05:03:36 jan Exp $
+ */
+
+$LANG['menu_edocnonapperror'] = 'Ikke godkendte afvigelser';
+$LANG['menu_edocapperror'] = 'Godkendte afvigelser';
+$LANG['menu_edocstat'] = 'Statistik på afvigelser';
+?>
