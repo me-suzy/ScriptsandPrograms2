61568cb0c167ce9b68abf96be2a3739de64b2cb9
me-suzy <me.suzana@gmail.com>
2025-08-12 14:20:42 +0300

    Add File Manipulation/download_engine_V1.4.1/download_engine_V1.4.1/download_engine/admin/includes/spaw/spaw_script.js.php

---
+++ File Manipulation/download_engine_V1.4.1/download_engine_V1.4.1/download_engine/admin/includes/spaw/spaw_script.js.php
@@ -0,0 +1,10 @@
+<?php
+   header('Content-Type: application/x-javascript');
+   $engine_path = "../../../"; 
+   include ($engine_path . "lib.inc.php"); // CONTENIDO
+   include $_ENGINE['eng_dir'].'admin/includes/spaw/config/spaw_control.config.php';
+
+   include $_ENGINE['eng_dir'].'admin/includes/spaw/class/script.js.php';
+
+?>
+
