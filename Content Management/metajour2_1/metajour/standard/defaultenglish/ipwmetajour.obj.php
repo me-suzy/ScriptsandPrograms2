a79edbe3696c6ceea2e5f8cc86727923cef3115e
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:12 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/ipwmetajour.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/ipwmetajour.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342676;
+$_objchilds[] = '_child_ipwmetajour20_342677';
+function objfile_342676 () {
+$obj = owNew('document');
+$objdata['name'] = "IPW METAjour";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
