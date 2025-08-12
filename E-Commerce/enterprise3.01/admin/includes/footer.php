8091249a9c9b2b3e80220db6e8eda2a307c88e37
me-suzy <me.suzana@gmail.com>
2025-08-12 11:38:14 +0300

    Add E-Commerce/enterprise3.01/admin/includes/footer.php

---
+++ E-Commerce/enterprise3.01/admin/includes/footer.php
@@ -0,0 +1,36 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+?>
+<br>
+<div align="center">
+<table border="1" width="98%" cellspacing="0" cellpadding="2" bgcolor="#00FFFF" bordercolorlight="#000000">
+  <tr>
+    <td align="center" class="smallText" bordercolor="#000000">
+<p><b><font size="4">Support The Continued Development of This Project! </font></b></p>
+<p><!--webbot bot="HTMLMarkup" startspan --><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but21.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBYEqSvXakwvyOU6bipBsuZJFMAp8/IlXwE4+8NaD6LEoxFfaN/3f+w8lYCKKnJ993/QYGSl5Auoaf41QDebJVyOvmUjy4ie71eQZBNvjMp41015J/UIiC58diS3OLWgoeLwSkvzsvHgXfYumImyV5QeGlR27FB4rdtnt03PkNN5zELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQI/AfloXtgm56AgZiFet6XiYGdj7kbf2QtXvFNiDJDwtIzzHm9Pp1dN1e6tE5uTwOrNyCNKG9JHlznQdkG1iF/9vB0pfl+CHqOdo3cCEljxomhrcNYFb7ESC78CyAPmrZpjLCC5XbmyYwxxXvP90VBznhj8gBCV70NS4ATUJjeOKeskcdXZ+o1yvbmXEl+nwt7rhfgJHlbDZb6YgK2XpdfMGAch6CCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTA0MTIzMDAyMjAyMlowIwYJKoZIhvcNAQkEMRYEFMU9a46nYldNyST7gO8ki/F41YDmMA0GCSqGSIb3DQEBAQUABIGAgy1Em9fSFvxTaWRDsGvytYOHBxE49/lpYx3RDlaQkKEsMDpEZCFhs5g/JoL0MS5xH1fzYqLJWHV+QVbvhIwrrWH2U9Vdwq8KpOs9d48HJ3Qhsnr+nFlvHlkE8iUKxLxv0VxEd+151pZ8FtDKZJSAQipOctdyjOTktI+Rgl6UU+U=-----END PKCS7-----
+">
+</form><!--webbot bot="HTMLMarkup" endspan --></p>
+<p align="left">Enterprise Cart v4.0 Professional is 
+currently in development. Enterprise Professional will not be an Open Source 
+Project and will have a retail price of $699 All users of the Enterprise Cart 
+v3.0 will receive the new Professional Version at no cost if they have 
+contributed to the development of Enterprise Cart v3.0 or the v4.0 Professional 
+release. Enterprise Cart v3.0 will 
+remain a Free cart solution. Check our website for updates on the release of 
+Enterprise Cart Professional v4.0 at <a href="http://www.enterprisecart.com">
+http://www.enterprisecart.com</a> </p>
+
+<?php
+
+?>
