a4ec7d3a0158e3893b9ffea7ad0eea7168be9b8a
me-suzy <me.suzana@gmail.com>
2025-08-12 21:55:51 +0300

    Add Polls and Voting/fungl/fungl/help/signup.php

---
+++ Polls and Voting/fungl/fungl/help/signup.php
@@ -0,0 +1,9 @@
+<div class="rightbox">
+	<h2>Help</h2>
+	<p><b>Username</b><br/>
+	The username must only contain a-z, 0-9, - and _ characters.
+	</p>
+	<p><b>Email</b><br/>
+	The email must be valid, we use the email to send you your account information and status messages about the system.
+	</p>
+</div>
