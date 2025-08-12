b7283861ecb7d89f11761572633cf87b3072c8c3
me-suzy <me.suzana@gmail.com>
2025-08-12 21:08:11 +0300

    Add News Publishing/zomplog34/zomplog34/language/dutch.php

---
+++ News Publishing/zomplog34/zomplog34/language/dutch.php
@@ -0,0 +1,182 @@
+<?
+
+// This is the Dutch language file
+
+//----------------- Dutch -----------------------//
+
+// Front end -> get.php
+$lang_number_comments = "opmerkingen"; //number of comments under posts
+$lang_no_results = "geen resultaten gevonden.";
+$lang_read_more = "lees meer...";
+$lang_listen = "luister naar mp3...";
+$lang_view = "bekijk film...";
+$lang_searched_for = "Je hebt gezocht naar:";
+$lang_posts_by = "Posts door";
+
+// Front end -> sidemenu.php
+$lang_search = "Zoek";
+$lang_pages = "Pagina's";
+$lang_recent_posts = "Recente Posts";
+$lang_categories = "Categori&euml;n";
+$lang_authors = "Auteurs";
+$lang_rss_feed = "Rss feed";
+$lang_admin = "Beheer";
+$lang_login = "Log in";
+$lang_register = "Registreer";
+$lang_powered = "Aangedreven door";
+$lang_archive = "Archief";
+
+// Front end -> detail.php
+$lang_write_comment = "Schrijf een opmerking";
+$lang_name = "naam";
+$lang_comment = "opmerking";
+$lang_comments = "Opmerkingen";
+$lang_no_comments_found = "geen opmerkingen gevonden";
+$lang_on = "op"; // as in "comment posted on date"
+$lang_error_name = "Je hebt je naam niet ingevuld";
+$lang_error_comment = "Je hebt geen opmerking ingevuld";
+$lang_error_comment_success = "Je opmerking is geplaatst.";
+
+// Front end -> page.php
+
+$lang_contact = "Contact";
+$lang_your_email_sent = "Je email is verstuurd!";
+$lang_name_mail = "Naam";
+$lang_email_mail = "Email";
+$lang_additional_remarks = "Opmerkingen";
+
+// Back end -> menu.php
+$lang_entries = "Artikels";
+$lang_newentry = "maak nieuw artikel aan";
+$lang_edit_delete = "wijzig / verwijder artikel";
+$lang_profile = "Jouw profiel";
+$lang_editprofile = "pas profiel aan";
+$lang_controlpanel = "Controlepaneel";
+$lang_settings = "systeem instellingen";
+$lang_moblog_settings = "moblog instellingen";
+$lang_content_management = "Contentbeheer";
+$lang_manage_users = "beheer gebruikers";
+$lang_manage_categories = "beheer categori&euml;n";
+$lang_manage_comments = "beheer opmerkingen";
+$lang_pages = "Pagina's";
+$lang_newpage = "nieuwe pagina";
+$lang_edit_delete_page = "wijzig / verwijder pagina";
+$lang_upload_images = "upload afbeeldingen";
+$lang_custom_upload_tool = "upload gereedschap";
+
+// Back end -> members.php
+$lang_total_number_posts = "Totaal aantal artikelen";
+$lang_total_number_pages = "Totaal aantal pagina's";
+$lang_total_number_users = "Totaal aantal auteurs";
+$lang_total_number_categories = "Totaal aantal categori&euml;n";
+$lang_total_number_comments = "Totaal aantal opmerkingen";
+$lang_lastpost = "Je laatste artikel was:";
+$lang_site_statistics = "Site Statistieken";
+$lang_your_statistics = "Jouw Statistieken";
+
+
+// Back end -> entries (newentry.php, editor.php, entry.php)
+
+$lang_title = "Titel";
+$lang_text = "Artikel";
+$lang_extended = "Uitgebreid artikel (optioneel)";
+$lang_upload_image = "Upload afbeelding";
+$lang_full_width = "Toon afbeelding(en) over volle breedte";
+$lang_category = "Categorie";
+$lang_choosemaincat = "Kies hoofdcategorie";
+$lang_replace_image = "Vervang afbeelding";
+$lang_media = "URL van video of mp3";
+$lang_choose_type = "Kies type";
+$lang_entries_by_others = "Artikels van anderen";
+$lang_your_entries = "Jouw artikels";
+$lang_order_by = "Orden op:";
+$lang_date = "datum";
+$lang_number_of_images = "Aantal afbeeldingen:";
+
+// Back end -> header.php
+
+$lang_members_area = "dashboard";
+$lang_view_site = "bekijk site";
+$lang_logout = "log uit";
+
+
+
+// Back end -> edit/delete
+$lang_edit = "bewerk";
+$lang_delete = "wis";
+
+// Back end -> User Management (profile.php, users.php)
+$lang_username = "gebruikersnaam";
+$lang_password = "wachtwoord";
+$lang_retype_password = "bevestig wachtwoord";
+$lang_email = "email";
+$lang_use_email_moblog = "gebruik dit email-adres voor moblogging";
+$lang_what_is_moblogging = "Wat is moblogging?";
+$lang_about_me = "over mij";
+$lang_administrator = "beheerder";
+$lang_user = "gebruiker";
+$lang_create_user = "maak een nieuwe gebruiker aan";
+$lang_current_users = "huidige gebruikers";
+
+// Back end -> Settings (settings.php)
+$lang_system_settings = "Systeem Instellingen";
+$lang_main_config = "Algemene Configuratie";
+$lang_weblog_title = "weblog titel";
+$lang_comments_on = "opmerkingen aan";
+$lang_categories_on = "categori&euml;n aan";
+$lang_choose_language = "kies taal";
+$lang_choose_skin = "kies skin";
+$lang_posts_per_page = "artikels per pagina";
+$lang_scroll = "scroll";
+$lang_scroll_explained = "(het aantal pagina's in het vorige/volgende gedeelte)";
+$lang_upload_on = "upload aan";
+$lang_max_upload = "maximum upload grootte";
+$lang_pages_admin = "wie kan pagina's aanmaken/wijzigen?";
+$lang_pages_admin_only = "alleen beheerder";
+$lang_date_time = "Datum/Tijd";
+$lang_date_time_format = "datum/tijd";
+$lang_mediafile_on = "films/mp3's aan";
+$lang_activate_site_parts = "Activeer onderdelen";
+$lang_admin_welcome_message = "Welkomsbericht voor dashboard";
+$lang_site_welcome_message = "Welkomsbericht voor website";
+$lang_messages = "Welkomsberichten";
+$lang_dashboard_rss_feed = "RSS feed URL voor dashboard";
+$lang_allow_register = "iedereen kan registreren";
+
+$lang_thumbnail_width = "thumbnail breedte";
+$lang_thumbnail_fullwidth = "maximum breedte";
+
+
+// Back end -> category.php
+$lang_new_category = "Maak nieuwe categorie aan";
+$lang_category_name = "categorie naam";
+$lang_edit_category = "Pas categorie aan";
+$lang_choose_category = "kies categorie";
+$lang_edit_category_name = "pas categorie aan";
+$lang_delete_category = "Wis categorie";
+
+// Back end -> comments_editor.php
+$lang_edit_comment = "Pas opmerking aan";
+$lang_comment_name = "naam";
+$lang_comment = "opmerking";
+$lang_ban = "ban";
+
+// Back end -> newpage.php
+$lang_add_contact_form = "contact-formulier toevoegen?";
+$lang_form_email = "email-adres voor formulier";
+
+//errors, well actually the messages if an action was succesful...
+$lang_error1 = "Artikel is succesvol toegevoegd!";
+$lang_error2 = "Artikel is succesvol aangepast!";
+$lang_error3 = "Informatie is succesvol verwijderd!";
+$lang_error4 = "Profiel is succesvol aangepast!";
+$lang_error5 = "Instellingen zijn succesvol gewijzigd!";
+$lang_error6 = "Categorie is succesvol toegevoegd!";
+$lang_error7 = "Categorie is succesvol aangepast!";
+$lang_error8 = "Opmerking is succesvol aangepast!";
+$lang_error9 = "Gebruiker is aangemaakt!";
+$lang_error10 = "Pagina is succesvol toegevoegd!";
+$lang_error11 = "Pagina is succesvol gewijzigd!";
+$lang_error12 = "Ip-adress has been succesfully banned!";
+$lang_error13 = "Ip-adress has been succesfully unbanned!";
+?>
