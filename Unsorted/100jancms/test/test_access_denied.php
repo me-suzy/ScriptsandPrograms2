17ffbdaa0188ab15b5c5c8e84ad099f331eb640c
me-suzy <me.suzana@gmail.com>
2025-08-13 00:10:06 +0300

    Add Unsorted/100jancms/test/test_access_denied.php

---
+++ Unsorted/100jancms/test/test_access_denied.php
@@ -0,0 +1,63 @@
+<?php 
+/***************************************\
+|					|
+|	    100janCMS v1.01		|
+|    ------------------------------	|
+|    Supplied & Nulled by GTT '2004	|
+|					|
+\***************************************/
+include "../100jancms/config_connection.php";
+?>
+<html>
+<head>
+<title>100janCMS Articles Control Test, powered by 100janCMS Articles Control</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link href="site_style.css" rel="stylesheet" type="text/css">
+<meta http-equiv="imagetoolbar" content="no">
+</head>
+
+<body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" class="maintext" scroll="auto">
+
+<center>
+  <table width="755" height="100%" border="0" cellpadding="0" cellspacing="0" background="images/bg_full.png" class="maintext">
+    <tr>
+      <td align="left" valign="top"> <table width="100%" height="80" border="0" cellspacing="5" cellpadding="0" class="maintext">
+          <tr> 
+            <td width="79%" > <span class="titletext0">100janCMS Articles Control 
+              Test</span><br>
+              <span class="titletext0blue"><strong>Access denied</strong></span><br> 
+              <span class="maintext">powered by 100janCMS 
+              Articles Control</span></td>
+            <td width="21%" align="right" valign="top" ><img src="images/logo_login.jpg" width="128" height="44" hspace="5" vspace="5" border="0"></td>
+          </tr>
+        </table>
+        <hr width="753" size="1" color="#DCDCDC">
+        <div align="left"><strong>&nbsp;&nbsp;<a class="fmenu" href="test_index.php">&nbsp;HOME&nbsp;</a> 
+          <a class="fmenu" href="test_register.php">&nbsp;REGISTER&nbsp;</a> <span class="maintext"><a class="fmenu" href="test_login.php">&nbsp;LOGIN&nbsp;</a> 
+          <a class="fmenu" href="test_logout.php">&nbsp;LOGOUT&nbsp;</a> </span></strong><span class="maintext"> 
+          <?php 
+if (isset($_COOKIE["website_member"])) {echo "You are logged in as <strong>".$_COOKIE["website_member"]."</strong>.";} else {echo "You are NOT logged in.";}
+
+?>
+          </span></div>
+        <hr width="753" size="1" color="#DCDCDC">
+        <br>
+        <br>
+        <br>
+        <!-- articles start -->
+        &nbsp;&nbsp;<span class="maintext">Access denied!</span> <img src="images/no_good.jpg" width="16" height="16" align="absmiddle"> 
+        <!-- articles end -->
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+		<br>
+		
+      </td>
+    </tr>
+  </table>
+</center>
+</body>
+</html>
