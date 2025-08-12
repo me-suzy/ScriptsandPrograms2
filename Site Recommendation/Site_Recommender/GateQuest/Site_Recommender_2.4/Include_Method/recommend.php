2d90eb408d0ca82ff72efe6f0d3eb701879f8ffe
me-suzy <me.suzana@gmail.com>
2025-08-12 23:52:33 +0300

    Add Site Recommendation/Site_Recommender/GateQuest/Site_Recommender_2.4/Include_Method/recommend.php

---
+++ Site Recommendation/Site_Recommender/GateQuest/Site_Recommender_2.4/Include_Method/recommend.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+	<title>GateQuest php Site Recommender - Include Method</title>
+	<link rel="stylesheet" href="recommend.css" type="text/css">
+</head>
+
+<body>
+
+<table width="100%" height="100%">
+	<tr>
+		<td height="65" style="padding: 10px 0px 0px 10px"><a href="javascript:history.go(-1);"><img alt="" border="0" width="100" height="65" src="/art/gq_logo_back.gif"></a></td>
+	</tr>
+	<tr>
+		<td align="center" valign="middle">
+			<?php include("inc.recommend.php"); ?>
+		</td>
+	</tr>
+</table>
+
+</body>
+</html>
