5af7b099326fa575cee3a85d1caa743efb056662
me-suzy <me.suzana@gmail.com>
2025-08-13 01:26:44 +0300

    Add Unsorted/cashcrusader/pages/confirm_info_sent.php

---
+++ Unsorted/cashcrusader/pages/confirm_info_sent.php
@@ -0,0 +1,13 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<center>
+<font size="4"><br>
+<br>
+An email containing a special signup URL,<br>
+has been sent to your email address. <br>
+You should receive it shortly.<br>
+<br>
+<br>
+</font> 
+</center>
+<? include("footer.php");?>
