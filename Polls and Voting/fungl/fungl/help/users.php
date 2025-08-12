536dc2f957c286f24949df412b05a6ac2877caac
me-suzy <me.suzana@gmail.com>
2025-08-12 21:55:53 +0300

    Add Polls and Voting/fungl/fungl/help/users.php

---
+++ Polls and Voting/fungl/fungl/help/users.php
@@ -0,0 +1,7 @@
+<div class="rightbox">
+	<h2>Help</h2>
+	<p>You can't edit or delete any user with higher level that you self.</p>
+	<p>When any user is changed the change will be mailed to the user.
+	IE. if the you change the email and password of a user the new password will be 
+	mailed to the user on the mail account present.</p>
+</div>
