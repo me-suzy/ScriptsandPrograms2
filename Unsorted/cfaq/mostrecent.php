a21d5f695337f668525069842263db9b18ba981f
me-suzy <me.suzana@gmail.com>
2025-08-13 01:35:46 +0300

    Add Unsorted/cfaq/mostrecent.php

---
+++ Unsorted/cfaq/mostrecent.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+	CascadianFAQ v4.1 - Last Updated: November 2003
+	Summer S. Wilson, Eclectic Designs, http://eclectic-designs.com
+	cfaq@eclectic-designs.com
+*/
+
+// Include configuration and database files
+require_once 'config.php';
+require_once 'functions.php';
+
+getmostrecent();
+
+?>
