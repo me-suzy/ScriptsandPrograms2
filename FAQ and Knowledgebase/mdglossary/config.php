951ebde4a30167c6714129437e8daebf89728f5d
me-suzy <me.suzana@gmail.com>
2025-08-12 14:10:50 +0300

    Add FAQ and Knowledgebase/mdglossary/config.php

---
+++ FAQ and Knowledgebase/mdglossary/config.php
@@ -0,0 +1,37 @@
+<?php
+/*
+-------------------------------------------------------------
+|MD Glossary                                                |
+|Version 1.0.0                                              |
+|This program is Copyright (c) Matthew Dingley 2003         |
+|For more help or assistance go to MD Web at:               |
+|www.matthewdingley.co.uk                                   |
+|For information on how to install or for basic licence     |
+|information, view the readme                               |
+|                                                           |
+|This program is not to be used on a commercial site without|
+|a commercial licence. Go to www.matthewdingley.co.uk for   |
+|more information. This program is not to be used without   |
+|the copyright notice intact                                |
+-------------------------------------------------------------
+*/
+//Configuration file
+
+//Username - The username of the database you are going to use
+$username="username";
+
+//Password - The password of the database you are going to use
+$password="password";
+
+//Host - The host that the database is stored on (usually 'localhost')
+$host="localhost";
+
+//Database name - The name of the database that your administrator has set up for you
+$databasename="databasename";
+
+//If you don't know any of the above contact your system administrator
+
+//Root to glossary - The complete (absolute) address to the glossary.
+$roottoglossary="www.yoursite.com/glossary/glossary.php";
+
+?>
