20972a1ff79959fa94bec5b1404ba1ad71b3d8ce
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:09 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_access.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_access.da.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Rettigheder';
+$LANG['text_1'] = 'Vælg website rettigheder for de valgte poster:';
+$LANG['text_2'] = 'Vælg system rettigheder for de valgte poster:';
+$LANG['text_3'] = 'Hold Ctrl nede for at vælge mere end 1 bruger/gruppe';
+?>
