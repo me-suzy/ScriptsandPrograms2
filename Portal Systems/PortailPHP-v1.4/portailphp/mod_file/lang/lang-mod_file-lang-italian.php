e11d21df3aff889377e82dd163f999911a0766aa
me-suzy <me.suzana@gmail.com>
2025-08-12 23:16:46 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_file/lang/lang-mod_file-lang-italian.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_file/lang/lang-mod_file-lang-italian.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE FILE
+$Mod_File_Taille="2000000";// Taille maximum des fichiers en Upload
+$Mod_File_Etiquette="Lima da trasferire";
+$Mod_File_Etiquette2="Categoria";
+$Mod_File_Etiquette3="Titolo";
+$Mod_File_Etiquette4="Nome";
+$Mod_File_Etiquette5="Email";
+$Mod_File_inde_Date="Data";
+$Mod_File_inde_Cat="Categoria";
+$Mod_File_inde_Titre="Nome";
+$Mod_File_inde_Fichiers="Lima";
+$Mod_File_inde_Clics="Scatti";
+$Mod_File_PostPar="Trasferito da";
+$Mod_File_Le="su";
+$Mod_File_Telecharger="Download";
+$Mod_File_PlRecent="10 download più recenti";
+$Mod_File_PlLu="10 download più scaricati";
+$Mod_File_JS_Nom="Il campo Nome deve essere riempito";
+$Mod_File_JS_Email="Il campo Mail deve essere riempito";
+$Mod_File_JS_Monfichier="Il campo Lima deve essere riempito";
+$Mod_File_JS_Titre="Il campo Titolo deve essere riempito";
+$Mod_File_Upload_OK="La vostra lima è stata trasferita correttamente";
+
+?>
