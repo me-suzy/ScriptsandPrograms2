9a14e36e1601dbeed9bec071053a6aed662f7556
me-suzy <me.suzana@gmail.com>
2025-08-13 00:31:52 +0300

    Add Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/style/xpgray1024/template/vmess.php

---
+++ Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/style/xpgray1024/template/vmess.php
@@ -0,0 +1,33 @@
+<?php
+$top=<<<MES
+<table width=975 border=0 cellspacing=0 cellpadding=0 align=center class=bgcol>
+
+<tr height=29>
+<td width=7><img width=7 height=29 border=0 src="%%RF%%style/%%STYLE%%/image/ttopl.gif"></td>
+<td width=961 background="%%RF%%style/%%STYLE%%/image/ttop.gif"><div class=tframe>%%HEADER%%</div></td>
+<td width=7><img width=7 height=29 border=0 src="%%RF%%style/%%STYLE%%/image/ttopr.gif"></td>
+</tr>
+
+<tr height=20 valign=middle>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenl.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+<td width=961 align=center>
+<div class=tareawsubt>
+<div class=tborder>
+<div class=vstable>%%MESSAGE%%</div>
+</div>
+</div>
+</td>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenr.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+</tr>
+
+<tr height=3>
+<td width=7><img width=7 height=3 border=0 src="%%RF%%style/%%STYLE%%/image/tbotl.gif"></td>
+<td height=3 width=961><img src="%%RF%%style/%%STYLE%%/image/tbot.gif" height=3 width=961></td>
+<td width=7><img width=7 height=3 border=0 src="%%RF%%style/%%STYLE%%/image/tbotr.gif"></td>
+</tr>
+
+</table>
+<br>
+
+MES;
+?>
