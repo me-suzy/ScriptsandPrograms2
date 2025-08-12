7ab2fb3b5bab5500ae9e0761da3ef8ce63866acb
me-suzy <me.suzana@gmail.com>
2025-08-12 03:33:39 +0300

    Add Content Management/free-content-management/UploadFilesHtml.php

---
+++ Content Management/free-content-management/UploadFilesHtml.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+include("Includes/PortalConection.php");
+include("Includes/Database.php");
+$strRootpath= "";
+include_once ("Includes/validsession.php");
+print "<HTML><HEAD><TITLE>Upload Files</TITLE>";
+include ("Includes/Styles.php");
+?>
+</HEAD>
+<BODY>
+<h3><img src="Includes/html.gif" width="36" height="36" alt="Upload HTML Files"> <img src="Includes/upload.gif" alt="upload" width="23" height="26" border="0">Upload HTML Files </h3>
+<form method=post
+      enctype="multipart/form-data"
+      action="">
+Your File:<BR><input type=file name=YourFile size=50><BR><BR>
+<input  class=button onmouseover="this.className='buttonover'" onmouseout="this.className='button'"  type=submit name=submit value="Upload">
+
+</form>
+<?php
+// In PHP earlier then 4.1.0, $HTTP_POST_FILES should be used instead of $_FILES. 
+if(!empty($_FILES["YourFile"])) { 
+    $uploaddir = FileUploadPathRelative;//FileUploadPath; // set this to wherever 
+    //copy the file to some permanent location 
+    if (move_uploaded_file($_FILES["YourFile"]["tmp_name"], $uploaddir . $_FILES["YourFile"]["name"])) 
+    {
+        echo("file uploaded");
+    } else 
+    {
+        echo ("error!");
+    }
+} 
+print "</BODY></HTML>";
+?>
+
+
