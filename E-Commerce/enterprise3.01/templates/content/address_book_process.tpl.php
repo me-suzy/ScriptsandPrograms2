53a8fa7af44855c4b5e7876292200b483d307d90
me-suzy <me.suzana@gmail.com>
2025-08-12 12:05:35 +0300

    Add E-Commerce/enterprise3.01/templates/content/address_book_process.tpl.php

---
+++ E-Commerce/enterprise3.01/templates/content/address_book_process.tpl.php
@@ -0,0 +1,121 @@
+    <?php if (!isset($HTTP_GET_VARS['delete'])) echo escs_draw_form('addressbook', escs_href_link(FILENAME_ADDRESS_BOOK_PROCESS, (isset($HTTP_GET_VARS['edit']) ? 'edit=' . $HTTP_GET_VARS['edit'] : ''), 'SSL'), 'post', 'onSubmit="return check_form(addressbook);"'); ?><table border="0" width="100%" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="pageHeading"><?php if (isset($HTTP_GET_VARS['edit'])) { echo HEADING_TITLE_MODIFY_ENTRY; } elseif (isset($HTTP_GET_VARS['delete'])) { echo HEADING_TITLE_DELETE_ENTRY; } else { echo HEADING_TITLE_ADD_ENTRY; } ?></td>
+            <td class="pageHeading" align="right"></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo escs_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+<?php
+  if ($messageStack->size('addressbook') > 0) {
+?>
+      <tr>
+        <td><?php echo $messageStack->output('addressbook'); ?></td>
+      </tr>
+      <tr>
+        <td><?php echo escs_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+<?php
+  }
+
+  if (isset($HTTP_GET_VARS['delete'])) {
+?>
+      <tr>
+        <td class="main"><b><?php echo DELETE_ADDRESS_TITLE; ?></b></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2" class="infoBox">
+          <tr class="infoBoxContents">
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td><?php echo escs_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td class="main" width="50%" valign="top"><?php echo DELETE_ADDRESS_DESCRIPTION; ?></td>
+                <td align="right" width="50%" valign="top"><table border="0" cellspacing="0" cellpadding="2">
+                  <tr>
+                    <td class="main" align="center" valign="top"><b><?php echo SELECTED_ADDRESS; ?></b><br><?php echo escs_image(DIR_WS_IMAGES . 'arrow_south_east.gif'); ?></td>
+                    <td><?php echo escs_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                    <td class="main" valign="top"><?php echo escs_address_label($customer_id, $HTTP_GET_VARS['delete'], true, ' ', '<br>'); ?></td>
+                    <td><?php echo escs_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                  </tr>
+                </table></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><?php echo escs_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2">
+          <tr>
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td width="10"><?php echo escs_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td><?php echo '<a href="' . escs_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL') . '">' . escs_image_button('button_back.gif', IMAGE_BUTTON_BACK) . '</a>'; ?></td>
+                <td align="right"><?php echo '<a href="' . escs_href_link(FILENAME_ADDRESS_BOOK_PROCESS, 'delete=' . $HTTP_GET_VARS['delete'] . '&action=deleteconfirm', 'SSL') . '">' . escs_image_button('button_delete.gif', IMAGE_BUTTON_DELETE) . '</a>'; ?></td>
+                <td width="10"><?php echo escs_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+<?php
+  } else {
+?>
+      <tr>
+        <td><?php include(DIR_WS_MODULES . 'address_book_details.php'); ?></td>
+      </tr>
+      <tr>
+        <td><?php echo escs_draw_separator('pixel_trans.gif', '100%', '10'); ?></td>
+      </tr>
+<?php
+    if (isset($HTTP_GET_VARS['edit']) && is_numeric($HTTP_GET_VARS['edit'])) {
+?>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2">
+          <tr>
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td width="10"><?php echo escs_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td><?php echo '<a href="' . escs_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL') . '">' . escs_image_button('button_back.gif', IMAGE_BUTTON_BACK) . '</a>'; ?></td>
+                <td align="right"><?php echo escs_draw_hidden_field('action', 'update') . escs_draw_hidden_field('edit', $HTTP_GET_VARS['edit']) . escs_image_submit('button_update.gif', IMAGE_BUTTON_UPDATE); ?></td>
+                <td width="10"><?php echo escs_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+<?php
+    } else {
+      if (sizeof($navigation->snapshot) > 0) {
+        $back_link = escs_href_link($navigation->snapshot['page'], escs_array_to_string($navigation->snapshot['get'], array(escs_session_name())), $navigation->snapshot['mode']);
+      } else {
+        $back_link = escs_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL');
+      }
+?>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="1" cellpadding="2">
+          <tr>
+            <td><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr>
+                <td width="10"><?php echo escs_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+                <td><?php echo '<a href="' . $back_link . '">' . escs_image_button('button_back.gif', IMAGE_BUTTON_BACK) . '</a>'; ?></td>
+                <td align="right"><?php echo escs_draw_hidden_field('action', 'process') . escs_image_submit('button_continue.gif', IMAGE_BUTTON_CONTINUE); ?></td>
+                <td width="10"><?php echo escs_draw_separator('pixel_trans.gif', '10', '1'); ?></td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+
+<?php
+    }
+  }
+?>
+    </table><?php if (!isset($HTTP_GET_VARS['delete'])) echo '</form>'; ?>
+
