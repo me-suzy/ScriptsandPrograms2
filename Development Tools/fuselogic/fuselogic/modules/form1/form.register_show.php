58fb3e12039e3360d8e7be673b41cf5d1821b1f7
me-suzy <me.suzana@gmail.com>
2025-08-12 09:23:22 +0300

    Add Development Tools/fuselogic/fuselogic/modules/form1/form.register_show.php

---
+++ Development Tools/fuselogic/fuselogic/modules/form1/form.register_show.php
@@ -0,0 +1,10 @@
+<div align="center">
+<h2>Create New Account</h2>
+<form action="<?php echo $XFA['action']; ?>" method="post">
+<p>Username: <input type="text" name="user"></p>
+<p>Password: <input type="password" name="pass"></p>
+<p>Confirm: <input type="password" name="conf"></p>
+<p>Email: <input type="text" name="email"></p>
+<p><input type="submit" name="register" value=" Register "></p>
+</form>
+</div>
