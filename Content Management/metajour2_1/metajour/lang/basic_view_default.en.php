6afac01b1e013c619be7d67976c534777e8b244b
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:01 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_default.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_default.en.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Set as default';
+$LANG['text_1'] = 'Are you sure that you want to set the selected record as default?';
+?>
