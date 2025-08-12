e179cf72851c83f08f3b1b1436df642c84f5156a
me-suzy <me.suzana@gmail.com>
2025-08-12 11:53:36 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/product_info.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/product_info.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('TEXT_PRODUCT_NOT_FOUND', 'Product not found!');
+define('TEXT_CURRENT_REVIEWS', 'Current Reviews:');
+define('TEXT_MORE_INFORMATION', 'For more information, please visit this products <a href="%s" target="_blank"><u>webpage</u></a>.');
+define('TEXT_DATE_ADDED', 'This product was added to our catalog on %s.');
+define('TEXT_DATE_AVAILABLE', '<font color="#ff0000">This product will be in stock on %s.</font>');
+define('TEXT_ALSO_PURCHASED_PRODUCTS', 'Customers who bought this product also purchased');
+define('TEXT_PRODUCT_OPTIONS', 'Available Options:');
+define('TEXT_CLICK_TO_ENLARGE', 'Click to enlarge');
+define('TEXT_XSELL_PRODUCTS', 'We Also Recommend :');
+define('TEXT_NO_MOPICS', 'Coming soon!');
+?>
