17b8310876a674ec2b42e56218cc2df25aa9ee34
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:30 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_publish.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_publish.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Udgivelse';
+$LANG['text_1'] = 'Udgivelsestidspunkt:';
+$LANG['text_2'] = 'Udløbstidspunkt:';
+?>
