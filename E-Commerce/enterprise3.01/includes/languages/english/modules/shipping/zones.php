d3e0dd9130a1078cc39fd5ce97a38c39ec952c01
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:19 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/modules/shipping/zones.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/modules/shipping/zones.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_ZONES_TEXT_TITLE', 'Zone Rates');
+define('MODULE_SHIPPING_ZONES_TEXT_DESCRIPTION', 'Zone Based Rates');
+define('MODULE_SHIPPING_ZONES_TEXT_WAY', 'Shipping to');
+define('MODULE_SHIPPING_ZONES_TEXT_UNITS', 'lb(s)');
+define('MODULE_SHIPPING_ZONES_INVALID_ZONE', 'No shipping available to the selected country');
+define('MODULE_SHIPPING_ZONES_UNDEFINED_RATE', 'The shipping rate cannot be determined at this time');
+?>
