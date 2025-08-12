f90c7f3e13c83ec0ba8e70cffcead4ccddf3ed5a
me-suzy <me.suzana@gmail.com>
2025-08-13 02:22:05 +0300

    Add Unsorted/datingscript/languages/tr/tra.php

---
+++ Unsorted/datingscript/languages/tr/tra.php
@@ -0,0 +1,122 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.1.1                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 04/05/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               tra.php                         #
+# File purpose            Additional langfile for admin    #
+# File created by         egerci <egerci@altikom.net>      #
+############################################################
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Yanlýþ Kullanýcý veya Þifre - Tekrar Deneyiniz!',//1
+'Yönetici Alaný',//2
+'Yönetici Giriþ',//3
+'Yönetici Þifre',//4
+'Giriþ',//5
+'Yönetici olmayanlar için bu sayfalar kullanýlamaz',//6
+'Ana Sayfa',//7
+'Bilgileri Temizle',//8
+'Tamir&Optimize Et',//9
+'Kullanýcý Yönetimi',//10
+'SSS Yönetimi',//11
+'E-Posta Listesi',//12
+'Çýkýþ',//13
+'Yönetim alanýndan baþarý ile çýktýnýz<br><br>Programýmýzý kullandýðýný için teþekkür ederiz.',//14
+'Toplam E-Posta',//15
+'Gönderilen E-Posta',//16
+'Sonraki Bölüm',//17
+'{0} E-Posta kullanýcýlara gödnerildi',//18
+'Okunmamýþ e-posta {0}',//19 {0} - SiteName (C_SNAME)
+'{0} üye bulunudu',//20
+'E-posta Liste Yönetimi',//21
+'Durumlarýna göre:',//22
+'E-Posta Alan ve özellikleri:',//23
+'E-Posta Cinsi',//24
+'Gönderen',//25
+'Konu',//26
+'Mesaj',//27
+'Sonuçlarý onayla',//28
+'Geri Dön',//29
+'E-Posta Gönder',//30
+'Bitilmemiþ - {0}',//31
+'Konu : {0} [{1} kullanýcýya gönderildi] :::>',//32
+'Bitir',//33
+'Teks',//34
+'HTML',//35
+'Ön Ýzleme',//36
+'Okunmamýþ mesajlar ve kullanýcýlar- ',//37
+'Toplam okunmamýþ mesajlar - ',//38
+'Son Mesaj No - ',//39
+'Okunmamýþ mesajlar ile kullanýcýya onay mektubu gönder',//40
+'Profil baþarý ile güncellendi',//41
+'SSS sorusu baþarý ile oluþturuldu',//42
+'Yeni Oluþtur',//43
+'Lütfen soruyu giriniz',//44
+'Lütfen cevabý giriniz',//45
+'SSS Yönetimi',//46
+'Yeni SSS Kategori Oluþtur',//47
+'SSS Kategori Düzenle',//48
+'SSS Kategori Sil',//49
+'SSS Oluþtur',//50
+'SSS Kategori Seçin',//51
+'SSS Soru',//52
+'SSS Cevap',//53
+'Öncelikle SSS kategorisi oluþturmalýsýnýz',//54
+'Lütfen kategori ismini giriniz',//55
+'Yeni Kategori Oluþtur',//56
+'Yeni Kategori Eklendi:',//57
+'SSS Kategori Ýsmi',//58
+'SSS Kategori Düzenle',//59
+'Düzenle',//60
+'Kategori deðiþtirildi',//61
+'SSS Kategori Düzenle',//62
+'SSS Silindi',//63
+'SSS Kategori Sil',//64
+'Not : Dikatli olun. Kategori silerek , bu kategorideki bütün sorularý silmiþ olursunuz.',//65
+'Sil',//66
+'0 - E-POsta kontrolünü bekle<br>1 - Kayýt için kontrol bekle<br>2 - GÜncelleme için kontrol bekle<br>3,8 - Silme için kontrol bekle(Göstererek/meyerek)<br>7 - Kullanýcý Aktif<br>',//67
+'Veritabaný tamir ve optimize edildi',//68
+'Kullanýcý Kodu#={0} olan kullanýcý baþarý ile silindi',//69
+'Bilgiler Silindi',//70
+'Baþarý ile onaylanan Kullanýcý Kodu={0}',//71
+'IP',//72
+'Gereken [0 - 255]',//73
+'Durum - (DÝKKATLÝ OLUN)',//74
+'Onayla',//75
+'Kullanýcý Yönetimi',//76
+'Toplam Aktif Kullanýcý :',//77
+'Onay bekleyen yeni kayýtlý kullanýcý',//78
+'Profil güncelleyen ve onay bekleyen kullanýcý',//79
+'Silinmeyi bekleyen kullanýcý',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
