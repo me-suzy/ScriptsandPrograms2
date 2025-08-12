e77ec12ad539c63cbba362a6cf22b8ff5efebfe2
me-suzy <me.suzana@gmail.com>
2025-08-12 16:32:27 +0300

    Add Games and Entertainment/realms_v0.0.2/realms v0.0.1/popinv.001.php

---
+++ Games and Entertainment/realms_v0.0.2/realms v0.0.1/popinv.001.php
@@ -0,0 +1,8 @@
+<?php
+
+print"<ul>";
+print"<li><a href=$PHP_SELF?p=items>Your Items</a>";
+print"<li><a href=$PHP_SELF?p=usershop>Your Shop</a>";
+print"<li><a href=$PHP_SELF?p=shops>Shops</a>";
+print"<li><a href=$PHP_SELF?p=bank>Bank</a>";
+print"</ul>";
