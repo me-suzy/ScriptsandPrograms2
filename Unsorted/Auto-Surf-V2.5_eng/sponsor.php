54edee0e8eed1101668609088541fd065608a98d
me-suzy <me.suzana@gmail.com>
2025-08-13 00:53:20 +0300

    Add Unsorted/Auto-Surf-V2.5_eng/sponsor.php

---
+++ Unsorted/Auto-Surf-V2.5_eng/sponsor.php
@@ -0,0 +1,55 @@
+<?php
+require('./prepend.inc.php');
+?>
+
+<?
+include("./templates/main-header.txt");
+?>
+
+
+<br><font size="3"><center><b><u>Visits on your website</u></font></b></center><br></div><p align="center"><font face="Verdana, Arial, Helvetica, sans-serif" size="2">All visitors are at least <? echo "$showup_time"; ?> seconds at your website</font></p>
+<table width="303" border="0" align="center">
+<tr>
+  <td width="147"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">500 visits</font></td>
+  <td width="146"><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><? echo "$besa"; ?> &euro;</font></td>
+</tr>
+<tr>
+  <td width="147"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">1.000 visits</font></td>
+  <td width="146"><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><? echo "$besb"; ?> &euro;</font></td>
+</tr>
+<tr>
+  <td width="147"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">5.000 visits</font></td>
+  <td width="146"><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><? echo "$besc"; ?> &euro;</font></td>
+</tr>
+<tr>
+  <td width="147"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">10.000 visits</font></td>
+  <td width="146"><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><? echo "$besd"; ?> &euro;</font></td>
+</tr>
+<tr>
+  <td width="147"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">50.000 visits</font></td>
+  <td width="146"><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><? echo "$bese"; ?> &euro;</font></td>
+</tr>
+<tr>
+  <td width="147"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">100.000 visits</font></td>
+  <td width="146"><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><? echo "$besf"; ?> &euro;</font></td>
+</tr>
+</table>
+<p align="center"><b><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><u>Bannerviews in the surfbar<u></font></b></p><div align="center"></div>
+<table width="303" border="0" align="center">
+<tr>
+   <td width="221"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">10.000 bannerviews</font></td>
+   <td width="72"><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><? echo "$baa"; ?> &euro;</font></td>
+</tr>
+<tr>
+  <td width="221"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">50.000 bannerviews</font></td>
+  <td width="72"><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><? echo "$bab"; ?> &euro;</font></td>
+</tr>
+<tr>
+  <td width="221"><font size="2" face="Verdana, Arial, Helvetica, sans-serif">100.000 bannerviews </font></td>
+  <td width="72"><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><? echo "$bac"; ?> &euro;</font></td>
+</tr>
+</table>
+
+<?
+include("./templates/main-footer.txt");
+?>
