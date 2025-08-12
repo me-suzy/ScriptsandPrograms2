2fb0a8ec0ddae1080a952374b963252f2adb1cc1
me-suzy <me.suzana@gmail.com>
2025-08-12 11:44:30 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/espanol/modules.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/espanol/modules.php
@@ -0,0 +1,21 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE_MODULES_PAYMENT', 'M&oacute;dulos de Pago');
+define('HEADING_TITLE_MODULES_SHIPPING', 'M&oacute;dulos de Envio');
+define('HEADING_TITLE_MODULES_ORDER_TOTAL', 'M&oacute;dules de Totalizaci&oacute;n');
+
+define('TABLE_HEADING_MODULES', 'M&oacute;dulos');
+define('TABLE_HEADING_SORT_ORDER', 'Orden');
+define('TABLE_HEADING_ACTION', 'Acci&oacute;n');
+
+define('TEXT_MODULE_DIRECTORY', 'Directorio de m&oacute;dulos:');
+?>
