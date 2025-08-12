4c6b4f8106ba485ff9cdd40dc5f166346a050d82
me-suzy <me.suzana@gmail.com>
2025-08-12 14:32:10 +0300

    Add File Manipulation/free_download_site_v1.0/Free download site/index.php

---
+++ File Manipulation/free_download_site_v1.0/Free download site/index.php
@@ -0,0 +1,32 @@
+<?php
+/*
+    Copyright (C) 2005 Adullam Limited., All Rights Reserved.
+
+    Unless explicitly acquired and licensed from Licensor under the Technical Pursuit License ("TPL") 
+    Version 1.0 or greater, the contents of this file are subject to the Reciprocal Public License 
+    ("RPL") Version 1.1, or subsequent versions as allowed by the RPL, and You may not copy or use 
+    this file in either source code or executable form, except in compliance with the terms and 
+    conditions of the RPL.
+
+    You may obtain a copy of both the TPL and the RPL (the "Licenses") from Technical Pursuit Inc. 
+    at http://www.technicalpursuit.com.
+
+    All software distributed under the Licenses is provided strictly on an "AS IS" basis, WITHOUT 
+    WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, AND Adullam Limited HEREBY DISCLAIMS ALL 
+    SUCH WARRANTIES, INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+    PARTICULAR PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the Licenses for specific 
+    language governing rights and limitations under the Licenses. 
+    
+    Project Name: Download Site System
+    File: index.php (homepage version)
+    File-Version: 1.0
+    Details:this is your homepage - customise it to make it look good...
+*/
+include("top.inc.php");
+?>
+<h1>Welcome to your own Free Download Site</h1>
+<h3>You have successfully installed at least the main page and now you should customise it.</h3>
+<p>Your downloads are all here: <a href="./my_downloads/">My Downloads</a></p>
+<?php
+include("bottom.inc.php");
+?>
