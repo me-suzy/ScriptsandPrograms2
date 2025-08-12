ab531b5559e8e4f9ac1cfb4e9b0760a016df929d
me-suzy <me.suzana@gmail.com>
2025-08-12 11:46:29 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/german/stats_customers.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/german/stats_customers.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Kunden mit den h&ouml;chsten Ums&auml;tzen');
+
+define('TABLE_HEADING_NUMBER', 'Nr.');
+define('TABLE_HEADING_CUSTOMERS', 'Kunde');
+define('TABLE_HEADING_TOTAL_PURCHASED', 'Gesamtsumme');
+?>
