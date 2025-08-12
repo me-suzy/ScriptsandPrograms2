26e9d2a48d1a23a3970c1987f652e9599e72fe25
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:34 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/lang_links_ru.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/links/lang_links_ru.php
@@ -0,0 +1,38 @@
+<?php
+
+// Form Titles
+$GLOBALS["tFormTitle"]		= 'áë«ª¨';
+$GLOBALS["tSubmitFormTitle"]	= 'â¯à ¢¨âì ááë«ªã';
+
+// List Headings
+$GLOBALS["tPostedBy"]		= 'â¯à ¢«¥­®';
+$GLOBALS["tPublishDate"]	= ' â ';
+$GLOBALS["tStatus"]		= 'â âãá';
+
+// List Functions
+$GLOBALS["tAddNewLink"]		= '®¡ ¢¨âì ááë«ªã';
+$GLOBALS["tEditLink"]		= 'à ¢¨âì ááë«ªã';
+$GLOBALS["tDeleteLink"]		= '¤ «¨âì ááë«ªã';
+$GLOBALS["tReleaseLink"]	= '®¤â¢¥à¤¨âì ááë«ªã';
+$GLOBALS["tEditExtraSettings"] 	= "à ¢¨âì ¤®¯®«­¨â¥«ì­ë¥ á¢®©áâ¢  ááë«ª¨";
+
+// Form Field Headings
+$GLOBALS["tCategory"] 		= ' â¥£®à¨ï';
+$GLOBALS["tWebURL"]		= 'Web ááë«ª ';
+$GLOBALS["tDescription"]	= '¯¨á ­¨¥';
+$GLOBALS["tSubmitLink"]		= 'â¯à ¢¨âì ááë«ªã';
+
+// Link Submission Texts
+$GLOBALS["tThankYou"]		= '¯ á¨¡® §  ®â¯à ¢ªã ááë«ª¨.';
+$GLOBALS["tReleaseRequired"] 	= '¡à â¨â¥ ¢­¨¬ ­¨¥, çâ® ááë«ª  ­¥ ¡ã¤¥â ¯®ª §ë¢ âìáï, ¯®ª  ­¥ ¡ã¤¥â ¯®¤â¢¥à¦¤¥­   ¤¬¨­¨áâà â®à®¬.';
+$GLOBALS["tClickToReturn"] 	= '¥«ª­¨â¥ §¤¥áì, çâ®¡ë ¢¥à­ãâìáï ­  ';
+$GLOBALS["tGalleryPage"] 	= 'áâà ­¨æã ááë«®ª';
+
+// Security Texts
+$GLOBALS["tMustLogin"] 		= 'ë ¤®«¦­ë';
+$GLOBALS["tLogin"] 		= '¢®©â¨ ¢ á¨áâ¥¬ã';
+$GLOBALS["tToSubmit"] 		= '¤«ï ®â¯à ¢ª¨ ááë«®ª';
+
+//Validation
+$GLOBALS["eWebURLEmpty"]	= "®«¥ ááë«ª¨ ­¥ ¤®«¦­® ¡ëâì ¯ãáâë¬.";
+?>
