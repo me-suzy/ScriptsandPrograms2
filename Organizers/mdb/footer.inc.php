c86f661adf654140ac28c30dd76a2a94917db924
me-suzy <me.suzana@gmail.com>
2025-08-12 21:24:14 +0300

    Add Organizers/mdb/footer.inc.php

---
+++ Organizers/mdb/footer.inc.php
@@ -0,0 +1,19 @@
+<p>&nbsp;</p></td>
+                    </tr>
+                  </table></td>
+              </tr>
+            </table></td>
+        </tr>
+      </table></td>
+  </tr>
+  <tr>
+    <td><div align="center"><font color="#999999" size="-2">My DataBook copyright &copy; 2003 Toby Davis All Rights Reserved</font></div></td>
+  </tr>
+</table>
+</body>
+</html>
+
+<?php
+mysql_close($CONNECT);
+
+?>
