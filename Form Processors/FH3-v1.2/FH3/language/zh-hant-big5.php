8192ca2fa4cd11b24ff436c8371607e0912a7af1
me-suzy <me.suzana@gmail.com>
2025-08-12 16:11:24 +0300

    Add Form Processors/FH3-v1.2/FH3/language/zh-hant-big5.php

---
+++ Form Processors/FH3-v1.2/FH3/language/zh-hant-big5.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * chinese language file
+ *
+ * language file: Traditional Chinese
+ * encoding big-5
+ * @author ??
+ * @package FormHandler
+ */
+
+$fh_lang = array(
+  1  => '¤@¤ë',
+  2  => '¤G¤ë',
+  3  => '¤T¤ë',
+  4  => '¥|¤ë',
+  5  => '¤­¤ë',
+  6  => '¤»¤ë',
+  7  => '¤C¤ë',
+  8  => '¤K¤ë',
+  9  => '¤E¤ë',
+  10 => '¤Q¤ë',
+  11 => '¤Q¤@¤ë',
+  12 => '¤Q¤G¤ë',
+  13 => '¤é´Á¿é¤J¿ù»~!',
+  14 => '¿é¤J­È¦³»~!',
+  15 => '±K½X¤£§k¦X!',
+  16 => '¦¹Äæ¥²¶ñ!',
+  17 => '±K½X¦Ü¤ÖÀ³¦³ %d ¦r¤¸!',
+  18 => '±K½X§tµL®Ä¦r¤¸!',
+  19 => '¦pªG±z¤£·Q«ü©w·s­È, ½Ð¸õ¹L¦¹Äæ¤£¶ñ. (<a href="%path%%filename%" target="_blank">%filename%</a>)<br />',
+  20 => '¶È¤¹³\¤U¦C©µ¦ùÀÉ¦W: %s.',
+  21 => '±z­n¤W¶ÇªºÀÉ®×¤w¦s¦b!',
+  22 => '½Ð°O±o¤W¶ÇÀÉ®×!',
+  23 => 'ÀÉ®×¤j¤p¶W¹L¤W­­ %s kb',
+  24 => 'µo¥Í¿ù»~. ½Ð¦A¸Õ¤@¦¸.',
+  25 => '<small>¦pªG±z¤£·Q«ü©w·s±K½X, ½Ð¸õ¹L¦¹Äæ¤£¶ñ.</small><br />',
+  26 => '°e¥X',
+  27 => '­«³]',
+  28 => '¨ú®ø',
+  29 => '¥i¿ï¨ú',
+  30 => '¤w¿ï¨ú',
+  31 => '¤W¶ÇÀÉ®×ªº«¬ºA¿ù»~!',
+  32 => '¹ÏÀÉ¼e°ª¹³¯À¤£¥i¶W¹L %d x %d. ±z¤W¶Ç¹Ïªº¼e°ª¬° %d x %d.',
+  33 => '¦]¬°¿ù»~µo¥Í, ©Ò¥H¦pªG±z­n¤W¶Ç <b>%filename%</b> , ³Â·Ð±z¦A¿ï¨ú¤@¦¸.<br />',
+  34 => '½Ð¦b±z­n²¾¨ì%s°Ïªº¶µ¥Ø¤W³æÀ», ©Î¬O¥ÎÂùÀ»§â©Ò¦³¶µ¥Ø²¾¹L¥h.',
+  35 => '¥»Äæ­È¥²¶·¬O°ß¤@. ±z¿é¤Jªº "%s" ¤w¸g¦s¦b¸ê®Æ®w¤º.',
+  36 => '<b>%d</b> characters remaining on your input limit',
+  37 => 'You have to supply a file with an extension!',
+  38 => 'Back',
+  39 => 'The combination of the fields "%s" has to be unique. This combination already exists in the database! (Violation of index "%s")'
+);
+
+?>
