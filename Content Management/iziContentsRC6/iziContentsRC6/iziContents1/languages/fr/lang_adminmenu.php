9a705f46188d2b9555f966ad4831c794fe1e35bf
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:15 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_adminmenu.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_adminmenu.php
@@ -0,0 +1,59 @@
+<?php
+
+//  Group Titles and descriptions
+$GLOBALS["tgaccess"] = 'Securite';
+$GLOBALS["tgdaccess"] = 'Maintenir les utilisateurs qui ont accès à la section administrative, et les autres caractéristiques de contrôle accès.';
+$GLOBALS["tgmenus"] = 'Menus';
+$GLOBALS["tgdmenus"] = 'Maintenir la structure des menus sur votre site web.';
+$GLOBALS["tgtemplates"] = 'Gabarits';
+$GLOBALS["tgdtemplates"] = 'Maintenir les formats de gabarits et norme pour votre contenu.';
+$GLOBALS["tgcontents"] = 'Contenus';
+$GLOBALS["tgdcontents"] = 'Maintenir les articles, les pages et les images.';
+$GLOBALS["tglanguages"] = 'Traduction';
+$GLOBALS["tgdlanguages"] = 'Traduire votre contenu de site dans les autres langues.';
+$GLOBALS["tgstyles"] = 'Styles';
+$GLOBALS["tgdstyles"] = 'Vous permettre de personnalise le regard de votre site web.';
+$GLOBALS["tgstatistics"] = 'Statistiques';
+$GLOBALS["tgdstatistics"] = 'Regarder votre statistique de visiteur de site.';
+$GLOBALS["tgbanners"] = 'Bannières';
+$GLOBALS["tgdbanners"] = 'Maintenir les bannières qui sont montrées sur votre site web.';
+
+//  Function Titles
+$GLOBALS["tfprivileges"] = 'Maintenir les privilèges';
+$GLOBALS["tfauthors"] = 'Maintenir les utilisateurs';
+$GLOBALS["tfsetregister"] = 'Montages d\'enregistrement';
+$GLOBALS["tfbackup"] = 'Sauvegarder/Restaurer la base de données';
+$GLOBALS["tfsites"] = 'Maintenir les sites';
+$GLOBALS["tffiletypes"] = 'Maintenir les types de fichier';
+$GLOBALS["tfuserdata"] = 'Les données Utilisateur Encadrent tracé';
+$GLOBALS["tfmenusettings"] = 'Maintenir les montages des menus';
+$GLOBALS["tftopgroups"] = 'Maintenir le menu haut niveau';
+$GLOBALS["tfgroups"] = 'Maintenir les menus';
+$GLOBALS["tfsubgroups"] = 'Maintenir les sous-menus';
+$GLOBALS["tftags"] = 'Maintenir les tags';
+$GLOBALS["tfsidebars"] = 'Maintenir les sidebars';
+$GLOBALS["tfimageformats"] = 'Maintenir les formats image';
+$GLOBALS["tfratings"] = 'Maintenir les paramétres du contenue';
+$GLOBALS["tfcontent"] = 'Maintenir contenus';
+$GLOBALS["tfsubcontent"] = 'Maintenir les modules';
+$GLOBALS["tfimages"] = 'Maintenir les images';
+$GLOBALS["tfscripts"] = 'Maintenir les scripts';
+$GLOBALS["tfdownloads"] = 'Maintenir les téléchargements';
+$GLOBALS["tflanguages"] = 'Maintenir les paramétre de la langue';
+$GLOBALS["tftusergroups"] = 'Traduire les noms des Groupe-Usager';
+$GLOBALS["tfttagcategories"] = 'Traduire la catégorie tag';
+$GLOBALS["tfttopgroups"] = 'Traduire le menu haut-niveau';
+$GLOBALS["tftgroups"] = 'Traduire les menus';
+$GLOBALS["tftsubgroups"] = 'Traduire les sous-menus';
+$GLOBALS["tftcontent"] = 'Traduire le contenue';
+$GLOBALS["tfserversettings"] = 'Paramétre réseau';
+$GLOBALS["tfsitesettings"] = 'Paramétre site';
+$GLOBALS["tfcolorsform"] = 'Paramétrer couleur, fonte';
+$GLOBALS["tfsetgraphics"] = 'Paramétrer graphiques';
+$GLOBALS["tfthemes"] = 'Maintenir les thèmes';
+$GLOBALS["tfviewstatistics"] = 'Statistique';
+$GLOBALS["tfprunestatistics"] = 'Résumer la statistique archive';
+$GLOBALS["tfbanners"] = 'Maintenir les bannières';
+$GLOBALS["tfexcelexport"] = 'Excel-Export';
+
+?>
