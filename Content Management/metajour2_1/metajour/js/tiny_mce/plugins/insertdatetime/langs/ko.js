922216209010fae48ce32e958f03eb48c15a0ad1
me-suzy <me.suzana@gmail.com>
2025-08-12 06:42:52 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/insertdatetime/langs/ko.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/insertdatetime/langs/ko.js
@@ -0,0 +1,12 @@
+// KO lang variables
+
+tinyMCE.addToLang('',{
+insertdate_def_fmt : '%Y-%m-%d',
+inserttime_def_fmt : '%H:%M:%S',
+insertdate_desc : '³¯Â¥ ³&Ouml;±â',
+inserttime_desc : '½Ã°£ ³&Ouml;±â',
+inserttime_months_long : new Array("1¿ù", "2¿ù", "3¿ù", "4¿ù", "5¿ù", "6¿ù", "7¿ù", "8¿ù", "9¿ù", "10¿ù", "11¿ù", "12¿ù"),
+inserttime_months_short : new Array("1¿ù", "2¿ù", "3¿ù", "4¿ù", "5¿ù", "6¿ù", "7¿ù", "8¿ù", "9¿ù", "10¿ù", "11¿ù", "12¿ù"),
+inserttime_day_long : new Array("ÀÏ¿&auml;ÀÏ", "¿ù¿&auml;ÀÏ", "È­¿&auml;ÀÏ", "¼&ouml;¿&auml;ÀÏ", "¸ñ¿&auml;ÀÏ", "±Ý¿&auml;ÀÏ", "&Aring;&auml;¿&auml;ÀÏ", "ÀÏ¿&auml;ÀÏ"),
+inserttime_day_short : new Array("ÀÏ", "¿ù", "È­", "¼&ouml;", "¸ñ", "±Ý", "&Aring;&auml;", "ÀÏ")
+});
