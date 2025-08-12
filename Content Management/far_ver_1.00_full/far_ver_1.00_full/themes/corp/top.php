04b00d000f194f0d04cebafc7a0482a98011a38e
me-suzy <me.suzana@gmail.com>
2025-08-12 03:30:52 +0300

    Add Content Management/far_ver_1.00_full/far_ver_1.00_full/themes/corp/top.php

---
+++ Content Management/far_ver_1.00_full/far_ver_1.00_full/themes/corp/top.php
@@ -0,0 +1,14 @@
+<table width="100%"  border="0" cellpadding="0" cellspacing="1" bordercolor="#1877B4" bgcolor="#1877B4">
+       <tr background="themes/corp/images/bar_sus.gif">
+        <td colspan="3" class="bara_sus"><div align="left">&nbsp;&nbsp;&nbsp;</div></td>
+  </tr>
+	  <tr>
+        <td width="250">   <img src="themes/corp/images/logo.gif" width="250" height="70"></td>
+        <td>
+<?php 
+body_far("banner");
+?>
+</td>
+        <td>&nbsp;</td>
+      </tr>
+</table>
