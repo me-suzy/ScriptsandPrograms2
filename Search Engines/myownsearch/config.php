3d9eabb2d1fa45458b6003ea6ac67e9e8c971f79
me-suzy <me.suzana@gmail.com>
2025-08-12 23:37:37 +0300

    Add Search Engines/myownsearch/config.php

---
+++ Search Engines/myownsearch/config.php
@@ -0,0 +1,34 @@
+<?
+#Please refer to config_variables.html for an explanation of each variable. :)
+$sqlhost = "";
+$sqllogin = "";
+$sqlpass = "";
+$sqldb = "";
+$table = "mos_sites";
+$adstable = "mos_ads";
+$enginestable = "mos_engines";
+$searchfeedid = "6410";
+$revenuepilotid = "2751";
+$webgerusername = "nukedweb";
+$numsearchfeed = "10";
+$numrevenuepilot = "10";
+$numwebger = "10";
+$rpfamfilt = "off";
+$purgedays = "60";
+$mainmod = "index.php";
+$engtitle = "MyOwnSearch";
+$header = "";
+$footer = "";
+$fontface = "Verdana";
+$fontsize = "-1";
+$color1 = "#DDDDDD";
+$color2 = "#EEEEEE";
+$textcolor = "#000000";
+$tablewidth = "90%";
+$maxresults = 30;
+$newwin = "1";
+$showmeta = 1;
+$adminpass = "thepass";
+$showadvert = 1;
+
+?>
