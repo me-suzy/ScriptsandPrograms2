30c26d4dce8f07c2c994c1e9e1eb8d569bb84a8b
me-suzy <me.suzana@gmail.com>
2025-08-12 17:12:23 +0300

    Add Groupware Tools/l4w_v0_5_2/modules/todos/views/docs.js

---
+++ Groupware Tools/l4w_v0_5_2/modules/todos/views/docs.js
@@ -0,0 +1,26 @@
+		/*function confirm_deleting(name, id) {
+		    text         = "<?=translate ('confirm_delete_entry')?>";
+			confirmation = confirm (text.replace (/%s/g,name));
+            if(confirmation != false) {
+                var link = "index.php?command=delete_entry&entry_id=" + id;
+                window.location.href = link;
+            }
+		}
+		
+		function OpenElement (obj) {
+		    var link = "index.php?command=show_contact&contact_id=" + obj.id;
+		    window.location.href = link;       
+		}    
+		
+        function set_filter (selection) {
+            eval ("var element = document.forms[0]."+selection);
+            link = "index.php?command=show_entries&"+selection+"="+element.value;
+            //alert (link);
+            document.location.href=link;
+        }
+        
+        function clear_filters () {
+            link = "index.php?command=show_entries&my_group=all&my_owner=all";
+            document.location.href=link;
+        }*/    
+
