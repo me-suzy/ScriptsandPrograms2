7503b56c896ada50b5805e8146c33f94f8d24ceb
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:35 +0300

    Add Content Management/metajour2_1/metajour/lang/document_view_editor.en.php

---
+++ Content Management/metajour2_1/metajour/lang/document_view_editor.en.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['menu_file'] = 'Files';
+$LANG['menu_filesave'] = 'Save';
+$LANG['menu_documentsection_properties'] = 'Section properties...';
+$LANG['menu_document_properties'] = 'Document properties...';
+
+$LANG['menu_edit'] = 'Edit';
+$LANG['menu_undo'] = 'Undo';
+$LANG['menu_redo'] = 'Redo';
+$LANG['menu_cut'] = 'Cut';
+$LANG['menu_copy'] = 'Copy';
+$LANG['menu_paste'] = 'Paste';
+$LANG['menu_delete'] = 'Delete';
+$LANG['menu_selectall'] = 'Select all';
+$LANG['menu_findtext'] = 'Search...';
+
+$LANG['menu_view'] = 'Show';
+$LANG['menu_toolbar'] = 'Toolbars';
+$LANG['menu_tbstd'] = 'Standard';
+$LANG['menu_tbfmt'] = 'Format';
+$LANG['menu_tbabs'] = 'Absolute positioning';
+$LANG['menu_tbtable'] = 'Table';
+$LANG['menu_tbadvanced'] = 'Advanced';
+$LANG['menu_tbsection'] = 'Sections';
+
+$LANG['menu_format'] = 'Format';
+$LANG['menu_font'] = 'Font';
+$LANG['menu_bold'] = 'Bold';
+$LANG['menu_italic'] = 'Italic';
+$LANG['menu_underline'] = 'Underlined';
+$LANG['menu_forecolor'] = 'Text color';
+$LANG['menu_backcolor'] = 'Background color';
+$LANG['menu_justifyleft'] = 'Align left';
+$LANG['menu_justifycenter'] = 'Align center';
+$LANG['menu_justifyright'] = 'Align right';
+
+$LANG['menu_html'] = 'HTML';
+$LANG['menu_hyperlink'] = 'Hyperlink...';
+$LANG['menu_anchor'] = 'Anchor...';
+$LANG['menu_image'] = 'Image...';
+$LANG['menu_visibleborders'] = 'Visible borders';
+$LANG['menu_showdetails'] = 'Show details';
+$LANG['menu_makeabs'] = 'Absolute positioning';
+$LANG['menu_lock'] = 'Lock';
+$LANG['menu_zorder'] = 'Order';
+
+$LANG['menu_bringfront'] = 'Bring to front';
+$LANG['menu_sendback'] = 'Send to back';
+$LANG['menu_bringforward'] = 'Bring forward';
+$LANG['menu_sendbackward'] = 'Send backwards';
+$LANG['menu_belowtext'] = 'Below text';
+$LANG['menu_abovetext'] = 'Above text';
+$LANG['menu_snaptogrid'] = 'Snap to grid';
+$LANG['menu_customhtml'] = 'Custom HTML';
+
+$LANG['menu_orderlist'] = 'Numbered list';
+$LANG['menu_unorderlist'] = 'Bullet list';
+$LANG['menu_indent'] = 'Indent';
+$LANG['menu_outdent'] = 'Outdent';
+
+$LANG['menu_inserttable'] = 'Insert table';
+$LANG['menu_insertrow'] = 'Insert row';
+$LANG['menu_deleterows'] = 'Delete rows';
+$LANG['menu_insertcol'] = 'Insert column';
+$LANG['menu_deletecols'] = 'Delete columns';
+$LANG['menu_insertcell'] = 'Insert cell';
+$LANG['menu_deletecells'] = 'Delete cells';
+$LANG['menu_mergecells'] = 'Merge cells';
+$LANG['menu_splitcell'] = 'Split cell';
+
+$LANG['menu_showhtml'] = 'Show HTML';
+
+$LANG['menu_newvariant'] = 'New variant';
+$LANG['menu_newsection'] = 'New section';
+$LANG['menu_newsectionbelow'] = 'New section below';
+$LANG['menu_moveupsection'] = 'Move section up';
+$LANG['menu_movedownsection'] = 'Move section down';
+$LANG['menu_deletesection'] = 'Delete section';
+$LANG['menu_requestapproval'] = 'Request approval';
+$LANG['menu_approvepublish'] = 'Approve and publish';
+$LANG['menu_preview'] = 'Preview';
+
+$LANG['color_title'] = 'Select color';
+$LANG['color_lable'] = 'Color: ';
+
+$LANG['table_inserttitle'] = 'Insert table';
+$LANG['table_edittitle'] = 'Edit table';
+$LANG['table_dimension'] = 'Dimensions';
+$LANG['table_rows'] = 'Rows:';
+$LANG['table_width'] = 'Width:';
+$LANG['table_columns'] = 'Columns:';
+$LANG['table_borderwidth'] = 'Border width:';
+$LANG['table_colors'] = 'Colors';
+$LANG['table_bordercolor'] = 'Border color:';
+$LANG['table_bgcolor'] = 'Background color:';
+$LANG['table_spacing'] = 'Spacing';
+$LANG['table_cellpadding'] = 'Cell padding:';
+$LANG['table_cellspacing'] = 'Cell spacing:';
+
+$LANG['table_select'] = 'Select';
+
+?>
