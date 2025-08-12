870cff5044ffb132f4c9d62aa345afda425c4c7c
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:11 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_access.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_access.en.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Access';
+$LANG['text_1'] = 'Select website access for selected records:';
+$LANG['text_2'] = 'Select system access for selected records:';
+$LANG['text_3'] = 'Press Ctrl to select more than 1 user/group';
+?>
