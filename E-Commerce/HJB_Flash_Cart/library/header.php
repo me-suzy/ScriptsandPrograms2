acd0817960249cfded39523db1dd0617f9115b86
me-suzy <me.suzana@gmail.com>
2025-08-12 12:22:50 +0300

    Add E-Commerce/HJB_Flash_Cart/library/header.php

---
+++ E-Commerce/HJB_Flash_Cart/library/header.php
@@ -0,0 +1,43 @@
+<?php
+/*
+***************************************************************************************************************************
+*****************************************COPYRIGHT 2005 YOU MAY NOT USE THIS WITHOUT PERMISSION****************************
+
+HJB IS PROVIDED "As Is" FOR USE ON WEBSITES WHERE A LICENSE FOR SUCH USE WAS PURCHASED.  IT MAY ONLY BE USED ON ONE SITE PER LICENSING
+FEE.  IN ORDER TO USE ON ADDITIONAL SITES, ADDITIONAL LICENSES MUST BE PURCHASED.  
+
+
+THE PHP SCRIPTS MAY BE ALTERED, AS LONG AS THE CREDIT LINE AND LINKS AT THE BOTTOM OF EACH PAGE REMAIN. THE FLASH MAY NOT IN ANY
+WAY BE CHANGED OR ALTERED.  ANY VIOLATION OF THESE TERMS WILL RESULT IN THE FORFEITING OF YOUR RIGHT TO USE THIS SOFTWARE.
+
+NationWideShelving.com does not guarantee this software in anyway.  You use this at your own risk.  NationWideShelving or any of its
+employees or subsidiaries are not responsible for any damage, and / or loss of business, reputation, or other damages of any kind
+which are caused whether actual or not, by the use of this product.  By using this product you agree to hold NationWideShelving, its
+employees, and all subsidiaries harmless for any and all reasons associated with your use of this product.
+
+Your installation of this software consititues an agreement to these terms.
+
+****************************************************************************************************************************
+	*/
+?>
+<style type="text/css">
+<!--
+.stylefifteen {	font-size: 14px;
+	color: #FF0000;
+}
+.stylehello {	color: #FF0000;
+	font-style: italic;
+}
+.styleQuck {
+	font-family: Verdana, Arial, Helvetica, sans-serif;
+	font-size: 10px;
+}
+-->
+</style>
+<link href="styles.css" rel="stylesheet" type="text/css">
+<table width="100%"  border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td width="5%" bgcolor="#010556"><a href="http://www.mans-garage.com/shopping_cart_info"><img src="/images/logo.gif" width="359" height="182" border="0"></a></td>
+    <td width="95%" bgcolor="#010556">&nbsp;</td>
+  </tr>
+</table>
