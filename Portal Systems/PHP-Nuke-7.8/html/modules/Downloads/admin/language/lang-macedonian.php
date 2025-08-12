f9d38f06049e4d09a7278570addfc954f1ef9f55
me-suzy <me.suzana@gmail.com>
2025-08-12 22:35:30 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Downloads/admin/language/lang-macedonian.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Downloads/admin/language/lang-macedonian.php
@@ -0,0 +1,112 @@
+<?php
+
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation go to the my website and send to me          */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+
+
+define("_URL","URL");
+define("_FUNCTIONS","Ôóíêöèè");
+define("_YES","Äà");
+define("_NO","Íå");
+define("_CATEGORY","Êàòåãîðè¼à");
+define("_SAVECHANGES","Ñíèìè èçìåíè");
+define("_OK","Âî ðåä!");
+define("_HITS","Õèòîâè");
+define("_THEREARE","Èìà");
+define("_CHECK","Ïðîâåðè");
+define("_AUTHORNAME","Èìå íà àâòîðîò");
+define("_AUTHOREMAIL","E-mail íà àâòîðîò");
+define("_DOWNLOADNAME","Èìå íà ïðîãðàìàòà");
+define("_INBYTES","âî áà¼òè");
+define("_FILESIZE","Ãîëåìèíà íà äàòîòåêàòà");
+define("_VERSION","Âåðçè¼à");
+define("_DESCRIPTION","Îïèñ");
+define("_AUTHOR","Àâòîð");
+define("_HOMEPAGE","Äîìàøíà ñòðàíà");
+define("_NAME","Èìå");
+define("_FILEURL","Ëèíê çà äàòîòåêàòà");
+define("_DOWNLOADID","Èäåíòèôèêàöè¼à íà äàòîòåêàòà");
+define("_PAGETITLE","Íàñëîâ íà ñòðàíàòà");
+define("_PAGEURL","Àäðåñà íà ñòðàíàòà");
+define("_ADDURL","Äîäà¼ ¼à îâàà àäðåñà");
+define("_DOWNLOAD","Ïðåçåìàà");
+define("_TITLE","Íàñëîâ");
+define("_STATUS","Ñòàòóñ");
+define("_ADD","Äîäàäè");
+define("_MODIFY","Èçìåíè");
+define("_DOWNLOADSINDB","ïðåçåìàà âî íàøàòà áàçà.");
+define("_DOWNLOADSWAITINGVAL","Ïðåçåìàà êîè ÷åêààò çà ïîòâðäà");
+define("_CLEANDOWNLOADSDB","Èçáðèøè ãè ãëàñîâèòå çà ïðåçåìààòà");
+define("_BROKENDOWNLOADSREP","Èçâåñòóâàà çà äåôåêòíî ïðåçåìàå");
+define("_DOWNLOADMODREQUEST","Áàðàà çà èçìåíà íà ïðåçåìààòà");
+define("_ADDNEWDOWNLOAD","Äîäàäè íîâî ïðåçåìàå");
+define("_MODDOWNLOAD","Èçìåíè ïðåçåìàå");
+define("_WEBDOWNLOADSADMIN","Àäìèíèñòðàöè¼à íà ïðåçåìàà");
+define("_DNOREPORTEDBROKEN","Íåìà ïðè¼àâåíè äåôåêòíè ïðåçåìàà.");
+define("_DUSERREPBROKEN","Îä êîðèñíèöè ïðè¼àâåíè äåôåêòíè ïðåçåìàà.");
+define("_DIGNOREINFO","Èãíîðèðà¼ (Ãè áðèøå ñèòå <b><i>áàðàà</i></b> çà îäðåäåíî ïðåçåìàå)");
+define("_DDELETEINFO","Èçáðèøè (Ãè áðèøå <b><i>íåâàæå÷êèòå ïðåçåìàà</i></b> è <b><i>áàðàà</i></b> çà îäðåäåíî ïðåçåìàå)");
+define("_DOWNLOADOWNER","Ñîïñòâåíèê íà ïðåçåìàåòî");
+define("_DUSERMODREQUEST","Áàðàà çà èçìåíà íà ïðåçåìààòà îä êîðèñíèöè");
+define("_DOWNLOADVALIDATION","Ïîòâðäà çà ïðåçåìàå");
+define("_CHECKALLDOWNLOADS","Ïðîâåðè ãè ÑÈÒÅ ïðåçåìàà");
+define("_VALIDATEDOWNLOADS","Ïîòâðäè ãè ïðåçåìààòà");
+define("_NEWDOWNLOADADDED","Íîâî ïðåçåìàå äîäàäåíî âî íàøàòà áàçà");
+define("_SUBMITTER","Ïîäíåñóâà÷");
+define("_VISIT","Ïîñåòè");
+define("_ADDMAINCATEGORY","Äîäàäè ÃËÀÂÍÀ êàòåãîðè¼à");
+define("_ADDSUBCATEGORY","Äîäàäè ÏÎÄ-Êàòåãîðè¼à");
+define("_IN","âî");
+define("_DESCRIPTION255","Îïèñ: (íà¼ìíîãó 255 çíàöè)");
+define("_MODCATEGORY","Èçìåíè êàòåãîðè¼à");
+define("_ADDEDITORIAL","Äîäàäè îñâðò íà óðåäíèêîò");
+define("_EDITORIALTITLE","Íàñëîâ");
+define("_EDITORIALTEXT","Òåêñò");
+define("_DATEWRITTEN","Íàïèøàíî íà");
+define("_IGNORE","Èãíîðèðà¼");
+define("_ORIGINAL","Îðèãèíàë");
+define("_PROPOSED","Ïðåäëîæåí");
+define("_NOMODREQUESTS","Âî ìîìåíòîâ íåìà áàðàà çà èçìåíà");
+define("_SUBCATEGORY","Ïîäêàòåãîðè¼à");
+define("_OWNER","Ñîïñòâåíèê");
+define("_ACCEPT","Ïðèôàòè");
+define("_ERRORTHECATEGORY","ÃÐÅØÊÀ: Êàòåãîðè¼àòà");
+define("_ALREADYEXIST","âå?å ïîñòîè!");
+define("_ERRORTHESUBCATEGORY","ÃÐÅØÊÀ: Ïîäêàòåãîðè¼àòà");
+define("_EDITORIALADDED","Îñâðòîò íà óðåäíèêîò å äîäàäåí âî áàçàòà");
+define("_EDITORIALMODIFIED","Îñâðòîò íà óðåäíèêîò å èçìåíåò");
+define("_EDITORIALREMOVED","Îñâðòîò íà óðåäíèêîò å èçáðèøàí îä áàçàòà");
+define("_CHECKCATEGORIES","Ïðîâåðè ãè êàòåãîðèèòå");
+define("_INCLUDESUBCATEGORIES","(âêëó÷è ãè è ïîäêàòåãîðèèòå)");
+define("_FAILED","Íåóñïåøíî!");
+define("_BEPATIENT","(áèäè ñòðïëèâ(à))");
+define("_VALIDATINGCAT","Ïðîâåðêà íà êàòåãîðè¼à (è ñèòå ïîäêàòåãîðèè)");
+define("_VALIDATINGSUBCAT","Ïðîâåðè ïîäêàòåãîðè¼à");
+define("_ERRORURLEXIST","ÃÐÅØÊÀ: Îâî¼ URL âå?å ïîñòîè âî áàçàòà!");
+define("_ERRORNOTITLE","ÃÐÅØÊÀ: Òðåáà äà âíåñåø ÍÀÑËÎÂ çà òâî¼îò URL!");
+define("_ERRORNOURL","ÃÐÅØÊÀ: Òðåáà äà âíåñåø URL çà òâî¼îò URL!");
+define("_ERRORNODESCRIPTION","ÃÐÅØÊÀ: Òðåáà äà âíåñåø ÎÏÈÑ çà òâî¼îò URL!");
+define("_EZTRANSFER","Ïðåíåñóâàå");
+define("_EZTHEREIS","Îâàà êàòåãîðè¼à èìà");
+define("_EZSUBCAT","ïîäêàòåãîðèè");
+define("_EZATTACHEDTOCAT","");
+define("_EZTRANSFERDOWNLOADS","Ïðåíåñè ãè ñèòå ïðåçåìàà îä êàòåãîðè¼à");
+define("_DELEZDOWNLOADSCATWARNING","ÂÍÈÌÀÍÈÅ: Ñèãðóðíî ëè ñàêàø äà ¼à èçáðèøåø êàòåãîðè¼àâà? ?å ãè èçáðèøåø è ñèòå ïîäêàòåãîðèè è ïðèêà÷åíè ïðåçåìàà!");
+define("_DOWNLOADTITLE","Ïðåçåìè íàñëîâ");
+
+
+?>
