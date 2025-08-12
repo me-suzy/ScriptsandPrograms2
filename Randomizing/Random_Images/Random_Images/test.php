b47a7c706c0642f25d6de7c52e94807ca29649ee
me-suzy <me.suzana@gmail.com>
2025-08-12 23:33:53 +0300

    Add Randomizing/Random_Images/Random_Images/test.php

---
+++ Randomizing/Random_Images/Random_Images/test.php
@@ -0,0 +1,9 @@
+<?php?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+"http://www.w3.org/TR/html4/strict.dtd"><html><head>
+<title>The test page for a random jpg script</title><link rel="stylesheet" type="text/css" href="style.css">
+</head><body><div>
+<?php 
+echo "The pictures loaded from your db";
+include ('Ran_Img.php');
+include('form.php');
+</div></body></html>
