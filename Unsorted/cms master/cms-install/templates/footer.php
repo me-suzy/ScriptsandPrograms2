7c0abb0d4a8e3c19028324105caa3d75ff481763
me-suzy <me.suzana@gmail.com>
2025-08-13 01:48:29 +0300

    Add Unsorted/cms master/cms-install/templates/footer.php

---
+++ Unsorted/cms master/cms-install/templates/footer.php
@@ -0,0 +1,11 @@
+<br>
+<hr size=1 color=#e2e2e2 width=450>
+</td>
+<td width=20 valign=top><img src=images/trans.gif width=20 height=1 border=0></td>
+</tr>
+</table>
+</center>
+
+
+</body>
+</html>
