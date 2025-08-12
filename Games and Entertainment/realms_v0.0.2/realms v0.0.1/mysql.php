1d2d623bb0e13744a0a5e5fcdf43606694be3d5b
me-suzy <me.suzana@gmail.com>
2025-08-12 16:32:12 +0300

    Add Games and Entertainment/realms_v0.0.2/realms v0.0.1/mysql.php

---
+++ Games and Entertainment/realms_v0.0.2/realms v0.0.1/mysql.php
@@ -0,0 +1,7 @@
+<?php
+
+//edit below, change localhost to your mysql hostname (usually localhost), change username to your mysql username, change password to your mysql password and change databasename to the name o fthe database that you executed the sql file into
+
+
+mysql_connect("localhost","contest_user","contest_pass");
+mysql_select_db("contest_db");
