183ac3dc302d57360d90e23061a7f3859ae00efd
me-suzy <me.suzana@gmail.com>
2025-08-12 08:14:52 +0300

    Add Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/modules/htmlarea/install.php

---
+++ Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/modules/htmlarea/install.php
@@ -0,0 +1,51 @@
+<?php
+
+// $Id: install.php 238 2005-11-21 10:43:34Z stefan $
+
+/*
+
+ Website Baker Project <http://www.websitebaker.org/>
+ Copyright (C) 2004-2005, Ryan Djurovich
+
+ Website Baker is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Website Baker is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Website Baker; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+if(defined('WB_URL')) {
+	
+	// Set the filename of the actual editor file
+	$editor_file = $module_dir.'/htmlarea.zip';
+	
+	if(file_exists($editor_file)) {
+	
+		// Setup the PclZip object
+		$editor_archive = new PclZip($editor_file);
+
+		// Unzip to module dir
+		$list = $editor_archive->extract(PCLZIP_OPT_PATH, $module_dir);
+		if(!$list) {
+			$admin->print_error($MESSAGE['GENERIC']['CANNOT_UNZIP']);
+		}
+		// Delete the zip file
+		if(file_exists($editor_file)) { unlink($editor_file); }
+	
+		// Chmod all the editors files
+		chmod_directory_contents($module_dir.'/htmlarea', OCTAL_FILE_MODE);
+	
+	}
+	
+}
+
+?>
