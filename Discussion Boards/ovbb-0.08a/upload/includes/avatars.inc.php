4a15c16f03851869858449d53573c71404c17f0e
me-suzy <me.suzana@gmail.com>
2025-08-12 10:45:47 +0300

    Add Discussion Boards/ovbb-0.08a/upload/includes/avatars.inc.php

---
+++ Discussion Boards/ovbb-0.08a/upload/includes/avatars.inc.php
@@ -0,0 +1,15 @@
+<?php
+//***************************************************************************//
+//                                                                           //
+//  Copyright (c) 2004-2005 Jonathon J. Freeman                              //
+//  All rights reserved.                                                     //
+//                                                                           //
+//  This program is free software. You may use, modify, and/or redistribute  //
+//  it under the terms of the OvBB License Agreement v2 as published by the  //
+//  OvBB Project at www.ovbb.org.                                            //
+//                                                                           //
+//***************************************************************************//
+
+//	$aAvatars[1]['title'] = 'Title';
+//	$aAvatars[1]['filename'] = 'filename.jpg';
+?>
