07e3df61563983d7c897135c6f18f73b35c50547
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:09 +0300

    Add Content Management/metajour2_1/metajour/lang/task.da.php

---
+++ Content Management/metajour2_1/metajour/lang/task.da.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Opgaver';
+$LANG['label_companyid'] = 'Virksomhed';
+$LANG['label_name'] = 'Navn';
+$LANG['label_content'] = 'Beskrivelse';
+$LANG['label_priority'] = 'Prioritet';
+$LANG['label_status'] = 'Status';
+$LANG['label_typecode'] = 'Type';
+$LANG['label_budget'] = 'Budget';
+$LANG['label_offer'] = 'Tilbud';
+$LANG['label_expstart'] = 'Forventet start';
+$LANG['label_expfinish'] = 'Forventet slut';
+$LANG['label_finished'] = 'Afsluttet';
+$LANG['label_taskid'] = 'Underopgave af';
+$LANG['option_status']['0'] = 'Ikke påbegyndt';
+$LANG['option_status']['5'] = 'Igangværende';
+$LANG['option_status']['10'] = 'Afsluttet - godkendelse ønskes';
+$LANG['option_status']['15'] = 'Afsluttet - godkendt';
+$LANG['option_typecode']['0'] = 'Rettelse';
+$LANG['option_typecode']['5'] = 'Ændring';
+$LANG['option_typecode']['10'] = 'Tilføjelse';
+?>
