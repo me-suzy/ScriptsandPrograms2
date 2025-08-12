072ba514736d8d36d3ee9c6bd7c1f0ed6c9a830f
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:26 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_images.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_images.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'Bilder verwalten';
+$GLOBALS["tFormTitle1"] = 'Bilder hochladen/Unterverzeichnisse erstellen';
+$GLOBALS["tFormTitle2"] = 'Bild hochladen';
+$GLOBALS["tFormTitle3"] = 'Unterverzeichnis erstellen';
+
+//  List Functions
+$GLOBALS["tAddNewImage"] = 'Neues Bild hinzuf&uuml;gen';
+$GLOBALS["tDeleteImage"] = 'Bild l&ouml;schen';
+$GLOBALS["tDeleteImageDir"] = 'Bildverzeichnis l&ouml;schen';
+
+//  List Headings
+$GLOBALS["tImageName"] = 'Bildname';
+
+//  Form Fields
+$GLOBALS["tImageFilename"] = 'Bild Dateiname';
+$GLOBALS["tSubdirectoryname"] = 'Unterverzeichnis-Name';
+
+//  Form Buttons
+$GLOBALS["bUpload"] = 'Hochladen!';
+$GLOBALS["bCreateDir"] = 'Erstellen!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] = 'Sie m&uuml;ssen eine Datei ausw&auml;hlen.';
+$GLOBALS["eNoDir"] = 'Sie m&uuml;ssen einen Verzeichnis-Namen eingeben.';
+$GLOBALS["eDirAlreadyExists"] = 'Dieses Unterverzeichnis besteht bereits, bitte geben Sie einen anderen Namen ein.';
+$GLOBALS["eZeroByteFile"] = 'Diese Datei hat eine L&auml;nge von 0 bytes';
+$GLOBALS["eNoFileUpload"] = 'Leider konnte die Datei nicht erfolgreich hochgeladen werden, bitte versuchen Sie es nocheinmal.';
+$GLOBALS["eInvalidFileSize"] = 'Diese Datei ist zu gross zum hochladen, die Begrenzung liegt bei 2 MB';
+$GLOBALS["eInvalidFileType"] = 'Diese Datei hat kein g&uuml;ltiges Bilddatei-Format, bitte w&auml;hlen Sie eine andere Datei.';
+$GLOBALS["eInvalidMimeType"] = 'MIME-Typ der Datei ist kein g&uuml;ltiger Bild-Typ';
+
+?>
