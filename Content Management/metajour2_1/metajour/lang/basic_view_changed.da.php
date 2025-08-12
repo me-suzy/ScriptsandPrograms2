9b2e2925133f1427a28098794b35860a529423dd
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:26 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_changed.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_changed.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Redigeret';
+$LANG['text_1'] = 'Angiv redigeringstidspunkt for de markerede poster';
+$LANG['text_2'] = 'Redigeret: ';
+?>
