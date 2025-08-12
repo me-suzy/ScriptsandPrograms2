2c77edc83f40f5f276f7fa25103466b461708560
me-suzy <me.suzana@gmail.com>
2025-08-12 12:01:09 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/modules/order_total/ot_subtotal.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/modules/order_total/ot_subtotal.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_TITLE', 'Zwischensumme');
+  define('MODULE_ORDER_TOTAL_SUBTOTAL_DESCRIPTION', 'Zwischensumme einer Bestellung');
+?>
