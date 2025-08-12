382c1235ac730aa681ee17525e0dc2c8f6f04fad
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:12 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_content.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_content.php
@@ -0,0 +1,92 @@
+<?php
+
+
+//  Formular Titel
+$GLOBALS["tFormTitle"] = 'Inhalte verwalten';
+$GLOBALS["tFormTitle2"] = '&Uuml;bersetzung des Inhaltes';
+
+//  Überschriften listen
+$GLOBALS["tMenu"] = 'Men&uuml;';
+$GLOBALS["tPageRef"] = 'Artikel-Name';
+$GLOBALS["tSubmenu"] = 'Untermen&uuml;';
+$GLOBALS["tTitle"] = 'Überschrift';
+
+//  Funktionen listen
+$GLOBALS["tMenuFilter"] = 'Men&uuml;-Filter';
+$GLOBALS["tAllMenus"] = 'Alle Men&uuml;s';
+$GLOBALS["tAddNewContent"] = 'Neue Inhalts-Seite hinzuf&uuml;gen';
+$GLOBALS["tEditContent"] = 'Inhalt &auml;ndern';
+$GLOBALS["tDeleteContent"] = 'Inhalt l&ouml;schen';
+$GLOBALS["tTranslate"] = 'Inhaltsseite &uuml;bersetzen';
+$GLOBALS["tViewTranslation"] = '&Uuml;bersetzung ansehen';
+$GLOBALS["tEditTranslation"] = '&Uuml;bersetzung &auml;ndern';
+$GLOBALS["tRSSen"] 		= 'RSS';
+$GLOBALS["tSearchen"] 		= 'Suche';
+
+//  Form Block Titel
+$GLOBALS["thContentLinks"] = 'Seiten-Link Details';
+$GLOBALS["thDates"] = 'Daten zur Anzeige';
+$GLOBALS["thHeader"] = '&Uuml;berschrift';
+$GLOBALS["thTeaser"] = 'Teaser';
+$GLOBALS["thBodyContent"] = 'Hauptinhalt';
+$GLOBALS["thFooter"] = 'Footer';
+$GLOBALS["thPosition"] = 'Position auf Seite';
+$GLOBALS["thRatings"] = 'Bewertungs-Optionen';
+$GLOBALS["thRSS"]	= 'RSS / Suche';
+
+//  Form Field Headings
+$GLOBALS["tPublishDate"] = 'Ver&ouml;ffentlichungsdatum';
+$GLOBALS["tExpiryDate"] = 'Verfall-Datum';
+$GLOBALS["tHeader"] = 'Text';
+$GLOBALS["tHeaderImage"] = 'Bild statt Text (optional)';
+$GLOBALS["tTeaser"] = 'Teaser';
+$GLOBALS["tBodyText"] = 'Haupttext';
+$GLOBALS["tShowHeader"] = 'Überschrift anzeigen';
+$GLOBALS["tShowAuthor"] = 'Autor anzeigen';
+$GLOBALS["tShowUpdate"] = 'Aktualisierungsdatum anzeigen';
+$GLOBALS["tTeaserImage"] = 'Teaser Bild';
+$GLOBALS["tTeaserImageAlign"] = 'Teaser Bild ausrichten';
+$GLOBALS["tDetailImage"] = 'Bild auf der Detailseite';
+$GLOBALS["tDetailImageAlign"] = 'Bild der Detailseite ausrichten';
+$GLOBALS["tLeftRight"] = 'Linke/Rechte Spalte';
+$GLOBALS["tCanRate"] = 'Artikelbewertung erlauben';
+$GLOBALS["tCanComment"] = 'Artikelkommentare erlauben';
+$GLOBALS["tPrinterFriendly"] = 'Druckerfreundliche Anzeige erlauben';
+$GLOBALS["tPDFPrint"] = 'PDF Anzeige erlauben';
+$GLOBALS["tTellFriend"] = 'Tell-a-Friend erlauben';
+$GLOBALS["tAuthor"] = 'Artikel-Autor';
+$GLOBALS["tRSS"] 		= 'Inhalt f&uuml; RSS bereitstellen';
+$GLOBALS["tSearch"]	= 'Inhalt f&uuml;r Suche bereitstellen';
+
+//  Form Detail Kommentare und Hilfetexte
+$GLOBALS["tDetails"] = 'In diesem Formular k&ouml;nnen Sie neue Autoren erstellen oder &auml;ndern, die Texte und Bilder in Ihrer Webseite &auml;ndern k&ouml;nnen.<br /><br />1. Wenn der Teaser-Text leer gelassen wird, dann wird nur der Hauptinhalt auf der Seite gezeigt. <br />2. Wenn der Teaser-Text ausgef&uuml;llt wird und im Hauptinhalt kein weiterer Text steht, wird kein \"weiterlesen...\" Link angezeigt. <br />3. Wurden Teaser-Text und Hauptinhaltstext eingegeben, dann wird der Teaser-Text auf der Inhaltsseite gezeigt, <br />und der \"weiterlesen...\" Link wird auf den Hauptinhalts-Text weiter verweisen. <br /><br />Wenn Sie HTML Code verwenden, wird er so verschl&uuml;sselt, dass der Inhalt so wie eingegeben angezeigt wird. <br />Sie k&ouml;nnen Kurzbefehle verwenden. Diese beinhalten:<br />';
+$GLOBALS["hMenu"] = 'Dies ist das Men&uuml;, das l&auml;ngsseitig dieser Inhaltsseite angezeigt wird. ';
+$GLOBALS["hSubmenu"] = 'Dies ist das Untermen&uuml;, das l&auml;ngsseitig dieser Inhaltsseite angezeigt wird.';
+$GLOBALS["hPublishDate"] = 'Das Ver&ouml;ffentlichungsdatum des Textes, vor dem die Seite nicht angezeigt wird.';
+$GLOBALS["hExpiryDate"] = 'Das Verfallsdatum des Textes, nach dem die Seite nicht mehr angezeigt wird.';
+$GLOBALS["hHeader"] = 'Der Titel, der als &Uuml;berschrifte f&uuml;r diese Seite angezeigt wird, wenn der \"Zeige &Uuml;berschrift\" -Haken gesetzt wurde.';
+$GLOBALS["hHeaderImage"] = 'Ein Bild, das anstelle von Text in der &Uuml;berschriften-Zeile angezeigt wird, wenn der \"Zeige &Uuml;berschrift\" -Haken gesetzt wurde.';
+$GLOBALS["hTeaser"] = 'Reizartikel-Text.<br />Wenn Sie Text in dieses Feld einf&uuml;gen, wird dieser statt dem Hauptinhalt auf der Seite erscheinen, mit einem \"weiterlesen...\" Link zum Hauptinhalt.';
+$GLOBALS["hBodyText"] = 'Dies ist der Haupttext zu Ihrem Inhalt.';
+$GLOBALS["hShowHeader"] = 'Wenn dieser Haken gesetzt wurde, wird der &Uuml;berschriften-Text oder das &Uuml;berschriften-Bild &uuml;ber dem Reizartikel oder dem Hauptinhalt angezeigt.';
+$GLOBALS["hShowAuthor"] = 'Wenn dieser Haken gesetzt wurde, wird eine Fusszeile an Ihren Inhalt angef&uuml;gt mit einem E-Mail-Link zum Autor des Textes.';
+$GLOBALS["hShowUpdate"] = 'Wenn dieser Haken gesetzt wurde, wird in der Fusszeile des Textes das Erstellungs- oder Aktualisierungs-Datum angezeigt.';
+$GLOBALS["hTeaserImage"] = 'Hier kann ein Bild angegeben werden, das oberhalb des Reizartikel angezeigt wird.';
+$GLOBALS["hTeaserImageAlign"] = 'Wenn Sie ein Bild f&uuml;r den Reizartikel ausgew&auml;hlt haben, k&ouml;nnen Sie es rechts oder links vom Reizartikel-Text ausrichten.';
+$GLOBALS["hDetailImage"] = 'Hier kann ein Bild angegeben werden, das oberhalb des Haupttextes angezeigt wird.';
+$GLOBALS["hDetailImageAlign"] = 'Wenn Sie ein Bild f&uuml;r den Haupttext ausgew&auml;hlt haben, k&ouml;nnen Sie es rechts oder links vom Haupttext ausrichten.';
+$GLOBALS["hLeftRight"] = 'Das Setzen dieses Hakens entscheidet, ob diese Inhaltsseite in der linken (mittleren) oder der rechten Spalte erscheint, innerhalb einer definierten Spalten-Anzeige.';
+$GLOBALS["hCanRate"] = 'Setzen Sie diese Markierung, um das Bewerten des Artikels zu erlauben.';
+$GLOBALS["hCanComment"] = 'Setzen Sie diese Markierung, um das Kommentieren von Besuchern f&uuml;r diesen Artikel zu erlauben.';
+$GLOBALS["hPrinterFriendly"] = 'Setzen Sie diese Markierung, um das Symbol f&uuml;r die \"druckerfreundliche Version\" f&uuml;r diesen Artikel anzuzeigen.';
+$GLOBALS["hPDFPrint"] = 'Setze diese Markierung, um das PDF Symbol im Artikel anzuzeigen';
+$GLOBALS["hTellFriend"] = 'Setze diese Markierung, um das Tell-a-Friend Symbol im Artikel anzuzeigen';
+$GLOBALS["hAuthor"] = 'Der Autor dieses Artikels.';
+$GLOBALS["hRSS"] 					= 'Inhalt f&uuml; RSS bereitstellen.';
+$GLOBALS["hSearch"] 			= 'Inhalt f&uuml;r Suche bereitstellen.';
+
+//  Fehlernachrichten
+$GLOBALS["eArticleExists"] = 'Ein Artikel mit diesem Namen besteht bereits, bitte w&auml;hlen Sie einen anderen Namen.';
+$GLOBALS["eInvalidName"] = 'Der Artikel-Name enth&auml;lt ung&uuml;ltige Zeichen.';
+
+?>
