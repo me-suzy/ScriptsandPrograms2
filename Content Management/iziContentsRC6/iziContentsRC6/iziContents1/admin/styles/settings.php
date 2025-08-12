ea16179717442e0aecfd08c6274b9ab5182f7250
me-suzy <me.suzana@gmail.com>
2025-08-12 03:40:30 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/styles/settings.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/styles/settings.php
@@ -0,0 +1,75 @@
+<?php
+
+/***************************************************************************
+
+ settings.php
+ -------------
+ copyright : (C) 2005 - The iziContents Development Team
+
+ iziContents version : 1.0
+ fileversion : 1.0.1
+ change date : 23 - 04 - 2005
+ ***************************************************************************/
+
+/***************************************************************************
+ The iziContents Development Team offers no warranties on this script.
+ The owner/licensee of the script is solely responsible for any problems
+ caused by installation of the script or use of the script.
+
+ All copyright notices regarding iziContents and ezContents must remain intact on the scripts and in the HTML for the scripts.
+
+ For more info on iziContents,
+ visit http://www.izicontents.com*/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the License which can be found within the
+ *   zipped package. Under the licence of GPL/GNU.
+ *
+ ***************************************************************************/
+
+$EzAdmin_Style["adminmenuwidth"]			= 190;				//	Width for the admin menu frame
+$EzAdmin_Style["adminsubmenuoffset"]		= 0;				//	Horizontal offset for sub-menu entries
+
+$EzAdmin_Style["menubutton"]				= 'menu_button';	//	Menu Button image template file name (without the file extension)
+$EzAdmin_Style["menubuttonMethod"]			= 'simulated';		// 'cacheable' once generated in the /admin/icache directory,
+																//		'simulated'
+$EzAdmin_Style["menubuttonFont"]			= 'Arial';			//	Menu button font face
+$EzAdmin_Style["menubuttonFontSize"]		= 14;				//	Menu button font size
+$EzAdmin_Style["menubuttonAlign"]			= 'left';			//	Menu button text alignment ('left', 'right' or 'center')
+$EzAdmin_Style["menubuttonValign"]			= 'center';			//	Menu button text vertical alignment ('top', 'bottom' or 'center')
+$EzAdmin_Style["menubuttonTextColour"]		= '#000000';		//	Must be an RGB numeric value, prefixed with '#'
+$EzAdmin_Style["menubuttonColour"]			= 'Blue';			//	menubuttoncolour, menubuttonborder and menubuttonbordercolour
+$EzAdmin_Style["menubuttonBorder"]			= 0;				//		are used only if the script is unable to generate or even
+$EzAdmin_Style["menubuttonBorderColour"]	= 'White';			//		simulate a graphic button and forced to resort to text
+
+$EzAdmin_Style["addbutton"]					= 'addnew_button';	//	Add Button image template file name (without the file extension)
+$EzAdmin_Style["addbuttonMethod"]			= 'simulated';		// 'cacheable' once generated in the /admin/icache directory,
+																//		'simulated'
+$EzAdmin_Style["addbuttonFont"]				= 'Arial';			//	Add button font face
+$EzAdmin_Style["addbuttonFontSize"]			= 8;				//	Add button font size
+$EzAdmin_Style["addbuttonAlign"]			= 'center';			//	Add button text alignment ('left', 'right' or 'center')
+$EzAdmin_Style["addbuttonValign"]			= 'middle';			//	Add button text vertical alignment ('top', 'bottom' or 'center')
+$EzAdmin_Style["addbuttonTextColour"]		= '#0000FF';		//	Must be an RGB numeric value, prefixed with '#'
+$EzAdmin_Style["addbuttonColour"]			= 'White';			//	addbuttoncolour, addbuttonborder and addbuttonbordercolour
+$EzAdmin_Style["addbuttonBorder"]			= 0;				//		are used only if the script is unable to generate or even
+$EzAdmin_Style["addbuttonBorderColour"]		= 'Black';			//		simulate a graphic button and forced to resort to text
+
+$EzAdmin_Style["FirstIcon"]			= 'first_button.gif';
+$EzAdmin_Style["PrevIcon"]			= 'prev_button.gif';
+$EzAdmin_Style["NextIcon"]			= 'next_button.gif';
+$EzAdmin_Style["LastIcon"]			= 'last_button.gif';
+$EzAdmin_Style["EditIcon"]			= 'edit_button.gif';
+$EzAdmin_Style["DeleteIcon"]		= 'del_button.gif';
+$EzAdmin_Style["ViewIcon"]			= 'view_button.gif';
+$EzAdmin_Style["FolderIcon"]		= 'folder_button.gif';
+$EzAdmin_Style["SelectIcon"]		= 'select_button.gif';
+$EzAdmin_Style["ToggleIcon"]		= 'rel_button.gif';
+$EzAdmin_Style["CatIcon"]			= 'cat_button.gif';
+$EzAdmin_Style["UpIcon"]			= 'up.gif';
+$EzAdmin_Style["DownIcon"]			= 'down.gif';
+$EzAdmin_Style["SortIcon"]			= 'sort.gif';
+$EzAdmin_Style["CurrentSortIcon"]	= 'current_sort.gif';
+
+?>
