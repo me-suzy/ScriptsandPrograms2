97ec6f5f4cb97c87754a4273fa50c6a564158913
me-suzy <me.suzana@gmail.com>
2025-08-12 19:55:46 +0300

    Add News Publishing/news_engine_V1.5.0/news_engine_V1.5.0/news_engine/admin/includes/bbcode/bbcode_language.js

---
+++ News Publishing/news_engine_V1.5.0/news_engine_V1.5.0/news_engine/admin/includes/bbcode/bbcode_language.js
@@ -0,0 +1,47 @@
+b_text = "fetten Text einfügen";
+i_text = "kursiven Text einfügen";
+u_text = "unterstrichenen Text einfügen";
+p_text = "Absatz einfügen";
+hr_text = "Horizontale Linie einfügen";
+sub_text = "Tiefgestellten Text einfügen";
+sup_text = "Hochgestellten Text einfügen";
+icq_text = "ICQ-Adresse einfügen";
+
+size_text = "Textgröße ändern";
+font_text = "Schriftart ändern";
+color_text = "Schriftfarbe ändern";
+align_text = "Text-Ausrichtung ändern";
+
+url_text = "Hyperlink einfügen";
+url2_text = "externen Hyperlink einfügen";
+email_text = "Email Adresse einfügen";
+img_text = "Grafik einfügen";
+
+code_text = "HTML Code einfügen";
+php_text = "PHP Code einfügen";
+list_text = "Liste einfügen";
+quote_text = "Zitat einfügen";
+
+norm_text = "einfacher Modus";
+enha_text = "erweiterter Modus";
+
+closecurrent_text = "aktuellen tag schließen";
+closeall_text = "alle tags schließen";
+
+enhanced_only_text = "<< nur im erweiterten Modus verfügbar >>";
+no_tags_text = "<< keine offenen tags vorhanden >>";
+already_open_text = "<< es ist bereit der selbe tag offen >>";
+
+HELPTEXT = "Einfaches Einfügen von BBCode und Smilies.\n\nDiese Kontrollen erlauben das schnelle und einfache Einfügen von BBcode in Nachrichten.\n\nBeim drücken eines Buttons öffnet sich ein Pop-up Fenster,\nwo man den zu formatierenden Text eingeben kann\nDer Text wird dann automatisch an der aktuellen Position des Nachricht angehangen.\n\nUm einen Smilie in die Nachricht einzufügen, muß man einfach auf den Gewünschten klicken, sofern diese entsprechend aktiviert sind.";
+
+tag_prompt = "Gebe einen Text ein:";
+img_prompt = "Gebe hier die Url zum Bild an:";
+
+font_formatter_prompt = "Gebe einen Text ein";
+
+link_text_prompt = "Gebe einen Linknamen ein";
+link_url_prompt = "Gebe die volle Adresse des Links ein";
+link_email_prompt = "Gebe eine Email Adesse ein";
+
+list_type_prompt = "was für eine Liste möchtest du? Gebe 'ol' ein für eine nummerierte Liste oder gar nichts für eine einfache Punktliste.";
+list_item_prompt = "Gebe eine Listepunkt ein.\nGebe nichts ein oder drücke 'Cancel' um die Liste fertigzustellen.";
