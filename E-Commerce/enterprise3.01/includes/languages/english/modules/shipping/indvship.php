3aefb95af8c44c8bcc35b5469f010b3568a45ca6
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:05 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/modules/shipping/indvship.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/modules/shipping/indvship.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  by Farrukh
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_INDVSHIP_TEXT_TITLE', 'Individual Shipping');
+define('MODULE_SHIPPING_INDVSHIP_TEXT_DESCRIPTION', 'Shipping and Handling');
+define('MODULE_SHIPPING_INDVSHIP_TEXT_WAY', 'Flat Shipping Rate');
+?>
