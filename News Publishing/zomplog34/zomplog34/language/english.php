5008742f4a29a02963a7836ad26ccbeea544b51c
me-suzy <me.suzana@gmail.com>
2025-08-12 21:08:17 +0300

    Add News Publishing/zomplog34/zomplog34/language/english.php

---
+++ News Publishing/zomplog34/zomplog34/language/english.php
@@ -0,0 +1,181 @@
+<?
+
+// This is the English language file
+
+//----------------- English -----------------------//
+
+// Front end -> get.php
+$lang_number_comments = "comments"; //number of comments under posts
+$lang_no_results = "no results found.";
+$lang_read_more = "read more...";
+$lang_listen = "listen to mp3...";
+$lang_view = "view movie...";
+$lang_searched_for = "You have searched for";
+$lang_posts_by = "Posts by";
+
+// Front end -> sidemenu.php
+$lang_search = "Search";
+$lang_pages = "Pages";
+$lang_recent_posts = "Recent posts";
+$lang_categories = "Categories";
+$lang_authors = "Authors";
+$lang_rss_feed = "Rss feed";
+$lang_admin = "Administrator";
+$lang_login = "Log in";
+$lang_register = "Register";
+$lang_powered = "Powered by";
+$lang_archive = "Archive";
+
+// Front end -> detail.php
+$lang_write_comment = "Write a comment";
+$lang_name = "name";
+$lang_comment = "comment";
+$lang_comments = "Comments";
+$lang_no_comments_found = "no comments found";
+$lang_on = "on"; // as in "comment posted on date"
+$lang_error_name = "You did not fill out your name";
+$lang_error_comment = "You did not write a comment";
+$lang_error_comment_success = "You have succesfully added a comment.";
+
+// Front end -> page.php
+
+$lang_contact = "Contact";
+$lang_your_email_sent = "Your email has been sent!";
+$lang_name_mail = "Name";
+$lang_email_mail = "Email";
+$lang_additional_remarks = "Additional Remarks";
+
+// Back end -> menu.php
+$lang_entries = "Posts";
+$lang_newentry = "new post";
+$lang_edit_delete = "edit / delete post";
+$lang_profile = "Personal Profile";
+$lang_editprofile = "edit personal profile";
+$lang_controlpanel = "Admin Control Panel";
+$lang_settings = "system settings";
+$lang_moblog_settings = "moblog settings";
+$lang_content_management = "Content Management";
+$lang_manage_users = "manage users";
+$lang_manage_categories = "manage categories";
+$lang_manage_comments = "manage comments";
+$lang_pages = "Pages";
+$lang_newpage = "new page";
+$lang_edit_delete_page = "edit / delete page";
+$lang_upload_images = "upload images";
+$lang_custom_upload_tool = "custom upload tool";
+
+// Back end -> members.php
+$lang_total_number_posts = "Total number of posts";
+$lang_total_number_pages = "Total number of pages";
+$lang_total_number_users = "Total number of users";
+$lang_total_number_categories = "Total number of categories";
+$lang_total_number_comments = "Total number of comments";
+$lang_lastpost = "Your last post was";
+$lang_site_statistics = "Site Statistics";
+$lang_your_statistics = "Your Statistics";
+
+// Back end -> header.php
+
+$lang_members_area = "dashboard";
+$lang_view_site = "view site";
+$lang_logout = "log out";
+
+// Back end -> entries (newentry.php, editor.php, entry.php)
+
+$lang_title = "Title";
+$lang_text = "Text";
+$lang_extended = "Extended (optional)";
+$lang_upload_image = "Upload image";
+$lang_full_width = "Display image(s) full width";
+$lang_category = "Category";
+$lang_choosemaincat = "Choose main category";
+$lang_replace_image = "Replace image";
+$lang_media = "URL of media file";
+$lang_choose_type = "Choose media type";
+$lang_entries_by_others = "Entries by others";
+$lang_your_entries = "Your entries";
+$lang_order_by = "Order by";
+$lang_date = "date";
+$lang_number_of_images = "Number of images:";
+
+// Back end -> edit/delete
+$lang_edit = "edit";
+$lang_delete = "delete";
+
+// Back end -> User Management (profile.php, users.php)
+$lang_username = "username";
+$lang_password = "password";
+$lang_retype_password = "retype password";
+$lang_email = "email";
+$lang_use_email_moblog = "use this email-adress for moblogging";
+$lang_what_is_moblogging = "What is moblogging?";
+$lang_about_me = "about me";
+$lang_administrator = "administrator";
+$lang_user = "user";
+$lang_create_user = "create a new user";
+$lang_current_users = "current users";
+
+// Back end -> Settings (settings.php)
+$lang_system_settings = "System Settings";
+$lang_main_config = "Main Configuration";
+$lang_weblog_title = "weblog title";
+$lang_comments_on = "comments on";
+$lang_categories_on = "categories on";
+
+$lang_mediafile_on = "mediafiles on";
+$lang_activate_site_parts = "Activate site parts";
+$lang_admin_welcome_message = "Members area welcome message";
+$lang_site_welcome_message = "Website welcome message";
+$lang_messages = "Welcome messages";
+$lang_dashboard_rss_feed = "RSS feed URL for dashboard";
+$lang_allow_register = "anyone can register";
+
+$lang_choose_language = "choose language";
+$lang_choose_skin = "choose skin";
+$lang_posts_per_page = "posts per page";
+$lang_scroll = "scroll";
+$lang_scroll_explained = "(the number of pagelinks in the previous-next part)";
+$lang_upload_on = "upload on";
+$lang_max_upload = "max upload size";
+$lang_pages_admin = "who can create/edit pages?";
+$lang_pages_admin_only = "admin only";
+$lang_date_time = "Date/Time";
+$lang_date_time_format = "date/time format";
+
+$lang_thumbnail_width = "thumbnail width";
+$lang_thumbnail_fullwidth = "maximum width";
+
+// Back end -> category.php
+$lang_new_category = "Create new category";
+$lang_category_name = "category name";
+$lang_edit_category = "Edit Category";
+$lang_choose_category = "choose category";
+$lang_edit_category_name = "edit category name";
+$lang_delete_category = "Delete Category";
+
+// Back end -> comments.php, comments_editor.php
+$lang_edit_comment = "Edit comment";
+$lang_comment_name = "name";
+$lang_comment = "comment";
+$lang_ban = "ban";
+
+// Back end -> newpage.php
+$lang_add_contact_form = "add contact form?";
+$lang_form_email = "form email";
+
+//errors, well actually the messages if an action was succesful...
+$lang_error1 = "Post has been succesfully added!";
+$lang_error2 = "Post has been succesfully updated!";
+$lang_error3 = "Data has been succesfully deleted.";
+$lang_error4 = "User profile has been succesfully updated!";
+$lang_error5 = "Settings have been updated succesfully!";
+$lang_error6 = "Category has been succesfully added!";
+$lang_error7 = "Category has been succesfully modified!";
+$lang_error8 = "Comment has been succesfully modified!";
+$lang_error9 = "User has been created!";
+$lang_error10 = "Page has been succesfully added!";
+$lang_error11 = "Page has been succesfully updated!";
+$lang_error12 = "Ip-adress has been succesfully banned!";
+$lang_error13 = "Ip-adress has been succesfully unbanned!";
+
+?>
