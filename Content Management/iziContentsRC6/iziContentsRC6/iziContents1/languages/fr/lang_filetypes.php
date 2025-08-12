570d3d025c5e68c17a3059171a978ccb0cad2e1d
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:28 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_filetypes.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_filetypes.php
@@ -0,0 +1,38 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'maintenir les types de fichier';
+
+//  List Headings
+$GLOBALS["tFileCategory"] = 'Catégorie';
+$GLOBALS["tFileType"] = 'Type de fichier';
+$GLOBALS["tMIMEType"] = 'Les types MIME';
+$GLOBALS["tFileIcon"] = 'Icone';
+
+//  Categories
+$GLOBALS["tFileCatBackup"] = 'Sauvegarde';
+$GLOBALS["tFileCatDownload"] = 'Téléchargement';
+$GLOBALS["tFileCatImage"] = 'Image';
+$GLOBALS["tFileCatScript"] = 'Scripte';
+
+//  List Functions
+$GLOBALS["tAddNewFiletype"] = 'Ajouter nouveau type de fichier';
+$GLOBALS["tViewFiletype"] = 'Vue des types de fichier';
+$GLOBALS["tEditFiletype"] = 'Editer le type du fichier';
+$GLOBALS["tDeleteFiletype"] = 'Effacer le type du fichier';
+
+//  Form Block Titles
+$GLOBALS["thFiletypeGeneral"] = 'Détaille du type du fichier';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Cette forme vous permez de définir le type de fichier de coutume pour l usage dans ezContents.';
+$GLOBALS["hFileCategory"] = 'La catégorie détermine quelles fonctions d entretien de fichier sont permises utiliser ce type de fichier.';
+$GLOBALS["hFileType"] = 'Ceci est le code du type du fichier, basé sur l extension de fichier.';
+$GLOBALS["hMIMEType"] = 'Ceci est une liste des types de MIME valides pour les fichiers avec cette extension.<br /><br />Vous pouvez entrer plusieurs MIME tape séparé par le point virgule (<b>;</b>) où il peut y avoir plusieurs MIME Tape associé avec cette extension de fichier particulière.<br />Un exemple de ceci est les fichiers de jpeg qui peuvent avoir un type de MIME de <b>image/jpeg</b> or <b>image/pjpeg</b>.';
+$GLOBALS["hFileIcon"] = 'Ceci est une icône pour utiliser pour l exposition du type de fichier.';
+
+//  Error Messages
+$GLOBALS["eNoFiletype"] = 'Le code du type de fichier ne peut pas être gauche vide.';
+$GLOBALS["eNoMIMEType"] = 'MIME le type ne peut pas être gauche vide.';
+
+?>
