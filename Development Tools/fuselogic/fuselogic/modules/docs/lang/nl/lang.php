bc627b0ca9761fb56195d7621cc44fce289f9639
me-suzy <me.suzana@gmail.com>
2025-08-12 09:20:44 +0300

    Add Development Tools/fuselogic/fuselogic/modules/docs/lang/nl/lang.php

---
+++ Development Tools/fuselogic/fuselogic/modules/docs/lang/nl/lang.php
@@ -0,0 +1,71 @@
+<?
+$lang['encoding']   = 'iso-8859-15';
+$lang['locales']    = array(
+                        'nld',
+                        'nl_NL@euro',
+                        'nl_NL',
+                        'nl',
+                        'dutch',
+                      );
+
+$lang['btn_edit']   = 'Pagina aanpassen';
+$lang['btn_search'] = 'Zoeken';
+$lang['btn_save']   = 'Opslaan';
+$lang['btn_preview']= 'Preview';
+$lang['btn_top']    = 'Terug naar boven';
+$lang['btn_revs']   = 'Oude revisies';
+$lang['btn_recent'] = 'Recente wijzigingen';
+$lang['btn_upload'] = 'Upload';
+$lang['btn_cancel'] = 'Annuleren';
+$lang['btn_index']  = 'Index';
+
+$lang['txt_upload'] = 'Selecteer een bestand om te uploaden';
+$lang['txt_filename'] = 'Vul wiki-naam in (optioneel)';
+
+$lang['mediaselect'] = 'Mediafile selectie';
+$lang['fileupload']  = 'Mediafile upload';
+$lang['uploadsucc']  = 'Upload geslaagd';
+$lang['uploadfail']  = 'Upload mislukt. Misschien verkeerde permissies?';
+
+$lang['hits']       = 'Hits';
+$lang['quickhits']  = 'Overeenkomende pagina namen';
+$lang['toc']        = 'Inhoudsopgave';
+$lang['current']    = 'huidige';
+$lang['diff']       = 'toon verschillen met huidige revisie';
+$lang['line']       = 'Lijn';
+$lang['breadcrumb'] = 'Geschiedenis';
+$lang['lastmod']    = 'Laatst gewijzigd';
+
+$lang['mail_newpage'] = '[DokuWiki] pagina toegevoegd:';
+$lang['mail_changed'] = '[DokuWiki] pagina gewijzigd:';
+
+$lang['qb_bold']    = 'Vette tekst';
+$lang['qb_italic']  = 'Cursieve tekst';
+$lang['qb_underl']  = 'Onderstreepte tekst';
+$lang['qb_code']    = 'Code tekst';
+$lang['qb_h1']      = 'Niveau 1 kop';
+$lang['qb_h2']      = 'Niveau 2 kop';
+$lang['qb_h3']      = 'Niveau 3 kop';
+$lang['qb_h4']      = 'Niveua 4 kop';
+$lang['qb_h5']      = 'Niveau 5 kop';
+$lang['qb_link']    = 'Interne link';
+$lang['qb_extlink'] = 'Externe link';
+$lang['qb_hr']      = 'Horizontale lijn';
+$lang['qb_ol']      = 'Geordende lijst item';
+$lang['qb_ul']      = 'Ongeordende lijst item';
+$lang['qb_media']   = 'Voeg plaatjes en andere bestanden toe';
+
+$lang['btn_source']	= 'Toon pagina broncode';
+$lang['btn_show']	= 'Toon pagina';
+$lang['btn_create']	= 'Maak deze pagina aan';
+$lang['btn_secedit']	= 'Aanpassen';
+$lang['lockedby']	= 'Momenteel in gebruik door';
+$lang['lockexpire']	= 'Exclusief gebruiksrecht vervalt op';
+$lang['willexpire']	= 'Je exclusieve gebruiksrecht voor het aanpassen van deze pagina verloopt over een minuut.\nOm problemen te voorkomen klik op de Preview knop om de exclusieve gebruiksrechten te verlengen.';
+$lang['notsavedyet']	= 'Er zijn op dit moment wijzigingen die nog niet zijn opgeslagen, deze zullen verloren gaan.\nWeet u zeker dat u wilt doorgaan?';
+$lang['rssfailed']	= 'Er is een fout opgetreden bij het ophalen van de feed: ';
+$lang['nothingfound']	= 'Er is niets gevonden.';
+$lang['yours']		= 'Jouw versie.';
+$lang['nosmblinks']	= 'Linken naar Windows shares werkt alleen in Microsoft Internet Explorer.\nJe kan de link wel kopiëren en plakken.';
+$lang['qb_alert']	= 'Voer de tekst in die je wilt opmaken.\nDe tekst wordt toegevoegd aan het eind van het document.';
+?>
