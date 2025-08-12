4768783f855203ab33ff134eb4d26a5a8049a889
me-suzy <me.suzana@gmail.com>
2025-08-12 15:19:23 +0300

    Add Flash and PHP/flash_xml_php_gallery/flash_xml_php_gallery/eyelash_files/global.inc.php

---
+++ Flash and PHP/flash_xml_php_gallery/flash_xml_php_gallery/eyelash_files/global.inc.php
@@ -0,0 +1,10 @@
+<?
+$arr_global=array();
+$arr_global["images_folder"]="eyelash_images/";
+$arr_global["images"]=$arr_global["images_folder"] . "big/";
+$arr_global["thumbnails"] = $arr_global["images_folder"] . "small/";
+$arr_global["texts"] = $arr_global["images_folder"] . "texts/";
+$arr_global["admin_username"] = "eye";										
+$arr_global["admin_password"] = "lash";	
+$arr_global["image_max_width"]=500;
+?>
