a57afed529f953bfebe58126475336b639e5288b
me-suzy <me.suzana@gmail.com>
2025-08-12 18:13:00 +0300

    Add Image Galleries/PHP_JK_Aug_01_2005/Includes/Config/i_Login.php

---
+++ Image Galleries/PHP_JK_Aug_01_2005/Includes/Config/i_Login.php
@@ -0,0 +1,13 @@
+<?php 
+
+	//************************************************************************************
+	// This include goes in the /PHPJK/UserArea/Login.php page
+	//************************************************************************************
+	
+	// Login/Password request email
+	// 		the 1: is replaced with the users login
+	//		the 2: is replaced with the users password
+	//		the \n should appear as a line break in the email
+	$CONF_LPemail = "Your login and password have been found!\n\n";
+	$CONF_LPemail .= "Login: 1:\nPassword: 2:";
+?>
