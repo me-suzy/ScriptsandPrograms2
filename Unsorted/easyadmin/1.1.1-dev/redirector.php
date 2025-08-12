141c121b91dc7cf5a79c4c9ead0ef923f6d8d3ea
me-suzy <me.suzana@gmail.com>
2025-08-13 02:53:38 +0300

    Add Unsorted/easyadmin/1.1.1-dev/redirector.php

---
+++ Unsorted/easyadmin/1.1.1-dev/redirector.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Easy Admin :: Javascript redirect for web servers with different defaults</title>
+<script language="Javascript">
+window.location="index.php"
+</script>
+</head>
+<body>
+<br>Click<a href="index.php">here</a> to login.<br><br>
+This Re-Director Is Distrobuted With Easy Admin
+</body>
+</html>
