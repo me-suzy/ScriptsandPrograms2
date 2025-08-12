e4dcb2bb69ac50d52fbf576d32b6ac50fcfce62e
me-suzy <me.suzana@gmail.com>
2025-08-12 11:31:26 +0300

    Add E-Commerce/enterprise3.01/admin/configuration.php

---
+++ E-Commerce/enterprise3.01/admin/configuration.php
@@ -0,0 +1,208 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  require('includes/application_top.php');
+
+  $action = (isset($HTTP_GET_VARS['action']) ? $HTTP_GET_VARS['action'] : '');
+
+  if (escs_not_null($action)) {
+    switch ($action) {
+      case 'save':
+        $configuration_value = escs_db_prepare_input($HTTP_POST_VARS['configuration_value']);
+        $cID = escs_db_prepare_input($HTTP_GET_VARS['cID']);
+
+        escs_db_query("update " . TABLE_CONFIGURATION . " set configuration_value = '" . escs_db_input($configuration_value) . "', last_modified = now() where configuration_id = '" . (int)$cID . "'");
+        // set the WARN_BEFORE_DOWN_FOR_MAINTENANCE to false if DOWN_FOR_MAINTENANCE = true
+        if ( (WARN_BEFORE_DOWN_FOR_MAINTENANCE == 'true') && (DOWN_FOR_MAINTENANCE == 'true') ) {
+        escs_db_query("update " . TABLE_CONFIGURATION . " set configuration_value = 'false', last_modified = '" . NOW . "' where configuration_key = 'WARN_BEFORE_DOWN_FOR_MAINTENANCE'"); }
+        escs_redirect(escs_href_link(FILENAME_CONFIGURATION, 'gID=' . $HTTP_GET_VARS['gID'] . '&cID=' . $cID));
+        break;
+    }
+  }
+
+  $gID = (isset($HTTP_GET_VARS['gID'])) ? $HTTP_GET_VARS['gID'] : 1;
+
+  $cfg_group_query = escs_db_query("select configuration_group_title from " . TABLE_CONFIGURATION_GROUP . " where configuration_group_id = '" . (int)$gID . "'");
+  $cfg_group = escs_db_fetch_array($cfg_group_query);
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
+<title><?php echo TITLE; ?></title>
+<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
+<script language="javascript" src="includes/general.js"></script>
+<script type="text/javascript" src="includes/browser.js">/************************************************ Jim's DHTML Menu v5.0- © Jim Salyer (jsalyer@REMOVETHISmchsi.com)* Visit Dynamic Drive: http://www.dynamicdrive.com for script and instructions* This notice must stay intact for use***********************************************/</script><script type="text/javascript" src="config.js"></script></head>
+<body onload="init();" marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF" onload="SetFocus();">
+<!-- header //-->
+<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+<!-- header_eof //-->
+
+<div align="center">
+<table border="0" cellspacing="0" cellpadding="4" width="65%">
+<tr>
+<td class="helpText">
+<? if($gID == '1')
+	{
+?>
+This screen allows you to edit basic store settings.  To edit a setting, click on the configuration setting
+you wish to edit.  An explanation of each feature is then displayed.  Click the edit button on the right side of the screen
+to edit the value of the configuration setting.  Now, change the text in the input box and click &quot;update&quot;.
+<?
+	}
+	else if($gID == '7')
+	{
+?>
+This screen allows you to edit your shipping and packaing settings. To edit a setting, click on the configuration setting you wish to edit. An explanation of each feature is then displayed. Click the edit button on the right side of the screen to edit the value of the configuration setting. Now, change the text in the input box and click "update".
+<?
+	}
+	else if($gID == '9')
+	{
+?>
+This screen allows you to edit settings relating to stocking your merchandise. To edit a setting, click on the configuration setting you wish to edit. An explanation of each feature is then displayed. Click the edit button on the right side of the screen to edit the value of the configuration setting. Now, change the text in the input box and click "update".
+<?
+	}
+	else if($gID == '13')
+	{
+?>
+This screen allows you to control downloadable products, such as software and ebooks. To edit a setting, click on the configuration setting you wish to edit. An explanation of each feature is then displayed. Click the edit button on the right side of the screen to edit the value of the configuration setting. Now, change the text in the input box and click "update".
+<?
+	}
+?>
+</td>
+</tr>
+</table>
+</div>
+
+<!-- body //-->
+<table border="0" width="100%" cellspacing="2" cellpadding="2">
+  <tr>
+    <td width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+    <td width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="pageHeading"><?php echo $cfg_group['configuration_group_title']; ?></td>
+            <td class="pageHeading" align="right"><?php echo escs_draw_separator('pixel_trans.gif', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
+          </tr>
+        </table></td>
+      </tr>
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+              <tr class="dataTableHeadingRow">
+                <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_CONFIGURATION_TITLE; ?></td>
+                <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_CONFIGURATION_VALUE; ?></td>
+                <td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_ACTION; ?>&nbsp;</td>
+              </tr>
+<?php
+  $configuration_query = escs_db_query("select configuration_id, configuration_title, configuration_value, use_function from " . TABLE_CONFIGURATION . " where configuration_group_id = '" . (int)$gID . "' order by sort_order");
+  while ($configuration = escs_db_fetch_array($configuration_query)) {
+    if (escs_not_null($configuration['use_function'])) {
+      $use_function = $configuration['use_function'];
+      if (ereg('->', $use_function)) {
+        $class_method = explode('->', $use_function);
+        if (!is_object(${$class_method[0]})) {
+          include(DIR_WS_CLASSES . $class_method[0] . '.php');
+          ${$class_method[0]} = new $class_method[0]();
+        }
+        $cfgValue = escs_call_function($class_method[1], $configuration['configuration_value'], ${$class_method[0]});
+      } else {
+        $cfgValue = escs_call_function($use_function, $configuration['configuration_value']);
+      }
+    } else {
+      $cfgValue = $configuration['configuration_value'];
+    }
+
+    if ((!isset($HTTP_GET_VARS['cID']) || (isset($HTTP_GET_VARS['cID']) && ($HTTP_GET_VARS['cID'] == $configuration['configuration_id']))) && !isset($cInfo) && (substr($action, 0, 3) != 'new')) {
+      $cfg_extra_query = escs_db_query("select configuration_key, configuration_description, date_added, last_modified, use_function, set_function from " . TABLE_CONFIGURATION . " where configuration_id = '" . (int)$configuration['configuration_id'] . "'");
+      $cfg_extra = escs_db_fetch_array($cfg_extra_query);
+
+      $cInfo_array = array_merge($configuration, $cfg_extra);
+      $cInfo = new objectInfo($cInfo_array);
+    }
+
+    if ( (isset($cInfo) && is_object($cInfo)) && ($configuration['configuration_id'] == $cInfo->configuration_id) ) {
+      echo '                  <tr id="defaultSelected" class="dataTableRowSelected" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . escs_href_link(FILENAME_CONFIGURATION, 'gID=' . $HTTP_GET_VARS['gID'] . '&cID=' . $cInfo->configuration_id . '&action=edit') . '\'">' . "\n";
+    } else {
+      echo '                  <tr class="dataTableRow" onmouseover="rowOverEffect(this)" onmouseout="rowOutEffect(this)" onclick="document.location.href=\'' . escs_href_link(FILENAME_CONFIGURATION, 'gID=' . $HTTP_GET_VARS['gID'] . '&cID=' . $configuration['configuration_id']) . '\'">' . "\n";
+    }
+?>
+                <td class="dataTableContent"><?php echo $configuration['configuration_title']; ?></td>
+                <td class="dataTableContent"><?php echo htmlspecialchars($cfgValue); ?></td>
+                <td class="dataTableContent" align="right"><?php if ( (isset($cInfo) && is_object($cInfo)) && ($configuration['configuration_id'] == $cInfo->configuration_id) ) { echo escs_image(DIR_WS_IMAGES . 'icon_arrow_right.gif', ''); } else { echo '<a href="' . escs_href_link(FILENAME_CONFIGURATION, 'gID=' . $HTTP_GET_VARS['gID'] . '&cID=' . $configuration['configuration_id']) . '">' . escs_image(DIR_WS_IMAGES . 'icon_info.gif', IMAGE_ICON_INFO) . '</a>'; } ?>&nbsp;</td>
+              </tr>
+<?php
+  }
+?>
+            </table></td>
+<?php
+  $heading = array();
+  $contents = array();
+
+  switch ($action) {
+    case 'edit':
+      $heading[] = array('text' => '<b>' . $cInfo->configuration_title . '</b>');
+
+      if ($cInfo->set_function) {
+        eval('$value_field = ' . $cInfo->set_function . '"' . htmlspecialchars($cInfo->configuration_value) . '");');
+      } else {
+        $value_field = escs_draw_input_field('configuration_value', $cInfo->configuration_value);
+      }
+
+      $contents = array('form' => escs_draw_form('configuration', FILENAME_CONFIGURATION, 'gID=' . $HTTP_GET_VARS['gID'] . '&cID=' . $cInfo->configuration_id . '&action=save'));
+      $contents[] = array('text' => TEXT_INFO_EDIT_INTRO);
+      $contents[] = array('text' => '<br><b>' . $cInfo->configuration_title . '</b><br>' . $cInfo->configuration_description . '<br>' . $value_field);
+      $contents[] = array('align' => 'center', 'text' => '<br>' . escs_image_submit('button_update.gif', IMAGE_UPDATE) . '&nbsp;<a href="' . escs_href_link(FILENAME_CONFIGURATION, 'gID=' . $HTTP_GET_VARS['gID'] . '&cID=' . $cInfo->configuration_id) . '">' . escs_image_button('button_cancel.gif', IMAGE_CANCEL) . '</a>');
+      break;
+    default:
+      if (isset($cInfo) && is_object($cInfo)) {
+        $heading[] = array('text' => '<b>' . $cInfo->configuration_title . '</b>');
+
+        $contents[] = array('align' => 'center', 'text' => '<a href="' . escs_href_link(FILENAME_CONFIGURATION, 'gID=' . $HTTP_GET_VARS['gID'] . '&cID=' . $cInfo->configuration_id . '&action=edit') . '">' . escs_image_button('button_edit.gif', IMAGE_EDIT) . '</a>');
+        $contents[] = array('text' => '<br>' . $cInfo->configuration_description);
+        $contents[] = array('text' => '<br>' . TEXT_INFO_DATE_ADDED . ' ' . escs_date_short($cInfo->date_added));
+        if (escs_not_null($cInfo->last_modified)) $contents[] = array('text' => TEXT_INFO_LAST_MODIFIED . ' ' . escs_date_short($cInfo->last_modified));
+      }
+      break;
+  }
+
+  if ( (escs_not_null($heading)) && (escs_not_null($contents)) ) {
+    echo '            <td width="25%" valign="top">' . "\n";
+
+    $box = new box;
+    echo $box->infoBox($heading, $contents);
+
+    echo '            </td>' . "\n";
+  }
+?>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+<!-- body_text_eof //-->
+  </tr>
+</table>
+<!-- body_eof //-->
+
+<!-- footer //-->
+<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br>
+</body>
+</html>
+<?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
