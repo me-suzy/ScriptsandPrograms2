302b13cb3ecf0f1eebed69469a99b9d5b2431767
me-suzy <me.suzana@gmail.com>
2025-08-12 09:08:03 +0300

    Add Development Tools/fuselogic/fuselogic/modules/adodb462/module_name.adodb.php

---
+++ Development Tools/fuselogic/fuselogic/modules/adodb462/module_name.adodb.php
@@ -0,0 +1,8 @@
+<?php
+
+$FL_MODULE_SETTING['module_name'] = 'adodb';
+$FL_MODULE_SETTING['sub_module']['_main'] = 'fuselogic_adodb.php';
+$FL_MODULE_SETTING['sub_module']['_session'] = 'fuselogic_session.php';
+$FL_MODULE_SETTING['sub_module']['session_test'] = 'fuselogic_test.php';
+
+?>
