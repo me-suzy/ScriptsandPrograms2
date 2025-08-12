3b0dae25eb67bffab2eb42718ac4f8890a09dc9d
me-suzy <me.suzana@gmail.com>
2025-08-12 12:01:19 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/modules/payment/cod.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/modules/payment/cod.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_PAYMENT_COD_TEXT_TITLE', 'Nachnahme');
+  define('MODULE_PAYMENT_COD_TEXT_DESCRIPTION', 'Nachnahme');
+?>
