d05d55f4b259195d9bee64a1c23d876b42c6483c
me-suzy <me.suzana@gmail.com>
2025-08-12 08:31:59 +0300

    Add Counters/DB-Download-Counter-v1.1/test.php

---
+++ Counters/DB-Download-Counter-v1.1/test.php
@@ -0,0 +1,19 @@
+<HTML>
+<HEAD>
+<TITLE>DB Download Counter Test Script</TITLE>
+</HEAD>
+<BODY>
+
+<font face="Verdana" size="2"><b>Download Counter Test</b>
+<?php
+include "download.php";
+$download = new download( "http://www.demonboard.co.uk/scripts/dcounter/DB-Download-Counter-v1.1.zip", "DB_Download_Counter", "Click the above button to download DB Download Counter for free!" );
+?>
+</font>
+
+</BODY>
+</HTML>
+<!---
+(C) 2004 Demon Board Ltd. All Rights Reserved.
+Please see Read Me.txt for more information on how to use the script!
+--->
