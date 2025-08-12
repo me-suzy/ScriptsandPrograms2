1e4a90837b78e6f22377916278fa3dabe5db202d
me-suzy <me.suzana@gmail.com>
2025-08-12 11:52:06 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/affiliate_details.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/affiliate_details.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'The Affiliate Program');
+define('NAVBAR_TITLE_2', 'Edit Affiliate Account');
+define('HEADING_TITLE', 'The Affiliate Program - Your Account Details<br><small>You may edit any information below:</small>');
+?>
