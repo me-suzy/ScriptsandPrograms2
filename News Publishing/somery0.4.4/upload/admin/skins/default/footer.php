754ce340ca34b888314919de6f410ac36d42f52e
me-suzy <me.suzana@gmail.com>
2025-08-12 20:50:39 +0300

    Add News Publishing/somery0.4.4/upload/admin/skins/default/footer.php

---
+++ News Publishing/somery0.4.4/upload/admin/skins/default/footer.php
@@ -0,0 +1,11 @@
+<?php
+// Somery, a weblogging script by Robin de Graaf, copyright 2001-2005
+// Somery is distributed under the Artistic License (see LICENSE.txt)
+//
+// ADMIN/SKINS/DEFAULT/FOOTER.PHP > 03-11-2005
+?>
+
+</div>
+
+</body>
+</html>
