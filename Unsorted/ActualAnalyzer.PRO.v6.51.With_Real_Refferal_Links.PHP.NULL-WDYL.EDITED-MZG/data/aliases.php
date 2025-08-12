596a6fb0be0a69dc00768a1192e5dd4fe99eb72b
me-suzy <me.suzana@gmail.com>
2025-08-13 00:27:18 +0300

    Add Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/data/aliases.php

---
+++ Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/data/aliases.php
@@ -0,0 +1,17 @@
+<?php
+
+/*------------------------------------------------------------------------------
+Add below this block the aliases of your domain
+For example:
+
+$alias[]='babelfish.altavista.com';
+$alias[]='actualscripts.org';
+
+ActualAnalyzer will store the statistics also for actualscripts.net and
+actualscripts.org
+------------------------------------------------------------------------------*/
+$alias[]='babelfish.altavista.com';
+$alias[]='translate.ru';
+$alias[]='translate.google.com';
+
+?>
