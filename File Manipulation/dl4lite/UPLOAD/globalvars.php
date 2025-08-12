115a257c8cecc2e007c1f64ab07c4ae75ba22dee
me-suzy <me.suzana@gmail.com>
2025-08-12 14:15:11 +0300

    Add File Manipulation/dl4lite/UPLOAD/globalvars.php

---
+++ File Manipulation/dl4lite/UPLOAD/globalvars.php
@@ -0,0 +1,122 @@
+<?php
+$CONFIG['allowMime'] = '1';
+$CONFIG['allowRegister'] = '1';
+$CONFIG['allowunknown'] = '1';
+$CONFIG['approve_uploads'] = '1';
+$CONFIG['approvedgroup'] = '4';
+$CONFIG['comments_per_page'] = '30';
+$CONFIG['copyright'] = '1';
+$CONFIG['copystring'] = 'RW::Scripts v4.0 - www.rwscripts.com';
+$CONFIG['dateformat'] = 'j-F-Y g:i a';
+$CONFIG['debuglevel'] = '0';
+$CONFIG['defaultLang'] = '1';
+$CONFIG['defaultSkin'] = '1';
+$CONFIG['default_c_order'] = 'ASC';
+$CONFIG['default_order'] = 'ASC';
+$CONFIG['default_sort'] = 'date';
+$CONFIG['display_thumbs'] = '1';
+$CONFIG['doCustomFields'] = '1';
+$CONFIG['dogdcheck'] = '1';
+$CONFIG['doscriptcheck'] = '1';
+$CONFIG['dupemail'] = '';
+$CONFIG['email'] = '';
+$CONFIG['email_files'] = '1';
+$CONFIG['email_friend_msg'] = 'Dear {friend},
+
+{username} has sent you this email to tell you about a file you may be interested in. The file can be found at {linkurl}.
+
+They also sent you this message
+
+------------------------------------------------
+
+{message}';
+$CONFIG['email_newfile_msg'] = 'This is an email from your download script at {siteurl}
+
+{username} has just uploaded a new file named {filename}. You can view the file at this location: {linkurl}
+
+You do not need to do anything as this file has been entered straight into the database.';
+$CONFIG['email_newuser'] = '1';
+$CONFIG['email_newuser_msg'] = 'Dear {username},
+
+Thankyou for registering an account with our download manager at {siteurl}. You are now an approved member and can use our script and configure it to your tastes using the user control panel';
+$CONFIG['email_newuseradmin_msg'] = 'This is an email from your download script at {siteurl}
+
+{username} has just registered a new account on your download manager. You do not need to do anything and this email is for information purposes only.';
+$CONFIG['email_newuseradminconfirm_msg'] = 'This is an email from your download script at {siteurl}
+
+{username} has just registered a new account on your download manager. This user requires admin approval before they can become an approved member. Please log into your Admin Control Panel to do this: {adminlink}';
+$CONFIG['email_newuserconfirm2_msg'] = 'Dear {username},
+
+Thankyou for registering an account with our download manager at {siteurl}. 
+
+IMPORTANT: Before you are an approved member an admin must validate your account manually. They have been informed they need to do this and you should be approved shortly. 
+
+Once you have been approved by an admin you will be a fully approved member and can configure the download manager to your own tastes using the user control panel';
+$CONFIG['email_newuserconfirm_msg'] = 'Dear {username},
+
+Thankyou for registering an account with our download manager at {siteurl}. 
+
+IMPORTANT: Before you are an approved member you MUST confirm your email address. You can do this very easilly by clicking the following link or copying and pasting it into your browsers address bar:
+
+{confirmlink}
+
+Once you visit the link you will be a fully approved member and can configure the download manager to your own tastes using the user control panel';
+$CONFIG['email_report_msg'] = 'This is an email from your download script at {siteurl}
+
+{username} has just reported a link is broken in your downloads database. The file can be found at:
+{linkurl}';
+$CONFIG['email_unapp'] = '1';
+$CONFIG['email_unappfile_msg'] = 'This is an email from your download script at {siteurl}
+
+{username} has just uploaded a new file named {filename}. You can view the file at this location: {linkurl}
+
+This file requires your approval before it is entered in the database. You can do this by logging into your admin control panel.';
+$CONFIG['guest_uploads'] = '1';
+$CONFIG['guestid'] = '2';
+$CONFIG['hta_pass'] = '';
+$CONFIG['hta_user'] = '';
+$CONFIG['isoffline'] = '0';
+$CONFIG['leech_allownorefer'] = '1';
+$CONFIG['leech_protect'] = '1';
+$CONFIG['links_per_page'] = '30';
+$CONFIG['mailtype'] = 'mail';
+$CONFIG['max_word_length'] = '';
+$CONFIG['newuseremail'] = '0';
+$CONFIG['nopassthrough'] = '1';
+$CONFIG['num_gall_cols'] = '3';
+$CONFIG['num_stats'] = '5';
+$CONFIG['offlinemsg'] = 'Downloads section is currently offline for maintainance testing';
+$CONFIG['overridetype'] = 'none';
+$CONFIG['partial_transfers'] = '1';
+$CONFIG['php_timeout'] = '180';
+$CONFIG['post_max_size'] = '';
+$CONFIG['post_set'] = '';
+$CONFIG['referers'] = 'localhost';
+$CONFIG['regconfirm'] = '0';
+$CONFIG['session'] = '15';
+$CONFIG['sitename'] = 'RW::Download 4.0';
+$CONFIG['sitepath'] = '';
+$CONFIG['siteurl'] = '';
+$CONFIG['smtphost'] = 'localhost';
+$CONFIG['smtppass'] = '';
+$CONFIG['smtpport'] = '25';
+$CONFIG['smtpusername'] = '';
+$CONFIG['speedlimit'] = '';
+$CONFIG['sqldatabase'] = '';
+$CONFIG['sqlhost'] = 'localhost';
+$CONFIG['sqlpassword'] = '';
+$CONFIG['sqlprefix'] = 'dl_';
+$CONFIG['sqlusername'] = '';
+$CONFIG['thumbHeight'] = '150';
+$CONFIG['thumbWidth'] = '150';
+$CONFIG['thumb_generate'] = 'gd2';
+$CONFIG['timeadjust'] = '0';
+$CONFIG['timezone'] = 'GMT';
+$CONFIG['ul_set'] = '8M';
+$CONFIG['upload_limit'] = '0';
+$CONFIG['usegzip'] = '1';
+$CONFIG['user_uploads'] = '1';
+$CONFIG['userfilepath'] = '';
+$CONFIG['userfileurl'] = '';
+$CONFIG['usertype'] = 'rwd4';
+?>
