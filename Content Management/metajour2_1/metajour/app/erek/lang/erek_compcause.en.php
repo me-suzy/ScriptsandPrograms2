4b675db8875c06cfe7d9749c508fcfd4886cca0b
me-suzy <me.suzana@gmail.com>
2025-08-12 06:01:30 +0300

    Add Content Management/metajour2_1/metajour/app/erek/lang/erek_compcause.en.php

---
+++ Content Management/metajour2_1/metajour/app/erek/lang/erek_compcause.en.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eReklamation
+ * @subpackage lang
+ * $Id: erek_compcause.en.php,v 1.1 2005/02/16 05:04:58 jan Exp $
+ */
+
+$LANG['name'] = 'Causes';
+$LANG['label_name'] = 'Cause';
+?>
