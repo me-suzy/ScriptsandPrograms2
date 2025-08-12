7c5302f33880385972efcc3e1a5b7ed9d7d51292
me-suzy <me.suzana@gmail.com>
2025-08-12 14:39:23 +0300

    Add File Manipulation/PHPfileNavigator/PHPfileNavigator-2.1.0/PHPfileNavigator/navega.php

---
+++ File Manipulation/PHPfileNavigator/PHPfileNavigator-2.1.0/PHPfileNavigator/navega.php
@@ -0,0 +1,52 @@
+<?php
+/****************************************************************************
+* navega.php
+*
+* Precarga de los includes para la ejecución de navega.inc.php
+*
+
+PHPfileNavigator versión 2.0.0
+
+Copyright (C) 2004-2005 Lito <lito@eordes.com>
+
+http://phpfilenavigator.litoweb.net/
+
+Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los
+términos de la Licencia Pública General de GNU según es publicada por la Free
+Software Foundation, bien de la versión 2 de dicha Licencia o bien (según su
+elección) de cualquier versión posterior. 
+
+Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA
+GARANTÍA, incluso sin la garantía MERCANTIL implícita o sin garantizar la
+CONVENIENCIA PARA UN PROPÓSITO PARTICULAR. Véase la Licencia Pública General de
+GNU para más detalles. 
+
+Debería haber recibido una copia de la Licencia Pública General junto con este
+programa. Si no ha sido así, escriba a la Free Software Foundation, Inc., en
+675 Mass Ave, Cambridge, MA 02139, EEUU. 
+*******************************************************************************/
+
+include ('paths.php');
+include_once ($paths['include'].'basicweb.php');
+
+session_write_close();
+
+$tempo->rexistra('precarga');
+
+include ($paths['plantillas'].'cab.inc.php');
+
+$tempo->rexistra('cabeceira');
+
+include ($paths['web'].'opcions.inc.php');
+
+$tempo->rexistra('opcions');
+
+include_once ($paths['include'].'class_imaxes.php');
+$imaxes = new PFN_Imaxes($conf);
+
+include ($paths['web'].'navega.inc.php');
+
+$tempo->rexistra('navega');
+
+include ($paths['plantillas'].'pe.inc.php');
+?>
