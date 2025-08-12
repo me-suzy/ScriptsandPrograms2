f7da1e118b85cec3575447a94331e74154846caa
me-suzy <me.suzana@gmail.com>
2025-08-12 23:51:40 +0300

    Add Site Recommendation/easiest-tell-a-friend/page.php

---
+++ Site Recommendation/easiest-tell-a-friend/page.php
@@ -0,0 +1,8 @@
+<html><head>
+<title>My page</title>
+<script language="JavaScript" src="w4ftell.js"></script>
+</head>
+<body>
+<a href="javascript:newWindow('sendpage.php?'+document.location.href,'tell',400,300,'')">Tell-a-friend about this page</a>
+</body>
+</html>
