232139db5abe6c041469d224ebfd6801cbb34ce6
me-suzy <me.suzana@gmail.com>
2025-08-12 11:43:20 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/stats_products_viewed.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/stats_products_viewed.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Best Viewed Products');
+
+define('TABLE_HEADING_NUMBER', 'No.');
+define('TABLE_HEADING_PRODUCTS', 'Products');
+define('TABLE_HEADING_VIEWED', 'Viewed');
+?>
