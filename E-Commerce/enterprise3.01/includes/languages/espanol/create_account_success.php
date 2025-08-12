d640c1f44aed33fc4e654c1e43c536915d2f599e
me-suzy <me.suzana@gmail.com>
2025-08-12 11:56:49 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/create_account_success.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/create_account_success.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Crear una Cuenta');
+define('NAVBAR_TITLE_2', 'Exito');
+define('HEADING_TITLE', 'Su cuenta ha sido creada!');
+define('TEXT_ACCOUNT_CREATED', 'Enhorabuena! Su cuenta ha sido creada con &eacute;xito! Ahora puede disfrutar de las ventajas de disponer de una cuenta para mejorar su navegaci&oacute;n en nuestro catalogo. Si tiene <small><b>CUALQUIER</b></small> pregunta sobre el funcionamiento del catalogo, por favor comuniquela al <a href="' . escs_href_link(FILENAME_CONTACT_US) . '">encargado</a>.<br><br>Se ha enviado una confirmaci&oacute;n a la direcci&oacute;n de correo que nos ha proporcionado. Si no lo ha recibido en 1 hora pongase en contacto con <a href="' . escs_href_link(FILENAME_CONTACT_US) . '">nosotros</a>.');
+?>
