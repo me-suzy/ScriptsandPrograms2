42a68da628347df29598f59b0353ef05d15405db
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:53 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_sideoversigtmedsgemulighed_69.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_sideoversigtmedsgemulighed_69.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 69;
+function objfile_69 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Sideoversigt med søgemulighed";
+$objdata['subname'] = "";
+$objdata['extension'] = "indexadv";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
