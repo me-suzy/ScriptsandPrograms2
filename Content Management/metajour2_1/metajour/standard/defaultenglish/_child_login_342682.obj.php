3a49fea6473a10d08658e459cec60b24ad1b1746
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:46 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_login_342682.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_login_342682.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342682;
+function objfile_342682 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Login";
+$objdata['subname'] = "";
+$objdata['extension'] = "login";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
