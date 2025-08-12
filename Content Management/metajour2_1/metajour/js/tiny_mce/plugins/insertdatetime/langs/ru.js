f3d882f39a98c46e90c6d15f80c0badcdc5fb6b4
me-suzy <me.suzana@gmail.com>
2025-08-12 06:43:26 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/insertdatetime/langs/ru.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/insertdatetime/langs/ru.js
@@ -0,0 +1,12 @@
+// RU lang variables cp1251
+
+tinyMCE.addToLang('',{
+insertdate_def_fmt : '%d %m %Y',
+inserttime_def_fmt : '%H:%M:%S',
+insertdate_desc : 'Âñòàâèòü äàòó',
+inserttime_desc : 'Âñòàâèòü âðåìÿ',
+inserttime_months_long : new Array("ÿíâàðÿ", "ôåâðàëÿ", "ìàðòà", "àïðåëÿ", "ìàÿ", "èþíÿ", "èþëÿ", "àâãóñòà", "ñåíòÿáðÿ", "îêòÿáðÿ", "íîÿáðÿ", "äåêàáðÿ"),
+inserttime_months_short : new Array("ÿíâ", "ôåâ", "ìàðò", "àïð", "ìàé", "èþíü", "èþëü", "àâã", "ñåíò", "îêò", "íáð", "äåê"),
+inserttime_day_long : new Array("âîñêðåñåíüå", "ïîíåäåëüíèê", "âòîðíèê", "ñðåäà", "÷åòâåðã", "ïÿòíèöà", "ñóááîòà", "âîñêðåñåíüå"),
+inserttime_day_short : new Array("âñê", "ïí", "âò", "ñð", "÷òâ", "ïòí", "ñá", "âñê")
+});
