b0580fd2139e3d2872f51148dcc7303dbb9a8a6b
me-suzy <me.suzana@gmail.com>
2025-08-12 04:01:43 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_ru.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/lang_search_ru.php
@@ -0,0 +1,16 @@
+<?
+
+$GLOBALS["module_title"]	= 'Ïîèñê ïî ñàéòó';
+$GLOBALS["search_keywords"]	= 'Êëþ÷åâûå ñëîâà äëÿ ïîèñêà';
+$GLOBALS["search_OR"]		= 'Ëþáàÿ êîìáèíàöèÿ èç êëþ÷åâûõ ñëîâ';
+$GLOBALS["search_AND"]		= 'Âñå êëþ÷åâûå ñëîâà';
+$GLOBALS["search_titles"]	= 'Ïîèñê çàãîëîâêîâ';
+$GLOBALS["search_teasers"]	= 'Ïîèñê àííîòàöèé';
+$GLOBALS["search_contents"]	= 'Ñîäåðæàíèå';
+$GLOBALS["search_button"]	= 'Ïîèñê';
+
+$GLOBALS["eEnterKeywords"]	= 'Ïîæàëóéñòà ââåäèòå êëþ÷åâûå ñëîâà äëÿ ïîèñêà.';
+$GLOBALS["eEnterElements"]	= 'Ïîæàëóéñòà óêàæèòå ýëåìåíòû ñòðàíèöû, â êîòîðûõ âåñòè ïîèñê.';
+$GLOBALS["eNoResults"]		= 'Íè÷åãî íå íàéäåíî.';
+
+?>
