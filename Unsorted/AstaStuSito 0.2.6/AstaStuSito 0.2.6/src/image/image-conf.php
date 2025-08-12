663720ecd004d5e06b9dd4b6f183c9db59cfdec0
me-suzy <me.suzana@gmail.com>
2025-08-13 00:52:13 +0300

    Add Unsorted/AstaStuSito 0.2.6/AstaStuSito 0.2.6/src/image/image-conf.php

---
+++ Unsorted/AstaStuSito 0.2.6/AstaStuSito 0.2.6/src/image/image-conf.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+AstaStuSito ver 0.2.6
+Copyright (C) 2001-2002 isazi <isazi@olografix.org>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+# STYLE
+
+# Css of the image section (insert absolute path)
+$CSS = "http://localhost/default.css";
+
+
+# IMAGES SECTION
+
+# Images's absolute path
+$ImageUrl = "http://localhost/image";
+# Image's dir
+$ImageDir = ".";
+# Page for images's archive
+$ImagePage = "./default.txt";
+# Title for images's index
+$ImageHtmlTitle = "Image Gallery";
+# Short paragraph before image gallery
+$ImageHtmlPar = "My gallery of pictures.";
+# Index path
+$ImageHtmlName = "./index.html";
+# Number of images in a row
+$ImageRow = 4;
+
+?>
