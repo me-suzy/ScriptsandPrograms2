741852df419724f1938d922d174110bac1802e91
me-suzy <me.suzana@gmail.com>
2025-08-12 10:28:14 +0300

    Add Discussion Boards/board2/ukiboard/languages/greek.php

---
+++ Discussion Boards/board2/ukiboard/languages/greek.php
@@ -0,0 +1,56 @@
+<?php
+
+$lang['Encoding'] = "windows-1253";
+$lang['Administrator'] = "Äéá÷åéñéóôÞò";
+$lang['Home'] = "ÊåíôñéêÞ";
+$lang['Settings'] = "Ñõèìßóåéò";
+$lang['Language'] = "Ãëþóóá";
+$lang['Theme'] = "ÈÝìá";
+$lang['Adminemail'] = "e-mail äéá÷åéñéóôÞ";
+$lang['Perpage'] = "ÈÝìáôá áíÜ óåëßäá";
+
+$lang['Category'] = "Êáôçãïñßá";
+$lang['Posts'] = "Ìçíýìáôá";
+$lang['Page'] = "Óåëßäá";
+
+$lang['Search'] = "ÁíáæÞôçóç";
+$lang['Admin'] = "ÐñïóáñìïãÞ";
+
+$lang['Name'] = "Ôï üíïìÜ óáò";
+$lang['Email'] = "E-mail";
+$lang['Post_subject'] = "ÈÝìá";
+$lang['Password'] = "Êùäéêüò";
+
+$lang['Passwordold'] = "Ðáëéüò Êùäéêüò";
+$lang['Passwordnew'] = "ÍÝïò Êùäéêüò";
+$lang['Passwordcon'] = "Åðéâåâáßùóç íÝïõ êùäéêïý";
+
+$lang['Fill'] = "ÐñÝðåé íá óõìðëçñùèåß";
+$lang['Confirm'] = "Åðéâåâáßùóç";
+$lang['Send'] = "ÁðïóôïëÞ";
+$lang['Post'] = "Äçìïóßåõóç";
+$lang['Searching'] = "ÁíáæÞôçóç";
+$lang['Searchenter'] = "ÅéóáãùãÞ ëÝîåùí ãéá áíáæÞôçóç";
+
+$lang['Author'] = "ÓõããñáöÝáò";
+$lang['Date'] = "Çìåñïìçíßá";
+$lang['Back'] = "Ðßóù";
+
+$lang['Search_keyword'] = "ËÝîç ÁíáæÞôçóçò";
+$lang['Where_search'] = "ÌÝñïò ÁíáæÞôçóçò";
+$lang['Topics_found'] = "ÈÝìáôá ðïõ âñÝèçêáí";
+
+$lang['Delete'] = "ÄéáãñáöÞ";
+$lang['Edit'] = "Åðåîåñãáóßá";
+$lang['Create'] = "Äçìéïõñãßá";
+$lang['Order'] = "Ðáñáããåëßá";
+$lang['Yes'] = "Íáé";
+
+$lang['Title'] = "Ôßôëïò";
+$lang['Description'] = "ÐåñéãñáöÞ";
+
+$lang['Deletesure'] = "Åßóôå óßãïõñïò ?";
+$lang['Changename'] = "ÁëëáãÞ Ïíüìáôïò";
+$lang['Changepassword'] = "ÁëëáãÞ Êùäéêïý";
+
+?>
