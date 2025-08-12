c9c21da3dfc417ad3e766b08d44a0b6f7dc57d72
me-suzy <me.suzana@gmail.com>
2025-08-12 21:56:02 +0300

    Add Polls and Voting/fungl/fungl/menu/user.php

---
+++ Polls and Voting/fungl/fungl/menu/user.php
@@ -0,0 +1,3 @@
+<li><a href="?page=project" class="nav">Projects</a></li>
+<li><a href="?page=account" class="nav">Account</a></li>
+<li><a href="?page=home&amp;action=logout" class="nav">Logout</a></li>
