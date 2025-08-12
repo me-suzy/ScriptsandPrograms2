6b7659897627a5aed8b19e85fc9cc857d3a6ef33
me-suzy <me.suzana@gmail.com>
2025-08-12 05:59:18 +0300

    Add Content Management/metajour2_1/metajour/app/edocument/edocument_document_model_create.php

---
+++ Content Management/metajour2_1/metajour/app/edocument/edocument_document_model_create.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package SSA
+ * @subpackage model
+ * $Id: edocument_document_model_create.php,v 1.1 2005/02/15 12:19:25 jan Exp $
+ */
+
+require_once($system_path.'basic_model_createobject.php');
+
+class edocument_document_model_create extends basic_model_createobject {
+
+}
+
+?>
