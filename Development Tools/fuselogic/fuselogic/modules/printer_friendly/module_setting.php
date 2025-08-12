247aa0d90874cb98677e5fb198a2c46ff1eca85e
me-suzy <me.suzana@gmail.com>
2025-08-12 09:25:05 +0300

    Add Development Tools/fuselogic/fuselogic/modules/printer_friendly/module_setting.php

---
+++ Development Tools/fuselogic/fuselogic/modules/printer_friendly/module_setting.php
@@ -0,0 +1,8 @@
+<?php
+
+$FL_MODULE_SETTING['module_name'] = 'printer_friendly';
+$FL_MODULE_SETTING['sub_module']['_print'] = 'scripts/printpage.php';
+$FL_MODULE_SETTING['sub_module']['enable'] = 'scripts/enable.php';
+$FL_MODULE_SETTING['sub_module']['disable'] = 'scripts/disable.php';
+
+?>
