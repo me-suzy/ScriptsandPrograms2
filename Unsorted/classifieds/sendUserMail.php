4e545fd419f0897075242fc35e4b76481220bcf6
me-suzy <me.suzana@gmail.com>
2025-08-13 01:41:25 +0300

    Add Unsorted/classifieds/sendUserMail.php

---
+++ Unsorted/classifieds/sendUserMail.php
@@ -0,0 +1,36 @@
+<?php
+
+/* D.E. Classifieds v1.04 
+   Copyright © 2002 Frank E. Fitzgerald 
+   Distributed under the GNU GPL .
+   See the file named "LICENSE".  */
+
+
+/**************************************
+ * File Name: sendUserMail.php        *
+ * ---------                          *
+ *                                    *
+ **************************************/
+
+
+// ************* START FUNCTION send_user_mail *************
+
+function sendUserMail()
+{
+    $headers .= "From: <".cnfg('replyEmail').">\r\n";
+    $headers .= "Reply-To: <".cnfg('replyEmail').">\r\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+    
+
+    if ($result) 
+    {   if( mail($to, $subject, $message, $headers) )
+        {   return true;
+        }
+    }
+
+} // end function send_user_mail
+
+// ************* END FUNCTION send_user_mail *************
+
+
+?>
