a5ab32b1e87f2ea7cf6af01595f7b295454e62fc
me-suzy <me.suzana@gmail.com>
2025-08-12 10:45:49 +0300

    Add Discussion Boards/ovbb-0.08a/upload/includes/censored.inc.php

---
+++ Discussion Boards/ovbb-0.08a/upload/includes/censored.inc.php
@@ -0,0 +1,24 @@
+<?php
+//***************************************************************************//
+//                                                                           //
+//  Copyright (c) 2004-2005 Jonathon J. Freeman                              //
+//  All rights reserved.                                                     //
+//                                                                           //
+//  This program is free software. You may use, modify, and/or redistribute  //
+//  it under the terms of the OvBB License Agreement v2 as published by the  //
+//  OvBB Project at www.ovbb.org.                                            //
+//                                                                           //
+//***************************************************************************//
+
+	$aCensored[] = array('bastard', '*******');
+	$aCensored[] = array('bitch', '*****');
+	$aCensored[] = array('cock', '****');
+	$aCensored[] = array('cum', '***');
+	$aCensored[] = array('cunt', '****');
+	$aCensored[] = array('damn', '****');
+	$aCensored[] = array('fuck', '****');
+	$aCensored[] = array('hell', '****');
+	$aCensored[] = array('nigger', '******');
+	$aCensored[] = array('piss', '****');
+	$aCensored[] = array('shit', '****');
+?>
