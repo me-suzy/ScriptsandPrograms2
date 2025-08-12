0433c5e6157767eef49acb7abcba52d00cd571b8
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:33 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_images.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_images.php
@@ -0,0 +1,35 @@
+<?
+
+// Form Title
+$GLOBALS["tFormTitle"]		= 'Gestion des Images';
+$GLOBALS["tFormTitle1"] = 'Télécharger images/Créer un sous-directory images';
+$GLOBALS["tFormTitle2"] = 'Télécharger image';
+$GLOBALS["tFormTitle3"] = 'Créer Sous-directory';
+
+// List Functions
+$GLOBALS["tAddNewImage"]	= 'Ajouter une nouvelle image';
+$GLOBALS["tDeleteImage"]	= 'Effacer une image';
+$GLOBALS["tDeleteImageDir"] = 'Effacer image sous-directory';
+
+// List Headings
+$GLOBALS["tImageName"]		= 'Nom de l\'Image';
+
+//  Form Fields
+$GLOBALS["tImageFilename"] = 'Image nom de fichier';
+$GLOBALS["tSubdirectoryname"] = 'Nom sous-directory';
+
+//  Form Buttons
+$GLOBALS["bUpload"] = 'Téléchargez!';
+$GLOBALS["bCreateDir"] = 'Créer!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] = 'Vous avez besoin de choisir un fichier pour télécharger.';
+$GLOBALS["eNoDir"] = 'Vous avez besoin d\'entrer un nom de directory.';
+$GLOBALS["eDirAlreadyExists"] = 'sous-directory existe déjà';
+$GLOBALS["eZeroByteFile"] = 'Le fichier est 0 octets dans la longueur';
+$GLOBALS["eNoFileUpload"] = 'Fichier ne pourrait pas être téléchargé avec succès.';
+$GLOBALS["eInvalidFileSize"] = 'Le fichier est trop grand pour être télécharger';
+$GLOBALS["eInvalidFileType"] = 'Le fichier n\'est pas un fichier d\'image valide';
+$GLOBALS["eInvalidMimeType"] = 'Le type MIME de ce Fichier n\'est pas un type d\'image valide';
+
+?>
