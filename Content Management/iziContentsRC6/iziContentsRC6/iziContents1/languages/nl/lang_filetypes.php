8f32351e72a9a89a338a7802c61466e3bd76098e
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:32 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_filetypes.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_filetypes.php
@@ -0,0 +1,38 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] 		= 'Beheer bestandtype';
+
+//  List Headings
+$GLOBALS["tFileCategory"] 	= 'Categorie';
+$GLOBALS["tFileType"] 		= 'Bestand type';
+$GLOBALS["tMIMEType"] 		= 'MIME type';
+$GLOBALS["tFileIcon"] 		= 'Icoon';
+
+//  Categories
+$GLOBALS["tFileCatBackup"] 	= 'Backup';
+$GLOBALS["tFileCatDownload"] 	= 'Download';
+$GLOBALS["tFileCatImage"] 	= 'Afbeelding';
+$GLOBALS["tFileCatScript"] 	= 'Script';
+
+//  List Functions
+$GLOBALS["tAddNewFiletype"] 	= 'Voeg bestandtype toe';
+$GLOBALS["tViewFiletype"] 	= 'Laat bestandstype zien';
+$GLOBALS["tEditFiletype"] 	= 'Bewerk bestandstype';
+$GLOBALS["tDeleteFiletype"] 	= 'Verwijder bestandstype';
+
+//  Form Block Titles
+$GLOBALS["thFiletypeGeneral"] 	= 'Bestandstype details';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] 		= 'Met dit formulier kunt u verschillende bestandstype aanmaken die gebruikt kunnen worden met ezContents.';
+$GLOBALS["hFileCategory"] 	= 'Categorie bepaalt welk bestand beheerd wordt en welke functies het bestandtype gebruiken kan.';
+$GLOBALS["hFileType"] 		= 'Dit is de bestandstype code, gebaseerd op de bestands extensie.';
+$GLOBALS["hMIMEType"] 		= 'Dit is een lijst van geldige MIME types voor bestanden met deze extensie.<br /><br />U kunt meerdere MIME types invoeren gescheiden door punt komma\'s (<b>;</b>) omdat er meerdere MIME Types werken met deze extensie.<br />Een voorbeeld van een jpeg bestand, deze kan een MIME type zijn voor <b>image/jpeg</b> of <b>image/pjpeg</b>.';
+$GLOBALS["hFileIcon"] 		= 'Dit icoon wordt gebruikt voor dit bestandstype.';
+
+//  Error Messages
+$GLOBALS["eNoFiletype"] 	= 'Bestanstype code kan niet leeg blijven.';
+$GLOBALS["eNoMIMEType"] 	= 'MIME type kan niet leeg blijven.';
+
+?>
