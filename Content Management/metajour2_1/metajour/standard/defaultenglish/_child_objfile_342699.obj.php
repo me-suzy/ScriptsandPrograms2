27b65caf58159dbfe5d04af286c91847ee41eef6
me-suzy <me.suzana@gmail.com>
2025-08-12 07:15:03 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_objfile_342699.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_objfile_342699.obj.php
@@ -0,0 +1,23 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342699;
+function objfile_342699 () {
+$obj = owNew('structureelement');
+$objdata['name'] = "";
+$objdata['pageid'] = "342719";
+$objdata['url'] = "";
+$objdata['binfile1'] = "0";
+$objdata['binfile2'] = "0";
+$objdata['binfile3'] = "0";
+$objdata['showtype'] = "0";
+$objdata['target'] = "";
+$objdata['image1'] = "";
+$objdata['image2'] = "";
+$objdata['image3'] = "";
+$objdata['templateid'] = "0";
+$objdata['structureid'] = "0";
+$obj->createObject($objdata);
+$obj->moveTo(12);
+return $obj;
+}
+?>
