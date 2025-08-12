7b4e9f75f04ac8c955fa6272a436f5d863228a85
me-suzy <me.suzana@gmail.com>
2025-08-13 01:48:36 +0300

    Add Unsorted/cms master/cms-install/templates/install-license.php

---
+++ Unsorted/cms master/cms-install/templates/install-license.php
@@ -0,0 +1,58 @@
+<center>
+<table width=700 cellpadding=0 cellspacing=0 border=0>
+<tr>
+<td width=210 valign=top>
+<table width=210 cellpadding=0 cellspacing=0 border=0>
+<tr>
+<td bgcolor=#e9e9e9><img src=images/trans.gif width=70 height=51 border=0></td>
+<td bgcolor=#d1d1d1><img src=images/trans.gif width=70 height=51 border=0></td>
+<td bgcolor=#e9e9e9><img src=images/trans.gif width=70 height=51 border=0></td>
+</tr>
+<tr>
+<td bgcolor=#b6b6b6><img src=images/trans.gif width=70 height=51 border=0></td>
+<td bgcolor=#e9e9e9><img src=images/trans.gif width=70 height=51 border=0></td>
+<td bgcolor=#ffffff><img src=images/trans.gif width=70 height=51 border=0></td>
+</tr>
+<tr>
+<td valign=top>
+<table cellpadding=0 cellspacing=0 border=0><tr><td bgcolor=#d1d1d1><img src=images/trans.gif width=70 height=51 border=0></td></tr></table>
+</td>
+<td bgcolor=#ffffff valign=top colspan=2>
+
+<!-- START MENU 2 -->
+&nbsp;
+<!-- END MENU 2 -->
+
+</td>
+</tr>
+</table>
+</td>
+<td width=20 valign=top><img src=images/trans.gif width=20 height=1 border=0></td>
+<td width=450 valign=top>
+<img src=images/trans.gif width=450 height=10 border=0>
+
+<h2>
+<?php echo $Lang_install->header_license ?>
+</h2>
+
+<!-- START CONTENT -->
+<table width=450 cellpadding=0 cellspacin=0 border=0>
+<tr><td bgcolor=#363636>
+<table width=100% cellpadding=0 cellspacing=1 border=0>
+<tr><td bgcolor=#ffffff>
+<table width=100% cellpadding=4 cellspacing=2 border=0>
+<form action=index.php method=POST>
+<input type=hidden name=mode value="options">
+<tr>
+<td bgcolor=#f3f3f3 valign=top><?php echo $license ?></td>
+</tr>
+<tr>
+<td bgcolor=#f3f3f3 valign=top align=center><input type=submit value="<?php echo $Lang_install->button_accept ?>" style="width:100%; border-width:1; border-color:#363636"></td>
+</tr>
+</form>
+</table>
+</td></tr>
+</table>
+</td></tr>
+</table>
+<!-- END CONTENT -->
