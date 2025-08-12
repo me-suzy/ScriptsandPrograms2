45f3e5fe33c9962e9487ffdfccacd5748b3b6eed
me-suzy <me.suzana@gmail.com>
2025-08-12 12:40:51 +0300

    Add E-Commerce/shop-script-free/includes/admin/sub/notavailable.php

---
+++ E-Commerce/shop-script-free/includes/admin/sub/notavailable.php
@@ -0,0 +1,17 @@
+<?php
+/*****************************************************************************
+ *                                                                           *
+ * Shop-Script FREE                                                          *
+ * Copyright (c) 2005 WebAsyst LLC. All rights reserved.                     *
+ *                                                                           *
+ ****************************************************************************/
+
+	//payment types list
+
+	if (!strcmp($sub, "payment"))
+	{
+		//set sub-department template
+		$smarty->assign("admin_sub_dpt", "notavailable.tpl.html");
+	}
+
+?>
