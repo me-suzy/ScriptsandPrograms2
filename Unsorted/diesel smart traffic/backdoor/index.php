9f172f5c17f07d936845989f398a03f8cf886eed
me-suzy <me.suzana@gmail.com>
2025-08-13 02:35:44 +0300

    Add Unsorted/diesel smart traffic/backdoor/index.php

---
+++ Unsorted/diesel smart traffic/backdoor/index.php
@@ -0,0 +1,53 @@
+<?php setcookie("sc",""); ?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<title> Exchange System</title>
+<link rel="stylesheet" href="../style.css">
+</head>
+<body bgcolor=FFFFFF class=bodytext link=0 vlink=0 alink=0 text=0>
+
+
+
+
+
+<table border=0 width=100% height=100%>
+    <td align=center> 
+      <form action=back.php  method=post>
+	<table border=0 cellspacing=0 cellpadding=1 bgcolor=CCCCCC><tr><td>
+	      <table border=0 cellspacing=4 cellpadding=4 bgcolor=FFFFFF>
+            <tr><td>
+    <table width=235 border=0 cellspacing=0 cellpadding=0 class=links bgcolor=FFFFFF>
+      <tr> 
+        <td colspan=2><font size=-1><b>Webmaster login<br>
+          <br>
+          </b></td>
+      </tr>
+      <tr> 
+        <td align="left" width="155"><font size=-1>Webmaster&nbsp;</td>
+        <td valign="middle" align="right" width="82"> 
+          <input type="text" name="login" size="20" class="bodytext">
+        </td>
+      </tr>
+      <tr> 
+        <td colspan=2 height=2></td>
+      </tr>
+      <tr> 
+        <td align="left" width="155"><font size=-1>Pasword&nbsp;</td>
+        <td valign="middle" align="right" width="82"> 
+          <input type="password" name="pswd" size="20" class="bodytext">
+        </td>
+      </tr>
+      <tr> 
+        <td colspan=2 height=2></td>
+      </tr><tr align=left valign=bottom height=30><td colspan=2><input type=submit name=Submit value="Log in" size=200 class=cmn>
+      </td>
+      </tr>
+    </table>
+	</td></tr></table>
+	</td></tr></table>
+</form>
+</td></table>
+</body>
+</html>
+ 
