6f29a777a377de7ead4f11a947b3e52bdb7f3a72
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:35 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_requestapproval.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_requestapproval.en.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Request approval';
+$LANG['text_1'] = 'Are you sure that you want to request approval of the selected records?';
+?>
