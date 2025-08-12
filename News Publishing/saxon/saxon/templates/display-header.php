18a9de94b3571c93da5bf42c2a8344e74e02701b
me-suzy <me.suzana@gmail.com>
2025-08-12 20:22:41 +0300

    Add News Publishing/saxon/saxon/templates/display-header.php

---
+++ News Publishing/saxon/saxon/templates/display-header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+<title>Deity2</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="keywords" content="quirm CMS content managment systems chess league system design development elfin esmi" />
+<meta name="Description" content="quirm net a design and development area set up by esmi and elfin" />
+<meta name="author" content="elfin and esmi" />
+<meta name="copyright" content="Copyright 2004 quirm.net. Most rights reserved." />
+<link rel="stylesheet" href="templates/news-display.css" type="text/css" media="screen" />
+</head>
+<body>
