39df48c0c24ef84a58f20b563a056759faef04cb
me-suzy <me.suzana@gmail.com>
2025-08-12 23:00:08 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Submit_News/language/lang-slovenian.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Submit_News/language/lang-slovenian.php
@@ -0,0 +1,63 @@
+<?php
+
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+
+define("_PRINTER","Prilagojeno za izpis");
+define("_FRIEND","Novico polji prijatelju");
+define("_YOURNAME","Vae ime");
+define("_OK","V redu");
+define("_ALLOWEDHTML","HTML koda je dovoljena:");
+define("_EXTRANS","Extrans (html tag v tekstu)");
+define("_HTMLFORMATED","HTML formatirano");
+define("_PLAINTEXT","Normalen tekst");
+define("_ARTICLES","Èlankih");
+define("_SUBMITNEWS","Polji èlanek");
+define("_SUBMITADVICE","Svoj èlanek/zgodbo napiite v sledeèi obrazec in preverite pred poiljanjem.<br>Moram propomniti, da vsak prejeti èlanek ne bo objavljen.<br>Va prispevek bo prevejen in morda spremenjen s strani naega osebja.");
+define("_SUBTITLE","Naslov");
+define("_BEDESCRIPTIVE","Opisujte enostavno in èisto");
+define("_BADTITLES","slabi naslovi= 'Poglejte to!' ali 'Èlanek'");
+define("_HTMLISFINE","HTML je omogoèen, vendar pa preverite pravilnost URL-jev in HTML oznak!");
+define("_AREYOUSURE","Ali ste vkljuèili URL? Ste ga testirali in pregledali?");
+define("_SUBPREVIEW","Pred poiljanje obvezno preverite va èlanek");
+define("_SELECTTOPIC","Izberite temo");
+define("_NEWSUBPREVIEW","Pregled pred poiljanjem èlanka");
+define("_STORYLOOK","Va èlanek bo izgledal nekako kot tole:");
+define("_CHECKSTORY","Prosim, preverite tekst, povezave, itd... pred poiljanjem èlanka!");
+define("_THANKSSUB","Hvala!");
+define("_SUBSENT","Va èlanek je bil poslan...");
+define("_SUBTEXT","Prevejen bo tekom nekaj naslednjih ur, in èe je zanimiv in resnièen bo kmalu objavljen.");
+define("_WEHAVESUB","Trenutno imamo");
+define("_WAITING","poslanih èlankov ki èakaju na preverjanje.");
+define("_PREVIEW","Pregled");
+define("_NEWUSER","Novi uporabnik");
+define("_USCORE","Ocena");
+define("_DATE","Datum");
+define("_STORYTEXT","Tekst èlanka");
+define("_EXTENDEDTEXT","Razirjeni tekst");
+define("_LANGUAGE","Jezik");
+
+define("_SELECTMONTH2VIEW","Please select the month you want to view:");
+define("_SHOWALLSTORIES","Show ALL Stories");
+define("_STORIESARCHIVE","Stories Archive");
+define("_ACTIONS","Actions");
+define("_ARCHIVESINDEX","Stories Archive Index");
+define("_ALLSTORIESARCH","All Stories");
+define("_NEXTPAGE","Next Page");
+define("_PREVIOUSPAGE","Previous Page");
+
+?>
