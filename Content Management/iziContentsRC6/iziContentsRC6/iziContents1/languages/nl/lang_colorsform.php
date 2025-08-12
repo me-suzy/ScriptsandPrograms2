7dba03f80b157475790207330395a166efc18934
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:25 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_colorsform.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_colorsform.php
@@ -0,0 +1,143 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] 			= 'Beheer van website Kleuren en Lettertype';
+
+//  Form Block Titles
+$GLOBALS["thBackground"] 		= 'Achtergrond Kleuren';
+$GLOBALS["thFont"] 			= 'Website Lettertype';
+$GLOBALS["thLinks"] 			= 'Link Kleur';
+$GLOBALS["thContent"] 			= 'Tekst Kleur';
+$GLOBALS["thRColContent"] 		= 'Rechterkolom tekst Kleuren';
+$GLOBALS["thTeaser"] 			= 'Intro Kleuren';
+
+//  Form Field Headings
+$GLOBALS["tMainBackground"] 		= 'Basis achtergrond';
+$GLOBALS["tHeaderBackground"] 	= 'Kop achtergrond';
+$GLOBALS["tFooterBackground"] 	= 'Voet achtergrond';
+$GLOBALS["tMenuBackground"] 		= 'Menu achtergrond';
+$GLOBALS["tTopmenuBackground"] 	= 'Kopmenu achtergrond';
+$GLOBALS["tFont"] 			= 'Website lettertype';
+$GLOBALS["tHeaderFontSize"] 		= 'Kop lettertype grootte';
+$GLOBALS["tRColHeaderFontSize"]	= 'Rechterkolom kop lettertype grootte';
+$GLOBALS["tTMFontSize"] 		= 'Kopmenu link lettertype grootte';
+$GLOBALS["tTMFontStyle"] 		= 'Kopmenu link tekst stijl';
+$GLOBALS["tMFontSize"] 			= 'Menu link lettertype grootte';
+$GLOBALS["tMFontStyle"] 		= 'Menu link tekst stijl';
+$GLOBALS["tLinkColour"] 		= 'Link kleur';
+$GLOBALS["tLinkColourHover"] 		= 'Link kleur zweeftekst';
+$GLOBALS["tLinkColourHistory"]	= 'Link kleur historie';
+$GLOBALS["tSLinkColour"] 		= 'Kleine link kleur';
+$GLOBALS["tSLinkColourHover"] 	= 'Kleine link kleur zweeftekst';
+$GLOBALS["tSLinkColourHistory"] 	= 'Kleine link kleur historie';
+$GLOBALS["tMenuLinkColour"] 		= 'Menu link kleur';
+$GLOBALS["tMenuLinkColourHover"] 	= 'Menu link kleur zweeftekst';
+$GLOBALS["tMenuLinkColourHistory"] 	= 'Menu link kleur historie';
+$GLOBALS["tTopMenuLinkColour"] 	= 'Kopmenu link kleur';
+$GLOBALS["tTopMenuLinkColourHover"] = 'Kopmenu link kleur zweeftekst';
+$GLOBALS["tTopMenuLinkColourHistory"] 	= 'Kopmenu link kleur historie';
+$GLOBALS["tRColLinkColour"] 		= 'Rechterkolom link kleur';
+$GLOBALS["tRColLinkColourHover"] 	= 'Rechterkolom link kleur zweeftekst';
+$GLOBALS["tRColLinkColourHistory"] 	= 'Rechterkolom link kleur historie';
+$GLOBALS["tSLinkFontSize"] 		= 'Kleine link lettertype grootte';
+$GLOBALS["tRColFontSize"] 		= 'Rechterkolom lettertype/links grootte';
+$GLOBALS["tRColFontStyle"] 		= 'Rechterkolom links stijl';
+$GLOBALS["tCBorder"] 			= 'Inhoud rand kleur';
+$GLOBALS["tCHeader"] 			= 'Inhoud kop lettertype kleur';
+$GLOBALS["tCBackground"] 		= 'Inhoud achtergrond';
+$GLOBALS["tCFont"] 			= 'Inhoud lettertype kleur';
+$GLOBALS["tTBorder"] 			= 'Intro rand kleur';
+$GLOBALS["tTHeader"] 			= 'Intro kop lettertype kleur';
+$GLOBALS["tTBackground"] 		= 'Intro achtergrond';
+$GLOBALS["tTFont"] 			= 'Intro lettertype kleur';
+$GLOBALS["tCFontSize"] 			= 'Inhoud/links lettertype grootte';
+$GLOBALS["tCFontStyle"] 		= 'Inhoud/links lettertype stijl';
+$GLOBALS["tRColCBorder"] 		= 'Rechterkolom rand kleur';
+$GLOBALS["tRColCHeader"] 		= 'Rechterkolom kop lettertype kleur';
+$GLOBALS["tRColCBackground"] 		= 'Rechterkolom achtergrond';
+$GLOBALS["tRColCFont"] 			= 'Rechterkolom lettertype kleur';
+
+//  Form Text Description
+$GLOBALS["tDetails"] 			= 'Met dit formulier kunt u de kleuren en de lettertypes aanpassen van uw website.';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["hMainBackground"] 		= 'Dit is de basis achtergrond kleur van uw website.';
+$GLOBALS["hHeaderBackground"] 	= 'Deze achtergrond kleur wordt gebruikt voor de kop van uw website.';
+$GLOBALS["hFooterBackground"] 	= 'Deze achtergrond kleur wordt gebruikt voor de voet van uw website.';
+$GLOBALS["hMenuBackground"] 		= 'Deze achtergrond kleur wordt gebruikt voor de menubalk en voor de links in de menubalk van uw website. Indien u een afbeelding als achtergrond gebruikt worden de menu links nog steeds in deze kleur weergegeven. Dit kunt u voorkomen door dit veld blanco te maken';
+$GLOBALS["hTopmenuBackground"] 	= 'Deze achtergrond kleur wordt gebruikt in het kopmenu van uw website (Als er gebruik gemaakt wordt van kop menu\'s; dit kunt u instellen bij de menukeuze Website instellingen).';
+$GLOBALS["hFont"] 			= 'Dit is het lettertype dat gebruikt wordt voor teksten van uw website.';
+$GLOBALS["hHeaderFontSize"] 		= 'Deze grootte van het lettertype wordt gebruikt in de kop.';
+$GLOBALS["hRColHeaderFontSize"] 	= 'Deze grootte van het lettertype wordt gebruikt in de rechterkolom.';
+$GLOBALS["hTMFontSize"] 		= 'Dit is de grootte van het lettertype dat gebruikt word voor kopmenu links. Dit is van toepassing, wanneer deze eigenschap aan gezet is in \'Website instellingen\', en wanneer u gebruik maakt van tekst voor deze menu links; het is niet van toepassing indien u gebruik maakt van afbeeldingen voor de menu links.';
+$GLOBALS["hTMFontStyle"] 		= 'Gebruik deze instelling om een lettertype stijl te kiezen voor de kop menu links. (Deze instelling is alleen van toepassing indien u gebruik maakt van tekst voor de kop menu link; is niet van toepassing indien u gebruik maakt van afbeeldingen voor deze menu links.)';
+$GLOBALS["hMFontSize"] 			= 'Dit is de grootte van het lettertype voor de menu link links.';
+$GLOBALS["hMFontStyle"] 		= 'Gebruik deze instelling om een stijl te kiezen voor de menu links. (Deze instelling is alleen van toepassing indien u gebruik maakt van tekst voor de menu links; is niet van toepassing indien u gebruik maakt van afbeeldingen voor deze menu links.)';
+$GLOBALS["hLinkColour"] 		= 'Gebruik deze instelling om de kleur van de links op uw website aan te geven.';
+$GLOBALS["hLinkColourHover"] 		= 'Gebruik deze instelling om de kleur aan te geven die gebruikt wordt als de cursor over de link gaat.';
+$GLOBALS["hLinkColourHistory"] 	= 'Deze instelling maakt het mogelijk om een kleur te kiezen voor links die een gebruiker al eens heeft gevolgd.';
+$GLOBALS["hSLinkColour"] 		= 'Deze kleur wordt gebruikt voor de \'kleine\' links in uw website.';
+$GLOBALS["hSLinkColourHover"] 	= 'Deze instelling geeft de kleur aan waarmee de \'kleine\' links worden geaccentueerd wanneer de cursur over de link gaat.';
+$GLOBALS["hSLinkColourHistory"] 	= 'Deze instelling maakt het mogelijk om een kleur te kiezen voor \'kleine\' links die een gebruiker al eens heeft gevolgd.';
+$GLOBALS["hMenuLinkColour"] 		= 'Deze insteling wordt gebruikt om de kleur van de menu links van de website aan te geven.';
+$GLOBALS["hMenuLinkColourHover"] 	= 'Gebruik deze instelling om aan te geven met welke kleur de menu links geaccentueerd worden als de cursor erover heen beweegt.';
+$GLOBALS["hMenuLinkColourHistory"] 	= 'Deze instelling maakt het mogelijk om een kleur te kiezen voor menu links die een gebruiker al eens heeft gevolgd.';
+$GLOBALS["hTopMenuLinkColour"] 	= 'Deze instelling geeft de kleur aan voor de topmenu links van de website.';
+$GLOBALS["hTopMenuLinkColourHover"] 	= 'Gebruik deze instelling om aan te geven met welke kleur de topmenu links geaccentueerd worden als de cursor erover heen beweegt.';
+$GLOBALS["hTopMenuLinkColourHistory"] = 'Deze instelling maakt het mogelijk om een kleur te kiezen voor topmenu links die een gebruiker al eens heeft gevolgd.';
+$GLOBALS["hRColLinkColour"] 		= 'Dit is de kleur die gebruikt wordt voor de links in de rechterkolom van uw website.';
+$GLOBALS["hRColLinkColourHover"] 	= 'Dit is de kleur waarmee de links in de rechterkolom geaccentueerd worden als de cursor erover heen beweegt.';
+$GLOBALS["hRColLinkColourHistory"] 	= 'Deze instelling maakt het mogelijk om een kleur te kiezen voor links in de rechterkolom die een gebruiker al eens heeft gevolgd.';
+$GLOBALS["hSLinkFontSize"] 		= 'Gebruik deze instelling om de grootte van het lettertype te wijzigen dat gebruikt wordt voor de \'kleine\' links.';
+$GLOBALS["hRColFontSize"] 		= 'Gebruik deze instelling om de grootte van het lettertype van de inhoude van de rechterkolom te wijzigen.';
+$GLOBALS["hRColFontStyle"] 		= 'Gebruik deze instelling om de stijl te wijzigen van de links in de inhoud van de rechterkolom.';
+$GLOBALS["hCBorder"] 			= 'Dit is de kleur die gebruikt wordt voor het kop blok en de randen van de items in de hoofdinhoud.';
+$GLOBALS["hCHeader"] 			= 'Dit is de kleur die gebruikt wordt voor de koptekst van de hoofd inhoud.';
+$GLOBALS["hCBackground"] 		= 'Dit is de achtergrond kleur van de items in de hoofdinhoud.';
+$GLOBALS["hCFont"] 			= 'Dit is de kleur van de tekst van de hoofd inhouds blokken.';
+$GLOBALS["hTBorder"] 			= 'Dit is de kleur die gebruikt wordt voor de kop en de randen van de intro items.';
+$GLOBALS["hTHeader"] 			= 'Dit is de kleur die gebruikt wordt voor de koptekst van de intro inhoud.';
+$GLOBALS["hTBackground"] 		= 'Dit is de achtergrondkleur van de inhoud van de intro.';
+$GLOBALS["hTFont"] 			= 'Dit is de kleur van de tekst van de intro inhoud.';
+$GLOBALS["hCFontSize"] 			= 'Deze instelling geeft de grootte van het lettertype aan voor de tekst op uw website.';
+$GLOBALS["hCFontStyle"] 		= 'Deze instelling geeft de stijl aan van de tekst op uw website.';
+$GLOBALS["hRColCBorder"] 		= 'Dit is de kleur die gebruikt wordt voor de kop en de randen van de items in de rechterkolom van de hoofdinhoud.';
+$GLOBALS["hRColCHeader"] 		= 'Dit is de kleur die gebruikt wordt voor de tekst in de kop van de rechterkolom in de hoofdinhoud.';
+$GLOBALS["hRColCBackground"] 		= 'Dit is de achtergrondkleur van de rechterkolom in de hoofdinhoud.';
+$GLOBALS["hRColCFont"] 			= 'Dit is de kleur van de tekst van de items in de rechterkolom van de hoofdinhoud.';
+
+//  Error Messages
+$GLOBALS["eMainBGWrong"] 		= 'Basis achtergrond ingeving is fout';
+$GLOBALS["eHeaderBGWrong"] 		= 'Kop achtergrond ingeving is fout';
+$GLOBALS["eFooterBGWrong"] 		= 'Voet achtergrond ingeving is fout';
+$GLOBALS["eContentBGWrong"] 		= 'Inhoud achtergrond ingeving is fout';
+$GLOBALS["eMenuBGWrong"] 		= 'Menu achtergrond ingeving is fout';
+$GLOBALS["eTopmenuBGWrong"] 		= 'Kop menu achtergrond ingeving is fout';
+$GLOBALS["eLinkWrong"] 			= 'Link kleur ingeving is fout';
+$GLOBALS["eLinkHoverWrong"] 		= 'Link kleur zweeftekst is fout';
+$GLOBALS["eLinkVisitedWrong"] 		= 'Link kleur bezocht is fout';
+$GLOBALS["eMenuLinkWrong"] 		= 'Menu link kleur is fout';
+$GLOBALS["eMenuLinkHoverWrong"] 	= 'Menu link kleur zweeftekst is fout';
+$GLOBALS["eMenuLinkVisitedWrong"] 	= 'Menu link kleur bezocht is fout';
+$GLOBALS["eTopMenuLinkWrong"] 		= 'Topmenu link kleur waarde is fout';
+$GLOBALS["eTopMenuLinkHoverWrong"] 	= 'Topmenu link kleur zweeftekst is fout';
+$GLOBALS["eTopMenuLinkVisitedWrong"] 	= 'Topmenu link kleur bezocht waarde is fout';
+$GLOBALS["eRColLinkWrong"] 		= 'Waarde van rechterkolom link kleur is fout';
+$GLOBALS["eRColLinkHoverWrong"] 	= 'Waarde van rechterkolon link kleur zweeftekst is fout';
+$GLOBALS["eRColLinkVisitedWrong"] 	= 'Waarde van rechterkolom link kleur bezocht is fout';
+$GLOBALS["eSLinkWrong"] 		= 'Waarde van kleine link kleur is fout';
+$GLOBALS["eSLinkHoverWrong"] 		= 'Waarde van kleine link kleur zweeftekst is fout';
+$GLOBALS["eSLinkVisitedWrong"] 		= 'Waarde van kleine link kleur bezocht is fout';
+$GLOBALS["eCHeaderWrong"] 		= 'Waarde van de kleur van lettertype van Inhouds Kop is fout';
+$GLOBALS["eCFontWrong"] 		= 'Waarde van de kleur van het inhouds lettertype is onjuist';
+$GLOBALS["eCBorderWrong"] 		= 'Waarde van de Inhouds randkleur is onjuist';
+$GLOBALS["eTeaserBGWrong"] 		= 'Waarde van de achtergrond van de Intro is onjuist';
+$GLOBALS["eTBorderWrong"] 		= 'Waarde van de randkleur van de Intro is onjuist';
+$GLOBALS["eTHeaderWrong"] 		= 'Waarde van de kleur van het lettertype van de Intro is onjuist';
+$GLOBALS["eTFontWrong"] 		= 'Waarde van de kleur van het lettertype is onjuist';
+$GLOBALS["eRColBGWrong"] 		= 'Waarde van de achtergrond van de Rechterkolom is onjuist';
+$GLOBALS["eRColBorderWrong"] 		= 'Waarde van de randkleur van de Rechterkolom is onjuist';
+$GLOBALS["eRColHeaderWrong"] 		= 'Waarde van de kleur van het kop lettertype van de Rechterkolom is onjuist';
+$GLOBALS["eRColFontWrong"] 		= 'Waarde van het lettertype kleur van de Rechterkolom is onjuist';
+
+?>
