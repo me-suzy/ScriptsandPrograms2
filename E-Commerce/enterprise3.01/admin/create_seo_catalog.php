f1e35920db6ac9dea48fe045f6b94113d257a711
me-suzy <me.suzana@gmail.com>
2025-08-12 11:31:43 +0300

    Add E-Commerce/enterprise3.01/admin/create_seo_catalog.php

---
+++ E-Commerce/enterprise3.01/admin/create_seo_catalog.php
@@ -0,0 +1,106 @@
+<?php
+/*
+  $Id$
+
+  Enterprise Shopping Cart
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  require('includes/application_top.php');
+
+?>
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html <?php echo HTML_PARAMS; ?>>
+<head>
+<title><? echo TITLE; ?></title>
+
+<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
+<script language="JavaScript1.2">
+
+function cOn(td)
+{
+  if(document.getElementById||(document.all && !(document.getElementById)))
+  {
+    td.style.backgroundColor="#CCCCCC";
+  }
+}
+
+function cOnA(td)
+{
+  if(document.getElementById||(document.all && !(document.getElementById)))
+  {
+    td.style.backgroundColor="#CCFFFF";
+  }
+}
+
+function cOut(td)
+{
+  if(document.getElementById||(document.all && !(document.getElementById)))
+  {
+    td.style.backgroundColor="DFE4F4";
+  }
+}
+</script>
+<script type="text/javascript" src="includes/browser.js">/************************************************ Jim's DHTML Menu v5.0- © Jim Salyer (jsalyer@REMOVETHISmchsi.com)* Visit Dynamic Drive: http://www.dynamicdrive.com for script and instructions* This notice must stay intact for use***********************************************/</script><script type="text/javascript" src="config.js"></script></head>
+<body onload="init();" marginwidth="0" marginheight="0" topmargin="0" bottommargin="0" leftmargin="0" rightmargin="0" bgcolor="#FFFFFF">
+<!-- header //-->
+<? include(DIR_WS_INCLUDES . 'header.php');  ?>
+<!-- header_eof //-->
+
+
+<table border="0" width="100%" cellspacing="2" cellpadding="2">
+  <tr>
+    <td width="<?php echo BOX_WIDTH; ?>" valign="top"><table border="0" width="<?php echo BOX_WIDTH; ?>" cellspacing="1" cellpadding="1" class="columnLeft">
+<!-- left_navigation //-->
+<?php require(DIR_WS_INCLUDES . 'column_left.php'); ?>
+<!-- left_navigation_eof //-->
+    </table></td>
+<!-- body_text //-->
+    <td width="100%" valign="top"><table border="0" width="100%" cellspacing="0" cellpadding="2">
+      <tr>
+        <td><table border="0" width="100%" cellspacing="0" cellpadding="0">
+          <tr>
+            <td class="pageHeading"><?php echo "Edit SSL Security Web Address"; ?></td>
+            <td class="pageHeading" align="right"><?php echo escs_draw_separator('pixel_trans.gif', HEADING_IMAGE_WIDTH, HEADING_IMAGE_HEIGHT); ?></td>
+          </tr>
+        </table></td>
+     </tr>
+
+<!-- body_text //-->
+    <td width="100%" valign="top">
+      <!-- Start of edit header //-->
+
+<?
+
+$result = escs_db_query('select categories_id, categories_name, categories_heading_title, categories_description from categories_description');
+$row =  escs_db_fetch_array($result);
+while($row)
+{
+	print $row['categories_id'] . '&nbsp;&nbsp;&nbsp;' .
+	$row['categories_name'] . '&nbsp;&nbsp;&nbsp;' .
+	$row['categories_heading_title'] . '&nbsp;&nbsp;&nbsp;' .
+	$row['categories_description'];
+}
+
+?>
+
+<h1>SEO Catalog created at <a href="/ecommerce_shopping_cart_software/seo-index.html">/ecommerce_shopping_cart_software/seo-index.html</a></h1>
+
+
+
+	<!-- End of edit header //-->
+	</td>
+<!-- products_attributes_eof //-->
+</tr></table>
+<!-- body_text_eof //-->
+<!-- footer //-->
+<? include(DIR_WS_INCLUDES . 'footer.php'); ?>
+<!-- footer_eof //-->
+<br>
+</body>
+</html>
+<? include(DIR_WS_INCLUDES . 'application_bottom.php');?>
