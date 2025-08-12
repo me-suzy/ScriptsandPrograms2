c9a131ac3da2badc3a5289fcecab0f0797e7484e
me-suzy <me.suzana@gmail.com>
2025-08-12 23:00:06 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Submit_News/language/lang-slovak.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Submit_News/language/lang-slovak.php
@@ -0,0 +1,77 @@
+<?php
+
+/*************************************************************************/
+/* The Slovak language file for module Submit_News                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/*************************************************************************/
+/* The Slovak language file for module Submit_News                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+
+define("_PRINTER","Stránka vhodná na tlaè");
+define("_FRIEND","Posla tento èlánok priate¾ovi, známemu");
+define("_YOURNAME","Vae meno");
+define("_OK","Ok!");
+define("_ALLOWEDHTML","Povoli HTML:");
+define("_EXTRANS","Extrans (html kód do textu)");
+define("_HTMLFORMATED","HTML formátovanie");
+define("_PLAINTEXT","obyèajný text");
+define("_ARTICLES","Èlánky");
+define("_SUBMITNEWS","Zasla príspevok");
+define("_SUBMITADVICE","Prosím napíte èlánok do nasledovného formulára a pred odoslaním ho skontrolujte.<br>Nie vetky èlánky musia by publikované.<br>Vá príspevok bude skontrolovaný a môe by opravený naim tímom");
+define("_SUBTITLE","Názov");
+define("_BEDESCRIPTIVE","Jednoducho, struène a jasne.");
+define("_BADTITLES","zlé názvy='Pozri toto!' alebo 'Èlánok'");
+define("_HTMLISFINE","HTML je fajn, ale otestujte prípadné URL a kód HTML!");
+define("_AREYOUSURE","Naozaj chcete vloi URL? Otestovali ste funkènos adresy?");
+define("_SUBPREVIEW","Pred odoslaním si príspevok prezrite");
+define("_SELECTTOPIC","Výber témy");
+define("_NEWSUBPREVIEW","Náh¾ad pred odoslaním príspevku");
+define("_STORYLOOK","Vá príspevok bude vyzera takto:");
+define("_CHECKSTORY","Prosím pred odoslaním skontrolujte texty, odkazy atp.");
+define("_THANKSSUB","Srdeèná vïaka za príspevok!");
+define("_SUBSENT","Vá príspevok bol odoslaný...");
+define("_SUBTEXT","Pozrieme si vá príspevok v najbliích hodinách. Ak je zaujímavý a relevantný, èoskoro ho publikujeme.");
+define("_WEHAVESUB","V tejto chvíli máme");
+define("_WAITING","príspevkov èakajúcich na publikovanie.");
+define("_PREVIEW","Prezrie");
+define("_NEWUSER","Nový uívate¾");
+define("_USCORE","Skóre");
+define("_DATE","Dátum");
+define("_STORYTEXT","Text èlánku");
+define("_EXTENDEDTEXT","Rozírený text");
+define("_LANGUAGE","Jazyk");
+
+define("_SELECTMONTH2VIEW","Vyberte mesiac, ktory chcete prezrie:");
+define("_SHOWALLSTORIES","Zobrazi vetky èlánky");
+define("_STORIESARCHIVE","Archív èlánkov");
+define("_ACTIONS","Monosti");
+define("_ARCHIVESINDEX","Index archívu èlánkov");
+define("_ALLSTORIESARCH","Vetky èlánky");
+define("_NEXTPAGE","Ïalia strana");
+define("_PREVIOUSPAGE","Predchádzajúca strana");
+
+?>
