c5460f9f9301203c466120a7f3127ac2696535a4
me-suzy <me.suzana@gmail.com>
2025-08-12 10:00:11 +0300

    Add Development Tools/pas-3.6/pas/sitetemplate/includes/lang_fr.inc.php

---
+++ Development Tools/pas-3.6/pas/sitetemplate/includes/lang_fr.inc.php
@@ -0,0 +1,40 @@
+<?php 
+/* Language File
+ */
+$strWelcome = "Bienvenue dans cette application utilisant PAS 3" ;
+
+// add record event:
+$strInsertError = "Erreur durant l'insertion de l'enregistrement";
+$strInsertOk = "L'enregistrement a &#233;t&#233; inser&#233; correctement" ;
+$strAddCancel = "L'insertion de l'enregistrement a &#233;t&#233; annul&#233;e"; 
+$strCancel = "Annuler";
+
+// mydb.check Email
+$strWrongEmail = "L'e-mail saisie n'existe pas" ;
+
+// mydb.check Required
+$strRequiredField = "Vous devez remplir tous les champs obligatoires." ;
+
+// mydb.check Username Password
+$strMissingField = "Le champ identifiant et le champ mot de passe sont obligatoires dans ce formulaire" ;
+$strErrorPasswordNotMatch = "Les deux mots de passe sont diff&#233;rents" ;
+$strErrorLoginAlreadyUsed = "Cet identifiant est d&#233;j&#224; utilis&#233;" ;
+
+// mydb Format pricture field 
+$strUnabletoSave = "Le fichier n'a pas pu &#234;tre enregistr&#233;" ;
+
+// mydb format time field
+$strWrongTimeFormat = "Le format est incorrect: HH:MM:SS avec hours < 24, minutes < 60 et seconds < 60";
+ 
+// mydb manage record
+$strNoActionsFound = "Pas d'action trouv&#233;e" ;
+$strMissingArgument = "Erreur, l'enregistrement n'a pas pu &#234;tre ins&#233;r&#233;, information manquante" ;
+$strConfirm = "&#202;tes vous s&#251;r de vouloir supprimer cet enregistrement ?" ;
+$strYes = "Oui" ;
+$strNo = "Non" ;
+
+// mydb table update record
+ $strInsertError = "Erreur durant la mise &#224; jour de l'enregistrement";
+ $strUpdateOk = "L'enregistrement a &#233;t&#233; mis &#224; jour" ;
+ $strCancel = "Annuler";
+ $strUpdateCancel = "La mise &#225; jour a &#233;t&#233; annul&#233;e"; 
