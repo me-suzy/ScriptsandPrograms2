3b0381884594cfc8f93b1f3921bfd0ac33225800
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:02 +0300

    Add Unsorted/cashcrusader/pages/help.php

---
+++ Unsorted/cashcrusader/pages/help.php
@@ -0,0 +1,108 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr> 
+<td> 
+<p align="center"><font size="3"><b><br>
+Frequently Asked Questions</b></font></p>
+<p>&nbsp;</p>
+<p><font size="3"><b><u>Account Questions:</u><br>
+</b></font> <br>
+<font size="2"><b>Am I allowed to have more than one account?</b></font><br>
+<font size="2">We allow one account per individual. There may be more than one 
+account per household, however each person registering an account must have a 
+separate computer and separate ISP to participate in our program.</font><br>
+<br>
+<font size="2"><b>How can I log into my account?</b></font><br>
+<font size="2">Log in at http://www.XX</font><br>
+<br>
+<font size="2"><b>How can I cancel my account?</b></font><br>
+<font size="2">Go to your user info page and at the bottom of the page you enter 
+your username and password, then click cancel.</font><br>
+<br>
+<font size="2"><b>How can I cancel receiving your newsletters?</b></font><br>
+<font size="2">Your membership in our program allows ZZ to send you email messages 
+and advertisements. You may not cancel receiving email ads from ZZ without 
+terminating your membership in our program and forfeiting any and all earnings. 
+Log in to your account and click the "User Account Info." Link. At the bottom 
+of the page you will find the option to "Cancel Account". </font><br>
+<br>
+<font size="2"><b>How can I change my personal information?</b></font><br>
+<font size="2">Log in to your account and click the "User Account Info." link. 
+You will be able to edit any of your information except your name and member ID.</font><br>
+<br>
+<font size="2"><b>I live outside the US, can I join?</b></font><br>
+<font size="2">Yes, we welcome all international members.</font><br>
+<br>
+<font size="2"><b>I have Web-TV, can I join too?</b></font><br>
+<font size="2">Yes, though some programs will not allow WEB-TV users to join their 
+program, we welcome WEB-TV users. Our efficient tracking method allows WEB-TV 
+users full earning privileges.</font><br>
+<br>
+<br>
+<font size="3"><b><u>Earnings Questions</u>:<br>
+</b></font> <br>
+<font size="2"><b>How much do I earn by participating in paid-emails?</b></font><br>
+<font size="2">Each advertiser pays a different amount per unique visit. These 
+amounts change frequently so we cannot give an exact amount for each visit, however 
+we do give you a large percentage of the amount we earn.</font><br>
+<br>
+<font size="2"><b>When can I request payment?</b></font><br>
+<font size="2">You may request payment after you have earned the minimum payout.</font><br>
+<br>
+<font size="2"><b>How can I request payment?</b></font><br>
+<font size="2">As soon as you reach payout, you will see the redemption option 
+appear in your account area, after submitting your redemption request, we will 
+validate your account, and then begin the payment process. You will also have 
+the option to redeem your earnings for discounted advertising with us. When you 
+qualify, you will see what options are available to you in your account area.</font><br>
+<br>
+<font size="2"><b>When am I paid?</b></font><br>
+<font size="2">After your payment request is made and your account is in good-standing 
+we will issue payment to you within 30 days.</font><br>
+<br>
+<font size="2"><b>How am I paid?</b></font><br>
+<font size="2">Payments are made via PayPal / eGold</font><br>
+<br>
+<br>
+<font size="3"><b><u>Referral Questions</u><br>
+</b></font> <br>
+<font size="2"><b>How can I refer other members?</b></font><br>
+<font size="2">You may place a link with your referral url to ZZ on your website. 
+You may add your referral url to your email signature. You can give out flyers 
+or advertise in any way you wish except for Spamming.</font><br>
+<br>
+<font size="2"><b>What is my referral link?</b></font><br>
+<font size="2">Your personal referral link can be found on your personal &quot;Referral 
+links&quot; page.</font><br>
+<br>
+<font size="2"><b>What can I earn through referrals?</b></font><br>
+<font size="2">Members will earn 20% of their direct referral earnings, 10% of 
+their indirect referral level earnings, 5% of their 3rd level referral earnings, 
+3% of their 4th level referral earnings, 2% of their 5th level referral earnings 
+and 1% of their 6th level referral earnings..</font><br>
+<br>
+<font size="2"><b>Are my referral earnings working?</b></font><br>
+<font size="2">Yes, it takes many paid emails received by members to have referral 
+earnings show up, even for members that have 100's or 1000's in their downline. 
+Referral earnings are displayed once a referred member has earned their referring 
+member at least 1 full cent. Technically a member with 1000 referrals that have 
+each earned him/her 1/2 cent has earned $5.00 but their earnings will not be shown 
+until an individual member has earned them 1 full cent.</font><br>
+<br>
+<br>
+<font size="3"><b><u>Advertising Questions</u>:</b></font><br>
+<br>
+<font size="2"><b>I have a website I would like to advertise, where can I find 
+information on your different programs and costs?</b></font><br>
+<font size="2">Visit our advertisers section of the website for complete details.</font><br>
+<br>
+<font size="2"><b>I have questions not answered on this FAQ page, what can I do?</b></font><br>
+<font size="2"><a href="/pages/contact.php">Contact Us</a> and supply as much 
+information as possible so we can solve the problem in a timely manner.</font><br>
+</p>
+</td>
+</tr>
+</table>
+<br>
+<? include("footer.php");?>
