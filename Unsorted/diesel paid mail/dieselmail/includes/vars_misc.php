da4a69c54abcd0c33a30129451b93c4f7a5790b4
me-suzy <me.suzana@gmail.com>
2025-08-13 02:32:54 +0300

    Add Unsorted/diesel paid mail/dieselmail/includes/vars_misc.php

---
+++ Unsorted/diesel paid mail/dieselmail/includes/vars_misc.php
@@ -0,0 +1,11 @@
+<?
+$mail_by_advertiser = "";
+$gold_membership = "ON";
+$gold_membership_amount = "20";
+$currency_symbol = "$";
+$max_days_for_login = "120";
+$signup_bonus = "2.00";
+$paid_email_charge = "";
+$nonpaid_email_charge = "";
+$banner_ads_charge = "";
+?>
