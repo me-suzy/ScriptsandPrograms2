803401936709c773114f517362e1543110b0c302
me-suzy <me.suzana@gmail.com>
2025-08-12 17:48:33 +0300

    Add Image Galleries/encapsgallery-0.1.0-demo/encapsgallery-1.0.0-demo/admin/common_head.php

---
+++ Image Galleries/encapsgallery-0.1.0-demo/encapsgallery-1.0.0-demo/admin/common_head.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Admin</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+<META content=0 http-equiv=Expires>
+  <meta HTTP-EQUIV="Pragma" CONTENT="no-cache">
+  <meta HTTP-EQUIV="Cache-Control" content="no-cache">
+<link href="html/admin.css" type="text/css" rel="stylesheet">
+</head>
+<html>
+<body >
+
+<div align="center">
