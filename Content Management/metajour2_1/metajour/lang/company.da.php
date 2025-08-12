665b42ca1fa45f5ced1cb954cebd6314936f3112
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:57 +0300

    Add Content Management/metajour2_1/metajour/lang/company.da.php

---
+++ Content Management/metajour2_1/metajour/lang/company.da.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Virksomheder';
+$LANG['label_name'] = 'Navn';
+$LANG['label_address1'] = 'Adresse 1';
+$LANG['label_address2'] = 'Adresse 2';
+$LANG['label_address3'] = 'Adresse 3';
+$LANG['label_address4'] = 'Adresse 4';
+$LANG['label_postalcode'] = 'Postnr';
+$LANG['label_city'] = 'By';
+$LANG['label_state'] = 'Stat/Region';
+$LANG['label_country'] = 'Land';
+$LANG['label_telephone1'] = 'Telefon 1';
+$LANG['label_telephone2'] = 'Telefon 2';
+$LANG['label_telephone3'] = 'Telefon 3';
+$LANG['label_telefax'] = 'Telefax';
+$LANG['label_website'] = 'Website';
+$LANG['label_email'] = 'Email';
+$LANG['label_timezone'] = 'Tidszone';
+$LANG['label_comment'] = 'Kommentar';
+?>
