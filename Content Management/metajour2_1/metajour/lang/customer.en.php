8d1216b46a3f5b8b45ed23bf805763ab9ff066e0
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:10 +0300

    Add Content Management/metajour2_1/metajour/lang/customer.en.php

---
+++ Content Management/metajour2_1/metajour/lang/customer.en.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Customers (webshop)';
+$LANG['label_name'] = 'Name';
+$LANG['label_address1'] = 'Address 1';
+$LANG['label_address2'] = 'Address 2';
+$LANG['label_address3'] = 'Address 3';
+$LANG['label_address4'] = 'Address 4';
+$LANG['label_userid'] = 'User';
+$LANG['label_companyid'] = 'Company';
+$LANG['label_contactid'] = 'Contact';
+$LANG['label_postalcode'] = 'Postal';
+$LANG['label_city'] = 'City';
+$LANG['label_state'] = 'State/Region';
+$LANG['label_country'] = 'Country';
+$LANG['label_telephone1'] = 'Phone 1';
+$LANG['label_telephone2'] = 'Phone 2';
+$LANG['label_telephone3'] = 'Phone 3';
+$LANG['label_telefax'] = 'Telefax';
+$LANG['label_website'] = 'Website';
+$LANG['label_email1'] = 'Email 1';
+$LANG['label_email2'] = 'Email 2';
+$LANG['label_email3'] = 'Email 3';
+$LANG['label_comment'] = 'Comment';
+?>
