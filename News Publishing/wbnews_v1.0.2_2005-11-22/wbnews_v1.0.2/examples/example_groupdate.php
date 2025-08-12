02c865bb8368bce5f9234c80179612b7b2df1172
me-suzy <me.suzana@gmail.com>
2025-08-12 20:57:07 +0300

    Add News Publishing/wbnews_v1.0.2_2005-11-22/wbnews_v1.0.2/examples/example_groupdate.php

---
+++ News Publishing/wbnews_v1.0.2_2005-11-22/wbnews_v1.0.2/examples/example_groupdate.php
@@ -0,0 +1,37 @@
+<?php
+
+define('wbnews',true);
+
+include "../global.php";
+include $config['installdir']."/news.php";
+
+if (!isset($_GET['action'])) 
+{
+    $news = new news($newsConfig);
+    if (!isset($_GET['newsid']))
+    {
+        $news->displayNewsDateGroup('all');
+        $news->pagination();
+    }
+    else
+        $news->displaySingleNews($_GET['newsid']);
+} 
+else 
+{
+    if ($_GET['action']!='sendto') 
+    {
+        include $config['installdir']."/comments.php";
+        $comments = new comments($newsConfig,@$_GET['newsid']);
+        $comments->showNews();
+        $comments->viewComments();
+        $comments->displayForm();
+    } 
+    else 
+    {
+        include $config['installdir']."/sendtofriend.php";
+        $sendto = new sendToFriend($newsConfig);
+        $sendto->sendFriend();
+    }
+}
+
+?>
