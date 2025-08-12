dbef0cb01b383bb682bd026bdf1650ab2e230453
me-suzy <me.suzana@gmail.com>
2025-08-12 11:22:01 +0300

    Add E-Commerce/enterprise3.01/advanced_search.php

---
+++ E-Commerce/enterprise3.01/advanced_search.php
@@ -0,0 +1,24 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  require('includes/application_top.php');
+
+  require(DIR_WS_LANGUAGES . $language . '/' . FILENAME_ADVANCED_SEARCH);
+
+  $breadcrumb->add(NAVBAR_TITLE_1, escs_href_link(FILENAME_ADVANCED_SEARCH));
+
+  $content = CONTENT_ADVANCED_SEARCH;
+  $javascript = $content . '.js.php';
+
+  require(DIR_WS_TEMPLATES . TEMPLATENAME_MAIN_PAGE);
+
+  require(DIR_WS_INCLUDES . 'application_bottom.php');
+?>
