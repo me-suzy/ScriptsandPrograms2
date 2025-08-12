6ee8ea4abb7fb546c08174273da9a398451a0588
me-suzy <me.suzana@gmail.com>
2025-08-12 16:24:56 +0300

    Add Games and Entertainment/ac_tradecode_php/glitchlist.php

---
+++ Games and Entertainment/ac_tradecode_php/glitchlist.php
@@ -0,0 +1,15 @@
+<?php
+/////////////
+// Unknown //
+/////////////
+$itemlist[0x2104] = "Unknown";
+$itemlist[0x252E] = "Unknown";
+$itemlist[0x2F04] = "Unknown";
+$itemlist[0x2F06] = "Unknown";
+
+//////////////////
+// Effect Codes //
+//////////////////
+$itemlist[0xFFFF] = "Lockup Item";
+
+?>
