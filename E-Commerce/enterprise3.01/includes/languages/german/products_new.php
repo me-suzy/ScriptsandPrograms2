aaa53fe7884e783c85403407ac7942769db9605e
me-suzy <me.suzana@gmail.com>
2025-08-12 12:00:39 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/products_new.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/products_new.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Neue Produkte');
+define('HEADING_TITLE', 'Neue Produkte');
+
+define('TEXT_DATE_ADDED', 'aufgenommen am:');
+define('TEXT_MANUFACTURER', 'Hersteller:');
+define('TEXT_PRICE', 'Preis:');
+?>
