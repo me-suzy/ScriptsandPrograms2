754cca057c4576fbc8f5c96075dbb5234aaba7e2
me-suzy <me.suzana@gmail.com>
2025-08-12 06:00:04 +0300

    Add Content Management/metajour2_1/metajour/app/eproject/eproject_project_view_combi.php

---
+++ Content Management/metajour2_1/metajour/app/eproject/eproject_project_view_combi.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eProject
+ * @subpackage view
+ * $Id: eproject_project_view_combi.php,v 1.2 2005/01/12 03:23:48 jan Exp $
+ */
+
+require_once($system_path.'basic_view_combi.php');
+
+class eproject_project_view_combi extends basic_view_combi {
+
+	function tabSubType() {
+	}
+
+	function tabProperties() {
+	}
+
+}
+
+?>
