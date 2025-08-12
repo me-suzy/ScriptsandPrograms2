565a4819b461eeaee87af818b0ecb41dde41be84
me-suzy <me.suzana@gmail.com>
2025-08-12 11:56:15 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_signup_ok.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_signup_ok.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Affiliate Signup');
+define('HEADING_TITLE', 'Congratulations!');
+define('TEXT_ACCOUNT_CREATED', 'Congratulations! Your new Affiliate account application has been submitted! You will shortly receive an email containing important information regarding your Affiliate Account, including you affiliate login details. If you have not received it within the hour, please <a href="' . escs_href_link(FILENAME_AFFILIATE_CONTACT) . '">contact us</a>.<br><br>If you have <small><b>ANY</b></small> questions about the affiliate program, please <a href="' . escs_href_link(FILENAME_AFFILIATE_CONTACT) . '">contact us</a>.');
+?>
