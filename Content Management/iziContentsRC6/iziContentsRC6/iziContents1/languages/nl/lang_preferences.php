a409e9ccab6870f770dadc9883be65cdb0d4c03e
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:45 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_preferences.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_preferences.php
@@ -0,0 +1,46 @@
+<?
+
+// Form Title
+$GLOBALS["tFormTitle"]		= 'Beheer gebruikersvoorkeuren';
+
+// List Headings
+$GLOBALS["tUsername"]		= 'Gebruikersnaam';
+$GLOBALS["tEMail"]		= 'E-mail';
+
+// Form Field Headings
+$GLOBALS["tLoginName"]		= 'Loginnaam';
+$GLOBALS["tRetainCookie"]	= 'Cookie behouden';
+$GLOBALS["tPassword"]		= 'Wachtwoord';
+$GLOBALS["tRetypePassword"]	= 'Herhaal wachtwoord';
+$GLOBALS["tAuthorname"]		= 'Getoonde naam';
+$GLOBALS["tUsergroup"]		= 'Gebruikersgroep';
+$GLOBALS["tEMail"]		= 'E-mailadres';
+$GLOBALS["tCountry"]		= 'Land';
+$GLOBALS["tLanguage"]		= 'Taal';
+
+// Form Detail Comments and Help Texts
+$GLOBALS["tDetails"]		 = 'Met dit formulier is het mogelijk om de gebruikersvoorkeuren in te stellen.';
+$GLOBALS["hLoginName"]		 = 'Dit is de loginnaam die gebruikt wordt in het wachtwoord scherm.';
+$GLOBALS["hRetainCookie"]	 = 'Als deze box aangevinkt wordt, zal een cookie worden opgeslagen "'.$GLOBALS["tLoginName"].'" zodat in de toekomst alleen een wachtwoord intypen voldoende is.';
+$GLOBALS["hRetainCookie"]	.= 'De cookie zal 1 jaar bewaard worden, na deze tijd moet de box in het beheer opnieuw aangevinkt worden.';
+$GLOBALS["hPassword"]		 = 'Geef de wachtwoord die je gaat gebruiken bij iziContents.';
+$GLOBALS["hRetypePassword"]	 = 'Herhaal je wachtwoord, om er zeker van te zijn dat het correct is ingetypt.';
+$GLOBALS["hAuthorname"]		 = 'Dit veld is een complete naam of een korte naam (nickname). Het zal zichtbaar zijn voor de gebruikers die je artikelen lezen.';
+$GLOBALS["hUsergroup"]		 = 'De gebruikersgroep waar je bij behoort.';
+$GLOBALS["hEMail"]		 = 'Je e-mailadres. Samen met de naam zal dit een hyperlink vormen die onder de artikelen geplaatst wordt.';
+$GLOBALS["hCountry"]		 = 'Het land waar u woont (optioneel).';
+$GLOBALS["hLanguage"]		 = 'Uw taal (optioneel).<br>';
+$GLOBALS["hLanguage"]		.= 'Als dit gezet is, zal je de menu opties, veldnamen, help, etc. zien in je eigen taal.';
+
+// Error Messages
+$GLOBALS["eLoginExists"]	= 'Loginnaam bestaat al, kies een andere.';
+$GLOBALS["eLoginEmpty"]		= 'Loginnaam kan niet leeg zijn.';
+$GLOBALS["ePasswordError"]	= 'Herhaalde wachtwoord is niet gelijk aan de gegeven wachtwoord.';
+$GLOBALS["eNameEmpty"]		= 'Je naam kan niet leeggelaten worden.';
+$GLOBALS["eEMailEmpty"]		= 'Je e-mailadres kan niet leeggelaten worden.';
+
+
+$GLOBALS["tPrefsUpdated"]	= 'Je gebruikersvoorkeuren zijn nu bijgewerkt.';
+$GLOBALS["tPrefsRegistered"]	= 'Je bent aangemeld als een nieuwe gebruiker.';
+
+?>
