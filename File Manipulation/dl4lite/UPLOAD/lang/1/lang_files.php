c9f1caf21cf142a3bb167c709f0c44813fcb1e8e
me-suzy <me.suzana@gmail.com>
2025-08-12 14:16:26 +0300

    Add File Manipulation/dl4lite/UPLOAD/lang/1/lang_files.php

---
+++ File Manipulation/dl4lite/UPLOAD/lang/1/lang_files.php
@@ -0,0 +1,39 @@
+<?php
+/*********************************************************
+* Name: lang_files.php
+* Author: RW::Download 4.0
+* Contact: realworld@blazefans.com
+* Description: Language variables
+* Version: 4.00
+*********************************************************/
+$lang = array(
+
+'commentadded' => "Thankyou for leaving your comments on this file.",
+'filerated' => "Your rating has been logged. Thankyou for rating this file",
+'dladded' => "Thankyou for adding this new file to our database.",
+'dlindb' => "Your file has been immediately added to our database.",
+'deldl' => "Download removed. Press continue to go back",
+'addcomment' => "Add Comment",
+'editcomment' => "Edit Comment",
+'ratefile' => "Rate File",
+'report' => "Report Broken Link",
+'reported' => "The admin has been emailed informing him that this link is broken.",
+'emaillink' => "Email to friend",
+'message' => "Message",
+'delcomment' => "This comment was successfully removed from the database",
+'invalidemail' => "Your email address is invalid",
+'invalidemail2' => "The recipients email address is invalid",
+'comment_edited' => "Comment Edited",
+'thumbs_added' => "thumbnails successfully uploaded",
+'norating' => "No rating",
+'yourname' => "Your Name",
+'youremail' => "Your Email",
+'friendsname' => "Friends Name",
+'friendsemail' => "Friends Email",
+'filedl' => "File successfully removed. Please remember to add a new download to this file before exiting",
+'optional' => "Optional",
+'massmove' => "Please select a folder to move the %d selected files to",
+'movedok' => "Files have been successfully moved",
+)
+
+?>
