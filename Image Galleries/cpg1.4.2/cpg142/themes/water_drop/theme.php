3570c5bad4e20ace3b9b488f889ce97094a2e453
me-suzy <me.suzana@gmail.com>
2025-08-12 17:47:59 +0300

    Add Image Galleries/cpg1.4.2/cpg142/themes/water_drop/theme.php

---
+++ Image Galleries/cpg1.4.2/cpg142/themes/water_drop/theme.php
@@ -0,0 +1,26 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2005 Coppermine Dev Team
+  v1.1 originaly written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  Coppermine version: 1.4.2
+  $Source: /cvsroot/coppermine/devel/themes/water_drop/theme.php,v $
+  $Revision: 1.65 $
+  $Author: gaugau $
+  $Date: 2005/10/25 01:16:24 $
+**********************************************/
+
+define('THEME_IS_XHTML10_TRANSITIONAL',1); // Remove this if you edit this template until
+                                           // you have validated it. See docs/theme.htm.
+
+// HTML template for template sys_menu spacer
+$template_sys_menu_spacer ='<img src="themes/water_drop/images/orange_carret.gif" width="8" height="8" border="0" alt="" />';
+
+?>
