a99c46e4f8fd7d3560b761cb08e2b3ac32417e1e
me-suzy <me.suzana@gmail.com>
2025-08-12 20:05:22 +0300

    Add News Publishing/phpfn/phpfn/Override.php

---
+++ News Publishing/phpfn/phpfn/Override.php
@@ -0,0 +1,11 @@
+<?php
+
+/*	+--------------------------------------------------------------
+	| PHPFreeNews - News Headlines on your website                |
+	| Developed by Jim Willsher.                                  |
+	| http://www.phpfreenews.co.uk                                |
+	+-------------------------------------------------------------+
+*/
+
+require_once('Config/Config.php');
+?>
