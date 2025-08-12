cb4bf4ff050cdf0619f2892cac77837a926a7694
me-suzy <me.suzana@gmail.com>
2025-08-12 05:07:55 +0300

    Add Content Management/jetbox_cms_v2.1.SR1/jetbox/includes/phpdig/libs/htmlmetas.php

---
+++ Content Management/jetbox_cms_v2.1.SR1/jetbox/includes/phpdig/libs/htmlmetas.php
@@ -0,0 +1,3 @@
+<meta http-equiv="Content-Type" content="text/html; charset=<?php print PHPDIG_ENCODING; ?>" />
+<meta http-equiv="Content-Language" content="<?php print $phpdig_language; ?>">
+<link href="<?php print $relative_script_path ?>/includes/style.css" rel="stylesheet" type="text/css" />
