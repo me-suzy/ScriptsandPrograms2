7c842ba3ffeb1d262b5e6211d5b67dedac4f9fdf
me-suzy <me.suzana@gmail.com>
2025-08-12 21:33:43 +0300

    Add Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/search.lang.php

---
+++ Organizers/phpmycomic-1.2.0/phpmycomic/lang/english/search.lang.php
@@ -0,0 +1,26 @@
+<?php
+
+// Head titles
+$tpl->assign("lang_head_search", "Search");
+$tpl->assign("lang_head_keyword", "Search By Keyword");
+$tpl->assign("lang_head_simple", "Simple Search");
+$tpl->assign("lang_head_story", "Story Search");
+$tpl->assign("lang_head_result", "Search Result");
+
+$tpl->assign("lang_list_name", "Name");
+$tpl->assign("lang_list_story", "Story Title");
+$tpl->assign("lang_list_issue", "Issue");
+$tpl->assign("lang_list_volume", "Volume");
+$tpl->assign("lang_list_type", "Type");
+$tpl->assign("lang_list_variation", "Variation");
+$tpl->assign("lang_list_issues", "Issues");
+$tpl->assign("lang_search_exact", "Exact Name:");
+$tpl->assign("lang_search_exact_nfo", "Exact Name");
+$tpl->assign("lang_search_list", "Sort list by:");
+$tpl->assign("lang_search_title", "Title");
+$tpl->assign("lang_search_type", "Type");
+
+// Other text
+$tpl->assign("lang_search_string", "Enter your search string");
+
+?>
