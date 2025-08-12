20027d34fb45bbb1791472661323d3ccbfcc8907
me-suzy <me.suzana@gmail.com>
2025-08-13 02:30:36 +0300

    Add Unsorted/DEVShopStop/VarCWC02.php

---
+++ Unsorted/DEVShopStop/VarCWC02.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************************/
+/*                                                                         */
+/* Copyright (c) 2001-2002 by CrossWalkCentral                             */
+/* http://www.crosswalkcentral.net                                         */
+/*                                                                         */
+/* CrossWalkCentral                                                        */
+/* You Web Hosting Community!                                              */
+/*                                                                         */
+/* Let us customize this script for you.                                   */
+/*                                                                         */
+/* Please let us know what you think of the NUKE-Shop Stop Shopping Center */
+/* at http://www.crosswalkcentral.net/modules.php?name=Forum               */
+/*                                                                         */
+/* This program is not free software.                                      */
+/*                                                                         */
+/**************************************************************************/
+
+$ver = "1.0";
+
+$paypalid = "";
+$logourl = "";
+$payreturn = "http://www.crosswalkcentral.net";
+$paycancel = "http://www.crosswalkcentral.net";
+$viewcart = "https://www.paypal.com/images/view_cart.gif";
+
+$paycart = "http://www.paypal.com/images/x-click-but22.gif";
+
+/************************************************************************/
+
+$nuke_admin = "on";
+$right_side = "on";
+/************************************************************************/
+
+/************************************************************************/
+
+/************************************************************************/
+
+?>
