0936257ddad8ecfe87774e27c557621860c2a02f
me-suzy <me.suzana@gmail.com>
2025-08-12 13:57:07 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/client/categoryconfirm.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/client/categoryconfirm.php
@@ -0,0 +1,81 @@
+<?
+$file_rev="041305";
+////////////////////////////////////////////////////////
+//                 phpBannerExchange                  //
+//                   by: Darkrose                     //
+//              (darkrose@eschew.net)                 //
+//                                                    //
+// You can redistribute this software under the terms //
+// of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of  //
+// the License, or (at your option) any later         //
+// version.                                           //
+//                                                    //
+// You should have received a copy of the GNU General //
+// Public License along with this program; if not,    //
+// write to the Free Software Foundation, Inc., 59    //
+// Temple Place, Suite 330, Boston, MA 02111-1307 USA //
+//                                                    //
+//     Copyright 2004 by eschew.net Productions.      //
+//   Please keep this copyright information intact.   //
+////////////////////////////////////////////////////////
+
+include("../config.php");
+include("../css.php");
+
+if($use_gzhandler==1){
+ob_start("ob_gzhandler");
+}
+
+require_once('../lib/template_class.php');
+include("../lang/client.php");
+
+// Begin login stuff
+$db=mysql_connect("$dbhost","$dbuser","$dbpass");
+mysql_select_db($dbname,$db);
+$result = mysql_query("select * from banneruser where login='$login' AND pass='$pass'");
+$get_userinfo=@mysql_fetch_array($result);
+$id=$get_userinfo[id];
+$login=$get_userinfo[login];
+$pass=$get_userinfo[pass];
+
+session_start();
+$session=session_id();
+$login=$_SESSION['login'];
+$pass=$_SESSION['pass'];
+$id=$_SESSION['id'];
+
+if($login=="" AND $pass=="" OR $pass=="") {
+	$page = new Page('../template/client_login_error.php');	
+	$page->replace_tags(array(	
+		'css' => "$css",
+		'session' => "$session",	
+		'baseurl' => "$baseurl",	
+		'title' => "$exchangename - $LANG_login_error_title",	
+		'shorttitle' => "$LANG_login_error_title",	
+		'msg' => "$LANG_login_error",	
+		'footer' => '../footer.php'));	
+	$page->output();	
+	session_destroy();
+}else{
+
+$category=$_REQUEST['category'];
+
+$changecat=mysql_query("update bannerstats set category='$category',approved='0' where uid='$id'");
+
+	$page = new Page('../template/client_cat_changed.php');
+	$page->replace_tags(array(
+	'css' => "$css",
+	'back' => "$LANG_back",
+	'baseurl' => "$baseurl",
+	'session' => "$session",
+	'title' => "$exchangename - $LANG_menu_cat",
+	'shorttitle' => "$LANG_menu_cat",
+	'msg' => "$LANG_catconf_message",
+	'footer' => '../footer.php',
+	'menu' => 'client_menuing.php'));
+
+	$page->output();
+
+}
+?>
