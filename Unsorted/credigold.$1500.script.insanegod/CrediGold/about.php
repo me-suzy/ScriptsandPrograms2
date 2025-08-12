99a4ba654438486aa3653cf610e60ad2d0ea599e
me-suzy <me.suzana@gmail.com>
2025-08-13 02:13:39 +0300

    Add Unsorted/credigold.$1500.script.insanegod/CrediGold/about.php

---
+++ Unsorted/credigold.$1500.script.insanegod/CrediGold/about.php
@@ -0,0 +1,153 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+
+<head>
+
+<title>Infinity Interactive</title>
+
+<!-- Writing meta tags for the search engines -->
+
+   <meta http-equiv="Content-Type"	content="text/html; charset=iso-8859-1">
+
+   <meta name="Author"				   content="Svetlin Staev & Kiril Angov">
+
+   <meta name="description"		   content="Infinity Interactive - a Web-based company">
+
+   <meta name="keywords"			   content="Svetlin Staev, Kiril Angov, web, development, internet, intranet, scripts, programs, download, purchase, online, security, tools, generators, clients, updates, javascript, php, mysql, sql, cheap, packages">
+
+   <meta name="language"			   content="English">
+
+   <meta name="coverage"			   content="Worldwide">
+
+   <meta name="robots"				   content="index follow">
+
+   <meta name="copyright"	         content="(C)2003 Infinity Interactive">
+
+<!-- End of writing meta tags -->
+
+</head>
+
+<style>
+
+a			   {text-decoration:none}
+
+a:link		{color: #FF0F0F}
+
+a:hover		{color: #D20000}
+
+a:visited	{color: #D20000}
+
+a:activ		{color: #D20000}
+
+.text		   {font-family: Verdana; font-size: 8pt; padding: 2pt;}
+
+.turn		   {font-family: Verdana; font-size: 8pt; color: #D20000;}
+
+#holder		{width: 100%; height: 100%; border: 1pt solid black;}
+
+</style>
+
+
+
+<body bgcolor=white text=black topmargin=0 leftmargin=0 marginheight=0 marginwidth=0 scroll=no>
+
+<div id=holder>
+
+<table border=0 cellspacing=0 cellpadding=0 align=center>
+
+   <tr>
+
+      <td><p align=center>
+
+      <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#3,0,0,0"	width=277 height=262 name=intro align=middle>
+
+         <param name=src value=about.swf>
+
+         <param name=loop value=false>
+
+         <param name=scale value=exactfit>
+
+         <param name=quality value=high>
+
+         <embed src=about.swf pluginspage="http://www.macromedia.com/shockwave/download/" type="application/x-shockwave-flash" width=277 height=262 loop=false align=middle quality=high name=about scale=exactfit>
+
+      </object>
+
+      </td>
+
+   </tr>
+
+</table>
+
+<table border=0 width=280 cellspacing=0 cellpadding=0 align=center>
+
+   <tr>
+
+      <td bgcolor=#004A95 colspan=3><img src=images/dot.gif width=278 height=1></td>
+
+   </tr>
+
+   <tr>
+
+      <td width=123 class=text>Yahoo IDs</td>
+
+      <td width=11 class=turn>�</td>
+
+      <td width=148 class=text>Kirokomara, Svetlins</td>
+
+   </tr>
+
+   <tr>
+
+      <td class=text>Telephone</td>
+
+      <td class=turn>�</td>
+
+      <td class=text>(+359) 42 239 10</td>
+
+   </tr>
+
+   <tr>
+
+      <td class=text>Email</td>
+
+      <td class=turn>�</td>
+
+      <td class=text><a href="mailto:svetlin@developer.bg?Subject=Question on your Web site" title="Contact us!">svetlin@developer.bg</a></td>
+
+   </tr>
+
+   <tr>
+
+      <td bgcolor=#004A95 colspan=3><img src=images/dot.gif width=278 height=1></td>
+
+   </tr>
+
+   <tr>
+
+      <td colspan=3><img src=images/dot.gif width=278 height=1></td>
+
+   </tr>
+
+   <tr>
+
+      <td bgcolor=black colspan=3><img src=images/dot.gif width=278 height=2></td>
+
+   </tr>
+
+   <tr>
+
+      <td colspan=3 align=right><font class=text><a href="javascript:window.close()" title="Close the notice!">close</a></font></td>
+
+   </tr>
+
+</table>
+
+</center>
+
+</div>
+
+</body>
+
+</html>
