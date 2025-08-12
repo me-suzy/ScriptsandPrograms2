d21ea0bd9eecfc1da4840f97231a26d0c6f59d35
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:56 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_tagcategories.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_tagcategories.php
@@ -0,0 +1,19 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Kurzbefehl-Kategorien &uuml;bersetzen';
+
+//  List Headings
+$GLOBALS["tCatDesc"] = 'Kategorie Name';
+
+//  List Functions
+$GLOBALS["tViewCategory"] = 'Kurzbefehl-Kategorie ansehen';
+$GLOBALS["tEditCategory"] = 'Kurzbefehl-Kategorie &uuml;bersetzen';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Dieses Formular erlaubt Ihnen, die Kategorie-Namen der Kurzbefehle zu &uuml;bersetzen.';
+
+//  Error Messages
+$GLOBALS["eNoTagcategoryDesc"] = 'Sie m&uuml;ssen eine &Uuml;bersetzung f&uuml;r jede Sprache eingeben.';
+
+?>
