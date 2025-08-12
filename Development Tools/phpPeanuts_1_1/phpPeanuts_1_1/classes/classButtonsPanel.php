bac0d1eb0b944a1f5f3fd0ceb48eac049c56293c
me-suzy <me.suzana@gmail.com>
2025-08-12 10:02:42 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/classButtonsPanel.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/classButtonsPanel.php
@@ -0,0 +1,20 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+includeClass('PntButtonsPanel', 'pnt/web/parts');
+
+/** Part that outputs html descirbing two rows of buttons.
+*
+* This concrete subclass is here to keep de application developers
+* code separated from the framework code.
+* @see http://www.phppeanuts.org/site/index_php/Menu/178
+* Framework code is in the superclass.
+* This class may be copied to an application folder to
+* make application specific overrides.
+* @see http://www.phppeanuts.org/site/index_php/Pagina/65
+*/
+class ButtonsPanel extends PntButtonsPanel {
+
+}
+?>
