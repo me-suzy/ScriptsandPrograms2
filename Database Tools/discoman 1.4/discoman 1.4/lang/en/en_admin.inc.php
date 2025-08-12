296a61d32b82db25cc15029fe84505f283e6b29b
me-suzy <me.suzana@gmail.com>
2025-08-12 08:53:40 +0300

    Add Database Tools/discoman 1.4/discoman 1.4/lang/en/en_admin.inc.php

---
+++ Database Tools/discoman 1.4/discoman 1.4/lang/en/en_admin.inc.php
@@ -0,0 +1,75 @@
+<?php
+
+//libellés noms de colonnes ou de champ
+$txt_images = "Images";
+$txt_image = "Image";
+$txt_abrege = "Short name";
+$txt_admin = "Admin";
+$txt_artistes = "Artists";
+$txt_artiste = "Artist";
+$txt_formats = "Formats";
+$txt_format = "Format";
+$txt_annees = "Years";
+$txt_annee = "Year";
+$txt_payss = "Countries";
+$txt_pays = "Country";
+$txt_refs = "Références";
+$txt_ref = "Référence";
+$txt_coms = "Comments";
+$txt_com = "Comment";
+$txt_titres = "Titles";
+$txt_titre = "Title";
+$txt_infos = "Infos";
+$txt_designations = "Descriptions";
+$txt_designation = "Description";
+
+//divers :
+$txt_nom = "Name";
+$txt_mdp = "Password";
+$txt_disques = "Records";
+$txt_utils = "Users";
+
+//Texte d'information
+$txt_saisir = "Please, enter your name and password.";
+$txt_choisir_artiste = "Please, select the artist you want to modify the records.";
+$txt_choisir_artiste_eff = "Please, select the artist you want to delete one or several records.";
+$txt_choisir_disque = "Select the record to modify.";
+$txt_choisir_disque_sup = "Select the record to delete.";
+$txt_choisir_artiste_maj = "Select the artist to modify.";
+$txt_choisir_artiste_sup = "Select the artist to delete.";
+
+//textes erreurs
+$txt_result_0 = "No result.";
+$txt_erreur_cases_vides = "Error : One or several lines are not filled.";
+$txt_lartiste = "The artist ";
+$txt_enregistre = "has been recorded successfully.";
+$txt_fiche = "File of";
+$txt_maj = "updated.";
+$txt_attention = "Beware !";
+$txt_sup_impos = "Some records have links to this data. Delete impossible, as far as the records have not been modified.";
+$txt_nouveau_for = "The new format";
+
+//libellés en-têtes de pages :
+$txt_maj_disque = "Update records";
+$txt_maj_artiste = "Update artists";
+$txt_resultat = "Résults for";
+$txt_suppression = "Delete records";
+$txt_ajout_art = "Add artists";
+$txt_sup_artist = "Delete artists";
+$txt_ajout_for = "Add format";
+$txt_ajout_pays = "Add country";
+
+//libellés boutons :
+$txt_raffraichir = "Refresh";
+$txt_envoyer = "Validate";
+$txt_modifier = "Modify";
+$txt_effacer = "Delete";
+$txt_ajouter = "Add";
+
+//Aide :
+$txt_aide_01 = "Maximum : 30 letters with in order, surname and name";
+$txt_aide_02 = "Maximum : 5 letters";
+$txt_aide_03 = "Maximum : 40 letters";
+$txt_aide_04 = "Maximum : 20 letters";
+$txt_aide_05 = "Maximum : 3 letters";
+?>
