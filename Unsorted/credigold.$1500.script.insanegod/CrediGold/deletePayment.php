07ec42774b56f66e4a5a192d6e35653cb4cea349
me-suzy <me.suzana@gmail.com>
2025-08-13 02:13:49 +0300

    Add Unsorted/credigold.$1500.script.insanegod/CrediGold/deletePayment.php

---
+++ Unsorted/credigold.$1500.script.insanegod/CrediGold/deletePayment.php
@@ -0,0 +1,97 @@
+<?
+
+// Delete Reoccuring Payment Module
+
+include("prepend.php3");
+
+page_open(array("sess" => "User_Session", "auth" => "Credigold_Auth"));
+
+global $dc, $rc, $id;
+
+$dc->query("SELECT user_id FROM ".$_Config["database_payments"]." WHERE id='$id' AND user_id='".$auth->auth["userNumber"]."';");
+
+if ($dc->num_rows() == 1)
+
+   {
+
+      $dc->query("DELETE FROM ".$_Config["database_payments"]." WHERE id='$id' LIMIT 1;");
+
+?>
+
+<link rel=stylesheet href=modules/styles.css>
+
+<script language=JavaScript src=modules/mod_gen.js></script>
+
+<body bgcolor=white leftmargin=10 topmargin=10 marginwidth=10 marginheight=10>
+
+<br>
+
+<p align=center class=head style=color:green>Reoccuring Payment Deleted Successfully!
+
+</p>
+
+<script>
+
+<!--
+
+function close()
+
+   {
+
+      setTimeout("parent.opener.location.href=parent.opener.location.href;top.window.close()",3000);
+
+   }
+
+window.onload = close;
+
+//-->
+
+</script>
+
+<?
+
+   }
+
+else
+
+   {
+
+?>
+
+<link rel=stylesheet href=modules/styles.css>
+
+<script language=JavaScript src=modules/mod_gen.js></script>
+
+<body bgcolor=white leftmargin=10 topmargin=10 marginwidth=10 marginheight=10>
+
+<br>
+
+<p align=center class=head style=color:red>Unauthorized Access to Module!
+
+</p>
+
+<script>
+
+<!--
+
+function close()
+
+   {
+
+      setTimeout("parent.opener.location.href=parent.opener.location.href;top.window.close()",3000);
+
+   }
+
+window.onload = close;
+
+//-->
+
+</script>
+
+<?
+
+   }
+
+// End of Delete Module
+
+?>
