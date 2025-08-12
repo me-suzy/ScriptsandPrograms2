94fa062045a4caff94114f9c3aa3975b14256efc
me-suzy <me.suzana@gmail.com>
2025-08-12 12:51:39 +0300

    Add Education/DigitalScribe1.4.1/DigitalScribe1.4.1/DigitalScribe/header1.php

---
+++ Education/DigitalScribe1.4.1/DigitalScribe1.4.1/DigitalScribe/header1.php
@@ -0,0 +1,28 @@
+</head>
+<body>
+
+<DIV id="navcontainer">
+<A HREF=index.php>
+<IMG SRC=/DigitalScribe/images/logosmall.gif width=158 height=63 alt="Digital Scribe Logo" border=0></a>
+<ul id="navlist">
+<li><A HREF=index.php>Home</a></li>
+
+<li><A HREF=liststuwork.php>Student Work</a></li>
+
+<li><A HREF=viewhw1.php>Homework</a></li>
+
+<li><A HREF=announcements.php>Announcements</a></li>
+
+<li><A HREF=addstuwork.php>Add Student Work</a></li>
+
+<li><A HREF=/DigitalScribe/login.php>Login</a></li>
+<li><A HREF=register.php>Register</a></li>
+<li><A HREF=http://www.digital-scribe.org/faq.php>Get Help</a></li>
+</ul>
+</div>
+
+
+<TABLE BORDER="0" CELLPADDING="0" CELLSPACING="4" BGCOLOR="#FFFFFF">
+<TR>
+
+<TD>
