9da6fc084eb52dd00d366db0f7a3362178a57415
me-suzy <me.suzana@gmail.com>
2025-08-12 09:59:49 +0300

    Add Development Tools/pas-3.6/pas/sitetemplate/index.php

---
+++ Development Tools/pas-3.6/pas/sitetemplate/index.php
@@ -0,0 +1,20 @@
+<?php 
+// Copyright 2001 - 2004 SQLFusion LLC           info@sqlfusion.com
+
+   /**
+    * Default index page.
+	*
+    * @package PASSiteTemplate
+    * @author Philippe Lewicki  <phil@sqlfusion.com>
+    * @copyright  SQLFusion LLC 2001-2004
+    * @version 3.0
+	*/
+
+  include_once("config.php") ;
+  $pageTitle = "Name of the Page" ;
+  include("includes/header.inc.php") ;
+?>
+
+<?php 
+  include("includes/footer.inc.php") ;
+?>
