e770a968b16fca4e305fc7e21ad625c57589e619
me-suzy <me.suzana@gmail.com>
2025-08-13 02:20:30 +0300

    Add Unsorted/datingscript/languages/es/esa.php

---
+++ Unsorted/datingscript/languages/es/esa.php
@@ -0,0 +1,122 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.1.1                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 04/05/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               esta.php                     #
+# File purpose            Additional langfile for admin    #
+# File created by         AzDG <support@azdg.com>          #
+############################################################
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Usuario o clav incorrecta - trate de nuevo',//1
+'Área de administración',//2
+'Nombre de administrador',//3
+'Clave de administrador',//4
+'Entrar',//5
+'Acceso cerrado para no administradores',//6
+'Página principal',//7
+'Borrar hits',//8
+'Reparar&Optimice',//9
+'Administración de usuarios',//10
+'Manejamiento de los FAQ',//11
+'Lista de correo',//12
+'Salir',//13
+'Usted salió de está área exitosamente<br><br>Gracias por usar este programa',//14
+'Total de correo',//15
+'Correo enviado',//16
+'La siguiente parte',//17
+'{0} mensajes enviaod a los usuarios',//18
+'Tiene mensajes sin leer {0}',//19 {0} - SiteName (C_SNAME)
+'Encontró {0} miembros',//20
+'Administración de lista de correo',//21
+'Con estado:',//22
+'Opciones de correo:',//23
+'Tipode correo',//24
+'De correo',//25
+'Asunto del mensaje',//26
+'Texto del mensaje',//27
+'Confirme el resultado',//28
+'Regrese',//29
+'Envíe correo',//30
+'Envíos sin terminar - {0}',//31
+'Asunto : {0} [Enviar a {1} usuarios] :::>',//32
+'Terminar este envío',//33
+'Texto',//34
+'HTML',//35
+'Vista preliminar',//36
+'Usuarios con mensajes por leer - ',//37
+'Total de mensajes por leer - ',//38
+'ID del ultimo mensaje - ',//39
+'Enviar mensaje de confirmación a usaurios con mensajes nuevo',//40
+'Perfil actualizado exitosamente',//41
+'Pregunta de FAQ creada exitosamente',//42
+'Crear nuevo',//43
+'Entre pregunta',//44
+'Entre respuesta',//45
+'FAQ administración',//46
+'Crear nueva categoria FAQ',//47
+'Edit FAQ category',//48
+'Delete FAQ category',//49
+'Create FAQ',//50
+'Select FAQ Category',//51
+'FAQ question',//52
+'FAQ answer',//53
+'First of all you must create FAQ categories',//54
+'Entre nombre de la nueva categoria',//55
+'Crear nueva categoria',//56
+'Nueva categoria creada:',//57
+'Nombre de nueva categoria',//58
+'Editar categoria',//59
+'Editar',//60
+'La categoria fue editada',//61
+'Editar categoria',//62
+'FAD fue borrada',//63
+'Categoria borrada',//64
+'Nota : Cuidado. usando borrar categoria, borrara todas las preguntas dentro de la categoria.',//65
+'Borrar',//66
+'0 - Esperando revisión de correo<br>1 - Esperar revisión después del registro<br>2 - Esperar revisión despues de actualizar<br>3,8 - Esperar para remover(sin,o mostrando)<br>7 - Usuario activo<br>',//67
+'Base de dato reparada',//68
+'Usuario ID#={0} fue borrado de la base de datos',//69
+'Las visitas fueron borradas',//70
+'Usuario agragado con id={0}',//71
+'IP',//72
+'Requirimiento [0 - 255]',//73
+'Estado - (Cuidado)',//74
+'Permitir',//75
+'Manejamiento de usuarios',//76
+'Total de usuarios activos :',//77
+'Usuarios esperando aprobación de perfil después de registar',//78
+'Usuarios esperando aprobación de perfil después de actualizar',//79
+'Ususarios esperando para remover',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
