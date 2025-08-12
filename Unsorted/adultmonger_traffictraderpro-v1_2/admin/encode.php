49563bd3faa6e63602fc44d786a05283f02e8951
me-suzy <me.suzana@gmail.com>
2025-08-13 00:44:21 +0300

    Add Unsorted/adultmonger_traffictraderpro-v1_2/admin/encode.php

---
+++ Unsorted/adultmonger_traffictraderpro-v1_2/admin/encode.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html><head><title>Stats</title>
+<STYLE type=text/css>.title {FONT: 10pt Verdana, Helvetica, sans-serif}</STYLE>
+<STYLE type=text/css>.main {FONT: 8pt Verdana, Helvetica, sans-serif; color:white}</STYLE>
+<STYLE type=text/css>.small {FONT: 7pt Verdana, Helvetica, sans-serif; color:white}</STYLE>
+<STYLE type=text/css>
+A:link {COLOR: #FFFFFF; TEXT-DECORATION: underline}
+A:visited {COLOR: #FFFFFF; TEXT-DECORATION: underline}
+A:active {COLOR: #FFFFFF; TEXT-DECORATION: underline}
+A:hover {COLOR: #0000FF; TEXT-DECORATION: none}
+</STYLE></head>
+
+<body bgcolor="#000000" background="../assets/am-interfacev1_r6_c4.jpg" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<table width="726" height="280" border="0" cellpadding="0" cellspacing="0" background="../assets/am-interfacev1_r6_c4.jpg">
+  <tr>
+    <td valign="middle" align=center class=main>
+<form name=enc>
+<table align=center cellspacing=1 cellpadding=3 border=0 width=300>
+    <tr><td bgcolor=#425B7E colspan=2><font class=main>Please use this page if you would like to add a gallery that consists of any abnormal characters such as "?,&,+". This tool is usefull if you are going to add hosted galleries to your TGP.</td></tr>
+    <tr><td bgcolor=#425B7E align=right><font class=main>URL:&nbsp;</td><td bgcolor=#557AB1 align=left><input type=text size=60 name=text1></td></tr>
+    <tr><td bgcolor=#425B7E align=right><font class=main>Encoded:&nbsp;</td><td bgcolor=#557AB1 align=left><input type=text size=60 name=text2></td></tr>
+    <tr><td bgcolor=#425B7E align=center colspan=2><input type=button onClick="javascript:enc.text2.value=escape(enc.text1.value);" value="Encode URL"></td></tr>
+</table>
+</form>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
+
+
