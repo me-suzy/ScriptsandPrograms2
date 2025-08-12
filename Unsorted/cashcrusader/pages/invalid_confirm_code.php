5a920ccec03065337c712a9d76d70af480cce490
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:05 +0300

    Add Unsorted/cashcrusader/pages/invalid_confirm_code.php

---
+++ Unsorted/cashcrusader/pages/invalid_confirm_code.php
@@ -0,0 +1,15 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" cellpadding="5" align="center">
+<tr> 
+<td align="center"><font size="4"><br>
+<br>
+Error!<br>
+Invalid confirmation code.<br>
+<br>
+<br>
+</font></td>
+</tr>
+</table>
+<br>
+<? include("footer.php");?>
