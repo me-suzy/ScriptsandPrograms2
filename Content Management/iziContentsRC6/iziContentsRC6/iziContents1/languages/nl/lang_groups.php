41b567235da3885b8e86aca1758eba4306439e9d
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:35 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_groups.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_groups.php
@@ -0,0 +1,72 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] 		= 'Beheer menu\'s';
+$GLOBALS["tFormTitle2"] 	= 'Beheer menu vertalingen';
+
+//  List Headings
+$GLOBALS["tMenuTitle"] 		= 'Menu titel';
+$GLOBALS["tHomepage"] 		= 'Startpagina menu';
+
+//  List Functions
+$GLOBALS["tMenuFilter"]		= 'Menu filter';
+$GLOBALS["tEditMenu"] 		= 'Bewerk menu link';
+$GLOBALS["tAddNewMenu"] 	= 'Nieuwe menu link toevoegen';
+$GLOBALS["tViewMenu"] 		= 'Laat menu link zien';
+$GLOBALS["tDeleteMenu"] 	= 'Verwijder menu link';
+$GLOBALS["tMakeHomepage"] 	= 'Maak startpagina';
+$GLOBALS["tHomepageSet"] 	= 'Actueel startpagina menu';
+$GLOBALS["tTranslate"] 		= 'Vertaal menu link';
+$GLOBALS["tViewTranslation"] 	= 'Toon vertaling';
+$GLOBALS["tEditTranslation"] 	= 'Bewerk vertaling';
+
+//  Form Block Titles
+$GLOBALS["thGeneral"] 		= 'Algemeen';
+$GLOBALS["thGraphics"] 		= 'Grafisch';
+$GLOBALS["thLinks"] 		= 'Modules en links';
+$GLOBALS["thSequence"] 		= 'Menu volgorde';
+$GLOBALS["thAccess"] 		= 'Toegangsbeveiliging';
+
+//  Form Field Headings
+$GLOBALS["tMenuRef"] 		= 'Menu naam';
+$GLOBALS["tMenuTitle"] 		= 'Menu titel';
+$GLOBALS["tParentMenu"] 	= 'Topmenu';
+$GLOBALS["tMenuLink"] 		= 'Menu link';
+$GLOBALS["tOpenMenuLink"] 	= 'Open menu link binnen pagina';
+$GLOBALS["tMenuImage1"] 	= 'Menu afbeelding';
+$GLOBALS["tMenuImage2"] 	= 'Menu afbeelding - muis erover';
+$GLOBALS["tMenuImage3"] 	= 'Menu afbeelding (geselecteerd)';
+$GLOBALS["tMenuImage4"] 	= 'Menu afbeelding (geselecteerd) - muis erover';
+$GLOBALS["tMenuHover"] 		= 'Menu zweeftekst';
+$GLOBALS["tShowMenu"] 		= 'Toon menu';
+$GLOBALS["tOrderBy"] 		= 'Volgorde menu op';
+$GLOBALS["tOrderDir"] 		= 'Volgorde richting';
+$GLOBALS["tMLoginReq"] 		= 'Login verplicht';
+$GLOBALS["tUsergroups"] 	= 'Gebruikersgroep';
+$GLOBALS["tAuthor"] 		= 'Menu eigenaar';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] 		= 'Met dit formulier kunt u menu links maken om door de website te navigeren.';
+$GLOBALS["hMenuRef"] 		= 'Dit is de interne naam die iziContents gebruikt.<br />Als u dit leeg laat, wordt er automatisch een referentie aangemaakt.';
+$GLOBALS["hMenuTitle"] 		= 'De naam van de menu groep. Dit wordt dan een link van de inhoud in dit menu.';
+$GLOBALS["hParentMenu"] 	= 'Het top menu link voor dit menu.';
+$GLOBALS["hMenuLink"] 		= 'Als dit veld wordt gevuld wordt er een harde link aangemaakt naar deze link door het menu, bijvoorbeeld: http:{groups';
+$GLOBALS["hOpenMenuLink"] 	= 'Menu links worden geopend in een nieuw venster wanneer de frames opties in het menu \'Server instellingen\' in ingesteld op \' geen frames\'.<br />Deze instelling hier aanvinken leidt tot het openen van de externe link binnen het inhoudssegment van het iziContents venster. Het zal alleen juist werken wanneer het script van de externe pagina ontworpen is om samen te werken met iziContents.';
+$GLOBALS["hMenuImage1"] 	= 'De afbeelding die de menu titel vervangt. (De afmeting in de breedte moet minder zijn dan de breedte van het menu.)';
+$GLOBALS["hMenuImage2"] 	= 'De afbeelding die getoond wordt wanneer de muis over menu afbeelding gaat.<br />(De afmeting in de breedte moet minder zijn dan de breedte van het menu.)';
+$GLOBALS["hMenuImage3"] 	= 'De afbeelding die getoond wordt wanneer het menu item het op dat moment geselecteerd menu is.<br />(De afmeting in de breedte moet minder zijn dan de breedte van het menu.)';
+$GLOBALS["hMenuImage4"] 	= 'De afbeelding die getoond wordt wanneer het menu item het op dat moment geselecteerd menu is en wanneer de muis over de menu afbeelding gaat.<br />(De afmeting in de breedte moet minder zijn dan de breedte van het menu.)';
+$GLOBALS["hMenuHover"] 		= 'De tekst die getoond wordt wanneer de cursor over een menu item zweeft. Wordt alleen getoond als deze eigenschap geactiveerd is.';
+$GLOBALS["hShowMenu"] 		= 'Indien dit vakje aangevinkt is, wordt dit menu getoond; geen vinkje, dan wordt het meu niet getoond.';
+$GLOBALS["hOrderBy"] 		= 'Selecteer op welke wijze de inhoud van dit menu in volgorde gezet moet worden';
+$GLOBALS["hOrderDir"] 		= 'Selecteer de richting van de volgorde(Oplopend of Aflopend).';
+$GLOBALS["hMLoginReq"] 		= 'Wanneer dit vakje is aangevinkt, dan is het noodzakelijk om ingelogd te zijn om toegang tot dit menu te krijgen.';
+$GLOBALS["hUsergroups"] 	= 'Login is restricted to members of these selected usergroups.';
+$GLOBALS["hAuthor"] 		= 'Dit is de \'eigenaar\' van het menu. Wanneer Onderdeel-level beveiliging is ingeschakeld, kan alleen de eigenaar van het menu items aan dit menu toevoegen.';
+
+//  Error Messages
+$GLOBALS["eMenuExists"] 	= 'Een menu met deze naam bestaat al.';
+$GLOBALS["eInvalidName"] 	= 'Er staan verkeerde tekens in de menu naam.';
+$GLOBALS["eTitleEmpty"] 	= 'Menu titel kan niet leeg zijn.';
+
+?>
