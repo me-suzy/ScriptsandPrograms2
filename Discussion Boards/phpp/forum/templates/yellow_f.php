da939a57647aa718c186fddc8ebee26d8953c711
me-suzy <me.suzana@gmail.com>
2025-08-12 10:48:09 +0300

    Add Discussion Boards/phpp/forum/templates/yellow_f.php

---
+++ Discussion Boards/phpp/forum/templates/yellow_f.php
@@ -0,0 +1,7 @@
+</div>
+<div id="footerbar"></div>
+<div class="shadow"><img src="gfx/blank.gif" width="1" height="1" alt="*"/></div>
+<div id="footer">Design © 2004 <a href="http://www.i-produce.co.uk" target="_blank">i-produce</a>. Powered by <a href="http://www.php-post.co.uk" target="_blank">PHP-Post</a> <? include "ver.php"; ?>.</div>
+</div>
+</body>
+</html>
