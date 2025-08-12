bb1522cacdf73b4ab28f5303902398fd397feec4
me-suzy <me.suzana@gmail.com>
2025-08-12 23:15:57 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/include/lang-spanish.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/include/lang-spanish.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric cedric.claire@safari-msi.com www.portailphp.com
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// LIBELLE DES RUBRIQUES
+$Lib_Rub_Navig="<font size=4>N</font>AVEGACION";
+$Lib_Rub_ForumLive="<font size=4>F</font>ORUM <font size=4>L</font>IVE";
+$Lib_Rub_WebRing="<font size=4>W</font>EBRING";
+$Lib_Rub_Param="<font size=4>P</font>ARAMETERS";
+$Lib_Rub_Parten="<font size=4>P</font>ARTNERS";
+$Lib_Rub_Pinup="<font size=4>P</font>INUP";
+$Lib_Rub_News="<font size=4>N</font>EWS";
+
+// LES RUBRIQUES
+$Rub_Home="Inicio";
+$Rub_Membres="Admin";
+$Rub_Navigation="Navegación";
+$Rub_News="Artículos";
+$Rub_Search="Búsqueda";
+$Rub_File="Descarga";
+$Rub_Liens="Vínculos";
+$Rub_Faq="FAQ";
+$Rub_Forum="Foro";
+$Rub_Photos="Fototeca";
+
+//LES SOUS RUBRIQUES
+$SsRub_News_plusrecents="Los + recientes";
+$SsRub_News_pluslus="Los + leidos";
+$SsRub_File_Proposer="Proponer";
+$SsRub_File_plusrecents="Los + recientes";
+$SsRub_File_plusclics="Los + clics";
+$SsRub_Liens_Proposer="Proponer";
+$SsRub_Liens_plusrecents="Los + recientes";
+$SsRub_Liens_top="El Top 10";
+
+//MESSAGES D'ERREURS
+$Err_Serveur="Imposible conectarse al servidor";
+$Err_Db="Imposible conectarse a la base";
+$Err_Modif="No se puede modificar";
+$Err_Search="No se puede efectuar la busqueda";
+$Err_Insert="No se puede efectuar la inserción";
+$Err_Supp="No se puede efectuar la supresión";
+$Err_Upload_Chmod="No se puede modificar los derechos";
+$Err_Upload_Move="No se puede escribir en el servidor";
+?>
