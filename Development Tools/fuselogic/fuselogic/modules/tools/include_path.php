90b9dbe971793fe34772beef1108a9da1a908bee
me-suzy <me.suzana@gmail.com>
2025-08-12 09:29:18 +0300

    Add Development Tools/fuselogic/fuselogic/modules/tools/include_path.php

---
+++ Development Tools/fuselogic/fuselogic/modules/tools/include_path.php
@@ -0,0 +1,19 @@
+<?php
+
+singletonQueue();
+
+require_once('class/class.include_path.php');
+$include_path = &new include_path();
+
+$path[] = getcwd().'/class';
+$path[] = getcwd().'/functions';
+$path[] = './class';
+$path[] = './functions';
+$path[] = './scripts';
+
+$include_path->add($path);
+
+//sequence is important
+require_once('function.__autoload.php');
+
+?>
