d8aafe985eced71d565ebdb548b5be4427d921bf
me-suzy <me.suzana@gmail.com>
2025-08-12 18:19:54 +0300

    Add Image Galleries/sb-photoalbum_0.36/SBpa/photoalbum/lang/cs.php

---
+++ Image Galleries/sb-photoalbum_0.36/SBpa/photoalbum/lang/cs.php
@@ -0,0 +1,43 @@
+<?php
+//******************************//
+//     PhotoAlbum Lang file     //
+//         ---> CZ <---         //
+//******************************//
+define("pa_charse", "windows-1250");
+define("pa_lang_code", "cs");
+define("pa_date_format", "d.m.Y H:i:s");
+// TEXTy
+define("pa_txt_loading", "Aplikace se naèítá, prosím poèkejte...<br /><br /><small>(Pro správnou funkènost aplikace je nutné povolit JavaScript)</small>");
+define("pa_txt_loading_dir", "Naèítám obsah adresáøe...");
+define("pa_txt_no_images_in_dir", "V adresáøi nejsou ádné obrázky.");
+define("pa_txt_yes", "ano");
+define("pa_txt_no", "ne");
+define("pa_txt_slideshow", "SLIDESHOW");
+define("pa_txt_slideshow_is_disabled", "Slideshow je zakázána nastavením fotogalerie.");
+define("pa_txt_slideshow_time", "Interval mezi obrázky");
+define("pa_txt_slideshow_time_depend_on_conn_speed", "tento èas se mùe prodlouit v závislosti na rychlosti pøipojení k internetu.");
+define("pa_txt_slideshow_fullscreen", "Zobrazit pøes celou obrazovku");
+define("pa_txt_slideshow_start", "spustit");
+define("pa_txt_slideshow_stop", "zastavit");
+define("pa_txt_imginfo", "OBRÁZEK");
+define("pa_txt_lang", "JAZYK");
+define("pa_txt_about", "O APLIKACI");
+define("pa_txt_fullscreen", "celá obrazovka");
+define("pa_txt_date", "datum");
+define("pa_txt_author", "autor");
+define("pa_txt_file_size", "velikost");
+define("pa_txt_image_resultion", "rozliení");
+define("pa_txt_image_link", "Trvalý odkaz na obrázek");
+
+define("pa_txt_image_exif_camera", "fotoaparát");
+define("pa_txt_image_exif_exposure_time", "doba expozice");
+define("pa_txt_image_exif_fnumber", "clona");
+define("pa_txt_image_exif_focal_length", "ohnisková vzdálenost");
+define("pa_txt_image_exif_iso", "citlivost (ISO)");
+define("pa_txt_image_exif_flash", "blesk");
+
+define("pa_txt_homepage", "více informací naleznete na");
+define("pa_txt_close_window", "ZAVØÍT OKNO");
+define("pa_txt_poweredby", "Pouívá");
+define("pa_txt_select_lang", "Zvolte si jazyk");
+?>
