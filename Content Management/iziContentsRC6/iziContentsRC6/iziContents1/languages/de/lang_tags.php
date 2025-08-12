10068c6a7d600de7020c8d9b0feb76e811fa0ed4
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:58 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_tags.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_tags.php
@@ -0,0 +1,30 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Kurzbefehle verwalten';
+
+//  List Headings
+$GLOBALS["tCategory"] = 'Kategorie';
+$GLOBALS["tTag"] = 'Tag';
+$GLOBALS["tTranslation"] = '&Uuml;bersetzung';
+
+//  List Functions
+$GLOBALS["tAddNewTag"] = 'Neuen Kurzbefehl hinzuf&uuml;gen';
+$GLOBALS["tViewTag"] = 'Kurzbefehl ansehen';
+$GLOBALS["tEditTag"] = 'Kurzbefehl &auml;ndern';
+$GLOBALS["tDeleteTag"] = 'Kurzbefehl l&ouml;schen';
+
+//  Form Block Titles
+$GLOBALS["thTagGeneral"] = 'Kurzbefehl-Details';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'In diesem Formular k&ouml;nnen Sie benutzerdefinierte Kurzbefehle definieren, die Sie in den Artikeln f&uuml;r die Inhalte Ihrer Webseite verwenden k&ouml;nnen, z.B. HTML-Tags. ';
+$GLOBALS["hCategory"] = 'W&auml;hlen Sie die Kategorie aus, die der Gruppe am n&auml;hesten kommt, zu der der neue Kurzbefehl geh&ouml;rt.';
+$GLOBALS["hTag"] = 'Dies ist der Kurzbefehl, den Sie innerhalb Ihres Artikel-Inhaltes verwenden werden.<br />Innerhalb des Artikels sollte der Kurzbefehl in eckigen Klammern geschrieben werden.<b>[tag]</b> Um diese Kurzbefehle auseinander zu halten, identifizieren Sie sie einfach mit einem Namen, z.B. <b>tag</b>.<br />Wenn Sie &ouml;ffnende und schliessende Kurzbefehle ben&ouml;tigen, wie sie bei fetter Schrift verwendet werden, (<b>[b]</b> und <b>[/b]</b>), m&uuml;ssen Sie beide Kurzbefehle definieren, z.B. <b>tag</b> and <b>/tag</b>.';
+$GLOBALS["hTranslation"] = 'Dies ist die &Uuml;bersetzung von Ihrem Kurzbefehl in sauberes HTML.';
+
+//  Error Messages
+$GLOBALS["eNoTag"] = 'Sie m&uuml;ssen einen Namen f&uuml;r den Kurzbefehl eingeben.';
+$GLOBALS["eNoTranslation"] = 'Die &Uuml;bersetzung muss eingegeben werden.';
+
+?>
