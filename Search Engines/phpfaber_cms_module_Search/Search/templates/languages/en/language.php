2ee17ff0a152f793bcb6991db8a7cc661881fc5d
me-suzy <me.suzana@gmail.com>
2025-08-12 23:39:05 +0300

    Add Search Engines/phpfaber_cms_module_Search/Search/templates/languages/en/language.php

---
+++ Search Engines/phpfaber_cms_module_Search/Search/templates/languages/en/language.php
@@ -0,0 +1,57 @@
+<?php
+
+// -----------------------------------------------------------------------------
+//
+// phpFaber CMS v.1.0
+// Copyright(C), phpFaber LLC, 2004-2005, All Rights Reserved.
+// E-mail: products@phpfaber.com
+//
+// All forms of reproduction, including, but not limited to, internet posting,
+// printing, e-mailing, faxing and recording are strictly prohibited.
+// One license required per site running phpFaber CMS.
+// To obtain a license for using phpFaber CMS, please register at
+// http://www.phpfaber.com/i/products/cms/
+//
+// 06:11 PM 08/10/2005
+//
+// -----------------------------------------------------------------------------
+
+if (!defined('INDEX_INCLUDED')) die('<code>You cannot access this file directly..</code>');
+
+$_LNG['_PAGE_TITLE_SL'] = 'Spider log';
+
+$_LNG['_SETT_SITE_SIZE'] = 'Site size';
+  $_LNG['_SETT_SITE_SIZE_T'] = 'Tiny ( ~1Mb )';
+  $_LNG['_SETT_SITE_SIZE_M'] = 'Medium ( ~10Mb )';
+  $_LNG['_SETT_SITE_SIZE_B'] = 'Big ( ~50Mb )';
+  $_LNG['_SETT_SITE_SIZE_L'] = 'Large ( >100Mb )';
+$_LNG['_SETT_RES_NUM'] = 'Number of rows per page';
+$_LNG['_SETT_MIN_LENGTH'] = 'Minimum word length to index';
+$_LNG['_SETT_INDEXING_SCHEME'] = 'Indexing scheme';
+  $_LNG['_SETT_INDEXING_SCHEME_1'] = 'Whole word';
+  $_LNG['_SETT_INDEXING_SCHEME_2'] = 'Beginning of the word';
+  $_LNG['_SETT_INDEXING_SCHEME_3'] = 'Every substring';
+$_LNG['_SETT_NUMBERS'] = 'Index numbers and other characters';
+$_LNG['_SETT_NUMBERS_NOTE'] = 'Leave it empty if you don\'t want to index numbers. You may add here other  characters, which you want to index';
+$_LNG['_SETT_STOP_WORDS'] = 'List of words, which should not be indexed';
+$_LNG['_SETT_EN_USE_META'] = 'Index META tags ?';
+$_LNG['_SETT_DESCR_SIZE'] = 'Define length of page description in output';
+
+$_LNG['_IND_SEARCH_WORDS'] = 'Search words';
+$_LNG['_IND_QUERY'] = 'Query';
+$_LNG['_IND_SEARCH_TIME'] = 'Search time';
+$_LNG['_IND_FOUND'] = 'Found';
+$_LNG['_IND_PAGES'] = 'pages';
+$_LNG['_IND_WORDS'] = 'words';
+
+$_LNG['_IND_ALL'] = 'ALL';
+$_LNG['_IND_ANY'] = 'ANY';
+
+$_LNG['_CRON_START'] = 'Start indexing';
+$_LNG['_CRON_SCAN_TIME'] = 'Scan took, sec';
+$_LNG['_CRON_INDEXED_NUM'] = "Total files has been indexed";
+$_LNG['_CRON_INDEXED_NO'] = 'No files has been indexed';
+$_LNG['_CRON_SITEWORDS'] = 'Writing SITEWORDS';
+$_LNG['_CRON_BUILDHASH'] = 'Build hash';
+
+?>
