2c5787d941656fa97755dd45b46575fb5418a027
me-suzy <me.suzana@gmail.com>
2025-08-12 11:43:01 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/packingslip.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/packingslip.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('TABLE_HEADING_COMMENTS', 'Comments');
+define('TABLE_HEADING_PRODUCTS_MODEL', 'Model');
+define('TABLE_HEADING_PRODUCTS', 'Products');
+
+define('ENTRY_SOLD_TO', 'SOLD TO:');
+define('ENTRY_SHIP_TO', 'SHIP TO:');
+define('ENTRY_PAYMENT_METHOD', 'Payment Method:');
+?>
