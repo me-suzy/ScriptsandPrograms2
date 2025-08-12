c7d9f6614217ed279ef7845d50a8dce3a6afa277
me-suzy <me.suzana@gmail.com>
2025-08-12 11:52:15 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/affiliate_password_forgotten.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/affiliate_password_forgotten.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Login');
+define('NAVBAR_TITLE_2', 'Affiliate Password Forgotten');
+define('HEADING_TITLE', 'I\'ve Forgotten My Affiliate Password!');
+define('TEXT_NO_EMAIL_ADDRESS_FOUND', '<font color="#ff0000"><b>NOTE:</b></font> The E-Mail Address was not found in our records. Please try again.');
+define('EMAIL_PASSWORD_REMINDER_SUBJECT', STORE_NAME . ' - New Affiliate Password');
+define('EMAIL_PASSWORD_REMINDER_BODY', 'A new affiliate password was requested from ' . $REMOTE_ADDR . '.' . "\n\n" . 'Your new affiliate password to \'' . STORE_NAME . '\' is:' . "\n\n" . '   %s' . "\n\n");
+define('TEXT_PASSWORD_SENT', 'A New Affiliate Password Has Been Sent To Your Email Address');
+?>
