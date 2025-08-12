74ce5aa81daab7a161398759f54e0f5c30013f78
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:17 +0300

    Add Content Management/metajour2_1/metajour/lang/customformelement.en.php

---
+++ Content Management/metajour2_1/metajour/lang/customformelement.en.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Name (a-z)';
+$LANG['label_label'] = 'Description';
+$LANG['label_fieldtype'] = 'Field type';
+$LANG['label_fieldrelation'] = 'Relation to';
+$LANG['label_fieldwidth'] = 'Width';
+$LANG['label_fieldheight'] = 'Height';
+$LANG['label_groupwithprev'] = 'Keep together with previous';
+$LANG['label_keeptogether'] = 'On line with previous';
+$LANG['label_fieldwidthpx'] = 'Width (exact)';
+$LANG['label_onlist'] = 'Show in listing';
+$LANG['name'] = 'Form fields';
+
+$LANG['option_fieldwidth'][0] = 'Normal';
+$LANG['option_fieldwidth'][1] = 'Narrow';
+$LANG['option_fieldwidth'][2] = 'Wide';
+
+$LANG['option_fieldtype']['UI_STRING'] = 'String';
+$LANG['option_fieldtype']['UI_TEXT_WRAP'] = 'Text area';
+$LANG['option_fieldtype']['UI_DATE'] = 'Date';
+$LANG['option_fieldtype']['UI_RELATION'] = 'Relation to table';
+$LANG['option_fieldtype']['UI_RELATION_MULTIPLE'] = 'Multiple relation to table';
+$LANG['option_fieldtype']['UI_LISTDIALOG'] = 'Relation to table (in popup)';
+
+?>
