5664d6a4bb4991581c0c3107ce75c5811780df5f
me-suzy <me.suzana@gmail.com>
2025-08-12 11:42:39 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/index.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/index.php
@@ -0,0 +1,43 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Choose an action..');
+
+define('BOX_TITLE_ORDERS', 'Orders');
+define('BOX_TITLE_STATISTICS', 'Statistics');
+
+define('BOX_ENTRY_SUPPORT_SITE', 'Support Site');
+define('BOX_ENTRY_SUPPORT_FORUMS', 'Support Forums');
+define('BOX_ENTRY_MAILING_LISTS', 'Mailing Lists');
+define('BOX_ENTRY_BUG_REPORTS', 'Bug Reports');
+define('BOX_ENTRY_FAQ', 'FAQ');
+define('BOX_ENTRY_LIVE_DISCUSSIONS', 'Live Discussions');
+define('BOX_ENTRY_CVS_REPOSITORY', 'CVS Repository');
+define('BOX_ENTRY_INFORMATION_PORTAL', 'Information Portal');
+define('BOX_ENTRY_OSCDOX', 'OSCdox.com');
+
+define('BOX_ENTRY_CUSTOMERS', 'Customers:');
+define('BOX_ENTRY_PRODUCTS', 'Products:');
+define('BOX_ENTRY_REVIEWS', 'Reviews:');
+
+define('BOX_CONNECTION_PROTECTED', 'You are protected by a %s secure SSL connection.');
+define('BOX_CONNECTION_UNPROTECTED', 'You are <font color="#ff0000">not</font> protected by a secure SSL connection.  It is HIGHLY RECOMMENDED to use SSL security.  Please NEVER view real credit card numbers over a connection not using SSL (https://).  To use SSL security, use the same URL you are using now except either switch the http:// with https:// or ask your web host what your ssl server address is and use that instead of your domain name.');
+define('BOX_CONNECTION_UNKNOWN', 'unknown');
+
+define('CATALOG_CONTENTS', 'Contents');
+
+define('REPORTS_PRODUCTS', 'Products');
+define('REPORTS_ORDERS', 'Orders');
+
+define('TOOLS_BACKUP', 'Backup');
+define('TOOLS_BANNERS', 'Banners');
+define('TOOLS_FILES', 'Files');
+?>
