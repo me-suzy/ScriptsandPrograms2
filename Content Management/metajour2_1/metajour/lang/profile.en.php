ec701f5c3eb32ab670b858181e17e0486cf58199
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:48 +0300

    Add Content Management/metajour2_1/metajour/lang/profile.en.php

---
+++ Content Management/metajour2_1/metajour/lang/profile.en.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Name';
+$LANG['name'] = 'Profiles';
+
+$LANG['label_editorstyle'] = 'Editor - basic style';
+$LANG['label_editortable'] = 'Editor - table functions';
+$LANG['label_editorcolor'] = 'Editor - colors';
+$LANG['label_editorspecial'] = 'Editor - links and images';
+?>
