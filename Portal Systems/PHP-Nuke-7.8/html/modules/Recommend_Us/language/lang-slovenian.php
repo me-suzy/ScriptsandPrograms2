2af368c96462ca8c424084f4be9ff6cd44717d81
me-suzy <me.suzana@gmail.com>
2025-08-12 22:53:26 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Recommend_Us/language/lang-slovenian.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Recommend_Us/language/lang-slovenian.php
@@ -0,0 +1,37 @@
+<?php
+
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+
+define("_SEND","Polji");
+define("_FYOURNAME","Vae ime:");
+define("_FYOUREMAIL","Va E-mail:");
+define("_FFRIENDNAME","Ime Vaega prijatelja:");
+define("_FFRIENDEMAIL","E-mail Vaega prijatelja:");
+define("_HELLO","Pozdravljen");
+define("_YOURFRIEND","Vaemu prijatelju");
+define("_RECOMMEND","Priporoèi stran prijatelju");
+define("_INTSITE","Zanimiva stran:");
+define("_OURSITE","se mu zdi naa stran");
+define("_INTSENT","zanimiva in Vam jo eli poslati.");
+define("_FSITENAME","Naslov strani:");
+define("_FSITEURL","Naslov (URL):");
+define("_FREFERENCE","Priporoèilo za nao stran je bilo poslano");
+define("_THANKSREC","Hvala za vae priporoèilo!");
+define("_SECURITYCODEERROR","<b>ERROR:</b> The security code you wrote is incorrect...<br>Please write it exactly as given bellow:");
+
+?>
