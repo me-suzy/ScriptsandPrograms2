8b4f0e7eef2301795132d968676214cb015bd08b
me-suzy <me.suzana@gmail.com>
2025-08-12 11:52:00 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/affiliate_banners.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/affiliate_banners.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Affiliate Program');
+define('HEADING_TITLE', 'Affiliate Program: Banners');
+
+define('TEXT_AFFILIATE_NAME', 'Banner Name:');
+define('TEXT_INFORMATION', 'Choose the banner or link you want to display on your website from the choices below:');
+define('TEXT_AFFILIATE_INFO', 'Copy the code shown below and paste into your website');
+define('TEXT_AFFILIATE_INDIVIDUAL_BANNER', 'BUILD-A-LINK');
+define('TEXT_AFFILIATE_INDIVIDUAL_BANNER_INFO', 'Enter the product number you wish to link to.');
+
+define('IMAGE_BUTTON_BUILD_A_LINK','Build a Link for a Product Banner.');
+?>
