11b8f4f58ae0d6d5ba5eda49fd5b592f1f201fd8
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:33 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/businesscardavery7413.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/businesscardavery7413.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 17;
+function objfile_17 () {
+$obj = owNew('filter');
+$objdata['name'] = "Businesscard, Avery 7413";
+$objdata['datatype'] = "contact";
+$objdata['content'] = "";
+$objdata['mimetype'] = "application/msword";
+$objdata['classtype'] = "";
+$objdata['filtertype'] = "5";
+$objdata['filterfiletype'] = "rtf";
+$objdata['binfileid'] = "Avery7413.rtf";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
