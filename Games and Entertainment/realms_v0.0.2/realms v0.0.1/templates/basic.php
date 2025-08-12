de4a97bc96457d7ce4b1d62a24f8204af2e1a542
me-suzy <me.suzana@gmail.com>
2025-08-12 16:33:17 +0300

    Add Games and Entertainment/realms_v0.0.2/realms v0.0.1/templates/basic.php

---
+++ Games and Entertainment/realms_v0.0.2/realms v0.0.1/templates/basic.php
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+<title>{title}</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<TABLE width=100% Border=0 valign=top><TR valign=top><TD valign=top>{menu}<br>{event}</TD></TR><TR><TD>
+<TABLE Border=1 valign=top><TR valign=top><TD valign=top>
+<TABLE Border=0 WIDTH=100% valign=top><TR valign=top><TD valign=top>{right}</TD></TR><TR><TD valign=top>{left}</TD></TR></TABLE>
+</TD><TD WIDTH=100% valign=top><TABLE Border=0 WIDTH=100% valign=top><TR valign=top><TD valign=top>{content}</TD></TR><TR><TD valign=top>{lowregion}<BR>{lowchat}</TD></TR></TABLE></TD></TR>
+</TABLE>
+</TD></TR></TABLE>
+</body>
+</html>
