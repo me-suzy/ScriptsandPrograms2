54bcbf8fc3c97bfc5eefbcbdd794b374b3254677
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:52 +0300

    Add Content Management/metajour2_1/metajour/lang/event.da.php

---
+++ Content Management/metajour2_1/metajour/lang/event.da.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Hændelser';
+$LANG['label_name'] = 'Navn';
+$LANG['label_content'] = 'Besked';
+$LANG['label_subject'] = 'Emnelinie';
+$LANG['label_triggertype'] = 'Tabel';
+$LANG['label_triggerevent'] = 'Ved handling';
+$LANG['label_msgtype1'] = 'Send besked som';
+$LANG['label_msgdest1'] = 'Til følgende';
+$LANG['label_msgtype2'] = 'Send besked som';
+$LANG['label_msgdest2'] = 'Til følgende';
+
+$LANG['option_triggerevent']['create'] = 'Opret';
+$LANG['option_triggerevent']['update'] = 'Opdater';
+$LANG['option_triggerevent']['delete'] = 'Slet';
+$LANG['option_triggerevent']['requestapproval'] = 'Anmod om godkendelse';
+$LANG['option_triggerevent']['approvepublish'] = 'Godkend og udgiv';
+
+$LANG['option_msgtype1']['0'] = '[INGEN]';
+$LANG['option_msgtype1']['1'] = 'Intern meddelelse';
+$LANG['option_msgtype1']['2'] = 'Email (hvis adresse findes på bruger)';
+
+$LANG['option_msgdest1']['0'] = '[INGEN]';
+$LANG['option_msgdest1']['1'] = 'EJER AF POST';
+$LANG['option_msgdest1']['2'] = 'EJER AF EJER AF POST';
+$LANG['option_msgdest1']['3'] = 'KONTROLLANT FOR POST';
+$LANG['option_msgdest1']['4'] = 'AKTUELLE BRUGER';
+$LANG['option_msgdest1']['5'] = 'EJER AF AKTUELLE BRUGER';
+$LANG['option_msgdest1']['6'] = 'BRUGERE MED ADGANG TIL POST';
+
+$LANG['option_msgtype2']['0'] = '[INGEN]';
+$LANG['option_msgtype2']['1'] = 'Intern meddelelse';
+$LANG['option_msgtype2']['2'] = 'Email (hvis adresse findes på bruger)';
+?>
