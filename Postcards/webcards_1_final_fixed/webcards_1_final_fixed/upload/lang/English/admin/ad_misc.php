712597bb4b3222019f2e1e830d177e8c79023110
me-suzy <me.suzana@gmail.com>
2025-08-12 23:28:29 +0300

    Add Postcards/webcards_1_final_fixed/webcards_1_final_fixed/upload/lang/English/admin/ad_misc.php

---
+++ Postcards/webcards_1_final_fixed/webcards_1_final_fixed/upload/lang/English/admin/ad_misc.php
@@ -0,0 +1,7 @@
+<?php
+$lang['bad_user_pass'] = "Your username and password combination were not found.";
+$lang['login_success'] = "Login Successful";
+$lang['click_proceed_acp'] = "Click here to proceed to the admin centre.";
+$lang['logged_out'] = "You have been logged out.";
+$lang['back_to_acp'] = "Back to the admin centre";
+?>
