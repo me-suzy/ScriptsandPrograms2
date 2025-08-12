6e32f68e60d39a7b063d30855f90a35d6634ad8a
me-suzy <me.suzana@gmail.com>
2025-08-12 20:04:30 +0300

    Add News Publishing/Null_News/config.php

---
+++ News Publishing/Null_News/config.php
@@ -0,0 +1,16 @@
+
+<?php
+$Admin_Email = "your email";
+$Site_Name = "your sites name";
+$Site_URL = "your url";
+$admin_Name = "your name";
+//admin pass
+$pass = "adminpass3";
+//database pass
+$password = "dbasebass";
+//username
+$username = "user";
+//dbase name.
+$dbase = "dbase";
+
+?>
