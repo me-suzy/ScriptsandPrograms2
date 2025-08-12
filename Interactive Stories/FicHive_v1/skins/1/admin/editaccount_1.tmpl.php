8e8ae3255e1e26cd760d5a817695dcfe996bbbb8
me-suzy <me.suzana@gmail.com>
2025-08-12 18:48:22 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/editaccount_1.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/editaccount_1.tmpl.php
@@ -0,0 +1,4 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%ACCOUNTNO%></td><td><input type='text' name='edit'></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>'></td></tr>
+</table>
