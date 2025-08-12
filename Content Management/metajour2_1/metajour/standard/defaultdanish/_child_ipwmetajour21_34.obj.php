8352a0f016928a2e9eb3fcfe1cb3c471b95a7e25
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:21 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ipwmetajour21_34.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ipwmetajour21_34.obj.php
@@ -0,0 +1,21 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 34;
+function objfile_34 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "IPW METAjour 2.1";
+$objdata['subname'] = "";
+$objdata['extension'] = "";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "<P align=left>IPW METAjour er&nbsp;et open source content management system fra <A href=\"http://www.ipwsystems.dk/\" target=_blank>IPW Systems A/S</A>. Hele IPW METAjour kan du ganske vederlagsfrit downloade fra vores open source website <A href=\"http://www.metajour.org\">www.metajour.org</A>.&nbsp;&nbsp;&nbsp; </P>
+<P align=left>IPW METAjour er et high-end content management system, hvor fokus er lagt på både brugervenlighed og fleksibilitet. Blandt andet derfor, har vi været i stand til at vinde en god position, i et marked hvor der allerede findes hundredevis af leverandører.</P>
+<P align=left>IPW METAjour er brugervenligt, således at enhver person hurtigt kan lære at vedligeholde og udbygge websites, intranet og extranet. Håndtering af dokumenter og menuer sker i en genkendelig brugerflade. Tilkobling af færdige komponenter som debatforum og webshop sker med kun ganske få museklik.</P>
+<P>IPW METAjour er fleksibelt, således at systemet kan håndtere websites af enhver størrelse, i vilkårligt&nbsp;antal sprog, og kan gennem skabeloner modificeres til at opfylde alle behov. Komplet brugerstyring giver mulighed for uddelegering af opgaver på ethvert niveau.</P>
+<P>Vil du&nbsp;vide mere om mulighederne i IPW METAjour, <A href=\"http://www.ipwsystems.dk/showpage.php?pageid=1488\" target=_blank>kan du læse mere her</A>.</P>";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
