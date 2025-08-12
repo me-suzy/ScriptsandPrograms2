2d3feaef6dc7618a89e407950627ebf2292a74ca
me-suzy <me.suzana@gmail.com>
2025-08-12 14:01:25 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/template/mail/removal.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/template/mail/removal.php
@@ -0,0 +1,5 @@
+<?
+
+$removal = "\n\n\n\n\n\nYou are receiving this message because you signed up for $exchangename. If you no longer wish to receive these newsletters, please <a href=\"$baseurl/client/remove.php?email=%email%&id=%id%\">click here</a> to set your mailing preferences to not receive mailings from us. Please note that we will still contact you, but only in emergencies or important information regarding the Banner Exchange..\n\n";
+
+?>
