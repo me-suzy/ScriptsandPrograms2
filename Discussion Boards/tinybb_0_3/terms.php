07789fa427b67cfb7a41900880eebcc1d29d5de4
me-suzy <me.suzana@gmail.com>
2025-08-12 10:54:26 +0300

    Add Discussion Boards/tinybb_0_3/terms.php

---
+++ Discussion Boards/tinybb_0_3/terms.php
@@ -0,0 +1,10 @@
+<?php
+require_once("headers.php");
+
+echo "<p>While the administrators and moderators of our forum will attempt to remove or edit any generally objectionable material as quickly as possible, it is impossible to review every message. Therefore you acknowledge that all posts made to these forums express the views and opinions of the author and not the administrators, moderators or webmaster (except for posts by these people) and hence will not be held liable.</p>\n";
+echo "<p>You agree not to post any abusive, obscene, vulgar, slanderous, hateful, threatening, sexually-oriented or any other material that may violate any applicable laws. Doing so may lead to you being immediately and permanently banned (and your service provider being informed). The IP address of all posts is recorded to aid in enforcing these conditions. You agree that the webmaster, administrator and moderators of this forum have the right to remove, edit, move or close any topic at any time should they see fit. As a user you agree to any information you have entered being stored in a database. While this information will not be disclosed to any third party without your consent the webmaster, administrator and moderators cannot be held responsible for any hacking attempt that may lead to the data being compromised.</p>\n";
+echo "<p>The e-mail address entered is used only for confirming your registration details &amp; password (and for sending new passwords should you forget your current one) and to e-mail you news relating to $tinybb_url.</p>\n";
+echo "<p>By registering you agree to be bound by these conditions.</p>\n";
+
+require "footers.php";
+?>
