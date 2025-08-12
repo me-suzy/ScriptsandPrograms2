5fa04717cd37608022699c3f9b92db904f159db5
me-suzy <me.suzana@gmail.com>
2025-08-12 07:31:57 +0300

    Add Content Management/mysource_2.10.2/mysource_2.10.2/xtras/wizards/bmail_summary/mysql_web_Changelog.sql

---
+++ Content Management/mysource_2.10.2/mysource_2.10.2/xtras/wizards/bmail_summary/mysql_web_Changelog.sql
@@ -0,0 +1,16 @@
+    ##############################################
+   ### MySource ------------------------------###
+  ##- MySource Reporting ------ MySQL --------##
+ #-- Copyright Squiz.net ---------------------#
+##############################################
+## $Source: /home/cvsroot/xtras/wizards/bmail_summary/mysql_web_Changelog.sql,v $
+## $Revision: 1.1 $
+## $Author: bvial $
+## $Date: 2003/12/05 00:05:03 $
+#######################################################################
+
+# 1.1 to 1.3
+
+ALTER TABLE xtra_wizard_bmail_summary DROP PRIMARY KEY;
+
+ALTER TABLE xtra_wizard_bmail_summary CHANGE bmailid bulkmailid MEDIUMINT UNSIGNED NOT NULL PRIMARY KEY;
