7a86568cc6cbda7ace4997e7adbfe0097060258f
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:24 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_komponenter_48.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_komponenter_48.obj.php
@@ -0,0 +1,34 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 48;
+$_objchilds[] = '_child_objfile_49';
+$_objchilds[] = '_child_objfile_50';
+$_objchilds[] = '_child_objfile_51';
+$_objchilds[] = '_child_objfile_52';
+$_objchilds[] = '_child_objfile_53';
+$_objchilds[] = '_child_objfile_54';
+$_objchilds[] = '_child_objfile_55';
+$_objchilds[] = '_child_objfile_56';
+$_objchilds[] = '_child_objfile_57';
+$_objchilds[] = '_child_objfile_58';
+$_objchilds[] = '_child_objfile_59';
+function objfile_48 () {
+$obj = owNew('structureelement');
+$objdata['name'] = "Komponenter";
+$objdata['pageid'] = "0";
+$objdata['url'] = "";
+$objdata['binfile1'] = "0";
+$objdata['binfile2'] = "0";
+$objdata['binfile3'] = "0";
+$objdata['showtype'] = "0";
+$objdata['target'] = "";
+$objdata['image1'] = "";
+$objdata['image2'] = "";
+$objdata['image3'] = "";
+$objdata['templateid'] = "0";
+$objdata['structureid'] = "0";
+$obj->createObject($objdata);
+$obj->moveTo(48);
+return $obj;
+}
+?>
