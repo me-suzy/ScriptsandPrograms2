838d66f6469be59bf5ed3b3b521d143bf88563fb
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:47 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_privileges.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_privileges.php
@@ -0,0 +1,37 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Beheer rechten gebruikersgroepen';
+
+//  List Headings
+$GLOBALS["tUsergroupname"] = 'Gebruikersgroep naam';
+$GLOBALS["tDefaultGroup"] = 'Standaard groep';
+$GLOBALS["tAdminGroup"] = 'Beheerder groep';
+
+//  List Functions
+$GLOBALS["tViewPrivilege"] = 'Bekijk gebruikersgroep';
+$GLOBALS["tAddNewPrivilege"] = 'Nieuwe gebruikersgroep toevoegen';
+$GLOBALS["tEditPrivilege"] = 'Bewerk gebruikersgroep';
+$GLOBALS["tDeletePrivilege"] = 'Verwijder gebruikersgroep';
+$GLOBALS["tMakeDefault"] = 'Maak standaard gebruikersgroep voor nieuwe gebruikers';
+$GLOBALS["tDefaultSet"] = 'Standaard rechten groep voor nieuwe gebruikers';
+$GLOBALS["tMakeAdmin"] = 'Maak beheerder groep';
+$GLOBALS["tAdminSet"] = 'Beheerder rechten groep';
+
+//  Form Field Headings
+$GLOBALS["tPrivilegesFor"] = 'Rechten voor gebruikersgroep code';
+$GLOBALS["tView"] = 'Bekijk';
+$GLOBALS["tEdit"] = 'Bewerk';
+$GLOBALS["tAdd"] = 'Voeg toe';
+$GLOBALS["tDelete"] = 'Verwijder';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Op dit formulier kun je gebruikersgroepen toevoegen of bewerken';
+
+//  Error Messages
+$GLOBALS["eNoUsergroupCode"] = 'Voer een code in voor de gebruikersgroep';
+$GLOBALS["eNoUsergroupName"] = 'Voer een naam in voor de gebruikersgroep';
+$GLOBALS["eCodeInUse"] = 'Deze rechtengroep bestaat al';
+$GLOBALS["eUsersExist"] = 'Kan de groep niet verwijderen omdat de volgende gebruikers nog zijn toegewezen aan deze groep:';
+
+?>
