e1a1cc0a3d25203a2fb809b430386d7f9183711b
me-suzy <me.suzana@gmail.com>
2025-08-13 02:20:14 +0300

    Add Unsorted/datingscript/languages/dk/dk_.php

---
+++ Unsorted/datingscript/languages/dk/dk_.php
@@ -0,0 +1,485 @@
+<?php 
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.0.3                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 11/01/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               dk_.php                          #
+# File purpose            Addition to danish langfile      #
+# File created by         Søren Egelund <sde@tdcadsl.dk>   #
+############################################################
+
+$wg=array(
+'Ukendt', 
+'Mand',
+'Kvinde'
+);
+
+$wp=array(
+'for Ukendte',
+'for Romantik',
+'for Kæreste',
+'for venskab',
+'for sex'
+);
+
+// Privileged users names
+$wpu=array(
+'Lite member', // Free user
+'Silver member',
+'Gold member',
+'Platinum member');
+// Privileged users timelife (in month)
+$wpt=array(
+'0', // Free user
+'3',
+'6',
+'12');
+// Payment for each member type in dollars
+$wpm=array(
+'0', // Free User
+'19',
+'29',
+'39');
+
+
+$wm=array(
+'Ikke specificeret', 
+'Single',
+'Skilt',
+'Separeret',
+'Enke',
+'Gift'
+);
+
+$wmm=array(
+'Ikke specificeret',
+'Januar',
+'Februar',
+'Marts',
+'April',
+'Maj',
+'Juni',
+'Juli',
+'August',
+'September',
+'Oktober', // [10]
+'November',
+'December'
+);
+
+$wc=array(
+'Ikke specificeret',
+'ingen',
+'1',
+'2',
+'3',
+'4',
+'5 eller flere'
+);
+
+$wh=array(
+'Ikke specificeret',
+'4\'2\' (127cm)',
+'4\'4\' (132cm)',
+'4\'6\' (137cm)',
+'4\'8\' (142cm)',
+'4\'1\' (152cm)',
+'5\'0\' (152cm)',
+'5\'2\' (157cm)',
+'5\'3\' (160cm)',
+'5\'4\' (163cm)',
+'5\'5\' (166cm)', // [10]
+'5\'6\' (168cm)',
+'5\'7\' (171cm)',
+'5\'8\' (173cm)',
+'5\'9\' (175cm)',
+'5\'1\' (178cm)',
+'6\'0\' (183cm)',
+'6\'2\' (188cm)',
+'6\'4\' (193cm)',
+'6\'6\' (198cm)',
+'6\'8\' (203cm)', // [20]
+'6\'1\' (208cm)',
+'7\'0\' (213cm)'
+);
+
+$ww=array(
+'Ikke specificeret',
+'80 lbs (36kg)',
+'85 lbs (39kg)',
+'90 lbs (41kg)',
+'95 lbs (43kg)',
+'100 lbs (45kg)',
+'105 lbs (48kg)',
+'110 lbs (50kg)',
+'115 lbs (52kg)',
+'120 lbs (54kg)',
+'125 lbs (57kg)', // [10]
+'130 lbs (59kg)',
+'135 lbs (61kg)',
+'140 lbs (63kg)',
+'145 lbs (66kg)',
+'150 lbs (68kg)',
+'155 lbs (70kg)',
+'160 lbs (73kg)',
+'165 lbs (75kg)',
+'170 lbs (77kg)',
+'175 lbs (79kg)', // [20]
+'180 lbs (82kg)',
+'185 lbs (84kg)',
+'190 lbs (86kg)',
+'195 lbs (89kg)',
+'200 lbs (91kg)',
+'205 lbs (93kg)',
+'210 lbs (95kg)',
+'215 lbs (97kg)',
+'220 lbs (100kg)',
+'225 lbs (102kg)', // [30]
+'230 lbs (104kg)',
+'235 lbs (107kg)',
+'240 lbs (109kg)',
+'245 lbs (111kg)',
+'250 lbs (113kg)',
+'255 lbs (116kg)',
+'260 lbs (118kg)',
+'265 lbs (120kg)',
+'270 lbs (122kg)',
+'275 lbs (125kg)', // [40]
+'280 lbs (127kg)',
+'285 lbs (129kg)',
+'290 lbs (131kg)',
+'295 lbs (134kg)',
+'300 lbs (136kg)'
+);
+
+$whc=array(
+'Ikke specificeret',
+'Sort',
+'Blond',
+'Brunt',
+'Brunette',
+'Kastanie',
+'Auburn',
+'Mørk-blond',
+'Gyldent',
+'Rødy',
+'Gråt', // [10]
+'Sølv',
+'Hvidt',
+'Skaldet',
+'Andre'
+);
+
+$we=array(
+'Ikke specificeret',
+'Sorte',
+'Bune',
+'Blå',
+'Gønne',
+'Grå',
+'Andre'
+);
+
+$wet=array(
+'Ikke specificeret',
+'Hvid',
+'Spansk',
+'Asiasisk',
+'Afrikansk Amerikansk/Sort',
+'Blandet',
+'Andre'
+);
+
+$wr=array(
+'Ikke specificeret',
+'Agnostik',
+'Atheist',
+'Baptist',
+'Tror på gud',
+'Buddhist',
+'Katolik',
+'Kristen',
+'Øst Orthodox',
+'Humanist',
+'Judaisme', // [10]
+'Lutheran',
+'Mormon (LDS)',
+'Muslim',
+'New Age',
+'Ikke troende',
+'Protestant',
+'Andre'
+);
+
+$ws=array(
+'Ikke specificeret',
+'Ikke ryger',
+'Lejlighedsvis',
+'Jævnligt'
+);
+
+$wd=array(
+'Ikke specificeret',
+'Drikker ikke',
+'Lejlighedsvis',
+'Jævnligt'
+);
+
+$wed=array(
+'Ikke specificeret',
+'Folkeskole',
+'Gymnasiet',
+'Universitets student',
+'Handelsskole uddannet',
+'Undet uddannelse'
+);
+
+$wu=array(
+'Ikke specificeret',
+'Banner reklamer',
+'Avis',
+'Fjernsynet',
+'Søgesystem',
+'Vens anbefaling',
+'Andre'
+);
+
+$whr=array(
+'Ikke specificeret',
+'Stenbukken',
+'Vandmanden',
+'Fiskene',
+'Vædderen',
+'Tyren',
+'Tvilligerne',
+'Krebsen',
+'Løven',
+'Jomfuen',
+'Vægten', // [10]
+'Skorpionen',
+'Skytten'
+);
+
+$wsb=array(
+'Dato lavest først',
+'Dato højest først',
+'Fornavn lavest først',
+'Fornavn højest',
+'Alder lavest først',
+'Alder højest først',
+'Højde lavest først',
+'Højde højest først',
+'Vægt lavest først',
+'Vægt højest først'
+);
+/* 
+$wsb only for search - you can`t add new or remove - 
+only translate!
+*/
+
+$wrg=array(
+'Alle perioder',
+'Sidste 7 dage',
+'Sidste måned',
+'Sidste 3 måneder',
+'Sidste år'
+);
+
+$wrgv=array(
+'', // Not require enter this value for All period
+'7', 
+'31',
+'92',
+'365'
+);
+
+
+$wst=array(
+'Alle brugere',
+'Venter på godkendelse af email',
+'Venter på registrerings godkendelse',
+'Venter på opdtaterings godkendelse',
+'Venter på administrator godkendelse til at fjerne',
+'Aktive brugere'
+);
+
+$wcr=array(
+' Not specified', // First space is required for sorting
+'Afganistan',
+'Albanien',
+'Algeriet',
+'Andorra',
+'Angola',
+'Antigua and arbuda',
+'Argentina', 
+'Armenien',
+'Australien',
+'Østrig',
+'Azerbaijan',
+'Bahamas',
+'Bahrain',
+'Bangladesh',
+'Barbados',
+'Belarus', 
+'Belgien',
+'Bolivia',
+'Bosnien og Herzegovina',
+'Botswana',
+'Brasilien',
+'Brunei Darussalam',
+'Bulgarien',
+'Burundi',
+'Cambodia',
+'Cameroon',
+'Canada',
+'Central African Republic',
+'Chad',
+'Chile',
+'Kina',
+'Colombia',
+'Congo',
+'Costa Rica',
+'Elfenbenskysten',
+'Kroatien',
+'Cuba',
+'Cypern',
+'Tjekkiet',
+'Danmark',
+'Dominikanske Republik',
+'Ekvador',
+'Egypten',
+'El Salvador',
+'Ny Guinea',
+'Estland',
+'Etiopien',
+'Fiji',
+'Finland',
+'Frankrig',
+'Gabon',
+'Gambia',
+'Georgien',
+'Tyskland', 
+'Ghana',
+'Gibraltar',
+'Grækenland',
+'Grønland',
+'Granada',
+'Guadeloupe',
+'Guatemala',
+'Guinea-Bissau',
+'Guinea', 
+'Guyana',
+'Haiti',
+'Honduras',
+'Hong Kong',
+'Ungarn',
+'Island',
+'Indien',
+'Indonesien',
+'Iran', 
+'Irak',
+'Irland',
+'Israel',
+'Italien',
+'Jamaica',
+'Japan',
+'Jordan',
+'Kazakhstan',
+'Kenya',
+'Nord Korea',
+'Syd Korea',
+'Kuvait',
+'Kyrgyzstan',
+'Laos',
+'Letland',
+'Libanon',
+'Lesotho',
+'Liberia',
+'Libya',
+'Liechtenstein',
+'Litauen',
+'Luxembourg',
+'Markedonien',
+'Madagascar',
+'Malaysia',
+'Mali', 
+'Malta',
+'Mauritania',
+'Mexico',
+'Moldova',
+'Monaco', 
+'Mongolia',
+'Morocco',
+'Mozambique',
+'Namibia',
+'Nepal',
+'Holland', 
+'New Zealand',
+'Nicaragua',
+'Nigeria',
+'Niger',
+'Norge',
+'Oman', 
+'Pakistan',
+'Panama',
+'Papua New Guinea',
+'Paraguay',
+'Peru',
+'Philippines',
+'Polen',
+'Portugal', 
+'Puerto Rico',
+'Qatar',
+'Romænien',
+'Rusland',
+'Rwanda',
+'Samoa',
+'San Marino',
+'Saudi Arabia',
+'Senegal',
+'Sierra Leone',
+'Singapore',
+'Slovakiet',
+'Slovenien', 
+'Solomon Øerne',
+'Somalia',
+'Syd Africa',
+'Spanien',
+'Sri Lanka',
+'Sudan',
+'Swaziland',
+'Sverige',
+'Sweitzh',
+'Syrien',
+'Taiwan',
+'Tajikistan',
+'Tanzania',
+'Thailand',
+'Tunesien',
+'Tyrkiet',
+'Turkmenistan',
+'Uganda',
+'Ukraine', 
+'Forenede arabiske emirater',
+'England',
+'USA',
+'Uruguay',
+'Uzbekistan',
+'Vatikan Staten',
+'Venezuela',
+'Viet Nam',
+'Yemen',
+'Juguslavien',
+'Zaire',
+'Zambia',
+'Zimbabwe' 
+);
+// No more 255 countries in this list!!!
+// You can change this list to cities of your country
+// Be carefull!
+?>
+
