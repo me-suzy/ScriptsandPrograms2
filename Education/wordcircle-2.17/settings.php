62d7afd3825c3235ea08c3da68624a87bf36865e
me-suzy <me.suzana@gmail.com>
2025-08-12 13:47:27 +0300

    Add Education/wordcircle-2.17/settings.php

---
+++ Education/wordcircle-2.17/settings.php
@@ -0,0 +1,16 @@
+<?php 
+
+$GLOBALS['configured'] = false;//WARNING IF THIS IS FALSE SOMEONE CAN RUN THE INSTALL
+ 
+$GLOBALS['dbServer'] = '';
+
+$GLOBALS['dbName'] = '';
+
+$GLOBALS['dbUser'] = '';
+
+$GLOBALS['dbPass'] = '';
+
+$GLOBALS['admin_email'] = '';
+
+
+?>
