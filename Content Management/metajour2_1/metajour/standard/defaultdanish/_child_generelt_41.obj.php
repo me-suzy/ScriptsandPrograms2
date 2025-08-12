a8ec11b9a4e5312a46ea0a1932945348cd8f6296
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:11 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_generelt_41.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_generelt_41.obj.php
@@ -0,0 +1,25 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 41;
+$_objchilds[] = '_child_objfile_42';
+$_objchilds[] = '_child_objfile_43';
+function objfile_41 () {
+$obj = owNew('structureelement');
+$objdata['name'] = "Generelt";
+$objdata['pageid'] = "0";
+$objdata['url'] = "";
+$objdata['binfile1'] = "0";
+$objdata['binfile2'] = "0";
+$objdata['binfile3'] = "0";
+$objdata['showtype'] = "0";
+$objdata['target'] = "";
+$objdata['image1'] = "";
+$objdata['image2'] = "";
+$objdata['image3'] = "";
+$objdata['templateid'] = "0";
+$objdata['structureid'] = "0";
+$obj->createObject($objdata);
+$obj->moveTo(20);
+return $obj;
+}
+?>
