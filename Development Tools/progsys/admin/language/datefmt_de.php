1fb541d294feeb9ac18920140774827b16ae1e0b
me-suzy <me.suzana@gmail.com>
2025-08-12 10:11:09 +0300

    Add Development Tools/progsys/admin/language/datefmt_de.php

---
+++ Development Tools/progsys/admin/language/datefmt_de.php
@@ -0,0 +1,15 @@
+<?php
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+/***************************************************************************
+ * Created by: Boesch IT-Consulting (info@boesch-it.de)
+ * (c)2002-2005 Boesch IT-Consulting
+ * *************************************************************************/
+$l_usrdateformat="d.m.Y";
+?>
