1abefbddcbe07c6e6f4f8703f79ac5974d692ad8
me-suzy <me.suzana@gmail.com>
2025-08-12 08:22:33 +0300

    Add Content Management/wizard/wizard/inc/languages/english.public.php

---
+++ Content Management/wizard/wizard/inc/languages/english.public.php
@@ -0,0 +1,126 @@
+<?php
+/*  
+ *  Language Translation 
+ *  Wizard Site Framework
+*/ 
+
+
+// Search and Login Bar at top
+	define("SEARCH", "Search" );  //note that the search button is a graphic: /admin/images/search.gif
+	define("CONTACT", "Contact Us" );
+	define("LOGIN", "Login" );
+	define("LOGOUT", "Logout" );
+	define("REGISTER", "Register" );
+	define("ADMIN", "Admin" );
+	define("SITEMAP", "Site Map" );
+	
+// Footer 
+   define("WIZARD", "Wizard Site Toolbox" );
+   define("LOGOUT", "Logout" );
+   define("EDIT", "Edit");
+   
+// Registration Form / Contact Form
+   define("REQUIRED", "Required Fields" );
+   define("USERPASS", "Username and Password" );
+   define("CUSERNAME", "Username" );
+   define("CPASSWORD", "Password" );
+   define("PASSAGAIN", "Password Again" );
+   define("CONTACTINFO", "Contact Information" );
+   define("FIRSTNAME", "First Name" );
+   define("LASTNAME", "Last Name" );
+   define("EMAIL", "Email");
+   define("PHONE", "Phone" );
+   define("ORGANIZATION", "Organization" );
+   define("ADDRESS", "Address" );
+   define("CITY", "City" );
+   define("STATE", "State" );
+   define("COUNTRY", "Country" );
+   define("POSTAL", "Postal Code" );
+   define("NEWSLETTER", "Newsletter");
+   define("RECEIVE", "Receive occasional marketing emails only from us?*" );
+   define("PLAINTEXT", "Plain Text" );
+   define("NOTHANKS", "No Thanks" );
+   define("PRIVACY", "Privacy Statement: We will never share your information<br /> without your permission." );
+   define("CHANGEPASS", "Change your password" );
+   define("CHANGEEMAIL", "Change your email address" );
+   define("LOST", "Lost password?" );
+   define("ERRORFIRSTNAME", "Error: Please enter your first name." );
+   define("ERRORLAST", "Error: Please enter your last name.");
+   define("ERROREMAIL", "Error: Please enter a valid email address.");
+   define("ERRORUSER", "Error: Please enter a username.");
+   define("ERRORPASS", "Error: Please enter a password.");
+   define("USERALREADY", "Error: The username is already in use. Please choose a different one.");
+   define("EMAILBAD", "Error: your email address is not formatted correctly.");
+   define("FAILED", "Registration failed. Please try again or contact us.");
+   define("MATCH", "Error: Passwords do not match.");
+   define("SUCCESS", "Success. To validate your account, click on the link in the email we just sent you.");
+   define("SUBMIT", "Submit");
+   define("RESET","Reset");
+   define("TOKENS", "Error: Username missing when creating cookies.");
+   define("NOHASH", "Error: Your confirmation code (hash) was not found."); //problem with the secret hash
+   define("CONFIRMED", "Your account is confirmed. Please click on the login link.");
+   define("CONFIRMFAILED", "Sorry, your email or confirmation code do not match our records.");
+   define("MISCONFIRM", "Error: Your confirmation code or email address is missing.");
+   define("GUEST", "Guest");
+   define("SPACES", "Error: There can be no spaces in the username.");
+   define("ALPHA", "There must be at least one alphabetic character in the username.");
+   define("ILLEGALCHAR", "Error: Illegal character in username.");
+   define("USERSHORT", "Error: Username must be at least 5 characters.");
+   define("USERLONG", "Error: Username must be less than 16 characters.");
+   define("SYSTEMUSE", "Error: The name you chose is reserved for system use.");
+   define("PASSINVALID", "Error: Passwords must be at least 5 characters and less than 16.");
+   define("THANKSREGISTER", "Thank-you for registering at:");
+   define("FOLLOWTHIS", "Simply follow this link to confirm your registration:");
+   define("HASREGISTERED", "has registered at");
+   define("CLICK", "Click on this link to go to the user list on the Admin Panel: ");
+   define("REGCONFIRM", "Registration Confirmation");
+   define("NEWUSER", "New User Registration");
+
+// Contact Form
+   define("CF_REQUEST", "Enter Your Request Here");
+   define("CF_COORDINATES", "Our Coordinates");
+   define("CF_REQUEST_INFO", "Request for Information");
+   define("CF_RECEIVED", "You have received a request for information from:");
+   define("CF_NAME", "Name");
+   define("CF_THANK_YOU", "Thank-you.  We will contact you shortly.");
+   
+   
+// Login Form
+   define("COOKIES", "Cookies must be turned on.");   
+   define("USERMISSING", "Error: Username or password not entered."); 
+   define("NOTUSERPASS", "Error: Username or password incorrect.");
+   define("NOTACTIVATED", "Account not activated. <br />Click on the link in the email we sent at registration.");
+   
+//Change Password or Email
+   define("FILLALL", "Error: All fields must be filled in.");
+   define("CHANGECONFIRMED", "Your membership record was updated.");
+   define("TRYAGAIN", "Error: Please try again.");
+   define("NOMATCH", "Error: No matching username or email found.");
+   define("PASSRESET", "Your password was reset to:");
+   define("CHANGEIT", "You can change your password at the bottom of the login screen.");
+   define("FROM", "From");
+   define("PASSEMAILED", "Your new password was emailed to you.");
+   define("CHANGECON", "Caution: You will use your new email address to confirm the change.");
+   define("OLDPASS", "Old Password");
+   define("NEWPASS", "New Password");
+   
+//authorization
+   define("NOTLOGGED", "You are not logged in.");   
+   define("ADMINONLY", "Access is restricted to administrators.");
+   define("WEBMASTERONLY", "Access is restricted to the webmaster.");
+   
+//search
+   define("S_NO_MATCH", "No match found.");
+   define("S_EMPTY_FORM", "No search term entered.");
+   define("S_NO_RESULTS", "The search returned no results.");
+   define("S_SEARCH_RESULTS", "Search Results");
+   define("S_SEARCH", "Search");
+   define("S_RESULTS", "results");
+   define("S_PREVIOUS_PAGE", "Previous Page");
+   define("S_PREV", "PREV");  //short form for previous page
+   define("S_NEXT_PAGE", "Next Page");
+   define("S_NEXT", "NEXT");  //short form for next page
+   
+   
+
+?>
