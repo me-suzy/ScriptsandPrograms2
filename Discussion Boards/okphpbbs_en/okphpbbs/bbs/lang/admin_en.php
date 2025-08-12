40b24b028eb83c9c75161a692a51c11543d4ae27
me-suzy <me.suzana@gmail.com>
2025-08-12 10:43:20 +0300

    Add Discussion Boards/okphpbbs_en/okphpbbs/bbs/lang/admin_en.php

---
+++ Discussion Boards/okphpbbs_en/okphpbbs/bbs/lang/admin_en.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ ***********************************************************************
+ * Copyright (c) 2003-2004 Okphp Group
+ * http://www.okphp.com
+ *
+ *  
+ ***********************************************************************
+ */
+
+
+//#######################
+// global
+$indications['yes'] = "Yes"; //*
+$indications['no'] = "No"; //*
+
+$indications['root'] = "Root";
+$indications['add_forum'] = "Add Forum";
+$indications['add_class'] = "Add Class";
+$indications['add_type'] = "Add Type";
+$indications['manage_comment'] = "Manage Comments";
+$indications['add_file'] = "Add File";
+$indications['back_'] = "Go Back";
+
+// page
+$indications['page_down'] = "Next Page&gt;";
+$indications['page_up'] = "&lt;Last Page";
+$indications['page_end'] = "End Page&gt;|";
+$indications['page_begin'] = "|&lt;First Page";
+$indications['all_page'] = "All Pages:";
+$indications['current_page'] = ",Current Page:";
+$indications['page_jump'] = "Jump to";
+
+// 3.0
+$indications['on_use'] = "On use";
+// #########################
+$messages['process_failed'] = "Process Failed!";
+$messages['process_ok'] = "Operation Success!";
+$messages['file_not_exists'] = "Sorry! The file is nonexistent";
+$messages['info_not_enough'] = "Please fill in all the required blanks ";
+$messages['must_files_folder'] = "You must select a  class";
+$messages['not_to_self'] = "The folder you have just choosed is current folder!";
+$messages['no_file_id'] = "Please select the file that you want to edit";
+$messages['no_such_user'] = "No such an user ";
+$messages['wrong_email'] = "The E-mail format is wrong!";
+$messages['login_failed'] = "Login failed, You can try a limited times:";
+$messages['access_dennied'] = "Access dennied!";
+$messages['not_match_ip'] = "The IP format is wrong! Correct format:123.123.123.123,123.123.123.,123.123.,123.,";
+$messages['no_count'] = "<font size=5px>Sorry! There is no record in selected date</font>"; // count
+
+// ###########################
+// 
+$messages['fill_all'] = "Please fill all the blanks required";
+$messages['delete_class'] = "Delete Class";
+$messages['deteted_ok'] = "Target has been Deleted Successfully!";
+$messages['find'] = "Find";
+$messages['records'] = "records";
+$messages['no_find'] = "Sorry! No record was finded.";
+$messages['moved_ok'] = "Target has been Moved Successfully!";
+$messages['modified_ok'] = "Target has been Modified Successfully!";
+$messages['added_ok'] = "Target has been Created Successfully!";
+
+// cache
+$messages['cache_up_ok'] = "Cache has been Updated Successfully!";
+
+// =======
+$indications['top'] = "Top";
+$indications['bottom'] = "Bottom";
+$indications['left'] = "Left";
+$indications['right'] = "Right";
+
+// guestbook
+$indications['replied'] = "Yes";
+$indications['not_reply'] = "Not";
+
+// log
+$indications['del_group'] = "Delete Group";
+$indications['add_group'] = "Add Group";
+$indications['modify_group'] = "Modify Group";
+$indications['modify_member'] = "Add Member";
+$indications['add_member'] = "Modify Member";
+$indications['success'] = "Success";
+$indications['faild'] = "Faild";
+
+// login
+$messages['not_req_for_trial'] = "Not required for Trial users and Class-Moderators";
+$messages['license_key_stored'] = "Your LICENSE KEY has been stored in your PC through COOKIES";
+$indications['username'] = "UserName";
+$indications['password'] = "PassWord";
+$indications['license_key'] = "LICENSE KEY";
+
+// confirm code
+$indications['confirm_'] = "Confirm Code";
+$indications['confirm_ex'] = "Enter the code exactly as you see it.";
+?>
