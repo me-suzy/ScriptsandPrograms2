60421f405d370afa6db8b335065d36750ba5fd32
me-suzy <me.suzana@gmail.com>
2025-08-12 16:13:16 +0300

    Add Form Processors/Form-Generator/config.php

---
+++ Form Processors/Form-Generator/config.php
@@ -0,0 +1,12 @@
+<?
+
+//the relative or absolute path were generated forms are to be stored
+$GENERATE_PATH = "generated/";
+
+//username
+$USERNAME = "admin";
+
+//password
+$PASSWORD = "admin";
+
+?>
