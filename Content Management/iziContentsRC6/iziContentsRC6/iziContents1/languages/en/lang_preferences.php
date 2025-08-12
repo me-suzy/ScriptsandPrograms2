9f1f02b32d850d20c4fe30e8860abb0762a177e7
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:37 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_preferences.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_preferences.php
@@ -0,0 +1,80 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'edit your user preferences';
+$GLOBALS["tFormTitle2"] = 'enter your registration details';
+
+//  List Headings
+$GLOBALS["tUsername"] = 'Username';
+$GLOBALS["tEMail"] = 'E-Mail address';
+
+//  Form Field Headings
+$GLOBALS["tLoginName"] = 'Login name';
+$GLOBALS["tRetainCookie"] = 'Retain Cookie';
+$GLOBALS["tPassword"] = 'Password';
+$GLOBALS["tRetypePassword"] = 'Retype password';
+$GLOBALS["tAuthorname"] = 'Your name';
+$GLOBALS["tUsergroup"] = 'User group';
+$GLOBALS["tEMail"] = 'E-mail address';
+$GLOBALS["tCountry"] = 'Country';
+$GLOBALS["tLanguage"] = 'Language';
+$GLOBALS["tPhone"] = 'Phone';
+$GLOBALS["tFax"] = 'Fax';
+$GLOBALS["tAddress"] = 'Address';
+$GLOBALS["tCity"] = 'City';
+$GLOBALS["tState"] = 'State';
+$GLOBALS["tZip"] = 'ZIP';
+$GLOBALS["tWebsite"] = 'Website';
+$GLOBALS["tNewsletter"] = 'Newsletter';
+$GLOBALS["tComments"] = 'Comments';
+$GLOBALS["tRegisterIntroText"] = 'To gain access to the restricted areas of the website you need to register.';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["hLoginName"] = 'This is your login name used on the password screen.';
+$GLOBALS["hRetainCookie"] = 'If you check this box, a cookie will be stored on your computer to store your Login Name so that you only need to type in your password in future. The cookie will be held for 1 year, after which time you will need to renew it by editing your user preferences and checking this box again.';
+$GLOBALS["hPassword"] = 'Enter the password you will use to log on to ezContents.';
+$GLOBALS["hRetypePassword"] = 'Re-enter your password, to ensure that you have typed it correctly.';
+$GLOBALS["hAuthorname"] = 'This field is a full name or nickname. It will be visible to viewers beneath any articles you submit.';
+$GLOBALS["hUsergroup"] = 'The user group that you belong to.';
+$GLOBALS["hEMail"] = 'Your E-mail address. Together with your name this will create a hyper link for mail on any articles you submit.';
+$GLOBALS["hCountry"] = 'The country where you live.';
+$GLOBALS["hLanguage"] = 'Your native language (optional).<br />If this is set, then you will see menu options, fieldnames, help, etc. in that language rather than the default language for the site.';
+$GLOBALS["hPhone"] = 'Your phone number';
+$GLOBALS["hFax"] = 'Your fax number';
+$GLOBALS["hAddress"] = 'Your mailing address';
+$GLOBALS["hCity"] = 'The city where you live';
+$GLOBALS["hState"] = 'The state where you live';
+$GLOBALS["hZip"] = 'Your ZIP code';
+$GLOBALS["hWebsite"] = 'Link to your website';
+$GLOBALS["hNewsletter"] = 'Do you wish to receive newsletter?';
+$GLOBALS["hComments"] = 'If you have any comments that you want to share with other users or administrators write it here.';
+
+//  Error Messages
+$GLOBALS["eLoginExists"] = 'Login name already exists, please choose another.';
+$GLOBALS["eLoginEmpty"] = 'Login name can not be left empty.';
+$GLOBALS["ePasswordError"] = 'Retyped password is not the same as the given password.';
+$GLOBALS["eNameEmpty"] = 'Your name can not be left empty.';
+$GLOBALS["eEMailEmpty"] = 'Your e-mail is empty or incorrect.';
+$GLOBALS["ePhoneEmpty"] = 'Phone field can not be left empty';
+$GLOBALS["eCountryEmpty"] = 'Country was not selected';
+$GLOBALS["eFaxEmpty"] = 'Fax field can not be left empty';
+$GLOBALS["eAddressEmpty"] = 'Address field can not be left empty';
+$GLOBALS["eCityEmpty"] = 'City field can not be left empty';
+$GLOBALS["eStateEmpty"] = 'State field can not be left empty';
+$GLOBALS["eZipEmpty"] = 'ZIP field can not be left empty';
+$GLOBALS["eWebsiteEmpty"] = 'Website field is mandatory';
+$GLOBALS["eCommentsEmpty"] = 'The comments field is mandatory';
+
+
+$GLOBALS["tPrefsUpdated"] = 'Your user preferences have been updated.';
+$GLOBALS["tPrefsRegistered"] = 'You have been registered as a new user.';
+$GLOBALS["tReturnMailSubjest"] = 'Your registration';
+$GLOBALS["tReturnMailBody"] = 'Thank you for registering at our ezContents driven website. You have now access to the parts of the site that require a username and password.';
+$GLOBALS["tServerUserEmail"] = 'E-mail server user';
+$GLOBALS["tRegister"] = 'Register';
+$GLOBALS["tUpdate"] = 'Update';
+
+$GLOBALS["tReturnMailSubject"] = 'Thank you for registering';
+$GLOBALS["tReturnMailBody"] = 'Below you see your registration details';
+
+?>
