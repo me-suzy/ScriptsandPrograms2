47f15b2553b2e290c29d3cd8aa320a2545f6c1c5
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:47 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_setregister.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_setregister.php
@@ -0,0 +1,55 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'registration settings';
+
+//  Form Block Titles
+$GLOBALS["thActiveFields"] = 'Active fields';
+$GLOBALS["thMailSettings"] = 'Mail settings';
+$GLOBALS["thExtraSettings"] = 'Extra settings';
+
+//  Form Field Headings
+$GLOBALS["tPhoneNumber"] = 'Phone number';
+$GLOBALS["tFaxNumber"] = 'Fax number';
+$GLOBALS["tAddress"] = 'Address';
+$GLOBALS["tCity"] = 'City';
+$GLOBALS["tState"] = 'State';
+$GLOBALS["tZip"] = 'Zip';
+$GLOBALS["tCountry"] = 'Country';
+$GLOBALS["tLanguage"] = 'Language';
+$GLOBALS["tWebsite"] = 'Website';
+$GLOBALS["tComments"] = 'Comments';
+$GLOBALS["tNewsletter"] = 'Newsletter';
+$GLOBALS["tSendConfMail"] = 'Confirmation e-mail';
+$GLOBALS["tServerUserEmail"] = 'Server user account';
+$GLOBALS["tErrFormFontColor"] = 'Error message font color';
+$GLOBALS["tShowHelptexts"] = 'Show helptexts';
+$GLOBALS["tHelpTextSize"] = 'Help text font size';
+$GLOBALS["tHelpTextColor"] = 'Help text font color';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'Here you can adjust registration and mail sending settings.';
+
+$GLOBALS["hPhoneNumber"] = 'Shows or disables the phone number on the user registration form. When shown, the field can be mandatory or optional.';
+$GLOBALS["hFaxNumber"] = 'Shows or disables the fax number on the user registration form. When shown, the field can be mandatory or optional.';
+$GLOBALS["hAddress"] = 'Shows or disables the address field on the user registration form. When shown, the field can be mandatory or optional.';
+$GLOBALS["hCity"] = 'Shows or disables the city field on the user registration form. When shown, the field can be mandatory or optional.';
+$GLOBALS["hState"] = 'Shows or disables the state field on the user registration form. When shown, the field can be mandatory or optional.';
+$GLOBALS["hZip"] = 'Shows or disables the Zip field on the user registration form. When shown, the field can be mandatory or optional.';
+$GLOBALS["hCountry"] = 'Shows or disables the country field on the user registration form. When shown, the field can be mandatory or optional.';
+$GLOBALS["hLanguage"] = 'Shows or disables the language field on the user registration form. When shown, the field can be mandatory or optional.';
+$GLOBALS["hWebsite"] = 'Shows or disables the Website field on the user registration form. When shown, the field can be mandatory or optional.';
+$GLOBALS["hComments"] = 'Shows or disables the description field on the user registration form. When shown, the field can be mandatory or optional.';
+$GLOBALS["hNewsletter"] = 'Shows or disables the option to subscribe for a newsletter on the user registration form.';
+$GLOBALS["hSendConfMail"] = 'E-mail confirmation with registration details will be send to the registered user.';
+$GLOBALS["hServerUserEmail"] = 'Sometimes it is needed to send e-mail with the server user account. If you leave this field empty the e-mail address of the admin user will be used in the FROM field. You can overrule this here.';
+$GLOBALS["hErrFormFontColor"] = 'Font color on registration form for validation error messages.';
+$GLOBALS["hShowHelptexts"] = 'Show help texts next to the registration fields.';
+$GLOBALS["hHelpTextSize"] = 'This sets the font size of the help texts that appear next to the registration fields.';
+$GLOBALS["hHelpTextColor"] = 'This sets the font color of the help texts that appear next to the registration fields.';
+
+// Error messages
+$GLOBALS["eErrorColorWrong"] = 'Error message font color has wrong value.';
+$GLOBALS["eHelptextColorWrong"] = 'Help text font color has wrong value.';
+
+?>
