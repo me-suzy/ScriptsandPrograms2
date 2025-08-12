38343489aba085b54a43d869ae3927502aa63fd7
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:11 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_colorsform.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_colorsform.php
@@ -0,0 +1,147 @@
+<?php
+
+
+//  Formular Titel
+$GLOBALS["tFormTitle"] = 'Farben und Schriften der Seite';
+
+//  Form Block Titles
+$GLOBALS["thBackground"] = 'Hintergrund-Farben';
+$GLOBALS["thFont"] = 'Seiten-Schriftarten';
+$GLOBALS["thLinks"] = 'Link Farben';
+$GLOBALS["thContent"] = 'Farben des Inhalts';
+$GLOBALS["thRColContent"] = 'Inhalts-Farben Rechte Spalte';
+$GLOBALS["thTeaser"] = 'Teaser Farben';
+
+//  Form Field Headings
+$GLOBALS["tMainBackground"] = 'Haupt-Hintergrund';
+$GLOBALS["tBorderBackground"] = 'Rahmen-Hintergrund';
+$GLOBALS["tHeaderBackground"] = 'Header-Hintergrund';
+$GLOBALS["tFooterBackground"] = 'Footer Hintergrund';
+$GLOBALS["tMenuBackground"] = 'Men&uuml; Hintergrund';
+$GLOBALS["tTopmenuBackground"] = 'Top Men&uuml; Hintergrund';
+$GLOBALS["tFont"] = 'Website Schriftart';
+$GLOBALS["tHeaderFontSize"] = '&Uuml;berschriften-Schriftgr&ouml;sse';
+$GLOBALS["tRColHeaderFontSize"] = '&Uuml;berschriften-Schriftgr&ouml;sse Rechte Spalte';
+$GLOBALS["tTMFontSize"] = 'Top Men&uuml; Link Schriftgr&ouml;sse';
+$GLOBALS["tTMFontStyle"] = 'Top Men&uuml; Link Schriftstil';
+$GLOBALS["tMFontSize"] = 'Men&uuml; Link Schriftgr&ouml;sse';
+$GLOBALS["tMFontStyle"] = 'Men&uuml; Link Schriftstil';
+$GLOBALS["tLinkColour"] = 'Link Farbe';
+$GLOBALS["tLinkColourHover"] = 'Link Farbe hover';
+$GLOBALS["tLinkColourHistory"] = 'Link Farbe besucht';
+$GLOBALS["tSLinkColour"] = 'Kleine Link Farbe';
+$GLOBALS["tSLinkColourHover"] = 'Kleine Link Farbe hover';
+$GLOBALS["tSLinkColourHistory"] = 'Kleine Link Farbe besucht';
+$GLOBALS["tMenuLinkColour"] = 'Men&uuml; Link Farbe';
+$GLOBALS["tMenuLinkColourHover"] = 'Men&uuml; Link Farbe hover';
+$GLOBALS["tMenuLinkColourHistory"] = 'Men&uuml; Link Farbe besucht';
+$GLOBALS["tTopMenuLinkColour"] = 'Top Men&uuml; Link Farbe';
+$GLOBALS["tTopMenuLinkColourHover"] = 'Top Men&uuml; Link Farbe hover';
+$GLOBALS["tTopMenuLinkColourHistory"] = 'Top Men&uuml; Link Farbe history';
+$GLOBALS["tRColLinkColour"] = 'Rechte Spalte Link Farbe';
+$GLOBALS["tRColLinkColourHover"] = 'Rechte Spalte Link Farbe hover';
+$GLOBALS["tRColLinkColourHistory"] = 'Rechte Spalte Link Farbe history';
+$GLOBALS["tSLinkFontSize"] = 'Kleine Link Schriftgr&ouml;sse';
+$GLOBALS["tRColFontSize"] = 'Rechte Spalte Schrift/Links Gr&ouml;sse';
+$GLOBALS["tRColFontStyle"] = 'Rechte Spalte Links Schriftstil';
+$GLOBALS["tCBorder"] = 'Inhalt Rand Farbe';
+$GLOBALS["tCHeader"] = 'Inhalts-&Uuml;berschriften-Schrift Farbe';
+$GLOBALS["tCBackground"] = 'Inhalt Hintergrund';
+$GLOBALS["tCFont"] = 'Inhalt Schrift Farbe';
+$GLOBALS["tTBorder"] = 'Teaser Rand Farbe';
+$GLOBALS["tTHeader"] = 'Teaser-&Uuml;berschrift Schrift Farbe';
+$GLOBALS["tTBackground"] = 'Teaser Hintergrund';
+$GLOBALS["tTFont"] = 'Teaser Schrift Farbe';
+$GLOBALS["tCFontSize"] = 'Inhalts/Links Schriftgr&ouml;sse';
+$GLOBALS["tCFontStyle"] = 'Inhalts/Links Schrift Schriftstil';
+$GLOBALS["tRColCBorder"] = 'Rechte Spalte Rand Farbe';
+$GLOBALS["tRColCHeader"] = 'Rechte Spalte &Uuml;berschrift-Schrift Farbe';
+$GLOBALS["tRColCBackground"] = 'Rechte Spalte Hintergrund';
+$GLOBALS["tRColCFont"] = 'Rechte Spalte Schrift Farbe';
+
+//  Form Text Beschreibung
+$GLOBALS["tDetails"] = 'In diesem Formular k&ouml;nnen Sie die Farben und Schriftarten f&uuml;r Ihre Webseite &auml;ndern.';
+
+//  Form Detail Kommentare und Hilfetexte
+$GLOBALS["hMainBackground"] = 'Dies ist die Haupt-Hintergrund Farbe f&uuml;r die Seite.';
+$GLOBALS["hBorderBackground"] = 'Dies ist die Rahmen-Hintergrund Farbe f&uuml;r die Seite.';
+$GLOBALS["hHeaderBackground"] = 'Diese Hintergrund-Farbe wird f&uuml;r die Seiten-&Uuml;berschrift verwendet.';
+$GLOBALS["hFooterBackground"] = 'Diese Hintergrund-Farbe wird f&uuml;r den Seiten-Fuß verwendet.';
+$GLOBALS["hMenuBackground"] = 'Diese Hintergrund-Farbe wird f&uuml;r das Seiten-Men&uuml; verwendet.';
+$GLOBALS["hTopmenuBackground"] = 'Diese Hintergrund-Farbe wird f&uuml;r das Top-Men&uuml; verwendet (wenn dies in den \'site settings\' verf&uuml;gbar ist).';
+$GLOBALS["hFont"] = 'Diese Schrift wird auf der Webseite verwendet';
+$GLOBALS["hHeaderFontSize"] = 'Diese Schriftgr&ouml;sse wird f&uuml;r den Inhalt und die Teaser-&Uuml;berschriften verwendet.';
+$GLOBALS["hRColHeaderFontSize"] = 'Diese Schriftgr&ouml;sse wird f&uuml;r den Inhalts-&Uuml;berschrift in der Rechten Spalte verwendet.';
+$GLOBALS["hTMFontSize"] = 'Diese Schriftgr&ouml;sse ist f&uuml;r die Top-&Uuml;berschrift-Men&uuml;punkte (wenn dieses Feature eingeschaltet ist in den  \'site settings\', und wenn Sie Testlinks statt Bilderlinks als Men&uuml;punkte verwenden).';
+$GLOBALS["hTMFontStyle"] = 'Benutzen Sie diese Einstellung, um einen Schriftstil f&uuml;r die Top-&Uuml;berschrift-Men&uuml;punkte auszuw&auml;hlen, wenn Sie Test statt Bilder verwenden.';
+$GLOBALS["hMFontSize"] = 'Dies ist die Schriftgr&ouml;sse f&uuml;r die Seiten-Men&uuml;punkte.';
+$GLOBALS["hMFontStyle"] = 'Hier wird der Schriftstil f&uuml;r Seiten-Men&uuml;punkte ausgew&auml;hlt, wenn Sie Texte f&uuml;r die einzelnen Men&uuml;punkte verwenden und keine Bilder.';
+$GLOBALS["hLinkColour"] = 'Dies ist die Farbe f&uuml;r die Links auf Ihrer Seite.';
+$GLOBALS["hLinkColourHover"] = 'Verwenden Sie diese Farb-Einstellung, um den Link hervorzuheben, wenn die Maus dar&uuml;ber geht.';
+$GLOBALS["hLinkColourHistory"] = 'Diese Einstellung erlaubt Ihnen, eine Farbe f&uuml;r Links auszuw&auml;hlen, die Besucher der Seite bereits angeklickt haben.';
+$GLOBALS["hSLinkColour"] = 'Diese Farbe wird f&uuml;r \'kleine\' Links auf Ihrer Seite verwendet.';
+$GLOBALS["hSLinkColourHover"] = 'Benutzen Sie diese Farb-Einstellung, um \'kleine\' Links hervorzuheben, wenn der Cursor &uuml;ber ihnen schwebt.';
+$GLOBALS["hSLinkColourHistory"] = 'Diese Einstellung erlaubt Ihnen, eine Farbe f&uuml;r \'kleine\' Links auszuw&auml;hlen, die Besucher Ihrer Seite bereits angeklickt haben.';
+$GLOBALS["hMenuLinkColour"] = 'Dies ist die Farbe f&uuml;r Men&uuml;-Links auf Ihrer Seite.';
+$GLOBALS["hMenuLinkColourHover"] = 'Verwenden Sie diese Farbeinstellung, um Men&uuml;-Links hervorzuheben, wenn mit dem Cursor darauf gezeigt wird.';
+$GLOBALS["hMenuLinkColourHistory"] = 'Diese Einstellung erlaubt Ihnen, eine Farbe f&uuml;r die Men&uuml;-Links auszuw&auml;hlen, denen Ihre Besucher bereits gefolgt sind.';
+$GLOBALS["hTopMenuLinkColour"] = 'Dies ist die Farbe f&uuml;r die Top Men&uuml; Links auf Ihrer Seite.';
+$GLOBALS["hTopMenuLinkColourHover"] = 'Verwenden Sie diese Farbeinstellung um Men&uuml;-Links hervorzuheben, wenn mit dem Cursor darauf gezeigt wird.';
+$GLOBALS["hTopMenuLinkColourHistory"] = 'Diese Einstellung erlaubt Ihnen, eine Farbe f&uuml;r die Men&uuml;-Links auszuw&auml;hlen, die Ihre Besucher bereits besucht haben.';
+$GLOBALS["hRColLinkColour"] = 'Dies ist die Farbe, die f&uuml;r Links in der rechten Spalte auf Ihrer Seite verwendet wird.';
+$GLOBALS["hRColLinkColourHover"] = 'Verwenden Sie diese Farbeinstellung, um Men&uuml;-Links hervorzuheben, die in der rechten Spalte verwendet werden, wenn mit dem Cursor darauf gezeigt wird.';
+$GLOBALS["hRColLinkColourHistory"] = 'Diese Einstellung erlaubt Ihnen, eine Farbe f&uuml;r die Men&uuml;-Links in der rechten Spalte auszuw&auml;hlen, die Ihre Besucher bereits besucht haben.';
+$GLOBALS["hSLinkFontSize"] = 'Verwenden Sie diese Einstellung, um die Schriftgr&ouml;sse f&uuml;r \'kleine\' Links zu &auml;ndern.';
+$GLOBALS["hRColFontSize"] = 'Verwenden Sie diese Einstellung, um die Schriftgr&ouml;sse f&uuml;r die Inhalte in der rechte Spalte zu &auml;ndern.';
+$GLOBALS["hRColFontStyle"] = 'Verwenden Sie diese Einstellung, um den Linkstil in der rechten Spalte zu &auml;ndern.';
+$GLOBALS["hCBorder"] = 'Dies ist die Farbe, die f&uuml;r den &Uuml;berschriften-Block und den Rand in den Hauptinhalten verwendet wird.';
+$GLOBALS["hCHeader"] = 'Diese ist die Farbe, die f&uuml;r den &Uuml;berschrift-Text in den Hauptinhalten verwendet wird.';
+$GLOBALS["hCBackground"] = 'Diese ist die Hintergrund-Farbe f&uuml;r den Hauptinhalt.';
+$GLOBALS["hCFont"] = 'Diese ist die Text-Farbe f&uuml;r den Hauptinhalt.';
+$GLOBALS["hTBorder"] = 'Diese ist die Farbe, die f&uuml;r den &Uuml;berschriften-Block und den Rand f&uuml;r den Teaser-Inhalt verwendet wird.';
+$GLOBALS["hTHeader"] = 'Diese ist die Farbe, die f&uuml;r den &Uuml;berschriften-Text im Teaser-Inhalt verwendet wird.';
+$GLOBALS["hTBackground"] = 'Diese ist die Hintergrund Farbe f&uuml;r den Teaser-Inhalt.';
+$GLOBALS["hTFont"] = 'Diese ist die Text-Farbe f&uuml;r den Teaser-Inhalt.';
+$GLOBALS["hCFontSize"] = 'Setzt die Schriftgr&ouml;sse f&uuml;r den Text auf Ihrer Seite.';
+$GLOBALS["hCFontStyle"] = 'Setzt den Schriftstil f&uuml;r den Text auf Ihrer Seite.';
+$GLOBALS["hRColCBorder"] = 'Dies ist die Farbe f&uuml;r den &Uuml;berschriften-Block und den Rand, die f&uuml;r den Hauptinhalt in der rechten Spalte verwendet wird.';
+$GLOBALS["hRColCHeader"] = 'Dies ist die Farbe f&uuml;r den &Uuml;berschriften-Text f&uuml;r den Hauptinhalt in der rechten Spalte.';
+$GLOBALS["hRColCBackground"] = 'Dies ist die Hintergrund-Farbe f&uuml;r den Hauptinhalt in der rechten Spalte.';
+$GLOBALS["hRColCFont"] = 'Dies ist die Text-Farbe, die im Hauptinhalt der rechten Spalte verwendet wird.';
+
+//  Fehlernachrichten
+$GLOBALS["eMainBGWrong"] = 'Der Wert f&uuml;r den Haupt-Hintergrund ist falsch';
+$GLOBALS["eBorderBGWrong"] = 'Der Wert f&uuml;r den Rahmen-Hintergrund ist falsch';
+$GLOBALS["eHeaderBGWrong"] = 'Der Wert f&uuml;r den &Uuml;berschriften-Hintergrund ist falsch';
+$GLOBALS["eFooterBGWrong"] = 'Der Wert f&uuml;r den Footer Hintergrund ist falsch';
+$GLOBALS["eContentBGWrong"] = 'Der Wert f&uuml;r den Hintergrund des Inhalts ist falsch';
+$GLOBALS["eMenuBGWrong"] = 'Der Wert f&uuml;r den Men&uuml; Hintergrund ist falsch';
+$GLOBALS["eTopmenuBGWrong"] = 'Der Wert f&uuml;r den Top Men&uuml; Hintergrund ist falsch';
+$GLOBALS["eLinkWrong"] = 'Der Wert f&uuml;r die Link Farbe ist falsch';
+$GLOBALS["eLinkHoverWrong"] = 'Der Wert f&uuml;r die Link Farbe hervorgehoben ist falsch';
+$GLOBALS["eLinkVisitedWrong"] = 'Der Wert f&uuml;r die Link Farbe besucht ist falsch';
+$GLOBALS["eMenuLinkWrong"] = 'Der Wert f&uuml;r die Men&uuml; Link Farbe ist falsch';
+$GLOBALS["eMenuLinkHoverWrong"] = 'Der Wert f&uuml;r die Link Farbe hervorgehoben im Men&uuml; ist falsch';
+$GLOBALS["eMenuLinkVisitedWrong"] = 'Der Wert f&uuml;r die Link Farbe besucht im Men&uuml; ist falsch';
+$GLOBALS["eTopMenuLinkWrong"] = 'Der Wert f&uuml;r die Link Farbe im Top Men&uuml;  ist falsch';
+$GLOBALS["eTopMenuLinkHoverWrong"] = 'Der Wert f&uuml;r die Link Farbe im Top Men&uuml; hervorgehoben ist falsch';
+$GLOBALS["eTopMenuLinkVisitedWrong"] = 'Der Wert f&uuml;r die Link Farbe im Top Men&uuml; besucht ist falsch';
+$GLOBALS["eRColLinkWrong"] = 'Der Wert f&uuml;r die Rechte Spalte Link Farbe ist falsch';
+$GLOBALS["eRColLinkHoverWrong"] = 'Der Wert f&uuml;r die Link Farbe hervorgehoben in der rechten Spalte  ist falsch';
+$GLOBALS["eRColLinkVisitedWrong"] = 'Der Wert f&uuml;r die Link Farbe besucht in der rechten Spalte ist falsch';
+$GLOBALS["eSLinkWrong"] = 'Der Wert f&uuml;r die Kleine Link Farbe ist falsch';
+$GLOBALS["eSLinkHoverWrong"] = 'Der Wert f&uuml;r die Kleine Link Farbe hervorgehoben ist falsch';
+$GLOBALS["eSLinkVisitedWrong"] = 'Der Wert f&uuml;r die Kleine Link Farbe besucht ist falsch';
+$GLOBALS["eCHeaderWrong"] = 'Der Wert f&uuml;r die Inhalt &Uuml;berschrift Schrift Farbe ist falsch';
+$GLOBALS["eCFontWrong"] = 'Der Wert f&uuml;r die Inhalt Schrift Farbe ist falsch';
+$GLOBALS["eCBorderWrong"] = 'Der Wert f&uuml;r die Farbe des Inhalt-Randes ist falsch';
+$GLOBALS["eTeaserBGWrong"] = 'Der Wert f&uuml;r den Teaser Hintergrund ist falsch';
+$GLOBALS["eTBorderWrong"] = 'Der Wert f&uuml;r die Teaser Rand Farbe ist falsch';
+$GLOBALS["eTHeaderWrong"] = 'Der Wert f&uuml;r die Schrift-Farbe der Teaser &Uuml;berschrift  ist falsch';
+$GLOBALS["eTFontWrong"] = 'Der Wert f&uuml;r die Teaser Schrift Farbe ist falsch';
+$GLOBALS["eRColBGWrong"] = 'Der Wert f&uuml;r die Rechte Spalte Hintergrund ist falsch';
+$GLOBALS["eRColBorderWrong"] = 'Der Wert f&uuml;r die Rand-Farbe der rechten Spalte ist falsch';
+$GLOBALS["eRColHeaderWrong"] = 'Der Wert f&uuml;r die Schrift-Farbe der &Uuml;berschrift in der rechten Spalte ist falsch';
+$GLOBALS["eRColFontWrong"] = 'Der Wert f&uuml;r die Schrift-Farbe in der rechten Spalte ist falsch';
+
+?>
