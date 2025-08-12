71d09ae792d2195b8138ef7bfc2bafa18bb86472
me-suzy <me.suzana@gmail.com>
2025-08-12 23:17:32 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_liens/lang/lang-mod_liens-lang-english.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_liens/lang/lang-mod_liens-lang-english.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE LIENS
+$Mod_Liens_PlRecent="10 most recent links";
+$Mod_Liens_Top="top 10 links";
+$Mod_Liens_form_Auteur="Author";
+$Mod_Liens_form_Mail="E-Mail";
+$Mod_Liens_form_Rubrique="Heading";
+$Mod_Liens_form_Titre="Title";
+$Mod_Liens_form_Lien="Link";
+$Mod_Liens_form_Desc="Description";
+$Mod_Liens_JS_Auteur="Author field must be filled";
+$Mod_Liens_JS_mail="E-Mail field must be filled";
+$Mod_Liens_JS_Rubrique="Heading field must be filled";
+$Mod_Liens_JS_Titre="Title field must be filled";
+$Mod_Liens_JS_Lien="Link field must be filled";
+$Mod_Liens_JS_Desc="Description field must be filled";
+$Mod_Lien_inde_Categorie="Type";
+$Mod_Liens_inde_Date="Date";
+$Mod_Liens_inde_Titre="Title";
+$Mod_Liens_inde_Auteur="Author";
+$Mod_Liens_inde_Clics="Hits";
+
+?>
