b6d67435f41f68cdafd9834f47dfbabf04989e93
me-suzy <me.suzana@gmail.com>
2025-08-13 00:55:25 +0300

    Add Unsorted/Auto-Surf-V2_5_eng/Auto-Surf-V2.5_eng/banner.php

---
+++ Unsorted/Auto-Surf-V2_5_eng/Auto-Surf-V2.5_eng/banner.php
@@ -0,0 +1,88 @@
+<?php
+require('./prepend.inc.php');
+?>
+
+<?
+include("./templates/main-header.txt");
+?>
+
+
+<br><font size="3"><table border="0" align="center">
+              <tr>
+                <td rowspan="6" width="483" valign="top">
+                  <div align="center">
+                    <p><center>Here are our banners.<br>
+                    </p>
+                    <center><table width="489" border="0">
+                      <tr>
+                        <td colspan="3" align="center"><a href="<? echo "$bannaurl"; ?>" target="_blank"><img src="<? echo "$banna"; ?>" width="<? echo "$einsa"; ?>" height="<? echo "$einsb"; ?>" border="0"></a></td>
+                      </tr>
+                      <tr>
+                        <td width="73"></td>
+                        <td width="77"></td>
+                        <td width="325"></td>
+                      </tr>
+                      <TR>
+                      <td colspan="3">
+                          <p align="center">HTML:<br>
+                            <textarea name="textfield" cols="50" rows="4"><a href="<? echo "$bannaurl"; ?>" target="_blank"><img src="<? echo "$banna"; ?>" width="<? echo "$einsa"; ?>" height="<? echo "$einsb"; ?>" border="0"></a></textarea>
+                          </p>
+                        </td>
+                      </TR>
+<center><table width="489" border="0">
+<tr>
+                        <td colspan="3" align="center"><a href="<? echo "$bannburl"; ?>" target="_blank"><img src="<? echo "$bannb"; ?>" width="<? echo "$zweia"; ?>" height="<? echo "$zweib"; ?>" border="0"></a></td>
+                      </tr>
+                      <tr>
+                        <td width="73"></td>
+                        <td width="77"></td>
+                        <td width="325"></td>
+                      </tr>
+                      <TR>
+                      <td colspan="3">
+                          <p align="center">HTML:<br>
+                            <textarea name="textfield" cols="50" rows="4"><a href="<? echo "$bannburl"; ?>" target="_blank"><img src="<? echo "$bannb"; ?>" width="<? echo "$zweia"; ?>" height="<? echo "$zweib"; ?>" border="0"></a></textarea>
+                          </p>
+                        </td>
+                      </TR>
+                    </table>
+                   </div>
+                </td>
+              </tr>
+<center><table width="489" border="0">
+<tr>
+                        <td colspan="3" align="center"><a href="<? echo "$banncurl"; ?>" target="_blank"><img src="<? echo "$bannc"; ?>" width="<? echo "$dreia"; ?>" height="<? echo "$dreib"; ?>" border="0"></a></td>
+                      </tr>
+                      <tr>
+                        <td width="73"></td>
+                        <td width="77"></td>
+                        <td width="325"></td>
+                      </tr>
+                      <TR>
+                      <td colspan="3">
+                          <p align="center">HTML:<br>
+                            <textarea name="textfield" cols="50" rows="4"><a href="<? echo "$banncurl"; ?>" target="_blank"><img src="<? echo "$bannc"; ?>" width="<? echo "$dreia"; ?>" height="<? echo "$dreib"; ?>" border="0"></a></textarea>
+                          </p>
+                        </td>
+                      </TR>
+<center><table width="489" border="0">
+<tr>
+                        <td colspan="3" align="center"><a href="<? echo "$banndurl"; ?>" target="_blank"><img src="<? echo "$bannd"; ?>" width="<? echo "$viera"; ?>" height="<? echo "$vierb"; ?>" border="0"></a></td>
+                      </tr>
+                      <tr>
+                        <td width="73"></td>
+                        <td width="77"></td>
+                        <td width="325"></td>
+                      </tr>
+                      <TR>
+                      <td colspan="3">
+                          <p align="center">HTML:<br>
+                            <textarea name="textfield" cols="50" rows="4"><a href="<? echo "$banndurl"; ?>" target="_blank"><img src="<? echo "$bannd"; ?>" width="<? echo "$viera"; ?>" height="<? echo "$vierb"; ?>" border="0"></a></textarea>
+                          </p>
+                        </td>
+                      </TR>
+            </table>
+
+<?
+include("./templates/main-footer.txt");
+?>
