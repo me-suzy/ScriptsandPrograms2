cedab67505d67b7df3534eb61eff3674c22049be
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:05 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_cykeltilsalg_62.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_cykeltilsalg_62.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 62;
+function objfile_62 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Cykel til salg";
+$objdata['subname'] = "Jan H. Andersen";
+$objdata['extension'] = "";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "Jeg har en cykel til salg. 2 år gammel. Fin stand. 4.700,- ";
+$obj->createObject($objdata);
+$obj->moveTo(3);
+return $obj;
+}
+?>
