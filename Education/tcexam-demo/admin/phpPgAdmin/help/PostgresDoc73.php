06b9c40ce82f4c4d304170da26c8c15c5a388e86
me-suzy <me.suzana@gmail.com>
2025-08-12 13:39:38 +0300

    Add Education/tcexam-demo/admin/phpPgAdmin/help/PostgresDoc73.php

---
+++ Education/tcexam-demo/admin/phpPgAdmin/help/PostgresDoc73.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Help links for PostgreSQL 7.3 documentation
+ *
+ * $Id: PostgresDoc73.php,v 1.2 2004/11/07 12:27:14 soranzo Exp $
+ */
+
+include('./help/PostgresDoc72.php');
+
+# TODO: Check and fix links for 7.3
+
+$this->help_base = 'http://www.postgresql.org/docs/7.3/interactive/';
+
+$this->help_page['pg.database.create'][1] = 'manage-ag-createdb.html';
+
+?>
