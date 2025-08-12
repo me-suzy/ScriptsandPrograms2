cd7bdc82e5cb64edad67f95a8c6f88a196eb8088
me-suzy <me.suzana@gmail.com>
2025-08-12 16:33:24 +0300

    Add Games and Entertainment/realms_v0.0.2/realms v0.0.1/templates/template.php

---
+++ Games and Entertainment/realms_v0.0.2/realms v0.0.1/templates/template.php
@@ -0,0 +1,11 @@
+{junk}
+<html>
+<head>
+<title>{title}</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+{menu}<br>{event}<br>{content}<br>{right} {left}<br>{lowregion}<br>{lowchat}
+</body>
+</html>
