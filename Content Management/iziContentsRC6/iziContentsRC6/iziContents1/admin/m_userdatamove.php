70522d8db53ff9d66e3e7b3e9f79d5a4c82d03a9
me-suzy <me.suzana@gmail.com>
2025-08-12 03:39:24 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_userdatamove.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/admin/m_userdatamove.php
@@ -0,0 +1,84 @@
+<?php
+
+/***************************************************************************
+
+ m_userdatamove.php
+ -------------------
+ copyright : (C) 2005 - The iziContents Development Team
+
+ iziContents version : 1.0
+ fileversion : 1.0.1
+ change date : 23 - 04 - 2005
+ ***************************************************************************/
+
+/***************************************************************************
+ The iziContents Development Team offers no warranties on this script.
+ The owner/licensee of the script is solely responsible for any problems
+ caused by installation of the script or use of the script.
+
+ All copyright notices regarding iziContents and ezContents must remain intact on the scripts and in the HTML for the scripts.
+
+ For more info on iziContents,
+ visit http://www.izicontents.com*/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the License which can be found within the
+ *   zipped package. Under the licence of GPL/GNU.
+ *
+ ***************************************************************************/
+
+include_once ("rootdatapath.php");
+
+$GLOBALS["form"] = 'userdata';
+$validaccess = VerifyAdminLogin();
+
+
+if ($GLOBALS["canedit"] == True) {
+   ShuffleUserdata();
+   Header("Location: ".BuildLink('m_userdata.php')."&page=".$_GET["page"]."&sort=".$_GET["sort"]);
+} else {
+   Header("Location: ".BuildLink('adminlogin.php'));
+}
+
+
+function ShuffleUserdata()
+{
+   global $_GET;
+
+   // get userdataorderid for the userdata element that we want to move....
+   $strQuery = "SELECT userdataorderid FROM ".$GLOBALS["eztbUserdata"]." WHERE userdataname='".$_GET["UserdataName"]."'";
+   $fromresult = dbRetrieve($strQuery,true,0,1);
+   while ($rs = dbFetch($fromresult)) {
+      $userdataorderid = $rs["userdataorderid"];
+
+      // get userdataorderid for the previous or next userdata element in the list
+      $newuserdataorderid = '';
+      if ($_GET["direction"] == 'up') {
+         $strQuery = "SELECT userdataname,userdataorderid FROM ".$GLOBALS["eztbUserdata"]." WHERE userdataorderid<'".$userdataorderid."' ORDER BY userdataorderid DESC";
+      } else {
+         $strQuery = "SELECT userdataname,userdataorderid FROM ".$GLOBALS["eztbUserdata"]." WHERE userdataorderid>'".$userdataorderid."' ORDER BY userdataorderid";
+      }
+      $toresult = dbRetrieve($strQuery,true,0,1);
+      while ($rs = dbFetch($toresult)) {
+         $newuserdataname    = $rs["userdataname"];
+         $newuserdataorderid = $rs["userdataorderid"];
+      }
+      dbFreeResult($toresult);
+
+      // Swap the two, as long as there was a next or previous record to swap with
+      if ($newuserdataorderid != '') {
+         $strQuery = "UPDATE ".$GLOBALS["eztbUserdata"]." SET userdataorderid='".$userdataorderid."' WHERE userdataname='".$newuserdataname."'";
+         $result = dbExecute($strQuery,true);
+         $strQuery = "UPDATE ".$GLOBALS["eztbUserdata"]." SET userdataorderid='".$newuserdataorderid."' WHERE userdataname='".$_GET["UserdataName"]."'";
+         $result = dbExecute($strQuery,true);
+      }
+   }
+   dbFreeResult($fromresult);
+   dbCommit();
+
+} // function ShuffleUserdata()
+
+?>
+
