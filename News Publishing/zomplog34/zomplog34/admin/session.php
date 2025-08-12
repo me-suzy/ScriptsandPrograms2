f8e1b865ece9a95c0670a71f4efd2bc8cad83312
me-suzy <me.suzana@gmail.com>
2025-08-12 21:06:42 +0300

    Add News Publishing/zomplog34/zomplog34/admin/session.php

---
+++ News Publishing/zomplog34/zomplog34/admin/session.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Written by Gerben Schmidt, http://scripts.zomp.nl */
+
+session_register("login");
+session_register("password");
+session_register("loggedIn");
+
+$messages=array();
+?>
