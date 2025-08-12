3b37052c7738a29a16b640943e11c4c70c72a486
me-suzy <me.suzana@gmail.com>
2025-08-12 05:04:47 +0300

    Add Content Management/jetbox_cms_v2.1.SR1/jetbox/admin/postlister_orig/table.php

---
+++ Content Management/jetbox_cms_v2.1.SR1/jetbox/admin/postlister_orig/table.php
@@ -0,0 +1,43 @@
+<?php
+require("functions.php");
+
+sidehoved("", 0);
+
+
+
+if (!$send) {
+	echo "$s6<p>\n";
+	echo "<form action=$PHP_SELF method=post>\n";
+	echo "<input type=submit name=send value=\"$s7\">\n";
+	echo "</form>\n";
+}
+
+
+
+else {
+	# The name of the main table must include at least one character (naturally),
+	# and numbers are allowed. However, table names consisting *only*
+	# of numbers are not allowed.
+	if (!ereg("^[A-Za-z]+[0-9]*$", $mainTable)) {
+		# "Error: The name of the main table is invalid":
+		fejl($s10);
+	}
+
+	mysql_prefix_query("create table $mainTable (
+		liste char ( 20 ),
+		tilmeldingsbesked text,
+		afmeldingsbesked text,
+		standardafsender char ( 100 ),
+		signatur text,
+		afsender char ( 100 ),
+		emne char ( 100 ),
+		ebrev text
+	)");
+
+	echo "$s11\n";
+}
+
+
+
+sidefod();
+?>
