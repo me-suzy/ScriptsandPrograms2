11a49f1061e986087444e99a0eef58ca68d93b50
me-suzy <me.suzana@gmail.com>
2025-08-12 07:22:42 +0300

    Add Content Management/mysource_2.10.2/mysource_2.10.2/squizlib/wysiwyg/pop_ups/spellchecker_iframe.php

---
+++ Content Management/mysource_2.10.2/mysource_2.10.2/squizlib/wysiwyg/pop_ups/spellchecker_iframe.php
@@ -0,0 +1,36 @@
+<?  ##############################################
+   ### SQUIZLIB ------------------------------###
+  ##- SPELL CHECKER ------ PHP4 --------------##
+ #-- Copyright Squiz.net ---------------------#
+##############################################
+## This file is subject to version 1.0 of the
+## MySource License, that is bundled with
+## this package in the file LICENSE, and is
+## available at through the world-wide-web at
+## http://mysource.squiz.net/
+## If you did not receive a copy of the MySource
+## license and are unable to obtain it through
+## the world-wide-web, please contact us at
+## mysource@squiz.net so we can mail you a copy
+## immediately.
+##
+## $Source: /home/cvsroot/squizlib/wysiwyg/pop_ups/spellchecker_iframe.php,v $
+## $Revision: 1.2 $
+## $Author: mmcintyre $
+## $Date: 2003/05/09 01:55:32 $
+#######################################################################
+
+#######################################################################
+?>
+
+<HTML>
+<HEAD>
+<TITLE>
+Spell Checker
+</TITLE>
+</HEAD>
+<BODY STYLE="background-color: #FFFFFF">
+<? //include_once("./spellchecker.php"); ?>
+<IFRAME SRC="spellchecker.php" NAME="wysiwyg_spellchecker" ID="wysiwyg_spellchecker" SCROLLING="auto" WIDTH="666" HEIGHT="305" MARGINWIDTH=0 MARGINHEIGHT=0 FRAMEBORDER="No" ALLOWTRANSPARENCY="true" STYLE="background-color: #FFFFFF"></IFRAME>
+</BODY>
+</HTML>
