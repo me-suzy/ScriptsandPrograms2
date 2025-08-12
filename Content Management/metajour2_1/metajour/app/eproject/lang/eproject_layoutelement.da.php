2602fa2bfb43637bd90fbb590c38a34da122e9ae
me-suzy <me.suzana@gmail.com>
2025-08-12 06:00:27 +0300

    Add Content Management/metajour2_1/metajour/app/eproject/lang/eproject_layoutelement.da.php

---
+++ Content Management/metajour2_1/metajour/app/eproject/lang/eproject_layoutelement.da.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eProject
+ * @subpackage lang
+ * $Id: eproject_layoutelement.da.php,v 1.2 2005/01/12 03:24:27 jan Exp $
+ */
+
+$LANG['name'] = 'Projekt layout element';
+$LANG['label_name'] = 'Afdeling/funktion';
+?>
