af4fc451cc83ca543c22adadc76cad3548359a54
me-suzy <me.suzana@gmail.com>
2025-08-12 23:55:24 +0300

    Add Top Sites/atsphp-5.0.1/languages/russian.php

---
+++ Top Sites/atsphp-5.0.1/languages/russian.php
@@ -0,0 +1,368 @@
+<?php
+//===========================================================================\\
+// Aardvark Topsites PHP 5                                                   \\
+// Copyright (c) 2003-2005 Jeremy Scheff.  All rights reserved.              \\
+//---------------------------------------------------------------------------\\
+// http://www.aardvarkind.com/                        http://www.avatic.com/ \\
+//---------------------------------------------------------------------------\\
+// This program is free software; you can redistribute it and/or modify it   \\
+// under the terms of the GNU General Public License as published by the     \\
+// Free Software Foundation; either version 2 of the License, or (at your    \\
+// option) any later version.                                                \\
+//                                                                           \\
+// This program is distributed in the hope that it will be useful, but       \\
+// WITHOUT ANY WARRANTY; without even the implied warranty of                \\
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General \\
+// Public License for more details.                                          \\
+//===========================================================================\\
+
+// When you make a new translation, fill out the following four variables to
+// get credit for you work.
+$translation = 'Russian';
+$translator_name = 'Andrey Kanevsky';
+$translator_email = 'admin@americaru.com';
+$translator_url = 'http://www.americaru.com/';
+
+// Global
+$LNG['g_form_submit_short'] = "Go";
+$LNG['g_username'] = "Èìÿ ïîëüçîâàòåëÿ";
+$LNG['g_url'] = "URL";
+$LNG['g_title'] = "Çàãîëîâîê";
+$LNG['g_description'] = "Îïèñàíèå";
+$LNG['g_category'] = "Êàòåãîðèÿ"; // 4.1.0
+$LNG['g_email'] = "Email";
+$LNG['g_banner_url'] = "URL áàííåðà";
+$LNG['g_password'] = "Ïàðîëü";
+$LNG['g_average'] = "Ñðåäíåå";
+$LNG['g_today'] = "Ñåãîäíÿ";
+$LNG['g_yesterday'] = "Â÷åðà";
+$LNG['g_daily'] = "Â äåíü"; // 5.0
+$LNG['g_this_month'] = "Â ýòîì ìåñÿöå"; // 5.0
+$LNG['g_last_month'] = "Â ïðîøëîì ìåñÿöå"; // 5.0
+$LNG['g_monthly'] = "Â ìåñÿö"; // 5.0
+$LNG['g_this_week'] = "Íà ýòîé íåäåëå"; // 5.0
+$LNG['g_last_week'] = "Íà ïðîøëîé íåäåëå"; // 5.0
+$LNG['g_weekly'] = "Â íåäåëþ"; // 5.0
+$LNG['g_pv'] = 'Ïîêàçîâ ñòðàíèö'; // 5.0
+$LNG['g_overall'] = 'Âñåãî'; // 5.0
+$LNG['g_in'] = 'Ïðèøëî'; // 5.0
+$LNG['g_out'] = 'Óøëî'; // 5.0
+$LNG['g_unq_pv'] = "Óíèêàëüíûõ ñòðàíèö"; // 5.0
+$LNG['g_tot_pv'] = "Âñåãî ñòðàíèö"; // 5.0
+$LNG['g_unq_in'] = "Óíèêàëüíûõ ïðèøëî"; // 5.0
+$LNG['g_tot_in'] = "Âñåãî ïðèøëî"; // 5.0
+$LNG['g_unq_out'] = "Óíèêàëüíûõ óøëî"; // 5.0
+$LNG['g_tot_out'] = "Âñåãî óøëî"; // 5.0
+$LNG['g_invalid_u_or_p'] = "Íåïðàâèëüíîå èìÿ ïîëüçîâàòåëÿ èëè ïàðîëü.  Ïîïðîáóéòå åùå ðàç."; // 5.0
+$LNG['g_invalid_u'] = "Íåïðàâèëüíîå èìÿ ïîëüçîâàòåëÿ.  Ïîïðîáóéòå åùå ðàç."; // 5.0
+$LNG['g_invalid_p'] = "Íåïðàâèëüíûé ïàðîëü.  Ïîïðîáóéòå åùå ðàç."; // 5.0
+$LNG['g_session_expired'] = "Ñåññèÿ îòýêñïàðèëàñü.  Ïîïðîáóéòå åùå ðàç."; // 5.0
+$LNG['g_error'] = "Îøèáêà"; // 5.0
+$LNG['g_delete_install'] = "Ïî ñîîáðàæåíèÿì áåçîïàñíîñòè Âû äîëæíû óäàëèòü äèðåêòîðèþ /install ïåðåä çàïóñêîì ñêðèïòà."; // 5.0
+
+// Edit Account
+$LNG['edit_header'] = "Ìîè äàííûå";
+$LNG['edit_info_edited'] = "Èçìåíåíèÿ íà âàøåì àêêàóíòå óñïåøíî ñîõðàíåíû.";
+$LNG['edit_password_blank'] = "Îñòàâüòå ýòî ïîëå ïóñòûì, ÷òîáû ñîõðàíèòü òåêóùèé ïàðîëü."; // 4.0
+
+// Gateway Page
+$LNG['gateway_header'] = "Øëþçîâàÿ ñòðàíèöà ðåéòèíãà";
+$LNG['gateway_text'] = "Äëÿ ïðåäîòâðàùåíèÿ íàêðóòêè âû ïåðåíàïðàâëåíû íà øëþçîâóþ ñòðàíèöó. Íàæìèòå íà ññûëêó íèæå äëÿ âõîäà â ðåéòèíã.";
+$LNG['gateway_vote'] = "Âîéòè è ïðîãîëîñîâàòü";
+$LNG['gateway_no_vote'] = "Âîéòè áåç ãîëîñîâàíèÿ"; // 5.0
+
+// Install
+$LNG['install_header'] = "Óñòàíîâèòü";
+$LNG['install_welcome'] = "Äîáðî ïîæàëîâàòü â ïðîãðàììó óñòàíîâêè Aardvark Topsites PHP 5. Çàïîëíèòå ôîðìó, ïðèâåäåííóþ íèæå, ÷òîáû íà÷àòü óñòàíîâêó.";
+$LNG['install_sql_prefix'] = "Ïðèñòàâêà ê èìåíè òàáëèö - ìåíÿéòå òîëüêî åñëè âû çàïóñêàåòå áîëåå îäíîãî ðåéòèíãà â òîé æå áàçå äàííûõ";
+$LNG['install_error_chmod'] = "Ïðîãðàììà óñòàíîâêè íå èìååò ïðàâ íà çàïèñü â ôàéë settings_sql.php.  Ïðîâåðüòå, ÷òî âû èçìåíèëè ïðàâà äîñòóïà íà 666 (chmod 666) äëÿ ôàéëà settings_sql.php.";
+$LNG['install_error_sql'] = "Ïðîãðàììà óñòàíîâêè íå ìîæåò ïðèñîåäèíèòüñÿ ê áàçå äàííûõ. Âåðíèòåñü è ïðîâåðüòå óñòàíîâêè ïîäêëþ÷åíèÿ ê áàçå.";
+$LNG['install_done'] = "Âàø Topsites ðåéòèíã óñïåøíî óñòàíîâëåí. Íåìåäëåííî óäàëèòå äèðåêòîðèþ /install.";
+$LNG['install_your'] = "Âàø ðåéòèíã Topsites";
+$LNG['install_admin'] = "Àäìèíèñòðèðîâàíèå";
+$LNG['install_manual'] = "Ðóêîâîäñòâî";
+$LNG['upgrade_header'] = "Îáíîâëåíèå";
+$LNG['upgrade_welcome'] = "Äîáðî ïîæàëîâàòü â ïðîãðàììó îáíîâëåíèÿ Aardvark Topsites PHP 5. Ïåðåä îáíîâëåíèåì îáÿçàòåëüíî ñäåëàéòå ðåçåðâíóþ êîïèþ áàçû äàííûõ.";
+$LNG['upgrade_error_version'] = "Îáíîâëåíèå âîçìîæíî òîëüêî äëÿ âåðñèè Aardvark Topsites PHP 4.1.0 èëè âûøå.";
+$LNG['upgrade_done'] = "Âàøà êîïèÿ Topsites ðåéòèíãà áûëà îáíîâëåíà. Íåìåäëåííî óäàëèòå äèðåêòîðèþ /install.";
+
+// Join
+$LNG['join_header'] = "Çàðåãèñòðèðîâàòü ñàéò â ðåéòèíãå";
+$LNG['join_enter_text'] = "Ââåäèòå òåêñò, êîòîðûé ïîêàçàí íà èçîáðàæåíèè íèæå:"; // 4.2.2
+$LNG['join_user'] = "Ó÷àñòíèê"; // 5.0
+$LNG['join_website'] = "Âåá ñàéò"; // 5.0
+$LNG['join_error_forgot'] = "Âû çàáûëè:";
+$LNG['join_error_username'] = "Èìÿ ïîëüçîâàòåëÿ ìîæåò ñîñòîÿòü òîëüêî èç: áóêâ, öèôð, ïîä÷åðêèâàíèé è òèðå."; // 5.0
+$LNG['join_error_username_duplicate'] = "Èñïîëüçóéòå äðóãîå èìÿ ïîëüçîâàòåëÿ, ýòî óæå çàíÿòî."; // 5.0
+$LNG['join_error_url'] = "Ââåäèòå ïðàâèëüíûé URL.";
+$LNG['join_error_email'] = "Ââåäèòå êîððåêòíûé e-mail àäðåñ.";
+$LNG['join_error_title'] = "Ââåäèòå çàãîëîâîê Âàøåãî ñàéòà.";
+$LNG['join_error_password'] = "Ââåäèòå ïàðîëü.";
+$LNG['join_error_urlbanner'] = "Ââåäèòå àäðåñ ñóùåñòâóþùåãî áàííåðà. Íå çàïîëíÿéòå ýòî ïîëå, åñëè ó Âàñ íåò áàííåðà. Îí äîëæåí áûòü ìåíüøå"; // 4.0
+$LNG['join_error_back'] = "Âåðíèòåñü è èñïðàâüòå îøèáêè.";
+$LNG['join_error_time'] = "Íå îáíîâëÿéòå ñòðàíèöó ñ ïîäòâåðæäåíèåì ðåãèñòðàöèè."; // 4.2.0
+$LNG['join_error_captcha'] = "Çíà÷åíèå, êîòîðîå âû ââåëè, íå ñîâïàäàåò ñ òåì, ÷òî íàïèñàíî íà èçîáðàæåíèè."; // 4.2.2
+$LNG['join_thanks'] = "Ñïàñèáî çà ðåãèñòðàöèþ! Âíåñèòå ýòîò êîä íà âàø ñàéò è îí ïîÿâèòñÿ â ðåéòèíãå.";
+$LNG['join_change_warning'] = "Èçìåíåíèÿ â êîäå ìîãóò íàðóøèòü åãî ðàáîòîñïîñîáíîñòü.";
+$LNG['join_welcome'] = "Äîáðî ïîæàëîâàòü â %s";
+$LNG['join_welcome_admin'] = "A new member has joined your topsites list.";
+
+// Link Code
+$LNG['link_code_header'] = "Êîä ññûëêè"; // 5.0
+
+// Lost Password
+$LNG['lost_pw_header'] = "Ïàðîëü óòåðÿí"; // 5.0
+$LNG['lost_pw_forgot'] = "Çàáûëè Âàø ïàðîëü?"; // 5.0
+$LNG['lost_pw_get'] = "Ïîëó÷èòü ïàðîëü"; // 5.0
+$LNG['lost_pw_emailed'] = "Ïðîâåðüòå âàø e-mail, âàì îòïðàâëåíî ïèñüìî ñ äàëüíåéøèìè óêàçàíèÿìè."; // 5.0
+$LNG['lost_pw_email'] = "×òîáû ïîëó÷èòü íîâûé ïàðîëü, íàæèòå íà ýòó ññûëêó:"; // 5.0
+$LNG['lost_pw_new'] = "Ââåäèòå íîâûé ïàðîëü"; // 5.0
+$LNG['lost_pw_set_new'] = "Ñîõðàíèòü íîâûé ïàðîëü"; // 5.0
+$LNG['lost_pw_finish'] = "Âàø ïàðîëü îáíîâëåí."; // 5.0
+
+// Main Page
+$LNG['main_header'] = "Ðåéòèíã"; // 5.0
+$LNG['main_all'] = "Âñå ñàéòû"; // 4.2.0
+$LNG['main_method'] = "Ìåòîä ðåéòèíãà";
+$LNG['main_members'] = "Ó÷àñòíèêè";
+$LNG['main_menu_rankings'] = "Ðåéòèíã";
+$LNG['main_menu_join'] = "Ñòàòü ó÷àñòíèêîì";
+$LNG['main_menu_random'] = "Ñëó÷àéíûé ñàéò";
+$LNG['main_menu_search'] = "Ïîèñê";
+$LNG['main_menu_lost_code'] = "Ïîëó÷èòü êîä"; // 5.0
+$LNG['main_menu_lost_password'] = "Ïîëó÷èòü ïàðîëü"; // 5.0
+$LNG['main_menu_edit'] = "Ðåäàêòèðîâàòü óñòàíîâêè";
+$LNG['main_menu_user_cp'] = "Êîíòðîëüíàÿ ïàíåëü ïîëüçîâàòåëÿ"; // 5.0
+$LNG['main_featured'] = "Ïðîäâèíóòûé ó÷àñòíèê"; // 4.0.2
+$LNG['main_executiontime'] = "Âðåìÿ çàïóñêà ñêðèïòà"; // 4.0
+$LNG['main_queries'] = "SQL çàïðîñîâ"; // 4.0
+$LNG['main_powered'] = "Ñ èñïîëüçîâàíèåì";
+
+// Ranking Table
+$LNG['table_stats'] = "Ñòàòèñòèêà";
+$LNG['table_unique'] = "Óíèêàëüíûõ";
+$LNG['table_total'] = "Âñåãî";
+$LNG['table_rank'] = "Ïîçèöèÿ";
+$LNG['table_title'] = "Íàçâàíèå"; // 4.0
+$LNG['table_description'] = "Îïèñàíèå"; // 4.0
+$LNG['table_movement'] = "Èçìåíåíèå";
+$LNG['table_up'] = "Ââåðõ"; // 5.0
+$LNG['table_down'] = "Âíèç"; // 5.0
+$LNG['table_neutral'] = "Áåç èçìåíåíèé"; // 5.0
+
+// Rate and Review
+$LNG['rate_header'] = "Îöåíêà è îòçûâ";
+$LNG['rate_rating'] = "Îöåíêà";
+$LNG['rate_review'] = "Îòçûâ  - èñïîëüçîâàíèå HTML çàïðåùåíî"; // 5.0
+$LNG['rate_thanks'] = "Ñïàñèáî çà âàøó îöåíêó.";
+$LNG['rate_error'] = "Âû óæå ãîëîñîâàëè çà ýòîò ñàéò.";
+$LNG['rate_back'] = "Âåðíóòüñÿ â ñòàòèñòèêó";
+
+// Search
+$LNG['search_header'] = "Ïîèñê";
+$LNG['search_off'] = "Ïîèñêîâàÿ ôóíêöèÿ âûêëþ÷åíà.";
+$LNG['search_for'] = "Âû èñêàëè: ";
+$LNG['search_no_sites'] = "Ïî çàäàííûì óñëîâèÿì ïîèñêà ñàéòîâ íå îáíàðóæåíî."; // 5.0
+$LNG['search_prev'] = "Ïðåäûäóùèé"; // 3.2.1
+$LNG['search_next'] = "Ñëåäóþùèé"; // 3.2.1
+
+// Stats
+$LNG['stats_header'] = "Ñòàòèñòèêà";
+$LNG['stats_info'] = "Èíôîðìàöèÿ";
+$LNG['stats_member_since'] = "Ó÷àñòíèê ñ"; // 5.0
+$LNG['stats_rating_avg'] = "Ñðåäíèé áàëë";
+$LNG['stats_rating_num'] = "Êîëè÷åñòâî îöåíîê";
+$LNG['stats_rate'] = "Îöåíèòå ýòîò ñàéò";
+$LNG['stats_reviews'] = "Îòçûâû";
+$LNG['stats_allreviews'] = "Ïîñìîòðåòü âñå îòçûâû"; // 4.0
+$LNG['stats_week'] = "Íåäåëÿ"; // 5.0
+$LNG['stats_highest'] = "Ëó÷øèé ïîêàçàòåëü"; // 5.0
+
+// ssi.php
+$LNG['ssi_top'] = "Ëó÷øèå %s ñàéòîâ"; // 4.0
+$LNG['ssi_new'] = "%s íîâûõ ó÷àñòíèêîâ"; // 5.0
+$LNG['ssi_all'] = "Âñå ñàéòû"; // 4.0
+
+// User Control Panel // 5.0
+$LNG['user_cp_header'] = "Êîíòðîëüíàÿ ïàíåëü ó÷àñòíèêà"; // 5.0
+$LNG['user_cp_login'] = "Âîéòè"; // 5.0
+$LNG['user_cp_logout'] = "Âûéòè"; // 5.0
+$LNG['user_cp_welcome'] = "Äîáðî ïîæàëîâàòü â êîíòðîëüíóþ ïàíåëü ó÷àñòíèêà.  Èñïîëüçóéòå ññûëêè ñëåâà äëÿ óïðàâëåíèÿ Âàøèì àêêàóíòîì."; // 5.0
+$LNG['user_cp_logout_message'] = "Âû âûøëè èç êîíòðîëüíîé ïàíåëè ó÷àñòíèêà."; // 5.0
+
+// Admin > Approve New Members // 4.0
+$LNG['a_approve_header'] = "Ðàçðåøèòü âõîä â ðåéòèíã íîâîìó ó÷àñòíèêó"; // 5.0
+$LNG['a_approve'] = "Ðàçðåøèòü"; // 4.0
+$LNG['a_approve_none'] = "Íåò ó÷àñòíèêîâ, îæèäàþùèõ ðàçðåøåíèÿ."; // 4.0
+$LNG['a_approve_done'] = "Ó÷àñòíèêó ðàçðåøåíî áûòü â ðåéòèíãå."; // 4.0
+$LNG['a_approve_dones'] = "Íîâûé ó÷àñòíèê äîïóùåí ê ðåéòèíãó."; // 4.0
+$LNG['a_approve_sel'] = "Ñ âûáðàííûìè:"; // 5.0
+
+// Admin > Approve New Reviews // 5.0
+$LNG['a_approve_rev_header'] = "Ðàçðåøèòü íîâûå îáçîðû"; // 5.0
+$LNG['a_approve_rev_none'] = "Íåò îáçîðîâ, îæèäàþùèõ ðàçðåøåíèÿ."; // 5.0
+$LNG['a_approve_rev_done'] = "Îáçîð áûë ðàçðåøåí."; // 5.0
+$LNG['a_approve_rev_dones'] = "Îáçîðû áûëè ðàçðåøåíû."; // 5.0
+
+// Admin > Delete Member
+$LNG['a_del_header'] = "Óäàëèòü ó÷àñòíèêà"; // 5.0
+$LNG['a_del_headers'] = "Óäàëèòü ó÷àñòíèêîâ"; // 5.0
+$LNG['a_del_done'] = "Ó÷àñòíèê áûë óäàëåí."; // 5.0
+$LNG['a_del_dones'] = "Ó÷àñòíèêè áûëè óäàëåíû."; // 5.0
+$LNG['a_del_warn'] = "Âû óâåðåíû, ÷òî õîòèòå óäàëèòü %s?"; // 5.0
+$LNG['a_del_multi'] = "âñåãî %s ó÷àñòíèêîâ"; //5.0
+
+// Admin > Delete Review // 5.0
+$LNG['a_del_rev_header'] = "Óäàëèòü îáçîð"; // 5.0
+$LNG['a_del_rev_headers'] = "Óäàëèòü îáçîðû"; // 5.0
+$LNG['a_del_rev_done'] = "Îáçîð áûë óäàëåí."; // 5.0
+$LNG['a_del_rev_dones'] = "Îáçîðû áûëè óäàëåíû."; // 5.0
+$LNG['a_del_rev_warn'] = "Âû óâåðåíû, ÷òî õîòèòå óäàëèòü ýòîò îáçîð?"; //5.0
+$LNG['a_del_rev_warns'] = "Âû óâåðåíû, ÷òî õîòèòå óäàëèòü ýòè îáçîðû?"; //5.0
+$LNG['a_del_rev_invalid_id'] = "Íåïðàâèëüíûé ID îáçîðà.  Ïîïðîáóéòå åùå ðàç."; // 5.0
+
+// Admin > Edit Member
+$LNG['a_edit_header'] = "Ðåäàêòèðîâàòü ó÷àñòíèêà"; // 5.0
+$LNG['a_edit_site_is'] = "Ýòî ñàéò"; // 4.0
+$LNG['a_edit_active'] = "Àêòèâåí (â ðåéòèíãå)"; // 4.0
+$LNG['a_edit_inactive'] = "Íåàêòèâåí (íå â ðåéòèíãå)"; // 5.0
+$LNG['a_edit_edited'] = "Ó÷àñòíèê áûë îòðåäàêòèðîâàí.";
+
+// Admin > Edit Review // 5.0
+$LNG['a_edit_rev_header'] = "Ðåäàêòèðîâàòü îáçîð"; // 5.0
+$LNG['a_edit_rev_edited'] = "Îáçîð áûë îòðåäàêòèðîâàí.";
+
+// Admin > Email Members
+$LNG['a_email_header'] = "Ïîñëàòü Email ó÷àñòíèêàì"; // 5.0
+$LNG['a_email_subject'] = "Òåìà"; // 4.2.0
+$LNG['a_email_message'] = "Ñîîáùåíèå"; // 4.2.0
+$LNG['a_email_msg_sent'] = "Email áûë ïîñëàí %s"; // 5.0
+$LNG['a_email_not_sent'] = "Email íå ìîæåò áûòü ïîñëàí %s"; // 5.0
+$LNG['a_email_sent'] = "%s ó÷àñòíèêàì áûë ïîñëàí email."; // 4.2.0
+$LNG['a_email_failed'] = "%s ó÷àñòíèêàì email íå áûë ïîñëàí."; // 4.2.0
+
+// Admin > Logout
+$LNG['a_logout_message'] = "Âû âûøëè èç àêêàóíòà àäìèíèñòðàòîðà."; // 5.0
+
+// Admin > Main
+$LNG['a_header'] = "Àäìèíèñòðàòîð"; // 5.0
+$LNG['a_main'] = "Äîáðî ïîæàëîâàòü â ïàíåëü àäìèíèñòðàòîðà.  Èñïîëüçóéòå ññûëêè â ëåâîé ÷àñòè ñòðàíèöû äëÿ óïðàâëåíèÿ Âàøèì ðåéòèíãîì."; // 5.0
+$LNG['a_main_approve'] = "1 ñàéò æäåò ðàçðåøåíèÿ."; // 5.0
+$LNG['a_main_approves'] = "%s ñàéòîâ æäóò ðàçðåøåíèÿ."; // 5.0
+$LNG['a_main_approve_rev'] = "1 îáçîð æäåò ðàçðåøåíèÿ."; // 5.0
+$LNG['a_main_approve_revs'] = "%s îáçîðîâ æäåò ðàçðåøåíèÿ."; // 5.0
+$LNG['a_main_your'] = "Âàøà âåðñèÿ"; // 5.0
+$LNG['a_main_latest'] = "Ïîñëåäíÿÿ âåðñèÿ"; // 5.0
+$LNG['a_main_new'] = "<a href=\"http://www.aardvarkind.com/\">Aardvark Topsites PHP âåá ñàéò</a>"; // 5.0
+
+// Admin > Manage Members
+$LNG['a_man_header'] = "Óïðàâëåíèå ó÷àñòíèêàìè"; // 5.0
+$LNG['a_man_actions'] = "Äåéñòâèÿ"; // 4.2.0
+$LNG['a_man_edit'] = "Ðåäàêòèðîâàòü"; // 4.2.0
+$LNG['a_man_delete'] = "Óäàëèòü"; // 4.2.0
+$LNG['a_man_email'] = "Email"; // 4.2.0
+$LNG['a_man_all'] = "Âûáðàòü âñå"; // 5.0
+$LNG['a_man_none'] = "Íè÷åãî íå âûáèðàòü"; // 5.0
+$LNG['a_man_del_sel'] = "Óäàëèòü âûáðàííûå"; // 5.0
+
+// Admin > Manage Reviews // 5.0
+$LNG['a_man_rev_header'] = "Óïðàâëåíèå îáçîðàìè ñàéòîâ"; // 5.0
+$LNG['a_man_rev_enter'] = "×òîáû ðåäàêòèðîâàòü îáçîð ñàéòà, ââåäèòå èìÿ ïîëüçîâàòåëÿ."; // 5.0
+$LNG['a_man_rev_id'] = "ID"; // 5.0
+$LNG['a_man_rev_rev'] = "Îáçîð"; // 5.0
+$LNG['a_man_rev_date'] = "Äàòà"; // 5.0
+
+// Admin > Menu
+$LNG['a_menu'] = "Ìåíþ";
+$LNG['a_menu_main'] = "Â íà÷àëî"; // 5.0
+$LNG['a_menu_approve'] = "Ïðîâåðèòü íîâûõ ó÷àñòíèêîâ";
+$LNG['a_menu_manage'] = "Óïðàâëåíèå ó÷àñòíèêàìè"; // 4.2.0
+$LNG['a_menu_settings'] = "Èçìåíèòü íàñòðîéêè"; // 5.0
+$LNG['a_menu_skins'] = "Øàáëîíû è êàòåãîðèè"; // 5.0
+$LNG['a_menu_approve_reviews'] = "Ïðîâåðèòü íîâûå îáçîðû"; // 5.0
+$LNG['a_menu_manage_reviews'] = "Ðåäàêòèðîâàòü îáçîðû"; // 5.0
+$LNG['a_menu_email'] = "Ïîñëàòü email ó÷àñòíèêàì";
+$LNG['a_menu_delete_review'] = "Óäàëèòü îáçîð";
+$LNG['a_menu_logout'] = "Âûéòè";
+$LNG['a_menu_delete'] = "Óäàëèòü ó÷àñòíèêà";
+$LNG['a_menu_edit'] = "Ðåäàêòèðîâàòü ó÷àñòíèêà";
+$LNG['a_header_members'] = "Ó÷àñòíèêè"; // 5.0
+$LNG['a_header_settings'] = "Óñòàíîâêè"; // 5.0
+$LNG['a_header_reviews'] = "Îïèñàíèÿ"; // 5.0
+
+// Admin > Settings
+$LNG['a_s_header'] = "Èçìåíèòü íàñòðîéêè";
+$LNG['a_s_general'] = "Îñíîâíûå íàñòðîéêè";
+$LNG['a_s_admin_password'] = "Ïàðîëü àäìèíèñòðàòîðà";
+$LNG['a_s_list_name'] = "Íàçâàíèå Âàøåãî ðåéòèíãà";
+$LNG['a_s_list_url'] = "URL äèðåêòîðèè, â êîòîðîé óñòàíîâëåí ðåéòèíã";
+$LNG['a_s_default_language'] = "Îñíîâíîé ÿçûê";
+$LNG['a_s_your_email'] = "Âàø email àäðåñ";
+
+$LNG['a_s_sql'] = "Íàñòðîéêè áàçû äàííûõ";
+$LNG['a_s_sql_type'] = "Òèï áàçû äàííûõ"; // 4.1.0
+$LNG['a_s_sql_host'] = "Èìÿ õîñòà";
+$LNG['a_s_sql_database'] = "Íàçâàíèå áàçû";
+$LNG['a_s_sql_username'] = "Èìÿ ïîëüçîâàòåëÿ";
+$LNG['a_s_sql_password'] = "Ïàðîëü";
+
+$LNG['a_s_ranking'] = "Íàñòðîéêè ðåéòèíãà";
+$LNG['a_s_num_list'] = "×èñëî ó÷àñòíèêîâ íà îäíîé ñòðàíèöå"; // 5.0
+$LNG['a_s_ranking_period'] = "Ïåðèîä ðåéòèíãà"; // 5.0
+$LNG['a_s_ranking_method'] = "Ìåòîä ðåéòèíãà"; // 5.0
+$LNG['a_s_ranking_average'] = "Ðåéòèíã ïî ñðåäíèì çíà÷åíèÿì èëè ïî %s"; // 5.0
+$LNG['a_s_featured_member'] = 'Ïðîäâèíóòûé ó÷àñòíèê - äîáàâüòå {$featured_member} â ôàéë wrapper.html ïîñëå âêëþ÷åíèÿ ýòîé óñòàíîâêè.'; // 4.1.0
+$LNG['a_s_top_skin_num'] = "Êîëè÷åñòâî ó÷àñòíèêîâ, êîòîðûå îòáðàæàþòñÿ â âèäå _top";
+$LNG['a_s_ad_breaks'] = "Ïîêàçûâàòü ðåêëàìíûå âñòàâêè ïîñëå ñëåäóþùèõ çíà÷åíèé (ðàçäåëèòü çàïÿòûìè)";
+
+$LNG['a_s_member'] = "Íàñòðîéêè ó÷àñòíèêîâ";
+$LNG['a_s_active_default'] = "Òðåáóåòñÿ ëè ðàçðåøåíèå íîâîìó ó÷àñòíèêó ïåðåä òåì, êàê îí íà÷íåò ïîêàçûâàòüñÿ â ðåéòèíãå";
+$LNG['a_s_active_default_review'] = "Íóæíî ëè ïðîâåðÿòü íîâûå îáçîðû ïåðåä òåì, êàê îíè ñòàíóò äîñòóïíû âñåì";
+$LNG['a_s_delete_after'] = "Ïîñëå ñêîëüêè äíåé áåçäåéñòâèÿ óäàëÿòü íåàêòèâíûõ ó÷àñòíèêîâ (åñëè âûáðàòü 0, óäàëÿòüñÿ íå áóäóò)"; // 4.1.0
+$LNG['a_s_email_admin_on_join'] = "Ïîñûëàòü Âàì email, êîãäà íîâûé ó÷àñòíèê çàðåãèñòðèðîâàëñÿ";
+$LNG['a_s_max_banner_width'] = "Ìàêñèìàëüíàÿ øèðèíà áàííåðà ó÷àñòíèêà (åñëè âûáðàòü 0, îãðàíè÷åíèé íå áóäåò)"; // 4.2.0
+$LNG['a_s_max_banner_height'] = "Ìàêñèìàëüíàÿ âûñîòà áàííåðà ó÷àñòíèêà (åñëè âûáðàòü 0, îãðàíè÷åíèé íå áóäåò)"; // 4.2.0
+$LNG['a_s_default_banner'] = "Áàííåð ïî óìîë÷àíèþ äëÿ òåõ ó÷àñòíèêîâ, êòî íå çàãðóçèë ñâîé";
+
+$LNG['a_s_button'] = "Íàñòðîéêè êíîïîê ðåéòèíãà";
+$LNG['a_s_ranks_on_buttons'] = "Ðåéòèíã íà êíîïêàõ - äëÿ ïîäðîáíîñòåé ïðî÷èòàéòå <a href=\"http://www.aardvarkind.com/topsitesphp/manual/\">ðóêîâîäñòâî</a>.  Âûáèðàéòå \"Êíîïêà ñî ñòàòèñòèêîé\" òîëüêî, åñëè Âû ïðî÷èòàëè ýòîò ðàçäåë ðóêîâîäñòâà.  Åñëè Âû âûáðàëè \"Êíîïêà ñî ñòàòèñòèêîé\", çíà÷åíèÿ âñåõ îñòàëüíûõ ïîëåé íå îêàæóò íèêàêîãî âîçäåéñòâèÿ íà íàñòðîéêè."; // 4.2.0
+$LNG['a_s_stat_buttons'] = "Êíîïêà ñî ñòàòèñòèêîé"; // 4.2.0
+$LNG['a_s_button_url'] = "Ïðè âûáîðå \"Äà\" èëè \"Íåò\" - äëÿ ïîêàçà íà ñàéòàõ ó÷àñòíèêîâ áóäåò âûáðàí URL êíîïêè ïî óìîë÷àíèþ"; // 4.0
+$LNG['a_s_button_dir'] = "Åñëè âûáðàíî \"Äà\" - URL äèðåêòîðèè, â êîòîðîé íàõîäèòñÿ êíîïêà"; // 4.0
+$LNG['a_s_button_ext'] = "Åñëè âûáðàíî \"Äà\" - ðàñøèðåíèå ôàéëà êíîïêè (gif, png, jpg, etc.)"; // 4.0
+$LNG['a_s_button_num'] = "Åñëè âûáðàíî \"Äà\" - ÷èñëî êíîïîê, êîòîðîå Âû ñäåëàåòå"; // 4.0
+
+$LNG['a_s_other'] = "Ïðî÷èå íàñòðîéêè";
+$LNG['a_s_search'] = "Ïîèñê";
+$LNG['a_s_time_offset'] = "Ñäâèã âðåìåíè ðåéòèíãà ïî îòíîøåíèþ ê âðåìåíè ñåðâåðà (â ÷àñàõ)";
+$LNG['a_s_gateway'] = "Øëþçîâàÿ ñòðàíèöà äëÿ ïåðåíàïðàâëåíèÿ íàêðóò÷èêîâ";
+$LNG['a_s_captcha'] = "Ïðîâåðêà ïî ââîäó ñèìâîëîâ ñ èçîáðàæåíèÿ - çàùèòà ïðîòèâ ñïàìåðîâ"; // 4.2.2
+
+$LNG['a_s_on'] = "Âêë";
+$LNG['a_s_off'] = "Âûêë";
+$LNG['a_s_days'] = "Äíåé";
+$LNG['a_s_months'] = "Ìåñÿöåâ";
+$LNG['a_s_weeks'] = "Íåäåëü"; // 4.2.0
+$LNG['a_s_yes'] = "Äà";
+$LNG['a_s_no'] = "Íåò";
+
+$LNG['a_s_updated'] = "Âàøè íàñòðîéêè áûëè îáíîâëåíû.";
+
+// Admin > Skins and Categories // 5.0
+$LNG['a_skins_header'] = "Øàáëîíû è êàòåãîðèè"; // 5.0
+$LNG['a_skins_default'] = "Øàáëîí ïî óìîë÷àíèþ"; // 5.0
+$LNG['a_skins_set_default'] = "Óñòàíîâèòü øàáëîí ïî óìîë÷àíèþ"; // 5.0
+$LNG['a_skins_anon'] = "Anonymous"; // 5.0
+$LNG['a_skins_default_done'] = "Øàáëîí ïî óìîë÷àíèþ áûë âûáðàí."; // 5.0
+$LNG['a_skins_categories_done'] = "Øàáëîí äëÿ êàòåãîðèè áûë âûáðàí."; // 5.0
+$LNG['a_skins_new_category_done'] = "Íîâàÿ êàòåãîðèÿ ñîçäàíà."; // 5.0
+$LNG['a_skins_delete_done'] = "Êàòåãîðèÿ áûëà óäàëåíà."; // 5.0
+$LNG['a_skins_edit_done'] = "Êàòåãîðèÿ áûëà îòðåäàêòèðîâàíà."; // 5.0
+$LNG['a_skins_invalid_skin'] = "Íåïðàâèëüíûé øàáëîí: %s. Ïîïðîáóéòå åùå ðàç."; // 5.0
+$LNG['a_skins_categories'] = "Êàòåãîðèè"; // 5.0
+$LNG['a_skins_new_category'] = "Ñîçäàòü íîâóþ êàòåãîðèþ"; // 5.0
+$LNG['a_skins_set_skins'] = "Âûáðàòü øàáëîí äëÿ êàòåãîðèè"; // 5.0
+$LNG['a_skins_edit_category'] = "Ðåäàêòèðîâàòü êàòåãîðèþ"; // 5.0
+$LNG['a_skins_category_name'] = "Íàçâàíèå êàòåãîðèè"; // 5.0
+$LNG['a_skins_diff_skins'] = "Åñëè Âû õîòèòå âûáðàòü ðàçíûå øàáëîíû äëÿ ðàçíûõ äèðåêòîðèé, âûáåðèòå èõ íèæå."; // 5.0
+?>
