bb292a06ff8427d9a2194ed5a201880739de3279
me-suzy <me.suzana@gmail.com>
2025-08-12 03:56:46 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_sidebars.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/de/lang_sidebars.php
@@ -0,0 +1,41 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Seitenbalken-Vorlagen verwalten';
+
+//  List Headings
+$GLOBALS["tName"] = 'Name';
+$GLOBALS["tBorder"] = 'Rand';
+$GLOBALS["tAlignment"] = 'Ausrichtung';
+$GLOBALS["tColour"] = 'Farbe';
+$GLOBALS["tWidth"] = 'Breite';
+
+//  List Functions
+$GLOBALS["tAddNewSidebar"] = 'neuen Seitenbalken hinzuf&uuml;gen';
+$GLOBALS["tViewSidebar"] = 'Seitenbalken ansehen';
+$GLOBALS["tEditSidebar"] = 'Seitenbalken &auml;ndern';
+$GLOBALS["tDeleteSidebar"] = 'Seitenbalken l&ouml;schen';
+
+//  Form Block Titles
+$GLOBALS["thSBGeneral"] = 'Seitenbalken Detailinformationen';
+
+//  Form Field Headings
+$GLOBALS["tSBName"] = 'Seitenbalken Name';
+$GLOBALS["tSBAlign"] = 'Seitenbalken Ausrichtung';
+$GLOBALS["tSBBorder"] = 'Randgr&ouml;sse';
+$GLOBALS["tSBWidth"] = 'Seitenbalken Breite';
+$GLOBALS["tSBBackground"] = 'Seitenbalken Hintergrundfarbe';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'In diesem Formular k&ouml;nnen Sie eigene Vorlagen f&uuml;r Seitenbalken (Rahmen f&uuml;r Artikel/Bilder) in Ihren Inhalten.';
+$GLOBALS["hSBName"] = 'Auf den Namen des Seitenbalken wird in der Funktion des [Seitenbalken]-Tags bezug genommen. Hiermit kann man den Seitenbalken in der Inhalts-Verwaltung ansprechen, damit er angezeigt wird.';
+$GLOBALS["hSBAlign"] = 'Die Seite des Artikels, auf der der Seitenbalken erscheinen soll, links oder rechts.';
+$GLOBALS["hSBBorder"] = 'Die Gr&ouml;sse des angezeigten Seitenbalkenrandes.';
+$GLOBALS["hSBWidth"] = 'Der maximale Prozentsatz, den die Breite eines Seitenbalkens im Inhaltsframe einnehmen soll.';
+$GLOBALS["hSBBackground"] = 'Hintergrundfarbe f&uuml;r den Seitenbalken.';
+
+//  Error Messages
+$GLOBALS["eNoName"] = 'Die Seitenbalken-Vorlage muss einen Namen haben.';
+$GLOBALS["eColourWrong"] = 'Der Wert der Hintergrundfarbe ist nicht korrekt.';
+
+?>
