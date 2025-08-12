1ec2735d7682479c2bb90a1ef979587b1edd25dd
me-suzy <me.suzana@gmail.com>
2025-08-12 11:45:01 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/espanol/whos_online.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/espanol/whos_online.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Usuarios Conectados');
+
+define('TABLE_HEADING_ONLINE', 'Conectado');
+define('TABLE_HEADING_CUSTOMER_ID', 'ID');
+define('TABLE_HEADING_FULL_NAME', 'Nombre');
+define('TABLE_HEADING_IP_ADDRESS', 'Direccion IP');
+define('TABLE_HEADING_ENTRY_TIME', 'Hora Entrada');
+define('TABLE_HEADING_LAST_CLICK', 'Ultimo Click');
+define('TABLE_HEADING_LAST_PAGE_URL', 'Ultima URL');
+define('TABLE_HEADING_ACTION', 'Acci&oacute;n');
+define('TABLE_HEADING_SHOPPING_CART', 'Cesta del Usuario');
+define('TEXT_SHOPPING_CART_SUBTOTAL', 'Subtotal');
+define('TEXT_NUMBER_OF_CUSTOMERS', 'Hay actualmente %s clientes en línea');
+?>
