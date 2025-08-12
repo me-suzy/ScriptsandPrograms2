e8370ab0f9f1eb60dda38ef3df9b2b1080e23194
me-suzy <me.suzana@gmail.com>
2025-08-13 01:38:52 +0300

    Add Unsorted/cjoverkill/cjadmin/template.php

---
+++ Unsorted/cjoverkill/cjadmin/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/******************************************************
+ * CjOverkill version 2.0.1
+ * © Kaloyan Olegov Georgiev
+ * http://www.icefire.org/
+ * spam@icefire.org
+ * 
+ * Please read the lisence before you start editing this script.
+ * 
+********************************************************/
+
+include ("../cj-conf.inc.php");
+include ("../cj-functions.inc.php");
+cjoverkill_connect();
+
+include ("security.inc.php");
+
+?>
