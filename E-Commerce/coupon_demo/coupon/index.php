75f5777250c0d4950e2238826a2ca820fa622bbe
me-suzy <me.suzana@gmail.com>
2025-08-12 11:18:01 +0300

    Add E-Commerce/coupon_demo/coupon/index.php

---
+++ E-Commerce/coupon_demo/coupon/index.php
@@ -0,0 +1,7 @@
+<?PHP 
+
+/* WARNING: This script is protected. Any attempt to reverse engineer, debug or de-code this file or its dependent files is strictly prohibited */
+
+$codelock_enc="pay_en.php"; $codelock_file="index.php"; $codelock_filed=dirname(__FILE__); include("pay_en.php"); return; ?>
+
+TY6xDoIwFEV/hkHCQo1EOkpJ5RUlLe2rsNoBKURcDPr31sTB7ebc3Jsjy7yOLs1yfUMt4xsaXBPmX9Aij+Ww5Kf0xhWqhIXczqIwaE1r1YAT7XBDHIwPB35XAzv8ehyQiFLadey3lLgjfZ7uxeJK6q8+nfptFjaFUURIE7+EmRztmMiirvnyRllRacw6jS3qMvx7/s+D11xprsIG3me+jsFxgMPXM9IKCcfUYmC1ZBRsTM5yhD3zefIB
