16a39fb26cec50b4dfd730697b399573dc36cab4
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:19 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ipwereklamation_45.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ipwereklamation_45.obj.php
@@ -0,0 +1,23 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 45;
+function objfile_45 () {
+$obj = owNew('structureelement');
+$objdata['name'] = "IPW eReklamation";
+$objdata['pageid'] = "24";
+$objdata['url'] = "";
+$objdata['binfile1'] = "0";
+$objdata['binfile2'] = "0";
+$objdata['binfile3'] = "0";
+$objdata['showtype'] = "0";
+$objdata['target'] = "";
+$objdata['image1'] = "";
+$objdata['image2'] = "";
+$objdata['image3'] = "";
+$objdata['templateid'] = "0";
+$objdata['structureid'] = "0";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
