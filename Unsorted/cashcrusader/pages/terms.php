0566cb7281c6f4218bd68ee0e0ff01062bd1313f
me-suzy <me.suzana@gmail.com>
2025-08-13 01:27:35 +0300

    Add Unsorted/cashcrusader/pages/terms.php

---
+++ Unsorted/cashcrusader/pages/terms.php
@@ -0,0 +1,105 @@
+<? include("setup.php");?>
+<? include("header.php");?>
+<table width="85%" border="0" cellspacing="5" align="center" cellpadding="5">
+<tr> 
+<td> 
+<p> 
+<center>
+<font size="2"><b><br>
+</b></font><b><font size="3">ZZ Terms Of Service </font></b> 
+</center>
+<font size="2"><br>
+<br>
+By signing up and actively using the ZZ program you agree to all terms and conditions 
+set forth in this agreement. ZZ may, at any time, choose to edit, add and/or delete 
+portions of this agreement and impose changes without prior notification of its 
+members. Members will be informed of any and all changes to this policy via an 
+email to their primary contact email address provided upon signing up with ZZ. 
+If any modification is unacceptable to you, your only recourse is to terminate 
+this agreement. Your continued participation in the ZZ program following our posting 
+of a change in policy notice or new agreement on our site will constitute binding 
+acceptance to the change.<br>
+<br>
+<b>Responsibility of Members:</b><br>
+<br>
+You must be 13 years of age or older to participate in this program. Our program 
+is open to both US and International members and is void where prohibited by local 
+governing laws.<br>
+<br>
+You agree to and must use your own name and email address in the sign-up process 
+and may not assume a false identity. You may not sign-up multiple times with different 
+email addresses. Your computer must not be shared with another individual that 
+has an existing ZZ account.<br>
+<br>
+You will use your email address / password combination to access your account, 
+it is your responsibility to keep this information confidential, you may change 
+this information by logging into your account and following the "User Account 
+Info." link.<br>
+You are solely responsible for any and all use of your ZZ account including authorization 
+by you to any third party individual who may use your account.<br>
+<br>
+Should any instances of fraud, system abuse, or any type of activity deemed to 
+be inappropriate or illegal by ZZ be detected it may result in member termination 
+and possible legal action. Any solicitation of advertisers for a confirmation 
+email for purposes of receiving credit in your account will be grounds for immediate 
+termination.<br>
+<br>
+You must maintain your email account that you used when signing up ZZ. You must 
+maintain your e-mail account that you used when signing up with ZZ. You agree 
+to receive e-mail advertising from ZZ. limited to no more than (10) per day.<br>
+<br>
+In the event your e-mail account on file becomes closed or blocked to messages 
+from ZZ, your account will be terminated. This especially applies to e-mail accounts 
+returned for being Full. Any member sending us gibberish will result in automatic 
+termination of account and Forfeiture of All earnings to date of termination. 
+Auto-responses will also be deleted immediately.<br>
+<br>
+</font> <font size="2"><b>Relationship of the Parties:</b><br>
+<br>
+You and ZZ are independent contractors, and nothing in this Agreement creates 
+any partnership, joint venture, agency, franchise, sales representative or employment 
+relationship between you and ZZ. You understand that you do not have authority 
+to make or accept any offers or make any representations on behalf of ZZ. You 
+may not make any statement, whether on your site or otherwise, that would contradict 
+anything in this section. You are solely responsible for the reporting and payment 
+of any taxes for money earned while using the program.<br>
+<br>
+<b>Limitation of Liability:</b><br>
+<br>
+ZZ will not be liable for lost profits, lost business opportunities, or any other 
+indirect, special, punitive, incidental or consequential damages arising out of 
+or related to this Agreement or ZZ program, even if ZZ has been advised of the 
+possibility of such damages. Furthermore, ZZ aggregate liability arising under 
+this Agreement will not exceed the amount of the total fees paid or payable to 
+you under this Agreement. The provisions of this Section survive termination or 
+expiration of the Agreement.<br>
+<br>
+<b>Payments:</b><br>
+<br>
+Members may earn by receiving paid-emails and visiting the advertiser websites 
+after they have entered their member ID in the area on the website provided by 
+ZZ. Members may earn by referring new members to the ZZ program. Members will 
+earn a percentage of advertising revenues collected by ZZ. Members will be paid 
+via PayPal / eGold. ZZ will pay members when their balance reaches the minimum 
+payout rate set by ZZ. Once the member has reached payout they may request payment. 
+Payments will be made within 30 days or a reasonable time after.<br>
+<br>
+<b>Spam:</b><br>
+<br>
+You may not promote your referral links through unsolicited emailing (i.e. SPAMMING), 
+newsgroup postings, or any other method of mass communication. Failure to comply 
+will result in immediate termination of your membership with ZZ and may result 
+in legal prosecution. ZZ strictly enforces anti-Spamming laws. Spamming is a federal 
+crime. Any member caught Spamming will not only have their account terminated 
+immediately and lose any past, present and future earnings, but shall also be 
+held liable for Spamming as we shall cooperate with any authorities and investigations 
+that may arise from the Spamming incident. ZZ may charge up to $5 per spam email 
+sent. Multiple sign-ups shall be grounds for immediate termination of all involved 
+accounts. Signing up multiple times from the same computer also constitutes fraud. 
+ZZ will file charges for recovery of any earnings received from multiple sign-ups.<br>
+<br>
+</font></p>
+</td>
+</tr>
+</table>
+<? include("footer.php");?>
