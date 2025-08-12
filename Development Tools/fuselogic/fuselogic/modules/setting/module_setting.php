9386fe8ed601af5285880dd00171dc3117519562
me-suzy <me.suzana@gmail.com>
2025-08-12 09:26:11 +0300

    Add Development Tools/fuselogic/fuselogic/modules/setting/module_setting.php

---
+++ Development Tools/fuselogic/fuselogic/modules/setting/module_setting.php
@@ -0,0 +1,7 @@
+<?php
+
+$FL_MODULE_SETTING['module_name'] = 'setting';
+$FL_MODULE_SETTING['sub_module']['_autoload']= 'haltebis_setting.php';
+$FL_MODULE_SETTING['sub_module']['test']= 'test.php';
+
+?>
