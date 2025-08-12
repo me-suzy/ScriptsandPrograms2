4a6ce51bf70df6ad4de6058907c43a617fa57874
me-suzy <me.suzana@gmail.com>
2025-08-13 02:20:39 +0300

    Add Unsorted/datingscript/languages/fr/fra.php

---
+++ Unsorted/datingscript/languages/fr/fra.php
@@ -0,0 +1,123 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.1.1                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 05/01/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               fra.php                           #
+# File purpose            Additional langfile for Admin    #
+# File created by         AzDG <support@azdg.com>          #
+# File translated by      Visum <admin@visum.fr.st>        #
+############################################################
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Nom d&#39utilisateur ou mot de passe incorrect - Veuillez réessayer',//1
+'Interface d&#39administration',//2
+'Login de l&#39administrateur',//3
+'Mot de passe de l&#39administrateur',//4
+'Entrer',//5
+'Accès restreint aux administrateurs',//6
+'Page principale',//7
+'Fermer les résultats',//8
+'Réparer&Optimiser',//9
+'Gestion Utilisateur',//10
+'Gestion FAQ',//11
+'Maillist',//12
+'Sortie',//13
+'Vous voilà sorti de l&#39aire d&#39administration<br><br>Merci d&#39utiliser notre script',//14
+'Total des messages',//15
+'Messages envoyés',//16
+'La partie suivante',//17
+'{0} mails a/ont été(s) envoyé(s) à l&#39utilisateur',//18
+'Vous avez des messages non lus dans {0}',//19 {0} - SiteName (C_SNAME)
+'{0} membres trouvés',//20
+'Gestionnaire Maillist',//21
+'Status:',//22
+'Options messages et champs:',//23
+'Type de message',//24
+'Message de',//25
+'Sujet du message',//26
+'Contenu du message',//27
+'Confirmer les résultats',//28
+'Retour',//29
+'Envoyer les messages',//30
+'Envois non-terminés - {0}',//31
+'Sujet : {0} [Envoyé à {1} utilisateurs] :::>',//32
+'Finir cet envoi',//33
+'Text',//34
+'HTML',//35
+'Aperçu',//36
+'Utilisateurs avec messages non lus - ',//37
+'Total des messages non lus - ',//38
+'Dernier message id - ',//39
+'Envoyer lettres de confirmation aux utilisateurs avec messages non lus',//40
+'Profil mis à jour avec succès',//41
+'La question pour la FAQ a été crée avec succès',//42
+'Nouveau',//43
+'Veuillez entrer une question',//44
+'Veuillez rédiger la réponse',//45
+'Gestion FAQ',//46
+'Créer une nouvelle catégorie pour la FAQ',//47
+'Editer la catégorie de la FAQ',//48
+'Supprimer une catégorie de la FAQ',//49
+'Créer une FAQ',//50
+'Sélectionner une catégorie de la FAQ',//51
+'Question de la FAQ',//52
+'Réponse de la FAQ',//53
+'Vous devez d&#39abord créer des catégories',//54
+'Veuillez entrer le nom de la catégorie',//55
+'Créer une catégorie pour la FAQ',//56
+'Nouvelle catégorie crée:',//57
+'Nom de la catégorie',//58
+'Editer catégorie',//59
+'Editer',//60
+'La catégorie a été modifiée',//61
+'Editer catégorie',//62
+'La FAQ a été supprimée',//63
+'La catégorie a été supprimée',//64
+'Attention : En supprimant une catégorie, vous effacez toutes les questions et les réponses qu&#39elle pouvait contenir.',//65
+'Supprimer',//66
+'0 - Attendez le mail de confirmation<br>1 - Attendez la confirmation après l&#39enregistrement<br>2 - Attendez la confirmation après la mise à jour<br>3,8 - Attendez l&#39authorisation pour la suppression(avec/sans annonce)<br>7 - Le membre est actif<br>',//67
+'La base de données a été réparée et optimisée',//68
+'L&#39utilisateur ID#={0} a été supprimé de la base de données',//69
+'Résultats supprimés',//70
+'Utilisateur id={0} accepté',//71
+'IP',//72
+'Requis [0 - 255]',//73
+'Status - (ATTENTION!)',//74
+'Autoriser',//75
+'Gestion Utilisateur',//76
+'Total des membres actifs :',//77
+'Utilisateurs attendant l&#39activation après inscription',//78
+'Utilisateurs attendant l&#39activation après mise à jour',//79
+'Utilisateurs attendant d&#39être supprimés de la base de données',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
