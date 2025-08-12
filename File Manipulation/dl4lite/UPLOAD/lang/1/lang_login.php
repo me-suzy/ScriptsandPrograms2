60fefae708662f79a89baacda0432457d6f54860
me-suzy <me.suzana@gmail.com>
2025-08-12 14:16:30 +0300

    Add File Manipulation/dl4lite/UPLOAD/lang/1/lang_login.php

---
+++ File Manipulation/dl4lite/UPLOAD/lang/1/lang_login.php
@@ -0,0 +1,18 @@
+<?php
+/*********************************************************
+* Name: lang_login.php
+* Author: RW::Download 4.0
+* Contact: realworld@blazefans.com
+* Description: Language variables
+* Version: 4.00
+*********************************************************/
+$lang = array(
+
+'login_ok' => "Thankyou for logging in. Please wait while we redirect you.",
+'logout_ok' => "Your cookies have been cleared and you are now logged out. Please wait while we redirect you.",
+'stayloggedin' => "Stay Logged In",
+'thankyou' => "Thankyou for logging in. Please wait while we redirect you to ",
+'cleared' => "Your cookies have been cleared and you are now logged out. Please wait while we redirect you.", 
+)
+
+?>
