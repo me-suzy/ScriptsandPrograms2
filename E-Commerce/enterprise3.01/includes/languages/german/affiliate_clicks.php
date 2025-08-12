491b9fa841337afd361946ac256d2cad866e1c1f
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:04 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/affiliate_clicks.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/affiliate_clicks.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Partnerprogramm');
+define('HEADING_TITLE', 'Partnerprogramm: Klicks');
+
+define('TABLE_HEADING_DATE', 'Datum');
+define('TABLE_HEADING_REFFERED', 'Referenz');
+define('TABLE_HEADING_IP', 'IP Adresse');
+define('TABLE_HEADING_BROWSER', 'Browser');
+define('TABLE_HEADING_CLICKED_PRODUCT', 'Produkt');
+
+define('TEXT_AFFILIATE_HEADER', 'Alle Klicks von Ihrer Website:');
+define('TEXT_NO_CLICKS', 'Es wurden noch keine Klicks von Ihrer Website aufgezeichnet.');
+define('TEXT_DISPLAY_NUMBER_OF_CLICKS', 'angezeigte Klicks <b>%d</b> bis <b>%d</b> (von <b>%d</b> insgesamt)');
+?>
