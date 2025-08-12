0b87ae0dea368df8e59423f0cca2cdc692e3437f
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:41 +0300

    Add Content Management/metajour2_1/metajour/lang/stylesheet.da.php

---
+++ Content Management/metajour2_1/metajour/lang/stylesheet.da.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Navn';
+$LANG['label_content'] = 'Typografi';
+$LANG['name'] = 'Typografier';
+$LANG['label_standard'] = '';
+$LANG['label_mapping'] = 'Editortypografi';
+?>
