0993f1e27c18318aa8a8f8698ef0aff4603d8c94
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:11 +0300

    Add Content Management/metajour2_1/metajour/lang/task.en.php

---
+++ Content Management/metajour2_1/metajour/lang/task.en.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Tasks';
+$LANG['label_companyid'] = 'Company';
+$LANG['label_name'] = 'Name';
+$LANG['label_content'] = 'Description';
+$LANG['label_priority'] = 'Priority';
+$LANG['label_status'] = 'Status';
+$LANG['label_typecode'] = 'Type';
+$LANG['label_budget'] = 'Budget';
+$LANG['label_offer'] = 'Offer';
+$LANG['label_expstart'] = 'Expected start';
+$LANG['label_expfinish'] = 'Expected finish';
+$LANG['label_finished'] = 'Finished';
+$LANG['label_taskid'] = 'Subtask of';
+$LANG['option_status']['0'] = 'Not started';
+$LANG['option_status']['5'] = 'Running';
+$LANG['option_status']['10'] = 'Finished - approval requested';
+$LANG['option_status']['15'] = 'Fnished - approved';
+$LANG['option_typecode']['0'] = 'Correction';
+$LANG['option_typecode']['5'] = 'Change';
+$LANG['option_typecode']['10'] = 'Addition';
+?>
