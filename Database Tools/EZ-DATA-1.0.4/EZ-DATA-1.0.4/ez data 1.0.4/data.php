aab08f03ae8b0a4d8e34c0701b3be66ed9740d5f
me-suzy <me.suzana@gmail.com>
2025-08-12 08:53:53 +0300

    Add Database Tools/EZ-DATA-1.0.4/EZ-DATA-1.0.4/ez data 1.0.4/data.php

---
+++ Database Tools/EZ-DATA-1.0.4/EZ-DATA-1.0.4/ez data 1.0.4/data.php
@@ -0,0 +1,18 @@
+<?php
+include ("includes/menu.inc");
+?>
+
+<html>
+<head>
+  <title>EZ-Data</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"></head>
+<body background="images/bg.jpg" text="#000000" link="#000000" vlink="#000000" alink="#000000">
+<div align="center"> 
+  <?
+include "includes/view_menu.inc";
+include ("includes/footer.inc");
+?>
+</div>
+<div align="center"></div>
+</body>
+</html>
