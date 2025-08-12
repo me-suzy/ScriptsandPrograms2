a82b20f84d0d32225dfc09e41901f127f087a6cd
me-suzy <me.suzana@gmail.com>
2025-08-12 05:08:12 +0300

    Add Content Management/jetbox_cms_v2.1.SR1/jetbox/includes/phpdig/locales/gr-language.php

---
+++ Content Management/jetbox_cms_v2.1.SR1/jetbox/includes/phpdig/locales/gr-language.php
@@ -0,0 +1,149 @@
+<?php
+/*
+--------------------------------------------------------------------------------
+PhpDig 1.6.x
+This program is provided under the GNU/GPL license.
+See LICENSE file for more informations
+All contributors are listed in the CREDITS file provided with this package
+
+PhpDig Website : http://phpdig.toiletoine.net/
+Contact email : phpdig@toiletoine.net
+Author and main maintainer : Antoine Bajolet (fr) bajolet@toiletoine.net
+--------------------------------------------------------------------------------
+*/
+//------------------------------------------
+//Greek language file from Sofoklis Magoulas
+//------------------------------------------
+//'keyword' => 'translation'
+$phpdig_mess = array (
+'yes'          =>'íáé',
+'no'           =>'ï÷é',
+'delete'       =>'äéáãñáöÞ',
+'reindex'      =>'Åðáêáôçãïñïðïßçóç',
+'back'         =>'ÅðéóôñïöÞ',
+'files'        =>'áñ÷åßá',
+'admin'        =>'Äåéïá÷åéñçóôÞò',
+'warning'      =>'Ðñïóï÷Þ !',
+'index_uri'    =>'Ðïéü URL èÝò íá êáôçãïñïðïéÞóåéò ?',
+'spider_depth' =>'ÂÜèïò áíáæÞôçóçò',
+'spider_warn'  =>"ÂåâáéùèÞôå ðùò êáíåßò Üëëïò äåí êÜíåé åíçìÝñùóç ôçò ìç÷áíÞò, ìåëëïíôéêÜ èá ìðåé óå ëåéôïõñãåßá ðñïóôáôåõôéêüò ìç÷áíéóìüò",
+'site_update'  =>"Åíçìåñþóôå ìéá ðåñéï÷Þ Þ åíüò áðü ôïí êëÜäï ôïõ",
+'clean'        =>'Êáèáñßóôå',
+'t_index'      =>"êáôçãïñéïðïßçóç",
+'t_dic'        =>'ëåîéêü',
+'t_stopw'      =>'êïéíÝò ëÝîåéò',
+
+'update'       =>'ÁíáðñïóáñìïãÞ',
+'tree_found'   =>'Åõñåóç äéêôýùóçò',
+'update_mess'  =>'Åðáêáôçãïñïðïßçóç Þ äéáãñáöÞ ìéá äéêôýùóçò ',
+'update_warn'  =>"Áðïêëåßóôå ôïõò ìüíéìïõò",
+'update_help'  =>'×ôõðÞóôå óôï óôáõñü ãéá íá äéáãñÜøåé ôïí êëÜäï
+÷ôõðÜ óôï ðñÜóéíï óçìÜäé ãéá íá ôï åíçìåñþóåé ',
+'branch_start' =>'ÅðéëÝîôå ôï öÜêåëëï íá åðéäåßîåé óôçí áñéóôåñÞ ðëåõñÜ ',
+'branch_help1' =>'ÅðéëÝîôå áõôÜ ôá Ýããñáöá ãéá áíáðñïóáñìïãÞ ÷ùñéóôÜ ',
+'branch_help2' =>'×ôõðÞóôå óôï óôáõñü ãéá íá äéáãñÜøåé Ýíá Ýããñáöï
+÷ôõðÜ óôï ðñÜóéíï óçìÜäé ãéá íá ôï åðáíåããñÜøåé
+Ôï âÝëïò ðñïùèåß ',
+'redepth'      =>'âÜèïò åðéðÝäùí ',
+'branch_warn'  =>"Ç äéáãñáöÞ åßíáé ìüíéìç",
+'to_admin'     =>"óôçí óåëßäá äéá÷åéñçóôÞ",
+
+'search'       =>'ÁíáæÞôçóç',
+'results'      =>'áðïôåëÝóìáôá',
+'display'      =>'åìöÜíéóç',
+'w_begin'      =>'ëÝîåéò îåêéíüíôáò ìå',
+'w_whole'      =>'ïëüêëçñç ëÝîç',
+'w_part'       =>'êÜèå êïììÜôé ëÝîçò',
+
+'id_start'     =>'Êáôçãïñéïðïßçóç Site',
+'id_end'       =>'Êáôçãïñéïðïßçóç ïëïêëçñþèçêå !',
+'id_recent'    =>'ÓõíôÜ÷èçêå ðñüóöáôá',
+'num_words'    =>'Áñéèìüò ëÝîåùí',
+'time'         =>'÷ñüíïò',
+'error'        =>'ÓöÜëìá',
+'no_spider'    =>'Spider äåí ëåéôïýñãçóå',
+'no_site'      =>'Äåí õðÜñ÷åé áõôü ôï site óôçí âÜóç äåäïìÝíùí',
+'no_temp'      =>'Äåí õðÜñ÷ïõí óõíäÝóåéò óôï ðñïóùñéíü ðßíáêá',
+'no_toindex'   =>'Ôéðïôá ãéá êáôçãïñéïðïßçóç',
+'double'       =>'Éäéï ìå õðÜñ÷ïí åããñáöï',
+
+'spidering'    =>'Spidering óå åîÝëéîç...',
+'links_more'   =>'áêüìá íÝåò óõíäÝóåéò',
+'level'        =>'åðßðåäï',
+'links_found'  =>'óõíäÝóåéò âñÝèçêáí',
+'define_ex'    =>'Êáèïñßóôå ôïõò áðïêëåéóìïýò',
+'index_all'    =>'êáôçãïñéïðïßçóç üëùí',
+
+'end'          =>'ôåëïò',
+'no_query'     =>'Please fill the search form field',
+'pwait'        =>'Please wait',
+'statistics'   =>'Statistics',
+
+// INSTALL
+'slogan'   =>'The smallest search engine in the universe : version',
+'installation'   =>'Installation',
+'instructions' =>'Type here the MySql parameters. Specify a valid existing user who can create databases if you choose create or update.',
+'hostname'   =>'Hostname  :',
+'port'   =>'Port (none = default) :',
+'sock'   =>'Sock (none = default) :',
+'user'   =>'User :',
+'password'   =>'Password :',
+'phpdigdatabase'   =>'PhpDig database :',
+'tablesprefix'   =>'Tables prefix :',
+'instructions2'   =>'* optional. Use lowercase characters, 16 characters max.',
+'installdatabase'   =>'Install phpdig database',
+'error1'   =>'Can\'t find connexion template. ',
+'error2'   =>'Can\'t write connexion template. ',
+'error3'   =>'Can\'t find init_db.sql file. ',
+'error4'   =>'Can\'t create tables. ',
+'error5'   =>'Can\'t find all config database files. ',
+'error6'   =>'Can\'t create database.<br />Verify user\'s rights. ',
+'error7'   =>'Can\'t connect to database<br />Verify connection datas. ',
+'createdb' =>'Create database',
+'createtables' =>'Create tables only',
+'updatedb' =>'Update existing database',
+'existingdb' =>'Write only connection parameters',
+// CLEANUP_ENGINE
+'cleaningindex'   =>'Cleaning index',
+'enginenotok'   =>' index references targeted an inexistent keyword.',
+'engineok'   =>'Engine is coherent.',
+// CLEANUP_KEYWORDS
+'cleaningdictionnary'   =>'Cleaning dictionnary',
+'keywordsok'   =>'All keywords are in one or more page.',
+'keywordsnotok'   =>' keywords where not in one page at least.',
+// CLEANUP_COMMON
+'cleanupcommon' =>'Cleanup common words',
+'cleanuptotal' =>'Total ',
+'cleaned' =>' cleaned.',
+'deletedfor' =>' deleted for ',
+// INDEX ADMIN
+'digthis' =>'Dig this !',
+'databasestatus' =>'DataBase status',
+'entries' =>' Entries ',
+'updateform' =>'Update form',
+'deletesite' =>'Delete site',
+// SPIDER
+'spiderresults' =>'Spider results',
+// STATISTICS
+'mostkeywords' =>'Most keywords',
+'richestpages' =>'Richest pages',
+'mostterms'    =>'Most search terms',
+'largestresults'=>'Largest results',
+'mostempty'     =>'Most searchs giving empty results',
+'lastqueries'   =>'Last search queries',
+'responsebyhour'=>'Response time by hour',
+// UPDATE
+'userpasschanged' =>'User/Password changed !',
+'uri' =>'URI : ',
+'change' =>'Change',
+'root' =>'Root',
+'pages' =>' pages',
+'locked' => 'Locked',
+'unlock' => 'Unlock site',
+'onelock' => 'A site is locked, because of spidering. You can\'t do this for now',
+// PHPDIG_FORM
+'go' =>'Go ...',
+// SEARCH_FUNCTION
+'noresults' =>'No results'
+);
+?>
