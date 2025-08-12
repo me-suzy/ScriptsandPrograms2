12adfd1c87e829cfac5fe6ead4afc5a7c82da540
me-suzy <me.suzana@gmail.com>
2025-08-13 01:05:33 +0300

    Add Unsorted/AzDGDatingGold_3.0.5_nullified.by.vaidas_GTT_team_/templates/countries.php

---
+++ Unsorted/AzDGDatingGold_3.0.5_nullified.by.vaidas_GTT_team_/templates/countries.php
@@ -0,0 +1,239 @@
+<OPTION>Afghanistan
+<OPTION>Albania
+<OPTION>Algeria
+<OPTION>American Samoa
+<OPTION>Andorra
+<OPTION>Angola
+<OPTION>Anguilla
+<OPTION>Antarctica
+<OPTION>Antigua and arbuda
+<OPTION>Argentina
+<OPTION>Armenia
+<OPTION>Aruba
+<OPTION>Australia
+<OPTION>Austria
+<OPTION>Azerbaijan
+<OPTION>Bahamas
+<OPTION>Bahrain
+<OPTION>Bangladesh
+<OPTION>Barbados
+<OPTION>Belarus
+<OPTION>Belgium
+<OPTION>Belize
+<OPTION>Benin
+<OPTION>Bermuda
+<OPTION>Bhutan
+<OPTION>Bolivia
+<OPTION>Bosnia and Herzegovina
+<OPTION>Botswana
+<OPTION>Bouvet Island
+<OPTION>Brazil
+<OPTION>British Indian Ocean Territory
+<OPTION>Brunei Darussalam
+<OPTION>Bulgaria
+<OPTION>Burkina Faso
+<OPTION>Burundi
+<OPTION>Cambodia
+<OPTION>Cameroon
+<OPTION>Canada
+<OPTION>Cape Verde
+<OPTION>Cayman Islands
+<OPTION>Central African Republic
+<OPTION>Chad
+<OPTION>Chile
+<OPTION>China
+<OPTION>Christmas Island
+<OPTION>Cocos Keeling Islands
+<OPTION>Colombia
+<OPTION>Comoros
+<OPTION>Congo
+<OPTION>Cook Islands
+<OPTION>Costa Rica
+<OPTION>Ivory Coast
+<OPTION>Croatia
+<OPTION>Cuba
+<OPTION>Cyprus
+<OPTION>Czech Republic
+<OPTION>Denmark
+<OPTION>Djibouti
+<OPTION>Dominican Republic
+<OPTION>Dominica
+<OPTION>East Timor
+<OPTION>Ecuador
+<OPTION>Egypt
+<OPTION>El Salvador
+<OPTION>Equatorial Guinea
+<OPTION>Eritrea
+<OPTION>Estonia
+<OPTION>Ethiopia
+<OPTION>Falkland Islands
+<OPTION>Faroe Islands
+<OPTION>Fiji
+<OPTION>Finland
+<OPTION>France
+<OPTION>French Guiana
+<OPTION>French Polynesia
+<OPTION>French Southern Territories
+<OPTION>Gabon
+<OPTION>Gambia
+<OPTION>Georgia
+<OPTION>Germany
+<OPTION>Ghana
+<OPTION>Gibraltar
+<OPTION>Greece
+<OPTION>Greenland
+<OPTION>Grenada
+<OPTION>Guadeloupe
+<OPTION>Guam
+<OPTION>Guatemala
+<OPTION>Guinea-Bissau
+<OPTION>Guinea
+<OPTION>Guyana
+<OPTION>Haiti
+<OPTION>Heard and McDonald Islands
+<OPTION>Honduras
+<OPTION>Hong Kong
+<OPTION>Hungary
+<OPTION>Iceland
+<OPTION>India
+<OPTION>Indonesia
+<OPTION>Iran
+<OPTION>Iraq
+<OPTION>Ireland
+<OPTION>Israel
+<OPTION>Italy
+<OPTION>Jamaica
+<OPTION>Japan
+<OPTION>Jordan
+<OPTION>Kazakhstan
+<OPTION>Kenya
+<OPTION>Kiribati
+<OPTION>North Korea
+<OPTION>South Korea
+<OPTION>Kuwait
+<OPTION>Kyrgyzstan
+<OPTION>Laos
+<OPTION>Latvia
+<OPTION>Lebanon
+<OPTION>Lesotho
+<OPTION>Liberia
+<OPTION>Libya
+<OPTION>Liechtenstein
+<OPTION>Lithuania
+<OPTION>Luxembourg
+<OPTION>Macau
+<OPTION>Macedonia
+<OPTION>Madagascar
+<OPTION>Malawi
+<OPTION>Malaysia
+<OPTION>Maldives
+<OPTION>Mali
+<OPTION>Malta
+<OPTION>Marshall slands
+<OPTION>Martinique
+<OPTION>Mauritania
+<OPTION>Mauritius
+<OPTION>Mayotte
+<OPTION>Mexico
+<OPTION>Micronesia
+<OPTION>Moldova
+<OPTION>Monaco
+<OPTION>Mongolia
+<OPTION>Montserrat
+<OPTION>Morocco
+<OPTION>Mozambique
+<OPTION>Myanmar
+<OPTION>Namibia
+<OPTION>Nauru
+<OPTION>Nepal
+<OPTION>Netherlands Antilles
+<OPTION>Netherlands
+<OPTION>New Caledonia
+<OPTION>New Zealand
+<OPTION>Nicaragua
+<OPTION>Nigeria
+<OPTION>Niger
+<OPTION>Niue
+<OPTION>Norfolk Island
+<OPTION>Northern Mariana slands
+<OPTION>Norway
+<OPTION>Oman
+<OPTION>Pakistan
+<OPTION>Palau
+<OPTION>Panama
+<OPTION>Papua New Guinea
+<OPTION>Paraguay
+<OPTION>Peru
+<OPTION>Philippines
+<OPTION>Pitcairn
+<OPTION>Poland
+<OPTION>Portugal
+<OPTION>Puerto Rico
+<OPTION>Qatar
+<OPTION>Reunion
+<OPTION>Romania
+<OPTION>Russian Federation
+<OPTION>Rwanda
+<OPTION>S Georgia and S Sandwich Isles
+<OPTION>Saint Kitts and Nevis
+<OPTION>Saint Lucia
+<OPTION>Saint Vincent and The Grenadines
+<OPTION>Samoa
+<OPTION>San Marino
+<OPTION>Sao Tome and Principe
+<OPTION>Saudi Arabia
+<OPTION>Senegal
+<OPTION>Seychelles
+<OPTION>Sierra Leone
+<OPTION>Singapore
+<OPTION>Slovak Republic
+<OPTION>Slovenia
+<OPTION>Solomon Islands
+<OPTION>Somalia
+<OPTION>South Africa
+<OPTION>Spain
+<OPTION>Sri Lanka
+<OPTION>St. Helena
+<OPTION>St. Pierre and Miquelon
+<OPTION>Sudan
+<OPTION>Suriname
+<OPTION>Svalbard and Jan Mayen Islands
+<OPTION>Swaziland
+<OPTION>Sweden
+<OPTION>Switzerland
+<OPTION>Syria
+<OPTION>Taiwan
+<OPTION>Tajikistan
+<OPTION>Tanzania
+<OPTION>Thailand
+<OPTION>Togo
+<OPTION>Tokelau
+<OPTION>Tonga
+<OPTION>Trinidad and Tobago
+<OPTION>Tunisia
+<OPTION>Turkey
+<OPTION>Turkmenistan
+<OPTION>Turks and Caicos Islands
+<OPTION>Tuvalu
+<OPTION>US Minor Outlying Islands
+<OPTION>Uganda
+<OPTION>Ukraine
+<OPTION>United Arab Emirates
+<OPTION>United Kingdom
+<OPTION>United States
+<OPTION>Uruguay
+<OPTION>Uzbekistan
+<OPTION>Vanuatu
+<OPTION>Vatican City State
+<OPTION>Venezuela
+<OPTION>Viet Nam
+<OPTION>British Virgin Islands
+<OPTION>US Virgin Islands
+<OPTION>Wallis and Futuna Islands
+<OPTION>Western Sahara
+<OPTION>Yemen
+<OPTION>Yugoslavia
+<OPTION>Zaire
+<OPTION>Zambia
+<OPTION>Zimbabwe
+</select>
