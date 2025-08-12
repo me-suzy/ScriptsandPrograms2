b51d48561f05ea50ca2f0a0663bfe0c73ca06181
me-suzy <me.suzana@gmail.com>
2025-08-12 06:39:05 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/iespell/langs/he.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/iespell/langs/he.js
@@ -0,0 +1,6 @@
+// HE lang variables by Liron Newman, http://eesh.net
+
+tinyMCE.addToLang('',{
+iespell_desc : 'äôòì áãé÷ú àéåú',
+iespell_download : "ìà ðîöà ieSpell. ìçõ òì àéùåø ëãé ìäâéò ìãó ääåøãä."
+});
