47894176ddc89d5238b991632b92e3555b47f132
me-suzy <me.suzana@gmail.com>
2025-08-12 08:53:43 +0300

    Add Database Tools/discoman 1.4/discoman 1.4/lang/fr/fr_admin.inc.php

---
+++ Database Tools/discoman 1.4/discoman 1.4/lang/fr/fr_admin.inc.php
@@ -0,0 +1,75 @@
+<?php
+
+//libellés noms de colonnes ou de champ
+$txt_images = "Images";
+$txt_image = "Image";
+$txt_abrege = "Abrégé";
+$txt_admin = "Admin";
+$txt_artistes = "Artistes";
+$txt_artiste = "Artiste";
+$txt_formats = "Formats";
+$txt_format = "Format";
+$txt_annees = "Années";
+$txt_annee = "Année";
+$txt_payss = "Pays";
+$txt_pays = "Pays";
+$txt_refs = "Références";
+$txt_ref = "Référence";
+$txt_coms = "Commentaires";
+$txt_com = "Commentaire";
+$txt_titres = "Titres";
+$txt_titre = "Titre";
+$txt_infos = "Infos";
+$txt_designations = "Désignations";
+$txt_designation = "Désignation";
+
+//divers :
+$txt_nom = "Nom";
+$txt_mdp = "Mot de passe";
+$txt_disques = "Disques";
+$txt_utils = "Utilisateurs";
+
+//Texte d'information
+$txt_saisir = "Veuillez saisir votre nom et votre mot de passe.";
+$txt_choisir_artiste = "Choisissez l'artiste dont vous souhaitez modifier les enregistrements.";
+$txt_choisir_artiste_eff = "Choisissez l'artiste dont vous souhaitez effacer un ou plusieurs enregistrements.";
+$txt_choisir_disque = "Choisissez le disque à modifier.";
+$txt_choisir_disque_sup = "Choisissez le disque à supprimer.";
+$txt_choisir_artiste_maj = "Choisissez l'artiste à modifier.";
+$txt_choisir_artiste_sup = "Choisissez l'artiste à supprimer.";
+
+//textes erreurs
+$txt_result_0 = "Pas de résultat.";
+$txt_erreur_cases_vides = "Erreur : Une ou plusieurs cases ne sont pas renseignées.";
+$txt_lartiste = "L'artiste ";
+$txt_enregistre = "a été enregistré avec succès.";
+$txt_fiche = "Fiche de";
+$txt_maj = "mise à jour.";
+$txt_attention = "Attention !";
+$txt_sup_impos = "Des enregistrements contiennent des références à cette donnée. Suppression impossible tant que les enregistrements n'ont pas été modifiés en conséquence.";
+$txt_nouveau_for = "Le nouveau format";
+
+//libellés en-têtes de pages :
+$txt_maj_disque = "Mise à jour enregistrements";
+$txt_maj_artiste = "Mise à jour artistes";
+$txt_resultat = "Résultats pour";
+$txt_suppression = "Suppression enregistrements";
+$txt_ajout_art = "Ajout artistes";
+$txt_sup_artist = "Suppression artistes";
+$txt_ajout_for = "Ajout format";
+$txt_ajout_pays = "Ajout pays";
+
+//libellés boutons :
+$txt_raffraichir = "Raffraîchir";
+$txt_envoyer = "Valider";
+$txt_modifier = "Modifier";
+$txt_effacer = "Supprimer";
+$txt_ajouter = "Ajouter";
+
+//Aide :
+$txt_aide_01 = "Maximum : 30 caractères avec dans l'ordre, le prénom puis le nom.";
+$txt_aide_02 = "Maximum : 5 caractères";
+$txt_aide_03 = "Maximum : 40 caractères";
+$txt_aide_04 = "Maximum : 20 caractères";
+$txt_aide_05 = "Maximum : 3 caractères";
+?>
