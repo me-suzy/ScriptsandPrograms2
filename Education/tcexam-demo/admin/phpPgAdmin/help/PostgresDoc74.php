cc7556a76aea928fd6e430a5cbc32ba105667442
me-suzy <me.suzana@gmail.com>
2025-08-12 13:39:39 +0300

    Add Education/tcexam-demo/admin/phpPgAdmin/help/PostgresDoc74.php

---
+++ Education/tcexam-demo/admin/phpPgAdmin/help/PostgresDoc74.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Help links for PostgreSQL 7.4 documentation
+ *
+ * $Id: PostgresDoc74.php,v 1.2.2.1 2004/12/02 02:28:54 chriskl Exp $
+ */
+ 
+include('./help/PostgresDoc73.php');
+
+$this->help_base = 'http://www.postgresql.org/docs/7.4/interactive/';
+
+$this->help_page['pg.aggregate.alter'] = 'sql-alteraggregate.html';
+
+?>
