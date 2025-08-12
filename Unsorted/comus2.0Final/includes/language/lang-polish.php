34d16112f880e71dbd866a838cb4566e1f8658d8
me-suzy <me.suzana@gmail.com>
2025-08-13 01:51:29 +0300

    Add Unsorted/comus2.0Final/includes/language/lang-polish.php

---
+++ Unsorted/comus2.0Final/includes/language/lang-polish.php
@@ -0,0 +1,263 @@
+<?
+
+define('GTGP_NO','Nie');
+define('GTGP_YES','Tak');
+define('GTGP_NO1','Brak');
+define('GTGP_YES1','Jest');
+define('GTGP_ADMIN_PAGE','Panel Administatora');
+define('GTGP_ADMIN_POST','Nowe Posty');
+define('GTGP_ADMIN_POST_OPIS','Tu mo¿esz zatwierdzaæ b±d¼ odrzucaæ zg³oszone galerie');
+define('GTGP_ADMIN_PREF','Partnerzy');
+define('GTGP_ADMIN_PREF_OPIS','Tu mo¿esz zarz±dzaæ Partnerami');
+define('GTGP_ADMIN_BL','Czarna Lista');
+define('GTGP_ADMIN_BL_OPIS','Tu mo¿esz zarz±dzaæ swoj± czarn± list±');
+define('GTGP_ADMIN_CAT','Kategorie');
+define('GTGP_ADMIN_CAT_OPIS','Tu mo¿esz dodawaæ lub usuwaæ kategorie');
+define('GTGP_ADMIN_CFG','Ustawienia');
+define('GTGP_ADMIN_CFG_OPIS','Ustawienia Gnat-TGPa');
+define('GTGP_ADMIN_TMPL','Szablony');
+define('GTGP_ADMIN_TMPL_OPIS','Tu mo¿esz zmieniæ ró¿ne szablony');
+define('GTGP_ADMIN_CU','Czyszczenie');
+define('GTGP_ADMIN_CU_OPIS','Tu mo¿esz sprawdzaæ niedzia³aj±ce linki');
+define('GTGP_ADMIN_VERS','Wersja Twojego skryptu');
+define('GTGP_ADMIN_CURR','Aktualna wersja skryptu');
+define('GTGP_ADMIN_POST_WAIT','Galerie oczekujace na akceptacje');
+define('GTGP_ADMIN_GAL_ADV','Zaawansowane ustawienia');
+define('GTGP_ADMIN_GAL_MKLNGBOX','Wybór jêzyka na stronie webmastera?');
+define('GTGP_ADMIN_GAL_POSTING','Przyjmowaæ nowe galerie?');
+define('GTGP_ADMIN_GAL_ADVCHECK','Sprawdzaæ galerie dok³adnie?');
+define('GTGP_ADMIN_GAL_MINPIC','Minimalna ilo¶æ zdjêæ');
+define('GTGP_ADMIN_GAL_MAXPIC','Maxymalna ilo¶æ zdjêæ');
+define('GTGP_ADMIN_GAL_MAXLINK','Maxymalna ilo¶æ obcych linków');
+define('GTGP_ADMIN_GAL_THUMBCHECK','Sprawdzaæ Thumbsy?');
+define('GTGP_ADMIN_GAL_THUMBW','Dozwolona szeroko¶æ Thumbsów');
+define('GTGP_ADMIN_GAL_THUMBH','Dozwolona wysoko¶æ Thumbsów');
+define('GTGP_SET_RETURN','Powrót do strony g³ównej');
+define('GTGP_SET_RETURN_C','Powrót');
+define('GTG_SETUP_UPDATED','Ustawienia zosta³y zapisane');
+define('GTGP_ADMIN_MYSQL','Ustawienia bazy Mysql');
+define('GTGP_ADMIN_MYSQL_HOST','MySql Host');
+define('GTGP_ADMIN_MYSQL_DB','Nazwa bazy Mysql');
+define('GTGP_ADMIN_MYSQL_USER','U¿ytkownik bazy Mysql');
+define('GTGP_ADMIN_MYSQL_PASS','Has³o bazy Mysql');
+define('GTGP_ADMIN_TGP','Ustawienia TGP');
+define('GTGP_ADMIN_TGP_EMAIL','Twój email');
+define('GTGP_ADMIN_TGP_URL','URL strony. (bez http://www. - np. <B>moja.strona.pl</B>)');
+define('GTGP_ADMIN_TGP_RURL','Link zwrotny (razem z http:// - np. <B>http://moja.strona.pl</B>');
+define('GTGP_ADMIN_TGP_OWNER','W³a¶ciciel strony (Xywka)');
+define('GTGP_ADMIN_TGP_LNG','Jêzyk');
+define('GTGP_ADMIN_TGP_BGCOL','Kolor t³a linków');
+define('GTGP_ADMIN_TGP_GALNUM','Ilo¶æ wy¶wietlanych galerii');
+define('GTGP_ADMIN_TGP_HMAIL','Wysy³aæ maile w htmlu?');
+define('GTGP_ADMIN_TGP_CHD','Zmieniaæ opis galeri? (Pierwsza litera wyrazu na du¿e)');
+define('GTGP_ADMIN_TGP_BL','U¿ywaæ blacklistê?');
+define('GTGP_ADMIN_TGP_CONFIRM','U¿ywaæ emaila potwierdzaj±cego?');
+define('GTGP_ADMIN_TGP_UEMAIL','Wysy³aæ mail o dodaniu/odrzuceniu galerii?');
+define('GTGP_ADMIN_TGP_RECIP','Wymagaæ linku zwrotnego?');
+define('GTGP_ADMIN_TGP_PARTNER','U¿ywaæ partnerów?');
+define('GTGP_ADMIN_TGP_POPUP','Zezwalaæ na popupy?');
+define('GTGP_ADMIN_TGP_JAVA','Zezwalaæ na JavaScript?');
+define('GTGP_ADMIN_TGP_FL','Zezwalaæ na migajace linki?');
+define('GTGP_ADMIN_TGP_IFRAME','Zezwalaæ na IFRAME?');
+define('GTGP_ADMIN_TGP_OBJECT','Zezwalaæ na OBJECT?');
+define('GTGP_ADMIN_TGP_DUP','Sprawdzaæ duplikaty?');
+define('GTGP_ADMIN_TGP_BANW','Sprawdzaæ banowane s³owa?');
+define('GTGP_ADMIN_TGP_BANWL','Lista zbanowanych s³ów (coma separated)');
+define('GTGP_ADMIN_TGP_GALINMAIN','Ile galerii na stronie glownej?');
+define('GTGP_ADMIN_TGP_LAP','Limit postów w Archiwum');
+define('GTGP_ADMIN_TGP_LDESC','Limit d³ugo¶ci opisu');
+define('GTGP_ADMIN_TGP_GALN','Limit dzienny normalnych galerii');
+define('GTGP_ADMIN_TGP_GALP','Limit dzienny galerii partnera');
+define('GTGP_ADMIN_TGP_USET','U¿ywasz skryptu do trejdu?');
+define('GTGP_ADMIN_TGP_TRB','Kod przed linkiem (np. <B>out.php?url=</B>)');
+define('GTGP_ADMIN_TGP_TRA','Kod za linkiem (np. <B>&link=gal&s=70&first=1</B>)');
+define('GTGP_ADMIN_TGP_MISC','Ustawienia ró¿ne');
+define('GTGP_ADMIN_TGP_DATE','Format daty - dla funkcji <B>date()</B>');
+define('GTGP_ADMIN_TGP_SUBMIT','Zapisz zmiany');
+define('GTGP_ADMIN_CAT_ADD','Dodaj kategoriê');
+define('GTGP_ADMIN_CAT_SUBMIT','Dodaj');
+define('GTGP_ADMIN_CAT_ALL','Wszystkie kategorie');
+define('GTGP_ADMIN_CAT_DELETE','Skasuj');
+define('GTGP_ADMIN_CAT_ADD_B','Kategoria');
+define('GTGP_ADMIN_CAT_ADD_A','zosta³a dodana');
+define('GTGP_ADMIN_PARTNER','Partnerzy');
+define('GTGP_ADMIN_PARTNER_EMAIL','Email partnera');
+define('GTGP_ADMIN_PARTNER_ADD','Dodaj partnera');
+define('GTGP_ADMIN_PARTNER_DEL','Skasuj');
+define('GTGP_ADMIN_PARTNER_ADD_B','Partner');
+define('GTGP_ADMIN_PARTNER_ADD_A','zosta³ dodany');
+define('GTGP_ADMIN_PARTNER_DEL_B','Partner');
+define('GTGP_ADMIN_PARTNER_DEL_A','zosta³ skasowany');
+define('GTGP_ADMIN_TMPL','Szablony');
+define('GTGP_ADMIN_TMPL_FILE','Plik');
+define('GTGP_ADMIN_TMPL_NEX','nie istnieje!');
+define('GTGP_ADMIN_TMPL_PMAIL','Mail do partnera');
+define('GTGP_ADMIN_TMPL_CMAIL','Mail potwierdzajacy');
+define('GTGP_ADMIN_TMPL_AMAIL','Mail o dodaniu galerii ');
+define('GTGP_ADMIN_TMPL_RMAIL','Mail o odrzuceniu galerii ');
+define('GTGP_ADMIN_TMPL_SUBMIT','Zapisz zmiany');
+define('GTGP_ADMIN_TMPL_HTML','Listing galerii');
+define('GTGP_ADMIN_BL','Czarna lista');
+define('GTGP_ADMIN_BL_ADD','Dodaj na BL');
+define('GTGP_ADMIN_BL_DEL','Usuñ');
+define('GTGP_ADMIN_BL_EMAIL','Email');
+define('GTGP_ADMIN_BL_DOMAIN','Domena');
+define('GTGP_ADMIN_BL_A','Email');
+define('GTGP_ADMIN_BL_B','zosta³ dodany na blacklistê');
+define('GTGP_ADMIN_BL_AD','Domena');
+define('GTGP_ADMIN_BL_BD','zosta³a dodana na blacklistê');
+define('GTGP_ADMIN_CL','Uwaga. Po klikniêciu na dan± galeriê w zale¿no¶ci od tego ile galerii posiadasz sprawdzenie wszystkich linków mo¿e trwaæ do¶æ d³ugo. Postaraj siê byæ cierpliwym...');
+define('GTGP_ADMIN_CL_CLICK','Kliknij na wybran± kategoriê');
+define('GTGP_ADMIN_CL_RES','Rezultat sprawdzania linków:');
+define('GTGP_ADMIN_CL_URL','Url');
+define('GTGP_ADMIN_CL_RURL','Zwrotny');
+define('GTGP_ADMIN_CL_BW','Z³e Wyrazy');
+define('GTGP_ADMIN_CL_POPUP','Popupy');
+define('GTGP_ADMIN_CL_JAVA','Java');
+define('GTGP_ADMIN_CL_FL','Migotki');
+define('GTGP_ADMIN_CL_IFRAME','Iframe');
+define('GTGP_ADMIN_CL_OBJECT','Object');
+define('GTGP_ADMIN_CL_DATE','Data');
+define('GTGP_ADMIN_CL_DEL','Kasuj');
+define('GTGP_ADMIN_CL_CLEAN','Czysta');
+define('GTGP_ADMIN_CL_JBW','Jest!');
+define('GTGP_ADMIN_CL_JP','Jest!');
+define('GTGP_ADMIN_CL_JJ','Jest!');
+define('GTGP_ADMIN_CL_JFL','Jest!');
+define('GTGP_ADMIN_CL_JIF','Jest!');
+define('GTGP_ADMIN_CL_DELP','Kasuj zaznaczone');
+define('GTGP_ADMIN_CL_DELETED','Posty zosta³y skasowane');
+define('GTGP_ADMIN_POST_URL','Url');
+define('GTGP_ADMIN_POST_ACC','Akceptacja');
+define('GTGP_ADMIN_POST_CAT','Kategoria');
+define('GTGP_ADMIN_POST_DES','Opis');
+define('GTGP_ADMIN_POST_REC','Link zwrotny');
+define('GTGP_ADMIN_POST_VOT','G³osy');
+define('GTGP_ADMIN_POST_DATE','Zg³oszono');
+define('GTGP_ADMIN_POST_SUBMITER','Zg³osi³');
+define('GTGP_ADMIN_POST_SUBMIT','Ok! Do dzie³a!');
+define('GTGP_ADMIN_POST_PIC','Ilo¶æ obrazów');
+define('GTGP_WM_LANG','Jêzyk');
+define('GTGP_WM_POSTING','Tymczasowo wstrzymali¶my przyjmowanie galerii.');
+define('GTGP_WM_POWERED','Stronê napêdza');
+define('GTGP_WM_NAME','Jak siê nazywasz?');
+define('GTGP_WM_EMAIL','Podaj swój email');
+define('GTGP_WM_URL','Adres Galerii');
+define('GTGP_WM_CAT','Kategoria');
+define('GTGP_WM_DES','Opis galerii');
+define('GTGP_WM_PAS','Has³o');
+define('GTGP_WM_PIC','Ilo¶æ obrazów');
+define('GTGP_WM_MAILME','Wy¶lij email o dodaniu/odrzuceniu galerii');
+define('GTGP_WM_SUBMIT','Wy¶lij zg³oszenie');
+define('GTGP_WM_CONTACT','Kontakt z webmasterem');
+define('GTGP_WM_ERROR_CAT','B³±d odczytu kategorii.');
+define('GTGP_WM_STOREUSER','Zapamietaj dane.');
+define('GTGP_WM_POSTID1','Twoja galeria otrzymala numer: <B>');
+define('GTGP_WM_POSTID2','</B>.<BR>');
+define('GTGP_WM_GALLIMIT','Przykro mi, ale mamy limit przyjmowanych galerii');
+define('GTGP_WM_GALLIMIT_N','dziennie dla galerników');
+define('GTGP_WM_GALLIMIT_P','dziennie dla partnerów');
+define('GTGP_WM_PARTNER_BPASS','Przykro mi, ale has³o partnera jest nieprawid³owe!');
+define('GTGP_POST_DU','Ten post jest ju¿ w naszej bazie!');
+define('GTGP_POST_PO','Zg³aszaj swoje galerie tylko raz!');
+define('GTGP_POST_BL','Niestety jeste¶ na Czarnej Li¶cie.');
+define('GTGP_POST_NF','Zg³aszana strona nie istnieje');
+define('GTGP_POST_BW','Przykro mi. U¿ywasz nie dozwolonego s³owa');
+define('GTGP_POST_POP','Nie zezwalamy na popupy na galeriach');
+define('GTGP_POST_JAVA','Nie zezwalamy na u¿ywanie JavaScriptu na galeriach');
+define('GTGP_POST_FL','Nie zezwalamy na u¿ywanie migajacych linków na galeriach');
+define('GTGP_POST_IFRAME','Nie zezwalamy na u¿ywanie IFRAME na galeriach');
+define('GTGP_POST_OBJECT','Nie zezwalamy na u¿ywanie taga OBJECT na galeriach');
+define('GTGP_POST_RU','Brak linku zwrotnego');
+define('GTGP_POST_RUU','Upewnij siê, ¿e na galerii masz link zwrotny do');
+define('GTGP_POST_TU','Dziêkujêmy za dodanie galerii do');
+define('GTGP_POST_DANE','Oto dane które poda³e¶');
+define('GTGP_POST_EMAIL','Email');
+define('GTGP_POST_URL','Url');
+define('GTGP_POST_CAT','Kategoria');
+define('GTGP_POST_DSC','Opis');
+define('GTGP_POST_NPIC','Ilo¶æ obrazów');
+define('GTGP_POST_NLINK','Ilo¶æ obcych linków');
+define('GTGP_POST_PARTNER','Jako, ¿e jeste¶ naszym partnerem Twoja galeria ju¿ jest listowana na naszej stronie');
+define('GTGP_POST_CONTACT','Je¿eli masz jak±¶ sprawê, pisz do mnie na email');
+define('GTGP_POST_CHECKING','Sprawdzam');
+define('GTGP_POST_MINPIC','Za ma³o obrazków!');
+define('GTGP_POST_MAXPIC','Za du¿o obrazków!');
+define('GTGP_POST_MAXLINK','Za du¿o reklam!');
+define('GTGP_POST_MINPIC_T','Dopuszczamy galerie na których jest przynajmniej');
+define('GTGP_POST_MAXPIC_T','Dopuszczamy galerie na których jest max');
+define('GTGP_POST_MAXLINK_T','Dopuszczamy galerie na których jest max');
+define('GTGP_POST_MAXLINK_P','linków poza obrazkami');
+define('GTGP_POST_MAXLINK_G','Na twojej galerii jest');
+define('GTGP_POST_PIC_P','obrazów');
+define('GTGP_POST_PIC_ERROR_READ','Nie mo¿na wczytaæ obrazka');
+define('GTGP_POST_PIC_GAL_PIC','Na zg³aszanej galerii jest');
+define('GTGP_POST_PIC_GAL_PIC_P','obrazów');
+define('GTGP_POST_PIC_GAL_FORM','Poda³e¶, ¿e jest');
+define('GTGP_POST_PIC_GAL_FORM_P','obrazów');
+define('GTGP_POST_PIC_GAL_CHANGE','Proszê popraw te dane!');
+define('GTGP_POST_PIC_OK','Ok!');
+define('GTGP_POST_PIC_WIDTH','Szeroko¶æ');
+define('GTGP_POST_PIC_HEIGHT','Wysoko¶æ');
+define('GTGP_POST_PIC_SIZE','Rozmiar');
+define('GTGP_POST_HTML_SIZE','Rozmiar');
+define('GTGP_POST_HTML_OK','Ok!');
+define('GTGP_POST_THUMB','Thumb');
+define('GTGP_POST_THUMB_OK','Ok!');
+define('GTGP_POST_THUMB_M','Niestety masz za ma³e Thumbnaile.');
+define('GTGP_POST_THUMB_M_B','Minimalne rozmiary jakie przyjmujemy to:');
+define('GTGP_POST_THUMB_D','Niestety masz za duze Thumbnaile.');
+define('GTGP_POST_THUMB_D_B','Maxymalne rozmiary jakie przyjmujemy to:');
+define('GTGP_POST_THUMB_ERROR','B³±d podczas odczytu wielko¶ci obrazu:');
+define('GTGP_ACC_TU','Dziêkuje za dodanie galerii do');
+define('GTGP_ACC_AC','Zg³oszenie zosta³o przyjête oraz potwierdzone');
+define('GTGP_ACC_RE','Twoja galeria zostanie sprawdzona ASAP ;-)');
+define('GTGP_POST_BACK','Powrót do formularza');
+define('GTGP_NAME','Gnat-TGP');
+define('GTGP_VERS','1.1.3');
+define('GTGP_UPDATE_DONE','Update zakoñczony!');
+define('GTGP_UPDATE_ERROR','Wyst±pi³ b³±d');
+define('GTGP_ACCESS','Generator zabezpieczenia');
+define('GTGP_USER','U¿ytkownik');
+define('GTGP_PASS','Has³o');
+define('GTGP_SUBMIT','Generuj');
+
+
+function tsl($co)
+{
+$tyes = GTGP_YES;
+$tno  = GTGP_NO;
+if($co == "Yes") { return $tyes;}
+if($co == "No")  { return $tno;}
+}
+
+function tslb($co)
+{
+$tyes = GTGP_YES1;
+$tno  = GTGP_NO1;
+if($co == "Yes") { return $tyes;}
+if($co == "No")  { return $tno;}
+}
+
+function tslbl($co)
+{
+$email  = GTGP_ADMIN_BL_EMAIL;
+$domain = GTGP_ADMIN_BL_DOMAIN;
+if($co == "email")  { return $email;}
+if($co == "domain") { return $domain;}
+}
+
+function printabout($ile)
+{
+	global $recip, $sitename;
+?>
+<TR>
+	<TD ALIGN="CENTER" COLSPAN="<? echo $ile ?>">
+		<? echo "<A HREF=\"http://gnat-tgp.mokre.com\" target=\"_blank\"><B>" . GTGP_NAME . "</B></A>&nbsp;" . GTGP_VERS . "<BR>" . GTGP_ADMIN_PAGE . "<BR>" . "<A HREF=\"". $recip ."\" TARGET=\"_BLANK\">". $sitename. "</A>"; ?>
+	</TD>
+</TR>
+<?
+}
+
+
+?>
