feaa5fb69fe2682beb931183bf92b1a0e4e63419
me-suzy <me.suzana@gmail.com>
2025-08-13 01:47:28 +0300

    Add Unsorted/cms master/cms-admin/templates/header.php

---
+++ Unsorted/cms master/cms-admin/templates/header.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title><?php echo $Lang->title ?></title>
+<link rel=StyleSheet href="style.css" type="text/css">
+</head>
+
+<body bgcolor=#ffffff text=#000000 link=#c80000 vlink=#c80000 alink=#c0c0c0 leftmargin=20 topmargin=20 marginwidth=20 marginheight=20>
+
+<center>
+<table width=700 cellpadding=0 cellspacing=0 border=0>
+<tr>
+<td width=200>
+<img src=images/logo.gif>
+</td>
+<td width=500 align=right valign=bottom>
+
+<!-- START MENU 1 -->
+<a href=pages.php><?php echo $Lang->menu_pages ?></a> | 
+<a href=website.php><?php echo $Lang->menu_website ?></a> | 
+<a href=images.php><?php echo $Lang->menu_images ?></a> | 
+<a href=files.php><?php echo $Lang->menu_files ?></a> | 
+<a href=modules.php><?php echo $Lang->menu_modules ?></a> | 
+<a href=system.php><?php echo $Lang->menu_system ?></a> | 
+<a href=logout.php><?php echo $Lang->menu_logout ?></a>
+<!-- END MENU 1 -->
+
+</td>
+</tr>
+<tr><td colspan=2><img src=images/trans.gif width=700 height=1 border=0></td></tr>
+</table>
+</center>
+
+<center>
+<table width=700 cellpadding=0 cellspacing=0 border=0>
+<tr><td width=700 bgcolor=#c80000><img src=images/trans.gif width=700 height=16 border=0></td></tr>
+<tr><td width=700 bgcolor=#363636><img src=images/trans.gif width=700 height=38 border=0></td></tr>
+<tr><td width=700><img src=images/trans.gif width=700 height=1 border=0></td></tr>
+</table>
+</center>
