1e801423bbf0f2db0633da71c2aa024510f107ca
me-suzy <me.suzana@gmail.com>
2025-08-12 13:49:15 +0300

    Add Email Systems/DSNewsletter/header.php

---
+++ Email Systems/DSNewsletter/header.php
@@ -0,0 +1,8 @@
+<?php
+include("config.php");
+?>
+<table width="100%" cellspacing=0 border=0 cellpadding=0>
+<tr bgcolor="#679767" height="75"><td>
+<font color="black" size="7"><b><center><? echo "$name"; ?></center></b></font>
+</td></tr>
+</table>
