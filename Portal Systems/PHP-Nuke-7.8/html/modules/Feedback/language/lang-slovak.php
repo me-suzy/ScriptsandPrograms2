ab28ed960d1ebfe510740bd1f1dffbf1ed08f408
me-suzy <me.suzana@gmail.com>
2025-08-12 22:42:27 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Feedback/language/lang-slovak.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Feedback/language/lang-slovak.php
@@ -0,0 +1,49 @@
+<?php
+
+/*************************************************************************/
+/* The Slovak language file for module Feedback                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/*************************************************************************/
+/* The Slovak language file for module Feedback                      */
+/* Author: AlRashid 			 Date: 09.09.2002   					 */
+/* alrashid@klokan.sk, http://alrashid.klokan.sk                         */
+/* Pripadne chyby mi prosim oznamte na alrashid@klokan.sk			     */
+/*  		Dakujem                                                      */
+/*************************************************************************/
+/**************************************************************************/
+
+define("_SEND","Posla");
+define("_YOURNAME","Vae meno");
+define("_MESSAGE","Odkaz");
+define("_YOUREMAIL","Vá e-mail");
+define("_FEEDBACKNOTE","Vae komentáre, návrhy a pripomienky k tejto stránke sú vítané a ve¾mi prospené. Ïakujeme.");
+define("_FEEDBACKTITLE","Formulár");
+define("_FEEDBACK","Napíte nám");
+define("_FBENTERNAME","Napíte Vae meno!");
+define("_FBENTEREMAIL","Napíte korektne Vau e-mailovú adresu!");
+define("_FBENTERMESSAGE","Napíte odkaz!");
+define("_SENDEREMAIL","Email odosielatela");
+define("_SENDERNAME","Meno odosielatela");
+define("_FBMAILSENT","Odkaz bol poslaný!");
+define("_FBTHANKSFORCONTACT","Ïakujeme, e ste nás kontaktovali.");
+
+?>
