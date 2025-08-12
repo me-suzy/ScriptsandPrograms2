76ed95de1497d533989dadb9ab73afd2a22fddc9
me-suzy <me.suzana@gmail.com>
2025-08-12 17:33:45 +0300

    Add Guestbooks/guestbook/guestbook/language/lang_english.php

---
+++ Guestbooks/guestbook/guestbook/language/lang_english.php
@@ -0,0 +1,84 @@
+<?php
+/****************************************************************/
+/*                       phphg Guestbook                        */
+/*                      lang_en.php file                        */
+/*                      (c)copyright 2003                       */
+/*                       By hinton design                       */
+/*                 http://www.hintondesign.org                  */
+/*                   support@hintondesign.org                   */
+/*                                                              */
+/* This program is free software. You can redistrabute it and/or*/
+/* modify it under the terms of the GNU General Public Licence  */
+/* as published by the Free Software Foundation; either version */
+/* 2 of the license.                                            */
+/*                                                              */
+/****************************************************************/
+// index.php language variables
+$lang['name'] = 'Name';
+$lang['comments'] = 'Comments';
+$lang['view'] = 'View Guestbook';
+$lang['sign'] = 'Sign Guestbook';
+$lang['admin'] = 'Admin Login';
+$lang['posted'] = 'Posted By:';
+$lang['location'] = 'Location:';
+$lang['date'] = 'Date Posted:';
+$lang['error'] = 'Error';
+$lang['success'] = 'Success';
+
+// sign.php language variables
+$lang['smilies'] = 'Smilies';
+$lang['info'] = 'Info';
+$lang['email'] = 'Email Address:';
+$lang['website'] = 'Website:';
+$lang['message'] = 'Message:';
+$lang['names'] = 'Name:';
+$lang['loc'] = 'Location:';
+
+// Admin Login language variables
+$lang['username'] = 'Username:';
+$lang['password'] = 'Password:';
+$lang['login'] = 'Admin Login';
+$lang['adlogout'] = 'Admin Logout';
+
+// Admin CP language variables
+$lang['navigation'] = 'Navigation';
+$lang['cp'] = 'Admin CP';
+$lang['wel_message'] = "Welcome to the admin CP " .$_COOKIE['username'];
+$lang['home'] = 'Admin Home';
+$lang['edit_message'] = 'Edit/Delete Messages';
+$lang['ban'] = 'Ban IP';
+$lang['edit_smile'] = 'Add/Delete Smilies';
+$lang['edit_word'] = 'Add/Delete Word Filter';
+$lang['logout'] = 'Logout';
+$lang['action'] = 'Action';
+$lang['browser'] = 'Users Browser:';
+$lang['ip'] = 'Users IP:';
+$lang['smilie_name'] = 'Smilie Name';
+$lang['file_name'] = 'Image Name';
+$lang['smilie_code'] = 'Smilie Code';
+$lang['add_smilie'] = 'Add Smilies';
+$lang['edit_smilies'] = 'Edit Smilie';
+$lang['settings'] = 'Settings';
+$lang['word'] = 'Word';
+$lang['replace'] = 'Replace Word';
+$lang['add_word'] = 'Add Word';
+$lang['edit_filter'] = 'Edit Word Filter';
+$lang['edit_settings'] = 'Edit Settings';
+$lang['title'] = 'Site Title';
+$lang['domain'] = 'Domain URL';
+$lang['script_path'] = 'Script Path';
+$lang['board_email'] = 'Board Email Address';
+$lang['limit'] = 'Limit value for how many entries to display on main page.';
+$lang['lang'] = 'Default Language';
+$lang['theme'] = 'Default Theme';
+$lang['langs'] = 'Edit/Add Languages';
+$lang['themes'] = 'Edit/Add Themes';
+$lang['lang_name'] = 'Language Name';
+$lang['add_lang'] = 'Add Language';
+$lang['edit_lang'] = 'Edit Language';
+$lang['theme_name'] = 'Theme Name';
+$lang['add_theme'] = 'Add Theme';
+$lang['edit_theme'] = 'Edit Theme';
+
+// That's all folks
+?>
