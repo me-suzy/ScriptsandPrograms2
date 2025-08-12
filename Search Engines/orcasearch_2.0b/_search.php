7e4ca74ae2cad564e48e8173dab06aca8b3f7e92
me-suzy <me.suzana@gmail.com>
2025-08-12 23:38:24 +0300

    Add Search Engines/orcasearch_2.0b/_search.php

---
+++ Search Engines/orcasearch_2.0b/_search.php
@@ -0,0 +1,35 @@
+<?php 
+
+include "os2/config.php";
+include "os2/head.php";
+
+?><html>
+<head>
+  <title>Orca Search <?php echo $dData['version']; ?></title>
+
+  <link rel="stylesheet" type="text/css" href="os2/body.xhtml.css" />
+
+  <!--
+    It is *strongly* recommended you change the charset value of this
+    meta element to match the majority of pages you index.
+  -->
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+
+  <style type="text/css">
+
+body {
+  background-color:#ffffff;
+  font:normal 100% Arial,sans-serif;
+}
+
+  </style>
+</head>
+
+<body>
+
+  <h1>Orca Search <?php echo $dData['version']; ?></h1>
+
+  <?php include "os2/body.xhtml.php"; ?>
+
+</body>
+</html>
