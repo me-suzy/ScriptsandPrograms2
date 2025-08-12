d2bb9a3d8b29288074b696eb9243c774e471885e
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:03 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/bulletinboard.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/bulletinboard.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342665;
+$_objchilds[] = '_child_bulletinboard_342666';
+function objfile_342665 () {
+$obj = owNew('document');
+$objdata['name'] = "Bulletin board";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
