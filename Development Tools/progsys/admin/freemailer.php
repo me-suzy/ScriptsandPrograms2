14c471439f6fe61645abb3bf45444084f19d76c2
me-suzy <me.suzana@gmail.com>
2025-08-12 10:09:34 +0300

    Add Development Tools/progsys/admin/freemailer.php

---
+++ Development Tools/progsys/admin/freemailer.php
@@ -0,0 +1,255 @@
+<?php
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+/***************************************************************************
+ * Created by: Boesch IT-Consulting (info@boesch-it.de)
+ * (c)2002-2005 Boesch IT-Consulting
+ * *************************************************************************/
+require('../config.php');
+require('./auth.php');
+if(!isset($lang) || !$lang)
+	$lang=$admin_lang;
+include('./language/lang_'.$lang.'.php');
+$page_title=$l_freemailerlist;
+require('./heading.php');
+$sql = "select * from ".$tableprefix."_layout where (layoutnr=1)";
+if(!$result = mysql_query($sql, $db)) {
+    die("Could not connect to the database.");
+}
+?>
+<table align="center" width="80%" CELLPADDING="1" CELLSPACING="0" border="0" valign="top">
+<tr><TD BGCOLOR="#000000">
+<?php
+if($admin_rights < 2)
+{
+	echo "<tr class=\"errorrow\"><td align=\"center\">";
+	die("$l_functionnotallowed");
+}
+if(isset($mode))
+{
+	if($mode=="display")
+	{
+		if($admin_rights < 1)
+		{
+			echo "<tr class=\"errorrow\"><td align=\"center\">";
+			die("$l_functionnotallowed");
+		}
+?>
+<table align="center" width="100%" border="0" cellspacing="1" cellpadding="1">
+<?php
+		$sql = "select * from ".$tableprefix."_freemailer where (entrynr=$input_entrynr)";
+		if(!$result = mysql_query($sql, $db))
+		    die("<tr class=\"errorrow\"><td>Could not connect to the database.");
+		if (!$myrow = mysql_fetch_array($result))
+			die("<tr class=\"errorrow\"><td>no such entry");
+?>
+<tr class="displayrow"><td align="right" width="30%"><?php echo $l_freemaileradr?>:</td><td><?php echo htmlentities($myrow["address"])?></td></tr>
+</table></td></tr></table>
+<?php
+		echo "<div class=\"bottombox\" align=\"center\"><a href=\"".do_url_session("$act_script_url?lang=$lang")."\">$l_freemailerlist</a></div>";
+	}
+	// Page called with some special mode
+	if($mode=="newadr")
+	{
+		if($admin_rights < 3)
+		{
+			echo "<tr class=\"errorrow\"><td align=\"center\">";
+			die("$l_functionnotallowed");
+		}
+?>
+<table align="center" width="100%" border="0" cellspacing="1" cellpadding="1">
+<tr class="headingrow"><td align="center" colspan="2"><b><?php echo $l_newfreemailer?></b></td></tr>
+<form method="post" action="<?php echo $act_script_url?>"><input type="hidden" name="lang" value="<?php echo $lang?>">
+<?php
+	if($sessid_url)
+		echo "<input type=\"hidden\" name=\"$sesscookiename\" value=\"$url_sessid\">";
+?>
+<tr class="inputrow"><td align="right" width="30%"><?php echo $l_freemaileradr?>:</td><td><input class="psysinput" type="text" name="address" size="30" maxlength="100"></td></tr>
+<tr class="actionrow"><td align="center" colspan="2"><input type="hidden" name="mode" value="add"><input class="psysbutton" type="submit" value="<?php echo $l_add?>"></td></tr>
+</form>
+</table></td></tr></table>
+<div class="bottombox" align="center"><a href="<?php echo do_url_session("$act_script_url?lang=$lang")?>"><?php echo $l_freemailerlist?></a></div>
+<?php
+	}
+	if($mode=="add")
+	{
+		if($admin_rights < 3)
+		{
+			echo "<tr class=\"errorrow\"><td align=\"center\">";
+			die("$l_functionnotallowed");
+		}
+?>
+<table align="center" width="100%" border="0" cellspacing="1" cellpadding="1">
+<?php
+		$errors=0;
+		if(!$address)
+		{
+			echo "<tr class=\"errorrow\" align=\"center\"><td>";
+			echo "$l_noaddress</td></tr>";
+			$errors=1;
+		}
+		if($errors==0)
+		{
+			$sql = "INSERT INTO ".$tableprefix."_freemailer (address) ";
+			$sql .="VALUES ('$address')";
+			if(!$result = mysql_query($sql, $db))
+			    die("<tr class=\"errorrow\"><td>Unable to add address to database.");
+			echo "<tr class=\"displayrow\" align=\"center\"><td>";
+			echo "$l_adradded";
+			echo "</td></tr></table></td></tr></table>";
+			echo "<div class=\"bottombox\" align=\"center\"><a href=\"".do_url_session("$act_script_url?mode=newadr&lang=$lang")."\">$l_newfreemailer</a></div>";
+			echo "<div class=\"bottombox\" align=\"center\"><a href=\"".do_url_session("$act_script_url?lang=$lang")."\">$l_freemailerlist</a></div>";
+		}
+		else
+		{
+			echo "<tr class=\"actionrow\" align=\"center\"><td>";
+			echo "<a href=\"javascript:history.back()\">$l_back</a>";
+			echo "</td></tr></table></td></tr></table>";
+		}
+	}
+	if($mode=="delete")
+	{
+		if($admin_rights < 3)
+		{
+			echo "<tr class=\"errorrow\"><td align=\"center\">";
+			die("$l_functionnotallowed");
+		}
+?>
+<table align="center" width="100%" border="0" cellspacing="1" cellpadding="1">
+<?php
+		$deleteSQL = "delete from ".$tableprefix."_freemailer where (entrynr=$input_entrynr)";
+		$success = mysql_query($deleteSQL);
+		if (!$success)
+			die("<tr class=\"errorrow\"><td>$l_cantdelete.");
+		echo "<tr class=\"displayrow\" align=\"center\"><td>";
+		echo "$l_deleted<br>";
+		echo "</td></tr></table></td></tr></table>";
+		echo "<div class=\"bottombox\" align=\"center\"><a href=\"".do_url_session("$act_script_url?lang=$lang")."\">$l_freemailerlist</a></div>";
+	}
+	if($mode=="edit")
+	{
+		if($admin_rights < 3)
+		{
+			echo "<tr class=\"errorrow\"><td align=\"center\">";
+			die("$l_functionnotallowed");
+		}
+?>
+<table align="center" width="100%" border="0" cellspacing="1" cellpadding="1">
+<?php
+		$sql = "select * from ".$tableprefix."_freemailer where (entrynr=$input_entrynr)";
+		if(!$result = mysql_query($sql, $db))
+		    die("<tr bgcolor=\"#cccccc\"><td>Could not connect to the database.");
+		if (!$myrow = mysql_fetch_array($result))
+			die("<tr bgcolor=\"#cccccc\"><td>no such entry");
+?>
+<tr class="headingrow"><td align="center" colspan="2"><b><?php echo $l_editfreemailer?></b></td></tr>
+<form method="post" action="<?php echo $act_script_url?>"><input type="hidden" name="lang" value="<?php echo $lang?>">
+<?php
+	if($sessid_url)
+		echo "<input type=\"hidden\" name=\"$sesscookiename\" value=\"$url_sessid\">";
+?>
+<input type="hidden" name="input_entrynr" value="<?php echo $myrow["entrynr"]?>">
+<tr class="inputrow"><td align="right" width="30%"><?php echo $l_freemaileradr?>:</td><td><input class="psysinput" type="text" name="address" size="30" maxlength="100" value="<?php echo htmlentities($myrow["address"])?>"></td></tr>
+<tr class="actionrow"><td align="center" colspan="2"><input type="hidden" name="mode" value="update"><input class="psysbutton" type="submit" value="<?php echo $l_update?>"></td></tr>
+</form>
+</table></td></tr></table>
+<div class="bottombox" align="center"><a href="<?php echo do_url_session("$act_script_url?lang=$lang")?>"><?php echo $l_freemailerlist?></a></div>
+<?php
+	}
+	if($mode=="update")
+	{
+		if($admin_rights < 3)
+		{
+			echo "<tr class=\"errorrow\"><td align=\"center\">";
+			die("$l_functionnotallowed");
+		}
+?>
+<table align="center" width="100%" border="0" cellspacing="1" cellpadding="1">
+<?php
+		$errors=0;
+		if(!$address)
+		{
+			echo "<tr class=\"errorrow\" align=\"center\"><td>";
+			echo "$l_noaddress</td></tr>";
+			$errors=1;
+		}
+		if($errors==0)
+		{
+			$sql = "UPDATE ".$tableprefix."_freemailer SET address='$address' ";
+			$sql .="WHERE (entrynr = $input_entrynr)";
+			if(!$result = mysql_query($sql, $db))
+			    die("<tr class=\"errorrow\"><td>Unable to update the database.");
+			echo "<tr class=\"displayrow\" align=\"center\"><td>";
+			echo "$l_adrupdated";
+			echo "</td></tr></table></td></tr></table>";
+			echo "<div class=\"bottombox\" align=\"center\"><a href=\"".do_url_session("$act_script_url?lang=$lang")."\">$l_freemailerlist</a></div>";
+		}
+		else
+		{
+			echo "<tr class=\"actionrow\" align=\"center\"><td>";
+			echo "<a href=\"javascript:history.back()\">$l_back</a>";
+			echo "</td></tr></table></td></tr></table>";
+		}
+	}
+}
+else
+{
+?>
+<table align="center" width="100%" border="0" cellspacing="1" cellpadding="1">
+<?php
+$sql = "select * from ".$tableprefix."_freemailer order by address";
+if(!$result = mysql_query($sql, $db)) {
+    die("Could not connect to the database.");
+}
+if (!$myrow = mysql_fetch_array($result))
+{
+	echo "<tr bgcolor=\"#c0c0c0\"><td align=\"center\">";
+	echo $l_noentries;
+	echo "</td></tr></table></td></tr></table>";
+}
+else
+{
+?>
+<tr class="rowheadings">
+<td align="center" width="5%"><b>#</b></td>
+<td align="center" width="85%"><b><?php echo $l_freemaileradr?></b></td>
+<td width="10%">&nbsp;</td></tr>
+<?php
+		do {
+		$act_id=$myrow["entrynr"];
+		echo "<tr class=\"displayrow\">";
+		echo "<td align=\"right\">".$myrow["entrynr"]."</td>";
+		echo "<td>".htmlentities($myrow["address"])."</td>";
+		echo "<td>";
+		if($admin_rights > 2)
+		{
+			$dellink=do_url_session("$act_script_url?mode=delete&input_entrynr=$act_id&lang=$lang");
+			if($admdelconfirm==1)
+				echo "<a class=\"listlink\" href=\"javascript:confirmDel('#$act_id','$dellink')\">";
+			else
+				echo "<a class=\"listlink\" href=\"$dellink\" valign=\"top\">";
+			echo "<img src=\"gfx/delete.gif\" border=\"0\" title=\"$l_delete\" alt=\"$l_delete\"></a> ";
+			echo "<a class=\"listlink\" href=\"".do_url_session("$act_script_url?mode=edit&lang=$lang&input_entrynr=$act_id")."\">";
+			echo "<img src=\"gfx/edit.gif\" border=\"0\" title=\"$l_edit\" alt=\"$l_edit\"></a> ";
+		}
+		echo "<a class=\"listlink\" href=\"".do_url_session("$act_script_url?mode=display&input_entrynr=$act_id&lang=$lang")."\">";
+		echo "<img src=\"gfx/view.gif\" border=\"0\" title=\"$l_display\" alt=\"$l_display\"></a>";
+		echo "</td></tr>";
+   } while($myrow = mysql_fetch_array($result));
+   echo "</table></tr></td></table>";
+}
+if($admin_rights > 2)
+{
+?>
+<div class="bottombox" align="center"><a href="<?php echo do_url_session("$act_script_url?mode=newadr&lang=$lang")?>"><?php echo $l_newfreemailer?></a></div>
+<?php
+}
+}
+include('trailer.php');
+?>
