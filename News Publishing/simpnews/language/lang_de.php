a01fc9c92bb1e15bca0804265e51abc1d860239c
me-suzy <me.suzana@gmail.com>
2025-08-12 20:48:07 +0300

    Add News Publishing/simpnews/language/lang_de.php

---
+++ News Publishing/simpnews/language/lang_de.php
@@ -0,0 +1,316 @@
+<?php
+/***************************************************************************
+ * (c)2002-2004 Boesch IT-Consulting (info@boesch-it.de)
+ ***************************************************************************/
+$l_poster = "Verfasser";
+$l_functiondisabled = "Funktion nicht verf&uuml;gbar";
+$l_subscriptionprelude = "Abonnieren der News";
+$l_email = "E-Mailadresse";
+$l_nofreemailer = "(keine Adressen bei Freemailern)";
+$l_subscribe = "abonnieren";
+$l_noemail = "Sie m&uuml;ssen eine E-Mailadresse angeben";
+$l_back = "zur&uuml;ck";
+$l_novalidemail = "Bitte eine g&uuml;ltige E-Mailadresse angeben";
+$l_forbidden_freemailer = "Eine E-Mailadresse bei diesem Freemailer ist f&uuml;r Abonemments nicht zul&auml;ssig";
+$l_hours = "Stunden";
+$l_subscriptionconfirmmail = "Hallo,\nSie haben sich beim Aboservice von {sitename} angemeldet.\n
+Um sicher zu stellen, dass Sie sich auch selbst angemeldet haben, haben wir diesen Bestaetigungsmechanismus aktiviert.\n
+Um das Abo zu aktivieren, besuchen sie bitte innerhalb von {confirmtime} folgende URL:\n{confirmurl}\n
+Sollten Sie die Anmeldung nicht aktivieren wollen, so brauchen Sie nichts zu tun.";
+$l_subscriptionconfirmmail_html = "<font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"2\">Hallo,\nSie haben sich beim Aboservice von {sitename} angemeldet.\n
+Um sicher zu stellen, dass Sie sich auch selbst angemeldet haben, haben wir diesen Best&auml;tigungsmechanismus aktiviert.\n
+Um das Abo zu aktivieren, besuchen sie bitte innerhalb von <b>{confirmtime}</b> folgende URL:\n<a href=\"{confirmurl}\"><font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"1\">{confirmurl}</font></a>\n
+Sollten Sie die Anmeldung nicht aktivieren wollen, so brauchen Sie nichts zu tun.</font>";
+$l_subscriptionconfirmsubject = "Aboservice ({sitename}) - Anmeldebestaetigung";
+$l_subscriptiondone = "Danke, dass Sie unsere News abonniert haben.";
+$l_allready_subscribed = "Ein Abonnement f&uuml;r diese E-Mailadresse besteht schon.";
+$l_allready_pending = "Eine offene Abonnementanforderung f&uuml;r diese E-Mailadresse besteht schon.<br>Warten Sie bitte die Best&auml;tigungsanforderung ab und best&auml;tigen Sie dann Ihr Abonnement.";
+$l_missingemail = "Keine Emailadresse &uuml;bergeben";
+$l_missingid = "Keine ID &uuml;bergeben";
+$l_noconfirmentry = "Kein entsprechender Abonnenteneintrag zur Best&auml;tigung gefunden.<br>Wahrscheinlich ist die maximale Wartezeit abgelaufen<br>oder Sie haben die Anforderung schon best&auml;tigt.";
+$l_subscriptionconfirmed = "Ihr Abonnement f&uuml;r die News ist nun aktiviert.";
+$l_noremoveentry = "Kein passender Abonnenteneintrag zum Entfernen gefunden.";
+$l_unsubscribed = "Abonnement entfernt.";
+$l_emailtype = "Format der E-Mail";
+$l_htmlmail = "HTML";
+$l_ascmail = "nur Text";
+$l_subscriptionremoveprelude = "Wollen Sie das Abonnement f&uuml;r {email} wirklich l&ouml;schen?";
+$l_yes = "Ja";
+$l_subscribe = "News per E-Mail abonnieren";
+$l_ok = "Ok";
+$l_unsubscribe = "Abonnement l&ouml;schen";
+$l_unsubscriptionconfirmmail = "Hallo,\nSie haben angefordert sich beim Aboservice von {sitename} abzumelden.\n
+Um sicher zu stellen, dass Sie sich auch selbst abmelden wollen, haben wir diesen Bestaetigungsmechanismus aktiviert.\n
+Um sich endgueltig abzumelden, besuchen sie bitte folgende URL:\n{confirmurl}\n
+Sollten Sie die Abmeldung nicht vornehmen wollen, so brauchen Sie nichts zu tun.";
+$l_unsubscriptionconfirmmail_html = "<font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"2\">Hallo,\nSie haben angefordert sich beim Aboservice von {sitename} abzumelden.\n
+Um sicher zu stellen, dass Sie sich auch selbst abmelden wollen, haben wir diesen Best&auml;tigungsmechanismus aktiviert.\n
+Um sich endg&uuml;ltig abzumelden, besuchen sie bitte folgende URL:\n<a href=\"{confirmurl}\"><font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"1\">{confirmurl}</font></a>\n
+Sollten Sie die Abmeldung nicht vornehmen wollen, so brauchen Sie nichts zu tun.</font>";
+$l_unsubscriptionconfirmsubject = "Aboservice ({sitename}) - Abmeldebestaetigung";
+$l_unsubscribesent = "Eine E-Mail mit dem Best&auml;tigungslink f&uuml;r die Abmeldung wurde verschickt.<br>Bitte befolgen Sie die Anweisungen in der E-Mail, um sich endg&uuml;ltig abzumelden.";
+$l_page = "Seite";
+$l_entries = "Eintr&auml;ge";
+$l_of = "von";
+$l_writecomment = "Kommentar schreiben";
+$l_name = "Name";
+$l_comment = "Kommentar";
+$l_noname = "Sie m&uuml;ssen Ihren Namen angeben";
+$l_nocomment = "Sie m&uuml;ssen einen Kommentar eingeben";
+$l_callingerror = "Aufruffehler";
+$l_commentposted = "Kommentar gespeichert";
+$l_news = "News";
+$l_comments = "Kommentare";
+$l_timeonserver = "aktuelle Uhrzeit am Server";
+$l_allnews = "alle News";
+$l_search = "Suche";
+$l_dosearch = "suchen";
+$l_result = "Ergebnis";
+$l_page_forward = "n&auml;chste Seite";
+$l_page_back = "Seite zur&uuml;ck";
+$l_page_last = "letzte Seite";
+$l_page_first = "erste Seite";
+$l_gotop = "zum Seitenanfang";
+$l_newnews = "neue News";
+$l_nonewnews = "keine neuen News";
+$l_noentriesfound = "keine passenden Eintr&auml;ge vorhanden";
+$l_help = "Hilfe";
+$l_attachement = "Dateianhang herunterladen";
+$l_weekday = array("Sonntag","Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag");
+$l_monthname = array("Januar","Februar","M&auml;rz","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember");
+$l_prevmonth = "Monat zur&uuml;ck";
+$l_nextmonth = "n&auml;chster Monat";
+$l_newsforthisdate = "News f&uuml;r dieses Datum";
+$l_noentries = "keine Eintr&auml;ge";
+$l_nosuchentry = "kein solcher Eintrag vorhanden";
+$l_noeventstoday = "keine Events f&uuml;r den heutigen Tag";
+$l_new = "neu";
+$l_more = "weiter";
+$l_eventsfor = "Events vom";
+$l_redirected = "Sie werden in K&uuml;rze zu den News weitergeleitet.";
+$l_text = "Text";
+$l_betweendate = "im Zeitrahmen";
+$l_startdate = "Anfangsdatum";
+$l_enddate = "Enddatum";
+$l_dateselformat = array("day","month","year");
+$l_day = "Tag";
+$l_month = "Monat";
+$l_year = "Jahr";
+$l_general = "Allgemeines";
+$l_morenews = "weitere News";
+$l_events = "Events";
+$l_showevents = "Events anzeigen";
+$l_showcategory = "Nur diese Kategorie anzeigen";
+$l_newslist = "Liste der News";
+$l_showentry = "Eintrag anzeigen";
+$l_temp_unavail = "Dienst momentan nicht verfügbar";
+$l_catlist = "Kategorien";
+$l_content = "Inhalt";
+$l_generated_with = "Powered by";
+$l_propose_news = "Newseintrag vorschlagen";
+$l_heading = "&Uuml;berschrift";
+$l_notext = "Sie m&uuml;ssen einen Text eingeben";
+$l_noheading = "Sie m&uuml;ssen eine &Uuml;berschrift eingeben";
+$l_proposeposted = "Ihr Vorschlag wurde gespeichert.";
+$l_jumpto = "Gehe zu";
+$l_noevents = "keine Events";
+$l_propose_event = "Event vorschlagen";
+$l_date = "Datum";
+$l_novaliddate = "Der {day}.{month}.{year} ist kein g&uuml;ltiges Datum";
+$l_novaliddate2 = "Bitte g&uuml;ltiges Datum angeben.";
+$l_redirected_events = "Sie werden in K&uuml;rze zu den Events weitergeleitet.";
+$l_pastdate = "Datum kann nicht in der Vergangenheit liegen";
+$l_all_cats = "alle Kategorien";
+$l_category = "Kategorie";
+$l_notsubj = "Neuer Vorschlag ({ptype}, {sitename})";
+$l_notmsg = "Es wurder ein neuer Vorschlag in {ptype},{category} gepostet";
+$l_print = "Druckversion";
+$l_withattachement = "Dateianhang";
+$l_eventsforthisdate = "Events f&uuml;r dieses Datum";
+$l_storeuserdata = "Benutzerdaten f&uuml;r sp&auml;tere Wiederverwendung speichern";
+$l_goto_actdate = "zum aktuellen Datum gehen";
+$l_csvexport = "Export als CSV";
+$l_language = "Sprache";
+$l_asclist = "Liste als ASCII";
+$l_editownproposals = "eigene Vorschl&auml;ge bearbeiten";
+$l_proposed_news = "vorgeschlagene Newseintr&auml;ge";
+$l_proposelistprelude = "Bitte geben Sie die E-Mailadresse an, die Sie beim Erfassen des Vorschlages verwendet haben.";
+$l_back2news = "Zur&uuml;ck zu den News";
+$l_transferrednews = "bereits in die News &uuml;bernommene Vorschl&auml;ge";
+$l_edit = "bearbeiten";
+$l_delete = "l&ouml;schen";
+$l_proposals = "Vorschl&auml;ge";
+$l_password = "Passwort";
+$l_requestpw = "Passwort anfordern";
+$l_nopassword = "Sie m&uuml;ssen ein Passwort angeben";
+$l_requestpwprelude = "Passwort f&uuml;r {email} anfordern";
+$l_passwordconfirm = "Passwortwiederholung";
+$l_passwordmismatch = "Passw&ouml;rter stimmen nicht &uuml;berein";
+$l_noentryforemail = "Kein Eintrag f&uuml;r diese E-Mailadresse vorhanden";
+$l_pwconfirmmail = "Hallo,\nSie haben auf {sitename} ein Passwort zur Verwaltung Ihrer vorgeschlagenen News/Events beantragt.
+Um dieses Passwort zu aktivieren besuchen Sie bitte folgenden Link: {confirmurl}";
+$l_pwconfirmmail_html = "<font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"2\">Hallo,\nSie haben auf {sitename} ein Passwort zur Verwaltung Ihrer vorgeschlagenen News/Events beantragt.
+Um dieses Passwort zu aktivieren besuchen Sie bitte folgenden Link:</font> <a href=\"{confirmurl}\"><font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"1\">{confirmurl}</font></a>";
+$l_pwconfirmsubject = "Passwortanforderung ({sitename})";
+$l_proppw = "Passwort zur Verwaltung vorgeschlagener News/Events";
+$l_pwrequested = "Passwortanforderung wurde durchgef&uuml;hrt.<br>Eine E-Mail wurde an {email} versand, die Anweisungen zur Aktivierung des Passwortes enth&auml;lt.";
+$l_pwallreadydefined = "Es ist schon ein Passwort f&uuml;r diese E-Mailadresse vergeben";
+$l_pwconfirmed = "Passwort best&auml;tigt und aktiviert";
+$l_erroronlogin = "Fehler bei der Anmeldung (Email/Passwort falsch)";
+$l_loggedinas = "Angemeldet als";
+$l_logout = "abmelden";
+$l_logged_out = "Sie sind jetzt abgemeldet";
+$l_login = "anmelden";
+$l_loginfirst = "Sie m&uuml;ssen sich zuerst anmelden";
+$l_edit_proposed_news = "Vorgeschlagenen Newseintrag bearbeiten";
+$l_entryupdated = "Eintrag aktualisiert";
+$l_delmsg = "Eintrag {entrynr} wirklich l&ouml;schen?";
+$l_bbbuttons = array("liststart"=>"Listenanfang","listend"=>"Listenende","listitem"=>"Listenelement","font"=>"Zeichensatz","fontsize"=>"Gr&ouml;sse","fontcolor"=>"Farbe","alignment"=>"Ausrichtung","bold"=>"Fettdruck","italic"=>"Kursivschrift","typewriter"=>"Schreibmaschinenschrift","center"=>"zentrieren","sub"=>"tief stellen","sup"=>"hoch stellen","code"=>"Kode","quote"=>"Zitat","url"=>"Link","email"=>"E-Mail","pic"=>"Bild","strike"=>"durchgestrichen","pic2"=>"Bild auswählen/uploaden","underline"=>"Unterstreichen");
+$l_availfonts = array("Andale Mono","Arial","Arial Black","Book Antiqua","Century Gothic","Comic Sans MS","Courier New","Georgia","Impact","Tahoma","Times New Roman","Script MT Bold","Verdana","Lucida Console");
+$l_availcolors = array("White","Black","Red","Yellow","Pink","Green","Orange","Purple","Blue","Beige","Brown","Teal","Navy","Maroon","Limegreen");
+$l_enterfont = "Bitte Zeichensatz eingeben";
+$l_entertextsize = "Bitte Zeichengrösse eingeben";
+$l_alignments = array("Left","Right","Center");
+$l_alignment = "Ausrichtung";
+$l_enterlink = "Bitte Link eingeben";
+$l_enteremail = "Bitte E-Mailadresse eingeben";
+$l_enterurl = "Bitte URL eingeben";
+$l_entertext = "Bitte Text eingeben";
+$l_entercolor = "Bitte Farbe eingeben";
+$l_bbcode_helps = array(
+	"Fettschrift: [b]Text[/b]",
+	"Kursivschrift: [i]Text[/i]",
+	"Schreibmaschinenschrift: [tt]Text[/tt]",
+	"durchgestrichener Text: [s]Text[/s]",
+	"zentrieren: [center]Text[/center]",
+	"tiefstellen: [sub]Text[/sub]",
+	"hochstellen: [sup]Text[/sup]",
+	"Code: [code]Text[/code]",
+	"Zitat: [quote]Text[/quote]",
+	"Listenanfang: [list]",
+	"Listenelement: [*]",
+	"Listenende: [/list]",
+	"E-Mail: [email]test@foo.bar[/email]",
+	"URL: [url]http://www.foo.bar[/url]",
+	"Bild einf&uuml;gen: [img]http://www.foo.bar/bild.gif[/img]",
+	"Bild auswählen/uploaden",
+	"Zeichensatz: [font=Arial]Text[/font]",
+	"Schriftgr&ouml;sse: [size=2]Text[size]",
+	"Textausrichtung: [align=right]Text[/align]",
+	"Textfarbe: [color=red]Text[/color]"
+);
+$l_fontcolor = "Schriftfarbe";
+$l_emoticonlist = "Liste der Smilies";
+$l_close = "schliessen";
+$l_code = "Code";
+$l_emotion = "Emotion";
+$l_bbcodehelp = "BBCode Hilfe";
+$l_options = "Optionen";
+$l_urlautoencode = "URLs automatisch umwandeln";
+$l_enablespcode = "BBTags aktivieren";
+$l_disableemoticons = "Smilies deaktivieren";
+$l_entrydeleted = "Eintrag gel&ouml;scht";
+$l_delnewsmail = "{email} hat eine Anforderung zum L&ouml;schen des Newseintrags #{newsnr} gestellt.";
+$l_delnewssubj = "Anforderung zum Loeschen eines Newseintrags";
+$l_proposed_events = "Vorgeschlagene Events";
+$l_back2events = "Zur&uuml;ck zu den Events";
+$l_transferredevents = "bereits in die Events &uuml;bernommene Vorschl&auml;ge";
+$l_deleventmail = "{email} hat eine Anforderung zum L&ouml;schen des Eventeintrags #{eventnr} gestellt.";
+$l_deleventsubj = "Anforderung zum Loeschen eines Events";
+$l_edit_proposed_events = "Vorgeschlagenen Event bearbeiten";
+$l_announcements = "Ank&uuml;ndigungen";
+$l_announcement = "Ank&uuml;ndigung";
+$l_global_announcement = "allgemeine Ank&uuml;ndigung";
+$l_back2search = "zur&uuml;ck zur Suche";
+$l_newsubsubj = "Neuer Abonnent ({sitename})";
+$l_newsubmsg = "{email} hat sich f&uuml;r den Newsletter zu {category} auf {sitename} angemeldet";
+$l_remsubsubj = "Abmeldung eines Abonnent ({sitename})";
+$l_remsubmsg = "{email} hat sich f&uuml;r den Newsletter zu {category} auf {sitename} abgemeldet";
+$l_attachfile = "Datei anh&auml;ngen";
+$l_noeventstoday2 = "keine Events für den heutigen Tag";
+$l_subscriptionconfirminfo = "Um sicher zu stellen, dass Sie auch der Besitzer der angegebenen E-Mailadresse sind<br>wurde eine Best&auml;tigungsanforderung per E-Mail an diese Adresse versand.<br>
+Zur Aktivierung des Abonnements verfahren Sie bitte, wie in der E-Mail beschrieben.";
+$l_edituserentry = "Benutzereintrag bearbeiten";
+$l_newproposal = "neuen Vorschlag schreiben";
+$l_newpassword = "neues Passwort";
+$l_emailentry = "Eintrag per E-Mail verschicken";
+$l_sendermail = "E-Mailadresse des Absenders";
+$l_receivermail = "E-Mailadresse des Empf&auml;ngers";
+$l_nosendermail = "Sie m&uuml;ssen eine E-Mailadresse f&uuml;r den Absender angeben";
+$l_noreceivermail = "Sie m&uuml;ssen eine E-Mailadresse f&uuml;r den Empf&auml;nger angeben";
+$l_novalidsendermail = "Ung&uuml;ltige E-Mailadresse f&uuml;r den Absender";
+$l_novalidreceivermail = "Ung&uuml;ltige E-Mailadresse f&uuml;r den Empf&auml;nger";
+$l_entrymailed = "Eintrag per E-Mail versand";
+$l_subject = "Betreff";
+$l_mailentrysubj = "Newseintrag";
+$l_ratingquestion = "Sollen mehr solche Meldungen erscheinen";
+$l_ratings = array(1=>"Ja",0=>"Nein");
+$l_ratingadded = "Ihre Bewertung wurde gespeichert";
+$l_timezone_note = "Alle Zeitangaben sind in";
+$l_by = "von";
+$l_on = "am";
+$l_newcommentsubj = "Neuer Kommentar ({sitename})";
+$l_newcommentmsg = "{email} hat einen neuen Kommentar zu Eintrag #{entry} auf {sitename} geschrieben";
+$l_nocatselected = "Sie m&uuml;ssen eine Kategorie ausw&auml;hlen";
+$l_unknown = "unbekannt";
+$l_msubscriptionconfirmmail = "Hallo,\nSie haben sich beim Aboservice fuer '{catname}' auf {sitename} angemeldet.\n
+Um sicher zu stellen, dass Sie sich auch selbst angemeldet haben, haben wir diesen Bestaetigungsmechanismus aktiviert.\n
+Um das Abo zu aktivieren, besuchen sie bitte innerhalb von {confirmtime} folgende URL:\n{confirmurl}\n
+Sollten Sie die Anmeldung nicht aktivieren wollen, so brauchen Sie nichts zu tun.";
+$l_msubscriptionconfirmmail_html = "<font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"2\">Hallo,\nSie haben sich beim Aboservice f&uuml;r <i>{catname}</i> auf {sitename} angemeldet.\n
+Um sicher zu stellen, dass Sie sich auch selbst angemeldet haben, haben wir diesen Best&auml;tigungsmechanismus aktiviert.\n
+Um das Abo zu aktivieren, besuchen sie bitte innerhalb von <b>{confirmtime}</b> folgende URL:\n<a href=\"{confirmurl}\"><font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"1\">{confirmurl}</font></a>\n
+Sollten Sie die Anmeldung nicht aktivieren wollen, so brauchen Sie nichts zu tun.</font>";
+$l_subscriptionmaint = "Verwaltung der Abos";
+$l_listsubs = "Abos anzeigen";
+$l_subscriptionsfor = "Abos f&uuml;r";
+$l_munsubscriptionconfirmmail = "Hallo,\nSie haben angefordert sich beim Aboservice fuer '{catname}' auf {sitename} abzumelden.\n
+Um sicher zu stellen, dass Sie sich auch selbst abmelden wollen, haben wir diesen Bestaetigungsmechanismus aktiviert.\n
+Um sich endgueltig abzumelden, besuchen sie bitte folgende URL:\n{confirmurl}\n
+Sollten Sie die Abmeldung nicht vornehmen wollen, so brauchen Sie nichts zu tun.";
+$l_munsubscriptionconfirmmail_html = "<font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"2\">Hallo,\nSie haben angefordert sich beim Aboservice f&uuml;r <i>{catname}</i> auf {sitename} abzumelden.\n
+Um sicher zu stellen, dass Sie sich auch selbst abmelden wollen, haben wir diesen Best&auml;tigungsmechanismus aktiviert.\n
+Um sich endg&uuml;ltig abzumelden, besuchen sie bitte folgende URL:\n<a href=\"{confirmurl}\"><font face=\"Verdana, Geneva, Arial, Helvetica, sans-serif\" size=\"1\">{confirmurl}</font></a>\n
+Sollten Sie die Abmeldung nicht vornehmen wollen, so brauchen Sie nichts zu tun.</font>";
+$l_lastupdated = "letzte Aktualisierung";
+$l_next_entry = "nächster Eintrag";
+$l_gotostart = "zum Anfang";
+$l_prev_entry = "vorheriger Eintrag";
+$l_goto_news = "zu den News";
+$l_goto_events = "zu den Events";
+$l_goto_announcements = "zu den Ankündigungen";
+$l_events_for_today = "heutige Events";
+$l_events_for_days = "Events der nächsten %d Tage";
+$l_numentries = "Anzahl Einträge";
+$l_search_events = "Events suchen";
+$l_do_search = "Suche durchführen";
+$l_search_results = "Suchergebnisse für den %s (%d)";
+$l_restart_search = "Neue Suche";
+$l_events_for = "Events der nächsten";
+$l_days = "Tage";
+$l_wap_evs_dayrange = "Tage +/-";
+$l_only_selday = "nur ausgewählter Tag";
+$l_search_results2 = "Suchergebnisse für den %s, +/- %d Tage (%d)";
+$l_nextpage = "nächste Seite";
+$l_prevpage = "vorherige Seite";
+$l_searchtext = "Suchbegriffe";
+$l_search_results3 = "Suchergebnisse %s - %s (%d)";
+$l_listofcats = "Liste der Kategorien";
+$l_collapse = "Eintrag verbergen";
+$l_expand = "Eintrag anzeigen";
+$l_mode = "Modus";
+$l_file = "Datei";
+$l_description = "Beschreibung";
+$l_rss_newsfeeds = "RSS Newsfeeds";
+$l_unallowedfiletype = "Dateien dieses Typs (.%s) d&uuml;rfen nicht als Dateianhang benutzt werden";
+$l_catundef = "Kategorie nicht definiert";
+$l_wholestory = "[hier lesen]";
+$l_noannouncements = "keine Ank&uuml;ndigungen";
+$l_searchby = "suchen &uuml;ber";
+$l_pwset = "Passwort wurde gsetzt";
+$l_noflooding = "Ein Beitrag kann nur einmal abgeschickt werden";
+// Wenn Sie auf den erzeuten HTML-Seiten eine Info dazu haben wollen, dass Sie die Sprachdatei
+// übersetzt haben, so plazieren Sie diese in dieser Variablen.
+// z.B. $l_translationnote = "schwäbische Übersetzung von Hans Mustermann"
+$l_translationnote = "";
+?>
