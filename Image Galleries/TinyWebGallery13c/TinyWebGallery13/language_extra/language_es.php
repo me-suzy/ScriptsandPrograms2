e2a54e82261d45388bec42c3226a70e58f31652c
me-suzy <me.suzana@gmail.com>
2025-08-12 18:33:30 +0300

    Add Image Galleries/TinyWebGallery13c/TinyWebGallery13/language_extra/language_es.php

---
+++ Image Galleries/TinyWebGallery13c/TinyWebGallery13/language_extra/language_es.php
@@ -0,0 +1,222 @@
+<?php
+/*************************
+  Copyright (c) 2004-2005 TinyWebGallery
+  written by Michael Dempfle
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  TWG version: 1.3c
+  $Date: 2005/11/15 09:02 $
+**********************************************/
+// language file - version 1.3 - if you translate a langauge file where already some parts are translated: Please don't remove the former translator completely - insert him/her behind the lang_translator as a comment
+$lang_translator = " - Translator: Jorge Redondo";
+$lang_optionen = "Opciones";
+$lang_menu_titel = "T&iacute;tulo";
+$lang_kommentar = "Comentarios";
+
+$lang_titel = "";
+$lang_titel_no = "No se ha subido ninguna galer&iacute;a todav&iacute;a.";
+$lang_select_gallery = "Selecciona una galer&iacute;a.";
+$lang_help = "Ayuda";
+$lang_new_window = "Nueva ventana";
+$lang_login = "Login";
+$lang_logout = "Logout";
+$lang_visitor = "Visitas:";
+$lang_total = "Total:";
+$lang_today = "Hoy:";
+$lang_galleries = "Galer&iacute;as";
+$lang_picture = "Imagen";
+$lang_pictures = "Im&aacute;genes";
+$lang_of = "de";
+
+$lang_overview = "Visi&oacute;n general";
+$lang_forward = "Siguiente";
+$lang_back = "Anterior";
+$lang_rotate_left = "Girar a la izquierda";
+$lang_rotate_right = "Girar a la derecha";
+$lang_start_slideshow = "Comenzar slideshow";
+$lang_stop_slideshow = "Parar slideshow";
+$lang_thumb_forward = "&gt;"; // = '>'
+$lang_thumb_back="&lt;"; // = '<'
+$lang_views = " Visitas";
+
+$lang_login_php_enter = "Introduce la contrase&ntilde;a para editar los t&iacute;tulos<br /> y borrar los comentarios.";
+$lang_login_php_enter_again = "Contrase&ntilde;a err&oacute;nea<br />Introd&uacute;cela de nuevo:";
+
+$lang_kommenar_php_both_fields = "Rellena ambos campos.<br />";
+$lang_kommenar_php_enter_name = "Introduce un nombre<br />(max. 30 caracteres)";
+$lang_kommenar_php_enter_comment = "Introduce un comentario<br />(max. 150 caracteres):";
+$lang_kommenar_php_name = "Nombre:";
+$lang_kommenar_php_comment = "Comentarios (max. 150 caracteres)";
+$lang_kommenar_php_speichern = "Guardar";
+
+$lang_privatelogin_php_wrong_password = "Contrase&ntilde;a incorrecta<br />Introd&uacute;cela de nuevo:";
+$lang_privatelogin_php_password = "Introduce la contrase&ntilde;a para la galer&iacute;a<br />'%s':";
+$lang_privatelogin_php_login = "Login";
+
+$lang_titel_php_titel = "Introduce un t&iacute;tulo<br />(max. 150 caracteres):";
+$lang_titel_php_save = "Guardar";
+
+$lang_slideshowid_php_loading = "Cargando slideshow ...";
+
+$lang_opionen_php_yes = "Si";
+$lang_opionen_php_no = "No";
+$lang_opionen_php_new_window = "Abrir galer&iacute;a en una nueva ventana";
+$lang_opionen_php_opt_slideshow = "Slideshow optimizado con transiciones";
+$lang_opionen_php_slideshowintervall = "Intervalo de slideshow (sec.)";
+$lang_opionen_php_ok = "OK";
+$lang_opionen_php_top_nav = "Mostrar solo la navegaci&oacute;n superior";
+
+$lang_back_topx = "Volver&nbsp;a&nbsp;las&nbsp;%s&nbsp;m&aacute;s&nbsp;visitadas";
+$lang_topx = "Las&nbsp;%s&nbsp;m&aacute;s&nbsp;visitadas";
+$lang_main_topx = "Volver a las galer&iacute;as";
+// new in 1.1
+$lang_loading = "cargando...";
+$lang_dhtml_navigation = "Usar navegaci&oacute;n DHTML";
+$lang_optionen_slideshow = "Tipo de slideshow";
+$lang_optionen_slideshow_fullscreen = "Maximizado";
+$lang_optionen_slideshow_optimized = "Disolver";
+$lang_optionen_slideshow_normal = "Normal";
+$lang_lowbandwidth = "TinyWebGallery est&aacute; optimizado para conexiones lentas.";
+$lang_highbandwidth = "TinyWebGallery est&aacute; optimizado para conexiones r&aacute;pidas.";
+// new in 1.2
+$lang_email_menu_user = "Notificaci&oacute;n por correo electr&oacute;nico";
+$lang_email_menu_admin = "Enviar correo electr&oacute;nico";
+// Sorrymessage for failed subscription
+$lang_email_sorrysignmessage = "Perd&oacute;n, el correo electr&oacute;nico %s ya est&aacute; registrado.";
+// Sorrymessage for blank email
+$lang_email_sorryblankmailmessage = "Por favor, introduce una direcci&oacute;n de correo electr&oacute;nico.";
+// Sorrymessage for invalid emails
+$lang_email_sorryoddmailmessage = "Perd&oacute;n, El correo electr&oacute;nico que has introducido, '%s', no parece ser una direcci&oacute;n correcta.";
+// Sorrymessage if someone entered your own mail
+$lang_email_sorryownmailmessage = "¡Perd&oacute;n, pero no quiero obtener mis propios boletines!";
+// Title of the newsletter, will be displayed in the FROM field of the mailclient
+$lang_email_subscribemail_subject = "Hay nuevas im&aacute;genes.";
+// Subscribemessage, will be shown when someone subscribes.
+$lang_email_subscribemessage = "Gracias por registrarte. Se te enviar&aacute; un correo electr&oacute;nico de confirmaci&oacute;n.";
+// Subscribemail, will be sent when someone subscribes.
+$lang_email_subscribemail = "Has sido a&ntilde;adido con &eacute;xito al bolet&iacute;n de notificaciones";
+// Unsubscribemessage for deletion, will be followed by the email!
+$lang_email_unsubscribemessage = "Se ha borrado del bolet&iacute;n de notificaciones el correo electr&oacute;nico '%s'.";
+// Unsubscribemessage for failed deletion, will be followed by the email!
+$lang_email_failedunsubscriptionmessage = "No se ha encontrado el correo electr&oacute;nico: %s ";
+// the welcometext
+$lang_email_welcomemessage = "Aqu&iacute; puedes a&ntilde;adir o eliminar tu direcci&oacute;n de correo electr&oacute;nico al bolet&iacute;n de notificaciones.<br/>El administrador de la galer&iacute;a te informar&aacute; si se a&ntilde;aden nuevas im&aacute;genes a la galer&iacute;a.";
+// email could not be sent.
+$lang_email_error_send_mail = "No se ha podido enviar el correo electr&oacute;nico de confimaci&oacute;n.<br/>El servidor de correo puede no estar configurado correctamente. Por favor, contacta con el administrador de la galer&iacute;a.";
+
+$lang_email_add = "A&ntilde;adir";
+$lang_email_remove = "Eliminar";
+$lang_email_send = "Enviar";
+
+$lang_email_admin_welcomemessage_send = 'Aqu&iacute; puedes informar a todos los usuarios registrados, (%s), sobre cambios de la galer&iacute;a.';
+$lang_email_admin_sorryoddsendermailmessage = "Perd&oacute;n, el correo electr&oacute;nico de remitente que has introducido, '%s', no parece ser una direcci&oacute;n de correo electr&oacute;nico v&oacute;lida.";
+$lang_email_admin_sendermail = "Direcci&oacute;n del remitente (tu correo electr&oacute;nico)";
+$lang_email_admin_subject = "Asunto";
+$lang_email_admin_message = "Mensaje";
+$lang_email_admin_sendbutton = "Enviar";
+$lang_email_admin_sent = "El siguiente mensaje ha sido enviado:";
+$lang_email_admin_from = "De";
+$lang_email_admin_notloggedin = "No est&aacute;s indentificado. Por favor, ve a la p&aacute;gina principal e indentif&iacute;cate.";
+
+$lang_visitor_30 = "Historial &uacute;ltimos 30 d&iacute;as";
+
+$lang_login_php_enter = "Por favor, introduce la contrase&ntilde;a para desbloquear las caracter&iacute;sticas extendidas de la galer&iacute;a.";
+$lang_opionen_php_zoom = "Vista maximizada";
+$lang_administration = "Administraci&oacute;n";
+$lang_opionen_php_zoom_message = "Atenci&oacute;n; La \'Vista maximizada\' es m&aacute;s lenta que la vista normal, ya que las im&aacute;genes se generan en ese momento. Tambi&eacuten tarda m&aacute;s en transferirla, porque son m&aacute;s grandes.\\n¡Por favor, usa esta opci&oacute;n s&oacute;lo si tienes una conexi&oacute;n r&aacute;pida a internet!";
+$lang_first = "Primera";
+$lang_last = "&Uacute;ltima";
+$lang_no_session = "Session is not available. Login to private galleries is not enabled.";
+$lang_not_loggedin = "You are not logged in.<br/>Please go to the main page and unlock this gallery.";
+$lang_no_topx_images = "No images could be found for this view. <br / >Protected galleries are only included if you unlock them first.";
+
+$lang_tips_overview = array('Tip: Please click on the help link, if you want to know what you can do with the gallery.');
+$lang_tips_thumb = array('Tip: On the small navigation in the left upper corner you can directly jump to all the parent levels.','Tip: The Top 13 on this page does only show the most viewed images of THIS directory.');
+$lang_tips_image = array('Tip: You can use the left/right arrow key to navigate to the next/last image.','Tip: If you login (right upper corner) you can enter captions, delete comments and rotate images permanently.','Tip: You can use smilies like :) or ;) in titles and comments.');
+
+// new 1.3
+$lang_dl_as_zip1 = "You can download all images of this directory in a single archive.<br />Do you want to download the whole directory or continue to the actual image?";
+$lang_dl_as_zip2 = "Actual image";
+$lang_dl_as_zip3 = "All";
+$lang_dl_as_zip4 = "Do not ask again in this session."; // we store this is the session
+
+$lang_download_counter = "Downloads";
+$lang_download_counter_short = "dl";
+// not used in 1.3 yet!
+$lang_email_subscribemail_2_subject = "Please confirm your registration at the TinyWebGallery";
+$lang_email_subscribemail_2 = "Dear TinyWebGallery user:\n\nYou have registered to the notification list of TinyWebGallery. To complete the registration please klick on the link below:\n%s\n\nYour TinyWebGallery";
+// --
+$lang_rating = "Vote";
+$lang_rating1 = "1";
+$lang_rating2 = "2";
+$lang_rating3 = "3";
+$lang_rating4 = "4";
+$lang_rating5 = "5";
+$lang_rating_button = "Rate";
+$lang_rating_text = "Please rate the actual image:";
+$lang_rating_new="New vote: ";
+$lang_rating_vote="Votes";
+
+$lang_rating_security="Please enter the 4 characters<br />of the security code [A-F,0-9]:";
+$lang_rating_help="Please click on the image,<br />if you cannot see the code.";
+$lang_rating_message1="You have already voted.";
+$lang_rating_message2="please close this window.";
+$lang_rating_message3="Thanks for voting.";
+$lang_rating_message4="You have entered a wrong code.";
+$lang_rating_send="Send";
+
+$lang_fileinfo="Info";
+$lang_fileinfo_name = "File name";
+$lang_fileinfo_date = "Date";
+$lang_fileinfo_size = "Size";
+$lang_fileinfo_resolution = "Resolution";
+$lang_fileinfo_views = "Views";
+$lang_fileinfo_dl = "Downloads";
+$lang_fileinfo_rating = "Rating";
+$lang_fileinfo_not_available = " - (remote)";
+$lang_exif_not_available = " - ";
+// please check the constants in the file inc/exifReader.inc.php for exif entries you can try - not all are available on all cameras! sometimes the constants don't work - try to google for the real exif field name!
+$lang_exif_info = array("Camera Model" => "model", "Exif Date" => "DateTime", "Focal Length" => 
+"focalLength", "f/Stop" => "fnumber", "Shutter Speed" => "exposureTime", "ISO" => "isoEquiv");
+
+$lang_comments = "Comments";
+$lang_last_comments="Newest comments";
+$lang_show_kommentar="Show comments";
+$lang_add_kommentar="Add comment";
+$lang_close_fullscreen="Back to the normal view";
+
+$lang_search_results="Search resuls";
+$lang_search_back="Back to the search results.";
+$lang_search="Search";
+$lang_search_text="Please enter the search text:";
+$lang_search_button="Search";
+$lang_search_where="Search in";
+$lang_search_max="Hits shown per page";
+$lang_search_all="All";
+$lang_search_hits="Found %s hits.";
+
+// height of the i_frames - please check if the settings match for your language!!
+$lang_height_comment = 150;
+$lang_height_caption = 90;
+$lang_height_login = 80;
+$lang_height_private = 80;
+$lang_height_options = 205;
+if (!$show_new_window) { $lang_height_options -= 30; }
+if (!$show_slideshow)  { $lang_height_options -= 60; }
+if (!$enable_maximized_view)  { $lang_height_options -= 30; }
+$lang_height_email_user = 180;
+$lang_height_email_admin = 280;
+$lang_height_info = 150;
+if (!$show_download_counter || !$enable_download_counter) { $lang_height_info -= 17; }
+if (!$show_count_views) { $lang_height_info -= 17; }
+if (!$show_image_rating) { $lang_height_info -= 17; }
+if ($show_exif_info) { $lang_height_info += (count($lang_exif_info) * 17);}
+$lang_height_rating = 150;
+$lang_height_dl_manager=150;
+$lang_height_search=175;
+$lang_xpos_lang_dropdown="200"; // this is only needed if you include twg and ie does not position the language dropdown correct because you include twg in several divs - in a fixed layout you can adjust this here - if you don't have a fix layout don't use the dropdown - use the normal setting! 
+?>
