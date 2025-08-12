a3ef3c8575bd576376356db070c126af74ed799f
me-suzy <me.suzana@gmail.com>
2025-08-12 06:14:20 +0300

    Add Content Management/metajour2_1/metajour/extension/listing/lang/listing.en.php

---
+++ Content Management/metajour2_1/metajour/extension/listing/lang/listing.en.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['name'] = 'Configuration of List';
+$LANG['label_name'] = 'Configuration name';
+$LANG['label_classname'] = 'Table';
+$LANG['label_allowview'] = 'Allow detail view';
+$LANG['label_category'] = 'Only records from categories';
+$LANG['text_1'] = 'Field';
+$LANG['text_2'] = 'Sorting';
+$LANG['text_3'] = 'Column';
+$LANG['text_4'] = 'Delete';
+$LANG['text_5'] = 'Add field';
+
+?>
