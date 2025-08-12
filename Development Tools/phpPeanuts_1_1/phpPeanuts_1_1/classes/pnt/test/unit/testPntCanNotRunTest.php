97d7486e7de4638d79fb0db96de4567cc85f9489
me-suzy <me.suzana@gmail.com>
2025-08-12 10:05:12 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/pnt/test/unit/testPntCanNotRunTest.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/pnt/test/unit/testPntCanNotRunTest.php
@@ -0,0 +1,18 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+   	
+includeClass('PntTestCase', 'pnt/unit');
+
+/**
+ * @package pnt/test/unit
+ * @author  Henk Verhoeven, MetaClass <henk@phpPeanuts.org>
+ */
+class PntCanNotRunTest extends PntTestCase {
+
+
+}
+
+return 'WrongName';
+?>
