c9a6dba2cc803456fae580137d56286e6168c7ea
me-suzy <me.suzana@gmail.com>
2025-08-12 06:13:45 +0300

    Add Content Management/metajour2_1/metajour/extension/gallery/lang/gallery.en.php

---
+++ Content Management/metajour2_1/metajour/extension/gallery/lang/gallery.en.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['label_name'] = 'Configuration name';
+$LANG['label_title'] = 'Title';
+$LANG['label_folderid'] = 'Folder with images';
+$LANG['label_xnum'] = 'Number of horisontal images';
+$LANG['label_ynum'] = 'Number of vertical images';
+$LANG['label_thumbsize'] = 'Max size of thumbnails (pixels)';
+$LANG['label_mediumsize'] = 'Max size of medium images (pixels)';
+$LANG['label_usemedium'] = 'Show medium images';
+$LANG['label_usefull'] = 'Show original images';
+$LANG['label_linktofull'] = 'Link to original images';
+$LANG['label_fullnewwindow'] = 'Show original images in new window';
+$LANG['label_mediumnewwindow'] = 'Show medium images in new window';
+
+$LANG['label_addcomment'] = 'Allow comments';
+$LANG['label_listcomment'] = 'Allow listing of comments';
+$LANG['label_commentpos'] = 'Position of comments';
+$LANG['option_commentpos'][0] = 'Below image';
+
+$LANG['label_templateid_index'] = 'Template for index-view';
+$LANG['label_templateid_medium'] = 'Template for medium-view';
+$LANG['label_templateid_full'] = 'Template for original-view';
+
+$LANG['name'] = 'Configuration of Image gallery';
+?>
