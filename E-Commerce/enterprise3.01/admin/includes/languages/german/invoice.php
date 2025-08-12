04c0bde59e9d1d1e1b651c7d9d82d97bc82103df
me-suzy <me.suzana@gmail.com>
2025-08-12 11:46:02 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/german/invoice.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/german/invoice.php
@@ -0,0 +1,29 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('TABLE_HEADING_COMMENTS', 'Kommentar');
+define('TABLE_HEADING_PRODUCTS_MODEL', 'Artikel-Nr.');
+define('TABLE_HEADING_PRODUCTS', 'Artikel');
+define('TABLE_HEADING_TAX', 'MwSt.');
+define('TABLE_HEADING_TOTAL', 'Summe');
+define('TABLE_HEADING_PRICE_EXCLUDING_TAX', 'Preis (exkl.)');
+define('TABLE_HEADING_PRICE_INCLUDING_TAX', 'Preis (inkl.)');
+define('TABLE_HEADING_TOTAL_EXCLUDING_TAX', 'Summe (exkl.)');
+define('TABLE_HEADING_TOTAL_INCLUDING_TAX', 'Summe (inkl.)');
+
+define('ENTRY_SOLD_TO', 'Rechnungsanschrift:');
+define('ENTRY_SHIP_TO', 'Lieferanschrift:');
+define('ENTRY_PAYMENT_METHOD', 'Zahlungsweise:');
+define('ENTRY_SUB_TOTAL', 'Zwischensumme:');
+define('ENTRY_TAX', 'MwSt.:');
+define('ENTRY_SHIPPING', 'Versandkosten:');
+define('ENTRY_TOTAL', 'Gesamtsumme:');
+?>
