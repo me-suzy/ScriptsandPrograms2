ca7f53bf2a1162740b63889966d04436255926d4
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:59 +0300

    Add Content Management/metajour2_1/metajour/lang/filter.da.php

---
+++ Content Management/metajour2_1/metajour/lang/filter.da.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Rapportdefinitioner';
+$LANG['option_filtertype']['0'] = 'Inline rapportdefinition';
+$LANG['option_filtertype']['5'] = 'Server rapportdefinition';
+$LANG['option_filtertype']['10'] = 'Bruger uploaded rapportdefinition';
+$LANG['label_name'] = 'Navn';
+$LANG['label_content'] = 'Indhold';
+$LANG['label_mimetype'] = 'MIME type';
+$LANG['label_classtype'] = 'Classtype';
+$LANG['label_filtertype'] = 'Rapport type';
+$LANG['label_filterfiletype'] = 'Filtype';
+$LANG['label_binfileid'] = 'Filnavn';
+$LANG['label_datatype'] = 'Tabel';
+?>
