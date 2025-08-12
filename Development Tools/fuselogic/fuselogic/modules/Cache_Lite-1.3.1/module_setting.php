751414231fe4eb5c8d1f3321eb79d8da02e6037d
me-suzy <me.suzana@gmail.com>
2025-08-12 09:13:11 +0300

    Add Development Tools/fuselogic/fuselogic/modules/Cache_Lite-1.3.1/module_setting.php

---
+++ Development Tools/fuselogic/fuselogic/modules/Cache_Lite-1.3.1/module_setting.php
@@ -0,0 +1,10 @@
+<?php
+
+$FL_MODULE_SETTING['module_name'] = 'cache';
+
+$FL_MODULE_SETTING['sub_module']['_start'] = 'start_end1.php';
+$FL_MODULE_SETTING['sub_module']['_end'] = 'start_end1.php';
+$FL_MODULE_SETTING['sub_module']['clear'] = 'start_end1.php';
+$FL_MODULE_SETTING['sub_module']['example'] = 'example.php';
+
+?>
