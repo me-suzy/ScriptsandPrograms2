39a8778353ec543e058701c641c07a6ae73b9a7d
me-suzy <me.suzana@gmail.com>
2025-08-12 20:21:48 +0300

    Add News Publishing/saxon/saxon/footer.php

---
+++ News Publishing/saxon/saxon/footer.php
@@ -0,0 +1,15 @@
+<?php
+include 'config.php';
+?>
+<!-- end content -->
+</div>
+
+<!-- end wrapper -->
+</div>
+
+<div id="footer">
+SAXON <?php echo $version ?> &copy; 2005 <a href="http://www.blackwidows.co.uk">Black Widow</a>
+</div>
+
+</body>
+</html>
