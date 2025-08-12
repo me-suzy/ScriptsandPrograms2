aadd4c0607f59a619ba6a01c34d9fbe2c93a29f9
me-suzy <me.suzana@gmail.com>
2025-08-13 01:00:19 +0300

    Add Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/new_hitclk.sql

---
+++ Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/new_hitclk.sql
@@ -0,0 +1,10 @@
+CREATE TABLE al_hitclk (
+  id bigint(20) unsigned NOT NULL auto_increment,
+  sent datetime NOT NULL default '0000-00-00 00:00:00',
+  site varchar(16) NOT NULL default '',
+  ref varchar(16) NOT NULL default '',
+  toref varchar(16) NOT NULL default '',
+  ip varchar(15) NOT NULL default '',
+  host varchar(40) NOT NULL default '',
+  PRIMARY KEY  (id)
+) TYPE=MyISAM;
