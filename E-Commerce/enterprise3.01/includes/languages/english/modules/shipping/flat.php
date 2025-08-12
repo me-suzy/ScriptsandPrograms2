bb19513d2c87cf9e8778a5010feda0ce44de9eb9
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:00 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/modules/shipping/flat.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/modules/shipping/flat.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_FLAT_TEXT_TITLE', 'Flat Rate');
+define('MODULE_SHIPPING_FLAT_TEXT_DESCRIPTION', 'Flat Rate');
+define('MODULE_SHIPPING_FLAT_TEXT_WAY', 'Best Way');
+?>
