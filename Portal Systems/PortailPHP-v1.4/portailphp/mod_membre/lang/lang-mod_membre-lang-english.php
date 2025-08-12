130c2cad96363f211ce62a3a926818f616f653f7
me-suzy <me.suzana@gmail.com>
2025-08-12 23:18:27 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_membre/lang/lang-mod_membre-lang-english.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_membre/lang/lang-mod_membre-lang-english.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE MEMBRES
+$Mod_Membres_Accueil_OFF="Reset connection";
+$Mod_Membres_News_Add="News add OK";
+$Mod_Membres_Liens_Add="Link add OK";
+$Mod_Membres_News_Edit="News update OK";
+$Mod_Membres_Info="Edit <strong>/include/config.php</strong> file to modify values";
+$Mod_Membres_InfoUser="Edit <strong>$BD_Tab_user</strong> table to modify values";
+$Mod_Membres_Info_Theme="Theme";
+$Mod_Membres_Info_Langue="Language";
+$Mod_Membres_Info_Titre="Title";
+$Mod_Membres_Info_Version="Version";
+$Mod_Membres_Info_Couleur="Logo Color";
+$Mod_Membres_Info_URL="URL";
+$Mod_Membres_Info_UploadLimite="Max. Upload";
+$Mod_Membres_Serveur_Info="PHP server informations";
+$Mod_Membres_Nom="Name";
+$Mod_Membres_Pseudo="Login";
+$Mod_Membres_Mail="E-Mail";
+$Mod_Membres_Date="Active since ";
+$Mod_Membres_Droit="Right";
+$Mod_Membres_Web="Web";
+$Mod_Membres_Password="Password";
+$Mod_Membres_Err_Field="You forgot to fill a field.";
+$Mod_Membres_Err_Login="Incorrect Login or password";
+$Mod_Membres_Rub_Accueil="Management root";
+$Mod_Membres_Rub_Info="Info";
+$Mod_Membres_Rub_News_Cat="Add heading to news";
+$Mod_Membres_Rub_News_Add="Add news";
+$Mod_Membres_Rub_News_Edit="Modify news";
+$Mod_Membres_Rub_News_Del="Delete news";
+$Mod_Membres_Rub_News_Del2="Delete";
+$Mod_Membres_Rub_News="Now, to delete or edit a news, a download, you must use the standard interface of the web site." ;
+$Mod_Membres_Rub_News_question="Are you sure you want delete the news : " ;
+$Mod_Membres_Rub_News_Form_Categorie="Heading name";
+$Mod_Membres_Rub_News_JS_Cat="Heading field must be filled";
+$Mod_Membres_Rub_News_Cat_OK="News heading update OK";
+$Mod_Membres_Rub_File_Cat="Add heading to File module";
+$Mod_Membres_Rub_File_Del="Delete File";
+$Mod_Membres_Rub_File_DelDataBase = "The file was delete to database but it not found on server." ;
+$Mod_Membres_Rub_File_Form_Categorie="Heading field";
+$Mod_Membres_Rub_File_JS_Cat="Heading field must be filled";
+$Mod_Membres_Rub_File_Cat_OK="File heading update OK";
+$Mod_Membres_Rub_Liens_Cat="Add heading to Links module";
+$Mod_Membres_Rub_Liens_Cat_OK="Links heading update OK";
+$Mod_Membres_Rub_Faq_Form_Categorie="Heading field";
+$Mod_Membres_Rub_Faq_Cat="Add heading to FAQ";
+$Mod_Membres_Rub_Faq_Add="Add FAQ";
+$Mod_Membres_Rub_Faq_Edit="Modify FAQ";
+$Mod_Membres_Rub_Faq_Del="Delete FAQ";
+$Mod_Membres_Rub_Faq_Add_OK="FAQ add OK";
+$Mod_Membres_Rub_Faq_Cat_OK="Faq heading update OK";
+$Mod_Membres_Rub_Upload_Photo="Add photo";
+$Mod_Membres_Rub_Del_Photo="Del 1 photo";
+$Mod_Membres_Rub_Upload_File = "Add file";
+$Mod_Membres_Rub_Liens_Edit="Add 1 link";
+$Mod_Membres_Nom="Name";
+$Mod_Membres_Regdate="Last save date";
+$Mod_Membres_Droit="Right";
+$Mod_Membres_Web="Web";
+$Mod_Membres_OK_Login="You have been connected";
+$Mod_Membres_OFF_Login="You unconnected";
+$Mod_Membres_Photos_Form_Photo="Photo (JPG)";
+$Mod_Membres_Photos_Form_Vign="Sticker (JPG, 90 width)";
+$Mod_Membres_Photos_Form_Titre="Title (50)";
+$Mod_Membres_Photos_Form_Date="Date (aaaa-mm-jj)";
+$Mod_Membres_Photos_Form_Texte="Text";
+// Ajouter à la version 1.2.1
+$Mod_Membres_Menu_General = "General menu" ; 
+$Mod_Membres_Menu_News = "News menus" ;
+$Mod_Membres_Menu_Fichiers = "Files menu" ;
+$Mod_Membres_Menu_Liens = "Links menu" ; 
+$Mod_Membres_Menu_Faq = "FAQ menu" ;
+$Mod_Membres_Menu_Photos = "Pictures menu" ;
+// Ajouter à la version 1.2.2
+$Mod_Membres_SC = "Security Code" ;
+$Mod_Membres_SC1 = "Write the security code above in the field below" ;
+$Mod_Membre_Mdp_dif = "The two passord was different. Please, retry." ;
+$Mod_Membre_MyProfil_OK = "Update ok." ;
+?>
