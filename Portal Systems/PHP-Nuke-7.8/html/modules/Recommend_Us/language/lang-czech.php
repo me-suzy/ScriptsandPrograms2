7447bfbebf15b2bf5ab686db80ff3a1a2351ef3b
me-suzy <me.suzana@gmail.com>
2025-08-12 22:52:43 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Recommend_Us/language/lang-czech.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Recommend_Us/language/lang-czech.php
@@ -0,0 +1,37 @@
+<?php
+
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation, please go to the site and send to me        */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+
+define("_SEND","Poslat");
+define("_FYOURNAME","Vae jméno:");
+define("_FYOUREMAIL","Vá e-mail:");
+define("_FFRIENDNAME","Adresátovo jméno(v 5.pádì):");
+define("_FFRIENDEMAIL","Adresátùv e-mail:");
+define("_HELLO","Ahoj");
+define("_YOURFRIEND","Vá pøítel");
+define("_RECOMMEND","Doporuète tuto stránku známým");
+define("_INTSITE","Zajímavá stránka:");
+define("_OURSITE","povauje nai stránku");
+define("_INTSENT","za zajímavou a rozhodl se vám ji poslat.");
+define("_FSITENAME","Jméno stránky:");
+define("_FSITEURL","Adresa:");
+define("_FREFERENCE","Doporuèení na nai stránku bylo odesláno");
+define("_THANKSREC","Díky za doporuèení!");
+define("_SECURITYCODEERROR","<b>ERROR:</b> The security code you wrote is incorrect...<br>Please write it exactly as given bellow:");
+
+?>
