e40f7d38f713b84fb1459c0305cfb2c5d4a6e8c3
me-suzy <me.suzana@gmail.com>
2025-08-12 04:01:27 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/install.sql

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/modules/search/install.sql
@@ -0,0 +1,13 @@
+# --------------------------------------------------------
+#
+# Data for table `modules`
+#
+INSERT INTO modules (modulename, extin, moduledirectory, modulescript, modulesubmit, hascats) VALUES ('Search', 'E', 'search', 'search.php', 'N', 'N');
+
+INSERT INTO modulesettings (modulename, settingname, settingvalue) VALUES ('search', 'addterms', 'any');
+
+
+# --------------------------------------------------------
+#
+# Data for table `specialcontents`
+#
