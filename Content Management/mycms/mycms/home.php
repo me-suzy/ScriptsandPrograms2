294e20133639b3fc1e22be34d3c89b178141742c
me-suzy <me.suzana@gmail.com>
2025-08-12 07:15:39 +0300

    Add Content Management/mycms/mycms/home.php

---
+++ Content Management/mycms/mycms/home.php
@@ -0,0 +1,56 @@
+<table width="100%" height="194" border="1" cellpadding="0" cellspacing="0">
+  <tr> 
+    <td align="left" valign="top" bgcolor="#F7F7F7" class ="phead">
+	
+	<table width="95%" border="0" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td width="814" > 
+		  
+		  <font size="3" face="Verdana, Arial, Helvetica, sans-serif"><strong> 
+            <? stitle($id,$type); ?>
+            </strong></font></td>
+        </tr>
+      </table>
+       <table width="95%" height="25" border="0" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td width="394" height="18" > 
+            <? sheading($id,$type); ?>
+          </td>
+        </tr>
+        <tr> 
+          <td height="4" ></td>
+        </tr>
+      </table>
+      <table width="95%" height="25" border="0" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td width="394" height="18" > 
+            <? simg1($id,$type); ?>
+          </td>
+        </tr>
+        <tr> 
+          <td height="4" ></td>
+        </tr>
+      </table>
+      <table width="95%" height="20" border="0" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td width="394" height="20" > 
+            <? scontent($id,$type); ?>
+          </td>
+        </tr>
+      </table>
+      <br> 
+      <table width="95%" border="0" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td width="33%"> 
+            <? simg2($id,$type); ?>
+          </td>
+          <td width="33%"> 
+            <? simg3($id,$type); ?>
+          </td>
+          <td width="33%">
+            <? simg4($id,$type); ?>
+          </td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
