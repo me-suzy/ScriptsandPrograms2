a14d06f26426e8d4f7e8d49365b604cdc6567a32
me-suzy <me.suzana@gmail.com>
2025-08-12 19:14:19 +0300

    Add Link Indexing/cncat/cncat/lang/lang_eng.php

---
+++ Link Indexing/cncat/cncat/lang/lang_eng.php
@@ -0,0 +1,181 @@
+<?
+$LANG=Array();
+$LANG["charset"]="iso-8859-1";
+
+$LANG["linksnotfound"]="No links was found in this category";
+$LANG["sortby"]="Sort by";
+$LANG["popuarity"]="Popularity";
+$LANG["title"]="Title";
+$LANG["modervote"]="Moderator vote";
+$LANG["main"]="Directory index";
+$LANG["linkcount"]="Links in this category";
+$LANG["addlink"]="Submit Your site";
+$LANG["moderators"]="For moderators";
+$LANG["category"]="Category";
+$LANG["notselected"]="Not selected";
+$LANG["sitetitle"]="Site title";
+$LANG["siteurl"]="Site URL";
+$LANG["sitedescription"]="Description";
+$LANG["email"]="E-Mail";
+$LANG["submit"]="Submit";
+$LANG["newlinks"]="New links";
+$LANG["groupop"]="Group operations";
+
+$LANG["errorsfound"]="Some errors was found";
+$LANG["mustbecat"]="Category not selected.";
+$LANG["mustbeemail"]="E-Mail must be entered.";
+$LANG["mustbeurl"]="URL must be entered.";
+$LANG["mustbetitle"]="Title must be entered.";
+$LANG["mustbedescription"]="Description must be entered";
+
+$LANG["administration"]="administration";
+$LANG["no"]="no";
+$LANG["delete"]="Delete";
+$LANG["asubmit"]="Submit";
+$LANG["tonew"]="To new";
+$LANG["change"]="Edit";
+$LANG["total"]="Total";
+$LANG["new"]="New";
+$LANG["submited"]="Submitted";
+$LANG["deleted"]="Deleted";
+$LANG["editcats"]="Edit categories";
+$LANG["duplinks"]="Duplicated links";
+$LANG["edit"]="Edit";
+
+$LANG["search"]="Search";
+$LANG["notfound"]="Your search did not match any documents";
+$LANG["pagesfound"]="Pages found";
+$LANG["emptyquery"]="Empty query";
+
+$LANG["createcategory"]="Create a new category";
+$LANG["createcategoryinsubcat"]="Create subcategory in category";
+$LANG["rootcat"]="Root category";
+$LANG["categoryname"]="Category title";
+$LANG["add"]="Add";
+$LANG["rename"]="Rename";
+$LANG["deletecategory"]="Delete category";
+$LANG["renamecategory"]="Rename category";
+$LANG["import"]="Import";
+
+$LANG["brokenlinks"]="Broken links";
+$LANG["alreadyexists"]="This link already exists";
+$LANG["brokensure"]="Are you sure this is a broken link?";
+$LANG["yes"]="yes";
+$LANG["no"]="no";
+$LANG["resetbroken"]="Reset broken counter";
+$LANG["broken"]="broken link";
+
+$LANG["placelink"]="You may place link to us at your web site ";
+$LANG["linkwasadded"]="You link was successfully added";
+$LANG["thx"]="<P>Thanks, your link was successfully added to our database and must be approved my moderator during the same time.</P>";
+
+/* new in CNCat 1.3 */
+$LANG["changelink"]="Modify link";
+$LANG["idanddate"]="ID and date";
+$LANG["fromsite"]="Jumps from site";
+$LANG["tosite"]="Jumps to site";
+$LANG["back"]="Back";
+
+/* new in CNCat 1.3.1 */
+$LANG["sync"]="Synchronization";
+$LANG["syncz"]="Synchronyze";
+$LANG["syncdelall"]="Phisically delete all links in the section &quot;Deleted&quot;";
+$LANG["synccount"]="Synchronization of number of links and sections names";
+$LANG["syncjumps"]="Jumps counters";
+$LANG["syncgin"]="Number of jumps to the directory";
+$LANG["syncgout"]="Number of jumps from the directory";
+$LANG["syncreset"]="Reset";
+$LANG["syncavgrowisize"]="Average row length";
+$LANG["syncdelrows"]="Rows deleted";
+$LANG["syncfrom"]="from";
+$LANG["syncmustfree"]="Deleting will free up";
+$LANG["bytes"]="byte";
+
+/* new in CNCat 1.4 */
+$LANG["sortbyin"]="Date";
+$LANG["remove_install"]="Delete install.php before using CNCat.";
+$LANG["plugins"]="Plugins";
+$LANG["plugin_next"]="Proceed";
+$LANG["plugin_example"]="Example of a simple plugin and sample database structure can be found in the <a href=../manual/manual.html#plugin>manual</a>.";
+$LANG["plugin_phpnuke"]="Import of links from PHPNuke directory";
+$LANG["plugin_phpnuke_sync"]="Synchronization";
+$LANG["plugin_phpnuke_finished"]="Finished";
+$LANG["plugin_phpnuke_cat"]="Importing categories...";
+$LANG["plugin_phpnuke_subcat"]="Importing subcategories...";
+$LANG["plugin_phpnuke_links"]="Importing links...";
+$LANG["plugin_export_rubrics"]="Export categories";
+$LANG["plugin_export_links"]="Export links";
+$LANG["plugin_export_design"]="Export design";
+$LANG["plugin_export"]="Export to XML";
+$LANG["plugin_export_download"]="Download XML";
+$LANG["plugin_import"]="Import from XML";
+$LANG["plugin_import_do"]="Import";
+$LANG["plugin_import_rubrics"]="Categories container (cncat_rubrics.xml)";
+$LANG["plugin_import_links"]="Links container (cncat_links.xml)";
+$LANG["plugin_import_skip"]="Skip, if a link with such ID already exists";
+$LANG["plugin_import_overwrite"]="Overwrite, if a link with such ID already exists";
+$LANG["plugin_import_delete"]="Clear directory before import<br>(all links are removed physically, new links are imported into the empty directory)";
+$LANG["plugin_import_finished"]="Import finished successfully.";
+$LANG["plugin_import_ed_links"]="Importing links";
+$LANG["plugin_import_ed_rubrics"]="Importing categories";
+$LANG["plugin_multiadd"]="Multiple links confirmation";
+$LANG["plugin_multiadd_count"]="Add all links at once";
+$LANG["plugin_multiadd_attention"]="if the field is empty, the link will not be added";
+$LANG["plugin_multiadd_modervote"]="Moderator's rating";
+$LANG["plugin_multiadd_add"]="adding link #";
+
+$LANG["secretcodeerror"]="The number displayed in the picture was not entered correctly";
+$LANG["secretcode"]="Enter the number, displayed on the picture";
+
+/* New in 1.5 */
+
+$LANG["mail_desc_1"]="E-mail message, sent when a link is submitted to the directory.";
+$LANG["mail_desc_2"]="E-mail message, sent when a link is confirmed by the moderator.";
+$LANG["mail_desc_3"]="E-mail message, sent when a link is declined by the moderator.";
+$LANG["mail_headers"]="Optional headers";
+$LANG["mail_body"]="Message body";
+$LANG["mail_from"]="From";
+$LANG["mail_subject"]="Subject";
+$LANG["mail_send"]="Send message";
+$LANG["mail_menu_add"]="Submission";
+$LANG["mail_menu_submit"]="Confirmation";
+$LANG["mail_menu_delete"]="Refuse";
+$LANG["mail_title"]="E-mails";
+
+$LANG["tmpl"]="Templates";
+$LANG["tmpl_bmenu"]="Menu";
+$LANG["tmpl_catname"]="Directory title and path";
+$LANG["tmpl_linkstop"]="Part of the document before the link list";
+$LANG["tmpl_linksmiddle"]="Part of the document between the best and regular links";
+$LANG["tmpl_linksbottom"]="Part of the document after link list";
+$LANG["tmpl_linksbit"]="One link";
+$LANG["tmpl_bestlinksbit"]="One best link";
+$LANG["tmpl_newlinkstop"]="Upper part of \"New Links\" section";
+$LANG["tmpl_newlinkstbottom"]="Bottom part of \"New Links\" section";
+$LANG["tmpl_newlinkstbit"]="New link";
+$LANG["tmpl_partstop"]="Upper part of categories section";
+$LANG["tmpl_partsdelimtop"]="Start of new line of categories section";
+$LANG["tmpl_partsdelimbottom"]="End of new line of categories section";
+$LANG["tmpl_partsbottom"]="Bottom part of categories section";
+$LANG["tmpl_partsbit"]="Name of the category";
+$LANG["tmpl_searchform"]="Search form";
+$LANG["tmpl_searchtop"]="Part of document before list of links";
+$LANG["tmpl_searchbottom"]="Part of the document after link list";
+$LANG["tmpl_sortby"]="Sorting section";
+$LANG["tmpl_brokenscript"]="Broken links information script";
+$LANG["tmpl_title_1"]="Link list";
+$LANG["tmpl_title_2"]="Section \"New Links\"";
+$LANG["tmpl_title_3"]="List of categories";
+$LANG["tmpl_title_4"]="Search";
+$LANG["tmpl_title_5"]="Miscellaneuos";
+$LANG["tmpl_about"]="Upper and bottom parts of the document may be changed in files _top.php and _bottom.php.<br>You may use PHP scripts in these files.";
+
+$LANG["links"]="Links";
+$LANG["other"]="Miscelaneous";
+$LANG["plugin_import_templates"]="Design templates container (cncat_design.xml)";
+
+$LANG["wrong_password"]="Incorrect password";
+$LANG["wrong_login"]="Incorrect login";
+$LANG["logout"]="Logout";
+$LANG["nobroken"]="No broken links reports found.";
+?>
