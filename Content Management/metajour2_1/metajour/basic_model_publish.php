855e25eb010619b845dce0769057ab7864e5b092
me-suzy <me.suzana@gmail.com>
2025-08-12 05:54:50 +0300

    Add Content Management/metajour2_1/metajour/basic_model_publish.php

---
+++ Content Management/metajour2_1/metajour/basic_model_publish.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage model
+ */
+
+require_once('basic_model.php');
+
+class basic_model_publish extends basic_model {
+
+	function model() {
+		foreach($this->objectid as $curid) {
+			$obj = owRead($curid);
+			$obj->setpublish($this->data['publish']);
+			$obj->setexpire($this->data['expire']);
+		}
+	}
+	
+}
+
+?>
