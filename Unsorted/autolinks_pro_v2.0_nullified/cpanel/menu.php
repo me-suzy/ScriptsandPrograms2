3a4e6dddf98b0f0af3752d9883cc683f48e020a9
me-suzy <me.suzana@gmail.com>
2025-08-13 00:59:28 +0300

    Add Unsorted/autolinks_pro_v2.0_nullified/cpanel/menu.php

---
+++ Unsorted/autolinks_pro_v2.0_nullified/cpanel/menu.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+<link rel="stylesheet" href="menu.css">
+</head>
+
+<body leftmargin="5" topmargin="5" marginwidth="5" marginheight="5">
+<table width="140" border="0" cellspacing="0" cellpadding="2" align="center">
+  <tr>
+    <td align="center" height="80" valign="top">
+      <img src="title.gif" width="135" height="40"><br>
+      <b>Professional 2.0<br>
+      </b><a href="home.php" target="mainFrame">Control Panel Home</a></td>
+  </tr>
+  <tr>
+    <td bgcolor="#7979BB"><font color="#FFFFFF">Script Settings</font></td>
+  </tr>
+  <tr>
+    <td><a href="nullified.php" target="mainFrame">Nillification info</a><br>
+    <a href="settings.php" target="mainFrame">Change Settings</a></td>
+  </tr>
+  <tr>
+    <td height="10"></td>
+  </tr>
+  <tr>
+    <td bgcolor="#7979BB"><font color="#FFFFFF">Websites</font></td>
+  </tr>
+  <tr>
+    <td><a href="site_add.php" target="mainFrame">Add New</a> | <a href="site_list.php" target="mainFrame">Edit</a><br>
+       <a href="site_select.php" target="mainFrame">Install New Site</a>
+	</td>
+  </tr>
+  <tr>
+    <td height="10"></td>
+  </tr>
+  <tr>
+    <td bgcolor="#7979BB"><font color="#FFFFFF">Referrers</font></td>
+  </tr>
+  <tr>
+    <td><a href="ref_invite.php" target="mainFrame">Invite New</a> | <a href="ref_list.php" target="mainFrame">Edit</a><br>
+      <a href="ref_notice.php" target="mainFrame">Send Notifications</a></td>
+  </tr>
+  <tr>
+    <td height="10"></td>
+  </tr>
+  <tr>
+    <td bgcolor="#7979BB"><font color="#FFFFFF">Categories</font></td>
+  </tr>
+  <tr>
+    <td><a href="cat_add.php" target="mainFrame">Add New</a> | <a href="cat_list.php" target="mainFrame">Edit</a></td>
+  </tr>
+  <tr>
+    <td height="10"></td>
+  </tr>
+  <tr>
+    <td bgcolor="#7979BB"><font color="#FFFFFF">Tags</font></td>
+  </tr>
+  <tr>
+    <td>
+      <p><a href="tag_add.php" target="mainFrame">Add New</a><a href="imgtag_add.php" target="mainFrame"></a> | <a href="tag_list.php" target="mainFrame">Edit</a></p>
+    </td>
+  </tr>
+  <tr>
+    <td height="10"></td>
+  </tr>
+  <tr>
+    <td bgcolor="#7979BB"><font color="#FFFFFF">Customizations</font></td>
+  </tr>
+  <tr>
+    <td><a href="custom_emails.php" target="mainFrame">Customize Emails</a><br>
+      <a href="custom_refarea.php" target="mainFrame"> Customize Ref. Area</a><a href="settings.php" target="mainFrame"></a></td>
+  </tr>
+  <tr>
+    <td height="10"></td>
+  </tr>
+  <tr>
+    <td bgcolor="#7979BB"><font color="#FFFFFF">Statistics</font></td>
+  </tr>
+  <tr>
+    <td>
+      <a href="stats_report.php" target="mainFrame">Generate Statistics</a><br>
+      <a href="logs_report.php" target="mainFrame">Generate IP Logs</a>
+	</td>
+  </tr>
+</table>
+</body>
+</html>
