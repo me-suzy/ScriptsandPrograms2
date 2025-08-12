90912f1661638981b15649f79c1dcb8ee5f54776
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:59 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_subcontent.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_subcontent.php
@@ -0,0 +1,37 @@
+<?
+
+// Form Titles
+$GLOBALS["tFormTitle"]		= 'Gestion du contenu soumis';
+$GLOBALS["tFormTitle2"]		= 'Gestion de la configaration du contenu soumis';
+
+// List Headings
+$GLOBALS["tEdit"]		= 'Editer';
+$GLOBALS["tModuleName"]		= 'Nom du Module';
+$GLOBALS["tModuleTitle"]	= 'Titre du Module';
+$GLOBALS["tAnonPosting"]	= 'Publication Anonyme Autorisée';
+$GLOBALS["tValReq"]		= 'Validation Requise';
+$GLOBALS["tTablename"]		= 'Nom de la Table';
+
+// List Functions
+$GLOBALS["tConfig"]		= 'Configuration';
+
+// Form Field Headings
+$GLOBALS["tModuleID"]		= 'ID du Module';
+$GLOBALS["tRegOnly"]		= 'Utilisateurs Enregistrés Seuls';
+$GLOBALS["tValReg"]		= 'Validation Requise';
+
+// Toggel Modules
+$GLOBALS["tConfirmRelease"] = 'activate / deactivate module';
+$GLOBALS["tReleaseModule"] = 'activate / deactivate module';
+
+// Form Detail Comments and Help Texts
+$GLOBALS["tDetails"]		 = 'Utilisez ce formulaire pour configurer les soumissions des utilisateurs dans le contenu des modules spéciaux des add ons.';
+$GLOBALS["hModuleName"]		 = 'Le Nom du module, un tag utilisé pour une identification en interne.';
+$GLOBALS["hModuleTitle"]	 = 'Le Titre du module apparait sur la page principale pour ce type de contenu.';
+$GLOBALS["hRegOnly"]		 = 'Seuls les Auteurs/Utilisateurs enregistrés peuvent publier ? ou n\'importe quel utilisateur ?';
+$GLOBALS["hValReq"]		 = 'Indique si la validation est requise avant de pouvoir publier un article, ';
+$GLOBALS["hValReq"]		.= 'ex: si la publication dépend de la validation de l\'administrateur.';
+
+// Error Messages
+
+?>
