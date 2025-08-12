8089d4d1025ce4262fbf3eeb429d2d9e3cc32e95
me-suzy <me.suzana@gmail.com>
2025-08-12 12:58:55 +0300

    Add Education/prm-2.1/footer.php

---
+++ Education/prm-2.1/footer.php
@@ -0,0 +1,19 @@
+
+<?php
+echo('<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+</td></tr>
+<TR>
+         <TD class="copyright" width="100%" align="center" bgcolor="#ffffff">Progress Report Manager &middot; Copyright Matt Reider 2004 &middot; <a href="license.txt">Licensed under General Public License</a>
+         </TD>
+        </TR>
+</table>
+</body>
+</html>
+');
+?>
