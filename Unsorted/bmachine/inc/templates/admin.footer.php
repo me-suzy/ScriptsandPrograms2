00d65cab6d826c980f6f9762ddf37d303cd7db2b
me-suzy <me.suzana@gmail.com>
2025-08-13 01:17:50 +0300

    Add Unsorted/bmachine/inc/templates/admin.footer.php

---
+++ Unsorted/bmachine/inc/templates/admin.footer.php
@@ -0,0 +1,23 @@
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+<tr>
+<td width="729" height="7" background="/images/fade-bottom.gif"></td>
+</tr>
+</table>
+<center>&nbsp;</center>
+<p align="center"><font>Powered by </font><a href="http://boastology.com" target="_blank"><font color="#666666">BoastMachine</font></a><font> 
+<? global $ver; echo $ver; ?><br>Developed by </font><a href="http://bnsoft.net" target="_blank"><font color="#666666">BN Soft</font></a></p>
+</body>
+</html>
