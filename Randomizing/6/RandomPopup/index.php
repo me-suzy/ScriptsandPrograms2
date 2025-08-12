f24f01c89b8a75c23aa474cf581556ab9bb56999
me-suzy <me.suzana@gmail.com>
2025-08-12 23:31:38 +0300

    Add Randomizing/6/RandomPopup/index.php

---
+++ Randomizing/6/RandomPopup/index.php
@@ -0,0 +1,24 @@
+<?
+#########################################################
+# Random Popup                                          #
+#########################################################
+#                                                       #
+# Author: Doni Ronquillo                                #
+#                                                       #
+# This script and all included functions, images,       #
+# and documentation are copyright 2003                  #
+# free-php.net (http://free-php.net) unless             #
+# otherwise stated in the module.                       #
+#                                                       #
+# Any copying, distribution, modification with          #
+# intent to distribute as new code will result          #
+# in immediate loss of your rights to use this          #
+# program as well as possible legal action.             #
+#                                                       #
+#########################################################
+   
+   include('inc/config.php');
+	include('inc/header.php');
+ 	include('inc/footer.php');
+    
+?>
