aef60d742ecdb6c5fc5fe30ddf7dda2b28589ce8
me-suzy <me.suzana@gmail.com>
2025-08-12 12:56:19 +0300

    Add Education/phplibrary_v1.0/library/footer.inc.php

---
+++ Education/phplibrary_v1.0/library/footer.inc.php
@@ -0,0 +1,11 @@
+  <tr class="color1" valign="bottom">
+    <td><div align="center"><font size="1" face="Arial, Helvetica, sans-serif">Powered by <a class="color1" href="http://www.sebflipper.com" target="_blank">PHP Item Library</a> <?php echo $library_ver; ?> (Current version is: <?php echo $library_current_ver; ?>)</font></div></td>
+  </tr>
+
+</td>
+</tr>
+</tbody>
+</table>
+
+</body>
+</html>
