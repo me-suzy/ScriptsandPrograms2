5577aea99b957fd25f5218ca4d767feea46f56a4
me-suzy <me.suzana@gmail.com>
2025-08-12 05:59:29 +0300

    Add Content Management/metajour2_1/metajour/app/edocument/core/edocument_edocerrorcodeclass.php

---
+++ Content Management/metajour2_1/metajour/app/edocument/core/edocument_edocerrorcodeclass.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eReklamation
+ * @subpackage core
+ * $Id: edocument_edocerrorcodeclass.php,v 1.1 2005/02/04 05:04:13 jan Exp $
+ */
+
+global $system_path;
+require_once($system_path.'core/basicclass.php');
+
+class edocument_edocerrorcode extends basic {
+
+	function edocument_edocerrorcode() {
+		$this->basic();
+		$this->setobjecttype('edocerrorcode');
+		$this->addcolumn('name',0,UI_STRING);
+		$this->removeview('createvariant');
+		$this->removeview('access');
+		$this->removeview('category');
+	}
+
+}
