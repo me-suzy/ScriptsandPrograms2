876f7912ca9888a24d8f6d309d69355c12e36e3d
me-suzy <me.suzana@gmail.com>
2025-08-12 17:11:53 +0300

    Add Groupware Tools/l4w_v0_5_2/modules/templates/models/fields_definition.inc.php

---
+++ Groupware Tools/l4w_v0_5_2/modules/templates/models/fields_definition.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+  /**
+    * $Id: fields_definition.inc.php,v 1.1 2005/07/05 10:45:02 carsten Exp $
+    *
+    * controls additional field definitions
+    *
+    * @author       Carsten Graef <evandor@gmx.de>
+    * @copyright    evandor media 2004
+    * @package      notes
+    */
+
+    // --- general ---
+
+	$this->entry['type']          = new easy_string ('');
+
+
+?>
