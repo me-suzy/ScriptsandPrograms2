bc375e4be3c9e20b84bc98ce070fa27d15b413c9
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:07 +0300

    Add Content Management/metajour2_1/metajour/lang/currency.en.php

---
+++ Content Management/metajour2_1/metajour/lang/currency.en.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Currencies';
+$LANG['label_name'] = 'Currency name';
+$LANG['label_currency'] = 'Factor';
+$LANG['label_standard'] = '';
+?>
