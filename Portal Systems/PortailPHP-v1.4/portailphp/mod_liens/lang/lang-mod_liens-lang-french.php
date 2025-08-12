b9fae51d1eb336719b1c93f583571243c74c51cc
me-suzy <me.suzana@gmail.com>
2025-08-12 23:17:34 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_liens/lang/lang-mod_liens-lang-french.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_liens/lang/lang-mod_liens-lang-french.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE LIENS
+$Mod_Liens_PlRecent="Les 10 Liens (les + récents)";
+$Mod_Liens_Top="Le top 10 des Liens";
+$Mod_Liens_form_Auteur="Auteur";
+$Mod_Liens_form_Mail="EMail";
+$Mod_Liens_form_Rubrique="Rubrique";
+$Mod_Liens_form_Titre="Titre";
+$Mod_Liens_form_Lien="Lien";
+$Mod_Liens_form_Desc="Description";
+$Mod_Liens_JS_Auteur="Le champ Auteur doit être rempli";
+$Mod_Liens_JS_mail="Le champ EMail doit être rempli";
+$Mod_Liens_JS_Rubrique="Le champ Rubrique doit être rempli";
+$Mod_Liens_JS_Titre="Le champ Titre doit être rempli";
+$Mod_Liens_JS_Lien="Le champ Lien doit être rempli";
+$Mod_Liens_JS_Desc="Le champ Description doit être rempli";
+$Mod_Lien_inde_Categorie="Catégorie";
+$Mod_Liens_inde_Date="Date";
+$Mod_Liens_inde_Titre="Titre";
+$Mod_Liens_inde_Auteur="Référent";
+$Mod_Liens_inde_Clics="Clics";
+?>
