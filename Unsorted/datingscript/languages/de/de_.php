e0ed9b85a2587f7722cf00df371383abe64bbd4d
me-suzy <me.suzana@gmail.com>
2025-08-13 02:19:57 +0300

    Add Unsorted/datingscript/languages/de/de_.php

---
+++ Unsorted/datingscript/languages/de/de_.php
@@ -0,0 +1,484 @@
+<?php 
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.0.3                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 05/02/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               de_.php                          #
+# File purpose            Addition to german langfile      #
+# File created by Sebastian Karkus <Sebastian@Karkus.de>   #
+############################################################
+
+$wg=array(
+'keine Angabe', 
+'maennlich',
+'weiblich'
+);
+
+$wp=array(
+'keine Angabe',
+'Romantik',
+'ernste Beziehung',
+'Freundschaft',
+'ONS'
+);
+
+// Privileged users names
+$wpu=array(
+'Lite member', // Free user
+'Silver member',
+'Gold member',
+'Platinum member');
+// Privileged users timelife (in month)
+$wpt=array(
+'0', // Free user
+'3',
+'6',
+'12');
+// Payment for each member type in dollars
+$wpm=array(
+'0', // Free User
+'19',
+'29',
+'39');
+
+
+$wm=array(
+'keine Angabe', 
+'Single',
+'geschieden',
+'getrennt',
+'verwitwet',
+'verheiratet'
+);
+
+$wmm=array(
+'keine Angabe',
+'Januar',
+'Februar',
+'Maerz',
+'April',
+'Mai',
+'Juni',
+'Juli',
+'August',
+'September',
+'Oktober', // [10]
+'November',
+'Dezember'
+);
+
+$wc=array(
+'keine Angabe',
+'keine',
+'1',
+'2',
+'3',
+'4',
+'5 und mehr'
+);
+
+$wh=array(
+'keine Angabe',
+'127cm',
+'132cm',
+'137cm',
+'142cm',
+'152cm',
+'152cm',
+'157cm',
+'160cm',
+'163cm',
+'166cm', // [10]
+'168cm',
+'171cm',
+'173cm',
+'175cm',
+'178cm',
+'183cm',
+'188cm',
+'193cm',
+'198cm',
+'203cm', // [20]
+'208cm',
+'213cm'
+);
+
+$ww=array(
+'keine Angabe',
+'36kg',
+'39kg',
+'41kg',
+'43kg',
+'45kg',
+'48kg',
+'50kg',
+'52kg',
+'54kg',
+'57kg', // [10]
+'59kg',
+'61kg',
+'63kg',
+'66kg',
+'68kg',
+'70kg',
+'73kg',
+'75kg',
+'77kg',
+'79kg', // [20]
+'82kg',
+'84kg',
+'86kg',
+'89kg',
+'91kg',
+'93kg',
+'95kg',
+'97kg',
+'100kg',
+'102kg', // [30]
+'104kg',
+'107kg',
+'109kg',
+'111kg',
+'113kg',
+'116kg',
+'118kg',
+'120kg',
+'122kg',
+'125kg', // [40]
+'127kg',
+'129kg',
+'131kg',
+'134kg',
+'136kg'
+);
+
+$whc=array(
+'keine Angabe',
+'Schwarz',
+'Blond',
+'Braun',
+'Bruenett',
+'Kastanie',
+'Goldbraun',
+'Dunkelblond',
+'Gold',
+'Rot',
+'Grau', // [10]
+'Silber',
+'Weiss',
+'Kahl',
+'Anderes'
+);
+
+$we=array(
+'keine Angabe',
+'Schwarz',
+'Braun',
+'Blau',
+'Gruen',
+'Grau',
+'andere'
+);
+
+$wet=array(
+'keine Angabe',
+'Weiss',
+'Latino/Latina',
+'Asiatisch',
+'Afroamerikaner/In',
+'Gemischt',
+'Andere'
+);
+
+$wr=array(
+'keine Angabe',
+'Agnostisch',
+'Atheist',
+'Baptisten',
+'Buddhist',
+'Christ',
+'Glaube an Gott',
+'Humanist',
+'Jude',// [10]
+'Katholisch',
+'Lutheraner',
+'Mormone',
+'Muslime',
+'New Age',
+'Nicht-Glaeubiger',
+'Ost-Orthodox',
+'Protestant',
+'Andere'
+);
+
+$ws=array(
+'keine Angabe',
+'Nichtraucher/In',
+'Gelegenheitsraucher/In',
+'Raucher/In'
+);
+
+$wd=array(
+'keine Angabe',
+'Anti-Alkoholiker/In',
+'Gelegenheitstrinker/In',
+'Trinkt gerne'
+);
+
+$wed=array(
+'keine Angabe',
+'Hauptschulabschluss',
+'Realschulabschluss',
+'Fachabitur',
+'Abitur',
+'Studium'
+);
+
+$wu=array(
+'keine Angabe',
+'Bannerwerbung',
+'Zeitschriften',
+'Fernsehen',
+'Suchmaschinen',
+'Empfehlung von Freunden',
+'anderes'
+);
+
+$whr=array(
+'keine Angabe',
+'Steinbock',
+'Wassermann',
+'Fische',
+'Widder',
+'Stier',
+'Zwillinge',
+'Krebs',
+'Loewe',
+'Jungfrau',
+'Waage', // [10]
+'Skorpion',
+'Schuetze'
+);
+
+$wsb=array(
+'Datum absteigend',
+'Datum aufsteigend',
+'Vorname absteigend',
+'Vorname aufsteigend',
+'Alter absteigend',
+'Alter aufsteigend',
+'Groesse absteigend',
+'Griesse aufsteigend',
+'Gewicht absteigend',
+'Gewicht aufsteigend'
+);
+/* 
+$wsb only for search - you can`t add new or remove - 
+only translate!
+*/
+
+$wrg=array(
+'Alle Zeitraeume',
+'letzten 7 Tage',
+'letzter Monat',
+'letzten 3 Monate',
+'letztes jahr'
+);
+
+$wrgv=array(
+'', // Not require enter this value for All period
+'7', 
+'31',
+'92',
+'365'
+);
+
+
+$wst=array(
+'Alle Mitglieder',
+'Mitglieder auf Mailcheck wartend',
+'Mitglieder auf Registrierungscheck wartend',
+'Mitglieder auf Updatecheck wartend',
+'Auf Admins Löschbestaetigung wartend',
+'Aktive Mitglieder'
+);
+
+$wcr=array(
+' keine Angabe', // First space is required for sorting
+'Afghanistan',
+'Albania',
+'Algeria',
+'Andorra',
+'Angola',
+'Antigua and arbuda',
+'Argentina', 
+'Armenia',
+'Australia',
+'Austria',
+'Azerbaijan',
+'Bahamas',
+'Bahrain',
+'Bangladesh',
+'Barbados',
+'Belarus', 
+'Belgium',
+'Bolivia',
+'Bosnia and Herzegovina',
+'Botswana',
+'Brazil',
+'Brunei Darussalam',
+'Bulgaria',
+'Burundi',
+'Cambodia',
+'Cameroon',
+'Canada',
+'Central African Republic',
+'Chad',
+'Chile',
+'China',
+'Colombia',
+'Congo',
+'Costa Rica',
+'Ivory Coast',
+'Croatia',
+'Cuba',
+'Cyprus',
+'Czech Republic',
+'Denmark',
+'Dominican Republic',
+'Ecuador',
+'Egypt',
+'El Salvador',
+'Equatorial Guinea',
+'Estonia',
+'Ethiopia',
+'Fiji',
+'Finland',
+'France',
+'Gabon',
+'Gambia',
+'Georgia',
+'Germany', 
+'Ghana',
+'Gibraltar',
+'Greece',
+'Greenland',
+'Grenada',
+'Guadeloupe',
+'Guatemala',
+'Guinea-Bissau',
+'Guinea', 
+'Guyana',
+'Haiti',
+'Honduras',
+'Hong Kong',
+'Hungary',
+'Iceland',
+'India',
+'Indonesia',
+'Iran', 
+'Iraq',
+'Ireland',
+'Israel',
+'Italy',
+'Jamaica',
+'Japan',
+'Jordan',
+'Kazakhstan',
+'Kenya',
+'North Korea',
+'South Korea',
+'Kuwait',
+'Kyrgyzstan',
+'Laos',
+'Latvia',
+'Lebanon',
+'Lesotho',
+'Liberia',
+'Libya',
+'Liechtenstein',
+'Lithuania',
+'Luxembourg',
+'Macedonia',
+'Madagascar',
+'Malaysia',
+'Mali', 
+'Malta',
+'Mauritania',
+'Mexico',
+'Moldova',
+'Monaco', 
+'Mongolia',
+'Morocco',
+'Mozambique',
+'Namibia',
+'Nepal',
+'Netherlands', 
+'New Zealand',
+'Nicaragua',
+'Nigeria',
+'Niger',
+'Norway',
+'Oman', 
+'Pakistan',
+'Panama',
+'Papua New Guinea',
+'Paraguay',
+'Peru',
+'Philippines',
+'Poland',
+'Portugal', 
+'Puerto Rico',
+'Qatar',
+'Romania',
+'Russian Federation',
+'Rwanda',
+'Samoa',
+'San Marino',
+'Saudi Arabia',
+'Senegal',
+'Sierra Leone',
+'Singapore',
+'Slovak Republic',
+'Slovenia', 
+'Solomon Islands',
+'Somalia',
+'South Africa',
+'Spain',
+'Sri Lanka',
+'Sudan',
+'Swaziland',
+'Sweden',
+'Switzerland',
+'Syria',
+'Taiwan',
+'Tajikistan',
+'Tanzania',
+'Thailand',
+'Tunisia',
+'Turkey',
+'Turkmenistan',
+'Uganda',
+'Ukraine', 
+'United Arab Emirates',
+'United Kingdom',
+'United States',
+'Uruguay',
+'Uzbekistan',
+'Vatican City State',
+'Venezuela',
+'Viet Nam',
+'Yemen',
+'Yugoslavia',
+'Zaire',
+'Zambia',
+'Zimbabwe' 
+);
+// No more 255 countries in this list!!!
+// You can change this list to cities of your country
+// Be carefull!
+?>
