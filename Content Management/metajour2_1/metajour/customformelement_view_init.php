7af10ed645cb198a682284d9217cbf3776e7359e
me-suzy <me.suzana@gmail.com>
2025-08-12 05:56:57 +0300

    Add Content Management/metajour2_1/metajour/customformelement_view_init.php

---
+++ Content Management/metajour2_1/metajour/customformelement_view_init.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('basic_view_split.php');
+
+class customformelement_view_init extends basic_view_split {
+	
+	function view() {
+		return $this->splitView('customformelement','customform');
+	}
+	
+}
+
+?>
