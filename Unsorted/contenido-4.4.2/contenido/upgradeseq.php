95707ef1436840019e48be4ab0d1ca29a7080271
me-suzy <me.suzana@gmail.com>
2025-08-13 01:54:28 +0300

    Add Unsorted/contenido-4.4.2/contenido/upgradeseq.php

---
+++ Unsorted/contenido-4.4.2/contenido/upgradeseq.php
@@ -0,0 +1,37 @@
+<?php
+
+/*****************************************
+* File      :   upgrade.php
+* Project   :   Contenido
+* Descr     :   Contenido upgrade script
+*
+* Authors   :   Timo A. Hummel
+*
+* Created   :   20.06.2003
+* Modified  :   20.06.2003
+*
+* © four for business AG, www.4fb.de
+******************************************/
+
+$cfg["path"]["classes"] = getcwd() . "/classes/";
+$cfg["path"]["includes"] = getcwd() . "/includes/";
+$cfg["path"]["conlib"] = getcwd() . "/../conlib/";
+
+include_once ($cfg["path"]["conlib"] . 'prepend.php3');
+include_once ($cfg["path"]["includes"] . 'cfg_sql.inc.php');
+include_once ($cfg["path"]["includes"] . 'functions.general.php');
+include_once ($cfg["path"]["includes"] . 'functions.str.php');
+include_once ($cfg["path"]["includes"] . 'functions.con.php');
+include_once ($cfg["path"]["includes"] . 'functions.database.php');
+include_once ($cfg["path"]["conlib"] . 'local.php');
+
+
+$db = new DB_Upgrade;
+
+$sql = "SHOW TABLES";
+$db->query($sql);
+		
+while ($db->next_record())
+{
+	dbUpdateSequence($prefix."_sequence", $db->f(0));	
+}
