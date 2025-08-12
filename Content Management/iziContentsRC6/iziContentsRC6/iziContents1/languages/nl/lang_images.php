ed31f7d433f6df82acd0ddcc2a765ae4b33158a0
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:38 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_images.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_images.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'Beheer afbeeldingen';
+$GLOBALS["tFormTitle1"] = 'upload afbeeldingen/creëeren afbeelding subdirectories';
+$GLOBALS["tFormTitle2"] = 'Upload afbeelding';
+$GLOBALS["tFormTitle3"] = 'Creëer Subdirectory';
+
+//  List Functions
+$GLOBALS["tAddNewImage"] = 'Nieuwe afbeelding toevoegen';
+$GLOBALS["tDeleteImage"] = 'Verwijder afbeelding';
+$GLOBALS["tDeleteImageDir"] = 'Verwijder afbeelding subdirectory';
+
+//  List Headings
+$GLOBALS["tImageName"] = 'Naam afbeelding';
+
+//  Form Fields
+$GLOBALS["tImageFilename"] = 'Bestandsnaam afbeelding';
+$GLOBALS["tSubdirectoryname"] = 'Subdirectory naam';
+
+//  Form Buttons
+$GLOBALS["bUpload"] = 'Upload!';
+$GLOBALS["bCreateDir"] = 'Creëer!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] = 'U moet een bestand selecteren om te uploaden.';
+$GLOBALS["eNoDir"] = 'U moet een directory naam ingeven.';
+$GLOBALS["eDirAlreadyExists"] = 'de subdirectory bestaat al';
+$GLOBALS["eZeroByteFile"] = 'Lengte bestand is 0 bytes';
+$GLOBALS["eNoFileUpload"] = 'Bestand kon niet succesvol worden geupload.';
+$GLOBALS["eInvalidFileSize"] = 'Het bestand is te groot om te kunnen uploaden';
+$GLOBALS["eInvalidFileType"] = 'Bestan is niet een geldig afbeeldingsbestand';
+$GLOBALS["eInvalidMimeType"] = 'MIME type van Bestand is niet een geldig type afbeelding';
+
+?>
