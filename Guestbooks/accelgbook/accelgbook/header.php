92df9028116a30bdcad5f33b729f9acaaf2b4303
me-suzy <me.suzana@gmail.com>
2025-08-12 17:26:43 +0300

    Add Guestbooks/accelgbook/accelgbook/header.php

---
+++ Guestbooks/accelgbook/accelgbook/header.php
@@ -0,0 +1,54 @@
+<?php require_once('config.php'); ?>
+<?php
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = (!get_magic_quotes_gpc()) ? addslashes($theValue) : $theValue;
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+
+$editFormAction = $_SERVER['PHP_SELF'];
+if (isset($_SERVER['QUERY_STRING'])) {
+  $editFormAction .= "?" . htmlentities($_SERVER['QUERY_STRING']);
+}
+
+if ((isset($_POST["MM_insert"])) && ($_POST["MM_insert"] == "addMessage")) {
+  $insertSQL = sprintf("INSERT INTO accelgbook (firstName, lastName, country, email, website, comment, `date`, marker) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                       GetSQLValueString($_POST['firstName'], "text"),
+                       GetSQLValueString($_POST['lastName'], "text"),
+                       GetSQLValueString($_POST['country'], "text"),
+                       GetSQLValueString($_POST['email'], "text"),
+                       GetSQLValueString($_POST['website'], "text"),
+                       GetSQLValueString($_POST['comment'], "text"),
+                       GetSQLValueString($_POST['date'], "text"),
+                       GetSQLValueString($_POST['date2'], "date"));
+
+  mysql_select_db($database, $accelgbook);
+  $Result1 = mysql_query($insertSQL, $accelgbook) or die(mysql_error());
+
+  $insertGoTo = "accelgbook_added.php?date=".$HTTP_POST_VARS['date']."";
+  if (isset($_SERVER['QUERY_STRING'])) {
+    $insertGoTo .= (strpos($insertGoTo, '?')) ? "&" : "?";
+    $insertGoTo .= $_SERVER['QUERY_STRING'];
+  }
+  header(sprintf("Location: %s", $insertGoTo));
+}
+?>
