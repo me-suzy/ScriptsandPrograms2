3d7c0bc184b54a15d0911e7b889df2d2a677be56
me-suzy <me.suzana@gmail.com>
2025-08-12 09:28:24 +0300

    Add Development Tools/fuselogic/fuselogic/modules/test1/module_setting.php

---
+++ Development Tools/fuselogic/fuselogic/modules/test1/module_setting.php
@@ -0,0 +1,8 @@
+<?php
+
+$FL_MODULE_SETTING['module_name'] = 'test1';
+$FL_MODULE_SETTING['sub_module']['test'] = 'test.php';
+$FL_MODULE_SETTING['sub_module']['default'] = 'test.php';
+$FL_MODULE_SETTING['sub_module']['phpinfo'] = 'phpinfo.php';
+
+?>
