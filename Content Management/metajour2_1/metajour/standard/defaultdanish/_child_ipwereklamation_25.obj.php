645ffa0f73c097f9bd2f059030283f602eefd7e0
me-suzy <me.suzana@gmail.com>
2025-08-12 07:13:17 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ipwereklamation_25.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/_child_ipwereklamation_25.obj.php
@@ -0,0 +1,27 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 25;
+function objfile_25 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "IPW eReklamation";
+$objdata['subname'] = "";
+$objdata['extension'] = "";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "<P>Effektiv reklamationsbehandling med IPW eReklamation giver fordele i hele virksomheden:</P>
+<UL>
+<LI>Salgsafdelingen yder bedre service til kunderne med brug af færre ressourcer 
+<LI>Produktions- og kvalitetsafdelingen får fuld overblik over eksterne og interne fejlkilder 
+<LI>Indkøbsafdelingen får specifik information om den enkelte leverandørs kvalitet 
+<LI>Økonomisk reduceres de administrative omkostninger - og hurtig pay-back sikres&nbsp;gennem systemets konkurrencedygtige pris. 
+<LI>På tværs af organisationen, uanset geografisk placering og sprog, har relevante medarbejdere adgang til informationerne og / eller til at arbejde i systemet.</LI></UL>
+<P>IPW eReklamation dækker ethvert behov - fra det absolut beskedne til det meget omfattende. Det kan leveres som stand-alone eller integreret med virksomhedens økonomisystem og andre databaser, hvor nyttige informationer kan hentes eller skal leveres. </P>
+<P>IPW eReklamation leveres altid fuldkommen tilpasset virksomhedens behov.</P>
+<P>IPW eReklamation er et købe-produkt fra <A href=\"http://www.ipwsystems.dk/\" target=_blank>IPW Systems A/S</A>.</P>
+<P>Kontakt os for at høre mere om mulighederne for din virksomhed.</P>";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
