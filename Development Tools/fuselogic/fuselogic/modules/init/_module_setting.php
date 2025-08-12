05b6fd877f332f7377076c8dd5a5513a009051fb
me-suzy <me.suzana@gmail.com>
2025-08-12 09:24:39 +0300

    Add Development Tools/fuselogic/fuselogic/modules/init/_module_setting.php

---
+++ Development Tools/fuselogic/fuselogic/modules/init/_module_setting.php
@@ -0,0 +1,16 @@
+<?php
+
+$FL_MODULE_SETTING['module_name'] = 'init';
+$FL_MODULE_SETTING['sub_module']['home'] = 'home.php';
+
+$FL_MODULE_SETTING['sub_module']['modules_info'] = 'modules_info.php';
+
+$FL_MODULE_SETTING['sub_module']['_prepend'] = 'prepend_append.php';
+$FL_MODULE_SETTING['sub_module']['_append'] = 'prepend_append.php';
+
+$FL_MODULE_SETTING['sub_module']['_modules'] = 'modules.php';
+
+//$FL_MODULE_SETTING['sub_module']['update_modules'] = 'update.php';
+$FL_MODULE_SETTING['sub_module']['modules_update'] = 'update.php';
+
+?>
