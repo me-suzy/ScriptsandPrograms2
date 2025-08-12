da068cc3ca4fd0560cd6b97e6d8222977650e314
me-suzy <me.suzana@gmail.com>
2025-08-12 23:18:47 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_news/lang/lang-mod_news-lang-spanish.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_news/lang/lang-mod_news-lang-spanish.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric claced@m6net.fr http://www.yoopla.net/portailphp/
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// MODULE NEWS
+$Mod_News_PostPar="Enviado por";
+$Mod_News_Le="El";
+$Mod_News_PlRecent="Los 10 Artículos (los + recientes)";
+$Mod_News_PlLu="Los 10 Artículos (los + leidos)";
+$Mod_News_inde_Date="Fecha";
+$Mod_News_inde_Sujets="Asuntos";
+$Mod_News_inde_Auteurs="Autores";
+$Mod_News_inde_Lectures="Lecturas";
+$Mod_News_inde_Categorie="Categoría";
+$Mod_News_Form_Categorie="Categoría";
+$Mod_News_Form_Sujet="Asunto";
+$Mod_News_Form_Date="Fecha";
+$Mod_News_Form_Corps="Contenido (HTML)";
+$Mod_News_JS_Categorie="El campo Categoría tiene que ser rellenado";
+$Mod_News_JS_Sujet="El campo Asunto tiene que ser rellenado";
+$Mod_News_JS_Contenu="El campo Contenido tiene que ser rellenado";
+
+?>
