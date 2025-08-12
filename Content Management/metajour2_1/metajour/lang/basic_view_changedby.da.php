82536b51550d7553066cdf470cda2080e322ac1a
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:29 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_changedby.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_changedby.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Redaktør';
+$LANG['text_1'] = 'Angiv redaktør for de markerede poster';
+$LANG['text_2'] = 'Redigeret af: ';
+?>
