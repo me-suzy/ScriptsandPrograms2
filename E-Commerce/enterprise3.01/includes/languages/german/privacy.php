1d28f566916f7ebd4c014c77fc2753fc49b817fe
me-suzy <me.suzana@gmail.com>
2025-08-12 12:00:37 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/privacy.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/privacy.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Privatsph&auml;re und Datenschutz');
+define('HEADING_TITLE', 'Privatsph&auml;re und Datenschutz');
+
+define('TEXT_INFORMATION', 'F&uuml;gen Sie hier Ihre Informationen &uuml;ber Privatsph&auml;re und Datenschutz ein.');
+?>
