e1ca0f8767ca123acfd42202c1b8dacf63e0cf55
me-suzy <me.suzana@gmail.com>
2025-08-12 05:59:26 +0300

    Add Content Management/metajour2_1/metajour/app/edocument/edocument_edocform_view_listclosed.php

---
+++ Content Management/metajour2_1/metajour/app/edocument/edocument_edocform_view_listclosed.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eProject
+ * @subpackage view
+ * $Id: edocument_edocform_view_listclosed.php,v 1.1 2005/02/04 06:20:44 jan Exp $
+ */
+
+require_once($system_path.'basic_view_list.php');
+
+class edocument_edocform_view_listclosed extends basic_view_list {
+
+	function setFilters() {
+		parent::setFilters();
+		$obj =& $this->_listobj;
+		$obj->setfilter_search('status', CASE_CLOSED, EQUAL);
+	}
+
+	function loadLanguage() {
+		parent::loadLanguage();
+		$this->loadLangFile('edocform_view_listclosed');
+	}
+
+}
+
+?>
