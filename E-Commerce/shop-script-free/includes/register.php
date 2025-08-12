96d26c6cfb150ac706f85ef1ce2fdb2dab250c4f
me-suzy <me.suzana@gmail.com>
2025-08-12 12:39:16 +0300

    Add E-Commerce/shop-script-free/includes/register.php

---
+++ E-Commerce/shop-script-free/includes/register.php
@@ -0,0 +1,17 @@
+<?php
+/*****************************************************************************
+ *                                                                           *
+ * Shop-Script FREE                                                          *
+ * Copyright (c) 2005 WebAsyst LLC. All rights reserved.                     *
+ *                                                                           *
+ ****************************************************************************/
+
+	// registration form
+
+	if (isset($register)) //customers registration
+	{
+
+			$smarty->assign("main_content_template", "notavailable.tpl.html");
+	}
+
+?>
