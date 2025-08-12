89794aa434762d874b1858f897823adf1a35ae80
me-suzy <me.suzana@gmail.com>
2025-08-12 11:59:28 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/affiliate_payment.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/affiliate_payment.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Partnerprogramm');
+define('HEADING_TITLE', 'Partnerprogramm: Provisionszahlungen');
+
+define('TEXT_AFFILIATE_HEADER', 'Ihre Provisionszahlungen:');
+
+define('TABLE_HEADING_DATE', 'Datum');
+define('TABLE_HEADING_PAYMENT', 'Provision');
+define('TABLE_HEADING_STATUS', 'Status');
+define('TABLE_HEADING_PAYMENT_ID','lfd-Nr.');
+define('TEXT_DISPLAY_NUMBER_OF_PAYMENTS', 'angezeigte Provisionszahlungen <b>%d</b> bis <b>%d</b> (von <b>%d</b> insgesamt)');
+define('TEXT_INFORMATION_PAYMENT_TOTAL', 'Ihre Gesamtprovision bisher:');
+define('TEXT_NO_PAYMENTS', 'Bis jetzt wurden noch keine Provisionszahlungen registriert.');
+?>
