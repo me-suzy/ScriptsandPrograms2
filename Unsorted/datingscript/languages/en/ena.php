b7e15ed43753e7265f9dc6d16e02398e919383ee
me-suzy <me.suzana@gmail.com>
2025-08-13 02:20:22 +0300

    Add Unsorted/datingscript/languages/en/ena.php

---
+++ Unsorted/datingscript/languages/en/ena.php
@@ -0,0 +1,122 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.0.3                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 05/02/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               ena.php                          #
+# File purpose            Additional langfile for admin    #
+# File created by         AzDG <support@azdg.com>          #
+############################################################
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Incorrect login or password - try again',//1
+'Administration Area',//2
+'Admin Login',//3
+'Admin Password',//4
+'Enter',//5
+'Access closed for non admins',//6
+'Main Page',//7
+'Clear hits',//8
+'Repair&Optimize',//9
+'User Management',//10
+'FAQ Management',//11
+'Maillist',//12
+'Exit',//13
+'You has been succesfully exited from Administration Area<br><br>Thanks for using our script',//14
+'Total mails',//15
+'Sended mails',//16
+'The following part',//17
+'{0} mails has been sent to users',//18
+'You have unread messages in {0}',//19 {0} - SiteName (C_SNAME)
+'Found {0} members',//20
+'Maillist manager',//21
+'With status:',//22
+'Mail options and fields:',//23
+'Mail type',//24
+'From Mail',//25
+'Mail subject',//26
+'Mail body',//27
+'Confirm about results',//28
+'Return back',//29
+'Send mails',//30
+'The unfinished dispatches - {0}',//31
+'Subject : {0} [Sended to {1} users] :::>',//32
+'Finish this dispatch',//33
+'Text',//34
+'HTML',//35
+'Preview',//36
+'Users with unreaded messages - ',//37
+'Total unreaded messages - ',//38
+'Last message id - ',//39
+'Send confirm letters to users with unreaded messages',//40
+'Profile successfully updated',//41
+'FAQ question has been succesfully created',//42
+'Create new',//43
+'Please enter question',//44
+'Please enter answer',//45
+'FAQ Management',//46
+'Create new FAQ category',//47
+'Edit FAQ category',//48
+'Delete FAQ category',//49
+'Create FAQ',//50
+'Select FAQ Category',//51
+'FAQ question',//52
+'FAQ answer',//53
+'First of all you must create FAQ categories',//54
+'Please enter category name',//55
+'FAQ category create',//56
+'New category created:',//57
+'FAQ category name',//58
+'FAQ category edit',//59
+'Edit',//60
+'Category has been changed',//61
+'FAQ category edit',//62
+'FAQ has been deleted',//63
+'FAQ category delete',//64
+'Note : Be carefull. With delete category, you will remove all questions in this category.',//65
+'Delete',//66
+'0 - Wait email check<br>1 - Wait check after register<br>2 - Wait check after update<br>3,8 - Wait allow to remove(without,with show)<br>7 - User is active<br>',//67
+'Database has been repaired and optimizied',//68
+'User with ID#={0} has been succesfully deleted from database',//69
+'Hits has been cleared',//70
+'Successful allowed user with id={0}',//71
+'IP',//72
+'Requirement [0 - 255]',//73
+'Status - (BE CAREFULL)',//74
+'Allow',//75
+'User management',//76
+'Total active users :',//77
+'Users waiting profile activization after register',//78
+'Users waiting profile activization after update',//79
+'Users waiting remove',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
