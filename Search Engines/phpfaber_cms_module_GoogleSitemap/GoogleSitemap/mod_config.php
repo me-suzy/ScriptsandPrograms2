5cd3daf2a7de96fb553c0d4a58c2553fcc1de909
me-suzy <me.suzana@gmail.com>
2025-08-12 23:38:28 +0300

    Add Search Engines/phpfaber_cms_module_GoogleSitemap/GoogleSitemap/mod_config.php

---
+++ Search Engines/phpfaber_cms_module_GoogleSitemap/GoogleSitemap/mod_config.php
@@ -0,0 +1,30 @@
+<?php
+
+// -----------------------------------------------------------------------------
+//
+// phpFaber CMS v.1.0
+// Copyright(C), phpFaber LLC, 2004-2005, All Rights Reserved.
+// E-mail: products@phpfaber.com
+//
+// All forms of reproduction, including, but not limited to, internet posting,
+// printing, e-mailing, faxing and recording are strictly prohibited.
+// One license required per site running phpFaber CMS.
+// To obtain a license for using phpFaber CMS, please register at
+// http://www.phpfaber.com/i/products/cms/
+//
+// 10:51 PM 09/04/2005
+//
+// -----------------------------------------------------------------------------
+
+if (!defined('INDEX_INCLUDED')) die('<code>You cannot access this file directly..</code>');
+
+$_MOD = array(
+  'name'  => 'Google Sitemap',
+  'descr' => 'Worrying about your site ranking? Use this module to help Google to index your site!',
+);
+
+$_MOD_CFG = array(
+  'MAP_FNAME' => 'google_sitemap.xml',
+);
+
+?>
