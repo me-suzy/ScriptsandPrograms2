fce8fc76be500efb73c0d98c01d93c487c79d6db
me-suzy <me.suzana@gmail.com>
2025-08-12 18:48:58 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/editlang_3.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/editlang_3.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%FILENAME%></td></tr>
+<tr><td class='frame'><textarea name='lgcontent' style='height:300'><%LANGFILE%></textarea></td></tr>
+<tr><td class='frame'><input type='submit' value='<%GO%>'></td></tr>
+</table>
