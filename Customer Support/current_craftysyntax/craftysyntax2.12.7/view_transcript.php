cfffee58f725109d314630e8c898b40d8040bc81
me-suzy <me.suzana@gmail.com>
2025-08-12 08:41:01 +0300

    Add Customer Support/current_craftysyntax/craftysyntax2.12.7/view_transcript.php

---
+++ Customer Support/current_craftysyntax/craftysyntax2.12.7/view_transcript.php
@@ -0,0 +1,57 @@
+<?php
+//===========================================================================
+//* --    ~~                Crafty Syntax Live Help                ~~    -- *
+//===========================================================================
+//           URL:   http://www.craftysyntax.com/    EMAIL: ericg@craftysyntax.com
+//         Copyright (C) 2003-2005 Eric Gerdes   (http://www.craftysyntax.com )
+// --------------------------------------------------------------------------
+// $              CVS will be released with version 3.1.0                   $
+// $    Please check http://www.craftysyntax.com/ or REGISTER your program for updates  $
+// --------------------------------------------------------------------------
+// NOTICE: Do NOT remove the copyright and/or license information any files. 
+//         doing so will automatically terminate your rights to use program.
+//         If you change the program you MUST clause your changes and note
+//         that the original program is Crafty Syntax Live help or you will 
+//         also be terminating your rights to use program and any segment 
+//         of it.        
+// --------------------------------------------------------------------------
+// LICENSE:
+//     This program is free software; you can redistribute it and/or
+//     modify it under the terms of the GNU General Public License
+//     as published by the Free Software Foundation; 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program in a file named LICENSE.txt .
+// -------------------------------------------------------------------------- 
+// BIG NOTE:
+//     At the time of the release of this version of CSLH, Version 3.1.0 
+//     which is a more modular, extendable , skinable version of CSLH
+//     was being developed.. please visit http://www.craftysyntax.com to see if it was released!  
+//===========================================================================
+require_once("admin_common.php");
+require_once("data_functions.php");
+require_once("ctabbox.php"); 
+
+validate_session($identity);
+
+?>
+<link title="new" rel="stylesheet" href="style.css" type="text/css">
+<meta http-equiv="Content-Type" content="text/html; charset=<?php echo $lang['charset']; ?>" >
+<?php
+
+ $query = "SELECT * FROM livehelp_transcripts WHERE recno=".intval($UNTRUSTED['view']);
+ $data = $mydatabase->query($query);
+ print "<table width=600 bgcolor=FFFFFF border=1><tr><td>";
+ $data = $data->fetchRow(DB_FETCHMODE_ASSOC);
+ print $data['sessiondata'];
+ print "<hr>";
+ print $data['transcript'];
+ print "</td></tr></table></td></tr></table>";
+ 
+print "<a href=javascript:window.close()>" . $lang['txt40'] . "</A>"; 
+
+?>
