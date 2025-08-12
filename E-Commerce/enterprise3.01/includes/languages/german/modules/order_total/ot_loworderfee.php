65246ae0fc835c6612ace58e12a82e2198cd3b24
me-suzy <me.suzana@gmail.com>
2025-08-12 12:01:04 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/modules/order_total/ot_loworderfee.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/modules/order_total/ot_loworderfee.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ORDER_TOTAL_LOWORDERFEE_TITLE', 'Mindermengenzuschlag');
+  define('MODULE_ORDER_TOTAL_LOWORDERFEE_DESCRIPTION', 'Zuschlag bei Unterschreitung des Mindestbestellwertes');
+?>
