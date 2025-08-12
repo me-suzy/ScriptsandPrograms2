8e824ae0b740fd1804aadb579d5490eacc535311
me-suzy <me.suzana@gmail.com>
2025-08-12 06:14:15 +0300

    Add Content Management/metajour2_1/metajour/extension/listing/lang/listing.da.php

---
+++ Content Management/metajour2_1/metajour/extension/listing/lang/listing.da.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['name'] = 'Konfiguration af Liste';
+$LANG['label_name'] = 'Konfigurationsnavn';
+$LANG['label_classname'] = 'Tabel';
+$LANG['label_allowview'] = 'Tillad detaljevisning';
+$LANG['label_category'] = 'Kun poster fra kategorierne';
+$LANG['text_1'] = 'Felt';
+$LANG['text_2'] = 'Rækkefølge';
+$LANG['text_3'] = 'Kolonne';
+$LANG['text_4'] = 'Slet';
+$LANG['text_5'] = 'Tilføj felt';
+
+?>
