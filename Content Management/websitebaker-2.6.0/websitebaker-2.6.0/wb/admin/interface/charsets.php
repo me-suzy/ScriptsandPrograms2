955e3a13d02e3ad47d1ed52c328e7bd73b61fbf5
me-suzy <me.suzana@gmail.com>
2025-08-12 08:10:46 +0300

    Add Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/admin/interface/charsets.php

---
+++ Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/admin/interface/charsets.php
@@ -0,0 +1,43 @@
+<?php
+
+// $Id: charsets.php 256 2005-11-28 08:33:29Z ryan $
+
+/*
+
+ Website Baker Project <http://www.websitebaker.org/>
+ Copyright (C) 2004-2005, Ryan Djurovich
+
+ Website Baker is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Website Baker is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Website Baker; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+/*
+
+Charset list file
+
+This file is used to generate a list of charsets for the user to select
+
+*/
+
+if(!defined('WB_URL')) {
+	header('Location: ../index.php');
+}
+
+// Create array
+$CHARSETS = array();
+$CHARSETS['utf-8'] = 'Unicode (utf-8)';
+$CHARSETS['iso-8859-1'] = 'Latin 1 (iso-8859-1)';
+
+?>
