a9ca61fe4f41a2ba2627f8c2c9d077a00df84fee
me-suzy <me.suzana@gmail.com>
2025-08-12 11:47:09 +0300

    Add E-Commerce/enterprise3.01/includes/counter.php

---
+++ E-Commerce/enterprise3.01/includes/counter.php
@@ -0,0 +1,27 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  $counter_query = escs_db_query("select startdate, counter from " . TABLE_COUNTER);
+
+  if (!escs_db_num_rows($counter_query)) {
+    $date_now = date('Ymd');
+    escs_db_query("insert into " . TABLE_COUNTER . " (startdate, counter) values ('" . $date_now . "', '1')");
+    $counter_startdate = $date_now;
+    $counter_now = 1;
+  } else {
+    $counter = escs_db_fetch_array($counter_query);
+    $counter_startdate = $counter['startdate'];
+    $counter_now = ($counter['counter'] + 1);
+    escs_db_query("update " . TABLE_COUNTER . " set counter = '" . $counter_now . "'");
+  }
+
+  $counter_startdate_formatted = strftime(DATE_FORMAT_LONG, mktime(0, 0, 0, substr($counter_startdate, 4, 2), substr($counter_startdate, -2), substr($counter_startdate, 0, 4)));
+?>
