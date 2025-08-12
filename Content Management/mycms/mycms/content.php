a4a2d8c6f41b64cf75ac7e6cca4b8a44bae9d09d
me-suzy <me.suzana@gmail.com>
2025-08-12 07:15:33 +0300

    Add Content Management/mycms/mycms/content.php

---
+++ Content Management/mycms/mycms/content.php
@@ -0,0 +1,64 @@
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <tr> 
+    <td align="center" height = "4"></td>
+  </tr>
+  <tr> 
+    <td align="center"> 
+      <? s_sponsors2($type,$id); ?>
+    </td>
+  </tr>
+</table>
+<br>
+
+<table width="100%" height="92" border="0" cellpadding="0" cellspacing="0">
+  <tr> 
+    <td width="77%" height="90" align="left" valign="top" bgcolor="#F7F7F7" class ="phead"><table width="98%" border="0" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td width="814" > <font size="3" face="Verdana, Arial, Helvetica, sans-serif"><strong> 
+            <? stitle($id,$type); ?>
+            </strong></font></td>
+        </tr>
+      </table>
+      <table width="98%" height="25" border="0" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td width="948" height="18" > 
+            <? sheading($id,$type); ?>
+          </td>
+        </tr>
+        <tr> 
+          <td height="4" ></td>
+        </tr>
+      </table>
+      <table width="98%" height="20" border="0" cellpadding="0" cellspacing="0">
+        <tr> 
+          <td width="394" height="20" > 
+            <? scontent($id,$type); ?>
+          </td>
+        </tr>
+      </table> <br> </td>
+    <td width="23%" align="left" valign="top" bgcolor="#F7F7F7" class ="phead"><table width="99%" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td height="22"> 
+            <? simg1($id,$type); ?>
+          </td>
+        </tr>
+        <tr> 
+          <td height="5"></td>
+        </tr>
+        <tr>
+          <td>
+            <? simg2($id,$type); ?>
+          </td>
+        </tr>
+        <tr> 
+          <td height="5"> </td>
+        </tr>
+        <tr> 
+          <td height="14"> 
+            <? simg3($id,$type); ?>
+          </td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+
