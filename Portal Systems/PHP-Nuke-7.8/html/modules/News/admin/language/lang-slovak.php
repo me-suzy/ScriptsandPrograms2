ecec64d0ab0430f51a97fcf217d15321ad613b47
me-suzy <me.suzana@gmail.com>
2025-08-12 22:49:56 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/News/admin/language/lang-slovak.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/News/admin/language/lang-slovak.php
@@ -0,0 +1,107 @@
+<?php
+
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation go to the my website and send to me          */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+
+
+define("_NEWS","News");
+define("_FUNCTIONS","Funkcie");
+define("_YES","Áno");
+define("_NO","Nie");
+define("_ALLTOPICS","Vetky témy");
+define("_CATEGORY","Kategória");
+define("_SAVECHANGES","Uloi? zmeny");
+define("_OK","Ok!");
+define("_SAVE","Uloi?");
+define("_NOSUBJECT","Chýba predmet");
+define("_ARTICLES","Èlánky");
+define("_ALL","Vetko");
+define("_AREYOUSURE","Naozaj chcete vloi? URL? Otestovali ste funkènos? adresy?");
+define("_SELECTTOPIC","Výber témy");
+define("_OPTION","Nastavenie");
+define("_AUTHOR","Autor");
+define("_NAME","Meno");
+define("_DATE","Dátum");
+define("_TITLE","Názov");
+define("_HOUR","hodina");
+define("_EDITCATEGORY","Upravi? kategóriu");
+define("_ARTICLEADMIN","Administrácia èlánkov");
+define("_ADDARTICLE","Prida? nový èlánok");
+define("_STORYTEXT","Text èlánku");
+define("_EXTENDEDTEXT","Rozírený text");
+define("_ARESUREURL","(Chcete vloi? URL? Otestovali si jeho platnos??)");
+define("_PUBLISHINHOME","Publikova? na úvodnú stránku?");
+define("_ONLYIFCATSELECTED","Funguje len keï nie je vybraná kategória <i>èlánky</i>");
+define("_ADD","Prida?");
+define("_PROGRAMSTORY","Chcete naprogramova? publikovanie tohto èlánku?");
+define("_NOWIS","Je");
+define("_DAY","Deò");
+define("_UMONTH","Mesiac");
+define("_YEAR","Rok");
+define("_PREVIEWSTORY","Prehliadnu? èlánok");
+define("_POSTSTORY","Posla? èlánok");
+define("_REMOVESTORY","Ste si istý: Vymaete èlánok ID #");
+define("_ANDCOMMENTS","a vetky jeho komentáre?");
+define("_CATEGORIESADMIN","Administrácia kategórií");
+define("_CATEGORYADD","Prida? novú kategóriu");
+define("_CATNAME","Názov kategórie");
+define("_NOARTCATEDIT","Nemôete upravova? kategóriu <i>èlánky</i>");
+define("_ASELECTCATEGORY","Vybra? kategóriu");
+define("_CATEGORYNAME","Názov kategórie");
+define("_DELETECATEGORY","Vymaza? kategóriu");
+define("_SELECTCATDEL","Vyberte kategóriu na zmazanie");
+define("_CATDELETED","Kategória bola zmazaná!");
+define("_WARNING","Pozor");
+define("_THECATEGORY","v kategórii");
+define("_HAS","je");
+define("_STORIESINSIDE","èlánkov");
+define("_DELCATWARNING1","Môete ZMAZA? túto kategóriu a vetky jej èlánky a komentáre");
+define("_DELCATWARNING2","alebo môete PREMIESTNI? vetky èlánky do novej kategórie.");
+define("_DELCATWARNING3","Èo chcete spravi??");
+define("_YESDEL","Ano! ZMAZA? vetko!");
+define("_NOMOVE","Nie! PREMIESTNI? èlánky");
+define("_MOVESTORIES","Premiestni? èlánky do novej kategórie");
+define("_ALLSTORIES","VETKY èlánky z kategórie");
+define("_WILLBEMOVED","budú premiestnené.");
+define("_SELECTNEWCAT","Prosím, vyberte novú kategóriu");
+define("_MOVEDONE","Gratulujem! Premiestnenie je dokonèené!");
+define("_CATEXISTS","Táto kategória u existuje!");
+define("_CATSAVED","Kategória uloená!");
+define("_GOTOADMIN","Spä? do administrátorskej èasti");
+define("_CATADDED","Nová kategória bola pridaná!");
+define("_AUTOSTORYEDIT","Upravi? naprogramovaný èlánok");
+define("_NOTES","Poznámky");
+define("_CHNGPROGRAMSTORY","Vyber nový dátum pre tento èlánok:");
+define("_SUBMISSIONSADMIN","Administrácia poslaných èlánkov");
+define("_DELETESTORY","Vymaza? èlánok");
+define("_EDITARTICLE","Upravi? èlánok");
+define("_NOSUBMISSIONS","iadne nové èlánky");
+define("_NEWSUBMISSIONS","Prevzatie nových èlánkov");
+define("_NOTAUTHORIZED1","Nemáte oprávnenie zasahova? do tohto èlánku!");
+define("_NOTAUTHORIZED2","Nemôete upravova?/maza? èlánky, ktoré ste nepublikovali");
+define("_POLLTITLE","Názov ankety");
+define("_POLLEACHFIELD","Do políèok zadajte kadú monú odpoveï.");
+define("_ACTIVATECOMMENTS","Aktivova? komentáre k tomuto èlánku?");
+define("_LANGUAGE","Jazyk");
+define("_ATTACHAPOLL","Pripoji? anketu k tomuto èlánku");
+define("_LEAVEBLANKTONOTATTACH","(Nechajte prázdne, ak nechcete pripoji? anketu.<br> Pozn: K automatickým a naprogramovaným èlánkom nie je moné pripoji? anketu.)");
+define("_USERPROFILE","User Profile");
+define("_EMAILUSER","Email User");
+define("_SENDPM","Send Private Message");
+
+
+?>
