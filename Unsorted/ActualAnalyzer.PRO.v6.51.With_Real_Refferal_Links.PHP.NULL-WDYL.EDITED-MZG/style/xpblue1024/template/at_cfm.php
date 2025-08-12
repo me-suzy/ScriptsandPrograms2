2b1353dfb0c1dc9ea70b7ba6cf91e6e39a22b6f0
me-suzy <me.suzana@gmail.com>
2025-08-13 00:29:42 +0300

    Add Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/style/xpblue1024/template/at_cfm.php

---
+++ Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/style/xpblue1024/template/at_cfm.php
@@ -0,0 +1,159 @@
+<?php
+
+$cmess=<<<CMESS
+<table width=975 border=0 cellspacing=0 cellpadding=0 align=center class=bgcol>
+
+<tr height=29>
+<td width=7><img width=7 height=29 border=0 src="%%RF%%style/%%STYLE%%/image/ttopl.gif"></td>
+<td width=961 background="%%RF%%style/%%STYLE%%/image/ttop.gif"><div class=tframe>%%HEADER%%</div></td>
+<td width=7><img width=7 height=29 border=0 src="%%RF%%style/%%STYLE%%/image/ttopr.gif"></td>
+</tr>
+
+<tr height=20 valign=middle>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenl.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+<td width=961 align=center>
+<div class=tareawsubt>
+<div class=tborder>
+<div class=vstable>%%CONFIRM%%</div>
+</div>
+</div>
+</td>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenr.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+</tr>
+
+<tr height=21 valign=middle>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenl.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+<td>
+<div class=tbarea>
+<table border=0 cellspacing=0 cellpadding=0 align=center>
+<tr height=21>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bleft.gif"></td>
+<td background="%%RF%%style/%%STYLE%%/image/bcen.gif"><div class=tbutton><a TABINDEX=1 href="%%SCRIPT%%.php" onclick='return false'><span onclick='FormIdExtParam(admin,"%%PGID%%","back","%%ACT%%")'>%%BACK%%</span></a></div></td>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bright.gif"></td>
+<td width=40>&nbsp;</td>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bleft.gif"></td>
+<td background="%%RF%%style/%%STYLE%%/image/bcen.gif"><div class=tbutton><a TABINDEX=2 href="%%SCRIPT%%.php" onclick='return false'><span onclick='FormIdExtParam(admin,"%%PGID%%","confirm","%%PARAM%%")'>%%NEXT%%</span></a></div></td>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bright.gif"></td>
+</tr>
+</table>
+</div>
+</td>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenr.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+</tr>
+
+<tr height=3>
+<td width=7><img width=7 height=3 border=0 src="%%RF%%style/%%STYLE%%/image/tbotl.gif"></td>
+<td height=3 width=961><img src="%%RF%%style/%%STYLE%%/image/tbot.gif" height=3 width=961></td>
+<td width=7><img width=7 height=3 border=0 src="%%RF%%style/%%STYLE%%/image/tbotr.gif"></td>
+</tr>
+
+</table>
+<br>
+
+CMESS;
+
+$topg=<<<TOPG
+<table width=975 border=0 cellspacing=0 cellpadding=0 align=center class=bgcol>
+
+<tr height=29>
+<td width=7><img width=7 height=29 border=0 src="%%RF%%style/%%STYLE%%/image/ttopl.gif"></td>
+<td width=961 background="%%RF%%style/%%STYLE%%/image/ttop.gif"><div class=tframe>%%HEADER%%</div></td>
+<td width=7><img width=7 height=29 border=0 src="%%RF%%style/%%STYLE%%/image/ttopr.gif"></td>
+</tr>
+
+<tr height=20 valign=middle>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenl.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+<td width=961 align=center>
+<div class=tareawsubt>
+<div class=tborder>
+<div class=vstable>%%CONFIRM%%&nbsp;&nbsp;'<i>%%GROUP%%</i>'&nbsp;?</div>
+</div>
+</div>
+</td>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenr.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+</tr>
+
+<tr height=21 valign=middle>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenl.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+<td>
+<div class=tbarea>
+<table border=0 cellspacing=0 cellpadding=0 align=center>
+<tr height=21>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bleft.gif"></td>
+<td background="%%RF%%style/%%STYLE%%/image/bcen.gif"><div class=tbutton><a TABINDEX=1 href="%%SCRIPT%%.php" onclick='return false'><span onclick='FormIdExtParam(admin,"%%PGID%%","back","%%ACT%%")'>%%BACK%%</span></a></div></td>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bright.gif"></td>
+<td width=40>&nbsp;</td>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bleft.gif"></td>
+<td background="%%RF%%style/%%STYLE%%/image/bcen.gif"><div class=tbutton><a TABINDEX=2 href="%%SCRIPT%%.php" onclick='return false'><span onclick='FormIdExtParam(admin,"%%PGID%%","confirm","%%PARAM%%")'>%%NEXT%%</span></a></div></td>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bright.gif"></td>
+</tr>
+</table>
+</div>
+</td>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenr.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+</tr>
+
+<tr height=3>
+<td width=7><img width=7 height=3 border=0 src="%%RF%%style/%%STYLE%%/image/tbotl.gif"></td>
+<td height=3 width=961><img src="%%RF%%style/%%STYLE%%/image/tbot.gif" height=3 width=961></td>
+<td width=7><img width=7 height=3 border=0 src="%%RF%%style/%%STYLE%%/image/tbotr.gif"></td>
+</tr>
+
+</table>
+<br>
+
+TOPG;
+
+$topp=<<<TOPP
+<table width=975 border=0 cellspacing=0 cellpadding=0 align=center class=bgcol>
+
+<tr height=29>
+<td width=7><img width=7 height=29 border=0 src="%%RF%%style/%%STYLE%%/image/ttopl.gif"></td>
+<td width=961 background="%%RF%%style/%%STYLE%%/image/ttop.gif"><div class=tframe>%%HEADER%%</div></td>
+<td width=7><img width=7 height=29 border=0 src="%%RF%%style/%%STYLE%%/image/ttopr.gif"></td>
+</tr>
+
+<tr height=20 valign=middle>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenl.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+<td width=961 align=center>
+<div class=tareawsubt>
+<div class=tborder>
+<div class=vstable>%%CONFIRM%%&nbsp;&nbsp;'<a href="%%URL%%" title="%%PAGE%%" target=_blank><code><i>%%PAGESHORT%%</i></code></a>'&nbsp;?</div>
+</div>
+</div>
+</td>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenr.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+</tr>
+
+<tr height=21 valign=middle>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenl.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+<td>
+<div class=tbarea>
+<table border=0 cellspacing=0 cellpadding=0 align=center>
+<tr height=21>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bleft.gif"></td>
+<td background="%%RF%%style/%%STYLE%%/image/bcen.gif"><div class=tbutton><a TABINDEX=1 href="%%SCRIPT%%.php" onclick='return false'><span onclick='FormIdExtParam(admin,"%%PGID%%","back","%%ACT%%")'>%%BACK%%</span></a></div></td>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bright.gif"></td>
+<td width=40>&nbsp;</td>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bleft.gif"></td>
+<td background="%%RF%%style/%%STYLE%%/image/bcen.gif"><div class=tbutton><a TABINDEX=2 href="%%SCRIPT%%.php" onclick='return false'><span onclick='FormIdExtParam(admin,"%%PGID%%","confirm","%%PARAM%%")'>%%NEXT%%</span></a></div></td>
+<td width=3><img width=3 height=21 border=0 src="%%RF%%style/%%STYLE%%/image/bright.gif"></td>
+</tr>
+</table>
+</div>
+</td>
+<td width=7 background="%%RF%%style/%%STYLE%%/image/tcenr.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=7></td>
+</tr>
+
+<tr height=3>
+<td width=7><img width=7 height=3 border=0 src="%%RF%%style/%%STYLE%%/image/tbotl.gif"></td>
+<td height=3 width=961 background="%%RF%%style/%%STYLE%%/image/tbot.gif"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width=3></td>
+<td width=7><img width=7 height=3 border=0 src="%%RF%%style/%%STYLE%%/image/tbotr.gif"></td>
+</tr>
+
+</table>
+<br>
+
+TOPP;
+
+?>
