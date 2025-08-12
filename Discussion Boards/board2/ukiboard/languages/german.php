f930ad0257bdedc704399af9b8e51a43db0a8834
me-suzy <me.suzana@gmail.com>
2025-08-12 10:28:12 +0300

    Add Discussion Boards/board2/ukiboard/languages/german.php

---
+++ Discussion Boards/board2/ukiboard/languages/german.php
@@ -0,0 +1,56 @@
+<?php
+
+$lang['Encoding'] = "iso-8859-1";
+$lang['Administrator'] = "Administrator";
+$lang['Home'] = "Home";
+$lang['Settings'] = "Einstellungen";
+$lang['Language'] = "Sprache";
+$lang['Theme'] = "Thema";
+$lang['Adminemail'] = "Administrator's E-Mail";
+$lang['Perpage'] = "Themen pro Seite";
+
+$lang['Category'] = "Kategorie";
+$lang['Posts'] = "Eintr&auml;ge";
+$lang['Page'] = "Seite";
+
+$lang['Search'] = "Suche";
+$lang['Admin'] = "Konfigurieren";
+
+$lang['Name'] = "Dein Name";
+$lang['Email'] = "E-Mail";
+$lang['Post_subject'] = "&Uuml;berschrift";
+$lang['Password'] = "Passwort";
+
+$lang['Passwordold'] = "Altes Passwort";
+$lang['Passwordnew'] = "Neues Passwort";
+$lang['Passwordcon'] = "Passwort bestätigen";
+
+$lang['Fill'] = "Pflichtfeld";
+$lang['Confirm'] = "bestätigen";
+$lang['Send'] = "absenden";
+$lang['Post'] = "eintragen";
+$lang['Searching'] = "suchen";
+$lang['Searchenter'] = "Suchbegriffe";
+
+$lang['Author'] = "Autor";
+$lang['Date'] = "Datum";
+$lang['Back'] = "zur&uuml;ck";
+
+$lang['Search_keyword'] = "Suchbegriff";
+$lang['Where_search'] = "Wo?";
+$lang['Topics_found'] = "gefundene Eintr&auml;ge";
+
+$lang['Delete'] = "l&ouml;schen";
+$lang['Edit'] = "bearbeiten";
+$lang['Create'] = "Neu";
+$lang['Order'] = "Bestellen";
+$lang['Yes'] = "Ja";
+
+$lang['Title'] = "Titel";
+$lang['Description'] = "Beschreibung";
+
+$lang['Deletesure'] = "Sicher?";
+$lang['Changename'] = "Name &auml;ndern";
+$lang['Changepassword'] = "Passwort &auml;ndern";
+
+?>
