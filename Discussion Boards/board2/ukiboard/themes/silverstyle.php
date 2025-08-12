581520a1c3dfb06a8e48363cfeac87bc65db1401
me-suzy <me.suzana@gmail.com>
2025-08-12 10:28:47 +0300

    Add Discussion Boards/board2/ukiboard/themes/silverstyle.php

---
+++ Discussion Boards/board2/ukiboard/themes/silverstyle.php
@@ -0,0 +1,20 @@
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+
+function MenuOver(element)
+{
+	if(element.style){
+		element.style.backgroundColor='#BB0033';
+		element.style.cursor='hand';
+	};
+};
+
+function MenuOut(element)
+{
+	if(element.style) element.style.backgroundColor='#990033';
+};
+
+window.defaultStatus="UKiBoard";
+
+//-->
+</SCRIPT>
