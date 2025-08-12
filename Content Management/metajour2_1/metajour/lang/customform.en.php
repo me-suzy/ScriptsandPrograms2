f67e133412d06f73cab2f1d0cf2f75cacaa983c6
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:14 +0300

    Add Content Management/metajour2_1/metajour/lang/customform.en.php

---
+++ Content Management/metajour2_1/metajour/lang/customform.en.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Forms';
+$LANG['label_name'] = 'Name (a-z)';
+$LANG['label_description'] = 'Description';
+$LANG['label_eventto'] = 'Event mail to';
+$LANG['label_createevent'] = 'Create event';
+$LANG['label_createtemplate'] = 'Create template';
+$LANG['label_createdocument'] = 'Create document';
+$LANG['label_uploadpageid'] = 'Upload document';
+?>
