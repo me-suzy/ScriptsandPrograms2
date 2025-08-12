d3a526d6c8f2f32c46e2854b67c223498d6987ab
me-suzy <me.suzana@gmail.com>
2025-08-13 01:46:59 +0300

    Add Unsorted/cms master/cms-admin/libs/lang-files.php

---
+++ Unsorted/cms master/cms-admin/libs/lang-files.php
@@ -0,0 +1,36 @@
+<?php
+//=============================================================================
+//
+//     CMS Master :: Content management system
+//     File: /cms-admin/libs/lang-files.php
+//     Version: 1.0
+//     Created date: 28 September 2003
+//     Modification date: 28 September 2003
+//
+//============================================================================
+
+class LangFiles {
+
+    var $header_files_list = "List of files";
+    
+    var $table_file_name = "Name";
+    var $table_file_size = "Size";
+    var $table_file_date = "Date";
+    var $table_file_cmd = "Action";
+    var $table_file_add = "Add file";
+
+    var $button_upload = "Upload";
+    
+    var $cmd_file_del = "Delete file";
+    var $cmd_file_view = "View file";
+    
+    var $msg_confirm_delete_file = "Confirm Delete file";
+    var $msg_delete_file_ok = "File deleted successfully";
+    var $msg_upload_file_ok = "File uploaded successfully";
+    var $msg_no_files = "There are no files";
+    var $msg_file_exists = "This File name already exists.";
+    
+    var $error_file_empty = "Error. Please choose file to upload.";
+}
+
+?>
