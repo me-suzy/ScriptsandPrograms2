ca450d53b8785d6cfa5f5b2618fa33be6a4825ee
me-suzy <me.suzana@gmail.com>
2025-08-12 23:33:26 +0300

    Add Randomizing/randomlink/admin/login.php

---
+++ Randomizing/randomlink/admin/login.php
@@ -0,0 +1,10 @@
+<html>
+<form method="POST" action="authenticate.php">
+Type Username Here: <input type="text" name="linkadmin" size="15"><br>
+Type Password Here: <input type="password" name="password" size="15"><br>
+<input type="submit" value="submit" name="submit">
+
+
+
+</form>
+</html>
