e3c5fcf38e6011cd2c401e468512b18e1609bd9a
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:42 +0300

    Add Content Management/metajour2_1/metajour/lang/email.en.php

---
+++ Content Management/metajour2_1/metajour/lang/email.en.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'To';
+$LANG['label_subject'] = 'Subject';
+$LANG['label_content'] = 'Content';
+$LANG['name'] = 'Emails';
+?>
