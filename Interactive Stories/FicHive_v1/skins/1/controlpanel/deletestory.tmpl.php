3fe48616c6636fae7d6ce83fd9f440575adb46e6
me-suzy <me.suzana@gmail.com>
2025-08-12 18:49:53 +0300

    Add Interactive Stories/FicHive_v1/skins/1/controlpanel/deletestory.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/controlpanel/deletestory.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='frame'><%WARN%></td></tr>
+<tr><td class='frame'><select name='sid' size='16'><%STORYLIST%></select></td></tr>
+<tr><td class='frame'><input type='submit' value='<%GO%>' onClick='javascript: return confirm("<%AREYOUSURE%>");'></td></tr>
+</table>
