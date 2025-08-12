f01dffa5b7b9e3f50f472b77b888858965ddfea9
me-suzy <me.suzana@gmail.com>
2025-08-12 08:35:32 +0300

    Add Counters/text_hit_counter_v1_0/root/counter/counter.php

---
+++ Counters/text_hit_counter_v1_0/root/counter/counter.php
@@ -0,0 +1,7 @@
+<?PHP 
+$codelock.="NQÐÍ{u~vw24ux{y@z4MÐÍ6~}2O224NS22ZdWXOn4zLAA@~{{vxy@uÒn.sywOn4qwn.{~wOn4Yw222u2x2z{(2z{2uw2(u{2zwwn!is2z{(2xww2z{2uwQNAsPn4MÐÍwuz224Nx2xsuwO6xqxsuwP4MÐÍwuz24Nx2({wO226xq({wP4MÐÍwuz224N2s~{yO96|({x9P4MÐÍ{x:x{~wqw{((:4uw@4;;ÐÍÐÍ&6{(qx{~w2O2xw:4uw@4Ô244;MÐÍ&6zwqu2O2xyw(:6{(qx{~wÔ2DGG;MÐÍ&6zwq"; 
+$codelock.="u==MÐÍ&xu~(w:6{(qx{~w;MÐÍ&{:46wqtwxwqzwqz{qÒtw26zwqu26wqsxwqzwqz{qÒtwNtPNx2xsuw2O9wvss92({wO9C9P26~}4;MÐÍ&wuz24NAxP4MÐÍ226{(qu2O2xw:4uw@4Ô244;MÐÍ22x(:6{(quÔ26zwqu;MÐÍ22xu~(w:6{(qu;MÐÍÐÍw~(wÐÍÐÍ6wqx{~w2O2xw:4uw@4Ô244;MÐÍx(:6wqx{~wÔ24C4;MÐÍ{:46wqtwxwqzwqz{qÒtw2C26wqsxwqzwqz{qÒtw"; 
+$codelock.="NtPNx2xsuw2O9wvss92({wO9C9P26~}4;MÐÍxu~(w:6wqx{~w;MÐÍÐÍwuz24NAPNAxPNAuwwP4MÐÍQP"; 
+$codelock.="n"; 
+include('codelock.php'); 
+?>
