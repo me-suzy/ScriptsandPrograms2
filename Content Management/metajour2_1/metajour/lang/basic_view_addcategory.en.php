0689228b8bc9cfcab087372cac1d898f4f16c8d0
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:17 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_addcategory.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_addcategory.en.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Add to category';
+$LANG['text_1'] = 'Are you sure that you want to add the selected records to the category?';
+?>
