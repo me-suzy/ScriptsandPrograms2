ad734ea1c5f3eaaf5c0cf273f4bb4a563b12aa70
me-suzy <me.suzana@gmail.com>
2025-08-12 13:49:06 +0300

    Add Email Systems/DSNewsletter/archive.php

---
+++ Email Systems/DSNewsletter/archive.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>Archives</title>
+</head>
+
+<body>
+<?
+include("header.php");
+?>
+<u>Archives:</u><br><br>
+<a href="01_.php">Issue Code 01_</a><br>
+<a href="02_.php">Issue Code 02_</a><br>
+<a href="03_.php">Issue Code 03_</a><br>
+<a href="04_.php">Issue Code 04_</a><br>
+<a href="05_.php">Issue Code 05_</a><br>
+<a href="06_.php">Issue Code 06_</a><br>
+<a href="07_.php">Issue Code 07_</a><br>
+<a href="08_.php">Issue Code 08_</a><br>
+<a href="09_.php">Issue Code 09_</a><br>
+<a href="10_.php">Issue Code 010_</a><br>
+<a href="11_.php">Issue Code 011_</a><br>
+<a href="12_.php">Issue Code 012_</a><br><br>
+
+<?
+include("footer.php");
+?>
+</body>
+</html>
