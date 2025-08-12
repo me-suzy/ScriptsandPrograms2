4a5923314c0cb215bb6353baad6e44d0e23e4b4d
me-suzy <me.suzana@gmail.com>
2025-08-12 09:31:13 +0300

    Add Development Tools/fuselogic/fuselogic/modules/tools/scripts/php_compat.php

---
+++ Development Tools/fuselogic/fuselogic/modules/tools/scripts/php_compat.php
@@ -0,0 +1,8 @@
+<?php
+if(!function_exists('ob_tidyhandler')){
+    require_once('functions/function.file_get_contents.php');
+    require_once('functions/function.file_put_contents.php');
+    require_once('functions/function.ob_tidyhandler.php');
+    
+}
+?>
