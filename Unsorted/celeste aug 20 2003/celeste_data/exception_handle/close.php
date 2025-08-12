2e76698d3f685cfc9d1afa13b454981e3f6b1c07
me-suzy <me.suzana@gmail.com>
2025-08-13 01:29:49 +0300

    Add Unsorted/celeste aug 20 2003/celeste_data/exception_handle/close.php

---
+++ Unsorted/celeste aug 20 2003/celeste_data/exception_handle/close.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Close Now...</title>
+<link type="text/css" href="style.css" rel="stylesheet">
+</head>
+<body>
+
+<table cellpadding='0' cellspacing='0' border='0' width="95%" align='center' height='85%'>
+  <tr align='center' valign='middle'>
+    <td>
+    <table cellpadding='10' cellspacing='0' border='0' width="80%" align='center'>
+    <tr>
+       <td valign='middle' align='center' bgcolor='#F1F1F1' id='redirect'>
+        <b>The board had been closed.</b><br>
+        <br><br>        <?=SET_BOARD_CLOSE_MESSAGE?>
+       </td>
+    </tr>
+    </table>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
