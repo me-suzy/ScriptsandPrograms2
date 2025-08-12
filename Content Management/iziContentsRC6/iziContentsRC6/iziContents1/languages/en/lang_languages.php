7bb94fd5b8615a54ce9a22b26b0227aa048b9851
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:32 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_languages.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_languages.php
@@ -0,0 +1,54 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintain languages';
+$GLOBALS["tFormTitle2"] = 'add a new language';
+
+//  List Headings
+$GLOBALS["tLanguageCode"] = 'Language Code';
+$GLOBALS["tLanguage"] = 'Language';
+$GLOBALS["tCharSet"] = 'Character Set';
+$GLOBALS["tDirection"] = 'Direction';
+$GLOBALS["tEnabled"] = 'Enabled';
+$GLOBALS["tFileName"] = 'Language file name';
+$GLOBALS["tFileNameM"] = 'Module file name';
+
+//  List Functions
+$GLOBALS["tAddNew"] = 'Enable language';
+$GLOBALS["tViewLanguage"] = 'View language settings';
+$GLOBALS["tEditLanguage"] = 'Edit language settings';
+$GLOBALS["tViewFiles"] = 'View language files';
+$GLOBALS["tEditFiles"] = 'Edit language files';
+$GLOBALS["tViewFile"] = 'View language file';
+$GLOBALS["tEditFile"] = 'Edit language file';
+$GLOBALS["tRet_Languages"] = 'Return to list of languages';
+$GLOBALS["tToggleLanguage"] = 'activate/deactivate Language ';
+$GLOBALS["defaultLangError"] = 'Language cannot be disabled because they are set to DEFAULT';
+
+//  Form Block Titles
+$GLOBALS["thLanguageGeneral"] = 'Language Details';
+$GLOBALS["thMainFiles"] = 'ezContents Main Files';
+$GLOBALS["thModuleFiles"] = 'Module Files';
+$GLOBALS["thJumpRef"] = '-- Select Block --';
+$GLOBALS["thJumptoBottom"] = 'Jump to Bottom of Form';
+$GLOBALS["thJumptoTop"] = 'Jump to Top of Form';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'This form lets you define language names and character sets.';
+$GLOBALS["hLanguage"] = 'This is name of the language.';
+$GLOBALS["hCharSet"] = 'This is the character set for this language.';
+$GLOBALS["hFileName"] = 'Select the language file to load.';
+$GLOBALS["hDirection"] = 'Is this language written left-to-right like English, or right-to-left like Arabic?';
+
+$GLOBALS["tRtL"] = 'Right to Left';
+$GLOBALS["tLtR"] = 'Left to Right';
+$GLOBALS["tRtLs"] = 'rtl';
+$GLOBALS["tLtRs"] = 'ltr';
+
+//  Error Messages
+$GLOBALS["eNoLanguage"] = 'Language name can not be left empty.';
+$GLOBALS["eNoFile"] = 'You must specify a language file.';
+$GLOBALS["eLanguageExists"] = 'This language already exists.';
+$GLOBALS["eNoEnableReady"] = 'No Language filesets are ready to be enabled.';
+
+?>
