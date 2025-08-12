64bba04cfd444bcb2a23ae17947e7db18ff32634
me-suzy <me.suzana@gmail.com>
2025-08-13 01:18:02 +0300

    Add Unsorted/bmachine/inc/templates/summary.inc.php

---
+++ Unsorted/bmachine/inc/templates/summary.inc.php
@@ -0,0 +1,41 @@
+<?php
+
+// DONOT TOUCH THE WORD " $tb=<<<EOF " below!
+// Enter your contents between <<<EOF and EOF;
+
+$tb=<<<EOF
+<table border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td>
+<span class="date">$date</span>
+</td>
+</tr>
+<tr>
+<td>
+<a href="index.php?id=$in"><span class="title">$title</span></a>
+</td>
+</tr>
+<tr>
+<td>
+<span class="content">$smr</span>
+</td>
+</tr>
+<tr>
+<td>
+<table border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td>
+<br>
+<span class="t_small">Posted by : </span><a href="mailto:$a_email"><span class="aname">$a_name</span></a>$cmn
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
+<img src="images/bar.gif">
+
+EOF;
+
+// DONOT TOUCH THE WORD " EOF; " above!
+?>
