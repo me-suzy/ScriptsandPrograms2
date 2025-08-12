3a19c34058bc4cfeb5db847f85eacbe9c2443332
me-suzy <me.suzana@gmail.com>
2025-08-13 00:24:05 +0300

    Add Unsorted/Abc.eStore.v3.0.nulled.by.[GTT]/Abc.eStore.v3.0.nulled.by.[GTT]/admin/footer.inc.php

---
+++ Unsorted/Abc.eStore.v3.0.nulled.by.[GTT]/Abc.eStore.v3.0.nulled.by.[GTT]/admin/footer.inc.php
@@ -0,0 +1,16 @@
+<!-- /content -->
+</td></tr>
+</table>
+
+<table border="0" cellspacing="0" cellpadding="0" width="100%">
+<tr><td bgcolor="#cccccc"><img src="images/z.gif" width="1" height="1" border="0" hspace="0" vspace="0"></td></tr>
+
+<tr>
+<td align="right">&nbsp;&nbsp;&nbsp;<br><br></td>
+</tr>
+
+</table>
+
+
+</body>
+</html>
