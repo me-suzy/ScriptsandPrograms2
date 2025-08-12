88b51101726fee9ba7f748cac055babcb01ba4fd
me-suzy <me.suzana@gmail.com>
2025-08-12 06:00:21 +0300

    Add Content Management/metajour2_1/metajour/app/eproject/lang/eproject_basic_view_menu.en.php

---
+++ Content Management/metajour2_1/metajour/app/eproject/lang/eproject_basic_view_menu.en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ * $Id: eproject_basic_view_menu.en.php,v 1.1 2005/02/16 05:04:16 jan Exp $
+ */
+
+$LANG['menu_eprojectcreate'] = 'Create new';
+$LANG['menu_eprojectactive'] = 'Active projects';
+$LANG['menu_eprojectclosed'] = 'Closed projects';
+
+?>
