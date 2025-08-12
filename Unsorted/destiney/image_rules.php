4425a246621ad70421bef567f2aa6a924fdc9891
me-suzy <me.suzana@gmail.com>
2025-08-13 02:26:27 +0300

    Add Unsorted/destiney/image_rules.php

---
+++ Unsorted/destiney/image_rules.php
@@ -0,0 +1,81 @@
+<?php
+include("./admin/config.php");
+include("$include_path/common.php");
+include("$include_path/$table_file");
+include("$include_path/doc_head.php");
+include("$include_path/styles.php");
+
+$final_output .= <<<FO
+</head>
+<body bgcolor="$page_bg_color">
+<table border="0" cellpadding="0" cellspacing="0" width="$total_width" align="center">
+<tr>
+	<td colspan="3" width="100%" valign="bottom">
+	<table border="0" cellpadding="0" cellspacing="0" width="100%">
+	<tr>
+		<td valign="middle" class="dc">$title_image</td>
+		<td align="right" valign="bottom">
+FO;
+
+include("$include_path/logged_status.php");
+
+$final_output .= <<<FO
+		</td>
+	</tr>
+	</table>
+	</td>
+</tr>
+<tr>
+<td width="$left_col_width" valign="top">
+FO;
+
+include("$include_path/left.php");
+
+$final_output .= <<<FO
+</td>
+<td width="$main_col_width" valign="top">
+<table cellpadding="0" cellspacing="0" border="0" width="100%" align="center">
+FO;
+
+$final_output .= <<<FO
+<tr>
+<td align="left" valign="top">
+FO;
+
+include("$include_path/image_rules.php");
+
+$title = "Image Rules";
+
+$final_output .= table($title, $image_rules . "<br><br><br>");
+
+$final_output .= <<<FO
+</td>
+</tr>
+FO;
+
+$final_output .= <<<FO
+</table>
+FO;
+
+include("$include_path/copyright.php");
+
+$final_output .= <<<FO
+</td>
+<td width="$right_col_width" valign="top">
+FO;
+
+include("$include_path/right.php");
+
+$final_output .= <<<FO
+</td>
+</tr>
+</table>
+</body>
+</html>
+FO;
+
+$final_output = final_output($final_output);
+
+echo $final_output;
+
+?>
