e6cabd182fa696c01a92d7e607313cddcb782b81
me-suzy <me.suzana@gmail.com>
2025-08-12 14:16:18 +0300

    Add File Manipulation/dl4lite/UPLOAD/lang/1/lang_ad_skins.php

---
+++ File Manipulation/dl4lite/UPLOAD/lang/1/lang_ad_skins.php
@@ -0,0 +1,45 @@
+<?php
+/*********************************************************
+* Name: lang_ad_skins.php
+* Author: RW::Download 4.0
+* Contact: realworld@blazefans.com
+* Description: Language variables
+* Version: 4.00
+*********************************************************/
+$lang = array(
+
+'skinupdated' => "Skin set successfully edited",
+'no_skins_directory' => "Could not create new skin directory. Please make sure that the \"skins\" folder in your script root is set with write permissions [0666/0777]",
+'invalid_skin' => "The skin set you selected could not be found.",
+'skin_created' => "New skin set created",
+'import_successful' => "Import successful",
+'nodatafile' => "Unable to write/create the skin data file for exporting this skin pack. Please ensure the skin directory and all subdirectories are world writeable [0666/0777]",
+'skingsetmissing' => "Skin set could not be found.",
+'save_changes' => "Save Changes",
+'noskins' => "No skins were found. That is quite a serrious problem",
+'skinname' => "Skin Name",
+'no_skin_files' => "Unable to save/create skin files. Please make sure the following file and all other files in this directory are writable [chmod 0666/0777]",
+'no_template' => "Template set could not be found. Try rebuilding from database.",
+'couldnotread' => "Could not open file for reading, skipping file...",
+'no_line_end' => "Could not find any line endings in file, skipping file...",
+'no_skin_dir' => "Could not open directory for reading!",
+'not_a_dir' => " is not a directory, please check the \$root_path variable in admin.php.",
+'rebuild_done' => "Completed database rebuild from PHP cache files.",
+'edit_details' => "Edit Details",
+'skindeleted' => "Skin successfully deleted",
+'skindelfail' => "Failed to delete directory for this skin from the server. It may have already been deleted",
+'select_skin' => "Select Skin",
+'baseskin' => "Base skin on: ",
+'globalskinmissing' => "Global skin element missing or unreadable. Please check that the main.htm and style.css files exist in the following folder:",
+'notwritable' => "The following file is not writable. Please make sure that the directory and the file are writeable [chmod 0777].",
+'globalelements' => "Global Skin Elements",
+'css' => "Cascading Style Sheet",
+'template' => "Global template",
+'skin_import' => "Available skins",
+'no_imports' => "No skins are currently available for import. Skin archives should be uploaded to the 'archive_in' folder",
+'select_lang' => "Select Theme to Export",
+'export_complete' => "Theme was successfully exported to the archive_out folder",
+'click_download' => "Click here to download",
+)
+
+?>
