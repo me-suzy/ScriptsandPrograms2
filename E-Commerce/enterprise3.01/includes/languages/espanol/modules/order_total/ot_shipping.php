5aae9b3a1702369f55e9c19d7b72551d24cacefa
me-suzy <me.suzana@gmail.com>
2025-08-12 11:57:44 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/modules/order_total/ot_shipping.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/modules/order_total/ot_shipping.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ORDER_TOTAL_SHIPPING_TITLE', 'Gastos de Env&iacute;o');
+  define('MODULE_ORDER_TOTAL_SHIPPING_DESCRIPTION', 'Gastos de Env&iacute;o del Pedido');
+
+  define('FREE_SHIPPING_TITLE', 'Env&iacute;o Gratuito');
+  define('FREE_SHIPPING_DESCRIPTION', 'Env&iacute;o gratuito para pedidos sobre %s');
+?>
