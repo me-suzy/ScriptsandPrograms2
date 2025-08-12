97eab3c4b381a3e3c78e07c50351385f00831e00
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:04 +0300

    Add Content Management/metajour2_1/metajour/lang/sys_view_statistics.en.php

---
+++ Content Management/metajour2_1/metajour/lang/sys_view_statistics.en.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['heading'] = 'Website Statistics';
+$LANG['label_sum_pageid'] = 'Number of pageviews';
+$LANG['label_sum_sessionid'] = 'Number of sessions';
+$LANG['label_sum_userid'] = 'Number of users';
+
+$LANG['label_group_pageid'] = 'Document name';
+$LANG['label_group_ip'] = 'IP address';
+$LANG['label_group_sessionid'] = 'Session ID';
+$LANG['label_group_userid'] = 'User ID';
+$LANG['label_group_browser'] = 'Browser';
+$LANG['label_group_browserlanguage'] = 'Browser language';
+$LANG['label_group_usercreated'] = 'Known user';
+$LANG['label_group_referer'] = 'Referer URL';
+$LANG['label_group_useragent'] = 'Useragent';
+$LANG['label_group_host'] = 'Host/Domain';
+$LANG['label_group_tld'] = 'Country/TopLevelDomain';
+$LANG['label_group_date'] = 'Date';
+$LANG['label_group_dayname'] = 'Day of week';
+$LANG['label_group_dayofmonth'] = 'Day of month';
+$LANG['label_group_monthname'] = 'Month';
+$LANG['label_group_quarter'] = 'Quarter';
+$LANG['label_group_year'] = 'Year';
+$LANG['label_group_hour'] = 'Hour';
+
+$LANG['label_date'] = 'Date: ';
+$LANG['label_from'] = ' from ';
+$LANG['label_to'] = ' to ';
+$LANG['label_done'] = 'Done!';
+$LANG['label_limitto'] = 'Limited to: ';
+$LANG['label_groupby'] = 'Grouped by: ';
+$LANG['label_sumby'] = 'Sum of: ';
+$LANG['label_count'] = 'Count';
+$LANG['label_grouped'] = 'Grouped by';
+$LANG['label_distinct'] = 'Number of distinct: ';
+$LANG['label_fromdate'] = 'From date: ';
+$LANG['label_todate'] = 'To date: ';
+$LANG['label_generate'] = 'Generate statistics';
+$LANG['label_collecthost'] = 'Collect host information';
+$LANG['label_collecthostwarn'] = 'This can take quite some time!';
+$LANG['label_limitto'] = "Search limited to records where ";
+$LANG['label_ulang'] = "Limit by language";
+
+?>
