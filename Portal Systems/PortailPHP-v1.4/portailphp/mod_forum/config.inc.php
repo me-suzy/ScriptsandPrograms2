3dcc063c2afd69a67f5c34d833307707083ab48a
me-suzy <me.suzana@gmail.com>
2025-08-12 23:17:03 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/mod_forum/config.inc.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/mod_forum/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+include "include/config.php";
+/* Infos Générales ---------------------------------------------------------------- */
+$limit = 10 ;                        // nombre de message à afficher dans la page Index
+$align_boxarc = "right";              // alignement de la searchbox (left, center ou right)
+$bgcolor_box = "#FFFFFF";             // couleur de fond des champs (uniquement sous IE)
+$carac = 50 ;
+/* Infos du tableau --------------------------------------------------------------- */
+$largeur = "100%";                    // largeur du tableau (pixel ou %)
+$align = "center";                    // alignement du tableau
+?>
