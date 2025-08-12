780fc3c7712feeebdf1cef47a129c87ba486e268
me-suzy <me.suzana@gmail.com>
2025-08-12 14:16:15 +0300

    Add File Manipulation/dl4lite/UPLOAD/lang/1/lang_ad_main.php

---
+++ File Manipulation/dl4lite/UPLOAD/lang/1/lang_ad_main.php
@@ -0,0 +1,21 @@
+<?php
+/*********************************************************
+* Name: lang_ad_main.php
+* Author: RW::Download 4.0
+* Contact: realworld@blazefans.com
+* Description: Language variables
+* Version: 4.00
+*********************************************************/
+$lang = array(
+
+'rwdownload' => "RW::Download",
+'acp' => "Admin Control Panel",
+'stats_newdls' => "Stats - Latest Downloads",
+'stats_mostdl' => "Stats - Most Downloaded",
+'stats_topdls' => "Stats - Top Rated Downloads",
+'news' => "News",
+
+
+)
+
+?>
