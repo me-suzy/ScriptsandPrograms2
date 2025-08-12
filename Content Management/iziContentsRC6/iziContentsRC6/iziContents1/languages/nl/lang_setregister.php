533d239de6a76d623b1f138b17ee8a0c8bcffba8
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:55 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_setregister.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_setregister.php
@@ -0,0 +1,55 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Registratie instellingen';
+
+//  Form Block Titles
+$GLOBALS["thActiveFields"] = 'Actieve velden';
+$GLOBALS["thMailSettings"] = 'Mail instellingen';
+$GLOBALS["thExtraSettings"] = 'Extra instellingen';
+
+//  Form Field Headings
+$GLOBALS["tPhoneNumber"] = 'Telefoon';
+$GLOBALS["tFaxNumber"] = 'Fax ';
+$GLOBALS["tAddress"] = 'Adres';
+$GLOBALS["tCity"] = 'Stad';
+$GLOBALS["tState"] = 'Provincie';
+$GLOBALS["tZip"] = 'Postcode';
+$GLOBALS["tCountry"] = 'Land';
+$GLOBALS["tLanguage"] = 'Taal';
+$GLOBALS["tWebsite"] = 'Website';
+$GLOBALS["tComments"] = 'Opmerkingen';
+$GLOBALS["tNewsletter"] = 'Nieuwsbrief';
+$GLOBALS["tSendConfMail"] = 'Bevestigings e-mail';
+$GLOBALS["tServerUserEmail"] = 'Server gebruikers account';
+$GLOBALS["tErrFormFontColor"] = 'Foutmelding letterkleur';
+$GLOBALS["tShowHelptexts"] = 'Toon helpteksten';
+$GLOBALS["tHelpTextSize"] = 'Lettergrootte helptekst';
+$GLOBALS["tHelpTextColor"] = 'Letterkleur helptekst';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'Hier kun je de registratie en mail gegevens instellen.';
+
+$GLOBALS["hPhoneNumber"] = 'Toont of verbergt het telefoonnummer op het gebruikers registratie formulier. Dit veld kan optioneel zijn of verplicht';
+$GLOBALS["hFaxNumber"] = 'Toont of verbergt het faxnummer op het gebruikers registratie formulier. Dit veld kan optioneel zijn of verplicht.';
+$GLOBALS["hAddress"] = 'Toont of verbergt het adres op het gebruikers registratie formulier. Dit veld kan optioneel zijn of verplicht';
+$GLOBALS["hCity"] = 'Toont of verbergt de stad op het gebruikers registratie formulier. Dit veld kan optioneel zijn of verplicht';
+$GLOBALS["hState"] = 'Toont of verbergt de provincie op het gebruikers registratie formulier. Dit veld kan optioneel zijn of verplicht';
+$GLOBALS["hZip"] = 'Toont of verbergt de postcode op het gebruikers registratie formulier. Dit veld kan optioneel zijn of verplicht';
+$GLOBALS["hCountry"] = 'Toont of verbergt het land op het gebruikers registratie formulier. Dit veld kan optioneel zijn of verplicht';
+$GLOBALS["hLanguage"] = 'Toont of verbergt de taal op het gebruikers registratie formulier. Dit veld kan optioneel zijn of verplicht';
+$GLOBALS["hWebsite"] = 'Toont of verbergt het website-veld op het gebruikers registratie formulier. Dit veld kan optioneel zijn of verplicht';
+$GLOBALS["hComments"] = 'Toont of verbergt de opmerkingen op het gebruikers registratie formulier. Dit veld kan optioneel zijn of verplicht';
+$GLOBALS["hNewsletter"] = 'Toont of verbergt de optie om in te schrijven op de nieuwsbrief op het gebruikers registratie formulier';
+$GLOBALS["hSendConfMail"] = 'E-mail confirmation with registration details will be send to the registered user.';
+$GLOBALS["hServerUserEmail"] = 'Soms is het nodig om een mail te sturen vanaf de server. Als je dit veld leeg laat, wordt het standaardadres van de admin-account.';
+$GLOBALS["hErrFormFontColor"] = 'Tekstkleur op het gebruikers registratie formulier voor validatieteksten en foutmeldingen.';
+$GLOBALS["hShowHelptexts"] = 'Toont hulpteksten naast de velden';
+$GLOBALS["hHelpTextSize"] = 'Lettergrootte van de hulpteksten naast de velden';
+$GLOBALS["hHelpTextColor"] = 'Letterkleur van de hulpteksten naast de velden';
+
+// Error messages
+$GLOBALS["eErrorColorWrong"] = 'Foutmelding lettergrootte heeft een verkeerde waarde';
+$GLOBALS["eHelptextColorWrong"] = 'Foutmelding letterkleur heeft een verkeerde waarde';
+
+?>
