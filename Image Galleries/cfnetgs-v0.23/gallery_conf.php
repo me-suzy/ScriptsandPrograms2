29e9f88803bb23cbc97dd4bb56df7605b9ae0211
me-suzy <me.suzana@gmail.com>
2025-08-12 17:40:53 +0300

    Add Image Galleries/cfnetgs-v0.23/gallery_conf.php

---
+++ Image Galleries/cfnetgs-v0.23/gallery_conf.php
@@ -0,0 +1,16 @@
+<?php
+// let user define some stuff
+$title = 'CyberFrogs.NET Gallery'; // name of your gallery
+$header = ''; // full path to the header file, leave blank if none
+$footer = ''; // full path to the footer file, leave blank if none
+$style = '/photo/gallery.css'; // url to style sheet
+$cols = 4;
+$rows = 4;
+$album_icon_width = 125; //width for album icon images
+$thmb_width = 125; //width for thumnail images
+$display_width = 600; // width of image to be displayed
+// Use Imagemagick or GD for creation of images?
+// $library = 'GD'; // for GD
+// $library = 'IM'; // for IM (default)
+$library = 'IM';
+?>
