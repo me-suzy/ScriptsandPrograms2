4fc53cd04cd867a1f19fdba8a4f7afba9627178f
me-suzy <me.suzana@gmail.com>
2025-08-12 08:17:54 +0300

    Add Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/modules/menu_link/view.php

---
+++ Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/modules/menu_link/view.php
@@ -0,0 +1,34 @@
+<?php
+
+// $Id: view.php 103 2005-09-14 07:19:34Z ryan $
+
+/*
+
+ Website Baker Project <http://www.websitebaker.org/>
+ Copyright (C) 2004-2005, Ryan Djurovich
+
+ Website Baker is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Website Baker is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Website Baker; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+/*
+Since there is nothing to show and users shouldn't really know this
+page exists, we might as well give them a link to the home page.
+*/
+
+?>
+<a href="<?php echo WB_URL; ?>">
+Click HERE to go to the main page
+</a>
