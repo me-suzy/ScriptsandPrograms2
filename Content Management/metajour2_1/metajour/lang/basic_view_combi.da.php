e7f69fdcedd66a8131680abfb69b10b2e35c13ca
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:39 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_combi.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_combi.da.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['title'] = 'Redigering';
+$LANG['tab_general'] = 'Generelt';
+$LANG['tab_childobjects'] = 'Underobjekter';
+$LANG['tab_dependentobjects'] = 'Afhængige objekter';
+$LANG['tab_requiredobjects'] = 'Påkrævede objekter';
+$LANG['tab_properties'] = 'Egenskaber';
+
+?>
