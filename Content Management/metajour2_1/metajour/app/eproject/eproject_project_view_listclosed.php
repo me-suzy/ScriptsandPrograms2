75f624a6fe53c65c0d8763ac449d2ee61e9994f9
me-suzy <me.suzana@gmail.com>
2025-08-12 06:00:09 +0300

    Add Content Management/metajour2_1/metajour/app/eproject/eproject_project_view_listclosed.php

---
+++ Content Management/metajour2_1/metajour/app/eproject/eproject_project_view_listclosed.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eProject
+ * @subpackage view
+ * $Id: eproject_project_view_listclosed.php,v 1.3 2005/02/15 12:20:39 jan Exp $
+ */
+
+require_once($system_path.'basic_view_list.php');
+
+class eproject_project_view_listclosed extends basic_view_list {
+
+	function setFilters() {
+		parent::setFilters();
+		$obj =& $this->_listobj;
+		$obj->setfilter_search('status', CASE_CLOSED, EQUAL);
+	}
+
+	function eproject_project_view_listclosed() {
+		$this->basic_view_list();
+		$this->_preset = true;
+	}
+
+}
+
+?>
