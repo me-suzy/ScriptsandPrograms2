b8b28bd9134539e865d4dc4d5b1b89bd1d40e9d0
me-suzy <me.suzana@gmail.com>
2025-08-12 18:54:01 +0300

    Add Interactive Stories/story/admin/login.php

---
+++ Interactive Stories/story/admin/login.php
@@ -0,0 +1,20 @@
+<center>
+<table border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#6B84AE" width="460" id="AutoNumber1" height="198">
+  <tr>
+    <td width="460" background="../../topbackground.jpg" height="20" valign="top">
+   <font color="blue"><center><b>Chipmunk Stories<b></center></font></td>
+  </tr>
+  <tr>
+    <td width="460" height="177" bgcolor="#F2F2F2" valign="top">
+
+
+
+<form method="POST" action="authenticate.php">
+Type Username Here: <input type="text" name="username" size="15"><br>
+Type Password Here: <input type="password" name="password" size="15"><br>
+<input type="submit" value="submit" name="submit">
+
+
+
+</form>
+</td></tr></table></center>
