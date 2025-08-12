8db3154cb8f601eda9d929e728d7a15b449381b3
me-suzy <me.suzana@gmail.com>
2025-08-12 10:28:39 +0300

    Add Discussion Boards/board2/ukiboard/themes/defaultstyle.php

---
+++ Discussion Boards/board2/ukiboard/themes/defaultstyle.php
@@ -0,0 +1,20 @@
+<SCRIPT LANGUAGE="JavaScript">
+<!--
+
+function MenuOver(element)
+{
+	if(element.style){
+		element.style.backgroundColor='#0099CC';
+		element.style.cursor='hand';
+	};
+};
+
+function MenuOut(element)
+{
+	if(element.style) element.style.backgroundColor='#006699';
+};
+
+window.defaultStatus="UKiBoard";
+
+//-->
+</SCRIPT>
