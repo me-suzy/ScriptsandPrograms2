832488544f3a87157b5f4e83b61bb5afa2ef5e62
me-suzy <me.suzana@gmail.com>
2025-08-12 13:54:55 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/footer.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/footer.php
@@ -0,0 +1,34 @@
+<?
+$file_rev="041305";
+////////////////////////////////////////////////////////
+//                 phpBannerExchange                  //
+//                   by: Darkrose                     //
+//              (darkrose@eschew.net)                 //
+//                                                    //
+// You can redistribute this software under the terms //
+// of the GNU General Public License as published by  //
+// the Free Software Foundation; either version 2 of  //
+// the License, or (at your option) any later         //
+// version.                                           //
+//                                                    //
+// You should have received a copy of the GNU General //
+// Public License along with this program; if not,    //
+// write to the Free Software Foundation, Inc., 59    //
+// Temple Place, Suite 330, Boston, MA 02111-1307 USA //
+//                                                    //
+//     Copyright 2004 by eschew.net Productions.      //
+//   Please keep this copyright information intact.   //
+////////////////////////////////////////////////////////
+
+require_once('lib/template_class.php');
+include("config.php");
+include("lang/common.php");
+
+$footer="$basepath/template/footer.php";
+
+$page = new Page("$footer");
+$page->replace_tags(array(
+'ownermail' => "$ownermail",
+'adminname' => "$adminname"));
+
+$page->output();
