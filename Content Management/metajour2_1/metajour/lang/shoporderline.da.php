7ec8bf9a9f9c6b31ebe358f7978af5e907c0b4f0
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:03 +0300

    Add Content Management/metajour2_1/metajour/lang/shoporderline.da.php

---
+++ Content Management/metajour2_1/metajour/lang/shoporderline.da.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Ordrelinier';
+$LANG['label_name'] = 'Varenr';
+$LANG['label_itemtext'] = 'Varetekst';
+$LANG['label_itemnum'] = 'Antal';
+$LANG['label_itemid'] = '';
+$LANG['label_orderid'] = '';
+$LANG['label_price'] = 'Pris';
+$LANG['label_disc'] = '';
+$LANG['label_actprice'] = '';
+$LANG['label_actpricecur'] = '';
+$LANG['label_pricevat'] = '';
+$LANG['label_discvat'] = '';
+$LANG['label_actpricevat'] = '';
+$LANG['label_actpricecurvat'] = '';
+$LANG['label_sumprice'] = 'Linietotal';
+$LANG['label_sumdisc'] = '';
+$LANG['label_sumactprice'] = '';
+$LANG['label_sumactpricecur'] = '';
+$LANG['label_sumpricevat'] = '';
+$LANG['label_sumdiscvat'] = '';
+$LANG['label_sumactpricevat'] = '';
+$LANG['label_sumactpricecurvat'] = '';
+?>
