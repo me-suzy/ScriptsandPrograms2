61cc05dcd99ce8ec99a264c2b707c67d7f14fdaf
me-suzy <me.suzana@gmail.com>
2025-08-13 02:14:02 +0300

    Add Unsorted/credigold.$1500.script.insanegod/CrediGold/index.php

---
+++ Unsorted/credigold.$1500.script.insanegod/CrediGold/index.php
@@ -0,0 +1,50 @@
+<?
+
+include("/www/u/urunity/htdocs/includes/prepend.php3");
+
+initPage();
+
+?>
+
+<div align=center class=head>Welcome to <?=$_Config["masterRef"]?>
+
+<br>
+
+<p align=justify class=text style=width:500px>
+
+&nbsp;&nbsp;Welcome to <?=$_Config["masterRef"]?> - the ultimate online payment system. We offer a wide variety of services from simple transfers, transaction history to referral programs and shopping carts. Should you experience any problems with the site our team will try to solve it in a nick of time via the FAQ section of <?=$_Config["masterRef"]?>
+
+<br>
+
+<br>
+
+&nbsp;&nbsp;&nbsp;<img src=images/point.gif width=9 height=9>&nbsp;Discover your profit through using the site.
+
+<br>
+
+&nbsp;&nbsp;&nbsp;<img src=images/point.gif width=9 height=9>&nbsp;Make transfers all over the world with only one mouse click.
+
+<br>
+
+&nbsp;&nbsp;&nbsp;<img src=images/point.gif width=9 height=9>&nbsp;Secure, fast, reliable - all the qualities we possess.
+
+<br>
+
+<br>
+
+&nbsp;&nbsp;Not having an account yet! Start earning today by registering a FREE <?=$_Config["masterRef"]?> Account from <a href=account.php?cmd=register>here</a>. 
+
+<br>
+
+<br>
+
+&nbsp;&nbsp;If you are already a member click <a href=transfer.php>here</a>.
+
+</p>
+
+<?
+
+endPage();
+
+?>
+
