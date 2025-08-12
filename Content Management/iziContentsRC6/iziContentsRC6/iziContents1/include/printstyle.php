271eac5921e0617b676b5a9563e3e857d2762561
me-suzy <me.suzana@gmail.com>
2025-08-12 03:41:28 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/include/printstyle.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/include/printstyle.php
@@ -0,0 +1,161 @@
+<?php
+
+/***************************************************************************
+
+ printstyle.php
+ ----------
+ copyright : (C) 2005 - The iziContents Development Team
+
+ iziContents version : 1.0
+ fileversion : 1.0.1
+ change date : 23 - 04 - 2005
+ ***************************************************************************/
+
+/***************************************************************************
+ The iziContents Development Team offers no warranties on this script.
+ The owner/licensee of the script is solely responsible for any problems
+ caused by installation of the script or use of the script.
+
+ All copyright notices regarding iziContents and ezContents must remain intact on the scripts and in the HTML for the scripts.
+
+ For more info on iziContents,
+ visit http://www.izicontents.com*/
+
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the License which can be found within the
+ *   zipped package. Under the licence of GPL/GNU.
+ *
+ ***************************************************************************/
+
+?>
+<style type="text/css">
+
+A
+{
+	FONT-SIZE: 13px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	TEXT-DECORATION: None;
+}
+
+A:visited
+{
+	FONT-SIZE: 13px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	TEXT-DECORATION: None;
+}
+
+A:hover
+{
+	FONT-SIZE: 13px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	TEXT-DECORATION: None;
+}
+
+
+TD
+{
+	FONT-SIZE: 13px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	BACKGROUND-IMAGE: NONE;
+}
+
+H1
+{
+	FONT-SIZE: 20px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+}
+
+.header
+{
+	FONT-SIZE: 17px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+}
+
+.rcolheader
+{
+	FONT-SIZE: 17px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+}
+
+.teaserheader
+{
+	FONT-SIZE: 17px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+}
+
+.headercontent
+{
+	FONT-SIZE: 13px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	BACKGROUND-COLOR: White;
+}
+
+.teaserheadercontent
+{
+	FONT-SIZE: 13px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	BACKGROUND-COLOR: White;
+}
+
+.rcolheadercontent
+{
+	FONT-SIZE: 13px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	BACKGROUND-COLOR: White;
+}
+
+.tablecontent
+{
+	FONT-SIZE: 13px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	BACKGROUND-COLOR: White;
+}
+
+.teasercontent
+{
+	FONT-SIZE: 13px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	BACKGROUND-COLOR: White;
+}
+
+.rcolcontent
+{
+	FONT-SIZE: 13px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	BACKGROUND-COLOR: White;
+}
+
+.tablecontentfooter
+{
+	FONT-SIZE: 11px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	BACKGROUND-COLOR: White;
+}
+
+.teasercontentfooter
+{
+	FONT-SIZE: 11px;
+	COLOR: Black;
+	FONT-FAMILY: Verdana;
+	BACKGROUND-COLOR: White;
+}
+
+-->
+</style>
