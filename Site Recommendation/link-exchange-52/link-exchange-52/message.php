1f7074b46e8e4c4f62ce64a1ce4f682021b53454
me-suzy <me.suzana@gmail.com>
2025-08-12 23:51:52 +0300

    Add Site Recommendation/link-exchange-52/link-exchange-52/message.php

---
+++ Site Recommendation/link-exchange-52/link-exchange-52/message.php
@@ -0,0 +1,11 @@
+<? include "_header.inc"; ?>
+
+<table>
+	<tr><td width=100% height=380 align=center>
+    <?=$_REQUEST["msg"]?>
+		<br>
+    <a href="javascript:history.back()">Go Back</a>
+	</tr>
+</table>
+
+<?  include "_footer.inc"; ?>
