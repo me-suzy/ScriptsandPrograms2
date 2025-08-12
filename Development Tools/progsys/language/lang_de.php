ea43bcf514756284b639520a31ddcf03e41cc3c7
me-suzy <me.suzana@gmail.com>
2025-08-12 10:12:36 +0300

    Add Development Tools/progsys/language/lang_de.php

---
+++ Development Tools/progsys/language/lang_de.php
@@ -0,0 +1,149 @@
+<?php
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+/***************************************************************************
+ * Created by: Boesch IT-Consulting (info@boesch-it.de)
+ * (c)2002-2005 Boesch IT-Consulting
+ * *************************************************************************/
+$l_noentries = "keine Eintr&auml;ge";
+$l_heading = "ProgSys";
+$l_undefined = "nicht definiert";
+$l_generated = "Erzeugt";
+$l_back = "zur&uuml;ck";
+$l_submit = "abschicken";
+$l_callingerror = "Aufruffehler";
+$l_function_disabled = "Funktion deaktiviert";
+$l_timezone_note = "Alle Zeitangaben sind in";
+$l_changelog_heading = "Changelog";
+$l_version = "Version";
+$l_changes = "&Auml;nderungen";
+$l_programm = "Programm";
+$l_bugtraq_heading = "Fehlertracking";
+$l_state = "Status";
+$l_states = array ("neu","offen","in Bearbeitung","behoben","verschoben");
+$l_fixversion = "Behoben ab Version";
+$l_bug = "Fehler";
+$l_fix = "Fix";
+$l_statefilter = "Nur Fehler mit folgendem Status anzeigen";
+$l_ok = "OK";
+$l_all = "alle";
+$l_enternewbug = "neuen Fehler erfassen";
+$l_yourname = "Ihr Name";
+$l_bugreport = "Fehlerbeschreibung";
+$l_noname = "Bitte geben Sie Ihren Namen ein";
+$l_nobugreport = "Bitte geben Sie eine Fehlerbeschreibung ein";
+$l_bugadded = "Ihr Fehlerreport wurde erfasst.";
+$l_buglist = "Fehlerliste";
+$l_noversion = "Bitte geben Sie die Programmversion ein";
+$l_sendermail = "Ihre E-Mailadresse";
+$l_usedversion = "benutzte Programmversion";
+$l_invalidemail = "Bitte geben Sie eine g&uuml;ltige E-Mailadresse an";
+$l_progundefined = "Kein solches Programm definiert";
+$l_references_heading = "Referenzen";
+$l_newsletter_heading = "Newsletter";
+$l_inputprelude = "Bitte f&uuml;llen Sie folgendes Formular aus,<br>wenn Sie das Programm auf Ihrer Website benutzen.<br>Wenn Sie der Ver&ouml;ffentlichung als Referenzseite zustimmen,<br>so wird nur der Websitename und die URL ver&ouml;ffentlicht.";
+$l_sitename = "Name der Website";
+$l_siteurl = "URL der Website";
+$l_contact = "Ansprechpartner";
+$l_email = "E-Mail";
+$l_publish = "Website in Referenzliste ver&ouml;ffentlichen";
+$l_heardfrom = "Wo haben Sie vom Programm erfahren";
+$l_nourl = "Sie m&uuml;ssen eine URL eingeben";
+$l_nositename = "Sie m&uuml;ssen den Namen der Website eingeben";
+$l_noemail = "Bitte geben Sie eine g&uuml;ltige E-Mailadresse an";
+$l_inputerrors = "Es sind Eingabefehler aufgetreten:";
+$l_entryadded = "Eintrag hinzugef&uuml;gt.";
+$l_reflist = "Referenzliste";
+$l_pin1 = "Ihre PIN lautet";
+$l_pin2 = "bitte notieren Sie diese PIN, damit Sie sp&auml;ter Ihren Eintrag noch ab&auml;ndern k&ouml;nnen";
+$l_reflistprelude = "Websites, die das Programm einsetzen:";
+$l_reportbroken = "defekten Link melden";
+$l_alsousing = "Benutzen Sie es auch?";
+$l_addsite = "Ihre Website eintragen";
+$l_updateentry = "Ihren Eintrag aktualisieren";
+$l_brokenprelude = "Folgenden Link als defekt melden:";
+$l_sendreport = "Meldung abschicken";
+$l_reason = "Bemerkung (Grund)";
+$l_brokendone = "Danke f&uuml;r die Mitteilung.<br>Wird werden uns darum k&uuml;mmern.";
+$l_pin = "PIN";
+$l_pinlost = "PIN vergessen";
+$l_nosuchentry = "Kein entsprechender Eintrag vorhanden.";
+$l_pinmail = "Ihre PIN fuer die Referenzliste lautet:\r\n";
+$l_pinsubject = "PIN fuer Referenzliste";
+$l_pinsent = "Ihre PIN wurde per E-Mail versand.";
+$l_salutation1 = "Sehr geehrte(r)";
+$l_salutation2 = "Sehr geehrte Damen und Herren";
+$l_greeting = "Mit freundlichen Gruessen";
+$l_entryupdated = "Eintrag aktualisiert";
+$l_note2us = "zus&auml;tzliche Mitteilung an uns";
+$l_broken_reasons = array("DNS: Name nicht gefunden","keine Antwort (keine Verbindung)","Verschoben","401 Authorization Required","404 Not Found","500 Server Error","Sonstiges");
+$l_todo_heading = "geplante Erweiterungen";
+$l_todo_states = array ("in Planung","in Bearbeitung","verschoben","fertig gestellt");
+$l_functiondisabled = "Funktion nicht verf&uuml;gbar";
+$l_subscriptionprelude = "Abonnieren des Newsletters f&uuml;r {progname}";
+$l_nofreemailer = "(keine Adressen bei Freemailern)";
+$l_emailtype = "Format der E-Mail";
+$l_htmlmail = "HTML";
+$l_ascmail = "nur Text";
+$l_subscribe = "abonnieren";
+$l_forbidden_freemailer = "Eine E-Mailadresse bei diesem Freemailer ist f&uuml;r die Newsletter nicht zul&auml;ssig";
+$l_allready_subscribed = "Ein Abonnement f&uuml;r diese E-Mailadresse besteht schon.";
+$l_allready_pending = "Eine offene Abonnementanforderung f&uuml;r diese E-Mailadresse besteht schon.<br>Warten Sie bitte die Best&auml;tigungsanforderung ab und best&auml;tigen Sie dann Ihr Abonnement.";
+$l_hours = "Stunden";
+$l_subscriptionconfirmmail = "Hallo,\nSie haben sich beim Newsletter für \"{progname}\" angemeldet.\n
+Um sicher zu stellen, dass Sie sich auch selbst angemeldet haben, haben wir diesen Bestätigungsmechanismus aktiviert.\n
+Um das Abo zu aktivieren, besuchen sie bitte innerhalb von {confirmtime} folgende URL:\n{confirmurl}\n
+Sollten Sie die Anmeldung nicht aktivieren wollen, so brauchen Sie nichts zu tun.\n\n
+Dies ist eine automatisch generierte E-Mail, bitte nicht darauf antworten.";
+$l_subscriptionconfirmsubject = "Newsletter ({progname}) - Anmeldebestaetigung";
+$l_subscriptiondone = "Danke, dass Sie den Newsletter abonniert haben";
+$l_missingemail = "Keine Emailadresse &uuml;bergeben";
+$l_missingid = "Keine ID &uuml;bergeben";
+$l_noconfirmentry = "Keine entsprechende Anforderung zur Best&auml;tigung gefunden.<br>Wahrscheinlich ist die maximale Wartezeit abgelaufen<br>oder Sie haben die Anforderung schon best&auml;tigt.";
+$l_subscriptionconfirmed = "Ihr Abonnement f&uuml;r den Newsletter ist nun aktiviert.";
+$l_noremoveentry = "Kein passender Abonnenteneintrag zum Entfernen gefunden.";
+$l_unsubscribed = "Abonnement entfernt.";
+$l_subscriptionremoveprelude = "Wollen Sie den Newsletter zu {progname} f&uuml;r {email} wirklich l&ouml;schen?";
+$l_yes = "Ja";
+$l_ratingprelude = "Wie wichtig w&auml;re Ihnen diese Funktion?";
+$l_rate = "bewerten";
+$l_ratings = array("unwichtig","weniger wichtig","ziemlich wichtig","sehr wichtig");
+$l_ratingdone = "Ihre Bewertung wurde eingetragen.";
+$l_subscriptionconfirminfo = "Um sicher zu stellen, dass Sie auch der Besitzer der angegebenen E-Mailadresse sind<br>wurde eine Best&auml;tigungsanforderung per E-Mail an diese Adresse versand.<br>
+Zur Aktivierung des Abonnements verfahren Sie bitte, wie in der E-Mail beschrieben.";
+$l_featurerequests = "Feature Requests";
+$l_newrequest = "neues Feature vorschlagen";
+$l_request = "Vorschlag";
+$l_norequesttext = "Bitte geben Sie Ihren Vorschlag ein";
+$l_requestadded = "Ihr Vorschlag wurde entgegen genommen";
+$l_page = "Seite";
+$l_of = "von";
+$l_entries = "Eintr&auml;ge";
+$l_releasestates = array("-----","vielleicht","wahrscheinlich","sehr wahrscheinlich","sicher","eher nicht","sicher nicht","implementiert");
+$l_releasestate = "wird umgesetzt";
+$l_modcomment = "Anmerkung eines Administrators";
+$l_unsubscribe = "Abonnement l&ouml;schen";
+$l_unsubscriptionconfirmmail = "Hallo,\nSie haben angefordert sich beim Newsletter für \"{progname}\" abzumelden.\n
+Um sicher zu stellen, dass Sie sich auch selbst abmelden wollen, haben wir diesen Bestätigungsmechanismus aktiviert.\n
+Um sich endgueltig abzumelden, besuchen sie bitte folgende URL:\n{confirmurl}\n
+Sollten Sie die Abmeldung nicht vornehmen wollen, so brauchen Sie nichts zu tun.";
+$l_unsubscriptionconfirmsubject = "Newsletter ({progname}) - Abmeldebestaetigung";
+$l_unsubscribesent = "Eine E-Mail mit dem Best&auml;tigungslink f&uuml;r die Abmeldung wurde verschickt.<br>Bitte befolgen Sie die Anweisungen in der E-Mail, um sich endg&uuml;ltig abzumelden.";
+$l_unsubscriptionprelude = "Abonnement des Newsletters f&uuml;r {progname} k&uuml;ndigen";
+$l_rating = "bisherige Bewertung";
+$l_votes = "Bewertungen";
+$l_forbidden = "Zugriff verweigert";
+$l_powered_by = "Powered by";
+$l_surename = "Nachname";
+$l_firstname = "Vorname";
+$l_mandatory_fields = "Pflichtfelder";
+$l_online = "verf&uuml;gbar";
+$l_tempoffline = "tempor&auml;r nicht verf&uuml;gbar";
+$l_notdefined = "Nicht definiert";
+?>
