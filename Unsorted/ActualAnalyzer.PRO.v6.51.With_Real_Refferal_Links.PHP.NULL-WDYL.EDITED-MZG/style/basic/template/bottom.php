b935470b7d037040a3df768261fceead2aa1ccce
me-suzy <me.suzana@gmail.com>
2025-08-13 00:28:39 +0300

    Add Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/style/basic/template/bottom.php

---
+++ Unsorted/ActualAnalyzer.PRO.v6.51.With_Real_Refferal_Links.PHP.NULL-WDYL.EDITED-MZG/style/basic/template/bottom.php
@@ -0,0 +1,10 @@
+<?php
+$bottom=<<<BOTTOM
+<table width=750 border=0 cellspacing=0 cellpadding=0><tr valign=center><td colspan=2 bgcolor="#cccccc"><img src="%%RF%%style/%%STYLE%%/image/0.gif" width="1" height="1"></td></tr>
+<tr><td colspan=2 height=3><img src="%%RF%%style/%%STYLE%%/image/0.gif" width="1" height="1"></td></tr>
+<tr height=25 valign=top><td><SPAN class=f11>&nbsp;&nbsp;Actual<b>&nbsp;</b></SPAN></td>
+<td align=right><SPAN class=f11>&nbsp;</SPAN></td>
+</tr></table></form></div></body></html>
+
+BOTTOM;
+?>
