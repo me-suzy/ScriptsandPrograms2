b7ebf9d331784e0f64384e8241a4f4221e126381
me-suzy <me.suzana@gmail.com>
2025-08-12 10:07:43 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/includes/skinButtonsPanel.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/includes/skinButtonsPanel.php
@@ -0,0 +1,15 @@
+<tr height=76>
+	<td height=76 valign=bottom>  
+		    <table height=76 width=100% class=pntGroupPane>
+		    	<tr>
+		    		<td class=pntGroupContent>
+		    			<table height=100% width=100%>
+		    			  <Tr><TD>
+		    					<?php $this->printPart('ButtonsListPart') ?>
+		    			  </TD></tr>
+		    			</table>
+		    		</td>		    	
+		    	</tr>
+		    </table>	
+	</td>
+</tr>
