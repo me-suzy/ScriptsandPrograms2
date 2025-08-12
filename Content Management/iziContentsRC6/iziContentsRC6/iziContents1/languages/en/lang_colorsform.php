e9262b76443580227b125dd69b08f5cd40d092e2
me-suzy <me.suzana@gmail.com>
2025-08-12 03:57:19 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_colorsform.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/en/lang_colorsform.php
@@ -0,0 +1,146 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Set site colours and fonts';
+
+//  Form Block Titles
+$GLOBALS["thBackground"] = 'Background Colours';
+$GLOBALS["thFont"] = 'Site Fonts';
+$GLOBALS["thLinks"] = 'Link Colours';
+$GLOBALS["thContent"] = 'Content Colours';
+$GLOBALS["thRColContent"] = 'Right Column Content Colours';
+$GLOBALS["thTeaser"] = 'Teaser Colours';
+
+//  Form Field Headings
+$GLOBALS["tMainBackground"] = 'Main background';
+$GLOBALS["tBorderBackground"] = 'Border background';
+$GLOBALS["tHeaderBackground"] = 'Header background';
+$GLOBALS["tFooterBackground"] = 'Footer background';
+$GLOBALS["tMenuBackground"] = 'Menu background';
+$GLOBALS["tTopmenuBackground"] = 'Top menu background';
+$GLOBALS["tFont"] = 'Website font';
+$GLOBALS["tHeaderFontSize"] = 'Header font size';
+$GLOBALS["tRColHeaderFontSize"] = 'Right column header font size';
+$GLOBALS["tTMFontSize"] = 'Top menu link font size';
+$GLOBALS["tTMFontStyle"] = 'Top menu link font style';
+$GLOBALS["tMFontSize"] = 'Menu link font size';
+$GLOBALS["tMFontStyle"] = 'Menu link font style';
+$GLOBALS["tLinkColour"] = 'Link colour';
+$GLOBALS["tLinkColourHover"] = 'Link colour hover';
+$GLOBALS["tLinkColourHistory"] = 'Link colour history';
+$GLOBALS["tSLinkColour"] = 'Small link colour';
+$GLOBALS["tSLinkColourHover"] = 'Small link colour hover';
+$GLOBALS["tSLinkColourHistory"] = 'Small link colour history';
+$GLOBALS["tMenuLinkColour"] = 'Menu link colour';
+$GLOBALS["tMenuLinkColourHover"] = 'Menu link colour hover';
+$GLOBALS["tMenuLinkColourHistory"] = 'Menu link colour history';
+$GLOBALS["tTopMenuLinkColour"] = 'Top menu link colour';
+$GLOBALS["tTopMenuLinkColourHover"] = 'Top menu link colour hover';
+$GLOBALS["tTopMenuLinkColourHistory"] = 'Top menu link colour history';
+$GLOBALS["tRColLinkColour"] = 'Right column link colour';
+$GLOBALS["tRColLinkColourHover"] = 'Right column link colour hover';
+$GLOBALS["tRColLinkColourHistory"] = 'Right column link colour history';
+$GLOBALS["tSLinkFontSize"] = 'Small link font size';
+$GLOBALS["tRColFontSize"] = 'Right column font/links size';
+$GLOBALS["tRColFontStyle"] = 'Right column links style';
+$GLOBALS["tCBorder"] = 'Content border colour';
+$GLOBALS["tCHeader"] = 'Content header font colour';
+$GLOBALS["tCBackground"] = 'Content background';
+$GLOBALS["tCFont"] = 'Content font colour';
+$GLOBALS["tTBorder"] = 'Teaser border colour';
+$GLOBALS["tTHeader"] = 'Teaser header font colour';
+$GLOBALS["tTBackground"] = 'Teaser background';
+$GLOBALS["tTFont"] = 'Teaser font colour';
+$GLOBALS["tCFontSize"] = 'Contents/links font size';
+$GLOBALS["tCFontStyle"] = 'Contents/links font style';
+$GLOBALS["tRColCBorder"] = 'Right column border colour';
+$GLOBALS["tRColCHeader"] = 'Right column header font colour';
+$GLOBALS["tRColCBackground"] = 'Right column background';
+$GLOBALS["tRColCFont"] = 'Right column font colour';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'This form lets you change the colours and fonts for your website.';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["hMainBackground"] = 'This is the main background colour for the site.';
+$GLOBALS["hBorderBackground"] = 'This is the border background colour for the site.';
+$GLOBALS["hHeaderBackground"] = 'This background colour is used for the site header.';
+$GLOBALS["hFooterBackground"] = 'This background colour is used for the site footer.';
+$GLOBALS["hMenuBackground"] = 'This background colour is used for the sidebar menu.';
+$GLOBALS["hTopmenuBackground"] = 'This background colour is used for the top header menu (if that feature is enabled in \'site settings\').';
+$GLOBALS["hFont"] = 'This is the font used for the web site';
+$GLOBALS["hHeaderFontSize"] = 'This is the font size used for content and teaser headers.';
+$GLOBALS["hRColHeaderFontSize"] = 'This is the font size used for content headers in the right column.';
+$GLOBALS["hTMFontSize"] = 'This is the font size for top header menu items (if this feature is enabled in \'site settings\', and if you are using text rather than images for the menu items).';
+$GLOBALS["hTMFontStyle"] = 'Use this setting to choose a font style for top header menu items if you are using text items rather than images.';
+$GLOBALS["hMFontSize"] = 'This is the font size for sidebar menu items.';
+$GLOBALS["hMFontStyle"] = 'Use this setting to choose a font style for sidebar menu items if you are using text items rather than images.';
+$GLOBALS["hLinkColour"] = 'This is the colour to use for links in your site.';
+$GLOBALS["hLinkColourHover"] = 'Use this colour setting to highlight links when the cursor is hovering over them.';
+$GLOBALS["hLinkColourHistory"] = 'This setting allows you to choose a colour for links that your visitor has already followed.';
+$GLOBALS["hSLinkColour"] = 'This is the colour to use for \'small\' links in your site.';
+$GLOBALS["hSLinkColourHover"] = 'Use this colour setting to highlight \'small\' links when the cursor is hovering over them.';
+$GLOBALS["hSLinkColourHistory"] = 'This setting allows you to choose a colour for \'small\' links that your visitor has already followed.';
+$GLOBALS["hMenuLinkColour"] = 'This is the colour to use for menu links in your site.';
+$GLOBALS["hMenuLinkColourHover"] = 'Use this colour setting to highlight menu links when the cursor is hovering over them.';
+$GLOBALS["hMenuLinkColourHistory"] = 'This setting allows you to choose a colour for menu links that your visitor has already followed.';
+$GLOBALS["hTopMenuLinkColour"] = 'This is the colour to use for top menu links in your site.';
+$GLOBALS["hTopMenuLinkColourHover"] = 'Use this colour setting to highlight top menu links when the cursor is hovering over them.';
+$GLOBALS["hTopMenuLinkColourHistory"] = 'This setting allows you to choose a colour for top menu links that your visitor has already followed.';
+$GLOBALS["hRColLinkColour"] = 'This is the colour to use for right column links in your site.';
+$GLOBALS["hRColLinkColourHover"] = 'Use this colour setting to highlight right column links when the cursor is hovering over them.';
+$GLOBALS["hRColLinkColourHistory"] = 'This setting allows you to choose a colour for right column links that your visitor has already followed.';
+$GLOBALS["hSLinkFontSize"] = 'Use this setting to change the font size used for \'small\' links.';
+$GLOBALS["hRColFontSize"] = 'Use this setting to change the font size in the right contents column.';
+$GLOBALS["hRColFontStyle"] = 'Use this setting to change the link style in the right contents column.';
+$GLOBALS["hCBorder"] = 'This is the colour used for the heading block and border for main body content items.';
+$GLOBALS["hCHeader"] = 'This is the colour used for the header text for the main body content.';
+$GLOBALS["hCBackground"] = 'This is the background colour for the main body content.';
+$GLOBALS["hCFont"] = 'This is the text colour used for the main body content.';
+$GLOBALS["hTBorder"] = 'This is the colour used for the heading block and border for teaser content items.';
+$GLOBALS["hTHeader"] = 'This is the colour used for the header text for teaser content.';
+$GLOBALS["hTBackground"] = 'This is the background colour for teaser content.';
+$GLOBALS["hTFont"] = 'This is the text colour used for teaser content.';
+$GLOBALS["hCFontSize"] = 'Set the font size to be used for the text on your site.';
+$GLOBALS["hCFontStyle"] = 'Set the font style to be used for the text on your site.';
+$GLOBALS["hRColCBorder"] = 'This is the colour used for the heading block and border for main body content items in the right column.';
+$GLOBALS["hRColCHeader"] = 'This is the colour used for the header text for the main body content in the right column.';
+$GLOBALS["hRColCBackground"] = 'This is the background colour for the main body content in the right column.';
+$GLOBALS["hRColCFont"] = 'This is the text colour used for the main body content in the right column.';
+
+//  Error Messages
+$GLOBALS["eMainBGWrong"] = 'Main background value is wrong';
+$GLOBALS["eBorderBGWrong"] = 'Border background value is wrong';
+$GLOBALS["eHeaderBGWrong"] = 'Header background value is wrong';
+$GLOBALS["eFooterBGWrong"] = 'Footer background value is wrong';
+$GLOBALS["eContentBGWrong"] = 'Content background value is wrong';
+$GLOBALS["eMenuBGWrong"] = 'Menu background value is wrong';
+$GLOBALS["eTopmenuBGWrong"] = 'Top menu background value is wrong';
+$GLOBALS["eLinkWrong"] = 'Link colour value is wrong';
+$GLOBALS["eLinkHoverWrong"] = 'Link colour hover value is wrong';
+$GLOBALS["eLinkVisitedWrong"] = 'Link colour visited value is wrong';
+$GLOBALS["eMenuLinkWrong"] = 'Menu link colour value is wrong';
+$GLOBALS["eMenuLinkHoverWrong"] = 'Menu link colour hover value is wrong';
+$GLOBALS["eMenuLinkVisitedWrong"] = 'Menu link colour visited value is wrong';
+$GLOBALS["eTopMenuLinkWrong"] = 'Top menu link colour value is wrong';
+$GLOBALS["eTopMenuLinkHoverWrong"] = 'Top menu link colour hover value is wrong';
+$GLOBALS["eTopMenuLinkVisitedWrong"] = 'Top menu link colour visited value is wrong';
+$GLOBALS["eRColLinkWrong"] = 'Right column link colour value is wrong';
+$GLOBALS["eRColLinkHoverWrong"] = 'Right column link colour hover value is wrong';
+$GLOBALS["eRColLinkVisitedWrong"] = 'Right column link colour visited value is wrong';
+$GLOBALS["eSLinkWrong"] = 'Small link colour value is wrong';
+$GLOBALS["eSLinkHoverWrong"] = 'Small link colour hover value is wrong';
+$GLOBALS["eSLinkVisitedWrong"] = 'Small link colour visited value is wrong';
+$GLOBALS["eCHeaderWrong"] = 'Content Header font colour value is wrong';
+$GLOBALS["eCFontWrong"] = 'Content font colour value is wrong';
+$GLOBALS["eCBorderWrong"] = 'Content border colour value is wrong';
+$GLOBALS["eTeaserBGWrong"] = 'Teaser background value is wrong';
+$GLOBALS["eTBorderWrong"] = 'Teaser border colour value is wrong';
+$GLOBALS["eTHeaderWrong"] = 'Teaser Header font colour value is wrong';
+$GLOBALS["eTFontWrong"] = 'Teaser font colour value is wrong';
+$GLOBALS["eRColBGWrong"] = 'Right column background value is wrong';
+$GLOBALS["eRColBorderWrong"] = 'Right column border colour value is wrong';
+$GLOBALS["eRColHeaderWrong"] = 'Right column header font colour value is wrong';
+$GLOBALS["eRColFontWrong"] = 'Right column font colour value is wrong';
+
+?>
