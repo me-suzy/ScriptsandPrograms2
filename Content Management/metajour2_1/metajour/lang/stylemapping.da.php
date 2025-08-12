2f8e056e7cd2599e140077ede80490230a3c5add
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:37 +0300

    Add Content Management/metajour2_1/metajour/lang/stylemapping.da.php

---
+++ Content Management/metajour2_1/metajour/lang/stylemapping.da.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Typografi mapping';
+$LANG['label_name'] = 'Navn';
+$LANG['label_mapping'] = 'CSS klasse';
+$LANG['label_orderby'] = 'Sortering';
+
+?>
