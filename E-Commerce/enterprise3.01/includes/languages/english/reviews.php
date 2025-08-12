4126a78c404e51129493b755dd0abc0d6a15558d
me-suzy <me.suzana@gmail.com>
2025-08-12 11:53:43 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/reviews.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/reviews.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Reviews');
+define('HEADING_TITLE', 'Read What Others Are Saying');
+define('TEXT_OF_5_STARS', '%s of 5 Stars!');
+?>
