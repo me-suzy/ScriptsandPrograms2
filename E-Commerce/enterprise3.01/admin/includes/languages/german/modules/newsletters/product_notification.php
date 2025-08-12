8b998869d5cee31f8fb6144303712dfd9eea90fa
me-suzy <me.suzana@gmail.com>
2025-08-12 11:46:44 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/german/modules/newsletters/product_notification.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/german/modules/newsletters/product_notification.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('TEXT_COUNT_CUSTOMERS', 'Anzahl der Kunden, welche das Rundschreiben erhalten: %s');
+define('TEXT_PRODUCTS', 'Produkte');
+define('TEXT_SELECTED_PRODUCTS', 'ausgew&auml;hlte Produkte');
+
+define('JS_PLEASE_SELECT_PRODUCTS', 'Bitte wählen Sie zumindest ein Produkt aus!');
+
+define('BUTTON_GLOBAL', 'Global');
+define('BUTTON_SELECT', '>>>');
+define('BUTTON_UNSELECT', '<<<');
+define('BUTTON_SUBMIT', 'Weiter');
+define('BUTTON_CANCEL', 'Abbrechen');
+?>
