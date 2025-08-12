99a4123c6de25041798e9d92fc114eca37c5efc6
me-suzy <me.suzana@gmail.com>
2025-08-12 19:41:55 +0300

    Add News Publishing/aWebNews/aWebNews/footer.php

---
+++ News Publishing/aWebNews/aWebNews/footer.php
@@ -0,0 +1,4 @@
+<div align="center"><div class="nav-box"><a href="http://labs.aweb.com.au/awebnews.php">aWebNews</a> is developed by <a href="http://labs.aweb.com.au">aWeb Labs</a>
+</div></div>
+	</body>
+</html>
