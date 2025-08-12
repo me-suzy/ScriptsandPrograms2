7f7162670bfa0d4f0e3dd019986cb8212451bf45
me-suzy <me.suzana@gmail.com>
2025-08-13 01:58:31 +0300

    Add Unsorted/contenido-4.4.2/contenido/external/wysiwyg/spaw/lib/themes/classic/js/toolbar.js.php

---
+++ Unsorted/contenido-4.4.2/contenido/external/wysiwyg/spaw/lib/themes/classic/js/toolbar.js.php
@@ -0,0 +1,21 @@
+<?php 
+  header('Content-Type: application/x-javascript');
+?>
+// toolbar button effects
+function SPAW_classic_bt_over(ctrl)
+{
+  ctrl.className = "SPAW_classic_tb_over";
+}
+function SPAW_classic_bt_out(ctrl)
+{
+  ctrl.className = "SPAW_classic_tb_out";
+}
+function SPAW_classic_bt_down(ctrl)
+{
+  ctrl.className = "SPAW_classic_tb_down";
+}
+function SPAW_classic_bt_up(ctrl)
+{
+  ctrl.className = "SPAW_classic_tb_out";
+}
+
