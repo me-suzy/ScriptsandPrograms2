c328a5625181536e57c4be4e4075701c84716ff5
me-suzy <me.suzana@gmail.com>
2025-08-12 18:47:36 +0300

    Add Interactive Stories/FicHive_v1/skins/1/story.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/story.tmpl.php
@@ -0,0 +1,8 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'>
+<table width='100%'><tr><td><b><%TITLE%></b><br><%AUTHOR%> &bull; <%RATING%> &bull; <%PUBLISHED%> &bull; <%REVIEWS%></td>
+<td><%REVIEW%> <%ADD2FAV%> <%PRINTOUT%> <%BOOKMARK%></td>
+<td><div align='right'><%CHNAV%></div></td></tr></table></td></tr>
+<tr><td class='chapter'><%CHAPTER%></td></tr>
+<tr><td class='fframe'><div align='right'><%CHNAV%></div></td></tr>
+</table>
