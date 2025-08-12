4c2c86f6949c512e1a19fb089747b66e485be808
me-suzy <me.suzana@gmail.com>
2025-08-12 12:00:42 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/product_info.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/product_info.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('TEXT_PRODUCT_NOT_FOUND', 'Produkt wurde nicht gefunden!');
+define('TEXT_CURRENT_REVIEWS', 'Aktuelle Meinungen:');
+define('TEXT_MORE_INFORMATION', 'F&uuml;r weitere Informationen, besuchen Sie bitte die <a href="%s" target="_blank"><u>Homepage</u></a> zu diesem Produkt.');
+define('TEXT_DATE_ADDED', 'Dieses Produkt haben wir am %s in unseren Katalog aufgenommen.');
+define('TEXT_DATE_AVAILABLE', '<font color="#ff0000">Dieses Produkt wird voraussichtlich ab dem %s wieder vorr&auml;tig sein.</font>');
+define('TEXT_ALSO_PURCHASED_PRODUCTS', 'Kunden, die dieses Produkt gekauft haben, haben auch folgende Produkte gekauft:');
+define('TEXT_PRODUCT_OPTIONS', 'Optionen:');
+define('TEXT_CLICK_TO_ENLARGE', 'F&uuml;r eine gr&ouml;ssere Darstellung<br>klicken Sie auf das Bild.');
+?>
