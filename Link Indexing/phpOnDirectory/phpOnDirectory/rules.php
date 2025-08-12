e7d7bbad83f24429fe2e5092087d03d6d451b117
me-suzy <me.suzana@gmail.com>
2025-08-12 19:22:26 +0300

    Add Link Indexing/phpOnDirectory/phpOnDirectory/rules.php

---
+++ Link Indexing/phpOnDirectory/phpOnDirectory/rules.php
@@ -0,0 +1,107 @@
+<?php
+
+# choose a banner
+
+include_once('includes/db_connect.php');
+
+include_once('banner.inc.php');
+
+include('Templates/maintemplate.header.inc.php');
+
+?>
+
+
+
+        <table border="0" cellpadding="0" cellspacing="0" width="95%">
+
+          <tr>
+
+            <td width="100%" height="30" colspan="3">
+
+              <h1>Listing Policy</h1></td>
+
+          </tr>
+
+          <tr>
+
+            <td width="100%" valign="top" align="left" colspan="3">
+
+            </td>
+
+          </tr>
+
+          <tr>
+
+            <td width="100%" colspan="3">
+
+        <p>You are invited to place your link on our online dating directory.
+
+                  We ask in return that you include a link back to the directory
+
+                  from an indexed HTML page on your site. </p>
+
+                <p>To qualify for inclusion in the directory, your site should
+
+                  meet the following criteria:</p>
+
+                <hr>
+
+                <p><em>You must place a link back to us using one of our supplied
+
+                  links, on an indexed page no further than 1 click from the home
+
+                  page of the site to be listed.</em></p>
+
+                <p><em> You must not be an affiliate site (such that your data
+
+                  comes from another sites' database or you drive users to another
+
+                  site).</em></p>
+
+                <p><em>Sites must contain a reasonable amount of content (i.e.
+
+                  adverts) to be eligible for listing. We only list dating and
+
+                  related romance sites. Adult contacts sites must be tasteful.
+
+                  No porn sites.  </em></p>
+
+                <p><em>To be listed under 100% free, you MUST be 100% free for
+
+                  ALL services that you provide in relation to the site you are
+
+                  listing.</em></p>
+
+                <p><em>Sites must be listed under their own single domain rather
+
+                  than a) spread across multiple domains or b) a sub-domain of
+
+                  a free hosting service (such as tripod, angelfire, geocities
+
+                  etc.)</em></p>
+
+                <p><em>Sites must provide a good user experience and not include
+
+                  multiple pop-ups, music, unasked for installations or pages
+
+                  that cannot be exited. </em></p>
+
+                <hr>
+
+                <p>If your site does not meet the criteria above then please do
+
+                  not apply. We only list high quality sites that give our visitors
+
+                  useful information or content.
+
+                <p>
+
+            </td>
+
+          </tr>
+
+        </table>
+
+
+
+<?include('Templates/maintemplate.footer.inc.php');?>
