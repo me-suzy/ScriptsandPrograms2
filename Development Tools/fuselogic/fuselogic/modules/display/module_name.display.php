9e08832c907e592c9b35297aa11e9ce5c54ddf7f
me-suzy <me.suzana@gmail.com>
2025-08-12 09:14:55 +0300

    Add Development Tools/fuselogic/fuselogic/modules/display/module_name.display.php

---
+++ Development Tools/fuselogic/fuselogic/modules/display/module_name.display.php
@@ -0,0 +1,9 @@
+<?php
+
+$FL_MODULE_SETTING['module_name'] = 'display';
+$FL_MODULE_SETTING['sub_module']['_replace'] = 'replace.php';
+$FL_MODULE_SETTING['sub_module']['xhtml'] = 'xhtml.php';
+$FL_MODULE_SETTING['sub_module']['show_info'] = 'show_fuselogic_info.php';
+$FL_MODULE_SETTING['sub_module']['hide_info'] = 'hide_fuselogic_info.php';
+
+?>
