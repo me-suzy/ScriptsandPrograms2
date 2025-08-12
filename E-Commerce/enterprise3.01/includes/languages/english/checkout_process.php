66f1d339ab67f6a8e05390d36f2e8f18e1f271ff
me-suzy <me.suzana@gmail.com>
2025-08-12 11:52:46 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/checkout_process.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/checkout_process.php
@@ -0,0 +1,27 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('EMAIL_TEXT_SUBJECT', 'Order Process');
+define('EMAIL_TEXT_ORDER_NUMBER', 'Order Number:');
+define('EMAIL_TEXT_INVOICE_URL', 'Detailed Invoice:');
+define('EMAIL_TEXT_DATE_ORDERED', 'Date Ordered:');
+define('EMAIL_TEXT_PRODUCTS', 'Products');
+define('EMAIL_TEXT_SUBTOTAL', 'Sub-Total:');
+define('EMAIL_TEXT_TAX', 'Tax:        ');
+define('EMAIL_TEXT_SHIPPING', 'Shipping: ');
+define('EMAIL_TEXT_TOTAL', 'Total:    ');
+define('EMAIL_TEXT_DELIVERY_ADDRESS', 'Delivery Address');
+define('EMAIL_TEXT_BILLING_ADDRESS', 'Billing Address');
+define('EMAIL_TEXT_PAYMENT_METHOD', 'Payment Method');
+
+define('EMAIL_SEPARATOR', '------------------------------------------------------');
+define('TEXT_EMAIL_VIA', 'via');
+?>
