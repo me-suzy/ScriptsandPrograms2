f43465e8ac229779ea74d6c93dc74013b46113ae
me-suzy <me.suzana@gmail.com>
2025-08-12 14:47:20 +0300

    Add File Manipulation/PHPfileNavigator/PHPfileNavigator-2.1.0/PHPfileNavigator/data/include/mime.php

---
+++ File Manipulation/PHPfileNavigator/PHPfileNavigator-2.1.0/PHPfileNavigator/data/include/mime.php
@@ -0,0 +1,149 @@
+<?php
+/****************************************************************************
+* data/include/mime.php
+*
+* Listado de iconos existentes con su correspondiente mimetype
+*
+
+PHPfileNavigator versión 2.0.2
+
+Copyright (C) 2004-2005 Lito <lito@eordes.com>
+
+http://phpfilenavigator.litoweb.net/
+
+Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los
+términos de la Licencia Pública General de GNU según es publicada por la Free
+Software Foundation, bien de la versión 2 de dicha Licencia o bien (según su
+elección) de cualquier versión posterior.
+
+Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA
+GARANTÍA, incluso sin la garantía MERCANTIL implícita o sin garantizar la
+CONVENIENCIA PARA UN PROPÓSITO PARTICULAR. Véase la Licencia Pública General de
+GNU para más detalles.
+
+Debería haber recibido una copia de la Licencia Pública General junto con este
+programa. Si no ha sido así, escriba a la Free Software Foundation, Inc., en
+675 Mass Ave, Cambridge, MA 02139, EEUU.
+*******************************************************************************/
+
+defined('OK') or die();
+
+return array(
+	'ace' => 'application/x-ace-compressed',
+	'ai' => 'application/postscript',
+	'aif' => 'audio/x-aiff',
+	'ani' => 'application/x-navi-animation',
+	'asf' => 'video/x-ms-asf',
+	'asp' => 'text/asp',
+	'avi' => 'video/x-msvideo',
+	'bak' => 'application/x-trash',
+	'bat' => 'application/x-msdos-program',
+	'bin' => 'application/x-binary',
+	'bmp' => 'image/bmp',
+	'bz2' => 'application/x-bzip2',
+	'bz' => 'application/x-bzip',
+	'cab' => 'application/x-compress',
+	'cdr' => 'application/x-cdr',
+	'cfg' => 'text/plain',
+	'com' => 'application/octet-stream',
+	'conf' => 'text/plain',
+	'cpt' => 'application/x-cpt',
+	'css' => 'text/css',
+	'cur' => 'image/x-win-bitmap',
+	'dat' => 'application/octet-stream',
+	'db' => 'application/x-paradox',
+	'dcr' => 'application/x-director',
+	'dic' => 'text/plain',
+	'diff' => 'application/x-patch',
+	'dir' => 'application/x-director',
+	'dll' => 'application/x-msdownload',
+	'dmg' => 'application/octet-stream',
+	'doc' => 'application/msword',
+	'dwg' => 'image/x-dwg',
+	'edir' => '',
+	'eml' => 'message/rfc822',
+	'eps' => 'application/postscript',
+	'exe' => 'application/octet-stream',
+	'fla' => 'application/x-shockwave-flash',
+	'fon' => 'application/x-truetype-font',
+	'gif' => 'image/gif',
+	'gz' => 'application/x-compressed',
+	'hqx' => 'application/x-binhex40',
+	'html' => 'text/html',
+	'htm' => 'text/html',
+	'ico' => 'image/x-icon',
+	'inc' => 'text/plain',
+	'ini' => 'text/plain',
+	'iso' => '',
+	'jpeg' => 'image/jpeg',
+	'jpg' => 'image/jpeg',
+	'js' => 'application/x-javascript',
+	'log' => 'text/plain',
+	'm3u' => 'audio/x-mpegurl',
+	'mdb' => 'pplication/x-msaccess',
+	'midi' => 'audio/midi',
+	'mid' => 'audio/midi',
+	'mov' => 'video/quicktime',
+	'mp3' => 'audio/mpeg3',
+	'mpeg' => 'video/mpeg',
+	'mpg' => 'video/mpeg',
+	'nfo' => 'text/plain',
+	'odb' => 'application/vnd.oasis.opendocument.database',
+	'odc' => 'application/vnd.oasis.opendocument.chart',
+	'odf' => 'application/vnd.oasis.opendocument.formula',
+	'odg' => 'application/vnd.oasis.opendocument.graphics',
+	'odm' => 'application/vnd.oasis.opendocument.text-master',
+	'odp' => 'application/vnd.oasis.opendocument.presentation',
+	'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+	'odt' => 'application/vnd.oasis.opendocument.text',
+	'ogg' => 'audio/x-ogg',
+	'otg' => 'application/vnd.oasis.opendocument.graphics-template',
+	'oth' => 'application/vnd.oasis.opendocument.text-web',
+	'otp' => 'application/vnd.oasis.opendocument.presentation-template',
+	'ots' => 'application/vnd.oasis.opendocument.spreadsheet-template',
+	'ott' => 'application/vnd.oasis.opendocument.text-template',
+	'patch' => 'application/x-patch',
+	'pdf' => 'application/pdf',
+	'php3' => 'application/x-httpd-php',
+	'php' => 'application/x-httpd-php',
+	'phtml' => 'application/x-httpd-php',
+	'pl' => 'text/x-script.perl',
+	'png' => 'image/png',
+	'pps' => 'application/mspowerpoint',
+	'ppt' => 'application/powerpoint',
+	'psd' => 'application/octet-stream',
+	'pwl' => 'text/plain',
+	'qt' => 'video/quicktime',
+	'ram' => 'audio/x-pn-realaudio',
+	'ra' => 'audio/x-realaudio',
+	'rar' => 'application/x-compressed',
+	'reg' => 'text/plain',
+	'rpm' => 'audio/x-pn-realaudio-plugin',
+	'rtf' => 'text/richtext',
+	'sh' => 'text/x-script.sh',
+	'shtml' => 'text/x-server-parsed-html',
+	'sit' => 'application/x-sit',
+	'sql' => 'text/plain',
+	'svg' => 'image/svg',
+	'swf' => 'application/x-shockwave-flash',
+	'sxc' => 'application/vnd.sun.xml.calc',
+	'sxi' => 'application/vnd.sun.xml.impress',
+	'sxw' => 'application/vnd.sun.xml.writer',
+	'sys' => '',
+	'tar' => 'application/x-tar',
+	'tgz' => 'application/x-compressed',
+	'tiff' => 'image/tiff',
+	'tif' => 'image/tiff',
+	'tmp' => '',
+	'tpl' => 'text/plain',
+	'ttf' => 'application/x-truetype-font',
+	'txt' => 'text/plain',
+	'wav' => 'audio/wav',
+	'wma' => 'audio/x-ms-wma',
+	'wmv' => 'video/x-ms-wmv',
+	'wp' => 'application/wordperfect',
+	'xls' => 'application/vnd.ms-excel',
+	'zip' => 'application/x-compressed',
+);
+?>
+
