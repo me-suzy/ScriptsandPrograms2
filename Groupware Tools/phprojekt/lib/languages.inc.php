6ec30fa3c4ac1f7377d6bfc0f4ae357019375004
me-suzy <me.suzana@gmail.com>
2025-08-12 17:18:25 +0300

    Add Groupware Tools/phprojekt/lib/languages.inc.php

---
+++ Groupware Tools/phprojekt/lib/languages.inc.php
@@ -0,0 +1,44 @@
+<?php
+
+// $Id: languages.inc.php,v 1.3 2005/03/17 15:59:40 fgraf Exp $
+
+$languages = array(
+                    'al' => 'Albanian',
+                    'br' => 'Brazilian',
+                    'bg' => 'Bulgarian',
+                    'ct' => 'Catalan',
+                    'zh' => 'China,P.R.C.(GB)',
+                    'cz' => 'Czech',
+                    'da' => 'Danish',
+                    'nl' => 'Dutch',
+                    'ee' => 'Estonian',
+                    'en' => 'English',
+                    'es' => 'Espanol (Castellano)',
+                    'sp' => 'Espanol (latin)',
+                    'eh' => 'Euskadi',
+                    'fi' => 'Finnish',
+                    'fr' => 'French',
+                    'ge' => 'Georgian',
+                    'de' => 'German',
+                    'gr' => 'Greek',
+                    'he' => 'Hebrew',
+                    'hu' => 'Hungarian',
+                    'it' => 'Italiano',
+                    'is' => 'Icelandic',
+                    'jp' => 'Japanese',
+                    'ko' => 'Korean',
+                    'lt' => 'Lithuanian',
+                    'lv' => 'Latvian',
+                    'no' => 'Norwegian',
+                    'pt' => 'Portugues',
+                    'pl' => 'Polish',
+                    'ro' => 'Romanian',
+                    'ru' => 'Russian',
+                    'si' => 'Slovenian',
+                    'sk' => 'Slovak',
+                    'se' => 'Svenska',
+                    'tr' => 'Turkish',
+                    'tw' => 'Taiwan,R.O.C.(TW)'
+                );
+
+?>
