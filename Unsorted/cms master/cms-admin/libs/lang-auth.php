180e342681645786acfc12263ead19cfdc1b6180
me-suzy <me.suzana@gmail.com>
2025-08-13 01:46:57 +0300

    Add Unsorted/cms master/cms-admin/libs/lang-auth.php

---
+++ Unsorted/cms master/cms-admin/libs/lang-auth.php
@@ -0,0 +1,25 @@
+<?php
+//=============================================================================
+//
+//     CMS Master :: Content management system
+//     File: /cms-admin/libs/lang-auth.php
+//     Version: 1.0
+//     Created date: 28 September 2003
+//     Modification date: 28 September 2003
+//
+//============================================================================
+
+class LangAuth {
+
+    var $page_header = "Login to CMS Master";
+    
+    var $field_login = "Login";
+    var $field_password = "Password";
+    
+    var $button_login = "Login";
+    
+    var $error_invalid_login = "Invalid login or password. Please try again.";
+
+}
+
+?>
