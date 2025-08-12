d24227a067c214afca72a35a0493436e151ffaa4
me-suzy <me.suzana@gmail.com>
2025-08-12 15:18:25 +0300

    Add Flash and PHP/flashslideshow/flashSlideShow-0.2/example2.php

---
+++ Flash and PHP/flashslideshow/flashSlideShow-0.2/example2.php
@@ -0,0 +1,28 @@
+<?php
+//include class file
+include('class.flashslideshow.php');
+
+/*
+create new object with the following variabel
+flashSlideShow($width, $height, $interval, $bgcolor)
+$width, movie width in pixel
+$height, movie height in pixel
+$interval, interval in seconds between each images
+$bgcolor, movie backgground color in hexadecimal value
+*/
+$movie = new flashSlideShow(300, 225, 3, '#FFFFFF');
+
+
+/*
+differences from example1.php
+instead of add image one by one, we add sequences of images in array
+this array may come from readdir function,database query etc
+*/
+$myfile = array('image1.jpg','image2.jpg');
+$movie->addImages($myfile);
+
+
+//save movie
+//now check file name test.swf in current directory
+$movie->save('test.swf');
+?>
