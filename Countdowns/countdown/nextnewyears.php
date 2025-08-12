3ee988b7e379071812ba09b4e66133a1401327d5
me-suzy <me.suzana@gmail.com>
2025-08-12 08:26:00 +0300

    Add Countdowns/countdown/nextnewyears.php

---
+++ Countdowns/countdown/nextnewyears.php
@@ -0,0 +1,10 @@
+<?
+echo "&toyear=".(date("Y")+1);
+echo "&event_name=Newyears ".(date("Y")+1);
+echo "&tosec=0";
+echo "&tomin=0";
+echo "&tohour=0";
+echo "&today=1";
+echo "&tomonth=1";
+echo "&vloaded=1";
+?>
