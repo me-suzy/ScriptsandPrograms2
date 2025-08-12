74764189610e7286a69965c10908ec907fdd2860
me-suzy <me.suzana@gmail.com>
2025-08-12 07:11:39 +0300

    Add Content Management/metajour2_1/metajour/lang/stylemapping.en.php

---
+++ Content Management/metajour2_1/metajour/lang/stylemapping.en.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Stylesheet mapping';
+$LANG['label_name'] = 'Name';
+$LANG['label_mapping'] = 'CSS class';
+$LANG['label_orderby'] = 'Sort order';
+
+?>
