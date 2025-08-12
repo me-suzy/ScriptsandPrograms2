a758b96a597fa9d017989ac2bdbd34f4428d105d
me-suzy <me.suzana@gmail.com>
2025-08-12 11:04:36 +0300

    Add Documents/circeOS_cv1.1/circeOS_cv/config.inc.php

---
+++ Documents/circeOS_cv1.1/circeOS_cv/config.inc.php
@@ -0,0 +1,13 @@
+<?
+
+$SITE_name = "circeOS Curriculum";
+
+/*--------------------------------------------------------------*/
+/* CV DB ORACLE CONFIGURATION									*/
+/*--------------------------------------------------------------*/
+
+$CV_ORACLE_db_name		= "(DESCRIPTION=(ADDRESS =(PROTOCOL = TCP)(HOST = sytrmworacle2)(PORT = 1521))(CONNECT_DATA = (SID = CURRICUL)))";
+$CV_ORACLE_db_user		= "curricadmin";
+$CV_ORACLE_db_password	= "curricadmin";
+
+?>
