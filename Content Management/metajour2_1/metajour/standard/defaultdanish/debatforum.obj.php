bfe92e26ee3e9989c2452a30f69a2c194db95227
me-suzy <me.suzana@gmail.com>
2025-08-12 07:12:35 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultdanish/debatforum.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultdanish/debatforum.obj.php
@@ -0,0 +1,18 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 18;
+$_objchilds[] = '_child_debatforum_19';
+function objfile_18 () {
+$obj = owNew('document');
+$objdata['name'] = "Debatforum";
+$objdata['alias'] = "";
+$objdata['templateid'] = "0";
+$objdata['stylesheetid'] = "0";
+$objdata['metadataid'] = "0";
+$objdata['structureid'] = "0";
+$objdata['nolist'] = "0";
+$objdata['nosearch'] = "0";
+$obj->createObject($objdata);
+return $obj;
+}
+?>
