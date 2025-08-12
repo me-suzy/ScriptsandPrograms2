03226829c9c33d85d59cbba8f993166373193c67
me-suzy <me.suzana@gmail.com>
2025-08-12 06:12:57 +0300

    Add Content Management/metajour2_1/metajour/extension/changepassword/changepassword.class.php

---
+++ Content Management/metajour2_1/metajour/extension/changepassword/changepassword.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+require_once($system_path . "extension/basicextension.class.php");
+
+class ext_changepassword extends basicextension {
+
+	function ext_changepassword() {
+		$this->basicextension();
+		$this->extname = 'changepassword';
+		$this->addextparam('templatename');
+		$this->addextparam('templateid');
+	}
+
+	function _do() {
+		$this->useTemplate('templatename','templateid','standard_change_password');
+	}
+
+}
+?>
