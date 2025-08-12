454c892508eaa47e3c211e5a0d0449f2ba6fd506
me-suzy <me.suzana@gmail.com>
2025-08-12 08:04:59 +0300

    Add Content Management/Vision_Source_0.5.0.2_Beta_Released_14th_November/Vision Source 0.5/visionsource/includes/config.php

---
+++ Content Management/Vision_Source_0.5.0.2_Beta_Released_14th_November/Vision Source 0.5/visionsource/includes/config.php
@@ -0,0 +1,25 @@
+<?php 
+/*
+//////////////////////////////////////////////////////////////
+//															//
+//		Vision Source v0.5 Beta								//
+//		Created by Ben Maynard copyright 2005				//		
+//		Email: volvorules@gmail.com							//
+//		URL: http://www.visionsource.org					//
+//		Created: 3rd March 2005								//
+//															//
+//----------------------------------------------------------//
+//															//
+//		Script: config.php									//
+//		written by: Ben Maynard								//
+//															//
+//////////////////////////////////////////////////////////////
+*/
+
+if ( ! defined( "DIRECT" ) )
+{	die("<h1>Access denied</h1>You are not allowed to access this file directly.");
+}
+
+//Please run the installer!!
+  
+?>
