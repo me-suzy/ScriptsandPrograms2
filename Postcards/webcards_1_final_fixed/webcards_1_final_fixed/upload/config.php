3970a044dd55f7459e978bb7987115cbb0a4c9cd
me-suzy <me.suzana@gmail.com>
2025-08-12 23:27:52 +0300

    Add Postcards/webcards_1_final_fixed/webcards_1_final_fixed/upload/config.php

---
+++ Postcards/webcards_1_final_fixed/webcards_1_final_fixed/upload/config.php
@@ -0,0 +1,52 @@
+<?php
+$conf['admin_script'] = "./admin.php";
+$conf['auto_expire'] = "n";
+$conf['ban_message'] = "Sorry!
+<br /><br />Your IP address has been banned by an adminsitrator.
+<br /><br />
+Please try again later!!";
+$conf['buffer'] = "y";
+$conf['cat_to_show'] = "5";
+$conf['cc_address'] = "";
+$conf['date_format'] = "d F Y - h:i a";
+$conf['db_driver'] = "MySQL";
+$conf['dbhost'] = "localhost";
+$conf['dbname'] = "webcards";
+$conf['dbpass'] = "";
+$conf['dbprefix'] = "";
+$conf['dbuser'] = "root";
+$conf['default_pub_lang'] = "English";
+$conf['dir'] = "";
+$conf['email_ban'] = "";
+$conf['email_from'] = "";
+$conf['enable_notify'] = "y";
+$conf['enable_resends'] = "y";
+$conf['expiry_time'] = "30";
+$conf['expiry_units'] = "86400";
+$conf['font_bg_colours'] = "white,black,red,blue,yellow,green,orange,purple,brown,gray,pink";
+$conf['font_colours'] = "black,black,red,blue,yellow,green,orange,purple,brown,gray,pink";
+$conf['font_faces'] = "verdana,arial,helvetica";
+$conf['font_sizes'] = "medium,small,large,x-large,xx-large";
+$conf['get_post_show'] = "b";
+$conf['img_per_row'] = "4";
+$conf['ip_ban'] = "";
+$conf['mailer'] = "WebCards Mailer";
+$conf['max_message_length'] = "2000";
+$conf['max_recip'] = "5";
+$conf['new_pop_imgs'] = "5";
+$conf['pickup_script'] = "./pickup.php";
+$conf['query_count'] = "b";
+$conf['render_time'] = "b";
+$conf['script'] = "./index.php";
+$conf['server_load'] = "b";
+$conf['smtp_host'] = "localhost";
+$conf['smtp_pass'] = "";
+$conf['smtp_port'] = "25";
+$conf['smtp_user'] = "";
+$conf['sql_show'] = "b";
+$conf['thumb_format'] = "png";
+$conf['thumb_height'] = "";
+$conf['thumb_width'] = "150";
+$conf['url'] = "";
+$conf['use_mail_smtp'] = "mail";
+?>
