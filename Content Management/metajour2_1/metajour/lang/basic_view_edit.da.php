1e363d25ab873713ba3f347e6beb5e037939d9aa
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:06 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_edit.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_edit.da.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['title'] = 'Redigering';
+
+$LANG['img_create'] = 'Opret ny';
+$LANG['img_list'] = 'Liste';
+$LANG['img_hierarchy'] = 'Hierarkisk oversigt';
+$LANG['img_createvariant'] = 'Opret variant';
+$LANG['img_delete'] = 'Slet';
+$LANG['img_previous'] = 'Forrige';
+$LANG['img_next'] = 'Næste';
+
+$LANG['button_save'] = 'Gem';
+?>
