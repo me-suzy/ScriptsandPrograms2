a3edd4d6aa06d9df1b429dafda6fa2e40218acfb
me-suzy <me.suzana@gmail.com>
2025-08-12 10:02:41 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/classButtonPart.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/classButtonPart.php
@@ -0,0 +1,20 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+includeClass('PntButtonPart', 'pnt/web/parts');
+
+/** Part that outputs a button in html. Used by ButtonsPanel.
+*
+* This concrete subclass is here to keep de application developers
+* code (including localization overrides) separated from the framework code.
+* @see http://www.phppeanuts.org/site/index_php/Menu/178
+* Framework code is in * Framework code is in superclass.
+* This class may be copied to an application folder to
+* make application specific overrides.
+* @see http://www.phppeanuts.org/site/index_php/Pagina/65
+*/
+class ButtonPart extends PntButtonPart {
+
+}
+?>
