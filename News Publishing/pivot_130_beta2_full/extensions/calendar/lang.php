570d82a24cc12556380d95bc2784f7a692f7b824
me-suzy <me.suzana@gmail.com>
2025-08-12 20:07:28 +0300

    Add News Publishing/pivot_130_beta2_full/extensions/calendar/lang.php

---
+++ News Publishing/pivot_130_beta2_full/extensions/calendar/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * These are the language strings used in PCalendar.
+ */
+
+$lang['pcalendar'] = array(
+
+	'calendar_summary'   => 'This table represents a calendar of entries in the weblog with hyperlinks on dates with entries.',
+	'calendar_noscript'  => 'The calendar provides a means to access entries in this weblog',
+
+);
+
+
+?>
