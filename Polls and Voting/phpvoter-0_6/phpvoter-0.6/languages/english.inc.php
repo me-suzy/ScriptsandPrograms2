8a3008bbd81fb5cbe0d0cf457f6f690a1ab99596
me-suzy <me.suzana@gmail.com>
2025-08-12 22:00:11 +0300

    Add Polls and Voting/phpvoter-0_6/phpvoter-0.6/languages/english.inc.php

---
+++ Polls and Voting/phpvoter-0_6/phpvoter-0.6/languages/english.inc.php
@@ -0,0 +1,86 @@
+<?php
+
+/* ***************************** */
+/* LANGUAGE STRINGS FOR PHPVOTER */
+/* ***************************** */
+
+$lang['vote_button'] = "Vote";
+$lang['onevote'] = "vote";
+$lang['votes'] = "votes";
+$lang['result_saved'] = "Your vote has been saved.<br />\n";
+$lang['createvote'] = "Create/Change a poll";
+$lang['question'] = "The poll";
+$lang['alreadyvoted'] = "You have already voted on this poll.";
+$lang['viewresults'] = "Show results";
+$lang['noip'] = "Your browser didn't set the REFERER so your vote hasn't been saved.<br />\n";
+$lang['notactive'] = "<h1>Sorry!</h1>\nThat poll is no longer active and cannot be voted on.\n";
+$lang['activequestion'] = "<b>Current poll:</b><br />\n";
+$lang['totalvoters'] = "<br />Number of votes: ";
+$lang['info'] = <<<ENDSTRING
+These polls are in no way scientific and the results should only be
+interpreted as indications and not as real facts. If you have requests
+for polls that we should post, just send us an <a href="mailto:nobody@nobody.com">email</a>.
+<br /><br />
+ENDSTRING;
+$lang['sorry'] = "Sorry";
+$lang['no_votes'] = "No votes";
+$lang['no_votes_in_db'] = "There are no polls in the database.";
+$lang['no_active_vote'] = "There is no active poll.";
+$lang['error_header'] = "An error has occured";
+$lang['admin_menu'] = "PHPVoter Administration";
+$lang['admin_list'] = "List all polls";
+$lang['admin_create'] = "Create a new poll";
+$lang['admin_list_header'] = "List of polls";
+$lang['voteid'] = "ID";
+$lang['question'] = "Question";
+$lang['date'] = "Date";
+$lang['state'] = "Type";
+$lang['answers'] = "Options";
+$lang['voters'] = "Votes";
+$lang['function'] = "Function";
+$lang['inactive'] = "Inactive";
+$lang['active'] = "Active";
+$lang['unfinished'] = "Unpublished";
+$lang['deleted'] = "Deleted";
+$lang['showvote'] = "Show/Edit";
+$lang['admin_show_vote'] = "Show/Edit a poll";
+$lang['admin_voter_list'] = "List of votes";
+$lang['IP'] = "IP";
+$lang['voterid'] = "ID";
+$lang['answer'] = "Vote";
+$lang['delete'] = "Remove";
+$lang['change'] = "Change";
+$lang['choice_id'] = "ID";
+$lang['admin_delete_question'] = "Remove this poll";
+$lang['admin_change_question'] = "Change this poll";
+$lang['admin_vote_question'] = "Vote on this poll";
+$lang['admin_set_this_active'] = "Make this poll active";
+$lang['admin_return'] = "Back to admin";
+$lang['identifier_deleted'] = "IP removed";
+$lang['votes_updated'] = "The number of votes has been updated.";
+$lang['votes_not_updated'] = "The number of votes has not been updated since the answers aren't stored in the database.";
+$lang['vote_activated'] = "This poll has been activated.";
+$lang['one_active_vote'] = "Only one active poll is allowed, all other polls has been made inactive.";
+$lang['question_deleted'] = "The chosen poll has been removed.";
+$lang['voters_deleted'] = "The list of voters has been deleted.";
+$lang['comment'] = "Comment to the poll (is shown in complete listing and on the result page)";
+$lang['savequestion'] = "Save poll / Add new option";
+$lang['new_answer'] = "New";
+$lang['save_question'] = "Save/Change poll";
+$lang['answer_deleted'] = "The option has been deleted";
+$lang['question_updated'] = "The poll has been updated.";
+$lang['error_incorrect_values'] = "One or more values were incorrectly entered or was missing, nothing has been saved.";
+$lang['error_access_denied'] = "Access denied!";
+$lang['publish'] = "Publishing date";
+$lang['information'] = "Information";
+$lang['admin_menu'] = "Menu";
+$lang['no_voters'] = "Noone has voted on this poll yet.";
+$lang['question_state'] = "Status of poll: ";
+$lang['question_comment'] = "Poll comment: ";
+$lang['vote_on_question'] = "Vote on a poll";
+$lang['set_active_vote'] = "Poll activated";
+$lang['error_sql_query'] = "SQL query";
+$lang['error_sql'] = "A SQL error occured";
+$lang['error_notemplate'] = "Couldn't find the required template file.";
+
+?>
