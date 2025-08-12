59e0c4ee8a5f96ca7349d8a9cdad3f1474e6c1be
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:47 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_affiliate.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_affiliate.php
@@ -0,0 +1,35 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Affiliate Entrar');
+define('HEADING_TITLE', 'Affiliate Entrar');;
+define('HEADING_TITLE_ERROR', 'Registration Error');
+
+define('TEXT_AFFILIATE_LOGOFF', 'Log Out');
+define('TEXT_AFFILIATE_ID', 'Affiliate E-Mail:');
+define('TEXT_AFFILIATE_PASSWORD', 'Contraseña:');
+
+define('HEADING_NEW_AFFILIATE', 'Nuevo Affiliate');
+define('TEXT_NEW_AFFILIATE', 'I am a new affiliate.');
+define('TEXT_NEW_AFFILIATE_INTRODUCTION', 'By creating an affiliate account at ' . STORE_NAME . ' you will be able to earn valuable extra revenue for by referring your website visitors to us.');
+define('TEXT_NEW_AFFILIATE_TERMS','Ponga aqui sus condiciones de Affiliate.');
+
+define('HEADING_RETURNING_AFFILIATE', 'Ya Soy Affiliate');
+define('TEXT_RETURNING_AFFILIATE', 'I am a returning affiliate.');
+
+define('TEXT_AFFILIATE_PASSWORD_FORGOTTEN', '¿Ha olvidado su contraseña? Siga este enlace y se la enviamos.');
+
+define('TEXT_LOGIN_ERROR', '<font color="#ff0000"><b>ERROR:</b></font> El \'E-Mail\' y/o \'Contraseña\' no figuran en nuestros datos.');
+?>
