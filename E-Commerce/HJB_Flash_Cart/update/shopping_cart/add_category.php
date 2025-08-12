abda404c183025cc671541c8133ff923158851e4
me-suzy <me.suzana@gmail.com>
2025-08-12 12:23:30 +0300

    Add E-Commerce/HJB_Flash_Cart/update/shopping_cart/add_category.php

---
+++ E-Commerce/HJB_Flash_Cart/update/shopping_cart/add_category.php
@@ -0,0 +1,117 @@
+<?php
+	include_once("$DOCUMENT_ROOT/library/auth.php");
+	include_once("$DOCUMENT_ROOT/library/db.php");
+	include_once("$DOCUMENT_ROOT/library/menus.php");
+
+/*
+***************************************************************************************************************************
+*****************************************COPYRIGHT 2005 YOU MAY NOT USE THIS WITHOUT PERMISSION****************************
+
+HJB IS PROVIDED "As Is" FOR USE ON WEBSITES WHERE A LICENSE FOR SUCH USE WAS PURCHASED.  IT MAY ONLY BE USED ON ONE SITE PER LICENSING
+FEE.  IN ORDER TO USE ON ADDITIONAL SITES, ADDITIONAL LICENSES MUST BE PURCHASED.  
+
+
+THE PHP SCRIPTS MAY BE ALTERED, AS LONG AS THE CREDIT LINE AND LINKS AT THE BOTTOM OF EACH PAGE REMAIN. THE FLASH MAY NOT IN ANY
+WAY BE CHANGED OR ALTERED.  ANY VIOLATION OF THESE TERMS WILL RESULT IN THE FORFEITING OF YOUR RIGHT TO USE THIS SOFTWARE.
+
+NationWideShelving.com does not guarantee this software in anyway.  You use this at your own risk.  NationWideShelving or any of its
+employees or subsidiaries are not responsible for any damage, and / or loss of business, reputation, or other damages of any kind
+which are caused whether actual or not, by the use of this product.  By using this product you agree to hold NationWideShelving, its
+employees, and all subsidiaries harmless for any and all reasons associated with your use of this product.
+
+Your installation of this software consititues an agreement to these terms.
+
+****************************************************************************************************************************
+	*/
+
+	session_start();
+
+	
+	//connect to database
+		$connect=mysql_connect($host_default, $login_default, $pw_default);
+		$select_db=mysql_select_db($db_default);
+	
+	//check to see if this is a submission
+	if ($_POST['submitted']=="true"){
+	
+		
+		
+		$query="INSERT INTO cartcategories (name) VALUES ('".$_POST['name']."')";
+		$result=mysql_query($query);
+		$catID=mysql_insert_id();
+		
+		$query2="INSERT INTO cartcategories_to_sites (sitesID, cartCategoriesID) VALUES ('".$_POST['siteID']."', '".$catID."')";
+		$result2=mysql_query($query2);
+		
+		if ($result){
+			header ("location: /update/shopping_cart/categories.php?siteID=".$_POST['siteID']);
+		}
+	}
+	$query="SELECT * FROM sites WHERE id='".$_GET['siteID']."'";
+	$result=mysql_query($query);
+	$site=mysql_fetch_array($result);
+	
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Manage Shopping Cart</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+.style1 {font-family: Verdana, Arial, Helvetica, sans-serif}
+.style12 {font-family: Verdana, Arial, Helvetica, sans-serif; color: #FF0000; font-size: 18pt; }
+-->
+</style>
+</head>
+
+<body>
+<?php //INCLUDE HEADER
+include ("$DOCUMENT_ROOT/library/header.php"); ?>
+<table width="100%"  border="1" cellpadding="0" cellspacing="0" bordercolor="#010556">
+  <tr>
+    <td><table width="100%"  border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td width="18%" valign="top" bgcolor="#010556"><div align="center"><br>
+              <br>
+              <br>
+              <br>
+              <?php menu_shopping_cart(); ?>          
+              <br>              
+          </div></td>
+          <td width="82%" valign="top"><table width="100%"  border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="1%">&nbsp;</td>
+                <td width="99%"><span class="style12">Add Category To <?php echo $site['name']; ?><br> 
+                  </span><br>
+                  <table width="100%"  border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <td width="4%">&nbsp;</td>
+                      <td width="96%"><form name="form1" method="post" action="">
+                        <table width="100%"  border="0" cellspacing="0" cellpadding="0">
+                          <tr>
+                            <td width="33%"><div align="right"><span class="style1">Category Name: </span></div></td>
+                            <td width="3%">&nbsp;</td>
+                            <td width="64%"><input name="name" type="text" id="name"></td>
+                          </tr>
+                          <tr>
+                            <td colspan="3"><div align="center">
+                              <input type="submit" name="Submit" value="Add Category">
+                              <input name="submitted" type="hidden" id="submitted" value="true">
+                              <input name="siteID" type="hidden" id="siteID" value="<?php echo $_GET ['siteID']; ?>">
+</div></td>
+                            </tr>
+                        </table>
+                      </form>                        <br></td>
+                    </tr>
+                  </table>                  
+                  <br></td>
+              </tr>
+          </table></td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+</body>
+</html>
