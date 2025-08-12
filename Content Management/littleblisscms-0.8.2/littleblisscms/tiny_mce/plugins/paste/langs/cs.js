49f7e2cbbba1c1a62ee09506988da5ac950df0c4
me-suzy <me.suzana@gmail.com>
2025-08-12 05:44:24 +0300

    Add Content Management/littleblisscms-0.8.2/littleblisscms/tiny_mce/plugins/paste/langs/cs.js

---
+++ Content Management/littleblisscms-0.8.2/littleblisscms/tiny_mce/plugins/paste/langs/cs.js
@@ -0,0 +1,8 @@
+// CS lang variables
+
+tinyMCELang['lang_paste_text_desc'] = 'Vloit neformátovaný text';
+tinyMCELang['lang_paste_text_title'] = 'Poui CTRL+V na klávesnici pro vloení textu do okna.';
+tinyMCELang['lang_paste_text_linebreaks'] = 'Nechej pr(eruení r(ádku*';
+tinyMCELang['lang_paste_word_desc'] = 'Vloit text z aplikace Word';
+tinyMCELang['lang_paste_word_title'] = 'Poui CTRL+V na klávesnici pro vloení textu do okna.';
+tinyMCELang['lang_selectall_desc'] = 'Oznac(it ve';
