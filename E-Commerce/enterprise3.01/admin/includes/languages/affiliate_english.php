16b635afe2dde2980f37e414870de44355cf8f60
me-suzy <me.suzana@gmail.com>
2025-08-12 11:41:16 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/affiliate_english.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/affiliate_english.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Ecommerce Shopping Cart Software
+  http://www.ecommerceshoppingcartsoftware.org
+
+  Copyright (c) 2004 Ecommerce Shopping Cart Software Developers.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+// reports box text in includes/boxes/affiliate.php
+define('BOX_HEADING_AFFILIATE', 'Affiliates');
+define('BOX_AFFILIATE_SUMMARY', 'Summary');
+define('BOX_AFFILIATE', 'Affiliates');
+define('BOX_AFFILIATE_PAYMENT', 'Payment');
+define('BOX_AFFILIATE_BANNERS', 'Banners');
+define('BOX_AFFILIATE_CONTACT', 'Contact');
+define('BOX_AFFILIATE_SALES', 'Sales');
+define('BOX_AFFILIATE_CLICKS', 'Clicks');
+?>
