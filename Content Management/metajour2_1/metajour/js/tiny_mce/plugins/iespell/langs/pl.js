332e50172a55fc2500323994b047186b70b7199e
me-suzy <me.suzana@gmail.com>
2025-08-12 06:39:39 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/iespell/langs/pl.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/iespell/langs/pl.js
@@ -0,0 +1,6 @@
+// PL lang variables
+
+tinyMCE.addToLang('',{
+iespell_desc : 'Uruchom sprawdzanie pisowni',
+iespell_download : "Nie wykryto pluginu, kliknij aby przejæ do strony z pluginami."
+});
