d82febb53ce420575eeb786a8b3ddd3aab2bb97d
me-suzy <me.suzana@gmail.com>
2025-08-12 13:57:50 +0300

    Add Exchanges/phpBannerExchange2.0/exchange/docs/installmenu.php

---
+++ Exchanges/phpBannerExchange2.0/exchange/docs/installmenu.php
@@ -0,0 +1,13 @@
+<div id="llinks">
+<div class="sidetitle"><? echo "Install Guide"; ?></div>
+  <div class="side">
+<a href="install.php#new">What's New</a><br>
+<a href="install.php#quickstart">Quick Start</a><br>
+<a href="install.php#detailed">Install.php</a><br>
+<a href="install.php#vars">Variables</a><br>
+<a href="install.php#dbstructure">Database Structure</a><br>
+<a href="install.php#upgrading">Upgrading</a><br>
+<a href="install.php#gethelp">Getting Help/Bugs</a>
+  </div>
+  </div>
+  </div>
