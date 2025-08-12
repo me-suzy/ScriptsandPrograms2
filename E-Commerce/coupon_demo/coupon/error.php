a194fde3a048c39c32179470e0dc0e810e36df3c
me-suzy <me.suzana@gmail.com>
2025-08-12 11:17:50 +0300

    Add E-Commerce/coupon_demo/coupon/error.php

---
+++ E-Commerce/coupon_demo/coupon/error.php
@@ -0,0 +1,17 @@
+<html>
+<head></head>
+
+<body>
+<center><table width = 50%>
+<tr><td>
+<p><font color="#FF0000">There was an error!</font> We have detected an older version of Netscape web browser.</p>
+
+<p>While we try to make our site compatible to many browser types there are some that we just can not accommodate due to the scripts we run like our coupon script.</p>
+
+<p>You may want to consider upgrading your browser to a newer version.</p>
+
+
+
+<p>Thank you for your understanding.</p>
+</td></tr></table></center>
+</body></html>
