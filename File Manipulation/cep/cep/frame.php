8c5f318ea5be02e67a1655e48c7cc7b08b2eed4c
me-suzy <me.suzana@gmail.com>
2025-08-12 14:12:48 +0300

    Add File Manipulation/cep/cep/frame.php

---
+++ File Manipulation/cep/cep/frame.php
@@ -0,0 +1,8 @@
+<HTML>
+<HEAD>
+</HEAD>
+<FRAMESET name=a COLS="100,*" frameborder=0 border=0 framespacing=0>
+<FRAME SRC="cpleft.php" NAME="cpLeft" frameborder=0 marginwidth=0 marginheight=0 topmargin=0 scrolling="AUTO">
+<FRAME SRC="cpright.php" NAME="cpRight" frameborder=0 marginwidth=2 marginheight=0 topmargin=0 scrolling="NO">
+</FRAMESET>
+</HTML>
