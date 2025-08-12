806fa80dc186b0957934bb6c952327155abe88b4
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:17 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_usergroups.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_usergroups.php
@@ -0,0 +1,19 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Vertaal gebruikersgroep namen';
+
+//  List Headings
+$GLOBALS["tUsergroupname"] = 'Gebruikersgroep naam';
+
+//  List Functions
+$GLOBALS["tViewPrivilege"] = 'Toon groepsrechten';
+$GLOBALS["tEditPrivilege"] = 'Vertaal groepsrechten';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Met dit formulier kunt u de namen van gebruikersgroepen vertalen.';
+
+//  Error Messages
+$GLOBALS["eNoUsergroupName"] = 'U moet een vertaling invoeren voor elke taal';
+
+?>
