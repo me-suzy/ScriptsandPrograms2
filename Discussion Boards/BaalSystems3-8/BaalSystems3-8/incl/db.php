7d1a96311afc70fb281f66997e5e5632aab99a46
me-suzy <me.suzana@gmail.com>
2025-08-12 10:25:38 +0300

    Add Discussion Boards/BaalSystems3-8/BaalSystems3-8/incl/db.php

---
+++ Discussion Boards/BaalSystems3-8/BaalSystems3-8/incl/db.php
@@ -0,0 +1,10 @@
+<?php
+    $db=array(
+              "host"=>"192.168.0.5",
+              "user"=>"root",
+              "pass"=>"slammer",
+              "dbname"=>"Baal"
+             );
+                
+    $tableprefix="_";
+?>
