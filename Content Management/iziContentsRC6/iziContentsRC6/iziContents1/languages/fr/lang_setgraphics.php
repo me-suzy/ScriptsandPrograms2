8a996bb28044765e08b97fe38994f721c3a08101
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:49 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_setgraphics.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_setgraphics.php
@@ -0,0 +1,88 @@
+<?
+
+// Form Title
+$GLOBALS["tFormTitle"]			= 'Réglages du Graphisme du Site';
+
+//  Form Block Titles
+$GLOBALS["thSiteLogo"] = 'Logo du Site';
+$GLOBALS["thBackgrounds"] = 'Images d\'Arrière-plan';
+$GLOBALS["thPagingIcons"] = 'Paging Icones';
+$GLOBALS["thMenuIcons"] = 'Menu Icones';
+$GLOBALS["thSpecialIcons"] = 'Spécial Icones';
+
+// Form Field Headings
+$GLOBALS["tHomepageLogo"]		= 'Image du Logo du Site';
+$GLOBALS["tTopBackground"] = 'Haut arrière-plan';
+$GLOBALS["tTopBackgroundRepeat"] = 'Répéter haut arrière-plan';
+$GLOBALS["tTopBackgroundFixed"] = 'Fixe le haut arrière-plan';
+$GLOBALS["tMainBackground"]		= 'Image du Fond de la Page Principale';
+$GLOBALS["tMainBackgroundRepeat"]	= 'Répéter Image Fond Page Principale';
+$GLOBALS["tMainBackgroundFixed"] = 'Fixer l\'arrière-plan principal de page';
+$GLOBALS["tContentBackground"]		= 'Image du Fond du Header du Contenu';
+$GLOBALS["tContentBackgroundRepeat"]	= 'Répéter Image du Fond du Header du Contenu';
+$GLOBALS["tTopMenuBackground"] = 'Menu arrière-plan';
+$GLOBALS["tTopMenuBackgroundRepeat"] = 'Répéter l\'arrière-plan de menu';
+$GLOBALS["tTopMenuBackgroundFixed"] = 'Fixer l\'arrière-plan du menu';
+$GLOBALS["tMenuBackground"]		= 'Image du Fond du Menu';
+$GLOBALS["tMenuBackgroundRepeat"]	= 'Répéter Image du Fond du Menu';
+$GLOBALS["tMenuBackgroundFixed"] = 'Fixer l\'arrière-plan du menu';
+$GLOBALS["tFooterBackground"] = 'Pied de page arrière-plan';
+$GLOBALS["tFooterBackgroundRepeat"] = 'Répéter le pied de page arrière-plan';
+$GLOBALS["tFooterBackgroundFixed"] = 'Fixed footer background';
+$GLOBALS["tFirstPageIcon"] = 'La première icône de Page';
+$GLOBALS["tPrevPageIcon"] = 'L\'icône précédente de Page';
+$GLOBALS["tNextPageIcon"] = 'L\'icône prochaine de Page';
+$GLOBALS["tLastPageIcon"] = 'La dernière icône de Page';
+$GLOBALS["tExpandIcon"] = 'Icon menu expansion';
+$GLOBALS["tCollapseIcon"] = 'Effondrer l\'icône de menu';
+$GLOBALS["tNoExpandIcon"] = 'L\'icône de menu non-expansion';
+$GLOBALS["tSecureIcon"] = 'L\'icône sécuritaire menu';
+$GLOBALS["tPrintIcon"] = '\'Printerfriendly icone\'';
+$GLOBALS["tRatingIcon"] = 'L\'icône de Classement d\'article';
+$GLOBALS["tCommentIcon"] = 'L\'icône Commentaire d\'article';
+$GLOBALS["tTopBackground"]		= 'Image du Fond du Haut de Page';
+$GLOBALS["tTopBackgroundRepeat"]	= 'Répéter Image du Fond du Haut de Page';
+
+// Form Text Description
+$GLOBALS["tDetails"]			 = 'Ici vous pouvez définir les images de fond de votre site.<br>';
+$GLOBALS["tDetails"]			.= 'Vérifiez que les images que vous voulez utiliser aient bien été téléchargées avec l\'utilitaire de téléchargement des images.';
+
+$GLOBALS["hHomepageLogo"]		 = 'Ceci est le logo principal de votre site, apparaissant en haut à gauche.';
+$GLOBALS["hTopBackground"] = 'Ceci réglera l\'arrière-plan pour le cadre d\'en-tête avec votre logo et la bannière, a en-tête \"userdata\" le cadre si vous avez configuré votre site pour ceci.';
+$GLOBALS["hTopBackgroundRepeat"] = 'Répéter (le carreau) l\'image d\'arrière-plan d\'en-tête.';
+$GLOBALS["hTopBackgroundFixed"] = 'Crochez cette boîte si vous voulez que l\'arrière-plan de cadre d\'en-tête pour soit resté a réparé si c\'est dans une région défilant';
+$GLOBALS["hMainBackground"]		 = 'Cela détermine le fond principal.<br>';
+$GLOBALS["hMainBackground"]		.= 'Si vous réglez la "Couleur de Fond du Tableau" et la "Couleur de la Bordure du Tableau" sur \'vide\' cela rendra le tableau du contenu  transparent et l\'image de fond ';
+$GLOBALS["hMainBackground"]		.= 'sera aussi visible \'sous\' le tableau.';
+$GLOBALS["hMainBackgroundRepeat"]	.= 'Répétition (remplissage) avec l\'image de fond principale.';
+$GLOBALS["hMainBackgroundFixed"] = 'Crochez cette boîte si vous voulez que l\'arrière-plan de page principal pour soit resté a réparé si c\'est dans une région défilant';
+$GLOBALS["hContentBackground"]		 = 'Image de Fond du Header du Contenu. Déteminée pour tous les articles publiés sur le site.<br>';
+$GLOBALS["hContentBackground"]		.= 'L\'avantage est de pouvoir placer du texte par dessus. Quand vous choisissez une image comme header ';
+$GLOBALS["hContentBackground"]		.= '(dans le management du contenu) vous aurez besoin de séparer les images pour chaque article. Vous pouvez aussi utiliser ';
+$GLOBALS["hContentBackground"]		.= 'l\'image du header et l\'image de fond ensemble.';
+$GLOBALS["hContentBackgroundRepeat"]	 = 'Répétition (remplissage) avec l\'image de fond du contenu.';
+$GLOBALS["hTopMenuBackground"] = 'Ceci réglera la première image d\'arrière-plan de menu. Si vous réglez la première couleur d\'arrière-plan de menu à l\'empty de "empty la table qui est d\'habitude montrée vers le menu disparaîtra donner un meilleur effet quand un arrière-plan est monté.';
+$GLOBALS["hTopMenuBackgroundRepeat"] = 'Répéter (le carreau) la première image d\'arrière-plan de menu.';
+$GLOBALS["hTopMenuBackgroundFixed"] = 'Vérifier cette boîte si vous voulez que le premier arrière-plan de cadre de menu pour soit resté a réparé si c\'est dans une région défilant';
+$GLOBALS["hMenuBackground"]		.= 'Ceci détermine l\'image de fond du Menu.';
+$GLOBALS["hMenuBackground"]		.= 'Si vous réglez la couleur de fond du menu sur "vide" le tableau qui apparait autour du menu disparaitra donnant un meilleur effet visuel lorsque qu\'une image de fond est utiliséee.';
+$GLOBALS["hMenuBackgroundRepeat"]	 = 'Répétition (remplissage) avec l\'image de fond du menu.';
+$GLOBALS["hMenuBackgroundFixed"] = 'Crochez cette boîte si vous voulez que l\'arrière-plan de cadre de menu pour soit resté a réparé si c\'est dans une région défilant';
+$GLOBALS["hFooterBackground"] = 'Ceci réglera l\'arrière-plan pour le cadre de pied de page si vous avez rendu capable cette caractéristique de ezContents.';
+$GLOBALS["hFooterBackgroundRepeat"] = 'Répéter (le carreau) l\'image d\'arrière-plan de pied de page.';
+$GLOBALS["hFooterBackgroundFixed"] = 'Crochez cette boîte si vous voulez que l\'arrière-plan de cadre de pied de page pour soit resté a réparé si c\'est dans une région défilant';
+$GLOBALS["hFirstPageIcon"] = 'Ceci est l\'icône montrée pour les articles de paged pour se retourner à la première page';
+$GLOBALS["hPrevPageIcon"] = 'Ceci est l\'icône montrée pour les articles de paged pour se retourner à la page précédente';
+$GLOBALS["hNextPageIcon"] = 'Ceci est l\'icône montrée pour les articles de paged pour se déplacer sur à la page prochaine';
+$GLOBALS["hLastPageIcon"] = 'Ceci est l\'icône montrée pour les articles de paged pour se déplacer sur à la dernière page';
+$GLOBALS["hExpandIcon"] = 'Ceci est l\'icône qui sera montrée à côté des entrées de menu qui peut être augmenté pour révéler un submenu.';
+$GLOBALS["hCollapseIcon"] = 'Cette icône apparaîtra à côté d\'un menu augmenté pour montrer que le sous menu peut s\'être effondré encore.';
+$GLOBALS["hNoExpandIcon"] = 'Cette icône de menu apparaîtra à côté des articles du menu qui ne peut pas être augmenté ou peut être effondré parce qu\'il y a non le sous menu pour augmenter.';
+$GLOBALS["hSecureIcon"] = 'Cette icône apparaîtra à côté des articles du menu limités pour montrer qu\'ils sont limités.';
+$GLOBALS["hPrintIcon"] = 'Cette icône fournit un lien à une imprimante une version amicale d\'un article.';
+$GLOBALS["hRatingIcon"] = 'Cette icône apparaît sur les articles qui peuvent être évalués par les lecteurs.';
+$GLOBALS["hCommentIcon"] = 'Cette icône apparaît sur les articles où les lecteurs peuvent faire des commentaires.';
+$GLOBALS["hTopBackground"]		 = 'Ceci détermine l\'image de fond de la frame du header avec votre logo et la bannière, la frame "données utilisateur" si vous l\'avez configurée pour cela, et le menu horizontal si vous avez validé cette option dans ezContents.';
+$GLOBALS["hTopBackgroundRepeat"]	 = 'Répétition (remplissage) avec l\'image de fond du header.';
+
+?>
