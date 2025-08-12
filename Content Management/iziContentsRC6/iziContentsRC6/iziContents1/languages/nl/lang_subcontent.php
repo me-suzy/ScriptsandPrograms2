52d84196aeef7692c69d151e7f02561c92bdffcc
me-suzy <me.suzana@gmail.com>
2025-08-12 04:00:04 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_subcontent.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_subcontent.php
@@ -0,0 +1,81 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'Beheer toegevoegde inhoud';
+$GLOBALS["tFormTitle2"] = 'Beheer toegevoegde inhoud configuration';
+$GLOBALS["tFormTitle3"] = 'Beheer toegevoegde inhoud categories';
+
+//  List Headings
+$GLOBALS["tEdit"] = 'Bewerk';
+$GLOBALS["tAddNew"] = 'Nieuwe module toevoegen';
+$GLOBALS["tAddNewCat"] = 'Nieuwe categorie toevoegen';
+$GLOBALS["tModuleName"] = 'Module naam';
+$GLOBALS["tModuleTitle"] = 'Module title';
+$GLOBALS["tAnonPosting"] = 'Anoniem Posten Toegestaan';
+$GLOBALS["tValReq"] = 'Bevestiging Vereist';
+$GLOBALS["tTablename"] = 'Tabel naam';
+$GLOBALS["tCategory"] = 'Category';
+$GLOBALS["tCatReference"] = 'Interne Referenties';
+
+//  List Functions
+$GLOBALS["tConfig"] = 'Configuratie';
+$GLOBALS["tCategories"] = 'Categorieën';
+
+//  Form Block Titles
+$GLOBALS["thGeneral"] = 'Toegevoegde Inhoud Configuratie';
+$GLOBALS["thSubmissions"] = 'Toevoeging Details';
+$GLOBALS["thSubmissionRules"] = 'Regels van toevoeging';
+$GLOBALS["thDisplay"] = 'Toon Settings ';
+
+// Toggel Modules
+$GLOBALS["tConfirmRelease"] = 'activeer / de-activeer module';
+$GLOBALS["tReleaseModule"] = 'activeer / de-activeer module';
+
+//  Form Field Headings
+$GLOBALS["tModuleID"] = 'Module ID';
+$GLOBALS["tCatParent"] = 'Huidige Categorie';
+$GLOBALS["tSText"] = 'Toegevoegde Text';
+$GLOBALS["tSTextDisplay"] = 'Toon Toegevoegde Text';
+$GLOBALS["tSGraphic"] = 'Toegevoegde Graphic';
+$GLOBALS["tSGraphicDisplay"] = 'Toon Toegevoegde Graphic';
+$GLOBALS["tRegOnly"] = 'Alleen Geregistreerde Gebruikers';
+$GLOBALS["tUsergroups"] = 'Gebruikersgroepen';
+$GLOBALS["tValReg"] = 'Bevestiging Vereist';
+$GLOBALS["tUseCategories"] = 'Gebruik Sub-categories';
+$GLOBALS["tPerPage"] = 'Per Pagina';
+$GLOBALS["tCatName"] = 'Categorie Naam';
+$GLOBALS["tHiddenCat"] = 'Verborgen Categorie';
+$GLOBALS["tOrderBy"] = 'Sorteer Op';
+$GLOBALS["tPostedBy"] = 'Toon gepost door';
+$GLOBALS["tPostedDate"] = 'Toon geposte datum';
+
+//  Form Detail Comments and Help Texts
+$GLOBALS["tDetails"] = 'Gebruik dit formulier om gebruikers toevoegingen te configureren in speciale inhoud modules.';
+$GLOBALS["hModuleName"] = 'De module naam, een tag voor interne identificatie.';
+$GLOBALS["hModuleTitle"] = 'De moduletitel wordt getoond op de hoofdpagina voor dit type inhoud.';
+$GLOBALS["hSText"] = 'Text to display in header for user submissions.<br>This value will only be used if there is no language-specific text defined in the language files.';
+$GLOBALS["hSTextDisplay"] = 'Moet toegevoegde tekst (either from the language files, or the \'Display Submission Text\' field on this form) getoond worden in de header';
+$GLOBALS["hSGraphic"] = 'Graphic image to display in header for user submissions';
+$GLOBALS["hSGraphicDisplay"] = 'Moeten de grafische toevoegingen worden getoond in de header';
+$GLOBALS["hRegOnly"] = 'Kunnen slechts de geregistreerde auteurs/gebruikers toevoegingen toevoegen? of mag iedere gebruiker dat?';
+$GLOBALS["hUsergroups"] = 'Registered users with privilege to submit entries is restricted to the highlighted usergroups.<br />Leave blank to allow any member to submit.';
+$GLOBALS["hValReq"] = 'Indicate whether validation is required before submitted entries are released to display, i.e. whether they are pending an administrator release.';
+$GLOBALS["hPerPage"] = 'Het aantal toevoegingen per pagina te tonen.';
+$GLOBALS["hUseCategories"] = 'If this flag is checked, user-submitted articles will be associated with a series of sub-categories that the site administrator can define.';
+$GLOBALS["hOrderBy"] = 'Sorteer de toevoegingen op datum of op categorie/datum (if categories are enabled).';
+$GLOBALS["hPostedBy"] = 'Toon de gebruikersnaam van de degene die dit heeft toegevoegd';
+$GLOBALS["hPostedDate"] = 'Toon de datum/ tijd toen deze toevoeging werd gepost';
+$GLOBALS["hCatName"] = 'Geef een tekstbeschrijving voor deze categorie';
+$GLOBALS["hCatParent"] = 'Selecteer \'parent category\' van de lijst als je wilt dat deze categorie een \'child category\'.';
+$GLOBALS["hHiddenCat"] = 'Vink "Yes" als je wilt dat deze categorie (and all its children) wordt verborgen in de gebruikers drop-down lijst van categorien';
+
+$GLOBALS["toCategory"] = 'Categorie/Datum';
+$GLOBALS["toDate"] = 'Datum';
+
+//  Error messages
+$GLOBALS["eNoModules"] = 'Er zijn geen modules voor installatie';
+
+$GLOBALS["ePerPageNumber"] = 'Per Pagina moet de waarde een aantal zijn';
+$GLOBALS["ePerPageZero"] = 'Het aantal per pagina moet groter zijn dan 0';
+
+?>
