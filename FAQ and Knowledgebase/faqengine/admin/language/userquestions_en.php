295b8b000a0e29899e552c3eb2062fb119d7ba2e
me-suzy <me.suzana@gmail.com>
2025-08-12 14:09:03 +0300

    Add FAQ and Knowledgebase/faqengine/admin/language/userquestions_en.php

---
+++ FAQ and Knowledgebase/faqengine/admin/language/userquestions_en.php
@@ -0,0 +1,12 @@
+<?php
+/***************************************************************************
+ * (c)2001-2005 Boesch IT-Consulting (info@boesch-it.de)
+ ***************************************************************************/
+$l_uq_answer_prelude = "On {date} You posted the following question:";
+$l_uq_answer_subject = "Answer to FAQ-question posting (#{refid})";
+$l_uq_answer_is = "The answer is:";
+$l_uq_sc_subject = "State for FAQ-question posting (#{refid}) changed";
+$l_uq_sc_states = array("new","work in progress","deferred","finished");
+$l_uq_sc_body = "The state of Your FAQ-question posting (#{refid}) on {date} has been changed.\nThe new state is: {state}";
+$l_uq_dateformat = "m/d/Y H:i:s";
+?>
