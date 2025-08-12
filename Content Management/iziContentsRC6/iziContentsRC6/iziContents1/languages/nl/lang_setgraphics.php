8e704fa93f6b3ed95f8a32931f856a6639e914a7
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:54 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_setgraphics.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_setgraphics.php
@@ -0,0 +1,77 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Beheer van grafische instellingen';
+
+//  Form Block Titles
+$GLOBALS["thSiteLogo"] = 'Website logo';
+$GLOBALS["thBackgrounds"] = 'Achtergrond afbeelding';
+$GLOBALS["thPagingIcons"] = 'Pagina iconen';
+$GLOBALS["thMenuIcons"] = 'Menu iconen';
+$GLOBALS["thSpecialIcons"] = 'Speciale iconen';
+
+//  Form Field Headings
+$GLOBALS["tHomepageLogo"] = 'Startpagina logo afbeelding';
+$GLOBALS["tTopBackground"] = 'Kop achtergrond';
+$GLOBALS["tTopBackgroundRepeat"] = 'Herhaal kop achtergrond';
+$GLOBALS["tTopBackgroundFixed"] = 'Gefixeerd kop achtergrond';
+$GLOBALS["tMainBackground"] = 'Hoofdpagina achtergrond';
+$GLOBALS["tMainBackgroundRepeat"] = 'Herhaal hoofd pagina achtergrond';
+$GLOBALS["tMainBackgroundFixed"] = 'Gefixeerde hoofd pagina achtergrond';
+$GLOBALS["tContentBackground"] = 'Kop inhoud achtergrond';
+$GLOBALS["tContentBackgroundRepeat"] = 'Herhaal kop inhoud achtergrond';
+$GLOBALS["tTopMenuBackground"] = 'Topmenu achtergrond';
+$GLOBALS["tTopMenuBackgroundRepeat"] = 'Herhaal topmenu achtergrond';
+$GLOBALS["tTopMenuBackgroundFixed"] = 'Gefixeerde topmenu achtergrond';
+$GLOBALS["tMenuBackground"] = 'Menu achtergrond';
+$GLOBALS["tMenuBackgroundRepeat"] = 'Herhaal menu achtergrond';
+$GLOBALS["tMenuBackgroundFixed"] = 'Gefixeerde menu achtergrond';
+$GLOBALS["tFooterBackground"] = 'Voet achtergrond';
+$GLOBALS["tFooterBackgroundRepeat"] = 'Herhaal voet achtergrond';
+$GLOBALS["tFooterBackgroundFixed"] = 'Gefixeerde voet achtergrond';
+$GLOBALS["tFirstPageIcon"] = 'Eerste pagina icoon';
+$GLOBALS["tPrevPageIcon"] = 'Vorige pagina icoon';
+$GLOBALS["tNextPageIcon"] = 'Volgende pagian icoon';
+$GLOBALS["tLastPageIcon"] = 'Laatste pagina icoon';
+$GLOBALS["tExpandIcon"] = 'Uitvouwbaar menu icoon';
+$GLOBALS["tCollapseIcon"] = 'Uitgevouwen menu icoon';
+$GLOBALS["tNoExpandIcon"] = 'Niet-uitvouwbaar menu icoon';
+$GLOBALS["tSecureIcon"] = 'Beperkt menu icoon';
+$GLOBALS["tPrintIcon"] = 'Print vriendelijk icoon';
+$GLOBALS["tRatingIcon"] = 'Artikel beoordeling icoon';
+$GLOBALS["tCommentIcon"] = 'Artikel commentaar icoon';
+
+//  Form Text Descriptions
+$GLOBALS["tDetails"] = 'Hier kunt u de instellingen beheren van de afbeeldingen en achtergronden van uw website.<br />De afbeeldingen waarvan u gebruik wilt maken, moeten eerst geupload worden; dit kunt u doen bij het menu Inhoud, Beheer afbeeldingen, toevoegen nieuwe afbeelding.';
+
+$GLOBALS["hHomepageLogo"] 		= 'Dit is het hoofdlogo van uw site. Het wordt getoond links boven in het venster van de browser.';
+$GLOBALS["hTopBackground"] 		= 'Dit stelt de achtergrond is voor het kop frame. Hierin staat ook uw logo, en indien u dit zo hebt ingesteld ook het reclameblok en het \'Gebruikers data frame\'.';
+$GLOBALS["hTopBackgroundRepeat"] 	= 'Herhaal (tile) de afbeelding in de kop achtergrond.';
+$GLOBALS["hTopBackgroundFixed"] 	= 'Vink dit vakje aan, indien u wilt dat de achtergrond van het kop frame gefixeerd blijft wanneer dit zich in een scrollbaar gebied bevindt';
+$GLOBALS["hMainBackground"] 		= 'Dit stelt de hoofd achtergrond in.<br />Wanneer u de \'Tabel achtergrond kleur\' en  \'Tabel rand kleur\' leeg laat, zal de inhoudstabel transparant maken en deze achtergrond zal dan ook te zien zijn in de tabel.';
+$GLOBALS["hMainBackgroundRepeat"] = 'Herhaal (tile) de hoofdachtergrond afbeelding.';
+$GLOBALS["hMainBackgroundFixed"] = 'Vink dit vakje aan, indien u wilt dat de achtergrond van de hoofdpagina gefixeerd blijft wanneer dit zich in een scrollbaar gebied bevindt';
+$GLOBALS["hContentBackground"] = 'De achtergrond afbeelding voor de kop van een artikel. Dit wordt dan ingesteld voor alle artikelen die op de site gepubliceerd worden.<br />Het voordeel hiervan is dat u tekst bovenop deze afbeelding kunt zetten in het artikel. Wanneer u de kop een afbeelding maakt in artikel in het menu Inhoud, Beheer Inhoud, moet u voor elk artikele een separate afbeelding maken. U kunt de kop afbeelding en de achtergrond afbeelding ook tegelijkertijd gebruiken.';
+$GLOBALS["hContentBackgroundRepeat"] = 'Herhaal (tile) de inhoud achtergrond afbeelding.';
+$GLOBALS["hTopMenuBackground"] = 'Dit stelt de achtergrond afbeelding van het topmenu is. Indien u de topmenu achtergrond kleur leeg maakt, zal de tabel die gewoonlijk getoond wordt rondom het menu verdwijnen en dit geeft een fraaier effect voor de ingestelde achtergrond.';
+$GLOBALS["hTopMenuBackgroundRepeat"] = 'Herhaal (tile) de achtergrond afbeelding van het topmenu.';
+$GLOBALS["hTopMenuBackgroundFixed"] = 'Vink dit vakje aan, indien u wilt dat de achtergrond van het topmenu gefixeerd blijft wanneer dit zich in een scrollbaar gebied bevindt.';
+$GLOBALS["hMenuBackground"] = 'Dit stelt de achtergrond afbeelding van het menu in. Indien u de menu achtergrond kleur leeg maakt zal de tabel die gewoonlijk getoond wordt rondom het menu verdwijnen en dit geeft een fraaier effect voor de ingestelde achtergrond.';
+$GLOBALS["hMenuBackgroundRepeat"] = 'Herhaal (tile) de achtergrond afbeelding van het menu.';
+$GLOBALS["hMenuBackgroundFixed"] = 'Vink dit vakje aan, indien u wilt dat de achtergrond van het menu frame gefixeerd blijft wanneer dit zich in een scrollbaar gebied bevindt.';
+$GLOBALS["hFooterBackground"] = 'Dit stelt de achtergrond in voor het voet frame; wordt alleen getoond indien het voet frame ook aangezet is voor de website.';
+$GLOBALS["hFooterBackgroundRepeat"] = 'Herhaal (tile) de achtergrond afbeelding van het voet frame.';
+$GLOBALS["hFooterBackgroundFixed"] = 'Vink dit vakje aan, indien u wilt dat de achtergrond van het voet frame gefixeerd blijft wanneer dit zich in een scrollbaar gebied bevindt.';
+$GLOBALS["hFirstPageIcon"] = 'Dit icoon wordt getoond om terug te kunnen gaan naar de eerste pagina; wordt gebruikt voor  gepagineerde items (d.w.z. items die over meerdere pagina\'s getoond worden).';
+$GLOBALS["hPrevPageIcon"] = 'Dit icoon wordt getoond om terug te kunnen gaan naar de vorige pagina; wordt gebruikt voor  gepagineerde items (d.w.z. items die over meerdere pagina\'s getoond worden).';
+$GLOBALS["hNextPageIcon"] = 'Dit icoon wordt getoond om terug te kunnen gaan naar de volgende pagina; wordt gebruikt voor  gepagineerde items (d.w.z. items die over meerdere pagina\'s getoond worden).';
+$GLOBALS["hLastPageIcon"] = 'Dit icoon wordt getoond om terug te kunnen gaan naar de laatste pagina; wordt gebruikt voor  gepagineerde items (d.w.z. items die over meerdere pagina\'s getoond worden).';
+$GLOBALS["hExpandIcon"] = 'Dit icoon wordt getoond naast menu items die uitgevouwen kunnen worden om een sub menu weer te geven.';
+$GLOBALS["hCollapseIcon"] = 'Dit icoon wordt getoond naast een menu item dat uitgevouwen is om te tonen dat het weer dichtgevouwen kan worden.';
+$GLOBALS["hNoExpandIcon"] = 'Dit icoon wordt getoond naast een menu item dat niet uitgevouwen kan worden, en waar dus geen submenu\'s onder aanwezig zijn.';
+$GLOBALS["hSecureIcon"] = 'Dit icoon wordt getoond naast een menu item om te laten zien dat beperkt toegankelijk is.';
+$GLOBALS["hPrintIcon"] = 'Dit icoon geeft een link aan naar een print vriendelijke versie van een artikel.';
+$GLOBALS["hRatingIcon"] = 'Dit icoon verschijnt bij artikelen die door gebruikers beoordeeld kunnen worden.';
+$GLOBALS["hCommentIcon"] = 'Dit icoon verschijnt bij artikelen waarbij door gebruikers commentaar geleverd kan worden.';
+
+?>
