2ebe18ed14b12011496a64efff56e0f6382883d3
me-suzy <me.suzana@gmail.com>
2025-08-13 00:10:39 +0300

    Add Unsorted/12-all.v3.8.2.0.1/brand.inc.php

---
+++ Unsorted/12-all.v3.8.2.0.1/brand.inc.php
@@ -0,0 +1,54 @@
+<?PHP
+$brand_name = "1-2-All Broadcast E-mail Software -=-= NULLED by [GTT]=-=-";
+
+// ----------------------------------------------------------------------
+// Logo
+// Should be the full location to your logo
+// Suggest size is 150wx40h pixels
+// Good example:
+//      $brand_logo = "http://www.mysite.com/logo.gif";
+
+$brand_logo = "media/h_l.gif";
+
+// ----------------------------------------------------------------------
+// Copyright
+// License agreement does not allow you to set your own copyright,
+// but you may opt to have the copyright invisibe to public/non-code view
+// Copyrights may never be removed from the actual code.
+// Options:
+//     Show ActiveCampaign, Inc. Copyright
+//        $brand_copyright = "0";
+//     Remove ActiveCampaign, Inc. Copyright
+//        $brand_copyright = "1";
+
+$brand_copyright = "0";
+
+// ----------------------------------------------------------------------
+// Show Links?
+// You have the option to make the "Software Homepage" and "Support Center"
+// links visible or invisible.  These links will direct the user to
+// ActiveCampaign's web site.
+// Options:
+//     Leave Links Visible:
+//        $brand_links = "0";
+//     Make Links InVisible:
+//        $brand_links = "1";
+
+$brand_links = "0";
+
+// ----------------------------------------------------------------------
+// Hide serial in software details page?
+// If selected, the serial will NOT be visible in the control panel and
+// specifically the software details page.  By default, this is hidden.
+// If you change this to viewable, you must note that the usage of your
+// serial is your responsibility and should be kept confidential.
+// Options:
+//     Hide serial:
+//        $brand_serial = "0";
+//     Show serial:
+//        $brand_serial = "1";
+
+$brand_serial = "0";
+
+
+?>
