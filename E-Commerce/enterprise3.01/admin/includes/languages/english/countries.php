02764befdeb5c8a3e1c6382dbd27db417b85103a
me-suzy <me.suzana@gmail.com>
2025-08-12 11:42:03 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/countries.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/countries.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Countries');
+
+define('TABLE_HEADING_COUNTRY_NAME', 'Country');
+define('TABLE_HEADING_COUNTRY_CODES', 'ISO Codes');
+define('TABLE_HEADING_ACTION', 'Action');
+
+define('TEXT_INFO_EDIT_INTRO', 'Please make any necessary changes');
+define('TEXT_INFO_COUNTRY_NAME', 'Name:');
+define('TEXT_INFO_COUNTRY_CODE_2', 'ISO Code (2):');
+define('TEXT_INFO_COUNTRY_CODE_3', 'ISO Code (3):');
+define('TEXT_INFO_ADDRESS_FORMAT', 'Address Format:');
+define('TEXT_INFO_INSERT_INTRO', 'Please enter the new country with its related data');
+define('TEXT_INFO_DELETE_INTRO', 'Are you sure you want to delete this country?');
+define('TEXT_INFO_HEADING_NEW_COUNTRY', 'New Country');
+define('TEXT_INFO_HEADING_EDIT_COUNTRY', 'Edit Country');
+define('TEXT_INFO_HEADING_DELETE_COUNTRY', 'Delete Country');
+?>
