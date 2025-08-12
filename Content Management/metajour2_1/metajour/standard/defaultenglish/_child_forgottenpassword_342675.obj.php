dc50c17e64bc3b99a8031883575145527335b035
me-suzy <me.suzana@gmail.com>
2025-08-12 07:14:41 +0300

    Add Content Management/metajour2_1/metajour/standard/defaultenglish/_child_forgottenpassword_342675.obj.php

---
+++ Content Management/metajour2_1/metajour/standard/defaultenglish/_child_forgottenpassword_342675.obj.php
@@ -0,0 +1,17 @@
+<?php
+#IPW METAZO 2.0 OBJECT
+$_objoldid = 342675;
+function objfile_342675 () {
+$obj = owNew('documentsection');
+$objdata['name'] = "Forgotten password";
+$objdata['subname'] = "";
+$objdata['extension'] = "forgottenpassword";
+$objdata['configset'] = "";
+$objdata['params'] = "";
+$objdata['script'] = "0";
+$objdata['content'] = "";
+$obj->createObject($objdata);
+$obj->moveTo(2);
+return $obj;
+}
+?>
