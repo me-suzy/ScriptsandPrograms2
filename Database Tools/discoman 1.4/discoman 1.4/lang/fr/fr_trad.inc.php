89136929a20cfa2bb978fa7dbf68b742c4b08fe5
me-suzy <me.suzana@gmail.com>
2025-08-12 08:53:45 +0300

    Add Database Tools/discoman 1.4/discoman 1.4/lang/fr/fr_trad.inc.php

---
+++ Database Tools/discoman 1.4/discoman 1.4/lang/fr/fr_trad.inc.php
@@ -0,0 +1,46 @@
+<?
+//Boutons :
+
+$txt_fichier = "Fichier";
+$txt_consultation = "Consultation";
+$txt_connexion = "Connexion";
+$txt_ajouter = "Ajouter";
+$txt_rechercher = "Rechercher";
+$txt_recents = "Récents";
+$txt_infos = "Infos";
+$txt_admin = "Admin";
+$txt_aide = "Aide";
+$txt_a_propos = "A&nbsp;propos";
+$txt_recherche_rapide = "Recherche rapide";
+$txt_recherche_disco = "Rechercher un disque";
+$txt_chercher = "Chercher";
+
+//Libellés de champs ou colonnes :
+
+$txt_artistes = "Artistes";
+$txt_artiste = "Artiste";
+$txt_formats = "Formats";
+$txt_format = "Format";
+$txt_annees = "Années";
+$txt_annee = "Année";
+$txt_payss = "Pays";
+$txt_pays = "Pays";
+$txt_refs = "Références";
+$txt_ref = "Référence";
+$txt_com = "Commentaire";
+$txt_titres = "Titres";
+$txt_titre = "Titre";
+$txt_exact = "exactement";
+
+//Textes divers :
+
+$txt_ajouter_disque = "Ajouter disque";
+$txt_le_disque = "Le disque";
+$txt_succes = "a été ajouté avec succès.";
+$txt_image = "Image";
+$txt_results = "30 derniers enregistrements";
+$txt_no = "Aucun résultat";
+$txt_resultat = "Résultats pour";
+$txt_prec = "Page précedente";
+$txt_enregistrements = "Nombre d'enregistrements dans la base de données :";
+?>
