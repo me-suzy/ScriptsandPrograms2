3873c20dbdc66b3f8d4575a4bd355d9bb02ab0dd
me-suzy <me.suzana@gmail.com>
2025-08-13 01:16:39 +0300

    Add Unsorted/banner manager/include/connection.php

---
+++ Unsorted/banner manager/include/connection.php
@@ -0,0 +1,9 @@
+<?php
+	
+	$site = "http://www.yoursite.com"; //Change it to your full path (the parent directory where you install your banner
+	$your_username = ""; //fill in this value
+	$your_password = ""; //fill in this value
+	$your_database = ""; //fill in this value
+	$con = mysql_connect('localhost',$your_username,$your_password);
+	
+?>
