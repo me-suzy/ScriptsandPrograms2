8e6c94ef389e52dc2599ff7bff183496aaf9b883
me-suzy <me.suzana@gmail.com>
2025-08-13 01:00:01 +0300

    Add Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/index.php

---
+++ Unsorted/autolinks_pro_v2.0_nullified/cpanel/install/index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<link rel="stylesheet" href="main.css">
+</head>
+<body>
+<p>Choose what you want to do:</p>
+<p>Nullified Version &copy WTN Team `2002</p>
+<ol>
+  <li><a href="mysqldetails.php?type=newinstall">New installation of AutoLinks 2.0</a></li>
+  <li><a href="mysqldetails.php?type=upgrade13">Upgrade from AutoLinks 1.3</a></li>
+  <li><a href="mysqldetails.php?type=upgrade12">Upgrade from AutoLinks 1.2</a></li>
+</ol>
+</body>
+</html>
