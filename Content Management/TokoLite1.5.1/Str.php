bf4b5f128c925c817ee8a54ca9ccbf6f50b89644
me-suzy <me.suzana@gmail.com>
2025-08-12 08:04:01 +0300

    Add Content Management/TokoLite1.5.1/Str.php

---
+++ Content Management/TokoLite1.5.1/Str.php
@@ -0,0 +1,17 @@
+<?php
+
+include( "LoadConfig.php" );
+
+?>
+<html>
+	<head>
+		<link rel="stylesheet" href="toko.css" type="text/css"/>
+	</head>
+	<body style="background-color: #384655;">
+		<table height="100%">
+			<tr>
+				<td align="center" valign="center" class="miniTitle"><?= GetStr( $str ) ?></td>
+			</tr>
+		</table>
+	</body>
+</html>
