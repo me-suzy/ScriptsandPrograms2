e65df8793dce69f46f3446c3258b26ba5525d2a0
me-suzy <me.suzana@gmail.com>
2025-08-12 17:47:09 +0300

    Add Image Galleries/cpg1.4.2/cpg142/themes/classic/theme.php

---
+++ Image Galleries/cpg1.4.2/cpg142/themes/classic/theme.php
@@ -0,0 +1,24 @@
+<?php
+/*************************
+  Coppermine Photo Gallery
+  ************************
+  Copyright (c) 2003-2005 Coppermine Dev Team
+  v1.1 originaly written by Gregory DEMAR
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  Coppermine version: 1.4.2
+  $Source: /cvsroot/coppermine/devel/themes/classic/theme.php,v $
+  $Revision: 1.71 $
+  $Author: gaugau $
+  $Date: 2005/10/25 01:16:24 $
+**********************************************/
+
+// ------------------------------------------------------------------------- //
+// This theme has all CORE items removed                                     //
+// ------------------------------------------------------------------------- //
+define('THEME_IS_XHTML10_TRANSITIONAL',1);
+?>
