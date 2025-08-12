6cf7ea165ca9720ea812f3fbcb7e771152ae44f4
me-suzy <me.suzana@gmail.com>
2025-08-12 18:47:51 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/addlang.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/addlang.tmpl.php
@@ -0,0 +1,5 @@
+<table border='0' width='100%'>
+<tr><td class='fframe'><%BASEON%></td><td><select name='lgid'><%LANGLIST%></select></td></tr>
+<tr><td class='fframe'><%LANGNAME%></td><td><input type='text' name='lgname'></td></tr>
+<tr><td class='frame' colspan='2'><input type='submit' value='<%GO%>'></td></tr>
+</table>
