88964a544cfcd3ce4087654bd5c3c98ee582a94a
me-suzy <me.suzana@gmail.com>
2025-08-12 11:47:28 +0300

    Add E-Commerce/enterprise3.01/includes/footer.php

---
+++ E-Commerce/enterprise3.01/includes/footer.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+/*
+  The following copyright announcement can only be
+  appropriately modified or removed if the layout of
+  the site theme has been modified to distinguish
+  itself from the default Enterprise Shopping Cart Software
+  copyrighted theme.
+
+  Please leave this comment intact together with the
+  following copyright announcement.
+*/
+
+?>
+<div align="center" class="smallText">
+	This shopping cart uses <a href="http://www.enterprisecart.com">Enterprise Shopping Cart Software</a>
+</div>
