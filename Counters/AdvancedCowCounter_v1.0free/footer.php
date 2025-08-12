94dcbf86fa2076aedcd7480ca6ba7bfe04fbf15a
me-suzy <me.suzana@gmail.com>
2025-08-12 08:27:12 +0300

    Add Counters/AdvancedCowCounter_v1.0free/footer.php

---
+++ Counters/AdvancedCowCounter_v1.0free/footer.php
@@ -0,0 +1,31 @@
+<?
+
+//---------------------------------------------------------------
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either version 2
+//of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//Meezerk's Advanced CowCounter - An Advanced Website Counter.
+//Copyright (C) 2004  Daniel Foster  dan_software@meezerk.com
+//---------------------------------------------------------------
+
+?>
+    </TD>
+  </TR>
+</TABLE>
+<HR WIDTH="50%">
+<TABLE BORDER="0" CELLSPACING="0" CELLPADDING="0" WIDTH="99%">
+  <TR>
+    <TD ALIGN=CENTER>
+      Advanced CowCounter is a product of <A HREF="http://www.meezerk.com">Meezerk.com</A> released under the GPL.
+    </TD>
+  </TR>
+</TABLE>
+</BODY>
+</HTML>
