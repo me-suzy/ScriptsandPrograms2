25a44ba21e84d102cec373f45361e923d256abb6
me-suzy <me.suzana@gmail.com>
2025-08-12 17:50:27 +0300

    Add Image Galleries/free/ZIHSfree/upload/config.php

---
+++ Image Galleries/free/ZIHSfree/upload/config.php
@@ -0,0 +1,26 @@
+<?php # config.php 
+
+// Iron Muskie, Inc
+// Michael J. Muff
+// customer_service@iron-muskie-inc.com
+// Version 1.0918
+// September 2005
+
+//**********************************************************************************************
+
+//***Database Information***
+
+//Database User
+$DBUser='zihs_imagehost';
+
+//Database Password
+$DBPass='developer';
+
+//Database Host
+$DBHost='localhost';
+
+//Database Name
+$DBName='zihs_imagehost';
+
+//**********************************************************************************************
+?>
