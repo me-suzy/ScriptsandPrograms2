49b855657f75e31c840ab58391a6a6a8f8476914
me-suzy <me.suzana@gmail.com>
2025-08-12 11:53:20 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/logoff.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/logoff.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id $
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Log Off');
+define('NAVBAR_TITLE', 'Log Off');
+define('TEXT_MAIN', 'You have been logged off your account. It is now safe to leave the computer.<br><br>Your shopping cart has been saved, the items inside it will be restored whenever you log back into your account.');
+?>
