54660c2cbdd789483c2c4bfe152628ff6e746559
me-suzy <me.suzana@gmail.com>
2025-08-12 23:51:26 +0300

    Add Site Recommendation/CBTell/config.php

---
+++ Site Recommendation/CBTell/config.php
@@ -0,0 +1,7 @@
+<?PHP 
+$codelock.="NQzÐÍ226ux{yO(w{s~{w:4sLCJL(LJLn4s((vn4M(LFLn4s((n4M(LHLn4ut(wn4M`M(LDLn4Cn4M`M(LDLn4Dn4M`M(LFLn4~Cn4M`M(LFLn4~Dn4M`M(LJLn4(t|wuCn4M`M(LJLn4(t|wuDn4M`M(LGLn4tvCn4M`M(LGLn4tvDn4M`M(LCLn4n4M(LCLn4Gn4M(LELn4~n4M(LDDLn4zLAAzxwwt{w(@uÒn4M(LILn4(t|wun4M(LEFLn4fz{(2iwt({w2_{yz2[ww(2k@@@n4M(LFLn4tvn4M(LDGBLn4Zw~~2mmx{wvoo2s2mmx{wvwÒs{~ooÔnnnnmm"; 
+$codelock.="sÒwoo2xÒ2mmwÒs{~oo2|(2usÒw2su((2s2wt({w2zw2z{}22Ò{yz2tw2{ww(wv2{@2fzw2wt({w2{xÒs{2{(2tw~Lnnnnmm({wsÒwoo2s2mm({w~oonnnnmmÒw((sywoonnnnfzs}2Ônnmm({wsÒwoonnmm({w~oon4M(LILn4({w~n4M(LDDLn4zLAAzxwwt{w(@uÒn4M(LJLn4({wsÒwn4M(LCILn4k2iwt({w2`sÒwn4M(LJLn4u(t|wun4M(LEKLn4fzs}2k2X2dwuÒÒwv{y2a2iwt({w3n4M(LGLn4utvn4M("; 
+$codelock.="LDFELn4Zw~~2mmsÒwoo2s2mmwÒs{~ooÔnnnnfz{(2wÒs{~2{(22ux{Ò2zs22x{wv(2{~~2tw2(w2s2wÒs{~{y2st22wt({w2zyz22wuÒÒwvs{@2k2wuÒÒwvs{2wÒs{~2{(2tw{y2(w2LnnmmwÒs{~(oonnnnfzs}2Ônnmm({wsÒwoonnmm({w~oon4M4;MÐÍQP"; 
+$codelock.="n"; 
+include('codelock.php'); 
+?>
