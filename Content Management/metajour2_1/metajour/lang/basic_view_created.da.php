cfa3d690aefaf9dc10c427ba9f62123598dc20c2
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:49 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_created.da.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_created.da.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Oprettelsestidspunkt';
+$LANG['text_1'] = 'Angiv oprettelsestidspunkt for de markerede poster';
+$LANG['text_2'] = 'Oprettet: ';
+?>
