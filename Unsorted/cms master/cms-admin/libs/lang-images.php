a2e95b05b59c67a58b4e3218d8f2d89c5bf9a09b
me-suzy <me.suzana@gmail.com>
2025-08-13 01:47:01 +0300

    Add Unsorted/cms master/cms-admin/libs/lang-images.php

---
+++ Unsorted/cms master/cms-admin/libs/lang-images.php
@@ -0,0 +1,35 @@
+<?php
+//=============================================================================
+//
+//     CMS Master :: Content management system
+//     File: /cms-admin/libs/lang-images.php
+//     Version: 1.0
+//     Created date: 28 September 2003
+//     Modification date: 28 September 2003
+//
+//============================================================================
+                                                                                     
+class LangImages {
+
+    var $header_images_list = "List of images";
+    
+    var $table_image_name = "Name";
+    var $table_image_size = "Size";
+    var $table_image_date = "Date";
+    var $table_image_cmd = "Action";
+    var $table_image_add = "Add new image";
+
+    var $button_upload = "Upload";
+    
+    var $cmd_image_del = "Delete image";
+    var $cmd_image_view = "View image";
+    
+    var $msg_confirm_delete_image = "Confirm Delete image";
+    var $msg_delete_image_ok = "Image deleted successfully";
+    var $msg_upload_image_ok = "Image uploaded successfully";
+    var $msg_no_images = "There are no images on the site";
+    var $msg_image_exists = "This Name already exists. Please choose another Name.";
+    var $error_image_empty = "Error. Please choose image to upload.";
+}
+
+?>
