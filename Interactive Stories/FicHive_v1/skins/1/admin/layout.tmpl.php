8bdb759e09ec317fbcce959d962c7d51f7cde8fa
me-suzy <me.suzana@gmail.com>
2025-08-12 18:49:23 +0300

    Add Interactive Stories/FicHive_v1/skins/1/admin/layout.tmpl.php

---
+++ Interactive Stories/FicHive_v1/skins/1/admin/layout.tmpl.php
@@ -0,0 +1,7 @@
+<table border='0' width='100%'><tr>
+<td class='fframe' valign='top'><%NAVIGATION%>
+<p>
+<iframe frameborder='0' style='width:100%; height:100' src='http://www.alter-idem.com/scripts/fichive/updates.html' scrolling='no'></iframe>
+</td>
+<td class='frame' valign='top'><%CONTENT%></td>
+</tr></table>
