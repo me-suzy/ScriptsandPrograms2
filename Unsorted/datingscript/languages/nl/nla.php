ccdbdc2566ac5b7bc15afe543a08d71c832845a9
me-suzy <me.suzana@gmail.com>
2025-08-13 02:21:26 +0300

    Add Unsorted/datingscript/languages/nl/nla.php

---
+++ Unsorted/datingscript/languages/nl/nla.php
@@ -0,0 +1,122 @@
+<?php
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.1.1                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 03/01/03        Last Modified 04/05/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               nla.php                          #
+# File purpose            Additional langfile for admin    #
+# File created by         Erik <erik@clubpriority.nl>      #
+############################################################
+### !!!!! Please read it: RULES for translate!!!!! ###
+### 1. Be carefull in translate - don`t use ' { } characters
+###    You can use them html-equivalent - &#39; &#123; &#125;
+### 2. Don`t translate {some_number} templates - you can only replace it 
+###    {0} - is not number - it template
+###################################
+$x=array(
+'<font color=red size=3>*</font>', //0 - Symbol for requirement field
+'Foutieve gebruikersnaam of wachtwoord - probeer het opnieuw',//1
+'Dating - Beheer',//2
+'Beheerder gebruikersnaam',//3
+'Beheerder wachtwoord',//4
+'Ga!',//5
+'Toegang uitsluitend voor beheerders',//6
+'Hoofdpagina',//7
+'Reset tellers',//8
+'Repareer&Optimaliseer',//9
+'Beheren gebruikers',//10
+'Beheren FAQ',//11
+'Maillijst',//12
+'Afsluiten',//13
+'Je bent succesvol uitgelogd als beheerder.<br><br>Tot de volgende keer!',//14
+'Aantal berichten',//15
+'Verzonden berichten',//16
+'Het volgende deel',//17
+'{0} berichten zijn verzonden naar gebruikers',//18
+'Je hebt ongelezen berichten op {0}',//19 {0} - SiteName (C_SNAME)
+'{0} Leden gevonden',//20
+'Beheren maillijst',//21
+'Met status:',//22
+'Mail opties en velden:',//23
+'Type mail',//24
+'Mail van',//25
+'Mail onderwerp',//26
+'Mail bericht',//27
+'Bevestig resultaten',//28
+'Ga terug',//29
+'Verstuur berichten',//30
+'De niet-afgemaakte verzendingen - {0}',//31
+'Onderwerp : {0} [verzonden naar {1} gebruikers(s)] :::>',//32
+'Maak deze verzending af',//33
+'Tekst',//34
+'HTML',//35
+'Preview',//36
+'Gebruikers met ongelezen berichten - ',//37
+'Aantal ongelezen berichten - ',//38
+'Laatste bericht id - ',//39
+'Verstuur bevestigingen naar gebruikers met ongelezen berichten',//40
+'Profiel succesvol bewerkt',//41
+'FAQ vraag is succesvol aangemaakt',//42
+'Nieuwe aanmaken',//43
+'Voer de vraag in',//44
+'Voer het antwoord in',//45
+'Beheren FAQ',//46
+'Maak een nieuwe FAQ categorie aan',//47
+'Bewerk FAQ categorie',//48
+'Verwijder FAQ categorie',//49
+'Maak FAQ',//50
+'Selecteer FAQ categorie',//51
+'FAQ vraag',//52
+'FAQ antwoord',//53
+'Ten eerste dien je FAQ categorieën aan te maken',//54
+'Voer de naam van de categorie in',//55
+'FAQ categorie aanmaken',//56
+'Nieuwe categorie aangemaakt:',//57
+'FAQ categorie naam',//58
+'FAQ category bewerken',//59
+'Bewerken',//60
+'Categorie gewijzigd',//61
+'FAQ categorie bewerken',//62
+'FAQ is verwijderd',//63
+'FAQ categorie verwijderen',//64
+'Let op : Wees voorzichtig. Door het verwijderen van de categorie verwijder je tevens alle vragen in deze categorie.',//65
+'Verwijder',//66
+'0 - Wacht op e-mailadres controle<br>1 - Wacht op controle na registratie<br>2 - Wacht na bewerken profiel<br>3,8 - Wacht op verwijdering (onzichtbaar, zichtbaar)<br>7 - Gebruiker is actief<br>',//67
+'Database is gerepareerd en geoptimaliseerd.',//68
+'Gebruiker met ID#={0} is succesvol verwijderd uit onze database',//69
+'Tellers zijn ge-reset',//70
+'Succesvol toegelaten gebruiker met id={0}',//71
+'IP',//72
+'Noodzakelijkheid [0 - 255]',//73
+'Status - (PAS OP)',//74
+'Toestaan',//75
+'Beheren gebruikers',//76
+'Totaal aantal actieve gebruikers :',//77
+'Gebruikers wachtend op activering na aanmelding',//78
+'Gebruikers wachtend op activering na wijziging',//79
+'Gebruikers wachtend op verwijdering',//80
+'Privilegied users',//81
+'Privilegied users Management',//82
+'',//83
+'',//84
+'',//85
+'',//86
+'',//87
+'',//88
+'',//89
+'',//90
+'',//91
+'',//92
+'',//93
+'',//94
+'',//95
+'',//96
+'',//97
+'',//98
+''//99
+); 
+?>
