0ce8e50a3348bd90fabfcce64fdf21b7291df11b
me-suzy <me.suzana@gmail.com>
2025-08-12 10:05:10 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/pnt/test/unit/classObjectToTest.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/pnt/test/unit/classObjectToTest.php
@@ -0,0 +1,16 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+   	
+/**
+ * @package pnt/test/unit
+ * @author  Henk Verhoeven, MetaClass <henk@phpPeanuts.org>
+ */
+class ObjectToTest {
+
+		var $var1 = 'initial value of ObjectToTest::var1';
+		
+}
+
+?>
