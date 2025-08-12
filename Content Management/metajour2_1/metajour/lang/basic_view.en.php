151385869344979eb30f30108afba6eb1850daf9
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:07 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view.en.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+/* ELEMENTS FOR BASIC_FIELD */
+/* ELEMENTS FOR BASIC_COLLECTION (and some for hierarchy and list) */
+
+$LANG['context_viewfile'] = 'Show file';
+$LANG['context_default'] = 'Set as default';
+$LANG['context_language'] = 'Language';
+$LANG['context_createdby'] = 'Created by';
+$LANG['context_checkedby'] = 'Checked by';
+$LANG['context_changedby'] = 'Changed by';
+$LANG['context_created'] = 'Created';
+$LANG['context_checked'] = 'Checked';
+$LANG['context_changed'] = 'Changed';
+$LANG['context_delete'] = 'Delete';
+$LANG['context_publish'] = 'Publishing and expiration';
+$LANG['context_createcopy'] = 'Create copy';
+$LANG['context_createvariant'] = 'Create variant';
+$LANG['context_createrevision'] = 'Create new revision';
+$LANG['context_requestapproval'] = 'Request approval';
+$LANG['context_approvepublish'] = 'Approve and publish';
+$LANG['context_active'] = 'Activate/Deactivate';
+$LANG['context_category'] = 'Add to category';
+$LANG['context_filter'] = 'Reports';
+$LANG['context_create'] = 'Create new';
+$LANG['context_createsub'] = 'Create subrecord';
+$LANG['context_recycle'] = 'Show Trashcan';
+$LANG['context_categories'] = 'Categories';
+$LANG['context_access'] = 'Access';
+$LANG['context_advanced'] = 'Advanced';
+$LANG['context_view'] = 'View';
+$LANG['context_edit'] = 'Edit';
+$LANG['context_properties'] = 'Properties';
+$LANG['context_statistics'] = 'Show statistics';
+$LANG['context_exportobj'] = 'Export to object archive';
+$LANG['context_variantcompare'] = 'Show variants';
+$LANG['context_preview'] = 'Preview';
+$LANG['select_all'] = 'ANY';
+$LANG['select_none'] = 'NONE';
+$LANG['select_owner'] = 'OWNER/ADMINISTRATORS/MANAGERS';
+$LANG['select_standard'] = 'STANDARD';
+$LANG['select_category'] = 'Select category';
+$LANG['select_language'] = 'SELECT LANGUAGE';
+$LANG['select_country'] = 'SELECT COUNTRY';
+$LANG['select_table'] = 'SELECT TABLE';
+$LANG['select_extension'] = 'SELECT EXTENSION';
+$LANG['select_file'] = 'SELECT FILE';
+$LANG['button_cancel'] = 'Cancel';
+$LANG['button_ok'] = 'OK';
+$LANG['button_save'] = 'Save';
+$LANG['button_create'] = 'Create';
+$LANG['button_apply'] = 'Apply';
+$LANG['_label_category'] = 'Categories';
+$LANG['_label_sysaccess'] = 'System access';
+$LANG['_label_webaccess'] = 'Website access';
+$LANG['_label_members'] = 'Members';
+$LANG['_label_memberof'] = 'Member of';
+
+?>
