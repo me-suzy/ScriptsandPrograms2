42df0963bbe4d180e90f0e8ee984de58c96f70dd
me-suzy <me.suzana@gmail.com>
2025-08-12 15:13:31 +0300

    Add Flash and PHP/AdesFormMail/AdesFormMail.php

---
+++ Flash and PHP/AdesFormMail/AdesFormMail.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>AdesFormMail</title>
+</head>
+<body bgcolor="#ffffff">
+<!--url's used in the movie-->
+<a href="http://www.adesdesign.net"></a>
+<!--text used in the movie-->
+<!--
+<p align="left"><font face="Verdana" size="10" color="#333333">Full Name:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">Email:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">Website:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#ff3300">*</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">Country:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">Message:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">Required fields are denoted by <font color="#ff3300">*</font></font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">Address 1:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">Address 2:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">State:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">Zip Code:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">City:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">Phone:</font></p>
+<p align="left"><font face="Verdana" size="10" color="#333333">Fax:</font></p>
+-->
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="450" height="480" id="AdesFormMail" align="middle">
+<param name="allowScriptAccess" value="sameDomain" />
+<param name="movie" value="AdesFormMail.swf" />
+<param name="quality" value="high" />
+<param name="bgcolor" value="#ffffff" />
+<embed src="AdesFormMail.swf" quality="high" bgcolor="#ffffff" width="450" height="480" name="AdesFormMail" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+</object>
+</body>
+</html>
