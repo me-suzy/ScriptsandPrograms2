ab3b603f4f4b63b64ff441c57859adf57ed1e0ee
me-suzy <me.suzana@gmail.com>
2025-08-12 08:46:09 +0300

    Add Customer Support/HelpDesk346/HelpDesk346/install/index.php

---
+++ Customer Support/HelpDesk346/HelpDesk346/install/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>Welcome to the Help Desk Setup Page http://www.helpdeskreloaded.com</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<table width="75%" border="1" cellspacing="0" cellpadding="0">
+  <tr> 
+    <td colspan="2"><img src="../images/help-desk-reload-software-main.jpg" width="599" height="63"></td>
+  </tr>
+  <tr> 
+    <td width="50%" valign="top"> <p>Before you begin the Help Desk installation 
+        process you should gather a few things. </p></td>
+    <td width="50%" valign="top">&nbsp;</td>
+  </tr>
+  <tr> 
+    <td valign="top"><p>You will need the following:</p>
+      <p><strong>MySQL Information:</strong></p>
+      <ul>
+        <li>Your MySql server name (host name).</li>
+        <li>Your Database Name (if you have one)</li>
+        <li>Database Username</li>
+        <li>Database Password</li>
+        <li>Database Prefix</li>
+      </ul>
+      <p><strong>Server Information.</strong></p>
+      <ul>
+        <li>Make sure your server is at least up to date par the requirements 
+          in 1stReadMe.txt</li>
+      </ul>
+      <p><strong>Help Desk Admins.</strong></p>
+      <ul>
+        <li>You can input 1 to 5 help desk administrators durring the setup process, 
+          later more can be added. </li>
+      </ul></td>
+    <td width="50%" valign="top"><div align="center"><a href="install.setup.php"><img src="../images/install-help-desk.jpg" alt="Install Help Desk Software" width="140" height="114" border="0"></a></div></td>
+  </tr>
+</table>
+<div align="center"><br>
+  <a href="http://www.helpdeskreloaded.com"><img src="http://www.helpdeskreloaded.com/reload/help-desk-copyright.jpg" alt="http://www.helpdeskreloaded.com Help Desk Software By  HelpDeskReloaded &quot;Help Desk Reloaded&quot;" border="0"></a> 
+</div>
+</body>
+</html>
