98287aae3d649f6226ec118a2ba19b06e6c4ebaa
me-suzy <me.suzana@gmail.com>
2025-08-12 10:24:19 +0300

    Add Discussion Boards/awebBB/awebBB/webmin/style.php

---
+++ Discussion Boards/awebBB/awebBB/webmin/style.php
@@ -0,0 +1,15 @@
+<style type="text/css">
+<!--
+body {
+font-family: verdana, arial, helvetica, sans-serif; font-size: 11px; background-color: white; }
+table {
+font-family: verdana, arial, helvetica, sans-serif; font-size: 11px;}
+a:link { font-family: verdana; font-size: 11px; color: blue; text-decoration: none; }
+a:visited { font-family: verdana; font-size: 11px; color: blue; text-decoration: none; }
+a:active { font-family: verdana; font-size: 11px; color: blue; text-decoration: none; }
+a:hover { font-family: verdana; font-size: 11px; color: blue; text-decoration: underline; }
+div.boxxy {position: absolute; display: block; width: 300px; padding: 0px; margin-bottom: 2px; margin-right: 2px; margin-left: 202px; margin-top: 168px; border: 1px solid gray; background-color: white;}
+div.boxtext {position: absolute; display: block; height:18px padding: 0px; margin-bottom: auto; margin-right: auto; margin-left: 211px; margin-top: 162px; background-color: white;}
+div.tbord {border-left: 1px solid gray; border-top: 1px solid gray; border-bottom: 1px solid gray; background-color: white;}
+//-->
+</style>	
