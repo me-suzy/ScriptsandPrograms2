8c2b6f4cbe4454226b6cc5a60c33bb449ae0bd6e
me-suzy <me.suzana@gmail.com>
2025-08-12 10:07:25 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/widgets/classCheckboxWidget.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/classes/widgets/classCheckboxWidget.php
@@ -0,0 +1,19 @@
+<?php
+// Copyright (c) MetaClass, 2003, 2004
+// Licensed under the Academic Free License version 2.0
+
+
+includeClass('PntCheckboxWidget', 'pnt/web/widgets');
+
+/** FormWidget that generates html specifying a checkbox. 
+*
+* This concrete subclass is here to keep de application developers
+* code separated from the framework code.
+* @see http://www.phppeanuts.org/site/index_php/Menu/178
+* Framework code is in the superclass.
+* @package widgets
+*/
+class CheckboxWidget extends PntCheckboxWidget {
+
+}
+?>
