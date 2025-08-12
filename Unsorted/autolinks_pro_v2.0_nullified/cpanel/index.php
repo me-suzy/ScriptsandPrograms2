016cb6d28eb8cc0d84cbe6295ec8eb86e7fd523e
me-suzy <me.suzana@gmail.com>
2025-08-13 00:59:17 +0300

    Add Unsorted/autolinks_pro_v2.0_nullified/cpanel/index.php

---
+++ Unsorted/autolinks_pro_v2.0_nullified/cpanel/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>AutoLinks 2.0n Control Panel</title>
+</head>
+
+<frameset cols="150,*" frameborder="NO" border="0" framespacing="0" rows="*"> 
+  <frame name="leftFrame" scrolling="NO" src="menu.php" frameborder="NO" noresize>
+  <frame name="mainFrame" src="home.php" frameborder="NO" noresize>
+</frameset>
+<noframes>
+<body bgcolor="#FFFFFF" text="#000000">
+
+</body>
+</noframes>
+</html>
