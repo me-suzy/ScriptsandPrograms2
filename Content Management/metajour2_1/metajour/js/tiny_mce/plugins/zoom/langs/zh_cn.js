3f3ebc6e1286ab7e11284bd35b4edf04cd87fcad
me-suzy <me.suzana@gmail.com>
2025-08-12 06:58:57 +0300

    Add Content Management/metajour2_1/metajour/js/tiny_mce/plugins/zoom/langs/zh_cn.js

---
+++ Content Management/metajour2_1/metajour/js/tiny_mce/plugins/zoom/langs/zh_cn.js
@@ -0,0 +1,5 @@
+// Simplified Chinese lang variables contributed by cube316 (cube316@gmail.com)
+//Çë·ÃÎÊ http://www.cube316.net/ ÒÔ»ñÈ¡TinyMCEµÄÖÐÎÄÖ§³Ö
+
+tinyMCELang['lang_dir'] = 'ltr';
+tinyMCELang['lang_zoom_prefix'] = 'Ëõ·Å';
