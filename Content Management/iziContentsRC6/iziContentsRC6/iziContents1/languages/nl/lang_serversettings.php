e5d5bdd3a29116c7cbf088af82ddbec159f25fe9
me-suzy <me.suzana@gmail.com>
2025-08-12 03:59:52 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_serversettings.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/nl/lang_serversettings.php
@@ -0,0 +1,53 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'Beheer server instellingen';
+
+//  Form Field Headings
+$GLOBALS["tGzipSetting"] 	= 'Gebruik gzip compressie';
+$GLOBALS["tSecureServer"] 	= 'Secure server';
+$GLOBALS["tGzipTest"] 		= 'Test compressie';
+$GLOBALS["tGzipSupported"] 	= 'Deze server ondersteunt GZIP compressie.';
+$GLOBALS["tGzipUnsupported"] 	= 'Deze server ondersteunt geen GZIP compressie.';
+$GLOBALS["tMultiSite"] 		= 'Multi-Site mogelijk';
+$GLOBALS["tMultiSiteAuthors"] = 'Multi-Site met algemene gebruikers';
+$GLOBALS["tMultiLanguage"] 	= 'Multi-Talen mogelijk';
+$GLOBALS["tMultiTheme"] 	= 'Multi-Theme mogelijk';
+$GLOBALS["tPageTimer"] 		= 'Tijdsduur schermopbouw';
+$GLOBALS["tDefaultLanguage"] 	= 'Standaard taal';
+$GLOBALS["tDateFormat"] 	= 'Datum formaat';
+$GLOBALS["tTimezone"] 		= 'Server tijdzone';
+$GLOBALS["tFrameSetting"] 	= 'Frames/Geen frames';
+$GLOBALS["tVisitorStats"] 	= 'Maak site statistieken mogelijk';
+
+//  Form Block Titles
+$GLOBALS["thServerOptions"] 	= 'Server Opties';
+$GLOBALS["thezContentsOptions"] = 'iziContents Opties';
+
+//  Form Field Options
+$GLOBALS["tGzipCompression"] 	= 'Comprimeer tekst';
+$GLOBALS["tGzipNoCompression"] = 'Comprimeer niet';
+$GLOBALS["tTimerDisplay"] 	= 'Laat timer zien';
+$GLOBALS["tTimerNoDisplay"] 	= 'Laat timer niet zien';
+$GLOBALS["tFrames"] 		= 'Frames';
+$GLOBALS["tNoFrames"] 		= 'Geen frames';
+
+//  Form Text Description
+$GLOBALS["tDetails"]		= 'Met dit formulier kunt u de algemene configuratie van uw website instellen.';
+$GLOBALS["hGzipSetting"]	= 'Zet deze instelling aan om gzip compressie te gebruiken als uw server dat ondersteunt.';
+$GLOBALS["hSecureServer"] 	= 'Indien uw webserver https ondersteunt kunt u dit aanvinken voor een \'secure login\'.<br /><br /><B>Nota bene:-</B> Deze functionaliteit is nog niet gïmplementeerd.';
+$GLOBALS["hMultiSite"] 		= 'Zet deze instelling op \'Ja\' indien u meer dan één iziContents site wilt draaien op deze server.';
+$GLOBALS["hMultiSiteAuthors"] = 'Zet deze instelling op \'Ja\' indien u wilt toestaan dat gebruikers toegang krijgen tot alle sites op deze server in de multi-site situatie.';
+$GLOBALS["hMultiLanguage"] 	= 'Zet deze instelling op \'Ja\' indien u van plan bent om de site te onderhouden en draaien in meer dan één taal.';
+$GLOBALS["hMultiTheme"] 	= 'Zet deze instelling op \'Ja\' indie u het mogelijk wilt maken om alternatieve thema\'s te hebben voor uw website.';
+$GLOBALS["hPageTimer"]		= 'Toont de timer die meet hoe lang het duurt om een pagina te genereren.<br>';
+$GLOBALS["hPageTimer"]		.= 'Dit is een diagnostisch hulpmiddel voor de ontwikkelaars van iziContents bij het testen van de efficiëntie van de code.';
+$GLOBALS["hDefaultLanguage"]	= 'Zet de voorkeurstaal op uw website.';
+// JAH $GLOBALS["hDateFormat"] 	= 'Formatting mask for displaying dates.<br /><br />Codes that can be used are based on the Open Group standard, and include:<br /><ul><li><b>%a</b> = day of week (3 letters)<br />eg. Fri<li><b>%A</b> = day of week (long)<br />eg. Friday<li><b>%d</b> = day of month (2 digits)<br />eg. 01<li><b>%e</b> = day of month (1 or 2 digit)<br />eg. 1<br />(Doesn\'t seem to work even though defined as part of the Open Group standard.)<li><b>%b</b> = month (textual, 3 letters)<br />eg. Mar<li><b>%B</b> = month (textual, long)<br />eg. March<li><b>%m</b> = month (2 digits)<br />eg. 03<li><b>%Y</b> = year (4 digits)<br />eg. 2002<li><b>%y</b> = year (2 digits)<br />eg. 02<br /><br /><li><b>%x</b> Standard format for locale<br />(Depends on the server.)</ul>';
+$GLOBALS["hDateFormat"] 	= 'Dit geeft het formaat aan waarmee de datum worden getoond.<br /><br />Codes die gebruikt kunnen worden zijn gebaseerd op de \'Open Group\' standaard, en zijn de volgende:<br /><ul><li><b>%a</b> = dag van de week week (3 letters)<br />bijv. Don<li><b>%A</b> = dag van de week (lang)<br />bijv. Vrijdag<li><b>%d</b> = dag van de maand (2 cijfers)<br />eg. 01<li><b>%e</b> = dag van de maand (1 of 2 cijfers)<br />bijv. 1<br />(Dit lijkt echter niet te werken ondanks dat het in de standaard gedefinieerd is.)<li><b>%b</b> = maand (tekst, 3 letters)<br />bijv. Jan<li><b>%B</b> = maand (tekst, lang)<br />bijv. Maart<li><b>%m</b> = maand (2 cijfers)<br />bijv. 03<li><b>%Y</b> = jaar (4 cijfers)<br />bijv. 2003<li><b>%y</b> = jaar (2 digits)<br />bijv. 03<br /><br /><li><b>%x</b> Standaard formaat voor locale<br />(Hangt af van de server.)</ul>';
+$GLOBALS["hTimezone"]		= 'Tijdszone die getoond wordt in de tijd/datum scherm.<br>';
+$GLOBALS["hTimezone"]		.= 'bv. "UST", "UST+1"';
+$GLOBALS["hFrameSetting"] 	= 'Toon de site met behulp van frames of juist zonder frames.';
+$GLOBALS["hVisitorStats"] 	= 'Geeft aan of er toegangs statistieken worden bijgehouden voor uw website?';
+
+?>
