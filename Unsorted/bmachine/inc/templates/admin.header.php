c3d568bd876f68cbeddff0067d7b7f0e25e19704
me-suzy <me.suzana@gmail.com>
2025-08-13 01:17:52 +0300

    Add Unsorted/bmachine/inc/templates/admin.header.php

---
+++ Unsorted/bmachine/inc/templates/admin.header.php
@@ -0,0 +1,73 @@
+<html>
+
+<head>
+<title><? echo $title; ?></title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<style>
+<!--
+a { font-family:Verdana; font-size:8pt; color:rgb(51,51,51); text-decoration:none; }
+a:hover { color:rgb(153,153,153); }
+body { font-family:Verdana; font-size:8pt; }
+font { font-family:Verdana; font-size:8pt; }
+p { font-family:Verdana; font-size:8pt; }
+
+.mnu_link { font-weight:bold; color:rgb(238,238,238); }
+-->
+</style>
+
+</head>
+
+<body bgcolor="#E2E2E2" text="black" link="blue" vlink="purple" alink="red">
+<table align="center" border="0" cellpadding="0" cellspacing="0" width="729">
+<tr>
+<td width="729">
+<table border="0" cellpadding="1" cellspacing="0" width="729" bgcolor="black">
+<tr>
+<td width="727">
+<table border="0" cellpadding="0" cellspacing="0" width="729" bgcolor="white">
+<tr>
+<td width="729" height="85">
+<img src="images/logo.gif" width="203" height="62" border="0"></td>
+</tr>
+<tr>
+<td width="729">
+      <TABLE border=0 cellPadding=0 cellSpacing=0 width="100%" bgcolor="#565693">
+        <TBODY>
+        <TR>
+          <TD height="20" width="12%">
+<table border="0" cellpadding="0" cellspacing="0" width="721">
+<tr>
+<td width="476">
+<p>&nbsp;<a class="mnu_link" href="<? global $c_urls; echo $c_urls; ?>">bMachine Home</a>
+&nbsp;&nbsp;&nbsp;<a class="mnu_link" href="admin.php">Admin Home</a>
+&nbsp;&nbsp;&nbsp;<a class="mnu_link" href="admin.php?id=logout">Logout</a></p>
+</td>
+<td width="245"><p align="right"><a class="mnu_link" href="http://boastology.com/pages/download.php">Updates</a></td>
+</tr>
+</table>
+</TD>
+</TR></TBODY></TABLE></td>
+</tr>
+<tr>
+<td width="729" height="2" bgcolor="#B3B3B3">
+</td>
+</tr>
+<tr>
+<td width="729" valign="top">
+<table border="0" cellpadding="0" cellspacing="0" width="721">
+<tr>
+<td width="132" height="109" bgcolor="#F9F9F9" valign="top">
+<table border="0" cellpadding="2" cellspacing="0" width="126">
+<tr>
+<td width="5" height="15">
+</td>
+<td width="0" height="15"><br><? echo $mnu; ?></td>
+</tr>
+</table>
+</td>
+<td width="1" height="109" bgcolor="#AAAAAA">
+</td>
+<td width="588" height="109" valign="top">
+<table border="0" cellpadding="10" cellspacing="0" width="566">
+<tr>
+<td width="552">
