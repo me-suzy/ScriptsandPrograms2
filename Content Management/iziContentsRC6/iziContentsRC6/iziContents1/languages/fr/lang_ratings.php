56e3255b2d23790e0b73fc0e01887b9c41072c0d
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:43 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_ratings.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_ratings.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Title
+$GLOBALS["tFormTitle"] = 'maintenir paramétre du contenue';
+
+//  Form Field Headings
+$GLOBALS["tAllowRatings"] = 'Permettre les Classements des Articles';
+$GLOBALS["tAllowComments"] = 'Permettre les Commentaires des Articles';
+$GLOBALS["tRatingMin"] = 'Valeur Minimum des Classements';
+$GLOBALS["tRatingMax"] = 'Valeur Maximum des Classements';
+$GLOBALS["tRatingImage1"] = 'Etoile Classement Image';
+$GLOBALS["tRatingImage2"] = 'Etoile Classement Image (pas de classement)';
+$GLOBALS["tVetComments"] = 'Ajouter Commentaire avant le relâchement';
+$GLOBALS["tPrintFriendly"] = 'Affichage de l icone en mode imprimante amical';
+$GLOBALS["tTeaserWithDetails"] = 'Un Résumé Texte sur la Page Détaile';
+
+//  Form Block Titles
+$GLOBALS["thPrinterFriendly"] = 'Imprimante Options Amicales';
+$GLOBALS["thContentRatings"] = 'Les Options Classement du Contenu';
+$GLOBALS["thContentComments"] = 'Les Options Commentaire du Contenu';
+$GLOBALS["thContentSettings"] = 'Autres Options Contenues';
+
+//  Form Text Description
+$GLOBALS["tDetails"] = 'Cette forme vous permet de régler quelques valeurs globales pour le classement de contenu sur votre site.';
+$GLOBALS["hAllowRatings"] = 'Souhaitez-vous permettre de l aux membres pour évaluer des articles de contenu?';
+$GLOBALS["hAllowComments"] = 'Souhaitez-vous permettre de l aux membres pour commenter sur les articles?';
+$GLOBALS["hRatingMin"] = 'Choisir le minimum la valeur évaluant.';
+$GLOBALS["hRatingMax"] = 'Choisir le maximum la valeur évaluant.';
+$GLOBALS["hRatingImage1"] = 'Choisir l image être montrée pour les classements.';
+$GLOBALS["hRatingImage2"] = 'Choisir l\' image être montrée pour les classements.';
+$GLOBALS["hVetComments"] = 'Faire vous voulez aux commentaires de vet avant qu\'ils soient visibles sur la page?<br />Noter que cette fonction n\'est pas pourtant disponible.';
+$GLOBALS["hPrintFriendly"] = 'Ceci est le défaut pour l\'option contente Imprimante Amical.';
+$GLOBALS["hTeaserWithDetails"] = 'Montrer le texte de taquin sur les pages de détail de même que le détail content véritable.';
+
+?>
