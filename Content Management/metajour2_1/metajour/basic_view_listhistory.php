6862b23f1eddbc1a39051b703bd95b7dcde6f1b6
me-suzy <me.suzana@gmail.com>
2025-08-12 05:56:09 +0300

    Add Content Management/metajour2_1/metajour/basic_view_listhistory.php

---
+++ Content Management/metajour2_1/metajour/basic_view_listhistory.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('basic_view_list.php');
+
+class basic_view_listhistory extends basic_view_list {
+
+	function setFilters() {
+		parent::setFilters();
+		$obj =& $this->_listobj;
+		$obj->setfilter_history($this->data['_relval']);
+	}
+}
+
+?>
