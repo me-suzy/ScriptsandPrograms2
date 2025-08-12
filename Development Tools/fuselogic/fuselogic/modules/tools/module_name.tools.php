353cbe10ca7845ed06556ffb77771a57aca4e7c4
me-suzy <me.suzana@gmail.com>
2025-08-12 09:29:19 +0300

    Add Development Tools/fuselogic/fuselogic/modules/tools/module_name.tools.php

---
+++ Development Tools/fuselogic/fuselogic/modules/tools/module_name.tools.php
@@ -0,0 +1,16 @@
+<?php
+
+$FL_MODULE_SETTING['module_name'] = 'tools';
+$FL_MODULE_SETTING['sub_module']['function_list'] = 'scripts/function_info.php';
+$FL_MODULE_SETTING['sub_module']['class_list'] = 'scripts/class_info.php';
+//$FL_MODULE_SETTING['sub_module']['_php_compat'] = 'scripts/php_compat.php';
+$FL_MODULE_SETTING['sub_module']['_time'] = '_time.php';
+$FL_MODULE_SETTING['sub_module']['_globals_on'] = 'mglobalson.php';
+$FL_MODULE_SETTING['sub_module']['no_spam'] = 'no_spam.php';
+//$FL_MODULE_SETTING['sub_module']['test'] = 'test.php';
+$FL_MODULE_SETTING['sub_module']['clear_cookies'] = 'clear_cookies.php';
+$FL_MODULE_SETTING['sub_module']['session_test'] = 'scripts/session_test.php';
+$FL_MODULE_SETTING['sub_module']['_autoload'] = 'scripts/include_path.php';
+//$FL_MODULE_SETTING['sub_module']['_include_path'] = 'scripts/include_path.php';
+
+?>
