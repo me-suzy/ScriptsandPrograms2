ff4e7454061b45b79be764e9d27938955d7dd046
me-suzy <me.suzana@gmail.com>
2025-08-12 09:01:21 +0300

    Add Database Tools/phpdbform-0.5-langsupport/phpdbform-0.5-langsupport/lang/lang.de.php

---
+++ Database Tools/phpdbform-0.5-langsupport/phpdbform-0.5-langsupport/lang/lang.de.php
@@ -0,0 +1,25 @@
+<?
+define( "_LANGMAINPAGE",	"Startseite" );
+define( "_LANGWELCOME",		"Willkommen" );
+define( "_LANGWELCOMEMSG",	"Benutzen Sie bitte das Menu auf der linken Seite." );
+
+define( "_LANGSUBMIT",		"Eintragen" );
+define( "_LANGDELETE",		"Löschen" );
+define( "_LANGLOGIN",		"Anmelden" );
+define( "_LANGLOGOUT",		"Abmelden" );
+define( "_LANGLOGINUSER",	"Benutzer" );
+define( "_LANGPASSWORD",	"Passwort" );
+
+define( "_LANGINSERTNEWRECORD",		"Neuer Eintrag" );
+
+define( "_LANGDATALOADDB",			"Loading data from DB" );
+define( "_LANGDATALOADFORM",		"Loading data into select form" );
+define( "_LANGDATAINSERT",			"Inserting data" );
+define( "_LANGDATADELETE",			"Deleting data" );
+define( "_LANGDATAUPDATE",			"Updating data" );
+
+define( "_LANGERRORINVALIDLOGIN",	"Ungültiger Benutzer oder Passswort." );
+define( "_LANGERRORCANTCONNECT",	"Can't connect to database" );
+define( "_LANGERRORFILTER",			"*ERROR* add_filter called without using selection form" );
+
+?>
