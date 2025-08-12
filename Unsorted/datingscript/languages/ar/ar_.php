79a695396b3f0ca6f7b40a2e54405980b3d5f637
me-suzy <me.suzana@gmail.com>
2025-08-13 02:19:39 +0300

    Add Unsorted/datingscript/languages/ar/ar_.php

---
+++ Unsorted/datingscript/languages/ar/ar_.php
@@ -0,0 +1,490 @@
+<?php 
+############################################################
+# \-\-\-\-\-\-\     AzDG  - S C R I P T S    /-/-/-/-/-/-/ #
+############################################################
+# AzDGDatingLite          Version 2.0.3                    #
+# Writed by               AzDG (support@azdg.com)          #
+# Created 10/01/03        Last Modified 05/02/03           #
+# Scripts Home:           http://www.azdg.com              #
+############################################################
+# File name               arabic_.php                      #
+# File purpose            Arabic language file             #
+# File created by         Mahmoud Sarhan <hayl@yalla.com>  #
+# Arabic Demo             www.wearabs.com                  #
+# ÇááÛÉ ÇáÚÑÈíÉ              ãÍãæÏ ÓÑÍÇä                   #
+############################################################
+
+$wg=array(
+'ÚÇã', 
+'ÐßÑ',
+'ÇäËì'
+);
+
+$wp=array(
+'ÚÜÜÜÇã',
+'ááÕÏÇÞÉ Ýì Çááå',
+'ááÊÚÇÑÝ Ýì Çááå',
+'ÃÎæÉ Ýì Çááå',
+'ááÒæÇÌ Úáì ÓäÉ Çááå',
+);
+
+// Privileged users names
+$wpu=array(
+'Lite member', // Free user
+'Silver member',
+'Gold member',
+'Platinum member');
+// Privileged users timelife (in month)
+$wpt=array(
+'0', // Free user
+'3',
+'6',
+'12');
+// Payment for each member type in dollars
+$wpm=array(
+'0', // Free User
+'19',
+'29',
+'39');
+
+
+$wm=array(
+'ÛíÑ ãÍÏÏ', 
+'ÃÚÒÈ',
+'ãÊÒæÌ',
+'ãØáÞ',
+'ÇÑãá',
+'ãÑÊÈØ'
+);
+
+$wmm=array(
+'ÛíÑ ãÍÏÏ',
+'íäÇíÑ',
+'ÝÈÑÇíÑ',
+'ãÇÑÓ',
+'ÇÈÑíá',
+'ãÇíæ',
+'íæäíæ',
+'íæáíÉ',
+'ÇÛÓØÓ',
+'ÓÈÊãÈÑ',
+'ÇßÊæÈÑ', // [10]
+'äæÝãÈÑ',
+'ÏíÓãÈÑ'
+);
+
+$wc=array(
+'ÛíÑ ãÍÏÏ',
+'áÇíæÌÏ',
+'1',
+'2',
+'3',
+'4',
+'5 Çæ ÇßËÑ'
+);
+
+$wh=array(
+'ÛíÑ ãÍÏÏ',
+' (127Óã)',
+' (132Óã)',
+' (137Óã)',
+' (142Óã)',
+' (152Óã)',
+' (152Óã)',
+' (157Óã)',
+' (160Óã)',
+' (163Óã)',
+' (166Óã)', // [10]
+' (168Óã)',
+' (171Óã)',
+' (173Óã)',
+' (175Óã)',
+' (178Óã)',
+' (183Óã)',
+' (188Óã)',
+' (193Óã)',
+' (198Óã)',
+' (203Óã)', // [20]
+' (208Óã)',
+' (213Óã)'
+);
+
+$ww=array(
+'ÛíÑ ãÍÏÏ',
+' (36ßÌã)',
+' (39ßÌã)',
+' (41ßÌã)',
+' (43ßÌã)',
+' (45ßÌã)',
+' (48ßÌã)',
+' (50ßÌã)',
+' (52ßÌã)',
+' (54ßÌã)',
+' (57ßÌã)', // [10]
+' (59ßÌã)',
+' (61ßÌã)',
+' (63ßÌã)',
+' (66ßÌã)',
+' (68ßÌã)',
+' (70ßÌã)',
+' (73ßÌã)',
+' (75ßÌã)',
+' (77ßÌã)',
+' (79ßÌã)', // [20]
+' (82ßÌã)',
+' (84ßÌã)',
+' (86ßÌã)',
+' (89ßÌã)',
+' (91ßÌã)',
+' (93ßÌã)',
+' (95ßÌã)',
+' (97ßÌã)',
+' (100ßÌã)',
+' (102ßÌã)', // [30]
+' (104ßÌã)',
+' (107ßÌã)',
+' (109ßÌã)',
+' (111ßÌã)',
+' (113ßÌã)',
+' (116ßÌã)',
+' (118ßÌã)',
+' (120ßÌã)',
+' (122ßÌã)',
+' (125ßÌã)', // [40]
+' (127ßÌã)',
+' (129ßÌã)',
+' (131ßÌã)',
+' (134ßÌã)',
+' (136ßÌã)'
+);
+
+$whc=array(
+'ÛíÑ ãÍÏÏ',
+'Black',
+'Blonde',
+'Brown',
+'Brunette',
+'Chestnut',
+'Auburn',
+'Dark-blonde',
+'Golden',
+'Red',
+'Gray', // [10]
+'Silver',
+'White',
+'Bald',
+'Other'
+);
+
+
+$we=array(
+'ÛíÑ ãÍÏÏ',
+'ÇÓæÏ',
+'Èäì',
+'ÇÒÑÞ',
+'ÇÎÖÑ',
+'ÑÕÇÕì',
+'ÛíÑ Ðáß'
+);
+
+$wet=array(
+'ÛíÑ ãÍÏÏ',
+'ÇÓãÑ',
+'ÇÈíÖ',
+'ÇÓíæì',
+'ÇÝÑíÞì',
+'ãáæä',
+'ÃÎÑì'
+);
+
+$wr=array(
+'ÛíÑ ãÍÏÏ',
+'Agnostic',
+'Atheist',
+'Baptist',
+'Belief in God',
+'Buddhist',
+'Catholic',
+'Christian',
+'Eastern Orthodox',
+'Humanist',
+'Judaism', // [10]
+'Lutheran',
+'Mormon (LDS)',
+'Muslim',
+'New Age',
+'Non-believer',
+'Protestant',
+'Other'
+);
+
+
+$ws=array(
+'ÛíÑ ãÍÏÏ',
+'Non-Smoker',
+'Occasionally',
+'Regularly'
+
+);
+
+$wd=array(
+'ÛíÑ ãÍÏÏ',
+'ÃÕáì',
+'ÃÍíÇäÇð',
+'ÞáíáÇð'
+);
+
+$wed=array(
+'ÛíÑ ãÍÏÏ',
+'ãÏÑÓÉ ËÇäæíÉ',
+'ãÚåÏ',
+'ØÇáÈ ÌÇãÚì',
+'ÏÑÇÓÇÊ ÚáíÇ',
+'ÏßÊæÑÇÉ'
+);
+
+$wu=array(
+'ÛíÑ ãÍÏÏ',
+'ÈÇäÑ ÅÚáÇäì',
+'ÇáÕÍÝ',
+'ÇáÊáÝÒíæä',
+'ãÍÑß ÈÍË',
+'ÕÏíÞ',
+'ÃÎÑì'
+);
+
+$whr=array(
+'ÛíÑ ãÍÏÏ',
+'ÇáÌÏì',
+'ÇáÏáæ',
+'ÇáÍæÊ',
+'ÇáÍãá',
+'ÇáÊæÑ',
+'ÇáÌæÒÇÁ',
+'ÇáÓÑØÇä',
+'ÇáÇÓÏ',
+'ÇáÚÐÑÇÁ',
+'ÇáãíÒÇä', // [10]
+'ÇáÚÞÑÈ',
+'ÇáÞæÓ'
+);
+
+$wsb=array(
+'ÊÇÑíÎ ÊäÇÒáì',
+'ÊÇÑíÎ ÊÕÇÚÏì',
+'ÇáÇÓã ÊäÇÒáì',
+'ÇáÇÓã ÊÕÇÚÏì',
+'ÇáÓä ÊäÇÒáì',
+'ÇáÓä ÊÕÇÚÏì',
+'ÇáØæá ÊäÇÒáì',
+'ÇáØæá ÊÕÇÚÏì',
+'ÇáæÒä ÊäÇÒáì',
+'ÇáæÒä ÊÕÇÚÏì'
+);
+/* 
+$wsb only for search - you can`t add new or remove - 
+only translate!
+*/
+
+$wrg=array(
+'Çáßá',
+'ÇÎÑ 7 ÇíÇã',
+'ÇÎÑ ÔåÑ',
+'ÇÎÑ 3 ÔåæÑ',
+'ÇÎÑ ÚÇã'
+);
+
+$wrgv=array(
+'', // Not require enter this value for All period
+'7', 
+'31',
+'92',
+'365'
+);
+
+
+$wst=array(
+'All users',
+'Waiting check from mail',
+'Waiting register check',
+'Waiting update check',
+'Waiting admin allow to remove',
+'Active users'
+);
+
+$wcr=array(
+' ÛíÑ ãÍÏÏ', // First space is required for sorting
+'Afghanistan',
+'Albania',
+'Algeria',
+'Andorra',
+'Angola',
+'Antigua and arbuda',
+'Argentina', 
+'Armenia',
+'Australia',
+'Austria',
+'Azerbaijan',
+'Bahamas',
+'Bahrain',
+'Bangladesh',
+'Barbados',
+'Belarus', 
+'Belgium',
+'Bolivia',
+'Bosnia and Herzegovina',
+'Botswana',
+'Brazil',
+'Brunei Darussalam',
+'Bulgaria',
+'Burundi',
+'Cambodia',
+'Cameroon',
+'Canada',
+'Central African Republic',
+'Chad',
+'Chile',
+'China',
+'Colombia',
+'Congo',
+'Costa Rica',
+'Ivory Coast',
+'Croatia',
+'Cuba',
+'Cyprus',
+'Czech Republic',
+'Denmark',
+'Dominican Republic',
+'Ecuador',
+'Egypt',
+'El Salvador',
+'Equatorial Guinea',
+'Estonia',
+'Ethiopia',
+'Fiji',
+'Finland',
+'France',
+'Gabon',
+'Gambia',
+'Georgia',
+'Germany', 
+'Ghana',
+'Gibraltar',
+'Greece',
+'Greenland',
+'Grenada',
+'Guadeloupe',
+'Guatemala',
+'Guinea-Bissau',
+'Guinea', 
+'Guyana',
+'Haiti',
+'Honduras',
+'Hong Kong',
+'Hungary',
+'Iceland',
+'India',
+'Indonesia',
+'Iran', 
+'Iraq',
+'Ireland',
+'Israel',
+'Italy',
+'Jamaica',
+'Japan',
+'Jordan',
+'Kazakhstan',
+'Kenya',
+'North Korea',
+'South Korea',
+'Kuwait',
+'Kyrgyzstan',
+'Laos',
+'Latvia',
+'Lebanon',
+'Lesotho',
+'Liberia',
+'Libya',
+'Liechtenstein',
+'Lithuania',
+'Luxembourg',
+'Macedonia',
+'Madagascar',
+'Malaysia',
+'Mali', 
+'Malta',
+'Mauritania',
+'Mexico',
+'Moldova',
+'Monaco', 
+'Mongolia',
+'Morocco',
+'Mozambique',
+'Namibia',
+'Nepal',
+'Netherlands', 
+'New Zealand',
+'Nicaragua',
+'Nigeria',
+'Niger',
+'Norway',
+'Oman', 
+'Pakistan',
+'Panama',
+'Papua New Guinea',
+'Paraguay',
+'Peru',
+'Philippines',
+'Poland',
+'Portugal', 
+'Puerto Rico',
+'Qatar',
+'Romania',
+'Russian Federation',
+'Rwanda',
+'Samoa',
+'San Marino',
+'Saudi Arabia',
+'Senegal',
+'Sierra Leone',
+'Singapore',
+'Slovak Republic',
+'Slovenia', 
+'Solomon Islands',
+'Somalia',
+'South Africa',
+'Spain',
+'Sri Lanka',
+'Sudan',
+'Swaziland',
+'Sweden',
+'Switzerland',
+'Syria',
+'Taiwan',
+'Tajikistan',
+'Tanzania',
+'Thailand',
+'Tunisia',
+'Turkey',
+'Turkmenistan',
+'Uganda',
+'Ukraine', 
+'United Arab Emirates',
+'United Kingdom',
+'United States',
+'Uruguay',
+'Uzbekistan',
+'Vatican City State',
+'Venezuela',
+'Viet Nam',
+'Yemen',
+'Yugoslavia',
+'Zaire',
+'Zambia',
+'Zimbabwe' 
+);
+// No more 255 countries in this list!!!
+// You can change this list to cities of your country
+// Be carefull!
+?>
+
