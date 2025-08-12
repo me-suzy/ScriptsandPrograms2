da9e36d3660402e6309699a62b50dfe9f074f8f7
me-suzy <me.suzana@gmail.com>
2025-08-12 17:08:23 +0300

    Add Groupware Tools/l4w_v0_5_2/modules/contacts/views/locked.php

---
+++ Groupware Tools/l4w_v0_5_2/modules/contacts/views/locked.php
@@ -0,0 +1,16 @@
+<?php
+  /**
+    * $Id: locked.php,v 1.3 2004/12/28 16:58:20 carsten Exp $
+    *
+    * template for locked contacts
+    *
+    * @author       Carsten Graef <evandor@gmx.de>
+    * @copyright    evandor media 2004
+    * @package contacts
+    */
+
+   /**
+    * Standard Inclusions 
+    */
+    include ("contact.tpl?locked=true");
+?>
