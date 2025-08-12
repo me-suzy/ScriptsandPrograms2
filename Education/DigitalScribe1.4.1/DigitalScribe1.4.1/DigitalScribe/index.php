d43c80d2b1c384b60ff00e09c7d2ba0e5903aa6b
me-suzy <me.suzana@gmail.com>
2025-08-12 12:51:44 +0300

    Add Education/DigitalScribe1.4.1/DigitalScribe1.4.1/DigitalScribe/index.php

---
+++ Education/DigitalScribe1.4.1/DigitalScribe1.4.1/DigitalScribe/index.php
@@ -0,0 +1,14 @@
+<HTML><HEAD><TITLE>Digital Scribe</TITLE>
+<LINK REL ="stylesheet" HREF="style.css" TYPE="text/css">
+<?
+include("header1.php");
+?>
+
+<span class=title>School Name</span>
+
+<P>Welcome to our school. Please use the navigation bar on the left to move around our site.
+
+
+<?
+include("footer.php");
+?>
