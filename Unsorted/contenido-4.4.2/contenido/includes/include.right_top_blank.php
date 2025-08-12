5341380dac34d1e8d5f4e1fda7f280dce805a103
me-suzy <me.suzana@gmail.com>
2025-08-13 02:01:48 +0300

    Add Unsorted/contenido-4.4.2/contenido/includes/include.right_top_blank.php

---
+++ Unsorted/contenido-4.4.2/contenido/includes/include.right_top_blank.php
@@ -0,0 +1,19 @@
+<?php
+
+/******************************************
+* File      :   include.default_right_top_blank.php
+* Project   :   Contenido
+* Descr     :   Builds empty third navigation
+*               layer
+*
+* Author    :   Olaf Niemann
+* Created   :   29.04.2003
+* Modified  :   29.04.2003
+*
+* © four for business AG
+******************************************/
+
+include ($cfg["path"]["contenido"].$cfg["path"]["templates"] . $cfg["templates"]["right_top_blank"]);
+
+
+?>
