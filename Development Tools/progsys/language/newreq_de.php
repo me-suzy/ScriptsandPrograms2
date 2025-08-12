ab9065aefb1103ea5608539542d36f26661fb978
me-suzy <me.suzana@gmail.com>
2025-08-12 10:12:43 +0300

    Add Development Tools/progsys/language/newreq_de.php

---
+++ Development Tools/progsys/language/newreq_de.php
@@ -0,0 +1,17 @@
+<?php
+/***************************************************************************
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+/***************************************************************************
+ * Created by: Boesch IT-Consulting (info@boesch-it.de)
+ * (c)2002-2005 Boesch IT-Consulting
+ * *************************************************************************/
+$l_newreqmail = "Es wurde ein neuer Featurerequest fuer '$progname' vorgenommen:\r\n
+$requesttext\r\n\r\n";
+$l_newreqsubject = "Neuer Featurerequest";
+?>
