cb83ac07e4a66c2228571ed14144a064c7c775d7
me-suzy <me.suzana@gmail.com>
2025-08-12 07:10:00 +0300

    Add Content Management/metajour2_1/metajour/lang/filter.en.php

---
+++ Content Management/metajour2_1/metajour/lang/filter.en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['name'] = 'Report definitions';
+$LANG['option_filtertype']['0'] = 'Inline report definition';
+$LANG['option_filtertype']['5'] = 'Server report definition';
+$LANG['option_filtertype']['10'] = 'User uploaded report definition';
+$LANG['label_name'] = 'Name';
+$LANG['label_content'] = 'Content';
+$LANG['label_mimetype'] = 'MIME type';
+$LANG['label_classtype'] = 'Table';
+$LANG['label_filtertype'] = 'Report type';
+$LANG['label_filterfiletype'] = 'File type';
+$LANG['label_binfileid'] = 'File name';
+$LANG['label_datatype'] = 'Table';
+?>
