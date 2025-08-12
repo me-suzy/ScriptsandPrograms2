b7ee0d4200ed63992ebd8586f8fa63dbeb791ecb
me-suzy <me.suzana@gmail.com>
2025-08-12 21:10:26 +0300

    Add News Publishing/zomplog34/zomplog34/skins/kindakubrick/header.php

---
+++ News Publishing/zomplog34/zomplog34/skins/kindakubrick/header.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+<title>Zomplog</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+
+<!-- leave this in place if you're changing the skin -->
+<link href="skins/kindakubrick/style.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="./popup/scripts/thumbnail.js"></script>
+  
+  <style type="text/css">
+   @import "./popup/css/popups_all.css";
+  </style>
+  <!--[if IE]>
+   <link href="./popup/css/popups_ie.css" rel="stylesheet" type="text/css" media="screen">
+   <script type="text/javascript"> onload = function() { document.focus() }</script>
+  <![endif]-->
+
+
+<script language="javascript" type="text/javascript">
+function OpenLarge (c) {
+    window.open(c,
+                    'large',
+                    'width=500,height=370,scrollbars=yes,status=yes');
+}
+</script> 
+
+<!-- you can change anything below -->
+
+
+<style type="text/css">
+<!--
+body {
+	background-image: url(skins/kindakubrick/images/bg.gif);
+}
+-->
+</style></head>
+
+<body>
+<table width="100" border="0" align="center" cellpadding="0" cellspacing="0" class="text">
+  <tr> 
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><a href="index.php"><img src="skins/kindakubrick/images/top.gif" border="0"></a></td>
+  </tr>
+  <tr>
+    <td background="skins/kindakubrick/images/middle.gif"><table width="100%"  border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td width="3%">&nbsp;</td>
+    <td width="94%"><img src="skins/kindakubrick/images/headertop.gif"></td>
+    <td width="3%">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td background="skins/kindakubrick/images/headermiddle.gif"><div align="center"><a href="index.php" class="weblog_title"><? echo "$settings[weblog_title]"; ?></a></div></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td><img src="skins/kindakubrick/images/headerbottom.gif"></td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+</td>
+  </tr>
+  <tr> 
+    <td background="skins/kindakubrick/images/middle.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0" class="text">
+        <tr> 
+          <td width="45"><img src="skins/kindakubrick/images/spacer.gif" width="45" height="17"></td>
+          <td width="467">&nbsp;</td>
+          <td width="40"><img src="skins/kindakubrick/images/spacer.gif" width="45" height="17"></td>
+        </tr>
+        <tr> 
+          <td>&nbsp;</td>
+		  <td valign="top">
