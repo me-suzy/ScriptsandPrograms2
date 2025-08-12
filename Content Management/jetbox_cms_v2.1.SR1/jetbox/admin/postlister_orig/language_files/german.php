3b3d755cad27b4fcec5dced380fe1ea9ac9ae97d
me-suzy <me.suzana@gmail.com>
2025-08-12 05:04:54 +0300

    Add Content Management/jetbox_cms_v2.1.SR1/jetbox/admin/postlister_orig/language_files/german.php

---
+++ Content Management/jetbox_cms_v2.1.SR1/jetbox/admin/postlister_orig/language_files/german.php
@@ -0,0 +1,139 @@
+<?php 
+
+// german translation by Dietmar Winkler 
+// dietmar.winkler@webtweaker.com, http://www.webtweaker.com 
+
+$s1 = "Hilfe (auf Englisch)"; 
+$s2 = "Mail schreiben"; 
+$s3 = "Adressen hinzufügen/entfernen"; 
+$s4 = "Listen bearbeiten"; 
+$s5 = "Listen anlegen/löschen"; 
+$s6 = "Jetzt wird die Haupttabelle von Postlister erstellt. Das muß man nur einmal machen. Sie haben den Namen <i>$mainTable</i> gewählt. Wenn Sie diesen Namen ändern wollen, müssen Sie in der Datei <i>settings.php3</i> die variable <i>\$mainTable</i> verändern. Oder sie drücken den unteren Button um die Tabelle zu erstellen."; 
+$s7 = "Tabelle anlegen"; 
+$s8 = "Ein Fehler ist aufgetreten"; 
+$s9 = "Zurück"; 
+$s10 = "Der Name der Tabelle ist nicht richtig. Es können nur Buchstaben und Zahlen enthalten sein -- keine Leer- oder Sonderzeichen."; 
+$s11 = "Postlister's Haupttabelle <i>$mainTable</i> wurde erstellt. Sie können nun <a href=lists.php3>Mailinglisten erstellen</a>."; 
+$s12 = "Mailingliste auswählen:"; 
+$s13 = "OK"; 
+$s14 = "Keine Mailinglisten verfügbar"; 
+$s15 = "Maillingliste anlegen"; 
+$s16 = "Maillingliste anlegen"; 
+$s17 = "Name der Liste:"; 
+$s18 = "Wählen Sie einen Namen für die Mailingliste. Die Bezeichnung darf nicht länger als 20 Zeichen sein und darf keine besondern Zeichen und Leerzeichen enthalten -- lediglich einfache Buchstaben (von A bis Z) und Zahlen."; 
+$s19 = "Mailingliste löschen"; 
+$s20 = "Welche Mailingliste wollen Sie löschen?"; 
+$s21 = "Löschen"; 
+$s22 = "Die Mailingliste <i>$listeOpret</i> wurde angelegt. Sie können die Liste <a href=edit.php3?liste=$listeOpret>nun editieren</a>."; 
+$s23 = "Wollen Sie die Liste <i>$listeSlet</i> wirklich löschen? Es werden alle Adressen mitgelöscht!"; 
+$s24 = "Abbrechen"; 
+$s25 = "löschen"; 
+$s26 = "Die Mailingliste <i>$listeSletBekräft</i> wurde gelöscht."; 
+$s27 = "Absender, z.B. <i>Ihr Name <vorname.nachname@$SERVER_NAME></i>:"; 
+$s28 = "Die Signatur, die am Ende jedes Mails angehängt wird:"; 
+$s29 = "Anmelde-Nachricht -- diese Nachricht wird an diejenigen gesendet, die sich in die Liste eintragen wollen."; 
+$s30 = "Änderungen speichern"; 
+$s31 = "Abmelde-Nachricht <b>muß</b> beinhaltet folgendes Wort: <i>[SUBSCRIBE_URL]</i>."; 
+
+# The following variable will go into an email body. Therfore, you need to break all lines after 72 characters. 
+$s32 = "Sie erhalten dieses E-Mail, da Sie sich für die Mailingliste 
+$listeOpret auf http://$HTTP_HOST angemeldet haben. 
+Bevor wir Sie zu dieser Liste hinzufügen können, müßen wir überprüfen, 
+ob die E-Mail-Adresse funktioniert bzw. richtig angegeben wurde und 
+daß Sie sich wirklich in unsere Liste eintragen wollen. Dazu bitten wir Sie, auf 
+folgende Adresse zu gehen, um Ihre Anmeldung bestätigen zu können: 
+
+<[SUBSCRIBE_URL]> 
+
+Vielen Dank!"; 
+
+$s33 = "Die Änderungen in der Liste <i>$liste</i> wurden gespeichert."; 
+$s34 = "E-Mail-Adresse hinzufügen"; 
+$s35 = "E-Mail-Adresse löschen"; 
+$s36 = "Hinzufügen"; 
+$s37 = "Schreiben Sie hier die E-Mail-Adresse hinein, die zur Liste hinzugefügt werden soll. -- z.B. <i>vorname.nachname@beispiel.com</i>:"; 
+$s38 = "<i>$epostadresseTilföj</i> ist keine gültige E-Mail-Adresse."; 
+$s39 = "Die E-Mail-Adresse <i>$epostadresseTilföj</i> wurde zur Liste <i>$liste</i> hinzugefügt."; 
+$s40 = "Offensichtlich existiert die Adresse <i>$epostadresseTilföj</i> schon in der Liste."; 
+$s41 = "Zeigen"; 
+$s42 = "alle Teilnehmer"; 
+$s43 = "geprüft"; 
+$s44 = "nicht geprüft"; 
+$s45 = "die mit folgendem Buchstaben beginnen:"; 
+$s46 = "die folgendes beinhalten:"; 
+$s47 = "Keine Ergebnisse."; 
+$s48 = "geprüft"; 
+$s49 = "nicht-geprüft"; 
+$s50 = "Die E-Mail-Adresse <i>$sletDenne</i> wurde von der Mailingliste <i>$liste</i> entfernt."; 
+$s51 = "Nachricht an die Liste <i>$liste</i> schreiben"; 
+$s52 = "Von:"; 
+$s53 = "Betreff:"; 
+$s54 = "Nachricht:"; 
+$s55 = "Zeile nach 72 Zeichen umbrechen"; 
+$s56 = "Vorschau"; 
+$s57 = "Nachricht Drucken"; 
+$s58 = "Anzahl der Wörter"; 
+$s59 = "Funktionen"; 
+$s60 = "Anzahl der Zeichen:"; 
+$s61 = "Anzahl der Wörter:"; 
+$s62 = "Sie benötigen das richtige Paßwort."; 
+$s63 = "Sie können die folgenden Variablen im E-Mail-Text verwenden:"; 
+$s64 = "E-Mail-Adresse des Empfängers."; 
+$s65 = "URL für die Abmeldung -- die URL, die der Empfänger benötigt, um sich von der Liste löschen zu können."; 
+$s66 = "An:"; 
+$s67 = "Senden"; 
+$s68 = "Zurück -- Ich will das Mail verändern"; 
+$s69 = "Mailinglisten"; 
+$s70 = "Für welche Themen interessieren Sie sich?"; 
+$s71 = "Ihre E-Mail-Adresse:"; 
+$s72 = "Thema auswählen:"; 
+$s73 = "Anmelden"; 
+$s74 = "Abmelden"; 
+$s75 = "<i>$email</i> ist keine gültige Adresse."; 
+$s76 = "Sie müssen auf Anmelden oder Abmelden klicken. Falls das Problem öfters auftritt, kontaktieren Sie bitte den Administrator."; 
+$s77 = "Anmeldung zur $list Mailingliste"; 
+$s78 = "Abmeldung von der Liste: $list"; 
+$s79 = "Vielen Dank für Ihre Anmeldung bei der Mailingliste <i>$list</i>. Sie erhalten in den nächsten Minuten ein E-Mail mit einer Adresse (URL), die Sie bitte besuchen, um Ihre Anmeldung bestätigen zu können."; 
+$s80 = "Bevor wir Sie von der Liste <i>$list</i> enfernen können, bitten wir Sie, das E-Mail für die Abmeldung zu bestätigen. Sie erhalten in den nächsten Minuten ein E-Mail mit einer Adresse (URL), die Sie bitte besuche, um Ihre Abmeldung bestätigen zu können."; 
+
+# Die folgende Variable wird im E-Mail-Body stehen. Deshalb müssen Sie alle Zeilen nach 72 Zeichen umbrechen.
+$s81 = "Sie erhalten dieses E-Mail, da Sie sich von der Mailingliste 
+$listeOpret auf http://$HTTP_HOST abgemeldet haben. 
+Bevor wir Sie endgültig aus der Liste löschen, möchten wir sicherstellen, 
+daß Sie auch wirklich aus der Liste entfernt werden wollen. 
+Gehen Sie bitte dazu auf folgende Adresse, um die Abmeldung zu bestätigen: 
+[UNSUBSCRIBE_URL]
+
+Vielen Dank!"; 
+
+$s82 = "Die Nachricht für die Abmeldung <b>muß</b> folgendes Wort beinhalten: <i>[UNSUBSCRIBE_URL]</i>."; 
+$s83 = "Die Abmelde-Nachricht -- die Nachricht, die diejenigen gesendet wird, die sich von der Liste abmelden wollen."; 
+$s84 = "Offensichtlich existiert die E-Mail-Adresse <i>$email</i> schon in der Mailingliste."; 
+$s85 = "Fertig! Das E-Mail wurde nun an alle Teilnehmer versendet!"; 
+$s86 = "Das Programm sendet gerade die E-Mail's"; 
+$s87 = "durch"; 
+$s88 = "Schließen Sie das Browser-Fenster NICHT! Wärend des Versende-Vorganges sollten Sie nichts berühren."; 
+$s89 = "Die E-Mail-Adresse <i>$email</i> existiert nicht. Sie können sich deshalb nicht abmelden."; 
+$s90 = "Keine E-Mail-Adresse wurde angegeben."; 
+$s91 = "Sie haben nicht ausgewählt, ob Sie sich an- oder abmelden wollen."; 
+$s92 = "Sie haben die E-Mail-ID nicht angegeben."; 
+$s93 = "Sie haben keine Maillingliste ausgewählt."; 
+$s94 = "Sie haben nicht die richtige ID für die E-Mail-Adresse <i>$epost</i> angegeben."; 
+$s95 = "Fertig! Sie sind nun zur Mailingliste <i>$liste</i> hinzugefügt worden."; 
+$s96 = "Sie sind nun von der Mailingliste <b><i>$liste</i></b> gelöscht worden und erhalten keine weiteren Informationen mehr."; 
+$s97 = "von"; 
+$s98 = "E-Mail-Adressen importieren"; 
+$s99 = "Öffnen und importieren"; 
+$s100 = "Die Datei <i>$importfil</i> wurde nicht gefunden."; 
+$s101 = "Fertig! Alle E-Mail-Adressen der Datei <i>$importfil</i> sind in die Liste <i>$liste</i> importiert worden."; 
+$s102 = "Wenn Sie eine Datei mit E-Mail-Adressen haben, können Sie diese in die Liste <i>$liste</i> importieren. Es ist wichtig, daß die Datei je Zeile nur eine E-Mail-Adresse enthält (und sonst nichts!). Anders gesagt, das Format sollte ungefähr so ausehen: <p><i>vorname.nachname@muster.com<br>joe.johnson@example.com<br>php@php.net</i>"; 
+$s103 = "Datei:"; 
+$s104 = "Zurück zur Postlister-Hauptseite"; 
+$s105 = "Importieren/exportieren"; 
+$s106 = "Export von E-Mail-Adressen"; 
+$s107 = "Exportieren"; 
+$s108 = "Mit dieser Funktion können Sie E-Mail-Adressen der Liste <i>$liste</i> exportieren. Alle E-Mail-Adresen werden in eine Datei gespeichert (eine Adresse pro Zeile). Der Name der Datei wird <i>postlister-$liste.txt</i> heißen, diese wird in dem unten angegebenem Verzeichnis gespeichert. <b>Es ist notwendig, daß Sie dem Verzeichnis, in welchem die Datei gespeichert werden soll, die notwendigen Berechtigungen vergeben (setzen Sie dazu mit einem FTP client oder SSH/telnet das Zielverzeichnis auf chmod 777).</b>"; 
+$s109 = "Das Verzeichnis, in welchem die Datei gespeichert werden soll:"; 
+$s110 = "<i>$eksport</i> ist kein Verzeichnis. Sie müssen ein Verzeichnis angeben, in welchem Sie die Datei mit den E-Mail-Adressen abspeichern wollen."; 
+$s111 = "Fertig! Alle E-Mail-Adressen der Liste <i>$liste</i> sind in der Datei <i>$eksport/postliste-$liste.txt</i> abgespeichert worden."; 
+?>
