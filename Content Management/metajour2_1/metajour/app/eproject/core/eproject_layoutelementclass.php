e118b37f5adb08336f0e8400a67160b8c048d31f
me-suzy <me.suzana@gmail.com>
2025-08-12 06:00:12 +0300

    Add Content Management/metajour2_1/metajour/app/eproject/core/eproject_layoutelementclass.php

---
+++ Content Management/metajour2_1/metajour/app/eproject/core/eproject_layoutelementclass.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eProject
+ * @subpackage core
+ * $Id: eproject_layoutelementclass.php,v 1.2 2005/01/12 03:24:08 jan Exp $
+ */
+
+require_once($system_path.'core/basicclass.php');
+
+class eproject_layoutelement extends basic {
+
+	function eproject_layoutelement() {
+		$this->basic();
+		$this->setobjecttype('layoutelement');
+		$this->setsupertype('layout');
+		$this->addcolumn('name',0,UI_STRING);
+	}
+
+}
