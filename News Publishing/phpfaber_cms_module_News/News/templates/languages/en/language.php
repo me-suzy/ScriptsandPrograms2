08fb7bb9e6c76091cc0de6fcc331112275114329
me-suzy <me.suzana@gmail.com>
2025-08-12 20:05:04 +0300

    Add News Publishing/phpfaber_cms_module_News/News/templates/languages/en/language.php

---
+++ News Publishing/phpfaber_cms_module_News/News/templates/languages/en/language.php
@@ -0,0 +1,67 @@
+<?php
+
+// -----------------------------------------------------------------------------
+//
+// phpFaber CMS v.1.0
+// Copyright(C), phpFaber LLC, 2004-2005, All Rights Reserved.
+// E-mail: products@phpfaber.com
+//
+// All forms of reproduction, including, but not limited to, internet posting,
+// printing, e-mailing, faxing and recording are strictly prohibited.
+// One license required per site running phpFaber CMS.
+// To obtain a license for using phpFaber CMS, please register at
+// http://www.phpfaber.com/i/products/cms/
+//
+// 10:52 PM 09/04/2005
+//
+// -----------------------------------------------------------------------------
+
+if (!defined('INDEX_INCLUDED')) die('<code>You cannot access this file directly..</code>');
+
+$_LNG['_HEADER_NEWSLIST_PAGE'] = 'News list';
+$_LNG['_HEADER_ADDNEW_PAGE'] = 'Add new news';
+$_LNG['_HEADER_EDIT_PAGE'] = 'Edit news';
+
+$_LNG['_SETT_RES_NUM'] = 'Number of news per page';
+$_LNG['_SETT_RES_NUM_NOTE'] = 'Short news amount per page.';
+$_LNG['_SETT_RSS_TITLE'] = 'RSS feeds title';
+$_LNG['_SETT_RSS_RES_NUM'] = 'Number of RSS feeds';
+$_LNG['_SETT_HIT_UNBAN'] = 'Hits unban time, minutes';
+$_LNG['_SETT_HIT_UNBAN_NOTE'] = '- till hits will be counted for the same news from the same IP';
+
+$_LNG['_TBL_NEWS'] = 'News';
+$_LNG['_TBL_VISIBLE'] = 'Visible ?';
+$_LNG['_TBL_DATE'] = 'Date';
+$_LNG['_TBL_SHOWED'] = 'Hits';
+$_LNG['_TBL_ACTION'] = 'Action';
+
+$_LNG['_TBL_NEWSTITLE'] = 'News title';
+$_LNG['_TBL_SHOW'] = 'Show on website';
+$_LNG['_TBL_TXTSHORT'] = 'Short text of news';
+$_LNG['_TBL_TXTFULL'] = 'Full text of news';
+$_LNG['_TBL_STARTD'] = 'Date Created';
+$_LNG['_TBL_EXPD'] = 'Expiration Date';
+
+$_LNG['_TBL_TOTALNEWS'] = 'News';
+
+$_LNG['_CMD_EDIT'] = 'Edit news';
+$_LNG['_CMD_DELETE'] = 'Delete news';
+
+$_LNG['_MSG_SHOWALLNEWS'] = 'View full news list';
+$_LNG['_MSG_VISIBLE'] = 'Visible';
+$_LNG['_MSG_HIDDEN'] = 'Hidden';
+$_LNG['_MSG_NO_NEWS'] = 'There are no news on the site.';
+$_LNG['_MSG_ADDED_SUCCESS'] = 'News added successfully';
+$_LNG['_MSG_DELETED_SUCCESS'] = 'News deleted successfully';
+$_LNG['_MSG_UPDATED_SUCCESS'] = 'News updated successfully';
+$_LNG['_MSG_CONFIRMUPD_CLOSE'] = 'Attempt to close page without saving. Are you sure?';
+$_LNG['_MSG_CONFIRM_DELETE'] = 'Confirm delete this news';
+
+$_LNG['_MSG_ER_RES_NUM'] = 'Number of news per page value is wrong';
+
+$_LNG['_MSG_A_ALL'] = 'All news';
+$_LNG['_MSG_A_ACTIVE'] = 'Active news';
+$_LNG['_MSG_A_PENDING'] = 'Pending news';
+$_LNG['_MSG_A_EXPIRED'] = 'Inactive/expired news';
+
+?>
