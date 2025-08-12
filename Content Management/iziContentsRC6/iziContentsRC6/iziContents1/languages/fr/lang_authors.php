6db7fc3452b274b6d1d95b7ea8f0a560ce02d23c
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:16 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_authors.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_authors.php
@@ -0,0 +1,80 @@
+<?
+
+// Form Title
+$GLOBALS["tFormTitle"]		= 'Gestion des Auteurs';
+
+// List Headings
+$GLOBALS["tLogin"] = 'Nom d\'accès';
+$GLOBALS["tUsername"]		= 'Nom d\'utilisateur';
+$GLOBALS["tEMail"]		= 'Courrier';
+$GLOBALS["tUsergroup"] = 'GroupeUsager';
+$GLOBALS["tEnabled"] = 'Activer';
+
+// List Functions
+$GLOBALS["tAuthorFilter"] = 'GroupeUsager filtre';
+$GLOBALS["tAddNewAuthor"]	= 'Ajouter nouveau auteur';
+$GLOBALS["tEditAuthor"]		= 'Editer auteur';
+$GLOBALS["tViewAuthor"] = 'Afficher usager';
+$GLOBALS["tDeleteAuthor"]	= 'Effacer auteur';
+$GLOBALS["tReleaseUser"] = 'Activer/Désactiver l\'usager';
+$GLOBALS["tConfirmRelease"] = 'Vous êtes sûr vous souhaitez rendre activer/désactiver cet utilisateur?';
+
+// Form Block Titles
+$GLOBALS["thLogin"]		= 'Accès Détails';
+$GLOBALS["thUser"]		= 'Usager Détails';
+$GLOBALS["thConfiguration"]	= 'Paramètre Personelle';
+
+// Form Field Headings
+$GLOBALS["tLogin"]		= 'Accès';
+$GLOBALS["tPassword"]		= 'Mot de passe';
+$GLOBALS["tRetypePassword"]	= 'Retapez mot de passe';
+$GLOBALS["tAuthorname"]		= 'Nom Auteur';
+$GLOBALS["tEMail"]		= 'Adresse Courriel';
+$GLOBALS["tPrivateEMail"] = 'Cachez l\'addresse Courriel';
+$GLOBALS["tCountry"]		= 'Pays';
+$GLOBALS["tLanguage"]		= 'Langue';
+$GLOBALS["tPhone"] = 'Téléphone';
+$GLOBALS["tFax"] = 'Fax';
+$GLOBALS["tAddress"] = 'Addresse';
+$GLOBALS["tCity"] = 'Ville';
+$GLOBALS["tState"] = 'État';
+$GLOBALS["tZip"] = 'ZIP';
+$GLOBALS["tWebsite"] = 'Site Web';
+$GLOBALS["tComments"] = 'Commentaire';
+$GLOBALS["tNewsletter"] = 'Bulletin';
+
+
+$GLOBALS["tUsergroup"]		= 'GroupeUsager';
+
+// Form Detail Comments and Help Texts
+$GLOBALS["tDetails"]		 = 'Ce formulaire vous permet d\'éditer ou de créer de nouveaux auteurs qui pourront changer le contenu.';
+$GLOBALS["hLogin"]		 = 'Ceci est le login de l\'auteur utilisé sur l\'interface de connexion.';
+$GLOBALS["hPassword"]		 = 'Entrez le password que l\'auteur utilisera pour se connecter a ezContents.';
+$GLOBALS["hRetypePassword"]	 = 'Re-entrez le password de l\'auteur, pour vérifier que vous l\'avez tapé correctement.';
+$GLOBALS["hAuthorname"]		 = 'Ce champ est le Nom Complet ou le Pseudo de l\'auteur. Il sera visible dans tous les articles publiés par cet auteur si le champ appropié à été coché dans le Formulaire de Gestion du Contenu.';
+$GLOBALS["hUsergroup"]		 = 'Le groupe d\'utilisateurs auquel l\'auteur appartient. Le groupe d\'utilisateurs déterminera les options disponibles pour cet auteur.';
+$GLOBALS["hEMail"]		 = 'L\'Adresse E-mail de cet auteur. Avec le Nom de l\'Auteur cela créera un hyperlien pour envoyer un email.';
+$GLOBALS["hPrivateEMail"] = 'Crochez cette boîte si vous souhaitez garder votre adresse courriel privée.';
+$GLOBALS["hCountry"]		 = 'Le Pays où vit l\'Auteur (optionnel).';
+$GLOBALS["hLanguage"]		 = 'La langue natale l\'Auteur (optionnel).<br>';
+$GLOBALS["hLanguage"]		.= 'Si cela est déterminé,  l\'auteur verra le menu des options, le nom des champs, l\'aide, etc. dans sa langue à la place du langage par défaut du site.';
+$GLOBALS["hPhone"] = 'Usager numéro de téléphone.';
+$GLOBALS["hFax"] = 'Usager numéro de FAX.';
+$GLOBALS["hAddress"] = 'Usager addresse.';
+$GLOBALS["hCity"] = 'La ville où l\'usager habite.';
+$GLOBALS["hState"] = 'Usager état';
+$GLOBALS["hZip"] = 'Usager ZIP code.';
+$GLOBALS["hWebsite"] = 'Usager site web addresse.';
+$GLOBALS["hComments"] = 'Usager commentaire.';
+$GLOBALS["hNewsletter"] = 'Usager s\'est abonné pour le bulletin.';
+
+// Error Messages
+$GLOBALS["eLoginExists"]	= 'Ce nom d\'accès existe déjà, svp choisissez en un autre.';
+$GLOBALS["eLoginEmpty"]		= 'Le champ d\'accès ne peut pas être vide.';
+$GLOBALS["ePasswordError"]	= 'Le second champ mot de passe est différend du premier champ mot de passe.';
+$GLOBALS["eNameEmpty"]		= 'Le Nom de L\'Auteur ne peut pas être vide.';
+$GLOBALS["eNameExists"] = 'Un auteur avec ce nom d\'affichage existe déjà.';
+$GLOBALS["eEMailEmpty"]		= 'Le champ courriel de l\'Auteur ne peut pas être vide.';
+$GLOBALS["eEmailIncorrect"]   = 'L\'addresse courrielle que vous avez entré est dans un format nul.';
+
+?>
