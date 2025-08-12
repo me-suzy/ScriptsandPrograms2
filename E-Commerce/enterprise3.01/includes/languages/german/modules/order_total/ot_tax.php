b80d1185d61c617e4a72ffd4a2505937c83744ce
me-suzy <me.suzana@gmail.com>
2025-08-12 12:01:11 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/modules/order_total/ot_tax.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/modules/order_total/ot_tax.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_ORDER_TOTAL_TAX_TITLE', 'MwSt.');
+  define('MODULE_ORDER_TOTAL_TAX_DESCRIPTION', 'Mehrwertsteuer');
+?>
