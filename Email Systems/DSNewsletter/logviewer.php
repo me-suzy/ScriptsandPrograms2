ef6a1541720da1f065813d9b6f9f862da107a90a
me-suzy <me.suzana@gmail.com>
2025-08-12 13:49:25 +0300

    Add Email Systems/DSNewsletter/logviewer.php

---
+++ Email Systems/DSNewsletter/logviewer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+	<title>ID Search</title>
+</head>
+
+<body>
+<?
+include("header.php");
+?>
+
+<form action="logs.php" method="POST">
+ID: <input name="id" type="text" value=""><br>
+<input type="submit" value="View Log">
+</form><br><br>
+
+<?
+include("footer.php");
+?>
+</body>
+</html>
+
