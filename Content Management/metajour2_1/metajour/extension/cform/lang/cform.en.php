5ede5d79c24293b078a0cf8c24931a6966d05cf0
me-suzy <me.suzana@gmail.com>
2025-08-12 06:12:53 +0300

    Add Content Management/metajour2_1/metajour/extension/cform/lang/cform.en.php

---
+++ Content Management/metajour2_1/metajour/extension/cform/lang/cform.en.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage extension
+ */
+
+$LANG['label_name'] = 'Name';
+$LANG['label_otype'] = 'Table';
+$LANG['label_formtype'] = 'Form type';
+$LANG['label_templateid'] = 'Template';
+$LANG['name'] = 'Configuration of Custom Form';
+$LANG['option_formtype'][0] = 'Create or modify record';
+$LANG['option_formtype'][1] = 'Create only';
+$LANG['option_formtype'][3] = 'Create or modify users own record';
+?>
