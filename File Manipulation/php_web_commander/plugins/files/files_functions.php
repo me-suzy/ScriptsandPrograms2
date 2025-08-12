8d9e26f25f2d1226222b56b8faee9fca55bbef0d
me-suzy <me.suzana@gmail.com>
2025-08-12 15:04:00 +0300

    Add File Manipulation/php_web_commander/plugins/files/files_functions.php

---
+++ File Manipulation/php_web_commander/plugins/files/files_functions.php
@@ -0,0 +1,47 @@
+<?php
+
+//
+// +----------------------------------------------------------------------+
+// | Web Manager                                                          |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2004 Protung Dragos (www.protung.ro)                   |
+// +----------------------------------------------------------------------+
+// +----------------------------------------------------------------------+
+// |   filename             : file.php                                    |
+// |   begin                : 20 07 2004                                  |
+// |   copyright            : (C) 2004 Dragos Protung                     |
+// |   email                : dragos@protung.ro                           |
+// |   lastedit             : 28/07/2004 15:09                            |
+// |                                                                      |
+// |                                                                      |
+// +----------------------------------------------------------------------+
+// | Author: Protung Dragos <dragos@protung.ro>                           |
+// +----------------------------------------------------------------------+
+//
+
+
+
+
+
+/**
+ * @return string
+ * @param string $ext
+ * @param string $icons_dir
+ * @param string $default_ico
+ * @desc Set the path to the icon for a file
+*/
+function set_ico($ext, $icons_dir = './templates/files/images/icons/', $default_ico = 'templates/files/images/icons/default.gif') {
+
+	$ico = $icons_dir . $ext . '.gif';
+
+	if (file_exists($ico)) {
+
+		return $ico;
+	} else {
+
+		return $default_ico;
+	}
+}
+
+
+?>
