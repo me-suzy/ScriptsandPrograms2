e2aeb85c8577c4c4051ccfcb1c6e52468ab86572
me-suzy <me.suzana@gmail.com>
2025-08-12 12:00:57 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/specials.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/specials.php
@@ -0,0 +1,14 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Angebote');
+define('HEADING_TITLE', 'Unsere Sonderangebote:');
+?>
