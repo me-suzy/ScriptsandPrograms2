5316d5e517ef500d71b4ad69ba3cabb6403a4594
me-suzy <me.suzana@gmail.com>
2025-08-13 01:26:41 +0300

    Add Unsorted/cashcrusader/pages/cash_timer.php

---
+++ Unsorted/cashcrusader/pages/cash_timer.php
@@ -0,0 +1,17 @@
+<? include("setup.php");?>
+<html><body><center>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<tr align="center"> 
+<td width="40%" valign="top"><font size=2 face=arial>Thank you for supporting 
+our sponsors! You will receive 
+<? echo $VA;?>
+ cents(s), once you have visited this sponsor's site for at least 
+<? echo $TI;?>
+ seconds</font></td>
+<td width="60%" valign="top"> 
+<? getad('Main');?>
+</td>
+</tr>
+</table>
+</center>
+</body></html>
