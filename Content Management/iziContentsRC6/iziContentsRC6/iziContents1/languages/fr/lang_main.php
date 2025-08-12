ad5b77966d1f1712ec899ddb07b47bc52502860e
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:37 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_main.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_main.php
@@ -0,0 +1,21 @@
+<?
+
+$GLOBALS["tLoggedInAs"]		= 'Vous êtes connecté en tant que';
+$GLOBALS["tNotLoggedIn"]	= 'Vous n\'êtes pas connecté';
+$GLOBALS["tAdminFunctions"]	= 'Fonctions d\'Admin';
+$GLOBALS["tPreferences"]	= 'Préférences';
+$GLOBALS["tMemberList"]		= 'Liste des Membres';
+$GLOBALS["tRegister"]		= 'S\'Enregistrer comme Membre';
+
+$GLOBALS["tReadMore"]		= 'Lire la suite...';
+$GLOBALS["tArticlePostedOn"]	= 'Posté le';
+$GLOBALS["tArticleBy"]		= 'par';
+$GLOBALS["tArticlePostedBy"]	= 'Posté par';
+
+$GLOBALS["tFramesUsed"]		= 'Ce site utilise des frames.';
+$GLOBALS["tNonFramesClick"]	= 'SVP cliquez ici pour une version sans-frames du site.';
+$GLOBALS["tNonFramesWarning"]	= 'SVP notez que dans cette version le site utilise des tableaux, des cookies et du javascript. Si votre Navigateur ne supporte pas ces fonctions, ou que vous les avez désactivées, Nous ne pouvons être tenus pour responsable.';
+
+$GLOBALS["tLanguage"]		= 'Langue';
+
+?>
