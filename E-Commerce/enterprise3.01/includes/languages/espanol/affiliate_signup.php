88468a31ced0bd2efd56bbecf34a59888ae197e8
me-suzy <me.suzana@gmail.com>
2025-08-12 11:56:12 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_signup.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/affiliate_signup.php
@@ -0,0 +1,35 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Affiliate Program');
+define('HEADING_TITLE', 'Affiliate Program - Sign Up');
+
+define('MAIL_AFFILIATE_SUBJECT', 'Welcome to the Affiliate Program');
+define('MAIL_AFFILIATE_HEADER', 'Dear Affiliate,
+
+thank you for joining the Affiliate Program.
+
+Your Account Information:
+***********************
+
+');
+define('MAIL_AFFILIATE_ID', 'Your Affiliate ID is: ');
+define('MAIL_AFFILIATE_USERNAME', 'Your Affiliate Username is: ');
+define('MAIL_AFFILIATE_PASSWORD', 'Your Password is: ');
+define('MAIL_AFFILIATE_LINK', 'Link to your account here:');
+define('MAIL_AFFILIATE_FOOTER', 'Have fun earning referal fees!
+
+Your Affiliate Team');
+?>
