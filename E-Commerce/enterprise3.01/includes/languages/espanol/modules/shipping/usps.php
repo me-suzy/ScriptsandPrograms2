c017bc1b1d78c32af82ab19d04ad882168b1730f
me-suzy <me.suzana@gmail.com>
2025-08-12 11:58:28 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/modules/shipping/usps.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/modules/shipping/usps.php
@@ -0,0 +1,18 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_USPS_TEXT_TITLE', 'United States Postal Service');
+define('MODULE_SHIPPING_USPS_TEXT_DESCRIPTION', 'United States Postal Service<br><br>You will need to have registered an account with USPS at http://www.uspsprioritymail.com/et_regcert.html to use this module<br><br>USPS expects you to use pounds as weight measure for your products.');
+define('MODULE_SHIPPING_USPS_TEXT_OPT_PP', 'Parcel Post');
+define('MODULE_SHIPPING_USPS_TEXT_OPT_PM', 'Priority Mail');
+define('MODULE_SHIPPING_USPS_TEXT_OPT_EX', 'Express Mail');
+define('MODULE_SHIPPING_USPS_TEXT_ERROR', 'Ha ocurrido un error calculando los gastos de envio.<br>Si aun desea usar USPS para su envio, contacte con el administrador.');
+?>
