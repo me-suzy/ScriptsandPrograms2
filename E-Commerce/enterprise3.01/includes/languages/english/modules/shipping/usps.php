3926b142d99fab65ecf229250c44463dff870f97
me-suzy <me.suzana@gmail.com>
2025-08-12 11:55:17 +0300

    Add E-Commerce/enterprise3.01/includes/languages/english/modules/shipping/usps.php

---
+++ E-Commerce/enterprise3.01/includes/languages/english/modules/shipping/usps.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  ++++ modified as USPS Methods 2.5 08/02/03 by Brad Waite and Fritz Clapp ++++
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('MODULE_SHIPPING_USPS_TEXT_TITLE', 'United States Postal Service');
+define('MODULE_SHIPPING_USPS_TEXT_DESCRIPTION', 'United States Postal Service<br><br>You will need to have registered an account with USPS at http://www.uspsprioritymail.com/et_regcert.html to use this module<br><br>USPS expects you to use pounds as weight measure for your products.');
+define('MODULE_SHIPPING_USPS_TEXT_OPT_PP', 'Parcel Post');
+define('MODULE_SHIPPING_USPS_TEXT_OPT_PM', 'Priority Mail');
+define('MODULE_SHIPPING_USPS_TEXT_OPT_EX', 'Express Mail');
+define('MODULE_SHIPPING_USPS_TEXT_ERROR', 'An error occured with the USPS shipping calculations.<br>If you prefer to use USPS as your shipping method, please contact the store owner.');
+define('MODULE_SHIPPING_USPS_TEXT_DAY', 'Day');
+define('MODULE_SHIPPING_USPS_TEXT_DAYS', 'Days');
+define('MODULE_SHIPPING_USPS_TEXT_WEEKS', 'Weeks');
+?>
