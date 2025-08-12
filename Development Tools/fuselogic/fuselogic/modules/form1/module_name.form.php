d563059bded8a5062024fe1a643ad3a4ecbd2ce3
me-suzy <me.suzana@gmail.com>
2025-08-12 09:23:41 +0300

    Add Development Tools/fuselogic/fuselogic/modules/form1/module_name.form.php

---
+++ Development Tools/fuselogic/fuselogic/modules/form1/module_name.form.php
@@ -0,0 +1,11 @@
+<?php
+
+$FL_MODULE_SETTING['module_name'] = 'form';
+
+$FL_MODULE_SETTING['sub_module']['example'] = 'form.register_controller.php';
+$FL_MODULE_SETTING['sub_module']['contact_us'] = 'form.contact_us_controller.php';
+$FL_MODULE_SETTING['sub_module']['text_on_image_example'] = 'form.php';
+$FL_MODULE_SETTING['sub_module']['image'] = 'img.php';
+$FL_MODULE_SETTING['sub_module']['check'] = 'verify.php';
+
+?>
