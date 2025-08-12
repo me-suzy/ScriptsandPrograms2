fa0b1564a49e5e724beab7f4f5eb94a418efff90
me-suzy <me.suzana@gmail.com>
2025-08-12 07:07:37 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_checkedby.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_checkedby.en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Checked by';
+$LANG['text_1'] = 'Select the user that checked the selected records';
+$LANG['text_2'] = 'Checked by: ';
+?>
