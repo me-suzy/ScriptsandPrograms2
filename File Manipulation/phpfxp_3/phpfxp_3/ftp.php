e26c6e2dd13113122c5e718abf1d358b3d483922
me-suzy <me.suzana@gmail.com>
2025-08-12 15:00:48 +0300

    Add File Manipulation/phpfxp_3/phpfxp_3/ftp.php

---
+++ File Manipulation/phpfxp_3/phpfxp_3/ftp.php
@@ -0,0 +1,39 @@
+<?
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	PHP FXP 3.0
+	by Harald Meyer (webmaster@harrym.nu) 
+	Feel free to use it, but don't delete these lines. 
+	PHP FXP is Freeware but all links to the PHP FXP homepage must not be deleted! 
+	If you want to use PHP FXP COMMERCIAL please contact me.
+	Please send me modified versions! 
+	If you use it on your page, a link back to its homepage (to PHP FXP's homepage) 
+	would be highly appreciated.
+	Homepage: http://fxp.harrym.nu
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+ /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	File: ftp.php
+	Description: ftp client frame
+	Last update: 25-09-2002
+	Created by: Harald Meyer
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+ ?>
+ <html>
+<head>
+<title>PHP FXP 3 FTP Client</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<frameset rows="*,145" cols="*" border="0" framespacing="0" frameborder="NO"> 
+  <frame src="upftp.php" noresize scrolling="AUTO" marginwidth="2" marginheight="0" frameborder="NO" name="servergui">
+  <frame src="dftp.php" frameborder="NO" noresize scrolling="NO" marginwidth="0" marginheight="0" name="store">
+</frameset>
+<noframes> 
+<body bgcolor="#FFFFFF" text="#000000">
+</body>
+</noframes> 
+</html>
+<?
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	END
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+?>
