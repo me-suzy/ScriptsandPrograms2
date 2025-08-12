a3faf88932515529a2ed141c401f9cf401880449
me-suzy <me.suzana@gmail.com>
2025-08-12 05:04:03 +0300

    Add Content Management/jetbox_cms_v2.1.SR1/jetbox/admin/postlister/settings.php

---
+++ Content Management/jetbox_cms_v2.1.SR1/jetbox/admin/postlister/settings.php
@@ -0,0 +1,41 @@
+<?php
+#
+# PLEASE EDIT THE FOLLOWING VARIABLES:
+#
+
+# Choose a language file:
+$languageFile = "language_files/english.php";
+
+# Choose a username for the Postlister administration area:
+$username = "beheer";
+
+# Choose a password for the Postlister administration area:
+$password = "openneer";
+
+# The host on which your database runs. Usually "localhost".
+# If you don't know what to write here, write "localhost".
+//$databaseHost = "database.widexs.nl";
+$databaseHost = "database.widexs.nl";
+
+# The name of your database:
+$databaseName = "ebate";
+
+# The username for your database:
+//$databaseUsername = "ebate";
+$databaseUsername = "ebate";
+
+# The password for your database:
+//$databasePassword = "gorcp";
+$databasePassword = "gorcp";
+
+# Choose a name for the Postlister main database table.
+# The table will be created by Postlister.
+# The name cannot include other characters than a-z
+# and 0-9. If you don't know what to write here,
+# just write "postlistermain".
+$mainTable = "postlistermain";
+
+#
+# THAT'S IT!
+#
+?>
