53649bc752d2195dc1697293f200d901e321a004
me-suzy <me.suzana@gmail.com>
2025-08-12 11:41:55 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/cache.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/cache.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Cache Control');
+
+define('TABLE_HEADING_CACHE', 'Cache Blocks');
+define('TABLE_HEADING_DATE_CREATED', 'Date Created');
+define('TABLE_HEADING_ACTION', 'Action');
+
+define('TEXT_FILE_DOES_NOT_EXIST', 'File does not exist');
+define('TEXT_CACHE_DIRECTORY', 'Cache Directory:');
+
+define('ERROR_CACHE_DIRECTORY_DOES_NOT_EXIST', 'Error: Cache directory does not exist. Please set this Configuration->Cache.');
+define('ERROR_CACHE_DIRECTORY_NOT_WRITEABLE', 'Error: Cache directory is not writeable.');
+?>
