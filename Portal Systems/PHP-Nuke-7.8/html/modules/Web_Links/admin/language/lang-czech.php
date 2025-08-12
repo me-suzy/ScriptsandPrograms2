8e6223e35d3b9d04de604cd9890ae508617d10fc
me-suzy <me.suzana@gmail.com>
2025-08-12 23:06:23 +0300

    Add Portal Systems/PHP-Nuke-7.8/html/modules/Web_Links/admin/language/lang-czech.php

---
+++ Portal Systems/PHP-Nuke-7.8/html/modules/Web_Links/admin/language/lang-czech.php
@@ -0,0 +1,112 @@
+<?php
+
+/**************************************************************************/
+/* PHP-NUKE: Advanced Content Management System                           */
+/* ============================================                           */
+/*                                                                        */
+/* This is the language module with all the system messages               */
+/*                                                                        */
+/* If you made a translation go to the my website and send to me          */
+/* the translated file. Please keep the original text order by modules,   */
+/* and just one message per line, also double check your translation!     */
+/*                                                                        */
+/* You need to change the second quoted phrase, not the capital one!      */
+/*                                                                        */
+/* If you need to use double quotes (") remember to add a backslash (\),  */
+/* so your entry will look like: This is \"double quoted\" text.          */
+/* And, if you use HTML code, please double check it.                     */
+/**************************************************************************/
+
+
+define("_URL","URL");
+define("_EMAIL","E-mail");
+define("_FUNCTIONS","Funkce");
+define("_YES","Ano");
+define("_NO","Ne");
+define("_CATEGORY","Kategorie");
+define("_SAVECHANGES","Uloit zmìny");
+define("_OK","Ok!");
+define("_HELLO","Ahoj");
+define("_LINKTITLE","Název odkazu");
+define("_HITS","Klinkutí");
+define("_THEREARE","V souèasné dobì je");
+define("_DESCRIPTION","Popis");
+define("_AUTHOR","Autor");
+define("_NAME","Jméno");
+define("_PAGETITLE","Název stránky");
+define("_PAGEURL","URL");
+define("_ADDURL","Pøidat tuto URL");
+define("_LINKID","Odkaz ID");
+define("_WEBLINKS","Archív odkazù");
+define("_TITLE","Název");
+define("_STATUS","Status");
+define("_ADD","Pøidat");
+define("_MODIFY","Upravit");
+define("_LINKSINDB","odkazù v databázi");
+define("_LINKSWAITINGVAL","Odkazy èekající na ovìøení");
+define("_SUBMITTER","Poslal");
+define("_VISIT","Navtívit");
+define("_CLEANLINKSDB","Vyèistit DB");
+define("_BROKENLINKSREP","Zprávy o neplatných odkazech");
+define("_LINKMODREQUEST","Odkazy k úpravì");
+define("_ADDMAINCATEGORY","Pøidat hlavní kategorii");
+define("_ADDSUBCATEGORY","Pøidat subkategorii");
+define("_IN","v");
+define("_ADDNEWLINK","Pøidat nový odkaz");
+define("_DESCRIPTION255","Popis: (max. 255 znakù)");
+define("_MODCATEGORY","Upravit kategorii");
+define("_MODLINK","Upravit odkaz");
+define("_WEBLINKSADMIN","Administrace Archívu odkazù");
+define("_ADDEDITORIAL","Pøidat komentáø");
+define("_EDITORIALTITLE","Nadpis komentáøe");
+define("_EDITORIALTEXT","Text komentáøe");
+define("_DATEWRITTEN","Datum");
+define("_NOREPORTEDBROKEN","Není zpráva o neplatných odkazech.");
+define("_USERREPBROKEN","Uivatel hlásí neplatný odkaz");
+define("_IGNOREINFO","Ignorovat (Smazat vechny <b><i>poadavky</i></b> na daný odkaz)");
+define("_DELETEINFO","Smazat (Smazat <b><i>neplatný odkaz</i></b> a <b><i>poadavky</i></b> na daný odkaz)");
+define("_LINK","Odkaz");
+define("_LINKOWNER","Majitel");
+define("_IGNORE","Ignorovat");
+define("_USERMODREQUEST","Poadavek uivatele na zmìnu odkazu");
+define("_ORIGINAL","Originál");
+define("_PROPOSED","Návrh");
+define("_NOMODREQUESTS","Není ádný poadavek na úpravu");
+define("_SUBCATEGORY","Subkategorie");
+define("_OWNER","Majitel");
+define("_ACCEPT","Pøijmout");
+define("_ERRORTHECATEGORY","CHYBA: Kategorie");
+define("_ALREADYEXIST","ji existuje!");
+define("_ERRORTHESUBCATEGORY","CHYBA: Subkategorie");
+define("_EDITORIALADDED","Úvodník pøidán do databáze");
+define("_EDITORIALMODIFIED","Úvodník byl upraven");
+define("_EDITORIALREMOVED","Úvodník byl odstranìn z databáze");
+define("_LINKVALIDATION","Ovìøení odkazu");
+define("_CHECKALLLINKS","Vybrat vechny odkazy");
+define("_CHECKCATEGORIES","Vybrat kategorie");
+define("_INCLUDESUBCATEGORIES","(vloené subkategorie)");
+define("_VALIDATELINKS","Ovìøené odkazy");
+define("_FAILED","Selhalo!");
+define("_BEPATIENT","(bu?te prosím trpìlivý(á))");
+define("_VALIDATINGCAT","Ovìøování kategorie (a vech subkategorií)");
+define("_VALIDATINGSUBCAT","Ovìøování subkategorií");
+define("_ERRORURLEXIST","CHYBA: Tato URL je ji zaøazena do databáze!");
+define("_ERRORNOTITLE","CHYBA: Musíte zadat název pro URL!");
+define("_ERRORNOURL","CHYBA: Musíte zadat adresu!");
+define("_ERRORNODESCRIPTION","CHYBA: Musíte zadat popis!");
+define("_NEWLINKADDED","Nový odkaz pøidán do databáze");
+define("_YOURLINKAT","Vá odkaz na");
+define("_YOUCANBROWSEUS","Ná Archív odkazù mùete najít na:");
+define("_THANKS4YOURSUBMISSION","Díky za pøíspìvek!");
+define("_TEAM","tým.");
+define("_EZTRANSFERLINKS","Transfer all links from category");
+define("_EZTRANSFER","Transfer");
+define("_DELEZLINKCATWARNING","WARNING : Are you sure you want to delete this category ? You will delete all sub-categories and attached links as well !");
+define("_EZTHEREIS","There is");
+define("_EZSUBCAT","sub-categories");
+define("_EZATTACHEDTOCAT","under this category");
+define("_EZBROKENLINKS","Broken Links");
+define("_LINKAPPROVEDMSG", "Congratulations! The web link you submitted to our links database has been approved right now.");
+
+
+?>
