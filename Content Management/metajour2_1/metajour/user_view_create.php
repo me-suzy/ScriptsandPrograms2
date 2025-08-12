b94cc8745ffb0c3c66cbbfebb89fc5db8f67fdc1
me-suzy <me.suzana@gmail.com>
2025-08-12 05:59:14 +0300

    Add Content Management/metajour2_1/metajour/user_view_create.php

---
+++ Content Management/metajour2_1/metajour/user_view_create.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package METAjour
+ * @subpackage view
+ */
+
+require_once('basic_view_create.php');
+
+class user_view_create extends basic_view_create {
+
+	function customFields() {
+		$result .= '<div class="mformfieldset" style=""><div class="mformlabel" style="">'.$this->gl('_label_memberof').'</div><div class="mformfield" style="">';
+		$field = new basic_field($this);
+		$result .= $field->usergroupSelection(0,'__membership__');
+		$result .= '</div></div>';
+		return $result;
+	}
+
+	function webAccessSelection() {
+	}
+
+	function sysAccessSelection() {
+	}
+			
+}
+
+?>
