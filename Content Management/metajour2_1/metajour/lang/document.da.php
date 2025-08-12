708b6e25eb157472c6c5a9a5faf2dcba04bd483a
me-suzy <me.suzana@gmail.com>
2025-08-12 07:09:22 +0300

    Add Content Management/metajour2_1/metajour/lang/document.da.php

---
+++ Content Management/metajour2_1/metajour/lang/document.da.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['label_name'] = 'Beskrivelse af siden';
+$LANG['label_templateid'] = 'Skabelon';
+$LANG['label_stylesheetid'] = 'Typografi';
+$LANG['name'] = 'Dokumenter';
+$LANG['label_metadataid'] = 'Metadata';
+$LANG['label_alias'] = 'Undernavn';
+$LANG['label_structureid'] = 'Struktur';
+$LANG['label_nolist'] = 'Vis ikke i lister';
+$LANG['label_nosearch'] = 'Indgår ikke i søgning';
+
+?>
