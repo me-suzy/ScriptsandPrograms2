bdefa4002468b3941e4b8b647df0e77071653290
me-suzy <me.suzana@gmail.com>
2025-08-13 00:14:14 +0300

    Add Unsorted/12-all.v3.8.2.0.1/lt/links_inc.php

---
+++ Unsorted/12-all.v3.8.2.0.1/lt/links_inc.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_openBrWindow(theURL,winName,features) { //v2.0
+  window.open(theURL,winName,features);
+}
+//-->
+</script>
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<table width="100%" border="0" cellspacing="0" cellpadding="8">
+  <tr bgcolor="#F0F0F0">
+    <td width="150" height="30"><font size="2" face="Arial, Helvetica, sans-serif"><b><?PHP print $lang_410; ?> </b></font><font size="2"><font size="2" face="Arial, Helvetica, sans-serif"><a href="#h1"  onclick="MM_openBrWindow('http://www./support/12all_kb_min/index.php?page=index_v2&id=41&c=9','Help','scrollbars=yes,width=350,height=375')">[?]</a></font></font><font size="2" face="Arial, Helvetica, sans-serif"><b></b></font><font size="2" face="Arial, Helvetica, sans-serif"><b>
+      </b></font></td>
+    <td height="30"> <div align="left"><font size="2" face="Arial, Helvetica, sans-serif">
+        <input type="checkbox" name="links" value="yes" checked>
+        <?PHP print $lang_197; ?></font></div></td>
+  </tr>
+</table>
+</body>
+</html>
