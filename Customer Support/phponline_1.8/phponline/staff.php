87b4afec1c1ff747769446390768d7806f7d71c7
me-suzy <me.suzana@gmail.com>
2025-08-12 08:49:46 +0300

    Add Customer Support/phponline_1.8/phponline/staff.php

---
+++ Customer Support/phponline_1.8/phponline/staff.php
@@ -0,0 +1,16 @@
+<HTML>
+<HEAD>
+<meta http-equiv=Content-Type content="text/html;  charset=ISO-8859-1">
+<TITLE>phpOnline - Admin Area</TITLE>
+</HEAD>
+<BODY bgcolor="#FFFFFF">
+<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+ codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"
+ WIDTH="550" HEIGHT="370" id="as" ALIGN="">
+ <PARAM NAME=movie VALUE="as.swf"> <PARAM NAME=quality VALUE=High> 
+	<PARAM NAME=bgcolor VALUE=#ece9d8> 
+	<EMBED src="as.swf" quality=High bgcolor=#ece9d8 WIDTH="550" HEIGHT="370" NAME="as" ALIGN=""
+ TYPE="application/x-shockwave-flash" PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"></EMBED>
+</OBJECT>
+</BODY>
+</HTML>
