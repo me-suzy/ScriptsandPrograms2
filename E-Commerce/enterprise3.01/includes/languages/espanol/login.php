ca0f498716d43bab2a18722114e2453bad738202
me-suzy <me.suzana@gmail.com>
2025-08-12 11:57:06 +0300

    Add E-Commerce/enterprise3.01/includes/languages/espanol/login.php

---
+++ E-Commerce/enterprise3.01/includes/languages/espanol/login.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Entrar');
+define('HEADING_TITLE', 'Dejame Entrar!');
+
+define('HEADING_NEW_CUSTOMER', 'Nuevo Cliente');
+define('TEXT_NEW_CUSTOMER', 'Soy un nuevo cliente.');
+define('TEXT_NEW_CUSTOMER_INTRODUCTION', 'Al crear una cuenta en ' . STORE_NAME . ' podrá realizar sus compras rapidamente, revisar el estado de sus pedidos y consultar sus operaciones anteriores.');
+
+define('HEADING_RETURNING_CUSTOMER', 'Ya Soy Cliente');
+define('TEXT_RETURNING_CUSTOMER', 'He comprado otras veces.');
+
+define('TEXT_PASSWORD_FORGOTTEN', '&iquest;Ha olvidado su contrase&ntilde;a? Siga este enlace y se la enviamos.');
+
+define('TEXT_LOGIN_ERROR', 'Error: El E-Mail y/o Contrase&ntilde;a no figuran en nuestros datos.');
+define('TEXT_VISITORS_CART', '<font color="#ff0000"><b>Nota:</b></font> El contenido de su &quot;Cesta de Visitante&quot; ser&aacute; a&ntilde;adido a su &quot;Cesta de Asociado&quot; una vez que haya entrado. <a href="javascript:session_win();">[M&aacute;s Informaci&oacute;n]</a>');
+?>
