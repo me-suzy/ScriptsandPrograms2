740878aebdd2caddc85b01971b41073323b03bb2
me-suzy <me.suzana@gmail.com>
2025-08-12 18:31:52 +0300

    Add Image Galleries/TinyWebGallery13c/TinyWebGallery13/language/language_da.php

---
+++ Image Galleries/TinyWebGallery13c/TinyWebGallery13/language/language_da.php
@@ -0,0 +1,234 @@
+<?php
+/*************************
+  Copyright (c) 2004-2005 TinyWebGallery
+  written by Michael Dempfle
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  ********************************************
+  TWG version: 1.3c
+  $Date: 2005/11/15 09:02 $
+**********************************************/
+// language file - version 1.3 - if you translate a langauge file where already some parts are translated: Please don't remove the former translator completely - insert him/her behind the lang_translator as a comment
+$lang_translator=" - Overs&aelig;tter: <a href=\"http://jenshenrik.com\" target=\"_blank\"><span class=\"twglink\">Jens Henrik</span></a>";
+$lang_optionen="Indstillinger";
+$lang_menu_titel="Titel";
+$lang_kommentar="Kommentar";
+
+$lang_titel="";
+$lang_titel_no="Ingen billeder i galleriet endnu.";
+$lang_select_gallery = "V&aelig;lg venligst et galleri.";
+$lang_help="Hj&aelig;lp";
+$lang_new_window="Nyt Vindue";
+$lang_login="Log Ind";
+$lang_logout="Log Ud";
+$lang_visitor="Bes&oslash;gende:";
+$lang_total="Totalt:";
+$lang_today="I dag:";
+$lang_galleries="Gallerier";
+$lang_picture="Billede";
+$lang_pictures="Billeder";
+$lang_of="/";
+
+$lang_overview="Til oversigten";
+$lang_forward="N&aelig;ste";
+$lang_back="Tilbage";
+$lang_rotate_left="Rot&eacute;r billedet mod venstre";
+$lang_rotate_right="Rot&eacute;r billedet mod h&oslash;jre";
+$lang_start_slideshow="Start diasshow";
+$lang_stop_slideshow="Stop diasshow";
+$lang_thumb_forward = "&gt;"; // = '>'
+$lang_thumb_back="&lt;"; // = '<'
+$lang_views=" gange";
+
+$lang_login_php_enter="Indtast venligst adgangskoden for at rette titler<br />og slette kommentarer.";
+$lang_login_php_enter_again="Forkert adgangskode<br />Pr&oslash;v venligst igen:";
+
+$lang_kommenar_php_both_fields="Udfyld venligst begge felter.<br />";
+$lang_kommenar_php_enter_name="Indtast venligst et navn<br />(maks. 30 tegn):";
+$lang_kommenar_php_enter_comment="Indtast venligst en kommentar<br />(maks. 150 karakterer):";
+$lang_kommenar_php_name="Navn:";
+$lang_kommenar_php_comment="Kommentar (maks. 150 karakterer):";
+$lang_kommenar_php_speichern="Gem";
+
+$lang_privatelogin_php_wrong_password="Forkert adgangskode<br />Pr&oslash;v venligst igen:";
+$lang_privatelogin_php_password="Indtast venligst Adgangskoden for galleriet<br />'%s':";
+$lang_privatelogin_php_login="Log Ind";
+
+$lang_titel_php_titel="Indtast venligst en titel<br />(maks. 150 karakterer):";
+$lang_titel_php_save="Gem";
+
+$lang_slideshowid_php_loading="Diasshow indl&aelig;ses ...";
+
+$lang_opionen_php_yes="Ja";
+$lang_opionen_php_no="Nej";
+$lang_opionen_php_new_window="&Aring;bn Galleriet i nyt vindue";
+$lang_opionen_php_opt_slideshow="Optimeret diasshow med overgange";
+$lang_opionen_php_slideshowintervall="Diasshow interval (sek.)";
+$lang_opionen_php_ok="OK";
+$lang_opionen_php_top_nav="Unlad avanceret visning ";
+
+$lang_back_topx="Tilbage&nbsp;til&nbsp;&nbsp;Top&nbsp;%s";
+$lang_topx="Top&nbsp;%s";
+$lang_main_topx="Tilbage til galleriet";
+
+// new in 1.1
+$lang_loading="indl&aelig;ser...";
+$lang_dhtml_navigation="Brug DHTML navigation";
+$lang_optionen_slideshow="Diasshow type";
+$lang_optionen_slideshow_fullscreen="Maksimeret";
+$lang_optionen_slideshow_optimized="Overlappende";
+$lang_optionen_slideshow_normal="Normal";
+$lang_lowbandwidth= "TinyWebGallery er optimeret for langsomme forbindelser.";
+$lang_highbandwidth="TinyWebGallery er optimeret for hurtige forbindelser.";
+// new in 1.2
+$lang_email_menu_user="E-mail nyhedsbrev";
+$lang_email_menu_admin="Send e-mail";
+// Sorrymessage for failed subscription
+$lang_email_sorrysignmessage = "Beklager! E-mail adressen %s er allerede registreret.";
+// Sorrymessage for blank email
+$lang_email_sorryblankmailmessage = "Indtast venligst e-mail adresse.";
+// Sorrymessage for invalid emails
+$lang_email_sorryoddmailmessage = "Beklager! Den indtastede e-mail adresse<br/> \"%s\" ser ikke ud til at eksistere.";
+// Sorrymessage if someone entered your own mail
+$lang_email_sorryownmailmessage = "Nej tak! Jeg vil helst ikke modtage nyhedsbreve.";
+// Title of the newsletter, will be displayed in the FROM field of the mailclient
+$lang_email_subscribemail_subject = "Mail fra billedgalleriet";
+// Subscribemessage, will be shown when someone subscribes.
+$lang_email_subscribemessage = "Tak for registreringen. Der er afsendt en bekr&aelig;ftigelse til e-mail adressen.";
+// Subscribemail, will be sent when someone subscribes.
+$lang_email_subscribemail = "Der er tilmeldt registrerering for modtagelse af nyhedsbreve til denne e-mail adresse: %s";
+// Unsubscribemessage for deletion, will be followed by the email!
+$lang_email_unsubscribemessage = "E-mail adressen '%s' er fjernet.<br/>Du kan registrere senere";
+// Unsubscribemessage for failed deletion, will be followed by the email!
+$lang_email_failedunsubscriptionmessage = "E-mail %s blev ikke fundet.";
+// the welcometext
+$lang_email_welcomemessage = "Tilmeld dig nyhedsbrev.<br/><br/>Nyhedsbreve udsendes, n&aring;r nye billeder er tilf&oslash;jet.<br/><br/>Tilmeldingen kan <u>altid</u> annulleres";
+// email could not be sent.
+$lang_email_error_send_mail = "Bekr&aelig;ftigelses e-mailen kunne ikke sendes.<br/>E-mail serveren er m&aring;ske ikke korrekt konfigureret. Kontakt venligst administratoren.";
+
+$lang_email_add="Tilf&oslash;j";
+$lang_email_remove="Fjern";
+$lang_email_send="Send";
+
+$lang_email_admin_welcomemessage_send = 'Informer alle registrerede brugere<br/>(%s) om &aelig;ndringer.';
+$lang_email_admin_sorryoddsendermailmessage = "Beklager! Den indtastede afsender-adresse<br/> \"%s\" ser ikke ud til at eksistere.";
+$lang_email_admin_sendermail = "Afsender-adresse (din e-mail)";
+$lang_email_admin_subject="Emne";
+$lang_email_admin_message="Besked";
+$lang_email_admin_sendbutton="Send";
+$lang_email_admin_sent = "F&oslash;lgende besked er afsendt:";
+$lang_email_admin_from = "Fra";
+$lang_email_admin_notloggedin="Du er ikke logget ind. G&aring; venligst til forsiden og log ind korrekt.";
+
+$lang_visitor_30="30 dages historik";
+
+$lang_login_php_enter="Indtast venligst adgangskoden for<br/>at aktivere de udvidede funktioner.";
+$lang_opionen_php_zoom="Maksimeret visning";
+$lang_administration="Administration";
+$lang_opionen_php_zoom_message="Bem&aelig;rk: \'Maksimeret visning\' er langsommere end normal visning,\\nfordi billederne er genereret \'On-The-Fly\'.\\nBillederne er ligeledes st&oslash;rre, og tager derfor l&aelig;ngere tid at overf&oslash;re.\\nDenne mulighed b&oslash;r kun benyttes ved en hurtig internetforbindelse!";
+$lang_first="F&oslash;rste";
+$lang_last="Sidste";
+$lang_no_session="Sessionen er ikke tilg&aelig;ngelig. Login til private gallerier er ikke tilladt.";
+$lang_not_loggedin = "Du er ikke logget ind. G&aring; venligst til forsiden og log ind korrekt.";
+$lang_no_topx_images = "Der kunne ikke findes nogle billeder. <br / >Beskyttede gallerier kan kun inkluderes hvis du l&aring;ser dem op f&oslash;rst.";
+
+$lang_tips_overview = array('Tip: Klik p&aring; hj&aelig;lp-linket, hvis du vil vide mere om betjeningen af galleriet.');
+$lang_tips_thumb = array('Tip: Naviger i &oslash;verste venstre hj&oslash;rne for at g&aring; til det &oslash;vre niveau.','Tip: Top 13 p&aring; denne side viser KUN de mest bes&oslash;gte i denne mappe.');
+$lang_tips_image = array('Tip: Du kan navigere frem og tilbage mellem billederne med pilene.','Tip: Hvis du logger ind (i &slash;verste h&oslash;jre hj&oslash;rne) kan du &aelig;ndre overskrifter, slette kommentarer og rotere billederne permanent.','Tip: Du kan benytte smilies som :) eller ;) i titler og kommentarer.');
+
+// new 1.3
+$lang_dl_as_zip1 = "Du kan downloade alle billederne i mappen som et samlet arkiv.<br />Vil du downloade hele mappen eller forts&aelig;tte til det aktuelle billede?";
+$lang_dl_as_zip2 = "Aktuelt billede";
+$lang_dl_as_zip3 = "Alle";
+$lang_dl_as_zip4 = "Sp&oslash;rg ikke igen i denne session."; // we store this is the session
+
+$lang_download_counter = "Downloads";
+$lang_download_counter_short = "dl";
+// not used in 1.3 yet!
+$lang_email_subscribemail_2_subject = "Bekr&aelig;ft venligst registrering af TinyWebGallery";
+$lang_email_subscribemail_2 = "K&aelig;re TinyWebGallery bruger:\n\nDu har tilmeld dig modtagelse af TinyWebGallery nyhedsbrev. For at fuldende registreringen: Klik p&aring; nedenst&aring;ende link:\n%s\n\nHilsen TinyWebGallery";
+// --
+$lang_rating = "Stem";
+$lang_rating1 = "1";
+$lang_rating2 = "2";
+$lang_rating3 = "3";
+$lang_rating4 = "4";
+$lang_rating5 = "5";
+$lang_rating_button = "Bed&oslash;m";
+$lang_rating_text = "Bed&oslash;m dette billede:";
+$lang_rating_new="Ny \"stemme\": ";
+$lang_rating_vote="Stemmer";
+
+$lang_rating_security="Indtast de 4 karakterer<br />af sikkerhedskoden [A-F,0-9]:";
+$lang_rating_help="Klik p&aring; billedet,<br />hvis du ikke kan se koden.";
+$lang_rating_message1="Du har allerede stemt.";
+$lang_rating_message2="luk venligst vinduet.";
+$lang_rating_message3="Tak for stemmen.";
+$lang_rating_message4="Du har indtastet en forkert kode.";
+$lang_rating_send="Send";
+
+$lang_fileinfo="Info";
+$lang_fileinfo_name = "Filnavn";
+$lang_fileinfo_date = "Dato";
+$lang_fileinfo_size = "St&oslash;rrelse";
+$lang_fileinfo_resolution = "Opl&oslash;sning";
+$lang_fileinfo_views = "Viste";
+$lang_fileinfo_dl = "Downloads";
+$lang_fileinfo_rating = "Placering";
+$lang_fileinfo_not_available = " - (remote)";
+$lang_exif_not_available = " - ";
+// please check the constants in the file inc/exifReader.inc.php for exif entries you can try - not all are available on all cameras! sometimes the constants don't work - try to google for the real exif field name!
+$lang_exif_info = array("Camera Model" => "model", "Exif Date" => "DateTime", "Focal Length" => 
+"focalLength", "f/Stop" => "fnumber", "Shutter Speed" => "exposureTime", "ISO" => "isoEquiv");
+
+$lang_comments = "Kommentarer";
+$lang_last_comments="Nyeste kommentarer";
+$lang_show_kommentar="Vis kommentarer";
+$lang_add_kommentar="Tilf&oslash;j kommentar";
+$lang_close_fullscreen="Tilbage til normal visning";
+
+$lang_search_results="S&oslash;geresultater";
+$lang_search_back="Tilbage til s&oslash;geresultaterne.";
+$lang_search="S&oslash;g";
+$lang_search_text="Indtast venligst s&oslash;getekst:";
+$lang_search_button="S&oslash;g";
+$lang_search_where="S&oslash;g i";
+$lang_search_max="Viste hits pr. side";
+$lang_search_all="Alle";
+$lang_search_hits="Fundet %s hits.";
+
+// height of the i_frames - please check if the settings match for your language!!
+$lang_height_comment = 150;
+$lang_height_caption = 90;
+$lang_height_login = 80;
+$lang_height_private = 80;
+$lang_height_options = 205;
+if (!$show_new_window) { $lang_height_options -= 30; }
+if (!$show_slideshow)  { $lang_height_options -= 60; }
+if (!$enable_maximized_view)  { $lang_height_options -= 30; }
+$lang_height_email_user = 180;
+$lang_height_email_admin = 280;
+$lang_height_info = 150;
+if (!$show_download_counter || !$enable_download_counter) { $lang_height_info -= 17; }
+if (!$show_count_views) { $lang_height_info -= 17; }
+if (!$show_image_rating) { $lang_height_info -= 17; }
+if ($show_exif_info) { $lang_height_info += (count($lang_exif_info) * 17);}
+$lang_height_rating = 150;
+$lang_height_dl_manager=150;
+$lang_height_search=175;
+$lang_xpos_lang_dropdown="200"; // this is only needed if you include twg and ie does not position the language dropdown correct because you include twg in several divs - in a fixed layout you can adjust this here - if you don't have a fix layout don't use the dropdown - use the normal setting!
+
+/*
+##  Danish special chars ##
+æ = &aelig;
+Æ = &AElig;
+ø = &oslash;
+Ø = &Oslash;
+&aring; = &aring;
+&Aring; = &Aring;
+
+*/
+?>
