c78c6aeb43388b69cb666d5c74af736e94fdceb5
me-suzy <me.suzana@gmail.com>
2025-08-12 07:08:31 +0300

    Add Content Management/metajour2_1/metajour/lang/basic_view_publish.en.php

---
+++ Content Management/metajour2_1/metajour/lang/basic_view_publish.en.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package METAjour
+ * @subpackage lang
+ */
+
+$LANG['text_title'] = 'Publishing';
+$LANG['text_1'] = 'Time of publication:';
+$LANG['text_2'] = 'Time of expiration:';
+?>
