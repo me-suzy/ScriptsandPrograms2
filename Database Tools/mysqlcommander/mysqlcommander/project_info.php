bf60cf58df288eaf8bc26981e9fb9643c36523d4
me-suzy <me.suzana@gmail.com>
2025-08-12 08:58:27 +0300

    Add Database Tools/mysqlcommander/mysqlcommander/project_info.php

---
+++ Database Tools/mysqlcommander/mysqlcommander/project_info.php
@@ -0,0 +1,9 @@
+project_name;MySQL Commander
+version;2.65
+release_date;2005-08-05
+
+author_name;Oliver Kührig
+author_email;oliver@bitesser.de
+
+homepage_text;bitesser
+homepage_link;http://www.bitesser.de
