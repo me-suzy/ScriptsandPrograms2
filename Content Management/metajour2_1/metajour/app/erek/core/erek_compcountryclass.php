b9309039f8bba30e784ddb9dab29fecdba6984db
me-suzy <me.suzana@gmail.com>
2025-08-12 06:01:08 +0300

    Add Content Management/metajour2_1/metajour/app/erek/core/erek_compcountryclass.php

---
+++ Content Management/metajour2_1/metajour/app/erek/core/erek_compcountryclass.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Jan H. Andersen <jha@ipwsystems.dk>
+ * @author Martin R. Larsen <mrl@ipwsystems.dk>
+ * @copyright {@link http://www.ipwsystems.dk/ IPW Systems a.s}
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @package eReklamation
+ * @subpackage core
+ * $Id: erek_compcountryclass.php,v 1.3 2005/02/15 12:22:25 jan Exp $
+ */
+
+global $system_path;
+require_once($system_path.'core/basicclass.php');
+
+class erek_compcountry extends basic {
+
+	function erek_compcountry() {
+		$this->basic();
+		$this->setobjecttype('compcountry');
+		$this->addcolumn('name',0,UI_STRING);
+		$this->removeview('createvariant');
+		$this->removeview('createfuture');
+		$this->removeview('approvepublish');
+		$this->removeview('requestapproval');
+		$this->removeview('access');
+		$this->removeview('category');
+	}
+
+}
