f0fe3ebb05c6b68eccbd3ef51283267685ff37b9
me-suzy <me.suzana@gmail.com>
2025-08-12 03:58:45 +0300

    Add Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_scripts.php

---
+++ Content Management/iziContentsRC6/iziContentsRC6/iziContents1/languages/fr/lang_scripts.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Form Titles
+$GLOBALS["tFormTitle"] = 'maintenir les scripts';
+$GLOBALS["tFormTitle1"] = 'télécharger scripte/créer scripte sous-directorie';
+$GLOBALS["tFormTitle2"] = 'Télécharger scripte';
+$GLOBALS["tFormTitle3"] = 'Créer Sous-Directorie';
+
+//  List Functions
+$GLOBALS["tAddNewScript"] = 'Ajouter nouveau scripte';
+$GLOBALS["tDeleteScript"] = 'Supprimer scripte';
+$GLOBALS["tDeleteScriptDir"] = 'Supprimer scripte sous-directorie';
+
+//  List Headings
+$GLOBALS["tScriptName"] = 'Nom du scripte';
+
+//  Form Fields
+$GLOBALS["tScriptFilename"] = 'Nom de fichier du scripted';
+$GLOBALS["tSubdirectoryname"] = 'Nom du sous-directorie ';
+
+//  Form Buttons
+$GLOBALS["bUpload"] = 'Télécharger!';
+$GLOBALS["bCreateDir"] = 'Créer!';
+
+//  Error Messages
+$GLOBALS["eNoFile"] = 'Vous avez besoin de choisir un fichier pour télécharger.';
+$GLOBALS["eNoDir"] = 'Vous avez besoin d\'entrer un nom de directorie.';
+$GLOBALS["eDirAlreadyExists"] = 'sous-directorie existe déjà';
+$GLOBALS["eZeroByteFile"] = 'Le fichier est 0 octets dans la longueur';
+$GLOBALS["eNoFileUpload"] = 'Fichier ne pourrait pas être téléchargé avec succès.';
+$GLOBALS["eInvalidFileSize"] = 'Le fichier est trop grand pour télécharger';
+$GLOBALS["eInvalidFileType"] = 'Le fichier n\'est pas un fichier de manuscrit valide';
+$GLOBALS["eInvalidMimeType"] = 'Le type de MIME de Fichier n\'est pas un type de manuscrit valide';
+
+?>
