0831c6c74b79e91a11f54f4438dc912f55be0ccd
me-suzy <me.suzana@gmail.com>
2025-08-12 18:02:40 +0300

    Add Image Galleries/PHP_JK_Aug_01_2005/Admin/index.php

---
+++ Image Galleries/PHP_JK_Aug_01_2005/Admin/index.php
@@ -0,0 +1,6 @@
+<?php
+	Require("../Includes/i_Includes.php");
+	Require("../Includes/Nav/PHP_JK_ADMIN_OPEN.php");
+	Require("../Includes/Nav/PHP_JK_ADMIN_DEFAULT.php");
+	Require("../Includes/Nav/PHP_JK_ADMIN_CLOSE.php");
+?>
