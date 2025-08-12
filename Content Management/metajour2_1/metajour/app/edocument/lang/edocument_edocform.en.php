70cbbad053e1bee97006ef0cb0a7deee63b12e53
me-suzy <me.suzana@gmail.com>
2025-08-12 05:59:47 +0300

    Add Content Management/metajour2_1/metajour/app/edocument/lang/edocument_edocform.en.php

---
+++ Content Management/metajour2_1/metajour/app/edocument/lang/edocument_edocform.en.php
@@ -0,0 +1,20 @@
+<?php
+$LANG['name'] = 'Error report';
+$LANG['label_errorcodeid'] = 'Error code';
+$LANG['label_correctionid'] = 'Action';
+$LANG['label_responsibleid'] = 'Responsibility';
+$LANG['label_name'] = 'Customer name';
+$LANG['label_cno'] = 'Customer no.';
+$LANG['label_cname'] = 'Contact';
+$LANG['label_caddress'] = 'Address';
+$LANG['label_cpostalcode'] = 'Postal';
+$LANG['label_ccity'] = 'City';
+$LANG['label_ccontact'] = 'Country';
+$LANG['label_cphone'] = 'Phone';
+$LANG['label_cemail'] = 'Email';
+$LANG['label_comment1'] = 'I did this';
+$LANG['label_comment2'] = 'Supplemental comments';
+$LANG['label_comment3'] = 'Correctional actions';
+$LANG['label_ccountryid'] = 'Country';
+$LANG['label_status'] = 'Status code';
+?>
