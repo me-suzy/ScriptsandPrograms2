2f13d1004e4c23f5b0ec1c40d48264f4c697d216
me-suzy <me.suzana@gmail.com>
2025-08-12 23:35:35 +0300

    Add Redirection/udiyoudirectit/index.php

---
+++ Redirection/udiyoudirectit/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// uDi - You Direct It, written by, and copyright Mike Cheesman.
+
+require "config.php";
+include "$header"; ?>
+uDi - You Direct It, allows you to offer <i><b>come.to</b></i> style redirection on <i>your</i> website.  It is simple to install, and simple to administer.  Simple to signup and edit accounts.  Allow your users to signup for <i><b>yourdomain.com/them</b></i>, and help them cover up those ugly free host URL's.<P><P>Enable easy to remember URL's, help change this:<br><i><b>http://server.uglyhostname.com/section/user</b></i><br>into this:<br><i><b>http://yourdomain.com/user</b></i><P>
+<?php include "$footer"; ?>
