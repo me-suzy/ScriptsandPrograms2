b5e6cb81cbadabfa6045a85c77c6b4f3e530819d
me-suzy <me.suzana@gmail.com>
2025-08-12 17:17:38 +0300

    Add Groupware Tools/phprojekt/layout/neptun/neptun_css_ie.php

---
+++ Groupware Tools/phprojekt/layout/neptun/neptun_css_ie.php
@@ -0,0 +1,92 @@
+<?php
+
+
+
+header("Content-type: text/css");
+
+
+
+?>
+
+/**
+
+ * PHProjekt CSS-File
+
+ *
+
+ * $Id: neptun_css_ie.php,v 1.4 2005/07/20 12:14:27 paolo Exp $
+
+ *
+
+ */
+
+
+
+.navbutton,
+
+a.navbutton,
+
+a.navbutton:hover,
+
+a.navbutton:visited {
+
+    position: relative;
+
+    top: 1px;
+
+    left: 3px;
+
+    padding-top: 1px;
+
+    padding-left: 20px;
+
+    padding-right :20px;
+
+    border-width: 2px;
+
+    color: #ffffff;
+
+    font-size: 1em;
+
+    font-weight:bold;
+
+    border-style:solid;
+
+    cursor: pointer;
+
+    width: auto;
+
+    text-decoration: none;
+
+}
+
+div.navImage {
+
+    display:inline;
+
+}
+
+html, body {
+
+   margin: 0;
+
+   overflow-y: hidden;
+
+   height: 100%;
+
+}
+
+div.navi {
+
+    position:absolute;
+
+}
+
+div.outer_content {
+
+   height: 100%;
+
+   overflow: auto;
+
+}
+
