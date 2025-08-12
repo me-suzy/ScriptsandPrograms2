d3a6a5660591ccf10a11207b1bb8f6bcaee99db0
me-suzy <me.suzana@gmail.com>
2025-08-12 10:08:00 +0300

    Add Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/includes/skinMenuPart.php

---
+++ Development Tools/phpPeanuts_1_1/phpPeanuts_1_1/includes/skinMenuPart.php
@@ -0,0 +1,5 @@
+<a href=../../site/index.php target=_blank>phpPeanuts home</a><BR>
+<a href=../pntUnit/index.php target=_blank>pnt/unit</a><BR>
+<a href=../emptyapp/index.php>Empty application</a><BR>
+<?php $this->printSubmenu('emptyapp') ?>
+&nbsp;
