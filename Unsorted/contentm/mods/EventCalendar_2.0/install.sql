9d354cd3d1b83abac76df8cb9ec0f76d8a7465db
me-suzy <me.suzana@gmail.com>
2025-08-13 02:09:17 +0300

    Add Unsorted/contentm/mods/EventCalendar_2.0/install.sql

---
+++ Unsorted/contentm/mods/EventCalendar_2.0/install.sql
@@ -0,0 +1,1 @@
+CREATE TABLE mod_eventcalendar ( id varchar(50) default NULL, date_mm int(2) default NULL, date_dd int(2) default NULL, date_yyyy int(4) default NULL, title varchar(50) default NULL, description text) TYPE=MyISAM;
