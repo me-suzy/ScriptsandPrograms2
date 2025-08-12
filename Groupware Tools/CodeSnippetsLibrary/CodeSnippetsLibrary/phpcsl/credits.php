c850000c92e9f54096341da63df5f6afeb681352
me-suzy <me.suzana@gmail.com>
2025-08-12 16:42:58 +0300

    Add Groupware Tools/CodeSnippetsLibrary/CodeSnippetsLibrary/phpcsl/credits.php

---
+++ Groupware Tools/CodeSnippetsLibrary/CodeSnippetsLibrary/phpcsl/credits.php
@@ -0,0 +1,49 @@
+<?
+/*
+* Project:      PHP-CSL (PHP Code Snippet Library)
+* Version:      0.9
+* Date:         2005/02/09 (y/m/d)
+* Author:       Stuart Cochrane
+* URL:          http://www.php-csl.com
+* Sourceforge:  http://www.sourceforge.net/projects/php-csl/
+*
+* Read: reame.txt, install.txt, license.txt
+*
+*
+*
+*
+*
+*/
+
+if(isset($_GET['op'])) {
+?>
+<br><br>
+<h1>Design Credits</h1>
+<ul>
+	<li>Design: PopScript.com</li>
+	<li>Date: 2005-10-18</li>
+	<li>URL: <a href="http://www.popscript.com">www.popscript.com</a></li>
+	<li>License: Non-Transferable Copyrights</li>
+</ul>
+<center><Img Src="images/JamesCSteinSr.jpg"></center>
+<br><br>
+<h1>Coding Credits</h1>
+<ul>
+	<li>Author: Stuart Cochrane</li>
+	<li>Date: 2005-02-09</li>
+	<li>URL: www.php-csl.com</li>
+	<li>License: GPL</li>
+</ul>
+<br>
+<h1>Other Credits</h1>
+<ul>
+	<li>Thanks to Sourceforge.net.</li>
+	<li>Thanks to all bug reporters over the years.</li>
+	<li>Thanks to the thousands of PHP-CSL users worldwide.</li>
+</ul>
+
+<?
+} else {
+	echo "Oppps... you should not see this message, this page should not be accessed directly !";
+}
+?>
