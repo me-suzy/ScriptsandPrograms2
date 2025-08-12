105fecd76c1f85bd8546a576728c217b63cbdefa
me-suzy <me.suzana@gmail.com>
2025-08-12 23:15:48 +0300

    Add Portal Systems/PortailPHP-v1.4/portailphp/include/lang-dutch.php

---
+++ Portal Systems/PortailPHP-v1.4/portailphp/include/lang-dutch.php
@@ -0,0 +1,51 @@
+<?php
+/*
+Copyright (C) 2002 CLAIRE Cédric cedric.claire@safari-msi.com www.portailphp.com
+Ce programme est un logiciel libre ; vous pouvez le redistribuer et/ou le modifier conformément aux dispositions de la Licence Publique Générale GNU, telle que publiée par la Free Software Foundation ; version 2 de la licence, ou encore (à votre choix) toute version ultérieure.
+Ce programme est distribué dans l'espoir qu'il sera utile, mais SANS AUCUNE GARANTIE ; sans même la garantie implicite de COMMERCIALISATION ou D'ADAPTATION A UN OBJET PARTICULIER. Pour plus de détail, voir la Licence Publique Générale GNU .
+Vous devez avoir reçu un exemplaire de la Licence Publique Générale GNU en même temps que ce programme ; si ce n'est pas le cas, écrivez à la Free Software Foundation Inc., 675 Mass Ave, Cambridge, MA 02139, Etats-Unis.
+Portail PHP
+La présente Licence Publique Générale n'autorise pas le concessionnaire à incorporer son programme dans des programmes propriétaires. Si votre programme est une bibliothèque de sous-programmes, vous pouvez considérer comme plus intéressant d'autoriser une édition de liens des applications propriétaires avec la bibliothèque. Si c'est ce que vous souhaitez, vous devrez utiliser non pas la présente licence, mais la Licence Publique Générale pour Bibliothèques GNU.
+*/
+
+// LIBELLE DES RUBRIQUES
+$Lib_Rub_Navig="<font size=4>N</font>AVIGATIE";
+$Lib_Rub_ForumLive="<font size=4>F</font>ORUM <font size=4>L</font>IVE";
+$Lib_Rub_WebRing="<font size=4>W</font>EBRING";
+$Lib_Rub_Param="<font size=4>P</font>ARAMETERS";
+$Lib_Rub_Parten="<font size=4>P</font>ARTNES";
+$Lib_Rub_Pinup="<font size=4>P</font>INUP";
+$Lib_Rub_News="<font size=4>N</font>EWS";
+
+// LES RUBRIQUES
+$Rub_Home="Start";
+$Rub_Membres="Admin";
+$Rub_Navigation="Navigatie";
+$Rub_News="Artikels";
+$Rub_Search="Zoeken";
+$Rub_File="Downloads";
+$Rub_Liens="Links";
+$Rub_Faq="FAQ";
+$Rub_Forum="Forum";
+$Rub_Photos="Foto's";
+
+//LES SOUS RUBRIQUES
+$SsRub_News_plusrecents="Meest recent";
+$SsRub_News_pluslus="Meest gelezen";
+$SsRub_File_Proposer="Suggestie";
+$SsRub_File_plusrecents="Meest recent";
+$SsRub_File_plusclics="Top 10";
+$SsRub_Liens_Proposer="Voorstel";
+$SsRub_Liens_plusrecents="Meest recent";
+$SsRub_Liens_top="Top 10";
+
+//MESSAGES D'ERREURS
+$Err_Serveur="Onmogelijk om met server te verbinden";
+$Err_Db="Onmogelijk om verbinding te maken met de database";
+$Err_Modif="Wijziging onmogelijk";
+$Err_Search="Zoeken niet mogelijk";
+$Err_Insert="Invoegen niet mogelijk";
+$Err_Supp="Verwijderen niet mogelijk";
+$Err_Upload_Chmod="Rechten veranderen niet mogelijk";
+$Err_Upload_Move="Schrijven naar server niet mogelijk";
+?>
