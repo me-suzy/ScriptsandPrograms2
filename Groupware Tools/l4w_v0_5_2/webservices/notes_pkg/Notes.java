14137841c8612cb28feddfb91fcb5ac84739d7f7
me-suzy <me.suzana@gmail.com>
2025-08-12 17:13:14 +0300

    Add Groupware Tools/l4w_v0_5_2/webservices/notes_pkg/Notes.java

---
+++ Groupware Tools/l4w_v0_5_2/webservices/notes_pkg/Notes.java
@@ -0,0 +1,16 @@
+/**
+ * Notes.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.2RC2 Nov 16, 2004 (12:19:44 EST) WSDL2Java emitter.
+ */
+
+package notes_pkg;
+
+public interface Notes extends javax.xml.rpc.Service {
+    public java.lang.String getnotesPortAddress();
+
+    public notes_pkg.NotesPortType getnotesPort() throws javax.xml.rpc.ServiceException;
+
+    public notes_pkg.NotesPortType getnotesPort(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
