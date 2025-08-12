196605c8ea5e2b4e4f8442d8565dd20fb7aaf6b1
me-suzy <me.suzana@gmail.com>
2025-08-12 16:49:34 +0300

    Add Groupware Tools/l4w_v0_5_2/division.php

---
+++ Groupware Tools/l4w_v0_5_2/division.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<head>
+	<title>
+	</title>
+	<link REL="SHORTCUT ICON"		HREF="http://www.evandor.com/icon.ico">
+	<meta http-equiv="content-type" content="text/html;charset=iso-8859-1">
+	<meta name="copyright"			content="evandor media GmbH">
+	<meta name="author" 			content="Carsten Gräf">
+	<meta name="publisher"			content="evandor media GmbH">
+	<meta http-equiv="expires" content="0">
+
+	<link rel='stylesheet' type='text/css' href='../../css/eclipse/left.css'>
+</head>
+
+<body background='../../img/eclipse/background_left.jpg' bgcolor='#bfbfbf' cellspacing=0 cellpadding=0>
+</body>
+</html>
