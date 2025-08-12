e40fd52ee2ca22e97743153c5afde23396727cc2
me-suzy <me.suzana@gmail.com>
2025-08-12 10:39:07 +0300

    Add Discussion Boards/forum/forum/style.php

---
+++ Discussion Boards/forum/forum/style.php
@@ -0,0 +1,73 @@
+<?php
+
+echo ("<style type=\"text/css\">
+
+.bordercolor
+{
+	background-color: $tborder_color;
+}
+
+.height
+{
+	height: 30px;
+}
+
+.background
+{
+	background-image: url('$skins/table/title_bg.gif');
+}
+
+.backgroundtm
+{
+	background-image: url('$skins/table/tm.gif');
+}
+
+.backgroundbm
+{
+	background-image: url('$skins/table/bm.gif');
+}
+.backgroundtm2
+{
+	background-image: url('$skins/table/tm2.gif');
+}
+
+.backgroundbm2
+{
+	background-image: url('$skins/table/bm2.gif');
+}
+
+.subnav1
+{
+	background-image: url('$skins/other/subnav_top.jpg');
+	height: 31px;
+}
+.subnav2
+{
+	background-image: url('$skins/other/subnav_bottom.jpg');
+	height: 31px;
+}
+
+
+input {
+
+border: 1px solid #0000BB;
+font-family: Verdana;
+text-decoration: none;
+background-color: $tbackground1;
+color: #339933;
+
+}
+
+textarea {
+
+border: 1px solid #0000BB;
+font-family: Verdana;
+font-weight: bold;
+background-color: $tbackground1;
+color: #339933;
+
+}
+
+</style>");
+
+?>
