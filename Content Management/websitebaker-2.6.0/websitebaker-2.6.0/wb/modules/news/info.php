2deb53ee71bc49110950a677c5e698c91288170b
me-suzy <me.suzana@gmail.com>
2025-08-12 08:18:13 +0300

    Add Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/modules/news/info.php

---
+++ Content Management/websitebaker-2.6.0/websitebaker-2.6.0/wb/modules/news/info.php
@@ -0,0 +1,35 @@
+<?php
+
+// $Id: info.php 136 2005-09-18 23:42:34Z ryan $
+
+/*
+
+ Website Baker Project <http://www.websitebaker.org/>
+ Copyright (C) 2004-2005, Ryan Djurovich
+
+ Website Baker is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Website Baker is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Website Baker; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+$module_directory = 'news';
+$module_name = 'News';
+$module_function = 'page';
+$module_version = '2.6';
+$module_platform = '2.6.x';
+$module_author = 'Ryan Djurovich';
+$module_license = 'GNU General Public License';
+$module_description = 'This page type is designed for making a news page.';
+
+?>
