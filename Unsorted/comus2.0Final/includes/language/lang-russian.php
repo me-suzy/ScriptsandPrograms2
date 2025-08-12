165500d51f3b624a8e50ecf4b4c35dc02c7a8c66
me-suzy <me.suzana@gmail.com>
2025-08-13 01:51:33 +0300

    Add Unsorted/comus2.0Final/includes/language/lang-russian.php

---
+++ Unsorted/comus2.0Final/includes/language/lang-russian.php
@@ -0,0 +1,264 @@
+<?
+
+define('GTGP_NO','Íåò');
+define('GTGP_YES','Äà');
+define('GTGP_NO1','Íåò');
+define('GTGP_YES1','Äà');
+define('GTGP_ACCESS','Ãåíåðèðîâàòü');
+define('GTGP_USER','Ïîëüçîâàòåëü');
+define('GTGP_PASS','Ïàðîëü');
+define('GTGP_SUBMIT','Îòïðàâèòü');
+define('GTGP_ADMIN_PAGE','Ñòðàíèöà àäìèíèñòðàòîðà');
+define('GTGP_ADMIN_POST','Îòïðàâèòü');
+define('GTGP_ADMIN_POST_OPIS','Ïðèíÿòü/Îòêëîíèòü ãàëåðåè');
+define('GTGP_ADMIN_PREF','Ïàðòí¸ðû');
+define('GTGP_ADMIN_PREF_OPIS','Äîáàâèòü/Óäàëèòü ïðèâèëåãèðîâàííûõ âåáìàñòåðîâ');
+define('GTGP_ADMIN_BL','×¸ðíûé ñïèñîê');
+define('GTGP_ADMIN_BL_OPIS','Äîáàâèòü/Óäàëèòü ÷¸ðíûé ñïèñîê');
+define('GTGP_ADMIN_CAT','Êàòåãîðèè');
+define('GTGP_ADMIN_CAT_OPIS','Äîáàâèòü/Óäàëèòü êàòåãîðèè');
+define('GTGP_ADMIN_CFG','Óñòàíîâêà');
+define('GTGP_ADMIN_CFG_OPIS','Óñòàíîâèòü ïðîãðàììó Comus TGP');
+define('GTGP_ADMIN_TMPL','Øàáëîíû');
+define('GTGP_ADMIN_TMPL_OPIS','Èçìåíèòü øàáëîíû');
+define('GTGP_ADMIN_CU','Ïðîâåðêà ññûëîê');
+define('GTGP_ADMIN_CU_OPIS','Ïðîâåðüòå ññûëêè çäåñü');
+define('GTGP_ADMIN_VERS','Âàøà âåðñèÿ ïðîãðàììû');
+define('GTGP_ADMIN_CURR','Òåêóùàÿ âåðñèÿ ïðîãðàììû');
+define('GTGP_ADMIN_LANG','ßçûê');
+define('GTGP_ADMIN_LANG_CHOICE','Âûáåðèòå ÿçûê');
+define('GTGP_ADMIN_MYSQL','Óñòàíîâêà áàçû äàííûõ MySQL');
+define('GTGP_ADMIN_MYSQL_HOST','Èìÿ õîñòà áàçû äàííûõ MySql');
+define('GTGP_ADMIN_MYSQL_DB','Èìÿ áàçû äàííûõ MySQL');
+define('GTGP_ADMIN_MYSQL_USER','Èìÿ ïîëüçîâàòåëÿ áàçû äàííûõ MySQL');
+define('GTGP_ADMIN_MYSQL_PASS','Ïàðîëü áàçû äàííûõ MySQL');
+define('GTGP_ADMIN_TGP','Óñòàíîâêà ïðîãðàììû TGP');
+define('GTGP_ADMIN_TGP_CHD','Ðåôîðìàòèðîâàòü îïèñàíèå?');
+define('GTGP_ADMIN_TGP_EMAIL','Âàø àäðåñ ýë. ïî÷òû');
+define('GTGP_ADMIN_TGP_URL','Àäðåñ. (Áåç http://www. - Íàïðèìåð: <B>testing.com</B>)');
+define('GTGP_ADMIN_TGP_RURL','Îáðàòíàÿ ññûëêà (Âêëþ÷àÿ http:// - Íàïðèìåð: <B>http://testing.com</B>');
+define('GTGP_ADMIN_TGP_OWNER','Âàøå èìÿ (Vasya Pupkin)');
+define('GTGP_ADMIN_TGP_BGCOL','Öâåò ôîíà');
+define('GTGP_ADMIN_TGP_GALNUM','Íîìåð ãàëåðåè');
+define('GTGP_ADMIN_TGP_BL','Èñïîëüçîâàòü ÷¸ðíûé ñïèñîê?');
+define('GTGP_ADMIN_TGP_HMAIL','Ïîñûëàòü ñîîáùåíèÿ ýë. ïî÷òû â âèäå HTML?');
+define('GTGP_ADMIN_TGP_CONFIRM','Èñïîëüçîâàòü ôóíêöèþ ïîäòâåðæäåíèÿ ÷åðåç ýë. ïî÷òó?');
+define('GTGP_ADMIN_TGP_UEMAIL','Èñïîëüçîâàòü ôóíêöèþ èçâåùåíèÿ î ïðè¸ìå/îòêàçå');
+define('GTGP_ADMIN_TGP_RECIP','Òðåáîâàòü îáðàòíóþ ññûëêó?');
+define('GTGP_ADMIN_TGP_PARTNER','Èñïîëüçîâàòü ôóíêöèþ ïðèâèëåãèðîâàííîãî âåáìàñòåðà?');
+define('GTGP_ADMIN_TGP_POPUP','Ðàçðåøèòü èñïîëüçîâàíèå Pop-ups íà ñòðàíèöå ãàëåðåè?');
+define('GTGP_ADMIN_TGP_JAVA','Ðàçðåøèòü èñïîëüçîâàíèå Javascript?');
+define('GTGP_ADMIN_TGP_FL','Ðàçðåøèòü èñïîëüçîâàíèå òåõíîëîãèè Flash íà ñòðàíèöå ãàëåðåè?');
+define('GTGP_ADMIN_TGP_DUP','Ïðîèçâîäèòü ïðîâåðêó ïîâòîðíî ïðèñëàííûõ ññûëîê?');
+define('GTGP_ADMIN_TGP_BANW','Ïðîèçâîäèòü ïðîâåðêó íàëè÷èÿ çàïðåùåííûõ ñëîâ?');
+define('GTGP_ADMIN_TGP_BANWL','Ââåäèòå çàïðåùåííûå ñëîâà (÷åðåç çàïÿòóþ)');
+define('GTGP_ADMIN_TGP_GALINMAIN','Êîëè÷åñòâî îòîáðàæàåìûõ ãàëåðåé');
+define('GTGP_ADMIN_TGP_LAP','Îãðàíè÷åííîå êîëè÷åñòâî êàòåãîðèé');
+define('GTGP_ADMIN_TGP_LDESC','Îãðàíè÷åííîå êîëè÷åñòâî ñèìâîëîâ â îïèñàíèè');
+define('GTGP_ADMIN_TGP_USET','Èñïîëüçîâàòü ïðîãðàììó îáìåíà ññûëêàìè (Link Trade)?');
+define('GTGP_ADMIN_TGP_TRB','Êîä ïåðåä ññûëêîé (Íàïðèìåð: <B>out.php?url=</B>)');
+define('GTGP_ADMIN_TGP_TRA','Êîä ïîñëå ññûëêè (Íàïðèìåð: <B>&link=gal&s=70&first=1</B>)');
+define('GTGP_ADMIN_TGP_MISC','Ðàçíîå');
+define('GTGP_ADMIN_TGP_DATE','Ôîðìàò äàòû - Ôóíêöèÿ äàòû<B>date()</B>');
+define('GTGP_ADMIN_TGP_SUBMIT','Âíåñòè èçìåíåíèÿ');
+define('GTGP_ADMIN_TGP_LNG','ßçûê ïðîãðàììû ïî óìîë÷àíèþ');
+define('GTGP_ADMIN_TGP_GALN','Ìàñêèìàëüíîå êîëè÷åñòâî ãàëåðåé îò âåáìàñòåðà â äåíü');
+define('GTGP_ADMIN_TGP_GALP','Ìàêñèìàëüíîå êîëè÷åñòâî ãàëåðåé îò ïðèâèëåãèðîâàííîãî âåáìàñòåðà â äåíü');
+define('GTGP_ADMIN_TGP_IFRAME','Ðàçðåøèòü èñïîëüççîâàíèå IFrames?');
+define('GTGP_ADMIN_TGP_OBJECT','Ðàçðåøèòü èñïîëüçîâàíèå âñòðîåííûõ îáúåêòîâ (imbedded objects)?');
+define('GTGP_ADMIN_GAL_MINPIC','Ìèíèìàëüíîå êîëè÷åñòâî ôîòîãðàôèé');
+define('GTGP_ADMIN_GAL_MAXPIC','Ìàêñèìàëüíîå êîëè÷åñòâî ôîòîãðàôèé');
+define('GTGP_ADMIN_GAL_MAXLINK','Ìàêñèìàëüíîå êîëè÷åñòâî âíåøíèõ ññûëîê íà ãàëåðåå');
+define('GTGP_ADMIN_GAL_THUMBCHECK','Ïðîâåðÿòü ðàçìåð thumbnail-èçîáðàæåíèé?');
+define('GTGP_ADMIN_GAL_THUMBW','Øèðèíà Thumbnail-èçîáðàæåíèÿ');
+define('GTGP_ADMIN_GAL_THUMBH','Âûñîòà Thumbnail-èçîáðàæåíèÿ');
+define('GTGP_ADMIN_GAL_POSTING','Ïðèíÿòü íîâûå ãàëåðåè?');
+define('GTGP_ADMIN_GAL_MKLNGBOX','Ïðîâåðÿòü êîëè÷åñòâî âíåøíèõ ññûëîê íà ãàëåðåå?');
+define('GTGP_ADMIN_GAL_ADVCHECK','Ïðîâåðÿòü ìèíèìàëüíîå/ìàêñèìàëüíîå êîëè÷åñòâî ôîòîãðàôèé?');
+define('GTGP_ADMIN_GAL_ADV','Äîïîëíèòåëüíûå íàñòðîéêè');
+define('GTGP_ADMIN_CAT_ADD','Íàçâàíèå êàòåãîðèè');
+define('GTGP_ADMIN_CAT_SUBMIT','Âíåñòè èçìåíåíèÿ');
+define('GTGP_ADMIN_CAT_ALL','Âûâåñòè ñïèñîê âñåõ êàòåãîðèé');
+define('GTGP_ADMIN_CAT_DELETE','Óäàëèòü');
+define('GTGP_ADMIN_CAT_ADD_B','Êàòåãîðèÿ');
+define('GTGP_ADMIN_CAT_ADD_A','Äîáàâëåíî');
+define('GTGP_ADMIN_PARTNER','Ïàðòí¸ð');
+define('GTGP_ADMIN_PARTNER_EMAIL','Ïîñûëàòü ïàðòí¸ðó ñîîáùåíèå ýë. ïî÷òû');
+define('GTGP_ADMIN_PARTNER_ADD','Äîáàâèòü ïàðòí¸ðà');
+define('GTGP_ADMIN_PARTNER_DEL','Óäàëèòü');
+define('GTGP_ADMIN_PARTNER_ADD_B','Ïàðòí¸ð');
+define('GTGP_ADMIN_PARTNER_ADD_A','Äîáàâëåíî');
+define('GTGP_ADMIN_PARTNER_DEL_B','Ïàðòí¸ð');
+define('GTGP_ADMIN_PARTNER_DEL_A','Óäàëåíî');
+define('GTGP_ADMIN_TMPL','Øàáëîíû');
+define('GTGP_ADMIN_TMPL_FILE','Ôàéë');
+define('GTGP_ADMIN_TMPL_NEX','Ãàëåðåÿ îòêëîíåíà');
+define('GTGP_ADMIN_TMPL_PMAIL','Îòïðàâëÿòü ñîîáùåíèå ïàðí¸ðó');
+define('GTGP_ADMIN_TMPL_CMAIL','Îòïðàâëÿòü ïîäòâåðæäåíèå');
+define('GTGP_ADMIN_TMPL_AMAIL','Îòïðàâëÿòü ñîîáùåíèå î êà÷åñòâåííîé ãàëåðåå');
+define('GTGP_ADMIN_TMPL_RMAIL','Îòïðàâëÿòü ñîîáùåíèå îá îòêëîíåííîé ãàëåðåå');
+define('GTGP_ADMIN_TMPL_SUBMIT','Îòïðàâèòü');
+define('GTGP_ADMIN_TMPL_HTML','Âíåñòè ãàëåðåþ â ñïèñîê');
+define('GTGP_ADMIN_BL','×¸ðíûé ñïèñîê');
+define('GTGP_ADMIN_BL_ADD','Äîáàâèòü â ÷åðíûé ñïèñîê');
+define('GTGP_ADMIN_BL_DEL','Óäàëèòü');
+define('GTGP_ADMIN_BL_EMAIL','Email');
+define('GTGP_ADMIN_BL_DOMAIN','Äîìåí');
+define('GTGP_ADMIN_BL_IP','IP');
+define('GTGP_ADMIN_BL_A','Email');
+define('GTGP_ADMIN_BL_B','Äîáàâëåíî â ÷¸ðíûé ñïèñîê');
+define('GTGP_ADMIN_BL_AD','Äîìåí');
+define('GTGP_ADMIN_BL_BD','Äîáàâëåíî â ÷åðíûé ñïèñîê');
+define('GTGP_ADMIN_BL_AI','IP');
+define('GTGP_ADMIN_BL_BI','Äîáàâëåíî â ÷åðíûé ñïèñîê');
+define('GTGP_ADMIN_CL','Ïðèìå÷àíèå: Ïðîâåðêà ññûëîê ìîæåò çàíÿòü íåêîòîðîå âðåìÿ... ');
+define('GTGP_ADMIN_CL_CLICK','Äàòü îöåíêó êàòåãîðèè');
+define('GTGP_ADMIN_CL_RES','Îáðàòíàÿ ññûëêà:');
+define('GTGP_ADMIN_CL_URL','URL');
+define('GTGP_ADMIN_CL_RURL','Âêëþ÷èòü');
+define('GTGP_ADMIN_CL_BW','Çàïðåù¸ííûå ñëîâà');
+define('GTGP_ADMIN_CL_POPUP','Pop-up');
+define('GTGP_ADMIN_CL_JAVA','Java');
+define('GTGP_ADMIN_CL_FL','Migotki');
+define('GTGP_ADMIN_CL_DATE','Äàòà');
+define('GTGP_ADMIN_CL_DEL','Óäàëèòü');
+define('GTGP_ADMIN_CL_CLEAN','Î÷èñòèòü');
+define('GTGP_ADMIN_CL_JBW','Íåò');
+define('GTGP_ADMIN_CL_JP','Íåò');
+define('GTGP_ADMIN_CL_JJ','Íåò');
+define('GTGP_ADMIN_CL_JFL','Íåò');
+define('GTGP_ADMIN_CL_DELP','Óäàëèòü');
+define('GTGP_ADMIN_CL_DELETED','Ññûëêè óäàëåíû');
+define('GTGP_ADMIN_POST_WAIT','Êîëè÷åñòâî ãàëåðåé, îæèäàþùèõ ðàññìîòðåíèÿ');
+define('GTGP_ADMIN_POST_URL','URL');
+define('GTGP_ADMIN_POST_ACC','Ïðèíÿòî');
+define('GTGP_ADMIN_POST_CAT','Êàòåãîðèÿ');
+define('GTGP_ADMIN_POST_DES','Îïèñàíèå');
+define('GTGP_ADMIN_POST_REC','Îáðàòíàÿ ññûëêà?');
+define('GTGP_ADMIN_POST_IP','IP');
+define('GTGP_ADMIN_POST_VOT','Ðåéòèíã');
+define('GTGP_ADMIN_POST_DATE','Äàòà');
+define('GTGP_ADMIN_POST_SUBMITER','Àïïëèêàíò');
+define('GTGP_ADMIN_POST_SUBMIT','Îáíîâèòå áàçó äàííûõ!');
+define('GTGP_ADMIN_POST_PIC','Êîëè÷åñòâî ôîòîãðàôèé');
+define('GTGP_POST_MINPIC','Ñëèøêîì ìàëî ôîòîãðàôèé!');
+define('GTGP_POST_MAXPIC','Ñëèøêîì ìíîãî ôîòîãðàôèé!');
+define('GTGP_POST_MAXLINK','Ñëèøêîì ìíîãî âíåøíèõ ññûëîê íà ãàëåðåå!');
+define('GTGP_POST_MINPIC_T','Ìèíèìàëüíî äîïóñòèìîå êîëè÷åñòâî ôîòîãðàôèé - ');
+define('GTGP_POST_MAXPIC_T','Ìàêñèìàëüíî äîïóñòèìîå êîëè÷åñòâî ôîòîãðàôèé - ');
+define('GTGP_POST_PIC_P','ôîòîãðàôèé');
+define('GTGP_POST_MAXLINK_T','Âàì ðàçðåøàåòñÿ èìåòü òîëüêî');
+define('GTGP_POST_MAXLINK_P','âíåøíèõ ññûëîê');
+define('GTGP_POST_MAXLINK_G','Ó Âàñ');
+define('GTGP_POST_THUMB','Thumb');
+define('GTGP_POST_THUMB_OK','Ok!');
+define('GTGP_POST_THUMB_M','Thumbnail-èçîáðàæåíèÿ ñëèøêîì ìàëåíüêèå.');
+define('GTGP_POST_THUMB_M_B','Ìèíèìàëüíûé äîïóñòèìûé ðàçìåð:');
+define('GTGP_POST_THUMB_D','Âàøè thumbnail-èçîáðàæåíèÿ ñëèøêîì áîëüøèå.');
+define('GTGP_POST_THUMB_D_B','Ìàêñèìàëüíûé äîïóñòèìûé ðàçìåð thumbnail-èçîáðàæåíèÿ:');
+define('GTGP_POST_THUMB_ERROR','Îøèáêà ïðè ïðîâåðêå thumbnail-èçîáðàæåíèé:');
+define('GTGP_WM_POWERED','Ðàçðàáîòàíî');
+define('GTGP_WM_NAME','Êàê Âàñ çîâóò?');
+define('GTGP_WM_EMAIL','Âàø àäðåñ ýë. ïî÷òû?');
+define('GTGP_WM_URL','URL');
+define('GTGP_WM_CAT','Êàòåãîðèÿ');
+define('GTGP_WM_DES','Îïèñàíèå');
+define('GTGP_WM_PAS','Ïàðîëü');
+define('GTGP_WM_PIC','Êîëè÷åñòâî ôîòîãðàôèé');
+define('GTGP_WM_MAILME','Ïðèñûëàéòå ìíå èçâåùåíèå î ïðèíÿòûõ/îòêëîíåííûõ ãàëåðåÿõ');
+define('GTGP_WM_SUBMIT','Îòïðàâèòü');
+define('GTGP_WM_CONTACT','Ñâÿçàòüñÿ ñ âåáìàñòåðîì');
+define('GTGP_WM_STOREUSER','Êëèêíèòå çäåñü, ÷òîáû ñîõðàíèòü Âàøó èíôîðìàöèþ íà áóäóùåå');
+define('GTGP_WM_LANG','ßçûê');
+define('GTGP_WM_POSTING','Â íàñòîÿùåå âðåìÿ ìû íå ïðèíèìàåì íîâûå ãàëåðåè.');
+define('GTGP_SET_RETURN','Âîçâðàò íà ãëàâíóþ ñòðàíèöó');
+define('GTGP_SET_RETURN_C','Âåðíóòüñÿ');
+define('GTG_SETUP_UPDATED','Èçìåíåíèÿ â óñòàíîâêå ïðîøëè óñïåøíî');
+define('GTGP_POST_DU','Âû óæå ïðèñûëàëè íàì ýòó ãàëåðåþ!');
+define('GTGP_POST_PO','Ïîæàëóéñòà, íå ïðèñûëàéòå ñâîè ãàëåðåè ïîâòîðíî');
+define('GTGP_POST_BL','Âû âíåñåíû â ÷¸ðíûé ñïèñîê');
+define('GTGP_POST_NF','Çàäàííûé URL íå ñóùåñòâóåò');
+define('GTGP_POST_BW','Âû èñïîëüçóåòå çàïðåùåííîå ñëîâî');
+define('GTGP_POST_POP','Çàïðåùåíî èñïîëüçîâàíèå Pop-up îêîí');
+define('GTGP_POST_JAVA','Çàïðåùåíî èñïîëüçîâàíèå Javascript');
+define('GTGP_POST_FL','Çàïðåùåíî èñïîëüçîâàíèå ñëåäóþùåãî ñëîâà â ãàëåðåå ');
+define('GTGP_POST_RU','Äàííàÿ ññûëêà íå ñóùåñòâóåò');
+define('GTGP_POST_RUU','Óáåäèòåñü â íàëè÷èå îáðàòíîé ññûëêè');
+define('GTGP_POST_TU','Áëàãîäàðèì Âàñ çà ïðèñëàííóþ ãàëåðåþ');
+define('GTGP_POST_DANE','Èíôîðìàöèÿ î âàøåé ãàëåðåå');
+define('GTGP_POST_EMAIL','Email');
+define('GTGP_POST_URL','Url');
+define('GTGP_POST_CAT','Êàòåãîðèÿ');
+define('GTGP_POST_DSC','Îïèñàíèå');
+define('GTGP_POST_NPIC','Êîëè÷åñòâî ôîòîãðàôèé');
+define('GTGP_POST_PARTNER','Óâàæàåìûé, ïðèâèëåãèðîâàííûé âåáìàñòåð, Âàøà ãàëåðåÿ äîáàâëåíà â ñïèñîê');
+define('GTGP_POST_CONTACT','Ñâÿæèòåñü ñî ìíîé');
+define('GTGP_POST_CHECKING','Ðàçìåùåíèå èçîáðàæåíèÿ');
+define('GTGP_POST_PIC_WIDTH','Øèðèíà èçîáðàæåíèÿ');
+define('GTGP_POST_PIC_HEIGHT','Âûñîòà èçîáðàæåíèÿ');
+define('GTGP_POST_PIC_SIZE','Ðàçìåð ôàéëà èçîáðàæåíèÿ');
+define('GTGP_POST_PIC_OK','Ok');
+define('GTGP_POST_HTML_OK','Ok!');
+define('GTGP_POST_HTML_SIZE','Ðàçìåð ôàéëà HTML');
+define('GTGP_POST_PIC_GAL_PIC','Ìû îáíàðóæèëè');
+define('GTGP_POST_PIC_GAL_PIC_P','èçîáðàæåíèé â Âàøåé ãàëåðåå');
+define('GTGP_POST_PIC_GAL_FORM','Âû óêàçàëè');
+define('GTGP_POST_PIC_GAL_FORM_P','èçîáðàæåíèé ïðè çàïîëíåíèè ôîðìû');
+define('GTGP_POST_PIC_GAL_CHANGE','Âàøà ïîïûòêà ïåðåñëàòü íàì ññûëêó íå óäàëàñü!');
+define('GTGP_POST_BACK','Âåðíóòüñÿ');
+define('GTGP_POST_NLINK','Èíîñòðàííûå ññûëêè');
+define('GTGP_WM_POSTID1','Íîìåð Âàøåé ãàëåðåè: <B>');
+define('GTGP_WM_POSTID2','</B>.<BR>');
+define('GTGP_ACC_TU','Áëàãîäàðèì Âàñ çà ãàëåðåþ, ïðèñëàííóþ íà ñàéò: ');
+define('GTGP_ACC_AC','Âàøà ññûëêà áûëà äîáàâëåíà â áàçó äàííûõ');
+define('GTGP_ACC_RE','è áóäåò ñêîðî ðàññìîòðåíà');
+define('GTGP_UPDATE_DONE','Îáíîâèòü!');
+define('GTGP_UPDATE_ERROR','Îøèáêà ïðè îáíîâëåíèè');
+
+
+// ######################################### //
+
+define('GTGP_NAME','ComusTGP');
+define('GTGP_VERS','2.0');
+
+function tsl($co)
+{
+$tyes = GTGP_YES;
+$tno  = GTGP_NO;
+if($co == "Yes") { return $tyes;}
+if($co == "No")  { return $tno;}
+}
+
+function tslb($co)
+{
+$tyes = GTGP_YES1;
+$tno  = GTGP_NO1;
+if($co == "Yes") { return $tyes;}
+if($co == "No")  { return $tno;}
+}
+
+function tslbl($co)
+{
+$email  = GTGP_ADMIN_BL_EMAIL;
+$domain = GTGP_ADMIN_BL_DOMAIN;
+$ip = GTGP_ADMIN_BL_IP;
+if($co == "email")  { return $email;}
+if($co == "domain") { return $domain;}
+if($co == "ip")     { return $ip;}
+}
+
+function printabout($ile)
+{
+	global $recip, $sitename;
+?>
+<TR>
+	<TD ALIGN="CENTER" COLSPAN="<? echo $ile ?>">
+		<? echo "<A HREF=\"http://nibbi.net/scripts/comus/\" target=\"_blank\"><B>" . GTGP_NAME . "</B></A>&nbsp;" . GTGP_VERS . "<BR>" . GTGP_ADMIN_PAGE . "<BR>" . "<A HREF=\"". $recip ."\" TARGET=\"_BLANK\">". $sitename. "</A>"; ?>
+	</TD>
+</TR>
+<?
+}
+
+
+?>
+
