79128b7e66cbdb9aa5c9fd3f1ae3d042c54387f1
me-suzy <me.suzana@gmail.com>
2025-08-12 11:41:37 +0300

    Add E-Commerce/enterprise3.01/admin/includes/languages/english/affiliate_clicks.php

---
+++ E-Commerce/enterprise3.01/admin/includes/languages/english/affiliate_clicks.php
@@ -0,0 +1,29 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  OSC-Affiliate
+
+  Contribution based on:
+
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Affiliate Clickthroughs');
+
+define('TABLE_HEADING_ONLINE', 'Clickthroughs');
+define('TABLE_HEADING_AFFILIATE_USERNAME', 'Affiliate');
+define('TABLE_HEADING_IPADDRESS', 'IP Address');
+define('TABLE_HEADING_ENTRY_TIME', 'Time');
+define('TABLE_HEADING_BROWSER', 'Browser');
+define('TABLE_HEADING_ENTRY_DATE', 'Date');
+define('TABLE_HEADING_CLICKED_PRODUCT','Clicked Product');
+define('TABLE_HEADING_REFERRAL_URL', 'Refering URL');
+
+define('TEXT_NO_CLICKS', 'No Clickthroughs have been recorded');
+define('TEXT_DISPLAY_NUMBER_OF_CLICKS', 'Displaying <b>%d</b> to <b>%d</b> (of <b>%d</b> clickthroughs)');
+?>
