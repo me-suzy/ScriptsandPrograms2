c62cbb0a42c196ad1c16c118368b6167d3af54a4
me-suzy <me.suzana@gmail.com>
2025-08-12 11:58:55 +0300

    Add E-Commerce/enterprise3.01/includes/languages/german/address_book.php

---
+++ E-Commerce/enterprise3.01/includes/languages/german/address_book.php
@@ -0,0 +1,25 @@
+<?php
+/*
+  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  Enterprise Shopping Cart Software
+  http://www.enterprisecart.com
+
+  Copyright (c) 2004 Enterprise Shopping Cart Software.  Portions Copyright (c) 2001-2004 osCommerce: http://www.oscommerce.com
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE_1', 'Ihr Konto');
+define('NAVBAR_TITLE_2', 'Adressbuch');
+
+define('HEADING_TITLE', 'Mein Pers&ouml;nliches Adressbuch');
+
+define('PRIMARY_ADDRESS_TITLE', 'Standardadresse');
+define('PRIMARY_ADDRESS_DESCRIPTION', 'Diese Adresse wird automatisch als Liefer- und Rechnungsadresse gew&auml;hlt wenn Sie eine Bestellung aufgeben.<br><br>Diese Adresse wird auch als Basis f&uuml;r die Berechnung eventueller Steuern udn Versandkosten verwendet.');
+
+define('ADDRESS_BOOK_TITLE', 'Adressbucheintr&auml;ge');
+
+define('PRIMARY_ADDRESS', '(Standardadresse)');
+
+define('TEXT_MAXIMUM_ENTRIES', '<font color="#ff0000"><b>Hinweis:</b></font> Ihnen stehen %s Adressbucheintr&auml;ge zur Verf&uuml;gung!');
+?>
