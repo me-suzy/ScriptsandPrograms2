6617c9ab0124197cc761483f811cc84148856f5c
me-suzy <me.suzana@gmail.com>
2025-08-13 04:49:36 +0300

    Add Unsorted/gcards/inc/lang/language_fi.php

---
+++ Unsorted/gcards/inc/lang/language_fi.php
@@ -0,0 +1,148 @@
+<?
+// index.php
+$index01 = 'Kaikki categoriat';
+$index02 = 'Valitse kortti alhaalta lähettääksesi sen!';
+
+// general navigation
+$nav01 = 'Takaisin';
+$nav02 = 'Seuraava';
+$nav03 = 'etusivu';
+$nav04 = 'Näytä';
+$nav05 = 'Postimestarin toimisto';
+$nav06 = 'Vaihda salasana';
+$nav07 = 'Kirjaudu ulos';
+$nav08 = 'Kirjaudu sisälle';
+$nav09 = 'Pohjana toimii';
+$nav10 = 'Riviä per sivu';
+$nav11 = 'Palaa <a href="javascript:history.go(-1);">takaisin</a>';
+$nav12 = 'Palaa takaisin kohteeseen';
+
+
+
+// UIfunctions.php
+$uifunc01 = 'Käyttäjä: ';
+
+// compose.php
+$compose01 = 'Korttia ei ole valittu!  Palaa <a href="index.php">takaisin</a> valitsemaan kortti';
+$compose02 = 'Täytä kohdat lähettääksesi kortti!';
+$compose03 = 'Nimesi';
+$compose04 = 'Emailisi';
+$compose05 = 'Keneltä:';
+$compose06 = 'kenelle:';
+$compose07 = 'Ilmoita, kun kortti on vastaanotettu';
+$compose08 = 'Yksi sähköpostiosoite per rivi';
+
+// preview.php
+$preview01 = "Täytä kaikki kohdat! $nav11";
+$preview02 = 'Emaili ei ole oikein';
+$preview03 = "Ei toimiva emaili! $nav11";
+$preview04 = 'Lähetä';
+
+// sendcard.php
+$sendcard01 = 'Virhe!  Kohtia ei ole täytetty oikein.  Palaa <a href="javascript:history.go(-1);">takaisin</a>';
+$sendcard02 = 'virhe syötössä: ';
+$sendcard03 = 'Kortti lähetetty...';
+$sendcard04 = 'Korttia ei voitu lähettää...';
+
+// getcard.php
+$getcard01 = 'Virhe - Ei kortti ID';
+$getcard02 = '$to_email on katsonut korttisi';
+$getcard03 = 'Lähetä henkilölle $from_name kortti';
+
+// showcard.php
+$showcard01 = 'Kortti henkilöltä';
+
+// news.php
+$news01 = 'Arkistoidut uutiset';
+$news02 = 'Lähetetty';
+$news03 = 'henkilöltä';
+$news04 = 'Uutis artikkelia per sivu';
+$news05 = 'Uutiset';
+$news06 = 'Aihe';
+$news07 = 'Teksti';
+$news08 = 'Päiväys';
+$news09 = 'omistaja';
+
+
+
+// user related text
+$user01 = 'Käyttäjänimi';
+$user02 = 'Salasana';
+$user03 = 'Et ole kirjautunut sisään - et voi kirjautua ulos!';
+$user04 = 'Kirjauduttu ulos systeemistä onnistuneesti...';
+$user05 = 'Salasanan vahvistus';
+$user06 = 'Sähköpostiosoite';
+$user07 = 'Rooli';
+$user08 = 'Käyttäjä';
+$user09 = 'Salasana ei täsmää - Yritä uudelleen';
+$user10 = '(Jätä tyhjäksi pitääksesi oletus arvo)';
+$user11 = 'Vanha salasana ei täsmää tämän hetkisen kanssa';
+$user12 = 'Salasana päivitetty onnistuneesti!';
+$user13 = 'Vanha salasana';
+$user14 = 'Uuusi salasana';
+
+/*
+*********************
+** ADMIN DIRECTORY **
+*********************
+*/
+
+// Authentication
+$auth01 = 'Tunnistu epäonnistunut käyttäjälle';
+$auth02 = 'Anna molemmat, käyttäjätunnus ja salasana.';
+$auth03 = 'Et ole kirjautunut sisään.';
+$auth04 = 'Sinulla ei ole oikuksia katsoa tätä sivua!';
+
+// admin.php
+$admin01 = 'Adiminin asetukset';
+$admin02 = 'Pääalue';
+$admin03 = "Kortti $admin02";
+$admin04 = "Categoria $admin02";
+$admin05 = "Uutiset $admin02";
+$admin06 = "Käyttäjät $admin02";
+$admin07 = 'Katso, lisää, päivitä, ja poista';
+$admin08 = 'ID';
+$admin09 = 'poistettu tietokannasta...';
+$admin10 = 'poistettu palvelimelta...';
+$admin11 = 'ei';
+$admin12 = 'päivitetty';
+$admin13 = 'poistettu';
+$admin14 = 'lisätty';
+$admin15 = 'Lisää uusi';
+$admin16 = 'Tallenna palvelimelle';
+
+// actions
+$action01 = 'Toiminta';
+$action02 = 'Lisää';
+$action03 = 'Editoi';
+$action04 = 'Poista';
+$action05 = 'katsele';
+$action06 = 'Päivitä';
+$action07 = 'Sulje';
+$action08 = 'Ennakko';
+
+// cards.php
+$cards01 = 'Kuva';
+$cards02 = 'Pienennos';
+$cards03 = 'Kertaa lähetetty';
+$cards04 = 'Kortti';
+$cards05 = 'Kortin nimi';
+$cards06 = 'Jätä tyhjäksi automaattiselle pienennokselle';
+
+// categories
+$cat01 = 'Categori';
+$cat02 = 'Categorin nimi';
+$cat03 = 'Kortit categoriassa';
+
+// upload.php
+$upload01 = 'Kuvaa ei ole tallennettu palvelimelle!';
+$upload02 = "Tiedosto kopiointi virhe 'move_uploaded_file' funktiossa!";
+$upload03 = 'Kuva talennettu poalvelimelle onnistuneesti...';
+$upload04 = 'Tiedoston kopiointi virhe!';
+$upload05 = 'Pienennos tallennettu palvelimelle onnistuneesti...';
+$upload06 = 'Palvelimelle tallennettua kuvaa käytetään pienennoksena...';
+$upload07 = 'Pienennos luotu onnistuneesti...';
+$upload08 = 'Tallenna palvelimelle pienennos, jos kuva ei ole .jpg muotoa.';
+
+
+?>
