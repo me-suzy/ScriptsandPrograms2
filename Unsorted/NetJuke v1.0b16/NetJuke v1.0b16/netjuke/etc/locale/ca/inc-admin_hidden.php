8ecaee6fe5b742469405793afcf6e0f2e4def21f
me-suzy <me.suzana@gmail.com>
2025-08-13 06:50:47 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_hidden.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_hidden.php
@@ -0,0 +1,20 @@
+<?php
+
+##################################################
+
+# /admin/hidden.php
+
+##################################################
+
+define( "HDDN_DENIED_BADDIR", "El directori que heu sol·licitat per escanejar no està registrat en el fitxer de preferències d'aquest directori de música." );
+define( "HDDN_DENIED_NODIR_1", "Si us plau, indiqueu un directori vàlid per escanejar per als arxius ocults." );
+define( "HDDN_DENIED_NODIR_2", "No hi ha dir. de música. Vegeu el fitxer de preferències." );
+
+define( "HDDN_HEADER", "CERCADOR DE FITXERS OCULTS" );
+define( "HDDN_CAPTION_1", "Escaneja el nom atorgat als fitxers ocults o sub-directoris (els noms d'arxius que comencen amb un punt) i genera un fitxer de lots adequat per esborrar-los en la línia d'ordre. És especialment útil per carregar arxius des del Mac a l'Unix (.AppleDouble, .DS_Store, etc.)" );
+define( "HDDN_CAPTION_2", "Si us plau, trameteu un directori per escanejar" );
+define( "HDDN_BTN_SCAN", "Comença l'escaneig" );
+
+##################################################
+
+?>
