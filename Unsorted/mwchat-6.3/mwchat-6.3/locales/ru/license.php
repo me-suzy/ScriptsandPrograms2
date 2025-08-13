f0d4d0b9bfdfbc6e0f1ce681059155d1f1ab41f3
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:53 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/license.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/license.php
@@ -0,0 +1,29 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[LICENSE_Title] = "ðÒÏÌÉÃÅÎÚÉÒÕÊÔÅ MWChat!";
+
+$LOCALE[LICENSE_Info] = "ðÏÖÁÌÕÊÓÔÁ, ××ÅÄÉÔÅ ×ÁÛ ÒÅÇÉÓÔÒÁÃÉÏÎÎÙÊ ÎÏÍÅÒ ÄÌÑ MWChat ÎÉÖÅ, ÞÔÏÂÙ ÚÁ×ÅÒÛÉÔØ ÕÓÔÁÎÏ×ËÕ.";
+
+$LOCALE[LICENSE_Key] = "òÅÇÉÓÔÒÁÃÉÏÎÎÙÊ ÎÏÍÅÒ";
+
+$LOCALE[LICENSE_Submit] = "ðÏÌÕÞÉÔØ ÌÉÃÅÎÚÉÉ";
+
+$LOCALE[LICENSE_Upgrade] = "îÁÖÍÉÔÅ ÚÄÅÓØ, ÅÓÌÉ ×Ù ÏÂÎÏ×ÌÑÅÔÅ ÓÔÁÒÕÀ ×ÅÒÓÉÀ MWChat.";
+
+$LOCALE[LICENSE_LoginButton] = "÷ÏÊÔÉ × MWChat";
+
+$LOCALE[LICENSE_Complete_basic] = "ðÏÚÄÒÁ×ÌÑÅÍ! ÷Ù ÕÓÐÅÛÎÏ ÐÏÌÕÞÉÌÉ ÂÁÚÏ×ÕÀ ÌÉÃÅÎÚÉÀ ÎÁ MWChat.";
+
+$LOCALE[LICENSE_Complete_pro] = "ðÏÚÄÒÁ×ÌÑÅÍ! ÷Ù ÕÓÐÅÛÎÏ ÐÏÌÕÞÉÌÉ ÐÒÏÆÅÓÓÉÏÎÁÌØÎÕÀ ÌÉÃÅÎÚÉÀ ÎÁ MWChat.";
+
+?>
