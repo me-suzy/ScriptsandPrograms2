6b4f3b617db07c9144bf30f8d1c0931b1c54ea0f
me-suzy <me.suzana@gmail.com>
2025-08-13 06:39:10 +0300

    Add Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/ru/lng_global.php

---
+++ Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/ru/lng_global.php
@@ -0,0 +1,65 @@
+<?php
+
+$lang['page_title']	    =	':: Secure ::';
+$lang['user']		    =	'ÐÏÌØÚÏ×ÁÔÅÌØ';
+$lang['login']		    =	'×ÏÊÔÉ';
+$lang['username']	    =	'éÍÑ ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['realname']	    =	'æ.é.ï.';
+$lang['password']	    =	'ðÁÒÏÌØ';
+$lang['next']		    =	'>>';
+$lang['previous']	    =	'<<';
+$lang['page']		    =	'ÓÔÒÁÎÉÃÁ';
+$lang['pages']		    =	'ÓÔÒÁÎÉÃÙ';
+$lang['of']		    =	'ÉÚ';
+$lang['add']	   	    =	'ÄÏÂÁ×ÉÔØ';
+$lang['delete']		    =	'ÕÄÁÌÉÔØ';
+$lang['edit']  		    =	'ÉÚÍÅÎÉÔØ';
+$lang['save']		    =	'ÓÏÈÒÁÎÉÔØ';
+$lang['date']  		    =	'ÄÁÔÁ';
+$lang['time']  		    =	'×ÒÅÍÑ';
+$lang['january']	    =	'ÑÎ×ÁÒØ';
+$lang['february']	    =	'ÆÅ×ÒÁÌØ';
+$lang['march']		    =	'ÍÁÒÔ';
+$lang['april']		    =	'ÁÐÒÅÌØ';
+$lang['may']		    =	'ÍÁÊ';
+$lang['june']		    =	'ÉÀÎØ';
+$lang['july']		    =	'ÉÀÌØ';
+$lang['augest']		    =	'Á×ÇÕÓÔ';
+$lang['september']	    =	'ÓÅÎÔÑÂÒØ';
+$lang['october']   	    =	'ÏËÔÑÂÒØ';
+$lang['november']	    =	'ÎÏÑÂÒØ';
+$lang['december']	    =	'ÄÅËÁÂÒØ';
+$lang['monday']		    =	'ÐÏÎÅÄÅÌØÎÉË';
+$lang['tuesday']	    =	'×ÔÏÒÎÉË';
+$lang['wednesday']	    =	'ÓÒÅÄÁ';
+$lang['thursday']	    =	'ÞÅÔ×ÅÒÇ';
+$lang['friday']		    =	'ÐÑÔÎÉÃÁ';
+$lang['saturday']	    =	'ÓÕÂÂÏÔÁ';
+$lang['sunday']		    =	'×ÏÓËÒÅÓÅÎØÅ';
+$lang['s_monday']       =	'ðÎ';
+$lang['s_tuesday']      =	'÷Ô';
+$lang['s_wednesday']    =	'óÒ';
+$lang['s_thursday']	    =	'þÔ';
+$lang['s_friday']       =	'ðÔ';
+$lang['s_saturday']	    =	'óÂ';
+$lang['s_sunday']       =	'÷Ó';
+$lang['year']   	    =	'ÇÏÄ';
+$lang['s_year']   	    =	'Ç.';
+$lang['email']   	    =	'Email';
+$lang['phone']	  	    =	'ôÅÌÅÆÏÎ';
+$lang['s_phone']   	    =	'ôÅÌ.';
+$lang['fax']	   	    =	'æÁËÓ';
+$lang['search']         =   'ðÏÉÓË';
+$lang['avdsearch']      =   'òÁÓÛÉÒÅÎÎÙÊ ÐÏÉÓË';
+$lang['sitemap']        =   'ëÁÒÔÁ ÓÁÊÔÁ';
+$lang['support']        =   'ðÏÄÄÅÒÖËÁ';
+$lang['feedback']       =   'ïÂÒÁÔÎÁÑ Ó×ÑÚØ';
+$lang['redirect_link_text']    =   'åÓÌÉ ÎÅÔ ÖÅÌÁÎÉÑ ÖÄÁÔØ, ÎÁÖÍÉÔÅ ÎÁ ÜÔÕ ÓÓÙÌËÕ';
+$lang['redirect_page_subtitle'] = 'ðÅÒÅÎÁÐÒÁ×ÌÅÎÉÅ';
+$lang['error_page_text']    = 'ðÒÏÉÚÏÝÌÁ ÏÛÉÂËÁ';
+$lang['go_back']            = '×ÅÒÎÕÔØÓÑ';
+$lang['contact_webmaster']  = 'webmaster';
+
+
+
+?>
