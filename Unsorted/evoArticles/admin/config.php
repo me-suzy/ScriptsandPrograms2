d6a53c3b75c6eaef23d26f5c8cb4e768aa14844c
me-suzy <me.suzana@gmail.com>
2025-08-13 04:07:39 +0300

    Add Unsorted/evoArticles/admin/config.php

---
+++ Unsorted/evoArticles/admin/config.php
@@ -0,0 +1,6 @@
+<?php
+$db['user'] = "root";  // mysql username
+$db['pass'] = ""; //mysql password
+$db['host'] = "localhost"; //mysql host/server
+$db['name'] = "evoArticles"; //mysql database name
+?>
