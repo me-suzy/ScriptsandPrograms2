eda524c2cc116436eddfb16d3200c6871387d082
me-suzy <me.suzana@gmail.com>
2025-08-13 03:39:49 +0300

    Add Unsorted/ebay/testsql/setup_ebay_marketplaces.sql

---
+++ Unsorted/ebay/testsql/setup_ebay_marketplaces.sql
@@ -0,0 +1,23 @@
+/*	$Id: setup_ebay_marketplaces.sql,v 1.2 1999/02/21 02:57:03 josh Exp $	*/
+/*
+ * setup_ebay_marketplaces.sql
+ *
+ * Until we build user functionality to add marketplaces,
+ * this is how we do it.
+ *
+ */
+
+ insert into ebay_marketplaces
+ (
+	id,
+	name
+ )
+ values
+ (
+	0,
+	'eBay AuctionWeb'
+ );
+
+ commit;
+
+
