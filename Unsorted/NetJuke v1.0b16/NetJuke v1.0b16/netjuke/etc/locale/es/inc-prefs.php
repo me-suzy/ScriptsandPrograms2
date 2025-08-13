501364fe0670411ec2c92e7599ad278e76029590
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:35 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-prefs.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-prefs.php
@@ -0,0 +1,46 @@
+<?php
+
+##################################################
+
+# /prefs.php
+
+##################################################
+
+define( "PREFS_DENIED_1", "Un error inesperado ha ocurrido. Por favor intente de nuevo." );
+
+define( "PREFS_CHECKFORM_1", "Por favor, seleccione un color para el fondo." );
+define( "PREFS_CHECKFORM_2", "Por favor, seleccione una lista de fuentes." );
+define( "PREFS_CHECKFORM_3", "Por favor, seleccione un tamaño de fuente." );
+define( "PREFS_CHECKFORM_4", "Por favor, seleccione un color para el texto." );
+define( "PREFS_CHECKFORM_5", "Por favor, seleccione un color para el Link." );
+define( "PREFS_CHECKFORM_6", "Por favor, seleccione un color para Link Activos." );
+define( "PREFS_CHECKFORM_7", "Por favor, seleccione un color para los Link visitados." );
+define( "PREFS_CHECKFORM_8", "Por favor, seleccione un color para el borde de la tablas." );
+define( "PREFS_CHECKFORM_9", "Por favor, seleccione un color para el Header de las tablas." );
+define( "PREFS_CHECKFORM_10", "Por favor, seleccione un color para la fuente en el encabezado de la tabla." );
+define( "PREFS_CHECKFORM_11", "Por favor, seleccione un color para el contenido de la tabla." );
+
+define( "PREFS_HEADER", "INFORMACIÓN DE LA CUENTA" );
+define( "PREFS_CAPTION", "Use el formulario siguiente para modificar la información de la cuenta." );
+define( "PREFS_PALETTE", "Use la paleta de colores para cambiar los colores." );
+
+define( "PREFS_FORMS_INVICN", "Iconos Inversos" );
+define( "PREFS_FORMS_INVICN_HELP", "Habilite a los usuarios para invertir los colores de los íconos: Sonar, Obtener Información, Filtro..." );
+define( "PREFS_FORMS_INVICN_ENABLED", "Habilitado" );
+define( "PREFS_FORMS_FONTFACE", "Tipo de fuente" );
+define( "PREFS_FORMS_FONTSIZE", "Tamaño de fuente" );
+define( "PREFS_FORMS_BGCOLOR", "Color del fondo" );
+define( "PREFS_FORMS_TEXT", "Color del Textor" );
+define( "PREFS_FORMS_LINK", "Color de los Links" );
+define( "PREFS_FORMS_ALINK", "Color de los Links Activos" );
+define( "PREFS_FORMS_VLINK", "Color de los Links Visitados" );
+define( "PREFS_FORMS_BORDER", "Color del Borde" );
+define( "PREFS_FORMS_HEADER", "Color del encabezado" );
+define( "PREFS_FORMS_HEADERFC", "Color de la fuente en el encabezado" );
+define( "PREFS_FORMS_CONTENT", "Color del contenido" );
+define( "PREFS_FORMS_BTN_SAVE", "Guardar" );
+define( "PREFS_FORMS_BTN_RESET", "Limpiar" );
+
+##################################################
+
+?>
