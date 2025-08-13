5991e4d517765070bc86e1a8003620c016700206
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:42 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.communicate.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.communicate.php
@@ -0,0 +1,172 @@
+<?php
+$L = array(
+
+
+"your_name" =>
+"Tu nombre",
+
+"your_email" =>
+"Tu email",
+
+"recipient" =>
+"Recipiente",
+
+"cc" =>
+"CC",
+
+"bcc" =>
+"CCO",
+
+"recipient_group" =>
+"Enviar a grupos de miembros",
+
+"manual_recipients_ignored" =>
+"El campo Recipiente se ignorará",
+
+"send_to_mailinglist" =>
+"¿Enviar a la lista de correo?",
+
+"honor_email_pref" =>
+"Enviar sólo a miembros que han escogido aceptar el correo",
+
+"separate_emails_with_comma" =>
+"Separar direcciones múltiples con comas",
+
+"no_email_matching_criteria" =>
+"No se ha encontrado ninguna dirección de correo que coincida con los criterios que has seleccionado",
+
+"not_allowed_to_email_members" =>
+"No tienes permiso para enviar correo a miembros",
+
+"not_allowed_to_email_member_groups" =>
+"No tienes permiso para enviar correo a grupos de miembros",
+
+"not_allowed_to_email_mailinglist" =>
+"No tienes permiso para enviar email a la lista de correo",
+
+"subject" =>
+"Asunto",
+
+"message" =>
+"Mensaje",
+
+"send_an_email" =>
+"Enviar un email",
+
+"sending_email" =>
+"Enviando email",
+
+"batchmode_ready_to_begin" =>
+"La rutina de envio de email comenzará en cinco segundos",
+
+"batchmode_warning" =>
+"¡No uses tu navegador hasta que el proceso haya sido completado!",
+
+"problem_with_id" =>
+"Se ha encontrado un problema con el número de ID que se necesita para enviar emails",
+
+"cache_data_missing" =>
+"No se han encontrado los datos del caché del correo",
+
+"currently_sending_batch" =>
+"Ahora enviando correo %x a %y",
+
+"emails_remaining" =>
+"Email restante:",
+
+"email_error" =>
+"Error de email",
+
+"send_it" =>
+"Enviar",
+
+"total_emails_sent" =>
+"Número total de emails enviados:",
+
+"plain_text" =>
+"Texto",
+
+"html" =>
+"HTML",
+
+"mail_format" =>
+"Formateando",
+
+"word_wrap" =>
+"Arrastre de palabra a siguiente línea",
+
+"priority" =>
+"Prioridad",
+
+"chars" =>
+"caracteres",
+
+"highest" =>
+"Altísima",
+
+"high" =>
+"Alta",
+
+"normal" =>
+"Normal",
+
+"low" =>
+"Baja",
+
+"lowest" =>
+"Bajísima",
+
+"empty_form_fields" =>
+"Te has dejado campos vacíos.",
+
+"email_sent_message" =>
+"Tu email ha sido enviado",
+
+"all_email_sent_message" =>
+"Todo tu email ha sido enviado",
+
+"email_sent" =>
+"Email enviado",
+
+"view_email_cache" =>
+"Vista previa de email enviado",
+
+"previous_email" =>
+"Previsualización de correo enviado",
+
+"email_title" =>
+"Título del email",
+
+"email_date" =>
+"Fecha de enviado",
+
+"total_recipients" =>
+"Total recipientes",
+
+"resend" =>
+"Reenviar",
+
+"view" =>
+"Ver",
+
+"no_cached_email" =>
+"No hay emails en pendientes",
+
+"delete_emails" =>
+"Email borrado",
+
+"delete_confirm" =>
+"Borrar confirmación de email",
+
+"delete_question" =>
+"¿Estás seguro que quieres borrar el(los) email(s) especificado(s)??",
+
+"email_deleted" =>
+"El email ha sido borrado",
+
+"mailinglist_unsubscribe" =>
+"Para eliminar tu email de nuestra lista de correo, haz un clic aquí:",
+
+''=>''
+);
+?>
