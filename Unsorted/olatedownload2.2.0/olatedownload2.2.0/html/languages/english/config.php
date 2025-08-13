691b68afa56b816a64b0feff56b9c4d5185f4a16
me-suzy <me.suzana@gmail.com>
2025-08-13 07:08:58 +0300

    Add Unsorted/olatedownload2.2.0/olatedownload2.2.0/html/languages/english/config.php

---
+++ Unsorted/olatedownload2.2.0/olatedownload2.2.0/html/languages/english/config.php
@@ -0,0 +1,26 @@
+<?php
+/***************************************************************************
+ *                      Olate Download v2 - Download Manager
+ *
+ *                           http://www.olate.com
+ *                            -------------------
+ *   author                : David Mytton
+ *   copyright             : (C) Olate 2003 
+ *
+ *   Support for Olate scripts is provided at the Olate website. Licensing
+ *   information is available in the license.htm file included in this
+ *   distribution and on the Olate website.                  
+ ***************************************************************************/
+
+$language_config = array();
+
+// Language details
+$language_config['language']			= 'English (British)'; 		// Language Name
+$language_config['scriptver']			= '2.2.0'; 					// Designed for Olate Download version
+$language_config['released']			= '12/01/04'; 				// Release date
+
+// Author/Translator Details
+$language_config['author']				= 'David Mytton'; 			// Author/Translator Name
+$language_config['url']					= 'http://www.olate.com'; 	// Author/Translator Web Address
+$language_config['email']				= 'david@olate.com'; 		// Author/Translator E-Mail Address
+?>
