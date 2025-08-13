3f24d1a7ca9c3b2ec48a202e4bd414286e002f50
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:08 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.home.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.home.php
@@ -0,0 +1,111 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Home page
+//----------------------------
+
+"total_validating_comments" =>
+"Kommentare, die auf Freigabe warten",
+
+"cp_home" =>
+"Meine Homepage",
+
+"current_user" =>
+"Aktueller Nutzer:",
+
+"system_status" =>
+"System-Status",
+
+"offline" =>
+"Offline",
+
+"online" =>
+"Online",
+
+"member_search" =>
+"Mitgliedersuche",
+
+"search_instructions" =>
+"Geben Sie ein ganzes Wort oder einen Teil davon ein",
+
+"member_group" =>
+"Mitgliedergruppe",
+
+"search_by" =>
+"Suchfeld",
+
+"screen_name" =>
+"Screen-Name",
+
+"email_address" =>
+"Email-Adresse",
+
+"url" =>
+"URL",
+
+"notepad" =>
+"Notizblock",
+
+"site_statistics" =>
+"Site-Statistik",
+
+"value" =>
+"Wert",
+
+"total_members" =>
+"Mitgliederanzahl",
+
+"total_validating_members" =>
+"Mitglieder im Wartestand",
+
+"total_entries" =>
+"Anzahl der Weblog-Eintr&auml;ge",
+
+"total_comments" =>
+"Anzahl der Kommentare",
+
+"total_trackbacks" =>
+"Anzahl der Trackbacks",
+
+"most_recent_entries" =>
+"Ihre neuesten Eintr&auml;ge",
+
+"most_recent_comments" =>
+"Neueste Kommentare/TB",
+
+"no_comments" =>
+"Zur Zeit gibt es keine Kommentare oder Trackbacks.",
+
+"no_entries" =>
+"Zur Zeit gibt es keine Weblogeintr&auml;ge.",
+
+"entry_title" =>
+"Titel des Eintrags",
+
+"comments" =>
+"Kommentare",
+
+"recent_members" =>
+"Neueste Mitglieder",
+
+"join_date" =>
+"Beitrittsdatum",
+
+"total_hits" =>
+"Anzahl aller Page-Hits",
+
+"demo_expiration" =>
+"Ihr Demo-Account erlischt in:",
+
+"install_lock_warning" =>
+"Warnung: Ihre Installationsdatei ist noch auf dem Server.",
+
+"install_lock_removal" =>
+"Entfernen Sie aus Sicherheitsgr&uuml;nden die Datei install.php vom Server.",
+
+// END
+''=>''
+);
+?>
