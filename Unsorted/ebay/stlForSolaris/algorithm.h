3e5cb9cbe137f28e86bba101010778697fba185b
me-suzy <me.suzana@gmail.com>
2025-08-13 03:35:46 +0300

    Add Unsorted/ebay/stlForSolaris/algorithm.h

---
+++ Unsorted/ebay/stlForSolaris/algorithm.h
@@ -0,0 +1,27 @@
+/*	$Id: algorithm.h,v 1.3 1998/12/17 04:13:29 josh Exp $	*/
+/*
+ *
+ * Copyright (c) 1997
+ * Moscow Center for SPARC Technology
+ *
+ * Permission to use, copy, modify, distribute and sell this software
+ * and its documentation for any purpose is hereby granted without fee,
+ * provided that the above copyright notice appear in all copies and
+ * that both that copyright notice and this permission notice appear
+ * in supporting documentation.  Moscow Center for SPARC Technology makes no
+ * representations about the suitability of this software for any
+ * purpose.  It is provided "as is" without express or implied warranty.
+ *
+ */
+
+// New style C++ header.
+// suppresses "using std;" clause.
+
+#ifndef __ALGORITHM__
+# define __ALGORITHM__
+# ifndef  __NO_USING_STD
+#  define __NO_USING_STD
+# endif
+#include <algo.h>
+#endif
+
