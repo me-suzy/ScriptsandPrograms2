eb26d683edca7e08a762d0a472110267365c9999
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:35 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-lib_inc-jscript.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-lib_inc-jscript.php
@@ -0,0 +1,16 @@
+<?php
+
+##################################################
+
+# /lib/inc-jscript.php
+
+# UNUSED SINCE 1.0b9, VALUES NOW IN inc-lib_inc-common.php
+
+##################################################
+
+define( "JSCRIPT_PLIST_1", "Veuillez sélectionner une liste éxistante, ou choisissez quelques pistes, et créez en une nouvelle." );
+define( "JSCRIPT_PLIST_2", "Vous n\\'avez sélectionné aucune piste dans la liste ci-dessous." );
+
+##################################################
+
+?>
