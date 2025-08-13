fdb2d19c9185c7c5ac997c11be5b887729c6b603
me-suzy <me.suzana@gmail.com>
2025-08-13 03:33:27 +0300

    Add Unsorted/ebay/sql/feedbackd01.sql

---
+++ Unsorted/ebay/sql/feedbackd01.sql
@@ -0,0 +1,12 @@
+/*	$Id: feedbackd01.sql,v 1.2 1999/02/21 02:54:26 josh Exp $	*/
+/*
+** feedbackd01.sql
+**
+** Create tablespace for feedback data
+** obsolete - see tablespaces.sql
+**
+create tablespace feedbackd01
+	datafile '/oracle02/ebay/oradata/feedbackd01.dbf'
+	size 100M
+	autoextend off;
+*/
