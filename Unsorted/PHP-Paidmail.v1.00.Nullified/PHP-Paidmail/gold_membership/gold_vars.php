7c61716251c3584dfa3d3a4b3a0380fb9506fd30
me-suzy <me.suzana@gmail.com>
2025-08-13 07:46:57 +0300

    Add Unsorted/PHP-Paidmail.v1.00.Nullified/PHP-Paidmail/gold_membership/gold_vars.php

---
+++ Unsorted/PHP-Paidmail.v1.00.Nullified/PHP-Paidmail/gold_membership/gold_vars.php
@@ -0,0 +1,29 @@
+<?
+       //include("../includes/vars.php");
+
+       //paths to folders
+       $gold_url="http://yourdomain.com/gold_membership"; 
+  //virtual path of the site folder    
+       $gold_path="/home/www/gold_membership";  
+//physical path of the site folder  $sitepath
+
+       $gold_admin_url="$gold_url/admin";
+       $gold_image_url="$gold_url/images";
+
+       $gold_html_path="$gold_path/html";
+
+
+       $from_email_id_for_regn="admin@yourdomain.com";
+
+       $gold_email_regn_subject="Gold Membership Purchased"; //Member Registeration email message subject
+       $email_subject_gold_member_regn = "Gold Membership Purchased";
+
+
+
+//##########DO Not Edit Anything Below This##############
+
+        $gold_EmailId=Array();
+        $gold_Info=Array();
+        $gold_Date=Array();
+
+?>
