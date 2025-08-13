0e785c2f10c13375b1b934b05b3649ab13576f60
me-suzy <me.suzana@gmail.com>
2025-08-13 05:14:22 +0300

    Add Unsorted/HiveMail.v1.30.Beta.2.English.Retail/hivemail13b2/upload/install/sql_upgrade_2.php

---
+++ Unsorted/HiveMail.v1.30.Beta.2.English.Retail/hivemail13b2/upload/install/sql_upgrade_2.php
@@ -0,0 +1,22 @@
+<?php
+// +-------------------------------------------------------------+
+// | HiveMail version 1.3 Beta 2 (English)
+// | Copyright ©2002-2003 Chen Avinadav
+// | Supplied by Scoons [WTN]
+// | Nullified by CyKuH [WTN]
+// | Distribution via WebForum, ForumRU and associated file dumps
+// +-------------------------------------------------------------+
+// | HIVEMAIL IS NOT FREE SOFTWARE
+// | If you have downloaded this software from a website other
+// +-------------------------------------------------------------+
+// +-------------------------------------------------------------+
+
+// ############################################################################
+// Table structure
+$tables[2] = array();
+
+// ############################################################################
+// Records
+$inserts[2] = array();
+
+?>
