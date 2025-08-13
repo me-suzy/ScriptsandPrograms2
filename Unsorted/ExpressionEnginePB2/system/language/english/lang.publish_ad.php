8a66a467f549bd3fe1d9f5c42f48534cb9f23b0d
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:24 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/english/lang.publish_ad.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/english/lang.publish_ad.php
@@ -0,0 +1,697 @@
+<?php
+
+$L = array(
+
+
+//----------------------------
+// Weblog Administration
+//----------------------------
+
+"search_excerpt" =>
+"Field for Search Excerpt",
+
+"weblog_prefs" => 
+"Weblog Preferences",
+
+"weblog_settings" =>
+"Weblog Posting Preferences",
+
+"comment_prefs" => 
+"Comment Posting Preferences",
+
+"comment_moderate" =>
+"Moderate Comments?",
+
+"comment_moderate_exp" =>
+"If set to yes, comments will not be visible until a moderator approves them.",
+
+"comment_system_enabled" =>
+"Allow comments in this weblog?",
+
+"edit_weblog_prefs" => 
+"Edit Weblog Preferences",
+
+"edit_group_prefs" => 
+"Edit Group Preferences",
+
+"no_weblogs_exist" => 
+"There are currently no weblogs",
+
+"create_new_weblog" => 
+"Create a New Weblog",
+
+"weblog_base_setup" =>
+"General Weblog Settings",
+
+"default_settings" =>
+"Administrative Settings",
+
+"short_weblog_name" => 
+"Short Name",
+
+"blog_url" =>
+"Weblog URL",
+
+"blog_lang" =>
+"XML Language",
+
+"blog_encoding" =>
+"XML Character Encoding",
+
+"blog_description" =>
+"Weblog Description",
+
+"illegal_characters" =>
+"The name you submitted may only contain alpha-numeric characters, spaces, underscores, and dashes",
+
+"comment_require_membership" =>
+"Require membership in order to post comments?",
+
+"weblog_require_membership" =>
+"Require membership in order to post weblog entries?",
+
+"comment_require_email" =>
+"Require email address to post comments?",
+
+"weblog_require_email" =>
+"Require email address to post weblog entries?",
+
+"weblog_max_chars" =>
+"Maximum number of characters allowed in weblog entries",
+
+"comment_max_chars" =>
+"Maximum number of characters allowed in comments",
+
+"comment_timelock" =>
+"Comment Time Interval",
+
+"comment_timelock_desc" =>
+"The number of seconds that must pass before a user can submit another comment.  Leave blank or set to zero for no limit.",
+
+"comment_text_formatting" =>
+"Comment Text Formatting",
+
+"weblog_text_formatting" =>
+"Default text formatting in weblog entries",
+
+"comment_html_formatting" =>
+"Comment HTML Formatting",
+
+"weblog_html_formatting" =>
+"Default HTML formatting in weblog entries",
+
+"convert_to_entities" =>
+"Convert HTML into character entities",
+
+"allow_safe_html" =>
+"Allow only safe HTML",
+
+"allow_all_html" =>
+"Allow ALL HTML",
+
+"allow_all_html_not_recommended" =>
+"Allow all HTML (not recommended)",
+
+"comment_notify" =>
+"Notify Admin of Comments and Trackbacks?",
+
+"comment_notify_note" =>
+"Separate multiple emails with a comma",
+
+"comment_notify_emails" =>
+"Email Address of Notification Recipient(s)",
+
+"comment_allow_img_urls" =>
+"Allow image URLs in comments?",
+
+"weblog_allow_img_urls" =>
+"Allow image URLs in weblog entries?",
+
+"auto_link_urls" =>
+"Automatically turn URLs and email addresses into links?",
+
+"single_word_no_spaces" => 
+"single word, no spaces",
+
+"full_weblog_name" => 
+"Full Weblog Name",
+
+"edit_weblog" => 
+"Edit Weblog",
+
+"weblog_name" => 
+"Weblog Name",
+
+"new_weblog" => 
+"New Weblog",
+
+"weblog_created" => 
+"Weblog Created:",
+
+"weblog_updated" => 
+"Weblog Updated:",
+
+"taken_weblog_name" => 
+"This weblog name is already taken.",
+
+"no_weblog_name" => 
+"You must give your weblog a \"short\" name.",
+
+"no_weblog_title" =>
+"You must give your weblog  a \"full\" name.",
+
+"invalid_short_name" => 
+"Your weblog name must contain only alpha-numeric characters and no spaces.",
+
+"delete_weblog" => 
+"Delete Weblog",
+
+"weblog_deleted" => 
+"Weblog Deleted:",
+
+"delete_weblog_confirmation" =>
+"Are you sure you want to permanently delete this weblog?",
+
+"be_careful" =>
+"BE CAREFUL!",
+
+"action_can_not_be_undone" =>
+"THIS ACTION CAN NOT BE UNDONE",
+
+"assign_group_to_weblog" =>
+"Note: In order to use your new group, you must assign it to a weblog.",
+
+"click_to_assign_group" =>
+"Click here to assign it",
+
+"default" =>
+"Default",
+
+"category" =>
+"Category",
+
+"default_status" =>
+"Default Status",
+
+"default_category" =>
+"Default Category",
+
+"deft_comments" =>
+"Check \"Allow Comments\" button in Publish page by default?",
+
+"deft_trackbacks" =>
+"Check \"Allow Trackbacks\" button in Publish page by default?",
+
+"enable_trackbacks" =>
+"Add Trackback Auto-discovery code to your pages?",
+
+"trackback_max_hits" =>
+"Maximum number of allowed trackback pings per hour",
+
+"trackback_field" =>
+"Which field should be used for Trackbacks excerpt?",
+
+"no_field_group_selected" =>
+"No field group available for this weblog",
+
+"missing_weblog_data_for_pings" =>
+"In order to send pings or trackbacks, your weblog needs a title and  URL.  Please update your weblog preferences.",
+
+"open" => 
+"Open",
+
+"closed" => 
+"Closed",
+
+"none" =>
+"None",
+
+"define_html_buttons" =>
+"HTML Formatting Button Defenitions",
+
+"htmlbutton_delete_instructions" =>
+"To delete an item submit the form with the tag name blank",
+
+"tag_name" =>
+"Tag Name",
+
+"tag_open" =>
+"Opening Tag",
+
+"tag_close" =>
+"Closing Tag",
+
+"accesskey" =>
+"Shortcut",
+
+"tag_order" =>
+"Order",
+
+"row" =>
+"Row",
+
+"server_name" =>
+"Server Name",
+
+"server_url" =>
+"Server URL/Path",
+
+"port" =>
+"Port",
+
+"protocol" =>
+"Protocol",
+
+"is_default" =>
+"Default",
+
+"server_order" =>
+"Order",
+
+"define_ping_servers" =>
+"These forms allows you to define a list of servers that can be pinged when you submit weblog entries",
+
+"pingserver_delete_instructions" =>
+"To delete an item submit the form with the server name blank",
+
+"assign_weblogs" =>
+"Choose which weblog(s) you want this group assigned to",
+
+//----------------------------
+// Category Administration
+//----------------------------
+
+"category_group" =>
+"Category Group",
+
+"category_groups" =>
+"Category Groups",
+
+"none" =>
+"None",
+
+"no_category_group_message" =>
+"There are currently no categories",
+
+"no_category_message" =>
+"There are currently no categories assigned to this group",
+
+"create_new_category_group" =>
+"Create a New Category Group",
+
+"edit_category_group" =>
+"Edit Category Group",
+
+"name_of_category_group" =>
+"Name of category group",
+
+"taken_category_group_name" => 
+"This group name is already taken.",
+
+"add_edit_categories" => 
+"Add/edit Categories",
+
+"edit_group_name" => 
+"Edit Group",
+
+"delete_group" => 
+"Delete Group",
+
+"category_group_created" => 
+"Category Group Created:",
+
+"category_group_updated" => 
+"Group Updated:",
+
+"delete_cat_group_confirmation" =>
+"Are you sure you want to permanently delete this category group?",
+
+"category_group_deleted" => 
+"Category Group Deleted:",
+
+"create_new_category" =>
+"Create a New Category",
+
+"add_new_category" =>
+"Add a New Category",
+
+"edit_category" =>
+"Edit Category",
+
+"delete_category" =>
+"Delete Category",
+
+"category_name" =>
+"Category Name",
+
+"category_image" =>
+"Category Image URL",
+
+"category_img_blurb" =>
+"This is an optional field that enables you to assign an image to your categories.",
+
+"category_parent" =>
+"Category Parent",
+
+
+//----------------------------
+// Custom field Administration
+//----------------------------
+
+"field_group" =>
+"Custom Field Group",
+
+"field_groups" =>
+"Field Groups",
+
+"field_group_name" =>
+"Field Group Name",
+
+"custom_fields" =>
+"Custom Fields",
+
+"no_field_group_message" =>
+"There are currently no custom weblog fields",
+
+"create_new_field_group" =>
+"Create a New Weblog Field Group",
+
+"new_field_group" =>
+"New Field Group",
+
+"add_edit_fields" => 
+"Add/Edit Custom Fields",
+
+"edit_field_group_name" =>
+"Edit Field Group",
+
+"delete_field_group" =>
+"Delete Field Group",
+
+"create_new_field" =>
+"Create a New Field",
+
+"edit_field" =>
+"Edit Field",
+
+"no_field_groups" =>
+"There are no custom fields in this group",
+
+"delete_field" =>
+"Delete Field",
+
+"field_deleted" =>
+"Custom Field Deleted:",
+
+"edit_field_order" =>
+"Change Field Order",
+
+"create_new_custom_field" =>
+"Create a New Custom Field",
+
+"field_label" =>
+"Field Label",
+
+"field_name" =>
+"Field Name",
+
+"field_name_cont" =>
+"Single word, no spaces. Underscores and dashes allowed",
+
+"field_type" =>
+"Field Type",
+
+"field_max_length" =>
+"Maxlength",
+
+"field_max_length_cont" =>
+"If you chose text field type",
+
+"textarea_rows" =>
+"Textarea Rows",
+
+"textarea_rows_cont" =>
+"If you chose textarea field type",
+
+"field_list_items" =>
+"Select Options",
+
+"field_list_items_cont" =>
+"If you chose drop-down menu",
+
+"field_list_instructions" =>
+"Put each item on a single line",
+
+"field_formatting" =>
+"Field Formatting",
+
+"field_formatting_cont" =>
+"If you chose textarea field type",
+
+"field_order" =>
+"Field Display Order",
+
+"is_field_searchable" =>
+"Is field searchable?",
+
+"is_field_required" =>
+"Is this a required field?",
+
+"text_input" =>
+"Text Input",
+
+"textarea" =>
+"Textarea",
+
+"select_list" =>
+"Drop-down List",
+
+"auto_br" =>
+"Auto &lt;br /&gt;",
+
+"xhtml" =>
+"xhtml",
+
+"no_field_name" =>
+"You must submit a field name",
+
+"no_field_label" =>
+"You must submit a field label",
+
+"invalid_characters" =>
+"The field name you submitted contains invalid characters",
+
+"custom_field_empty" =>
+"The following field is required:",
+
+"duplicate_field_name" =>
+"The field name you chose is already taken",
+
+"taken_field_group_name" =>
+"The name you have chosen is already taken",
+
+"field_group_created" =>
+"Field Group Created:",
+
+"field_group_updated" =>
+"Field Group Updated:",
+
+"field_group_deleted" =>
+"Field group Deleted:",
+
+"field_group" =>
+"Field Group",
+
+"delete_field_group_confirmation" =>
+"Are you sure you want to permanently delete this custom field group?",
+
+"delete_field_confirmation" =>
+"Are you sure you want to permanently delete this custom field?",
+
+"weblog_entries_will_be_deleted" =>
+"All weblog entries contained in the above field(s) will be permanently deleted.",
+
+
+//----------------------------
+// Status Administration
+//----------------------------
+
+"status_group" =>
+"Status Group",
+
+"status_groups" =>
+"Status Groups",
+
+"no_status_group_message" =>
+"There are currently no custom statuses",
+
+"create_new_status_group" =>
+"Create New Status Group",
+
+"edit_status_group" =>
+"Edit Status Group",
+
+"name_of_status_group" =>
+"Name of Status Group",
+
+"taken_status_group_name" => 
+"This status group name is already taken.",
+
+"invalid_status_name" => 
+"Status names can only have alpha-numeric characters, as well as spaces, underscores and hyphens.",
+
+"duplicate_status_name" =>
+"A status already exists with the same name.",
+
+"status_group_created" => 
+"Status Group Created:",
+
+"new_status" => 
+"New Status",
+
+"status_group_updated" => 
+"Status Group Updated:",
+
+"add_edit_statuses" => 
+"Add/Edit Statuses",
+
+"edit_status_group_name" => 
+"Edit Status Group",
+
+"delete_status_group" => 
+"Delete Status Group",
+
+"delete_status_group_confirmation" =>
+"Are you sure you want to permanently delete this status group?",
+
+"status_group_deleted" => 
+"Status Group Deleted:",
+
+"create_new_status" => 
+"Create a New Status",
+
+"status_name" => 
+"Status Name",
+
+"status_order" =>
+"Status Order",
+
+"change_status_order" =>
+"Change Status Order",
+
+"highlight" =>
+"Highlight Color (optional)",
+
+"statuses" => 
+"Statuses",
+
+"edit_status" =>
+"Edit Status",
+
+"delete_status" =>
+"Delete Status",
+
+"delete_status_confirmation" =>
+"Your you sure you want to delete the following status?",
+
+"edit_file_upload_preferences" =>
+"Edit File Upload Preferences",
+
+"new_file_upload_preferences" =>
+"New File Upload Preferences",
+
+"file_upload_preferences" =>
+"File Upload Preferences",
+
+"no_upload_prefs" =>
+"There are currently no file upload preferences",
+
+"create_new_upload_pref" =>
+"Create New Upload Destination",
+
+"upload_pref_name" =>
+"Descriptive name of upload directory",
+
+"new_file_upload_preferences" =>
+"New File Upload Destination",
+
+"server_path" =>
+"Server Path to Upload Directory",
+
+"url_to_upload_dir" =>
+"URL of Upload Directory",
+
+"allowed_types" =>
+"Allowed File Types",
+
+"max_size" =>
+"Maximum File Size (in bytes)",
+
+"max_height" =>
+"Maximum Image Height (in pixels)",
+
+"max_width" =>
+"Maximum Image Width",
+
+"properties" =>
+"Image Properties",
+
+"pre_format" =>
+"Pre Formatting",
+
+"post_format" =>
+"Post Formatting",
+
+"no_upload_dir_name" =>
+"You must submit a name for your upload directory",
+
+"no_upload_dir_path" =>
+"You must submit the path to your upload directory",
+
+"no_upload_dir_url" =>
+"You must submit the URL your upload directory",
+
+"duplicate_dir_name" =>
+"The name of your directory is already taken",
+
+"delete_upload_preference" =>
+"Delete Upload Preference",
+
+"delete_upload_pref_confirmation" =>
+"Are you sure you want to permanently delete this preference?",
+
+"upload_pref_deleted" =>
+"Upload Preference Deleted:",
+
+"current_upload_prefs" =>
+"Current Preferences",
+
+"restrict_to_group" =>
+"Restrict file uploading to select member groups",
+
+"restrict_notes_1" =>
+"These radio buttons let you to specify who can upload files and images and display them in their weblog entries",
+
+"restrict_notes_2" =>
+"Super Admins can always upload files",
+
+"restrict_notes_3" =>
+"Note: File uploading is currently only allowed via the control panel",
+
+"member_group" =>
+"Member Group",
+
+"can_upload_files" =>
+"Can upload files",
+
+"images_only" =>
+"Images only",
+
+"all_filetypes" =>
+"All file types",
+
+
+// END
+''=>''
+);
+?>
