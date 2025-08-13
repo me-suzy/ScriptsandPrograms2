71bacd6c59fea29206b3d5d7c83281fbcf9337e0
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:03 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-play.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-play.php
@@ -0,0 +1,13 @@
+<?php
+
+##################################################
+
+# /play.php
+
+##################################################
+
+define( "PLAYER_NOID", "Bitte wählen sie mindestens einen Titel oder Spielliste zum anhören aus." );
+
+##################################################
+
+?>
