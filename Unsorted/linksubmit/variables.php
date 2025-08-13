5dcfddcb6c4177fe9ba02548c28d578237b24043
me-suzy <me.suzana@gmail.com>
2025-08-13 06:03:52 +0300

    Add Unsorted/linksubmit/variables.php

---
+++ Unsorted/linksubmit/variables.php
@@ -0,0 +1,37 @@
+<?php
+
+// This is the page where you will put your free for fall links on. This is a URL!
+$viewpage = "index.php";
+
+// The directory to where you will upload your images. DO NOT END WITH TRAILING SLASH!
+$imagedir = "images";
+
+// Website Title
+$title = "Title Of Link Page";
+
+// Text 1 for top of pages
+$text1 = "Your Text Here";
+
+// Text 2 for top of pages
+$text2 = "Second Line of Text";
+
+// Link for Top Of Page
+$url1 = "http://www.yahoo.com";
+
+//URL 1 Text
+$urltext1 = "Text Of URL";
+
+// Text 1 for bottom of pages
+$text3 = "Your Text Here";
+
+// Text 2 for bottom of pages
+$text4 = "Second Line of Text";
+
+// Link for Top Of Page
+$url2 = "http://www.google.com";
+
+//URL 1 Text
+$urltext2 = "Text Of URL";
+
+
+?>
