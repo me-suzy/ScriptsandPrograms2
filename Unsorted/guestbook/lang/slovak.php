21afeb9a462b40ecb5de612461abd55fe258fe5d
me-suzy <me.suzana@gmail.com>
2025-08-13 05:01:48 +0300

    Add Unsorted/guestbook/lang/slovak.php

---
+++ Unsorted/guestbook/lang/slovak.php
@@ -0,0 +1,107 @@
+<?php
+
+// language: slovak
+// translator: Ondrej Jombík <nepto@pobox.sk>
+// date: 30/3/2002
+// encoding: iso-8859-2
+
+# General
+
+$LANG["metatag"]    = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-2\">";
+
+$LANG["BookMess1"]  = "HTML kód je vypnutý";
+$LANG["BookMess2"]  = "HTML kód je zapnutý";
+$LANG["BookMess3"]  = "Nový príspevok do Knihy náv¹tev";
+$LANG["BookMess4"]  = "Spä» do Knihy náv¹tev";
+$LANG["BookMess5"]  = "Administrácia";
+$LANG["BookMess6"]  = "Ïakujem Vám za náv¹tevu mojej stránky. Tu je miesto kde mi mô¾ete necha» krátky odkaz.";
+$LANG["BookMess7"]  = "Komentár";
+$LANG["BookMess8"]  = "Spä» do formuláru";
+$LANG["BookMess9"]  = "Napísa» komentár k tomuto príspevku.";
+$LANG["BookMess10"] = "<u>Ïakujem za Vá¹ príspevok do Knihy náv¹tev.</u><br><br>Záznam bol úspe¹ne ulo¾ený do databázy. V priebehu dvoch sekúnd by ste mali by» presmerovaný naspä» do Knihy náv¹tev.";
+
+$LANG["EmailAdminSubject"] = "Nový príspevok";
+$LANG["EmailGuestSubject"] = "Poïakovanie za Vá¹ príspevok do Knihy náv¹tev";
+
+$LANG["AltIP"]    = "IP adresa zaznamenaná";
+$LANG["AltICQ"]   = "Posla» ICQ správu";
+$LANG["AltEmail"] = "Posla» e-mail";
+$LANG["AltUrl"]   = "Pozrie» web stránku";
+$LANG["AltAim"]   = "Instant Messenger";
+$LANG["AltCom"]   = "Napísa» komentár";
+
+# Form
+$LANG["FormMess1"]   = "Vyplòte ni¾¹ieuvedený formulár pre zaradenie príspevku do Knihy náv¹tev. Po potvrdení budete do Knihy náv¹tev opätovne presmerovaný. Polia oznaèené (*) sú povinné.";
+$LANG["FormMess2"]   = "Emotikony sú zapnuté";
+$LANG["FormMess3"]   = "AG kód je zapnutý";
+$LANG["FormMess4"]   = "Ukáza» legendu";
+$LANG["FormMess5"]   = "Takto vyzerá Vá¹ príspevok do Knihy náv¹tev.";
+$LANG["FormMess6"]   = "AG kód je vypnutý";
+$LANG["FormMess7"]   = "Emotikony sú vypnuté";
+$LANG["FormName"]    = "Meno";
+$LANG["FormEmail"]   = "E-mail";
+$LANG["FormLoc"]     = "Krajina";
+$LANG["FormPic"]     = "Obrázok";
+$LANG["FormUrl"]     = "Web stránka";
+$LANG["FormGender"]  = "Pohlavie";
+$LANG["FormMale"]    = "Mu¾";
+$LANG["FormFemale"]  = "®ena";
+$LANG["FormMessage"] = "Va¹a správa";
+$LANG["FormSelect"]  = "Kniha náv¹tev";
+$LANG["FormUser"]    = "U¾ívateµské meno";
+$LANG["FormPass"]    = "Heslo";
+$LANG["FormReset"]   = "Vymaza»";
+$LANG["FormSubmit"]  = "Potvrdi»";
+$LANG["FormPreview"] = "Ukáza»";
+$LANG["FormBack"]    = "Upravi»";
+$LANG["FormEnter"]   = "Kniha náv¹tev - Administraèné centrum<br>Musíte zada» platné u¾ívateµské meno a heslo pre vstup do systému.";
+$LANG["FormButton"]  = "Choï";
+$LANG["FormPriv"]    = "Súkromná správa";
+
+# Navigation Bar
+$LANG["NavTotal"]   = "Záznamov celkom:";
+$LANG["NavRecords"] = "Záznamov na stránke:";
+$LANG["NavPrev"]    = "Predchádzajúca stránka";
+$LANG["NavNext"]    = "Nasledujúca stránka";
+
+# Post Errors
+$LANG["ErrorPost1"]  = "Zabudli ste vyplni» pole s menom. Prosím, opravte chybu a potvrïte formulár znovu.";
+$LANG["ErrorPost2"]  = "Zabudli ste vyplni» pole s príspevkom. Prosím, opravte chybu a potvrïte formulár znovu.";
+$LANG["ErrorPost3"]  = "Va¹a správa je príli¹ krátka alebo veµmi dlhá. Prosím, opravte chybu a potvrïte formulár znovu.";
+$LANG["ErrorPost4"]  = "Niektorý zo vstupných údajov nie je správne vypnený.";
+$LANG["ErrorPost5"]  = "Kniha náv¹tev - Chyba";
+$LANG["ErrorPost6"]  = "Súbor s obrázkom je príli¹ veµký";
+$LANG["ErrorPost7"]  = "Nesprávny formát súboru s obrázkom";
+$LANG["ErrorPost8"]  = "Prepáète, máme aktivovanú anti-flood ochranu. Prosím, vyskú¹ajte posla» Vá¹ príspevok o malú chvíµu.";
+$LANG["ErrorPost9"]  = "Va¹a IP adresa bola zablokovaná!";
+$LANG["ErrorPost10"] = "Vá¹ príspevok obsahuje niektoré zo zakázaných slov. Prosím, opravte chybu a potvrïte formulár znovu.";
+$LANG["ErrorPost11"]  = "Zabudli ste vyplni» pole s komentárom. Prosím, opravte chybu a potvrïte formulár znovu.";
+
+$LANG["PassMess1"] = "Prosím zadajte platné u¾ívateµské meno a heslo:";
+$LANG["PassMess2"] = "Nesprávne u¾ívateµské meno alebo heslo. Prosím, skúste znovu.";
+$LANG["PassMess3"] = "¥utujem, ale zadané heslo nie je správne.";
+
+# Days
+$weekday[0] = "Nedeµa";
+$weekday[1] = "Pondelok";
+$weekday[2] = "Utorok";
+$weekday[3] = "Streda";
+$weekday[4] = "©tvrtok";
+$weekday[5] = "Piatok";
+$weekday[6] = "Sobota";
+
+# Months
+$months[0]  = "Január";
+$months[1]  = "Február";
+$months[2]  = "Marec";
+$months[3]  = "Apríl";
+$months[4]  = "Máj";
+$months[5]  = "Jún";
+$months[6]  = "Júl";
+$months[7]  = "August";
+$months[8]  = "September";
+$months[9]  = "Október";
+$months[10] = "November";
+$months[11] = "December";
+
+?>
