c24bdee09eac8872a85e44c02a64c34a983914e0
me-suzy <me.suzana@gmail.com>
2025-08-13 06:31:31 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/common.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/common.php
@@ -0,0 +1,78 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[COMMON_Registered_Only] = "Est pour les utilisateurs enregistrés seulement.";
+
+$LOCALE[COMMON_Unregistered_Only] = "Est pour les utilisateurs non matriculés seulement.";
+	
+$LOCALE[COMMON_Username] = "Username";
+
+$LOCALE[COMMON_Function_Invalid] = "Est un ordre nul ou est indisponible à vous.";
+
+$LOCALE[COMMON_Operator] = "A Eté fait un opérateur.";
+
+$LOCALE[COMMON_No_Operator] = "Est non plus long un opérateur.";
+
+$LOCALE[COMMON_Login_Button] = "Login maintenant";
+
+$LOCALE[COMMON_Not_Accept] = "N'accepter pas de messages privés.";
+
+$LOCALE[COMMON_Help] = "Aide";
+
+$LOCALE[COMMON_No_Encryption] = "MWChat vous a détecté n'a pas mcrypt installé dans php, le cryptage qui rendre infirme.";
+
+$LOCALE[COMMON_System_Message] = "Message de système";
+
+$LOCALE[COMMON_Click_Close] = "Le déclic pour fermer";
+
+$LOCALE[COMMON_YOB] = "L'année de naissance";
+
+$LOCALE[COMMON_Location] = "Emplacement";
+
+$LOCALE[COMMON_Job] = "Occupation";
+
+$LOCALE[COMMON_Hobbies] = "Passe-temps";
+
+$LOCALE[COMMON_Status] = "Statut";
+
+$LOCALE[COMMON_Comments] = "Commentaires supplémentaires";
+
+$LOCALE[COMMON_Password] = "Nouveau mot de passe";
+
+$LOCALE[COMMON_Confirm_Password] = "Confirmer le nouveau mot de passe";
+
+$LOCALE[COMMON_Logout] = "La sortie du système de MWChat";
+
+$LOCALE[COMMON_Err_Pass] = "Les mots de passe que vous n'êtes pas entré l'allumette ou n'êtes pas au moins cinq caractères dans la longueur.";
+
+$LOCALE[COMMON_HelpClick] = "Cliqueter ici pour l'aide";
+
+$LOCALE[COMMON_Text] = "Message";
+
+$LOCALE[COMMON_Bug_Report] = "Description d'insecte";
+
+$LOCALE[COMMON_Reason] = "Raison";
+
+$LOCALE[COMMON_Action] = "Action";
+
+$LOCALE[COMMON_Topic] = "Sujet";
+
+$LOCALE[COMMON_URL] = "URL";
+
+$LOCALE[COMMON_Alias] = "est un alias pour l'ordre";
+
+$LOCALE[COMMON_Room_Name] = "Nom de salle";
+
+$LOCALE[COMMON_Topic] = "Aucun";
+
+?>
