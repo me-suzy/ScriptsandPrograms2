55fbb9e995959d02540f890fc7745dd252087dce
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:34 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_inc-admin.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_inc-admin.php
@@ -0,0 +1,14 @@
+<?php
+
+##################################################
+
+# UNUSED SINCE 1.0b8, NOW /lib/inc-admin.php
+
+##################################################
+
+define( "INCADM_DENIED", "Acceso Denegado! Tu no Puedes Editar esta Pista." );
+define( "INCADM_NOPERM", "Error de permiso con el netjuke/var/data/import directorio." );
+
+##################################################
+
+?>
