f39b179029956115164f80c13d07bd72c4ebe221
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:09 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_installer.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_installer.php
@@ -0,0 +1,43 @@
+<?php
+
+##################################################
+
+# UNUSED SINCE 1.0b8, NOW /installer/installer.php
+
+##################################################
+
+define( "ADMINST_ERR_VERSION", "Dieses Installationsskript kann nicht mit der gewählten \\nKopie von Netjuke verwendet werden. Die Versionen stimmen nicht überein.\\nBitte besorgen Sie sich die neuste Version und versuchen Sie es erneut." );
+
+define( "ADMINST_ERR_FSPATH_1", "Bitte geben sie das Verzeichniss der Netjuke-Installation an." );
+define( "ADMINST_ERR_FSPATH_2", "Der Installationspfad darf nicht :// enthalten" );
+define( "ADMINST_ERR_WEBPATH_1", "Bitte geben sie den Web-Pfad für Netjuke ein." );
+define( "ADMINST_ERR_WEBPATH_2", "Der Web-Pfad muss http:// oder https:// enthalten" );
+
+define( "ADMINST_ERR_DBTYPE", "Bitte geben sie einen DB Typ an." );
+define( "ADMINST_ERR_DBHOST", "Bitte geben sie einen DB Host an." );
+define( "ADMINST_ERR_DBNAME", "Bitte geben sie einen DB Name an." );
+define( "ADMINST_ERR_DBCONN", "Kann nicht mit der Datenbank verbinden." );
+
+define( "ADMINST_ERR_EMAIL", "Bitte geben Sie die email-Adresse des Netjuke-Administrator ein." );
+define( "ADMINST_ERR_PASS", "Bitte geben Sie ein Passwort für den Netjuke-Administrator ein." );
+
+define( "ADMINST_ERR_STREAM", "Bitte geben Sie den Pfad des Streaming Server ein." );
+
+define( "ADMINST_ERR_NOSQL", "Der Installation fehlen wichtige Komponenten." );
+
+define( "ADMINST_ERR_DENIED_1", "- Entschuldigung, aber sie können das Installationsskript nicht mit der gewählten Kopier von Netjuke verwenden, da diese bereits läuft.\\n- Bitte benutzen Sie statt dessen das Upgrade-Skript." );
+define( "ADMINST_ERR_DENIED_2", "- Dieses Upgrade-Skript kann nicht mit der gewählten Kopie von Netjuke benutzt werden..\\n- Die Datenbank wurde nie generiert (Installation notwendig)." );
+define( "ADMINST_ERR_DENIED_3", "Der Installations-Pfad scheint ungültig zu sein." );
+define( "ADMINST_ERR_DENIED_4", "Zugriff verweigert: Der angegebene Admin-Benutzer und das Passwort passen zu keinem bestehenden Administrator-Account." );
+
+define( "ADMINST_ERR_EXEC", "Problem beim ausführend des erzeugten sql.\\nBitte erneut versuchen." );
+
+define( "ADMINST_FILEINFO_1", "Erstellt" );
+define( "ADMINST_FILEINFO_2", "Von" );
+define( "ADMINST_FILEINFO_3", "Speichern unter" );
+define( "ADMINST_FILEINFO_4", "nach dem umbennen des temporären Installations-Verzeichnisses =" );
+define( "ADMINST_FILEINFO_5", "Erfolgreich gespeichert.Sie können nun auf die neue Version zugreifen, unter:" );
+
+##################################################
+
+?>
