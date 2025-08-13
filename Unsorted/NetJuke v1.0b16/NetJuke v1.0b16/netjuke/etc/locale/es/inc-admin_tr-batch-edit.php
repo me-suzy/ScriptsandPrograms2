23ac5372706973d72a3c9d06c6ab65a6a4d6beb9
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:56 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_tr-batch-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_tr-batch-edit.php
@@ -0,0 +1,40 @@
+<?php
+
+##################################################
+
+# /admin/tr-batch-edit.php
+
+##################################################
+
+define( "TRBEDIT_NOTRID", "Por favor, seleccione un mínimo de una canción para poder procesar." );
+
+define( "TRBEDIT_EDITED_MSG", "La selección de las canciones han sido procesadas satisfactoriamente." );
+
+define( "TRBEDIT_HEADER", "EDITAR INFORMACIÓN DE MULTIPLES CANCIONES" );
+define( "TRBEDIT_SELECTED", "seleccionadas" );
+
+define( "TRBEDIT_FORM_TR", "Nombre Canción" );
+define( "TRBEDIT_FORM_AR", "Nombre Canción" );
+define( "TRBEDIT_FORM_AL", "Nombre Álbum" );
+define( "TRBEDIT_FORM_GE", "Nombre género" );
+define( "TRBEDIT_FORM_FN", "Ubicación" );
+define( "TRBEDIT_FORM_LC", "Contador de veces escuchada" );
+define( "TRBEDIT_FORM_RESET", "Iniciar contador" );
+define( "TRBEDIT_FORM_TR_IMG", "Imagen canción" );
+
+define( "TRBEDIT_FORM_TR_COM", "Comentarios" );
+define( "TRBEDIT_FORM_TR_LYR", "Liricos" );
+
+define( "TRBEDIT_FORM_BTN_SAVE", "Guardar" );
+define( "TRBEDIT_FORM_BTN_RESET", "Limpiar" );
+
+define( "TRBEDIT_PLAY", "Sonar ahora" );
+define( "TRBEDIT_PLAY_HELP", "Click para Sonar estas canciones ahora." );
+define( "TRBEDIT_CLOSEWIN", "Cerrar ventana" );
+define( "TRBEDIT_CLOSEWIN_HELP", "Cerrar esta ventana" );
+
+define( "TRBEDIT_ERROR_INSERT", "Error al insertar" );
+
+##################################################
+
+?>
