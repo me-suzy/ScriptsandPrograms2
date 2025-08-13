9a2815cdcd6b388108c45afe15860a7d57fa92cc
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:20 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.myaccount.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.myaccount.php
@@ -0,0 +1,463 @@
+<?php
+
+$L = array(
+
+"preferences_updated" =>
+"Einstellungen aktualisiert",
+
+"homepage_preferences" =>
+"Homepage-Einstellungen",
+
+"set_display_order" =>
+"Anzeigereihenfolge festlegen",
+
+"please_update_order" =>
+"Bitte setzen Sie die Reihenfolge fest, in der Elemente angezeigt werden.",
+
+"left_column" =>
+"Linke Spalte",
+
+"right_column" =>
+"Rechte Spalte",
+
+"do_not_show" =>
+"Nicht anzeigen",
+
+"recent_entries" =>
+"Ihre zehn neuesten Eintr&auml;ge",
+
+"recent_comments" =>
+"Die zehn neuesten Kommentare",
+
+"recent_members" =>
+"Die zehn neuesten Anmeldungen",
+
+"new_release_indicator" =>
+"Anzeige f&uuml;r neuen Software-Release",
+
+"site_statistics" =>
+"Site-Statistiken",
+
+"member_search_form" =>
+"Mitglieder-Suchformular",
+
+"validating_members" =>
+"Mitglieder im Wartestand",
+
+"php_sql_versions" =>
+"PHP/SQL-Versionen",
+
+"system_offline_indicator" =>
+"Offline-Anzeige",
+
+"personal_settings" =>
+"Pers&ouml;nliche Einstellungen",
+
+"weblog_settings" =>
+"Weblog-Einstellungen",
+
+"control_panel_settings" =>
+"CP-Einstellungen",
+
+"administrative_settings" =>
+"Admin-Einstellungen",
+
+"current_member" =>
+"Mitglied:",
+
+"member_stats" =>
+"Account-Statistiken f&uuml;r:",
+
+"join_date" =>
+"Beitritt",
+
+"last_visit" =>
+"Letzter Besuch",
+
+"total_entries" =>
+"Anzahl Weblog-Eintr&auml;ge",
+
+"last_entry_date" =>
+"Datum des letzten Eintrags",
+
+"total_comments" =>
+"Anzahl Kommentare",
+
+"last_comment_date" =>
+"Datum des letzten Kommentars",
+
+"notepad" =>
+"Notizblock",
+
+"can_not_view_superadmin_profile" =>
+"Sie k&ouml;nnen das Profil eines Super-Admin nicht ansehen.",
+
+"only_self_notpad_access" =>
+"Sie haben keinen Zugang zum Notizblock eines anderen Mitglieds.",
+
+"notepad_size" =>
+"Notizblock-Gr&ouml;&szlig;e",
+
+"notepad_instructions" =>
+"Benutzen Sie dieses Formular, um Notizen zu speichen.",
+
+"notepad_updated" =>
+"Notizblock aktualisiert",
+
+"localization_settings" =>
+"Ortseinstellungen",
+
+"localization_updated" =>
+"Ortseinstellungen aktualisiert",
+
+"daylight_savings_time" =>
+"Sommerzeit",
+
+"timezone" =>
+"W&auml;hlen Sie Ihre Zeitzone",
+
+"language_choice" =>
+"W&auml;hlen Sie Ihre Sprache",
+
+"username_and_password" =>
+"Username und Passwort",
+
+"extras" =>
+"Extras",
+
+"edit_profile" =>
+"Profil bearbeiten",
+
+"profile_form" =>
+"Profil-Formular",
+
+"base_profile" =>
+"Einfaches Profil",
+
+"extended_profile" =>
+"Erweitertes Profil",
+
+"localization" =>
+"Ortseinstellungen",
+
+"time_format" =>
+"Format f&uuml;r Uhrzeit",
+
+"united_states" =>
+"United States",
+
+"european" =>
+"Europa",
+
+"your_ping_servers" =>
+"Ping-Server",
+
+"your_html_buttons" =>
+"HTML-Buttons",
+
+"change_password" =>
+"Passwort &auml;ndern",
+
+"email_settings" =>
+"E-Mail-Einstellungen",
+
+"subscriptions" =>
+"Subskriptionen",
+
+"edit_subscriptions" =>
+"Subskriptionen ansehen",
+
+"customize_homepage" =>
+"Control-Panel-Homepage anpassen",
+
+"customize_cp" =>
+"Control-Panel anpassen",
+
+"cp_homepage" =>
+"Control-Panel-Homepage",
+
+"cp_theme" =>
+"Control-Panel-Thema",
+
+"choose_theme" =>
+"W&auml;hlen Sie ein Thema f&uuml;r Ihr Control-Panel.",
+
+"administrative_options" =>
+"Administrative Einstellungen",
+
+"administrative_options_updated" =>
+"Administrative Einstellungen aktualisiert",
+
+"screen_name" =>
+"Screen-Name",
+
+"email" =>
+"E-Mail-Adresse",
+
+"url" =>
+"URL",
+
+"bio" =>
+"Bio",
+
+"birthday" =>
+"Geburtstag",
+
+"year" =>
+"Jahr",
+
+"month" =>
+"Monat",
+
+"day" =>
+"Tag",
+
+"accept_admin_email" =>
+"E-Mail von Administratoren annehmen",
+
+"accept_user_email" =>
+"E-Mail von anderen Mitgliedern annehmen",
+
+"notify_by_default" =>
+"E-Mail-Benachrichtigungen immer aktivieren, wenn Sie einen Eintrag posten",
+
+"location" =>
+"Ort",
+
+"occupation" =>
+"Beruf",
+
+"interests" =>
+"Interessen",
+
+"icq" =>
+"ICQ",
+
+"aol_im" =>
+"AOL IM",
+
+"yahoo_im" =>
+"Yahoo IM",
+
+"msn_im" =>
+"MSN IM",
+
+"user_ip_address" =>
+"IP-Adresse:",
+
+"member_group_assignment" =>
+"Zugewiesene Mitgliedsgruppe",
+
+"member_group_warning" =>
+"Vorsicht beim Zuweisen!",
+
+"include_in_multiauthor_list" =>
+"User in die Multi-Autoren-Liste auf der &quot;Ver&ouml;ffentlichen&quot;-Seite aufnehmen?",
+
+"profile_updated" =>
+"Profil aktualisiert",
+
+"super_admin_demotion_alert" =>
+"Als Super-Admin d&uuml;rfen Sie Ihre eigene Gruppenzugeh&ouml;rigkeit nicht &auml;ndern!",
+
+"username" =>
+"Username",
+
+"username_change_not_allowed" =>
+"Der Administrator erlaubt keine &Auml;nderung des Usernamens.",
+
+"password" =>
+"Passwort",
+
+"password_change" =>
+"Passwort-&Auml;nderungs-Formular",
+
+"new_password" =>
+"Neues Passwort",
+
+"password_confirm" =>
+"Passwort best&auml;tigen",
+
+"new_password_confirm" =>
+"Neues Passwort best&auml;tigen",
+
+"existing_password" =>
+"Existierendes Passwort",
+
+"invalid_email_address" =>
+"Die angegebene Adresse ist nicht g&uuml;ltig.",
+
+"password_required" =>
+"Sie m&uuml;ssen Ihr gegenw&auml;rtiges Passwort angeben, um diese Seite zu aktualisieren.",
+
+"password_required_for_email" =>
+"Um Ihre Adresse zu &auml;ndern, m&uuml;ssen Sie Ihr gegenw&auml;rtiges Passwort angeben.",
+
+"leave_blank" =>
+"Frei lassen, wenn Sie keine &Auml;nderung wollen.",
+
+"missing_username" =>
+"Sie m&uuml;ssen einen Usernamen angeben.",
+
+"invalid_characters_in_username" =>
+"Ihr Username enth&auml;lt nicht erlaubte Zeichen.",
+
+"missing_password" =>
+"Sie m&uuml;ssen ein Passwort angeben.",
+
+"missing_current_password" =>
+"F&uuml;r &Auml;nderungen m&uuml;ssen Sie Ihr gegenw&auml;rtiges Passwort angeben.",
+
+"password_in_dictionary" =>
+"W&ouml;rter aus einer Lexikonliste sind als Passw&ouml;rter nicht zugelassen.",
+
+"not_secure_password" =>
+"Ihr Password mu&szlig; mindestens einen Gro&szlig;buchstaben, einen Kleinbuchstaben und eine Ziffer enthalten.",
+
+"invalid_password" =>
+"Das angegebene Passwort ist falsch.",
+
+"missing_email" =>
+"Sie m&uuml;ssen eine E-Mailadresse angeben.",
+
+"banned_email" =>
+"Die angegebene E-Mailadresse ist gesperrt.",
+
+"missmatched_passwords" =>
+"Passwort und Passwort-Wiederholung stimmen nicht &uuml;berein.",
+
+"username_too_short" =>
+"Ihr Username mu&szlig; mindestens %x Zeichen lang sein.",
+
+"password_too_short" =>
+"Ihr Passwort mu&szlig; mindestens %x Zeichen lang sein.",
+
+"username_password_too_long" =>
+"Username und Passwort d&uuml;rfen nicht l&auml;nger als 32 Zeichen sein.",
+
+"username_taken" =>
+"Der gew&auml;hlte Username ist nicht verf&uuml;gbar.",
+
+"screen_name_taken" =>
+"Der gew&auml;hlte Screen-Name ist nicht verf&uuml;gbar.",
+
+"email_taken" =>
+"Die gew&auml;hlte Adresse ist schon registriert.",
+
+"password_based_on_username" =>
+"Das Passwort darf nicht vom Usernamen abgeleitet sein.",
+
+"username_changed" =>
+"Username ge&auml;ndert in:",
+
+"screen_name_changed" =>
+"Screen-Name ge&auml;ndert in:",
+
+"email_changed" =>
+"E-Mail ge&auml;ndert in:",
+
+"password_change_requires_login" =>
+"Hinweis: Wenn Sie Ihr Passwort ge&auml;ndert haben, m&uuml;ssen Sie sich neu einloggen.",
+
+"settings_updated" =>
+"Einstellungen aktualisiert",
+
+"new_member_added" =>
+"Mitgliedsprofil erstellt:",
+
+"password_change_warning" =>
+"Hinweis: Sie haben Ihr Passwort ge&auml;ndert, also m&uuml;ssen Sie sich neu einloggen.",
+
+"ping_servers" =>
+"Ping-Server",
+
+"pingservers_updated" =>
+"Ping-Server aktualisiert",
+
+"html_buttons" =>
+"HTML-Buttons",
+
+"html_buttons_updated" =>
+"HTML-Buttons aktualisiert",
+
+"quick_links" =>
+"Quick Links",
+
+"only_self_qucklink_access" =>
+"Sie haben keinen Zugang zu den Quick Links anderer Mitglieder.",
+
+"quicklinks_updated" =>
+"Quick Links aktualisiert",
+
+"quick_link_description" =>
+"Benutzen Sie dieses Formular, um pers&ouml;nliche Links zu der Toolbar im Kopf dieser Seite hinzuzuf&uuml;gen.",
+
+"quick_link_description_more" =>
+"Diese Links werden nur f&uuml;r Sie sichtbar sein.",
+
+"quicklinks_delete_instructions" =>
+"Um einen Eintrag zu l&ouml;schen, schicken Sie dieses Formular mit einem leeren Titelfeld ab.",
+
+"link_title" =>
+"Link-Titel",
+
+"link_url" =>
+"Link-URL",
+
+"link_order" =>
+"Reihenfolge",
+
+"no_extended_fields" =>
+"Zur Zeit gibt es keine erweiterten Profilfelder",
+
+"bookmarklet" =>
+"Bookmarklet",
+
+"bookmarklet_created" =>
+"Bookmarklet erstellt",
+
+"bookmarklet_info" =>
+"Ein Bookmarklet erlaubt Ihnen das Posten von Eintr&auml;gen, ohne sich in das Control-Panel einzuloggen.",
+
+"consult_manual" =>
+"F&uuml;r mehr Informationen siehe Handbuch.",
+
+"bookmarklet_name" =>
+"Name Ihres Bookmarklets",
+
+"single_word_no_spaces" =>
+"ein Wort, kein Leerzeichen",
+
+"weblog_name" =>
+"W&auml;hlen Sie ein Weblog zum Posten aus.",
+
+"no_blogs_assigned_to_user" =>
+"Ihrem Account sind keine Weblogs zugewiesen.",
+
+"select_field" =>
+"W&auml;hlen Sie ein Standardfeld zum Posten aus.",
+
+"bookmarklet_next_step" =>
+"N&auml;chster Schritt",
+
+"create_the_bookmarklet" =>
+"Bookmarklet erstellen",
+
+"no_fields_assigned_to_blog" =>
+"Dem gew&auml;hlten Weblog ist keine Feldergruppe zugewiesen.",
+
+"safari_users" =>
+"Klicken Sie hier, wenn Sie Safari als Browser benutzen.",
+
+"bookmarklet_instructions" =>
+"Speichern Sie diesen Link in Ihrem Bookmarkverzeichnis:",
+
+"subscriptions" =>
+"Subskriptionen",
+
+
+
+// END
+''=>''
+);
+?>
