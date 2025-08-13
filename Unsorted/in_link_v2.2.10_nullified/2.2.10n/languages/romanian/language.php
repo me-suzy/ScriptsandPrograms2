29f457a630ac5606560fadfe0508e5f97bbbf439
me-suzy <me.suzana@gmail.com>
2025-08-13 05:47:57 +0300

    Add Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/romanian/language.php

---
+++ Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/romanian/language.php
@@ -0,0 +1,307 @@
+<?php
+## In-link User Language File - Romanian
+## Author: Intechnic Corporation
+## Translation: Marius Herea; marius@mcd.ro
+## Date: 07/01/2002
+##
+## Acest fisier este destinat urmatoarei versiuni a In-link: 2.0.6
+## Poti modifica textul in acest fisier astfel incat sa se potriveasca cu necesitatile lingvistice pe care le ai.
+## Asigura-te ca ca ai folosit ai pus intre ghilimele \" citatele.
+
+#########################################################################################
+#Language variables
+
+$lu_char_set								=   "ISO-8859-1";
+$lu_this_language							=	"Romanian";	
+
+#########################################################################################
+#Navigation Menu
+
+$lu_getting_rated							=	"Obtine voturi";
+$lu_go_to_page								=	"Mergi la pagina";
+$lu_search_results						=	"Rezultatele cautarii";
+$lu_nav_addlink								=	"Adauga legatura";
+$lu_nav_editorspick							=	"Alegerea editorului";
+$lu_nav_gettingrated							=	"Fiind Calsificat";
+$lu_nav_home								=	"Acasa";
+$lu_nav_login								=	"Intra";
+$lu_nav_logout								=	"iesi";
+$lu_nav_maillist							=	"Subscrie";
+$lu_nav_modlink								=	"Modifica link";
+$lu_nav_new									=	"Linkuri noi";
+$lu_nav_pop									=	"Cele mai populare";
+$lu_nav_profile								=	"Profil";
+$lu_nav_register							=	"Inscriere";
+$lu_nav_search								=	"Cauta";
+$lu_nav_search_advanced						=	"Cautare avansata";
+$lu_nav_suggestcat							=	"Sugereaza categorie";
+$lu_nav_top									=	"Cele mai votate";
+$lu_navbar_seperator						=	" >";
+
+
+#########################################################################################
+#Page Titles
+
+$lu_title_add_link							=	"Adauga link";
+$lu_title_add_review						=	"Adauga opinie";
+$lu_title_confirm							=	"Confirmare";
+$lu_title_edit_link							=	"Modifica link";
+$lu_title_editors_picks						=	"Alegerile editorului";
+$lu_title_error								=	"Eroare";
+$lu_title_gettingrated						=	"Obtine voturi";
+$lu_title_login								=	"Intra";
+$lu_title_modify							=	"Modifica linkurile tale";
+$lu_title_most_popular						=	"cele mai populare linkuri";
+$lu_title_msg								=	"Mesaj Sistem";
+$lu_title_new_links							=	"Linkuri noi";
+$lu_title_profile							=	"Profil membru";
+$lu_title_rate								=	"Voteaza acest link";
+$lu_title_registration						=	"Inregistrare";
+$lu_title_reviews							=	"Pareri";
+$lu_title_search							=	"Cauta";
+$lu_title_search_advanced					=	"Cautare avansata";
+$lu_title_subscribe							=	"Subscrie la lista de mail";
+$lu_title_suggest							=	"Sugereaza categorie";
+$lu_title_top_rated							=	"Cele mai votate linkuri";
+
+
+#########################################################################################
+#Link and Category Markers (Can replace them with graphics)
+
+$lu_cat_new									=	"nou";
+$lu_cat_pick								=	"alege";
+$lu_link_new								=	"nou";
+$lu_link_pick								=	"alege";
+$lu_link_pop								=	"popular";
+$lu_link_top								=	"top";
+
+
+#########################################################################################
+#System Messages
+
+$lu_no_categories							=	"Nu exista categorii";
+$lu_no_links								=	"Nu exista linkuri";
+
+
+#########################################################################################
+#Link/Category Details 
+
+$lu_added									=	"Adaugat";
+$lu_cat										=	"categorie";
+$lu_date_created							=	"Data la care a fost creat";
+$lu_desc									=	"Descriere";
+$lu_edit									=	"Modifica";
+$lu_editor_pick								=	"Alegerea editorului";
+$lu_from									=	"De la";
+$lu_general									=	"General";
+$lu_image									=	"Imagine";
+$lu_hits									=	"Accesari";
+$lu_link									=	"Link";
+$lu_name									=	"Nume";
+$lu_no										=	"Nu";
+$lu_rateit									=	"Voteaza-l";
+$lu_rating									=	"Scor";
+$lu_reviews									=	"Pareri";
+$lu_to										=	"Catre";
+$lu_votes									=	"Voturi";
+$lu_url										=	"URL";
+$lu_yes										=	"Da";
+
+
+#########################################################################################
+#Search 
+
+$lu_boolean_type_search						=	"Tip bolean";
+$lu_return_number_results					=	"Afisare numar rezultate";
+
+
+#########################################################################################
+#Errors
+
+$lu_already_voted							=	"Inregistrarile noastre arata ca ai votat deja. Intr-o luna nu poti vota decat o singura data!.";
+$lu_already_voted							=	"Nu ai dreturile necesare pentru a vota aceste resurse!";
+$lu_email_used								=	"Acest e-mail este folosit";
+$lu_error_404								=	"Fisierul nu poate fi deschis.";
+$lu_error_addlink_not_allowed				=	"Adaugarea legaturilor nu este permisa";
+$lu_error_addreview_not_allowed				=	"Adaugarea parerilor nu este permisa";
+$lu_error_addto_cat_not_allowed				=	"Aceasta categorie nu este suficient de specifica! Te rugam sa alegi o alta categorie.";
+$lu_error_cannot_remove_cat					=	"Ultima categorie nu poate fi indepartata!";
+$lu_error_db								=	"Eroare baza de date!";
+$lu_error_file_not_found					=	"Fisierul este disponibil:";
+$lu_error_pass_match						=	"Parolele introduse nu se potrivesc.";
+$lu_error_pass_wrong						=	"Parola introdusa este incorecta.";
+$lu_error_review_not_filled					=	"Opinia nu poate lipsi";
+$lu_error_user_not_allowed					=	"Nu-ti este permis sa te inregistrezi!";	
+$lu_error_wrong_user						=	"Legatura apartine unui alt proprietar!";
+$lu_invalid_entry							=	"Intrare invalida";
+$lu_login_incorrect							=	"Login Incorect";
+$lu_pass_not_match							=	"Parolele nu se potrivesc";
+$lu_username_used							=	"Numele utilizator este folosit";
+
+#########################################################################################
+#Confirmations
+
+$lu_confirm_registration					=	"Inregistrarea ta a fost trimisa.";
+$lu_confirm_suggest_site					=	"Multumim pentru sugerarea acestui site unui prieten.";
+$lu_confirm_profile							=	"Profilul tau a fost reactualizat.";
+$lu_confirm_linkupdate						=	"Legatura ta a fost reactualizata.";
+$lu_confirm_addlink							=	"Legatura ta a fost submisa.";
+$lu_confirm_suggest							=	"Multumim pentru sugerarea unei noi categorii.";
+$lu_confirm_addreview						=	"Opinia ta a fost submisa.";
+$lu_confirm_subscribe						=	"Ai fost inscris in lista de mail.";
+$lu_confirm_unsubscribe						=	"Ai fost indepartat din lista de mail.";
+
+#########################################################################################
+#Buttons
+
+$lu_button_addcat							=	"Adauga categorie";
+$lu_button_addlink							=	"Adauga legatura";
+$lu_button_addreview						=	"Adauga opinie";
+$lu_button_back								=	"Inapoi";
+$lu_button_cancel							=	"Renunta";
+$lu_button_editlink							=	"editeaza legatura";
+$lu_button_go								=	"Go";
+$lu_button_login							=	"Login";
+$lu_button_ok								=	"Ok";
+$lu_button_rate								=	"Evalueaza";
+$lu_button_register							=	"Inscrie";
+$lu_button_reset							=	"Anuleaza";
+$lu_button_search							=	"Cauta";
+$lu_button_search_cats						=	"Cauta categorii";
+$lu_button_search_links						=	"Cauta legaturi";
+$lu_button_send								=	"Trimite";
+$lu_button_subscribe						=	"Subscrie";
+$lu_button_unsubscribe						=	"Desubscrie";
+$lu_button_update							=	"Actualizare";
+$lu_mailing_list							=	"Lista mail";
+$lu_not_mailing_list						=	"Inexistent in lista mail";
+
+
+#########################################################################################
+#Statistics
+
+$lu_cats									=	"Categorii:";
+$lu_links									=	"Legaturi:";
+$lu_total_cats								=	"Total categorii in baza de date:  ";
+$lu_total_hits								=	"Hits: ";
+$lu_total_links								=	"Total legaturi in baza de date: ";
+
+
+#########################################################################################
+#Addlink template
+
+$lu_add_cat									=	"Adauga aceasta categorie";
+$lu_additional_cats							=	"categorii aditionale:";
+$lu_remove									=	"Indeparteaza";
+$lu_select									=	"Selecteaza";
+$lu_submitting_to							=	"Subscrii la:";
+
+
+#########################################################################################
+#Register template
+
+$lu_first_name								=	"Prenume";
+$lu_last_name								=	"Nume";
+$lu_re_password								=	"Din nou parola";
+$lu_user_name								=	"Nume utilizator";
+
+
+#########################################################################################
+#Miscellaneos Pages
+
+$lu_advanced_search							=	"Cautare avansata";
+$lu_atleast_3								=	"(minim 3)";
+$lu_email									=	"E-mail";
+$lu_hint_suggest							=	"Sugereaza acest sit unui prieten";
+$lu_keyword									=	"Cuvant cheie";
+$lu_login									=	"Login";
+$lu_not_logged_in							=	"Nu esti logat";
+$lu_password								=	"Parola";
+$lu_register								=	"Inregistreaza";
+$lu_review									=	"Opinie";
+$lu_search									=	"Cauta";
+$lu_search_for								=	"Cauta dupa";
+$lu_show									=	"Arata";
+$lu_sort_by									=	"Sortat dupa";
+$lu_status									=	"Status";
+$lu_suggest									=	"Sugereaza";
+$lu_user									=	"utilizator";
+$lu_user_menu								=	"Meniu utilizator";
+$lu_welcome									=	"Bine ai venit";
+
+
+#########################################################################################
+#Dropdowns
+
+$lu_drop_and								=	"si";
+$lu_drop_as_many_as_found					=	"toate";
+$lu_drop_ascending							=	"Crescator";
+$lu_drop_date								=	"data";
+$lu_drop_desc								=	"Descriere";
+$lu_drop_descending							=	"Descrescator";
+$lu_drop_editors_pick						=	"Alegerea editorului";
+$lu_drop_hits								=	"Hits";
+$lu_drop_image								=	"Imagine";
+$lu_drop_name								=	"Nume";
+$lu_drop_numlinks							=	"Numar de legaturi";
+$lu_drop_numsubs							=	"Numar de subscrieri";
+$lu_drop_or									=	"Sau";
+$lu_drop_perm								=	"Permisiuni";
+$lu_drop_rating								=	"Cotare";
+$lu_drop_url								=	"URL";
+$lu_drop_user								=	"Utilizator";
+$lu_drop_vis								=	"Visibilitate";
+$lu_drop_votes								=	"Voturi";
+
+
+#########################################################################################
+#Custom Fields
+
+$lu_categories_custom_fields1			    =	"Nume camp #1";
+$lu_categories_custom_fields2			    =	"Nume camp #2";
+$lu_categories_custom_fields3			    =	"Nume camp #3";
+$lu_categories_custom_fields4			    =	"Nume camp #4";
+$lu_categories_custom_fields5			    =	"Nume camp #5";
+$lu_categories_custom_fields6			    =	"Nume camp #6";
+$lu_links_custom_fields1					=	"Nume camp #1";
+$lu_links_custom_fields2					=	"Nume camp #2";
+$lu_links_custom_fields3					=	"Nume camp #3";
+$lu_links_custom_fields4					=	"Nume camp #4";
+$lu_links_custom_fields5					=	"Nume camp #5";
+$lu_links_custom_fields6					=	"Nume camp #6";
+$lu_users_custom_fields1					=	"Nume camp #1";
+$lu_users_custom_fields2					=	"Nume camp #2";
+$lu_users_custom_fields3					=	"Nume camp #3";
+$lu_users_custom_fields4					=	"Nume camp #4";
+$lu_users_custom_fields5					=	"Nume camp #5";
+$lu_users_custom_fields6					=	"Nume camp #6";
+
+
+#########################################################################################
+#Ratings
+
+$lu_excellent								=	"Excelent!";
+$lu_fair									=	"Potrivit";
+$lu_good									=	"Bun";
+$lu_poor									=	"Slab";
+$lu_verygood								=	"Foarte bun";
+
+
+#########################################################################################
+#Text segments
+
+$lu_txt_gettingrated	=	"Poti introduce urmatorul cod HTML in situl tau pentru a permite vizitatorilor sa voteze pentru aceasta resursa:";
+
+#########################################################################################
+#New for version 2.0.7
+$lu_confirm_approval						=	"  Va deveni activ dupa aprobare.";
+
+#New for version 2.0.9
+
+$lu_report_dead						=	"Raporteaza o legatura moarta";
+$lu_confirm_report_dead						=	"Legatura a fost raportata ca fiind moarta. Iti multumim!";
+$lu_error_login_to_add						=	"Te rugam sa te loghezi pentru a adauga o legatura!";
+
+#New for version 2.0.10
+$lu_error_addto_cat_added					=	"Aceasta categorie a fost deja adaugata";
+?>
