b57ac90a96d4ef1cdac4af4e3f9eff591509c94a
me-suzy <me.suzana@gmail.com>
2025-08-13 07:07:03 +0300

    Add Unsorted/Olate.Download.v2.translated.by.[GTT]/html/languages/english/main.php

---
+++ Unsorted/Olate.Download.v2.translated.by.[GTT]/html/languages/english/main.php
@@ -0,0 +1,61 @@
+<?php
+/***************************************************************************
+ *                      Olate Download v2 - Download Manager
+ *
+ *                           http://www.olate.com
+ *                            -------------------
+ *   author                : David Mytton
+ *   copyright             : (C) Olate 2003 
+ *
+ *   Support for Olate scripts is provided at the Olate website. Licensing
+ *   information is available in the license.htm file included in this
+ *   distribution and on the Olate website.                  
+ ***************************************************************************/
+
+// Define admin texts - see config.php for author information
+$language = array();
+$language['button_search']					= 'Search';
+$language['button_rate']					= 'Rate';
+$language['credits_poweredby']				= 'Powered by Olate Download ';
+$language['description_details']			= 'Additional information about the file you selected is shown below.';
+$language['description_download_begin']		= 'Your download will begin in 5 seconds, if it does not, ';
+$language['description_index']				= 'Select a category to view the downloads in it. Click the name of the download to view more information.';
+$language['description_nodownload']			= 'There is no download with that ID. <a href="index.php">Click here to go to the main page.</a>';
+$language['description_nodlcategory']		= 'There are no downloads in this category.';
+$language['description_noresults']			= 'There were no results for your search.';
+$language['description_search']				= 'To search the available downloads, enter a search term in the box below and click Search:';
+$language['info_downloads']					= 'Downloads:';
+$language['info_filesize']					= 'File Size:';
+$language['info_rating']					= 'Rating:';
+$language['info_votes']						= 'Votes:';
+$language['link_clickhere']					= 'Click here';
+$language['link_clickheregoback']			= 'Click here to go back';
+$language['link_downloadnow']				= 'Download Now';
+$language['link_moreinfo']					= 'More Info';
+$language['menu_all']						= 'All Downloads';
+$language['menu_downloads']					= 'Downloads';
+$language['menu_top']						= 'Top';
+$language['rating_0']						= '0 - Awful!';
+$language['rating_1']						= '1 - Poor';
+$language['rating_2']						= '2 - Bad';
+$language['rating_3']						= '3 - Fair';
+$language['rating_4']						= '4 - Good';
+$language['rating_5']						= '5 - Excellent!';
+$language['rating_already']					= 'Sorry! You have already voted! ';
+$language['rating_notrated']				= 'Not Rated';
+$language['rating_outof']					= ' out of 5.00.';
+$language['rating_thanks']					= 'Thank for your vote!</p> <p>The rating for this download is now: ';
+$language['title_categories']				= 'Categories';
+$language['title_download']					= 'Download';
+$language['title_pages']					= 'Page: ';
+$language['title_pages_prev']				= '<< Previous';
+$language['title_pages_next']				= 'Next >>';
+$language['title_rate']						= 'Rate';
+$language['title_ratedownload']				= 'Rate Download';
+$language['title_search']					= 'Search';
+$language['title_search_results']			= 'Search Results:';
+$language['title_sortby']					= 'Sort By:';
+$language['title_sorting_downloads']		= 'Downloads';
+$language['title_sorting_name']				= 'Name';
+$language['title_sorting_rating']			= 'Rating';
+?>
