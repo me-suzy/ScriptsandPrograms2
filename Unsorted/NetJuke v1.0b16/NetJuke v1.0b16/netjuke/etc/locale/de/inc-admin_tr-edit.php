a69b9d9d0913a13c62ffe1a4fa80daffbeb46605
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:28 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_tr-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_tr-edit.php
@@ -0,0 +1,71 @@
+<?php
+
+##################################################
+
+# /admin/tr-edit.php
+
+##################################################
+
+define( "TREDIT_DELETED_HELP", "Der gewählte Titel wurde gelöscht." );
+
+define( "TREDIT_DELCHILD_HELP_1", "Der N/A Eintrag ist vorgeschrieben und kann nicht gelöscht werden." );
+define( "TREDIT_DELCHILD_HELP_2", "Der N/A Eintrag ist vorgeschrieben und kann nicht verändert werden..\\nBitte erstellen Sie einen neuen Eintrag, in dem Sie das Kontrollkästchen NEU markieren." );
+
+define( "TREDIT_HEADER", "TITEL INFORMATION BEARBEITEN" );
+
+define( "TREDIT_ERROR_INSERT", "Fehler beim einügen in" );
+define( "TREDIT_ERROR_TR_T", "Fehler beim aktualisieren von netjuke_tracks" );
+define( "TREDIT_ERROR_AR_T", "Fehler beim aktualisieren von netjuke_artists" );
+define( "TREDIT_ERROR_AL_T", "Fehler beim aktualisieren von netjuke_albums" );
+define( "TREDIT_ERROR_GE_T", "Fehler beim aktualisieren von netjuke_genres" );
+define( "TREDIT_ERROR_TR", "Der Titelname darf nicht leer sein" );
+define( "TREDIT_ERROR_TN", "Die Titelnummer ist leer." );
+define( "TREDIT_ERROR_BR", "Die Bitrate ist notwendig und muss grösser 0 sein." );
+define( "TREDIT_ERROR_SR", "Die Samle Rate ist notwendig und muss grösser 0 sein." );
+define( "TREDIT_ERROR_FK", "Der Dateityp ist leer." );
+define( "TREDIT_ERROR_LC", "Der Dateiablageort ist leer." );
+define( "TREDIT_ERROR_NORES", "Bitte ein gültiges Titel wählen" );
+
+define( "TREDIT_FORM_CM", "Kommentare bearbeiten" );
+define( "TREDIT_FORM_LY", "Texte bearbeiten" );
+
+define( "TREDIT_FORM_NEW", "Neu" );
+
+define( "TREDIT_FORM_TR", "Titel Name" );
+define( "TREDIT_FORM_AR", "Interpreten Name" );
+define( "TREDIT_FORM_AL", "Album Name" );
+define( "TREDIT_FORM_GE", "Genre Name" );
+define( "TREDIT_FORM_TI", "Spieldauer" );
+define( "TREDIT_FORM_SECONDS", "Sekunden" );
+define( "TREDIT_FORM_TN", "Titel Nummer" );
+define( "TREDIT_FORM_BR", "Bitrate" );
+define( "TREDIT_FORM_SR", "Sample Rate" );
+define( "TREDIT_FORM_FK", "Dateityp" );
+define( "TREDIT_FORM_FS", "Dateigrösse" );
+define( "TREDIT_FORM_FN", "Ablageort" );
+define( "TREDIT_FORM_LC", "Anzahl gehört" );
+define( "TREDIT_FORM_RESET", "Zähler zurücksetzen" );
+
+define( "TREDIT_FORM_TR_IMG", "Bild Titel" );
+define( "TREDIT_FORM_AR_IMG", "Bild Interpret" );
+define( "TREDIT_FORM_AL_IMG", "Bild Album" );
+
+define( "TREDIT_FORM_BTN_SAVE", "Speichern" );
+define( "TREDIT_FORM_BTN_RESET", "Zurücksetzen" );
+define( "TREDIT_FORM_BTN_DUP", "Duplizieren" );
+define( "TREDIT_FORM_BTN_NEW", "Neuer Titel" );
+define( "TREDIT_FORM_BTN_DEL", "Löschen" );
+
+define( "TREDIT_FORM_BTN_DEL_CONF", "Sind Sie sciher das Sie diese Titel löschen wollen?" );
+define( "TREDIT_FORM_BTN_DELCHILD_CONF", "Sind Sie sciher das Sie diesen Abschnitt löschen wollen?" );
+
+define( "TREDIT_PLAY", "Anhören" );
+define( "TREDIT_PLAY_HELP", "Klicken um das Titel nun anzuhören." );
+define( "TREDIT_VIEW", "Ansichtsmodus" );
+define( "TREDIT_VIEW_HELP", "In den Ansichtsmodus wechseln." );
+define( "TREDIT_CLOSEWIN", "Fenster schliessen" );
+define( "TREDIT_CLOSEWIN_HELP", "Schliesst dieses Fenster" );
+
+##################################################
+
+?>
