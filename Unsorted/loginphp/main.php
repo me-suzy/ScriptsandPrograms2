153f7540b234a5ed42f455c968be2572f0e95f41
me-suzy <me.suzana@gmail.com>
2025-08-13 06:15:38 +0300

    Add Unsorted/loginphp/main.php

---
+++ Unsorted/loginphp/main.php
@@ -0,0 +1,8 @@
+<?php
+//include some stuff
+require("inc.php");
+?><center>
+this is the main page.  the admin can customize it...<br><br>
+<!-- add html, php, or javascript here -->
+<!-- you can delete the profile if you want -->
+<a href="profile.php">profile</a>
