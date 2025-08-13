3e6c93f2b6a0b0b5bf5964c53e0acde00b918cff
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:36 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/about.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/about.php
@@ -0,0 +1,15 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[ABOUT_Info] = "MWChat - íâ® ç â ­  ¡ §¥ PHP, ­ ¯¨á ­­ë© Application Index. Application Index - ª®¬¯ ­¨ï, § ­¨¬ îé ïáï à §à ¡®âª®© ¯à®£à ¬¬­®£® ®¡¥á¯¥ç¥­¨ï, ­ å®¤¨âáï ¢ £. «ìä , èâ â ««¨­®©á, . MWChat ¯à¥¤« £ ¥â ¢®§¬®¦­®áâ¨ ®¡¬¥­  ä ©« ¬¨, á®§¤ ­¨ï á¯¨áª®¢ ¯à¨ïâ¥«¥©, ¯®áë«ª¨ ç áâ­ëå á®®¡é¥­¨©, á®§¤ ­¨ï ç áâ­ëå ¨ ®¡é¨å ª®¬­ â, ­ §­ ç¥­¨ï ¯ à ¬¥âà®¢ ¯®«ì§®¢ â¥«¥©, ¯®«ì§®¢ â¥«ìáª¨å §­ çª®¢, â¥¬ë ¨ â.¤.! MWChat ¡®«ìè¥ ¯®å®¦ ­  IRC-ª«¨¥­â ­  ®á­®¢¥ ¡à ã§¥à , á â¥¬ ®â«¨ç¨¥¬, çâ® á¥à¢¥à IRC ­¥ ­ã¦¥­,   ¯à®£à ¬¬­®¥ ®¡¥á¯¥ç¥­¨¥ ¯®¤¤¥à¦¨¢ ¥âáï á ¬®áâ®ïâ¥«ì­®. ë ¬®¦¥â¥ áª ç âì MWChat ¯®  ¤à¥áã:";
+
+?>
