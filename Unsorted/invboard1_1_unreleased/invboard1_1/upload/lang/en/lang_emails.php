b9d7be3232ecaae8784b2c2ce2abc5f7af9840fa
me-suzy <me.suzana@gmail.com>
2025-08-13 05:37:22 +0300

    Add Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_emails.php

---
+++ Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_emails.php
@@ -0,0 +1,59 @@
+<?php
+
+$lang = array (
+
+'msn_send_msg'  => "Send this member an MSN Message",
+'msn_add_contact' => "Add this member to your MSN buddy list",
+
+'msn_name'      => "Members MSN Name",
+
+
+'yahoo_send_msg'  => "Send this member a Yahoo! Message",
+'yahoo_view_profile' => "View this members Yahoo! Profile",
+'yahoo_status'       => "Yahoo! Online Status",
+'yahoo_name'      => "Members Yahoo! Name",
+
+'report_title'   => "Report this post to a moderator",
+'report_submit'  => "Submit Report",
+'report_message' => "<b>Enter your report</b><br><br>Please note: The moderator will be made aware of the link to the post and the topic title.<br><br>This form is to be used ONLY for reporting objectional content, etc and is not to be used as a method of communicating with moderators for other reasons.",
+'report_topic'   => "Topic Title",
+
+
+'report_subject' => "Reported Post from",
+
+'report_redirect' => "the report has been submitted, returning you to the topic",
+
+
+message					=>	"Message",
+title					=>	"Send a page to a friend",
+'send_lang'             =>  "Send this email in",
+to_name					=>	"Send to (Person's Name):",
+redirect					=>	"The email has been sent",
+to_email					=>	"Send to (Email Address):",
+subject					=>	"Subject",
+submit_send					=>	"Send Email",
+send_email_to					=>	"Send an email to",
+message					=>	"Message",
+show_address_text					=>	"You can send this member an email by clicking on the link below. Invision Board does not show email addresses in the HTML code to prevent email addresses being harvested by 'spam bots'.",
+send_header					=>	", please fill in this form fully to send an email to",
+member_address_title					=>	"Members Email Address",
+send_title					=>	"Email Form",
+msg_txt					=>	"Note: By using this form, the recipient will be able to view your email address",
+subject					=>	"Subject",
+email_sent_txt					=>	"Thanks, the email has been successfully sent to",
+email_sent					=>	"The email has been sent",
+
+invite_subject					=>	"<#MEMBER_NAME#> thought you would like to see this",
+subs_redirect					=>	"You have subscribed to this topic",
+invite_redirect					=>	"The email has been sent to  ",
+sub_added					=>	"Your subscription has been added",
+
+icq_title					=>	"ICQ Pager",
+submit					=>	"Send this message",
+aol_title					=>	"AOL Pager",
+msg					=>	"Enter your message",
+email					=>	"Your Email",
+name					=>	"Your Name",
+forum_jump => "Forum Jump",
+);
+?>
