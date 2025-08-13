9ae7ff9ea71b3278896830cc3101afa4fcaa60ab
me-suzy <me.suzana@gmail.com>
2025-08-13 06:31:59 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/upload.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/upload.php
@@ -0,0 +1,35 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[UPLOAD_Title] = "Envoyer un fichier!";
+
+$LOCALE[UPLOAD_Info] = "S'il vous plaît choisir un fichier pour télécharger et l'utilisateur ce fichier est pour.";
+
+$LOCALE[UPLOAD_Button] = "Télécharger Maintenant";
+
+$LOCALE[UPLOAD_File_Send] = "Le fichier pour envoyer";
+
+$LOCALE[UPLOAD_User_Send] = "Envoyer à";
+
+$LOCALE[UPLOAD_Complete] = "Votre fichier a été envoyé avec succès!";
+
+$LOCALE[UPLOAD_Sent_File] = "J'ai seulement vous a envoyé le fichier";
+
+$LOCALE[UPLOAD_Err_Large] = "Le fichier que vous choisissez est à grand, ou n'a pas téléchargé correctement.";
+
+$LOCALE[UPLOAD_Err_Accept] = "L'utilisateur n'est pas enregistré ou n'accepte pas de fichier.";
+
+$LOCALE[UPLOAD_Err_Exists] = "Le fichier que vous envoyez déjà existe.";
+
+$LOCALE[UPLOAD_Err_Full] = "L'utilisateur n'a pas l'espace d'emmagasinage assez libre pour votre fichier.";
+
+?>
