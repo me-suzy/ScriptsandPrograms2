b553bb4e764a997d1c1033ac55ca93cd1f4711a2
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:09 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_tr-batch-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_tr-batch-edit.php
@@ -0,0 +1,39 @@
+<?php
+
+##################################################
+
+# /admin/tr-batch-edit.php
+
+##################################################
+
+define( "TRBEDIT_NOTRID", "Si us plau, seleccioneu almenys una pista per a processar." );
+
+define( "TRBEDIT_EDITED_MSG", "La pista seleccionada s'ha processat correctament." );
+
+define( "TRBEDIT_HEADER", "EDICIÓ DE PISTES MÚLTIPLES" );
+define( "TRBEDIT_SELECTED", "Seleccionades" );
+
+define( "TRBEDIT_FORM_TR", "TÃtol de la canÁÛ" );
+define( "TRBEDIT_FORM_AR", "Nom de l'intÀrpret" );
+define( "TRBEDIT_FORM_AL", "TÃtol de l'àlbum" );
+define( "TRBEDIT_FORM_GE", "Nom del gÀnere" );
+define( "TRBEDIT_FORM_FN", "UbicaciÛ" );
+define( "TRBEDIT_FORM_LC", "Comptador d'audicions" );
+define( "TRBEDIT_FORM_RESET", "Posa a zero" );
+define( "TRBEDIT_FORM_TR_IMG", "Icona de canÁÛ" );
+define( "TRBEDIT_FORM_TR_COM", "Comentaris" );
+define( "TRBEDIT_FORM_TR_LYR", "Lletres" );
+
+define( "TRBEDIT_FORM_BTN_SAVE", "Desa" );
+define( "TRBEDIT_FORM_BTN_RESET", "DesfÈs canvis" );
+
+define( "TRBEDIT_PLAY", "Reprodueix" );
+define( "TRBEDIT_PLAY_HELP", "Feu clic per reproduir aquestes canÁons." );
+define( "TRBEDIT_CLOSEWIN", "Tanca la finestra" );
+define( "TRBEDIT_CLOSEWIN_HELP", "Tanca aquesta finestra" );
+
+define( "TRBEDIT_ERROR_INSERT", "Hi ha hagut un error de creaciÛ en" );
+
+##################################################
+
+?>
