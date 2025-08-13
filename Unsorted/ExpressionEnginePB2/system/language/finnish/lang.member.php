fc725fdda31ca888ee1ac5248a7752476c044d62
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:06 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.member.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.member.php
@@ -0,0 +1,376 @@
+<?php
+$L = array(
+
+
+"member_module_name" =>
+"Jäsenyys",
+
+"member_module_description" =>
+"Jäsenten hallinta systeemi",
+
+"mbr_menu" =>
+"Valikko",
+
+"mbr_show" =>
+"Näytä",
+
+"mbr_sort" =>
+"Lajittele",
+
+"mbr_order" =>
+"Järjestys",
+
+"mbr_rows" =>
+"Rivit",
+
+"mbr_my_account" =>
+"Oma tili",
+
+"mbr_logged_in_as" =>
+"Kirjautuneena:",
+
+"mbr_logout" =>
+"Logout",
+
+"mbr_member_registration" =>
+"Jäsenen rekisteröinti",
+
+"mbr_forgotten_password" =>
+"Unohtunut salasana",
+
+"mbr_memberlist" =>
+"Jäsenten lista",
+
+"notice" =>
+"Huomiot",
+
+"mbr_registration_not_allowed" =>
+"Uusia jäsenyystilejä ei hyväksytä tällä hetkellä.",
+
+"mbr_login" =>
+"Login",
+
+"mbr_form_empty" =>
+"Sinun tulee syöttää käyttäjätunnus ja salasana",
+
+"mbr_account_not_active" =>
+"Jäsenyystilisi ei ole aktivoitu vielä.",
+
+"mbr_you_are_logged_in" =>
+"Olet kirjautunut sisään.",
+
+"mbr_you_are_logged_out" =>
+"Olet ulos kirjautunut.",
+
+"mbr_no_reset_id" =>
+"Antamasi ID numero ei näytä olevan oikein. Tarkista linkki, jota seurasit.",
+
+"mbr_id_not_found" =>
+"Antamasi koodiarvoa ei löytynyt tietokannasta.",
+
+"mbr_username" =>
+"Käyttätunnus",
+
+"mbr_password" =>
+"Salasana",
+
+"mbr_submit" =>
+"Lähetä",
+
+"mbr_update" =>
+"Päivitä",
+
+"mbr_password_confirm" =>
+"Salasanan vahvistus",
+
+"mbr_auto_login" =>
+"Auto-login seuraavilla kerroilla",
+
+"mbr_show_name" =>
+"Näytä nimeni online käyttäjien listalla",
+
+"mbr_forgot_password" =>
+"Unohtuiko salasana?",
+
+"mbr_email_address" =>
+"Emai osoite",
+
+"mbr_your_email" =>
+"Sinun email osoite",
+
+"mbr_email_short" =>
+"Email",
+
+"mbr_icq_console" =>
+"ICQ Konsoli",
+
+"mbr_aim_console" =>
+"AOL IM",
+
+"mbr_aol_short" =>
+"AOL",
+
+"mbr_icq_short" =>
+"ICQ",
+
+"mbr_www" =>
+"URL",
+
+"mbr_msn_short" =>
+"MSN",
+
+"mbr_yahoo_short" =>
+"Yahoo",
+
+"mbr_screen_name" =>
+"Näyttönimi",
+
+"mbr_name" =>
+"Nimi",
+
+"mbr_screen_name_exp" =>
+"Jos jätät kentän tyhjäksi, näyttönimenä käytetään käyttäjätunnustasi",
+
+"mbr_your_url" =>
+"Kotisivujesi URL",
+
+"mbr_field_required" =>
+"Seuraavat kentät ovat pakollisia",
+
+"mbr_username_length" =>
+"Käyttäjätunnuksen tulee olla %x merkkiä pitkä",
+
+"mbr_password_length" =>
+"Salasanan tulee olla %x merkkiä pitkä",
+
+"mbr_registration_complete" =>
+"Rekisteröinti valmis",
+
+"mbr_registration_completed" =>
+"Rekisteröitymisesi onnistui.",
+
+"mbr_admin_will_activate" =>
+"Järjestelmänvalvoja aktivoi tilisi ja ilmoittaa sinulle, kun se on valmis.",
+
+"mbr_membership_instructions_email" =>
+"Sinulle on juuri lähetetty viesti, joka sisältää aktivointi ohjeet.",
+
+"mbr_membership_instructions_cont" =>
+"Tarkista postisi ja seuraa ohjeita.",
+
+"mbr_your_are_logged_in" =>
+"Olet sisäänkirjautuneena ja valmis käyttämään uutta tiliäsi.",
+
+"mbr_activation" =>
+"Tilin aktivointi",
+
+"mbr_problem_activating" =>
+"Epäkelpo aktivointi pyyntö.",
+
+"mbr_activation_success" =>
+"Tilisi on aktivoitu",
+
+"mbr_may_now_log_in" =>
+"Voit nyt kirjautua ja käyttää sitä",
+
+"mbr_not_allowed_to_view_profiles" =>
+"Sinulla ei ole oikeutta tarkastella jäsenten profiileita",
+
+"mbr_member_profile" =>
+"Jäsenen profiili",
+
+"mbr_member_group" =>
+"Jäsenten ryhmä:",
+
+"mbr_last_visit" =>
+"Viimeinen vierailu",
+
+"mbr_most_recent_entry" =>
+"Viimeisin viesti",
+
+"mbr_most_recent_comment" =>
+"Viimeisin kommentti",
+
+"mbr_required_fields" =>
+"Osoittaa vaadittavat kentät",
+
+"mbr_join_date" =>
+"Liittymispäivä",
+
+"mbr_total_entries" =>
+"Viestejä kaikkiaan",
+
+"mbr_total_comments" =>
+"Kommentteja kaikkiaan",
+
+"mbr_comments" =>
+"Kommentit",
+
+"mbr_member_timezone" =>
+"Jäsenen aikavyöhyke",
+
+"mbr_member_local_time" =>
+"Jäsenen paikallinen aika",
+
+"mbr_email" =>
+"Email",
+
+"mbr_url" =>
+"Kotisivu",
+
+"mbr_location" =>
+"Sijainti",
+
+"mbr_birthday" =>
+"Syntymäpäivä",
+
+"mbr_your_stats" =>
+"Tilisi tilastot",
+
+"mbr_edit_your_profile" =>
+"Muokaa profiiliasi",
+
+"mbr_profile_homepage" =>
+"Profiilin kotisivu",
+
+"mbr_profile_has_been_updated" =>
+"Profiilisi on päivitetty",
+
+"mbr_email_has_been_updated" =>
+"Email ominaisuudet on päivitetty",
+
+"mbr_email_updated" =>
+"Email ominaisuudet päivitetty",
+
+"username_disallowed" =>
+"Käyttäjätunnuksen vaihtaminen ei ole sallittua",
+
+"mbr_settings_updated" =>
+"Asetuksesi on päivitetty",
+
+"mbr_notepad_updated" =>
+"Muistiosi on päivitetty",
+
+"mbr_localization_settings_updated" =>
+"Paikallisasetukset on päivitetty",
+
+"mbr_terms_of_service" =>
+"Palvelun säännöt",
+
+"mbr_i_accept" =>
+"Hyväksyn palvelun säännöt",
+
+"mbr_terms_of_service_required" =>
+"Sinun tulee klikata &quot;hyväksyn palvelun säänöt&quot; valintaa",
+
+"mbr_custom_field_empty" =>
+"Seuraavat kentät ovat pakollisia",
+
+"mbr_all_member_groups" =>
+"Kaikki jäsenten ryhmät",
+
+"mbr_member_name" =>
+"Jäsenen nimi",
+
+"mbr_ascending" =>
+"Nouseva",
+
+"mbr_descending" =>
+"Laskeva",
+
+"mbr_back_to_login" =>
+"Takaisin kirjautumiseen",
+
+"mbr_back_to_main" =>
+"Takaisin pääsivulle",
+
+"mbr_aol_im" =>
+"AOL Instant Messenger",
+
+"mbr_icq" =>
+"ICQ numero",
+
+"mbr_yahoo" =>
+"Yahoo Messenger",
+
+"mbr_msn" =>
+"MSN Messenger",
+
+"mbr_bio" =>
+"Jäsenen henkilöhistoria ",
+
+"mbr_interests" =>
+"Harrastukset",
+
+"mbr_occupation" =>
+"Koulutus",
+
+"mbr_email_member" =>
+"Email Konsoli",
+
+"mbr_subject" =>
+"Emailin aihe",
+
+"mbr_message" =>
+"Emailin viesti",
+
+"mbr_close_window" =>
+"Sulje ikkuna",
+
+"mbr_recipient" =>
+"Emailin vastaanottajat:",
+
+"mbr_email_not_accepted" =>
+"Tämä jäsen ei tällä hetkellä hyväksy emaileja.",
+
+"mbr_email_disclaimer" =>
+"Tämän viestin lähetys paljastaa email osoitteesi vastaanottajalle.",
+
+"mbr_email_logged" =>
+"Huom: Email viestit menevät lokiin ja ovat järjestelmänvalvojan luettavissa",
+
+"mbr_missing_fields" =>
+"Kaikki kentät ovat pakollisia",
+
+"mbr_send_email" =>
+"Lähetä viesti",
+
+"mbr_email_forwarding" =>
+"Tämä viesti on lähetettyt sinulle tilisi kautta, joka on sivulla: ",
+
+"mbr_email_forwarding_cont" =>
+"Jos et halua myöhemmin vastaanottaa viestejä, tulee sinun muuttaa jäsenyytesi profiilia.",
+
+"mbr_email_error" =>
+"Tapahtui virhe sähköpostin lähetyksessä.",
+
+"mbr_good_email" =>
+"Sähköpostisi lähetettiin onnistuneesti.",
+
+"mbr_send_self_copy" =>
+"Lähetä minulle kopio tästä viestistä",
+
+"mbr_icq_number" =>
+"ICQ Numero:",
+
+"mbr_icq_recipient" =>
+"Vastaanottaja:",
+
+"mbr_icq_subject" =>
+"Aihe",
+
+"mbr_icq_message" =>
+"Viesti",
+
+"mbr_not_allowed_to_use_email_console" =>
+"Sinulla ei ole oikeutta käyttää email konsolia",
+
+"mbr_email_timelock_not_expired" =>
+"Voit käyttää Email konsolia joka %x minuutti.",
+
+"mbr_you_are_registered" =>
+"Olet jo rekisteröitynyt ja kirjautunut",
+
+''=>''
+);
+?>
