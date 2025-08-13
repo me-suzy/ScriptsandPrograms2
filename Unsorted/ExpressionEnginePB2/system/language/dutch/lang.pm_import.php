490c5bbbce2678b1a136291b2c0198d2b8c96fbf
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:21 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.pm_import.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.pm_import.php
@@ -0,0 +1,313 @@
+<?php
+$L = array(
+
+
+"pmachine_import_utitity" =>
+"pMachine Importeer Programma",
+
+"pmachine_import_welcome" =>
+"Dit programma geeft u de mogelijkheid om data uit Pmachine te importeren in ExpressionEngine",
+
+"pmachine_import_disclaimer" =>
+"Als u geen gebruik maakt van pMachine dan heeft u dit programma niet nodig",
+
+"pmachine_import_information" =>
+"Importeer Handleiding",
+
+"pmachine_import_removal" =>
+"Let Op: Gebruik dit programma niet als u op dit moment geen pMachine gebruikt.",
+
+"configuration_blurb" =>
+"Om dit programma te kunnen gebruiken moet u eerst uw pMachine database informatie invoeren.",
+
+"configuration_blurb_cont" =>
+"U vindt deze gegevens in uw pMachine config.php bestand.",
+
+"database_info" =>
+"Database Informatie",
+
+"sql_server" =>
+"MySQL Server Adres",
+
+"sql_username" =>
+"MySQL Gebruikersnaam",
+
+"sql_password" =>
+"MySQL Wachtwoord",
+
+"sql_database" =>
+"MySQL Database",
+
+"sql_prefix" =>
+"Tabel Voorvoegsel",
+
+"leave_prefix_blank" =>
+"Laat leeg tenzij u gebruik wilt maken van een eigen voorvoegsel",
+
+"configure" =>
+"Configureer",
+
+"empty_field_warning" =>
+"U heeft een aantal velden leeg gelaten",
+
+"no_database_connection" =>
+"Niet mogelijk een contact te krijgen met uw database met de gegevens die u heeft ingevoerd",
+
+"table_name" =>
+"Tabelnaam",
+
+"table_rows" =>
+"Tabelrij",
+
+"table_action" =>
+"Actie",
+
+"table_status" =>
+"Status",
+
+"completed" =>
+"Voltooid",
+
+"no_rows_exist" =>
+"Importeren niet nodig",
+
+"pending" =>
+"Klaar om te importeren",
+
+"pending_stats" =>
+"Klaar om uit te voeren",
+
+"import_now" =>
+"Importeer Data",
+
+"recalculate" =>
+"Uitvoeren van data",
+
+"step_one" =>
+"Stap 1",
+
+"step_two" =>
+"Stap 2",
+
+"step_three" =>
+"Stap 3",
+
+"step_four" =>
+"Stap 4",
+
+"step_five" =>
+"Stap 5",
+
+"import_options" =>
+"Importeer Opties",
+
+"import_members" =>
+"Importeer Leden",
+
+"import_weblog_entries" =>
+"Importeer Weblog Toevoeging",
+
+"recalculate_statistics" =>
+"Statistieken opnieuw tellen",
+
+"click_to_reset_statistics" =>
+"Klik hier om de statistieken te updaten",
+
+"reset_statistics_info" =>
+"U zult nu naar een apart hertelling programma gaan. Als u klaar bent, zult u automatisch terugkomen bij stap 5",
+
+"members" =>
+"Leden",
+
+"member_stats" =>
+"Leden statistieken",
+
+"weblog_stats" =>
+"Weblog statistieken",
+
+"total_members" =>
+"Totaal aantal leden in uw pMachine database:",
+
+"select_your_account" =>
+"Selecteer het pMachine account dat van u is",
+
+"ignore_instructions" =>
+"Omdat u al een account heeft in ExpressionEngine heeft het geen zin om nog eens een gebruikersnaam toe te voegen die uit uw pMachine informatie komt. Zou dit wel gebeuren dan krijgt u dus dubbele accounts. Als u uw huidige pMachine account selecteerd zullen de weblog toevoegingen gekoppeld worden aan uw huidige ExpressionEngine account",
+
+"i_have_no_account" =>
+"Ik heb geen pMachine account",
+
+"members_per_cycle" =>
+"Aantal leden per cyclus",
+
+"cycle_instructions" =>
+"Om te voorkomen dat er timeout problemen ontstaan bij het importeren van de pMachine data, zal dit in zogenaamde batches gebeuren. Laat weten hoeveel records u per batch wilt importeren. 1000 is een veilig aantal voor de meeste servers. Als u een sneller server of een dedicated server heeft kan het aantal op enkele duizenden per keer gezet gezet worden.",
+
+"blog_cycle_instructions" =>
+"Het importeren van weblog berichten is een intensief proces, vooral als u veel reacties en trackbacks heeft. Om te voorkomen dat er timeout problemen ontstaan bij het importeren van de pMachine weblog berichten zal dit in zogenaamde batches gebeuren. 100 import the pMachine weblog entries in limited batches. 100 is een veilig aantal voor de meeste servers. Als u een sneller server of een dedicated server heeft , of als uw entries geen, of niet veel reacties bevatten dan kunt u een hoger aantal invullen.",
+
+"save_settings" =>
+"Bewaar instellingen",
+
+"member_import_complete" =>
+"Het importeren van de leden is voltooid",
+
+"return_to_overview" =>
+"Start het importeren van de leden",
+
+"start_member_import" =>
+"Klaar om te beginnen",
+
+"members_remaining" =>
+"Totaal aantal leden te gaan:",
+
+"click_to_import_members" =>
+"Importeer leden %x t/m %y",
+
+"no_table_rows" =>
+"Er bestaan geen rijen",
+
+"default_member_group" =>
+"Leden groep toewijzing",
+
+"member_group_instructions" =>
+"Uw geïmporteerde leden moeten toegewezen worden aan een leden groep.  Kies aan welke groep u uw leden wilt toewijzen. ",
+
+"members_imported" =>
+"Alle leden zijn geïmporteerd",
+
+"return_to_main_menu" =>
+"Terug naar het Hoofdmenu",
+
+"weblog_name" =>
+"Weblog naam",
+
+"total_weblog_entries" =>
+"Totaal aantal weblog berichten in deze weblog:",
+
+"select_destination_blog" =>
+"Selecteer de bestemming van de weblog",
+
+"destination_instructions" =>
+"Kies de ExpressionEngine weblog waar u de pMachine berichten geïmporteerd wilt hebben.",
+
+"destination_note" =>
+"Let Op: Als een weblog niet bestaat zult u het eerst aan moeten maken in de ADMIN pagina",
+
+"entries_per_cycle" =>
+"Aantal weblog berichten per cyclus",
+
+"select_destination_fields" =>
+"Selecteer het bestemmings veld",
+
+"field_destination_instructions" =>
+"Selecteer het ExpressionEngine veld waar u de pMachine velden in geïmporteerd wilt hebben.",
+
+"field_destination_instructions_two" =>
+"Kies &quot;Niet importeren&quot; bij de velden die niet geïmporteerd hoeven te worden.",
+
+"fields_not_unique_warning" =>
+"U kunt niet meer dan 1 veld uit pMachine toekennen aan hetzelfde veld uit ExpressionEngine",
+
+"no_fields_assigned" =>
+"De weblog die u gekozen heeft heeft geen toegekende veldgroep. Ga naar de ADMIN pagina en ken een veldgroep toe aan deze weblog.",
+
+"no_textarea_fields" =>
+"De veldgroep die u toegekent heeft aan deze weblog bevat geen tekstvelden",
+
+"pmachine_field" =>
+"pMachine veld",
+
+"ee_field" =>
+"ExpressionEngine veld",
+
+"none" =>
+"Niet importeren",
+
+"blurb" =>
+"Samenvatting",
+
+"body" =>
+"Hoofdtekst",
+
+"more" =>
+"Meer",
+
+"custom1" =>
+"Custom1",
+
+"custom2" =>
+"Custom2",
+
+"custom3" =>
+"Custom3",
+
+"you_must_select_fields" =>
+"U heeft geen enkele veld toekenning geselecteerd",
+
+"entries_remaining" =>
+"Aantal berichten te gaan:",
+
+"entries_imported" =>
+"Weblog berichten zijn geïmporteerd",
+
+"click_to_import_entries" =>
+"Importeer de weblog berichten %x t/m %y",
+
+"batch_complete" =>
+"Batch Voltooid...",
+
+"select_upload_blog" =>
+"Selecteer de afbeelding upload locatie",
+
+"upload_instructions" =>
+"Als uw pMachine weblog berichten foto&#39;s bevatten, zullen we deze moeten omzetten naar de nieuwe lokatie.",
+
+"upload_note" =>
+"Let Op:  Dit importeer programma zal uw huidige foto&#39;s niet verplaatsen.  Dit zult u handmatig moeten doen naar de gekozen bestemming.",
+
+"note_regarding_categories" =>
+"Betreffende de Weblog Categorieën",
+
+"category_note" =>
+"Als uw pMachine weblog berichten zijn toegekend aan categorieën, dan moet u precies dezelfde categorieën ook aanmaken in ExpressionEngine.  Tijdens het importeren zal elke pMachine categorie gekoppeld worden aan de ExpressionEngine categorie.  Als het geen overeenkomstige categorie kan vinden zal deze genegeert worden voor dat bericht.",
+
+"import_mailinglist" =>
+"Importeer Mailing Lijst",
+
+"total_emails" =>
+"Totaal aantal Emails:",
+
+"emails_remaining" =>
+"Total Emails te gaan:",
+
+"emails_per_cycle" =>
+"Aantal Email Adressen per cyclus",
+
+"email_instructions" =>
+"Om te voorkomen dat er timeout problemen ontstaan bij het importeren van de email adressen zal dit in zogenaamde batches gebeuren.  Deze actie zal niet veel processorkracht vragen dus 5000 email adressen per batch is voldoende voor een gemiddelde server. U kunt het aantal hier eventueel veranderen.",
+
+"mailinglist" =>
+"Mailing Lijst",
+
+"mailinglist_imported" =>
+"De Mailing Lijst is geïmporteerd",
+
+"click_to_import_mailinglist" =>
+"Importeer de email adressen %x t/m %y",
+
+"clear_preferences" =>
+"Voorkeuren verwijderen",
+
+"click_to_clear_prefs" =>
+"Klik hier om de voorkeuren te verwijderen",
+
+"clear_preferences_info" =>
+"Tijdens het importeren hebben we al uw voorkeuren opgeslagen in uw config bestand.  Omdat u nu klaar bent met importeren is het aan te raden at u deze data verwijderd. ",
+
+"you_are_done_importing" =>
+"Het importeren is succesvol verlopen!",
+
+''=>''
+);
+?>
