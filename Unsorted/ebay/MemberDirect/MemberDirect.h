1741eed5b156b2a32a052c4865621dc9636a90d5
me-suzy <me.suzana@gmail.com>
2025-08-13 03:24:54 +0300

    Add Unsorted/ebay/MemberDirect/MemberDirect.h

---
+++ Unsorted/ebay/MemberDirect/MemberDirect.h
@@ -0,0 +1,10 @@
+/*	$Id: MemberDirect.h,v 1.3 1999/02/21 02:23:30 josh Exp $	*/
+#if !defined(AFX_MEMBERDIRECT_H__96E87D67_480F_11D2_981D_00400541AE82__INCLUDED_)
+#define AFX_MEMBERDIRECT_H__96E87D67_480F_11D2_981D_00400541AE82__INCLUDED_
+
+// MEMBERDIRECT.H - Header file for your Internet Server
+//    MemberDirect Filter
+
+#include "resource.h"
+
+#endif // !defined(AFX_MEMBERDIRECT_H__96E87D67_480F_11D2_981D_00400541AE82__INCLUDED)
