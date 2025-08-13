acdb57025708c7baaf212a42f01d9af7432eca1b
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:23 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.publish.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.publish.php
@@ -0,0 +1,450 @@
+<?php
+$L = array(
+
+"status_changed" =>
+"Status Changed",
+
+"publish" =>
+"Toevoegen",
+
+"no_weblog_exits" =>
+"U probeert toegang te krijgen tot een weblog die niet bestaat",
+
+"no_entries_matching_that_criteria" =>
+"Er zijn geen berichten die aan uw criteria voldoen",
+
+"no_entries_exist" =>
+"Er zijn geen berichten in deze weblog",
+
+"select_blog_to_post_in" =>
+"Kies een weblog om een bericht in te plaatsen",
+
+"select_blog_to_edit" =>
+"Kies een weblog om te bekijken",
+
+"this_entry_will_appear_in" =>
+"Dit bericht zal verschijnen in:",
+
+"unauthorized_for_this_blog" =>
+"U bent niet gemachtigd om in deze weblog een bericht te plaatsen",
+
+"unauthorized_for_any_blogs" =>
+"U bent niet gemachtigd berichten te plaatsen",
+
+"unauthorized_to_delete_others" =>
+"U bent alleen gemachtigd om uw eigen berichten te verwijderen",
+
+"unauthorized_to_delete_self" =>
+"U bent niet gemachtigd om uw eigen berichten te verwijderen",
+
+"weblog" =>
+"Weblog",
+
+"weblogs" =>
+"Weblogs",
+
+"new_entry" =>
+"Nieuw bericht",
+
+"button_mode" =>
+"Knoppen Modus",
+
+"guided" =>
+"Begeleid",
+
+"normal" =>
+"Normaal",
+
+"view_entry" =>
+"Bekijk Bericht",
+
+"preview_entry" =>
+"Voorbeeld v/h Bericht",
+
+"edit_entry" =>
+"Wijzig bericht",
+
+"edit_weblog_entries" =>
+"Wijzig Weblog Bericht",
+
+"continue_editing" =>
+"Verder Wijzigen",
+
+"entry_status" =>
+"Bericht Status",
+
+"status" =>
+"Status",
+
+"comments" =>
+"Reacties",
+
+"open" =>
+"Open",
+
+"close" => 
+"Close",
+
+"closed" =>
+"Gesloten",
+
+"options" =>
+"Opties",
+
+"sticky" =>
+"Zet Bericht Vast",
+
+"allow_comments" =>
+"Reacties Toegestaan",
+
+"allow_trackbacks" =>
+"Trackbacks Toegestaan",
+
+"ping_sites" =>
+"Sites om naar te pingen",
+
+"ping_urls" =>
+"Trackback URLs om naar te pingen",
+
+"auto_discovery" =>
+"Trackback Auto-discovery",
+
+"select_entries_to_ping" =>
+"Selecteer de berichten die u gepingt wilt hebben",
+
+"view_previous_pings" =>
+"Bekijk eerdere Pings",
+
+"previiously_pinged_urls" =>
+"U heeft heeft eerder de volgende URL&#39;s gepingt",
+
+"date" =>
+"Datum",
+
+"expiration_date" =>
+"Datum Van Afloop",
+
+"invalid_date_formatting" =>
+"De datum die u ingevoerd heeft is niet in het juiste formaat geschreven (jaar-maand-dag uur:minuut)",
+
+"date_outside_of_range" =>
+"De datum die u heeft ingevoerd valt buiten de toegestane reeks (1902 - 2037).",
+
+"category" =>
+"Categorie",
+
+"categories" =>
+"Categoriën",
+
+"no_categories" =>
+"Er bestaat geen Categorie",
+
+"title" =>
+"Titel",
+
+"url_title" =>
+"URL Titel",
+
+"author" =>
+"Auteur",
+
+"newline_format" =>
+"Formaat:",
+
+"none" =>
+"Geen",
+
+"link" =>
+"Link",
+
+"image" =>
+"Plaatje",
+
+"email" =>
+"Email",
+
+"upload" =>
+"Uploaden",
+
+"file_upload" =>
+"Bestand Uploaden",
+
+"emoticons" =>
+"Emoticons",
+
+"choose_a_destination_for_emoticon" =>
+"Kies de bestemming van uw emoticon",
+
+"click_emoticon" =>
+"Klik op een foto om het in uw bericht te plaatsen",
+
+"no_emoticons" =>
+"kan de locatie van uw emoticons niet vinden",
+
+"close_all" =>
+"Alles afsluiten",
+
+"missing_title" =>
+"Uw bericht moet een titel hebben.",
+
+"missing_date" =>
+"Uw bericht moet een datum hebben.",
+
+"title_not_unique" =>
+"Er bestaat al een bericht met deze titel. De titel moet uniek zijn.",
+
+"url_title_not_unique" =>
+"Er bestaat al een bericht met deze URL titel. URL titels moeten uniek zijn.",
+
+"entry_has_been_added" =>
+"Nieuw Bericht Toegevoegd",
+
+"entry_has_been_updated" =>
+"Bericht Is Ge-update",
+
+"trackback_url_errors" =>
+"De volgende trackback pings zijn niet geaccepteerd:",
+
+"xmlrpc_ping_errors" =>
+"De volgende XML-RPC pings zijn niet geaccepteerd:",
+
+"click_to_view_your_entry" =>
+"Klik hier om uw bericht te bekijken",
+
+"view" =>
+"Bekijken",
+
+"edit_this_entry" =>
+"Wijzig Dit Bericht",
+
+"date_range" =>
+"Datum Reeks",
+
+"today" =>
+"Vandaag",
+
+"past_week" =>
+"Afgelopen 7 dagen",
+
+"past_month" =>
+"Afgelopen 30 dagen",
+
+"past_six_months" =>
+"Afgelopen 180 dagen",
+
+"past_year" =>
+"Afgelopen 365 dagen",
+
+"any_date" =>
+"Alle Data",
+
+"results_per_page" =>
+"Resultaten Per Pagina",
+
+"order" =>
+"Sorteren",
+
+"ascending" =>
+"Oudste Eerst",
+
+"descending" =>
+"Nieuwste Eerst",
+
+"alpha" =>
+"Alfabetisch",
+
+"delete_confirm" =>
+"Bevestig verwijdering",
+
+"delete_entry_confirm" =>
+"Weet u zeker dat u dit bericht permanent wilt verwijderen?",
+
+"delete_entries_confirm" =>
+"Weet u zeker dat u deze berichten permanent wilt verwijderen?",
+
+"entries_deleted" =>
+"Verwijderd",
+
+"html_buttons_no_cursor" =>
+"Om deze knoppen te kunnen gebruiken moet u eerst de cursor in een veld plaatsen",
+
+"html_buttons_url_text" =>
+"Voer de URL in",
+
+"html_buttons_webpage_text" =>
+"Voer de titel van de link in",
+
+"html_buttons_title_text" =>
+"Optioneel: Plaats een titel kenmerk",
+
+"html_buttons_image_text" =>
+"Voer de URL van het plaatje in",
+
+"html_buttons_email_text" =>
+"Voer het email adres in",
+
+"html_buttons_email_title" =>
+"Voer de titel van de link in (of laat het veld leeg om het email adres als titel te gebruiken.)",
+
+"html_buttons_enter_text" =>
+"Voer de tekst in die u wilt opmaken",
+
+"warning" =>
+"Waarschuwing",
+
+"file_exists" =>
+"Er bestaat al een bestand met deze naam",
+
+"overwrite_instructions" =>
+"U kunt er voor kiezen het bestand een nieuwe naam te geven, of u kunt het bestand toevoegen met dezelfde naam waarna het bestand met dezelfde naam overschreven zal worden.",
+
+"select_destination_dir" =>
+"Selecteer een bestemming",
+
+"file_type" =>
+"Bestandstype",
+
+"non_image" =>
+"Bestand",
+
+"success" =>
+"Succesvol!",
+
+"you_have_uploaded" =>
+"U heeft het volgende bestand ge-upload:",
+
+"choose_a_destination" =>
+"Waar moet het bestand verschijnen?",
+
+"no_entry_fields" =>
+"Er zijn geen berichtvelden in deze weblog.",
+
+"no_comments_or_trackbacks" =>
+"Er zijn geen reacties en trackbacks voor dit bericht",
+
+"posted_by" =>
+"Auteur:",
+
+"located_in" =>
+"Locatie:",
+
+"comment_date" =>
+"Datum:",
+
+"comment_email" =>
+"Email:",
+
+"comment_url" =>
+"URL:",
+
+"comment_action" =>
+"Actie:",
+
+"comment_ip" =>
+"IP:",
+
+"weblog_name" =>
+"Weblog:",
+
+"comment" =>
+"Reactie",
+
+"missing_comment" =>
+"U moet een reactie toevoegen",
+
+"edit_comment" =>
+"Reactie Wijzigen",
+
+"delete_comment" =>
+"Reactie Verwijderen",
+
+"comment_deleted" =>
+"Reactie Verwijderd",
+
+"delete_trackback" =>
+"Verwijder Trackback",
+
+"trackback_deleted" =>
+"Trackback Verwijderd",
+
+"comment_updated" =>
+"Reactie Ge-update",
+
+"delete_comment_confirm" =>
+"Weet u zeker dat u deze reactie permanent wilt verwijderen?",
+
+"delete_trackback_confirm" =>
+"Weet u zeker dat u deze trackback permanent wilt verwijderen?",
+
+"name" =>
+"Naam",
+
+"url" =>
+"URL",
+
+"location" =>
+"Locatie",
+
+"continue" =>
+"Verder",
+
+"image_options" =>
+"Afbeelding opties",
+
+"create_popup" =>
+"Een pop-up voor deze afbeelding maken?",
+
+"create_thumb" =>
+"Maak een Thumbnail van deze afbeelding",
+
+"constrain_proportions" =>
+"Verander verhouding",
+
+"thumb_settings" =>
+"Instellingen van Afbeelding",
+
+"choose_a_destination_for_thumb" =>
+"Waar moet de afbeelding verschijnen?",
+
+"do_not_place_file" =>
+"Plaats geen bestanden in het bericht",
+
+"image_size_not_different" =>
+"De hoogte en breedte die u heeft ingevoerd zijn hetzelfde als het origineel.",
+
+"width" =>
+"Breedte",
+
+"height" =>
+"Hoogte",
+
+"pixels" =>
+"Pixels",
+
+"percent" =>
+"Percentage",
+
+"resize_image" =>
+"Verklein Afbeelding",
+
+"create_thumb_copy" =>
+"Maak een aparte kopie",
+
+"resize_original" =>
+"Verklein de originele afbeelding",
+
+"thumb_instructions" =>
+"U kunt optioneel uw ge-uploade afbeelding verkleinen, of een aparte thumbnail maken.",
+
+"thumbnail_created" =>
+"Thumbnail Gemaakt",
+
+"image_resized" =>
+"Uw afbeelding is verkleind",
+
+"close_window" =>
+"Sluit Venster",
+
+''=>''
+);
+?>
