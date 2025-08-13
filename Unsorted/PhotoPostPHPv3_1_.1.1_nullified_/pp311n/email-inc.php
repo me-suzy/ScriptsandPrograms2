965880b4d81d9e395aa4304eefd054fa40d09499
me-suzy <me.suzana@gmail.com>
2025-08-13 07:40:07 +0300

    Add Unsorted/PhotoPostPHPv3_1_.1.1_nullified_/pp311n/email-inc.php

---
+++ Unsorted/PhotoPostPHPv3_1_.1.1_nullified_/pp311n/email-inc.php
@@ -0,0 +1,19 @@
+<?
+//////////////////////////// COPYRIGHT NOTICE //////////////////////////////
+// Program Name  	 : PhotoPost PHP                                  //
+// Program Version 	 : 3.11                                           //
+// Contributing Developer: Michael Pierce                                 //
+// Supplied By           : Poncho                                         //
+// Nullified By          : CyKuH [WTN]                                    //
+//  This script is part of PhotoPost PHP, a software application by       //
+// All Enthusiast, Inc.  Use of any kind of part or all of this           //
+// script or modification of this script requires a license from All      //
+// Enthusiast, Inc.  Use or modification of this script without a license //
+// constitutes Software Piracy and will result in legal action from All   //
+//                                                                        //
+//           PhotoPost Copyright 2002, All Enthusiast, Inc.               //
+//                       Copyright WTN Team`2002                          //
+////////////////////////////////////////////////////////////////////////////
+// This file is no longer needed
+?>
+
