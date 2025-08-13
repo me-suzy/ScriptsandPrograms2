bb2f2b00e6fc4a8696a6541d8198a65c2df7bd89
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:32 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-lib_inc-header.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-lib_inc-header.php
@@ -0,0 +1,36 @@
+<?php
+
+##################################################
+
+# /lib/inc-header.php
+
+##################################################
+
+define( "TB1_BROWSE", "EXPLORA" );
+define( "TB1_BROWSE_HELP", "Feu clic per tornar a la pàgina d'inici." );
+define( "TB1_SEARCH", "RECERCA" );
+define( "TB1_SEARCH_HELP", "Feu clic aquÌ per accedir a la utilitat de Recerca AvanÁada de Música." );
+define( "TB1_RANDOM", "ALEATORI");
+define( "TB1_RANDOM_HELP", "Feu clic per accedir a l'eina de selecció aleatòria de música." );
+define( "TB1_COMMUNITY", "COMUNITAT" );
+define( "TB1_COMMUNITY_HELP", "Feu clic per accedir a la secció comunitària." );
+define( "TB1_RADIO", "RÀDIO" );
+define( "TB1_RADIO_HELP", "Feu clic per sintonitzar la ràdio internet associada a aquest lloc." );
+define( "TB1_PLISTS", "LLISTES DE CANÇONS" );
+define( "TB1_PLISTS_HELP", "Feu clic per veure o modificar les llistes de cançons existents." );
+define( "TB1_ACCOUNT", "COMPTE" );
+define( "TB1_ACCOUNT_HELP", "Feu clic per veure o modificar la informació del vostre compte d'usuari." );
+define( "TB1_PREFS", "FAVORITS" );
+define( "TB1_PREFS_HELP", "Feu clic per veure o modificar les preferències del vostre compte d'usuari." );
+define( "TB1_SYSADMIN", "ADMIN." );
+define( "TB1_SYSADMIN_HELP", "Feu clic per accedir a l'eina d'administració de sistema." );
+define( "TB1_LOGIN", "ENTRA" );
+define( "TB1_LOGIN_HELP", "Feu clic per entrar al vostre compte (login)." );
+define( "TB1_LOGOUT", "SURT" );
+define( "TB1_LOGOUT_HELP", "Feu clic per sortir del vostre compte (logout)." );
+define( "TB1_REGISTER", "REGISTRE" );
+define( "TB1_REGISTER_HELP", "Feu clic per accedir al formulari de registre." );
+
+##################################################
+
+?>
