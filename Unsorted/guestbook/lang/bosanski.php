8c122fa35a0af69373f7551d9de529f61c352fc1
me-suzy <me.suzana@gmail.com>
2025-08-13 05:00:47 +0300

    Add Unsorted/guestbook/lang/bosanski.php

---
+++ Unsorted/guestbook/lang/bosanski.php
@@ -0,0 +1,100 @@
+<?php
+# General
+
+$LANG["metatag"]    = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">";
+
+$LANG["BookMess1"]  = "HTML Kod je deaktiviran";
+$LANG["BookMess2"]  = "HTML Kod je aktiviran";
+$LANG["BookMess3"]  = "Prilog Dodati";
+$LANG["BookMess4"]  = "Knjigu pogledati";
+$LANG["BookMess5"]  = "Administracija";
+$LANG["BookMess6"]  = "Dobro dosli na nasu Knjigu Gostiju. Ovdje mozete ostaviti neku poruku.";
+$LANG["BookMess7"]  = "Komentar";
+$LANG["BookMess8"]  = "Nazad na formular";
+$LANG["BookMess9"]  = "Napisite komentar na ovaj prilog.";
+$LANG["BookMess10"] = "<u>Hvala lijepo za vas prilog.</u><br><br>Vas prilog je uspjesno unesen!<br>Vi cete sada biti automatski vraceni na Knjigu Gostiju.";
+
+$LANG["EmailAdminSubject"] = "Novi prilog u Knjizi Gostiju";
+$LANG["EmailGuestSubject"] = "Hvala lijepo za vas prilog";
+
+$LANG["AltIP"]    = "IP je snimljen";
+$LANG["AltICQ"]   = "ICQ poruku poslati";
+$LANG["AltEmail"] = "E-mail napisati";
+$LANG["AltUrl"]   = "Webstranu posjetiti";
+$LANG["AltAim"]   = "Instant Messenger";
+$LANG["AltCom"]   = "Komentar napisati";
+
+# Form
+$LANG["FormMess1"]   = "Da bi ste se upisali ispunite sva polja. Kada ste vas prilog poslali biti cete automatski vraceni na Knjigu Gostiju. Polja sa (*) su obavezna polja.";
+$LANG["FormMess2"]   = "Smilies su dozvoljeni";
+$LANG["FormMess3"]   = "AG Kod je dozvoljen";
+$LANG["FormMess4"]   = "Pokazati legendu";
+$LANG["FormMess5"]   = "Vas prilog ce ovako poslije da izgleda u Knjizi Gostiju.";
+$LANG["FormMess6"]   = "AG Kod nije dozvoljen";
+$LANG["FormMess7"]   = "Smilies nisu dozvoljeni";
+$LANG["FormName"]    = "Ime";
+$LANG["FormEmail"]   = "E-Mail";
+$LANG["FormLoc"]     = "Mjesto";
+$LANG["FormUrl"]     = "Web strana";
+$LANG["FormGender"]  = "Vas pol";
+$LANG["FormMale"]    = "Muski";
+$LANG["FormFemale"]  = "Zenski";
+$LANG["FormMessage"] = "Vas prilog";
+$LANG["FormSelect"]  = "Knjiga Gostiju";
+$LANG["FormUser"]    = "Korisnicko Ime";
+$LANG["FormPass"]    = "Lozinka";
+$LANG["FormReset"]   = "Ponistiti";
+$LANG["FormSubmit"]  = "Poslati";
+$LANG["FormPreview"] = "Prijegled";
+$LANG["FormBack"]    = "Nazad";
+$LANG["FormEnter"]   = "Knjiga Gostiju - Administracija<br>Prije nego sto nastavite morate se identificirati.";
+$LANG["FormButton"]  = "Dalje";
+$LANG["FormPriv"]    = "Privatna poruka";
+
+# Navigation Bar
+$LANG["NavTotal"]   = "Dosadasnji prilozi:";
+$LANG["NavRecords"] = "Prilozi po strani:";
+$LANG["NavPrev"]    = "Prosla strana";
+$LANG["NavNext"]    = "Slijedeca strana";
+
+# Post Errors
+$LANG["ErrorPost1"]  = "Niste unijeli Ime!";
+$LANG["ErrorPost2"]  = "Niste unijeli prilog!";
+$LANG["ErrorPost3"]  = "Vas prilog je ili predug ili prekratak.";
+$LANG["ErrorPost4"]  = "Nesto sto ste unijeli nije prihvatljivo!";
+$LANG["ErrorPost5"]  = "Greska u Knjizi Gostiju";
+$LANG["ErrorPost6"]  = "Vasa slika je prevelika.";
+$LANG["ErrorPost7"]  = "Format slike nije dobar.";
+$LANG["ErrorPost8"]  = "Aktivirana je zastita! Molimo sacekajte trenutak.";
+$LANG["ErrorPost9"]  = "Vasa IP Adresa je za Knjigu gostiju blokirana!";
+$LANG["ErrorPost10"] = "Vas prilog sadrzi neprihvatljive rijeci.";
+$LANG["ErrorPost11"] = "Niste jos unijeli komentar.";
+
+$LANG["PassMess1"] = "Vi se morate identificirati:";
+$LANG["PassMess2"] = "Nevazeca kombinacija korisnik/lozinka. Pokusajte ponovo.";
+$LANG["PassMess3"] = "Vasa lozinka nije ispravna.";
+
+# Days
+$weekday[0] = "Nedjelja";
+$weekday[1] = "Ponedjeljak";
+$weekday[2] = "Utorak";
+$weekday[3] = "Srijeda";
+$weekday[4] = "Cetvrtak";
+$weekday[5] = "Petak";
+$weekday[6] = "Subota";
+
+# Months
+$months[0]  = "Januar";
+$months[1]  = "Februar";
+$months[2]  = "Mart";
+$months[3]  = "April";
+$months[4]  = "Maj";
+$months[5]  = "Juni";
+$months[6]  = "Juli";
+$months[7]  = "August";
+$months[8]  = "Septembar";
+$months[9]  = "Oktobar";
+$months[10] = "Novembar";
+$months[11] = "Dezembar";
+
+?>
