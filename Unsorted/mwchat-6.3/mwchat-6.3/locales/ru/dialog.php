c60b9b0643d6b64e8af31ee86599caef0fe742bc
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:43 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/dialog.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/dialog.php
@@ -0,0 +1,83 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[DIALOG_Syntax_Error] = "óÉÎÔÁËÓÉÞÅÓËÁÑ ÏÛÉÂËÁ";
+
+$LOCALE[DIALOG_Available_Buddies] = "ðÒÉÑÔÅÌÉ";
+
+$LOCALE[DIALOG_Available_Ignored] = "éÇÎÏÒÉÒÕÅÍÙÅ ÐÏÌØÚÏ×ÁÔÅÌÉ";
+
+$LOCALE[DIALOG_On_Buddy] = "× ÓÐÉÓËÅ ×ÁÛÉÈ ÐÒÉÑÔÅÌÅÊ.";
+
+$LOCALE[DIALOG_Not_Buddy] = "ÎÅ × ÓÐÉÓËÅ ×ÁÛÉÈ ÐÒÉÑÔÅÌÅÊ.";
+
+$LOCALE[DIALOG_Add_Buddy] = "ÄÏÂÁ×ÌÅÎ × ÓÐÉÓÏË ×ÁÛÉÈ ÐÒÉÑÔÅÌÅÊ.";
+
+$LOCALE[DIALOG_Del_Buddy] = "ÕÄÁÌÅÎ ÉÚ ÓÐÉÓËÁ ×ÁÛÉÈ ÐÒÉÑÔÅÌÅÊ.";
+
+$LOCALE[DIALOG_Block] = "÷Ù ÚÁÂÌÏËÉÒÏ×ÁÌÉ ÐÒÉÅÍ ÌÉÞÎÙÈ ÓÏÏÂÝÅÎÉÊ.";
+
+$LOCALE[DIALOG_Unblock] = "÷Ù ÒÁÚÒÅÛÉÌÉ ÐÒÉÅÍ ÌÉÞÎÙÈ ÓÏÏÂÝÅÎÉÊ.";
+
+$LOCALE[DIALOG_Away] = "ÏÔÓÕÔÓÔ×ÕÅÔ.";
+
+$LOCALE[DIALOG_Back] = "×ÅÒÎÕÌÓÑ.";
+
+$LOCALE[DIALOG_Bug_Sent] = "óÏÏÂÝÅÎÉÅ ÏÂ ÏÛÉÂËÅ ÏÔÐÒÁ×ÌÅÎÏ.";
+
+$LOCALE[DIALOG_Colors] = "÷ÁÛÉ Ã×ÅÔÁ ÏÂÎÏ×ÌÅÎÙ.";
+
+$LOCALE[DIALOG_ChangeColor] = "óÍÅÎÉÔØ Ã×ÅÔ ÎÁ";
+
+$LOCALE[DIALOG_Click] = "îÁÖÍÉÔÅ, ÞÔÏÂÙ ÐÏÓÅÔÉÔØ";
+
+$LOCALE[DIALOG_OP_Only] = "ÔÏÌØËÏ ÄÌÑ ÏÐÅÒÁÔÏÒÏ×.";
+
+$LOCALE[DIALOG_Topic_Updated] = "ÏÂÎÏ×ÉÌ ÔÅÍÕ ËÏÍÎÁÔÙ.";
+
+$LOCALE[DIALOG_Smiles] = "äÏÓÔÕÐÎÙÅ ÓÍÁÊÌÙ É ×ÙÒÁÖÅÎÉÑ";
+
+$LOCALE[DIALOG_Smiles_Disabled] = "óÍÁÊÌÙ ÂÙÌÉ ÚÁÐÒÅÝÅÎÙ × ×ÁÛÉÈ Ó×ÏÊÓÔ×ÁÈ ÉÌÉ ÁÄÍÉÎÉÓÔÒÁÔÏÒÏÍ.";
+
+$LOCALE[DIALOG_Rolled] = "ÂÒÏÓÉÌ ËÏÓÔÉ!";
+
+$LOCALE[DIALOG_Not_Online] = "ÓÅÊÞÁÓ ÎÅÔ × ÏÎÌÁÊÎÅ.";
+
+$LOCALE[DIALOG_Exit] = "ÐÏËÉÎÕÌ ËÏÍÎÁÔÕ.";
+
+$LOCALE[DIALOG_Not_Ignored] = "ÎÅ ÉÇÎÏÒÉÒÕÅÔÓÑ.";
+
+$LOCALE[DIALOG_Is_Ignored] = "ÉÇÎÏÒÉÒÕÅÔÓÑ.";
+
+$LOCALE[DIALOG_Unregistered] = "ÎÅÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎÎÏÅ ÉÍÑ ÐÏÌØÚÏ×ÁÔÅÌÑ.";
+
+$LOCALE[DIALOG_No_Ignored] = "÷Ù ÎÅ ÉÇÎÏÒÉÒÕÅÔÅ ÎÉËÏÇÏ ÉÚ ÐÏÌØÚÏ×ÁÔÅÌÅÊ.";
+
+$LOCALE[DIALOG_No_Buddies] = "õ ×ÁÓ ÎÅÔ ÐÒÉÑÔÅÌÅÊ.";
+
+$LOCALE[DIALOG_Located] = "ÏÂÎÁÒÕÖÅÎ × ËÏÍÎÁÔÅ";
+
+$LOCALE[DIALOG_Located_Private] = "ÏÂÎÁÒÕÖÅÎ × ÞÁÓÔÎÏÊ ËÏÍÎÁÔÅ.";
+
+$LOCALE[DIALOG_Not_Located] = "ÎÅ ÒÁÚÒÅÛÁÅÔ ÏÂÎÁÒÕÖÉ×ÁÔØ ÓÅÂÑ.";
+
+$LOCALE[DIALOG_Kicked] = "ÕÄÁÌÅÎ ÉÚ ÓÉÓÔÅÍÙ.";
+
+$LOCALE[DIALOG_Say_Button] = "ðÏÓÌÁÔØ ÓÏÏÂÝÅÎÉÅ";
+
+$LOCALE[DIALOG_Clear_Window] = "ïÞÉÓÔÉÔØ ÜËÒÁÎ";
+
+$LOCALE[DIALOG_Private_Message] = "þÁÓÔÎÏÅ ÓÏÏÂÝÅÎÉÅ";
+	
+$LOCALE[DIALOG_Private_Message_Refresh] = "ïÂÎÏ×ÉÔØ ÓÐÉÓÏË";
+	
+?>
