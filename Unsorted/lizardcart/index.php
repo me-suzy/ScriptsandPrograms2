aa9ae3e6fc37c6311524424c49aaa9ee31a956e7
me-suzy <me.suzana@gmail.com>
2025-08-13 06:13:31 +0300

    Add Unsorted/lizardcart/index.php

---
+++ Unsorted/lizardcart/index.php
@@ -0,0 +1,22 @@
+<?php
+include ("config.inc.php");?>
+
+<? include ("header.php");?>
+
+<table width="600" cellspacing="10" cellpadding="10" align="center">
+<tr>
+	<td width="100%" height="20px">
+	<div align="center"><h1>Adamantine Health System</h1></div>
+	</td>
+</tr>	
+<tr>
+	<td>
+	<div align="center"><b>Bold Text</b></div>
+<p align="justify"><b>Lizard Cart CMS offers an easy way for people with limited HTML experience to manage the content of their websites. There is an online content editing system for all content. You can edit the contents of your Web page from anywhere in the world with just a Web browser. It comes complete with a WYSIWYG (What You See Is What You Get) online editor and you don't need any special knowledge of HTML code. You can even cut and past a Word Document right into your pages.</b></p>
+<div align="center">Regular text</div>
+<p align="justify">Lizard Cart CMS is a data base driven content management systems using php /mysql on a unix platform It may work on a windows but I have not tested yet. It has PayPal IPN, LinkPoint,Worldpay Authorize.net intgration with more to come. For those who have a merchent account at there bank there is a way to store Credit Card info encrypted on the server using mcrypt, it uses a key for encrypting/decrypting cc numbers to and from the data base in SSL.</p>
+	</td>
+</tr>
+</table>
+
+<? include ("footer.php");?>
