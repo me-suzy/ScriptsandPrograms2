81ddde19a01d9c68d6dcb20e96d07a73ab5fbe97
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:45 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-lib_inc-common.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-lib_inc-common.php
@@ -0,0 +1,75 @@
+<?php
+
+##################################################
+
+# /lib/inc-common.php
+
+##################################################
+
+define( "COMMON_ERR_DBCONN", "Kann mit den eingestellten Informationen nicht mit der Datenbank verbinden." );
+
+define( "COMMON_ACCESS_DENIED_1", "Zugriff verweigert. Sie müssen Administrator sein, um sich anmelden zu können." );
+define( "COMMON_ACCESS_DENIED_2", "Zugriff verweigert. Bitte versuchen Sie es erneut." );
+
+define( "COMMON_PLISTSELECT_1", "Neue Spielliste" );
+
+define( "COMMON_ALPHABET_AR_HELP", "Klicken um alle Interpreten anzuzeigen die mit ... beginnen" );
+define( "COMMON_ALPHABET_AL_HELP", "Klicken um alle Alben anzuzeigen die mit ... beginnen" );
+
+define( "COMMON_CHECK_IMAGE_HELP", "Klicken um das verbundene Bild anzuzeigen" );
+
+define( "COMMON_NOTRACK", "Sie haben keinen Titel aus der Liste gewählt." );
+
+define( "COMMON_PLIST_1", "Bitte wählen sie eine existierende Spielliste zum bearbeiten.\\nOder wählen sie einige Titel und erstellen Sie eine neue." );
+
+define( "COMMON_BATCHEDIT_1", "Sind Sie sicher das Sie die gewählten Titel aus Netjuke löschen wollen?" );
+
+define( "COMMON_RADIOEDIT_1", "Sind Sie sicher das Sie bestehende Spielliste komplett ersetzen wollen?" );
+
+define( "COMMON_JUKEBOXEDIT_1", "Sind Sie sicher das Sie bestehende JUKEBOX Spielliste komplett ersetzen wollen?" );
+
+define( "COMMON_BEDIT_HEADER", "ADMINISTRATOR UND BEARBEITER TOOLS" );
+define( "COMMON_BEDIT_TB_BATCHEDIT", "Mehrfach Bearbeitung" );
+define( "COMMON_BEDIT_TB_BATCHEDIT_HELP", "Hier klicken um die ausgewählten Titel zusammen zu bearbeiten." );
+define( "COMMON_BEDIT_TB_BATCHDEL", "Mehrfach Löschen" );
+define( "COMMON_BEDIT_TB_BATCHDEL_HELP", "Hier klicken um alle gewählten Titel zu löschen" );
+define( "COMMON_BEDIT_TB_RADIOADD", "Zur Radio Spielliste hinzufügen" );
+define( "COMMON_BEDIT_TB_RADIOADD_HELP", "Hier klicken um die ausgewählten Titel der Radio Spielliste hinzuzufügen." );
+define( "COMMON_BEDIT_TB_RADIOREP", "Radio Spielliste ersetzen" );
+define( "COMMON_BEDIT_TB_RADIOREP_HELP", "Hier klicken um die bestehende Radio Spielliste mit den ausgewählten Titeln zu überschreiben." );
+define( "COMMON_BEDIT_TB_RADIOPLVIEW", "Radio Spielliste anzeigen" );
+define( "COMMON_BEDIT_TB_RADIOPLVIEW_HELP", "Hier klicken um die als Radio-Spielliste benutzte Textdatei zu betrachten." );
+define( "COMMON_BEDIT_TB_JUKEBOXSTART", "Start Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXSTART_HELP", "Hier klicken um die JUKEBOX Spielliste auf dem Server abzuspielen." );
+define( "COMMON_BEDIT_TB_JUKEBOXSTOP", "Stop Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXSTOP_HELP", "Hier klicken um das Apsielen der JUKEBOX auf dem Server zu stoppen." );
+define( "COMMON_BEDIT_TB_JUKEBOXADD", "Zur Jukebox Spielliste hinzufügen" );
+define( "COMMON_BEDIT_TB_JUKEBOXADD_HELP", "Hier klicken um die ausgewählten Titel der Jukebox Spielliste hinzuzufügen." );
+define( "COMMON_BEDIT_TB_JUKEBOXREP", "Jukebox Spielliste ersetzten" );
+define( "COMMON_BEDIT_TB_JUKEBOXREP_HELP", "Hier klicken um die bestehende Jukebox Spielliste mit den ausgewählten Titeln zu überschreiben." );
+define( "COMMON_BEDIT_TB_JUKEBOXPLVIEW", "Jukebox Spielliste anzeigen" );
+define( "COMMON_BEDIT_TB_JUKEBOXPLVIEW_HELP", "Hier klicken um die als Jukebox Spielliste benutzte Textdatei zu betrachten." );
+
+define( "COMMON_SUMHEAD_QS_HEADER", "Schnell-Suche" );
+define( "COMMON_SUMHEAD_QS_OPT_AR", "Suche nach Interpreten-Name" );
+define( "COMMON_SUMHEAD_QS_OPT_TR", "Suche nach Titel-Name" );
+define( "COMMON_SUMHEAD_QS_OPT_AL", "Suche nach Album Name" );
+define( "COMMON_SUMHEAD_QS_OPT_GE", "Suche nach Genre" );
+define( "COMMON_SUMHEAD_QS_OPT_TI", "Suche nach Dauer (Sekunden)" );
+define( "COMMON_SUMHEAD_QS_BTN", "Los!" );
+
+define( "COMMON_SUMHEAD_SUMM_HEADER", "Zusammenfassung" );
+define( "COMMON_SUMHEAD_SUMM_TR", "Titel" );
+define( "COMMON_SUMHEAD_SUMM_AR", "Interpr." );
+define( "COMMON_SUMHEAD_SUMM_AL", "Alben" );
+define( "COMMON_SUMHEAD_SUMM_GE", "Genren" );
+
+define( "COMMON_SUMHEAD_ALPHA_AR_HEADER", "INTERPR." );
+define( "COMMON_SUMHEAD_ALPHA_AL_HEADER", "ALBEN" );
+
+define( "COMMON_SUMHEAD_ALL_TITLE", "Zeige alle Einträge in alphabetischer Reihenfolge" );
+define( "COMMON_SUMHEAD_ALL_LINK", "Zeige Alle" );
+
+##################################################
+
+?>
