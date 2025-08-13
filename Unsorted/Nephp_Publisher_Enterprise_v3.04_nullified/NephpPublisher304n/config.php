f09aab551e26a02069ca507594755529a5fe7e4c
me-suzy <me.suzana@gmail.com>
2025-08-13 06:39:34 +0300

    Add Unsorted/Nephp_Publisher_Enterprise_v3.04_nullified/NephpPublisher304n/config.php

---
+++ Unsorted/Nephp_Publisher_Enterprise_v3.04_nullified/NephpPublisher304n/config.php
@@ -0,0 +1,69 @@
+<?
+///////////////////////////////////////////////////////////////////////////////
+//      =   =       ====  =   = ====                                         //
+//      =   =       =   = =   = =   =                                        //
+//      ==  =  ===  =   = =   = =   =                                        //
+//      = = = =   = ====  ===== ====                                         //
+//      =  == ===== =     =   = =                                            //
+//      =   = =     =     =   = =                                            //
+//      =   =  ==== =     =   = =                                            //
+//      ------------------------------------------------------               //
+//      ====        =     ===     =         =                                //
+//      =   =       =       =               =                                //
+//      =   = =   = ====    =   ===    ==== ====   ===   ===                 //
+//      ====  =   = =   =   =     =   =     =   = =   = =   =                //
+//      =     =   = =   =   =     =    ===  =   = ===== =                    //
+//      =     =   = =   =   =     =       = =   = =     =                    //
+//      =      ===  ====  ===== ===== ====  =   =  ==== =                    //
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// Program Name         : Nephp Publisher Enterprise                         //
+// Release Version      : 3.04                                               //
+// Program Author       : Kenny Ngo     (CTO of Nelogic Technologies.)       //
+// Program Author       : Ewdision Then (CEO of Nelogic Technologies.)       //
+// Retail Price         : $499.00 United States Dollars                      //
+// WebForum Price       : $000.00 Always 100% Free                           //
+// ForumRu Price        : $000.00 Always 100% Free                           //
+// xCGI Price           : $000.00 Always 100% Free                           //
+// Supplied by          : Scoons [WTN]                                       //
+// Nullified by         : CyKuH [WTN]                                        //
+// Distribution         : via WebForum, ForumRU and associated file dumps    //
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
+// DYNAMIC SETTINGS                                     //
+//////////////////////////////////////////////////////////
+$_dfig[admin_user]="test";
+$_dfig[admin_pass]="test";
+$_dfig[admin_name]="Adminstrator";
+$_dfig[admin_email]="you_email@mydomain.com";
+$_dfig[op_span]="20";
+$_dfig[span]="10";
+$_dfig[span_review]="5";
+$_dfig[span_members]="10";
+$_dfig[main_feature_limit]="3";
+$_dfig[main_feature_row_limit]="3";
+$_dfig[num_sub_cat]="3";
+$_dfig[num_news_display]="9";
+$_dfig[num_toprate_display]="10";
+$_dfig[news_by_cat]="0";
+$_dfig[news_by_cat_numb]="4";
+$_dfig[show_sub_icons]="1";
+$_dfig[history_days]="24";
+$_dfig[archive_sub_limit]="5";
+$_dfig[show_relate_articles]="1";
+$_dfig[show_relate_numb]="5";
+$_dfig[permission_guest]="review=0|post=0|edit=0|del=0|medit=0|mdel=0|aa=0|cpanel=0";
+$_dfig[permission_usr]="review=1|post=1|edit=1|del=0|medit=0|mdel=0|aa=0|cpanel=0";
+$_dfig[permission_mod]="review=1|post=1|edit=1|del=1|medit=1|mdel=1|aa=1|cpanel=0";
+$_dfig[permission_admin]="review=1|post=1|edit=1|del=1|medit=1|mdel=1|aa=1|cpanel=1";
+$_dfig[platform]="Unix";
+$_dfig[allow_guest_review]="1";
+$_dfig[sendmail]="/usr/sbin/sendmail -t";
+$_dfig[smtp]="smtp.youdomain.com";
+$_dfig[upload_limit]="1000000";
+$_dfig[auto_sign_up]="1";
+$_dfig[auto_approve]="0";
+$_dfig[static_pages]="0";
+$_dfig[auto_update]="0";
+$_dfig[template]="babyblue";
+?>
