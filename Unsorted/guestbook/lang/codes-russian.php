f23dd0e89b3610071413ec77b761e812573061d0
me-suzy <me.suzana@gmail.com>
2025-08-13 05:01:14 +0300

    Add Unsorted/guestbook/lang/codes-russian.php

---
+++ Unsorted/guestbook/lang/codes-russian.php
@@ -0,0 +1,284 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<HTML>
+
+<HEAD>
+
+<TITLE>Smilies And AGCodes</TITLE>
+
+<META content="text/html; charset=windows-1251" http-equiv=Content-Type>
+
+<style type="text/css">
+
+<!--
+
+td {  font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 8pt}
+
+-->
+
+</style>
+
+</HEAD>
+
+<BODY bgColor=#ffffff link=#000080 text=#000000 vLink=#000080>
+
+<CENTER>
+
+  <table width="95%" border="0" cellspacing="1" cellpadding="0">
+
+    <tr>
+
+      <td height="25"> ×ÒÎ ÒÀÊÎÅ&nbsp;&nbsp;&nbsp;S M I L I E S ?</td>
+
+    </tr>
+
+    <tr>
+
+      <td>
+
+        <p>'Smilies' - íåáîëüøèå ãðàôè÷åñêèå
+        èçîáðàæåíèÿ, îòîáðàæàþùèå íàñòðîåíèå.
+        Åñëè Âû êîãäà-íèáóäü ïîëüçîâàëèñü
+        èíòåðíåò ÷àòàìè, òî íàâåðíÿêà çíàêîìû
+        ñî ñìàéëèêàìè. Smiles çàìåíÿåò ýòè
+        ñìàéëèêè íà ãðàôè÷åñêèå èçîáðàæåíèÿ.</p>
+
+        <p>Ñïèñîê äîñòóïíûõ Smiles: </p>
+
+      </td>
+
+    </tr>
+
+  </table>
+
+  <table bgcolor=#f7f7f7 border=0 width="95%" cellspacing="1" cellpadding="4">
+
+    <tbody>
+
+    <tr>
+
+      <td bgcolor="#996699"><font color="#ffffff"><b>Ñìàéëèêè</b></font></td>
+
+      <td bgcolor="#996699"><font color="#ffffff"><b>Ýìîöèÿ</b></font></td>
+
+      <td bgcolor="#996699"><font color="#ffffff"><b>Òàê áóäåò
+        âûãëÿäåòü</b></font></td>
+
+    </tr>
+
+
+
+<?php include ("./smilies.inc"); ?>
+
+
+
+    </tbody>
+
+  </table>
+
+  <br>
+
+  <table width="95%" border="0" cellspacing="1" cellpadding="0">
+
+    <tr>
+
+      <td height="25">×ÒÎ ÒÀÊÎÅ&nbsp;&nbsp;&nbsp;A D V A
+
+        N D C E D &nbsp;&nbsp;&nbsp;G U E S T B O O K &nbsp;&nbsp;&nbsp;C O D E&nbsp;
+        (AGcode) ? </td>
+
+    </tr>
+
+    <tr>
+
+      <td>
+
+        <p>AGCode ðàçíîâèäíîñòü HTML òýãîâ. Âû ìîæåòå
+        èñïîëüçîâàòü AGCode åñëè HTML îòêëþ÷åíî â
+        Ãîñòåâîé êíèãå.<p>Äîñòóïíûå AGCodes:
+
+      </td>
+
+    </tr>
+
+  </table>
+
+<TABLE border=0 cellPadding=0 cellSpacing=0 width="95%" align="center">
+
+  <TBODY>
+
+  <TR>
+
+    <TD bgColor=#000000>
+
+      <TABLE border=0 cellPadding=4 cellSpacing=1 width="100%">
+
+        <TBODY>
+
+        <TR bgColor=#0099CC>
+
+          <TD><b><font color="#FFFFFF">URL Hyperlinking</font></b></TD>
+
+        </TR>
+
+        <TR bgColor=#ffffff>
+
+          <TD>Åñëè AGCode âêëþ÷åí, Âàì íå íóæíî
+            èñïîëüçîâàòü äëèííûé êîä äëÿ
+            ñîçäàíèÿ ññûëêè, ïðîñòî èñïîëüçóéòå
+            òýã [URL]:
+
+            <UL>
+
+              <LI>Äëÿ äîáàâëåíèÿ URL â ñîîáùåíèå
+                èñïîëüçóéòå ñëåäóþùèé êîä (AGCode - <font color="#ff0000">êðàñí</font>).
+
+                <P align="center">
+
+                    <FONT color=#ff0000>[url]</FONT>http://www.proxy2.de<FONT color=#ff0000>[/url]</FONT>
+
+                <p>Çàïîìíèòå, Âû äîëæíû
+                èñïîëüçîâàòü ïîëíûé http:// àäðåñ èëè
+                êîðîòêèé, íà÷èíàþùèéñÿ ñ www. Åñëè â
+                àäðåñå íå ïðèñóòñòâóåò &quot;www&quot;, Âû
+                äîëæíû íà÷àòü åãî ñ &quot;http://&quot;.
+
+                <BR>
+
+              <LI>Âû ìîæåòå èñïîëüçîâàòü [url] êîä è â
+                òàêîì ôîðìàòå: <br><br>
+
+                <CENTER>
+
+                  <FONT color=#ff0000>[url=http://www.proxy2.de]</FONT>hyperlink<FONT color=#ff0000>[/url]</FONT></CENTER>
+
+            </UL>
+
+          </TD>
+
+        <TR bgColor=#0099CC>
+
+          <TD><b><font color="#FFFFFF">E-mail</font></b></TD>
+
+        </TR>
+
+        <TR bgColor=#ffffff>
+
+          <TD>Äëÿ äîáàâëåíèÿ e-mail â ñîîáùåíèå
+            èñïîëüçóéòå ñëåäóþùèé êîä (AGCode - <font color="#ff0000">êðàñí</font>).
+
+            <P>
+
+              <CENTER>
+
+                <FONT color=#ff0000>[email]</FONT>webmaster@proxy2.de<FONT color=#ff0000>[/email]<br>
+                </FONT>
+
+              </CENTER>
+
+          </TD>
+
+        </TR>
+
+        <TR bgColor=#0099CC>
+
+          <TD><b><font color="#FFFFFF">Bold è Italics</font></b></TD>
+
+        </TR>
+
+        <TR bgColor=#ffffff>
+
+          <TD>Èñïîëüçóéòå îáû÷íûå [b] [/b] è [i] [/i]
+            òýãè.
+
+            <P>
+
+              <CENTER>
+
+                Hello, <FONT color=#ff0000>[b]</FONT><B>John</B><FONT color=#ff0000>[/b]</FONT><BR><br>
+
+                Hello, <FONT color=#ff0000>[i]</FONT><I>Maria</I><font color="#FF0000">[i]<br>
+                </font>
+
+              </CENTER>
+
+          </TD>
+
+        </TR>
+
+        <TR bgColor=#0099CC>
+
+          <TD><b><font color="#FFFFFF">Äîáàâëåíèå
+            èçîáðàæåíèé</font></b></TD>
+
+        </TR>
+
+        <TR bgColor=#ffffff>
+
+          <TD>Äëÿ äîáàâëåíèÿ èçîáðàæåíèÿ â
+            ñîîáùåíèå èñïîëüçóéòå ñëåäóþùèé êîä
+            (AGCode - <font color="#ff0000">êðàñí</font>).
+
+            <P>
+
+              <CENTER>
+
+                <FONT color=#ff0000>[img]</FONT>http://www.yourURL.com/image/logo.gif<FONT color=#ff0000>[/img]</FONT>
+
+              </CENTER>
+
+            <P>Çàïîìíèòå: &quot;http://&quot; ÷àñòü
+            ÎÁßÇÀÒÅËÜÍÀ â <FONT color=#ff0000>[img]</FONT>  êîäå.<br>
+            </P>
+
+          </TD>
+
+        </TR>
+
+        </TBODY>
+
+      </TABLE>
+
+    </TD>
+
+  </TR>
+
+  </TBODY>
+
+</TABLE>
+
+<table width="95%" border="0" cellspacing="1" cellpadding="4" align="center">
+
+  <tr>
+
+    <td><font color="#800000">Çàïîìíèòå:</font><br>
+
+      Âû íå äîëæíû èñïîëüçîâàòü âìåñòå HTML è AGCode
+      äëÿ îäíîé è òîé æå ôóíêöèè. Â AGCode <font color=#ff0000>[URL]</font>
+      = <font color=#ff0000>[url]</font>.<br><br>
+
+      <font color="#800000">Íåïðàâèëüíîå èñïîëüçîâàíèå AGCode:</font> <br>
+
+       <font color="#ff0000">[url]</font> www.proxy2.de <font color=#ff0000>[/url]</font> -
+      íå èñïîëüçóéòå ïðîáåëû ïåðåä èëè ïîñëå
+      ññûëêè.<br>
+
+        <br>
+
+        <font color="#ff0000">[email]</font>webmaster@proxy2.de<font color=#ff0000>[email]</font> -
+      íå çàáûâàéòå ñëåø â çàêðûâàþùåì òýãå (<font color=#ff0000>[/email]</font>)
+
+    </td>
+
+  </tr>
+
+</table>
+
+</CENTER>
+
+<BR>
+
+</BODY>
+
+</HTML>
+
