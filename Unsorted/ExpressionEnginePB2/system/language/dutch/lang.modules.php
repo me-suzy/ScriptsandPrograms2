ff56fa14e0f3b2a91f1b1cdce7f23fb2059babf3
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:14 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.modules.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.modules.php
@@ -0,0 +1,49 @@
+<?php
+$L = array(
+
+
+"module_name" =>
+"Module Naam",
+
+"module_access" =>
+"Module Wijzigen",
+
+"module_backend" =>
+"Gebruikers CP",
+
+"module_version" =>
+"Versie",
+
+"module_status" =>
+"Status",
+
+"module_description" =>
+"Beschrijving",
+
+"module_action" =>
+"Actie",
+
+"not_installed" =>
+"Niet geïnstalleerd",
+
+"installed" =>
+"Geïnstalleerd",
+
+"install" =>
+"Installeren",
+
+"deinstall" =>
+"Verwijderen",
+
+"module_can_not_be_found" =>
+"Kan de bestanden niet vinden die nodig zijn om deze module te installeren",
+
+"module_has_been_installed" =>
+"Geïnstalleerde modules:",
+
+"module_has_been_removed" =>
+"Verwijderde modules:",
+
+''=>''
+);
+?>
