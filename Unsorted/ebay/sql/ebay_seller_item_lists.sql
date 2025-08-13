29cd7356a899e13e9bbbd7ab2082005b38daa631
me-suzy <me.suzana@gmail.com>
2025-08-13 03:32:37 +0300

    Add Unsorted/ebay/sql/ebay_seller_item_lists.sql

---
+++ Unsorted/ebay/sql/ebay_seller_item_lists.sql
@@ -0,0 +1,38 @@
+/*	$Id: ebay_seller_item_lists.sql,v 1.2 1999/02/21 02:54:03 josh Exp $	*/
+/*
+ * ebay_seller_item_lists.sql
+ *
+ */
+	drop table ebay_seller_item_lists;
+
+	create table ebay_seller_item_lists
+	(
+		id						int
+			constraint			item_lists_id_nn
+			not null,
+		item_count				int
+			constraint			item_lists_item_count_nn
+			not null,
+		item_list_size			int
+			constraint			item_lists_list_size_nn
+			not null,
+		item_list_size_used		int
+			constraint			item_lists_list_used_nn
+			not null,
+		item_list_valid			char(1)
+			constraint			item_lists_valid_nn
+			not null,
+		item_list				long raw,
+		constraint				item_lists_pk
+			primary key(id)
+			using index	storage(initial 50m next 20m)
+							tablespace ruseri06,
+		constraint				item_lists_fk
+			foreign key (id)
+			references	ebay_users(id)
+	)
+	tablespace ruserd06
+	storage (initial 100M next 20M);
+
+	commit;
+
