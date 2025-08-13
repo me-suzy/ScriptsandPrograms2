95152657e403b740a5bd8545e88e6d317cbc3bf5
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:53 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-search-adv.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-search-adv.php
@@ -0,0 +1,40 @@
+<?php
+
+##################################################
+
+# /search-adv.php
+
+##################################################
+
+define( "SEARCH_ADV_HEADER", "Recerca AvanÁada de Música" );
+
+define( "SEARCH_ADV_FORM_COND", "CondiciÛ" );
+define( "SEARCH_ADV_FORM_COND_AND", "I" );
+define( "SEARCH_ADV_FORM_COND_OR", "O" );
+define( "SEARCH_ADV_FORM_TR", "TÃtol de la canÁÛ" );
+define( "SEARCH_ADV_FORM_AR", "Nom de l'intÀrpret" );
+define( "SEARCH_ADV_FORM_AL", "TÃtol de l'àlbum" );
+define( "SEARCH_ADV_FORM_GE", "Nom del gÀnere" );
+define( "SEARCH_ADV_FORM_TI", "Durada de la pista" );
+define( "SEARCH_ADV_FORM_SECONDS", "Segons" );
+define( "SEARCH_ADV_FORM_TN", "Número de pista" );
+define( "SEARCH_ADV_FORM_BR", "Bit Rate" );
+define( "SEARCH_ADV_FORM_SR", "Sample Rate" );
+define( "SEARCH_ADV_FORM_FK", "Tipus de fitxer" );
+define( "SEARCH_ADV_FORM_FS", "Mida del fitxer" );
+define( "SEARCH_ADV_FORM_FN", "UbicaciÛ" );
+define( "SEARCH_ADV_FORM_CM", "Comentaris" );
+define( "SEARCH_ADV_FORM_LY", "Lletres" );
+define( "SEARCH_ADV_FORM_LC", "Comptador d'audicions" );
+
+define( "SEARCH_ADV_FORM_BTN_SEARCH", "Recerca" );
+define( "SEARCH_ADV_FORM_BTN_RESET", "Refer" );
+
+define( "SEARCH_ADV_CLOSEWIN", "Tanca la finestra" );
+define( "SEARCH_ADV_CLOSEWIN_HELP", "Tanca la finestra" );
+
+define( "SEARCH_ADV_ANY_VALUE", "Qualsevol valor" );
+
+##################################################
+
+?>
