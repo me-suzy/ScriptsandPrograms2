c5446f5070c42b823eb276853962afdd93f2b320
me-suzy <me.suzana@gmail.com>
2025-08-13 07:03:18 +0300

    Add Unsorted/newsgroup/lang/de/global.php

---
+++ Unsorted/newsgroup/lang/de/global.php
@@ -0,0 +1,251 @@
+<?
+// Language:                    German
+// Translator:                  Marcel Pertermann
+// Created:                     12/07/2002
+// Last Update:                 12/07/2002
+// MyNewsGroups :) Version:     0.4
+
+
+// Charset
+define("_CHARSET","charset = iso-8859-1");
+
+// HOW THE PHP date() function displays the date
+define("_MYNGDATEDISPLAY","d-m-Y"); 
+
+// Header
+define("_MYNGWELCOME"," Willkommen bei MyNewsGroups :) v ");
+define("_MYNGSYSTEM_INFO"," Systeminformation:");
+define("_MYNGMENU_HOME"," Start ");
+define("_MYNGMENU_NEWSGROUPS"," News Gruppen ");
+define("_MYNGMENU_STATS"," Statistik ");
+define("_MYNGMENU_SEARCH"," Suchen ");
+define("_MYNGMENU_ABOUT"," &Uuml;ber ");
+
+// Footer
+define("_MYNGHOME_PAGE","Projekt Startseite:");
+define("_MYNGGPL","GPL Lizensiert");
+
+// Login
+define("_MYNGLOGIN","Anmelden");
+define("_MYNGPASSWORD","Kennwort");
+define("_MYNGREGISTER","Registrieren");
+
+// NewsGroups
+define("_MYNGBROWSE_NG","NewsGroups durchsuchen");
+define("_MYNGARTICLE_NUMBER","Anzahl Artikel");
+define("_MYNGGROUP_NAME","Gruppen Name");
+define("_MYNGDESCRIPTION","Beschreibung");
+define("_MYNGSUBSCRIBE_GROUP","Gruppe abonnieren");
+
+// Register
+define("_MYNGFILL_FORM","Bitte f&uuml;llen Sie dieses Formular aus und best&auml;tigen es zum Registrieren.");
+define("_MYNGUSER_NAME","Benutzer");
+define("_MYNGPASSWORD","Kennwort");
+define("_MYNGEMAIL","Email");
+define("_MYNGPASSWORD_AGAIN","Kennwort wiederholen");
+// Phrase: 'All fields are required'
+define("_MYNGALL_FIELDS","Alle Felder sind");
+define("_MYNGREQUIRED","erforderlich");
+define("_MYNGVISIBLE","Sichtbar");
+define("_MYNGYES","Ja");
+define("_MYNGNO","Nein");
+define("_MYNGCOUNTRY","Land");
+define("_MYNGINSERT_VALID_EMAIL","Bitte tragen Sie eine g&uuml;ltige eMail Adresse ein. Diese wird gepr&uuml;ft.");
+define("_MYNGPASSWORD_TRANSFER","Ihre Email sollte f&uuml;r alle sichtbar sein, aber Sie wird gegen SPAM bots gesichert, auch wenn Sie Artikel schreiben.");
+define("_MYNGSPAM_PROTECTED","Ihr Kennwort wird nicht &uuml;ber das Internet &uuml;bertragen.");
+define("_MYNGSECURE_LOGIN","Unser Anmeldesystem ist sicher, es ist nur ein Hash-Wert f&uuml;r andere sichtbar.");
+define("_MYNGREGISTRATION_COMPLETE","Registrierung vollst&auml;ndig");
+define("_MYNGTHANKS","Vielen Dank!");
+define("_MYNGDATA_STORED","Ihre Daten wurden korrekt in unserer Datenbank gespeichert.");
+define("_MYNGEDIT_INFO","Sie k&ouml;nnen die gesandten Informationen auf Ihrer Startseite &auml;ndern.");
+define("_MYNGSUBMITTED_INFO","Diese Informationen haben Sie an uns &uuml;bertragen:");
+define("_MYNGREGISTER_WELCOME","Willkommen in der neuen Community!");
+define("_MYNGSTAFF","Die MyNewsGroups :) Personen.");
+define("_MYNGLOGIN_TRANSFER","Wenn Sie sich anmelden, wird das Kennwort nicht &uuml;ber das Internet &uuml;bertragen.");
+define("_MYNGCOOKIES","Cookies m&uuml;ssen eingeschaltet sein um sich im System anzumelden.");
+
+// Login
+define("_MYNGHOME","Startseite");
+define("_MYNGWELCOME_USER","Willkommen");
+define("_MYNGPOST","Schreiben");
+define("_MYNGREPLY","Antworten");
+define("_MYNGSAVE","Speichern");
+define("_MYNGSTATS","Statistik");
+define("_MYNGSUBSCRIBE","Abonnieren");
+define("_MYNGOPTIONS","Optionen");
+define("_MYNGPROFILE","Profil");
+define("_MYNGNEW_ARTICLES","neue Artikel");
+define("_MYNGTO_ARTICLES","zum Artikel");
+define("_MYNGEDIT_YOUR","&Auml;ndere deine");
+define("_MYNGTO_GROUPS","neuer Gruppen");
+define("_MYNGYOUR_ARTICLES","deiner bevorzugten Artikel");
+define("_MYNGVIEW_YOUR","zeige deine Beitr&auml;ge");
+
+// My Bar
+define("_MYNGMY_BOX","Meine Box");
+define("_MYNGMY_HOME","Meine Seite");
+define("_MYNGMY_GROUPS","Meine Gruppen");
+define("_MYNGMY_ARTICLES","Meine Artikel");
+define("_MYNGLOG_OUT","Abmelden");
+define("_MYNGMY_STATS","Meine Statistik");
+
+// Tree
+define("_MYNGNEWSGROUP_UNSELECTED","Keine News Gruppen ausgew&auml;hlt");
+define("_MYNGPOST_ARTICLE","Schreibe einen Artikel");
+define("_MYNGDATE","Datum");
+define("_MYNGSUBJECT","Betreff");
+define("_MYNGAUTHOR","Autor");
+define("_MYNGARTICLES","Artikel");
+define("_MYNGNEXT","vor");
+define("_MYNGPREVIOUS","zur&uuml;ck");
+define("_MYNGMARK_ALL_READ","Alles als gelesen markieren");
+define("_MYNGMARK_THREAD_READ","Thema als gelesen markieren"); 
+
+// Article
+define("_MYNGREPLY","Antwort");
+define("_MYNGSEND_FRIEND","einem Freund senden");
+define("_MYNGPRINT","Drucken");
+define("_MYNGSAVE_ARTICLE","Artikel speichern");
+
+// Search Engine
+define("_MYNGSEARCH_RESULTS","Such Ergebnisse");
+define("_MYNGNOARTICLES_FOUND","Es wurden leider keine Artikel gefunden mit dem Inhalt");
+define("_MYNGSEARCH_ENGINE","Suchmaschine");
+define("_MYNGSEARCH_ONE_GROUP","Suche in einer Gruppe");
+define("_MYNGSEARCH_ALL_GROUPS","Suche in allen Gruppen");
+define("_MYNGAND_OPERATION","Wenn Sie mehr als ein Wort eingeben wird eine logische UND Verkn&uuml;pfung vorgenommen.");
+define("_MYNGPOWERED_BY","Powered by ");
+define("_MYNGGO","Los!");
+
+// Search Results
+define("_MYNGGROUP","Gruppe");
+define("_MYNGREADINGS","Gelesen");
+
+// Stats
+define("_MYNGHOT_NG","Aktivste Newsgruppen");
+define("_MYNGPOPULAR_NG","Beliebteste Newsgruppe");
+define("_MYNGCROWDED_NG","Meiste Anwender in NewsGruppen");
+define("_MYNGHOT_ARTICLES","Aktivste Artikel");
+define("_MYNGPOPULAR_ARTICLES","Beliebteste Artikel");
+define("_MYNGACTIVE_USER","am meisten aktive Benutzer");
+define("_MYNGARG_USER","Benutzer mit mehr Antworten");
+define("_MYNGSTATS_ARTICLES","Artikel");
+define("_MYNGSTATS_READINGS","gelesen");
+define("_MYNGUSERS","Benutzer");
+define("_MYNGPOSTINGS","Eintr&auml;ge");
+define("_MYNGREPLIES","Antworten");
+
+// My Groups
+define("_MYNGMUST_LOGIN","Sie m&uuml;ssen angemeldet sein um diese Funktion zu nutzen.");
+define("_MYNGSUBSCRIPTION_SYSTEM","Abonnement-System");
+define("_MYNGCHOOSE_GROUP","W&auml;hlen Sie eine Gruppe");
+define("_MYNGSURE","Ich bin sicher");
+define("_MYNGGROUPS_SUBSCRIBED","Dies sind die Gruppen die Sie abonniert haben");
+define("_MYNGNEW","Neu");
+define("_MYNGUNREAD","Ungelesen");
+define("_MYNGPOSTED","Geschrieben");
+define("_MYNGADD","Zuf&uuml;gen");
+define("_MYNGDEL","L&ouml;schen");
+
+// My Articles
+define("_MYNGARTICLES_SAVED","Dies sind die Artikel die Sie gespeichert haben");
+
+// Print article
+define("_MYNGPRINT_ARTICLE","Diesen Artikel drucken");
+define("_MYNGUSER_AGENT","User Agent");
+define("_MYNGGENERATED_BY","Erzeugt von");
+define("_MYNGFROM","Von");
+
+// Email article
+
+define("_MYNGEMAIL_SENT","Email wurde erfolgreich verschickt");
+define("_MYNGEMAIL_NOT_SENT","Es gab eine Fehler: die Email wurde NICHT erfolgreich verschickt");
+define("_MYNGEMAIL_FRIEND","Sende diesen Artikel per eMail an einen Freund");
+define("_MYNGSENDYOU_FROM"," sendet Ihnen diesen Artikel von ");
+define("_MYNGDEAR_FRIEND","Liebe(r) (_friend_name)");
+// the string '_friend_name' it must be the same in '_MYNGDEAR_FRIEND' and in '_MYNGFRIEND_NAME'.
+define("_MYNGFRIEND_NAME","\(_friend_name\)");
+define("_MYNGYOUR_FRIEND","Dein Freund ");
+define("_MYNGWANT2SHARE",", will Dir den nachfolgenden Artikel zum lesen vorschlagen \nvom Forum ");
+define("_MYNGNSERT_FRIEND_INFO","Tragen Sie die Informationen zu ihrem Freund ein");
+define("_MYNGMAIL_MUSTBE_REAL","Hinweis: Die eMail Adresse mu&szlig; existieren, diese wird zuvor gepr&uuml;ft, <br>&nbsp; aber nicht &uuml;berwacht oder gespeichert.");
+define("_MYNGNAME","Name");
+define("_MYNGEMAIL_LOOK","Die eMail Adresse w&auml;re dann");
+define("_MYNGEMAIL_IT","Versenden!");
+
+// Post
+define("_MYNGMISSING_EMAIL","Es mu&szlig; eine eMail Adresse angegeben werden.");
+define("_MYNGMISSING_MESSAGE","Es mu&szlig; eine Nachricht eingegben werden.");
+define("_MYNGWRONG_EMAIL","Die eMail Adresse ist falsch");
+define("_MYNGMISSING_NAME","Es mu&szlig; ein Name oder wenigstens ein Alias angegeben werden.");
+define("_MYNG_MISSING_SUBJECT","Es mu&szlig; ein Betreff eingegeben werden.");
+define("_MYNGMESSAGE_POSTED","Die Nachricht wurde erfolgreich versandt.");
+define("_MYNGERROR_SERVER","Der News Server hat die Nachricht nicht angenommen.");
+define("_MYNGREAD_ONLY","Die Gruppe ist schreibgesch&uuml;tzt");
+define("_MYNGWROTE"," geschrieben:\n\n");
+define("_MYNGFOLLOWUP_NOT_ALLOWED","Sie sind nicht berechtigt in dieser Gruppe Beitr&auml;ge zu schreiben: ");
+define("_MYNGPOST_FAILED","Verbindung fehlgeschlagen. Versuchen Sie sp&auml;ter neu zu laden.");
+define("_MYNGFOOTER1","         Artikel geschrieben mit MyNewsGroups :)");
+define("_MYNGFOOTER2","         http://mynewsgroups.sourceforge.net");
+define("_MYNGFOOTER3","         Share your knowledge in... ");
+define("_MYNGFILL_FORM2POST","Nutzen Sie dieses Formulat um neue Beitr&auml;ge zu schreiben");
+define("_MYNGEMAIL_MUSTBE_REAL","Hinweis: Die eMail Adresse mu&szlig; existieren. Die eMail Adresse wird gepr&uuml;ft");
+define("_MYNGPOST","Senden");
+define("_MYNGPOSTING_COMPLETE","Versand abgeschlossen");
+define("_MYNGTHANKX","Danke!");
+define("_MYNGPOSTED_SUCCESFULLY","Ihr Beitrag wurde erfolgreich versandt");
+define("_MYNGINFO_SUBMITTED","Dies sind die Informationen die Sie an den Server &uuml;bertragen haben");
+define("_MYNGBODY","Body");
+define("_MYNGCLICK2RETURN","Hier klicken um zur&uuml;ckzukehren");
+define("_MYNGEMAIL_SPAM_PROTECTED","Ihre eMail wurde vor dem Versand gesch&uuml;tzt um zu vermeiden von SPAM Bots gefunden zu werden");
+define("_MYNGEMAIL_CHANGED","Ihre eMail Adresse sieht wie folgt aus");
+
+// Error template
+define("_MYNGSOMETH_WRONG","Irgendwas ging gewaltig schief ...");
+define("_MYNGERROR_MESSAGE","Fehler");
+define("_MYNGHAVE_DOUBT","Wenn Sie Zweifel haben, wenden Sie sich an ihren System Administrator");
+define("_MYNGERROR","Fehler!");
+
+// Admin
+define("_MYNGADMIN_LOGIN","Sie m&uuml;ssen sich als System Administrator anmelden.");
+define("_MYNGADMIN","Administration");
+define("_MYNGENTER_GROUP_NAME","Gruppen Namen eingeben");
+define("_MYNGADD_GROUP","Gruppe zuf&uuml;gen");
+define("_MYNGCHOOSE_SERVER","Server ausw&auml;hlen");
+define("_MYNGARTICLES2FETCH","Anzahl Artikel holen");
+define("_MYNGDELETE_GROUP","Gruppe l&ouml;schen");
+define("_MYNGDELETE","L&ouml;schen");
+define("_MYNGEDIT_GROUP","Gruppe &auml;ndern");
+define("_MYNGALLOW_POSTING","Beitr&auml;ge zulassen");
+define("_MYNGEDIT","&Auml;ndern");
+define("_MYNGADD_SERVER","News Server zuf&uuml;gen");
+define("_MYNGHOST","Host");
+define("_MYNGPORT","Port");
+define("_MYNGDELETE_SERVER","News Server l&ouml;schen");
+define("_MYNGALL","Alle");
+
+
+// Errors
+define("_MYNGNOGROUPS","Es sind derzeit keine Gruppen registriert.");
+define("_MYNGCON_ERROR","Verbindungsfehlet!");
+define("_MYNGUSERNAME_TAKEN","Schade, der Benutzernamen wird schon von jemand anderem genutzt.");
+define("_MYNGEMAIL_TAKEN","Schade, deine eMail Adresse wird schon von jemand anderem genutzt.");
+define("_MYNGLOGIN_INCORRECT","Schade, die Anmeldung war falsch. Bitte nochmal versuchen.");
+define("_MYNGINVALID_EMAIL","Schade, die eMail Adresse ist nicht g&uuml;ltig. Geben Sie eine richtige eMail Adresse ein.");
+define("_MYNGINVALID_PASSWORD","Schade, die Kennw&ouml;rter sind nicht identisch.");
+define("_MYNGBLANK_PASSWORD","Bitte geben Sie ein Kennwort ein.");
+define("_MYNGSERVER_REJECT","Ihr Mail Server hat die Adresse zur&uuml;ckgewiesen");
+define("_MYNGNO_RESPONSE","Keine Antwort von ihrem Mail Server");
+define("_MYNGCANT_CONNECT_SERVER","Keine Verbindung zum Mail Server");
+define("_MYNGAPPEARS_VALID"," scheint g&uuml;ltig zu sein");
+define("_MYNGNOT_PROPERLY_FORMATTED"," ist nicht richtig formatiert");
+define("_MYNGNO_GROUP","Gruppe ist nicht verf&uuml;gbar auf ");
+define("_MYNGUNKNOWN_GROUP"," Diese Gruppe ist nicht im System hinterlegt.");
+
+// Greets
+define("_MYNGREGISTRATION_COMPLETED","Gl&uuml;ckwunsch! Ihre Registrierung wurde erfolgreich vorgenommen.");
+define("_MYNGLOGGED","Sie sind jetzt im System angemeldet!");
+define("_MYNGLOGGED_OUT","Das Abmelden war erfolgreich.");
+
+
+?>
