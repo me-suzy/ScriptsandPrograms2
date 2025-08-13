2c0902f5d53f9e7f9615d142ba68ce4c8eabf756
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:58 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/english/lang.cp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/english/lang.cp.php
@@ -0,0 +1,176 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Control Panel
+//----------------------------
+
+"consult_user_guide" =>
+"Consult User Guide",
+
+"powered_by" =>
+"Powered By",
+
+"control_panel" => 
+"Control Panel",
+
+"main_menu" => 
+"Control Panel Home",
+
+"username" => 
+"Username",
+
+"password" => 
+"Password",
+
+"publish" => 
+"Publish",
+
+"edit" => 
+"Edit",
+
+"design" => 
+"Templates",
+
+"communicate" => 
+"Communicate",
+
+"modules" => 
+"Modules",
+
+"admin" => 
+"Admin",
+
+"my_account" => 
+"My Account",
+
+"user_account" => 
+"Member Account",
+
+"user_guide" => 
+"User Guide",
+
+"logout" => 
+"Log-out",
+
+"copyright" => 
+"Copyright",
+
+"rights_reserved" => 
+"All Rights Reserved",
+
+"page_rendered" => 
+"Script executed in %x seconds",
+
+"queries_executed" => 
+"%x SQL queries used",
+
+"build" =>
+"Build:",
+
+"forgot_password" =>
+"Forgot your password?",
+
+"e_no_css" => 
+"The requested CSS file could not be opened.",
+
+"unauthorized" => 
+"Unauthorized",
+
+"unauthorized_access" => 
+"You are not authorized to access this page",
+
+"be_careful" =>
+"BE CAREFUL!",
+
+"action_can_not_be_undone" =>
+"THIS ACTION CAN NOT BE UNDONE",
+
+"back" =>
+"Back",
+
+"no" =>
+"No",
+
+"yes" =>
+"Yes",
+
+"all" =>
+"All",
+
+"any" =>
+"Any",
+
+"required_fields" =>
+"Indicates required fields",
+
+"please_consult_manual" => 
+"Please consult the user guide.",
+
+"login" => 
+"Login",
+
+"forgotten_password" => 
+"Forgotten password",
+
+"submit_email_address" => 
+"Submit your email address",
+
+
+//----------------------------
+// Buttons
+//----------------------------
+
+"submit" => 
+"Submit",
+
+"preview" => 
+"Preview",
+
+"update" => 
+"Update",
+
+"delete" => 
+"Delete",
+
+"save_changes" => 
+"Save changes",
+
+
+
+//----------------------------
+// Log
+//----------------------------
+
+"clear_logs" =>
+"Clear Log Files",
+
+"member_id" =>
+"Member ID",
+
+"ip_address" =>
+"IP Address",
+
+"date" =>
+"Date",
+
+"action" =>
+"Action",
+
+
+"member_logged_in" =>
+"Logged in",
+
+"member_logged_out" =>
+"Logged out",
+
+"cleared_logs" =>
+"Cleared Log Files",
+
+
+
+// END
+''=>''
+);
+?>
