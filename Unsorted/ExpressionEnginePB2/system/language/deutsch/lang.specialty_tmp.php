3b180f57efca30bea388495b829c631e6bd0bb3b
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:35 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.specialty_tmp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.specialty_tmp.php
@@ -0,0 +1,85 @@
+<?php
+
+$L = array(
+
+
+"email_title" =>
+"Email-Titel",
+
+"email_message" =>
+"Email-Nachricht",
+
+"admin_notify_comment" =>
+"Kommentar-Benachrichtigung f&uuml;r Admins",
+
+"admin_notify_comment_desc" =>
+"Diese Vorlage beschreibt die Nachricht, die ein Administrator beim Speichern eines Kommentars erh&auml;lt.",
+
+"admin_notify_trackback" =>
+"Trackback-Benachrichtigung f&uuml;r Admins",
+
+"admin_notify_trackback_desc" =>
+"Diese Vorlage beschreibt die Nachricht, die ein Administrator beim Empfang eines Trackbacks erh&auml;lt.",
+
+"admin_notify_reg" =>
+"Admin-Benachrichtigung bei Neuanmeldung eines Nutzers",
+
+"admin_notify_reg_desc" =>
+"Diese Vorlage beschreibt die Nachricht, die ein Administrator bei der Anmeldung eines neuen Mitglieds erh&auml;lt.",
+
+"comment_notification" =>
+"Kommentarbenachrichtigung",
+
+"comment_notification_desc" =>
+"Diese Vorlage beschreibt die Nachricht, die ein Nutzer erh&auml;lt, wenn jemand auf seinen Kommentar geantwortet hat.",
+
+"forgot_password_instructions" =>
+"Anweisungen f&uuml;r verlorenes Passwort",
+
+"forgot_password_instructions_desc" =>
+"Diese Vorlage beschreibt die Anweisungen, die ein Nutzer bekommt, wenn er sein Passwort vergessen hat.",
+
+"mailinglist_activation_instructions" =>
+"Anweisungen f&uuml;r die Aktivierung der Mailing-Liste",
+
+"mailinglist_activation_instructions_desc" =>
+"Diese Vorlage beschreibt die Anweisungen zur Aktivierung der Mailing-Liste.",
+
+"mbr_activation_instructions" =>
+"Anweisungen f&uuml;r die Aktivierung der Mitgliedschaft",
+
+"mbr_activation_instructions_desc" =>
+"Diese Vorlage beschreibt die Anweisungen zur Aktivierung einer Neumitgliedschaft.",
+
+"reset_password_notification" =>
+"Benachrichtigung f&uuml;r Passwort-&Auml;nderung",
+
+"reset_password_notification_desc" =>
+"Diese Vorlage beschreibt die Anweisungen zur &Auml;nderung des Passwortes.",
+
+"validated_member_notify" =>
+"Benachrichtigung &uuml;ber die Pr&uuml;fung eines Mitgliedsaccounts",
+
+"validated_member_notify_desc" =>
+"Diese Vorlage beschreibt die Nachricht, die ein Mitglied erh&auml;lt, wenn ein Administrator seinen Account freigeschaltet hat.",
+
+"use_this_template" =>
+"Vorlage aktivieren?",
+
+"use_this_template_exp" =>
+"Wenn Sie <b>nein</b> klicken, wird diese Vorlage nicht benutzt. Stattdessen bekommt jeder Nutzer eine Nachricht in seiner bevorzugten Sprache.",
+
+"user_messages_template_desc" =>
+"Der Sinn dieser Vorlage ist, Fehler- und andere Nachrichten anzuzeigen, wenn Nutzer Daten (wie Registrierung etc.) abgeschickt haben.",
+
+"user_messages_template_warning" =>
+"Keine der f&uuml;nf erforderlichen Variablen l&ouml;schen:",
+
+"available_variables" =>
+"Die folgenden Variablen k&ouml;nnen genutzt werden:",
+
+
+// END
+''=>''
+);
+?>
