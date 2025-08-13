cd32769695ac3d6eec36f8b40702f73e56bd6fab
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:27 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/print.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/print.php
@@ -0,0 +1,23 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[PRINT_Notice] = "Esta página foi gerada para impressão fácil. Pode ir a [ARQUIVO] --> [IMPRESSÃO] imprimir este documento.";
+
+$LOCALE[PRINT_IP] = "O endereço de IP";
+
+$LOCALE[PRINT_Agent] = "O agente de Browser";
+
+$LOCALE[PRINT_Time] = "Timestamp";
+
+$LOCALE[PRINT_Misc] = "Misc info";
+
+?>
