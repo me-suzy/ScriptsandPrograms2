d307301afc386beca84e4fa4063c265205987449
me-suzy <me.suzana@gmail.com>
2025-08-13 05:46:09 +0300

    Add Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/finnish/language.php

---
+++ Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/finnish/language.php
@@ -0,0 +1,298 @@
+<?php
+## In-link User Language File - Finnish/Suomi
+## Author: Antti Tuppurainen - SystemProfes Oy - www.profes.fi
+## Author Email: antti.tuppurainen@profes.fi
+## Date: 07/26/2001
+##
+## This file is designed for the following version of In-link: 2.0.6
+## You can modify the text in this file to suit your language needs. 
+## Make sure to escape quotation marks with \" . 
+
+#########################################################################################
+#Language variables
+
+$lu_char_set								=   	"ISO-8859-1";
+$lu_this_language							=	"Suomi";	
+
+#########################################################################################
+#Navigation Menu
+
+$lu_getting_rated							=	"Lisää arvostelu sivuillesi";
+$lu_go_to_page								=	"Mene sivulle";
+$lu_search_results							=	"Hakutulokset";
+$lu_nav_addlink								=	"Lisää linkki";
+$lu_nav_editorspick							=	"Poimintoja";
+$lu_nav_gettingrated							=	"Lisää kommentti";
+$lu_nav_home								=	"Etusivu";
+$lu_nav_login								=	"Kirjaudu";
+$lu_nav_logout								=	"Poistu";
+$lu_nav_maillist							=	"Liity";
+$lu_nav_modlink								=	"Muokka linkki";
+$lu_nav_new								=	"Uudet linkit";
+$lu_nav_pop								=	"Suositut";
+$lu_nav_profile								=	"Profiili";
+$lu_nav_register							=	"Rekisteröidy";
+$lu_nav_search								=	"Etsi";
+$lu_nav_search_advanced							=	"Laajennettu haku";
+$lu_nav_suggestcat							=	"Ehdota kategoriaa";
+$lu_nav_top								=	"Parhaat äänet";
+$lu_navbar_seperator							=	" >";
+
+
+#########################################################################################
+#Page Titles
+
+$lu_title_add_link							=	"Lisää linkki";
+$lu_title_add_review							=	"Lisää kommentti";
+$lu_title_confirm							=	"Vahvistus";
+$lu_title_edit_link							=	"Muokkaa linkki";
+$lu_title_editors_picks							=	"Toimittajan valinnat ja suosittelemat";
+$lu_title_error								=	"Virhe";
+$lu_title_gettingrated							=	"Lisää arviointi -lomake sivuillesi";
+$lu_title_login								=	"Kirjaudu";
+$lu_title_modify							=	"Muokkaa omia linkkejäsi";
+$lu_title_most_popular							=	"Suosituimmat linkit";
+$lu_title_msg								=	"Järjestelmä viesti";
+$lu_title_new_links							=	"Uudet linkit";
+$lu_title_profile							=	"Jäsen profiili";
+$lu_title_rate								=	"Arvioi linkki";
+$lu_title_registration							=	"Rekisteröityminen";
+$lu_title_reviews							=	"Arvostelut";
+$lu_title_search							=	"Etsi";
+$lu_title_search_advanced						=	"Laajennettu haku";
+$lu_title_subscribe							=	"Liity postituslistalle";
+$lu_title_suggest							=	"Ehdota kategoria";
+$lu_title_top_rated							=	"Huippu arvostellut linkit";
+
+
+#########################################################################################
+#Link and Category Markers (Can replace them with graphics)
+
+$lu_cat_new								=	"Uusi";
+$lu_cat_pick								=	"Valinta";
+$lu_link_new								=	"Uusi";
+$lu_link_pick								=	"Valinta";
+$lu_link_pop								=	"Napsahtava";
+$lu_link_top								=	"Huippu";
+
+
+#########################################################################################
+#System Messages
+
+$lu_no_categories							=	"Ei kategorioita";
+$lu_no_links								=	"Ei linkkejä";
+
+
+#########################################################################################
+#Link/Category Details 
+
+$lu_added								=	"Lisätty";
+$lu_cat									=	"Kategoria";
+$lu_date_created							=	"Luotu";
+$lu_desc								=	"Kuvaus";
+$lu_edit								=	"Muokkaa";
+$lu_editor_pick								=	"Toimittajan valinta";
+$lu_from								=	"Mistä";
+$lu_general								=	"Yleinen";
+$lu_image								=	"Kuva";
+$lu_hits								=	"Osumia";
+$lu_link								=	"Linkki";
+$lu_name								=	"Nimi";
+$lu_no									=	"Ei";
+$lu_rateit								=	"Arvostele";
+$lu_rating								=	"Arvostelut";
+$lu_reviews								=	"Kommentit";
+$lu_to									=	"Mihin";
+$lu_votes								=	"Ääniä";
+$lu_url									=	"URL";
+$lu_yes									=	"Kyllä";
+
+
+#########################################################################################
+#Search 
+
+$lu_boolean_type_search							=	"Boolean";
+$lu_return_number_results						=	"Tuloksia sivulle";
+
+
+#########################################################################################
+#Errors
+
+$lu_already_voted			=	"Kantamme kertoo, että olet jo kerran äänestänyt. Saat äänestää vain kerran kuussa!";
+$lu_already_voted			=	"Sinulle ei ole oikeuksia äänestää tätä resurssia!";
+$lu_email_used				=	"Sähköposti osoite on varattu";
+$lu_error_404				=	"En saa auki tiedostoa.";
+$lu_error_addlink_not_allowed		=	"Linkkien lisääminen ei ole sallittua";
+$lu_error_addreview_not_allowed		=	"Arvostelujen lisääminen ei ole sallittua";
+$lu_error_addto_cat_not_allowed		=	"Tämä kategoria ei ole spesifioitu tarpeeksi hyvin. Ole hyvä ja valitse toinen kategoria.";
+$lu_error_cannot_remove_cat		=	"En kykene poistamaan viimeistä kategoriaa!";
+$lu_error_db				=	"Tietokanta virhe!";
+$lu_error_file_not_found		=	"Tiedosto on saatavilla:";
+$lu_error_pass_match			=	"Salasanat eivät täsmänneet.";
+$lu_error_pass_wrong			=	"Antamasi salasana ei ollut oikea.";
+$lu_error_review_not_filled		=	"Arvostelu -kenttä ei voi olla tyhjä";
+$lu_error_user_not_allowed		=	"Rekisteröityminen ei ole sallittua!";	
+$lu_error_wrong_user			=	"Linkki on toisen omaisuutta!";
+$lu_invalid_entry			=	"Väärä merkintä";
+$lu_login_incorrect			=	"Kirjautuminen epäonnistui";
+$lu_pass_not_match			=	"Salasanat eivät täsmää";
+$lu_username_used			=	"Käyttäjätunnus on varattu";
+
+#########################################################################################
+#Confirmations
+
+$lu_confirm_registration		=	"Rekisteröinti tietosi on lähetetty eteenpäin.";
+$lu_confirm_suggest_site		=	"Kiitos, että suosittelit sivustoa kaverillesi!";
+$lu_confirm_profile			=	"Profiilisi tiedot on päivitetty.";
+$lu_confirm_linkupdate			=	"Linkkisi tiedot on päivitetty.";
+$lu_confirm_addlink			=	"Linkkisi tiedot on välitetty eteenpäin.";
+$lu_confirm_suggest			=	"Kiitos uuden kategorian ehdottamisesta.";
+$lu_confirm_addreview			=	"Arvostelusi on välitetty eteenpäin.";
+$lu_confirm_subscribe			=	"Sinut on onnistuneesti lisätty postituslistallemme.";
+$lu_confirm_unsubscribe			=	"Sinut on onnistuneesti poistettu postituslistaltamme.";
+
+#########################################################################################
+#Buttons
+
+$lu_button_addcat			=	"Lisää kategoria";
+$lu_button_addlink			=	"Lisää linkki";
+$lu_button_addreview			=	"Lisää kommentti";
+$lu_button_back				=	"Takaisin";
+$lu_button_cancel			=	"Peruuta";
+$lu_button_editlink			=	"Muokkaa linkkiä";
+$lu_button_go				=	" Go ";
+$lu_button_login			=	"Kirjaudu";
+$lu_button_ok				=	"Ok";
+$lu_button_rate				=	"Arvostele";
+$lu_button_register			=	"Rekisteröidy";
+$lu_button_reset			=	"Tyhjennä";
+$lu_button_search			=	"Etsi";
+$lu_button_search_cats			=	"Etsi kategorioita";
+$lu_button_search_links			=	"Etsi linkeistä";
+$lu_button_send				=	"Lähetä";
+$lu_button_subscribe			=	"Liity";
+$lu_button_unsubscribe			=	"Eroa";
+$lu_button_update			=	"Päivitä";
+$lu_mailing_list			=	"Postituslistalla";
+$lu_not_mailing_list			=	"Ei postituslistalla";
+
+
+#########################################################################################
+#Statistics
+
+$lu_cats				=	"Kategoriat:";
+$lu_links				=	"Linkit:";
+$lu_total_cats				=	"Kategorioita kannassa:  ";
+$lu_total_hits				=	"Osumia: ";
+$lu_total_links				=	"Linkkejä kannassa: ";
+
+
+#########################################################################################
+#Addlink template
+
+$lu_add_cat				=	"Lisää kategoria";
+$lu_additional_cats			=	"Vaihtoehtoiset kategoriat:";
+$lu_remove				=	"Poista";
+$lu_select				=	"Valitse";
+$lu_submitting_to			=	"Olet lisäämässä kategoriaan:";
+
+
+#########################################################################################
+#Register template
+
+$lu_first_name				=	"Etunimi";
+$lu_last_name				=	"Sukunimi";
+$lu_re_password				=	"Salasanan varmistus";
+$lu_user_name				=	"Käyttäjätunnus";
+
+
+#########################################################################################
+#Miscellaneos Pages
+
+$lu_advanced_search			=	"Laajennettu haku";
+$lu_atleast_3				=	"(minimi 3)";
+$lu_email				=	"Sähköpostiosoite";
+$lu_hint_suggest			=	"suosittele kaverille sivustoa";
+$lu_keyword				=	"hakusana";
+$lu_login				=	"Kirjaudu";
+$lu_not_logged_in			=	"Et ole kirjautunut";
+$lu_password				=	"Salasana";
+$lu_register				=	"Rekisteröidy";
+$lu_review				=	"kommenttisi";
+$lu_search				=	"Etsi";
+$lu_search_for				=	"Etsi sanaa";
+$lu_show				=	"Näytä";
+$lu_sort_by				=	"Lajittele";
+$lu_status				=	"Status";
+$lu_suggest				=	"Suosittele kaverille";
+$lu_user				=	"Käyttäjä";
+$lu_user_menu				=	"Käyttäjän valikko";
+$lu_welcome				=	"Tervetuloa";
+
+
+#########################################################################################
+#Dropdowns
+
+$lu_drop_and				=	"Ja";
+$lu_drop_as_many_as_found		=	"Kaikki";
+$lu_drop_ascending			=	"Nousevasti";
+$lu_drop_date				=	"Päivä";
+$lu_drop_desc				=	"Kuvaus";
+$lu_drop_descending			=	"Laskevasti";
+$lu_drop_editors_pick			=	"Toimittajan valinta";
+$lu_drop_hits				=	"Osumia";
+$lu_drop_image				=	"Kuva";
+$lu_drop_name				=	"Nimi";
+$lu_drop_numlinks			=	"Linkkien määrä";
+$lu_drop_numsubs			=	"Alaosastojen määrä";
+$lu_drop_or				=	"Tai";
+$lu_drop_perm				=	"Oikeudet";
+$lu_drop_rating				=	"Arvostelu";
+$lu_drop_url				=	"URL";
+$lu_drop_user				=	"Käyttäjä";
+$lu_drop_vis				=	"Näkyminen";
+$lu_drop_votes				=	"Äänet";
+
+
+#########################################################################################
+#Custom Fields
+
+$lu_categories_custom_fields1		=	"Muokattava kentän nimi #1";
+$lu_categories_custom_fields2		=	"Muokattava kentän nimi #2";
+$lu_categories_custom_fields3		=	"Muokattava kentän nimi #3";
+$lu_categories_custom_fields4		=	"Muokattava kentän nimi #4";
+$lu_categories_custom_fields5		=	"Muokattava kentän nimi #5";
+$lu_categories_custom_fields6		=	"Muokattava kentän nimi #6";
+
+$lu_links_custom_fields1		=	"Muokattava kentän nimi #1";
+$lu_links_custom_fields2		=	"Muokattava kentän nimi #2";
+$lu_links_custom_fields3		=	"Muokattava kentän nimi #3";
+$lu_links_custom_fields4		=	"Muokattava kentän nimi #4";
+$lu_links_custom_fields5		=	"Muokattava kentän nimi #5";
+$lu_links_custom_fields6		=	"Muokattava kentän nimi 6";
+
+$lu_users_custom_fields1		=	"Muokattava kentän nimi #1";
+$lu_users_custom_fields2		=	"Muokattava kentän nimi #2";
+$lu_users_custom_fields3		=	"Muokattava kentän nimi #3";
+$lu_users_custom_fields4		=	"Muokattava kentän nimi #4";
+$lu_users_custom_fields5		=	"Muokattava kentän nimi #5";
+$lu_users_custom_fields6		=	"Muokattava kentän nimi #6";
+
+
+#########################################################################################
+#Ratings
+
+$lu_excellent				=	"Loistava!";
+$lu_verygood				=	"todella hyvä";
+$lu_good				=	"Hyvä";
+$lu_fair				=	"Menettelee";
+$lu_poor				=	"Heikko";
+
+
+
+#########################################################################################
+#Text segments
+
+$lu_txt_gettingrated	=	"Voit liittää alla olevan HTML -koodin kotisivuillesi, jotta sivuilla vierailijat voisivat arvostella ja äänestää kyseistä resurssia:";
+
+?>
