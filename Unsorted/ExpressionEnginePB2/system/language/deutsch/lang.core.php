a516c181c45a5476d53be804a7dc1be00db462b4
me-suzy <me.suzana@gmail.com>
2025-08-13 04:18:59 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.core.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.core.php
@@ -0,0 +1,373 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// General word list
+//----------------------------
+
+"on" =>
+"ein",
+
+"off" =>
+"aus",
+
+"first" =>
+"erste",
+
+"last" =>
+"letzte",
+
+"enabled" =>
+"aktiv",
+
+"disabled" =>
+"inaktiv",
+
+"back" =>
+"Zur&uuml;ck",
+
+"submit" =>
+"Abschicken",
+
+"update" =>
+"Aktualisieren",
+
+"thank_you" =>
+"Danke!",
+
+"system_off_msg" =>
+"Diese Seite ist zur Zeit nicht aktiv.",
+
+"not_authorized" =>
+"Sie haben nicht die erforderliche Berechtigung.",
+
+"auto_redirection" =>
+"Sie werden in %x Sekunden automatisch weitergeleitet.",
+
+//----------------------------
+// Errors
+//----------------------------
+
+"error" =>
+"Fehler",
+
+"invalid_url" =>
+"Der angegebene URL ist ung&uuml;ltig.",
+
+"submission_error" =>
+"Das Formular, das Sie abgeschickt haben, enthielt folgende Fehler:",
+
+"general_error" =>
+"Folgende Fehler traten auf:",
+
+"invalid_action" =>
+"Die angeforderte Aktion ist nicht zugelassen.",
+
+//----------------------------
+// Member Groups
+//----------------------------
+
+"banned" =>
+"Gesperrt",
+
+"guests" =>
+"G&auml;ste",
+
+"members" =>
+"Mitglieder",
+
+"pending" =>
+"Im Wartestand",
+
+"super_admins" =>
+"Super-Admins",
+
+
+//----------------------------
+// Template.php
+//----------------------------
+
+"error_tag_syntax" =>
+"Das folgende Tag hat einen Syntaxfehler:",
+
+"error_fix_syntax" =>
+"Bitte, korrigieren Sie die Syntax in Ihrer Vorlage.",
+
+//----------------------------
+// Email
+//----------------------------
+
+"forgotten_email_sent" =>
+"Anweisungen zum &Auml;ndern Ihres Passwortes wurden Ihnen zugeschickt.",
+
+"error_sending_email" =>
+"Kann zur Zeit keine Email versenden.",
+
+"no_email_found" =>
+"Die angegebene Email-Adresse ist nicht in der Datenbank zu finden.",
+
+"your_new_login_info" =>
+"Login-Information",
+
+"reset_link" =>
+"Zum &Auml;ndern Ihres Passwortes gehen Sie bitte zu der folgenden Seite:",
+
+"password_will_be_reset" =>
+"Ihr Passwort wird automatisch ge&auml;ndert, und Sie erhalten ein neues Passwort per E-Mail.",
+
+"ignore_password_message" =>
+"Wenn Sie Ihr Passwort nicht &auml;ndern wollen, ignorieren Sie diese Nachricht. Sie wird in 24 Stunden ung&uuml;ltig.",
+
+"password_has_been_reset" =>
+"Ihr Passwort wurde ge&auml;ndern, und Sie erhalten ein neues Passwort per Email.",
+
+"new_login_info" =>
+"Hier ist Ihre neue Login-Information:",
+
+
+//----------------------------
+// Date
+//----------------------------
+
+"year" =>
+"Jahr",
+
+"years" =>
+"Jahre",
+
+"month" =>
+"Monat",
+
+"months" =>
+"Monate",
+
+"week" =>
+"Woche",
+
+"weeks" =>
+"Wochen",
+
+"day" =>
+"Tag",
+
+"days" =>
+"Tage",
+
+"hour" =>
+"Stunde",
+
+"hours" =>
+"Stunden",
+
+"minute" =>
+"Minute",
+
+"minutes" =>
+"Minuten",
+
+"second" =>
+"Sekunde",
+
+"seconds" =>
+"Sekunden",
+
+"am" =>
+"am",
+
+"pm" =>
+"pm",
+
+"AM" =>
+"AM",
+
+"PM" =>
+"PM",
+
+"Sun" =>
+"Son",
+
+"Mon" =>
+"Mon",
+
+"Tue" =>
+"Die",
+
+"Wed" =>
+"Mit",
+
+"Thu" =>
+"Don",
+
+"Fri" =>
+"Fre",
+
+"Sam" =>
+"Sat",
+
+"Sunday" =>
+"Sonntag",
+
+"Monday" =>
+"Montag",
+
+"Tuesday" =>
+"Dienstag",
+
+"Wednesday" =>
+"Mittwoch",
+
+"Thursday" =>
+"Donnerstag",
+
+"Friday" =>
+"Freitag",
+
+"Saturday" =>
+"Samstag",
+
+"Jan" =>
+"Jan",
+
+"Feb" =>
+"Feb",
+
+"Mar" =>
+"Mar",
+
+"Apr" =>
+"Apr",
+
+"May" =>
+"Mai",
+
+"Jun" =>
+"Jun",
+
+"Jul" =>
+"Jul",
+
+"Aug" =>
+"Aug",
+
+"Sep" =>
+"Sep",
+
+"Oct" =>
+"Okt",
+
+"Nov" =>
+"Nov",
+
+"Dec" =>
+"Dez",
+
+"January" =>
+"Januar",
+
+"February" =>
+"Februar",
+
+"March" =>
+"M&auml;rz",
+
+"April" =>
+"April",
+
+"May" =>
+"Mai",
+
+"June" =>
+"Juni",
+
+"July" =>
+"Juli",
+
+"August" =>
+"August",
+
+"September" =>
+"September",
+
+"October" =>
+"Oktober",
+
+"November" =>
+"November",
+
+"December" =>
+"Dezember",
+
+
+
+"UM12" =>
+"(UTC - 12:00) Enitwetok, Kwajalien",
+"UM11" =>
+"(UTC - 11:00) Nome, Midway Island, Samoa",
+"UM10" =>
+"(UTC - 10:00) Hawaii",
+"UM9" =>
+"(UTC - 9:00) Alaska",
+"UM8" =>
+"(UTC - 8:00) Pacific Time",
+"UM7" =>
+"(UTC - 7:00) Mountain Time",
+"UM6" =>
+"(UTC - 6:00) Central Time, Mexico City",
+"UM5" =>
+"(UTC - 5:00) Eastern Time, Bogota, Lima, Quito",
+"UM4" =>
+"(UTC - 4:00) Atlantic Time, Caracas, La Paz",
+"UM25" =>
+"(UTC - 3:30) Newfoundland",
+"UM3" =>
+"(UTC - 3:00) Brazil, Buenos Aires, Georgetown, Falkland Is.",
+"UM2" =>
+"(UTC - 2:00) Mid-Atlantic, Ascention Is., St Helena",
+"UM1" =>
+"(UTC - 1:00) Azores, Cape Verde Islands",
+"UTC" =>
+"(UTC) Casablanca, Dublin, Edinburgh, London, Lisbon, Monrovia",
+"UP1" =>
+"(UTC + 1:00) Berlin, Brussels, Copenhagen, Madrid, Paris, Rome",
+"UP2" =>
+"(UTC + 2:00) Kaliningrad, South Africa, Warsaw",
+"UP3" =>
+"(UTC + 3:00) Baghdad, Riyadh, Moscow, Nairobi",
+"UP25" =>
+"(UTC + 3:30) Tehran",
+"UP4" =>
+"(UTC + 4:00) Adu Dhabi, Baku, Muscat, Tbilisi",
+"UP35" =>
+ "(UTC + 4:30) Kabul",
+"UP5" =>
+"(UTC + 5:00) Islamabad, Karachi, Tashkent",
+"UP45" =>
+"(UTC + 5:30) Bombay, Calcutta, Madras, New Delhi",
+"UP6" =>
+"(UTC + 6:00) Almaty, Colomba, Dhakra",
+"UP7" =>
+"(UTC + 7:00) Bangkok, Hanoi, Jakarta",
+"UP8" =>
+"(UTC + 8:00) Beijing, Hong Kong, Perth, Singapore, Taipei",
+"UP9" =>
+ "(UTC + 9:00) Osaka, Sapporo, Seoul, Tokyo, Yakutsk",
+"UP85" =>
+"(UTC + 9:30) Adelaide, Darwin",
+"UP10" =>
+"(UTC + 10:00) Melbourne, Papua New Guinea, Sydney, Vladivostok",
+"UP11" =>
+"(UTC + 11:00) Magadan, New Caledonia, Solomon Islands",
+"UP12" =>
+"(UTC + 12:00) Auckland, Wellington, Fiji, Marshall Island",
+
+
+
+
+
+
+
+
+
+// IGNORE
+''=>'');?>
