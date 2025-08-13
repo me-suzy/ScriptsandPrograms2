70402773b165fda7096093b87f6e9f789e2e3594
me-suzy <me.suzana@gmail.com>
2025-08-13 06:29:00 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/config/license.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/config/license.php
@@ -0,0 +1,37 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+/* 
+
+   This is your MWChat Serial Number. It is REQUIRED for your licenses to work. If you do not put
+   your serial number in below, you will not be able to login to MWChat. Your serial number is also
+   stored online with your appindex.net membership information. See the README file for details.
+   
+   IMPORTANT NOTE:
+
+   If you have purchased a "Pro" license, you need to edit mwchat/config/chat.php and enable 
+   registered users and optionally file sharing if you wish to use those feature.
+
+*/
+
+$CONFIG[System_Serial] = "";
+
+/* 
+
+   If you IP address is dynamic (changing) then the option below to true. Please be sure your address is dynamic before chaning this option.
+
+*/
+
+$CONFIG[System_Dynamic] = "false";
+
+?>
