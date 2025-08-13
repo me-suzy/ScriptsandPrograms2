c17e40a352cc04068c72dfe903859de624a3b8fb
me-suzy <me.suzana@gmail.com>
2025-08-13 03:40:18 +0300

    Add Unsorted/ebay/ThumbServe/clsSynchronize.h

---
+++ Unsorted/ebay/ThumbServe/clsSynchronize.h
@@ -0,0 +1,71 @@
+/* $Id: clsSynchronize.h,v 1.2 1999/02/21 02:24:53 josh Exp $ */
+//
+// File: clsSynchronize
+//
+// Author: Martin Hess (marty@ebay.com)
+//
+// Description: This a set of class for thread synchronization.
+//				This is part of the Gallery project.
+//
+
+#ifndef clsSynchronize_h
+#define clsSynchronize_h
+
+#include <windows.h>
+
+class clsSynchronize;
+class clsSynchronizeable;
+
+// On windows there are many ways to synchronize processes. The main way
+// is through event objects which work cross process and potentially 
+// accross a network. Critical sections is another way, but they only
+// work within a single process, however they are much faster.
+// Define USE_CRITICALSECTION if you want to use critical sections
+// instead of event objects for synchronization.
+#define USE_CRITICALSECTION
+
+class clsSynchronizeable
+{
+public:
+	clsSynchronizeable();
+	~clsSynchronizeable();
+
+	friend clsSynchronize;
+
+	long Lock(long dwTimeout = INFINITE);
+	void Unlock();
+
+private:
+	long mLockCount;
+	long mOwnCount;
+	DWORD mOwnerThreadId;
+#if defined(USE_CRITICALSECTION)
+	CRITICAL_SECTION mSemaphore;
+#else
+	HANDLE mEvent;
+#endif
+
+	clsSynchronizeable(const clsSynchronizeable& );
+	clsSynchronizeable& operator=(const clsSynchronizeable& );
+		// no copying
+};
+
+class clsSynchronize
+{
+public:
+	explicit clsSynchronize(clsSynchronizeable& cs, unsigned long timeout = INFINITE) :
+	  mclsSynchronizeable(cs)
+		{ mclsSynchronizeable.Lock(timeout); }
+
+	~clsSynchronize() { mclsSynchronizeable.Unlock(); }
+
+
+private:
+	clsSynchronizeable& mclsSynchronizeable;
+
+	clsSynchronize(const clsSynchronize& );
+	clsSynchronize& operator=(const clsSynchronize& );
+		// no copying
+};
+
+#endif // clsSynchronize_h
