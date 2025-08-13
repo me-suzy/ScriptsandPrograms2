ea5455d32a787b0f5cbf28140c9f5046b8f65ffb
me-suzy <me.suzana@gmail.com>
2025-08-13 05:48:13 +0300

    Add Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/russian/language.php

---
+++ Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/russian/language.php
@@ -0,0 +1,295 @@
+<?php
+## In-link User Language File - Russian
+## Author: Andrew Kucheriavy
+## Date: 07/19/2001
+##
+## This file is designed for the following version of In-link: 2.0.6
+## You can modify the text in this file to suit your language needs. 
+## Make sure to escape quotation marks with \" . 
+
+#########################################################################################
+#Language variables
+
+$lu_char_set								=   "windows-1251";
+$lu_this_language							=	"Ðóññêèé";	
+
+#########################################################################################
+#Navigation Menu
+
+$lu_getting_rated							=	"Ïîëó÷èòü Ãîëîñ";
+$lu_go_to_page								=	"Ñòðàíèöû";
+$lu_search_results							=	"Ðåçóëüòàòû Ïîèñêà";
+$lu_nav_addlink								=	"Äîáàâèòü Ññûëêó";
+$lu_nav_editorspick							=	"Ññûëêè Ðåäàêòîðà";
+$lu_nav_gettingrated						=	"Ïîëó÷èòü Ãîëîñ";
+$lu_nav_home								=	"Íà÷àëüíàÿ Ñòðàíèöà";
+$lu_nav_login								=	"Âõîä Äëÿ Ïîëüçîâàòåëÿ";
+$lu_nav_logout								=	"Âûõîä Èç Ñèñòåìû";
+$lu_nav_maillist							=	"Ïîäïèñêà";
+$lu_nav_modlink								=	"Èçìåíèòü Ññûëêó";
+$lu_nav_new									=	"Íîâûå Ññûëêè";
+$lu_nav_pop									=	"Ïîïóëÿðíûå Ññûëêè";
+$lu_nav_profile								=	"Äàííûå Ïîëüçîâàòåëÿ";
+$lu_nav_register							=	"Ðåãèñòðàöèÿ";
+$lu_nav_search								=	"Ïîèñê";
+$lu_nav_search_advanced						=	"Ðàñøèðåííûé Ïîèñê";
+$lu_nav_suggestcat							=	"Ïðåäëîæèòü Êàòåãîðèþ";
+$lu_nav_top									=	"Ëó÷øèå Ññûëêè";
+$lu_navbar_seperator						=	" >";
+
+
+#########################################################################################
+#Page Titles
+
+$lu_title_add_link							=	"Äîáàâèòü Ññûëêó";
+$lu_title_add_review						=	"Äîáàâèòü Êîììåíòàðèé";
+$lu_title_confirm							=	"Ïîòäâåðæäåíèå";
+$lu_title_edit_link							=	"Èçìåíèòü Ññûëêó";
+$lu_title_editors_picks						=	"Ññûëêè Ðåäàêòîðà";
+$lu_title_error								=	"Îøèáêà";
+$lu_title_gettingrated						=	"Ïîëó÷èòü Ãîëîñ";
+$lu_title_login								=	"Âõîä";
+$lu_title_modify							=	"Èçìåíèòü Âàøè Ññûëêè";
+$lu_title_most_popular						=	"Ëó÷øèå Ññûëêè";
+$lu_title_msg								=	"Âíèìàíèå!";
+$lu_title_new_links							=	"Íîâûå Ññûëêè";
+$lu_title_profile							=	"Äàííûå Ïîëüçîâàòåëÿ";
+$lu_title_rate								=	"Ïðîãîëîñîâàòü çà ýòó Ññûëêó";
+$lu_title_registration						=	"Ðåãèñòðàöèÿ";
+$lu_title_reviews							=	"Êîììåíòàðèè";
+$lu_title_search							=	"Ïîèñê";
+$lu_title_search_advanced					=	"Ðàñøèðåííûé Ïîèñê";
+$lu_title_subscribe							=	"Ïîäïèñàòüñÿ Íà Ðàññûëêó";
+$lu_title_suggest							=	"Ïðåäëîæèòü Êàòåãîðèþ";
+$lu_title_top_rated							=	"Ñàìûå Ïîïóëÿðíûå Ññûëêè";
+
+
+#########################################################################################
+#Link and Category Markers (Can replace them with graphics)
+
+$lu_cat_new									=	"new";
+$lu_cat_pick								=	"pick";
+$lu_link_new								=	"new";
+$lu_link_pick								=	"pick";
+$lu_link_pop								=	"pop";
+$lu_link_top								=	"top";
+
+
+#########################################################################################
+#System Messages
+
+$lu_no_categories							=	"Êàòåãîðèé íåò";
+$lu_no_links								=	"Ññûëîê íåò";
+
+
+#########################################################################################
+#Link/Category Details 
+
+$lu_added									=	"Äîáàâëåííî";
+$lu_cat										=	"Êàòåãîðèÿ";
+$lu_date_created							=	"Äàòà äîáàâëåíèÿ";
+$lu_desc									=	"Îïèñàíèå";
+$lu_edit									=	"Èçìåíèòü";
+$lu_editor_pick								=	"Ññûëêà Ðåäàêòîðà";
+$lu_from									=	"Îò";
+$lu_general									=	"Ïðî÷åå";
+$lu_image									=	"Êàðòèíêà";
+$lu_hits									=	"Ïîñåùåíèÿ";
+$lu_link									=	"Ññûëêà";
+$lu_name									=	"Íàçâàíèå";
+$lu_no										=	"Íåò";
+$lu_rateit									=	"Ïðîãîëîñîâàòü";
+$lu_rating									=	"Ðåéòèíã";
+$lu_reviews									=	"Êîììåíòàðèè";
+$lu_to										=	"Äî";
+$lu_votes									=	"Ãîëîñà";
+$lu_url										=	"Àäðåñ";
+$lu_yes										=	"Äà";
+
+
+#########################################################################################
+#Search 
+
+$lu_boolean_type_search						=	"Òèï Ïîèñêà";
+$lu_return_number_results					=	"Êîëëè÷åñòâî Ðåçóëüòàòîâ";
+
+
+#########################################################################################
+#Errors
+
+$lu_already_voted							=	"Íàø ðåãèñòð óêàçûâàåò ÷òî âû óæå ïðîãîëîñîâàëè çà äàííóþ ññûëêó. Ïîâòîðíîå ãîñîâàíèå çà îäèíàêîâûå ññûëêè íå ðàçðåøåííî àäìèíèñòðàòîðîì.";
+$lu_no_voted								=	"Ãîëîñîâàíèå íå ðàçðåøåííî!";
+$lu_email_used								=	"Íàø ðåãèñòð óêàçûâàåò ÷òî äàííûé àäðåñ ýëåêòðîííîé ïî÷òû óæå èìååòñÿ â áàçå äàííûõ.  Ïîæàëóéñòà, óêàæèòå äðóãîé.";
+$lu_error_404								=	"Ôàéë íå íàéäåí";
+$lu_error_addlink_not_allowed				=	"Äîáàëåíèå ññûëîê íå ðàçðåøåíî";
+$lu_error_addreview_not_allowed				=	"Äîáàëåíèå êîììåòàðèåâ íå ðàçðåøåíî";
+$lu_error_addto_cat_not_allowed				=	"Äîáàëåíèå â ýòó êàòåãîðèþ íå ðàçðåøåíî.  Ïîæàëóéñòà, âûáèðåòå áîëåå êîíêðåòíóþ êàòåãîðèþ äëÿ âàøåé ññûëêè.";
+$lu_error_cannot_remove_cat					=	"Íåîáõîäèìî îñòàâèòü êàê ìèíèìóì îäíó êàòåãîðèþ äëÿ äîáàâëåíèÿ!";
+$lu_error_db								=	"Îøèáêà áàçû äàííûõ!";
+$lu_error_file_not_found					=	"Äîñòóï ê ôàéëó íå ðàçðåø¸í:";
+$lu_error_pass_match						=	"Ââåäåííûå ïàðîëè íå ñîâïàäàþò.";
+$lu_error_pass_wrong						=	"Ââåäåííûé ïàðîëü íåâåðåí.";
+$lu_error_review_not_filled					=	"Êîììåíòàðèé íå ìîæåò áûòü ïóñòûì.";
+$lu_error_user_not_allowed					=	"Ðåãèñòðàöèÿ íå ðàçðåøåííà!";	
+$lu_error_wrong_user						=	"Äàííàÿ ññûëêà ïðèíàäëåæèò äðóãîìó ïîëüçîâàòåëþ!";
+$lu_invalid_entry							=	"Îøèáî÷íûé ââîä";
+$lu_login_incorrect							=	"Äîñòóï íå ðàçðåøåí";
+$lu_pass_not_match							=	"Ââåäåííûå ïàðîëè íå ñîâïàäàþò.";
+$lu_username_used							=	"Íàø ðåãèñòð óêàçûâàåò ÷òî äàííîå èìÿ ïîëüçîâàòåëÿ óæå èìååòñÿ â ñèñòåìå.  Ïîæàëóéñòà, óêàæèòå äðóãîå.";
+
+#########################################################################################
+#Confirmations
+
+$lu_confirm_registration					=	"Âàøà ðåãèñòðàöèÿ áëàãîïîëó÷íî çàâåðøåííà.";
+$lu_confirm_suggest							=	"Ñïàñèáî çà ðåêîììåíäàöèþ ýòîãî ñàéòà äðóãó.";
+$lu_confirm_profile							=	"Äàíûå ïîëüçîâàåòëÿ áëàãîïîëó÷íî èçìåíåííû.";
+$lu_confirm_linkupdate						=	"Âàøà ññûëêà áûëà îáíîâëåííà.";
+$lu_confirm_addlink							=	"Âàøà ññûëêà áûëà äîáàâëåííà â ñèñòåìó.";
+$lu_confirm_suggest							=	"Ñïàñèáî çà ïðåäëîæåííóþ êàòåãîðèþ.";
+$lu_confirm_addreview						=	"Âàø êîììåíòàðèé áûë äîáàâëåí.";
+$lu_confirm_subscribe						=	"Âû ïîäïèñàëèñü íà ñïèñîê ðàññûëîê.";
+$lu_confirm_unsubscribe						=	"Âàøà ïîäïèñêà íà ñïèñîê ðàññûëîê îòìåíåííà.";
+
+#########################################################################################
+#Buttons
+
+$lu_button_addcat							=	"Äîáàâèòü Êàòåãîðèþ";
+$lu_button_addlink							=	"Äîáàâèòü Ññûëêó";
+$lu_button_addreview						=	"Äîáàâèòü Êîììåíòàðèé";
+$lu_button_back								=	"Îáðàòíî";
+$lu_button_cancel							=	"Îòìåíèòü";
+$lu_button_editlink							=	"Èçìåíèòü Ññûëêó";
+$lu_button_go								=	"Âûáðàòü";
+$lu_button_login							=	"Âîéòè";
+$lu_button_ok								=	"Ok";
+$lu_button_rate								=	"Ïðîãîëîñîâàòü";
+$lu_button_register							=	"Ðåãèñòðàöèÿ";
+$lu_button_reset							=	"Î÷èñòèòü";
+$lu_button_search							=	"Ïîèñê";
+$lu_button_search_cats						=	"Ïîèñê ïî êàòåãîðèÿì";
+$lu_button_search_links						=	"Ïîèñê ïî ññûëêàì";
+$lu_button_send								=	"Ïîñëàòü";
+$lu_button_subscribe						=	"Ïîäïèñàòüñÿ";
+$lu_button_unsubscribe						=	"Îòìåíèòü ïîäïèñêó";
+$lu_button_update							=	"Îáíîâèòü Èíôîðìàöèþ";
+$lu_mailing_list							=	"Ïîäïèñêà Ðàññûëîê";
+$lu_not_mailing_list						=	"Íå â Ñïèñêå Ðàññûëîê";
+
+
+#########################################################################################
+#Statistics
+
+$lu_cats									=	"Êàòåãîðèè:";
+$lu_links									=	"Ññûëêè:";
+$lu_total_cats								=	"Êîëëè÷åñòâî êàòåãîðèé â áàçå äàííûõ:  ";
+$lu_total_hits								=	"Ïîñåùåíèé: ";
+$lu_total_links								=	"Êîëëè÷åñòâî ññûëîê â áàçå äàííûõ: ";
+
+
+#########################################################################################
+#Addlink template
+
+$lu_add_cat									=	"Äîáàâèòü â ýòó êàòåãîðèþ";
+$lu_additional_cats							=	"Äîïîëíèòåëüíûå Êàòåãîðèè:";
+$lu_remove									=	"Óäàëèòü";
+$lu_select									=	"Âûáðàòü";
+$lu_submitting_to							=	"Âû äîáàâëÿåòå ññûëêó â êàòåãîðèþ:";
+
+
+#########################################################################################
+#Register template
+
+$lu_first_name								=	"Èìÿ";
+$lu_last_name								=	"Ôàìèëèÿ";
+$lu_re_password								=	"Ïàðîëü (Åù¸ ðàç)";
+$lu_user_name								=	"Èìÿ Ïîëüçîâàòåëÿ";
+
+
+#########################################################################################
+#Miscellaneos Pages
+
+$lu_advanced_search							=	"Ðàñøèðåííûé Ïîèñê";
+$lu_atleast_3								=	"(ìèíèìóì 3 ñèìâîëà)";
+$lu_email									=	"Àäðåñ ýëåêòîðîííîé ïî÷òû";
+$lu_hint_suggest							=	"Ïîðåêîìåíäîâàòü ýòîò ñàéò äðóãó";
+$lu_keyword									=	"Çàïðîñ";
+$lu_login									=	"Âõîä äëÿ ïîëüçîâàòåëåé";
+$lu_not_logged_in							=	"Íå àâòîðèçèðîâàí";
+$lu_password								=	"Ïàðîëü";
+$lu_register								=	"Ðåãèñòðàöèÿ";
+$lu_review									=	"Êîììåíòàðèé";
+$lu_search									=	"Ïîèñê";
+$lu_search_for								=	"Èñêàòü";
+$lu_show									=	"Ïîêàçàòü";
+$lu_sort_by									=	"Ñîðòèðîâàòü ïî";
+$lu_status									=	"Ñòàòóñ";
+$lu_suggest									=	"Ïîðåêîìåíäîâàòü";
+$lu_user									=	"Ïîëüçîâàòåëü";
+$lu_user_menu								=	"Ìåíþ Ïîëüçîâàòåëÿ";
+$lu_welcome									=	"Äîáðî ïîæàëîâàòü";
+
+
+#########################################################################################
+#Dropdowns
+
+$lu_drop_and								=	"È";
+$lu_drop_as_many_as_found					=	"Âñå";
+$lu_drop_ascending							=	"Âîñõîäÿùèé";
+$lu_drop_date								=	"Äàòå";
+$lu_drop_desc								=	"Îïèñàíèþ";
+$lu_drop_descending							=	"Íèñõîäÿùèé";
+$lu_drop_editors_pick						=	"Ññûëêàì Ðåäàêòîðà";
+$lu_drop_hits								=	"Ïîñåùåíèÿì";
+$lu_drop_image								=	"Êàðòèíêàì";
+$lu_drop_name								=	"Íàçâàíèÿì";
+$lu_drop_numlinks							=	"Êîëëè÷åñòâó ññûëîê";
+$lu_drop_numsubs							=	"Êîëëè÷åñòâó êàòåãîðèé";
+$lu_drop_or									=	"ÈËÈ";
+$lu_drop_perm								=	"Ïðàâàì ïîëüçîâàòåëÿ";
+$lu_drop_rating								=	"Ðåéòèíãó";
+$lu_drop_url								=	"Àäðåñó ññûëêè";
+$lu_drop_user								=	"Ïîëüçîâàòåëÿì";
+$lu_drop_vis								=	"Âèäèìîñòè";
+$lu_drop_votes								=	"Ãîëîñàì";
+
+
+#########################################################################################
+#Custom Fields
+
+$lu_categories_custom_fields1			    =	"Äîáàâî÷íîå ïîëå #1";
+$lu_categories_custom_fields2			    =	"Äîáàâî÷íîå ïîëå #2";
+$lu_categories_custom_fields3			    =	"Äîáàâî÷íîå ïîëå #3";
+$lu_categories_custom_fields4			    =	"Äîáàâî÷íîå ïîëå #4";
+$lu_categories_custom_fields5			    =	"Äîáàâî÷íîå ïîëå #5";
+$lu_categories_custom_fields6			    =	"Äîáàâî÷íîå ïîëå #6";
+$lu_links_custom_fields1					=	"Äîáàâî÷íîå ïîëå #1";
+$lu_links_custom_fields2					=	"Äîáàâî÷íîå ïîëå #2";
+$lu_links_custom_fields3					=	"Äîáàâî÷íîå ïîëå #3";
+$lu_links_custom_fields4					=	"Äîáàâî÷íîå ïîëå #4";
+$lu_links_custom_fields5					=	"Äîáàâî÷íîå ïîëå #5";
+$lu_links_custom_fields6					=	"Äîáàâî÷íîå ïîëå #6";
+$lu_users_custom_fields1					=	"Äîáàâî÷íîå ïîëå #1";
+$lu_users_custom_fields2					=	"Äîáàâî÷íîå ïîëå #2";
+$lu_users_custom_fields3					=	"Äîáàâî÷íîå ïîëå #3";
+$lu_users_custom_fields4					=	"Äîáàâî÷íîå ïîëå #4";
+$lu_users_custom_fields5					=	"Äîáàâî÷íîå ïîëå #5";
+$lu_users_custom_fields6					=	"Äîáàâî÷íîå ïîëå #6";
+
+
+#########################################################################################
+#Ratings
+
+$lu_excellent								=	"Âåëèêîëåïíî!";
+$lu_fair									=	"Ïîñðåäñòâåííî";
+$lu_good									=	"Õîðîøî";
+$lu_poor									=	"Íåóäîâëåòâîðèòåëüíî";
+$lu_verygood								=	"Î÷åíü Õîðîøî";
+
+
+#########################################################################################
+#Text segments
+
+$lu_txt_gettingrated	=	"Âû ìîæåòå ðàçìåñòèòü ñëåäóþùèé HTML êîä íà Âàøåì ñàéòå äëÿ òîãî ñòîáû ïîñåòèòåëè Âàøåãî ñàéòà ìîãëè ãîëîñîâàòü çà Âàø ðåñóðñ ïðÿìî íà Âàøåì ñàéòå:";
+
+
+?>
