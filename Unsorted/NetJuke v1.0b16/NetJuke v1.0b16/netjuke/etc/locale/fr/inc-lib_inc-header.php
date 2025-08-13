df0f20a20550024da8f1dfdec8cb28b970e4ea81
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:33 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-lib_inc-header.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-lib_inc-header.php
@@ -0,0 +1,36 @@
+<?php
+
+##################################################
+
+# /lib/inc-header.php
+
+##################################################
+
+define( "TB1_BROWSE", "EXPLORER" );
+define( "TB1_BROWSE_HELP", "Cliquez pour accéder à la page de départ." );
+define( "TB1_SEARCH", "RECHERCHE" );
+define( "TB1_SEARCH_HELP", "Cliquez pour accéder à l'outil de recherche avancée." );
+define( "TB1_RANDOM", "ALÉATOIRE" );
+define( "TB1_RANDOM_HELP", "Cliquez pour accéder à l'outil de selection aléatoire de musique." );
+define( "TB1_RADIO", "RADIO" );
+define( "TB1_RADIO_HELP", "Cliquez pour commencer à jouer la radio internet associée à ce site." );
+define( "TB1_COMMUNITY", "COMMUNAUTÉ" );
+define( "TB1_COMMUNITY_HELP", "Cliquez pour accéder à la section communautaire." );
+define( "TB1_PLISTS", "LISTES D'ÉCOUTE" );
+define( "TB1_PLISTS_HELP", "Cliquez pour accéder à vos listes d'écoute existantes." );
+define( "TB1_ACCOUNT", "COMPTE" );
+define( "TB1_ACCOUNT_HELP", "Cliquez pour accéder à et/ou modifier votre compte d'usager." );
+define( "TB1_PREFS", "PRÉFÉRENCES" );
+define( "TB1_PREFS_HELP", "Cliquez pour accéder à et/ou modifier vos préférences." );
+define( "TB1_SYSADMIN", "ADMIN" );
+define( "TB1_SYSADMIN_HELP", "Cliquez pour accéder aux outils d'administration de systême." );
+define( "TB1_LOGIN", "ENTRER" );
+define( "TB1_LOGIN_HELP", "Cliquez pour accéder à votre compte (login)" );
+define( "TB1_LOGOUT", "SORTIR" );
+define( "TB1_LOGOUT_HELP", "Cliquez pour quitter votre compte (logout)." );
+define( "TB1_REGISTER", "INSCRIPTION" );
+define( "TB1_REGISTER_HELP", "Cliquez pour accéder à l'écran d'inscrption publique." );
+
+##################################################
+
+?>
