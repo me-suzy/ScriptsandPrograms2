41faf09d609aa314a600d6d7279ba950d73cbe28
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:42 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.trackback.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.trackback.php
@@ -0,0 +1,18 @@
+<?php
+$L = array(
+
+
+"trackback_module_name" =>
+"Trackback",
+
+"trackback_module_description" =>
+"Trackback module",
+
+"trackbacks_not_allowed" =>
+"Trackbacks are disabled for this entry",
+
+
+
+''=>''
+);
+?>
