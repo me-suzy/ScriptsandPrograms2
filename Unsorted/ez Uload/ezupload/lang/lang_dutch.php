995724d266a41fd71c28b2fab7de62c5a705f70d
me-suzy <me.suzana@gmail.com>
2025-08-13 04:30:43 +0300

    Add Unsorted/ez Uload/ezupload/lang/lang_dutch.php

---
+++ Unsorted/ez Uload/ezupload/lang/lang_dutch.php
@@ -0,0 +1,51 @@
+<?
+  
+  $LANGPACK[$i]['name'] = "Dutch";
+  $LANGPACK[$i]['file'] = "lang_dutch.php";
+  
+  // Javascript validation (checkfields.php)
+  $_LANG['js_error'] = "De volgende velden zijn niet correct";
+  $_LANG['missing_field'] = "Dit veld mist";
+  $_LANG['invalid_address'] = "Dit is een ongeldig e-mail adres";
+  $_LANG['max_chars'] = "Maximaal aantal karakters";
+  $_LANG['min_chars'] = "Minimaal aantal karakters";
+  $_LANG['no_option_checked'] = "Geen optie gekozen";
+  $_LANG['invalid_extension'] = "Dit bestandstype wordt niet geaccepteerd";
+  $_LANG['large_files_warning'] = "Uw bestanden worden nu ge-uploaded, dit kan even duren al naar gelang de grote van de bestanden en de snelheid van uw verbinding. Blijft u rustig wachten en sluit deze pagina niet.";
+
+  // Password page (form_password.php)
+  $_LANG['enter_pass'] = "Vul uw password in";
+  $_LANG['enter_user'] = "Vul uw password in"; // when user login is turned on
+  $_LANG['your_email'] = "Uw E-Mail";
+  $_LANG['your_pass'] = "Uw Password";
+  $_LANG['forgot_pass'] = "Password vergeten?";
+  $_LANG['email_admin'] = "E-mail de webmaster";
+  $_LANG['enter'] = "Enter"; // for the button
+	
+  // Form page (form_show.php)
+  $_LANG['upload_files'] = "Verzend uw bestelling"; // for the upload button
+  $_LANG['upload_folder'] = "Upload uw map"; // if user has to select sub-directory
+
+  // Upload page (form_upload.php)
+  $_LANG['dir_creation_error'] = "Error! Kan geen folder aanmaken! (Toegang geweigerd)";
+  $_LANG['ext_not_accepted'] = "Bestandstype wordt niet geaccepteerd";
+  $_LANG['file_too_large'] = "Bestandstype is te groot (Max. ";
+  $_LANG['file_too_small'] = "Bestandstype is te klein (Min. ";
+  $_LANG['image_height'] = "Hoogte van de afbeelding moet zijn ";
+  $_LANG['image_width'] = "Breedte van de afbeelding moet zijn ";
+  $_LANG['pixels'] = " pixels"; // follows the two field above
+  $_LANG['required_missing'] = "Een verplicht veld ontbreekt";
+  $_LANG['maxsize_settings'] = "Bestand is te groot";
+  $_LANG['maxsize_form'] = "Bestand is te groot";
+  $_LANG['partial_upload'] = "Het bestand werd slechts gedeeltelijk ge-uploaded";
+  $_LANG['invalid_file'] = "Ongeldig bestand (Gebruik de Browse button)";
+  $_LANG['value_too_short'] = "Waarde is te kort (Min. ";
+  $_LANG['value_too_long'] = "Waarde is te lang (Max. ";
+  
+  // Results page (form_results.php)
+  $_LANG['upload_error'] = "Attentie! Uw upload was niet succesvol! Klik op Nogmaals Proberen voor een nieuwe poging.";
+  $_LANG['try_again'] = "Nogmaals Proberen"; // for a button
+  $_LANG['upload_more'] = "Nog meer uploaden"; // for a button
+  $_LANG['continue'] = "Ga verder"; // for a button
+  
+?>
