a7c3c1b3c7a5b8fffdceaed681438d424b71452c
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:28 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-lib_inc-admin.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-lib_inc-admin.php
@@ -0,0 +1,14 @@
+<?php
+
+##################################################
+
+# /lib/inc-admin.php
+
+##################################################
+
+define( "INCADM_DENIED", "Accés refusé! Vous ne pouvez pas modifier cette chanson." );
+define( "INCADM_NOPERM", "Erreur de privilèges avec le répertoire netjuke/var/data/." );
+
+##################################################
+
+?>
