c360596d45c34a498fdb73a0ebdfc4b73d4b67ac
me-suzy <me.suzana@gmail.com>
2025-08-13 04:49:28 +0300

    Add Unsorted/gcards/inc/lang/language_cat.php

---
+++ Unsorted/gcards/inc/lang/language_cat.php
@@ -0,0 +1,150 @@
+<?
+//catalan language by thesimnet.com thesim@iespana.es
+
+// index.php
+$index01 = 'Totes les categories';
+$index02 = 'Escull una postal abans de començar';
+
+// general navigation
+$nav01 = 'Enderrera';
+$nav02 = 'Endevant';
+$nav03 = 'Inici';
+$nav04 = 'Mostrar';
+$nav05 = 'Administrar';
+$nav06 = 'Cambiar la contrasenya';
+$nav07 = 'Sortir';
+$nav08 = 'Ingresar';
+$nav09 = 'Powered by';
+$nav10 = 'filas per pàgina';
+$nav11 = 'Siusplau torna <a href="javascript:history.go(-1);">enrrere</a>';
+$nav12 = 'Tornar a:';
+
+
+
+// UIfunctions.php
+$uifunc01 = 'Usuari: ';
+
+// compose.php
+$compose01 = 'No vase escullir cap postal! Vingui <a href="index.php">enderrera</a> y esculli una';
+$compose02 = 'Posa-hi texte abaix per enviar la postal !';
+$compose03 = 'Nom';
+$compose04 = 'Email';
+$compose05 = 'De:';
+$compose06 = 'Per:';
+$compose07 = 'Notificar-me quan la postal sigui rebuda';
+$compose08 = 'Direccions emails separades per comes';
+
+// preview.php
+$preview01 = "Completa tots els camps del formulari ! $nav11";
+$preview02 = 'no és una direcció de mail valida';
+$preview03 = "Emails invalids! $nav11";
+$preview04 = 'Enviar';
+
+// sendcard.php
+$sendcard01 = 'Error! Formulari no completat . Vingui <a href="javascript:history.go(-1);">enrere</a>';
+$sendcard02 = 'error insertat: ';
+$sendcard03 = 'eCard enviada...';
+$sendcard04 = 'eCard no pot ser enviada...';
+
+// getcard.php
+$getcard01 = 'Error - no hi ha ID';
+$getcard02 = '$to_email eligida tu postal';
+$getcard03 = 'Enivia a $from_name una postal';
+
+// showcard.php
+$showcard01 = 'Una postal de:';
+
+// news.php
+$news01 = 'Notícies antigues';
+$news02 = 'Enviat';
+$news03 = 'Per';
+$news04 = 'Artícles per pàgines';
+$news05 = 'Notícies';
+$news06 = 'Asunte';
+$news07 = 'Texte';
+$news08 = 'Data';
+$news09 = 'Autor';
+
+
+
+// user related text
+$user01 = 'Usuari';
+$user02 = 'Contrasenya';
+$user03 = 'No has ingresat - no pots sortir!';
+$user04 = 'Has sortit amb èxit...';
+$user05 = 'Confirmació de la contrasenya';
+$user06 = 'Email';
+$user07 = 'Rol';
+$user08 = 'Usuari';
+$user09 = 'Les contrasenyes no concorden - intenta-ho de nou';
+$user10 = '(Deixa-ho en blanc per deixar el mateix valor)';
+$user11 = 'La contrasenya antiga, no concorda ';
+$user12 = 'Contrasenya actualitzada amb èxit!';
+$user13 = 'Contrasenya antiga';
+$user14 = 'nova Contrasenya';
+
+/*
+*********************
+** ADMIN DIRECTORY **
+*********************
+*/
+
+// Authentication
+$auth01 = 'La autentificación falló para l-usuari';
+$auth02 = 'Posi-hi el nom dusuari i la contrasenya .';
+$auth03 = 'No has ingresat.';
+$auth04 = 'Els teus privilegis són insuficients per veuere aquesta pàgina!';
+
+// admin.php
+$admin01 = 'Opcions Administratives';
+$admin02 = 'Manteniment';
+$admin03 = "$admin02-Postals";
+$admin04 = "$admin02-Categories";
+$admin05 = "$admin02-Noticíes";
+$admin06 = "$admin02-Usuaris";
+$admin07 = 'Veure, agregar, actualitzar, i borrar';
+$admin08 = 'ID';
+$admin09 = 'borrat de la BD...';
+$admin10 = 'borrat del servidor...';
+$admin11 = 'no';
+$admin12 = 'actualitzat';
+$admin13 = 'borrat';
+$admin14 = 'agregat';
+$admin15 = 'Agrega una nova';
+$admin16 = 'Puixar';
+
+// actions
+$action01 = 'Acció';
+$action02 = 'Agregar';
+$action03 = 'Editar';
+$action04 = 'Borrar';
+$action05 = 'Veure';
+$action06 = 'Actualitzar';
+$action07 = 'Cancelar';
+$action08 = 'Vista previa';
+
+// cards.php
+$cards01 = 'Imatge';
+$cards02 = 'Thumbnail';
+$cards03 = 'Times Sent';
+$cards04 = 'Postal';
+$cards05 = 'Nom de la postal';
+$cards06 = 'Deixi un espai en blanc per el thumbail automàtic';
+
+// categories
+$cat01 = 'Categoria';
+$cat02 = 'Nom categoria';
+$cat03 = 'Postals en categoria';
+
+// upload.php
+$upload01 = 'Imatge no cargada al servidor!';
+$upload02 = "error en la copia del arxiu amb la funció 'move_uploaded_file'!";
+$upload03 = 'Imatge almatzenada amb exit...';
+$upload04 = 'Error en la copia de l-arxiu!';
+$upload05 = 'Thumbnail puixada amb èxit...';
+$upload06 = 'Imatge subida usada com thumbnail...';
+$upload07 = 'Thumbnail creada amb èxit...';
+$upload08 = 'Siusplau puixa un thumbail si la imatge NO és JPEG.';
+
+
+?>
