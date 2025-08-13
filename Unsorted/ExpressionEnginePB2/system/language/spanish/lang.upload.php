e63c34e3442be49727ca1f33cfcfb1c0b469f023
me-suzy <me.suzana@gmail.com>
2025-08-13 04:24:26 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.upload.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.upload.php
@@ -0,0 +1,52 @@
+<?php
+$L = array(
+
+
+"path_does_not_exist" =>
+"El camino especificado no existe",
+
+"file_exceeds_ini_limit" =>
+"El fichero subido excede el tamaño máximo permitido en su fichero de configuración del PHP",
+
+"file_partially_uploaded" =>
+"El fichero se subió parcialmente",
+
+"no_file_selected" =>
+"No has seleccionado ningún fichero para subir",
+
+"file_upload_error" =>
+"Se ha producido un error de tipo desconocido",
+
+"invalid_filetype" =>
+"El tipo de fichero que intentas subir no está permitido",
+
+"invalid_filesize" =>
+"El fichero que estás intentando subir es más grande que el tamaño máximo permitido",
+
+"invalid_dimensions" =>
+"La imagen que estás intentando subir excede el ancho ó el alto máximo",
+
+"upload_error" =>
+"Se ha encontrado un problema al intentar subir tu fichero",
+
+"copy_error" =>
+"Se ha encontrado un problema al intentar reemplazar el fichero",
+
+"unsupported_protocol" =>
+"Tu servidor no soporta el protocolo de redimensionado de imágenes que hay especificado en tus preferencias.",
+
+"jpg_or_png_required" =>
+"El protocolo de redimensionado de imagen especificado en tus preferencias sólo trabaja con imágenes JPEG ó PNG.",
+
+"jpg_gif_png_required" =>
+"El protocolo de redimensionado de imagen especificado en tus preferencias sólo trabaja con imágenes GIF, JPEG ó PNG.",
+
+"libpath_invalid" =>
+"El camino a tu librería de imágenes no es correcto. Por favor selecciona el camino correcto en tus preferencias.",
+
+"image_resize_failed" =>
+"Ha fallado el redimensionamiento de imagen. Por favor verifica que tu servidor soporta el protocolo escogido y que el camino a tu librería de imágenes es correcto.",
+
+''=>''
+);
+?>
