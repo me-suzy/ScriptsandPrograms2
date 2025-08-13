5e1124f9688bf72be71651642c2bfaaa137506a5
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:02 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_radio-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_radio-edit.php
@@ -0,0 +1,17 @@
+<?php
+
+##################################################
+
+# /admin/radio-edit.php
+
+##################################################
+
+define( "RADIO_LIST_NOPATH", "Fichier Inconnu:\\nLa liste de radio visée ne peut être trouvée." );
+
+define( "RADIO_LIST_NOPATH", "Erreur De Permissions:\\nLa liste de radio visée ne peut être éditée." );
+
+define( "RADIO_LIST_NOPATH", "La liste de radio visée a été sauvegardée avec succés." );
+
+##################################################
+
+?>
