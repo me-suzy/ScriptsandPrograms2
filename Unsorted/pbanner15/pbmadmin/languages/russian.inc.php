6b9733a2cd66e8ed4fe671296876d95f3e7cfea2
me-suzy <me.suzana@gmail.com>
2025-08-13 07:37:34 +0300

    Add Unsorted/pbanner15/pbmadmin/languages/russian.inc.php

---
+++ Unsorted/pbanner15/pbmadmin/languages/russian.inc.php
@@ -0,0 +1,288 @@
+<?php
+/******************************************************************************
+  Russian Language File for Power Banner Manager 1.5
+  (russian.inc.php)
+
+  File Author: Dmitri
+  E-mail: admin@groovetation.com
+******************************************************************************/
+
+/******************************************************************************
+  Author Comments
+******************************************************************************/
+$author_comments = "Äàííûé ïåðåâîä÷èê D:mk@.
+Áîëåå ïîäðîáíàÿ èíôîðìàöèÿ î ïîääåðæêè äðóãèõ ÿçûêîâ
+íàõîäèòñÿ òóò <a href='http://www.ak85.tk'>www.ak85.tk</a> :)";
+
+
+/******************************************************************************
+  Needed charset for this language
+******************************************************************************/
+$charset = "windows-1251";
+$chardir = "";
+
+/******************************************************************************
+  Header Text
+******************************************************************************/
+$header_page_title_text = "Power Banner Manager";
+$header_panel_main_text = "Ïàíåëü Àäìèíèñòðèðîâàíèÿ Power Banner!";
+$header_mysql_ver_text = "MySQL âåðñèÿ:";
+$header_user_logged_text = "Ïîëüçîâàòåëü:";
+
+
+/******************************************************************************
+  Main Menu Text
+******************************************************************************/
+$menu_banner_list_text = "Ñïèñîê áàííåðîâ";
+$menu_add_banner_text = "Äîáàâèòü áàííåð";
+$menu_zone_manage_text = "Ïàíåëü óïðàâëåíèÿ";
+$menu_banner_search_text = "Ïîèñê áàííåðà";
+$menu_pbm_options_text = "Ñâîéñòâà";
+$menu_logout_text = "Âûõîä";
+
+// for administrator
+$menu_user_manage_text = "Óïðàâëåíèå ïîëüçîâàòåëÿìè";
+
+//for normal user
+$menu_change_password_text = "Çàìåíèòü ïàðîëü";
+
+
+/******************************************************************************
+  Banner Listing Text
+******************************************************************************/
+$list_banner_name_text = "Èìÿ:";
+$list_banner_zone_text = "Çîíà:";
+$list_if_banner_in_unsorted_zone_text = "Áåç ñîðòèðîâêè";
+$list_see_next_page_text = "Ñëåäóùàÿ ñòðàíèöà >>";
+$list_no_permition_text = "Ó Âàñ íåò ïðàâ äîñòóïà äëÿ ïðîñìîòðà!";
+
+//text for banner details
+$list_banner_user_name_text = "Èìÿ ïîëüçîâàòåëÿ:";
+$list_banner_user_id_text = "ID ïîëüçîâàòåëÿ:";
+$list_banner_format_text = "Ôîðìàò:";
+
+//text for format
+$list_banner_format_picture_text = "Ðèñóíîê";
+$list_banner_format_flash_text = "Flash";
+
+//text for buttons right to banner
+$list_button_more_info_text = "[äîïîëíèòåëüíàÿ èíôîðìàöèÿ]";
+$list_button_go_to_site_text = "[íà ñàéò]";
+$list_button_modify_text = "[èçìåíèòü]";
+$list_button_delete_text = "[óäàëèòü]";
+$list_button_visitor_text = "[ïîñåòèòåëè]";
+
+
+/******************************************************************************
+  Add Banner Text
+******************************************************************************/
+$add_banner_text = "Äîáàâèòü íîâûé áàííåð";
+$add_banner_format_text = "Ôîðìàò:";
+$add_banner_button_text = "Äîáàâèòü íîâûé áàííåð";
+
+//modify banner text
+$modify_banner_button_text = "Ïîìåíÿòü èíôî î áàííåðå";
+$modify_banner_id_text = "ID:";
+$modify_banner_no_permition = "Ó Âàñ íåò ïðàâ äëÿ èçìåíåíèÿ äàííîãî áàííåðà !";
+$modify_banner_done_text = "Èíôîðìàöèÿ óñïåøíî îáíîâëåíà";
+
+//delete banner text
+$delete_banner_sure_text = "Âû òî÷íî õîòèòå óäàëèòü áàííåð?";
+$delete_banner_done_text = "Áàííåð óäàë¸í!";
+$delete_banner_no_permition = "Ó Âàñ íåò ïðàâ äëÿ óäàëåíèÿ äàííîãî áàííåðà !";
+
+//more banner info text
+$info_banner_total_dis_text = "Âñåãî ïîêàçàíî";
+$info_banner_of_text = "îò";
+$info_banner_procent_text = "Ïðîöåíò";
+$info_banner_of_all_text = "îò âñåõ áàííåðîâ";
+$info_banner_clicks_text = "Êëèêîâ";
+$info_banner_of_dis_text = "îò ïîêàçàííûõ";
+$info_banner_of_total_dis_text = "îò âñåõ ïîêàçàíèé ýòîãî áàííåðà";
+
+//more banner info months text
+$info_banner_month_dis_text = "Ïîêàçàíî â ìåñÿö:";
+$info_banner_select_month_text = "- Âûáåðèòå ìåñÿö -";
+$info_banner_month_january = "ßíâàðü";
+$info_banner_month_february = "Ôåâðàëü";
+$info_banner_month_march = "Ìàðò";
+$info_banner_month_april = "Àïðåëü";
+$info_banner_month_may = "Ìàé";
+$info_banner_month_june = "Èþíü";
+$info_banner_month_july = "Èþëü";
+$info_banner_month_august = "Àâãóñò";
+$info_banner_month_september = "Ñåíòÿáðü";
+$info_banner_month_october = "Îêòÿáðü";
+$info_banner_month_november = "Íîÿáðü";
+$info_banner_month_december = "Äåêàáðü";
+
+$info_banner_dis_on_month = "Ïîêàçàíî â ìåñÿöå:";
+
+//test for format
+$add_banner_picture_format_text = "Êàðòèíêà";
+$add_banner_flash_format_text = "Flash Format";
+
+$add_banner_name_text = "Èìÿ:";
+$add_banner_source_text = "Ïóòü:";
+$add_banner_alt_text = "Òåêñò:";
+$add_banner_url_text = "ññûëêà";
+$add_banner_flash_dim_text = "øèðèíà x âûñîòà";
+
+//banner zone text
+$add_banner_zone_text = "Çîíà Áàííåðîâ:";
+$add_banner_zone_unsorted_text = "- Áåç ñîðòèðîâêè -";
+$add_banner_zone_select_text = "Âûáåðè ñâîþ çîíó";
+$add_banner_zone_add_zone_text = "Äîáàâèòü";
+
+//times to display banner text
+$add_banner_times_text = "Ñêîëüêî ðàç áàííåð<br>ìîæåò áûòü ïîêàçàí:";
+$add_banner_times_unlimited_text = "Áåç ëèìèòà";
+$add_banner_enter_value_combo_text = "- Çíà÷åíèå -";
+$add_banner_enter_value_text = "Ââåäèòå çíà÷åíèå:";
+
+//display type text
+$add_banner_diplay_type_text = "Âèä ýêðàíà:";
+$add_banner_type_standard_text = "Ñòàíäàðòíûé HTML";
+$add_banner_type_popup_text = "Îòêðûâàþùèåñÿ îòêíî";
+$add_banner_type_watermark_text = "JavaScript WaterMark";
+
+//watermark location text
+$add_banner_wm_location_text = "WM ðàçïîëîæåíèå:";
+$add_banner_wn_top_left_text = "Ââåðõó - ñëåâà";
+$add_banner_wn_top_right_text = "Ââåðõó - ñïðàâà";
+$add_banner_wn_bottom_left_text = "Âíèçó - ñëåâà";
+$add_banner_wn_bottom_right_text = "Âíèçó - ñïðàâà";
+
+//target window text
+$add_banner_target_text = "Ãäå îòêðûâàòü:";
+$add_banner_target_current_text = "Â ñâî¸ì îêíå";
+$add_banner_target_new_text = "Â íîâîì îêíå";
+
+//new banner added text
+$add_banner_done_text = "Íîâûé áàííåð äîáàâëåí";
+$add_banner_missing_fields_text = "Âàì íåîáõîäèìî çàïîëíèòü âñå îáÿçàòåëüíûå ïîëÿ!";
+
+
+/******************************************************************************
+  Zone Management Text
+******************************************************************************/
+
+// add zones text
+$zones_add_title_text = "Äîáàâèòü íîâóþ ãðóïïó";
+$zones_add_new_zone_name_text = "Èìÿ íîâîé ãðóïïû:";
+$zones_add_zone_add_button_text = "Äîáàâèòü";
+
+// list zones text
+$zones_list_zones_text = "Ñïèñîê ãðóïï";
+$zones_list_zone_name_text = "Èìÿ ãðóïïû:";
+$zones_list_user_text = "Ïîëüçîâàòåëü:";
+$zones_list_zone_id_text = "ID ãðóïïû:";
+$zones_list_modify_text = "Èçìåíèòü";
+$zones_list_delete_text = "Óäàëèòü";
+
+// add new zone text
+$zones_new_added_text = "Íîâàÿ ãðóïïà äîáàâëåíà";
+
+// delete zone text
+$zone_delete_sure_text = "Âû óâåðåíû ÷òî õîòèòå óäàëèòü ýòó ãðóïïó?";
+$zone_delete_done_text = "Ãðóïïà óäàëåíà";
+$zone_delete_no_permition = "Ó Âàñ íåò ïðàâ äëÿ óäàëåíèå äàííîé ãðóïïû! ";
+
+// modify zone text
+$zone_modify_title_text = "Èçìåíèòü ãðóïïó";
+$zone_modify_zone_name_text = "Èçìåíÿþùååñÿ èìÿ:";
+$zone_modify_button_text = "Èçìåíèòü èìÿ";
+$zone_modify_no_permition_text = "Ó Âàñ íåò ïðàâ äëÿ èçìåíåíèÿ äàííîé ãðóïïû!";
+$zone_modify_done_text = "Ãðóïïà èçìåíåíà";
+
+
+/******************************************************************************
+  Banner Search Text
+******************************************************************************/
+$search_title_text = "Ïîèñê áàííåðîâ";
+
+//search by text
+$search_by_text = "Èñêàòü ïî:";
+$search_by_banner_id_text = "ID áàííåðà";
+$search_by_banner_name_text = "Èìåíè áàííåðà";
+$search_by_banner_url_text = "URL áàííåðà";
+
+$search_something_like_text = "Êàê:";
+$search_button_text = "Èñêàòü";
+$search_reset_text = "Ñáðîñ";
+
+
+/******************************************************************************
+  User Management Text
+******************************************************************************/
+$users_title_text = "Óïðàâëåíèå ïîëüçîâàòåëÿìè";
+$users_login_text = "Èìÿ ïîëüçîâàòåëÿ:";
+$users_permition_text = "Ïðàâà:";
+$users_id_text = "ID ïîëüçîâàòåëÿ:";
+$users_last_ip_text = "Ïîñëåäíèé IP:";
+$users_last_time_text = "Ïîñëåäíåå âðåìÿ:";
+$users_change_pass_text = "Èçìåíèòü ïàðîëü";
+$users_delete_text = "Óäàëèòü";
+
+//users types text
+$users_normal_user_text = "Ïîëüçîâàòåëü";
+$users_admin_text = "Àäìèíèñòðàòîð";
+
+$users_create_new_user_text = "Ñîçäàòü íîâîãî ïîëüçîâàòåëÿ";
+
+//change password text
+$users_change_pass_title_text = "Èçìåòèòü ïàðîëü";
+$users_change_pass_enter_text = "Ââåäèòå íîâûé ïàðîëü";
+$users_change_pass_enter_again_text = "Ïîâòîðèòå íîâûé ïàðîëü";
+$users_change_pass_button_text = "Èçìåíèòü ïàðîëü";
+
+$users_change_pass_done_text = "Ïàðîëü èçìåí¸í ! ";
+$users_change_pass_dont_match_text = "Ââåä¸íûå ïàðîëè íå ñîâïàäàþò !";
+
+//delete user text
+$users_delete_sure_text = "Âû óâåðåíû ÷òî õîòèòå óäàëèòü ïîëüçîâàòåëÿ?";
+$users_delete_dont_leave_banners_text = "[ÄÀ! óäàëèòü âìåñòå ñ áàííåðàìè îò ýòîãî ïîëüçîâàòåëÿ]";
+$users_delete_leave_banners_text = "[ÄÀ! ïåðåäàòü åãî áàííåðû Àäìèíèñòðàòîðó]";
+$users_delete_done_text = "Ïîëüçîâàòåëü óäàë¸í !";
+$users_delete_no_permition_text = "Ó âàñ íåò ïðàâ äëÿ óäàëåíèÿ!";
+
+//add new user text
+$users_add_title_text = "Äîáàâèòü íîâîãî ïîëüçîâàòåëÿ";
+$users_add_login_text = "Ïîëüçîâàòåëü:";
+$users_add_password_text = "Ïàðîëü:";
+$users_add_permition_text = "Ïðàâà ïîëüçîâàòåëÿ:";
+$users_add_button_text = "Äîáàâèòü ïîëüçîâàòåëÿ";
+$users_add_done_text = "Ïîëüçîâàòåëü äîáàâëåí";
+$users_add_fill_all_text = "Íåîáõîäèìî çàïîëíèòü âñå îáÿçàòåëüíûå ïîëÿ !";
+
+
+/******************************************************************************
+  Visitor Info Text
+******************************************************************************/
+$visitor_title_text = "Èíôîðìàöèÿ î ïîñåòèòåëÿõ";
+$visitor_address_text = "Àäðåñà ïîñåòèòåëåé";
+$visitor_browser_text = "Áðîäèëêà ïîñåòèòåëåé";
+$visitor_date_time_text = "Âðåìÿ è äàòà";
+$visitor_ref_by_text = "Ññûëêà";
+
+/******************************************************************************
+  PBM Options Text
+******************************************************************************/
+$options_title_text = "Power Banner Íàñòðîéêè";
+$options_chose_language_text = "Ïîæàëóéñòà âûáåðèòå êàêîé ÿçûê âû õîòèòå èñïîëüçîâàòü èç óñòàíîâëåííûõ íà âàøåì ñåðâåðå:";
+$options_button_change_text = "Èçìåíèòü ÿçûê";
+$options_language_changed_text = "Ïîæàëóéñòà âîéäèòå â ñèñòåìó çàíîâî äëÿ àêòèâàöèè ñìåíû ÿçûêà!";
+
+
+/******************************************************************************
+  Logout Text
+******************************************************************************/
+$logout_main_text = "Âû âûøëè !";
+$logout_login_again_text = "[Íàæìèòå ñþäà, ÷òîáû âîéòè çàíîâî]";
+
+
+/******************************************************************************
+  Footer Copyright Text
+******************************************************************************/
+$footer_copyright_text = "Ïðàâà";
+?>
