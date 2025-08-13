ec0a0d2afd7eda433c6521149193ff8c630d2be6
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:34 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.upload.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.upload.php
@@ -0,0 +1,52 @@
+<?php
+$L = array(
+
+
+"path_does_not_exist" =>
+"Määriteltyä polkua ei ole",
+
+"file_exceeds_ini_limit" =>
+"Ladattu tiedosto ylittää suurimman sallitun ko&#39;on, joka on määritelty sinun PHP configuraatio tiedostossa",
+
+"file_partially_uploaded" =>
+"Tiedosto on vain osittain ladattu",
+
+"no_file_selected" =>
+"Et valinnut tiedostoa ladattavaksi",
+
+"file_upload_error" =>
+"Virhe, tuntematon tiedosto tyyppi.",
+
+"invalid_filetype" =>
+"Tiedosto tyypi, jota yritit ladata, on kielletty",
+
+"invalid_filesize" =>
+"Tiedosto, jota yritit ladata, on ylittää suurimman sallitun ko&#39;on",
+
+"invalid_dimensions" =>
+"Kuva, jonka yritit ladata, ylittää suurimman sallitun leveyden tai korkeuden",
+
+"upload_error" =>
+"Ongelmia tiedoston lataamisessa",
+
+"copy_error" =>
+"Ongelmia tiedoston korvaamisessa",
+
+"unsupported_protocol" =>
+"Serverisi ei tue kuvien muokkausprotokollaa, joka on määritelty ominaisuuksissa.",
+
+"jpg_or_png_required" =>
+"Määrittelemäsi kuvien muokkausprotokolla toimii vain JPEG tai PNG kuvissa",
+
+"jpg_gif_png_required" =>
+"Määrittelemäsi kuvien muokkausprotokolla toimii vain GIF, JPEG tai PNG kuvissa",
+
+"libpath_invalid" =>
+"Polku kuvakansioon ei ole oikein. Aseta oikea polku kuvien ominaisuuksissa.",
+
+"image_resize_failed" =>
+"Kuvan muokkaus epäonnistui. Tarkista, että serverisi tukee valittua protokollaa ja polku kuvakansioon on oikea.",
+
+''=>''
+);
+?>
