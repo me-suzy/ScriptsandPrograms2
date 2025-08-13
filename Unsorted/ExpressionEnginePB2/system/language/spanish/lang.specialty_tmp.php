32f0ebbfb5a642dead03c7fbf3bd1ef3d958ab01
me-suzy <me.suzana@gmail.com>
2025-08-13 04:24:19 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.specialty_tmp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.specialty_tmp.php
@@ -0,0 +1,82 @@
+<?php
+$L = array(
+
+
+"email_title" =>
+"Título email",
+
+"email_message" =>
+"Mensaje email",
+
+"admin_notify_comment" =>
+"Notificación de comentarios al administrador ",
+
+"admin_notify_comment_desc" =>
+"Esta plantilla define el mensaje que recibe el administrador cuando se publica un comentario.",
+
+"admin_notify_trackback" =>
+"Notificación al administrador de Trackbacks",
+
+"admin_notify_trackback_desc" =>
+"Esta plantilla define el mensaje que recibe el administrador cuando se ha recibido un Trackback.",
+
+"admin_notify_reg" =>
+"Notificación al administrador de registro de un nuevo miembro",
+
+"admin_notify_reg_desc" =>
+"Esta plantilla define el mensaje que recibe el administrador cuando se ha registrado una persona.",
+
+"comment_notification" =>
+"Notificación de comentario al usuario.",
+
+"comment_notification_desc" =>
+"Esta plantilla define el mensaje que recibe el usuario cuando alguien ha respondido a su comentario.",
+
+"forgot_password_instructions" =>
+"Instrucciones de contraseña olvidada",
+
+"forgot_password_instructions_desc" =>
+"Esta plantilla define las instrucciones que recibe un usuario que se ha olvidado de su contraseña.",
+
+"mailinglist_activation_instructions" =>
+"Instrucciones de activación de lista de correo",
+
+"mailinglist_activation_instructions_desc" =>
+"Esta plantilla define las instrucciones de activación de la lista de correo",
+
+"mbr_activation_instructions" =>
+"Instrucciones de activación de cuenta de miembro",
+
+"mbr_activation_instructions_desc" =>
+"Esta plantilla define las instrucciones de activación que se envian a las personas que se registran en este sitio.",
+
+"reset_password_notification" =>
+"Notificación de reinicio de contraseña",
+
+"reset_password_notification_desc" =>
+"Esta plantilla define las instrucciones de reinicio de contraseña.",
+
+"validated_member_notify" =>
+"Notificación de validación de la cuenta de miembro",
+
+"validated_member_notify_desc" =>
+"Esta plantilla define la notificación que reciben los miembros cuando el administrador ha validado su cuenta",
+
+"use_this_template" =>
+"¿Activar esta plantilla?",
+
+"use_this_template_exp" =>
+"Si haces click en no, esta plantilla no se utilizará.  En su lugar se generará dinámicamente un mensaje en el lenguaje elegido por el usuario.",
+
+"user_messages_template_desc" =>
+"El propósito de esta plantilla es mostrar errores y otros mensajes a los usuarios después que ellos hayan realizado ciertas acciones como conectarse, enviar un formulario, etc.",
+
+"user_messages_template_warning" =>
+"No borres ninguna de las cinco variables requeridas:",
+
+"available_variables" =>
+"Se pueden utilizar las siguientes variables:",
+
+''=>''
+);
+?>
