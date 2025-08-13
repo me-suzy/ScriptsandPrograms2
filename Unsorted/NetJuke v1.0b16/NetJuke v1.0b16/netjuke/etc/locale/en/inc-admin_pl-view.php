12ffc6a9304c572e29440489150e9efe2169929b
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:27 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_pl-view.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_pl-view.php
@@ -0,0 +1,19 @@
+<?php
+
+##################################################
+
+# /admin/pl-view.php
+
+##################################################
+
+define( "PLVIEW_HEADER_RADIO", "CURRENT RADIO PLAYLIST" );
+define( "PLVIEW_HEADER_JUKEBOX", "CURRENT JUKEBOX PLAYLIST" );
+
+define( "PLVIEW_PAGETOP", "Page Top" );
+define( "PLVIEW_PAGETOP_HELP", "Scrolls Back To The Top Of The Page" );
+define( "PLVIEW_CLOSEWIN", "Close Window" );
+define( "PLVIEW_CLOSEWIN_HELP", "Closes This Window" );
+
+##################################################
+
+?>
