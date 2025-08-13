b00652dd8e3ae4c656d6a829f4ba44af19e53046
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:08 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.mailinglist.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.mailinglist.php
@@ -0,0 +1,112 @@
+<?php
+$L = array(
+
+
+"mailinglist_module_name" =>
+"Mailing Lijst",
+
+"mailinglist_module_description" =>
+"Standaard mailing Lijst beheer",
+
+"ml_mailinglist" =>
+"Mailing List",
+
+"ml_total_in_mailinglist" =>
+"Totaal aantal Email Adressen:",
+
+"ml_view_mailinglist" =>
+"Bekijk Mailing Lijst",
+
+"ml_email_search" =>
+"Email Zoeken",
+
+"ml_email_search_cont" =>
+"Voer een volledig email adres in, of een gedeelte er van",
+
+"ml_email_address" =>
+"Email",
+
+"ml_delete" =>
+"Verwijderen",
+
+"ml_no_results" =>
+"Er zijn geen resultaten gevonden",
+
+"ml_delete_confirm" =>
+"Verwijdern Email adres",
+
+"ml_delete_question" =>
+"Weet u zeker dat u de geselecteerde email adressen wilt verwijderen?",
+
+"ml_email_deleted" =>
+"Email adres is verwijderd",
+
+"ml_emails_deleted" =>
+"Email adressen zijn verwijderd",
+
+"ml_missing_email" =>
+"U moet een email adres invoeren",
+
+"ml_invalid_email" =>
+"Ongeldig email adres",
+
+"ml_email_already_in_list" =>
+"Uw email adres staat al in de mailinglijst",
+
+"ml_banned_email" =>
+"Email adres is niet toegestaan",
+
+"ml_email_accepted" =>
+"Uw email adres is toegevoegd aan onze mailinglijst.",
+
+"ml_email_confirmation_sent" =>
+"Er is een bevestiging naar uw email adres toegestuurd",
+
+"ml_click_confirmation_link" =>
+"Klik op de link in uw email om uw toevoeging aan de mailinglist te bevestigen",
+
+"ml_mailinglist_confirmation" =>
+"Email adres bevestiging",
+
+"ml_thankyou_for_joining" =>
+"Bedankt voor uw aanmelding op onze mailinglijst",
+
+"ml_activation_instructions" =>
+"Klik op de link hieronde om uw email adres te registreren.",
+
+"ml_expiration_blurb" =>
+"Als u niet aan onze mailinglist toegevoegd wilt worden, negeer deze email dan.",
+
+"ml_account_confirmed" =>
+"Uw email adres is geaccepteerd",
+
+"ml_expired_date" =>
+"Uw aanmelding is verlopen.",
+
+"ml_unsubscribe" =>
+"Uw email adres is verwijderd van onze mailinglijst",
+
+"ml_unsubscribe_failed" =>
+"Niet mogelijk uw aanmelding te vinden in onze database",
+
+"ml_add_email_addresses" =>
+"Email adres toevoegen",
+
+"ml_add_email_addresses_cont" =>
+"Scheid elk email adres met een spatie of plaats op een nieuwe regel",
+
+"ml_emails_imported" =>
+"Toevoegen van uw email adres is succesvol verlopen",
+
+"ml_total_emails_imported" =>
+"Totaal aantal ingeschreven email adressen:",
+
+"ml_bad_email_heading" =>
+"De volgende email adressen konden niet geimporteerd worden:",
+
+"ml_total_duplicate_emails" =>
+"Totaal aantal bestaande email adressen:",
+
+''=>''
+);
+?>
