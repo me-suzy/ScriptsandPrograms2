d61bb570c62f9872a9b709981d74082e03931446
me-suzy <me.suzana@gmail.com>
2025-08-13 06:50:44 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-account.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-account.php
@@ -0,0 +1,44 @@
+<?php
+
+##################################################
+
+# /account.php
+
+##################################################
+
+define( "ACCNT_CHECKFORM_1", "Si us plau, introduïu el vostre nom." );
+define( "ACCNT_CHECKFORM_2", "Si us plau, introduïu la vostra adreça electrònica." );
+define( "ACCNT_CHECKFORM_3", "Si us plau, introduïu una adreça electrònica vàlida." );
+define( "ACCNT_CHECKFORM_4", "Si us plau, escolliu una contrasenya." );
+define( "ACCNT_CHECKFORM_5", "Si us plau, confirmeu la nova contrasenya." );
+define( "ACCNT_CHECKFORM_6", "La nova contrasenya és massa curta.\\n   Si us plau, utilitzeu un mínim de 6 caràcters." );
+define( "ACCNT_CHECKFORM_7", "La nova contrasenya és massa llarga.\\n   Si us plau, utilitzeu un màxim de 20 caràcters." );
+define( "ACCNT_CHECKFORM_8", "Les contrasenyes no coincideixen.\\n   Si us plau, torneu a escriure la nova contrasenya." );
+define( "ACCNT_CHECKFORM_9", "Si us plau, introduïu la contrasenya existent per desar els canvis" );
+
+define( "ACCNT_DENIED", "Aquesta adreça electrònica o sobrenom ja estan registrats." );
+
+define( "ACCNT_NEW_HEADER", "REGISTRE D'USUARI" );
+define( "ACCNT_NEW_CAPTION", "Si us plau, ompliu aquest formulari per obrir un altre compte." );
+define( "ACCNT_NEW_SUCCESS_1", "Heu estat registrats com a:" );
+define( "ACCNT_NEW_SUCCESS_2", "Escolliu una opció de la barra d'eines per començar a utilitzar el nou compte.");
+
+define( "ACCNT_EDIT_HEADER", "INFORMACIÓ DEL COMPTE" );
+define( "ACCNT_EDIT_CAPTION", "Utilitzeu aquest formulari per modificar la informació del compte." );
+
+define( "ACCNT_FORMS_NAME", "Nom complet" );
+define( "ACCNT_FORMS_NICKNAME", "Sobrenom" );
+define( "ACCNT_FORMS_EMAIL", "Adreça electrònica" );
+define( "ACCNT_FORMS_PASSWD_1", "Contrasenya" );
+define( "ACCNT_FORMS_PASSWD_2", "Confirmeu contrasenya" );
+define( "ACCNT_FORMS_PASSWD_3", "Contrasenya actual" );
+define( "ACCNT_FORMS_PASSWD_4", "Nova contrasenya" );
+define( "ACCNT_FORMS_PASSWD_5", "Confirmeu la nova contrasenya" );
+define( "ACCNT_FORMS_BTN_LOGIN", "Registra" );
+define( "ACCNT_FORMS_BTN_REGISTER", "Entra" );
+define( "ACCNT_FORMS_BTN_SAVE", "Desa" );
+define( "ACCNT_FORMS_BTN_RESET", "Desfés canvis" );
+
+##################################################
+
+?>
