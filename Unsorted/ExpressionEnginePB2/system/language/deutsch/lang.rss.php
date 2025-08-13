6fba3958f35479049a01d80b013631f10da69b64
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:31 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.rss.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.rss.php
@@ -0,0 +1,24 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"rss_module_name" =>
+"RSS",
+
+"rss_module_description" =>
+"Modul zur Erzeugung von RSS-Seiten",
+
+//----------------------------------------
+
+"rss_invalid_weblog" =>
+"Das in Ihrem RSS-Feed angegebene Weblog existiert nicht.",
+
+
+// END
+''=>''
+);
+?>
