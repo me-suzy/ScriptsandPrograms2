d18ced9d76010653bb9cd299be50d9a151debe67
me-suzy <me.suzana@gmail.com>
2025-08-13 03:23:44 +0300

    Add Unsorted/ebay/Listings/categoryNumber.h

---
+++ Unsorted/ebay/Listings/categoryNumber.h
@@ -0,0 +1,30 @@
+
+//	File categoryNumber.h
+//	Generated ---- 4/21/99   by Steve Yan (stevey@ebay.com)
+
+//	This files defines the enum for the top category and level one category numbers,
+//  Use these enums instead of hardcoded category numbers
+
+
+
+#ifndef CATEGORY_NUMBER_INCLUDE
+#define CATEGORY_NUMBER_INCLUDE
+
+	enum categoryNumber
+    {
+        TopCategory	= 0,
+        CatNumAntiq	= 353,
+		catNumBooks	= 266,
+		catNumCoins	= 866,
+		catNumCollectible	= 1,
+		catNumComputer	= 160,		
+		catNumDolls	= 237,
+		catNumJewelry	= 281,
+		catNumPottery	= 870,
+		catNumPhoto	= 1047,
+		catNumSports	= 888,
+		catNumToys	= 220,
+		catNumMiscellaneous	= 99
+    };
+
+#endif
