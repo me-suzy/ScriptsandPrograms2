e3307befa48e5d0bb3381fc93bcf03056fa8e55a
me-suzy <me.suzana@gmail.com>
2025-08-13 07:47:29 +0300

    Add Unsorted/PHP-Paidmail.v1.00.Nullified/PHP-Paidmail/includes/vars_commission.php

---
+++ Unsorted/PHP-Paidmail.v1.00.Nullified/PHP-Paidmail/includes/vars_commission.php
@@ -0,0 +1,16 @@
+<?
+$Total_Tiers = "5";
+
+$PerClickRate[0] = 50;
+$PerClickRate[1] = 40;
+$PerClickRate[2] = 30;
+$PerClickRate[3] = 20;
+$PerClickRate[4] = 5;
+
+$PerReferralRate[0] = 0;
+$PerReferralRate[1] = 0;
+$PerReferralRate[2] = 0;
+$PerReferralRate[3] = 0;
+$PerReferralRate[4] = 0;
+
+?>
