b53bc9f73f275af8dbb37160729c10a27f4f3bbf
me-suzy <me.suzana@gmail.com>
2025-08-13 06:31:24 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/es/view.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/es/view.php
@@ -0,0 +1,21 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[VIEW_Wait] = "Espere por favor su archivo para empezar a cargar.";
+
+$LOCALE[VIEW_Downloading] = "Cargar";
+
+$LOCALE[VIEW_Click] = "Chasquee aquí si el archivo no comienza automáticamente.";
+
+$LOCALE[VIEW_Error] = "El archivo que usted procuran cargar está indisponible.";
+
+?>
