5bd19d397933417676e63601f59c4de883193068
me-suzy <me.suzana@gmail.com>
2025-08-13 05:57:14 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/template.affiliate.xml.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/template.affiliate.xml.php
@@ -0,0 +1,26 @@
+<%include file="header.php"%>
+
+<div height="20"> &nbsp</div>
+
+<%include file="affiliate.menu.php"%>
+
+<TABLE ALIGN="CENTER" BORDER="0" CELLPADDING="5" CELLSPACING="1" WIDTH="" background="">
+<TR ALIGN="left" bgcolor="" >
+        <TD Align="center">
+                <b>XML format.</b>
+        </TD>
+</TR>
+</TABLE>
+
+<TABLE ALIGN="CENTER" BORDER="0" CELLPADDING="10" CELLSPACING="1" WIDTH="" background="">
+<TR ALIGN="left" bgcolor="" >
+        <TD Align="left">
+           
+        </TD>
+</TR>
+</TABLE>
+
+<%include file="xmlformat.php"%>
+
+
+<%include file="affiliate.footer.php"%>
