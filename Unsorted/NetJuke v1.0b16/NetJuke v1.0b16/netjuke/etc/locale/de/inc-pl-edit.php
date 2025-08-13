dc9b18079eab9404d24bfd8f33b1f5f3ad8f0369
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:56 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-pl-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-pl-edit.php
@@ -0,0 +1,91 @@
+<?php
+
+##################################################
+
+# /pl-edit.php
+
+##################################################
+
+define( "PLEDIT_NOPLID", "Bitte wählen Sie eine Spielliste zum Bearbeiten aus." );
+
+define( "PLEDIT_ADDTO_NOID", "Bitte wählen Sie mindestens einen Titel zum speichern aus." );
+define( "PLEDIT_ADDTO_TITLE_1", "Unbenannte Spielliste" );
+define( "PLEDIT_ADDTO_TITLE_2", "Zufall" );
+
+define( "PLEDIT_DELPL_NOID", "Bitte wählen Sie eine Spielliste zum Löschen aus." );
+define( "PLEDIT_DELPL_CONFIRM", "Sind Sie sicher das Sie diese Spielliste löschen wollen?" );
+
+define( "PLEDIT_DELPT_NOTRID", "Bitte wählen Sie mindestens einen Titel zum löschen aus." );
+define( "PLEDIT_DELPT_CONFIRM", "Sind Sie sicher das Sie die ausgewählten \\nTitel aus der Spielliste löschen wollen?" );
+
+define( "PLEDIT_DENIED", "Das ist keine Ihrer Spiellisten. Bitte wählen Sie erneut." );
+
+define( "PLEDIT_CHCK_HELP", "Klicken um diesen Titel auszuwählen" );
+define( "PLEDIT_PLAY_HELP", "Klicken um diesen Titel anzuhören" );
+define( "PLEDIT_DLOAD_HELP", "Klicken um diesen Titel herunter zu laden" );
+define( "PLEDIT_INFO_HELP", "Klicken um die Information dieses Titel anzuzeigen" );
+define( "PLEDIT_AR_HELP", "Klicken um alle Titel des Interpreten anzuzeigen." );
+define( "PLEDIT_AL_HELP", "Klicken um alle Titel des Albums anzuzeigen." );
+define( "PLEDIT_GE_HELP", "Klicken um alle Titel dieses Genres anzuzeigen." );
+
+define( "PLEDIT_MUSTCLICK", "Bitte klicken sie rechts auf Speichern um Ihre Spielliste zu sichern." );
+
+define( "PLEDIT_HEADER_1", "SPIELLISTE" );
+
+define( "PLEDIT_SHRD", "Gemeinsam" );
+
+define( "PLEDIT_BTN_SAVE", "Speichern" );
+define( "PLEDIT_BTN_DELETE", "Löschen" );
+define( "PLEDIT_BTN_CANCEL", "Abbrechen" );
+
+define( "PLEDIT_ROWCNT_1", "Gespeicherte Titel" );
+define( "PLEDIT_ROWCNT_2", "für" );
+
+define( "PLEDIT_QS_AR", "Interpret" );
+define( "PLEDIT_QS_TR", "Titel" );
+define( "PLEDIT_QS_AL", "Album" );
+define( "PLEDIT_QS_GE", "Genre" );
+define( "PLEDIT_QS_TI", "Dauer" );
+define( "PLEDIT_QS_BTN", "Suchen" );
+
+define( "PLEDIT_TB_PLIST_BTN", "Bearbeiten" );
+define( "PLEDIT_TB_PLAY_ALL", "Anhören" );
+define( "PLEDIT_TB_PLAY_ALL_HELP", "Klicken um Spielliste anzuhören." );
+define( "PLEDIT_TB_SELECT", "Alle auswählen" );
+define( "PLEDIT_TB_SELECT_HELP", "Klicken um alle Titel dieser Seite auszuwählen" );
+define( "PLEDIT_TB_RESET", "Alle abwählen" );
+define( "PLEDIT_TB_RESET_HELP", "Klicken um alle Titel dieser Seite abzuwählen" );
+define( "PLEDIT_TB_PLAY", "Ausgewählte anhören" );
+define( "PLEDIT_TB_PLAY_HELP", "Klicken um ausgewählte Titel anzuhören" );
+define( "PLEDIT_TB_DELETE", "Ausgewählte löschen" );
+define( "PLEDIT_TB_DELETE_HELP", "Klicken um ausgewählte Titel aus der Playliste zu löschen" );
+define( "PLEDIT_TB_ADJUST", "AReihenfolge ändern" );
+define( "PLEDIT_TB_ADJUST_HELP", "Klicken um die Reihenfolge der akutellen Sortierung zu speichern" );
+define( "PLEDIT_TB_ADDTO", "Zur Spielliste kopieren" );
+define( "PLEDIT_TB_ADDTO_HELP", "Klicken um alle makierten Titel zur ausgewählten Spielliste hinzuzufügen" );
+
+define( "PLEDIT_COLS_OP", "Optionen" );
+define( "PLEDIT_COLS_SQ", "Nr." );
+define( "PLEDIT_COLS_SQ_HELP", "Klicken um die Seite nach der gespeicherten Reihenfolge zu sortieren" );
+define( "PLEDIT_COLS_TR", "Titel Name" );
+define( "PLEDIT_COLS_TR_HELP", "Klicken um die Seite nach dem Titel zu sortieren" );
+define( "PLEDIT_COLS_TI", "Dauer" );
+define( "PLEDIT_COLS_TI_HELP", "Klicken um die Seite nach der Dauer zu sortieren" );
+define( "PLEDIT_COLS_AR", "Interpret" );
+define( "PLEDIT_COLS_AR_HELP", "Klicken um die Seite nach dem Interpet zu sortieren" );
+define( "PLEDIT_PLAY_AR_HELP", "Klicken um alle Titel des Interpreten an zu hören" );
+define( "PLEDIT_FILTER_AR_HELP", "Klicken um alle Alben des Interpreten anzuzeigen." );
+define( "PLEDIT_COLS_AL", "Album" );
+define( "PLEDIT_COLS_AL_HELP", "Klicken um die Seite nach dem Album zu sortieren" );
+define( "PLEDIT_PLAY_AL_HELP", "Klicken um alle Titel des Albums an zu hören" );
+define( "PLEDIT_FILTER_AL_HELP", "Klicken um alle Interpreten dieses Albums anzuzeigen." );
+define( "PLEDIT_COLS_TN", "#" );
+define( "PLEDIT_COLS_TN_HELP", "Klicken um die Seite nach der Titel-Nummer zu sortieren" );
+define( "PLEDIT_COLS_GE", "Genre" );
+define( "PLEDIT_COLS_GE_HELP", "Klicken um die Seite nach dem Genre zu sortieren" );
+define( "PLEDIT_PLAY_GE_HELP", "Klicken um alle Titel des Genres an zu hören" );
+
+
+##################################################
+
+?>
