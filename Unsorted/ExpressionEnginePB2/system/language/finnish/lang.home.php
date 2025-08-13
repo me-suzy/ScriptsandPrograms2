0d072a03bcdf6286f34193d1ac0e3958e24b6741
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:01 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.home.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.home.php
@@ -0,0 +1,106 @@
+<?php
+$L = array(
+
+
+"total_validating_comments" =>
+"Comments Awaiting Validation",
+
+"cp_home" =>
+"Kotisivu",
+
+"current_user" =>
+"Tämän hetkinen käyttäjä:",
+
+"system_status" =>
+"Systeemin Status",
+
+"offline" =>
+"Offline",
+
+"online" =>
+"Online",
+
+"member_search" =>
+"Etsi jäseniä",
+
+"search_instructions" =>
+"Syötä osa tai koko sana",
+
+"member_group" =>
+"Jäsenten ryhmä",
+
+"search_by" =>
+"Etsintä kenttä",
+
+"screen_name" =>
+"Nimi näytössä",
+
+"email_address" =>
+"Email osoite",
+
+"url" =>
+"URL",
+
+"notepad" =>
+"Muistio",
+
+"site_statistics" =>
+"Sivujen tilastot",
+
+"value" =>
+"Arvo",
+
+"total_members" =>
+"Jäseniä kaikkiaan",
+
+"total_validating_members" =>
+"Jäseniä, jotka odottavat aktivointia",
+
+"total_entries" =>
+"Viestejä kaikkiaan",
+
+"total_comments" =>
+"Kommetteja kaikkiaan",
+
+"total_trackbacks" =>
+"Trackbackejä kaikkiaan",
+
+"most_recent_entries" =>
+"Viimeisin weblogin viesti",
+
+"most_recent_comments" =>
+"Viimeisin kommentti/Trackback",
+
+"no_comments" =>
+"Tällä hetkellä ei ole kommentteja eikä trackbackejä",
+
+"no_entries" =>
+"Tällä hetkellä ei ole weblogien viestejä",
+
+"entry_title" =>
+"Viestin otsikko",
+
+"comments" =>
+"Kommentit",
+
+"recent_members" =>
+"Viimeisin uusi jäsen",
+
+"join_date" =>
+"Liittymispäivä",
+
+"total_hits" =>
+"Kaikki haetut sivut",
+
+"demo_expiration" =>
+"Demoversion käyttöoikeus päättyy:",
+
+"install_lock_warning" =>
+"Varoitus: install.php tiedosto on vielä serverillä.",
+
+"install_lock_removal" =>
+"Turvallisuus syistä poista install.php tiedosto serveriltäsi käyttämällä FTP-ohjelmaa.",
+
+''=>''
+);
+?>
