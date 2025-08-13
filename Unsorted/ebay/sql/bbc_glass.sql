cb0d536afad67e477bc13bcd7ea4631e19a11e2e
me-suzy <me.suzana@gmail.com>
2025-08-13 03:28:58 +0300

    Add Unsorted/ebay/sql/bbc_glass.sql

---
+++ Unsorted/ebay/sql/bbc_glass.sql
@@ -0,0 +1,34 @@
+/*	$Id: bbc_glass.sql,v 1.3 1999/02/21 02:52:21 josh Exp $	*/
+/* create a new board */
+/* when creating a new board, choose a unique board_id by seeing what exists first */
+
+INSERT INTO ebay_bulletin_board_control
+( board_id,
+  board_name,
+  board_short_name,
+  board_short_description,
+  board_description_len,
+  board_max_post_count,
+  board_max_post_age,
+  board_flags,
+  board_type,
+  board_last_post_time,
+  board_description
+)
+values
+( 19,
+  'Glass',
+  'glass',
+  'Glass',
+  1,
+  0,
+  0,
+  1,
+  8,
+  sysdate,
+  hextoraw('00')
+);
+
+/* then used http://localhost/aw-cgi/eBayISAPI.dll?AdminBoardChangeShow&boardname=glass to change the board description */
+<font size="4"><b>Chat with other glass collectors in the eBay community!</b></font><br><br>
+<font size="2">eBay encourages open communication between members of the eBay community. We provide this discussion board for collectors, experts and friends to chat with each other. No business please! The use of this discussion board for buying or selling, trading, tax evasion, money laundering, theft, bank robbery, fraud, embezzlement or bribery will not be tolerated! Violations may result in the elimination of this discussion board and/or in the suspension of board privileges for offenders!</font>
