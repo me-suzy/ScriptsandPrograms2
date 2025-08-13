a66b0997df137e5021bc5f663f8935a273a84e4d
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:00 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_user-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_user-edit.php
@@ -0,0 +1,51 @@
+<?php
+
+##################################################
+
+# /admin/user-edit.php
+
+##################################################
+
+define( "USEDIT_CHECKFORM_1", "Por favor, entre un nombre." );
+define( "USEDIT_CHECKFORM_2", "Por favor, entre una dirección de correo." );
+define( "USEDIT_CHECKFORM_3", "Por favor, entre una dirección de correo valido." );
+define( "USEDIT_CHECKFORM_4", "Por favor, seleccione un password." );
+define( "USEDIT_CHECKFORM_5", "Por favor, confirme el nuevo password." );
+define( "USEDIT_CHECKFORM_6", "El nuevo password es muy corto.\\n  Por favor, use un mínimo de 6 caracteres." );
+define( "USEDIT_CHECKFORM_7", "El nuevo password es muy largo.\\n   Por favor use un máximo de 20 caracteres." );
+define( "USEDIT_CHECKFORM_8", "Los passwords no coinciden.\\n  Por favor, redigite el nuevo password." );
+
+define( "USEDIT_DENIED_1", "Lo siento. No puedo\\ encontrar la lista." );
+define( "USEDIT_DENIED_2", "Acceso Denegado. por favor, intentelo de nuevo." );
+define( "USEDIT_DENIED_3", "Lo siento, pero no puede eliminarse a usted mismo." );
+define( "USEDIT_DENIED_4", "La dirección de correo y/o alias están registrado." );
+
+define( "USEDIT_NEW_HEADER", "REGISTRO DE USUARIO" );
+define( "USEDIT_NEW_CAPTION", "Por favor, llene el siguiente formulario para abrir una nueva cuenta." );
+define( "USEDIT_NEW_SUCCESS", "Ud. tiene registrado el siguiente usuario" );
+
+define( "USEDIT_EDIT_HEADER", "INFORMACIÓN DE LA CUENTA" );
+define( "USEDIT_EDIT_CAPTION", "Use el siguiente formulario para modificar información de su cuenta." );
+
+define( "USEDIT_FORMS_GROUP", "Grupo" );
+define( "USEDIT_FORMS_NAME", "Nombre Completo" );
+define( "USEDIT_FORMS_NICKNAME", "Alias" );
+define( "USEDIT_FORMS_EMAIL", "Direccioón de correo" );
+define( "USEDIT_FORMS_PASSWD_1", "Password" );
+define( "USEDIT_FORMS_PASSWD_2", "Confirme Password" );
+define( "USEDIT_FORMS_PASSWD_3", "Nuevo Password" );
+define( "USEDIT_FORMS_PASSWD_4", "Confirme Nuevo Password" );
+define( "USEDIT_FORMS_CREATED", "Fecha de Creación" );
+define( "USEDIT_FORMS_UPDATED", "Fecha de actualización" );
+define( "USEDIT_FORMS_LOGIN_CNT", "Conteo de Ingresos" );
+define( "USEDIT_FORMS_BTN_LOGIN", "Registro" );
+define( "USEDIT_FORMS_BTN_REGISTER", "Ingreso" );
+define( "USEDIT_FORMS_BTN_SAVE", "Guardar" );
+define( "USEDIT_FORMS_BTN_RESET", "Limpiar");
+define( "USEDIT_FORMS_BTN_DEL", "Eliminar" );
+define( "USEDIT_FORMS_BTN_DEL_CONF", "¿Está seguro de eliminar este usuario?" );
+define( "USEDIT_FORMS_BTN_USLIST", "Lista de usuario" );
+
+##################################################
+
+?>
