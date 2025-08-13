f456fc93fd2e570025b9aae3a7e5daaf9fc7dc74
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:05 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.emoticon.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.emoticon.php
@@ -0,0 +1,42 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"emoticon_module_name" =>
+"Smilies",
+
+"emoticon_module_description" =>
+"Smiliemodul",
+
+
+//----------------------------------------
+// Emoticon language lines
+//----------------------------------------
+
+"emoticon_heading" =>
+"Smilies",
+
+"emoticon_glyph" =>
+"Symbol",
+
+"emoticon_image" =>
+"Bild",
+
+"emoticon_width" =>
+"Breite",
+
+"emoticon_height" =>
+"H&ouml;he",
+
+"emoticon_alt" =>
+"Alt-tag",
+
+
+// END
+''=>''
+);
+?>
