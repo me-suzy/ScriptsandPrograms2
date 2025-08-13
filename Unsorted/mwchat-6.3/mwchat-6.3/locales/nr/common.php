5ff51a37ca37c34694580f5edb3f68c5a725a168
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:23 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/common.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/common.php
@@ -0,0 +1,78 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[COMMON_Registered_Only] = "Er for registrerte brukere bare.";
+
+$LOCALE[COMMON_Unregistered_Only] = "Er for uregistrerte brukere bare.";
+	
+$LOCALE[COMMON_Username] = "Username";
+
+$LOCALE[COMMON_Function_Invalid] = "Er en ugyldig kommando eller er utilgjengelig til De.";
+
+$LOCALE[COMMON_Operator] = "Lager en operatør.";
+
+$LOCALE[COMMON_No_Operator] = "Er ikke mer en operatør.";
+
+$LOCALE[COMMON_Login_Button] = "Login nå";
+
+$LOCALE[COMMON_Not_Accept] = "Aksepterer ikke private budskap.";
+
+$LOCALE[COMMON_Help] = "Hjelp";
+
+$LOCALE[COMMON_No_Encryption] = "MWChat oppdaget De gjør NOT har mcrypt installert inn i php, sikringsencryption.";
+
+$LOCALE[COMMON_System_Message] = "System Message";
+
+$LOCALE[COMMON_Click_Close] = "Klikk stenge";
+
+$LOCALE[COMMON_YOB] = "År av fødsel";
+
+$LOCALE[COMMON_Location] = "Plassering";
+
+$LOCALE[COMMON_Job] = "Stilling";
+
+$LOCALE[COMMON_Hobbies] = "Hobbyer";
+
+$LOCALE[COMMON_Status] = "Status";
+
+$LOCALE[COMMON_Comments] = "Ytterligere kommentarer";
+
+$LOCALE[COMMON_Password] = "Nytt passord";
+
+$LOCALE[COMMON_Confirm_Password] = "Bekreft nytt passord";
+
+$LOCALE[COMMON_Logout] = "Logout av MWChat";
+
+$LOCALE[COMMON_Err_Pass] = "Passordene som De gått inn i ikke passer eller er ikke minst fem karakterer i lengde.";
+
+$LOCALE[COMMON_HelpClick] = "Klikk her for hjelp";
+
+$LOCALE[COMMON_Text] = "Budskap";
+
+$LOCALE[COMMON_Bug_Report] = "Avlytt beskrivelse";
+
+$LOCALE[COMMON_Reason] = "Grunn";
+
+$LOCALE[COMMON_Action] = "Tiltak";
+
+$LOCALE[COMMON_Topic] = "Emne";
+
+$LOCALE[COMMON_URL] = "URL";
+
+$LOCALE[COMMON_Alias] = "er et dekknavn for kommandoen";
+
+$LOCALE[COMMON_Room_Name] = "Rom Name";
+
+$LOCALE[COMMON_Topic] = "Ingen";
+
+?>
