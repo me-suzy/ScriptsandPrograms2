1f48fcb17f39fc049ad64a1ceab27eb77dd2df80
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:41 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_tr-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_tr-edit.php
@@ -0,0 +1,71 @@
+<?php
+
+##################################################
+
+# /admin/tr-edit.php
+
+##################################################
+
+define( "TREDIT_DELETED_HELP", "The selected track has been deleted." );
+
+define( "TREDIT_DELCHILD_HELP_1", "The N/A value is mandatory and cannot be deleted." );
+define( "TREDIT_DELCHILD_HELP_2", "The N/A value is mandatory and cannot be modified.\\nPlease create a new record using the New checkbox." );
+
+define( "TREDIT_HEADER", "EDIT TRACK INFORMATION" );
+
+define( "TREDIT_ERROR_INSERT", "Error inserting into" );
+define( "TREDIT_ERROR_TR_T", "Error updating netjuke_tracks table" );
+define( "TREDIT_ERROR_AR_T", "Error updating netjuke_artists table" );
+define( "TREDIT_ERROR_AL_T", "Error updating netjuke_albums table" );
+define( "TREDIT_ERROR_GE_T", "Error updating netjuke_genres table" );
+define( "TREDIT_ERROR_TR", "The Track Name has been left empty" );
+define( "TREDIT_ERROR_TN", "The Track Number has been left empty." );
+define( "TREDIT_ERROR_BR", "The Bit Rate is mandatory and must be greater then 0." );
+define( "TREDIT_ERROR_SR", "The Sample Rate is mandatory and must be greater then 0." );
+define( "TREDIT_ERROR_FK", "The Track Kind has been left empty." );
+define( "TREDIT_ERROR_LC", "The Track Location has been left empty." );
+define( "TREDIT_ERROR_NORES", "Please select a valid track" );
+
+define( "TREDIT_FORM_CM", "Edit Comments" );
+define( "TREDIT_FORM_LY", "Edit Lyrics" );
+
+define( "TREDIT_FORM_NEW", "New" );
+
+define( "TREDIT_FORM_TR", "Track Name" );
+define( "TREDIT_FORM_AR", "Artist Name" );
+define( "TREDIT_FORM_AL", "Album Name" );
+define( "TREDIT_FORM_GE", "Genre Name" );
+define( "TREDIT_FORM_TI", "Track Time" );
+define( "TREDIT_FORM_SECONDS", "Seconds" );
+define( "TREDIT_FORM_TN", "Track Number" );
+define( "TREDIT_FORM_BR", "Bit Rate" );
+define( "TREDIT_FORM_SR", "Sample Rate" );
+define( "TREDIT_FORM_FK", "File Kind" );
+define( "TREDIT_FORM_FS", "File Size" );
+define( "TREDIT_FORM_FN", "Location" );
+define( "TREDIT_FORM_LC", "Listener Count" );
+define( "TREDIT_FORM_RESET", "Reset Counter" );
+
+define( "TREDIT_FORM_TR_IMG", "Track Image" );
+define( "TREDIT_FORM_AR_IMG", "Artist Image" );
+define( "TREDIT_FORM_AL_IMG", "Album Image" );
+
+define( "TREDIT_FORM_BTN_SAVE", "Save" );
+define( "TREDIT_FORM_BTN_RESET", "Reset" );
+define( "TREDIT_FORM_BTN_DUP", "Duplicate" );
+define( "TREDIT_FORM_BTN_NEW", "New Track" );
+define( "TREDIT_FORM_BTN_DEL", "Delete" );
+
+define( "TREDIT_FORM_BTN_DEL_CONF", "Are you sure you want to delete this track?" );
+define( "TREDIT_FORM_BTN_DELCHILD_CONF", "Are you sure you want to delete this selection?" );
+
+define( "TREDIT_PLAY", "Play Now" );
+define( "TREDIT_PLAY_HELP", "Click To Play This Track Now." );
+define( "TREDIT_VIEW", "View Mode" );
+define( "TREDIT_VIEW_HELP", "Switch to View Mode." );
+define( "TREDIT_CLOSEWIN", "Close Window" );
+define( "TREDIT_CLOSEWIN_HELP", "Closes This Window" );
+
+##################################################
+
+?>
