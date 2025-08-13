75c4f898a1fdc6d8943eaa90e94c7075d209ee36
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:44 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-pl-shrd-view.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-pl-shrd-view.php
@@ -0,0 +1,79 @@
+<?php
+
+##################################################
+
+# /pl-shrd-view.php
+
+##################################################
+
+define( "PLSHRD_NOPLID", "Si us plau, seleccioneu una llista de cançons per editar." );
+
+define( "PLSHRD_SAVE_NOID", "Si us plau, seleccioneu una llista de cançons per desar-la com a favorita." );
+
+define( "PLSHRD_DELPL_NOID", "Si us plau, seleccioneu la llista de cançons a esborrar." );
+define( "PLSHRD_DELPL_CONFIRM", "Esteu segur de voler esborrar aquesta llista de cançons de la vostra llista de favorits ?" );
+
+define( "PLSHRD_CHCK_HELP", "Feu clic per seleccionar aquesta pista." );
+define( "PLSHRD_PLAY_HELP", "Feu clic per reproduir aquesta pista." );
+define( "PLSHRD_DLOAD_HELP", "Feu clic per descarregar aquesta pista." );
+define( "PLSHRD_INFO_HELP", "Feu clic per accedir als detalls de la pista." );
+define( "PLSHRD_AR_HELP", "Feu clic per veure totes les pistes d'aquest intèrpret." );
+define( "PLSHRD_AL_HELP", "Feu clic per veure totes les pistes d'aquest àlbum." );
+define( "PLSHRD_GE_HELP", "Feu clic per veure totes les pistes d'aquest gènere." );
+
+define( "PLSHRD_HEADER_FAV", "LLISTA COMUNITÀRIA FAVORITS" );
+define( "PLSHRD_HEADER_PUB", "LLISTA COMUNITÀRIA" );
+define( "PLSHRD_HEADER_BY", "Per" );
+
+define( "PLSHRD_BTN_DELETE", "Esborra" );
+define( "PLSHRD_BTN_CANCEL", "Cancel·la" );
+
+define( "PLSHRD_ROWCNT_1", "Pista/es enregistrada/es" );
+define( "PLSHRD_ROWCNT_2", "en" );
+
+define( "PLSHRD_QS_AR", "Intèrpret" );
+define( "PLSHRD_QS_TR", "Títol" );
+define( "PLSHRD_QS_AL", "Àlbum" );
+define( "PLSHRD_QS_GE", "Gènere" );
+define( "PLSHRD_QS_TI", "Durada" );
+define( "PLSHRD_QS_BTN", "Recerca" );
+
+define( "PLSHRD_TB_PLIST_BTN", "Edita" );
+define( "PLSHRD_TB_ADDED", "Afegeix" );
+define( "PLSHRD_TB_PLAY_ALL", "Reprodueix" );
+define( "PLSHRD_TB_PLAY_ALL_HELP", "Feu clic per reproduir la llista tal i com l'ha desat l'autor." );
+define( "PLSHRD_TB_SAVE", "Desa" );
+define( "PLSHRD_TB_SAVE_HELP", "Feu clic per desar la llista de cançons a la vostra llista de favorits." );
+define( "PLSHRD_TB_SELECT", "Selecciona" );
+define( "PLSHRD_TB_SELECT_HELP", "Feu clic per seleccionar totes les pistes d'questa pàgina" );
+define( "PLSHRD_TB_RESET", "Deselecciona tot" );
+define( "PLSHRD_TB_RESET_HELP", "Feu clic per deseleccionar totes les pistes de la pàgina" );
+define( "PLSHRD_TB_PLAY", "Reprodueix selecció" );
+define( "PLSHRD_TB_PLAY_HELP", "Feu clic per reproduir les pistes seleccionades." );
+define( "PLSHRD_TB_ADDTO", "Copia a la llista" );
+define( "PLSHRD_TB_ADDTO_HELP", "Feu clic per copiar les pistes seleccionades a la llista de cançons escollida." );
+
+define( "PLSHRD_COLS_OP", "Opcions" );
+define( "PLSHRD_COLS_SQ", "Seq." );
+define( "PLSHRD_COLS_SQ_HELP", "Feu clic per ordenar aquesta llista per número de seqüència." );
+define( "PLSHRD_COLS_TR", "Títol" );
+define( "PLSHRD_COLS_TR_HELP", "Feu clic per ordenar aquesta llista per títol." );
+define( "PLSHRD_COLS_TI", "Durada" );
+define( "PLSHRD_COLS_TI_HELP", "Feu clic per ordenar aquesta llista per durada de la pista." );
+define( "PLSHRD_COLS_AR", "Intèrpret" );
+define( "PLSHRD_COLS_AR_HELP", "Feu clic per ordenar aquesta llista per intèrprets" );
+define( "PLSHRD_PLAY_AR_HELP", "Feu clic per reproduir totes les canÁons d'aquest artista." );
+define( "PLSHRD_FILTER_AR_HELP", "Feu click per llistar tots els àlbums d'aquest artista." );
+define( "PLSHRD_COLS_AL", "Àlbum" );
+define( "PLSHRD_COLS_AL_HELP", "Feu clic per ordenar aquesta llista per Àlbums" );
+define( "PLSHRD_PLAY_AL_HELP", "Feu clic per reproduir totes les canÁons d'aquest àlbum" );
+define( "PLSHRD_FILTER_AL_HELP", "Feu clic per llistar tots els artistes d'aquest àlbum." );
+define( "PLSHRD_COLS_TN", "Núm." );
+define( "PLSHRD_COLS_TN_HELP", "Feu clic per ordenar aquesta llista per número de pista" );
+define( "PLSHRD_COLS_GE", "Gènere" );
+define( "PLSHRD_COLS_GE_HELP", "Feu clic per ordenar aquesta llista per gènere" );
+
+
+##################################################
+
+?>
