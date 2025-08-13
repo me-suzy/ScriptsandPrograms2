403171e13aae7d74ecafa629c9140e406c4785ae
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:51 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-random.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-random.php
@@ -0,0 +1,23 @@
+<?php
+
+##################################################
+
+# /community.php
+
+##################################################
+
+define( "RNDM_NO_TR", "S'ha de tenir com a mínim 2 cançons en aquest(s)\\ngènere(s) abans d\\'utilitzar aquesta opció." );
+
+define( "RNDM_FORM_HEADER", "SELECCIÓ ALEATÒRIA DE CANÇONS" );
+define( "RNDM_FORM_LIMIT_1", "Limita la llista a" );
+define( "RNDM_FORM_LIMIT_2", "Pistes (màxim)" );
+define( "RNDM_FORM_GENRES", "Limita els gèneres a" );
+define( "RNDM_FORM_GENRES_ALL", "Tots els gèneres" );
+define( "RNDM_FORM_BTN_SEARCH", "Genera la selecció aleatòria" );
+
+define( "RNDM_CLOSEWIN", "Tanca la finestra" );
+define( "RNDM_CLOSEWIN_HELP", "Tanca aquesta finestra" );
+
+##################################################
+
+?>
