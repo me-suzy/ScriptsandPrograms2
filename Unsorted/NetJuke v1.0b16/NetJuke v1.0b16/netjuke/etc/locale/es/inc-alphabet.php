2d5865845ed1b8ce081c7a25efb861c0b9434f50
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:03 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-alphabet.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-alphabet.php
@@ -0,0 +1,27 @@
+<?php
+
+##################################################
+
+# /alphabet.php
+
+##################################################
+
+define( "ALPHA_HEADER_1", "LISTADO ALFABÉTICO" );
+define( "ALPHA_HEADER_2", "Canciones" );
+define( "ALPHA_ARTISTS", "Artistas" );
+define( "ALPHA_ALBUMS", "Álbunes" );
+define( "ALPHA_FOUND", "Buscar" );
+
+define( "ALPHA_LIST_ALL_HELP", "Lista todas las canciones asociadas con " );
+define( "ALPHA_PLAY_ALL_HELP", "Comenzar  la lista con todas las canciones asociadas con " );
+define( "ALPHA_FILTER_ALBUMS_HELP", "Lista Todos Los Artistas Pertenecientes a Este Album");
+define( "ALPHA_FILTER_ARTIST_HELP", "Lista Todos Los Album(S) Pertenecientes a Este Artista");
+
+define( "ALPHA_PAGETOP", "Ir al principio de la página" );
+define( "ALPHA_PAGETOP_HELP", "Ir a la parte superior de la página" );
+define( "ALPHA_CLOSEWIN", "Cerrar Ventana" );
+define( "ALPHA_CLOSEWIN_HELP", "Cerrar esta ventana" );
+
+##################################################
+
+?>
