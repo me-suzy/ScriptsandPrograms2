bb81985c6b83b9a62793cbe3c496b44f63540f93
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:33 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-alphabet.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-alphabet.php
@@ -0,0 +1,27 @@
+<?php
+
+##################################################
+
+# /alphabet.php
+
+##################################################
+
+define( "ALPHA_HEADER_1", "ALAPHABETISCHE AUFLISTUNG" );
+define( "ALPHA_HEADER_2", "Titel" );
+define( "ALPHA_ARTISTS", "Interpreten" );
+define( "ALPHA_ALBUMS", "Alben" );
+define( "ALPHA_FOUND", "Gefunden" );
+
+define( "ALPHA_LIST_ALL_HELP", "Zeige alle Titel die mit diesem verbunden sind." );
+define( "ALPHA_PLAY_ALL_HELP", "Spiele alle Titel die mit diesem verbunden sind." );
+define( "ALPHA_FILTER_ALBUMS_HELP", "Zeige alle Interpreten dieses Albums." );
+define( "ALPHA_FILTER_ARTISTS_HELP", "Zeige alle Alben dieses Interpreten." );
+
+define( "ALPHA_PAGETOP", "Seitenanfang" );
+define( "ALPHA_PAGETOP_HELP", "Geht zum Seitenanfang zurück" );
+define( "ALPHA_CLOSEWIN", "Fenster Schliessen" );
+define( "ALPHA_CLOSEWIN_HELP", "Schliesst dieses Fenster" );
+
+##################################################
+
+?>
