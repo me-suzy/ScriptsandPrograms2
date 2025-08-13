98efee227fe64c923079e12508743320b61369f0
me-suzy <me.suzana@gmail.com>
2025-08-13 06:32:47 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/it/dialog.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/it/dialog.php
@@ -0,0 +1,83 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[DIALOG_Syntax_Error] = "L'errore di sintassi";
+
+$LOCALE[DIALOG_Available_Buddies] = "Gli operatori di amico sono";
+
+$LOCALE[DIALOG_Available_Ignored] = "Gli operatori ignorati sono";
+
+$LOCALE[DIALOG_On_Buddy] = "È sul suo elenco di amico.";
+
+$LOCALE[DIALOG_Not_Buddy] = "Non è sul suo elenco di amico.";
+
+$LOCALE[DIALOG_Add_Buddy] = "È stato aggiunto al suo elenco di amico.";
+
+$LOCALE[DIALOG_Del_Buddy] = "È stato tolto dal suo elenco di amico.";
+
+$LOCALE[DIALOG_Block] = "Lei bloccano adesso dei messaggi privati.";
+
+$LOCALE[DIALOG_Unblock] = "Lei accettano adesso dei messaggi privati.";
+
+$LOCALE[DIALOG_Away] = "È assente.";
+
+$LOCALE[DIALOG_Back] = "Eha di ritorno.";
+
+$LOCALE[DIALOG_Bug_Sent] = "Una relazione di insetto è stato inviata.";
+
+$LOCALE[DIALOG_Colors] = "I suoi colori sono stato aggiornati.";
+
+$LOCALE[DIALOG_ChangeColor] = "Cambiare il colore a";
+
+$LOCALE[DIALOG_Click] = "Lo scatto qui di visitare";
+
+$LOCALE[DIALOG_OP_Only] = "È per gli operatori soltanto.";
+
+$LOCALE[DIALOG_Topic_Updated] = "Ha aggiornato il soggetto di stanza.";
+
+$LOCALE[DIALOG_Smiles] = "I sorrisi disponibili e le espressioni";
+
+$LOCALE[DIALOG_Smiles_Disabled] = "Le facce di Smiley * nelle sue preferenze o dal .";
+
+$LOCALE[DIALOG_Rolled] = "Ha rotolato i dadi!";
+
+$LOCALE[DIALOG_Not_Online] = "Non è attualmente *.";
+
+$LOCALE[DIALOG_Exit] = "Ha la sinistra la stanza.";
+
+$LOCALE[DIALOG_Not_Ignored] = "Non è ignorato.";
+
+$LOCALE[DIALOG_Is_Ignored] = "È ignorato.";
+
+$LOCALE[DIALOG_Unregistered] = "È un ha commercialisto il 0.";
+
+$LOCALE[DIALOG_No_Ignored] = "Lei non ignorano qualunque operatori.";
+
+$LOCALE[DIALOG_No_Buddies] = "Lei non ha qualunque amici.";
+
+$LOCALE[DIALOG_Located] = "è localizzato nella stanza";
+
+$LOCALE[DIALOG_Located_Private] = "È localizzato in una stanza privata.";
+
+$LOCALE[DIALOG_Not_Located] = "Non lascia che le persone per localizzarlo.";
+
+$LOCALE[DIALOG_Kicked] = "È stato tolto dal sistema.";
+
+$LOCALE[DIALOG_Say_Button] = "Inviare il Messaggio";
+
+$LOCALE[DIALOG_Clear_Window] = "Lo Schermo chiaro";
+
+$LOCALE[DIALOG_Private_Message] = "Il messaggio privato";
+	
+$LOCALE[DIALOG_Private_Message_Refresh] = "Rinfrescare l'elenco";
+	
+?>
