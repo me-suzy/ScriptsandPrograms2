748c556500e9404442cfa4eae833851d89a39c85
me-suzy <me.suzana@gmail.com>
2025-08-13 07:35:14 +0300

    Add Unsorted/p4CMS.v1.05.Nullified/p4cms/modules/comment/c.config.php

---
+++ Unsorted/p4CMS.v1.05.Nullified/p4cms/modules/comment/c.config.php
@@ -0,0 +1,12 @@
+<?
+//=============================================
+// Config-Datei  Modul "Kommentare" für p4CMS
+//=============================================
+
+$maxlaenge = "50";  // Maximale Länge des Kommentar-Titels auf der Seite
+$breitepop = "400"; // Breite des Pop-Up Fenster
+$hoehepop = "400"; // Hoehe des Pop-Up Fenster
+$maxshow = "5"; // Maximale Kommentare auf der Seite
+$maxpost = "5000"; // Anzahl der maximal zu postenden Zeichen
+$maxtextseite = "200";
+?>
