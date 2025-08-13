3eea2e2eebd2ea448305a97ac10337ef384731d6
me-suzy <me.suzana@gmail.com>
2025-08-13 06:17:23 +0300

    Add Unsorted/MailWorksPro_v1.2n/MailWorksPro/mwadmin/conf.php

---
+++ Unsorted/MailWorksPro_v1.2n/MailWorksPro/mwadmin/conf.php
@@ -0,0 +1,33 @@
+<?php
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//   Program Name         : MailWorks Professional                           //
+//   Release Version      : 1.2                                              //
+//   Program Author       : SiteCubed Pty. Ltd.                              //
+//   Supplied by          : CyKuH [WTN]                                      //
+//   Packaged by          : WTN Team                                         //
+//   Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                           //
+//                       WTN Team `2000 - `2002                              //
+///////////////////////////////////////////////////////////////////////////////
+// MailWorksPro Configuration File
+$isSetup = 0;
+
+$siteName = "Your Site";
+$siteURL = "";
+
+$dbServer = "";
+$dbUser = "";
+$dbPass = "";
+$dbName = "";
+
+$useTemplates = 0;
+$topTemplate = "";
+$bottomTemplate = "";
+
+$privacyPolicyStmt = '';
+
+$adminUser = "";
+$adminPass = "";
+
+?>
