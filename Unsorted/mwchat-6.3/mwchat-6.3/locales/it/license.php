97062b466d4d7b9074ab04eec2cc82c0f05ef024
me-suzy <me.suzana@gmail.com>
2025-08-13 06:32:58 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/it/license.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/it/license.php
@@ -0,0 +1,29 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[LICENSE_Title] = "Autorizzare MWChat adesso!";
+
+$LOCALE[LICENSE_Info] = "Per favore di entrare il suo Numero di matricola di MWChat al di sotto di completare l'organizzazione.";
+
+$LOCALE[LICENSE_Key] = "Il Numero di matricola";
+
+$LOCALE[LICENSE_Submit] = "Ricuperare le Licenze";
+
+$LOCALE[LICENSE_Upgrade] = "Lo scatto qui se lei migliorano da una versione precedente di MWChat.";
+
+$LOCALE[LICENSE_LoginButton] = "Accedere a MWChat";
+
+$LOCALE[LICENSE_Complete_basic] = "Le congratulazioni! Lei ha autorizzato con successo MWChat con una licenza fondamentale.";
+
+$LOCALE[LICENSE_Complete_pro] = "Le congratulazioni! Lei ha il sucessfully MWChat autorizzato con una licenza professionale.";
+
+?>
