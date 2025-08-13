e50eee07ea9e464c78c6c7da3bad35984e4a7caa
me-suzy <me.suzana@gmail.com>
2025-08-13 03:31:44 +0300

    Add Unsorted/ebay/sql/ebay_finance.sql

---
+++ Unsorted/ebay/sql/ebay_finance.sql
@@ -0,0 +1,33 @@
+/*	$Id: ebay_finance.sql,v 1.2 1999/02/21 02:53:38 josh Exp $	*/
+/*
+ * ebay_finance.sql
+ *
+ *	This table contains daily financial information
+ *
+ */
+
+	drop table ebay_finance;
+
+	create table ebay_finance
+	(
+		when			date
+			constraint	finance_when_nn
+			not null,
+		action			number(3)
+			constraint	finance_action_fk
+			not null,
+		count			number(38)
+			constraint	finance_count_nn
+			not null,
+		amount			number(12,2)
+			constraint	finance_amount_nn
+			not null,
+
+		constraint		finance_pk
+		primary key		(when, action)
+		using index tablespace	statsi01
+		storage (initial 2M next 1M)
+	)
+tablespace statsd01
+storage (initial 10M next 2M);
+
