33b91ccc11927a2b26af58148b68e47ba9c5f71a
me-suzy <me.suzana@gmail.com>
2025-08-13 06:32:17 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/help.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/help.php
@@ -0,0 +1,207 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[HELP_Title] = "Hilfe für";
+
+$LOCALE[HELP_Unavailable] = "Eine hilfreiche Beschreibung dieser Frage ist nicht verfügbar.";
+
+$LOCALE[HELP_Username] = "Ein Name oder Benutzername ist der Wunschname, den eine Person für die Buntzung des Chats wählt. Ein Benutzername kann Ihrem wirklichen Namen ähnlich sein oder einfach frei erfunden sein.";
+
+$LOCALE[HELP_Resolution] = "Bildschirmauflösung wird als die Anzahl der Punkte (Bildpunkte,Pixel) auf dem gesamten Schirm definiert. Zum Beispiel stellt eine 640x480 Pixel grosse Bildschirmauflösung, 640 eindeutige Punkte auf 480 Linien dar, mit gesamt rund 300,000 Bildpunkten. Die Auflösung  in dpi (Bildpunkte per inch) ist aber abhängig von der Größe des Bildschirmes und von der eingestellten Bildschirmuflösung. Zum Beispiel hat ein 15 Zoll Monitor bei einer Bildschirmuflösung von 640x480 über 50 dpi (Bildpunkte pro inch). MWChat versucht, diesen Wert automatisch zu ermitteln, aber Sie können ihn bei Bedarf auch manuell einstellen. Obwohl diese Einstellung zum chatten nicht lebenswichtig ist, stellt sie sicher, dass die Größe des Chats für den User proportional richtig dargestellt wird.";
+
+$LOCALE[HELP_Language] = "MWChat hat die Fähigkeit, einige unterschiedliche Sprachen anzuzeigen. Diese Auswahl erlaubt Ihnen, zu wählen, welche Sprache Sie verwenden möchten.";
+
+$LOCALE[HELP_Password] = "Administrator, Operator und registrierte Konten benötigen ein Kennwort zum Login. Dieses verhindert nicht Autorisierten den Zugang zu Ihrem Konto .";
+
+$LOCALE[HELP_Lobby_Public] = "Die öffentliche Raumliste ist eine Auswahl verschiedener Räume, in denen Sie chatten können. Wählen Sie einen Raum welcher Sie am meisten interessiert oder erstellen Sie Ihren eigenen Raum.";
+
+$LOCALE[HELP_Lobby_Private] = "Die private Raumliste wird für Standardbenutzer versteckt. Wenn Sie in einem privaten Raum chatten möchten, tragen Sie den Namen des gewünschten Raumes unten ein. Private Räume werden nicht auf irgendeiner Raumliste angezeigt, also können Sie private Gespräche ohne die uneingeladenen Benutzer halten, die Ihnen dann nicht in den Raum folgen können.";
+
+$LOCALE[HELP_Lobby_Public_Create] = "Wenn Sie keine Raum aus der Liste oben interessiert, können Sie Ihren eigenen Raum durch eintragen des Namens, des gewünschten Raumes im Feld unten erstellen.";
+
+$LOCALE[HELP_Lobby_Private_Create] = "Tragen Sie den Namen des privaten Raumes ein, den Sie betreten möchten. Wenn der Raum nicht besteht, wird er für Sie erstellt werden. ";
+
+$LOCALE[HELP_Private_Message] = "Wählen Sie einen User,aus dem Drop Down Menü. Alle Nachrichten, die Sie schicken, werden nur zu dem ausgewählten Benutzer gehen. Um zurück zum ganzen Zimmer zu schalten, wählen Sie die Standardsprünge. Privates Messaging kann auch durch Gebrauch vom /msg Befehl gemacht werden.";
+
+$LOCALE[HELP_Private_Message_Refresh] = "Klicken Sie diesen Link an, um die private Nachrichtenbenutzerliste zu aktualisieren.";
+
+$LOCALE[HELP_Colors] = "Wenn Sie die Farbe des Textes ändern möchten, dann klicken Sie auf irgendeiner Farbe in der Farbauswahl. Sie werden eine Nachricht sehen, die Ihnen mitteilt daß Ihre Farben aktualisiert worden sind. Sie können irgendwo in der unter Hälfte der Farbauswahl klicken, um die Standardfarbe wiederherzustellen.";
+
+$LOCALE[HELP_Enter_Password] = "Tragen Sie ein neues Passwort ein, das Sie benutzen möchten. Dieses Passwort kann irgendein alpha numerischer Text sein. Notieren Sie sich dieses Passwort und verwahren Sie es an einem sicheren Ort. Es wird verlangt, daß das Passwort mindesten fünf oder mehr Zeichen lang ist.";
+
+$LOCALE[HELP_Icons] = "Wählen Sie ein Icon aus der Liste unten, welches Sie während des chattens verwenden möchten. Alle Benutzer können Ihr Bild sehen.";
+
+$LOCALE[HELP_Theme] = "Wählen Sie ein Thema aus untenstehender Liste. Ein Thema ändert Ihre Chat Farben. Damit alle Themen Änderungen durchgeführt werden, müssen Sie zuerst zurück zur Lobby and dann wieder in den Chat Raum zurück.";
+
+$LOCALE[HELP_Confirm_Password] = "Tragen Sie das neue Kennwort , das Sie hier gewählt haben erneut ein. Dies bestätigt, daß Sie keinen Tippfehler gemacht haben, und wissen Sie sichert, was Ihr Kennwort ist.";
+
+$LOCALE[HELP_YOB] = "Das ist das Geburtsjahr. Diese Angabe wird verwendet um das Alter auszurechnen, da in manchen Ländern ein Mindestalter vorgeschrieben ist.";
+
+$LOCALE[HELP_Occupation] = "Dies ist ihr Beruf. Gewöhnlich ihre Haupteinkommensquelle.";
+
+$LOCALE[HELP_Hobbies] = "Dieses sind Tätigkeiten, die eine Person genießt zu tun. Wie Hobbys die meisten in der Freizeit vollbracht werden.";
+
+$LOCALE[HELP_Location] = "Dies ist der geographische Ort des Benutzers.";
+
+$LOCALE[HELP_Comments] = "Dies sind alle mögliche Anmerkungen, oder Meinungen, die der Benutzer haben kann oder das  äußern von Wünschen.";
+
+$LOCALE[HELP_Status] = "Dieses ist der gegenwärtige Status der Benutzer. Normalerweise, angebend, wenn ein Benutzer abwesend, online, registriert, ein Administrator, oder ein Operator ist.";
+
+$LOCALE[HELP_Upload_File] = "Dies ist die Datei, die Sie einem Benutzer senden möchten. Klicken Sie die Auswahltaste an, um die Datei zu wählen, die Sie senden möchten.";
+
+$LOCALE[HELP_Upload_User] = "Dies ist der Benutzer, der Ihnen eine Datei senden möchte.";
+
+$LOCALE[HELP_Enter_Message] = "Wenn Sie einen Raum betreten, wird diese Anzeige zusammen mit der Nachricht gesendet, daß Sie den Raum betreten haben.";
+
+$LOCALE[HELP_Exit_Message] = "Wenn Sie einen Raum verlassen, wird diese Anzeige zusammen mit der Nachricht gesendet, daß Sie den Raum verlassen haben .";
+
+$LOCALE[HELP_Away_Message] = "Wenn Sie weggehen, wird diese Anzeige zusammen mit der Nachricht gesendet, das Sie weg sind.";
+
+$LOCALE[HELP_Back_Message] = "Wenn Sie weg waren und wieder zurück kommen, wird diese Nachricht zusammen mit der Nachricht gesendet, das Sie wieder zurück gekommen sind.";
+
+$LOCALE[HELP_Default_Color] = "Durch Einstellen dieser Option verwenden Sie eine bestimmte Farbeinstellung jedesmal wenn Sie den Chat Raum betreten.";
+
+$LOCALE[HELP_Smiles] = "Diese Option lässt Sie zwischen graphischen und Text Smiley´s wählen.";
+
+$LOCALE[HELP_Locate] = "Diese Option lässt Sie wählen, ob Sie anderen Benutzern erlauben (oder verbieten) Sie zu finden.Beim Suchen eines Benutzers wird bekanntgegeben in welchem Raum man sich aufhält.";
+
+$LOCALE[HELP_Upload] = "Durch aktivieren dieser Option ,können Sie Daten von anderen Benutzern empfangen.";
+
+$LOCALE[HELP_Room] = "Dies ist der Raum, in dem Sie sich zur Zeit befinden, oder der Raum den Sie erstellen und oder betreten möchten.";
+
+$LOCALE[HELP_Room_Password] = "Einige private Räume sind durch Passwörter geschützt. Wenn der Raum, den Sie betreten möchten Passwort geschützt ist tragen Sie es hier ein.";
+
+$LOCALE[HELP_Number_Users] = "Dies ist die Gesamtanzahl der Benutzer, die zur Zeit im Raum sind.";
+
+$LOCALE[HELP_Topic] = "Dies ist das jetzige Thema des Raumes.";
+
+$LOCALE[HELP_Buddylist] = "Dieses ist Ihre Freundesliste. Dieses enthält die Benutzernamen aller Benutzer in diesem Raum und alle möglichen Freunde, die Sie haben können. Sie können jeden Namen an klicken, um Informationen über diesen Benutzer zu erhalten, wenn sie registriert wurden.";
+
+$LOCALE[HELP_Serial_Number] = "Dieses ist die MWChat Seriennummer. Sie können eine Seriennummer unter http://www.appindex.net/mwchat erhalten ";
+
+$LOCALE[HELP_Storage] = "Dies ist Ihre Dateispeicherinformationen. Die erste Zahl zeigt, wieviel Platz Sie zur Verfügung haben. Die zweite Zahl zeigt, wie groß Ihre erlaubte Gesamtablage ist. Unten ist eine Liste irgend welcher Files, die Sie haben könnten. Klicken Sie auf einem Dateinamen, um diese Datei down zuladen, oder klicken Sie das Auswahlfenster an, um die Datei zu löschen. Sie können auch die Größe jeder Datei sehen, wann Sie die Datei empfangen haben, und der wer die Dateigesendet hat.";
+
+$LOCALE[HELP_ERROR_License] = "Die Lizenz, die Sie haben, ist falsch oder es gibt Sie nicht. Wiederholen Sie Ihre Lizenz nochmals und wenn es noch immer funktioniert, fordern Sie eine neue Seriennummer an oder kontaktieren sie Appindex Netz Support.";
+
+$LOCALE[HELP_ERROR_AlreadyIn] = "Jemand ist bereits mit Ihrem Benutzernamen eingeloggt. Es ist möglich, wenn Sie sich nicht richtig ausgeloggt haben, das Ihr Account noch immer aktiv ist. Warten Sie zehn Minuten und Versuchen Sie es dann erneut.";
+
+$LOCALE[HELP_ERROR_Logout] = "Ihr Account ist ausgeloggt worden. Sie können sich neu anmelden, wenn Sie möchten.";
+
+$LOCALE[HELP_ERROR_Expired] = "Wenn Sie zu Lange im Leerlauf waren. Sie können auch durch einen Administrator oder Operator hinaus gekickt worden sein.";
+
+$LOCALE[HELP_ERROR_RoomLong] = "Der Raumname den Sie gewählt haben ist zu lang, er darf maximal 15 Zeichen lang sein.";
+
+$LOCALE[HELP_Live] = "Der Chat ist offline. Dies könnte sein, weil ein Administator  das System aktualisiert.";
+
+$LOCALE[HELP_ERROR_Profanity] = "Sie sind ausgeloggt worden da Sie Schweinerein verwendet haben.Solche Ausdrücke sind hier nicht erlaubt.";
+
+$LOCALE[HELP_ERROR_NoData] = "Sie haben nicht alle Felder richtig aufgefüllt. Bitte versuchen Sie es erneut.";
+
+$LOCALE[HELP_ERROR_BadData] = "Die Informationen, die Sie eingegeben sind inkorrekt. Benutzen Sie bitte nur alpha nummerische Zeichen.";
+
+$LOCALE[HELP_ERROR_Serial_Invalid] = "Die Seriennummer, passt nicht mit Ihrem Provider überein. Sie werden eine neue Seriennummer brauchen, überprüfen Sie die IP-Adresse Ihres Webservers.";
+
+$LOCALE[HELP_ERROR_Serial_None] = "Appindex Netz hat keine Seriennummer von Ihnen gespeichert. Bitte registrieren Sie Ihr Produkt dann erhalten Sie eine neue Seriennummer.";
+
+$LOCALE[HELP_ERROR_Critical] = "Ein unbekannter kritischer Fehler hat stattgefunden. Wenn das Problem weiter besteht, kontaktieren Sie den Support von Appindex.Net.";
+
+$LOCALE[HELP_ERROR_Javascript] = "MWChat erfordert ein Webbrowser, der zum Anzeigen von Javascript fähig ist. Aktivieren Sie bitte Javascript in Ihrem Webbrowser.";
+
+$LOCALE[HELP_ERROR_Browser] = "Der Webbrowser den Sie verwenden, ist nicht kompatible mit MWChat. Wir empfehlen Ihnen, Netscape oder Internet Explorer zu verwenden .";
+
+$LOCALE[HELP_ERROR_Restricted] = "Der Benutzername, den Sie gewählt haben, ist ein eingeschränkter oder reservierter Name. Wählen Sie bitte einen anderen Namen und melden Sie sich erneut an.";
+
+$LOCALE[HELP_ERROR_BadPass] = "Ihr Passwort ist falsch. Kontaktieren Sie einen Administrator.";
+
+$LOCALE[HELP_ERROR_IP] = "MWChat hat entdeckt, daß sich Ihre IP Adresse geändert hat. Aus Sicherheitsgründen haben wir Sie abgemeldet. Bitte melden Sie sich neu an.";
+
+$LOCALE[HELP_COMMAND_addbuddy] = "Fügen Sie einen Freund Ihrer Freundliste hinzu. Dieser Befehl kann nur von registrierten Benutzern verwendet werden .";
+
+$LOCALE[HELP_COMMAND_admin] = "Ermöglicht einem Administrator Log Dateien anzusehen oder einen Anwender abzumelden. Wenn Sie die zweite Option wählen (Anwendername) können Sie  die Logdateien eines bestimmten Anwenders ansehen oder ihn abmelden.";
+
+$LOCALE[HELP_COMMAND_afk] = "Ändert die Anwendung auf 'nicht anwesend'. Sie können eine 'nicht anwesend' Nachricht eingeben";
+
+$LOCALE[HELP_COMMAND_block] = "Blockiert alle privaten Nachrichten von allen Benutzern.";
+
+$LOCALE[HELP_COMMAND_bug] = "Senden Sie einen Fehler Bericht.";
+
+$LOCALE[HELP_COMMAND_about] = "Einige Informationen über MWChat und die Entwickler.";
+
+$LOCALE[HELP_COMMAND_clear] = "Löscht das gegenwärtige Chatfenster.";
+
+$LOCALE[HELP_COMMAND_color] = "Stellen Sie die Textfarbe auf eine Farbe Ihrer Wahl ein. Diese Farbe muß in diesem Format #000000 sein.";
+
+$LOCALE[HELP_COMMAND_deop] = "Entfernen Sie einen Benutzeroperatorstatus. Dieser Befehl kann nur von den Administratoren und von den Operatoren verwendet werden.";
+
+$LOCALE[HELP_COMMAND_global] = "Schicken Sie allen Benutzern in allen Räumen eine Nachricht. Dieser Befehl kann nur von einem Administrator verwendet werden.";
+
+$LOCALE[HELP_COMMAND_help] = "Zeigt dieses Dokument an.";
+
+$LOCALE[HELP_COMMAND_ignore] = "Fügen Sie einen Benutzer Ihrer Ignorier-Liste hinzu. Jene Benutzer können Ihnen keine privaten Nachrichten senden und Sie können deren Nachrichten nicht sehen. Dieser Befehl kann nur von registrierten Benutzern verwendet werden.";
+
+$LOCALE[HELP_COMMAND_unignore] = "Entfernen Sie einen Benutzer aus Ihrer Ignorier-Liste. So das der Benutzer Ihnen wieder private Nachrichten senden kann und sie seine Nachrichten wieder sehen können. Dieser Befehl kann nur von registrierten Benutzern verwendet werden.";
+
+$LOCALE[HELP_COMMAND_kick] = "Kicken Sie einen Benutzer aus dem System. Sie können einen Grund dafür angeben. Nur Administratoren und Operators können diesen Befehl verwenden.";
+
+$LOCALE[HELP_COMMAND_lobby] = "Zurück zur Lobby. Sie können eine Ausgangsnachricht eingeben.";
+
+$LOCALE[HELP_COMMAND_locate] = "Suchen Sie einen spezifizierten Benutzer. Dieser Befehl kann nur von zugelassenen Benutzern verwendet werden.";
+
+$LOCALE[HELP_COMMAND_logout] = "Loggt einen Benutzer aus dem System aus. Sie können wahlweise eine Nachricht eingeben.";
+
+$LOCALE[HELP_COMMAND_me] = "Beschreiben Sie was Sie machen ohne auf sich selbst Bezug zu nehmen.";
+
+$LOCALE[HELP_COMMAND_motd] = "Zeigt die Nachricht des Tages an .";
+
+$LOCALE[HELP_COMMAND_myfiles] = "Zeigt Ihre Online-Daten an .";
+
+$LOCALE[HELP_COMMAND_op] = "Erteilen Sie einem Benutzeroperator Status. Dieser Befehl kann nur von Administratoren und Operatoren verwendet werden.";
+
+$LOCALE[HELP_COMMAND_prefs] = "Zeigt Ihre MWChat Präferenzen an .";
+
+$LOCALE[HELP_COMMAND_print] = "Zeigt Ihre Unterhaltungen in einem druckbaren Fenster an.";
+
+$LOCALE[HELP_COMMAND_msg] = "Senden Sie einem Benutzer eine private Nachricht. Niemand sonst kann diese Nachricht sehen.";
+
+$LOCALE[HELP_COMMAND_register] = "Registriert den gegenwärtig verwendeten Benutzernamen. Das Registrieren erlaubt Benutzern, Daten aus zu tauschen und Freundeslisten zu erstellen. ";
+
+$LOCALE[HELP_COMMAND_removebuddy] = "Entfernt einen Freund aus Ihrer Freundesliste. Dieser Befehl kann von registrierten Benutzern verwendet werden.";
+
+$LOCALE[HELP_COMMAND_roll] = "Rollt die Chatraumwürfel. Dies ist für Online-Spiele nützlich.";
+
+$LOCALE[HELP_COMMAND_showbuddy] = "Zeigt alle Benutzer aus Ihrer Freundesliste an. Dieser Befehl kann nur von registrierten Benutzern verwendet werden.";
+
+$LOCALE[HELP_COMMAND_showignored] = "Zeigt alle Benutzer aus Ihrer ignorieren Liste an. Dieser Befehl kann nur von registrierten Benutzern verwendet werden.";
+
+$LOCALE[HELP_COMMAND_smiley] = "Zeigt alle vorhandenen smileygesichter an.";
+
+$LOCALE[HELP_COMMAND_topic] = "Ändert das Raumthema. Dieser Befehl kann nur von Administratoren und Operators benutzt werden.";
+
+$LOCALE[HELP_COMMAND_unafk] = "Ändert Ihren Status von 'nicht anwesen' auf 'anwesend'. Man kann auch eine 'wieder zurüch' Nachricht eingeben.";
+
+$LOCALE[HELP_COMMAND_unblock] = "Erlaubt Ihnen wieder, private Nachrichten von allen Benutzern anzunehmen.";
+
+$LOCALE[HELP_COMMAND_topic] = "Entfernt einen Benutzer aus Ihrer Ignorier-Liste. Dieser Befehl kann nur von registrierten Benutzern verwendet werden.";
+
+$LOCALE[HELP_COMMAND_unregister] = "Löschen Ihres Benutzernamens. Danach kann jeder Benutzer diesen Namen verwenden und neu registrieren.";
+
+$LOCALE[HELP_COMMAND_upload] = "Zeigt die Datei die gerade gesendet wird an. Dieser Befehl kann nur durch registrierten Benutzer benutzt werden.";
+
+$LOCALE[HELP_COMMAND_url] = "Erlaubt, daß Sie eine URL in den Chat einfügen.";
+
+$LOCALE[HELP_COMMAND_whois] = "Zeigt Informatioen über einen speziellen Benutzer. Das kann nur von registrierter Benutzer verwendet werden.";
+
+$LOCALE[HELP_COMMAND_Title] = "Verfügbare Befehle";
+
+$LOCALE[HELP_COMMAND_Type] = "{}Dieses Zeichen steht für ein 'Mussfeld', []dieses Zeichen steht für ein 'Kannfeld'.";
+
+?>
