3139fafc078680acb90a5b3ec75cbe82a84bf317
me-suzy <me.suzana@gmail.com>
2025-08-13 06:25:45 +0300

    Add Unsorted/ModernBill311Tier2Null[darkspeed]/ModernBill311Tier2Null/order/include/echo.inc.php

---
+++ Unsorted/ModernBill311Tier2Null[darkspeed]/ModernBill311Tier2Null/order/include/echo.inc.php
@@ -0,0 +1,97 @@
+<?php
+// +----------------------------------------------------------------------+
+// | ModernBill [TM] .:. Client Billing System                            |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2001-2002 ModernGigabyte, LLC                          |
+// +----------------------------------------------------------------------+
+// | This source file is subject to the ModernBill End User License       |
+// | Agreement (EULA), that is bundled with this package in the file      |
+// | LICENSE, and is available at through the world-wide-web at           |
+// | http://www.modernbill.com/extranet/LICENSE.txt                       |
+// | If you did not receive a copy of the ModernBill license and are      |
+// | unable to obtain it through the world-wide-web, please send a note   |
+// | to license@modernbill.com so we can email you a copy immediately.    |
+// +----------------------------------------------------------------------+
+// | Authors: ModernGigabyte, LLC <info@moderngigabyte.com>               |
+// | Support: http://www.modernsupport.com/modernbill/                    |
+// +----------------------------------------------------------------------+
+// | ModernGigabyte and ModernBill are trademarks of ModernGigabyte, LLC. |
+// +----------------------------------------------------------------------+
+##
+## [DO NOT MODIFY/REMOVE BELOW]
+##
+if ($DIR && ($HTTP_COOKIE_VARS[DIR] || $HTTP_POST_VARS[DIR] || $HTTP_GET_VARS[DIR] || $_COOKIE[DIR] || $_POST[DIR] || $_GET[DIR])) {
+    $ip   = $HTTP_SERVER_VARS[REMOTE_ADDR];
+    $host = gethostbyaddr($ip);
+    $url  = $HTTP_SERVER_VARS["HTTP_HOST"].$HTTP_SERVER_VARS["REQUEST_URI"];
+    $admin= ($GLOBALS[SERVER_ADMIN]) ? $GLOBALS[SERVER_ADMIN] : "security@your.server.com";
+    $body = "IP:\t$ip\nHOST:\t$host\nURL:\t$url\nVER:\t$version\nTIME:\t".date("Y/m/d: h:i:s")."\n";
+    @mail($admin,"Possible breakin attempt.",$body,"From: $admin\r\n");
+    print str_repeat(" ", 300)."\n";
+    flush();
+    ?>
+    <html><head><body>
+    <center><h3><tt><b><font color=RED>Security violation from: <?=$ip?> @ <?=$host?></font></b></tt></h3></center>
+    <hr>
+    <pre><? @system("traceroute ".escapeshellcmd($ip)." 2>&1"); ?></pre>
+    <hr>
+    <center><h2><tt><b><font color=RED>The admin has been alerted.</font></b></tt></h2></center>
+    </body></html>
+    <?
+    exit;
+}
+
+include ($DIR."include/misc/echo.php");
+
+$string  = "billing_ip_address=".urlencode(getenv("REMOTE_ADDR"))."&";
+$string .= "product_description=". urlencode($x_Description)."&".
+$string .= "cc_number=".         urlencode($x_Card_Num)."&";
+list($ccexp_month,$ccexp_year) = explode("/",$x_Exp_Date);
+$string .= "ccexp_month=".       urlencode($ccexp_month)."&";
+$string .= "ccexp_year=".        urlencode($ccexp_year)."&";
+$string .= "cnp_security=".      urlencode($x_Card_Code)."&";
+$string .= "grand_total=".       urlencode($x_Amount)."&";
+$string .= "billing_first_name=".urlencode($x_First_Name)."&";
+$string .= "billing_last_name=". urlencode($x_Last_Name)."&";
+$string .= "billing_company_name=".urlencode($x_Company)."&";
+$string .= "billing_address1=".  urlencode($x_Address)."&";
+$string .= "billing_address2=".  urlencode($x_Address_2)."&";
+$string .= "billing_city=".      urlencode($x_City)."&";
+$string .= "billing_state=".     urlencode($x_State)."&";
+$string .= "billing_zip=".       urlencode($x_Zip)."&";
+$string .= "billing_country=".   urlencode($x_Country)."&";
+$string .= "billing_phone=".     urlencode($x_Phone)."&";
+$string .= "billing_fax=".       urlencode($x_Fax)."&";
+$string .= "billing_email=$x_Email&";
+
+$this_charge=echo_gateway($string);
+
+switch($this_charge['status'])
+{
+     case G: ## APPROVED
+          $auth_return = 1;
+          $auth_code = $this_charge['auth_code'];
+          $avs_code  = $this_charge['avs_result'];
+          $trans_id  = $this_charge['order_number'];
+          if ($debug) echo "$auth_code,$avs_code,$trans_id<br>";
+          $cc_result = "<b>".YOURORDERSUCCESS."</b><br>";
+          $cc_result.= AUTHCODE.": $auth_code<br>";
+          $cc_result.= AVSCODE.": $avs_code<br>";
+          $cc_result.= TRANSID.": $trans_id<br>";
+     break;
+
+     case D: ## DECLINED
+          $auth_return = 2;
+          $error_msg = "<b>".YOURORDERDECLINED."</b><br>";
+          $declined  = 1;
+          $z++;
+     break;
+
+     default: ## ERROR
+          $auth_return = 3;
+          $error_msg = "<b>".YOURORDEREERROR."</b><br>";
+          $error     = 1;
+          $z++;
+     break;
+}
+?>
