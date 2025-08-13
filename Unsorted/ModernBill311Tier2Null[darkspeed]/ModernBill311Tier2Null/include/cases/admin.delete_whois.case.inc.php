255095660c7af81d6a9ccbe20920d88b39dda569
me-suzy <me.suzana@gmail.com>
2025-08-13 06:20:38 +0300

    Add Unsorted/ModernBill311Tier2Null[darkspeed]/ModernBill311Tier2Null/include/cases/admin.delete_whois.case.inc.php

---
+++ Unsorted/ModernBill311Tier2Null[darkspeed]/ModernBill311Tier2Null/include/cases/admin.delete_whois.case.inc.php
@@ -0,0 +1,21 @@
+<?
+/*
+** ModernBill [TM] (Copyright::2001)
+** Questions? webmaster@modernbill.com
+**
+**
+**          Always save a backup before your upgrade!
+**          Proceed with caution. You have been warned.
+*/
+
+## Must be included ONLY once!
+include_once("include/functions.inc.php");
+
+## Validate that the user is an ADMIN or log them out
+if (!testlogin()||!$this_admin||$this_user)  { Header("Location: http://$standard_url?op=logout"); exit; }
+
+/* ---- DELETE ----*/
+if(!$dbh)dbconnect();
+@mysql_query("DELETE FROM whois_stats",$dbh);
+Header("Location: $page?".session_id()."");
+?>
