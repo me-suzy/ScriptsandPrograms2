e803e6f16cfb66028850aae6acf9e28be8c70c4f
me-suzy <me.suzana@gmail.com>
2025-08-13 07:03:16 +0300

    Add Unsorted/newsgroup/lang/da/global.php

---
+++ Unsorted/newsgroup/lang/da/global.php
@@ -0,0 +1,316 @@
+<?
+// Language:                    DANSK
+// Translator:                  Kevin Steffer
+// Created:                     24/07/2002
+// Last Update:                 30/07/2002
+// MyNewsGroups :) Version:     0.4
+
+
+// Charset
+define("_CHARSET","charset = iso-8859-1");
+
+// HOW THE PHP date() function displays the date
+define("_MYNGDATEDISPLAY","d-m-Y"); 
+
+// Header
+define("_MYNGWELCOME"," Velkommen til MyNewsGroups :) v ");
+define("_MYNGSYSTEM_INFO"," Info:");
+define("_MYNGMENU_HOME","Hjem");
+define("_MYNGMENU_NEWSGROUPS"," Nyhedsgrupper ");
+define("_MYNGMENU_STATS","Stat");
+define("_MYNGMENU_SEARCH","Søg");
+define("_MYNGMENU_ABOUT","Om");
+
+// Footer
+define("_MYNGHOME_PAGE","Projektets hjemmeside:");
+define("_MYNGGPL","GPL Licenseret");
+define("_MYNGDEBUG","Debug?");
+
+// Login
+define("_MYNGLOGIN","Brugernavn");
+define("_MYNGPASSWORD","Password");
+define("_MYNGREGISTER","Registrer");
+
+// NewsGroups
+define("_MYNGBROWSE_NG","Se nyhedsgrupper");
+define("_MYNGARTICLE_NUMBER","Antal artikler");
+define("_MYNGGROUP_NAME","Gruppenavn");
+define("_MYNGDESCRIPTION","Beskrivelse");
+define("_MYNGSUBSCRIBE_GROUP","Abonnér på denne gruppe");
+
+// Register
+define("_MYNGFILL_FORM","Udfyld venligst formularen for at registrere.");
+define("_MYNGUSER_NAME","Brugernavn");
+define("_MYNGPASSWORD","Password");
+define("_MYNGEMAIL","Email");
+define("_MYNGPASSWORD_AGAIN","Password igen");
+
+// Phrase: 'All fields are required'
+define("_MYNGALL_FIELDS","Alle felter er");
+define("_MYNGREQUIRED","påkrævet");
+define("_MYNGVISIBLE","Synlig");
+define("_MYNGYES","Ja");
+define("_MYNGNO","Nej");
+define("_MYNGCOUNTRY","Land");
+define("_MYNGINSERT_VALID_EMAIL","Benyt venligst en gyldig email adresse, - den skal kunne godkendes.");
+define("_MYNGPASSWORD_TRANSFER","Din email adresse skal være synlig, men den bliver beskyttet mod SPAM-bots, endda også når du skriver artikler.");
+define("_MYNGSPAM_PROTECTED","Dit password sendes ikke over Internettet.");
+define("_MYNGSECURE_LOGIN","Vores system er sikkert, - kun krypteret tekst vil være synlig for andre");
+define("_MYNGREGISTRATION_COMPLETE","Registrering fuldendt");
+define("_MYNGTHANKS","Tak!");
+define("_MYNGDATA_STORED","Dine data er er gemt");
+define("_MYNGEDIT_INFO","Du kan redigere informationerne som du har sendt til din hjemmeside");
+define("_MYNGSUBMITTED_INFO","Her er informationerne som du har sendt");
+define("_MYNGREGISTER_WELCOME","Velkommen til dit nye community!");
+define("_MYNGSTAFF","MyNewsGroups :) Team.");
+define("_MYNGLOGIN_TRANSFER","Når du logger ind bliver dit password ikke sendt over internettet");
+define("_MYNGCOOKIES","Cookies skal accepteres for at kunne logge på");
+
+// Login
+define("_MYNGHOME","Startside");
+define("_MYNGWELCOME_USER","Velkommen");
+define("_MYNGPOST","Skriv");
+define("_MYNGREPLY","Besvar");
+define("_MYNGSAVE","Gem");
+define("_MYNGSTATS","Statistik");
+define("_MYNGSUBSCRIBE","Abonnér");
+define("_MYNGOPTIONS","Indstillinger");
+define("_MYNGPROFILE","Profil");
+define("_MYNGNEW_ARTICLES","nye artikler");
+define("_MYNGTO_ARTICLES","til artikler");
+define("_MYNGEDIT_YOUR","Rediger din");
+define("_MYNGTO_GROUPS","til nye nyhedsgrupper");
+define("_MYNGYOUR_ARTICLES","dine favorit artikler");
+define("_MYNGVIEW_YOUR","se dine egne");
+
+// My Bar
+define("_MYNGMY_BOX","Min menu");
+define("_MYNGMY_HOME","Start");
+define("_MYNGMY_GROUPS","Mine grupper");
+define("_MYNGMY_ARTICLES","Mine Artikler");
+define("_MYNGLOG_OUT","Log ud");
+define("_MYNGMY_STATS","Min Statistik");
+
+// Tree
+define("_MYNGNEWSGROUP_UNSELECTED","Ingen grupper er valgt");
+define("_MYNGPOST_ARTICLE","Skriv en artikel");
+define("_MYNGDATE","Dato");
+define("_MYNGSUBJECT","Emne");
+define("_MYNGAUTHOR","Forfatter");
+define("_MYNGARTICLES","Artikler");
+define("_MYNGNEXT","Næste");
+define("_MYNGPREVIOUS","Forrige");
+define("_MYNGMARK_ALL_READ","Marker alle læst");
+define("_MYNGMARK_THREAD_READ","Marker tråd læst");
+
+// Article
+define("_MYNGREPLY","Besvar");
+define("_MYNGSEND_FRIEND","Tip en ven");
+define("_MYNGPRINT","Udskriv");
+define("_MYNGSAVE_ARTICLE","Gem artikel");
+
+// Search Engine
+define("_MYNGSEARCH_RESULTS","Søgeresultat");
+define("_MYNGNOARTICLES_FOUND","Desværre, - ingen artikler indeholder");
+define("_MYNGSEARCH_ENGINE","Søgemaskine");
+define("_MYNGSEARCH_ONE_GROUP","Søg in én gruppe");
+define("_MYNGSEARCH_ALL_GROUPS","Søg i alle grupper");
+define("_MYNGAND_OPERATION","Hvis du skriver mere end 1 ord så vil en OG klausul indtræde.");
+define("_MYNGPOWERED_BY","Powered by ");
+define("_MYNGGO","Kør!");
+
+// Search Results
+define("_MYNGGROUP","Gruppe");
+define("_MYNGREADINGS","Læsninger");
+
+// Stats
+define("_MYNGHOT_NG","Særlig gruppe");
+define("_MYNGPOPULAR_NG","Mest populær gruppe");
+define("_MYNGCROWDED_NG","Mest aktive gruppe");
+define("_MYNGHOT_ARTICLES","Særlige artikler");
+define("_MYNGPOPULAR_ARTICLES","Mest populære artikler");
+define("_MYNGACTIVE_USER","Mest aktiv bruger");
+define("_MYNGARG_USER","Bruger med flest besvarelser");
+define("_MYNGSTATS_ARTICLES","artikler");
+define("_MYNGSTATS_READINGS","læsninger");
+define("_MYNGUSERS","brugere");
+define("_MYNGPOSTINGS","emner");
+define("_MYNGREPLIES","besvarelser");
+
+// My Groups
+define("_MYNGMUST_LOGIN","Du skal logge ind for at kunne bruge denne mulighed.");
+define("_MYNGSUBSCRIPTION_SYSTEM","Abonnementsystem");
+define("_MYNGCHOOSE_GROUP","Vælg en gruppe");
+define("_MYNGSURE","Jeg er sikker");
+define("_MYNGGROUPS_SUBSCRIBED","Oversigt over abonnerede grupper");
+define("_MYNGNEW","Ny");
+define("_MYNGUNREAD","Ulæst");
+define("_MYNGPOSTED","Sendt");
+define("_MYNGADD","Tilføj");
+define("_MYNGDEL","Slet");
+
+// My Articles
+define("_MYNGARTICLES_SAVED","Oversigt over gemte artikler");
+
+// Print article
+define("_MYNGPRINT_ARTICLE","Udskriv artikel");
+define("_MYNGUSER_AGENT","Bruger agent");
+define("_MYNGGENERATED_BY","Genereret af");
+define("_MYNGFROM","Fra");
+
+// Email article
+define("_MYNGEMAIL_SENT","Email sendt");
+define("_MYNGEMAIL_NOT_SENT","Der er opstået en fejl: Email blev IKKE sendt");
+define("_MYNGEMAIL_FRIEND","Email artikel til en ven");
+define("_MYNGSENDYOU_FROM"," sender denne artikel fra ");
+define("_MYNGDEAR_FRIEND","Kære (_friend_name)");
+// the string '_friend_name' it must be the same in '_MYNGDEAR_FRIEND' and in '_MYNGFRIEND_NAME'.
+define("_MYNGFRIEND_NAME","\(_friend_name\)");
+define("_MYNGYOUR_FRIEND","Din ven ");
+define("_MYNGWANT2SHARE",", har sendt \ndig denne artikel fra ");
+define("_MYNGNSERT_FRIEND_INFO","Indsæt din vens informationer");
+define("_MYNGMAIL_MUSTBE_REAL","Note: Email adressen skal være gyldig, den bliver valideret, <br>&nbsp; den spores eller gemmes ikke.");
+define("_MYNGNAME","Navn");
+define("_MYNGEMAIL_LOOK","Emailen vil se sådan ud");
+define("_MYNGEMAIL_IT","Send den!");
+
+// Post
+define("_MYNGMISSING_EMAIL","Du skal angive en email adresse.");
+define("_MYNGMISSING_MESSAGE","Du skal skrive en besked.");
+define("_MYNGWRONG_EMAIL","Email adressen er forkert");
+define("_MYNGMISSING_NAME","Du skal angive et navn eller alias.");
+define("_MYNG_MISSING_SUBJECT","Du skal angive et emne.");
+define("_MYNGMESSAGE_POSTED","Beskeden er sendt.");
+define("_MYNGERROR_SERVER","Serveren accepterede ikke beskeden.");
+define("_MYNGREAD_ONLY","Denne gruppe er skrivebeskyttet");
+define("_MYNGWROTE"," skrev:\n\n");
+define("_MYNGFOLLOWUP_NOT_ALLOWED","Du har ikke tilladelse til at skrive til grupperne: ");
+define("_MYNGPOST_FAILED","Forbindelse fejlede - prøv at OPDATER siden senere.");
+define("_MYNGFOOTER1","         Artikel skrevet fra MyNewsGroups :)");
+define("_MYNGFOOTER2","         http://mynewsgroups.sourceforge.net");
+define("_MYNGFOOTER3","         Share your knowledge in... ");
+define("_MYNGFILL_FORM2POST","Udfyld formularen for at skrive en ny aritkel");
+define("_MYNGEMAIL_MUSTBE_REAL","Note: Email adressen skal være gyldig - den bliver valideret");
+define("_MYNGPOST","Send");
+define("_MYNGPOSTING_COMPLETE","Afsendt");
+define("_MYNGTHANKX","Tak!");
+define("_MYNGPOSTED_SUCCESFULLY","Din artikel er sendt");
+define("_MYNGINFO_SUBMITTED","Dette er informationerne du sendte");
+define("_MYNGBODY","Besked");
+define("_MYNGCLICK2RETURN","Klik her for at vende tilbage til");
+define("_MYNGEMAIL_SPAM_PROTECTED","Din email adresse er blevet beskyttet mod SPAM-bots");
+define("_MYNGEMAIL_CHANGED","Din email adresse vil være synlig på denne måde");
+
+// Error template
+define("_MYNGSOMETH_WRONG","Det er opstået en fejl...");
+define("_MYNGERROR_MESSAGE","Fejl besked");
+define("_MYNGHAVE_DOUBT","Hvis du er i tvivl kontakt webmaster");
+define("_MYNGERROR","FEJL!");
+
+// Admin
+define("_MYNGADMIN_LOGIN","Du skal være logget på som Administrator.");
+define("_MYNGADMIN","Administration");
+define("_MYNGENTER_GROUP_NAME","Skriv gruppenavn");
+define("_MYNGADD_GROUP","Tilføj gruppe");
+define("_MYNGCHOOSE_SERVER","Vælg en server");
+define("_MYNGARTICLES2FETCH","Artikler som skal hentes");
+define("_MYNGDELETE_GROUP","Slet gruppe");
+define("_MYNGDELETE","Slet");
+define("_MYNGEDIT_GROUP","Redigér gruppe");
+define("_MYNGALLOW_POSTING","Tillad skrivning");
+define("_MYNGEDIT","Redigér");
+define("_MYNGADD_SERVER","Tilføj nyhedsserver");
+define("_MYNGHOST","Vært");
+define("_MYNGPORT","Port");
+define("_MYNGDELETE_SERVER","Slet nyhedsserver");
+define("_MYNGALL","Alle");
+
+
+// Errors
+define("_MYNGNOGROUPS","Der er ingen registerede grupper.");
+define("_MYNGCON_ERROR","Forbindelses fejl!");
+define("_MYNGUSERNAME_TAKEN","Desværre, brugernavnet er allerede taget af en anden bruger.");
+define("_MYNGEMAIL_TAKEN","Desværre, email adresse er allerede brugt af en anden bruger.");
+define("_MYNGLOGIN_INCORRECT","Desværre, login var forkert - prøv igen.");
+define("_MYNGINVALID_EMAIL","Desværre email adressen er ikke gyldig. Indsæt en gyldig email adresse.");
+define("_MYNGINVALID_PASSWORD","Desværre, passwords er ikke ens.");
+define("_MYNGBLANK_PASSWORD","Indsæt venligst et password.");
+define("_MYNGSERVER_REJECT","Mailserveren afviste adressen");
+define("_MYNGNO_RESPONSE","Ingen respons fra mailserver");
+define("_MYNGCANT_CONNECT_SERVER","Kan ikke etablere forbindelse med mail serveren");
+define("_MYNGAPPEARS_VALID"," synes at være ok");
+define("_MYNGNOT_PROPERLY_FORMATTED"," er ikke af korrekt format");
+define("_MYNGNO_GROUP","Gruppen findes ikke i ");
+define("_MYNGUNKNOWN_GROUP"," Denne gruppe tilhører ikke systemet.");
+
+
+// Greets
+define("_MYNGREGISTRATION_COMPLETED","Tillykke! Du er nu registreret.");
+define("_MYNGLOGGED","Du er nu logget på systemet!");
+define("_MYNGLOGGED_OUT","Du er nu logget ud.");
+
+
+
+
+
+/*
+
+Please!! Don't translate the following lines, they will be deprecated in future versions!
+Thank you very much for your support!
+
+$text_header["from"]="<b>From:</b> ";
+$text_header["subject"]="<b>Subject:</b> ";
+$text_header["newsgroups"]="<b>Newsgroups:</b> ";
+$text_header["followup"]="<b>Followup:</b> ";
+$text_header["organization"]="<b>Organization:</b> ";
+$text_header["date"]="<b>Date:</b> ";
+$text_header["message-id"]="<b>Message-ID:</b> ";
+$text_header["references"]="<b>References:</b> ";
+$text_header["user-agent"]="<b>User-Agent:</b> ";
+$text_header["date_format"]="M d Y H:i:s";
+
+$text_thread["no_articles"]="<p><b>No articles aviable</b></p>";
+$text_thread["button_write"]="Write";
+$text_thread["button_grouplist"]="Newsgroups";
+$text_thread["button_top"]="Back to Top";
+$text_thread["date"]="<b>Date</b>";
+$text_thread["subject"]="<b>Subject</b>";
+$text_thread["author"]="<b>Author</b>";
+$text_thread["no_such_group"]="No such Group.";
+
+$text_groups["newsgroup"]="<b>Newsgroup</b>";
+$text_groups["description"]="<b>Description</b>";
+
+$text_article["button_answer"]="Answer";
+
+$text_post["missing_message"]="You must include a message to post.";
+$text_post["missing_email"]="You must include an email address.";
+$text_post["missing_name"]="You must include a name or at least an alias.";
+$text_post["missing_subject"]="You must include a subject.";
+$text_post["button_post"]="Post";
+$text_post["remember"]="Remember name and email address";
+$text_post["message"]="Message";
+$text_post["group_head"]="Compose article for ";
+$text_post["group_tail"]="";
+$text_post["followup_not_allowed"]="You aren't allowed to post to the groups:";
+$text_post["message_posted"]="Message posted";
+$text_post["message_posted2"]="The message was successfully posted.";
+$text_post["button_back"]="Back";
+$text_post["button_back2"]="to";
+$text_post["error_newsserver"]="The newsserver does not accept the message.";
+$text_post["error_readonly"]="The group is write protected";
+$text_post["error_wrong_email"]="The email adress is wrong";
+
+$text_error["error:"]='<font color="red">Error:</font>';
+$text_error["connection_failed"]="The connection to the Newsserver failed";
+$text_error["article_not_found"]="<p>The article doesn't exist.</p>";
+$text_error["read_access_denied"]="<p>No permission to read!</p>";
+$text_error["post_failed"]="Connection failed. Try to reload later.";
+$text_error["auth_error"]="Authentification at the Newsserver failed.";
+$text_error["spool_error"]="Error in spoolfile. Please reload.";
+
+*/
+
+
+
+?>
