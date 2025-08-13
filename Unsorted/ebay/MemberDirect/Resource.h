835d8dd219c962336aa5e0a85a4613242212bd62
me-suzy <me.suzana@gmail.com>
2025-08-13 03:24:55 +0300

    Add Unsorted/ebay/MemberDirect/Resource.h

---
+++ Unsorted/ebay/MemberDirect/Resource.h
@@ -0,0 +1,12 @@
+/*	$Id: Resource.h,v 1.3 1999/02/21 02:23:31 josh Exp $	*/
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MemberDirect.rc
+//
+
+#define IDS_FILTER        101
+
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_CONTROL_VALUE         103
+#define _APS_NEXT_SYMED_VALUE           103
+#define _APS_NEXT_COMMAND_VALUE         32768
