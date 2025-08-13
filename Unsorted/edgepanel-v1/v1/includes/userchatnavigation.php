2eda54a26b350989c45f30f3ae1b8ca1df89a2ff
me-suzy <me.suzana@gmail.com>
2025-08-13 03:59:22 +0300

    Add Unsorted/edgepanel-v1/v1/includes/userchatnavigation.php

---
+++ Unsorted/edgepanel-v1/v1/includes/userchatnavigation.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>Chat Top</title>
+
+<style type="text/css">
+<!--
+td {  font-family: Verdana; font-size: 11px; color: #383838;line-height: 20px}
+a {  color: #4F769E; text-decoration: none}
+a:hover {  text-decoration: underline}
+.heading { color: #4F769E;font-weight: bold}
+INPUT {  font-family: Verdana; font-size: 11px; color: #383838;}
+SELECT {  font-family: Verdana; font-size: 11px; color: #383838;}
+TEXTAREA {  font-family: Verdana; font-size: 11px; color: #383838;}
+.highlight { color: #EE9700;}
+.unhighlight { }
+-->
+
+</style>
+
+<script language=Javascript>
+function closeWindow() {
+
+	parent.frames["mainFrame"].document.location.href = '../exitchat.php';
+	
+}
+</script>
+</head>
+<body topmargin=0 leftmargin=0 background=../i/chat-navbar-bg.jpg onUnload=closeWindow()>
+
+<br>
+<table width="90%" border="0" cellspacing="0" cellpadding="0">
+                      <tr>
+                        <td valign="top" height="27"><span class="heading">&nbsp;&nbsp;&nbsp;<a href="../exitchat.php">&raquo;
+                          Exit Chat</a></span></td>
+                      </tr>                    
+                    </table>
+</body>
+</html>
