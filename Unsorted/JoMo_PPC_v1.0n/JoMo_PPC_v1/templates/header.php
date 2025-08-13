5d99d45359399d74b0234fab47e1aa23ce1225d5
me-suzy <me.suzana@gmail.com>
2025-08-13 05:55:54 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/header.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+        <TITLE><%$pageTitle%></TITLE>
+        <META http-equiv="Pragma" content="no-cache">
+        <META http-equiv="Cache-Control" content="no-cache, must-revalidate">
+        <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+</HEAD>
+
+<BODY BGCOLOR="white" MARGINHEIGHT=0 MARGINWIDTH=0 TOPMARGIN=0 LEFTMARGIN=0>
+
+<table width="100%" align="center" >
+<tr>
+<td>
+
+<!--{top menu-->
+
+<table border="0" width="100%" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="220" height="75" valign="bottom"><a href="<%$selfURL%>?mode=home"><img border="0" src="../images/ppc_logo.gif" align="right"></a></td>
+    <td height="75" valign="bottom">
+      <p align="right"><img border="0" src="../images/ppc-special.gif"></td>
+  </tr>
+  <tr>
+    <td colspan="2">
+      <table border="1" cellpadding="0" cellspacing="0" width="100%" bordercolor="#000099">
+        <tr>
+          <td width="20%" bgcolor="#0066CC" bordercolor="#0066CC" align="center"><a href="<%$selfURL%>?mode=home"><font color="white">Home</font></a></td>
+          <td width="20%" bgcolor="#0066CC" bordercolor="#0066CC" align="center"><a href="<%$selfURL%>?mode=members"><font color="white">Member area</font></a></td>
+          <td width="20%" bgcolor="#0066CC" bordercolor="#0066CC" align="center"><a href="<%$selfURL%>?mode=affiliates"><font color="white">Affiliate area</font></a></td>
+          <td width="20%" bgcolor="#0066CC" bordercolor="#0066CC" align="center"><a href="admin.php"><font color="white">Admin Area</font></a></td>
+          <td width="20%" bgcolor="#0066CC" bordercolor="#0066CC" align="center"><a href="mailto:<%$adminEmail%>" ><font color="white">Contact Us</font></a></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+
+
+<!--}top menu-->
