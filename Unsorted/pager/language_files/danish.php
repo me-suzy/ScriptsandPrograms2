1a00fb06d604dc6e35f0fe3ac56e578e1d0b2f46
me-suzy <me.suzana@gmail.com>
2025-08-13 07:36:49 +0300

    Add Unsorted/pager/language_files/danish.php

---
+++ Unsorted/pager/language_files/danish.php
@@ -0,0 +1,26 @@
+<?
+$l_send =			"Beskeden skal sendes til flg. ICQ-nummer:";
+$l_emne =			"Emne:";
+$l_besked =			"Besked:";
+$l_ditnavn =			"Dit navn:";
+$l_dinepost =			"Din e-postadresse:";
+$l_sendbeskeden =		"Send beskeden";
+$l_lavkoden =			"Lav koden";
+$l_redigerformularen =		"Her kan du bestemme ICQ-formularens udseende. Hvis du ikke ved, hvad du skal gøre, så tryk bare på \"$l_lavkoden\".";
+$l_tabelbaggrund =		"Baggrundsfarve:";
+$l_ramme =			"Rammebredde:";
+$l_celleafstand =		"Celleafstand:";
+$l_cellevaeg =			"Cellevæg:";
+$l_skrifttype =			"Skrifttype:";
+$l_skriftstr =			"Skriftstørrelse:";
+$l_skriftfarve =		"Skriftfarve:";
+$l_gennemse =			"Sådan kommer formularen til at se ud:";
+$l_kopier =			"Kopier nedenstående kode og indsæt den i &eacute;t eller flere af dine HTML-dokumenter:";
+$l_icqikkeudfyldt =		"Du har glemt at skrive det ICQ-nummer, beskeden skal sendes til.";
+$l_ingenbesked =		"Du har glemt at skrive en besked.";
+$l_tilbage =			"Tilbage";
+$l_over450tegn =		"Beskeden er for lang. Den må højst være på 450 tegn.";
+$l_epostikkeudfyldt =		"Du har glemt at skrive din e-postadresse.";
+$l_beskedenafsendt =		"Beskeden er afsendt.";
+$l_hjaelp =			"Hjælp";
+?>
