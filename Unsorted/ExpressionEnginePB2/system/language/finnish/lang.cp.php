b57339b35749abb7dd1600492835e706b4086748
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:54 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.cp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.cp.php
@@ -0,0 +1,157 @@
+<?php
+$L = array(
+
+
+"consult_user_guide" =>
+"Lisää käyttöohjeessa",
+
+"powered_by" =>
+"Julkaisujärjestelmä",
+
+"control_panel" =>
+"Ohjauspaneeli",
+
+"main_menu" =>
+"Ohjauspaneelin etusivu",
+
+"username" =>
+"Käyttäjätunnus",
+
+"password" =>
+"Salasana",
+
+"publish" =>
+"Julkaise",
+
+"edit" =>
+"Muokkaa",
+
+"design" =>
+"Sivupohjat",
+
+"communicate" =>
+"Postitoimisto",
+
+"modules" =>
+"Modulit",
+
+"admin" =>
+"Järjestelmän_hallinta",
+
+"my_account" =>
+"Oma_tilisi",
+
+"user_account" =>
+"Jäsenten tilit",
+
+"user_guide" =>
+"Käyttöohjekirja",
+
+"logout" =>
+"Log-out",
+
+"copyright" =>
+"Copyright",
+
+"rights_reserved" =>
+"Kaikki oikeudet pidätetään",
+
+"page_rendered" =>
+"Scripti käänneettin %x sekunnissa",
+
+"queries_executed" =>
+"%x SQL kyselyä käytettiin",
+
+"build" =>
+"Build:",
+
+"forgot_password" =>
+"Unohtuiko salasana?",
+
+"e_no_css" =>
+"Haettua CSS tiedostoa ei voitu avata",
+
+"unauthorized" =>
+"Luvaton",
+
+"unauthorized_access" =>
+"Sinulla ei ole oikeutta päästä tälle sivulle",
+
+"be_careful" =>
+"OLE VAROVAINEN!",
+
+"action_can_not_be_undone" =>
+"TÄTÄ TOIMINTOA EI VOI PERUUTTAA",
+
+"back" =>
+"Takaisin ",
+
+"no" =>
+"Ei",
+
+"yes" =>
+"Kyllä",
+
+"all" =>
+"Kaikki",
+
+"any" =>
+"Mikä tahansa",
+
+"required_fields" =>
+"Osoittaa vaadittavat kentät",
+
+"please_consult_manual" =>
+"Tarkista käyttöohjekirjasta",
+
+"login" =>
+"Login",
+
+"forgotten_password" =>
+"Unohtuiko salasana",
+
+"submit_email_address" =>
+"Lähetä emailosoitteesi",
+
+"submit" =>
+"Lähetä",
+
+"preview" =>
+"Esikatsele",
+
+"update" =>
+"Päivitä",
+
+"delete" =>
+"Poista",
+
+"save_changes" =>
+"Tallenna muutokset",
+
+"clear_logs" =>
+"Tyhjennä lokitiedostot",
+
+"member_id" =>
+"Jäsenen ID",
+
+"ip_address" =>
+"IP osoite",
+
+"date" =>
+"Päiväys",
+
+"action" =>
+"Toiminto",
+
+"member_logged_in" =>
+"Sisäänkirjautunut",
+
+"member_logged_out" =>
+"Ulos kirjautunut",
+
+"cleared_logs" =>
+"Tyhjennetyt lokitiedostot",
+
+''=>''
+);
+?>
