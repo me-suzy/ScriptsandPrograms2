01212aba06f663f597cd698f6efa67de38662b7a
me-suzy <me.suzana@gmail.com>
2025-08-13 05:58:57 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/admin/template.admin.xml.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/admin/template.admin.xml.php
@@ -0,0 +1,26 @@
+<%include file="admin/admin.header.php"%>
+
+<BR>
+<%include file="admin/admin.menu.php"%>
+<BR>
+
+<TABLE ALIGN="CENTER" BORDER="0" CELLPADDING="5" CELLSPACING="1" WIDTH="" background="">
+<TR ALIGN="left" bgcolor="" >
+        <TD Align="center">
+                <b>XML format.</b>
+        </TD>
+</TR>
+</TABLE>
+
+<TABLE ALIGN="CENTER" BORDER="0" CELLPADDING="10" CELLSPACING="1" WIDTH="" background="">
+<TR ALIGN="left" bgcolor="" >
+        <TD Align="left">
+           
+        </TD>
+</TR>
+</TABLE>
+
+<%include file="xmlformat.php"%>
+
+
+<%include file="admin/admin.footer.php"%>
