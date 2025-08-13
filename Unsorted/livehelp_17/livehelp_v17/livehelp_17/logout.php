5156afe1b45a51221dff7b5e2f69dd32ec89a48a
me-suzy <me.suzana@gmail.com>
2025-08-13 06:09:45 +0300

    Add Unsorted/livehelp_17/livehelp_v17/livehelp_17/logout.php

---
+++ Unsorted/livehelp_17/livehelp_v17/livehelp_17/logout.php
@@ -0,0 +1,43 @@
+<?
+//****************************************************************************************/
+//  Crafty Syntax Live Help (CS Live Help)  by Eric Gerdes (http://craftysyntax.com )
+//======================================================================================
+// NOTICE: Do NOT remove the copyright and/or license information from this file. 
+//         doing so will automatically terminate your rights to use this program.
+// ------------------------------------------------------------------------------------
+// ORIGINAL CODE: 
+// ---------------------------------------------------------
+// CS LIVE HELP http://www.craftysyntax.com/livehelp/
+// Copyright (C) 2003  Eric Gerdes 
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program in a file named LICENSE.txt .
+// if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// ---------------------------------------------------------  
+// MODIFICATIONS: 
+// ---------------------------------------------------------  
+// [ Programmers who change this code should cause the  ]
+// [ modified changes here and the date of any change.  ]
+//======================================================================================
+//****************************************************************************************/
+
+include("config.php");
+include("user_access.php");
+
+$timeof = date("YmdHis");
+$timeof_old = $timeof - 100000;
+
+$username = $username;
+checkuser();
+logout();
+$mydatabase->close_connect();
+?>
