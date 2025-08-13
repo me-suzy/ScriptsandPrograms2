72812b321eaad78162fdea8bf5d2128ccf6684e0
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:57 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-text-view.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-text-view.php
@@ -0,0 +1,21 @@
+<?php
+
+##################################################
+
+# /text-view.php
+
+##################################################
+
+define( "TXTVIEW_NOID", "Si us plau, seleccioneu una pista vàlida." );
+
+define( "TXTVIEW_HEADER_COMMENTS", "COMENTARIS" );
+define( "TXTVIEW_HEADER_LYRICS", "LLETRES" );
+
+define( "TXTVIEW_PAGETOP", "CapÁalera" );
+define( "TXTVIEW_PAGETOP_HELP", "Torna a la capÁalera" );
+define( "TXTVIEW_CLOSEWIN", "Tanca la finestra" );
+define( "TXTVIEW_CLOSEWIN_HELP", "Tanca aquesta finestra" );
+
+##################################################
+
+?>
