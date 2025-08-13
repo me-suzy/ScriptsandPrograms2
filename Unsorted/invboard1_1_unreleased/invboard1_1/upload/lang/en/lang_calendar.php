c9d05ea9998848857b9d38a39e1eda0890b9aa12
me-suzy <me.suzana@gmail.com>
2025-08-13 05:37:21 +0300

    Add Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_calendar.php

---
+++ Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_calendar.php
@@ -0,0 +1,38 @@
+<?php
+
+$lang = array (
+
+'page_title'  => 'Calendar',
+'table_title' => 'Forums Event Calendar',
+
+'form_submit_show' => "Show",
+
+'entry_birthdays'  => "%s Birthday(s)",
+
+'post_new_event'   => "Post New Event",
+
+'new_event_redirect' => "your calendar event has been posted",
+
+'cal_title_events'  => "Calendar Events",
+
+'cal_birthdays'     => "Birthdays",
+
+'back'              => "Back",
+
+'private_event'     => "Private Event",
+'public_event'      => "Public Event",
+'restricted_event'  => "Restricted Event",
+
+'joined'  => "Joined:",
+'posts'   => "Posts:",
+'group'   => "Group:",
+
+'edit_event' => "Edit Event:",
+
+'edit_event_redirect' => "the calendar event has been edited",
+'delete_event_redirect' => "the calendar event has been <b>deleted</b>",
+
+'event_date'  => "Event Date:",
+
+);
+?>
