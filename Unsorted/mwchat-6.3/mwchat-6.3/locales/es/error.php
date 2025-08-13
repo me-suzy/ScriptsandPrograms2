51081ac761a6a765767e0b76c71911b79b59836d
me-suzy <me.suzana@gmail.com>
2025-08-13 06:30:57 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/es/error.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/es/error.php
@@ -0,0 +1,52 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[ERROR_License] = "Hay un error con su licencia. Por favor licencia MWChat ahora.";
+
+$LOCALE[ERROR_AlreadyIn] = "El nombre que usted escogió es ya en uso.";
+
+$LOCALE[ERROR_Title] = "El error";
+
+$LOCALE[ERROR_Logout] = "Usted ha sido apuntado fuera.";
+
+$LOCALE[ERROR_Expired] = "Su login ha expirado, por favor login otra vez.";
+
+$LOCALE[ERROR_RoomLong] = "Esta habitación denomina usted escoge está a largo.";
+
+$LOCALE[ERROR_Live] = "El sistema está actualmente indisponible.";
+
+$LOCALE[ERROR_Profanity] = "La información que usted especificó contiene lo profano.";
+
+$LOCALE[ERROR_NoData]   = "Llene por favor en total campos.";
+
+$LOCALE[ERROR_BadData]  = "Especifique por favor los datos válidos.";
+
+$LOCALE[ERROR_Serial_Invalid] = "Su número de serie es inválido, re-registra por favor su producto.";
+
+$LOCALE[ERROR_Serial_None] = "No puede encontrar un número de serie en el registro, re-registra por favor su producto.";
+
+$LOCALE[ERROR_Critical] = "¡El error crítico!";
+
+$LOCALE[ERROR_Javascript] = "Este programa requiere un JavaScript examinador capaz.";
+
+$LOCALE[ERROR_Browser] = "Este programa no funcionará correctamente con su examinador.";
+
+$LOCALE[ERROR_Restricted] = "El nombre que usted escogió es restringido.";
+
+$LOCALE[ERROR_BadPass]  = "Entre por favor una seña válida.";
+
+$LOCALE[ERROR_IP] = "El IP dirige su es usar ha cambiado. Usted ha sido apuntado fuera para razones de seguridad.";
+
+$LOCALE[ERROR_PHPV] = "MWChat requiere una versión mínima de PHP de";
+
+?>
