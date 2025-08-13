1f0bb0c6584b9535583a46f0dd5e8afe80a6668f
me-suzy <me.suzana@gmail.com>
2025-08-13 03:27:27 +0300

    Add Unsorted/ebay/RedirectCompleted/Stdafx.cpp

---
+++ Unsorted/ebay/RedirectCompleted/Stdafx.cpp
@@ -0,0 +1,7 @@
+/*	$Id: Stdafx.cpp,v 1.3 1999/02/21 02:24:22 josh Exp $	*/
+// stdafx.cpp : source file that includes just the standard includes
+//	RedirectCompleted.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
