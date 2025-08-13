f716b4ea5a50920e5f9ad27481b8ae183748cfb3
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:40 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.templates.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.templates.php
@@ -0,0 +1,330 @@
+<?php
+$L = array(
+
+"php_parse_location" =>
+"Stage at which PHP should be parsed",
+
+"input" =>
+"Input",
+
+"output" =>
+"Output",
+
+"template_loop" =>
+"U heeft een Sjabloon-loop veroorzaakt wat een gevolg is van te veel sub-sjablonen die naar elkaar verwijzen",
+
+"template_intro" =>
+"Op deze pagina kunt u de sjablonen beheren",
+
+"create_new_template_group" =>
+"Maak een nieuwe sjabloon groep",
+
+"new_template_group_form" =>
+"Nieuwe Sjabloon Groep",
+
+"edit_template_group_form" =>
+"Wijzig Sjabloon Groep",
+
+"name_of_template_group" =>
+"Sjablonen Groep Naam",
+
+"template_group_instructions" =>
+"De naam moet bestaan uit een enkel woord zonder spaties",
+
+"undersores_allowed" =>
+"(underscores en platte streepjes zijn toegestaan)",
+
+"new_template_form" =>
+"Nieuw Sjaboon Formulier",
+
+"edit_template_form" =>
+"Wijzig Sjaboon Formulier",
+
+"name_of_template" =>
+"Sjaboon Naam",
+
+"group_name" =>
+"Groep Naam",
+
+"order" =>
+"Sorteer/Volgorde",
+
+"reserved_name" =>
+"De naam die u gekozen heeft is een reeds gereserveerd woord wat niet gebruikt kan worden",
+
+"type_of_template" =>
+"Type Sjabloon",
+
+"webpage" =>
+"Website",
+
+"css_stylesheet" =>
+"CSS Stylesheet",
+
+"rss" =>
+"RSS Pagina",
+
+"missing_name" =>
+"Uw sjabloon moet een naam hebben",
+
+"template_name_taken" =>
+"De naam van uw sjabloon bestaat al",
+
+"template_group_del_conf" =>
+"Bevestig verwijderen van sjabloon groep",
+
+"template_del_conf" =>
+"Bevestig verwijderen van sjabloon",
+
+"edit_template" =>
+"Wijzig Sjabloon",
+
+"add_a_template" =>
+"Voeg een nieuw sjabloon toe",
+
+"delete_template_group" =>
+"Verwijder Groep",
+
+"edit_template_group" =>
+"Wijzig Groep",
+
+"group_order" =>
+"Volgorde van de groep",
+
+"export_templates" =>
+"Exporteer Sjablonen",
+
+"template_name" =>
+"Sjabloon naam:",
+
+"template_management" =>
+"Sjabloon Beheer",
+
+"current_template" =>
+"Huidig Sjabloon:",
+
+"preferences" =>
+"Voorkeuren",
+
+"prefs" =>
+"Voorkeuren",
+
+"hits" =>
+"Hits",
+
+"hit_counter" =>
+"Hits teller",
+
+"template_updated" =>
+"Sjabloon Geupdatet",
+
+"template_created" =>
+"Sjabloon Aangemaakt",
+
+"form_is_empty" =>
+"U heeft een leeg formulier ingevoerd",
+
+"all_fields_required" =>
+"U moet alle velden invoeren",
+
+"illegal_characters" =>
+"De naam die u ingevoerd heeft mag alleen uit alfa-nummerieke karakters, underscores en platte streepjes bestaan",
+
+"edit_template_group_order" =>
+"Wijzig Groeps Volgorde",
+
+"template_group_updated" =>
+"Sjabloon Groep Geupdatet",
+
+"template_group_created" =>
+"Sjabloon Groep Aangemaakt",
+
+"delete_this_group" =>
+"Verwijder de volgende Sjabloon groep",
+
+"all_templates_will_be_nuked" =>
+"ALLE SJABLONEN IN DEZE GROEP ZULLEN PERMANENT VERWIJDERD WORDEN!",
+
+"template_group_deleted" =>
+"Sjabloon groep verwijderd",
+
+"edit_group_order" =>
+"Wijzig groeps volgorde",
+
+"template_size" =>
+"Sjabloon grootte",
+
+"small" =>
+"Klein",
+
+"medium" =>
+"Middel",
+
+"large" =>
+"Groot",
+
+"view_rendered_template" =>
+"Bekijk gemaakte Sjabloon",
+
+"save_history" =>
+"Sla &#39;Herzien-bestand&#39; op",
+
+"view" =>
+"Bekijk",
+
+"clear" =>
+"Verwijder",
+
+"select_all" =>
+"Selecteer Alles",
+
+"export" =>
+"Exporteer",
+
+"export_as" =>
+"Exporteer bestanden als",
+
+"export_will_be_zip" =>
+"Let Op: Uw geëxporteerde data zal verstuurd worden als Zip-bestand",
+
+"choose_templates" =>
+"Kies de sjablonen die u als bestand wilt exporteren",
+
+"unable_to_write_files" =>
+"Niet mogelijk om de bestanden in uw cache map te plaatsen",
+
+"revision_history" =>
+"Bekijk &quot;Herzien&quot; geschiedenis",
+
+"clear_revision_history" =>
+"Verwijder &quot;Herzien&quot; geschiedenis",
+
+"history_cleared" =>
+"Sjabloon &quot;Herzien&quot; geschiedenis is verwijderd",
+
+"close_window" =>
+"Sluit Venster",
+
+"template_preferences" =>
+"Sjabloon Voorkeuren",
+
+"delete_template" =>
+"Verwijder Sjabloon",
+
+"delete_this_template" =>
+"Verwijder het volgende Sjabloon:",
+
+"template_deleted" =>
+"Sjabloon Verwijderd",
+
+"default_site_page" =>
+"Geeft de standaar hoofdpagina aan.",
+
+"is_site_default" =>
+"Maak van het index sjabloon in deze groep de homepage van je site?",
+
+"allow_php" =>
+"PHP in deze template toestaan?",
+
+"enable_cache" =>
+"Inschakelen van Sjabloon Caching?",
+
+"cache_interval" =>
+"Tijdsinterval tussen cache refresh",
+
+"in_minutes" =>
+"Tijd in minuten",
+
+"php_security_warning" =>
+"Door dit op &quot;ja&quot; te zetten heeft dit gevolgen voor de veiligheid",
+
+"consult_manual" =>
+"Bekijk de handleiding",
+
+"preferences_updated" =>
+"Voorkeuren Geupdatet",
+
+"choose_default_data" =>
+"Standaard Sjabloon data",
+
+"blank_template" =>
+"Geen - maak een leeg sjabloon aan",
+
+"template_from_library" =>
+"Gebruik een sjabloon uit uw bibliotheek",
+
+"an_existing_template" =>
+"Dupliceer een bestaand sjabloon",
+
+"you_must_submit_a_name" =>
+"Uw sjabloon moet een naam bevatten",
+
+"member_group" =>
+"Leden Groep",
+
+"can_view_template" =>
+"Kan deze pagina bekijken",
+
+"require_logged_in" =>
+"Inloggen verplicht om deze pagina te bekijken?",
+
+"restrict_to_group" =>
+"Beperk het kijken alleen tot bepaalde ledengroepen",
+
+"logged_out_select_blurb" =>
+"Als je &quot;ja&quot; heeft geselecteerd, en een niet ingelogde gebruiker deze pagina probeert  te bezoeken laat dan deze pagina zien:",
+
+"no_access_select_blurb" =>
+"Als u hierboven &quot;nee&quot; heeft geselecteerd: ",
+
+"no_access_instructions" =>
+"Wanneer ongeauthoriseerde gebruikers deze pagina proberen te bezoeken laat dan deze pagina zien:",
+
+"template_notes" =>
+"Sjabloon aantekeningen",
+
+"template_notes_desc" =>
+"Gebruik dit veld om aantekeningen en informatie over dit Sjabloon op te slaan",
+
+"global_variables" =>
+"Globale Variabelen",
+
+"global_variables_explanation" =>
+"U kunt uw eigen variabelen creeëren en deze toevoegen aan uw sjablonen",
+
+"no_global_variables" =>
+"Er zijn op dit moment geen globale variabelen",
+
+"global_variable_name" =>
+"Naam Variabele",
+
+"create_new_global_variable" =>
+"Maak een Nieuwe Globale Variabele aan",
+
+"variable_name" =>
+"Naam Variabele",
+
+"global_variable_syntax" =>
+"Achtervoegsel van Variabele",
+
+"variable_data" =>
+"Inhoud Variabele",
+
+"global_var_created" =>
+"Globale Variabele Aangemaakt",
+
+"global_var_updated" =>
+"Globale Variabele Geupdatet",
+
+"delete_global_variable" =>
+"Verwijder Globale Variabele",
+
+"delete_this_variable" =>
+"Wilt u de volgende Globale Variabele verwijderen?",
+
+"variable_deleted" =>
+"Variabele Verwijderd",
+
+''=>''
+);
+?>
