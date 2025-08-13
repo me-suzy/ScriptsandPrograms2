3e2187a2d458729f08c1711b7a44979ab72c7d09
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:22 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.pm_import.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.pm_import.php
@@ -0,0 +1,329 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// pM Imoprt
+//----------------------------
+
+"pmachine_import_utitity" =>
+"pMachine-Import-Tool",
+
+"pmachine_import_welcome" =>
+"Dieses Tool hilft Ihnen, pMachine-Eintr&auml;ge in ExpressionEngine zu importieren.",
+
+"pmachine_import_disclaimer" =>
+"Wenn Sie pMachine nicht benutzen, brauchen Sie dieses Tool nicht.",
+
+"pmachine_import_information" =>
+"Import-Anleitung",
+
+"pmachine_import_removal" =>
+"Hinweis: Benutzen Sie dieses Tool nicht, wenn Sie nicht pMachine-Nutzer sind.",
+
+"configuration_blurb" =>
+"Um dieses Tool zu nutzen, m&uuml;ssen Sie zun&auml;chst die Zugangsdaten zu Ihrer pMachine-Datenbank eingeben.",
+
+"configuration_blurb_cont" =>
+"Sie finden diese Information in Ihrer pMachine-config.php-Datei.",
+
+"database_info" =>
+"Datenbank-Info",
+
+"sql_server" =>
+"MySQL-Server-Adresse",
+
+"sql_username" =>
+"MySQL-Username",
+
+"sql_password" =>
+"MySQL-Passwort",
+
+"sql_database" =>
+"MySQL-Datenbank",
+
+"sql_prefix" =>
+"Tabellen-Prefix",
+
+"leave_prefix_blank" =>
+"Leer lassen, au&szlig;er wenn Sie einen speziellen Prefix nutzen wollen.",
+
+"configure" =>
+"Konfigurieren",
+
+"empty_field_warning" =>
+"Sie haben nicht alle Felder ausgef&uuml;llt.",
+
+"no_database_connection" =>
+"Ich kann mit den angegebenen Daten keine Datenbankverbindung aufbauen.",
+
+"table_name" =>
+"Tabellen-Name",
+
+"table_rows" =>
+"Tabellenzeilen",
+
+"table_action" =>
+"Aktion",
+
+"table_status" =>
+"Status",
+
+"completed" =>
+"Fertig",
+
+"no_rows_exist" =>
+"Kein Import n&ouml;tig",
+
+"pending" =>
+"Bereit zum Import",
+
+"pending_stats" =>
+"Bereit zur Verarbeitung",
+
+"import_now" =>
+"Importiere Daten",
+
+"recalculate" =>
+"Verarbeite Daten",
+
+"step_one" =>
+"Schritt eins",
+
+"step_two" =>
+"Schritt zwei",
+
+"step_three" =>
+"Schritt drei",
+
+"step_four" =>
+"Schritt vier",
+
+"step_five" =>
+"Schritt f&uuml;nf",
+
+"import_options" =>
+"Importiere Optionen",
+
+"import_members" =>
+"Importiere Mitglieder",
+
+"import_weblog_entries" =>
+"Importiere Weblogeintr&auml;ge",
+
+"recalculate_statistics" =>
+"Statistiken neu berechnen",
+
+"click_to_reset_statistics" =>
+"Hier klicken, um die Statistiken zu aktualisieren",
+
+"reset_statistics_info" =>
+"Sie werden zum Z&auml;hl-Tool weitergeleitet. Wenn Sie fertig sind, kommen Sie zur&uuml;ck zu dieser Seite, zu Schritt f&uuml;nf.",
+
+"members" =>
+"Mitglieder",
+
+"member_stats" =>
+"Mitgliederstatistiken",
+
+"weblog_stats" =>
+"Weblogstatistiken",
+
+"configure" =>
+"Konfigurieren",
+
+"total_members" =>
+"Anzahl der Mitglieder in Ihrer pMachine-Datenbank:",
+
+"select_your_account" =>
+"W&auml;hlen Sie Ihren pers&ouml;nlichen pMachine-Account aus.",
+
+"ignore_instructions" =>
+"Weil Sie schon einen ExpressionEngine-Account haben, hat es keinen Sinn, noch einen Account mit den Informationen aus Ihrem pMachine-Account zu erstellen. Wenn Sie Ihren existierenden pMachine-Account ausw&auml;hlen, werden Ihre pMachine-Eintr&auml;ge Ihrem ExpressionEngine-Account zugeschlagen.",
+
+"i_have_no_account" =>
+"Ich habe keinen pMachine-Account.",
+
+"members_per_cycle" =>
+"Mitgliederanzahl pro Runde",
+
+"cycle_instructions" =>
+"Um Time-Out-Probleme zu vermeiden, werden die pMachine-Daten in Paketen importiert. Bitte geben Sie an, wieviele Datens&auml;tze pro Runde importiert werden sollen. F&uuml;r die meisten Server ist 1000 ein guter Wert. Wenn Sie einen Hochleistungsserver haben, k&ouml;nnen Sie mehrere tausend Datens&auml;tze auf einmal importieren.",
+
+"blog_cycle_instructions" =>
+"Die Importroutine verlangt eine Menge Rechnerleistung, ganz besonders, wenn viele Kommentare oder Trackbacks mit importiert werden. Um Time-Out-Probleme zu vermeiden, werden die pMachine-Eintr&auml;ge in Paketen importiert. Bitte geben Sie an, wieviele Datens&auml;tze pro Runde importiert werden sollen. F&uuml;r die meisten Server ist 100 ein guter Wert. Wenn Sie einen Hochleistungsserver haben, oder wenn Ihre Eintr&auml;ge wenige Kommentare haben, k&ouml;nnen Sie auch mehr Eintr&auml;ge auf einmal importieren.",
+
+"save_settings" =>
+"Einstellungen speichern",
+
+"member_import_complete" =>
+"Der Mitglieder-Import ist abgeschlossen.",
+
+"return_to_overview" =>
+"Zur&uuml;ck zur Haupt-Importseite",
+
+"start_member_import" =>
+"Fertig zum Import",
+
+"return_to_overview" =>
+"Beginne Mitglieder-Import",
+
+"members_remaining" =>
+"Anzahl der Mitglieder, die auf den Import warten:",
+
+"click_to_import_members" =>
+"Importiere Mitglieder %x bis %y",
+
+"no_table_rows" =>
+"Keine Zeilen gefunden",
+
+"default_member_group" =>
+"Mitglieder-Gruppenzuweisung",
+
+"member_group_instructions" =>
+"Die importierten Mitglieder m&uuml;ssen einer Gruppe zugewiesen werden. W&auml;hlen Sie eine Gruppe aus, der die importierten Mitglieder zugewiesen werden sollen.",
+
+"members_imported" =>
+"Alle Mitglieder wurden importiert.",
+
+"return_to_main_menu" =>
+"Zur&uuml;ck zum Hauptmenu",
+
+"import_weblog_entries" =>
+"Importiere Weblogeintr&auml;ge",
+
+"weblog_name" =>
+"Weblogname:",
+
+"total_weblog_entries" =>
+"Anzahl der Eintr&auml;ge in diesem Weblog:",
+
+"select_destination_blog" =>
+"W&auml;hlen Sie das Ziel-Weblog aus",
+
+"destination_instructions" =>
+"Bitte w&auml;hlen Sie das ExpressionEngine-Weblog aus, in das die pMachine-Eintr&auml;ge importiert werden sollen.",
+
+"destination_note" =>
+"Hinweis: Wenn das Zielweblog noch nicht existiert, m&uuml;ssen Sie es auf der Admin-Seite zuerst erstellen.",
+
+"entries_per_cycle" =>
+"Zahl der Eintr&auml;ge pro Runde",
+
+"select_destination_fields" =>
+"W&auml;hlen Sie die Zielfelder aus",
+
+"field_destination_instructions" =>
+"Bitte w&auml;hlen sie alle ExpressionEngine-Felder aus, in die pMachine-Eintr&auml;ge importiert werden sollen.",
+
+"field_destination_instructions_two" =>
+"W&auml;hlen Sie &quot;Nicht Importieren&quot;, um ein Feld vom Import auszuschlie&szlig;en.",
+
+"fields_not_unique_warning" =>
+"Sie k&ouml;nnen nicht mehr als ein pMachine-Feld pro ExpressionEngine-Feld ausw&auml;hlen.",
+
+"no_fields_assigned" =>
+"Das gew&auml;hlte Weblog hat keine Feldergruppe. Bitte gehen Sie zu der Admin-Seite und weisen Sie dem Weblog eine Feldergruppe zu.",
+
+"no_textarea_fields" =>
+"Die Feldergruppe, die Sie dem Weblog zugewiesen haben, enth&auml;lt keine Textfelder.",
+
+"pmachine_field" =>
+"pMachine-Feld",
+
+"ee_field" =>
+"ExpressionEngine-Feld",
+
+"none" =>
+"Nicht importieren",
+
+"blurb" =>
+"Exzerpt",
+
+"body" =>
+"Body",
+
+"more" =>
+"Mehr",
+
+"custom1" =>
+"Nutzerdefiniert 1",
+
+"custom2" =>
+"Nutzerdefiniert 2",
+
+"custom3" =>
+"Nutzerdefiniert 3",
+
+"you_must_select_fields" =>
+"Sie haben keine Felder zugewiesen.",
+
+"entries_remaining" =>
+"Wartende Eintr&auml;ge:",
+
+"entries_imported" =>
+"Weblogeintr&auml;ge wurden importiert",
+
+"click_to_import_entries" =>
+"Importiere Weblogeintr&auml;ge %x bis %y",
+
+"batch_complete" =>
+"Runde beendet...",
+
+"select_upload_blog" =>
+"W&auml;hlen Sie das Grafik-Zielverzeichnis aus",
+
+"upload_instructions" =>
+"Wenn Ihre pMachine-Weblogeintr&auml;ge Grafiken enthalten, m&uuml;ssen wir das derzeitige Verzeichnis auf das neue Verzeichnis mappen.",
+
+"upload_note" =>
+"Hinweis: Ihre Grafiken werden nicht verschoben. Sie m&uuml;ssen Sie manuell in das gew&auml;hlte Verzeichnis verschieben.",
+
+"note_regarding_categories" =>
+"&Uuml;ber Weblogkategorien",
+
+"category_note" =>
+"Wenn Ihre pMachine-Eintr&auml;ge in Kategorien (Rubriken) aufgeteilt sind, m&uuml;ssen die selben Kategorien in ExpressionEngine bereit stehen. W&auml;hrend des Imports wird dieses Tool versuchen, pMachine-Kategorien mit ExpressionEngine-Kategorien zu verbinden. Wenn keine Verbindung m&ouml;glich ist, wird die Kategorie f&uuml;r diesen Eintrag ignoriert.",
+
+"import_mailinglist" =>
+"Importiere Mailing-Liste",
+
+"total_emails" =>
+"Anzahl von E-Mailadreesen:",
+
+"emails_remaining" =>
+"Anzahl wartender E-Mailadressen:",
+
+"emails_per_cycle" =>
+"Anzahl von E-Mailadressen pro Runde",
+
+"email_instructions" =>
+"Um Time-Out-Probleme zu vermeiden, werden die E-Mailadressen in Paketen importiert. Das fordert nicht allzu viel Prozessorleistung; ein durchschnittlicher Server schafft 5000 Adressen auf einmal. Bitte geben Sie die Anzahl von Adressen an, die Sie auf einmal importieren wollen. ",
+
+"mailinglist" =>
+"Mailing-Liste",
+
+"mailinglist_imported" =>
+"Die Mailing-Liste wurde importiert.",
+
+"click_to_import_mailinglist" =>
+"Importiere E-Mails %x bis %y",
+
+
+"clear_preferences" =>
+"Einstellungen leeren",
+
+"click_to_clear_prefs" =>
+"Klicken Sie hier, um die Einstellungen zu leeren.",
+
+"clear_preferences_info" =>
+"W&auml;hrend des Importprozesses habe ich alle Einstellungen in der Haupt-Konfigurationsdatei gespeichert. Weil der Prozess jetzt abgeschlossen ist, empfehle ich, diese Daten jetzt zu l&ouml;schen.",
+
+"you_are_done_importing" =>
+"Sie haben den Import erfolgreich abgeschlossen!",
+
+
+// END
+''=>''
+);
+?>
