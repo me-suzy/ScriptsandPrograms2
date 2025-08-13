b5da57070b262d79979db7a9d026da89f6fc60f8
me-suzy <me.suzana@gmail.com>
2025-08-13 03:00:58 +0300

    Add Unsorted/ebay/bugslayer/Internal.h

---
+++ Unsorted/ebay/bugslayer/Internal.h
@@ -0,0 +1,31 @@
+//	$Id: Internal.h,v 1.2 1999/03/22 00:09:29 josh Exp $
+/*----------------------------------------------------------------------
+       John Robbins - Microsoft Systems Journal Bugslayer Column
+
+The internal header for the BugslayerUtil.DLL code.
+----------------------------------------------------------------------*/
+
+#ifndef _INTERNAL_H
+#define _INTERNAL_H
+
+// The NT4 specific version of GetLoadedModules.
+BOOL NT4GetLoadedModules ( DWORD     dwPID        ,
+                           UINT      uiCount      ,
+                           HMODULE * paModArray   ,
+                           LPUINT    puiRealCount   ) ;
+
+// The TOOLHELP32 specific version of GetLoadedModules.
+BOOL TLHELPGetLoadedModules ( DWORD     dwPID        ,
+                              UINT      uiCount      ,
+                              HMODULE * paModArray   ,
+                              LPUINT    puiRealCount   ) ;
+
+// The NT version of GetModuleBaseName.
+DWORD __stdcall NTGetModuleBaseName ( HANDLE  hProcess   ,
+                                      HMODULE hModule    ,
+                                      LPTSTR  lpBaseName ,
+                                      DWORD   nSize       ) ;
+
+#endif  // _INTERNAL_H
+
+
