3daca8a69aeb39c35e37d142cc3f579bdb4eb9c4
me-suzy <me.suzana@gmail.com>
2025-08-13 04:24:01 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.modules.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.modules.php
@@ -0,0 +1,49 @@
+<?php
+$L = array(
+
+
+"module_name" =>
+"Nombre del módulo",
+
+"module_access" =>
+"Modificar módulo",
+
+"module_backend" =>
+"CP Usuario",
+
+"module_version" =>
+"Versión",
+
+"module_status" =>
+"Estado",
+
+"module_description" =>
+"Descripción",
+
+"module_action" =>
+"Acción",
+
+"not_installed" =>
+"No instalado",
+
+"installed" =>
+"Instalado",
+
+"install" =>
+"Instalar",
+
+"deinstall" =>
+"Eliminar",
+
+"module_can_not_be_found" =>
+"No se localizan los ficheros precisos para instalar éste módulo",
+
+"module_has_been_installed" =>
+"Módulo instalado:",
+
+"module_has_been_removed" =>
+"Módulo eliminado:",
+
+''=>''
+);
+?>
