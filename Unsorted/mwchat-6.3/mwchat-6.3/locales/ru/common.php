8abf4c51a32da4638173653b156464cd458e9f7a
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:39 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/common.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/common.php
@@ -0,0 +1,78 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[COMMON_Registered_Only] = "ÔÏÌØËÏ ÄÌÑ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎÎÙÈ ÐÏÌØÚÏ×ÁÔÅÌÅÊ.";
+
+$LOCALE[COMMON_Unregistered_Only] = "ÔÏÌØËÏ ÄÌÑ ÎÅÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎÎÙÈ ÐÏÌØÚÏ×ÁÔÅÌÅÊ.";
+	
+$LOCALE[COMMON_Username] = "éÍÑ ÐÏÌØÚÏ×ÁÔÅÌÑ";
+
+$LOCALE[COMMON_Function_Invalid] = "ÎÅ×ÅÒÎÁÑ ÉÌÉ ÎÅÄÏÓÔÕÐÎÁÑ ÄÌÑ ×ÁÓ ËÏÍÁÎÄÁ.";
+
+$LOCALE[COMMON_Operator] = "ÓÔÁÌ ÏÐÅÒÁÔÏÒÏÍ.";
+
+$LOCALE[COMMON_No_Operator] = "ÂÏÌØÛÅ ÎÅ ÏÐÅÒÁÔÏÒ.";
+
+$LOCALE[COMMON_Login_Button] = "÷ÏÊÔÉ ÓÅÊÞÁÓ";
+
+$LOCALE[COMMON_Not_Accept] = "ÎÅ ÐÒÉÎÉÍÁÅÔ ÞÁÓÔÎÙÅ ÓÏÏÂÝÅÎÉÑ.";
+
+$LOCALE[COMMON_Help] = "ðÏÍÏÝØ";
+
+$LOCALE[COMMON_No_Encryption] = "MWChat ÏÂÎÁÒÕÖÉÌ, ÞÔÏ Õ ×ÁÓ îåô mcrypt, ÕÓÔÁÎÏ×ÌÅÎÎÏÇÏ × php, ÐÒÏÉÚ×ÅÄÅÎÏ ÏÔËÌÀÞÅÎÉÅ ÛÉÆÒÏ×ÁÎÉÑ.";
+
+$LOCALE[COMMON_System_Message] = "óÉÓÔÅÍÎÏÅ ÓÏÏÂÝÅÎÉÅ";
+
+$LOCALE[COMMON_Click_Close] = "îÁÖÍÉÔÅ, ÞÔÏÂÙ ÚÁËÒÙÔØ";
+
+$LOCALE[COMMON_YOB] = "çÏÄ ÒÏÖÄÅÎÉÑ";
+
+$LOCALE[COMMON_Location] = "íÅÓÔÏ";
+
+$LOCALE[COMMON_Job] = "òÏÄ ÚÁÎÑÔÉÊ";
+
+$LOCALE[COMMON_Hobbies] = "õ×ÌÅÞÅÎÉÑ";
+
+$LOCALE[COMMON_Status] = "óÔÁÔÕÓ";
+
+$LOCALE[COMMON_Comments] = "äÏÐÏÌÎÉÔÅÌØÎÙÅ ËÏÍÍÅÎÔÁÒÉÉ";
+
+$LOCALE[COMMON_Password] = "îÏ×ÙÊ ÐÁÒÏÌØ";
+
+$LOCALE[COMMON_Confirm_Password] = "ðÏÄÔ×ÅÒÄÉÔÅ ÎÏ×ÙÊ ÐÁÒÏÌØ";
+
+$LOCALE[COMMON_Logout] = "÷ÙÊÔÉ ÉÚ MWChat";
+
+$LOCALE[COMMON_Err_Pass] = "ðÁÒÏÌÉ, ËÏÔÏÒÙÅ ×Ù ××ÅÌÉ, ÎÅ ÓÏ×ÐÁÄÁÀÔ ÉÌÉ ËÏÒÏÞÅ ÐÑÔÉ ÚÎÁËÏ×.";
+
+$LOCALE[COMMON_HelpClick] = "îÁÖÍÉÔÅ ÄÌÑ ÐÏÌÕÞÅÎÉÑ ÐÏÍÏÝÉ";
+
+$LOCALE[COMMON_Text] = "óÏÏÂÝÅÎÉÅ";
+
+$LOCALE[COMMON_Bug_Report] = "ïÐÉÓÁÎÉÅ ÏÛÉÂËÉ";
+
+$LOCALE[COMMON_Reason] = "ðÒÉÞÉÎÁ";
+
+$LOCALE[COMMON_Action] = "äÅÊÓÔ×ÉÅ";
+
+$LOCALE[COMMON_Topic] = "ôÅÍÁ";
+
+$LOCALE[COMMON_URL] = "URL";
+
+$LOCALE[COMMON_Alias] = "Ñ×ÌÑÅÔÓÑ ÁÌÉÁÓÏÍ ËÏÍÁÎÄÙ";
+
+$LOCALE[COMMON_Room_Name] = "ëÏÍÎÁÔÁ";
+
+$LOCALE[COMMON_Topic] = "îÅÔ";
+
+?>
