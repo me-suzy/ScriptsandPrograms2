c7875f70a61d062c837ca1208df10ac7a56946c8
me-suzy <me.suzana@gmail.com>
2025-08-13 05:58:11 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/admin/admin.header.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/admin/admin.header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+        <TITLE><%$pageTitle%></TITLE>
+        <META http-equiv="Pragma" content="no-cache">
+        <META http-equiv="Cache-Control" content="no-cache, must-revalidate">
+        <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+</HEAD>
+
+<BODY BGCOLOR="white" MARGINHEIGHT=0 MARGINWIDTH=0 TOPMARGIN=0 LEFTMARGIN=0>
+
+<table width="100%" align="center" >
+<tr>
+<td>
+
+<!--{top menu-->
+
+<table border="0" width="100%" cellpadding="0" cellspacing="0">
+  <tr>
+    <td width="220" height="75" valign="bottom"><a href="../index.php?mode=home"><img border="0" src="../images/ppc_logo.gif" align="right"></a></td>
+    <td height="75" valign="bottom">
+      <p align="right"><img border="0" src="../images/ppc-special.gif"></td>
+  </tr>
+  <tr>
+    <td colspan="2">
+<hr size=1>
+    </td>
+  </tr>
+</table>
+<TABLE width="100%" align="center" border="0" bgcolor="#ffffff">
+<TR>
+<TD>
+
+
