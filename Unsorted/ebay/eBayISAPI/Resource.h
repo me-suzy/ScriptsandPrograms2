f51295be8292a03fff912e2d248a504e2efd987c
me-suzy <me.suzana@gmail.com>
2025-08-13 03:13:28 +0300

    Add Unsorted/ebay/eBayISAPI/Resource.h

---
+++ Unsorted/ebay/eBayISAPI/Resource.h
@@ -0,0 +1,17 @@
+/*	$Id: Resource.h,v 1.2 1998/06/23 04:22:34 josh Exp $	*/
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by eBayISAPI.rc
+//
+#define IDS_SERVER                      102
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         103
+#define _APS_NEXT_SYMED_VALUE           103
+#endif
+#endif
