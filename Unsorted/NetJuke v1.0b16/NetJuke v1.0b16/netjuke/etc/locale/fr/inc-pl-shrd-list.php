dfb724d0b103dd045b8cf6b470080bc132b68e13
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:44 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-pl-shrd-list.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-pl-shrd-list.php
@@ -0,0 +1,28 @@
+<?php
+
+##################################################
+
+# /pl-shrd-list.php
+
+##################################################
+
+define( "PLSHRD_SHRD_HEADER", "LISTE COMPLÈTE DE LISTES PARTAGÉES" );
+define( "PLSHRD_SHRD_PLAY_HELP", "Cliquez pour débuter la lecture de cette liste d'écoute." );
+define( "PLSHRD_SHRD_VIEW_HELP", "Cliquez pour accéder aux détails de cette liste d'écoute." );
+define( "PLSHRD_SHRD_BY", "Par" );
+define( "PLSHRD_SHRD_FOR", "pour" );
+
+define( "PLSHRD_TB_PREV", "Page Précédente" );
+define( "PLSHRD_TB_PREV_HELP", "Cliquez pour accéder à la page précédente de listes partagées." );
+define( "PLSHRD_TB_NEXT", "Page Suivante" );
+define( "PLSHRD_TB_NEXT_HELP", "Cliquez pour accéder à la prochaine page de listes partagées." );
+define( "PLSHRD_TB_PAGE", "Page" );
+define( "PLSHRD_TB_PAGEBOT", "Bas De Page" );
+define( "PLSHRD_TB_PAGEBOT_HELP", "Navique au bas de cette page." );
+define( "PLSHRD_TB_PAGETOP", "Haut De Page" );
+define( "PLSHRD_TB_PAGETOP_HELP", "Retourne au début de cette page" );
+define( "PLSHRD_TB_PAGE_BTN", "Accéder" );
+
+##################################################
+
+?>
