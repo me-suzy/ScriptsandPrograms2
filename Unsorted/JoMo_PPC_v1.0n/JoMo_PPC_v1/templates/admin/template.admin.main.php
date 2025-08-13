4877799ff20542143f599aa7ffd12b0cd3c5080d
me-suzy <me.suzana@gmail.com>
2025-08-13 05:58:41 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/admin/template.admin.main.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/admin/template.admin.main.php
@@ -0,0 +1,28 @@
+<%include file="admin/admin.header.php"%>
+
+<%include file="admin/admin.menu.php"%>
+
+<div height="20"> &nbsp</div>
+
+<TABLE WIDTH="100%" ALIGN="CENTER" BORDER="0" CELLSPACING="0" CELLPADDING="1" BGCOLOR="#ffffff" background="">
+        <TR>
+          <TD ALIGN="CENTER" CLASS="clsText1"><h2>Welcome to admin area</h2></TD>
+        </TR>
+        
+</TABLE>
+
+<TABLE WIDTH="100%" ALIGN="CENTER" BORDER="0" CELLSPACING="0" CELLPADDING="1" BGCOLOR="#ffffff" background="">
+    <TR>
+      <TD ALIGN="left" CLASS="clsText1"><b>Operations</b>
+		<UL>
+			<LI> <a href="#">view links</a>
+			<LI> <a href="#">view accounts</a>
+			<LI> <a href="#">view accounts</a>
+			
+		</UL>
+      </TD>
+    </TR>
+		
+</TABLE>
+
+<%include file="admin/admin.footer.php"%>
