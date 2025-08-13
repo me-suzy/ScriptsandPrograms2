eb6492f0fd94d192b121b2786dd1a38e7029be90
me-suzy <me.suzana@gmail.com>
2025-08-13 06:30:13 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/en/common.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/en/common.php
@@ -0,0 +1,78 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[COMMON_Registered_Only] = "is for registered users only.";
+
+$LOCALE[COMMON_Unregistered_Only] = "is for unregistered users only.";
+	
+$LOCALE[COMMON_Username] = "Username";
+
+$LOCALE[COMMON_Function_Invalid] = "is an invalid command or is unavailable to you.";
+
+$LOCALE[COMMON_Operator] = "has been made an operator.";
+
+$LOCALE[COMMON_No_Operator] = "is no longer an operator.";
+
+$LOCALE[COMMON_Login_Button] = "Login now";
+
+$LOCALE[COMMON_Not_Accept] = "is not accepting private messages.";
+
+$LOCALE[COMMON_Help] = "Help";
+
+$LOCALE[COMMON_No_Encryption] = "MWChat detected you do NOT have mcrypt installed into php, disabling encryption.";
+
+$LOCALE[COMMON_System_Message] = "System Message";
+
+$LOCALE[COMMON_Click_Close] = "Click to close";
+
+$LOCALE[COMMON_YOB] = "Year of birth";
+
+$LOCALE[COMMON_Location] = "Location";
+
+$LOCALE[COMMON_Job] = "Occupation";
+
+$LOCALE[COMMON_Hobbies] = "Hobbies";
+
+$LOCALE[COMMON_Status] = "Status";
+
+$LOCALE[COMMON_Comments] = "Additional comments";
+
+$LOCALE[COMMON_Password] = "New password";
+
+$LOCALE[COMMON_Confirm_Password] = "Confirm new password";
+
+$LOCALE[COMMON_Logout] = "Logout of MWChat";
+
+$LOCALE[COMMON_Err_Pass] = "The passwords you entered do not match or are not at least five characters in length.";
+
+$LOCALE[COMMON_HelpClick] = "Click here for help";
+
+$LOCALE[COMMON_Text] = "Message";
+
+$LOCALE[COMMON_Bug_Report] = "Bug description";
+
+$LOCALE[COMMON_Reason] = "Reason";
+
+$LOCALE[COMMON_Action] = "Action";
+
+$LOCALE[COMMON_Topic] = "Topic";
+
+$LOCALE[COMMON_URL] = "URL";
+
+$LOCALE[COMMON_Alias] = "is an alias for the command";
+
+$LOCALE[COMMON_Room_Name] = "Room Name";
+
+$LOCALE[COMMON_Topic] = "None";
+
+?>
