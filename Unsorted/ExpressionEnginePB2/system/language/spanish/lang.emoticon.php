859858a5057450984c76500ee152483d25299adc
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:49 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.emoticon.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.emoticon.php
@@ -0,0 +1,31 @@
+<?php
+$L = array(
+
+
+"emoticon_module_name" =>
+"Emoticono",
+
+"emoticon_module_description" =>
+"Módulo de emiticonos (smileys)",
+
+"emoticon_heading" =>
+"Emoticonos",
+
+"emoticon_glyph" =>
+"Glipho",
+
+"emoticon_image" =>
+"Imagen",
+
+"emoticon_width" =>
+"Ancho",
+
+"emoticon_height" =>
+"Alto",
+
+"emoticon_alt" =>
+"Alt tag",
+
+''=>''
+);
+?>
