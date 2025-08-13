5cd6492f4f4a49f7ec19f4f8127ac466eb9cbe67
me-suzy <me.suzana@gmail.com>
2025-08-13 04:18:58 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.communicate.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.communicate.php
@@ -0,0 +1,178 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Communicate
+//----------------------------
+
+"your_name" =>
+"Ihr Name",
+
+"your_email" =>
+"Ihre E-Mail-Adresse",
+
+"recipient" =>
+"Empf&auml;nger",
+
+"cc" =>
+"CC",
+
+"bcc" =>
+"BCC",
+
+"recipient_group" =>
+"An Mitgliedsgruppen schicken",
+
+"manual_recipients_ignored" =>
+"Das obige Empf&auml;ngerfeld wird ignoriert.",
+
+"send_to_mailinglist" =>
+"An die Mailing-Liste versenden?",
+
+"honor_email_pref" =>
+"Nur an Mitglieder senden, die E-Mail annehmen",
+
+"separate_emails_with_comma" =>
+"Mehrere E-Mail-Adressen durch Kommata trennen",
+
+"no_email_matching_criteria" =>
+"Es wurden keine E-Mail-Adressen gefunden, die Ihren Suchkriterien entsprechen.",
+
+"not_allowed_to_email_members" =>
+"Sie d&uuml;rfen keine E-Mail an Mitglieder versenden.",
+
+"not_allowed_to_email_member_groups" =>
+"Sie d&uuml;rfen keine E-Mail an Mitgliedsgruppen versenden.",
+
+"not_allowed_to_email_mailinglist" =>
+"Sie d&uuml;rfen keine E-Mail an die Mailing-Liste versenden.",
+
+"subject" =>
+"Betreff",
+
+"message" =>
+"Nachricht",
+
+"send_an_email" =>
+"E-Mail senden",
+
+"sending_email" =>
+"Sende E-Mail",
+
+"batchmode_ready_to_begin" =>
+"Der E-Mail-Versand beginnt in f&uuml;nf Sekunden.",
+
+"batchmode_warning" =>
+"Fassen Sie Ihren Browser nicht an, solange dieser Proze&szlig; l&auml;uft!",
+
+"problem_with_id" =>
+"Es gab ein Problem mit der ID-Nummer, die zum Versand der Mail ben&ouml;tigt wird.",
+
+"cache_data_missing" =>
+"Ich konnte die E-Mail-Cachedaten nicht finden.",
+
+"currently_sending_batch" =>
+"Sende E-Mails %x bis %y",
+
+"emails_remaining" =>
+"Verbleibende E-Mails:",
+
+"email_error" =>
+"E-Mail-Fehler",
+
+"send_it" =>
+"Absenden",
+
+"total_emails_sent" =>
+"Summe der versendeten E-Mails:",
+
+"plain_text" =>
+"Nur Text",
+
+"html" =>
+"HTML",
+
+"mail_format" =>
+"Format",
+
+"word_wrap" =>
+"Zeilenumbruch",
+
+"priority" =>
+"Vorrang",
+
+"chars" =>
+"Zeichen",
+
+"highest" =>
+"Sehr hoch",
+
+"high" =>
+"Hoch",
+
+"normal" =>
+"Normal",
+
+"low" =>
+"Niedrig",
+
+"lowest" =>
+"Sehr niedrig",
+
+"empty_form_fields" =>
+"Sie haben nicht alle Felder ausgef&uuml;llt.",
+
+"email_sent_message" =>
+"Ihre E-Mail wurde versendet.",
+
+"all_email_sent_message" =>
+"Alle E-Mails wurden versendet.",
+
+"email_sent" =>
+"E-Mail versendet",
+
+"view_email_cache" =>
+"Fr&uuml;her versendete E-Mails ansehen",
+
+"previous_email" =>
+"Fr&uuml;her versendete E-Mail",
+
+"email_title" =>
+"E-Mail-Titel",
+
+"email_date" =>
+"Verschickt am",
+
+"total_recipients" =>
+"Summe der Empf&auml;nger",
+
+"resend" =>
+"Nochmal versenden",
+
+"view" =>
+"Ansehen",
+
+"no_cached_email" =>
+"Es gibt keine gespeicherten E-Mails.",
+
+"delete_emails" =>
+"E-Mail l&ouml;schen",
+
+"delete_confirm" =>
+"L&ouml;schbest&auml;tigung",
+
+"delete_question" =>
+"Soll(en) diese E-Mail(s) wirklich gel&ouml;scht werden?",
+
+"email_deleted" =>
+"E-Mail gel&ouml;scht",
+
+"mailinglist_unsubscribe" =>
+"Um Ihre E-Mail-Adresse von der Liste zu entfernen, hier klicken:",
+
+
+// END
+''=>''
+);
+?>
