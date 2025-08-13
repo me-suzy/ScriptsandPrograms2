1de7ef13e64e4d62f79045352dd097b9057b91d6
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:23 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-index.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-index.php
@@ -0,0 +1,25 @@
+<?php
+
+##################################################
+
+# /index.php
+
+##################################################
+
+define( "BRWS_GE_HEADER", "GENRES DISPONIBLES" );
+define( "BRWS_GE_HELP", "Cliquez pour voir toutes les pistes de ce Genre." );
+define( "BRWS_PLAY_GE_HELP", "Jouer Toutes Les Pistes De Ce Genre" );
+
+define( "BRWS_AR_HEADER", "NOUVEAUX ARTISTES" );
+define( "BRWS_AR_HELP", "Cliquez pour lister toutes les pistes par le(s) même(s) Artiste(s)" );
+define( "BRWS_PLAY_AR_HELP", "Jouer Toutes Les Pistes Par Cet Artiste" );
+define( "BRWS_FILTER_AR_HELP", "Lister tous les albums figurant cet artiste." );
+
+define( "BRWS_AL_HEADER", "NOUVEAUX ALBUMS" );
+define( "BRWS_AL_HELP", "Cliquez pour lister toutes les pistes sur cet Album." );
+define( "BRWS_PLAY_AL_HELP", "Jouer Toutes Les Pistes Sur Cet Album" );
+define( "BRWS_FILTER_AL_HELP", "Lister tous les artistes figurant sur cet album." );
+
+##################################################
+
+?>
