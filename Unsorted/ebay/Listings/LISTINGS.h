d3a134cf52ebf77d2c247a2afe580b2f35dac64d
me-suzy <me.suzana@gmail.com>
2025-08-13 03:24:21 +0300

    Add Unsorted/ebay/Listings/LISTINGS.h

---
+++ Unsorted/ebay/Listings/LISTINGS.h
@@ -0,0 +1,9 @@
+/* $Id: LISTINGS.h,v 1.2 1998/06/23 04:20:59 josh Exp $ */
+#if !defined(AFX_LISTING2_H__EFF2C209_B9CD_11D1_9810_00400541AE82__INCLUDED_)
+#define AFX_LISTINGS_H__EFF2C209_B9CD_11D1_9810_00400541AE82__INCLUDED_
+
+// LISTINGS.H - Header file for your Internet Server
+//    Listings Filter
+// Woohoo. We're doing away with MFC. This file becomes quite small and simple.
+#include "resource.h"
+#endif // !defined(AFX_LISTINGS_H__EFF2C209_B9CD_11D1_9810_00400541AE82__INCLUDED)
