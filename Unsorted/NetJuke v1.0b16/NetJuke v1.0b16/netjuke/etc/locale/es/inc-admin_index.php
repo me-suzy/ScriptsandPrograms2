46bf2c845b25015a88cca64ac6acd2bd0fc361d1
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:36 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_index.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_index.php
@@ -0,0 +1,47 @@
+<?php
+
+##################################################
+
+# /admin/index.php
+
+##################################################
+
+define( "ADMNDX_SYS_HEADER", "ADMINISTRACIÓN DEL SISTEMA" );
+define( "ADMNDX_CONT_HEADER", "ADMINISTRACIÓN MUSICAL" );
+
+define( "ADMNDX_EDIT_HELP", "Click acá para acceder a esta Herramienta de Administración." );
+define( "ADMNDX_INFO_HELP", "AYUDA: Click aquí para más información sobre esta Herramienta de Administración." );
+
+define( "ADMNDX_PREFEDIT", "ARCHIVO DE PREFERENCIAS" );
+define( "ADMNDX_PREFEDIT_HELP", "Use este formulario para editar las preferencias del archivo de la base de datos de la información de Usuario, importante ruta del archivo, encabezado html & pie, etc." );
+
+define( "ADMNDX_USERMAINT", "CONFIGURACION CUENTA USUARIO" );
+define( "ADMNDX_USERMAINT_HELP", "Le permite a Ud. ver / adicionar / editar / eliminar usuarios." );
+
+define( "ADMNDX_HIDFILE", "BUSQUEDA DE ARCHIVOS ESCONDIDOS" );
+define( "ADMNDX_HIDFILE_HELP", "Buscar archivos escondidos en el directorios o subdirectorios (Nombre de archivo empieza com w/ un punto) y genera un apropiado archivo de procesos y borrarlos desde la línea de comandos. Esto es especialmente usado cuando sube archivos desde un Mac a Unix (.AppleDouble, .DS_Store, etc)" );
+
+define( "ADMNDX_PHPINFO", "INFORMACIÓN PHP" );
+define( "ADMNDX_PHPINFO_HELP", "Este Script le muestra alguna información de valores relacionados con PHP en el servidor de  NetJuke." );
+
+define( "ADMNDX_TRADD", "ADDICIONE NUEVA INFORMACIÓN DE LA CANCIÓN" );
+define( "ADMNDX_TRADD_HELP", "El uso de este formulario es para adicionar información de nuevas canciones en la base de datos. Esto es especialmente interesante para música remota que no puede ser buscada en su directorio de música, tal como las estaciones de radio y streaming de musica encontradas en estos sitios." );
+
+define( "ADMNDX_MP3UPL", "SUBIR ARCHIVO DE AUDIO" );
+define( "ADMNDX_MP3UPL_HELP", "Use este formulario para subir archivos de audio en el servidor local. Los archivos pueden subirse individualmente como .mp3, o por lotes como archivos .zip/.tar.gz/.tgz." );
+
+define( "ADMNDX_MP3FIND", "BUSCAR ARCHIVOS DE AUDIO RECURSIVAMENTE" );
+define( "ADMNDX_MP3FIND_HELP", "Escaneo Recursivo dando el nombre del directorio para archivos terminados en \".mp3\" o \".ogg\" o \".wma\" (caso insensible). Al final, te será presentada una forma para entrar información opcionalmente de los archivos que tienen meta tags válidos." );
+
+define( "ADMNDX_TABFILEUPL", "SUBIR ARCHIVO TAB-DELIMITADOR" );
+define( "ADMNDX_TABFILEUPL_HELP", "Usado para subir archivos de texto para ser importados a la base de datos. Click para mas información en formato de archivo, etc." );
+
+define( "ADMNDX_TABFILEIMP", "IMPORTAR ARCHIVO TAB-DELIMITADOR" );
+define( "ADMNDX_TABFILEIMP_HELP", "Esta utilidad procesa todos los archivos de texto tab-delimitadores que han sido cargados, o generados por otras herramientas, e importado su contenido de una base de datos activa." );
+
+define( "ADMNDX_DBMAINTAIN", "MANTENIMIENTO DE LA BASE DE DATOS MUSICAL" );
+define( "ADMNDX_DBMAINTAIN_HELP", "Utilidades para respaldo de la base de datos musical, escaneo de archivos de audio perdidos, etc." );
+
+##################################################
+
+?>
