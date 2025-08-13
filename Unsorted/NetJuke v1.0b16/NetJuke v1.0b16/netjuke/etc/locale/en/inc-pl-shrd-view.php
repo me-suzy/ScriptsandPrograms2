84044d94d9cc2c7d3217a10a854fa95de8314fb8
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:15 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-pl-shrd-view.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-pl-shrd-view.php
@@ -0,0 +1,80 @@
+<?php
+
+##################################################
+
+# /pl-shrd-view.php
+
+##################################################
+
+define( "PLSHRD_NOPLID", "Please specify a playlist to view." );
+
+define( "PLSHRD_SAVE_NOID", "Please specify a playlist to be saved as a favorite." );
+
+define( "PLSHRD_DELPL_NOID", "Please specify a playlist to delete from your favorites." );
+define( "PLSHRD_DELPL_CONFIRM", "Are you sure you want to delete this playlist from your favorites?" );
+
+define( "PLSHRD_CHCK_HELP", "Click To Select This Track" );
+define( "PLSHRD_PLAY_HELP", "Click To Play This Track" );
+define( "PLSHRD_DLOAD_HELP", "Click To Download This Track" );
+define( "PLSHRD_INFO_HELP", "Click To View This Track's Detailed Information." );
+define( "PLSHRD_AR_HELP", "Click To View All The Tracks By This Artist." );
+define( "PLSHRD_AL_HELP", "Click To View All The Tracks On This Album." );
+define( "PLSHRD_GE_HELP", "Click To View All The Tracks Of This Genre." );
+
+define( "PLSHRD_HEADER_FAV", "FAVORITE PLAYLIST" );
+define( "PLSHRD_HEADER_PUB", "PUBLIC PLAYLIST" );
+define( "PLSHRD_HEADER_BY", "By" );
+
+define( "PLSHRD_BTN_DELETE", "Delete" );
+define( "PLSHRD_BTN_CANCEL", "Cancel" );
+
+define( "PLSHRD_ROWCNT_1", "Registered Track(s)" );
+define( "PLSHRD_ROWCNT_2", "for" );
+
+define( "PLSHRD_QS_AR", "Artist" );
+define( "PLSHRD_QS_TR", "Track" );
+define( "PLSHRD_QS_AL", "Album" );
+define( "PLSHRD_QS_GE", "Genre" );
+define( "PLSHRD_QS_TI", "Time" );
+define( "PLSHRD_QS_BTN", "Search" );
+
+define( "PLSHRD_TB_PLIST_BTN", "Edit" );
+define( "PLSHRD_TB_ADDED", "Added" );
+define( "PLSHRD_TB_PLAY_ALL", "Play" );
+define( "PLSHRD_TB_PLAY_ALL_HELP", "Click To Play This Playlist As Saved By The Author" );
+define( "PLSHRD_TB_SAVE", "Save As Favorite" );
+define( "PLSHRD_TB_SAVE_HELP", "Click To Save This Playlist to Your List Of Favorites" );
+define( "PLSHRD_TB_SELECT", "Select All" );
+define( "PLSHRD_TB_SELECT_HELP", "Click To Select All The Tracks Listed On This Page" );
+define( "PLSHRD_TB_RESET", "Release All" );
+define( "PLSHRD_TB_RESET_HELP", "Click To Release All Your Selected Tracks" );
+define( "PLSHRD_TB_PLAY", "Play Selected" );
+define( "PLSHRD_TB_PLAY_HELP", "Click To Start Playing All The Selected Tracks" );
+define( "PLSHRD_TB_ADDTO", "Copy To Playlist" );
+define( "PLSHRD_TB_ADDTO_HELP", "Click To Copy All Your Selected Tracks To The Selected Playlist" );
+
+define( "PLSHRD_COLS_OP", "Options" );
+define( "PLSHRD_COLS_SQ", "Seq." );
+define( "PLSHRD_COLS_SQ_HELP", "Click To Sort This Page By Sequence Number" );
+define( "PLSHRD_COLS_TR", "Track Name" );
+define( "PLSHRD_COLS_TR_HELP", "Click To Sort This Page By Track Name" );
+define( "PLSHRD_COLS_TI", "Time" );
+define( "PLSHRD_COLS_TI_HELP", "Click To Sort This Page By Track Time" );
+define( "PLSHRD_COLS_AR", "Artist" );
+define( "PLSHRD_COLS_AR_HELP", "Click To Sort This Page By Artist Name" );
+define( "PLSHRD_PLAY_AR_HELP", "Start Playing All The Tracks By This Artist" );
+define( "PLSHRD_FILTER_AR_HELP", "Click To List All The Albums Showcasing This Artist." );
+define( "PLSHRD_COLS_AL", "Album" );
+define( "PLSHRD_COLS_AL_HELP", "Click To Sort This Page By Album Name" );
+define( "PLSHRD_PLAY_AL_HELP", "Start Playing All The Tracks On This Album" );
+define( "PLSHRD_FILTER_AL_HELP", "Click To List All The Artists Showcased On This Album." );
+define( "PLSHRD_COLS_TN", "#" );
+define( "PLSHRD_COLS_TN_HELP", "Click To Sort This Page By Track Number" );
+define( "PLSHRD_COLS_GE", "Genre" );
+define( "PLSHRD_COLS_GE_HELP", "Click To Sort This Page By Genre Name" );
+define( "PLSHRD_PLAY_GE_HELP", "Start Playing All The Tracks In This Genre" );
+
+
+##################################################
+
+?>
