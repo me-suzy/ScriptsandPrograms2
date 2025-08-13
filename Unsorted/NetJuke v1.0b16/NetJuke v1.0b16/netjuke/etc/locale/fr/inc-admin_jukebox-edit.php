a1607e5ca5206294cb9ec98a8febcbaf4baa1784
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:56 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_jukebox-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_jukebox-edit.php
@@ -0,0 +1,23 @@
+<?php
+
+##################################################
+
+# /admin/jukebox-edit.php
+
+##################################################
+
+define( "JUKEBOX_LIST_NOPATH", "Fichier Inconnu:\\nLa liste de jukebox ne peut être trouvée." );
+
+define( "JUKEBOX_LIST_NOPERM", "Erreur de Permissions:\\nLa liste de jukebox ne peut être écrite." );
+
+define( "JUKEBOX_LIST_DONE", "La liste de jukebox visée a été sauvegardée avec succés." );
+
+define( "JUKEBOX_START_DONE", "La liste de radio visée a été démarrée avec succés." );
+
+define( "JUKEBOX_STOP_DONE", "La liste de radio arrêté a été démarrée avec succés." );
+
+define( "JUKEBOX_NOPLAYER", "Fichier Inconnu:\\nLe lecteur audio ne peut être trouvé sur le serveur." );
+
+##################################################
+
+?>
