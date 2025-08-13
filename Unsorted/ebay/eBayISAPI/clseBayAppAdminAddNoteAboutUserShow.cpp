5b6f29ac2dbe74f6370599512d935a344d37b7b3
me-suzy <me.suzana@gmail.com>
2025-08-13 03:06:30 +0300

    Add Unsorted/ebay/eBayISAPI/clseBayAppAdminAddNoteAboutUserShow.cpp

---
+++ Unsorted/ebay/eBayISAPI/clseBayAppAdminAddNoteAboutUserShow.cpp
@@ -0,0 +1,271 @@
+/*	$Id: clseBayAppAdminAddNoteAboutUserShow.cpp,v 1.3.428.1 1999/08/01 02:51:39 barry Exp $	*/
+//
+//	File:		clseBayAppAdminAddNoteAboutUser.cpp
+//
+//	Class:		clseBayApp
+//
+//	Author:		Michael Wilson (michael@ebay.com)
+//
+//	Function:
+//
+//				This function draws the "form" for adding a note 
+//				about a user. It's a function (rather than a static
+//				form) so a private routine inside of it can be called
+//				to emit a form with the fields "intact" from a previous
+//				invocation.
+//
+//	Modifications:
+//				- 04/03/98 michael	Created.
+//				- 07/19/99 nsacco	- Switched from mpMarketPlace->GetName() to GetCurrentPartnerName()
+//
+#include "ebihdr.h"
+
+void clseBayApp::ShowAddNoteAboutUser(char *pUser,
+									  char *pPass,
+									  char *pAboutUser,
+									  char *pSubject,
+									  int type,
+									  char *pText)
+{
+	*mpStream <<	"<form method=post action="
+					"\""
+			  <<	mpMarketPlace->GetCGIPath(PageAddNoteAboutUser)
+			  <<	"eBayISAPI.dll"
+					"\""
+					">"
+					"<INPUT TYPE=HIDDEN "
+					"NAME=\"MfcISAPICommand\" "
+					"VALUE=\"AdminAddNoteAboutUser\">";
+
+	*mpStream <<	"<table border=\"1\" cellpadding=\"3\" cellspacing=\"0\">"
+					"<tr>" 
+					"<td width=\"160\" bgcolor=\"#EFEFEF\" valign=\"top\">"
+					"<strong>"
+					"<font size=\"3\" color=\"#006600\">"
+					"Your User ID / Password"
+					"</font>"
+					"</strong>"
+					"</td>"
+					"<td width=\"430\">"
+					"<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">"
+					"<tr>" 
+					"<td width=\"50%\" valign=\"top\">" 
+					"<input type=\"text\" name=\"userid\" size=\"24\" maxlength=\""
+			   <<	EBAY_MAX_USERID_SIZE
+			   <<	"\"";
+	if (pUser != NULL					&&
+		strcmp(pUser, "default") != 0		)
+	{
+		*mpStream <<	" VALUE=\""
+				  <<	pUser
+				  <<	"\" ";
+	}
+	
+	*mpStream <<	">"
+					"<br>"
+					"<font size=\"2\">"
+					"<strong>"
+					"User ID"
+					"</strong>"
+					" or E-mail address"
+					"</font>"
+					"</td>"
+					"<td width=\"50%\" valign=\"top\">" 
+					"<input type=\"password\" name=\"pass\" size=\"18\" maxlength=\""
+				<<	EBAY_MAX_PASSWORD_SIZE
+				<<	"\"";
+	if (pPass != NULL					&&
+		strcmp(pPass, "default") != 0		)
+	{
+		*mpStream <<	" VALUE=\""
+				  <<	pPass
+				  <<	"\" ";
+	}
+	
+	*mpStream <<	">"
+					"<br>"
+					"<font size=\"2\">"
+					"Password"
+					"</font>"
+					"</td>"
+					"</tr>"
+					"</table>"
+					"</td>"
+					"</tr>"
+					"<tr>" 
+					"<td width=\"160\" bgcolor=\"#EFEFEF\" valign=\"top\">"
+					"<font size=\"3\" color=\"#006600\">"
+					"<strong>"
+					"This eNote is about:"
+					"</strong>" 
+					"</font>"
+					"</td>"
+					"<td width=\"430\">" 
+					"<input type=\"text\" name=\"aboutuser\" size=\"24\" maxlength=\""
+				<<	EBAY_MAX_USERID_SIZE
+				<<	"\"";
+				
+	if (pAboutUser != NULL					&&
+		strcmp(pAboutUser, "default") != 0		)
+	{
+		*mpStream <<	" VALUE=\""
+				  <<	pAboutUser
+				  <<	"\"";
+	}
+	
+	*mpStream <<	">"
+					"<br>"
+					"<font size=\"2\">"
+					"<strong>"
+					"User ID"
+					"</strong>"
+					" or E-mail address"
+					"</font>"
+					"</td>"
+					"</tr>"
+					"<tr> "
+					"<td width=\"160\" bgcolor=\"#EFEFEF\" valign=\"top\">"
+					"<font size=\"3\" color=\"#006600\">"
+					"<strong>"
+					"Subject:"
+					"</strong>" 
+					"</font>"
+					"</td>"
+					"<td width=\"430\">" 
+					"<input type=\"text\" name=\"subject\" size=\"60\" maxlength=\"120\"";
+	
+	if (pSubject != NULL				&&
+		strcmp(pSubject, "default") != 0	)
+	{
+		*mpStream <<	" VALUE=\""
+				  <<	pSubject
+				  <<	"\"";
+	}
+	
+	*mpStream <<	">"
+					"<font size=\"2\">"
+					"<br>"
+					"<strong>"
+					"120"
+					"</strong>"
+					" characters max!"
+					"</font>"
+					"</td>"
+					"</tr>"
+					"<tr>" 
+					"<td width=\"160\" bgcolor=\"#EFEFEF\">"
+					"<strong>"
+					"<font size=\"3\" color=\"#006600\">"
+					"What's this eNote about?"
+					"</font>"
+					"</strong>"
+					"</td>"
+					"<td width=430>"
+					"<SELECT name=\"type\" size=\"1\">";
+
+	clsNote::EmitNoteTypesAsHTMLOptions(mpStream, eNoteMajorTypeUnknown, type);
+										
+	*mpStream <<	"</SELECT>"
+					"</td>"
+					"</tr>"
+					"<tr>"
+					"<td width=\"160\" bgcolor=\"#EFEFEF\" valign=\"top\">"
+					"<font size=\"3\" color=\"#006600\">"
+					"<strong>"
+					"Text"
+					"</strong>"
+					"</font>"
+					"<font size=\"2\">"
+					" (HTML&nbsp;ok)"
+					"</font>"
+					"</td>"
+					"<td width=\"430\">"
+					"<textarea name=\"text\" cols=\"56\" rows=\"8\">";
+	if (pText != NULL				&&
+		strcmp(pText, "default") != 0	)
+	{
+		*mpStream <<	pText;
+	}
+
+	*mpStream <<	"</textarea>";
+	
+	*mpStream <<	"</textarea>"
+					"<font size=\"2\" color=\"#006600\">"
+					" (required)"
+					"</font>"
+					"</td>"
+					"</tr>"
+					"</table>"
+					"<p>"
+					"<br>"
+					"</p>"
+					"<table border=\"0\" width=\"590\">"
+					"<tr>"
+					"<td>"
+					"<p>"
+					"<strong>"
+					"Press " 
+					"<input type=\"submit\" value=\"Submit\">"
+					" to send this eNote"
+					"</strong>"
+					"</p>"
+					"<p>"
+					"Press " 
+					"<input type=\"reset\" value=\"clear form\">"
+					" to start over."
+					"</p>"
+					"</td>"
+					"</tr>"
+					"</table>"
+					"</form>";
+
+	return;
+}
+
+
+
+void clseBayApp::AdminAddNoteAboutUserShow(CEBayISAPIExtension *pThis, 
+										   char *pUser,
+										   eBayISAPIAuthEnum authLevel)
+{
+	SetUp();
+
+	// We'll need a title here
+	*mpStream <<	"<html>"
+					"<head>"
+					"<TITLE>"
+			  <<	mpMarketPlace->GetCurrentPartnerName() 
+			  <<	" Adding a note about a user"
+			  <<	"</TITLE>"
+					"</head>"
+			  <<	flush;
+
+	*mpStream <<	mpMarketPlace->GetHeader()
+			  <<	"<br>";
+
+	// Let's see if we're allowed to do this
+	if (!CheckAuthorization(authLevel))
+	{
+		CleanUp();
+
+		return;
+	}
+
+	if (pUser != NULL && strcmp(pUser, "default") != 0)
+	{
+		ShowAddNoteAboutUser(NULL, NULL, pUser, NULL, 0, NULL);
+	}
+	else
+	{
+		ShowAddNoteAboutUser(NULL, NULL, NULL, NULL, 0, NULL);
+	}
+
+
+	*mpStream <<	"<br>"
+			  <<	mpMarketPlace->GetFooter();
+
+	CleanUp();
+	return;
+
+}
+
