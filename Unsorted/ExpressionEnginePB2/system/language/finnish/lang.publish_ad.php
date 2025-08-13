5021af064fd581c647a7f98265da3be1fdd22dd1
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:18 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.publish_ad.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.publish_ad.php
@@ -0,0 +1,666 @@
+<?php
+$L = array(
+
+"search_excerpt" =>
+"Field for Search Excerpt",
+
+"weblog_prefs" =>
+"Weblogin ominaisuudet",
+
+"weblog_settings" =>
+"Weblogin viestien ominaisuudet",
+
+"comment_prefs" =>
+"Kommenttien lisäämisen ominaisuudet",
+
+"comment_moderate" =>
+"Moderate Comments?",
+
+"comment_moderate_exp" =>
+"If set to yes, comments will not be visible until a moderator approves them.",
+
+"comment_system_enabled" =>
+"Salli kommentit tällä weblogilla?",
+
+"edit_weblog_prefs" =>
+"Muokka weblogin ominaisuuksia",
+
+"edit_group_prefs" =>
+"Muokaa ryhmä ominaisuuksia",
+
+"no_weblogs_exist" =>
+"Tällä hetkellä ei ole weblogeja",
+
+"create_new_weblog" =>
+"Muodosta uusi weblogi",
+
+"weblog_base_setup" =>
+"Weblogin yleiset ominaisuudet",
+
+"default_settings" =>
+"Hallinnan asetukset",
+
+"short_weblog_name" =>
+"Lyhyt nimi",
+
+"blog_url" =>
+"Weblog URL",
+
+"blog_lang" =>
+"XML kieli",
+
+"blog_encoding" =>
+"XML merkkien koodaus",
+
+"blog_description" =>
+"Weblogin kuvaus",
+
+"illegal_characters" =>
+"Antamasi nimi voi sisältää vain aakkosia, numeroita, välilyöntejä, alaviivoja ja viivoja",
+
+"comment_require_membership" =>
+"Vaadi jäsenyys kommentin lähetykseen?",
+
+"weblog_require_membership" =>
+"Vaadi jäsenyys weblogin viestin lähetykseen?",
+
+"comment_require_email" =>
+"Vaadi email osoite kommentin lähetykseen?",
+
+"weblog_require_email" =>
+"Vaadi email osoite weblogin viestin lähetykseen?",
+
+"weblog_max_chars" =>
+"Merkkien maksimi määrä weblogin viestissä",
+
+"comment_max_chars" =>
+"Merkkien maksimi määrä kommenteissa",
+
+"comment_timelock" =>
+"Kommentin aikaintervalli",
+
+"comment_timelock_desc" =>
+"Sekuntien määrä, joka tulee olla käyttäjän kahden kommentin välissä. Jätä tyhjäksi tai laita nolla, jos et halua asettaa estoja.",
+
+"comment_text_formatting" =>
+"Kommentin tekstin muotoilu",
+
+"weblog_text_formatting" =>
+"Oletus tekstin formatointi weblogin viestissä",
+
+"comment_html_formatting" =>
+"Kommentin HTML formatointi",
+
+"weblog_html_formatting" =>
+"Oletus HTML muotoilu weblogin viesteissä",
+
+"convert_to_entities" =>
+"Muunna HTML merkki entiteeteiksi",
+
+"allow_safe_html" =>
+"Salli vain turvallinen HTML",
+
+"allow_all_html" =>
+"Salli KAIKKI HTML",
+
+"allow_all_html_not_recommended" =>
+"Salli kaikki HTML (ei suositella)",
+
+"comment_notify" =>
+"Ilmoita järjestelmänvalvojalle kommenteista ja Trackbakeistä?",
+
+"comment_notify_note" =>
+"Erota useampi email osoite pilkulla",
+
+"comment_notify_emails" =>
+"Ilmoituksen vastaanottajan email osoite(et)",
+
+"comment_allow_img_urls" =>
+"Salli kuvalinkkejä kommenteissa?",
+
+"weblog_allow_img_urls" =>
+"Salli kuvalinkkejä viesteissä?",
+
+"auto_link_urls" =>
+"Muunna automaattisesti URL-osoitteet ja email osoitteet linkeiksi?",
+
+"single_word_no_spaces" =>
+"Yksi sana, ei välilyöntejä",
+
+"full_weblog_name" =>
+"Weblogin koko nimi",
+
+"edit_weblog" =>
+"Muokaa weblogia",
+
+"weblog_name" =>
+"Weblogin nimi",
+
+"new_weblog" =>
+"Uusi weblog",
+
+"weblog_created" =>
+"Weblog muodostettu:",
+
+"weblog_updated" =>
+"Weblog päivitetty:",
+
+"taken_weblog_name" =>
+"Tämä weblogin nimi on jo käytössä.",
+
+"no_weblog_name" =>
+"Sinun tulee antaa weblogisi &quot;lyhyt&quot;nimi",
+
+"no_weblog_title" =>
+"Sinun tulee antaa weblogisi &quot;koko&quot;nimi",
+
+"invalid_short_name" =>
+"Weblogin nimi tulee sisältää vain aakkosnumeerisia merkkejä ilman välilyöntejä.",
+
+"delete_weblog" =>
+"Poista weblog",
+
+"weblog_deleted" =>
+"Weblog poistettu:",
+
+"delete_weblog_confirmation" =>
+"Oletko varma, että haluat lopullisesti poistaa tämän weblogin?",
+
+"be_careful" =>
+"OLE VAROVAINEN!",
+
+"action_can_not_be_undone" =>
+"TÄTÄ TOIMINTOA EI VOI PERUUTTAA",
+
+"assign_group_to_weblog" =>
+"Huom: Käyttääksesi uutta ryhmää, tulee sinun liittää siihen weblog.",
+
+"click_to_assign_group" =>
+"Klikkaa tästä liittääksesi se",
+
+"default" =>
+"Oletus",
+
+"category" =>
+"Kategoria",
+
+"default_status" =>
+"Oletus status",
+
+"default_category" =>
+"Oletus kategoria",
+
+"deft_comments" =>
+"Valitse &quot;salli kommentit&quot; painike julkaisusivulla oletuksena?",
+
+"deft_trackbacks" =>
+"Valitse &quot;salli Trackback&quot; painike julkaisusivulla oletuksena?",
+
+"enable_trackbacks" =>
+"Lisää trackbackin automaattilöydön koodi sivuillesi?",
+
+"trackback_max_hits" =>
+"Suurin sallittu trackback pingiä per tunti",
+
+"trackback_field" =>
+"Mitä kenttää käytetään Trackbakin lyhennelmään?",
+
+"no_field_group_selected" =>
+"Tälle weblogille ei ole tietokenttiä",
+
+"missing_weblog_data_for_pings" =>
+"Pingin tai trackbackin lähetykseen tulee weblogilla olla nimi ja URL. Päivitä weblogisi ominaisuudet.",
+
+"open" =>
+"Näytetään",
+
+"closed" =>
+"Ei näytetä",
+
+"none" =>
+"Ei mitään",
+
+"define_html_buttons" =>
+"HTML muotoilupainikkeiden määritykset",
+
+"htmlbutton_delete_instructions" =>
+"Poistaaksesi kohteen lähetä lomake, jossa tagi nimeltä blank ",
+
+"tag_name" =>
+"Tagin nimi",
+
+"tag_open" =>
+"Aloittava tagi",
+
+"tag_close" =>
+"Päättävä tagi",
+
+"accesskey" =>
+"Pikakuvake",
+
+"tag_order" =>
+"Järjestys",
+
+"row" =>
+"Rivi",
+
+"server_name" =>
+"Serverin nimi",
+
+"server_url" =>
+"Serverin URL/polku",
+
+"port" =>
+"Portti",
+
+"protocol" =>
+"Protokolla",
+
+"is_default" =>
+"Oletus",
+
+"server_order" =>
+"Järjestys",
+
+"define_ping_servers" =>
+"Nämä kentät mahdollistavat sinun listata servereitä jota voidaan pingata, kun lähetät weblogin viestin",
+
+"pingserver_delete_instructions" =>
+"Poistaaksesi kohteen lähetä lomake, jossa tagi nimeltä blank ",
+
+"assign_weblogs" =>
+"Valitse mitkä weblogit haluat littää tähän ryhmään",
+
+"category_group" =>
+"Kategoria ryhmä",
+
+"category_groups" =>
+"Kategoria ryhmät",
+
+"no_category_group_message" =>
+"Tällä hetkellä ei ole kategorioita",
+
+"no_category_message" =>
+"Tähän ryhmään ei ole liitetty yhtään kategoriaa ",
+
+"create_new_category_group" =>
+"Muodosta uusi katategoria ryhmä",
+
+"edit_category_group" =>
+"Muokaa kategoria ryhmää",
+
+"name_of_category_group" =>
+"Kategoria ryhmän nimi",
+
+"taken_category_group_name" =>
+"Tämä ryhmän nimi on jo käytössä.",
+
+"add_edit_categories" =>
+"Lisää/muokkaa kategorioita",
+
+"edit_group_name" =>
+"Muokaa ryhmää",
+
+"delete_group" =>
+"Poista ryhmä",
+
+"category_group_created" =>
+"Kategoria ryhmä muodostettu:",
+
+"category_group_updated" =>
+"Ryhmä päivitetty:",
+
+"delete_cat_group_confirmation" =>
+"Oletko varma, että haluat lopullisesti poistaa tämän kategoria ryhmän?",
+
+"category_group_deleted" =>
+"Kategoria ryhmä poistettu:",
+
+"create_new_category" =>
+"Muodosta uusi kategoria",
+
+"add_new_category" =>
+"Lisää uusi kategoria",
+
+"edit_category" =>
+"Muokkaa kategoriaa",
+
+"delete_category" =>
+"Poista kategoria",
+
+"category_name" =>
+"Kategorian nimi",
+
+"category_image" =>
+"Kategorian kuvian URL",
+
+"category_img_blurb" =>
+"Tämä on valinnainen kenttä joka mahdollistaa sinun littää kuvan kategorioihisi.",
+
+"category_parent" =>
+"Pää kategoria",
+
+"field_group" =>
+"Kenttä ryhmä",
+
+"field_groups" =>
+"Kenttä ryhmät",
+
+"field_group_name" =>
+"Kenttä ryhmän nimi",
+
+"custom_fields" =>
+"Muokattu kenttä",
+
+"no_field_group_message" =>
+"Tällä hetkellä ei oli kustomoituja weblogien kenttiä",
+
+"create_new_field_group" =>
+"Muodosta uusi weblogin kenttä ryhmä",
+
+"new_field_group" =>
+"Uusi kenttä ryhmä",
+
+"add_edit_fields" =>
+"Lisää/muokkaa kustomoituja kenttiä",
+
+"edit_field_group_name" =>
+"Muokkaa kenttä ryhmää",
+
+"delete_field_group" =>
+"Poista kenttä ryhmää",
+
+"create_new_field" =>
+"Muodosta uusi kenttä",
+
+"edit_field" =>
+"Muokkaa kenttää",
+
+"no_field_groups" =>
+"Tällä hetkellä ei ole kustomoituja kenttiä tässä ryhmässä",
+
+"delete_field" =>
+"Poista kenttä",
+
+"field_deleted" =>
+"Kustomoitu kenttä poistettu:",
+
+"edit_field_order" =>
+"Vaihda kentän järjestystä",
+
+"create_new_custom_field" =>
+"Muodosta uusi kustomoitu kenttä",
+
+"field_label" =>
+"Kentän nimi",
+
+"field_name" =>
+"Kentän nimi (järjestelmän)",
+
+"field_name_cont" =>
+"Yksi sana, ei välilyöntejä. Alaviiva ja viiva sallittuja",
+
+"field_type" =>
+"Kentän tyyppi",
+
+"field_max_length" =>
+"Maksimi leveys",
+
+"field_max_length_cont" =>
+"Jos valitsit tyypiksi tekstikentän (text field)",
+
+"textarea_rows" =>
+"Tekstikentän rivit",
+
+"textarea_rows_cont" =>
+"Jos valitsit tyypiksi tekstikentän (textarea)",
+
+"field_list_items" =>
+"Valitse ominaisuudet",
+
+"field_list_items_cont" =>
+"Jos valitsit pudotusvalikon (drop-down menu)",
+
+"field_list_instructions" =>
+"Laita jokainen asia omalle riville",
+
+"field_formatting" =>
+"Kentän muotoilu",
+
+"field_formatting_cont" =>
+"Jos valitsit tyypiksi tekstikentän (textarea)",
+
+"field_order" =>
+"Kentän näyttöjärjestys",
+
+"is_field_searchable" =>
+"Voidaanko kentän tiedoista suorittaa etsintä?",
+
+"is_field_required" =>
+"Onko tämä &quot;pakollinen tieto&quot; kenttä",
+
+"text_input" =>
+"Tekstin syöttö",
+
+"textarea" =>
+"Tekstialue",
+
+"select_list" =>
+"Pudotus valikon lista",
+
+"auto_br" =>
+"Automaattiset &lt;br /&gt;",
+
+"xhtml" =>
+"xhtml",
+
+"no_field_name" =>
+"Sinun tulee syöttää kentän nimi (järjästelmälle)",
+
+"no_field_label" =>
+"Sinun tulee syötää kentän nimi",
+
+"invalid_characters" =>
+"Antamasi kentän nimi sisälsi kiellettyjä merkkejä",
+
+"custom_field_empty" =>
+"Seuraava kenttä on pakollinen",
+
+"duplicate_field_name" =>
+"Antamasi kentän nimi on jo käytössä",
+
+"taken_field_group_name" =>
+"Antamasi nimi on jo käytössä",
+
+"field_group_created" =>
+"Kenttä ryhmä muodostettu:",
+
+"field_group_updated" =>
+"Kenttä ryhmä päivitetty:",
+
+"field_group_deleted" =>
+"Kenttä ryhmä poistettu:",
+
+"delete_field_group_confirmation" =>
+"Oletko varma, että haluat lopullisesti poista kustomoidun kenttä ryhmän?",
+
+"delete_field_confirmation" =>
+"Oletko varma, että haluat lopullisesti poista kustomoidun kentän?",
+
+"weblog_entries_will_be_deleted" =>
+"Kaikki weblogien viestit, jotka sisältävät tämä kentän(t) tullaan lopullisesti poistamaan.",
+
+"status_group" =>
+"Status ryhmä",
+
+"status_groups" =>
+"Status ryhmät",
+
+"no_status_group_message" =>
+"Tällä hetkellä ei ole kustomoituja statuksia",
+
+"create_new_status_group" =>
+"Muodosta uusi status ryhmä",
+
+"edit_status_group" =>
+"Muokkaa status ryhmää",
+
+"name_of_status_group" =>
+"Status ryhmän nimi",
+
+"taken_status_group_name" =>
+"Tämä status ryhmän nimi on jo käytössä",
+
+"invalid_status_name" =>
+"Status nimi voi sisältää vain aakkosnumeerisia merkkejä, välilyöntejä, alaviivan ja viivan",
+
+"duplicate_status_name" =>
+"Saman niminen status on jo olemassa.",
+
+"status_group_created" =>
+"Status ryhmä muodostettu:",
+
+"new_status" =>
+"Uusi status",
+
+"status_group_updated" =>
+"Status ryhmä päivitetty:",
+
+"add_edit_statuses" =>
+"Lisää/muokkaa statuksia",
+
+"edit_status_group_name" =>
+"Muokkaa status ryhmää",
+
+"delete_status_group" =>
+"Poista status ryhmä",
+
+"delete_status_group_confirmation" =>
+"Haluatko varmasti poistaa tämän status ryhmän?",
+
+"status_group_deleted" =>
+"Status ryhmä poistettu:",
+
+"create_new_status" =>
+"Muodosta uusi status",
+
+"status_name" =>
+"Statuksen nimi",
+
+"status_order" =>
+"Statuksen järjestys",
+
+"change_status_order" =>
+"Muuta statuksen järjestystä",
+
+"highlight" =>
+"Korostuksen väri (valinnainen)",
+
+"statuses" =>
+"Statuksia",
+
+"edit_status" =>
+"Muokkaa statuksia",
+
+"delete_status" =>
+"Poista status",
+
+"delete_status_confirmation" =>
+"Oletko varma, että haluat poistaa statuksen?",
+
+"edit_file_upload_preferences" =>
+"Muokkaa tiedoston latauksen ominaisuuksia",
+
+"new_file_upload_preferences" =>
+"Uusi tiedoston latauksen kohde (kansio)",
+
+"file_upload_preferences" =>
+"Tiedoston latauksen ominaisuudet",
+
+"no_upload_prefs" =>
+"Tällä hetkellä ei ole tiedoston latauksen ominaisuuksia",
+
+"create_new_upload_pref" =>
+"Muodosta uusi latauskansio",
+
+"upload_pref_name" =>
+"Kuvaava nimi latauskansiolle",
+
+"server_path" =>
+"Serverin polku latauskansioon",
+
+"url_to_upload_dir" =>
+"Latauskansion URL",
+
+"allowed_types" =>
+"Sallitut tiedosto tyypit",
+
+"max_size" =>
+"Maksimi tiedoston koko (biteissä)",
+
+"max_height" =>
+"Maksimi kuvan korkeus (pikseleissä)",
+
+"max_width" =>
+"Maksimi kuvan leveys",
+
+"properties" =>
+"Kuvan ominaisuudet",
+
+"pre_format" =>
+"Esiformatointi",
+
+"post_format" =>
+"Jälkiformatointi",
+
+"no_upload_dir_name" =>
+"Sinun tulee antaa latauskansion nimi",
+
+"no_upload_dir_path" =>
+"Sinun tulee antaa latauskansion polku",
+
+"no_upload_dir_url" =>
+"Sinun tulee antaa latauskansion URL",
+
+"duplicate_dir_name" =>
+"Saman niminen kansio on jo olemassa",
+
+"delete_upload_preference" =>
+"Poista latauksen ominaisuudet",
+
+"delete_upload_pref_confirmation" =>
+"Oletko varma, että haluat poistaa tämän ominaisuuden?",
+
+"upload_pref_deleted" =>
+"Latauksen ominaisuudet poistettu:",
+
+"current_upload_prefs" =>
+"Nykyiset ominaisuudet",
+
+"restrict_to_group" =>
+"Estä tiedoston lataaminen valituilta jäsenten ryhmiltä",
+
+"restrict_notes_1" =>
+"Nämä radio painikkeet mahdollistavat sinun määrittää ketkä voivat  ladata tiedostoja ja kuvia heidän weblogien viesteihin",
+
+"restrict_notes_2" =>
+"Pääkäyttäjä voi aina ladata tiedotoja",
+
+"restrict_notes_3" =>
+"Huom: tiedoston lataus on tällä hetkellä mahdollista vain ohjauspaneelin kautta",
+
+"member_group" =>
+"Jäsenten ryhmä",
+
+"can_upload_files" =>
+"Voi ladata tiedostoja",
+
+"images_only" =>
+"Vain kuvia",
+
+"all_filetypes" =>
+"Kaikkia tiedosto tyyppejä",
+
+''=>''
+);
+?>
