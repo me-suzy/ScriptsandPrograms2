bfc468fe11bb8878fc5fadc576bdf7a4d410e26b
me-suzy <me.suzana@gmail.com>
2025-08-13 05:27:01 +0300

    Add Unsorted/ImageFolio_Commerce_v1.0__nullified_/IFCOMB/store_files/ifc_images/lb.js

---
+++ Unsorted/ImageFolio_Commerce_v1.0__nullified_/IFCOMB/store_files/ifc_images/lb.js
@@ -0,0 +1,298 @@
+// lightbox v 1.0
+// add-on to imageFolio written by Dirk Koppers                           
+// ---------------------------------------------------------------------------
+// JAVASCRIPT NAME : lb.js
+// VERSION : 1.0 build 500
+// LAST MODIFIED : 09/06/2001
+// ===========================================================================
+// COPYRIGHT NOTICE :
+//
+// Copyright (c) 1999-2000 Dirk Koppers, Inc. All Rights Reserved.
+// Selling the code for this program without prior written consent is
+// expressly forbidden.
+//
+// Obtain written permission before redistributing this software over the
+// Internet or in any other medium.  In all cases copyright and header must
+// remain intact.
+//
+// Feel free to modify the code of this program to suit your likings.
+//
+// Although this program has been thoroughly tested on my server, I
+// do not warrant that it works on all servers and will not be held liable
+// for anything, including but not limited to, misusage, error, or loss of data.
+//
+// Use at your own risk!
+// ===========================================================================
+// Do not modify below this line unless you know what you are doing.
+// ===========================================================================
+
+var today = new Date();
+var expiry = new Date(today.getTime() + 90 * 24 * 60 * 60 * 1000);
+var agt=navigator.userAgent.toLowerCase();
+var is_major = parseInt(navigator.appVersion);
+
+var is_ie   = ((agt.indexOf("msie") != -1) && (agt.indexOf("opera") == -1));
+var is_ie3  = (is_ie && (is_major < 4));
+var is_ie4  = (is_ie && (is_major == 4) && (agt.indexOf("msie 5")==-1) && (agt.indexOf("msie 6")==-1));
+var is_ie5up  = (is_ie  && !is_ie3 && !is_ie4);
+var is_nav  = ((agt.indexOf('mozilla')!=-1) && (agt.indexOf('spoofer')==-1) && (agt.indexOf('compatible') == -1) && (agt.indexOf('opera')==-1) && (agt.indexOf('webtv')==-1) && (agt.indexOf('hotjava')==-1));
+var is_nav6up = (is_nav && (is_major >= 5));
+
+if (document.images) {
+		img1 = new Image(); img1.src = if_images_directory+'/'+lightbox_no_gif;
+		img2 = new Image(); img2.src = if_images_directory+'/'+lightbox_yes_gif;
+	}
+
+index = document.cookie.indexOf(cookiename);
+countbegin = (document.cookie.indexOf("=", index) + 1);
+countend = document.cookie.indexOf(";", index);
+if (countend == -1) {countend = document.cookie.length;}
+fulllist = document.cookie.substring(countbegin, countend);
+realitems1=fulllist.split(']');
+realitems2=realitems1[0].split(cookiename);
+if (!realitems2[1]) {document.cookie=""+cookiename+"="+cookiename+".; path=/";}
+
+function showCount() {
+				index = document.cookie.indexOf(cookiename);
+		countbegin = (document.cookie.indexOf("=", index) + 1);
+			countend = document.cookie.indexOf(";", index);
+			if (countend == -1) {
+					countend = document.cookie.length;
+			}
+		fulllist = document.cookie.substring(countbegin, countend);
+
+
+				total = 0;
+		for (var i = 0; i <= fulllist.length; i++) {
+			if (fulllist.substring(i,i+1) == '[') {
+				itemstart = i+1;
+				thisitem = 1;
+			} else if (fulllist.substring(i,i+1) == ']') {
+				itemend = i;
+				thequantity = fulllist.substring(itemstart, itemend);
+							total=total+1;}}
+countlayerdiv='countlayerdiv';
+if (is_ie5up || is_nav6up){
+document.write('<div id="'+countlayerdiv+'"><a class="mainnav" href="'+imagefolio_url+'?lightbox=view">'+lightbox_menu_text+':&nbsp;'+total+'</a></div>');
+}
+else{document.write('<a class="mainnav" href="'+imagefolio_url+'?lightbox=view">'+lightbox_menu_text+':&nbsp;'+total+'</a>');}
+}
+
+function showItems() {
+		index = document.cookie.indexOf(cookiename);
+		countbegin = (document.cookie.indexOf("=", index) + 1);
+			countend = document.cookie.indexOf(";", index);
+			if (countend == -1) {
+					countend = document.cookie.length;
+			}
+		fulllist = document.cookie.substring(countbegin, countend);
+
+				okay=0;
+		itemlist = 0;
+		for (var i = 0; i <= fulllist.length; i++) {
+			if (fulllist.substring(i,i+1) == '[') {
+				itemstart = i+1;
+				thisitem = 1;
+			} else if (fulllist.substring(i,i+1) == ']') {
+				itemend = i;
+				thequantity = fulllist.substring(itemstart, itemend);
+							itemlist=itemlist+1;
+
+
+{						compare=path + imageid + extension;
+						 compareto=thePath + theImageid + theExtension;
+						 if (compare==compareto) {
+							okay=1;
+							which=itemlist;}
+}
+			} else if (fulllist.substring(i,i+1) == '|') {
+				if (thisitem==1) theImageid = fulllist.substring(itemstart, i);
+								if (thisitem==2) thePath = fulllist.substring(itemstart, i);
+								if (thisitem==3) theExtension = fulllist.substring(itemstart, i);
+								thisitem++;
+				itemstart=i+1;
+			}
+		}
+
+div='div';
+layerdiv=div+path+imageid+extension;
+
+if (usetextlinks==1){
+if (okay==1){
+		if (is_ie5up || is_nav6up){ document.writeln('<div id="'+layerdiv+'"><A HREF="javascript:removeItem(\''+imageid+'\',\''+path+'\',\''+extension+'\')" onMouseOver="Status(\'Remove from '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><nobr><'+font_remove_link+'>Remove from '+lightbox_name+'</font></nobr></a></div>');}
+		else { document.writeln('<a href="javascript:removeItem(\''+imageid+'\',\''+path+'\',\''+extension+'\')" onMouseOver="Status(\'Remove from '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><nobr><'+font_remove_link+'>Remove from '+lightbox_name+'</font></nobr></a>');}
+	}
+		else {
+		if (is_ie5up || is_nav6up){ document.writeln('<div id="'+layerdiv+'"><A HREF="javascript:addtolightbox(\''+imageid+'\',\''+path+'\',\''+extension+'\')" onMouseOver="Status(\'Add to '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><nobr><'+font_add_link+'>Add to '+lightbox_name+'</font></nobr></a></div>');}
+		else { document.writeln('<a href="javascript:addtolightbox(\''+imageid+'\',\''+path+'\',\''+extension+'\')" onMouseOver="Status(\'Add to '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><nobr><'+font_add_link+'>Add to '+lightbox_name+'</font></nobr></A>');}
+	}
+}
+else {
+if (okay==1){
+		if (is_ie5up || is_nav6up){ document.writeln('<div id="'+layerdiv+'"><A HREF="javascript:removeItem(\''+imageid+'\',\''+path+'\',\''+extension+'\')" onMouseOver="Status(\'Remove from '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><img src="'+if_images_directory+'/'+lightbox_yes_gif+'" border=0 alt="Remove from '+lightbox_name+'"></a></div>');}
+		else { document.writeln('<a href="javascript:removeItem(\''+imageid+'\',\''+path+'\',\''+extension+'\')" onMouseOver="Status(\'Remove from '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><img src="'+if_images_directory+'/'+lightbox_yes_gif+'" border=0 alt="Remove from '+lightbox_name+'"></a>');}
+	}
+		else {
+		if (is_ie5up || is_nav6up){ document.writeln('<div id="'+layerdiv+'"><A HREF="javascript:addtolightbox(\''+imageid+'\',\''+path+'\',\''+extension+'\')" onMouseOver="Status(\'Add to '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><img src="'+if_images_directory+'/'+lightbox_no_gif+'" border=0 alt="Add to '+lightbox_name+'"></a></div>');}
+		else { document.writeln('<a href="javascript:addtolightbox(\''+imageid+'\',\''+path+'\',\''+extension+'\')" onMouseOver="Status(\'Add to '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><img src="'+if_images_directory+'/'+lightbox_no_gif+'" border=0 alt="Add to '+lightbox_name+'"></A>');}
+	}
+}
+
+
+	}
+
+	function removeItem(newImageid, newPath, newExtension) {
+				newlocation = location.href;
+				newlocationarray=newlocation.split("&lightbox");
+				newstr=location.search.substring(1);
+				newarray=newstr.split("=");
+				if (newarray[0]=='action'){
+				lightbox = '&lightbox=1';
+				newtogo=newlocationarray[0]+lightbox;}
+				else {newtogo=location.href;}
+				index = document.cookie.indexOf(cookiename);
+		countbegin = (document.cookie.indexOf("=", index) + 1);
+			countend = document.cookie.indexOf(";", index);
+			if (countend == -1) {
+					countend = document.cookie.length;
+
+			}
+		fulllist = document.cookie.substring(countbegin, countend);
+				newItemList = cookiename;
+		itemlist = 0;
+
+		for (var i = 0; i <= fulllist.length; i++) {
+			if (fulllist.substring(i,i+1) == '[') {
+				itemstart = i+1;
+								thisitem = 1;
+			} else if (fulllist.substring(i,i+1) == ']') {
+				itemend = i;
+				theitem = fulllist.substring(itemstart, itemend);
+				itemlist=itemlist+1;
+								compare=newPath + newImageid + newExtension;
+								compareto=thePath + theImageid + theExtension;
+								if (compare!=compareto) {
+					newItemList = newItemList+'['+theImageid+'|'+thePath+'|'+theExtension+'|]';
+				}
+
+			}else if (fulllist.substring(i,i+1) == '|') {
+				if (thisitem==1) theImageid = fulllist.substring(itemstart, i);
+								if (thisitem==2) thePath = fulllist.substring(itemstart, i);
+								if (thisitem==3) theExtension = fulllist.substring(itemstart, i);
+								thisitem++;
+				itemstart=i+1;
+			}
+		}
+
+
+		index = document.cookie.indexOf(cookiename);
+		document.cookie=""+cookiename+"="+newItemList+"; expires="+expiry.toGMTString()+"; path=/";
+
+newstr=location.search.substring(1);
+if (newstr=='lightbox=view'){location.href = location.href;}
+else if (is_ie5up || is_nav6up){
+div='div';
+layerdiv=div+newPath+newImageid+newExtension;
+countlayerdiv='countlayerdiv';
+totalremove=itemlist-1;
+if (usetextlinks==1){
+		if (is_ie5up){
+			document.all[layerdiv].innerHTML = '<A HREF="javascript:addtolightbox(\''+newImageid+'\',\''+newPath+'\',\''+newExtension+'\')" onMouseOver="Status(\'Add to '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><nobr><'+font_add_link+'>Add to '+lightbox_name+'</font></nobr></a>';
+			document.all[countlayerdiv].innerHTML = '<a class="mainnav" href="'+imagefolio_url+'?lightbox=view">'+lightbox_menu_text+':&nbsp;'+totalremove+'</a>';}
+		else {
+			document.getElementById(layerdiv).innerHTML = '<A HREF="javascript:addtolightbox(\''+newImageid+'\',\''+newPath+'\',\''+newExtension+'\')" onMouseOver="Status(\'Add to '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><nobr><'+font_add_link+'>Add to '+lightbox_name+'</font></nobr></a>';
+			document.getElementById(countlayerdiv).innerHTML = '<'+netscapecountfont+'><a class="mainnav" href="'+imagefolio_url+'?lightbox=view">'+lightbox_menu_text+':&nbsp;'+totalremove+'</a></font>';}
+	}
+else{
+		if (is_ie5up){
+			document.all[layerdiv].innerHTML = '<A HREF="javascript:addtolightbox(\''+newImageid+'\',\''+newPath+'\',\''+newExtension+'\')" onMouseOver="Status(\'Add to '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><img src="'+if_images_directory+'/'+lightbox_no_gif+'" border=0 alt="Add to '+lightbox_name+'"></a>';
+			document.all[countlayerdiv].innerHTML = '<a class="mainnav" href="'+imagefolio_url+'?lightbox=view">'+lightbox_menu_text+':&nbsp;'+totalremove+'</a>';}
+		else {
+			document.getElementById(layerdiv).innerHTML = '<A HREF="javascript:addtolightbox(\''+newImageid+'\',\''+newPath+'\',\''+newExtension+'\')" onMouseOver="Status(\'Add to '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><img src="'+if_images_directory+'/'+lightbox_no_gif+'" border=0 alt="Add to '+lightbox_name+'"></a>';
+			document.getElementById(countlayerdiv).innerHTML = '<'+netscapecountfont+'><a class="mainnav" href="'+imagefolio_url+'?lightbox=view">'+lightbox_menu_text+':&nbsp;'+totalremove+'</a></font>';}
+	}
+}
+		   else {	location.href = location.href;}
+	}
+
+
+
+	function addtolightbox(newImageid, newPath, newExtension) {
+			newlocation = location.href;
+				newlocationarray=newlocation.split("&lightbox");
+				newstr=location.search.substring(1);
+				newarray=newstr.split("=");
+				if (newarray[0]=='action'){
+				lightbox = '&lightbox=1';
+				newtogo=newlocationarray[0]+lightbox;}
+				else {newtogo=location.href;}
+				 index = document.cookie.indexOf(cookiename);
+		countbegin = (document.cookie.indexOf("=", index) + 1);
+			countend = document.cookie.indexOf(";", index);
+			if (countend == -1) {
+					countend = document.cookie.length;
+
+			}
+		fulllist = document.cookie.substring(countbegin, countend);
+				itemlist = 0;
+		for (var i = 0; i <= fulllist.length; i++) {
+			if (fulllist.substring(i,i+1) == '[') {
+				itemstart = i+1;
+				thisitem = 1;
+			} else if (fulllist.substring(i,i+1) == ']') {
+				itemend = i;
+				thequantity = fulllist.substring(itemstart, itemend);
+							itemlist=itemlist+1;}}
+
+if (fulllist.length<3500){closeto=3501;}
+else{
+rawitemsize=fulllist.length/itemlist;
+exactitemsize=Math.round(rawitemsize);
+itemsize=exactitemsize+50;
+closeto=4096-itemsize;}
+if (fulllist.length>closeto){alert('NOTICE:\r\r----------------------------------\r\rYour '+lightbox_text+' contains '+itemlist+' files and is full.\rYou have to remove some files from your '+lightbox_text+' before you can add more.\r\r----------------------------------');}
+else if (itemlist>(lightbox_size-1)){alert('Dear '+theowner+',\r\rYour '+lightbox_text+' contains '+itemlist+' images and is full. You have to remove some files from your '+lightbox_text+' before you can add more. '+lightbox_size_text+'\r\r');}
+else {
+				index = document.cookie.indexOf(cookiename);
+				countbegin = (document.cookie.indexOf("=", index) + 1);
+					countend = document.cookie.indexOf(";", index);
+					if (countend == -1) {
+							countend = document.cookie.length;
+					}
+						document.cookie=""+cookiename+"="+document.cookie.substring(countbegin, countend)+"["+newImageid+"|"+newPath+"|"+newExtension+"|]"+"; expires="+expiry.toGMTString()+"; path=/";
+if (is_ie5up || is_nav6up){
+div='div';
+layerdiv=div+newPath+newImageid+newExtension;
+countlayerdiv='countlayerdiv';
+totaladd=itemlist+1;
+if (usetextlinks==1){
+		if (is_ie5up){
+			document.all[layerdiv].innerHTML = '<A HREF="javascript:removeItem(\''+newImageid+'\',\''+newPath+'\',\''+newExtension+'\')" onMouseOver="Status(\'Remove from '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><nobr><'+font_remove_link+'>Remove from '+lightbox_name+'</font></nobr></a>';
+			document.all[countlayerdiv].innerHTML = '<a class="mainnav" href="'+imagefolio_url+'?lightbox=view">'+lightbox_menu_text+':&nbsp;'+totaladd+'</a>';}
+		else {
+			document.getElementById(layerdiv).innerHTML = '<A HREF="javascript:removeItem(\''+newImageid+'\',\''+newPath+'\',\''+newExtension+'\')" onMouseOver="Status(\'Remove from '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><nobr><'+font_remove_link+'>Remove from '+lightbox_name+'</font></nobr></a>';
+			document.getElementById(countlayerdiv).innerHTML = '<'+netscapecountfont+'><a class="mainnav" href="'+imagefolio_url+'?lightbox=view">'+lightbox_menu_text+':&nbsp;'+totaladd+'</a></font>';}
+	}
+else{
+		if (is_ie5up){
+			document.all[layerdiv].innerHTML = '<A HREF="javascript:removeItem(\''+newImageid+'\',\''+newPath+'\',\''+newExtension+'\')" onMouseOver="Status(\'Remove from '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><img src="'+if_images_directory+'/'+lightbox_yes_gif+'" border=0 alt="Remove from '+lightbox_name+'"></a>';
+			document.all[countlayerdiv].innerHTML = '<a class="mainnav" href="'+imagefolio_url+'?lightbox=view">'+lightbox_menu_text+':&nbsp;'+totaladd+'</a>';}
+		else {
+			document.getElementById(layerdiv).innerHTML = '<A HREF="javascript:removeItem(\''+newImageid+'\',\''+newPath+'\',\''+newExtension+'\')" onMouseOver="Status(\'Remove from '+lightbox_name+'\');return true" onMouseOut="Status(\'\');return true"><img src="'+if_images_directory+'/'+lightbox_yes_gif+'" border=0 alt="Remove from '+lightbox_name+'"></a>';
+			document.getElementById(countlayerdiv).innerHTML = '<'+netscapecountfont+'><a class="mainnav" href="'+imagefolio_url+'?lightbox=view">'+lightbox_menu_text+':&nbsp;'+totaladd+'</a></font>';}
+	}
+}
+		   else {	location.href = location.href;}}
+}
+
+
+function Status(text){window.status = text;}
+
+function clearLightbox() {
+		if (confirm('Are you sure you wish to clear the '+lightbox_text+'')) {
+			index = document.cookie.indexOf(cookiename);
+			document.cookie=""+cookiename+"=.; path=/";
+			location.href = location.href;
+		}
+	}
+
