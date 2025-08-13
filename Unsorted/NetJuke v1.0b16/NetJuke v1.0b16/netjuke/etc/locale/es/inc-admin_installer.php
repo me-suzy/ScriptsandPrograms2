6013fa41a93e372f19b9ef1b99f6583de46fca4f
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:38 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_installer.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_installer.php
@@ -0,0 +1,43 @@
+<?php
+
+##################################################
+
+# UNUSED SINCE 1.0b8, NOW /installer/installer.php
+
+##################################################
+
+define( "ADMINST_ERR_VERSION", "Este instalador no puede ser usado con el objetivo\\nde copiar el Netjuke. Con Versiones no compatibles. \\Por favor consiga la última versión e inténtelo de nuevo." );
+
+define( "ADMINST_ERR_FSPATH_1", "Por favor, entre una ruta para el sistema de archivo." );
+define( "ADMINST_ERR_FSPATH_2", "La ruta no contiene un sistema de archivo ://" );
+define( "ADMINST_ERR_WEBPATH_1", "Por favor, entre una ruta a Web." );
+define( "ADMINST_ERR_WEBPATH_2", "La ruta a Web debe contener http:// o https://" );
+
+define( "ADMINST_ERR_DBTYPE", "Por favor, entre el tipo de DB." );
+define( "ADMINST_ERR_DBHOST", "Por favor, entre el servidor de DB." );
+define( "ADMINST_ERR_DBNAME", "por favor, entre el nombre de la DB." );
+define( "ADMINST_ERR_DBCONN", "No se puede conectar a la base de datos utilizando la información suministrada." );
+
+define( "ADMINST_ERR_EMAIL", "Por favor, entre la dirección de correo del administrador." );
+define( "ADMINST_ERR_PASS", "Por favor, seleccione un password para el administrador." );
+
+define( "ADMINST_ERR_STREAM", "Por favor, entre un servidor de streaming." );
+
+define( "ADMINST_ERR_NOSQL", "A su instalación le falta algunos componentes críticos." );
+
+define( "ADMINST_ERR_DENIED_1", "- Lo siento, pero no se puede usar este instalador con la copia de NetJuke por que está actualmente funcionando.\\n- Por favor, use el proceso de actualización, Recomendado." );
+define( "ADMINST_ERR_DENIED_2", "- La herramienta de actualización no puede usarse con la copia de NetJuke.\\n- La base de datos nunca será generada (necesita instalar)." );
+define( "ADMINST_ERR_DENIED_3", "La ruta del sistema de archivo no parece ser valido." );
+define( "ADMINST_ERR_DENIED_4", "Acceso denegado: La cuenta o el password de administrador digitado no coincide con la cuenta de un administrador existente." );
+
+define( "ADMINST_ERR_EXEC", "Problemas ejecutando la generación de la parte de sql.\\nPor favor, intentelo de nuevo." );
+
+define( "ADMINST_FILEINFO_1", "Creado" );
+define( "ADMINST_FILEINFO_2", "Desde" );
+define( "ADMINST_FILEINFO_3", "Guardar Como" );
+define( "ADMINST_FILEINFO_4", "Después renombre el directorio temporal de instalación por seguridad" );
+define( "ADMINST_FILEINFO_5", "Guardado satisfactoriamente. Ud. puede ahora acceder a la nueva versión" );
+
+##################################################
+
+?>
