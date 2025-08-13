bb95dfbe6a6e0d38a3faf5ec82ca156e4843e92b
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:31 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.upload.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.upload.php
@@ -0,0 +1,52 @@
+<?php
+$L = array(
+
+
+"path_does_not_exist" =>
+"Il percorso specificato non esiste",
+
+"file_exceeds_ini_limit" =>
+"Il file caricato eccede la dimensione massima indicata nel file di configurazione",
+
+"file_partially_uploaded" =>
+"Il file è stato caricato solo parzialmente",
+
+"no_file_selected" =>
+"Non hai selezionato alcun file da caricare",
+
+"file_upload_error" =>
+"Si è verificato un errore di tipo sconosciuto.",
+
+"invalid_filetype" =>
+"Stai tentando di caricare un tipo di file non ammesso (birba che sei)",
+
+"invalid_filesize" =>
+"Stai tentando di caricare un file che eccede le dimensioni consentite",
+
+"invalid_dimensions" =>
+"Stai tentando di caricare un file che eccede l&#39;altezza o la larghezza consentite",
+
+"upload_error" =>
+"Si è verificato un problema durante l&#39;invio del file",
+
+"copy_error" =>
+"Si è verificato un problema durante la sovrascrittura del file",
+
+"unsupported_protocol" =>
+"Il tuo server non supporta il protocollo di manipolazione immagini indicato nelle preferenze.",
+
+"jpg_or_png_required" =>
+"Il protocollo di manipolazione indicato nelle preferenze opera soltanto con immagini JPEG o PNG.",
+
+"jpg_gif_png_required" =>
+"Il protocollo di manipolazione indicato nelle preferenze opera soltanto con immagini GIF, JPEG o PNG.",
+
+"libpath_invalid" =>
+"Il percorso verso la libreria di immagini non è corretto. Specifica il percorso esatto nelle preferenze.",
+
+"image_resize_failed" =>
+"Ridimensionamento dell&#39;immagine fallito. Verifica il supporto del protocollo indicato e la correttezza del percorso vero la libreria di immagini.",
+
+''=>''
+);
+?>
