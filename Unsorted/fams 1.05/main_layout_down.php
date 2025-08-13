c28b4e086fb51ff2ba3c35a7b9164b2fbb246d1d
me-suzy <me.suzana@gmail.com>
2025-08-13 04:33:56 +0300

    Add Unsorted/fams 1.05/main_layout_down.php

---
+++ Unsorted/fams 1.05/main_layout_down.php
@@ -0,0 +1,53 @@
+<?php
+
+/* ------------------------------------------------------------ */
+/*								*/
+/*	File Area Management System (FAMS)			*/
+/*								*/
+/*	Copyright (c) 2001 by Bastian 'Buddy' Grimm		*/
+/*	Autor: Bastian Grimm					*/
+/*	Publisher: [ BG-Studios ]				*/
+/*	eMail: bastian@bg-studios.de				*/
+/*	WebSite: http://www.bg-studios.de			*/
+/*	Date: 06.05.01		Version: Main Layout Down 1.00	*/
+/*	Last changed: 13.05.01					*/
+/*								*/
+/* ------------------------------------------------------------	*/
+
+require("./config.php");
+require("./copyright.php");
+
+$pagetyp = "main";
+
+?>
+
+
+<BR>
+
+</TD>
+</TR>
+
+<TR>
+<TD WIDTH=450 CLASS=main-headline HEIGHT=18>
+<P>
+<IMG BORDER=0 SRC=./images/group-link.gif>Database powered by [ <A HREF=http://www.mellesoft.com TARGET=new>Mellesoft</A> ]
+<BR>
+<?php print($copy1); ?>
+
+</TD>
+</TR>
+</TABLE>
+
+
+</TD>
+</TR> 
+<TR>
+<TD COLSPAN=2 HEIGHT=10 CLASS=nav-background>
+</TD>
+</TR>
+</TABLE>
+
+
+</CENTER>
+</BODY>
+</HTML>
