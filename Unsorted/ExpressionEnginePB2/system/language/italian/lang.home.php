7b16b8c2b745aceaa343f244cbaa9cd6746a11e5
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:55 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.home.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.home.php
@@ -0,0 +1,105 @@
+<?php
+$L = array(
+
+"total_validating_comments" =>
+"Comments Awaiting Validation",
+
+"cp_home" =>
+"My Home Page",
+
+"current_user" =>
+"Utente attuale:",
+
+"system_status" =>
+"Stato sistema",
+
+"offline" =>
+"Offline",
+
+"online" =>
+"Online",
+
+"member_search" =>
+"Ricerca utente",
+
+"search_instructions" =>
+"Specifica termini interi o parziali",
+
+"member_group" =>
+"Categoria utente",
+
+"search_by" =>
+"Campo ricerca",
+
+"screen_name" =>
+"Screen-name",
+
+"email_address" =>
+"Indirizzo e-mail",
+
+"url" =>
+"URL",
+
+"notepad" =>
+"Notepad",
+
+"site_statistics" =>
+"Statistiche del sito",
+
+"value" =>
+"Valore",
+
+"total_members" =>
+"Utenti totali",
+
+"total_validating_members" =>
+"Utenti in attesa di attivazione",
+
+"total_entries" =>
+"Interventi totali",
+
+"total_comments" =>
+"Commenti totali",
+
+"total_trackbacks" =>
+"Trackbacks totali",
+
+"most_recent_entries" =>
+"Interventi più recenti",
+
+"most_recent_comments" =>
+"Commenti/TB più recenti",
+
+"no_comments" =>
+"Non ci sono commenti/TB attuali",
+
+"no_entries" =>
+"Non ci sono interventi attuali",
+
+"entry_title" =>
+"Titolo intervento",
+
+"comments" =>
+"Com.ti",
+
+"recent_members" =>
+"Nuovi iscritti",
+
+"join_date" =>
+"Data di iscrizione",
+
+"total_hits" =>
+"Hits totali",
+
+"demo_expiration" =>
+"Il tuo account demo scadrà entro:",
+
+"install_lock_warning" =>
+"Attenzione: il file di installazione è ancora sul server.",
+
+"install_lock_removal" =>
+"Per motivi di sicurezza, cancella il file install.php dal server utilizzando un programma FTP.",
+
+''=>''
+);
+?>
