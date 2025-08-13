5d07a969eb8e41565ae783f237ae4e51e9bca975
me-suzy <me.suzana@gmail.com>
2025-08-13 06:30:11 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/en/buddy.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/en/buddy.php
@@ -0,0 +1,30 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[BUDDY_Operator] = "Operator";
+
+$LOCALE[BUDDY_Away] = "Away";
+
+$LOCALE[BUDDY_Buddy] = "Buddy";
+
+$LOCALE[BUDDY_Ignored] = "Ignored";
+
+$LOCALE[BUDDY_Admin] = "Administrator";
+
+$LOCALE[BUDDY_Online] = "Online";
+
+$LOCALE[BUDDY_Registered] = "Registered";
+
+$LOCALE[BUDDY_Room_List] = "Room Users \ Buddy List";
+
+
+?>
