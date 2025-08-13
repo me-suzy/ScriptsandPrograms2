2c6573fee4a440f8ba678fe2e55b49fc6039b52a
me-suzy <me.suzana@gmail.com>
2025-08-13 05:10:37 +0300

    Add Unsorted/hitweb-3.0/webalizer/index.php

---
+++ Unsorted/hitweb-3.0/webalizer/index.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<!-- Information sur HITWEB -->
+    <meta name="hitweb" content="version 3.0">
+
+<!-- Information pour les ROBOTS -->
+    <meta name="revisit-after" content="30 days">
+
+<!-- Information sur les PAGES HTML -->
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta name="Content-language" content="fr">
+
+<!-- Information sur l'AUTEUR -->
+    <meta name="Author-Corporate" content="Brian FRAVAL">
+    <meta name="copyright" content="&copy; 2001 - Brian FRAVAL">
+    <meta name="Author" lang="fr" content="Brian FRAVAL">
+    <meta name="Reply-to" content="webmaster@hitweb.org">
+
+<!-- Information sur le SITE -->
+    <meta name="base" content="http://www.hitweb.org/">
+    <meta name="Identifier-URL" content="http://www.hitweb.org/">
+    <meta name="Date-Creation-yyyymmdd" content="20000802">
+    <meta name="Date-Revision-yyyymmdd" content="20010620">
+
+<!-- Mots clés pour les ANGLAIS -->
+    <meta name="title" lang="en" content="The best of the world wide web">
+    <meta name="Description" lang="en" content="">
+    <meta name="keywords" lang="en" content="best, site, web, internet, hitweb, brian, fraval, decaen, duhaut, gpl, free">
+    <meta name="Category" content="Internet">
+
+<!-- Mots clés pour les FRANCAIS -->
+    <meta name="title" lang="fr" content="Le classement des meilleurs sites web">
+    <meta name="Description" lang="fr" content="Classement des meilleurs sites internet par thèmes. Ce classement est dynamique par rapport au nombre de 
+point de chaques sites référencés dans HITWEB">
+    <meta name="keywords" lang="fr" content="annuaire, moteur de recherche, meilleur, site, web, internet, hitweb, brian, fraval, référencement, decaen, 
+duhaut, classement, gpl, libre">
+    <meta name="Category" content="Internet">
+
+<!-- Feuille de style -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <title>HITWEB : Le classement des meilleurs sites 
+web.................................................................................</title>
+  </head>
+<body>
+
+<h1>Analyser les fichiers log de Apache</h1>
+
+Je vous conseil l'utilisation de <b>WEBALIZER</b>, qui est vraiment suffisant...
+pour l'analyse régulier d'un site personnel.
+<p>
+
+Voici l'adresse du site webalizer :<br>
+<a href='http://www.mrunix.net/webalizer/'>http://www.mrunix.net/webalizer/</a>
+
+<body>
+</html>
