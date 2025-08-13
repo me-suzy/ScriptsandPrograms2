1168cfd1e65f44a012a12e1c49845162c4b6cdbb
me-suzy <me.suzana@gmail.com>
2025-08-13 07:32:55 +0300

    Add Unsorted/p4CMS.v1.05.Nullified/p4cms/dokument_vorschau.php

---
+++ Unsorted/p4CMS.v1.05.Nullified/p4cms/dokument_vorschau.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" language="JavaScript">
+<!--
+var header = opener.document.dokument.<?=$_REQUEST[VAL];?>.value;
+document.writeln("" + opener.document.dokument.<?=$_REQUEST[VAL];?>.value + "");
+//-->
+</script> 
