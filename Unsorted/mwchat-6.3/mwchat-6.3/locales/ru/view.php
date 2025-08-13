59cb7bd63f9b8bf736da54c9a61908bde5504e76
me-suzy <me.suzana@gmail.com>
2025-08-13 06:35:11 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/view.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/view.php
@@ -0,0 +1,21 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[VIEW_Wait] = "ðÏÖÁÌÕÊÓÔÁ, ÐÏÄÏÖÄÉÔÅ, ÐÏËÁ ×ÁÛ ÆÁÊÌ ÎÁÞÎÅÔ ÚÁÇÒÕÖÁÔØÓÑ.";
+
+$LOCALE[VIEW_Downloading] = "úÁÇÒÕÖÁÅÔÓÑ";
+
+$LOCALE[VIEW_Click] = "îÁÖÍÉÔÅ ÓÀÄÁ, ÅÓÌÉ Õ ×ÁÓ ÎÅ ÓÒÁÂÏÔÁÌ Á×ÔÏÍÁÔÉÞÅÓËÉÊ ÚÁÐÕÓË.";
+
+$LOCALE[VIEW_Error] = "æÁÊÌ, ËÏÔÏÒÙÊ ×Ù ÐÙÔÁÅÔÅÓØ ÚÁÇÒÕÚÉÔØ, ÎÅÄÏÓÔÕÐÅÎ.";
+
+?>
