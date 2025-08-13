a387c3e19e85fa9d2aca55ffc8e4c8ad8567aa5f
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:45 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.cp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.cp.php
@@ -0,0 +1,157 @@
+<?php
+$L = array(
+
+
+"consult_user_guide" =>
+"Consultar Guia de Usuario",
+
+"powered_by" =>
+"Autorizado Por",
+
+"control_panel" =>
+"Panel de Control",
+
+"main_menu" =>
+"Centro de Panel de Control",
+
+"username" =>
+"Usuario",
+
+"password" =>
+"Contraseña",
+
+"publish" =>
+"Publicar",
+
+"edit" =>
+"Editar",
+
+"design" =>
+"Plantillas",
+
+"communicate" =>
+"Mensajería",
+
+"modules" =>
+"Módulos",
+
+"admin" =>
+"Admin",
+
+"my_account" =>
+"Mi Cuenta",
+
+"user_account" =>
+"Cuenta del Miembro",
+
+"user_guide" =>
+"Guia de usuario",
+
+"logout" =>
+"Desconectar",
+
+"copyright" =>
+"Copyright",
+
+"rights_reserved" =>
+"Todos los derechos reservados",
+
+"page_rendered" =>
+"Guión ejecutado en %x segundos",
+
+"queries_executed" =>
+"se han usado %x peticiones SQL",
+
+"build" =>
+"Build:",
+
+"forgot_password" =>
+"¿Olvidaste tu contraseña?",
+
+"e_no_css" =>
+"No se ha podido abrir la hoja de estilo CSS solicitada.",
+
+"unauthorized" =>
+"No autorizado",
+
+"unauthorized_access" =>
+"No estás autorizado para acceder a esta página",
+
+"be_careful" =>
+"¡CUIDADO!",
+
+"action_can_not_be_undone" =>
+"ESTA ACCION NO PUEDE DESHACERSE",
+
+"back" =>
+"Atrás",
+
+"no" =>
+"No",
+
+"yes" =>
+"Sí",
+
+"all" =>
+"Todo",
+
+"any" =>
+"Cualquiera",
+
+"required_fields" =>
+"Indica campos requeridos",
+
+"please_consult_manual" =>
+"Por favor, consulta la guia de usuario",
+
+"login" =>
+"Conectar",
+
+"forgotten_password" =>
+"Contraseña olvidada",
+
+"submit_email_address" =>
+"Envia tu dirección de correo",
+
+"submit" =>
+"Enviar",
+
+"preview" =>
+"Vista previa",
+
+"update" =>
+"Actualizar",
+
+"delete" =>
+"Borrar",
+
+"save_changes" =>
+"Guardar cambios",
+
+"clear_logs" =>
+"Borrar ficheros de registro",
+
+"member_id" =>
+"ID del miembro",
+
+"ip_address" =>
+"Dirección IP",
+
+"date" =>
+"Fecha",
+
+"action" =>
+"Acción",
+
+"member_logged_in" =>
+"Conectado",
+
+"member_logged_out" =>
+"Desconectado",
+
+"cleared_logs" =>
+"Ficheros de registro borrados",
+
+''=>''
+);
+?>
