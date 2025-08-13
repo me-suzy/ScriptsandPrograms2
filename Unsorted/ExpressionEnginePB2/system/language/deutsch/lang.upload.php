3780d9eb7d174a225412d54bb2550782d5604285
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:42 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.upload.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.upload.php
@@ -0,0 +1,53 @@
+<?php
+
+$L = array(
+
+
+"path_does_not_exist" =>
+"Der angegebene Pfad existiert nicht.",
+
+"file_exceeds_ini_limit" =>
+"Die hochgeladene Datei ist gr&ouml;&szlig;er als in Ihrer PHP-Konfiguration erlaubt.",
+
+"file_partially_uploaded" =>
+"Die Datei wurde nur zum Teil hochgeladen.",
+
+"no_file_selected" =>
+"Sie haben keine Datei zum Hochladen ausgew&auml;hlt.",
+
+"file_upload_error" =>
+"Ein Fehler unbekannter Art ist aufgetreten.",
+
+"invalid_filetype" =>
+"Es ist nicht erlaubt, eine Datei von diesem Dateityp hochzuladen.",
+
+"invalid_filesize" =>
+"Die Datei, die Sie hochzuladen versuchen, ist gr&ouml;&szlig;er als erlaubt.",
+
+"invalid_dimensions" =>
+"Die Grafik, die Sie hochzuladen versuchen, &uuml;berschreitet die h&ouml;chstm&ouml;gliche H&ouml;he oder Breite.",
+
+"upload_error" =>
+"Beim Hochladen Ihrer Datei ist ein Problem aufgetreten.",
+
+"copy_error" =>
+"Beim Ersetzen der Datei ist ein Problem aufgetreten.",
+
+"unsupported_protocol" =>
+"Ihr Server unterst&uuml;tzt die in Ihren Einstellungen angegebene Bildgr&ouml;&szlig;enanpassung nicht.",
+
+"jpg_or_png_required" =>
+"Die in Ihren Einstellungen angegebene Bildgr&ouml;&szlig;enanpassung funktioniert nur mit Grafiken im JPEG- oder PNG-Format.",
+
+"jpg_gif_png_required" =>
+"Die in Ihren Einstellungen angegebene Bildgr&ouml;&szlig;enanpassung funktioniert nur mit Grafiken im GIF-, JPEG- oder PNG-Format.",
+
+"libpath_invalid" =>
+"Der Pfad zu Ihrer Bibliothek ist nicht korrekt. Bitte geben Sie den korrekten Pfad in Ihren Grafik-Einstellungen an.",
+
+"image_resize_failed" =>
+"Bildgr&ouml;&szlig;enanpassung fehlgeschlagen. Bitte &uuml;berpr&uuml;fen Sie, dass Ihr Server das angegebene Anpassungsprotokoll unterst&uuml;tzt, und dass der Pfad richtig gesetzt ist.",
+
+
+// IGNORE
+''=>'');?>
