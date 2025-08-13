399327102ff523a03636ae316ef21ae1112096cd
me-suzy <me.suzana@gmail.com>
2025-08-13 06:39:02 +0300

    Add Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/en/lng_idx.php

---
+++ Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/en/lng_idx.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['page_sub_title']	    =	'Member area';
+$lang['submit_login']	    =	'LogIn';
+$lang['username']	    =	'UserName';
+$lang['password']	    =	'PassWord';
+$lang['remember_login']	    =	'Remember me?';
+$lang['logout']	            =	'LogOut';
+$lang['welcome']            =	'Welcome ';
+$lang['last_visit']	    =	'Your last visit was ';
+$lang['menu_edit_profile']  =   'Edit Your profile';
+$lang['menu_edit_password'] =   'Change password';
+$lang['menu_accessing_dirs']=   'Accessible dirs';
+$lang['menu_anounces']      =   'Our anounces';
+$lang['anounces_header']    =   'Our anounces';
+$lang['no_anounces']        =   'No anounces';
+$lang['more_anounces_link'] =   'More anounces';
+$lang['profile_header']     =   'Your profile';
+$lang['profile_saved']      =   'Your profile has been updated. Please wait while we redirect You ...';
+$lang['save_profile']       =   'update my profile';
+$lang['regdate']            =   'Registration date';
+$lang['expire']             =   'Expiration date';
+$lang['interface_lang']     =   'Interface language';
+$lang['change_passwd_header'] = 'Change Your password';
+$lang['old_password']       =   'Old password';
+$lang['new_password']       =   'New password';
+$lang['new_password_again'] =   'Repeat new password';
+$lang['create_random_passwd'] = 'Create password for me';
+$lang['change_my_passwd']   =   'change my password';
+$lang['passwd_changed']     =   'Your password has been updated.<br>New password has been sent to Your registered Email.<br> Please wait while we redirect You ...';
+$lang['passwd_creation_message'] = 'If You want new password to be generated automaticaly, check box named "create password for me". New password will be sent You via Email.';
+$lang['pass_incorrect']     = 'Sorry, incorrect password';
+$lang['pass_missmatch']     = 'Confirmation password missmatch';
+$lang['accessing_dirs_header'] = 'Your accessible dirs';
+
+?>
