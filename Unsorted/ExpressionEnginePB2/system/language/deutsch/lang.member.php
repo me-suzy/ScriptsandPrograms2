6ab87f0f8491ae3c31e2aa842668feec5697897e
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:13 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.member.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.member.php
@@ -0,0 +1,390 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"member_module_name" =>
+"Mitglieder",
+
+"member_module_description" =>
+"Mitgliedsmanagementsystem",
+
+//----------------------------------------
+
+"mbr_menu" =>
+"Menu",
+
+"mbr_show" =>
+"Anzeigen",
+
+"mbr_sort" =>
+"Sortieren",
+
+"mbr_order" =>
+"Reihenfolge",
+
+"mbr_rows" =>
+"Zeilen",
+
+"mbr_my_account" =>
+"Mein Account",
+
+"mbr_logged_in_as" =>
+"Eingeloggt als:",
+
+"mbr_logout" =>
+"Logout",
+
+"mbr_member_registration" =>
+"Anmeldung",
+
+"mbr_forgotten_password" =>
+"Passwort vergessen",
+
+"mbr_memberlist" =>
+"Mitgliederliste",
+
+"notice" =>
+"Hinweis",
+
+"mbr_registration_not_allowed" =>
+"Zur Zeit werden keine neuen Anmeldungen angenommen.",
+
+"mbr_login" =>
+"Login",
+
+"mbr_form_empty" =>
+"Sie m&uuml;ssen Usernamen und Passwort angeben.",
+
+"mbr_account_not_active" =>
+"Ihr Account ist noch nicht aktiviert.",
+
+"mbr_you_are_logged_in" =>
+"Sie sind jetzt eingeloggt.",
+
+"mbr_you_are_logged_out" =>
+"Sie sind jetzt ausgeloggt.",
+
+"mbr_no_reset_id" =>
+"Die eingegebene ID-Nummer scheint ung&uuml;ltig zu sein. Bitte pr&uuml;fen Sie den Link, dem Sie gefolgt sind.",
+
+"mbr_id_not_found" =>
+"Die eingegebene ID-Nummer ist nicht in der Datenbank zu finden.",
+
+"mbr_username" =>
+"Username",
+
+"mbr_password" =>
+"Passwort",
+
+"mbr_submit" =>
+"Abschicken",
+
+"mbr_update" =>
+"Aktualisieren",
+
+"mbr_password_confirm" =>
+"Passwortbest&auml;tigung",
+
+"mbr_auto_login" =>
+"Autologin bei k&uuml;nftigen Besuchen",
+
+"mbr_show_name" =>
+"Meinen Namen in der Online-Liste anzeigen",
+
+"mbr_forgot_password" =>
+"Passwort vergessen?",
+
+"mbr_email_address" =>
+"Emailadresse",
+
+"mbr_your_email" =>
+"Ihre Emailadresse",
+
+"mbr_email_short" =>
+"Email",
+
+"mbr_icq_console" =>
+"ICQ Console",
+
+"mbr_aim_console" =>
+"AOL IM",
+
+"mbr_aol_short" =>
+"AOL",
+
+"mbr_icq_short" =>
+"ICQ",
+
+"mbr_www" =>
+"URL",
+
+"mbr_msn_short" =>
+"MSN",
+
+"mbr_yahoo_short" =>
+"Yahoo",
+
+"mbr_screen_name" =>
+"Screen-Name",
+
+"mbr_name" =>
+"Name",
+
+"mbr_screen_name_exp" =>
+"Wenn Sie dieses Feld frei lassen, wird Ihr Screen-Name der gleiche wie Ihr Username sein.",
+
+"mbr_your_url" =>
+"URL Ihrer Web Site",
+
+"mbr_field_required" =>
+"Das folgende Feld muss ausgef&uuml;llt werden:",
+
+"mbr_username_length" =>
+"Usernamen m&uuml;ssen mindestens %x Zeichen lang sein.",
+
+"mbr_password_length" =>
+"Passw&ouml;rter m&uuml;ssen mindestens %x Zeichen lang sein",
+
+"mbr_registration_complete" =>
+"Anmeldung erfolgreich",
+
+"mbr_registration_completed" =>
+"Ihre Anmeldung war erfolgreich.",
+
+"mbr_admin_will_activate" =>
+"Ein Administrator wird Ihren Account aktivieren und Sie dann benachrichtigen.",
+
+"mbr_membership_instructions_email" =>
+"Sie erhalten gleich eine Email mit Anweisungen, wie Sie Ihren Account aktivieren k&ouml;nnen.",
+
+"mbr_membership_instructions_cont" =>
+"Bitte, sehen Sie in Ihrer Mailbox nach und folgen Sie den Anweisungen in der Nachricht.",
+
+"mbr_your_are_logged_in" =>
+"Sie sind jetzt eingeloggt und k&ouml;nnen Ihren neuen Account nutzen.",
+
+"mbr_activation" =>
+"Accountaktivierung",
+
+"mbr_problem_activating" =>
+"Ung&uuml;ltige Anforderung.",
+
+"mbr_activation_success" =>
+"Ihr Account wurde aktiviert.",
+
+"mbr_may_now_log_in" =>
+"Sie koennen sich jetzt einloggen.",
+
+"mbr_not_allowed_to_view_profiles" =>
+"Sie d&uuml;rfen sich keine Mitgliedsprofile ansehen.",
+
+"mbr_member_profile" =>
+"Mitgliedsprofil",
+
+"mbr_member_group" =>
+"Mitgliedsgruppe:",
+
+"mbr_last_visit" =>
+"Letzter Besuch",
+
+"mbr_most_recent_entry" =>
+"Neuester Eintrag",
+
+"mbr_most_recent_comment" =>
+"Neuester Kommentar",
+
+"mbr_required_fields" =>
+"Bezeichnet Pflichtfelder",
+
+"mbr_submit" =>
+"Abschicken",
+
+"mbr_join_date" =>
+"Beitritt",
+
+"mbr_total_entries" =>
+"Anzahl der Eintr&auml;ge",
+
+"mbr_total_comments" =>
+"Anzahl der Kommentare",
+
+"mbr_comments" =>
+"Kommentare",
+
+"mbr_member_timezone" =>
+"Zeitzone des Mitglieds",
+
+"mbr_member_local_time" =>
+"Ortszeit des Mitglieds",
+
+"mbr_email" =>
+"Email",
+
+"mbr_url" =>
+"Homepage",
+
+"mbr_location" =>
+"Ort",
+
+"mbr_birthday" =>
+"Geburtstag",
+
+"mbr_your_stats" =>
+"Ihre Statistik",
+
+"mbr_edit_your_profile" =>
+"Profil bearbeiten",
+
+"mbr_profile_homepage" =>
+"Profil-Homepage",
+
+"mbr_required_fields" =>
+"Bezeichnet Pflichtfelder",
+
+"mbr_profile_has_been_updated" =>
+"Ihr Profil wurde aktualisiert.",
+
+"mbr_email_has_been_updated" =>
+"Ihre Email-Einstellungen wurden aktualisiert.",
+
+"mbr_email_updated" =>
+"Email-Einstellungen aktualisiert",
+
+"username_disallowed" =>
+"Der Administrator erlaubt das &Auml;ndern von Usernamen nicht.",
+
+"mbr_settings_updated" =>
+"Ihre Einstellungen wurden aktualisiert.",
+
+"mbr_notepad_updated" =>
+"Ihr Notizblock wurde aktualisiert.",
+
+"mbr_localization_settings_updated" =>
+"Ihre lokalen Einstellungen wurden aktualisiert.",
+
+"mbr_terms_of_service" =>
+"Nutzungsbedingungen",
+
+"mbr_i_accept" =>
+"Ich stimme den Nutzungsbedingungen zu.",
+
+"mbr_terms_of_service_required" =>
+"Sie m&uuml;ssen \"Ich stimme den Nutzungsbedingungen zu\" ankreuzen.",
+
+"mbr_custom_field_empty" =>
+"Das folgende Feld muss ausgef&uuml;llt werden:",
+
+"mbr_all_member_groups" =>
+"Alle Mitgliedsgruppen",
+
+"mbr_member_name" =>
+"Mitlgiedsname",
+
+"mbr_ascending" =>
+"Aufsteigend",
+
+"mbr_descending" =>
+"Absteigend",
+
+"mbr_back_to_login" =>
+"Zur&uuml;ck zu Login",
+
+"mbr_back_to_main" =>
+"ZUr&uuml;ck zur Hauptseite",
+
+"mbr_aol_im" =>
+"AOL Instant Messenger",
+
+"mbr_icq" =>
+"ICQ Number",
+
+"mbr_yahoo" =>
+"Yahoo Messenger",
+
+"mbr_msn" =>
+"MSN Messenger",
+
+"mbr_bio" =>
+"Bio",
+
+"mbr_interests" =>
+"Interessen",
+
+"mbr_occupation" =>
+"Beruf",
+
+"mbr_email_member" =>
+"Emailkonsole",
+
+"mbr_subject" =>
+"Email-Betreff",
+
+"mbr_message" =>
+"Email-Nachricht",
+
+"mbr_close_window" =>
+"Fenster schlie&szlig;en",
+
+"mbr_recipient" =>
+"Email-Empf&auml;nger:",
+
+"mbr_email_not_accepted" =>
+"Dieses Mitglied nimmt zur Zeit keine Email an.",
+
+"mbr_email_disclaimer" =>
+"Beim Senden dieser Mail wird Ihre Emailadresse dem Empf&auml;nger mitgeteilt.",
+
+"mbr_email_logged" =>
+"Hinweis: Email wird protokolliert und kann vom Administrator gelesen werden.",
+
+"mbr_missing_fields" =>
+"Alle Felder m&uuml;ssen ausgef&uuml;llt werden.",
+
+"mbr_send_email" =>
+"Email abschicken",
+
+"mbr_email_forwarding" =>
+"Diese Mail wurde Ihnen zugeschickt durch Ihren Account bei:",
+
+"mbr_email_forwarding_cont" =>
+"Wenn Sie keine weiteren Nachrichten auf diesem Weg erhalten wollen, koennen Sie das in Ihren Accounteinstellungen festlegen.",
+
+"mbr_email_error" =>
+"Beim Versand Ihrer Nachricht ist ein Fehler aufgetreten.",
+
+"mbr_good_email" =>
+"Ihre Email wurde verschickt.",
+
+"mbr_send_self_copy" =>
+"Ich will eine Kopie dieser Email.",
+
+"mbr_icq_number" =>
+"ICQ-Nummer:",
+
+"mbr_icq_recipient" =>
+"Empf&auml;nger:",
+
+"mbr_icq_subject" =>
+"Betreff",
+
+"mbr_icq_message" =>
+"Nachricht",
+
+"mbr_not_allowed_to_use_email_console" =>
+"Sie d&uuml;rfen die Emailkonsole nicht benutzen.",
+
+"mbr_email_timelock_not_expired" =>
+"Sie d&uuml;rfen nur alle %x Minuten eine Mail verschicken.",
+
+"mbr_you_are_registered" =>
+"Sie sind bereits registriert und eingeloggt.",
+
+
+// END
+''=>''
+);
+?>
