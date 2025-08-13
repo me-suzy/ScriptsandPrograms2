b0de33057fe9454b9626778bc11d0ef76c25f0ef
me-suzy <me.suzana@gmail.com>
2025-08-13 07:07:08 +0300

    Add Unsorted/Olate.Download.v2.translated.by.[GTT]/html/languages/russian/main.php

---
+++ Unsorted/Olate.Download.v2.translated.by.[GTT]/html/languages/russian/main.php
@@ -0,0 +1,65 @@
+<?php
+/***************************************************************************
+ *                      Olate Download v2 - Download Manager
+ *
+ *                           http://www.olate.com
+ *                            -------------------
+ *   author                : David Mytton
+ *   copyright             : (C) Olate 2003 
+ *
+ *   Support for Olate scripts is provided at the Olate website. Licensing
+ *   information is available in the license.htm file included in this
+ *   distribution and on the Olate website.                  
+ *   
+ *   
+ *   
+ *   Fully translated by GTT                  
+ ***************************************************************************/
+
+// Define admin texts - see config.php for author information
+$language = array();
+$language['button_search']					= 'Ïîèñê';
+$language['button_rate']					= 'Îöåíèòü';
+$language['credits_poweredby']				= 'Èñïîëüçóåòñÿ ñêðèïò Olate Download ';
+$language['description_details']			= 'Äîïîëíèòåëüíàÿ èíôîðìàöèÿ î ôàéëå ïîêàçàíà íèæå.';
+$language['description_download_begin']		= 'Çàêà÷êà íà÷í¸òñÿ ÷åðåç 5 ñåêóíä, åñëè ýòîãî íå ïðîèçîøëî, ';
+$language['description_index']				= 'Âûáåðèòå êàòåãîðèþ, ÷òîáû ïîñìîòðåòü äîñòóïíûå ôàéëû. Íàæìèòå íà íàçâàíèå ôàéëà, ÷òîáû ïðî÷èòàòü äîïîëíèòåëüíóþ èíôîðìàöèþ.';
+$language['description_nodownload']			= 'Ôàéëà ñ òàêèì ID íå ñóùåñòâóåò. <a href="index.php">Íàæìèòå, ÷òîáû âåðíóòüñÿ.</a>';
+$language['description_nodlcategory']		= 'Â ýòîé êàòåãîðèè íåò ôàéëîâ.';
+$language['description_noresults']			= 'Ïîèñê íå âûäàë íèêàêèõ ðåçóëüòàòîâ.';
+$language['description_search']				= 'Ââåäèòå â ñòðîêó ïîèñêà ñëîâî èëè ÷àñòü ñëîâà:';
+$language['info_downloads']					= 'Ñêà÷àíî:';
+$language['info_filesize']					= 'Ðàçìåð ôàëà:';
+$language['info_rating']					= 'Ðåéòèíã:';
+$language['info_votes']						= 'Ãîëîñîâ:';
+$language['link_clickhere']					= 'Íàæìèòå çäåñü';
+$language['link_clickheregoback']			= 'Íàæìèòå, ÷òîáû âåðíóòüñÿ';
+$language['link_downloadnow']				= 'Ñêà÷àòü ôàéë';
+$language['link_moreinfo']					= 'Äîï. èíôîðìàöèÿ';
+$language['menu_all']						= 'Âñå ôàéëû';
+$language['menu_downloads']					= 'Ôàéëîâ';
+$language['menu_top']						= 'Òîï';
+$language['rating_0']						= '0 - Î÷. ïëîõî!';
+$language['rating_1']						= '1';
+$language['rating_2']						= '2';
+$language['rating_3']						= '3';
+$language['rating_4']						= '4';
+$language['rating_5']						= '5 - Îòëè÷íî!';
+$language['rating_already']					= 'Èçâèíèòå! Âû óæå ãîëîñîâàëè! ';
+$language['rating_notrated']				= 'Íåò îöåíêè';
+$language['rating_outof']					= ' èç 5.00.';
+$language['rating_thanks']					= 'Ñïàñèáî çà âàø ãîëîñ!</p> <p>Ðåéòèíã ýòîãî ôàéëà òåïåðü: ';
+$language['title_categories']				= 'Êàòåãîðèè';
+$language['title_download']					= 'Download';
+$language['title_pages']					= 'Ñòðàíèöà: ';
+$language['title_pages_prev']				= '<< Ïðåä.';
+$language['title_pages_next']				= 'Ñëåä. >>';
+$language['title_rate']						= 'Rate';
+$language['title_ratedownload']				= 'Îöåíêà ôàéëó';
+$language['title_search']					= 'Ïîèñê';
+$language['title_search_results']			= 'Ðåçóëüòàòû ïîèñêà:';
+$language['title_sortby']					= 'Ñîðòèðîâàòü ïî:';
+$language['title_sorting_downloads']		= 'Êîë-âó ñêà÷èâàíèé';
+$language['title_sorting_name']				= 'Èìåíè';
+$language['title_sorting_rating']			= 'Ðåéòèíãó';
+?>
