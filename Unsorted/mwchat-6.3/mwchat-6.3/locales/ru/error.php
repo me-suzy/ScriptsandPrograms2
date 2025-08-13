251becb829d3e223cbe7421f1d808186fefa2af7
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:44 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/error.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/error.php
@@ -0,0 +1,52 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[ERROR_License] = "÷ ×ÁÛÅÊ ÌÉÃÅÎÚÉÉ ÏÛÉÂËÁ. ðÏÖÁÌÕÊÓÔÁ, ÐÒÏÌÉÃÅÎÚÉÒÕÊÔÅ MWChat.";
+
+$LOCALE[ERROR_AlreadyIn] = "éÍÑ, ËÏÔÏÒÏÅ ×Ù ×ÙÂÒÁÌÉ, ÕÖÅ ÉÓÐÏÌØÚÕÅÔÓÑ.";
+
+$LOCALE[ERROR_Title] = "ïÛÉÂËÁ";
+
+$LOCALE[ERROR_Logout] = "÷Ù ×ÙÛÌÉ.";
+
+$LOCALE[ERROR_Expired] = "÷ÁÛÁ ÕÞÅÔÎÁÑ ÚÁÐÉÓØ ÕÓÔÁÒÅÌ, ÐÏÖÁÌÕÊÓÔÁ, ×ÏÊÄÉÔÅ ÓÎÏ×Á.";
+
+$LOCALE[ERROR_RoomLong] = "éÍÑ ËÏÍÎÁÔÙ, ËÏÔÏÒÏÅ ×Ù ×ÙÂÒÁÌÉ, ÓÌÉÛËÏÍ ÄÌÉÎÎÏÅ.";
+
+$LOCALE[ERROR_Live] = "÷ ÎÁÓÔÏÑÝÅÅ ×ÒÅÍÑ ÓÉÓÔÅÍÁ ÎÅÄÏÓÔÕÐÎÁ.";
+
+$LOCALE[ERROR_Profanity] = "÷ ×ÁÛÅÊ ÉÎÆÏÒÍÁÃÉÉ ÅÓÔØ ÎÅÃÅÎÚÕÒÎÙÅ ×ÙÒÁÖÅÎÉÑ.";
+
+$LOCALE[ERROR_NoData]   = "ðÏÖÁÌÕÊÓÔÁ, ÚÁÐÏÌÎÉÔÅ ×ÓÅ ÐÏÌÑ.";
+
+$LOCALE[ERROR_BadData]  = "ðÏÖÁÌÕÊÓÔÁ, ××ÅÄÉÔÅ ÄÏÐÕÓÔÉÍÙÅ ÄÁÎÎÙÅ.";
+
+$LOCALE[ERROR_Serial_Invalid] = "÷ÁÛ ÒÅÇÉÓÔÒÁÃÉÏÎÎÙÊ ÎÏÍÅÒ ÎÅ ×ÅÒÅÎ, ÐÏÖÁÌÕÊÓÔÁ, ÐÅÒÅÒÅÇÉÓÔÒÉÒÕÊÔÅ ×ÁÛ ÐÒÏÄÕËÔ.";
+
+$LOCALE[ERROR_Serial_None] = "òÅÇÉÓÔÒÁÃÉÏÎÎÙÊ ÎÏÍÅÒ ÎÅ ÎÁÊÄÅÎ, ÐÏÖÁÌÕÊÓÔÁ, ÐÅÒÅÒÅÇÉÓÔÒÉÒÕÊÔÅ ×ÁÛ ÐÒÏÄÕËÔ.";
+
+$LOCALE[ERROR_Critical] = "ëÒÉÔÉÞÅÓËÁÑ ÏÛÉÂËÁ!";
+
+$LOCALE[ERROR_Javascript] = "üÔÏÊ ÐÒÏÇÒÁÍÍÅ ÔÒÅÂÕÅÔÓÑ ÂÒÁÕÚÅÒ Ó ÐÏÄÄÅÒÖËÏÊ JavaScript.";
+
+$LOCALE[ERROR_Browser] = "üÔÁ ÐÒÏÇÒÁÍÍÁ ÎÅ ÂÕÄÅÔ ËÏÒÒÅËÔÎÏ ÒÁÂÏÔÁÔØ Ó ×ÁÛÉÍ ÂÒÁÕÚÅÒÏÍ.";
+
+$LOCALE[ERROR_Restricted] = "÷ÙÂÒÁÎÎÏÅ ×ÁÍÉ ÉÍÑ ÚÁÐÒÅÝÅÎÏ.";
+
+$LOCALE[ERROR_BadPass]  = "ðÏÖÁÌÕÊÓÔÁ, ××ÅÄÉÔÅ ÄÏÐÕÓÔÉÍÙÊ ÐÁÒÏÌØ.";
+
+$LOCALE[ERROR_IP] = "IP-ÁÄÒÅÓ, ËÏÔÏÒÙÊ ×Ù ÉÓÐÏÌØÚÕÅÔÅ, ÉÚÍÅÎÉÌÓÑ. ÷Ù ÂÙÌÉ ×Ù×ÅÄÅÎÙ ÐÏ ÐÒÉÞÉÎÁÍ ÚÁÝÉÔÙ.";
+
+$LOCALE[ERROR_PHPV] = "MWChat ÔÒÅÂÕÅÔ ×ÅÒÓÉÉ PHP ÎÅ ÎÉÖÅ";
+
+?>
