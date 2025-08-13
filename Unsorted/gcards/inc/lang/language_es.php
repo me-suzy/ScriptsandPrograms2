0dc4033879a292b9f0fa5080e004c6adaabd4832
me-suzy <me.suzana@gmail.com>
2025-08-13 04:49:34 +0300

    Add Unsorted/gcards/inc/lang/language_es.php

---
+++ Unsorted/gcards/inc/lang/language_es.php
@@ -0,0 +1,150 @@
+<?
+//spanish language by judas_iscariote judas_iscariote@piscola.com
+
+// index.php
+$index01 = 'Todas las categorias';
+$index02 = 'elige una postal antes de empezar!';
+
+// general navigation
+$nav01 = 'Atras';
+$nav02 = 'Proxima';
+$nav03 = 'Inicio';
+$nav04 = 'Mostrar';
+$nav05 = 'Consola de administracion';
+$nav06 = 'Cambiar password';
+$nav07 = 'Salir';
+$nav08 = 'Ingresar';
+$nav09 = 'Powered by';
+$nav10 = 'filas por pagina';
+$nav11 = 'Por favor vuelve <a href="javascript:history.go(-1);">atras</a>';
+$nav12 = 'Volver a:';
+
+
+
+// UIfunctions.php
+$uifunc01 = 'Usuario: ';
+
+// compose.php
+$compose01 = 'No eligio postal! Vaya <a href="index.php">atras</a> y elija una';
+$compose02 = 'Agregue el texto abajo para enviar su postal !';
+$compose03 = 'Nombre';
+$compose04 = 'Email';
+$compose05 = 'De:';
+$compose06 = 'Para:';
+$compose07 = 'Notificarme cuando la postal sea recibida';
+$compose08 = 'Direcciones de email separadas por comas';
+
+// preview.php
+$preview01 = "Complete por favor todos los valores del formulario ! $nav11";
+$preview02 = 'no es una direccion de email valida';
+$preview03 = "Emails invalidos! $nav11";
+$preview04 = 'Enviar';
+
+// sendcard.php
+$sendcard01 = 'Error! Formulario no completado correctamente . Vaya <a href="javascript:history.go(-1);">atras</a>';
+$sendcard02 = 'error insertando: ';
+$sendcard03 = 'eCard enviada...';
+$sendcard04 = 'eCard no puede ser enviada...';
+
+// getcard.php
+$getcard01 = 'Error - no card ID';
+$getcard02 = '$to_email eligida tu postal';
+$getcard03 = 'Enivia a $from_name una postal';
+
+// showcard.php
+$showcard01 = 'Una postal de:';
+
+// news.php
+$news01 = 'Noticias antiguas';
+$news02 = 'Enviado';
+$news03 = 'Por';
+$news04 = 'Articulos por paginas';
+$news05 = 'Noticias';
+$news06 = 'Asunto';
+$news07 = 'Texto';
+$news08 = 'Fecha';
+$news09 = 'Autor';
+
+
+
+// user related text
+$user01 = 'Usuario';
+$user02 = 'Password';
+$user03 = 'No has ingresado - no puedes salir!';
+$user04 = 'Has salido con exito...';
+$user05 = 'Confirmacion del password';
+$user06 = 'Email';
+$user07 = 'Rol';
+$user08 = 'Usuario';
+$user09 = 'Los passwords no concuerdan - intenta nuevamente';
+$user10 = '(Leave blank to keep current value)';
+$user11 = 'La vieja contraseña no concuerda con su contraseña existente ';
+$user12 = 'Password actualizado con exito!';
+$user13 = 'Antiguo Password';
+$user14 = 'Nuevo Password';
+
+/*
+*********************
+** ADMIN DIRECTORY **
+*********************
+*/
+
+// Authentication
+$auth01 = 'La autentificación falló para el usuario';
+$auth02 = 'Incorpore por favor un username y la contraseña .';
+$auth03 = 'No has ingresado.';
+$auth04 = 'Tus privilegios son insuficientes para vr esta pagima!';
+
+// admin.php
+$admin01 = 'Opciones de administracion';
+$admin02 = 'Mantenimiento';
+$admin03 = "$admin02-Postales";
+$admin04 = "$admin02-Categorias";
+$admin05 = "$admin02-Noticias";
+$admin06 = "$admin02-Usuarios";
+$admin07 = 'Ver, agregar, actualizar, y borrar';
+$admin08 = 'ID';
+$admin09 = 'borrado de la BD...';
+$admin10 = 'borrado del servidor...';
+$admin11 = 'no';
+$admin12 = 'actualizado';
+$admin13 = 'borrado';
+$admin14 = 'agregado';
+$admin15 = 'Agrega una nueva';
+$admin16 = 'Subir';
+
+// actions
+$action01 = 'Accion';
+$action02 = 'Agregar';
+$action03 = 'Editar';
+$action04 = 'Borrar';
+$action05 = 'Ver';
+$action06 = 'Actualizar';
+$action07 = 'Cancelar';
+$action08 = 'Vista previa';
+
+// cards.php
+$cards01 = 'Imagen';
+$cards02 = 'Thumbnail';
+$cards03 = 'Times Sent';
+$cards04 = 'Postal';
+$cards05 = 'Nombre de la postal';
+$cards06 = 'Deje el espacio en blanco para el thumbnail automático';
+
+// categories
+$cat01 = 'Categoria';
+$cat02 = 'Nombre categoria';
+$cat03 = 'Postales en categoria';
+
+// upload.php
+$upload01 = 'Imagen no cargada al servidor!';
+$upload02 = "error en la copia de archivo con la fucncion'move_uploaded_file'!";
+$upload03 = 'Imagen almacenada con exito...';
+$upload04 = 'Error en la copia de archivo!';
+$upload05 = 'Thumbnail subida con exito...';
+$upload06 = 'Imagen subida usada como thumbnail...';
+$upload07 = 'Thumbnail creada con exito...';
+$upload08 = 'Por favor sube un thumbnail si la imagen NO es JPEG.';
+
+
+?>
