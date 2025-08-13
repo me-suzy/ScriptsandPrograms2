f254a6c9c6ee00f6a0e79d98d4bbf0b2405fde2f
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:32 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/english/lang.specialty_tmp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/english/lang.specialty_tmp.php
@@ -0,0 +1,85 @@
+<?php
+
+$L = array(
+
+
+"email_title" =>
+"Email Title",
+
+"email_message" =>
+"Email Message",
+
+"admin_notify_comment" =>
+"Admin Notification of Comments",
+
+"admin_notify_comment_desc" =>
+"This template defines the message that an admin receives when a comment is posted.",
+
+"admin_notify_trackback" =>
+"Admin Notification of Trackbacks",
+
+"admin_notify_trackback_desc" =>
+"This template defines the message that an admin receives when a trackback is received.",
+
+"admin_notify_reg" =>
+"Admin Notification of New Member Registration",
+
+"admin_notify_reg_desc" =>
+"This template defines the message that an admin receives when a new member has registered.",
+
+"comment_notification" =>
+"User Comment Notification",
+
+"comment_notification_desc" =>
+"This template defines the message that a user receives when someone has responded to their commment.",
+
+"forgot_password_instructions" =>
+"Forgotten Password Instructions",
+
+"forgot_password_instructions_desc" =>
+"This template defines the instructions that a user who has forgotten their password receives.",
+
+"mailinglist_activation_instructions" =>
+"Mailing List Activation Instructions",
+
+"mailinglist_activation_instructions_desc" =>
+"This template defines the mailing list activation instructions.",
+
+"mbr_activation_instructions" =>
+"Member Account Activation Instructions",
+
+"mbr_activation_instructions_desc" =>
+"This template defines the member activation instructions that are sent to people who register at this site.",
+
+"reset_password_notification" =>
+"Password Reset Notification",
+
+"reset_password_notification_desc" =>
+"This template defines the password resetting instructions.",
+
+"validated_member_notify" =>
+"Member Account Validation Notification",
+
+"validated_member_notify_desc" =>
+"This template defines the notificatioin that members receive when an admin has validated their membership account.",
+
+"use_this_template" =>
+"Enable This Template?",
+
+"use_this_template_exp" =>
+"If you click <b>no</b>, this template will not be used.  Instead, the message will be generated dynamically in each user\'s chosen language.",
+
+"user_messages_template_desc" =>
+"The purpose of this template is to show error and other messages to users after they perform certain actions, like logging in, submitting a form, etc.",
+
+"user_messages_template_warning" =>
+"Do not delete any of the five required variables:",
+
+"available_variables" =>
+"The following variables can be used:",
+
+
+// END
+''=>''
+);
+?>
