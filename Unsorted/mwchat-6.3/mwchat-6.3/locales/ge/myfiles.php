34418bbbb3400c476a2b0acdc786fadaddb67018
me-suzy <me.suzana@gmail.com>
2025-08-13 06:32:29 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/myfiles.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/myfiles.php
@@ -0,0 +1,33 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[MYFILES_File_Title] = "Meine Online-Daten!";
+
+$LOCALE[MYFILES_File_Info] = "Klicken Sie auf eine Datei, die Sie herunterladen möchten oder überprüfen Sie alle Dateien, die Sie entfernen wollen.";
+
+$LOCALE[MYFILES_File_Button] = "Entfernen Sie markierte Dateien";
+
+$LOCALE[MYFILES_File_None] = "Sie haben keine Dateien.";
+
+$LOCALE[MYFILES_From] = "Datei von";
+
+$LOCALE[MYFILES_Remove] = "Entfernen";
+
+$LOCALE[MYFILES_Time] = "Dateizeit";
+
+$LOCALE[MYFILES_Size] = "Dateigröße";
+
+$LOCALE[MYFILES_Name] = "Dateiname";
+
+$LOCALE[MYFILES_Storage] = "Ihr gesamter verfügbarer Speicherplatz ist";
+
+?>
