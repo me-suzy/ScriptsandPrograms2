b1dd0f01d4fa587a8d2693db176594695dc702f3
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:25 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_jukebox-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_jukebox-edit.php
@@ -0,0 +1,23 @@
+<?php
+
+##################################################
+
+# /admin/jukebox-edit.php
+
+##################################################
+
+define( "JUKEBOX_LIST_NOPATH", "File Not Found:\\nThe targeted jukebox playlist cannot be found." );
+
+define( "JUKEBOX_LIST_NOPERM", "Permissions Error:\\nThe targeted jukebox playlist cannot be written to." );
+
+define( "JUKEBOX_LIST_DONE", "The jukebox playist has been edited successfully." );
+
+define( "JUKEBOX_START_DONE", "The jukebox playist has been started successfully." );
+
+define( "JUKEBOX_STOP_DONE", "The jukebox playist has been stopped successfully." );
+
+define( "JUKEBOX_NOPLAYER", "Error:\\nThe targeted player cannot be found. Please\\ncheck the path you entered in your preference file." );
+
+##################################################
+
+?>
