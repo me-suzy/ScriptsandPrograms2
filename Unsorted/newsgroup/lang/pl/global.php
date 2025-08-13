b75c136f0fe20eed6f0b27818401ee431e75dcf6
me-suzy <me.suzana@gmail.com>
2025-08-13 07:03:30 +0300

    Add Unsorted/newsgroup/lang/pl/global.php

---
+++ Unsorted/newsgroup/lang/pl/global.php
@@ -0,0 +1,261 @@
+<?
+// Language:                    Polish
+// Translator:                  Adam Kochanowski (http://aip.pl/)
+// Created:                     2003-01-17
+// Last Update:                 2003-01-17
+// MyNewsGroups :) Version:     0.5 and higer
+
+
+// Charset
+define("_CHARSET","charset=iso-8859-2");
+
+// HOW THE PHP date() function displays the date
+define("_MYNGDATEDISPLAY","Y-m-d"); 
+
+// Header
+define("_MYNGWELCOME"," Zapraszamy do MyNewsGroups :) v ");
+define("_MYNGSYSTEM_INFO"," Jeste¶ w:");
+define("_MYNGMENU_HOME"," Home ");
+define("_MYNGMENU_NEWSGROUPS"," Grupy ");
+define("_MYNGMENU_STATS"," Statystyki ");
+define("_MYNGMENU_SEARCH"," Szukaj ");
+define("_MYNGMENU_ABOUT"," O... ");
+
+// Footer
+define("_MYNGHOME_PAGE","Strona domowa projektu:");
+define("_MYNGGPL","Licencja GPL");
+define("_MYNGDEBUG","Debug?");
+
+// Login
+define("_MYNGLOGIN","U¿ytkownik");
+define("_MYNGPASSWORD","Has³o");
+define("_MYNGREGISTER","Zarejestruj");
+
+// NewsGroups
+define("_MYNGBROWSE_NG","Przegl±daj grupy");
+define("_MYNGARTICLE_NUMBER","Wiadomo¶ci");
+define("_MYNGGROUP_NAME","Nazwa grupy");
+define("_MYNGDESCRIPTION","Opis");
+define("_MYNGSUBSCRIBE_GROUP","Subskrybuj grupê");
+
+// Register
+define("_MYNGFILL_FORM","Aby siê zarejestrowaæ wype³nij i wy¶lij poni¿szy formularz.");
+define("_MYNGUSER_NAME","U¿ytkownik");
+define("_MYNGPASSWORD","Has³o");
+define("_MYNGEMAIL","Adres e-mail");
+define("_MYNGPASSWORD_AGAIN","Powtórz has³o");
+// Phrase: 'All fields are required'
+define("_MYNGALL_FIELDS","Wszystkie pola");
+define("_MYNGREQUIRED","wymagane");
+define("_MYNGVISIBLE","widoczne");
+define("_MYNGYES","Tak");
+define("_MYNGNO","Nie");
+define("_MYNGCOUNTRY","Kraj");
+define("_MYNGINSERT_VALID_EMAIL","Wprowad¼ prawid³owy adres e-mail. Adres bêdzie sprawdzony.");
+define("_MYNGPASSWORD_TRANSFER","Twój e-mail bêdzie widoczny dla innych, lecz niezrozumia³y dla spam-botów.");
+define("_MYNGSPAM_PROTECTED","Twoje has³o nie bêdzie przesy³ane przez internet");
+define("_MYNGSECURE_LOGIN","Has³o nie jest przesy³ane przez internet, a tylko suma kontrolna, co podwy¿sza bezpieczeñstwo.");
+define("_MYNGREGISTRATION_COMPLETE","Rejestracja zakoñczona pomy¶lnie");
+define("_MYNGTHANKS","Dziêkujemy!");
+define("_MYNGDATA_STORED","Twoje dane zosta³y poprawnie zapisane.");
+define("_MYNGEDIT_INFO","Mo¿esz zmieniæ informacje, które wprowadzi³e¶ w sekcji Home.");
+define("_MYNGSUBMITTED_INFO","To s± informacje, które wprowadzi³e¶:");
+define("_MYNGREGISTER_WELCOME","Witamy!");
+define("_MYNGSTAFF","Developerzy The MyNewsGroups :).");
+define("_MYNGLOGIN_TRANSFER","Logowanie nie powoduje przes³ania has³a poprzez internet .");
+define("_MYNGCOOKIES","Aby korzystaæ z systemu wymagana jest obs³uga cookie.");
+
+// Login
+define("_MYNGHOME","Home Page");
+define("_MYNGWELCOME_USER","Witamy");
+define("_MYNGPOST","Wy¶lij");
+define("_MYNGREPLY","Odpowiedz");
+define("_MYNGSAVE","Zapisz");
+define("_MYNGSTATS","Statystyki");
+define("_MYNGSUBSCRIBE","Subskrybuj");
+define("_MYNGOPTIONS","Opcje");
+define("_MYNGPROFILE","Profile");
+define("_MYNGNEW_ARTICLES","nowa wiadomo¶æ");
+define("_MYNGTO_ARTICLES","poka¿ wiadomo¶æ");
+define("_MYNGEDIT_YOUR","Edytuj swoje");
+define("_MYNGTO_GROUPS","do grup dyskusyjnych");
+define("_MYNGYOUR_ARTICLES","wiadomo¶ci wyró¿nione");
+define("_MYNGVIEW_YOUR","zobacz swoje");
+
+// My Bar
+define("_MYNGMY_BOX","Skrzynka");
+define("_MYNGMY_HOME","Home");
+define("_MYNGMY_GROUPS","Grupy");
+define("_MYNGMY_ARTICLES","Wiadomo¶ci");
+define("_MYNGLOG_OUT","Wyloguj");
+define("_MYNGMY_STATS","Statystyki");
+
+// Tree
+define("_MYNGNEWSGROUP_UNSELECTED","Nie wybrano ¿adnej grupy");
+define("_MYNGPOST_ARTICLE","Wy¶lij wiadomo¶æ");
+define("_MYNGDATE","Data");
+define("_MYNGSUBJECT","Temat");
+define("_MYNGAUTHOR","Autor");
+define("_MYNGARTICLES","Wiadomo¶æ");
+define("_MYNGNEXT","Nastêpna");
+define("_MYNGPREVIOUS","Ppoprzednia");
+define("_MYNGMARK_ALL_READ","Zaznacz wszystkio jako przeczytane");
+define("_MYNGMARK_THREAD_READ","Zaznacz w±tek jako przeczytany");
+
+// Article
+define("_MYNGREPLY","Odpowiedz");
+define("_MYNGSEND_FRIEND","Wyslij do znajomego");
+define("_MYNGPRINT","Drukuj");
+define("_MYNGSAVE_ARTICLE","Zapisz wiadomo¶æ");
+
+// Search Engine
+define("_MYNGSEARCH_RESULTS","Wyniki wyszukiwania");
+define("_MYNGNOARTICLES_FOUND","Nie znaleziono ¿adnego wiadomo¶co zawieraj±cej");
+define("_MYNGSEARCH_ENGINE","Wyszukiwarka");
+define("_MYNGSEARCH_ONE_GROUP","Szukaj w grupie");
+define("_MYNGSEARCH_ALL_GROUPS","Szukaj we wszystki grupach");
+define("_MYNGAND_OPERATION","Je¶li wprowadzisz wiecej ni¿ jedno s³owo po³±czne operatorem AND.");
+define("_MYNGPOWERED_BY","Powered by ");
+define("_MYNGGO","Id¼!");
+
+// Search Results
+define("_MYNGGROUP","Grupa");
+define("_MYNGREADINGS","Czytaj±cy");
+
+// Stats
+define("_MYNGHOT_NG","Gor±ca grupa");
+define("_MYNGPOPULAR_NG","Najpopularniejsza grupa");
+define("_MYNGCROWDED_NG","Grupa z najwiêksz± liczb± pisz±cych");
+define("_MYNGHOT_ARTICLES","Gor±ce tematy");
+define("_MYNGPOPULAR_ARTICLES","Najpopularniejsze tematy");
+define("_MYNGACTIVE_USER","Najaktywniejszy u¿ytkownik");
+define("_MYNGARG_USER","Najczê¶ciej odpowiadaj±cy");
+define("_MYNGSTATS_ARTICLES","wiadomo¶ci");
+define("_MYNGSTATS_READINGS","przeczytane");
+define("_MYNGUSERS","uzytkownicy");
+define("_MYNGPOSTINGS","wysy³ane");
+define("_MYNGREPLIES","odpowiedzi");
+
+// My Groups
+define("_MYNGMUST_LOGIN","Musisz byæ zalogowany, by skorzystaæ z tej opcji.");
+define("_MYNGSUBSCRIPTION_SYSTEM","Subskrybuj");
+define("_MYNGCHOOSE_GROUP","Wybierz grupê");
+define("_MYNGSURE","Jestem pewien");
+define("_MYNGGROUPS_SUBSCRIBED","Subskrybowane grupy");
+define("_MYNGNEW","Nowe");
+define("_MYNGUNREAD","Nieprzeczytane");
+define("_MYNGPOSTED","Wys³ane");
+define("_MYNGADD","Dodaj");
+define("_MYNGDEL","Usuñ");
+
+// My Articles
+define("_MYNGARTICLES_SAVED","Zapisane wiadomo¶ci");
+
+// Print article
+define("_MYNGPRINT_ARTICLE","Drukuj wiadomo¶æ");
+define("_MYNGUSER_AGENT","Program u¿ytkownika");
+define("_MYNGGENERATED_BY","Utworzony przez");
+define("_MYNGFROM","Od");
+
+// Email article
+
+define("_MYNGEMAIL_SENT","e-mail wys³any poprawnie");
+define("_MYNGEMAIL_NOT_SENT","B³±d: e-mail nie zosta³ wys³any");
+define("_MYNGEMAIL_FRIEND","Prz¶lij wiadomo¶æ do znajomego");
+define("_MYNGSENDYOU_FROM"," wys³a³ Ci wiadomo¶æ z ");
+define("_MYNGDEAR_FRIEND"," (_friend_name)");
+// the string '_friend_name' it must be the same in '_MYNGDEAR_FRIEND' and in '_MYNGFRIEND_NAME'.
+define("_MYNGFRIEND_NAME","\(_friend_name\)");
+define("_MYNGYOUR_FRIEND"," ");
+define("_MYNGWANT2SHARE",", pragnie podzieliæ siê z Tob± \nwiadomo¶ci± pobran± z ");
+define("_MYNGNSERT_FRIEND_INFO","Podaj informacje o znajomym");
+define("_MYNGMAIL_MUSTBE_REAL","Informacja: e-mail zostanie sprawdzony pod k±tem poprawno¶ci, <br>&nbsp; ale nie bêdzie zapisany.");
+define("_MYNGNAME","Nazwa");
+define("_MYNGEMAIL_LOOK","Wiadomo¶æ");
+define("_MYNGEMAIL_IT","Wy¶lij!");
+
+// Post
+define("_MYNGMISSING_EMAIL","Musisz podaæ adres e-mail.");
+define("_MYNGMISSING_MESSAGE","Musisz za³±czyæ wiadomo¶æ do wys³ania.");
+define("_MYNGWRONG_EMAIL","Adres e-mail jest niepoprawny");
+define("_MYNGMISSING_NAME","Podaj imiê i nazwisko, lub pseudonim.");
+define("_MYNG_MISSING_SUBJECT","Musisz podaæ temat.");
+define("_MYNGMESSAGE_POSTED","Wiadomo¶æ zosta³a wys³ana");
+define("_MYNGERROR_SERVER","Serwer nie zaakceptowa³ wiadomo¶ci.");
+define("_MYNGREAD_ONLY","Grupa tylko do odczytu");
+define("_MYNGWROTE"," napisa³:\n\n");
+define("_MYNGFOLLOWUP_NOT_ALLOWED","Nie masz uprawnieñ do pisania na grupy: ");
+define("_MYNGPOST_FAILED","Brak po³±czenia. Spróbuj po¼niej.");
+define("_MYNGFOOTER1","         Wiadomo¶æ wys³ana z MyNewsGroups :)");
+define("_MYNGFOOTER2","         http://mynewsgroups.sourceforge.net");
+define("_MYNGFOOTER3","         Share your Knowledge... ");
+define("_MYNGFILL_FORM2POST","By wys³aæ now± wiadomo¶æ wype³nij formularz");
+define("_MYNGEMAIL_MUSTBE_REAL","Informacja: adres e-mail zostanie sprawdzony");
+define("_MYNGPOST","Wy¶lij");
+define("_MYNGPOSTING_COMPLETE","Wys³ano");
+define("_MYNGTHANKX","Dziêkujemy!");
+define("_MYNGPOSTED_SUCCESFULLY","Wiadomo¶æ zosta³a wys³ana");
+define("_MYNGINFO_SUBMITTED","Wiadomo¶æ, któr± wys³a³e¶ do serwera news");
+define("_MYNGBODY","Tre¶æ");
+define("_MYNGCLICK2RETURN","Kliknij tutaj by wróciæ do");
+define("_MYNGEMAIL_SPAM_PROTECTED","Twój adres e-mail jest chroniony przed wykorzystaniem przez spam-boty");
+define("_MYNGEMAIL_CHANGED","Twój adres e-mail bêdzie wygl±da³ tak");
+
+
+// Error template
+define("_MYNGSOMETH_WRONG","Co¶ posz³o nie tak...");
+define("_MYNGERROR_MESSAGE","B³±d");
+define("_MYNGHAVE_DOUBT","Je¶li masz w±tpliwo¶ci, skontaktuj siê z administratorem systemu");
+define("_MYNGERROR","B³±d!");
+
+// Admin
+define("_MYNGADMIN_LOGIN","Musisz siê zalogowaæ jako administrator systemu.");
+define("_MYNGADMIN","Zarz±dzanie");
+define("_MYNGENTER_GROUP_NAME","Wprowad¼ nazwê grupy");
+define("_MYNGADD_GROUP","Dodaj grupê");
+define("_MYNGCHOOSE_SERVER","Wybierz serwer");
+define("_MYNGARTICLES2FETCH","Wiadomo¶ci do pobrania");
+define("_MYNGDELETE_GROUP","Usuñ grupê");
+define("_MYNGDELETE","Usuñ");
+define("_MYNGEDIT_GROUP","Modyfikuj grupê");
+define("_MYNGALLOW_POSTING","Dopu¶æ wysy³anie wiadomo¶ci");
+define("_MYNGEDIT","Modyfikuj");
+define("_MYNGADD_SERVER","Dodaj nowy serwer");
+define("_MYNGHOST","Host");
+define("_MYNGPORT","Port");
+define("_MYNGDELETE_SERVER","Usuñ serwer news");
+define("_MYNGALL","Wszystko");
+
+
+// Errors
+define("_MYNGNOGROUPS","Brak zarejestrowanych grup.");
+define("_MYNGCON_ERROR","B³±d w po³±czeniu!");
+define("_MYNGUSERNAME_TAKEN","Nazwa u¿ytkownika jest ju¿ wykorzystana.");
+define("_MYNGEMAIL_TAKEN","Twój e-mail jest przypisany do innego u¿ytkownika.");
+define("_MYNGLOGIN_INCORRECT","Nieprawid³owa nazwa u¿ytkownika.");
+define("_MYNGINVALID_EMAIL","Nieprawid³owy adres e-mail. Wprowad¼ poprawny adres.");
+define("_MYNGINVALID_PASSWORD","Nieprawid³owe has³o.");
+define("_MYNGBLANK_PASSWORD","Wprowad¼ has³o.");
+define("_MYNGSERVER_REJECT","Twój serwer e-mail odrzuci³ adres");
+define("_MYNGNO_RESPONSE","Brak odpowiedzi od Twojego serwera e-mail");
+define("_MYNGCANT_CONNECT_SERVER","Nie mo¿na uzyskaæ po³±czenia do serwera e-mail");
+define("_MYNGAPPEARS_VALID"," wygl±da na prawid³owy");
+define("_MYNGNOT_PROPERLY_FORMATTED"," jest ¼le sformatowany");
+define("_MYNGNO_GROUP","Grupa niedostêpna z ");
+define("_MYNGUNKNOWN_GROUP"," Ta grupa nie nale¿y do systemu.");
+
+
+// Greets
+define("_MYNGREGISTRATION_COMPLETED","Rejestracja ukoñczona pomy¶lnie.");
+define("_MYNGLOGGED","Nie jeste¶ zalogowany do systemu!");
+define("_MYNGLOGGED_OUT","Wylogowano.");
+
+
+// 2002 - 04 - 22
+define("_MYNGDELETE_ARTICLES","Usuñ wiadomo¶ci");
+define("_MYNGDELETE_LASTN","Usuñ najstarsze n wiadomo¶ci: ");
+
+// 2002 - 05 - 03
+define("_MYNGMY_ADMIN","Admin");
+
+?>
