abeaa4a46e556373f25b8847e9d54ff14a37726c
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:06 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/error.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/error.php
@@ -0,0 +1,52 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[ERROR_License] = "Há um erro com seua licença. Por MWChat de licença de favor agora.";
+
+$LOCALE[ERROR_AlreadyIn] = "O nome que você escolheu é já em uso.";
+
+$LOCALE[ERROR_Title] = "O erro";
+
+$LOCALE[ERROR_Logout] = "Foi logged para fora.";
+
+$LOCALE[ERROR_Expired] = "Seu login expirou, por login de favor novamente.";
+
+$LOCALE[ERROR_RoomLong] = "Este nome de lugar que você escolhe é a longo.";
+
+$LOCALE[ERROR_Live] = "O sistema está atualmente indisponível.";
+
+$LOCALE[ERROR_Profanity] = "A informação que você especificou contem profanidade.";
+
+$LOCALE[ERROR_NoData]   = "Por favor encha em todos campos.";
+
+$LOCALE[ERROR_BadData]  = "Por favor especifique dados válidos.";
+
+$LOCALE[ERROR_Serial_Invalid] = "Seu número de série é inválido, por re registra de favor seu produto.";
+
+$LOCALE[ERROR_Serial_None] = "Nao possa achar um número de série em registro, por re registra de favor seu produto.";
+
+$LOCALE[ERROR_Critical] = "Erro crítico!";
+
+$LOCALE[ERROR_Javascript] = "Este programa requer um JavaScript browser capaz.";
+
+$LOCALE[ERROR_Browser] = "Este programa nao funcionará corretamente com seu browser.";
+
+$LOCALE[ERROR_Restricted] = "O nome que você escolheu é restringido.";
+
+$LOCALE[ERROR_BadPass]  = "Por favor entre uma senha válida.";
+
+$LOCALE[ERROR_IP] = "O IP endereça seu é usar mudou. Foi logged para fora para razões de segurança.";
+
+$LOCALE[ERROR_PHPV] = "MWChat requer uma versão mínima de PHP de";
+
+?>
