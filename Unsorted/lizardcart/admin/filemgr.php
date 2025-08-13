77b4c90f4d59bea8e379672d8c073cc5a8d9304e
me-suzy <me.suzana@gmail.com>
2025-08-13 06:14:03 +0300

    Add Unsorted/lizardcart/admin/filemgr.php

---
+++ Unsorted/lizardcart/admin/filemgr.php
@@ -0,0 +1,12 @@
+<?
+include ("atho.inc.php");
+include ("config.inc.php");
+
+include ("header.php");
+
+echo"<iframe src=\"../graphics/_fileboy.php\" width=\"100%\" height=\"100%\" frameborder=\"0\"></iframe>";
+
+include ("footer.php");
+
+?>
+
