5f9098772f60e2080165a0f4235a8906e4c9a47b
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:17 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.myaccount.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.myaccount.php
@@ -0,0 +1,457 @@
+<?php
+$L = array(
+
+
+"preferences_updated" =>
+"Voorkeuren Geupdatet",
+
+"homepage_preferences" =>
+"Homepage Voorkeuren",
+
+"set_display_order" =>
+"Kies volgorde van weergave",
+
+"please_update_order" =>
+"Kies volgorde van de items voor weergave",
+
+"left_column" =>
+"Linker Kolom",
+
+"right_column" =>
+"Rechter Kolom",
+
+"do_not_show" =>
+"Laat niet zien",
+
+"recent_entries" =>
+"Uw tien meest recente weblog toevoegingen",
+
+"recent_comments" =>
+"De tien meest recente weblog toevoegingen",
+
+"recent_members" =>
+"De tien meest recente ledenregistraties",
+
+"new_release_indicator" =>
+"Nieuwe softwarereleas indicator",
+
+"site_statistics" =>
+"Site statistieken",
+
+"member_search_form" =>
+"Leden zoek formulier",
+
+"validating_members" =>
+"Leden die wachten op activatie",
+
+"php_sql_versions" =>
+"PHP/SQL versie",
+
+"system_offline_indicator" =>
+"Systeem offline indicator",
+
+"personal_settings" =>
+"Persoonlijke Instellingen",
+
+"weblog_settings" =>
+"Weblog Instelling",
+
+"control_panel_settings" =>
+"CP aanpassen aan de gebruiker",
+
+"administrative_settings" =>
+"Admin Instellingen",
+
+"current_member" =>
+"Leden:",
+
+"member_stats" =>
+"Account Statistieken voor:",
+
+"join_date" =>
+"Datum van aanmelding",
+
+"last_visit" =>
+"Laatste bezoek",
+
+"total_entries" =>
+"Totaal aantal Berichten",
+
+"last_entry_date" =>
+"Datum van het Meest Recente Bericht",
+
+"total_comments" =>
+"Totaal aantal Reacties",
+
+"last_comment_date" =>
+"Datum van de meest recente reactie",
+
+"notepad" =>
+"Kladblok",
+
+"can_not_view_superadmin_profile" =>
+"U kunt het profiel van een Super Admin niet bekijken",
+
+"only_self_notpad_access" =>
+"U bent niet gemachtigd om een ander z&#39;n kladblok te bekijken",
+
+"notepad_size" =>
+"Kladbok Grootte",
+
+"notepad_instructions" =>
+"Gebruik dit formulier om uw persoonlijke notities en informatie op te slaan",
+
+"notepad_updated" =>
+"Notepad Ge-update",
+
+"localization_settings" =>
+"Lokale Instellingen",
+
+"localization_updated" =>
+"Lokale Instellingen Ge-update",
+
+"daylight_savings_time" =>
+"Zomertijd",
+
+"timezone" =>
+"Selecteer uw tijdszone",
+
+"language_choice" =>
+"Selecteer uw Taal",
+
+"username_and_password" =>
+"Gebruikersnaam en Wachtwoord",
+
+"extras" =>
+"Extra&#39;s",
+
+"edit_profile" =>
+"Wijzig Uw Profiel",
+
+"profile_form" =>
+"Profiel Formulier",
+
+"base_profile" =>
+"Standaard Profiel",
+
+"extended_profile" =>
+"Uitgebreid Profiel",
+
+"localization" =>
+"Lokale Instellingen",
+
+"time_format" =>
+"Tijdsformaat",
+
+"united_states" =>
+"Verenigde Staten",
+
+"european" =>
+"Europese",
+
+"your_ping_servers" =>
+"Ping Servers",
+
+"your_html_buttons" =>
+"HTML Formaat Knoppen",
+
+"change_password" =>
+"Verander Password",
+
+"email_settings" =>
+"Email Instellingen",
+
+"subscriptions" =>
+"Inschrijvingen",
+
+"edit_subscriptions" =>
+"Bekijk uw Inschrijvingen",
+
+"customize_homepage" =>
+"Beheerpaneel homepage aanpassen aan de gebruiker",
+
+"customize_cp" =>
+"Beheerpaneel aanpassen aan de gebruiker",
+
+"cp_homepage" =>
+"Beheerpaneel Homepage",
+
+"cp_theme" =>
+"Beheerpaneel Thema",
+
+"choose_theme" =>
+"Kies uw Beheerpaneel Thema",
+
+"administrative_options" =>
+"Administrator Optie&#39;s",
+
+"administrative_options_updated" =>
+"Administrator Optie&#39;s Geupdatet",
+
+"screen_name" =>
+"Schermnaam",
+
+"email" =>
+"Email Adres",
+
+"url" =>
+"URL",
+
+"bio" =>
+"Bio",
+
+"birthday" =>
+"Verjaardag",
+
+"year" =>
+"Jaar",
+
+"month" =>
+"Maand",
+
+"day" =>
+"Dag",
+
+"accept_admin_email" =>
+"Accepteer email van de administrators van deze site?",
+
+"accept_user_email" =>
+"Accepteer email van andere leden van deze site?",
+
+"notify_by_default" =>
+"Schakel automatisch de email notificaties in als u berichten plaatst?",
+
+"location" =>
+"Lokatie",
+
+"occupation" =>
+"Beroep",
+
+"interests" =>
+"Interesses",
+
+"icq" =>
+"ICQ",
+
+"aol_im" =>
+"AOL IM",
+
+"yahoo_im" =>
+"Yahoo IM",
+
+"msn_im" =>
+"MSN IM",
+
+"user_ip_address" =>
+"IP Adres",
+
+"member_group_assignment" =>
+"Ledengroep toewijzing",
+
+"member_group_warning" =>
+"Wees voorzichtig met het toewijzen hiervan ",
+
+"include_in_multiauthor_list" =>
+"Voeg de gebruiker in de PUBLICEER pagina toe aan de auteurslijst?",
+
+"profile_updated" =>
+"Profiel Ge-update",
+
+"super_admin_demotion_alert" =>
+"Als Super Admin bent u niet gemachtigd om uw toewijzing van de ledengroep te wijzigen",
+
+"username" =>
+"Gebruikersnaam",
+
+"username_change_not_allowed" =>
+"De administrators mogen geen gebruikersnamen wijzigen",
+
+"password" =>
+"Wachtwoord",
+
+"password_change" =>
+"Formulier Wachtwoord Wijzigen",
+
+"new_password" =>
+"Nieuw Wachtwoord",
+
+"password_confirm" =>
+"Wachtwoord Bevestigen",
+
+"new_password_confirm" =>
+"Nieuw Wachtwoord Bevestigen",
+
+"existing_password" =>
+"Uw Huidige Wachtwoord",
+
+"invalid_email_address" =>
+"Het email adres wat u gekozen heeft is niet geldig",
+
+"password_required" =>
+"U moet uw huidige wachtwoord invoeren om deze pagina te updaten",
+
+"password_required_for_email" =>
+"Om uw email adres te wijzigen moet u uw huidige wachtwoord invoeren",
+
+"leave_blank" =>
+"Laat leeg als u dit niet wilt wijzigen",
+
+"missing_username" =>
+"U moet een gebruikersnaam invoeren",
+
+"invalid_characters_in_username" =>
+"Uw gebruikersnaam bevat niet toegestane karakters",
+
+"missing_password" =>
+"U moet een wachtwoord invoeren",
+
+"missing_current_password" =>
+"Om de veranderingen door te voeren moet u uw huidige wachtwoord invoeren",
+
+"password_in_dictionary" =>
+"Het is niet toegestaan een woord te kiezen die in het woordenboek voorkomt.",
+
+"not_secure_password" =>
+"Uw wachtwoord moet op z&#39;n minst 1 hoofdletter, 1 kleine letter en een cijfer bevatten",
+
+"invalid_password" =>
+"Het wachtwoord wat u invoerde is niet juist",
+
+"missing_email" =>
+"U moet een email adres invoeren",
+
+"banned_email" =>
+"Het email adres wat u invoerde is geblokkeerd",
+
+"missmatched_passwords" =>
+"De 2 wachtwoorden die u invoerde komen niet overeen",
+
+"username_too_short" =>
+"De gebruikersnaam die u invoerd moet minimaal uit %x karakters bestaan",
+
+"password_too_short" =>
+"Het wachtwoord wat u invoerd moet minimaal uit %x karakters bestaan",
+
+"username_password_too_long" =>
+"Gebruikersnamen en wachtwoorden kunnen niet meer dan 32 karakters lang zijn",
+
+"username_taken" =>
+"De gebruikersnaam die u koos is niet beschikbaar",
+
+"screen_name_taken" =>
+"De weergave naam die u koos is niet beschikbaar",
+
+"email_taken" =>
+"Het email adres wat u koos is niet beschikbaar",
+
+"password_based_on_username" =>
+"Het wachtwoord kan niet gebasseerd zijn op uw gebruikersnaam",
+
+"username_changed" =>
+"Uw gebruikersnaam is veranderd in:",
+
+"screen_name_changed" =>
+"Uw weergave naam is veranderd in:",
+
+"email_changed" =>
+"Uw email adres is veranderd in:",
+
+"password_change_requires_login" =>
+"Let Op:  Als u uw wachtwoord veranderd moet u opnieuw inloggen",
+
+"settings_updated" =>
+"Instellingen zijn Ge-update",
+
+"new_member_added" =>
+"Leden Profiel gemaakt:",
+
+"password_change_warning" =>
+"Let Op: U heeft uw wachtwoord veranderd en u zult opnieuw moeten inloggen.",
+
+"ping_servers" =>
+"Ping Servers",
+
+"pingservers_updated" =>
+"Ping Servers Ge-update",
+
+"html_buttons" =>
+"HTML Formaat Knoppen",
+
+"html_buttons_updated" =>
+"HTML Knoppen Ge-update",
+
+"quick_links" =>
+"Snelle Links",
+
+"only_self_qucklink_access" =>
+"U bent niet gemachtigd om de Snelle Links van een ander lid te bekijken",
+
+"quicklinks_updated" =>
+"De Snelle Links zijn Ge-update",
+
+"quick_link_description" =>
+"Gebruik dit formulier om uw persoonlijke links toe te voegen aan de toolbar",
+
+"quick_link_description_more" =>
+"Deze links zijn alleen zichtbaar voor u",
+
+"quicklinks_delete_instructions" =>
+"Om een item te verwijderen maak het titelveld leeg voordat u het formulier invoert",
+
+"link_title" =>
+"Link Titel",
+
+"link_url" =>
+"Link URL",
+
+"link_order" =>
+"Volgorde",
+
+"no_extended_fields" =>
+"Er zijn op dit moment geen uitgebreide leden profiel velden",
+
+"bookmarklet" =>
+"Bladwijzer",
+
+"bookmarklet_created" =>
+"Bladwijzer aangemaakt",
+
+"bookmarklet_info" =>
+"Een Bladwijzer geeft u de mogelijkheid om toevoegingen aan uw weblog te maken zonder in te loggen op het beheerpaneel",
+
+"consult_manual" =>
+"Raadpleeg de Gebruikershandleiding voor meer informatie",
+
+"bookmarklet_name" =>
+"Naam Van Uw Bladwijzer",
+
+"single_word_no_spaces" =>
+"1 woord, geen spaties",
+
+"weblog_name" =>
+"Kies de weblog waarin u uw bericht geplaatst moet worden",
+
+"no_blogs_assigned_to_user" =>
+"Er zijn geen weblogs toegekend aan uw account",
+
+"select_field" =>
+"Selecteer het standaard veld waarin uw bericht geplaatst moet worden",
+
+"bookmarklet_next_step" =>
+"Volgde stap",
+
+"create_the_bookmarklet" =>
+"Maak een Bladwijzer aan",
+
+"no_fields_assigned_to_blog" =>
+"De weblog die u koos heeft geen veldgroep toegewezen gekregen",
+
+"safari_users" =>
+"Klik hier als u Safari als browser gebruikt",
+
+"bookmarklet_instructions" =>
+"Bewaar deze link in uw favorieten menu:",
+
+''=>''
+);
+?>
