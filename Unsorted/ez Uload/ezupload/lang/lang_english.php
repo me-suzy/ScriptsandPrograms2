4b28239eff6a0d52079423be9796bf83519b04d5
me-suzy <me.suzana@gmail.com>
2025-08-13 04:30:44 +0300

    Add Unsorted/ez Uload/ezupload/lang/lang_english.php

---
+++ Unsorted/ez Uload/ezupload/lang/lang_english.php
@@ -0,0 +1,51 @@
+<?
+  
+  $LANGPACK[$i]['name'] = "English";
+  $LANGPACK[$i]['file'] = "lang_english.php";
+  
+  // Javascript validation (checkfields.php)
+  $_LANG['js_error'] = "The following fields are incorrect";
+  $_LANG['missing_field'] = "Missing field";
+  $_LANG['invalid_address'] = "Invalid email address";
+  $_LANG['max_chars'] = "Max. chars";
+  $_LANG['min_chars'] = "Min. chars";
+  $_LANG['no_option_checked'] = "No option checked";
+  $_LANG['invalid_extension'] = "Extension not accepted";
+  $_LANG['large_files_warning'] = "Your files are going to be uploaded now. Depending on the size of these files and your internet connection, this may take some time. Please be patient.";
+
+  // Password page (form_password.php)
+  $_LANG['enter_pass'] = "Enter the password to access upload form";
+  $_LANG['enter_user'] = "Authentify yourself to access the upload form"; // when user login is turned on
+  $_LANG['your_email'] = "Email Address";
+  $_LANG['your_pass'] = "Your Password";
+  $_LANG['forgot_pass'] = "Don't know the password?";
+  $_LANG['email_admin'] = "Email webmaster";
+  $_LANG['enter'] = "Enter"; // for the button
+  
+  // Form page (form_show.php)
+  $_LANG['upload_files'] = "Upload Files"; // for the upload button
+  $_LANG['upload_folder'] = "Upload Folder"; // if user has to select sub-directory
+
+  // Upload page (form_upload.php)
+  $_LANG['dir_creation_error'] = "Error! Could not create directory! (Permission denied)";
+  $_LANG['ext_not_accepted'] = "Extension not accepted";
+  $_LANG['file_too_large'] = "File is too large (Max. ";
+  $_LANG['file_too_small'] = "File is too small (Min. ";
+  $_LANG['image_height'] = "Image height must be ";
+  $_LANG['image_width'] = "Image width must be ";
+  $_LANG['pixels'] = " pixels"; // follows the two field above
+  $_LANG['required_missing'] = "Required field is missing";
+  $_LANG['maxsize_settings'] = "upload_max_filesize directive exceeded";
+  $_LANG['maxsize_form'] = "MAX_FILE_SIZE directive exceeded";
+  $_LANG['partial_upload'] = "The file was only partially uploaded";
+  $_LANG['invalid_file'] = "Invalid file (Use the Browse button)";
+  $_LANG['value_too_short'] = "Value is too short (Min. ";
+  $_LANG['value_too_long'] = "Value is too long (Max. ";
+  
+  // Results page (form_results.php)
+  $_LANG['upload_error'] = "Attention, your upload was not successful! See below for the details and click on Try Again to make another attempt.";
+  $_LANG['try_again'] = "Try Again"; // for a button
+  $_LANG['upload_more'] = "Upload More"; // for a button
+  $_LANG['continue'] = "Continue"; // for a button
+  
+?>
