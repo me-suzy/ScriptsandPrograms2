1faec603e62caf384b12c70f4465cb785f46875c
me-suzy <me.suzana@gmail.com>
2025-08-13 06:08:57 +0300

    Add Unsorted/livehelp_17/user_top.php

---
+++ Unsorted/livehelp_17/user_top.php
@@ -0,0 +1,42 @@
+<?
+//****************************************************************************************/
+//  Crafty Syntax Live Help (CS Live Help)  by Eric Gerdes (http://craftysyntax.com )
+//======================================================================================
+// NOTICE: Do NOT remove the copyright and/or license information from this file. 
+//         doing so will automatically terminate your rights to use this program.
+// ------------------------------------------------------------------------------------
+// ORIGINAL CODE: 
+// ---------------------------------------------------------
+// CS LIVE HELP http://www.craftysyntax.com/livehelp/
+// Copyright (C) 2003  Eric Gerdes 
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program in a file named LICENSE.txt .
+// if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// ---------------------------------------------------------  
+// MODIFICATIONS: 
+// ---------------------------------------------------------  
+// [ Programmers who change this code should cause the  ]
+// [ modified changes here and the date of any change.  ]
+//======================================================================================
+//****************************************************************************************/
+?>
+<html>
+<body marginheight=0 marginwidth=0 leftmargin=0 topmargin=0 onload=window.focus()>
+<table cellpadding=0 cellspacing=0 border=0>
+<tr>
+<!--<td width=1%><img src=images/nav_livehelp.gif width=414 height=36 border=0></td>-->
+<td width=100% background=images/nav_bg.gif><img src=images/blank.gif width=700 height=36></td>
+</tr>
+</table>
+</body>
+</html>
