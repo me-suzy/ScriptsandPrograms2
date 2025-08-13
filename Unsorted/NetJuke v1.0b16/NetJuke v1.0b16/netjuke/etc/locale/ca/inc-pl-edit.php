4fe284e490d824b646905199ca4e6629e79ef08b
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:39 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-pl-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-pl-edit.php
@@ -0,0 +1,90 @@
+<?php
+
+##################################################
+
+# /pl-edit.php
+
+##################################################
+
+define( "PLEDIT_NOPLID", "Si us plau, seleccioneu una llista per editar." );
+
+define( "PLEDIT_ADDTO_NOID", "Si us plau, seleccioneu almenys una pista per desar." );
+define( "PLEDIT_ADDTO_TITLE_1", "Nova llista" );
+define( "PLEDIT_ADDTO_TITLE_2", "Aleatori" );
+
+define( "PLEDIT_DELPL_NOID", "Si us plau, seleccioneu una llista per esborrar." );
+define( "PLEDIT_DELPL_CONFIRM", "Esteu segur que voleu esborrar aquesta llista?" );
+
+define( "PLEDIT_DELPT_NOTRID", "Si us plau, seleccioneu almenys una pista per esborrar de la llista." );
+define( "PLEDIT_DELPT_CONFIRM", "Esteu segur que voleu esborrar \\nles pistes seleccionades de la llista?" );
+
+define( "PLEDIT_DENIED", "Aquesta no és una de les vostres llistes. Si us plau, torneu a intentar-ho." );
+
+define( "PLEDIT_CHCK_HELP", "Feu clic per seleccionar aquesta pista" );
+define( "PLEDIT_PLAY_HELP", "Feu clic per reproduir aquesta pista" );
+define( "PLEDIT_DLOAD_HELP", "Feu clic per descarregar aquesta pista" );
+define( "PLEDIT_INFO_HELP", "Feu clic per veure els detalls d'aquesta pista." );
+define( "PLEDIT_AR_HELP", "Feu clic per veure totes les pistes d'aquest intèrpret." );
+define( "PLEDIT_AL_HELP", "Feu clic per veure totes les pistes d'aquest àlbum." );
+define( "PLEDIT_GE_HELP", "Feu clic per veure totes les pistes d'aquest gènere." );
+
+define( "PLEDIT_MUSTCLICK", "Si us plau, feu clic sobre el botó de la dreta per desar la llista." );
+
+define( "PLEDIT_HEADER_1", "LLISTA DE CANÇONS" );
+
+define( "PLEDIT_SHRD", "Compartida" );
+
+define( "PLEDIT_BTN_SAVE", "Desa" );
+define( "PLEDIT_BTN_DELETE", "Esborra" );
+define( "PLEDIT_BTN_CANCEL", "Cancel·la" );
+
+define( "PLEDIT_ROWCNT_1", "Pista/es enregistrada/es" );
+define( "PLEDIT_ROWCNT_2", "per a" );
+
+define( "PLEDIT_QS_AR", "Intèrpret" );
+define( "PLEDIT_QS_TR", "Pista" );
+define( "PLEDIT_QS_AL", "Album" );
+define( "PLEDIT_QS_GE", "Gènere" );
+define( "PLEDIT_QS_TI", "Durada" );
+define( "PLEDIT_QS_BTN", "Cerca" );
+
+define( "PLEDIT_TB_PLIST_BTN", "Edita" );
+define( "PLEDIT_TB_PLAY_ALL", "Reprodueix" );
+define( "PLEDIT_TB_PLAY_ALL_HELP", "Feu clic per reproduir aquesta llista tal com ha estat desada." );
+define( "PLEDIT_TB_SELECT", "Selecciona" );
+define( "PLEDIT_TB_SELECT_HELP", "Feu clic per seleccionar totes les pistes d'aquesta pàgina" );
+define( "PLEDIT_TB_RESET", "Deselecciona" );
+define( "PLEDIT_TB_RESET_HELP", "Feu clic per deseleccionar totes les pistes seleccionades" );
+define( "PLEDIT_TB_PLAY", "Reprodueix la selecció" );
+define( "PLEDIT_TB_PLAY_HELP", "Feu clic per començar a reproduir totes les cançons seleccionades" );
+define( "PLEDIT_TB_DELETE", "Esborra la selecció" );
+define( "PLEDIT_TB_DELETE_HELP", "Feu clic per esborrar les cançons seleccionades de la llista" );
+define( "PLEDIT_TB_ADJUST", "Modifica la seqüència");
+define( "PLEDIT_TB_ADJUST_HELP", "Feu clic per modificar l'ordre de la seqüència" );
+define( "PLEDIT_TB_ADDTO", "Copia a la llista" );
+define( "PLEDIT_TB_ADDTO_HELP", "Feu clic per copiar totes les cançons seleccionades a la llista escollida" );
+
+define( "PLEDIT_COLS_OP", "Opcions" );
+define( "PLEDIT_COLS_SQ", "Seq." );
+define( "PLEDIT_COLS_SQ_HELP", "Feu clic per ordenar aquesta pàgina per número de seqüència" );
+define( "PLEDIT_COLS_TR", "Títol de cançó" );
+define( "PLEDIT_COLS_TR_HELP", "Feu clic per ordenar aquesta pàgina per títol de cançó" );
+define( "PLEDIT_COLS_TI", "Durada" );
+define( "PLEDIT_COLS_TI_HELP", "Feu clic per ordenar aquesta pàgina per durada de cançó" );
+define( "PLEDIT_COLS_AR", "Intèrpret" );
+define( "PLEDIT_COLS_AR_HELP", "Feu clic per ordenar aquesta pàgina per nom d'intèrpret" );
+define( "PLEDIT_PLAY_AR_HELP", "Feu clic per reproduir totes les canÁons d'aquest artista." );
+define( "PLEDIT_FILTER_AR_HELP", "Feu clic per llistar tots els àlbums d'aquest artista." );
+define( "PLEDIT_COLS_AL", "Àlbum" );
+define( "PLEDIT_COLS_AL_HELP", "Feu clic per ordenar aquesta pàgina per títol d'àlbum" );
+define( "PLEDIT_PLAY_AL_HELP", "Feu clic per reproduir totes les canÁons d'aquest àlbum." );
+define( "PLEDIT_FILTER_AL_HELP", "Feu clic per llistar tots els artistes d'aquest àlbum." );
+define( "PLEDIT_COLS_TN", "Núm." );
+define( "PLEDIT_COLS_TN_HELP", "Feu clic per ordenar aquesta pàgina per número de cançó" );
+define( "PLEDIT_COLS_GE", "Gènere" );
+define( "PLEDIT_COLS_GE_HELP", "Feu clic per ordenar aquesta pàgina per gènere" );
+
+
+##################################################
+
+?>
