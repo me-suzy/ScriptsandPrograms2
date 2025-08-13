d5cea82caeaa246d1c4dd786f367bbd47c2bd928
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:15 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.pm_import.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.pm_import.php
@@ -0,0 +1,313 @@
+<?php
+$L = array(
+
+
+"pmachine_import_utitity" =>
+"pMachinen tuontiohjelma",
+
+"pmachine_import_welcome" =>
+"Tämä ohjelma mahdollistaa sinun tuoda pMachinen dataa ExpressionEngineen",
+
+"pmachine_import_disclaimer" =>
+"Jos et käytä pMachinea, et tarvitse tätä.",
+
+"pmachine_import_information" =>
+"Tuonti ohje",
+
+"pmachine_import_removal" =>
+"Huom: Älä käytä tätä osiota, jos et käytä tällä hetkellä pMachinea",
+
+"configuration_blurb" =>
+"Käyttäksesi tätä osiota, sinun tulee ensin syöttää pMachinen tietokannan  tiedot.",
+
+"configuration_blurb_cont" =>
+"Löydät nämä tiedot pMachinen config.php tiedostosta",
+
+"database_info" =>
+"Tietokannan info",
+
+"sql_server" =>
+"MySQL Serverin osoite",
+
+"sql_username" =>
+"MySQL käyttäjätunnus",
+
+"sql_password" =>
+"MySQL salasana",
+
+"sql_database" =>
+"MySQL tietokanta",
+
+"sql_prefix" =>
+"Table etuliite",
+
+"leave_prefix_blank" =>
+"Jätä tyhjäksi ellet ole muokannut etuliitettä",
+
+"configure" =>
+"Configuroi",
+
+"empty_field_warning" =>
+"Jätit jonkun kentän tyhjäksi",
+
+"no_database_connection" =>
+"Tietokantayhteyttä  ei voitu muodostaa antamillasi tiedoilla",
+
+"table_name" =>
+"Taulun nimi",
+
+"table_rows" =>
+"Taulun rivit",
+
+"table_action" =>
+"Toiminta",
+
+"table_status" =>
+"Status",
+
+"completed" =>
+"Valmis",
+
+"no_rows_exist" =>
+"Tuontia ei tarvita",
+
+"pending" =>
+"Valmis tuontiin",
+
+"pending_stats" =>
+"Valmis jatkamaan",
+
+"import_now" =>
+"Tuo data",
+
+"recalculate" =>
+"Käsittele data",
+
+"step_one" =>
+"Toiminto yksi",
+
+"step_two" =>
+"Toiminto kaksi",
+
+"step_three" =>
+"Toiminto kolme",
+
+"step_four" =>
+"Toiminto neljä",
+
+"step_five" =>
+"Toiminto viisi",
+
+"import_options" =>
+"Tuonti vaihtoehdot",
+
+"import_members" =>
+"Tuo jäsenet",
+
+"import_weblog_entries" =>
+"Tuo weblogien viestit",
+
+"recalculate_statistics" =>
+"Laske tilastot uudelleen",
+
+"click_to_reset_statistics" =>
+"Klikkaa tästä päivittääksesi tilastot",
+
+"reset_statistics_info" =>
+"Sinut siirretään udelleenlasku osioon. Kun olet valmis, palaa tälle sivulle askel viiteen",
+
+"members" =>
+"Jäsenet",
+
+"member_stats" =>
+"Jäsenten tilastot",
+
+"weblog_stats" =>
+"Weblogin tilastot",
+
+"total_members" =>
+"pMachinen tietokannassa on jäseniä kaikkiaan:",
+
+"select_your_account" =>
+"Valitse pMachinen tili joka kuulu sinulle",
+
+"ignore_instructions" =>
+"Koska sinulla on jo ExpressionEnginen tili, ei ole järkeä muodostaa uutta pMachien tiedoilla. Tekemällä niin sinulla on kaksi tiliä. Jos valitset olemassa olevan pMachinen tilin, viestisi liitetään olemassa olevaan ExpressionEnginen tiliin",
+
+"i_have_no_account" =>
+"Minulla ei ole pMachinen tiliä ",
+
+"members_per_cycle" =>
+"Jäseniä per sykli",
+
+"cycle_instructions" =>
+"Estääksemme serveri timeoutin, data tuodaan paketeissa. Anna tuotavien tietueiden määrä per paketti. 1000 on hyvä luku useimmille servereille.  ",
+
+"blog_cycle_instructions" =>
+"Weblogien tuonti vaatii prosessoritehoa, varsinkin jos viesteissäsi on paljon kommentteja ja trackbackkejä. Estääksemme serveri timeoutin, data tuodaan paketeissa. 1000 on hyvä luku useimmille servereille.",
+
+"save_settings" =>
+"Tallenna asetukset",
+
+"member_import_complete" =>
+"Jäsenten tuontirutiini on suoritettu",
+
+"return_to_overview" =>
+"Aloita jäsenten tuontirutiini",
+
+"start_member_import" =>
+"Valmis aloittamaan ",
+
+"members_remaining" =>
+"Tuotavia jäseniä jäljellä:",
+
+"click_to_import_members" =>
+"Tuo jäseniä %x kaikkiaan %y",
+
+"no_table_rows" =>
+"Rivejä ei löydy",
+
+"default_member_group" =>
+"Jäsenten ryhmä määrittely",
+
+"member_group_instructions" =>
+"Tuotavien jäsenten tulee kuulua johonkin jäsenten ryhmään. Valitse mihin ryhmään haluat jäsenten kuuluvan.",
+
+"members_imported" =>
+"Kaikki jäsenet on tuotu",
+
+"return_to_main_menu" =>
+"Palaa päävalikkoon",
+
+"weblog_name" =>
+"Weblog name:",
+
+"total_weblog_entries" =>
+"Weblogien viestejä kaikkiaan:",
+
+"select_destination_blog" =>
+"Valitse weblogien kohde",
+
+"destination_instructions" =>
+"Valitse ExpressionEngine weblog mille haluat pMachine viestit tuotavaksi.",
+
+"destination_note" =>
+"Huom: jos weblogia ei ole tulee sinun mennä HALLINTA sivulle ja muodostaa se ensin.",
+
+"entries_per_cycle" =>
+"Weblogien viestejä per paketti",
+
+"select_destination_fields" =>
+"Valiltse kohdekenttä",
+
+"field_destination_instructions" =>
+"Valitse kukin ExpressionEnginen kenttä mille haluat tuoda pMachinen kentät.",
+
+"field_destination_instructions_two" =>
+"Valitse &quot;Älä tuo dataa&quot; jättääksesi tietyn kentän tiedot tuomatta",
+
+"fields_not_unique_warning" =>
+"Et voi tuoda kuin yhden pMachinen kentän samaan ExpressionEnginen kenttään",
+
+"no_fields_assigned" =>
+"Weblog jonka valitsit ei sisällä kenttien ryhmää. Mene HALLINTA sivulle ja liitä kenttien ryhmä tähän weblogiin.",
+
+"no_textarea_fields" =>
+"Valitsemasi kenttien ryhmä ei sisällä yhtään tekstikenttää",
+
+"pmachine_field" =>
+"pMachine kenttä",
+
+"ee_field" =>
+"ExpressionEngine kenttä",
+
+"none" =>
+"Älä tuo Dataa",
+
+"blurb" =>
+"Tiivistelmä",
+
+"body" =>
+"Runko",
+
+"more" =>
+"Lisää",
+
+"custom1" =>
+"Muokattu1",
+
+"custom2" =>
+"Muokattu2",
+
+"custom3" =>
+"Muokattu3",
+
+"you_must_select_fields" =>
+"Et valinnut kenttiä liitettäväksi ",
+
+"entries_remaining" =>
+"Viestejä jäljellä:",
+
+"entries_imported" =>
+"Weblogin viestit tuotu",
+
+"click_to_import_entries" =>
+"Tuo weblogien viestejä %x kaikkiaan %y",
+
+"batch_complete" =>
+"Paketti valmis...",
+
+"select_upload_blog" =>
+"Valitse kuvien latauskansio",
+
+"upload_instructions" =>
+"Jos pMachinen viestit sisältävät kuvia, täytyy nykyinen kuvakansion sijainti korjata uudeksi sijainniksi ",
+
+"upload_note" =>
+"Huom: tämä toimenpide ei siirrä itse kuvia, vaan korjaa likityksen. Sinun tulee manuaalisesti siirtää kuva uuteen hakemistoon",
+
+"note_regarding_categories" =>
+"Weblogien kategorioiden käsittely",
+
+"category_note" =>
+"Jos pMachinen weblogeilla on kategorioita, tulee identtiset kategoriat löytyä ExpressionEnginestä. Tuontiprosessin yhteydessä tarkistetaan vastaavatko pMachinen kategoriat ExpressionEnginen kategorioita. Jos vastaavaa ei löydy, ohitetaan kyseisen viestin kategoriat.",
+
+"import_mailinglist" =>
+"Tuo postituslista",
+
+"total_emails" =>
+"Jäseniä kaikkiaan:",
+
+"emails_remaining" =>
+"Email osoitteita jäljellä:",
+
+"emails_per_cycle" =>
+"Email osoitteita per paketti",
+
+"email_instructions" =>
+"Estääksemme serveri timeoutin, data tuodaan paketeissa. Email osoitteiden tuonti ei vaadi prosessoritehoja, joten 5000 per paketti on hyvä arvo useimmille serverille. ",
+
+"mailinglist" =>
+"Postituslista",
+
+"mailinglist_imported" =>
+"Postituslista on tuotu",
+
+"click_to_import_mailinglist" =>
+"Tuo osoitteita %x kaikkiaan %y",
+
+"clear_preferences" =>
+"Tyhjennä asetukset",
+
+"click_to_clear_prefs" =>
+"Klikkaa tästä tyhjentääsesi asetukset",
+
+"clear_preferences_info" =>
+"Tuontiprosessin aikana tallennetaan kaikki asetukset main config tiedostoon. Kun tuonti on suoritettu, on suositeltavaa tyhjentää tämä tieto. ",
+
+"you_are_done_importing" =>
+"Tuontiprosessi on suoritettu onnistuneesti!",
+
+''=>''
+);
+?>
