5c6db6bd7867bc92597bd3b52d83e522050b21f4
me-suzy <me.suzana@gmail.com>
2025-08-13 04:06:35 +0300

    Add Unsorted/EPal Pro/footer.php

---
+++ Unsorted/EPal Pro/footer.php
@@ -0,0 +1,9 @@
+<?
+echo "
+<a class=main href=?v=about>About Us</a>
+&nbsp;&nbsp;<a class=main href=?v=fees>Fees</a>
+&nbsp;&nbsp;<a class=main href=?v=legal>Legal</a>
+&nbsp;&nbsp;<a class=main href=?v=privacy>Privacy</a>
+&nbsp;&nbsp;<a class=main href=?v=contact>Contact</a>
+";
+?>
