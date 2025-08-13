1bee0c36f49c3966670e5a72d91e81c67b7f9133
me-suzy <me.suzana@gmail.com>
2025-08-13 05:00:56 +0300

    Add Unsorted/guestbook/lang/codes-bulgarian.php

---
+++ Unsorted/guestbook/lang/codes-bulgarian.php
@@ -0,0 +1,159 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE>Óñìèâêè è AG êîä</TITLE>
+<META content="text/html; charset=windows-1251" http-equiv=Content-Type>
+<style type="text/css">
+<!--
+td {  font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 8pt}
+-->
+</style>
+</HEAD>
+<BODY bgColor=#9CB5DC link=#000080 text=#000000 vLink=#000080>
+<CENTER>
+  <table width="95%" border="0" cellspacing="1" cellpadding="0">
+    <tr>
+      <td height="25"> Ê À Ê Â Î &nbsp;&nbsp;&nbsp;Ñ À &nbsp;&nbsp;&nbsp;Ó Ñ Ì È Â Ê È Ò Å?</td>
+    </tr>
+    <tr>
+      <td>
+        <p>'Óñìèâêèòå' ñà ìàëêè êàðòèíêè êîèòî ìîãàò äà áúäàò èçïîëçâàíè äà ïðèäàäàò åìîöèÿ íà Âàøåòî ñúîáùåíèå.Àêî ñòå èçïîëçâàëè e-mail èëè IRC, 
+          Âèå ñòå çàïîçíàòè âå÷å ñ òåçè "èçðàçè". Òóê ñòàíäàðòíèÿ òåêñòîâ íèç
+          àâòîìàòè÷íî ñå êîíâåðòèðà â óñìèâêà. Çàâúðòåòå ãëàâàòà ñè íà åäíà ñòðàíà
+          è àêî íå âèäèòå óñìèâêà; èçïîëçâàéòå ìàëêî âúîáðàæåíèå.</p>
+        <p> Òóê å ñïèñúêà ñ èçïîëçâàíèòå óñìèâêè: </p>
+      </td>
+    </tr>
+  </table>
+  <table bgcolor=#9CB5DC border=0 width="95%" cellspacing="1" cellpadding="4">
+    <tbody>
+    <tr>
+      <td bgcolor="#6288C6"><font color=#ffffff>Êàêâî äà íàïèøà</font></td>
+      <td bgcolor="#6288C6"><font color=#ffffff>Åìîöèÿ</font></td>
+      <td bgcolor="#6288C6"><font color=#ffffff>Êàðòèíêà, êîÿòî ñå ïîÿâÿâà</font></td>
+    </tr>
+
+<?php include ("./smilies.inc"); ?>
+
+    </tbody>
+  </table>
+  <br>
+  <table width="95%" border="0" cellspacing="1" cellpadding="0">
+    <tr>
+      <td height="25">Ê À Ê Â Î &nbsp;&nbsp;&nbsp; Å  &nbsp;&nbsp;&nbsp; Ó Ñ Ú Â Ú Ð Ø Å Í Ñ Ò Â À Í &nbsp;&nbsp;&nbsp;Ê Î Ä  &nbsp;&nbsp;&nbsp;Ç À  &nbsp;&nbsp;&nbsp; Ê Í È Ã À&nbsp;&nbsp;&nbsp;   Ç À &nbsp;&nbsp;&nbsp; Ã Î Ñ Ò È ? </td>
+    </tr>
+    <tr>
+      <td>
+        <p>AG êîäà å âàðèàíò íà HTML òàãîâåòå,  ñ êîèòî ìîæå áè âå÷å ñòå çàïîçíàòè. Îñíîâíî, ïîçâîëÿâà ôóíêöèîíàëíîñò è ñòèë â Âàøèòå
+          ñúîáùåíèÿ, çà êîèòî ñå èçèñêâà HTML. Âèå ìîæåòå äà èçïîëçâàòå AG êîä äîðè êîãàòî
+          HTML å çàáðàíåí â êíèãàòà çà ãîñòè. Âèå ìîæåòå äà ãî èçïîëçâàòå è êîãàòî HTML å ïîçâîëåí,
+          à ñúùî òàêà å è ïî-áåçîïàñåí äà óïîòðåáà (íåêîðåêòíèÿ ñèíòàêñèñ íÿìà äà ñúçäàäå ïðîáëåì).
+        <p>Òåêóù óñúâúðøåíñòâàí AG êîä:
+      </td>
+    </tr>
+  </table>
+<TABLE border=0 cellPadding=0 cellSpacing=0 width="95%" align="center">
+  <TBODY>
+  <TR>
+    <TD bgColor=#000000>
+      <TABLE border=0 cellPadding=0 cellSpacing=0 width="100%">
+        <TBODY>
+        <TR bgColor=#6288C6>
+          <TD><b><font color="#FFFFFF">URL õèïåðâðúçêà</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>Êîãàòî AG êîäà å àêòèâèðàí, âå÷å íå å íåîáõîäèìî äà èçïîëçâàòå
+            [URL] êîä çà äà ñúçäàäåòå õèïåðâðúçêà. Ïðîñòî íàïèøåòå ïúëíèÿ URL êàêòî å ïîñî÷åíî ïî-äîëó è õèïåðâðúçêàòà ùå áúäå ñúçäàäåíà
+            àâòîìàòè÷íî:
+            <UL>
+              <LI><font color="#800000">http://www.yourURL.com </font>
+              <LI><FONT color=#800000>www.yourURL.com </FONT>Çàáåëåæåòå, ÷å ìîæåòå äà èçïîëçâàòå ïúëíèÿ  http:// èëè êðàòêèÿ www
+                domain. Àêî àäðåñà íå çàïî÷âà ñ "www", Âèå òðÿáâà äà èçïîëçâàòå 
+                ïúëíèÿ "http://" àäðåñ. Ñúùî ìîæå äà èçïîëçâàòå https è  ftp URL
+                ïðåôèêñ â autolink ðåæèìà(when AGCode is ON). <BR>
+                <BR>
+              <LI>Ìîæå ñúùî äà èìàòå èñòèíñêà õèïåðâðúçêà [url] êîä. Ïðîñòî
+                èçïîëçâàéòå ñëåäíèÿ ôîðìàò: <br><br>
+                <CENTER>
+                  <FONT color=#ff0000>[url=http://www.atlanticradio.net]</FONT>hyperlink<FONT color=#ff0000>[/url]</FONT>
+                </CENTER><br><br>
+              <LI>
+                <P>Ñòàðèÿ [URL] êîä ïàê ùå ñè ðàáîòè, êàêòî å ïîêàçàíî. Ïðîñòî
+                  â ñëó÷àé,  âðúçêàòà å ïîêàçàíà â ñëåäíèÿ ïðèìåð(AGCode is
+                  in <FONT color=#ff0000>red</FONT>).
+                <P>
+                  <CENTER>
+                    <FONT color=#ff0000>[url]</FONT>http://www.atlanticradio.net<FONT color=#ff0000>[/url]</FONT>
+                  </CENTER>
+                <P>Â ãîðíèòå ïðèìåðè,  AG êîäà àâòîìàòè÷íî ãåíåðèðà
+                  õèïåðâðúçêà äî  URL êîéòî å îçíà÷åí. Òîâà ñúùî îñèãóðÿâà,
+                  ÷å âðúçêàòà ùå ñå îòâîðè â íîâ ïðîçîðåö ïðè êëèê
+                  âúðõó íåÿ.  </P>
+              </LI>
+            </UL>
+          </TD>
+        <TR bgColor=#6288C6>
+          <TD><b><font color="#FFFFFF">Email âðúçêè</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>Çà äà ïîñòàâèòå õèïåðâðúçêà íà email àäðåñ âúâ Âàøåòî ñúîáùåíèå, ïðîñòî îãðàäåòå
+             email àäðåñà, êàêòî å ïîêàçàíî â ñëåäíèÿ ïðèìåð(AGCode is in
+            <FONT color=#ff0000>red</FONT>).
+            <P>
+              <CENTER>
+                <FONT color=#ff0000>[email]</FONT>webmaster@atlanticradio.net<FONT color=#ff0000>[/email]</FONT>
+              </CENTER>
+            <P> AG êîäà àâòîìàòèìíî ãåíåðèðà õèïåðâðúçêà
+              êúì îçíà÷åíèÿ e-mail. </P>
+          </TD>
+        </TR>
+        <TR bgColor=#6288C6>
+          <TD><b><font color="#FFFFFF">Bold and Italics</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>Ìîæåòå äà ñúçäàâàòå áîëä è èòàëèê òåêñò êàòî îãðàæäàòå ñúîòâåòíèòå òåêñòîâè ñåêöèè ñúñ ñëåäíèòå [b] [/b] or [i] [/i] òàãîâå.
+            <P>
+              <CENTER>
+                Hello, <FONT color=#ff0000>[b]</FONT><B>John</B><FONT color=#ff0000>[/b]</FONT><BR><br>
+                Hello, <FONT color=#ff0000>[i]</FONT><I>Maria</I><font color="#FF0000">[i]</font>
+              </CENTER>
+          </TD>
+        </TR>
+        <TR bgColor=#6288C6>
+          <TD><b><font color="#FFFFFF">Äîáàâÿíå íà êàðòèíêè</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>Çà äà ïîñòàâèòå ãðàôè÷åí îáåêò âúâ Âàøåòî ñúîáùåíèå, ïðîñòî îãðàäåòå URL íà
+            ãðàôè÷íèÿ îáåêò, êàêòî å ïîêàçàíî â ñëåäíèÿ ïðèìåð(AGCode is in <FONT color=#ff0000>red</FONT>).
+            <P>
+              <CENTER>
+                <FONT color=#ff0000>[img]</FONT>http://www.yourURL.com/image/logo.gif<FONT color=#ff0000>[/img]</FONT>
+              </CENTER>
+            <P>Â ïðèìåðà AG êîäà ïðàâè àâòîìàòè÷íî Âàøèÿ ãðàôè÷åí îáåêò âèäèì. Çàáåëåæåòå "http://" ÷àñòòà àò URL å
+              ÍÅÎÁÕÎÄÈÌÀ çà  <FONT color=#ff0000>[img]</FONT> code.</P>
+          </TD>
+        </TR>
+        </TBODY>
+      </TABLE>
+    </TD>
+  </TR>
+  </TBODY>
+</TABLE>
+<table width="95%" border="0" cellspacing="1" cellpadding="4" align="center">
+  <tr>
+    <td><font color=#800000>Of Note</font><br>
+      Ìîæåòå äà èçïîëçâàòåè äâàòà HTML è AGCode çà åäíà è ñúùà ôóíêöèÿ. Ñàìî çàáåëåæåòå
+      ÷å AG êîäà íå å case ÷óâñòâèòåëåí (ìîæåòå äà èçïîëçâàòå <font color=#ff0000>[URL]</font>
+      è <font color=#ff0000>[url]</font>).<br><br>
+      <font color="#800000">Incorrect AGCode Usage:</font> <br>
+       <font color="#ff0000">[url]</font> www.atlanticradio.net <font color=#ff0000>[/url]</font> - íå îñòàâÿéòå ðàçñòîÿíèÿ ìåæäó êîäà â ñêîáèòå
+        .<br>
+        <br>
+        <font color="#ff0000">[email]</font>webmaster@atlanticradio.net<font color=#ff0000>[email]</font>  (<font color=#ff0000>[/email]</font>)
+    </td>
+  </tr>
+</table>
+</CENTER>
+<BR>
+</BODY>
+</HTML>
