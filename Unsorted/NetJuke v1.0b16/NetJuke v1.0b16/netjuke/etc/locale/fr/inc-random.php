b468b84d54283594295a926d955a828ab6aaf77e
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:51 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-random.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-random.php
@@ -0,0 +1,27 @@
+<?php
+
+##################################################
+
+# /community.php
+
+##################################################
+
+define( "RNDM_NO_TR", "Vous devez avoir au moins 2 chansons dans ce(s)\\ngenre(s) avant d\\'utiliser cette option." );
+
+define( "RNDM_FORM_HEADER", "SÉLECTEUR ALÉATOIRE DE MUSIQUE" );
+define( "RNDM_FORM_LIMIT_1", "Limiter La Liste À" );
+define( "RNDM_FORM_LIMIT_2", "Pistes (Maximum)" );
+define( "RNDM_FORM_GENRES", "Limiter Les Genres À" );
+define( "RNDM_FORM_GENRES_ALL", "Tous Les Genres" );
+define( "RNDM_FORM_TRACKMINTIME_1", "Durée Min. de Piste" );
+define( "RNDM_FORM_TRACKMINTIME_2", "Secondes par piste" );
+define( "RNDM_FORM_TRACKMAXTIME_1", "Durée Max. de Piste" );
+define( "RNDM_FORM_TRACKMAXTIME_2", "Secondes par piste" );
+define( "RNDM_FORM_BTN_SEARCH", "Générer La Sélection Aléatoire" );
+
+define( "RNDM_CLOSEWIN", "Fermer La Fenêtre" );
+define( "RNDM_CLOSEWIN_HELP", "Ferme cette fenêtre." );
+
+##################################################
+
+?>
