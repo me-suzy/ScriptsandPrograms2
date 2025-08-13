72b5aa5a769d754c0d126e722c0d7f9ae9acb49f
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:26 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_tr-batch-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_tr-batch-edit.php
@@ -0,0 +1,39 @@
+<?php
+
+##################################################
+
+# /admin/tr-batch-edit.php
+
+##################################################
+
+define( "TRBEDIT_NOTRID", "Mindestens einen Titel auswählen um fortzufahren." );
+
+define( "TRBEDIT_EDITED_MSG", "Der gewählte Titel wurde erfolgreich bearbeitet." );
+
+define( "TRBEDIT_HEADER", "INFORMATIONEN MEHRERE TITEL BEARBEITEN" );
+define( "TRBEDIT_SELECTED", "ausgewählt" );
+
+define( "TRBEDIT_FORM_TR", "Titel Name" );
+define( "TRBEDIT_FORM_AR", "Interpreten Name" );
+define( "TRBEDIT_FORM_AL", "Album Name" );
+define( "TRBEDIT_FORM_GE", "Genre Name" );
+define( "TRBEDIT_FORM_FN", "Ablageort" );
+define( "TRBEDIT_FORM_LC", "Anzahl gehört" );
+define( "TRBEDIT_FORM_RESET", "Zähler zurücksetzen" );
+define( "TRBEDIT_FORM_TR_IMG", "Bild Titel" );
+define( "TRBEDIT_FORM_TR_COM", "Kommentare" );
+define( "TRBEDIT_FORM_TR_LYR", "Texte" );
+
+define( "TRBEDIT_FORM_BTN_SAVE", "Speichern" );
+define( "TRBEDIT_FORM_BTN_RESET", "Zurücksetzen" );
+
+define( "TRBEDIT_PLAY", "Anhören" );
+define( "TRBEDIT_PLAY_HELP", "Klicken um diesen Titel nun anzuhören." );
+define( "TRBEDIT_CLOSEWIN", "Fenster schliessen" );
+define( "TRBEDIT_CLOSEWIN_HELP", "Schliesst dieses Fenster" );
+
+define( "TRBEDIT_ERROR_INSERT", "Fehler beim einfügen in:" );
+
+##################################################
+
+?>
