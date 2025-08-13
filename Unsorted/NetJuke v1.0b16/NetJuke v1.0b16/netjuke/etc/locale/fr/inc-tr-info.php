b09c7efff075d6435f29b757249f96f7446f6f8a
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:58 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-tr-info.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-tr-info.php
@@ -0,0 +1,42 @@
+<?php
+
+##################################################
+
+# /tr-info.php
+
+##################################################
+
+define( "TRINFO_NOID", "Veuilez choisir une piste valide." );
+
+define( "TRINFO_HEADER", "INFORMATION DÉTAILLÉE" );
+
+define( "TRINFO_FORM_TR", "Titre De Piste" );
+define( "TRINFO_FORM_AR", "Nom De l'Artiste" );
+define( "TRINFO_FORM_AL", "Nom De l'Album" );
+define( "TRINFO_FORM_GE", "Nom Du l'Artiste" );
+define( "TRINFO_FORM_TI", "Durée" );
+define( "TRINFO_FORM_TN", "Numéro De Piste" );
+define( "TRINFO_FORM_BR", "\"Bit Rate\"" );
+define( "TRINFO_FORM_SR", "\"Sample Rate\"" );
+define( "TRINFO_FORM_FK", "Type De Fichier" );
+define( "TRINFO_FORM_FS", "Taille Du Fichier" );
+define( "TRINFO_FORM_FN", "Nom Du Fichier" );
+define( "TRINFO_FORM_LC", "Compteur D'Écoute" );
+
+define( "TRINFO_FORM_CM", "Voir les Commentaires" );
+define( "TRINFO_FORM_LY", "Voir les Paroles" );
+
+define( "TRINFO_IMG_TR", "Icône de Piste" );
+define( "TRINFO_IMG_AR", "Icône d'Artiste" );
+define( "TRINFO_IMG_AL", "Icône d'Album" );
+
+define( "TRINFO_PLAY", "Lecture" );
+define( "TRINFO_PLAY_HELP", "Cliquez pour débuter la lecture de cette piste." );
+define( "TRINFO_EDIT", "Mode D'Édition" );
+define( "TRINFO_EDIT_HELP", "Accède à la vue d'édition de cette piste afin de modifier l'information associée." );
+define( "TRINFO_CLOSEWIN", "Fermer La Fenêtre" );
+define( "TRINFO_CLOSEWIN_HELP", "Ferme Cette Fenêtre" );
+
+##################################################
+
+?>
