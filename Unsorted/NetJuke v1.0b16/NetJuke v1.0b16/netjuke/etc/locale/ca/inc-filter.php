997f0e8d05db58ab84c8bf78321044fb8dce44c1
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:20 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-filter.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-filter.php
@@ -0,0 +1,30 @@
+<?php
+
+##################################################
+
+# /filter.php
+
+##################################################
+
+define( "FILTER_HEADER", "Cerca filtre" );
+define( "FILTER_ARTISTS", "Intèrprets" );
+define( "FILTER_ALBUMS", "Àlbums" );
+define( "FILTER_GENRES", "Gèneres" );
+define( "FILTER_FOUND", "Trobat" );
+define( "FILTER_TRACKS", "Pistes" );
+
+define( "FILTER_LIST_ALL_HELP", "Llista totes les canÁons d'aquesta recerca" );
+define( "FILTER_PLAY_ALL_HELP", "Feu clic per reproduir totes les canÁons associades" );
+define( "FILTER_BACKBTN", "Retrocedir" );
+define( "FILTER_BACKBTN_HELP", "Torna a l'anterior llista" );
+
+define( "FILTER_BACKBTN", "Go Back" );
+define( "FILTER_BACKBTN_HELP", "Returns To The Previous Listing" );
+define( "FILTER_PAGETOP", "Capçalera" );
+define( "FILTER_PAGETOP_HELP", "Torna a la capçalera" );
+define( "FILTER_CLOSEWIN", "Tanca la finestra" );
+define( "FILTER_CLOSEWIN_HELP", "Tanca aquesta finestra" );
+
+##################################################
+
+?>
