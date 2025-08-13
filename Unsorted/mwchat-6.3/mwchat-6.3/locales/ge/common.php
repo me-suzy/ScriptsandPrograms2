e9fc1cc65986d1d3825bd8ca108c3436f3a5b2c7
me-suzy <me.suzana@gmail.com>
2025-08-13 06:32:08 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/common.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/common.php
@@ -0,0 +1,77 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[COMMON_Registered_Only] = "Nur für registrierte Benutzer.";
+
+$LOCALE[COMMON_Unregistered_Only] = "Nur für nicht registrierte Benutzer.";
+
+$LOCALE[COMMON_Username] = "Benutzername";
+
+$LOCALE[COMMON_Function_Invalid] = "Ist ein unzulässiger Befehl oder für Sie nicht verwendbar .";
+
+$LOCALE[COMMON_Operator] = "wurde zum Operator gemacht";
+
+$LOCALE[COMMON_No_Operator] = "ist kein Operator mehr.";
+
+$LOCALE[COMMON_Login_Button] = "Login jetzt";
+
+$LOCALE[COMMON_Not_Accept] = "Nimmt keine privaten Nachrichten an.";
+
+$LOCALE[COMMON_Help] = "Hilfe";
+
+$LOCALE[COMMON_No_Encryption] = "MWChat hat festgestellt das Sie mcrypt nicht in php installiert haben, Verschlüsselung wurde gesperrt.";
+
+$LOCALE[COMMON_System_Message] = "Systemnachricht";
+
+$LOCALE[COMMON_Click_Close] = "Klicken um zu schließen";
+
+$LOCALE[COMMON_YOB] = "Geburtsjahr";
+
+$LOCALE[COMMON_Location] = "Position";
+
+$LOCALE[COMMON_Job] = "Beruf";
+
+$LOCALE[COMMON_Hobbies] = "Hobbys";
+
+$LOCALE[COMMON_Status] = "Status";
+
+$LOCALE[COMMON_Comments] = "Zusätzliche Bemerkungen";
+
+$LOCALE[COMMON_Password] = "Neues Passwort";
+
+$LOCALE[COMMON_Confirm_Password] = "Bestätigen Sie das neue Passwort";
+
+$LOCALE[COMMON_Logout] = "Logout von MWChat";
+
+$LOCALE[COMMON_Err_Pass] = "Die Kennwörter, die Sie eingetragen haben, stimmen nicht überein oder sind nicht mindestens fünf Buchstaben lang.";
+
+$LOCALE[COMMON_HelpClick] = "Klicken Sie hier für Hilfe";
+
+$LOCALE[COMMON_Text] = "Nachricht";
+
+$LOCALE[COMMON_Bug_Report] = "Fehler Beschreibung";
+
+$LOCALE[COMMON_Reason] = "Grund";
+
+$LOCALE[COMMON_Action] = "Aktion";
+
+$LOCALE[COMMON_Topic] = "Thema";
+
+$LOCALE[COMMON_URL] = "URL";
+
+$LOCALE[COMMON_Alias] = "Ist eine Namensabkürzung für den Befehl ";
+
+$LOCALE[COMMON_Room_Name] = "Raum Name";
+
+$LOCALE[COMMON_Topic] = "Keiner";
+
+?>
