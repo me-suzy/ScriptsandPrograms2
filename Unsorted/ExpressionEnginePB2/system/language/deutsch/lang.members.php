9b4a7a904debc057ed816dd86d2d14bae2da279a
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:15 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.members.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.members.php
@@ -0,0 +1,644 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Members
+//----------------------------
+
+
+"can_moderate_comments" =>
+"Darf Kommentare Moderieren",
+
+"total_members" =>
+"Anzahl der Mitglieder:",
+
+"view_members" =>
+"Mitglieder ansehen",
+
+"validate_selected" =>
+"Ausgew&auml;hlte Mitglieder aktivieren",
+
+"delete_selected" =>
+"Ausgew&auml;hlte Mitglieder l&ouml;schen",
+
+"send_email_notification" =>
+"Eine Benachrichtigung an jedes aktivierte Mitglied versenden",
+
+"your_account_validated" =>
+"Ihr Account wurde aktiviert.",
+
+"your_account_ready" =>
+"Ihr Account wurde aktiviert und wartet jetzt auf Sie!.",
+
+"thank_you",
+"Danke!",
+
+"no_members_to_validate" =>
+"Zur Zeit sind keine Mitglieder im Wartestand.",
+
+"members_are_validated" =>
+"Mitglied(er) aktiviert",
+
+"members_are_deleted" =>
+"Mitglied(er) gel&ouml;scht",
+
+"ascending" =>
+"Nach Datum, &auml;lteste zuerst",
+
+"descending" =>
+"Nach Datum, neueste zuerst",
+
+"alpha" =>
+"Alphabetisch",
+
+"alpha_desc" =>
+"Umgekehrt alphabetisch",
+
+"username_asc" =>
+"Nach Username, aufsteigend",
+
+"username_desc" =>
+"Nach Username, absteigend",
+
+"screen_name_asc" =>
+"Nach Screen-Name, aufsteigend",
+
+"screen_name_desc" =>
+"Nach Screen-Name, absteigend",
+
+"email_asc" =>
+"Nach E-Mail, aufsteigend",
+
+"email_desc" =>
+"Nach E-Mail, absteigend",
+
+"sort_order" =>
+"Reihenfolge",
+
+"join_date" =>
+"Beitritt",
+
+"last_visit" =>
+"Letzter Besuch",
+
+"member_group" =>
+"Mitgliedsgruppe",
+
+"group_id" =>
+"Gruppen-ID",
+
+"no_members_matching_that_criteria" =>
+"Es wurden keine Mitglieder gefunden, die Ihren Suchkriterien entsprechen.",
+
+"mbrs" =>
+"Mitglieder",
+
+"edit_profile" =>
+"Profil bearbeiten",
+
+"register_member" =>
+"Neues Mitglied anmelden",
+
+"edit_group" =>
+"Gruppe bearbeiten",
+
+"group_title" =>
+"Gruppentitel",
+
+"security_lock" =>
+"Security-Lock",
+
+"group_lock" =>
+"Gruppen-Security Lock",
+
+"enable_lock" =>
+"Gruppen-Security Lock aktivieren",
+
+"locked" =>
+"Gesperrt",
+
+"locked" =>
+"Entsperrt",
+
+"lock_description" =>
+"Wenn eine Gruppe gesperrt ist, kann nur ein Super-Admin dieser Gruppe Mitglieder zuordnen. Wenn auch andere Mitglieder Accounts verwalten, empfiehlt es sich, nur die Gruppen zu entsperren, denen sie Mitglieder zuordnen d&uuml;rfen.",
+
+"locked" =>
+"Gesperrt",
+
+"unlocked" =>
+"Entsperrt",
+
+"create_new_member_group" =>
+"Neue Mitgliedsgruppe erstellen",
+
+"member_has_cp_access" =>
+"hat Zugang zum Control-Panel",
+
+"edit_member_group" =>
+"Mitgliedsgruppe bearbeiten",
+
+"create_member_group" =>
+"Mitgliedsgruppe erstellen",
+
+"delete_member_group" =>
+"Mitgliedsgruppe l&ouml;schen",
+
+"member_group_deleted" =>
+"Gruppe gel&ouml;scht",
+
+"delete_member_group_confirm" =>
+"Soll diese Gruppe wirklich gel&ouml;scht werden?",
+
+"delete_member" =>
+"Mitglied l&ouml;schen",
+
+"delete_member_confirm" =>
+"Soll dieses Mitglied wirklich gel&ouml;scht werden?",
+
+"delete_members_confirm" =>
+"Sollen diese Mitglieder wirklich gel&ouml;scht werden?",
+
+"can_not_delete_superadmin" =>
+"Dieses Mitglied k&ouml;nnen Sie nicht l&ouml;schen.",
+
+"member_deleted" =>
+"Mitglied gel&ouml;scht",
+
+"members_deleted" =>
+"Mitglieder gel&ouml;scht",
+
+//----------------------------
+// Member banning
+//----------------------------
+
+"user_banning" =>
+"Nutzersperrung",
+
+"ban_preferences_updated" =>
+"Einstellungen aktualisiert",
+
+"ip_address_banning" =>
+"Gesperrte IP-Adressen",
+
+"ip_banning_instructions" =>
+"F&uuml;r jede IP-Adresse eine neue Zeile beginnen",
+
+"ip_banning_instructions_cont" =>
+"Nutzen Sie wildcards f&uuml;r teilweise bekannte IP-Adressen. Beispiel:  123.345.*",
+
+"email_address_banning" =>
+"Gesperrte E-Mail-Adresse",
+
+"email_banning_instructions" =>
+"F&uuml;r jede E-Mail-Adresse eine neue Zeile beginnen",
+
+"email_banning_instructions_cont" =>
+"Nutzen Sie wildcards f&uuml;r teilweise bekannte E-Mailadressen. Beispiel:  *@domain.com",
+
+"ban_options" =>
+"Wenn ein Nutzer versucht, von einer gesperrten IP aus die Seite zu sehen,",
+
+"restrict_to_viewing" =>
+"auf Nur-Sehen begrenzen",
+
+"show_this_message" =>
+"diese Nachricht anzeigen",
+
+"send_to_site" =>
+"ihn zu dieser Seite weiterleiten",
+
+"username_banning" =>
+"Gesperrte Usernamen",
+
+"username_banning_instructions" =>
+"F&uuml;r jeden Usernamen eine neue Zeile beginnen",
+
+"screen_name_banning" =>
+"Gesperrte Screen-Namen",
+
+"screen_name_banning_instructions" =>
+"F&uuml;r jeden Screen-Namen eine neue Zeile beginnen",
+
+
+//----------------------------
+// Member group settings
+//----------------------------
+
+"only_superadmins_can_admin_groups" =>
+"Nur Super-Admins haben Zugang zur Mitgliedsgruppen-Seite.",
+
+"create_group_based_on_old" =>
+"Neue Gruppe als Kopie einer existierenden erstellen",
+
+"group_name" =>
+"Name der Mitgliedsgruppe",
+
+"missing_group_title" =>
+"Die Mitgliedsgruppe braucht einen Namen",
+
+"site_access" =>
+"Site-Zugang",
+
+"can_view_online_system" =>
+"Kann die Seite sehen, wenn sie online ist",
+
+"can_view_offline_system" =>
+"Kann die Seite sehen, wenn sie offline ist",
+
+"mbr_profile_privs" =>
+"Mitglieder-Profilrechte",
+
+"can_view_profiles" =>
+"Kann &ouml;ffentliche Profile ansehen",
+
+"commenting_privs" =>
+"Kommentarrechte",
+
+"can_post_comments" =>
+"Kann kommentieren",
+
+"cp_weblog_post_privs" =>
+"Weblog-Zuweisung",
+
+"cp_module_access_privs" =>
+"Modul-Rechte",
+
+"can_access_mod" =>
+"Hat Zugang zum Modul:",
+
+"global_cp_access" =>
+"Control-Panel-Zugang",
+
+"can_access_cp" =>
+"Hat Zugang zum Control-Panel?",
+
+"cp_section_access" =>
+"Zugang zu Bereichen des Control-Panels",
+
+"can_access_publish" =>
+"Hat Zugang zur POSTEN-Seite",
+
+"can_access_edit" =>
+"Hat Zugang zur BEARBEITEN-Seite",
+
+"can_access_design" =>
+"Hat Zugang zur VORLAGEN-Seite",
+
+"can_access_comm" =>
+"Hat Zugang zur KOMMUNIKATION-Seite",
+
+"can_access_modules" =>
+"Hat Zugang zur MODULE-Seite",
+
+"can_access_admin" =>
+"Hat Zugang zur ADMIN-Seite",
+
+"can_ban_users" =>
+"Darf Nutzer sperren",
+
+"cp_admin_privs" =>
+"Control-Panel-Verwaltungsrechte",
+
+"cp_comment_privs" =>
+"Kommentar-Verwaltungsrechte",
+
+"can_view_other_comments" =>
+"Darf Kommentare zu den Eintr&auml;gen anderer ansehen",
+
+"can_edit_own_comments" =>
+"Darf Kommentare zu eigenen Eintr&auml;gen bearbeiten",
+
+"can_delete_own_comments" =>
+"Darf Kommentare zu eigenen Eintr&auml;gen l&ouml;schen",
+
+"can_edit_all_comments" =>
+"Darf Kommentare zu allen Eintr&auml;gen bearbeiten",
+
+"can_delete_all_comments" =>
+"Darf Kommentare zu allen Eintr&auml;gen l&ouml;schen",
+
+"can_admin_weblogs" =>
+"Darf Weblogs und Weblogeinstellungen verwalten",
+
+"can_admin_members" =>
+"Darf Mitglieder-Accounts verwalten",
+
+"can_delete_members" =>
+"Darf Mitglieder l&ouml;schen",
+
+"can_admin_mbr_groups" =>
+"Darf die Gruppenzugeh&ouml;rigkeit eines Mitglieds &auml;ndern (nur in entsperrten Gruppen)",
+
+"can_admin_mbr_templates" =>
+"Darf Profilvorlagen verwalten",
+
+"can_admin_utilities" =>
+"Darf System-Tools verwalten",
+
+"can_admin_templates" =>
+"Darf Vorlagen und Vorlagengruppen verwalten",
+
+"can_admin_preferences" =>
+"Darf allgemeine Einstellungen verwalten",
+
+"can_admin_modules" =>
+"Darf Module installieren und entfernen",
+
+"cp_email_privs" =>
+"E-Mail-Rechte",
+
+"can_send_email" =>
+"Darf E-Mail &uuml;ber das Control-Panel verschicken",
+
+"can_email_members" =>
+"Darf E-Mail an einzelne Mitglieder schicken",
+
+"can_email_member_groups" =>
+"Darf E-Mail an Mitgliedsgruppen schicken",
+
+"can_email_mailinglist" =>
+"Darf E-Mail an Mailing-Liste schicken",
+
+"can_send_cached_email" =>
+"Darf gespeicherte/gesendete Mails ansehen",
+
+"warning" =>
+"WARNUNG:",
+
+"super_admin_edit_note" =>
+"Hinweis: Sie k&ouml;nnen nur den Namen der Super-Admin-Gruppe &auml;ndern",
+
+"be_careful_assigning_groups" =>
+"Seien Sie SEHR vorsichtig beim Zuweisen von Gruppeneinstellungen - besonders der hervorgehobenen.",
+
+"member_group_updated" =>
+"Mitgliedsgruppe aktualisiert:",
+
+"member_group_created" =>
+"Mitgliedsgruppe erstellt:",
+
+"cp_weblog_privs" =>
+"Posting- und -Bearbeitungsrechte",
+
+"can_view_other_entries" =>
+"Darf Eintr&auml;ge von anderen ansehen",
+
+"can_post_in" =>
+"Darf Eintr&auml;ge ver&ouml;ffentlichen und editieren in:",
+
+"can_edit_other_entries" =>
+"Darf Eintr&auml;ge von anderen bearbeiten",
+
+"can_assign_post_authors" =>
+"Darf den Autorennamen beim Posten von Eintr&auml;gen &auml;ndern",
+
+"can_delete_self_entries" =>
+"Darf eigene Eintr&auml;ge l&ouml;schen",
+
+"can_delete_all_entries" =>
+"Darf alle Eintr&auml;ge l&ouml;schen",
+
+"cp_template_access_privs" =>
+"Vorlagen-Editierrechte",
+
+"can_access_tg" =>
+"Darf Vorlagen in dieser Gruppe &auml;ndern:",
+
+"can_email_from_profile" =>
+"Darf E-Mail an andere Mitglieder durch die Profil-Mailkonsole verschicken",
+
+
+//----------------------------
+// Member fields
+//----------------------------
+
+"for_profile_page" =>
+"Wird auf der Profilseite angezeigt",
+
+"custom_member_fields" =>
+"Spezielle Profilfelder",
+
+"current_fields" =>
+"Mitglieder-Profilfelder",
+
+"edit_member_field" =>
+"Feld bearbeiten",
+
+"create_member_field" =>
+"Feld erstellen",
+
+"field_updated" =>
+"Feld aktualisiert",
+
+"create_new_profile_field" =>
+"Neues Profilfeld erstellen",
+
+"is_field_public" =>
+"Ist dieses Feld in &ouml;ffentlichen Profilen sichtbar?",
+
+"is_field_public_cont" =>
+"Wenn Sie &quot;nein&quot; w&auml;hlen, wird es nur f&uuml;r Admins sichtbar sein.",
+
+"is_field_reg" =>
+"Ist dieses Feld auf der Anmeldeseite sichtbar?",
+
+"field_width" =>
+"Feldbreite",
+
+"field_width_cont" =>
+"(in Pixels oder Prozent)",
+
+"fieldname" =>
+"Feld-Name",
+
+"fieldname_cont" =>
+"Ein Wort, keine Leerzeichen, Unterstreichungen und Trennungsstriche sind erlaubt",
+
+"field_format" =>
+"Text-Format",
+
+"max_length" =>
+"Maximall&auml;nge",
+
+"max_length_cont" =>
+"F&uuml;r Textfelder",
+
+"text_area_rows" =>
+"Textfeld-Zeilen",
+
+"text_area_rows_cont" =>
+"F&uuml;r Textfelder",
+
+"pull_down_items" =>
+"Auswahl-Optionen",
+
+"pull_down_items_cont" =>
+"F&uuml;r Drop-Down-Menus",
+
+"pull_down_instructions" =>
+"Jede Option auf eine eigene Zeile",
+
+"invalid_characters" =>
+"Der eingegebene Feldname enth&auml;lt ung&uuml;ltige Zeichen.",
+
+"member_data_will_be_deleted" =>
+"Alle Mitgliedsdaten in diesem Feld werden dauerhaft gel&ouml;scht.",
+
+"profile_field_deleted" =>
+"Profilfeld gel&ouml;scht:",
+
+"no_custom_profile_fields" =>
+"Zur Zeit gibt es keine benutzerdefinierten Profilfelder",
+
+"member_search" =>
+"Mitgliedersuche",
+
+"member_search_results" =>
+"Suchergebnisse",
+
+"no_search_results" =>
+"Ihre Suche hat kein Ergebnis erbracht.",
+
+"member_search_instructions" =>
+"F&uuml;llen Sie mindestens ein Feld aus. Sie k&ouml;nnen W&ouml;rter ganz oder teilweise eingeben.",
+
+"can_not_delete_only_member" =>
+"Sie k&ouml;nnen das einzige Mitglied in der Datenbank nicht l&ouml;schen.",
+
+"can_not_delete_super_admin" =>
+"Sie k&ouml;nnen einen Super-Admin nur l&ouml;schen, wenn es noch mindestens einen anderen gibt.",
+
+"must_be_superadmin_to_delete_one" =>
+"Nur ein Super-Admin kann einen Super-Admin l&ouml;schen.",
+
+"email_console_log" =>
+"E-Mailkonsolen-Logs",
+
+"no_cached_email" =>
+"Zur Zeit sind keine E-Mail-Nachrichten gespeichert.",
+
+"email_title" =>
+"E-Mail-Titel",
+
+"from" =>
+"Von",
+
+"to" =>
+"An",
+
+"message_sent_to" =>
+"Nachricht gesendet an:",
+
+"email_deleted" =>
+"E-Mail-Nachricht(en) gel&ouml;scht",
+
+"template_set_names" =>
+"Verf&uuml;gbare Vorlagens&auml;tze",
+
+"unable_to_find_templates" =>
+"Ich kann die Profil-Vorlagen nicht finden.",
+
+"unable_to_find_template_file" =>
+"Ich kann die Mitglieds-Vorlagendatei nicht finden.",
+
+"template_updated" =>
+"Vorlage wurde aktualisiert",
+
+"template_set" =>
+"Vorlagensatz:",
+
+"file_not_writable" =>
+"Hinweis: Diese Datei kann nicht &uuml;berschrieben werden.",
+
+"error_opening_template" =>
+"Fehler: Ich kann die Vorlagendatei nicht &ouml;ffnen, um ihren Inhalt zu lesen.",
+
+"file_writing_instructions" =>
+"Sie k&ouml;nnen die &Auml;nderungen zu dieser Vorlage nur speichern, wenn Sie diese Datei auf 666 setzen:",
+
+"aim_console" =>
+"AOL Instant Messenger Console",
+
+"breadcrumb" =>
+"Breadcrumb",
+
+"copyright" =>
+"Copyright-Hinweis",
+
+"custom_profile_fields" =>
+"Formular f&uuml;r benutzerdefinierte Formularfelder",
+
+"edit_profile_form" =>
+"Mitgliederprofil-Formular",
+
+"email_form" =>
+"E-Mailkonsole",
+
+"email_prefs_form" =>
+"Formular f&uuml;r E-Mail-Einstellungen",
+
+"email_user_message" =>
+"E-Mail-Nachrichten",
+
+"forgot_form" =>
+"Formular f&uuml;r vergessenes Passwort",
+
+"home_page" =>
+"Mitglieds-Profil-Homepage",
+
+"icq_console" =>
+"ICQ Console",
+
+"localization_form" =>
+"Formular f&uuml;r Ortseinstellungen",
+
+"login_form" =>
+"Login-Formular",
+
+"memberlist" =>
+"Mitgliederliste",
+
+"memberlist_rows" =>
+"Mitgliederlisten-Zeilen",
+
+"menu" =>
+"Mitglieds-Profil-Menu",
+
+"notepad_form" =>
+"Notizblock-Formular",
+
+"password_change_warning" =>
+"Warnung bei Passwort&auml;nderung",
+
+"public_custom_profile_fields" =>
+"Zeilen in benutzerdefinierten Profilfeldern",
+
+
+"public_profile" =>
+"&Ouml;ffentliches Mitgliedsprofil",
+
+"registration_form" =>
+"Anmeldeformular",
+
+"stylesheet" =>
+"CSS-Stylesheet",
+
+"success" =>
+"Aktualisierungsnachricht",
+
+"username_change_disallowed" =>
+"Nachricht zu unzul&auml;ssiger Namens&auml;nderung",
+
+"username_password_form" =>
+"Username/Passwort-Formular",
+
+"username_row" =>
+"Usernamen-Formular-Zeile",
+
+
+// END
+''=>''
+);
+?>
