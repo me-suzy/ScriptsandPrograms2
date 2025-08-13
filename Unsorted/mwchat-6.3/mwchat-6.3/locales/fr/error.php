452002f98af8038b1ff3342319667184e6b049a9
me-suzy <me.suzana@gmail.com>
2025-08-13 06:31:34 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/error.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/error.php
@@ -0,0 +1,52 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[ERROR_License] = "Il y a une erreur avec votre permis. S'il vous plaît MWChat de permis maintenant.";
+
+$LOCALE[ERROR_AlreadyIn] = "Le nom que vous avez choisi est déjà dans l'usage.";
+
+$LOCALE[ERROR_Title] = "Erreur";
+
+$LOCALE[ERROR_Logout] = "Vous avez été hors noté.";
+
+$LOCALE[ERROR_Expired] = "Votre login a expiré, s'il vous plaît login encore.";
+
+$LOCALE[ERROR_RoomLong] = "Cette salle nomme vous choisissez êtes à long.";
+
+$LOCALE[ERROR_Live] = "Le système est actuellement indisponible.";
+
+$LOCALE[ERROR_Profanity] = "L'information que vous avez spécifiée contient l'impiété.";
+
+$LOCALE[ERROR_NoData]   = "S'il vous plaît remplit tous champs.";
+
+$LOCALE[ERROR_BadData]  = "S'il vous plaît spécifier des données valides.";
+
+$LOCALE[ERROR_Serial_Invalid] = "Votre numéro de série est nul, s'il vous plaît re enregistre votre produit.";
+
+$LOCALE[ERROR_Serial_None] = "Ne peut pas trouver un numéro de série sur le disque, s'il vous plaît re enregistre votre produit.";
+
+$LOCALE[ERROR_Critical] = "Erreur critique!";
+
+$LOCALE[ERROR_Javascript] = "Ce programme exige un JavaScript browser capable.";
+
+$LOCALE[ERROR_Browser] = "Ce programme ne fonctionnera pas correctement avec votre browser.";
+
+$LOCALE[ERROR_Restricted] = "Le nom que vous avez choisi est limité.";
+
+$LOCALE[ERROR_BadPass]  = "S'il vous plaît entrer un mot de passe valide.";
+
+$LOCALE[ERROR_IP] = "Le IP adresse votre utilisation a changé. Vous avez été hors noté pour les raisons de sécurité.";
+
+$LOCALE[ERROR_PHPV] = "MWChat exige une version de PHP minimum de";
+
+?>
