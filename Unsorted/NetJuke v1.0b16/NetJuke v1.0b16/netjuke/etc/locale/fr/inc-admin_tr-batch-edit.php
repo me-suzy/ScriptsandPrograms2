f20d7afbada8be9de443beb20accb1bbd4ddfa38
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:11 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_tr-batch-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_tr-batch-edit.php
@@ -0,0 +1,39 @@
+<?php
+
+##################################################
+
+# /admin/tr-batch-edit.php
+
+##################################################
+
+define( "TRBEDIT_NOTRID", "Veuillex sélectioner au moins une piste à être éditée." );
+
+define( "TRBEDIT_EDITED_MSG", "Les pistes sélectionnées on été éditées avec succés." );
+
+define( "TRBEDIT_HEADER", "ÉDITION DE PISTES MULTIPLES" );
+define( "TRBEDIT_SELECTED", "sélectionnées" );
+
+define( "TRBEDIT_FORM_TR", "Titre De Piste" );
+define( "TRBEDIT_FORM_AR", "Nom De l'Artiste" );
+define( "TRBEDIT_FORM_AL", "Nom De l'Album" );
+define( "TRBEDIT_FORM_GE", "Nom Du Genre" );
+define( "TRBEDIT_FORM_FN", "Location" );
+define( "TRBEDIT_FORM_LC", "Compteur D'Écoute" );
+define( "TRBEDIT_FORM_RESET", "Mettre À Zéro" );
+define( "TRBEDIT_FORM_TR_IMG", "Icône de Piste" );
+define( "TRBEDIT_FORM_TR_COM", "Commentaires" );
+define( "TRBEDIT_FORM_TR_LYR", "Paroles" );
+
+define( "TRBEDIT_FORM_BTN_SAVE", "Sauvegarder" );
+define( "TRBEDIT_FORM_BTN_RESET", "Recommencer" );
+
+define( "TRBEDIT_PLAY", "Lecture" );
+define( "TRBEDIT_PLAY_HELP", "Cliquez pour débuter la lecture de cette piste." );
+define( "TRBEDIT_CLOSEWIN", "Fermer La Fenêtre" );
+define( "TRBEDIT_CLOSEWIN_HELP", "Ferme Cette Fenêtre" );
+
+define( "TRBEDIT_ERROR_INSERT", "Erreur de création avec" );
+
+##################################################
+
+?>
