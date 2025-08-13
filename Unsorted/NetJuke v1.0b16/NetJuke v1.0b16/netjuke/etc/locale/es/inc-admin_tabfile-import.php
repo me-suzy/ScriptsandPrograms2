31c31a432e1089295c37dfff1d5dab4147002459
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:49 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_tabfile-import.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_tabfile-import.php
@@ -0,0 +1,37 @@
+<?php
+
+##################################################
+
+# /admin/tabfile-import.php
+
+##################################################
+
+define( "TFIMPT_HEADER", "LENGUETA DELIMITADORA DEL ARCHIVO DE IMPORTACIÓN" );
+
+define( "TFIMPT_VIEW", "Ver archivo" );
+define( "TFIMPT_PROCEED", "Proceder con la importación del archivo" );
+define( "TFIMPT_DELETE", "Borra los archivos seleccionados" );
+define( "TFIMPT_DELETE_SUCCESS", "Eliminados" );
+define( "TFIMPT_DELETE_ERROR", "Error de permisos: No se puede eliminar" );
+
+define( "TFIMPT_FOUND_1", "Encontrar" );
+define( "TFIMPT_FOUND_2", "Archivo(s) a procesar en la carpeta de importar por defecto." );
+
+define( "TFIMPT_PROCESS", "Procesando" );
+
+define( "TFIMPT_IGNORE_COLNAMES", "Ignorando nombres de columnas" );
+
+define( "TFIMPT_INSERT_1", "Insertados" );
+define( "TFIMPT_INSERT_2", "Cancion(es)" );
+define( "TFIMPT_INSERT_3", "Artista(s)" );
+define( "TFIMPT_INSERT_4", "Álbun(es)" );
+define( "TFIMPT_INSERT_5", "Género(s)" );
+
+define( "TFIMPT_ARCHIVE_1", "Archivando" );
+define( "TFIMPT_ARCHIVE_2", "Como2" );
+
+define( "TFIMPT_DONE", "Terminado" );
+
+##################################################
+
+?>
