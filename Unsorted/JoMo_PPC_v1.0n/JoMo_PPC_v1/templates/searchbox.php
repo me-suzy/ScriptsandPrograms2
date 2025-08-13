a0e54d3edb9a3a6433ed176dd6921806487ece35
me-suzy <me.suzana@gmail.com>
2025-08-13 05:56:22 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/searchbox.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/searchbox.php
@@ -0,0 +1,9 @@
+<FORM NAME="formSearch" action="<%$selfURL%>" method="post">
+<input type="hidden" name="mode" value="search">
+<table bgcolor="#0066CC" cellpadding="5" border="1" bordercolor="#000000">
+<tr>
+<td bordercolor="#0066CC"><font color="#FFFFFF">Search for:</font>
+</td>   <td bordercolor="#0066CC"><input type="text" name="str"></td>
+<td bordercolor="#0066CC"><input type="submit" value="Search">
+</td></tr></table>
+</form>
