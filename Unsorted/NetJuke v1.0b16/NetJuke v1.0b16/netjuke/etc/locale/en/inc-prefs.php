c36bcafe113bcd6d1070167e690638ef23952a5b
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:18 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-prefs.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-prefs.php
@@ -0,0 +1,46 @@
+<?php
+
+##################################################
+
+# /prefs.php
+
+##################################################
+
+define( "PREFS_DENIED_1", "An unexpected error occured. Please try again." );
+
+define( "PREFS_CHECKFORM_1", "Please choose a Background color." );
+define( "PREFS_CHECKFORM_2", "Please choose a list of Fonts." );
+define( "PREFS_CHECKFORM_3", "Please choose a Font Size." );
+define( "PREFS_CHECKFORM_4", "Please choose a Text color." );
+define( "PREFS_CHECKFORM_5", "Please choose a Link color." );
+define( "PREFS_CHECKFORM_6", "Please choose an Active Link color." );
+define( "PREFS_CHECKFORM_7", "Please choose a Visited Link color." );
+define( "PREFS_CHECKFORM_8", "Please choose a Table Border color." );
+define( "PREFS_CHECKFORM_9", "Please choose a Table Header color." );
+define( "PREFS_CHECKFORM_10", "Please choose a Table Header font color." );
+define( "PREFS_CHECKFORM_11", "Please choose a Table Content color." );
+
+define( "PREFS_HEADER", "ACCOUNT INFORMATION" );
+define( "PREFS_CAPTION", "Use the form below to modify account information." );
+define( "PREFS_PALETTE", "Use this color palette to choose your environment." );
+
+define( "PREFS_FORMS_INVICN", "Inverse Icons" );
+define( "PREFS_FORMS_INVICN_HELP", "Enables users to inverse the color of the icons: Play, Get Info, Filter..." );
+define( "PREFS_FORMS_INVICN_ENABLED", "Enabled" );
+define( "PREFS_FORMS_FONTFACE", "Font Face" );
+define( "PREFS_FORMS_FONTSIZE", "Font Size" );
+define( "PREFS_FORMS_BGCOLOR", "Background Color" );
+define( "PREFS_FORMS_TEXT", "Text Color" );
+define( "PREFS_FORMS_LINK", "Links Color" );
+define( "PREFS_FORMS_ALINK", "Active Links Color" );
+define( "PREFS_FORMS_VLINK", "Visited Links Color" );
+define( "PREFS_FORMS_BORDER", "Border Color" );
+define( "PREFS_FORMS_HEADER", "Header Color" );
+define( "PREFS_FORMS_HEADERFC", "Header Font Color" );
+define( "PREFS_FORMS_CONTENT", "Content Color" );
+define( "PREFS_FORMS_BTN_SAVE", "Save" );
+define( "PREFS_FORMS_BTN_RESET", "Reset" );
+
+##################################################
+
+?>
