9a9b40a6fa76893cd11a25afafeca0938cea8edd
me-suzy <me.suzana@gmail.com>
2025-08-13 05:51:36 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/code/affiliate.main.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/code/affiliate.main.php
@@ -0,0 +1,60 @@
+<?
+/*
+###############################
+#
+# JoMo Easy Pay-Per-Click Search Engine v1.0
+#
+#
+###############################
+#
+# Date                 : September 16, 2002
+# supplied by          : CyKuH [WTN]
+# nullified by         : CyKuH [WTN]
+#
+#################
+#
+# This script is copyright L 2002-2012 by Rodney Hobart (JoMo Media Group),
+All Rights Reserved.
+#
+# The use of this script constitutes acceptance of any terms or conditions,
+#
+# Conditions:
+#  -> Do NOT remove any of the copyright notices in the script.
+#  -> This script can not be distributed or resold by anyone else than the
+author, unless special permisson is given.
+#
+# The author is not responsible if this script causes any damage to your
+server or computers.
+#
+#################################
+
+*/
+?>
+<?php
+
+/**
+affiliates. main
+*/
+
+/**
+input:
+affiliateID
+*/
+
+	checkAffiliatePage();
+
+ 
+ $memberType = "affiliate";
+ $tpl->assign("memberType",$memberType);
+ 
+
+ // member info
+ $affiliate = getMember($affiliateID, $memberType);
+ 
+ $tpl->assign("info",$affiliate["info"]);
+ $tpl->assign("account",$affiliate["account"]);
+ 
+ $tpl->display("template.affiliate.main.php");
+
+
+?>
