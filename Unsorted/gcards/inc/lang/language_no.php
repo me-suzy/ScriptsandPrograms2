71db9b79b8a9b5a835bc0de2611616bf2cf526c1
me-suzy <me.suzana@gmail.com>
2025-08-13 04:49:41 +0300

    Add Unsorted/gcards/inc/lang/language_no.php

---
+++ Unsorted/gcards/inc/lang/language_no.php
@@ -0,0 +1,148 @@
+<?
+// index.php
+$index01 = 'Alle kategorier';
+$index02 = 'Velg først et kort nedenfor!';
+
+// general navigation
+$nav01 = 'Tilbake';
+$nav02 = 'Neste';
+$nav03 = 'Hjem';
+$nav04 = 'Vis';
+$nav05 = 'gCards Administrasjons konsoll';
+$nav06 = 'Endre passord';
+$nav07 = 'Logg ut';
+$nav08 = 'Logg inn';
+$nav09 = 'Driftet av';
+$nav10 = 'rader pr side';
+$nav11 = 'Gå <a href="javascript:history.go(-1);">tilbake</a>';
+$nav12 = 'Returner til';
+
+
+
+// UIfunctions.php
+$uifunc01 = 'Bruker: ';
+
+// compose.php
+$compose01 = 'Kort er ikke valgt! Gå <a href="index.php">tilbake</a> for å velge et kort';
+$compose02 = 'Legg til tekst nedenfor og send ditt ePostkort!';
+$compose03 = 'Navn';
+$compose04 = 'Emailadresse';
+$compose05 = 'Fra:';
+$compose06 = 'Til:';
+$compose07 = 'Meddel meg når kortet er åpnet';
+$compose08 = 'Skill emailadressene med komma';
+
+// preview.php
+$preview01 = "Vennligst fyll inn alle felter i skjemaet! $nav11";
+$preview02 = 'er ikke en gyldig emailadresse';
+$preview03 = "Ugyldige emiladresse(r)! $nav11";
+$preview04 = 'Send';
+
+// sendcard.php
+$sendcard01 = 'Feil! Skjemaet ble ikke korrekt utfyllt. Vennligst gå <a href="javascript:history.go(-1);">tilbake</a>';
+$sendcard02 = 'feil innsetting: ';
+$sendcard03 = 'ePostkort sendt...';
+$sendcard04 = 'ePostkort kunne ikke sendes...';
+
+// getcard.php
+$getcard01 = 'Feil - ingen kort-ID';
+$getcard02 = '$to_email har åpnet ditt ePostkort';
+$getcard03 = 'Send $from_name et ePostkort';
+
+// showcard.php
+$showcard01 = 'Et ePostkort fra';
+
+// news.php
+$news01 = 'Arkiverte nyheter';
+$news02 = 'Innsendt';
+$news03 = 'av';
+$news04 = 'nyhetsartikler pr side';
+$news05 = 'Nyhter';
+$news06 = 'Subjekt';
+$news07 = 'Tekst';
+$news08 = 'Dato';
+$news09 = 'Forfatter';
+
+
+
+// user related text
+$user01 = 'Brukernavn';
+$user02 = 'Passord';
+$user03 = 'Du er ikke innlogget - kan ikke logge ut!';
+$user04 = 'Vellykket utlogging av systemet...';
+$user05 = 'Bekreft passord';
+$user06 = 'Emailadresse';
+$user07 = 'Rolle';
+$user08 = 'Bruker';
+$user09 = 'Passordene stemmer ikke - vennligst forsøk på nytt';
+$user10 = '(Leave blank to keep current value)';
+$user11 = 'Gammelt passord stemmer ikke med ditt eksisterende passord';
+$user12 = 'Vellykket oppdatering av passord!';
+$user13 = 'Gammelt Passord';
+$user14 = 'Nytt Passord';
+
+/*
+*********************
+** ADMIN DIRECTORY **
+*********************
+*/
+
+// Authentication
+$auth01 = 'Autentifikasjon feilet for bruker';
+$auth02 = 'Vennligst fyll inn både brukernavn og passord.';
+$auth03 = 'Du er ikke logget inn.';
+$auth04 = 'Du har ikke tilstrekkelige privilegier til å vise denne siden!';
+
+// admin.php
+$admin01 = 'Administrative valg';
+$admin02 = 'vedlikehold';
+$admin03 = "Kort - $admin02";
+$admin04 = "Kategori - $admin02";
+$admin05 = "Nyhet - $admin02";
+$admin06 = "Bruker - $admin02";
+$admin07 = 'Vis, legg til, oppdater og slett';
+$admin08 = 'ID';
+$admin09 = 'slettet fra database...';
+$admin10 = 'slettet fra server...';
+$admin11 = 'ikke';
+$admin12 = 'oppdatert';
+$admin13 = 'slettet';
+$admin14 = 'lagt til';
+$admin15 = 'Legg til et nytt';
+$admin16 = 'Last opp';
+
+// actions
+$action01 = 'Utfør';
+$action02 = 'Legg til';
+$action03 = 'Rediger';
+$action04 = 'Slett';
+$action05 = 'Vis';
+$action06 = 'Oppdater';
+$action07 = 'Avbryt';
+$action08 = 'Forhåndsvisning';
+
+// cards.php
+$cards01 = 'Bilde';
+$cards02 = 'Miniatyr';
+$cards03 = 'Ganger sendt';
+$cards04 = 'Kort';
+$cards05 = 'Kortnavn';
+$cards06 = 'Blank for automatisk miniatyr';
+
+// categories
+$cat01 = 'Kategori';
+$cat02 = 'Kategorinavn';
+$cat03 = 'Kort i kategori';
+
+// upload.php
+$upload01 = 'Bildet ble ikke lastet opp!';
+$upload02 = "fil-kopi-feil med 'move_uploaded_file' funksjon!";
+$upload03 = 'Vellykket bildeopplasting...';
+$upload04 = 'Fil-kopi-feil!';
+$upload05 = 'Vellykket opplasting av miniatyr...';
+$upload06 = 'Opplastet bilde brukt som miniatyr...';
+$upload07 = 'Vellykket miniatyropprettelse...';
+$upload08 = 'Vennligst last opp en miniatyr dersom bilet ikke er en JPEG-fil.';
+
+
+?>
