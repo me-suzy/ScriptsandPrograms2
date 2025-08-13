68741c3cb8ff0acaf12aecb42ccf2b82ada3d2ae
me-suzy <me.suzana@gmail.com>
2025-08-13 04:24:22 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.templates.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.templates.php
@@ -0,0 +1,330 @@
+<?php
+$L = array(
+
+"php_parse_location" =>
+"Stage at which PHP should be parsed",
+
+"input" =>
+"Input",
+
+"output" =>
+"Output",
+
+"template_loop" =>
+"Has causado una referencia circular (bucle) debido a un anidamiento inadecuado de subplantillas.",
+
+"template_intro" =>
+"En esta página se gestionan las plantillas.",
+
+"create_new_template_group" =>
+"Crear un nuevo grupo de plantillas",
+
+"new_template_group_form" =>
+"Nuevo grupo de plantillas",
+
+"edit_template_group_form" =>
+"Modificar un grupo de plantillas",
+
+"name_of_template_group" =>
+"Nombre del grupo de plantillas",
+
+"template_group_instructions" =>
+"El nombre debe de ser una sola palabra, sin espacios",
+
+"undersores_allowed" =>
+"(Se permiten guiones y barras bajas)",
+
+"new_template_form" =>
+"Nuevo formulario de plantilla",
+
+"edit_template_form" =>
+"Modificar formulario de plantilla",
+
+"name_of_template" =>
+"Nombre de la plantilla",
+
+"group_name" =>
+"Nombre del grupo",
+
+"order" =>
+"Orden",
+
+"reserved_name" =>
+"El nombre que has elegido es una palabra reservada que no se puede utilizar.",
+
+"type_of_template" =>
+"Tipo de plantilla",
+
+"webpage" =>
+"Página Web",
+
+"css_stylesheet" =>
+"Hoja de estilo CCS",
+
+"rss" =>
+"Página RSS",
+
+"missing_name" =>
+"Tu plantilla debe de tener un nombre",
+
+"template_name_taken" =>
+"El nombre que has seleccionado ya existe",
+
+"template_group_del_conf" =>
+"Confirmación de eliminación de grupo de plantillas",
+
+"template_del_conf" =>
+"Confirmación de eliminación de pantilla",
+
+"edit_template" =>
+"Modificar plantilla",
+
+"add_a_template" =>
+"Añadir una nueva plantilla",
+
+"delete_template_group" =>
+"Eliminar grupo",
+
+"edit_template_group" =>
+"Modificar grupo",
+
+"group_order" =>
+"Orden del grupo",
+
+"export_templates" =>
+"Exportar plantillas",
+
+"template_name" =>
+"Nombre de la plantilla:",
+
+"template_management" =>
+"Gestión de plantillas",
+
+"current_template" =>
+"Plantilla actual:",
+
+"preferences" =>
+"Preferencias",
+
+"prefs" =>
+"Prefs",
+
+"hits" =>
+"Impactos",
+
+"hit_counter" =>
+"Contador de impactos",
+
+"template_updated" =>
+"Plantilla actualizada",
+
+"template_created" =>
+"Plantilla creada",
+
+"form_is_empty" =>
+"Has enviado un formulario vacío",
+
+"all_fields_required" =>
+"Debes de llenar todos los campos",
+
+"illegal_characters" =>
+"El nombre sólo puede contener caracteres alfanuméricos (A-Z, a-z) (1-9), guiones (-) y barras bajas (_)",
+
+"edit_template_group_order" =>
+"Modificar orden del grupo",
+
+"template_group_updated" =>
+"Grupo de plantillas actualizado",
+
+"template_group_created" =>
+"Grupo de plantillas creado",
+
+"delete_this_group" =>
+"¿Borrar el siguiente Grupo de plantillas?",
+
+"all_templates_will_be_nuked" =>
+"¡¡¡TODAS LAS PLANTILLAS CONTENIDAS EN ESTE GRUPO SE VAN A BORRAR PERMANENTEMENTE!!!",
+
+"template_group_deleted" =>
+"Grupo de plantillas eliminado",
+
+"edit_group_order" =>
+"Modificar orden del grupo",
+
+"template_size" =>
+"Tamaño de la plantilla",
+
+"small" =>
+"Pequeño",
+
+"medium" =>
+"Medio",
+
+"large" =>
+"Grande",
+
+"view_rendered_template" =>
+"Visualizar plantilla",
+
+"save_history" =>
+"Guardar revisión",
+
+"view" =>
+"Ver",
+
+"clear" =>
+"Borrar",
+
+"select_all" =>
+"Seleccionar todo",
+
+"export" =>
+"Exportar",
+
+"export_as" =>
+"Exportar ficheros como",
+
+"export_will_be_zip" =>
+"Nota: los datos exportados se enviarán como un archivo ZIP",
+
+"choose_templates" =>
+"Selecciona las plantillas que deseas exportar como ficheros",
+
+"unable_to_write_files" =>
+"No se pueden grabar los ficheros en tu directorio de caché",
+
+"revision_history" =>
+"Ver historial de revisiones",
+
+"clear_revision_history" =>
+"Borrar historial de revisiones",
+
+"history_cleared" =>
+"Se ha borrado el historial de revisiones de la plantilla",
+
+"close_window" =>
+"Cerrar ventana",
+
+"template_preferences" =>
+"Preferencias de plantilla",
+
+"delete_template" =>
+"Borrar plantilla",
+
+"delete_this_template" =>
+"Borrar la plantilla siguiente",
+
+"template_deleted" =>
+"Plantilla eliminada",
+
+"default_site_page" =>
+"Indica la página por defecto del sitio maestro",
+
+"is_site_default" =>
+"¿Hacer que la plantilla índice de este grupo sea la página inicial de tu sitio?",
+
+"allow_php" =>
+"¿Se permite PHP en esta plantilla?",
+
+"enable_cache" =>
+"Activar la caché de plantillas",
+
+"cache_interval" =>
+"Intervalo de tiempo entre refrescos de caché",
+
+"in_minutes" =>
+"Tiempo en minutos",
+
+"php_security_warning" =>
+"Si se pone esta opción en &quot;si&quot; hay implicaciones de seguridad",
+
+"consult_manual" =>
+"Por favor, consulta el manual",
+
+"preferences_updated" =>
+"Preferencias actualizadas",
+
+"choose_default_data" =>
+"Usar los datos de la plantilla por defecto",
+
+"blank_template" =>
+"Ninguno - crear una plantilla vacía",
+
+"template_from_library" =>
+"Usar una plantilla de tu librería",
+
+"an_existing_template" =>
+"Duplicar una plantilla existente",
+
+"you_must_submit_a_name" =>
+"Tu plantilla debe de tener un nombre",
+
+"member_group" =>
+"Grupo de miembros",
+
+"can_view_template" =>
+"Se puede ver esta página",
+
+"require_logged_in" =>
+"¿Se requiere que el usuario esté conectado para poder ver esta página?",
+
+"restrict_to_group" =>
+"Restringir a grupos de miembros",
+
+"logged_out_select_blurb" =>
+"Si has seleccionado &quot;si&quot;, cuando un usuario desconectado intenta acceder a esta página, mostrar esta otra en su lugar:",
+
+"no_access_select_blurb" =>
+"Si has seleccionado &quot;no&quot; en cualquiera de las anteriores:",
+
+"no_access_instructions" =>
+"Cuando un usuario no autorizado intenta acceder a esta página, mostrarle esta otra en su lugar:",
+
+"template_notes" =>
+"Notas de la plantilla",
+
+"template_notes_desc" =>
+"Usa este formulario para guardar notas e información acerca de esta plantilla",
+
+"global_variables" =>
+"Variable globales",
+
+"global_variables_explanation" =>
+"Puedes definir tus propias variables y usarlas en cualquiera de tus plantillas",
+
+"no_global_variables" =>
+"Actualmente no hay variables globales",
+
+"global_variable_name" =>
+"Nombre de la variable",
+
+"create_new_global_variable" =>
+"Crear una nueva variable global",
+
+"variable_name" =>
+"Nombre de la variable",
+
+"global_variable_syntax" =>
+"Sintaxis de la variable",
+
+"variable_data" =>
+"Contenido de la variable",
+
+"global_var_created" =>
+"Variable global creada",
+
+"global_var_updated" =>
+"Variable global actualizada",
+
+"delete_global_variable" =>
+"Borrar variable global",
+
+"delete_this_variable" =>
+"¿Eliminar la siguiente variable global?",
+
+"variable_deleted" =>
+"Variable eliminada",
+
+''=>''
+);
+?>
