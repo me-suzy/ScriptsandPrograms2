0fe4479342072c2e85c2c0fe47f5e6762984add1
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:04 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_hidden.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_hidden.php
@@ -0,0 +1,20 @@
+<?php
+
+##################################################
+
+# /admin/hidden.php
+
+##################################################
+
+define( "HDDN_DENIED_BADDIR", "Das Verzeichniss welches durchsucht werden sollte ist nicht registriert als das Musik Verzeichniss in der Einstellungsdatei." );
+define( "HDDN_DENIED_NODIR_1", "Bitte geben Sie einen gültigen Pfad an, welcher nach versteckten Dateien durchsucht werden soll." );
+define( "HDDN_DENIED_NODIR_2", "Kein Musik Verzeichniss. In der Einstellungsdatei nachschauen." );
+
+define( "HDDN_HEADER", "VERSTECKTE DATEIEN SUCHEN" );
+define( "HDDN_CAPTION_1", "Durchsucht den angegebenen Pfad nach versteckten Dateien oder Verzeichnissen (deren Namen mit einem .Punkt beginnen) und erstellt eine entsprechende Batch-Datei um diese von der Kommandzeile aus zu löschen. Dies ist besonders beim Upload von einem Mac auf Unix nützlich (.AppleDouble, .DS_Store, etc)" );
+define( "HDDN_CAPTION_2", "Bitte ein Verzeichniss zum durchsuchen angeben" );
+define( "HDDN_BTN_SCAN", "Starte Suche" );
+
+##################################################
+
+?>
