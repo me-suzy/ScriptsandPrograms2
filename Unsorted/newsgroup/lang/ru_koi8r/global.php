ff7133ec7dfa684002f69ec78246af0c1e52bd72
me-suzy <me.suzana@gmail.com>
2025-08-13 07:03:33 +0300

    Add Unsorted/newsgroup/lang/ru_koi8r/global.php

---
+++ Unsorted/newsgroup/lang/ru_koi8r/global.php
@@ -0,0 +1,254 @@
+<?php
+// Language:                    Russian
+// Translator:                  Sergey Sazonenkov aka SeazoN
+// Created:                     28/05/2002
+// Last Update:                 04/06/2002
+// MyNewsGroups :) Version:     0.4
+
+//Charset
+define("_MYNGCHARSET","koi8-r");
+
+// HOW THE PHP date() function displays the date
+define("_MYNGDATEDISPLAY","d-m-Y"); 
+
+// Header
+define("_MYNGWELCOME"," ðÒÉ×ÅÔÓÔ×ÕÅÍ × MyNewsGroups :) ×. ");
+define("_MYNGSYSTEM_INFO"," óÉÓÔÅÍÎÁÑ éÎÆÏÒÍÁÃÉÑ:");
+define("_MYNGMENU_HOME"," çÌÁ×ÎÁÑ ");
+define("_MYNGMENU_NEWSGROUPS"," îÏ×ÏÓÔÉ ");
+define("_MYNGMENU_STATS"," óÔÁÔÉÓÔÉËÁ ");
+define("_MYNGMENU_SEARCH"," ðÏÉÓË ");
+define("_MYNGMENU_ABOUT"," ï&nbsp;ÎÁÓ ");
+
+// Footer
+define("_MYNGHOME_PAGE","äÏÍÁÛÎÑÑ ÓÔÒÁÎÉÃÁ ÐÒÏÅËÔÁ:");
+define("_MYNGGPL","GPL ìÉÃÅÎÚÉÑ");
+
+// Login
+define("_MYNGLOGIN","ìÏÇÉÎ");
+define("_MYNGPASSWORD","ðÁÒÏÌØ");
+define("_MYNGREGISTER","òÅÇÉÓÔÒÁÃÉÑ");
+
+// NewsGroups
+define("_MYNGBROWSE_NG","ðÏËÁÚÁÔØ îÏ×ÏÓÔÉ");
+define("_MYNGARTICLE_NUMBER","ëÏÌÉÞÅÓÔ×Ï óÏÏÂÝÅÎÉÊ");
+define("_MYNGGROUP_NAME","éÍÑ ÇÒÕÐÐÙ");
+define("_MYNGDESCRIPTION","ïÐÉÓÁÎÉÅ");
+define("_MYNGSUBSCRIBE_GROUP","ðÏÄÐÉÓÁÔØÓÑ ÎÁ ÜÔÕ ÇÒÕÐÐÕ");
+
+// Register
+define("_MYNGFILL_FORM","ðÏÖÁÌÕÊÓÔÁ ÚÁÐÏÌÎÉÔÅ É ÏÔÐÒÁ×ØÔÅ ÜÔÕ ÆÏÒÍÕ ÄÌÑ ÒÅÇÉÓÔÒÁÃÉÉ.");
+define("_MYNGUSER_NAME","éÍÑ ÐÏÌØÚÏ×ÁÔÅÌÑ");
+define("_MYNGPASSWORD","ðÁÒÏÌØ");
+define("_MYNGEMAIL","Email");
+define("_MYNGPASSWORD_AGAIN","ðÏ×ÔÏÒÉÔÅ ÐÁÒÏÌØ");
+// Phrase: 'All fields are required'
+define("_MYNGALL_FIELDS","÷ÓÅ ÐÏÌÑ");
+define("_MYNGREQUIRED","ÏÂÑÚÁÔÅÌØÎÙ");
+define("_MYNGVISIBLE","ðÏËÁÚÙ×ÁÔØ");
+define("_MYNGYES","äÁ");
+define("_MYNGNO","îÅÔ");
+define("_MYNGCOUNTRY","óÔÒÁÎÁ");
+define("_MYNGINSERT_VALID_EMAIL","ðÏÖÁÌÕÊÔÓÔÁ ÚÁÐÏÌÎÉÔÅ ÄÅÊÓÔ×ÉÔÅÌØÎÙÊ email ÁÄÒÅÓÓ, ÏÎ ÂÕÄÅÔ ÐÒÉÎÑÔ.");
+define("_MYNGPASSWORD_TRANSFER","÷ÁÛ email ÂÕÄÅÔ ×ÉÄÉÍ ÄÌÑ ÄÒÕÇÉÈ, ÎÏ ÚÁÝÉÝ£Î ÏÔ óðáí ÂÏÔÏ×, ÄÁÖÅ ÅÓÌÉ ×Ù ÐÕÂÌÉËÕÅÔÅ ÓÏÏÂÝÅÎÉÑ.");
+define("_MYNGSPAM_PROTECTED","÷ÁÛ ÐÁÒÏÌØ ÎÅ ÂÕÄÅÔ ÍÎÏÇÏËÒÁÔÎÏ ÐÅÒÅÄÁ×ÔØÓÑ ÞÅÒÅÚ éÎÔÅÒÎÅÔ.");
+define("_MYNGSECURE_LOGIN","îÁÛÁ ÓÉÓÔÅÍÁ ÒÅÇÉÓÔÒÁÃÉÉ ÚÁÝÉÝÅÎÁ, ÔÏÌØËÏ ÈÜÛ ÓÕÍÍÁ ÂÕÄÅÔ ×ÉÄÎÁ ÄÒÕÇÉÍ.");
+define("_MYNGREGISTRATION_COMPLETE","òÅÇÉÓÔÒÁÃÉÑ úÁËÏÎÞÅÎÁ.");
+define("_MYNGTHANKS","óÐÁÓÉÂÏ ÷ÁÍ!");
+define("_MYNGDATA_STORED","÷ÁÛÁ ÉÎÆÏÒÍÁÃÉÑ ÕÓÐÅÛÎÏ ÓÏÈÒÁÎÅÎÁ × ÂÁÚÅ ÄÁÎÎÙÈ.");
+define("_MYNGEDIT_INFO","÷Ù ÍÏÖÅÔÅ ÉÚÍÅÎÉÔØ ÉÎÆÏÒÍÁÃÉÀ, ËÏÔÏÒÕÀ ×Ù ÏÔÐÒÁ×ÉÌÉ ÎÁ ×ÁÛÕ ìÉÞÎÕÀ óÔÒÁÎÉÃÕ.");
+define("_MYNGSUBMITTED_INFO","üÔÏ ÉÎÆÏÒÍÁÃÉÑ, ËÏÔÏÒÕÀ ×Ù ÏÔÐÒÁ×ÉÌÉ ÎÁÍ:");
+define("_MYNGREGISTER_WELCOME","äÏÂÒÏ ðÏÖÁÌÏ×ÁÔØ × ×ÁÛÅ ÎÏ×ÏÅ óÏÏÂÝÅÓÔ×Ï!");
+define("_MYNGSTAFF","ûÔÁÔ MyNewsGroups :) .");
+define("_MYNGLOGIN_TRANSFER","úÁÒÅÇÉÓÔÒÉÒÏ×Á×ÛÉÓØ, ÷ÁÛ ÐÁÒÏÌØ ÎÅ ÂÕÄÅÔ ÍÎÏÇÏËÒÁÔÎÏ ÐÅÒÅÄÁ×ÔØÓÑ ÞÅÒÅÚ éÎÔÅÒÎÅÔ.");
+define("_MYNGCOOKIES","ëÕËÉ ÄÏÌÖÎÙ ÂÙÔØ ×ËÌÀÞÅÎÙ ÄÌÑ ÔÏÇÏ ÞÔÏÂÙ ÍÏÖÎÏ ÂÙÌÏ ×ÏÊÔÉ × ÓÉÓÔÅÍÕ.");
+
+// Login
+define("_MYNGHOME","çÌÁ×ÎÁÑ ÓÔÒÁÎÉÃÁ");
+define("_MYNGWELCOME_USER","äÏÂÒÏ ðÏÖÁÌÏ×ÁÔØ");
+define("_MYNGPOST","ïÔÐÒÁ×ÉÔØ");
+define("_MYNGREPLY","ïÔ×ÅÔÉÔØ");
+define("_MYNGSAVE","óÏÈÒÁÎÉÔØ");
+define("_MYNGSTATS","óÔÁÔÉÓÔÉËÁ");
+define("_MYNGSUBSCRIBE","ðÏÄÐÉÓËÁ");
+define("_MYNGOPTIONS","ïÐÃÉÉ");
+define("_MYNGPROFILE","ðÒÏÆÁÊÌ");
+define("_MYNGNEW_ARTICLES","ÎÏ×ÙÅ ÓÏÏÂÝÅÎÉÑ");
+define("_MYNGTO_ARTICLES","× ÓÏÏÂÝÅÎÉÑ");
+define("_MYNGEDIT_YOUR","éÚÍÅÎÉÔØ ×ÁÛÅ");
+define("_MYNGTO_GROUPS","× ÎÏ×ÕÀ ÇÒÕÐÐÕ");
+define("_MYNGYOUR_ARTICLES","×ÁÛÉ ÓÏÈÒÁÎ£ÎÎÙÅ ÓÏÏÂÝÅÎÉÑ");
+define("_MYNGVIEW_YOUR","ÐÏËÁÚÁÔØ ×ÁÛÉ");
+
+// My Bar
+define("_MYNGMY_BOX","íÏÊñÝÉË");
+define("_MYNGMY_HOME","íÏÑóÔÒÁÎÉÃÁ");
+define("_MYNGMY_GROUPS","íÏÉçÒÕÐÐÙ");
+define("_MYNGMY_ARTICLES","íÏÉóÏÏÂÝÅÎÉÑ");
+define("_MYNGLOG_OUT","÷ÙÊÔÉ");
+define("_MYNGMY_STATS","íÏÑóÔÁÔÉÓÔÉËÁ");
+
+// Tree
+define("_MYNGNEWSGROUP_UNSELECTED","îÅ ×ÙÂÒÁÎÎÁ çÒÕÐÐÁ ÎÏ×ÏÓÔÅÊ");
+define("_MYNGPOST_ARTICLE","ïÔÐÒÁ×ÉÔØ ÓÏÏÂÝÅÎÉÅ");
+define("_MYNGDATE","äÁÔÁ");
+define("_MYNGSUBJECT","ôÅÍÁ");
+define("_MYNGAUTHOR","á×ÔÏÒ");
+define("_MYNGARTICLES","óÏÏÂÝÅÎÉÑ");
+define("_MYNGNEXT","óÌÅÄÕÀÝÅÅ");
+define("_MYNGPREVIOUS","ðÒÅÄÙÄÕÝÅÅ");
+
+// Article
+define("_MYNGREPLY","ïÔ×ÅÔÉÔØ");
+define("_MYNGSEND_FRIEND","ðÏÓÌÁÔØ ÄÒÕÇÕ");
+define("_MYNGPRINT","òÁÓÐÅÞÁÔÁØ");
+define("_MYNGSAVE_ARTICLE","óÏÈÒÁÎÉÔØ ÓÏÏÂÝÅÎÉÅ");
+
+// Search Engine
+define("_MYNGSEARCH_RESULTS","òÅÚÕÌØÔÁÔÙ ðÏÉÓËÁ");
+define("_MYNGNOARTICLES_FOUND","éÚ×ÅÎÉÔÅ, ÎÉ ÏÄÎÏÇÏ ÓÏÏÂÝÅÎÉÑ ÎÅ ÎÁÊÄÅÎÏ, ÓÏÄÅÒÖÁÝÅÇÏ");
+define("_MYNGSEARCH_ENGINE","áÌÇÏÒÉÔÍ ðÏÉÓËÁ");
+define("_MYNGSEARCH_ONE_GROUP","× ÇÒÕÐÐÅ");
+define("_MYNGSEARCH_ALL_GROUPS","×Ï ×ÓÅÈ ÇÒÕÐÐÁÈ");
+define("_MYNGAND_OPERATION","åÓÌÉ ×Ù ××ÅÌÉ ÂÏÌÅÅ ÞÅÍ ÏÄÎÏ ÓÌÏ×Ï, ÂÕÄÅÔ ÄÏÂÁ×ÌÅÎÁ ÌÏÇÉËÁ AND (&/é).");
+define("_MYNGPOWERED_BY","Powered by ");
+define("_MYNGGO","ðÏÉÓË!");
+
+// Search Results
+define("_MYNGGROUP","çÒÕÐÐÁ");
+define("_MYNGREADINGS","ðÒÏÞÔÅÎÉÑ");
+// Stats
+define("_MYNGHOT_NG","óÁÍÙÅ çÒÕÐÐÙîÏ×ÏÓÔÅÊ");
+define("_MYNGPOPULAR_NG","óÁÍÙÅ ðÏÐÕÌÑÒÎÕÅ çÒÕÐÐÙîÏ×ÏÓÔÅÊ");
+define("_MYNGCROWDED_NG","óÁÍÙÅ âÏÌØÛÉÅ çÒÕÐÐÙîÏ×ÏÓÔÅÊ");
+define("_MYNGHOT_ARTICLES","ó×ÅÖÉÅ óÏÏÂÝÅÎÉÑ");
+define("_MYNGPOPULAR_ARTICLES","óÁÍÙÅ ÐÏÐÕÌÑÒÎÙÅ óÏÏÂÝÅÎÉÑ");
+define("_MYNGACTIVE_USER","óÁÍÙÊ ÁËÔÉ×ÎÙÊ ÀÚÅÒ");
+define("_MYNGARG_USER","àÚÅÒ Ó ÎÁÉÂÏÌØÛÉÍ ÞÉÓÌÏÍ ÏÔ×ÅÔÏ×");
+define("_MYNGSTATS_ARTICLES","ÓÏÏÂÝÅÎÉÑ");
+define("_MYNGSTATS_READINGS","ÐÒÏÞÔÅÎÉÑ");
+define("_MYNGUSERS","ÐÏÌØÚÏ×ÁÔÅÌÉ");
+define("_MYNGPOSTINGS","ÓÏÏÂÝÅÎÉÊ");
+define("_MYNGREPLIES","ÏÔ×ÅÔÏ×");
+
+// My Groups
+define("_MYNGMUST_LOGIN","äÌÑ ÉÓÐÏÌØÚÏ×ÁÎÉÑ ÜÔÏÊ ×ÏÚÍÏÖÎÏÓÔÉ÷Ù ÄÏÌÖÎÙ ÚÁÌÏÇÉÎÉÔØÓÑ.");
+define("_MYNGSUBSCRIPTION_SYSTEM","óÉÓÔÅÍÁ ÐÏÄÐÉÓËÉ");
+define("_MYNGCHOOSE_GROUP","÷ÙÂÒÁÔØ çÒÕÐÐÕ");
+define("_MYNGSURE","ñ Õ×ÅÒÅÎ(Á)");
+define("_MYNGGROUPS_SUBSCRIBED","üÔÏ ÇÒÕÐÐÙ, ÎÁ ËÏÔÏÒÙÅ ×Ù ÐÏÄÐÉÓÁÎÙ");
+define("_MYNGNEW","îÏ×ÏÅ");
+define("_MYNGUNREAD","îÅÐÒÏÞÔÅÎÏ");
+define("_MYNGPOSTED","ïÔÐÒÁ×ÌÅÎÏ");
+define("_MYNGADD","äÏÂÁ×ÉÔØ");
+define("_MYNGDEL","õÄÁÌÉÔØ");
+
+// My Articles
+define("_MYNGARTICLES_SAVED","üÔÏ ÓÏÈÒÁÎ£ÎÎÙÅ ×ÁÍÉ ÓÏÏÂÝÅÎÉÑ");
+
+// Print article
+define("_MYNGPRINT_ARTICLE","òÁÓÐÅÞÁÔÁÔØ ÓÏÏÂÝÅÎÉÅ");
+define("_MYNGUSER_AGENT","User Agent");
+define("_MYNGGENERATED_BY","Generated by");
+define("_MYNGFROM","ïÔ");
+
+// Email article
+
+define("_MYNGEMAIL_SENT","Email ÂÙÌ ÕÓÐÅÛÎÏ ÏÔÐÒÁ×ÌÅÎ");
+define("_MYNGEMAIL_NOT_SENT","ðÒÏÉÚÏÛÌÁ ÏÛÉÂËÁ: Email ÎÅ ÂÙÌ ÏÔÐÒÁ×ÌÅÎ");
+define("_MYNGEMAIL_FRIEND","ïÔÐÒÁ×ÉÔØ ÄÒÕÇÕ ÜÔÏ ÓÏÏÂÝÅÎÉÅ");
+define("_MYNGSENDYOU_FROM"," ÐÒÉÓÌÁÌ(Á) ÜÔÏ ÓÏÏÂÝÅÎÉÅ ÉÚ ");
+define("_MYNGDEAR_FRIEND","õ×ÁÖÁÅÍÙÊ(ÁÑ) (_friend_name)");
+// the string '_friend_name' it must be the same in '_MYNGDEAR_FRIEND' and in '_MYNGFRIEND_NAME'.
+define("_MYNGFRIEND_NAME","\(_friend_name\)");
+define("_MYNGYOUR_FRIEND","÷ÁÛ ÄÒÕÇ");
+define("_MYNGWANT2SHARE",", ÈÏÞÅÔ ÏÚÎÁËÏÍÉÔØ ×ÁÓ \n Ó ÜÔÉÍ ÓÏÏÂÝÅÎÉÅÍ ÉÚ ");
+define("_MYNGNSERT_FRIEND_INFO","÷ÓÔÁ×ÉÔØ ÉÎÆÏÒÍÁÃÉÀ ×ÁÛÅÇÏ ÄÒÕÇÁ");
+define("_MYNGMAIL_MUSTBE_REAL","÷ÎÉÍÁÎÉÅ: email ÁÄÒÅÓÓ ÄÏÌÖÅÎ ÂÙÔØ ÒÅÁÌØÎÙÍ, ÏÎ ÂÕÄÅÔ ÐÒÏ×ÅÒÅÎ, <br>&nbsp; ïÎ ÂÕÄÅÔ ÏÔÔÒÁÓÓÉÒÏ×ÁÎ, ÎÏ ÎÅ ÓÏÈÒÁÎ£Î × ÞÉÓÔÏÍ ×ÉÄÅ.");
+define("_MYNGNAME","éÍÑ");
+define("_MYNGEMAIL_LOOK","åmail âÕÄÅÔ ×ÙÇÌÑÄÅÔ ÔÁË");
+define("_MYNGEMAIL_IT","ïÔÐÒÁ×ÉÔØ!");
+
+// Post
+define("_MYNGMISSING_EMAIL","÷Ù ÄÏÌÖÎÙ ××ÅÓÔÉ email ÁÄÒÅÓÓ.");
+define("_MYNGMISSING_MESSAGE","÷Ù ÄÏÌÖÎÙ ××ÅÓÔÉ ÓÏÏÂÝÅÎÉÅ Ë ÐÉÓØÍÕ.");
+define("_MYNGWRONG_EMAIL","îÅÐÒÁ×ÉÌØÎÙÊ email");
+define("_MYNGMISSING_NAME","÷Ù ÄÏÌÖÎÙ ××ÅÓÔÉ ÉÍÑ, ÐÏ ËÒÁÊÎÅÊ ÍÅÒÅ ÎÉË.");
+define("_MYNG_MISSING_SUBJECT","÷Ù ÄÏÌÖÎÙ ××ÅÓÔÉ ÔÅÍÕ.");
+define("_MYNGMESSAGE_POSTED","óÏÏÂÝÅÎÉÅ ÂÙÌÏ ÕÓÐÅÛÎÏ ÏÔÐÒÁ×ÌÅÎÏ.");
+define("_MYNGERROR_SERVER","óÅÒ×ÅÒ ÎÏ×ÏÓÔÅÊ ÎÅ ÐÒÉÎÑÌ ÓÏÏÂÝÅÎÉÅ.");
+define("_MYNGREAD_ONLY","üÔÁ ÇÒÕÐÐÁ ÚÁËÒÙÔÁ ÄÌÑ ÚÁÐÉÓÉ");
+define("_MYNGWROTE"," ÎÁÐÉÓÁÌ(Á):\n\n");
+define("_MYNGFOLLOWUP_NOT_ALLOWED","÷ÁÍ ÎÅ ÒÁÚÒÅÛÅÎÏ ÐÉÓÁÔØ × ÇÒÕÐÐÙ: ");
+define("_MYNGPOST_FAILED","ïÛÉÂËÁ ÓÏÅÄÉÎÅÎÉÑ, ðÏÐÒÏÂÕÊÔÅ ÐÏÚÖÅ ÅÝ£ ÒÁÚ.");
+define("_MYNGFOOTER1","         óÏÏÂÝÅÎÉÅ ÏÔÐÒÁ×ÌÅÎÏ Ó MyNewsGroups :)");
+define("_MYNGFOOTER2","         http://mynewsgroups.sourceforge.net");
+define("_MYNGFOOTER3","         ðÏÄÅÌÉÓØ Ó×ÏÉÍÉ ÚÎÁÎÉÑÍÉ × ... ");
+define("_MYNGFILL_FORM2POST","úÁÐÏÌÎÉÔÅ ÜÔÕ ÆÏÒÍÕ ÄÌÑ ÎÏ×ÏÇÏ ÓÏÏÂÝÅÎÉÑ");
+define("_MYNGEMAIL_MUSTBE_REAL","÷ÎÉÍÁÎÉÅ: email ÁÄÒÅÓÓ ÄÏÌÖÅÎ ÂÙÔØ ÒÅÁÌØÎÙÍ, ÏÎ ÂÕÄÅÔ ÐÒÏ×ÅÒÅÎ");
+define("_MYNGPOST","ïÔÐÒÁ×ÉÔØ");
+define("_MYNGPOSTING_COMPLETE","ïÔÐÒÁ×ËÁ ÚÁ×ÅÒÛÅÎÁ");
+define("_MYNGTHANKX","óÐÁÓÉÂÏ ×ÁÍ!");
+define("_MYNGPOSTED_SUCCESFULLY","÷ÁÛÅ ÓÏÏÂÝÅÎÉÅ ÂÙÌÏ ÕÓÐÅÛÎÏ ÏÔÐÒÁ×ÌÅÎÏ");
+define("_MYNGINFO_SUBMITTED","üÔÏ ÉÎÆÏÒÍÁÃÉÑ, ËÏÔÏÒÕÀ ×Ù ÏÔÐÒÁ×ÉÌÉ ÎÁ ÓÅÒ×ÅÒ ÎÏ×ÏÓÔÅÊ");
+define("_MYNGBODY","óÏÄÅÒÖÁÎÉÅ");
+define("_MYNGCLICK2RETURN","îÁÖÍÉÔÅ ÚÄÅÓØ, ÞÔÏÂÙ ×ÅÒÎÕÔØÓÑ ×");
+define("_MYNGEMAIL_SPAM_PROTECTED","÷ÁÛ email ÂÕÄÅÔ ÚÁÝÉÝ£Î ÐÒÉ ÏÔÐÒÁ×ËÅ ÄÌÑ ÔÏÇÏ ÞÔÏÂÙ ÉÚÂÅÖÁÔØ ÏÂÎÁÒÕÖÅÎÉÑ SPAM ÂÏÔÁÍÉ");
+define("_MYNGEMAIL_CHANGED","÷ÁÛ email ÂÕÄÅÔ ×ÉÄÉÍ");
+
+// Error template
+define("_MYNGSOMETH_WRONG","þÔÏ-ÔÏ ÐÏÛÌÏ ÎÅ ÔÁË...");
+define("_MYNGERROR_MESSAGE","óÏÏÂÝÅÎÉÅ ïÂ ïÛÉÂËÅ");
+define("_MYNGHAVE_DOUBT","åÓÌÉ Õ ×ÁÓ ËÁËÉÅ-ÎÉÂÕÄØ ÐÒÏÂÌÅÍÙ, ÐÏÖÁÌÕÊÓÔÁ ÏÂÒÁÔÉÔÅÓØ Ë óÉÓÔÅÍÎÏÍÕ áÄÍÉÎÉÓÔÒÁÔÏÒÕ");
+define("_MYNGERROR","ïÛÉÂËÁ!");
+
+// Admin
+define("_MYNGADMIN_LOGIN","÷Ù ÄÏÌÖÎÙ ÚÁÌÏÇÉÎÉÔØÓÑ ËÁË óÉÓÔÅÍÎÙÊ áÄÍÉÎÉÓÔÒÁÔÏÒ.");
+define("_MYNGADMIN","áÄÍÉÎÉÓÔÒÁÃÉÑ");
+define("_MYNGENTER_GROUP_NAME","÷×ÅÄÉÔÅ éÍÑ çÒÕÐÐÙ");
+define("_MYNGADD_GROUP","äÏÂÁ×ÉÔØ çÒÕÐÐÕ");
+define("_MYNGCHOOSE_SERVER","÷ÙÂÒÁÔØ óÅÒ×ÅÒ");
+define("_MYNGARTICLES2FETCH","ðÏÌÕÞÉÔØ óÏÏÂÝÅÎÉÑ");
+define("_MYNGDELETE_GROUP","õÄÁÌÉÔØ çÒÕÐÐÕ");
+define("_MYNGDELETE","õÄÁÌÉÔØ");
+define("_MYNGEDIT_GROUP","òÅÄÁËÔÉÒÏ×ÁÔØ çÒÕÐÐÕ");
+define("_MYNGALLOW_POSTING","òÁÚÒÅÛÉÔØ ÏÔÐÒÁ×ËÕ ÓÏÏÂÝÅÎÉÊ");
+define("_MYNGEDIT","òÅÄÁËÔÉÒÏ×ÁÔØ");
+define("_MYNGADD_SERVER","äÏÂÁ×ÉÔØ óÅÒ×ÅÒ îÏ×ÏÓÔÅÊ");
+define("_MYNGHOST","èÏÓÔ");
+define("_MYNGPORT","ðÏÒÔ");
+define("_MYNGDELETE_SERVER","õÄÁÌÉÔØ óÅÒ×ÅÒ îÏ×ÏÓÔÅÊ");
+define("_MYNGALL","÷ÓÅ");
+
+
+// Errors
+define("_MYNGNOGROUPS","îÁ ÄÁÎÎÙÊ ÍÏÍÅÎÔ ÎÅÔ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎÎÙÈ ÇÒÕÐÐ.");
+define("_MYNGCON_ERROR","ïÛÉÂËÁ óÏÅÄÉÎÅÎÉÑ!");
+define("_MYNGUSERNAME_TAKEN","éÚ×ÉÎÉÔÅ, ÎÏ ÜÔÏ ÉÍÑ ÕÖÅ ÉÓÐÏÌØÚÕÅÔÓÑ ÄÒÕÇÉÍ ÐÏÌØÚÏ×ÁÔÅÌÅÍ.");
+define("_MYNGEMAIL_TAKEN","éÚ×ÉÎÉÔÅ, ÎÏ ÜÔÏÔ email ÕÖÅ ÉÓÐÏÌØÚÕÅÔÓÑ ÄÒÕÇÉÍ ÐÏÌØÚÏ×ÁÔÅÌÅÍ.");
+define("_MYNGLOGIN_INCORRECT","éÚ×ÉÎÉÔÅ, ÎÅÐÒÁ×ÉÌØÎÙÊ ÌÏÇÉÎ, ÐÏÖÁÌÕÊÓÔÁ ÐÏÐÒÏÂÕÊÔÅ ÅÝ£ ÒÁÚ.");
+define("_MYNGINVALID_EMAIL","éÚ×ÉÎÉÔÅ, ÎÅÄÅÊÓÔ×ÉÔÅÌØÎÙÊ email. ÷×ÅÄÉÔÅ ÄÅÊÓÔ×ÉÔÅÌØÎÙÊ email.");
+define("_MYNGINVALID_PASSWORD","éÚ×ÉÎÉÔÅ, ÎÅÐÒÁ×ÉÌØÎÏ ××ÅÄ£Î ÐÁÒÏÌØ.");
+define("_MYNGBLANK_PASSWORD","ðÏÖÁÌÕÊÓÔÁ, ÷×ÅÄÉÔÅ ÐÁÒÏÌØ.");
+define("_MYNGSERVER_REJECT","÷ÁÛ ÐÏÞÔÏ×ÙÊ ÓÅÒ×ÅÒ Ï ÎÅ ÐÒÉÎÑÌ ÕËÁÚÁÎÎÙÊ email");
+define("_MYNGNO_RESPONSE","îÅÔ ÏÔ×ÅÔÁ ÏÔ ×ÁÛÅÇÏ ÐÏÞÔÏ×ÏÇÏ ÓÅÒ×ÅÒÁ");
+define("_MYNGCANT_CONNECT_SERVER","îÅ ÍÏÇÕ ÓÏÅÄÉÎÉÔØÓÑ Ó ÐÏÞÔÏ×ÙÍ ÓÅÒ×ÅÒÏÍ");
+define("_MYNGAPPEARS_VALID"," appears to be valid");
+//
+define("_MYNGNOT_PROPERLY_FORMATTED"," ÎÅ ÐÒÁ×ÉÌØÎÏÇÏ ÆÏÒÍÁÔÁ");
+define("_MYNGNO_GROUP","çÒÕÐÐÁ ÎÅ ÄÏÓÔÕÐÎÁ ÎÁ ");
+define("_MYNGUNKNOWN_GROUP"," üÔÁ ÇÒÕÐÐÁ ÎÅ ÐÒÉÎÁÄÌÅÖÉÔ óÉÓÔÅÍÅ.");
+
+
+// Greets
+define("_MYNGREGISTRATION_COMPLETED","ðÏÚÄÒÁ×ÌÑÅÍ!÷ÁÛÁ ÒÅÇÉÓÔÒÁÃÉÑ ÕÓÐÅÛÎÏ ÚÁ×ÅÒÛÅÎÁ.");
+define("_MYNGLOGGED","÷Ù ÎÅ ×ÏÛÌÉ × ÓÉÓÔÅÍÕ!");
+define("_MYNGLOGGED_OUT","÷Ù ÕÓÐÅÛÎÏ ×ÙÛÌÉ ÉÚ ÓÉÓÔÅÍÙ.");
+
+
+
+
+
+
+
+?>
