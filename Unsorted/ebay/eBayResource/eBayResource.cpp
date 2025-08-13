b6fecc94512b72613448b06dde383f942bc883f1
me-suzy <me.suzana@gmail.com>
2025-08-13 03:13:38 +0300

    Add Unsorted/ebay/eBayResource/eBayResource.cpp

---
+++ Unsorted/ebay/eBayResource/eBayResource.cpp
@@ -0,0 +1,18 @@
+/*	$Id: eBayResource.cpp,v 1.2 1999/02/21 02:33:36 josh Exp $	*/
+//
+//	File:		eBayResource.cpp
+//
+//	Author:		Wen Wen (wen@ebay.com)
+//
+//	Function:
+//				Resources for eBay
+//
+//	Modifications:
+//				- 06/18/97	Wen	- Created
+
+#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hDLL, DWORD dwReason, LPVOID lpReserved)
+{
+  return(TRUE);
+}
