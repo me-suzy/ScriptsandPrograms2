2e8f0ceafc9c56ae01144d7350bf418c32d02a96
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:51 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_tabfile-recursive.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_tabfile-recursive.php
@@ -0,0 +1,64 @@
+<?php
+
+##################################################
+
+# /admin/tabfile-recursive.php
+
+##################################################
+
+define( "TFREC_HEADER", "BUSCAR ARCHIVOS DE AUDIO RECURSIVAMENTE" );
+define( "TFREC_HEADER_AUTO", "Modo Automático" );
+define( "TFREC_HEADER_INTER", "Modo Interactivo" );
+define( "TFREC_CAPTION_1", "" );
+define( "TFREC_CAPTION_2", "Por Favor Seleccione un Directotrio para Revisar, o haga click sobre el botón para empezar en el top de tu directorio musical. Los Directorios a revisar DEBEN estar en el directorio musical definido en las Preferencias. Si usted quiere revisar un directorio por primera vez, nosotros le aconsejamos limitar la busqueda a los sub-directorios." );
+define( "TFREC_OPTION_1", "¿Recursivo?" );
+define( "TFREC_OPTION_2", " ¿Consiga la información de nombre de fichero de los archivos que no tienen una etiqueta ID3 y no los hicieron cola?");
+define( "TFREC_OPTION_3", "¿Revisando?" );
+define( "TFREC_OPTION_4", " Importar directamente a la Base de Datos");
+define( "TFREC_BTN", "Comenzar búsqueda" );
+
+define( "TFREC_COLS_TR", "Título" );
+define( "TFREC_COLS_AR", "Artista" );
+define( "TFREC_COLS_AL", "Álbun" );
+define( "TFREC_COLS_GE", "Género" );
+define( "TFREC_COLS_FS", "Tamaño" );
+define( "TFREC_COLS_TI", "Tiempo" );
+define( "TFREC_COLS_TN", "Pista Número" );
+define( "TFREC_COLS_TC", "Contador de pistas" );
+define( "TFREC_COLS_YR", "Año" );
+define( "TFREC_COLS_DT", "Fecha" );
+define( "TFREC_COLS_DA", "Fecha de adición" );
+define( "TFREC_COLS_BR", "Tasa de bits" );
+define( "TFREC_COLS_SR", "Frecuencia de muestreo" );
+define( "TFREC_COLS_VA", "Ajuste de Volumen" );
+define( "TFREC_COLS_FK", "Categoría" );
+define( "TFREC_COLS_CT", "Comentarios" );
+define( "TFREC_COLS_LC", "Ubicación" );
+
+define( "TFREC_ERROR_NOMUDIR_1", "El Directorio que Usted Solicitó para Revisar no está re." );
+define( "TFREC_ERROR_NOMUDIR_2", "No está en el Directorio Musical Definido." );
+define( "TFREC_ERROR_NODIR", "Por Favor Seleccione Un Directorio Musical Válido par ser revisado en busca de Archivos MP3." );
+define( "TFREC_ERROR_NOFILE", "Ningún Archivo Nuevo para la Importación." );
+
+define( "TFREC_VIEW", "Observar el Archivo para Importación." );
+define( "TFREC_PROCEED", "Proceder con la Importación del Archivo. Herramienta script." );
+
+define( "TFREC_SUCCESS_1", "Archivos MP3 que tuvierón exito para la importación." );
+define( "TFREC_SUCCESS_2", "No se encontrarón Errores." );
+define( "TFREC_SUCCESS_3", "total de Archivos MP3 que tuvierón exito para la importación." );
+
+define( "TFREC_INSERT_1", "Insertado");
+define( "TFREC_INSERT_2", "Pista(s)");
+define( "TFREC_INSERT_3", "Artista(s)");
+define( "TFREC_INSERT_4", "Album(s)");
+define( "TFREC_INSERT_5", "Genéro(s)");
+
+define( "TFREC_FORM_HELP_1", "El siguiente" );
+define( "TFREC_FORM_HELP_2", "Estos Archivo(s) no tienen ningun ID3 valido (o similar) tag." );
+define( "TFREC_FORM_HELP_3", "Para importar estos archivos, tu tendras que ingresar la información manualmente." );
+define( "TFREC_FORM_HELP_4", "Solo unos pocos archivos son listados en la forma abajo para limitar el tamaño de la pantalla presente." );
+define( "TFREC_FORM_BTN", "Crear un Nuevo Archivo Importado" );
+
+##################################################
+
+?>
