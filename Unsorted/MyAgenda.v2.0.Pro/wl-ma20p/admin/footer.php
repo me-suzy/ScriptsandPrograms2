3a0f3276bdcfeeb56357fd9fab26140ca2963047
me-suzy <me.suzana@gmail.com>
2025-08-13 06:36:56 +0300

    Add Unsorted/MyAgenda.v2.0.Pro/wl-ma20p/admin/footer.php

---
+++ Unsorted/MyAgenda.v2.0.Pro/wl-ma20p/admin/footer.php
@@ -0,0 +1,23 @@
+<table border=0 cellspacing=0 cellpadding=1 width="100%" bgcolor="#333333" align="center">
+ <tr>
+	<td>
+
+	<table border=0 cellspacing=0 cellpadding=0 width="100%" bgcolor="#FFFFFF">
+ 	 <tr>
+		<td>
+		  <table border=0 cellpadding=1 cellspacing=1 width="100%">
+			  <tr>
+				 <td bgcolor="#f3f3f3" align="center">
+				 <a href="./"><b><?=$LANGUAGE['strHome'];?></b></a>
+				 </td>
+			  </tr>
+			 </table>
+		</td>
+	 </tr>
+	</table>
+
+	</td>
+ </tr>
+</table>
+</BODY>
+</HTML>
