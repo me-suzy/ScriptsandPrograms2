62839f5fd534dad8b8b9d21ac471ea042774ab82
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:22 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-index.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-index.php
@@ -0,0 +1,24 @@
+<?php
+
+##################################################
+
+# /index.php
+
+##################################################
+
+define( "BRWS_GE_HEADER", "GÈNERES DISPONIBLES" );
+define( "BRWS_GE_HELP", "Llista totes les pistes d'aquest gènere." );
+
+define( "BRWS_AR_HEADER", "NOUS INTÈRPRETS" );
+define( "BRWS_AR_HELP", "Feu clic per veure totes les pistes d'aquest intèrpret." );
+define( "BRWS_PLAY_AR_HELP", "Feu clic per reproduir totes les canÁons d'aquest artista" );
+define( "BRWS_FILTER_AR_HELP", "Feu clic per llistar tots els àlbums d'aquest artista." );
+
+define( "BRWS_AL_HEADER", "NOUS ÀLBUMS" );
+define( "BRWS_AL_HELP", "Feu clic per veure totes les pistes d'aquest àlbum." );
+define( "BRWS_PLAY_AL_HELP", "Feu clic per reproduir totes les canÁons d'aquest àlbum" );
+define( "BRWS_FILTER_AL_HELP", "Feu clic per llistar tots els artistes d'aquest àlbum." );
+
+##################################################
+
+?>
