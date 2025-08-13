9d79b669143169e2ab4746ae7fbbb2f9400ddf47
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:57 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.emoticon.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.emoticon.php
@@ -0,0 +1,31 @@
+<?php
+$L = array(
+
+
+"emoticon_module_name" =>
+"Kuva smiley",
+
+"emoticon_module_description" =>
+"Kuva smileyt moduuli",
+
+"emoticon_heading" =>
+"Kuva smileyt",
+
+"emoticon_glyph" =>
+"Kirjoitusmerkki",
+
+"emoticon_image" =>
+"Kuva",
+
+"emoticon_width" =>
+"Leveys",
+
+"emoticon_height" =>
+"Korkeus",
+
+"emoticon_alt" =>
+"Alt tagi",
+
+''=>''
+);
+?>
