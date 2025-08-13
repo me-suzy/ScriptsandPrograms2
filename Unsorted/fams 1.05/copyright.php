53ef9fb23f4851303770d9c23c5b4ff9c5fe11d7
me-suzy <me.suzana@gmail.com>
2025-08-13 04:33:39 +0300

    Add Unsorted/fams 1.05/copyright.php

---
+++ Unsorted/fams 1.05/copyright.php
@@ -0,0 +1,25 @@
+<?php
+
+/* ------------------------------------------------------------ */
+/*								*/
+/*	File Area Management System (FAMS)			*/
+/*								*/
+/*	Copyright (c) 2001 by Bastian 'Buddy' Grimm		*/
+/*	Autor: Bastian Grimm					*/
+/*	Publisher: [ BG-Studios ]				*/
+/*	eMail: bastian@bg-studios.de				*/
+/*	WebSite: http://www.bg-studios.de			*/
+/*	Date: 06.05.01		Version: Copyright 1.03		*/
+/*	Geändert am: 23.07.01					*/
+/*								*/
+/* ------------------------------------------------------------	*/
+
+
+$pagetyp = "main";
+
+
+$copy1 = "<IMG BORDER=0 SRC=./images/group-link.gif>File Area Management System 1.0 &copy; 2001 by [ <A HREF=http://www.reduniverse.de TARGET=new>Red Universe Community</A> ]";
+$copy2 = "<IMG BORDER=0 SRC=../images/group-link.gif>File Area Management System 1.0 &copy; 2001 by [ <A HREF=http://www.reduniverse.de TARGET=new>Red Universe</A> ]";
+
+?>
+
