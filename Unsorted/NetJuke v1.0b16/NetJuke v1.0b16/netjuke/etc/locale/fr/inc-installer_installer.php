84344f5d439aa9183d4f67d742ffece6fdd86dbc
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:24 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-installer_installer.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-installer_installer.php
@@ -0,0 +1,45 @@
+<?php
+
+##################################################
+
+# /installer/*.php
+
+##################################################
+
+define( "ADMINST_ERR_VERSION", "Cet outils d\\'installation ne peut être utilisé\\navec cette copie du Netjuke. Les version ne sont pas les mêmes." );
+
+define( "ADMINST_ERR_FSPATH_1", "Veuillez indiquer la location des fichiers (/location/du/netjuke)." );
+define( "ADMINST_ERR_FSPATH_2", "Une location de fichier ne peut pas contenir ://" );
+define( "ADMINST_ERR_WEBPATH_1", "Veuillez indiquer l\\'adresse du Netjuke sur le Web (http://adresse/du/netjuke)." );
+define( "ADMINST_ERR_WEBPATH_2", "Une adresse Web doit contenir http:// ou https://" );
+
+define( "ADMINST_ERR_FSPATH_1", "Please enter a Filesystem Path." );
+define( "ADMINST_ERR_WEBPATH_1", "Please enter a Web Path." );
+
+define( "ADMINST_ERR_DBTYPE", "Veuillez sélectioner un type de base de données." );
+define( "ADMINST_ERR_DBHOST", "Veuillez indiquer le nom du serveur de base de données." );
+define( "ADMINST_ERR_DBNAME", "Veuillez indiquer le nom de votre base de données." );
+define( "ADMINST_ERR_DBCONN", "Impossible de connecter à la base de donnée avec l\\'information fournie." );
+
+define( "ADMINST_ERR_EMAIL", "Veuillez soumettre une adresse de courrier électronique\\npour le premier compte d'administrateur." );
+define( "ADMINST_ERR_PASS", "Veuillez soumettre un mot de passe pour le premier\\ncompte d'administrateur." );
+
+define( "ADMINST_ERR_STREAM", "Veuillez indiquer un serveur de débit de musique (Streaming Server)." );
+
+define( "ADMINST_ERR_NOSQL", "Cet outils d\\'installation est deffectueux. Veuillez acquérir une nouvelle copie." );
+
+define( "ADMINST_ERR_DENIED_1", "- Cet outils d\\'installation ne peut être utilisé avec cette copie du Netjuke. La copie visée est déjà initialisée." );
+define( "ADMINST_ERR_DENIED_2", "- Cet outils de mise à jour ne peut pas être utilisé avec cette copie du Netjuke.\\n- La base de donnée visée est n\\'existe pas encore." );
+define( "ADMINST_ERR_DENIED_3", "La location donnée pour le fichiers ne parait pas valide." );
+define( "ADMINST_ERR_DENIED_4", "Accés refusé: l\\'adresse électronique et le mot de passe fournis ne correspondent pas à un compte d\\'administrateur existant." );
+
+define( "ADMINST_ERR_EXEC", "Problémes avec le scripte SQL généré. Veuillez essayer à nouveau." );
+
+define( "ADMINST_FILEINFO_1", "Créé" );
+define( "ADMINST_FILEINFO_2", "Depuis" );
+define( "ADMINST_FILEINFO_3", "Sauvegarder à" );
+define( "ADMINST_FILEINFO_4", "Sauvegardé. Vous pouvez maintenant accéder à la nouvelle version à" );
+
+##################################################
+
+?>
