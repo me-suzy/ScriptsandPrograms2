d2bfe4848ec0b9d5b7c2c7f62dab7e88231433f1
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:24 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/dialog.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/dialog.php
@@ -0,0 +1,83 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[DIALOG_Syntax_Error] = "Syntaksfeil";
+
+$LOCALE[DIALOG_Available_Buddies] = "Buddy brukere er";
+
+$LOCALE[DIALOG_Available_Ignored] = "Ignorerte brukere er";
+
+$LOCALE[DIALOG_On_Buddy] = "Er på Deres buddy liste.";
+
+$LOCALE[DIALOG_Not_Buddy] = "Er ikke på Deres buddy liste.";
+
+$LOCALE[DIALOG_Add_Buddy] = "Tilføyer til Deres buddy liste.";
+
+$LOCALE[DIALOG_Del_Buddy] = "Fjerner fra Deres buddy liste.";
+
+$LOCALE[DIALOG_Block] = "De blokkerer nå private budskap.";
+
+$LOCALE[DIALOG_Unblock] = "De aksepterer nå private budskap.";
+
+$LOCALE[DIALOG_Away] = "Er borte.";
+
+$LOCALE[DIALOG_Back] = "Er tilbake.";
+
+$LOCALE[DIALOG_Bug_Sent] = "En basillrapport sender.";
+
+$LOCALE[DIALOG_Colors] = "Deres farger oppdatert.";
+
+$LOCALE[DIALOG_ChangeColor] = "Forandr farge til";
+
+$LOCALE[DIALOG_Click] = "Klikk her besøke";
+
+$LOCALE[DIALOG_OP_Only] = "Er for operatører bare.";
+
+$LOCALE[DIALOG_Topic_Updated] = "Oppdaterer romemnet.";
+
+$LOCALE[DIALOG_Smiles] = "Tilgjengelige smiler og uttrykk";
+
+$LOCALE[DIALOG_Smiles_Disabled] = "Smiley ansikter udyktiggjort i Deres fortrinnsrett eller ved forsterkningen.";
+
+$LOCALE[DIALOG_Rolled] = "Rullet terningene!";
+
+$LOCALE[DIALOG_Not_Online] = "Er ikke for tiden on-line.";
+
+$LOCALE[DIALOG_Exit] = "Har til venstre romet.";
+
+$LOCALE[DIALOG_Not_Ignored] = "Ignorerer ikke.";
+
+$LOCALE[DIALOG_Is_Ignored] = "Ignorerer.";
+
+$LOCALE[DIALOG_Unregistered] = "Er en uregistrert username.";
+
+$LOCALE[DIALOG_No_Ignored] = "De ignorerer ikke noen brukere.";
+
+$LOCALE[DIALOG_No_Buddies] = "De har ikke noe buddies.";
+
+$LOCALE[DIALOG_Located] = "plasserer i rom";
+
+$LOCALE[DIALOG_Located_Private] = "Plasserer i et privat rom.";
+
+$LOCALE[DIALOG_Not_Located] = "Tillater ikke folk plassere ham.";
+
+$LOCALE[DIALOG_Kicked] = "Fjerner fra systemet.";
+
+$LOCALE[DIALOG_Say_Button] = "Send Message";
+
+$LOCALE[DIALOG_Clear_Window] = "Klar Screen";
+
+$LOCALE[DIALOG_Private_Message] = "Privat budskap";
+	
+$LOCALE[DIALOG_Private_Message_Refresh] = "Forfrisk liste";
+	
+?>
