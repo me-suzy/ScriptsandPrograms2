07eaa4e094937e568bb53b06052c46ad673d28a1
me-suzy <me.suzana@gmail.com>
2025-08-13 04:35:56 +0300

    Add Unsorted/fcart/fcart/meta.php

---
+++ Unsorted/fcart/fcart/meta.php
@@ -0,0 +1,3 @@
+
+<meta name="description" content="The ultimate e-commerce solution for web stores is based on PHP4/MySQL database with highly configurable implementation.">
+<meta name="keywords" content="f-cart, fcart, shopping cart, shop, e-commerce, store, web store, gift certificates, wish list, top sellers">
