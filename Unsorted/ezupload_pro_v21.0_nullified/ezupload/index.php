809cfb49afd9cc35d1600bb8c7ad4d2b2e060312
me-suzy <me.suzana@gmail.com>
2025-08-13 04:32:45 +0300

    Add Unsorted/ezupload_pro_v21.0_nullified/ezupload/index.php

---
+++ Unsorted/ezupload_pro_v21.0_nullified/ezupload/index.php
@@ -0,0 +1,8 @@
+<table border="0" cellspacing="0" cellpadding="0" valign="middle" width="100%" height="100%">
+  <tr>
+    <td>
+	  <?php $path = ""; include( $path . "form.php" ); ?>
+	</td>
+  </tr>
+</table>
+
