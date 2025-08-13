12e776a899583a1d1193ae005f1d289a0fe2e44f
me-suzy <me.suzana@gmail.com>
2025-08-13 06:14:05 +0300

    Add Unsorted/lizardcart/admin/footer.php

---
+++ Unsorted/lizardcart/admin/footer.php
@@ -0,0 +1,66 @@
+
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+		  <!-- end of main content -->
+        </td>
+      </tr>
+      <tr height="52" bgcolor="#003399">
+        <td valign="top">
+		<!-- begin bottom nav -->
+          <table border="0" cellspacing="0" cellpadding="1" width="100%"
+          height="100%">
+            <tr>
+              <td valign="top">
+			  <!-- begin bottom nav links -->
+                <table border="0" cellspacing="0" cellpadding="0" height="18">
+                  <tr
+                  style="color: White; font-size: 10px; font-weight: bold; font-family: Tahoma;">
+                    <td onmouseover="on(this)" onmouseout="off(this)"
+                    style="cursor: hand" onclick="go('mailto:?body='+location.href)">
+                      &#160;&#160;<a href="mailto:ts@2tonewebdesign.com?subject=Great Site!! Mail&body=Just Love This Cart!!"><font color="#FFFFFF">Contact</font></a>&#160;&#160;
+                    </td>
+                    <td class="sep">
+                    </td>
+                    <td onmouseover="on(this)" onmouseout="off(this)"
+                    style="cursor: hand onclick="print()">
+                      &#160;&#160;<a href="index.php"><font color="#FFFFFF">List Products</font></a>&#160;&#160;
+                    </td>
+                    <td class="sep">
+                    </td>
+                    <td onmouseover="on(this)" onmouseout="off(this)"
+                    style="cursor: hand" onclick="go('javascript:void(0)')">
+                      &#160;&#160;<a href="pagelist.php"><font color="#FFFFFF">list Pages</font></a>&#160;&#160;
+                    </td>
+					                    <td class="sep">
+                    </td>
+                    <td onmouseover="on(this)" onmouseout="off(this)"
+                    style="cursor: hand" onclick="go('javascript:void(0)')">
+                      &#160;&#160;<a href=""><font color="#FFFFFF">Help</font></a>&#160;&#160;
+                    </td>
+                  </tr>
+                </table>
+				<!-- end of bottom nav links -->
+                <br />
+                 &#160;&#160;<span
+                style="color: White; font-family: Tahoma; font-size: 10px;">&#169;
+                2002 Lizard Cart. ( <a href="http://www.2tonewebdesign.com" class="bottom" target="_blank">2tone Web Design</a> ).</span>
+              </td>
+            </tr>
+          </table>
+		  <!-- end bottom nav -->
+        </td>
+      </tr>
+    </table>
+	<!-- end of main table -->
+<div align="center"><br><A class=nolinebold href="#top" target=_self>| Top of Page |</a><br><br><b>Copyleft © 2tone Web Design<br>
+Lizard Cart By © <a href="http://www.2tonewebdesign.com/" class="noline">2tone Web Design</a></b></div>
+<div align="center"><b>This free software is licensed under the terms of the GNU public license</b></div>
+</body>
+<script>QBPATH='<? echo "$qbpath";?>'; VISUAL=0; SECURE=1;</script>
+<script src='<? echo "$qbpathjs";?>'></script>
+<script src='<? echo "$qbpathtable";?>'></script>
+</html>
