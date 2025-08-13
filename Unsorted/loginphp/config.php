7c76f6fe28e7574f199d5c8b059d023932307c89
me-suzy <me.suzana@gmail.com>
2025-08-13 06:15:22 +0300

    Add Unsorted/loginphp/config.php

---
+++ Unsorted/loginphp/config.php
@@ -0,0 +1,10 @@
+<?php
+$DBHOST = "localhost";      //mysql host name
+$DBUSER = "";               //database username
+$DBPASS = "";               //database password
+$DBNAME = "";               //database name
+$ADMINUNAME = "";           //admin username
+$ADMINPWORD = "";           //admin password
+$ADMINEMAIL = "";           //admin email
+require("dbconnection.php");
+?>
