ccf1c866fca714b001f35e12ae52ebc338813c63
me-suzy <me.suzana@gmail.com>
2025-08-13 07:07:57 +0300

    Add Unsorted/olatedownload2.2.0/olatedownload2.2.0/html/admin/logout.php

---
+++ Unsorted/olatedownload2.2.0/olatedownload2.2.0/html/admin/logout.php
@@ -0,0 +1,21 @@
+<?php
+/***************************************************************************
+ *                      Olate Download v2 - Download Manager
+ *
+ *                           http://www.olate.com
+ *                            -------------------
+ *   author                : David Mytton
+ *   copyright             : (C) Olate 2003 
+ *
+ *   Support for Olate scripts is provided at the Olate website. Licensing
+ *   information is available in the license.htm file included in this
+ *   distribution and on the Olate website.                  
+ ***************************************************************************/
+
+// Start script
+$admin = 1;
+require_once('../includes/init.php'); 
+
+// Function: Logout user
+admin_logout();
+?>
