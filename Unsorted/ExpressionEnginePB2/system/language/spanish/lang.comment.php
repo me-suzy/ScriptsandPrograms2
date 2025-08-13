a9f374ec430fa79cae15a5885f9bd5e5bd508a05
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:40 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.comment.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.comment.php
@@ -0,0 +1,85 @@
+<?php
+$L = array(
+
+
+"comment_module_name" =>
+"Comentario",
+
+"comment_module_description" =>
+"Sistema de comentarios de usuario",
+
+"cmt_name_not_allowed" =>
+"The name you are posting with is reserved.  Please choose a different name.",
+
+"cmt_must_be_member" =>
+"Debes de ser un miembro registrado para publicar comentarios",
+
+"cmt_no_authorized_for_comments" =>
+"No estás autorizado a publicar comentarios",
+
+"cmt_account_not_active" =>
+"Tu cuenta de miembro aún no ha sido activada",
+
+"cmt_comments_not_allowed" =>
+"No se permiten comentarios en este weblog",
+
+"cmt_missing_name" =>
+"Se requiere el campo de nombre",
+
+"cmt_missing_email" =>
+"Se requeire el campo de email",
+
+"cmt_invalid_email" =>
+"La dirección de email que has indicado no es válida",
+
+"cmt_banned_email" =>
+"La dirección de email que has enviado está prohibida",
+
+"cmt_too_large" =>
+"El comentario que has enviado contiene %n caracteres y sólo se permiten %x.",
+
+"cmt_comments_timelock" =>
+"Sólo estás autorizado a publicar comentarios cada %s segundos.",
+
+"cmt_duplicate_comment_warning" =>
+"Imposible recibir tu comentario en estos momentos.",
+
+"cmt_no_preview_template_specified" =>
+"La etiqueta de formulario del comentario no contiene la ubicación de la plantilla de vista previa",
+
+"cmt_comment_response_title" =>
+"Alguien ha respondido a tu comentario",
+
+"cmt_comment_response_body" =>
+"Alguien ha respondido al artículo al que te subscribiste:",
+
+"cmt_comment_title" =>
+"El título del artículo es:",
+
+"cmt_comment_url" =>
+"Puedes ver el comentario en la siguiente URL:",
+
+"cmt_comment_removal" =>
+"Para dejar de recibir notificaciones sobre este comentario, haz click aquí:",
+
+"cmt_comment_removal_all" =>
+"Para dejar de recibir todas las notificaciones, haz click aquí:",
+
+"cmt_notification_removal" =>
+"Eliminación de notificaciones",
+
+"cmt_you_have_been_removed" =>
+"Ha sido borrado de la recepción de notificaciones a partir de ahora",
+
+"cmt_comment_accepted" =>
+"Comment Accepted",
+
+"cmt_will_be_reviewed" =>
+"Your comment will be reviewed by a moderator for approval.",
+
+"cmt_return_to_comments" =>
+"Return to Comments",
+
+''=>''
+);
+?>
