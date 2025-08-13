9cac862c8d1db6ad3d39673b5ac741e61370eef7
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:16 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/english/lang.mt_import.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/english/lang.mt_import.php
@@ -0,0 +1,99 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// MT Import
+//----------------------------
+
+"mt_import_utility" =>
+"Movable Type Import Utility",
+
+"mt_import_welcome" =>
+"This utility enables you to import Movable Type data into ExpressionEngine",
+
+"mt_import_disclaimer" =>
+"If you are not using Movable Type you will not need this utility.",
+
+"file_blurb" =>
+"In order to use this utility, you must first put the file containing your exported MT entries on your server and then submit the location of this file.",
+
+"file_blurb2" =>
+"The location of the file must be relative to your site's control panel folder.  (ex:  ../mt_file.txt), if placed at the site root.",
+
+"field_blurb" =>
+"The weblog which is receiving this data must have fields named 'summary' (for MT's excerpt field), 'body', 'extended', and 'keywords' available to it in order to receive
+all of the MT Import fields.",
+
+"import_info" =>
+"Import Information",
+
+"file_info" =>
+"Movable Type file location",
+
+"weblog_select" =>
+"Choose the weblog in which to import your entries.",
+
+"admin_select" =>
+"Choose the EE member to whom these entries will be assigned.",
+
+"status_select" =>
+"Status of imported entries.",
+
+"category_select" =>
+"Category of imported entries.",
+
+"auto_create" =>
+"Auto Create Categories",
+
+"submit_info" =>
+"Begin Importing Entries",
+
+"empty_field_warning" =>
+"You left some fields empty",
+
+"invalid_path" =>
+"File not found.",
+
+"invalid_file" =>
+"File contains no exported MT entries.",
+
+'import_complete' =>
+"Import Complete",
+
+"total_weblog_entries" =>
+"Total number of weblog entries entered:",
+
+"total_weblog_comments" =>
+"Total number of comments entered:",
+
+"total_weblog_trackbacks" =>
+"Total number of trackbacks entered:",
+
+"total_categories_entered" =>
+"Total number of categories created:",
+
+"more_importing_info" =>
+"If you have more MT entries to import, click the link below.  If you are finished importing,
+ then recount your statistics.",
+
+"return_to_import" =>
+"Return to the MT Import page",
+
+"you_are_done_importing" =>
+"You have successfully completed the import process!",
+
+"recalculate_statistics" =>
+"Recount Statistics",
+
+"click_to_reset_statistics" =>
+"Click here to update weblog statistics",
+
+"you_are_done_importing" =>
+"You have successfully completed importing your entries!",
+
+
+// END
+''=>''
+);
+?>
