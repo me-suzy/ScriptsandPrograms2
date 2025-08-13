9d1419a30a1f864ef48b25e7ce3a8e7b917d88b9
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:49 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.admin.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.admin.php
@@ -0,0 +1,859 @@
+<?php
+$L = array(
+
+
+"word_separator" =>
+"Word Separator for URL Titles",
+
+"dash" =>
+"Dash",
+
+"underscore" =>
+"Underscore",
+
+"site_name" =>
+"Naam van de website",
+
+"system_admin" =>
+"Systeem administratie",
+
+"system_preferences" =>
+"Systeem voorkeuren",
+
+"is_system_on" =>
+"Is het systeem aan",
+
+"is_system_on_explanation" =>
+"Als het systeem offline is, alleen Super Admins zijn bevoegd de site te bekijken",
+
+"system_off_msg" =>
+"&#39;Systeem Offline&#39; Bericht",
+
+"offline_template" =>
+"&#39;Systeem Uit&#39; Template",
+
+"offline_template_desc" =>
+"Deze template bevat de pagina die zichtbaar is wanneer uw site offline is.",
+
+"template_updated" =>
+"Template geupdatet",
+
+"preference_information" =>
+"Voorkeur begeleiding",
+
+"preference" =>
+"Voorkeur",
+
+"value" =>
+"Waarde",
+
+"general_cfg" =>
+"Algemene Configuratie",
+
+"enable_db_caching" =>
+"Inschakelen van SQL Query Caching ",
+
+"db_cache_refresh" =>
+"Query Cache opschonen",
+
+"db_cache_refresh_exp" =>
+"Het aantal &lt;b&gt;DAGEN&lt;/b&gt; dat oude cache files niet geupdatet zijn. 30 dagen is normaal voor een gemiddelde website.",
+
+"member_cfg" =>
+"Lidmaatschap Voorkeuren",
+
+"profile_templates" =>
+"Leden  Profiel Templates",
+
+"allow_member_registration" =>
+"Nieuwe leden registraties toestaan?",
+
+"req_mbr_activation" =>
+"Activatie van een lid-account vereist?",
+
+"no_activation" =>
+"Geen activatie vereist",
+
+"email_activation" =>
+"Zelf-activatie via email",
+
+"manual_activation" =>
+"Handmatige activatie door een administrator",
+
+"require_terms_of_service" =>
+"Terms of Service vereist?",
+
+"member_theme" =>
+"Leden Profiel Thema",
+
+"member_theme_exp" =>
+"Kiezen welk Thema gebruikt moet worden voor de leden pagina&#39;s: inloggen, registratie, profiel, etc.",
+
+"member_images" =>
+"Pad naar leden plaatjes.",
+
+"member_images_exp" =>
+"Dit is het pad naar de map die de plaatjes, die gebruikt worden in de leden-profiel pagina, bevatten ",
+
+"require_terms_of_service_exp" =>
+"Deze optie op &quot;ja&quot; zetten zorgt ervoor dat de gebruikers de &quot;accepteer de Algemene Voorwaarden&quot; moeten aanvinken tijdens de registratie.",
+
+"new_member_notification" =>
+"Laat de administrators weten wanneer er nieuwe registraties zijn?",
+
+"mbr_notification_emails" =>
+"Email adres voor het bericht",
+
+"separate_emails" =>
+"Scheid de verschillende email adressen met komma&#39;s",
+
+"email_console_timelock" =>
+"&#39;Email Mogelijkheid&#39; Tijdslot",
+
+"email_console_timelock_exp" =>
+"Het aantal minuten dat een lid moet wachten om weer een nieuwe e-mail te kunnen versturen.  Let op:  Dit geldt alleen voor de &#39;Email mogelijkheid&#39; in de leden-profiel pagina.",
+
+"log_email_console_msgs" =>
+"Log &#39;Email mogelijkheid&#39; Berichten",
+
+"log_email_console_msgs_exp" =>
+"Deze voorkeur logt alle berichten die zijn verstuurd via de &#39;Email Mogelijkheid&#39; in de leden-profiel pagina.",
+
+"default_member_group" =>
+"Standaard Ledengroep waarin nieuwe leden worden geplaatst",
+
+"group_assignment_defaults_to_two" =>
+"Wanneer u account-activatie vereist, zullen ze aan deze groep worden toegevoegd totdat ze ge-activeerd zijn",
+
+"view_email_logs" =>
+"&#39;Email Mogelijkheid&#39; Logs",
+
+"user_session_type" =>
+"Gebruikerssessie Type",
+
+"admin_session_type" =>
+"Control Panel Sessie Type",
+
+"security_cfg" =>
+"Beveiliging en Sessie Instellingen",
+
+"un_min_len" =>
+"Minimale lengte van de Gebruikersnaam",
+
+"pw_min_len" =>
+"Minimale lengte van het Paswoord",
+
+"cs_session" =>
+"Cookies en sessie ID",
+
+"c_session" =>
+"Alleen Cookies",
+
+"s_session" =>
+"Alleen Sessie ID",
+
+"secure_forms" =>
+"Proces formulier data in Beveiligde Modus?",
+
+"deny_duplicate_data" =>
+"Dezelfde gegevens niet accepteren?",
+
+"deny_duplicate_data_explanation" =>
+"Deze optie zorgt ervoor dat gegevens die toegevoegd worden door gebruikers (commentaar, trackbacks, etc.) niet worden geaccepteerd als deze precies hetzelfde zijn als hetgeen al eerder geschreven is.",
+
+"secure_forms_explanation" =>
+"Voorkom automatische spam en meerdere &#39;perongeluk&#39; dezelfde toevoegingen",
+
+"allow_multi_logins" =>
+"Meerdere keren inloggen van hetzelfde account toestaan?",
+
+"allow_multi_logins_explanation" =>
+"Voorkomt dat meer dan 1 persoon tegelijk kan inloggen onder hetzelfde account. Let op: Als de bovenstaande Sessie Type op &quot;Alleen Cookies&quot; staat al deze functie niet werken.",
+
+"password_lockout" =>
+"Wachtwoord blokkeren inschakelen?",
+
+"password_lockout_explanation" =>
+"Als deze functie is ingeschakeld, mag een gebruiker maar 4 keer verkeerd inloggen. Daarna zal hij de onderstaande tijd moeten wachten voordat hij weer kan inloggen. Dit om te voorkomen dat hackers heel veel mogelijkheden proberen om achter het wachtwoord te komen. ",
+
+"password_lockout_interval" =>
+"De tijd dat een account geblokkeerd moet zijn",
+
+"login_interval_explanation" =>
+"Het aantal is aangegeven in minuten. U kunt deze tot 1 decimaal nauwkeurig achter de komma instellen. Bijvoorbeeld:  1.5",
+
+"require_ip_for_login" =>
+"IP-adres en User-Agent vereist om in te loggen?",
+
+"require_ip_explanation" =>
+"Voorkomt dat gebruikers kunnen inloggen zonder dat hun browser een IP-adres en User Agent data verstuurd. Dit weerhoudt hackers ervan om in te loggen met directe socket connecties.",
+
+"allow_multi_emails" =>
+"Meerdere Accounts met hetzelfde emailadres toestaan?",
+
+"allow_username_change" =>
+"Toestaan dat leden hun gebruikersnaam veranderen?",
+
+"require_secure_passwords" =>
+"Beveiligde wachtwoorden vereist?",
+
+"secure_passwords_explanation" =>
+"Gebruikers moeten een wachtwoord kiezen die tenminste bestaat uit een hoofdletter, een kleine letter en een nummeriek teken.",
+
+"allow_dictionary_pw" =>
+"Woordenboek-woorden toestaan als wachtwoord?",
+
+"real_word_explanation" =>
+"Deze optie voorkomt dat er gebruikers zijn die woorden gaan gebruiken die in een woordenboek te vinden zijn.",
+
+"dictionary_note" =>
+"Let op: Om deze functie te kunnen gebruiken moet het &#39;woordenboek&#39; bestand ge-installeerd worden. Raadpleeg de handleiding.",
+
+"name_of_dictionary_file" =>
+"Naam van de map",
+
+"dictionary_explanation" =>
+"Naam van het bestand die de woordenlijst bevat",
+
+"image_path" =>
+"Pad naar de afbeeldingen map",
+
+"cp_url" =>
+"URL naar je Control Panel  pagina",
+
+"with_trailing_slash" =>
+"Met verplichte slash",
+
+"site_url" =>
+"URL naar de hoofd map van uw website",
+
+"url_explanation" =>
+"Dit is de map die de index file bevat van uw website",
+
+"doc_url" =>
+"URL naar de Documentatie Map",
+
+"doc_url_explanation" =>
+"Hoofd map alleen, met slash",
+
+"site_index" =>
+"Naam van uw website",
+
+"system_path" =>
+"Absolute pad naar je %x map",
+
+"force_query_string" =>
+"Force URL query strings",
+
+"safe_mode" =>
+"Loopt PHP op uw server op dit moment in de Safe Mode?",
+
+"force_query_string_explanation" =>
+"Dit is een veiligheidsmechanisme voor servers die niet de PATH_INFO variabele ondersteunen.",
+
+"debug" =>
+"Foutopsporing Voorkeuren",
+
+"debug_explanation" =>
+"Zorgt er voor dat foutmeldingen zichtbaar worden. Handig als u bezig bent met ontwikkeling van uw website",
+
+"show_queries" =>
+"SQL Queries zichtbaar maken?",
+
+"show_queries_explanation" =>
+"Indien aangezet, Super Admins kunnen alle SQL queries zien aan de onderkant van hun browser. Handig om fouten op te sporen.",
+
+"debug_zero" =>
+"0: Geen PHP/SQL foutmeldingen genereren",
+
+"debug_one" =>
+"1: PHP/SQL foutmeldingen alleen zichtbaar voor Super Admins",
+
+"debug_two" =>
+"2: PHP/SQL foutmeldingen zichtbaar voor iedereen - NIET VEILIG",
+
+"deft_lang" =>
+"Standaard taal",
+
+"xml_lang" =>
+"Standaard XML taal",
+
+"used_in_meta_tags" =>
+"Gebruik in het Control Panel meta tags",
+
+"charset" =>
+"Standaard Character Set",
+
+"gzip_output" =>
+"Inschakelen van GZIP uitvoering?",
+
+"gzip_output_explanation" =>
+"Wanneer deze ingeschakeld is, zal uw website in een gecomprimeerd formaat zichtbaar zijn voor een snellere laadtijd",
+
+"send_headers" =>
+"Genereer HTTP Page Headers?",
+
+"redirect_method" =>
+"Redirection Methode",
+
+"location_method" =>
+"Locatie (sneller)",
+
+"refresh_method" =>
+"Ververs (Windows servers)",
+
+"localization_cfg" =>
+"Locatie Instellingen",
+
+"time_format" =>
+"TijdsFormaat",
+
+"united_states" =>
+"Verenigde Staten",
+
+"european" =>
+"Europese",
+
+"server_timezone" =>
+"Server Tijd Zone",
+
+"server_offset" =>
+"Server Offset (in minuten)",
+
+"server_offset_explain" =>
+"Use the minus sign to subtract minutes: -15",
+
+"daylight_savings" =>
+"Zomertijd",
+
+"cookie_cfg" =>
+"Cookie Instellingen",
+
+"cookie_domain" =>
+"Cookie Domein",
+
+"cookie_domain_explanation" =>
+"Gebruik .uwdomein.nl voor &#39;cookies over de hele website&#39;",
+
+"cookie_prefix" =>
+"Cookie Voorvoegsel",
+
+"cookie_prefix_explain" =>
+"Gebruik dit alleen als je meerdere installaties van dit programma hebt draaien.",
+
+"cookie_path" =>
+"Cookie Pad",
+
+"cookie_path_explain" =>
+"Gebruik dit alleen als je een specifiek server pad eist voor je cookies",
+
+"image_cfg" =>
+"Afbeelding voorkeuren",
+
+"enable_image_resizing" =>
+"Inschakelen Afbeeldingen Verkleinen",
+
+"enable_image_resizing_exp" =>
+"Indien ingeschakeld bent u in de gelegenheid om foto&#39;s te verkleinen als u deze wilt plaatsen in uw weblog berichten",
+
+"image_resize_protocol" =>
+"Afbeelding Verklein Protocol",
+
+"image_resize_protocol_exp" =>
+"Controleer samen met uw hosting provider of uw server in staat is het gebruikte protocol te gebruiken",
+
+"image_library_path" =>
+"Afbeelding Verzameling pad",
+
+"image_library_path_exp" =>
+"Als u of ImageMagick of NetPBM gebruik moet u het pad specificeren naar de verzameling.",
+
+"gd" =>
+"GD",
+
+"gd2" =>
+"GD 2",
+
+"netpbm" =>
+"NetPBM",
+
+"imagemagick" =>
+"ImageMagik",
+
+"thumbnail_prefix" =>
+"Afbeelding Verklein Achtervoegsel",
+
+"thumbnail_prefix_exp" =>
+"Dit achtervoegsel zal automatisch toegeveogd worden aan alleen verkleinde afbeeldingen.  Voorbeeld: foto_thumb.jpg",
+
+"email_cfg" =>
+"Email Configuratie",
+
+"mail_protocol" =>
+"Email Protocol",
+
+"smtp_server" =>
+"SMTP Server Adres",
+
+"smtp_username" =>
+"SMTP Gebruikersnaam",
+
+"smtp_password" =>
+"SMTP Wachtwoord",
+
+"only_if_smpte_chosen" =>
+"Gebruik dit alleen als u gekozen heeft voor SMTP",
+
+"email_batchmode" =>
+"Wilt u de Batch Mode gebruiken?",
+
+"batchmode_explanation" =>
+"Batch Mode zal grote mailings splitsen in kleinere groepjes, die gedeeltelijk verstuurd zullen worden.  Aanbevolen als uw site gehost wordt op een shared-hosting account.",
+
+"email_batch_size" =>
+"Aantal Emails per Batch",
+
+"batch_size_explanation" =>
+"Voor een gemiddelde server is 300 een veilig aantal",
+
+"webmaster_email" =>
+"Antwoord email adres voor automatisch gegenereerde emails",
+
+"return_email_explanation" =>
+"Als je dit veld leeg laat, zullen veel e-mailservers je mail als spam markeren.",
+
+"php_mail" =>
+"PHP Mail",
+
+"sendmail" =>
+"Sendmail",
+
+"smtp" =>
+"SMTP",
+
+"plain_text" =>
+"Eenvoudige tekst",
+
+"html" =>
+"HTML",
+
+"mail_format" =>
+"Standaard Mail Formaat",
+
+"word_wrap" =>
+"Schakel woordafbreking standaard in?",
+
+"cp_theme" =>
+"Standaard Control Panel Thema",
+
+"template_cfg" =>
+"Template Voorkeuren",
+
+"save_tmpl_revisions" =>
+"Bewaar de &#39;Template herzien&#39; standaard",
+
+"censoring_cfg" =>
+"Woord Censurering",
+
+"enable_censoring" =>
+"Woord Censurering inschakeling?",
+
+"censored_words" =>
+"Gesencureerde Woorden",
+
+"censored_explanation" =>
+"Plaats elk woord op een aparte lijn",
+
+"emoticon_cfg" =>
+"Emoticon Voorkeuren",
+
+"enable_emoticons" =>
+"Smileys zichtbaar maken?",
+
+"emoticon_path" =>
+"URL naar de map die je smiley bevatten",
+
+"referrer_cfg" =>
+"Referrer Voorkeuren",
+
+"log_referrers" =>
+"Inschakelen van Referrer Tracking?",
+
+"log_referrers_explanation" =>
+"Indien ingeschakeld , één extra databasequery, per keer dat je pagina geladen wordt, zal uitgevoerd worden.",
+
+"weblog_administration" =>
+"Weblog Administratie",
+
+"weblog_management" =>
+"Weblog Beheer",
+
+"field_management" =>
+"Aan te passen Weblog Velden",
+
+"file_upload_prefs" =>
+"Bestand Upload Voorkeuren",
+
+"categories" =>
+"Categorie Beheer",
+
+"default_ping_servers" =>
+"Standaard Ping Servers",
+
+"status_management" =>
+"Aantepassen Toevoeg-status",
+
+"edit_preferences" =>
+"Wijzig Voorkeuren",
+
+"preferences_updated" =>
+"Voorkeuren Geupdatet",
+
+"edit_groups" =>
+"Wijzig Groepen",
+
+"members_and_groups" =>
+"Leden en Groepen",
+
+"view_members" =>
+"Bekijk Leden",
+
+"member_validation" =>
+"Activeer Leden in de wachtrij",
+
+"register_member" =>
+"Nieuwe Leden Registratie",
+
+"member_search" =>
+"Leden Zoeken",
+
+"user_banning" =>
+"Gebruikers Blokkeren",
+
+"custom_profile_fields" =>
+"Aantepassen Profiel velden",
+
+"email_notification_template" =>
+"Email Notificatie Templates",
+
+"member_groups" =>
+"Leden Groepen",
+
+"utilities" =>
+"Utilities",
+
+"view_log_files" =>
+"Bekijk Log Bestanden",
+
+"clear_caching" =>
+"Verwijder Gecachte Data Bestanden",
+
+"page_caching" =>
+"Pagina (template) cache bestanden",
+
+"tag_caching" =>
+"Tag cache Bestanden",
+
+"db_caching" =>
+"Database cache bestanden",
+
+"all_caching" =>
+"Alle gecachte bestanden",
+
+"cache_deleted" =>
+"Cache bestanden zijn verwijderd",
+
+"php_info" =>
+"PHP Informatie",
+
+"sql_manager" =>
+"SQL Beheer",
+
+"sql_info" =>
+"SQL Informatie",
+
+"sql_utilities" =>
+"SQL Utilities",
+
+"database_type" =>
+"Database Type",
+
+"sql_version" =>
+"Database Versie",
+
+"database_size" =>
+"Database grootte",
+
+"database_uptime" =>
+"Database Uptime",
+
+"total_queries" =>
+"Totaal aantal server aanvragen sinds opstarten database",
+
+"sql_status" =>
+"Status Informatie",
+
+"sql_system_vars" =>
+"Systeem Variabelen",
+
+"sql_processlist" =>
+"Lijst van Processen",
+
+"sql_query" =>
+"Database Query Formulier",
+
+"query_result" =>
+"Query Resultaat",
+
+"browse" =>
+"Doorzoeken",
+
+"tables" =>
+"tabellen",
+
+"table_name" =>
+"Tabel Naam",
+
+"records" =>
+"Database Records",
+
+"size" =>
+"Grootte",
+
+"type" =>
+"Type",
+
+"analize" =>
+"Analizeer Tabellen",
+
+"optimize" =>
+"Optimaliseer SQL Tabellen",
+
+"repair" =>
+"Repareer SQL Tabellen",
+
+"optimize_table" =>
+"Optimaliseer geselecteerde tabellen",
+
+"repair_table" =>
+"Repareer geselecteerde tabellen",
+
+"view_table_sql" =>
+"Bekijk SQL structuur en data",
+
+"backup_tables_file" =>
+"Backup geselecteerde tabellen - Tekst bestand",
+
+"backup_tables_zip" =>
+"Backup geselecteerde tabellen - Zip bestand",
+
+"backup_tables_gzip" =>
+"Backup geselecteerde tabellen - Gzip bestand",
+
+"select_all" =>
+"Selecteer Alles",
+
+"no_buttons_selected" =>
+"U moet de tabellen selecteren om deze actie uit te voeren",
+
+"unsupported_compression" =>
+"Uw PHP installatie ondersteund niet deze compressie methode",
+
+"backup_info" =>
+"Gebruik dit formulier om een back-up van uw database te maken",
+
+"save_as_file" =>
+"Bewaar SQL backup als bestand",
+
+"view_in_browser" =>
+"Bekijk SQL backup in uw browser",
+
+"sql_query_instructions" =>
+"Gebruik dit formulier om een SQL Query in te voeren",
+
+"file_type" =>
+"Bestandstype: ",
+
+"zip" =>
+"Zip",
+
+"gzip" =>
+"Gzip",
+
+"advanced_users_only" =>
+"Alleen Voor Ervaren Gebruikers",
+
+"recount_stats" =>
+"Statistieken opnieuw tellen",
+
+"preference_updated" =>
+"Voorkeuren Geupdatet",
+
+"click_to_recount" =>
+"Klik om de rijen %x t/m %y opnieuw te tellen",
+
+"items_remaining" =>
+"Records te gaan:",
+
+"recount_completed" =>
+"Hertelling voltooid",
+
+"return_to_recount_overview" =>
+"Terug naar de Hoofd Hertelling pagina",
+
+"recounting" =>
+"Hertelling",
+
+"recount_info" =>
+"De links hieronder geven je de mogelijkheid  om diverse statistieken te updaten. Bijv: hoeveel berichten elk lid heeft toegevoegd.",
+
+"source" =>
+"Bron",
+
+"total_records" =>
+"Totaal aantal Records:",
+
+"recalculate" =>
+"Hertel Statistieken",
+
+"do_recount" =>
+"Hertelling Uitvoeren",
+
+"set_recount_prefs" =>
+"Hertelling Voorkeuren",
+
+"recount_instructions" =>
+"Totaal aantal rijden die per &#39;Batch&#39; uitgevoerd worden.",
+
+"recount_instructions_cont" =>
+"Om te voorkomen dat de server vastloopt, het hertellen zal in &#39;batches&#39; plaatsvinden. Voor de meeste servers is 1000 een mooi aantal. Als u een sneller server of een dedicated server heeft kan het aantal batches hoger gezet worden.",
+
+"exp_members" =>
+"Leden",
+
+"exp_weblog_titles" =>
+"Weblog Toevoegingen",
+
+"search_and_replace" =>
+"Zoeken en Vervangen",
+
+"data_pruning" =>
+"Data opschonen",
+
+"sandr_instructions" =>
+"Dit formulier geeft je de mogelijkheid om te zoeken naar een bepaalde tekst en deze te vervangen door een ander tekst.",
+
+"search_term" =>
+"Zoeken naar deze tekst",
+
+"replace_term" =>
+"En vervangen door deze tekst",
+
+"replace_where" =>
+"In welk databaseveld moet de vervangen gebeuren?",
+
+"search_replace_disclaimer" =>
+"Vanwege het gebruikte achtervoegsel, deze functie kan tot ongewenste resultaten leiden. Raadpleeg de handleiding.",
+
+"title" =>
+"Titel",
+
+"weblog_entry_title" =>
+"Weblog Toevoeging Titels",
+
+"weblog_fields" =>
+"Weblog Velden:",
+
+"templates" =>
+"Templates",
+
+"rows_replaced" =>
+"Aantal database records waarin vervangen zal worden:",
+
+"view_database" =>
+"Beheer Database Tabellen",
+
+"sql_backup" =>
+"Database Backup",
+
+"sql_no_result" =>
+"Er zijn geen resultaten gevonden voor deze zoekopdracht",
+
+"sql_not_allowed" =>
+"Sorry, maar dit is geen toegestane zoekopdracht",
+
+"translation_tool" =>
+"Vertaal Programma",
+
+"translation_dir_unwritable" =>
+"Waarschuwing: De &#39;Translation&#39; map is niet schrijfbaar.",
+
+"please_set_permissions" =>
+"Zet de &#39;permissions&#39; op 666 of 777 van de volgende map:",
+
+"choose_translation_file" =>
+"Kies een bestand om te vertalen",
+
+"core_language_files" =>
+"Standaard taal bestanden:",
+
+"module_language_files" =>
+"Module taal bestanden:",
+
+"file_saved" =>
+"Het bestand is opgeslagen",
+
+"default_html_buttons" =>
+"Standaard HTML Knoppen",
+
+"import_from_pm" =>
+"pMachine Importeer Programma",
+
+"import_from_mt" =>
+"Movable Type Importeer Programma",
+
+"specialty_templates" =>
+"Specialty Templates",
+
+"user_messages_template" =>
+"Gebruikers Bericht Template",
+
+"plugin_manager" =>
+"Plugin Beheer",
+
+"installed_plugins" =>
+"Geïnstalleerde Plugins",
+
+"no_plugins_exist" =>
+"Er zijn op dit moment geen plugins geïnstalleerd",
+
+"view_info" =>
+"Bekijk de Informatie",
+
+"plugin_information" =>
+"Plugin Informatie",
+
+"pi_name" =>
+"Naam",
+
+"pi_author" =>
+"Auteur",
+
+"pi_version" =>
+"Versie",
+
+"pi_author_url" =>
+"Auteur URL",
+
+"pi_description" =>
+"Beschrijving",
+
+"pi_usage" =>
+"Gebruik",
+
+"no_additional_info" =>
+"Er is geen extra informatie beschikbaar voor deze plugin",
+
+''=>''
+);
+?>
