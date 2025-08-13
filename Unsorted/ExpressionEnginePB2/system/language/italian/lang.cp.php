f5a3917c120d60dfe2cc76ee9048cf4d8eb8310c
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:48 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.cp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.cp.php
@@ -0,0 +1,157 @@
+<?php
+$L = array(
+
+
+"consult_user_guide" =>
+"Consulta la documentazione",
+
+"powered_by" =>
+"Powered by",
+
+"control_panel" =>
+"Control Panel",
+
+"main_menu" =>
+"Control Panel Home",
+
+"username" =>
+"Username",
+
+"password" =>
+"Password",
+
+"publish" =>
+"Pubblica",
+
+"edit" =>
+"Modifica",
+
+"design" =>
+"Modelli",
+
+"communicate" =>
+"Comunica",
+
+"modules" =>
+"Moduli",
+
+"admin" =>
+"Admin",
+
+"my_account" =>
+"My account",
+
+"user_account" =>
+"Account utente",
+
+"user_guide" =>
+"Documentazione",
+
+"logout" =>
+"Log-out",
+
+"copyright" =>
+"Copyright",
+
+"rights_reserved" =>
+"Tutti i diritti riservati",
+
+"page_rendered" =>
+"Script eseguito in %x secondi",
+
+"queries_executed" =>
+"%x sulle interrogazioni SQL totali",
+
+"build" =>
+"Build:",
+
+"forgot_password" =>
+"Password smarrita?",
+
+"e_no_css" =>
+"Il file CSS richiesto non può essere aperto.",
+
+"unauthorized" =>
+"Non autorizzato",
+
+"unauthorized_access" =>
+"Non sei autorizzato ad accedere a questa pagina",
+
+"be_careful" =>
+"ATTENZIONE!",
+
+"action_can_not_be_undone" =>
+"QUESTA OPERAZIONE È IRREVERSIBILE",
+
+"back" =>
+"Indietro",
+
+"no" =>
+"No",
+
+"yes" =>
+"Sì",
+
+"all" =>
+"Tutti",
+
+"any" =>
+"Ogni",
+
+"required_fields" =>
+"Indica i campi richiesti",
+
+"please_consult_manual" =>
+"Consulta la documentazione.",
+
+"login" =>
+"Log-in",
+
+"forgotten_password" =>
+"Password smarrita",
+
+"submit_email_address" =>
+"Specifica il tuo indirizzo e-mail",
+
+"submit" =>
+"Invia",
+
+"preview" =>
+"Anteprima",
+
+"update" =>
+"Aggiorna",
+
+"delete" =>
+"Cancella",
+
+"save_changes" =>
+"Salva modifiche",
+
+"clear_logs" =>
+"Azzera i file log",
+
+"member_id" =>
+"ID utente",
+
+"ip_address" =>
+"Indirizzo IP",
+
+"date" =>
+"Data",
+
+"action" =>
+"Operazione",
+
+"member_logged_in" =>
+"Collegato",
+
+"member_logged_out" =>
+"Scollegato",
+
+"cleared_logs" =>
+"File log azzerati",
+
+''=>''
+);
+?>
