5564cda06b01b1fc1259751916cec76cfe5cff37
me-suzy <me.suzana@gmail.com>
2025-08-13 03:31:01 +0300

    Add Unsorted/ebay/sql/ebay_category_filters.sql

---
+++ Unsorted/ebay/sql/ebay_category_filters.sql
@@ -0,0 +1,26 @@
+/*	$Id: ebay_category_filters.sql,v 1.2 1999/05/19 02:35:06 josh Exp $	*/
+/*
+ * ebay_category_filters.sql
+ *	contains category-filter cross-reference information
+ */
+
+drop table ebay_category_filters;
+
+ create table ebay_category_filters
+ (
+	CATEGORY_ID			NUMBER(38)
+		constraint		cat_filters_cat_id_nn
+			not null,
+	FILTER_ID			NUMBER(38)
+		constraint		cat_filters_id_nn
+			not null
+)
+tablespace itemd01
+storage (initial 1M next 500K);
+
+alter table ebay_category_filters
+	add constraint		category_filters_pk
+		primary key		(category_id, filter_id)
+		using index tablespace statmisci
+		storage (initial 1M next 500K);
+
