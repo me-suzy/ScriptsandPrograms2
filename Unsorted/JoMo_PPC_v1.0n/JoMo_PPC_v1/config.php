2853260e4fb062a5a94d806844072604063d9d74
me-suzy <me.suzana@gmail.com>
2025-08-13 05:50:33 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/config.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/config.php
@@ -0,0 +1,56 @@
+<?
+/*
+###############################
+#
+# JoMo Easy Pay-Per-Click Search Engine v1.0
+#
+#
+###############################
+#
+# Date                 : September 16, 2002
+# supplied by          : CyKuH [WTN]
+# nullified by         : CyKuH [WTN]
+#
+#################
+#
+# This script is copyright L 2002-2012 by Rodney Hobart (JoMo Media Group),
+All Rights Reserved.
+#
+# The use of this script constitutes acceptance of any terms or conditions,
+#
+# Conditions:
+#  -> Do NOT remove any of the copyright notices in the script.
+#  -> This script can not be distributed or resold by anyone else than the
+author, unless special permisson is given.
+#
+# The author is not responsible if this script causes any damage to your
+server or computers.
+#
+#################################
+
+*/
+?>
+<?php
+define("__CFG_HOSTNAME","localhost");
+define("__CFG_DATABASE","ppc");
+define("__CFG_USERNAME","ppc");
+define("__CFG_PASSWORD","ppc");
+define("__CFG_ADMIN_USERNAME","admin");
+define("__CFG_ADMIN_PASSWORD","admin");
+define("__CFG_GUEST_ADMIN_USERNAME","demo");
+define("__CFG_GUEST_ADMIN_PASSWORD","demo");
+define("__CFG_GUEST_ADMIN_ENABLE",1);
+define("__CFG_ADMIN_EMAIL","sales@youdomain.com");
+define("__CFG_KEYWORD_CATCHALL","[catch all]");
+define("__CFG_PATH_LIBS","libs/");
+define("__CFG_PATH_CODE","code/");
+define("__CFG_PATH_TEMPLATE","templates/");
+define("__CFG_PATH_JAVASCRIPT","resources/js/");
+define("__CFG_PATH_IMAGES","images/");
+define("__CFG_PATH_STYLESHEET","resources/css/");
+define("__CFG_PATH_COMPILE","resources/compile/");
+define("__SMARTY_DIR","libs/smarty/");
+define("__GOOGLE_DIR","libs/google/");
+define("__CFG_PATH_BANNERS","resources/banners/");
+define("__CFG_SCRIPT_STATUS", "work"); 
+?>
