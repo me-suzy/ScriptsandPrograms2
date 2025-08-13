233e6ecc6b96f58a7eb848f494bba9bcc81f1b3a
me-suzy <me.suzana@gmail.com>
2025-08-13 05:37:57 +0300

    Add Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_ucp.php

---
+++ Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_ucp.php
@@ -0,0 +1,397 @@
+<?php
+
+$lang = array (
+
+'forum_subs_header' => "Your Forum Subscriptions",
+
+'forum_subs_none' => "You are not subscribed to any forums at present",
+'f_none'  => "--",
+'in' => "In:",
+'by' => "By:",
+
+'ft_forum' => "Forum",
+'ft_posts' => "Replies",
+'ft_topics' => "Topics",
+'ft_unsub'  => "Unsubscribe",
+'ft_newpost'  => "Start New Topic",
+'ft_last_post' => "Last Post",
+
+'ft_unsub_all' => "Unsubscribe from all forums",
+
+'subs_today'    => "Last day",
+'subs_day'       => "Last %s days",
+'subs_all'       => "the beginning",
+
+'show_topics_from' => "Show topics from:",
+
+'subs_none'      => "There are no subscriptions to show, try selecting a less restrictive date range",
+
+'auto_prune'     => "Note: Subscriptions are pruned if there is no reply to the topic after %s days",
+'no_auto_prune'  => "Subscription auto pruning is switched off at present",
+
+'auto_track'      => "<b>Enable 'Email Notification' by default?</b><br>Enabling this option will automatically subscribe you to any topic you start or reply to.",
+
+'cf_required'     => "Required Information, this section must be completed",
+
+'pp_number_posts' => "Number of posts to show for each topic page",
+'pp_number_topics' => "Number of topics to show for each forum page",
+'pp_use_default'   => "Use forum default",
+
+'mess_saved'   => "Saved (Unsent) PM's",
+'dst_box' => "<b>Is daylight savings time in effect?</b><br>(If you have selected the correct time zone and the clock appears<br>an hour out, check or uncheck box to suit)",
+
+
+
+'lp_subject' => "Email address change at",
+
+'avup_yes' => "<b>You already have an uploaded avatar (shown at the top of this page)</b><br>Unless you choose to enter a URL for an avatar, or upload another avatar, your current uploaded avatar will remain",
+'avup_no'  => "You do not have an uploaded avatar stored, you may choose to upload an avatar by using the box above",
+
+'set_updated'    => "Settings updated",
+
+'hide_session_txt'   => "<b>Do you wish to hide your session id in the links?</b><br>(Note: Your computer must be able to receive temporary cookies to use this feature)<br>Choosing 'no' may cause problems when sharing links with people on the same proxy as you.",
+
+'m_messenger'  => "Messenger",
+
+'av_tt_one'  => "I don't wish to use an avatar (remove all current)",
+'av_tt_two'  => "Selecting this option will remove all current avatars if you have any selected/uploaded or entered in the URL field",
+
+'mess_new'     => "Compose New Message",
+
+'mess_contact' => "PM Buddies/Block List",
+'mess_folders' => "Edit Storage Folders",
+
+'mess_inbox'   => "Go to Inbox",
+
+'mess_archive' => "Archive Messages",
+'mess_tracker' => "Message Tracker",
+
+
+
+
+'forum_jump'  => "Forum Jump",
+
+'m_personal'  => "Personal Profile",
+'m_contact_info' => "Edit Profile info",
+'m_bio_info'     => "Edit Biography",
+'m_sig_info'     => "Edit Signature",
+'m_avatar_info'  => "Edit Avatar Settings",
+
+'m_options'   => "Options",
+
+'m_email_opt' => "Email Settings",
+'m_board_opt' => "Board Settings",
+'m_skin_lang' => "Skin and Languages",
+'m_email_change' => "Change Email Address",
+'m_passy_opt'    => "Change Password",
+
+'m_tracker'      => "Subscriptions",
+'m_view_subs'    => "View Topics",
+'m_view_forum'    => "View Forums",
+
+'m_links'        => "Personal Links",
+'m_edit_links'   => "Edit Links",
+
+'qc_header'       => "Quick Clicks",
+
+'qc_messenger'    => "Messenger",
+'qc_inbox'        => "|_ Inbox",
+'qc_sent'         => "|_ Sent Items",
+'qc_compose'      => "|_ Send Msg",
+'qc_prefs'        => "|_ Msg Prefs",
+
+'qc_search'       => "Search",
+'qc_home'         => "Forum Home",
+'qc_help'         => "Help",
+
+'qc_your_links'   => "Your links",
+
+'ta_size'   => 'Notepad size:',
+
+'new_window'  => "New Window",
+
+'subs_delete' => "Unsubscribe",
+
+'with_selected'  => "with selected",
+
+'ta_s'  => 'small',
+'ta_m' => 'medium',
+'ta_l'  => 'large',
+
+'cp_edit_sig'  => "Edit your signature",
+'cp_current_sig'  => "Your current Signature",
+'cp_submit_sig'   => "Update my Signature",
+
+'messenger_summary' => "Your messenger summary",
+'total_messages'    => "Total messages (in all folders)",
+'messages_left'     => "Message space left: ",
+'folders_full'      => "YOUR MESSENGER FOLDERS ARE FULL!",
+'total_capacity'    => "of total capacity",
+
+'change_email_title'  => "Change your registered email address",
+
+'ce_new_email'  => "Enter your new email address",
+'ce_new_email2' => "Confirm the new email address",
+
+'ce_current'    => "<b>Your current email address:</b> ",
+'ce_auth'       => "<br><br>PLEASE NOTE: You will have to re-activate your account after changing your email address.<br>An email will be sent to the email address you specify below, outlining what you need to do.<br><br>Proceeding with this action will log you out. It is strongly recommended that you reset your password before changing your email address if you are unable to remember your password.",
+
+'ce_redirect'   => "An email has been sent, redirecting to the activation screen",
+
+'ce_subject' => "Email change request from ",
+
+'pass_change_text' => "You may change your password here.<br><br>We will attempt to update your current session after your successful password change. If, however, you do experience difficulties, please try logging out and logging back in before contacting a staff member to help resolve the problem",
+
+'pass_redirect'    => "Your password has been changed. Depending on your cookie settings, you may need to log back in with your new password.",
+
+'upload_avatar'    => "<u>OR</u> Upload an avatar",
+
+//----------------------
+
+'cp_skin_preview'  => "Preview this skin",
+
+
+total_posts					=>	"Total Posts",
+aol_name					=>	"AOLAIM Name",
+home_page					=>	"Home Page",
+viewing_profile					=>	"Viewing member profile",
+interests					=>	"Interests",
+registration_date					=>	"Registration Date",
+change_email					=>	"Email Address Change request",
+no_posts					=>	"Member has yet to post",
+no_information					=>	"<i>No Information Entered</i>",
+msn_name					=>	"MSN Identity",
+location					=>	"Location",
+email_address					=>	"Email Address",
+profile_edited					=>	"Your profile has been edited",
+member_level					=>	"Member Level",
+post_average					=>	"Post Average",
+b_date					=>	"Your Date of Birth",
+send_pm					=>	"Click here to send <# NAME #> a Private Message",
+M_1					=>	"Jan.",
+M_2					=>	"Feb.",
+thank_you					=>	"Thank you",
+M_3					=>	"Mar.",
+send_email					=>	"Send <# NAME #> an email",
+M_4					=>	"April",
+D_0					=>	"Sun",
+registration_process					=>	"Email address change request",
+M_5					=>	"May",
+D_1					=>	"Mon",
+per_day					=>	"posts per day",
+M_6					=>	"June",
+D_2					=>	"Tue",
+no_photo					=>	"[ No Personal Photograph ]",
+M_7					=>	"July",
+D_3					=>	"Wed",
+M_8					=>	"Aug.",
+D_4					=>	"Thur",
+M_9					=>	"Sep.",
+D_5					=>	"Fri",
+D_6					=>	"Sat",
+hidden_email					=>	"<# NAME #> has chosen to keep his/her email address private",
+auth_text					=>	"Your Email address change request has been processed.<br><br>The board administrator has chosen to require validation for all email addresses. Within the next 10 minutes (usually instantly) you'll receive an email with instructions on the next step.<br>The email has been sent to",
+icq_number					=>	"ICQ Number",
+M_10					=>	"Oct.",
+M_11					=>	"Nov.",
+M_12					=>	"Dec.",
+add_book					=>	"Click here to add <# NAME #> to your contact book",
+contact_header					=>	"Contact Details",
+signature					=>	"Signature",
+last_modification					=>	"Last Profile Update",
+yahoo_name					=>	"Yahoo Identity",
+personal_header					=>	"Statistics and Personal Information for <# NAME #>",
+
+//----
+
+'note_pad'        => "Your personal note pad",
+'submit_notepad'  => "Update",
+
+'note_pad_empty'  => "You may store post snippets, or any other information you need to hand here",
+
+month1					=>	"January",
+birthday					=>	"Your Date of Birth",
+month2					=>	"February",
+month3					=>	"March",
+month4					=>	"April",
+av_personal					=>	"You are currently using a personal avatar",
+width					=>	"Width",
+month5					=>	"May",
+js_used					=>	"You have used",
+month6					=>	"June",
+month7					=>	"July",
+avatar_pre_title					=>	"Pre-installed avatars",
+month8					=>	"August",
+lp_redirect					=>	"Email dispatched",
+month9					=>	"September",
+settings_lang_txt					=>	"Please select a language for the board interface",
+settings_hour					=>	"Hours",
+edit_profile					=>	"Edit your profile",
+messenger_url					=>	"Go to your messenger",
+no					=>	"No",
+settings_time_txt					=>	"You may adjust the base time zone<br>The time (including your current adjustment) is:",
+personal_help					=>	"This is where you can amend your personal profile for other visitors to view",
+subs_forum					=>	"Forum",
+settings_submit					=>	"Change my Account Options",
+js_location					=>	"Location",
+
+avatar_url_allowed					=>	"<br>You can either select an avatar already installed on this board, or enter a URL for an image you've got uploaded on your website. Please choose which you'd prefer.",
+privacy_settings					=>	"Privacy Settings",
+avatar_show_all					=>	"<br><u>[ Help Cards: Show all avatars ]",
+account_title					=>	"Modify Your Account",
+welcome					=>	"Welcome to your control panel ",
+maximum					=>	"Maximum",
+signature					=>	"<b>Your Signature</b>",
+avatar_pre_txt					=>	"Please select an avatar from the list below, simply click on the avatar's name to view it.",
+height					=>	"Height",
+icq					=>	"<b>Your ICQ UIN</b>",
+subs_replies					=>	"Replies",
+avatar_url_title					=>	"Your image avatars",
+avatar_title					=>	"Your avatar options",
+month10					=>	"October",
+month11					=>	"November",
+account					=>	"Modify Account",
+month12					=>	"December",
+hide_email					=>	"<b>Hide my email address from other members</b><br>Enabling this option will deny other members sending you an email via the board.",
+account_email_txt					=>	"Your Email Address",
+settings_skin					=>	"Board 'Skin'",
+account_pass_new2					=>	"Please confirm your NEW password",
+website					=>	"<b>Your website url</b>",
+account_pass_title					=>	"Your Password",
+personal_ops					=>	"Your personal options",
+settings_title					=>	"Your Account Options",
+yahoo					=>	"<b>Your Yahoo identity</b>",
+subs_cancel					=>	"Cancel",
+
+account_email_title					=>	"Your registered email address",
+js_signature					=>	"Signature",
+settings_viewimg					=>	"Do you wish to view images in posts, such as smilies and posted images?",
+t_title					=>	"Your control panel",
+
+av_none_selected					=>	"[ No Avatar Selected ]",
+subs_header					=>	"Current Subscriptions",
+profile_title					=>	"Your personal profile, this section is optional",
+board_prefs					=>	"Board Preferences",
+subs_left					=>	"Days Left",
+subs_topic					=>	"Topic",
+settings_viewava					=>	"Do you wish to view members avatars when reading threads?",
+personal_ops_txt					=>	"this is where you can modify your personal information to allow others to find out a little more about you. This is also where you can set up your avatar. An avatar is a little image that is displayed under your name when you make a post.",
+subs_start					=>	"Subscribed on",
+submit_email					=>	"Amend my email settings",
+
+
+'msg_options'					=>	"Options",
+avatar_dims					=>	"<b>Avatar Dimensions</b>",
+avatar					=>	"<b>Your avatar</b>",
+subs_view					=>	"Views",
+email_text_lp					=>	"Lost Password request at",
+avatar_ops_txt					=>	"This is where you can choose your avatar to use when making a post. This is completely optional, but many members use them as a fun way to express themselves.",
+js_max					=>	"The maximumm allowed length is",
+interests					=>	"<b>Your interests</b>",
+submit_profile					=>	"Amend my profile",
+avatar_click					=>	"Click on one of the avatars in this list to add it into the 'Your Avatar' box.",
+location					=>	"<b>Your Location</b>",
+avatar_url_ext					=>	"The following file types are allowed: ",
+email_address					=>	"Your registered email address",
+pm_reminder					=>	"<b>Send a confirmation email when I receive a new private message</b><br>Enabling this option will send an email confirmation of any new private messages sent to you.",
+subs_last_post					=>	"Last Post",
+email					=>	"Email Settings",
+account_email_submit					=>	"Change my Email Address",
+settings					=>	"Account Options",
+photo					=>	"<b>You may enter a URL for a personal photograph</b><br>[This will only be displayed on your profile - not with your posts]",
+js_so_far					=>	"characters so far",
+email_help					=>	"This is where you can change your email settings",
+settings_dopopup					=>	"Do you wish to get a pop up notification when you get a new private message?",
+settings_display					=>	"Board Display Settings",
+settings_skin_txt					=>	"You may choose a 'skin' to use.<br>This will change the way the board looks",
+send_full_msg					=>	"<b>Include a copy of the post when emailing me from a subscribed topic</b><br>Enabling this option will send a plain text version of the post in the subscription email.",
+member_title					=>	"Custom member title",
+yes					=>	"Yes",
+account_pass_new					=>	"Enter your NEW password",
+settings_viewsig					=>	"Do you wish to view members signatures when reading threads?",
+js_interests					=>	"Interests",
+av_preset					=>	"You are currently using a pre-installed avatar",
+aol					=>	"<b>Your AOL identity</b>",
+settings_lang					=>	"Board Interface Language",
+pixels					=>	"pixels",
+t_welcome					=>	"Welcome to your control panel",
+personal					=>	"Personal Info",
+daily_average					=>	"Average number of posts per day",
+avatar_ops					=>	"Avatar Options",
+account_pass_submit					=>	"Change my Password",
+stats_header					=>	"Your account summary",
+settings_time					=>	"Time Zone",
+subs_title					=>	"Your Topic Subscriptions",
+no_posts					=>	"You've yet to post",
+subs					=>	"Topic Tracker",
+current_avatar					=>	"Avatar Preview",
+subs_deleted					=>	"The subscription has been deleted",
+avatar_pre_submit					=>	"Add this avatar to my profile",
+msn					=>	"<b>Your MSN Messenger identity</b>",
+avatar_sub					=>	"this is where you can choose to use an avatar under yourname when you post a message on these boards.",
+registered					=>	"You registered on ",
+avatar_url_submit					=>	"Add my own image as my avatar",
+subs_by					=>	"By:",
+number_posts					=>	"Total number of posts to date",
+account_pass_old					=>	"Enter your CURRENT password",
+js_characters					=>	"characters",
+account_email_req					=>	"You will receive an email asking you to verify this change",
+email_title					=>	"Your email settings",
+av_current					=>	"Your Current Avatar",
+check_length					=>	"Check Length",
+admin_send					=>	"<b>Send me any updates sent by the board administrator</b><br>Enabling this option will add your email address to the administrators mail list and you will receive any updates sent.",
+);
+
+
+// Time zones...
+
+$lang['time_-12'] = "(GMT - 12:00 hours) Enitwetok, Kwajalien";
+$lang['time_-11'] = "(GMT - 11:00 hours) Midway Island, Samoa";
+$lang['time_-10'] = "(GMT - 10:00 hours) Hawaii";
+$lang['time_-9']  = "(GMT - 9:00 hours) Alaska";
+$lang['time_-8']  = "(GMT - 8:00 hours) Pacific Time (US &amp; Canada)";
+$lang['time_-7']  = "(GMT - 7:00 hours) Mountain Time (US &amp; Canada)";
+$lang['time_-6']  = "(GMT - 6:00 hours) Central Time (US &amp; Canada), Mexico City";
+$lang['time_-5']  = "(GMT - 5:00 hours) Eastern Time (US &amp; Canada), Bogota, Lima, Quito";
+$lang['time_-4']  = "(GMT - 4:00 hours) Atlantic Time (Canada), Caracas, La Paz";
+$lang['time_-3.5'] = "(GMT - 3:30 hours) Newfoundland";
+$lang['time_-3']   = "(GMT - 3:00 hours) Brazil, Buenos Aires, Georgetown, Falkland Is.";
+$lang['time_-2']   = "(GMT - 2:00 hours) Mid-Atlantic, Ascention Is., St Helena";
+$lang['time_-1']   = "(GMT - 1:00 hours) Azores, Cape Verde Islands";
+$lang['time_0']    = "(GMT) Casablanca, Dublin, Edinburgh, London, Lisbon, Monrovia";
+$lang['time_1']    = "(GMT + 1:00 hours) Berlin, Brussels, Copenhagen, Madrid, Paris, Rome";
+$lang['time_2']    = "(GMT + 2:00 hours) Kaliningrad, South Africa, Warsaw";
+$lang['time_3']    = "(GMT + 3:00 hours) Baghdad, Riyadh, Moscow, Nairobi";
+$lang['time_3.5']  = "(GMT + 3:30 hours) Tehran";
+$lang['time_4']    = "(GMT + 4:00 hours) Adu Dhabi, Baku, Muscat, Tbilisi";
+$lang['time_4.5']  = "(GMT + 4:30 hours) Kabul";
+$lang['time_5']    = "(GMT + 5:00 hours) Ekaterinburg, Islamabad, Karachi, Tashkent";
+$lang['time_5.5']  = "(GMT + 5:30 hours) Bombay, Calcutta, Madras, New Delhi";
+$lang['time_6']    = "(GMT + 6:00 hours) Almaty, Colomba, Dhakra";
+$lang['time_7']    = "(GMT + 7:00 hours) Bangkok, Hanoi, Jakarta";
+$lang['time_8']    = "(GMT + 8:00 hours) Beijing, Hong Kong, Perth, Singapore, Taipei";
+$lang['time_9']    = "(GMT + 9:00 hours) Osaka, Sapporo, Seoul, Tokyo, Yakutsk";
+$lang['time_9.5']  = "(GMT + 9:30 hours) Adelaide, Darwin";
+$lang['time_10']   = "(GMT + 10:00 hours) Melbourne, Papua New Guinea, Sydney, Vladivostok";
+$lang['time_11']   = "(GMT + 11:00 hours) Magadan, New Caledonia, Solomon Islands";
+$lang['time_12']   = "(GMT + 12:00 hours) Auckland, Wellington, Fiji, Marshall Island";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
