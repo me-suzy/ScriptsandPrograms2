34cb63930c03c1e1425c6baa15fec76298587a3b
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:14 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_user-list.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_user-list.php
@@ -0,0 +1,23 @@
+<?php
+
+##################################################
+
+# /admin/user-list.php
+
+##################################################
+
+define( "USLIST_EDIT_HELP", "Feu clic per editar aquest usuari" );
+define( "USLIST_BTN_NEW", "Nou compte" );
+
+define( "USLIST_US_HEADER", "USUARIS" );
+define( "USLIST_US_HELP", "Els usuaris tenen accés a tota la música, als seus comptes, a les seves llistes i a la comunitat." );
+
+define( "USLIST_ED_HEADER", "EDITORS" );
+define( "USLIST_ED_HELP", "Els editors tenen el mateix nivell d'accés que els usuaris, però a més poden editar les pistes i afegir o editar intèrprets, àlbums i gèneres." );
+
+define( "USLIST_AD_HEADER", "ADMINISTRADORS" );
+define( "USLIST_AD_HELP", "Els administradors tenen accés a totes les opcions de l'aplicació. Actualment, sou administrador." );
+
+##################################################
+
+?>
