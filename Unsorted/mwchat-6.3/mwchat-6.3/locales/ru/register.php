0d34af324764b2becb0bf94619e1a406941d8935
me-suzy <me.suzana@gmail.com>
2025-08-13 06:35:07 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/register.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/register.php
@@ -0,0 +1,25 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[REGISTER_Title] = "úÁÒÅÇÉÓÔÒÉÒÕÊÔÅ ×ÁÛÅ ÉÍÑ!";
+
+$LOCALE[REGISTER_Info] = "ðÏÖÁÌÕÊÓÔÁ, ÐÒÅÄÏÓÔÁ×ØÔÅ ÓÌÅÄÕÀÝÕÀ ÉÎÆÏÒÍÁÃÉÀ, ÞÔÏÂÙ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÔØ ×ÁÛÅ ÉÍÑ.";
+
+$LOCALE[REGISTER_Is_Registered] = "ÔÅÐÅÒØ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎ.";
+
+$LOCALE[REGISTER_Complete] = "ðÏÚÄÒÁ×ÌÑÅÍ! ÷Ù ÕÓÐÅÛÎÏ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÌÉ Ó×ÏÅ ÉÍÑ.";
+
+$LOCALE[REGISTER_Next] = "ôÅÐÅÒØ ×Ù ÍÏÖÅÔÅ ÕÓÔÁÎÏ×ÉÔØ Ó×ÏÉ ÐÁÒÁÍÅÔÒÙ ÄÌÑ ÞÁÔÁ ÉÚ ÓÅËÃÉÉ ÓÓÙÌÏË ÇÌÁ×ÎÏÇÏ ÏËÎÁ.";
+
+$LOCALE[REGISTER_Button] = "úÁÒÅÇÉÓÔÒÉÒÏ×ÁÔØ ÓÅÊÞÁÓ";
+
+?>
