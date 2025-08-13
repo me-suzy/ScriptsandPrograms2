eb94191fcfb935cf5c0d732c5291c285d530d3f5
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:38 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.admin.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.admin.php
@@ -0,0 +1,862 @@
+<?php
+$L = array(
+
+
+"word_separator" =>
+"Word Separator for URL Titles",
+
+"dash" =>
+"Dash",
+
+"underscore" =>
+"Underscore",
+
+"site_name" =>
+"Nombre del sitio",
+
+"system_admin" =>
+"Administración del sistema",
+
+"system_preferences" =>
+"Preferencias del Sistema",
+
+"is_system_on" =>
+"¿Está el sistema en marcha?",
+
+"is_system_on_explanation" =>
+"Si el sistema está apagado, sólo los super administradores pueden ver el sitio",
+
+"system_off_msg" =>
+"Mensaje de sistema apagado",
+
+"offline_template" =>
+"Plantilla de sistema fuera de línea",
+
+"offline_template_desc" =>
+"Esta plantilla contiene la página que se muestra cuando el sitio está fuera de línea.",
+
+"template_updated" =>
+"Plantilla actualizada",
+
+"preference_information" =>
+"Guía de preferencia",
+
+"preference" =>
+"Preferencia",
+
+"value" =>
+"Valor",
+
+"general_cfg" =>
+"Configuración general",
+
+"enable_db_caching" =>
+"Activar caché de peticiones SQL",
+
+"db_cache_refresh" =>
+"Podar la caché de peticiones",
+
+"db_cache_refresh_exp" =>
+"No ha sido actualizado el número de DIAS para mantener los ficheros antiguos de la caché.  Para un sitio normal, 30 días es una buena cifra.",
+
+"member_cfg" =>
+"Preferencias de la membresía",
+
+"profile_templates" =>
+"Plantillas del perfil de miembros",
+
+"allow_member_registration" =>
+"¿Permitir registro de nuevos miembros?",
+
+"req_mbr_activation" =>
+"¿Se requiere activación de la cuenta de nuevos miembros?",
+
+"no_activation" =>
+"No se requiere activación",
+
+"email_activation" =>
+"Auto activación vía correo electrónico",
+
+"manual_activation" =>
+"Activación manual por el administrator",
+
+"require_terms_of_service" =>
+"Requiere aceptar los términos del servicio",
+
+"member_theme" =>
+"Tema del perfil de miembros",
+
+"member_theme_exp" =>
+"Determina qué tema se usará en las diversas páginas de membresía: acceso, registro, perfil, etc.",
+
+"member_images" =>
+"Camino a las imágenes de miembros",
+
+"member_images_exp" =>
+"Este es el camino al directorio que contiene las imágenes usadas en las páginas de perfil de miembro.",
+
+"require_terms_of_service_exp" =>
+"Si se pone &quot;Si&quot; se fuerza a marcar  &quot;aceptar términos&quot; durante el registro",
+
+"new_member_notification" =>
+"¿Notificar los nuevos registros a los administradores?",
+
+"mbr_notification_emails" =>
+"Dirección de email para la notificación",
+
+"separate_emails" =>
+"Separar múltiples direcciones con comas",
+
+"email_console_timelock" =>
+"Temporizador de la consola de email",
+
+"email_console_timelock_exp" =>
+"El número de minutos que deben de transcurrir antes de permitir a un miembro enviar otro email. Nota: Sólo se aplica a la consola de email en las páginas de perfil de miembro.",
+
+"log_email_console_msgs" =>
+"Consola de registro de mensajes",
+
+"log_email_console_msgs_exp" =>
+"Permite llevar un registro de todos los mensajes enviados desde la consola de email en las páginas de perfil de miembro",
+
+"default_member_group" =>
+"Grupo por defecto al que asignar los nuevos miembros",
+
+"group_assignment_defaults_to_two" =>
+"Si se requiere activación de la cuenta, los miembros se colocarán en ese grupo una vez activados",
+
+"view_email_logs" =>
+"Registros de la consola de email",
+
+"user_session_type" =>
+"Tipo de sesión de usuario",
+
+"admin_session_type" =>
+"Tipo de panel de control de la sesión",
+
+"security_cfg" =>
+"Fijaciones de seguridad y sesión",
+
+"un_min_len" =>
+"Longitud mínima de nombre de usuario (ID)",
+
+"pw_min_len" =>
+"Longitud mínima de contraseña",
+
+"cs_session" =>
+"Cookies e identificador de sesión",
+
+"c_session" =>
+"Sólo Cookies",
+
+"s_session" =>
+"Sólo identificador de sesión",
+
+"secure_forms" =>
+"¿Procesar los datos en forma segura?",
+
+"deny_duplicate_data" =>
+"¿Denegar datos duplicados?",
+
+"deny_duplicate_data_explanation" =>
+"Esta opción previene que se reciban los datos enviados por los usuarios (comentarios, etc.) en caso de que sean un duplicado exacto de otros ya existentes.",
+
+"secure_forms_explanation" =>
+"Evita envío automático de correo no deseado (spam) y envíos múltiples inadvertidos.",
+
+"allow_multi_logins" =>
+"¿Permitir conexiones múltiples a una sola cuenta?",
+
+"allow_multi_logins_explanation" =>
+"Determina si más de una persona puede acceder simultáneamente al sistema usando una sola cuenta. Nota: Si su tipo de sesión está fijada a &quot;Sólo Cookies&quot;, esta opción no funcionará.",
+
+"password_lockout" =>
+"¿Activar bloqueo de contraseña?",
+
+"password_lockout_explanation" =>
+"Si se activa, sólo se permiten cuatro intentos de acceso en el intervalo de tiempo que se especifica a continuación. Se trata de un protección contra personas que intenten acceder de forma no legítima probando contraseñas diversas.",
+
+"password_lockout_interval" =>
+"Intervalo de tiempo para el bloqueo",
+
+"login_interval_explanation" =>
+"Expresado en minutos, se pueden usar fracciones decimales como 1.5, por ejemplo",
+
+"require_ip_for_login" =>
+"¿Se requiere una dirección IP y agente de usuario para conectarse?",
+
+"require_ip_explanation" =>
+"Evita que los usuarios se conecten si su navegador no informa la dirección IP y datos de agente de usuario. Ello previene accesos ilegítimos usando conexiones directas a los puertos en lugar de mediante un navegador.",
+
+"allow_multi_emails" =>
+"¿Permitir cuentas múltiples que usen la misma dirección de correo electrónico?",
+
+"allow_username_change" =>
+"¿Se permite a los miembros modificar su nombre de usuario (ID)?",
+
+"require_secure_passwords" =>
+"¿Se requieren contraseñas seguras?",
+
+"secure_passwords_explanation" =>
+"Forzar a los usuarios a usar contraseñas que tengan al menos una mayúscula, una minúscula y un dígito",
+
+"allow_dictionary_pw" =>
+"¿Permitir contraseñas con palabras que estén en el diccionario?",
+
+"real_word_explanation" =>
+"Forzar a los usuarios a no utilizar contraseñas que contengan nombres y palabras que se puedan hallar en un diccionario",
+
+"dictionary_note" =>
+"Nota: Para usar esta opción, es preciso instalar un fichero con el diccionario. Consulta el manual.",
+
+"name_of_dictionary_file" =>
+"Nombre del fichero de diccionario",
+
+"dictionary_explanation" =>
+"Nombre del fichero que contiene la lista de palabras no permitidas",
+
+"image_path" =>
+"Camino al directorio de imágenes",
+
+"cp_url" =>
+"URL de tu página índice del Panel de Control",
+
+"with_trailing_slash" =>
+"Con barra (/) previa",
+
+"site_url" =>
+"URL del directorio raiz de tu sitio",
+
+"url_explanation" =>
+"Se trata del directorio que contiene el fichero índice de tu sitio web",
+
+"doc_url" =>
+"URL del directorio de documentación",
+
+"doc_url_explanation" =>
+"Sólo directorio raiz, con barra (/) previa",
+
+"site_index" =>
+"Nombre de la página inicial del sistio",
+
+"system_path" =>
+"Camino absoluto a la carpeta %x",
+
+"force_query_string" =>
+"Forzar cadenas de peticiones de  URL",
+
+"safe_mode" =>
+"¿Ejecuta tu servidor PHP in modo seguro?",
+
+"force_query_string_explanation" =>
+"Es un mecanismo de seguridad para servidores que no soportan la variable PATH_INFO.",
+
+"debug" =>
+"Preferencia de depuración (Debug)",
+
+"debug_explanation" =>
+"Activa la visualización de mensajes de error, útiles durante el desarrollo del sitio",
+
+"show_queries" =>
+"¿Mostrar peticiones SQL?",
+
+"show_queries_explanation" =>
+"Si se activa, los Super Administradores verán todas las peticiones SQL al final (abajo) de la ventana del navegador. Util para depurar.",
+
+"debug_zero" =>
+"0: No se han generado mensajes de error PHP/SQL",
+
+"debug_one" =>
+"1: Los mensajes de error PHP/SQL se muestran sólo a los súper administradores",
+
+"debug_two" =>
+"2: Los mensajes de error PHP/SQL se muestran a cualquiera - NO ES SEGURO",
+
+"deft_lang" =>
+"Lenguaje por defecto",
+
+"xml_lang" =>
+"Lenguaje XML por defecto",
+
+"used_in_meta_tags" =>
+"Usado en meta tags del Panel de Control",
+
+"charset" =>
+"Conjunto de caracteres por defecto",
+
+"gzip_output" =>
+"¿Activar salida GZIP?",
+
+"gzip_output_explanation" =>
+"Cuando está activado, tu sitio se mostrará en formato comprimido para una descarga más rápida de las páginas",
+
+"send_headers" =>
+"¿Generar cabeceras de página HTTP?",
+
+"redirect_method" =>
+"Método de redirección",
+
+"location_method" =>
+"Ubicación (más rápido)",
+
+"refresh_method" =>
+"Refresco (servidores Windows)",
+
+"localization_cfg" =>
+"Configuración local",
+
+"time_format" =>
+"Formato de hora",
+
+"united_states" =>
+"Estados Unidos",
+
+"european" =>
+"Europeo",
+
+"server_timezone" =>
+"Uso horario del servidor",
+
+"server_offset" =>
+"Compensación del servidor (en minutos)",
+
+"server_offset_explain" =>
+"Usa el signo menos para substraer minutos: -15",
+
+"daylight_savings" =>
+"Horario de verano",
+
+"cookie_cfg" =>
+"Fijaciones de Cookies",
+
+"cookie_domain" =>
+"Dominio de Cookie",
+
+"cookie_domain_explanation" =>
+"Usar .tudominio.com para cookies de todo el sitio",
+
+"cookie_prefix" =>
+"Prefijo de Cookie",
+
+"cookie_prefix_explain" =>
+"Usar sólo si estás ejecutando mútiples instalaciones de éste programa",
+
+"cookie_path" =>
+"Camino de Cookies",
+
+"cookie_path_explain" =>
+"Usar sólo si se requiere un camino específico para los  cookies",
+
+"image_cfg" =>
+"Preferencias de imagen",
+
+"enable_image_resizing" =>
+"Activar redimensionamiento de imagen",
+
+"enable_image_resizing_exp" =>
+"Cuando está activado, podrás crear miniaturas cuando subas imágenes para colocar en tus artículos.",
+
+"image_resize_protocol" =>
+"Protocolo de redimensionamiento de imágenes",
+
+"image_resize_protocol_exp" =>
+"Verifica con tu proveedor que el servidor soporte los protocolos seleccionados.",
+
+"image_library_path" =>
+"Camino de la librería de imágenes",
+
+"image_library_path_exp" =>
+"Si seleccionas ImageMagick o NetPBM debes de especificar el camino de la libería en el servidor.",
+
+"gd" =>
+"GD",
+
+"gd2" =>
+"GD 2",
+
+"netpbm" =>
+"NetPBM",
+
+"imagemagick" =>
+"ImageMagik",
+
+"thumbnail_prefix" =>
+"Sufijo de la miniatura de imagen (thumb, mini, etc.)",
+
+"thumbnail_prefix_exp" =>
+"Este sufijo se añade a todas las miniaturas autogeneradas. Por ejemplo: foto_thumb.jpg, foto_mini.jpg, etc.",
+
+"email_cfg" =>
+"Configuración de email",
+
+"mail_protocol" =>
+"Protocolo de email",
+
+"smtp_server" =>
+"Dirección del servidor SMTP",
+
+"smtp_username" =>
+"Usuario SMTP",
+
+"smtp_password" =>
+"Contraseña SMTP",
+
+"only_if_smpte_chosen" =>
+"Usa ésto sólo si optas por SMTP",
+
+"email_batchmode" =>
+"¿Usar modo por lotes?",
+
+"batchmode_explanation" =>
+"El modo por lotes divide los envíos de email de gran volumen en pequeños grupos (lotes) que se envían a intervalos. Se recomienda si tu sitio está alojado en una cuenta compartida.",
+
+"email_batch_size" =>
+"Número de mensajes por lote",
+
+"batch_size_explanation" =>
+"Para un servidor normal, 300 es una buena cantidad",
+
+"webmaster_email" =>
+"Dirección de remitente en emails autogenerados",
+
+"return_email_explanation" =>
+"Si lo dejas en blanco, muchos servidores considerarán estos mensajes como correo no deseado (spam)",
+
+"php_mail" =>
+"PHP Mail",
+
+"sendmail" =>
+"Enviar email",
+
+"smtp" =>
+"SMTP",
+
+"plain_text" =>
+"Texto llano",
+
+"html" =>
+"HTML",
+
+"mail_format" =>
+"Formato de correo por defecto",
+
+"word_wrap" =>
+"¿Activar pase automático de palabras  a la línea siguiente por defecto?",
+
+"cp_theme" =>
+"Tema por defecto del Panel de Control",
+
+"template_cfg" =>
+"Preferencias de plantilla",
+
+"save_tmpl_revisions" =>
+"Por defecto, guardar las revisiones de las plantillas",
+
+"censoring_cfg" =>
+"Censura de palabras",
+
+"enable_censoring" =>
+"¿Activar la censura de palabras?",
+
+"censored_words" =>
+"Palabras censuradas",
+
+"censored_explanation" =>
+"Coloca cada palabra en una línea separada",
+
+"emoticon_cfg" =>
+"Preferencias de emoticonos",
+
+"enable_emoticons" =>
+"¿Mostar emoticonos?",
+
+"emoticon_path" =>
+"URL del directorio que contiene los emoticonos",
+
+"referrer_cfg" =>
+"Preferencias del  Referrer",
+
+"log_referrers" =>
+"¿Activar seguimiento del Referrer?",
+
+"log_referrers_explanation" =>
+"Cuando está activado, se ejecutará una petición adicional por cada página a la base de datos",
+
+"weblog_administration" =>
+"Administración del Weblog",
+
+"weblog_management" =>
+"Gestión del Weblog",
+
+"field_management" =>
+"Campos personalizados del Weblog",
+
+"file_upload_prefs" =>
+"Preferencias de subida de ficheros",
+
+"categories" =>
+"Gestión de categorías",
+
+"default_ping_servers" =>
+"Servidores Ping por defecto",
+
+"status_management" =>
+"Estados personalizados de entrada",
+
+"edit_preferences" =>
+"Modificar preferencias",
+
+"preferences_updated" =>
+"Preferencias actualizadas",
+
+"edit_groups" =>
+"Modificar grupos",
+
+"members_and_groups" =>
+"Miembros y grupos",
+
+"view_members" =>
+"Ver miembros",
+
+"member_validation" =>
+"Activar miembros pendientes",
+
+"register_member" =>
+"Registro de nuevo miembro",
+
+"member_search" =>
+"Búsqueda de miembro",
+
+"user_banning" =>
+"Expulsión de usuario",
+
+"custom_profile_fields" =>
+"Campos personalizados de perfil",
+
+"email_notification_template" =>
+"Plantillas de notificación de email",
+
+"member_groups" =>
+"Grupos de miembros",
+
+"utilities" =>
+"Utilidades",
+
+"view_log_files" =>
+"Ver ficheros de registro",
+
+"clear_caching" =>
+"Borra caché de datos",
+
+"page_caching" =>
+"Ficheros de caché de página (plantilla)",
+
+"tag_caching" =>
+"Ficheros de caché de etiquetas",
+
+"db_caching" =>
+"Ficheros de caché de bases de datos",
+
+"all_caching" =>
+"Todos lso ficheros de caché",
+
+"cache_deleted" =>
+"Se han borrado los ficheros de caché",
+
+"php_info" =>
+"Información PHP",
+
+"sql_manager" =>
+"Gestor SQL",
+
+"sql_info" =>
+"Información SQL",
+
+"sql_utilities" =>
+"Utilidades SQL",
+
+"database_type" =>
+"Tipo de base de datos",
+
+"sql_version" =>
+"Versión de base de datos",
+
+"database_size" =>
+"Tamaño de base de datos",
+
+"database_uptime" =>
+"Tiempo que lleva la base de datos en marcha",
+
+"total_queries" =>
+"Total de peticiones al servidor desde la puesta en marcha",
+
+"sql_status" =>
+"Información de estado",
+
+"sql_system_vars" =>
+"Variables del Sistema",
+
+"sql_processlist" =>
+"Lista de procesos",
+
+"sql_query" =>
+"Formulario de petición a la base de datos",
+
+"query_result" =>
+"Resultado de la petición",
+
+"browse" =>
+"Mirar",
+
+"tables" =>
+"tablas",
+
+"table_name" =>
+"Nombre de tabla",
+
+"records" =>
+"Registro de la base de datos",
+
+"size" =>
+"Tamaño",
+
+"type" =>
+"Tipo",
+
+"analize" =>
+"Analizar tablas",
+
+"optimize" =>
+"Optimizar tablas SQL",
+
+"repair" =>
+"Reparar tablas SQL",
+
+"optimize_table" =>
+"Optimizar las tablas seleccionadas",
+
+"repair_table" =>
+"Reparar las tablas seleccionadas",
+
+"view_table_sql" =>
+"Ver la estructura y datos SQL",
+
+"backup_tables_file" =>
+"Copia de seguridad de tablas seleccionadas -Fichero de texto",
+
+"backup_tables_zip" =>
+"Copia de seguridad de tablas seleccionadas -Fichero ZIP",
+
+"backup_tables_gzip" =>
+"Copia de seguridad de tablas seleccionadas -Fichero GZIP",
+
+"select_all" =>
+"Seleccionar todo",
+
+"no_buttons_selected" =>
+"Debes de seleccionar las tablas para proceder con esta acción",
+
+"unsupported_compression" =>
+"Tu instalación PHP no soporta este método de compresión",
+
+"backup_info" =>
+"Usa este formulario para hacer la copia de seguridad de tu base de datos",
+
+"save_as_file" =>
+"Guardar la copia de seguridad SQL como un fichero",
+
+"view_in_browser" =>
+"Ver la copia de seguridad SQL en un navegador",
+
+"sql_query_instructions" =>
+"Usa este formulario para enviar una petición SQL",
+
+"file_type" =>
+"Tipo de fichero: ",
+
+"zip" =>
+"Zip",
+
+"gzip" =>
+"Gzip",
+
+"advanced_users_only" =>
+"Sólo usuarios avanzados",
+
+"recount_stats" =>
+"Recontar las estadísticas",
+
+"preference_updated" =>
+"Preferencia actualizada",
+
+"click_to_recount" =>
+"Click para recontar filas de  %x a %y",
+
+"items_remaining" =>
+"Registros restantes:",
+
+"recount_completed" =>
+"Recuento completado",
+
+"return_to_recount_overview" =>
+"Volver a la página principal de recuentos",
+
+"recounting" =>
+"Recontando",
+
+"recount_info" =>
+"Los enlaces que siguen te permiten actualizar diversas estadísticas, como cuantas entradas ha enviado cada miembro.",
+
+"source" =>
+"Fuente",
+
+"total_records" =>
+"Total registros:",
+
+"recalculate" =>
+"Recuento de estadísticas",
+
+"do_recount" =>
+"Ejecutar recuento",
+
+"set_recount_prefs" =>
+"Preferencias de recuento",
+
+"recount_instructions" =>
+"Número total de filas de base de datos procesadas por lote",
+
+"recount_instructions_cont" =>
+"Con el fin de prevenir un fuera de tiempo en el servidor, se recuentan las estadísticas por lotes. 1000 es una cifra segura para la mayoría de los servidores. Si estas usando un servidor de altas prestaciones o dedicado puedes incrementar la cifra.",
+
+"exp_members" =>
+"Miembros",
+
+"exp_weblog_titles" =>
+"Artículos del Weblog",
+
+"search_and_replace" =>
+"Buscar y reemplazar",
+
+"data_pruning" =>
+"Poda de datos",
+
+"sandr_instructions" =>
+"Estos formularios te permiten buscar texto específico y reemplazarlo por otro",
+
+"search_term" =>
+"Buscar este texto",
+
+"replace_term" =>
+"Y reemplazarlo por este otro",
+
+"replace_where" =>
+"¿En qué campo de la base de datos quieres que se produzca el reemplazo?",
+
+"search_replace_disclaimer" =>
+"Dependiendo de la sintaxis usada, esta función puede producir resultados no deseados. Consulta el manual.",
+
+"title" =>
+"Título",
+
+"weblog_entry_title" =>
+"Títulos de artículos del Weblog",
+
+"weblog_fields" =>
+"Campos del Weblog:",
+
+"templates" =>
+"Plantillas",
+
+"rows_replaced" =>
+"Número de registros de la base de datos en los que se ha producido el remplazo:",
+
+"view_database" =>
+"Gestionar tablas de la base de datos",
+
+"sql_backup" =>
+"Copia de seguridad de la base de datos",
+
+"sql_no_result" =>
+"No han habido resultados para esta petición.",
+
+"sql_not_allowed" =>
+"Perdón, ese tipo de petición no está permitida.",
+
+"site_statistics" =>
+"Estadísticas del sitio",
+
+"translation_tool" =>
+"Utilidad de traducción",
+
+"translation_dir_unwritable" =>
+"Precaución: Tu directorio de traducciones no tiene permisos de escrtiura",
+
+"please_set_permissions" =>
+"Pon los permisos (chomod) a 666 o 777 en el siguiente directorio:",
+
+"choose_translation_file" =>
+"Elije un fichero para traducir",
+
+"core_language_files" =>
+"Ficheros de lenguaje del núcleo:",
+
+"module_language_files" =>
+"Ficheros de lenguaje de módulos:",
+
+"file_saved" =>
+"El fichero se ha guardado",
+
+"default_html_buttons" =>
+"Botones HTML por defecto",
+
+"import_from_pm" =>
+"Utilidad de importación desde pMachine",
+
+"import_from_mt" =>
+"Utilidad de importación desde Movable Type",
+
+"specialty_templates" =>
+"Plantillas especializadas",
+
+"user_messages_template" =>
+"Plantilla de mensaje de usuario",
+
+"plugin_manager" =>
+"Gestor de plugines",
+
+"installed_plugins" =>
+"Plugines isntalados",
+
+"no_plugins_exist" =>
+"No hay plugines instalados",
+
+"view_info" =>
+"Ver información",
+
+"plugin_information" =>
+"Información de plugin",
+
+"pi_name" =>
+"Nombre",
+
+"pi_author" =>
+"Autor",
+
+"pi_version" =>
+"Versión",
+
+"pi_author_url" =>
+"URL del autor",
+
+"pi_description" =>
+"Descripción",
+
+"pi_usage" =>
+"Uso",
+
+"no_additional_info" =>
+"No hay información adicional para este plugin",
+
+''=>''
+);
+?>
