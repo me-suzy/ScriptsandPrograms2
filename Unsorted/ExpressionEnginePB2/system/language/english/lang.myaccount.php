2094d8e705dc351e59214e5f76697a917a5d655f
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:18 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/english/lang.myaccount.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/english/lang.myaccount.php
@@ -0,0 +1,463 @@
+<?php
+
+$L = array(
+
+"preferences_updated" =>
+"Preferences Updated",
+
+"homepage_preferences" =>
+"Home Page Preferences",
+
+"set_display_order" =>
+"Set Display Order",
+
+"please_update_order" =>
+"Please set the item display order",
+
+"left_column" =>
+"Left Column",
+
+"right_column" =>
+"Right Column",
+
+"do_not_show" =>
+"Do Not Display",
+
+"recent_entries" =>
+"Your ten most recent weblog entries",
+
+"recent_comments" =>
+"The ten most recent comments",
+
+"recent_members" =>
+"The ten most recent member registrations",
+
+"new_release_indicator" =>
+"New software release indicator",
+
+"site_statistics" =>
+"Site statistics",
+
+"member_search_form" =>
+"Member search form",
+
+"validating_members" =>
+"Members awaiting activation",
+
+"php_sql_versions" =>
+"PHP/SQL versions",
+
+"system_offline_indicator" =>
+"System offline indicator",
+
+"personal_settings" =>
+"Personal Settings",
+
+"weblog_settings" =>
+"Weblog Settings",
+
+"control_panel_settings" =>
+"CP Customization",
+
+"administrative_settings" =>
+"Admin Settings",
+
+"current_member" =>
+"Member:",
+
+"member_stats" =>
+"Account Statistics For:",
+
+"join_date" =>
+"Join Date",
+
+"last_visit" =>
+"Last Visit",
+
+"total_entries" =>
+"Total Weblog Entries",
+
+"last_entry_date" =>
+"Date of Most Recent Entry",
+
+"total_comments" =>
+"Total Comments",
+
+"last_comment_date" =>
+"Date of Most Recent Comment",
+
+"notepad" =>
+"Notepad",
+
+"can_not_view_superadmin_profile" =>
+"You can not view the profile of a Super Admin",
+
+"only_self_notpad_access" =>
+"You are not allowed to access another member\'s notepad",
+
+"notepad_size" =>
+"Notepad Size",
+
+"notepad_instructions" =>
+"Use this form to store personal notes and information",
+
+"notepad_updated" =>
+"Notepad Updated",
+
+"localization_settings" =>
+"Localization Settings",
+
+"localization_updated" =>
+"Localization Updated",
+
+"daylight_savings_time" =>
+"Daylight Saving Time",
+
+"timezone" =>
+"Select Your Time Zone",
+
+"language_choice" =>
+"Select Your Language",
+
+"username_and_password" =>
+"Username and Password",
+
+"extras" =>
+"Extras",
+
+"edit_profile" =>
+"Edit Your Profile",
+
+"profile_form" =>
+"Profile Form",
+
+"base_profile" =>
+"Base Profile",
+
+"extended_profile" =>
+"Extended Profile",
+
+"localization" =>
+"Localization Settings",
+
+"time_format" => 
+"Time Formatting",
+
+"united_states" => 
+"United States",
+
+"european" => 
+"European",
+
+"your_ping_servers" =>
+"Ping Servers",
+
+"your_html_buttons" => 
+"HTML Formatting Buttons",
+
+"change_password" =>
+"Change Password",
+
+"email_settings" =>
+"Email Settings",
+
+"subscriptions" =>
+"Subscriptions",
+
+"edit_subscriptions" =>
+"View Your Subscriptions",
+
+"customize_homepage" =>
+"Customize Control Panel Homepage",
+
+"customize_cp" =>
+"Customize Control Panel",
+
+"cp_homepage" =>
+"Control Panel Homepage",
+
+"cp_theme" =>
+"Control Panel Theme",
+
+"choose_theme" =>
+"Choose Your Control Panel Theme",
+
+"administrative_options" =>
+"Administrative Options",
+
+"administrative_options_updated" =>
+"Administrative Options Updated",
+
+"screen_name" =>
+"Screen Name",
+
+"email" =>
+"Email Address",
+
+"url" =>
+"URL",
+
+"bio" =>
+"Bio",
+
+"birthday" =>
+"Birthday",
+
+"year" =>
+"Year",
+
+"month" =>
+"Month",
+
+"day" =>
+"Day",
+
+"accept_admin_email" =>
+"Accept email from site administrators",
+
+"accept_user_email" =>
+"Accept email from other members of this site",
+
+"notify_by_default" =>
+"Enable email notifications by default when you post messages",
+
+"location" =>
+"Location",
+
+"occupation" =>
+"Occupation",
+
+"interests" =>
+"Interests",
+
+"icq" =>
+"ICQ",
+
+"aol_im" =>
+"AOL IM",
+
+"yahoo_im" =>
+"Yahoo IM",
+
+"msn_im" =>
+"MSN IM",
+
+"user_ip_address" =>
+"IP Address:",
+
+"member_group_assignment" =>
+"Member Group Assignment",
+
+"member_group_warning" =>
+"Be very careful assigning this",
+
+"include_in_multiauthor_list" =>
+"Include user in PUBLISH page multi-author list?",
+
+"profile_updated" =>
+"Profile Updated",
+
+"super_admin_demotion_alert" =>
+"As a Super Admin, you are not allowed to change your member group assignment",
+
+"username" =>
+"Username",
+
+"username_change_not_allowed" =>
+"The administrators do not allow username changes",
+
+"password" =>
+"Password",
+
+"password_change" =>
+"Password Change Form",
+
+"new_password" =>
+"New Password",
+
+"password_confirm" =>
+"Password Confirm",
+
+"new_password_confirm" =>
+"New Password Confirm",
+
+"existing_password" =>
+"Your Existing Password",
+
+"invalid_email_address" =>
+"The email you submitted is not valid",
+
+"password_required" =>
+"You must submit your current password to update this page",
+
+"password_required_for_email" =>
+"In order to change your email address you must submit your current password",
+
+"leave_blank" =>
+"Leave blank if you do not wish to change it",
+
+"missing_username" =>
+"You must submit a username",
+
+"invalid_characters_in_username" =>
+"Your username contains illegal characters.",
+
+"missing_password" =>
+"You must submit a password",
+
+"missing_current_password" =>
+"In order to make changes you must submit your existing password",
+
+"password_in_dictionary" =>
+"You are not allowed to use a word found in a dictionary as a password",
+
+"not_secure_password" =>
+"Your password must contain at least one uppercase character, one lowercase character and one number",
+
+"invalid_password" =>
+"The password you submitted was not correct",
+
+"missing_email" =>
+"You must submit an email address",
+
+"banned_email" =>
+"You email address you submitted is banned",
+
+"missmatched_passwords" =>
+"The password and password confirmation do not match",
+
+"username_too_short" =>
+"The username you submitted must be at least %x characters long",
+
+"password_too_short" =>
+"The password you submitted must be at least %x characters long",
+
+"username_password_too_long" =>
+"Usernames and passwords can not be over 32 characters in length",
+
+"username_taken" =>
+"The username you chose is not available",
+
+"screen_name_taken" =>
+"The screen name you chose is not available",
+
+"email_taken" =>
+"The email you chose is not available",
+
+"password_based_on_username" =>
+"The password can not be based on the username",
+
+"username_changed" =>
+"Username was changed to:",
+
+"screen_name_changed" =>
+"Screen name was changed to:",
+
+"email_changed" =>
+"Email was changed to:",
+
+"password_change_requires_login" =>
+"Note:  If you change your password you will have to log back in",
+
+"settings_updated" =>
+"Settings Have Been Updated",
+
+"new_member_added" =>
+"Member profile created:",
+
+"password_change_warning" =>
+"Note: You have changed your password so you will have to log back in.",
+
+"ping_servers" =>
+"Ping Servers",
+
+"pingservers_updated" =>
+"Ping Servers Updated",
+
+"html_buttons" =>
+"HTML Formatting Buttons",
+
+"html_buttons_updated" =>
+"HTML Buttons Updated",
+
+"quick_links" =>
+"Quick Links",
+
+"only_self_qucklink_access" =>
+"You are not allowed to access another member\'s Quick Links",
+
+"quicklinks_updated" =>
+"Quick Links Have Been Updated",
+
+"quick_link_description" =>
+"Use this form to add your personal links to the top toolbar.",
+
+"quick_link_description_more" =>
+"These links will be visible only to you.",
+
+"quicklinks_delete_instructions" =>
+"To delete an item submit the form with the title blank",
+
+"link_title" =>
+"Link Title",
+
+"link_url" =>
+"Link URL",
+
+"link_order" =>
+"Order",
+
+"no_extended_fields" =>
+"There are currently no extended member profile fields",
+
+"bookmarklet" =>
+"Bookmarklet",
+
+"bookmarklet_created" =>
+"Bookmarklet Created",
+
+"bookmarklet_info" =>
+"A bookmarklet allows you to add entries to your weblog without logging into the full control panel.",
+
+"consult_manual" =>
+"Please consult the user guide for more info.",
+
+"bookmarklet_name" =>
+"Name Your Bookmarklet",
+
+"single_word_no_spaces" => 
+"single word, no spaces",
+
+"weblog_name" =>
+"Choose the weblog you want your entries posted to",
+
+"no_blogs_assigned_to_user" =>
+"There are no weblogs assigned to your account.",
+
+"select_field" =>
+"Select the default field you want the entry posted to",
+
+"bookmarklet_next_step" =>
+"Next Step",
+
+"create_the_bookmarklet" =>
+"Create the Bookmarklet",
+
+"no_fields_assigned_to_blog" =>
+"The weblog you chose does not have a field group assigned to it",
+
+"safari_users" =>
+"Click if you use Safari as your browser",
+
+"bookmarklet_instructions" =>
+"Save this link to your favorites menu:",
+
+"subscriptions" =>
+"Subscriptions",
+
+
+
+// END
+''=>''
+);
+?>
