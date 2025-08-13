f0d5b8d29303565f5cae373d2b4ef8fbbd49b22a
me-suzy <me.suzana@gmail.com>
2025-08-13 04:46:33 +0300

    Add Unsorted/GB CJ 1.5.6/skin.php

---
+++ Unsorted/GB CJ 1.5.6/skin.php
@@ -0,0 +1,17 @@
+<style type="text/css">
+body{background-color:#81C081;margin:0}
+body,td,th{font-family:Arial;color:#000000;font-size:9pt}
+input,select{font-size:9pt}
+tr{background-color:#FFFFCC}
+th{background-color:#99CC99}
+A{color:#000080}
+A:hover{color:#ff0000}
+table{border:0}
+#1,#3,#5,#7,#9,#11,#13,#15,#17,#19,#21{background-color:#FFFFEE}
+</style>
+<script for=r event=onmouseover>
+this.style.backgroundColor='#ccccff';
+</script>
+<script for=r event=onmouseout>
+this.style.backgroundColor='#ffffcc';
+</script>
