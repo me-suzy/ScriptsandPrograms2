0174322c6c53048070ab54cdf8b559c6a87500fe
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:21 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_tabfile-recursive.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_tabfile-recursive.php
@@ -0,0 +1,57 @@
+<?php
+
+##################################################
+
+# /admin/tabfile-recursive.php
+
+##################################################
+
+define( "TFREC_HEADER", "VERZEICHNISSE NACH AUDIO-DATEIEN DURCHSUCHEN" );
+define( "TFREC_HEADER_AUTO", "Automatisch" );
+define( "TFREC_HEADER_INTER", "Benutzergesteuert" );
+define( "TFREC_CAPTION_1", "" );
+define( "TFREC_CAPTION_2", "Bitte geben sie ein Unterverzeichniss zum durchsuchen an, oder klicken Sie auf Start um das gesammte Verzeichniss zu durchsuchen. Die zu durchsuchenden Verzeichnisse müssen innerhalb des in der Einstellungsdatei angegebenden Musikverzeichnisses ligen. Falls Sie ihr Musikverzeichniss zum ersten Mal durchsuchen, empfehlen wir die Suche auf Unterverzeichnisse zu beschränken." );
+define( "TFREC_OPTION_1", "Unterverzeichnisse einbeziehen?" );
+define( "TFREC_OPTION_2", "Bei Dateien ohne ID3-Tag Information aus Dateinamen beziehen?" );
+define( "TFREC_OPTION_3", "Protokoll?" );
+define( "TFREC_BTN", "Durchsuchen starten" );
+
+define( "TFREC_COLS_TR", "Titel" );
+define( "TFREC_COLS_AR", "Interpret" );
+define( "TFREC_COLS_AL", "Album" );
+define( "TFREC_COLS_GE", "Genre" );
+define( "TFREC_COLS_FS", "Grösse" );
+define( "TFREC_COLS_TI", "Dauer" );
+define( "TFREC_COLS_TN", "Titel Number" );
+define( "TFREC_COLS_TC", "Anzahl gehört" );
+define( "TFREC_COLS_YR", "Jahr" );
+define( "TFREC_COLS_DT", "Datum" );
+define( "TFREC_COLS_DA", "Datum hinzugefügt" );
+define( "TFREC_COLS_BR", "Bitrate" );
+define( "TFREC_COLS_SR", "Sample Rate" );
+define( "TFREC_COLS_VA", "Lautstärke Anpassung" );
+define( "TFREC_COLS_FK", "Typ" );
+define( "TFREC_COLS_CT", "Kommentare" );
+define( "TFREC_COLS_LC", "Ablageort" );
+
+define( "TFREC_ERROR_NOMUDIR_1", "Das angegebene Verzeichniss ist nicht innerhalb des in den Einstellungen angegebenen Musik-Verzeichnisses." );
+define( "TFREC_ERROR_NOMUDIR_2", "ist nicht das definierte Musik-Verzeichniss." );
+define( "TFREC_ERROR_NODIR", "Bitte geben Sie ein gültiges Verzeichniss zur Suche nach Audio-Dateien an." );
+define( "TFREC_ERROR_NOFILE", "Keine neue Datei zum einfügen gefunden." );
+
+define( "TFREC_VIEW", "Einzufügende Dateien anzeigen." );
+define( "TFREC_PROCEED", "Mit dem Einfügen der Dateien weitermachen." );
+
+define( "TFREC_SUCCESS_1", "Audio-Datei(en) wurden zum Einfügen gefunden." );
+define( "TFREC_SUCCESS_2", "Keine Fehler gefunden." );
+define( "TFREC_SUCCESS_3", "Audio-Dateien wurden zum Einfügen vorbereitet." );
+
+define( "TFREC_FORM_HELP_1", "Die folgenden" );
+define( "TFREC_FORM_HELP_2", "Datei(en) hatten kein gültiges ID3-Tag (oä)" );
+define( "TFREC_FORM_HELP_3", "Um diese Dateien einzufügen, müssen die erforderlichen Informationen manuell eingegeben werden." );
+define( "TFREC_FORM_HELP_4", "Es werden nicht alle Datien dargestellt, um den Anzeigeplatz zu begrenzen." );
+define( "TFREC_FORM_BTN", "Neue Importdatei erstellt" );
+
+##################################################
+
+?>
