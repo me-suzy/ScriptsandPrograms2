d0e31d4a2694e6a52253a891d32511782b11f103
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:08 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-search-adv.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-search-adv.php
@@ -0,0 +1,40 @@
+<?php
+
+##################################################
+
+# /search-adv.php
+
+##################################################
+
+define( "SEARCH_ADV_HEADER", "ERWEITERTE MUSIK SUCHE" );
+
+define( "SEARCH_ADV_FORM_COND", "Bedingung" );
+define( "SEARCH_ADV_FORM_COND_AND", "und" );
+define( "SEARCH_ADV_FORM_COND_OR", "oder" );
+define( "SEARCH_ADV_FORM_TR", "Titel Name" );
+define( "SEARCH_ADV_FORM_AR", "Interpreten Name" );
+define( "SEARCH_ADV_FORM_AL", "Album Name" );
+define( "SEARCH_ADV_FORM_GE", "Genre Name" );
+define( "SEARCH_ADV_FORM_TI", "Spieldauer" );
+define( "SEARCH_ADV_FORM_SECONDS", "Sekunden" );
+define( "SEARCH_ADV_FORM_TN", "Titel Nummer" );
+define( "SEARCH_ADV_FORM_BR", "Bit Rate" );
+define( "SEARCH_ADV_FORM_SR", "Sample Rate" );
+define( "SEARCH_ADV_FORM_FK", "Dateityp" );
+define( "SEARCH_ADV_FORM_FS", "Dateigrösse" );
+define( "SEARCH_ADV_FORM_FN", "Ablageort" );
+define( "SEARCH_ADV_FORM_CM", "Kommentare" );
+define( "SEARCH_ADV_FORM_LY", "Texte" );
+define( "SEARCH_ADV_FORM_LC", "Anzahl gehört" );
+
+define( "SEARCH_ADV_FORM_BTN_SEARCH", "Suchen" );
+define( "SEARCH_ADV_FORM_BTN_RESET", "Zurücksetzen" );
+
+define( "SEARCH_ADV_CLOSEWIN", "Fenster Schliessen" );
+define( "SEARCH_ADV_CLOSEWIN_HELP", "Schliesst dieses Fenster" );
+
+define( "SEARCH_ADV_ANY_VALUE", "Jeder Wert" );
+
+##################################################
+
+?>
