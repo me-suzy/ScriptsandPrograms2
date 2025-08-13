27b2348784789f1ead8d6fcecc5188025dc0f8ea
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:24 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.specialty_tmp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.specialty_tmp.php
@@ -0,0 +1,82 @@
+<?php
+$L = array(
+
+
+"email_title" =>
+"Titolo messaggio",
+
+"email_message" =>
+"Messaggio e-mail",
+
+"admin_notify_comment" =>
+"Notifica dei commenti all&#39;amministratore",
+
+"admin_notify_comment_desc" =>
+"Questo modello specifica il messaggio inviato all&#39;amministratore alla pubblicazione di un commento.",
+
+"admin_notify_trackback" =>
+"Notifica dei trackback all&#39;amministratore",
+
+"admin_notify_trackback_desc" =>
+"Questo modello specifica il messaggio inviato all&#39;amministratore alla ricezione di un trackback.",
+
+"admin_notify_reg" =>
+"Notifica delle nuove iscrizioni all&#39;amministratore",
+
+"admin_notify_reg_desc" =>
+"Questo modello specifica il messaggio inviato all&#39;amministratore all&#39;iscrizione di un nuovo utente.",
+
+"comment_notification" =>
+"Notifica dei commenti",
+
+"comment_notification_desc" =>
+"Questo modello specifica il messaggio inviato a un utente alla riposta ad un suo commento.",
+
+"forgot_password_instructions" =>
+"Istruzioni per password smarrita",
+
+"forgot_password_instructions_desc" =>
+"Questo modello specifica le istruzioni inviate a un utente che ha smarrito la propria password.",
+
+"mailinglist_activation_instructions" =>
+"Istruzioni per sottoscrizione mailing-list",
+
+"mailinglist_activation_instructions_desc" =>
+"Questo modello specifica le istruzioni per la sottoscrizione della mailing-list.",
+
+"mbr_activation_instructions" =>
+"Istruzioni per attivazione account",
+
+"mbr_activation_instructions_desc" =>
+"Questo modello specifica le istruzioni per attivare il proprio account a chi si registra sul sito.",
+
+"reset_password_notification" =>
+"Notifica per ripristino password",
+
+"reset_password_notification_desc" =>
+"Questo modello specifica le istruzioni per ripristinare la password.",
+
+"validated_member_notify" =>
+"Notifica per conferma account",
+
+"validated_member_notify_desc" =>
+"Questo modello specifica la notifica inviata agli utenti quando un amministratore conferma il loro account.",
+
+"use_this_template" =>
+"Abilita questo modello?",
+
+"use_this_template_exp" =>
+"Se selezioni &lt;b&gt;no&lt;/b&gt;, questo modello non verrà utilizzato. Il messaggio verrà invece generato dinamicamente nella lingua prescelta da ogni utente.",
+
+"user_messages_template_desc" =>
+"Lo scopo di questo modello è mostrare errori o altri avvisi agli utenti dopo l&#39;esecuzione di determinate operazioni, come log-in, invio di form, ecc.",
+
+"user_messages_template_warning" =>
+"Non cancellare nessuna delle cinque variabili richieste:",
+
+"available_variables" =>
+"Possono essere utilizzate le seguenti variabili:",
+
+''=>''
+);
+?>
