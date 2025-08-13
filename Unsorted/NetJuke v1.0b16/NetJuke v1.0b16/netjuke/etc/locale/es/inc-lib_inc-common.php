64ff3feaf353dabf6637e507d5459794d5719f5c
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:16 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-lib_inc-common.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-lib_inc-common.php
@@ -0,0 +1,75 @@
+<?php
+
+##################################################
+
+# /lib/inc-common.php
+
+##################################################
+
+define( "COMMON_ERR_DBCONN", "No se puede conectar a la base de datos con la informacin suministrada." );
+
+define( "COMMON_ACCESS_DENIED_1", "Acceso denegado. Ud. debe logearse como Administrador." );
+define( "COMMON_ACCESS_DENIED_2", "Acceso denegado. Por favor intente de nuevo." );
+
+define( "COMMON_PLISTSELECT_1", "Nueva lista" );
+
+define( "COMMON_ALPHABET_AR_HELP", "Click para listar a todos los artistas que empiezan con" );
+define( "COMMON_ALPHABET_AL_HELP", "Click para listar a todos los albumes que empiezan con" );
+
+define( "COMMON_CHECK_IMAGE_HELP", "Click para ver la imagen asociada" );
+
+define( "COMMON_NOTRACK", "No seleccionó alguna pista de la lista anterior." );
+
+define( "COMMON_PLIST_1", "Por favor seleccione una lista existente para ser editada.\\no Seleccione una(s) pista(s) para crear una nueva." );
+
+define( "COMMON_BATCHEDIT_1", "Está seguro de querer borrar las pistas seleccionadas del netjuke?" );
+
+define( "COMMON_RADIOEDIT_1", "Est seguro de querer reemplazar completamente la lista actual?" );
+
+define( "COMMON_JUKEBOXEDIT_1", "Are you sure you want to completely replace the existing jukebox playlist?" );
+
+define( "COMMON_BEDIT_HEADER", "ESPECIALES... ADMINISTRATORES Y HERRAMIENTAS DE EDICIîN" );
+define( "COMMON_BEDIT_TB_BATCHEDIT", "Editar Batch" );
+define( "COMMON_BEDIT_TB_BATCHEDIT_HELP", "Click aqu para editar las pistas seleccionadas en modo batch." );
+define( "COMMON_BEDIT_TB_BATCHDEL", "Borrar Batch" );
+define( "COMMON_BEDIT_TB_BATCHDEL_HELP", "Click aqu para borrarlas pistas seleccionadas en modo batch" );
+define( "COMMON_BEDIT_TB_RADIOADD", "Adicionar a la lista de Radio" );
+define( "COMMON_BEDIT_TB_RADIOADD_HELP", "Click aqu para adicionar las pistas seleccionadas a la lista actual de radio." );
+define( "COMMON_BEDIT_TB_RADIOREP", "Reemplazar la lista de Radio" );
+define( "COMMON_BEDIT_TB_RADIOREP_HELP", "Click aqu para crear una nueva listade radio, o reemplazar la actual, con las pistas seleccionadas." );
+define( "COMMON_BEDIT_TB_RADIOPLVIEW", "Ver Lista de Reproduccin de Radio");
+define( "COMMON_BEDIT_TB_RADIOPLVIEW_HELP", "Click para Ver El Archivo de Texto Actual Usado como una Lista de Reproduccin de Radio");
+define( "COMMON_BEDIT_TB_JUKEBOXSTART", "Iniciar Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXSTART_HELP", "Clic aquí para empezar a reproducir con el jukebox la lista de reproducción en el servidor." );
+define( "COMMON_BEDIT_TB_JUKEBOXSTOP", "Parar Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXSTOP_HELP", "Clic aquí para parar la reproducción de la lista del jukebox' del servidor." );
+define( "COMMON_BEDIT_TB_JUKEBOXADD", "Adicionar a la lista de reproducción del Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXADD_HELP", "Clic aquí para adicionar las pistas seleccionadas a la lista de reproducción actual del jukebox." );
+define( "COMMON_BEDIT_TB_JUKEBOXREP", "Reemplazar Lista de Reproducción Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXREP_HELP", "Clic aquí para crear una nueva lista de reproducción jukebox,o reemplazar la existente con las pistas seleccionadas." );
+define( "COMMON_BEDIT_TB_JUKEBOXPLVIEW", "Ver Lista de Reproducción Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXPLVIEW_HELP", "Clic aquí para ver el actual archivo de texto usado para lista de reproducción jukebox." );
+
+define( "COMMON_SUMHEAD_QS_HEADER", "BòSQUEDA RçPIDA" );
+define( "COMMON_SUMHEAD_QS_OPT_AR", "Buscar por nombre de Artista" );
+define( "COMMON_SUMHEAD_QS_OPT_TR", "Buscar por nombre de cancin" );
+define( "COMMON_SUMHEAD_QS_OPT_AL", "Buscar por nombre de çlbun" );
+define( "COMMON_SUMHEAD_QS_OPT_GE", "Buscar por nombre de Gnero" );
+define( "COMMON_SUMHEAD_QS_OPT_TI", "Buscar por duracin (Segundos)" );
+define( "COMMON_SUMHEAD_QS_BTN", "Buscar!" );
+
+define( "COMMON_SUMHEAD_SUMM_HEADER", "RESUMEN DEL CONTENIDO" );
+define( "COMMON_SUMHEAD_SUMM_TR", "Cancin" );
+define( "COMMON_SUMHEAD_SUMM_AR", "Artistas" );
+define( "COMMON_SUMHEAD_SUMM_AL", "Albunes" );
+define( "COMMON_SUMHEAD_SUMM_GE", "Gneros" );
+
+define( "COMMON_SUMHEAD_ALPHA_AR_HEADER", "ARTISTAS" );
+define( "COMMON_SUMHEAD_ALPHA_AL_HEADER", "çLBUMES" );
+
+define( "COMMON_SUMHEAD_ALL_TITLE", "Listar a todas entradas en orden alfabtico" );
+define( "COMMON_SUMHEAD_ALL_LINK", "Listar todo" );
+
+##################################################
+
+?>
