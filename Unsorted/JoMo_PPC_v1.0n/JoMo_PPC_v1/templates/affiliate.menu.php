3cb228dc03a747df38e6beec155dcde412ecdbb8
me-suzy <me.suzana@gmail.com>
2025-08-13 05:55:50 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/affiliate.menu.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/affiliate.menu.php
@@ -0,0 +1,17 @@
+<!--{member: top menu-->
+
+<table width="100%" align="center" border="1" bgcolor="#FFFF99" cellspacing="0" bordercolor="#FFCC00">
+ <tr align="center">
+  <td> <a href="<%$selfURL%>?mode=affiliates&affMode=main">Main</a>
+  <td> <a href="<%$selfURL%>?mode=affiliates&affMode=searchboxes">search boxes</a>
+  <td> <a href="<%$selfURL%>?mode=affiliates&affMode=customize">customize search results</a>
+  <td> <a href="<%$selfURL%>?mode=affiliates&affMode=xml">XML format</a>  
+  <td> <a href="<%$selfURL%>?mode=affiliates&affMode=info">Info</a>  
+  <td> <a href="<%$selfURL%>?mode=affiliates&affMode=account">Account</a>
+  <td> <a href="<%$selfURL%>?mode=affiliates&affMode=clickstats">Statistics</a>
+  <td> <a href="<%$selfURL%>?mode=affiliates&affMode=logout">Logout</a>
+ </tr>
+
+</table>
+
+<!--}top menu-->
