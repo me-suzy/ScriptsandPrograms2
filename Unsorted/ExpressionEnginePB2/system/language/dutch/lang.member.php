2be9591c7ba0d570f2b034806e8657842e54fa22
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:10 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.member.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.member.php
@@ -0,0 +1,376 @@
+<?php
+$L = array(
+
+
+"member_module_name" =>
+"Lid",
+
+"member_module_description" =>
+"Leden beheer systeem",
+
+"mbr_menu" =>
+"Menu",
+
+"mbr_show" =>
+"Bekijk",
+
+"mbr_sort" =>
+"Sorteer",
+
+"mbr_order" =>
+"Volgorde",
+
+"mbr_rows" =>
+"Rijen",
+
+"mbr_my_account" =>
+"Mijn Account",
+
+"mbr_logged_in_as" =>
+"Ingelogd als",
+
+"mbr_logout" =>
+"Uitloggen",
+
+"mbr_member_registration" =>
+"Lid Registratie",
+
+"mbr_forgotten_password" =>
+"Wachtwoord Vergeten",
+
+"mbr_memberlist" =>
+"Leden Lijst",
+
+"notice" =>
+"Bericht",
+
+"mbr_registration_not_allowed" =>
+"Op dit moment worden er geen nieuwe leden geaccepteerd",
+
+"mbr_login" =>
+"Inloggen",
+
+"mbr_form_empty" =>
+"U moet uw gebruikersnaam en wachtwoord invoeren",
+
+"mbr_account_not_active" =>
+"Uw lidmaatschaps account is nog niet geactiveerd.",
+
+"mbr_you_are_logged_in" =>
+"U bent nu ingelogd.",
+
+"mbr_you_are_logged_out" =>
+"U bent nu uitgelogd.",
+
+"mbr_no_reset_id" =>
+"Het ID nummer wat u ingevoerd heeft is niet geldig. Controleer de link die u aangeklikt heeft.",
+
+"mbr_id_not_found" =>
+"Het code nummer wat u invoerde is niet gevonden in de database.",
+
+"mbr_username" =>
+"Gebruikersnaam",
+
+"mbr_password" =>
+"Wachtwoord",
+
+"mbr_submit" =>
+"Toevoegen",
+
+"mbr_update" =>
+"Update",
+
+"mbr_password_confirm" =>
+"Bevestig wachtwoord",
+
+"mbr_auto_login" =>
+"Automatisch inloggen bij uw volgende bezoek",
+
+"mbr_show_name" =>
+"Laat mijn naam zien in de online gebruikers lijst",
+
+"mbr_forgot_password" =>
+"Wachtwoord vergeten?",
+
+"mbr_email_address" =>
+"Email adres",
+
+"mbr_your_email" =>
+"Uw Email adres",
+
+"mbr_email_short" =>
+"Email",
+
+"mbr_icq_console" =>
+"ICQ Bedieningspaneel",
+
+"mbr_aim_console" =>
+"AOL IM",
+
+"mbr_aol_short" =>
+"AOL",
+
+"mbr_icq_short" =>
+"ICQ",
+
+"mbr_www" =>
+"URL",
+
+"mbr_msn_short" =>
+"MSN",
+
+"mbr_yahoo_short" =>
+"Yahoo",
+
+"mbr_screen_name" =>
+"Naam op het Scherm",
+
+"mbr_name" =>
+"Naam",
+
+"mbr_screen_name_exp" =>
+"Als u dit veld leeg laat, dan zal uw naam hetzelfde zijn als uw gebruikersnaam",
+
+"mbr_your_url" =>
+"URL van uw website",
+
+"mbr_field_required" =>
+"Het volgende veld is verplicht:",
+
+"mbr_username_length" =>
+"Gebruikersnamen moeten tenminste %x tekens lang zijn",
+
+"mbr_password_length" =>
+"Wachtwoorden moeten tenminste %x tekens lang zijn",
+
+"mbr_registration_complete" =>
+"Registratie Voltooien",
+
+"mbr_registration_completed" =>
+"Uw registratie is succesvol voltooid.",
+
+"mbr_admin_will_activate" =>
+"Een administrator zal uw account activeren en u op de hoogte brengen wanneer u het kunt gebruiken.",
+
+"mbr_membership_instructions_email" =>
+"Er is zojuist een email naar u gestuurd met instructies om het account te activeren.",
+
+"mbr_membership_instructions_cont" =>
+"Controleer uw email en volg de instructies die in de email staan.",
+
+"mbr_your_are_logged_in" =>
+"U bent u ingelogd en klaar om uw account te gebruiken.",
+
+"mbr_activation" =>
+"Account Activeren",
+
+"mbr_problem_activating" =>
+"Ongeldige Activatie Verzoek",
+
+"mbr_activation_success" =>
+"Uw account is geactiveerd",
+
+"mbr_may_now_log_in" =>
+"U kunt nu inloggen en uw account gebruiken.",
+
+"mbr_not_allowed_to_view_profiles" =>
+"U bent niet gemachtigd om leden profielen te bekijken.",
+
+"mbr_member_profile" =>
+"Leden Profiel",
+
+"mbr_member_group" =>
+"Leden Groep:",
+
+"mbr_last_visit" =>
+"Laatste Bezoek",
+
+"mbr_most_recent_entry" =>
+"Meest Recente Toevoeging",
+
+"mbr_most_recent_comment" =>
+"Meest Recente Reactie",
+
+"mbr_required_fields" =>
+"Verplichte Velden",
+
+"mbr_join_date" =>
+"Datum van Aanmelding",
+
+"mbr_total_entries" =>
+"Totaal aantal Berichten",
+
+"mbr_total_comments" =>
+"Totaal aantal Reacties",
+
+"mbr_comments" =>
+"Reacties",
+
+"mbr_member_timezone" =>
+"Tijdzone van Lid",
+
+"mbr_member_local_time" =>
+"Lokale Tijd van Lid",
+
+"mbr_email" =>
+"Email",
+
+"mbr_url" =>
+"Homepage",
+
+"mbr_location" =>
+"Lokatie",
+
+"mbr_birthday" =>
+"Geboortedatum",
+
+"mbr_your_stats" =>
+"Uw Account Statistieken",
+
+"mbr_edit_your_profile" =>
+"Wijzig Uw Profiel",
+
+"mbr_profile_homepage" =>
+"Profiel Homepage",
+
+"mbr_profile_has_been_updated" =>
+"Uw profiel is succesvol geupdatet",
+
+"mbr_email_has_been_updated" =>
+"Uw email voorkeuren zijn succesvol geupdatet.",
+
+"mbr_email_updated" =>
+"Email voorkeuren geupdate",
+
+"username_disallowed" =>
+"Het is niet toegestaan om uw gebruikersnaam te veranderen",
+
+"mbr_settings_updated" =>
+"Uw instellingen zijn geupdatet",
+
+"mbr_notepad_updated" =>
+"Uw kladblok is geupdatet",
+
+"mbr_localization_settings_updated" =>
+"Uw lokatie voorkeuren zijn succesvol geupdatet",
+
+"mbr_terms_of_service" =>
+"Algemene Voorwaarden",
+
+"mbr_i_accept" =>
+"Ik ga akkoord met de Algemene Voorwaarden",
+
+"mbr_terms_of_service_required" =>
+"U moet &quot;Akkoord met de Algemene Voorwaarden&quot; aanklikken",
+
+"mbr_custom_field_empty" =>
+"Het volgende veld is verplicht:",
+
+"mbr_all_member_groups" =>
+"Alle Leden Groepen",
+
+"mbr_member_name" =>
+"Lidnaam",
+
+"mbr_ascending" =>
+"Oplopend",
+
+"mbr_descending" =>
+"Aflopend",
+
+"mbr_back_to_login" =>
+"Terug naar Inloggen",
+
+"mbr_back_to_main" =>
+"Terug naar Begin",
+
+"mbr_aol_im" =>
+"AOL Instant Messenger",
+
+"mbr_icq" =>
+"ICQ Nummer",
+
+"mbr_yahoo" =>
+"Yahoo Messenger",
+
+"mbr_msn" =>
+"MSN Messenger",
+
+"mbr_bio" =>
+"Leden Biografie",
+
+"mbr_interests" =>
+"Interesses",
+
+"mbr_occupation" =>
+"Werkzaamheden",
+
+"mbr_email_member" =>
+"Email Bedieningspaneel",
+
+"mbr_subject" =>
+"Email Onderwerp",
+
+"mbr_message" =>
+"Email Bericht",
+
+"mbr_close_window" =>
+"Sluit Venster",
+
+"mbr_recipient" =>
+"Email Ontvanger:",
+
+"mbr_email_not_accepted" =>
+"Dit lid accepteert op dit moment geen email.",
+
+"mbr_email_disclaimer" =>
+"Door dit bericht te versturen wordt uw email adres zichtbaar bij de ontvanger.",
+
+"mbr_email_logged" =>
+"Let Op: Alle email berichten worden geregistreerd en zichtbaar gemaakt voor administrators",
+
+"mbr_missing_fields" =>
+"Alle velden zijn verplicht",
+
+"mbr_send_email" =>
+"Verstuur Email",
+
+"mbr_email_forwarding" =>
+"Dit bericht is verstuurd naar jou via jou account op:",
+
+"mbr_email_forwarding_cont" =>
+"Als je niet langer emails wenst te ontvangen kun je dit aangeven in de voorkeuren van je ledenprofiel op de website.",
+
+"mbr_email_error" =>
+"Er is een fout opgetreden tijdens het versturen van de email.",
+
+"mbr_good_email" =>
+"Uw email is succesvol verstuurd.",
+
+"mbr_send_self_copy" =>
+"Stuur me een kopie van deze email",
+
+"mbr_icq_number" =>
+"ICQ Nummer:",
+
+"mbr_icq_recipient" =>
+"Ontvanger:",
+
+"mbr_icq_subject" =>
+"Onderwerp",
+
+"mbr_icq_message" =>
+"Bericht",
+
+"mbr_not_allowed_to_use_email_console" =>
+"U bent niet gemachtigd om dit Email Bedieningspaneel te gebruiken",
+
+"mbr_email_timelock_not_expired" =>
+"U kunt maximaal 1 keer per %x minuten gebruik maken van dit Email Bedieningspaneel.",
+
+"mbr_you_are_registered" =>
+"U bent al geregistreerd en ingelogd.",
+
+''=>''
+);
+?>
