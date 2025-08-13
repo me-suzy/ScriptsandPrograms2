b08c9c614e69c7ec079002d1a092d507537ac88a
me-suzy <me.suzana@gmail.com>
2025-08-13 04:35:21 +0300

    Add Unsorted/fcart/fcart/bottom.php

---
+++ Unsorted/fcart/fcart/bottom.php
@@ -0,0 +1,30 @@
+<td bgcolor="<? echo $cl_tab_top ?>" width="2%">&nbsp;</td>
+</tr>
+<tr> 
+<td colspan="<? echo $tabcount-1; ?>" bgcolor="<? echo $cl_bot_2 ?>" height="16"> 
+<div align="right"><b><font size="-2" color="<? echo $cl_win_title ?>">
+&copy; 2000-2001 <a href="http://www.rrf.ru" style="text-decoration: underline; color: <? echo $cl_win_title ?>">RRF.ru development</a>&nbsp;&nbsp;<br>
+&copy; 2000-2001 <? echo $main_www ?>&nbsp;&nbsp;</font></b></div>
+</td>
+<td bgcolor="<? echo $cl_bot_2 ?>" height="16" width="2%"><img src="images/null.gif" width="1" height="1"></td>
+</tr>
+<tr> 
+<?
+if (empty($dont_display_lc)) {
+	echo "<td width=\"10%\" bgcolor=\"$cl_bot_1\" valign=\"top\"><img src=\"images/null.gif\" width=\"1\" height=\"1\"></td>";
+}
+?>
+<td colspan="<? echo $tabcount-1; ?>" bgcolor="<? echo $cl_bot_2 ?>"><img src="images/null.gif" width="1" height="1"></td>
+<td bgcolor="<? echo $cl_bot_2 ?>" width="2%"><img src="images/null.gif" width="1" height="1"></td>
+</tr>
+<tr> 
+<?
+if (empty($dont_display_lc)) {
+	echo "<td width=\"10%\" bgcolor=\"$cl_bot_1\" valign=\"top\"><img src=\"images/null.gif\" width=\"1\" height=\"1\"></td>";
+}
+?>
+<td colspan="<? echo $tabcount-1; ?>" bgcolor="<? echo $cl_bot_2 ?>"><img src="images/null.gif" width="1" height="1"></td>
+<td bgcolor="<? echo $cl_bot_2 ?>" width="2%"><img src="images/null.gif" width="1" height="1"></td>
+</tr>
+</table>
+</center>
