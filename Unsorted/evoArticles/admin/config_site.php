a1e6665f767813f29139bb6442da6c0afe70872f
me-suzy <me.suzana@gmail.com>
2025-08-13 04:07:41 +0300

    Add Unsorted/evoArticles/admin/config_site.php

---
+++ Unsorted/evoArticles/admin/config_site.php
@@ -0,0 +1,103 @@
+<?php
+
+$settings['dateformat'] = "F j, Y";
+
+$settings['email'] = "tech@email.com";
+
+$settings['closereason'] = "This section of the site is currently closed.";
+
+$settings['deflang'] = "lang_english";
+
+$settings['usewysiwyg'] = "1";
+
+$settings['tplfolder'] = "templates/";
+
+$settings['navsplit'] = "/";
+
+$settings['cookiedomain'] = "";
+
+$settings['seccookie'] = "0";
+
+$settings['sestimeout'] = "3600";
+
+$settings['defstyle'] = "default";
+
+$settings['usecomment'] = "0";
+
+$settings['sitename'] = "SiteName.com";
+
+$settings['isclose'] = "0";
+
+$settings['imgfolder'] = "img/";
+
+$settings['allowmeta_cat'] = "1";
+
+$settings['showlatest'] = "1";
+
+$settings['showhowmany'] = "10";
+
+$settings['allowmeta_art'] = "1";
+
+$settings['siteurl'] = "http://siteurl.com";
+
+$settings['useavatar'] = "1";
+
+$settings['canhtmlsummary'] = "0";
+
+$settings['maxdimension'] = "60";
+
+$settings['avmaxsize'] = "20000";
+
+$settings['avallowedmime'] = "jpg,gif,png";
+
+$settings['uniqueonly'] = "1";
+
+$settings['usecache'] = "0";
+
+$settings['tpldetail'] = "1";
+
+$settings['rss_total'] = "5";
+
+$settings['useses'] = "0";
+
+$settings['commentsystem'] = "internal";
+
+$settings['internal_canhtml'] = "1";
+
+$settings['internal_canbbcode'] = "1";
+
+$settings['internal_censor'] = "shit
+
+fuck
+
+crap
+
+fuckers
+
+asshole
+
+bullshit
+
+ass";
+
+$settings['internal_usevalidation'] = "1";
+
+$settings['art_showcustom'] = "1";
+
+$settings['art_useimage'] = "1";
+
+$settings['img_maxdimension'] = "250";
+
+$settings['img_maxsize'] = "20000";
+
+$settings['img_allowedmime'] = "jpg,gif,png";
+
+$settings['use_relatedart'] = "1";
+
+$settings['usegzip'] = "0";
+
+$settings['site_listarticles'] = "1";
+
+
+
+?>
