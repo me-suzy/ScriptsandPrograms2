de53b0c3021b696ea5b78b7b21ebfabf46c89019
me-suzy <me.suzana@gmail.com>
2025-08-13 06:30:18 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/en/error.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/en/error.php
@@ -0,0 +1,52 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[ERROR_License] = "There is an error with your license. Please license MWChat now.";
+
+$LOCALE[ERROR_AlreadyIn] = "The name you chose is already in use.";
+
+$LOCALE[ERROR_Title] = "Error";
+
+$LOCALE[ERROR_Logout] = "You have been logged out.";
+
+$LOCALE[ERROR_Expired] = "Your login has expired, please login again.";
+
+$LOCALE[ERROR_RoomLong] = "This room name you choose is to long.";
+
+$LOCALE[ERROR_Live] = "The system is currently unavailable.";
+
+$LOCALE[ERROR_Profanity] = "The information you specified contains profanity.";
+
+$LOCALE[ERROR_NoData]   = "Please fill in all fields.";
+
+$LOCALE[ERROR_BadData]  = "Please specify valid data.";
+
+$LOCALE[ERROR_Serial_Invalid] = "Your serial number is invalid, please re-register your product.";
+
+$LOCALE[ERROR_Serial_None] = "Cannot find a serial number on record, please re-register your product.";
+
+$LOCALE[ERROR_Critical] = "Critical error!";
+
+$LOCALE[ERROR_Javascript] = "This program requires a JavaScript capable browser.";
+
+$LOCALE[ERROR_Browser] = "This program will not function correctly with your browser.";
+
+$LOCALE[ERROR_Restricted] = "The name you chose is restricted.";
+
+$LOCALE[ERROR_BadPass]  = "Please enter a valid password.";
+
+$LOCALE[ERROR_IP] = "The IP address your using has changed. You have been logged out for security reasons.";
+
+$LOCALE[ERROR_PHPV] = "MWChat requires a minimum PHP version of";
+
+?>
