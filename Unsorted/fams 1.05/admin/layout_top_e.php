bef19219ad80f2e6e46814440cd267423d30050a
me-suzy <me.suzana@gmail.com>
2025-08-13 04:34:49 +0300

    Add Unsorted/fams 1.05/admin/layout_top_e.php

---
+++ Unsorted/fams 1.05/admin/layout_top_e.php
@@ -0,0 +1,60 @@
+<?php
+
+/* ------------------------------------------------------------ */
+/*								*/
+/*	File Area Management System (FAMS)			*/
+/*								*/
+/*	Copyright (c) 2001 by Bastian 'Buddy' Grimm		*/
+/*	Autor: Bastian Grimm					*/
+/*	Publisher: [ BG-Studios ]				*/
+/*	eMail: bastian@bg-studios.de				*/
+/*	WebSite: http://www.bg-studios.de			*/
+/*	Date: 03.05.01		Version: Admin Layout 1.10	*/
+/*	Last changed: 24.07.01					*/
+/*								*/
+/* ------------------------------------------------------------	*/
+
+require("../config.php");
+
+?>
+
+
+<HTML>
+
+<HEAD>
+<TITLE>
+FAMS by [ BG-Studios ] - Admin Area
+</TITLE>
+<META NAME="copyright" CONTENT="Bastian Grimm">
+<META NAME="publisher" CONTENT="BG-Studios">
+<META NAME="publisher-email" CONTENT="info@bg-studios.de">
+<META NAME="author" CONTENT="Bastian Grimm">
+
+<link rel="stylesheet" href="./admin_style.css">
+
+</HEAD>
+
+<BODY>
+<CENTER>
+
+
+<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=2 WIDTH=600 BGCOLOR=#FFFFFF>
+<TR>
+
+<TD VALIGN=top WIDTH=600 CLASS=main>
+<BR>
+
+
+
+<TABLE BORDER=0 CELLSPACING=1 CELLPADDING=2 WIDTH=600 HEIGHT=96% BGCOLOR=#FFFFFF>
+<TR>
+<TD WIDTH=600 CLASS=main-headline HEIGHT=18>
+
+
+<IMG BORDER=0 SRC=../images/group-link.gif><B>File Area Management System - Log In</B>
+
+</TD>
+</TR>
+<TR>
+<TD WIDTH=600 VALIGN=top CLASS=main-content>
+<BR>
