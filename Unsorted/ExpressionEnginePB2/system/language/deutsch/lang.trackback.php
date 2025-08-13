bd27ac7227175b22520fd0a34723cd53bb1c8cf1
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:40 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.trackback.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.trackback.php
@@ -0,0 +1,24 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"trackback_module_name" =>
+"Trackback",
+
+"trackback_module_description" =>
+"Trackback-Modul",
+
+//----------------------------------------
+
+"trackbacks_not_allowed" =>
+"Trackbacks are disabled for this entry",
+
+
+// END
+''=>''
+);
+?>
