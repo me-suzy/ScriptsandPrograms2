270669adacc161b24edaa6635cb15e88615f2b77
me-suzy <me.suzana@gmail.com>
2025-08-13 04:49:38 +0300

    Add Unsorted/gcards/inc/lang/language_fr.php

---
+++ Unsorted/gcards/inc/lang/language_fr.php
@@ -0,0 +1,148 @@
+<?
+// index.php
+$index01 = 'Voir toutes les catégories';
+$index02 = 'Avant tout, choisissez la carte que vous aller envoyer!';
+
+// general navigation
+$nav01 = 'Retour';
+$nav02 = 'Suivant';
+$nav03 = 'Accueil';
+$nav04 = 'Montrer';
+$nav05 = 'Console d\'administration';
+$nav06 = 'Changer le password';
+$nav07 = 'Se déconnecter';
+$nav08 = 'Se connecter';
+$nav09 = 'Un script de ';
+$nav10 = 'Colonnes par page';
+$nav11 = '<a href="javascript:history.go(-1);">Précédent</a>';
+$nav12 = 'Retourner à';
+
+
+
+// UIfunctions.php
+$uifunc01 = 'Loggé en tant que: ';
+
+// compose.php
+$compose01 = 'Vous n\'avez pas choisi de carte à envoyer!  <a href="index.php">Cliquez ici</a> pour choisir une carte.';
+$compose02 = 'Ajouter votre texte avant d\'envoyer votre carte virtuelle!';
+$compose03 = 'Votre nom';
+$compose04 = 'Votre email';
+$compose05 = '<b>De:</b>';
+$compose06 = '<b>A:</b>';
+$compose07 = 'Me prévenir lorsque la/ les cartes virtuelles ont été vu.';
+$compose08 = 'Séparez les adresses email par des virgules';
+
+// preview.php
+$preview01 = "<br><br>Veuillez remplir TOUS les champs du formulaire! $nav11";
+$preview02 = 'n\'est pas une adresse email valide';
+$preview03 = "Adresses emails invalides! $nav11";
+$preview04 = 'Envoyer';
+
+// sendcard.php
+$sendcard01 = 'Erreur!  Le formulaire n\'a pas été rempli correctement.  <a href="javascript:history.go(-1);">Cliquez ici</a> pour recommencer';
+$sendcard02 = 'error inserting: ';
+$sendcard03 = 'La carte virtuelle a été envoyée...';
+$sendcard04 = 'La carte virtuelle n\'a pas pu être envoyée...';
+
+// getcard.php
+$getcard01 = 'Erreur: cette carte n\'existe pas/ plus.';
+$getcard02 = '$to_email a vu votre carte virtuelle.';
+$getcard03 = 'Envoyer une carte virtuelle à $from_name';
+
+// showcard.php
+$showcard01 = 'Une carte virtuelle de';
+
+// news.php
+$news01 = 'Archives';
+$news02 = 'Posté';
+$news03 = 'par';
+$news04 = 'News par page';
+$news05 = 'News';
+$news06 = 'Sujet';
+$news07 = 'Texte';
+$news08 = 'Date';
+$news09 = 'Auteur';
+
+
+
+// user related text
+$user01 = 'Identifiant';
+$user02 = 'Mot de passe';
+$user03 = 'Vous n\'êtes pas identifié. Vous ne pouvez pas vous déconnecter!';
+$user04 = 'Vous avez été déconnecté. A bientôt.';
+$user05 = 'Confirmation du mot de passe';
+$user06 = 'Adresse email ';
+$user07 = 'Role';
+$user08 = 'Utilisateur';
+$user09 = 'Mot de passe incorrect. Veuillez réessayer.';
+$user10 = 'Laisser vide pour ne pas modifier les données.';
+$user11 = 'L\'ancien mot de passe est incorrect.';
+$user12 = 'Mot de passe mis à jour avec succès.';
+$user13 = 'Ancien mot de passe';
+$user14 = 'Nouveau mot de passe';
+
+/*
+*********************
+** ADMIN DIRECTORY **
+*********************
+*/
+
+// Authentication
+$auth01 = 'L\'authentification a échoué...';
+$auth02 = 'Veuillez entrer vos identifiant et mot de passe.';
+$auth03 = 'Vous n\'êtes pas connecté.';
+$auth04 = 'Vous n\'avez pas l\'autorisation nécessaire pour voir cette page.';
+
+// admin.php
+$admin01 = 'Options d\'administration';
+$admin02 = 'Maintenance';
+$admin03 = "$admin02 des cartes";
+$admin04 = "$admin02 des catégories";
+$admin05 = "$admin02 des news";
+$admin06 = "$admin02 des utilisateurs";
+$admin07 = 'Voir, ajouter, mettre à jour et effacer';
+$admin08 = 'ID';
+$admin09 = 'effacé de la base de donnée...';
+$admin10 = 'effacé du serveur...';
+$admin11 = 'non';
+$admin12 = 'mis à jour';
+$admin13 = 'mis à jour';
+$admin14 = 'ajouté';
+$admin15 = 'Ajouter une news';
+$admin16 = 'Envoyer';
+
+// actions
+$action01 = 'Action';
+$action02 = 'Ajouter';
+$action03 = 'Editer';
+$action04 = 'Effacer';
+$action05 = 'Voir';
+$action06 = 'Mettre à jour';
+$action07 = 'Annuler';
+$action08 = 'Prévisualiser';
+
+// cards.php
+$cards01 = 'Image';
+$cards02 = 'Vignette';
+$cards03 = 'Nb d\'envois';
+$cards04 = 'Carte';
+$cards05 = 'Nom de la carte';
+$cards06 = 'Laisser vide pour la création automatique de la vignette';
+
+// categories
+$cat01 = 'Categorie';
+$cat02 = 'Nom de la catégorie';
+$cat03 = 'cartes dans la catégorie';
+
+// upload.php
+$upload01 = 'L\'image n\'a pas été envoyé sur le serveur.';
+$upload02 = "Erreur de copie avec la fonction 'move_uploaded_file'.";
+$upload03 = 'Image envoyé sur le serveur avec succès.';
+$upload04 = 'Erreur de copie du fichier!';
+$upload05 = 'Vignette envoyée sur le serveur avec succès...';
+$upload06 = 'Image utilisée comme vignette...';
+$upload07 = 'Vignette créée avec succès...';
+$upload08 = 'Veuillez soumettre une vignette si le format de l\'image n\'est pas .jpeg';
+
+
+?>
