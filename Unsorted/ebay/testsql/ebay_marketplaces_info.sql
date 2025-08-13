a9fd217811ef7cfd28e22df3f9df20c2a4ead96f
me-suzy <me.suzana@gmail.com>
2025-08-13 03:38:55 +0300

    Add Unsorted/ebay/testsql/ebay_marketplaces_info.sql

---
+++ Unsorted/ebay/testsql/ebay_marketplaces_info.sql
@@ -0,0 +1,42 @@
+/*	$Id: ebay_marketplaces_info.sql,v 1.2 1999/02/21 02:56:36 josh Exp $	*/
+/*
+ * ebay_marketplaces_info.sql
+ *
+ * It holds information abount number of items, and bids in a
+ * marketplace.
+ *
+ */
+
+/* 	drop table ebay_marketplaces_info;
+ */
+
+	create table ebay_marketplaces_info
+	(
+		id						int
+			constraint	marketplaces_info_id_nn
+			not null,
+		item_count				int
+			default 0,
+		daily_item_count		int
+			default 0,
+		bid_count				int
+			default 0,
+		constraint		marketplaces_info_fk
+			foreign key (id)
+			references	ebay_marketplaces(id),
+		constraint		marketplaces_info_pk
+			primary key (id)
+			using index tablespace tuseri01
+			storage (initial 1K next 1K)
+	)
+	tablespace tuserd01
+	storage (initial 1K next 1K);
+
+/*
+ * Prime the table
+ */
+	insert into ebay_marketplaces_info
+		(id)
+		select id from ebay_marketplaces;
+
+	commit;
