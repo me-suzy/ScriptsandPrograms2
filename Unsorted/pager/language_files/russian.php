3d98deb5095dfe6ecfd518cd263f3f043b13a087
me-suzy <me.suzana@gmail.com>
2025-08-13 07:36:52 +0300

    Add Unsorted/pager/language_files/russian.php

---
+++ Unsorted/pager/language_files/russian.php
@@ -0,0 +1,28 @@
+<?
+# Translated by Yurik_A <museum@irk.ru>.
+
+$l_send =			"Ïîñëàòü ñîîáùåíèå ýòîìó ICQ íîìåðó:";
+$l_emne =			"Òåìà:";
+$l_besked =			"Ñîîáùåíèå:";
+$l_ditnavn =			"Âàøå èìÿ:";
+$l_dinepost =			"Âàø e-mail àäðåñ:";
+$l_sendbeskeden =		"Îòïðàâèòü ñîîáùåíèå";
+$l_lavkoden =			"Ñãåíèðîâàòü êîä";
+$l_redigerformularen =		"Ðåäàêòðîâàòü ôîðìó ICQ ïåéäæåðà. Åñëè íå óâåðåíû íàæìèòå êíîïêó \"$l_lavkoden\" .";
+$l_tabelbaggrund =		"Öâåò ôîíà:";
+$l_ramme =			"Ðàìêà:";
+$l_celleafstand =		"Èíòåðâàë ÿ÷åéêè:";
+$l_cellevaeg =			"Ãðàíèöà ÿ÷åéêè:";
+$l_skrifttype =			"Ùðèôò:";
+$l_skriftstr =			"Ðàçìåð øðèôòà:";
+$l_skriftfarve =		"Öâåò øðèôòà:";
+$l_gennemse =			"Ïðåäâàðèòåëüíûé ïðîñìîòð ôîðìû:";
+$l_kopier =			"Ñîïèðóéòå è âñòàâüòå ýòîò êîä â Âàø HTML äîêóìåíò:";
+$l_icqikkeudfyldt =		"Âû íå ââåëè íîìåð ICQ, êîòîðîìó ïîñûëàåòñÿ ñîîáùåíèå.";
+$l_ingenbesked =		"Âû íå íàïèñàëè ñîîáùåíèå.";
+$l_tilbage =			"Íàçàä";
+$l_over450tegn =		"Ñîîáùåíèå ïðåâûøàåò íîðìó. Äîïóñòèìî òîëüêî 450 ñèìâîëîâ.";
+$l_epostikkeudfyldt =		"Âû íå ââåëè ñâîé e-mail àäðåñ.";
+$l_beskedenafsendt =		"Ñîîáùåíèå îòïðàâëåíî.";
+$l_hjaelp =			"Ïîìîùü";
+?>
