6499395bae9311b660481bbe5d366b227c73bf35
me-suzy <me.suzana@gmail.com>
2025-08-13 06:57:32 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/installer/lib/sql/upgrade/mysql/account.sql.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/installer/lib/sql/upgrade/mysql/account.sql.php
@@ -0,0 +1,9 @@
+<?php
+
+############################################################
+
+array_push($sql_statements,"update netjuke_sessions set session_id = 'JUST A DUMMY SQL QUERY' where session_id = 'JUST A DUMMY SQL QUERY'");
+
+############################################################
+
+?>
