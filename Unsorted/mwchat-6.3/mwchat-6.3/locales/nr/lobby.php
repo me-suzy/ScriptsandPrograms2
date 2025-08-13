7e243dc51afd093ea5a1d5744ae066243886c465
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:39 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/lobby.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/lobby.php
@@ -0,0 +1,37 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[LOBBY_Entered_Room] = "Går inn i romet.";
+
+$LOCALE[LOBBY_Room_Full] = "Romet som De anmodet er fult.";
+
+$LOCALE[LOBBY_Room_Title] = "De er nå i Lobby";
+
+$LOCALE[LOBBY_Visitors] = "besøkende";
+
+$LOCALE[LOBBY_Public_List] = "Offentlig romliste";
+
+$LOCALE[LOBBY_Private_List] = "Privat romliste";
+
+$LOCALE[LOBBY_Create_Public] = "Skap et nytt offentlig rom";
+
+$LOCALE[LOBBY_Create_Private] = "Skap et nytt privat rom";
+
+$LOCALE[LOBBY_Button] = "Gå inn i eller Create rom";
+
+$LOCALE[LOBBY_Room_Password] = "Rom Password";
+
+$LOCALE[LOBBY_Hidden] = "Gjemt";
+
+$LOCALE[LOBBY_Password_Incorrect] = "Passordet som De forsynt for det private romet er ukorrekt.";
+
+?>
