3ca75941ca314bf3984a17a9072f5919818327a5
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:10 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-installer_installer.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-installer_installer.php
@@ -0,0 +1,42 @@
+<?php
+
+##################################################
+
+# /installer/*.php
+
+##################################################
+
+define( "ADMINST_ERR_VERSION", "Este instalador no puede ser usado como actualización\\n copia de Netjuke. Versiones no compatibles.\\nPor favor consigue la última copia de actualización y trata de nuevo." );
+
+define( "ADMINST_ERR_FSPATH_1", "Por Favor entre el path del filesystem." );
+define( "ADMINST_ERR_FSPATH_2", "Un ruta de Filesystem no puede contener ://" );
+define( "ADMINST_ERR_WEBPATH_1", "Por favor entre el path de la Web." );
+define( "ADMINST_ERR_WEBPATH_2", "El path de la web debe contener http:// or https://" );
+
+define( "ADMINST_ERR_DBTYPE", "Por favor, entre el tipo de DB." );
+define( "ADMINST_ERR_DBHOST", "Por favor, entre el servidor DB." );
+define( "ADMINST_ERR_DBNAME", "Por facor, entre el nombre DB." );
+define( "ADMINST_ERR_DBCONN", "No se pudo conectar con la base de datos con la informaci'on suministrada." );
+
+define( "ADMINST_ERR_EMAIL", "Por favor entre un Sys por defecto. Correo de administrador." );
+define( "ADMINST_ERR_PASS", "Por favor inserte un Sys por defecto. Password de administrador." );
+
+define( "ADMINST_ERR_STREAM", "Por favor entre un servidor de Streaming." );
+
+define( "ADMINST_ERR_NOSQL", "Su instalación fracaso.  carece de algunos componentes." );
+
+define( "ADMINST_ERR_DENIED_1", "- Disculpe, pero no podrá usar este instalador con el objetivo de copiar de el Netjuke porque esto esta realmente instaldo.\\n- Por favor use el proceso de actualización instantanea." );
+define( "ADMINST_ERR_DENIED_2", "- Esta herramienta de actualización no puede ser usada con el objetico de copiar del netjuke.\\n- La base de Datos nunca fue generada (necesario para instalar)." );
+define( "ADMINST_ERR_DENIED_3", "La ruta del filesystem no pareceser valida." );
+define( "ADMINST_ERR_DENIED_4", "Acceso denegado: El usuario proporcionado Y/O su contraseña no corresponde a ninguna cuenta existente de administración." );
+
+define( "ADMINST_ERR_EXEC", "Problema executando parte de el generador sql.\\nPor Favor Intente Nuevamente." );
+
+define( "ADMINST_FILEINFO_1", "Creado" );
+define( "ADMINST_FILEINFO_2", "Para" );
+define( "ADMINST_FILEINFO_3", "Guardar Como" );
+define( "ADMINST_FILEINFO_4", "Terminado el Guardado. Tu podrás acceder a la nueva versión ahora en" );
+
+##################################################
+
+?>
