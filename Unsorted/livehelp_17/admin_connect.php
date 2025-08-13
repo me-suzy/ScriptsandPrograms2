e6f4ddc6b2e1d6358498fe0ad25704169dea81fb
me-suzy <me.suzana@gmail.com>
2025-08-13 06:07:45 +0300

    Add Unsorted/livehelp_17/admin_connect.php

---
+++ Unsorted/livehelp_17/admin_connect.php
@@ -0,0 +1,18 @@
+<META HTTP-EQUIV="refresh" content="5;URL=<?= $HTTP_GET_VARS['urlof'] ?>">
+
+<b>Connecting...please wait...</b>
+<br><br>
+<blockquote>
+Some Servers do not honor the flush() buffer 
+statment in php and some browsers will not load
+other frames if some of the frames have not 
+finished loading. if you are still reading this 
+a minute from now or all you ever see is the 
+words <b>Refreshing...</b>.. Then then your server does not
+honor the flush statment. do not worry.. There is 
+still hope for you :-). To Fix this Click on the 
+<b>Admin</b> tab and select <b>refresh</b> as the type of
+chat and then click save. This will solve the problem. 
+do it now!! because if you are reading this line then it 
+is not working and you need to change it to Refresh mode....
+</blockquote>
