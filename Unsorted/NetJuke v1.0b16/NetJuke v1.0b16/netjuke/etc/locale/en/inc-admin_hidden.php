cf21768ef6ec78991c929478e7f04e9bdd893a51
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:18 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_hidden.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_hidden.php
@@ -0,0 +1,20 @@
+<?php
+
+##################################################
+
+# /admin/hidden.php
+
+##################################################
+
+define( "HDDN_DENIED_BADDIR", "The directory you requested to scan is not registered as this site's music directory in the preference file." );
+define( "HDDN_DENIED_NODIR_1", "Please provide a valid directory to be scanned for hidden files." );
+define( "HDDN_DENIED_NODIR_2", "No Music Dir. See Preference File." );
+
+define( "HDDN_HEADER", "HIDDEN FILE FINDER" );
+define( "HDDN_CAPTION_1", "Scans the named given for hidden files or sub-directories (filename starting w/ a dot) and generate an appropriate batch file to delete them from the command line. This is especially useful when uploading files from Mac to Unix (.AppleDouble, .DS_Store, etc)" );
+define( "HDDN_CAPTION_2", "Please submit a directory to scan" );
+define( "HDDN_BTN_SCAN", "Start Scanning" );
+
+##################################################
+
+?>
