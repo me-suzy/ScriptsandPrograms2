71f7d3865485efa156b623e69369602f8bcc6f7c
me-suzy <me.suzana@gmail.com>
2025-08-13 05:37:28 +0300

    Add Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_global.php

---
+++ Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_global.php
@@ -0,0 +1,116 @@
+<?php
+
+$lang = array (
+
+
+'mod_cp'  => "Mod CP",
+
+'tb_help' => "Help",
+'tb_search' => "Search",
+'tb_mlist'  => "Members",
+'tb_calendar' => "Calendar",
+
+'gzip_on' => "GZIP Enabled",
+'gzip_off' => "GZIP Disabled",
+
+'l_qb' => 'My Assistant',
+'view_new_posts' => "View New Posts",
+
+'your_cp'					=>	"My Controls",
+
+
+'bb_tool_tip'  => "View posts since your last visit, moderator lists and more...",
+'cp_tool_tip'  => "Edit my settings, such as signature, avatar and more...",
+
+
+'find_user_names' => "Search for user names",
+'opens_new_window' => "( opens in new window )",
+
+
+'warn_offline' => "(OFFLINE)",
+
+'all_emoticons' => "Show All",
+
+'jmp_go'  => "Go!",
+'forum_jump' => "Forum Jump",
+
+'yes' => "Yes",
+'no'  => "No",
+
+'ct_size' => "SIZE",
+'ct_lrg'  => "Large",
+'ct_sml'  => "Small",
+'ct_lest' => "Largest",
+
+'ct_font' => "FONT",
+'ct_arial' => "Arial",
+'ct_times' => "Times",
+'ct_courier' => "Courier",
+'ct_impact'  => "Impact",
+'ct_geneva'  => "Geneva",
+
+'ct_color'  => "COLOR",
+'ct_blue'   => "Blue",
+'ct_red'    => "Red",
+'ct_purple' => "Purple",
+'ct_orange' => "Orange",
+'ct_yellow' => "Yellow",
+'ct_grey'   => "Gray",
+'ct_green'  => "Green",
+
+'msg_full' => "<span id='highlight'>Messenger FULL!</span>",
+
+ba_edit => "Edit",
+ba_delete => "Delete",
+
+submit_li					=>	"Log in",
+quote_text					=>	"Quote",
+pass					=>	"Password",
+thanks					=>	"Thanks",
+forum_read					=>	"The forum has been marked as read",
+guest_stuff					=>	"Welcome Guest",
+offline_title					=>	"IBForums Offline Mode",
+you_last_visit					=>	"You last visited on:",
+code_sample					=>	"Code Sample",
+transfer_you					=>	"Please wait while we transfer you...",
+new_posts					=>	"New Posts",
+admin_login					=>	"Administrators Log in",
+you_running_time					=>	"Your last action was on:",
+admin_cp					=>	"Admin CP",
+
+M_1					=>	"January",
+M_2					=>	"February",
+M_3					=>	"March",
+M_4					=>	"April",
+M_5					=>	"May",
+M_6					=>	"June",
+M_7					=>	"July",
+M_8					=>	"August",
+M_9					=>	"September",
+M_10				=>	"October",
+M_11				=>	"November",
+M_12				=>	"December",
+
+D_0					=>	"Sunday",
+D_1					=>	"Monday",
+D_2					=>	"Tuesday",
+D_3					=>	"Wednesday",
+D_4					=>	"Thursday",
+D_5					=>	"Friday",
+D_6					=>	"Saturday",
+
+
+
+name					=>	"Name",
+register					=>	"Register",
+'logged_in_as'					=>	"Logged in as: ",
+
+msg_new					=>	"%s new messages",
+offline					=>	"Sorry, the boards are offline at the moment",
+log_out					=>	"Log Out",
+dont_wait					=>	"Or click here if you do not wish to wait",
+stand_by					=>	"Please stand by...",
+your_messenger					=>	"Your messenger",
+log_in					=>	"Log In",
+);
+?>
