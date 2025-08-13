80793bdc30b6ecfaa9772f3ee4ac6cd478e81155
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:24 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.publish.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.publish.php
@@ -0,0 +1,463 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Publish page
+//----------------------------
+
+"status_changed" =>
+"Status ge&auml;ndert",
+
+"publish" =>
+"Posten",
+
+"no_weblog_exits" =>
+"Sie haben versucht, ein Weblog zu &ouml;ffnen, das es nicht gibt.",
+
+"no_entries_matching_that_criteria" =>
+"Es gibt keine Eintr&auml;ge, die Ihren Suchkriterien entsprechen.",
+
+"no_entries_exist" =>
+"In diesem Weblog gibt es noch keine Eintr&auml;ge.",
+
+"select_blog_to_post_in" =>
+"W&auml;hlen Sie ein Weblog zur Ver&ouml;ffentlichung aus.",
+
+"select_blog_to_edit" =>
+"W&auml;hlen Sie ein Weblog zur Ansicht aus.",
+
+"this_entry_will_appear_in" =>
+"Dieser Eintrag erscheint in:",
+
+"unauthorized_for_this_blog" =>
+"Sie d&uuml;fen in dieses Weblog nicht posten.",
+
+"unauthorized_for_any_blogs" =>
+"Sie d&uuml;rfen keine Eintr&auml;ge posten.",
+
+"unauthorized_to_delete_others" =>
+"Sie d&uuml;rfen nur Ihre eigenen Eintr&auml;ge l&ouml;schen.",
+
+"unauthorized_to_delete_self" =>
+"Sie d&uuml;rfen Ihre Eintr&auml;ge nicht l&ouml;schen.",
+
+"weblog" =>
+"Weblog",
+
+"weblogs" =>
+"Weblogs",
+
+"new_entry" =>
+"Neuer Eintrag",
+
+"button_mode" =>
+"Button-Modus:",
+
+"guided" =>
+"Angeleitet",
+
+"normal" =>
+"Normal",
+
+"view_entry" =>
+"Eintrag ansehen",
+
+"preview_entry" =>
+"Eintragsvorschau",
+
+"edit_entry" =>
+"Eintrag bearbeiten",
+
+"edit_weblog_entries" =>
+"Weblog-Eintr&auml;ge bearbeiten",
+
+"continue_editing" =>
+"Weiter bearbeiten",
+
+"entry_status" =>
+"Eintrag-Status",
+
+"status" =>
+"Status",
+
+"comments" =>
+"Kommentare",
+
+"open" =>
+"Offen",
+
+"close" =>
+"Close",
+
+"closed" =>
+"Geschlossen",
+
+"options" =>
+"Optionen",
+
+"sticky" =>
+"Eintrag &quot;sticky&quot; machen",
+
+"allow_comments" =>
+"Kommentare zulassen",
+
+"allow_trackbacks" =>
+"Trackbacks zulassen",
+
+"ping_sites" =>
+"Server f&uuml;r Pings",
+
+"ping_urls" =>
+"Trackback-URLs f&uuml;r Pings",
+
+"auto_discovery" =>
+"Trackback-Autoentdeckung",
+
+"select_entries_to_ping" =>
+"W&auml;hlen Sie die Eintr&auml;ge aus, die Sie anpingen wollen.",
+
+"view_previous_pings" =>
+"Fr&uuml;here Pings ansehen",
+
+"previiously_pinged_urls" =>
+"Sie haben die folgenden URLs schon angepingt",
+
+"date" =>
+"Datum",
+
+"expiration_date" =>
+"Verfallsdatum",
+
+"invalid_date_formatting" =>
+"Das angegebene Datum war nicht korrekt formatiert (Jahr-Monat-Tag Stunde:Minute)",
+
+"date_outside_of_range" =>
+"Das angegebene Datum ist nicht im zul&auml;ssigen Bereich (1902 - 2037).",
+
+"category" =>
+"Kategorie",
+
+"categories" =>
+"Kategorien",
+
+"no_categories" =>
+"Es gibt keine Kategorien",
+
+"title" =>
+"Titel",
+
+"url_title" =>
+"URL-Titel",
+
+"author" =>
+"Autor",
+
+"newline_format" =>
+"Format:",
+
+"none" =>
+"Keine",
+
+"link" =>
+"Link",
+
+"image" =>
+"Grafik",
+
+"email" =>
+"Email",
+
+"upload" =>
+"Upload",
+
+"file_upload" =>
+"Datei-Upload",
+
+"emoticons" =>
+"Smilies",
+
+"choose_a_destination_for_emoticon" =>
+"W&auml;hlen Sie ein Ziel f&uuml;r Ihren Smiley aus.",
+
+"click_emoticon" =>
+"Klicken Sie auf eine Grafik, um sie in Ihrem Eintrag zu platzieren.",
+
+"no_emoticons" =>
+"Ich kann das Verzeichnis mit Ihren Smilies nicht finden.",
+
+"close_all" =>
+"Alle schlie&szlig;en",
+
+"missing_title" =>
+"Ihr Weblogeintrag braucht einen Titel.",
+
+"missing_date" =>
+"Ihr Weblogeintrag braucht ein Datum.",
+
+"title_not_unique" =>
+"Es gibt bereits einen Eintrag mit diesem Titel. Titel m&uuml;ssen eindeutig sein.",
+
+"url_title_not_unique" =>
+"Es gibt bereits einen Eintrag mit diesem URL-Titel. URLs m&uuml;ssen eindeutig sein.",
+
+"entry_has_been_added" =>
+"Neuer Eintrag abgeschickt",
+
+"entry_has_been_updated" =>
+"Eintrag wurde aktualisiert",
+
+"trackback_url_errors" =>
+"Die folgenden Trackback-Pings wurden nicht akzeptiert:",
+
+"xmlrpc_ping_errors" =>
+"Die folgenden XML-RPC-Pings wurden nicht akzeptiert:",
+
+"click_to_view_your_entry" =>
+"Hier klicken, um Ihren Eintrag anzusehen",
+
+"view" =>
+"Ansehen",
+
+"edit_this_entry" =>
+"Eintrag bearbeiten",
+
+"date_range" =>
+"Datumsbereich",
+
+"today" =>
+"Heute",
+
+"past_week" =>
+"Letzte 7 Tage",
+
+"past_month" =>
+"Letzte 30 Tage",
+
+"past_six_months" =>
+"Letzte 180 Tage",
+
+"past_year" =>
+"Letzte 365 Tage",
+
+"any_date" =>
+"Jedes Datum",
+
+"results_per_page" =>
+"Resultate pro Seite",
+
+"order" =>
+"Reihenfolge",
+
+"ascending" =>
+"&Auml;lteste zuerst",
+
+"descending" =>
+"Neueste zuerst",
+
+"alpha" =>
+"Alphabetisch",
+
+"delete_confirm" =>
+"L&ouml;schen best&auml;tigen",
+
+"delete_entry_confirm" =>
+"Soll dieser Eintrag wirklich gel&ouml;scht werden?",
+
+"delete_entries_confirm" =>
+"Sollen diese Eintr&auml;ge wirklich gel&ouml;scht werden?",
+
+"entries_deleted" =>
+"Gel&ouml;scht",
+
+"html_buttons_no_cursor" =>
+"Um diese Buttons zu benutzen, m&uuml;ssen Sie zuerst den Cursor in ein Feld setzen.",
+
+"html_buttons_url_text" =>
+"Hyperlink-URL eingeben",
+
+"html_buttons_webpage_text" =>
+"Text des Links eingeben",
+
+"html_buttons_title_text" =>
+"Optional: Titel-Attribut eingeben",
+
+"html_buttons_image_text" =>
+"Grafik-URL eingeben",
+
+"html_buttons_image_text" =>
+"Grafik-URL eingeben",
+
+"html_buttons_email_text" =>
+"Emailadresse eingeben",
+
+"html_buttons_email_title" =>
+"Linktitel eingeben (oder leer lassen, um die Emailadresse als Link zu verwenden)",
+
+"html_buttons_enter_text" =>
+"Text eingeben, der formatiert werden soll",
+
+"warning" =>
+"Warnung",
+
+"file_exists" =>
+"Es gibt bereits eine Datei mit diesem Namen.",
+
+"overwrite_instructions" =>
+"Sie k&ouml;nnen entweder einen neuen Dateinamen w&auml;hlen, oder Sie schicken dieses Formular mit diesem Namen ab, und die alte Datei wird &uuml;berschrieben.",
+
+"select_destination_dir" =>
+"Zielverzeichnis ausw&auml;hlen",
+
+"file_type" =>
+"Dateityp",
+
+"image" =>
+"Grafik",
+
+"non_image" =>
+"Andere Datei",
+
+"success" =>
+"Erfolg!",
+
+"you_have_uploaded" =>
+"Sie haben diese Datei hochgeladen:",
+
+"choose_a_destination" =>
+"Wo soll diese Datei angezeigt werden?",
+
+"no_entry_fields" =>
+"In diesem Weblog gibt es keine Eintragsfelder.",
+
+"no_comments_or_trackbacks" =>
+"Es gibt f&uuml;r diesen Eintrag keine Kommentare oder Trackbacks.",
+
+"posted_by" =>
+"Autor:",
+
+"located_in" =>
+"Ort:",
+
+"comment_date" =>
+"Datum:",
+
+"comment_email" =>
+"Email:",
+
+"comment_url" =>
+"URL:",
+
+"comment_action" =>
+"Aktion:",
+
+"comment_ip" =>
+"IP:",
+
+"weblog_name" =>
+"Weblog:",
+
+"comment" =>
+"Kommentar",
+
+"missing_comment" =>
+"Sie m&uuml;ssen einen Kommentar abschicken.",
+
+"edit_comment" =>
+"Kommentar bearbeiten",
+
+"delete_comment" =>
+"Kommentar l&ouml;schen",
+
+"comment_deleted" =>
+"Kommentar gel&ouml;scht",
+
+"delete_trackback" =>
+"Trackback l&ouml;schen",
+
+"trackback_deleted" =>
+"Trackback gel&ouml;scht",
+
+"comment_updated" =>
+"Kommentar aktualisiert",
+
+"delete_comment_confirm" =>
+"Soll dieser Kommentar wirklich gel&ouml;scht werden?",
+
+"delete_trackback_confirm" =>
+"Soll dieser Trackback wirklich gel&ouml;scht werden?",
+
+"name" =>
+"Name",
+
+"url" =>
+"URL",
+
+"location" =>
+"Ort",
+
+"continue" =>
+"Weiter",
+
+"image_options" =>
+"Grafik-Optionen",
+
+"create_popup" =>
+"Popup f&uuml;r die Grafik erstellen?",
+
+"create_thumb" =>
+"Thumbnail erstellen",
+
+"constrain_proportions" =>
+"Proportionen einhalten",
+
+"thumb_settings" =>
+"Grafikeinstellungen",
+
+"choose_a_destination_for_thumb" =>
+"Wo soll das Bild angezeigt werden?",
+
+"do_not_place_file" =>
+"Datei nicht in den Eintrag setzen",
+
+"image_size_not_different" =>
+"Die angegebene Breite und H&ouml;he sind die gleichen wie im Original.",
+
+"width" =>
+"Breite",
+
+"height" =>
+"H&ouml;he",
+
+"pixels" =>
+"Pixels",
+
+"percent" =>
+"Prozent",
+
+"resize_image" =>
+"Gr&ouml;&szlig;e &auml;ndern",
+
+"create_thumb_copy" =>
+"Kopie anlegen",
+
+"resize_original" =>
+"Gr&ouml;&szlig;e des Originals &auml;ndern",
+
+"thumb_instructions" =>
+"Sie k&ouml;nnen die Gr&ouml;&szlig;e des hochgeladenen Bildes &auml;ndern oder ein separates Thumbnail erstellen.",
+
+"thumbnail_created" =>
+"Thumbnail erstellt",
+
+"image_resized" =>
+"Bildgr&ouml;&szlig;e ge&auml;ndert",
+
+"close_window" =>
+"Fenster schlie&szlig;en",
+
+// END
+''=>''
+);
+
+?>
