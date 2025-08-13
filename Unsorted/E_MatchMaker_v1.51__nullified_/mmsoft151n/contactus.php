4cd343e465e5482605bc7595892d8711ba011e11
me-suzy <me.suzana@gmail.com>
2025-08-13 04:33:24 +0300

    Add Unsorted/E_MatchMaker_v1.51__nullified_/mmsoft151n/contactus.php

---
+++ Unsorted/E_MatchMaker_v1.51__nullified_/mmsoft151n/contactus.php
@@ -0,0 +1,38 @@
+<?
+##############################################################################
+#                                                                            #
+#                              contactus.php                                 #
+#                                                                            #
+##############################################################################
+# PROGRAM : E-MatchMaker                                                     #
+# VERSION : 1.51                                                             #
+#                                                                            #
+# NOTES   : site using facetwoface layout and graphics                       #
+##############################################################################
+# All source code, images, programs, files included in this distribution     #
+# Copyright (c) 2001-2002                                                    #
+# Supplied by          : CyKuH [WTN]                                         #
+# Nullified by         : CyKuH [WTN]                                         #
+# Distribution:        : via WebForum and xCGI Forums File Dumps             #
+##############################################################################
+#                                                                            #
+#    While we distribute the source code for our scripts and you are         #
+#    allowed to edit them to better suit your needs, we do not               #
+#    support modified code.  Please see the license prior to changing        #
+#    anything. You must agree to the license terms before using this         #
+#    software package or any code contained herein.                          #
+#                                                                            #
+#    Any redistribution without permission of MatchMakerSoftware             #
+#    is strictly forbidden.                                                  #
+#                                                                            #
+##############################################################################
+?>
+<?
+
+include("siteconfig.php");
+$centerimage = "images/contact_big_red.gif";
+include("static/header.html");
+include("static/contactus.html");
+
+?>
+
