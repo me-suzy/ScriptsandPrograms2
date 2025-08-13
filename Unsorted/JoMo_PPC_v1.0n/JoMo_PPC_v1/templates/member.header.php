019af54bf783d1b5bb06c81595f29cf1f8cddc18
me-suzy <me.suzana@gmail.com>
2025-08-13 05:56:06 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/member.header.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/member.header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+        <TITLE><%$pageTitle%></TITLE>
+        <META http-equiv="Pragma" content="no-cache">
+        <META http-equiv="Cache-Control" content="no-cache, must-revalidate">
+        <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+</HEAD>
+
+<BODY BGCOLOR="white" MARGINHEIGHT=0 MARGINWIDTH=0 TOPMARGIN=0 LEFTMARGIN=0>
+
+<table width="100%" align="center">
+<tr align="center">
+<td>
