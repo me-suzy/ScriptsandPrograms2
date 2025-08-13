740dbfb1633225e325953e59c87f77bcf8f4c61a
me-suzy <me.suzana@gmail.com>
2025-08-13 06:30:38 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/en/options.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/en/options.php
@@ -0,0 +1,46 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[OPTIONS_Info] = "Please make any changes and click the update options button below.";
+
+$LOCALE[OPTIONS_Title] = "View or modify your preferences!";
+
+$LOCALE[OPTIONS_Msg_Enter] = "Enter message";
+
+$LOCALE[OPTIONS_Msg_Exit] = "Exit message";
+
+$LOCALE[OPTIONS_Msg_Away] = "Away message";
+
+$LOCALE[OPTIONS_Msg_Back] = "Back message";
+
+$LOCALE[OPTIONS_Color] = "Default text color";
+
+$LOCALE[OPTIONS_Smiles] = "Show smiley faces";
+
+$LOCALE[OPTIONS_Yes] = "YES";
+
+$LOCALE[OPTIONS_No] = "NO";
+
+$LOCALE[OPTIONS_Locate] = "Users can locate you";
+
+$LOCALE[OPTIONS_Upload] = "Users can send you files";
+
+$LOCALE[OPTIONS_Button] = "Update preferences";
+
+$LOCALE[OPTIONS_Complete] = "Your preferences have been updated!";
+
+$LOCALE[OPTIONS_Icons] = "Choose your personal icon.";
+
+$LOCALE[OPTIONS_Theme] = "Choose a theme.";
+
+?>
