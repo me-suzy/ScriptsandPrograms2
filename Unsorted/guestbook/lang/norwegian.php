abbc3442aae6ad4e839fba23365ada7870f5feef
me-suzy <me.suzana@gmail.com>
2025-08-13 05:01:39 +0300

    Add Unsorted/guestbook/lang/norwegian.php

---
+++ Unsorted/guestbook/lang/norwegian.php
@@ -0,0 +1,101 @@
+<?php
+# General
+
+$LANG["metatag"]    = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">";
+
+$LANG["BookMess1"]  = "HTML kode er deaktivert";
+$LANG["BookMess2"]  = "HTML kode er aktivert";
+$LANG["BookMess3"]  = "Signer gjesteboken";
+$LANG["BookMess4"]  = "tilbake til gjestebok";
+$LANG["BookMess5"]  = "Administrasjon";
+$LANG["BookMess6"]  = "Takk for at du stoppet på min hjemmeside, her kan du legge igjen en hilsen.";
+$LANG["BookMess7"]  = "kommentar";
+$LANG["BookMess8"]  = "Tilbake til redigering";
+$LANG["BookMess9"]  = "Skriv en komentar i gjesteboken.";
+$LANG["BookMess10"] = "<u>Takk for at du signerte gjesteboken.</u><br><br>Din hilsen er blitt lagt til! Du vil nå bli sendt tilbake til gjesteboken om 2 sekunder.";
+
+$LANG["EmailAdminSubject"] = "Ny hilsen i gjesteboken";
+$LANG["EmailGuestSubject"] = "Takk for at du signerte gjesteboken";
+
+$LANG["AltIP"]    = "IP logget";
+$LANG["AltICQ"]   = "Send ICQ melding";
+$LANG["AltEmail"] = "Send E-post";
+$LANG["AltUrl"]   = "se hjemmeside";
+$LANG["AltAim"]   = "Instant Messenger";
+$LANG["AltCom"]   = "Skriv en kommentar";
+
+# Form
+$LANG["FormMess1"]   = "Fyll ut de tomme boksene under for å signere gjesteboken. Etter at du lagrer hilsnen, vil du bli sendt tilbake til gjesteboken. Feltene med (*) MÅ fylles inn.";
+$LANG["FormMess2"]   = "Smil er PÅ/ON";
+$LANG["FormMess3"]   = "AGCode er PÅ/ON";
+$LANG["FormMess4"]   = "Vis legend";
+$LANG["FormMess5"]   = "Under er din gjestebok hilsen.";
+$LANG["FormMess6"]   = "AG kode er AV/OFF";
+$LANG["FormMess7"]   = "Smil er AV/OFF";
+$LANG["FormName"]    = "Navn";
+$LANG["FormEmail"]   = "E-post";
+$LANG["FormLoc"]     = "Sted";
+$LANG["FormPic"]     = "bilde";
+$LANG["FormUrl"]     = "Hjemmeside";
+$LANG["FormGender"]  = "Kjøn";
+$LANG["FormMale"]    = "Mann";
+$LANG["FormFemale"]  = "Dame";
+$LANG["FormMessage"] = "Din hilsen";
+$LANG["FormSelect"]  = "Gjestebok";
+$LANG["FormUser"]    = "Brukernavn";
+$LANG["FormPass"]    = "Passord";
+$LANG["FormReset"]   = "Reset";
+$LANG["FormSubmit"]  = "Lagre";
+$LANG["FormPreview"] = "Forhåndsvisning";
+$LANG["FormBack"]    = "Gå tilbake";
+$LANG["FormEnter"]   = "Gjestebok - Administrasjons senter<br>Før du kan fortsette må du skrive inn gyldig brukernavn og passord!";
+$LANG["FormButton"]  = "Gå";
+$LANG["FormPriv"]    = "Privat melding";
+
+# Navigation Bar
+$LANG["NavTotal"]   = "Totalt antall:";
+$LANG["NavRecords"] = "Antall hilsner per side:";
+$LANG["NavPrev"]    = "Forrige side";
+$LANG["NavNext"]    = "Neste side";
+
+# Post Errors
+$LANG["ErrorPost1"]  = "Du glemte å fylle inn Navn feltet. Du må fylle in dette feltet før du lagrer.";
+$LANG["ErrorPost2"]  = "Du glemte å fylle inn Melding feltet. Du må fylle in dette feltet før du lagrer.";
+$LANG["ErrorPost3"]  = "Din melding er enten for kort eller for lang, prøv igjen før du lagrer.";
+$LANG["ErrorPost4"]  = "En av felten er ikke korekt....";
+$LANG["ErrorPost5"]  = "Gjestebok - Error";
+$LANG["ErrorPost6"]  = "Bilde er for stort";
+$LANG["ErrorPost7"]  = "Feil bilde type";
+$LANG["ErrorPost8"]  = "Desverre! Vi har overflod kontroll på. Prøv igjen om en stund!";
+$LANG["ErrorPost9"]  = "Din IP daresse er ikke ønsket!";
+$LANG["ErrorPost10"] = "Din melding inneholder ord som ikke er ønsket. prøv igjen før du lagrer.";
+$LANG["ErrorPost11"] = "Du glemte å fylle inn Kommentar feltet, prøv igjen før du lagrer";
+
+$LANG["PassMess1"] = "Du må fylle inn et gyldig brukernavn og passord:";
+$LANG["PassMess2"] = "Ikke gyldig brukernavn eller passord.Prøv igjen.";
+$LANG["PassMess3"] = "Desverre, men passordet du har skrevet inn er ikke korrekt.";
+
+# Days
+$weekday[0] = "Søndag";
+$weekday[1] = "Mandag";
+$weekday[2] = "Tirsdag";
+$weekday[3] = "Onsdag";
+$weekday[4] = "Torsdag";
+$weekday[5] = "Fredag";
+$weekday[6] = "Lørdag";
+
+# Months
+$months[0]  = "Januar";
+$months[1]  = "Februar";
+$months[2]  = "Mars";
+$months[3]  = "April";
+$months[4]  = "Mai";
+$months[5]  = "Juni";
+$months[6]  = "Juli";
+$months[7]  = "August";
+$months[8]  = "September";
+$months[9]  = "Oktober";
+$months[10] = "November";
+$months[11] = "Desember";
+
+?>
