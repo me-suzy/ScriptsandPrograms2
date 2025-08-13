66ba14de3edad23ffc98f180af50be7b59f795ff
me-suzy <me.suzana@gmail.com>
2025-08-13 07:47:26 +0300

    Add Unsorted/PHP-Paidmail.v1.00.Nullified/PHP-Paidmail/includes/vars.php

---
+++ Unsorted/PHP-Paidmail.v1.00.Nullified/PHP-Paidmail/includes/vars.php
@@ -0,0 +1,34 @@
+<?
+       $admin_mail_id="you@you.com";   //Site Admin's email adds. for contact
+
+       //paths to folders
+       $url="http://url.com";
+       $path="/home/user/site/paidmail"; //example
+
+       $site_url="$url/site";   //virtual path of the site folder       
+       $site_path="$path/site";   //physical path of the site folder  
+       $siteadds="domain.com/paidmail";   //site address
+       $sitename="Yourdomain.com";   //site name
+
+       $dbhost = "localhost";   //Host of the DataBase
+       $dbname = "dbname";   //DB Name
+       $dbuser = "login";   //DB user name
+       $dbpwd = "pass";    //DB user password
+
+       $admin_url="$url/admin";   //virtual path of the admin folder
+       $admin_path="$path/admin";  //physical path of the admin folder
+       $include_path="$path/includes";  //physical path of the includes folder
+
+       $admin_html_path="$admin_path/html";  //virtual path of the admin html folder
+       $site_html_path="$site_path/html";   //virtual path of the site html folder
+       $banner_url="$url/images";   //virtual path of the images folder
+
+       $advt_regn_email_subject="Advertisement Purchased";  //Advertiser Registeration email message subject
+       $email_regn_subject="Login Details for $sitename.com"; //Member Registeration email message subject
+       $email_signup_subject="Signup Confirmation Email from $sitename.com"; //Sign up confirmation email subject
+
+       $email_contactus_subject="$sitename.com Contact Us Message";
+
+       $color_feedback_good = "green";   //normal feedback message
+       $color_feedback_bad = "red";    //error feedback message
+?>
