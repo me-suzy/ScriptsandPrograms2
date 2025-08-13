80cce98a3039c0e1e86665a5567d9551a2746aa4
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:13 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-pl-shrd-list.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-pl-shrd-list.php
@@ -0,0 +1,28 @@
+<?php
+
+##################################################
+
+# /pl-shrd-list.php
+
+##################################################
+
+define( "PLSHRD_SHRD_HEADER", "SHARED PLAYLIST LISTING" );
+define( "PLSHRD_SHRD_PLAY_HELP", "Click To Play This Playlist Now." );
+define( "PLSHRD_SHRD_VIEW_HELP", "Click To View This Playist's Detailed Information." );
+define( "PLSHRD_SHRD_BY", "By" );
+define( "PLSHRD_SHRD_FOR", "for" );
+
+define( "PLSHRD_TB_PREV", "Previous Page" );
+define( "PLSHRD_TB_PREV_HELP", "Click To Fetch The Previous Page Of Shared Playlists" );
+define( "PLSHRD_TB_NEXT", "Next Page" );
+define( "PLSHRD_TB_NEXT_HELP", "Click To Fetch The Next Page Of Shared Playlists" );
+define( "PLSHRD_TB_PAGE", "Page" );
+define( "PLSHRD_TB_PAGEBOT", "To Bottom" );
+define( "PLSHRD_TB_PAGEBOT_HELP", "Scrolls To The Bottom Of The Page" );
+define( "PLSHRD_TB_PAGETOP", "To Top" );
+define( "PLSHRD_TB_PAGETOP_HELP", "Scrolls To The Top Of The Page" );
+define( "PLSHRD_TB_PAGE_BTN", "View" );
+
+##################################################
+
+?>
