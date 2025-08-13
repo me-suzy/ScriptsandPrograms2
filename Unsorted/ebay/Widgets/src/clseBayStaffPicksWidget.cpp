c882274aee68c68024cd02a6520796f2fa7a8257
me-suzy <me.suzana@gmail.com>
2025-08-13 03:54:21 +0300

    Add Unsorted/ebay/Widgets/src/clseBayStaffPicksWidget.cpp

---
+++ Unsorted/ebay/Widgets/src/clseBayStaffPicksWidget.cpp
@@ -0,0 +1,117 @@
+/*	$Id: clseBayStaffPicksWidget.cpp,v 1.5 1999/03/07 08:15:17 josh Exp $	*/
+//
+//	File:	clseBayStaffPicksWidget.cpp
+//
+//	Class:	clseBayStaffPicksWidget
+//
+//	Author:	Poon
+//
+//	Function:
+//			Widget that shows staff-picked items using clseBayItemWidget.
+//
+// Modifications:
+//				- 10/14/97	Poon - Created
+//
+#include "widgets.h"
+#include "clseBayStaffPicksWidget.h"
+
+clseBayStaffPicksWidget::clseBayStaffPicksWidget(clsMarketPlace *pMarketPlace) :
+	clseBayItemWidget(pMarketPlace)
+{
+	mCatId = 0;
+}
+
+clseBayStaffPicksWidget::~clseBayStaffPicksWidget()
+{
+}
+
+void clseBayStaffPicksWidget::SetParams(vector<char *> *pvArgs)
+{
+	int p;
+	char *cArg;
+	char cArgCopy[256];
+	char *cName;
+	char *cValue;
+	bool handled = false;
+	int x;
+
+	// reverse through these so that deletions are safe.
+	//  stop at 1, because we don't care about the tagname
+	for (p=pvArgs->size()-1; p>=1; p--)
+	{
+		cArg = (*pvArgs)[p];
+		handled = false;
+
+		// separate the name from the value
+		strncpy(cArgCopy, cArg, sizeof(cArgCopy)-1);
+		cName = cArgCopy;
+		cValue = strchr(cArgCopy, '=');
+		if (cValue) 
+		{
+			cValue[0]='\0';		// lock in cName
+			cValue++;			// set cValue
+		}
+		else
+			cValue="";
+
+		// remove start & end quotes if they were provided
+		x = strlen(cValue);
+		if ((x>1) && (cValue[0]=='\"' && cValue[x-1]=='\"'))
+		{
+			cValue[x-1]='\0';		// remove ending "
+			cValue++;				// remove beginning "
+		}
+
+		// try to handle this parameter
+		if ((!handled) && (strcmp("categoryid", cName)==0))
+		{
+			SetCategoryId(atoi(cValue));
+			handled=true;
+		}
+
+		// if this parameter was handled, remove (and delete the char*) it from the vector
+		if (handled)
+		{
+			pvArgs->erase(pvArgs->begin()+p);	
+			delete [] cArg;	// don't need the parameter anymore
+		}
+	}
+
+	// ok, now pass the rest of the parameters up to the parent to handle
+	clseBayItemWidget::SetParams(pvArgs);
+
+}
+
+// Retrieve ids of all staff-picked items and stuff them into pvItemIds.
+void clseBayStaffPicksWidget::GetItemIds(vector<int> *pvItemIds)
+{
+	clsItems	*pItems = NULL;
+	time_t		CurrentTime;
+
+	// for stats reporting
+	time_t t;
+	char pDate[128];
+	char pTime[128];
+
+	CurrentTime = time(0);
+
+	if (mpMarketPlace) pItems = mpMarketPlace->GetItems();
+
+	if (mpLoggingStream)
+	{
+		t = time(0);
+		clsUtilities::GetDateAndTime(t, pDate, pTime);
+		*mpLoggingStream << pDate << " " << pTime << " Start Getting Staff Pick Item Ids for Category " << mCatId << "\n";
+	}
+
+	if (pItems) pItems->GetStaffPicksItemIds(pvItemIds, CurrentTime, mCatId);
+
+	if (mpLoggingStream)
+	{
+		t = time(0);
+		clsUtilities::GetDateAndTime(t, pDate, pTime);
+		*mpLoggingStream << pDate << " " << pTime << " End Getting Staff Pick Item Ids for Category " << mCatId << "\n\n";
+	}
+
+
+}
