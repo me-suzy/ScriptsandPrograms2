4a83c952529e591e1d557bae444cb359a2501f2b
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:30 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.rss.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.rss.php
@@ -0,0 +1,17 @@
+<?php
+$L = array(
+
+
+"rss_module_name" =>
+"RSS",
+
+"rss_module_description" =>
+"RSS genereer pagina module",
+
+"rss_invalid_weblog" =>
+"The weblog specified in your RSS feed does not exist.",
+
+
+''=>''
+);
+?>
