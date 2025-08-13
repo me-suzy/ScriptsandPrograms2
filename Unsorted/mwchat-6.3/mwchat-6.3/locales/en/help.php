d03da0be164ec081b91ba8d48aeb2559eca1df39
me-suzy <me.suzana@gmail.com>
2025-08-13 06:30:24 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/en/help.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/en/help.php
@@ -0,0 +1,205 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[HELP_Title] = "Help on";
+
+$LOCALE[HELP_Unavailable] = "A helpful description of this query is unavailable.";
+
+$LOCALE[HELP_Username] = "A name or username is the person you wish to be known as while in the chat. Similar to a nickname, this can be your real name or a made up one.";
+
+$LOCALE[HELP_Language] = "MWChat has the ability to display several different languages. This box allows you to choose whichever language you prefer to use.";
+
+$LOCALE[HELP_Password] = "Administrator, Operator, and registered accounts require a password to login. This prevents unauthorized access to your account.";
+
+$LOCALE[HELP_Lobby_Public] = "The public room list is a selection of various rooms you may chat in. Choose a room that intrests you most or create your own.";
+
+$LOCALE[HELP_Lobby_Private] = "The private room list is hidden to standard users. If you wish to chat in a private room, enter the name of the requested room below. Private rooms are not displayed on any room list, so you can hold private conversations without the uninvited users walking in on you.";
+
+$LOCALE[HELP_Lobby_Public_Create] = "If you cannot find a room that interests you in the list above, you can create your own room by typing the name of the requested room in the field below.";
+
+$LOCALE[HELP_Lobby_Private_Create] = "Enter the name of the private room you wish to enter. If the room doesn't exist, it will be created for you.";
+
+$LOCALE[HELP_Private_Message] = "Choose a user listed in this drop down box. Any messages you send will go only to the specifed user. To switch back to the entire room, choose the default dashes. Private messaging can also be done using the /msg command.";
+
+$LOCALE[HELP_Private_Message_Refresh] = "Click this link to refresh the private message user list.";
+
+$LOCALE[HELP_Colors] = "If you would like to change the color of the text you enter, click on any color in the color bar. You will see a message saying that your colors have been updated. You can click anywhere in the lower half of the color bar to restore the default color.";
+
+$LOCALE[HELP_Enter_Password] = "Enter a new password you would like to use. This password can be any alpha numeric text. Make sure you write this password down and keep it in a safe place. It is required that the password be five or more characters in length.";
+
+$LOCALE[HELP_Icons] = "Choose an icon from the list below to represent you in that chat. All users can see your icon.";
+
+$LOCALE[HELP_Theme] = "Choose a theme from the list below. A theme will change all your chat colors. You may need to return to the lobby, and then back into the room for all theme changes to take effect.";
+
+$LOCALE[HELP_Confirm_Password] = "Enter the new password you chose again here. This confirms that you haven't made a typo and ensures you know what your password is.";
+
+$LOCALE[HELP_YOB] = "This is the year of birth. This information is used to caculate age in some case, as some laws may require users to be of a certain again.";
+
+$LOCALE[HELP_Occupation] = "This is the users occupation or job. Usually their main source of income.";
+
+$LOCALE[HELP_Hobbies] = "This are things a user enjoys doing. Most of the time spent on hobbies is usually spare time.";
+
+$LOCALE[HELP_Location] = "This is the geographical location of the user.";
+
+$LOCALE[HELP_Comments] = "This is any comments or opinions the user may have or wish to voice.";
+
+$LOCALE[HELP_Status] = "This is the users current status. Usually stating if a user is away, online, registered, an administrator, or an operator.";
+
+$LOCALE[HELP_Upload_File] = "This is the file you want to send to a user. Click the browse button to choose the file you wish to send.";
+
+$LOCALE[HELP_Upload_User] = "This is the user you want to send a file to.";
+
+$LOCALE[HELP_Enter_Message] = "When you enter a room, this message will be sent along with the notice that you've entered the room.";
+
+$LOCALE[HELP_Exit_Message] = "When you exit a room, this message will be sent along with the notice that you've left the room.";
+
+$LOCALE[HELP_Away_Message] = "When you go away, this message will be sent along with the notice that your away.";
+
+$LOCALE[HELP_Back_Message] = "When your back from being away, this message will be sent along with the notice that your back.";
+
+$LOCALE[HELP_Default_Color] = "If you want a default color to be used every time you enter a room, this option will allow you to set it.";
+
+$LOCALE[HELP_Smiles] = "This option toggles your ability to see graphical or text smiley faces in the chat.";
+
+$LOCALE[HELP_Locate] = "This option toggles your ability to allow or disallow users to locate you. Locating a user tells what room you are currently in.";
+
+$LOCALE[HELP_Upload] = "This option toggles your ability to receive files from other users.";
+
+$LOCALE[HELP_Room] = "This is the room you are currently in, or the room you wish to create or enter.";
+
+$LOCALE[HELP_Room_Password] = "Some private rooms are password protected. If the room you wish to enter is password protected, enter it here.";
+
+$LOCALE[HELP_Number_Users] = "This is the total number of users that are currently in the room.";
+
+$LOCALE[HELP_Topic] = "This is the current topic of the room.";
+
+$LOCALE[HELP_Buddylist] = "This is your buddy list. This contains the usernames of all users in this room, and any buddies you may have. You can click on each name to get information about that user if they are registered.";
+
+$LOCALE[HELP_Serial_Number] = "This is the MWChat serial number. You can obtain a serial number at http://www.appindex.net/mwchat";
+
+$LOCALE[HELP_Storage] = "This is your file storage information. The first number shows how much space you have available for use. The second number shows what your total allowed storage is. Below is a list of any files you may have. Click on a filename to download that file, or click the check box to remove the file. You can also see the size of each file, when you received the file, and who sent you the file.";
+
+$LOCALE[HELP_ERROR_License] = "The license you have is incorrect, or doesn't exit. Retry your license and if it still doesn't work get a new serial number or contact Appindex.Net support.";
+
+$LOCALE[HELP_ERROR_AlreadyIn] = "Someone is already logged in with that username. It's possible if you didn't logout correctly, that your account hasn't idled out. Wait ten minutes and try again.";
+
+$LOCALE[HELP_ERROR_Logout] = "Your account has been logged out. You may login again if you like.";
+
+$LOCALE[HELP_ERROR_Expired] = "If you've been idle this could be why you were expired. You may have also been kicked out by an administrator or operator.";
+
+$LOCALE[HELP_ERROR_RoomLong] = "The room name you chose is too long. The maximum character limit it 15 characters.";
+
+$LOCALE[HELP_Live] = "The chat system is offline. This could be because an administrator is doing upgrades.";
+
+$LOCALE[HELP_ERROR_Profanity] = "You have been logged out for using profanity in the chat. Profanity is not allowed.";
+
+$LOCALE[HELP_ERROR_NoData] = "You did not correctly fill in all fields. Please try again.";
+
+$LOCALE[HELP_ERROR_BadData] = "The information you entered contained bad characters. Please use only alpha numeric characters.";
+
+$LOCALE[HELP_ERROR_Serial_Invalid] = "The serial number your provided doesn't match your server. You will need to get a new serial number and verify your webserver's IP address.";
+
+$LOCALE[HELP_ERROR_Serial_None] = "Appindex.Net does not have a serial number on record for you. Please register your product and get a new serial number.";
+
+$LOCALE[HELP_ERROR_Critical] = "An unknown critical error has occurred. If the problem persists, contact Appindex.Net support.";
+
+$LOCALE[HELP_ERROR_Javascript] = "MWChat requires a web browser capable of displaying javascript. Please enable javascript in your webbrowser or upgrade.";
+
+$LOCALE[HELP_ERROR_Browser] = "The web browser you are using in not compatible with MWChat. We recommend using Netscape or Internet Explorer.";
+
+$LOCALE[HELP_ERROR_Restricted] = "The username you chose is a restricted or reserved name. Please choose another name and login again.";
+
+$LOCALE[HELP_ERROR_BadPass] = "The password you supplied is incorrect. Check with an administrator for further details.";
+
+$LOCALE[HELP_ERROR_IP] = "MWChat detected that your IP address has changed. For security reasons, we have logged you out. Please login again.";
+
+$LOCALE[HELP_COMMAND_addbuddy] = "Add a buddy to your buddy list. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_admin] = "Allows administrators to unregister users or view logs. Specify the second option(username) to view logs for a specfic user or to unregister that user.";
+
+$LOCALE[HELP_COMMAND_afk] = "Changes your mode to away from keyboard. You may specify an optional away message.";
+
+$LOCALE[HELP_COMMAND_block] = "Blocks all private messages from any users.";
+
+$LOCALE[HELP_COMMAND_bug] = "Send a bug report.";
+
+$LOCALE[HELP_COMMAND_about] = "View information about MWChat and the creators.";
+
+$LOCALE[HELP_COMMAND_clear] = "Clears the current chat window.";
+
+$LOCALE[HELP_COMMAND_color] = "Set the text color to a color of your choice. This color must be in #000000 format.";
+
+$LOCALE[HELP_COMMAND_deop] = "Remove a users operators status. This command can only be used by administrators and operators.";
+
+$LOCALE[HELP_COMMAND_global] = "Send a message to all users in all rooms. This command can only be used by an administrator.";
+
+$LOCALE[HELP_COMMAND_help] = "Displays this document.";
+
+$LOCALE[HELP_COMMAND_ignore] = "Add a user to your ignore list. That user cannot send you private messages and you can not see messages they send. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_unignore] = "Remove a user from your ignore list. That user can now send you private messages and you can see messages they send. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_kick] = "Kick a user from the system. You may specify an optional reason. This command can only be used by administrators or operators.";
+
+$LOCALE[HELP_COMMAND_lobby] = "Returns you to the lobby. You may specify an optional exit message.";
+
+$LOCALE[HELP_COMMAND_locate] = "Locates a specified user. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_logout] = "Logs a user out of the system. You may specify an optional exit message.";
+
+$LOCALE[HELP_COMMAND_me] = "Describe what you are doing without referring to yourself directly.";
+
+$LOCALE[HELP_COMMAND_motd] = "Displays the message of the day.";
+
+$LOCALE[HELP_COMMAND_myfiles] = "Displays your online files.";
+
+$LOCALE[HELP_COMMAND_op] = "Give a user operator status. This command can only be used by administrators and operators.";
+
+$LOCALE[HELP_COMMAND_prefs] = "Displays your MWChat preferences.";
+
+$LOCALE[HELP_COMMAND_print] = "Displays your conversations in a printable window.";
+
+$LOCALE[HELP_COMMAND_msg] = "Send a private message to a user. Noone else can see this message.";
+
+$LOCALE[HELP_COMMAND_register] = "Registers the current username your using. Registering allows users to share files and create buddy lists.";
+
+$LOCALE[HELP_COMMAND_removebuddy] = "Removes a buddy from your buddy list. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_roll] = "Rolls the chat room dice. This is useful for online gaming.";
+
+$LOCALE[HELP_COMMAND_showbuddy] = "Displays all users on your buddy list. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_showignored] = "Displays all users on your ignore list. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_smiley] = "Displays all available smiley faces.";
+
+$LOCALE[HELP_COMMAND_topic] = "Changes the room topic. This command can only be used by administrators and operators.";
+
+$LOCALE[HELP_COMMAND_unafk] = "Changes your mode to back from away from keyboard. You may specify an optional back message.";
+
+$LOCALE[HELP_COMMAND_unblock] = "Allows you to accept private messages from all users again.";
+
+$LOCALE[HELP_COMMAND_topic] = "Removes a user from your ignore list. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_unregister] = "Unregisters your username. This will make it available for anyone to use. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_upload] = "Displays the file sharing interface. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_url] = "Allows you to insert a URL into the chat.";
+
+$LOCALE[HELP_COMMAND_whois] = "Displays information about the specified user. This command can only be used by registered users.";
+
+$LOCALE[HELP_COMMAND_Title] = "Available commands";
+
+$LOCALE[HELP_COMMAND_Type] = "{} indicates a required option, [] indicates an optional option.";
+
+?>
