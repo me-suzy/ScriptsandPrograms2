080eb94c7e26df8aaabb54556ee7bd25fde0d4b5
me-suzy <me.suzana@gmail.com>
2025-08-13 05:32:59 +0300

    Add Unsorted/intranet/upgrade0.2.8.sql

---
+++ Unsorted/intranet/upgrade0.2.8.sql
@@ -0,0 +1,13 @@
+ALTER TABLE timesheets ADD lunchmoin VARCHAR(5);
+ALTER TABLE timesheets ADD lunchmoout VARCHAR(5);
+ALTER TABLE timesheets ADD lunchtuin VARCHAR(5);
+ALTER TABLE timesheets ADD lunchtuout VARCHAR(5);
+ALTER TABLE timesheets ADD lunchwein VARCHAR(5);
+ALTER TABLE timesheets ADD lunchweout VARCHAR(5);
+ALTER TABLE timesheets ADD lunchthin VARCHAR(5);
+ALTER TABLE timesheets ADD lunchthout VARCHAR(5);
+ALTER TABLE timesheets ADD lunchfrin VARCHAR(5);
+ALTER TABLE timesheets ADD lunchfrout VARCHAR(5);
+ALTER TABLE timesheets ADD lunchwkendin VARCHAR(5);
+ALTER TABLE timesheets ADD lunchwkendout VARCHAR(5);
+
