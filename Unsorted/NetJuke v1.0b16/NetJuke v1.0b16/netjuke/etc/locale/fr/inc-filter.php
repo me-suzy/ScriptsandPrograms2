a63b5f5f6f7972639f85b4bb251431e556fd9c11
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:21 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-filter.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-filter.php
@@ -0,0 +1,30 @@
+<?php
+
+##################################################
+
+# /filter.php
+
+##################################################
+
+define( "FILTER_HEADER", "RÉSULTATS FILTRÉS" );
+define( "FILTER_ARTISTS", "Artistes" );
+define( "FILTER_ALBUMS", "Albums" );
+define( "FILTER_GENRES", "Genres" );
+define( "FILTER_FOUND", "Trouvés" );
+define( "FILTER_TRACKS", "Pistes" );
+
+define( "FILTER_LIST_ALL_HELP", "Lister Toutes Les Pistes Associées Dans Les Résultats Courants: " );
+define( "FILTER_PLAY_ALL_HELP", "Jouer Toutes Les Pistes Associées Avec Ce(t/s) " );
+define( "FILTER_FILTER_ALBUMS_HELP", "Lister tous les artistes figurant sur cet album." );
+define( "FILTER_FILTER_ARTISTS_HELP", "Lister tous les albums figurant cet artiste." );
+
+define( "FILTER_BACKBTN", "Retourner" );
+define( "FILTER_BACKBTN_HELP", "Retourner à la liste précédente" );
+define( "FILTER_PAGETOP", "Haut de Page" );
+define( "FILTER_PAGETOP_HELP", "Retourne au début de cette page" );
+define( "FILTER_CLOSEWIN", "Fermer La Fenêtre" );
+define( "FILTER_CLOSEWIN_HELP", "Ferme cette fenêtre." );
+
+##################################################
+
+?>
