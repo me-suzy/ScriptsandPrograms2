d561fae9219924ed16a53c6402249bce4347e423
me-suzy <me.suzana@gmail.com>
2025-08-13 05:09:30 +0300

    Add Unsorted/hitweb-3.0/odp/header.php

---
+++ Unsorted/hitweb-3.0/odp/header.php
@@ -0,0 +1,21 @@
+<html>
+<body bgcolor="#FFFFFF">
+
+<center>
+<table border=0 bgcolor="#669933" cellpadding=3 cellspacing=0 width=100%><tr><td valign=middle align=left>
+&nbsp;
+<img src="http://dmoz.org/img/odphead.gif" width=222 height=25 alt="Open Directory Project"
+align=middle></td></tr><tr><td bgcolor="#cccccc" align=right>
+<a href="http://dmoz.org/about.html"><b>about dmoz</b></a>
+|
+<a href="http://dmoz.org/add.html">add URL</a>
+|
+<a href="http://dmoz.org/cgi-bin/feedback.cgi?where=top">feedback</a>
+|
+<a href="http://dmoz.org/link.html">link</a>
+|
+<a href="http://dmoz.org/editors/">editor login</a>
+</font>
+</td></tr></table>
+</center>
+<p>
