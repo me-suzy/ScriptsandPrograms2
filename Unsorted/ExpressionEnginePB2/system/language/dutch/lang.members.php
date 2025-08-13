881d42de41cdb63a46a03e65e2ab2aabf0e6968d
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:12 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.members.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.members.php
@@ -0,0 +1,616 @@
+<?php
+$L = array(
+
+
+"can_moderate_comments" =>
+"Can Moderate Comments",
+
+"total_members" =>
+"Totaal aantal leden",
+
+"view_members" =>
+"Laat leden zien",
+
+"validate_selected" =>
+"Activeer de geselecteerde leden",
+
+"delete_selected" =>
+"Verwijder de geselecteerde leden",
+
+"send_email_notification" =>
+"Verzend een herinneringsemail naar ieder geactiveerd lid",
+
+"your_account_validated" =>
+"Uw account is geactiveerd",
+
+"your_account_ready" =>
+"Uw lidmaatschapsaccount is geactiveerd en klaar voor gebruik.",
+
+"1" =>
+"Dank u!",
+
+"no_members_to_validate" =>
+"Er zijn geen leden die wachten op activering van hun account",
+
+"members_are_validated" =>
+"Leden zijn geactiveerd",
+
+"members_are_deleted" =>
+"Leden zijn verwijderd",
+
+"ascending" =>
+"Datum - oudste eerst",
+
+"descending" =>
+"Datum - nieuwste eerst",
+
+"alpha" =>
+"Alfabetisch oplopend ",
+
+"alpha_desc" =>
+"Alfabetisch aflopend",
+
+"username_asc" =>
+"Gebruikersnaam - oplopend",
+
+"username_desc" =>
+"Gebruikersnaam - aflopend",
+
+"screen_name_asc" =>
+"Schermnaam - oplopend",
+
+"screen_name_desc" =>
+"Schermnaam - aflopend",
+
+"email_asc" =>
+"Email - oplopend",
+
+"email_desc" =>
+"Email - aflopend",
+
+"sort_order" =>
+"Sorteringsvolgorde",
+
+"join_date" =>
+"Datum toelating",
+
+"last_visit" =>
+"Laatste bezoek",
+
+"member_group" =>
+"Groep",
+
+"group_id" =>
+"Groep ID",
+
+"no_members_matching_that_criteria" =>
+"Er zijn geen leden die aan uw voorwaarden voldoen",
+
+"mbrs" =>
+"Leden",
+
+"edit_profile" =>
+"Bewerk profiel",
+
+"register_member" =>
+"Registreer een nieuw lid",
+
+"edit_group" =>
+"Bewerk groep",
+
+"group_title" =>
+"Groepnaam",
+
+"security_lock" =>
+"Beveiliging",
+
+"group_lock" =>
+"Leden Groep Beveiliging",
+
+"enable_lock" =>
+"Schakel beveiliging van de groep in",
+
+"locked" =>
+"Gesloten",
+
+"lock_description" =>
+"Indien een groep gesloten is kan alleen de Super Admin iemand toelaten tot deze groep. Als u een andere gebruiker toestaat leden toe te laten, is het aanbevolen dat u ALLEEN groepen vrijgeeft waarvan u wilt dat andere leden toegelaten worden.",
+
+"unlocked" =>
+"Vrijgegeven",
+
+"create_new_member_group" =>
+"Maak een nieuwe groep",
+
+"member_has_cp_access" =>
+"heeft toegang tot het Control Panel ",
+
+"edit_member_group" =>
+"Bewerk groep",
+
+"create_member_group" =>
+"Maak een groep",
+
+"delete_member_group" =>
+"Verwijder groep",
+
+"member_group_deleted" =>
+"Groep verwijderd",
+
+"delete_member_group_confirm" =>
+"Weet u zeker dat u deze groep wilt verwijderen?",
+
+"delete_member" =>
+"Verwijder lid",
+
+"delete_member_confirm" =>
+"Weet u zeker dat u dit lid wilt verwijderen?",
+
+"delete_members_confirm" =>
+"Weet u zeker dat u deze leden wilt verwijderen?",
+
+"can_not_delete_superadmin" =>
+"U kunt het eerste lid niet verwijderen",
+
+"member_deleted" =>
+"Lid verwijderd",
+
+"members_deleted" =>
+"Leden verwijderd",
+
+"user_banning" =>
+"Gebruiker is toegang ontzegd",
+
+"ban_preferences_updated" =>
+"Ontzeggingseigenschappen aangepast",
+
+"ip_address_banning" =>
+"IP-adres is toegang ontzegd",
+
+"ip_banning_instructions" =>
+"Zet elk IP-adres op een andere regel",
+
+"ip_banning_instructions_cont" =>
+"Gebruik een asterisk voor gedeeltelijke IP-adressen. Bijvoorbeeld: 123.45.*",
+
+"email_address_banning" =>
+"Emailadressen waarvan toegang ontzegd is",
+
+"email_banning_instructions" =>
+"Zet elk emailadres op een andere regel",
+
+"email_banning_instructions_cont" =>
+"Gebruik een asterisk voor gedeeltelijke emailadressen. Voorbeeld: *@domein.com",
+
+"ban_options" =>
+"Wanneer een IP-adres deze site bezoekt waarvan de toegang ontzegd is",
+
+"restrict_to_viewing" =>
+"Beperk ze om alleen te bekijken",
+
+"show_this_message" =>
+"Laat dit bericht zien",
+
+"send_to_site" =>
+"Verstuur ze naar deze site",
+
+"username_banning" =>
+"Beperking gebruikersnaam",
+
+"username_banning_instructions" =>
+"Zet elke gebruikersnaam op een nieuwe regel",
+
+"screen_name_banning" =>
+"Beperking schermnamen",
+
+"screen_name_banning_instructions" =>
+"Plaats elke schermnaam op een aparte regel",
+
+"only_superadmins_can_admin_groups" =>
+"Alleen Super Admins kunnen de Leden Groep pagina bezoeken",
+
+"create_group_based_on_old" =>
+"Maak een nieuwe Groep gebasseerd op een reeds bestaande",
+
+"group_name" =>
+"Naam van Leden Groep",
+
+"missing_group_title" =>
+"De Leden Groep met een naam hebben",
+
+"site_access" =>
+"Site Toegang",
+
+"can_view_online_system" =>
+"Kan de website bekijken wanneer hij online is",
+
+"can_view_offline_system" =>
+"Kan de website bekijken wanneer hij offline is",
+
+"mbr_profile_privs" =>
+"Leden Profiel Privileges",
+
+"can_view_profiles" =>
+"Kan publieke Profielen bekijken",
+
+"commenting_privs" =>
+"Reactie plaatsen privileges",
+
+"can_post_comments" =>
+"Kan reacties toevoegen",
+
+"cp_weblog_post_privs" =>
+"Weblog Toewijzing",
+
+"cp_module_access_privs" =>
+"Module Toegang Privileges",
+
+"can_access_mod" =>
+"Kan de volgende modules bekijken:",
+
+"global_cp_access" =>
+"Control Panel Toegang",
+
+"can_access_cp" =>
+"Toegang tot het Control Panel ?",
+
+"cp_section_access" =>
+"Controlepaneel Toegang",
+
+"can_access_publish" =>
+"Toegang tot de TOEVOEGEN pagina ",
+
+"can_access_edit" =>
+"Toegang tot de WIJZIGEN pagina ",
+
+"can_access_design" =>
+"Toegang tot de SJABLONEN pagina ",
+
+"can_access_comm" =>
+"Toegang tot de COMMUNICATIE pagina ",
+
+"can_access_modules" =>
+"Toegang tot de MODULES pagina ",
+
+"can_access_admin" =>
+"Toegang tot de ADMIN pagina ",
+
+"can_ban_users" =>
+"Kan gebruikers bannen",
+
+"cp_admin_privs" =>
+"Admin rechten in Control Panel",
+
+"cp_comment_privs" =>
+"Reactie rechten in Control Panel",
+
+"can_view_other_comments" =>
+"Kan reacties bekijken in berichten van andere auteurs.",
+
+"can_edit_own_comments" =>
+"Kan reacties wijzigen in eigen berichten",
+
+"can_delete_own_comments" =>
+"Kan reacties verwijderen in eigen berichten",
+
+"can_edit_all_comments" =>
+"Kan reacties wijzigen in ALLE berichten",
+
+"can_delete_all_comments" =>
+"Kan reacties verwijderen in ALLE berichten",
+
+"can_admin_weblogs" =>
+"Admin rechten voor weblogs en weblog voorkeuren",
+
+"can_admin_members" =>
+"Admin rechten voor leden accounts",
+
+"can_delete_members" =>
+"Kan leden verwijderen",
+
+"can_admin_mbr_groups" =>
+"Kan de groep waar een lid aan toegewezen is wijzigen (Alleen vrijgegeven groepen)",
+
+"can_admin_mbr_templates" =>
+"Admin rechten voor leden profiel sjabonen",
+
+"can_admin_utilities" =>
+"Admin rechten voor systeem programma&#39;s",
+
+"can_admin_templates" =>
+"Admin rechten voor sjabloongroepen ",
+
+"can_admin_preferences" =>
+"Admin rechten voor algemene voorkeuren",
+
+"can_admin_modules" =>
+"Kan modules installeren/verwijderen",
+
+"cp_email_privs" =>
+"Control Panel Email Privileges",
+
+"can_send_email" =>
+"Kan email versturen via het Control Panel",
+
+"can_email_members" =>
+"Kan email versturen naar individuele leden",
+
+"can_email_member_groups" =>
+"Kan email versturen naar Leden Groepen",
+
+"can_email_mailinglist" =>
+"Kan email versturen naar Mailinglijst",
+
+"can_send_cached_email" =>
+"Kijk gecachte emails bekijken/versturen",
+
+"warning" =>
+"WAARSCHUWING:",
+
+"super_admin_edit_note" =>
+"Let Op: U kunt alleen de naam van de Super Admin groep wijzigen",
+
+"be_careful_assigning_groups" =>
+"Wees VERSCHRIKKELIJK voorzichtig met toekennen van groepsvoorkeuren - vooral de gemarkeerde.",
+
+"member_group_updated" =>
+"Leden Groep Geupdatet:",
+
+"member_group_created" =>
+"Leden Groep aangemaakt:",
+
+"cp_weblog_privs" =>
+"Berichten plaatsen en wijzigen Privileges",
+
+"can_view_other_entries" =>
+"Kan berichten bekijken geschreven door anderen",
+
+"can_post_in" =>
+"Kan berichten plaatsen en wijzigen in:",
+
+"can_edit_other_entries" =>
+"Kan berichten wijzigen geschreven door anderen",
+
+"can_assign_post_authors" =>
+"Kan de naam van de auteur veranderen bij plaatsen van nieuwe berichten",
+
+"can_delete_self_entries" =>
+"Kan eigen berichten verwijderen",
+
+"can_delete_all_entries" =>
+"Kan ALLE berichten verwijderen",
+
+"cp_template_access_privs" =>
+"Sjabloon wijzigen Privileges",
+
+"can_access_tg" =>
+"Kan Sjablonen wijzigen in deze groep:",
+
+"can_email_from_profile" =>
+"Kan email versturen naar andere leden via email beheerpaneel in profiel",
+
+"for_profile_page" =>
+"Zichtbaar in leden profiel pagina",
+
+"custom_member_fields" =>
+"Custom Profiel Velden",
+
+"current_fields" =>
+"Leden Profiel Velden",
+
+"edit_member_field" =>
+"Wijzig Veld",
+
+"create_member_field" =>
+"Maak Veld",
+
+"field_updated" =>
+"Veld Geupdatet",
+
+"create_new_profile_field" =>
+"Maak een nieuw Profiel Veld",
+
+"is_field_public" =>
+"Is dit veld zichtbaar in de publieke profielen?",
+
+"is_field_public_cont" =>
+"Als u kiest voor nee, zal het alleen zichtbaar zijn voor admins",
+
+"is_field_reg" =>
+"Is dit veld zichtbaar in de registratie pagina?",
+
+"field_width" =>
+"Breedte van Veld",
+
+"field_width_cont" =>
+"Kan in pixels of procenten zijn",
+
+"fieldname" =>
+"Veldnaam",
+
+"fieldname_cont" =>
+"Enkel woord, geen spaties. Underscores en platte streepjes zijn toegestaan",
+
+"field_format" =>
+"Tekst Opmaak",
+
+"max_length" =>
+"Maximale lengte",
+
+"max_length_cont" =>
+"Voor tekst veld types",
+
+"text_area_rows" =>
+"Textarea Rijen",
+
+"text_area_rows_cont" =>
+"Voor textarea veld types",
+
+"pull_down_items" =>
+"Selecteer List opties",
+
+"pull_down_items_cont" =>
+"Voor drop-down menu&#39;s",
+
+"pull_down_instructions" =>
+"Zet elk item op een parte lijn",
+
+"invalid_characters" =>
+"De veldnaam die u invoerde bevat onjuiste tekens",
+
+"member_data_will_be_deleted" =>
+"Alle data van de leden in dit veld zal permanent verwijderd worden",
+
+"profile_field_deleted" =>
+"Profiel Veld verwijderd:",
+
+"no_custom_profile_fields" =>
+"Er zijn op dit moment geen custom profiel velden",
+
+"member_search" =>
+"Leden Zoeken",
+
+"member_search_results" =>
+"Resultaten Leden Zoeken",
+
+"no_search_results" =>
+"Er zijn geen resultaten gevonden",
+
+"member_search_instructions" =>
+"Vul tenminste 1 veld in. U kunt hele woorden of delen daarvan invoeren.",
+
+"can_not_delete_only_member" =>
+"U kunt niet het enige lid in de database verwijderen",
+
+"can_not_delete_super_admin" =>
+"U kunt niet een Super Admin verwijderen zonder dat er een andere Super Admin is.",
+
+"must_be_superadmin_to_delete_one" =>
+"U moet Super Admin zijn om een Super Admin te kunnen verwijderen",
+
+"email_console_log" =>
+"Email Beheerpaneel Logs",
+
+"no_cached_email" =>
+"Er zijn geen gecachte email berichten.",
+
+"email_title" =>
+"Email titel",
+
+"from" =>
+"Van",
+
+"to" =>
+"Aan",
+
+"message_sent_to" =>
+"Bericht Verstuurd aan:",
+
+"email_deleted" =>
+"Email Bericht(en) Verwijderd",
+
+"template_set_names" =>
+"Beschikbare Sjablonen Sets",
+
+"unable_to_find_templates" =>
+"Niet mogelijk om het leden profiel sjablonen te lokaliseren",
+
+"unable_to_find_template_file" =>
+"Niet mogelijk om het leden profiel sjabloon te lokaliseren",
+
+"template_updated" =>
+"Sjabloon is geupdatet",
+
+"template_set" =>
+"Sjabloon Set",
+
+"file_not_writable" =>
+"Let op: Dit bestand is niet beschrijfbaar",
+
+"error_opening_template" =>
+"Error: Niet mogelijk om deze sjabloon te openen om inhoud weg te schrijven.",
+
+"file_writing_instructions" =>
+"U kunt geen veranderingen aanbrengen in deze sjabloon totdat u de rechten van het bestand op 666 zet:",
+
+"aim_console" =>
+"AOL Instant Messenger Beheerpaneel",
+
+"breadcrumb" =>
+"Breadcrumb",
+
+"copyright" =>
+"Copyright Bericht",
+
+"custom_profile_fields" =>
+"Custom Profiel Veld Formulier",
+
+"edit_profile_form" =>
+"Leden Profiel Formulier",
+
+"email_form" =>
+"Email Beheerpaneel ",
+
+"email_prefs_form" =>
+"Email Voorkeuren Formulier",
+
+"email_user_message" =>
+"Email Berichten",
+
+"forgot_form" =>
+"Wachtwoord Vergeten Formulier",
+
+"home_page" =>
+"Leden Profiel Homepage",
+
+"icq_console" =>
+"ICQ Beheerpaneel",
+
+"localization_form" =>
+"Localization Voorkeuren Formulier",
+
+"login_form" =>
+"Login Formulier",
+
+"memberlist" =>
+"Leden Lijst Pagina",
+
+"memberlist_rows" =>
+"Leden Lijst Rijen",
+
+"menu" =>
+"Leden Profiel Menu",
+
+"notepad_form" =>
+"Kladblok Formulier",
+
+"password_change_warning" =>
+"Wachtwoord Veranderen Waarschuwing",
+
+"public_custom_profile_fields" =>
+"Custom Profiel Velden Rijen",
+
+"public_profile" =>
+"Publieke Leden Profiel",
+
+"registration_form" =>
+"Registratie Formulier",
+
+"stylesheet" =>
+"CSS Stylesheet",
+
+"success" =>
+"Profiel Update Bericht",
+
+"username_change_disallowed" =>
+"Gebruikersnaam veranderen &quot;niet toegestaan&quot; Bericht",
+
+"username_password_form" =>
+"Gebruikersnaam en Wachtwoord Formulier",
+
+"username_row" =>
+"Gebruikersnaam Formulier Rij",
+
+''=>''
+);
+?>
