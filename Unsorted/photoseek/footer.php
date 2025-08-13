d07b903dba29248175d96728784d700c521dbab8
me-suzy <me.suzana@gmail.com>
2025-08-13 07:41:44 +0300

    Add Unsorted/photoseek/footer.php

---
+++ Unsorted/photoseek/footer.php
@@ -0,0 +1,29 @@
+<!-- beginning of footer -->
+
+<P>
+<TABLE WIDTH=100% BGCOLOR=#aaaaaa CELLSPACING=0 CELLPADDING=3
+ BORDER=0>
+ <TR>
+ <TD BGCOLOR=#ffffff>&nbsp;</TD>
+ <TD BGCOLOR=#eeeeee>&nbsp;</TD>
+ <TD BGCOLOR=#d5d5d5>&nbsp;</TD>
+ <TD BGCOLOR=#cccccc>&nbsp;</TD>
+ <TD BGCOLOR=#bbbbbb>&nbsp;</TD>
+ <TD ALIGN=CENTER VALIGN=MIDDLE>
+  <CENTER>
+   <FONT SIZE=-1 FACE="Arial, Verdana" COLOR=#555555>
+    &copy; 2000 under the GNU Public License, v2
+   </FONT>
+  </CENTER>
+ </TD>
+ <TD BGCOLOR=#bbbbbb>&nbsp;</TD>
+ <TD BGCOLOR=#cccccc>&nbsp;</TD>
+ <TD BGCOLOR=#d5d5d5>&nbsp;</TD>
+ <TD BGCOLOR=#eeeeee>&nbsp;</TD>
+ <TD BGCOLOR=#ffffff>&nbsp;</TD>
+ </TR>
+</TABLE>
+<P>
+
+</BODY>
+</HTML>
