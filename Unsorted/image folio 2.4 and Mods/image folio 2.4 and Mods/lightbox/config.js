c1bde4dc52b7e557cc95bdfc44073e802ebf0745
me-suzy <me.suzana@gmail.com>
2025-08-13 05:22:18 +0300

    Add Unsorted/image folio 2.4 and Mods/image folio 2.4 and Mods/lightbox/config.js

---
+++ Unsorted/image folio 2.4 and Mods/image folio 2.4 and Mods/lightbox/config.js
@@ -0,0 +1,34 @@
+// http://alex.nezabudka.ru/lightbox/config.js
+// Please don't edit this file manually.
+// First edit config.pl and lightbox_config.pl then run lightbox_build.cgi in your
+// browser. This file (config.js) is needed so the lightbox knows your settings.
+site_title='Gallery';
+imageurl='http://alex.nezabudka.ru/gallery';
+imagefolio_url='http://alex.nezabudka.ru/cgi-bin/gallery/imageFolio.cgi';
+homelink='http://alex.nezabudka.ru/cgi-bin/gallery/imageFolio.cgi';
+if_images_directory='http://alex.nezabudka.ru/gallery';
+icons_dir='http://alex.nezabudka.ru/gallery/icons';
+lightbox_scripts_url='http://alex.nezabudka.ru/cgi-bin/gallery/if_lightbox';
+lightbox_url='http://alex.nezabudka.ru/lightbox';
+lightbox_send_url='http://alex.nezabudka.ru/cgi-bin/gallery/if_lightbox/lightbox_send.cgi';
+lightbox_zip_url='http://alex.nezabudka.ru/cgi-bin/gallery/if_lightbox/lightbox_zip.cgi';
+mypattern='tn_';
+table_background_color='#eeeeee';
+font='font face="Verdana,Arial,Helvetica" color="#000000" size=1';
+columns=4;
+thumb_table_bgcolor='#ffffff';
+thumb_table_border_color='#cccccc';
+image_alignment='center';
+alt_text='Download';
+slideshow='1';
+emailsave='1';
+downloadsave='1';
+lightbox_name='Lightbox';
+lightbox_text='lightbox';
+lightbox_yes_gif='lightbox_yes.gif';
+lightbox_no_gif='lightbox_no.gif';
+lightbox_clear_gif='lightbox_clear.gif';
+lightbox_view_gif='lightbox_view.gif';
+slideshow_window_height='440';
+slideshow_window_width='550';
+cookiename='lightbox';
