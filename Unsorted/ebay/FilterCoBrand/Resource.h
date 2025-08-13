6f9bc4501ed593f70a8d100d7fd76cf0325d7712
me-suzy <me.suzana@gmail.com>
2025-08-13 03:14:10 +0300

    Add Unsorted/ebay/FilterCoBrand/Resource.h

---
+++ Unsorted/ebay/FilterCoBrand/Resource.h
@@ -0,0 +1,18 @@
+/*	$Id: Resource.h,v 1.2 1999/02/21 02:21:55 josh Exp $	*/
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by cobrand.rc
+//
+#define IDS_FILTER                      101
+#define IDD_DIALOG1                     103
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         103
+#define _APS_NEXT_SYMED_VALUE           103
+#endif
+#endif
