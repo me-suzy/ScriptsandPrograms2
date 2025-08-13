710437307c28f6fb62de45825a63d48a043853cd
me-suzy <me.suzana@gmail.com>
2025-08-13 04:57:04 +0300

    Add Unsorted/GMail-3.0.0-en/admin/ibanner.php

---
+++ Unsorted/GMail-3.0.0-en/admin/ibanner.php
@@ -0,0 +1,62 @@
+<? include("../config.inc.php"); ?>
+<html>
+<head>
+ <title>Adminbereich</title>
+ <SCRIPT language=javascript>
+  <!--
+
+   function LmOver(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+   function LmOut(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+   function LmDown(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+  //-->
+ </SCRIPT>
+ <style> 
+  <!--  
+   a:link     { text-decoration: none      }
+   a:hover    { text-decoration: underline }
+   a:active   { text-decoration: none      }
+   a:visited  { text-decoration: none      }
+
+   a.dl:link     { color: #ffffff; text-decoration: none      }
+   a.dl:hover    { color: #ffffff; text-decoration: underline }
+   a.dl:active   { color: #ffffff; text-decoration: none      }
+   a.dl:visited  { color: #ffffff; text-decoration: none      }
+  -->
+ </style>
+</head>
+
+<body bgcolor="#ffffff">
+<center>
+ <table width="60%" bgcolor="#003366" cellspacing="1">
+  <tr>
+    <td background="table.gif" width="100%"><font face="arial" size="2" color="#ffffff"><b>&nbsp;Ad-Managment: Add banner</b></font></td>
+  </tr>
+  <tr>
+    <td bgcolor="#336699" width="100%" align="center"><font face="arial" size="2" color="#ffffff"><br>The banner was added successfuly.
+<br>
+<?
+
+$verbindung=mysql_connect($sql_server,$sql_user,$sql_passwort);
+if (!$verbindung)
+echo "Es konnte keine Datenbankverbindung hergestellt werden.";
+
+mysql_select_db($sql_db, $verbindung);
+$sql = "INSERT INTO b1gmail_banner(code) VALUES ('$code')";
+$ergebnis = mysql_query($sql, $verbindung);
+
+mysql_close($verbindung);
+
+?>
+<br>
+</font></td>
+  </tr>
+ </table>
+</center>
+</body>
+</html>
