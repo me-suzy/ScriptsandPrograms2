50e8a28e0fcbff33e40269b750ff9f3cf6b41708
me-suzy <me.suzana@gmail.com>
2025-08-13 05:01:43 +0300

    Add Unsorted/guestbook/lang/russian.php

---
+++ Unsorted/guestbook/lang/russian.php
@@ -0,0 +1,101 @@
+<?php
+# General
+
+$LANG["metatag"]    = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1251\">";
+
+$LANG["BookMess1"]  = "HTML êîä ÍÅ ÄÎÑÒÓÏÅÍ";
+$LANG["BookMess2"]  = "HTML êîä ÄÎÑÒÓÐÏÅÍ";
+$LANG["BookMess3"]  = "ÄÎÁÀÂÈÒÜ ÊÎÌÌÅÍÒÀÐÈÉ";
+$LANG["BookMess4"]  = "ÂÅÐÍÓÒÜÑß Â ÃÎÑÒÅÂÓÞ";
+$LANG["BookMess5"]  = "[A]";
+$LANG["BookMess6"]  = "";
+$LANG["BookMess7"]  = "Êîììåíòàðèè";
+$LANG["BookMess8"]  = "Âåðíóòüñÿ ê çàïîëíåíèþ ôîðìû";
+$LANG["BookMess9"]  = "Ïðîêîììåíòèðîâàòü ýòó çàïèñü.";
+$LANG["BookMess10"] = "<u>Ñïàñèáî çà äîáàâëåíèå çàïèñè â íàøó Ãîñòåâóþ êíèãó.</u><br><br>Ïîäîæäèòå íåñêîëüêî ñåêóíä.";
+
+$LANG["EmailAdminSubject"] = "Íîâûé êîììåíòàðèé";
+$LANG["EmailGuestSubject"] = "Ñïàñèáî çà äîáàâëåíèå çàïèñè â íàøó Ãîñòåâóþ";
+
+$LANG["AltIP"]    = "IP àâòîðà";
+$LANG["AltICQ"]   = "Ïîñëàòü íà ICQ ñîîáùåíèå";
+$LANG["AltEmail"] = "Íàïèñàòü E-mail";
+$LANG["AltUrl"]   = "Ïîñåòèòü Web Page àâòîðà";
+$LANG["AltAim"]   = "Instant Messenger";
+$LANG["AltCom"]   = "Ïðîêîììåíòèðîâàòü";
+
+# Form
+$LANG["FormMess1"]   = "Ïîæàëóéñòà, çàïîëíèòå ôîðìó. Ïîëÿ ïîìå÷åíûå (*) ÎÁßÇÀÒÅËÜÍÛ äëÿ çàïîëíåíèÿ!";
+$LANG["FormMess2"]   = "Smilies ÄÎÑÒÓÏÍÛ";
+$LANG["FormMess3"]   = "AGCode ÄÎÑÒÓÏÅÍ";
+$LANG["FormMess4"]   = "_?_";
+$LANG["FormMess5"]   = "Íèæå - Âàøà çàïèñü.";
+$LANG["FormMess6"]   = "AGCode ÍÅ ÄÎÑÒÓÏÅÍ";
+$LANG["FormMess7"]   = "Smilies ÍÅ ÄÎÑÒÓÏÍÛ";
+$LANG["FormName"]    = "Èìÿ";
+$LANG["FormEmail"]   = "E-mail";
+$LANG["FormLoc"]     = "Ìåñòîíàõîæäåíèå";
+$LANG["FormPic"]     = "Âñòàâèòü ðèñóíîê";
+$LANG["FormUrl"]     = "Homepage";
+$LANG["FormGender"]  = "Ïîë";
+$LANG["FormMale"]    = "Ìóæñêîé";
+$LANG["FormFemale"]  = "Æåíñêèé";
+$LANG["FormMessage"] = "Âàøå ñîîáùåíèå";
+$LANG["FormSelect"]  = "Ãîñòåâàÿ êíèãà";
+$LANG["FormUser"]    = "Èìÿ";
+$LANG["FormPass"]    = "Ïàðîëü";
+$LANG["FormReset"]   = "Î÷èñòèòü";
+$LANG["FormSubmit"]  = "   Îê   ";
+$LANG["FormPreview"] = "Ïðîñìîòð";
+$LANG["FormBack"]    = "Íàçàä";
+$LANG["FormEnter"]   = "Öåíòð óïðàâëåíèÿ<br>Ïðåæäå ÷åì âîéòè, ââåäèòå èìÿ è ïàðîëü!";
+$LANG["FormButton"]  = " Îê ";
+$LANG["FormPriv"]    = "×àñòíîå ñîîáùåíèå";
+
+# Navigation Bar
+$LANG["NavTotal"]   = "Âñåãî çàïèñåé:";
+$LANG["NavRecords"] = "×èñëî çàïèñåé íà ñòðàíèöå:";
+$LANG["NavPrev"]    = "Ïðåäûäóùàÿ ñòðàíèöà";
+$LANG["NavNext"]    = "Ñëåäóþùàÿ ñòðàíèöà";
+
+# Post Errors
+$LANG["ErrorPost1"]  = "Âû çàáûëè ââåñòè ñâîå èìÿ. Ïîæàëóéñòà, ïîâòîðèòå ââîä.";
+$LANG["ErrorPost2"]  = "Âû çàáûëè ââåñòè ñîîáùåíèå. Ïîæàëóéñòà, ïîâòîðèòå ââîä.";
+$LANG["ErrorPost3"]  = "Âàøå ñîîáùåíèå ñëèøêîì êîðîòêîå èëè äëèííîå. Ïîæàëóéñòà, ïîâòîðèòå ââîä.";
+$LANG["ErrorPost4"]  = "Îäíî èç ïîëåé çàïîëíåíî íåâåðíî.";
+$LANG["ErrorPost5"]  = "Îøèáêà â Ãîñòåâîé êíèãå!!!";
+$LANG["ErrorPost6"]  = "Ðàçìåð ôàéëà ñëèøêîì áîëüøîé";
+$LANG["ErrorPost7"]  = "Íåïðàâèëüíûé ôîðìàò ôàéëà";
+$LANG["ErrorPost8"]  = "Èçâèíèòå! Áûë àêòèâèðîâàí ôëóä-êîíòðîëü. Âîçâðàùàéòåñü ÷åðåç íåêîòîðîå âðåìÿ!";
+$LANG["ErrorPost9"]  = "Âàø IP çàáàíèëè! :(";
+$LANG["ErrorPost10"] = "Âàøå ñîîáùåíèå ñîäåðæèò íåäîïóñòèìûå ñèìâîëû. Ïîæàëóéñòà, ïîâòîðèòå ââîä.";
+$LANG["ErrorPost11"] = "Âû çàáûëè ââåñòè êîììåíòàðèé. Ïîæàëóéñòà, ïîâòîðèòå ââîä.";
+
+$LANG["PassMess1"] = "Ïîæàëóéñòà, ââåäèòå èìÿ è ïàðîëü:";
+$LANG["PassMess2"] = "Íåïðàâèëüíîå èìÿ èëè ïàðîëü. Ïîïðîáóéòå åùå ðàç.";
+$LANG["PassMess3"] = "Èçâèíèòå, ââåäåíûé Âàìè ïàðîëü íåâåðåí.";
+
+# Days
+$weekday[0] = "Âîñêðåñåíèå";
+$weekday[1] = "Ïîíåäåëüíèê";
+$weekday[2] = "Âòîðíèê";
+$weekday[3] = "Ñðåäà";
+$weekday[4] = "×åòâåðã";
+$weekday[5] = "Ïÿòíèöà";
+$weekday[6] = "Ñóááîòà";
+
+# Months
+$months[0]  = "ßíâàðÿ";
+$months[1]  = "Ôåâðàëÿ";
+$months[2]  = "Ìàðòà";
+$months[3]  = "Àïðåëÿ";
+$months[4]  = "Ìàÿ";
+$months[5]  = "Èþíÿ";
+$months[6]  = "Èþëÿ";
+$months[7]  = "Àâãóñòà";
+$months[8]  = "Ñåíòÿáðÿ";
+$months[9]  = "Îêòÿáðÿ";
+$months[10] = "Íîÿáðÿ";
+$months[11] = "Äåêàáðÿ";
+
+?>
