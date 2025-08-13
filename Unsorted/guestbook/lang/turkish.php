095752bd5ea06c690267b5e17cc7dd36954b07a0
me-suzy <me.suzana@gmail.com>
2025-08-13 05:01:59 +0300

    Add Unsorted/guestbook/lang/turkish.php

---
+++ Unsorted/guestbook/lang/turkish.php
@@ -0,0 +1,101 @@
+<?php
+# General
+
+$LANG["metatag"]    = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-9\">";
+
+$LANG["BookMess1"]  = "HTML kodlarý kapalý";
+$LANG["BookMess2"]  = "HTML kodlarý açýk";
+$LANG["BookMess3"]  = "Ziyaretçi Defterimize Yazýn";
+$LANG["BookMess4"]  = "Ziyaretçi Defterine Dön";
+$LANG["BookMess5"]  = "Administration";
+$LANG["BookMess6"]  = "Sitemizi ziyaret ettiðiniz için teþekkür ederiz.<br>Ziyaretçi defterimize bir þeyler karalarsanýz memnun oluruz.";
+$LANG["BookMess7"]  = "Yorumlar";
+$LANG["BookMess8"]  = "Form'a geri dön";
+$LANG["BookMess9"]  = "Ziyaretçi defterindeki bu yoruma yeni bir yorum ekleyin.";
+$LANG["BookMess10"] = "<u>Ziyaretçi defterimize yazdýðýnýz yorum için teþekkür ederiz.</u><br><br>Yorumunuz baþarýlý bir þekilde eklendi! Ziyaretçi Defterine 2 saniye içinde geri dönüyorsunuz.";
+
+$LANG["EmailAdminSubject"] = "Ziyaretçi Defterine Yeni Kayýt Yapýldý";
+$LANG["EmailGuestSubject"] = "Ziyaretçi Defterimize yaptýðýnýz kayýt için Teþekkür ederiz";
+
+$LANG["AltIP"]    = "IP kayýtlý";
+$LANG["AltICQ"]   = "ICQ Mesajý Gönder";
+$LANG["AltEmail"] = "E-mail Gönder";
+$LANG["AltUrl"]   = "Web Sayfasý";
+$LANG["AltAim"]   = "Instant Messenger";
+$LANG["AltCom"]   = "Yorum Ekle";
+
+# Form
+$LANG["FormMess1"]   = "Ziyaretçi Defterimize yorum eklemek için aþaðýdaki boþ alanlarý doldurun. Yorumunuz eklendikten sonra Ziyaretçi Defterine geri döneceksiniz.<br>(*) ile iþaretli alanlarý doldurmak zorunludur.";
+$LANG["FormMess2"]   = "Smilie ler AÇIK";
+$LANG["FormMess3"]   = "AGCode AÇIK";
+$LANG["FormMess4"]   = "Kodlarýn Açýklamasý";
+$LANG["FormMess5"]   = "Girdiðiniz kayýt aþaðýdadýr.";
+$LANG["FormMess6"]   = "AG Code KAPALI";
+$LANG["FormMess7"]   = "Smilie ler KAPALI";
+$LANG["FormName"]    = "Ýsim";
+$LANG["FormEmail"]   = "E-mail adresiniz";
+$LANG["FormLoc"]     = "Bulunduðunuz yer";
+$LANG["FormPic"]     = "Resim";
+$LANG["FormUrl"]     = "Web Sayfanýz";
+$LANG["FormGender"]  = "Cinsiyet";
+$LANG["FormMale"]    = "Erkek";
+$LANG["FormFemale"]  = "Bayan";
+$LANG["FormMessage"] = "Mesajýnýz";
+$LANG["FormSelect"]  = "Ziyaretçi Defteri";
+$LANG["FormUser"]    = "Kullanýcý Adý";
+$LANG["FormPass"]    = "Þifre";
+$LANG["FormReset"]   = "Temizle";
+$LANG["FormSubmit"]  = "Gönder";
+$LANG["FormPreview"] = "Ön Ýzleme";
+$LANG["FormBack"]    = "Geri Dön";
+$LANG["FormEnter"]   = "Ziyaretçi Defteri - Ýdare Merkezi<br><b>Geçerli Kullanýcý Adý ve Þifre Girmelisiniz!</b>";
+$LANG["FormButton"]  = "Git";
+$LANG["FormPriv"]    = "Admin'e Özel Mesaj";
+
+# Navigation Bar
+$LANG["NavTotal"]   = "Toplam Yorum:";
+$LANG["NavRecords"] = "Bu Sayfada Görünen Yorum:";
+$LANG["NavPrev"]    = "Önceki Sayfa";
+$LANG["NavNext"]    = "Sonraki Sayfa";
+
+# Post Errors
+$LANG["ErrorPost1"]  = "Adýnýzý yazmadýnýz. Lütfen kontrol edip, yazarak tekrar gönderin.";
+$LANG["ErrorPost2"]  = "Mesajýnýzý yazmadýnýz.Lütfen kontrol edip, yazarak tekrar gönderin.";
+$LANG["ErrorPost3"]  = "Mesajýnýz ya çok kýsa yada çok uzun oldu.Lütfen kontrol edip, yazarak tekrar gönderin.";
+$LANG["ErrorPost4"]  = "One of the input fields does not seem to be valid.";
+$LANG["ErrorPost5"]  = "Ziyaretçi Defteri - Hata";
+$LANG["ErrorPost6"]  = "Ýmaj Dosyasý büyük";
+$LANG["ErrorPost7"]  = "Ýmaj dosyasý tipi hatalý";
+$LANG["ErrorPost8"]  = "Özür Dileriz! Fazla giriþ-kontrolü aktiftir. Lütfen, bir süre sonra tekrar deneyiniz!";
+$LANG["ErrorPost9"]  = "IP adresiniz banlanmýþ!";
+$LANG["ErrorPost10"] = "Mesajýnýzda yazým kurallarýna bazý aykýrýlýklar mevcuttur. Lütfen kontrol edip, yazým kurallarýna uygun yazýp tekrar gönderin.";
+$LANG["ErrorPost11"] = "Yorumunuzu yazmadýnýz.Lütfen kontrol edip, yazarak tekrar gönderin.";
+
+$LANG["PassMess1"] = "Lütfen geçerli Kullanýcý Adý ve Þifre Giriniz:";
+$LANG["PassMess2"] = "Hatalý Kullanýcý Adý ve Þifre. Lütfen tekrar deneyiniz.";
+$LANG["PassMess3"] = "Üzgünüz, girdiðiniz þifre uygun deðil.";
+
+# Days
+$weekday[0] = "Pazar";
+$weekday[1] = "Pazartesi";
+$weekday[2] = "Salý";
+$weekday[3] = "Çarþamba";
+$weekday[4] = "Perþembe";
+$weekday[5] = "Cuma";
+$weekday[6] = "Cumartesi";
+
+# Months
+$months[0]  = "Ocak";
+$months[1]  = "Þubat";
+$months[2]  = "Mart";
+$months[3]  = "Nisan";
+$months[4]  = "Mayýs";
+$months[5]  = "Haziran";
+$months[6]  = "Temmuz";
+$months[7]  = "Aðustos";
+$months[8]  = "Eylül";
+$months[9]  = "Ekim";
+$months[10] = "Kasým";
+$months[11] = "Aralýk";
+
+?>
