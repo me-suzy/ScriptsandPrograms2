bd28034f2e36c072e809fa5c2838af4f806ebc1b
me-suzy <me.suzana@gmail.com>
2025-08-13 06:13:25 +0300

    Add Unsorted/lizardcart/footer.php

---
+++ Unsorted/lizardcart/footer.php
@@ -0,0 +1,65 @@
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+          </table>
+		  <!-- end of main content -->
+        </td>
+      </tr>
+      <tr height="52" bgcolor="#003399">
+        <td valign="top">
+		<!-- begin bottom nav -->
+          <table border="0" cellspacing="0" cellpadding="1" width="100%"
+          height="100%">
+            <tr>
+              <td valign="top">
+			  <!-- begin bottom nav links -->
+                <table border="0" cellspacing="0" cellpadding="0" height="18">
+                  <tr
+                  style="color: White; font-size: 10px; font-weight: bold; font-family: Tahoma;">
+                    <td onmouseover="on(this)" onmouseout="off(this)"
+                    style="cursor: hand" onclick="go('mailto:?body='+location.href)">
+                      &#160;&#160;<a href="mailto:ts@2tonewebdesign.com?subject=Great Site!! Mail&body=Just Love This Cart!!"><font color="#FFFFFF">Contact</font></a>&#160;&#160;
+                    </td>
+                    <td class="sep">
+                    </td>
+                    <td onmouseover="on(this)" onmouseout="off(this)"
+                    style="cursor: hand onclick="print()">
+                      &#160;&#160;<a href="<?echo"$nosecureurl" ?>index.php"><font color="#FFFFFF">Home</font></a>&#160;&#160;
+                    </td>
+                    <td class="sep">
+                    </td>
+                    <td onmouseover="on(this)" onmouseout="off(this)"
+                    style="cursor: hand" onclick="go('javascript:void(0)')">
+                      &#160;&#160;<a href="<?echo"$nosecureurl" ?>managecart.php"><font color="#FFFFFF">What's In My Cart?</font></a>&#160;&#160;
+                    </td>
+					<td class="sep">
+                    </td>
+                    <td onmouseover="on(this)" onmouseout="off(this)"
+                    style="cursor: hand" onclick="go('javascript:void(0)')">
+                      &#160;&#160;<a href="<?echo"$nosecureurl" ?>form.php" ><font color="#FFFFFF">Send this page to a friend</font></a>&#160;&#160;
+                    </td>
+					 <td class="sep">
+                    </td>
+                    <td onmouseover="on(this)" onmouseout="off(this)"
+                    style="cursor: hand" onclick="go('javascript:void(0)')">
+                      &#160;&#160;<a href="<?echo"$nosecureurl" ?>admin/index.php" target="_blank" ><font color="#FFFFFF">Admin</font></a>&#160;&#160;
+                    </td>
+                  </tr>
+                </table>
+				<!-- end of bottom nav links -->
+                <br />
+                 &#160;&#160;<span
+                style="color: White; font-family: Tahoma; font-size: 10px;">&#169;
+                2002 Lizard Cart. ( <a href="http://www.2tonewebdesign.com" class="bottom" target="_blank">2tone Web Design</a> ).</span>
+              </td>
+            </tr>
+          </table>
+		  <!-- end bottom nav -->
+        </td>
+      </tr>
+    </table>
+	<!-- end of main table -->
+  </body>
+</html>
