4bf4724d74060d3af327b50a371477ac4c8a0db0
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:32 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/help.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/help.php
@@ -0,0 +1,205 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[HELP_Title] = "Hjelp på";
+
+$LOCALE[HELP_Unavailable] = "En nyttig beskrivelse av denne f0respørselen er utilgjengelig.";
+
+$LOCALE[HELP_Username] = "Et navn eller username er personen som De ønsker visst som mens i pratet. Liknende til et økenavn er dette Deres virkelig navn eller et laget opp et.";
+
+$LOCALE[HELP_Language] = "MWChat har evnen vise adskillige forskjellige språk. Denne esken tillater De velge uansett hvilken språk De foretrekker bruke.";
+
+$LOCALE[HELP_Password] = "Forsterkning, Operator, og registrerte konti krever et passord til login. Dette forhindrer uautorisert adgang til Deres konto.";
+
+$LOCALE[HELP_Lobby_Public] = "Det offentlige romet lister være opp en utvelging av forskjellige rom som De prater i. Velg et rom den intrests De mest eller skap Deres egent.";
+
+$LOCALE[HELP_Lobby_Private] = "Det private romet lister gjemme opp til normale brukere. Om De ønsker prate i et privat rom, går inn i navnet av det anmodede romet under. Private rom viser ikke på noe romliste, derfor De holder private samtaler uten uninvited brukere gående i på De.";
+
+$LOCALE[HELP_Lobby_Public_Create] = "Om De ikke finner et rom interesserer som De i listen ovenfor, De skaper Deres egent rom ved å maskinskrive navnet av det anmodede romet i feltet under.";
+
+$LOCALE[HELP_Lobby_Private_Create] = "Gå inn i navnet av det private romet De ønsker gå inn i. Om romet ikke finnes, skapt det for De.";
+
+$LOCALE[HELP_Private_Message] = "Velg en bruker opplistet i denne dråpen ned eske. Noen budskap som De sender drar bare til specifed bruker. Kople tilbake til det hele romet, velg standardsprangene. Privat messaging gjort også bruk av det /msg kommando.";
+
+$LOCALE[HELP_Private_Message_Refresh] = "Klikk dette leddet forfriske den private budskapbrukerliste.";
+
+$LOCALE[HELP_Colors] = "Om De forandrer fargen av teksten som De går inn i, klikk på noe farge i fargebar. De ser et budskap siende at Deres farger oppdatert. De klikker hvor som helst i den nedre halvdelen av fargebar restaurere standardfargen.";
+
+$LOCALE[HELP_Enter_Password] = "Gå inn i et nytt passord De bruker. Dette passordet er noe alfa numerisk tekst. Sjekk at De skriver dette passordet ned og behold det i et sikkert sted. Det krever at passordet er fem eller mere karakterer i lengde.";
+
+$LOCALE[HELP_Icons] = "Velg et ikon fra listen under representere De i det pratet. Alle brukere ser Deres ikon.";
+
+$LOCALE[HELP_Theme] = "Velg et tema fra listen under. Et tema forandrer all Deres pratfarger. De returnerer til lobbyen, og da rygger inn i romet for all temaforandringer tre i kraft.";
+
+$LOCALE[HELP_Confirm_Password] = "Gå inn i det nye passordet De valgte igjen her. Dette bekrefter at De ikke lager en typo og sikrer at De vet hva Deres passord er.";
+
+$LOCALE[HELP_YOB] = "Dette er året av fødsel. Denne informasjonen bruker til caculate alder i noe tilfelle, da noen lover krever at brukere er av et sikkert igjen.";
+
+$LOCALE[HELP_Occupation] = "Dette er brukerstillingen eller jobb. Vanligvis deres hovedkilde av inntekt.";
+
+$LOCALE[HELP_Hobbies] = "Dette er ting en bruker nyter gjøre. For det meste brukt er på hobbyer vanligvis reservetid.";
+
+$LOCALE[HELP_Location] = "Dette er den geografiske plasseringen av brukeren.";
+
+$LOCALE[HELP_Comments] = "Dette er noen kommentarer eller meninger brukeren har eller ønsker til stemme.";
+
+$LOCALE[HELP_Status] = "Dette er brukerne nåværende status. Vanligvis angiende om en bruker er borte, on-line, registrert, en forsterkning, eller en operatør.";
+
+$LOCALE[HELP_Upload_File] = "Dette er arkivet som De sender til en bruker. Klikk browse knapp velge arkivet ønsker som De sende.";
+
+$LOCALE[HELP_Upload_User] = "Dette er brukeren De sender et arkiv til.";
+
+$LOCALE[HELP_Enter_Message] = "Når De går inn i et rom, sendt dette budskapet sammen med det legger merke til at De De' ve gått inn i romet.";
+
+$LOCALE[HELP_Exit_Message] = "Når De går ut et rom, sendt dette budskapet sammen med det legger merke til at De De' ve forlatt romet.";
+
+$LOCALE[HELP_Away_Message] = "Når De forsvinner, sendt dette budskapet sammen med det legger merke til at Deres borte.";
+
+$LOCALE[HELP_Back_Message] = "Når Deres rygg fra å være borte, sendt dette budskapet sammen med det legger merke til at Deres rygg.";
+
+$LOCALE[HELP_Default_Color] = "Om De vil ha en standardfarge brukt hver gang De går inn i et rom, tillater dette alternativet De sette det.";
+
+$LOCALE[HELP_Smiles] = "Dette alternativet omkopler Deres evne se grafisk eller tekst smiley ansikter i pratet.";
+
+$LOCALE[HELP_Locate] = "Dette alternativet omkopler Deres evne tillate eller forbyr brukere plassere De. Plasserende en bruker forteller hva rom som De er for tiden i.";
+
+$LOCALE[HELP_Upload] = "Dette alternativet omkopler Deres evne motta arkiver fra andre brukere.";
+
+$LOCALE[HELP_Room] = "Dette er romet som De er for tiden i, eller romet som De ønsker skape eller går inn i.";
+
+$LOCALE[HELP_Room_Password] = "Noen private rom er beskyttet passord. Om romet som De ønsker gå inn i er beskyttet passord, går inn i det her.";
+
+$LOCALE[HELP_Number_Users] = "Dette er den totale antallet brukere som er for tiden i romet.";
+
+$LOCALE[HELP_Topic] = "Dette er det nåværendee emnet av romet.";
+
+$LOCALE[HELP_Buddylist] = "Dette er Deres buddy liste. Dette inneholder usernames av alle brukere i dette romet, og noe buddies De har. De klikker på hver navn få informasjon om den brukeren om de registrerer.";
+
+$LOCALE[HELP_Serial_Number] = "Dette er MWChat serienummer. De får et serienummer på http: //www.appindex.net/mwchat";
+
+$LOCALE[HELP_Storage] = "Dette er Deres arkiv lagerinformasjon. Først antallet viser seg hvor mye skille som De har tilgjengelig for bruk. Andre antallutstillingene hva Deres total tillatt lagring er. Under er en liste av noen arkiver som De har. Klikk på et filnavn til download som arkiverer, eller klikk kontrollesken fjerne arkivet. De ser også størrelsen av hver arkiv, da De mottatt arkivet, sendt og som De arkivet.";
+
+$LOCALE[HELP_ERROR_License] = "Tillatelsen som De har er ukorrekt, eller ikke går ut. Prøv på nytt Deres tillatelse og om det enda ikke arbeider får et nytt serienummer eller kontaktAppindex Net støtte.";
+
+$LOCALE[HELP_ERROR_AlreadyIn] = "Noen er allerede logged i med den username. Det er mulig om De gjorde ikke logout riktig, har som Deres konto ikke idled ut. Vent ti minutter og forsøk igjen.";
+
+$LOCALE[HELP_ERROR_Logout] = "Deres konto vært logged ut. De kan login igjen om De liker.";
+
+$LOCALE[HELP_ERROR_Expired] = "Om De De' ve vært ledig dette er ekspirerte hvorfor som De. De sparket også ut ved en forsterkning eller operatør.";
+
+$LOCALE[HELP_ERROR_RoomLong] = "Romet kaller De valgte er også lang. Maksimumskarakteren begrenser det 15 karakterer.";
+
+$LOCALE[HELP_Live] = "Pratsystemet er offline. Dette er fordi en forsterkning gjør stigninger.";
+
+$LOCALE[HELP_ERROR_Profanity] = "De vært logged ut for bruking av profanity i pratet. Profanity tillater ikke.";
+
+$LOCALE[HELP_ERROR_NoData] = "De ikke fylte riktig i alle felt. Behag forsøk igjen.";
+
+$LOCALE[HELP_ERROR_BadData] = "Informasjonen som De gått inn i inneholdte dårlige karakterer. Behag bruk bare alfa numeriske karakterer.";
+
+$LOCALE[HELP_ERROR_Serial_Invalid] = "Serienummeret Deres git passer ikke Deres tjener. De får et nytt serienummer og bekrefter Deres webservers IP adresse.";
+
+$LOCALE[HELP_ERROR_Serial_None] = "Appindex garn har ikke et serienummer som vÆrer schriftlich belegt for De. Behag register Deres produkt og få et nytt serienummer.";
+
+$LOCALE[HELP_ERROR_Critical] = "En ukjent kritisk feil forekommer. Om problemet holder ut, kontaktAppindex Net støtte.";
+
+$LOCALE[HELP_ERROR_Javascript] = "MWChat krever et vev browser som i stand til viser javascript. Vær så snill og muliggjør javascript i Deres webbrowser eller stigning.";
+
+$LOCALE[HELP_ERROR_Browser] = "Vevet browser som De bruker i ikke forenelig med MWChat. Vi anbefaler at det bruke av Netscape eller Internet Explorer.";
+
+$LOCALE[HELP_ERROR_Restricted] = "Username som De valgte er et innskrenket eller reservert kaller. Vær så snill og velg enda et navn og login igjen.";
+
+$LOCALE[HELP_ERROR_BadPass] = "Passordet som De forsynt er ukorrekt. Sjekk med en forsterkning for ytterligeree detaljer.";
+
+$LOCALE[HELP_ERROR_IP] = "MWChat oppdaget at Deres IP adresse forandrer seg. For sikkerhetshar grunner, vi logged De ut. Behag login igjen.";
+
+$LOCALE[HELP_COMMAND_addbuddy] = "Tilføy en buddy til Deres buddy liste. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_admin] = "Tillater forsterkninger til unregister brukere eller siktlogger. Spesifiser andre alternativet (username) sikte logger for en specfic bruker eller til unregister den brukeren.";
+
+$LOCALE[HELP_COMMAND_afk] = "Forandrer Deres modus til borte fra tastatur. De spesifiserer et valgfrit borte budskap.";
+
+$LOCALE[HELP_COMMAND_block] = "Blokkerer alle private budskap fra noen brukere.";
+
+$LOCALE[HELP_COMMAND_bug] = "Send en basillrapport.";
+
+$LOCALE[HELP_COMMAND_about] = "Sikt informasjon om MWChat og creators.";
+
+$LOCALE[HELP_COMMAND_clear] = "Rydder det nåværendee pratvindu.";
+
+$LOCALE[HELP_COMMAND_color] = "Sett tekstfargen til en farge av Deres valg. Denne fargen er i #000000 formaterer.";
+
+$LOCALE[HELP_COMMAND_deop] = "Fjern en brukeroperatørstatus. Denne kommandoen brukt bare av forsterkninger og operatører.";
+
+$LOCALE[HELP_COMMAND_global] = "Send et budskap til alle brukere i alle rom. Denne kommandoen brukt bare av en forsterkning.";
+
+$LOCALE[HELP_COMMAND_help] = "Viser dette dokumentet.";
+
+$LOCALE[HELP_COMMAND_ignore] = "Tilføy en bruker til Deres ignorerer liste. Den brukeren sender De ikke private budskap og De ser ikke budskap som de sender. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_unignore] = "Fjern en bruker fra Deres ignorerer liste. Den brukeren sender De nå private budskap og De ser budskap som de sender. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_kick] = "Spark en bruker fra systemet. De spesifiserer en valgfri grunn. Denne kommandoen brukt bare av forsterkninger eller operatører.";
+
+$LOCALE[HELP_COMMAND_lobby] = "Returnerer De til lobbyen. De spesifiserer et valgfrit utgangbudskap.";
+
+$LOCALE[HELP_COMMAND_locate] = "Plasserer en spesifisert bruker. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_logout] = "Logger en bruker ut av systemet. De spesifiserer et valgfrit utgangbudskap.";
+
+$LOCALE[HELP_COMMAND_me] = "Beskriv hva som De gjør uten det visesr til Dem direktett.";
+
+$LOCALE[HELP_COMMAND_motd] = "Viser budskapet av dagen.";
+
+$LOCALE[HELP_COMMAND_myfiles] = "Viser Deres on-line arkiver.";
+
+$LOCALE[HELP_COMMAND_op] = "Gi en brukeroperatørstatus. Denne kommandoen brukt bare av forsterkninger og operatører.";
+
+$LOCALE[HELP_COMMAND_prefs] = "Viser Deres MWChat fortrinnsrett.";
+
+$LOCALE[HELP_COMMAND_print] = "Viser Deres samtaler i et trykkbart vindu.";
+
+$LOCALE[HELP_COMMAND_msg] = "Send et privat budskap til en bruker. Noone ser ellers dette budskapet.";
+
+$LOCALE[HELP_COMMAND_register] = "Registrerer den nåværendee username Deres bruk. Registrering tillater brukere dele arkiver og skaper buddy lister.";
+
+$LOCALE[HELP_COMMAND_removebuddy] = "Fjerner en buddy fra Deres buddy liste. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_roll] = "Ruller pratromterningene. Dette er nyttig for on-line gaming.";
+
+$LOCALE[HELP_COMMAND_showbuddy] = "Viser alle brukere på Deres buddy liste. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_showignored] = "Viser alle brukere på Deres ignorerer liste. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_smiley] = "Viser all tilgjengelig smiley ansikter.";
+
+$LOCALE[HELP_COMMAND_topic] = "Forandrer romemnet. Denne kommandoen brukt bare av forsterkninger og operatører.";
+
+$LOCALE[HELP_COMMAND_unafk] = "Forandrer Deres modus til rygg fra borte fra tastatur. De spesifiserer et valgfrit bakbudskap.";
+
+$LOCALE[HELP_COMMAND_unblock] = "Tillater De akseptere private budskap fra alle brukere igjen.";
+
+$LOCALE[HELP_COMMAND_topic] = "Fjerner en bruker fra Deres ignorerer liste. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_unregister] = "Unregisters Deres username. Dette stiller det til disposisjon for noen bruke. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_upload] = "Viser arkivet som dele grensesnitt. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_url] = "Tillater De tilføye en URL inn i pratet.";
+
+$LOCALE[HELP_COMMAND_whois] = "Utstillingsinformasjon om den spesifiserte brukeren. Denne kommandoen brukt bare av registrerte brukere.";
+
+$LOCALE[HELP_COMMAND_Title] = "Tilgjengelige kommandoer";
+
+$LOCALE[HELP_COMMAND_Type] = "Æå angir et krevd alternativ, ÆÅ angir et valgfrit alternativ.";
+
+?>
