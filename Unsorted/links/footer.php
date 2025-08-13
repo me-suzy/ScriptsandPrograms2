93a562fa3ec32611a79b5723bb39e8e8e35b0741
me-suzy <me.suzana@gmail.com>
2025-08-13 06:00:17 +0300

    Add Unsorted/links/footer.php

---
+++ Unsorted/links/footer.php
@@ -0,0 +1,28 @@
+<?php
+////////////////////////////////////////////////////////////////////////////
+// db Masters' Links Directory 3.0, Copyright (c) 2003 db Masters Multimedia
+// Content Manager comes with ABSOLUTELY NO WARRANTY
+// Licensed under the AGPL
+// See license.txt and readme.txt for details
+////////////////////////////////////////////////////////////////////////////
+?>
+	</td>
+</tr>
+<tr>
+	<td colspan="2" class="footer">
+<table cellpadding="0" cellspacing="0" class="credits">
+<tr>
+	<td align="left" class="bodysm">
+<?php echo $site_title; ?><br />Powered by <a href="http://www.dbmasters.net/">dB Masters Multimedia</a>
+	</td>
+	<td align="right" class="bodysm">
+<a href="http://validator.w3.org/check/referer"><img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1!" style="border:0;width:88px;height:31px"/></a>
+<a href="http://jigsaw.w3.org/css-validator/"><img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" style="border:0;width:88px;height:31px"/></a>
+	</td>
+</tr>
+</table>
+	</td>
+</tr>
+</table>
+</body>
+</html>
