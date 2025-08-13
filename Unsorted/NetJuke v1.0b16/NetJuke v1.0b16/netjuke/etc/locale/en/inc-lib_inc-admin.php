d556dce966e5ba4d22018d9baa7ab245dcaeda65
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:57 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-lib_inc-admin.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-lib_inc-admin.php
@@ -0,0 +1,14 @@
+<?php
+
+##################################################
+
+# /lib/inc-admin.php
+
+##################################################
+
+define( "INCADM_DENIED", "Access denied! You cannot edit this track." );
+define( "INCADM_NOPERM", "Permissions Error with the netjuke/var/data/ directory." );
+
+##################################################
+
+?>
