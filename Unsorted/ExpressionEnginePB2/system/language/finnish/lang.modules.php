1d65834aef6dd1b0359e32effda0441bea3543d9
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:10 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.modules.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.modules.php
@@ -0,0 +1,49 @@
+<?php
+$L = array(
+
+
+"module_name" =>
+"Modulin nimi",
+
+"module_access" =>
+"Muokkaa modulia",
+
+"module_backend" =>
+"Käyttäjän muokkaama",
+
+"module_version" =>
+"Versio",
+
+"module_status" =>
+"Status",
+
+"module_description" =>
+"Selitys",
+
+"module_action" =>
+"Toiminto",
+
+"not_installed" =>
+"Ei asennettu",
+
+"installed" =>
+"Asennettu",
+
+"install" =>
+"Asenna",
+
+"deinstall" =>
+"Poista",
+
+"module_can_not_be_found" =>
+"Moduulin asennustiedostoa ei löydetty",
+
+"module_has_been_installed" =>
+"Moduuli asennettu:",
+
+"module_has_been_removed" =>
+"Moduuli poistettu:",
+
+''=>''
+);
+?>
