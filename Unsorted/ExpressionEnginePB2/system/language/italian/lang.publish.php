1535c022327cf5a12b202dc138098009c49901c2
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:11 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.publish.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.publish.php
@@ -0,0 +1,450 @@
+<?php
+$L = array(
+
+"status_changed" =>
+"Status Changed",
+
+"publish" =>
+"Pubblica",
+
+"no_weblog_exits" =>
+"Hai tentato di accedere a un weblog inesistente.",
+
+"no_entries_matching_that_criteria" =>
+"La ricerca non ha prodotto risultati",
+
+"no_entries_exist" =>
+"Non ci sono interventi in questo weblog",
+
+"select_blog_to_post_in" =>
+"Scegli un weblog su cui scrivere",
+
+"select_blog_to_edit" =>
+"Scegli un weblog da consultare",
+
+"this_entry_will_appear_in" =>
+"Questo intervneto sarà visibile in:",
+
+"unauthorized_for_this_blog" =>
+"Non sei autorizzato a scrivere su questo weblog",
+
+"unauthorized_for_any_blogs" =>
+"Non sei autorizzato a pubblicare interventi",
+
+"unauthorized_to_delete_others" =>
+"Ti è consentito cancellare solo gli interventi che hai scritto",
+
+"unauthorized_to_delete_self" =>
+"Non ti è consentito cancellare i tuoi interventi",
+
+"weblog" =>
+"Weblog",
+
+"weblogs" =>
+"Weblog",
+
+"new_entry" =>
+"Nuovo intervento",
+
+"button_mode" =>
+"Modalità pulsanti:",
+
+"guided" =>
+"Guidata",
+
+"normal" =>
+"Normale",
+
+"view_entry" =>
+"Mostra intervento",
+
+"preview_entry" =>
+"Intervento precedente",
+
+"edit_entry" =>
+"Modifica intervento",
+
+"edit_weblog_entries" =>
+"Modifica interventi",
+
+"continue_editing" =>
+"Continua la modifica",
+
+"entry_status" =>
+"Stato intervento",
+
+"status" =>
+"Stato",
+
+"comments" =>
+"Commenti",
+
+"open" =>
+"Aperto",
+
+"close" => 
+"Close",
+
+"closed" =>
+"Chiuso",
+
+"options" =>
+"Opzioni",
+
+"sticky" =>
+"Mantieni visibile",
+
+"allow_comments" =>
+"Abilita commenti",
+
+"allow_trackbacks" =>
+"Abilita trackback",
+
+"ping_sites" =>
+"Ping ai siti",
+
+"ping_urls" =>
+"Trackback diretto agli URL",
+
+"auto_discovery" =>
+"Ricerca automatica trackback",
+
+"select_entries_to_ping" =>
+"Seleziona gli interventi da cui vorresti inviare un ping",
+
+"view_previous_pings" =>
+"Mostra ping precedenti",
+
+"previiously_pinged_urls" =>
+"Hai già inviato ping ai seguenti URL",
+
+"date" =>
+"Data",
+
+"expiration_date" =>
+"Scadenza",
+
+"invalid_date_formatting" =>
+"La data specificata non è formattata correttamente (aaaa-mm-gg hh:mm)",
+
+"date_outside_of_range" =>
+"La data specificata esce dal range ammesso (1902 - 2037).",
+
+"category" =>
+"Categoria",
+
+"categories" =>
+"Categorie",
+
+"no_categories" =>
+"Non ci sono categorie",
+
+"title" =>
+"Titolo",
+
+"url_title" =>
+"Titolo URL",
+
+"author" =>
+"Autore",
+
+"newline_format" =>
+"Formato:",
+
+"none" =>
+"Nessuno",
+
+"link" =>
+"Link",
+
+"image" =>
+"Immagine",
+
+"email" =>
+"E-mail",
+
+"upload" =>
+"Upload",
+
+"file_upload" =>
+"File upload",
+
+"emoticons" =>
+"Emoticon",
+
+"choose_a_destination_for_emoticon" =>
+"Scegli una destinazione per il tuo emoticon",
+
+"click_emoticon" =>
+"Clicca su un&#39;immagine per inserirla nell&#39; intervento",
+
+"no_emoticons" =>
+"Impossibile individuare la posizione degli emoticon",
+
+"close_all" =>
+"Chiudi tutto",
+
+"missing_title" =>
+"Devi assegnare un titolo all&#39;intervento.",
+
+"missing_date" =>
+"Devi assegare una data all&#39;intervento.",
+
+"title_not_unique" =>
+"Esiste già un intervento con questo titolo. I titoli devono essere univoci.",
+
+"url_title_not_unique" =>
+"Esiste già un intervento con questo titolo URL. I titoli URL devono essere univoci.",
+
+"entry_has_been_added" =>
+"Nuovo intervento pubblicato",
+
+"entry_has_been_updated" =>
+"L&#39;intervento è stato aggiornato",
+
+"trackback_url_errors" =>
+"I seguenti ping trackback non sono stati accettati:",
+
+"xmlrpc_ping_errors" =>
+"I seguenti ping XML-RPC non sono stati accettati:",
+
+"click_to_view_your_entry" =>
+"Clicca qui per vedere il tuo intervento",
+
+"view" =>
+"Mostra",
+
+"edit_this_entry" =>
+"Modifica questo intervento",
+
+"date_range" =>
+"Periodo",
+
+"today" =>
+"Oggi",
+
+"past_week" =>
+"Ultimi 7 giorni",
+
+"past_month" =>
+"Ultimi 30 giorni",
+
+"past_six_months" =>
+"Ultimi 180 giorni",
+
+"past_year" =>
+"Ultimi 365 giorni",
+
+"any_date" =>
+"Tutte le date",
+
+"results_per_page" =>
+"Risultati per pagina",
+
+"order" =>
+"Ordine",
+
+"ascending" =>
+"Vecchi prima",
+
+"descending" =>
+"Recenti prima",
+
+"alpha" =>
+"Alfabetico",
+
+"delete_confirm" =>
+"Conferma cancellazione",
+
+"delete_entry_confirm" =>
+"Vuoi veramente cancellare definitivamente questo intervento?",
+
+"delete_entries_confirm" =>
+"Vuoi veramente eliminare definitivamente questi interventi?",
+
+"entries_deleted" =>
+"Cancellato",
+
+"html_buttons_no_cursor" =>
+"Per utilizzare questi pulsanti, devi innanzitutto posizionare il cursore all&#39;interno di un campo.",
+
+"html_buttons_url_text" =>
+"Specifica l&#39;URL",
+
+"html_buttons_webpage_text" =>
+"Specifica il testo del link",
+
+"html_buttons_title_text" =>
+"Opzionate: specifica l&#39;attributo &quot;title&quot;",
+
+"html_buttons_image_text" =>
+"Specifica l&#39;URL dell&#39;immagine",
+
+"html_buttons_email_text" =>
+"Specifica l&#39;indirizzo e-mail",
+
+"html_buttons_email_title" =>
+"Specifica il testo del link (o lascia il campo vuoto per utilizzare l&#39;indirizzo come testo attivo)",
+
+"html_buttons_enter_text" =>
+"Specifica il testo che desideri formattare",
+
+"warning" =>
+"Attenzione",
+
+"file_exists" =>
+"Esiste già un file con lo stesso nome",
+
+"overwrite_instructions" =>
+"Specifica un nuovo nome per rinominare il file, oppure lascia inalterato il nome per sovrascriverlo al file esistente.",
+
+"select_destination_dir" =>
+"Seleziona una cartella di destinazione",
+
+"file_type" =>
+"Tipo di file",
+
+"non_image" =>
+"Non-immagine",
+
+"success" =>
+"Riuscito!",
+
+"you_have_uploaded" =>
+"Hai caricato il seguente file:",
+
+"choose_a_destination" =>
+"Dove desideri che il file appaia?",
+
+"no_entry_fields" =>
+"Non ci sono campi su questo weblog.",
+
+"no_comments_or_trackbacks" =>
+"Non ci sono commenti o trackback per questo intervento",
+
+"posted_by" =>
+"Autore:",
+
+"located_in" =>
+"Località:",
+
+"comment_date" =>
+"Data:",
+
+"comment_email" =>
+"E-mail:",
+
+"comment_url" =>
+"URL:",
+
+"comment_action" =>
+"Operazione:",
+
+"comment_ip" =>
+"IP:",
+
+"weblog_name" =>
+"Weblog:",
+
+"comment" =>
+"Commento",
+
+"missing_comment" =>
+"Devi inviare un commento",
+
+"edit_comment" =>
+"Modifica commento",
+
+"delete_comment" =>
+"Cancella commento",
+
+"comment_deleted" =>
+"Commento cancellato",
+
+"delete_trackback" =>
+"Cancella trackback",
+
+"trackback_deleted" =>
+"Trackback cancellato",
+
+"comment_updated" =>
+"Commento aggiornato",
+
+"delete_comment_confirm" =>
+"Vuoi veramente eliminare definitivamente questo commento?",
+
+"delete_trackback_confirm" =>
+"Vuoi veramente eliminare definitivamente questo trackback?",
+
+"name" =>
+"Nome",
+
+"url" =>
+"URL",
+
+"location" =>
+"Località",
+
+"continue" =>
+"Continua",
+
+"image_options" =>
+"Opzioni immagine",
+
+"create_popup" =>
+"Crea un pop-up per l&#39;immagine?",
+
+"create_thumb" =>
+"Crea miniatura immagine",
+
+"constrain_proportions" =>
+"Mantieni proporzioni",
+
+"thumb_settings" =>
+"Settaggi immagine",
+
+"choose_a_destination_for_thumb" =>
+"Dove desideri che l&#39;immagine appaia?",
+
+"do_not_place_file" =>
+"Non inserire file nell&#39;intervento",
+
+"image_size_not_different" =>
+"La larghezza e l&#39;altezza specificate sono le stesse dell&#39;originale.",
+
+"width" =>
+"Larghezza",
+
+"height" =>
+"Altezza",
+
+"pixels" =>
+"Pixel",
+
+"percent" =>
+"Percentuale",
+
+"resize_image" =>
+"Ridimensiona immagine",
+
+"create_thumb_copy" =>
+"Crea una copia aggiuntiva",
+
+"resize_original" =>
+"Ridimensiona l&#39;immagine originale",
+
+"thumb_instructions" =>
+"Puoi scegliere di ridimensionare l&#39;immagine caricata, oppure creare una miniatura aggiuntiva.",
+
+"thumbnail_created" =>
+"Miniatura creata",
+
+"image_resized" =>
+"L&#39;immagine è stata ridimensionata",
+
+"close_window" =>
+"Chiudi finestra",
+
+''=>''
+);
+?>
