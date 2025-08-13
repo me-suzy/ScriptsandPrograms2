f0151e6e583f1e9d427fdcf5d2ce7ce72e5fa730
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:46 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/options.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/options.php
@@ -0,0 +1,46 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[OPTIONS_Info] = "Vær så snill og lag noen forandringer og klikk oppdateringsalternativknappen under.";
+
+$LOCALE[OPTIONS_Title] = "Sikt eller modifiser Deres fortrinnsrett!";
+
+$LOCALE[OPTIONS_Msg_Enter] = "Gå inn i budskap";
+
+$LOCALE[OPTIONS_Msg_Exit] = "Gå ut budskap";
+
+$LOCALE[OPTIONS_Msg_Away] = "Borte budskap";
+
+$LOCALE[OPTIONS_Msg_Back] = "Bakbudskap";
+
+$LOCALE[OPTIONS_Color] = "Standardtekstfarge";
+
+$LOCALE[OPTIONS_Smiles] = "Vis smiley ansikter";
+
+$LOCALE[OPTIONS_Yes] = "YES";
+
+$LOCALE[OPTIONS_No] = "NO";
+
+$LOCALE[OPTIONS_Locate] = "Brukere plasserer De";
+
+$LOCALE[OPTIONS_Upload] = "Brukere sender De arkiverer";
+
+$LOCALE[OPTIONS_Button] = "Oppdater fortrinnsrett";
+
+$LOCALE[OPTIONS_Complete] = "Deres fortrinnsrett oppdatert!";
+
+$LOCALE[OPTIONS_Icons] = "Velg Deres personlig ikon.";
+
+$LOCALE[OPTIONS_Theme] = "Velg et tema.";
+
+?>
