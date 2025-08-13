de276123cca58e72b6f24638f88a98e402847e73
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:04 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_tabfile-recursive.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_tabfile-recursive.php
@@ -0,0 +1,65 @@
+<?php
+
+##################################################
+
+# /admin/tabfile-recursive.php
+
+##################################################
+
+define( "TFREC_HEADER", "RECERCA RECURSIVA DE FITXERS ÀUDIO" );
+define( "TFREC_HEADER_AUTO", "Mode automàtic" );
+define( "TFREC_HEADER_INTER", "Mode interactiu" );
+define( "TFREC_CAPTION_1", "" );
+define( "TFREC_CAPTION_2", "Si us plau, trameteu un sub-directori per escanejar, o feu clic sobre el botó del formulari per escanejar tot el directori de música definit en el fitxer de preferències. Si és la primera vegada que importeu música, es recomana que limiteu la recerca a sub-directoris." );
+define( "TFREC_OPTION_1", "Recursiu" );
+define( "TFREC_OPTION_2", "Utilitza el nom del fitxer per als arxius que no tenen informació ID3 i importa" );
+define( "TFREC_OPTION_3", "Detallat" );
+define( "TFREC_OPTION_4", "Importa a la Base de Dades directament" );
+define( "TFREC_BTN", "Comença el procés" );
+
+define( "TFREC_COLS_TR", "Títol" );
+define( "TFREC_COLS_AR", "Intèrpret" );
+define( "TFREC_COLS_AL", "Àlbum" );
+define( "TFREC_COLS_GE", "Gènere" );
+define( "TFREC_COLS_FS", "Mida" );
+define( "TFREC_COLS_TI", "Durada" );
+define( "TFREC_COLS_TN", "Número de cançó" );
+define( "TFREC_COLS_TC", "Comptador de cançons" );
+define( "TFREC_COLS_YR", "Any" );
+define( "TFREC_COLS_DT", "Data" );
+define( "TFREC_COLS_DA", "Data afegida" );
+define( "TFREC_COLS_BR", "Bit rate" );
+define( "TFREC_COLS_SR", "Sample rate" );
+define( "TFREC_COLS_VA", "Ajust de volum" );
+define( "TFREC_COLS_FK", "Tipus de fitxer" );
+define( "TFREC_COLS_CT", "Comentaris" );
+define( "TFREC_COLS_LC", "Ubicació" );
+
+define( "TFREC_ERROR_NOMUDIR_1", "El directori que heu seleccionat per escanejar no forma part del que teniu desat al fitxer de preferències." );
+define( "TFREC_ERROR_NOMUDIR_2", "no es troba al directori de música definit." );
+define( "TFREC_ERROR_NODIR", "Si us plau, indiqueu un directori vàlid per a buscar per als fitxers de MP3." );
+define( "TFREC_ERROR_NOFILE", "Tots els fitxers de la seva llibreria de música han estat importats prËviament." );
+
+define( "TFREC_VIEW", "Veure el fitxer a importar." );
+define( "TFREC_PROCEED", "Accedeix a les eines d'importació." );
+define( "TFIMPT_PROCEED", "Cataloga els fitxers de música." );
+
+define( "TFREC_SUCCESS_1", "s'han trobat fitxers MP3 i han estat tramesos a importació." );
+define( "TFREC_SUCCESS_2", "No s'ha trobat cap error." );
+define( "TFREC_SUCCESS_3", "s'han creat fitxers d'importació i s'ha tramés per ser importats." );
+
+define( "TFREC_INSERT_1", "Ha insertat" );
+define( "TFREC_INSERT_2", "cançó(ns)" );
+define( "TFREC_INSERT_3", "intèrpret(s)" );
+define( "TFREC_INSERT_4", "àlbum(s)" );
+define( "TFREC_INSERT_5", "gènere(s)" );
+
+define( "TFREC_FORM_HELP_1", "Els següents" );
+define( "TFREC_FORM_HELP_2", "fitxer(s) no tenen etiquetes ID3 (o semblants) válides." );
+define( "TFREC_FORM_HELP_3", "Per importar aquests fitxers, heu d'introduir manualment la informació adequada." );
+define( "TFREC_FORM_HELP_4", "A la llista de sota sols apareixen uns pocs fitxers per tal de limitar el temps de descàrrega d'aquesta pantalla." );
+define( "TFREC_FORM_BTN", "Crea un nou fixter d'importació" );
+
+##################################################
+
+?>
