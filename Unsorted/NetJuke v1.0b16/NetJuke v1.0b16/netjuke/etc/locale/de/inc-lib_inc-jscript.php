7d67b3c218dad7fe38a200884795da1eb778e295
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:51 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-lib_inc-jscript.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-lib_inc-jscript.php
@@ -0,0 +1,16 @@
+<?php
+
+##################################################
+
+# /lib/inc-jscript.php
+
+# UNUSED SINCE 1.0b9, VALUES NOW IN inc-lib_inc-common.php
+
+##################################################
+
+define( "JSCRIPT_PLIST_1", "Bitte wählen sie eine bestehende Spielliste zum editieren aus.\\nOder wählen sie einige Titel aus und erstellen sie eine neue." );
+define( "JSCRIPT_PLIST_2", "Sie haben keinen der unteren Titel gewählt." );
+
+##################################################
+
+?>
