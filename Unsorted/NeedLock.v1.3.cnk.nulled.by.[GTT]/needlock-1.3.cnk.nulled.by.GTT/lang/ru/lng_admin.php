c0c93da6c0179c9b14deaef25bbcaae418dfe211
me-suzy <me.suzana@gmail.com>
2025-08-13 06:39:08 +0300

    Add Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/ru/lng_admin.php

---
+++ Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/ru/lng_admin.php
@@ -0,0 +1,292 @@
+<?php
+
+$lang['yes']                        =   'äÁ';
+$lang['no']                         =   'îÅÔ';
+$lang['user']		                =	'ÐÏÌØÚÏ×ÁÔÅÌØ';
+$lang['login']		                =	'×ÏÊÔÉ';
+$lang['username']	                =	'éÍÑ ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['realname']	                =	'æ.é.ï.';
+$lang['password']	                =	'ðÁÒÏÌØ';
+$lang['next']		                =	'>>';
+$lang['previous']	                =	'<<';
+$lang['page']		                =	'ÓÔÒÁÎÉÃÁ';
+$lang['pages']		                =	'ÓÔÒÁÎÉÃÙ';
+$lang['of']		                    =	'ÉÚ';
+$lang['add']	   	                =	'ÄÏÂÁ×ÉÔØ';
+$lang['delete']		                =	'ÕÄÁÌÉÔØ';
+$lang['edit']  		                =	'ÉÚÍÅÎÉÔØ';
+$lang['suspend']  		            =	'ÐÒÉÏÓÔÁÎÏ×ÉÔØ';
+$lang['ban']  		                =	'ÚÁÐÒÅÔÉÔØ';
+$lang['save']		                =	'ÓÏÈÒÁÎÉÔØ';
+$lang['date']  		                =	'ÄÁÔÁ';
+$lang['time']  		                =	'×ÒÅÍÑ';
+$lang['never']                      =   'îÉËÏÇÄÁ';
+$lang['january']	                =	'ÑÎ×ÁÒØ';
+$lang['february']	                =	'ÆÅ×ÒÁÌØ';
+$lang['march']		                =	'ÍÁÒÔ';
+$lang['april']		                =	'ÁÐÒÅÌØ';
+$lang['may']		                =	'ÍÁÊ';
+$lang['june']		                =	'ÉÀÎØ';
+$lang['july']		                =	'ÉÀÌØ';
+$lang['augest']		                =	'Á×ÇÕÓÔ';
+$lang['september']	                =	'ÓÅÎÔÑÂÒØ';
+$lang['october']   	                =	'ÏËÔÑÂÒØ';
+$lang['november']	                =	'ÎÏÑÂÒØ';
+$lang['december']	                =	'ÄÅËÁÂÒØ';
+$lang['monday']		                =	'ÐÏÎÅÄÅÌØÎÉË';
+$lang['tuesday']	                =	'×ÔÏÒÎÉË';
+$lang['wednesday']	                =	'ÓÒÅÄÁ';
+$lang['thursday']	                =	'ÞÅÔ×ÅÒÇ';
+$lang['friday']		                =	'ÐÑÔÎÉÃÁ';
+$lang['saturday']	                =	'ÓÕÂÂÏÔÁ';
+$lang['sunday']		                =	'×ÏÓËÒÅÓÅÎØÅ';
+$lang['s_monday']                   =	'ðÎ';
+$lang['s_tuesday']                  =	'÷Ô';
+$lang['s_wednesday']                =	'óÒ';
+$lang['s_thursday']	                =	'þÔ';
+$lang['s_friday']                   =	'ðÔ';
+$lang['s_saturday']	                =	'óÂ';
+$lang['s_sunday']                   =	'÷Ó';
+$lang['year']   	                =	'ÇÏÄ';
+$lang['s_year']   	                =	'Ç.';
+$lang['email']   	                =	'Email';
+$lang['phone']	  	                =	'ôÅÌÅÆÏÎ';
+$lang['s_phone']   	                =	'ôÅÌ.';
+$lang['fax']	   	                =	'æÁËÓ';
+$lang['search']                     =   'ðÏÉÓË';
+$lang['of']		                    =	'ÉÚ';
+$lang['in']		                    =	'×';
+$lang['search_go']                  =   'îÁÊÔÉ!';
+$lang['search_no_results']			=	'îÉÞÅÇÏ ÎÅ ÎÁÊÄÅÎÏ';
+$lang['avdsearch']                  =       'ÒÁÓÛÉÒÅÎÎÙÊ ÐÏÉÓË';
+$lang['sitemap']                    =       'ëÁÒÔÁ ÓÁÊÔÁ';
+$lang['support']                    =       'òÏÄÄÅÒÖËÁ';
+$lang['feedback']                   =       'ïÂÒÁÔÎÁÑ Ó×ÑÚØ';
+$lang['redirect_link_text']         =       'äÌÑ ÂÙÓÔÒÏÇÏ ÐÅÒÅÈÏÄÁ ÎÁÖÍÉÔÅ ÎÁ ÜÔÕ ÓÓÙÌËÕ';
+$lang['redirect_page_subtitle']     =       'ðÅÒÅÎÁÐÒÁ×ÌÅÎÉÅ';
+$lang['error_page_text']            =       'ðÒÏÉÚÏÛÌÁ ÏÛÉÂËÁ';
+$lang['go_back']                    =       '×ÅÒÎÕÔØÓÑ';
+$lang['level']                      =       'ÕÒÏ×ÅÎØ';
+$lang['contact_webmaster']          =       'webmaster';
+$lang['fields_not_filled']          =       'úÁÐÏÌÎÅÎÙ ÎÅ ×ÓÅ ÐÏÌÑ';
+$lang['session_expired']            =       'éÚ×ÉÎÉÔÅ, ×ÒÅÍÑ õÁÛÅÇÏ ÓÅÁÎÓÁ ÒÁÂÏÔÙ ÉÓÔÅËÌÏ. ïÖÉÄÁÊÔÅ Á×ÔÏÍÁÔÉÞÅÓËÏÊ ÐÅÒÅÁÄÒÅÓÁÃÉÉ...';
+
+$lang['page_title']	                =      'áÄÍÉÎÉÓÔÒÁÔÉ×ÎÁÑ ÐÁÎÅÌØ ÕÐÒÁ×ÌÅÎÉÑ';
+$lang['words_admin_login_header']   =       '÷ÏÊÔÉ × ÁÄÍÉÎÉÓÔÒÁÔÉ×ÎÕÀ ÐÁÎÅÌØ ÕÐÒÁ×ÌÅÎÉÑ';
+$lang['login_success_text']         =       '÷Ù ÕÓÐÅÛÎÏ ×ÏÛÌÉ × ÓÉÓÔÅÍÕ. ïÖÉÄÁÊÔÅ Á×ÔÏÍÁÔÉÞÅÓËÏÊ ÐÅÒÅÁÄÒÅÓÁÃÉÉ...';
+$lang['logout_success_text']        =       '÷Ù ÕÓÐÅÛÌÉ ×ÙÛÌÉ × ÓÉÓÔÅÍÕ. ïÖÉÄÁÊÔÅ Á×ÔÏÍÁÔÉÞÅÓËÏÊ ÐÅÒÅÁÄÒÅÓÁÃÉÉ...';
+$lang['submit_login']               =       '÷ÏÊÔÉ';
+$lang['logout']	                    =       '÷ÙÊÔÉ';
+$lang['welcome']                    =	'äÏÂÒÏ ÐÏÖÁÌÏ×ÁÔØ!';
+
+$lang['menu_system_information']    =       'ÉÎÆÏÒÍÁÃÉÑ';
+$lang['menu_system_configuration']  =       'ËÏÎÆÉÇÕÒÁÃÉÑ';
+$lang['menu_anouncement_system']    =       'ÁÎÏÎÓÙ';
+$lang['menu_user_management']       =       'ÕÐÒÁ×ÌÅÎÉÅ ÐÏÌØÚÏ×ÁÔÅÌÅÍ';
+$lang['menu_administrators']        =       'ÁÄÍÉÎÉÓÔÒÁÔÏÒÙ';
+$lang['menu_backup_system']         =       'ÓÉÓÔÅÍÁ ÒÅÚÅÒ×ÎÏÇÏ ËÏÐÉÒÏ×ÁÎÉÑ';
+$lang['menu_statistics_system']     =       'ÓÔÁÔÉÓÔÉËÁ';
+
+////////////
+
+$lang['part_system_info']               =       'éÎÆÏÒÍÁÃÉÑ';
+
+$lang['part_global_setup']              =       'ëÏÎÆÉÇÕÒÁÃÉÑ';
+$lang['subpart_global_setup_status']    =       'ÓÔÁÔÕÓ ÓÉÓÔÅÍÙ';
+$lang['subpart_global_setup_global']    =       'ÇÌÏÂÁÌØÎÏ';
+$lang['subpart_global_setup_sql']       =       'ÂÁÚÁ ÄÁÎÎÙÈ';
+$lang['subpart_global_setup_email']     =       'ÐÏÞÔÁ';
+$lang['subpart_global_setup_users']     =       'ÐÏÌØÚÏ×ÁÔÅÌÉ';
+$lang['subpart_global_setup_dirs']	=	'ÄÉÒÅËÔÏÒÉÉ';
+
+$lang['part_anouncements']              =       'áÎÏÎÓÙ';
+$lang['subpart_anouncements_list']      =       'ÓÐÉÓÏË';
+$lang['subpart_anouncements_new']       =       'ÓÏÚÄÁÔØ';
+
+$lang['part_user_control']              =       'ðÏÌØÚÏ×ÁÔÅÌÉ';
+$lang['subpart_user_control_search']    =       'ÐÏÉÓË';
+$lang['subpart_user_control_prereg']    =       'ÒÅÇÉÓÔÒÁÃÉÑ';
+$lang['subpart_user_control_approve']   =       'ÐÏÄÔ×ÅÒÖÄÅÎÉÅ';
+$lang['subpart_user_control_unsuspend'] =       '×ÏÓÓÔÁÎÏ×ÉÔØ';
+$lang['subpart_user_control_unban']     =       'ÓÎÑÔØ ÚÁÐÒÅÔ';
+
+$lang['part_admins_control']            =       'áÄÍÉÎÉÓÔÒÁÔÏÒÙ';
+$lang['subpart_admins_control_list']    =       'ÓÐÉÓÏË';
+$lang['subpart_admins_control_create']  =       'ÓÏÚÄÁÔØ';
+
+$lang['part_backups']                   =       'Back-Up';
+$lang['subpart_backups_create']         =       'ÓÏÚÄÁÔØ';
+$lang['subpart_backups_restore']        =       '×ÏÓÓÔÁÎÏ×ÉÔØ';
+
+$lang['part_statistic']                 =       'CÔÁÔÉÓÔÉËÁ';
+
+$lang['part_admin_logs']                =       'öÕÒÎÁÌ ÓÉÓÔÅÍÙ';
+
+////////////
+
+$lang['sysinfo_header']                 =       'éÎÆÏÒÍÁÃÉÑ Ï ÓÉÓÔÅÍÅ';
+$lang['server_load']			=	'úÁÇÒÕÚËÁ ÓÅÒ×ÅÒÁ';
+$lang['system_status']			=	'ÓÔÁÔÕÓ ÓÉÓÔÅÍÙ';
+$lang['system_status_online']		=	'<strong>Online</strong>';
+$lang['system_status_offline']		=	'<strong>Offline</strong>';
+$lang['system_status_unknown']		=	'<strong>Undefined</strong>';
+$lang['signup_status']			=	'ÓÔÁÔÕÓ ÒÅÇÉÓÔÒÁÃÉÉ';
+$lang['new_regs_allow']			=	'<strong>On</strong>';
+$lang['new_regs_disallow']		=	'<strong>Off</strong>';
+$lang['new_regs_unknown']		=	'<strong>Undefined</strong>';
+$lang['total_users']			=	'÷ÓÅÇÏ ÐÏÌØÚÏ×ÁÔÅÌÅÊ';
+$lang['users_not_activated']		=	'îÅÁËÔÉ×ÉÒÏ×ÁÎÎÙÅ ÐÏÌØÚÏ×ÁÔÅÌÉ';
+$lang['users_not_approved']		=	'îÅÐÏÄÔ×ÅÒÖÄÅÎÎÙÅ ÐÏÌØÚÏ×ÁÔÅÌÉ';
+$lang['users_suspended']		=	'ðÒÉÏÓÔÁÎÏ×ÌÅÎÎÙÅ ÐÏÌØÚÏ×ÁÔÅÌÉ';
+
+$lang['anounces_header']		=	'îÁÓÔÒÏÊËÁ ÁÎÏÎÓÏ×';
+$lang['anounces_subheader']             =       'äÏÂÁ×ÉÔØ / ÉÚÍÅÎÉÔØ / ÕÄÁÌÉÔØ ÁÎÏÎÓÙ';
+$lang['no_anounces']			=	'îÅÔ ÁÎÏÎÓÏ×';
+$lang['anounce_date']			=	'äÁÔÁ';
+$lang['anounce_header']			=	'úÁÇÏÌÏ×ÏË';
+$lang['anounce_body']			=	'ôÅËÓÔ';
+$lang['edit_anounce']			=	'ÓÏÈÒÁÎÉÔØ';
+$lang['add_anounce']			=	'ÄÏÂÁ×ÉÔØ ÁÎÏÎÓ';
+$lang['delete_anounce']			=	'ÕÄÁÌÉÔØ';
+$lang['add_anounce_link']		=	'ÄÏÂÁ×ÉÔØ ÎÏ×ÙÊ ÁÎÏÎÓ';
+$lang['incorrect_date_format']	=	'îÅ×ÅÒÎÙÊ ÆÏÒÍÁÔ ÄÁÔÙ. üÒÁ×ÉÌØÎÙÊ ÆÏÒÍÁÔ ÇÏÄ-ÍÅÓÑÃ-ÄÅÎØ .';
+$lang['anounce_added']			=	'îÏ×ÙÊ ÁÎÏÎÓ ÄÏÂÁ×ÌÅÎ ÕÓÐÅÛÎÏ. ïÖÉÄÁÊÔÅ Á×ÔÏÍÁÔÉÞÅÓËÏÊ ÐÅÒÅÁÄÒÅÓÁÃÉÉ...';
+$lang['anounce_edited']			=	'áÎÏÎÓ ÉÚÍÅÎÅÎ ÕÓÐÅÛÎÏ. ïÖÉÄÁÊÔÅ Á×ÔÏÍÁÔÉÞÅÓËÏÊ ÐÅÒÅÁÄÒÅÓÁÃÉÉ...';
+$lang['anounce_deleted']		=	'áÎÏÎÓ ÕÄÁÌÅÎ ÕÓÐÅÛÎÏ. ïÖÉÄÁÊÔÅ Á×ÔÏÍÁÔÉÞÅÓËÏÊ ÐÅÒÅÁÄÒÅÓÁÃÉÉ...';
+$lang['incorrect_anounce_id']	=	'îÉÞÅÇÏ ÎÅ ÎÁÊÄÅÎÏ ÐÏ ÄÁÎÎÏÍÕ ID ÁÎÏÎÓÁ';
+$lang['spam_anounce']			=	'ïÔÐÒÁ×ÉÔØ ÎÏ×ÙÊ ÁÎÏÎÓ ÐÏÌØÚÏ×ÁÔÅÌÑÍ';
+
+$lang['save_config']		=   'ÓÏÈÒÁÎÉÔØ ËÏÎÆÉÇÕÒÁÃÉÀ';
+
+$lang['global_settings']        =   'çÌÏÂÁÌØÎÙÅ ÎÁÓÔÒÏÊËÉ';
+$lang['pathes']                 =   'ðÕÔÉ É ÁÄÒÅÓÁ';
+$lang['cookies']                =   'Cookies';
+$lang['site_name']              =   '<strong>éÍÑ ÓÁÊÔÁ</strong><br>ÇÌÏÂÁÌØÎÏÅ ÉÍÑ ÓÁÊÔÁ';
+$lang['base_url']               =   '<strong>ïÓÎÏ×ÎÏÊ URL ÓÉÓÔÅÍÙ</strong><br>× ËÏÎÃÅ ÏÂÑÚÁÔÅÌØÎÙÊ ÓÉÍ×ÏÌ /';
+$lang['base_path']              =   '<strong>ïÓÎÏ×ÎÏÊ ÐÕÔØ ÓÉÓÔÅÍÙ</strong><br>× ËÏÎÃÅ ÏÂÑÚÁÔÅÌØÎÙÊ ÓÉÍ×ÏÌ /';
+$lang['img_url']                =   '<strong>URL ËÁÒÔÉÎÏË</strong><br>× ËÏÎÃÅ ÏÂÑÚÁÔÅÌØÎÙÊ ÓÉÍ×ÏÌ /';
+$lang['img_path']               =   '<strong>ðÕÔØ Ë ËÁÒÔÉÎËÁÍ</strong><br>× ËÏÎÃÅ ÏÂÑÚÁÔÅÌØÎÙÊ ÓÉÍ×ÏÌ /';
+$lang['php_ext']                =   '<strong>òÁÓÛÉÒÅÎÉÅ PHP ÆÁÊÌÏ×</strong><br>ÏÂÙËÎÏ×ÅÎÎÏ .php';
+$lang['hta_top_dir']            =   '<strong>÷ÅÒÈÎÑÑ HTA ÄÉÒÅËÔÏÒÉÑ</strong><br>ÄÁÎÎÁÑ ÄÉÒÅËÔÏÒÉÑ ÓÏÄÅÒÖÉÔ ×ÓÅ ÐÁÐËÉ ÐÏÌØÚÏ×ÁÔÅÌÅÊ ÓÉÓÔÅÍÙ, ÚÁÝÉÝÅÎÎÙÅ Ó ÐÏÍÏÝØÀ .htpasswd<br>(× ÎÁÓÔÏÑÝÅÅ ×ÒÅÍÑ ÉÓÐÏÌØÚÕÅÔÓÑ ÔÏÌØËÏ ÄÉÒÅËÔÏÒÉÑ ÐÅÒ×ÏÇÏ ÕÒÏ×ÎÑ)';
+$lang['cookie_domain']          =   '<strong>äÏÍÅÎ ÄÌÑ cookies</strong><br>ÉÓÐÏÌØÚÕÊÔÅ .domain.com ÄÌÑ ÇÌÏÂÁÌØÎÙÈ cookies';
+$lang['cookie_path']            =   '<strong>ðÕÔØ Ë cookies</strong><br>ÏÔÎÏÓÉÔÅÌØÎÙÊ ÐÕÔØ ÉÚ ÄÏÍÅÎÁ × ËÏÒÎÅ×ÕÀ ÄÉÒÅËÔÏÒÉÀ';
+$lang['cookie_id']              =   '<strong>õÎÉËÁÌØÎÙÊ ID ÄÌÑ cookies</strong><br>ÉÓÐÏÌØÚÏ×ÁÔØ ÔÏÌØËÏ ÎÁÛÉ cookies';
+
+
+$lang['email_settings']         =   'îÁÓÔÒÏÊËÉ email';
+$lang['email_set_up']           =   'îÁÓÔÒÏÉÔØ ÐÏÞÔÏ×ÕÀ ÏÔÐÒÁ×ËÕ';
+$lang['admin_email']            =   'Email ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ';
+$lang['email_in']               =   'Email ÄÌÑ ×ÈÏÄÑÝÉÈ ÓÏÏÂÝÅÎÉÊ';
+$lang['email_out']              =   'Email ÄÌÑ ÉÓÈÏÄÑÝÉÈ ÓÏÏÂÝÅÎÉÊ';
+$lang['email_sender']           =   'óÐÏÓÏÂ ÏÐÒÁ×ËÉ ÐÏÞÔÙ';
+$lang['email_format']           =   'æÏÒÍÁÔ ÉÓÈÏÄÑÝÉÈ ÓÏÏÂÝÅÎÉÊ';
+$lang['email_charset']          =   'ëÏÄÉÒÏ×ËÁ ÉÓÈÏÄÑÝÉÈ ÓÏÏÂÝÅÎÉÊ';
+
+$lang['sql_settings']           =   'îÁÓÔÒÏÊËÉ ÂÁÚÙ ÄÁÎÎÙÈ SQL';
+$lang['database_set_up']        =   'îÁÓÔÒÏÉÔØ ÂÁÚÕ ÄÁÎÎÙÈ';
+$lang['sql_driver']             =   '<strong>ôÉÐ ÂÁÚÙ ÄÁÎÎÙÈ SQL</strong>';
+$lang['sql_host']               =   '<strong>èÏÓÔ ÂÁÚÙ ÄÁÎÎÙÈ SQL</strong>';
+$lang['sql_user']               =   '<strong>ðÏÌØÚÏ×ÁÔÅÌØ ÂÁÚÙ ÄÁÎÎÙÈ SQL</strong>';
+$lang['sql_pswd']               =   '<strong>ðÁÒÏÌØ Ë ÂÁÚÅ ÄÁÎÎÙÈ SQL</strong>';
+$lang['sql_port']               =   '<strong>ðÏÒÔ ÂÁÚÙ ÄÁÎÎÙÈ SQL</strong><br>ÎÅÏÂÑÚÁÔÅÌØÎÏÅ ÐÏÌÅ, ÉÓÐÏÌØÚÏ×ÁÔØ ÄÌÑ ÎÅÓÔÁÎÄÁÒÔÎÙÈ ÐÏÒÔÏ×';
+$lang['sql_name']               =   '<strong>éÍÑ ÂÁÚÙ ÄÁÎÎÙÈ SQL</strong>';
+$lang['sql_prefix']             =   '<strong>ðÒÅÆÉËÓ ÔÁÂÌÉÃ SQL</strong><br>ÉÓÐÏÌØÚÏ×ÁÔØ × ÓÌÕÞÁÅ ÏÄÎÏÊ ÂÁÚÙ ÄÁÎÎÙÈ';
+
+$lang['IS_OFFLINE']             =   'çÌÏÂÁÌØÎÙÊ ÓÔÁÔÕÓ ÓÉÓÔÅÍÙ';
+
+$lang['users_settings']         =   'îÁÓÔÒÏÊËÉ ÐÏÌØÚÏ×ÁÔÅÌÅÊ';
+$lang['users_set_up']           =   'îÁÓÔÒÏÉÔØ ÐÁÒÁÍÅÔÒÙ ÐÏÌØÚÏ×ÁÔÅÌÅÊ';
+$lang['sess_expire']            =   '<strong>äÌÉÔÅÌØÎÏÓÔØ ÓÅÓÓÉÉ (× ÓÅËÕÎÄÁÈ)</strong><br>ÅÓÌÉ ÐÏÌØÚÏ×ÁÔÅÌØ ÉÌÉ ÁÄÍÉÎÉÓÔÒÁÔÏÒ ÂÅÚÄÅÊÓÔ×ÕÅÔ × ÔÅÞÅÎÉÅ ÄÁÎÎÏÇÏ ×ÒÅÍÅÎÉ, ÓÅÓÓÉÑ ÂÕÄÅÔ Á×ÔÏÍÁÔÉÞÅÓËÉ ÚÁËÒÙÔÁ';
+$lang['is_regs_allowed']        =   '<strong>òÁÚÒÅÛÉÔØ ÎÏ×ÙÅ ÒÅÇÉÓÔÒÁÃÉÉ?</strong><br>ÍÏÖÎÏ ÏÔËÌÀÞÉÔØ ÐÒÉ ÉÚÍÅÎÅÎÉÑÈ ÌÉÂÏ ÐÅÒÅÚÁÇÒÕÚËÅ ÓÅÒ×ÅÒÁ';
+$lang['use_authcodes']          =   '<strong>éÓÐÏÌØÚÏ×ÁÔØ ËÏÄÙ Á×ÔÏÒÉÚÁÃÉÉ?</strong><br>ÐÏÌØÚÏ×ÁÔÅÌØ ÄÏÌÖÅÎ ÂÕÄÅÔ ÓÁÍÏÓÔÏÑÔÅÌØÎÏ ÁËÔÉ×ÉÒÏ×ÁÔØ Ó×ÏÀ ÕÞÅÔÎÕÀ ÚÁÐÉÓØ';
+$lang['regs_notify']            =   '<strong>éÚ×ÅÝÁÔØ Ï ÎÏ×ÙÈ ÒÅÇÉÓÔÒÁÃÉÑÈ?</strong><br>ÁÄÍÉÎÉÓÔÒÁÔÏÒ ÂÕÄÅÔ ÐÏÌÕÞÁÔØ ÐÏ ÐÏÞÔÅ ÉÚ×ÅÝÅÎÉÑ ÏÂÏ ×ÓÅÈ ÎÏ×ÙÈ ÐÏÌØÚÏ×ÁÔÅÌÑÈ';
+$lang['regs_approve']           =   '<strong>ðÏÄÔ×ÅÒÖÄÁÔØ ÕÞÅÔÎÙÅ ÚÁÐÉÓÉ</strong><br>ÐÏÄÔ×ÅÒÄÉÔØ ÌÉÂÏ ÏÔËÌÏÎÉÔØ ÌÀÂÕÀ ÎÏ×ÕÀ ÒÅÇÉÓÔÒÁÃÉÀ';
+$lang['default_hta_used']        =   '<strong>éÓÐÏÌØÚÏ×ÁÔØ .htpasswd ÐÏÌØÚÏ×ÁÔÅÌÑ ÐÏ ÕÍÏÌÞÁÎÉÀ</strong><br>×Ï ×ÓÅ ÚÁÝÉÝÅÎÎÙÅ ÄÉÒÅËÔÏÒÉÉ ÂÕÄÅÔ Á×ÔÏÍÁÔÉÞÅÓËÉ ÄÏÂÁ×ÌÅÎ ÐÏÌØÚÏ×ÁÔÅÌØ ÐÏ ÕÍÏÌÞÁÎÉÀ';
+$lang['default_hta_user']       =   '<strong>éÍÑ .htpasswd ÐÏÌØÚÏ×ÁÔÅÌÑ ÐÏ ÕÍÏÌÞÁÎÉÀ</strong>';
+$lang['default_hta_pass']       =   '<strong>ðÁÒÏÌØ .htpasswd ÐÏÌØÚÏ×ÁÔÅÌÑ ÐÏ ÕÍÏÌÞÁÎÉÀ</strong>';
+$lang['logins_decline']         =   '<strong>úÁÐÒÅÝÅÎÎÙÅ ÄÌÑ ÒÅÇÉÓÔÒÁÃÉÉ ÎÏ×ÙÅ ÉÍÅÎÁ ÐÏÌØÚÏ×ÁÔÅÌÅÊ</strong><br>ÎÁÐÒÉÍÅÒ, ÁÄÍÉÎ, ÁÄÍÉÎÉÓÔÒÁÔÏÒ É Ô.Ä.<br><br><span style="color:red; font-weight: bold;">æÏÒÍÁÔ: login|login1|login2....<br>âÅÚ ×ÅÒÔÉËÁÌØÎÏÊ ÞÅÒÔÙ × ÎÁÞÁÌÅ, × ËÏÎÃÅ, ÂÅÚ ÐÒÏÂÅÌÏ×!</span>';
+
+
+
+$lang['status_settings']        =   'çÌÏÂÁÌØÎÙÊ ÓÔÁÔÕÓ ÓÉÓÔÅÍÙ';
+$lang['sys_status']             =   '<strong>ÓÔÁÔÕÓ ÓÉÓÔÅÍÙ</strong>';
+$lang['sys_status_online']      =   'Online';
+$lang['sys_status_offline']     =   'Offline';
+$lang['sys_status_off_msg']     =   '<strong>Offline ÓÏÏÂÝÅÎÉÅ</strong><br>ÓÏÏÂÝÅÎÉÅ, ×Ù×ÏÄÉÍÏÅ ËÏÇÄÁ ÓÉÓÔÅÍÁ × ÓÏÓÔÏÑÎÉÉ offline';
+
+$lang['users_management']	    =   'ÕÐÒÁ×ÌÅÎÉÅ ÐÏÌØÚÏ×ÁÔÅÌÑÍÉ';
+
+$lang['control_member_h']		=	'îÁÓÔÒÏÉÔØ ÐÒÏÆÉÌÉ ÐÏÌØÚÏ×ÁÔÅÌÅÊ';
+
+$lang['prereg_member_h']		=	'ðÒÅÄ×ÁÒÉÔÅÌØÎÁÑ ÒÅÇÉÓÔÒÁÃÉÑ ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['interface_lang']         =   'ñÚÙË ÉÎÔÅÒÆÅÊÓÁ';
+$lang['prereg_member']		    =	'ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÔØ ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['member_registered']		=	'îÏ×ÙÊ ÐÏÌØÚÏ×ÁÔÅÌØ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎ ÕÓÐÅÛÎÏ. ÈÞÅÔÎÁÑ ÚÁÐÉÓØ ÁËÔÉ×ÉÒÏ×ÁÎÁ ÕÓÐÅÛÎÏ.<br>äÁÎÎÙÅ ÒÅÇÉÓÔÒÁÃÉÉ ÏÔÐÒÁ×ÌÅÎÙ ÎÁ email ÐÏÌØÚÏ×ÁÔÅÌÑ.';
+
+$lang['approve_member_h']		=	'ðÏÄÔ×ÅÒÄÉÔØ ÕÞÅÔÎÕÀ ÚÁÐÉÓØ ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['approve']				=	'ÐÏÄÔ×ÅÒÄÉÔØ';
+$lang['decline']				=	'ÏÔËÌÏÎÉÔØ';
+$lang['member_approved']		=	'ÒÅÇÉÓÔÒÁÃÉÑ ÐÏÌØÚÏ×ÁÔÅÌÑ ÐÏÄÔ×ÅÒÖÄÅÎÁ ÕÓÐÅÛÎÏ.<br>éÚ×ÅÝÅÎÉÅ ÏÔÐÒÁ×ÌÅÎÏ ÎÁ email.';
+$lang['member_declined']		=	'ÒÅÇÉÓÔÒÁÃÉÑ ÐÏÌØÚÏ×ÁÔÅÌÑ ÏÔËÌÏÎÅÎÁ ÕÓÐÅÛÎÏ.<br>ÈÞÅÔÎÁÑ ÚÁÐÉÓØ ÐÏÌØÚÏ×ÁÔÅÌÑ ÕÄÁÌÅÎÁ ÉÚ ÂÁÚÙ ÄÁÎÎÙÈ.';
+
+$lang['suspended_member_h']		=	'÷ÏÓÓÔÁÎÏ×ÉÔØ ÕÞÅÔÎÕÀ ÚÁÐÉÓØ ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['unsuspend']				=	'×ÏÓÓÔÁÎÏ×ÉÔØ';
+$lang['member_suspended']		=	'ÕÞÅÔÎÁÑ ÚÁÐÉÓØ ÐÏÌØÚÏ×ÁÔÅÌÑ ÐÒÉÏÓÔÁÎÏ×ÌÅÎÁ ÕÓÐÅÛÎÏ.<br>éÚ×ÅÝÅÎÉÅ ÏÔÐÒÁ×ÌÅÎÏ ÎÁ email.';
+$lang['member_unsuspended']		=	'ÕÞÅÔÎÁÑ ÚÁÐÉÓØ ÐÏÌØÚÏ×ÁÔÅÌÑ ×ÏÓÓÔÁÎÏ×ÌÅÎÁ ÕÓÐÅÛÎÏ.<br>éÚ×ÅÝÅÎÉÅ ÏÔÐÒÁ×ÌÅÎÏ ÎÁ email.';
+$lang['member_deleted']                 =       'ÕÞÅÔÎÁÑ ÚÁÐÉÓØ ÐÏÌØÚÏ×ÁÔÅÌÑ ÕÄÁÌÅÎÁ ÕÓÐÅÛÎÏ.<br>éÚ×ÅÝÅÎÉÅ ÏÔÐÒÁ×ÌÅÎÏ ÎÁ email.';
+$lang['member_baned']                   =       'ÕÞÅÔÎÁÑ ÚÁÐÉÓØ ÐÏÌØÚÏ×ÁÔÅÌÑ ÚÁÐÒÅÝÅÎÁ ÕÓÐÅÛÎÏ.';
+$lang['control_member_h']               =       'ÕÐÒÁ×ÌÅÎÉÅ ÐÏÌØÚÏ×ÁÔÅÌÑÍÉ';
+$lang['baned_member_h']                 =       'ÓÎÑÔØ ÚÁÐÒÅÔ Ó ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['unban']                          =       'ÓÎÑÔØ ÚÁÐÒÅÔ';
+$lang['member_undaned']                 =       'Õ ÕÞÅÔÎÏÊ ÚÁÐÉÓÉ ÐÏÌØÚÏ×ÁÔÅÌÑ ÚÁÐÒÅÔ ÓÎÑÔ ÕÓÐÅÛÎÏ.';
+
+$lang['edit_member_h']                  =       'éÚÍÅÎÉÔØ ÐÒÏÆÉÌØ ÐÏÌØÚÏ×ÁÔÅÌÑ.';
+$lang['id']                             =       'ID ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['regdate']                        =       'äÁÔÁ ÒÅÇÉÓÔÒÁÃÉÉ';
+$lang['expire']                         =       'äÁÔÁ ÉÓÔÅÞÅÎÉÑ ÓÒÏËÁ ÄÅÊÓÔ×ÉÑ';
+$lang['interface_lang']                 =       'ñÚÙË ÉÎÔÅÒÆÅÊÓÁ';
+$lang['last_login']                     =       'ðÏÓÌÅÄÎÉÊ ×ÈÏÄ × ÓÉÓÔÅÍÕ';
+$lang['access_dirs']                    =       'äÏÓÔÕÐÎÙÅ ÄÉÒÅËÔÏÒÉÉ';
+$lang['member_edited']                  =       'ÕÞÅÔÎÁÑ ÚÁÐÉÓØ ÐÏÌØÚÏ×ÁÔÅÌÑ ÉÚÍÅÎÅÎÁ ÕÓÐÅÛÎÏ.';
+
+$lang['admins_management']              =       'ÕÐÒÁ×ÌÅÎÉÅ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁÍÉ';
+$lang['admin_reg_h']                    =       'úÁÒÅÇÉÓÔÒÉÒÏ×ÁÔØ ÎÏ×ÏÇÏ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ';
+$lang['admin_edit_h']                   =       'ÒÅÄÁËÔÉÒÏ×ÁÔØ ÐÒÏÆÉÌØ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ';
+$lang['adm_list_h']                     =       'ÓÐÉÓÏË ÁÄÍÉÎÉÓÔÒÁÔÏÒÏ×';
+$lang['admin_level']                    =       'ÕÒÏ×ÅÎØ ÄÏÓÔÕÐÁ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ';
+$lang['level']                          =       'ÕÒÏ×ÅÎØ';
+$lang['admin_rights']                   =       'ðÒÁ×Á ÄÏÓÔÕÐÁ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ';
+$lang['admins_levels_info_h']           =       'Ó×ÅÄÅÎÉÑ ÏÂ ÕÒÏ×ÎÑÈ ÄÏÓÔÕÐÁ ÁÄÍÉÎÉÓÔÒÁÔÏÒÏ×';
+$lang['create_admin']                   =       'ÓÏÚÄÁÔØ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ';
+$lang['update_admin']                   =       'ÏÂÎÏ×ÉÔØ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ';
+$lang['admin_created']                  =       'îÏ×ÁÑ ÕÞÅÔÎÁÑ ÚÁÐÉÓØ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ ÓÏÚÄÁÎÁ ÕÓÐÅÛÎÏ.<br>äÁÎÎÙÅ ÏÔÐÒÁ×ÌÅÎÙ ÎÁ email.';
+$lang['admin_updated']                  =       'õÞÅÔÎÁÑ ÚÁÐÉÓØ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ ÉÚÍÅÎÅÎÁ ÕÓÐÅÛÎÏ.<br>äÁÎÎÙÅ ÏÔÐÒÁ×ÌÅÎÙ ÎÁ email.';
+$lang['admin_removed']                  =       'õÞÅÔÎÁÑ ÚÁÐÉÓØ ÁÄÍÉÎÉÓÔÒÁÔÏÒÁ ÕÄÁÌÅÎÁ yÓÐÅÛÎÏ.<br>éÚ×ÅÝÅÎÉÅ ÏÔÐÒÁ×ÌÅÎÏ ÎÁ email.';
+
+$lang['logging_system']                 =       'óÉÓÔÅÍÎÙÊ ÖÕÒÎÁÌ';
+$lang['admin_logs_h']                   =       'öÕÒÎÁÌ ÄÅÊÓÔ×ÉÊ ÁÄÍÉÎÉÓÔÒÁÔÏÒÏ× (ÐÏÓÌÅÄÎÉÅ 30 ÚÁÐÉÓÅÊ)';
+
+$lang['stat_system']                    =       'óÔÁÔÉÓÔÉËÁ';
+$lang['stat_sys_h']                     =       'óÔÁÔÉÓÔÉËÁ ÄÅÊÓÔ×ÉÊ ÐÏÌØÚÏ×ÁÔÅÌÅÊ';
+$lang['sort']                           =       'ÓÏÒÔÉÒÏ×ËÁ';
+$lang['order_by']                       =       'ÓÏÒÔÉÒÏ×ÁÔØ ÐÏ';
+$lang['sort_key']                       =       'ÐÏÒÑÄÏË';
+$lang['descending']                     =       'ÐÏ ÕÂÙ×ÁÎÉÀ';
+$lang['ascending']                      =       'ÐÏ ×ÏÚÒÁÓÔÁÎÉÀ';
+$lang['count_visits']                   =       'ÐÏÓÅÝÅÎÉÑ';
+$lang['used_lang']                      =       'ÑÚÙË';
+$lang['ip_address']                     =       'IP ÁÄÒÅÓ';
+$lang['user_agent']                     =       'ÂÒÁÕÚÅÒ ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['sort_now']                       =       'ÓÏÒÔÉÒÏ×ÁÔØ';
+
+$lang['protection']						=		'îÁÓÔÒÏÊËÁ ÚÁÝÉÔÙ ÄÉÒÅËÔÏÒÉÊ';
+$lang['directory_protect_unprotect']    =	    'ÕÓÔÁÎÏ×ÉÔØ / ÕÎÑÔØ ÚÁÝÉÔÕ Ó ÄÉÒÅËÔÏÒÉÊ';
+$lang['protected_dirs']					=		'úÁÝÉÝÅÎÎÙÅ ÄÉÒÅËÔÏÒÉÉ';
+$lang['notprotected_dirs']				=		'îÅÚÁÝÉÝÅÎÎÙÅ ÄÉÒÅËÔÏÒÉÉ';
+$lang['protect']						=		'ÕÓÔÁÎÏ×ÉÔØ ÚÁÝÉÔÕ';
+$lang['unprotect']						=		'ÓÎÑÔØ ÚÁÝÉÔÕ';
+$lang['no_protected_dirs']				=		'ÎÅÔ ÚÁÝÉÝÅÎÎÙÈ ÄÉÒÅËÔÏÒÉÊ';
+$lang['no_unprotected_dirs']			=		'ÎÅÔ ÎÅÚÁÝÉÝÅÎÎÙÈ ÄÉÒÅËÔÏÒÉÊ';
+
+
+?>
