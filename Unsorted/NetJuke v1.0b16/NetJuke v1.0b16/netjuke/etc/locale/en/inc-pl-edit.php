f7936b5781e2b226fe772675bef82fdd346a95ee
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:10 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-pl-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-pl-edit.php
@@ -0,0 +1,91 @@
+<?php
+
+##################################################
+
+# /pl-edit.php
+
+##################################################
+
+define( "PLEDIT_NOPLID", "Please specify a playlist to edit." );
+
+define( "PLEDIT_ADDTO_NOID", "Please select at least one track to save." );
+define( "PLEDIT_ADDTO_TITLE_1", "Untitled Playlist" );
+define( "PLEDIT_ADDTO_TITLE_2", "Random" );
+
+define( "PLEDIT_DELPL_NOID", "Please specify a playlist to delete." );
+define( "PLEDIT_DELPL_CONFIRM", "Are you sure you want to delete this playlist?" );
+
+define( "PLEDIT_DELPT_NOTRID", "Please select at least one track to delete from your playlist." );
+define( "PLEDIT_DELPT_CONFIRM", "Are you sure you want to delete the\\nselected tracks from this playlist?" );
+
+define( "PLEDIT_DENIED", "This is not one of your playlists. Please try again." );
+
+define( "PLEDIT_CHCK_HELP", "Click To Select This Track" );
+define( "PLEDIT_PLAY_HELP", "Click To Play This Track" );
+define( "PLEDIT_DLOAD_HELP", "Click To Download This Track" );
+define( "PLEDIT_INFO_HELP", "Click To View This Track's Detailed Information." );
+define( "PLEDIT_AR_HELP", "Click To View All The Tracks By This Artist." );
+define( "PLEDIT_AL_HELP", "Click To View All The Tracks On This Album." );
+define( "PLEDIT_GE_HELP", "Click To View All The Tracks Of This Genre." );
+
+define( "PLEDIT_MUSTCLICK", "Please click on the Save button to the right to save your playlist." );
+
+define( "PLEDIT_HEADER_1", "PLAYLIST" );
+
+define( "PLEDIT_SHRD", "Shared" );
+
+define( "PLEDIT_BTN_SAVE", "Save" );
+define( "PLEDIT_BTN_DELETE", "Delete" );
+define( "PLEDIT_BTN_CANCEL", "Cancel" );
+
+define( "PLEDIT_ROWCNT_1", "Registered Track(s)" );
+define( "PLEDIT_ROWCNT_2", "for" );
+
+define( "PLEDIT_QS_AR", "Artist" );
+define( "PLEDIT_QS_TR", "Track" );
+define( "PLEDIT_QS_AL", "Album" );
+define( "PLEDIT_QS_GE", "Genre" );
+define( "PLEDIT_QS_TI", "Time" );
+define( "PLEDIT_QS_BTN", "Search" );
+
+define( "PLEDIT_TB_PLIST_BTN", "Edit" );
+define( "PLEDIT_TB_PLAY_ALL", "Play" );
+define( "PLEDIT_TB_PLAY_ALL_HELP", "Click To Play This Playlist As Saved." );
+define( "PLEDIT_TB_SELECT", "Select All" );
+define( "PLEDIT_TB_SELECT_HELP", "Click To Select All The Tracks Listed On This Page" );
+define( "PLEDIT_TB_RESET", "Release All" );
+define( "PLEDIT_TB_RESET_HELP", "Click To Release All Your Selected Tracks" );
+define( "PLEDIT_TB_PLAY", "Play Selected" );
+define( "PLEDIT_TB_PLAY_HELP", "Click To Start Playing All The Selected Tracks" );
+define( "PLEDIT_TB_DELETE", "Delete Selected" );
+define( "PLEDIT_TB_DELETE_HELP", "Click To Delete Your Selected Tracks From The Current Playlist" );
+define( "PLEDIT_TB_ADJUST", "Adjust Sequence" );
+define( "PLEDIT_TB_ADJUST_HELP", "Click To Adjust The Sequence Numbering To Your Current Sort Order" );
+define( "PLEDIT_TB_ADDTO", "Copy To Playlist" );
+define( "PLEDIT_TB_ADDTO_HELP", "Click To Copy All Your Selected Tracks To The Selected Playlist" );
+
+define( "PLEDIT_COLS_OP", "Options" );
+define( "PLEDIT_COLS_SQ", "Seq." );
+define( "PLEDIT_COLS_SQ_HELP", "Click To Sort This Page By Sequence Number" );
+define( "PLEDIT_COLS_TR", "Track Name" );
+define( "PLEDIT_COLS_TR_HELP", "Click To Sort This Page By Track Name" );
+define( "PLEDIT_COLS_TI", "Time" );
+define( "PLEDIT_COLS_TI_HELP", "Click To Sort This Page By Track Time" );
+define( "PLEDIT_COLS_AR", "Artist" );
+define( "PLEDIT_COLS_AR_HELP", "Click To Sort This Page By Artist Name" );
+define( "PLEDIT_PLAY_AR_HELP", "Start Playing All The Tracks By This Artist" );
+define( "PLEDIT_FILTER_AR_HELP", "Click To List All The Albums Showcasing This Artist." );
+define( "PLEDIT_COLS_AL", "Album" );
+define( "PLEDIT_COLS_AL_HELP", "Click To Sort This Page By Album Name" );
+define( "PLEDIT_PLAY_AL_HELP", "Start Playing All The Tracks On This Album" );
+define( "PLEDIT_FILTER_AL_HELP", "Click To List All The Artists Showcased On This Album." );
+define( "PLEDIT_COLS_TN", "#" );
+define( "PLEDIT_COLS_TN_HELP", "Click To Sort This Page By Track Number" );
+define( "PLEDIT_COLS_GE", "Genre" );
+define( "PLEDIT_COLS_GE_HELP", "Click To Sort This Page By Genre Name" );
+define( "PLEDIT_PLAY_GE_HELP", "Start Playing All The Tracks In This Genre" );
+
+
+##################################################
+
+?>
