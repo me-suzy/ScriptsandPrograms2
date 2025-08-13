19203d48fe8a977e7e06e37240f1ffd9582c8a47
me-suzy <me.suzana@gmail.com>
2025-08-13 06:50:53 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_installer.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_installer.php
@@ -0,0 +1,43 @@
+<?php
+
+##################################################
+
+# UNUSED SINCE 1.0b8, NOW /installer/installer.php
+
+##################################################
+
+define( "ADMINST_ERR_VERSION", "Aquest instal·lador no es pot utilitzar amb la còpia \\nde Netjuke apuntada. Les versions no coincideixen.\\nSi us plau, aconseguiu la darrera còpia i torneu a intentar-ho." );
+
+define( "ADMINST_ERR_FSPATH_1", "Si us plau, introduïu la ubicació dels fitxers." );
+define( "ADMINST_ERR_FSPATH_2", "La ubicació dels fitxers no pot contenir://" );
+define( "ADMINST_ERR_WEBPATH_1", "Si us plau, introduïu una ubicació Web." );
+define( "ADMINST_ERR_WEBPATH_2", "La ubicació Web ha de contenir http:// or https://" );
+
+define( "ADMINST_ERR_DBTYPE", "Si us plau, introduïu un tipus de BD." );
+define( "ADMINST_ERR_DBHOST", "Si us plau, introduïu un host de BD." );
+define( "ADMINST_ERR_DBNAME", "Si us plau, introduïu un nom de BD." );
+define( "ADMINST_ERR_DBCONN", "Amb la informació proporcionada no es pot connectar a la base de dades." );
+
+define( "ADMINST_ERR_EMAIL", "Si us plau, introduïu una adreça electrònica per a l'administrador de sistemes." );
+define( "ADMINST_ERR_PASS", "Si us plau, introduïu una contrasenya per a l'administrador de sistemes." );
+
+define( "ADMINST_ERR_STREAM", "Si us plau, introduïu un servidor Streaming." );
+
+define( "ADMINST_ERR_NOSQL", "Manquen alguns components essencials a l'instal·lador.");
+
+define( "ADMINST_ERR_DENIED_1", "- No podeu utilitzar aquest instal·lador amb la còpia de Netjuke apuntada perquè ja està activa.\\n- Si us plau, utilitzeu el procés d'actualització." );
+define( "ADMINST_ERR_DENIED_2", "- Aquesta eina d'actualització no es pot utilitzar amb la còpia del Netjuke apuntada.\\n- No s'ha generat la base de dades (ha de ser instal·lada)." );
+define( "ADMINST_ERR_DENIED_3", "La ubicació dels fitxers indicada no és vàlida." );
+define( "ADMINST_ERR_DENIED_4", "Accés denegat: L'usuari administrador i la constrasenya indicats no coincideixen amb cap compte d'administrador existent." );
+
+define( "ADMINST_ERR_EXEC", "Hi ha hagut un problema en executar part del sql generat.\\nSi us plau, torneu a intentar-ho." );
+
+define( "ADMINST_FILEINFO_1", "Creat" );
+define( "ADMINST_FILEINFO_2", "Des de" );
+define( "ADMINST_FILEINFO_3", "Anomena i desa" );
+define( "ADMINST_FILEINFO_4", "després de tornar a anomenar el directori d'instal·lació temporal que encara és" );
+define( "ADMINST_FILEINFO_5", "Ha estat desat correctament. Ja podeu accedir a la nova versió a" );
+
+##################################################
+
+?>
