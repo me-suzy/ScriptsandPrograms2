41e59abcf63c19722851e9ff6a2960ac6c8f653a
me-suzy <me.suzana@gmail.com>
2025-08-13 04:24:06 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.pm_import.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.pm_import.php
@@ -0,0 +1,313 @@
+<?php
+$L = array(
+
+
+"pmachine_import_utitity" =>
+"Utilidad de importación desde pMachine",
+
+"pmachine_import_welcome" =>
+"Esta utilidad permite importar la información de pMachine a ExpressionEngine",
+
+"pmachine_import_disclaimer" =>
+"Si no usas pMachine no necesitarás esta utilidad.",
+
+"pmachine_import_information" =>
+"Guia de importación",
+
+"pmachine_import_removal" =>
+"Nota: No uses esta utilidad si no estás usando pMachine",
+
+"configuration_blurb" =>
+"Para usar esta utilidad, primero debes enviar la información de tu base de datos desde pMachine",
+
+"configuration_blurb_cont" =>
+"Encontrarás esta información en el fichero config.php",
+
+"database_info" =>
+"Info de la base de datos",
+
+"sql_server" =>
+"Dirección de servidor MySQL",
+
+"sql_username" =>
+"Nombre de usuario MySQL",
+
+"sql_password" =>
+"Contraseña MySQL",
+
+"sql_database" =>
+"Base de datos MySQL",
+
+"sql_prefix" =>
+"Prefijo de la tabla",
+
+"leave_prefix_blank" =>
+"Dejarlo en blanco a menos que estés usando un prefijo personalizado",
+
+"configure" =>
+"Configurar",
+
+"empty_field_warning" =>
+"Dejaste algunos campos vacíos",
+
+"no_database_connection" =>
+"Imposible establecer una conexión a la base de datos con los parámetros que has especificado",
+
+"table_name" =>
+"Nombre de la tabla",
+
+"table_rows" =>
+"Filas de la tabla",
+
+"table_action" =>
+"Acción",
+
+"table_status" =>
+"Estado",
+
+"completed" =>
+"Completado",
+
+"no_rows_exist" =>
+"No necesita importar",
+
+"pending" =>
+"Listo para importar",
+
+"pending_stats" =>
+"Listo para continuar",
+
+"import_now" =>
+"Importar datos",
+
+"recalculate" =>
+"Procesar datos",
+
+"step_one" =>
+"Paso Uno",
+
+"step_two" =>
+"Paso Dos",
+
+"step_three" =>
+"Paso Tres",
+
+"step_four" =>
+"Paso Cuatro",
+
+"step_five" =>
+"Paso Cinco",
+
+"import_options" =>
+"Opciones para importar",
+
+"import_members" =>
+"Miembros para importtar",
+
+"import_weblog_entries" =>
+"Artículos para importar",
+
+"recalculate_statistics" =>
+"Recuento de estadísticas",
+
+"click_to_reset_statistics" =>
+"Haz un clic aquí para actualizar las estadísticas",
+
+"reset_statistics_info" =>
+"Serás transferido a la utilidad de recuento. Cuando hayas terminado, vuelve a esta página para realizar el paso cinco",
+
+"members" =>
+"Miembros",
+
+"member_stats" =>
+"Estadísticas de miembro",
+
+"weblog_stats" =>
+"Estadísticas del Weblog",
+
+"total_members" =>
+"Número total de miembros de la base de datos de pMachine:",
+
+"select_your_account" =>
+"Selecciona tu cuenta en el pMachine",
+
+"ignore_instructions" =>
+"Como ya tienes una cuenta de miembro en el ExpressionEngine, no tiene sentido crear otra basada en la información del pMachine. De hacerlo así duplicarías las cuentas. Si seleccionas la cuenta existente en pMachine, tus artículos en el weblog quedarán asignados a tu actual cuenta en el ExpressionEngine.",
+
+"i_have_no_account" =>
+"No tengo una cuenta en pMachine",
+
+"members_per_cycle" =>
+"Número de miembros por ciclo",
+
+"cycle_instructions" =>
+"Para evitar problemas de desconexión importaremos la información del pMachine en grupos limitados. Por favor indica el número de registros que te gustaría importar de cada grupo. 1000 es un número seguro por la mayoría de los servidores. Si tienes un ordenador de altas prestaciones o un servidor dedicado puedes importar varios miles de una sola vez.",
+
+"blog_cycle_instructions" =>
+"La rutina de importación del weblog requiere mucha potencia de proceso, particularmente si los artículos contienen muchos comentarios y trackbacks. Para evitar problemas de desconexión importaremos los artículos en grupos limitados. 100 es un número seguro para los servidores. Si tienes un ordenador de altas prestaciones o un servidor dedicado, o si los artículos no contienen demasiados comentarios, entonces puedes importar un número mayor.",
+
+"save_settings" =>
+"Guardar Configuraciones",
+
+"member_import_complete" =>
+"La rutina de importación ha sido completada.",
+
+"return_to_overview" =>
+"Comenzar rutina de importación",
+
+"start_member_import" =>
+"Listo para comenzar",
+
+"members_remaining" =>
+"Total miembros que quedan para importar:",
+
+"click_to_import_members" =>
+"Importar miembros de %x a %y",
+
+"no_table_rows" =>
+"No existen filas",
+
+"default_member_group" =>
+"Asignación de grupo de miembro",
+
+"member_group_instructions" =>
+"Los miembros importados deben asignarse a un grupo. Por favor selecciona el grupo que te gustaría asignarles.",
+
+"members_imported" =>
+"Todos los miembros han sido importados",
+
+"return_to_main_menu" =>
+"Volver al menú inicial",
+
+"weblog_name" =>
+"Nombre del Weblog:",
+
+"total_weblog_entries" =>
+"Número total de artículos en este weblog:",
+
+"select_destination_blog" =>
+"Seleccionar el destino del weblog",
+
+"destination_instructions" =>
+"Por favor selecciona el weblog en ExpressionEngine donde te gustaría que se importaran los artículos del pMachine.",
+
+"destination_note" =>
+"Nota: Si no existe ningún weblog tendrás que ir a la página de ADMIN para crear el primero.",
+
+"entries_per_cycle" =>
+"Número de artículos por ciclo.",
+
+"select_destination_fields" =>
+"Seleccionar los campos de destino",
+
+"field_destination_instructions" =>
+"Por favor selecciona cada campo en ExpressionEngine donde te gustaría que se importaran los campos del pMachine.",
+
+"field_destination_instructions_two" =>
+"Selecciona &quot;No importar&quot; para evitar importar campos.",
+
+"fields_not_unique_warning" =>
+"No puedes asignar más de un campo de pMachine dentro del mismo campo de ExpressionEngine",
+
+"no_fields_assigned" =>
+"El weblog que has seleccionado no tiene ningún campo asignado. Por favor ves a la página de ADMIN y asigna un grupo de campos a este weblog.",
+
+"no_textarea_fields" =>
+"El grupo de campos asignado al weblog que has seleccionado no contiene ningún campo de texto",
+
+"pmachine_field" =>
+"Campo pMachine",
+
+"ee_field" =>
+"Campo ExpressionEngine",
+
+"none" =>
+"No importar",
+
+"blurb" =>
+"Entradilla",
+
+"body" =>
+"Cuerpo",
+
+"more" =>
+"Más",
+
+"custom1" =>
+"Personalizar1",
+
+"custom2" =>
+"Personalizar2",
+
+"custom3" =>
+"Personalizar3",
+
+"you_must_select_fields" =>
+"No has seleccionado ningún campo",
+
+"entries_remaining" =>
+"Artículos restantes:",
+
+"entries_imported" =>
+"Artículos que han sido importados",
+
+"click_to_import_entries" =>
+"Importar artículos de %x hasta %y",
+
+"batch_complete" =>
+"Sesión terminada...",
+
+"select_upload_blog" =>
+"Seleccionar el destino de la imagen subida",
+
+"upload_instructions" =>
+"Si los articulos de pMachine contienen imágenes, necesitamos rehacer la nueva localización.",
+
+"upload_note" =>
+"Note:  Esta utilidad de importación no moverá las imágenes ya existentes. Tendrás que hacerlo manualmente.",
+
+"note_regarding_categories" =>
+"Categorías del weblog",
+
+"category_note" =>
+"Si los artículos de tu pMachine están asignados a categorías, tienes que tener las mismas categorías disponibles en ExpressionEngine. Durante el proceso de importación, este script intentará emparejar cada categoría de pMachine con la correspondiente en ExpressionEngine. Si no lo hace, ese artículo ignorará la categoría.",
+
+"import_mailinglist" =>
+"Importar lista de correo:",
+
+"total_emails" =>
+"Número total de emails:",
+
+"emails_remaining" =>
+"Emails pendientes:",
+
+"emails_per_cycle" =>
+"Número de direcciones de email por ciclo",
+
+"email_instructions" =>
+"Para evitar problemas de desconexión importaremos las direcciones de email en grupos limitados. Esta rutina requiere mucha potencia de proceso, por lo tanto 5000 emails por grupo es un buen número para los servidores. Por favor indica el número de registros por grupo que te gustaría importar.",
+
+"mailinglist" =>
+"Lista de correo",
+
+"mailinglist_imported" =>
+"La lista de correo ha sido importada",
+
+"click_to_import_mailinglist" =>
+"Importar emails de %x hasta  %y",
+
+"clear_preferences" =>
+"Borrar las preferencias",
+
+"click_to_clear_prefs" =>
+"Haz un clic aquí para borrar las preferencias",
+
+"clear_preferences_info" =>
+"Durante este proceso de importación hemos guardado todas tus preferencias en el fichero de configuración principal. Cuando termine la importación, mejor que borres esta información.",
+
+"you_are_done_importing" =>
+"¡Ha finalizado el proceso de importación satisfactóriamente!",
+
+''=>''
+);
+?>
