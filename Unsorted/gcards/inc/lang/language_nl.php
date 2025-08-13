2a733126941352a0038b058d835076555ae3edfa
me-suzy <me.suzana@gmail.com>
2025-08-13 04:49:40 +0300

    Add Unsorted/gcards/inc/lang/language_nl.php

---
+++ Unsorted/gcards/inc/lang/language_nl.php
@@ -0,0 +1,148 @@
+<?
+// index.php
+$index01 = 'Alle Categorieen';
+$index02 = 'Kies een kaart om te versturen!';
+
+// general navigation
+$nav01 = 'Terug';
+$nav02 = 'Volgend';
+$nav03 = 'Home';
+$nav04 = 'Aantal';
+$nav05 = 'gCards Administratie Console';
+$nav06 = 'Wijzig wachtwoord';
+$nav07 = 'Log uit';
+$nav08 = 'Log In';
+$nav09 = 'Powered by';
+$nav10 = 'per pagina';
+$nav11 = 'ga <a href="javascript:history.go(-1);">back</a>';
+$nav12 = 'Terug naar';
+
+
+
+// UIfunctions.php
+$uifunc01 = 'User: ';
+
+// compose.php
+$compose01 = 'Kies een kaart!  Go <a href="index.php">back</a> to choose a card';
+$compose02 = 'Bepaal de inhoud van je eCard!';
+$compose03 = 'Naam';
+$compose04 = 'Email Adres';
+$compose05 = 'Van:';
+$compose06 = 'Aan:';
+$compose07 = 'Stuur me een bericht zodra de eCard is opgehaald.';
+$compose08 = 'Typ achter het Email adres een komma';
+
+// preview.php
+$preview01 = "Niet alle gegevens zijn ingevuld! $nav11";
+$preview02 = 'Dit is geen geldig Email adres';
+$preview03 = "Ongeldig Email adres! $nav11";
+$preview04 = 'Verzenden';
+
+// sendcard.php
+$sendcard01 = 'Error!  Formulier niet correct ingevuld.  Please go <a href="javascript:history.go(-1);">back</a>';
+$sendcard02 = 'Error kan niet invoegen: ';
+$sendcard03 = 'eCard zenden...';
+$sendcard04 = 'eCard kan niet verzonden worden...';
+
+// getcard.php
+$getcard01 = 'Error - geen kaart ID';
+$getcard02 = '$to_email haal je eCard op';
+$getcard03 = 'Zend $from_name aan eCard';
+
+// showcard.php
+$showcard01 = 'Een eCard van';
+
+// news.php
+$news01 = 'Nieuws archief';
+$news02 = 'Gepost';
+$news03 = 'door';
+$news04 = 'Nieuws artikel per pagina';
+$news05 = 'Nieuws';
+$news06 = 'Onderwerp';
+$news07 = 'Text';
+$news08 = 'Datum';
+$news09 = 'Autheur';
+
+
+
+// user related text
+$user01 = 'Gebruikersnaam';
+$user02 = 'Wachtwoord';
+$user03 = 'Je bent niet ingelogd - Je kunt niet uiloggen!';
+$user04 = 'Je bent succesvol uitgelogd...';
+$user05 = 'Wachtwoord bevestiging';
+$user06 = 'Email Adres';
+$user07 = 'Rol';
+$user08 = 'Gebruiker';
+$user09 = 'Paswoord niet juist - probeer het nog een keer';
+$user10 = '(Laat dit veld leeg om de huidige instellingen te behouden)';
+$user11 = 'Het oude wachtwoord is niet gelijk aan jou bestaande wachtwoord';
+$user12 = 'Wachtwoord met succes gewijzigd!';
+$user13 = 'Oude wachtwoord';
+$user14 = 'Nieuw wachtwoord';
+
+/*
+*********************
+** ADMIN DIRECTORY **
+*********************
+*/
+
+// Authentication
+$auth01 = 'Gebruiker kan niet worden aangemeld, probeer het nog eens';
+$auth02 = 'Vul gebruikers naam en wachtwoord in.';
+$auth03 = 'Je bent niet ingelogd.';
+$auth04 = 'Je hebt niet genoeg rechten om deze pagina te bekijken!';
+
+// admin.php
+$admin01 = 'Administratieve Opties';
+$admin02 = 'Onderhoud';
+$admin03 = "Kaart $admin02";
+$admin04 = "Categorie $admin02";
+$admin05 = "Nieuws $admin02";
+$admin06 = "Gebruikers $admin02";
+$admin07 = 'Bekijken, toevoegen, wijzigen, en verwijderen';
+$admin08 = 'ID';
+$admin09 = 'Verwijderen van database...';
+$admin10 = 'Verwijderen van server...';
+$admin11 = 'niet';
+$admin12 = 'Gewijzigd';
+$admin13 = 'Verwijderd';
+$admin14 = 'Toegevoegd';
+$admin15 = 'Toevoegen';
+$admin16 = 'Verzenden';
+
+// actions
+$action01 = 'Actie';
+$action02 = 'Toevoegen';
+$action03 = 'Wijzig';
+$action04 = 'Verwijder';
+$action05 = 'Bekijk';
+$action06 = 'Verzenden';
+$action07 = 'Afbreken';
+$action08 = 'Bekijk';
+
+// cards.php
+$cards01 = 'Afbeelding';
+$cards02 = 'Miniatuur';
+$cards03 = 'Aantal keren verzonden';
+$cards04 = 'Kaart';
+$cards05 = 'Kaart Naam';
+$cards06 = 'Laat dit veld leeg voor automatische miniatuur';
+
+// categories
+$cat01 = 'Categorie';
+$cat02 = 'Categorie Naam';
+$cat03 = 'Kaarten binnen Categorie';
+
+// upload.php
+$upload01 = 'Afbeelding niet verstuurd!';
+$upload02 = "Fout met kopieeren van bestand 'move_uploaded_file' function!";
+$upload03 = 'Afbeelding met succes verstuurd...';
+$upload04 = 'Fout met kopieeren bestand!';
+$upload05 = 'Miniatuur met succes verstuurd...';
+$upload06 = 'Verstuurde afbeelding gebruiken als minatuur...';
+$upload07 = 'Miniatuur met succes gemaakt...';
+$upload08 = 'Verstuur een miniatuur als de afbeelding geen JPEG formaat heeft.';
+
+
+?>
