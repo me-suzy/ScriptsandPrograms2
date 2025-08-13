1acedbccd34dec8f9b77c544f439052742dc9bdc
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:31 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-pl-shrd-view.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-pl-shrd-view.php
@@ -0,0 +1,80 @@
+<?php
+
+##################################################
+
+# /pl-shrd-view.php
+
+##################################################
+
+define( "PLSHRD_NOPLID", "Por favor especifique una lista para verla." );
+
+define( "PLSHRD_SAVE_NOID", "Por favor seleccione una lista para guardarla como favorita." );
+
+define( "PLSHRD_DELPL_NOID", "Por Favor Especifíque una Lista de Reproducción para ser Eliminada de sus Favoritos ." );
+define( "PLSHRD_DELPL_CONFIRM", "Esta seguro de eliminar está lista de reproducción de sus favoritos?" );
+
+define( "PLSHRD_CHCK_HELP", "Click para selecionar esta canción" );
+define( "PLSHRD_PLAY_HELP", "Click para escuchar esta canción" );
+define( "PLSHRD_DLOAD_HELP", "Click para descargar esta canción" );
+define( "PLSHRD_INFO_HELP", "Click para ver información detallada de la Canción." );
+define( "PLSHRD_AR_HELP", "Click para ver todas las canciones por este Artista." );
+define( "PLSHRD_AL_HELP", "Click para ver todas las canciones en este Álbun." );
+define( "PLSHRD_GE_HELP", "Click para ver todas las canciones de este Género." );
+
+define( "PLSHRD_HEADER_FAV", "LISTA DE FAVORITOS" );
+define( "PLSHRD_HEADER_PUB", "LISTA PUBLICA" );
+define( "PLSHRD_HEADER_BY", "Por" );
+
+define( "PLSHRD_BTN_DELETE", "Eliminar" );
+define( "PLSHRD_BTN_CANCEL", "Cancelar" );
+
+define( "PLSHRD_ROWCNT_1", "Cancion(es) registradas" );
+define( "PLSHRD_ROWCNT_2", "Por" );
+
+define( "PLSHRD_QS_AR", "Artista" );
+define( "PLSHRD_QS_TR", "Canción" );
+define( "PLSHRD_QS_AL", "Álbum" );
+define( "PLSHRD_QS_GE", "Género" );
+define( "PLSHRD_QS_TI", "Duración" );
+define( "PLSHRD_QS_BTN", "Buscar" );
+
+define( "PLSHRD_TB_PLIST_BTN", "Editar" );
+define( "PLSHRD_TB_ADDED", "Adicionado" );
+define( "PLSHRD_TB_PLAY_ALL", "Escuchar" );
+define( "PLSHRD_TB_PLAY_ALL_HELP", "Click para escuchar la lista guardada por Autor" );
+define( "PLSHRD_TB_SAVE", "Guardar como Favorito" );
+define( "PLSHRD_TB_SAVE_HELP", "Click para guardar la lista en su lista de Favoritos" );
+define( "PLSHRD_TB_SELECT", "Seleccionar Todos" );
+define( "PLSHRD_TB_SELECT_HELP", "Click para escuchar las canciones seleccionadas en esta página" );
+define( "PLSHRD_TB_RESET", "Descargar Todos" );
+define( "PLSHRD_TB_RESET_HELP", "Click para descargar todas las canciones seleccionadas" );
+define( "PLSHRD_TB_PLAY", "Escuchar seleccionadas" );
+define( "PLSHRD_TB_PLAY_HELP", "Click para escuchar todas las canciones seleccionadas" );
+define( "PLSHRD_TB_ADDTO", "Copiar a lista" );
+define( "PLSHRD_TB_ADDTO_HELP", "Click para copiar toda las canciones seleccionadas de la lista" );
+
+define( "PLSHRD_COLS_OP", "Opciones" );
+define( "PLSHRD_COLS_SQ", "Secuencia." );
+define( "PLSHRD_COLS_SQ_HELP", "Click para ordenar esta página por secuencia numérica.");
+define( "PLSHRD_COLS_TR", "Nombre Canción" );
+sdefine( "PLSHRD_COLS_TR_HELP", "Click para ordenar esta página por canción" );
+define( "PLSHRD_COLS_TI", "Duración" );
+define( "PLSHRD_COLS_TI_HELP", "Click para ordenar esta página por duración de la canción" );
+define( "PLSHRD_COLS_AR", "Artista" );
+define( "PLSHRD_COLS_AR_HELP", "Click para ordenar esta página por Artísta" );
+define( "PLSHRD_PLAY_AR_HELP", "Escuchar todas las canciones por este artísta" );
+define( "PLSHRD_FILTER_AR_HELP", "Click Para Listar Todos Los Album(S) Para Este Artista" );
+define( "PLSHRD_COLS_AL", "Album" );
+define( "PLSHRD_COLS_AL_HELP", "Click para ordenar esta páginas por Álbum" );
+define( "PLSHRD_PLAY_AL_HELP", "Escuchar todas las canciones de este Álbum" );
+define( "PLSHRD_FILTER_AL_HELP", "Click Para Listar Todos Los Artistas En Este Album" );
+define( "PLSHRD_COLS_TN", "#" );
+define( "PLSHRD_COLS_TN_HELP", "Clic para ordenar esta página por número de canción" );
+define( "PLSHRD_COLS_GE", "Género" );
+define( "PLSHRD_COLS_GE_HELP", "Click para ordenar esta página por Género" );
+define( "PLSHRD_PLAY_GE_HELP", "Escuchar todas las canciones en este Género" );
+
+
+##################################################
+
+s?>
