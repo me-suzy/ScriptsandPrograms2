707673c84b62e38a01e496c46de69ff616c0f3c0
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:13 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_user-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_user-edit.php
@@ -0,0 +1,51 @@
+<?php
+
+##################################################
+
+# /admin/user-edit.php
+
+##################################################
+
+define( "USEDIT_CHECKFORM_1", "Si us plau, introduïu un nom." );
+define( "USEDIT_CHECKFORM_2", "Si us plau, introduïu una adreça electrònica." );
+define( "USEDIT_CHECKFORM_3", "Si us plau, introduïu una adreça electrònica vàlida." );
+define( "USEDIT_CHECKFORM_4", "Si us plau, escolliu una contrasenya." );
+define( "USEDIT_CHECKFORM_5", "Si us plau, confirmeu la nova contrasenya." );
+define( "USEDIT_CHECKFORM_6", "La nova contrasenya és massa curta.\\n   Si us plau, utilitzeu un mínim de 6 caràcters." );
+define( "USEDIT_CHECKFORM_7", "La nova contrasenya és massa llarga.\\n   Si us plau, utilitzeu un màxim de 20 caràcters." );
+define( "USEDIT_CHECKFORM_8", "Les contrasenyes no coincideixen.\\n   Si us plau, torneu a escriure la nova contrasenya." );
+
+define( "USEDIT_DENIED_1", "No s'ha trobat aquesta pista." );
+define( "USEDIT_DENIED_2", "Accés denegat. Si us plau, torneu a intentar-ho." );
+define( "USEDIT_DENIED_3", "No podeu esborrar-vos vosaltres mateixos." );
+define( "USEDIT_DENIED_4", "Aquesta adreça electrònica o sobrenom ja estan registrats." );
+
+define( "USEDIT_NEW_HEADER", "REGISTRE D'USUARI");
+define( "USEDIT_NEW_CAPTION", "Si us plau, ompliu aquest formulari per obrir un nou compte." );
+define( "USEDIT_NEW_SUCCESS", "Heu registrat el següent usuari" );
+
+define( "USEDIT_EDIT_HEADER", "INFORMACIÓ DEL COMPTE" );
+define( "USEDIT_EDIT_CAPTION", "Utilitzeu aquest formulari per modificar la informació del compte." );
+
+define( "USEDIT_FORMS_GROUP", "Grup" );
+define( "USEDIT_FORMS_NAME", "Nom complet" );
+define( "USEDIT_FORMS_NICKNAME", "Sobrenom" );
+define( "USEDIT_FORMS_EMAIL", "Adreça electrònica" );
+define( "USEDIT_FORMS_PASSWD_1", "Contrasenya" );
+define( "USEDIT_FORMS_PASSWD_2", "Confirmeu la contrasenya" );
+define( "USEDIT_FORMS_PASSWD_3", "Nova contrasenya" );
+define( "USEDIT_FORMS_PASSWD_4", "Confirmeu la nova contrasenya" );
+define( "USEDIT_FORMS_CREATED", "Data creació" );
+define( "USEDIT_FORMS_UPDATED", "Data actualització" );
+define( "USEDIT_FORMS_LOGIN_CNT", "Compte d'entrada" );
+define( "USEDIT_FORMS_BTN_LOGIN", "Registre" );
+define( "USEDIT_FORMS_BTN_REGISTER", "Entra" );
+define( "USEDIT_FORMS_BTN_SAVE", "Desa" );
+define( "USEDIT_FORMS_BTN_RESET", "Desfés canvis" );
+define( "USEDIT_FORMS_BTN_DEL", "Esborra" );
+define( "USEDIT_FORMS_BTN_DEL_CONF", "Esteu segur que voleu esborrar aquest usuari?" );
+define( "USEDIT_FORMS_BTN_USLIST", "Llista d'usuaris" );
+
+##################################################
+
+?>
