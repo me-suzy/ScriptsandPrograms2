2cb73a2685985466bc1c711ccb5d5bbf89172cda
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:36 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/english/lang.templates.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/english/lang.templates.php
@@ -0,0 +1,337 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Templates
+//----------------------------
+
+"php_parse_location" =>
+"Stage at which PHP should be parsed",
+
+"input" =>
+"Input",
+
+"output" =>
+"Output",
+
+"template_loop" =>
+"You have causes a template loop due to improperly nested sub-templates",
+
+"template_intro" =>
+"This page is where templates are managed.",
+
+"create_new_template_group" =>
+"Create a New Template Group",
+
+"new_template_group_form" =>
+"New Template Group",
+
+"edit_template_group_form" =>
+"Edit Template Group",
+
+"name_of_template_group" =>
+"Template Group Name",
+
+"template_group_instructions" =>
+"The name must be a single word with no spaces",
+
+"undersores_allowed" =>
+"(underscores and dashes are allowed)",
+
+"new_template_form" =>
+"New Template Form",
+
+"edit_template_form" =>
+"Edit Template Form",
+
+"name_of_template" =>
+"Template Name",
+
+"group_name" =>
+"Group Name",
+
+"order" =>
+"Order",
+
+"reserved_name" =>
+"The name you have chosen is a reserved word that can not be used.",
+
+"type_of_template" =>
+"Type of Template",
+
+"webpage" =>
+"Web Page",
+
+"css_stylesheet" =>
+"CSS Stylesheet",
+
+"rss" =>
+"RSS Page",
+
+"missing_name" =>
+"Your template must have a name",
+
+"template_name_taken" =>
+"The template name you submitted is already taken",
+
+"template_group_del_conf" =>
+"Template group delete confirm",
+
+"template_del_conf" =>
+"Template delete confirm",
+
+"edit_template" =>
+"Edit Template",
+
+"add_a_template" =>
+"Add a New Template",
+
+"delete_template_group" =>
+"Delete Group",
+
+"edit_template_group" =>
+"Edit Group",
+
+"group_order" =>
+"Group order",
+
+"export_templates" =>
+"Export Templates",
+
+"template_name" =>
+"Template Name:",
+
+"template_management" =>
+"Template Management",
+
+"current_template" =>
+"Current Template:",
+
+"preferences" =>
+"Preferences",
+
+"prefs" =>
+"Prefs",
+
+"hits" =>
+"Hits",
+
+"hit_counter" =>
+"Hit counter",
+
+"template_updated" =>
+"Template Updated",
+
+"template_created" =>
+"Template Created",
+
+"form_is_empty" =>
+"You submitted an empty form",
+
+"all_fields_required" =>
+"You must submit all form fields",
+
+"illegal_characters" =>
+"The name you submitted may only contain alpha-numeric characters, underscores, and dashes",
+
+"edit_template_group_order" =>
+"Edit Group Order",
+
+"template_group_updated" =>
+"Template Group Updated",
+
+"template_group_created" =>
+"Template Group Created",
+
+"delete_this_group" =>
+"Delete the following template group?",
+
+"all_templates_will_be_nuked" =>
+"ALL TEMPLATES CONTAINED WITHIN THIS GROUP WILL BE PERMANENTLY DELETED!",
+
+"template_group_deleted" =>
+"Template group deleted",
+
+"edit_group_order" =>
+"Edit Group Order",
+
+"template_size" =>
+"Template Size",
+
+"small" =>
+"Small",
+
+"medium" =>
+"Medium",
+
+"large" =>
+"Large",
+
+"view_rendered_template" =>
+"View Rendered Template",
+
+"save_history" =>
+"Save Revision",
+
+"view" =>
+"View",
+
+"clear" =>
+"Clear",
+
+"select_all" =>
+"Select All",
+
+"export" =>
+"Export",
+
+"export_as" =>
+"Export files as",
+
+"export_will_be_zip" =>
+"Note: Your exported data will sent as a ZIP archive",
+
+"choose_templates" =>
+"Choose the templates you would like to export as files",
+
+"unable_to_write_files" =>
+"Unable to write the files to your cache directory",
+
+"revision_history" =>
+"View Revision History",
+
+"clear_revision_history" =>
+"Clear Revision History",
+
+"history_cleared" =>
+"Template revision history has been deleted.",
+
+"close_window" =>
+"Close Window",
+
+"template_preferences" =>
+"Template Preferences",
+
+"delete_template" =>
+"Delete Template",
+
+"delete_this_template" =>
+"Delete the following template:",
+
+"template_deleted" =>
+"Template Deleted",
+
+"default_site_page" =>
+"Indicates master site default page",
+
+"is_site_default" =>
+"Make the index template in this group your site's home page?",
+
+"allow_php" =>
+"Allow PHP in this template?",
+
+"enable_cache" =>
+"Enable Template Caching?",
+
+"cache_interval" =>
+"Time interval between cache refresh",
+
+"in_minutes" =>
+"Time is set in minutes",
+
+"php_security_warning" =>
+"Setting this to \"yes\" has security implications",
+
+"consult_manual" =>
+"Please consult manual",
+
+"preferences_updated" =>
+"Preferences Updated",
+
+"choose_default_data" =>
+"Default template data",
+
+"blank_template" =>
+"None - create an empty template",
+
+"template_from_library" =>
+"Use a template from your library",
+
+"an_existing_template" =>
+"Duplicate an existing template",
+
+"you_must_submit_a_name" =>
+"Your template must have a name",
+
+"member_group" =>
+"Member Group",
+
+"can_view_template" =>
+"Can View This Page",
+
+"require_logged_in" =>
+"Require users to be logged-in to view this page?",
+
+"restrict_to_group" =>
+"Restrict viewing to member groups",
+
+"logged_out_select_blurb" =>
+"If you selected \"yes\", when a logged-out user tries to access this page, show this one instead:",
+
+"no_access_select_blurb" =>
+"If you selected \"no\" in any of the above:",
+
+"no_access_instructions" =>
+"When unauthorized users try to access this page, show this one instead:",
+
+"template_notes" =>
+"Template Notes",
+
+"template_notes_desc" =>
+"Use this form field to store notes and information about this template",
+
+"global_variables" =>
+"Global Variables",
+
+"global_variables_explanation" =>
+"You can define your own variables and add them any of your templates.",
+
+"no_global_variables" =>
+"There are currently no global variables",
+
+"global_variable_name" =>
+"Variable Name",
+
+"create_new_global_variable" =>
+"Create a New Global Variable",
+
+"variable_name" =>
+"Variable Name",
+
+"global_variable_syntax" =>
+"Variable Syntax",
+
+"variable_data" =>
+"Variable Content",
+
+"global_var_created" =>
+"Global Variable Created",
+
+"global_var_updated" =>
+"Global Variable Updated",
+
+"delete_global_variable" =>
+"Delete Global Variable",
+
+"delete_this_variable" =>
+"Delete the following Global Variable?",
+
+"variable_deleted" =>
+"Variable Deleted",
+
+
+// END
+''=>''
+);
+?>
