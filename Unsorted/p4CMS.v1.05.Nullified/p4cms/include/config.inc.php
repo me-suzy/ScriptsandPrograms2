dca89a6e69c0cde848ce99b68ebec65f97c1c3a6
me-suzy <me.suzana@gmail.com>
2025-08-13 07:34:37 +0300

    Add Unsorted/p4CMS.v1.05.Nullified/p4cms/include/config.inc.php

---
+++ Unsorted/p4CMS.v1.05.Nullified/p4cms/include/config.inc.php
@@ -0,0 +1,23 @@
+<?
+
+$sql_server = "localhost";
+
+$sql_user = "admin";
+
+$sql_passwort = "root";
+
+$sql_db = "rad7";
+
+$sql_prefix = "p4cms_";
+
+
+
+$dok_pfad = "/";
+
+$p4cms_pfad = "/p4CMS";
+
+$abs_pfad = "c:/myserver/apache/htdocs/p4cms/";
+
+
+
+?>
