b53074640f8389709b3d5bf9e9a7b93e8003e2db
me-suzy <me.suzana@gmail.com>
2025-08-13 06:03:40 +0300

    Add Unsorted/linksubmit/footer.php

---
+++ Unsorted/linksubmit/footer.php
@@ -0,0 +1,16 @@
+
+<center>
+<br>
+<font size="4">
+<?php
+echo $text3 ?>
+<br>
+<?php echo $text4 ?><br>
+  <a href="<?php echo $url2 ?>" target=_blank"> 
+  <?php 
+ echo $urltext2 ?>
+  </a></font> <br>
+</center><br>
+<div align="center"><a href="submit.php"><font size="3" face="Verdana, Arial, Helvetica, sans-serif">Click 
+  Here To Submit Your Site</font></a></div><br>
+</html>
