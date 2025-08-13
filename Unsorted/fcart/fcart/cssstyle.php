80251f4e37102e31c87fc8a262c1d8d565533143
me-suzy <me.suzana@gmail.com>
2025-08-13 04:35:32 +0300

    Add Unsorted/fcart/fcart/cssstyle.php

---
+++ Unsorted/fcart/fcart/cssstyle.php
@@ -0,0 +1,7 @@
+<style type="text/css">
+A:link {text-decoration: none; color : <? echo $cl_css_link ?>}
+A:visited {text-decoration: none; color : <? echo $cl_css_vizited ?>}
+A:hover {text-decoration: none; color : <? echo $cl_css_hover ?>}
+A:active {text-decoration: none; color : <? echo $cl_css_hover ?>}
+body,p,div,th,td{<? echo $font_css ?>}
+</style> 
