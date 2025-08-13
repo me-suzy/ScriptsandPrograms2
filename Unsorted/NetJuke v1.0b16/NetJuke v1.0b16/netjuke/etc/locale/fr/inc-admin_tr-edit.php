51055a8840fbff7d481cdff4cb3d8b8cb4315847
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:12 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_tr-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_tr-edit.php
@@ -0,0 +1,69 @@
+<?php
+
+##################################################
+
+# /admin/tr-edit.php
+
+##################################################
+
+define( "TREDIT_HEADER", "ÉDITION DE PISTE" );
+
+define( "TREDIT_DELCHILD_HELP_1", "La valeur N/A est mandataire et ne peut être éffacée." );
+define( "TREDIT_DELCHILD_HELP_2", "La valeur N/A est mandataire et ne peut être modifiée.\\nVeuillez créer une nouvelle entrée pour modifier la valeur." );
+
+define( "TREDIT_ERROR_INSERT", "Error d\\'insertion dans" );
+define( "TREDIT_ERROR_TR_T", "Erreur de mise à jour avec netjuke_tracks" );
+define( "TREDIT_ERROR_AR_T", "Erreur de mise à jour avec netjuke_artists" );
+define( "TREDIT_ERROR_AL_T", "Erreur de mise à jour avec netjuke_albums" );
+define( "TREDIT_ERROR_GE_T", "Erreur de mise à jour avecnetjuke_genres" );
+define( "TREDIT_ERROR_TR", "Veuillez soumettre un titre de fichier" );
+define( "TREDIT_ERROR_TN", "Veuillez soumettre un numéro de piste." );
+define( "TREDIT_ERROR_BR", "Le Bit Rate est mandataire doit être supérieur à 0." );
+define( "TREDIT_ERROR_SR", "Le Sample Rate est mandataire doit être supérieur à 0." );
+define( "TREDIT_ERROR_FK", "Veuillez soumettre un type de piste." );
+define( "TREDIT_ERROR_LC", "Veuillez soumettre la location ou l\\'adresse du fichier." );
+define( "TREDIT_ERROR_NORES", "Veuilez choisir une piste valide." );
+
+define( "TREDIT_FORM_CM", "Éditer les Commentaires" );
+define( "TREDIT_FORM_LY", "Éditer les Paroles" );
+
+define( "TREDIT_FORM_NEW", "Nouveau" );
+
+define( "TREDIT_FORM_TR", "Titre De Piste" );
+define( "TREDIT_FORM_AR", "Nom De l'Artiste" );
+define( "TREDIT_FORM_AL", "Nom De l'Album" );
+define( "TREDIT_FORM_GE", "Nom Du Genre" );
+define( "TREDIT_FORM_TI", "Durée" );
+define( "TREDIT_FORM_SECONDS", "Secondes" );
+define( "TREDIT_FORM_TN", "Numéro De Piste" );
+define( "TREDIT_FORM_BR", "\"Bit Rate\"" );
+define( "TREDIT_FORM_SR", "\"Sample Rate\"" );
+define( "TREDIT_FORM_FK", "Type De Fichier" );
+define( "TREDIT_FORM_FS", "Taille Du Fichier" );
+define( "TREDIT_FORM_FN", "Location" );
+define( "TREDIT_FORM_LC", "Compteur D'Écoute" );
+define( "TREDIT_FORM_RESET", "Mettre À Zéro" );
+
+define( "TREDIT_FORM_TR_IMG", "Icône de Piste" );
+define( "TREDIT_FORM_AR_IMG", "Icône d'Artiste" );
+define( "TREDIT_FORM_AL_IMG", "Icône d'Album" );
+
+define( "TREDIT_FORM_BTN_SAVE", "Sauvegarder" );
+define( "TREDIT_FORM_BTN_RESET", "Recommencer" );
+define( "TREDIT_FORM_BTN_DUP", "Duplicata" );
+define( "TREDIT_FORM_BTN_NEW", "Nouvelle Piste" );
+define( "TREDIT_FORM_BTN_DEL", "Effacer" );
+
+define( "TREDIT_FORM_BTN_DEL_CONF", "Êtes-vous sur de vouloir effacer cette piste?" );
+define( "TREDIT_FORM_BTN_DELCHILD_CONF", "Êtes-vous sur de vouloir effacer cette sélection?" );
+
+define( "TREDIT_PLAY", "Lecture" );
+define( "TREDIT_PLAY_HELP", "Cliquez pour débuter la lecture de cette piste." );
+define( "TREDIT_VIEW", "Voir Info" );
+define( "TREDIT_VIEW_HELP", "Accéde à la vue d'information de base" );
+define( "TREDIT_CLOSEWIN", "Fermer La Fenêtre" );
+define( "TREDIT_CLOSEWIN_HELP", "Ferme Cette Fenêtre" );
+
+##################################################
+
+?>
