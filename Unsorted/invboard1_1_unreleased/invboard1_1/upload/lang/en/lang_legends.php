dac153e90e2d8a321404934c727449081ceb2099
me-suzy <me.suzana@gmail.com>
2025-08-13 05:37:31 +0300

    Add Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_legends.php

---
+++ Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_legends.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang = array (
+
+'emo_title' => "Emoticons",
+'emo_type'  => "Code",
+'emo_img'   => "Image",
+
+'fu_title'  => "Find User Names",
+'fu_close_win' => "Close Window",
+'fu_enter_name' => "Enter part or all of the name",
+'fu_search_but' => "Search",
+
+'fu_error'      => "Error",
+
+'fu_no_data'    => "You must enter all or part of a name to search by",
+'fu_no_match'   => "We found no matches using your search criteria",
+'fu_kc_loads'   => "We found over 100 matches, please try and enter more of the username",
+'fu_back'       => "Back",
+'fu_add_mem'    => "Insert member into form",
+'fu_add_desc'   => "Please select a name from the drop down box and click on the button to add the name into the form that opened this window",
+
+
+);
+?>
