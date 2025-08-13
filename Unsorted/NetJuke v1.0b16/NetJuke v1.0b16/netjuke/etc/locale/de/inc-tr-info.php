71ad0e85afe91f3133cd67998603412af3b977d7
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:13 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-tr-info.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-tr-info.php
@@ -0,0 +1,41 @@
+<?php
+
+##################################################
+
+# /tr-info.php
+
+##################################################
+define( "TRINFO_NOID", "Bitte wählen Sie einen gültigen Titel aus." );
+
+define( "TRINFO_HEADER", "DETAILIERTE TITEL INFORMATION" );
+
+define( "TRINFO_FORM_TR", "Titel Name" );
+define( "TRINFO_FORM_AR", "Interpreten Name" );
+define( "TRINFO_FORM_AL", "Album Name" );
+define( "TRINFO_FORM_GE", "Genre Name" );
+define( "TRINFO_FORM_TI", "Dauer" );
+define( "TRINFO_FORM_TN", "Titel Number" );
+define( "TRINFO_FORM_BR", "Bitrate" );
+define( "TRINFO_FORM_SR", "Sample Rate" );
+define( "TRINFO_FORM_FK", "Dateityp" );
+define( "TRINFO_FORM_FS", "Dateigrösse" );
+define( "TRINFO_FORM_FN", "Dateiname" );
+define( "TRINFO_FORM_LC", "Anzahl gehört" );
+
+define( "TRINFO_FORM_CM", "Zeige Kommentare" );
+define( "TRINFO_FORM_LY", "Zeige Texte" );
+
+define( "TRINFO_IMG_TR", "Titel Thumbnail" );
+define( "TRINFO_IMG_AR", "Interpret Thumbnail" );
+define( "TRINFO_IMG_AL", "Album Thumbnail" );
+
+define( "TRINFO_PLAY", "Jetzt anhören" );
+define( "TRINFO_PLAY_HELP", "Klicken um diesen Titel anzuhören." );
+define( "TRINFO_EDIT", "Bearbeitungs-Modus" );
+define( "TRINFO_EDIT_HELP", "Wechselt in den Bearbeitungs-Modus." );
+define( "TRINFO_CLOSEWIN", "Fenster schliessen" );
+define( "TRINFO_CLOSEWIN_HELP", "Dieses Fenster schliessen" );
+
+##################################################
+
+?>
