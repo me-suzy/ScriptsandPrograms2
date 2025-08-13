1ed49e17e09da7181499ee164352ae58c2e7688d
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:09 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.pm_import.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.pm_import.php
@@ -0,0 +1,313 @@
+<?php
+$L = array(
+
+
+"pmachine_import_utitity" =>
+"Utilità importazione da pMachine",
+
+"pmachine_import_welcome" =>
+"Questa utilità ti permette di trasferire dati da pMachine a ExpressionEngine",
+
+"pmachine_import_disclaimer" =>
+"Se non utilizzi pMachine questa utilità non ti occorre.",
+
+"pmachine_import_information" =>
+"Guida all&#39;imoportazione",
+
+"pmachine_import_removal" =>
+"Nota: non utilizzare questa utilità se non stai utilizzando pMachine.",
+
+"configuration_blurb" =>
+"Per utilizzare questa utilità, prima di tutto devi specificare i settaggi del database.",
+
+"configuration_blurb_cont" =>
+"Puoi recuperare queste informazioni dal file config.php di pMachine.",
+
+"database_info" =>
+"Settaggi database",
+
+"sql_server" =>
+"Indirizzo server MySQL",
+
+"sql_username" =>
+"Username MySQL",
+
+"sql_password" =>
+"Password MySQL",
+
+"sql_database" =>
+"Database MySQL",
+
+"sql_prefix" =>
+"Prefisso tabelle",
+
+"leave_prefix_blank" =>
+"Non compilare se utilizzi il prefisso di default",
+
+"configure" =>
+"Configura",
+
+"empty_field_warning" =>
+"Hai lasciato qualche campo vuoto",
+
+"no_database_connection" =>
+"Non è possibile effettuare una connessiona al database con i settaggi specificati",
+
+"table_name" =>
+"Nome tabella",
+
+"table_rows" =>
+"Righe tabella",
+
+"table_action" =>
+"Operazione",
+
+"table_status" =>
+"Stato",
+
+"completed" =>
+"Completato",
+
+"no_rows_exist" =>
+"Importazione non necessaria",
+
+"pending" =>
+"Pronto per l&#39;importazione",
+
+"pending_stats" =>
+"Pronto per l&#39;esecuzione",
+
+"import_now" =>
+"Importa dati",
+
+"recalculate" =>
+"Elabora dati",
+
+"step_one" =>
+"Fase 1",
+
+"step_two" =>
+"Fase 2",
+
+"step_three" =>
+"Fase 3",
+
+"step_four" =>
+"Fase 4",
+
+"step_five" =>
+"Fase 5",
+
+"import_options" =>
+"Importa opzioni",
+
+"import_members" =>
+"Importa utenti",
+
+"import_weblog_entries" =>
+"Importa interventi",
+
+"recalculate_statistics" =>
+"Ricalcola statistiche",
+
+"click_to_reset_statistics" =>
+"Clicca qui per aggiornare le statistiche",
+
+"reset_statistics_info" =>
+"Sarai trasferito all&#39;utilità di ricalcolo statitiche.  Appena terminato, torna a questa pagina per proseguire con la fase 5.",
+
+"members" =>
+"Utenti",
+
+"member_stats" =>
+"Statistiche utenti",
+
+"weblog_stats" =>
+"Statistiche weblog",
+
+"total_members" =>
+"Numero totale di utenti nel database di pMachine:",
+
+"select_your_account" =>
+"Seleziona il tuo account in pMachine",
+
+"ignore_instructions" =>
+"Poiché possiedi già un account in ExpressionEngine, non avrebbe senso crearne un altro con le informazioni di pMachine. Farlo creerebbe un duplicato. Se selezioni l&#39;attuale account di pMachine, i tuoi interventi saranno assegnati al tuo attuale account in ExpressionEngine.",
+
+"i_have_no_account" =>
+"Non possiedo un account in pMachine",
+
+"members_per_cycle" =>
+"Numero di utenti per ciclo",
+
+"cycle_instructions" =>
+"Per prevenire problemi di time-out i dati di pMachine verranno importati in cicli successivi. Indica il numero di record per ciclo da importare. 1000 è un numero adeguato per la maggioranza dei server, ma se disponi di prestazioni elevate e/o di un server dedicato puoi aumentare questo numero a diverse migliaia.",
+
+"blog_cycle_instructions" =>
+"La procedura di importazione è un processo gravoso, specialmente se gli interventi contengono molti commenti e trackback. Per prevenire problemi di time-out i dati di pMachine verranno importati in cicli successivi. 100 è un numero adeguato per la maggioranza dei server, ma se disponi di prestazioni elevate e/o di un server dedicato, o se gli interventi non contengono troppi commenti, puoi incrementare questo numero.",
+
+"save_settings" =>
+"Salva settaggi",
+
+"member_import_complete" =>
+"Il processo di importazione degli utenti è concluso.",
+
+"return_to_overview" =>
+"Avvia il processo di importazione",
+
+"start_member_import" =>
+"Pronto per proseguire",
+
+"members_remaining" =>
+"Utenti residui da importare:",
+
+"click_to_import_members" =>
+"Importo utenti da %x a %y",
+
+"no_table_rows" =>
+"Non esistono righe",
+
+"default_member_group" =>
+"Assegnazione categorie utenti",
+
+"member_group_instructions" =>
+"Gli utenti importati devono essere assegnati a una categoria. Scegli a quale categoria vuoi assegnarli.",
+
+"members_imported" =>
+"Tutti gli utenti sono stati importati",
+
+"return_to_main_menu" =>
+"Torna al menu principale",
+
+"weblog_name" =>
+"Nome weblog:",
+
+"total_weblog_entries" =>
+"Numero totale di interventi in questo weblog:",
+
+"select_destination_blog" =>
+"Scegli il weblog di destinazione",
+
+"destination_instructions" =>
+"Scegli il weblog di ExpressionEngine su cui trasferire gli interventi da pMachine.",
+
+"destination_note" =>
+"Nota: se non esiste ancora alcun weblog, devi prima crearlo dalla pagina ADMIN.",
+
+"entries_per_cycle" =>
+"Numero di interventi per ciclo:",
+
+"select_destination_fields" =>
+"Seleziona i campi di destinazione",
+
+"field_destination_instructions" =>
+"Seleziona ogni campo di ExpressionEngine in cui vuoi inserire i campi importati da pMachine.",
+
+"field_destination_instructions_two" =>
+"Seleziona &quot;Non importare&quot; se desideri escludere il campo dall&#39;importazione.",
+
+"fields_not_unique_warning" =>
+"Non puoi assegnare più di un campo di pMachin per ogni campo di ExpressionEngine.",
+
+"no_fields_assigned" =>
+"Al weblog prescelto non è stato assegnato un gruppo di campi. Indica questo settaggio dalla pagina ADMIN prima di procedere.",
+
+"no_textarea_fields" =>
+"Il gruppo di campi assegnato al weblog non ne contiene nessuno di tipo &quot;textarea&quot;",
+
+"pmachine_field" =>
+"Campo pMachine",
+
+"ee_field" =>
+"Campo ExpressionEngine",
+
+"none" =>
+"Non importare",
+
+"blurb" =>
+"Blurb",
+
+"body" =>
+"Body",
+
+"more" =>
+"More",
+
+"custom1" =>
+"Custom1",
+
+"custom2" =>
+"Custom2",
+
+"custom3" =>
+"Custom3",
+
+"you_must_select_fields" =>
+"Non hai assegnato alcun campo",
+
+"entries_remaining" =>
+"Interventi residui:",
+
+"entries_imported" =>
+"Gli interventi sono stati trasferiti",
+
+"click_to_import_entries" =>
+"Importo gli interventi da %x a %y",
+
+"batch_complete" =>
+"Ciclo completo...",
+
+"select_upload_blog" =>
+"Seleziona la destinazione per le immagini da caricare",
+
+"upload_instructions" =>
+"Se gli interventi in pMachine contengono immagini, occorre riassegnare loro una destinazione.",
+
+"upload_note" =>
+"Nota: questa utilità di importazione non sposta fisicamente le immagini esistenti. Dovrai trasferirle manualmente alla destinazione scelta.",
+
+"note_regarding_categories" =>
+"A proposito di categorie",
+
+"category_note" =>
+"Se gli interventi in pMachine sono assegnati a categorie, dovresti rendere disponibili le stesse in ExpressionEngine. Durante il processo di importazione lo script tenterà il confronto tra le categorie di pMachine e quelle di ExpressionEngine. In caso di esito negativo, la categoria per quel particolare intervento verrà ignorata.",
+
+"import_mailinglist" =>
+"Importa mailing-list",
+
+"total_emails" =>
+"Numero totale di indirizzi:",
+
+"emails_remaining" =>
+"Indirizzi residui:",
+
+"emails_per_cycle" =>
+"Numero di indirizzi e-mail per ciclo",
+
+"email_instructions" =>
+"Per prevenire problemi di time-out gli indirizzi e-mail verranno importati in cicli successivi. Questo processo non è particolarmente gravoso, quindi 5000 indirizzi rappresentano un numero adeguato per la maggioranza dei server. Indica il numero di record da importare in ciascun ciclo.",
+
+"mailinglist" =>
+"Mailing-list",
+
+"mailinglist_imported" =>
+"La mailing-list è stata importata",
+
+"click_to_import_mailinglist" =>
+"Importo gli indirizzi da %x a %y",
+
+"clear_preferences" =>
+"Azzera preferenze",
+
+"click_to_clear_prefs" =>
+"Clicca qui per azzerare le preferenze",
+
+"clear_preferences_info" =>
+"Durante il processo di importazione tutte le preferenze specificate sono state salvate nel file config principale. Alla fine del processo è raccomandato l&#39;azzeramento di tali preferenze.",
+
+"you_are_done_importing" =>
+"Hai completato l&#39;importazione con successo!",
+
+''=>''
+);
+?>
