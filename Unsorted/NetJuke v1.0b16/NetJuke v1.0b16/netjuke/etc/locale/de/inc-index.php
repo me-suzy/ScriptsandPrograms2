8b95bdc49a6d39592bc1603c4f874e61d62a4815
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:38 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-index.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-index.php
@@ -0,0 +1,25 @@
+<?php
+
+##################################################
+
+# /index.php
+
+##################################################
+
+define( "BRWS_GE_HEADER", "ALLE GENREN" );
+define( "BRWS_GE_HELP", "Zeige alle Titel dieses Genres." );
+define( "BRWS_PLAY_GE_HELP", "Spiele alle Titel dieses Genres." );
+
+define( "BRWS_AR_HEADER", "NEUSTE INTERPRETEN" );
+define( "BRWS_AR_HELP", "Klicken um alle Titel dieses Interpreten anzuzeigen." );
+define( "BRWS_PLAY_AR_HELP", "Spiele alle Titel dieses Interpreten" );
+define( "BRWS_FILTER_AR_HELP", "Klicken um alle Alben des Interpreten anzuzeigen." );
+
+define( "BRWS_AL_HEADER", "NEUSTE ALBEN" );
+define( "BRWS_AL_HELP", "Klicken um alle Titel dieses Albums anzuzeigen." );
+define( "BRWS_PLAY_AL_HELP", "Spiele alle Titel dieses Albums" );
+define( "BRWS_FILTER_AL_HELP", "Klicken um alle Interpreten dieses Albums anzuzeigen." );
+
+##################################################
+
+?>
