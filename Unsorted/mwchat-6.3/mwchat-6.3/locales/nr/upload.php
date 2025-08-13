0193bed3149f4e8e098cad9b491f291e770fce0a
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:53 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/upload.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/upload.php
@@ -0,0 +1,35 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[UPLOAD_Title] = "Send et arkiv!";
+
+$LOCALE[UPLOAD_Info] = "Vær så snill og velg et arkiv til upload og brukeren som dette arkivet er for.";
+
+$LOCALE[UPLOAD_Button] = "Upload Now";
+
+$LOCALE[UPLOAD_File_Send] = "Arkiv sende";
+
+$LOCALE[UPLOAD_User_Send] = "Send til";
+
+$LOCALE[UPLOAD_Complete] = "Deres arkiv sendte vellykket!";
+
+$LOCALE[UPLOAD_Sent_File] = "Jeg har akkurat sendt De arkivet";
+
+$LOCALE[UPLOAD_Err_Large] = "Arkivet som De velger er til stor, eller gjorde ikke upload riktig.";
+
+$LOCALE[UPLOAD_Err_Accept] = "Brukeren registrerer ikke eller ikke aksepterer arkiver.";
+
+$LOCALE[UPLOAD_Err_Exists] = "Arkivet som De sender allerede finnes.";
+
+$LOCALE[UPLOAD_Err_Full] = "Brukeren har ikke nok fri lagringsskille for Deres arkiv.";
+
+?>
