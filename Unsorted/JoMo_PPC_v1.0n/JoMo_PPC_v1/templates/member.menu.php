0b4c72774ec69a3cfc9c925a73df6948d44fb333
me-suzy <me.suzana@gmail.com>
2025-08-13 05:56:11 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/member.menu.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/member.menu.php
@@ -0,0 +1,17 @@
+<!--{member: top menu-->
+
+<table width="100%" align="center" border="1" bgcolor="#FFFF99" cellspacing="0" bordercolor="#FFCC00">
+ <tr align="center">
+  <td> <a href="<%$selfURL%>?mode=members&memberMode=main">Main</a>
+  <td> <a href="<%$selfURL%>?mode=members&memberMode=urls">URLs</a>
+  <td> <a href="<%$selfURL%>?mode=members&memberMode=links">Keywords</a>
+  <td> <a href="<%$selfURL%>?mode=members&memberMode=banners">Banners</a>
+  <td> <a href="<%$selfURL%>?mode=members&memberMode=info">Info</a>  
+  <td> <a href="<%$selfURL%>?mode=members&memberMode=account">Account</a>
+  <td> <a href="<%$selfURL%>?mode=members&memberMode=stats">Statistics</a>
+  <td> <a href="<%$selfURL%>?mode=members&memberMode=logout">Logout</a>
+ </tr>
+
+</table>
+
+<!--}top menu-->
