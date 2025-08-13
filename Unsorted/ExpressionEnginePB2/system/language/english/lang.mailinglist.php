4e92435e8bb1494b0671bdd168cd4c909a02ca66
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:09 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/english/lang.mailinglist.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/english/lang.mailinglist.php
@@ -0,0 +1,121 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"mailinglist_module_name" =>
+"Mailing List",
+
+"mailinglist_module_description" =>
+"Basic mailing List manager",
+
+//----------------------------------------
+
+"ml_mailinglist" =>
+"Mailing List",
+
+"ml_total_in_mailinglist" =>
+"Total Email Addresses:",
+
+"ml_view_mailinglist" =>
+"View Mailing List",
+
+"ml_email_search" =>
+"Email Search",
+
+"ml_email_search_cont" =>
+"Submit a full or partial email address",
+
+"ml_email_address" =>
+"Email",
+
+"ml_delete" =>
+"Delete",
+
+"ml_no_results" =>
+"There are no results matching the criteria you submitted",
+
+"ml_delete_confirm" =>
+"Delete Email Addresses",
+
+"ml_delete_question" =>
+"Are you sure you want to delete the selected email(s)?",
+
+"ml_email_deleted" =>
+"Email address has been deleted",
+
+"ml_emails_deleted" =>
+"Email addresses have been deleted",
+
+"ml_missing_email" =>
+"You must submit an email address",
+
+"ml_invalid_email" =>
+"Invalid email address",
+
+"ml_email_already_in_list" =>
+"Your email address is already in the mailing list",
+
+"ml_banned_email" =>
+"Email address is not allowed",
+
+"ml_email_accepted" =>
+"Your email address has been added to our mailing list.",
+
+"ml_email_confirmation_sent" =>
+"You have been sent a confirmation email.",
+
+"ml_click_confirmation_link" =>
+"Please click the link contained in the email to activate your mailing list account.",
+
+"ml_mailinglist_confirmation" =>
+"Email Confirmation",
+
+"ml_thankyou_for_joining" =>
+"Thank you for joining our mailing list!",
+
+"ml_activation_instructions" =>
+"Please click the link below to confirm your email.",
+
+"ml_expiration_blurb" =>
+"If you do not want to be added to our list, ignore this email.",
+
+"ml_account_confirmed" =>
+"Your email adddress has been confirmed.",
+
+"ml_expired_date" =>
+"The authorization code you submitted has expired",
+
+"ml_unsubscribe" =>
+"Your email address has been removed from our mailing list.",
+
+"ml_unsubscribe_failed" =>
+"Unable to find the specified account number.",
+
+"ml_add_email_addresses" =>
+"Subscribe Email Addresses",
+
+"ml_add_email_addresses_cont" =>
+"Separate each email address with a space or a newline",
+
+"ml_emails_imported" =>
+"Email subscription was successful",
+
+"ml_total_emails_imported" =>
+"Total subscribed emails:",
+
+"ml_bad_email_heading" =>
+"The following emails could not be imported:",
+
+"ml_total_duplicate_emails" =>
+"Total existing emails:",
+
+
+
+// END
+''=>''
+);
+?>
