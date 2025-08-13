abc7b08b394d3a70c7c1eeb167663d9dba9e1c35
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:43 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_user-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_user-edit.php
@@ -0,0 +1,51 @@
+<?php
+
+##################################################
+
+# /admin/user-edit.php
+
+##################################################
+
+define( "USEDIT_CHECKFORM_1", "Please enter a name." );
+define( "USEDIT_CHECKFORM_2", "Please enter an email address." );
+define( "USEDIT_CHECKFORM_3", "Please enter a valid email address." );
+define( "USEDIT_CHECKFORM_4", "Please choose a password." );
+define( "USEDIT_CHECKFORM_5", "Please confirm the new password." );
+define( "USEDIT_CHECKFORM_6", "The new password is too short.\\n   Please use a minimum of 6 characters." );
+define( "USEDIT_CHECKFORM_7", "The new password is too long.\\n   Please use a maximum of 20 characters." );
+define( "USEDIT_CHECKFORM_8", "Passwords do not match.\\n   Please retype the new password." );
+
+define( "USEDIT_DENIED_1", "Sorry. Can\\'t find this playlist." );
+define( "USEDIT_DENIED_2", "Access Denied. Please try again." );
+define( "USEDIT_DENIED_3", "Sorry, but you cannot delete yourself." );
+define( "USEDIT_DENIED_4", "This email address and/or nickname are already registered." );
+
+define( "USEDIT_NEW_HEADER", "USER REGISTRATION" );
+define( "USEDIT_NEW_CAPTION", "Please fill the form below to open a new account." );
+define( "USEDIT_NEW_SUCCESS", "You have registered the following user" );
+
+define( "USEDIT_EDIT_HEADER", "ACCOUNT INFORMATION" );
+define( "USEDIT_EDIT_CAPTION", "Use the form below to modify account information." );
+
+define( "USEDIT_FORMS_GROUP", "Group" );
+define( "USEDIT_FORMS_NAME", "Full Name" );
+define( "USEDIT_FORMS_NICKNAME", "Nickname" );
+define( "USEDIT_FORMS_EMAIL", "Email Address" );
+define( "USEDIT_FORMS_PASSWD_1", "Password" );
+define( "USEDIT_FORMS_PASSWD_2", "Confirm Password" );
+define( "USEDIT_FORMS_PASSWD_3", "New Password" );
+define( "USEDIT_FORMS_PASSWD_4", "Confirm New Password" );
+define( "USEDIT_FORMS_CREATED", "Date Created" );
+define( "USEDIT_FORMS_UPDATED", "Date Updated" );
+define( "USEDIT_FORMS_LOGIN_CNT", "Login Count" );
+define( "USEDIT_FORMS_BTN_LOGIN", "Register" );
+define( "USEDIT_FORMS_BTN_REGISTER", "Login" );
+define( "USEDIT_FORMS_BTN_SAVE", "Save" );
+define( "USEDIT_FORMS_BTN_RESET", "Reset" );
+define( "USEDIT_FORMS_BTN_DEL", "Delete" );
+define( "USEDIT_FORMS_BTN_DEL_CONF", "Are you sure you want to delete this user?" );
+define( "USEDIT_FORMS_BTN_USLIST", "User List" );
+
+##################################################
+
+?>
