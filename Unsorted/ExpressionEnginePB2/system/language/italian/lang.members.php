3cd79ddec76a03a83f42bddc9844c328d14975a7
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:02 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.members.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.members.php
@@ -0,0 +1,615 @@
+<?php
+$L = array(
+
+"can_moderate_comments" =>
+"Can Moderate Comments",
+
+"total_members" =>
+"Utenti totali:",
+
+"view_members" =>
+"Mostra utenti",
+
+"validate_selected" =>
+"Abilita gli utenti selezionati",
+
+"delete_selected" =>
+"Cancella gli utenti selezionati",
+
+"send_email_notification" =>
+"Spedisci un messaggio di notifica per ogni utente abilitato",
+
+"your_account_validated" =>
+"Il tuo account è stato attivato.",
+
+"your_account_ready" =>
+"Il tuo account è stato attivato ed è pronto per l&#39;uso.",
+
+"1" =>
+"Grazie!",
+
+"no_members_to_validate" =>
+"Non ci sono account da attivare",
+
+"members_are_validated" =>
+"L&#39;utente(i) è stato attivato",
+
+"members_are_deleted" =>
+"L&#39;utente(i) è stato cancellato",
+
+"ascending" =>
+"Data - vecchi prima",
+
+"descending" =>
+"Data - recenti prima",
+
+"alpha" =>
+"Alfabetico ascendente",
+
+"alpha_desc" =>
+"Alfabetico inverso",
+
+"username_asc" =>
+"Username - ascendente",
+
+"username_desc" =>
+"Username - discendente",
+
+"screen_name_asc" =>
+"Screen-name - ascendente",
+
+"screen_name_desc" =>
+"Screen-name - discendente",
+
+"email_asc" =>
+"E-mail - ascendente",
+
+"email_desc" =>
+"E-mail - discendente",
+
+"sort_order" =>
+"Ordine di visualizzazione",
+
+"join_date" =>
+"Data di iscrizione",
+
+"last_visit" =>
+"Ultima visita",
+
+"member_group" =>
+"Categoria utenti",
+
+"group_id" =>
+"ID categoria",
+
+"no_members_matching_that_criteria" =>
+"Nessun utente corriponde ai criteri impostati",
+
+"mbrs" =>
+"Utenti",
+
+"edit_profile" =>
+"Modifica profilo",
+
+"register_member" =>
+"Iscrivi un nuovo utente",
+
+"edit_group" =>
+"Modifica categoria",
+
+"group_title" =>
+"Titolo categoria",
+
+"security_lock" =>
+"Blocco di sicurezza",
+
+"group_lock" =>
+"Blocco categorie utenti",
+
+"enable_lock" =>
+"Abilita blocco categorie",
+
+"locked" =>
+"Abilitato",
+
+"lock_description" =>
+"Se bloccata, solo un Super Admin può assegnare un utente a questa categoria. Se desideri consentire ad altri l'amministrazione degli account, è caldamente raccomandato impostare SOLO le categorie per le quali vuoi consentire le assegnazioni.",
+
+"unlocked" =>
+"Disabilitato",
+
+"create_new_member_group" =>
+"Crea una nuova categoria di utenti",
+
+"member_has_cp_access" =>
+"può accedere al pannello di controllo",
+
+"edit_member_group" =>
+"Modifica categoria utenti",
+
+"create_member_group" =>
+"Crea una categoria utenti",
+
+"delete_member_group" =>
+"Cancella categoria utenti",
+
+"member_group_deleted" =>
+"Categoria utenti cancellata",
+
+"delete_member_group_confirm" =>
+"Vuoi veramente eliminare questa categoria?",
+
+"delete_member" =>
+"Cancella utente",
+
+"delete_member_confirm" =>
+"Vuoi veramente eliminare questo utente?",
+
+"delete_members_confirm" =>
+"Vuoi veramente eliminare questi utenti?",
+
+"can_not_delete_superadmin" =>
+"Non puoi cancellare l&#39;utente principale",
+
+"member_deleted" =>
+"Utente cancellato",
+
+"members_deleted" =>
+"Utenti cancellati",
+
+"user_banning" =>
+"Interdizione utenti",
+
+"ban_preferences_updated" =>
+"Preferenze di interdizione aggiornate",
+
+"ip_address_banning" =>
+"Indirizzi IP interdetti",
+
+"ip_banning_instructions" =>
+"Inserisci ogni indirizzo IP in una singola riga",
+
+"ip_banning_instructions_cont" =>
+"Utilizza un operatore wildcard (jolly) con indirizzi IP parziali. Es.: 123.345.*",
+
+"email_address_banning" =>
+"Indirizzo e-mail interdetto",
+
+"email_banning_instructions" =>
+"Inserisci ogni indirizzo e-mail in una singola riga",
+
+"email_banning_instructions_cont" =>
+"Utilizza un operatore wildcard (jolly) con indirizzi e-mail parziali. Es.: *@domain.com",
+
+"ban_options" =>
+"Quando è tentato l&#39;accesso al sito da IP interdetti",
+
+"restrict_to_viewing" =>
+"Limita la visualizzazione solo a",
+
+"show_this_message" =>
+"Mostra questo messaggio",
+
+"send_to_site" =>
+"reindirizzali a questo sito",
+
+"username_banning" =>
+"Username riservati",
+
+"username_banning_instructions" =>
+"Inserisci ogni username in una singola riga",
+
+"screen_name_banning" =>
+"Screen-name riservati",
+
+"screen_name_banning_instructions" =>
+"Inserisci ogni screen-name in una singola riga",
+
+"only_superadmins_can_admin_groups" =>
+"Solo i Super Admin hanno accesso alla pagina Categorie utenti",
+
+"create_group_based_on_old" =>
+"Crea una nuova categoria basata su una già esistente",
+
+"group_name" =>
+"Nome della categoria utenti",
+
+"missing_group_title" =>
+"Alla categoria utenti deve essere assegnato un nome",
+
+"site_access" =>
+"Accesso al sito",
+
+"can_view_online_system" =>
+"Può vedere il sito quando è on-line",
+
+"can_view_offline_system" =>
+"Può vedere il sito quando è off-line",
+
+"mbr_profile_privs" =>
+"Privilegi profilo utente",
+
+"can_view_profiles" =>
+"Può vedere i profili pubblici",
+
+"commenting_privs" =>
+"Privilegi di pubblicazione commenti",
+
+"can_post_comments" =>
+"Può pubblicare commenti",
+
+"cp_weblog_post_privs" =>
+"Assegnazione weblog",
+
+"cp_module_access_privs" =>
+"Privilegi d&#39;accesso ai moduli",
+
+"can_access_mod" =>
+"Può accedere al modulo:",
+
+"global_cp_access" =>
+"Accesso al pannello di controllo",
+
+"can_access_cp" =>
+"Può accedere al pannello di controllo?",
+
+"cp_section_access" =>
+"Accesso all&#39;area Control Panel",
+
+"can_access_publish" =>
+"Può accedere all&#39;area PUBBLICA",
+
+"can_access_edit" =>
+"Può accedere all&#39;area MODIFICA",
+
+"can_access_design" =>
+"Può accedere all&#39;area MODELLI",
+
+"can_access_comm" =>
+"Può accedere all&#39;area COMUNICA",
+
+"can_access_modules" =>
+"Può accedere all&#39;area MODULI",
+
+"can_access_admin" =>
+"Può accedere all&#39;area ADMIN",
+
+"can_ban_users" =>
+"Può interdire altri utenti",
+
+"cp_admin_privs" =>
+"Privilegi di amministrazione pannello di controllo",
+
+"cp_comment_privs" =>
+"Privilegi di amministrazione commenti",
+
+"can_view_other_comments" =>
+"Può vedere i commenti agli interventi scritti da altri",
+
+"can_edit_own_comments" =>
+"Può modificare i commenti ai propri interventi",
+
+"can_delete_own_comments" =>
+"Può cancellare i commenti ai propri interventi",
+
+"can_edit_all_comments" =>
+"Può modificare i commenti a QUALUNQUE intervento",
+
+"can_delete_all_comments" =>
+"Può cancellare i commenti a QUALUNQUE intervento",
+
+"can_admin_weblogs" =>
+"Può amministrare i weblog e le rispettive preferenze",
+
+"can_admin_members" =>
+"Può amministrare gli account utenti",
+
+"can_delete_members" =>
+"Può eliminare utenti",
+
+"can_admin_mbr_groups" =>
+"Può cambiare categoria a un utente (solo per gruppi sbloccati)",
+
+"can_admin_mbr_templates" =>
+"Può amministrare i modelli per Profilo utente",
+
+"can_admin_utilities" =>
+"Può amministrare le utilità di sistema",
+
+"can_admin_templates" =>
+"Può amministrare i gruppi di modelli e i modelli relativi",
+
+"can_admin_preferences" =>
+"Può amministrare le preferenze generali",
+
+"can_admin_modules" =>
+"Può installare/rimuovere moduli",
+
+"cp_email_privs" =>
+"Privilegi pannello di controllo E-mail",
+
+"can_send_email" =>
+"Può spedire mesaggi tramite il pannello di controllo",
+
+"can_email_members" =>
+"Può spedire messaggi ai singoli utenti",
+
+"can_email_member_groups" =>
+"Può spedire messaggi a categorie di utenti",
+
+"can_email_mailinglist" =>
+"Può spedire messaggi alla mailing-list",
+
+"can_send_cached_email" =>
+"Può vedere/spedire e-mail archiviate",
+
+"warning" =>
+"ATTENZIONE:",
+
+"super_admin_edit_note" =>
+"Nota: nella categoria Super Admin è concesso esclusivamente di modificarne il nome",
+
+"be_careful_assigning_groups" =>
+"Usare la MASSIMA attenzione nel configurare le preferenze delle categorie - specialmente quelle evidenziate",
+
+"member_group_updated" =>
+"Categoria utenti aggiornata:",
+
+"member_group_created" =>
+"Categoria utenti creata:",
+
+"cp_weblog_privs" =>
+"Privilegi di pubblicazione e modifica weblog",
+
+"can_view_other_entries" =>
+"Può consultare gli interventi scritti da altri",
+
+"can_post_in" =>
+"Può pubblicare e modificare interventi in:",
+
+"can_edit_other_entries" =>
+"Può modificare interventi scritti da altri",
+
+"can_assign_post_authors" =>
+"Può cambiare il nome dell&#39;autore quando pubblica un intervento",
+
+"can_delete_self_entries" =>
+"Può cancellare i propri interventi",
+
+"can_delete_all_entries" =>
+"Può cancellare OGNI intervento",
+
+"cp_template_access_privs" =>
+"Privilegi di modifica modelli",
+
+"can_access_tg" =>
+"Può modificare i modelli di questo gruppo:",
+
+"can_email_from_profile" =>
+"Può scrivere ad altri utenti tramite la console e-mail nei profili",
+
+"for_profile_page" =>
+"Sarà visualizzato nella pagina Profilo utente",
+
+"custom_member_fields" =>
+"Campi Profilo personalizzati",
+
+"current_fields" =>
+"Campi Profilo utenti",
+
+"edit_member_field" =>
+"Modifica campo",
+
+"create_member_field" =>
+"Crea campo",
+
+"field_updated" =>
+"Campo aggiornato",
+
+"create_new_profile_field" =>
+"Crea un nuovo campo Profilo",
+
+"is_field_public" =>
+"Questo campo sarà visibile nei profili pubblici?",
+
+"is_field_public_cont" =>
+"Se selezioni &quot;no&quot;, sarà visibile solo per gli amministratori",
+
+"is_field_reg" =>
+"Queto campo sarà visibile nella pagina di registrazione?",
+
+"field_width" =>
+"Larghezza campo",
+
+"field_width_cont" =>
+"Può essere specificata in pixel o in percentuale",
+
+"fieldname" =>
+"Nome campo",
+
+"fieldname_cont" =>
+"Parola singola senza pazi. Sono consentiti underscore e trattini",
+
+"field_format" =>
+"Formato testo",
+
+"max_length" =>
+"Lungh. max",
+
+"max_length_cont" =>
+"Per i campi tipo &quot;text&quot;",
+
+"text_area_rows" =>
+"Righe area testo",
+
+"text_area_rows_cont" =>
+"Per i campi tipo &quot;textarea&quot;",
+
+"pull_down_items" =>
+"Opzioni lista di selezione",
+
+"pull_down_items_cont" =>
+"Per menu a tendina",
+
+"pull_down_instructions" =>
+"Inserisci ogni elemento in una singola riga",
+
+"invalid_characters" =>
+"Il nome del campo specificato contiene caratteri non ammessi",
+
+"member_data_will_be_deleted" =>
+"Tutti i dati utente contenuti in questo campo saranno irreversibilmente cancellati",
+
+"profile_field_deleted" =>
+"Campo profilo cancellato:",
+
+"no_custom_profile_fields" =>
+"Al momento non ci sono campi personalizzati Profilo",
+
+"member_search" =>
+"Ricerca utente",
+
+"member_search_results" =>
+"Risultati ricerca utente",
+
+"no_search_results" =>
+"La ricerca non ha prodotto risultati",
+
+"member_search_instructions" =>
+"Compila almeno un campo. Puoi specificare termini parziali o completi.",
+
+"can_not_delete_only_member" =>
+"Non puoi eliminare l&#39;unico utente nel database",
+
+"can_not_delete_super_admin" =>
+"Non puoi cancellare un Super admin, a meno che ne esista almeno un altro",
+
+"must_be_superadmin_to_delete_one" =>
+"Solo un Super Admin può cancellarne un altro",
+
+"email_console_log" =>
+"Log console e-mail",
+
+"no_cached_email" =>
+"Non ci sono messaggi e-mail in archivio.",
+
+"email_title" =>
+"Titolo e-mail",
+
+"from" =>
+"Da",
+
+"to" =>
+"A",
+
+"message_sent_to" =>
+"Messaggio spedito a:",
+
+"email_deleted" =>
+"Messaggio(i) cancellato",
+
+"template_set_names" =>
+"Set di modelli disponibili",
+
+"unable_to_find_templates" =>
+"Impossibile individuare i modelli per Profilo utente.",
+
+"unable_to_find_template_file" =>
+"Impossibile individuare i file dei modelli utente.",
+
+"template_updated" =>
+"Il modello è stato aggiornato",
+
+"template_set" =>
+"Set modello:",
+
+"file_not_writable" =>
+"Nota: questo campo non è scrivibile",
+
+"error_opening_template" =>
+"Errore: impossibile aprire il file del modello per editarne il contenuto.",
+
+"file_writing_instructions" =>
+"Non sarà possibile salvare le modifiche a questo modello finché i permessi non aranno settati a 666:",
+
+"aim_console" =>
+"AOL Instant Messenger Console",
+
+"breadcrumb" =>
+"Breadcrumb",
+
+"copyright" =>
+"Copyright Notice",
+
+"custom_profile_fields" =>
+"Modulo campi personalizzati Profilo",
+
+"edit_profile_form" =>
+"Modulo per Profilo utenti",
+
+"email_form" =>
+"Console e-mail",
+
+"email_prefs_form" =>
+"Preferenze modulo e-mail",
+
+"email_user_message" =>
+"Messaggi e-mail",
+
+"forgot_form" =>
+"Modulo password smarrita",
+
+"home_page" =>
+"Home page profilo utenti",
+
+"icq_console" =>
+"ICQ Console",
+
+"localization_form" =>
+"Modulo per Preferenze di localizzazione",
+
+"login_form" =>
+"Modulo log-in",
+
+"memberlist" =>
+"Pagina lista utenti",
+
+"memberlist_rows" =>
+"Righe lista utenti",
+
+"menu" =>
+"Menu profilo utenti",
+
+"notepad_form" =>
+"Modulo per Notepad",
+
+"password_change_warning" =>
+"Avviso modifica password",
+
+"public_custom_profile_fields" =>
+"Righe campi personalizzati Profilo",
+
+"public_profile" =>
+"Profilo utenti pubblico",
+
+"registration_form" =>
+"Modulo di iscrizione",
+
+"stylesheet" =>
+"Foglio di stile CSS",
+
+"success" =>
+"Messaggio di Profilo aggiornato",
+
+"username_change_disallowed" =>
+"Messaggio di divieto modifica username",
+
+"username_password_form" =>
+"Modulo username e password",
+
+"username_row" =>
+"Righe modulo username",
+
+''=>''
+);
+?>
