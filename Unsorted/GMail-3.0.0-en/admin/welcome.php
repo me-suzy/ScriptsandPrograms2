549bb89e7619d5fbaea0d01cb9c9d117cd4a6835
me-suzy <me.suzana@gmail.com>
2025-08-13 04:57:27 +0300

    Add Unsorted/GMail-3.0.0-en/admin/welcome.php

---
+++ Unsorted/GMail-3.0.0-en/admin/welcome.php
@@ -0,0 +1,59 @@
+<? include("../config.inc.php"); ?>
+<html>
+<head>
+ <title>Adminbereich</title>
+ <SCRIPT language=javascript>
+  <!--
+
+   function LmOver(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+   function LmOut(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+   function LmDown(elem, clr)
+   {elem.style.backgroundColor = clr;}
+
+  //-->
+ </SCRIPT>
+ <style> 
+  <!--  
+   a:link     { text-decoration: none      }
+   a:hover    { text-decoration: underline }
+   a:active   { text-decoration: none      }
+   a:visited  { text-decoration: none      }
+
+   a.dl:link     { color: #ffffff; text-decoration: none      }
+   a.dl:hover    { color: #ffffff; text-decoration: underline }
+   a.dl:active   { color: #ffffff; text-decoration: none      }
+   a.dl:visited  { color: #ffffff; text-decoration: none      }
+  -->
+ </style>
+</head>
+
+<body bgcolor="#ffffff">
+<center>
+ <table width="60%" bgcolor="#003366" cellspacing="1">
+  <tr>
+    <td background="table.gif" width="100%"><font face="arial" size="2" color="#ffffff"><b>&nbsp;Welcome</b></font></td>
+  </tr>
+  <tr>
+    <td bgcolor="#336699" width="100%" align="center"><font face="arial" size="2" color="#ffffff"><br>Welcome to the b1gMail admin.<br>Here you can manage your Mail Service.
+<br>
+<br>
+<table width="80%" bgcolor="#336699" cellspacing="1">
+  <tr>
+    <td bgcolor="#003366" width="100%" align="center"><font face="arial" size="2" color="#ffffff">
+      b1gMail License Key: "(not necessary)"
+    </font></td>
+  </tr>
+ </table>
+<br>
+If you have questions please click on "Support" or use the support system at www.b1g.de!
+<br><br>
+</font></td>
+  </tr>
+ </table>
+</center>
+</body>
+</html>
