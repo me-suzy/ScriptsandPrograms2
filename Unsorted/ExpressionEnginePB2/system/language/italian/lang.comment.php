f94ab280faf147596480f7041ca822676e2fe9ab
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:43 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.comment.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.comment.php
@@ -0,0 +1,85 @@
+<?php
+$L = array(
+
+
+"comment_module_name" =>
+"Commenti",
+
+"comment_module_description" =>
+"Sistema commenti",
+
+"cmt_name_not_allowed" =>
+"The name you are posting with is reserved.  Please choose a different name.",
+
+"cmt_must_be_member" =>
+"Devi essere un utente registrato per pubblicare commenti",
+
+"cmt_no_authorized_for_comments" =>
+"Non sei abilitato alla pubblicazione di commenti",
+
+"cmt_account_not_active" =>
+"Il tuo account non è ancora attivo",
+
+"cmt_comments_not_allowed" =>
+"In questo weblog non sono ammessi commenti",
+
+"cmt_missing_name" =>
+"Il campo &quot;nome&quot; è obbligatorio",
+
+"cmt_missing_email" =>
+"Il campo &quot;e-mail&quot; è obbligatorio",
+
+"cmt_invalid_email" =>
+"L&#39;indirizzo e-mail specificato non è valido",
+
+"cmt_banned_email" =>
+"L&#39;indirizzo e-mail specificato è bandito",
+
+"cmt_too_large" =>
+"Il commento inviato contiene %n caratteri.  Sono ammessi solo %x caratteri.",
+
+"cmt_comments_timelock" =>
+"È consentita la pubblicazione di un solo commento ogni %s secondi.",
+
+"cmt_duplicate_comment_warning" =>
+"Al momento non è possibile accettare il commento.",
+
+"cmt_no_preview_template_specified" =>
+"Il tag relativo al form commenti non contiene un riferimento al modello Anteprima",
+
+"cmt_comment_response_title" =>
+"Il tuo commento ha appena ricevuto una risposta",
+
+"cmt_comment_response_body" =>
+"Qualcuno ha appena risposto all&#39;intervento da te sottoscritto alle:",
+
+"cmt_comment_title" =>
+"Il titolo dell&#39;intervento è:",
+
+"cmt_comment_url" =>
+"Puoi leggere il commento a questo URL:",
+
+"cmt_comment_removal" =>
+"Per interrompere le notifiche da questo commento, clicca qui:",
+
+"cmt_comment_removal_all" =>
+"Per interrompere tutte le notifiche, clicca qui:",
+
+"cmt_notification_removal" =>
+"Notifiche annullate",
+
+"cmt_you_have_been_removed" =>
+"Non riceverai ulteriori notifiche.",
+
+"cmt_comment_accepted" =>
+"Comment Accepted",
+
+"cmt_will_be_reviewed" =>
+"Your comment will be reviewed by a moderator for approval.",
+
+"cmt_return_to_comments" =>
+"Return to Comments",
+
+''=>''
+);
+?>
