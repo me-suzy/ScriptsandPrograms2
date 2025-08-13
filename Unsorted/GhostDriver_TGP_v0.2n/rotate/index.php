77a4ea94f5e7fc79a35e7f433d96f0c287d4ac71
me-suzy <me.suzana@gmail.com>
2025-08-13 04:55:35 +0300

    Add Unsorted/GhostDriver_TGP_v0.2n/rotate/index.php

---
+++ Unsorted/GhostDriver_TGP_v0.2n/rotate/index.php
@@ -0,0 +1,51 @@
+<table width="700" border="1" cellspacing="0" cellpadding="3">
+ <tr valign="top"> 
+ <td width="33%"> 
+ <div align="center"><b>All Categories</b></div>
+ </td>
+ <td width="33%"> 
+ <div align="center"><b>Butts</b></div>
+ </td>
+ <td width="33%"> 
+ <div align="center"><b>Plumpers</b></div>
+ </td>
+ </tr>
+ <tr valign="top"> 
+ <td width="33%"> 
+ <?PHP
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//   Program Name         : GhostDriver TGP  (Random Gallery Rotator)        //
+//   Release Version      : 0.2                                              //
+//   Supplied by          : CyKuH [WTN]                                      //
+//   Nullified by         : CyKuH [WTN]                                      //
+//   Distribution         : via WebForum, ForumRU and associated file dumps  //
+//                                                                           //
+//           Random Gallery Rotator  (c) Copyright  Nibbi `2002              //
+//                    Copyright  WTN Team `2000 - `2002                      //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+include("inc.php");
+include("All.inc.txt");
+?>
+ </td>
+ <td width="33%"> 
+ <?PHP
+include("inc.php");
+include("Butts.inc.txt");
+?>
+ </td>
+ <td width="33%"> 
+ <?PHP
+include("inc.php");
+include("Ebony.inc.txt");
+?>
+ </td>
+ </tr>
+</table>
+<br>
+<br>
+<?PHP
+include("pic.inc.php");
+include("Mature.pinc.txt");
+?>
