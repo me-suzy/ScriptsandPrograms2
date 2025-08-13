ab3329484972277243850f840b6091f078e508f1
me-suzy <me.suzana@gmail.com>
2025-08-13 05:30:40 +0300

    Add Unsorted/Internet_Service_Manager_nullified_/admin/nullification.php

---
+++ Unsorted/Internet_Service_Manager_nullified_/admin/nullification.php
@@ -0,0 +1,23 @@
+<?
+$section="nullification"; //this is the section, needs to be fed to the auth script or it will assume it is general, all admin access.
+include "header.php";
+?>
+<font face="Verdana, Arial, Helvetica, sans-serif" size="2" color="white">
+    <table cellspacing="1" cellpadding="4" border="0" align="" width="100%"><tr><td colspan="2" bgcolor="#006699"><b><font color=white>Nullification  Information</td></tr><tr><td bgcolor="#EFEFEF" colspan="2"><center>Extra Note</td></tr><tr><td bgcolor="#efefef">
+      Program Name</td><td bgcolor="#EFF3EF" >Internet Service Manager</td></tr><tr><td bgcolor="#efefef">
+        Program version</td><td bgcolor="#EFF3EF" >1.0</td></tr><tr><td bgcolor="#EFEFEF">
+        Program Author</td><td bgcolor="#EFF3EF" >InternetServiceManager.com</td></tr><tr><td bgcolor="#EFEFEF">
+        Home Page</td><td bgcolor="#EFF3EF" >http://www.internetservicemanager.com</td></tr><tr><td bgcolor="#EFEFEF">
+        Retail Price</td><td bgcolor="#EFF3EF" >$120.00 United States Dollars </td></tr><tr><td bgcolor="#EFEFEF">
+        xCGI Price </td><td bgcolor="#EFF3EF" >$000.00 Always 100% Free</td></tr><tr><td bgcolor="#EFEFEF">
+        WebForum Price</td><td bgcolor="#EFF3EF" >$000.00 Always 100% Free</td></tr><tr><td bgcolor="#EFEFEF">
+        Supplied by </td><td bgcolor="#EFF3EF" >Zeedy2k</td></tr><tr><td bgcolor="#EFEFEF">
+        Nullified by </td><td bgcolor="#EFF3EF" >CyKuH [WTN]</td></tr><tr><td bgcolor="#EFEFEF">
+        Distribution</td><td bgcolor="#EFF3EF" >via WebForum and Forums File 
+        Dumps</td></tr><tr><td bgcolor="#EFEFEF">Protection </td><td bgcolor="#EFF3EF" >
+        CallHome, Referer Links, Hot links</td></tr><tr><td bgcolor="#EFEFEF">Language </td><td bgcolor="#EFF3EF" >
+        PHP, MySql</td></tr><tr><td bgcolor="#EFEFEF" colspan="2">This software contained several callback features!</td></tr><tr><td colspan="2" bgcolor="#006699" height="30">Enjoy, &copy  WTN Team `2002<br>End Of Nullification Information</td></tr>
+</table></td></tr></table>
+<?
+include "footer.php";
+?>
