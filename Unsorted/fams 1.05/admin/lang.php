54a68b38bb0ea2eb1d4bfcc3b2a2d96db76d5464
me-suzy <me.suzana@gmail.com>
2025-08-13 04:34:41 +0300

    Add Unsorted/fams 1.05/admin/lang.php

---
+++ Unsorted/fams 1.05/admin/lang.php
@@ -0,0 +1,24 @@
+<?php
+
+/* ------------------------------------------------------------ */
+/*								*/
+/*	File Area Management System (FAMS)			*/
+/*								*/
+/*	Copyright (c) 2001 by Bastian 'Buddy' Grimm		*/
+/*	Autor: Bastian Grimm					*/
+/*	Publisher: [ BG-Studios ]				*/
+/*	eMail: bastian@bg-studios.de				*/
+/*	WebSite: http://www.bg-studios.de			*/
+/*	Date: 25.07.01		Version: Lang 1.02		*/
+/*	Geändert am: 25.07.01					*/
+/*								*/
+/* ------------------------------------------------------------	*/
+
+
+
+$loginfailed = "<B>Fehler!</B><BR>Sie sind nicht angemeldet oder waren länger als 60 Minuten eingeloggt. Aus Sicherheitsgründen wurde die Verbindung beendet! <P>[ <A HREF=./index.php>Einloggen</A> ]";
+
+$nopermission = "<B>Fehler!</B><BR>Sie sind nicht berechtigt, die gewünschte Seite anzuzeigen!";
+
+
+?>
