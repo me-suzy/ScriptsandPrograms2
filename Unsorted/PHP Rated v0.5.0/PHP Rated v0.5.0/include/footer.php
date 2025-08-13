763cbef3762ab285eb01205e19f1982d3f0be4d9
me-suzy <me.suzana@gmail.com>
2025-08-13 07:45:07 +0300

    Add Unsorted/PHP Rated v0.5.0/PHP Rated v0.5.0/include/footer.php

---
+++ Unsorted/PHP Rated v0.5.0/PHP Rated v0.5.0/include/footer.php
@@ -0,0 +1,22 @@
+<?
+
+/*
+ * $Id: footer.php,v 1.1.1.1 2002/08/31 14:25:16 destiney Exp $
+ *
+ */
+
+$content = <<<EOF
+<table cellpadding="2" cellspacing="0" border="0" width="100%">
+<tr>
+<td class="smallregular" align="center">Copyright © 2001 <a class="small" href="$base_url" target="_top">$site_title</a> - All Rights Reserved<br />Powered By: <a class="small" href="http://destiney.com/prated/" target="_blank">pRated</a></td>
+</tr>
+</table>
+EOF;
+$final_output .= content_table($content);
+
+/*
+ * $Id: footer.php,v 1.1.1.1 2002/08/31 14:25:16 destiney Exp $
+ *
+ */
+
+?>
