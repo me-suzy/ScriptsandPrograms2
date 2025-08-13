47df59c96510157cf48c926e4baffd5e7735a6f3
me-suzy <me.suzana@gmail.com>
2025-08-13 03:39:35 +0300

    Add Unsorted/ebay/testsql/ebay_user_pages_text.sql

---
+++ Unsorted/ebay/testsql/ebay_user_pages_text.sql
@@ -0,0 +1,29 @@
+/* $Id: ebay_user_pages_text.sql,v 1.2 1998/10/16 01:09:42 josh Exp $ */
+/*
+ * ebay_user_pages_text --
+ * storage of the raw data for serving user
+ * pages.
+ *
+ * N.B.: Index and tablespace still need to be fixed for this.
+ */
+
+drop table ebay_user_pages_text;
+
+create table ebay_user_pages_text
+(
+user_id		number(38)
+	constraint	upagest_user_id_fk
+		references ebay_users(id),
+page_number	number(5)
+	constraint	upagest_pn_nn
+		not null,
+data_dict long raw
+	constraint	upagest_dd_nn
+		not null,
+constraint ebay_user_pagest_pk
+	primary key (user_id, page_number)
+	using index tablespace tbizdevi01
+	storage (initial 5K next 5K)
+)
+tablespace tbizdevd01
+storage (initial 1M next 1M);
