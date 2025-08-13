c72b1caeeef8efe018af8d089d22395c51394a46
me-suzy <me.suzana@gmail.com>
2025-08-13 05:01:41 +0300

    Add Unsorted/guestbook/lang/polish.php

---
+++ Unsorted/guestbook/lang/polish.php
@@ -0,0 +1,101 @@
+<?php
+# General
+
+$LANG["metatag"]    = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-2\">";
+
+$LANG["BookMess1"]  = "Blokada kodu HTML";
+$LANG["BookMess2"]  = "Kod HTML dozwolony";
+$LANG["BookMess3"]  = "Dopisz swoje zdanie..";
+$LANG["BookMess4"]  = "Wróæ do ksiêgi";
+$LANG["BookMess5"]  = "Admin..";
+$LANG["BookMess6"]  = "Dziêki za zatrzymanie siê na tej stronie. Pozostaw po sobie znak/sygna³..:)";
+$LANG["BookMess7"]  = "Komentarz";
+$LANG["BookMess8"]  = "Powrót do formularza";
+$LANG["BookMess9"]  = "Napisz komentarz do tej pozycji.";
+$LANG["BookMess10"] = "<u>Dziêki za dopisanie swojego zdania.</u><br><br>Wpis zakoñczony sukcesem! Za 3 sekundy zostaniesz przeniesiony spowrotem do ksiêgi.<br>";
+
+$LANG["EmailAdminSubject"] = "Nowy wpis";
+$LANG["EmailGuestSubject"] = "Dziêki za wpisanie siê.";
+
+$LANG["AltIP"]    = "IP zalogowany";
+$LANG["AltICQ"]   = "Wy¶lij wiadomoæ ICQ";
+$LANG["AltEmail"] = "Wy¶lij E-mail";
+$LANG["AltUrl"]   = "Poka¿ stronê";
+$LANG["AltAim"]   = "numer Gadu-Gadu: ";
+$LANG["AltCom"]   = "Twój komentarz";
+
+# Form
+$LANG["FormMess1"]   = "Wype³nij czyste pola. Po wys³aniu zostaniesz automatycznie przeniesiony do ksiêgi. Pola oznaczone (*) musz± zostaæ wype³nione.";
+$LANG["FormMess2"]   = "Emotikony w³±czone";
+$LANG["FormMess3"]   = "AGCode w³±czony";
+$LANG["FormMess4"]   = "Poka¿ legendê";
+$LANG["FormMess5"]   = "Poni¿ej znajduje sie twój wpis.";
+$LANG["FormMess6"]   = "AG Code wy³±czony";
+$LANG["FormMess7"]   = "Emotikony wy³±czone";
+$LANG["FormName"]    = "Nick";
+$LANG["FormEmail"]   = "E-mail";
+$LANG["FormLoc"]     = "Lokalizacja";
+$LANG["FormPic"]     = "Obrazek";
+$LANG["FormUrl"]     = "Strona domowa";
+$LANG["FormGender"]  = "P³eæ";
+$LANG["FormMale"]    = "Mê¿czyzna";
+$LANG["FormFemale"]  = "Kobieta";
+$LANG["FormMessage"] = "Tre¶æ";
+$LANG["FormSelect"]  = "...::::  Ksiêga Go¶ci  ::::...";
+$LANG["FormUser"]    = "U¿ytkownik";
+$LANG["FormPass"]    = "Has³o";
+$LANG["FormReset"]   = "Kasuj";
+$LANG["FormSubmit"]  = "OK";
+$LANG["FormPreview"] = "Podgl±d";
+$LANG["FormBack"]    = "Powrót";
+$LANG["FormEnter"]   = "Ksiêga - Centrum administracji<br>Wpisz poprawn± nazwê u¿ytkownika i has³o!";
+$LANG["FormButton"]  = "Dalej";
+$LANG["FormPriv"]    = "Wiadomo¶æ prywatna";
+
+# Navigation Bar
+$LANG["NavTotal"]   = "Wszystkich wpisów:";
+$LANG["NavRecords"] = "Ilo¶æ wpisów na stronie:";
+$LANG["NavPrev"]    = "Poprzednia strona";
+$LANG["NavNext"]    = "Nastêpna strona";
+
+# Post Errors
+$LANG["ErrorPost1"]  = "Wype³nij pole NICK.";
+$LANG["ErrorPost2"]  = "Wype³nij pole TREÆ.";
+$LANG["ErrorPost3"]  = "Twoja wiadomo¶æ jest za krótka lub za d³uga - min. 6 znaków max. 30 s³ów!!!";
+$LANG["ErrorPost4"]  = "Jedno z pól nie jest wype³nione poprawnie.";
+$LANG["ErrorPost5"]  = "Ksiêga - BL¡D";
+$LANG["ErrorPost6"]  = "Obrazek jest za du¿y!!";
+$LANG["ErrorPost7"]  = "Z³y typ brazka!!";
+$LANG["ErrorPost8"]  = "Akywna ochrona przed flood - spróbuj póniej!!";
+$LANG["ErrorPost9"]  = "IP zabanowany :((";
+$LANG["ErrorPost10"] = "Twoja wiadomoæ zawiera zabronione s³owa - popraw je:((";
+$LANG["ErrorPost11"] = "Nie wype³ni³e pola Komentarz";
+
+$LANG["PassMess1"] = "Wpisz poprawnego usera i has³o";
+$LANG["PassMess2"] = "Niepoprawny user lub has³o!!!!";
+$LANG["PassMess3"] = "Has³o nie jest poprawne - wypad!!!";
+
+# Days
+$weekday[0]='Niedziela';
+$weekday[1]='Poniedzia³ek';
+$weekday[2]='Wtorek';
+$weekday[3]='¦roda';
+$weekday[4]='Czwartek';
+$weekday[5]='Pi±tek';
+$weekday[6]='Sobota';
+
+# Months
+$months[0]='Stycznia';
+$months[1]='Lutego';
+$months[2]='Marca';
+$months[3]='Kwietnia';
+$months[4]='Maja';
+$months[5]='Czerwca';
+$months[6]='Lipca';
+$months[7]='Sierpnia';
+$months[8]='Wrze¶nia';
+$months[9]='Pa¼dziernika';
+$months[10]='Listopada';
+$months[11]='Grudnia';
+
+?>
