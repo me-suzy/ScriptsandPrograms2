2d0304352dae766dfaad89657b1b3cefbd52fc71
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:55 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-search.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-search.php
@@ -0,0 +1,77 @@
+<?php
+
+##################################################
+
+# /search.php
+
+##################################################
+
+define( "SRCH_NOSTR", "Si us plau, refineu la vostra recerca." );
+
+define( "SRCH_CHCK_HELP", "Feu clic per seleccionar aquesta pista" );
+define( "SRCH_PLAY_HELP", "Feu clic per reproduir aquesta pista." );
+define( "SRCH_DLOAD_HELP", "Feu clic per descarregar aquesta pista." );
+define( "SRCH_INFO_HELP", "Feu clic per accedir als detalls de la pista." );
+define( "SRCH_AR_HELP", "Feu clic per veure totes les pistes d'aquest intèrpret." );
+define( "SRCH_AL_HELP", "Feu clic per veure totes les pistes d'aquest àlbum." );
+define( "SRCH_GE_HELP", "Feu clic per veure totes les pistes d'aquest gènere." );
+
+define( "SRCH_HEADER", "RESULTATS DE LA RECERCA" );
+
+define( "SRCH_ROWCNT_1", "Pista/es trobada/es" );
+define( "SRCH_ROWCNT_2", "Pàgina" );
+define( "SRCH_ROWCNT_3", "de" );
+define( "SRCH_ROWCNT_4", "per" );
+
+define( "SRCH_QS_AR", "Intèrpret" );
+define( "SRCH_QS_TR", "Títol" );
+define( "SRCH_QS_AL", "Àlbum" );
+define( "SRCH_QS_GE", "Gènere" );
+define( "SRCH_QS_TI", "Durada" );
+define( "SRCH_QS_BTN", "Recerca" );
+
+define( "SRCH_TB_PLIST_BTN", "Edita" );
+define( "SRCH_TB_LOGIN", "Entra" );
+define( "SRCH_TB_REGISTER", "Registre" );
+define( "SRCH_TB_SELECT", "Selecciona tot" );
+define( "SRCH_TB_SELECT_HELP", "Feu clic per seleccionar totes les pistes de la pàgina" );
+define( "SRCH_TB_RESET", "Deselecciona tot" );
+define( "SRCH_TB_RESET_HELP", "Feu clic per deseleccionar totes les pistes de la pàgina" );
+define( "SRCH_TB_PLAYSEL", "Reprodueix selecció" );
+define( "SRCH_TB_PLAYSEL_HELP", "Feu clic per reproduir les pistes seleccionades." );
+define( "SRCH_TB_ADDTO", "Afegeix a" );
+define( "SRCH_TB_ADDTO_HELP", "Feu clic per afegir totes les pistes seleccionades a la llista de cançons predeterminada." );
+
+define( "SRCH_TB_PREV", "Pàgina anterior" );
+define( "SRCH_TB_PREV_HELP", "Feu clic per accedir a la pàgina de resultats anteriors." );
+define( "SRCH_TB_NEXT", "Pàgina següent" );
+define( "SRCH_TB_NEXT_HELP", "Feu clic per accedir a la següent pàgina de resultats." );
+define( "SRCH_TB_PAGE", "Pàgina" );
+define( "SRCH_TB_PAGEBOT", "Vés al peu de la pàgina" );
+define( "SRCH_TB_PAGEBOT_HELP", "Baixa al peu de pàgina." );
+define( "SRCH_TB_PAGETOP", "Vés a la capçalera de la pàgina" );
+define( "SRCH_TB_PAGETOP_HELP", "Torna a la capçalera de la pàgina" );
+define( "SRCH_TB_PAGE_BTN", "Visualitza" );
+
+define( "SRCH_COLS_OP", "Opcions" );
+define( "SRCH_COLS_TR", "Títol" );
+define( "SRCH_COLS_TR_HELP", "Feu clic per ordenar la llista per títol" );
+define( "SRCH_COLS_TI", "Durada" );
+define( "SRCH_COLS_TI_HELP", "Feu clic per ordenar la llista per durada" );
+define( "SRCH_COLS_AR", "Intèrpret" );
+define( "SRCH_COLS_AR_HELP", "Feu clic per ordenar la llista per intèrpret" );
+define( "SRCH_PLAY_AR_HELP", "Feu clic per reproduir totes les canÁons d'aquest artista" );
+define( "SRCH_FILTER_AR_HELP", "Feu click per llistar tots els àlbums d'aquest artista." );
+define( "SRCH_COLS_AL", "Àlbum" );
+define( "SRCH_COLS_AL_HELP", "Feu clic per ordenar la llista per àlbums" );
+define( "SRCH_PLAY_AL_HELP", "Feu clic per reproduir totes les canÁons d'aquest àlbum." );
+define( "SRCH_FILTER_AL_HELP", "Feu clic per llistar tots els artistes d'aquest àlbum." );
+define( "SRCH_COLS_TN", "Núm." );
+define( "SRCH_COLS_TN_HELP", "Feu clic per ordenar la llista per número de pista" );
+define( "SRCH_COLS_GE", "Gènere" );
+define( "SRCH_COLS_GE_HELP", "Feu clic per ordenar la llista per gènere" );
+
+
+##################################################
+
+?>
