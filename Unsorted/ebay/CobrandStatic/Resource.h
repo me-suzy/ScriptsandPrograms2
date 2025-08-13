56b06cf5bb7e3c054bab521bd79eed3187b523e8
me-suzy <me.suzana@gmail.com>
2025-08-13 03:04:44 +0300

    Add Unsorted/ebay/CobrandStatic/Resource.h

---
+++ Unsorted/ebay/CobrandStatic/Resource.h
@@ -0,0 +1,13 @@
+/*	$Id: Resource.h,v 1.3 1999/02/21 02:21:24 josh Exp $	*/
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by CobrandStatic.rc
+//
+
+#define IDS_FILTER        101
+#define IDS_SERVER        102
+
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_CONTROL_VALUE         103
+#define _APS_NEXT_SYMED_VALUE           103
+#define _APS_NEXT_COMMAND_VALUE         32768
