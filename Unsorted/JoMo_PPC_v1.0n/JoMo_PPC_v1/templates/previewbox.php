c7fd25ac1a0be7942890884d23c1f636d5dbff6f
me-suzy <me.suzana@gmail.com>
2025-08-13 05:56:17 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/previewbox.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/previewbox.php
@@ -0,0 +1,10 @@
+<TABLE ALIGN="CENTER" BORDER="0" CELLPADDING="5" CELLSPACING="1" width="100%" background="">
+<TR STYLE="font-size:11px; font-family:Verdana;" ALIGN="CENTER" bgcolor="#eeeeee">
+        <TD align="left" colspan="8">
+				<%$htmlcode%>
+        </TD>
+</tr>
+</table>
+
+
+
