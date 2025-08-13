6d48433d1e60247f228eb55898eef31599aaf260
me-suzy <me.suzana@gmail.com>
2025-08-13 07:35:49 +0300

    Add Unsorted/p4CMS.v1.05.Nullified/p4cms/modules/kontakt/settings.ini.php

---
+++ Unsorted/p4CMS.v1.05.Nullified/p4cms/modules/kontakt/settings.ini.php
@@ -0,0 +1,6 @@
+<?PHP
+$hintergrundfarbe = "#FFFFFF";
+$titel = "Vielen Dank!";
+$danketext = "Wir haben Ihre eMail erhalten, und werden diese schnellstm&ouml;glich bearbeiten.";
+$textschliessen = "Fenster schliessen";
+?>
