7ae2b6a29863380dd6f877c13c25d93e9f2c6559
me-suzy <me.suzana@gmail.com>
2025-08-13 03:04:43 +0300

    Add Unsorted/ebay/CobrandStatic/CobrandStatic.h

---
+++ Unsorted/ebay/CobrandStatic/CobrandStatic.h
@@ -0,0 +1,10 @@
+/*	$Id: CobrandStatic.h,v 1.3 1999/02/21 02:21:23 josh Exp $	*/
+#if !defined(AFX_COBRANDSTATIC_H__547DA8A5_3883_11D2_981B_00400541AE82__INCLUDED_)
+#define AFX_COBRANDSTATIC_H__547DA8A5_3883_11D2_981B_00400541AE82__INCLUDED_
+
+// COBRANDSTATIC.H - Header file for your Internet Server
+//    CobrandStatic Filter and CobrandStatic Extension
+
+#include "resource.h"
+
+#endif // !defined(AFX_COBRANDSTATIC_H__547DA8A5_3883_11D2_981B_00400541AE82__INCLUDED)
