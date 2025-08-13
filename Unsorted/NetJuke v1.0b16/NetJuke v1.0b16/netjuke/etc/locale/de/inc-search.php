6a00c1e86bd5281f4dbd3dd3471dfb85fb0fa99d
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:10 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-search.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-search.php
@@ -0,0 +1,80 @@
+<?php
+
+##################################################
+
+# /search.php
+
+##################################################
+
+define( "SRCH_NOSTR", "Bitte ändern Sie ihre Suchkriterien." );
+
+define( "SRCH_CHCK_HELP", "Klicken um diesen Titel auszuwählen" );
+define( "SRCH_PLAY_HELP", "Klicken um diesen Titel anzuhören" );
+define( "SRCH_DLOAD_HELP", "Klicken um diesen Titel herunterzuladen" );
+define( "SRCH_INFO_HELP", "Klicken um die Information dieses Titels anzuzeigen." );
+define( "SRCH_AR_HELP", "Klicken um alle Titel dieses Interpreten anzuzeigen." );
+define( "SRCH_AL_HELP", "Klicken um alle Titel dieses Albums anzuzeigen." );
+define( "SRCH_GE_HELP", "Klicken um alle Titel dieses Genres anzuzeigen." );
+
+define( "SRCH_HEADER", "SUCHERGEBNISSE UND OPTIONEN" );
+
+define( "SRCH_ROWCNT_1", "Gefundene Titel" );
+define( "SRCH_ROWCNT_2", "Seite" );
+define( "SRCH_ROWCNT_3", "von" );
+define( "SRCH_ROWCNT_4", "für" );
+
+define( "SRCH_QS_AR", "Interpret" );
+define( "SRCH_QS_TR", "Titel" );
+define( "SRCH_QS_AL", "Album" );
+define( "SRCH_QS_GE", "Genre" );
+define( "SRCH_QS_TI", "Dauer" );
+define( "SRCH_QS_BTN", "Suche" );
+
+define( "SRCH_TB_PLIST_BTN", "Bearbeiten" );
+define( "SRCH_TB_LOGIN", "Anmelden" );
+define( "SRCH_TB_REGISTER", "Konto beantragen" );
+define( "SRCH_TB_SELECT", "Alle auswählen" );
+define( "SRCH_TB_SELECT_HELP", "Klicken um alle Titel dieser Seite auszuwählen" );
+define( "SRCH_TB_RESET", "Alle abwählen" );
+define( "SRCH_TB_RESET_HELP", "Klicken um alle Titel dieser Seite abzuwählen" );
+define( "SRCH_TB_PLAYSEL", "Ausgewählte anhören" );
+define( "SRCH_TB_PLAYSEL_HELP", "Klicken um die ausgewählten Titel anzuhören" );
+define( "SRCH_TB_ADDTO", "Zur Spielliste kopieren" );
+define( "SRCH_TB_ADDTO_HELP", "Klicken um die ausgewählten Titel zur gewählten Spielliste hinzuzufügen" );
+
+define( "SRCH_TB_PREV", "vorherige Seite" );
+define( "SRCH_TB_PREV_HELP", "Klicken um zur vorherigen Seite der Sucherergebnisse zu gelangen" );
+define( "SRCH_TB_NEXT", "nächste Seite" );
+define( "SRCH_TB_NEXT_HELP", "Klicken um zur nächsten Seite der Sucherergebnisse zu gelangen" );
+define( "SRCH_TB_PAGE", "Seite" );
+define( "SRCH_TB_PAGEBOT", "zum Ende" );
+define( "SRCH_TB_PAGEBOT_HELP", "Seitenende anzeigen" );
+define( "SRCH_TB_PAGETOP", "zum Anfang" );
+define( "SRCH_TB_PAGETOP_HELP", "Seitenanfang anzeigen" );
+define( "SRCH_TB_PAGE_BTN", "Los!" );
+
+define( "SRCH_COLS_OP", "Optionen" );
+define( "SRCH_COLS_SQ", "Nr." );
+define( "SRCH_COLS_SQ_HELP", "Klicken um die Seite nach der gespeicherten Reihenfolge zu sortieren" );
+define( "SRCH_COLS_TR", "Titel Name" );
+define( "SRCH_COLS_TR_HELP", "Klicken um die Seite nach dem Titel zu sortieren" );
+define( "SRCH_COLS_TI", "Dauer" );
+define( "SRCH_COLS_TI_HELP", "Klicken um die Seite nach der Dauer zu sortieren" );
+define( "SRCH_COLS_AR", "Interpret" );
+define( "SRCH_COLS_AR_HELP", "Klicken um die Seite nach dem Interpet zu sortieren" );
+define( "SRCH_PLAY_AR_HELP", "Klicken um alle Titel des Interpreten an zu hören" );
+define( "SRCH_FILTER_AR_HELP", "Klicken um alle Alben des Interpreten anzuzeigen." );
+define( "SRCH_COLS_AL", "Album" );
+define( "SRCH_COLS_AL_HELP", "Klicken um die Seite nach dem Album zu sortieren" );
+define( "SRCH_PLAY_AL_HELP", "Klicken um alle Titel des Albums an zu hören" );
+define( "SRCH_FILTER_AL_HELP", "Klicken um alle Interpreten dieses Albums anzuzeigen." );
+define( "SRCH_COLS_TN", "#" );
+define( "SRCH_COLS_TN_HELP", "Klicken um die Seite nach der Titel-Nummer zu sortieren" );
+define( "SRCH_COLS_GE", "Genre" );
+define( "SRCH_COLS_GE_HELP", "Klicken um die Seite nach dem Genre zu sortieren" );
+define( "SRCH_PLAY_GE_HELP", "Klicken um alle Titel des Genres an zu hören" );
+
+
+##################################################
+
+?>
