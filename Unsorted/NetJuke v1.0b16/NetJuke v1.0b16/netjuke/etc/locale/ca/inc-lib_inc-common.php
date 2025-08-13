680fe6862187f5d693c9adb768096e8d6f000e99
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:28 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-lib_inc-common.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-lib_inc-common.php
@@ -0,0 +1,75 @@
+<?php
+
+##################################################
+
+# /lib/inc-common.php
+
+##################################################
+
+define( "COMMON_ERR_DBCONN", "No es pot accedir a la base de dades amb aquesta informació." );
+
+define( "COMMON_ACCESS_DENIED_1", "Accés denegat. Heu de ser administrador per entrar-hi." );
+define( "COMMON_ACCESS_DENIED_2", "Accés denegat. Si us plau, torneu a intentar-ho." );
+
+define( "COMMON_PLISTSELECT_1", "Nova llista" );
+
+define( "COMMON_ALPHABET_AR_HELP", "Feu clic per veure tots els intèrprets que comencen per" );
+define( "COMMON_ALPHABET_AL_HELP", "Feu clic per veure tots els àlbums que comencen per" );
+
+define( "COMMON_CHECK_IMAGE_HELP", "Feu clic per veure la imatge associada" );
+
+define( "COMMON_NOTRACK", "No heu seleccionat cap pista de la llista de sota." );
+
+define( "COMMON_PLIST_1", "Si us plau, seleccioneu una llista existent per editar.\\nO seleccioneu algunes pistes i creeu-ne una de nova." );
+
+define( "COMMON_BATCHEDIT_1", "Esteu segur que voleu esborrar les pistes seleccionades del Netjuke?" );
+
+define( "COMMON_RADIOEDIT_1", "Esteu segur que voleu substituir tota la llista de ràdio existent?" );
+
+define( "COMMON_JUKEBOXEDIT_1", "Esteu segur que voleu substituir tota la llista del Jukebox existent?" );
+
+define( "COMMON_BEDIT_HEADER", "EINES ESPECIALS D'ADMINISTRADORS I EDITORS" );
+define( "COMMON_BEDIT_TB_BATCHEDIT", "Edita en sèrie" );
+define( "COMMON_BEDIT_TB_BATCHEDIT_HELP", "Feu clic aquí per editar les pistes seleccionades en sèrie." );
+define( "COMMON_BEDIT_TB_BATCHDEL", "Esborra en sËrie" );
+define( "COMMON_BEDIT_TB_BATCHDEL_HELP", "Feu clic aquí per esborrar les pistes seleccionades en sèrie" );
+define( "COMMON_BEDIT_TB_RADIOADD", "Afegeix a la llista Ràdio" );
+define( "COMMON_BEDIT_TB_RADIOADD_HELP", "Feu clic aquí per afegir les pistes seleccionades a la llista de l'opció Ràdio." );
+define( "COMMON_BEDIT_TB_RADIOREP", "Reemplaça llista Ràdio" );
+define( "COMMON_BEDIT_TB_RADIOREP_HELP", "Feu clic aquí per crear una nova llista de l'opció Ràdio, o per reemplaçar l'existent, amb les pistes seleccionades." );
+define( "COMMON_BEDIT_TB_RADIOPLVIEW", "View Radio Playlist" );
+define( "COMMON_BEDIT_TB_RADIOPLVIEW_HELP", "Feu clic aquí per veure el fitxer actual de text utilitzat com a llista de reproducció de radio." );
+define( "COMMON_BEDIT_TB_JUKEBOXSTART", "Reprodueix el Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXSTART_HELP", "Feu clic aquí per reproduir la llista del Jukebox del servidor." );
+define( "COMMON_BEDIT_TB_JUKEBOXSTOP", "Para el Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXSTOP_HELP", "Feu clic aquí per parar la reproducciÛ de la llista del Jukebox del servidor." );
+define( "COMMON_BEDIT_TB_JUKEBOXADD", "Afegeix a la llista del Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXADD_HELP", "Feu clic aquí per afegir les cançons seleccionades a la llista actual del Jukebox." );
+define( "COMMON_BEDIT_TB_JUKEBOXREP", "Substitueix la llista del Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXREP_HELP", "Feu clic aquí per crear una nova llista del Jukebox, o substituir l'existent per les cançons seleccionades." );
+define( "COMMON_BEDIT_TB_JUKEBOXPLVIEW", "Consulta la llista del Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXPLVIEW_HELP", "Feu clic aquí per veure el fitxer de text actual utilitzat com a llista del Jukebox." );
+
+define( "COMMON_SUMHEAD_QS_HEADER", "RECERCA RÀPIDA" );
+define( "COMMON_SUMHEAD_QS_OPT_AR", "Cerca per intèrpret" );
+define( "COMMON_SUMHEAD_QS_OPT_TR", "Cerca per títol de cançó" );
+define( "COMMON_SUMHEAD_QS_OPT_AL", "Cerca per títol d'àlbum" );
+define( "COMMON_SUMHEAD_QS_OPT_GE", "Cerca per gènere" );
+define( "COMMON_SUMHEAD_QS_OPT_TI", "Cerca per durada(segons)" );
+define( "COMMON_SUMHEAD_QS_BTN", "Cerca!" );
+
+define( "COMMON_SUMHEAD_SUMM_HEADER", "RESUM DEL CONTINGUT" );
+define( "COMMON_SUMHEAD_SUMM_TR", "Cançons" );
+define( "COMMON_SUMHEAD_SUMM_AR", "Intèrprets" );
+define( "COMMON_SUMHEAD_SUMM_AL", "Àlbums" );
+define( "COMMON_SUMHEAD_SUMM_GE", "Gèneres" );
+
+define( "COMMON_SUMHEAD_ALPHA_AR_HEADER", "INTÈRPR." );
+define( "COMMON_SUMHEAD_ALPHA_AL_HEADER", "ÀLBUMS" );
+
+define( "COMMON_SUMHEAD_ALL_TITLE", "Llista totes les entrades per ordre alfabètic" );
+define( "COMMON_SUMHEAD_ALL_LINK", "Llista" );
+
+##################################################
+
+?>
