d4093e4987928bdac512abbc3e6a2257c35b1e0a
me-suzy <me.suzana@gmail.com>
2025-08-13 03:31:14 +0300

    Add Unsorted/ebay/sql/ebay_credit_info.sql

---
+++ Unsorted/ebay/sql/ebay_credit_info.sql
@@ -0,0 +1,26 @@
+/*	$Id: ebay_credit_info.sql,v 1.2 1999/02/21 02:53:32 josh Exp $	*/
+/*
+ * ebay_users.sql
+ */
+
+	drop table ebay_credit_info;
+
+	create table ebay_credit_info
+	(
+		id							int
+			constraint	credit_info_nn
+			not null,
+		credit_card_on_file	char
+			constraint	credit_info_cc_nn
+			not null,
+		good_credit				char
+			constraint	credit_info_gc_nn
+			not null,
+		constraint		credit_info_pk
+			primary key (id),
+		constraint		credit_info_fk
+			foreign key (id)
+			references	ebay_users(id)
+	);
+
+	commit;
