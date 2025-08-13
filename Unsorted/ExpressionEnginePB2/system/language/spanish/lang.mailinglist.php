01b793e9edf653e33b7949b80d92d8f8637a74eb
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:56 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.mailinglist.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.mailinglist.php
@@ -0,0 +1,112 @@
+<?php
+$L = array(
+
+
+"mailinglist_module_name" =>
+"Lista de correos",
+
+"mailinglist_module_description" =>
+"Gestor básico de listas de correos",
+
+"ml_mailinglist" =>
+"Lista de correos",
+
+"ml_total_in_mailinglist" =>
+"Total de subscriptores a la lista:",
+
+"ml_view_mailinglist" =>
+"Ver lista de correos",
+
+"ml_email_search" =>
+"Búsqueda de email",
+
+"ml_email_search_cont" =>
+"Enviar direcciones de email parciales o completas",
+
+"ml_email_address" =>
+"Email",
+
+"ml_delete" =>
+"Borrar",
+
+"ml_no_results" =>
+"No hay resultados que coincidan con los criterios que has enviado",
+
+"ml_delete_confirm" =>
+"Borra direcciones de email",
+
+"ml_delete_question" =>
+"¿Estás seguro que deseas borrar las direcciones de email seleccionadas?",
+
+"ml_email_deleted" =>
+"Las direcciones han sido eliminadas",
+
+"ml_emails_deleted" =>
+"Las direcciones han sido eliminadas",
+
+"ml_missing_email" =>
+"Debes de enviar una dirección de email",
+
+"ml_invalid_email" =>
+"Dirección no válida",
+
+"ml_email_already_in_list" =>
+"Tu dirección de email ya existe en la lista de correos",
+
+"ml_banned_email" =>
+"No se permiten direcciones de email",
+
+"ml_email_accepted" =>
+"Tu dirección de email se ha añadido a la lista de correo.",
+
+"ml_email_confirmation_sent" =>
+"Ha sido enviado un email de confirmación.",
+
+"ml_click_confirmation_link" =>
+"Haz click en el enlace contenido en el email que has recibido para activar tu cuenta en la lista de correo",
+
+"ml_mailinglist_confirmation" =>
+"Confirmación email",
+
+"ml_thankyou_for_joining" =>
+"¡Gracias por apuntarte en nuestra lista de correo!",
+
+"ml_activation_instructions" =>
+"Click en este enlace para confirmar tu email.",
+
+"ml_expiration_blurb" =>
+"Si no deseas que te añadamos a la lista, ignora este mensaje.",
+
+"ml_account_confirmed" =>
+"Tu dirección de email ha sido confirmada.",
+
+"ml_expired_date" =>
+"El código de autorización que no has enviado está expirado",
+
+"ml_unsubscribe" =>
+"Tu dirección de email se ha eliminado de nuestra lista.",
+
+"ml_unsubscribe_failed" =>
+"No se puede localizar el número de cuenta especificado.",
+
+"ml_add_email_addresses" =>
+"Subscriptores email",
+
+"ml_add_email_addresses_cont" =>
+"Separa cada dirección con un espacio o una nueva línea",
+
+"ml_emails_imported" =>
+"La subscripción se ha realizado correctamente",
+
+"ml_total_emails_imported" =>
+"Total de email subscritos:",
+
+"ml_bad_email_heading" =>
+"Los siguientes emails no se han podido importar:",
+
+"ml_total_duplicate_emails" =>
+"Total de emails existentes:",
+
+''=>''
+);
+?>
