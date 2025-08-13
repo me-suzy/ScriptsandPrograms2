0b5b5f98c2c7797faddb089b1863b87d6fd76ba3
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:17 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.publish.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.publish.php
@@ -0,0 +1,450 @@
+<?php
+$L = array(
+
+"status_changed" =>
+"Status Changed",
+
+"publish" =>
+"Julkaise",
+
+"no_weblog_exits" =>
+"Yritit päästä weblogille jota ei ole olemassa.",
+
+"no_entries_matching_that_criteria" =>
+"Valittu kriteeri ei täsmännyt yhteenkään viestiin",
+
+"no_entries_exist" =>
+"Tässä weblogissa ei ole viestejä",
+
+"select_blog_to_post_in" =>
+"Valitse weblog jolle julkaistaan",
+
+"select_blog_to_edit" =>
+"Valitse tarkasteltava weblog",
+
+"this_entry_will_appear_in" =>
+"Tämä viesti näytetään:",
+
+"unauthorized_for_this_blog" =>
+"Sinulla ei ole oikeutta julkaista viestejä tälle weblogille",
+
+"unauthorized_for_any_blogs" =>
+"Sinulla ei ole oikeutta julkaista weblogien viestejä",
+
+"unauthorized_to_delete_others" =>
+"Sinulla on oikeus poistaa vain ne viestit jotka olet itse julkaissut ",
+
+"unauthorized_to_delete_self" =>
+"Sinulla ei ole oikeutta poistaa weblogisi viestejä",
+
+"weblog" =>
+"Weblog",
+
+"weblogs" =>
+"Weblogit",
+
+"new_entry" =>
+"Uusi viesti",
+
+"button_mode" =>
+"Painike Moodi:",
+
+"guided" =>
+"Ohjattu",
+
+"normal" =>
+"Normaali",
+
+"view_entry" =>
+"Näytä viesti",
+
+"preview_entry" =>
+"Esikatsele viesti",
+
+"edit_entry" =>
+"Muokkaa viestiä",
+
+"edit_weblog_entries" =>
+"Muokkaa weblogin viestejä",
+
+"continue_editing" =>
+"Jatka editointia",
+
+"entry_status" =>
+"Viestin status",
+
+"status" =>
+"Status",
+
+"comments" =>
+"Kommentit",
+
+"open" =>
+"Esitetään",
+
+"close" => 
+"Close",
+
+"closed" =>
+"Ei esitetä",
+
+"options" =>
+"Vaihtoehdot",
+
+"sticky" =>
+"Pakota viesti alkuun (ks.ohje)",
+
+"allow_comments" =>
+"Salli kommentit",
+
+"allow_trackbacks" =>
+"Salli trackbackit",
+
+"ping_sites" =>
+"Pingattavat sivut",
+
+"ping_urls" =>
+"Trackback URL:t  pingaukseen",
+
+"auto_discovery" =>
+"Trackback Automaattinen löytäminen",
+
+"select_entries_to_ping" =>
+"Valitse weblogin viestit joita haluat pingata",
+
+"view_previous_pings" =>
+"Näytä edelliset pingit",
+
+"previiously_pinged_urls" =>
+"Sinua on pingattu seuraavista URL:stä",
+
+"date" =>
+"Päiväys",
+
+"expiration_date" =>
+"Päättymispäivä",
+
+"invalid_date_formatting" =>
+"Antamasi päiväys ei ollut oikein (vuosi-kuukausi-päivä tunti:minuutti)",
+
+"date_outside_of_range" =>
+"Antamasi päiväys oli hyväksytyn ajan ulkopuolella (1902 - 2037).",
+
+"category" =>
+"Kategoria",
+
+"categories" =>
+"Kategoriat",
+
+"no_categories" =>
+"Ei kategorioita",
+
+"title" =>
+"Otsikko",
+
+"url_title" =>
+"URL otsikko",
+
+"author" =>
+"Tekijä",
+
+"newline_format" =>
+"Muotoilu:",
+
+"none" =>
+"Ei mitään",
+
+"link" =>
+"Linkki",
+
+"image" =>
+"Kuva",
+
+"email" =>
+"Email",
+
+"upload" =>
+"Lataa",
+
+"file_upload" =>
+"Tiedoston lataus",
+
+"emoticons" =>
+"Kuva-smileyt",
+
+"choose_a_destination_for_emoticon" =>
+"Valitse kuva-smileyn kohde",
+
+"click_emoticon" =>
+"Klikkaa kuvaa lisätäksesi se viestiisi",
+
+"no_emoticons" =>
+"Kuva-smileytä ei löydetty",
+
+"close_all" =>
+"Sulje kaikki",
+
+"missing_title" =>
+"Viestilläsi tulee olla otsikko.",
+
+"missing_date" =>
+"Viestillä tulee olla päiväys.",
+
+"title_not_unique" =>
+"Saman niminen viesti on jo olemassa. Otsikon tulee olla uniikki.",
+
+"url_title_not_unique" =>
+"Saman niminen URL otsikko on jo olemassa. Otsikon tulee olla uniikki",
+
+"entry_has_been_added" =>
+"Uusi viesti lähetetty",
+
+"entry_has_been_updated" =>
+"Viesti on päivitetty",
+
+"trackback_url_errors" =>
+"Seuraavat trackback pingit eivät toimineet:",
+
+"xmlrpc_ping_errors" =>
+"Seuraavat XML-RPC pingit eivät toimineet:",
+
+"click_to_view_your_entry" =>
+"Klikkaa tästä katsoaksesi viestiä",
+
+"view" =>
+"Näytä",
+
+"edit_this_entry" =>
+"Muokaa tätä viestiä",
+
+"date_range" =>
+"Aikarajaus",
+
+"today" =>
+"Tänään",
+
+"past_week" =>
+"Viimeiset 7 päivää",
+
+"past_month" =>
+"Viimeiset 30 päivää",
+
+"past_six_months" =>
+"Viimeiset 180 päivää",
+
+"past_year" =>
+"Viimeiset 365 päivää",
+
+"any_date" =>
+"Kaikki päivät",
+
+"results_per_page" =>
+"Tuloksia per sivu",
+
+"order" =>
+"Järjestys",
+
+"ascending" =>
+"Vanhin ensin",
+
+"descending" =>
+"Uusin ensin",
+
+"alpha" =>
+"Aakkosjärjestys",
+
+"delete_confirm" =>
+"Poiston vahvistus",
+
+"delete_entry_confirm" =>
+"Oletko varma, että haluat lopullisesti poistaa tämän viestin?",
+
+"delete_entries_confirm" =>
+"Oletko varma, että haluat lopullisesti poistaa nämä viestit?",
+
+"entries_deleted" =>
+"Poista",
+
+"html_buttons_no_cursor" =>
+"Käyttääksesi näitä painikkeita tulee sinun siirtää kursori johonkin kenttään.",
+
+"html_buttons_url_text" =>
+"Lisää hyperlinkki",
+
+"html_buttons_webpage_text" =>
+"Lisää linkin otsikko",
+
+"html_buttons_title_text" =>
+"Vaihtoehto: lisää otsikon ominaisuudet",
+
+"html_buttons_image_text" =>
+"Lisää kuvan osoite",
+
+"html_buttons_email_text" =>
+"Lisää email",
+
+"html_buttons_email_title" =>
+"Lisää linkin otsikko (tai jätä tyhjäksi niin otsikoksi tulee email osoite)",
+
+"html_buttons_enter_text" =>
+"Lisää teksi jonka haluat muotoilla",
+
+"warning" =>
+"Varoitus",
+
+"file_exists" =>
+"Saman niminen kenttä on jo olemassa",
+
+"overwrite_instructions" =>
+"Voit valita uuden nimen ja veisti uudelleen nimetään, tai jos et muuta nimeä tallennetaan uusi vanhan päälle.",
+
+"select_destination_dir" =>
+"Valitse kohdekansio",
+
+"file_type" =>
+"Tiedosto tyyppi",
+
+"non_image" =>
+"Ei kuvatiedosto",
+
+"success" =>
+"Onnistui!",
+
+"you_have_uploaded" =>
+"Olet ladannut seuraavat tiedostot:",
+
+"choose_a_destination" =>
+"Missä haluat tiedostojen näkyvän?",
+
+"no_entry_fields" =>
+"Tällä weblogilla ei ole runko-kenttää.",
+
+"no_comments_or_trackbacks" =>
+"Tälle viestille ei ole kommentteja eikä trackbackejä",
+
+"posted_by" =>
+"Kirjoittaja:",
+
+"located_in" =>
+"Sijainti:",
+
+"comment_date" =>
+"Päiväys:",
+
+"comment_email" =>
+"Email:",
+
+"comment_url" =>
+"URL:",
+
+"comment_action" =>
+"Toimenpide:",
+
+"comment_ip" =>
+"IP:",
+
+"weblog_name" =>
+"Weblog:",
+
+"comment" =>
+"Kommentit",
+
+"missing_comment" =>
+"Sinun tulee syöttää kommentti",
+
+"edit_comment" =>
+"Muokka kommenttia",
+
+"delete_comment" =>
+"Poista kommentti",
+
+"comment_deleted" =>
+"Kommentti poistettu",
+
+"delete_trackback" =>
+"Poista Trackback",
+
+"trackback_deleted" =>
+"Trackback poistettu",
+
+"comment_updated" =>
+"Kommentti päivitetty",
+
+"delete_comment_confirm" =>
+"Oletko varma, että haluat lopullisesti poistaa tämän kommentin?",
+
+"delete_trackback_confirm" =>
+"Oletko varma, että haluat lopullisesti poistaa tämän trackbackin?",
+
+"name" =>
+"Nimi",
+
+"url" =>
+"URL",
+
+"location" =>
+"Sijainti",
+
+"continue" =>
+"Jatka",
+
+"image_options" =>
+"Kuvan ominaisuudet",
+
+"create_popup" =>
+"Muodosta pop-up kuvalle?",
+
+"create_thumb" =>
+"Muodosta kuvasta pikkukuva (thumbnail)",
+
+"constrain_proportions" =>
+"Pakota mittasuhteet",
+
+"thumb_settings" =>
+"Kuvan asetukset",
+
+"choose_a_destination_for_thumb" =>
+"Missä haluat kuvan näkyvän?",
+
+"do_not_place_file" =>
+"Älä sijoita kuva viestiin",
+
+"image_size_not_different" =>
+"Antamasi leveys ja korkeus arvot ovat samat kuin alkuperäisen kuvan.",
+
+"width" =>
+"Leveys",
+
+"height" =>
+"Korkeus",
+
+"pixels" =>
+"Pixeleitä",
+
+"percent" =>
+"Prosentteja",
+
+"resize_image" =>
+"Muuta kuvan kokoa",
+
+"create_thumb_copy" =>
+"Muodosta erillinen kopio",
+
+"resize_original" =>
+"Muuta alkuperäisen kuvan kokoa",
+
+"thumb_instructions" =>
+"Voit vaihtoehtoisesti muttaa ladatun kuvan kokoa, tai muodostaa erillisen pikkukuvan.",
+
+"thumbnail_created" =>
+"Muodosta pikkukuva",
+
+"image_resized" =>
+"Kuvasi koko on muutettu",
+
+"close_window" =>
+"Sulje ikkuna",
+
+''=>''
+);
+?>
