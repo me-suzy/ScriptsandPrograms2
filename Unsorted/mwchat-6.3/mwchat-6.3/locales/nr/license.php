f902a6f9149bcf56ff2364298c19313bb1fe7570
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:35 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/license.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/license.php
@@ -0,0 +1,29 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[LICENSE_Title] = "Tillat MWChat nå!";
+
+$LOCALE[LICENSE_Info] = "Vær så snill og gå inn i Deres MWChat Serial Number under fullføre arrangementet.";
+
+$LOCALE[LICENSE_Key] = "Seriell Number";
+
+$LOCALE[LICENSE_Submit] = "Gjenfinn Licenses";
+
+$LOCALE[LICENSE_Upgrade] = "Klikk her om De De' ang. oppgradering fra en forrige versjon av MWChat.";
+
+$LOCALE[LICENSE_LoginButton] = "Login til MWChat";
+
+$LOCALE[LICENSE_Complete_basic] = "Congratulations! De De' ve tillatt vellykket MWChat med en grunnleggende tillatelse.";
+
+$LOCALE[LICENSE_Complete_pro] = "Congratulations! De De' tillatt ve sucessfully MWChat med en protillatelse.";
+
+?>
