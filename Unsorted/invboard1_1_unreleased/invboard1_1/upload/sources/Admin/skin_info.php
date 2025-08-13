39100a1879dd56c44fca40d9876295cc9127bc4d
me-suzy <me.suzana@gmail.com>
2025-08-13 05:40:04 +0300

    Add Unsorted/invboard1_1_unreleased/invboard1_1/upload/sources/Admin/skin_info.php

---
+++ Unsorted/invboard1_1_unreleased/invboard1_1/upload/sources/Admin/skin_info.php
@@ -0,0 +1,109 @@
+<?php
+
+
+$skin_names = array(
+
+					 'skin_boards'  => array(
+					 						   'Board Index Sections',
+					 						   'Elements for the board index listing (shows you all the forums and is the first page you see when visiting the board) such as
+					 						   forum link HTML, active user overview HTML, board statistics overview HTML, today\'s birthdays overview HTML',
+					 						),
+					 						
+					 'skin_buddy'   => array(
+					 							'Browse Buddy Sections',
+					 							'Elements for the browse buddy feature, including links and search boxes',
+					 						),
+					 'skin_calendar'=> array(
+					 							'Calendar Sections',
+					 							'Elements for the calendar, including calendar view, view events',
+					 						),
+					 'skin_emails'  => array(
+					 							'Member Contact Sections',
+					 							'Elements from various contact methods, such as ICQ, AIM pager, email form and report post to moderator form',
+					 						),
+					 'skin_forum'   => array(
+					 							'Forum Index Sections',
+					 							'Elements for forum view (list all topics in forum). Includes forum log in window, forum rules',
+					 						),
+					 'skin_global'  => array(
+					 							'Global HTML elements',
+					 							'Various HTML elements such as board header, redirect page, error page',
+					 						),
+					 'skin_help'	=> array(	
+					 							'Help Sections',
+					 							'Elements for the help screen, including search boxes and view help files',
+					 						),
+					 'skin_legends' => array(
+					 							'Board Legends Sections',
+					 							'Elements for the "view all emoticons" windows, and the "Find User" sections',
+					 						),
+					 'skin_login'   => array(
+					 							'Log In Sections',
+					 							'Elements for the log in form',
+					 						),
+					 'skin_mlist'   => array(
+					 							'Member List Sections',
+					 							'Elements for the member list views',
+					 						),
+					 'skin_mod'		=> array(
+					 							'Moderator Function Sections',
+					 							'Elements for the moderator tools such as delete topic, edit topic and the moderator CP sections',
+					 						),
+					 'skin_msg'     => array(
+					 							'Messenger Sections',
+					 							'Elements for the messenger, such as view inbox, view message, archive messages, etc',
+					 						),
+					 'skin_online'  => array (
+					 							'Online List Sections',
+					 							'Elements for the "Show all online users" link',
+					 						),
+					 'skin_poll'    => array (
+					 							'View Poll Sections',
+					 							'Elements for the poll view, includes vote form and vote results',
+					 						),
+					 'skin_post'    => array(
+					 							'Post Screen Sections',
+					 							'Elements for the post screens such as new topic, reply to topic, quote post, edit post, some messenger new PM sections, some calender new event sections',
+					 						),
+					 'skin_printpage'=> array(
+					 							'Printable Topic Sections',
+					 							'Elements for displaying the "printable topic" page',
+					 						),
+					 'skin_profile'	=> array(
+					 							'Profile View Sections',
+					 							'Elements for viewing a members profile',
+					 						),
+					 'skin_register' => array(
+					 							'Register Sections',
+					 							'Elements for the register form, validate account form and COPPA forms',
+					 						),
+					 'skin_search'   => array(
+					 							'Search Sections',
+					 							'Elements for the search forum and the results view',
+					 						),
+					 'skin_stats'    => array(
+					 							'Statistics Section',
+					 							'Elements for various functions such as "View Moderating Team", "View todays top 10 posts", "View Active Topics"',
+					 						),
+					 'skin_topic'	 => array(
+					 							'Topic View Section',
+					 							'Elements for the topic view screen, such as post view, inline attachment view, etc.',
+					 						),
+					 
+					 'skin_modcp'	 => array(
+					 							'Mod CP Section',
+					 							'Elements for the moderators control panel sections.',
+					 						),
+					 						
+					 						
+					 'skin_ucp'      => array(
+					 							'User Control Panel Section',
+					 							'Elements for the user control panel, such as the menu (also used in Messenger Views), edit profile, edit signature, edit avatar, etc.',
+					 						)
+);
+
+
+
+
+
+?>
