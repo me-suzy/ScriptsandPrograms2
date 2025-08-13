483023064a74076168b4e6ae93277baf00304bc6
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:06 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.mt_import.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.mt_import.php
@@ -0,0 +1,82 @@
+<?php
+$L = array(
+
+
+"mt_import_utility" =>
+"Utilità di importazione da Movable Type",
+
+"mt_import_welcome" =>
+"Questa utilitè permette di trasferire dati da Movable Type a ExpressionEngine",
+
+"mt_import_disclaimer" =>
+"Se non utilizzi Movable Type questa utilità non è necessaria.",
+
+"file_blurb" =>
+"Per utilizzare questa funzione, occorre innanzitutto caricare il file contenente i dati esportati da MT sul proprio server, quindi indicare la posizione di questo file.",
+
+"file_blurb2" =>
+"Il percorso del file deve essere relativo alla cartella del pannello di controllo. (es. ../mt_file.txt, se posizionato nella directory root)",
+
+"field_blurb" =>
+"Per ricevere tutti i dati importati da MT, il weblog interessato deve avere disponibili campi nominati &#39;summary&#39; (per il campo &#39;excerpt&#39; di MT), &#39;body&#39;, &#39;extended&#39; e &#39;keywords&#39;.",
+
+"import_info" =>
+"Informazioni d&#39;importazione",
+
+"file_info" =>
+"Poizione file di Movable Type",
+
+"weblog_select" =>
+"Scegli il weblog in cui importare gli interventi.",
+
+"admin_select" =>
+"Scegli l&#39;utente di EE a cui vuoi assegnare questi interventi.",
+
+"status_select" =>
+"Stato degli interventi importati.",
+
+"submit_info" =>
+"Inizia l&#39;importazione",
+
+"empty_field_warning" =>
+"Hai lasciato qualche campo vuoto",
+
+"invalid_path" =>
+"File non trovato.",
+
+"invalid_file" =>
+"Il file non contiene dati esportati da MT.",
+
+"import_complete" =>
+"Importazione completata",
+
+"total_weblog_entries" =>
+"Totale interventi inseriti:",
+
+"total_weblog_comments" =>
+"Totale commenti inseriti:",
+
+"total_weblog_trackbacks" =>
+"Totale trackbacks inseriti:",
+
+"total_categories_entered" =>
+"Totale categorie create:",
+
+"more_importing_info" =>
+"Se ci sono altri dati di MT da importare, seleziona il link seguente.  Se l&#39;importazione è conclusa, aggiorna le statistiche.",
+
+"return_to_import" =>
+"Torna alla pagina di importazione MT",
+
+"you_are_done_importing" =>
+"L&#39;importazione è stata completata con successo!",
+
+"recalculate_statistics" =>
+"Aggiornamento statistiche",
+
+"click_to_reset_statistics" =>
+"Clicca qui per aggiornare le statistiche",
+
+''=>''
+);
+?>
