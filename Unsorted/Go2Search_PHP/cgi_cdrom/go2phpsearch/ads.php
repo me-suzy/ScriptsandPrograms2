71594cd0cccb4e12703a8fb089d0bf327735a3e0
me-suzy <me.suzana@gmail.com>
2025-08-13 04:58:42 +0300

    Add Unsorted/Go2Search_PHP/cgi_cdrom/go2phpsearch/ads.php

---
+++ Unsorted/Go2Search_PHP/cgi_cdrom/go2phpsearch/ads.php
@@ -0,0 +1,16 @@
+<?php
+
+# Ads.php
+#####################################################
+
+$target_ad['amazon']="<table border=1 cellspacing=0 width=88 height=31><tr><td width=88 height=31 valign=middle align=center>Amazon Ad</td></tr></table>";
+
+$target_ad['go2cgi']="<table border=1 cellspacing=0 width=88 height=31><tr><td width=88 height=31 valign=middle align=center>Go2cgi Ad</td></tr></table>";
+
+$target_ad['imdb']="<table border=1 cellspacing=0 width=88 height=31><tr><td width=88 height=31 valign=middle align=center>IMDb Ad</td></tr></table>";
+
+$target_ad['art']="<table border=1 cellspacing=0 width=88 height=31><tr><td width=88 height=31 valign=middle align=center>Art Ad</td></tr></table>";
+
+$target_ad['default']="<table border=1 cellspacing=0 width=88 height=31><tr><td width=88 height=31 valign=middle align=center>Default Ad</td></tr></table>";
+
+?>
