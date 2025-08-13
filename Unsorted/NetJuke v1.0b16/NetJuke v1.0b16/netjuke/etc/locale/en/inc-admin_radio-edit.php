ffb1aa04b5a19b311fcc1f4c756a62f6c9fbcb1e
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:31 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_radio-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_radio-edit.php
@@ -0,0 +1,17 @@
+<?php
+
+##################################################
+
+# /admin/radio-edit.php
+
+##################################################
+
+define( "RADIO_LIST_NOPATH", "File Not Found:\\nThe targeted radio playlist cannot be found." );
+
+define( "RADIO_LIST_NOPERM", "Permissions Error:\\nThe targeted radio playlist cannot be written to." );
+
+define( "RADIO_LIST_DONE", "The radio Playist has been edited successfully." );
+
+##################################################
+
+?>
