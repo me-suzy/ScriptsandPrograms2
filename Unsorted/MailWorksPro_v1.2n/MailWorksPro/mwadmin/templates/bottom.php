4780e3c709f6b7908a4252fe635ec6443494141a
me-suzy <me.suzana@gmail.com>
2025-08-13 06:17:51 +0300

    Add Unsorted/MailWorksPro_v1.2n/MailWorksPro/mwadmin/templates/bottom.php

---
+++ Unsorted/MailWorksPro_v1.2n/MailWorksPro/mwadmin/templates/bottom.php
@@ -0,0 +1,13 @@
+	  </td>
+	</tr>
+	<tr>
+	  <td width="209" colspan="2">
+	  </td>
+	  <td width="561" bgcolor="#d4d0c8" height="25" align="center">
+	    <span class="MenuText">&copy; Copyright 2002 <!--CyKuH [WTN]-->MailWorksPro. All rights reserved.</span>
+	  </td>
+	</tr>
+  </table>
+</body>
+</html>
+<?php ob_end_flush(); ?>
