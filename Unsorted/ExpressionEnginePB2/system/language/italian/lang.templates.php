09b4b9d0cc53fc5581122ab8d09f969dbe1d3b4e
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:27 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.templates.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.templates.php
@@ -0,0 +1,330 @@
+<?php
+$L = array(
+
+"php_parse_location" =>
+"Stage at which PHP should be parsed",
+
+"input" =>
+"Input",
+
+"output" =>
+"Output",
+
+"template_loop" =>
+"Hai provocato il loop di questo modello ad causa di un utilizzo improprio di sub-modelli annidati",
+
+"template_intro" =>
+"Qusta pagina si trova alla gestione modelli.",
+
+"create_new_template_group" =>
+"Crea un nuovo set di modelli",
+
+"new_template_group_form" =>
+"Nuovo set modelli",
+
+"edit_template_group_form" =>
+"Modifica gruppo modelli",
+
+"name_of_template_group" =>
+"Nome set modelli",
+
+"template_group_instructions" =>
+"Il nome deve essere una parola singola senza spazi",
+
+"undersores_allowed" =>
+"(underscore e trattini consentiti)",
+
+"new_template_form" =>
+"Form nuovo modello",
+
+"edit_template_form" =>
+"Modifica form modelli",
+
+"name_of_template" =>
+"Nome modello",
+
+"group_name" =>
+"Nome set",
+
+"order" =>
+"Ordinamento",
+
+"reserved_name" =>
+"Il nome prescelto è una parola riservata e non può essere utilizzata.",
+
+"type_of_template" =>
+"Tipo di modello",
+
+"webpage" =>
+"Pagina web",
+
+"css_stylesheet" =>
+"Foglio di stile CSS",
+
+"rss" =>
+"Pagina RSS",
+
+"missing_name" =>
+"Al modello deve essere assegnato un nome",
+
+"template_name_taken" =>
+"Il nome indicato è già utilizzato",
+
+"template_group_del_conf" =>
+"Conferma cancellazione set modelli",
+
+"template_del_conf" =>
+"Conferma cancellazione modello",
+
+"edit_template" =>
+"Modifica modello",
+
+"add_a_template" =>
+"Aggiungi un nuovo modello",
+
+"delete_template_group" =>
+"Cancella set",
+
+"edit_template_group" =>
+"Modifica set",
+
+"group_order" =>
+"Ordine set",
+
+"export_templates" =>
+"Esporta modelli",
+
+"template_name" =>
+"Nome modello:",
+
+"template_management" =>
+"Gestione modelli",
+
+"current_template" =>
+"Modello attuale:",
+
+"preferences" =>
+"Preferenze",
+
+"prefs" =>
+"Pref.ze",
+
+"hits" =>
+"Hits",
+
+"hit_counter" =>
+"Contatore hits",
+
+"template_updated" =>
+"Modello aggiornato",
+
+"template_created" =>
+"Modello creato",
+
+"form_is_empty" =>
+"Hai inviato un form vuoto",
+
+"all_fields_required" =>
+"Devi compilare tutti i campi del form",
+
+"illegal_characters" =>
+"Il nome può contenere solo caratteri alfanumerici, underscore e trattini",
+
+"edit_template_group_order" =>
+"Modifica ordine set",
+
+"template_group_updated" =>
+"Set modelli aggiornato",
+
+"template_group_created" =>
+"Set modelli creato",
+
+"delete_this_group" =>
+"Cancella il seguente set di modelli?",
+
+"all_templates_will_be_nuked" =>
+"TUTTI I MODELLI ATTRIBUITI A QUESTO SET SARANNO CANCELLATI DEFINITIVAMENTE!",
+
+"template_group_deleted" =>
+"Set modelli cancellato",
+
+"edit_group_order" =>
+"Modifica ordine set",
+
+"template_size" =>
+"Dimensione modello",
+
+"small" =>
+"Piccolo",
+
+"medium" =>
+"Medio",
+
+"large" =>
+"Grande",
+
+"view_rendered_template" =>
+"Mostra il rendering del modello",
+
+"save_history" =>
+"Salva revisione",
+
+"view" =>
+"Mostra",
+
+"clear" =>
+"Azzera",
+
+"select_all" =>
+"Seleziona tutti",
+
+"export" =>
+"Esporta",
+
+"export_as" =>
+"Esporta i file come",
+
+"export_will_be_zip" =>
+"Nota: i dati esportati genereranno un archivio ZIP",
+
+"choose_templates" =>
+"Scegli il modello che vuoi esportare come file",
+
+"unable_to_write_files" =>
+"Impossibile scrivere file nella cartella di registro",
+
+"revision_history" =>
+"Mostra cronologia revisioni",
+
+"clear_revision_history" =>
+"Azzera cronologia revisioni",
+
+"history_cleared" =>
+"Il registro revisioni è stato cancellato.",
+
+"close_window" =>
+"Chiudi finestra",
+
+"template_preferences" =>
+"Preferenze modelli",
+
+"delete_template" =>
+"Cancella modello",
+
+"delete_this_template" =>
+"Cancella i seguenti modelli:",
+
+"template_deleted" =>
+"Modello cancellato",
+
+"default_site_page" =>
+"Spcifica la pagina principale di default del sito",
+
+"is_site_default" =>
+"Definisci il modello index di questo set come home page del sito?",
+
+"allow_php" =>
+"Consenti l&#39;inserimento di codice PHP in questo modello?",
+
+"enable_cache" =>
+"Abilita il registro modelli?",
+
+"cache_interval" =>
+"Intervallo di tempo tra gli aggiornamenti del registro",
+
+"in_minutes" =>
+"Tempo specificato in minuti",
+
+"php_security_warning" =>
+"Selezionare &quot;sì&quot; ha implicazioni sulla sicurezza",
+
+"consult_manual" =>
+"Consulta la documentazione",
+
+"preferences_updated" =>
+"Preferenze aggiornate",
+
+"choose_default_data" =>
+"Impostazioni di default dei modelli",
+
+"blank_template" =>
+"Nessuna - crea un modello vuoto",
+
+"template_from_library" =>
+"Utilizza un modello dalla libreria",
+
+"an_existing_template" =>
+"Duplica un modello esistente",
+
+"you_must_submit_a_name" =>
+"Al modello deve essere assegnato un nome",
+
+"member_group" =>
+"Categoria utenti",
+
+"can_view_template" =>
+"Può vedere questa pagina",
+
+"require_logged_in" =>
+"È richiesto il log-in per vedere questa pagina?",
+
+"restrict_to_group" =>
+"Limita l&#39;accesso agli utenti",
+
+"logged_out_select_blurb" =>
+"Se selezioni &quot;sì&quot;, quando un utente non autenticato cerca di accedere a questa pagina, mostra in sostituzione questa:",
+
+"no_access_select_blurb" =>
+"Se selezioni &quot;no&quot; in tutti i casi precedenti:",
+
+"no_access_instructions" =>
+"Quando utenti non autenticati cercano di accedere a questa pagina, mostra questa in sostituzione:",
+
+"template_notes" =>
+"Note al modello",
+
+"template_notes_desc" =>
+"Utilizza questo campo per archiviare note e informazioni su questo modello",
+
+"global_variables" =>
+"Variabili globali",
+
+"global_variables_explanation" =>
+"Puoi definire variabili personalizzate e inserirle ovunque nei tuoi modelli",
+
+"no_global_variables" =>
+"Al momento non ci sono variabili globali",
+
+"global_variable_name" =>
+"Nome variabile",
+
+"create_new_global_variable" =>
+"Definici una nuova variabile globale",
+
+"variable_name" =>
+"Nome variabile",
+
+"global_variable_syntax" =>
+"Sintassi variabile",
+
+"variable_data" =>
+"Contenuto variabile",
+
+"global_var_created" =>
+"Variabile creata",
+
+"global_var_updated" =>
+"Variabile aggiornata",
+
+"delete_global_variable" =>
+"Cancella variabile",
+
+"delete_this_variable" =>
+"Elimino la seguente variabile?",
+
+"variable_deleted" =>
+"Variabile cancellata",
+
+''=>''
+);
+?>
