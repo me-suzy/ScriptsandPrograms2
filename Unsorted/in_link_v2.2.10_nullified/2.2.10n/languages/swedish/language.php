689e8b1546268fad35ba276ef4026519b6842be2
me-suzy <me.suzana@gmail.com>
2025-08-13 05:48:41 +0300

    Add Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/swedish/language.php

---
+++ Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/swedish/language.php
@@ -0,0 +1,306 @@
+<?php
+## In-link User Language File - English
+## Author: Intechnic Corporation
+## Date: 07/24/2001
+##
+## This file is designed for the following version of In-link: 2.0.6
+## You can modify the text in this file to suit your language needs. 
+## Make sure to escape quotation marks with \" . 
+
+#########################################################################################
+#Language variables
+
+$lu_char_set								=   "ISO-8859-1";
+$lu_this_language							=	"Svenska";	
+
+#########################################################################################
+#Navigation Menu
+
+$lu_getting_rated							=	"Bli Omdömd";
+$lu_go_to_page								=	"Gå till sida";
+$lu_search_results							=	"Sökresultat";
+$lu_nav_addlink								=	"Lägg till länk";
+$lu_nav_editorspick							=	"Administratörens val";
+$lu_nav_gettingrated					          	=	"Bli Omdömd";
+$lu_nav_home								=	"Hem";
+$lu_nav_login								=	"Logga in";
+$lu_nav_logout								=	"Logga ut";
+$lu_nav_maillist							=	"Prenumerera";
+$lu_nav_modlink								=	"Modifiera länk";
+$lu_nav_new									=	"Nya länkar";
+$lu_nav_pop									=	"Populäraste...";
+$lu_nav_profile								=	"Profil";
+$lu_nav_register							=	"Registrera";
+$lu_nav_search								=	"Sök";
+$lu_nav_search_advanced					        	=	"Avancerad sökning";
+$lu_nav_suggestcat							=	"Föreslå kategori";
+$lu_nav_top									=	"Topp-Röstade";
+$lu_navbar_seperator					        	=	" >";
+ 
+
+#########################################################################################
+#Page Titles
+
+$lu_title_add_link							=	"Lägg till länk";
+$lu_title_add_review					        	=	"Lägg till recension";
+$lu_title_confirm							=	"Bekräftelse";
+$lu_title_edit_link							=	"Modifiera länk";
+$lu_title_editors_picks						=	"Administratörens val";
+$lu_title_error								=	"Fel";
+$lu_title_gettingrated						=	"Bli Omdömd";
+$lu_title_login								=	"Logga in";
+$lu_title_modify							=	"Modifiera Dina Länkar";
+$lu_title_most_popular						=	"Populäraste länkar";
+$lu_title_msg								=	"System Meddelande";
+$lu_title_new_links							=	"Nya länkar";
+$lu_title_profile							=	"Medlemsprofil";
+$lu_title_rate								=	"Rösta på denna länk";
+$lu_title_registration						=	"Registration";
+$lu_title_reviews							=	"Recensioner";
+$lu_title_search							=	"Sök";
+$lu_title_search_advanced					=	"Avancerad sökning";
+$lu_title_subscribe							=	"Prenumerera på ett nyhetsbrev";
+$lu_title_suggest							=	"Föreslå kategori";
+$lu_title_top_rated							=	"Topp-Röstade Länkar";
+
+
+#########################################################################################
+#Link and Category Markers (Can replace them with graphics)
+
+$lu_cat_new									=	"ny";
+$lu_cat_pick								=	"välj";
+$lu_link_new								=	"ny";
+$lu_link_pick								=	"välj";
+$lu_link_pop								=	"pop";
+$lu_link_top								=	"topp";
+
+
+#########################################################################################
+#System Messages
+
+$lu_no_categories							=	"Inga kategorier";
+$lu_no_links								=	"Inga länkar";
+
+
+#########################################################################################
+#Link/Category Details 
+
+$lu_added									=	"Tillagd";
+$lu_cat										=	"Kategori";
+$lu_date_created							=	"Datum skapad";
+$lu_desc									=	"Beskrivning";
+$lu_edit									=	"Modifiera";
+$lu_editor_pick								=	"Administratörens val";
+$lu_from									=	"Från";
+$lu_general									=	"Allmänt";
+$lu_image									=	"Bild";
+$lu_hits									=	"Träffar";
+$lu_link									=	"Länk";
+$lu_name									=	"Namn";
+$lu_no										=	"Nej";
+$lu_rateit									=	"Rösta";
+$lu_rating									=	"Röstning";
+$lu_reviews									=	"Recensioner";
+$lu_to										=	"Till";
+$lu_votes									=	"Röster";
+$lu_url										=	"URL";
+$lu_yes										=	"Ja";
+
+
+#########################################################################################
+#Search 
+
+$lu_boolean_type_search						=	"Boolean-Typ";
+$lu_return_number_results					=	"Visa antal resultat";
+
+
+#########################################################################################
+#Errors
+
+$lu_already_voted							=	"Vår statistik visar att du redan röstat. Du kan inte rösta fler än en gång per månad!";
+$lu_already_voted							=	"Du får inte rösta på denna resurs!";
+$lu_email_used								=	"E-postadressen används redan";
+$lu_error_404								=	"Kan inte öppna filen.";
+$lu_error_addlink_not_allowed				=	"Att lägga till länkar är inte tillåtet";
+$lu_error_addreview_not_allowed				=	"Att lägga till recensioner är inte tillåtet";
+$lu_error_addto_cat_not_allowed				=	"Denna kategori är inte tillräckligt specifik! Välj en annan kategori.";
+$lu_error_cannot_remove_cat					=	"Kan inte ta bort föregående kategori!";
+$lu_error_db								=	"Databas fel!";
+$lu_error_file_not_found					=	"Filen är tillgänglig:";
+$lu_error_pass_match						=	"Lösenorden du fyllde i är ogiltiga.";
+$lu_error_pass_wrong						=	"Lösenordet du fyllde i är ogiltigt.";
+$lu_error_review_not_filled					=	"Recensionen får inte vara tom";
+$lu_error_user_not_allowed					=	"Du får inte registrera dig!";	
+$lu_error_wrong_user						=	"Länken tillhör en annan användare!";
+$lu_invalid_entry							=	"Ogiltig inträde";
+$lu_login_incorrect							=	"Inkorrekt inloggning";
+$lu_pass_not_match							=	"Lösenorden matchar inte";
+$lu_username_used							=	"Användarnamnet används";
+
+#########################################################################################
+#Confirmations
+
+$lu_confirm_registration					=	"Din registration har skickats iväg.";
+$lu_confirm_suggest_site					=	"Tack för att du föreslog denna sida till en vän.";
+$lu_confirm_profile							=	"Din profil har uppdaterats.";
+$lu_confirm_linkupdate						=	"Din länk har uppdaterats.";
+$lu_confirm_addlink							=	"Din länk har skickats.";
+$lu_confirm_suggest							=	"Tack för att du föreslog en kategori.";
+$lu_confirm_addreview						=	"Denna recension har skickats iväg.";
+$lu_confirm_subscribe						=	"Du har lagts till på följande nyhetsbrev.";
+$lu_confirm_unsubscribe						=	"Du har tagits bort från följande nyhetsbrev.";
+
+#########################################################################################
+#Buttons
+
+$lu_button_addcat							=	"Lägg till kategori";
+$lu_button_addlink							=	"Lägg till länk";
+$lu_button_addreview						=	"Lägg till recension";
+$lu_button_back								=	"Tillbaka";
+$lu_button_cancel							=	"Avbryt";
+$lu_button_editlink							=	"Ändra länk";
+$lu_button_go								=	"Gå";
+$lu_button_login							=	"Logga in";
+$lu_button_ok								=	"Ok";
+$lu_button_rate								=	"Rösta";
+$lu_button_register							=	"Registrera";
+$lu_button_reset							=	"Återställ";
+$lu_button_search							=	"Sök";
+$lu_button_search_cats						=	"Sök kategorier";
+$lu_button_search_links						=	"Sök länkar";
+$lu_button_send								=	"Skicka";
+$lu_button_subscribe						=	"Prenumerare";
+$lu_button_unsubscribe						=	"Avbeställ prenumeration";
+$lu_button_update							=	"Uppdatera";
+$lu_mailing_list							=	"Nyhetsbrev";
+$lu_not_mailing_list						=	"Inte i nyhetsbrevet";
+
+
+#########################################################################################
+#Statistics
+
+$lu_cats									=	"Kategorier:";
+$lu_links									=	"Länkar:";
+$lu_total_cats								=	"Totala kategorier i databasen:  ";
+$lu_total_hits								=	"Träffar: ";
+$lu_total_links								=	"Totala länkar i databasen: ";
+
+
+#########################################################################################
+#Addlink template
+
+$lu_add_cat									=	"Lägg till denna kategori";
+$lu_additional_cats							=	"Ytterligare kategorier:";
+$lu_remove									=	"Ta bort";
+$lu_select									=	"Välj";
+$lu_submitting_to							=	"Du försöker prenumerera på:";
+
+
+#########################################################################################
+#Register template
+
+$lu_first_name								=	"Förnamn";
+$lu_last_name								=	"Efternamn";
+$lu_re_password								=	"Lösenord igen";
+$lu_user_name								=	"Användarnamn";
+
+
+#########################################################################################
+#Miscellaneos Pages
+
+$lu_advanced_search							=	"Avancerad Sökning";
+$lu_atleast_3								=	"(minimum 3)";
+$lu_email									=	"E-post";
+$lu_hint_suggest							=	"Tipsa en vän om denna sida";
+$lu_keyword									=	"Nyckelord";
+$lu_login									=	"Logga in";
+$lu_not_logged_in							=	"Inte inloggad";
+$lu_password								=	"Lösenord";
+$lu_register								=	"Registrera";
+$lu_review									=	"Recension";
+$lu_search									=	"Sök";
+$lu_search_for								=	"Sök efter";
+$lu_show									=	"Visa";
+$lu_sort_by									=	"Sortera efter";
+$lu_status									=	"Status";
+$lu_suggest									=	"Förslå";
+$lu_user									=	"Användare";
+$lu_user_menu								=	"Användarmeny";
+$lu_welcome									=	"Välkommen";
+
+
+#########################################################################################
+#Dropdowns
+
+$lu_drop_and								=	"Och";
+$lu_drop_as_many_as_found					=	"alla";
+$lu_drop_ascending							=	"Stiga upp";
+$lu_drop_date								=	"Datum";
+$lu_drop_desc								=	"Beskrivning";
+$lu_drop_descending							=	"Stiga ned";
+$lu_drop_editors_pick						=	"Redaktörens val";
+$lu_drop_hits								=	"Träffar";
+$lu_drop_image								=	"Bild";
+$lu_drop_name								=	"Namn";
+$lu_drop_numlinks							=	"Antal länkar";
+$lu_drop_numsubs							=	"Antal sublänkar";
+$lu_drop_or									=	"Eller";
+$lu_drop_perm								=	"Tillåtelser";
+$lu_drop_rating								=	"Röstning";
+$lu_drop_url								=	"URL";
+$lu_drop_user								=	"Användare";
+$lu_drop_vis								=	"Synlighet";
+$lu_drop_votes								=	"Röster";
+
+
+#########################################################################################
+#Custom Fields
+
+$lu_categories_custom_fields1			    =	"Vanligt Fält Namn #1";
+$lu_categories_custom_fields2			    =	"Vanligt Fält Namn #2";
+$lu_categories_custom_fields3			    =	"Vanligt Fält Namn #3";
+$lu_categories_custom_fields4			    =	"Vanligt Fält Namn #4";
+$lu_categories_custom_fields5			    =	"Vanligt Fält Namn #5";
+$lu_categories_custom_fields6			    =	"Vanligt Fält Namn #6";
+$lu_links_custom_fields1					=	"Vanligt Fält Namn #1";
+$lu_links_custom_fields2					=	"Vanligt Fält Namn #2";
+$lu_links_custom_fields3					=	"Vanligt Fält Namn #3";
+$lu_links_custom_fields4					=	"Vanligt Fält Namn #4";
+$lu_links_custom_fields5					=	"Vanligt Fält Namn #5";
+$lu_links_custom_fields6					=	"Vanligt Fält Namn #6";
+$lu_users_custom_fields1					=	"Vanligt Fält Namn #1";
+$lu_users_custom_fields2					=	"Vanligt Fält Namn #2";
+$lu_users_custom_fields3					=	"Vanligt Fält Namn #3";
+$lu_users_custom_fields4					=	"Vanligt Fält Namn #4";
+$lu_users_custom_fields5					=	"Vanligt Fält Namn #5";
+$lu_users_custom_fields6					=	"Vanligt Fält Namn #6";
+
+
+#########################################################################################
+#Ratings
+
+$lu_excellent								=	"Utmärkt!";
+$lu_fair									=	"Hygglig";
+$lu_good									=	"Bra";
+$lu_poor									=	"Dålig";
+$lu_verygood								=	"Mycket bra";
+
+
+#########################################################################################
+#Text segments
+
+$lu_txt_gettingrated	=	"Du kan placera följande HTML-kod på din egen hemsida så också dina besökare kan rösta på denna resurs:";
+
+#########################################################################################
+#New for version 2.0.7
+$lu_confirm_approval						=	"Den kommer bli aktiv vid godkännande";
+
+#New for version 2.0.9
+
+$lu_report_dead						=	"Rapportera en död länk";
+$lu_confirm_report_dead						=	"Länken har rapporterats. Tack!";
+$lu_error_login_to_add						=	"Var snäll och logga in för att lägga till en länk!";
+
+#New for version 2.0.10
+$lu_error_addto_cat_added					=	"Denna kategori har redan lagts till";
+?>
