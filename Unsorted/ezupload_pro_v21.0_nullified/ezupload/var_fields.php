daedbb55e2147494462e888df38df63a045b627e
me-suzy <me.suzana@gmail.com>
2025-08-13 04:32:59 +0300

    Add Unsorted/ezupload_pro_v21.0_nullified/ezupload/var_fields.php

---
+++ Unsorted/ezupload_pro_v21.0_nullified/ezupload/var_fields.php
@@ -0,0 +1,84 @@
+<?php
+/////////////////////////////////////////////////////////////
+// Program Name         : EzUpload Pro                       
+// Program Version      : 2.0                                
+// Program Author       : ScriptsCenter.com                  
+// Supplied by          : Stive [WTN]                        
+// Nullified and tested : CyKuH [WTN]                        
+// Distribution         : via WebForum and Forums File Dumps 
+//                    WTN Team `2002
+/////////////////////////////////////////////////////////////
+$VARS[0]['type'] = "text";
+$VARS[0]['name'] = "Email Address";
+$VARS[0]['description'] = "";
+$VARS[0]['minchars'] = 0;
+$VARS[0]['maxchars'] = 50;
+$VARS[0]['required'] = 1;
+$VARS[0]['id'] = 3;
+$VARS[0]['order'] = 2;
+$VARS[0]['default'] = "";
+$VARS[1]['type'] = "text";
+$VARS[1]['name'] = "Full Name";
+$VARS[1]['description'] = "";
+$VARS[1]['minchars'] = 0;
+$VARS[1]['maxchars'] = 50;
+$VARS[1]['required'] = 1;
+$VARS[1]['id'] = 1;
+$VARS[1]['order'] = 1;
+$VARS[1]['default'] = "";
+$VARS[3]['type'] = "file";
+$VARS[3]['name'] = "File #1";
+$VARS[3]['description'] = "";
+$VARS[3]['minchars'] = "";
+$VARS[3]['maxchars'] = "";
+$VARS[3]['required'] = 1;
+$VARS[3]['id'] = 8;
+$VARS[3]['order'] = 3;
+$VARS[3]['default'] = "";
+$VARS[4]['type'] = "file";
+$VARS[4]['name'] = "File #2";
+$VARS[4]['description'] = "";
+$VARS[4]['minchars'] = "";
+$VARS[4]['maxchars'] = "";
+$VARS[4]['required'] = 0;
+$VARS[4]['default'] = "";
+$VARS[4]['id'] = 4;
+$VARS[4]['order'] = 4;
+$VARS[5]['type'] = "file";
+$VARS[5]['name'] = "File #3";
+$VARS[5]['description'] = "";
+$VARS[5]['minchars'] = "";
+$VARS[5]['maxchars'] = "";
+$VARS[5]['required'] = 0;
+$VARS[5]['default'] = "";
+$VARS[5]['id'] = 5;
+$VARS[5]['order'] = 5;
+$VARS[6]['type'] = "file";
+$VARS[6]['name'] = "File #4";
+$VARS[6]['description'] = "";
+$VARS[6]['minchars'] = "";
+$VARS[6]['maxchars'] = "";
+$VARS[6]['required'] = 0;
+$VARS[6]['default'] = "";
+$VARS[6]['id'] = 6;
+$VARS[6]['order'] = 6;
+$VARS[7]['type'] = "file";
+$VARS[7]['name'] = "File #5";
+$VARS[7]['description'] = "";
+$VARS[7]['minchars'] = "";
+$VARS[7]['maxchars'] = "";
+$VARS[7]['required'] = 0;
+$VARS[7]['default'] = "";
+$VARS[7]['id'] = 7;
+$VARS[7]['order'] = 7;
+$VARS[8]['type'] = "textarea";
+$VARS[8]['name'] = "Description";
+$VARS[8]['description'] = "";
+$VARS[8]['minchars'] = 0;
+$VARS[8]['maxchars'] = 1000;
+$VARS[8]['required'] = 0;
+$VARS[8]['id'] = 2;
+$VARS[8]['order'] = 8;
+$VARS[8]['default'] = "";
+
+?>
