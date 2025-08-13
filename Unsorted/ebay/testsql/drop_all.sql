767c199cb71f9b4ecf0e13ff32f98da6660777d2
me-suzy <me.suzana@gmail.com>
2025-08-13 03:37:07 +0300

    Add Unsorted/ebay/testsql/drop_all.sql

---
+++ Unsorted/ebay/testsql/drop_all.sql
@@ -0,0 +1,21 @@
+/*	$Id: drop_all.sql,v 1.2 1999/02/21 02:55:43 josh Exp $	*/
+/*
+** drop_all.sql
+** 
+** Drops all tables in the right order to 
+** avoid integrity constraints
+**
+*/
+ drop table ebay_accounts;
+ drop table ebay_account_balances;
+ drop table ebay_feedback;
+
+ drop table ebay_feedback_detail;
+ drop table ebay_bids;
+ drop table ebay_items;
+
+ drop table ebay_item_counts;
+
+ drop table ebay_user_info;
+ drop table ebay_users;
+
