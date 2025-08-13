57a9b337c6a083b35579501ac8733a39a6768c2c
me-suzy <me.suzana@gmail.com>
2025-08-13 05:09:27 +0300

    Add Unsorted/hitweb-3.0/forum/index.php

---
+++ Unsorted/hitweb-3.0/forum/index.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+<!-- Information sur HITWEB -->
+    <meta name="hitweb" content="version 3.0">
+
+<!-- Information pour les ROBOTS -->
+    <meta name="revisit-after" content="30 days">
+
+<!-- Information sur les PAGES HTML -->
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <meta name="Content-language" content="fr">
+
+<!-- Information sur l'AUTEUR -->
+    <meta name="Author-Corporate" content="Brian FRAVAL">
+    <meta name="copyright" content="&copy; 2001 - Brian FRAVAL">
+    <meta name="Author" lang="fr" content="Brian FRAVAL">
+    <meta name="Reply-to" content="webmaster@hitweb.org">
+
+<!-- Information sur le SITE -->
+    <meta name="base" content="http://www.hitweb.org/">
+    <meta name="Identifier-URL" content="http://www.hitweb.org/">
+    <meta name="Date-Creation-yyyymmdd" content="20000802">
+    <meta name="Date-Revision-yyyymmdd" content="20010620">
+
+<!-- Mots clés pour les ANGLAIS -->
+    <meta name="title" lang="en" content="The best of the world wide web">
+    <meta name="Description" lang="en" content="">
+    <meta name="keywords" lang="en" content="best, site, web, internet, hitweb, brian, fraval, decaen, duhaut, gpl, free">
+    <meta name="Category" content="Internet">
+
+<!-- Mots clés pour les FRANCAIS -->
+    <meta name="title" lang="fr" content="Le classement des meilleurs sites web">
+    <meta name="Description" lang="fr" content="Classement des meilleurs sites internet par thèmes. Ce classement est dynamique par rapport au nombre de 
+point de chaques sites référencés dans HITWEB">
+    <meta name="keywords" lang="fr" content="annuaire, moteur de recherche, meilleur, site, web, internet, hitweb, brian, fraval, référencement, decaen, 
+duhaut, classement, gpl, libre">
+    <meta name="Category" content="Internet">
+
+<!-- Feuille de style -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+  <title>HITWEB : Le classement des meilleurs sites 
+web.................................................................................</title>
+  </head>
+<body>
+
+<h1>Installation d'un forum sous license libre</h1>
+
+<a href="http://w-agora.araxe.fr">http://w-agora.araxe.fr</a><br>
+Si vous désirez poser des questions sur l'installation de cette application,
+contacter directement l'auteur, <a href="mailto:mdruilhe@w-agora.com">Marc Druilhe</a>
+
+<hr>
+
+<a href="http://www.phorum.org">http://www.phorum.org</a><br>
+<a href="http://phpinfo.net/?p=articles&rub=phorum3211">Tutorial par J-Pierre DEZELUS - phpinfo.net</a>
+
+<body>
+</html>
