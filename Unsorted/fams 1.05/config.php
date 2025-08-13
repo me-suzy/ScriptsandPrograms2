fa80a0e9d13796c64de2abb2e108cda06553cf41
me-suzy <me.suzana@gmail.com>
2025-08-13 04:33:37 +0300

    Add Unsorted/fams 1.05/config.php

---
+++ Unsorted/fams 1.05/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/* ------------------------------------------------------------ */
+/*								*/
+/*	File Area Management System (FAMS)			*/
+/*								*/
+/*	Copyright (c) 2001 by Bastian 'Buddy' Grimm		*/
+/*	Autor: Bastian Grimm					*/
+/*	Publisher: [ BG-Studios ]				*/
+/*	eMail: bastian@bg-studios.de				*/
+/*	WebSite: http://www.bg-studios.de			*/
+/*	Date: 02.05.01		Version: Config 1.08		*/
+/*	Geändert am: 28.07.01					*/
+/*								*/
+/* ------------------------------------------------------------	*/
+
+
