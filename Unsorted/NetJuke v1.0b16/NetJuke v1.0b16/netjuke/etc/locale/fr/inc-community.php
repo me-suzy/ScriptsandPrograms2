f31fc7fcac636b902c2d89a7fa892d90d38b6171
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:19 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-community.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-community.php
@@ -0,0 +1,28 @@
+<?php
+
+##################################################
+
+# /community.php
+
+##################################################
+
+define( "CMNT_SHRD_HEADER", "NOUVELLES LISTES PARTAGÉES" );
+define( "CMNT_SHRD_LIST_BTN", "Liste Complète" );
+define( "CMNT_SHRD_PLAY_HELP", "Cliquez pour débuter la lecture de cette liste musicale." );
+define( "CMNT_SHRD_VIEW_HELP", "Cliquez pour accéder aux détails de cette liste musicale." );
+define( "CMNT_SHRD_BY", "Par" );
+
+define( "CMNT_DL_HEADER", "PISTES LES PLUS POPULAIRES" );
+define( "CMNT_DL_TOP10_BTN", "Ecouter Le Top 10" );
+define( "CMNT_DL_PLAY_HELP", "Cliquez pour débuter la lecture de cette piste." );
+define( "CMNT_DL_DLOAD_HELP", "Cliquez pour télécharger cette piste." );
+define( "CMNT_DL_INFO_HELP", "Cliquez pour accéder aux détails de cette piste." );
+define( "CMNT_DL_GENRE_HELP", "Cliquez pour voir toutes les piste du même Genre." );
+define( "CMNT_DL_ARTIST", "AR" );
+define( "CMNT_DL_ARTIST_HELP", "Cliquez pour voir toutes les piste par le(s) même(s) Artiste(s)" );
+define( "CMNT_DL_ALBUM", "AL" );
+define( "CMNT_DL_ALBUM_HELP", "Cliquez pour voir toutes les piste sur cet Album." );
+
+##################################################
+
+?>
