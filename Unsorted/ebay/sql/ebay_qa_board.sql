8166668b533f02790d551fcd0e7f5a8a49b8ca68
me-suzy <me.suzana@gmail.com>
2025-08-13 03:32:31 +0300

    Add Unsorted/ebay/sql/ebay_qa_board.sql

---
+++ Unsorted/ebay/sql/ebay_qa_board.sql
@@ -0,0 +1,27 @@
+/*	$Id: ebay_qa_board.sql,v 1.2 1999/02/21 02:54:00 josh Exp $	*/
+/*
+ * ebay_qa_board.sql
+ *
+ * Q&A Bulletin Board
+*/
+
+create table ebay_qa_board
+(	id				int
+		constraint qa_board_id_nn
+		not null,
+	when			date
+		constraint qa_board_date_nn
+		not null,
+	entry_len	int
+		constraint qa_board_entry_len_nn
+		not null,
+	entry			long raw
+		constraint qa_board_entry_nn
+		not null,
+	constraint		qa_board_fk
+		foreign key (id)
+		references	ebay_users(id)
+)
+tablespace bbd01
+storage (initial 1M next 1M);
+
