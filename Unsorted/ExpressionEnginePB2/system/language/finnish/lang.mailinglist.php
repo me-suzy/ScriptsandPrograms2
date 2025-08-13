5cbf983bf46a8719605333e01c520f68c13cf6f5
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:04 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.mailinglist.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.mailinglist.php
@@ -0,0 +1,112 @@
+<?php
+$L = array(
+
+
+"mailinglist_module_name" =>
+"Postituslista",
+
+"mailinglist_module_description" =>
+"Perus postituslistan hallinta",
+
+"ml_mailinglist" =>
+"Postituslista",
+
+"ml_total_in_mailinglist" =>
+"Email osoitteita kaikkiaan:",
+
+"ml_view_mailinglist" =>
+"Näytä poistituslista",
+
+"ml_email_search" =>
+"Osoiteiden etsintä",
+
+"ml_email_search_cont" =>
+"Syötä osa tai koko email osoite",
+
+"ml_email_address" =>
+"Email",
+
+"ml_delete" =>
+"Poista",
+
+"ml_no_results" =>
+"Antamasi kriteeri ei tuottanut tulosta ",
+
+"ml_delete_confirm" =>
+"Poista email osoitteita",
+
+"ml_delete_question" =>
+"Oletko varma, että haluat poista valitsemasi email osoitteet?",
+
+"ml_email_deleted" =>
+"Osoite on poistettu",
+
+"ml_emails_deleted" =>
+"Osoitteet on poistettu",
+
+"ml_missing_email" =>
+"Sinun tulee syöttää email osoite",
+
+"ml_invalid_email" =>
+"Epäkelpo email osoite",
+
+"ml_email_already_in_list" =>
+"Osoitteesi on jo postituslistalla",
+
+"ml_banned_email" =>
+"Email osoite ei ole sallittu",
+
+"ml_email_accepted" =>
+"Email osoitteesi on lisätty postituslistallemme",
+
+"ml_email_confirmation_sent" =>
+"Sinulle on lähetetty vahvistusviesti",
+
+"ml_click_confirmation_link" =>
+"Aktivoidaksesi postituslistan tilisi tulee sinun klikata linkkiä, joka on vahvistus viestissä. ",
+
+"ml_mailinglist_confirmation" =>
+"Email vahvistus",
+
+"ml_thankyou_for_joining" =>
+"Kiitos, kun liityit postituslistallemme!",
+
+"ml_activation_instructions" =>
+"Seuraa linkkiä vahvistaaksesi email osoitteesi",
+
+"ml_expiration_blurb" =>
+"Jos et halua liittyä postituslistallemme, unohda tämä viesti.",
+
+"ml_account_confirmed" =>
+"Email osoitteesi on vahvistettu",
+
+"ml_expired_date" =>
+"Saamasi vahvistuskoodi on vanhentunut",
+
+"ml_unsubscribe" =>
+"Email osoitteesi on poistettu postituslistaltamme.",
+
+"ml_unsubscribe_failed" =>
+"Määriteltyä tilin numeroa ei löytynyt",
+
+"ml_add_email_addresses" =>
+"Määritä email osoitteet",
+
+"ml_add_email_addresses_cont" =>
+"Erota kukin email osoite välilyönnillä tai kirjoita ne eri riveille",
+
+"ml_emails_imported" =>
+"Email osoitteiden määrittely onnistui",
+
+"ml_total_emails_imported" =>
+"Määriteltyjä email osoitteita kaikkiaan:",
+
+"ml_bad_email_heading" =>
+"Seuraavia email osoitteita ei voitu tuoda:",
+
+"ml_total_duplicate_emails" =>
+"Email osoitteita kaikkiaan:",
+
+''=>''
+);
+?>
