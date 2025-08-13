7f25307c0af9ac5a7b941f46760a123ffd06e976
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:26 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/error.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/error.php
@@ -0,0 +1,52 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[ERROR_License] = "Det er en feil med Deres tillatelse. Behag tillatelse MWChat nå.";
+
+$LOCALE[ERROR_AlreadyIn] = "Navnet som De valgte er allerede i bruk.";
+
+$LOCALE[ERROR_Title] = "Feil";
+
+$LOCALE[ERROR_Logout] = "De vært logged ut.";
+
+$LOCALE[ERROR_Expired] = "Deres login ekspirerer, behager login igjen.";
+
+$LOCALE[ERROR_RoomLong] = "Dette romnavn som De velger er til lang.";
+
+$LOCALE[ERROR_Live] = "Systemet er for tiden utilgjengelig.";
+
+$LOCALE[ERROR_Profanity] = "Informasjonen som De spesifisert inneholder profanity.";
+
+$LOCALE[ERROR_NoData]   = "Behag fylle i alle felt.";
+
+$LOCALE[ERROR_BadData]  = "Behag spesifisere gyldige data.";
+
+$LOCALE[ERROR_Serial_Invalid] = "Deres serienummer er ugyldig, behager ang. registrerer Deres produkt.";
+
+$LOCALE[ERROR_Serial_None] = "Finner ikke et serienummer schriftlich belegt, behager ang. registrerer Deres produkt.";
+
+$LOCALE[ERROR_Critical] = "Kritisk feil!";
+
+$LOCALE[ERROR_Javascript] = "Dette programet krever en Javascript dyktig browser.";
+
+$LOCALE[ERROR_Browser] = "Dette programet fungerer ikke riktig med Deres browser.";
+
+$LOCALE[ERROR_Restricted] = "Navnet som De valgte innskrenker.";
+
+$LOCALE[ERROR_BadPass]  = "Vær så snill og gå inn i et gyldig passord.";
+
+$LOCALE[ERROR_IP] = "IP adresserer Deres bruk forandrer seg. De vært logged ut for sikkerhetsgrunner.";
+
+$LOCALE[ERROR_PHPV] = "MWChat krever en minimum PHP versjon av";
+
+?>
