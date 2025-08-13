0ac552a8217b691f7ca6003b21705d6667c50e4b
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:26 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.publish_ad.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.publish_ad.php
@@ -0,0 +1,697 @@
+<?php
+
+$L = array(
+
+
+//----------------------------
+// Weblog Administration
+//----------------------------
+
+"search_excerpt" =>
+"Feld f&uuml;r Suche-Auszug",
+
+"weblog_prefs" =>
+"Weblog-Einstellungen",
+
+"weblog_settings" =>
+"Posting-Einstellungen",
+
+"comment_prefs" =>
+"Kommentar-Einstellungen",
+
+"comment_moderate" =>
+"Kommentare moderieren?",
+
+"comment_moderate_exp" =>
+"Wenn &quot;Ja&quot;, wird ein Kommentar erst angezeigt, wenn ein Moderator ihn freigegeben hat.",
+
+"comment_system_enabled" =>
+"Kommentare in diesem Weblog zulassen?",
+
+"edit_weblog_prefs" =>
+"Weblog-Einstellungen bearbeiten",
+
+"edit_group_prefs" =>
+"Gruppen-Einstellungen bearbeiten",
+
+"no_weblogs_exist" =>
+"Zur Zeit gibt es keine Weblogs.",
+
+"create_new_weblog" =>
+"Neues Weblog erstellen",
+
+"weblog_base_setup" =>
+"Allgemeine Weblog-Einstellungen",
+
+"default_settings" =>
+"Administrative Einstellungen",
+
+"short_weblog_name" =>
+"Kurzer Name",
+
+"blog_url" =>
+"Weblog-URL",
+
+"blog_lang" =>
+"XML-Sprache",
+
+"blog_encoding" =>
+"XML-Zeichensatz",
+
+"blog_description" =>
+"Weblog-Beschreibung",
+
+"illegal_characters" =>
+"Der gew&auml;hlte Name darf nur aus Buchstaben, Ziffern, Unterstreichungen und Trennungsstrichen bestehen.",
+
+"comment_require_membership" =>
+"Mitgliedschaft erforderlich zum Kommentieren?",
+
+"weblog_require_membership" =>
+"Mitgliedschaft erforderlich zum Verfassen von Eintr&auml;gen?",
+
+"comment_require_email" =>
+"Email-Adresse zum Kommentieren erforderlich?",
+
+"weblog_require_email" =>
+"Email-Adresse zum Verfassen von Eintr&auml;gen erforderlich?",
+
+"weblog_max_chars" =>
+"Erlaubte L&auml;nge von Eintr&auml;gen",
+
+"comment_max_chars" =>
+"Erlaubte L&auml;nge von Kommentaren",
+
+"comment_timelock" =>
+"Kommentar-Intervall",
+
+"comment_timelock_desc" =>
+"Zahl der Sekunden, die ein Nutzer warten mu&szlig;, bevor er einen weiteren Kommentar absetzen darf. Frei lassen, wenn keine Grenze gesetzt werden soll.",
+
+"comment_text_formatting" =>
+"Kommentar-Textformat",
+
+"weblog_text_formatting" =>
+"Standard-Textformat in Weblog-Eintr&auml;gen",
+
+"comment_html_formatting" =>
+"Kommentar-HTML-Format",
+
+"weblog_html_formatting" =>
+"Standard-HTML-Format in Weblog-Eintr&auml;gen",
+
+"convert_to_entities" =>
+"HTML in Entities umwandeln",
+
+"allow_safe_html" =>
+"Nur sicheren HTML-Code zulassen",
+
+"allow_all_html" =>
+"ALLE HTML-Tags zulassen",
+
+"allow_all_html_not_recommended" =>
+"Alle HTML-Tags zulassen (nicht empfohlen)",
+
+"comment_notify" =>
+"Admin von Kommentaren und Trackbacks benachrichtigen?",
+
+"comment_notify_note" =>
+"Mehrere Email-Adressen durch Kommata trennen",
+
+"comment_notify_emails" =>
+"Email-Adresse(n) f&uuml;r den Empfang von Benachrichtigungen",
+
+"comment_allow_img_urls" =>
+"Grafik-URLs in Kommentaren zulassen?",
+
+"weblog_allow_img_urls" =>
+"Grafik-URLs in Weblog-Eintr&auml;gen zulassen?",
+
+"auto_link_urls" =>
+"URLs und Email-Aressen automatisch in Links umwandeln?",
+
+"single_word_no_spaces" =>
+"ein Wort, keine Leerzeichen",
+
+"full_weblog_name" =>
+"Voller Name des Weblogs",
+
+"edit_weblog" =>
+"Weblog bearbeiten",
+
+"weblog_name" =>
+"Weblog-Name",
+
+"new_weblog" =>
+"Neues Weblog",
+
+"weblog_created" =>
+"Weblog erstellt:",
+
+"weblog_updated" =>
+"Weblog aktualisiert:",
+
+"taken_weblog_name" =>
+"Dieser Weblogname ist schon vergeben.",
+
+"no_weblog_name" =>
+"Ihr Weblog braucht einen &quot;kurzen&quot; Namen.",
+
+"no_weblog_title" =>
+"Ihr Weblog braucht einen &quot;vollen&quot; Namen.",
+
+"invalid_short_name" =>
+"Ihr Weblogname darf nur Buchstaben und Ziffern, aber keine Leerzeichen enthalten.",
+
+"delete_weblog" =>
+"Weblog l&ouml;schen",
+
+"weblog_deleted" =>
+"Weblog gel&ouml;scht:",
+
+"delete_weblog_confirmation" =>
+"Soll dieses Weblog wirklich gel&ouml;scht werden?",
+
+"be_careful" =>
+"VORSICHT!",
+
+"action_can_not_be_undone" =>
+"DIESE AKTION KANN NICHT R&Uuml;CKG&Auml;NGIG GEMACHT WERDEN!",
+
+"assign_group_to_weblog" =>
+"Hinweis: Um die neue Gruppe nutzen zu k&ouml;nnen, m&uuml;ssen Sie sie einem Weblog zuweisen.",
+
+"click_to_assign_group" =>
+"Zur Zuweisung hier klicken",
+
+"default" =>
+"Standard",
+
+"category" =>
+"Kategorie",
+
+"default_status" =>
+"Standard-Status",
+
+"default_category" =>
+"Standard-Kategorie",
+
+"deft_comments" =>
+"&quot;Kommentare zulassen&quot; beim Schreiben von Eintr&auml;gen automatisch ankreuzen?",
+
+"deft_trackbacks" =>
+"&quot;Trackbacks zulassen&quot; beim Schreiben von Eintr&auml;gen automatisch ankreuzen?",
+
+"enable_trackbacks" =>
+"Code zum automatischen Entdecken von Trackbacks zu Ihren Seiten hinzuf&uuml;gen?",
+
+"trackback_max_hits" =>
+"H&ouml;chstzahl der erlaubten Trackback-Pings pro Stunde",
+
+"trackback_field" =>
+"Welches Feld sollte f&uuml;r den Trackback-Ausschnitt verwendet werden?",
+
+"no_field_group_selected" =>
+"Keine Feldergruppe f&uuml;r dieses Weblog verf&uuml;gbar",
+
+"missing_weblog_data_for_pings" =>
+"Um Pings oder Trackbacks zu verschicken, braucht Ihr Weblog einen Titel und einen URL. Bitte aktualisieren Sie Ihre Einstellungen.",
+
+"open" =>
+"Offen",
+
+"closed" =>
+"Geschlossen",
+
+"none" =>
+"Keine",
+
+"define_html_buttons" =>
+"Definitionen der HTML-Formatier-Buttons",
+
+"htmlbutton_delete_instructions" =>
+"Um einen Eintrag zu l&ouml;schen, schicken Sie das Formular mit einem leeren Namensfeld ab.",
+
+"tag_name" =>
+"Tag-Name",
+
+"tag_open" =>
+"&Ouml;ffnungs-Tag",
+
+"tag_close" =>
+"Schluss-Tag",
+
+"accesskey" =>
+"Shortcut",
+
+"tag_order" =>
+"Reihenfolge",
+
+"row" =>
+"Zeile",
+
+"server_name" =>
+"Server-Name",
+
+"server_url" =>
+"Server-URL/Pfad",
+
+"port" =>
+"Port",
+
+"protocol" =>
+"Protokoll",
+
+"is_default" =>
+"Standard",
+
+"server_order" =>
+"Reihenfolde",
+
+"define_ping_servers" =>
+"Diese Formulare erlauben Ihnen, eine Liste von Servern anzulegen, die bei einem neuen Eintrag angepingt werden sollen.",
+
+"pingserver_delete_instructions" =>
+"Um einen Eintrag zu l&ouml;schen, schicken Sie dieses Formular mit einem leeren Namensfeld ab.",
+
+"assign_weblogs" =>
+"Welche Weblogs sollen dieser Gruppe zugeordnet werden?",
+
+//----------------------------
+// Category Administration
+//----------------------------
+
+"category_group" =>
+"Kategoriengruppe",
+
+"category_groups" =>
+"Kategoriengruppen",
+
+"none" =>
+"Keine",
+
+"no_category_group_message" =>
+"Zur Zeit gibt es keine Kategorien.",
+
+"no_category_message" =>
+"Zur Zeit sind dieser Gruppe keine Kategorien zugeordnet.",
+
+"create_new_category_group" =>
+"Neue Kategoriengruppe erstellen",
+
+"edit_category_group" =>
+"Kategoriengruppe bearbeiten",
+
+"name_of_category_group" =>
+"Name der Gruppe",
+
+"taken_category_group_name" =>
+"Dieser Name ist schon vergeben.",
+
+"add_edit_categories" =>
+"Kategorien erstellen/bearbeiten",
+
+"edit_group_name" =>
+"Gruppe bearbeiten",
+
+"delete_group" =>
+"Gruppe l&ouml;schen",
+
+"category_group_created" =>
+"Kategoriengruppe erstellt:",
+
+"category_group_updated" =>
+"Gruppe aktualisiert:",
+
+"delete_cat_group_confirmation" =>
+"Soll diese Kategoriengruppe wirklich gel&ouml;scht werden?",
+
+"category_group_deleted" =>
+"Kategoriengruppe gel&ouml;scht:",
+
+"create_new_category" =>
+"Neue Kategorie erstellen",
+
+"add_new_category" =>
+"Neue Kategorie hinzuf&uuml;gen",
+
+"edit_category" =>
+"Kategorie bearbeiten",
+
+"delete_category" =>
+"Kategorie l&ouml;schen",
+
+"category_name" =>
+"Name der Kategorie",
+
+"category_image" =>
+"Grafik-URL der Kategorie",
+
+"category_img_blurb" =>
+"Dies ist ein optionales Feld, um einer Kategorie eine Grafik zuzuweisen.",
+
+"category_parent" =>
+"Mutter-Kategorie",
+
+
+//----------------------------
+// Custom field Administration
+//----------------------------
+
+"field_group" =>
+"Gruppe f&uuml;r nutzerdefinierte Felder",
+
+"field_groups" =>
+"Feldergruppen",
+
+"field_group_name" =>
+"Name der Feldergruppen",
+
+"custom_fields" =>
+"Nutzerdefinierte Felder",
+
+"no_field_group_message" =>
+"Zur Zeit gibt es keine nutzerdefinierte Felder f&uuml;r Weblogs.",
+
+"create_new_field_group" =>
+"Neue Feldergruppe erstellen",
+
+"new_field_group" =>
+"Neue Feldergruppe",
+
+"add_edit_fields" =>
+"Nutzerdefinierte Felder erstellen/bearbeiten",
+
+"edit_field_group_name" =>
+"Feldergruppe bearbeiten",
+
+"delete_field_group" =>
+"Feldergruppe l&ouml;schen",
+
+"create_new_field" =>
+"Neues Feld erstellen",
+
+"edit_field" =>
+"Feld bearbeiten",
+
+"no_field_groups" =>
+"Es gibt keine nutzerdefinierten Felder in dieser Gruppe.",
+
+"delete_field" =>
+"Feld l&ouml;schen",
+
+"field_deleted" =>
+"Nutzerdefiniertes Feld gel&ouml;lscht:",
+
+"edit_field_order" =>
+"Reihenfolge der Felder &auml;ndern",
+
+"create_new_custom_field" =>
+"Nutzerdefiniertes Feld erstellen",
+
+"field_label" =>
+"Feld-Label",
+
+"field_name" =>
+"Feld-Name",
+
+"field_name_cont" =>
+"Ein Wort, keine Leerzeichen. Unterstreichungen und Trennungsstriche sind erlaubt.",
+
+"field_type" =>
+"Feld-Typ",
+
+"field_max_length" =>
+"Maximall&auml;nge",
+
+"field_max_length_cont" =>
+"wenn Sie diesen Feldtyp w&auml;lhen",
+
+"textarea_rows" =>
+"Textfeldzeilen",
+
+"textarea_rows_cont" =>
+"Wenn Sie ein Textfeld w&auml;hlen",
+
+"field_list_items" =>
+"Optionen ausw&auml;hlen",
+
+"field_list_items_cont" =>
+"Wenn Sie das Drop-Down-Menu w&auml;hlen",
+
+"field_list_instructions" =>
+"Jede Option in eine eigene Zeile schreiben",
+
+"field_formatting" =>
+"Feld-Formatierung",
+
+"field_formatting_cont" =>
+"Wenn Sie ein Textfeld w&auml;hlen",
+
+"field_order" =>
+"Reihenfolge der Felddarstellung",
+
+"is_field_searchable" =>
+"Ist das Feld durchsuchbar?",
+
+"is_field_required" =>
+"Is dieses Feld ein Pflichtfeld?",
+
+"text_input" =>
+"Text-Input",
+
+"textarea" =>
+"Textfeld",
+
+"select_list" =>
+"Drop-down-Liste",
+
+"auto_br" =>
+"Auto &lt;br /&gt;",
+
+"xhtml" =>
+"xhtml",
+
+"no_field_name" =>
+"Sie m&uuml;ssen einen Feldnamen eingeben.",
+
+"no_field_label" =>
+"Sie m&uuml;ssen ein Feldlabel eingeben.",
+
+"invalid_characters" =>
+"Der angegebene Name enth&auml;lt ung&uuml;ltige Zeichen.",
+
+"custom_field_empty" =>
+"Das folgende Feld ist erforderlich:",
+
+"duplicate_field_name" =>
+"Der gew&auml;hlte Feldname ist schon vergeben.",
+
+"taken_field_group_name" =>
+"Der gew&auml;hlte Name ist schon vergeben.",
+
+"field_group_created" =>
+"Feldergruppe erstellt:",
+
+"field_group_updated" =>
+"Feldergruppe aktualisiert:",
+
+"field_group_deleted" =>
+"Feldergruppe gel&ouml;scht:",
+
+"field_group" =>
+"Feldergruppe",
+
+"delete_field_group_confirmation" =>
+"Soll diese Feldergruppe wirklich gel&ouml;scht werden?",
+
+"delete_field_confirmation" =>
+"Soll dieses Feld wirklich gel&ouml;scht werden?",
+
+"weblog_entries_will_be_deleted" =>
+"Alle Eintr&auml;ge in den obenstehenden Feldern werden dauerhaft gel&ouml;scht.",
+
+
+//----------------------------
+// Status Administration
+//----------------------------
+
+"status_group" =>
+"Statusgruppe",
+
+"status_groups" =>
+"Statusgruppen",
+
+"no_status_group_message" =>
+"Zur Zeit gibt es keinen nutzerdefinierten Status.",
+
+"create_new_status_group" =>
+"Neue Statusgruppe erstellen",
+
+"edit_status_group" =>
+"Statusgruppe bearbeiten",
+
+"name_of_status_group" =>
+"Name der Statusgruppe",
+
+"taken_status_group_name" =>
+"Dieser Name ist bereits vergeben.",
+
+"invalid_status_name" =>
+"Statusnamen d&uuml;rfen nur aus Buchstaben, Ziffern, Unterstreichungen und Trennungsstrichen bestehen.",
+
+"duplicate_status_name" =>
+"Einen Status mit diesem Namen gibt es bereits.",
+
+"status_group_created" =>
+"Statusgruppe erstellt:",
+
+"new_status" =>
+"Neuer Status",
+
+"status_group_updated" =>
+"Statusgruppe aktualisiert:",
+
+"add_edit_statuses" =>
+"Status hinzuf&uuml;gen/bearbeiten",
+
+"edit_status_group_name" =>
+"Statusgruppe bearbeiten",
+
+"delete_status_group" =>
+"Statusgruppe l&ouml;schen",
+
+"delete_status_group_confirmation" =>
+"Soll diese Statusgruppe wirklich gel&ouml;scht werden?",
+
+"status_group_deleted" =>
+"Statusgruppe gel&ouml;scht:",
+
+"create_new_status" =>
+"Neuen Status erstellen",
+
+"status_name" =>
+"Status-Name",
+
+"status_order" =>
+"Status-Nummer",
+
+"change_status_order" =>
+"Status-Nummer &auml;ndern",
+
+"highlight" =>
+"Highlight-Farbe (optional)",
+
+"statuses" =>
+"Status",
+
+"edit_status" =>
+"Status bearbeiten",
+
+"delete_status" =>
+"Status l&ouml;schen",
+
+"delete_status_confirmation" =>
+"Soll dieser Status wirklich gel&ouml;scht werden?",
+
+"edit_file_upload_preferences" =>
+"Upload-Einstellungen bearbeiten",
+
+"new_file_upload_preferences" =>
+"Neue Upload-Einstellungen",
+
+"file_upload_preferences" =>
+"Upload-Einstellungen",
+
+"no_upload_prefs" =>
+"Zur Zeit gibt es keine Upload-Einstellungen",
+
+"create_new_upload_pref" =>
+"Neues Zielverzeichnis f&uuml;r Uploads erstellen",
+
+"upload_pref_name" =>
+"Beschreibung des Zielverzeichnisses",
+
+"new_file_upload_preferences" =>
+"Neues Zielverzeichnis",
+
+"server_path" =>
+"Serverpfad zum Zielverzeichnis",
+
+"url_to_upload_dir" =>
+"URL des Zielverzeichnisses",
+
+"allowed_types" =>
+"Zugelassene Dateitypen",
+
+"max_size" =>
+"Maximale Dateigr&ouml;&szlig;e (in Bytes)",
+
+"max_height" =>
+"Maximale Bildh&ouml;he (in Pixels)",
+
+"max_width" =>
+"Maximale Bildbreite",
+
+"properties" =>
+"Grafikeigenschaften",
+
+"pre_format" =>
+"Pre-Formatting",
+
+"post_format" =>
+"Post-Formatting",
+
+"no_upload_dir_name" =>
+"Sie m&uuml;ssen einen Namen f&uuml;r Ihr Zielverzeichnis angeben.",
+
+"no_upload_dir_path" =>
+"Sie m&uuml;ssen den Pfad zu Ihrem Zielverzeichnis angeben.",
+
+"no_upload_dir_url" =>
+"Sie M&uuml;ssen den URL zu Ihrem Zielverzeichnis angeben.",
+
+"duplicate_dir_name" =>
+"Dieser Name ist bereits vergeben.",
+
+"delete_upload_preference" =>
+"Upload-Einstellung &auml;ndern",
+
+"delete_upload_pref_confirmation" =>
+"Soll diese Einstellung wirklich gel&ouml;scht werden?",
+
+"upload_pref_deleted" =>
+"Einstellung gel&ouml;scht:",
+
+"current_upload_prefs" =>
+"Aktuelle Einstellungen",
+
+"restrict_to_group" =>
+"Uploads auf ausgew&auml;hlte Mitgliedsgruppen beschr&auml;nken",
+
+"restrict_notes_1" =>
+"Diese Radiobuttons lassen Sie bestimmen, wer Dateien und Bilder hochladen und in ein Weblog aufnehmen darf.",
+
+"restrict_notes_2" =>
+"Super-Admins k&ouml;nnen immer Dateien hochladen.",
+
+"restrict_notes_3" =>
+"Hinweis: Zur Zeit ist Dateienupload nur durch das Control-Panel m&ouml;glich.",
+
+"member_group" =>
+"Mitgliedsgruppe",
+
+"can_upload_files" =>
+"Kann Dateien hochladen",
+
+"images_only" =>
+"Nur Grafiken",
+
+"all_filetypes" =>
+"Alle Dateitypen",
+
+
+// END
+''=>''
+);
+?>
