750c6db6c777ad53754935e60e51215a8bd0f153
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:08 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.members.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.members.php
@@ -0,0 +1,615 @@
+<?php
+$L = array(
+
+"can_moderate_comments" =>
+"Can Moderate Comments",
+
+"total_members" =>
+"Jäseniä kaikkiaan:",
+
+"view_members" =>
+"Katsele jäseniä",
+
+"validate_selected" =>
+"Aktivoi valitut jäsenet",
+
+"delete_selected" =>
+"Poista valitut jäsenet",
+
+"send_email_notification" =>
+"Lähetä sähköposti ilmoitus kullekin aktivoidulle jäsenelle",
+
+"your_account_validated" =>
+"Tilisi on aktivoitu",
+
+"your_account_ready" =>
+"Jäsenyys tilisi on aktivoitu ja valmiina käyttöön",
+
+"1" =>
+"Kiitos!",
+
+"no_members_to_validate" =>
+"Aktivointia vaativia jäseniä ei ole",
+
+"members_are_validated" =>
+"Jäsen/jäsenet on aktivoitu",
+
+"members_are_deleted" =>
+"Jäsen/jäsenet on poistettu",
+
+"ascending" =>
+"Päiväys - vanhin ensin",
+
+"descending" =>
+"Päiväys - uusin ensin",
+
+"alpha" =>
+"Aakkosjärjestys a-ö",
+
+"alpha_desc" =>
+"Aakkosjärjestys ö-a",
+
+"username_asc" =>
+"Käyttäjätunnus a-ö",
+
+"username_desc" =>
+"Käyttäjätynnus ö-a",
+
+"screen_name_asc" =>
+"Näyttönimi a-ö",
+
+"screen_name_desc" =>
+"Näyttönimi ö-a",
+
+"email_asc" =>
+"Email a-ö",
+
+"email_desc" =>
+"Email ö-a",
+
+"sort_order" =>
+"Lajittelu järjestys",
+
+"join_date" =>
+"Liittymispäivä",
+
+"last_visit" =>
+"Viimeinen vierailu",
+
+"member_group" =>
+"Jäsenten ryhmä",
+
+"group_id" =>
+"Ryhmän ID",
+
+"no_members_matching_that_criteria" =>
+"Tällä kriteerillä ei löytynyt jäseniä",
+
+"mbrs" =>
+"Jäsenet",
+
+"edit_profile" =>
+"Muokkaa profiilia",
+
+"register_member" =>
+"Rekisteröi uusi jäsen",
+
+"edit_group" =>
+"Muokkaa ryhmää",
+
+"group_title" =>
+"Ryhmän tunnus",
+
+"security_lock" =>
+"Turvaluokka",
+
+"group_lock" =>
+"Jäsenten ryhmän turvaluokka",
+
+"enable_lock" =>
+"Salli ryhmän turvaluokka",
+
+"locked" =>
+"Lukittu",
+
+"lock_description" =>
+"Kun ryhmä on lukittu, vain pääkäyttäjä voi liittää jäseniä tähän ryhmään. Jos sallit muiden muokata jäsenten tilejä, on suositeltavaa, että lukitset kaikki muut ryhmät paitsi ne joihin haluat muiden lisäävän jäseniä.",
+
+"unlocked" =>
+"Ei lukittu",
+
+"create_new_member_group" =>
+"Muodosta uusi jäsenten ryhmä",
+
+"member_has_cp_access" =>
+"Voi kirjautua ohjauspaneeliin",
+
+"edit_member_group" =>
+"Muokkaa jäsenten ryhmää",
+
+"create_member_group" =>
+"Muodosta jäsenten ryhmä",
+
+"delete_member_group" =>
+"Poista jäsenten ryhmä",
+
+"member_group_deleted" =>
+"Jäsenten ryhmä poistettu",
+
+"delete_member_group_confirm" =>
+"Oletko varma, että halua poistaa tämän jäsenten ryhmän?",
+
+"delete_member" =>
+"Poista jäsen",
+
+"delete_member_confirm" =>
+"Oletko varma, että haluat poistaa tämän jäsenen?",
+
+"delete_members_confirm" =>
+"Oletko varma, että haluat poistaa nämä jäsenet?",
+
+"can_not_delete_superadmin" =>
+"Et voi poistaa pääjäsentä ",
+
+"member_deleted" =>
+"Jäsen poistettu",
+
+"members_deleted" =>
+"Jäsenet poistettu",
+
+"user_banning" =>
+"Käyttäjien pannaanjulistaminen (blokkaus)",
+
+"ban_preferences_updated" =>
+"Pannaanjulistamisen ominaisuudet päivitetty",
+
+"ip_address_banning" =>
+"Blokatut IP osoitteet",
+
+"ip_banning_instructions" =>
+"Sijoita kukin IP osoite omalle riville",
+
+"ip_banning_instructions_cont" =>
+"Käytä jokeria tietyn alueen IP osoitteille. Esim:  123.345.*",
+
+"email_address_banning" =>
+"Blokatut email osoitteet",
+
+"email_banning_instructions" =>
+"Sijoita kukin email osoite omalle riville",
+
+"email_banning_instructions_cont" =>
+"Käytä jokeria tietyn domainin osoitteille. Esim:  *@domain.com",
+
+"ban_options" =>
+"Kun blokattu IP osoite yrittää päästä tälle sivulle",
+
+"restrict_to_viewing" =>
+"Salli heidän vain katsella",
+
+"show_this_message" =>
+"Näytä tämä viesti",
+
+"send_to_site" =>
+"Siirrä heidät tälle sivulle",
+
+"username_banning" =>
+"Estetyt käyttäjätunnukset",
+
+"username_banning_instructions" =>
+"Sijoita kukin käyttäjätunnus omalle riville",
+
+"screen_name_banning" =>
+"Estetyt näyttönimet",
+
+"screen_name_banning_instructions" =>
+"Sijoita kukin näyttönimi omalle riville",
+
+"only_superadmins_can_admin_groups" =>
+"Vain pääkäyttäjä voi päästä jäsen ryhmät sivulle",
+
+"create_group_based_on_old" =>
+"Tee uusi ryhmä joka perustuu jo olemassa olevaan",
+
+"group_name" =>
+"Jäsenten ryhmän nimi",
+
+"missing_group_title" =>
+"Jäsenten ryhmällä tulee olla nimi",
+
+"site_access" =>
+"Oikeudet sivuille",
+
+"can_view_online_system" =>
+"Voi nähdä sivun, kun se on online-tilassa",
+
+"can_view_offline_system" =>
+"Voi nähdä sivun, kun se on offline-tilassa",
+
+"mbr_profile_privs" =>
+"Jäsenten profiilien oikeudet",
+
+"can_view_profiles" =>
+"Voi katsella julkisia profiileja",
+
+"commenting_privs" =>
+"Kommentin lähetyksen oikeudet",
+
+"can_post_comments" =>
+"Voi lähettää kommentteja",
+
+"cp_weblog_post_privs" =>
+"Weblog määrittelyt",
+
+"cp_module_access_privs" =>
+"Moduulehin pääsyn oikeudet",
+
+"can_access_mod" =>
+"Voi päästä moduuleihin:",
+
+"global_cp_access" =>
+"Ohjauspaneeliin pääsy ",
+
+"can_access_cp" =>
+"Voi päästä ohjauspaneeliin?",
+
+"cp_section_access" =>
+"Ohjauspaneelin sisäiset rajoitukset",
+
+"can_access_publish" =>
+"Voi päästä JULKAISE sivulle",
+
+"can_access_edit" =>
+"Voi päästä MUOKKAA sivulle",
+
+"can_access_design" =>
+"Voi päästä SIVUPOHJAT sivulle",
+
+"can_access_comm" =>
+"Voi päästä POSTITOIMISTO sivulle",
+
+"can_access_modules" =>
+"Voi päästä MODULIT sivulle",
+
+"can_access_admin" =>
+"Voi päästä JÄRJESTELMÄN HALLINTA sivulle",
+
+"can_ban_users" =>
+"Voi blokata jäseniä",
+
+"cp_admin_privs" =>
+"Ohjauspaneelin hallinan oikeudet",
+
+"cp_comment_privs" =>
+"Kommenttien hallinnan oikeudet",
+
+"can_view_other_comments" =>
+"Voi katsella toisten lähettämiä kommentteja ",
+
+"can_edit_own_comments" =>
+"Voi muokata kommetteja heidän omista viesteistään",
+
+"can_delete_own_comments" =>
+"Voi poistaa kommetteja heidän omista viesteistään",
+
+"can_edit_all_comments" =>
+"Voi muokata kommetteja kaikista weblogin viesteistä",
+
+"can_delete_all_comments" =>
+"Voi poistaa kommentin kaikista weblogin viesteistä",
+
+"can_admin_weblogs" =>
+"Voi hallita weblogeja ja niiden ominaisuuksia",
+
+"can_admin_members" =>
+"Voi hallita jäsenten tilejä",
+
+"can_delete_members" =>
+"Voi poistaa jäseniä",
+
+"can_admin_mbr_groups" =>
+"Voi vaihtaa jäsenen ryhmää (vain ei lukitut ryhmät)",
+
+"can_admin_mbr_templates" =>
+"Voi hallita jäsenten profiilin sivupohjaa",
+
+"can_admin_utilities" =>
+"Voi hallita systeemin ominaisuuksia",
+
+"can_admin_templates" =>
+"Voi hallita sivupohjien ryhmiä ja sivupohjia",
+
+"can_admin_preferences" =>
+"Voi hallita yleisiäominaisuuksia",
+
+"can_admin_modules" =>
+"Voi asentaa/poistaa moduuleja",
+
+"cp_email_privs" =>
+"Ohjauspaneelin email oikeudet",
+
+"can_send_email" =>
+"Voi lähettää postia ohjauspaneelin kautta",
+
+"can_email_members" =>
+"Voi lähettää postia yksittäiselle jäsenelle",
+
+"can_email_member_groups" =>
+"Voi lähettää postia jäsenten ryhmälle",
+
+"can_email_mailinglist" =>
+"Voi lähettää emailia postituslistalle",
+
+"can_send_cached_email" =>
+"Voi katsella/lähettää taltioituja viestejä",
+
+"warning" =>
+"VAROITUS:",
+
+"super_admin_edit_note" =>
+"Huom: Voit vain editoida pääkäyttäjien ryhmän nimeä",
+
+"be_careful_assigning_groups" =>
+"Ole ERITYISEN varovainen määritellessäsi ryhmän ominaisuuksia, varsinkin korostettuja.",
+
+"member_group_updated" =>
+"Jäsenten ryhmä päivitetty:",
+
+"member_group_created" =>
+"Jäsenten ryhmä muodostettu:",
+
+"cp_weblog_privs" =>
+"Weblogien viestien lähetys- ja muokkausoikeudet",
+
+"can_view_other_entries" =>
+"Voi katsella muiden viestejä",
+
+"can_post_in" =>
+"Voi lähettä ja muokata viestejä:",
+
+"can_edit_other_entries" =>
+"Voi muokata muiden viestejä",
+
+"can_assign_post_authors" =>
+"Voi muuttaa kirjoittajan nimeä viesteissä",
+
+"can_delete_self_entries" =>
+"Voi poistaa omia viestejä",
+
+"can_delete_all_entries" =>
+"Voi poistaa kaikkien viestejä",
+
+"cp_template_access_privs" =>
+"Sivupohjien muokkauksen oikeudet",
+
+"can_access_tg" =>
+"Voi muokata sivupohjia tässä ryhmässä:",
+
+"can_email_from_profile" =>
+"Voi lähettää postia jäsenille profiilin email konsolista",
+
+"for_profile_page" =>
+"Esitetään jäsenenä jäsenten profiilit sivulla",
+
+"custom_member_fields" =>
+"Muokatut profiili kentät",
+
+"current_fields" =>
+"Jäsenen profiili kenttä",
+
+"edit_member_field" =>
+"Muokkaa kenttää",
+
+"create_member_field" =>
+"Muodosta kenttä",
+
+"field_updated" =>
+"Kenttä päivitetty",
+
+"create_new_profile_field" =>
+"Muodosta uusi profiili kenttä",
+
+"is_field_public" =>
+"Näytetäänkö tämä kenttä jukisessa profiilissa? ",
+
+"is_field_public_cont" =>
+"Jos valitsit EI, se näytetään vain järjestelmänvalvojille",
+
+"is_field_reg" =>
+"Näytetäänkö kenttä rekisteröintisivulla?",
+
+"field_width" =>
+"Kentän leveys",
+
+"field_width_cont" =>
+"Voi olla pikseleitä tai prosentteja",
+
+"fieldname" =>
+"Kentän nimi",
+
+"fieldname_cont" =>
+"Yksi sana, ei välilyöntejä. Viiva ja alaviiva on sallittu",
+
+"field_format" =>
+"Tekstin muotoilu",
+
+"max_length" =>
+"Max pituus",
+
+"max_length_cont" =>
+"Tekstikenttien tyypeille",
+
+"text_area_rows" =>
+"Tekstialueen rivit",
+
+"text_area_rows_cont" =>
+"Tekstialueen tyypeille",
+
+"pull_down_items" =>
+"Valitse listojen ominaisuudet",
+
+"pull_down_items_cont" =>
+"Pudotusvalikko menuille",
+
+"pull_down_instructions" =>
+"Laita kukin tieto omalle riville",
+
+"invalid_characters" =>
+"Antamasi kentännimi sisälti ei sallittuja merkkejä",
+
+"member_data_will_be_deleted" =>
+"Kaikki jäsenen data, tähän kenttään liittyen, poistetaan",
+
+"profile_field_deleted" =>
+"Profiilin kenttä poistettu:",
+
+"no_custom_profile_fields" =>
+"Tällä hetkellä ei ole kustomoituja profiilikenttiä",
+
+"member_search" =>
+"Jäsenten etsintä ",
+
+"member_search_results" =>
+"Jäsenten etsinnän tulokset",
+
+"no_search_results" =>
+"Hakusi ei tuottanut tulosta",
+
+"member_search_instructions" =>
+"Täytä edes yksi kenttä. Voit kirjoittaa osan tai koko sanan/sanoja",
+
+"can_not_delete_only_member" =>
+"Et voi poistaa ainoaa jäsentä tietokannasta",
+
+"can_not_delete_super_admin" =>
+"Et voi poistaa pääkäyttäjää ellei järjestelmässä oli vahintään toista pääkäyttäjää",
+
+"must_be_superadmin_to_delete_one" =>
+"Sinun tulee olla pääkäyttäjä poistaaksesi toisen pääkäyttäjän",
+
+"email_console_log" =>
+"Email konsollin lokit",
+
+"no_cached_email" =>
+"Järjestelmässä ei ole välimuistiin tallennettuja email viestejä",
+
+"email_title" =>
+"Emalin otsikko",
+
+"from" =>
+"Lähettäjä",
+
+"to" =>
+"Vastaanottaja",
+
+"message_sent_to" =>
+"Lähetettävä viesti:",
+
+"email_deleted" =>
+"Email viesti/viestit poistettu",
+
+"template_set_names" =>
+"Saatavilla olevat sivupohjat",
+
+"unable_to_find_templates" =>
+"Jäsenten profiili sivupohjaa ei löydetty.",
+
+"unable_to_find_template_file" =>
+"Jäsenten sivupohjaa ei löydetty.",
+
+"template_updated" =>
+"Sivupohja on päivitetty",
+
+"template_set" =>
+"Sivupohjan asetus:",
+
+"file_not_writable" =>
+"Huom: Tämä tiesdosto ei ole kirjoitettavissa",
+
+"error_opening_template" =>
+"Virhe: Sivupohjaa ei voitu avata kirjoitusta varten",
+
+"file_writing_instructions" =>
+"Et voi tallentaa muutoksia ennen kuin tiedoston oikeudet ovat 666:",
+
+"aim_console" =>
+"AOL Instant Messenger Konsoli",
+
+"breadcrumb" =>
+"Murusia",
+
+"copyright" =>
+"Tekijänoikeus ilmoitus",
+
+"custom_profile_fields" =>
+"Muokattu profiilin kenttä lomake",
+
+"edit_profile_form" =>
+"Jäsenen profiili lomake",
+
+"email_form" =>
+"Email konsoli",
+
+"email_prefs_form" =>
+"Email ominaisuuden lomake",
+
+"email_user_message" =>
+"Email viesti",
+
+"forgot_form" =>
+"Unohtuneen salasana lomake",
+
+"home_page" =>
+"Jäsenen profiilin kotisivu",
+
+"icq_console" =>
+"ICQ konsoli",
+
+"localization_form" =>
+"Paikallisten ominaisuuksien lomake",
+
+"login_form" =>
+"Login lomake",
+
+"memberlist" =>
+"Jäsenten lista sivu",
+
+"memberlist_rows" =>
+"Jäsenten lista rivi",
+
+"menu" =>
+"Jäsenten profiili valikko",
+
+"notepad_form" =>
+"Muistio lomake",
+
+"password_change_warning" =>
+"Salasanan vaihto varoitus",
+
+"public_custom_profile_fields" =>
+"Muokantun profiili kentän rivit",
+
+"public_profile" =>
+"Julkinen jäsenen profiili",
+
+"registration_form" =>
+"Rekisteröitymislomake",
+
+"stylesheet" =>
+"CSS tyylipohjat",
+
+"success" =>
+"Profiili päivitetty-viesti",
+
+"username_change_disallowed" =>
+"Käyttäjätunnuksen vaihdon estoviesti",
+
+"username_password_form" =>
+"Käyttäjätunnus ja salasana kenttä",
+
+"username_row" =>
+"Käyttäjätunnuksen lomakkeen rivi",
+
+''=>''
+);
+?>
