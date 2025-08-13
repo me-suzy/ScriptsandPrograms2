e326501a4a3c50950364722d14b2c90ef97c8949
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:02 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/common.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/common.php
@@ -0,0 +1,78 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[COMMON_Registered_Only] = "É para operadores registrados só.";
+
+$LOCALE[COMMON_Unregistered_Only] = "É para operadores de unregistered só.";
+	
+$LOCALE[COMMON_Username] = "Username";
+
+$LOCALE[COMMON_Function_Invalid] = "É um comando de inválido ou está indisponível a você.";
+
+$LOCALE[COMMON_Operator] = "Foi feito um operador.";
+
+$LOCALE[COMMON_No_Operator] = "Não seja mais operador.";
+
+$LOCALE[COMMON_Login_Button] = "O Login agora";
+
+$LOCALE[COMMON_Not_Accept] = "Nao aceita mensagens privadas.";
+
+$LOCALE[COMMON_Help] = "A ajuda";
+
+$LOCALE[COMMON_No_Encryption] = "MWChat captou-o NAO tem mcrypt instalado em php, que incapacitando encryption.";
+
+$LOCALE[COMMON_System_Message] = "A Mensagem de sistema";
+
+$LOCALE[COMMON_Click_Close] = "O estalido fechar";
+
+$LOCALE[COMMON_YOB] = "O ano de nascimento";
+
+$LOCALE[COMMON_Location] = "A localidade";
+
+$LOCALE[COMMON_Job] = "A ocupação";
+
+$LOCALE[COMMON_Hobbies] = "Os passatempos";
+
+$LOCALE[COMMON_Status] = "O estado";
+
+$LOCALE[COMMON_Comments] = "Comentários adicionais";
+
+$LOCALE[COMMON_Password] = "Nova senha";
+
+$LOCALE[COMMON_Confirm_Password] = "Confirme nova senha";
+
+$LOCALE[COMMON_Logout] = "O Logout de MWChat";
+
+$LOCALE[COMMON_Err_Pass] = "As senhas que você não entrou combinam nem não são pelo menos cinco caráteres em comprimento.";
+
+$LOCALE[COMMON_HelpClick] = "O estalido aqui para ajuda";
+
+$LOCALE[COMMON_Text] = "A mensagem";
+
+$LOCALE[COMMON_Bug_Report] = "A descrição de inseto";
+
+$LOCALE[COMMON_Reason] = "A razão";
+
+$LOCALE[COMMON_Action] = "A ação";
+
+$LOCALE[COMMON_Topic] = "O tema";
+
+$LOCALE[COMMON_URL] = "URL";
+
+$LOCALE[COMMON_Alias] = "é um alias para o comando";
+
+$LOCALE[COMMON_Room_Name] = "O Nome de lugar";
+
+$LOCALE[COMMON_Topic] = "Nenhum";
+
+?>
