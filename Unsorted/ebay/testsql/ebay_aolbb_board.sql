70e60b3f140c661cb3a802e7296501876e961af0
me-suzy <me.suzana@gmail.com>
2025-08-13 03:37:34 +0300

    Add Unsorted/ebay/testsql/ebay_aolbb_board.sql

---
+++ Unsorted/ebay/testsql/ebay_aolbb_board.sql
@@ -0,0 +1,20 @@
+/*	$Id: ebay_aolbb_board.sql,v 1.2 1999/02/21 02:55:59 josh Exp $	*/
+create table ebay_aolbb_board
+(	id				int
+		constraint aolbb_board_id_nn
+		not null,
+	when			date
+		constraint aolbb_board_date_nn
+		not null,
+	entry_len	int
+		constraint aolbb_board_entry_len_nn
+		not null,
+	entry			long raw
+		constraint aolbb_board_entry_nn
+		not null,
+	constraint		aolbb_board_fk
+		foreign key (id)
+		references	ebay_users(id)
+)
+tablespace tbbd01
+storage ( initial 1K next 1K);
