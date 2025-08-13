89301fdd5274bc6f18e68b4ceda0b46523e57c33
me-suzy <me.suzana@gmail.com>
2025-08-13 04:36:06 +0300

    Add Unsorted/fcart/fcart/poweredby.php

---
+++ Unsorted/fcart/fcart/poweredby.php
@@ -0,0 +1,14 @@
+<br>
+<br>
+<hr width="90%">
+<table width="90%" align="center" border="0" bgcolor="<? echo $cl_win_border ?>" cellpadding="1" cellspacing="0">
+<tr><td>
+  <table width="100%" border="0" cellpadding="5" cellspacing="0" height="100%" bgcolor="#FFFFFF">
+  <tr>
+  <td align="center"><img src="images/poweredby.gif" width="84" height="16"><br>
+  <a href="http://www.fcart.com"><img src="images/fcart.gif" width="100" height="66" border="0"></a>
+  </td>
+  </tr>
+  </table>
+</td></tr>
+</table>
