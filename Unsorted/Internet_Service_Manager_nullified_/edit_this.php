592ff7faf76941893ab35916215d01f5dfa77c42
me-suzy <me.suzana@gmail.com>
2025-08-13 05:29:37 +0300

    Add Unsorted/Internet_Service_Manager_nullified_/edit_this.php

---
+++ Unsorted/Internet_Service_Manager_nullified_/edit_this.php
@@ -0,0 +1,24 @@
+<?
+//////////////////////////////////////////////////////////////
+// Program Name         : Internet Service Manager             
+// Program Version      : 1.0                                  
+// Program Author       : InternetServiceManager.com 
+// Supplied by          : Zeedy2k                              
+// Nullified by         : CyKuH [WTN]                          
+// YOU WILL NEED TO EDIT ALL THE VARIABLES IN THIS FILE FOR ISM TO FUNCTION PROPERLY!
+//////////////////////////////////////////////////////////////
+
+$main_dir="/usr/home/www"; //no ending slash!
+$main_url="http://www.mydomain.com/"; //URL to the above dir...no ending slash!
+  
+  $billing_email_address="myemail@mydomain.com";
+  $settle_no_payment_key="enter"; //a password that will be needed if an admin wants to settle an invoice before full payment is received..leave blank for none!
+  
+//mysql_connect_details
+  $mysql_host="localhost";
+  $mysql_user="root";
+  $mysql_pass="";
+  $mysql_db="ism";
+
+
+?>
