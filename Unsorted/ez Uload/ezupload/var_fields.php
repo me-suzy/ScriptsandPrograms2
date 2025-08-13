74392723b6e358bb9c40f5ca2e40fa1af8947bad
me-suzy <me.suzana@gmail.com>
2025-08-13 04:30:10 +0300

    Add Unsorted/ez Uload/ezupload/var_fields.php

---
+++ Unsorted/ez Uload/ezupload/var_fields.php
@@ -0,0 +1,11 @@
+<?php
+/////////////////////////////////////////////////////////////
+// Program Name         : EzUpload Pro                       
+// Program Version      : 2.20                                
+// Program Author       : ScriptsCenter.com                  
+// Supplied by          : CyKuH [WTN]                        
+// Nullified and tested : CyKuH [WTN]                        
+// Distribution         : via WebForum and Forums File Dumps 
+//                    WTN Team `2004
+/////////////////////////////////////////////////////////////
+?>
