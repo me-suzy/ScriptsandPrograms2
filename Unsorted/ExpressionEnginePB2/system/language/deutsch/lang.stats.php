f0372a8b312ee1989e4fcb809c77fd3de879d4a0
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:36 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.stats.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.stats.php
@@ -0,0 +1,24 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"stats_module_name" =>
+"Statistiken",
+
+"stats_module_description" =>
+"Modul zur Darstellung von Statistiken",
+
+//----------------------------------------
+
+
+
+
+
+// END
+''=>''
+);
+?>
