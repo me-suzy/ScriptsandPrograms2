4ee04dd021109ce8caf414e3ea839b0294cd2afc
me-suzy <me.suzana@gmail.com>
2025-08-13 04:08:25 +0300

    Add Unsorted/evoArticles/admin/lib/class_comments.php

---
+++ Unsorted/evoArticles/admin/lib/class_comments.php
@@ -0,0 +1,19 @@
+<?php
+// +------------------------------------------------------------------+
+// | evoArticles 
+// | Copyright (c) 2003-2004 Evo-Dev
+// +------------------------------------------------------------------+
+
+class Comments
+{
+	function Comments()
+	{
+		//cons
+	}
+
+	// forum userid
+	// forum password
+	// forum url
+	// category mapping
+
+}
