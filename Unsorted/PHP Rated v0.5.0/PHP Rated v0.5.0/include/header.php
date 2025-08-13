ab8078f95842b76747aa15b01297827d617e954c
me-suzy <me.suzana@gmail.com>
2025-08-13 07:45:11 +0300

    Add Unsorted/PHP Rated v0.5.0/PHP Rated v0.5.0/include/header.php

---
+++ Unsorted/PHP Rated v0.5.0/PHP Rated v0.5.0/include/header.php
@@ -0,0 +1,22 @@
+<?
+
+/*
+ * $Id: header.php,v 1.1.1.1 2002/08/31 14:25:16 destiney Exp $
+ *
+ */
+
+$doc_head = <<<EOF
+<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>&nbsp;.: $site_title :.</title>
+<meta name="keywords" content="prated,rate,amatuer,pictures,xxx,rated,mysql,porn,members,jpg,bmp,gif" />
+<meta name="description" content="pRated is a commercial PHP Script which allows you to enhance your site by adding a upload and rate me section." />
+EOF;
+
+/*
+ * $Id: header.php,v 1.1.1.1 2002/08/31 14:25:16 destiney Exp $
+ *
+ */
+
+?>
