b8d9333d87f6dc58915653760f538835186377ea
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:18 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-community.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-community.php
@@ -0,0 +1,29 @@
+<?php
+
+##################################################
+
+# /community.php
+
+##################################################
+
+define( "CMNT_SHRD_HEADER", "NOVES LLISTES COMPARTIDES" );
+define( "CMNT_SHRD_LIST_BTN", "Llista completa" );
+define( "CMNT_SHRD_PLAY_HELP", "Feu clic per reproduir aquesta llista de cançons." );
+define( "CMNT_SHRD_VIEW_HELP", "Feu clic per veure els detalls d\' aquesta llista." );
+define( "CMNT_SHRD_BY", "Per" );
+define( "CMNT_SHRD_FOR", "En" );
+
+define( "CMNT_DL_HEADER", "LES CANÇONS MÉS POPULARS" );
+define( "CMNT_DL_TOP10_BTN", "Reprodueix el Top10" );
+define( "CMNT_DL_PLAY_HELP", "Feu clic per reproduir aquesta cançó." );
+define( "CMNT_DL_DLOAD_HELP", "Feu clic per descarregar aquesta pista." );
+define( "CMNT_DL_INFO_HELP", "Feu clic per veure els detalls de la pista." );
+define( "CMNT_DL_GENRE_HELP", "Feu clic per veure totes les pistes d'aquest gènere." );
+define( "CMNT_DL_ARTIST", "INT" );
+define( "CMNT_DL_ARTIST_HELP", "Feu clic per veure totes les pistes d'aquest intèrpret." );
+define( "CMNT_DL_ALBUM", "AL" );
+define( "CMNT_DL_ALBUM_HELP", "Feu clic per veure totes les pistes d'aquest àlbum." );
+
+##################################################
+
+?>
