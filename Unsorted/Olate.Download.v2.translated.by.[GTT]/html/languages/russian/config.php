1895475e3b48d00d46fa277cd4a6613a7254a373
me-suzy <me.suzana@gmail.com>
2025-08-13 07:07:06 +0300

    Add Unsorted/Olate.Download.v2.translated.by.[GTT]/html/languages/russian/config.php

---
+++ Unsorted/Olate.Download.v2.translated.by.[GTT]/html/languages/russian/config.php
@@ -0,0 +1,31 @@
+<?php
+/***************************************************************************
+ *                      Olate Download v2 - Download Manager
+ *
+ *                           http://www.olate.com
+ *                            -------------------
+ *   author                : David Mytton
+ *   copyright             : (C) Olate 2003 
+ *
+ *   Support for Olate scripts is provided at the Olate website. Licensing
+ *   information is available in the license.htm file included in this
+ *   distribution and on the Olate website.
+ *   distribution and on the Olate website.                  
+ *   
+ *   
+ *   
+ *   Fully translated by GTT                  
+ ***************************************************************************/
+
+$language_config = array();
+
+// Language details
+$language_config['language']			= 'Russian'; 		            // Íàçâàíèå ÿçûêà
+$language_config['scriptver']			= '2.2.0'; 					// Äëÿ êàêîé âåðñèè ñêðèïòà Olate Download ïåðåâîä
+$language_config['released']			= '03/03/04'; 				// Äàòà ðåëèçà
+
+// Author/Translator Details
+$language_config['author']				= 'Shadoff[GTT]'; 			// Àâòîð ïåðåâîäà
+$language_config['url']					= 'http://fully.private.url'; 	// Ñàéò àâòîðà ïåðåâîäà
+$language_config['email']				= 'not@available.now'; 		      // E-Mail àâòîðà ïåðåâîäà
+?>
