30b2b874d83e417c37753955e862621ca3f810e1
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:14 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_user-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_user-edit.php
@@ -0,0 +1,51 @@
+<?php
+
+##################################################
+
+# /admin/user-edit.php
+
+##################################################
+
+define( "USEDIT_CHECKFORM_1", "Veuillez soumettre votre nom complet." );
+define( "USEDIT_CHECKFORM_2", "Veuillez soumettre votre adresse de courrier électronique." );
+define( "USEDIT_CHECKFORM_3", "Veuillez soumettre une adresse de courrier électronique valide." );
+define( "USEDIT_CHECKFORM_4", "Veuillez choisir un mot de passe." );
+define( "USEDIT_CHECKFORM_5", "Veuillez confirmer votre mot de passe." );
+define( "USEDIT_CHECKFORM_6", "Votre mot de passe est trops court.\\n   Veuillez utiliser un minimum de 6 caractères." );
+define( "USEDIT_CHECKFORM_7", "Votre mot de passe est trops long.\\n   Veuillez utiliser un maximum de 20 caractères." );
+define( "USEDIT_CHECKFORM_8", "Les mots de passe ne sont pas les mêmes. Veuillez entrez vos mots de passe à nouveau." );
+
+define( "USEDIT_DENIED_1", "Désolé, mais cette adresse électronique n'est pas reconnue." );
+define( "USEDIT_DENIED_2", "Accés refusé. Veuillez recommencer." );
+define( "USEDIT_DENIED_3", "Désolé, mais vous ne pouvez pas effacer votre prôpre compte." );
+define( "USEDIT_DENIED_4", "Cette adresses électronique et/ou ce surnom sont déjà enregistrés." );
+
+define( "USEDIT_NEW_HEADER", "NOUVEL USAGER" );
+define( "USEDIT_NEW_CAPTION", "Utilisez le formulaire suivant pour ouvrir un nouveau compte." );
+define( "USEDIT_NEW_SUCCESS", "Vous avez inscris l'usager suivant" );
+
+define( "USEDIT_EDIT_HEADER", "COMPTE D'USAGER" );
+define( "USEDIT_EDIT_CAPTION", "Utilisez le formulaire suivant pour modifier un compte." );
+
+define( "USEDIT_FORMS_GROUP", "Groupe" );
+define( "USEDIT_FORMS_NAME", "Nom Complet" );
+define( "USEDIT_FORMS_NICKNAME", "Surnom" );
+define( "USEDIT_FORMS_EMAIL", "Adresse Électronique" );
+define( "USEDIT_FORMS_PASSWD_1", "Mot De Passe" );
+define( "USEDIT_FORMS_PASSWD_2", "Confirmez Le Mot De Passe" );
+define( "USEDIT_FORMS_PASSWD_3", "Nouveau Mot De Passe" );
+define( "USEDIT_FORMS_PASSWD_4", "Confirmez Le Mot De Passe" );
+define( "USEDIT_FORMS_CREATED", "Date Créé" );
+define( "USEDIT_FORMS_UPDATED", "Date Changé" );
+define( "USEDIT_FORMS_LOGIN_CNT", "Compte D'Entrée" );
+define( "USEDIT_FORMS_BTN_LOGIN", "Inscription" );
+define( "USEDIT_FORMS_BTN_REGISTER", "Entrer" );
+define( "USEDIT_FORMS_BTN_SAVE", "Sauvegarder" );
+define( "USEDIT_FORMS_BTN_RESET", "Recommencer" );
+define( "USEDIT_FORMS_BTN_DEL", "Effacer" );
+define( "USEDIT_FORMS_BTN_DEL_CONF", "Êtes-vous sûr de vouloir fermer ce compte?" );
+define( "USEDIT_FORMS_BTN_USLIST", "Liste D'usagers" );
+
+##################################################
+
+?>
