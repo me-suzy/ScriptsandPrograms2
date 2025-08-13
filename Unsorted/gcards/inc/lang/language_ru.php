3f7f9e1fa888544dbfcaaab702a37a0d6ed14b80
me-suzy <me.suzana@gmail.com>
2025-08-13 04:49:43 +0300

    Add Unsorted/gcards/inc/lang/language_ru.php

---
+++ Unsorted/gcards/inc/lang/language_ru.php
@@ -0,0 +1,148 @@
+<?
+// index.php
+$index01 = 'Âñå Êàòåãîðèè';
+$index02 = 'Âûáåðèòå îòêðûòêó íèæå, ÷òîáû íà÷àòü!';
+
+// general navigation
+$nav01 = 'Íàçàä';
+$nav02 = 'Ñëåäóþùèé';
+$nav03 = 'Äîìîé';
+$nav04 = 'Ïîêàçàòü';
+$nav05 = 'Ìåíþ Àäìèíèñòðàòîðà';
+$nav06 = 'Ñìåíèòü Ïàðîëü';
+$nav07 = 'Logout';
+$nav08 = 'Log In';
+$nav09 = 'Powered by';
+$nav10 = 'rows per page';
+$nav11 = 'Ïîæàëóéñòà èäèòå <a href="javascript:history.go(-1);">íàçàä</a>';
+$nav12 = 'Âåðíóòüñÿ ê';
+
+
+
+// UIfunctions.php
+$uifunc01 = 'Ïîëüçîâàòåëü: ';
+
+// compose.php
+$compose01 = 'Îòêðûòêà, íå âûáðàíà! Èäèòå  <a href="index.php">íàçàä</a> ÷òîáû âûáðàòü îòêðûòêó';
+$compose02 = 'Äîáàâòå òåêñò íèæå, ÷òîáû îòïðàâèòü âàøó îòêðûòêó!';
+$compose03 = 'Èìÿ';
+$compose04 = 'Àäðåñ ýëåêòðîííîé ïî÷òû';
+$compose05 = 'Îò:';
+$compose06 = 'To:';
+$compose07 = 'Óâåäîìëÿòü ìåíÿ, êîãäà îòêðûòêà ïîëó÷åíà';
+$compose08 = 'Separate email addresses with a comma';
+
+// preview.php
+$preview01 = "Ïîæàëóéñòà çàïîëíèòå âñå ïîëÿ ôîðìû!";
+$preview02 = 'íå âåðíûé àäðåñ ýëåêòðîííîé ïî÷òû';
+$preview03 = "íå âåðíûé àäðåñ ýëåêòðîííîé ïî÷òû! $nav11";
+$preview04 = 'Îòïðàâèòü';
+
+// sendcard.php
+$sendcard01 = 'Îøèáêà! Ôîðìà, çàïîëíåíà íå ïðàâèëüíî. Ïîæàëóéñòà èäèòå <a href="javascript:history.go(-1);">íàçàä</a>';
+$sendcard02 = 'error inserting: ';
+$sendcard03 = 'Îòêðûòêà îòïðàâëåíà...';
+$sendcard04 = 'Îòêðûòêà íå ìîæåò áûòü îòïðàâëåíà...';
+
+// getcard.php
+$getcard01 = 'Îøèáêà - no card ID';
+$getcard02 = '$to_email ïîëó÷èë âàøó îòêðûòêó';
+$getcard03 = 'Îòïðàâèòü åùå îäíó îòêðûòêó $from_name ';
+
+// showcard.php
+$showcard01 = 'Ýòà îòêðûòêà îò';
+
+// news.php
+$news01 = 'Àðõèâèðîâàííûå Íîâîñòè';
+$news02 = 'Posted';
+$news03 = 'by';
+$news04 = 'news articles per page';
+$news05 = 'Íîâîñòè';
+$news06 = 'Òåìà';
+$news07 = 'Òåêñò';
+$news08 = 'Äàòà';
+$news09 = 'Àâòîð';
+
+
+
+// user related text
+$user01 = 'Èìÿ ïîëüçîâàòåëÿ';
+$user02 = 'Ïàðîëü';
+$user03 = 'Âû íå çàðåãèñòðèðîâàíû â - Âû íå ìîæåòå logout!';
+$user04 = 'Âû óñïåøíî ïîêèíóëè ïàíåëü óïðàâëåíèÿ...';
+$user05 = 'Ïîäòâåðæäåíèå Ïàðîëÿ';
+$user06 = 'Àäðåñ ýëåêòðîííîé ïî÷òû';
+$user07 = 'Ðîëü';
+$user08 = 'Ïîëüçîâàòåëü';
+$user09 = 'Ïàðîëü íå îïîçíàí - ïîæàëóéñòà ïðîáóéòå ñíîâà';
+$user10 = '(Leave blank to keep current value)';
+$user11 = 'Ñòàðûé ïàðîëü íå ñîîòâåòñòâîâàë âàøåìó ñóùåñòâóþùåìó ïàðîëþ';
+$user12 = 'Ïàðîëü îáíîâëåí óñïåøíî!';
+$user13 = 'Ñòàðûé Ïàðîëü';
+$user14 = 'Íîâûé Ïàðîëü';
+
+/*
+*********************
+** ADMIN DIRECTORY **
+*********************
+*/
+
+// Authentication
+$auth01 = 'Óñòàíîâëåíèå ïîäëèííîñòè íåóäà÷íî äëÿ äàííîãî ïîëüçîâàòåëÿ';
+$auth02 = 'Ïîæàëóéñòà ââåäèòå èìÿ ïîëüçîâàòåëÿ è ïàðîëü.';
+$auth03 = 'Âû íå çàðåãèñòðèðîâàíû.';
+$auth04 = 'Âû íå èìååòå äîñòàòî÷íûõ ïðèâèëåãèé äëÿ ïðîñìîòðà ýòîé ñòðàíèöû!';
+
+// admin.php
+$admin01 = 'Îïöèè Àäìèíèñòðàòîðà';
+$admin02 = 'Óïðàâëåíèå';
+$admin03 = "Îòêðûòêà $admin02";
+$admin04 = "Êàòåãîðèÿ $admin02";
+$admin05 = "Íîâîñòè $admin02";
+$admin06 = "Ïîëüçîâàòåëü $admin02";
+$admin07 = 'Ïîñìîòðåòü, äîáàâèòü, îáíîâèòü, è óäàëèòü';
+$admin08 = 'ID';
+$admin09 = 'óäàëåíî èç áàçû äàííûõ...';
+$admin10 = 'óäàëåíî ñ ñåðâåðà...';
+$admin11 = 'íåò';
+$admin12 = 'îáíîâèòü';
+$admin13 = 'óäàëèòü';
+$admin14 = 'äîáàâèòü';
+$admin15 = 'Äîáàâèòü íîâîå';
+$admin16 = 'Çàãðóçèòü';
+
+// actions
+$action01 = 'Äåéñòâèå';
+$action02 = 'Äîáàâèòü';
+$action03 = 'Ðåäàêòèðîâàòü';
+$action04 = 'Óäàëèòü';
+$action05 = 'Ïîñìîòðåòü';
+$action06 = 'Îáíîâèòü';
+$action07 = 'Âûõîä';
+$action08 = 'Ïðåäâàðèòåëüíûé ïðîñìîòð';
+
+// cards.php
+$cards01 = 'Èçîáðàæåíèå';
+$cards02 = 'Thumbnail';
+$cards03 = 'Âðåìÿ îòïðàâêè';
+$cards04 = 'Èçîáðàæåíèå';
+$cards05 = 'Èìÿ Îòêðûòêè';
+$cards06 = 'Leave blank for automatic thumbnail';
+
+// categories
+$cat01 = 'Êàòåãîðèÿ';
+$cat02 = 'Íàçâàíèå Êàòåãîðèè';
+$cat03 = 'Îòêðûòêè â Êàòåãîðèè';
+
+// upload.php
+$upload01 = 'Èçîáðàæåíèå íå çàãðóæåíî!';
+$upload02 = "file copy error with 'move_uploaded_file' function!";
+$upload03 = 'Èçîáðàæåíèå çàãðóæåíî óñïåøíî...';
+$upload04 = 'File copy error!';
+$upload05 = 'Thumbnail uploaded successfully...';
+$upload06 = 'Uploaded image used as thumbnail...';
+$upload07 = 'Thumbnail created successfully...';
+$upload08 = 'Please upload a thumbnail if image is not a JPEG.';
+
+
+?>
