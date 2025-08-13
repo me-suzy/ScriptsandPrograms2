256e2e8d96b47d68bbe88a31eb396dfa39aff864
me-suzy <me.suzana@gmail.com>
2025-08-13 05:26:57 +0300

    Add Unsorted/ImageFolio_Commerce_v1.0__nullified_/IFCOMB/store_files/ifc_images/imageprotect.js

---
+++ Unsorted/ImageFolio_Commerce_v1.0__nullified_/IFCOMB/store_files/ifc_images/imageprotect.js
@@ -0,0 +1,19 @@
+
+<!-- BizDesign ImageFolio Professional Edition.  Copyright 2000 BizDesign, Inc. -->
+
+<!--
+function right(e) {
+if (navigator.appName == 'Netscape' && (e.which == 2 || e.which == 3)) {
+alert('This image is copyright protected');
+return false;
+}
+else if (navigator.appName == 'Microsoft Internet Explorer' && (event.button==2 || event.button == 3)) {
+alert('This image is copyright protected');
+return false;
+}
+return true;
+}
+document.onmousedown=right;
+if (document.layers) window.captureEvents(Event.MOUSEDOWN);
+window.onmousedown=right;
+// -->
