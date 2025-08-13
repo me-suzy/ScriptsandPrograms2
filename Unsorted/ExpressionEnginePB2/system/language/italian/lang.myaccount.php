d713790c16a27de0abaa267cc2d0b38dbad6e330
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:08 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.myaccount.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.myaccount.php
@@ -0,0 +1,457 @@
+<?php
+$L = array(
+
+
+"preferences_updated" =>
+"Preferenze aggiornate",
+
+"homepage_preferences" =>
+"Preferenze Home Page",
+
+"set_display_order" =>
+"Ordine di visualizzazione",
+
+"please_update_order" =>
+"Imposta l&#39;ordine di visualizzazione delle voci",
+
+"left_column" =>
+"Colonna sinistra",
+
+"right_column" =>
+"Colonna destra",
+
+"do_not_show" =>
+"Non visualizzare",
+
+"recent_entries" =>
+"I tuoi dieci interventi più recenti",
+
+"recent_comments" =>
+"I dieci commenti più recenti",
+
+"recent_members" =>
+"Le dieci iscrizioni più recenti",
+
+"new_release_indicator" =>
+"Avviso nuova release software",
+
+"site_statistics" =>
+"Statistiche del sito",
+
+"member_search_form" =>
+"Modulo di ricerca utenti",
+
+"validating_members" =>
+"Utenti in attesa di attivazione",
+
+"php_sql_versions" =>
+"Versioni PHP/SQL",
+
+"system_offline_indicator" =>
+"Avviso di sistema offline",
+
+"personal_settings" =>
+"Settaggi personali",
+
+"weblog_settings" =>
+"Settaggi weblog",
+
+"control_panel_settings" =>
+"Personalizzazione CP",
+
+"administrative_settings" =>
+"Settaggi amministrazione",
+
+"current_member" =>
+"Utente:",
+
+"member_stats" =>
+"Statistiche account per:",
+
+"join_date" =>
+"Data di iscrizione",
+
+"last_visit" =>
+"Ultima visita",
+
+"total_entries" =>
+"Interventi totali",
+
+"last_entry_date" =>
+"Data dell&#39;intervento più recente",
+
+"total_comments" =>
+"Commenti totali",
+
+"last_comment_date" =>
+"Data del commento più recente",
+
+"notepad" =>
+"Notepad",
+
+"can_not_view_superadmin_profile" =>
+"Non puoi consultare il profilo del Super Admin",
+
+"only_self_notpad_access" =>
+"Non puoi consultare il notepad di altri utenti",
+
+"notepad_size" =>
+"Dimensione notepad",
+
+"notepad_instructions" =>
+"Utilizza questo form per memorizzare note e informazioni personali",
+
+"notepad_updated" =>
+"Notepad aggiornato",
+
+"localization_settings" =>
+"Settaggi localizzazione",
+
+"localization_updated" =>
+"Localizzazione aggiornata",
+
+"daylight_savings_time" =>
+"Ora legale",
+
+"timezone" =>
+"Seleziona il fuso orario",
+
+"language_choice" =>
+"Seleziona la lingua",
+
+"username_and_password" =>
+"Username e password",
+
+"extras" =>
+"Extra",
+
+"edit_profile" =>
+"Modifica il tuo profilo",
+
+"profile_form" =>
+"Form profilo",
+
+"base_profile" =>
+"Profilo base",
+
+"extended_profile" =>
+"Profilo avanzato",
+
+"localization" =>
+"Settaggi localizzazione",
+
+"time_format" =>
+"Formato ora e data",
+
+"united_states" =>
+"Stati Uniti",
+
+"european" =>
+"Europeo",
+
+"your_ping_servers" =>
+"Ping server",
+
+"your_html_buttons" =>
+"Pulsanti formati HTML",
+
+"change_password" =>
+"Cambia password",
+
+"email_settings" =>
+"Settaggi e-mail",
+
+"subscriptions" =>
+"Sottoscrizioni",
+
+"edit_subscriptions" =>
+"Conulta le tue sottoscrizioni",
+
+"customize_homepage" =>
+"Personalizza Control Panel Homepage",
+
+"customize_cp" =>
+"Personalizza Control Panel",
+
+"cp_homepage" =>
+"Control Panel Homepage",
+
+"cp_theme" =>
+"Tema Control Panel",
+
+"choose_theme" =>
+"Scegli il tema del pannello di controllo",
+
+"administrative_options" =>
+"Opzioni di amministrazione",
+
+"administrative_options_updated" =>
+"Opzioni di amministrazione aggiornate",
+
+"screen_name" =>
+"Nome sul sito",
+
+"email" =>
+"Indirizzo e-mail",
+
+"url" =>
+"URL",
+
+"bio" =>
+"Bio",
+
+"birthday" =>
+"Compleanno",
+
+"year" =>
+"Anno",
+
+"month" =>
+"Mese",
+
+"day" =>
+"Giorno",
+
+"accept_admin_email" =>
+"Accetta messaggi dagli amminitratori del sito",
+
+"accept_user_email" =>
+"Accetta messaggi dagli altri utenti del sito",
+
+"notify_by_default" =>
+"Abilita di default i messaggi di notifica quando spedisci messaggi",
+
+"location" =>
+"Località",
+
+"occupation" =>
+"Occupazione",
+
+"interests" =>
+"Interessi",
+
+"icq" =>
+"ICQ",
+
+"aol_im" =>
+"AOL IM",
+
+"yahoo_im" =>
+"Yahoo IM",
+
+"msn_im" =>
+"MSN IM",
+
+"user_ip_address" =>
+"Indirizzo IP",
+
+"member_group_assignment" =>
+"Assegnazione categorie utenti",
+
+"member_group_warning" =>
+"Fare molta attenzione nell&#39;assegnare questi settaggi",
+
+"include_in_multiauthor_list" =>
+"Includi l&#39;utente nella lista autori della pagina PUBBLICA?",
+
+"profile_updated" =>
+"Profilo aggiornato",
+
+"super_admin_demotion_alert" =>
+"In quanto Super Admin, non ti è permesso cambiare categoria utenti",
+
+"username" =>
+"Username",
+
+"username_change_not_allowed" =>
+"Non è permesso modificare lo username prescelto",
+
+"password" =>
+"Password",
+
+"password_change" =>
+"Form modifica password",
+
+"new_password" =>
+"Nuova password",
+
+"password_confirm" =>
+"Conferma password",
+
+"new_password_confirm" =>
+"Conferma nuova password",
+
+"existing_password" =>
+"Password attuale",
+
+"invalid_email_address" =>
+"L&#39;indirizzo e-mail specificato non è valido",
+
+"password_required" =>
+"Devi specificare la password attuale per aggiornare questi dati",
+
+"password_required_for_email" =>
+"Per modificare l&#39;indirizzo e-mail devi specificare la password attuale",
+
+"leave_blank" =>
+"Non compilare se non desideri fare modifiche",
+
+"missing_username" =>
+"Devi specificare uno username",
+
+"invalid_characters_in_username" =>
+"Il tuo username contiene caratteri non ammessi.",
+
+"missing_password" =>
+"Devi specificare una password",
+
+"missing_current_password" =>
+"Per applicare le modifiche devi specificare la password attuale",
+
+"password_in_dictionary" =>
+"L&#39;utilizzo di alcuni termini specifici non è consentito",
+
+"not_secure_password" =>
+"La password deve contenere almeno un carattere maiuscolo, uno minuscolo e un numero",
+
+"invalid_password" =>
+"La password specificata non è corretta",
+
+"missing_email" =>
+"Devi specificare un indirizzo e-mail",
+
+"banned_email" =>
+"L&#39;indirizzo e-mail specificato è interdetto",
+
+"missmatched_passwords" =>
+"La password e la sua conferma non coincidono",
+
+"username_too_short" =>
+"Lo username deve contenere almeno %x caratteri",
+
+"password_too_short" =>
+"La password deve contenere almeno %x caratteri",
+
+"username_password_too_long" =>
+"Usernames e passwords non possono superare la lunghezza di 32 caratteri",
+
+"username_taken" =>
+"Lo username che hai scelto non è disponibile",
+
+"screen_name_taken" =>
+"Il nome sul sito che hai scelto non è disponibile",
+
+"email_taken" =>
+"L&#39;indirizzo e-mail che hai scelto non è disponibile",
+
+"password_based_on_username" =>
+"La pasword non può essere basata sullo username",
+
+"username_changed" =>
+"Username modificato per:",
+
+"screen_name_changed" =>
+"Nome sul sito modificato per:",
+
+"email_changed" =>
+"E-mail modificato per:",
+
+"password_change_requires_login" =>
+"Nota: se modifichi la password devi ripetere il log-in",
+
+"settings_updated" =>
+"Settaggi aggiornati",
+
+"new_member_added" =>
+"Creato l&#39;account:",
+
+"password_change_warning" =>
+"Nota: hai modificato la tua password, quindi devi ripetere il log-in",
+
+"ping_servers" =>
+"Ping server",
+
+"pingservers_updated" =>
+"Ping server aggiornati",
+
+"html_buttons" =>
+"Pulsanti formati HTML",
+
+"html_buttons_updated" =>
+"Pulsanti HTML aggiornati",
+
+"quick_links" =>
+"Link rapidi",
+
+"only_self_qucklink_access" =>
+"Non è consentito consultare i link di altri utenti",
+
+"quicklinks_updated" =>
+"Link rapidi aggiornati",
+
+"quick_link_description" =>
+"Utilizza questo modulo per aggiungere link personali nella prima barra del menu.",
+
+"quick_link_description_more" =>
+"Questi link saranno non saranno visibili da altri.",
+
+"quicklinks_delete_instructions" =>
+"Per cancellare una voce, invia il form lasciando vuoto il campo Titolo link",
+
+"link_title" =>
+"Titolo link",
+
+"link_url" =>
+"URL link",
+
+"link_order" =>
+"Ordine",
+
+"no_extended_fields" =>
+"Non esistono attualmente campi estesi per il profilo utenti",
+
+"bookmarklet" =>
+"Bookmarklet",
+
+"bookmarklet_created" =>
+"Bookmarklet creato",
+
+"bookmarklet_info" =>
+"Un bookmarklet permette di pubblicare un intervento sul sito senza accedere al pannello di controllo.",
+
+"consult_manual" =>
+"Consulta la documentazione per maggiori informazioni.",
+
+"bookmarklet_name" =>
+"Nome del tuo bookmarklet",
+
+"single_word_no_spaces" =>
+"Parole singole senza spazi",
+
+"weblog_name" =>
+"Scegli il weblog di destinazione",
+
+"no_blogs_assigned_to_user" =>
+"Nessun weblog è assegnato al tuo account.",
+
+"select_field" =>
+"Seleziona il campo in cui &quot;incollare&quot; automaticamente",
+
+"bookmarklet_next_step" =>
+"Prossima fase",
+
+"create_the_bookmarklet" =>
+"Crea il bookmarklet",
+
+"no_fields_assigned_to_blog" =>
+"Al weblog selezionato non è stato assegnato alcun gruppo di campi",
+
+"safari_users" =>
+"Seleziona se usi Safari come browser",
+
+"bookmarklet_instructions" =>
+"Salva questo link tra i tuoi preferiti:",
+
+''=>''
+);
+?>
