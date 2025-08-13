b6ea5c2a7bad5b7a943795fc15efbc506da1fd8c
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:30 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_user-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_user-edit.php
@@ -0,0 +1,51 @@
+<?php
+
+##################################################
+
+# /admin/user-edit.php
+
+##################################################
+
+define( "USEDIT_CHECKFORM_1", "Bitte geben Sie Ihren Namen ein." );
+define( "USEDIT_CHECKFORM_2", "Bitte geben Sie Ihre email adresse ein." );
+define( "USEDIT_CHECKFORM_3", "Bitte geben Sie eine gültige email adresse ein." );
+define( "USEDIT_CHECKFORM_4", "Bitte geben Sie ein Passwort ein." );
+define( "USEDIT_CHECKFORM_5", "Bitte geben Sie das Passwort erneut ein." );
+define( "USEDIT_CHECKFORM_6", "Das neue Passwort ist zu kurz.\\n   Bitte benutzen Sie mindestens 6 Zeichen." );
+define( "USEDIT_CHECKFORM_7", "Das neue Passwort ist zu lang.\\n   Bitte benutzen Sie maximal 20 Zeichen." );
+define( "USEDIT_CHECKFORM_8", "Die Passwörter stimmen nicht überein.\\n   Bitte wiederholen Sie Ihr neues Passwort." );
+
+define( "USEDIT_DENIED_1", "Zugriff verweigert. Sie müssen Administrator sein, um sich anmelden zu können." );
+define( "USEDIT_DENIED_2", "Zugriff verweigert. Bitte versuchen Sie es erneut." );
+define( "USEDIT_DENIED_3", "Sie können sich nicht selbst löschen." );
+define( "USEDIT_DENIED_4", "Diese email-Adresse und/oder dieser Benutzernamen sind bereits registriert." );
+
+define( "USEDIT_NEW_HEADER", "BENUTZERKONTO ERSTELLEN" );
+define( "USEDIT_NEW_CAPTION", "Bitte die Felder unten ausfüllen um ein neues Benutzerkonto einzurichten." );
+define( "USEDIT_NEW_SUCCESS", "Sie haben folgenden Benutzer erstellt:" );
+
+define( "USEDIT_EDIT_HEADER", "BENUTZERKONTO INFORMATION" );
+define( "USEDIT_EDIT_CAPTION", "Benutzen Sie die Felder um Änderungen an ihrem Benutzerkonto vorzunehmen." );
+
+define( "USEDIT_FORMS_GROUP", "Gruppe" );
+define( "USEDIT_FORMS_NAME", "Nachname, Vorname" );
+define( "USEDIT_FORMS_NICKNAME", "Benutzername" );
+define( "USEDIT_FORMS_EMAIL", "Email-Adresse" );
+define( "USEDIT_FORMS_PASSWD_1", "Passwort" );
+define( "USEDIT_FORMS_PASSWD_2", "Passwort bestätigen" );
+define( "USEDIT_FORMS_PASSWD_3", "Neues Passwort" );
+define( "USEDIT_FORMS_PASSWD_4", "Neues Passwort bestätigen" );
+define( "USEDIT_FORMS_CREATED", "Erstellungs-Datum" );
+define( "USEDIT_FORMS_UPDATED", "Änderungs-Datum" );
+define( "USEDIT_FORMS_LOGIN_CNT", "Anmeldungszähler" );
+define( "USEDIT_FORMS_BTN_LOGIN", "Konto erstellen" );
+define( "USEDIT_FORMS_BTN_REGISTER", "Anmelden" );
+define( "USEDIT_FORMS_BTN_SAVE", "Speichern" );
+define( "USEDIT_FORMS_BTN_RESET", "Zurücksetzen" );
+define( "USEDIT_FORMS_BTN_DEL", "Löschen" );
+define( "USEDIT_FORMS_BTN_DEL_CONF", "Sind Sie sicher das Sie diesen Benutzer löschen wollen?" );
+define( "USEDIT_FORMS_BTN_USLIST", "Benutzer Liste" );
+
+##################################################
+
+?>
