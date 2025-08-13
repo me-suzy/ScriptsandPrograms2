d1b9f0adfe524e406172ad19c4cdb425e0f4cf19
me-suzy <me.suzana@gmail.com>
2025-08-13 04:16:33 +0300

    Add Unsorted/expired_domain_name_script_null.zip/test.php

---
+++ Unsorted/expired_domain_name_script_null.zip/test.php
@@ -0,0 +1,9 @@
+<?php
+	include ("vars.inc.php");
+	
+	echo get_pop_altavista("google.com")."<br>";
+	echo get_pop_google("google.com")."<br>";
+	echo get_pop_ms("google.com")."<br>";
+	echo get_pop_yahoo("google.com")."<br>";
+
+?>
