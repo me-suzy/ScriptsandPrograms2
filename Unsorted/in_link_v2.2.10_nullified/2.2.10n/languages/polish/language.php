c11c69ed7f78138f8f3c3db6b3d65ea2b29b7a42
me-suzy <me.suzana@gmail.com>
2025-08-13 05:47:29 +0300

    Add Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/polish/language.php

---
+++ Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/polish/language.php
@@ -0,0 +1,306 @@
+<?php
+## In-link User Language File - Polish
+## Author: Intechnic Corporation
+## Date: 10/22/2001
+##
+## This file is designed for the following version of In-link: 2.0.6
+## You can modify the text in this file to suit your language needs. 
+## Make sure to escape quotation marks with \" . 
+
+#########################################################################################
+#Language variables
+
+$lu_char_set								=   "ISO-8859-2";
+$lu_this_language							=	"Polska";	
+
+#########################################################################################
+#Navigation Menu
+
+$lu_getting_rated							=	"Jak Zdobyæ Oceny";
+$lu_go_to_page								=	"Przejd do Strony";
+$lu_search_results							=	"Wyniki Wyszukiwania";
+$lu_nav_addlink								=	"Dodaj Adres";
+$lu_nav_editorspick							=	"Redakcja Poleca";
+$lu_nav_gettingrated						=	"Jak Zdobyæ Oceny";
+$lu_nav_home								=	"Front";
+$lu_nav_login								=	"Logowanie";
+$lu_nav_logout								=	"Wylogowywanie";
+$lu_nav_maillist							=	"Subskrypcja";
+$lu_nav_modlink								=	"Modyfikacja Odnonika";
+$lu_nav_new									=	"Nowe Linki";
+$lu_nav_pop									=	"Najbardziej Popularne";
+$lu_nav_profile								=	"Profil";
+$lu_nav_register							=	"Rejestracja";
+$lu_nav_search								=	"Wyszukiwanie";
+$lu_nav_search_advanced						=	"Wyszukiwanie Zaawansowane";
+$lu_nav_suggestcat							=	"Zasugeruj Kategoriê";
+$lu_nav_top									=	"Najwy¿ej Notowane";
+$lu_navbar_seperator						=	" >";
+
+
+#########################################################################################
+#Page Titles
+
+$lu_title_add_link							=	"Dodaj Adres";
+$lu_title_add_review						=	"Napisz Recenzjê";
+$lu_title_confirm							=	"Potwierdzenie";
+$lu_title_edit_link							=	"Modyfikacja Adresu";
+$lu_title_editors_picks						=	"Redakcja Poleca";
+$lu_title_error								=	"B³¹d";
+$lu_title_gettingrated						=	"Zdobywanie Ocen";
+$lu_title_login								=	"Logowanie";
+$lu_title_modify							=	"Zmieñ Swój Odnonik";
+$lu_title_most_popular						=	"Najbardziej Popularne Odnoniki";
+$lu_title_msg								=	"Wiadomoæ Systemowa";
+$lu_title_new_links							=	"Nowe Odnoniki";
+$lu_title_profile							=	"Dane U¿ytkownika";
+$lu_title_rate								=	"Oceñ ten Odnonik";
+$lu_title_registration						=	"Rejestracja";
+$lu_title_reviews							=	"Recenzje";
+$lu_title_search							=	"Wyszukiwanie";
+$lu_title_search_advanced					=	"Wyszukiwanie Zaawansowane";
+$lu_title_subscribe							=	"Zapis do Biuletynu";
+$lu_title_suggest							=	"Zasugeruj Kategoriê";
+$lu_title_top_rated							=	"Najwy¿ej Ocenione Odnoniki";
+
+
+#########################################################################################
+#Link and Category Markers (Can replace them with graphics)
+
+$lu_cat_new									=	"nowoæ";
+$lu_cat_pick								=	"polecane";
+$lu_link_new								=	"nowoæ";
+$lu_link_pick								=	"polecane";
+$lu_link_pop								=	"popularne";
+$lu_link_top								=	"najlepsze";
+
+
+#########################################################################################
+#System Messages
+
+$lu_no_categories							=	"Nie ma Kategorii";
+$lu_no_links								=	"Nie ma Odnoników";
+
+
+#########################################################################################
+#Link/Category Details 
+
+$lu_added									=	"Dodane";
+$lu_cat										=	"Kategoria";
+$lu_date_created							=	"Data Utworzenia";
+$lu_desc									=	"Opis";
+$lu_edit									=	"Zmodyfikuj";
+$lu_editor_pick								=	"Wybór Redakcji";
+$lu_from									=	"Od";
+$lu_general									=	"Generalny";
+$lu_image									=	"Obrazek";
+$lu_hits									=	"Iloæ Klikniêæ";
+$lu_link									=	"Odnonik";
+$lu_name									=	"Nazwa";
+$lu_no										=	"Nie";
+$lu_rateit									=	"Oceñ";
+$lu_rating									=	"Ocena";
+$lu_reviews									=	"Recenzje";
+$lu_to										=	"Do";
+$lu_votes									=	"G³osy";
+$lu_url										=	"URL";
+$lu_yes										=	"Tak";
+
+
+#########################################################################################
+#Search 
+
+$lu_boolean_type_search						=	"Typ £¹czeniowy";
+$lu_return_number_results					=	"Liczba Wyników Wyszukiwania";
+
+
+#########################################################################################
+#Errors
+
+$lu_already_voted							=	"Nasz system wskazuje, ¿e g³os by³ ju¿ uprzednio oddany przez Ciebie. G³osowanie dozwolone jest raz w miesi³cu!";
+$lu_already_voted							=	"Nie posiadasz uprawnieñ do g³osowania na dan¹ pozycjê!";
+$lu_email_used								=	"Adres email jest ju¿ w u¿yciu.";
+$lu_error_404								=	"Nie mogê otworzyæ pliku.";
+$lu_error_addlink_not_allowed				=	"Dodawanie odnoników jest niedozwolone";
+$lu_error_addreview_not_allowed				=	"Dodawanie recenzji jest niedozwolone";
+$lu_error_addto_cat_not_allowed				=	"Ta kategoria nie jest wystarczaj¹co specyficzna! Prosimy wybraæ inn¹ kategoriê.";
+$lu_error_cannot_remove_cat					=	"Nie mogê usun¹æ ostaniej kategorii!";
+$lu_error_db								=	"B³¹d w Bazie Danych";
+$lu_error_file_not_found					=	"Plik jest dostêpny:";
+$lu_error_pass_match						=	"Wpisane has³a nie zgadzaj¹ siê.";
+$lu_error_pass_wrong						=	"Wpisane has³o jest niew³aciwe.";
+$lu_error_review_not_filled					=	"Recenzja nie mo¿e pozostaæ pusta";
+$lu_error_user_not_allowed					=	"Nie jeste uprawniony do rejestracji!";	
+$lu_error_wrong_user						=	"Odnonik nale¿y do innego w³aciciela!";
+$lu_invalid_entry							=	"B³êdny Wpis";
+$lu_login_incorrect							=	"Logowanie jest niew³aciwe";
+$lu_pass_not_match							=	"Has³a nie zgadzaj¹ siê";
+$lu_username_used							=	"Nazwa u¿ytkownika jest ju¿ w u¿yciu";
+
+#########################################################################################
+#Confirmations
+
+$lu_confirm_registration					=	"Twoje zg³oszenie rejestracyjne zosta³o wys³ane.";
+$lu_confirm_suggest_site					=	"Dziêkujemy za rekomendacjê tej witryny przyjacielowi.";
+$lu_confirm_profile							=	"Twoje dane osobowe zosta³y zaktualizowane.";
+$lu_confirm_linkupdate						=	"Twój odnonik zosta³ zaktualizowany.";
+$lu_confirm_addlink							=	"Twój odnonik zosta³ przyjêty.";
+$lu_confirm_suggest							=	"Dziêkujemy za zaproponowanie nowej kategorii.";
+$lu_confirm_addreview						=	"Twoja recenzja zosta³a przyjêta.";
+$lu_confirm_subscribe						=	"Zosta³e(a) zapisany(a) do naszego biuletynu wysy³kowego.";
+$lu_confirm_unsubscribe						=	"Zosta³e(a) usuniêty(a) z naszego biuletynu wysy³kowego.";
+
+#########################################################################################
+#Buttons
+
+$lu_button_addcat							=	"Dodaj Kategoriê";
+$lu_button_addlink							=	"Dodaj Odnonik";
+$lu_button_addreview						=	"Napisz Recenzjê";
+$lu_button_back								=	"Wróæ";
+$lu_button_cancel							=	"Anuluj";
+$lu_button_editlink							=	"Edytuj Odnonik";
+$lu_button_go								=	"Potwierd";
+$lu_button_login							=	"Zaloguj siê";
+$lu_button_ok								=	"OK";
+$lu_button_rate								=	"Oceñ";
+$lu_button_register							=	"Zarejestruj siê";
+$lu_button_reset							=	"Skasuj";
+$lu_button_search							=	"Szukaj";
+$lu_button_search_cats						=	"Przeszukaj Kategorie";
+$lu_button_search_links						=	"Przeszukaj Odnoniki";
+$lu_button_send								=	"Wylij";
+$lu_button_subscribe						=	"Zapisz siê";
+$lu_button_unsubscribe						=	"Zrezygnuj";
+$lu_button_update							=	"Zaktualizuj";
+$lu_mailing_list							=	"Biuletyn Wysy³kowy";
+$lu_not_mailing_list						=	"Brak w Biuletynie";
+
+
+#########################################################################################
+#Statistics
+
+$lu_cats									=	"Kategorie:";
+$lu_links									=	"Odnoniki:";
+$lu_total_cats								=	"Suma kategorii w bazie danych:  ";
+$lu_total_hits								=	"Iloæ Klikniêæ: ";
+$lu_total_links								=	"Suma odnoników w bazie danych: ";
+
+
+#########################################################################################
+#Addlink template
+
+$lu_add_cat									=	"Dodaj Kategoriê";
+$lu_additional_cats							=	"Dodatkowe Kategorie:";
+$lu_remove									=	"Usuñ";
+$lu_select									=	"Zaznacz";
+$lu_submitting_to							=	"Przy³¹czasz do:";
+
+
+#########################################################################################
+#Register template
+
+$lu_first_name								=	"Imiê";
+$lu_last_name								=	"Nazwisko";
+$lu_re_password								=	"Powtórz has³o";
+$lu_user_name								=	"Indentyfikator";
+
+
+#########################################################################################
+#Miscellaneos Pages
+
+$lu_advanced_search							=	"Wyszukiwanie Zaawansowane";
+$lu_atleast_3								=	"(minimum 3)";
+$lu_email									=	"E-mail";
+$lu_hint_suggest							=	"Zarekomenduj tê witrynê przyjacielowi";
+$lu_keyword									=	"S³owo";
+$lu_login									=	"Logowanie";
+$lu_not_logged_in							=	"Wylogowany";
+$lu_password								=	"Has³o";
+$lu_register								=	"Zarejestruj siê";
+$lu_review									=	"Napisz Recenzjê";
+$lu_search									=	"Szukaj";
+$lu_search_for								=	"Szukaj";
+$lu_show									=	"Poka¿";
+$lu_sort_by									=	"Sortuj przez";
+$lu_status									=	"Stan";
+$lu_suggest									=	"Zasugeruj";
+$lu_user									=	"U¿ytkownik";
+$lu_user_menu								=	"Menu U¿ytkownika";
+$lu_welcome									=	"Witaj";
+
+
+#########################################################################################
+#Dropdowns
+
+$lu_drop_and								=	"i";
+$lu_drop_as_many_as_found					=	"wszystkie";
+$lu_drop_ascending							=	"Wzrastaj¹co";
+$lu_drop_date								=	"Data";
+$lu_drop_desc								=	"Opis";
+$lu_drop_descending							=	"Opadaj¹co";
+$lu_drop_editors_pick						=	"Wybór Redakcji";
+$lu_drop_hits								=	"Klikniêcia";
+$lu_drop_image								=	"Obrazek";
+$lu_drop_name								=	"Nazwa";
+$lu_drop_numlinks							=	"Liczba odnoników";
+$lu_drop_numsubs							=	"Liczba podrzêdników";
+$lu_drop_or									=	"Lub";
+$lu_drop_perm								=	"Przywileje";
+$lu_drop_rating								=	"Ocena";
+$lu_drop_url								=	"URL";
+$lu_drop_user								=	"U¿ytkownik";
+$lu_drop_vis								=	"Wizualnoæ";
+$lu_drop_votes								=	"G³osy";
+
+
+#########################################################################################
+#Custom Fields
+
+$lu_categories_custom_fields1			    =	"Dowolne Pole #1";
+$lu_categories_custom_fields2			    =	"Dowolne Pole #2";
+$lu_categories_custom_fields3			    =	"Dowolne Pole #3";
+$lu_categories_custom_fields4			    =	"Dowolne Pole #4";
+$lu_categories_custom_fields5			    =	"Dowolne Pole #5";
+$lu_categories_custom_fields6			    =	"Dowolne Pole #6";
+$lu_links_custom_fields1					=	"Dowolne Pole #1";
+$lu_links_custom_fields2					=	"Dowolne Pole #2";
+$lu_links_custom_fields3					=	"Dowolne Pole #3";
+$lu_links_custom_fields4					=	"Dowolne Pole #4";
+$lu_links_custom_fields5					=	"Dowolne Pole #5";
+$lu_links_custom_fields6					=	"Dowolne Pole #6";
+$lu_users_custom_fields1					=	"Dowolne Pole #1";
+$lu_users_custom_fields2					=	"Dowolne Pole #2";
+$lu_users_custom_fields3					=	"Dowolne Pole #3";
+$lu_users_custom_fields4					=	"Dowolne Pole #4";
+$lu_users_custom_fields5					=	"Dowolne Pole #5";
+$lu_users_custom_fields6					=	"Dowolne Pole #6";
+
+
+#########################################################################################
+#Ratings
+
+$lu_excellent								=	"Rewelacja!";
+$lu_fair									=	"Doskonale";
+$lu_good									=	"Dobrze";
+$lu_poor									=	"S³abo";
+$lu_verygood								=	"Bardzo Dobrze";
+
+
+#########################################################################################
+#Text segments
+
+$lu_txt_gettingrated	=	"Wklej poni¿szy kod HTML w Twoj¹ stronê, aby umo¿liwiæ u¿ytkownikom g³osowanie na dan¹ witrynê:";
+
+#########################################################################################
+#New for version 2.0.7
+$lu_confirm_approval						=	"  Zostanie uruchomione po zatwierdzeniu.";
+
+#New for version 2.0.9
+
+$lu_report_dead						=	"Zg³o niedzia³aj¹cy odnonik";
+$lu_confirm_report_dead						=	"Odnonik zosta³ zg³oszony jako niedzia³aj¹cy. Dziêkujemy!";
+$lu_error_login_to_add						=	"Musisz byæ zalogowany, aby dodaæ odnonik!";
+
+#New for version 2.0.10
+$lu_error_addto_cat_added					=	"Ta kategoria zosta³a ju¿ uprzednio dodana";
+?>
