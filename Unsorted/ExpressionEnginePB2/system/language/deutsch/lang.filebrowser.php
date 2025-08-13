17bc5a3b7b8be80fa1056d968eb7a3e3f19303a9
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:06 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.filebrowser.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.filebrowser.php
@@ -0,0 +1,43 @@
+<?php
+
+$L = array(
+
+"file_browser" =>
+"Dateibrowser",
+
+"view" =>
+"Ansehen",
+
+"path_does_not_exist" =>
+"Der angegebene Pfad existiert nicht.",
+
+"file_viewing_error" =>
+"Ein unbekannter Fehler ist aufgetreten.",
+
+"fp_no_files" =>
+"Keine Dateien in diesem Verzeichnis verf&uuml;gbar.",
+
+"fb_view_images" =>
+"Grafiken ansehen",
+
+"fb_view_image" =>
+"Grafik ansehen",
+
+"fb_insert_file" =>
+"Datei einf&uuml;gen",
+
+"fb_insert_files" =>
+"Dateien einf&uuml;gen",
+
+"fb_select_field" =>
+"Feld ausw&auml;hlen",
+
+"fb_select_files" =>
+"Dateien ausw&auml;hlen",
+
+"fb_non_images" =>
+"* Bezeichnet Nicht-Grafiken. Nur Grafiken k&ouml;nnen angesehen werden.",
+
+
+// IGNORE
+''=>'');?>
