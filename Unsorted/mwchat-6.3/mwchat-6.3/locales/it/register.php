ab31c9c59317b0e0132be7a890d347f9f31059c1
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:12 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/it/register.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/it/register.php
@@ -0,0 +1,25 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[REGISTER_Title] = "Iscriversi il suo nome utente!";
+
+$LOCALE[REGISTER_Info] = "Per favore di completare le informazioni seguenti per iscriversi il nome utente.";
+
+$LOCALE[REGISTER_Is_Registered] = "È adesso iscritto.";
+
+$LOCALE[REGISTER_Complete] = "Le congratulazioni! Lei è iscritto con successo questo nome utente.";
+
+$LOCALE[REGISTER_Next] = "Lei può aggiustare adesso il suo ciarla le preferenze dalla sezione di maglie sulla finestra principale.";
+
+$LOCALE[REGISTER_Button] = "Iscriversi Adesso";
+
+?>
