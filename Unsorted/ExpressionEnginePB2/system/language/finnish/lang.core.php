524f8a929bf7517dabfc35ff441ac336967abbc5
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:52 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.core.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.core.php
@@ -0,0 +1,364 @@
+<?php
+$L = array(
+
+
+"on" =>
+"on",
+
+"off" =>
+"off",
+
+"first" =>
+"Ensimmäinen",
+
+"last" =>
+"Viimeinen",
+
+"enabled" =>
+"Sallittu",
+
+"disabled" =>
+"Estetty",
+
+"back" =>
+"Takaisin",
+
+"submit" =>
+"Lähetä",
+
+"update" =>
+"Päivitä",
+
+"thank_you" =>
+"Kiitos!",
+
+"system_off_msg" =>
+"Tämä sivusto ei toimi tällä hetkellä.",
+
+"not_authorized" =>
+"Sinulla ei ole oikeuksia suoritta tätä toimintaa",
+
+"auto_redirection" =>
+"Sinut uudellen ohjataan automaattisesti %x sekunnin kuluttua",
+
+"error" =>
+"Virhe",
+
+"invalid_url" =>
+"URL jonka annoit ei ole toimiva.",
+
+"submission_error" =>
+"Lomake jonka lähetit sisälsi seuraavat virheet:",
+
+"general_error" =>
+"Seuraavat virheet havaittiin:",
+
+"invalid_action" =>
+"Toiminto jota pyysit on virheellinen.",
+
+"banned" =>
+"Kielletty",
+
+"guests" =>
+"Vieras",
+
+"members" =>
+"Jäsenet",
+
+"pending" =>
+"Vireillä",
+
+"super_admins" =>
+"Pääkäyttäjät",
+
+"error_tag_syntax" =>
+"Seuraavassa tagissa oli syntaksi virhe:",
+
+"error_fix_syntax" =>
+"Korjaa virhe sivupohjaasi",
+
+"forgotten_email_sent" =>
+"Ohjeet salasanan resetointiin lähetettiin juuri sinulle",
+
+"error_sending_email" =>
+"Viestiä ei pystytty lähettämään tällä kertaa.",
+
+"no_email_found" =>
+"Antamaasi email osoitetta ei löytynyt tietokannasta",
+
+"your_new_login_info" =>
+"Kirjautumisohjeet",
+
+"reset_link" =>
+"Salasanan resetointi, mene seuraavalle sivulle:",
+
+"password_will_be_reset" =>
+"Salasanasi resetoidaan automaattisesti ja uusi salasana lähetetään sinulle.",
+
+"ignore_password_message" =>
+"Jos et halua resetoida salasanaasi, unohda tämä viesti. Se vanheneen 24 tunnissa.",
+
+"password_has_been_reset" =>
+"Salasanasi on resetoitu ja uusi salasana on lähetetty sinulle",
+
+"new_login_info" =>
+"Tässä on sinun uusi kirjautumisohjeesi:",
+
+"year" =>
+"year",
+
+"years" =>
+"years",
+
+"month" =>
+"month",
+
+"months" =>
+"months",
+
+"week" =>
+"week",
+
+"weeks" =>
+"weeks",
+
+"day" =>
+"day",
+
+"days" =>
+"Päivät",
+
+"hour" =>
+"hour",
+
+"hours" =>
+"Tunnit",
+
+"minute" =>
+"minute",
+
+"minutes" =>
+"Minuutit",
+
+"second" =>
+"second",
+
+"seconds" =>
+"Sekunnit",
+
+"am" =>
+"am",
+
+"pm" =>
+"pm",
+
+"AM" =>
+"AM",
+
+"PM" =>
+"PM",
+
+"Sun" =>
+"Su",
+
+"Mon" =>
+"ma",
+
+"Tue" =>
+"Ti",
+
+"Wed" =>
+"Ke",
+
+"Thu" =>
+"To",
+
+"Fri" =>
+"Pe",
+
+"Sat" =>
+"La",
+
+"Sunday" =>
+"Sunnuntai",
+
+"Monday" =>
+"Maanatai",
+
+"Tuesday" =>
+"Tiistai",
+
+"Wednesday" =>
+"keskiviikko",
+
+"Thursday" =>
+"Torstai",
+
+"Friday" =>
+"Perjantai",
+
+"Saturday" =>
+"Lauantai",
+
+"Jan" =>
+"Tammi",
+
+"Feb" =>
+"Helmi",
+
+"Mar" =>
+"Maalis",
+
+"Apr" =>
+"Huhti",
+
+"May" =>
+"Touko",
+
+"Jun" =>
+"Kesä",
+
+"Jul" =>
+"Heinä",
+
+"Aug" =>
+"Elo",
+
+"Sep" =>
+"Syys",
+
+"Oct" =>
+"Loka",
+
+"Nov" =>
+"Marras",
+
+"Dec" =>
+"Joulu",
+
+"January" =>
+"Tammikuu",
+
+"February" =>
+"Helmikuu",
+
+"March" =>
+"Maaliskuu",
+
+"April" =>
+"Huhtikuu",
+
+"June" =>
+"Kesäkuu",
+
+"July" =>
+"Heinäkuu",
+
+"August" =>
+"Elokuu",
+
+"September" =>
+"Syyskuu",
+
+"October" =>
+"Lokakuu",
+
+"November" =>
+"Marraskuu",
+
+"December" =>
+"Joulukuu",
+
+"UM12" =>
+"(UTC - 12:00) Enitwetok, Kwajalien",
+
+"UM11" =>
+"(UTC - 11:00) Nome, Midway Island, Samoa",
+
+"UM10" =>
+"(UTC - 10:00) Hawaii",
+
+"UM9" =>
+"(UTC - 9:00) Alaska",
+
+"UM8" =>
+"(UTC - 8:00) Pacific Time",
+
+"UM7" =>
+"(UTC - 7:00) Mountain Time",
+
+"UM6" =>
+"(UTC - 6:00) Central Time, Mexico City",
+
+"UM5" =>
+"(UTC - 5:00) Eastern Time, Bogota, Lima, Quito",
+
+"UM4" =>
+"(UTC - 4:00) Atlantic Time, Caracas, La Paz",
+
+"UM25" =>
+"(UTC - 3:30) Newfoundland",
+
+"UM3" =>
+"(UTC - 3:00) Brazil, Buenos Aires, Georgetown, Falkland Is.",
+
+"UM2" =>
+"(UTC - 2:00) Mid-Atlantic, Ascention Is., St Helena",
+
+"UM1" =>
+"(UTC - 1:00) Azores, Cape Verde Islands",
+
+"UTC" =>
+"(UTC) Casablanca, Dublin, Edinburgh, London, Lisbon, Monrovia",
+
+"UP1" =>
+"(UTC + 1:00) Berlin, Brussels, Copenhagen, Madrid, Paris, Rome",
+
+"UP2" =>
+"(UTC + 2:00) Helsinki, Kaliningrad, South Africa, Warsaw",
+
+"UP3" =>
+"(UTC + 3:00) Baghdad, Riyadh, Moscow, Nairobi",
+
+"UP25" =>
+"(UTC + 3:30) Tehran",
+
+"UP4" =>
+"(UTC + 4:00) Adu Dhabi, Baku, Muscat, Tbilisi",
+
+"UP35" =>
+"(UTC + 4:30) Kabul",
+
+"UP5" =>
+"(UTC + 5:00) Islamabad, Karachi, Tashkent",
+
+"UP45" =>
+"(UTC + 5:30) Bombay, Calcutta, Madras, New Delhi",
+
+"UP6" =>
+"(UTC + 6:00) Almaty, Colomba, Dhakra",
+
+"UP7" =>
+"(UTC + 7:00) Bangkok, Hanoi, Jakarta",
+
+"UP8" =>
+"(UTC + 8:00) Beijing, Hong Kong, Perth, Singapore, Taipei",
+
+"UP9" =>
+"(UTC + 9:00) Osaka, Sapporo, Seoul, Tokyo, Yakutsk",
+
+"UP85" =>
+"(UTC + 9:30) Adelaide, Darwin",
+
+"UP10" =>
+"(UTC + 10:00) Melbourne, Papua New Guinea, Sydney, Vladivostok",
+
+"UP11" =>
+"(UTC + 11:00) Magadan, New Caledonia, Solomon Islands",
+
+"UP12" =>
+"(UTC + 12:00) Auckland, Wellington, Fiji, Marshall Island",
+
+''=>''
+);
+?>
