6495b3d4c83dd2de75986a15dfd6135b35adef43
me-suzy <me.suzana@gmail.com>
2025-08-13 05:46:22 +0300

    Add Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/french/language.php

---
+++ Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/french/language.php
@@ -0,0 +1,334 @@
+<?php
+## In-link User Language File - French/Français
+## Author: Cedric Fontaine for Intechnic Corporation
+## Date: 04/19/2002
+##
+## This file is designed for the following version of In-link: 2.2.8
+## You can modify the text in this file to suit your language needs. 
+## Make sure to escape quotation marks with \" . 
+
+#########################################################################################
+#Language variables
+
+$lu_char_set								=   "ISO-8859-1";
+$lu_this_language							=	"French";	
+
+#########################################################################################
+#Navigation Menu
+
+$lu_getting_rated							=	"Etre évalué";
+$lu_go_to_page								=	"Aller à la page";
+$lu_search_results							=	"Résultats de la recherche";
+$lu_nav_addlink								=	"Ajouter un Lien";
+$lu_nav_editorspick							=	"Sélection de l'Editeur";
+$lu_nav_gettingrated							=	"Etre évalué";
+$lu_nav_home								=	"Accueil";
+$lu_nav_login								=	"Identification";
+$lu_nav_logout								=	"Déconnexion";
+$lu_nav_maillist							=	"S'abonner";
+$lu_nav_modlink								=	"Modifier un Lien";
+$lu_nav_new								=	"Nouveaux Liens";
+$lu_nav_pop								=	"Les Plus Populaires";
+$lu_nav_profile								=	"Profil";
+$lu_nav_register							=	"S'enregistrer";
+$lu_nav_search								=	"Recherche";
+$lu_nav_search_advanced							=	"Recherche avancée";
+$lu_nav_suggestcat							=	"Suggérer une Catégorie";
+$lu_nav_top								=	"Les mieux notés";
+$lu_navbar_seperator							=	"|";
+
+
+#########################################################################################
+#Page Titles
+
+$lu_title_add_link							=	"Ajouter un Lien";
+$lu_title_add_review							=	"Ajouter un avis";
+$lu_title_confirm							=	"Confirmation";
+$lu_title_edit_link							=	"Modifier un Lien";
+$lu_title_editors_picks							=	"Sélection de l'Editeur";
+$lu_title_error								=	"Erreur";
+$lu_title_gettingrated							=	"Etre évalué";
+$lu_title_login								=	"Identification";
+$lu_title_modify							=	"Modifier vos Liens";
+$lu_title_most_popular							=	"Liens les Plus Populaires";
+$lu_title_msg								=	"Message Système";
+$lu_title_new_links							=	"Nouveaux Liens";
+$lu_title_profile							=	"Profil Membre";
+$lu_title_rate								=	"Noter ce lien";
+$lu_title_registration							=	"Enregistrement";
+$lu_title_reviews							=	"Avis";
+$lu_title_search							=	"Recherche";
+$lu_title_search_advanced						=	"Recherche avancée";
+$lu_title_subscribe							=	"S'abonner à une mailing-list";
+$lu_title_suggest							=	"Suggérer une Catégorie";
+$lu_title_top_rated							=	"Liens les mieux notés";
+
+
+#########################################################################################
+#Link and Category Markers (Can replace them with graphics)
+
+$lu_cat_new								=	"nouveau";
+$lu_cat_pick								=	"sélection";
+$lu_link_new								=	"nouveau";
+$lu_link_pick								=	"sélection";
+$lu_link_pop								=	"pop";
+$lu_link_top								=	"top";
+
+
+#########################################################################################
+#System Messages
+
+$lu_no_categories							=	"Pas de Catégories";
+$lu_no_links								=	"Pas de Liens";
+
+
+#########################################################################################
+#Link/Category Details 
+
+$lu_added								=	"Ajouté";
+$lu_cat									=	"Catégorie";
+$lu_date_created							=	"Date de création";
+$lu_desc								=	"Description";
+$lu_edit								=	"Modifier";
+$lu_editor_pick								=	"Sélection de l'Editeur";
+$lu_from								=	"De";
+$lu_general								=	"Général";
+$lu_image								=	"Image";
+$lu_hits								=	"Hits";
+$lu_link								=	"Lien";
+$lu_name								=	"Nom";
+$lu_no									=	"Non";
+$lu_rateit								=	"Evaluez le";
+$lu_rating								=	"Evaluation";
+$lu_reviews								=	"Avis";
+$lu_to									=	"A";
+$lu_votes								=	"Votes";
+$lu_url									=	"URL";
+$lu_yes									=	"Oui";
+
+
+#########################################################################################
+#Search 
+
+$lu_boolean_type_search						=	"Opérateur Booléen";
+$lu_return_number_results					=	"Nombre de Résultats Retourné";
+
+
+#########################################################################################
+#Errors
+
+$lu_already_voted							=	"Nos enregistrements montrent que vous avez déjà voté. Vous ne pouvez votre plus d'une fois par mois !";
+$lu_already_voted							=	"Vous n'avez pas le droit de voter pour cette ressource!";
+$lu_email_used								=	"L'email est utilisé";
+$lu_error_404								=	"Impossible d'ouvrir le fichier.";
+$lu_error_addlink_not_allowed						=	"Ajouter des liens n'est pas autorisé";
+$lu_error_addreview_not_allowed						=	"Ajouter des avis n'est pas autorisé";
+$lu_error_addto_cat_not_allowed						=	"Cette catégorie n'est pas assez spécifique ! Merci de choisir une autre catégorie.";
+$lu_error_cannot_remove_cat						=	"Impossible d'enlever la dernière catégorie !";
+$lu_error_db								=	"Erreur dans la Base de Données !";
+$lu_error_file_not_found						=	"Le fichier est disponible :";
+$lu_error_pass_match							=	"Les mots de passe que vous avez saisis ne correspondent pas.";
+$lu_error_pass_wrong							=	"Le mot de passe saisi est incorrect.";
+$lu_error_review_not_filled						=	"Un avis ne peut être vide";
+$lu_error_user_not_allowed						=	"Vous n'êtes pas autorisé à vous enregistrer !";	
+$lu_error_wrong_user							=	"Le lien appartient à un autre utilisateur !";
+$lu_invalid_entry							=	"Saisie invalide";
+$lu_login_incorrect							=	"Identification incorrecte";
+$lu_pass_not_match							=	"Les mots de passe ne correspondent pas";
+$lu_username_used							=	"Le nom d'utilisateur est déjà utilisé";
+
+#########################################################################################
+#Confirmations
+
+$lu_confirm_registration					=	"Votre inscription a été expédiée.";
+$lu_confirm_suggest_site					=	"Merci de suggérer ce site à un ami.";
+$lu_confirm_profile						=	"Votre profil a été mis à jour.";
+$lu_confirm_linkupdate						=	"Votre lien a été mis à jour.";
+$lu_confirm_addlink						=	"Votre lien a été proposé.";
+$lu_confirm_suggest						=	"Merci d'avoir suggérer une catégorie.";
+$lu_confirm_addreview						=	"Votre avis a été proposé.";
+$lu_confirm_subscribe						=	"Vous avez inscrit à la mailing-list.";
+$lu_confirm_unsubscribe						=	"Vous avez été désinscrit de la mailing-list.";
+
+#########################################################################################
+#Buttons
+
+$lu_button_addcat							=	"Ajouter une Catégorie";
+$lu_button_addlink							=	"Ajouter un Lien";
+$lu_button_addreview							=	"Ajouter un avis";
+$lu_button_back								=	"Retour";
+$lu_button_cancel							=	"Abandon";
+$lu_button_editlink							=	"Editer le Lien";
+$lu_button_go								=	"Aller";
+$lu_button_login							=	"Identification";
+$lu_button_ok								=	"Ok";
+$lu_button_rate								=	"Voter";
+$lu_button_register							=	"S'enregistrer";
+$lu_button_reset							=	"Annuler";
+$lu_button_search							=	"Recherche";
+$lu_button_search_cats							=	"Chercher une catégorie";
+$lu_button_search_links							=	"Chercher un lien";
+$lu_button_send								=	"Envoyer";
+$lu_button_subscribe							=	"S'inscrire";
+$lu_button_unsubscribe							=	"Se désinscrire";
+$lu_button_update							=	"Mettre à jour";
+$lu_mailing_list							=	"Mailing List";
+$lu_not_mailing_list							=	"Pas dans la Mailing List";
+
+
+#########################################################################################
+#Statistics
+
+$lu_cats								=	"Catégories:";
+$lu_links								=	"Liens:";
+$lu_total_cats								=	"Catégories dans la base:  ";
+$lu_total_hits								=	"Hits: ";
+$lu_total_links								=	"Liens dans la base: ";
+
+
+#########################################################################################
+#Addlink template
+
+$lu_add_cat								=	"Ajouter cette catégorie";
+$lu_additional_cats							=	"Catégories additionnelles:";
+$lu_remove								=	"Enlever";
+$lu_select								=	"Sélectionner";
+$lu_submitting_to							=	"Vous soumettez dans:";
+
+
+#########################################################################################
+#Register template
+
+$lu_first_name								=	"Prénom";
+$lu_last_name								=	"Nom";
+$lu_re_password								=	"Mot de passe une nouvelle fois";
+$lu_user_name								=	"Nom d'utilisateur";
+
+
+#########################################################################################
+#Miscellaneos Pages
+
+$lu_advanced_search							=	"Recherche avancée";
+$lu_atleast_3								=	"(minimum 3)";
+$lu_email								=	"E-mail";
+$lu_hint_suggest							=	"Suggérer ce site à un ami";
+$lu_keyword								=	"Mot-clé";
+$lu_login								=	"Identification";
+$lu_not_logged_in							=	"Non connecté";
+$lu_password								=	"Mot de passe";
+$lu_register								=	"S'enregistrer";
+$lu_review								=	"Donner un avis";
+$lu_search								=	"Recherche";
+$lu_search_for								=	"Rechercher";
+$lu_show								=	"Afficher";
+$lu_sort_by								=	"Trier par";
+$lu_status								=	"Statut";
+$lu_suggest								=	"Suggérer";
+$lu_user								=	"Utilisateur";
+$lu_user_menu								=	"Menu Utilisateur";
+$lu_welcome								=	"Bienvenue";
+
+
+#########################################################################################
+#Dropdowns
+
+$lu_drop_and							=	"Et";
+$lu_drop_as_many_as_found					=	"Tout";
+$lu_drop_ascending						=	"Croissant";
+$lu_drop_date							=	"Date";
+$lu_drop_desc							=	"Description";
+$lu_drop_descending						=	"Décroissant";
+$lu_drop_editors_pick						=	"Sélection de l'Editeur";
+$lu_drop_hits							=	"Hits";
+$lu_drop_image							=	"Image";
+$lu_drop_name							=	"Nom";
+$lu_drop_numlinks						=	"Nombre de liens";
+$lu_drop_numsubs						=	"Nombre de sous-catégories";
+$lu_drop_or							=	"Ou";
+$lu_drop_perm							=	"Permissions";
+$lu_drop_rating							=	"Notations";
+$lu_drop_url							=	"URL";
+$lu_drop_user							=	"Utilisateur";
+$lu_drop_vis							=	"Visibilité";
+$lu_drop_votes							=	"Votes";
+
+
+#########################################################################################
+#Custom Fields
+
+$lu_categories_custom_fields1			   		=	"Champ Personnalisable Nom #1";
+$lu_categories_custom_fields2					=	"Champ Personnalisable Nom #2";
+$lu_categories_custom_fields3			   		=	"Champ Personnalisable Nom #3";
+$lu_categories_custom_fields4			    		=	"Champ Personnalisable Nom #4";
+$lu_categories_custom_fields5			  		=	"Champ Personnalisable Nom #5";
+$lu_categories_custom_fields6			    		=	"Champ Personnalisable Nom #6";
+$lu_links_custom_fields1					=	"Champ Personnalisable Nom #1";
+$lu_links_custom_fields2					=	"Champ Personnalisable Nom #2";
+$lu_links_custom_fields3					=	"Champ Personnalisable Nom #3";
+$lu_links_custom_fields4					=	"Champ Personnalisable Nom #4";
+$lu_links_custom_fields5					=	"Champ Personnalisable Nom #5";
+$lu_links_custom_fields6					=	"Champ Personnalisable Nom #6";
+$lu_users_custom_fields1					=	"Champ Personnalisable Nom #1";
+$lu_users_custom_fields2					=	"Champ Personnalisable Nom #2";
+$lu_users_custom_fields3					=	"Champ Personnalisable Nom #3";
+$lu_users_custom_fields4					=	"Champ Personnalisable Nom #4";
+$lu_users_custom_fields5					=	"Champ Personnalisable Nom #5";
+$lu_users_custom_fields6					=	"Champ Personnalisable Nom #6";
+
+
+#########################################################################################
+#Ratings
+
+$lu_excellent								=	"Excellent!";
+$lu_fair								=	"Décevant";
+$lu_good								=	"Bien";
+$lu_poor								=	"Mauvais";
+$lu_verygood								=	"Très bien";
+
+
+#########################################################################################
+#Text segments
+
+$lu_txt_gettingrated	=	"Vous pouvez placer le code HTML suivant sur votre site pour permettre à vos visiteurs de voter pour cette ressource :";
+
+#########################################################################################
+#New for version 2.0.7
+$lu_confirm_approval						=	"Il devra actif après approbation.";
+
+#New for version 2.0.9
+
+$lu_report_dead							=	"Signaler un lien mort";
+$lu_confirm_report_dead						=	"Le lien vient d'être rapporté comme mort. Merci !";
+$lu_error_login_to_add						=	"Merci de vous enregistrer pour proposer un lien!";
+
+#New for version 2.0.10
+$lu_error_addto_cat_added					=	"Cette catégorie a déjà été ajoutée";
+
+#########################################################################################
+#New for version 2.2.5
+$lu_link_send_password						=	"Mot de passe oublié ?";
+$lu_title_send_password						=	"Envoyer le mot de passe";
+$lu_button_send_password					=	"Envoyer le mot de passe";
+$lu_send_password						=	"Entrez vos nom d'utilisateur et e-mail pour recevoir le nouveau mot de passe";
+$lu_confirm_send_password					=   	"Votre nouveau mot de passe vous a été envoyé à votre adresse mail";
+$lu_invalid_username_password					=   	"Votre nom d'utilisateur ou mot de passe n'a pas été retrouvé";
+$lu_send_password_disabled					=   	"Cette fonction est désactivé pour root et les administrateurs";
+$lu_conf_registration_email_confirmation			=	"Vos informations d'inscription ont été envoyées à votre adresse mail";
+#########################################################################################
+#New for version 2.2.6
+$lu_link_in_current						=	"Liens dans la catégorie actuelle";
+$lu_link_in_subcats						=	"Liens dans les sous-catégories";
+$lu_link_in_entire						=	"Liens dans le répertoire entier";
+$lu_error_for_simple_search					=	"Saisissez au moins 3 caractères pour une recherche";
+$lu_error_for_adv_search					=	"Saisissez au moins 1 mot-clé pour une recherche";
+#########################################################################################
+#New for version 2.2.8
+$lu_nav_favorites							=	"Favoris";
+$lu_add_to_fav								=	"Ajouter aux Favoris";
+$lu_related_cats							=	"Catégories liées";
+$lu_del_favorites							=	"Le lien a bien été retiré des Favoris.";
+$lu_add_favorites							=	"Le lien a bien été ajouté aux Favoris.";
+$lu_favorites_add_login							=	"Vous devez vous connecter avant d'ajouter des Favoris.";
+
+
+?>
