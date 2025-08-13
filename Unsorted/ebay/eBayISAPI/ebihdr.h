c6340e28c83704fc66aa179bfbed21224acf8a76
me-suzy <me.suzana@gmail.com>
2025-08-13 03:13:24 +0300

    Add Unsorted/ebay/eBayISAPI/ebihdr.h

---
+++ Unsorted/ebay/eBayISAPI/ebihdr.h
@@ -0,0 +1,30 @@
+/*      $Id: ebihdr.h,v 1.6 1999/04/17 20:22:14 wwen Exp $      */
+//
+// ebihdr.h -- eBayISAPI pre-compiled header include file
+#ifndef _EBIHDR_H_
+#define _EBIHDR_H_
+
+#include "stdafx.h"
+#include "eBayISAPI.h"
+
+#include "clsMarketPlace.h"
+#include "clsMarketPlaces.h"
+
+
+#include "clsEnvironment.h"
+#include "clsDatabase.h"
+#include "eBayDebug.h"
+#include "clsUserValidation.h"
+#include "clseBayCookie.h"
+
+#include "clsUtilities.h"
+#include "clsMail.h"
+#include "clsAnnouncements.h"
+#include "clsUserIdWidget.h"
+#include "clsCurrencyWidget.h"
+#include "clsPartners.h"
+#include "clsMailControl.h"
+
+#endif
+ 
+  
