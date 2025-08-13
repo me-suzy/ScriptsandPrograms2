71d6668f806ae405d8cc091269086de64e9e9ea9
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:46 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-pl-shrd-view.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-pl-shrd-view.php
@@ -0,0 +1,80 @@
+<?php
+
+##################################################
+
+# /pl-shrd-view.php
+
+##################################################
+
+define( "PLSHRD_NOPLID", "Veuillez spécifier une liste d\\'écoute à éditer." );
+
+define( "PLSHRD_SAVE_NOID", "Veuillez choisir une liste à sauvegarder dans votre sélection de listes communautaires préférées." );
+
+define( "PLSHRD_DELPL_NOID", "Veuillez choisir une liste à effacer de votre sélection de listes communautaires préférées." );
+define( "PLSHRD_DELPL_CONFIRM", "Êtes-vous sûr de vouloir effacer cette liste de votre sélection de listes communautaires préférées.?" );
+
+define( "PLSHRD_CHCK_HELP", "Cliquez pour sélectioner cette piste." );
+define( "PLSHRD_PLAY_HELP", "Cliquez pour débuter la lecture de cette piste." );
+define( "PLSHRD_DLOAD_HELP", "Cliquez pour télécharger cette piste." );
+define( "PLSHRD_INFO_HELP", "Cliquez pour accéder aux détails de cette piste." );
+define( "PLSHRD_AR_HELP", "Cliquez pour voir toutes les piste par le(s) même(s) Artiste(s)." );
+define( "PLSHRD_AL_HELP", "Cliquez pour voir toutes les piste sur cet Album." );
+define( "PLSHRD_GE_HELP", "Cliquez pour voir toutes les piste du même Genre." );
+
+define( "PLSHRD_HEADER_FAV", "LISTE COMMUNAUTAIRE PRÉFÉRÉE" );
+define( "PLSHRD_HEADER_PUB", "LISTE COMMUNAUTAIRE" );
+define( "PLSHRD_HEADER_BY", "Par" );
+
+define( "PLSHRD_BTN_DELETE", "Effacer" );
+define( "PLSHRD_BTN_CANCEL", "Annuler" );
+
+define( "PLSHRD_ROWCNT", "Piste(s) Enregistrée(s)" );
+define( "PLSHRD_ROWCNT_2", "pour" );
+
+define( "PLSHRD_QS_AR", "Artiste" );
+define( "PLSHRD_QS_TR", "Titre" );
+define( "PLSHRD_QS_AL", "Album" );
+define( "PLSHRD_QS_GE", "Genre" );
+define( "PLSHRD_QS_TI", "Durée" );
+define( "PLSHRD_QS_BTN", "Chercher" );
+
+define( "PLSHRD_TB_PLIST_BTN", "Éditer" );
+define( "PLSHRD_TB_ADDED", "Ajoutée" );
+define( "PLSHRD_TB_PLAY_ALL", "Lecture" );
+define( "PLSHRD_TB_PLAY_ALL_HELP", "Cliquez pour débuter la lecture de cette liste telles que sauvegardée par l'auteur." );
+define( "PLSHRD_TB_SAVE", "Sauvegarder" );
+define( "PLSHRD_TB_SAVE_HELP", "Cliquez pour suavegarder cette liste d'écoute dans sauvegarder dans votre sélection de listes communautaires préférées." );
+define( "PLSHRD_TB_SELECT", "Sélectionner" );
+define( "PLSHRD_TB_SELECT_HELP", "Cliquez pour sélectionner toutes les pistes dans cette écran" );
+define( "PLSHRD_TB_RESET", "Relâcher" );
+define( "PLSHRD_TB_RESET_HELP", "Cliquez pour relcher toutes les pistes dans cette écran" );
+define( "PLSHRD_TB_PLAY", "Lecture Sélective" );
+define( "PLSHRD_TB_PLAY_HELP", "Cliquez pour débuter la lecture des pistes sélectionnées." );
+define( "PLSHRD_TB_ADDTO", "Copier Vers" );
+define( "PLSHRD_TB_ADDTO_HELP", "Cliquez pour copier les pistes sélectionnées vers la liste d'écoute choisie ci-dessus." );
+
+define( "PLSHRD_COLS_OP", "Options" );
+define( "PLSHRD_COLS_SQ", "Séq." );
+define( "PLSHRD_COLS_SQ_HELP", "Cliquez pour classer cette liste par Numéro de séquence." );
+define( "PLSHRD_COLS_TR", "Titre" );
+define( "PLSHRD_COLS_TR_HELP", "Cliquez pour classer cette liste par Titre de Piste" );
+define( "PLSHRD_COLS_TI", "Durée" );
+define( "PLSHRD_COLS_TI_HELP", "Cliquez pour classer cette liste par Durée de Piste" );
+define( "PLSHRD_COLS_AR", "Artiste" );
+define( "PLSHRD_COLS_AR_HELP", "Cliquez pour classer cette liste par Artistes" );
+define( "PLSHRD_PLAY_AR_HELP", "Jouer Toutes Les Pistes Par Cet Artiste" );
+define( "PLSHRD_FILTER_ARTISTS_HELP", "Lister tous les albums figurant cet artiste." );
+define( "PLSHRD_COLS_AL", "Album" );
+define( "PLSHRD_COLS_AL_HELP", "Cliquez pour classer cette liste par Albums" );
+define( "PLSHRD_PLAY_AL_HELP", "Jouer Toutes Les Pistes Sur Cet Album" );
+define( "PLSHRD_FILTER_ALBUMS_HELP", "Lister tous les artistes figurant sur cet album." );
+define( "PLSHRD_COLS_TN", "#" );
+define( "PLSHRD_COLS_TN_HELP", "Cliquez pour classer cette liste par Numéro de Piste" );
+define( "PLSHRD_COLS_GE", "Genre" );
+define( "PLSHRD_COLS_GE_HELP", "Cliquez pour classer cette liste par Genre" );
+define( "PLSHRD_PLAY_GE_HELP", "Jouer Toutes Les Pistes De Ce Genre" );
+
+
+##################################################
+
+?>
