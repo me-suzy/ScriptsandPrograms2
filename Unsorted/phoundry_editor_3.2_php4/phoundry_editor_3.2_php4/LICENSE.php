d06bf6bc888a13104c1d034bbd09e0d29b83f5b3
me-suzy <me.suzana@gmail.com>
2025-08-13 07:43:58 +0300

    Add Unsorted/phoundry_editor_3.2_php4/phoundry_editor_3.2_php4/LICENSE.php

---
+++ Unsorted/phoundry_editor_3.2_php4/phoundry_editor_3.2_php4/LICENSE.php
@@ -0,0 +1,184 @@
+<?php
+/*
+---- Read this License Agreement, and if you agree, scroll down and ----
+---- enter your Phoundry Editor License Keys.                       ----
+
+End-User Software License Agreement
+
+IMPORTANT NOTICE - READ CAREFULLY: This is a legal agreement between you, the
+end user, (either an individual or an entity), and Web Power BV. ("Web Power")
+for the software product Phoundry Editor PHP4 Encrypted ("Software"). This 
+license ("License") contains rights and restrictions associated with use of the 
+accompanying Software including, but not limited to, one or more of the 
+following: source code, object code, classes, images and scripts together with 
+the accompanying documentation and examples. Read the License carefully before 
+opening the Software package, downloading, installing or using the Software. By 
+opening this package, downloading, installing or using the Software you agree 
+to be bound by the terms and conditions of this License. If you do not agree to
+the terms of this Agreement, do not download, install, copy or use the 
+Software. The Software is protected by copyright laws and international 
+copyright treaties, as well as other intellectual property laws and treaties. 
+The Software is licensed, not sold. 
+
+1. Limited License Grant. Subject to payment of any applicable license fees, 
+Web Power grants to you ("Licensee") a non-exclusive, non-transferable limited 
+license to use the Software. Except for the foregoing, Licensee may not re-
+distribute the Software in whole or in part, either separately or included with 
+a product. You may: install the Software on any single domain; copy the 
+Software for archival purposes, provided any copy must contain all of the 
+original Software's proprietary notices. All Additional copies of Software 
+must be purchased directly from Web Power. 
+
+2. Restrictions. The Software is confidential copyrighted information of Web 
+Power and title, ownership rights, and intellectual property rights in the 
+Software and to all copies is retained by Web Power. The Software is protected 
+by the copyright laws and treaties. Title and related rights in the content 
+accessed through the Software is the property of the applicable content owner 
+and may be protected by applicable law. This License gives you no rights to 
+such content. You may not: install the Software on more than a single domain; 
+translate, reverse engineer, decompile, decrypt, extract, disassemble, or 
+create derivative works based on the Software; copy the Software other than as 
+specified in Section 1 of this License; sell, rent, lease, grant a security 
+interest in, or otherwise transfer rights to the Software; or alter or remove 
+any proprietary notices or labels on the Software. Software may not be leased, 
+assigned, or sublicensed, in whole or in part, except as specifically 
+authorized in Section 1. Licensee warrants that it will not use or redistribute
+the Software for such purposes. 
+
+3. Trademarks and Logos. This License does not authorize Licensee to use any 
+Web Power name, trademark, or logo without prior written permission. Licensee 
+acknowledges that Web Power owns all related trademarks, logos and icons. 
+
+4. WARRANTY DISCLAIMER. THIS SOFTWARE AND ANY RELATED SERVICES OR DOCUMENTATION 
+IS PROVIDED ON AN "AS IS" BASIS, WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR 
+IMPLIED REPRESENTATIONS AND WARRANTIES, INCLUDING WITHOUT LIMITATION ANY 
+IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-
+INFRINGEMENT, ARE HEREBY EXCLUDED. THE ENTIRE RISK AS TO THE QUALITY AND 
+PERFORMANCE OF THE SOFTWARE IS BORNE BY YOU. SHOULD THE SOFTWARE PROVE 
+DEFECTIVE, YOU AND NOT Web Power ASSUME THE ENTIRE COST OF ANY SERVICE AND 
+REPAIR. THIS DISCLAIMER OF WARRANTY CONSTITUTES AN ESSENTIAL PART OF THE 
+AGREEMENT. IN SOME JURISDICTIONS THIS MAY NOT APPLY TO YOU AND YOU MAY HAVE 
+OTHER LEGAL RIGHTS THAT ARE BY JURISDICTION. 
+
+5. LIMITATION OF LIABILITY. TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, 
+IN NO EVENT SHALL WEB POWER OR ITS SUPPLIERS BE LIABLE FOR ANY DAMAGES SUFFERED 
+BY LICENSEE OR ANY THIRD PARTY AS A RESULT OF USING OR DISTRIBUTING SOFTWARE. 
+IN NO EVENT AND UNDER NO CIRCUMSTANCES WILL Web Power OR ITS SUPPLIERS BE 
+LIABLE FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, 
+CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES OR LOSS OF GOODWILL, WORK 
+STOPPAGE, COMPUTER FAILURE OR MALFUNCTION, OR ANY OTHER COMMERCIAL DAMAGES OR 
+LOSSES, HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, TORT, 
+CONTRACT, OR OTHERWISE, ARISING OUT OF THE USE OF, MISUSE OF OR INABILITY TO 
+USE SOFTWARE, EVEN IF Web Power HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGES. IN SOME JURISDICTIONS THIS MAY NOT APPLY TO YOU AND YOU MAY HAVE OTHER
+LEGAL RIGHTS THAT ARE BY JURISDICTION. 
+
+In no event will Web Power's liability to Licensee, whether in contract, tort 
+(including negligence), or otherwise exceed the total price paid for the 
+software to Web Power by Licensee for the twelve (12) months preceding the 
+event giving rise to the claim. No action, whether in contract or tort 
+including but not limited to negligence, arising out of or in connection with 
+this Agreement may be brought by either party more than eighteen (18) months 
+after the cause of action has accrued. 
+
+6. Termination. Licensee may terminate this License at any time by destroying 
+all copies of Software. This License will terminate immediately without notice 
+from Web Power if Licensee fails to comply with any provision of this License. 
+Upon such termination, Licensee must destroy all copies of Software. 
+
+7. Governing Law. Any action related to this License will be governed by Dutch 
+law. No choice of law rules of any jurisdiction will apply. 
+
+8. Severability. IF ANY OF THE ABOVE PROVISIONS ARE HELD TO BE IN VIOLATION OF 
+APPLICABLE LAW, VOID, OR UNENFORCEABLE IN ANY JURISDICTION, THEN SUCH 
+PROVISIONS ARE HEREWITH WAIVED OR AMENDED TO THE EXTENT NECESSARY FOR THE 
+LICENSE TO BE OTHERWISE ENFORCEABLE IN SUCH JURISDICTION. HOWEVER, IF IN WEB 
+POWER'S OPINION DELETION OR AMENDMENT OF ANY PROVISIONS OF THE LICENSE BY 
+OPERATION OF THIS PARAGRAPH UNREASONABLY COMPROMISES THE RIGHTS OR INCREASE THE
+LIABILITIES OF WEB POWER, WEB POWER AUTOMATICALLY TERMINATES THE LICENSE 
+WITHOUT ANY REMEDY TO THE LICENSEE. UPON SUCH TERMINATION, LICENSEE MUST 
+DESTROY ALL COPIES OF THE SOFTWARE. 
+
+9. Breach of Agreement. It is understood and agreed that, notwithstanding any 
+other provision of this Agreement, Licensee's breach of any of the provisions
+of this Agreement will cause Web Power irreparable damage for which recovery of 
+money damages would be inadequate, and that Web Power will therefore be 
+entitled to seek timely injunctive relief to protect Web Power's rights under 
+this Agreement in addition to any and all remedies available at law. 
+
+11. Miscellaneous. This Agreement is the parties' entire agreement relating to 
+its subject matter. It supersedes all prior or contemporaneous oral or written 
+communications, proposals, conditions, representations and warranties and 
+prevails over any conflicting or additional terms of any quote, order, 
+acknowledgment, or other communication between the parties relating to its 
+subject matter during the term of this Agreement. No modification to this 
+Agreement will be binding, unless in writing and signed by an authorized 
+representative of each party. 
+
+12. Should you have any questions concerning this Agreement, or if you desire 
+to contact Web Power for any reason, please write: 
+Web Power BV.
+Anthonie Fokkerstraat 5
+3772 MP Barneveld
+The Netherlands
+e-mail: info@webpower.nl. 
+
+End-User Software License Agreement ends here.
+*/
+
+	/**
+	 * Enter your Phoundry Editor license-key(s) here:
+	 * 
+	 * Example for a 1 domain license:
+	 *   $editorKey = array('my_license_key');
+	 *
+	 * Example for a 3 domain license:
+	 *   $editorKey = array('my_license_key_for_domain_1',
+	 *                      'my_license_key_for_domain_2',
+	 *                      'my_license_key_for_domain_3');
+	 *
+	 * If you don't have license key (yet) or you just want
+	 * to try the Phoundry Editor, define $editorKey like this:
+	 *   $editorKey = array();
+	 */
+	
+	$editorKey = array();
+
+	/**
+	 * Enter the php extension you use here.
+	 * Then rename all .php files in this directory, subdirectory
+	 * 'tidy' and subdirectory 'lang' to the
+	 * extension you need, *EXCEPT* common.php and LICENSE.php.
+	 * For more information, see http://editor.phoundry.com/docs/faq.php
+	 */
+
+	$php_ext = 'php';
+	 
+
+	/**
+	 * You can set the permissions of files and directories
+	 * created by the Phoundry Editor, when using the Upload feature
+	 * for images and Flash files.
+	 * Note: on a Windows Server, those settings have no effect.
+	 */
+
+	$editorFileMask = 0644;
+	$editorDirMask = 0755;
+
+
+	/**
+	 * Enter the ispell-command to call for spell-checking.
+	 * The -a and -S arguments are required.
+	 * If you wish to use a custom dictionary, pass the dictionary-file
+	 * with the -p argument. Make sure this file is writable for user
+	 * nobody!
+	 * Example for ispell:
+	 *   $editorSpeller = '/usr/bin/ispell -a -S -W 2 -p /usr/share/ispell.nobody';
+	 * Example for aspell:
+	 *   $editorSpeller = '/usr/local/bin/aspell -a -S -W 2 -p /usr/share/aspell.nobody';
+	 * (Warning: aspell is slow compared to ispell!)
+	 *   
+	 */
+
+	$editorSpeller = '/usr/bin/ispell -a -S -W 2 -p /usr/share/ispell.nobody';
+?>
