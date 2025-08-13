46af31149053204925aeca4355931ce55bebb302
me-suzy <me.suzana@gmail.com>
2025-08-13 07:36:51 +0300

    Add Unsorted/pager/language_files/english.php

---
+++ Unsorted/pager/language_files/english.php
@@ -0,0 +1,26 @@
+<?
+$l_send =			"Send the message to this ICQ number:";
+$l_emne =			"Subject:";
+$l_besked =			"Message:";
+$l_ditnavn =			"Your name:";
+$l_dinepost =			"Your email address:";
+$l_sendbeskeden =		"Send the message";
+$l_lavkoden =			"Generate the code";
+$l_redigerformularen =		"Edit the appearence of the ICQ pager form. If unsure, just press the \"$l_lavkoden\" button.";
+$l_tabelbaggrund =		"Background color:";
+$l_ramme =			"Border:";
+$l_celleafstand =		"Cellspacing:";
+$l_cellevaeg =			"Cellpadding:";
+$l_skrifttype =			"Font:";
+$l_skriftstr =			"Font size:";
+$l_skriftfarve =		"Font color:";
+$l_gennemse =			"Here's a preview of the form:";
+$l_kopier =			"Copy the code below and paste it into one or more of your HTML documents:";
+$l_icqikkeudfyldt =		"You forgot to fill in the ICQ number that the message should be sent to.";
+$l_ingenbesked =		"You forgot to write a message.";
+$l_tilbage =			"Back";
+$l_over450tegn =		"The message is too long. Maximum is 450 characters.";
+$l_epostikkeudfyldt =		"You forgot to fill in your email address.";
+$l_beskedenafsendt =		"The message has been sent.";
+$l_hjaelp =			"Help";
+?>
