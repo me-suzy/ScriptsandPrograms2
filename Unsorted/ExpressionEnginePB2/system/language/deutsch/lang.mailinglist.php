1c1fbb3cbfedc5d885c5e3f5e7aec9d6702469c7
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:12 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.mailinglist.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.mailinglist.php
@@ -0,0 +1,121 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"mailinglist_module_name" =>
+"Mailing-Liste",
+
+"mailinglist_module_description" =>
+"Einfacher Mailinglistenmanager",
+
+//----------------------------------------
+
+"ml_mailinglist" =>
+"Mailing-Liste",
+
+"ml_total_in_mailinglist" =>
+"Anzahl der E-Mail-Adressen:",
+
+"ml_view_mailinglist" =>
+"Mailingliste ansehen",
+
+"ml_email_search" =>
+"E-Mail-Suche",
+
+"ml_email_search_cont" =>
+"Geben Sie eine vollst&auml;ndige oder teilweise E-Mail-Adresse ein",
+
+"ml_email_address" =>
+"E-Mail",
+
+"ml_delete" =>
+"L&ouml;schen",
+
+"ml_no_results" =>
+"F&uuml;r Ihre Suchkriterien gibt es keine Ergebnisse.",
+
+"ml_delete_confirm" =>
+"E-Mail-Adressen l&ouml;schen",
+
+"ml_delete_question" =>
+"Soll(en) diese Adresse(n) wirklich gel&ouml;scht werden?",
+
+"ml_email_deleted" =>
+"Adresse wurde gel&ouml;scht.",
+
+"ml_emails_deleted" =>
+"Adressen wurden gel&ouml;scht.",
+
+"ml_missing_email" =>
+"Sie m&uuml;ssen eine E-Mail-Adresse eingeben.",
+
+"ml_invalid_email" =>
+"Ung&uuml;ltige E-Mail-Adresse",
+
+"ml_email_already_in_list" =>
+"Ihre E-Mail-Adresse befindet sich schon auf der Liste.",
+
+"ml_banned_email" =>
+"Diese E-Mail-Adresse ist nicht erlaubt.",
+
+"ml_email_accepted" =>
+"Ihre E-Mail-Adresse wurde in die Liste aufgenommen.",
+
+"ml_email_confirmation_sent" =>
+"Sie erhalten gleich eine E-Mailbest&auml;tigung.",
+
+"ml_click_confirmation_link" =>
+"Bitte klicken Sie den Link, den Sie in dieser E-Mail finden, um Ihren Account zu aktivieren.",
+
+"ml_mailinglist_confirmation" =>
+"E-Mailbestaetigung",
+
+"ml_thankyou_for_joining" =>
+"Danke f&uuml;r Ihr Interesse an unserer Mailing-Liste!",
+
+"ml_activation_instructions" =>
+"Bitte klicken Sie den untenstehenden Link, um Ihre Adresse zu verifizieren.",
+
+"ml_expiration_blurb" =>
+"Wenn Sie nicht in unsere Liste aufgenommen werden wollen, ignorieren Sie einfach diese Nachricht.",
+
+"ml_account_confirmed" =>
+"Ihre E-Mailadresse ist best&auml;tigt.",
+
+"ml_expired_date" =>
+"Der eingegebene Autorisierungscode ist abgelaufen.",
+
+"ml_unsubscribe" =>
+"Ihre E-Mail-Adresse wurde aus unserer Liste gel&ouml;scht.",
+
+"ml_unsubscribe_failed" =>
+"Ich kann die angegebene Accountnummer nicht finden.",
+
+"ml_add_email_addresses" =>
+"E-Mail-Adressen subskribieren",
+
+"ml_add_email_addresses_cont" =>
+"Trennen Sie die einzelnen E-Mailadressen durch Leerzeichen oder die RETURN-Taste.",
+
+"ml_emails_imported" =>
+"E-Mail-Subscription erfolgreich",
+
+"ml_total_emails_imported" =>
+"Anzahl der subscribierten E-Mails:",
+
+"ml_bad_email_heading" =>
+"Die folgenden Adressen konnten nicht importiert werden:",
+
+"ml_total_duplicate_emails" =>
+"Anzahl existierender E-Mails:",
+
+
+
+// END
+''=>''
+);
+?>
