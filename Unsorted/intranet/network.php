db17aee0e016ae0b585ae9d3bc8accf395890c1d
me-suzy <me.suzana@gmail.com>
2025-08-13 05:32:32 +0300

    Add Unsorted/intranet/network.php

---
+++ Unsorted/intranet/network.php
@@ -0,0 +1,19 @@
+<html>
+<?php
+include("config.php");
+include("identity.php");
+if ($refok == "yes")
+	{
+?>
+<head><meta http-equiv="refresh" content="1; url=system.php?hdigkeng=diwnbj9990"></head>
+<?php
+/*  E5DDDD */
+
+$appheaderstring='Network';
+include("header.php");
+
+echo "<center><table width='95%' border='0' cellpadding='0' cellspacing='0'><tr><td> Please wait while the server examines the network and internet.</td></tr>";
+?>
+</table></center></body></html>
+<?php
+	} ?>
