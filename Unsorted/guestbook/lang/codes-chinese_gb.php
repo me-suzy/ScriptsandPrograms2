afd6f4d6eb08a5215bb7197d62542c75f65eb54a
me-suzy <me.suzana@gmail.com>
2025-08-13 05:00:59 +0300

    Add Unsorted/guestbook/lang/codes-chinese_gb.php

---
+++ Unsorted/guestbook/lang/codes-chinese_gb.php
@@ -0,0 +1,120 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE>Smilies And AGCodes</TITLE>
+<META content="text/html; charset=gb2312" http-equiv=Content-Type>
+<style type="text/css">
+<!--
+td {  font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 9pt}
+-->
+</style>
+</HEAD>
+<BODY bgColor=#ffffff link=#000080 text=#000000 vLink=#000080>
+<CENTER>
+  <table width="95%" border="0" cellspacing="1" cellpadding="0">
+    <tr>
+      <td height="25">Ê²Ã´ÊÇÐ¦Á³£¿</td>
+    </tr>
+    <tr>
+      <td>
+        <p>Ð¦Á³¾ÍÊÇÒ»Ð©Ð¡µÄÁ³µ°Í¼Ïñ£¬¾ÍÏóInternetÉÏµÄ¸ÐÇé·ûºÅÒ»Ñù¡£ÔÚÎÒÃÇµÄÁôÑÔ±¾ÖÐ£¬Ö»ÒªÄãÊäÈëÒ»Ð©ÌØ¶¨µÄ¸ÐÇé·ûºÅ£¬»á±»×Ô¶¯×ª»»³ÉÎªÒ»Ð©Ð¦Á³Í¼ÏñÏÔÊ¾¡£
+        <p>ÕâÀïÊÇÁôÑÔ±¾ÖÐ¸ÐÇé·ûºÅºÍÐ¦Á³Í¼ÏñµÄ¶ÔÕÕ±í£º</p>
+      </td>
+    </tr>
+  </table>
+  <table bgcolor=#f7f7f7 border=0 width="95%" cellspacing="1" cellpadding="4">
+    <tbody>
+    <tr>
+      <td bgcolor="#996699"><font color=#ffffff><b>¸ÐÇé·ûºÅ</b></font></td>
+      <td bgcolor="#996699"><font color=#ffffff><b>¸ÐÇé</b></font></td>
+      <td bgcolor="#996699"><font color=#ffffff><b>±»×ª»»³ÉµÄÐ¦Á³Í¼Ïñ</b></font></td>
+    </tr>
+
+<?php include ("./smilies.inc"); ?>
+
+    </tbody>
+  </table>
+  <br>
+  <table width="95%" border="0" cellspacing="1" cellpadding="0">
+    <tr>
+      <td height="25">Ê²Ã´ÊÇ¸ß¼¶´úÂë£¿</td>
+    </tr>
+    <tr>
+      <td>
+        <p>¸ß¼¶´úÂëÈÃÄãºÜÇáËÉµØÔÚÁôÑÔÖÐ²åÈëÍøÕ¾Á´½Ó¡¢E-MailµØÖ·¡¢Í¼Æ¬¡£
+        <p>µ±Ç°¿ÉÒÔÊ¹ÓÃµÄ¸ß¼¶´úÂë£º
+      </td>
+    </tr>
+  </table>
+<TABLE border=0 cellPadding=0 cellSpacing=0 width="95%" align="center">
+  <TBODY>
+  <TR>
+    <TD bgColor=#000000>
+      <TABLE border=0 cellPadding=4 cellSpacing=1 width="100%">
+        <TBODY>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">ÍøÕ¾Á´½Ó</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>¼ÙÈç¹ÜÀíÔ±¿ª·ÅÁËÕâ¸öÁôÑÔ±¾ÖÐµÄ¸ß¼¶´úÂë¹¦ÄÜ£¬Äã¾Í¿ÉÒÔÊ¹ÓÃ[URL]´úÂëÔÚÁôÑÔÖÐ²åÈëÍøÕ¾Á´½Ó£º
+            <UL>
+              <LI><font color="#800000">http://www.yourURL.com </font>
+              <LI><FONT color=#800000>www.yourURL.com </FONT>Äã¿ÉÒÔ²»ÊäÈëhttp://£¬ÒòÎªÏµÍ³»á×Ô¶¯Ìí¼Ó<BR>
+              <LI>Ê¹ÓÃ¸ñÊ½¿´ÏÂÃæµÄÀý×Ó£º<br><br>
+                <CENTER>
+                  <FONT color=#ff0000>[url=http://www.946.com.cn]</FONT>RadioFoshan<FONT color=#ff0000>[/url]</FONT>
+                </CENTER><br><br>
+              <LI>
+                <P>ÏÂÃæµÄ¸ñÊ½Ò²¿ÉÒÔÓÐÐ§£º
+                <P>
+                  <CENTER>
+                    <FONT color=#ff0000>[url]</FONT>www.radiofoshan.com.cn<FONT color=#ff0000>[/url]</FONT>
+                  </CENTER>
+                <P>ÔÚÉÏÃæµÄÀý×ÓÖÐ£¬ÏµÍ³»á×Ô¶¯ÔÚÁ´½ÓÇ°¼ÓÈë"http://"</P>
+              </LI>
+            </UL>
+          </TD>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">EmailÁ´½Ó</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>Òª¼ÓÈëÒ»¸öEmailÁ´½Ó£¬¿´ÏÂÃæµÄÀý×Ó£º
+            <P>
+              <CENTER>
+                <FONT color=#ff0000>[email]</FONT>bd7nq@helloq.net<FONT color=#ff0000>[/email]</FONT>
+              </CENTER>
+          </TD>
+        </TR>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">´ÖÌåºÍÐ±Ìå</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>Ê¹ÓÃ[b] [/b] ºÍ [i] [/i] ¿ÉÊµÏÖ´ÖÌåºÍÐ±Ìå×Ö·ûµÄÏÔÊ¾£º
+            <P>
+              <CENTER>
+                Hello, <FONT color=#ff0000>[b]</FONT><B>John</B><FONT color=#ff0000>[/b]</FONT><BR><br>
+                Hello, <FONT color=#ff0000>[i]</FONT><I>Maria</I><font color="#FF0000">[i]</font>
+              </CENTER>
+          </TD>
+        </TR>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">¼ÓÈëÍ¼Æ¬</b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>ÔÚÁôÑÔÖÐ¼ÓÈëÍ¼Æ¬£¬Çë²Î¿´ÏÂÃæµÄ¸ñÊ½£¬<FONT color=#ff0000>ºìÉ«</FONT>µÄÄÚÈÝ¾ÍÊÇÄãÒªÏÔÊ¾µÄÍ¼Æ¬µÄÂ·¾¶£ºTo add a graphic within your message, just encase the URL of the
+            <P>
+              <CENTER>
+                <FONT color=#ff0000>[img]</FONT>http://www.yourURL.com/image/logo.gif<FONT color=#ff0000>[/img]</FONT>
+              </CENTER>
+          </TD>
+        </TR>
+        </TBODY>
+      </TABLE>
+    </TD>
+  </TR>
+  </TBODY>
+</TABLE>
+</CENTER>
+<BR>
+</BODY>
+</HTML>
