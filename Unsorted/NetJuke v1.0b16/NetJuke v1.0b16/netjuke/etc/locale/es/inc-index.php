419f19094fc17db24b2610532997b66515f3200e
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:08 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-index.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-index.php
@@ -0,0 +1,25 @@
+<?php
+
+##################################################
+
+# /index.php
+
+##################################################
+
+define( "BRWS_GE_HEADER", "TODOS LOS GENEROS" );
+define( "BRWS_GE_HELP", "Lista Total de Pistas de este Genero." );
+define( "BRWS_PLAY_GE_HELP", "Reproducir Todos las Pistas para este Genero." );
+
+define( "BRWS_AR_HEADER", "ARTISTAS MÁS RECIENTES." );
+define( "BRWS_AR_HELP", "Click para visualizar todas las pistas disponibles de este artista." );
+define( "BRWS_PLAY_AR_HELP", "Reproducir Todos las Pistas para este Artista." );
+define( "BRWS_FILTER_AR_HELP", "Click Para Listar Todos Los Album(S) Pertenecientes a Este Artista." );
+
+define( "BRWS_AL_HEADER", "ALBUNES MÁS RECIENTES." );
+define( "BRWS_AL_HELP", "Click para visualizar todas las pistas de este Album." );
+define( "BRWS_PLAY_AL_HELP", "Reproducir Todas las pistas de este Album." );
+define( "BRWS_FILTER_AL_HELP", "Cilck Para Listar Todos Los Artistas Incluidos En Este Album." );
+
+##################################################
+
+?>
