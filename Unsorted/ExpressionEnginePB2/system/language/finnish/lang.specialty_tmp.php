1c5c821ab9a78104c98fd3e802ee664695a59d2c
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:27 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.specialty_tmp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.specialty_tmp.php
@@ -0,0 +1,82 @@
+<?php
+$L = array(
+
+
+"email_title" =>
+"Emailin otsikko",
+
+"email_message" =>
+"Emalin viesti",
+
+"admin_notify_comment" =>
+"Kommentin ilmoitus järjestelmänvalvojalle",
+
+"admin_notify_comment_desc" =>
+"Tämä sivupohja määrittä viestin sisällön, joka lähetetään järjestelmänvalvojalle, kun kommentti lisätään sivulle.",
+
+"admin_notify_trackback" =>
+"Trackbackin ilmoitus järjestelmänvalvojalle",
+
+"admin_notify_trackback_desc" =>
+"Tämä sivupohja määrittä viestin sisällön, joka lähetetään järjestelmänvalvojalle, kun trackback vastaanotetaan.",
+
+"admin_notify_reg" =>
+"Uuden jäsenen rekisteröitymisen ilmoitus järjestelmänvalvojalle",
+
+"admin_notify_reg_desc" =>
+"Tämä sivupohja määrittä viestin sisällön, joka lähetetään järjestelmänvalvojalle, kun uusi jäsen rekisteröityy",
+
+"comment_notification" =>
+"Kommentin ilmoitus käyttäjälle",
+
+"comment_notification_desc" =>
+"Tämä sivupohja määrittää viestin sisällön, jonka käyttäjä saa, kun joku vastaa heidän kommenttiinsa",
+
+"forgot_password_instructions" =>
+"Unohtuneen salasanan ohjeet",
+
+"forgot_password_instructions_desc" =>
+"Tämä sivupohja määrittää ohjeet, jotka käyttäjä saa, kun hän on unohtanut salasanansa.",
+
+"mailinglist_activation_instructions" =>
+"Postituslistan aktivointiohjeet",
+
+"mailinglist_activation_instructions_desc" =>
+"Tämä sivupohja määrittä postituslistan aktivointiohjeet",
+
+"mbr_activation_instructions" =>
+"Jäsenen tilin aktivointiohjeet",
+
+"mbr_activation_instructions_desc" =>
+"Tämä sivupohja määrittää jäsenen tilin aktivointiohjeet, jotka jäsen saa, kun hän rekisteröityy sivuille.",
+
+"reset_password_notification" =>
+"Salasanan resetointiohjeet",
+
+"reset_password_notification_desc" =>
+"Tämä sivupohja määrittää ohjeet salasanan resetointiin.",
+
+"validated_member_notify" =>
+"Jäsenen tilin vahvistusilmoitus",
+
+"validated_member_notify_desc" =>
+"Tämä sivupohja määrittää ilmoituksen jonka jäsen saa, kun järjestelmänvalvoja on vahvistanut jäsenyystilin",
+
+"use_this_template" =>
+"Salli tämä sivupohja?",
+
+"use_this_template_exp" =>
+"Jos valitsit EI, tätä sivupohjaa ei käytetä.  Sen sijaan, ilmoitus muodostetaan dynaamisesti kunkin käyttäjän valitsemalla kielellä.",
+
+"user_messages_template_desc" =>
+"Tämän tarkoitus on näyttää virheet ja muut ilmoitukset käyttäjälle heidän suoritettua tietyt toimenpiteet, kuten Kirjautuminen, jne.",
+
+"user_messages_template_warning" =>
+"Älä poista mitään viidestä vaadittavasta muuttujasta:",
+
+"available_variables" =>
+"Seuraavia muuttujia voidaan käyttää:",
+
+''=>''
+);
+?>
