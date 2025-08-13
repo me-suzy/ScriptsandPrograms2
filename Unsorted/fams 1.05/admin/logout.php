6cd3e52af4b1f81cf6cb75af6d1c3ef35098729b
me-suzy <me.suzana@gmail.com>
2025-08-13 04:34:50 +0300

    Add Unsorted/fams 1.05/admin/logout.php

---
+++ Unsorted/fams 1.05/admin/logout.php
@@ -0,0 +1,63 @@
+<?php
+ob_start();
+
+/* ------------------------------------------------------------ */
+/*								*/
+/*	File Area Management System (FAMS)			*/
+/*								*/
+/*	Copyright (c) 2001 by Bastian 'Buddy' Grimm		*/
+/*	Autor: Bastian Grimm					*/
+/*	Publisher: [ BG-Studios ]				*/
+/*	eMail: bastian@bg-studios.de				*/
+/*	WebSite: http://www.bg-studios.de			*/
+/*	Date: 04.05.01		Version: Delte Cat 1.01		*/
+/*	Geändert am: 25.07.01					*/
+/*								*/
+/* ------------------------------------------------------------	*/
+
+require("../config.php");
+require("./lang.php");
+
+if(!$nick)
+{
+include("layout_top_e.php");
+echo $loginfailed;
+include("layout_down_e.php");
+exit;
+}
+else
+{
+	if(!$passwort)
+	{
+	include("layout_top_e.php");
+	echo $loginfailed;
+	include("layout_down_e.php");
+	exit;
+	}
+	else
+	{
+
+	$logon = mysql_query("SELECT * FROM dl_users WHERE (user_name = '$nick') && (user_pwd = '$passwort')");
+	$loggedon=mysql_fetch_array($logon);	
+	if (!$loggedon)
+	{
+	include("layout_top_e.php");
+	echo $loginfailed;
+	include("layout_down_e.php");
+	exit;		
+	}
+	else
+	{
+
+
+setcookie ("nick", "$user_name", time()+3000);
+setcookie ("passwort", "$user_pwd", time()+3000);
+
+header("location: ./index.php");
+	
+	}
+
+}
+}
+
+?>
