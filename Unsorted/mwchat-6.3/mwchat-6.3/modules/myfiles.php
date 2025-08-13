d08b225c4de9cef58bf0b6521129a507804cc05d
me-suzy <me.suzana@gmail.com>
2025-08-13 06:35:50 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/modules/myfiles.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/modules/myfiles.php
@@ -0,0 +1,3 @@
+<?PHP
+  if ($CONFIG[Chat_Upload] == "true" and $STATUS[Registered] == "yes") { $rgProcess_INSERT = db_query(Validate(1011), $CONN); } else { Post_System("<B>$LOCALE[DIALOG_Syntax_Error]</B> - /$rgMatches[1] $LOCALE[COMMON_Registered_Only]", "syntax", $Room); } ?>
+
