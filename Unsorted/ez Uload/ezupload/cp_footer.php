ef8e483930b813eb3b3157336dd30e780966b052
me-suzy <me.suzana@gmail.com>
2025-08-13 04:29:30 +0300

    Add Unsorted/ez Uload/ezupload/cp_footer.php

---
+++ Unsorted/ez Uload/ezupload/cp_footer.php
@@ -0,0 +1,39 @@
+<? 
+/////////////////////////////////////////////////////////////
+// Program Name         : EzUpload Pro                       
+// Program Version      : 2.20                                
+// Program Author       : ScriptsCenter.com                  
+// Supplied by          : CyKuH [WTN]                        
+// Nullified and tested : CyKuH [WTN]                        
+// Distribution         : via WebForum and Forums File Dumps 
+//                    WTN Team `2004
+/////////////////////////////////////////////////////////////
+  global $safemode, $uploadon, $ezu_version;
+  
+  $safemodestatus = $safemode ? "On" : "Off";
+?>
+
+		        </td>
+              </tr>
+            </table>
+		  </td>
+        </tr>
+      </table>
+      <table width="500" border="0" cellspacing="0" cellpadding="0" align="center" height="10">
+        <tr>
+          <td></td>
+        </tr>
+      </table>
+      <table width="500" border="0" cellspacing="0" cellpadding="1" align="center">
+        <tr>
+          <td align="center"><!--CyKuH [WTN]-->ezUPLOAD Pro <?=$ezu_version?> &copy; 2002-2003 ScriptsCenter. All Rights Reserved.</td>
+        </tr>
+	    <tr>
+		  <td align="center">Server running on <b>PHP <?=phpversion()?></b> with Safe Mode <b><?=$safemodestatus?></b></td>
+		</tr>
+      </table>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
