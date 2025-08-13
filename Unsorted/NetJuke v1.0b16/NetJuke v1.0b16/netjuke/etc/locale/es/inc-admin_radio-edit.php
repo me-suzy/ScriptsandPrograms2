7d2cb270f967bcbb654ba625d7883ed2a9b8b5b9
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:45 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_radio-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_radio-edit.php
@@ -0,0 +1,17 @@
+<?php
+
+##################################################
+
+# /admin/radio-edit.php
+
+##################################################
+
+define( "RADIO_LIST_NOPATH", "Arvhivo no encontrado:\\nLa lista de destino no se pudo encontrar." );
+
+define( "RADIO_LIST_NOPERM", "Error de permisos:\\nLa lista de destino no se pudo escribir." );
+
+define( "RADIO_LIST_DONE", "La lista ha sido editada satisfactoriamente." );
+
+##################################################
+
+?>
