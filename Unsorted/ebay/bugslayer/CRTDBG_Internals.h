7c6134937a3767ff51a49174e0d4ab37d81f2fe2
me-suzy <me.suzana@gmail.com>
2025-08-13 03:00:51 +0300

    Add Unsorted/ebay/bugslayer/CRTDBG_Internals.h

---
+++ Unsorted/ebay/bugslayer/CRTDBG_Internals.h
@@ -0,0 +1,34 @@
+//	$Id: CRTDBG_Internals.h,v 1.2 1999/03/22 00:09:29 josh Exp $
+/*----------------------------------------------------------------------
+John Robbins
+Microsoft Systems Journal, October 1997 - Bugslayer
+----------------------------------------------------------------------*/
+
+#ifndef _CRTDBG_INTERNALS_H
+#define _CRTDBG_INTERNALS_H
+
+#define nNoMansLandSize 4
+
+typedef struct _CrtMemBlockHeader
+{
+    struct _CrtMemBlockHeader * pBlockHeaderNext        ;
+    struct _CrtMemBlockHeader * pBlockHeaderPrev        ;
+    char *                      szFileName              ;
+    int                         nLine                   ;
+    size_t                      nDataSize               ;
+    int                         nBlockUse               ;
+    long                        lRequest                ;
+    unsigned char               gap[nNoMansLandSize]    ;
+    /* followed by:
+     *  unsigned char           data[nDataSize];
+     *  unsigned char           anotherGap[nNoMansLandSize];
+     */
+} _CrtMemBlockHeader;
+
+#define pbData(pblock) ((unsigned char *) \
+                                     ((_CrtMemBlockHeader *)pblock + 1))
+#define pHdr(pbData) (((_CrtMemBlockHeader *)pbData)-1)
+
+#endif      // _CRTDBG_INTERNALS_H
+
+
