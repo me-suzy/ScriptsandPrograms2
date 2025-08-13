59d342371608f945bef0a3ed993400c627feb683
me-suzy <me.suzana@gmail.com>
2025-08-13 03:14:13 +0300

    Add Unsorted/ebay/FilterCoBrand/StdAfx.h

---
+++ Unsorted/ebay/FilterCoBrand/StdAfx.h
@@ -0,0 +1,19 @@
+/*	$Id: StdAfx.h,v 1.2 1999/02/21 02:21:57 josh Exp $	*/
+#if !defined(AFX_STDAFX_H__12F4CD9D_801D_11D1_A755_00A024BC16C3__INCLUDED_)
+#define AFX_STDAFX_H__12F4CD9D_801D_11D1_A755_00A024BC16C3__INCLUDED_
+
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#include <afx.h>
+#include <afxwin.h>
+#include <afxmt.h>		// for synchronization objects
+#include <afxext.h>
+#include <afxisapi.h>
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Developer Studio will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__12F4CD9D_801D_11D1_A755_00A024BC16C3__INCLUDED)
