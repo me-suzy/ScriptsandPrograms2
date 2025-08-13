5e76c336711c48a4eae80f08346e68e73a69f443
me-suzy <me.suzana@gmail.com>
2025-08-13 06:03:41 +0300

    Add Unsorted/linksubmit/header.php

---
+++ Unsorted/linksubmit/header.php
@@ -0,0 +1,22 @@
+<?php 
+require "variables.php";
+?>
+<html>
+<head>
+<title><?php echo $title ?> </title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<body bgcolor="#006699" text="#FFFFFF" link="#FFFFFF" vlink="#FFFF00" alink="#FF0000"><font color="#FFFFFF" size="1" face="verdana"> 
+<br>
+<center><font size="4">
+<?php echo $text1 ?>
+<br>
+<?php echo $text2 ?>
+<br>
+  <a href="<?php echo $url1 ?>" target=_blank"> 
+  <?php 
+ echo $urltext1 ?>
+  </a><br></font> 
+</center><br>
+
+<div align="center"><a href="submit.php"><font size="3" face="Verdana, Arial, Helvetica, sans-serif">Click 
+  Here To Submit Your Site</font></a></div><br>
