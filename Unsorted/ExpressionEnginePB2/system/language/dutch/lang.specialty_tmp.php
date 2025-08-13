9230b5d3b3037c4a8d4a07f48e2a70ad61f96fa8
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:36 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.specialty_tmp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.specialty_tmp.php
@@ -0,0 +1,82 @@
+<?php
+$L = array(
+
+
+"email_title" =>
+"Email Titel",
+
+"email_message" =>
+"Email Bericht",
+
+"admin_notify_comment" =>
+"Admin Berichten bij nieuwe reacties",
+
+"admin_notify_comment_desc" =>
+"Dit sjabloon bevat het bericht wat een admin ontvangt als er een nieuwe reactie geplaatst is.",
+
+"admin_notify_trackback" =>
+"Admin Berichten bij nieuwe Trackbacks",
+
+"admin_notify_trackback_desc" =>
+"Dit sjabloon bevat het bericht wat een admin ontvangt als er een nieuwe trackback ontvangen is",
+
+"admin_notify_reg" =>
+"Admin berichten bij nieuwe registratie van een lid",
+
+"admin_notify_reg_desc" =>
+"Dit sjabloon bevat het bericht wat een admin ontvangt als er een nieuw lid geregistreerd is",
+
+"comment_notification" =>
+"Gebruikers berichten bij nieuwe reacties",
+
+"comment_notification_desc" =>
+"Dit sjabloon bevat het bericht wat een gebruiker ontvangt wanneer iemand een reageert op een reactie.",
+
+"forgot_password_instructions" =>
+"Instructies bij Vergeten van Wachtwoord",
+
+"forgot_password_instructions_desc" =>
+"Dit sjabloon bevat de instructie die een geruiker ontvangt als hij z&#39;n password vergeten is.",
+
+"mailinglist_activation_instructions" =>
+"Instructies voor Activeren Mailinglijst",
+
+"mailinglist_activation_instructions_desc" =>
+"Dit sjabloon bevat de instructie voor het activeren van de mailinglijst.",
+
+"mbr_activation_instructions" =>
+"Instructies voor het Activeren van een Lid Account",
+
+"mbr_activation_instructions_desc" =>
+"Dit sjabloon bevat de instructie voor het activeren van het lid account wat gestuurd wordt naar mensen die zich geregistreerd hebben.",
+
+"reset_password_notification" =>
+"Bericht voor Resetten van Wachtwoord",
+
+"reset_password_notification_desc" =>
+"Dit sjabloon bevat de instructie voor het resetten van het wachtwoord.",
+
+"validated_member_notify" =>
+"Bericht voor Lid Account Validatie",
+
+"validated_member_notify_desc" =>
+"Dit sjabloon bevat het bericht dat leden ontvangen wanneer een administrator hun account heeft goedgekeurd.",
+
+"use_this_template" =>
+"Dit sjabloon inschakelen?",
+
+"use_this_template_exp" =>
+"Als u op nee klikt, zal dit sjabloon niet gebruikt worden. In plaats daarvan zal het bericht dynamisch gegenereerd worden in de gekozen taal van de gebruiker. ",
+
+"user_messages_template_desc" =>
+"The bedoeling van dit sjabloon is dat de error bericht en andere berichten voor gebruikers die zichtbaar worden na een bepaalde actie, zoals inloggen, een formulier invullen etc.",
+
+"user_messages_template_warning" =>
+"Verwijder de volgende 5 variabelen niet:",
+
+"available_variables" =>
+"De volgende variabelen kunnen gebruikt worden:",
+
+''=>''
+);
+?>
