5b5546d6c120381f535edbfb62f95681d6cc4d99
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:44 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/myfiles.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/myfiles.php
@@ -0,0 +1,33 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[MYFILES_File_Title] = "Mine on-line arkiver!";
+
+$LOCALE[MYFILES_File_Info] = "Klikk på et arkiv som De ønsker til download eller sikt, eller sjekker noen arkiver som De ønsker fjerne.";
+
+$LOCALE[MYFILES_File_Button] = "Fjern Marked Files";
+
+$LOCALE[MYFILES_File_None] = "De har ikke noen arkiver.";
+
+$LOCALE[MYFILES_From] = "Arkiver fra";
+
+$LOCALE[MYFILES_Remove] = "Fjern";
+
+$LOCALE[MYFILES_Time] = "Arkiver tid";
+
+$LOCALE[MYFILES_Size] = "Arkiver størrelse";
+
+$LOCALE[MYFILES_Name] = "Arkiver navn";
+
+$LOCALE[MYFILES_Storage] = "Deres total tilgjengelig lagerskille er";
+
+?>
