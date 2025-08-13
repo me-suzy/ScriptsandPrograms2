1b1b8cda624d29e736fcdf8935d229b8050d6cfd
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:54 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.core.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.core.php
@@ -0,0 +1,364 @@
+<?php
+$L = array(
+
+
+"on" =>
+"aan",
+
+"off" =>
+"uit",
+
+"first" =>
+"Eerste",
+
+"last" =>
+"Laatste",
+
+"enabled" =>
+"ingeschakeld",
+
+"disabled" =>
+"uitgeschakeld",
+
+"back" =>
+"Terug",
+
+"submit" =>
+"Verzenden",
+
+"update" =>
+"Vernieuwen",
+
+"thank_you" =>
+"Dank u!",
+
+"system_off_msg" =>
+"Deze site is tijdelijk buiten bedrijf.",
+
+"not_authorized" =>
+"U heeft geen toestemming om dit uit te voeren",
+
+"auto_redirection" =>
+"U wordt automatisch teruggeleid binnen %x seconden",
+
+"error" =>
+"Fout",
+
+"invalid_url" =>
+"De de door u verstuurde URL bestaat niet",
+
+"submission_error" =>
+"Het formulier wat u verstuurde bevat de volgende fouten:",
+
+"general_error" =>
+"de volgende fouten zijn geconstateerd:",
+
+"invalid_action" =>
+"De taak die u wilde uitvoeren is ongeldig.",
+
+"banned" =>
+"Verwijderd",
+
+"guests" =>
+"Gasten",
+
+"members" =>
+"Leden",
+
+"pending" =>
+"Pending",
+
+"super_admins" =>
+"Super Admins",
+
+"error_tag_syntax" =>
+"De volgende tag heeft een syntaxfout:",
+
+"error_fix_syntax" =>
+"Corrigeer a.u.b. de syntax in uw voorbeeld.",
+
+"forgotten_email_sent" =>
+"Instructies om uw wachtwoord te veranderen zijn naar u gemaild. ",
+
+"error_sending_email" =>
+"Niet mogelijk om email te versturen op dit tijdstip.",
+
+"no_email_found" =>
+"Het door u verzonden email-adres is niet gevonden in onze gegevens.",
+
+"your_new_login_info" =>
+"Inloginformatie",
+
+"reset_link" =>
+"Ga naar de volgende pagina om uw wachtwoord te veranderen: ",
+
+"password_will_be_reset" =>
+"Uw wachtwoord zal automatisch worden veranderd, het nieuwe wachtwoord zal naar u worden gemaild.",
+
+"ignore_password_message" =>
+"Als u uw wachtwoord niet wilt veranderen, negeer dan dit bericht. Het zal na 24 uur verlopen.",
+
+"password_has_been_reset" =>
+"Uw wachtwoord is veranderd en een nieuw is naar u gemaild.",
+
+"new_login_info" =>
+"Hier zijn uw nieuwe inloggegevens:",
+
+"year" =>
+"year",
+
+"years" =>
+"years",
+
+"month" =>
+"month",
+
+"months" =>
+"months",
+
+"week" =>
+"week",
+
+"weeks" =>
+"weeks",
+
+"day" =>
+"day",
+
+"days" =>
+"dagen",
+
+"hour" =>
+"hour",
+
+"hours" =>
+"uren",
+
+"minute" =>
+"minute",
+
+"minutes" =>
+"minuten",
+
+"second" =>
+"second",
+
+"seconds" =>
+"seconden",
+
+"am" =>
+"am",
+
+"pm" =>
+"pm",
+
+"AM" =>
+"AM",
+
+"PM" =>
+"PM",
+
+"Sun" =>
+"zon",
+
+"Mon" =>
+"maa",
+
+"Tue" =>
+"din",
+
+"Wed" =>
+"woe",
+
+"Thu" =>
+"don",
+
+"Fri" =>
+"vri",
+
+"Sat" =>
+"zat",
+
+"Sunday" =>
+"zondag",
+
+"Monday" =>
+"maandag",
+
+"Tuesday" =>
+"dinsdag",
+
+"Wednesday" =>
+"woensdag",
+
+"Thursday" =>
+"donderdag",
+
+"Friday" =>
+"vrijdag",
+
+"Saturday" =>
+"zaterdag",
+
+"Jan" =>
+"jan",
+
+"Feb" =>
+"feb",
+
+"Mar" =>
+"maa",
+
+"Apr" =>
+"apr",
+
+"May" =>
+"mei",
+
+"Jun" =>
+"jun",
+
+"Jul" =>
+"jul",
+
+"Aug" =>
+"aug",
+
+"Sep" =>
+"sep",
+
+"Oct" =>
+"okt",
+
+"Nov" =>
+"nov",
+
+"Dec" =>
+"dec",
+
+"January" =>
+"januari",
+
+"February" =>
+"februari",
+
+"March" =>
+"maart",
+
+"April" =>
+"april",
+
+"June" =>
+"juni",
+
+"July" =>
+"juli",
+
+"August" =>
+"augustus",
+
+"September" =>
+"september",
+
+"October" =>
+"oktober",
+
+"November" =>
+"november",
+
+"December" =>
+"december",
+
+"UM12" =>
+"(UTC - 12:00) Enitwetok, Kwajalien",
+
+"UM11" =>
+"(UTC - 11:00) Nome, Midway Island, Samoa",
+
+"UM10" =>
+"(UTC - 10:00) Hawaii",
+
+"UM9" =>
+"(UTC - 9:00) Alaska",
+
+"UM8" =>
+"(UTC - 8:00) Pacific Time",
+
+"UM7" =>
+"(UTC - 7:00) Mountain Time",
+
+"UM6" =>
+"(UTC - 6:00) Central Time, Mexico City",
+
+"UM5" =>
+"(UTC - 5:00) Eastern Time, Bogota, Lima, Quito",
+
+"UM4" =>
+"(UTC - 4:00) Atlantic Time, Caracas, La Paz",
+
+"UM25" =>
+"(UTC - 3:30) Newfoundland",
+
+"UM3" =>
+"(UTC - 3:00) Brazil, Buenos Aires, Georgetown, Falkland Is.",
+
+"UM2" =>
+"(UTC - 2:00) Mid-Atlantic, Ascention Is., St Helena",
+
+"UM1" =>
+"(UTC - 1:00) Azores, Cape Verde Islands",
+
+"UTC" =>
+"(UTC) Casablanca, Dublin, Edinburgh, London, Lisbon, Monrovia",
+
+"UP1" =>
+"(UTC + 1:00) Berlin, Brussels, Copenhagen, Madrid, Paris, Rome",
+
+"UP2" =>
+"(UTC + 2:00) Kaliningrad, South Africa, Warsaw",
+
+"UP3" =>
+"(UTC + 3:00) Baghdad, Riyadh, Moscow, Nairobi",
+
+"UP25" =>
+"(UTC + 3:30) Tehran",
+
+"UP4" =>
+"(UTC + 4:00) Adu Dhabi, Baku, Muscat, Tbilisi",
+
+"UP35" =>
+"(UTC + 4:30) Kabul",
+
+"UP5" =>
+"(UTC + 5:00) Islamabad, Karachi, Tashkent",
+
+"UP45" =>
+"(UTC + 5:30) Bombay, Calcutta, Madras, New Delhi",
+
+"UP6" =>
+"(UTC + 6:00) Almaty, Colomba, Dhakra",
+
+"UP7" =>
+"(UTC + 7:00) Bangkok, Hanoi, Jakarta",
+
+"UP8" =>
+"(UTC + 8:00) Beijing, Hong Kong, Perth, Singapore, Taipei",
+
+"UP9" =>
+"(UTC + 9:00) Osaka, Sapporo, Seoul, Tokyo, Yakutsk",
+
+"UP85" =>
+"(UTC + 9:30) Adelaide, Darwin",
+
+"UP10" =>
+"(UTC + 10:00) Melbourne, Papua New Guinea, Sydney, Vladivostok",
+
+"UP11" =>
+"(UTC + 11:00) Magadan, New Caledonia, Solomon Islands",
+
+"UP12" =>
+"(UTC + 12:00) Auckland, Wellington, Fiji, Marshall Island",
+
+''=>''
+);
+?>
