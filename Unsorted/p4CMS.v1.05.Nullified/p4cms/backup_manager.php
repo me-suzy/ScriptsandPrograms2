7110c0f8037da35ecbb8770bcd5799e52f9ee10f
me-suzy <me.suzana@gmail.com>
2025-08-13 07:32:44 +0300

    Add Unsorted/p4CMS.v1.05.Nullified/p4cms/backup_manager.php

---
+++ Unsorted/p4CMS.v1.05.Nullified/p4cms/backup_manager.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title></title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+</head>
+
+<body>
+<font size="2" face="Verdana, Arial, Helvetica, sans-serif"><b>Lieber Besucher,</b><br>
+diese Funktion ist in der Demo-Version nicht verf&uuml;gbar.
+</font>
+</body>
+</html>
