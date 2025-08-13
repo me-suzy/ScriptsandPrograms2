f9364b7bb4fd5ed1371c3935340a93060bcca2b1
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:01 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.member.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.member.php
@@ -0,0 +1,376 @@
+<?php
+$L = array(
+
+
+"member_module_name" =>
+"Utenti",
+
+"member_module_description" =>
+"Sistema gestione utenti",
+
+"mbr_menu" =>
+"Menu",
+
+"mbr_show" =>
+"Mostra",
+
+"mbr_sort" =>
+"Ordina",
+
+"mbr_order" =>
+"Ordine",
+
+"mbr_rows" =>
+"Righe",
+
+"mbr_my_account" =>
+"My Account",
+
+"mbr_logged_in_as" =>
+"Collegato:",
+
+"mbr_logout" =>
+"Log-out",
+
+"mbr_member_registration" =>
+"Iscrizione utente",
+
+"mbr_forgotten_password" =>
+"Password smarrita",
+
+"mbr_memberlist" =>
+"Elenco utenti",
+
+"notice" =>
+"Avviso",
+
+"mbr_registration_not_allowed" =>
+"Al momento non sono accettate nuove iscrizioni.",
+
+"mbr_login" =>
+"Log-in",
+
+"mbr_form_empty" =>
+"Devi specificare username e password",
+
+"mbr_account_not_active" =>
+"Il tuo account non è ancora attivo.",
+
+"mbr_you_are_logged_in" =>
+"Ora sei collegato.",
+
+"mbr_you_are_logged_out" =>
+"Ora sei scollegato (adieu...)",
+
+"mbr_no_reset_id" =>
+"Il numero ID specificato sembra non essere valido. Controlla il link che hai seguito.",
+
+"mbr_id_not_found" =>
+"Il codice specificato non h presente nel database.",
+
+"mbr_username" =>
+"Username",
+
+"mbr_password" =>
+"Password",
+
+"mbr_submit" =>
+"Invia",
+
+"mbr_update" =>
+"Aggiorna",
+
+"mbr_password_confirm" =>
+"Conferma password",
+
+"mbr_auto_login" =>
+"Log-in automatico alle prossime visite",
+
+"mbr_show_name" =>
+"Mostra il mio nome nell&#39;elenco utenti pubblico",
+
+"mbr_forgot_password" =>
+"Password smarrita?",
+
+"mbr_email_address" =>
+"Indirizzo e-mail",
+
+"mbr_your_email" =>
+"Il tuo indirizzo e-mail",
+
+"mbr_email_short" =>
+"E-mail",
+
+"mbr_icq_console" =>
+"ICQ Console",
+
+"mbr_aim_console" =>
+"AOL IM",
+
+"mbr_aol_short" =>
+"AOL",
+
+"mbr_icq_short" =>
+"ICQ",
+
+"mbr_www" =>
+"URL",
+
+"mbr_msn_short" =>
+"MSN",
+
+"mbr_yahoo_short" =>
+"Yahoo",
+
+"mbr_screen_name" =>
+"Nome sul sito",
+
+"mbr_name" =>
+"Nome",
+
+"mbr_screen_name_exp" =>
+"Se lasci questo campo vuoto, sul sito verrà mostrato il tuo username (è meglio di no)",
+
+"mbr_your_url" =>
+"L&#39;URL del tuo sito",
+
+"mbr_field_required" =>
+"I seguenti campi sono obbligatori:",
+
+"mbr_username_length" =>
+"Lo username deve contenere almeno %x caratteri",
+
+"mbr_password_length" =>
+"La password deve contenere almeno %x caratteri",
+
+"mbr_registration_complete" =>
+"Iscrizione completa",
+
+"mbr_registration_completed" =>
+"La tua iscrizione è andata a buon fine.",
+
+"mbr_admin_will_activate" =>
+"Un amministratore del sito attiverà l&#39;account e ti comunicherà la possibilità di utilizzarlo.",
+
+"mbr_membership_instructions_email" =>
+"Ti è appena stato spedito un messaggio con le istruzioni di attivazione account.",
+
+"mbr_membership_instructions_cont" =>
+"Controlla la tua e-mail e segui le istruzioni contenute nel messaggio.",
+
+"mbr_your_are_logged_in" =>
+"Sei collegato e puoi iniziare ad utilizzare il nuovo account.",
+
+"mbr_activation" =>
+"Attivazione account",
+
+"mbr_problem_activating" =>
+"Richiesta di attivazione non valida.",
+
+"mbr_activation_success" =>
+"Il tuo account è stato attivato.",
+
+"mbr_may_now_log_in" =>
+"Ora puoi collegarti ed iniziare a usarlo.",
+
+"mbr_not_allowed_to_view_profiles" =>
+"Non sei abilitato alla consultazione dei profili utente.",
+
+"mbr_member_profile" =>
+"Profilo utente",
+
+"mbr_member_group" =>
+"Catergoria utente:",
+
+"mbr_last_visit" =>
+"Ultima visita",
+
+"mbr_most_recent_entry" =>
+"Interventi più recenti",
+
+"mbr_most_recent_comment" =>
+"Commenti più recenti",
+
+"mbr_required_fields" =>
+"Indica i campi obbligatori",
+
+"mbr_join_date" =>
+"Data di iscrizione",
+
+"mbr_total_entries" =>
+"Interventi totali",
+
+"mbr_total_comments" =>
+"Commenti totali",
+
+"mbr_comments" =>
+"Commenti",
+
+"mbr_member_timezone" =>
+"Fuso orario utente",
+
+"mbr_member_local_time" =>
+"Ora locale utente",
+
+"mbr_email" =>
+"E-mail",
+
+"mbr_url" =>
+"Homepage",
+
+"mbr_location" =>
+"Località",
+
+"mbr_birthday" =>
+"Data di nascita",
+
+"mbr_your_stats" =>
+"Statistiche del tuo account",
+
+"mbr_edit_your_profile" =>
+"Modifica il tuo profilo",
+
+"mbr_profile_homepage" =>
+"Homepage profilo",
+
+"mbr_profile_has_been_updated" =>
+"Il tuo profilo è stato aggiornato",
+
+"mbr_email_has_been_updated" =>
+"Le tue preferenze e-mail sono state aggiornate",
+
+"mbr_email_updated" =>
+"Preferenze e-mail aggiornate",
+
+"username_disallowed" =>
+"La modifica dello username non è consentita",
+
+"mbr_settings_updated" =>
+"I tuoi settaggi sono stati aggiornati",
+
+"mbr_notepad_updated" =>
+"Il tuo notepad è stato aggiornato",
+
+"mbr_localization_settings_updated" =>
+"Le tue preferenze di localizzazione sono state aggiornate",
+
+"mbr_terms_of_service" =>
+"Condizioni del servizio",
+
+"mbr_i_accept" =>
+"Accetto le condizioni del servizio",
+
+"mbr_terms_of_service_required" =>
+"Devi spuntare il checkbox &quot;accetto le condizioni del servizio&quot;",
+
+"mbr_custom_field_empty" =>
+"I campi seguenti sono obbligatori:",
+
+"mbr_all_member_groups" =>
+"Tutte le categorie di utenti",
+
+"mbr_member_name" =>
+"Nome utente",
+
+"mbr_ascending" =>
+"Ascendente",
+
+"mbr_descending" =>
+"Discendente",
+
+"mbr_back_to_login" =>
+"Torna al log-in",
+
+"mbr_back_to_main" =>
+"Torna alla pagina principale",
+
+"mbr_aol_im" =>
+"AOL Instant Messenger",
+
+"mbr_icq" =>
+"ICQ Number",
+
+"mbr_yahoo" =>
+"Yahoo Messenger",
+
+"mbr_msn" =>
+"MSN Messenger",
+
+"mbr_bio" =>
+"Bio",
+
+"mbr_interests" =>
+"Interessi",
+
+"mbr_occupation" =>
+"Occupazione",
+
+"mbr_email_member" =>
+"Console e-mail",
+
+"mbr_subject" =>
+"Oggetto e-mail",
+
+"mbr_message" =>
+"Messaggio e-mail",
+
+"mbr_close_window" =>
+"Chiudi finestra",
+
+"mbr_recipient" =>
+"Destinatario e-mail:",
+
+"mbr_email_not_accepted" =>
+"Questo utente non desidera ricevere e-mail.",
+
+"mbr_email_disclaimer" =>
+"Inviando questo messaggio, mostrerai al destinatario il tuo indirizzo e-mail.",
+
+"mbr_email_logged" =>
+"Nota: i messaggi inviati vengono archiviati e sono visibili agli amministratori del sito",
+
+"mbr_missing_fields" =>
+"Tutti i campi sono obbligatori",
+
+"mbr_send_email" =>
+"Invia e-mail",
+
+"mbr_email_forwarding" =>
+"Questo messaggio è stato spedito dal tuo account a:",
+
+"mbr_email_forwarding_cont" =>
+"Se non desideri ricevere ulteriori messaggi, puoi impedirne la ricezione tramite le preferenze dalla pagina del tuo profilo.",
+
+"mbr_email_error" =>
+"Si è verificato un errore durante la spedizione del tuo messaggio.",
+
+"mbr_good_email" =>
+"Il tuo messaggio è stato inviato con successo.",
+
+"mbr_send_self_copy" =>
+"Inoltrami una copia di questo messaggio.",
+
+"mbr_icq_number" =>
+"Numero ICQ:",
+
+"mbr_icq_recipient" =>
+"Destinatario:",
+
+"mbr_icq_subject" =>
+"Oggetto:",
+
+"mbr_icq_message" =>
+"Messaggio:",
+
+"mbr_not_allowed_to_use_email_console" =>
+"Non sei abilitato all&#39;utilizzo della console e-mail",
+
+"mbr_email_timelock_not_expired" =>
+"E&#39; consentito utilizzare la console solo una volta ogni %x minuti.",
+
+"mbr_you_are_registered" =>
+"Sei già registrato e collegato.",
+
+''=>''
+);
+?>
