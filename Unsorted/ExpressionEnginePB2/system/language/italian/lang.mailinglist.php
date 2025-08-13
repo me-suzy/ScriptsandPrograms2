d5413e5aaa8f3a250a19cb00bb8507c42a346c4d
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:59 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.mailinglist.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.mailinglist.php
@@ -0,0 +1,112 @@
+<?php
+$L = array(
+
+
+"mailinglist_module_name" =>
+"Mailing-list",
+
+"mailinglist_module_description" =>
+"Gestione mailing-list",
+
+"ml_mailinglist" =>
+"Mailing-list",
+
+"ml_total_in_mailinglist" =>
+"Indirizzi e-mail totali:",
+
+"ml_view_mailinglist" =>
+"Mostra mailing-list",
+
+"ml_email_search" =>
+"Cerca e-mail",
+
+"ml_email_search_cont" =>
+"Specifica un indirizzo e-mail completo o parziale",
+
+"ml_email_address" =>
+"E-mail",
+
+"ml_delete" =>
+"Cancella",
+
+"ml_no_results" =>
+"La ricerca non ha prodotto riultati",
+
+"ml_delete_confirm" =>
+"Cancella indirizzi e-mail",
+
+"ml_delete_question" =>
+"Sei sicuro di voler cancellare l&#39;indirizzo(i) selezionato?",
+
+"ml_email_deleted" =>
+"L&#39;indirizzo e-mail è stato cancellato",
+
+"ml_emails_deleted" =>
+"Gli indirizzi e-mail sono stati cancellati",
+
+"ml_missing_email" =>
+"Devi specificare un indirizzo e-mail",
+
+"ml_invalid_email" =>
+"Indirizzo e-mail non valido",
+
+"ml_email_already_in_list" =>
+"Il tuo indirizzo e-mail è già iscritto alla mailing-list",
+
+"ml_banned_email" =>
+"Indirizzo e-mail non ammesso",
+
+"ml_email_accepted" =>
+"Il tuo indirizzo e-mail è stato aggiunto alla nostra mailing-list.",
+
+"ml_email_confirmation_sent" =>
+"Un messaggio di conferma ti è appena stato inviato.",
+
+"ml_click_confirmation_link" =>
+"Seleziona il link contenuto nel messaggio per confermare l&#39;iscrizione alla mailing-list.",
+
+"ml_mailinglist_confirmation" =>
+"Messaggio di conferma",
+
+"ml_thankyou_for_joining" =>
+"Grazie per esserti iscritto alla nostra mailing-list!",
+
+"ml_activation_instructions" =>
+"Seleziona il link seguente per confermare la sottoscrizione.",
+
+"ml_expiration_blurb" =>
+"Se non ti interessa sottoscrivere la nostra mailing-list, ignora questo messaggio.",
+
+"ml_account_confirmed" =>
+"La tua sottoscrizione è stata confermata.",
+
+"ml_expired_date" =>
+"Il codice di autorizzazione che hai inviato è scaduto",
+
+"ml_unsubscribe" =>
+"Il tuo indirizzo e-mail è stato rimosso dalla nostra mailing-list.",
+
+"ml_unsubscribe_failed" =>
+"Il numero di account specificato non è rintracciabile.",
+
+"ml_add_email_addresses" =>
+"Sottoscrizione indirizzi e-mail",
+
+"ml_add_email_addresses_cont" =>
+"Separa ogni indirizzo con uno spazio o un ritorno a capo",
+
+"ml_emails_imported" =>
+"Sottoscrizione completata!",
+
+"ml_total_emails_imported" =>
+"Totale indirizzi e-mail sottoscritti:",
+
+"ml_bad_email_heading" =>
+"Questi indirizzi e-mail non possono essere importati:",
+
+"ml_total_duplicate_emails" =>
+"Totale indirizzi e-mail già esistenti:",
+
+''=>''
+);
+?>
