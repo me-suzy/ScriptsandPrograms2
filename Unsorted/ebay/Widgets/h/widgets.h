3d21c25d45b8ccc186ece49660d8645be4400452
me-suzy <me.suzana@gmail.com>
2025-08-13 03:53:15 +0300

    Add Unsorted/ebay/Widgets/h/widgets.h

---
+++ Unsorted/ebay/Widgets/h/widgets.h
@@ -0,0 +1,33 @@
+/* $Id: widgets.h,v 1.5 1999/05/19 02:34:08 josh Exp $ */
+
+// The precompiled header for this project.
+#ifndef widgets_h
+#define widgets_h
+
+#include "clsWidgetPage.h"
+#include "clsMarketPlaces.h"
+#include "clsMarketPlace.h"
+
+#include "clsApp.h"
+#include "clsEnvironment.h"
+#include "clsDatabase.h"
+#include "eBayDebug.h"
+
+#include "clsWidgetHandler.h"
+#include "clsWidgetContext.h"
+#include "clseBayWidget.h"
+#include "clseBayTableWidget.h"
+#include "clseBayItemWidget.h"
+#include "clsDataPool.h"
+#include "clsUtilities.h"
+
+#include <time.h>
+#include <stdio.h>
+
+#include "clsUserIdWidget.h"
+
+#include "clsCurrencyWidget.h"
+#include "clsBidBoxWidget.h"
+#include "clseBayTimeWidget.h"
+
+#endif /* widgets_h */
