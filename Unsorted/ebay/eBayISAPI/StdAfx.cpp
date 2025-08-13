a36ca9c50f22d1bbde6619839099be953891a63e
me-suzy <me.suzana@gmail.com>
2025-08-13 03:13:30 +0300

    Add Unsorted/ebay/eBayISAPI/StdAfx.cpp

---
+++ Unsorted/ebay/eBayISAPI/StdAfx.cpp
@@ -0,0 +1,12 @@
+/*	$Id: StdAfx.cpp,v 1.5 1999/02/21 02:30:57 josh Exp $	*/
+// stdafx.cpp : source file that includes just the standard includes
+//	eBayISAPI.pch will be the pre-compiled header
+//	stdafx.obj will contain the pre-compiled type information
+
+
+#pragma warning(disable:4786)
+#pragma message("THE FOLLOWING WARNING IS EXPECTED DUE TO A BUG IN VC5 AND MAY BE IGNORED.")
+// The warning will be warning 4786; pragma warning disable is busted for 4786 for some reason.
+
+#include "stdafx.h"
+#include "ebihdr.h"
