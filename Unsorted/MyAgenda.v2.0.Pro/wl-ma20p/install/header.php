813be07f6c9d4080410fff6395455c833592df87
me-suzy <me.suzana@gmail.com>
2025-08-13 06:37:19 +0300

    Add Unsorted/MyAgenda.v2.0.Pro/wl-ma20p/install/header.php

---
+++ Unsorted/MyAgenda.v2.0.Pro/wl-ma20p/install/header.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>myAgenda <?=$version;?> Installation</title>
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-Control" content="no-cache">
+<meta http-equiv="Expires" content="-1">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+td 			{font-family:MS Sans Serif, verdana; font-size:9pt;}
+input		{font-size: 8pt; font-family: MS Sans Serif, verdana, arial, sans-serif;}
+.textarea {  font-family: MS Sans Serif; width: 450px; background-color: #C6C3C6; height: 140px; clip:  rect(   ); font-size: 9pt}
+.txt {font-size: 12px; padding:0px; margin:0px; border:1px inset window;}
+-->
+</style>
+<script language="Javascript">
+<!--
+function Cancel() {
+	if (confirm("Do you wish to cancel the installation?")) {
+		location.href = "http://www.google.com";
+	}
+}
+function Back() {
+  history.go(-1);
+}
+// -->
+</script>
