cf630cfd0b1ca4ac675642a41dbcf3e0b67b654c
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:50 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-prefs.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-prefs.php
@@ -0,0 +1,46 @@
+<?php
+
+##################################################
+
+# /prefs.php
+
+##################################################
+
+define( "PREFS_DENIED_1", "S'ha detectat un error. Si us plau, torneu a intentar-ho." );
+
+define( "PREFS_CHECKFORM_1", "Si us plau, seleccioneu un color de fons (bgcolor)." );
+define( "PREFS_CHECKFORM_2", "Si us plau, seleccioneu una llista tipogràfica(font-family)" );
+define( "PREFS_CHECKFORM_3", "Si us plau, seleccioneu la mida de la tipografia (font-size)" );
+define( "PREFS_CHECKFORM_4", "Si us plau, seleccioneu un color de text (text)" );
+define( "PREFS_CHECKFORM_5", "Si us plau, seleccioneu un color per als enllaços (link)." );
+define( "PREFS_CHECKFORM_6", "Si us plau, seleccioneu un color per als enllaços actius (alink)." );
+define( "PREFS_CHECKFORM_7", "Si us plau, seleccioneu un color per als enllaços visitats (vlink)." );
+define( "PREFS_CHECKFORM_8", "Si us plau, seleccioneu un color per a la vora (border)." );
+define( "PREFS_CHECKFORM_9", "Si us plau, seleccioneu un color per al fons de la capçalera (header)." );
+define( "PREFS_CHECKFORM_10", "Si us plau, seleccioneu un color per al text de la capçalera (header)." );
+define( "PREFS_CHECKFORM_11", "Si us plau, seleccioneu un color per al fons del contingut (content)." );
+
+define( "PREFS_HEADER", "PREFERÈNCIES" );
+define( "PREFS_CAPTION", "Utilitzeu aquest formulari per modificar el vostre compte." );
+define( "PREFS_PALETTE", "Utilitzeu aquesta paleta de colors per configurar el vostre entorn." );
+
+define( "PREFS_FORMS_INVICN", "Inverteix les icones" );
+define( "PREFS_FORMS_INVICN_HELP", "Permet invertir els colors de les icones: Reproducció, Informació, Filtre, etc." );
+define( "PREFS_FORMS_INVICN_ENABLED", "Actiu" );
+define( "PREFS_FORMS_FONTFACE", "Llista tipogràfica" );
+define( "PREFS_FORMS_FONTSIZE", "Mida de la tipografia" );
+define( "PREFS_FORMS_BGCOLOR", "Color del fons" );
+define( "PREFS_FORMS_TEXT", "Color del text" );
+define( "PREFS_FORMS_LINK", "Color dels enllaços" );
+define( "PREFS_FORMS_ALINK", "Color dels enllaços actius" );
+define( "PREFS_FORMS_VLINK", "Color dels enllaços visitats" );
+define( "PREFS_FORMS_BORDER", "Color de la vora" );
+define( "PREFS_FORMS_HEADER", "Color del fons de capçalera" );
+define( "PREFS_FORMS_HEADERFC", "Color del text de capçalera" );
+define( "PREFS_FORMS_CONTENT", "Color del fons del contingut" );
+define( "PREFS_FORMS_BTN_SAVE", "Desa" );
+define( "PREFS_FORMS_BTN_RESET", "Desfés canvis" );
+
+##################################################
+
+?>
