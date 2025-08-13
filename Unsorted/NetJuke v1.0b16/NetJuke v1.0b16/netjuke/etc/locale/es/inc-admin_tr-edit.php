96194c6cc20afc6e7aaba25a0b54c7c55e0ee115
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:58 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_tr-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_tr-edit.php
@@ -0,0 +1,70 @@
+<?php
+
+##################################################
+
+# /admin/tr-edit.php
+
+##################################################
+
+define( "TREDIT_DELETED_HELP", "Las canciones seleccionadas han sido eliminadas." );
+
+define( "TREDIT_DELCHILD_HELP_1", "El valor N/A es obligatorio y no puede ser borrado." );
+define( "TREDIT_DELCHILD_HELP_2", "El valor de N/A es obligatorio y no puede ser modificado.\\nPor favor cree un nuevo registro usando la nueva caja de chequeo." );
+
+define( "TREDIT_HEADER", "EDITAR INFORMACION DE LA CANCION" );
+
+define( "TREDIT_ERROR_INSERT", "Error al insertar en" );
+define( "TREDIT_ERROR_TR_T", "Error actualizando la tabla netjuke_pistas" );
+define( "TREDIT_ERROR_AR_T", "Error actualizando la tabla netjuke_artistas" );
+define( "TREDIT_ERROR_AL_T", "Error actualizando la tabla netjuke_albums" );
+define( "TREDIT_ERROR_GE_T", "Error actualizando la tabla netjuke_generos" );
+define( "TREDIT_ERROR_TR", "El nombre de la canción no debe estar vacia" );
+define( "TREDIT_ERROR_TN", "El número de canciones se ha dejado vacío." );
+define( "TREDIT_ERROR_BR", "La tasa de bits es obligatorio y debe ser mayor que 0." );
+define( "TREDIT_ERROR_SR", "La frecuencia de muestreo es obligatorio y debe ser mayor que 0." );
+define( "TREDIT_ERROR_FK", "El tipo de pista se ha dejado vacío." );
+define( "TREDIT_ERROR_LC", "La localización de la pista se ha dejado vacío." );
+define( "TREDIT_ERROR_NORES", "Por favor, seleccione una canción válida" );
+define( "TREDIT_FORM_CM", "Editar Comentarios");
+define( "TREDIT_FORM_LY", "Editar Liricos");
+
+define( "TREDIT_FORM_NEW", "Nueva" );
+
+define( "TREDIT_FORM_TR", "Nombre Canción" );
+define( "TREDIT_FORM_AR", "Nombre Artista" );
+define( "TREDIT_FORM_AL", "Nombre Álbum" );
+define( "TREDIT_FORM_GE", "Nombre Género" );
+define( "TREDIT_FORM_TI", "Duración" );
+define( "TREDIT_FORM_SECONDS", "Segundos" );
+define( "TREDIT_FORM_TN", "Número canción" );
+define( "TREDIT_FORM_BR", "Tasa de bits" );
+define( "TREDIT_FORM_SR", "Frecuencia de muestreo" );
+define( "TREDIT_FORM_FK", "Tipo de Archivo" );
+define( "TREDIT_FORM_FS", "Tamaño archivo" );
+define( "TREDIT_FORM_FN", "Ubicación" );
+define( "TREDIT_FORM_LC", "Contador de oyentes" );
+define( "TREDIT_FORM_RESET", "Iniciar contador" );
+
+define( "TREDIT_FORM_TR_IMG", "Imagen canción" );
+define( "TREDIT_FORM_AR_IMG", "Imagen artísta" );
+define( "TREDIT_FORM_AL_IMG", "Imagen Álbum" );
+
+define( "TREDIT_FORM_BTN_SAVE", "Guardar" );
+define( "TREDIT_FORM_BTN_RESET", "Limpiar" );
+define( "TREDIT_FORM_BTN_DUP", "Duplicado" );
+define( "TREDIT_FORM_BTN_NEW", "Nueva Canción" );
+define( "TREDIT_FORM_BTN_DEL", "Eliminar" );
+
+define( "TREDIT_FORM_BTN_DEL_CONF", "¿Está seguro de eliminar esta canción?" );
+define( "TREDIT_FORM_BTN_DELCHILD_CONF", "¿Está seguro de eliminar esta selección?" );
+
+define( "TREDIT_PLAY", "Sonar ahora" );
+define( "TREDIT_PLAY_HELP", "Click para tocar esta canción ahora." );
+define( "TREDIT_VIEW", "Modo Ver" );
+define( "TREDIT_VIEW_HELP", "Cambiar a modo Ver." );
+define( "TREDIT_CLOSEWIN", "Cerrar ventana" );
+define( "TREDIT_CLOSEWIN_HELP", "Cerrar esta ventana" );
+
+##################################################
+
+?>
