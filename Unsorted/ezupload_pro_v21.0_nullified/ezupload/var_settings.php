520b6cecf6208d99aa57b4dfe9c13c40c4854360
me-suzy <me.suzana@gmail.com>
2025-08-13 04:33:04 +0300

    Add Unsorted/ezupload_pro_v21.0_nullified/ezupload/var_settings.php

---
+++ Unsorted/ezupload_pro_v21.0_nullified/ezupload/var_settings.php
@@ -0,0 +1,47 @@
+<?php
+/////////////////////////////////////////////////////////////
+// Program Name         : EzUpload Pro                       
+// Program Version      : 2.0                                
+// Program Author       : ScriptsCenter.com                  
+// Supplied by          : Stive [WTN]                        
+// Nullified and tested : CyKuH [WTN]                        
+// Distribution         : via WebForum and Forums File Dumps 
+//                    WTN Team `2002
+/////////////////////////////////////////////////////////////
+
+$VARS[0]['id'] = 1;
+$VARS[0]['extensions'] = "bat exe php cgi phtml php3 inc shtml";
+$VARS[0]['extmode'] = "except";
+$VARS[0]['limitsize'] = 1;
+$VARS[0]['sizemin'] = 0;
+$VARS[0]['sizemax'] = 2000;
+$VARS[0]['limitdim'] = 0;
+$VARS[0]['widthmin'] = 0;
+$VARS[0]['widthmax'] = 1000;
+$VARS[0]['heightmin'] = 0;
+$VARS[0]['heightmax'] = 1000;
+$VARS[0]['subdir'] = 3;
+$VARS[0]['existing'] = "addnumber";
+$VARS[0]['thankyoumsg'] = "Thank you for your submission! We will look at what you submitted and get back to you as soon as possible. Feel free to upload more files below.";
+$VARS[0]['redirecturl'] = "http://";
+$VARS[0]['moreuploads'] = 1;
+$VARS[0]['sendconfirmation'] = 0;
+$VARS[0]['confirmtitle'] = "";
+$VARS[0]['confirmmsg'] = "";
+$VARS[0]['attachments'] = "only";
+$VARS[0]['attachmaxsize'] = 1000;
+$VARS[0]['notification'] = "none";
+$VARS[0]['emailfield'] = 3;
+$VARS[0]['takeip'] = 1;
+$VARS[0]['fieldsize'] = 45;
+$VARS[0]['displayinfo'] = "description";
+$VARS[0]['fonttype'] = "Verdana, Arial, Helvetica, sans-serif";
+$VARS[0]['adminemail'] = "";
+$VARS[0]['adminname'] = "";
+$VARS[0]['adminpass'] = "";
+$VARS[0]['tablealign'] = "center";
+$VARS[0]['showrequired'] = 1;
+$VARS[0]['fontsizel'] = "13px";
+$VARS[0]['fontsizes'] = "10px";
+
+?>
