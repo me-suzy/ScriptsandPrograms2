e1b76691aee2baf0b50c3e0507a70442b1ed05d6
me-suzy <me.suzana@gmail.com>
2025-08-13 05:58:22 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/admin/template.admin.affiliates.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/admin/template.admin.affiliates.php
@@ -0,0 +1,22 @@
+<%include file="admin/admin.header.php"%>
+<BR>
+<%include file="admin/admin.menu.php"%>
+<BR>
+
+<TABLE ALIGN="CENTER" BORDER="0" CELLPADDING="5" CELLSPACING="1" WIDTH="" background="">
+<TR ALIGN="left" bgcolor="" >
+        <TD Align="left">
+                <UL>
+                	<LI><a href="<%$selfURL%>?mode=searchboxes">search boxes for affiliates</a>
+<!--
+                	<LI><a href="<%$selfURL%>?mode=affbanners">banners for affiliates</a>
+-->					
+                	<LI><a href="<%$selfURL%>?mode=affxml">XML data format</a>
+                </UL>
+        </TD>
+</TR>
+</TABLE>
+
+
+
+<%include file="admin/admin.footer.php"%>
