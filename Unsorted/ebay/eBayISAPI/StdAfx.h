2c2870aa787eb45df6226979162dcab2be9cdbbf
me-suzy <me.suzana@gmail.com>
2025-08-13 03:13:31 +0300

    Add Unsorted/ebay/eBayISAPI/StdAfx.h

---
+++ Unsorted/ebay/eBayISAPI/StdAfx.h
@@ -0,0 +1,25 @@
+/*	$Id: StdAfx.h,v 1.4 1999/02/21 02:30:58 josh Exp $	*/
+#if !defined(AFX_STDAFX_H__6269792B_1DC8_11D1_B6FA_00A024D30D0B__INCLUDED_)
+#define AFX_STDAFX_H__6269792B_1DC8_11D1_B6FA_00A024D30D0B__INCLUDED_
+
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+#ifndef __AFX_H__
+#include <afx.h>
+#endif
+
+#include <afxwin.h>
+
+#ifndef __AFXMT_H__
+#include <afxmt.h>		// for synchronization objects
+#endif
+
+#include <afxext.h>
+#include <afxisapi.h>
+#pragma warning(disable: 4786)
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Developer Studio will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__6269792B_1DC8_11D1_B6FA_00A024D30D0B__INCLUDED)
