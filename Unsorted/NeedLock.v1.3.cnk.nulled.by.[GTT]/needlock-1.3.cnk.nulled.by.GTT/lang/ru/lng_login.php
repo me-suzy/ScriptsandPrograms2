64e1e009c9cbbc9f7930cde72f286cfdb1544b2e
me-suzy <me.suzana@gmail.com>
2025-08-13 06:39:13 +0300

    Add Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/ru/lng_login.php

---
+++ Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/ru/lng_login.php
@@ -0,0 +1,18 @@
+<?php
+
+$lang['login_page_subtitle']            =	'÷ÈÏÄ ÄÌÑ ÐÏÌØÚÏ×ÁÔÅÌÅÊ';
+$lang['submit_login']	                =	'÷ÏÊÔÉ';
+$lang['username']	                =	'éÍÑ ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['password']	                =	'ðÁÒÏÌØ';
+$lang['remember_login']	                =	'úÁÐÏÍÎÉÔØ ÄÁÎÎÙÅ?';
+$lang['success_text']                   =       '÷Ù ÕÓÐÅÛÎÏ ×ÏÛÌÉ × ÓÉÓÔÅÍÕ. ïÖÉÄÁÊÔÅ Á×ÔÏÍÁÔÉÞÅÓËÏÊ ÐÅÒÅÁÄÒÅÓÁÃÉÉ ...';
+$lang['logout_text']                    =       '÷Ù ÕÓÐÅÛÎÏ ×ÙÛÌÉ ÉÚ ÓÉÓÔÅÍÙ. ïÖÉÄÁÊÔÅ Á×ÔÏÍÁÔÉÞÅÓËÏÊ ÐÅÒÅÁÄÒÅÓÁÃÉÉ ...';
+$lang['lost_passwd_link_text']          =       'úÁÂÙÌÉ Ó×ÏÊ ÐÁÒÏÌØ?';
+$lang['register_link_text']             =       'úÁÒÅÇÉÓÔÒÉÒÏ×ÁÔØÓÑ?';
+$lang['lostpasswd_page_subtitle']       =       'óÉÓÔÅÍÁ ×ÏÓÓÔÁÎÏ×ÌÅÎÉÑ ÐÁÒÏÌÅÊ';
+$lang['submit_email']                   =       'ÎÁÐÏÍÎÉÔØ ÄÁÎÎÙÅ ÄÌÑ ×ÈÏÄÁ';
+$lang['lpwd_email']                     =       'ðÏÖÁÌÕÊÓÔÁ, ××ÅÄÉÔÅ ÷ÁÛ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎÎÙÊ email';
+$lang['pwd_email_sent']                 =       '÷ÁÛÉ ÄÁÎÎÙÅ ÄÌÑ ×ÈÏÄÁ ÏÔÐÒÁ×ÌÅÎÙ ÎÁ ÷ÁÛ email';
+$lang['procced_to_login_link_text']     =       '÷ÏÊÔÉ × ÓÉÓÔÅÍÕ';
+
+?>
