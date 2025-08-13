ed224120669b368c3a31fed5999fff3ef8554f40
me-suzy <me.suzana@gmail.com>
2025-08-13 04:12:41 +0300

    Add Unsorted/evoBook/lang/lang_english.php

---
+++ Unsorted/evoBook/lang/lang_english.php
@@ -0,0 +1,131 @@
+<?php
+$evoLANG['title_main'] = "Main"; // if no title is presented on a page, it will display this
+
+$evoLANG['loginok'] = "Welcome!";
+$evoLANG['location'] = "Location";
+$evoLANG['home'] = "Home";
+$evoLANG['welcomehome'] = "Welcome to the Control Panel, you can edit nearly every aspect of the site from here";
+$evoLANG['filedeleted'] = "File was successfully deleted";
+$evoLANG['filesuccess'] = "File Succesfully created/edited";
+$evoLANG['delfile'] = "Delete File?";
+$evoLANG['addfile'] = "Add New File";
+$evoLANG['file'] = "File";
+$evoLANG['s_slang'] = "Universal Language?";
+$evoLANG['s_defaultstyle'] = "Default Style";
+$evoLANG['d_defaultstyle'] = "Style used on the main topsite looks";
+$evoLANG['backupsystem'] = "Backup System";
+$evoLANG['backedup'] = "Backed Up Files";
+$evoLANG['none'] = "None";
+$evoLANG['logout'] = "Logout";
+
+$evoLANG['logintext'] = "Login";
+$evoLANG['xusername'] = "Invalid Username. Please try again.";
+$evoLANG['xpass'] = "Password is incorrect. Please Try again. Password are case-sensitive";
+$evoLANG['sesexpired'] = "Session Expired. Please re-login";
+$evoLANG['xid'] = "Invalid ID";
+
+$evoLANG['noid'] = "No ID was given so back off!";
+$evoLANG['xlogin'] = "Either you've been here for more than an hour or you didn't login.";
+$evoLANG['noperm'] = "You are not authorized to perform this action or visit this page.";
+$evoLANG['expired'] = "It seems your session has expired.";
+$evoLANG['pagegenerated'] = "Generated in (seconds):";
+$evoLANG['word_name'] = "Name";
+$evoLANG['word_option'] = "Option(s)";
+$evoLANG['word_done'] = "Done!";
+$evoLANG['word_yes'] = "yes";
+$evoLANG['word_no'] = "no";
+$evoLANG['word_welcome'] = "Welcome!";
+$evoLANG['word_folder'] = "Folder";
+$evoLANG['word_file'] = "File(s)";
+$evoLANG['created'] = "created";
+$evoLANG['word_setup'] = "setup";
+$evoLANG['word_total'] = "Total";
+$evoLANG['word_notrated'] = "Not Rated";
+$evoLANG['voted'] = "You Already Voted ";
+$evoLANG['thanksvote'] = "Thank you for voting";
+$evoLANG['printpage'] = "Print";
+$evoLANG['wrongpass'] = "Wrong Password and Username Match";
+$evoLANG['totalpage'] = "Total Page";
+$evoLANG['page'] = "Page";
+$evoLANG['remember'] = "Remember Me";
+$evoLANG['word_delete'] = "Delete";
+$evoLANG['button_go'] = "Go";
+$evoLANG['clear_comment'] = "Clear Comments";
+$evoLANG['word_edit'] = "Edit";
+$evoLANG['manage_members'] = "Manage Members";
+$evoLANG['export'] = "Export";
+$evoLANG['import'] = "Import";
+$evoLANG['styles_main'] = "Styles Management";
+$evoLANG['styles_edit'] = "Edit Style";
+$evoLANG['styles_edittpl'] = "Edit Template(s)";
+$evoLANG['styles_global'] = "Global Templates (Header & Footer)";
+$evoLANG['alloweddimension'] = "Max. Width / Height (in pixels)";
+$evoLANG['rate'] = "Rate";
+$evoLANG['rating'] = "Rating";
+$evoLANG['views'] = "Views";
+$evoLANG['styles_add'] = "Add New Style";
+$evoLANG['closed'] = "Closed";
+$evoLANG['main'] = "Main";
+$evoLANG['asc'] = "Ascending";
+$evoLANG['desc'] = "Descending";
+$evoLANG['in'] = "In";
+$evoLANG['alphabet'] = "Alphabet";
+$evoLANG['approveall'] = "Approve All";
+$evoLANG['approve'] = "Approve";
+$evoLANG['perpage'] = "Results Per Page";
+$evoLANG['disabled'] = "Disabled";
+$evoLANG['and'] = "And";
+
+// guestbook's
+$evoLANG['addentry'] = 'Add Entry';
+$evoLANG['editentry'] = 'Edit Entry';
+$evoLANG['country'] = 'Country';
+$evoLANG['email'] = 'Email';
+$evoLANG['website'] = 'Website';
+$evoLANG['optional'] = 'Optional';
+$evoLANG['msg'] = 'Message';
+$evoLANG['male'] = 'Male';
+$evoLANG['female'] = 'Female';
+$evoLANG['gender'] = 'Gender';
+$evoLANG['ym'] = 'Yahoo! Messenger ID';
+$evoLANG['icq'] = 'ICQ';
+$evoLANG['totalentries'] = 'Total Entries';
+$evoLANG['floodmsg'] = 'The Administrator has set that you can only add entry every '.$settings['floodcheck'].' seconds';
+$evoLANG['manageentries'] = 'Manage Entries';
+
+// smilies
+$evoLANG['addsmiley'] = 'Add Smiley';
+$evoLANG['s_before'] = 'Before';
+$evoLANG['s_before_d'] = 'i.e: <b>:)</b>';
+$evoLANG['s_after'] = 'After';
+$evoLANG['s_after_d'] = 'Smiley Image URL<br />Use {smiley_path} for smilies default folder path <br /><br /> i.e: {smiley_path}<b>smile.gif</b>';
+$evoLANG['isclick'] = 'Is Clickable?';
+$evoLANG['isclick_d'] = 'Clickable smilies will be listed on smiley box';
+$evoLANG['s_xavail'] = 'No Smilies Available';
+$evoLANG['managesmilies'] = 'Manage Smilies';
+$evoLANG['editsmiley'] = 'Edit Smiley';
+$evoLANG['smilies'] = 'Smilies';
+$evoLANG['moresmilies'] = 'More Smilies';
+$evoLANG['noshouts'] = 'No Shouts';
+$evoLANG['shoutbox'] = 'Shoutbox';
+$evoLANG['shout'] = 'Your Shout';
+$evoLANG['autobr'] = 'Auto BR';
+$evoLANG['htmlallowed'] = 'HTML Allowed';
+
+/*-------------------- month's name ---------------- */
+$evoLANG['months'] = array (
+							"1"  =>  "january",
+							"2"  =>  "february",
+							"3"  =>  "march",
+							"4"  =>  "april",
+							"5"  =>  "may",
+							"6"  =>  "june",
+							"7"  =>  "july",
+							"8"  =>  "august",
+							"9"  =>  "september",
+							"10"  =>  "october",
+							"11"  =>  "november",
+							"12"  =>  "december"
+						);
+
+?>
