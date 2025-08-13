f528a8a44920914388a8b0dc7239840666401dfb
me-suzy <me.suzana@gmail.com>
2025-08-13 06:40:02 +0300

    Add Unsorted/Nephp_Publisher_Enterprise_v3.04_nullified/NephpPublisher304n/plugins/admin_misc.php

---
+++ Unsorted/Nephp_Publisher_Enterprise_v3.04_nullified/NephpPublisher304n/plugins/admin_misc.php
@@ -0,0 +1,40 @@
+<?
+///////////////////////////////////////////////////////////////////////////////
+//      =   =       ====  =   = ====                                         //
+//      =   =       =   = =   = =   =                                        //
+//      ==  =  ===  =   = =   = =   =                                        //
+//      = = = =   = ====  ===== ====                                         //
+//      =  == ===== =     =   = =                                            //
+//      =   = =     =     =   = =                                            //
+//      =   =  ==== =     =   = =                                            //
+//      ------------------------------------------------------               //
+//      ====        =     ===     =         =                                //
+//      =   =       =       =               =                                //
+//      =   = =   = ====    =   ===    ==== ====   ===   ===                 //
+//      ====  =   = =   =   =     =   =     =   = =   = =   =                //
+//      =     =   = =   =   =     =    ===  =   = ===== =                    //
+//      =     =   = =   =   =     =       = =   = =     =                    //
+//      =      ===  ====  ===== ===== ====  =   =  ==== =                    //
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+// Program Name         : Nephp Publisher Enterprise                         //
+// Release Version      : 3.04                                               //
+// Program Author       : Kenny Ngo     (CTO of Nelogic Technologies.)       //
+// Program Author       : Ewdision Then (CEO of Nelogic Technologies.)       //
+// Retail Price         : $499.00 United States Dollars                      //
+// WebForum Price       : $000.00 Always 100% Free                           //
+// ForumRu Price        : $000.00 Always 100% Free                           //
+// xCGI Price           : $000.00 Always 100% Free                           //
+// Supplied by          : Scoons [WTN]                                       //
+// Nullified by         : CyKuH [WTN]                                        //
+// Distribution         : via WebForum, ForumRU and associated file dumps    //
+///////////////////////////////////////////////////////////////////////////////
+function admin_misc()
+{
+
+        global $gbl_env,$gbl_mnv,$gbl_microtime,$_cfig,$gbl_type;
+	$tpl_template_url=$_cfig[url_tpl];
+
+	Header("Location: /");
+}
+?>
