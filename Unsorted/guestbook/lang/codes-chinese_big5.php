7d2db3c9b6a6ed6dfb42eea5a0889265474b2a26
me-suzy <me.suzana@gmail.com>
2025-08-13 05:00:57 +0300

    Add Unsorted/guestbook/lang/codes-chinese_big5.php

---
+++ Unsorted/guestbook/lang/codes-chinese_big5.php
@@ -0,0 +1,142 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE>Smilies And AGCodes</TITLE>
+<META content="text/html; charset=big5" http-equiv=Content-Type>
+<style type="text/css">
+<!--
+td {  font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 8pt}
+-->
+</style>
+</HEAD>
+<BODY bgColor=#ffffff link=#000080 text=#000000 vLink=#000080>
+<CENTER>
+  <table width="95%" border="0" cellspacing="1" cellpadding="0">
+    <tr>
+      <td height="25">¤°»ò¬Oªí±¡²Å¸¹?</td>
+    </tr>
+    <tr>
+      <td>
+		<p>ªí±¡²Å¸¹¬O¥Ñ¤@­Ó¤p«¬ªº¹Ï®×¨Ó¥Nªí±¡ºü¡C°²¦p§A´¿¨Ï¥Î¹L¹q¤l¶l¥ó©Îºô¸ô²á¤Ñ¡A§AÀ³¸Ó
+		   «Ü¼ô±x¬Y¨Ç¯ºÁy²Å¸¹¡C¦b¦¹¯d¨¥¸Ì¡A§A¥i¥H¿é¤J¤@¨Ç¯S©w²Å¸¹¡A¦b±z°e¥X«á¥¦·|¦Û¦æÂà´«
+		   ¦¨ªí±¡¹Ï®×¡C</p>
+        <p>ªí±¡¹Ï®×¹ï·Óªí: </p>
+      </td>
+    </tr>
+  </table>
+  <table bgcolor=#f7f7f7 border=0 width="95%" cellspacing="1" cellpadding="4">
+    <tbody>
+    <tr>
+      <td bgcolor="#996699"><font color=#ffffff><b>²Å¸¹</b></font></td>
+      <td bgcolor="#996699"><font color=#ffffff><b>±¡ºü</b></font></td>
+      <td bgcolor="#996699"><font color=#ffffff><b>¹Ï§ÎÅã¥Ü</b></font></td>
+    </tr>
+
+<?php include ("./smilies.inc"); ?>
+
+    </tbody>
+  </table>
+  <br>
+  <table width="95%" border="0" cellspacing="1" cellpadding="0">
+    <tr>
+      <td height="25">¦ó¿×AGCode (ADVANDCED GUESTBOOK CODE)</td>
+    </tr>
+    <tr>
+      <td>
+        <p>AGCode¬O¤@ºØÂ²©öªº¼ÐÅÒ¨Ó¥N´ÀHTML½X¡AÅý±z¯à°÷«Ü¤è«Kªº¿é¤Jºô§}¡B¹Ï¤ù¡BEmailµ¥µ¥¡A
+		   §Y¨Ï¦bHTML½XÃö³¬®É¡A±z¤]¥i¥H»´©öªº¨Ï¥ÎAGCode¡C
+        <p>¥Ø«eªºAGCode:
+      </td>
+    </tr>
+  </table>
+<TABLE border=0 cellPadding=0 cellSpacing=0 width="95%" align="center">
+  <TBODY>
+  <TR>
+    <TD bgColor=#000000>
+      <TABLE border=0 cellPadding=4 cellSpacing=1 width="100%">
+        <TBODY>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">URL ºô§}Ãìµ²</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>°²¦pAGCode¬O¶}±Òªº¡A§A¥i¥H¨Ï¥Î¥¦¨Ó¥Nªíºô§}Ãìµ²
+            <UL>
+              <LI><font color="#800000">http://www.yourURL.com </font>
+              <LI><FONT color=#800000>www.yourURL.com </FONT>ª`·N ¦pªGºô§}¤£¬Owww¶}ÀYªº¡A
+			    §A¥²¶·¥[¤J http:// ¨Ó§¹¦¨¡A¦pªGºô§}¬Owww¶}ÀYªº¡A¦b±z°e¥X¯d¨¥«á¡A¨t²Î·|¦Û¦æ
+				¥[¤Jhttp://¡Ahttps©Mftp¤]¬O¦P¼Ëªº¤è¦¡¡C<BR>
+                <BR>
+              <LI>§A¥i¥H¨Ï¥Î¤U¦Cªº¤è¦¡: <br><br>
+                <CENTER>
+                  <FONT color=#ff0000>[url=http://www.proxy2.de]</FONT>ºô§}<FONT color=#ff0000>[/url]</FONT>
+                </CENTER><br><br>
+              <LI>
+                <P>¤U¦C³oºØ¤è¦¡¤]¬O¦³®Äªº
+                <P>
+                  <CENTER>
+                    <FONT color=#ff0000>[url]</FONT>http://www.proxy2.de<FONT color=#ff0000>[/url]</FONT>
+                  </CENTER>
+                <P>¦b¤W­zªº¨Ò¤l¸Ì¡AAGCode·|¦Û¦æÂà´«¬°¶W³sµ²¡A«ö¤U¦¹³sµ²«h·|¥H¶}±Ò·sµøµ¡®i¥Ü¡C</P>
+              </LI>
+            </UL>
+          </TD>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">EmailÃìµ²</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>­n¥[¤JEmail¥i¥H«ö·Ó¤U¦C¤è¦¡¡C
+            <P>
+              <CENTER>
+                <FONT color=#ff0000>[email]</FONT>webmaster@proxy2.de<FONT color=#ff0000>[/email]</FONT>
+              </CENTER>
+			<P>¬õ¦â¦rÅé¬°AGCode</P>
+          </TD>
+        </TR>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">²ÊÅé©M±×Åé</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>¨Ï¥Î²ÊÅé¥i¿é¤J[b] [/b]¼ÐÅÒ¡A¨Ï¥Î±×Åé¥i¿é¤J[i] [/i]¼ÐÅÒ¡C
+            <P>
+              <CENTER>
+                Hello, <FONT color=#ff0000>[b]</FONT><B>John</B><FONT color=#ff0000>[/b]</FONT><BR><br>
+                Hello, <FONT color=#ff0000>[i]</FONT><I>Maria</I><font color="#FF0000">[i]</font>
+              </CENTER>
+			<P>¬õ¦â¦rÅé¬°AGCode</P>
+          </TD>
+        </TR>
+        <TR bgColor=#0099CC>
+          <TD><b><font color="#FFFFFF">¥[¤J¹Ï¤ù</font></b></TD>
+        </TR>
+        <TR bgColor=#ffffff>
+          <TD>­Y­n¥[¤J¹Ï¤ù¥i°Ñ¦Ò¤U¦C¤è¦¡¡C
+            <P>
+              <CENTER>
+                <FONT color=#ff0000>[img]</FONT>http://www.yourURL.com/image/logo.gif<FONT color=#ff0000>[/img]</FONT>
+              </CENTER>
+            <P>¬õ¦â¦rÅé¬°AGCode</P>
+          </TD>
+        </TR>
+        </TBODY>
+      </TABLE>
+    </TD>
+  </TR>
+  </TBODY>
+</TABLE>
+<table width="95%" border="0" cellspacing="1" cellpadding="4" align="center">
+  <tr>
+    <td><font color=#800000>µù</font><br>
+      ¨Ï¥ÎAGCode®É¤£¥i¥H¨Ï¥ÎHTML½X°µµ²§À¡C¥t¥~
+      AGCode¬O¤£¤À¤j¤p¼gªº (¦]¦¹§A¥i¥H¨Ï¥Î <font color=#ff0000>[URL]</font>
+      or <font color=#ff0000>[url]</font>).<br><br>
+      <font color="#800000">¤£¥¿½Tªº¨Ï¥Î¤è¦¡:</font> <br>
+       <font color="#ff0000">[url]</font> www.proxy2.de <font color=#ff0000>[/url]</font> - ½Ð¤Å¦bAGCode¶¡´¡¤JªÅ¥Õ¡C<br>
+        <br>
+        <font color="#ff0000">[email]</font>webmaster@proxy2.de<font color=#ff0000>[email]</font> - AGCodeªºµ²§À»Ý¥[¤J±×½u "/" (<font color=#ff0000>[/email]</font>)
+    </td>
+  </tr>
+</table>
+</CENTER>
+<BR>
+</BODY>
+</HTML>
