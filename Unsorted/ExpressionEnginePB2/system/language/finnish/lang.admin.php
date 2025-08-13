83d314ac167b10e77eb6c38cd7ad6d511e2831a3
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:47 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.admin.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.admin.php
@@ -0,0 +1,863 @@
+<?php
+$L = array(
+
+
+"word_separator" =>
+"Word Separator for URL Titles",
+
+"dash" =>
+"Dash",
+
+"underscore" =>
+"Underscore",
+
+"site_name" =>
+"Sivujesi nimi",
+
+"system_admin" =>
+"Järjestelmänvalvoja",
+
+"system_preferences" =>
+"Järjestelmän ominaisuudet",
+
+"is_system_on" =>
+"Onko syteemi päällä?",
+
+"is_system_on_explanation" =>
+"Jos systeemi on pois päältä, vain pääkäyttäjä voi nähdä sivut",
+
+"system_off_msg" =>
+"Systeemi suljettu- viesti",
+
+"offline_template" =>
+"Systeemi Offline sivupohja",
+
+"offline_template_desc" =>
+"Tämä sivupohja sisältää sivun joka näytetään, kun sivusi on offline-tilassa. ",
+
+"template_updated" =>
+"Sivupohja päivitetty",
+
+"preference_information" =>
+"Käyttöohje",
+
+"preference" =>
+"Ominaisuus",
+
+"value" =>
+"Arvo",
+
+"general_cfg" =>
+"Yleiset ominaisuudet",
+
+"enable_db_caching" =>
+"Salli SQL kyselyn välimuistitallennus",
+
+"db_cache_refresh" =>
+"Kysely välimuistin rajoitus",
+
+"db_cache_refresh_exp" =>
+"&lt;b&gt;Päivien määrä&lt;/b&gt; kuinka kauan  välimuistin tietoja säilytetään mikäli tietoja ei ole päivitetty.  30 päivää on hyvä perusasetus.",
+
+"member_cfg" =>
+"Jäsenyyden ominaisuudet",
+
+"profile_templates" =>
+"Jäsenen Profiili sivupohjat",
+
+"allow_member_registration" =>
+"Salli uuden jäsenen rekisteröinti?",
+
+"req_mbr_activation" =>
+"Vaadi jäsenen tilin aktivointi?",
+
+"no_activation" =>
+"Aktivointia ei vaadita",
+
+"email_activation" =>
+"Automaattinen aktivointi sähköpostin välityksellä",
+
+"manual_activation" =>
+"Manuaalinen järjestelmänvalvojan aktivointi",
+
+"require_terms_of_service" =>
+"Vaadi palvelun ehdot",
+
+"member_theme" =>
+"Jäsenen Profiilin teema",
+
+"member_theme_exp" =>
+"Määrittää mitä teemoja käytetään eri jäsenyyden sivuilla: Login, rekisteräityminen, profiili, jne.",
+
+"member_images" =>
+"Polku jäsenen kuviin",
+
+"member_images_exp" =>
+"Tämä polku on kansioon, joka sisältää kuvat, joita käytetään jäsenten profiili sivuilla.",
+
+"require_terms_of_service_exp" =>
+"Asetus &quot;kyllä&quot; pakottaa käyttäjän &quot;hyväksymään ehdot&quot; rekisteröitymisessä. ",
+
+"new_member_notification" =>
+"Ilmoita järjestelmänvalvojalle uudesta rekisteröinnistä?",
+
+"mbr_notification_emails" =>
+"Ilmoituksen email-osoite(et)",
+
+"separate_emails" =>
+"Erota emailit pilkulla",
+
+"email_console_timelock" =>
+"Email Konsolin aikalukko",
+
+"email_console_timelock_exp" =>
+"Minuuttien määrä jonka tulee kulua, että jäsen voi lähettä uuden emailviesti. Huom. tämä kohdistuu vain Email Konsoliin jäsenen profiiili sivulla.",
+
+"log_email_console_msgs" =>
+"Kirjaa lokii Email Konsolin viestit",
+
+"log_email_console_msgs_exp" =>
+"Tämä ominaisuus mahdollistaa sinun kirjata lokiin kaikki viestit, jotka lähetetään Email Konsolin kautta jäsenten profiili sivuilta",
+
+"default_member_group" =>
+"Uuden jäsenen oletus ryhmä",
+
+"group_assignment_defaults_to_two" =>
+"Jos vaadit tilin aktivoinnin, jäsenet asetetaan täksi kun he ovat aktivoituneet.",
+
+"view_email_logs" =>
+"Email Konsolin Lokit",
+
+"user_session_type" =>
+"Käyttäjäistunnon tyyppi",
+
+"admin_session_type" =>
+"Istunnon tyypin ohjauspaneeli",
+
+"security_cfg" =>
+"Turvallisuuden ja istunnon asetukset",
+
+"un_min_len" =>
+"Minimi käyttäjätunnuksen pituus",
+
+"pw_min_len" =>
+"Minimi salasanan pituus",
+
+"cs_session" =>
+"Evästeet ja istunnontunnus",
+
+"c_session" =>
+"Vain evästeet",
+
+"s_session" =>
+"Vain istunnontunnus",
+
+"secure_forms" =>
+"Käsittele lomakkeen data suojatussa moodissa?",
+
+"deny_duplicate_data" =>
+"Estä duplikaatti data?",
+
+"deny_duplicate_data_explanation" =>
+"Tämä valinta estää jäsenten  lähettämän datan (Kommentit, trackback, jne.) vastaanottamisen, jos tarkka dublikaatti datasta on jo olemassa.",
+
+"secure_forms_explanation" =>
+"Estää automaattisen spamin ja usean tahattoman lähetyksen.",
+
+"allow_multi_logins" =>
+"Salli useampi kirjautuminen yhdeltä tililtä?",
+
+"allow_multi_logins_explanation" =>
+"Määrittää jos useampi kuin yksi henkilö voi samanaikaisesti päästä järjestelmää käyttämällä samaa käyttäjätiliä. Huom: jos istunnontyyppi yllä on asetettu &quot;vain evästeet&quot; tämä ominaisuus ei toimi.",
+
+"password_lockout" =>
+"Salli salasanan aikalukko?",
+
+"password_lockout_explanation" =>
+"Jos sallittu, vain neljä väärää kirjautumista sallitaan aikavälillä joka on alla. Tämä on suoja hakkereille, jotka yrittävät arvata huonosti valitun salasanan.",
+
+"password_lockout_interval" =>
+"Aikaintervalli aikalukolle",
+
+"login_interval_explanation" =>
+"Aika minuuteissa. Voit käyttää desimaaleja. Esim:  1.5",
+
+"require_ip_for_login" =>
+"Vaadi IP-osoite ja käyttäjäntunniste kirjautumiseen",
+
+"require_ip_explanation" =>
+"Estää käyttäjiä kirjautumasta ellei heidän selain lähetä IP-osoitetta ja käyttäjän tunnistetietoa. Tämä estää hakkereiden kirjautumisen käytäämällä suoraa yhteyttä.",
+
+"allow_multi_emails" =>
+"Salli useampi tili samalle email-osoitteelle",
+
+"allow_username_change" =>
+"Salli jäsenien muuttaa käyttäjätunnusta?",
+
+"require_secure_passwords" =>
+"Vaadi suojattu salasana?",
+
+"secure_passwords_explanation" =>
+"Käyttäjien tulee valita salasanaan ainakin yksi isokirjain, yksi pienikirjain ja numero",
+
+"allow_dictionary_pw" =>
+"Salli sanakirjan sana salasanaksi?",
+
+"real_word_explanation" =>
+"Tämä asetus estää käyttäjiä käyttämästä tunnusta, joka sisältyy salasanaan.",
+
+"dictionary_note" =>
+"Huom: Käyttääksesi tätä ominaisuutta sinun tulee asentaa sanakirjatiedosto. Katso lisää manuaalista.",
+
+"name_of_dictionary_file" =>
+"Sanakirjan nimi",
+
+"dictionary_explanation" =>
+"Tiedoston nimi, joka sisältää sanalistasi",
+
+"image_path" =>
+"Polku kuvakansioon",
+
+"cp_url" =>
+"URL ohjauspaneelin index-sivulle",
+
+"with_trailing_slash" =>
+"kauttaviivalla",
+
+"site_url" =>
+"URL sivujesi juureen",
+
+"url_explanation" =>
+"Tämä kansio sisältää sivujesi index-sivun.",
+
+"doc_url" =>
+"URL Dokumentointi kansioon",
+
+"doc_url_explanation" =>
+"Vain juuri kansio, kauttaviivalla",
+
+"site_index" =>
+"Sivujesi nimi",
+
+"system_path" =>
+"Absoluuttinen polku %x kansioon",
+
+"force_query_string" =>
+"Pakota URL:än kysymysmerkki",
+
+"safe_mode" =>
+"Toimiiko palvelimesi PHP Safe Modessa?",
+
+"force_query_string_explanation" =>
+"Tämä on turvallinen mekanismi palvelimille, jotka eivät tue PATH_INFO muuttujaa.",
+
+"debug" =>
+"Virheen korjaus ominaisuudet",
+
+"debug_explanation" =>
+"Sallii virheilmoitusten näyttämisen, joka on arvokas tieto sivuja kehitettäessä",
+
+"show_queries" =>
+"Näytä SQL kyselyt?",
+
+"show_queries_explanation" =>
+"Jos sallittu, pääkäyttäjä näkee kaikki SQL kyselyt selaimen alaosassa. Käyttökelpoinen virheen etsinnässä.",
+
+"debug_zero" =>
+"0: PHP/SQL error viestejä ei tuotetta",
+
+"debug_one" =>
+"1: PHP/SQL error viesti näytetään vain pääkäyttäjälle",
+
+"debug_two" =>
+"2: PHP/SQL error viestit näkee kuka tahansa-EI TURVALLINEN",
+
+"deft_lang" =>
+"Oletus kieli",
+
+"xml_lang" =>
+"Oletus XML kieli",
+
+"used_in_meta_tags" =>
+"Käytetään ohjauspaneelin mata tageissa",
+
+"charset" =>
+"Oletus kirjasin asetus",
+
+"gzip_output" =>
+"Salli GZIP tuotos?",
+
+"gzip_output_explanation" =>
+"Kun sallittu, sivusi näytetään pakatussa muodossa nopeamman lataamisen mahdollistamiseksi",
+
+"send_headers" =>
+"Tuota HTTP Sivun osikko?",
+
+"redirect_method" =>
+"Uudelleenohjaus menetelmä",
+
+"location_method" =>
+"Sijainti (nopeampi)",
+
+"refresh_method" =>
+"Päivitä (Windows serveri)",
+
+"localization_cfg" =>
+"Sijainti asetukset",
+
+"time_format" =>
+"Ajan muotoilu",
+
+"united_states" =>
+"Amerikkalainen",
+
+"european" =>
+"Eurooppalainen",
+
+"server_timezone" =>
+"Serverin aikavyöhyke",
+
+"server_offset" =>
+"Serverin Offset (minuuteissa)",
+
+"server_offset_explain" =>
+"Käytä miinusmerkkiä osoittamaan vähennystä minuuteissa: -15",
+
+"daylight_savings" =>
+"Päivänkoiton tallennus aika",
+
+"cookie_cfg" =>
+"Eväste asetukset",
+
+"cookie_domain" =>
+"Evästeen domain",
+
+"cookie_domain_explanation" =>
+"Käytä .sinundomain.com sivujesi evästeisiin (huom. piste alussa)",
+
+"cookie_prefix" =>
+"Evästeen etuliite",
+
+"cookie_prefix_explain" =>
+"Käytä tätä vain, jos olet asentanut useamman version tästä ohjelmasta",
+
+"cookie_path" =>
+"Evasteen polku",
+
+"cookie_path_explain" =>
+"Käytä vain, jos vaadit tiettyä serverin polkua evästelle",
+
+"image_cfg" =>
+"Kuvien ominaisuudet ",
+
+"enable_image_resizing" =>
+"Salli kuvien ko&#39;on muutos",
+
+"enable_image_resizing_exp" =>
+"Kun sallittu, voit tehdä pikkukuvia (thumbnails) ladatessasi kuvia viesteihisi",
+
+"image_resize_protocol" =>
+"Kuvien muokkaus protokolla",
+
+"image_resize_protocol_exp" =>
+"Tarkista palveluntarjoajalta, että serverisi tukee valittua protokollaa",
+
+"image_library_path" =>
+"Kuvakokoelman polku",
+
+"image_library_path_exp" =>
+"Jos valitset ImageMagick:n tai NetPBM:n tulee sinun määritellä serverin polku kuvakokoelmaan",
+
+"gd" =>
+"GD",
+
+"gd2" =>
+"GD 2",
+
+"netpbm" =>
+"NetPBM",
+
+"imagemagick" =>
+"ImageMagik",
+
+"thumbnail_prefix" =>
+"Pikkukuvien tunniste",
+
+"thumbnail_prefix_exp" =>
+"Tunnistetta käytetään tehtäessä/nimettäessä pikkukuvia. Esim: kuva_pieni.jpg",
+
+"email_cfg" =>
+"Email Asetukset",
+
+"mail_protocol" =>
+"Email Protokolla",
+
+"smtp_server" =>
+"SMTP palvelimen osoite",
+
+"smtp_username" =>
+"SMTP käyttäjätunnus",
+
+"smtp_password" =>
+"SMTP salasane",
+
+"only_if_smpte_chosen" =>
+"Käytä tätä vain, jos valitsit SMTP protokollan",
+
+"email_batchmode" =>
+"Käytä eräajo-moodia?",
+
+"batchmode_explanation" =>
+"Eräajo-moodi pilkkoo suuret viestien määrät pienempiin ryhmiin, jotka lähetetään osissa.  Suositeltavaa käyttää tätä, jos sivusi ovat jaetulla palvelutilillä.
+",
+
+"email_batch_size" =>
+"Viestien määrä / eräajo",
+
+"batch_size_explanation" =>
+"Normaalisti 300 on turvallinen arvo",
+
+"webmaster_email" =>
+"Vastausosoite automaattisesti tuotetuille emaileille",
+
+"return_email_explanation" =>
+"Jos jätät tämän tyhjäksi, monet email serverit tulkitsevat viestisi spamiksi",
+
+"php_mail" =>
+"PHP Mail",
+
+"sendmail" =>
+"Sendmail",
+
+"smtp" =>
+"SMTP",
+
+"plain_text" =>
+"Vain teksti",
+
+"html" =>
+"HTML",
+
+"mail_format" =>
+"Postin oletusarvo",
+
+"word_wrap" =>
+"Salli sanankierrätys oletuksena?",
+
+"cp_theme" =>
+"Ohjauspanelin oletus teema",
+
+"template_cfg" =>
+"Sivupohjan asetukset",
+
+"save_tmpl_revisions" =>
+"Tallenna sivupohjan muotoilu oletuksena",
+
+"censoring_cfg" =>
+"Sanojen sensuuri",
+
+"enable_censoring" =>
+"Salli sanojen sensurointi?",
+
+"censored_words" =>
+"Sensuroidut sanat",
+
+"censored_explanation" =>
+"Kukin sana omalle riville",
+
+"emoticon_cfg" =>
+"Smiley-ikonien ominaisuudet",
+
+"enable_emoticons" =>
+"Näytä Smileyt?",
+
+"emoticon_path" =>
+"URL smiley kansioon",
+
+"referrer_cfg" =>
+"Viitausten ominaisuudet",
+
+"log_referrers" =>
+"Salli viittausten jäljitys?",
+
+"log_referrers_explanation" =>
+"Kun sallittu, yksi lisä tietokanna kysely suoritetaan sivua ladattaessa",
+
+"weblog_administration" =>
+"Weblogien hallinta",
+
+"weblog_management" =>
+"Weblogien muokkaus",
+
+"field_management" =>
+"Kustomoidut weblogien kentät ",
+
+"file_upload_prefs" =>
+"Tiedostojen latauksen ominaisuudet",
+
+"categories" =>
+"Kategorioiden muokkaus",
+
+"default_ping_servers" =>
+"Oletus Ping serveri",
+
+"status_management" =>
+"Muokka viestin tilaa",
+
+"edit_preferences" =>
+"Muokka ominaisuuksia",
+
+"preferences_updated" =>
+"Ominaisuudet tallennettu",
+
+"edit_groups" =>
+"Muokkaa ryhmiä",
+
+"members_and_groups" =>
+"Jäsenet ja ryhmät",
+
+"view_members" =>
+"Tarkastele jäseniä",
+
+"member_validation" =>
+"Aktivoi jäsenet, joiden rekisteröinti on vireillä",
+
+"register_member" =>
+"Uuden jäsenen rekisteröinti",
+
+"member_search" =>
+"Etsi jäseniä",
+
+"user_banning" =>
+"Käyttäjän blokkaus (banning)",
+
+"custom_profile_fields" =>
+"Muokkaa profiili kenttiä",
+
+"email_notification_template" =>
+"Email ilmoituksen sivupohja",
+
+"member_groups" =>
+"Jäsenten ryhmät",
+
+"utilities" =>
+"Muut",
+
+"view_log_files" =>
+"Tarkatele lokitietoja",
+
+"clear_caching" =>
+"Tyhjennä välimuistin datatiedot",
+
+"page_caching" =>
+"Sivun (template) välimuistitiedostot",
+
+"tag_caching" =>
+"Tagien välimuistitiedostot",
+
+"db_caching" =>
+"Tietokannan välimuistitiedostot",
+
+"all_caching" =>
+"Kaikki välimuistitiedostot ",
+
+"cache_deleted" =>
+"Välimuisti on tyhjennetty",
+
+"php_info" =>
+"PHP Info",
+
+"sql_manager" =>
+"SQL hallinta",
+
+"sql_info" =>
+"SQL Info",
+
+"sql_utilities" =>
+"SQL arvot",
+
+"database_type" =>
+"Tietokanna tyyppi",
+
+"sql_version" =>
+"Tietokanna versio",
+
+"database_size" =>
+"Tietokanna koko",
+
+"database_uptime" =>
+"Tietokanna käyntiaika (uptime)",
+
+"total_queries" =>
+"Hakuja tietokannan käynnistyksen jälkeen",
+
+"sql_status" =>
+"Status Info",
+
+"sql_system_vars" =>
+"Systemin muuttujat",
+
+"sql_processlist" =>
+"Prosessi lista",
+
+"sql_query" =>
+"Tietokannan kyselylomake",
+
+"query_result" =>
+"Kyselyn tulokset",
+
+"browse" =>
+"Selaa",
+
+"tables" =>
+"Taulut",
+
+"table_name" =>
+"Taulun nimi",
+
+"records" =>
+"Tietokannan tietueet",
+
+"size" =>
+"Koko",
+
+"type" =>
+"Tyyppi",
+
+"analize" =>
+"Analysoi tauluja",
+
+"optimize" =>
+"Optimoi SQL taulut",
+
+"repair" =>
+"Korjaa SQL taulut",
+
+"optimize_table" =>
+"Optimoi valitut taulut",
+
+"repair_table" =>
+"Korjaa valitut taulut",
+
+"view_table_sql" =>
+"Näytä SQL:n rakenne ja data",
+
+"backup_tables_file" =>
+"Varmuuskopioi valitut taulut - tekstitiedosto",
+
+"backup_tables_zip" =>
+"Varmuuskopioi valitut taulut - Zip-tiedosto",
+
+"backup_tables_gzip" =>
+"Varmuuskopioi valitut taulut - Gzip-tiedosto",
+
+"select_all" =>
+"Valitse kaikki",
+
+"no_buttons_selected" =>
+"Sinun tulee valita taulut joihin toimita kohdistetaan",
+
+"unsupported_compression" =>
+"PHP:n asetukset eivät tue tätä pakkausmenetelmää",
+
+"backup_info" =>
+"Käytä tätä muotoa tietokannan varmuuskopiointiin",
+
+"save_as_file" =>
+"Tallenna SQL varmuuskopio",
+
+"view_in_browser" =>
+"Katsele SQL varmuuskopioita selaimella",
+
+"sql_query_instructions" =>
+"Käytä tätä muotoa tehdäksesi SQL kysely",
+
+"file_type" =>
+"Tiedosto tyyppi:",
+
+"zip" =>
+"Zip",
+
+"gzip" =>
+"Gzip",
+
+"advanced_users_only" =>
+"Vain edistyneille käyttäjille",
+
+"recount_stats" =>
+"Tarkista tilastot",
+
+"preference_updated" =>
+"Ominaisuudet päivitetty",
+
+"click_to_recount" =>
+"Paina laskeaksesi rivit %x  %y:n",
+
+"items_remaining" =>
+"Tallenteita jäljellä:",
+
+"recount_completed" =>
+"Tallenne valmis",
+
+"return_to_recount_overview" =>
+"Palaa tarkistuslaskennan pääsivulle",
+
+"recounting" =>
+"Tarkistuslaskenta",
+
+"recount_info" =>
+"Linkit alla mahdollistaa sinun päivittää eri tilastoja, kuten kuinka monta viestiä kukin jäsen on lähettänyt",
+
+"source" =>
+"Lähde",
+
+"total_records" =>
+"Tallenteita kaikkiaan:",
+
+"recalculate" =>
+"Tarkista tilastot",
+
+"do_recount" =>
+"Suorita tarkistus",
+
+"set_recount_prefs" =>
+"Tarkista ominaisuudet",
+
+"recount_instructions" =>
+"Käsiteltävien tietokannan rivien määrä per eräajo",
+
+"recount_instructions_cont" =>
+"Estääksesi serverin timeoutin, laskemme tilastot erissä. 1000 on turvallinen luku useimmille servereille. Jos käytät korkealuokkaista serveriä tai omistat sen voit nostaa lukemaa.",
+
+"exp_members" =>
+"Jäsenet",
+
+"exp_weblog_titles" =>
+"Weblogin viestit",
+
+"search_and_replace" =>
+"Etsi ja korvaa",
+
+"data_pruning" =>
+"Datan karsinta",
+
+"sandr_instructions" =>
+"Nämä lomakkeet mahdollistavat sinun etsiä tiettyä tekstiä ja korvata se toisella tekstillä",
+
+"search_term" =>
+"Etsi tämä teksti",
+
+"replace_term" =>
+"Ja korvaa se tällä tekstillä",
+
+"replace_where" =>
+"Mille tietokannan kentälle haluat korvaustoiminteen tapahtuvan?",
+
+"search_replace_disclaimer" =>
+"Käyttämästäsi syntaksista riippuen, tämä toiminto voi tuottaa ei toivottuja tuloksia. Lue lisää manuaalista.",
+
+"title" =>
+"Otsikko",
+
+"weblog_entry_title" =>
+"Weblogin viestin otsikot",
+
+"weblog_fields" =>
+"Weblogin kentät:",
+
+"templates" =>
+"Sivupohjat",
+
+"rows_replaced" =>
+"Tietokanna tietueiden määrä mille korvaustoiminne suoritettiin:",
+
+"view_database" =>
+"Tietokanna taulujen hallinta",
+
+"sql_backup" =>
+"Tietokannan varmuuskopiointi",
+
+"sql_no_result" =>
+"Tämä kysely ei tuottanut tulosta",
+
+"sql_not_allowed" =>
+"Sorry, mutta tämä ei ole sallittu kyselytyyppi",
+
+"site_statistics" =>
+"Sivujen tilastot",
+
+"translation_tool" =>
+"Käännösohjelma",
+
+"translation_dir_unwritable" =>
+"Varoitus: Sinun käännösten kansio ei ole kirjoitettavissa",
+
+"please_set_permissions" =>
+"Aseta seuraaville kansioille oikeudeksi 666 tai 777: ",
+
+"choose_translation_file" =>
+"Valitse käännettävä tiedosto",
+
+"core_language_files" =>
+"Ytimen kielitiedostot:",
+
+"module_language_files" =>
+"Moduulien kielitiedostot:",
+
+"file_saved" =>
+"Tiedosto on tallennettu",
+
+"default_html_buttons" =>
+"Oletus HTML painikkeet",
+
+"import_from_pm" =>
+"pMachine tuonti(import)ohjelma",
+
+"import_from_mt" =>
+"Movable Type tuonti(import)ohjelma",
+
+"specialty_templates" =>
+"Erikoissivupohjat",
+
+"user_messages_template" =>
+"Käyttäjän viestisivupohja",
+
+"plugin_manager" =>
+"Plugin hallinta",
+
+"installed_plugins" =>
+"Asennetut pluginit",
+
+"no_plugins_exist" =>
+"Tällä hetkellä ei ole asennettuja plugineja",
+
+"view_info" =>
+"Näytä info",
+
+"plugin_information" =>
+"Plugin tiedot",
+
+"pi_name" =>
+"Nimi",
+
+"pi_author" =>
+"Tekijä",
+
+"pi_version" =>
+"Versio",
+
+"pi_author_url" =>
+"Tekijän  URL",
+
+"pi_description" =>
+"Kuvaus",
+
+"pi_usage" =>
+"Käyttö",
+
+"no_additional_info" =>
+"Tästä pluginista ei ole lisätietoja",
+
+''=>''
+);
+?>
