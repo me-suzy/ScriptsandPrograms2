d1d21323daeb91289bd0b052ac1a980c140947e6
me-suzy <me.suzana@gmail.com>
2025-08-13 04:38:08 +0300

    Add Unsorted/fet_404_php/files/mainset.php

---
+++ Unsorted/fet_404_php/files/mainset.php
@@ -0,0 +1,18 @@
+<?php
+
+// Path to /cgi-bin/fet catalog
+$fet_path="FET_CJ_PATH";
+
+// Path to the main logs catalog
+$log_path="FET_CJ_LOG_PATH";
+
+// Site Domain, without WWW
+$fet_domain="FET_CJ_DOMAIN";
+
+// Default Url to send traffic to in case of effors or overclicking activity
+$def_url="FET_CJ_DEFAULT_URL";
+
+// Path to the HTML files catalog
+$html_path="FET_CJ_HTML_PATH";
+
+?>
