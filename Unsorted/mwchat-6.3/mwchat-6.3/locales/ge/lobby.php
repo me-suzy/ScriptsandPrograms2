34465dd6e416ab85dfed1b229efcc15badc95452
me-suzy <me.suzana@gmail.com>
2025-08-13 06:32:24 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/lobby.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/lobby.php
@@ -0,0 +1,37 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[LOBBY_Entered_Room] = "Hat den Raum betreten.";
+
+$LOCALE[LOBBY_Room_Full] = "Der Raum, den Sie verlangten, ist voll.";
+
+$LOCALE[LOBBY_Room_Title] = "Sie sind jetzt in der Lobby";
+
+$LOCALE[LOBBY_Visitors] = "Besucher";
+
+$LOCALE[LOBBY_Public_List] = "Öffentliche Raumliste";
+
+$LOCALE[LOBBY_Private_List] = "Private Raumliste";
+
+$LOCALE[LOBBY_Create_Public] = "Erstellen Sie einen neuen öffentlichen Raum ";
+
+$LOCALE[LOBBY_Create_Private] = "Erstellen Sie einen neuen privaten Raum";
+
+$LOCALE[LOBBY_Button] = "Betreten Sie oder erstellen Sie einen Raum ";
+
+$LOCALE[LOBBY_Room_Password] = "Raum Passwort";
+
+$LOCALE[LOBBY_Hidden] = "Versteckt";
+
+$LOCALE[LOBBY_Password_Incorrect] = "Das Passwort, das Sie für den privaten Raum eingegeben haben, ist falsch .";
+
+?>
