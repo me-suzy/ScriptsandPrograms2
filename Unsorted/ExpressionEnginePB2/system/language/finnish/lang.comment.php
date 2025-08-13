c5fbfa71d60753f6528ea3ec29f66a69034bae35
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:49 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.comment.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.comment.php
@@ -0,0 +1,85 @@
+<?php
+$L = array(
+
+
+"comment_module_name" =>
+"Kommentti",
+
+"comment_module_description" =>
+"Käyttäjien kommentointi systeemi",
+
+"cmt_name_not_allowed" =>
+"The name you are posting with is reserved.  Please choose a different name.",
+
+"cmt_must_be_member" =>
+"Sinun tulee olla rekisteröitynyt käyttäjä voidaksesi kommentoida",
+
+"cmt_no_authorized_for_comments" =>
+"Sinulla ei ole oikeutta kommentoida viestejä",
+
+"cmt_account_not_active" =>
+"Jäsenyystiliäsi ei ole aktivoitu vielä",
+
+"cmt_comments_not_allowed" =>
+"Tällä weblogilla eivät kommentit ole sallittuja",
+
+"cmt_missing_name" =>
+"Nimi kettä on pakollinen",
+
+"cmt_missing_email" =>
+"Email on pakollinen",
+
+"cmt_invalid_email" =>
+"Antamasi email osoite on ei kelpaa",
+
+"cmt_banned_email" =>
+"Antamasi email osoite on blokattu",
+
+"cmt_too_large" =>
+"Antamasi kommentti sisälsi %n merkkiä. Vain %x merkkiä on sallittu.",
+
+"cmt_comments_timelock" =>
+"Voi kommentoida vain joka %s sekunti.",
+
+"cmt_duplicate_comment_warning" =>
+"Kommenttiasi ei voitu vastaanottaa tallä kertaa.",
+
+"cmt_no_preview_template_specified" =>
+"Esikatselun sivupohjassa ei ole komettilomakkeen tagia",
+
+"cmt_comment_response_title" =>
+"Joku juuri vastasi komenttiisi",
+
+"cmt_comment_response_body" =>
+"Joku juuri vastasi kirjoittamaasi viestiin: ",
+
+"cmt_comment_title" =>
+"Viestin otsikko on:",
+
+"cmt_comment_url" =>
+"Voit nähdä kommentin seuraavassa osoitteessa:",
+
+"cmt_comment_removal" =>
+"Jos et halua enää ilmoituksia tästä kommentista, klikkaa tästä:",
+
+"cmt_comment_removal_all" =>
+"Lopeta kaikki ilmoitukset, klikkaa tästä:",
+
+"cmt_notification_removal" =>
+"Ilmoituksen poisto",
+
+"cmt_you_have_been_removed" =>
+"Sinulle ei enää tule ilmoituksia.",
+
+"cmt_comment_accepted" =>
+"Comment Accepted",
+
+"cmt_will_be_reviewed" =>
+"Your comment will be reviewed by a moderator for approval.",
+
+"cmt_return_to_comments" =>
+"Return to Comments",
+
+''=>''
+);
+?>
