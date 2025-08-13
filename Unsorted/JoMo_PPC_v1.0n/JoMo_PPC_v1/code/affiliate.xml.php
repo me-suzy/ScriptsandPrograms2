1bb920327569a64e420fbb09fd3fd0b425188b07
me-suzy <me.suzana@gmail.com>
2025-08-13 05:51:45 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/code/affiliate.xml.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/code/affiliate.xml.php
@@ -0,0 +1,57 @@
+<?
+/*
+###############################
+#
+# JoMo Easy Pay-Per-Click Search Engine v1.0
+#
+#
+###############################
+#
+# Date                 : September 16, 2002
+# supplied by          : CyKuH [WTN]
+# nullified by         : CyKuH [WTN]
+#
+#################
+#
+# This script is copyright L 2002-2012 by Rodney Hobart (JoMo Media Group),
+All Rights Reserved.
+#
+# The use of this script constitutes acceptance of any terms or conditions,
+#
+# Conditions:
+#  -> Do NOT remove any of the copyright notices in the script.
+#  -> This script can not be distributed or resold by anyone else than the
+author, unless special permisson is given.
+#
+# The author is not responsible if this script causes any damage to your
+server or computers.
+#
+#################################
+
+*/
+?>
+<?
+
+	checkAffiliatePage();
+
+
+	$sID->assign("mode","affiliates");
+	$sID->assign("affMode","xml");
+	
+	$tpl->assign("affiliateID",$affiliateID);
+	
+	
+/**
+$cmd=
+
+*/
+
+    if (!isset($cmd) || empty($cmd))  $cmd="";
+	
+    $tpl->assign("cmd",$cmd);
+
+    $tpl->assign("xmlformat",getXMLFormat($affiliateID));	
+    $tpl->assign("xmlurl",getSampleXMLURL($affiliateID));
+		
+	$tpl->display("template.affiliate.xml.php");
+?>
