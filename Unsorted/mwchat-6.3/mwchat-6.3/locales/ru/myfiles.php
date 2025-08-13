1e024c84a90b3476ff149221b5c1d541ddc63f50
me-suzy <me.suzana@gmail.com>
2025-08-13 06:35:02 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/myfiles.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/myfiles.php
@@ -0,0 +1,33 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[MYFILES_File_Title] = "íÏÉ ÆÁÊÌÙ ÏÎÌÁÊÎ!";
+
+$LOCALE[MYFILES_File_Info] = "îÁÖÍÉÔÅ ÎÁ ÆÁÊÌ, ËÏÔÏÒÙÊ ×Ù ÈÏÔÉÔÅ ÓËÁÞÁÔØ ÉÌÉ ÐÒÏÓÍÏÔÒÅÔØ, ÉÌÉ ×ÙÂÅÒÉÔÅ ÆÁÊÌÙ, ËÏÔÏÒÙÅ ÎÕÖÎÏ ÕÄÁÌÉÔØ.";
+
+$LOCALE[MYFILES_File_Button] = "õÄÁÌÉÔØ ÏÔÍÅÞÅÎÎÙÅ ÆÁÊÌÙ";
+
+$LOCALE[MYFILES_File_None] = "õ ×ÁÓ ÎÅÔ ÆÁÊÌÏ×.";
+
+$LOCALE[MYFILES_From] = "æÁÊÌ ÏÔ";
+
+$LOCALE[MYFILES_Remove] = "õÄÁÌÉÔØ";
+
+$LOCALE[MYFILES_Time] = "÷ÒÅÍÑ";
+
+$LOCALE[MYFILES_Size] = "òÁÚÍÅÒ";
+
+$LOCALE[MYFILES_Name] = "éÍÑ ÆÁÊÌÁ";
+
+$LOCALE[MYFILES_Storage] = "ïÂÝÉÊ ÏÂßÅÍ ×ÁÛÅÇÏ ÄÉÓËÏ×ÏÇÏ ÐÒÏÓÔÒÁÎÓÔ×Á";
+
+?>
