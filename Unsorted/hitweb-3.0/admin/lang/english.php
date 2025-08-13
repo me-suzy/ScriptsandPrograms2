f469fae4e9340f0e24171080ec6cb3b84c4a716e
me-suzy <me.suzana@gmail.com>
2025-08-13 05:08:14 +0300

    Add Unsorted/hitweb-3.0/admin/lang/english.php

---
+++ Unsorted/hitweb-3.0/admin/lang/english.php
@@ -0,0 +1,81 @@
+<? # $Id: english.php,v 1.2 2001/08/28 10:47:41 hitweb Exp $
+
+// Title of the pages or categories
+$title_admin = "AADMINISTRATION OF LIKE DIRECTORIES" ;
+$title_som_admin = "CONTENTS" ;
+$title_conf = "Management file configuration" ;
+$title_categories = "Management of directories" ;
+$title_categories_liens = "Management directorie";
+$title_links = "Management of link" ;
+$title_update_cat = "Modification of a categorie" ;
+$title_add_cat = "Add a categorie :";
+
+// Hypertext links
+$link_conf_db = "Management of database";
+$link_conf_file = "File configuration" ;
+$link_application = "Management of application" ;
+$link_valid_url = "Links to be validated" ;
+$link_polls = "Management of the banner of advertisement" ;
+$link_check_url = "Analyze the validity of the links" ;
+$link_international_admin = "Internationalization of the part administration" ; 
+$licence = "../gpl.txt" ;
+
+$link_update = "Modify" ;
+$link_delete = "Delete" ;
+$link_categories = "Categorie" ;
+$link_add_categories = "Add a categorie" ;
+$link_add_links = "Add a link" ;
+
+
+// Label
+$lib_dbhost = "Serveur name" ;
+$lib_dbname = "Database name" ;
+$lib_dbuser = "User name" ;
+$lib_dbpass = "Password" ;
+$lib_type_db = "Type of database" ;
+$lib_repclass = "Directorie class" ;
+$lib_replangadmin = "Directorie lang admin" ;
+$lib_langadmin = "Language for admin" ;
+$lib_ext_php = "Extension PHP" ;
+$lib_ext_tpl = "Extension TPL" ;
+$lib_rep_tpl = "Template";
+$lib_use_mail = "Use mail";
+$lib_func_mail = "Mail function in your provider";
+$lib_mail_moderator = "Mail Moderator" ;
+$lib_address_site = "Address of the site" ;
+
+$lib_id = "Id :" ;
+$lib_name = "Name :";
+$lib_lastname = "First name :";
+$lib_mail = "Mail :";
+$lib_address = "Address :";
+$lib_keyword = "Meta tags :";
+$lib_subject = "Subject :";
+$lib_description = "Description :";
+
+// Message
+$mes_select_categorie = "You have to select a categorie then to propose a site.";
+$mes_fields_empty = "You should fill all the fields !!!";
+$mes_link_in_hitweb = "This site is already present in HITWEB !!!";
+$mes_link_not_valid = "<b><font color='#FF0000'>ATTENTION !</font><br>The site isn't valid. If the problem persists, contact me by mail</b>";
+
+$mes_enre_hitweb = "
+ Thank you !<p>
+
+          Your site is add in directorie in the part links to be validated. It
+          will be really validated after the visit of the moderator of HITWEB";
+
+$mes_pb_meta = "
+<p>
+	  I did not find information about the description of your page and about the
+          keywords corresponding to your site... This information is importante, 
+          if you want to be placed correctly in search engines.
+";
+
+$mes_update_link = "Your modifications have just been registered.";
+
+
+// Form Button
+$bt_enre = "Submit" ;
+$bt_reset = "Reset" ;
+?>
