3c8e90c8e55d96fd13e2bbcf1f81f6465f5d33cc
me-suzy <me.suzana@gmail.com>
2025-08-13 05:09:28 +0300

    Add Unsorted/hitweb-3.0/odp/footer.php

---
+++ Unsorted/hitweb-3.0/odp/footer.php
@@ -0,0 +1,19 @@
+<p>
+<table border=0 cellpadding=0 cellspacing=0 width=100%>
+<td align=left valign=bottom>
+&nbsp; &nbsp; <a href="http://dmoz.org/about.html">
+<img src="http://dmoz.org/img/home_attribute.gif" border=0 width=412 height=27 alt="Help build the largest human-edited directory of the web"></a>
+</td><td align=right valign=bottom>
+<img src="http://dmoz.org/img/lizard2a.gif" width=106 height=36 alt="">
+</td></tr></table>
+
+<table border=0 bgcolor="#669933" cellpadding=2 cellspacing=0 width=100%><tr><td valign=middle>
+<font color="#ffffff" face="sans-serif, Arial, Helvetica" size="-2">
+&nbsp; &nbsp;
+Copyright &copy; 1998-2001 Netscape
+</font>
+</td></tr></table><font size="-2"><br></font>
+
+
+</font>
+</center>
