9618b99e64922cb823dfc570601eb47f775fe838
me-suzy <me.suzana@gmail.com>
2025-08-13 05:58:15 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/admin/admin.menu.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/admin/admin.menu.php
@@ -0,0 +1,19 @@
+<!--{top menu-->
+
+<table width="100%" align="center" border="1" bgcolor="#ccccff" color="black" cellspacing="1" bordercolor="#999999">
+ <tr align="center">
+  <td> <a href="<%$selfURL%>?mode=main">main</a>
+  <td> <a href="<%$selfURL%>?mode=links">links</a>
+  <td> <a href="<%$selfURL%>?mode=banners">banners</a>
+  <td> <a href="<%$selfURL%>?mode=accounts">accounts</a>
+  <td> <a href="<%$selfURL%>?mode=affiliates">affiliates</a>
+  <td> <a href="<%$selfURL%>?mode=notifications">notifications</a>
+  <td> <a href="<%$selfURL%>?mode=stats">statistics</a>
+  <td> <a href="<%$selfURL%>?mode=options">options</a>
+  <td> <a href="<%$selfURL%>?mode=faq">FAQ</a>
+  <td> <a href="<%$selfURL%>?mode=logout">Logout</a>
+ </tr>
+
+</table>
+
+<!--}top menu-->
