1dbcdeaec5c44c76afd2d37f44c1f2f3a7d9c81e
me-suzy <me.suzana@gmail.com>
2025-08-13 06:39:12 +0300

    Add Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/ru/lng_idx.php

---
+++ Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/ru/lng_idx.php
@@ -0,0 +1,36 @@
+<?php
+
+$lang['page_sub_title']	                =       'ðÏÌØÚÏ×ÁÔÅÌØÓËÉÊ ÒÁÚÄÅÌ';
+$lang['submit_login']	                =       '÷ÏÊÔÉ';
+$lang['username']	                =	'éÍÑ ÐÏÌØÚÏ×ÁÔÅÌÑ';
+$lang['password']	                =	'ðÁÒÏÌØ';
+$lang['remember_login']	                =	'úÁÐÏÍÎÉÔØ ×ÈÏÄ?';
+$lang['logout']	                        =	'÷ÙÊÔÉ';
+$lang['welcome']                        =	'äÏÂÒÏ ÐÏÖÁÌÏ×ÁÔØ ';
+$lang['last_visit']	                =	'÷ÁÛ ÐÏÓÌÅÄÎÉÊ ×ÉÚÉÔ ÂÙÌ ';
+$lang['menu_edit_profile']              =       'éÚÍÅÎÉÔØ ÐÒÏÆÉÌØ';
+$lang['menu_edit_password']             =       'éÚÍÅÎÉÔØ ÐÁÒÏÌØ';
+$lang['menu_accessing_dirs']            =       'äÏÓÔÕÐÎÙÅ ÐÁÐËÉ';
+$lang['menu_anounces']                  =       'îÁÛÉ ÁÎÏÎÓÙ';
+$lang['anounces_header']                =       'îÁÛÉ ÁÎÏÎÓÙ';
+$lang['no_anounces']                    =       'îÅÔ ÁÎÏÎÓÏ×';
+$lang['more_anounces_link']             =       'óÍÏÔÒÅÔØ ×ÓÅ ÁÎÏÎÓÙ';
+$lang['profile_header']                 =       '÷ÁÛ ÐÒÏÆÉÌØ';
+$lang['profile_saved']                  =       '÷ÁÛ ÐÒÏÆÉÌØ ÂÙÌ ÕÓÐÅÛÎÏ ÉÚÍÅÎÅÎ';
+$lang['save_profile']                   =       'ÉÚÍÅÎÉÔØ ÍÏÊ ÐÒÏÆÉÌØ';
+$lang['regdate']                        =       'äÁÔÁ ÒÅÇÉÓÔÒÁÃÉÉ';
+$lang['expire']                         =       'äÁÎÎÙÅ ÄÅÊÓÔ×ÉÔÅÌØÎÙ ÄÏ';
+$lang['interface_lang']                 =       'ñÚÙË ÉÎÔÅÒÆÅÊÓÁ';
+$lang['change_passwd_header']           =       'éÚÍÅÎÉÔØ ÐÁÒÏÌØ';
+$lang['old_password']                   =       'óÔÁÒÙÊ ÐÁÒÏÌØ';
+$lang['new_password']                   =       'îÏ×ÙÊ ÐÁÒÏÌØ';
+$lang['new_password_again']             =       'ðÁÒÏÌØ ÐÏ×ÔÏÒÎÏ';
+$lang['create_random_passwd']           =       'óÏÚÄÁÔØ ÍÎÅ ÐÁÒÏÌØ';
+$lang['change_my_passwd']               =       'ÉÚÍÅÎÉÔØ ÍÏÊ ÐÁÒÏÌØ';
+$lang['passwd_changed']                 =       '÷ÁÛ ÐÁÒÏÌØ ÂÙÌ ÕÓÐÅÛÎÏ ÉÚÍÅÎÅÎ.<br>îÏ×ÙÊ ÐÁÒÏÌØ ÏÔÐÒÁ×ÌÅÎ ÎÁ ÷ÁÛ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎÎÙÊ ÁÄÒÅÓ email.<br> ïÖÉÄÁÊÔÅ ...';
+$lang['passwd_creation_message']        =       'åÓÌÉ ÷Ù ÈÏÔÉÔÅ, ÞÔÏÂÙ ÐÁÒÏÌØ ÂÙÌ ÓÇÅÎÅÒÉÒÏ×ÁÎ ÓÉÓÔÅÍÏÊ, ÏÔÍÅÔØÔÅ ÐÕÎËÔ "óÏÚÄÁÔØ ÍÎÅ ÐÁÒÏÌØ". îÏ×ÙÊ ÐÁÒÏÌØ ÂÕÄÅÔ ×ÙÓÌÁÎ ÎÁ ÷ÁÛ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎÎÙÊ ÁÄÒÅÓ Email';
+$lang['pass_incorrect']                 =       'îÅ×ÅÒÎÙÊ ÓÔÁÒÙÊ ÐÁÒÏÌØ';
+$lang['pass_missmatch']                 =       'ðÏÌÑ "ÎÏ×ÙÊ ÐÁÒÏÌØ" É "ÐÁÒÏÌØ ÐÏ×ÔÏÒÎÏ" ÎÅ ÓÏ×ÐÁÄÁÀÔ. ðÏ×ÔÏÒÉÔÅ ÅÝÅ ÒÁÚ ×ÎÉÍÁÔÅÌØÎÅÅ.';
+$lang['accessing_dirs_header']          =       'äÏÓÔÕÐÎÙÅ ÷ÁÍ ÄÉÒÅËÔÏÒÉÉ';
+
+?>
