3ce33a80c8bec6c0e13b39bac970d6a4cb87c8b5
me-suzy <me.suzana@gmail.com>
2025-08-13 03:01:15 +0300

    Add Unsorted/ebay/bugslayer/WarningsOff.h

---
+++ Unsorted/ebay/bugslayer/WarningsOff.h
@@ -0,0 +1,28 @@
+//	$Id: WarningsOff.h,v 1.2 1999/03/22 00:09:29 josh Exp $
+/*----------------------------------------------------------------------
+       John Robbins - Microsoft Systems Journal Bugslayer Column
+------------------------------------------------------------------------
+FILE        :   WarningsOff.h
+DISCUSSION  :
+    Turns off warnings so standard headers will compile.  Any warning
+turned off, must be turned back in in WarningsOn.h
+----------------------------------------------------------------------*/
+
+#ifndef _WARNINGSOFF_H
+#define _WARNINGSOFF_H
+
+/* Nonstandard extension : nameless struct/union */
+#pragma warning ( disable : 4201 )
+
+/* Unreferenced in inline function removed */
+#pragma warning ( disable : 4514 )
+
+/* conditional expression is constant */
+#pragma warning ( disable : 4127 )
+
+/* default constructor could not be generated */
+#pragma warning ( disable : 4510 )
+
+#endif  /* _WARNINGSOFF_H */
+
+
