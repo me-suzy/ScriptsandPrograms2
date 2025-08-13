ddb9dd5dfedc8d30bf71561406b2af05a275bf95
me-suzy <me.suzana@gmail.com>
2025-08-13 06:39:01 +0300

    Add Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/en/lng_global.php

---
+++ Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/en/lng_global.php
@@ -0,0 +1,65 @@
+<?php
+
+$lang['user']		    =	'user';
+$lang['login']		    =	'log in';
+$lang['username']	    =	'UserName';
+$lang['realname']	    =	'Real name';
+$lang['password']	    =	'PassWord';
+$lang['next']		    =	'>>';
+$lang['previous']	    =	'<<';
+$lang['page']		    =	'page';
+$lang['pages']		    =	'pages';
+$lang['of']		    =	'of';
+$lang['add']	   	    =	'add';
+$lang['delete']		    =	'remove';
+$lang['edit']  		    =	'edit';
+$lang['save']		    =	'save';
+$lang['date']  		    =	'date';
+$lang['time']  		    =	'time';
+$lang['january']	    =	'january';
+$lang['february']	    =	'february';
+$lang['march']		    =	'march';
+$lang['april']		    =	'april';
+$lang['may']		    =	'may';
+$lang['june']		    =	'june';
+$lang['july']		    =	'july';
+$lang['augest']		    =	'augest';
+$lang['september']	    =	'september';
+$lang['october']   	    =	'october';
+$lang['november']	    =	'november';
+$lang['december']	    =	'december';
+$lang['monday']		    =	'monday';
+$lang['tuesday']	    =	'tuesday';
+$lang['wednesday']	    =	'wednesday';
+$lang['thursday']	    =	'thursday';
+$lang['friday']		    =	'friday';
+$lang['saturday']	    =	'saturday';
+$lang['sunday']		    =	'sunday';
+$lang['s_monday']           =	'Mon';
+$lang['s_tuesday']          =	'Tue';
+$lang['s_wednesday']        =	'Wed';
+$lang['s_thursday']	    =	'Thu';
+$lang['s_friday']           =	'Fri';
+$lang['s_saturday']	    =	'Sat';
+$lang['s_sunday']           =	'Sun';
+$lang['year']   	    =	'year';
+$lang['s_year']   	    =	'year';
+$lang['email']   	    =	'Email';
+$lang['phone']	  	    =	'Phone';
+$lang['s_phone']   	    =	'Tel.';
+$lang['fax']	   	    =	'Fax';
+$lang['search']             =   'Search';
+$lang['avdsearch']          =   'Advanced search';
+$lang['sitemap']            =   'Sitemap';
+$lang['support']            =   'Support';
+$lang['feedback']           =   'Feedback';
+$lang['redirect_link_text']    =   'If You don\'t want to wait, click here';
+$lang['redirect_page_subtitle'] = 'Redirect';
+$lang['error_page_text']    = 'An error has ocure';
+$lang['go_back']            = 'go back';
+$lang['contact_webmaster']  = 'webmaster';
+$lang['fields_not_filled']  = 'Not all required fields completed';
+
+
+
+?>
