b7ecdaad2a906b7d27573154a2f1a744ad7dca8f
me-suzy <me.suzana@gmail.com>
2025-08-13 03:37:28 +0300

    Add Unsorted/ebay/testsql/ebay_agg_site_out_data.sql

---
+++ Unsorted/ebay/testsql/ebay_agg_site_out_data.sql
@@ -0,0 +1,30 @@
+/*	$Id: ebay_agg_site_out_data.sql,v 1.2 1999/02/21 02:55:56 josh Exp $	*/
+/*
+ * ebay_agg_site_out_data
+ *
+ *	This table contains information
+ *  about the sites we (as a company)
+ *  link out to. It does not contain
+ *  the links made by our users.
+ *  It also contains the dates those links
+ *  were active -- entries with a null
+ *  end date are still active.
+ *
+ */
+
+drop table ebay_agg_site_out_data;
+
+create table ebay_agg_site_out_data
+(
+	covers_day_start date
+	constraint nn_sol_start_date
+		not null,
+	covers_day_end date
+	constraint cbn_sol_end_date
+		null,
+	site_out_location varchar(255)
+	constraint nn_sol_location
+		not null
+)
+tablespace tbizdevd01
+storage (initial 10K next 10K);
