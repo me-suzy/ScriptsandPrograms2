a94fe8e3ad7650cfe23128b619b52c8eb2e56971
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:01 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.cp.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.cp.php
@@ -0,0 +1,176 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Control Panel
+//----------------------------
+
+"consult_user_guide" =>
+"Im Handbuch nachlesen",
+
+"powered_by" =>
+"Powered By",
+
+"control_panel" =>
+"Control-Panel",
+
+"main_menu" =>
+"Control-Panel Home",
+
+"username" =>
+"Username",
+
+"password" =>
+"Passwort",
+
+"publish" =>
+"Posten",
+
+"edit" =>
+"Bearbeiten",
+
+"design" =>
+"Vorlagen",
+
+"communicate" =>
+"Kommunikation",
+
+"modules" =>
+"Module",
+
+"admin" =>
+"Admin",
+
+"my_account" =>
+"Mein Account",
+
+"user_account" =>
+"Mitglieds-Account",
+
+"user_guide" =>
+"Handbuch",
+
+"logout" =>
+"Logout",
+
+"copyright" =>
+"Copyright",
+
+"rights_reserved" =>
+"Alle Rechte vorbehalten",
+
+"page_rendered" =>
+"Script ausgef&uuml;hrt in %x Sekunden",
+
+"queries_executed" =>
+"%x SQL-Abfragen benutzt",
+
+"build" =>
+"Build:",
+
+"forgot_password" =>
+"Passwort vergessen?",
+
+"e_no_css" =>
+"Die gew&uuml;nschte CSS-Datei konnte nicht ge&ouml;ffnet werden.",
+
+"unauthorized" =>
+"Nicht autorisiert",
+
+"unauthorized_access" =>
+"Sie sind nicht berechtigt, diese Seite zu sehen",
+
+"be_careful" =>
+"VORSICHT!",
+
+"action_can_not_be_undone" =>
+"DIESE AKTION KANN NICHT R&Uuml;CKG&Auml;NGIG GEMACHT WERDEN",
+
+"back" =>
+"Zur&uuml;ck",
+
+"no" =>
+"Nein",
+
+"yes" =>
+"Ja",
+
+"all" =>
+"Alle",
+
+"any" =>
+"Jede",
+
+"required_fields" =>
+"Bezeichnet Pflichtfelder",
+
+"please_consult_manual" =>
+"Bitte im Handbuch nachsehen.",
+
+"login" =>
+"Login",
+
+"forgotten_password" =>
+"Passwort vergessen",
+
+"submit_email_address" =>
+"Geben Sie Ihre E-Mail-Adresse an",
+
+
+//----------------------------
+// Buttons
+//----------------------------
+
+"submit" =>
+"Abschicken",
+
+"preview" =>
+"Vorschau",
+
+"update" =>
+"Aktualisieren",
+
+"delete" =>
+"L&ouml;schen",
+
+"save_changes" =>
+"&Auml;nderungen speichern",
+
+
+
+//----------------------------
+// Log
+//----------------------------
+
+"clear_logs" =>
+"Log-Dateien leeren",
+
+"member_id" =>
+"Mitglieds-ID",
+
+"ip_address" =>
+"IP-Adresse",
+
+"date" =>
+"Datum",
+
+"action" =>
+"Aktion",
+
+
+"member_logged_in" =>
+"Eingeloggt",
+
+"member_logged_out" =>
+"Ausgeloggt",
+
+"cleared_logs" =>
+"Logdateien geleert",
+
+
+
+// END
+''=>''
+);
+?>
