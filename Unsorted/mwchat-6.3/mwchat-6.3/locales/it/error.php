ed69e481a8948ddfc7f8b5472ceaf462aba2baad
me-suzy <me.suzana@gmail.com>
2025-08-13 06:32:49 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/it/error.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/it/error.php
@@ -0,0 +1,52 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[ERROR_License] = "* * Un errore con la sua licenza. Diffida di MWChat di licenza adesso.";
+
+$LOCALE[ERROR_AlreadyIn] = "Il nome che lei ha scelto è già in uso.";
+
+$LOCALE[ERROR_Title] = "L'errore";
+
+$LOCALE[ERROR_Logout] = "Lei è stato fuori abbattuto.";
+
+$LOCALE[ERROR_Expired] = "La sua apertura di sessione ha scaduto, per favore l'apertura di sessione ancora.";
+
+$LOCALE[ERROR_RoomLong] = "Questa stanza lo dà un nome a sceglie è a lungo.";
+
+$LOCALE[ERROR_Live] = "Il sistema è attualmente non disponibile.";
+
+$LOCALE[ERROR_Profanity] = "Le informazioni che lei ha specificata contiene la profanità.";
+
+$LOCALE[ERROR_NoData]   = "Per favore di riempire tutto mette in campo.";
+
+$LOCALE[ERROR_BadData]  = "Per favore di specificare i dati validi.";
+
+$LOCALE[ERROR_Serial_Invalid] = "Il suo numero di matricola è l'invalido, per favore il re iscrive il suo prodotto.";
+
+$LOCALE[ERROR_Serial_None] = "Non trovare un numero di matricola sul disco, per favore il re iscrive il suo prodotto.";
+
+$LOCALE[ERROR_Critical] = "L'errore critico!";
+
+$LOCALE[ERROR_Javascript] = "Questo programma richiede un JavaScript il browser capace.";
+
+$LOCALE[ERROR_Browser] = "Questo programma non funzionerà correttamente col suo browser.";
+
+$LOCALE[ERROR_Restricted] = "Il nome che lei ha scelto è limitato.";
+
+$LOCALE[ERROR_BadPass]  = "Per favore di entrare una parola d'ordine valida.";
+
+$LOCALE[ERROR_IP] = "L'IP indirizza suo è di usare ha cambiato. Lei è stato fuori abbattuto per le ragioni di sicurezza.";
+
+$LOCALE[ERROR_PHPV] = "MWChat richiede una versione di PHP minima di";
+
+?>
