d8de270d11ed17aa75b53adf506fdfc3edc7b7fb
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:28 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-pl-list.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-pl-list.php
@@ -0,0 +1,24 @@
+<?php
+
+##################################################
+
+# /pl-list.php
+
+##################################################
+
+define( "PLIST_PLAY_HELP", "Click para escuchar la lista." );
+define( "PLIST_EDIT_HELP", "Click para editar la lista " );
+define( "PLIST_VIEW_HELP", "Click para ver el contenido de la lista" );
+
+define( "PLIST_PRVT_HEADER", "LISTAS PRIVADAS" );
+define( "PLIST_PRVT_HELP", "Las listas privadas sólo pueden ser vistas, accedidas o editadas y/o eliminadas por el usuario propietario." );
+
+define( "PLIST_SHRD_HEADER", "LISTAS COMPARTIDAS" );
+define( "PLIST_SHRD_HELP", "Las listas compartidas sólo pueden ser editadas y/o eliminadas por Ud., pero podría mirar los casos en la sección de comunidad." );
+
+define( "PLIST_FAV_HEADER", "LISTA DE LA COMUNIDAD" );
+define( "PLIST_FAV_HELP", "Las listas de la comunidad estan compartidas por otros y pueden ser accedidas por la sección de comunidad." );
+
+##################################################
+
+?>
