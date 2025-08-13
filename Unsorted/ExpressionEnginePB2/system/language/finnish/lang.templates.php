6c191c64f4f03a8fc8402428f29a737d0df8df3f
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:31 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.templates.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.templates.php
@@ -0,0 +1,330 @@
+<?php
+$L = array(
+
+"php_parse_location" =>
+"Stage at which PHP should be parsed",
+
+"input" =>
+"Input",
+
+"output" =>
+"Output",
+
+"template_loop" =>
+"Olet aiheuttanut sivupohja luupin sopimattomilla sisäkkäisillä alisivupohjille",
+
+"template_intro" =>
+"Tämä sivu on missä sivupohjia hallitaan",
+
+"create_new_template_group" =>
+"Muodosta uusi sivupohja ryhmä",
+
+"new_template_group_form" =>
+"Uusi sivupohja ryhmä",
+
+"edit_template_group_form" =>
+"Muokka sivupohja ryhmää",
+
+"name_of_template_group" =>
+"Sivupohja ryhmän nimi",
+
+"template_group_instructions" =>
+"Nimen tulee olla yksi sana ilman välilyöntejä",
+
+"undersores_allowed" =>
+"(viiva ja alaviiva sallitaan)",
+
+"new_template_form" =>
+"Uusi sivupohja lomake",
+
+"edit_template_form" =>
+"Muokkaa sivupohja lomaketta",
+
+"name_of_template" =>
+"Sivupohjan nimi",
+
+"group_name" =>
+"Ryhmän nimi",
+
+"order" =>
+"Järjestys",
+
+"reserved_name" =>
+"Nimi jonka valitsi on varattu nimi eikä sitä voida käyttää",
+
+"type_of_template" =>
+"Sivupohjan tyyppi",
+
+"webpage" =>
+"Web sivu",
+
+"css_stylesheet" =>
+"CSS tyylipohja",
+
+"rss" =>
+"RSS sivu",
+
+"missing_name" =>
+"Sivupohjalla tulee olla nimi",
+
+"template_name_taken" =>
+"Nimi, jonka annoit, on jo olemassa",
+
+"template_group_del_conf" =>
+"Sivupohjaryhmän poiston vahvistus",
+
+"template_del_conf" =>
+"Sivupohjan poiston vahvistus",
+
+"edit_template" =>
+"Muokkaa sivupohjaa",
+
+"add_a_template" =>
+"Lisää uusi sivupohja",
+
+"delete_template_group" =>
+"Poista ryhmä",
+
+"edit_template_group" =>
+"Muokkaa ryhmää ",
+
+"group_order" =>
+"Ryhmän järjestys",
+
+"export_templates" =>
+"Exporttaa sivupohja",
+
+"template_name" =>
+"Sivupohjan nimi:",
+
+"template_management" =>
+"Sivupohjan hallinta",
+
+"current_template" =>
+"Nykyinen sivupohja: ",
+
+"preferences" =>
+"Ominaisuudet",
+
+"prefs" =>
+"Ominaisuudet",
+
+"hits" =>
+"Osumia",
+
+"hit_counter" =>
+"Laskuri",
+
+"template_updated" =>
+"Sivupohja päivitetty",
+
+"template_created" =>
+"Sivupohja päivitetty",
+
+"form_is_empty" =>
+"Lähetit tyhjän lomakkeen",
+
+"all_fields_required" =>
+"Sinun tulee täyttää kaikki lomakkeen kentät",
+
+"illegal_characters" =>
+"Nimi voi sisältää vain aakkosia, numeroita, viivoja ja alaviivoja",
+
+"edit_template_group_order" =>
+"Muokaa ryhmän järjestystä ",
+
+"template_group_updated" =>
+"Sivupohja ryhmä päivitetty",
+
+"template_group_created" =>
+"Sivupohja ryhmä muodostettu",
+
+"delete_this_group" =>
+"Poista seuraava sivupohja ryhmä?",
+
+"all_templates_will_be_nuked" =>
+"KAIKKI SIVUPOHJAT TÄSSÄ RYHMÄSSÄ TULLAAN LOPULLISESTI POISTAMAAN!",
+
+"template_group_deleted" =>
+"Sivupohja ryhmä poistettu",
+
+"edit_group_order" =>
+"Muokkaa ryhmän järjestystä",
+
+"template_size" =>
+"Sivupohjan koko",
+
+"small" =>
+"Pieni",
+
+"medium" =>
+"Keskisuuri",
+
+"large" =>
+"Suuri",
+
+"view_rendered_template" =>
+"Näytä renderoitu sivupohja",
+
+"save_history" =>
+"Tallenna muutos",
+
+"view" =>
+"Näytä",
+
+"clear" =>
+"Tyhjennä",
+
+"select_all" =>
+"Valitse kaikki",
+
+"export" =>
+"Vie",
+
+"export_as" =>
+"Vie tiedosto nimellä",
+
+"export_will_be_zip" =>
+"Huom: viety tiedoston data lähetetään ZIP tiedostona",
+
+"choose_templates" =>
+"Valitse sivupohjat jotka haluat viedä ",
+
+"unable_to_write_files" =>
+"Ei voitu kirjoittaa välimuisti hakemistoon",
+
+"revision_history" =>
+"Näytä muutoshistoria",
+
+"clear_revision_history" =>
+"Tyhjennä muutoshistoria",
+
+"history_cleared" =>
+"Sivupohjan muutoshistoria on poistettu",
+
+"close_window" =>
+"Sulje ikkuna",
+
+"template_preferences" =>
+"Sivupohjan ominaisuudet",
+
+"delete_template" =>
+"Poista sivupohja",
+
+"delete_this_template" =>
+"Poista seuraavat sivupohjat:",
+
+"template_deleted" =>
+"Sivupohja poistettu",
+
+"default_site_page" =>
+"Osoittaa pääsivun oletus sivun",
+
+"is_site_default" =>
+"Tee tämän ryhmän index sivu sivujesi aloitussivuksi",
+
+"allow_php" =>
+"Salli PHP tässä sivupohjassa?",
+
+"enable_cache" =>
+"Salli sivupohjan välimuistitallennus",
+
+"cache_interval" =>
+"Aikaintevalli välimuistin päivitykseen",
+
+"in_minutes" =>
+"Aseta aika minuuteissa",
+
+"php_security_warning" =>
+"Asettamalla tämä asentoon &quot;kyllä&quot;, aiheutat turvallisuus riskin",
+
+"consult_manual" =>
+"Lue lisää manuaalista",
+
+"preferences_updated" =>
+"Ominaisuudet tallennettu",
+
+"choose_default_data" =>
+"Sivupohjan oletus data",
+
+"blank_template" =>
+"Ei - muodosta tyhjä sivupohja",
+
+"template_from_library" =>
+"Käytä kokoelman sivupohjaa",
+
+"an_existing_template" =>
+"Kopioi olemassa oleva sivupohja",
+
+"you_must_submit_a_name" =>
+"Sivupohjallasi tulee olla nimi",
+
+"member_group" =>
+"Jäsenten ryhmä",
+
+"can_view_template" =>
+"Voi nähdä tämän sivun",
+
+"require_logged_in" =>
+"Vaadi käyttäjien kirjautua nähdäkseen tämän sivun?",
+
+"restrict_to_group" =>
+"Rajoita näkymä jäsenten ryhmälle",
+
+"logged_out_select_blurb" =>
+"Jos valitsit &quot;kyllä&quot;, kun ei-kirjautuneet käyttäjät yrittävät päästä sivulle näytetään tämä:",
+
+"no_access_select_blurb" =>
+"Jos valitsit &quot;ei&quot; mihin tahansa ylläolevista:",
+
+"no_access_instructions" =>
+"Kun luvattomat käyttäjät yrittävät päästä tälle sivulle, näytetään tämä sivu:",
+
+"template_notes" =>
+"Sivupohjan muistiinpanot",
+
+"template_notes_desc" =>
+"Käytä tätä muistiota tallentaaksesi muistiinpanoja ja tietoja tästä sivupohjasta",
+
+"global_variables" =>
+"Globaalit muuttujat",
+
+"global_variables_explanation" =>
+"Voit määritellä omia muuttujia ja lisätä niitä sivupohjillesi.",
+
+"no_global_variables" =>
+"Tällä hetkellä ei ole globaaleja muuttujia",
+
+"global_variable_name" =>
+"Muuttujan nimi",
+
+"create_new_global_variable" =>
+"Muodosta uusi globaali muuttuja",
+
+"variable_name" =>
+"Muuttujan nimi",
+
+"global_variable_syntax" =>
+"Muuttujan  syntaksi",
+
+"variable_data" =>
+"Muuttujan sisältö",
+
+"global_var_created" =>
+"Globaali muuttuja muodostettu",
+
+"global_var_updated" =>
+"Globaali muuttuja päivitetty",
+
+"delete_global_variable" =>
+"Poista globaali muuttuja",
+
+"delete_this_variable" =>
+"Poista seuraavat globaalit muuttujat?",
+
+"variable_deleted" =>
+"Muuttuja poistettu",
+
+''=>''
+);
+?>
