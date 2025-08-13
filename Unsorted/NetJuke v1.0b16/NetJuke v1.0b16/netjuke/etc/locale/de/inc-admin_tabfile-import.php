7e81051dec007a4543d986d92445eaca8e71d5d8
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:18 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_tabfile-import.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_tabfile-import.php
@@ -0,0 +1,41 @@
+<?php
+
+##################################################
+
+# /admin/tabfile-import.php
+
+##################################################
+
+define( "TFIMPT_HEADER", "TAB-GETRENNTER DATEI IMPORT" );
+
+define( "TFIMPT_VIEW", "Datei anzeigen" );
+define( "TFIMPT_PROCEED", "Mit dem Dateiimport fortfahren" );
+define( "TFIMPT_DELETE", "Die gewählten Datein löschen" );
+define( "TFIMPT_DELETE_SUCCESS", "Gelöscht" );
+define( "TFIMPT_DELETE_ERROR", "Rechte Problem: Konnte nicht löschen" );
+
+define( "TFIMPT_FOUND_1", "Habe" );
+define( "TFIMPT_FOUND_2", "Datei(en) zur Verarbeitung im vorgegebenen Imüport-Verzeichniss gefunden." );
+
+define( "TFIMPT_PROCESS", "Verarbeitung" );
+
+define( "TFIMPT_ERROR_1", "Ferhler beim einfügen in" );
+define( "TFIMPT_ERROR_2", "Folgende(r) Fehler trat(en) auf" );
+
+define( "TFIMPT_IGNORE_COLNAMES", "Ignoriere Spaltennamen" );
+define( "TFIMPT_IGNORE_DUPLICATE", "Ignoriere doppelte" );
+
+define( "TFIMPT_INSERT_1", "Eingefügt:" );
+define( "TFIMPT_INSERT_2", "Titel(er)" );
+define( "TFIMPT_INSERT_3", "Interpret(en)" );
+define( "TFIMPT_INSERT_4", "Album/en" );
+define( "TFIMPT_INSERT_5", "Genre(s)" );
+
+define( "TFIMPT_ARCHIVE_1", "Archiviere" );
+define( "TFIMPT_ARCHIVE_2", "als" );
+
+define( "TFIMPT_DONE", "Erstellt mit" );
+
+##################################################
+
+?>
