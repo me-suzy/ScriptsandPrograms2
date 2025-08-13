4843b79c67663d470f59b2d56d079689a49be2f1
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:13 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/english/lang.members.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/english/lang.members.php
@@ -0,0 +1,653 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Members
+//----------------------------
+
+"can_moderate_comments" =>
+"Can Moderate Comments",
+
+"total_members" =>
+"Total Members:",
+
+"view_members" =>
+"View Members",
+
+"validate_selected" =>
+"Activate Selected Members",
+
+"delete_selected" =>
+"Delete Selected Members",
+
+"send_email_notification" =>
+"Send an email notification to each activated member",
+
+"your_account_validated" =>
+"Your account has been activated.",
+
+"your_account_ready" =>
+"Your membership account has been activated and is ready for use.",
+
+"thank_you",
+"Thank you!",
+
+"no_members_to_validate" =>
+"There are no member accounts awaiting activation",
+
+"members_are_validated" =>
+"Member(s) have been activated",
+
+"members_are_deleted" =>
+"Member(s) have been deleted",
+
+"ascending" =>
+"Date - oldest first",
+
+"descending" =>
+"Date - newest first",
+
+"alpha" =>
+"Alphabetical Ascending",
+
+"alpha_desc" =>
+"Alphabetical Descending",
+
+"username_asc" =>
+"Username - ascending",
+
+"username_desc" =>
+"Username - descending",
+
+"screen_name_asc" =>
+"Screen Name - ascending",
+
+"screen_name_desc" =>
+"Screen Name - descending",
+
+"email_asc" =>
+"Email - ascending",
+
+"email_desc" =>
+"Email - descending",
+
+"sort_order" =>
+"Sort Order",
+
+"join_date" =>
+"Join Date",
+
+"last_visit" =>
+"Last Visit",
+
+"member_group" =>
+"Member Group",
+
+"group_id" =>
+"Group ID",
+
+"no_members_matching_that_criteria" =>
+"There are no members matching the criteria you selected",
+
+"mbrs" =>
+"Members",
+
+"edit_profile" =>
+"Edit Profile",
+
+"register_member" =>
+"Register a New Member",
+
+"edit_group" => 
+"Edit Group",
+
+"group_title" => 
+"Group Title",
+
+"security_lock" =>
+"Security Lock",
+
+"group_lock" =>
+"Member Group Security Lock",
+
+"enable_lock" =>
+"Enable Group Security Lock",
+
+"locked" =>
+"Locked",
+
+"locked" =>
+"Unlocked",
+
+"lock_description" =>
+"When a group is locked, only a Super Admin can assign a member to that group.  If you allow other users to administrate member accounts, it is highly recommended that you ONLY unlock groups you want them to be able to assign other users to.",
+
+"locked" =>
+"Locked",
+
+"unlocked" =>
+"Unlocked",
+
+"create_new_member_group" =>
+"Create a New Member Group",
+
+"member_has_cp_access" =>
+"can access the control panel",
+
+"edit_member_group" => 
+"Edit Member Group",
+
+"create_member_group" => 
+"Create a Member Group",
+
+"delete_member_group" => 
+"Delete Member Group",
+
+"member_group_deleted" =>
+"Member Group Deleted",
+
+"delete_member_group_confirm" => 
+"Are you sure you want to delete this member group?",
+
+"delete_member" =>
+"Delete Member",
+
+"delete_member_confirm" => 
+"Are you sure you want to delete this member?",
+
+"delete_members_confirm" => 
+"Are you sure you want to delete these members?",
+
+"can_not_delete_superadmin" =>
+"You can not delete the primary member",
+
+"member_deleted" =>
+"Member Deleted",
+
+"members_deleted" =>
+"Members Deleted",
+
+//----------------------------
+// Member banning
+//----------------------------
+
+"user_banning" =>
+"User Banning",
+
+"ban_preferences_updated" =>
+"Banning Preferences Updated",
+
+"ip_address_banning" =>
+"Banned IP Adddress",
+
+"ip_banning_instructions" =>
+"Place each IP address on a separate line",
+
+"ip_banning_instructions_cont" =>
+"Use wildcards for partial IP addresses. Example:  123.345.*",
+  
+"email_address_banning" =>
+"Banned Email Adddress",
+
+"email_banning_instructions" =>
+"Place each email address on a separate line",
+ 
+"email_banning_instructions_cont" =>
+"Use wildcards for partial email addresses. Example:  *@domain.com",
+
+"ban_options" =>
+"When a banned IP tries to access the site",
+
+"restrict_to_viewing" =>
+"Restrict them to viewing only",
+   
+"show_this_message" =>
+"Show this message",
+
+"send_to_site" =>
+"Send them to this site",
+
+"username_banning" =>
+"Restricted Usernames",
+
+"username_banning_instructions" =>
+"Place each username on a separate line",
+
+"screen_name_banning" =>
+"Restricted Screen Names",
+
+"screen_name_banning_instructions" =>
+"Place each screen name on a separate line",
+
+
+//----------------------------
+// Member group settings
+//----------------------------
+
+"only_superadmins_can_admin_groups" =>
+"Only Super Admins can access the Member Groups page",
+
+"create_group_based_on_old" =>
+"Create a new group based on an existing one",
+
+"group_name" =>
+"Name of Member Group",
+
+"missing_group_title" =>
+"The member group must have a name",
+
+"site_access" =>
+"Site Access",
+
+"can_view_online_system" =>
+"Can view site when it is online",
+
+"can_view_offline_system" =>
+"Can view site when it is offline",
+
+"mbr_profile_privs" =>
+"Member Profile Privileges",
+
+"can_view_profiles" =>
+"Can view public profiles",
+
+"commenting_privs" =>
+"Comment Posting Privileges",
+
+"can_post_comments" =>
+"Can submit comments",
+
+"cp_weblog_post_privs" =>
+"Weblog Assignment",
+
+"cp_module_access_privs" =>
+"Module Access Privileges",
+
+"can_access_mod" =>
+"Can access module:",
+
+"global_cp_access" =>
+"Control Panel Access",
+
+"can_access_cp" =>
+"Can access the control panel?",
+
+"cp_section_access" =>
+"Control Panel Area Access",
+
+"can_access_publish" =>
+"Can access PUBLISH page",
+
+"can_access_edit" =>
+"Can access EDIT page",
+
+"can_access_design" =>
+"Can access TEMPLATES page",
+
+"can_access_comm" =>
+"Can access COMMUNICATE page",
+
+"can_access_modules" =>
+"Can access MODULES page",
+
+"can_access_admin" =>
+"Can access ADMIN page",
+
+"can_ban_users" =>
+"Can ban users",
+
+"cp_admin_privs" =>
+"Control Panel Administrative Privileges",
+
+"cp_comment_privs" =>
+"Comment Administration Privileges",
+
+"can_view_other_comments" =>
+"Can view comments in weblog entries authored by others",
+
+"can_edit_own_comments" =>
+"Can edit comments in their own weblog entries",
+
+"can_delete_own_comments" =>
+"Can delete comments in their own weblog entries",
+
+"can_edit_all_comments" =>
+"Can edit comments in ANY weblog entries",
+
+"can_delete_all_comments" =>
+"Can delete comments in ANY weblog entries",
+
+"can_admin_weblogs" =>
+"Can administrate weblogs and weblog preferences",
+
+"can_admin_members" =>
+"Can administrate members accounts",
+
+"can_delete_members" =>
+"Can delete members",
+
+"can_admin_mbr_groups" =>
+"Can change the group that a member is assigned to (Unlocked groups only)",
+
+"can_admin_mbr_templates" =>
+"Can administrate member profile templates",
+
+"can_admin_utilities" =>
+"Can administrate system utilities",
+
+"can_admin_templates" =>
+"Can administrate template groups and templates",
+
+"can_admin_preferences" =>
+"Can administrate general preferences",
+
+"can_admin_modules" =>
+"Can install/remove modules",
+
+"cp_email_privs" =>
+"Control Panel Email Privileges",
+
+"can_send_email" =>
+"Can send email via the Control Panel",
+
+"can_email_members" =>
+"Can send email to individual members",
+
+"can_email_member_groups" =>
+"Can send email to Member Groups",
+
+"can_email_mailinglist" =>
+"Can send email to Mailing List",
+
+"can_send_cached_email" =>
+"Can view/send cached email",
+
+"search_privs" =>
+"Search Privileges",
+
+"can_search" =>
+"Can use search utility",
+
+"search_flood_control" =>
+"Number of seconds between searches",
+
+
+"warning" =>
+"WARNING:",
+
+"super_admin_edit_note" =>
+"Note: You can only edit the name of the Super Admin group",
+
+"be_careful_assigning_groups" =>
+"Be EXTREMELY careful assigning group preferences - especially the highlighted ones.",
+
+"member_group_updated" =>
+"Member Group Updated:",
+
+"member_group_created" =>
+"Member Group Created:",
+
+"cp_weblog_privs" =>
+"Weblog Posting and Editing Privileges",
+
+"can_view_other_entries" =>
+"Can view weblog entries authored by others",
+
+"can_post_in" =>
+"Can post and edit entries in:",
+
+"can_edit_other_entries" =>
+"Can edit entries authored by others",
+
+"can_assign_post_authors" =>
+"Can change the author name when posting weblog entries",
+
+"can_delete_self_entries" =>
+"Can delete their own weblog entries",
+
+"can_delete_all_entries" =>
+"Can delete ANY weblog entries",
+
+"cp_template_access_privs" =>
+"Template Editing Privileges",
+
+"can_access_tg" =>
+"Can edit templates in this group:",
+
+"can_email_from_profile" =>
+"Can email other members via the profile email console",
+
+
+//----------------------------
+// Member fields
+//----------------------------
+
+"for_profile_page" =>
+"Will be displayed in the member profile page",
+
+"custom_member_fields" =>
+"Custom Profile Fields",
+
+"current_fields" =>
+"Member Profile Fields",
+
+"edit_member_field" =>
+"Edit Field",
+
+"create_member_field" =>
+"Create Field",
+
+"field_updated" =>
+"Field Updated",
+
+"create_new_profile_field" =>
+"Create a New Profile Field",
+
+"is_field_public" =>
+"Is this field visible in public profiles?",
+
+"is_field_public_cont" =>
+"If your choose no, it will only be visible to admins",
+
+"is_field_reg" =>
+"Is field visible in registration page?",
+
+"field_width" =>
+"Field Width",
+
+"field_width_cont" =>
+"Can be in pixels or percent",
+
+"fieldname" =>
+"Field Name",
+
+"fieldname_cont" =>
+"Single word, no spaces.  Underscores and dashes allowed",
+
+"field_format" =>
+"Text Formatting",
+
+"max_length" =>
+"Maxlength",
+
+"max_length_cont" =>
+"For text field types",
+
+"text_area_rows" =>
+"Textarea Rows",
+
+"text_area_rows_cont" =>
+"For textarea field types",
+
+"pull_down_items" =>
+"Select List Options",
+
+"pull_down_items_cont" =>
+"For drop-down menus",
+
+"pull_down_instructions" =>
+"Put each item on a separate line",
+
+"invalid_characters" =>
+"The field name you submitted contains invalid characters",
+
+"member_data_will_be_deleted" =>
+"All member data contained in this field will be permanently deleted",
+
+"profile_field_deleted" =>
+"Profile Field Deleted:",
+
+"no_custom_profile_fields" =>
+"There are currently no custom profile fields",
+
+"member_search" =>
+"Member Search",
+
+"member_search_results" =>
+"Member Search Results",
+
+"no_search_results" =>
+"Your search resulted in no matches",
+
+"member_search_instructions" =>
+"Fill out at least one field.  You can submit partial or full words.",
+
+"can_not_delete_only_member" =>
+"You can not delete the only member in the database",
+
+"can_not_delete_super_admin" =>
+"You can not delete a Super Admin unless there exists at least one other",
+
+"must_be_superadmin_to_delete_one" =>
+"You must be a Super Admin to delete a Super Admin",
+
+"email_console_log" =>
+"Email Console Logs",
+
+"no_cached_email" =>
+"There are no cached email messages.",
+
+"email_title" =>
+"Email Title",
+
+"from" =>
+"From",
+
+"to" =>
+"To",
+
+"message_sent_to" =>
+"Message Sent to:",
+
+"email_deleted" =>
+"Email Message(s) Deleted",
+
+"template_set_names" =>
+"Available Template Sets",
+
+"unable_to_find_templates" =>
+"Unable to locate the member profile templates.",
+
+"unable_to_find_template_file" =>
+"Unable to locate the member template file.",
+
+"template_updated" =>
+"Template Has Been Updated",
+
+"template_set" =>
+"Template Set:",
+
+"file_not_writable" =>
+"Note: This file is not writable",
+
+"error_opening_template" =>
+"Error: Unable to open the template file in order to write its contents.",
+
+"file_writing_instructions" =>
+"You will not be able to save changes to this template until you set this file to 666:",
+
+"aim_console" =>
+"AOL Instant Messenger Console",
+
+"breadcrumb" =>
+"Breadcrumb",
+
+"copyright" =>
+"Copyright Notice",
+
+"custom_profile_fields" =>
+"Custom Profile Field Form",
+
+"edit_profile_form" =>
+"Member Profile Form",
+
+"email_form" =>
+"Email Console",
+
+"email_prefs_form" =>
+"Email Preferences Form",
+
+"email_user_message" =>
+"Email Messages",
+
+"forgot_form" =>
+"Forgot Password Form",
+
+"home_page" =>
+"Member Profile Home Page",
+
+"icq_console" =>
+"ICQ Console",
+
+"localization_form" =>
+"Localization Preferences Form",
+
+"login_form" =>
+"Login Form",
+
+"memberlist" =>
+"Member List Page",
+
+"memberlist_rows" =>
+"Member List Rows",
+
+"menu" =>
+"Member Profile Menu",
+
+"notepad_form" =>
+"Notepad Form",
+
+"password_change_warning" =>
+"Password Change Warning",
+
+"public_custom_profile_fields" =>
+"Custom Profile Field Rows",
+
+
+"public_profile" =>
+"Public Member Profile",
+
+"registration_form" =>
+"Registration Form",
+
+"stylesheet" =>
+"CSS Stylesheet",
+
+"success" =>
+"Profile Update Message",
+
+"username_change_disallowed" =>
+"Username Change Disallowed Message",
+
+"username_password_form" =>
+"Username and Password Form",
+
+"username_row" =>
+"Username Form Row",
+
+
+// END
+''=>''
+);
+?>
