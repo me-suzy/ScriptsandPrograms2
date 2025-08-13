f9dac9afae7fa31a79a330c63de8447a3c49ee16
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:31 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_user-list.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_user-list.php
@@ -0,0 +1,23 @@
+<?php
+
+##################################################
+
+# /admin/user-list.php
+
+##################################################
+
+define( "USLIST_EDIT_HELP", "Klicken um diesen Benutzer zu bearbeiten" );
+define( "USLIST_BTN_NEW", "Neues Benutzerkonto" );
+
+define( "USLIST_US_HEADER", "BENUTZER" );
+define( "USLIST_US_HELP", "Benutzer haben Zugriff auf alle Titel, ihre benutzerspezifischen Einstellungen, ihre Spiellisten und den Gemeinschaftsbereich." );
+
+define( "USLIST_ED_HEADER", "BEARBEITER" );
+define( "USLIST_ED_HELP", "Bearbeiter haben die gleichen Zugriffsrechte wie Benutzer, können aber zusätzlich die Titelinformationen bearbeiten." );
+
+define( "USLIST_AD_HEADER", "ADMINISTRATOREN" );
+define( "USLIST_AD_HELP", "Administratoren haben uneingeschränkten Zugriff auf alle Optionen der Anwendung. Sie sind im Moment mit Administratorrechten angemeldet." );
+
+##################################################
+
+?>
