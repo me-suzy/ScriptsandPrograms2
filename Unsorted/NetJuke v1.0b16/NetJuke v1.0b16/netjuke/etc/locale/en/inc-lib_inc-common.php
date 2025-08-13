ccccf290c50177750c0f1c36ff796f5b60b50525
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:59 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-lib_inc-common.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-lib_inc-common.php
@@ -0,0 +1,75 @@
+<?php
+
+##################################################
+
+# /lib/inc-common.php
+
+##################################################
+
+define( "COMMON_ERR_DBCONN", "Cannot connect to the database using the provided information." );
+
+define( "COMMON_ACCESS_DENIED_1", "Access Denied. You Must Be An Administrator To Login." );
+define( "COMMON_ACCESS_DENIED_2", "Access Denied. Please try again." );
+
+define( "COMMON_PLISTSELECT_1", "New Playlist" );
+
+define( "COMMON_ALPHABET_AR_HELP", "Click To List All Artists Starting With" );
+define( "COMMON_ALPHABET_AL_HELP", "Click To List All Albums Starting With" );
+
+define( "COMMON_CHECK_IMAGE_HELP", "Click To View The Associated Image" );
+
+define( "COMMON_NOTRACK", "You did not select any track from the list below." );
+
+define( "COMMON_PLIST_1", "Please select an existing playlist to be edited.\\nOr Select a few tracks and create a new one." );
+
+define( "COMMON_BATCHEDIT_1", "Are you sure you want to delete the selected tracks from the Netjuke?" );
+
+define( "COMMON_RADIOEDIT_1", "Are you sure you want to completely replace the existing radio playlist?" );
+
+define( "COMMON_JUKEBOXEDIT_1", "Are you sure you want to completely replace the existing jukebox playlist?" );
+
+define( "COMMON_BEDIT_HEADER", "SPECIAL ADMINISTRATOR AND EDITOR TOOLS" );
+define( "COMMON_BEDIT_TB_BATCHEDIT", "Batch Edit" );
+define( "COMMON_BEDIT_TB_BATCHEDIT_HELP", "Click here to edit the selected tracks in batch mode." );
+define( "COMMON_BEDIT_TB_BATCHDEL", "Batch Delete" );
+define( "COMMON_BEDIT_TB_BATCHDEL_HELP", "Click here to delete the selected tracks in batch mode" );
+define( "COMMON_BEDIT_TB_RADIOADD", "Add To Radio Playlist" );
+define( "COMMON_BEDIT_TB_RADIOADD_HELP", "Click here to add the selected tracks to the current radio playlist." );
+define( "COMMON_BEDIT_TB_RADIOREP", "Replace Radio Playlist" );
+define( "COMMON_BEDIT_TB_RADIOREP_HELP", "Click here to create a new radio playlist, or replace the existing one, with the selected tracks." );
+define( "COMMON_BEDIT_TB_RADIOPLVIEW", "View Radio Playlist" );
+define( "COMMON_BEDIT_TB_RADIOPLVIEW_HELP", "Click here to view the current text file used as a radio playlist." );
+define( "COMMON_BEDIT_TB_JUKEBOXSTART", "Start Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXSTART_HELP", "Click here to start playin the jukebox' playlist on the server." );
+define( "COMMON_BEDIT_TB_JUKEBOXSTOP", "Stop Jukebox" );
+define( "COMMON_BEDIT_TB_JUKEBOXSTOP_HELP", "Click here to stop playin the jukebox' playlist on the server." );
+define( "COMMON_BEDIT_TB_JUKEBOXADD", "Add To Jukebox Playlist" );
+define( "COMMON_BEDIT_TB_JUKEBOXADD_HELP", "Click here to add the selected tracks to the current jukebox playlist." );
+define( "COMMON_BEDIT_TB_JUKEBOXREP", "Replace Jukebox Playlist" );
+define( "COMMON_BEDIT_TB_JUKEBOXREP_HELP", "Click here to create a new jukebox playlist, or replace the existing one, with the selected tracks." );
+define( "COMMON_BEDIT_TB_JUKEBOXPLVIEW", "View Jukebox Playlist" );
+define( "COMMON_BEDIT_TB_JUKEBOXPLVIEW_HELP", "Click here to view the current text file used as a jukebox playlist." );
+
+define( "COMMON_SUMHEAD_QS_HEADER", "QUICK SEARCH" );
+define( "COMMON_SUMHEAD_QS_OPT_AR", "Search By Artist Name" );
+define( "COMMON_SUMHEAD_QS_OPT_TR", "Search By Track Name" );
+define( "COMMON_SUMHEAD_QS_OPT_AL", "Search By Album Name" );
+define( "COMMON_SUMHEAD_QS_OPT_GE", "Search By Genre Name" );
+define( "COMMON_SUMHEAD_QS_OPT_TI", "Search By Time (Seconds)" );
+define( "COMMON_SUMHEAD_QS_BTN", "Go!" );
+
+define( "COMMON_SUMHEAD_SUMM_HEADER", "CONTENT SUMMARY" );
+define( "COMMON_SUMHEAD_SUMM_TR", "Tracks" );
+define( "COMMON_SUMHEAD_SUMM_AR", "Artists" );
+define( "COMMON_SUMHEAD_SUMM_AL", "Albums" );
+define( "COMMON_SUMHEAD_SUMM_GE", "Genres" );
+
+define( "COMMON_SUMHEAD_ALPHA_AR_HEADER", "ARTISTS" );
+define( "COMMON_SUMHEAD_ALPHA_AL_HEADER", "ALBUMS" );
+
+define( "COMMON_SUMHEAD_ALL_TITLE", "List all entries in alphabetical order" );
+define( "COMMON_SUMHEAD_ALL_LINK", "List All" );
+
+##################################################
+
+?>
