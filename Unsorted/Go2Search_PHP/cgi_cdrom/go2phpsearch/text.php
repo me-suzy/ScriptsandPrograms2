8a849b597b7f14dc380d2d9d4ba6f5893b6f65c6
me-suzy <me.suzana@gmail.com>
2025-08-13 04:59:40 +0300

    Add Unsorted/Go2Search_PHP/cgi_cdrom/go2phpsearch/text.php

---
+++ Unsorted/Go2Search_PHP/cgi_cdrom/go2phpsearch/text.php
@@ -0,0 +1,164 @@
+<?php
+
+# Text.php
+#####################################################
+
+# Buttons #
+###########
+
+$button['addurl']="Add Url";
+
+$button['search']="Normal Search";
+
+$button['quick']="QuickFind";
+
+$button['begin_search']="Begin Power Search";
+
+$button['add_terms']="Add More Terms...";
+
+# Page Elements #
+#################
+
+$page['title']="Zeed:UK Search";
+
+$page['separator']=": ";
+
+$page['add_url']="Add Url";
+
+$page['simple_search']="Simple Search";
+
+$page['power_search']="Power Search";
+
+$page['search_results']="Search Results";
+
+$page['rate_entry']="Rate Entry";
+
+$page['search_error']="Simple Search".$page['separator']."Search Error";
+
+# Text #
+########
+
+$text['top_five']="Top Five";
+
+$text['spider_now']="Spider Now!";
+
+$text['skip']="s k i p";
+
+$text['power_search']="<font face=arial size=-2>P o w e r &nbsp; S e a r c h</font>";
+
+$text['simple_search']="<font face=arial size=-2>B a c k &nbsp; T o &nbsp; N o r m a l &nbsp; S e a r c h</font>";
+
+$text['unrated']="<font face=arial size=-2>U N R A T E D</font>";
+
+$text['rate']="<font face=arial size=-2>Rate This Entry</font>";
+
+$text['must']="Must contain";
+
+$text['cannot']="Cannot contain";
+
+$text['or']="...or/nor";
+
+$text['excellent']="Excellent";
+
+$text['good']="   Good   ";
+
+$text['fair']="    Fair    ";
+
+$text['poor']="    Poor   ";
+
+$text['terrible']=" Terrible ";
+
+$text['stars']="stars";
+
+$text['star']="star";
+
+$text['next']="<font face=arial size=-1>N E X T &gt;</font>";
+
+$text['last']="<font face=arial size=-1>&lt; L A S T</font>";
+
+$text['none_found']="None Found";
+
+$text['view_cache']="View Cached Page";
+
+$text['number_start']="<font face=arial size=-2> (";
+
+$text['number_end']=")</font>";
+
+# Countries #
+#############
+
+$tld_c['int']="International";
+
+$tld_c['Afghanistan']="Afghanistan";
+
+$tld_c['Australia']="Australia";
+
+$tld_c['Austria']="Austria";
+
+$tld_c['Belgium']="Belgium";
+
+$tld_c['Brazil']="Brazil";
+
+$tld_c['Canada']="Canada";
+
+$tld_c['Denmark']="Denmark";
+
+$tld_c['France']="France";
+
+$tld_c['Germany']="Germany";
+
+$tld_c['Greece']="Greece";
+
+$tld_c['HongKong']="Hong Kong";
+
+$tld_c['India']="India";
+
+$tld_c['Indonesia']="Indonesia";
+
+$tld_c['Ireland']="Ireland";
+
+$tld_c['Italy']="Italy";
+
+$tld_c['Japan']="Japan";
+
+$tld_c['Korea']="Republic of Korea";
+
+$tld_c['Mexico']="Mexico";
+
+$tld_c['Netherlands']="The Netherlands";
+
+$tld_c['Zealand']="New Zealand";
+
+$tld_c['Norway']="Norway";
+
+$tld_c['Portugal']="Portugal";
+
+$tld_c['PuertoRico']="Puerto Rico";
+
+$tld_c['Arabia']="Saudi Arabia";
+
+$tld_c['Singapore']="Singapore";
+
+$tld_c['SouthAfrica']="South Africa";
+
+$tld_c['Spain']="Spain";
+
+$tld_c['Sweden']="Sweden";
+
+$tld_c['Switzerland']="Switzerland";
+
+$tld_c['Taiwan']="Taiwan";
+
+$tld_c['Thailand']="Thailand";
+
+$tld_c['Turkey']="Turkey";
+
+$tld_c['Emirates']="United Arab Emirates";
+
+$tld_c['UK']="United Kingdom";
+
+$tld_c['US']="United States";
+
+$tld_c['Venezuela']="Venezuela";
+
+?>
