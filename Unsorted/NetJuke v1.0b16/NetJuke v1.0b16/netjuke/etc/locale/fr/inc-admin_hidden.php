910b181596e1376413f02743423eb0320bc5c9db
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:49 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_hidden.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-admin_hidden.php
@@ -0,0 +1,20 @@
+<?php
+
+##################################################
+
+# /admin/hidden.php
+
+##################################################
+
+define( "HDDN_DENIED_BADDIR", "Le répertoire que vous avez soumis ne correspond pas au répertoire de musique listé dans le fichier de préférence." );
+define( "HDDN_DENIED_NODIR_1", "Veuillez soumettre un répertoire valide." );
+define( "HDDN_DENIED_NODIR_2", "Répertoire de musique introuvable. Veuillez consulter le fichier de préférence." );
+
+define( "HDDN_HEADER", "CHERCHEUR DE FICHIERS CACHÉS" );
+define( "HDDN_CAPTION_1", "Cherche le répertoire soumis pour des fichiers \"invisibles\" (commençant par un point) afin de générer un scripte approprié à les effacer en groupe. Ceci est particuliêrement intéressant si vous téléchargez des fichers depuis un Mac vers un serveur Unix (.AppleDouble, .DS_Store, etc)." );
+define( "HDDN_CAPTION_2", "Veuillez soumettre un répertoire à chercher." );
+define( "HDDN_BTN_SCAN", "Lancer La Recherche" );
+
+##################################################
+
+?>
