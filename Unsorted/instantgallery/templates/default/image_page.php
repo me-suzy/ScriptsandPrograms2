eaf5ce87a84cacfb18286eecac8cf50857c1a88b
me-suzy <me.suzana@gmail.com>
2025-08-13 05:28:49 +0300

    Add Unsorted/instantgallery/templates/default/image_page.php

---
+++ Unsorted/instantgallery/templates/default/image_page.php
@@ -0,0 +1,14 @@
+<br><br><br>
+<TABLE cellspacing="0" cellpadding="1" border="0">
+<tr>
+	<td bgcolor="#000000"><IMG src="<? echo $img ?>" alt="" border="0"></td>
+</tr>
+</table>
+
+<!-- begin scrollbar table -->
+<DIV id="navigate" style="position:absolute; top:250; left:0; z-index:1;">
+<TABLE align="right" cellspacing="0" cellpadding="2" border="0" width="210">
+<TR><TD bgcolor="#ffffff" nowrap><span class="scrolltxt" id="black"><? getScrollBar( $imgno ); ?></span></TD></TR>
+</TABLE>
+</DIV>
+<!-- end scrollbar table -->
