d1cfbd2e6c6fc9462d36f97aee555ac798a13de6
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:33 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_hidden.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-admin_hidden.php
@@ -0,0 +1,20 @@
+<?php
+
+##################################################
+
+# /admin/hidden.php
+
+##################################################
+
+define( "HDDN_DENIED_BADDIR", "El directorio donde se realizará la búsqueda no se encuentra registrado en el archivo de preferencias." );
+define( "HDDN_DENIED_NODIR_1", "Por favor, de un directorio valiso para buscar los archivos escondidos." );
+define( "HDDN_DENIED_NODIR_2", "Directorio sin contenido Musical. Ver Archivo de Prefrencias." );
+
+define( "HDDN_HEADER", "HIDDEN FILE FINDER" );
+define( "HDDN_CAPTION_1", "Buscar los nombres en determinados archivos o subdirectorios (archivos empiezan con w/ un punto) y generar un archivo de proceso que eliminará desde la línea de comandos. Esto es especialmente usado cuando cuando suben archivos desde un Mac a un Unix (.AppleDouble, .DS_Store, etc)" );
+define( "HDDN_CAPTION_2", "Por favor Seleccione un Directorio para Explorar" );
+define( "HDDN_BTN_SCAN", "Iniciando Exploración" );
+
+##################################################
+
+?>
