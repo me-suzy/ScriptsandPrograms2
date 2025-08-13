5486773ec8e33410140bb9c782e7bd451a2585b7
me-suzy <me.suzana@gmail.com>
2025-08-13 05:55:56 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/header_old.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/templates/header_old.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+        <TITLE><%$pageTitle%></TITLE>
+        <META http-equiv="Pragma" content="no-cache">
+        <META http-equiv="Cache-Control" content="no-cache, must-revalidate">
+        <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+</HEAD>
+
+<BODY BGCOLOR="white" MARGINHEIGHT=0 MARGINWIDTH=0 TOPMARGIN=0 LEFTMARGIN=0>
+
+<table width="100%" align="center" >
+<tr>
+<td>
+
+<!--{top menu-->
+
+<table width="100%" align="center" border="1" bgcolor="#cccccc" cellspacing="1" bordercolor="#999999">
+ <tr align="center">
+  <td> <a href="<%$selfURL%>?mode=home">Home</a>
+  <td> <a href="<%$selfURL%>?mode=members">Member area</a>
+  <td> <a href="<%$selfURL%>?mode=affiliates">Affiliate area</a>
+  <td> <a href="admin.php" >Admin area</a>
+  <td> <a href="<%$selfURL%>?mode=home" onclick="alert('under construction');return false;">About</a>
+ </tr>
+
+</table>
+
+<!--}top menu-->
