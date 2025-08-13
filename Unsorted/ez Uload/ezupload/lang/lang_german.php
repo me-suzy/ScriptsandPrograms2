3c9b980a730c79ac348a086737ff66ff348d4d0e
me-suzy <me.suzana@gmail.com>
2025-08-13 04:30:48 +0300

    Add Unsorted/ez Uload/ezupload/lang/lang_german.php

---
+++ Unsorted/ez Uload/ezupload/lang/lang_german.php
@@ -0,0 +1,51 @@
+<?
+  
+  $LANGPACK[$i]['name'] = "Deutsch";
+  $LANGPACK[$i]['file'] = "lang_german.php";
+  
+  // Javascript validation (checkfields.php)
+  $_LANG['js_error'] = "Die folgenden Felder sind falsch";
+  $_LANG['missing_field'] = "Fehlende felder";
+  $_LANG['invalid_address'] = "Falsche E-Mailadresse";
+  $_LANG['max_chars'] = "Max. Zeichen";
+  $_LANG['min_chars'] = "Min. Zeichen";
+  $_LANG['no_option_checked'] = "Keine Option gewählt";
+  $_LANG['invalid_extension'] = "Erweiterung nicht akzeptiert";
+  $_LANG['large_files_warning'] = "Die Dateien werde jetzt hochgeladen. Abhängig von der Grösse und der Verbindungsgeschwindigkeit kann dies einige Zeit in Anspruch nehmen. Bitte etwas geduld haben.";
+
+  // Password page (form_password.php)
+  $_LANG['enter_pass'] = "Bitte Passwort eingeben um zum Uploadformular zu gelannegn"; 
+  $_LANG['enter_user'] = "Bitte Passwort eingeben um zum Uploadformular zu gelannegn"; // when user login is turned on
+  $_LANG['your_email'] = "E-Mail Addresse";
+  $_LANG['your_pass'] = "Ihre Passwort";
+  $_LANG['forgot_pass'] = "Passwort vergessen?";
+  $_LANG['email_admin'] = "E-Mail Webmaster";
+  $_LANG['enter'] = "Eingabe"; // for the button
+	
+  // Form page (form_show.php)
+  $_LANG['upload_files'] = "Dateien hochladen"; // for the upload button
+  $_LANG['upload_folder'] = "Verzeichnis"; // if user has to select sub-directory
+
+  // Upload page (form_upload.php)
+  $_LANG['dir_creation_error'] = "Fehler! Verzeichnis konnte nicht angelegt werdeen! (Zugriff verweigert)";
+  $_LANG['ext_not_accepted'] = "Erweiterung nicht akzeptiert";
+  $_LANG['file_too_large'] = "Datei ist zu gross (Max. ";
+  $_LANG['file_too_small'] = "Datei ist zu klein (Min. ";
+  $_LANG['image_height'] = "Das Bild muss folgende HÖhe haben: ";
+  $_LANG['image_width'] = "Das Bild muss folgende Breite haben: ";
+  $_LANG['pixels'] = " Pixels"; // follows the two field above
+  $_LANG['required_missing'] = "Beötigtes Feld nicht angegeben";
+  $_LANG['maxsize_settings'] = "Maximale Dateigrösse zu Hochladen für das Verzeichnis überschritten";
+  $_LANG['maxsize_form'] = "Maximale Dateigrösse für das Verzeichnis überschritten";
+  $_LANG['partial_upload'] = "Die Datei wurde nur teilweise hochgeladen";
+  $_LANG['invalid_file'] = "Datei hinfällig (Bitte Zurück-Button benutzen)";
+  $_LANG['value_too_short'] = "Eingabe ist zu kurz (Min. ";
+  $_LANG['value_too_long'] = "Eingabe ist zu lang (Max. ";
+  
+  // Results page (form_results.php)
+  $_LANG['upload_error'] = "Achtung! Der Upload war nicht erfolgreich! Bitte die unteren Details ansehen und mit veränderten Einstellungen noch mal versuchen.";
+  $_LANG['try_again'] = "Noch mal versuchen"; // for a button
+  $_LANG['upload_more'] = "Weitere Dateien Hochladen"; // for a button
+  $_LANG['continue'] = "Weiter"; // for a button
+  
+?>
