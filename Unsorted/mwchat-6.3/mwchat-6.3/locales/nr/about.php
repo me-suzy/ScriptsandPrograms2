fb4fe3f7c5f15052b5cc9490a1a882442be71ab0
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:19 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/about.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/nr/about.php
@@ -0,0 +1,15 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[ABOUT_Info] = "MWChat er et PHP on-line pratsystem som skrevet av Application Index. Anvendelse Index er et programvareselskap som basert i Alpha, IL. MWChat gir kjennetegn slik som arkiv delende, buddy lister, private budskap, private og offentlige rom, brukerprofiler, brukerikoner, temaer, og mere! MWChat er meget lik en browser basert IRC klient bare der ikke er noen behov for en backend tjener, og programvaren vedlikeholder seg. De kan download MWChat på:";
+
+?>
