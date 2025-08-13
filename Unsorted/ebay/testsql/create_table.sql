b8c03d80c4af9ab1787a23f0a19fc0ed92a19097
me-suzy <me.suzana@gmail.com>
2025-08-13 03:37:02 +0300

    Add Unsorted/ebay/testsql/create_table.sql

---
+++ Unsorted/ebay/testsql/create_table.sql
@@ -0,0 +1,33 @@
+/*	$Id: create_table.sql,v 1.2 1999/02/21 02:55:40 josh Exp $	*/
+/* create testsql table */
+
+@ebay_marketplaces
+@ebay_users
+@ebay_categories
+@ebay_items
+@ebay_user_info
+
+@ebay_account_balances    
+@ebay_accounts            
+@ebay_admin             
+@ebay_bb_board 
+@ebay_qa_board    
+@ebay_bids              
+          
+@ebay_feedback
+@ebay_feedback_detail 
+@ebay_item_counts        
+@ebay_item_desc       
+@ebay_item_info      
+              
+@ebay_marketplaces_info 
+
+@ebay_renamed_users
+@ebay_special_users
+
+@insert_users
+@setup_ebay_marketplaces
+
+@category_script
+
+@ebay_item_high_bidder
