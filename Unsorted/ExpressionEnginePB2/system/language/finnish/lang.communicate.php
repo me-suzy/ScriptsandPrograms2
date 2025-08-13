9d2df62c963a727469f4e472ac156b0724fbdafa
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:50 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.communicate.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.communicate.php
@@ -0,0 +1,172 @@
+<?php
+$L = array(
+
+
+"your_name" =>
+"Nimesi",
+
+"your_email" =>
+"Emailisi",
+
+"recipient" =>
+"Vastaanottaja",
+
+"cc" =>
+"Kopio",
+
+"bcc" =>
+"Piilokopio",
+
+"recipient_group" =>
+"Lähetä jäsenten ryhmälle",
+
+"manual_recipients_ignored" =>
+"Vastaanottajan kenttä alla jätetään huomioimatta",
+
+"send_to_mailinglist" =>
+"Lähetä postituslistalle?",
+
+"honor_email_pref" =>
+"Lähetä vain jäsenille jotka ovat valinneet hyväksy email",
+
+"separate_emails_with_comma" =>
+"Erota osoitteet pilkulla",
+
+"no_email_matching_criteria" =>
+"Valitsemallasi kriteerillä ei löytynyt email-osoitteita",
+
+"not_allowed_to_email_members" =>
+"Sinulla ei ole oikeutta lähettä emailia jäsenille",
+
+"not_allowed_to_email_member_groups" =>
+"Sinulla ei ole oikeutta lähettää emailia jäsenten ryhmään",
+
+"not_allowed_to_email_mailinglist" =>
+"Sinulla ei ole oikeutta lähettää emailia postituslistalle",
+
+"subject" =>
+"Aihe",
+
+"message" =>
+"Viesti",
+
+"send_an_email" =>
+"Lähetä email",
+
+"sending_email" =>
+"Lähetetään emailia",
+
+"batchmode_ready_to_begin" =>
+"Viestin lähetys toiminto alkaa viiden(5) sekunnin sisällä",
+
+"batchmode_warning" =>
+"Älä koske selaimeesi ennen kuin lähetysprosessi on kokonaan valmis ",
+
+"problem_with_id" =>
+"Havaittin ongelma tunnusnumeron kanssa lähettäessä emaileja",
+
+"cache_data_missing" =>
+"Emailin välimuisti dataa ei havaittu",
+
+"currently_sending_batch" =>
+"Nyt lähetetään emailia %x  %y:stä",
+
+"emails_remaining" =>
+"Emaileja jäljellä:",
+
+"email_error" =>
+"Email virhe",
+
+"send_it" =>
+"Lähetä viesti",
+
+"total_emails_sent" =>
+"Kaikkiaan viestejä lähetettiin:",
+
+"plain_text" =>
+"Vain teksti",
+
+"html" =>
+"HTML",
+
+"mail_format" =>
+"Muotoilu",
+
+"word_wrap" =>
+"Sanankierrätys",
+
+"priority" =>
+"Prioriteetti",
+
+"chars" =>
+"Merkkejä",
+
+"highest" =>
+"Korkein",
+
+"high" =>
+"Korkea",
+
+"normal" =>
+"Normaali",
+
+"low" =>
+"Alhainen",
+
+"lowest" =>
+"Alhaisin",
+
+"empty_form_fields" =>
+"Jätit jonkun kentän tyhjäksi.",
+
+"email_sent_message" =>
+"Viestisi on lähetetty",
+
+"all_email_sent_message" =>
+"kaikki viestisi on lähetetty",
+
+"email_sent" =>
+"Emailin lähetys",
+
+"view_email_cache" =>
+"Katsele aiemmin lähetettyjä emaileja",
+
+"previous_email" =>
+"Aiemmin lähetetty email",
+
+"email_title" =>
+"Emailin otsikko",
+
+"email_date" =>
+"Lähetyspäivä",
+
+"total_recipients" =>
+"Vastaanottajia",
+
+"resend" =>
+"Uudelleen lähetys",
+
+"view" =>
+"Tarkastele",
+
+"no_cached_email" =>
+"Välimuistissa ei ole viestejä",
+
+"delete_emails" =>
+"Poista viesti",
+
+"delete_confirm" =>
+"Poista viestin vahvistus",
+
+"delete_question" =>
+"Oletko varma, että haluat poistaa valitut viestit?",
+
+"email_deleted" =>
+"Viesti on poistettu",
+
+"mailinglist_unsubscribe" =>
+"Poistaaksesi emailisi postituslistalta, paina tästä:",
+
+''=>''
+);
+?>
