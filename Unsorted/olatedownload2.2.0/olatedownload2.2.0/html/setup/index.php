85ef3154eae44384b5e764a289654cde669c0919
me-suzy <me.suzana@gmail.com>
2025-08-13 07:09:02 +0300

    Add Unsorted/olatedownload2.2.0/olatedownload2.2.0/html/setup/index.php

---
+++ Unsorted/olatedownload2.2.0/olatedownload2.2.0/html/setup/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Setup - Welcome</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" type="text/css" href="css/style.css" title="default" />
+</head>
+
+<body>
+<table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td valign="top"><hr width="1" size="1" color="#FFFFFF">
+<table class="admin_main" align="center" border="1">
+<tr>
+<td class="admin_title"><table class="admin_title_table">
+<tr>
+<td class="large admin_title">Olate Download - Download Management Script</td>
+</tr>
+</table></td>
+</tr>
+<tr>
+<td class="admin_breadcrumb">
+<table width="99%"  border="0" align="center" cellpadding="0" cellspacing="0">
+<tr>
+<td width="100%"><strong>Setup - Welcome</strong></td>
+</tr>
+</table></td>
+</tr>
+<tr>
+<td valign="top" bordercolor="#FFFFFF"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td><p>Welcome to the Olate Download Download Management Script Setup. </p>
+<p>This script allows you to create a new installation of Olate Download or upgrade an existing installation. If this is the first time you have ever installed Olate Download please choose the Install option below. However, if you wish to import existing data from Olate Download v2.0/2.1 and upgrade to v2.2, please select Upgrade below. </p>
+<p><form action="install1.php" method="post">
+<p><strong>Install</strong></p>
+<p>Create a new installation of Olate Download 2.2</p>
+<p>
+<input type="submit" value="Proceed" name="Submit">
+</p>
+</form><form action="upgrade1.php" method="post">
+<p><strong>Upgrade</strong></p>
+<p>Upgrade from Olate Download v2.0/2.1 to v2.2. </p>
+<p>
+<input type="submit" value="Proceed" name="Submit">
+</p>
+</form></td>
+</tr>
+</table></td>
+</tr>
+<tr>
+<td height="25" valign="middle" bordercolor="#FFFFFF" bgcolor="#E3E8EF">
+<!--Begin Credit Line. Please leave-->
+<div align="center"><span class="small"><a href="http://www.olate.com" target="_blank">Powered 
+by Olate Download v2.2.0 </a></span></div></td>
+</tr>
+</table>
+<hr width="1" size="1" color="#FFFFFF"></td>
+</tr>
+</table>
+</body>
+</html>
