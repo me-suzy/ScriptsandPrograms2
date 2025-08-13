0928f58336e3543626f32ddbd279a300eba3009d
me-suzy <me.suzana@gmail.com>
2025-08-13 04:21:22 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/english/lang.publish.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/english/lang.publish.php
@@ -0,0 +1,463 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Publish page
+//----------------------------
+
+"status_changed" =>
+"Status Changed",
+
+"publish" => 
+"Publish",
+
+"no_weblog_exits" => 
+"You have tried to access a weblog that does not exist.",
+
+"no_entries_matching_that_criteria" => 
+"There are no entries matching the criteria you selected",
+
+"no_entries_exist" => 
+"There are no entries in this weblog",
+
+"select_blog_to_post_in" => 
+"Choose a weblog to post in",
+
+"select_blog_to_edit" => 
+"Choose a weblog to view",
+
+"this_entry_will_appear_in" => 
+"This entry will appear in:",
+
+"unauthorized_for_this_blog" => 
+"You are not authorized to post in this weblog",
+
+"unauthorized_for_any_blogs" => 
+"You are not authorized to post weblog entries",
+
+"unauthorized_to_delete_others" =>
+"You are only authorized to delete weblog entries that you authored",
+
+"unauthorized_to_delete_self" =>
+"You are not authorized to delete your own weblog entries",
+
+"weblog" =>
+"Weblog",
+
+"weblogs" =>
+"Weblogs",
+
+"new_entry" =>
+"New Entry",
+
+"button_mode" =>
+"Button Mode:",
+
+"guided" =>
+"Guided",
+
+"normal" =>
+"Normal",
+
+"view_entry" =>
+"View Entry",
+
+"preview_entry" =>
+"Preview Entry",
+
+"edit_entry" =>
+"Edit entry",
+
+"edit_weblog_entries" =>
+"Edit Weblog Entries",
+
+"continue_editing" => 
+"Continue Editing",
+
+"entry_status" => 
+"Entry Status",
+
+"status" => 
+"Status",
+
+"comments" => 
+"Comments",
+
+"open" => 
+"Open",
+
+"close" => 
+"Close",
+
+"closed" => 
+"Closed",
+
+"options" => 
+"Options",
+
+"sticky" => 
+"Make Entry Sticky",
+
+"allow_comments" => 
+"Allow Comments",
+
+"allow_trackbacks" => 
+"Allow Trackbacks",
+
+"ping_sites" => 
+"Sites To Ping",
+
+"ping_urls" => 
+"Trackback URLs to ping",
+
+"auto_discovery" =>
+"Trackback Auto-discovery",
+
+"select_entries_to_ping" =>
+"Select the weblog entries you would like to ping",
+
+"view_previous_pings" => 
+"View Previous Pings",
+
+"previiously_pinged_urls" => 
+"You previously pinged the following URLs",
+
+"date" => 
+"Date",
+
+"expiration_date" => 
+"Expiration Date",
+
+"invalid_date_formatting" => 
+"The date you submitted was not formatted correctly (year-month-day hour:minute)",
+
+"date_outside_of_range" => 
+"The date you submitted is outside the accepted range (1902 - 2037).",
+
+"category" =>
+"Category",
+
+"categories" => 
+"Categories",
+
+"no_categories" => 
+"No Categories Exist",
+
+"title" => 
+"Title",
+
+"url_title" => 
+"URL Title",
+
+"author" => 
+"Author",
+
+"newline_format" => 
+"Formatting:",
+
+"none" => 
+"None",
+
+"link" => 
+"Link",
+
+"image" => 
+"Image",
+
+"email" => 
+"Email",
+
+"upload" => 
+"Upload",
+
+"file_upload" =>
+"File Upload",
+
+"emoticons" =>
+"Emoticons",
+
+"choose_a_destination_for_emoticon" =>
+"Choose a destination for your emoticon",
+
+"click_emoticon" =>
+"Click an image to place it in your entry",
+
+"no_emoticons" =>
+"Unable to find the location of your emoticons",
+
+"close_all" => 
+"Close All",
+
+"missing_title" =>
+"Your weblog entry must have a title.",
+
+"missing_date" =>
+"Your weblog entry must have a date.",
+
+"title_not_unique" =>
+"An entry already exists with this title.  Titles must be unique.",
+
+"url_title_not_unique" =>
+"An entry already exists with this URL title.  URL titles must be unique.",
+
+"entry_has_been_added" =>
+"New Entry Submitted",
+
+"entry_has_been_updated" =>
+"Entry Has Been Updated",
+
+"trackback_url_errors" =>
+"The following trackback pings were not accepted:",
+
+"xmlrpc_ping_errors" =>
+"The following XML-RPC pings were not accepted:",
+
+"click_to_view_your_entry" =>
+"Click here to view your entry",
+
+"view" => 
+"View",
+
+"edit_this_entry" => 
+"Edit This Entry",
+
+"date_range" =>
+"Date Range",
+
+"today" =>
+"Today",
+
+"past_week" =>
+"Last 7 days",
+
+"past_month" =>
+"Last 30 days",
+
+"past_six_months" =>
+"Last 180 days",
+
+"past_year" =>
+"Last 365 days",
+
+"any_date" =>
+"Any Date",
+
+"results_per_page" =>
+"Results Per Page",
+
+"order" =>
+"Order",
+
+"ascending" =>
+"Oldest First",
+
+"descending" =>
+"Newest First",
+
+"alpha" =>
+"Alphabetical",
+
+"delete_confirm" =>
+"Delete Confirm",
+
+"delete_entry_confirm" =>
+"Are you sure you want to permanently delete this entry?",
+
+"delete_entries_confirm" =>
+"Are you sure you want to permanently delete these entries?",
+
+"entries_deleted" =>
+"Deleted",
+
+"html_buttons_no_cursor" =>
+"In order to use these buttons you must first place your cursor in a field.",
+
+"html_buttons_url_text" =>
+"Enter the hyperlink URL",
+
+"html_buttons_webpage_text" =>
+"Enter the title of the link",
+
+"html_buttons_title_text" =>
+"Optional: Enter a title attribute",
+
+"html_buttons_image_text" =>
+"Enter the image URL",
+
+"html_buttons_image_text" =>
+"Enter the image URL",
+
+"html_buttons_email_text" =>
+"Enter the email address",
+
+"html_buttons_email_title" =>
+"Enter the link title (or leave the field empty to use the email address as the title.)",
+
+"html_buttons_enter_text" =>
+"Enter the text you want to format",
+
+"warning" =>
+"Warning",
+
+"file_exists" =>
+"A file already exists with this name",
+
+"overwrite_instructions" =>
+"You can either choose a new name and the file will be renamed, or you can submit this form with the same name and the file will be overwritten.",
+
+"select_destination_dir" =>
+"Select a destination directory",
+
+"file_type" =>
+"File Type",
+
+"image" =>
+"Image",
+
+"non_image" =>
+"Non-image",
+
+"success" =>
+"Success!",
+
+"you_have_uploaded" =>
+"You have uploaded the following file:",
+
+"choose_a_destination" =>
+"Where would like your file to appear?",
+
+"no_entry_fields" =>
+"There are no entry field in this weblog.",
+
+"no_comments_or_trackbacks" =>
+"There are no commments or trackbacks for this entry",
+
+"posted_by" =>
+"Author:",
+
+"located_in" =>
+"Location:",
+
+"comment_date" =>
+"Date:",
+
+"comment_email" =>
+"Email:",
+
+"comment_url" =>
+"URL:",
+
+"comment_action" =>
+"Action:",
+
+"comment_ip" =>
+"IP:",
+
+"weblog_name" =>
+"Weblog:",
+
+"comment" =>
+"Comment",
+
+"missing_comment" =>
+"You must submit a comment",
+
+"edit_comment" =>
+"Edit Comment",
+
+"delete_comment" =>
+"Delete Comment",
+
+"comment_deleted" =>
+"Comment Deleted",
+
+"delete_trackback" =>
+"Delete Trackback",
+
+"trackback_deleted" =>
+"Trackback Deleted",
+
+"comment_updated" =>
+"Comment Updated",
+
+"delete_comment_confirm" =>
+"Are you sure you want to permanently delete this comment?",
+
+"delete_trackback_confirm" =>
+"Are you sure you want to permanently delete this trackback?",
+
+"name" =>
+"Name",
+
+"url" =>
+"URL",
+
+"location" =>
+"Location",
+
+"continue" =>
+"Continue",
+
+"image_options" =>
+"Image Options",
+
+"create_popup" =>
+"Create pop-up for image?",
+
+"create_thumb" =>
+"Create Image Thumbnail",
+
+"constrain_proportions" =>
+"Constrain Proportions",
+
+"thumb_settings" =>
+"Image Settings",
+
+"choose_a_destination_for_thumb" =>
+"Where would you like the image to appear?",
+
+"do_not_place_file" =>
+"Do not place file in entry",
+
+"image_size_not_different" =>
+"The width and height values you submitted are the same as the original.",
+
+"width" =>
+"Width",
+
+"height" =>
+"Height",
+
+"pixels" =>
+"Pixels",
+
+"percent" =>
+"Percent",
+
+"resize_image" =>
+"Resize Image",
+
+"create_thumb_copy" =>
+"Create a separate copy",
+
+"resize_original" =>
+"Resize the original image",
+
+"thumb_instructions" =>
+"You may optionally resize your uploaded image, or create a separate thumbnail.",
+
+"thumbnail_created" =>
+"Thumbnail Created",
+
+"image_resized" =>
+"Your image has been resized",
+
+"close_window" =>
+"Close Window",
+
+// END
+''=>''
+);
+
+?>
