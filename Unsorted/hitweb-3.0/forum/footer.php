794b73bfb2299b96cdd5ff0ded51a72c13a73d3a
me-suzy <me.suzana@gmail.com>
2025-08-13 05:09:23 +0300

    Add Unsorted/hitweb-3.0/forum/footer.php

---
+++ Unsorted/hitweb-3.0/forum/footer.php
@@ -0,0 +1,10 @@
+      </td>
+      <td bgcolor='#E0C695' width='16'>&nbsp;</td>
+    </tr>
+    <tr>
+      <td bgcolor='#E0C695' colspan='3' height='5'>&nbsp;</td>
+    </tr>
+  </table>
+</center>
+</body>
+</html>
