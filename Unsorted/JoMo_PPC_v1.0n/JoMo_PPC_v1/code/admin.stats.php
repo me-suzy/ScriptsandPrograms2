12fbf0d6277950979bb95502dd05146b53f68eaa
me-suzy <me.suzana@gmail.com>
2025-08-13 05:51:19 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/code/admin.stats.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/code/admin.stats.php
@@ -0,0 +1,60 @@
+<?
+/*
+###############################
+#
+# JoMo Easy Pay-Per-Click Search Engine v1.0
+#
+#
+###############################
+#
+# Date                 : September 16, 2002
+# supplied by          : CyKuH [WTN]
+# nullified by         : CyKuH [WTN]
+#
+#################
+#
+# This script is copyright L 2002-2012 by Rodney Hobart (JoMo Media Group),
+All Rights Reserved.
+#
+# The use of this script constitutes acceptance of any terms or conditions,
+#
+# Conditions:
+#  -> Do NOT remove any of the copyright notices in the script.
+#  -> This script can not be distributed or resold by anyone else than the
+author, unless special permisson is given.
+#
+# The author is not responsible if this script causes any damage to your
+server or computers.
+#
+#################################
+
+*/
+?>
+<?PHP
+
+/**
+admin statistics 
+$statsMode = clickstats, keywordstats
+*/
+
+
+        if (!$sID->assigned("isAdminLogin")) {
+                header("Location: admin.php?mode=login");
+                exit;
+        }
+        
+/**
+input:
+
+filter:
+
+$groupby=
+$detailsLevel
+
+*/
+
+
+    $tpl->display("admin/template.admin.stats.php");
+
+?>
+
