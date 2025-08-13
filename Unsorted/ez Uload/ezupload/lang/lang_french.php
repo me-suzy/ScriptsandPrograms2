700002a4d502c53f057c796ea289d4fb8ea75548
me-suzy <me.suzana@gmail.com>
2025-08-13 04:30:46 +0300

    Add Unsorted/ez Uload/ezupload/lang/lang_french.php

---
+++ Unsorted/ez Uload/ezupload/lang/lang_french.php
@@ -0,0 +1,51 @@
+<?
+ 
+  $LANGPACK[$i]['name'] = "Francais";
+  $LANGPACK[$i]['file'] = "lang_french.php";
+  
+  // Javascript validation (checkfields.php)
+  $_LANG['js_error'] = "The champs suivants ne sont pas corrects";
+  $_LANG['missing_field'] = "Champ manquant";
+  $_LANG['invalid_address'] = "Adresse email non valide";
+  $_LANG['max_chars'] = "Caractères maximum";
+  $_LANG['min_chars'] = "Caractères minimum";
+  $_LANG['no_option_checked'] = "Aucune option sélectionnée";
+  $_LANG['invalid_extension'] = "Extension pas acceptée";
+  $_LANG['large_files_warning'] = "Vos fichiers vont être envoyés maintenant. Selon la taille de ces fichiers et la vitesse de votre connection Internet, il se peut que cela prenne un certain temps. Soyez patient.";
+
+  // Password page (form_password.php)
+  $_LANG['enter_pass'] = "Entrez le mot de passe pour continuer";
+  $_LANG['enter_user'] = "Identifiez vous pour accéder au formulaire"; // when user login is turned on
+  $_LANG['your_email'] = "Adresse Email";
+  $_LANG['your_pass'] = "Mot de Passe";
+  $_LANG['forgot_pass'] = "Pas de mot de passe?";
+  $_LANG['email_admin'] = "Ecrire au webmestre";
+  $_LANG['enter'] = "Entrer"; // for the button
+	
+  // Form page (form_show.php)
+  $_LANG['upload_files'] = "Soumettre"; // for the upload button
+  $_LANG['upload_folder'] = "Dossier"; // if user has to select sub-directory
+
+  // Upload page (form_upload.php)
+  $_LANG['dir_creation_error'] = "Erreur! Impossible de créer le dossier! (Pas la permission)";
+  $_LANG['ext_not_accepted'] = "Extension pas acceptée";
+  $_LANG['file_too_large'] = "Fichier trop gros (Max. ";
+  $_LANG['file_too_small'] = "Fichier trop petit (Min. ";
+  $_LANG['image_height'] = "L'hauteur de l'image doit être de ";
+  $_LANG['image_width'] = "La largeur de l'image doit être de ";
+  $_LANG['pixels'] = " pixels"; // follows the two field above
+  $_LANG['required_missing'] = "Ce champ requis est manquant";
+  $_LANG['maxsize_settings'] = "Valeur de upload_max_filesize dépassée";
+  $_LANG['maxsize_form'] = "Valeur de MAX_FILE_SIZE dépassée";
+  $_LANG['partial_upload'] = "Le fichier a été que partiellement uploadé";
+  $_LANG['invalid_file'] = "Fichier incorrect (Utilisez bouton Parcourir)";
+  $_LANG['value_too_short'] = "Valeur trop courte (Min. ";
+  $_LANG['value_too_long'] = "Valeur trop longue (Max. ";
+  
+  // Results page (form_results.php)
+  $_LANG['upload_error'] = "Attention, votre submission n'a pas été faite! Les détails sont ci-dessous. Cliquez sur \"Réessayer\" pour faire un autre essai.";
+  $_LANG['try_again'] = "Réessayer"; // for a button
+  $_LANG['upload_more'] = "Envoyer Plus"; // for a button
+  $_LANG['continue'] = "Continuer"; // for a button
+  
+?>
