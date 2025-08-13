c2bded97aadd70b5ddd7ceeae1ee1210cdbd0b38
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:44 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.core.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.core.php
@@ -0,0 +1,364 @@
+<?php
+$L = array(
+
+
+"on" =>
+"activado",
+
+"off" =>
+"desactivado",
+
+"first" =>
+"Primero",
+
+"last" =>
+"Ultimo",
+
+"enabled" =>
+"activado",
+
+"disabled" =>
+"desactivado",
+
+"back" =>
+"Atrás",
+
+"submit" =>
+"Enviar",
+
+"update" =>
+"Actualizar",
+
+"thank_you" =>
+"¡Gracias por tu visita!",
+
+"system_off_msg" =>
+"Este sitio está actualmente inactivo.",
+
+"not_authorized" =>
+"No estás autorizado a realizar ésta acción",
+
+"auto_redirection" =>
+"Autoconexión en %x segundos",
+
+"error" =>
+"Error",
+
+"invalid_url" =>
+"La URL que has enviado no es válida.",
+
+"submission_error" =>
+"El formulario que acabas de enviar contiene los siguientes errores:",
+
+"general_error" =>
+"Se han hallado los errores siguientes:",
+
+"invalid_action" =>
+"La acción que has solicitado no es válida.",
+
+"banned" =>
+"Expulsado",
+
+"guests" =>
+"Invitados",
+
+"members" =>
+"Miembros",
+
+"pending" =>
+"Pendiente",
+
+"super_admins" =>
+"Super Administradores",
+
+"error_tag_syntax" =>
+"La siguiente etiqueta tiene un error de sintaxis:",
+
+"error_fix_syntax" =>
+"Por favor corrige la sintaxis en tu plantilla.",
+
+"forgotten_email_sent" =>
+"Se han enviado por email instrucciones para reiniciar tu contraseña.",
+
+"error_sending_email" =>
+"No es posible enviar email en este momento.",
+
+"no_email_found" =>
+"La dirección de email que has indicado no se encuentra en la base de datos.",
+
+"your_new_login_info" =>
+"Información de conexión",
+
+"reset_link" =>
+"Para reiniciar tu contraseña, por favor vete a la página siguiente:",
+
+"password_will_be_reset" =>
+"Tu contraseña se reiniciará auntomáticamente y recibirás una nueva por email.",
+
+"ignore_password_message" =>
+"Si no deseas reiniciar tu contraseña, ignora este mensaje. Expirará en 24 horas.",
+
+"password_has_been_reset" =>
+"Se ha reiniciato tu contraseña y enviado una nueva por email a tu dirección.",
+
+"new_login_info" =>
+"Esta es tu nueva información de acceso:",
+
+"year" =>
+"year",
+
+"years" =>
+"years",
+
+"month" =>
+"month",
+
+"months" =>
+"months",
+
+"week" =>
+"week",
+
+"weeks" =>
+"weeks",
+
+"day" =>
+"day",
+
+"days" =>
+"dias",
+
+"hour" =>
+"hour",
+
+"hours" =>
+"horas",
+
+"minute" =>
+"minute",
+
+"minutes" =>
+"minutos",
+
+"second" =>
+"second",
+
+"seconds" =>
+"segundos",
+
+"am" =>
+"am",
+
+"pm" =>
+"pm",
+
+"AM" =>
+"AM",
+
+"PM" =>
+"PM",
+
+"Sun" =>
+"Dom",
+
+"Mon" =>
+"Lun",
+
+"Tue" =>
+"Mar",
+
+"Wed" =>
+"Mie",
+
+"Thu" =>
+"Jue",
+
+"Fri" =>
+"Vie",
+
+"Sat" =>
+"Sab",
+
+"Sunday" =>
+"Domingo",
+
+"Monday" =>
+"Lunes",
+
+"Tuesday" =>
+"Martes",
+
+"Wednesday" =>
+"Miércoles",
+
+"Thursday" =>
+"Jueves",
+
+"Friday" =>
+"Viernes",
+
+"Saturday" =>
+"Sábado",
+
+"Jan" =>
+"Ene",
+
+"Feb" =>
+"Feb",
+
+"Mar" =>
+"Mar",
+
+"Apr" =>
+"Abr",
+
+"May" =>
+"May",
+
+"Jun" =>
+"Jun",
+
+"Jul" =>
+"Jul",
+
+"Aug" =>
+"Ago",
+
+"Sep" =>
+"Sep",
+
+"Oct" =>
+"Oct",
+
+"Nov" =>
+"Nov",
+
+"Dec" =>
+"Dic",
+
+"January" =>
+"Enero",
+
+"February" =>
+"Febrero",
+
+"March" =>
+"Marzo",
+
+"April" =>
+"Abril",
+
+"June" =>
+"Junio",
+
+"July" =>
+"Julio",
+
+"August" =>
+"Agosto",
+
+"September" =>
+"Septiembre",
+
+"October" =>
+"Octubre",
+
+"November" =>
+"Noviembre",
+
+"December" =>
+"Diciembre",
+
+"UM12" =>
+"(UTC - 12:00) Enitwetok, Kwajalien",
+
+"UM11" =>
+"(UTC - 11:00) Nome, Midway Island, Samoa",
+
+"UM10" =>
+"(UTC - 10:00) Hawaii",
+
+"UM9" =>
+"(UTC - 9:00) Alaska",
+
+"UM8" =>
+"(UTC - 8:00) Pacific Time",
+
+"UM7" =>
+"(UTC - 7:00) Mountain Time",
+
+"UM6" =>
+"(UTC - 6:00) Central Time, Mexico City",
+
+"UM5" =>
+"(UTC - 5:00) Eastern Time, Bogota, Lima, Quito",
+
+"UM4" =>
+"(UTC - 4:00) Atlantic Time, Caracas, La Paz",
+
+"UM25" =>
+"(UTC - 3:30) Newfoundland",
+
+"UM3" =>
+"(UTC - 3:00) Brazil, Buenos Aires, Georgetown, Falkland Is.",
+
+"UM2" =>
+"(UTC - 2:00) Mid-Atlantic, Ascention Is., St Helena",
+
+"UM1" =>
+"(UTC - 1:00) Azores, Cape Verde Islands",
+
+"UTC" =>
+"(UTC) Casablanca, Dublin, Edinburgh, London, Lisbon, Monrovia",
+
+"UP1" =>
+"(UTC + 1:00) Berlin, Brussels, Copenhagen, Madrid, Paris, Rome",
+
+"UP2" =>
+"(UTC + 2:00) Kaliningrad, South Africa, Warsaw",
+
+"UP3" =>
+"(UTC + 3:00) Baghdad, Riyadh, Moscow, Nairobi",
+
+"UP25" =>
+"(UTC + 3:30) Tehran",
+
+"UP4" =>
+"(UTC + 4:00) Adu Dhabi, Baku, Muscat, Tbilisi",
+
+"UP35" =>
+"(UTC + 4:30) Kabul",
+
+"UP5" =>
+"(UTC + 5:00) Islamabad, Karachi, Tashkent",
+
+"UP45" =>
+"(UTC + 5:30) Bombay, Calcutta, Madras, New Delhi",
+
+"UP6" =>
+"(UTC + 6:00) Almaty, Colomba, Dhakra",
+
+"UP7" =>
+"(UTC + 7:00) Bangkok, Hanoi, Jakarta",
+
+"UP8" =>
+"(UTC + 8:00) Beijing, Hong Kong, Perth, Singapore, Taipei",
+
+"UP9" =>
+"(UTC + 9:00) Osaka, Sapporo, Seoul, Tokyo, Yakutsk",
+
+"UP85" =>
+"(UTC + 9:30) Adelaide, Darwin",
+
+"UP10" =>
+"(UTC + 10:00) Melbourne, Papua New Guinea, Sydney, Vladivostok",
+
+"UP11" =>
+"(UTC + 11:00) Magadan, New Caledonia, Solomon Islands",
+
+"UP12" =>
+"(UTC + 12:00) Auckland, Wellington, Fiji, Marshall Island",
+
+''=>''
+);
+?>
