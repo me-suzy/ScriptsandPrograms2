7f515915f40677b2145fd32066ed25e5fac71aae
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:44 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-tr-info.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-tr-info.php
@@ -0,0 +1,42 @@
+<?php
+
+##################################################
+
+# /tr-info.php
+
+##################################################
+
+define( "TRINFO_NOID", "Por favor seleccione una cancion valida." );
+
+define( "TRINFO_HEADER", "INFORMACIÓN DETALLADA DE LA CANCIÓN" );
+
+define( "TRINFO_FORM_TR", "Nombre Canción" );
+define( "TRINFO_FORM_AR", "Nombre Artísta" );
+define( "TRINFO_FORM_AL", "Nombre Álbum" );
+define( "TRINFO_FORM_GE", "Nombre Género" );
+define( "TRINFO_FORM_TI", "Duración Canción" );
+define( "TRINFO_FORM_TN", "Canción Número" );
+define( "TRINFO_FORM_BR", "Tasa de Bits" );
+define( "TRINFO_FORM_SR", "Frecuencia de muestreo" );
+define( "TRINFO_FORM_FK", "Tipo de Archivo" );
+define( "TRINFO_FORM_FS", "Tamaño Archivo" );
+define( "TRINFO_FORM_FN", "Nombre del Archivo" );
+define( "TRINFO_FORM_LC", "Contador de oyentes" );
+
+define( "TRINFO_FORM_CM", "Ver Comentarios" );
+define( "TRINFO_FORM_LY", "Ver Liricos" );
+
+define( "TRINFO_IMG_TR", "Muestra de Canciones" );
+define( "TRINFO_IMG_AR", "Muestra de Artista" );
+define( "TRINFO_IMG_AL", "Muestra de Álbumes" );
+
+define( "TRINFO_PLAY", "Tocar de nuevo" );
+define( "TRINFO_PLAY_HELP", "Click para tocar esta canción." );
+define( "TRINFO_EDIT", "Modo de edición" );
+define( "TRINFO_EDIT_HELP", "Cambiar a modo de edición." );
+define( "TRINFO_CLOSEWIN", "Cerrar ventana" );
+define( "TRINFO_CLOSEWIN_HELP", "Cerrar esta ventana" );
+
+##################################################
+
+?>
