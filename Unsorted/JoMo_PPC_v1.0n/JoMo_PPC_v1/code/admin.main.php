c783af4d522cf7fd650535f52f6babd01ce2cc52
me-suzy <me.suzana@gmail.com>
2025-08-13 05:51:08 +0300

    Add Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/code/admin.main.php

---
+++ Unsorted/JoMo_PPC_v1.0n/JoMo_PPC_v1/code/admin.main.php
@@ -0,0 +1,45 @@
+<?
+/*
+###############################
+#
+# JoMo Easy Pay-Per-Click Search Engine v1.0
+#
+#
+###############################
+#
+# Date                 : September 16, 2002
+# supplied by          : CyKuH [WTN]
+# nullified by         : CyKuH [WTN]
+#
+#################
+#
+# This script is copyright L 2002-2012 by Rodney Hobart (JoMo Media Group),
+All Rights Reserved.
+#
+# The use of this script constitutes acceptance of any terms or conditions,
+#
+# Conditions:
+#  -> Do NOT remove any of the copyright notices in the script.
+#  -> This script can not be distributed or resold by anyone else than the
+author, unless special permisson is given.
+#
+# The author is not responsible if this script causes any damage to your
+server or computers.
+#
+#################################
+
+*/
+?>
+<?php
+
+/**
+admin. main
+*/
+
+	checkAdminPage();
+ 
+ // member info
+ 
+ $tpl->display("admin/template.admin.main.php");
+
+?>
