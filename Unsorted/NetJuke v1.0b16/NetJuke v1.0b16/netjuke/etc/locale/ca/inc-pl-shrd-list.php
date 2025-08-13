4febbdfd75154a382fad8abeef9e25a0fe74b767
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:42 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-pl-shrd-list.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-pl-shrd-list.php
@@ -0,0 +1,28 @@
+<?php
+
+##################################################
+
+# /pl-shrd-list.php
+
+##################################################
+
+define( "PLSHRD_SHRD_HEADER", "LLISTA COMPLETA DE LLISTES COMPARTIDES" );
+define( "PLSHRD_SHRD_PLAY_HELP", "Feu clic per reproduir la llista de cançons." );
+define( "PLSHRD_SHRD_VIEW_HELP", "Feu clic per accedir als detalls d'aquesta llista de cançons." );
+define( "PLSHRD_SHRD_BY", "Per" );
+define( "PLSHRD_SHRD_FOR", "en" );
+
+define( "PLSHRD_TB_PREV", "Pàgina anterior" );
+define( "PLSHRD_TB_PREV_HELP", "Feu clic per accedir a la pàgina anterior de llistes compartides." );
+define( "PLSHRD_TB_NEXT", "Pàgina següent" );
+define( "PLSHRD_TB_NEXT_HELP", "Feu clic per accedir a la pàgina següent de llistes compartides." );
+define( "PLSHRD_TB_PAGE", "Pàgina" );
+define( "PLSHRD_TB_PAGEBOT", "Peu de pàgina" );
+define( "PLSHRD_TB_PAGEBOT_HELP", "Baixa fins al peu de pàgina." );
+define( "PLSHRD_TB_PAGETOP", "Capçalera de la pàgina" );
+define( "PLSHRD_TB_PAGETOP_HELP", "Torna a la capçalera de la pàgina" );
+define( "PLSHRD_TB_PAGE_BTN", "Accedeix" );
+
+##################################################
+
+?>
