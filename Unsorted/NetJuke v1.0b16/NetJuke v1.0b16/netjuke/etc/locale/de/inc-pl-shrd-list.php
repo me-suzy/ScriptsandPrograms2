62672e29df703269a7feecfdcc9d425e8fd3bf66
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:59 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-pl-shrd-list.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-pl-shrd-list.php
@@ -0,0 +1,28 @@
+<?php
+
+##################################################
+
+# /pl-shrd-list.php
+
+##################################################
+
+define( "PLSHRD_SHRD_HEADER", "LISTE GEMEINSAMER SPIELLISTEN" );
+define( "PLSHRD_SHRD_PLAY_HELP", "Klicken um diese Spielliste an zu hören." );
+define( "PLSHRD_SHRD_VIEW_HELP", "Klicken um Informationen dieser Spieliste anzuzeigen." );
+define( "PLSHRD_SHRD_BY", "von" );
+define( "PLSHRD_SHRD_FOR", "für" );
+
+define( "PLSHRD_TB_PREV", "Vorherige Seite" );
+define( "PLSHRD_TB_PREV_HELP", "Klicken um die vorherige Seite zu sehen" );
+define( "PLSHRD_TB_NEXT", "Nächste Seite" );
+define( "PLSHRD_TB_NEXT_HELP", "Klicken um die nächste Seite zu sehen" );
+define( "PLSHRD_TB_PAGE", "Seite" );
+define( "PLSHRD_TB_PAGEBOT", "zum Ende" );
+define( "PLSHRD_TB_PAGEBOT_HELP", "Zeigt das Ende der Seite" );
+define( "PLSHRD_TB_PAGETOP", "zum Anfang" );
+define( "PLSHRD_TB_PAGETOP_HELP", "Zeigt den Anfang der Seite" );
+define( "PLSHRD_TB_PAGE_BTN", "Los!" );
+
+##################################################
+
+?>
