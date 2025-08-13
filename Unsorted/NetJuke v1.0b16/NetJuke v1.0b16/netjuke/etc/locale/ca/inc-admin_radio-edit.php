f692dd6eac798a71c3734e817a44e899bc255342
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:00 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_radio-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_radio-edit.php
@@ -0,0 +1,17 @@
+<?php
+
+##################################################
+
+# /admin/radio-edit.php
+
+##################################################
+
+define( "RADIO_LIST_NOPATH", "Fitxer desconegut:\\nNo s'ha trobat la llista de ràdio sol·licitada." );
+
+define( "RADIO_LIST_NOPERM", "Error d'autorització:\\nLa llista sol·licitada no es pot editar." );
+
+define( "RADIO_LIST_DONE", "La llista de ràdio s'ha desat correctament." );
+
+##################################################
+
+?>
