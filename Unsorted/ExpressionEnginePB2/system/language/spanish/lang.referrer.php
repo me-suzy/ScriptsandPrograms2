1a170b32717a07af69c441b05c81b701eeb0db16
me-suzy <me.suzana@gmail.com>
2025-08-13 04:24:14 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.referrer.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.referrer.php
@@ -0,0 +1,84 @@
+<?php
+$L = array(
+
+"referrer_module_name" =>
+"Remitidor",
+
+"referrer_module_description" =>
+"Módulo de rastreo de remitidores",
+
+"referrer_ip" =>
+"Referrer IP",
+
+"referrer_date" =>
+"Referrer Date",
+
+"referrers_deleted" =>
+"Refferers have been deleted",
+
+"pmachine_blacklist" =>
+"Download pMachine Blacklist",
+
+"blacklist_updated" =>
+"Blacklist updated successfully",
+
+'ref_no_license' =>
+"Error: no license number.",
+
+'ref_blacklist_irretrievable' =>
+"Error: new blacklist was irretrievable.",
+
+'ref_view_blacklist' =>
+"Referrer Blacklist",
+
+'ref_no_blacklist' =>
+"There is currently nothing blacklisted",
+
+'ref_ip' =>
+"IP Address",
+
+'ref_user_agent' =>
+"User Agent",
+
+'ref_url' =>
+"URL",
+
+'ref_blacklisted' =>
+"Blacklisted",
+
+'ref_no_blacklist_table' =>
+"Blacklist database table does not exist",
+
+'ref_type' =>
+"Type",
+
+"referrers" =>
+"Remitidores",
+
+"view_referrers" =>
+"Ver Remitidores",
+
+"clear_referrers" =>
+"Borrar Remitidores",
+
+"referrer_from" =>
+"Remitidor desde",
+
+"referrer_to" =>
+"Remitidor hacia",
+
+"no_referrers" =>
+"No hay ningún Remitidor en este momento",
+
+"total_referrers" =>
+"Total de Remitidores:",
+
+"save_instructions" =>
+"¿Cuántos Remitidores recientes quieres guardar?",
+
+"referrers_deleted" =>
+"Se han borrado los Remitidores",
+
+''=>''
+);
+?>
