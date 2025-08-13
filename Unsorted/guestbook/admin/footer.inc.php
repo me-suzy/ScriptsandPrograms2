784af6ad825f2bfef5736a441d88eb9b4bafe10b
me-suzy <me.suzana@gmail.com>
2025-08-13 05:00:18 +0300

    Add Unsorted/guestbook/admin/footer.inc.php

---
+++ Unsorted/guestbook/admin/footer.inc.php
@@ -0,0 +1,4 @@
+<center><font face="Arial, Helvetica, sans-serif" color="#CCCCCC" size="1"><b>Advanced Guestbook 2.3.1<br>
+  Powered by PHP &amp; MySQL - <a href="http://www.proxy2.de" target="_blank"><font color="#CCCCCC">http://www.proxy2.de</font></a></b></font></center>
+</body>
+</html>
