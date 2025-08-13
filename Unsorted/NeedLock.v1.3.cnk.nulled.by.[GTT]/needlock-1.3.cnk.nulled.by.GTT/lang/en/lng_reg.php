29c9fea438289136b0dce299e284bdf4ae265d50
me-suzy <me.suzana@gmail.com>
2025-08-13 06:39:06 +0300

    Add Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/en/lng_reg.php

---
+++ Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/en/lng_reg.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['login_page_subtitle']            = 'Register new member';
+$lang['regs_not_allow']                 = 'Sorry, registering of a new members temporary disallowed by administrator';
+$lang['username']	                = 'UserName';
+$lang['password']	                = 'PassWord';
+$lang['success_text']                   = 'You successfully loged in! Please wait while we redirect You ...';
+$lang['register_header']                = 'Registration of a new member';
+$lang['desired_username']               = 'Desired UserName';
+$lang['register_me']                    = 'register me';
+$lang['needed_fields_missed']           = 'Sorry, not all fields completed. Try again, please.';
+$lang['username_exists']                = 'Sorry, but such UserName exists. Try another, please.';
+$lang['email_exists']                   = 'Sorry, but such Email exists. Try another, please.';
+$lang['email_invalid']                  = 'Sorry, but Your Email incorrect. Try another, please.';
+$lang['login_usage_denied']             = 'Sorry, registering this login denied by administrator.';
+$lang['proceed_to_login_link_text']     = 'Proceed to LogIn';
+$lang['reg_done_with_authcode']         = 'Your registratin successfully completed.<br>To activate Your account follow the instractions in account activation letter.<br>';
+$lang['reg_done_without_authcode_with_approve']         = 'Your registratin successfully completed.<br>Your login details sent You via email. Your account will be activated after administrator\'s approve<br>';
+$lang['reg_done_without_authcode_without_approve']         = 'Your registratin successfully completed.<br>Your login details sent You via email.<br>';
+$lang['authcode']                       = 'Activatin code';
+$lang['activate_account']               = 'activate account';
+$lang['incorrect_autcode']              = 'Sorry, no account awaiting activation with such AuthCode. Try again more attentively, please.';
+
+?>
