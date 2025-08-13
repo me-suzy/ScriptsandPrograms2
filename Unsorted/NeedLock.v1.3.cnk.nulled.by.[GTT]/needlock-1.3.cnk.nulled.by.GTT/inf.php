b916ef3f6baec94a21161ba82d5ef2e6d42e3cd8
me-suzy <me.suzana@gmail.com>
2025-08-13 06:38:38 +0300

    Add Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/inf.php

---
+++ Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/inf.php
@@ -0,0 +1,64 @@
+<?php
+
+$INFO["SITE_NAME"]		=	"Site_name.com";
+$INFO["BASE_URL"]		=	"";
+$INFO["BASE_PATH"]		=	"";
+$INFO["IMG_URL"]		=	"";
+$INFO["IMG_PATH"]		=	"";
+$INFO["PHP_EXT"]		=	".php";
+$INFO["HTA_TOP_DIR"]		=	"";
+$INFO["DEFAULT_LANG"]		=	"en";
+$INFO["ADMIN_EMAIL"]		=	"";
+$INFO["EMAIL_IN"]		=	"";
+$INFO["EMAIL_OUT"]		=	"";
+$INFO["EMAIL_SENDER"]		=	"mail";
+$INFO["EMAIL_FORMAT"]		=	"text/plain";
+$INFO["EMAIL_CHARSET"]		=	"koi8-r";
+$INFO["EMAIL_SMTP_HOST"]	=	"localhost";
+$INFO["EMAIL_SMTP_USER"]	=	"";
+$INFO["EMAIL_SMTP_PASS"]	=	"";
+$INFO["INSTALLED_LANGS"]	=	"en:English";
+$INFO["SUPPORTED_SQLS"]		=	"mysql";
+$INFO["SQL_DRIVER"]		=	"mysql";
+$INFO["SQL_HOST"]		=	"localhost";
+$INFO["SQL_USER"]		=	"";
+$INFO["SQL_PSWD"]		=	"";
+$INFO["SQL_PORT"]		=	"";
+$INFO["SQL_NAME"]		=	"";
+$INFO["SQL_PREFIX"]		=	"ns_";
+$INFO["cookie_domain"]		=	"";
+$INFO["cookie_path"]		=	"";
+$INFO["cookie_id"]		=	"";
+$INFO["IS_OFFLINE"]		=	"0";
+$INFO["OFFLINE_MSG"]		=	"Sorry, the system is currently offline. Please visit us later.";
+$INFO["NEW_REGS_ALLOW"]		=	"0";
+$INFO["NOTIFY_NEW_REGS"]	=	"1";
+$INFO["REG_AUTH_CODES"]		=	"1";
+$INFO["REG_ADMIN_APPROVE"]	=	"1";
+$INFO["SESS_EXPIRE"]		=	"3600";
+$INFO["DEFAULT_HTA_USED"]	=	"1";
+$INFO["DEFAULT_HTA_USER"]	=	"lock";
+$INFO["DEFAULT_HTA_PASS"]	=	"lock";
+$INFO["ADD_FIELD_1_ON"]		=	"0";
+$INFO["ADD_FIELD_1_NM"]		=	"no";
+$INFO["ADD_FIELD_2_ON"]		=	"0";
+$INFO["ADD_FIELD_2_NM"]		=	"no";
+$INFO["ADD_FIELD_3_ON"]		=	"0";
+$INFO["ADD_FIELD_3_NM"]		=	"no";
+$INFO["ADD_FIELD_4_ON"]		=	"0";
+$INFO["ADD_FIELD_4_NM"]		=	"no";
+$INFO["ADD_FIELD_5_ON"]		=	"0";
+$INFO["ADD_FIELD_5_NM"]		=	"no";
+$INFO["ADD_FIELD_6_ON"]		=	"0";
+$INFO["ADD_FIELD_6_NM"]		=	"no";
+$INFO["ADD_FIELD_7_ON"]		=	"0";
+$INFO["ADD_FIELD_7_NM"]		=	"no";
+$INFO["ADD_FIELD_8_ON"]		=	"0";
+$INFO["ADD_FIELD_8_NM"]		=	"no";
+$INFO["ADD_FIELD_9_ON"]		=	"0";
+$INFO["ADD_FIELD_9_NM"]		=	"no";
+$INFO["ADD_FIELD_10_ON"]	=	"0";
+$INFO["ADD_FIELD_10_NM"]	=	"no";
+
+
+?>
