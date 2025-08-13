0c107f94c566a8fc9d455dc12ff5a3e8e74fa9fd
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:16 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_radio-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-admin_radio-edit.php
@@ -0,0 +1,17 @@
+<?php
+
+##################################################
+
+# /admin/radio-edit.php
+
+##################################################
+
+define( "RADIO_LIST_NOPATH", "Datei nicht gefunden:\\nDie gewählte Radio Spielliste konnte nicht gefunden werden." );
+
+define( "RADIO_LIST_NOPERM", "Rechte Problem:\\nIn die gewählte Radio Spielliste konnte nicht geschrieben werden." );
+
+define( "RADIO_LIST_DONE", "Die Radio Spielliste wurde erfolgreich geändert." );
+
+##################################################
+
+?>
