6331f8b2b719a248ab10cd8368d9e2e541f45601
me-suzy <me.suzana@gmail.com>
2025-08-13 05:01:32 +0300

    Add Unsorted/guestbook/lang/german.php

---
+++ Unsorted/guestbook/lang/german.php
@@ -0,0 +1,101 @@
+<?php
+# General
+
+$LANG["metatag"]    = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">";
+
+$LANG["BookMess1"]  = "HTML Code ist deaktiviert";
+$LANG["BookMess2"]  = "HTML Code ist aktiviert";
+$LANG["BookMess3"]  = "Eintrag hinzuf&uuml;gen";
+$LANG["BookMess4"]  = "G&auml;stebuch ansehen";
+$LANG["BookMess5"]  = "Administration";
+$LANG["BookMess6"]  = "Willkomen in unserem G&auml;stebuch. Hier k&ouml;nnen Sie einen Beitrag hinterlassen.";
+$LANG["BookMess7"]  = "Kommentar";
+$LANG["BookMess8"]  = "Zur&uuml;ck zum Formular";
+$LANG["BookMess9"]  = "Schreiben Sie einen Kommentar f&uuml;r diesen G&auml;stebucheintrag.";
+$LANG["BookMess10"] = "<u>Vielen Dank f&uuml;r Ihren Eintrag.</u><br><br>Ihr Eintrag wurde erfolgreich aufgenommen!<br>Sie werden jetzt automatisch zum G&auml;stebuch weitergeleitet.";
+
+$LANG["EmailAdminSubject"] = "Neuer Gästebuch Eintrag";
+$LANG["EmailGuestSubject"] = "Vielen Dank f&uuml;r Ihren Eintrag";
+
+$LANG["AltIP"]    = "IP gespeichert";
+$LANG["AltICQ"]   = "ICQ Nachricht senden";
+$LANG["AltEmail"] = "E-mail schreiben";
+$LANG["AltUrl"]   = "Homepage besuchen";
+$LANG["AltAim"]   = "Instant Messenger";
+$LANG["AltCom"]   = "Kommentar schreiben";
+
+# Form
+$LANG["FormMess1"]   = "Um sich einzutragen f&uuml;llen Sie bitte alle Felder unten aus. Nachdem Sie Ihren Beitrag abgeschickt werden Sie automatisch zum G&auml;stebuch weitergeleitet. Die Felder mit (*) markiert sind Pflichtfelder.";
+$LANG["FormMess2"]   = "Smilies sind erlaubt";
+$LANG["FormMess3"]   = "AG Code ist erlaubt";
+$LANG["FormMess4"]   = "Legende zeigen";
+$LANG["FormMess5"]   = "So wird Ihr Beitrag sp&auml;ter im G&auml;stebuch zu sehen sein.";
+$LANG["FormMess6"]   = "AG Code ist nicht erlaubt";
+$LANG["FormMess7"]   = "Smilies sind nicht erlaubt";
+$LANG["FormName"]    = "Name";
+$LANG["FormEmail"]   = "E-Mail";
+$LANG["FormLoc"]     = "Ort";
+$LANG["FormPic"]     = "Bild";
+$LANG["FormUrl"]     = "Homepage";
+$LANG["FormGender"]  = "Geschlecht";
+$LANG["FormMale"]    = "m&auml;nnlich";
+$LANG["FormFemale"]  = "weiblich";
+$LANG["FormMessage"] = "Ihr Beitrag";
+$LANG["FormSelect"]  = "G&auml;stebuch";
+$LANG["FormUser"]    = "Benutzername";
+$LANG["FormPass"]    = "Kennwort";
+$LANG["FormReset"]   = "Zur&uuml;cksetzen";
+$LANG["FormSubmit"]  = "Abschicken";
+$LANG["FormPreview"] = "Vorschau";
+$LANG["FormBack"]    = "Zur&uuml;ck";
+$LANG["FormEnter"]   = "G&auml;stebuch - Administrationsbereich<br>Bevor Sie fortfahren k&ouml;nnen, m&uuml;ssen Sie sich identifizieren.";
+$LANG["FormButton"]  = "Hin";
+$LANG["FormPriv"]    = "Pers&ouml;nliche Nachricht";
+
+# Navigation Bar
+$LANG["NavTotal"]   = "Bisherige Eintr&auml;ge:";
+$LANG["NavRecords"] = "Eintr&auml;ge pro Seite:";
+$LANG["NavPrev"]    = "Vorherige Seite";
+$LANG["NavNext"]    = "N&auml;chste Seite";
+
+# Post Errors
+$LANG["ErrorPost1"]  = "Sie haben noch keinen Namen eingegeben!";
+$LANG["ErrorPost2"]  = "Sie haben noch keinen Beitrag eingeben!";
+$LANG["ErrorPost3"]  = "Der Beitrag ist entweder zu kurz oder zu lang.";
+$LANG["ErrorPost4"]  = "Eine Eingabe von Ihnen ist vermutlich ung&uuml;ltig!";
+$LANG["ErrorPost5"]  = "G&auml;stebuch - Fehler";
+$LANG["ErrorPost6"]  = "Die Bilddatei ist zu gross.";
+$LANG["ErrorPost7"]  = "Falsches Bildformat.";
+$LANG["ErrorPost8"]  = "Flutschutz aktiviert! Bitte warten Sie einen kurzen Moment.";
+$LANG["ErrorPost9"]  = "Ihre IP Adresse ist f&uuml;r das G&auml;stebuch gesperrt!";
+$LANG["ErrorPost10"] = "Ihr Beitrag enth&auml;lt ung&uuml;ltige Wörter.";
+$LANG["ErrorPost11"] = "Sie haben noch keinen Kommentar angegeben.";
+
+$LANG["PassMess1"] = "Bitte identifizieren Sie sich:";
+$LANG["PassMess2"] = "Ung&uuml;ltige Benutzer/Kennwort Kombination. Versuchen Sie es erneut.";
+$LANG["PassMess3"] = "Das erforderliche Kennwort stimmt nicht.";
+
+# Days
+$weekday[0] = "Sonntag";
+$weekday[1] = "Montag";
+$weekday[2] = "Dienstag";
+$weekday[3] = "Mittwoch";
+$weekday[4] = "Donnerstag";
+$weekday[5] = "Freitag";
+$weekday[6] = "Samstag";
+
+# Months
+$months[0]  = "Januar";
+$months[1]  = "Februar";
+$months[2]  = "M&auml;rz";
+$months[3]  = "April";
+$months[4]  = "Mai";
+$months[5]  = "Juni";
+$months[6]  = "Juli";
+$months[7]  = "August";
+$months[8]  = "September";
+$months[9]  = "Oktober";
+$months[10] = "November";
+$months[11] = "Dezember";
+
+?>
