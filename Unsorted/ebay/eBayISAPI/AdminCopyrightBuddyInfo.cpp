471043e5b8829e80bd4df08c4f87fad0e414bc6a
me-suzy <me.suzana@gmail.com>
2025-08-13 03:05:49 +0300

    Add Unsorted/ebay/eBayISAPI/AdminCopyrightBuddyInfo.cpp

---
+++ Unsorted/ebay/eBayISAPI/AdminCopyrightBuddyInfo.cpp
@@ -0,0 +1,375 @@
+/*	$Id: AdminCopyrightBuddyInfo.cpp,v 1.9 1999/05/19 02:34:13 josh Exp $	*/
+//
+//	File:		AdminCopyrightBuddyInfo.cpp
+//
+//	Class:		clseBayApp
+//
+//	Author:		Michael Wilson (michael@ebay.com)
+//
+//	Function:
+//
+//	Contains the information about Copyright buddies
+//
+//	Modifications:
+//				- 11/28/98	michael	- created
+//
+#include "ebihdr.h"
+
+clsCopyrightBuddyInfo clseBayApp::mCopyrightBuddyInfo[] =
+{	{	1,
+		"BSA",	
+		"Business Software Alliance",
+		"allens@bsa.org"
+	},
+
+	{	2,
+		"Cartier",	
+		"Cartier",
+		"mail@colucci-umans.com"
+	},
+
+	{	3,
+		"CEC",	
+		"CEC Management, Ben Folds Five",
+		"cecus@aol.com"
+	},
+
+	{	4,
+		"CEC",	
+		"CEC Management, Ben Folds Five",
+		"cecus@aol.com"
+	},
+
+	{	5,
+		"Decipher",	
+		"Deciper, Inc.",
+		"avittone@mezzullo.com"
+	},
+
+	{	6,
+		"MPAA",	
+		"Motion Picture Associtation of America",
+		"Mikhail_Reider@mpaa.org"
+	},
+
+	{	7,
+		"MPAA",	
+		"Motion Picture Associtation of America",
+		"Mikhail_Reider@mpaa.org"
+	},
+	{	8,
+		"Forefront",	
+		"Forefront Group, Inc.",
+		"Mikhail_Reider@mpaa.org"
+	},
+	{	9,
+		"Hasbro",	
+		"Hasbro",
+		"armoss@pbwt.com"
+	},
+	{	10,
+		"MGM",	
+		"MGM",
+		"ltunberg@mgm.com"
+	},
+	{	11,
+		"Viagra",	
+		"Viagra",
+		"witscca@newyork.whitecase.com, fujiin@pfizer.com"
+	},
+
+	{	12,
+		"Virtual Express",	
+		"Virtual Express",
+		"chris@virexpress.com"
+	},
+	{	13,
+		"WSB Creations",	
+		"WSB Creations",
+		"WSBCreate@aol.com"
+	},
+	{	14,
+		"Auto Desk",	
+		"Auto Desk",
+		"colita@smtp.donahue.com"
+	},
+
+	{	15,
+		"Banned from Television",	
+		"Banned from Television",
+		"elana@fall-line.com"
+	},
+
+	{	16,
+		"SPA",	
+		"Blizzard Entertainment",
+		"pberuk@spa.org"
+	},
+	{	17,
+		"CBS",	
+		"CBS Law Department",
+		"tflane@cbs.com"
+	},
+	{	18,
+		"Corel Draw",	
+		"Corel Draw",
+		"iwonaa@COREL.CA"
+	},
+
+	{	19,
+		"David Duchovny",	
+		"David Duchovny",
+		"edstef@meganet.net"
+	},
+
+	{	20,
+		"Oakley",	
+		"Oakley",
+		"donnas@oakley.com"
+	},
+
+	{	21,
+		"South Park",	
+		"Comedy Central",
+		"gjackson@comedycentral.com"
+	},
+
+	{	22,
+		"Star Wars",	
+		"Star Wars",
+		"danderman@lucasfilm.com"
+	},
+
+	{	23,
+		"Activision	",	
+		"Activision	",
+		"DSilverman@activision.com"
+	},
+
+	{	24,
+		"Bon Jovi",	
+		"Bon Jovi",
+		"edelstein.laird.sobel@MCIONE.com"
+	},
+
+	{	25,
+		"Gateway 2000, Inc.",	
+		"Gateway 2000, Inc.",
+		"CLAIBANT@Gateway.com"
+	},
+
+	{	26,
+		"RealNetworks, Inc.",	
+		"RealNetworks, Inc.",
+		"jwinther@real.com"
+	},
+
+	{	27,
+		"RIAA",	
+		"Recording Industry Association of America",
+		"Denise_Incorvaia/RIAA@riaa.com"
+	},
+
+	{	28,
+		"ESRI",	
+		"ESRI",
+		"eberliner@esri.com"
+	},
+
+	{	29,
+		"Gloria Vanderbilt",	
+		"Gloria Vanderbilt",
+		"macrae@snet.net"
+	},
+
+	{	30,
+		"Hanson Rock Group",	
+		"Hanson Rock Group",
+		"kmcallej@mwbb.com"
+	},
+
+	{	31,
+		"Micron Electronics",	
+		"Micron Electronics",
+		"bradleefraz@micronpc.com"
+	},
+
+	{	32,
+		"Mother Productions",	
+		"Mother Productions",
+		"jerry@jlpokorny.com"
+	},
+
+	{	33,
+		"Ty Beanie Babies",	
+		"Ty Beanie Babies",
+		"KHOLATA@GCD.COM"
+	},
+
+	{	34,
+		"Dreamworks",	
+		"Dreamworks",
+		"DCAPLAN@baker-hostetler.com, jmurrelladams@dreamworks.com"
+	},
+
+	{	35,
+		"Fox.",	
+		"Fox",
+		"DCAPLAN@baker-hostetler.com/ACGUNDZI@llgm.com/FOXIP@foxinc.com"
+	},
+
+	{	36,
+		"Harbor Freight Tools",	
+		"Harbor Freight Tools",
+		"MFriedman@harborfreight.com"
+	},
+
+	{	37,
+		"IDSA",	
+		"Interactive Digital Software Association",
+		"dennis@idsa.com"
+	},
+
+	{	38,
+		"Inclose",	
+		"Inclose Design, Inc",
+		"kfortin@peninsulaw.com"
+	},
+
+	{	39,
+		"Louis Vuitton/Rolex",	
+		"Louis Vuitton/Rolex",
+		"macaluso@gibney.com, kdsuzan@gibney.com"
+	},
+
+	{	40,
+		"Lucasfilm Ltd",	
+		"Lucasfilm Ltd",
+		"danderman@lucasfilm.com"
+	},
+
+	{	41,
+		"Philip Morris",	
+		"Philip Morris",
+		"eckrj@idt.net"
+	},
+
+	{	42,
+		"SPA",	
+		"Software Publisher's Association",
+		"Bjones@spa.org"
+	},
+
+	{	43,
+		"Tommy Hilfiger/Nautica/Nike/Fubu",	
+		"Tommy Hilfiger/ Nautica/Nike/Fubu",
+		"GurskyLaw@aol.com"
+	},
+
+	{	44,
+		"Transcender",	
+		"Transcender",
+		"lgibson@transcender.com"
+	},
+
+	{	45,
+		"The Antioch Company",	
+		"The Antioch Company",
+		"jwaletzko@antioch.com"
+	},
+
+	{	46,
+		"Creative Memories",	
+		"Creative Memories",
+		"dtschida@antioch.com"
+	},
+
+	{	47,
+		"Nike",	
+		"Nike",
+		"dave.simpson@nike.com"
+	},
+
+	{	48,
+		"Novell",	
+		"Novell",
+		"MCHILD@novell.com"
+	},
+
+	{	49,
+		"Adobe",	
+		"Adobe",
+		"piracy@Adobe.COM"
+	},
+
+	{	50,
+		"Lightpost Publishing",	
+		"Lightpost Publishing",
+		"phillips_sweet@mediarts.com"
+	},
+
+	{	51,
+		"Skippy Productions",
+		"Bones, Gore, and Guts, Limited",
+		"skippy@ebay.com"
+	},
+
+	{	52,
+		"LuLu Productions",
+		"Homey Homilies, Limited",
+		"lulu@ebay.com"
+	},
+
+	{	53,
+		"Shari Screenplays",
+		"Shariesque Films, Inc",
+		"shari@ebay.com"
+	},
+
+	{	54,
+		"Sony",
+		"Sony",
+		"mjohnson@ebay.com,piracytaskforce@playstation.sony.com"
+	},
+
+	{	55,
+		"Nintendo",
+		"Nintendo",
+		"noalegal@noa.nintendo.com"
+	},
+
+	{	56,
+		"Jeepster",
+		"Jeepster",
+		"jeepster@buddy.ebay.com"
+	},
+
+	{	57,
+		"Sega",
+		"Sega",
+		"haynesi@sega.com"
+	},
+
+	{	58,
+		"Electronic Arts",
+		"Electronic Arts",
+		"lroybal@ea.com"
+	},
+
+
+	{	59,
+		"Spare Buddy",
+		"Spare Buddy",
+		"notify@ebay.com"
+	},
+
+	{	0,
+		"None",
+		"None",
+		NULL
+	}
+
+
+
+
+};
