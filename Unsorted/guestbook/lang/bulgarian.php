5d2374fc77314bac5ead920bcb75dc6367281ef5
me-suzy <me.suzana@gmail.com>
2025-08-13 05:00:49 +0300

    Add Unsorted/guestbook/lang/bulgarian.php

---
+++ Unsorted/guestbook/lang/bulgarian.php
@@ -0,0 +1,101 @@
+<?php
+# General
+
+$LANG["metatag"]    = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1251\">";
+
+$LANG["BookMess1"]  = "HTML êîäà å èçêëþ÷åí";
+$LANG["BookMess2"]  = "HTML êîäà å âêëþ÷åí";
+$LANG["BookMess3"]  = "  èçïðàòè ìíåíèå  ";
+$LANG["BookMess4"]  = "  îáðàòíî â êíèãàòà çà ãîñòè  ";
+$LANG["BookMess5"]  = "";
+$LANG["BookMess6"]  = "áëàãîäàðÿ";
+$LANG["BookMess7"]  = "êîìåíòàðè";
+$LANG["BookMess8"]  = "îáðàòíî âúâ ôîðìàòà";
+$LANG["BookMess9"]  = "Çàïèøè ìíåíèå â êíèãàòà.";
+$LANG["BookMess10"] = "<u>Áëàãîäàðèì Âè, ÷å ïîñåòèõòå íàøàòà êíèãà çà ãîñòè.</u><br><br>Âàøåòî ìíåíèå å äîáàâåíî óñïåøíî! Ùå áúäåòå ïðåíàñî÷åíè êúì êíèãàòà ñëåä 2 ñåêóíäè.";
+
+$LANG["EmailAdminSubject"] = "èçïðàòè ìíåíèå";
+$LANG["EmailGuestSubject"] = "áëàãîäàðèì âè çà ìíåíèåòî";
+
+$LANG["AltIP"]    = "IP logged";
+$LANG["AltICQ"]   = "Send ICQ Message";
+$LANG["AltEmail"] = "Send E-mail";
+$LANG["AltUrl"]   = "View Web Page";
+$LANG["AltAim"]   = "Instant Messenger";
+$LANG["AltCom"]   = "Write a comment";
+
+# Form
+$LANG["FormMess1"]   = "ïîëåòàòà îçíà÷åíè ñúñ * ñà çàäúëæèòåëíè";
+$LANG["FormMess2"]   = "óñìèâêèòå ñà âêëþ÷åíè";
+$LANG["FormMess3"]   = "AGCode is ON";
+$LANG["FormMess4"]   = "ïîêàæè ëåãåíäà";
+$LANG["FormMess5"]   = "ñëåäâà âàøåòî ìíåíèå.";
+$LANG["FormMess6"]   = "AG Code is OFF";
+$LANG["FormMess7"]   = "óñìèâêèòå ñà èçêëþ÷åíè";
+$LANG["FormName"]    = "èìå";
+$LANG["FormEmail"]   = "e-mail";
+$LANG["FormLoc"]     = "àäðåñ";
+$LANG["FormPic"]     = "ñíèìêà";
+$LANG["FormUrl"]     = "ëè÷íà ñòðàíèöà";
+$LANG["FormGender"]  = "ïîë";
+$LANG["FormMale"]    = "ìúæ";
+$LANG["FormFemale"]  = "æåíà";
+$LANG["FormMessage"] = "âàøåòî ñúîáùåíèå";
+$LANG["FormSelect"]  = "êíèãà çà ãîñòè";
+$LANG["FormUser"]    = "ïîòðåáèòåë";
+$LANG["FormPass"]    = "ïàðîëà";
+$LANG["FormReset"]   = "èç÷èñòè";
+$LANG["FormSubmit"]  = "ïîòâúðäè";
+$LANG["FormPreview"] = "ïðåãëåä";
+$LANG["FormBack"]    = "îáðàòíî";
+$LANG["FormEnter"]   = "êíèãà - àäìèíèñòðàöèÿ<br>Ïðåäè äà ïðîäúëæèòå òðÿáâà äà âúâåäåòå ïðàâèëíè ïîòðåáèòåë è ïàðîëà!";
+$LANG["FormButton"]  = "íàïðåä";
+$LANG["FormPriv"]    = "ëè÷íî ñúîáùåíèå";
+
+# Navigation Bar
+$LANG["NavTotal"]   = "îáùî ìíåíèÿ:";
+$LANG["NavRecords"] = "ìíåíèÿ íà åäíà ñòðàíèöà:";
+$LANG["NavPrev"]    = "ïðåäèøíà ñòðàíèöà";
+$LANG["NavNext"]    = "ñëåäâàùà ñòðàíèöà";
+
+# Post Errors
+$LANG["ErrorPost1"]  = "Çàáðàâèëè ñòå äà ïîïúëíèòå ïîëåòî Èìå. Ìîëÿ, êîðèãèðàéòå è èçïðàòåòå.";
+$LANG["ErrorPost2"]  = "Çàáðàâèëè ñòå äà ïîïúëíèòå ïîëåòî Ñúîáùåíèå. Ìîëÿ, êîðèãèðàéòå è èçïðàòåòå.";
+$LANG["ErrorPost3"]  = "Âàøåòî ñúîáùåíèå å òâúðäå êúñî èëè òâúðäå äúëãî. Ìîëÿ, êîðèãèðàéòå è èçïðàòåòå.";
+$LANG["ErrorPost4"]  = "Íÿêîè îò ïîëåòàòà ñà íåâàëèäíî ïîïúëíåíè.";
+$LANG["ErrorPost5"]  = "Êíèãà - ãðåøêà";
+$LANG["ErrorPost6"]  = "Ñíèìêàòà å òâúðäå ãîëÿìà";
+$LANG["ErrorPost7"]  = "Ãðåøåí ôîðìàò íà ñíèìêàòà";
+$LANG["ErrorPost8"]  = "Ñúæàëÿâàìå. Óñòàíîâåíî å ïðåòîâàðâàíå. Ìîëÿ, èç÷àêàéòå èçâåñòíî âðåìå ïðåäè äà èçïðàòèòå ñëåäâàùî ìíåíèå!";
+$LANG["ErrorPost9"]  = "Âàøèÿ IP àäðåñ å áëîêèðàí!";
+$LANG["ErrorPost10"] = "Âàøåòî ñúîáùåíèå ñúäúðæà íåöåíçóðíè äóìè. Ìîëÿ, êîðèãèðàéòå è èçïðàòåòå.";
+$LANG["ErrorPost11"] = "Íå ñòå ñëîæèëè êîìåíòàð. Ìîëÿ, êîðèãèðàéòå è èçïðàòåòå.";
+
+$LANG["PassMess1"] = "Ìîëÿ, âúâåäåòå âàëèäíè ïîòðåáèòåë è ïàðîëà:";
+$LANG["PassMess2"] = "Íåâàëèäíè ïîòðåáèòåë è ïàðîëà. Ìîëÿ, îïèòàéòå ïàê.";
+$LANG["PassMess3"] = "Ñúæàëÿâàìå, ïàðîëàòà Âè å íåâàëèäíà.";
+
+# Days
+$weekday[0] = "Íåäåëÿ";
+$weekday[1] = "Ïîíåäåëíèê";
+$weekday[2] = "Âòîðíèê";
+$weekday[3] = "Ñðÿäà";
+$weekday[4] = "×åòâúðòúê";
+$weekday[5] = "Ïåòúê";
+$weekday[6] = "Ñúáîòà";
+
+# Months
+$months[0]  = "ßíóàðè";
+$months[1]  = "Ôåâðóàðè";
+$months[2]  = "Ìàðò";
+$months[3]  = "Àïðèë";
+$months[4]  = "Ìàé";
+$months[5]  = "Þíè";
+$months[6]  = "Þëè";
+$months[7]  = "Àâãóñò";
+$months[8]  = "Ñåïòåìâðè";
+$months[9]  = "Îêòîìâðè";
+$months[10] = "Íîåìâðè";
+$months[11] = "Äåêåìâðè";
+
+?>
