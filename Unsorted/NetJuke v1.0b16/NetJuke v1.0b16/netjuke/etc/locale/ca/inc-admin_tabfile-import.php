ae6ae860dee8131556791bf220079ea15c473210
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:02 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_tabfile-import.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_tabfile-import.php
@@ -0,0 +1,37 @@
+<?php
+
+##################################################
+
+# /admin/tabfile-import.php
+
+##################################################
+
+define( "TFIMPT_HEADER", "IMPORTACIÓ DE FITXERS DELIMITATS" );
+
+define( "TFIMPT_VIEW", "Veure fitxer" );
+define( "TFIMPT_PROCEED", "Comença la importació de fitxers" );
+define( "TFIMPT_DELETE", "Esborra els fitxers seleccionats" );
+define( "TFIMPT_DELETE_SUCCESS", "Esborrat" );
+define( "TFIMPT_DELETE_ERROR", "Error d'autorització: no es pot esborrar" );
+
+define( "TFIMPT_FOUND_1", "S'han trobat" );
+define( "TFIMPT_FOUND_2", "fitxer(s) per processar a la carpeta d'importació per defecte." );
+
+define( "TFIMPT_PROCESS", "S'està processant" );
+
+define( "TFIMPT_IGNORE_COLNAMES", "Sense tenir en compte el nom de les columnes" );
+
+define( "TFIMPT_INSERT_1", "Ha insertat" );
+define( "TFIMPT_INSERT_2", "cançó(ns)" );
+define( "TFIMPT_INSERT_3", "intèrpret(s)" );
+define( "TFIMPT_INSERT_4", "àlbum(s)" );
+define( "TFIMPT_INSERT_5", "gènere(s)" );
+
+define( "TFIMPT_ARCHIVE_1", "Ha arxivat" );
+define( "TFIMPT_ARCHIVE_2", "com a" );
+
+define( "TFIMPT_DONE", "Ha acabat amb" );
+
+##################################################
+
+?>
