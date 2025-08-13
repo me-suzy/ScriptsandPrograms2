f8a24fa94d522a731b440cfdaa6ac65edebd018d
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:24 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.rss.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.rss.php
@@ -0,0 +1,17 @@
+<?php
+$L = array(
+
+
+"rss_module_name" =>
+"RSS",
+
+"rss_module_description" =>
+"RSS sivun generointimoduli",
+
+"rss_invalid_weblog" =>
+"The weblog specified in your RSS feed does not exist.",
+
+
+''=>''
+);
+?>
