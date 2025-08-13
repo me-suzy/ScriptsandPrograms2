828e363f703dc30d71e787c044f253ac04f8f106
me-suzy <me.suzana@gmail.com>
2025-08-13 06:33:14 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/it/upload.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/it/upload.php
@@ -0,0 +1,35 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[UPLOAD_Title] = "Inviare un file!";
+
+$LOCALE[UPLOAD_Info] = "Per favore di scegliere un file di caricare e l'operatore questo file è per.";
+
+$LOCALE[UPLOAD_Button] = "Caricare Adesso";
+
+$LOCALE[UPLOAD_File_Send] = "Il file di inviare";
+
+$LOCALE[UPLOAD_User_Send] = "Inviare a";
+
+$LOCALE[UPLOAD_Complete] = "Il suo file è stato inviato con successo!";
+
+$LOCALE[UPLOAD_Sent_File] = "Ho appena l'ha inviato il file";
+
+$LOCALE[UPLOAD_Err_Large] = "Il file che lei sceglie è a grande, o non ha caricato correttamente.";
+
+$LOCALE[UPLOAD_Err_Accept] = "L'operatore non è iscritto o non accetta di file.";
+
+$LOCALE[UPLOAD_Err_Exists] = "Il file che lei inviano già esiste.";
+
+$LOCALE[UPLOAD_Err_Full] = "L'operatore non ha lo spazio di magazzino abbastanza libero per il suo file.";
+
+?>
