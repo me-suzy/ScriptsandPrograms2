25ddae221c19bf13d13fb8786cedaea96cdf2f78
me-suzy <me.suzana@gmail.com>
2025-08-13 06:50:55 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_jukebox-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_jukebox-edit.php
@@ -0,0 +1,23 @@
+<?php
+
+##################################################
+
+# /admin/jukebox-edit.php
+
+##################################################
+
+define( "JUKEBOX_LIST_NOPATH", "Fitxer inexistent:\\nNo s'ha trobat la llista de jukebox sol·licitada." );
+
+define( "JUKEBOX_LIST_NOPERM", "Error d'autorització:\\nNo es pot escriure sobre aquesta llista de jukebox." );
+
+define( "JUKEBOX_LIST_DONE", "La llista de jukebox s'ha editat correctament." );
+
+define( "JUKEBOX_START_DONE", "La llista de jukebox s'ha iniciat correctament." );
+
+define( "JUKEBOX_STOP_DONE", "La llista de jukebox s'ha parat correctament." );
+
+define( "JUKEBOX_NOPLAYER", "Error:\\nNo s'ha trobat el reproductor. Per favor\\nrevisa la ruta introduïda en el fitxer de preferències." );
+
+##################################################
+
+?>
