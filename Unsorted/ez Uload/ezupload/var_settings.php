2ec762a51221572d439fdee616b8175ad8ea97bd
me-suzy <me.suzana@gmail.com>
2025-08-13 04:30:15 +0300

    Add Unsorted/ez Uload/ezupload/var_settings.php

---
+++ Unsorted/ez Uload/ezupload/var_settings.php
@@ -0,0 +1,89 @@
+<?php
+/////////////////////////////////////////////////////////////
+// Program Name         : EzUpload Pro                       
+// Program Version      : 2.20                                
+// Program Author       : ScriptsCenter.com                  
+// Supplied by          : CyKuH [WTN]                        
+// Nullified and tested : CyKuH [WTN]                        
+// Distribution         : via WebForum and Forums File Dumps 
+//                    WTN Team `2004
+/////////////////////////////////////////////////////////////
+  $VARS[0]['id'] = 1;
+  $VARS[0]['extensions'] = "bat exe cgi php phtml php3 inc shtml pl php4 phtm asp asx";
+  $VARS[0]['extmode'] = "except";
+  $VARS[0]['limitsize'] = 0;
+  $VARS[0]['sizemin'] = 0;
+  $VARS[0]['sizemax'] = 2000;
+  $VARS[0]['limitdim'] = 0;
+  $VARS[0]['widthmin'] = 0;
+  $VARS[0]['widthmax'] = 1000;
+  $VARS[0]['heightmin'] = 0;
+  $VARS[0]['heightmax'] = 1000;
+  $VARS[0]['subdir'] = "date";
+  $VARS[0]['existing'] = "addnumber";
+  $VARS[0]['thankyoumsg'] = "Thank you for your submission! We will look at the files you submitted and get back to you as soon as possible.";
+  $VARS[0]['redirecturl'] = "http://";
+  $VARS[0]['moreuploads'] = 0;
+  $VARS[0]['sendconfirmation'] = 0;
+  $VARS[0]['confirmtitle'] = "";
+  $VARS[0]['confirmmsg'] = "";
+  $VARS[0]['attachments'] = "never";
+  $VARS[0]['attachmaxsize'] = 1000;
+  $VARS[0]['notification'] = "none";
+  $VARS[0]['emailfield'] = 1;
+  $VARS[0]['takeip'] = 0;
+  $VARS[0]['fieldsize'] = 40;
+  $VARS[0]['displayinfo'] = "description";
+  $VARS[0]['fonttype'] = "Verdana, Arial, Helvetica, sans-serif";
+  $VARS[0]['adminemail'] = "";
+  $VARS[0]['adminname'] = "";
+  $VARS[0]['adminpass'] = "";
+  $VARS[0]['showrequired'] = 1;
+  $VARS[0]['fontsizel'] = "13px";
+  $VARS[0]['fontsizes'] = "10px";
+  $VARS[0]['email_method'] = "default";
+  $VARS[0]['smtp_host'] = "localhost";
+  $VARS[0]['smtp_port'] = 25;
+  $VARS[0]['subdir_field'] = 3;
+  $VARS[0]['header'] = "<html>
+<head>
+<title>Upload Form</title>
+</head>
+<body bgcolor=\"#FFFFFF\">
+<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" valign=\"middle\" align=\"center\" height=\"100%\">
+  <tr>
+    <td>
+      <table border=\"0\" cellspacing=\"1\" cellpadding=\"10\" bgcolor=\"#000000\">
+        <tr bgcolor=\"#F2F2F2\"> 
+          <td> ";
+  $VARS[0]['footer'] = "          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+</body>
+</html>";
+  $VARS[0]['formpass'] = "";
+  $VARS[0]['namefield'] = 2;
+  $VARS[0]['includelinks'] = 1;
+  $VARS[0]['notifyemails'] = "";
+  $VARS[0]['language_pack'] = "lang_english.php";
+  $VARS[0]['display_warning'] = 0;
+  $VARS[0]['js_detection'] = 1;
+  $VARS[0]['timezone'] = 0;
+  $VARS[0]['autodel_files'] = 0;
+  $VARS[0]['autodel_info'] = 0;
+  $VARS[0]['autodel_dir'] = 0;
+  $VARS[0]['storage_method'] = "file";
+  $VARS[0]['dbhost'] = "";
+  $VARS[0]['dbuser'] = "";
+  $VARS[0]['dbpass'] = "";
+  $VARS[0]['dbname'] = "";
+  $VARS[0]['formprotect'] = "none";
+  $VARS[0]['banned_ips'] = "";
+  $VARS[0]['subdir_user'] = "name";
+  $VARS[0]['reload_info'] = 0;
+  $VARS[0]['version'] = "";
+
+?>
