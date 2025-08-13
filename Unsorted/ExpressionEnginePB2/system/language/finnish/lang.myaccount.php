2ee12438e2eabc6bbb2bee3e9a5ac11a6ac72fdd
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:13 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.myaccount.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.myaccount.php
@@ -0,0 +1,457 @@
+<?php
+$L = array(
+
+
+"preferences_updated" =>
+"Ominaisudet päivitetty",
+
+"homepage_preferences" =>
+"Kotisivun ominaisuudet",
+
+"set_display_order" =>
+"Aseta näyttöjärjestys",
+
+"please_update_order" =>
+"Valitse kohteiden näyttöjärjestys",
+
+"left_column" =>
+"Vasen sarake",
+
+"right_column" =>
+"Oikea sarake",
+
+"do_not_show" =>
+"Älä näytä",
+
+"recent_entries" =>
+"Sinun 10 viimeisintä weblogin viestiä",
+
+"recent_comments" =>
+"10 viimeisintä kommenttia",
+
+"recent_members" =>
+"10 viimeisintä rekisteröitymistä",
+
+"new_release_indicator" =>
+"Uuden ohjelmaversion indikaattori",
+
+"site_statistics" =>
+"Sivujen tilastot",
+
+"member_search_form" =>
+"Jäsenten etsintä lomake",
+
+"validating_members" =>
+"Aktivointia odottavat jäsenet",
+
+"php_sql_versions" =>
+"PHP/SQL versio",
+
+"system_offline_indicator" =>
+"Systeemin offline indikaattori",
+
+"personal_settings" =>
+"Henkilökohtaiset asetukset",
+
+"weblog_settings" =>
+"Weblogin asetukset",
+
+"control_panel_settings" =>
+"Käyttäjän kustomointi",
+
+"administrative_settings" =>
+"Järjestelmänvalvojan asetukset",
+
+"current_member" =>
+"Jäsen:",
+
+"member_stats" =>
+"Tilin tilastot:",
+
+"join_date" =>
+"Liittymispäivä",
+
+"last_visit" =>
+"Viimeinen kirjautuminen",
+
+"total_entries" =>
+"Viestejä kaikkiaan",
+
+"last_entry_date" =>
+"Viimeisimmän viestin päiväys",
+
+"total_comments" =>
+"Kommentteja kaikkian:",
+
+"last_comment_date" =>
+"Päivä, jolloin eniten kommentteja",
+
+"notepad" =>
+"Muistio",
+
+"can_not_view_superadmin_profile" =>
+"Et voi tarkastella pääkäyttäjän tietoja",
+
+"only_self_notpad_access" =>
+"Et voi päästä muiden jäsenten muistioon",
+
+"notepad_size" =>
+"Muistion koko",
+
+"notepad_instructions" =>
+"Käytä tätä lomaketta tallentaaksesi henkilökohtaisia viestejä ja huomautuksia",
+
+"notepad_updated" =>
+"Muistio päivitetty",
+
+"localization_settings" =>
+"Paikalliset asetukset",
+
+"localization_updated" =>
+"Paikallisasetukset päivitetty",
+
+"daylight_savings_time" =>
+"Aamun tallennus aika",
+
+"timezone" =>
+"Valitse aikavyöhykkeesi",
+
+"language_choice" =>
+"Valitse kielesi",
+
+"username_and_password" =>
+"Käyttäjätunnus ja salasana",
+
+"extras" =>
+"Extrat",
+
+"edit_profile" =>
+"Muokkaa profiiliasi",
+
+"profile_form" =>
+"Profiili lomake",
+
+"base_profile" =>
+"Perusprofiili",
+
+"extended_profile" =>
+"Laajennettu profiili",
+
+"localization" =>
+"Paikallisasetukset",
+
+"time_format" =>
+"Ajan esitysmuoto",
+
+"united_states" =>
+"Amerikkalainen",
+
+"european" =>
+"Euroopalainen",
+
+"your_ping_servers" =>
+"Ping Serverit",
+
+"your_html_buttons" =>
+"HTML pikapainikkeet",
+
+"change_password" =>
+"Vaihda salasana",
+
+"email_settings" =>
+"Email asetukset",
+
+"subscriptions" =>
+"Allekirjoitus",
+
+"edit_subscriptions" =>
+"Katsele allekirjoitustasi",
+
+"customize_homepage" =>
+"Muokkaa ohjauspaneelin kotisivua",
+
+"customize_cp" =>
+"Muokkaa ohjauspaneelia",
+
+"cp_homepage" =>
+"Ohjauspaneelin kotisivu",
+
+"cp_theme" =>
+"Ohjauspaneelin teema",
+
+"choose_theme" =>
+"Valitse ohjauspaneelisi teema",
+
+"administrative_options" =>
+"Hallinnan ominaisuudet",
+
+"administrative_options_updated" =>
+"Hallinan ominaisuudet päivitetty",
+
+"screen_name" =>
+"Näyttönimi",
+
+"email" =>
+"Email osoite",
+
+"url" =>
+"URL",
+
+"bio" =>
+"Henkilöhistoria",
+
+"birthday" =>
+"Syntymäpäivä",
+
+"year" =>
+"Vuosi",
+
+"month" =>
+"Kuukausi",
+
+"day" =>
+"Päivä",
+
+"accept_admin_email" =>
+"Hyväksy email muilta pääkäyttäjiltä",
+
+"accept_user_email" =>
+"Hyväksy email muilta jäseniltä tältä sivulta",
+
+"notify_by_default" =>
+"Salli email ilmoitus oletuksena kun kirjoitat viestin",
+
+"location" =>
+"Sijainti",
+
+"occupation" =>
+"Koulutus",
+
+"interests" =>
+"Harrastukset",
+
+"icq" =>
+"ICQ",
+
+"aol_im" =>
+"AOL IM",
+
+"yahoo_im" =>
+"Yahoo IM",
+
+"msn_im" =>
+"MSN IM",
+
+"user_ip_address" =>
+"IP osoite:",
+
+"member_group_assignment" =>
+"Käyttäjäryhmän määritys",
+
+"member_group_warning" =>
+"Ole varovainen määritellessäsi tätä",
+
+"include_in_multiauthor_list" =>
+"Lisää käyttäjiä JULKAISE sivun käyttäjienlistalle",
+
+"profile_updated" =>
+"Profiili päivitetty",
+
+"super_admin_demotion_alert" =>
+"Pääkäyttäjänä, et voi muuttaa käyttäjäryhmäsi määrittelyä",
+
+"username" =>
+"Käyttäjätunnus",
+
+"username_change_not_allowed" =>
+"Ylläpito ei salli käyttäjätunnuksen muutosta",
+
+"password" =>
+"Salasana",
+
+"password_change" =>
+"Salasanan muutos lomake",
+
+"new_password" =>
+"Uusi salasana",
+
+"password_confirm" =>
+"Vahvista salasana",
+
+"new_password_confirm" =>
+"Uuden salasanan vahvistus",
+
+"existing_password" =>
+"Tämän hetkinen salasanasi",
+
+"invalid_email_address" =>
+"Email osoitteesi ei ollut käypä",
+
+"password_required" =>
+"Sinun tulee syöttää tämän hetkinen salasanasi päivittääksesi tämän sivun",
+
+"password_required_for_email" =>
+"Vaihtaaksesi email osoitteesi tulee sinun antaa oikea salasanasi",
+
+"leave_blank" =>
+"Jätä tyhjäksi, jos et halua muuttaa tietoja",
+
+"missing_username" =>
+"Sinun tulee antaa käyttäjätunnus",
+
+"invalid_characters_in_username" =>
+"Käyttäjätunnuksessasi on kiellettyjä merkkejä",
+
+"missing_password" =>
+"Sinun tulee syöttää salasana",
+
+"missing_current_password" =>
+"Muuttaaksesi tietoja tulee sinun antaa oikea salasanasi",
+
+"password_in_dictionary" =>
+"Et voi käyttää salasanassa sanoja, jotka löytyvät sanakirjasta",
+
+"not_secure_password" =>
+"Salasanan tulee sisältää ainakin yksi isoilla kirjaimilla, yksi pienillä kirjoitettu kirjain ja numero",
+
+"invalid_password" =>
+"Antamasi salasana ei ollut oikein ",
+
+"missing_email" =>
+"Sinun tulee antaa email osoitteesi",
+
+"banned_email" =>
+"Antamasi email osoite on kielletty (blokattu)",
+
+"missmatched_passwords" =>
+"Salasana ja sen vahvistus eivät täsmää",
+
+"username_too_short" =>
+"Käyttäjätunnuksen tulee olla ainakin  %x merkkiä pitkä",
+
+"password_too_short" =>
+"Salasanan tulee olla ainakin %x merkkiä pitkä",
+
+"username_password_too_long" =>
+"Käyttäjätunnus ja salasana ei voi olla 32 merkkiä pitempi",
+
+"username_taken" =>
+"Valitsemasi käyttäjätunnus ei ole käypä",
+
+"screen_name_taken" =>
+"Näyttönimi ei ole käypä",
+
+"email_taken" =>
+"Valisemasi email osoite ei ole käytettävissä",
+
+"password_based_on_username" =>
+"Salasana ei voi perustua käyttäjätunnukseen",
+
+"username_changed" =>
+"Käyttäjätunnus muutettiin seuraavaksi:",
+
+"screen_name_changed" =>
+"Näyttönimi muutettiin seuraavaksi:",
+
+"email_changed" =>
+"email muutettiin seuraavaksi:",
+
+"password_change_requires_login" =>
+"Huom: jos muutit salasana tulee sinun uudelleen kirjautua",
+
+"settings_updated" =>
+"Asetukset on tallennettu",
+
+"new_member_added" =>
+"Jäsenen profiili muodostettu:",
+
+"password_change_warning" =>
+"Huom: sinä muutit salasanasi, joten sinun tulee kirjautua uudestaan.",
+
+"ping_servers" =>
+"Ping Serveri",
+
+"pingservers_updated" =>
+"Ping Serveri päivitetty",
+
+"html_buttons" =>
+"HTML muotoilu painikkeet",
+
+"html_buttons_updated" =>
+"HTML painikkeet päivitetty",
+
+"quick_links" =>
+"Pikalinkit",
+
+"only_self_qucklink_access" =>
+"Sinulla ei ole oikeutta muiden jäsenten pikalinkkeihin",
+
+"quicklinks_updated" =>
+"Pikalinkit on päivitetty",
+
+"quick_link_description" =>
+"Käytä tätä lomaketta lisätäksesi henkilökohtaisia linkkejä ylätyökaluriville",
+
+"quick_link_description_more" =>
+"Nämä linkit näkyvät vain sinulle",
+
+"quicklinks_delete_instructions" =>
+"Poistaaksesi kohde lähetä lomake jossa tyhjä otsikkokenttä",
+
+"link_title" =>
+"Linkin otsikko",
+
+"link_url" =>
+"Linkin URL",
+
+"link_order" =>
+"Järjestys",
+
+"no_extended_fields" =>
+"Tällä hetkellä ei ole laajennettuja jäsen profiilikentiä",
+
+"bookmarklet" =>
+"Bookmarklet",
+
+"bookmarklet_created" =>
+"Bookmarklet muodostettu",
+
+"bookmarklet_info" =>
+"Bookmarklet mahdollistaa sinun lisätä vietejä ilman kirjautumista koko ohjauspaneeliin.",
+
+"consult_manual" =>
+"Käyttöohjekirjassa on lisä infoa",
+
+"bookmarklet_name" =>
+"Bookmarkletin nimi",
+
+"single_word_no_spaces" =>
+"Yksi sana, ei välilyöntejä",
+
+"weblog_name" =>
+"Valitse weblog, jolle haluat viestin lähettää",
+
+"no_blogs_assigned_to_user" =>
+"Tilillesi ei ole yhdistetty yhtään weblogia",
+
+"select_field" =>
+"Valitse oletus kenttä, jolle haluat viestin ilmestyvän",
+
+"bookmarklet_next_step" =>
+"Seuraava askel",
+
+"create_the_bookmarklet" =>
+"Muodosta bookmarklet",
+
+"no_fields_assigned_to_blog" =>
+"Valitsemasi weblog ei sisällä kenttää jonka vallitsit",
+
+"safari_users" =>
+"Paina, jos käytät selaimenasi Safaria ",
+
+"bookmarklet_instructions" =>
+"Tallenna tämä linkki suosikkeihisi:",
+
+''=>''
+);
+?>
