ecbd86bcdee33942ab10f01d52e3b4cc23758bd8
me-suzy <me.suzana@gmail.com>
2025-08-13 04:32:38 +0300

    Add Unsorted/ezupload_pro_v21.0_nullified/ezupload/footer.php

---
+++ Unsorted/ezupload_pro_v21.0_nullified/ezupload/footer.php
@@ -0,0 +1,35 @@
+<? 
+/////////////////////////////////////////////////////////////
+// Program Name         : EzUpload Pro                       
+// Program Version      : 2.0                                
+// Program Author       : ScriptsCenter.com                  
+// Supplied by          : Stive [WTN]                        
+// Nullified and tested : CyKuH [WTN]                        
+// Distribution         : via WebForum and Forums File Dumps 
+//                    WTN Team `2002
+/////////////////////////////////////////////////////////////
+  global $safemode;
+  $safemodestatus = $safemode ? "On" : "Off";
+?>
+
+		  </td>
+        </tr>
+      </table>
+      <table width="500" border="0" cellspacing="0" cellpadding="0" align="center" height="10">
+        <tr>
+          <td></td>
+        </tr>
+      </table>
+      <table width="500" border="0" cellspacing="0" cellpadding="1" align="center">
+        <tr>
+          <td align="center"><!--CyKuH-->EzUpload Pro 2.0 &copy; 2001-2002 ScriptsCenter.com. All Rights Reserved.</td>
+        </tr>
+	    <tr>
+		  <td align="center">Server running on <b>PHP <?=phpversion()?></b> with Safe Mode <b><?=$safemodestatus?></b></td>
+		</tr>
+      </table>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
