c5d5b85ebee4d5f20a792f7732ab3d37b92aeaf1
me-suzy <me.suzana@gmail.com>
2025-08-13 03:27:25 +0300

    Add Unsorted/ebay/RedirectCompleted/Resource.h

---
+++ Unsorted/ebay/RedirectCompleted/Resource.h
@@ -0,0 +1,12 @@
+/*	$Id: Resource.h,v 1.3 1999/02/21 02:24:21 josh Exp $	*/
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by RedirectCompleted.rc
+//
+
+#define IDS_FILTER        101
+
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_CONTROL_VALUE         103
+#define _APS_NEXT_SYMED_VALUE           103
+#define _APS_NEXT_COMMAND_VALUE         32768
