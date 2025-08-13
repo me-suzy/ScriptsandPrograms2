6736f2d6284efe0469c06d227fccf7a568baa333
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:23 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-installer_installer.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-installer_installer.php
@@ -0,0 +1,42 @@
+<?php
+
+##################################################
+
+# /installer/*.php
+
+##################################################
+
+define( "ADMINST_ERR_VERSION", "Aquesta eina d'instal·lació no es pot utilitzar amb aquesta\\n còpia del Netjuke. Les versions no coincideixen.\\nSi us plau, aconseguiu la darrera còpia i torneu a intentar-ho." );
+
+define( "ADMINST_ERR_FSPATH_1", "Si us plau, indiqueu la ubicació dels fitxers." );
+define( "ADMINST_ERR_FSPATH_2", "La ubicació del fitxer no pot contenir://" );
+define( "ADMINST_ERR_WEBPATH_1", "Si us plau, indiqueu l'adreça del Netjuke a la Web." );
+define( "ADMINST_ERR_WEBPATH_2", "L'adreça Web ha de contenir http:// or https://" );
+
+define( "ADMINST_ERR_DBTYPE", "Si us plau, seleccioneu el tipus de base de dades." );
+define( "ADMINST_ERR_DBHOST", "Si us plau, seleccioneu el host de la base de dades." );
+define( "ADMINST_ERR_DBNAME", "Si us plau, seleccioneu el nom de la base de dades." );
+define( "ADMINST_ERR_DBCONN", "No es pot connectar a la base de dades amb la informació proporcionada." );
+
+define( "ADMINST_ERR_EMAIL", "Si us plau, introduïu una adreça electrònica per al primer compte d'administrador." );
+define( "ADMINST_ERR_PASS", "Si us plau, introduïu una contrasenya per al primer compte d'administrador." );
+
+define( "ADMINST_ERR_STREAM", "Si us plau, introduïu un servidor Streaming." );
+
+define( "ADMINST_ERR_NOSQL", "Aquesta eina d'instal·lació és defectuosa. Si us plau, aconseguiu una nova còpia" );
+
+define( "ADMINST_ERR_DENIED_1", "- Aquesta eina d'instal·lació no es pot utilitzar amb aquesta còpia del Netjuke perquè ja està inicialitzada.\\n- Si us plau, utilitzeu el procés d'actualització." );
+define( "ADMINST_ERR_DENIED_2", "- Aquesta eina d'actualització no es pot utilitzar amb aquesta còpia del Netjuke.\\n- No s'ha instal·latla base de dades." );
+define( "ADMINST_ERR_DENIED_3", "La ubicació del fitxer no és vàlida." );
+define( "ADMINST_ERR_DENIED_4", "Accéss denegat: L'adreça electrònica i la contrasenya indicades no corresponen a cap compte d'administrador existent." );
+
+define( "ADMINST_ERR_EXEC", "Hi ha hagut un problema en l'execució del sql generat.\\nSi us plau, torneu a intentar-ho." );
+
+define( "ADMINST_FILEINFO_1", "Creat" );
+define( "ADMINST_FILEINFO_2", "Des de" );
+define( "ADMINST_FILEINFO_3", "Anomena i desa" );
+define( "ADMINST_FILEINFO_4", "S'ha desat correctament. Ja podeu accedir a la nova versió a" );
+
+##################################################
+
+?>
