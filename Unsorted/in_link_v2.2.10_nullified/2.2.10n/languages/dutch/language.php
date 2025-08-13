b4d3e9c42d7d14568ea49670625e3db3b16f7487
me-suzy <me.suzana@gmail.com>
2025-08-13 05:45:39 +0300

    Add Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/dutch/language.php

---
+++ Unsorted/in_link_v2.2.10_nullified/2.2.10n/languages/dutch/language.php
@@ -0,0 +1,295 @@
+<?php
+## In-link Gebruiker Language File - English
+## Author: Intechnic Corporation
+## Datum: 07/15/2001
+##
+## This file is designed for the following version of In-link: 2.0.6
+## You can modify the text in this file to suit your language needs. 
+## Make sure to escape quotation marks with \" . 
+
+#########################################################################################
+#Language variables
+
+$lu_char_set								=   "ISO-8859-1";
+$lu_this_language							=	"Dutch";	
+
+#########################################################################################
+#Navigation Menu
+
+$lu_getting_rated							=	"Vraag waardering";
+$lu_go_to_page								=	"Ga naar pagina";
+$lu_search_results							=	"Zoek resultaten";
+$lu_nav_addlink								=	"Link toevoegen";
+$lu_nav_editorspick							=	"Aanbevolen";
+$lu_nav_gettingrated						=	"Vraag waardering";
+$lu_nav_home								=	"Home";
+$lu_nav_login								=	"Inloggen";
+$lu_nav_logout								=	"Uitloggen";
+$lu_nav_maillist							=	"Registreren";
+$lu_nav_modlink								=	"Wijzig link";
+$lu_nav_new									=	"Nieuwe links";
+$lu_nav_pop									=	"Populair";
+$lu_nav_profile								=	"Profiel";
+$lu_nav_register							=	"Registreer";
+$lu_nav_search								=	"Zoeken";
+$lu_nav_search_advanced						=	"Uitgebreid zoeken";
+$lu_nav_suggestcat							=	"Rubriek voorstellen";
+$lu_nav_top									=	"Hoog gewaardeerd";
+$lu_navbar_seperator						=	" >";
+
+
+#########################################################################################
+#Page Titles
+
+$lu_title_add_link							=	"Link toevoegen";
+$lu_title_add_review						=	"Waardering toevoegen";
+$lu_title_confirm							=	"Bevestiging";
+$lu_title_edit_link							=	"Wijzig link";
+$lu_title_editors_picks						=	"Aanbevolen";
+$lu_title_error								=	"Error";
+$lu_title_gettingrated						=	"Vraag waardering";
+$lu_title_login								=	"Inloggen";
+$lu_title_modify							=	"Uw links wijzigen";
+$lu_title_most_popular						=	"Populaire links";
+$lu_title_msg								=	"Systeem bericht";
+$lu_title_new_links							=	"Nieuwe links";
+$lu_title_profile							=	"Gebruikers profiel";
+$lu_title_rate								=	"Geef waardering";
+$lu_title_registration						=	"Registratie";
+$lu_title_reviews							=	"Reakties";
+$lu_title_search							=	"Zoeken";
+$lu_title_search_advanced					=	"Uitgebreid zoeken";
+$lu_title_subscribe							=	"Aanmelden voor mailinglist";
+$lu_title_suggest							=	"Rubriek voorstellen";
+$lu_title_top_rated							=	"Hoog gewaardeerd";
+
+
+#########################################################################################
+#Link and Category Markers (Can replace them with graphics)
+
+$lu_cat_new									=	"nieuw";
+$lu_cat_pick								=	"tip";
+$lu_link_new								=	"nieuw";
+$lu_link_pick								=	"tip";
+$lu_link_pop								=	"pop";
+$lu_link_top								=	"top";
+
+
+#########################################################################################
+#System Messages
+
+$lu_no_categories							=	"Geen rubrieken";
+$lu_no_links								=	"Geen links";
+
+
+#########################################################################################
+#Link/Category Details 
+
+$lu_added									=	"Toegevoegd";
+$lu_cat										=	"Rubriek";
+$lu_date_created							=	"Datum toegevoegd";
+$lu_desc									=	"Beschrijving";
+$lu_edit									=	"Wijzig";
+$lu_editor_pick								=	"Aanbevolen";
+$lu_from									=	"Van";
+$lu_general									=	"Algemeen";
+$lu_image									=	"Icoon";
+$lu_hits									=	"Hits";
+$lu_link									=	"Link";
+$lu_name									=	"Naam";
+$lu_no										=	"Nee";
+$lu_rateit									=	"Geef waardering";
+$lu_rating									=	"Waardering";
+$lu_reviews									=	"Reakties";
+$lu_to										=	"tot";
+$lu_votes									=	"Stemmen";
+$lu_url										=	"URL";
+$lu_yes										=	"Ja";
+
+
+#########################################################################################
+#Zoeken 
+
+$lu_boolean_type_search						=	"Trefwoorden";
+$lu_return_number_results					=	"Resultaten per pagina";
+
+
+#########################################################################################
+#Errors
+
+$lu_already_voted							=	"Volgens ons systeem heeft u al gestemd. U kunt maar eens per maand stemmen!";
+$lu_already_voted							=	"U bent niet gerechtigd om te stemmen!";
+$lu_email_used								=	"Het e-mailadres is al in gebruik";
+$lu_error_404								=	"Kan het file niet openen.";
+$lu_error_addlink_not_allowed				=	"Links toevoegen is niet toegestaan";
+$lu_error_addreview_not_allowed				=	"Rubrieken toevoegen is niet toegestaan";
+$lu_error_addto_cat_not_allowed				=	"Deze rubriek is niet specifiek genoeg, kies een andere rubriek.";
+$lu_error_cannot_remove_cat					=	"Kan de laatste rubriek niet verwijderen!";
+$lu_error_db								=	"Database fout!";
+$lu_error_file_not_found					=	"Het file is aanwezig:";
+$lu_error_pass_match						=	"De opgegeven wachtwoorden zijn niet gelijk.";
+$lu_error_pass_wrong						=	"Het wachtwoord is onjuist.";
+$lu_error_review_not_filled					=	"De reaktie mag niet leeg zijn";
+$lu_error_user_not_allowed					=	"U kunt zich niet registreren!";	
+$lu_error_wrong_user						=	"De link hoort bij een andere gebruiker!";
+$lu_invalid_entry							=	"Onjuiste invoer";
+$lu_login_incorrect							=	"Login onjuist";
+$lu_pass_not_match							=	"Wachtwoorden zijn niet gelijk";
+$lu_username_used							=	"De gebruikersnaam is al in gebruik";
+
+#########################################################################################
+#Confirmations
+
+$lu_confirm_registration					=	"Uw registratie is verzonden.";
+$lu_confirm_suggest_site					=	"Bedankt voor het attenderen.";
+$lu_confirm_profile							=	"Uw profiel is gewijzigd.";
+$lu_confirm_linkupdate						=	"Uw link is gewijzigd.";
+$lu_confirm_addlink							=	"Uw link is opgeslagen.";
+$lu_confirm_suggest							=	"Bedankt voor uw voorstel.";
+$lu_confirm_addreview						=	"Uw recentie is opgeslagen.";
+$lu_confirm_subscribe						=	"U bent aangemeld in de mailinglist.";
+$lu_confirm_unsubscribe						=	"U bent verwijderd uit de mailinglist.";
+
+#########################################################################################
+#Buttons
+
+$lu_button_addcat							=	"Rubriek toevoegen";
+$lu_button_addlink							=	"Link toevoegen";
+$lu_button_addreview						=	"Reaktie toevoegen";
+$lu_button_back								=	"Terug";
+$lu_button_cancel							=	"Stop";
+$lu_button_editlink							=	"Wijzig link";
+$lu_button_go								=	"Uitvoeren";
+$lu_button_login							=	"Inloggen";
+$lu_button_ok								=	"Ok";
+$lu_button_rate								=	"Rate";
+$lu_button_register							=	"Registreer";
+$lu_button_reset							=	"Herstel";
+$lu_button_search							=	"Zoeken";
+$lu_button_search_cats						=	"Rubrieken doorzoeken";
+$lu_button_search_links						=	"Links doorzoeken";
+$lu_button_send								=	"Verzenden";
+$lu_button_subscribe						=	"Aanmelden";
+$lu_button_unsubscribe						=	"Afmelden";
+$lu_button_update							=	"Update";
+$lu_mailing_list							=	"Mailinglist";
+$lu_not_mailing_list						=	"Niet in mailinglist";
+
+
+#########################################################################################
+#Statistics
+
+$lu_cats									=	"Rubrieken:";
+$lu_links									=	"Links:";
+$lu_total_cats								=	"Aantal rubrieken in de database:  ";
+$lu_total_hits								=	"Hits: ";
+$lu_total_links								=	"Aantal links in de database: ";
+
+
+#########################################################################################
+#Addlink template
+
+$lu_add_cat									=	"Deze rubriek toevoegen";
+$lu_additional_cats							=	"Aanvullende rubrieken:";
+$lu_remove									=	"Verwijder";
+$lu_select									=	"Selecteer";
+$lu_submitting_to							=	"You are submiting to:";
+
+
+#########################################################################################
+#Registreren template
+
+$lu_first_name								=	"Voornaam";
+$lu_last_name								=	"Achternaam";
+$lu_re_password								=	"Wachtwoord opnieuw";
+$lu_user_name								=	"Gebruikersnaam";
+
+
+#########################################################################################
+#Miscellaneos Pages
+
+$lu_advanced_search							=	"Uitgebreid zoeken";
+$lu_atleast_3								=	"(minimaal 3)";
+$lu_email									=	"E-mail";
+$lu_hint_suggest							=	"Attendeer een vriend(din) op deze site";
+$lu_keyword									=	"Trefwoord";
+$lu_login									=	"Inloggen";
+$lu_not_logged_in							=	"Niet ingelogd";
+$lu_password								=	"Wachtwoord";
+$lu_register								=	"Registreren";
+$lu_review									=	"Review";
+$lu_search									=	"Zoeken";
+$lu_search_for								=	"Zoeken naar";
+$lu_show									=	"Laat zien";
+$lu_sort_by									=	"Sorteer op";
+$lu_status									=	"Status";
+$lu_suggest									=	"Voortellen";
+$lu_user									=	"Gebruiker";
+$lu_user_menu								=	"Gebruikers menu";
+$lu_welcome									=	"Welkom";
+
+
+#########################################################################################
+#Dropdowns
+
+$lu_drop_and								=	"Exact";
+$lu_drop_as_many_as_found					=	"alle";
+$lu_drop_ascending							=	"Oplopend";
+$lu_drop_date								=	"Datum";
+$lu_drop_desc								=	"Beschrijving";
+$lu_drop_descending							=	"Aflopend";
+$lu_drop_editors_pick						=	"Aanbevolen";
+$lu_drop_hits								=	"Hits";
+$lu_drop_image								=	"Icoon";
+$lu_drop_name								=	"Naam";
+$lu_drop_numlinks							=	"Aantal links";
+$lu_drop_numsubs							=	"Aantal rubrieken";
+$lu_drop_or									=	"Enkele woorden";
+$lu_drop_perm								=	"Rechten";
+$lu_drop_rating								=	"Waardering";
+$lu_drop_url								=	"URL";
+$lu_drop_user								=	"Gebruiker";
+$lu_drop_vis								=	"Zichtbaarheid";
+$lu_drop_votes								=	"Stemmen";
+
+
+#########################################################################################
+#Custom Fields
+
+$lu_categories_custom_fields1			    =	"Custom veldnaam #1";
+$lu_categories_custom_fields2			    =	"Custom veldnaam #2";
+$lu_categories_custom_fields3			    =	"Custom veldnaam #3";
+$lu_categories_custom_fields4			    =	"Custom veldnaam #4";
+$lu_categories_custom_fields5			    =	"Custom veldnaam #5";
+$lu_categories_custom_fields6			    =	"Custom veldnaam #6";
+$lu_links_custom_fields1					=	"Custom veldnaam #1";
+$lu_links_custom_fields2					=	"Custom veldnaam #2";
+$lu_links_custom_fields3					=	"Custom veldnaam #3";
+$lu_links_custom_fields4					=	"Custom veldnaam #4";
+$lu_links_custom_fields5					=	"Custom veldnaam #5";
+$lu_links_custom_fields6					=	"Custom veldnaam #6";
+$lu_users_custom_fields1					=	"Custom veldnaam #1";
+$lu_users_custom_fields2					=	"Custom veldnaam #2";
+$lu_users_custom_fields3					=	"Custom veldnaam #3";
+$lu_users_custom_fields4					=	"Custom veldnaam #4";
+$lu_users_custom_fields5					=	"Custom veldnaam #5";
+$lu_users_custom_fields6					=	"Custom veldnaam #6";
+
+
+#########################################################################################
+#Waarderings
+
+$lu_excellent								=	"Geweldig!";
+$lu_fair									=	"Redelijk";
+$lu_good									=	"Goed";
+$lu_poor									=	"Slecht";
+$lu_verygood								=	"Erg goed";
+
+
+#########################################################################################
+#Text segments
+
+$lu_txt_gettingrated	=	"Als je de volgende code op je website plaatst geef je je bezoekers de mogelijkheid te stemmen:";
+
+
+?>
