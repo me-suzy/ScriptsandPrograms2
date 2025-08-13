52e875f2042144c1b8e85a69d5b4ad15ce69d69a
me-suzy <me.suzana@gmail.com>
2025-08-13 03:05:26 +0300

    Add Unsorted/ebay/dbscript/etu.sql

---
+++ Unsorted/ebay/dbscript/etu.sql
@@ -0,0 +1,5 @@
+/*	$Id: etu.sql,v 1.2 1999/02/21 02:30:52 josh Exp $	*/
+create table ebay_temp_users(userid varchar2(100))
+tablespace SUMMARY
+storage(initial 1M next 1M minextents 1 maxextents 99 pctincrease 0)
+/
