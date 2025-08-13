1d3457f3991c671270ca68282f7d8eebfc1601a0
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:20 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/log.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/log.php
@@ -0,0 +1,15 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[LOG_License] = "O software de Licensed";
+
+?>
