21f6ae65fbc5ac750622620a2576739db619b0f6
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:52 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.home.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.home.php
@@ -0,0 +1,105 @@
+<?php
+$L = array(
+
+"total_validating_comments" =>
+"Comments Awaiting Validation",
+
+"cp_home" =>
+"Mi página incial",
+
+"current_user" =>
+"Usuario actual:",
+
+"system_status" =>
+"Estado del sistema",
+
+"offline" =>
+"Desconectado",
+
+"online" =>
+"Conectado",
+
+"member_search" =>
+"Búsqueda de miembros",
+
+"search_instructions" =>
+"Enviar palabras parciales o completas",
+
+"member_group" =>
+"Grupo de miembros",
+
+"search_by" =>
+"Campo de búsqueda",
+
+"screen_name" =>
+"Nombre público (por ejemplo: tu nombre real)",
+
+"email_address" =>
+"Dirección de email",
+
+"url" =>
+"URL",
+
+"notepad" =>
+"Bloc de notas",
+
+"site_statistics" =>
+"Estadísticas del sitio",
+
+"value" =>
+"Valor",
+
+"total_members" =>
+"Total de miembros",
+
+"total_validating_members" =>
+"Miembros esperando activación",
+
+"total_entries" =>
+"Total artículos",
+
+"total_comments" =>
+"Total comentarios",
+
+"total_trackbacks" =>
+"Total Trackbacks",
+
+"most_recent_entries" =>
+"Tus artículos más recientes",
+
+"most_recent_comments" =>
+"Comentarios/TB más recientes",
+
+"no_comments" =>
+"Actualmente no hay comentarios ni trackbacks",
+
+"no_entries" =>
+"Actualmente no hay artículos",
+
+"entry_title" =>
+"Título del artículo",
+
+"comments" =>
+"Cmts",
+
+"recent_members" =>
+"Miembros nuevos más recientes",
+
+"join_date" =>
+"Fecha de alta",
+
+"total_hits" =>
+"Total de accesos a página combinada",
+
+"demo_expiration" =>
+"Tu cuenta de demostración expirará en:",
+
+"install_lock_warning" =>
+"Atención, el fichero de instalación todavía está en el servidor.",
+
+"install_lock_removal" =>
+"Por razones de seguridad, deberías de eliminar el fichero &quot;install.php&quot; usando tu acceso FTP",
+
+''=>''
+);
+?>
