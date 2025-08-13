a07d8939a3c697199c0a0b9be7b18681a0663e7d
me-suzy <me.suzana@gmail.com>
2025-08-13 06:39:04 +0300

    Add Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/en/lng_login.php

---
+++ Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/en/lng_login.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['login_page_subtitle']            = 'Member LogIn';
+$lang['submit_login']	                = 'Log me in';
+$lang['username']	                = 'UserName';
+$lang['password']	                = 'PassWord';
+$lang['remember_login']	                = 'Remember me?';
+$lang['success_text']                   = 'You successfully loged in! Please wait while we redirect You ...';
+$lang['logout_text']                    = 'You successfully loged out! Please wait while we redirect You ...';
+$lang['lost_passwd_link_text']          = 'Remind password?';
+$lang['register_link_text']             = 'Want to register?';
+$lang['lostpasswd_page_subtitle']       = 'Password remind system';
+$lang['submit_email']                   = 'retrieve password';
+$lang['lpwd_email']                     = 'Please, enter Your registered email address';
+$lang['pwd_email_sent']                 = 'Your login details was sent to Your registered email address';
+$lang['procced_to_login_link_text']     = 'Proceed to LogIn';
+
+
+?>
