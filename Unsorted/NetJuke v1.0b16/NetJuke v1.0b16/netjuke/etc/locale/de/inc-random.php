df57a192269f3e9f2a186a1e04569d8506bae9d0
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:06 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-random.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-random.php
@@ -0,0 +1,25 @@
+<?php
+
+##################################################
+
+# /random.php
+
+##################################################
+
+define( "RNDM_NO_TR", "Sie m&üssen mindestens 2 Titel in diesem \\nGenre(n) haben bevor Sie die Zufallsfunktion benutzen können." );
+
+define( "RNDM_FORM_HEADER", "ZUFÄLLIGE MUSIK AUSWAHL" );
+define( "RNDM_FORM_LIMIT_1", "Spielliste begrenzen auf" );
+define( "RNDM_FORM_LIMIT_2", "Titel (Maximum)" );
+define( "RNDM_FORM_GENRES", "Genren begrenzen auf" );
+define( "RNDM_FORM_GENRES_ALL", "Alle Genren" );
+define( "RNDM_FORM_TRACKMINTIME_1", "Min. Titel Dauer" );
+define( "RNDM_FORM_TRACKMINTIME_2", "Sekunden pro Titel" );
+define( "RNDM_FORM_TRACKMAXTIME_1", "Max. Titel Dauer" );
+define( "RNDM_FORM_TRACKMAXTIME_2", "Sekunden pro Titel" );
+define( "RNDM_FORM_BTN_SEARCH", "Zufällige Spielliste Erstellen" );
+
+
+##################################################
+
+?>
