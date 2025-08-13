211979eb4730aa008714f2080dab0e0ec27bf128
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:30 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/upload.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/upload.php
@@ -0,0 +1,35 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[UPLOAD_Title] = "Emvie um arquivo!";
+
+$LOCALE[UPLOAD_Info] = "Por favor escolha um arquivo a upload e para o operador este arquivo é para.";
+
+$LOCALE[UPLOAD_Button] = "O Upload Agora";
+
+$LOCALE[UPLOAD_File_Send] = "O arquivo emviar";
+
+$LOCALE[UPLOAD_User_Send] = "Emvie a";
+
+$LOCALE[UPLOAD_Complete] = "Seu arquivo com êxito foi emviado!";
+
+$LOCALE[UPLOAD_Sent_File] = "Acabo de emviar você o arquivo";
+
+$LOCALE[UPLOAD_Err_Large] = "O arquivo que você escolhe é a grande, ou não fez upload corretamente.";
+
+$LOCALE[UPLOAD_Err_Accept] = "O operador nao é registrado nem nao aceita arquivos.";
+
+$LOCALE[UPLOAD_Err_Exists] = "O arquivo que você emviam já existe.";
+
+$LOCALE[UPLOAD_Err_Full] = "O operador nao tem espaço livre suficiente de armazenagem para seu arquivo.";
+
+?>
