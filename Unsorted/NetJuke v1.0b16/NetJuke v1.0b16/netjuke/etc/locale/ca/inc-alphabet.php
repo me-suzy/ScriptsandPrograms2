4f56cff18c7381822a13ce5427fcb82e55787add
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:16 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-alphabet.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-alphabet.php
@@ -0,0 +1,27 @@
+<?php
+
+##################################################
+
+# /alphabet.php
+
+##################################################
+
+define( "ALPHA_HEADER_1", "LLISTA ALFABÈTICA" );
+define( "ALPHA_HEADER_2", "Pistes" );
+define( "ALPHA_ARTISTS", "Intèrprets" );
+define( "ALPHA_ALBUMS", "Àlbums" );
+define( "ALPHA_FOUND", "Trobats" );
+
+define( "ALPHA_LIST_ALL_HELP", "Llista totes les canÁons associades" );
+define( "ALPHA_PLAY_ALL_HELP", "Feu clic per reproduir totes les canÁons associades" );
+define( "ALPHA_FILTER_ALBUMS_HELP", "Llista tots els artistes d'aquest àlbum." );
+define( "ALPHA_FILTER_ARTISTS_HELP", "Llista tots els àlbums d'aquest artista." );
+
+define( "ALPHA_PAGETOP", "Capçalera de la pàgina" );
+define( "ALPHA_PAGETOP_HELP", "Torna a la capçalera de la pàgina" );
+define( "ALPHA_CLOSEWIN", "Tanca la finestra" );
+define( "ALPHA_CLOSEWIN_HELP", "Tanca la finestra" );
+
+##################################################
+
+?>
