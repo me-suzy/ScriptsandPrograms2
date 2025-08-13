12e0c3e13c1e996338fd40d49b552c795805794f
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:40 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-search.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-search.php
@@ -0,0 +1,78 @@
+<?php
+
+##################################################
+
+# /search.php
+
+##################################################
+
+define( "SRCH_NOSTR", "Por favor especifique más su consulta." );
+
+define( "SRCH_CHCK_HELP", "Click para seleccionar esta pista" );
+define( "SRCH_PLAY_HELP", "Click para tocar esta pista" );
+define( "SRCH_DLOAD_HELP", "Click para bajar esta pista" );
+define( "SRCH_INFO_HELP", "Click para ver los detalles de la pista." );
+define( "SRCH_AR_HELP", "Click para ver todas las pistas de este artista." );
+define( "SRCH_AL_HELP", "Click para ver todas las pistas de este album." );
+define( "SRCH_GE_HELP", "Click para ver todas las pistas de este género." );
+
+define( "SRCH_HEADER", "RESULTADOS DE LA BÚSQUEDA Y OPCIONES" );
+
+define( "SRCH_ROWCNT_1", "Pista(s) Encontradas" );
+define( "SRCH_ROWCNT_2", "Página" );
+define( "SRCH_ROWCNT_3", "de" );
+define( "SRCH_ROWCNT_4", "para" );
+
+define( "SRCH_QS_AR", "Artista" );
+define( "SRCH_QS_TR", "Pista" );
+define( "SRCH_QS_AL", "Album" );
+define( "SRCH_QS_GE", "Género" );
+define( "SRCH_QS_TI", "Tiempo" );
+define( "SRCH_QS_BTN", "Búsqueda" );
+
+define( "SRCH_TB_PLIST_BTN", "Editar" );
+define( "SRCH_TB_LOGIN", "Usuario" );
+define( "SRCH_TB_REGISTER", "Registro" );
+define( "SRCH_TB_SELECT", "Seleccionar Todo" );
+define( "SRCH_TB_SELECT_HELP", "Click para seleccionar todas las pistas listadas en esta página." );
+define( "SRCH_TB_RESET", "Lanzar Todo" );
+define( "SRCH_TB_RESET_HELP", "Click para Lanzar Todas sus Pistas Seleccioadas" );
+define( "SRCH_TB_PLAYSEL", "Sonar Seleccionadas" );
+define( "SRCH_TB_PLAYSEL_HELP", "Click para empezar a tocar todas las pistas seleccionadas" );
+define( "SRCH_TB_ADDTO", "Adicionar a la lista" );
+define( "SRCH_TB_ADDTO_HELP", "Click para adicionar todas las pistas seleccionadas a la lista seleccionada" );
+
+define( "SRCH_TB_PREV", "Página Anterior." );
+define( "SRCH_TB_PREV_HELP", "Click para ir a la página anterior de los resultados de la búsqueda actual." );
+define( "SRCH_TB_NEXT", "Siguiente Página." );
+define( "SRCH_TB_NEXT_HELP", "Click para ir a la página siguiente de los resultados de la búsqueda actual." );
+define( "SRCH_TB_PAGE", "Página" );
+define( "SRCH_TB_PAGEBOT", "Hacia Abajo" );
+define( "SRCH_TB_PAGEBOT_HELP", "Arrastrar hacia abajo en la página." );
+define( "SRCH_TB_PAGETOP", "Hacia Arriba" );
+define( "SRCH_TB_PAGETOP_HELP", "Arrastrar hacia arriba en la página." );
+define( "SRCH_TB_PAGE_BTN", "Ver" );
+
+define( "SRCH_COLS_OP", "Opciones" );
+define( "SRCH_COLS_TR", "Nombre de la Pista" );
+define( "SRCH_COLS_TR_HELP", "Click para ordenar esta página por Nombre de la Pista." );
+define( "SRCH_COLS_TI", "Tiempo" );
+define( "SRCH_COLS_TI_HELP", "Click para ordenar esta página pot Tiempo de Pistas." );
+define( "SRCH_COLS_AR", "Artista" );
+define( "SRCH_COLS_AR_HELP", "Click para ordenar esta página por Nombre de Artista." );
+define( "SRCH_PLAY_AR_HELP", "Empezar a tocar todas las pistas por este Artista." );
+define( "SRCH_FILTER_AR_HELP", "Click para Listar Todos Los Album(S) Pertenecientes a Este Artista." );
+define( "SRCH_COLS_AL", "Album" );
+define( "SRCH_COLS_AL_HELP", "Click para ordenar esta página por Nombre de Album." );
+define( "SRCH_PLAY_AL_HELP", "Empezar a tocar todas las pistas de este Album." );
+define( "SRCH_FILTER_AL_HELP", "Click Para Listar Todos Los Artistas Para Este Album." );
+define( "SRCH_COLS_TN", "#" );
+define( "SRCH_COLS_TN_HELP", "Click para ordenar esta página por Número de Pista." );
+define( "SRCH_COLS_GE", "Género" );
+define( "SRCH_COLS_GE_HELP", "Click para ordenar esta página por Nombre de Género." );
+define( "SRCH_PLAY_GE_HELP", "Empezar a tocar todas las pistas en este género." );
+
+
+##################################################
+
+?>
