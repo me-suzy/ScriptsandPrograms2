763ca6a02089f8d5a7ccf3b7c31dbc94ee46c5d4
me-suzy <me.suzana@gmail.com>
2025-08-13 04:49:30 +0300

    Add Unsorted/gcards/inc/lang/language_de.php

---
+++ Unsorted/gcards/inc/lang/language_de.php
@@ -0,0 +1,148 @@
+<?
+// index.php
+$index01 = 'Alle Kategorien';
+$index02 = 'Wähle eine der Karten aus!';
+
+// general navigation
+$nav01 = 'Zurück';
+$nav02 = 'Weiter';
+$nav03 = 'Home';
+$nav04 = 'Anzeigen';
+$nav05 = 'gCards Administrations Konsole';
+$nav06 = 'Passwort ändern';
+$nav07 = 'Logout';
+$nav08 = 'Log In';
+$nav09 = 'Powered by';
+$nav10 = 'Spalten pro Seite';
+$nav11 = 'Bitte gehe einen Schritt <a href="javascript:history.go(-1);">zurück</a>';
+$nav12 = 'Zurück zu';
+
+
+
+// UIfunctions.php
+$uifunc01 = 'User: ';
+
+// compose.php
+$compose01 = 'Keine Karte ausgewählt!  Gehe <a href="index.php">zurück</a> um eine Karte auszuwählen';
+$compose02 = 'Schreibe bitte den gewünschten Text, um deine eCard zu senden!';
+$compose03 = 'Name';
+$compose04 = 'eMail-Adresse';
+$compose05 = 'Von:';
+$compose06 = 'An:';
+$compose07 = 'Benachrichtige mich, wenn die Karte abgeholt wurde';
+$compose08 = 'Trenne mehrere eMail-Adressen mit einem Komma';
+
+// preview.php
+$preview01 = "Bitte fülle alle Felder aus! $nav11";
+$preview02 = 'ist keine gültige eMail-Adresse';
+$preview03 = "Ungültige eMail-Adressen! $nav11";
+$preview04 = 'Senden';
+
+// sendcard.php
+$sendcard01 = 'Fehler!  Formular wurde nicht korrekt ausgefüllt.  Bitte gehe <a href="javascript:history.go(-1);">zurück</a>';
+$sendcard02 = 'Fehler beim Einfügen von: ';
+$sendcard03 = 'eCard wurde gesendet...';
+$sendcard04 = 'eCard konnte nicht gesendet werden...';
+
+// getcard.php
+$getcard01 = 'Fehler - Keine gültige CardID';
+$getcard02 = '$to_email hat deine eCard abgeholt';
+$getcard03 = 'Sende $from_name eine eCard';
+
+// showcard.php
+$showcard01 = 'Eine eCard von';
+
+// news.php
+$news01 = 'Archivierte News';
+$news02 = 'Geschrieben';
+$news03 = 'von';
+$news04 = 'Newsmeldungen pro Seite';
+$news05 = 'News';
+$news06 = 'Betreff';
+$news07 = 'Text';
+$news08 = 'Datum';
+$news09 = 'Autor';
+
+
+
+// user related text
+$user01 = 'Username';
+$user02 = 'Passwort';
+$user03 = 'Du bist nicht eingeloggt - Du kannst dich nicht ausloggen!';
+$user04 = 'Du wurdest erfolgreich ausgeloggt...';
+$user05 = 'Passwort wiederholen';
+$user06 = 'eMail-Adresse';
+$user07 = 'Status';
+$user08 = 'User';
+$user09 = 'Passwörter haben nicht übereingestimmt - bitte nocheinmal eingeben';
+$user10 = '(Leer lassen, um den aktuellen Inhalt beizubehalten)';
+$user11 = 'Altest Passwort stimmte nicht mit deinem neuen überein';
+$user12 = 'Passwort erfolgreich geändert!';
+$user13 = 'Altes Passwort';
+$user14 = 'Neues Passwort';
+
+/*
+*********************
+** ADMIN DIRECTORY **
+*********************
+*/
+
+// Authentication
+$auth01 = 'Authentifikation für User fehlgeschlagen';
+$auth02 = 'Bitte gebe Benutzernamen und Passwort ein.';
+$auth03 = 'Du bist nicht eingeloggt.';
+$auth04 = 'Du hast nicht die entsprechenden Privilegien um dir diese Seite anzusehen!';
+
+// admin.php
+$admin01 = 'Administrative Optionen';
+$admin02 = 'Einstellungen';
+$admin03 = "Karte $admin02";
+$admin04 = "Kategorie $admin02";
+$admin05 = "News $admin02";
+$admin06 = "User $admin02";
+$admin07 = 'Ansehen, hinzufügen, aktualisieren, und löschen';
+$admin08 = 'ID';
+$admin09 = 'Aus Datenbank gelöscht...';
+$admin10 = 'Vom Server gelöscht...';
+$admin11 = 'nicht';
+$admin12 = 'aktualisiert';
+$admin13 = 'gelöscht';
+$admin14 = 'added';
+$admin15 = 'Füge eine neue hinzu';
+$admin16 = 'Hochladen';
+
+// actions
+$action01 = 'Aktion';
+$action02 = 'Hinzufügen';
+$action03 = 'Editieren';
+$action04 = 'Löschen';
+$action05 = 'Ansehen';
+$action06 = 'Aktualisieren';
+$action07 = 'Abbrechen';
+$action08 = 'Vorschau';
+
+// cards.php
+$cards01 = 'Bild';
+$cards02 = 'Thumbnail';
+$cards03 = 'Male gesenden';
+$cards04 = 'Karte';
+$cards05 = 'Karten-Name';
+$cards06 = 'Leer lassen, um ein automatisches Thumbnail zu erstellen';
+
+// categories
+$cat01 = 'Kategorie';
+$cat02 = 'Kategorie Name';
+$cat03 = 'Karten in der Kategorie';
+
+// upload.php
+$upload01 = 'Bild nicht hochgeladen!';
+$upload02 = "Datei Upload-Fehler bei Benutzung der 'move_uploaded_file' Funktion!";
+$upload03 = 'Bild erfolgreich geuploaded...';
+$upload04 = 'Fehler bim kopieren der Datei!';
+$upload05 = 'Thumbnail erfolgreich hochgeladen...';
+$upload06 = 'Bild, welches als Thumbnail verwendet, wird erfolgreich hochgeladen...';
+$upload07 = 'Thumbnail erfolgreich erstellt...';
+$upload08 = 'Bitte lade ein Thumbnail hoch, falls das Bild nicht das Format JPEG hat.';
+
+
+?>
