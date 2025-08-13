87f25b4e708dd68084e6dab63ba04c20602e6571
me-suzy <me.suzana@gmail.com>
2025-08-13 04:12:27 +0300

    Add Unsorted/evoBook/admin/config.php

---
+++ Unsorted/evoBook/admin/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$db['user'] = "root"; // mysql username
+$db['pass'] = "YOU DB PASS"; //mysql password
+$db['host'] = "localhost"; //mysql host/server
+$db['name'] = "evobook"; //mysql database name
+
+$conf['username'] = 'admin'; // username
+$conf['password'] = 'admin'; // password
+?>
