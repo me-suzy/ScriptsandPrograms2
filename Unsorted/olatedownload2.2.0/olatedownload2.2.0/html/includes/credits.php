94a8e3a846b565c1cdac15cbb3b4d587fce58d56
me-suzy <me.suzana@gmail.com>
2025-08-13 07:08:48 +0300

    Add Unsorted/olatedownload2.2.0/olatedownload2.2.0/html/includes/credits.php

---
+++ Unsorted/olatedownload2.2.0/olatedownload2.2.0/html/includes/credits.php
@@ -0,0 +1,17 @@
+<?php
+/***************************************************************************
+ *                      Olate Download v2 - Download Manager
+ *
+ *                           http://www.olate.com
+ *                            -------------------
+ *   author                : David Mytton
+ *   copyright             : (C) Olate 2003 
+ *
+ *   Support for Olate scripts is provided at the Olate website. Licensing
+ *   information is available in the license.htm file included in this
+ *   distribution and on the Olate website.                  
+ ***************************************************************************/
+?>
+<!--Begin Credit Line. Leave intact-->
+<div align="center"><span class="small"><a href="http://www.olate.com" target="_blank"><?= $language['credits_poweredby'].$config['version']; ?></a></span></div>
+<!--End Credit Line-->
