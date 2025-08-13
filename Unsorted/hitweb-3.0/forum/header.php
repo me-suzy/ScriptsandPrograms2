4beefd2d28d1d6b3c338460f1fcb5f843d69c348
me-suzy <me.suzana@gmail.com>
2025-08-13 05:09:25 +0300

    Add Unsorted/hitweb-3.0/forum/header.php

---
+++ Unsorted/hitweb-3.0/forum/header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML PUBLIC '-//W3C//DTD HTML 4.0//EN' 'http://www.w3.org/TR/REC-html40/strict.dtd'>
+
+<html>
+  <head>
+<!-- Information pour les ROBOTS -->
+    <meta name='revisit-after' content='7 days'>
+
+<!-- Information sur les PAGES HTML -->
+    <meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
+    <meta name='Content-language' content='fr'>
+
+<!-- Information sur l'AUTEUR -->
+    <meta name='Author-Corporate' content='Brian FRAVAL'>
+    <meta name='copyright' content='&copy; 2000 - Brian FRAVAL'>
+    <meta name='Author' lang='fr' content='Brian FRAVAL'>
+    <mate name='Reply-to' content='webmaster@hitweb.org'>
+
+<!-- Information sur le SITE -->
+    <meta name='base' content='http://www.hitweb.org/'>
+    <meta name='Identifier-URL' content='http://www.hitweb.org/'>
+    <meta name='Date-Creation-yyyymmdd' content='20000802'>
+    <meta name='Date-Revision-yyyymmdd' content='20000904'>
+
+<!-- Mots clés pour les ANGLAIS -->
+    <meta name='title' lang='en' content='The best of the world wide web'>
+    <meta name='Description' lang='en' content=''>
+    <meta name='keywords' lang='en' content='best, site, web, internet, hitweb, brian, fraval, decaen, duhaut, gpl, free'>
+    <meta name='Category' content='Internet'>
+
+<!-- Mots clés pour les FRANCAIS -->
+    <meta name='title' lang='fr' content='Le classement des meilleurs sites web'>
+    <meta name='Description' lang='fr' content='Classement des meilleurs sites internet par thèmes. Ce classement est dynamique par rapport au nombre de point de chaques sites référencés dans HITWEB'>
+    <meta name='keywords' lang='fr' content='annuaire, moteur de recherche, meilleur, site, web, internet, hitweb, brian, fraval, référencement, decaen, duhaut, classement, gpl, libre'>
+    <meta name='Category' content='Internet'>
+
+<!-- Feuille de style -->
+    <link rel='stylesheet' type='text/css' href='../css/style.css'>
+
+  <title>HITWEB : Le classement des meilleurs sites web.................................................................................</title>
+  </head>
+
+<!-- fin de meta.tpl -->
+
+
+<!-- début de appreciation.tpl -->
+
+<body background='../tpl/hitweb/img/fond.jpg' link="#000000" vlink="#000000" alink="#000000" topmargin="0" leftmargin="0" >
+
+<!-- TITRE DU SITE AVEC LOGO + BANNIERE (très cher la PLACE) -->
+
+<center>
+<table border='0' cellspacing='0' cellpadding='0' width='702'>
+  <tr>
+    <td valign='bottom' width='221'><a href="../index.php"><img src='../tpl/hitweb/img/hitweb.gif' border='0' width='221' height='50'></a></td>
+    <td background='../tpl/hitweb/img/fondtitre.gif' valign=bottom>&nbsp;</td>
+    <td valign='bottom' width='468' ><a href="http://www.chez.com/"><img src='../images/ban/ban1.gif' border='0' width='468' height='60'></a><br><img src='../tpl/hitweb/img/banbas.jpg'></td>
+    <td valign='bottom' align='right' width='13'><img src='../tpl/hitweb/img/cote.jpg' border='0' width='13' height='28'></td>
+  </tr>
+</table>
+<!-- MENU + PARTIES -->
+
+<!-- MENU + PARTIES -->
+<map name="menu">
+  <area href="../appreciation.php" shape="polygon" coords="0, 51, 81, 50, 90, 58, 81, 69, 0, 68">
+  <area href="../proposite.php" shape="polygon" coords="1, 91, 81, 91, 91, 100, 83, 109, 2, 109">
+  <area href="../lienavalider.php" shape="polygon" coords="1, 132, 82, 132, 92, 140, 82, 149, 2, 148">
+  <area href="../projet.php" shape="polygon" coords="1, 172, 83, 171, 90, 179, 82, 189, 0, 189">
+  <area href="../rechercher.php" shape="polygon" coords="0, 212, 82, 211, 90, 221, 81, 229, 2, 230">
+  <area href="../top50.php" shape="polygon" coords="0, 252, 81, 252, 90, 259, 84, 268, 1, 269">
+  <area href="index.php" shape="polygon" coords="0, 291, 82, 291, 92, 299, 82, 310, 0, 309">
+  <area href="../partenaria.php" shape="polygon" coords="0, 331, 82, 331, 92, 340, 80, 350, 1, 349">
+  <area href="../webalizer/" shape="polygon" coords="1, 370, 84, 370, 90, 379, 81, 390, 2, 390">
+</map>
+
+  <table border='0' cellspacing='0' cellpadding='0' width='705'>
+    <tr>
+      <td bgcolor='#E0C695' colspan='3' height='5'>&nbsp; <a href="../index.php">Hitweb /</a></td>
+    </tr>
+    <tr>
+	  <td background='../tpl/hitweb/img/fondmenu.gif' valign='top' width='139'><img src='../tpl/hitweb/img/menu1.gif' border='0' width='40' height='466'><img border="0" src="../tpl/hitweb/img/menu2.gif" usemap="#menu" width='99' height='466'></td>
+      <td bgcolor='#F7F0E4' valign='top' width='550'>
