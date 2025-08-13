f1cbe57d42b7dc2bcb440decdbad8c139bc559a1
me-suzy <me.suzana@gmail.com>
2025-08-13 07:34:11 +0300

    Add Unsorted/p4CMS.v1.05.Nullified/p4cms/editor/_i3/inc/config.php

---
+++ Unsorted/p4CMS.v1.05.Nullified/p4cms/editor/_i3/inc/config.php
@@ -0,0 +1,95 @@
+<?php
+$settings[version] = "";
+$settings[build] = 1;
+$settings[copyright] = "&copy;&nbsp;2003 RA";
+$settings[product_name] = "ÂÈÇÓÀËÜÍÛÉ HTML ÐÅÄÀÊÒÎÐ";
+$settings[language] = "english";
+$settings[security] = "0";
+$settings[password] = "password";
+
+$settings[bgcolor] = "#F0F0F0";
+$settings[bgcolor2] = "#F5F5F5";
+$settings[border_color] = "#000000";
+
+$settings[textcolor1] = "#222222";
+$settings[textcolor2] = "#999999";
+$settings[textcolor3] = "#CE0000";
+
+$option[1] = 1;                // 1. Local image selector  0 - 0ff / 1 - On
+$option[2] = 1;                // 2. Table functions  0 - Off / 1 - On
+$option[3] = 1;                // 3. File Functions  0 - Off / 1 - On
+$option[4] = 1;                // 4. Color Picker  0 - Off / 1 - On
+$option[5] = 1;                // 5. Font Settings  0 - Off / 1 - On
+$option[6] = 0;                // 6. Relative Paths 0 - Off / 1 - On
+$option[7] = "";        // 7. Cascade Style Sheet - filename with path
+
+$settings[disable_html_view] = 0;
+$settings[images_root] = "../../uploads/mediapool";
+/**************************************************************************
+Ðàçìåð èçîáðàæåíèÿ - Ñîîòíîøåíèå: 100000 = 100kB
+***************************************************************************/
+$settings[max_img_size] = "2500000";
+$settings[img_file_types][1] = "image/jpeg";
+$settings[img_file_types][2] = "image/pjpeg";
+$settings[img_file_types][3] = "image/gif";
+$settings[img_file_types][4] = "image/png";
+$settings[files_root] = "../../uploads/mediapool";
+/**************************************************************************
+Ðàçìåð - Ñîîòíîøåíèå: 100000 = 100kB
+***************************************************************************/
+$settings[max_file_size] = "2500000";
+
+$settings[mime_file_ext][1] = ".html";
+$settings[mime_file_ext][2] = ".htm";
+$settings[mime_file_ext][3] = ".asc";
+$settings[mime_file_ext][4] = ".txt";
+$settings[mime_file_ext][5] = ".jpeg";
+$settings[mime_file_ext][6] = ".asc";
+$settings[mime_file_ext][7] = ".jpeg";
+$settings[mime_file_ext][8] = ".jpg";
+$settings[mime_file_ext][9] = ".gif";
+$settings[mime_file_ext][10] = ".png";
+$settings[mime_file_ext][11] = ".js";
+$settings[mime_file_ext][12] = ".pdf";
+$settings[mime_file_ext][13] = ".ai";
+$settings[mime_file_ext][14] = ".eps";
+$settings[mime_file_ext][15] = ".ps";
+$settings[mime_file_ext][16] = ".doc";
+$settings[mime_file_ext][17] = ".hqx";
+$settings[mime_file_ext][18] = ".tar";
+$settings[mime_file_ext][19] = ".bin";
+$settings[mime_file_ext][20] = ".uu";
+$settings[mime_file_ext][21] = ".exe";
+$settings[mime_file_ext][22] = ".rtf";
+$settings[mime_file_ext][23] = ".rar";
+$settings[mime_file_ext][24] = ".zip";
+$settings[mime_file_ext][25] = ".wav";
+$settings[mime_file_ext][26] = ".au";
+$settings[mime_file_ext][27] = ".snd";
+$settings[mime_file_ext][28] = ".mpeg";
+$settings[mime_file_ext][29] = ".mpg";
+$settings[mime_file_ext][30] = ".mp3";
+$settings[mime_file_ext][31] = ".qt";
+$settings[mime_file_ext][32] = ".mov";
+$settings[mime_file_ext][33] = ".avi";
+
+$settings[font_size_type] = "num";
+
+$settings[font_size_num][1] = 1;
+$settings[font_size_num][2] = 2;
+$settings[font_size_num][3] = 3;
+$settings[font_size_num][4] = 4;
+$settings[font_size_num][5] = 5;
+$settings[font_size_num][6] = 6;
+$settings[font_size_num][7] = 7;
+$settings[font_size_pt][1] = 8;
+$settings[font_size_pt][2] = 9;
+$settings[font_size_pt][3] = 10;
+$settings[font_size_pt][4] = 11;
+$settings[font_size_pt][5] = 12;
+$settings[font_size_pt][6] = 13;
+$settings[font_size_pt][7] = 14;
+$settings[font_size_pt][8] = 15;
+$settings[font_size_pt][9] = 16;
+
+?>
