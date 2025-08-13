15dfcef41d6c509821ad5b775f9e4ffc0a02ca65
me-suzy <me.suzana@gmail.com>
2025-08-13 06:38:59 +0300

    Add Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/en/lng_admin.php

---
+++ Unsorted/NeedLock.v1.3.cnk.nulled.by.[GTT]/needlock-1.3.cnk.nulled.by.GTT/lang/en/lng_admin.php
@@ -0,0 +1,292 @@
+<?php
+
+$lang['yes']                        =   'Yes';
+$lang['no']                         =   'No';
+$lang['user']		                =	'user';
+$lang['login']		                =	'log in';
+$lang['username']	                =	'UserName';
+$lang['realname']	                =	'Real name';
+$lang['password']	                =	'PassWord';
+$lang['next']		                =	'>>';
+$lang['previous']	                =	'<<';
+$lang['page']		                =	'page';
+$lang['pages']		                =	'pages';
+$lang['of']		                    =	'of';
+$lang['add']	   	                =	'add';
+$lang['delete']		                =	'remove';
+$lang['edit']  		                =	'edit';
+$lang['suspend']  		            =	'suspend';
+$lang['ban']  		                =	'ban';
+$lang['save']		                =	'save';
+$lang['date']  		                =	'date';
+$lang['time']  		                =	'time';
+$lang['never']                      =   'Never';
+$lang['january']	                =	'january';
+$lang['february']	                =	'february';
+$lang['march']		                =	'march';
+$lang['april']		                =	'april';
+$lang['may']		                =	'may';
+$lang['june']		                =	'june';
+$lang['july']		                =	'july';
+$lang['augest']		                =	'augest';
+$lang['september']	                =	'september';
+$lang['october']   	                =	'october';
+$lang['november']	                =	'november';
+$lang['december']	                =	'december';
+$lang['monday']		                =	'monday';
+$lang['tuesday']	                =	'tuesday';
+$lang['wednesday']	                =	'wednesday';
+$lang['thursday']	                =	'thursday';
+$lang['friday']		                =	'friday';
+$lang['saturday']	                =	'saturday';
+$lang['sunday']		                =	'sunday';
+$lang['s_monday']                   =	'Mon';
+$lang['s_tuesday']                  =	'Tue';
+$lang['s_wednesday']                =	'Wed';
+$lang['s_thursday']	                =	'Thu';
+$lang['s_friday']                   =	'Fri';
+$lang['s_saturday']	                =	'Sat';
+$lang['s_sunday']                   =	'Sun';
+$lang['year']   	                =	'year';
+$lang['s_year']   	                =	'year';
+$lang['email']   	                =	'Email';
+$lang['phone']	  	                =	'Phone';
+$lang['s_phone']   	                =	'Tel.';
+$lang['fax']	   	                =	'Fax';
+$lang['search']                     =   'Search';
+$lang['of']		                    =	'of';
+$lang['in']		                    =	'in';
+$lang['search_go']                  =   'search!';
+$lang['search_no_results']			=	'No entries found';
+$lang['avdsearch']                  =       'Advanced search';
+$lang['sitemap']                    =       'Sitemap';
+$lang['support']                    =       'Support';
+$lang['feedback']                   =       'Feedback';
+$lang['redirect_link_text']         =       'If You don\'t want to wait, click here';
+$lang['redirect_page_subtitle']     =       'Redirect';
+$lang['error_page_text']            =       'An error has ocure';
+$lang['go_back']                    =       'go back';
+$lang['level']                      =       'level';
+$lang['contact_webmaster']          =       'webmaster';
+$lang['fields_not_filled']          =       'Not all required fields completed';
+$lang['session_expired']            =       'Sorry, Your session expired, proceed to log in ...';
+
+$lang['page_title']	                =      'Admin CP';
+$lang['words_admin_login_header']   =       'LogIn to Admin CP';
+$lang['login_success_text']         =       'You successfully loged in. Please wait while we transfer You...';
+$lang['logout_success_text']        =       'You successfully loged out. Please wait while we transfer You...';
+$lang['submit_login']               =       'log me in';
+$lang['logout']	                    =       'LogOut';
+$lang['welcome']                    =	'Welcome ';
+
+$lang['menu_system_information']    =       'information';
+$lang['menu_system_configuration']  =       'configuration';
+$lang['menu_anouncement_system']    =       'anouncements';
+$lang['menu_user_management']       =       'user&#39;s control';
+$lang['menu_administrators']        =       'administrators';
+$lang['menu_backup_system']         =       'backup system';
+$lang['menu_statistics_system']     =       'statistics';
+
+////////////
+
+$lang['part_system_info']               =       'System information';
+
+$lang['part_global_setup']              =       'System configuration';
+$lang['subpart_global_setup_status']    =       'system status';
+$lang['subpart_global_setup_global']    =       'global set up';
+$lang['subpart_global_setup_sql']       =       'database set up';
+$lang['subpart_global_setup_email']     =       'email set up';
+$lang['subpart_global_setup_users']     =       'members set up';
+$lang['subpart_global_setup_dirs']		=		'dirs protection';
+
+$lang['part_anouncements']              =       'Anouncements';
+$lang['subpart_anouncements_list']      =       'list anounces';
+$lang['subpart_anouncements_new']       =       'new anounce';
+
+$lang['part_user_control']              =       'Members control';
+$lang['subpart_user_control_search']    =       'search';
+$lang['subpart_user_control_prereg']    =       'pre-register';
+$lang['subpart_user_control_approve']   =       'approve/decline';
+$lang['subpart_user_control_unsuspend'] =       'unsuspend';
+$lang['subpart_user_control_unban']     =       'unban';
+
+$lang['part_admins_control']            =       'Admins control';
+$lang['subpart_admins_control_list']    =       'full list';
+$lang['subpart_admins_control_create']  =       'create admin';
+
+$lang['part_backups']                   =       'Backups';
+$lang['subpart_backups_create']         =       'create backup';
+$lang['subpart_backups_restore']        =       'restore from backup';
+
+$lang['part_statistic']                 =       'Statistic';
+
+$lang['part_admin_logs']                =       'Admins activity logs';
+
+////////////
+
+$lang['sysinfo_header']                 =       'System information summary';
+$lang['server_load']			=	'Server load';
+$lang['system_status']			=	'System status';
+$lang['system_status_online']		=	'<strong>Online</strong>';
+$lang['system_status_offline']		=	'<strong>Offline</strong>';
+$lang['system_status_unlnown']		=	'<strong>Undefined</strong>';
+$lang['signup_status']			=	'SignUp status';
+$lang['new_regs_allow']			=	'<strong>On</strong>';
+$lang['new_regs_disallow']		=	'<strong>Off</strong>';
+$lang['new_regs_unknown']		=	'<strong>Undefined</strong>';
+$lang['total_users']			=	'Total users';
+$lang['users_not_activated']		=	'Not activated users';
+$lang['users_not_approved']		=	'Not approved users';
+$lang['users_suspended']		=	'Suspended users';
+
+$lang['anounces_header']		=	'Anounces Set Up';
+$lang['anounces_subheader']             =       'Add / edit / delete anounces';
+$lang['no_anounces']			=	'No anounces';
+$lang['anounce_date']			=	'Date';
+$lang['anounce_header']			=	'Header';
+$lang['anounce_body']			=	'Body';
+$lang['edit_anounce']			=	'save';
+$lang['add_anounce']			=	'add anounce';
+$lang['delete_anounce']			=	'delete';
+$lang['add_anounce_link']		=	'add new anounce';
+$lang['incorrect_date_format']	=	'Incorrect DATE format. Must be in format yyyy-mm-dd only.';
+$lang['anounce_added']			=	'New anounce added successfully. Please wait while we redirect You ...';
+$lang['anounce_edited']			=	'Anounce edited successfully. Please wait while we redirect You ...';
+$lang['anounce_deleted']		=	'Anounce deleted successfully. Please wait while we redirect You ...';
+$lang['incorrect_anounce_id']	=	'No entry found for such anounce ID';
+$lang['spam_anounce']			=	'Send new anounce to members';
+
+$lang['save_config']		=   'save configuration';
+
+$lang['global_settings']        =   'Global Set Up';
+$lang['pathes']                 =   'Pathes and urls';
+$lang['cookies']                =   'Cookies';
+$lang['site_name']              =   '<strong>Site name</strong><br>global name of Your web-site';
+$lang['base_url']               =   '<strong>Base system url</strong><br>end with trailing slash';
+$lang['base_path']              =   '<strong>Base system path</strong><br>end with trailing slash';
+$lang['img_url']                =   '<strong>Images url</strong><br>end with trailing slash';
+$lang['img_path']               =   '<strong>Images path</strong><br>end with trailing slash';
+$lang['php_ext']                =   '<strong>PHP files extention</strong><br>commonly .php';
+$lang['hta_top_dir']            =   '<strong>Top HTA directory</strong><br>this directory will contain all directories protected by .htpasswd for system members<br>( currently only first level directory can be used )';
+$lang['cookie_domain']          =   '<strong>Cookies domain</strong><br>use .domain.com for global cookies';
+$lang['cookie_path']            =   '<strong>Cookies path</strong><br>relative from domain to root NS dir';
+$lang['cookie_id']              =   '<strong>Cookies unique ID</strong><br>to use only our cookies';
+
+
+$lang['email_settings']         =   'Set Up Email';
+$lang['email_set_up']           =   'Configure mailer';
+$lang['admin_email']            =   'Administrator\'s email address';
+$lang['email_in']               =   'Email for incoming messages';
+$lang['email_out']              =   'Email for outgoing messages';
+$lang['email_sender']           =   'System for sending mail';
+$lang['email_format']           =   'Outgoing emails format';
+$lang['email_charset']          =   'Outfoing emails charset';
+
+$lang['sql_settings']           =   'SQL Database Settings';
+$lang['database_set_up']        =   'Set up database';
+$lang['sql_driver']             =   '<strong>SQL database type</strong>';
+$lang['sql_host']               =   '<strong>SQL database host</strong>';
+$lang['sql_user']               =   '<strong>SQL database user</strong>';
+$lang['sql_pswd']               =   '<strong>SQL database password</strong>';
+$lang['sql_port']               =   '<strong>SQL database port</strong><br>optional, if non-standart';
+$lang['sql_name']               =   '<strong>SQL database name</strong>';
+$lang['sql_prefix']             =   '<strong>SQL tables prefix</strong><br>usefull, if You have only one database';
+
+$lang['IS_OFFLINE']             =   'General system status';
+
+$lang['users_settings']         =   'Members Settings';
+$lang['users_set_up']           =   'Set up members preferences';
+$lang['sess_expire']            =   '<strong>Session expiration time (seconds)</strong><br>member or admin session will be automatically closed if no activity during this time';
+$lang['is_regs_allowed']        =   '<strong>Is new registrations allowed?</strong><br>can be disabled during some changes or server overload';
+$lang['use_authcodes']          =   '<strong>Use AuthCodes?</strong><br>if Yes, user need to activate it\'s account itself';
+$lang['regs_notify']            =   '<strong>Notify about new registrations?</strong><br>admin will be notified via email about all new members';
+$lang['regs_approve']           =   '<strong>Approve accounts</strong><br>you can approve or decline any new registration';
+$lang['default_hta_used']        =   '<strong>Use default .htpasswd user</strong><br>default user will be added automaticaly, in all protected dirs';
+$lang['default_hta_user']       =   '<strong>Default .htpasswd username</strong>';
+$lang['default_hta_pass']       =   '<strong>Default .htpasswd password</strong>';
+$lang['logins_decline']         =   '<strong>Logins which will be decline as new username</strong><br>exp.: admin, administrator, etc.<br><br><span style="color:red; font-weight: bold;">FORMAT: login|login1|login2....<br>No first pipe, no last pipe, no breaks!</span>';
+
+
+
+$lang['status_settings']        =   'Global System Status';
+$lang['sys_status']             =   '<strong>System status</strong>';
+$lang['sys_status_online']      =   'Online';
+$lang['sys_status_offline']     =   'Offline';
+$lang['sys_status_off_msg']     =   '<strong>Offline message</strong><br>displays for users while system offline';
+
+$lang['users_management']	    =   'Members Management';
+
+$lang['control_member_h']		=	'Control members profiles';
+
+$lang['prereg_member_h']		=	'Pre-register a member';
+$lang['interface_lang']         =   'Interface language';
+$lang['prereg_member']		    =	'register a member';
+$lang['member_registered']		=	'New member registered successfully. Account activated successfully.<br>Member login details sent to member via email.';
+
+$lang['approve_member_h']		=	'Approve members accounts';
+$lang['approve']				=	'approve';
+$lang['decline']				=	'decline';
+$lang['member_approved']		=	'Member\'s registration successfully approved.<br>Notification sent via email.';
+$lang['member_declined']		=	'Member\'s registration successfully declined.<br>Member\'s account deleted from database.';
+
+$lang['suspended_member_h']		=	'Unsuspend members accounts';
+$lang['unsuspend']				=	'unsuspend';
+$lang['member_suspended']		=	'Member\'s account successfully suspended.<br>Notification sent via email.';
+$lang['member_unsuspended']		=	'Member\'s account successfully unsuspended.<br>Notification sent via email.';
+$lang['member_deleted']                 =       'Member\'s account successfully deleted.<br>Notification sent via email.';
+$lang['member_baned']                   =       'Member\'s account successfully baned.';
+$lang['control_member_h']               =       'Members control';
+$lang['baned_member_h']                 =       'Unbaning members';
+$lang['unban']                          =       'unban';
+$lang['member_undaned']                 =       'Member\'s account successfully unbaned.';
+
+$lang['edit_member_h']                  =       'Edit member profile.';
+$lang['id']                             =       'Member ID';
+$lang['regdate']                        =       'Registration date';
+$lang['expire']                         =       'Expiration date';
+$lang['interface_lang']                 =       'Interface language';
+$lang['last_login']                     =       'Last logined';
+$lang['access_dirs']                    =       'Accessible dirs';
+$lang['member_edited']                  =       'Member\'s account successfully updated.';
+
+$lang['admins_management']              =       'Administrators management';
+$lang['admin_reg_h']                    =       'Register new administrator';
+$lang['admin_edit_h']                   =       'Edit administrator&#39;s profile';
+$lang['adm_list_h']                     =       'Administrators list';
+$lang['admin_level']                    =       'Admin access level';
+$lang['level']                          =       'Level';
+$lang['admin_rights']                   =       'Admin access rights';
+$lang['admins_levels_info_h']           =       'Administrators access levels info';
+$lang['create_admin']                   =       'create administrator';
+$lang['update_admin']                   =       'update administrator';
+$lang['admin_created']                  =       'New administrator&#39;s account created successfully.<br>All needed information sent via email.';
+$lang['admin_updated']                  =       'Administrator&#39;s account updated successfully.<br>All needed information sent via email.';
+$lang['admin_removed']                  =       'Administrator&#39;s account removed successfully.<br>Notification sent via email.';
+
+$lang['logging_system']                 =       'Logging system';
+$lang['admin_logs_h']                   =       'Administrators&#39; actions log ( 30 last records )';
+
+$lang['stat_system']                    =       'Statistic system';
+$lang['stat_sys_h']                     =       'Members activity statistic';
+$lang['sort']                           =       'sort';
+$lang['order_by']                       =       'Order by';
+$lang['sort_key']                       =       'sort';
+$lang['descending']                     =       'descending';
+$lang['ascending']                      =       'ascending';
+$lang['count_visits']                   =       'visits';
+$lang['used_lang']                      =       'language';
+$lang['ip_address']                     =       'ip address';
+$lang['user_agent']                     =       'user agent';
+$lang['sort_now']                       =       'sort now';
+
+$lang['protection']						=		'Directories Protection Set Up';
+$lang['directory_protect_unprotect']    =	    'Protect / Unprotect directories';
+$lang['protected_dirs']					=		'Protected dirs';
+$lang['notprotected_dirs']				=		'Not protected dirs';
+$lang['protect']						=		'protect directory';
+$lang['unprotect']						=		'unprotect directory';
+$lang['no_protected_dirs']				=		'no protected dirs';
+$lang['no_unprotected_dirs']			=		'no unprotected dirs';
+
+
+?>
