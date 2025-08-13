77b9fc6a9f728b62a3c70c8c7229884c47b7dd80
me-suzy <me.suzana@gmail.com>
2025-08-13 04:08:35 +0300

    Add Unsorted/evoArticles/admin/lib/db_tables.php

---
+++ Unsorted/evoArticles/admin/lib/db_tables.php
@@ -0,0 +1,23 @@
+<?php
+// +------------------------------------------------------------------+
+// | evoArticles 
+// | Copyright (c) 2003-2004 Evo-Dev
+// +------------------------------------------------------------------+
+
+$database['article_article'] = $site['prefix']."article_art";
+$database['article_field'] = $site['prefix']."article_artfield";
+$database['article_cat'] = $site['prefix']."article_cat";
+$database['article_catfield'] = $site['prefix']."article_catfield";
+$database['article_comment'] = $site['prefix']."article_comment";
+$database['article_image'] = $site['prefix']."article_image";
+$database['article_support'] = $site['prefix']."article_support";
+$database['article_styles'] = $site['prefix']."article_styles";
+$database['article_access'] = $site['prefix']."article_access";
+
+$database['article_usergroup'] = $site['prefix']."usergroup";
+$database['article_settings'] = $site['prefix']."settings";
+$database['article_sgroup'] = $site['prefix']."settings_group";
+$database['article_user'] = $site['prefix']."user";
+$database['article_userfield'] = $site['prefix']."userfield";
+
+?>
