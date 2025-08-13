35fec7b14a42d7690623372110b106ae2d2ffaeb
me-suzy <me.suzana@gmail.com>
2025-08-13 04:23:54 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/spanish/lang.login.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/spanish/lang.login.php
@@ -0,0 +1,31 @@
+<?php
+$L = array(
+
+
+"remember_me" =>
+"¿Conexión automática en visitas futuras?",
+
+"no_username" =>
+"El nombre que indicas no consta en la base de datos",
+
+"no_password" =>
+"Contraseña incorrecta.",
+
+"no_email" =>
+"Debes de enviar tu dirección de email",
+
+"multi_login_warning" =>
+"Ya hay alguien conectado usando esta cuenta.",
+
+"return_to_login" =>
+"Volver a conexión",
+
+"password_lockout_in_effect" =>
+"Solo lo puedes intentar cuatro veces en %x minuto(s)",
+
+"unauthorized_request" =>
+"No estás autorizado a realizar ésta acción",
+
+''=>''
+);
+?>
