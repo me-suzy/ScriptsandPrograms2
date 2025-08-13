a6d4ad4dd7d29fa6ae2b37da2536199921a43aac
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:05 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-prefs.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-prefs.php
@@ -0,0 +1,46 @@
+<?php
+
+##################################################
+
+# /prefs.php
+
+##################################################
+
+define( "PREFS_DENIED_1", "Ein unerwarteter Fehler ist aufgetreten. Bitte versuchen Sie es erneut." );
+
+define( "PREFS_CHECKFORM_1", "Bitte wählen Sie eine Hintergrundfarbe." );
+define( "PREFS_CHECKFORM_2", "Bitte wählen Sie eine Schrift." );
+define( "PREFS_CHECKFORM_3", "Bitte wählen Sie eine Schriftgröße." );
+define( "PREFS_CHECKFORM_4", "Bitte wählen Sie eine Schriftfarbe." );
+define( "PREFS_CHECKFORM_5", "Bitte wählen Sie eine Linkfarbe." );
+define( "PREFS_CHECKFORM_6", "Bitte wählen Sie eine Farbe für aktive Links." );
+define( "PREFS_CHECKFORM_7", "Bitte wählen Sie eine Farbe für besuchte Links." );
+define( "PREFS_CHECKFORM_8", "Bitte wählen Sie eine Randfarbe." );
+define( "PREFS_CHECKFORM_9", "Bitte wählen Sie eine Kopffarbe." );
+define( "PREFS_CHECKFORM_10", "Bitte wählen Sie eine Kopfschriftfarbe." );
+define( "PREFS_CHECKFORM_11", "Bitte wählen Sie eine Farbe für den Inhalt." );
+
+define( "PREFS_HEADER", "BENUTZERKONTO INFORMATION" );
+define( "PREFS_CAPTION", "Benutzen Sie diese Maske um Ihre Benutzerinformationen zu bearbeiten." );
+define( "PREFS_PALETTE", "Benutzen Sie diese Farbpalette um ihre Umgebung anzupassen." );
+
+define( "PREFS_FORMS_INVICN", "Invertiere Icons" );
+define( "PREFS_FORMS_INVICN_HELP", "Ermöglicht den Benutzern die Icons zu invertieren: Play, Get Info, Filter..." );
+define( "PREFS_FORMS_INVICN_ENABLED", "Aktiviert" );
+define( "PREFS_FORMS_FONTFACE", "Schrifttyp" );
+define( "PREFS_FORMS_FONTSIZE", "Schriftgröße" );
+define( "PREFS_FORMS_BGCOLOR", "Hintergrundfarbe" );
+define( "PREFS_FORMS_TEXT", "Textfarbe" );
+define( "PREFS_FORMS_LINK", "Linkfarbe" );
+define( "PREFS_FORMS_ALINK", "Farbe aktiver Links" );
+define( "PREFS_FORMS_VLINK", "Farbe besuchter Links" );
+define( "PREFS_FORMS_BORDER", "Randfarbe" );
+define( "PREFS_FORMS_HEADER", "Kopffarbe" );
+define( "PREFS_FORMS_HEADERFC", "Kopfschriftfarbe" );
+define( "PREFS_FORMS_CONTENT", "Inhaltsfarbe" );
+define( "PREFS_FORMS_BTN_SAVE", "Speichern" );
+define( "PREFS_FORMS_BTN_RESET", "Zurücksetzen" );
+
+##################################################
+
+?>
