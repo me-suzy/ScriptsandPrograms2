232eb3a64aad5d76c1f51da587ee6b7b67174ea4
me-suzy <me.suzana@gmail.com>
2025-08-13 04:37:23 +0300

    Add Unsorted/fcart/fcart/adm/top.php

---
+++ Unsorted/fcart/fcart/adm/top.php
@@ -0,0 +1,13 @@
+<center>
+<table width="749" border="0">
+<tr>
+<td width="20%" rowspan="4">
+  <table width="100%" border="0" cellpadding="0" cellspacing="0" height="100%">
+  <tr>
+  <td align="center"><img src="../images/selleasy.gif" width="97" height="16"></td>
+  </tr>
+  <tr>
+  <td align="center"><a href="http://www.fcart.com"><img src="../images/fcart.gif" width="100" height="66" border="0"></a></td>
+  </tr>
+  </table>
+</td>
