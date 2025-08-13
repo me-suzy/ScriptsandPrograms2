dbb1444df79abf96a8d8ae4da0ad7f5accd30a71
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:57 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/lobby.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/lobby.php
@@ -0,0 +1,37 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[LOBBY_Entered_Room] = "×ÏÛÅÌ × ËÏÍÎÁÔÕ.";
+
+$LOCALE[LOBBY_Room_Full] = "úÁÐÒÁÛÉ×ÁÅÍÁÑ ËÏÍÎÁÔÁ ÐÏÌÎÁ.";
+
+$LOCALE[LOBBY_Room_Title] = "÷Ù × ÐÒÉÅÍÎÏÊ";
+
+$LOCALE[LOBBY_Visitors] = "ÐÏÌØÚÏ×ÁÔÅÌÉ";
+
+$LOCALE[LOBBY_Public_List] = "óÐÉÓÏË ÏÂÝÉÈ ËÏÍÎÁÔ";
+
+$LOCALE[LOBBY_Private_List] = "óÐÉÓÏË ÞÁÓÔÎÙÈ ËÏÍÎÁÔ";
+
+$LOCALE[LOBBY_Create_Public] = "óÏÚÄÁÔØ ÎÏ×ÕÀ ÏÂÝÕÀ ËÏÍÎÁÔÕ";
+
+$LOCALE[LOBBY_Create_Private] = "óÏÚÄÁÔØ ÎÏ×ÕÀ ÞÁÓÔÎÕÀ ËÏÍÎÁÔÕ";
+
+$LOCALE[LOBBY_Button] = "÷ÏÊÄÉÔÅ ÉÌÉ ÓÏÚÄÁÊÔÅ ËÏÍÎÁÔÕ";
+
+$LOCALE[LOBBY_Room_Password] = "ðÁÒÏÌØ ÄÌÑ ËÏÍÎÁÔÙ";
+
+$LOCALE[LOBBY_Hidden] = "óËÒÙÔÙÊ";
+
+$LOCALE[LOBBY_Password_Incorrect] = "ðÁÒÏÌØ ÄÌÑ ÞÁÓÔÎÏÊ ËÏÍÎÁÔÙ ÎÅ×ÅÒÅÎ.";
+
+?>
