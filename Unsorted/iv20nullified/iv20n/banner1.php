03dae87ffda8195862d91add4e957c38de709b2f
me-suzy <me.suzana@gmail.com>
2025-08-13 05:49:20 +0300

    Add Unsorted/iv20nullified/iv20n/banner1.php

---
+++ Unsorted/iv20nullified/iv20n/banner1.php
@@ -0,0 +1,41 @@
+<?php 
+//////////////////////////////////////////////////////////////////////////////
+// Program Name         : Image Vote - Photo Rating System                  //
+// Release Version      : 2.0.0                                             //
+// Program Author       : Ronald James                                      //
+// Supplied by          : Scoons [WTN]                                      //
+// Nullified by         : CyKuH [WTN]                                       //
+//////////////////////////////////////////////////////////////////////////////
+// COPYRIGHT NOTICE                                                         //
+// (c) 2002 Ronald James    All Rights Reserved.                            //
+// Distributed under the licencing agreement located in wtn_release.nfo     //
+//////////////////////////////////////////////////////////////////////////////
+
+srand((double)microtime()*1000000); 
+switch (rand (1,5)) {
+
+	case 1: ?>
+
+<!--CyKuH [WTN]-->
+
+	<? break;
+	case 2: ?>
+
+<!--CyKuH [WTN]-->
+
+	<? break;
+	case 3: ?>
+
+<!--CyKuH [WTN]-->
+
+	<? break;
+	case 4: ?>
+
+<!--CyKuH [WTN]-->
+
+	<? break;
+	case 5: ?>
+
+<!--CyKuH [WTN]-->
+	<? break; } ?>
+
