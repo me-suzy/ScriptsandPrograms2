316c40ba8adb26aebfd83c4455e1214b244a70f1
me-suzy <me.suzana@gmail.com>
2025-08-13 06:56:37 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-login.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/fr/inc-login.php
@@ -0,0 +1,20 @@
+<?php
+
+##################################################
+
+# /login.php
+
+##################################################
+
+define( "LOGIN_CHECKFORM_1", "Veuillez soumettre votre adresse de courrier électronique.." );
+define( "LOGIN_CHECKFORM_2", "Veuillez soumettre une adresse de courrier électronique valide." );
+define( "LOGIN_CHECKFORM_3", "Veuillez choisir un mot de passe." );
+define( "LOGIN_HEADER", "COMPTE D'USAGER" );
+define( "LOGIN_EMAIL", "Adresse électronique" );
+define( "LOGIN_PASSWD", "Mot De Passe" );
+define( "LOGIN_BTN_1", "Entrer" );
+define( "LOGIN_BTN_2", "Inscription" );
+
+##################################################
+
+?>
