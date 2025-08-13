97a8e8c56a912a6ac4cebf899872d83296c541ee
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:11 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/help.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/pg/help.php
@@ -0,0 +1,205 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[HELP_Title] = "Ajude em";
+
+$LOCALE[HELP_Unavailable] = "Uma descrição útil de isto inquire está indisponível.";
+
+$LOCALE[HELP_Username] = "Um nome ou username é a pessoa que você deseja ser sabida como enquanto na conversa. Semelhante a um apelido, isto pode ser seu nome real ou um composto um.";
+
+$LOCALE[HELP_Language] = "MWChat tem a capacidade de exibir várias linguagens diferentes. Esta caixa permite-o escolher linguagem quaisquea que você prefere usar.";
+
+$LOCALE[HELP_Password] = "O administrador, Operador, e contas registradas requerem uma senha a login. Isto preveine acesso de unauthorized a seua conta.";
+
+$LOCALE[HELP_Lobby_Public] = "A lista pública de lugar é uma seleção de várias salas você pode conversar em. Escolha um lugar aquele intrests você a maioria de ou cria o próprio.";
+
+$LOCALE[HELP_Lobby_Private] = "A lista privada de lugar é escondida a operadores normais. Se deseja conversar num lugar privado, entra o nome do lugar requerido embaixo. Salas privadas nao são exibidas em qualquer lista de lugar, então pode segurar conversas privadas sem os operadores de uninvited entrando em você.";
+
+$LOCALE[HELP_Lobby_Public_Create] = "Se você nao pode achar um lugar que o interessa na lista acima, pode criar o próprio lugar por bater o nome do lugar requerido no campo embaixo.";
+
+$LOCALE[HELP_Lobby_Private_Create] = "Entre o nome do lugar privado que você deseja entrar. Se o lugar nao existe, será criado para você.";
+
+$LOCALE[HELP_Private_Message] = "Escolha um operador alistado nesta gota para caixa baixo. Qualquer mensagens que você emvia irá só ao operador de specifed. Trocar costas ao lugar inteiro, escolhe os traços de omissão. Messaging privado também pode ser feito usar o /comando de msg.";
+
+$LOCALE[HELP_Private_Message_Refresh] = "O estalido este elo refrescar-se a lista privada de operador de mensagem.";
+
+$LOCALE[HELP_Colors] = "Se gostaria de mudar a cor do texto que você entra, estalido em qualquer cor na barra de cor. Verá uma mensagem que dizendo que seuas cores foram atualizadas. Pode estalido em qualquer lugar na metade mais baixa da barra de cor restaurar a cor de omissão.";
+
+$LOCALE[HELP_Enter_Password] = "Entre uma nova senha você gostaria de usar. Esta senha pode ser qualquer texto de numeric de alfa. Assegure-se-o escrever esta senha para baixo e mantem num lugar seguro. É requerido que a senha é cinco ou mais caráteres em comprimento.";
+
+$LOCALE[HELP_Icons] = "Escolha um icon da lista embaixo representaá-lo naquela conversa. Todos operadores podem ver seu icon.";
+
+$LOCALE[HELP_Theme] = "Escolha um tema da lista embaixo. Um tema mudará todas seuas cores de conversa. Pode necessitar retornar ao vestíbulo, e então costas no lugar para todo tema muda tomar efeito.";
+
+$LOCALE[HELP_Confirm_Password] = "Entre a nova senha que você escolheu novamente aqui. Isto confirma que você nao fez um typo e o assegura saber que o que seua senha é.";
+
+$LOCALE[HELP_YOB] = "Isto é o ano de nascimento. Esta informação é usada a idade de caculate em algum caso, como algumas leis pode requerer operadores ser de um certo novamente.";
+
+$LOCALE[HELP_Occupation] = "Isto é a ocupação de operadores ou trabalho. Normalmente seua fonte principal de renda.";
+
+$LOCALE[HELP_Hobbies] = "Isto são coisas operador goza fazer. A maioria de do tempo gastado em passatempos é tempo normalmente sobressalente.";
+
+$LOCALE[HELP_Location] = "Isto é a localidade geográfica do operador.";
+
+$LOCALE[HELP_Comments] = "Isto é qualquer comenta ou opiniões o operador pode ter ou pode desejar a voz.";
+
+$LOCALE[HELP_Status] = "Isto é os operadores estado atual. Normalmente declarar se um operador é longe, online, registrados, administradores, ou operadores.";
+
+$LOCALE[HELP_Upload_File] = "Isto é o arquivo que você quer emviar a um operador. O estalido o olha botão escolher o arquivo que você deseja emviar.";
+
+$LOCALE[HELP_Upload_User] = "Isto é o operador que você quer emviar um arquivo a.";
+
+$LOCALE[HELP_Enter_Message] = "Quando entra um lugar, esta mensagem será emviada junto com o aviso que você entrou o lugar.";
+
+$LOCALE[HELP_Exit_Message] = "Quando você saída um lugar, esta mensagem serão emviados junto com o aviso que você tem à esquerda o lugar.";
+
+$LOCALE[HELP_Away_Message] = "Quando vai embora, esta mensagem será emviada junto com o aviso que seu longe.";
+
+$LOCALE[HELP_Back_Message] = "Quando as seuas costas de ser longe, esta mensagem será emviada junto com o aviso que as seuas costas.";
+
+$LOCALE[HELP_Default_Color] = "Se quer uma cor de omissão ser usado cada tempo você entra um lugar, esta opção o permitirá poô-lo.";
+
+$LOCALE[HELP_Smiles] = "Este toggles de opção seua capacidade de ver rostos gráficos de smiley de texto na conversa.";
+
+$LOCALE[HELP_Locate] = "Este toggles de opção seua capacidade de permitir ou operadores de disallow localizaá-lo. Localizar um operador conta o que lugar que você estão atualmente em.";
+
+$LOCALE[HELP_Upload] = "Este toggles de opção seua capacidade de receber arquivos de outros operadores.";
+
+$LOCALE[HELP_Room] = "Isto é o lugar que você estão atualmente em, ou o lugar que você deseja criar ou entrar.";
+
+$LOCALE[HELP_Room_Password] = "Algumas salas privadas são senha protegido. Se o lugar que você deseja entrar está senha protegido, entra-o aqui.";
+
+$LOCALE[HELP_Number_Users] = "Isto é o número total de operadores que estão atualmente no lugar.";
+
+$LOCALE[HELP_Topic] = "Isto é o tema atual do lugar.";
+
+$LOCALE[HELP_Buddylist] = "Isto é seua lista de companheiro. Isto contem o usernames de todos operadores neste lugar, e qualquer companheiros que você pode ter. Pode estalido em cada nome receber informação sobre aquele operador se são registrados.";
+
+$LOCALE[HELP_Serial_Number] = "Isto é o número de série de MWChat. Pode obter um número de série em http: //www.appindex.net/mwchat";
+
+$LOCALE[HELP_Storage] = "Isto é seua informação de armazenagem de arquivo. O primeiro número mostra espaço quanto você tem disponível para uso. O segundo número mostra o que seu total permitiu armazenagem é. Embaixo está uma lista de qualquer arquiva-o ter. O estalido num filename a download que arquiva, ou estalido o verificar caixa remover o arquivo. Você também pode ver o tamanho de cada arquivo, quando recebeu o arquivo, e quem lhe emviou o arquivo.";
+
+$LOCALE[HELP_ERROR_License] = "A licença que você tem é incorreta, ou não faz saída. O Retry seua licença e se ainda nao trabalha recebem um novo número de série nem apoio de Appindex Rede de contato.";
+
+$LOCALE[HELP_ERROR_AlreadyIn] = "Alguém é já logged em com aquele username. É possível se você não fez logout corretamente, que seua conta não tem idled para fora. Espere dez minutos e tente novamente.";
+
+$LOCALE[HELP_ERROR_Logout] = "Seua conta foi logged para fora. Pode login novamente se você como.";
+
+$LOCALE[HELP_ERROR_Expired] = "Se esteve desocupado isto pode ser por quê você foram expirados. Você também pode ter sido chutado para fora por um administrador ou operador.";
+
+$LOCALE[HELP_ERROR_RoomLong] = "O lugar nomeia-o escolheu é muito longo. O caráter máximo limita-o 15 caráteres.";
+
+$LOCALE[HELP_Live] = "O sistema de conversa é desligada. Isto pode ser porque administrador faz upgrades.";
+
+$LOCALE[HELP_ERROR_Profanity] = "Foi logged para fora para usar profanidade na conversa. A profanidade nao é permitida.";
+
+$LOCALE[HELP_ERROR_NoData] = "Você corretamente nao encheu em todos campos. Por favor tente novamente.";
+
+$LOCALE[HELP_ERROR_BadData] = "A informação você entrou caráteres maus contidos. Por uso de favor só caráteres de numeric de alfa.";
+
+$LOCALE[HELP_ERROR_Serial_Invalid] = "A série numera seu tem equipado nao combina seu servidor. Necessitará receber um novo número de série e verifica-se seu endereço de IP do webserver.";
+
+$LOCALE[HELP_ERROR_Serial_None] = "A Appindex rede nao tem um número de série em registro para você. Por favor registre seu produto e receba um novo número de série.";
+
+$LOCALE[HELP_ERROR_Critical] = "Um erro crítico desconhecido ocorreu. Se o problema persiste, apoio de Appindex Rede de contato.";
+
+$LOCALE[HELP_ERROR_Javascript] = "MWChat requer um browser de teia capaz de exibir javascript. Por favor capacite javascript em seu webbrowser ou upgrade.";
+
+$LOCALE[HELP_ERROR_Browser] = "O browser de teia você usam em nao compatível com MWChat. Recomendamos que usando Netscape ou Explorador de Internet.";
+
+$LOCALE[HELP_ERROR_Restricted] = "O username que você escolheu é um nome restringido ou reservado. Por favor escolha outro nome e login novamente.";
+
+$LOCALE[HELP_ERROR_BadPass] = "A senha que você forneceu é incorreta. Verifique com um administrador para mais detalhes.";
+
+$LOCALE[HELP_ERROR_IP] = "MWChat captou que seu endereço de IP mudou. Para razões de segurança, nós logged-o para fora. Por login de favor novamente.";
+
+$LOCALE[HELP_COMMAND_addbuddy] = "Adicione um companheiro a seua lista de companheiro. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_admin] = "Permite administradores sem operadores de registro ou toras de vista. Especifique a segunda opção (username) ver toras para um operador de specfic ou a sem registro aquele operador.";
+
+$LOCALE[HELP_COMMAND_afk] = "Mude seu modo a longe de teclado. Pode especificar um opcional longe mensagem.";
+
+$LOCALE[HELP_COMMAND_block] = "Bloqueie todas mensagens privadas de qualquer operadores.";
+
+$LOCALE[HELP_COMMAND_bug] = "Emvie um relatório de inseto.";
+
+$LOCALE[HELP_COMMAND_about] = "Veja informação sobre MWChat e os criadores.";
+
+$LOCALE[HELP_COMMAND_clear] = "Aclara a janela atual de conversa.";
+
+$LOCALE[HELP_COMMAND_color] = "Ponha a cor de texto a uma cor de seua escolha. Esta cor deve estar em #000000 formato.";
+
+$LOCALE[HELP_COMMAND_deop] = "Remova um estado de operadores de operadores. Este comando só pode ser usado por administradores e operadores.";
+
+$LOCALE[HELP_COMMAND_global] = "Emvie uma mensagem a todos operadores em todas salas. Este comando só pode ser usado por um administrador.";
+
+$LOCALE[HELP_COMMAND_help] = "Exiba este documento.";
+
+$LOCALE[HELP_COMMAND_ignore] = "Adicione um operador a seu ignora lista. Aquele operador nao pode emviar lhe mensagens privadas e você nao pode ver mensagens que eles emviam. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_unignore] = "Remova um operador de seu ignora lista. Aquele operador agora pode emviar lhe mensagens privadas e você pode ver mensagens que eles emviam. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_kick] = "Chute um operador do sistema. Pode especificar uma razão opcional. Este comando só pode ser usado por administradores ou operadores.";
+
+$LOCALE[HELP_COMMAND_lobby] = "Retorne-o ao vestíbulo. Pode especificar uma mensagem opcional de saída.";
+
+$LOCALE[HELP_COMMAND_locate] = "Localiza um operador especificado. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_logout] = "As toras um operador para fora do sistema. Pode especificar uma mensagem opcional de saída.";
+
+$LOCALE[HELP_COMMAND_me] = "Descreva o que você prescinde de refere a si diretamente.";
+
+$LOCALE[HELP_COMMAND_motd] = "Exiba a mensagem do dia.";
+
+$LOCALE[HELP_COMMAND_myfiles] = "Exiba seus arquivos online.";
+
+$LOCALE[HELP_COMMAND_op] = "Dê um estado de operador de operador. Este comando só pode ser usado por administradores e operadores.";
+
+$LOCALE[HELP_COMMAND_prefs] = "Exiba seuas preferências de MWChat.";
+
+$LOCALE[HELP_COMMAND_print] = "Exiba seuas conversas numa janela de printable.";
+
+$LOCALE[HELP_COMMAND_msg] = "Emvie uma mensagem privada a um operador. O Noone mais pode ver esta mensagem.";
+
+$LOCALE[HELP_COMMAND_register] = "Registre o username atual seu é usar. Registrar permite operadores compartilhar arquivos e cria listas de companheiro.";
+
+$LOCALE[HELP_COMMAND_removebuddy] = "Remove um companheiro de seua lista de companheiro. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_roll] = "Role o dado de lugar de conversa. Isto é útil para gaming online.";
+
+$LOCALE[HELP_COMMAND_showbuddy] = "Exiba todos operadores em seua lista de companheiro. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_showignored] = "Exiba todos operadores em seu ignora lista. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_smiley] = "Exiba todo smiley disponível encara.";
+
+$LOCALE[HELP_COMMAND_topic] = "Mude o tema de lugar. Este comando só pode ser usado por administradores e operadores.";
+
+$LOCALE[HELP_COMMAND_unafk] = "Mude seu modo a costas de longe de teclado. Pode especificar uma mensagem opcional de costas.";
+
+$LOCALE[HELP_COMMAND_unblock] = "Permite-o aceitar mensagens privadas de todos operadores novamente.";
+
+$LOCALE[HELP_COMMAND_topic] = "Remove um operador de seu ignora lista. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_unregister] = "Sem registro seu username. Isto fazrá ele disponível para qualquer um usar. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_upload] = "Exiba o arquivo compartilhar interface. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_url] = "Permite-o inserir um URL na conversa.";
+
+$LOCALE[HELP_COMMAND_whois] = "A informação de exposições sobre o operador especificado. Este comando só pode ser usado por operadores registrados.";
+
+$LOCALE[HELP_COMMAND_Title] = "Comandos disponíveis";
+
+$LOCALE[HELP_COMMAND_Type] = "{} indica uma opÇão necessária, [] indica uma opção opcional.";
+
+?>
