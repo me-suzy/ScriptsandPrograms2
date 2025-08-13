957f948260f6a9fd71ae407d4a270c07f1520084
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:33 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.search.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.search.php
@@ -0,0 +1,156 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"search_module_name" =>
+"Search",
+
+"search_module_description" =>
+"Search module",
+
+//----------------------------------------
+
+
+"search_total_results" =>
+"Total Search Results:",
+
+"search_weblog" =>
+"Weblog",
+
+"search_excerpt" =>
+"Excerpt",
+
+"search_author" =>
+"Author",
+
+"search_any_weblog" =>
+"Any Weblog",
+
+"search_total_comments" =>
+"Total Comments",
+
+"search_recent_comment_date" =>
+"Recent Comment",
+
+"search_by_keyword" =>
+"Search by Keyword",
+
+"search_in_titles" =>
+"Search in titles only",
+
+"search_entries" =>
+"Search in titles and entries",
+
+"search_everywhere" =>
+"Search in titles, entries and comments",
+
+"search_exact_phrase_match" =>
+"Exact Phrase Match",
+
+"search_by_member_name" =>
+"Search by Member Name",
+
+"search_exact_name_match" =>
+"Exact Name Match",
+
+"search_weblogs" =>
+"Weblogs",
+
+"search_categories" =>
+"Categories",
+
+"search_also_search_comments" =>
+"Also search in comments",
+
+"search_any_date" =>
+"Any date",
+
+"search_today_and" =>
+"Today and...",
+
+"search_this_week_and" =>
+"This Week and...",
+
+"search_one_month_ago_and" =>
+"1 Month Ago and...",
+
+"search_three_months_ago_and" =>
+"3 Months Ago and...",
+
+"search_six_months_ago_and" =>
+"6 Months Ago and...",
+
+"search_one_year_ago_and" =>
+"1 Year Ago and...",
+
+"search_newer" =>
+"Newer",
+
+"search_older" =>
+"Older",
+
+"search_sort_results_by" =>
+"Sort Results By",
+
+"search_date" =>
+"Date",
+
+"search_title" =>
+"Title",
+
+"search_most_comments" =>
+"Most Comments",
+
+"search_recent_comment" =>
+"Most Recent Comment",
+
+"search_descending" =>
+"Descending",
+
+"search_ascending" =>
+"Ascending",
+
+"search_entries_from" =>
+"Search Entries From",
+
+"search_any_category" =>
+"Any Category",
+
+"search_engine" =>
+"Search Engine",
+
+"search" =>
+"Search",
+
+"search_path_error" =>
+"The search path contains an error.  Please contact the system administrator.",
+
+"search_not_allowed" =>
+"You are not permitted to use the search engine.",
+
+"search_no_keywords" =>
+"You did not submit a search term",
+
+"search_min_length" =>
+"Search terms must be at least %x characters in length",
+
+"search_result_heading" =>
+"Search Results:",
+
+"search_no_result" =>
+"Your search did not return any results.",
+
+"search_time_not_expired" =>
+"You are only allowed to search every %x seconds.",
+
+"search_results" =>
+"Search Results",
+
+// END
+''=>''
+);
+?>
