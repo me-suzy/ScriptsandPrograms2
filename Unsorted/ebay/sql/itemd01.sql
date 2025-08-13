f41f31076a999c3039a8fe86d25c69f53f09ddeb
me-suzy <me.suzana@gmail.com>
2025-08-13 03:33:48 +0300

    Add Unsorted/ebay/sql/itemd01.sql

---
+++ Unsorted/ebay/sql/itemd01.sql
@@ -0,0 +1,12 @@
+/*	$Id: itemd01.sql,v 1.2 1999/02/21 02:54:38 josh Exp $	*/
+/*
+** itemd01.sql
+**
+** Create tablespace for item data
+** obsolete - see tablespaces.sql
+**
+create tablespace itemd01
+	datafile '/oracle03/ebay/oradata/itemd01.dbf'
+	size 120M
+	autoextend off;
+*/
