78e1ceeefa24e27f63a555c75255193bb9fcd759
me-suzy <me.suzana@gmail.com>
2025-08-13 03:20:06 +0300

    Add Unsorted/ebay/kernel/src/clsDailyAd.cpp

---
+++ Unsorted/ebay/kernel/src/clsDailyAd.cpp
@@ -0,0 +1,102 @@
+/*	$Id: clsDailyAd.cpp,v 1.4 1998/08/25 03:20:44 josh Exp $	*/
+//
+//	File:		clsAd.h
+//
+//	Class:		clsDailyAd
+//
+//	Author:		Wen Wen (wwen@ebay.com)
+//
+//	Function:
+//		class to hold daily information of an ad
+//
+//
+//	Modifications:
+//				- 11/14/97 Wen	- Created
+//
+//////////////////////////////////////////////////////////////////////
+#include "eBayKernel.h"
+#include "clsDailyAd.h"
+
+clsDailyAd::clsDailyAd(	int		CatId,
+						int		AdId,
+						int		Impressions,
+						const char*	pURL,
+						const char*	pImageSource,
+						const char*	pAlt,
+						const char*	pOther)
+{
+	mCatId		= CatId;
+	mAdId		= AdId;
+	mImpressions= Impressions;
+
+	mpURL = NULL;
+	if (pURL)
+	{
+		mpURL = new char[strlen(pURL) + 1];
+		strcpy(mpURL, pURL);
+	}
+
+	mpImageSource = NULL;
+	if (pImageSource)
+	{
+		mpImageSource = new char[strlen(pImageSource) + 1];
+		strcpy(mpImageSource, pImageSource);
+	}
+
+	mpAlt = NULL;
+	if (pAlt)
+	{
+		mpAlt = new char[strlen(pAlt) + 1];
+		strcpy(mpAlt, pAlt);
+	}
+
+	mpOther = NULL;
+	if (pOther)
+	{
+		mpOther = new char[strlen(pOther) + 1];
+		strcpy(mpOther, pOther);
+	}
+}
+
+clsDailyAd::~clsDailyAd()
+{
+	delete [] mpURL;
+	delete [] mpImageSource;
+	delete [] mpAlt;
+	delete [] mpOther;
+}
+
+int clsDailyAd::GetCatId()
+{
+	return mCatId;
+}
+
+int clsDailyAd::GetAdId()
+{
+	return mAdId;
+}
+
+int clsDailyAd::GetImpressions()
+{
+	return mImpressions;
+}
+
+const char* clsDailyAd::GetURL()
+{
+	return mpURL;
+}
+
+const char* clsDailyAd::GetImageSource()
+{
+	return mpImageSource;
+}
+
+const char* clsDailyAd::GetAlt()
+{
+	return mpAlt;
+}
+
+const char* clsDailyAd::GetOther()
+{
+	return mpOther;
+}
