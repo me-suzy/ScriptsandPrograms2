a1515c291498ae65e89470ae8099bf2339b46a1d
me-suzy <me.suzana@gmail.com>
2025-08-13 04:34:45 +0300

    Add Unsorted/fams 1.05/admin/layout_down_e.php

---
+++ Unsorted/fams 1.05/admin/layout_down_e.php
@@ -0,0 +1,49 @@
+<?php
+
+/* ------------------------------------------------------------ */
+/*								*/
+/*	File Area Management System (FAMS)			*/
+/*								*/
+/*	Copyright (c) 2001 by Bastian 'Buddy' Grimm		*/
+/*	Autor: Bastian Grimm					*/
+/*	Publisher: [ BG-Studios ]				*/
+/*	eMail: bastian@bg-studios.de				*/
+/*	WebSite: http://www.bg-studios.de			*/
+/*	Date: 03.05.01		Version: Admin Layout 1.10	*/
+/*	Last changed: 24.07.01					*/
+/*								*/
+/* ------------------------------------------------------------	*/
+
+require("../config.php");
+
+?>
+
+<BR><BR>
+</TD>
+</TR>
+
+<TR>
+<TD WIDTH=600 CLASS=main-headline HEIGHT=18>
+
+<?php
+require("../copyright.php");
+echo $copy2;
+?>
+
+</TD>
+</TR>
+</TABLE>
+
+
+</TD>
+</TR> 
+<TR>
+<TD COLSPAN=2 HEIGHT=10 CLASS=nav-background>
+</TD>
+</TR>
+</TABLE>
+
+
+</CENTER>
+</BODY>
+</HTML>
