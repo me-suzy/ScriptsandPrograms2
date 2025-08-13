02106b82148995872e716ee742c565c4ae293c3e
me-suzy <me.suzana@gmail.com>
2025-08-13 04:12:29 +0300

    Add Unsorted/evoBook/admin/config_site.php

---
+++ Unsorted/evoBook/admin/config_site.php
@@ -0,0 +1,38 @@
+<?php
+
+$settings['name'] = "evobook";
+
+$settings['url'] = "http://localhost/evobook";
+
+$settings['dateformat'] = "j F , Y, g:i a";
+
+$settings['deflang'] = "lang_english";
+
+$settings['email'] = "amunzir@tm.net.my";
+
+$settings['imgfolder'] = "img/";
+
+$settings['img_smilies'] = "img/smilies/";
+
+$settings['perpage'] = "5";
+
+$settings['allow_html'] = "1";
+
+$settings['filter'] = "shit
+fuck
+crap
+fuckers
+asshole
+bullshit
+ass";
+
+$settings['floodcheck'] = "60";
+
+$settings['mode'] = "gb";
+
+$settings['output'] = "1";
+
+$settings['autobr'] = "1";
+
+
+?>
