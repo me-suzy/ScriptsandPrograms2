8f50ed921befeea4a43114d1959f2c266921bb95
me-suzy <me.suzana@gmail.com>
2025-08-13 06:35:04 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/options.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/options.php
@@ -0,0 +1,46 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+
+$LOCALE[OPTIONS_Info] = "óÄÅÌÁÊÔÅ ÉÚÍÅÎÅÎÉÑ É ÎÁÖÍÉÔÅ ×ÎÉÚÕ ËÎÏÐËÕ ÏÂÎÏ×ÌÅÎÉÑ ÐÁÒÁÍÅÔÒÏ×.";
+
+$LOCALE[OPTIONS_Title] = "ðÒÏÓÍÏÔÒÅÔØ ÉÌÉ ÉÚÍÅÎÉÔØ ×ÁÛÉ ÐÁÒÁÍÅÔÒÙ!";
+
+$LOCALE[OPTIONS_Msg_Enter] = "óÏÏÂÝÅÎÉÅ ÐÒÉ ×ÈÏÄÅ";
+
+$LOCALE[OPTIONS_Msg_Exit] = "óÏÏÂÝÅÎÉÅ ÐÒÉ ×ÙÈÏÄÅ";
+
+$LOCALE[OPTIONS_Msg_Away] = "óÏÏÂÝÅÎÉÅ ÐÒÉ ÏÔÓÕÔÓÔ×ÉÉ";
+
+$LOCALE[OPTIONS_Msg_Back] = "óÏÏÂÝÅÎÉÅ ÐÒÉ ×ÏÚ×ÒÁÝÅÎÉÉ";
+
+$LOCALE[OPTIONS_Color] = "ã×ÅÔ ÔÅËÓÔÁ ÐÏ ÕÍÏÌÞÁÎÉÀ";
+
+$LOCALE[OPTIONS_Smiles] = "ðÏËÁÚÙ×ÁÔØ ÓÍÁÊÌÙ";
+
+$LOCALE[OPTIONS_Yes] = "äá";
+
+$LOCALE[OPTIONS_No] = "îåô";
+
+$LOCALE[OPTIONS_Locate] = "ðÏÌØÚÏ×ÁÔÅÌÉ ÍÏÇÕÔ ÉÓËÁÔØ ×ÁÓ";
+
+$LOCALE[OPTIONS_Upload] = "ðÏÌØÚÏ×ÁÔÅÌÉ ÍÏÇÕÔ ÐÏÓÙÌÁÔØ ×ÁÍ ÆÁÊÌÙ";
+
+$LOCALE[OPTIONS_Button] = "ïÂÎÏ×ÉÔØ ÐÁÒÁÍÅÔÒÙ";
+
+$LOCALE[OPTIONS_Complete] = "÷ÁÛÉ ÐÁÒÁÍÅÔÒÙ ÏÂÎÏ×ÌÅÎÙ!";
+
+$LOCALE[OPTIONS_Icons] = "÷ÙÂÅÒÉÔÅ ×ÁÛÕ ÐÅÒÓÏÎÁÌØÎÕÀ ËÁÒÔÉÎËÕ.";
+
+$LOCALE[OPTIONS_Theme] = "÷ÙÂÅÒÉÔÅ ÔÅÍÕ.";
+
+?>
