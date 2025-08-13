b3d8bd0df3f130eabed8e41ca2ff89e8dc05d890
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:03 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.email.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.email.php
@@ -0,0 +1,46 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Email
+//----------------------------
+
+"email_module_name" =>
+"Email",
+
+"email_module_description" =>
+"Nutzer-Email-Modul",
+
+'em_banned_from_email' =>
+"Die angegebene Absender-Emailadresse ist gesperrt.",
+
+'em_banned_recipient' =>
+"Eine oder mehrere Ihrer Empf&auml;nger-Adressen ist/sind gesperrt.",
+
+'em_invalid_recipient' =>
+"Eine oder mehrere Ihrer Empf&auml;nger-Adressen ist/sind ung&uuml;ltig.",
+
+'em_no_valid_recipients' =>
+"Es wurden keine g&uuml;ltigen Empf&auml;nger angegeben.",
+
+'em_sender_required' =>
+"Eine g&uuml;ltige Absenderadresse ist erforderlich.",
+
+"em_unauthorized_request" =>
+"Sie haben die f&uuml;r diese Aktion erforderliche Berechtigung nicht.",
+
+'em_limit_exceeded' =>
+"Sie haben das erlaubte Maximum von Emails pro Tag &uuml;berschritten.",
+
+'em_interval_warning' =>
+"Sie d&uuml;rfen nur alle %s Sekunden eine Email verschicken.",
+
+"em_email_sent" =>
+"Ihre Email wurde verschickt.",
+
+
+// END
+''=>''
+);
+?>
