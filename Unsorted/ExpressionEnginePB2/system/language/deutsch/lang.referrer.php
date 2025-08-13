e181f2666d6d27cac9fb6d2e30c5149bb92d4226
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:29 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.referrer.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.referrer.php
@@ -0,0 +1,95 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"referrer_module_name" =>
+"Referrer",
+
+"referrer_module_description" =>
+"Referrertracking-Modul",
+
+//----------------------------------------
+
+"referrer_ip" =>
+"Referrer-IP",
+
+"referrer_date" =>
+"Referrer-Datum",
+
+"referrers_deleted" =>
+"Referrer wurden gel&ouml;scht",
+
+"pmachine_blacklist" =>
+"pMachine-Blacklist herunterladen",
+
+"blacklist_updated" =>
+"Blacklist erfolgreich aktualisiert",
+
+'ref_no_license' =>
+"Fehler: keine Lizenznummer.",
+
+'ref_blacklist_irretrievable' =>
+"Fehler: neue Blacklist konnte nicht geladen werden.",
+
+'ref_view_blacklist' =>
+"Referrer-Blacklist",
+
+'ref_no_blacklist' =>
+"Zur Zeit steht nichts auf der Blacklist",
+
+'ref_ip' =>
+"IP-Adresse",
+
+'ref_user_agent' =>
+"User Agent (Browser)",
+
+'ref_url' =>
+"URL",
+
+'ref_blacklisted' =>
+"Auf der Blacklist",
+
+'ref_no_blacklist_table' =>
+"Blacklist-Datenbanktabelle existiert nicht",
+
+'ref_type' =>
+"Typ",
+
+"referrers" =>
+"Referrer",
+
+"view_referrers" =>
+"Referrer ansehen",
+
+"clear_referrers" =>
+"Referrer l&ouml;schen",
+
+"referrers" =>
+"Referrer",
+
+"referrer_from" =>
+"Referrer von",
+
+"referrer_to" =>
+"Referrer zu",
+
+"no_referrers" =>
+"Zur Zeit gibt es keine Referrer.",
+
+"total_referrers" =>
+"Anzahl der Referrer:",
+
+"save_instructions" =>
+"Wieviele der neuesten Referrer wollen Sie speichern?",
+
+"referrers_deleted" =>
+"Referrer wurden gel&ouml;scht.",
+
+// END
+''=>''
+);
+?>
