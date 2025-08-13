f969f49ba6871c7013d2c47adf49953cfc1f88a2
me-suzy <me.suzana@gmail.com>
2025-08-13 03:29:17 +0300

    Add Unsorted/ebay/sql/bidi01.sql

---
+++ Unsorted/ebay/sql/bidi01.sql
@@ -0,0 +1,12 @@
+/*	$Id: bidi01.sql,v 1.2 1999/02/21 02:52:32 josh Exp $	*/
+/*
+** bidi01.sql
+**
+** Create tablespace for bid indicies
+** obsolete - see tablespaces.sql
+**
+create tablespace bidi01
+	datafile '/oracle05/ebay/oradata/bidi01.dbf'
+	size 100M
+	autoextend off;
+*/
