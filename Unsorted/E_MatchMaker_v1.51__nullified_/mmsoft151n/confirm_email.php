706a61a7e841e30b9f643fd5989a974f7cfe4747
me-suzy <me.suzana@gmail.com>
2025-08-13 04:33:20 +0300

    Add Unsorted/E_MatchMaker_v1.51__nullified_/mmsoft151n/confirm_email.php

---
+++ Unsorted/E_MatchMaker_v1.51__nullified_/mmsoft151n/confirm_email.php
@@ -0,0 +1,46 @@
+<?
+##############################################################################
+#                                                                            #
+#                         confirm_email.php                                  #
+#                                                                            #
+##############################################################################
+# PROGRAM : E-MatchMaker                                                     #
+# VERSION : 1.51                                                             #
+#                                                                            #
+# NOTES   : site using default site layout and graphics                      #
+##############################################################################
+# All source code, images, programs, files included in this distribution     #
+# Copyright (c) 2001-2002                                                    #
+# Supplied by          : CyKuH [WTN]                                         #
+# Nullified by         : CyKuH [WTN]                                         #
+# Distribution:        : via WebForum and xCGI Forums File Dumps             #
+##############################################################################
+#                                                                            #
+#    While we distribute the source code for our scripts and you are         #
+#    allowed to edit them to better suit your needs, we do not               #
+#    support modified code.  Please see the license prior to changing        #
+#    anything. You must agree to the license terms before using this         #
+#    software package or any code contained herein.                          #
+#                                                                            #
+#    Any redistribution without permission of MatchMakerSoftware             #
+#    is strictly forbidden.                                                  #
+#                                                                            #
+##############################################################################
+?>
+<?
+
+require_once("siteconfig.php");
+require_once("login-functions.php");
+
+$confirm = $loginlib->confirm_email($id, $email, $mdhash);
+
+if ($confirm != 2) {
+	include("static/header.html");
+        include("static/confirm_error.html");
+
+}
+else {
+	header("Location: index.php");
+}
+
+?>
