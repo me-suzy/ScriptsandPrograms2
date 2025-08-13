16f2f59967e2ba4822dc86a6faa01292ea1ed0a5
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:02 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.login.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.login.php
@@ -0,0 +1,31 @@
+<?php
+$L = array(
+
+
+"remember_me" =>
+"Auto log-in seuraavilla kerroilla?",
+
+"no_username" =>
+"Käyttäjätunnusta, jonka annoit, ei löydy tietokannasta ",
+
+"no_password" =>
+"Salasana, jonka annoit oli väärä",
+
+"no_email" =>
+"Sinun tulee antaa emai-osoitteesi",
+
+"multi_login_warning" =>
+"Joko on jo kirjautunut näillä tunnuksilla",
+
+"return_to_login" =>
+"Palaa kirjautumiseen",
+
+"password_lockout_in_effect" =>
+"Vain neljä kirjautumis yritystä joka %x minuutissa",
+
+"unauthorized_request" =>
+"Sinulla ei ole oikeuksia muutta tätä toimintaa.",
+
+''=>''
+);
+?>
