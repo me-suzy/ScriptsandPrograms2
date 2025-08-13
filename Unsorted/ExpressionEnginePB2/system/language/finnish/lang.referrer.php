91b1be41b1451a810b62816191d688b71b1fde52
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:22 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/finnish/lang.referrer.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/finnish/lang.referrer.php
@@ -0,0 +1,85 @@
+<?php
+$L = array(
+
+
+"referrer_module_name" =>
+"Linkitykset muualta",
+
+"referrer_module_description" =>
+"Linkitysten seuranta moduli",
+
+"referrer_ip" =>
+"Referrer IP",
+
+"referrer_date" =>
+"Referrer Date",
+
+"referrers_deleted" =>
+"Refferers have been deleted",
+
+"pmachine_blacklist" =>
+"Download pMachine Blacklist",
+
+"blacklist_updated" =>
+"Blacklist updated successfully",
+
+'ref_no_license' =>
+"Error: no license number.",
+
+'ref_blacklist_irretrievable' =>
+"Error: new blacklist was irretrievable.",
+
+'ref_view_blacklist' =>
+"View Blacklist",
+
+'ref_no_blacklist' =>
+"There is currently nothing blacklisted",
+
+'ref_ip' =>
+"IP Address",
+
+'ref_user_agent' =>
+"User Agent",
+
+'ref_url' =>
+"URL",
+
+'ref_blacklisted' =>
+"Blacklisted",
+
+'ref_no_blacklist_table' =>
+"Blacklist database table does not exist",
+
+'ref_type' =>
+"Type",
+
+"referrers" =>
+"Linkittäjät ",
+
+"view_referrers" =>
+"Näytä linkittäjät",
+
+"clear_referrers" =>
+"Poista linkittäjät",
+
+"referrer_from" =>
+"Linkki, mistä",
+
+"referrer_to" =>
+"Linkki, mihin",
+
+"no_referrers" =>
+"Tällä hetkellä ei oli linkityksiä",
+
+"total_referrers" =>
+"Linkityksiä kaikkiaan:",
+
+"save_instructions" =>
+"Kuinka monta viimeisintä linkitystä haluat tallentaa?",
+
+"referrers_deleted" =>
+"Linkitykset on poistettu",
+
+''=>''
+);
+?>
