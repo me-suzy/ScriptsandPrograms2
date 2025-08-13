97e95d87ab0a702df014caeeb25e87184846b6b1
me-suzy <me.suzana@gmail.com>
2025-08-13 05:37:47 +0300

    Add Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_profile.php

---
+++ Unsorted/invboard1_1_unreleased/invboard1_1/upload/lang/en/lang_profile.php
@@ -0,0 +1,64 @@
+<?php
+
+$lang = array (
+
+'user_local_time' => "User's local time",
+
+'head_title'   => "Viewing Profile",
+
+'head_contact' => "Contact Details",
+
+'head_posts'   => "Posting Information",
+
+'head_info'    => "Member Information",
+
+'no_info'      => "<i>No Information</i>",
+
+'click_here'   => "<i>Click Here</i>",
+'private'      => "<i>Private</i>",
+
+'active_stats' => "Active Stats",
+
+'total_posts'   => "Total Posts:",
+'joined'        => "Joined:",
+'posts_per_day'  => "Posts per day:",
+'total_percent'  => "of total forum posts",
+'fav_forum'      => "Most active in:",
+'fav_posts'      => "posts in this forum",
+'fav_percent'    => "of this member's posts",
+
+'communicate'    => "Communicate",
+"info"           => "Information",
+'post_detail'    => "Posting Details",
+
+"back"  => "back",
+
+'email'   => "Email:",
+'aim'     => "AIM Name:",
+'icq'     => "ICQ Number:",
+'yahoo'   => "Yahoo Identity:",
+'msn'     => "MSN Identity:",
+'pm'      => "Private Message:",
+
+'page_title' => "Viewing Profile",
+
+'find_posts'  => "Find all posts by this member",
+'add_to_contact' => "Add to contact list",
+
+'edit_my_sig'    => "Edit my Signature",
+'edit_profile'   => "Edit my Profile",
+'edit_avatar'    => "Avatar Options",
+
+
+'mgroup'  => "Member Group:",
+'mtitle'  => "Member Title:",
+'homepage'=> "Home Page:",
+'birthday' => "Birthday:",
+'location' => "Location:",
+'interests' => "Interests:",
+'avatar'    => "Avatar:",
+'siggie'    => "Signature:",
+
+
+);
+?>
