534e35f3001bcdc1559ddf94362823ca2867f2ff
me-suzy <me.suzana@gmail.com>
2025-08-13 06:32:54 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/it/help.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/it/help.php
@@ -0,0 +1,205 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[HELP_Title] = "Aiutare su";
+
+$LOCALE[HELP_Unavailable] = "Una descrizione utile di questo quesito è non disponibile.";
+
+$LOCALE[HELP_Username] = "Un nome o il 0 è la persona che lei desidera essere come saputa mentre nel ciarla. Simile a un soprannome, questo può essere il suo nome reale o un ha rinunciato un.";
+
+$LOCALE[HELP_Language] = "MWChat ha la capacità per mostrare parecche lingue diverse. Questa scatola lascia per che lui scegliere qualunque lingua che lei preferisce usare.";
+
+$LOCALE[HELP_Password] = "Amministratore, Operatore, ed iscritto richiede una parola d'ordine di accedere. Questo evita l'accesso non autorizzato al suo conto.";
+
+$LOCALE[HELP_Lobby_Public] = "L'elenco di stanza pubblico è una selezione di varie stanze che lei può ciarlare in. Scegliere una stanza quell'intrests lei complimentare o creare suo proprio.";
+
+$LOCALE[HELP_Lobby_Private] = "L'elenco di stanza privato è nascosto agli operatori standard. Se lei desidera ciarlare in una stanza privata, entra il nome della stanza domandata al di sotto. Le stanze private non sono mostrate su qualunque elenco di stanza, dunque lei può tenere delle conversazioni private senza gli operatori di uninvited entrando su lei.";
+
+$LOCALE[HELP_Lobby_Public_Create] = "Se lei non può trovare una stanza che interessa lei nell'elenco sopra, lei può creare la sua propria stanza da alimentazione il nome della stanza domandata nel campo vagabondaggio.";
+
+$LOCALE[HELP_Lobby_Private_Create] = "Entrare il nome della stanza privata che lei desidera entrare. Se la stanza non esiste, sarà creato per lei.";
+
+$LOCALE[HELP_Private_Message] = "Scegliere un operatore elencato in questa goccia questa giù scatola. Qualunque messaggi che lei invia andranno soltanto all'operatore di specifed. Per commutare alla stanza intera, scegliere le lineette predefinite. Il messaging privato può essere fatto anche per usare il /il comando di msg.";
+
+$LOCALE[HELP_Private_Message_Refresh] = "Scattare questa maglia per rinfrescare l'elenco di operatore di messaggio privato.";
+
+$LOCALE[HELP_Colors] = "Se lei amerebbe per cambiare il colore del testo che lei entra, lo scatto su qualunque colore nella sbarra di colore. Lei vedrà un messaggio che dindo che i suoi colori sono stato aggiornati. Lei può scattare dovunque nella metà più bassa della sbarra di colore per restaurare il colore predefinito.";
+
+$LOCALE[HELP_Enter_Password] = "Entrare una parola d'ordine nuova che lei amerebbe usare. Questa parola d'ordine può essere qualunque alfa qualunque testo numerico. Assicurarlo scrive questa parola d'ordine giù e tenerlo in un luogo sicuro. Esso è richiesto che i è cinque o più in .";
+
+$LOCALE[HELP_Icons] = "Scegliere un'icona dall'elenco vagabondaggio di farlo un'offerta in che ciarlare. Tutti gli operatori possono vedere la sua icona.";
+
+$LOCALE[HELP_Theme] = "Scegliere un tema dall'elenco al di sotto. Un tema cambierà tutti il ciarla i colori. Lei può avere bisogno di ritornare all'anticamera, e sostiene poi nella stanza per tutto il tema cambia per portare l'effetto.";
+
+$LOCALE[HELP_Confirm_Password] = "Entrare la parola d'ordine nuova che lei ha scelta ancora qui. Questo conferma che lei non ha fatto un refuso e l'assicura sa che la sua parola d'ordine è.";
+
+$LOCALE[HELP_YOB] = "Questo è l'anno di nascita. Queste informazioni è usata all'età di caculate in alcuno caso, come alcune leggi possono richiedere che gli operatori di essere di un certo ancora.";
+
+$LOCALE[HELP_Occupation] = "Questo è l'occupazione di operatori o il lavoro. Di solito la loro fonte principale di reddito.";
+
+$LOCALE[HELP_Hobbies] = "Questo sono delle cose che un operatore piace per fare. La maggior parte del tempo speso sui hobby è di solito risparmia il tempo.";
+
+$LOCALE[HELP_Location] = "Questo è la posizione geografica dell'operatore.";
+
+$LOCALE[HELP_Comments] = "Questo è qualunque commenti o qualunque opinioni che l'operatore può avere o desidera alla voce.";
+
+$LOCALE[HELP_Status] = "Questo è gli operatori lo stato attuale. Di solito dichiarare se un operatore scompare, *, iscritto, un , o un operatore.";
+
+$LOCALE[HELP_Upload_File] = "Questo è il file che lei vuole inviare a un operatore. Scattare il di curiosare il bottone per scegliere il file che lei desidera inviare.";
+
+$LOCALE[HELP_Upload_User] = "Questo è l'operatore che lei vuole inviare un file a.";
+
+$LOCALE[HELP_Enter_Message] = "Quando lei entra una stanza, questo messaggio sarà inviato porta l'avviso che lei è entrato la stanza.";
+
+$LOCALE[HELP_Exit_Message] = "Quando lei esce una stanza, questo messaggio sarà inviato con l'avviso che lei ha la sinistra la stanza.";
+
+$LOCALE[HELP_Away_Message] = "Quando lei va via, questo messaggio sarà inviato con l'avviso che suo lontano.";
+
+$LOCALE[HELP_Back_Message] = "Quando il suo dorso dall'essere lontano, questo messaggio sarà inviato porta l'avviso che il suo dorso.";
+
+$LOCALE[HELP_Default_Color] = "Se lei vuole che un colore predefinito essere usato ogni volta lei entra una stanza, quest'opzione lascerà per lo che lui regolare.";
+
+$LOCALE[HELP_Smiles] = "Quest'opzione commuta la sua capacità per vedere grafico o le facce di smiley di testo nel ciarla.";
+
+$LOCALE[HELP_Locate] = "Quest'opzione commuta la sua capacità per lasciare o rifiuta gli operatori per localizzarlo. Localizzare che un operatore dice che la stanza lei sono attualmente in.";
+
+$LOCALE[HELP_Upload] = "Quest'opzione commuta la sua capacità per ricevere i file dagli altri operatori.";
+
+$LOCALE[HELP_Room] = "Questo è la stanza che lei sono attualmente in, o la stanza che lei desidera creare o entra.";
+
+$LOCALE[HELP_Room_Password] = "Alcune stanze private sono delle parole d'ordine protette. Se la stanza che lei desidera entrare è la parola d'ordine protetta, l'entra qui.";
+
+$LOCALE[HELP_Number_Users] = "Questo è il numero totale di operatori che sono attualmente nella stanza.";
+
+$LOCALE[HELP_Topic] = "Questo è il soggetto attuale della stanza.";
+
+$LOCALE[HELP_Buddylist] = "Questo è il suo elenco di amico. Questo contiene i nomi utente di tutti gli operatori in questa stanza, e qualunque amici che lei può avere. Lei può scattare su ogni nome per prendere le informazioni di quell'operatore se si sono iscritti.";
+
+$LOCALE[HELP_Serial_Number] = "Questo è il numero di matricola di MWChat. Lei può ottenere un numero di matricola al http: //www.appindex.net/mwchat";
+
+$LOCALE[HELP_Storage] = "Questo è le sue informazioni di magazzino di file. Le prime mostre di numero troppo % spazio lei ha disponibile per l'uso. Le seconde mostre di numero che il suo totale ha lasciata che il magazzino è. È vagabondaggio un elenco di qualunque file che lei può avere. Lo scatto su un agente provocatore di scaricare quel file, o scatta la scatola di assegno per togliere il file. Lei può vedere anche la misura di ogni file, quando lei ha ricevuto il file, e che ha inviato lei il file.";
+
+$LOCALE[HELP_ERROR_License] = "La licenza che lei ha è inesatta, o non esce. Ritentare la sua licenza e se non lavora prende tuttavia un'Appindex Rete di numero di matricola o contatto sostegno di nuovo.";
+
+$LOCALE[HELP_ERROR_AlreadyIn] = "Qualcuno è già abbattuto in con quel nome utente. È possibile se lei non ha scollegato correttamente, che il suo conto non ha oziato fuori. Attendere dieci minuti e tentare ancora.";
+
+$LOCALE[HELP_ERROR_Logout] = "Il suo conto è stato fuori abbattuto. Lei può accedere ancora se lei come.";
+
+$LOCALE[HELP_ERROR_Expired] = "Se lei è stato pigro che questo potrebbe essere perché lei sono stato scaduto. Lei può essere stato anche calciato fuori da un amministratore o dall'operatore.";
+
+$LOCALE[HELP_ERROR_RoomLong] = "La stanza lo dà un nome a ha scelto è troppo lungo. Il carattere massimo lo limita 15 caratteri.";
+
+$LOCALE[HELP_Live] = "Il ciarla il sistema è fuori linea. Questo potrebbe essere perché un amministratore fa delle salite.";
+
+$LOCALE[HELP_ERROR_Profanity] = "Lei è stato fuori abbattuto per usare profanità nel ciarla. la. La profanità non è lasciata.";
+
+$LOCALE[HELP_ERROR_NoData] = "Lei non ha riempito correttamente tutto mette in campo. Per favore di tentare ancora.";
+
+$LOCALE[HELP_ERROR_BadData] = "Le informazioni lei è entrato contenuto cattivo caratteri. Per favore l'uso soltanto alfa caratteri numerici.";
+
+$LOCALE[HELP_ERROR_Serial_Invalid] = "Il numero di matricola suo ha fornito non uguaglia il suo server. Lei avrà bisogno di prendere un numero di matricola nuovo e verifica il suo indirizzo di IP del webserver.";
+
+$LOCALE[HELP_ERROR_Serial_None] = "L'Appindex rete non ha un numero di matricola sul disco per lei. Per favore iscriversi il suo prodotto e prendere un numero di matricola nuovo.";
+
+$LOCALE[HELP_ERROR_Critical] = "Uno sconosciuto critico errore è accaduto. Se il problema persiste, il sostegno di Appindex Rete di contatto.";
+
+$LOCALE[HELP_ERROR_Javascript] = "MWChat richiede un tela browser capace di mostrare javascript. del. Per favore di permettere il javascript nel suo webbrowser o la sua salita.";
+
+$LOCALE[HELP_ERROR_Browser] = "Non il browser di tela lei usano in compatibile con MWChat. Raccomandiamo l'Esploratore di Netscape o Internet usando.";
+
+$LOCALE[HELP_ERROR_Restricted] = "Il nome utente lei ha scelto è un limitato o riservato nome. Per favore di scegliere un altri nome ed un'apertura di sessione ancora.";
+
+$LOCALE[HELP_ERROR_BadPass] = "La parola d'ordine che lei ha fornita è inesatta. Controllare con un amministratore per i dettagli ulteriori.";
+
+$LOCALE[HELP_ERROR_IP] = "MWChat ha rivelato che il suo indirizzo di IP ha cambiato. Per le ragioni di sicurezza, l'abbiamo abbatteto fuori. Per favore l'apertura di sessione ancora.";
+
+$LOCALE[HELP_COMMAND_addbuddy] = "Aggiungere un amico al suo elenco di amico. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_admin] = "Lascia degli amministratori per annullare la registrazione degli operatori o osservano dei ceppi. Specificare la seconda opzione (il nome utente) di osservare i ceppi per un operatore di specfic o annullare la registrazione quell'operatore.";
+
+$LOCALE[HELP_COMMAND_afk] = "Cambia il suo modo a lontano dalla tastiera. Lei può specificare un facoltativo lontano messaggio.";
+
+$LOCALE[HELP_COMMAND_block] = "Blocca tutti i messaggi privati da qualunque operatori.";
+
+$LOCALE[HELP_COMMAND_bug] = "Inviare una relazione di insetto.";
+
+$LOCALE[HELP_COMMAND_about] = "Osservare le informazioni di MWChat ed i creatori.";
+
+$LOCALE[HELP_COMMAND_clear] = "Chiarisce la corrente ciarla la finestra.";
+
+$LOCALE[HELP_COMMAND_color] = "Ha regolato il colore di testo a un colore della sua scelta. Questo colore deve essere in #000000 formato.";
+
+$LOCALE[HELP_COMMAND_deop] = "Togliere uno stato di operatori di operatori. Questo comando può essere usato soltanto dagli amministratori e dagli operatori.";
+
+$LOCALE[HELP_COMMAND_global] = "Inviare un messaggio a tutti gli operatori in tutte le stanze. Questo comando può essere usato soltanto da un amministratore.";
+
+$LOCALE[HELP_COMMAND_help] = "Mostra questo documento.";
+
+$LOCALE[HELP_COMMAND_ignore] = "Aggiungere un operatore al suo ignora l'elenco. Quell'operatore non lo può inviare i messaggi privati e lei non può vedere di messaggi che inviano. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_unignore] = "Togliere un operatore dal suo ignora l'elenco. Quell'operatore lo può inviare adesso i messaggi privati e lei può vedere dei messaggi che inviano. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_kick] = "Calciare un operatore dal sistema. Lei può specificare una ragione facoltativa. Questo comando può essere usato soltanto dai o dagli operatori.";
+
+$LOCALE[HELP_COMMAND_lobby] = "Ritorna lei all'anticamera. Lei può specificare un messaggio di uscita facoltativo.";
+
+$LOCALE[HELP_COMMAND_locate] = "Localizza un operatore specificato. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_logout] = "Abbatte un operatore fuori del sistema. Lei può specificare un messaggio di uscita facoltativo.";
+
+$LOCALE[HELP_COMMAND_me] = "Descrivere che lei fanno a meno di fare riferimento a te stesso direttamente.";
+
+$LOCALE[HELP_COMMAND_motd] = "Mostra il messaggio del giorno.";
+
+$LOCALE[HELP_COMMAND_myfiles] = "Mostra i suoi file in linea.";
+
+$LOCALE[HELP_COMMAND_op] = "Dare uno stato di operatore di operatore. Questo comando può essere usato soltanto dai e dagli operatori.";
+
+$LOCALE[HELP_COMMAND_prefs] = "Mostra le sue preferenze di MWChat.";
+
+$LOCALE[HELP_COMMAND_print] = "Mostra le sue conversazioni in una finestra stampabile.";
+
+$LOCALE[HELP_COMMAND_msg] = "Inviare un messaggio privato a un operatore. Il Noone può vedere altro questo messaggio.";
+
+$LOCALE[HELP_COMMAND_register] = "Iscrive il nome utente attuale suo è di usare. Iscrivere lascia degli operatori per dividere dei file e creano gli elenchi di amico.";
+
+$LOCALE[HELP_COMMAND_removebuddy] = "Toglie un amico dal suo elenco di amico. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_roll] = "Rotola i dadi di chat-room. Questo è utile per il gaming in linea.";
+
+$LOCALE[HELP_COMMAND_showbuddy] = "Mostra tutti gli operatori sul suo elenco di amico. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_showignored] = "Mostra tutti gli operatori sul suo ignora l'elenco. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_smiley] = "Mostra tutto lo smiley disponibile affronta.";
+
+$LOCALE[HELP_COMMAND_topic] = "Cambia il soggetto di stanza. Questo comando può essere usato soltanto dagli amministratori e dagli operatori.";
+
+$LOCALE[HELP_COMMAND_unafk] = "Cambia il suo modo per sostenere da lontano dalla tastiera. Lei può specificare un facoltativo posteriore messaggio.";
+
+$LOCALE[HELP_COMMAND_unblock] = "Lascia per che lui accettare dei messaggi privati da tutti gli operatori ancora.";
+
+$LOCALE[HELP_COMMAND_topic] = "Toglie un operatore dal suo ignora l'elenco. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_unregister] = "Annulla la registrazione il suo nome utente. Questo lo farà disponibile per nessuno usare. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_upload] = "Mostra il file che la dividendo interfaccia. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_url] = "Lascia per che lui inserire un URL nel ciarla.";
+
+$LOCALE[HELP_COMMAND_whois] = "Le informazioni di mostre dell'operatore specificato. Questo comando può essere usato soltanto dagli operatori iscritti.";
+
+$LOCALE[HELP_COMMAND_Title] = "I comandi disponibili";
+
+$LOCALE[HELP_COMMAND_Type] = "{} indica un'opzione richiesta, [] indica un'opzione facoltativa.";
+
+?>
