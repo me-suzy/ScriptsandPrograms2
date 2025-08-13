24bac0a3ab808e1ff22e3c09cdfd5c0d11d75378
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:38 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.templates.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.templates.php
@@ -0,0 +1,337 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Templates
+//----------------------------
+
+"php_parse_location" =>
+"Stadium, in dem PHP geparsed (gelesen) werden soll",
+
+"input" =>
+"Input",
+
+"output" =>
+"Output",
+
+"template_loop" =>
+"Sie haben durch unsauber verschachtelte Unter-Vorlagen einen Vorlagen-Loop verursacht.",
+
+"template_intro" =>
+"Auf dieser Seite werden die Vorlagen verwaltet.",
+
+"create_new_template_group" =>
+"Neue Vorlagengruppe erstellen",
+
+"new_template_group_form" =>
+"Neue Vorlagengruppe",
+
+"edit_template_group_form" =>
+"Vorlagengruppe bearbeiten",
+
+"name_of_template_group" =>
+"Name der Vorlagengruppe",
+
+"template_group_instructions" =>
+"Der Name mu&szlig; aus einem einzigen Wort bestehen und darf keine Leerzeichen enthalten.",
+
+"undersores_allowed" =>
+"(Unterstreichungen und Trennungen sind erlaubt)",
+
+"new_template_form" =>
+"Neues Vorlagenformular",
+
+"edit_template_form" =>
+"Vorlagenformular bearbeiten",
+
+"name_of_template" =>
+"Vorlagen-Name",
+
+"group_name" =>
+"Gruppen-Name",
+
+"order" =>
+"Ordnen",
+
+"reserved_name" =>
+"Der gew&auml;hlte Name ist reserviert und kann nicht benutzt werden.",
+
+"type_of_template" =>
+"Vorlagentyp",
+
+"webpage" =>
+"Webpage",
+
+"css_stylesheet" =>
+"CSS-Stylesheet",
+
+"rss" =>
+"RSS-Seite",
+
+"missing_name" =>
+"Ihre Vorlage braucht einen Namen.",
+
+"template_name_taken" =>
+"Der gew&auml;hlte Namen ist bereits vergeben.",
+
+"template_group_del_conf" =>
+"L&ouml;schung einer Vorlagengruppe best&auml;tigen",
+
+"template_del_conf" =>
+"L&ouml;schung einer Vorlage best&auml;tigen",
+
+"edit_template" =>
+"Vorlage bearbeiten",
+
+"add_a_template" =>
+"Neue Vorlage hinzuf&uuml;gen",
+
+"delete_template_group" =>
+"Gruppe l&ouml;schen",
+
+"edit_template_group" =>
+"Gruppe bearbeiten",
+
+"group_order" =>
+"Gruppen-Reihenfolge",
+
+"export_templates" =>
+"Vorlagen exportieren",
+
+"template_name" =>
+"Vorlagenname:",
+
+"template_management" =>
+"Vorlagen-Management",
+
+"current_template" =>
+"Aktuelle Vorlage:",
+
+"preferences" =>
+"Einstellungen",
+
+"prefs" =>
+"Einstellungen",
+
+"hits" =>
+"Hits",
+
+"hit_counter" =>
+"Hitcounter",
+
+"template_updated" =>
+"Vorlage aktualisiert",
+
+"template_created" =>
+"Vorlage erstellt",
+
+"form_is_empty" =>
+"Sie haben ein leeres Formular abgeschickt.",
+
+"all_fields_required" =>
+"Sie m&uuml;ssen alle Felder ausf&uuml;llen.",
+
+"illegal_characters" =>
+"Der Name darf nur Buchstaben, Ziffern, Unterstreichungen und Trennungsstriche enthalten.",
+
+"edit_template_group_order" =>
+"Reihenfolge bearbeiten",
+
+"template_group_updated" =>
+"Vorlagengruppe aktualisiert",
+
+"template_group_created" =>
+"Vorlagengruppe erstellt",
+
+"delete_this_group" =>
+"Diese Gruppe l&ouml;schen?",
+
+"all_templates_will_be_nuked" =>
+"ALLE VORLAGEN IN DIESER GRUPPE WERDEN DAUERHAFT GEL&Ouml;SCHT!",
+
+"template_group_deleted" =>
+"Vorlagengruppe gel&ouml;scht",
+
+"edit_group_order" =>
+"Reihenfolge bearbeiten",
+
+"template_size" =>
+"Gr&ouml;&szlig;e der Vorlage",
+
+"small" =>
+"Klein",
+
+"medium" =>
+"Mittel",
+
+"large" =>
+"Gro&szlig;",
+
+"view_rendered_template" =>
+"Vorlage ansehen",
+
+"save_history" =>
+"&Auml;nderung speichern",
+
+"view" =>
+"Ansehen",
+
+"clear" =>
+"Leeren",
+
+"select_all" =>
+"Alle ausw&auml;hlen",
+
+"export" =>
+"Exportieren",
+
+"export_as" =>
+"Exportiere Dateien als",
+
+"export_will_be_zip" =>
+"Hinweis: Ihre exportierten Daten werden als ZIP-Archiv verschickt.",
+
+"choose_templates" =>
+"W&auml;hlen Sie Vorlagen zum Export aus",
+
+"unable_to_write_files" =>
+"Ich kann die Dateien nicht in Ihr Cache-Verzeichnis schreiben",
+
+"revision_history" =>
+"Liste der &Auml;nderungen ansehen",
+
+"clear_revision_history" =>
+"Liste der &Auml;nderungen leeren",
+
+"history_cleared" =>
+"Liste der &Auml;nderungen wurde gel&ouml;scht.",
+
+"close_window" =>
+"Fenster schlie&szlig;en",
+
+"template_preferences" =>
+"Vorlagen-Einstellungen",
+
+"delete_template" =>
+"Vorlage l&ouml;schen",
+
+"delete_this_template" =>
+"Diese Vorlage l&ouml;schen:",
+
+"template_deleted" =>
+"Vorlage gel&ouml;scht",
+
+"default_site_page" =>
+"Bezeichnet die Anfangsseite der Hauptsite",
+
+"is_site_default" =>
+"Soll die Index-Vorlage in dieser Gruppe zur Homepage Ihrer Site gemacht werden?",
+
+"allow_php" =>
+"PHP in dieser Vorlage zulassen?",
+
+"enable_cache" =>
+"Vorlagen-Caching aktivieren?",
+
+"cache_interval" =>
+"Zeitraum zwischen Cache-Refreshes",
+
+"in_minutes" =>
+"Zeitraum in Minuten",
+
+"php_security_warning" =>
+"&quot;Ja&quot; hat Auswirkungen auf die Sicherheit Ihrer Site",
+
+"consult_manual" =>
+"Bitte im Handbuch nachlesen",
+
+"preferences_updated" =>
+"Einstellungen aktualisiert",
+
+"choose_default_data" =>
+"Standard-Vorlagen-Daten",
+
+"blank_template" =>
+"Keine - leere Vorlage erstellen",
+
+"template_from_library" =>
+"Vorlage aus der Bibliothek benutzen",
+
+"an_existing_template" =>
+"Existierende Vorlage kopieren",
+
+"you_must_submit_a_name" =>
+"Ihre Vorlage braucht einen Namen.",
+
+"member_group" =>
+"Mitgliedsgruppe",
+
+"can_view_template" =>
+"Kann diese Seite ansehen",
+
+"require_logged_in" =>
+"M&uuml;ssen Nutzer eingeloggt sein, um diese Seite zu sehen?",
+
+"restrict_to_group" =>
+"Ansehen auf Mitgliedsgruppen beschr&auml;nken",
+
+"logged_out_select_blurb" =>
+"Wenn Sie &quot;Ja&quot; gew&auml;hlt haben, soll ein nicht eingeloggter Nutzer anstelle dieser Seite die folgende angezeigt bekommen:",
+
+"no_access_select_blurb" =>
+"Wenn Sie bei einem der obigen &quot;nein&quot; gew&auml;hlt haben:",
+
+"no_access_instructions" =>
+"Wenn unautorisierte Nutzer diese Seite anzusehen versuchen, sollen sie stattdessen die folgende Seite zu sehen bekommen:",
+
+"template_notes" =>
+"Vorlagen-Notizen",
+
+"template_notes_desc" =>
+"Nutzen Sie dieses Feld, um Notizen und Informationen &uuml;ber diese Vorlage zu speichern:",
+
+"global_variables" =>
+"Globale Variablen",
+
+"global_variables_explanation" =>
+"Sie k&ouml;nnen Ihre eigenen Variablen definieren und zu Ihren Vorlagen hinzuf&uuml;gen.",
+
+"no_global_variables" =>
+"Zur Zeit gibt es keine globalen Variablen",
+
+"global_variable_name" =>
+"Name der globalen Variablen",
+
+"create_new_global_variable" =>
+"Neue globale Variable erstellen",
+
+"variable_name" =>
+"Name der Variablen",
+
+"global_variable_syntax" =>
+"Syntax der Variablen",
+
+"variable_data" =>
+"Inhalt der Variablen",
+
+"global_var_created" =>
+"Globale Variable erstellt",
+
+"global_var_updated" =>
+"Globale Variable aktualisiert",
+
+"delete_global_variable" =>
+"Globale Variable l&ouml;schen",
+
+"delete_this_variable" =>
+"Die folgende globale Variable l&ouml;schen?",
+
+"variable_deleted" =>
+"Variable gel&ouml;scht",
+
+
+// END
+''=>''
+);
+?>
