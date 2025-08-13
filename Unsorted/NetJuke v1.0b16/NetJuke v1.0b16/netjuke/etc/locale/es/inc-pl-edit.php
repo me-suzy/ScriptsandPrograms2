91851f1e9185014be8eec94a815cb6d3e1eec274
me-suzy <me.suzana@gmail.com>
2025-08-13 06:55:26 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-pl-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/es/inc-pl-edit.php
@@ -0,0 +1,91 @@
+<?php
+
+##################################################
+
+# /pl-edit.php
+
+##################################################
+
+define( "PLEDIT_NOPLID", "Por favor, seleccione una lista para editar." );
+
+define( "PLEDIT_ADDTO_NOID", "Por favor, seleccione por lo menos una canción para guardar." );
+define( "PLEDIT_ADDTO_TITLE_1", "Lista sin título" );
+define( "PLEDIT_ADDTO_TITLE_2", "Aleatorio" );
+
+define( "PLEDIT_DELPL_NOID", "Por favor, especifique la lista a borrar." );
+define( "PLEDIT_DELPL_CONFIRM", "¿Está seguro de eliminar la lista?" );
+
+define( "PLEDIT_DELPT_NOTRID", "Por favor, selecione por lo menos una canción para eliminarla de la lista." );
+define( "PLEDIT_DELPT_CONFIRM", "¿Está seguro de eliminar las \\ncanciones seleccionadas de la lista?" );
+
+define( "PLEDIT_DENIED", "Esta no es una de sus listas. Por favor, intente de nuevo." );
+
+define( "PLEDIT_CHCK_HELP", "Click para seleccionar esta canción" );
+define( "PLEDIT_PLAY_HELP", "Click para escuchar esta canción" );
+define( "PLEDIT_DLOAD_HELP", "Click para descargar esta canción" );
+define( "PLEDIT_INFO_HELP", "Click para ver información detallada de esta canción." );
+define( "PLEDIT_AR_HELP", "Click para ver todas las canciones por este Artista." );
+define( "PLEDIT_AL_HELP", "Click para ver todas las canciones en este Álbun." );
+define( "PLEDIT_GE_HELP", "Click para ver todas las canciones de este Género." );
+
+define( "PLEDIT_MUSTCLICK", "Por favor, de click en el boton de guardar su lista." );
+
+define( "PLEDIT_HEADER_1", "LISTAS" );
+
+define( "PLEDIT_SHRD", "Compartidas" );
+
+define( "PLEDIT_BTN_SAVE", "Guardar" );
+define( "PLEDIT_BTN_DELETE", "Eliminar" );
+define( "PLEDIT_BTN_CANCEL", "Cancelar" );
+
+define( "PLEDIT_ROWCNT_1", "Registradas cancion(es)" );
+define( "PLEDIT_ROWCNT_2", "para" );
+
+define( "PLEDIT_QS_AR", "Artistas" );
+define( "PLEDIT_QS_TR", "Canciones" );
+define( "PLEDIT_QS_AL", "Álbunes" );
+define( "PLEDIT_QS_GE", "Género" );
+define( "PLEDIT_QS_TI", "Duración" );
+define( "PLEDIT_QS_BTN", "Buscar" );
+
+define( "PLEDIT_TB_PLIST_BTN", "Editar" );
+define( "PLEDIT_TB_PLAY_ALL", "Escuchar" );
+define( "PLEDIT_TB_PLAY_ALL_HELP", "Click para escuchar una lista guardada." );
+define( "PLEDIT_TB_SELECT", "Seleccionar todo" );
+define( "PLEDIT_TB_SELECT_HELP", "Click para escuchar todas las pistas en en esta página" );
+define( "PLEDIT_TB_RESET", "Lanzar Todo" );
+define( "PLEDIT_TB_RESET_HELP", "Click Para Sonar Todas Sus Pistas Seleccionadas" );
+define( "PLEDIT_TB_PLAY", "Sonar Seleccionadas" );
+define( "PLEDIT_TB_PLAY_HELP", "Click para empezar a escuchar todas las canciones seleccionadas" );
+define( "PLEDIT_TB_DELETE", "Eliminar seleccionadas" );
+define( "PLEDIT_TB_DELETE_HELP", "Click para eliminar la selección de canciones de la lista actual" );
+define( "PLEDIT_TB_ADJUST", "Ajuste de secuencia" );
+define( "PLEDIT_TB_ADJUST_HELP", "Click para ajustar la secuencia de las condiciones de ordenación" );
+define( "PLEDIT_TB_ADDTO", "Copiar la lista" );
+define( "PLEDIT_TB_ADDTO_HELP", "Click para copiar todas las canciones seleccionas a la lista" );
+
+define( "PLEDIT_COLS_OP", "Opciones" );
+define( "PLEDIT_COLS_SQ", "Secuencia." );
+define( "PLEDIT_COLS_SQ_HELP", "Click para ordenar esta página por secuencia numérica" );
+define( "PLEDIT_COLS_TR", "Nombre de la canción" );
+define( "PLEDIT_COLS_TR_HELP", "Click para ordenar esta página por canción" );
+define( "PLEDIT_COLS_TI", "Duración" );
+define( "PLEDIT_COLS_TI_HELP", "Click para ordener esta página por duración" );
+define( "PLEDIT_COLS_AR", "Artista" );
+define( "PLEDIT_COLS_AR_HELP", "Click para ordenar esta página por artístas" );
+define( "PLEDIT_PLAY_AR_HELP", "Escuchar todas las canciones por este autor" );
+define( "PLEDIT_FILTER_AR_HELP", "Click Para Listar Todos Los Album(S) Para este Artista" );
+define( "PLEDIT_COLS_AL", "Álbum" );
+define( "PLEDIT_COLS_AL_HELP", "Click para ordenar esta pagina por Álbum" );
+define( "PLEDIT_PLAY_AL_HELP", "Escuchar todas las canciones de este Álbum" );
+define( "PLEDIT_FILTER_AL_HELP", "Click para Listar Todos Los Artistas de Este Album" );
+define( "PLEDIT_COLS_TN", "#" );
+define( "PLEDIT_COLS_TN_HELP", "Click para ordenar esta página por número de canción" );
+define( "PLEDIT_COLS_GE", "Género" );
+define( "PLEDIT_COLS_GE_HELP", "Click para ordenar esta página por género" );
+define( "PLEDIT_PLAY_GE_HELP", "Escuchar todas las canciones por este género" );
+
+
+##################################################
+
+?>
