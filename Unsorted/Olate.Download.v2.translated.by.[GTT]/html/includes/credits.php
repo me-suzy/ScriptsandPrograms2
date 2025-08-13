2b0830cd8585b914efca8898574c8c3ef7dfa708
me-suzy <me.suzana@gmail.com>
2025-08-13 07:06:48 +0300

    Add Unsorted/Olate.Download.v2.translated.by.[GTT]/html/includes/credits.php

---
+++ Unsorted/Olate.Download.v2.translated.by.[GTT]/html/includes/credits.php
@@ -0,0 +1,23 @@
+<?php
+/***************************************************************************
+ *                      Olate Download v2 - Download Manager
+ *
+ *                           http://www.olate.com
+ *                            -------------------
+ *   author                : David Mytton
+ *   copyright             : (C) Olate 2003 
+ *
+ *   Support for Olate scripts is provided at the Olate website. Licensing
+ *   information is available in the license.htm file included in this
+ *   distribution and on the Olate website.                  
+ ***************************************************************************/
+?>
+<!--Begin Credit Line. Leave intact-->
+<div align="center"><span class="small"><a href="http://www.olate.com" target="_blank"><?= $language['credits_poweredby'].$config['version']; ?></a>
+</span>
+<!--End Credit Line-->
+<br>
+<!--Begin Translated by GTT credits-->
+<span class="small"><?= $language['credits_translatedby']; ?></span>
+</div>
+<!--End Credit Line-->
