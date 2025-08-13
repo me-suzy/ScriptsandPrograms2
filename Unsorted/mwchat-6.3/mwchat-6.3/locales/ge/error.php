4fdbd5812ba95b3d60cf71a2ddb292e2fb7c6cd4
me-suzy <me.suzana@gmail.com>
2025-08-13 06:32:12 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/error.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/error.php
@@ -0,0 +1,51 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[ERROR_License] = "Es gibt einen Fehler mit Ihrer Lizenz. Bitte registrieren Sie MWChat jetzt.";
+
+$LOCALE[ERROR_AlreadyIn] = "Der Name, den Sie gewählt haben, ist schon in Verwendung.";
+
+$LOCALE[ERROR_Title] = "Fehler";
+
+$LOCALE[ERROR_Logout] = "Sie sind abgemeldet worden.";
+
+$LOCALE[ERROR_Expired] = "Ihr Login ist abgelaufen, bitte erneut anmelden.";
+
+$LOCALE[ERROR_RoomLong] = "Der Raum Name, den Sie gewählt haben, ist zu lang.";
+
+$LOCALE[ERROR_Live] = "Das System ist zur Zeit nicht erreichbar.";
+
+$LOCALE[ERROR_Profanity] = "Ihre Eingaben enthalten schmutzige Wörter.";
+
+$LOCALE[ERROR_NoData]   = "Füllen Sie bitte alle Felder aus.";
+
+$LOCALE[ERROR_BadData]  = "Geben Sie bitte gültige Daten an.";
+
+$LOCALE[ERROR_Serial_Invalid] = "Ihre Seriennummer ist ungültig, bitte registrieren Ihr Produkt erneut.";
+
+$LOCALE[ERROR_Serial_None] = "Kann keine eingetragene Seriennummer finden, bitte registrieren Ihr Produkt erneut.";
+
+$LOCALE[ERROR_Critical] = "Kritischer Fehler!";
+
+$LOCALE[ERROR_Javascript] = "Dieses Programm erfordert einen JavaScript fähigen Browser.";
+
+$LOCALE[ERROR_Browser] = "Dieses Programm wird mit Ihrem Browser nicht richtig funktionieren.";
+
+$LOCALE[ERROR_Restricted] = "Der Name, den Sie gewählt haben, ist eingeschränkt.";
+
+$LOCALE[ERROR_BadPass]  = "Tragen Sie bitte ein gültiges Passwort ein.";
+
+$LOCALE[ERROR_IP] = "Ihre IP hat sich geändert. Sie sind aus Sicherheitsgründen abgemeldet worden.";
+
+$LOCALE[ERROR_PHPV] = "MWChat erfordert zumindest die PHP Version";
+
+?>
