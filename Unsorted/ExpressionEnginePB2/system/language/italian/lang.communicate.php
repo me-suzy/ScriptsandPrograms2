6990ee6a0ceb992167f37857f10d5aa81ec8dbc2
me-suzy <me.suzana@gmail.com>
2025-08-13 04:22:45 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/italian/lang.communicate.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/italian/lang.communicate.php
@@ -0,0 +1,172 @@
+<?php
+$L = array(
+
+
+"your_name" =>
+"Il tuo nome",
+
+"your_email" =>
+"Il tuo indirizzo e-mail",
+
+"recipient" =>
+"Destinatario",
+
+"cc" =>
+"CC",
+
+"bcc" =>
+"CCN",
+
+"recipient_group" =>
+"Spedisci a queste categorie di iscritti?",
+
+"manual_recipients_ignored" =>
+"Il campo destinatario precedente verrà ignorato",
+
+"send_to_mailinglist" =>
+"Spedisci alla mailing-list?",
+
+"honor_email_pref" =>
+"Spedisci solo agli utenti che hanno dato il consenso",
+
+"separate_emails_with_comma" =>
+"Separa gli indirizzi multipli con una virgola (,)",
+
+"no_email_matching_criteria" =>
+"La ricerca non ha prodotto risultati",
+
+"not_allowed_to_email_members" =>
+"Non sei autorizzato a spedire messaggi agli utenti",
+
+"not_allowed_to_email_member_groups" =>
+"Non sei autorizzato a spedire messaggi a categorie di utenti",
+
+"not_allowed_to_email_mailinglist" =>
+"Non sei autorizzato a spedire messaggi alla mailing-list",
+
+"subject" =>
+"Oggetto",
+
+"message" =>
+"Messaggio",
+
+"send_an_email" =>
+"Spedisci un&#39;e-mail",
+
+"sending_email" =>
+"Spedizione e-mail",
+
+"batchmode_ready_to_begin" =>
+"Il processo di spedizione inizierà entro cinque secondi",
+
+"batchmode_warning" =>
+"Non agire sul browser finché il processo non è completamente terminato!",
+
+"problem_with_id" =>
+"Si è verificato un problema con il numero ID necessario alla spedizione",
+
+"cache_data_missing" =>
+"L&#39;archivio messaggi non contiene dati.",
+
+"currently_sending_batch" =>
+"Sto spedendo i messaggi da %x a %y",
+
+"emails_remaining" =>
+"Messaggi residui:",
+
+"email_error" =>
+"Errore e-mail",
+
+"send_it" =>
+"Spedisci",
+
+"total_emails_sent" =>
+"Totale messaggi spediti:",
+
+"plain_text" =>
+"Solo testo",
+
+"html" =>
+"HTML",
+
+"mail_format" =>
+"Formato",
+
+"word_wrap" =>
+"A capo forzato",
+
+"priority" =>
+"Priorità",
+
+"chars" =>
+"caratteri",
+
+"highest" =>
+"Urgente",
+
+"high" =>
+"Alta",
+
+"normal" =>
+"Normale",
+
+"low" =>
+"Bassa",
+
+"lowest" =>
+"Minima",
+
+"empty_form_fields" =>
+"Hai lasciato qualche campo vuoto.",
+
+"email_sent_message" =>
+"Il tuo messaggio è stato spedito",
+
+"all_email_sent_message" =>
+"Tutti i messaggi sono stati spediti",
+
+"email_sent" =>
+"Messaggio spedito",
+
+"view_email_cache" =>
+"Mostra l&#39;archivio messaggi",
+
+"previous_email" =>
+"Archivio messaggi",
+
+"email_title" =>
+"Titolo messaggio",
+
+"email_date" =>
+"Data di spedizione",
+
+"total_recipients" =>
+"Totale destinatari",
+
+"resend" =>
+"Rispedisci",
+
+"view" =>
+"Mostra",
+
+"no_cached_email" =>
+"Non ci sono messaggi in archivio",
+
+"delete_emails" =>
+"Cancella messaggio",
+
+"delete_confirm" =>
+"Conferma cancellazione messaggio",
+
+"delete_question" =>
+"Sei sicuro di voler cancellate i messaggi selezionati?",
+
+"email_deleted" =>
+"Messaggi cancellati",
+
+"mailinglist_unsubscribe" =>
+"Per cancellarti dalla nostra mailing-list, clicca qui:",
+
+''=>''
+);
+?>
