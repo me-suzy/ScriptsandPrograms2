e32a6fd98e259ab6f51d9325ed8514ecbe821384
me-suzy <me.suzana@gmail.com>
2025-08-13 04:18:56 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.comment.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.comment.php
@@ -0,0 +1,92 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"comment_module_name" =>
+"Kommentar",
+
+"comment_module_description" =>
+"Kommentarsystem f&uuml;r Nutzer",
+
+//----------------------------------------
+
+"cmt_name_not_allowed" =>
+"Der Name, unter dem Sie kommentieren wollen, ist reserviert. Bitte w&auml;hlen Sie einen anderen Namen.",
+
+"cmt_must_be_member" =>
+"Sie d&uuml;rfen nur als registriertes Mitglied Kommentare abgeben.",
+
+"cmt_no_authorized_for_comments" =>
+"Sie d&uuml;rfen nicht kommentieren.",
+
+"cmt_account_not_active" =>
+"Ihr Account ist noch nicht aktiviert.",
+
+"cmt_comments_not_allowed" =>
+"In diesem Weblog sind Kommentare nicht erlaubt.",
+
+"cmt_missing_name" =>
+"Sie m&uuml;ssen Ihren Namen angeben.",
+
+"cmt_missing_email" =>
+"Sie m&uuml;ssen Ihre Email-Adresse angeben.",
+
+"cmt_invalid_email" =>
+"Die angegebene Emailadresse ist nicht g&uuml;ltig.",
+
+"cmt_banned_email" =>
+"Die angegebene Emailadresse ist gesperrt.",
+
+"cmt_too_large" =>
+"Ihr Kommentar ist %n Zeichen lang. Es sind aber nur %x Zeichen erlaubt.",
+
+"cmt_comments_timelock" =>
+"Sie d&uuml;rfen nur alle %s Sekunden einen Kommentar abschicken.",
+
+"cmt_duplicate_comment_warning" =>
+"Ich kann Ihren Kommentar zur Zeit nicht empfangen.",
+
+"cmt_no_preview_template_specified" =>
+"Das Tag f&uuml;r Ihr Kommentarformular enth&auml;lt keine Angabe, wo die Vorschauvorlage zu finden ist.",
+
+"cmt_comment_response_title" =>
+"Jemand hat auf Ihren Kommentar geantwortet.",
+
+"cmt_comment_response_body" =>
+"Jemand hat gerade einen Kommentar zu dem abonnierten Eintrag abgegeben bei:",
+
+"cmt_comment_title" =>
+"Der Beitragstitel lautet:",
+
+"cmt_comment_url" =>
+"Sie finden den Kommentar unter dem folgenden URL:",
+
+"cmt_comment_removal" =>
+"Wenn Sie keine Benachrichtigungen zu diesem Kommentar mehr bekommen wollen, klicken Sie bitte hier:",
+
+"cmt_comment_removal_all" =>
+"Wenn Sie gar keine Benachrichtigungen mehr bekommen wollen, klicken Sie bitte hier:",
+
+"cmt_notification_removal" =>
+"Nachrichtenstopp",
+
+"cmt_you_have_been_removed" =>
+"Sie werden keine weiteren Benachrichtigungen mehr erhalten.",
+
+"cmt_comment_accepted" =>
+"Kommentar angenommen",
+
+"cmt_will_be_reviewed" =>
+"Ihr Kommentar wird von einem Moderator vor der Freigabe gegengelesen.",
+
+"cmt_return_to_comments" =>
+"Zur&uuml;ck zu den Kommentaren",
+
+// END
+''=>''
+);
+?>
