4bd2dc715be995854258fd424e861e2e4d70e8c4
me-suzy <me.suzana@gmail.com>
2025-08-13 06:31:47 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/lobby.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/lobby.php
@@ -0,0 +1,37 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[LOBBY_Entered_Room] = "Est entré la salle.";
+
+$LOCALE[LOBBY_Room_Full] = "La salle que vous avez demandée est pleine.";
+
+$LOCALE[LOBBY_Room_Title] = "Vous êtes maintenant dans le Vestibule";
+
+$LOCALE[LOBBY_Visitors] = "visiteurs";
+
+$LOCALE[LOBBY_Public_List] = "La liste publique de salle";
+
+$LOCALE[LOBBY_Private_List] = "La liste privée de salle";
+
+$LOCALE[LOBBY_Create_Public] = "Créer une nouvelle salle publique";
+
+$LOCALE[LOBBY_Create_Private] = "Créer une nouvelle salle privée";
+
+$LOCALE[LOBBY_Button] = "Entrer ou Créer la salle";
+
+$LOCALE[LOBBY_Room_Password] = "Mot de passe de salle";
+
+$LOCALE[LOBBY_Hidden] = "Caché";
+
+$LOCALE[LOBBY_Password_Incorrect] = "Le mot de passe que vous avez fourni pour la salle privée est inexact.";
+
+?>
