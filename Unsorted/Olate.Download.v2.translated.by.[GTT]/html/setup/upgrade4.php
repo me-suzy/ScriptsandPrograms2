e7e5ba9211464410f06d06199568bfd62841908e
me-suzy <me.suzana@gmail.com>
2025-08-13 07:07:25 +0300

    Add Unsorted/Olate.Download.v2.translated.by.[GTT]/html/setup/upgrade4.php

---
+++ Unsorted/Olate.Download.v2.translated.by.[GTT]/html/setup/upgrade4.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Setup - Upgrade - Step 4</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" type="text/css" href="css/style.css" title="default" />
+</head>
+
+<body>
+<table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0">
+<tr>
+<td valign="top"><hr width="1" size="1" color="#FFFFFF">
+<table class="admin_main" align="center" border="1">
+<tr>
+<td class="admin_title"><table class="admin_title_table">
+<tr>
+<td class="large admin_title">Olate Download - Download Management Script</td>
+</tr>
+</table></td>
+</tr>
+<tr>
+<td class="admin_breadcrumb">
+<table width="99%"  border="0" align="center" cellpadding="0" cellspacing="0">
+<tr>
+<td width="100%"><strong>Setup - Upgrade - Step 4 </strong></td>
+</tr>
+</table></td>
+</tr>
+<tr>
+<td valign="top" bordercolor="#FFFFFF"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+<tr>
+<td><p>Congratulations! The upgrade of Olate Download is now complete!</p>
+<p>Before continuing, you must delete the setup/ directory and all its contents for security reasons. Once you have done that, you can choose one of the following options:</p>
+<ul>
+<li><a href="<?=$_POST['urlpath']; ?>">Proceed to main page</a></li>
+<li><a href="<?=$_POST['urlpath']; ?>/admin/index.php">Proceed to admin area</a> </li>
+</ul>
+<p>Note: You will need to reset your General Config options via the admin area.</p>
+<p>If you want to be notified when a new update is released or an announcement is made relating to Olate Download, subscribe to the free Mailing List and get updates via e-mail! Simply enter your details into the form at <a href="http://www.olate.com/list" target="_blank">http://www.olate.com/list</a>. You can unsubscribe at any time and we keep your details secure and confidential - no releasing to 3rd parties! </p></td>
+</tr>
+</table></td>
+</tr>
+<tr>
+<td height="25" valign="middle" bordercolor="#FFFFFF" bgcolor="#E3E8EF">
+<!--Begin Credit Line. Please leave-->
+<div align="center"><span class="small"><a href="http://www.olate.com" target="_blank">Powered 
+by Olate Download v2.2.0 </a></span></div></td>
+</tr>
+</table>
+<hr width="1" size="1" color="#FFFFFF"></td>
+</tr>
+</table>
+</body>
+</html>
