d364675c3060f37922f652b552cd11898edd76c3
me-suzy <me.suzana@gmail.com>
2025-08-13 06:31:45 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/links.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/links.php
@@ -0,0 +1,23 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[LINKS_Lobby] = "Se retourner au Vestibule";
+
+$LOCALE[LINKS_Options] = "Regarder des préférences";
+
+$LOCALE[LINKS_Register] = "Enregistrer ce nom";
+
+$LOCALE[LINKS_My_Files] = "Mes fichiers en ligne";
+
+$LOCALE[LINKS_Upload] = "L'envoyer un fichier";
+
+?>
