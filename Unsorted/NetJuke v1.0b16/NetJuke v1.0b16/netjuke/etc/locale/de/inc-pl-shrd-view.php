445faf46ec9ec9973a979e5337127c569d2e2a99
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:01 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-pl-shrd-view.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-pl-shrd-view.php
@@ -0,0 +1,80 @@
+<?php
+
+##################################################
+
+# /pl-shrd-view.php
+
+##################################################
+
+define( "PLSHRD_NOPLID", "Bite wählen Sie eine Spielliste zum anzeigen aus." );
+
+define( "PLSHRD_SAVE_NOID", "Bite wählen Sie eine Spielliste aus, die unter Ihren Favoriten gespeichert werden soll." );
+
+define( "PLSHRD_DELPL_NOID", "Bite wählen Sie eine Spielliste aus, die aus Ihren Favoriten gelöscht werden soll." );
+define( "PLSHRD_DELPL_CONFIRM", "Sind Sie sciher das die Spielliste aus Ihren Favoriten gelöscht werden soll.?" );
+
+define( "PLSHRD_CHCK_HELP", "Klicken um diesen Titel zu löschen" );
+define( "PLSHRD_PLAY_HELP", "Klicken um diesen Titel anzuhören" );
+define( "PLSHRD_DLOAD_HELP", "Klicken um diesen Titel herunterzuladen" );
+define( "PLSHRD_INFO_HELP", "Klicken um die Information dieses Titels anzuzeigen." );
+define( "PLSHRD_AR_HELP", "Klicken um alle Titel dieses Interpreten anzuzeigen." );
+define( "PLSHRD_AL_HELP", "Klicken um alle Titel dieses Albums anzuzeigen." );
+define( "PLSHRD_GE_HELP", "Klicken um alle Titel dieses Genres anzuzeigen." );
+
+define( "PLSHRD_HEADER_FAV", "LIEBLINGS SPIELLISTE" );
+define( "PLSHRD_HEADER_PUB", "ÖFFENTLICHE SPIELLISTE" );
+define( "PLSHRD_HEADER_BY", "Von" );
+
+define( "PLSHRD_BTN_DELETE", "Löschen" );
+define( "PLSHRD_BTN_CANCEL", "Abbrechen" );
+
+define( "PLSHRD_ROWCNT_1", "Gespeicherte Titel" );
+define( "PLSHRD_ROWCNT_2", "von" );
+
+define( "PLSHRD_QS_AR", "Interpret" );
+define( "PLSHRD_QS_TR", "Titel" );
+define( "PLSHRD_QS_AL", "Album" );
+define( "PLSHRD_QS_GE", "Genre" );
+define( "PLSHRD_QS_TI", "Dauer" );
+define( "PLSHRD_QS_BTN", "Suchen" );
+
+define( "PLSHRD_TB_PLIST_BTN", "Bearbeiten" );
+define( "PLSHRD_TB_ADDED", "Hinzugefügt" );
+define( "PLSHRD_TB_PLAY_ALL", "Anhören" );
+define( "PLSHRD_TB_PLAY_ALL_HELP", "Klicken um diese Spielliste anzuhören" );
+define( "PLSHRD_TB_SAVE", "Als Favorit speichern" );
+define( "PLSHRD_TB_SAVE_HELP", "Klicken um diese Spielliste unter den Favoriten zu speichern" );
+define( "PLSHRD_TB_SELECT", "Alle auswählen" );
+define( "PLSHRD_TB_SELECT_HELP", "Klicken um alle Titel dieser Seite auszuwählen" );
+define( "PLSHRD_TB_RESET", "Alle abwählen" );
+define( "PLSHRD_TB_RESET_HELP", "Klicken um alle Titel dieser Seite abzuwählen" );
+define( "PLSHRD_TB_PLAY", "Ausgewählt anhören" );
+define( "PLSHRD_TB_PLAY_HELP", "Klicken um die ausgewählten Titel anzuhören" );
+define( "PLSHRD_TB_ADDTO", "Zur Spielliste kopieren" );
+define( "PLSHRD_TB_ADDTO_HELP", "Klicken um die ausgewählten Titel zur gewählten Spielliste hinzuzufügen" );
+
+define( "PLSHRD_COLS_OP", "Optionen" );
+define( "PLSHRD_COLS_SQ", "Nr." );
+define( "PLSHRD_COLS_SQ_HELP", "Klicken um die Seite nach der gespeicherten Reihenfolge zu sortieren" );
+define( "PLSHRD_COLS_TR", "Titel Name" );
+define( "PLSHRD_COLS_TR_HELP", "Klicken um die Seite nach dem Titel zu sortieren" );
+define( "PLSHRD_COLS_TI", "Dauer" );
+define( "PLSHRD_COLS_TI_HELP", "Klicken um die Seite nach der Dauer zu sortieren" );
+define( "PLSHRD_COLS_AR", "Interpret" );
+define( "PLSHRD_COLS_AR_HELP", "Klicken um die Seite nach dem Interpet zu sortieren" );
+define( "PLSHRD_PLAY_AR_HELP", "Klicken um alle Titel des Interpreten an zu hören" );
+define( "PLSHRD_FILTER_AR_HELP", "Klicken um alle Alben des Interpreten anzuzeigen." );
+define( "PLSHRD_COLS_AL", "Album" );
+define( "PLSHRD_COLS_AL_HELP", "Klicken um die Seite nach dem Album zu sortieren" );
+define( "PLSHRD_PLAY_AL_HELP", "Klicken um alle Titel des Albums an zu hören" );
+define( "PLSHRD_FILTER_AL_HELP", "Klicken um alle Interpreten dieses Albums anzuzeigen." );
+define( "PLSHRD_COLS_TN", "#" );
+define( "PLSHRD_COLS_TN_HELP", "Klicken um die Seite nach der Titel-Nummer zu sortieren" );
+define( "PLSHRD_COLS_GE", "Genre" );
+define( "PLSHRD_COLS_GE_HELP", "Klicken um die Seite nach dem Genre zu sortieren" );
+define( "PLSHRD_PLAY_GE_HELP", "Klicken um alle Titel des Genres an zu hören" );
+
+
+##################################################
+
+?>
