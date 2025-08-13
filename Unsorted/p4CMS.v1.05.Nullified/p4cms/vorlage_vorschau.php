e7782e73651f251831253da6446d40f6210c95fa
me-suzy <me.suzana@gmail.com>
2025-08-13 07:33:59 +0300

    Add Unsorted/p4CMS.v1.05.Nullified/p4cms/vorlage_vorschau.php

---
+++ Unsorted/p4CMS.v1.05.Nullified/p4cms/vorlage_vorschau.php
@@ -0,0 +1,6 @@
+<script type="text/javascript" language="JavaScript">
+<!--
+var header = opener.document.vorlage.vorlageneditor.value;
+document.writeln("" + opener.document.vorlage.vorlageneditor.value + "");
+//-->
+</script> 
