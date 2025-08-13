985cb8ea7b8a11c579326a5d3e525be4e9cfb680
me-suzy <me.suzana@gmail.com>
2025-08-13 06:35:12 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/whois.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/whois.php
@@ -0,0 +1,19 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[WHOIS_Title] = "ðÁÒÁÍÅÔÒÙ ÐÏÌØÚÏ×ÁÔÅÌÑ MWChat!";
+
+$LOCALE[WHOIS_No_Profile] = "ÎÅ Ñ×ÌÑÅÔÓÑ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎÎÙÍ ÐÏÌØÚÏ×ÁÔÅÌÅÍ É ÎÅ ÉÍÅÅÔ ÐÁÒÁÍÅÔÒÏ×.";
+
+$LOCALE[WHOIS_Not_Specified] = "îÅ ÕËÁÚÁÎÏ";
+
+?>
