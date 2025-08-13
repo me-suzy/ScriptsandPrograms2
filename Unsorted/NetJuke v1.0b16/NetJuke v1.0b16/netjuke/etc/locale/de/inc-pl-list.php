c7994b2fa8966583e05fd84178ad304aa9aa1dea
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:58 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-pl-list.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-pl-list.php
@@ -0,0 +1,24 @@
+<?php
+
+##################################################
+
+# /pl-list.php
+
+##################################################
+
+define( "PLIST_PLAY_HELP", "Klicken um diese Spielliste an zu hören" );
+define( "PLIST_EDIT_HELP", "Klicken um diese Spielliste zu bearbeiten" );
+define( "PLIST_VIEW_HELP", "Klicken um diese Spielliste anzuzeigen" );
+
+define( "PLIST_PRVT_HEADER", "PRIVATE SPIELLISTEN" );
+define( "PLIST_PRVT_HELP", "Private Spiellisten können nur durch Sie betrachtet, editiert, zugegriffen und gelöscht werden." );
+
+define( "PLIST_SHRD_HEADER", "GEMEINSAME SPIELLISTEN" );
+define( "PLIST_SHRD_HELP", "Gemeinsame Spiellisten können nur durch Sie editiert und glöscht werden, werden aber im Gemeinschaftsbereich angezeigt." );
+
+define( "PLIST_FAV_HEADER", "SPIELLISTEN ANDERER" );
+define( "PLIST_FAV_HELP", "Gemeinsame Spiellisten anderer Benutzer." );
+
+##################################################
+
+?>
