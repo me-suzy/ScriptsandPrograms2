1d706ca1809e94746f3f9a0140ac685a8895f132
me-suzy <me.suzana@gmail.com>
2025-08-13 06:32:28 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/login.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ge/login.php
@@ -0,0 +1,17 @@
+<?
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[LOGIN_Password] = "Tragen Sie Ihr Passwort ein";
+
+$LOCALE[LOGIN_Title] = "Dieser Account wird registriert";
+
+?>
