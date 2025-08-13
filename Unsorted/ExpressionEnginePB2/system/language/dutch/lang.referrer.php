7c443eb61fa3b4edafe6895ac6ae54d1cee9af76
me-suzy <me.suzana@gmail.com>
2025-08-13 04:20:28 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/dutch/lang.referrer.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/dutch/lang.referrer.php
@@ -0,0 +1,85 @@
+<?php
+$L = array(
+
+
+"referrer_module_name" =>
+"Referrer",
+
+"referrer_module_description" =>
+"Referrer tracking module",
+
+"referrer_ip" =>
+"Referrer IP",
+
+"referrer_date" =>
+"Referrer Date",
+
+"referrers_deleted" =>
+"Refferers have been deleted",
+
+"pmachine_blacklist" =>
+"Download pMachine Blacklist",
+
+"blacklist_updated" =>
+"Blacklist updated successfully",
+
+'ref_no_license' =>
+"Error: no license number.",
+
+'ref_blacklist_irretrievable' =>
+"Error: new blacklist was irretrievable.",
+
+'ref_view_blacklist' =>
+"Referrer Blacklist",
+
+'ref_no_blacklist' =>
+"There is currently nothing blacklisted",
+
+'ref_ip' =>
+"IP Address",
+
+'ref_user_agent' =>
+"User Agent",
+
+'ref_url' =>
+"URL",
+
+'ref_blacklisted' =>
+"Blacklisted",
+
+'ref_no_blacklist_table' =>
+"Blacklist database table does not exist",
+
+'ref_type' =>
+"Type",
+
+"referrers" =>
+"Referrers",
+
+"view_referrers" =>
+"Bekijk Referrers",
+
+"clear_referrers" =>
+"Opschonen Referrers",
+
+"referrer_from" =>
+"Referrer Van",
+
+"referrer_to" =>
+"Referrer Naar",
+
+"no_referrers" =>
+"Er zijn op dit moment geen referrers",
+
+"total_referrers" =>
+"Totaal aantal Referrers:",
+
+"save_instructions" =>
+"Hoe veel van de meest recente referrers wilt u bewaren?",
+
+"referrers_deleted" =>
+"Refferers zijn verwijderd",
+
+''=>''
+);
+?>
