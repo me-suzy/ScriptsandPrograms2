007e4a3fc07b9ea3d89527dbd25bdbb031488118
me-suzy <me.suzana@gmail.com>
2025-08-13 06:34:41 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/defs.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/defs.php
@@ -0,0 +1,55 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[DEFS_addbuddy] = "{ $LOCALE[COMMON_Username] }";
+
+$LOCALE[DEFS_admin] = "{ viewlogs | unregister } [ $LOCALE[COMMON_Username] ]";
+
+$LOCALE[DEFS_afk] = "[ $LOCALE[COMMON_Text] ]";
+
+$LOCALE[DEFS_bug] = "{ $LOCALE[COMMON_Bug_Report] }";
+
+$LOCALE[DEFS_color] = "{ #000000 }";
+
+$LOCALE[DEFS_deop] = "{ $LOCALE[COMMON_Username] }";
+
+$LOCALE[DEFS_op] = "{ $LOCALE[COMMON_Username] }";
+
+$LOCALE[DEFS_global] = "{ $LOCALE[COMMON_Text] }";
+
+$LOCALE[DEFS_ignore] = "{ $LOCALE[COMMON_Username] }";
+
+$LOCALE[DEFS_kick] = "{ $LOCALE[COMMON_Username] } [ $LOCALE[COMMON_Reason] ]";
+
+$LOCALE[DEFS_lobby] = "[ $LOCALE[COMMON_Text] ]";
+
+$LOCALE[DEFS_locate] = "{ $LOCALE[COMMON_Username] }";
+
+$LOCALE[DEFS_logout] = "[ $LOCALE[COMMON_Text] ]";
+
+$LOCALE[DEFS_me] = "{ $LOCALE[COMMON_Action] }";
+
+$LOCALE[DEFS_msg] = "{ $LOCALE[COMMON_Username] } { $LOCALE[COMMON_Text] }";
+
+$LOCALE[DEFS_removebuddy] = "{ $LOCALE[COMMON_Username] }";
+
+$LOCALE[DEFS_topic] = "{ $LOCALE[COMMON_Topic] }";
+
+$LOCALE[DEFS_unafk] = "[ $LOCALE[COMMON_Text] ]";
+
+$LOCALE[DEFS_unignore] = "{ $LOCALE[COMMON_Username] }";
+
+$LOCALE[DEFS_url] = "{ $LOCALE[COMMON_URL] }";
+
+$LOCALE[DEFS_whois] = "{ $LOCALE[COMMON_Username] }";
+
+?>
