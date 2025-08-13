34b236fc0b07662073e9da7b398adb4427772253
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:38 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_text-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_text-edit.php
@@ -0,0 +1,24 @@
+<?php
+
+##################################################
+
+# /admin/text-edit.php
+
+##################################################
+
+define( "TXTEDIT_NOID", "Please select a valid track." );
+
+define( "TXTEDIT_HEADER_COMMENTS", "COMMENTS" );
+define( "TXTEDIT_HEADER_LYRICS", "LYRICS" );
+
+define( "TXTEDIT_FORM_BTN_SAVE", "Save" );
+define( "TXTEDIT_FORM_BTN_RESET", "Reset" );
+
+define( "TXTEDIT_PAGETOP", "Page Top" );
+define( "TXTEDIT_PAGETOP_HELP", "Scrolls Back To The Top Of The Page" );
+define( "TXTEDIT_CLOSEWIN", "Close Window" );
+define( "TXTEDIT_CLOSEWIN_HELP", "Closes This Window" );
+
+##################################################
+
+?>
