cdb556454a26ac902305d589404f16229a965971
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:00 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-account.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-account.php
@@ -0,0 +1,44 @@
+<?php
+
+##################################################
+
+# /account.php
+
+##################################################
+
+define( "ACCNT_CHECKFORM_1", "Bitte geben Sie Ihren Namen ein." );
+define( "ACCNT_CHECKFORM_2", "Bitte geben Sie Ihre email adresse ein." );
+define( "ACCNT_CHECKFORM_3", "Bitte geben Sie eine gültige email adresse ein." );
+define( "ACCNT_CHECKFORM_4", "Bitte geben Sie ein Passwort ein." );
+define( "ACCNT_CHECKFORM_5", "Bitte geben Sie das Passwort erneut ein." );
+define( "ACCNT_CHECKFORM_6", "Das neue Passwort ist zu kurz.\\n   Bitte benutzen Sie mindestens 6 Zeichen." );
+define( "ACCNT_CHECKFORM_7", "Das neue Passwort ist zu lang.\\n   Bitte benutzen Sie maximal 20 Zeichen." );
+define( "ACCNT_CHECKFORM_8", "Die Passwörter stimmen nicht überein.\\n   Bitte wiederholen Sie Ihr neues Passwort." );
+define( "ACCNT_CHECKFORM_9", "Bitte geben Sie Ihr bestehendes Passwort ein, um die Änderungen zu speichern" );
+
+define( "ACCNT_DENIED", "Diese email-Adresse und/oder dieser Benutzernamen sind bereits registriert." );
+
+define( "ACCNT_NEW_HEADER", "BENUTZERKONTO ERSTELLEN" );
+define( "ACCNT_NEW_CAPTION", "Bitte die Felder unten ausfüllen um ein neues Benutzerkonto einzurichten." );
+define( "ACCNT_NEW_SUCCESS_1", "GRATULATION! Sie wurden registriert als:" );
+define( "ACCNT_NEW_SUCCESS_2", "Bitte wählen Sie eine Option aus dem Menübalken aus um fortzufahren." );
+
+define( "ACCNT_EDIT_HEADER", "BENUTZERKONTO INFORMATION" );
+define( "ACCNT_EDIT_CAPTION", "Benutzen Sie die Felder um Änderungen an ihrem Benutzerkonto vorzunehmen." );
+
+define( "ACCNT_FORMS_NAME", "Nachname, Vorname" );
+define( "ACCNT_FORMS_NICKNAME", "Benutzername" );
+define( "ACCNT_FORMS_EMAIL", "Email-Adresse" );
+define( "ACCNT_FORMS_PASSWD_1", "Passwort" );
+define( "ACCNT_FORMS_PASSWD_2", "Passwort bestätigen" );
+define( "ACCNT_FORMS_PASSWD_3", "Aktuelles Passwort" );
+define( "ACCNT_FORMS_PASSWD_4", "Neues Passwort" );
+define( "ACCNT_FORMS_PASSWD_5", "Neues Passwort bestätigen" );
+define( "ACCNT_FORMS_BTN_LOGIN", "Konto erstellen" );
+define( "ACCNT_FORMS_BTN_REGISTER", "Anmelden" );
+define( "ACCNT_FORMS_BTN_SAVE", "Speichern" );
+define( "ACCNT_FORMS_BTN_RESET", "Zurücksetzen" );
+
+##################################################
+
+?>
