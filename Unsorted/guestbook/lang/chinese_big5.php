a5a1604aac2de55e8e77afaaceb22748bf00151d
me-suzy <me.suzana@gmail.com>
2025-08-13 05:00:50 +0300

    Add Unsorted/guestbook/lang/chinese_big5.php

---
+++ Unsorted/guestbook/lang/chinese_big5.php
@@ -0,0 +1,101 @@
+<?php
+# General
+
+$LANG["metatag"]    = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=big5\">";
+
+$LANG["BookMess1"]  = "HTML code Ãö³¬";
+$LANG["BookMess2"]  = "HTML code ¶}±Ò";
+$LANG["BookMess3"]  = "§Ú­n¯d¨¥";
+$LANG["BookMess4"]  = "¦^¨ì¯d¨¥ªO";
+$LANG["BookMess5"]  = "ºÞ²zªÌ";
+$LANG["BookMess6"]  = "·PÁÂ±zªº°±¯d¡A¦¹³B¯à°÷Åý§AÀH·N¶î¾~¡C";
+$LANG["BookMess7"]  = "¯d¨¥¤º®e";
+$LANG["BookMess8"]  = "¦^¨ì°e¥Xªí³æ";
+$LANG["BookMess9"]  = "¦^ÂÐ¦¹½g¯d¨¥";
+$LANG["BookMess10"] = "<u>·PÁÂ±zªºÄéµ@¡C</u><br><br>±zªº¯d¨¥¤w¥[¤J¦¨¥\! 2¬í«á±N¦^¨ì¯d¨¥ªO¡C";
+
+$LANG["EmailAdminSubject"] = "·s¯d¨¥ªO";
+$LANG["EmailGuestSubject"] = "·PÁÂ±zªº¯d¨¥";
+
+$LANG["AltIP"]    = "IP°O¿ý";
+$LANG["AltICQ"]   = "ICQ";
+$LANG["AltEmail"] = "¼g«Hµ¹¹ï¤è";
+$LANG["AltUrl"]   = "­º­¶";
+$LANG["AltAim"]   = "Instant Messenger";
+$LANG["AltCom"]   = "¦^ÂÐ";
+
+# Form
+$LANG["FormMess1"]   = "½Ð¶ñ¼g±zªº¯d¨¥¡A°e¥X«á±z±N·|¦^¨ì¯d¨¥ªO¡C¦³(*)ªÌ¬°¥²¶ñÄæ¦ì¡C";
+$LANG["FormMess2"]   = "ªí±¡¹Ï®× ¶}±Ò";
+$LANG["FormMess3"]   = "AGCode ¶}±Ò";
+$LANG["FormMess4"]   = "½d¨Ò";
+$LANG["FormMess5"]   = "¤U¤è¬O¹wÄýµe­±";
+$LANG["FormMess6"]   = "AGCode Ãö³¬";
+$LANG["FormMess7"]   = "ªí±¡¹Ï®× Ãö³¬";
+$LANG["FormName"]    = "³X«È©m¦W";
+$LANG["FormEmail"]   = "¹q¤l¶l¥ó";
+$LANG["FormLoc"]     = "¦aÂI";
+$LANG["FormPic"]     = "¹Ï¤ù";
+$LANG["FormUrl"]     = "­º­¶";
+$LANG["FormGender"]  = "©Ê§O";
+$LANG["FormMale"]    = "¨k";
+$LANG["FormFemale"]  = "¤k";
+$LANG["FormMessage"] = "¯d¨¥¤º®e";
+$LANG["FormSelect"]  = "¯d¨¥ªO";
+$LANG["FormUser"]    = "±b¸¹";
+$LANG["FormPass"]    = "±K½X";
+$LANG["FormReset"]   = "²M°£";
+$LANG["FormSubmit"]  = "°e¥X";
+$LANG["FormPreview"] = "¹wÄý";
+$LANG["FormBack"]    = "¦^¤W­¶";
+$LANG["FormEnter"]   = "¯d¨¥ªO - ºÞ²z¤¤¤ß";
+$LANG["FormButton"]  = "½T©w";
+$LANG["FormPriv"]    = "¨p±K°T®§(¯d¨¥µ¹ºÞ²zªÌ)";
+
+# Navigation Bar
+$LANG["NavTotal"]   = "¯d¨¥Á`¼Æ:";
+$LANG["NavRecords"] = "¨C­¶¯d¨¥¼Æ:";
+$LANG["NavPrev"]    = "¤W¤@­¶";
+$LANG["NavNext"]    = "¤U¤@­¶";
+
+# Post Errors
+$LANG["ErrorPost1"]  = "½Ð¶ñ¼g³X«È©m¦W";
+$LANG["ErrorPost2"]  = "½Ð¶ñ¼g¯d¨¥¤º®e";
+$LANG["ErrorPost3"]  = "¯d¨¥¤º®e¤Óµu©Î¤Óªø";
+$LANG["ErrorPost4"]  = "¦³Äæ¦ì¿é¤J¤£¥¿½T";
+$LANG["ErrorPost5"]  = "¯d¨¥ªO - ¿ù»~";
+$LANG["ErrorPost6"]  = "¹ÏÀÉ¤Ó¤j";
+$LANG["ErrorPost7"]  = "¹ÏÀÉ®æ¦¡¤£¥¿½T";
+$LANG["ErrorPost8"]  = "¾ã²z¤¤¡A½Ðµy«á¦A¨Ï¥Î!";
+$LANG["ErrorPost9"]  = "±zªºIP³Q©Ú!";
+$LANG["ErrorPost10"] = "¯d¨¥¤º®e¸Ì¦³¤@¨Ç¤£³Q±µ¨üªº¤å¦r¡A½Ð­«·s¿é¤J!";
+$LANG["ErrorPost11"] = "½Ð¿é¤J¯d¨¥¤º®e";
+
+$LANG["PassMess1"] = "½Ð¿é¤J¦Xªkªº±b¸¹©M±K½X:";
+$LANG["PassMess2"] = "¤£¥¿½Tªº±b¸¹©Î±K½X¡A½Ð­«·s¿é¤J¡C";
+$LANG["PassMess3"] = "©êºp¡A±zªº±K½X¤£¥¿½T¡C";
+
+# Days
+$weekday[0] = "¬P´Á¤é";
+$weekday[1] = "¬P´Á¤@";
+$weekday[2] = "¬P´Á¤G";
+$weekday[3] = "¬P´Á¤T";
+$weekday[4] = "¬P´Á¥|";
+$weekday[5] = "¬P´Á¤­";
+$weekday[6] = "¬P´Á¤»";
+
+# Months
+$months[0]  = "¤@¤ë";
+$months[1]  = "¤G¤ë";
+$months[2]  = "¤T¤ë";
+$months[3]  = "¥|¤ë";
+$months[4]  = "¤­¤ë";
+$months[5]  = "¤»¤ë";
+$months[6]  = "¤C¤ë";
+$months[7]  = "¤K¤ë";
+$months[8]  = "¤E¤ë";
+$months[9]  = "¤Q¤ë";
+$months[10] = "¤Q¤@¤ë";
+$months[11] = "¤Q¤G¤ë";
+
+?>
