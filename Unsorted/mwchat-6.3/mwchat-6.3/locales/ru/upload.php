7f371aadff46595b6d4256006d5197c8ec4b69da
me-suzy <me.suzana@gmail.com>
2025-08-13 06:35:09 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/upload.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/ru/upload.php
@@ -0,0 +1,35 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[UPLOAD_Title] = "ðÏÓÌÁÔØ ÆÁÊÌ!";
+
+$LOCALE[UPLOAD_Info] = "ðÏÖÁÌÕÊÓÔÁ, ×ÙÂÅÒÉÔÅ ÆÁÊÌ ÄÌÑ ÚÁËÁÞËÉ É ÐÏÌØÚÏ×ÁÔÅÌÑ, ÄÌÑ ËÏÔÏÒÏÇÏ ÏÎ ÐÒÅÄÎÁÚÎÁÞÅÎ.";
+
+$LOCALE[UPLOAD_Button] = "úÁËÁÞÁÔØ ÓÅÊÞÁÓ";
+
+$LOCALE[UPLOAD_File_Send] = "æÁÊÌ ÄÌÑ ÚÁËÁÞËÉ";
+
+$LOCALE[UPLOAD_User_Send] = "ðÏÓÌÁÔØ";
+
+$LOCALE[UPLOAD_Complete] = "÷ÁÛ ÆÁÊÌ ÕÓÐÅÛÎÏ ÐÏÓÌÁÎ!";
+
+$LOCALE[UPLOAD_Sent_File] = "ñ ÔÏÌØËÏ ÞÔÏ ÐÏÓÌÁÌ ÔÅÂÅ ÆÁÊÌ";
+
+$LOCALE[UPLOAD_Err_Large] = "÷ÙÂÒÁÎÎÙÊ ×ÁÍÉ ÆÁÊÌ ÓÌÉÛËÏÍ ×ÅÌÉË ÉÌÉ ÚÁËÁÞÁÌÓÑ Ó ÏÛÉÂËÁÍÉ.";
+
+$LOCALE[UPLOAD_Err_Accept] = "ðÏÌØÚÏ×ÁÔÅÌØ ÎÅ ÚÁÒÅÇÉÓÔÒÉÒÏ×ÁÎ ÉÌÉ ÎÅ ÐÒÉÎÉÍÁÅÔ ÆÁÊÌÙ.";
+
+$LOCALE[UPLOAD_Err_Exists] = "æÁÊÌ, ËÏÔÏÒÙÊ ×Ù ÐÏÓÙÌÁÅÔÅ, ÕÖÅ ÓÕÝÅÓÔ×ÕÅÔ.";
+
+$LOCALE[UPLOAD_Err_Full] = "õ ÐÏÌØÚÏ×ÁÔÅÌÑ ÎÅÔ Ó×ÏÂÏÄÎÏÇÏ ÄÉÓËÏ×ÏÇÏ ÐÒÏÓÔÒÁÎÓÔ×Á ÄÌÑ ×ÁÛÅÇÏ ÆÁÊÌÁ.";
+
+?>
