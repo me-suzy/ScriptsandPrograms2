83b7919ec38233e1f70f8cf3281167289458586b
me-suzy <me.suzana@gmail.com>
2025-08-13 06:31:40 +0300

    Add Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/help.php

---
+++ Unsorted/mwchat-6.3/mwchat-6.3/locales/fr/help.php
@@ -0,0 +1,205 @@
+<?PHP
+
+/*
+        |: MWChat (My Web based Chat)
+        |: Web\HTTP based chat application
+        |:
+        |: Copyright (C) 2000, 2001, 2002, 2003
+        |: Distributed under the terms of license provided.
+        |: Available at http://www.appindex.net
+        |: Authored by Appindex.net - <support@appindex.net>
+*/
+
+$LOCALE[HELP_Title] = "Aider sur";
+
+$LOCALE[HELP_Unavailable] = "Une description serviable de cette question est indisponible.";
+
+$LOCALE[HELP_Username] = "Un nom ou username sont la personne que vous souhaitez être sue comme pendant que dans la conversation. Similaire à un surnom, ceci peut être votre vrai nom ou un celui a formé.";
+
+$LOCALE[HELP_Language] = "MWChat a la capacité pour montrer plusieurs langues différentes. Cette boîte vous permet de choisir la langue n'importe quelle que vous préférez utiliser.";
+
+$LOCALE[HELP_Password] = "L'administrateur, l'Opérateur, et les comptes enregistrés exige un mot de passe à login. Ceci empêche l'accès inautorisé à votre compte.";
+
+$LOCALE[HELP_Lobby_Public] = "La liste publique de salle est une sélection de diverses salles que vous pouvez bavarder dans. Choisir une salle ce intrests vous la plupart des ou créer votre propre.";
+
+$LOCALE[HELP_Lobby_Private] = "La liste privée de salle est cachée aux utilisateurs standard. Si vous souhaitez bavarder dans une salle privée, entrez le nom de la salle demandée au dessous. Les salles privées ne sont pas montrées sur la liste de salle, donc vous pouvez tenir des conversations privées sans les utilisateurs de uninvited marchants dans sur vous.";
+
+$LOCALE[HELP_Lobby_Public_Create] = "Si vous ne pouvez pas trouver une salle qui s'intéresse vous dans la liste au-dessus, vous pouvez créer votre propre salle en tapant le nom de la salle demandée dans le champs au dessous.";
+
+$LOCALE[HELP_Lobby_Private_Create] = "Entrer le nom de la salle privée que vous souhaitez entrer. Si la salle n'existe pas, il sera créé pour vous.";
+
+$LOCALE[HELP_Private_Message] = "Choisir un utilisateur énuméré dans ce dépot cette en bas boîte. N'importe quels messages que vous envoyez iront seulement à l'utilisateur de specifed. Pour changer le dos à la salle entière, choisir le défaut précipiter. Messaging privé peut être fait aussi l'utilisation du/l'ordre de msg.";
+
+$LOCALE[HELP_Private_Message_Refresh] = "Cliqueter ce lien pour rafraîchir la liste d'utilisateur de message privée.";
+
+$LOCALE[HELP_Colors] = "Si vous aimeriez changer la couleur du texte que vous entrez, le déclic sur n'importe quelle couleur dans le discrimination raciale. Vous verrez un message qui dit que vos couleurs ont été mises à jour. Vous pouvez cliqueter n'importe où dans la moitié plus basse du discrimination raciale pour restaurer la couleur implicite.";
+
+$LOCALE[HELP_Enter_Password] = "Entrer un nouveau mot de passe que vous aimeriez utiliser. Ce mot de passe peut être n'importe quel alpha n'importe quel texte numérique. S'assure que vous notez ce mot de passe et le gardez dans un endroit sûr. Il est exigé que le mot de passe soit cinq ou plus de caractères dans la longueur.";
+
+$LOCALE[HELP_Icons] = "Choisir une icône de la liste au dessous de vous représenter dans cette conversation. Tous utilisateurs peuvent voir votre icône.";
+
+$LOCALE[HELP_Theme] = "Choisir un thème de la liste au dessous. Un thème changera toutes vos couleurs de conversation. Vous pouvez avoir besoin de se retourner au vestibule, et l'alors dos dans la salle pour tous changements de thème pour entrer en vigueur.";
+
+$LOCALE[HELP_Confirm_Password] = "Entrer le nouveau mot de passe que vous avez choisi encore ici. Ceci confirme que vous n'avez pas fait une faute de frappe et assurez vous sait que votre mot de passe est.";
+
+$LOCALE[HELP_YOB] = "Ceci est l'année de naissance. Cette information est utilisée à l'âge de caculate dans quelque cas, comme quelques lois peuvent exiger que les utilisateurs pour ait été d'un certain encore.";
+
+$LOCALE[HELP_Occupation] = "Ceci est l'occupation d'utilisateurs ou le travail. D'habitude leur source principale de revenu.";
+
+$LOCALE[HELP_Hobbies] = "Ceci est des choses qu'un utilisateur apprécie faire. La plupart du temps dépensé sur les passe-temps est d'habitude épargne le temps.";
+
+$LOCALE[HELP_Location] = "Ceci est l'emplacement géographique de l'utilisateur.";
+
+$LOCALE[HELP_Comments] = "Ceci est n'importe quels commentaires ou n'importe quelles opinions que l'utilisateur peut avoir ou peut souhaiter à la voix.";
+
+$LOCALE[HELP_Status] = "Ceci est les utilisateurs le statut actuel. D'habitude déclarer si un utilisateur est loin, en ligne, enregistré, un administrateur, ou un opérateur.";
+
+$LOCALE[HELP_Upload_File] = "Ceci est le fichier que vous voulez envoyer à un utilisateur. Cliqueter le brouter le bouton pour choisir le fichier que vous souhaitez envoyer.";
+
+$LOCALE[HELP_Upload_User] = "Ceci est l'utilisateur que vous voulez envoyer un fichier à.";
+
+$LOCALE[HELP_Enter_Message] = "Quand vous entrez une salle, ce message sera envoyé avec la notification que vous êtes entré la salle.";
+
+$LOCALE[HELP_Exit_Message] = "Quand vous sortez une salle, ce message sera envoyé avec la notification que vous avez partez la salle.";
+
+$LOCALE[HELP_Away_Message] = "Quand vous partez, ce message sera envoyé avec la notification qui votre loin.";
+
+$LOCALE[HELP_Back_Message] = "Quand votre dos d'est loin, ce message sera envoyé avec la notification qui votre dos.";
+
+$LOCALE[HELP_Default_Color] = "Si vous voulez qu'une couleur implicite être utilisée chaque temps vous entrez une salle, cette option vous permettra de le régler.";
+
+$LOCALE[HELP_Smiles] = "Cette option bascule votre capacité pour voir graphique ou les faces de smiley de texte dans la conversation.";
+
+$LOCALE[HELP_Locate] = "Cette option bascule votre capacité pour permettre ou rejeter des utilisateurs pour vous localiser. Localiser un utilisateur dit quelle salle vous êtes-il actuellement dans.";
+
+$LOCALE[HELP_Upload] = "Cette option bascule votre capacité pour recevoir des fichiers des autres utilisateurs.";
+
+$LOCALE[HELP_Room] = "Ceci est la salle que vous êtes actuellement dans, ou la salle que vous souhaitez créer ou entrer.";
+
+$LOCALE[HELP_Room_Password] = "Quelques salles privées sont des mots de passe protégées. Si la salle que vous souhaitez entrer est le mot de passe protégée, l'entrez ici.";
+
+$LOCALE[HELP_Number_Users] = "Ceci est le numéro total d'utilisateurs qui sont actuellement dans la salle.";
+
+$LOCALE[HELP_Topic] = "Ceci est le sujet actuel de la salle.";
+
+$LOCALE[HELP_Buddylist] = "Ceci est votre liste de copain. Ceci contient le usernames de tous utilisateurs dans cette salle, et n'importe quels copains que vous pouvez avoir. Vous pouvez cliqueter sur chaque nom pour recevoir l'information de cet utilisateur s'ils sont enregistrés.";
+
+$LOCALE[HELP_Serial_Number] = "Ceci est le numéro de série de MWChat. Vous pouvez obtenir un numéro de série à http: //www.appindex.net/mwchat";
+
+$LOCALE[HELP_Storage] = "Ceci est votre information d'emmagasinage de fichier. Le premier numéro montre combien l'espace vous avez disponible pour l'usage. Les deuxièmes expositions de numéro que votre total a permise à l'emmagasinage est. Au dessous d'est une liste de n'importe quels fichiers que vous pouvez avoir. Le déclic sur un nom de fichier pour charger ce fichier, ou cliqueter la case de pointage pour enlever le fichier. Vous pouvez voir aussi la taille de chaque fichier, quand vous avez reçu le fichier, et qui a envoyé vous le fichier.";
+
+$LOCALE[HELP_ERROR_License] = "Le permis que vous avez est inexact, ou pas la sortie. Retry votre permis et s'il ne travaille pas reçoit toujours un nouvel Appindex Filet de numéro de série ou contact soutien de.";
+
+$LOCALE[HELP_ERROR_AlreadyIn] = "Quelqu'un est déjà abonné avec ce username. C'est possible si vous n'avez pas deconnecté correctement, que votre compte n'a pas tourné au ralenti hors. Attendre dix minutes et essayer encore.";
+
+$LOCALE[HELP_ERROR_Logout] = "Votre compte a été hors noté. Vous pouvez login encore si vous aimez.";
+
+$LOCALE[HELP_ERROR_Expired] = "Si vous avez été inoccupé que ceci pourrait être pourquoi vous avez été expiré. Vous aussi pouvez avoir été donné un coup de pied hors par un administrateur ou un opérateur.";
+
+$LOCALE[HELP_ERROR_RoomLong] = "La salle nomme vous avez choisi êtes trop long. Le caractère maximum le limite 15 caractères.";
+
+$LOCALE[HELP_Live] = "Le système de conversation est autonome. Ceci pourrait être parce qu'un administrateur fait des montées.";
+
+$LOCALE[HELP_ERROR_Profanity] = "Vous avez été hors noté pour utiliser d'impiété dans la conversation. L'impiété n'est pas permise.";
+
+$LOCALE[HELP_ERROR_NoData] = "Vous n'avez pas rempli correctement tous champs. S'il vous plaît essayer encore.";
+
+$LOCALE[HELP_ERROR_BadData] = "L'information vous êtes entré de contenus mauvais caractères. S'il vous plaît l'usage seulement alpha caractères numériques.";
+
+$LOCALE[HELP_ERROR_Serial_Invalid] = "Le numéro de série votre fourni n'égale pas votre serveur. Vous aurez besoin de recevoir un nouveau numéro de série et vérifie l'adresse DU IP de VOTRE webserver.";
+
+$LOCALE[HELP_ERROR_Serial_None] = "L'Appindex filet n'a pas un numéro de série sur le disque pour vous. S'il vous plaît enregistrer votre produit et reçoit un nouveau numéro de série.";
+
+$LOCALE[HELP_ERROR_Critical] = "Une erreur critique inconnue a arrivé. Si le problème persiste, le soutien d'Appindex Filet de contact.";
+
+$LOCALE[HELP_ERROR_Javascript] = "MWChat exige un browser de toile capable de montrer de javascript. S'il vous plaît rend capable javascript dans votre webbrowser ou la montée.";
+
+$LOCALE[HELP_ERROR_Browser] = "Le browser de toile vous utilisez dans pas compatible avec MWChat. Nous recommandons Netscape qui utilise ou l'Internet Explorateur.";
+
+$LOCALE[HELP_ERROR_Restricted] = "Le username que vous avez choisi est un nom limité ou réservé. S'il vous plaît choisir un autre nom et login encore.";
+
+$LOCALE[HELP_ERROR_BadPass] = "Le mot de passe que vous avez fourni est inexact. Vérifier avec un administrateur pour les plus amples détails.";
+
+$LOCALE[HELP_ERROR_IP] = "MWChat a détecté que votre adresse de IP a changé. Pour les raisons de sécurité, nous vous avons noté hors. S'il vous plaît login encore.";
+
+$LOCALE[HELP_COMMAND_addbuddy] = "Ajouter un copain à votre liste de copain. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_admin] = "Permet des administrateur aux utilisateurs de unregister ou les journaux de bord de vue. Spécifier la deuxième option (username) regarder des journaux de bord pour un utilisateur de specfic ou à unregister cet utilisateur.";
+
+$LOCALE[HELP_COMMAND_afk] = "Change votre mode à éloigné du clavier. Vous pouvez spécifier un facultatif loin message.";
+
+$LOCALE[HELP_COMMAND_block] = "Bloque tous messages privés de n'importe quels utilisateurs.";
+
+$LOCALE[HELP_COMMAND_bug] = "Envoyer un rapport d'insecte.";
+
+$LOCALE[HELP_COMMAND_about] = "Regarder l'information de MWChat et les créateurs.";
+
+$LOCALE[HELP_COMMAND_clear] = "Eclaircit la fenêtre actuelle de conversation.";
+
+$LOCALE[HELP_COMMAND_color] = "Régler la couleur de texte à une couleur de votre choix. Cette couleur doit être dans #000000 met en format.";
+
+$LOCALE[HELP_COMMAND_deop] = "Enlever un statut d'opérateurs d'utilisateurs. Cet ordre peut être utilisé seulement par les administrateur et les opérateurs.";
+
+$LOCALE[HELP_COMMAND_global] = "Envoyer un message à tous utilisateurs dans toutes salles. Cet ordre peut être utilisé seulement par un administrateur.";
+
+$LOCALE[HELP_COMMAND_help] = "Montre ce document.";
+
+$LOCALE[HELP_COMMAND_ignore] = "Ajouter un utilisateur à votre néglige la liste. Cet utilisateur ne peut pas vous envoyer de message privés et vous ne pouvez pas voir de message qu'ils envoient. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_unignore] = "Enlever un utilisateur de votre néglige la liste. Cet utilisateur peut vous envoyer maintenant des messages privés et vous pouvez voir des messages qu'ils envoient. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_kick] = "Donne un coup de pied un utilisateur du système. Vous pouvez spécifier une raison facultative. Cet ordre peut être utilisé seulement par les administrateur ou les opérateurs.";
+
+$LOCALE[HELP_COMMAND_lobby] = "Vous retourne au vestibule. Vous pouvez spécifier un message facultatif de sortie.";
+
+$LOCALE[HELP_COMMAND_locate] = "Localise un utilisateur spécifié. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_logout] = "Note un utilisateur du système. Vous pouvez spécifier un message facultatif de sortie.";
+
+$LOCALE[HELP_COMMAND_me] = "Décrire que vous faites sans vous réfère à vous directement.";
+
+$LOCALE[HELP_COMMAND_motd] = "Montre le message du jour.";
+
+$LOCALE[HELP_COMMAND_myfiles] = "Montre vos fichiers en ligne.";
+
+$LOCALE[HELP_COMMAND_op] = "Donner un statut d'opérateur d'utilisateur. Cet ordre peut être utilisé seulement par les administrateur et les opérateurs.";
+
+$LOCALE[HELP_COMMAND_prefs] = "Montre vos préférences de MWChat.";
+
+$LOCALE[HELP_COMMAND_print] = "Montre vos conversations dans une fenêtre imprimable.";
+
+$LOCALE[HELP_COMMAND_msg] = "Envoyer un message privé à un utilisateur. Noone peut voir autrement ce message.";
+
+$LOCALE[HELP_COMMAND_register] = "Enregistre le username actuel votre utilisation. Enregistrer permet de l'aux utilisateurs pour partager des fichiers et crée les listes de copain.";
+
+$LOCALE[HELP_COMMAND_removebuddy] = "Enlève un copain de votre liste de copain. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_roll] = "Roule les dés de salle de conversation. Ceci est utile pour le jeu en ligne.";
+
+$LOCALE[HELP_COMMAND_showbuddy] = "Montre tous utilisateurs sur votre liste de copain. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_showignored] = "Montre tous utilisateurs sur votre néglige la liste. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_smiley] = "Montre toutes faces disponibles de smiley.";
+
+$LOCALE[HELP_COMMAND_topic] = "Change le sujet de salle. Cet ordre peut être utilisé seulement par les administrateur et les opérateurs.";
+
+$LOCALE[HELP_COMMAND_unafk] = "Change votre mode pour soutenir d'éloigné du clavier. Vous pouvez spécifier un message postérieur facultatif.";
+
+$LOCALE[HELP_COMMAND_unblock] = "Vous permet d'accepter des messages privés de tous utilisateurs encore.";
+
+$LOCALE[HELP_COMMAND_topic] = "Enlève un utilisateur de votre néglige la liste. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_unregister] = "Unregisters votre username. Ceci le fera disponible pour n'importe qui utiliser. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_upload] = "Montre le fichier interface qui partage. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_url] = "Vous permet d'insérer un URL dans la conversation.";
+
+$LOCALE[HELP_COMMAND_whois] = "L'information d'expositions de l'utilisateur spécifié. Cet ordre peut être utilisé seulement par les utilisateurs enregistrés.";
+
+$LOCALE[HELP_COMMAND_Title] = "Ordres disponibles";
+
+$LOCALE[HELP_COMMAND_Type] = "{} indique une option exigée, [] indique une option facultative.";
+
+?>
