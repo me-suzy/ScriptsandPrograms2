d614b1da906e3faa444f11c17845ac4ebe0a84b8
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:28 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.query.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.query.php
@@ -0,0 +1,21 @@
+<?php
+
+$L = array(
+
+//----------------------------------------
+// Required for MODULES page
+//----------------------------------------
+
+"query_module_name" =>
+"Abfrage",
+
+"query_module_description" =>
+"SQL-Abfragemodul f&uuml;r Vorlagen",
+
+//----------------------------------------
+
+
+// END
+''=>''
+);
+?>
