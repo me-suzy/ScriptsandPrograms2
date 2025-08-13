7f5cf0f471520131bda5fa120f522838318bcb45
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:41 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-pl-list.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-pl-list.php
@@ -0,0 +1,24 @@
+<?php
+
+##################################################
+
+# /pl-list.php
+
+##################################################
+
+define( "PLIST_PLAY_HELP", "Feu clic per reproduir aquesta llista." );
+define( "PLIST_EDIT_HELP", "Feu clic per modificar aquesta llista" );
+define( "PLIST_VIEW_HELP", "Feu clic per veure aquesta llista" );
+
+define( "PLIST_PRVT_HEADER", "LLISTES PRIVADES DE CANÇONS");
+define( "PLIST_PRVT_HELP", "Les llistes privades de cançons sols es poden veure, modificar o editar a través del vostre compte." );
+
+define( "PLIST_SHRD_HEADER", "LLISTES COMPARTIDES" );
+define( "PLIST_SHRD_HELP", "Les llistes compartides sols les pot modificar o esborrar l'usuari registrat, però estan disponibles per a la resta a la secció comunitària." );
+
+define( "PLIST_FAV_HEADER", "LLISTES COMUNITÀRIES" );
+define( "PLIST_FAV_HELP", "Les llistes comunitàries es comparteixen amb altres usuaris i s'hi pot accedir a través de la secció comunitària." );
+
+##################################################
+
+?>
