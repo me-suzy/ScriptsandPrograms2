03d3f1594d8b105d7aee88833467db8a8180ec9c
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:04 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-lib_inc-jscript.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-lib_inc-jscript.php
@@ -0,0 +1,16 @@
+<?php
+
+##################################################
+
+# /lib/inc-jscript.php
+
+# UNUSED SINCE 1.0b9, VALUES NOW IN inc-lib_inc-common.php
+
+##################################################
+
+define( "JSCRIPT_PLIST_1", "Please select an existing playlist to be edited.\\nOr Select a few tracks and create a new one." );
+define( "JSCRIPT_PLIST_2", "You did not select any track from the list below." );
+
+##################################################
+
+?>
