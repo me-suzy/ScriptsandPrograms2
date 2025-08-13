2b1d3979b9d4a8ed99ffa94366b6988bb9dd4628
me-suzy <me.suzana@gmail.com>
2025-08-13 06:53:45 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_user-list.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-admin_user-list.php
@@ -0,0 +1,23 @@
+<?php
+
+##################################################
+
+# /admin/user-list.php
+
+##################################################
+
+define( "USLIST_EDIT_HELP", "Click To Edit This user" );
+define( "USLIST_BTN_NEW", "New Account" );
+
+define( "USLIST_US_HEADER", "USERS" );
+define( "USLIST_US_HELP", "Users have access to all the music, their account preferences, their playlists and the community." );
+
+define( "USLIST_ED_HEADER", "EDITORS" );
+define( "USLIST_ED_HELP", "Editors have the same access level as users, but can also edit tracks as well as add/edit artists, albums and genres." );
+
+define( "USLIST_AD_HEADER", "ADMINISTRATORS" );
+define( "USLIST_AD_HELP", "Administrators have unrestricted access to all the features throughout the application. You currently are an Administrator." );
+
+##################################################
+
+?>
