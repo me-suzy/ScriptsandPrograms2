b7c3bded53f863b6f1a7ff4a1eaf9866ac52db08
me-suzy <me.suzana@gmail.com>
2025-08-13 06:54:20 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-random.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/en/inc-random.php
@@ -0,0 +1,27 @@
+<?php
+
+##################################################
+
+# /random.php
+
+##################################################
+
+define( "RNDM_NO_TR", "You must have at least 2 tracks in this\\ngenre(s) before using the Random feature." );
+
+define( "RNDM_FORM_HEADER", "RANDOM MUSIC SELECTOR" );
+define( "RNDM_FORM_LIMIT_1", "Limit Playlist To" );
+define( "RNDM_FORM_LIMIT_2", "Tracks (Maximum)" );
+define( "RNDM_FORM_GENRES", "Limit Genres To" );
+define( "RNDM_FORM_GENRES_ALL", "All Genres" );
+define( "RNDM_FORM_TRACKMINTIME_1", "Min. Track Time" );
+define( "RNDM_FORM_TRACKMINTIME_2", "Seconds per track" );
+define( "RNDM_FORM_TRACKMAXTIME_1", "Max. Track Time" );
+define( "RNDM_FORM_TRACKMAXTIME_2", "Seconds per track" );
+define( "RNDM_FORM_BTN_SEARCH", "Generate Random Playlist" );
+
+define( "RNDM_CLOSEWIN", "Close Window" );
+define( "RNDM_CLOSEWIN_HELP", "Closes This Window" );
+
+##################################################
+
+?>
