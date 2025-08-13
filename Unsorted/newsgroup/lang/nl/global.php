829f59cc66d8357cdb2687de1dd9b098ffc5e11c
me-suzy <me.suzana@gmail.com>
2025-08-13 07:03:28 +0300

    Add Unsorted/newsgroup/lang/nl/global.php

---
+++ Unsorted/newsgroup/lang/nl/global.php
@@ -0,0 +1,288 @@
+<?
+// Language:                    Dutch
+// Translator:                  Garry
+// Created:                     18/02/2002
+// Last Update:                 20/04/2002
+// MyNewsGroepen  Version:     0.5.3
+
+
+// Charset
+define("_CHARSET","charset=iso-8859-1");
+
+// HOW THE PHP date() function displays the date
+define("_MYNGDATEDISPLAY","d-m-Y"); 
+
+// Header
+define("_MYNGWELCOME"," Welkom bij MijnNewsGroepen ");
+define("_MYNGSYSTEM_INFO"," Systeem Info:");
+define("_MYNGMENU_HOME"," Hoofd Pagina ");
+define("_MYNGMENU_NEWSGROUPS"," NewsGroepen ");
+define("_MYNGMENU_STATS"," Statestieken ");
+define("_MYNGMENU_SEARCH"," Zoeken ");
+define("_MYNGMENU_ABOUT"," Admin ");
+
+// Footer
+define("_MYNGHOME_PAGE","Project Hoofd Pagina:");
+define("_MYNGGPL","GPL Licensed");
+define("_MYNGDEBUG","Debug?");
+
+// Login
+define("_MYNGLOGIN","Inloggen");
+define("_MYNGPASSWORD","Paswoord");
+define("_MYNGREGISTER","Inschijving");
+
+// NewsGroepen
+define("_MYNGBROWSE_NG","Blader in de NewsGroepen");
+define("_MYNGARTICLE_NUMBER","Aantal");
+define("_MYNGGROUP_NAME","Groep Naam");
+define("_MYNGDESCRIPTION","Omschrijving");
+define("_MYNGSUBSCRIBE_GROUP","Inschrijven op deze groep");
+
+// Register
+define("_MYNGFILL_FORM","Voer u gegevens in en verzend het om
+te registreren.");
+define("_MYNGUSER_NAME","Gebruikers naam");
+define("_MYNGPASSWORD","Paswoord");
+define("_MYNGEMAIL","Email");
+define("_MYNGPASSWORD_AGAIN","Nogmaals de paswoord");
+// Phrase: 'Alle velden zijn verplicht'
+define("_MYNGALL_FIELDS","Alle velden zijn");
+define("_MYNGREQUIRED","verplicht");
+define("_MYNGVISIBLE","Zichtbaar");
+define("_MYNGYES","Ja");
+define("_MYNGNO","Nee");
+define("_MYNGCOUNTRY","Land");
+define("_MYNGINSERT_VALID_EMAIL","Vul aub hier een geldige email adres in, het
+woord gecontroleerd.");
+define("_MYNGPASSWORD_TRANSFER","Je email adres kan gezien worden door andere
+abbonees, maar het is wel SPAM bots beveiligt, zelfs wanneer je een nieuw artikel
+plaatst.");
+define("_MYNGSPAM_PROTECTED","Je paswoord zal niet verzonden worden over het
+Internet.");
+define("_MYNGSECURE_LOGIN","Onze registratie is beveiligt, slechts een klein
+gedeelte zal zichtbaar zijn bij medegebruikers.");
+define("_MYNGREGISTRATION_COMPLETE","Registratie voldaan");
+define("_MYNGTHANKS","Dank je!");
+define("_MYNGDATA_STORED","Je gegevens zijn goed verzonden in het
+databestand.");
+define("_MYNGEDIT_INFO","Je kan je gegevens veranderen in je gebruikers
+pagina.");
+define("_MYNGSUBMITTED_INFO","Deze gegevens had je verzonden naar ons:");
+define("_MYNGREGISTER_WELCOME","Welkom op je nieuwe plek!");
+define("_MYNGSTAFF","De MyNewsGroepen  Beheer.");
+define("_MYNGLOGIN_TRANSFER","Als je ingelogged bent, je paswoord zal niet verzonden
+worden over de Internet.");
+define("_MYNGCOOKIES","Cookies moeten aangeschakeld zijn om in te kunnen
+loggen.");
+
+// Login
+define("_MYNGHOME","Hoofd pagina");
+define("_MYNGWELCOME_USER","Welkom");
+define("_MYNGPOST","Post");
+define("_MYNGREPLY","Beantwoorden");
+define("_MYNGSAVE","Opslaan");
+define("_MYNGSTATS","Statestieken");
+define("_MYNGSUBSCRIBE","Aanmelden");
+define("_MYNGOPTIONS","Optie's");
+define("_MYNGPROFILE","Omschijving");
+define("_MYNGNEW_ARTICLES","Nieuwe Postingen");
+define("_MYNGTO_ARTICLES","Naar de Postingen");
+define("_MYNGEDIT_YOUR","Bewerk je");
+define("_MYNGTO_GROUPS","naar nieuwe groep");
+define("_MYNGYOUR_ARTICLES","je favorieten artikelen");
+define("_MYNGVIEW_YOUR","bekijk je eigen");
+
+// My Bar
+define("_MYNGMY_BOX","Mijn plek");
+define("_MYNGMY_HOME","Mijn pagina");
+define("_MYNGMY_GROUPS","Mijn Groepen");
+define("_MYNGMY_ARTICLES","Mijn Postingen");
+define("_MYNGLOG_OUT","Uit Loggen");
+define("_MYNGMY_STATS","Mijn Statestieken");
+
+// Tree
+define("_MYNGNEWSGROUP_UNSELECTED","Geen newsgroep geselecteert");
+define("_MYNGPOST_ARTICLE","Plaats een artikel");
+define("_MYNGDATE","Datum");
+define("_MYNGSUBJECT","Onderwerp");
+define("_MYNGAUTHOR","Uitgever");
+define("_MYNGARTICLES","Aantal");
+define("_MYNGNEXT","Volgende");
+define("_MYNGPREVIOUS","Vorige");
+define("_MYNGMARK_ALL_READ","Markeer als gelezen");
+define("_MYNGMARK_THREAD_READ","Markeer onderwerp als gelezen");
+
+// Article
+define("_MYNGREPLY","Beantwoord");
+define("_MYNGSEND_FRIEND","Verzend naar een vriend");
+define("_MYNGPRINT","Print");
+define("_MYNGSAVE_ARTICLE","Sla deze artikel op");
+
+// Search Engine
+define("_MYNGSEARCH_RESULTS","Zoek resultaat");
+define("_MYNGNOARTICLES_FOUND","Sorrie, Er zijn geen postingen gevonden met");
+define("_MYNGSEARCH_ENGINE","Zoek machine");
+define("_MYNGSEARCH_ONE_GROUP","Zoek in een groep");
+define("_MYNGSEARCH_ALL_GROUPS","Zoek in alle groepen");
+define("_MYNGAND_OPERATION","Als je meer dan een woord invoert, een AND is er
+aan toegevoegt.");
+define("_MYNGPOWERED_BY","Powered by ");
+define("_MYNGGO","ZOEKEN!");
+
+// Search Results
+define("_MYNGGROUP","Groep");
+define("_MYNGREADINGS","Gelezen");
+
+// Stats
+define("_MYNGHOT_NG","Heetste NewsGroepen");
+define("_MYNGPOPULAR_NG","Meest Populaire NewsGroepen");
+define("_MYNGCROWDED_NG","Meest Bevolkte NewsGroepen");
+define("_MYNGHOT_ARTICLES","Heetste Postingen");
+define("_MYNGPOPULAR_ARTICLES","Meest Populaire Postingen");
+define("_MYNGACTIVE_USER","Meest Actieve gebruikers");
+define("_MYNGARG_USER","Gebuiker die het meest heeft beantwoord");
+define("_MYNGSTATS_ARTICLES","artikels");
+define("_MYNGSTATS_READINGS","gelezen");
+define("_MYNGUSERS","gebruikers");
+define("_MYNGPOSTINGS","keer gepost");
+define("_MYNGREPLIES","antwoorden");
+
+// My Groups
+define("_MYNGMUST_LOGIN","Je moet inloggen om gebruik te maken van deze
+opties.");
+define("_MYNGSUBSCRIPTION_SYSTEM","Inschijvings Systeem");
+define("_MYNGCHOOSE_GROUP","Kies een groep");
+define("_MYNGSURE","Ja.. ik ben er zeker van");
+define("_MYNGGROUPS_SUBSCRIBED","Op deze groepen ben je ingeschreven");
+define("_MYNGNEW","Nieuw");
+define("_MYNGUNREAD","Ongelezen");
+define("_MYNGPOSTED","Gepost");
+define("_MYNGADD","Toegevoegt");
+define("_MYNGDEL","Verwijdert");
+
+// My Articles
+define("_MYNGARTICLES_SAVED","Dit zijn de Postingen die zijn opgeslagen");
+
+// Print article
+define("_MYNGPRINT_ARTICLE","Print deze artikel");
+define("_MYNGUSER_AGENT","User Agent");
+define("_MYNGGENERATED_BY","Gegenereerd door");
+define("_MYNGFROM","Van");
+
+// Email article
+
+define("_MYNGEMAIL_SENT","Email was goed verzonden");
+define("_MYNGEMAIL_NOT_SENT","Er is een fout: Email is niet goed verzonden");
+define("_MYNGEMAIL_FRIEND","Email deze artikel naar een vriend");
+define("_MYNGSENDYOU_FROM"," heeft je deze artikel verzonden van ");
+define("_MYNGDEAR_FRIEND","Beste (_friend_name)");
+// the string '_friend_name' it must be the same in '_MYNGDEAR_FRIEND' and in
+'_MYNGFRIEND_NAME'.
+define("_MYNGFRIEND_NAME","\(_friend_name\)");
+define("_MYNGYOUR_FRIEND","Je vriend ");
+define("_MYNGWANT2SHARE",", wil het volgende  \nmet je delen namelijk deze artikel
+van ");
+define("_MYNGNSERT_FRIEND_INFO","Voeg je vriend's informatie toe");
+define("_MYNGMAIL_MUSTBE_REAL","Note: De mail adres moet een echte zijn, het
+zal worden gecontroleert, <br>&nbsp; maar het zl niet gepingt worden en
+of opgeslagen.");
+define("_MYNGNAME","Naam");
+define("_MYNGEMAIL_LOOK","De mail zal er zo ongeveer uit zien");
+define("_MYNGEMAIL_IT","Email het!");
+
+// Post
+define("_MYNGMISSING_EMAIL","Je bent een email adres vergeten.");
+define("_MYNGMISSING_MESSAGE","Je het bericht vergeten erbij te voegen.");
+define("_MYNGWRONG_EMAIL","Het email adres is verkeerd");
+define("_MYNGMISSING_NAME","Je bent een naam en of roepnaam vergeten.");
+define("_MYNG_MISSING_SUBJECT","Je bent een onderwerp vergeten.");
+define("_MYNGMESSAGE_POSTED","Het bericht is goed verzonden.");
+define("_MYNGERROR_SERVER","De newsserver heeft het bericht niet
+geaccepteerd.");
+define("_MYNGREAD_ONLY","De groep is tegen schrijven beveiligt");
+define("_MYNGWROTE"," screef:\n\n");
+define("_MYNGFOLLOWUP_NOT_ALLOWED","Je heb geen toegang om op deze groep te
+posten: ");
+define("_MYNGPOST_FAILED","Connectie gaat fout. Probeer het later nog eens.");
+define("_MYNGFOOTER1","         Article posted with MyNewsGroup ");
+define("_MYNGFOOTER2","         http://myNewsGroepen.sourceforge.net");
+define("_MYNGFOOTER3","         Share your knowledge in... ");
+define("_MYNGFILL_FORM2POST","Vul dit in om een nieuw artikel te posten");
+define("_MYNGEMAIL_MUSTBE_REAL","Note: Het email adres moet de echte zijn, het
+zal gecontroleert worden");
+define("_MYNGPOST","Verzenden");
+define("_MYNGPOSTING_COMPLETE","Verzending gelukt");
+define("_MYNGTHANKX","Bedankt!");
+define("_MYNGPOSTED_SUCCESFULLY","Je artikel is goed verzonden");
+define("_MYNGINFO_SUBMITTED","Dit is de informatie die je verzonden had naar
+deze news server");
+define("_MYNGBODY","Body");
+define("_MYNGCLICK2RETURN","klik Hier om terug te gaan");
+define("_MYNGEMAIL_SPAM_PROTECTED","Je email was voordat het verzonden werd
+beveiligt tegen SPAM bots");
+define("_MYNGEMAIL_CHANGED","Je email adres zal zo ongeveer te zien zijn");
+
+// Error template
+define("_MYNGSOMETH_WRONG","Er is een fout opgetreden...");
+define("_MYNGERROR_MESSAGE","Fout bericht");
+define("_MYNGHAVE_DOUBT","Bij twijfel, neem dan contact op met de Systeem
+beheerder");
+define("_MYNGERROR","Fout!");
+
+// Admin
+define("_MYNGADMIN_LOGIN","Je moet ingelogged zijn als System beheerder.");
+define("_MYNGADMIN","Administratie");
+define("_MYNGENTER_GROUP_NAME","Voer een groep naam in");
+define("_MYNGADD_GROUP","Voeg groep toe");
+define("_MYNGCHOOSE_SERVER","Kies een server");
+define("_MYNGARTICLES2FETCH","Aantal Postingen die nog binne gehaalt moeten
+worden");
+define("_MYNGDELETE_GROUP","Verwijder groep");
+define("_MYNGDELETE","Verwijder");
+define("_MYNGEDIT_GROUP","Bewerk groep");
+define("_MYNGALLOW_POSTING","Posten toestaan");
+define("_MYNGEDIT","Bewerk");
+define("_MYNGADD_SERVER","Voeg een news server toe");
+define("_MYNGHOST","Host");
+define("_MYNGPORT","Poort");
+define("_MYNGDELETE_SERVER","Verwijder news server");
+define("_MYNGALL","Alle");
+
+
+// Errors
+define("_MYNGNOGROUPS","Er zijn op dit moment geen groepen geregistreerd.");
+define("_MYNGCON_ERROR","Connectie Fout!");
+define("_MYNGUSERNAME_TAKEN","Sorrie, de naam word al gebruikt door een andere
+gebruiker.");
+define("_MYNGEMAIL_TAKEN","Sorrie, je email adres word al gebruikt door een
+andere gebruiker.");
+define("_MYNGLOGIN_INCORRECT","Sorrie, het inloggen ging niet goed, probeer
+het nog is?.");
+define("_MYNGINVALID_EMAIL","Sorrie, dit email adres is niet geldig. Voer een
+geldige email adres in.");
+define("_MYNGINVALID_PASSWORD","Sorrie, het paswoord komt niet overheen.");
+define("_MYNGBLANK_PASSWORD","AUB, Voeg een paswoord in.");
+define("_MYNGSERVER_REJECT","Jou mail server weigerde dit address");
+define("_MYNGNO_RESPONSE","Geen reactie van jou mail server");
+define("_MYNGCANT_CONNECT_SERVER","Kan niet connecten met de mail server");
+define("_MYNGAPPEARS_VALID"," blijkt goed te zijn");
+define("_MYNGNOT_PROPERLY_FORMATTED"," is niet goed ingevoerd");
+define("_MYNGNO_GROUP","Groep niet aanwezig bij ");
+define("_MYNGUNKNOWN_GROUP"," Deze groep hoort niet bij deze Systeem.");
+
+
+// Greets
+define("_MYNGREGISTRATION_COMPLETED","Je registratie is goed gelukt.");
+define("_MYNGLOGGED","Je bent nu ingelogged!");
+define("_MYNGLOGGED_OUT","Je bent nu uitgelogged.");
+
+
+// 2002 - 04 - 22
+define("_MYNGDELETE_ARTICLES","Verwijder Postingen");
+define("_MYNGDELETE_LASTN","Verwijder de oudste Postingen: ");
+
+// 2002 - 05 - 03
+define("_MYNGMY_ADMIN","Admin");
+
+
+?> 
