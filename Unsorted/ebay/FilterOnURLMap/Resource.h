e70eaec4349a93aadc86af8a9285f6075249e929
me-suzy <me.suzana@gmail.com>
2025-08-13 03:14:18 +0300

    Add Unsorted/ebay/FilterOnURLMap/Resource.h

---
+++ Unsorted/ebay/FilterOnURLMap/Resource.h
@@ -0,0 +1,12 @@
+/*	$Id: Resource.h,v 1.2 1999/02/21 02:22:04 josh Exp $	*/
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by FilterOnURLMap.rc
+//
+
+#define IDS_FILTER        101
+
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_CONTROL_VALUE         103
+#define _APS_NEXT_SYMED_VALUE           103
+#define _APS_NEXT_COMMAND_VALUE         32768
