3505d2a960c70ae83666628580434a9c68dd4646
me-suzy <me.suzana@gmail.com>
2025-08-13 07:12:42 +0300

    Add Unsorted/osCAffiliate v2.02/catalog/includes/languages/english/affiliate_news.php

---
+++ Unsorted/osCAffiliate v2.02/catalog/includes/languages/english/affiliate_news.php
@@ -0,0 +1,29 @@
+<?php
+/*
+  $Id: affiliate_banners.php,v 2.00 2003/10/12
+
+  OSC-Affiliate
+
+  Contribution based on:
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2002 - 2003 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Affiliate Program');
+define('HEADING_TITLE', 'Affiliate News');
+
+define('TABLE_HEADING_AFFILIATE_NEWS', 'Affiliate News');
+
+define('TEXT_AFFILIATE_NAME', 'Banner Name:');
+define('TEXT_INFORMATION', 'Choose the banner or link you want to display on your website from the choices below:');
+define('TEXT_AFFILIATE_INFO', 'Copy the code shown below and paste into your website');
+define('TEXT_AFFILIATE_INDIVIDUAL_BANNER', 'BUILD-A-LINK');
+define('TEXT_AFFILIATE_INDIVIDUAL_BANNER_INFO', 'Enter the product number you wish to link to.');
+
+define('IMAGE_BUTTON_BUILD_A_LINK','Build a Link for a Product Banner.');
+?>
