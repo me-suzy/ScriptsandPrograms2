c9435392de84aed3848fb45d78d7f460b3d82afb
me-suzy <me.suzana@gmail.com>
2025-08-13 06:52:49 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-lib_inc-header.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/de/inc-lib_inc-header.php
@@ -0,0 +1,36 @@
+<?php
+
+##################################################
+
+# /lib/inc-header.php
+
+##################################################
+
+define( "TB1_BROWSE", "SUCHE" );
+define( "TB1_BROWSE_HELP", "Klicken um zurück zur Startseite zu gelangen." );
+define( "TB1_SEARCH", "ERWEITERTE SUCHE" );
+define( "TB1_SEARCH_HELP", "Klicken um zur Erwiterten Suche zu gelangen" );
+define( "TB1_RANDOM", "ZUFALL" );
+define( "TB1_RANDOM_HELP", "Klicken um zur zufälligen Musikauswahl zu gelangen." );
+define( "TB1_COMMUNITY", "GEMEINSCHAFT" );
+define( "TB1_COMMUNITY_HELP", "Klicken um zum Gemeinschaftsbereich zu gelangen." );
+define( "TB1_RADIO", "RADIO" );
+define( "TB1_RADIO_HELP", "Klicken um auf einen verknüpften Internet Radio Stream zu gelangen." );
+define( "TB1_PLISTS", "SPIELLISTEN" );
+define( "TB1_PLISTS_HELP", "Klicken um bestehende Spiellisten zu betrachten/bearbeiten." );
+define( "TB1_ACCOUNT", "KONTO" );
+define( "TB1_ACCOUNT_HELP", "Klicken um Ihr Benutzerkonto zu betrachten/bearbeiten." );
+define( "TB1_PREFS", "EINSTELLUNGEN" );
+define( "TB1_PREFS_HELP", "Klicken um Ihre Einstellungen zu betrachten/bearbeiten." );
+define( "TB1_SYSADMIN", "VERWALTUNG" );
+define( "TB1_SYSADMIN_HELP", "Klicken um zur Verwaltung zu gelangen." );
+define( "TB1_LOGIN", "ANMELDEN" );
+define( "TB1_LOGIN_HELP", "Klicken Sie hier um sich anzumelden." );
+define( "TB1_LOGOUT", "ABMELDEN" );
+define( "TB1_LOGOUT_HELP", "Klicken um sich nun abzumelden." );
+define( "TB1_REGISTER", "REGISTRIEREN" );
+define( "TB1_REGISTER_HELP", "Klicken um ein neues Konto zu beantragen." );
+
+##################################################
+
+?>
