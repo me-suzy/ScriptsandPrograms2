ce8c4c65b396d143c634602430cf1e73f88c9735
me-suzy <me.suzana@gmail.com>
2025-08-13 04:19:10 +0300

    Add Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.login.php

---
+++ Unsorted/ExpressionEnginePB2/system/language/deutsch/lang.login.php
@@ -0,0 +1,36 @@
+<?php
+
+$L = array(
+
+//----------------------------
+// Log-in
+//----------------------------
+
+"remember_me" =>
+"Auto-Log-in bei k&uuml;nftigen Besuchen?",
+
+"no_username" =>
+"Der angegebene Username ist nicht in der Datenbank zu finden.",
+
+"no_password" =>
+"Das angegebene Passwort ist nicht korrekt.",
+
+"no_email" =>
+"Sie m&uuml;ssen Ihre Email-Adresse angeben.",
+
+"multi_login_warning" =>
+"Jemand anderes ist schon unter diesem Namen eingeloggt.",
+
+"return_to_login" =>
+"Zur&uuml;ck zu Login",
+
+"password_lockout_in_effect" =>
+"Sie d&uuml;rfen innerhalb von %x Minuten nur vier Anmeldeversuche starten.",
+
+"unauthorized_request" =>
+"Sie haben die f&uuml;r diese Aktion n&ouml;tige Berechtigung leider nicht.",
+
+// END
+''=>''
+);
+?>
