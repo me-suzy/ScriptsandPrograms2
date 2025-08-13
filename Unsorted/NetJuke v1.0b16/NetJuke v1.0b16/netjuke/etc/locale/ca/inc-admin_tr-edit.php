28d642826dd9715acb8033e6fbc4937cc6b9a01e
me-suzy <me.suzana@gmail.com>
2025-08-13 06:51:11 +0300

    Add Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_tr-edit.php

---
+++ Unsorted/NetJuke v1.0b16/NetJuke v1.0b16/netjuke/etc/locale/ca/inc-admin_tr-edit.php
@@ -0,0 +1,70 @@
+<?php
+
+##################################################
+
+# /admin/tr-edit.php
+
+##################################################
+
+define( "TREDIT_DELETED_HELP", "La pista seleccionada ha estat esborrada." );
+
+define( "TREDIT_DELCHILD_HELP", "Aquest valor Ès obligatori i no es pot esborrar." );
+
+define( "TREDIT_HEADER", "EDICIÓ DE PISTA" );
+
+define( "TREDIT_ERROR_INSERT", "Hi ha hagut un error d'inserciÛ en" );
+define( "TREDIT_ERROR_TR_T", "Hi ha hagut un error en l'actualitzaciÛ de la taula netjuke_tracks" );
+define( "TREDIT_ERROR_AR_T", "Hi ha hagut un error en l'actualitzaciÛ de la taula netjuke_artists" );
+define( "TREDIT_ERROR_AL_T", "Hi ha hagut un error en l'actualitzaciÛ de la taula netjuke_albums" );
+define( "TREDIT_ERROR_GE_T", "Hi ha hagut un error en l'actualitzaciÛ de la taula netjuke_genres" );
+define( "TREDIT_ERROR_TR", "Si us plau, introduÔu un nom de fitxer" );
+define( "TREDIT_ERROR_TN", "Si us plau, introduÔu un número de pista." );
+define( "TREDIT_ERROR_BR", "El bit rate Ès obligatori i ha de ser superior a 0." );
+define( "TREDIT_ERROR_SR", "El sample rate Ès obligatori i ha de ser superior a 0." );
+define( "TREDIT_ERROR_FK", "Si us plau, introduÔu un tipus de pista." );
+define( "TREDIT_ERROR_LC", "Si us plau, indiqueu la ubicaciÛ de la pista." );
+define( "TREDIT_ERROR_NORES", "Si us plau, seleccioneu una pista vàlida" );
+
+define( "TREDIT_FORM_CM", "Edita els comentaris" );
+define( "TREDIT_FORM_LY", "Edita les lletres" );
+
+define( "TREDIT_FORM_NEW", "Nou" );
+
+define( "TREDIT_FORM_TR", "TÃtol de la canÁÛ" );
+define( "TREDIT_FORM_AR", "Nom de l'intÀrpret" );
+define( "TREDIT_FORM_AL", "TÃtol de l'àlbum" );
+define( "TREDIT_FORM_GE", "Nom del gÀnere" );
+define( "TREDIT_FORM_TI", "Durada de la pista" );
+define( "TREDIT_FORM_SECONDS", "Segons" );
+define( "TREDIT_FORM_TN", "Número de pista" );
+define( "TREDIT_FORM_BR", "Bit rate" );
+define( "TREDIT_FORM_SR", "Sample rate" );
+define( "TREDIT_FORM_FK", "Tipus de fitxer" );
+define( "TREDIT_FORM_FS", "Mida del fitxer" );
+define( "TREDIT_FORM_FN", "UbicaciÛ" );
+define( "TREDIT_FORM_LC", "Comptador d'audicions" );
+define( "TREDIT_FORM_RESET", "Posa a zero" );
+
+define( "TREDIT_FORM_TR_IMG", "Icona de la pista" );
+define( "TREDIT_FORM_AR_IMG", "Icona de l'intÀrpret" );
+define( "TREDIT_FORM_AL_IMG", "Icona de l'àlbum" );
+
+define( "TREDIT_FORM_BTN_SAVE", "Desa" );
+define( "TREDIT_FORM_BTN_RESET", "Torna a comenÁar" );
+define( "TREDIT_FORM_BTN_DUP", "Duplicat" );
+define( "TREDIT_FORM_BTN_NEW", "Nova pista" );
+define( "TREDIT_FORM_BTN_DEL", "Esborra" );
+
+define( "TREDIT_FORM_BTN_DEL_CONF", "Esteu segur que voleu esborrar aquesta pista?" );
+define( "TREDIT_FORM_BTN_DELCHILD_CONF", "Esteu segur que voleu esborrar aquesta selecciÛ?" );
+
+define( "TREDIT_PLAY", "Reprodueix" );
+define( "TREDIT_PLAY_HELP", "Feu clic per reproduir aquesta canÁÛ." );
+define( "TREDIT_VIEW", "Veure informaciÛ" );
+define( "TREDIT_VIEW_HELP", "Canvia a la consulta d'informaciÛ." );
+define( "TREDIT_CLOSEWIN", "Tanca la finestra" );
+define( "TREDIT_CLOSEWIN_HELP", "Tanca aquesta finestra" );
+
+##################################################
+
+?>
